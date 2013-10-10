b5691ed51f84b6fa57ea1eec0cb870ae798a0a32
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-11 01:03:20 +0900

    Add hostname collector

---
+++ pkg/gohai/hostname/hostname.go
@@ -0,0 +1,15 @@
+package hostname
+
+import (
+	"os"
+)
+
+type Hostname struct{}
+
+func (self *Hostname) Collect() (result map[string]string, err error) {
+	hostname, err := os.Hostname()
+
+	return map[string]string{
+		"hostname": hostname,
+	}, err
+}
