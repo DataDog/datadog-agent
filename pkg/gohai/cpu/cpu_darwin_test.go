345d6914551c4393e5de09166f15763719f39b45
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-11 01:49:59 +0900

    Add cpu collector (not working yet)

---
+++ pkg/gohai/cpu/cpu_darwin_test.go
@@ -0,0 +1,21 @@
+package cpu
+
+import (
+	"debug/macho"
+	"fmt"
+	. "github.com/r7kamura/gospel"
+	"testing"
+)
+
+func TestCpu(t *testing.T) {
+	Describe(t, "cpu.Collect", func() {
+		collector := &Cpu{}
+		result, _ := collector.Collect()
+		cpu := (&macho.FileHeader{}).Cpu
+        fmt.Println(cpu.String())
+
+		It("should be able to collect hostname", func() {
+			Expect(result["cpu"]).To(Equal, cpu.String())
+		})
+	})
+}
