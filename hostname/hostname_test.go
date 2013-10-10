a9f3a4fb1fe60fe5e814e1537e6fbe212520ae17
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-11 01:03:20 +0900

    Add hostname collector

---
+++ hostname/hostname_test.go
@@ -0,0 +1,19 @@
+package hostname
+
+import (
+	. "github.com/r7kamura/gospel"
+	"os"
+	"testing"
+)
+
+func TestHostname(t *testing.T) {
+	Describe(t, "verity.Collector.Hostname.Collect", func() {
+		collector := &Hostname{}
+		result, _ := collector.Collect()
+		hostname, _ := os.Hostname()
+
+		It("should be able to collect hostname", func() {
+			Expect(result["hostname"]).To(Equal, hostname)
+		})
+	})
+}
