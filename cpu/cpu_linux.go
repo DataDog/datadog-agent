a3899452153b0f2feee8e635b8313f58a4865090
Gosuke Miyashita <gosukenator@gmail.com>
2013-10-11 17:26:18 +0900

    Fix indent

--- cpu/cpu_linux.go
+++ cpu/cpu_linux.go
@@ -35,14 +35,14 @@ func getCpuInfo() (cpuinfo map[string]string, err error) {
 }
 
 func regSplit(text string, delimeter string) []string {
-    reg := regexp.MustCompile(delimeter)
-    indexes := reg.FindAllStringIndex(text, -1)
-    laststart := 0
-    result := make([]string, len(indexes) + 1)
-    for i, element := range indexes {
-            result[i] = text[laststart:element[0]]
-            laststart = element[1]
-    }
-    result[len(indexes)] = text[laststart:len(text)]
-    return result
+	reg := regexp.MustCompile(delimeter)
+	indexes := reg.FindAllStringIndex(text, -1)
+	laststart := 0
+	result := make([]string, len(indexes) + 1)
+	for i, element := range indexes {
+		result[i] = text[laststart:element[0]]
+		laststart = element[1]
+	}
+	result[len(indexes)] = text[laststart:len(text)]
+	return result
 }
