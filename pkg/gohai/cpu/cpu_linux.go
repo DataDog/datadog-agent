ffbf420e0f78ed0c12534fc3100a1fa834bc2bdb
Gosuke Miyashita <gosukenator@gmail.com>
2013-10-11 16:45:54 +0900

    Change result format and how to handle /proc/cpuinfo

--- pkg/gohai/cpu/cpu_linux.go
+++ pkg/gohai/cpu/cpu_linux.go
@@ -2,17 +2,46 @@ package cpu
 
 import (
 	"io/ioutil"
+	"strings"
 	"regexp"
 )
 
 type Cpu struct{}
 
-func (self *Cpu) Collect() (result map[string]string, err error) {
-	re := regexp.MustCompile("model name\t: (.*)")
-	cpuinfo, err := ioutil.ReadFile("/proc/cpuinfo")
-	if err != nil { panic(err) }
-	cpu := re.FindStringSubmatch(string(cpuinfo))[1]
-	return map[string]string{
-		"cpu": cpu,
+func (self *Cpu) Collect() (result map[string]map[string]string, err error) {
+	return map[string]map[string]string{
+		"cpu": getCpuInfo(),
 	}, err
+}
+
+func getCpuInfo() (cpuinfo map[string]string) {
+	contents, err := ioutil.ReadFile("/proc/cpuinfo")
+	if err != nil {
+		return
+	}
+
+	lines := strings.Split(string(contents), "\n")
+
+	cpuinfo = make(map[string]string)
+
+	for _, line := range(lines) {
+		fields := regSplit(line, "\t+: ")
+		switch fields[0] {
+		case "model name": cpuinfo["model_name"] = fields[1]
+		}
+	}
+	return
+}
+
+func regSplit(text string, delimeter string) []string {
+    reg := regexp.MustCompile(delimeter)
+    indexes := reg.FindAllStringIndex(text, -1)
+    laststart := 0
+    result := make([]string, len(indexes) + 1)
+    for i, element := range indexes {
+            result[i] = text[laststart:element[0]]
+            laststart = element[1]
+    }
+    result[len(indexes)] = text[laststart:len(text)]
+    return result
 }
