e2e25cdd5c6cc6e2a1bdd2d7c691cd02493cd93e
Gosuke Miyashita <gosukenator@gmail.com>
2013-10-11 16:45:54 +0900

    Change result format and how to handle /proc/cpuinfo

--- pkg/gohai/cpu/cpu_linux_test.go
+++ pkg/gohai/cpu/cpu_linux_test.go
@@ -4,24 +4,16 @@ import (
 	"fmt"
 	. "github.com/r7kamura/gospel"
 	"testing"
-	"io/ioutil"
-	"regexp"
 )
 
 func TestCpu(t *testing.T) {
 	Describe(t, "cpu.Collect", func() {
 		collector := &Cpu{}
 		result, _ := collector.Collect()
+		fmt.Println(result["cpu"]["model_namea"], ":")
 
-		re := regexp.MustCompile("model name\t: (.*)")
-		cpuinfo, err := ioutil.ReadFile("/proc/cpuinfo")
-		if err != nil { panic(err) }
-		cpu := re.FindStringSubmatch(string(cpuinfo))[1]
-
-		fmt.Println(cpu)
-
-		It("should be able to collect hostname", func() {
-			Expect(result["cpu"]).To(Equal, cpu)
+		It("should be able to collect cpu model name", func() {
+			Expect(result["cpu"]["model_name"]).To(NotEqual, "")
 		})
 	})
 }
