3917393b93aadd91c7acb74f0c69357355cd3412
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-12 21:22:08 +0900

    Add verity command

---
+++ pkg/gohai/bin/verity.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "encoding/json"
+    "github.com/kentaro/verity"
+    "os"
+)
+
+func main() {
+    verity, err := verity.Collect()
+
+    if err != nil {
+        panic(err)
+    }
+
+    buf, err := json.Marshal(verity)
+
+    if err != nil {
+        panic(err)
+    }
+
+    os.Stdout.Write(buf)
+}
