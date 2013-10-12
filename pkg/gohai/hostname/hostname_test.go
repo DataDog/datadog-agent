f08069ada8a88ddfb38454139a88c286a11414d4
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-12 20:38:34 +0900

    Refactor collectors

--- pkg/gohai/hostname/hostname_test.go
+++ pkg/gohai/hostname/hostname_test.go
@@ -1,4 +1,4 @@
-package verity
+package hostname
 
 import (
 	. "github.com/r7kamura/gospel"
@@ -7,13 +7,21 @@ import (
 )
 
 func TestHostname(t *testing.T) {
+	Describe(t, "hostname.Name", func() {
+		collector := &Hostname{}
+
+		It("should have its own name", func() {
+			Expect(collector.Name()).To(Equal, "hostname")
+		})
+	})
+
 	Describe(t, "verity.Collector.Hostname.Collect", func() {
 		collector := &Hostname{}
 		result, _ := collector.Collect()
 		hostname, _ := os.Hostname()
 
 		It("should be able to collect hostname", func() {
-			Expect(result["hostname"]).To(Equal, hostname)
+			Expect(result.(string)).To(Equal, hostname)
 		})
 	})
 }
--- pkg/gohai/memory/memory_linux_test.go
+++ pkg/gohai/memory/memory_linux_test.go
@@ -1,17 +1,26 @@
-package verity
+package memory
 
 import (
+	"fmt"
 	. "github.com/r7kamura/gospel"
 	"testing"
 )
 
 func TestMemory(t *testing.T) {
+	Describe(t, "memory.Name", func() {
+		collector := &Memory{}
+
+		It("should have its own name", func() {
+			Expect(collector.Name()).To(Equal, "memory")
+		})
+	})
+
 	Describe(t, "memory.Collect", func() {
 		collector := &Memory{}
 		result, _ := collector.Collect()
 
 		It("should be able to collect total memory size", func() {
-			Expect(result["memory"]["total"]).To(NotEqual, "")
+			Expect(result.(map[string]string)["total"]).To(NotEqual, "")
 		})
 	})
 }
