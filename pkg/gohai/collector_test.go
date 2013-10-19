1e5ef48683cc66d6b7e968117e1c9d036d0650d9
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-19 20:22:00 +0900

    Integrate env collector

--- pkg/gohai/collector_test.go
+++ pkg/gohai/collector_test.go
@@ -2,19 +2,26 @@ package verity
 
 import (
 	. "github.com/r7kamura/gospel"
+	"os"
 	"testing"
 )
 
 func TestCollector(t *testing.T) {
 	Describe(t, "Collect", func() {
-		result, _ := Collect()
+		os.Setenv("VERITY_TEST", "1")
 
-		It("should be able to collect hostname info", func() {
-			Expect(result["hostname"]).To(Exist)
-		})
+		result, _ := Collect()
 
 		It("should be able to collect CPU info", func() {
 			Expect(result["cpu"]).To(Exist)
+		})
+
+		It("should be able to collect env info", func() {
+			Expect(result["test"]).To(Equal, "1")
+		})
+
+		It("should be able to collect hostname info", func() {
+			Expect(result["hostname"]).To(Exist)
 		})
 
 		It("should be able to collect memory info", func() {
