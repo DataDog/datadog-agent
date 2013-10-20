ceef35b229ca206591a75edc248c1cf3eda6b584
Keisuke KITA <kei.kita2501@gmail.com>
2013-10-20 10:34:42 +0900

    Add Network collectors

--- pkg/gohai/ipaddress/ipaddress_test.go
+++ pkg/gohai/ipaddress/ipaddress_test.go
@@ -5,22 +5,12 @@ import (
 	"testing"
 )
 
-func TestHostname(t *testing.T) {
+func TestIpAddress(t *testing.T) {
 	Describe(t, "ipaddress.Name", func() {
 		collector := &IpAddress{}
 
 		It("should have its own name", func() {
 			Expect(collector.Name()).To(Equal, "ipaddress")
-		})
-	})
-
-	Describe(t, "verity.Collector.IpAddress.Collect", func() {
-		collector := &IpAddress{}
-		result, _ := collector.Collect()
-		ipaddress := "10.0.2.15"
-
-		It("should be able to collect ipaddress", func() {
-			Expect(result.(string)).To(Equal, ipaddress)
 		})
 	})
 }
