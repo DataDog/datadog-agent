5d2935cbcfb8b207084afbffc8a5b774bacce7e1
Tristan Michelet <tristan@datadoghq.com>
2014-05-14 21:24:45 +0000

    Collect all cpus

--- pkg/gohai/cpu/cpu_linux.go
+++ pkg/gohai/cpu/cpu_linux.go
@@ -26,17 +26,16 @@ var cpuMap = map[string]string{
 	"cpu cores":  "cpu_cores",
 	"cpu MHz\t":  "mhz",
 	"cache size": "cache_size",
-	// "processor":   "processor",
-	// "cpu family":  "family",
-	// "model":       "model",
-	// "stepping":    "stepping",
-	// "physical id": "physical_id",
-	// "flags":       "flags",
+	"processor":   "processor",
+	"cpu family":  "family",
+	"model\t":       "model",
+	"stepping":    "stepping",
+	"physical id": "physical_id",
 }
 
 // fixme tri This collector saves only the last processor's info
 
-func getCpuInfo() (cpuInfo map[string]string, err error) {
+func getCpuInfo() (cpuInfo map[string]interface{}, err error) {
 	file, err := os.Open("/proc/cpuinfo")
 
 	if err != nil {
@@ -55,21 +54,23 @@ func getCpuInfo() (cpuInfo map[string]string, err error) {
 		return
 	}
 
-	cpuInfo = make(map[string]string)
-	count := 0
+	cpuInfo = make(map[string]interface{})
+	count := 1
+	currentCPU := make(map[string]string)
 
-	for _, line := range lines {
+	for _, line := range lines[1:] {
 		pair := regexp.MustCompile("\t: ").Split(line, 2)
 
 		key, ok := cpuMap[pair[0]]
-		if pair[0] == "processor" {
+		if pair[0] == "processor" { // next processor's information
+			cpuInfo[strconv.Itoa(count)] = currentCPU
 			count += 1
 		}
 		if ok {
-			cpuInfo[key] = pair[1]
+			currentCPU[key] = pair[1]
 		}
 	}
+	cpuInfo[strconv.Itoa(count)] = currentCPU
 
-	cpuInfo["total"] = strconv.Itoa(count)
 	return
 }
