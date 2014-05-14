d406a5dfcff0fdea7a3f5497927720c68086c094
Tristan Michelet <tristan@datadoghq.com>
2014-05-14 19:32:24 +0000

    Add arch info to system collector

--- pkg/gohai/platform/platform.go
+++ pkg/gohai/platform/platform.go
@@ -2,7 +2,6 @@ package platform
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"regexp"
 	"runtime"
@@ -23,21 +22,44 @@ func (self *Platform) Collect() (result interface{}, err error) {
 }
 
 func getPlatformInfo() (platformInfo map[string]interface{}, err error) {
-	platformInfo = make(map[string]interface{})
 
-	hostname, err := os.Hostname()
+	platformInfo, err = getArchInfo()
 	if err != nil {
 		return platformInfo, err
 	}
-	platformInfo["hostname"] = hostname
 
-	platformInfo["os"] = runtime.GOOS
 	platformInfo["goV"] = strings.Replace(runtime.Version(), "go", "", -1)
 	pythonV, err := getPythonVersion()
 	if err != nil {
 		return platformInfo, err
 	}
 	platformInfo["pythonV"] = pythonV
+
+	return
+}
+
+func getArchInfo() (archInfo map[string]interface{}, err error) {
+	archInfo = make(map[string]interface{})
+
+	out, err := exec.Command("uname", "-s", "-n", "-r", "-m", "-p", "-i", "-o").Output()
+	if err != nil {
+		return nil, err
+	}
+	line := fmt.Sprintf("%s", out)
+	values := regexp.MustCompile(" +").Split(line, 7)
+	archInfo["kernel_name"] = values[0]
+	archInfo["hostname"] = values[1]
+	archInfo["kernel_release"] = values[2]
+    archInfo["machine"] = values[3]
+    archInfo["processor"] = values[4]
+    archInfo["hardware_platform"] = values[5]
+    archInfo["os"] = strings.Replace(values[6], "\n", "", -1)
+
+    out, err = exec.Command("uname", "-v").Output()
+    if err != nil {
+        return nil, err
+    }
+    archInfo["kernel_version"] = strings.Replace(fmt.Sprintf("%s", out), "\n", "", -1)
 
 	return
 }
