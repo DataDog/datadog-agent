744e7d0354418d5f33c4cac1940e990b4361f855
tristan <tristan@datadoghq.com>
2014-05-15 18:47:39 -0400

    Collect windows network and platform

--- pkg/gohai/network/network.go
+++ pkg/gohai/network/network.go
@@ -1,3 +1,5 @@
+// +build linux darwin
+
 package network
 
 import (
@@ -154,86 +156,4 @@ func macAddress() (string, error) {
 		}
 	}
 	return "", errors.New("not connected to the network")
-}
-
-/* Not currently used */
-
-func getDetailedNetworkInfo() (networkInfo map[string]interface{}, err error) {
-	ifaces, err := net.Interfaces()
-
-	if err != nil {
-		return
-	}
-
-	networkInfo = make(map[string]interface{})
-	var interfaces = make(map[string]interface{})
-	networkInfo["interfaces"] = interfaces
-
-	for _, iface := range ifaces {
-		newIface := make(map[string]interface{})
-		flags := getFlags(iface)
-		addresses, _ := getAddresses(iface)
-		newIface["mtu"] = iface.MTU
-		newIface["flags"] = flags
-		newIface["addresses"] = addresses
-		interfaces[iface.Name] = newIface
-	}
-
-	return
-}
-
-func getFlags(iface net.Interface) (flags []string) {
-	if iface.Flags&net.FlagUp != 0 {
-		flags = append(flags, "UP")
-	}
-	if iface.Flags&net.FlagLoopback != 0 {
-		flags = append(flags, "LOOPBACK")
-	}
-	if iface.Flags&net.FlagBroadcast != 0 {
-		flags = append(flags, "BROADCASE")
-	}
-	if iface.Flags&net.FlagMulticast != 0 {
-		flags = append(flags, "MULTICAST")
-	}
-	if iface.Flags&net.FlagPointToPoint != 0 {
-		flags = append(flags, "POINTTOPOINT")
-	}
-
-	return
-}
-
-func getAddresses(iface net.Interface) (addresses map[string]interface{}, err error) {
-	addresses = make(map[string]interface{})
-
-	addrs, err := iface.Addrs()
-	if err != nil {
-		return nil, err
-	}
-	for _, addr := range addrs {
-		var ip net.IP
-		addressInfo := make(map[string]string)
-		switch v := addr.(type) {
-		case *net.IPNet:
-			ip = v.IP
-		case *net.IPAddr:
-			ip = v.IP
-		}
-
-		netmask := ip.DefaultMask()
-		if netmask != nil {
-			addressInfo["family"] = "inet"
-		} else {
-			addressInfo["family"] = "inet6"
-		}
-		addresses[ip.String()] = addressInfo
-	}
-
-	mac := iface.HardwareAddr
-	if mac != nil {
-		addressInfo := make(map[string]string)
-		addressInfo["family"] = "lladdr"
-		addresses[mac.String()] = addressInfo
-	}
-
-	return
 }
