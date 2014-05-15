f0eec877219b10c1501bc1bb9877e0fa230ab7d1
tristan <tristan@datadoghq.com>
2014-05-15 11:26:47 -0400

    Save filesystem as array instead of dict

--- pkg/gohai/filesystem/filesystem_darwin.go
+++ pkg/gohai/filesystem/filesystem_darwin.go
@@ -3,9 +3,9 @@ package filesystem
 var dfOptions = []string{"-l", "-k"}
 var expectedLength = 9
 
-func updatefileSystemInfo(fileSystemInfo map[string]interface{}, values []string) {
-	name := values[0]
-	fileSystemInfo[name] = map[string]string{
+func updatefileSystemInfo(values []string) map[string]string {
+	return map[string]string{
+		"name":       values[0],
 		"kb_size":    values[1],
 		"mounted_on": values[8],
 	}
--- pkg/gohai/filesystem/filesystem_linux.go
+++ pkg/gohai/filesystem/filesystem_linux.go
@@ -3,9 +3,9 @@ package filesystem
 var dfOptions = []string{"-l"}
 var expectedLength = 6
 
-func updatefileSystemInfo(fileSystemInfo map[string]interface{}, values []string) {
-	name := values[0]
-	fileSystemInfo[name] = map[string]string{
+func updatefileSystemInfo(values []string) map[string]string {
+	return map[string]string{
+		"name":       values[0],
 		"kb_size":    values[1],
 		"mounted_on": values[5],
 	}
