a4b9ff2a52161dfbe56607ea1b1a7e75b3451f8a
Tristan Michelet <tristan@datadoghq.com>
2014-06-30 15:21:19 +0000

    CPU number of cores fix on linux
    Since we can't rely on the "cpu cores" field of /proc/cpuinfo, we need to instead count the number of results of this command

--- pkg/gohai/cpu/cpu_linux.go
+++ pkg/gohai/cpu/cpu_linux.go
@@ -4,12 +4,12 @@ import (
 	"bufio"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 var cpuMap = map[string]string{
 	"vendor_id":  "vendor_id",
 	"model name": "model_name",
-	"cpu cores":  "cpu_cores",
 	"cpu MHz\t":  "mhz",
 	"cache size": "cache_size",
 	"cpu family": "family",
@@ -38,14 +38,22 @@ func getCpuInfo() (cpuInfo map[string]string, err error) {
 
 	cpuInfo = make(map[string]string)
 
+	// count # of occurences to get # of cores, since the field
+	// "cpu cores" isn't reliable
+	numCores := 0
+
 	for _, line := range lines[1:] {
 		pair := regexp.MustCompile("\t: ").Split(line, 2)
 
 		key, ok := cpuMap[pair[0]]
 		if ok {
 			cpuInfo[key] = pair[1]
+			if pair[0] == "vendor_id" {
+				numCores += 1
+			}
 		}
 	}
+	cpuInfo["cpu_cores"] = strconv.Itoa(numCores)
 
 	return
 }
