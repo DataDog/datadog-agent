5afe3f7f7ad2942375270267817113ca00f6039a
talwai <aaditya.21@gmail.com>
2015-08-19 18:06:20 -0400

    [filesystem] better signal handling for timeout

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -15,8 +15,8 @@ func getFileSystemInfo() (interface{}, error) {
 	/* Grab filesystem data from df	*/
 	cmd := exec.Command("df", dfOptions...)
 
-	outCh := make(chan []byte)
-	errCh := make(chan error)
+	outCh := make(chan []byte, 1)
+	errCh := make(chan error, 1)
 
 	var out interface{}
 	var err error
@@ -24,28 +24,33 @@ func getFileSystemInfo() (interface{}, error) {
 	go func() {
 		_out, _err := cmd.Output()
 		if _err != nil {
-			errCh <- _err
+			errCh <- fmt.Errorf("df failed to collect filesystem data: %s", _err)
 			return
 		}
 		outCh <- _out
 	}()
 
-	select {
-	case res := <-outCh:
-		if res != nil {
-			out, err = parseDfOutput(string(res))
-		} else {
-			out, err = nil, fmt.Errorf("df process timed out and was killed!")
+WAIT:
+	for {
+		select {
+		case res := <-outCh:
+			if res != nil {
+				out, err = parseDfOutput(string(res))
+			} else {
+				out, err = nil, fmt.Errorf("df failed to collect filesystem data")
+			}
+			break WAIT
+		case err = <-errCh:
+			out = nil
+			break WAIT
+		case <-time.After(2 * time.Second):
+			// Kill the process if it takes too long
+			if killErr := cmd.Process.Kill(); killErr != nil {
+				log.Fatal("failed to kill:", killErr)
+				// Force goroutine to exit
+				<-outCh
+			}
 		}
-	case err = <-errCh:
-		out = nil
-	case <-time.After(5 * time.Second):
-		// Kill the process if it takes too long
-		if killErr := cmd.Process.Kill(); killErr != nil {
-			log.Fatal("failed to kill:", killErr)
-		}
-		//Let goroutine exit
-		<-outCh
 	}
 
 	return out, err
