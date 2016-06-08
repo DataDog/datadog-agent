71fe5927468a41b78cfb94fbb1f06035796e3e60
Massimiliano Pippi <massi@datadoghq.com>
2016-06-08 16:03:05 -0400

    avoid talking to the aggregator during unit tests

--- py/tests/testcheck.py
+++ py/tests/testcheck.py
@@ -2,5 +2,8 @@ from checks import AgentCheck
 
 class TestCheck(AgentCheck):
     def check(self, instance):
-        self.gauge('foo', 0)
-        self.gauge('foo', 1)
+        """
+        Do not interact with the Aggregator during
+        unit tests. Doing anything is ok here.
+        """
+        pass
