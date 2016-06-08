82e1abfbb30a02f3d2c42a03e0dd676c084d1729
Massimiliano Pippi <massi@datadoghq.com>
2016-06-08 15:53:18 -0400

    fixed checks tests

--- checks/common_test.go
+++ checks/common_test.go
@@ -12,34 +12,18 @@ type TestCheck struct {
 
 func (c *TestCheck) String() string { return "TestCheck" }
 
-func (c *TestCheck) Run() (CheckResult, error) {
+func (c *TestCheck) Run() error {
 	if c.doErr {
 		msg := "A tremendous error occurred."
-		return CheckResult{Result: "", Error: msg}, errors.New(msg)
+		return errors.New(msg)
 	}
-	return CheckResult{Result: "Foo", Error: ""}, nil
+	return nil
 }
 
 func TestRunner(t *testing.T) {
 	pending := make(chan Check)
-	results := make(chan CheckResult)
-	go Runner(pending, results)
+	go Runner(pending)
 
 	pending <- &TestCheck{doErr: false}
-	res := <-results
-	if res.Error != "" {
-		t.Fatalf("Expected empty error message, found: %s", res.Error)
-	}
-	if res.Result != "Foo" {
-		t.Fatalf("Expected: %s, found: %s", "Foo", res.Result)
-	}
-
 	pending <- &TestCheck{doErr: true}
-	res = <-results
-	if res.Error == "" {
-		t.Fatal("Found empty error message")
-	}
-	if res.Result != "" {
-		t.Fatalf("Expected empty Result")
-	}
 }
