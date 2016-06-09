93cfd5e235b03c974a2b5ce2f537b106cb3cab44
Massimiliano Pippi <massi@datadoghq.com>
2016-06-09 18:00:05 -0400

    removed more unused modules

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 ORG_PATH="github.com/DataDog"
 REPO_PATH="${ORG_PATH}/datadog-agent"
+BIN_PATH="bin/agent"
 
 eval $(go env)
 
-go build -o ./bin/agent ${REPO_PATH}/cmd/agent
+go build -o .${BIN_PATH}/agent ${REPO_PATH}/cmd/agent
+cp -r pkg/py/dist ${BIN_PATH}
