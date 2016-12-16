311ad14bba3e86050aab640e66a02742560ed864
Derek Brown <derek.brown@datadoghq.com>
2016-12-15 17:06:24 -0800

    Add routine for parsing a config file, and creating the map of log file to parser

---
+++ pkg/dogstream/config.go
@@ -0,0 +1,41 @@
+package dogstream
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type streamEntry struct {
+	Name   string   `yaml:"name"`
+	File   string   `yaml:"file"`
+	Parser []string `yaml:"parser"`
+}
+
+type streamConfig struct {
+	Instances []streamEntry `yaml:"instances"`
+}
+
+func loadConfig(filename string) (map[string][]Parser, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var config streamConfig
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+	retmap := make(map[string][]Parser)
+
+	for _, inst := range config.Instances {
+		for _, parser := range inst.Parser {
+			dsp, err := Load(parser)
+			if err != nil {
+				return nil, err
+			}
+			retmap[inst.File] = append(retmap[inst.File], dsp)
+		}
+	}
+	return retmap, nil
+}
