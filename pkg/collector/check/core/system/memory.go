353c725c53bc32b78360818b382c564aaf2c5023
Maxime Mouial <maxime.mouial@gmail.com>
2016-12-20 18:55:10 +0100

    Add a Memory check to the agent: ref #70
    This add a memory and swap check for linux, freebasd and darwin. The SunOS will come later due to limitatio in gopsutil.

--- pkg/collector/check/core/system/memory.go
+++ pkg/collector/check/core/system/memory.go
@@ -1,6 +1,7 @@
 package system
 
 import (
+	"runtime"
 	"time"
 
 	"github.com/DataDog/datadog-agent/pkg/collector/check"
@@ -11,6 +12,11 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/aggregator"
 )
 
+// For testing purpose
+var virtualMemory = mem.VirtualMemory
+var swapMemory = mem.SwapMemory
+var runtimeOS = runtime.GOOS
+
 // MemoryCheck doesn't need additional fields
 type MemoryCheck struct {
 	sender aggregator.Sender
@@ -20,10 +26,52 @@ func (c *MemoryCheck) String() string {
 	return "MemoryCheck"
 }
 
+const mbSize float64 = 1024 * 1024
+
+func (c *MemoryCheck) linuxSpecificMemoryCheck(v *mem.VirtualMemoryStat, s *mem.SwapMemoryStat) {
+	c.sender.Gauge("system.mem.cached", float64(v.Cached)/mbSize, "", nil)
+	c.sender.Gauge("system.mem.shared", float64(v.Shared)/mbSize, "", nil)
+	c.sender.Gauge("system.mem.slab", float64(v.Slab)/mbSize, "", nil)
+	c.sender.Gauge("system.mem.page_tables", float64(v.PageTables)/mbSize, "", nil)
+	c.sender.Gauge("system.swap.cached", float64(v.SwapCached)/mbSize, "", nil)
+}
+
+func (c *MemoryCheck) freebsdSpecificMemoryCheck(v *mem.VirtualMemoryStat, s *mem.SwapMemoryStat) {
+	c.sender.Gauge("system.mem.cached", float64(v.Cached)/mbSize, "", nil)
+}
+
 // Run executes the check
 func (c *MemoryCheck) Run() error {
-	v, _ := mem.VirtualMemory()
-	c.sender.Gauge("system.mem.total", float64(v.Total), "", []string{})
+
+	v, err := virtualMemory()
+	if err != nil {
+		log.Error("system.MemoryCheck: could not retrieve virtual memory stats: %s", err)
+		return err
+	}
+	s, err := swapMemory()
+	if err != nil {
+		log.Error("system.MemoryCheck: could not retrieve swap memory stats: %s", err)
+		return err
+	}
+
+	c.sender.Gauge("system.mem.total", float64(v.Total)/mbSize, "", nil)
+	c.sender.Gauge("system.mem.free", float64(v.Free)/mbSize, "", nil)
+	c.sender.Gauge("system.mem.used", float64(v.Used)/mbSize, "", nil)
+	c.sender.Gauge("system.mem.usable", float64(v.Available)/mbSize, "", nil)
+	c.sender.Gauge("system.mem.pct_usable", float64(100-v.UsedPercent)/100, "", nil)
+
+	c.sender.Gauge("system.swap.total", float64(s.Total)/mbSize, "", nil)
+	c.sender.Gauge("system.swap.free", float64(s.Free)/mbSize, "", nil)
+	c.sender.Gauge("system.swap.used", float64(s.Used)/mbSize, "", nil)
+	c.sender.Gauge("system.swap.pct_free", float64(100-s.UsedPercent)/100, "", nil)
+
+	switch runtimeOS {
+	case "linux":
+		c.linuxSpecificMemoryCheck(v, s)
+	case "freebsd":
+		c.freebsdSpecificMemoryCheck(v, s)
+	}
+
 	c.sender.Commit()
 	return nil
 }
---
+++ pkg/collector/check/core/system/memory_test.go
@@ -0,0 +1,141 @@
+package system
+
+import (
+	"testing"
+
+	"github.com/shirou/gopsutil/mem"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockSender struct {
+	mock.Mock
+}
+
+func (m *MockSender) Destroy()                                                               {}
+func (m *MockSender) Rate(metric string, value float64, hostname string, tags []string)      {}
+func (m *MockSender) Histogram(metric string, value float64, hostname string, tags []string) {}
+
+func (m *MockSender) Commit() {
+	m.Called()
+}
+
+func (m *MockSender) Gauge(metric string, value float64, hostname string, tags []string) {
+	m.Called(metric, value, hostname, tags)
+}
+
+func VirtualMemory() (*mem.VirtualMemoryStat, error) {
+	return &mem.VirtualMemoryStat{
+		Total:        12345667890,
+		Available:    234567890,
+		Used:         10000000000,
+		UsedPercent:  81,
+		Free:         11554304000,
+		Active:       2506516070400,
+		Inactive:     970587111424,
+		Wired:        0,
+		Buffers:      353818902528,
+		Cached:       2596446142464,
+		Writeback:    0,
+		Dirty:        314572800,
+		WritebackTmp: 0,
+		Shared:       327680000000,
+		Slab:         327680000000,
+		PageTables:   37790679040,
+		SwapCached:   25000000000,
+	}, nil
+}
+
+func SwapMemory() (*mem.SwapMemoryStat, error) {
+	return &mem.SwapMemoryStat{
+		Total:       100000,
+		Used:        40000,
+		Free:        60000,
+		UsedPercent: 40,
+		Sin:         21,
+		Sout:        22,
+	}, nil
+}
+
+func TestMemoryCheckLinux(t *testing.T) {
+	virtualMemory = VirtualMemory
+	swapMemory = SwapMemory
+	memCheck := new(MemoryCheck)
+
+	mock := new(MockSender)
+	memCheck.sender = mock
+	runtimeOS = "linux"
+
+	mock.On("Gauge", "system.mem.total", 12345667890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.free", 11554304000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.used", 10000000000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.usable", 234567890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.pct_usable", 0.19, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.cached", 2596446142464.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.shared", 327680000000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.slab", 327680000000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.page_tables", 37790679040.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.total", 100000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.free", 60000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.used", 40000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.pct_free", 0.6, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.cached", 25000000000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Commit").Return().Times(1)
+	memCheck.Run()
+
+	mock.AssertExpectations(t)
+	mock.AssertNumberOfCalls(t, "Gauge", 14)
+	mock.AssertNumberOfCalls(t, "Commit", 1)
+}
+
+func TestMemoryCheckFreebsd(t *testing.T) {
+	virtualMemory = VirtualMemory
+	swapMemory = SwapMemory
+	memCheck := new(MemoryCheck)
+
+	mock := new(MockSender)
+	memCheck.sender = mock
+	runtimeOS = "freebsd"
+
+	mock.On("Gauge", "system.mem.total", 12345667890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.free", 11554304000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.used", 10000000000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.usable", 234567890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.pct_usable", 0.19, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.cached", 2596446142464.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.total", 100000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.free", 60000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.used", 40000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.pct_free", 0.6, "", []string(nil)).Return().Times(1)
+	mock.On("Commit").Return().Times(1)
+	memCheck.Run()
+
+	mock.AssertExpectations(t)
+	mock.AssertNumberOfCalls(t, "Gauge", 10)
+	mock.AssertNumberOfCalls(t, "Commit", 1)
+}
+
+func TestMemoryCheckDarwin(t *testing.T) {
+	virtualMemory = VirtualMemory
+	swapMemory = SwapMemory
+	memCheck := new(MemoryCheck)
+
+	mock := new(MockSender)
+	memCheck.sender = mock
+	runtimeOS = "darwin"
+
+	mock.On("Gauge", "system.mem.total", 12345667890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.free", 11554304000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.used", 10000000000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.usable", 234567890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.pct_usable", 0.19, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.total", 100000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.free", 60000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.used", 40000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.pct_free", 0.6, "", []string(nil)).Return().Times(1)
+	mock.On("Commit").Return().Times(1)
+	memCheck.Run()
+
+	mock.AssertExpectations(t)
+	mock.AssertNumberOfCalls(t, "Gauge", 9)
+	mock.AssertNumberOfCalls(t, "Commit", 1)
+}
