e92e78b818d54346017b3edb5c22ba57a6c27f7d
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2016-12-21 18:21:15 -0500

    [rake] Fail when we're in a CI env

--- go.rb
+++ go.rb
@@ -1,10 +1,12 @@
 
 
-def go_fmt(path)
+def go_fmt(path, fail_on_mod)
   out = `go fmt #{path}/...`
   errors = out.split("\n")
   if errors.length > 0
+    puts "Reformatted the following files:"
     puts out
+    fail if fail_on_mod
   end
 end
 
