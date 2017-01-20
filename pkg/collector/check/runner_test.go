65da995cc8520aac5b2b8f5fd57604f4368604d4
Maxime Mouial <maxime.mouial@gmail.com>
2017-01-20 15:47:30 +0100

    Initialize runner at runner creation.
    The 'GetChan' method return the pending queue (to initialize the
    scheduler). Therefore the internal channel should initialize before
    running the runner. NewRunner now return a valid runner.
    
    At this point we merge the 'Run' method with 'NewRunner' function.

--- pkg/collector/check/runner_test.go
+++ pkg/collector/check/runner_test.go
@@ -8,27 +8,13 @@ import (
 )
 
 func TestNewRunner(t *testing.T) {
-	r := NewRunner()
-	assert.Nil(t, r.pending)
-}
-
-func TestRun(t *testing.T) {
-	r := NewRunner()
-
-	// let's fake the runner already started, this should be a noop
-	r.pending = make(chan Check)
-	r.Run(1)
-
-	r = NewRunner()
-	r.Run(1)
+	r := NewRunner(1)
 	assert.NotNil(t, r.pending)
 	assert.NotNil(t, r.runningChecks)
-	close(r.pending)
 }
 
 func TestStop(t *testing.T) {
-	r := NewRunner()
-	r.Run(1)
+	r := NewRunner(1)
 	r.Stop()
 	_, ok := <-r.pending
 	assert.False(t, ok)
@@ -38,15 +24,12 @@ func TestStop(t *testing.T) {
 }
 
 func TestGetChan(t *testing.T) {
-	r := NewRunner()
-	assert.Nil(t, r.GetChan())
-	r.Run(1)
+	r := NewRunner(1)
 	assert.NotNil(t, r.GetChan())
 }
 
 func TestWork(t *testing.T) {
-	r := NewRunner()
-	r.Run(1)
+	r := NewRunner(1)
 	c1 := TestCheck{}
 	c2 := TestCheck{doErr: true}
 
@@ -56,8 +39,7 @@ func TestWork(t *testing.T) {
 	r.Stop()
 
 	// fake a check is already running
-	r = NewRunner()
-	r.Run(1)
+	r = NewRunner(1)
 	c3 := new(TestCheck)
 	r.runningChecks[c3.String()] = c3
 	r.pending <- c3
