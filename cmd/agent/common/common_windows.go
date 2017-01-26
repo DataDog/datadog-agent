8b64aaf6ece29a42fbf6f3801225b6939c99fcd8
Massimiliano Pippi <massi@datadoghq.com>
2017-01-26 15:31:42 +0100

    added tags to track known issues

--- cmd/agent/common/common_windows.go
+++ cmd/agent/common/common_windows.go
@@ -1,5 +1,6 @@
 package common
 
+// BUG(massi): which is the final destination of the DIST folder on Windows?
 var configPaths = []string{
 	DistPath,
 }
--- pkg/pidfile/pidfile_windows.go
+++ pkg/pidfile/pidfile_windows.go
@@ -1,10 +1,13 @@
 package pidfile
 
+// BUG(massi): This needs to be implemented
 func isProcess(pid int) bool {
 	return false
 }
 
-// Path returns a suitable location for the pidfile under OSX
+// Path returns a suitable location for the pidfile under Windows
+//
+// BUG(massi): This needs to be implemented
 func Path() string {
 	return ""
 }
