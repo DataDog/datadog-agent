591d9ec7b27fd1003b20743c5a4c4102903b5a33
Massimiliano Pippi <mpippi@gmail.com>
2017-02-06 17:13:01 +0100

    for now, get only the hostname from GCE metadata api

--- pkg/util/gce/gce.go
+++ pkg/util/gce/gce.go
@@ -1,8 +1,8 @@
 package gce
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -12,50 +12,34 @@ const (
 	timeout     time.Duration = 300 * time.Millisecond
 )
 
-type metadata struct {
-	instance struct {
-		hostname string
-	}
-}
-
 // GetHostname returns the hostname querying GCE Metadata api
 func GetHostname() (string, error) {
-	md, err := getMetadata()
-	if err != nil {
-		return "", fmt.Errorf("error fetching GCE hostname, %s", err)
-	}
-	return md.instance.hostname, nil
-}
-
-func getMetadata() (*metadata, error) {
 	client := http.Client{
 		Timeout: timeout,
 	}
 
-	url := metadataURL + "?recursive=true"
+	url := metadataURL + "instance/hostname"
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	req.Header.Add("Metadata-Flavor", "Google")
 	res, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-
 	defer res.Body.Close()
+
 	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("status code %d trying to fetch %s", res.StatusCode, url)
+		return "", fmt.Errorf("GCE hostname, status code %d trying to GET %s", res.StatusCode, url)
 	}
 
-	decoder := json.NewDecoder(res.Body)
-	var md metadata
-	err = decoder.Decode(&md)
+	all, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal metadata, %s", err)
+		return "", fmt.Errorf("GCE hostname, error reading response body: %s", err)
 	}
 
-	return &md, nil
+	return string(all), nil
 }
