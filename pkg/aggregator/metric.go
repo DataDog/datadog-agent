66c3b1b3c98ff80880557c153d10a51dda29a408
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-02-16 12:30:04 -0500

    [aggregator] Add some comments on the MonotonicCount's behavior

--- pkg/aggregator/metric.go
+++ pkg/aggregator/metric.go
@@ -147,9 +147,10 @@ func (r *Rate) flush(timestamp int64) ([]*Serie, error) {
 
 // MonotonicCount tracks a raw counter, based on increasing counter values.
 // Samples that have a lower value than the previous sample are ignored (since it usually
-// means that the underlying raw count has been reset).
+// means that the underlying raw counter has been reset).
 // Example:
-// * samples 2, 3, 6, 7 returns 5 (i.e. 7-2) on 1st flush, then sample 11 returns 4 (i.e. 11-7) on 2nd flush
+//  submitting samples 2, 3, 6, 7 returns 5 (i.e. 7-2) on flush ;
+//  then submitting samples 10, 11 on the same MonotonicCount returns 4 (i.e. 11-7) on flush
 type MonotonicCount struct {
 	previousSample        float64
 	currentSample         float64
@@ -167,6 +168,8 @@ func (mc *MonotonicCount) addSample(sample float64, timestamp int64) {
 		mc.hasPreviousSample = true
 	}
 
+	// To handle cases where the samples are not monotonically increasing, we always add the difference
+	// between 2 consecutive samples to the value that'll be flushed (if the difference is >0).
 	diff := mc.currentSample - mc.previousSample
 	if mc.sampledSinceLastFlush && mc.hasPreviousSample && diff > 0. {
 		mc.value += diff
@@ -179,6 +182,7 @@ func (mc *MonotonicCount) flush(timestamp int64) ([]*Serie, error) {
 	}
 
 	value := mc.value
+	// reset struct fields
 	mc.previousSample, mc.currentSample, mc.value = mc.currentSample, 0., 0.
 	mc.sampledSinceLastFlush = false
 
