7f6809285e4a0a7f2396547ccfb5da3e201e4a3d
Maxime Mouial <maxime.mouial@gmail.com>
2017-02-23 18:08:30 +0100

    Adding packaging of dogstatsd to gitlab

--- Rakefile
+++ Rakefile
@@ -124,6 +124,35 @@ namespace :dogstatsd do
   task :run => %w[dogstatsd:build] do
     system("#{DOGSTATSD_BIN_PATH}/dogstatsd")
   end
+
+  desc "Build omnibus installer"
+  task :omnibus do
+    # omnibus log level
+    log_level = ENV["AGENT_OMNIBUS_LOG_LEVEL"] || "info"
+
+    # omnibus config overrides
+    overrides_cmd = ""
+    overrides = []
+    base_dir = ENV["AGENT_OMNIBUS_BASE_DIR"]
+    if base_dir
+      overrides.push("base_dir:#{base_dir}")
+    end
+
+    package_dir = ENV["AGENT_OMNIBUS_PACKAGE_DIR"]
+    if package_dir
+      overrides.push("package_dir:#{package_dir}")
+    end
+
+    Dir.chdir('omnibus') do
+      system("bundle install --without development")
+
+      if overrides.size > 0
+        overrides_cmd = "--override=" + overrides.join(" ")
+      end
+
+      system("omnibus build dogstatsd --log-level=#{log_level} #{overrides_cmd}")
+    end
+  end
 end
 
 
---
+++ omnibus/config/projects/dogstatsd.rb
@@ -0,0 +1,91 @@
+#
+# Copyright 2016 Datadog
+#
+# All Rights Reserved.
+#
+require "./lib/ostools.rb"
+
+name 'dogstatsd'
+maintainer 'Datadog Packages <package@datadoghq.com>'
+homepage 'http://www.datadoghq.com'
+install_dir '/opt/datadog-dogstatsd'
+
+build_version do
+  source :git, from_dependency: 'dogstatsd'
+  output_format :dd_agent_format
+end
+
+build_iteration 1
+
+description 'Datadog dogstatsd agent
+ The Datadog dogstatsd agent is a lightweight process that will receive
+ dogstatsd packet, aggregate them and forward them to Datadog backend. The main
+ purpose of the agent is to handle custom metrics from external processes.
+ .
+ This package installs and runs the dogstatsd agent.
+ .
+ See http://www.datadoghq.com/ for more information
+'
+
+# ------------------------------------
+# Generic package information
+# ------------------------------------
+
+# .deb specific flags
+package :deb do
+  vendor 'Datadog <package@datadoghq.com>'
+  epoch 1
+  license 'Simplified BSD License'
+  section 'utils'
+  priority 'extra'
+end
+
+# .rpm specific flags
+package :rpm do
+  vendor 'Datadog <package@datadoghq.com>'
+  epoch 1
+  license 'Simplified BSD License'
+  category 'System Environment/Daemons'
+  priority 'extra'
+  if ENV.has_key?('RPM_SIGNING_PASSPHRASE') and not ENV['RPM_SIGNING_PASSPHRASE'].empty?
+    signing_passphrase "#{ENV['RPM_SIGNING_PASSPHRASE']}"
+  end
+end
+
+# OSX .pkg specific flags
+package :pkg do
+  identifier 'com.datadoghq.agent'
+  #signing_identity 'Developer ID Installer: Datadog, Inc. (JKFCB4CN7C)'
+end
+compress :dmg do
+  window_bounds '200, 200, 750, 600'
+  pkg_position '10, 10'
+end
+
+# ------------------------------------
+# OS specific DSLs and dependencies
+# ------------------------------------
+
+# Linux
+if linux?
+  if debian?
+    extra_package_file '/etc/init/dogstatsd.conf'
+    extra_package_file '/lib/systemd/system/dogstatsd.service'
+  end
+end
+
+# ------------------------------------
+# Dependencies
+# ------------------------------------
+
+# creates required build directories
+dependency 'preparation'
+
+# version manifest file
+dependency 'version-manifest'
+
+# Dogstatsd
+dependency 'dogstatsd'
+
+exclude '\.git*'
+exclude 'bundler\/git'
