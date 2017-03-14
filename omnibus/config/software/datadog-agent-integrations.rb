e7a4c63193ee1a4a8d8f97ef8013fbdf01239926
Massimiliano Pippi <mpippi@gmail.com>
2017-03-14 15:33:58 +0100

    let omnibus ship integrations core with the agent

---
+++ omnibus/config/software/datadog-agent-integrations.rb
@@ -0,0 +1,42 @@
+name 'datadog-agent-integrations'
+
+dependency 'pip'
+dependency 'datadog-agent'
+dependency 'integration-deps'
+
+relative_path 'integrations-core'
+
+source git: 'https://github.com/DataDog/integrations-core.git'
+default_version 'massi/rakefile'
+
+build do
+  # The checks
+  checks_dir = "#{install_dir}/agent/checks.d"
+  mkdir checks_dir
+
+  # The confs
+  if linux?
+    conf_directory = "/etc/dd-agent/conf.d"
+  elsif osx?
+    conf_directory = "#{install_dir}/etc"
+  elsif windows?
+    conf_directory = "../../extra_package_files/EXAMPLECONFSLOCATION"
+  end
+
+  # Copy the checks and generate the requriments file
+  command "rake copy_checks conf_dir=#{conf_directory} checks_dir=#{checks_dir}"
+
+  # Install all the requirements
+  pip_args = "install --install-option=\"--install-scripts=#{windows_safe_path(install_dir)}/bin\" -c #{install_dir}/agent/requirements.txt -r check_requirements.txt"
+  if windows?
+    command "#{windows_safe_path(install_dir)}\\embedded\\scripts\\pip.exe #{pip_args}"
+  else
+    build_env = {
+      "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+      "PATH" => "/#{install_dir}/embedded/bin:#{ENV['PATH']}",
+    }
+    command "pip #{pip_args}", :env => build_env
+  end
+
+  copy '/check_requirements.txt', "#{install_dir}/agent/"
+end
