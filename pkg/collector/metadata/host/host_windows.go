2acd2a5ff2e9fb4787f54d26d05cf54c32723868
Derek Brown <derek.brown@datadoghq.com>
2017-03-24 07:53:23 -0700

    [windows] add implementation of fillOsVersion (definition of osVersion type) so windows will build

--- pkg/collector/metadata/host/host_windows.go
+++ pkg/collector/metadata/host/host_windows.go
@@ -2,7 +2,9 @@ package host
 
 import "github.com/shirou/gopsutil/host"
 
+type osVersion [2]string
+
 func fillOsVersion(stats *systemStats, info *host.InfoStat) {
 	// TODO
-	panic("TODO")
+	stats.Winver = osVersion{info.PlatformFamily, info.PlatformVersion}
 }
