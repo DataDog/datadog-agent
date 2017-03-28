9ccbb9869d70e87150229da9d195c91572f83fdb
Derek Brown <derek.brown@datadoghq.com>
2017-03-28 14:06:40 -0700

    [windows] change http rest api server implementation to named pipes rather than http over tcp.

--- cmd/agent/api/listener_windows.go
+++ cmd/agent/api/listener_windows.go
@@ -1,7 +1,31 @@
 package api
 
-import "net"
+import (
+	"net"
+	"net/http"
 
+	"github.com/Microsoft/go-winio"
+)
+
+const pipename = `\\.\pipe\ddagent`
+
+// getListener returns a listening connection to a Windows named
+// pipe for IPC
 func getListener() (net.Listener, error) {
-	return net.Listen("tcp", ":80")
+	return winio.ListenPipe(pipename, nil)
+}
+
+// HTTP doesn't need anything from the transport, so we can use
+// a named pipe
+func fakeDial(proto, addr string) (conn net.Conn, err error) {
+	return winio.DialPipe(pipename, nil)
+}
+
+// GetClient is a convenience function returning an http
+// client suitable to use a named pipe transport
+func GetClient() *http.Client {
+	tr := &http.Transport{
+		Dial: fakeDial,
+	}
+	return &http.Client{Transport: tr}
 }
