6f912f371b308a827d19f8d894ef3fd1425de6d0
Derek Brown <derek.brown@datadoghq.com>
2017-03-29 07:33:23 -0700

    Add specific ACLs for the named pipe

--- cmd/agent/api/listener_windows.go
+++ cmd/agent/api/listener_windows.go
@@ -12,7 +12,31 @@ const pipename = `\\.\pipe\ddagent`
 // getListener returns a listening connection to a Windows named
 // pipe for IPC
 func getListener() (net.Listener, error) {
-	return winio.ListenPipe(pipename, nil)
+
+	//SecurityDescriptor: "D:P(A;;FA;;;WD)", // "WD is EVERYONE"
+	allowedGroups := []string{
+		"(A;;FA;;;BA)", // BA is built-in administrators
+		"(A;;FA;;;DA)", // DA is domain admins
+		"(A;;FA;;;EA)", // EA is Enterprise admin
+		"(A;;FA;;;LA)", // LA is Local admin
+		"(A;;FA;;;SY)", // SY is local system
+
+	}
+	secDesc := "D:P"
+	// domain admins and enterprise admins may not exist,
+	// especially in non-domain machines.  Which causes ListenPipe
+	// to fail.  So, check each one for existance before adding to
+	// the final security descriptor.
+	for _, desc := range allowedGroups {
+		_, err := winio.SddlToSecurityDescriptor("D:P" + desc)
+		if err == nil {
+			secDesc += desc
+		}
+	}
+	c := winio.PipeConfig{
+		SecurityDescriptor: secDesc,
+	}
+	return winio.ListenPipe(pipename, &c)
 }
 
 // HTTP doesn't need anything from the transport, so we can use
