217ccfde3d31367bdd3b8014f54ece9df0b22afa
Xavier Vello <xavier.vello@datadoghq.com>
2017-03-31 18:41:58 +0200

    lightweight dogstatsd-alpine docker image (#211)
    lightweight dogstatsd-alpine docker image rakefile and dockerfile
    
    - new rake dogstatsd:build_static target to build a static stripped dogstastd binary
    - new Dockerfiles/dogstatsd-alpine docker builder to build a 14mb image
    - new dogstatsd_non_local_traffic option in datadog.yaml to enable access out of the container

---
+++ Dockerfiles/dogstatsd/alpine/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#set -e
+
+
+##### Core config #####
+
+touch /datadog.yaml
+
+echo "dogstatsd_non_local_traffic: yes" >> /datadog.yaml
+
+if [[ $DD_API_KEY ]]; then
+	echo "api_key: ${API_KEY}/" >> /datadog.yaml
+else
+	echo "You must set DD_API_KEY environment variable to run the Datadog Agent container"
+	exit 1
+fi
+
+if [[ $DD_URL ]]; then
+    echo "dd_url: ${DD_URL}" >> /datadog.yaml
+else
+    echo "dd_url: https://app.datadoghq.com" >> /datadog.yaml
+
+fi
+
+
+##### Starting up #####
+
+exec "$@"
