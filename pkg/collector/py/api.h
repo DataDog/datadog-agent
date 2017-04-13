c11ab70ec5b2affc291637d49d15b76301e7757f
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-04-13 10:37:09 -0400

    [py] Improve error handling in bindings (#219)
    * Improve error handling in aggregator submission bindings:
      We try to handle all easily recoverable/ignorable errors in the
      bindings directly (usually by logging the issue and continuing the
      submission). The other errors are bubbled up to the python interpreter
      which will then raise an exception.
      All python C API calls have their errors handled or a comment
      explaining why we can abstain from checking errors.
    
    * Log as much info as possible on check instance loading error:
      Log the whole traceback if there's one. If there's none log the pvalue.
      As a last resort try just logging the ptype.
      Includes tests (except for ptype, I don't know how to make the py code
      raise an exception with only a ptype unfortunately.)
    
    * Add basic README

--- pkg/collector/py/api.h
+++ pkg/collector/py/api.h
@@ -17,6 +17,7 @@ void initaggregator();
 PyObject* _none();
 int _is_none(PyObject*);
 int _PyDict_Check(PyObject*);
+int _PyInt_Check(PyObject*);
 int _PyString_Check(PyObject*);
 PyObject* PySequence_Fast_Get_Item(PyObject*, Py_ssize_t);
 Py_ssize_t PySequence_Fast_Get_Size(PyObject*);
