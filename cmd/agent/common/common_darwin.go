3e8a0ba8282a1208e69ffdc59e347a91a4adbc44
Derek Brown <derek.brown@datadoghq.com>
2017-04-27 13:22:55 -0700

    Go fmt

--- cmd/agent/common/common_darwin.go
+++ cmd/agent/common/common_darwin.go
@@ -1,9 +1,13 @@
 package common
 
+import (
+	"path/filepath"
+)
+
 const defaultConfPath = "/opt/datadog-agent/etc"
 
 // DistPath holds the path to the folder containing distribution files
-const distPath = filepath.Join(_here, "dist")
+var distPath = filepath.Join(_here, "dist")
 
 // GetDistPath returns the fully qualified path to the 'dist' directory
 func GetDistPath() string {
--- cmd/agent/common/common_linux.go
+++ cmd/agent/common/common_linux.go
@@ -1,9 +1,13 @@
 package common
 
+import (
+	"path/filepath"
+)
+
 const defaultConfPath = "/etc/dd-agent"
 
 // DistPath holds the path to the folder containing distribution files
-const distPath = filepath.Join(_here, "dist")
+var distPath = filepath.Join(_here, "dist")
 
 // GetDistPath returns the fully qualified path to the 'dist' directory
 func GetDistPath() string {
