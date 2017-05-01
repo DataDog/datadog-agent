89e8929749467a71f6d66a96e93bbfc31f1a0175
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-05-01 16:43:20 -0400

    [agent/app] Support custom py checks in conf folder
    Configurable via the `additional_checksd` option. Same behavior as
    in the agent 5:
    
    * the option has a sensible default
    * the checks from the conf folder take precedence over the ones
    from `integrations-core`

--- pkg/config/config_linux.go
+++ pkg/config/config_linux.go
@@ -1,3 +1,4 @@
 package config
 
 const defaultConfdPath = "/etc/dd-agent/conf.d"
+const defaultAdditionalChecksPath = "/etc/dd-agent/checks.d"
--- pkg/config/config_windows.go
+++ pkg/config/config_windows.go
@@ -1,3 +1,4 @@
 package config
 
 const defaultConfdPath = "c:\\programdata\\datadog\\conf.d"
+const defaultAdditionalChecksPath = "c:\\programdata\\datadog\\checks.d"
