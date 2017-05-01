b990d756b2bc3b5bd32a963552cfd5a8edcf5028
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-05-01 18:30:41 -0400

    [agent][logging] Use a rotating file for logging
    For the agent binary. The log file path can be configured, and has
    a sensible default.

--- cmd/agent/common/common_windows.go
+++ cmd/agent/common/common_windows.go
@@ -1,3 +1,4 @@
 package common
 
 const defaultConfPath = "c:\\programdata\\datadog"
+const defaultLogPath = "c:\\programdata\\datadog\\logs\\agent.log"
---
+++ pkg/config/log.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"fmt"
+
+	log "github.com/cihub/seelog"
+)
+
+const logFileMaxSize = 10 * 1024 * 1024 // 10MB
+
+func SetupLogger(logFile string) error {
+	configTemplate := `<seelog>
+    <outputs>
+        <console />
+        <rollingfile type="size" filename="%s" maxsize="%d" maxrolls="1" />
+    </outputs>
+</seelog>`
+	config := fmt.Sprintf(configTemplate, logFile, logFileMaxSize)
+
+	logger, err := log.LoggerFromConfigAsString(config)
+	if err != nil {
+		return err
+	}
+	log.ReplaceLogger(logger)
+	return nil
+}
