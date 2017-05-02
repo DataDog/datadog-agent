e850360c5602109e9d4f4f2bb2c62a6b1d9f8aa1
Derek Brown <derek.brown@datadoghq.com>
2017-05-02 14:31:10 -0700

    review feedback. Removed license separated `delete` into separate command line option

--- cmd/manage/app/getcheck.go
+++ cmd/manage/app/getcheck.go
@@ -9,18 +9,17 @@ import (
 func init() {
 	ManageCmd.AddCommand(getCheckCommand)
 	getCheckCommand.Flags().StringVarP(&checkname, "checkname", "c", "", "name of check")
-	getCheckCommand.Flags().BoolVarP(&deletecheck, "delete", "d", false, "Delete the check")
 }
 
 var getCheckCommand = &cobra.Command{
 	Use:   "getcheck",
 	Short: "Query the running agent for the status of a given check.",
 	Long:  ``,
-	RunE:  dogetCheck,
+	RunE:  doGetCheck,
 }
 
 // query for the version
-func dogetCheck(cmd *cobra.Command, args []string) error {
+func doGetCheck(cmd *cobra.Command, args []string) error {
 
 	if len(checkname) == 0 {
 		return fmt.Errorf("Must supply a check name to query")
@@ -29,12 +28,7 @@ func dogetCheck(cmd *cobra.Command, args []string) error {
 	urlstr := "http://" + sockname + "/check/" + checkname
 	var e error
 	var body []byte
-	if deletecheck {
-		// todo, change to DELETE
-		body, e = doGet(c, urlstr)
-	} else {
-		body, e = doGet(c, urlstr)
-	}
+	body, e = doGet(c, urlstr)
 
 	if e != nil {
 		fmt.Printf("Error getting check status for check %s: %s\n", checkname, e)
