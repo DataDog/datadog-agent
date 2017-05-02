a65c9d1817b59fedd6c2e1c823cd95e266cf64d3
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2017-05-02 11:34:56 -0400

    Better error handling

--- pkg/collector/metadata/gohai/gohai.go
+++ pkg/collector/metadata/gohai/gohai.go
@@ -17,30 +17,44 @@ func GetPayload() *Payload {
 	}
 }
 
-func logError(module string, err error) {
-	if err != nil {
-		log.Errorf("Failed to retrieve %s metadata: %s", module, err)
-	}
-}
-
 func getGohaiInfo() *gohai {
 
-	cpuPayload, err := new(cpu.Cpu).Collect()
-	logError("cpu", err)
-	fileSystemPayload, err := new(filesystem.FileSystem).Collect()
-	logError("filesystem", err)
-	memoryPayload, err := new(memory.Memory).Collect()
-	logError("memory", err)
-	networkPayload, err := new(network.Network).Collect()
-	logError("network", err)
-	platformPayload, err := new(platform.Platform).Collect()
-	logError("platform", err)
+	res := new(gohai)
 
-	return &gohai{
-		CPU:        cpuPayload,
-		FileSystem: fileSystemPayload,
-		Memory:     memoryPayload,
-		Network:    networkPayload,
-		Platform:   platformPayload,
+	cpuPayload, err := new(cpu.Cpu).Collect()
+	if err == nil {
+		res.CPU = cpuPayload
+	} else {
+		log.Errorf("Failed to retrieve cpu metadata: %s", err)
 	}
+
+	fileSystemPayload, err := new(filesystem.FileSystem).Collect()
+	if err == nil {
+		res.FileSystem = fileSystemPayload
+	} else {
+		log.Errorf("Failed to retrieve filesystem metadata: %s", err)
+	}
+
+	memoryPayload, err := new(memory.Memory).Collect()
+	if err == nil {
+		res.Memory = memoryPayload
+	} else {
+		log.Errorf("Failed to retrieve memory metadata: %s", err)
+	}
+
+	networkPayload, err := new(network.Network).Collect()
+	if err == nil {
+		res.Network = networkPayload
+	} else {
+		log.Errorf("Failed to retrieve network metadata: %s", err)
+	}
+
+	platformPayload, err := new(platform.Platform).Collect()
+	if err == nil {
+		res.Platform = platformPayload
+	} else {
+		log.Errorf("Failed to retrieve platform metadata: %s", err)
+	}
+
+	return res
 }
