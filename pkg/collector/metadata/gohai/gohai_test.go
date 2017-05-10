36b8f2b8539ef07fc6a59895cd1d26e332cde4e5
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2017-05-10 10:45:11 -0400

    Fix flaky test (#247)

--- pkg/collector/metadata/gohai/gohai_test.go
+++ pkg/collector/metadata/gohai/gohai_test.go
@@ -1,28 +1,16 @@
 package gohai
 
 import (
-	"github.com/DataDog/gohai/cpu"
-	"github.com/DataDog/gohai/filesystem"
-	"github.com/DataDog/gohai/memory"
-	"github.com/DataDog/gohai/network"
-	"github.com/DataDog/gohai/platform"
-
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGetPayload(t *testing.T) {
-	cpuPayload, _ := new(cpu.Cpu).Collect()
-	fileSystemPayload, _ := new(filesystem.FileSystem).Collect()
-	memoryPayload, _ := new(memory.Memory).Collect()
-	networkPayload, _ := new(network.Network).Collect()
-	platformPayload, _ := new(platform.Platform).Collect()
-
 	gohai := GetPayload()
 
-	assert.Equal(t, cpuPayload, gohai.Gohai.CPU)
-	assert.Equal(t, fileSystemPayload, gohai.Gohai.FileSystem)
-	assert.Equal(t, memoryPayload, gohai.Gohai.Memory)
-	assert.Equal(t, networkPayload, gohai.Gohai.Network)
-	assert.Equal(t, platformPayload, gohai.Gohai.Platform)
+	assert.NotNil(t, gohai.Gohai.CPU)
+	assert.NotNil(t, gohai.Gohai.FileSystem)
+	assert.NotNil(t, gohai.Gohai.Memory)
+	assert.NotNil(t, gohai.Gohai.Network)
+	assert.NotNil(t, gohai.Gohai.Platform)
 }
