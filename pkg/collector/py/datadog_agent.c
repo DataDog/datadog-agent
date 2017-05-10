783479dd3468a7e5bcf04979b7313eb36c58cd9a
Maxime Mouial <maxime.mouial@gmail.com>
2017-05-10 15:16:50 +0200

    Forward python logs to the agent logger
    This commit add a Handler to every check logger to forward log lines to the agent logger (currently seelog).

--- pkg/collector/py/datadog_agent.c
+++ pkg/collector/py/datadog_agent.c
@@ -3,6 +3,7 @@
 PyObject* GetVersion(PyObject *self, PyObject *args);
 PyObject* Headers(PyObject *self, PyObject *args);
 PyObject* GetHostname(PyObject *self, PyObject *args);
+PyObject* LogMessage(char *message, int logLevel);
 PyObject* GetConfig(char *key);
 
 static PyObject *get_config(PyObject *self, PyObject *args) {
@@ -20,11 +21,29 @@ static PyObject *get_config(PyObject *self, PyObject *args) {
     return GetConfig(key);
 }
 
+static PyObject *log_message(PyObject *self, PyObject *args) {
+    char *message;
+    int  log_level;
+
+    PyGILState_STATE gstate;
+    gstate = PyGILState_Ensure();
+
+    // datadog_agent.log(message, log_level)
+    if (!PyArg_ParseTuple(args, "si", &message, &log_level)) {
+      PyGILState_Release(gstate);
+      Py_RETURN_NONE;
+    }
+
+    PyGILState_Release(gstate);
+    return LogMessage(message, log_level);
+}
+
 static PyMethodDef datadogAgentMethods[] = {
   {"get_version", GetVersion, METH_VARARGS, "Get the Agent version."},
   {"get_config", get_config, METH_VARARGS, "Get value from the agent configuration."},
   {"headers", Headers, METH_VARARGS, "Get basic HTTP headers with the right UserAgent."},
   {"get_hostname", GetHostname, METH_VARARGS, "Get the agent hostname."},
+  {"log", log_message, METH_VARARGS, "Log a message through the agent logger."},
   {NULL, NULL}
 };
 
