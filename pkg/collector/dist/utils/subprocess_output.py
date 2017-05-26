6f48972af6dd035c2cf319b8392dd5dc57b8c9ba
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-05-26 14:56:16 -0400

    [py/utils] Add subprocess_output util (#265)
    Since this helper doesn't rely on any core agent functionality, it
    makes sense to define it as a pure python helper, at least for now.
    We can revisit this later if needed.
    
    Taken as-is from the agent5, and confirmed it works in agent6.
    
    Also, fix typo in variable name

---
+++ pkg/collector/dist/utils/subprocess_output.py
@@ -0,0 +1,57 @@
+# (C) Datadog, Inc. 2010-2017
+# All rights reserved
+
+# stdlib
+from functools import wraps
+import logging
+import subprocess
+import tempfile
+
+log = logging.getLogger(__name__)
+
+
+class SubprocessOutputEmptyError(Exception):
+    pass
+
+
+def get_subprocess_output(command, log, raise_on_empty_output=True):
+    """
+    Run the given subprocess command and return its output. Raise an Exception
+    if an error occurs.
+    """
+
+    # Use tempfile, allowing a larger amount of memory. The subprocess.Popen
+    # docs warn that the data read is buffered in memory. They suggest not to
+    # use subprocess.PIPE if the data size is large or unlimited.
+    with tempfile.TemporaryFile() as stdout_f, tempfile.TemporaryFile() as stderr_f:
+        proc = subprocess.Popen(command, stdout=stdout_f, stderr=stderr_f)
+        proc.wait()
+        stderr_f.seek(0)
+        err = stderr_f.read()
+        if err:
+            log.debug("Error while running {0} : {1}".format(" ".join(command), err))
+
+        stdout_f.seek(0)
+        output = stdout_f.read()
+
+    if not output and raise_on_empty_output:
+        raise SubprocessOutputEmptyError("get_subprocess_output expected output but had none.")
+
+    return (output, err, proc.returncode)
+
+
+def log_subprocess(func):
+    """
+    Wrapper around subprocess to log.debug commands.
+    """
+    @wraps(func)
+    def wrapper(*params, **kwargs):
+        fc = "%s(%s)" % (func.__name__, ', '.join(
+            [a.__repr__() for a in params] +
+            ["%s = %s" % (a, b) for a, b in kwargs.items()]
+        ))
+        log.debug("%s called" % fc)
+        return func(*params, **kwargs)
+    return wrapper
+
+subprocess.Popen = log_subprocess(subprocess.Popen)
