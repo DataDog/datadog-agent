c5ad72d7a924f11675b27285ae732166c95019ae
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-05-26 13:37:37 -0400

    [iostats] refactor, windows numbers differ greatly. Currently not BW compatible.

--- pkg/collector/corechecks/system/iostats.go
+++ pkg/collector/corechecks/system/iostats.go
@@ -42,37 +42,7 @@ func (c *IOCheck) String() string {
 	return "IOCheck"
 }
 
-func (c *IOCheck) nixSpecificIO(ioStats, ioStats2 *disk.IOCountersStat, tags []string) {
-	rrqms := (ioStats2.MergedReadCount - ioStats.MergedReadCount)
-	wrqms := (ioStats2.MergedWriteCount - ioStats.MergedWriteCount)
-	avgqusz := float64(ioStats2.WeightedIO-ioStats.WeightedIO) / 1000
-
-	diffNRIO := float64(ioStats2.ReadCount - ioStats.ReadCount)
-	diffNWIO := float64(ioStats2.WriteCount - ioStats.WriteCount)
-	diffNIO := diffNRIO + diffNWIO
-
-	tput := diffNIO * float64(hz)
-	util := float64(ioStats2.IoTime - ioStats.IoTime)
-	svctime := 0.0
-	if tput != 0 {
-		svctime = util / tput
-	}
-
-	c.sender.Gauge("system.io.rrqm_s", float64(rrqms), "", tags)
-	c.sender.Gauge("system.io.wrqm_s", float64(wrqms), "", tags)
-	c.sender.Gauge("system.io.avg_q_sz", avgqusz, "", tags)
-	if hz > 0 { // only send if we were able to collect HZ
-		c.sender.Gauge("system.io.svctm", svctime, "", tags)
-	}
-
-	// Stats should be per device no device groups.
-	// If device groups ever become a thing - util / 10.0 / n_devs_in_group
-	// See more: (https://github.com/sysstat/sysstat/blob/v11.5.6/iostat.c#L1033-L1040)
-	c.sender.Gauge("system.io.util", (util / 10.0), "", tags)
-}
-
-// Run executes the check
-func (c *IOCheck) Run() error {
+func (c *IOCheck) nixIO() error {
 	// See: https://www.xaprb.com/blog/2010/01/09/how-linux-iostat-computes-its-results/
 	//      https://www.kernel.org/doc/Documentation/iostats.txt
 	iomap, err := ioCounters()
@@ -89,6 +59,7 @@ func (c *IOCheck) Run() error {
 		return err
 	}
 
+	var tagbuff bytes.Buffer
 	for device, ioStats := range iomap {
 		ioStats2, ok := iomap2[device]
 		if !ok {
@@ -96,15 +67,14 @@ func (c *IOCheck) Run() error {
 			continue
 		}
 
-		var tagbuff bytes.Buffer
-		tagbuff.WriteString("device:")
-		tagbuff.WriteString(device)
-
 		// TODO: Different OS's might not have everything - make this OSX/Windows safe
 		rs := (ioStats2.ReadCount - ioStats.ReadCount)
 		ws := (ioStats2.WriteCount - ioStats.WriteCount)
 		rkbs := float64(ioStats2.ReadBytes-ioStats.ReadBytes) / kB
 		wkbs := float64(ioStats2.WriteBytes-ioStats.WriteBytes) / kB
+		rrqms := (ioStats2.MergedReadCount - ioStats.MergedReadCount)
+		wrqms := (ioStats2.MergedWriteCount - ioStats.MergedWriteCount)
+		avgqusz := float64(ioStats2.WeightedIO-ioStats.WeightedIO) / 1000
 
 		rAwait := 0.0
 		wAwait := 0.0
@@ -119,14 +89,24 @@ func (c *IOCheck) Run() error {
 
 		avgrqsz := 0.0
 		aWait := 0.0
-
-		diffNIO := float64(diffNRIO + diffNWIO)
+		diffNIO := diffNRIO + diffNWIO
 		if diffNIO != 0 {
 			avgrqsz = float64((ioStats2.ReadBytes-ioStats.ReadBytes+ioStats2.WriteBytes-ioStats.WriteBytes)/SectorSize) / diffNIO
 			aWait = float64(ioStats2.ReadTime-ioStats.ReadTime+ioStats2.WriteTime-ioStats.WriteTime) / diffNIO
 		}
 
+		tput := diffNIO * float64(hz)
+		util := float64(ioStats2.IoTime - ioStats.IoTime)
+		svctime := 0.0
+		if tput != 0 {
+			svctime = util / tput
+		}
+
+		tagbuff.Reset()
+		tagbuff.WriteString("device:")
+		tagbuff.WriteString(device)
 		tags := []string{tagbuff.String()}
+
 		c.sender.Gauge("system.io.r_s", float64(rs), "", tags)
 		c.sender.Gauge("system.io.w_s", float64(ws), "", tags)
 		c.sender.Gauge("system.io.rkb_s", rkbs, "", tags)
@@ -135,15 +115,62 @@ func (c *IOCheck) Run() error {
 		c.sender.Gauge("system.io.await", aWait, "", tags)
 		c.sender.Gauge("system.io.r_await", float64(rAwait), "", tags)
 		c.sender.Gauge("system.io.w_await", float64(wAwait), "", tags)
-
-		if runtime.GOOS != "windows" { //unavailable on windows
-			c.nixSpecificIO(&ioStats, &ioStats2, tags)
+		c.sender.Gauge("system.io.rrqm_s", float64(rrqms), "", tags)
+		c.sender.Gauge("system.io.wrqm_s", float64(wrqms), "", tags)
+		c.sender.Gauge("system.io.avg_q_sz", avgqusz, "", tags)
+		if hz > 0 { // only send if we were able to collect HZ
+			c.sender.Gauge("system.io.svctm", svctime, "", tags)
 		}
 
-		c.sender.Commit()
+		// Stats should be per device no device groups.
+		// If device groups ever become a thing - util / 10.0 / n_devs_in_group
+		// See more: (https://github.com/sysstat/sysstat/blob/v11.5.6/iostat.c#L1033-L1040)
+		c.sender.Gauge("system.io.util", (util / 10.0), "", tags)
+
 	}
 
 	return nil
+}
+
+func (c *IOCheck) windowsIO() error {
+	iomap, err := ioCounters()
+	if err != nil {
+		log.Errorf("system.IOCheck: could not retrieve io stats: %s", err)
+		return err
+	}
+
+	var tagbuff bytes.Buffer
+	for device, ioStats := range iomap {
+		tagbuff.Reset()
+		tagbuff.WriteString("device:")
+		tagbuff.WriteString(device)
+		tags := []string{tagbuff.String()}
+
+		c.sender.Gauge("system.io.r_s", float64(ioStats.ReadCount), "", tags)
+		c.sender.Gauge("system.io.w_s", float64(ioStats.WriteCount), "", tags)
+		c.sender.Gauge("system.io.rkb_s", float64(ioStats.ReadBytes)/kB, "", tags)
+		c.sender.Gauge("system.io.wkb_s", float64(ioStats.WriteBytes)/kB, "", tags)
+		// TODO: c.sender.Gauge("system.io.avg_q_sz", avgqusz, "", tags)
+	}
+
+	return nil
+}
+
+// Run executes the check
+func (c *IOCheck) Run() error {
+	var err error
+
+	switch os := runtime.GOOS; os {
+	case "windows":
+		err = c.windowsIO()
+	default: // Should cover Unices (Linux, OSX, FreeBSD,...)
+		err = c.nixIO()
+	}
+
+	if err == nil {
+		c.sender.Commit()
+	}
+	return err
 }
 
 // Configure the CPU check doesn't need configuration
