b76e4aecad3cadc7679e118d7fe9ac4fb0e28ef6
Xavier Vello <xavier.vello@datadoghq.com>
2017-05-30 15:03:36 +0200

    docker integration test simplification
    - move the image creation to the docker:build_dogstatsd task
    - remove docker installation as the runners will have it
    - don't bind the socket out of the container (not needed for the test)
    - update .gitignore

--- rake/docker.rb
+++ rake/docker.rb
@@ -1,17 +1,24 @@
 require_relative './common'
 
 namespace :docker do
+  DOGSTATSD_TAG="datadog/dogstatsd:master"
 
-  desc "Run docker integration tests"
-  task :integration_test  do
+  desc "Build datadog/dogstatsd docker image"
+  task :build_dogstatsd do
     if ENV['skip_rebuild'] == "true" then
       puts "Skipping DogStatsD build"
     else
-      puts "Building DogStatsD static binary"
+      puts "Building DogStatsD"
       Rake::Task["dogstatsd:build_static"].invoke
     end
+    FileUtils.cp("bin/static/dogstatsd", "Dockerfiles/dogstatsd/alpine/")
+    system("docker build -t #{DOGSTATSD_TAG} Dockerfiles/dogstatsd/alpine/") || exit(1)
+  end
+
+  desc "Run docker integration tests"
+  task :integration_test => %w[docker:build_dogstatsd]  do
     puts "Starting docker integration tests"
-    system("bash test/integration/docker/dsd_alpine_listening.sh") || exit(1)
+    system("DOCKER_IMAGE=#{DOGSTATSD_TAG} ./test/integration/docker/dsd_alpine_listening.sh") || exit(1)
   end
 
 end
--- test/integration/docker/dsd_alpine_listening.sh
+++ test/integration/docker/dsd_alpine_listening.sh
@@ -1,34 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 #
-# System check for the dsd-alpine image. Builds and runs the image
-# both in UDP and socket mode. With lsof, we test that dogstatsd is
-# running and listening on the right interface
+# System check for the dsd-alpine image. Runs the image both in UDP and
+# socket mode. With lsof, we test that dogstatsd is running and listening
+# on the right interface
 
-DOCKER_IMAGE="dsd-alpine:system_test"
-SOCKET_PATH="/statsd/statsd.socket"
-DD_ARGS="-e DD_DD_URL=http://dummy -e DD_API_KEY=dummy -v `pwd`:/statsd:rw"
-TEST_FAIL=0
 
-if [ !$(which docker) ]; then
-    echo "Installing docker (temporary workaround, to remove when builder image has docker installed)"
-    curl -sL https://get.docker.com/builds/Linux/x86_64/docker-17.04.0-ce.tgz | tar xfz -
-    chmod -R 0777 docker
-    export PATH=$PWD/docker:$PATH
-fi
-
-echo "Building docker image:"
-
-OUT=`cp bin/static/dogstatsd Dockerfiles/dogstatsd/alpine && \
-docker build -t $DOCKER_IMAGE Dockerfiles/dogstatsd/alpine`
-
-if [ $? -ne 0 ]; then
-    echo "Error building the imagge"
-    echo $OUT
-    docker image rm $DOCKER_IMAGE > /dev/null
+# $DOCKER_IMAGE has to be given as an environment variable
+if [ -z $DOCKER_IMAGE ]; then
+    echo "You must set the DOCKER_IMAGE environment variable to run the test"
     exit 1
-else
-    echo "OK"
 fi
+
+SOCKET_PATH="/tmp/statsd.socket"
+DD_ARGS="-e DD_DD_URL=http://dummy -e DD_API_KEY=dummy"
+TEST_FAIL=0
 
 # UDP_CO should listen on UDP 8125, but not on the socket
 
@@ -58,6 +43,7 @@ fi
 docker stop $UDP_CO > /dev/null
 
 # SOCKET_CO should listen on the socket, but not on UDP 8125
+# We don't bind the socket out of the container as we don't need to send anything
 
 SOCKET_CO=`docker run --rm -d -e DD_DOGSTATSD_SOCKET=$SOCKET_PATH $DD_ARGS $DOCKER_IMAGE`
 
@@ -84,13 +70,7 @@ fi
 
 docker stop $SOCKET_CO > /dev/null
 
-# Cleanup and conclusion
-
-if [ -f statsd.socket ]; then
-    rm statsd.socket
-fi
-
-docker image rm $DOCKER_IMAGE > /dev/null
+# Conclusion
 
 if [ $TEST_FAIL -eq 0 ]; then
     echo "Test succeeded"
