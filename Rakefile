1bec0113428a24b190b2afcc1bb8042f7ec4e598
Xavier Vello <xavier.vello@datadoghq.com>
2017-05-31 10:45:13 +0200

    add docker image integration test (#261)
    - add rake docker:build_dogstatsd target to build dogstatsd docker image
    - add rake docker:integration_test target for integration test
    - rework gitlab pipeline to run integration test before package building
    - update .gitignore

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@ require_relative './rake/common'
 require_relative './rake/agent'
 require_relative './rake/dogstatsd'
 require_relative './rake/benchmarks'
+require_relative './rake/docker'
 
 PKG_CONFIG_LIBDIR=File.join(Dir.pwd, "pkg-config", os)
 ORG_PATH="github.com/DataDog"
---
+++ test/integration/docker/dsd_alpine_listening.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# System check for the dsd-alpine image. Runs the image both in UDP and
+# socket mode. With lsof, we test that dogstatsd is running and listening
+# on the right interface
+
+
+# $DOCKER_IMAGE has to be given as an environment variable
+if [ -z $DOCKER_IMAGE ]; then
+    echo "You must set the DOCKER_IMAGE environment variable to run the test"
+    exit 1
+fi
+
+SOCKET_PATH="/tmp/statsd.socket"
+DD_ARGS="-e DD_DD_URL=http://dummy -e DD_API_KEY=dummy"
+TEST_FAIL=0
+
+# UDP_CO should listen on UDP 8125, but not on the socket
+
+UDP_CO=`docker run --rm -d $DD_ARGS $DOCKER_IMAGE`
+
+echo "Testing UDP container:"
+docker exec $UDP_CO apk add --no-cache lsof > /dev/null
+
+OUT=`docker exec $UDP_CO lsof -U | grep $SOCKET_PATH`
+if [ $? -ne 1 ]; then
+    TEST_FAIL=1
+    echo "Error: listening on socket"
+    echo $OUT
+fi
+
+OUT=`docker exec $UDP_CO lsof -i | grep "*:8125"`
+if [ $? -ne 0 ]; then
+    TEST_FAIL=1
+    echo "Error: not listening on UDP"
+    echo $OUT
+fi
+
+if [ $TEST_FAIL -eq 0 ]; then
+    echo "OK"
+fi
+
+docker stop $UDP_CO > /dev/null
+
+# SOCKET_CO should listen on the socket, but not on UDP 8125
+# We don't bind the socket out of the container as we don't need to send anything
+
+SOCKET_CO=`docker run --rm -d -e DD_DOGSTATSD_SOCKET=$SOCKET_PATH $DD_ARGS $DOCKER_IMAGE`
+
+echo "Testing socket container:"
+docker exec $SOCKET_CO apk add --no-cache lsof > /dev/null
+
+OUT=`docker exec $SOCKET_CO lsof -U | grep $SOCKET_PATH`
+if [ $? -ne 0 ]; then
+    TEST_FAIL=1
+    echo "Error: not listening on socket"
+    echo $OUT
+fi
+
+OUT=`docker exec $SOCKET_CO lsof -i | grep 8125`
+if [ $? -ne 1 ]; then
+    TEST_FAIL=1
+    echo "Error: listening on UDP"
+    echo $OUT
+fi
+
+if [ $TEST_FAIL -eq 0 ]; then
+    echo "OK"
+fi
+
+docker stop $SOCKET_CO > /dev/null
+
+# Conclusion
+
+if [ $TEST_FAIL -eq 0 ]; then
+    echo "Test succeeded"
+else
+    echo "Test failed"
+fi
+
+exit $TEST_FAIL
