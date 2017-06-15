d0a044fe657a7b9a50b1648fedefb70eb26ab764
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-06-15 05:11:55 -0400

    [py] Support yet another `AgentCheck.__init__` pattern (#298)
    This time when `instances` is the only argument passed in `kwargs`.
    
    This pattern is quite common in the existing checks (since it's the
    signature of the agent5's `AgentCheck.__init__`), so it makes sense
    to support it.
    
    Added a test case, and assertions in the python test code.

--- pkg/collector/py/tests/old_alt_init_signature.py
+++ pkg/collector/py/tests/old_alt_init_signature.py
@@ -2,8 +2,8 @@ from checks import AgentCheck
 
 
 class TestCheck(AgentCheck):
-    def __init__(self, *args, **kwargs):
-        super(TestCheck, self).__init__(*args, **kwargs)
+    def __init__(self, name, init_config, agentConfig, instances=None):
+        super(TestCheck, self).__init__(name, init_config, agentConfig, instances=instances)
         assert hasattr(self, 'instances')
         assert isinstance(self.instances, list)
         assert len(self.instances) > 0
