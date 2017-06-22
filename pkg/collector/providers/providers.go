367ee2bde0b451371d9585ae388ac8e7eb0b3b46
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-06-22 18:24:46 +0200

    [providers] fixing lint issues.

--- pkg/collector/providers/providers.go
+++ pkg/collector/providers/providers.go
@@ -1,9 +1,9 @@
 package providers
 
-// LoaderCatalog keeps track of Go loaders by name
+// ProviderCatalog keeps track of Go loaders by name
 var ProviderCatalog = make(map[string]ConfigProvider)
 
-// RegisterLoader adds a loader to the loaderCatalog
+// RegisterProvider adds a loader to the loaderCatalog
 func RegisterProvider(name string, p ConfigProvider) {
 	ProviderCatalog[name] = p
 }
