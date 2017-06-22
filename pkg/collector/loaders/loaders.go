0d20757b31855798d5f8f09052b8f090c6fa85ef
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-06-22 14:03:09 +0200

    [lint] fixing lint issues.

--- pkg/collector/loaders/loaders.go
+++ pkg/collector/loaders/loaders.go
@@ -4,9 +4,10 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/collector/check"
 )
 
+// LoaderFactory a factory to check loaders
 type LoaderFactory func() check.Loader
 
-// Catalog keeps track of Go loaders by name
+// LoaderCatalog keeps track of Go loaders by name
 var LoaderCatalog = make(map[string]LoaderFactory)
 
 // RegisterLoader adds a loader to the loaderCatalog
