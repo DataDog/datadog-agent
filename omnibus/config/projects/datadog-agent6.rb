9e67996abcd3cf4895d2b27a61f9e9e0a60bfc8c
Greg Meyer <greg.meyer@datadoghq.com>
2017-06-28 15:36:43 -0400

    changes conditional

--- omnibus/config/projects/datadog-agent6.rb
+++ omnibus/config/projects/datadog-agent6.rb
@@ -107,12 +107,7 @@ end
 
 # Linux
 if linux?
-  if debian?
-    extra_package_file '/etc/init/datadog-agent6.conf'
-    extra_package_file '/lib/systemd/system/datadog-agent6.service'
-  end
-
-  if redhat?
+  if debian? || redhat?
     extra_package_file '/etc/init/datadog-agent6.conf'
     extra_package_file '/lib/systemd/system/datadog-agent6.service'
   end
