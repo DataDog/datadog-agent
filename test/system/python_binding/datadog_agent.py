3e52c5fbb74a567a774c5d7b1f279a1a2de4e952
Maxime Mouial <maxime.mouial@gmail.com>
2017-06-30 17:58:35 +0200

    Adding some very basic tests for the 'datadog-agent' synthetic module
    The new binary 'py-launcher' run a python script as the agent would. This allow us to write python test with every custom Go module loaded.

---
+++ test/system/python_binding/datadog_agent.py
@@ -0,0 +1,22 @@
+import sys
+import socket
+import nose
+from nose.tools import assert_equals
+import datadog_agent
+
+
+def test_agent_verion():
+    assert_equals(datadog_agent.get_version(), "6.0.0")
+
+def test_get_config():
+    assert_equals(datadog_agent.get_config('dd_url'), "https://test.datadoghq.com")
+
+def test_headers():
+    assert_equals(datadog_agent.headers(), {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'text/html, */*', 'User-Agent': 'Datadog Agent/6.0.0'})
+
+def test_get_hostname():
+    assert_equals(datadog_agent.get_hostname(), "test.hostname")
+
+if __name__ == '__main__':
+    if not nose.run(defaultTest=__name__):
+        sys.exit(1)
