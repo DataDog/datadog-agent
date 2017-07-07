5c00b3c1b24ec3a2f35953d9f9d5126c3142c643
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-07-08 01:59:03 +0200

    [dogstatsd6][test] removing legacy tests - no longer needed.

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"bytes"
-	"path"
-	"runtime"
 	"testing"
 
 	"github.com/spf13/viper"
@@ -103,58 +101,4 @@ additional_endpoints:
 
 	assert.Nil(t, err)
 	assert.EqualValues(t, expectedMultipleEndpoints, multipleEndpoints)
-}
-
-func TestGetMultipleEndpointsApiKeyDeduping(t *testing.T) {
-	datadogYaml := `
-dd_url: "https://app.datadoghq.com"
-api_key: fakeapikey
-
-additional_endpoints:
-  "https://app.datadoghq.com":
-  - fakeapikey2
-  - fakeapikey
-  "https://foo.datadoghq.com":
-  - someapikey
-  - someotherapikey
-  - someapikey
-`
-
-	testConfig := setupViperConf(datadogYaml)
-
-	multipleEndpoints, err := getMultipleEndpoints(testConfig)
-
-	expectedMultipleEndpoints := map[string][]string{
-		"https://app.datadoghq.com": {
-			"fakeapikey",
-			"fakeapikey2",
-		},
-		"https://foo.datadoghq.com": {
-			"someapikey",
-			"someotherapikey",
-		},
-	}
-
-	assert.Nil(t, err)
-	assert.EqualValues(t, expectedMultipleEndpoints, multipleEndpoints)
-}
-
-func TestLegacyIniConfig(t *testing.T) {
-	origCfgPath := Datadog.GetString("conf_path")
-	defer Datadog.Set("conf_path", origCfgPath)
-
-	_, f, _, _ := runtime.Caller(0)
-	cfgIni := path.Join(path.Dir(f), "test", "datadog.conf.example")
-
-	Datadog.Set("conf_path", cfgIni)
-	err := ReadLegacyConfig()
-
-	assert.Nil(t, err)
-
-	assert.EqualValues(t, Datadog.GetString("hostname"), "foo.bar")
-	assert.EqualValues(t, Datadog.GetString("api_key"), "deadbeeffeebdaeddeadbeeffeebdaed")
-	assert.EqualValues(t, Datadog.GetBool("use_dogstatsd"), true)
-	assert.EqualValues(t, Datadog.GetInt64("dogstatsd_port"), 8126)
-	assert.EqualValues(t, Datadog.GetInt64("dogstatsd_buffer_size"), 16384)
-	assert.EqualValues(t, Datadog.GetBool("dogstatsd_stats_enable"), true)
 }
