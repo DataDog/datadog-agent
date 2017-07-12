b8fd6a3882c1c30c869fcd966e1d3680ae52d2e4
jeerim <jee.rim@datadoghq.com>
2017-07-12 09:21:32 -0400

    [percentile] change GKArray methods to value receivers

--- pkg/aggregator/percentile/sketch_series.go
+++ pkg/aggregator/percentile/sketch_series.go
@@ -36,6 +36,16 @@ func NewQSketch() QSketch {
 	return QSketch{NewGKArray()}
 }
 
+// Add a value to the qsketch
+func (q QSketch) Add(v float64) QSketch {
+	return QSketch{GKArray: q.GKArray.Add(v)}
+}
+
+// Compress the qsketch
+func (q QSketch) Compress() QSketch {
+	return QSketch{GKArray: q.GKArray.Compress()}
+}
+
 // NoSketchError is the error returned when not enough samples have been
 //submitted to generate a sketch
 type NoSketchError struct{}
@@ -79,7 +89,6 @@ func unmarshalSketches(summarySketches []agentpayload.SketchPayload_Summary_Sket
 			})
 	}
 	return sketches
-
 }
 
 // MarshalSketchSeries serializes sketch series using protocol buffers
