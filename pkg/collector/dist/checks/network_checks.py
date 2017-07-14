d7652e212fc08482a09e5ece0220e4d9b593fb8d
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-07-13 20:17:15 -0400

    [py] Add NetworkCheck class
    Thin class that allows running the integrations-core `NetworkCheck`s
    out of the box.
    
    Also, added the `default_integration_http_timeout` config parameter
    and `AgentCheck` attribute that some checks use.

---
+++ pkg/collector/dist/checks/network_checks.py
@@ -0,0 +1,53 @@
+from checks import AgentCheck
+
+
+class Status:
+    DOWN = "DOWN"
+    WARNING = "WARNING"
+    CRITICAL = "CRITICAL"
+    UP = "UP"
+
+
+# Deprecated since we don't reporting statuses as events anymore
+# Keep the class here so that imports don't fail
+class EventType:
+    pass
+
+
+class NetworkCheck(AgentCheck):
+    """
+    This class should never be directly instantiated.
+    """
+
+    STATUS_TO_SERVICE_CHECK = {
+        Status.UP: AgentCheck.OK,
+        Status.WARNING: AgentCheck.WARNING,
+        Status.CRITICAL: AgentCheck.CRITICAL,
+        Status.DOWN: AgentCheck.CRITICAL,
+    }
+
+    def check(self, instance):
+        try:
+            statuses = self._check(instance)
+        except Exception:
+            self.log.exception(
+                u"Failed to run instance '%s'.", instance.get('name', u"")
+            )
+        else:
+            if isinstance(statuses, tuple):
+                # Assume the check only returns one service check
+                status, msg = statuses
+                self.report_as_service_check(None, status, instance, msg)
+
+            elif isinstance(statuses, list):
+                for status in statuses:
+                    sc_name, status, msg = status
+                    self.report_as_service_check(sc_name, status, instance, msg)
+
+    def _check(self, instance):
+        """This function should be implemented by inherited classes"""
+        raise NotImplementedError
+
+    def report_as_service_check(self, sc_name, status, instance, msg=None):
+        """This function should be implemented by inherited classes"""
+        raise NotImplementedError
