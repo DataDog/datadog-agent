7faf708c137444ab54abbbdfc47e040cc78fb690
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-07-26 14:10:09 -0400

    [metadata/gohai] Marshal value twice to comply with v5 format

--- pkg/metadata/v5/payload_gohai.go
+++ pkg/metadata/v5/payload_gohai.go
@@ -10,8 +10,44 @@ import (
 )
 
 // GohaiPayload wraps Payload from the gohai package
+// As weird as it sounds, in the v5 payload the value of the "gohai" field
+// is a JSON-formatted string. So this struct contains a MarshalledGohaiPayload
+// which will be marshalled as a JSON-formatted string.
 type GohaiPayload struct {
-	gohai.Payload
+	Marshalled MarshalledGohaiPayload `json:"gohai"`
+}
+
+// MarshalledGohaiPayload contains the marshalled payload
+type MarshalledGohaiPayload struct {
+	gohai gohai.Payload
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+// It marshals the gohai struct twice (to a string) to comply with
+// the v5 payload format
+func (m MarshalledGohaiPayload) MarshalJSON() ([]byte, error) {
+	marshalledPayload, err := json.Marshal(m.gohai.Gohai)
+	if err != nil {
+		return []byte(""), err
+	}
+	doubleMarshalledPayload, err := json.Marshal(string(marshalledPayload))
+	if err != nil {
+		return []byte(""), err
+	}
+	return doubleMarshalledPayload, nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// Unmarshals the passed bytes twice (first to a string, then to gohai.Gohai)
+func (m *MarshalledGohaiPayload) UnmarshalJSON(bytes []byte) error {
+	firstUnmarshall := ""
+	err := json.Unmarshal(bytes, &firstUnmarshall)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal([]byte(firstUnmarshall), &(m.gohai.Gohai))
+	return err
 }
 
 // Payload handles the JSON unmarshalling of the metadata payload
