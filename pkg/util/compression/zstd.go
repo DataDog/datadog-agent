69e318e1d4c41b0b46d5f84c9a35ed0dd98c0d3a
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-08-10 14:54:32 -0400

    [compression] Use zlib by default instead of zstd
    The `zstd` version used in the backend is currently not suitable for
    use by the agent: it still uses an unstable (pre-v1) version of the
    compression format.
    
    Let's use `zlib` until the backend makes use of a stable version of
    the `zstd` compression format.

--- pkg/util/compression/zstd.go
+++ pkg/util/compression/zstd.go
@@ -4,6 +4,9 @@ package compression
 
 import "github.com/DataDog/zstd"
 
+// TODO: the intake still uses a pre-v1 (unstable) version of the zstd compression format.
+// The agent shouldn't use zstd compression until the intake supports a stable v1 format.
+
 // ContentEncoding describes the HTTP header value associated with the compression method
 // var instead of const to ease testing
 var ContentEncoding = "zstd"
