2dd5ddb5086af2de2b008a202a2f9e743f691383
Massimiliano Pippi <mpippi@gmail.com>
2017-08-18 17:59:26 +0200

    Use unittests to save a dependency (#473)
    * drop nosetests in favor of unittests to avoid a dependency
    
    * added output message

--- test/system/python_binding/datadog_agent.py
+++ test/system/python_binding/datadog_agent.py
@@ -2,26 +2,26 @@
 # under the Apache License Version 2.0.
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2017 Datadog, Inc.
-
+import unittest
 import sys
 import socket
-import nose
-from nose.tools import assert_equals
 import datadog_agent
 
 
-def test_agent_verion():
-    assert_equals(datadog_agent.get_version(), "6.0.0")
+class TestAgent(unittest.TestCase):
 
-def test_get_config():
-    assert_equals(datadog_agent.get_config('dd_url'), "https://test.datadoghq.com")
+    def test_agent_version(self):
+        self.assertEqual(datadog_agent.get_version(), "6.0.0")
 
-def test_headers():
-    assert_equals(datadog_agent.headers(), {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'text/html, */*', 'User-Agent': 'Datadog Agent/6.0.0'})
+    def test_get_config(self):
+        self.assertEqual(datadog_agent.get_config('dd_url'), "https://test.datadoghq.com")
 
-def test_get_hostname():
-    assert_equals(datadog_agent.get_hostname(), "test.hostname")
+    def test_headers(self):
+        self.assertEqual(datadog_agent.headers(), {'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'text/html, */*', 'User-Agent': 'Datadog Agent/6.0.0'})
+
+    def test_get_hostname(self):
+        self.assertEqual(datadog_agent.get_hostname(), "test.hostname")
+
 
 if __name__ == '__main__':
-    if not nose.run(defaultTest=__name__):
-        sys.exit(1)
+    unittest.main()
