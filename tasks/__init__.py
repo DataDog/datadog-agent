56450f5d651f099f2e1f5c067c6cb80016489886
Massimiliano Pippi <mpippi@gmail.com>
2017-09-01 11:18:17 +0200

    fixed invoke run on windows

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,6 +1,7 @@
 """
 Invoke entrypoint, import here all the tasks we want to make available
 """
+import os
 from invoke import Collection
 
 from . import agent, benchmarks, docker, dogstatsd, pylauncher
@@ -27,3 +28,11 @@ ns.add_collection(benchmarks, name="bench")
 ns.add_collection(docker)
 ns.add_collection(dogstatsd)
 ns.add_collection(pylauncher)
+
+# workaround waiting for a fix being merged on Invoke,
+# see https://github.com/pyinvoke/invoke/pull/407
+ns.configure({
+    'run': {
+        'shell': os.environ.get('COMSPEC', os.environ.get('SHELL'))
+    }
+})
