2b1ef0999b25c5f721470311a66b08e508ad395c
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-09-04 18:40:01 +0200

    [jmx] implement singleton behavior.

--- pkg/collector/corechecks/embed/jmx.go
+++ pkg/collector/corechecks/embed/jmx.go
@@ -44,15 +44,6 @@ var jmxChecks = [...]string{
 	"kafka",
 }
 
-// Structures to parse the yaml containing a list of jmx checks config files
-type instanceCfg struct {
-	Files []string `yaml:"files"`
-}
-
-type jmxCfg struct {
-	instance instanceCfg
-}
-
 // Structures to parse the config of an actual jmx check
 type conf struct {
 	Include map[string]interface{} `yaml:"include,omitempty"`
@@ -85,118 +76,17 @@ type checkInitCfg struct {
 	Conf           []conf   `yaml:"conf,omitempty"`
 }
 
-type checkCfg struct {
-	InitConf  checkInitCfg       `yaml:"init_config,omitempty"`
-	Instances []checkInstanceCfg `yaml:"instances,omitempty"`
-}
-
-func (cfg *jmxCfg) Parse(data []byte) error {
-
-	if err := yaml.Unmarshal(data, &(cfg.instance)); err != nil {
-		return err
-	}
-
-	if len(cfg.instance.Files) == 0 {
-		return fmt.Errorf("Error parsing configuration: no config files")
-	}
-
-	return nil
-
-}
-
-func (cfg *checkCfg) Parse(data []byte) error {
-
-	if err := yaml.Unmarshal(data, cfg); err != nil {
-		return err
-	}
-
-	return nil
-
-}
-
-func readJMXConf(checkConf *checkCfg, filename string) (
-	javaBinPath string, javaOptions string, toolsJarPath string, customJarPaths []string, err error) {
-
-	javaBinPath = checkConf.InitConf.JavaBinPath
-	javaOptions = checkConf.InitConf.JavaOptions
-	toolsJarPath = checkConf.InitConf.ToolsJarPath
-	customJarPaths = checkConf.InitConf.CustomJarPaths
-	isAttachAPI := false
-
-	if len(checkConf.Instances) == 0 {
-		return "", "", "", nil, fmt.Errorf("You need to have at least one instance " +
-			"defined in the YAML file for this check")
-	}
-
-	for _, instance := range checkConf.Instances {
-		// if these were not set in init config but in an instance, set to the first one encountered
-		if toolsJarPath == "" && instance.ToolsJarPath != "" {
-			toolsJarPath = instance.ToolsJarPath
-		}
-		if javaBinPath == "" && instance.JavaBinPath != "" {
-			javaBinPath = instance.JavaBinPath
-		}
-		if javaOptions == "" && instance.JavaOptions != "" {
-			javaOptions = instance.JavaOptions
-		}
-
-		if instance.ProcessNameRegex != "" {
-			isAttachAPI = true
-		} else if instance.JMXUrl != "" {
-			if instance.Name == "" {
-				return "", "", "", nil, fmt.Errorf("A name must be specified when using a jmx_url")
-			}
-		} else {
-			if instance.Host == "" {
-				return "", "", "", nil, fmt.Errorf("A host must be specified")
-			}
-			if instance.Port == 0 {
-				return "", "", "", nil, fmt.Errorf("A numeric port must be specified")
-			}
-		}
-
-		confs := instance.Conf
-		if confs == nil {
-			confs = checkConf.InitConf.Conf
-		}
-
-		if len(confs) == 0 {
-			log.Warnf("%s doesn't have a 'conf' section. Only basic JVM metrics"+
-				" will be collected. %s", filename, linkToDoc)
-		} else {
-			for _, conf := range confs {
-				if len(conf.Include) == 0 {
-					return "", "", "", nil, fmt.Errorf("Each configuration must have an"+
-						" 'include' section. %s", linkToDoc)
-				}
-			}
-		}
-	}
-
-	if isAttachAPI {
-		if toolsJarPath == "" {
-			return "", "", "", nil, fmt.Errorf("You must specify the path to tools.jar. %s", linkToDoc)
-		} else if _, err := os.Open(toolsJarPath); err != nil {
-			return "", "", "", nil, fmt.Errorf("Unable to find tools.jar at %s", toolsJarPath)
-		}
-	} else {
-		toolsJarPath = ""
-	}
-
-	for _, path := range customJarPaths {
-		if _, err := os.Open(path); err != nil {
-			return "", "", "", nil, fmt.Errorf("Unable to find custom jar at %s", path)
-		}
-	}
-
-	return javaBinPath, javaOptions, toolsJarPath, customJarPaths, nil
-}
-
 // JMXCheck keeps track of the running command
 type JMXCheck struct {
-	cmd          *exec.Cmd
-	checks       map[string]struct{}
-	ExitFilePath string
+	cmd                *exec.Cmd
+	checks             map[string]struct{}
+	ExitFilePath       string
+	javaBinPath        string
+	javaOptions        string
+	javaToolsJarPath   string
+	javaCustomJarPaths []string
+	isAttachAPI        bool
+	running            bool
 }
 
 // singleton for the JMXCheck
@@ -213,11 +103,74 @@ func (c *JMXCheck) Run() error {
 		return fmt.Errorf("No JMX checks configured - skipping.")
 	}
 
-	checkArgs := []string{"--check"}
-	for check, _ := range c.checks {
-		checkArgs = append(checkArgs, check)
+	if c.running {
+		log.Info("JMX already running.")
+		return nil
 	}
-	c.cmd.Args = append(c.cmd.Args, checkArgs...)
+
+	here, _ := osext.ExecutableFolder()
+	jmxConfPath := path.Join(config.Datadog.GetString("confd_path"), "jmx")
+	classpath := path.Join(here, "dist", "jmx", jmxJarName)
+	if c.javaToolsJarPath != "" {
+		classpath = fmt.Sprintf("%s:%s", c.javaToolsJarPath, classpath)
+	}
+	if len(c.javaCustomJarPaths) > 0 {
+		classpath = fmt.Sprintf("%s:%s", strings.Join(c.javaCustomJarPaths, ":"), classpath)
+	}
+	bindHost := config.Datadog.GetString("bind_host")
+	if bindHost == "" || bindHost == "0.0.0.0" {
+		bindHost = "localhost"
+	}
+	reporter := fmt.Sprintf("statsd:%s:%s", bindHost, config.Datadog.GetString("dogstatsd_port"))
+
+	//TODO : support auto discovery
+
+	subprocessArgs := []string{}
+
+	// Specify a maximum memory allocation pool for the JVM
+	javaOptions := c.javaOptions
+	if !strings.Contains(javaOptions, "Xmx") && !strings.Contains(javaOptions, "XX:MaxHeapSize") {
+		javaOptions += jvmDefaultMaxMemoryAllocation
+	}
+	// Specify the initial memory allocation pool for the JVM
+	if !strings.Contains(javaOptions, "Xms") && !strings.Contains(javaOptions, "XX:InitialHeapSize") {
+		javaOptions += jvmDefaultInitialMemoryAllocation
+	}
+
+	subprocessArgs = append(subprocessArgs, strings.Fields(javaOptions)...)
+
+	subprocessArgs = append(subprocessArgs,
+		"-classpath", classpath,
+		jmxMainClass,
+		"--check_period", fmt.Sprintf("%v", int(check.DefaultCheckInterval/time.Millisecond)), // Period of the main loop of jmxfetch in ms
+		"--conf_directory", jmxConfPath, // Path of the conf directory that will be read by jmxfetch,
+		"--log_level", "INFO", //FIXME : Use agent log level when available
+		"--log_location", path.Join(here, "dist", "jmx", "jmxfetch.log"), // FIXME : Path of the log file. At some point we should have a `run` folder
+		"--reporter", reporter, // Reporter to use
+		"--status_location", path.Join(here, "dist", "jmx", "jmx_status.yaml"), // FIXME : Path to the status file to write. At some point we should have a `run` folder
+		jmxCollectCommand, // Name of the command
+	)
+	if len(c.checks) > 0 {
+		subprocessArgs = append(subprocessArgs, "--check")
+		for c, _ := range c.checks {
+			subprocessArgs = append(subprocessArgs, c)
+		}
+	} else {
+		log.Errorf("No valid JMX configuration found in %s", jmxConfPath)
+	}
+
+	if jmxExitFile != "" {
+		c.ExitFilePath = path.Join(here, "dist", "jmx", jmxExitFile) // FIXME : At some point we should have a `run` folder
+		// Signal handlers are not supported on Windows:
+		// use a file to trigger JMXFetch exit instead
+		subprocessArgs = append(subprocessArgs, "--exit_file_location", c.ExitFilePath)
+	}
+
+	javaBinPath := c.javaBinPath
+	if javaBinPath == "" {
+		javaBinPath = "java"
+	}
+	c.cmd = exec.Command(javaBinPath, subprocessArgs...)
 
 	// remove the exit file trigger (windows)
 	if jmxExitFile != "" {
@@ -252,120 +205,55 @@ func (c *JMXCheck) Run() error {
 		return err
 	}
 
-	return c.cmd.Wait()
+	c.running = true
+	err = c.cmd.Wait()
+	c.running = false
+
+	return err
+}
+
+func (c *JMXCheck) Parse(data, initConfig check.ConfigData) error {
+
+	var initConf checkInitCfg
+	var instanceConf checkInstanceCfg
+
+	// unmarshall instance info
+	if err := yaml.Unmarshal(data, instanceConf); err != nil {
+		return err
+	}
+
+	// unmarshall init config
+	if err := yaml.Unmarshal(data, initConf); err != nil {
+		return err
+	}
+
+	if c.javaBinPath == "" {
+		c.javaBinPath = initConf.JavaBinPath
+	}
+	if c.javaOptions == "" {
+		c.javaOptions = initConf.JavaOptions
+	}
+	if c.javaToolsJarPath == "" {
+		c.javaToolsJarPath = initConf.ToolsJarPath
+	}
+	if c.javaCustomJarPaths != nil {
+		c.javaCustomJarPaths = initConf.CustomJarPaths
+	}
+
+	if instanceConf.ProcessNameRegex != "" {
+		c.isAttachAPI = true
+	}
+
+	return nil
 }
 
 // Configure the JMXCheck
 func (c *JMXCheck) Configure(data, initConfig check.ConfigData) error {
-	jmxConfPath := path.Join(config.Datadog.GetString("confd_path"), "jmx")
-	jmxConf := new(jmxCfg)
 
-	jmxChecks := []string{}
-	javaBinPath := ""
-	javaOptions := ""
-	toolsJarPath := ""
-	customJarPaths := []string{}
-
-	if err := jmxConf.Parse(data); err != nil {
+	err := c.Parse(data, initConfig)
+	if err != nil {
 		return err
 	}
-
-	for _, confFile := range jmxConf.instance.Files {
-		checkConf := new(checkCfg)
-		fpath := path.Join(jmxConfPath, confFile)
-
-		// Read file contents
-		// FIXME: ReadFile reads the entire file, possible security implications
-		yamlFile, err := ioutil.ReadFile(fpath)
-		if err != nil {
-			return err
-		}
-
-		if err := checkConf.Parse(yamlFile); err != nil {
-			log.Errorf("Unable to parse %s: %s", confFile, err)
-			continue
-		}
-
-		if checkJavaBinPath, checkJavaOptions, checkToolsJarPath, checkCustomJarPaths, err := readJMXConf(checkConf, confFile); err == nil {
-			jmxChecks = append(jmxChecks, confFile)
-			c.checks[confFile] = struct{}{}
-			if javaBinPath == "" && checkJavaBinPath != "" {
-				javaBinPath = checkJavaBinPath
-			}
-			if javaOptions == "" && checkJavaOptions != "" {
-				javaOptions = checkJavaOptions
-			}
-			if toolsJarPath == "" && checkToolsJarPath != "" {
-				toolsJarPath = checkToolsJarPath
-			}
-			if checkCustomJarPaths != nil {
-				customJarPaths = append(customJarPaths, checkCustomJarPaths...)
-			}
-		} else {
-			log.Errorf("Invalid JMX configuration in file %s: %s", confFile, err)
-			continue
-		}
-	}
-
-	here, _ := osext.ExecutableFolder()
-	jmxJarPath := path.Join(here, "dist", "jmx", jmxJarName)
-	classpath := jmxJarPath
-	if toolsJarPath != "" {
-		classpath = fmt.Sprintf("%s:%s", toolsJarPath, classpath)
-	}
-	if len(customJarPaths) > 0 {
-		classpath = fmt.Sprintf("%s:%s", strings.Join(customJarPaths, ":"), classpath)
-	}
-	bindHost := config.Datadog.GetString("bind_host")
-	if bindHost == "" || bindHost == "0.0.0.0" {
-		bindHost = "localhost"
-	}
-	reporter := fmt.Sprintf("statsd:%s:%s", bindHost, config.Datadog.GetString("dogstatsd_port"))
-
-	//TODO : support auto discovery
-
-	subprocessArgs := []string{}
-
-	// Specify a maximum memory allocation pool for the JVM
-	if !strings.Contains(javaOptions, "Xmx") && !strings.Contains(javaOptions, "XX:MaxHeapSize") {
-		javaOptions += jvmDefaultMaxMemoryAllocation
-	}
-	// Specify the initial memory allocation pool for the JVM
-	if !strings.Contains(javaOptions, "Xms") && !strings.Contains(javaOptions, "XX:InitialHeapSize") {
-		javaOptions += jvmDefaultInitialMemoryAllocation
-	}
-
-	subprocessArgs = append(subprocessArgs, strings.Fields(javaOptions)...)
-
-	subprocessArgs = append(subprocessArgs,
-		"-classpath", classpath,
-		jmxMainClass,
-		"--check_period", fmt.Sprintf("%v", int(check.DefaultCheckInterval/time.Millisecond)), // Period of the main loop of jmxfetch in ms
-		"--conf_directory", jmxConfPath, // Path of the conf directory that will be read by jmxfetch,
-		"--log_level", "INFO", //FIXME : Use agent log level when available
-		"--log_location", path.Join(here, "dist", "jmx", "jmxfetch.log"), // FIXME : Path of the log file. At some point we should have a `run` folder
-		"--reporter", reporter, // Reporter to use
-		"--status_location", path.Join(here, "dist", "jmx", "jmx_status.yaml"), // FIXME : Path to the status file to write. At some point we should have a `run` folder
-		jmxCollectCommand, // Name of the command
-	)
-	if len(jmxChecks) > 0 {
-		subprocessArgs = append(subprocessArgs, "--check")
-		subprocessArgs = append(subprocessArgs, jmxChecks...)
-	} else {
-		return fmt.Errorf("No valid JMX configuration found in %s", jmxConfPath)
-	}
-
-	if jmxExitFile != "" {
-		c.ExitFilePath = path.Join(here, "dist", "jmx", jmxExitFile) // FIXME : At some point we should have a `run` folder
-		// Signal handlers are not supported on Windows:
-		// use a file to trigger JMXFetch exit instead
-		subprocessArgs = append(subprocessArgs, "--exit_file_location", c.ExitFilePath)
-	}
-
-	if javaBinPath == "" {
-		javaBinPath = "java"
-	}
-	c.cmd = exec.Command(javaBinPath, subprocessArgs...)
 
 	return nil
 }
@@ -393,6 +281,7 @@ func (c *JMXCheck) Stop() {
 			log.Errorf("unable to stop JMX check: %s", err)
 		}
 	}
+	c.running = true
 }
 
 // GetWarnings does not return anything in JMX
