6f9fbf8fc2cbc1f793165f1665c416351864e067
Xavier Vello <xavier.vello@datadoghq.com>
2017-09-18 10:25:21 +0200

    fix imports for merged code + add cgroup swap stats

--- pkg/util/docker/docker.go
+++ pkg/util/docker/docker.go
@@ -1,12 +1,14 @@
 package docker
 
 import (
+	"bufio"
 	"context"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"net"
 	"os"
+	"path/filepath"
 	"regexp"
 	"sort"
 	"strconv"
@@ -61,19 +63,38 @@ type containerFilter struct {
 
 // HostnameProvider docker implementation for the hostname provider
 func HostnameProvider(hostName string) (string, error) {
-       return GetHostname()
+	return GetHostname()
+}
 
 // DefaultGateway returns the default Docker gateway.
 func DefaultGateway() (net.IP, error) {
-       procRoot := config.Datadog.GetString("proc_root")
-       netRouteFile := filepath.Join(procRoot, "net", "route")
-       f, err := os.Open(netRouteFile)
-       if os.IsNotExist(err) || os.IsPermission(err) {
-               log.Errorf("unable to open %s: %s", netRouteFile, err)
-               return nil, nil
-       } else if err != nil {
-               // Unknown error types will bubble up for handling.
-               return nil, err
+	procRoot := config.Datadog.GetString("proc_root")
+	netRouteFile := filepath.Join(procRoot, "net", "route")
+	f, err := os.Open(netRouteFile)
+	if os.IsNotExist(err) || os.IsPermission(err) {
+		log.Errorf("unable to open %s: %s", netRouteFile, err)
+		return nil, nil
+	} else if err != nil {
+		// Unknown error types will bubble up for handling.
+		return nil, err
+	}
+	defer f.Close()
+
+	ip := make(net.IP, 4)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		if len(fields) >= 3 && fields[1] == "00000000" {
+			ipInt, err := strconv.ParseInt(fields[2], 16, 32)
+			if err != nil {
+				return nil, fmt.Errorf("unable to parse ip %s, from %s: %s", fields[2], netRouteFile, err)
+			}
+			binary.LittleEndian.PutUint32(ip, uint32(ipInt))
+			break
+		}
+	}
+	return ip, nil
+}
 
 // NewcontainerFilter creates a new container filter from a two slices of
 // regexp patterns for a whitelist and blacklist. Each pattern should have
