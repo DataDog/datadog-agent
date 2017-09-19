f8b70b52cc82734260eebba8d96ef118cf37732d
Xavier Vello <xavier.vello@datadoghq.com>
2017-09-19 15:35:26 +0200

    add docker envvar extraction as tags

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -111,6 +111,7 @@ func init() {
 	Datadog.SetDefault("autoconf_template_dir", "/datadog/check_configs")
 	// Docker
 	Datadog.SetDefault("docker_labels_as_tags", []string{})
+	Datadog.SetDefault("docker_env_as_tags", []string{})
 	// Kubernetes
 	Datadog.SetDefault("kubernetes_http_kubelet_port", 10255)
 	Datadog.SetDefault("kubernetes_https_kubelet_port", 10250)
--- pkg/tagger/collectors/docker_extract.go
+++ pkg/tagger/collectors/docker_extract.go
@@ -9,6 +9,7 @@ package collectors
 
 import (
 	"fmt"
+	"strings"
 
 	log "github.com/cihub/seelog"
 	"github.com/docker/docker/api/types"
@@ -16,19 +17,25 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/docker"
 )
 
-// ExtractFromInspect extract tags for a container inspect JSON
-func (c *DockerCollector) ExtractFromInspect(co types.ContainerJSON) ([]string, []string, error) {
+// extractFromInspect extract tags for a container inspect JSON
+func (c *DockerCollector) extractFromInspect(co types.ContainerJSON) ([]string, []string, error) {
 	var low, high []string
 
-	docker_image, _ := docker.ResolveImageName(co.Image)
-	image_name, image_tag, err := docker.SplitImageName(docker_image)
-
-	low = append(low, fmt.Sprintf("docker_image:%s", docker_image))
-	low = append(low, fmt.Sprintf("image_name:%s", image_name))
-	low = append(low, fmt.Sprintf("image_tag:%s", image_tag))
-
+	docker_image, err := docker.ResolveImageName(co.Image)
 	if err != nil {
-		log.Debugf("error spliting %s: %s", docker_image, err)
+		log.Debugf("error resolving image %s: %s", co.Image, err)
+
+	} else {
+		image_name, image_tag, err := docker.SplitImageName(docker_image)
+
+		low = append(low, fmt.Sprintf("docker_image:%s", docker_image))
+
+		if err != nil {
+			log.Debugf("error spliting %s: %s", docker_image, err)
+		} else {
+			low = append(low, fmt.Sprintf("image_name:%s", image_name))
+			low = append(low, fmt.Sprintf("image_tag:%s", image_tag))
+		}
 	}
 
 	for _, label := range c.labelsAsTags {
@@ -37,8 +44,22 @@ func (c *DockerCollector) ExtractFromInspect(co types.ContainerJSON) ([]string, 
 		}
 	}
 
-	// TODO: to be completed once docker utils are merged
-	low = append(low, "low:test")
-	high = append(high, fmt.Sprintf("container_name:%s", co.ID))
+	if len(c.envAsTags) > 0 {
+		for _, envvar := range co.Config.Env {
+			parts := strings.SplitN(envvar, "=", 2)
+			if len(parts) != 2 {
+				continue
+			}
+			for _, envAsTagConfigEntry := range c.envAsTags {
+				if parts[0] == envAsTagConfigEntry {
+					low = append(low, fmt.Sprintf("%s:%s", parts[0], parts[1]))
+				}
+			}
+		}
+	}
+
+	high = append(high, fmt.Sprintf("container_name:%s", co.Name))
+	high = append(high, fmt.Sprintf("container_id:%s", co.ID))
+
 	return low, high, nil
 }
