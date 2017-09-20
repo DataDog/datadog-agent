bba35c21c1487e4bc869c4d8b2aa92e0b7497119
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-09-20 05:19:05 -0400

    [py] Remove `dist/checks` directory from python path (#569)
    * [py] Remove `dist/checks` directory from python path
    
    `dist/` is already in the python path, so all the modules in
    `dist/checks` can and should be imported with
    `import checks.my_module` instead of `import my_module`.
    
    From what I've checked the agent6 core py code already accounts for
    the `checks.` prefix when it loads the AgentCheck class for example,
    so everything should still work.
    
    * remove unused import
    
    * [py] Add the `dist/checks.d` directory instead
    
    Having `dist/checks.d` in the python path reflects well the path
    structure of an installed agent and still allows devs to easily load
    their custom checks in the development environment.

--- cmd/agent/common/common.go
+++ cmd/agent/common/common.go
@@ -39,8 +39,8 @@ var (
 // should load python modules and checks
 func GetPythonPaths() []string {
 	return []string{
-		GetDistPath(),                                  // some common modules are shipped in the dist path directly
-		filepath.Join(GetDistPath(), "checks"),         // other common modules (e.g. `AgentCheck` class)
+		GetDistPath(),                                  // common modules are shipped in the dist path directly or under the "checks/" sub-dir
+		filepath.Join(GetDistPath(), "checks.d"),       // custom checks in the "checks.d/" sub-dir of the dist path
 		config.Datadog.GetString("additional_checksd"), // custom checks, have precedence over integrations-core checks
 		PyChecksPath,                                   // integrations-core checks
 	}
