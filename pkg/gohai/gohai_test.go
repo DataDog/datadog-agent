ad2ef06fe3a9f864ec7573b1f6661c8f1981c551
tariqibrahim <tariq1890@yahoo.in>
2017-09-20 23:42:48 -0400

    Minor code cleanups and test cases added - Adding go format.

--- pkg/gohai/gohai_test.go
+++ pkg/gohai/gohai_test.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestSelectedCollectors_String(t *testing.T) {
 	sc := &SelectedCollectors{
-		"foo" : struct{}{},
-		"bar" : struct{}{},
+		"foo": struct{}{},
+		"bar": struct{}{},
 	}
 	assert.Equal(t, "[foo bar]", sc.String())
 }
