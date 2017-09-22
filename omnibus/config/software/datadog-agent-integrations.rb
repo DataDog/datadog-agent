9b135ddf94edb4dd62e3bac1fbf1214bdcd33e0d
Derek Brown <derek.brown@datadoghq.com>
2017-09-22 07:39:30 -0700

    Set integrations-core back to 'master'

--- omnibus/config/software/datadog-agent-integrations.rb
+++ omnibus/config/software/datadog-agent-integrations.rb
@@ -6,7 +6,7 @@ relative_path 'integrations-core'
 whitelist_file "embedded/lib/python2.7"
 
 source git: 'https://github.com/DataDog/integrations-core.git'
-default_version 'db/wmi_check_ambiguity'
+default_version 'master'
 
 build do
   # The checks
