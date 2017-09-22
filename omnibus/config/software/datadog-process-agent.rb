36baf5c0533eefa8ea899c2503e8c43bc4cb7128
Massimiliano Pippi <mpippi@gmail.com>
2017-09-22 14:30:59 +0200

    add trace and process config files to the package

--- omnibus/config/software/datadog-process-agent.rb
+++ omnibus/config/software/datadog-process-agent.rb
@@ -1,18 +1,29 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https:#www.datadoghq.com/).
+# Copyright 2017 Datadog, Inc.
+
 name "datadog-process-agent"
 always_build true
 
-process_agent_branch = ENV['PROCESS_AGENT_BRANCH']
-if process_agent_branch.nil? || process_agent_branch.empty?
-    process_agent_branch = "master"
-end
-default_version process_agent_branch
+default_version 'master'
 
 
 build do
-  ship_license "https://raw.githubusercontent.com/DataDog/datadog-trace-agent/#{version}/LICENSE"
+  ship_license "https://github.com/DataDog/datadog-process-agent/blob/#{version}/LICENSE"
+
   binary = "process-agent-amd64-#{version}"
-  url = "https://s3.amazonaws.com/datad0g-process-agent/#{binary}"
-  command "curl #{url} -o #{binary}"
+  config = "dd-process-agent.ini.example"
+  config_url = "https://raw.githubusercontent.com/DataDog/datadog-process-agent/#{version}/packaging/debian/package/etc/dd-agent/#{config}"
+  binary_url = "https://s3.amazonaws.com/datad0g-process-agent/#{binary}"
+
+  # fetch the binary and move to install_dir
+  command "curl #{binary_url} -o #{binary}"
   command "chmod +x #{binary}"
-  command "mv #{binary} #{install_dir}/embedded/bin/process-agent"
-end 
+  move binary, "#{install_dir}/embedded/bin/process-agent"
+
+  # fetch the default config file, remove api_key settings and move to install_dir
+  command "curl #{config_url} -o #{config}"
+  command "sed '/^api_key/d' < #{config} > process-agent.ini" # works both on linux and mac
+  move "process-agent.ini", "#{install_dir}/etc/datadog-agent"
+end
