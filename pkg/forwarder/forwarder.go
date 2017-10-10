6bf911e8e29ceb77abe6429ba37b622287955169
Massimiliano Pippi <mpippi@gmail.com>
2017-10-10 19:55:50 +0200

    changed testing approach to remove a race cond, polished the api

--- pkg/forwarder/forwarder.go
+++ pkg/forwarder/forwarder.go
@@ -89,15 +89,15 @@ type Transaction interface {
 type Forwarder interface {
 	Start() error
 	Stop()
-	SubmitV1Series(payload Payloads, extraHeaders map[string]string) error
-	SubmitV1Intake(payload Payloads, extraHeaders map[string]string) error
-	SubmitV1CheckRuns(payload Payloads, extraHeaders map[string]string) error
-	SubmitSeries(payload Payloads, extraHeaders map[string]string) error
-	SubmitEvents(payload Payloads, extraHeaders map[string]string) error
-	SubmitServiceChecks(payload Payloads, extraHeaders map[string]string) error
-	SubmitSketchSeries(payload Payloads, extraHeaders map[string]string) error
-	SubmitHostMetadata(payload Payloads, extraHeaders map[string]string) error
-	SubmitMetadata(payload Payloads, extraHeaders map[string]string) error
+	SubmitV1Series(payload Payloads, extra http.Header) error
+	SubmitV1Intake(payload Payloads, extra http.Header) error
+	SubmitV1CheckRuns(payload Payloads, extra http.Header) error
+	SubmitSeries(payload Payloads, extra http.Header) error
+	SubmitEvents(payload Payloads, extra http.Header) error
+	SubmitServiceChecks(payload Payloads, extra http.Header) error
+	SubmitSketchSeries(payload Payloads, extra http.Header) error
+	SubmitHostMetadata(payload Payloads, extra http.Header) error
+	SubmitMetadata(payload Payloads, extra http.Header) error
 }
 
 // DefaultForwarder is in charge of receiving transaction payloads and sending them to Datadog backend over HTTP.
@@ -133,14 +133,14 @@ func (v byCreatedTime) Len() int           { return len(v) }
 func (v byCreatedTime) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
 func (v byCreatedTime) Less(i, j int) bool { return v[i].GetCreatedAt().After(v[j].GetCreatedAt()) }
 
-func (f *DefaultForwarder) retryTransactions(tickTime time.Time) {
+func (f *DefaultForwarder) retryTransactions(retryBefore time.Time) {
 	newQueue := []Transaction{}
 	droppedTransaction := 0
 
 	sort.Sort(byCreatedTime(f.retryQueue))
 
 	for _, t := range f.retryQueue {
-		if t.GetNextFlush().Before(tickTime) {
+		if t.GetNextFlush().Before(retryBefore) {
 			f.waitingPipe <- t
 			transactionsCreation.Add("SuccessfullyRetried", 1)
 		} else if len(newQueue) < f.retryQueueLimit {
@@ -246,7 +246,7 @@ func (f *DefaultForwarder) Stop() {
 	log.Info("DefaultForwarder stopped")
 }
 
-func (f *DefaultForwarder) createHTTPTransactions(endpoint string, payloads Payloads, apiKeyInQueryString bool, extraHeaders map[string]string) ([]*HTTPTransaction, error) {
+func (f *DefaultForwarder) createHTTPTransactions(endpoint string, payloads Payloads, apiKeyInQueryString bool, extra http.Header) []*HTTPTransaction {
 	transactions := []*HTTPTransaction{}
 	for _, payload := range payloads {
 		for domain, apiKeys := range f.KeysPerDomains {
@@ -266,14 +266,14 @@ func (f *DefaultForwarder) createHTTPTransactions(endpoint string, payloads Payl
 					t.apiKeyStatusKey += apiKey[len(apiKey)-5:]
 				}
 
-				for k, v := range extraHeaders {
-					t.Headers.Set(k, v)
+				for key := range extra {
+					t.Headers.Set(key, extra.Get(key))
 				}
 				transactions = append(transactions, t)
 			}
 		}
 	}
-	return transactions, nil
+	return transactions
 }
 
 func (f *DefaultForwarder) sendHTTPTransactions(transactions []*HTTPTransaction) error {
@@ -289,100 +289,66 @@ func (f *DefaultForwarder) sendHTTPTransactions(transactions []*HTTPTransaction)
 }
 
 // SubmitSeries will send a series type payload to Datadog backend.
-func (f *DefaultForwarder) SubmitSeries(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(seriesEndpoint, payload, false, extraHeaders)
-	if err != nil {
-		return err
-	}
-
+func (f *DefaultForwarder) SubmitSeries(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(seriesEndpoint, payload, false, extra)
 	transactionsCreation.Add("Series", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitEvents will send an event type payload to Datadog backend.
-func (f *DefaultForwarder) SubmitEvents(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(eventsEndpoint, payload, false, extraHeaders)
-	if err != nil {
-		return err
-	}
-
+func (f *DefaultForwarder) SubmitEvents(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(eventsEndpoint, payload, false, extra)
 	transactionsCreation.Add("Events", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitServiceChecks will send a service check type payload to Datadog backend.
-func (f *DefaultForwarder) SubmitServiceChecks(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(serviceChecksEndpoint, payload, false, extraHeaders)
-	if err != nil {
-		return err
-	}
-
+func (f *DefaultForwarder) SubmitServiceChecks(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(serviceChecksEndpoint, payload, false, extra)
 	transactionsCreation.Add("ServiceChecks", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitSketchSeries will send payloads to Datadog backend - PROTOTYPE FOR PERCENTILE
-func (f *DefaultForwarder) SubmitSketchSeries(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(sketchSeriesEndpoint, payload, true, extraHeaders)
-	if err != nil {
-		return err
-	}
+func (f *DefaultForwarder) SubmitSketchSeries(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(sketchSeriesEndpoint, payload, true, extra)
 	transactionsCreation.Add("SketchSeries", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitHostMetadata will send a host_metadata tag type payload to Datadog backend.
-func (f *DefaultForwarder) SubmitHostMetadata(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(hostMetadataEndpoint, payload, false, extraHeaders)
-	if err != nil {
-		return err
-	}
-
+func (f *DefaultForwarder) SubmitHostMetadata(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(hostMetadataEndpoint, payload, false, extra)
 	transactionsCreation.Add("HostMetadata", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitMetadata will send a metadata type payload to Datadog backend.
-func (f *DefaultForwarder) SubmitMetadata(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(metadataEndpoint, payload, false, extraHeaders)
-	if err != nil {
-		return err
-	}
-
+func (f *DefaultForwarder) SubmitMetadata(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(metadataEndpoint, payload, false, extra)
 	transactionsCreation.Add("Metadata", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitV1Series will send timeserie to v1 endpoint (this will be remove once
 // the backend handles v2 endpoints).
-func (f *DefaultForwarder) SubmitV1Series(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(v1SeriesEndpoint, payload, true, extraHeaders)
-	if err != nil {
-		return err
-	}
-
+func (f *DefaultForwarder) SubmitV1Series(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(v1SeriesEndpoint, payload, true, extra)
 	transactionsCreation.Add("TimeseriesV1", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitV1CheckRuns will send service checks to v1 endpoint (this will be removed once
 // the backend handles v2 endpoints).
-func (f *DefaultForwarder) SubmitV1CheckRuns(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(v1CheckRunsEndpoint, payload, true, extraHeaders)
-	if err != nil {
-		return err
-	}
-
+func (f *DefaultForwarder) SubmitV1CheckRuns(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(v1CheckRunsEndpoint, payload, true, extra)
 	transactionsCreation.Add("CheckRunsV1", 1)
 	return f.sendHTTPTransactions(transactions)
 }
 
 // SubmitV1Intake will send payloads to the universal `/intake/` endpoint used by Agent v.5
-func (f *DefaultForwarder) SubmitV1Intake(payload Payloads, extraHeaders map[string]string) error {
-	transactions, err := f.createHTTPTransactions(v1IntakeEndpoint, payload, true, extraHeaders)
-	if err != nil {
-		return err
-	}
+func (f *DefaultForwarder) SubmitV1Intake(payload Payloads, extra http.Header) error {
+	transactions := f.createHTTPTransactions(v1IntakeEndpoint, payload, true, extra)
 
 	// the intake endpoint requires the Content-Type header to be set
 	for _, t := range transactions {
