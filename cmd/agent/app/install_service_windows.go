ce4f25875196f526dadc9d25028ba7fbdd543f6f
Derek Brown <derek.brown@datadoghq.com>
2017-10-11 15:09:49 +0000

    Upgrade windows config from agent5 on service start (#688)
    * [Windows] Add check on service start to see if config needs to be
    updated.
    
    Refactored config upgrade code from cmd/agent/app so it could be shared.
    
    * Fix formatting
    
    * [Windows] Fix bad merge
    
    * Fix build problem
    
    * Fix linting issue

--- cmd/agent/app/install_service_windows.go
+++ cmd/agent/app/install_service_windows.go
@@ -15,6 +15,7 @@ import (
 	"golang.org/x/sys/windows/svc/mgr"
 )
 
+// Default name of windows service
 const ServiceName = "DatadogAgent"
 
 func init() {
@@ -25,10 +26,10 @@ var instsvcCommand = &cobra.Command{
 	Use:   "installservice",
 	Short: "Installs the agent within the service control manager",
 	Long:  ``,
-	RunE:  install_service,
+	RunE:  installService,
 }
 
-func install_service(cmd *cobra.Command, args []string) error {
+func installService(cmd *cobra.Command, args []string) error {
 	exepath, err := exePath()
 	if err != nil {
 		return err
