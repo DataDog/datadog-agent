9f81030200426385d1f222a53c441049d0899270
CharlyF <charly@datadoghq.com>
2017-10-18 17:40:36 +0200

    adding tagger extractor and unitests

---
+++ pkg/tagger/collectors/ecs_main.go
@@ -0,0 +1,71 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2017 Datadog, Inc.
+
+// +build docker
+
+package collectors
+
+import (
+	"fmt"
+	ecsutil "github.com/DataDog/datadog-agent/pkg/util/ecs"
+)
+
+const (
+	ecsCollectorName = "ecs"
+)
+
+// ECSCollector listen to the ECS agent to get ECS metadata.
+// And feed a stream of TagInfo.
+
+type ECSCollector struct {
+	infoOut chan<- []*TagInfo
+}
+
+// Detect tries to connect to the ecs agent
+func (c *ECSCollector) Detect(out chan<- []*TagInfo) (CollectionMode, error) {
+	if ecsutil.IsInstance() {
+		c.infoOut = out
+		return PullCollection, nil
+	} else {
+		return NoCollection, fmt.Errorf("Failed to connect to ecs, ECS tagging will not work")
+	}
+
+}
+
+// No need to use the Pull for ECS, metadata collection relies on Fetch.
+// Implementing for compliance with tagger interface.
+func (c *ECSCollector) Pull() error {
+	return nil
+}
+
+// Fetch fetches ECS tags
+func (c *ECSCollector) Fetch(container string) ([]string, []string, error) {
+
+	tasks_list, err := ecsutil.GetTasks()
+	if err != nil {
+		return []string{}, []string{}, err
+	}
+	updates, err := c.parseTasks(tasks_list)
+	if err != nil {
+		return []string{}, []string{}, err
+	}
+	c.infoOut <- updates
+
+	for _, info := range updates {
+		if info.Entity == container {
+			return info.LowCardTags, info.HighCardTags, nil
+		}
+	}
+	// container not found in updates
+	return []string{}, []string{}, fmt.Errorf("entity %s not found in tasklist", container)
+}
+
+func ecsFactory() Collector {
+	return &ECSCollector{}
+}
+
+func init() {
+	registerCollector(ecsCollectorName, ecsFactory)
+}
