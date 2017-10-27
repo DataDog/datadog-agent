671b00bbc6a10d0b821f3d1d1c9b62aaf1aaabbd
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-10-27 15:59:16 +0200

    [long-running checks] Fix check retries & fix races on `stop` (#736)
    Use a different approach to long-running check retries.
    
    Before:
    * Long running checks would always be retried when they returned an
      error. This meant that if a check was stopped and returned a non-zero
      exit code, it would also be retried. For some reason JMXFetch sometimes
      returns a non-zero exit code when killed, so we would end up with
      orphaned JMXFetch processes.
    * some panics could arise because the Kill signal was sent even when
      the process had already exited
    * retries would fail on apm, logs-agent & process-agent because the
      `Cmd` struct needs to be re-instantiated before `Start` is called
      again on it
    * the runner would stop running checks one by one sequentially
    
    After:
    
    * The runner doesn't handle check retries. The long-running checks call
      the retries from their `Run` method, which allows them to handle stop
      requests more gracefully. Retries are attempted only on a certain
      type of error (`check.RetryableError`). Right now, this type of error
      is used only when the child process returns a non-zero exit code
    * Long-running checks have a synchronisation logic that prevents them
      from restarting when a stop was requested, solving the orphaned
      processes issue and the race conditions
    * retries now work on all long-running checks
    * When stopping the runner, stop checks concurrently and use a global
      timeout
    
    There's quite some code duplication between long-running checks. I'm
    leaving the code duplication there since we have plans to move to a
    different way of running the external agents (i.e. all long-running
    checks except JMXFetch).

--- pkg/collector/corechecks/embed/jmx.go
+++ pkg/collector/corechecks/embed/jmx.go
@@ -83,27 +83,170 @@ type JMXCheck struct {
 	javaCustomJarPaths []string
 	isAttachAPI        bool
 	running            uint32
+	stop               chan struct{}
+	stopDone           chan struct{}
 }
 
-var jmxLauncher = JMXCheck{checks: make(map[string]struct{})}
+var jmxLauncher = JMXCheck{
+	checks:   make(map[string]struct{}),
+	stop:     make(chan struct{}),
+	stopDone: make(chan struct{}),
+}
 
 func (c *JMXCheck) String() string {
 	return "JMX Check"
 }
 
-// Run executes the check
+// Run executes the check with retries
 func (c *JMXCheck) Run() error {
-
 	if len(c.checks) == 0 {
 		return fmt.Errorf("No JMX checks configured - skipping.")
 	}
 
-	if atomic.LoadUint32(&c.running) == 1 {
-		log.Info("JMX already running.")
-		return nil
-	}
 	atomic.StoreUint32(&c.running, 1)
+	// TODO: retries should be configurable with meaningful default values
+	err := check.Retry(defaultRetryDuration, defaultRetries, c.run, c.String())
+	atomic.StoreUint32(&c.running, 0)
 
+	return err
+}
+
+// run executes the check
+func (c *JMXCheck) run() error {
+	select {
+	// poll the stop channel once to make sure no stop was requested since the last call to `run`
+	case <-c.stop:
+		log.Info("Not starting JMX: stop requested")
+		c.stopDone <- struct{}{}
+		return nil
+	default:
+	}
+
+	err := c.start()
+	if err != nil {
+		return retryExitError(err)
+	}
+
+	processDone := make(chan error)
+	go func() {
+		processDone <- c.cmd.Wait()
+	}()
+
+	select {
+	case err = <-processDone:
+		return retryExitError(err)
+	case <-c.stop:
+		if jmxExitFile == "" {
+			// Unix
+			err = c.cmd.Process.Signal(os.Kill)
+			if err != nil {
+				log.Errorf("unable to stop JMX check: %s", err)
+			}
+		} else {
+			// Windows
+			if err = ioutil.WriteFile(c.ExitFilePath, nil, 0644); err != nil {
+				log.Errorf("unable to stop JMX check: %s", err)
+			}
+		}
+	}
+
+	// wait for process to exit
+	err = <-processDone
+	c.stopDone <- struct{}{}
+	return err
+}
+
+func (c *JMXCheck) Parse(data, initConfig check.ConfigData) error {
+
+	var initConf checkInitCfg
+	var instanceConf checkInstanceCfg
+
+	// unmarshall instance info
+	if err := yaml.Unmarshal(data, &instanceConf); err != nil {
+		return err
+	}
+
+	// unmarshall init config
+	if err := yaml.Unmarshal(initConfig, &initConf); err != nil {
+		return err
+	}
+
+	if c.javaBinPath == "" {
+		if instanceConf.JavaBinPath != "" {
+			c.javaBinPath = instanceConf.JavaBinPath
+		} else if initConf.JavaBinPath != "" {
+			c.javaBinPath = initConf.JavaBinPath
+		}
+	}
+	if c.javaOptions == "" {
+		if instanceConf.JavaOptions != "" {
+			c.javaOptions = instanceConf.JavaOptions
+		} else if initConf.JavaOptions != "" {
+			c.javaOptions = initConf.JavaOptions
+		}
+	}
+	if c.javaToolsJarPath == "" {
+		if instanceConf.ToolsJarPath != "" {
+			c.javaToolsJarPath = instanceConf.ToolsJarPath
+		} else if initConf.ToolsJarPath != "" {
+			c.javaToolsJarPath = initConf.ToolsJarPath
+		}
+	}
+	if c.javaCustomJarPaths == nil {
+		if initConf.CustomJarPaths != nil {
+			c.javaCustomJarPaths = initConf.CustomJarPaths
+		}
+	}
+
+	if instanceConf.ProcessNameRegex != "" {
+		if c.javaToolsJarPath == "" {
+			return fmt.Errorf("You must specify the path to tools.jar. %s", linkToDoc)
+		}
+		c.isAttachAPI = true
+	}
+
+	return nil
+}
+
+// Configure the JMXCheck
+func (c *JMXCheck) Configure(data, initConfig check.ConfigData) error {
+
+	err := c.Parse(data, initConfig)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Interval returns the scheduling time for the check, this will be scheduled only once
+// since `Run` won't return, thus implementing a long running check.
+func (c *JMXCheck) Interval() time.Duration {
+	return 0
+}
+
+// ID returns the name of the check since there should be only one instance running
+func (c *JMXCheck) ID() check.ID {
+	return "JMX_Check"
+}
+
+// GetMetricStats returns the stats from the last run of the check, but there aren't any yet
+func (c *JMXCheck) GetMetricStats() (map[string]int64, error) {
+	return make(map[string]int64), nil
+}
+
+// Stop sends a termination signal to the JMXFetch process
+func (c *JMXCheck) Stop() {
+	if atomic.LoadUint32(&c.running) == 0 {
+		log.Info("JMX not running.")
+		return
+	}
+
+	c.stop <- struct{}{}
+	<-c.stopDone
+}
+
+func (c *JMXCheck) start() error {
 	here, _ := osext.ExecutableFolder()
 	jmxConfPath := config.Datadog.GetString("confd_path")
 	classpath := path.Join(here, "dist", "jmx", jmxJarName)
@@ -202,108 +345,8 @@ func (c *JMXCheck) Run() error {
 	}()
 
 	log.Debugf("Args: %v", subprocessArgs)
-	if err = c.cmd.Start(); err != nil {
-		return err
-	}
 
-	err = c.cmd.Wait()
-	atomic.StoreUint32(&c.running, 0)
-
-	return err
-}
-
-func (c *JMXCheck) Parse(data, initConfig check.ConfigData) error {
-
-	var initConf checkInitCfg
-	var instanceConf checkInstanceCfg
-
-	// unmarshall instance info
-	if err := yaml.Unmarshal(data, &instanceConf); err != nil {
-		return err
-	}
-
-	// unmarshall init config
-	if err := yaml.Unmarshal(initConfig, &initConf); err != nil {
-		return err
-	}
-
-	if c.javaBinPath == "" {
-		if instanceConf.JavaBinPath != "" {
-			c.javaBinPath = instanceConf.JavaBinPath
-		} else if initConf.JavaBinPath != "" {
-			c.javaBinPath = initConf.JavaBinPath
-		}
-	}
-	if c.javaOptions == "" {
-		if instanceConf.JavaOptions != "" {
-			c.javaOptions = instanceConf.JavaOptions
-		} else if initConf.JavaOptions != "" {
-			c.javaOptions = initConf.JavaOptions
-		}
-	}
-	if c.javaToolsJarPath == "" {
-		if instanceConf.ToolsJarPath != "" {
-			c.javaToolsJarPath = instanceConf.ToolsJarPath
-		} else if initConf.ToolsJarPath != "" {
-			c.javaToolsJarPath = initConf.ToolsJarPath
-		}
-	}
-	if c.javaCustomJarPaths == nil {
-		if initConf.CustomJarPaths != nil {
-			c.javaCustomJarPaths = initConf.CustomJarPaths
-		}
-	}
-
-	if instanceConf.ProcessNameRegex != "" {
-		if c.javaToolsJarPath == "" {
-			return fmt.Errorf("You must specify the path to tools.jar. %s", linkToDoc)
-		}
-		c.isAttachAPI = true
-	}
-
-	return nil
-}
-
-// Configure the JMXCheck
-func (c *JMXCheck) Configure(data, initConfig check.ConfigData) error {
-
-	err := c.Parse(data, initConfig)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Interval returns the scheduling time for the check, this will be scheduled only once
-// since `Run` won't return, thus implementing a long running check.
-func (c *JMXCheck) Interval() time.Duration {
-	return 0
-}
-
-// ID returns the name of the check since there should be only one instance running
-func (c *JMXCheck) ID() check.ID {
-	return "JMX_Check"
-}
-
-// GetMetricStats returns the stats from the last run of the check, but there aren't any yet
-func (c *JMXCheck) GetMetricStats() (map[string]int64, error) {
-	return make(map[string]int64), nil
-}
-
-// Stop sends a termination signal to the JMXFetch process
-func (c *JMXCheck) Stop() {
-	if jmxExitFile == "" {
-		err := c.cmd.Process.Signal(os.Kill)
-		if err != nil {
-			log.Errorf("unable to stop JMX check: %s", err)
-		}
-	} else {
-		if err := ioutil.WriteFile(c.ExitFilePath, nil, 0644); err != nil {
-			log.Errorf("unable to stop JMX check: %s", err)
-		}
-	}
-	atomic.StoreUint32(&c.running, 0)
+	return c.cmd.Start()
 }
 
 // GetWarnings does not return anything in JMX
