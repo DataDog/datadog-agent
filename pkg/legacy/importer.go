c3e0d97f41e1be8901b506e0e365442154383887
Maxime Mouial <maxime.mouial@gmail.com>
2017-10-27 16:08:17 -0400

    Change histogram_percentiles in the legacy config importer

--- pkg/legacy/importer.go
+++ pkg/legacy/importer.go
@@ -108,7 +108,7 @@ func GetAgentConfig(datadogConfPath string) (Config, error) {
 
 	// these values are postprocessed in config.py, manually overwrite them
 	config["histogram_aggregates"] = "['max', 'median', 'avg', 'count']"
-	config["histogram_percentiles"] = "[0.95]"
+	config["histogram_percentiles"] = "['0.95']"
 	config["endpoints"] = "{}"
 	config["version"] = "5.18.0"
 	config["proxy_settings"] = "{'host': 'my-proxy.com', 'password': 'password', 'port': 3128, 'user': 'user'}"
