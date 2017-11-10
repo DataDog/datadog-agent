cf2d8a05ab26fe27bf6df6133547ef558e271c09
Derek Brown <derek.brown@datadoghq.com>
2017-11-09 17:22:23 -0800

    Add support for single instance of multi-instance counter

--- pkg/collector/dist/checks/libs/win/pdhbasecheck.py
+++ pkg/collector/dist/checks/libs/win/pdhbasecheck.py
@@ -14,11 +14,11 @@ except ImportError:
 
 class PDHBaseCheck(AgentCheck):
     """
-    WMI check.
+    PDH based check.  check.
 
     Windows only.
     """
-    def __init__(self, name, init_config, agentConfig, instances=None, counter_list):
+    def __init__(self, name, init_config, agentConfig, instances, counter_list):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
         self._countersettypes = {}
         self._counters = {}
@@ -38,9 +38,9 @@ class PDHBaseCheck(AgentCheck):
                 # which is the pdh name, datadog metric name, type, and the
                 # pdh counter object
                 self._metrics[key] = []
-                for counterset, inst_name, dd_name, mtype in counter_list:
+                for counterset, inst_name, counter_name, dd_name, mtype in counter_list:
                     m = getattr(self, mtype.lower())
-                    obj = WinPDHCounter(counterset, inst_name, self.log, inst_name)
+                    obj = WinPDHCounter(counterset, counter_name, self.log, inst_name)
                     entry = [inst_name, dd_name, m, obj]
                     self.log.debug("entry: %s" % str(entry))
                     self._metrics[key].append(entry)
@@ -52,13 +52,18 @@ class PDHBaseCheck(AgentCheck):
     def check(self, instance):
         key = hash_mutable(instance)
         for inst_name, dd_name, metric_func, counter in self._metrics[key]:
-            vals = counter.get_all_values()
-            for key, val in vals.iteritems():
-                tags = []
-                if key in self._tags:
-                    tags = self._tags[key]
+            try:
+                vals = counter.get_all_values()
+                for key, val in vals.iteritems():
+                    tags = []
+                    if key in self._tags:
+                        tags = self._tags[key]
 
-                if not counter.is_single_instance():
-                    tag = "instance=%s" % key
-                    tags.append(tag)
-                metric_func(dd_name, val, tags)
+                    if not counter.is_single_instance():
+                        tag = "instance=%s" % key
+                        tags.append(tag)
+                    metric_func(dd_name, val, tags)
+            except Exception as e:
+                # don't give up on all of the metrics because one failed
+                self.log.error("Failed to get data for %s %s: %s" % (inst_name, dd_name, str(e)))
+                pass
