66b0bef6c1a3da4f1bbc50c24fa1a55606a5a66a
Pierre Margueritte <mfpierre@gmail.com>
2017-11-20 10:32:11 +0100

    Decorrelate AD from docker to introduce multiple listeners (#814)
    *  Decorrelate AD from docker to introduce multiple listeners
    
    * Move DockerService with implementation

--- pkg/collector/listeners/types.go
+++ pkg/collector/listeners/types.go
@@ -5,28 +5,21 @@
 
 package listeners
 
+import log "github.com/cihub/seelog"
+
 // ID is the representation of the unique ID of a Service
 type ID string
-
-// DockerService implements and store results from the Service interface
-type DockerService struct {
-	ID            ID                // unique ID
-	ADIdentifiers []string          // identifiers on which templates will be matched
-	Hosts         map[string]string // network --> IP address
-	Ports         []int
-	Pid           int // Process identifier
-}
 
 // Service represents an application we can run a check against.
 // It should be matched with a check template by the ConfigResolver using the
 // ADIdentifiers field.
 type Service interface {
-	GetID() ID
-	GetADIdentifiers() ([]string, error)
-	GetHosts() (map[string]string, error)
-	GetPorts() ([]int, error)
-	GetTags() ([]string, error)
-	GetPid() (int, error)
+	GetID() ID                            // unique ID
+	GetADIdentifiers() ([]string, error)  // identifiers on which templates will be matched
+	GetHosts() (map[string]string, error) // network --> IP address
+	GetPorts() ([]int, error)             // network ports
+	GetTags() ([]string, error)           // tags
+	GetPid() (int, error)                 // process identifier
 }
 
 // ServiceListener monitors running services and triggers check (un)scheduling
@@ -36,4 +29,22 @@ type Service interface {
 type ServiceListener interface {
 	Listen(newSvc, delSvc chan<- Service)
 	Stop()
+}
+
+// ServiceListenerFactory builds a service listener
+type ServiceListenerFactory func() (ServiceListener, error)
+
+// ServiceListenerFactories holds the registered factories
+var ServiceListenerFactories = make(map[string]ServiceListenerFactory)
+
+// Register registers a service listener factory
+func Register(name string, factory ServiceListenerFactory) {
+	if factory == nil {
+		log.Warnf("Service listener factory %s does not exist.", name)
+	}
+	_, registered := ServiceListenerFactories[name]
+	if registered {
+		log.Errorf("Service listener factory %s already registered. Ignoring.", name)
+	}
+	ServiceListenerFactories[name] = factory
 }
