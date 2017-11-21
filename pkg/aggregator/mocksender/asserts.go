adff7183cdff2437d571b347cd4feaea8e5f3ac0
JulienBalestra <julien.balestra@datadoghq.com>
2017-11-21 14:01:23 +0100

    [mocksender] Cover by unittests the logic of AnythingBut, move private func to the bottom of the file + other style-fix

--- pkg/aggregator/mocksender/asserts.go
+++ pkg/aggregator/mocksender/asserts.go
@@ -18,27 +18,27 @@ import (
 // Additional tags over the ones specified don't make it fail
 // Assert the ServiceCheck wasn't called with any other possible status
 func (m *MockSender) AssertServiceCheck(t *testing.T, checkName string, status metrics.ServiceCheckStatus, hostname string, tags []string, message string) bool {
-	okCall := m.Mock.AssertCalled(t, "ServiceCheck", checkName, status, hostname, AssertTagsContains(tags), message)
-	notOkCalls := m.Mock.AssertNotCalled(t, "ServiceCheck", checkName, AnythingBut(status), hostname, AssertTagsContains(tags), mock.AnythingOfType("string"))
+	okCall := m.Mock.AssertCalled(t, "ServiceCheck", checkName, status, hostname, MatchTagsContains(tags), message)
+	notOkCalls := m.Mock.AssertNotCalled(t, "ServiceCheck", checkName, AnythingBut(status), hostname, MatchTagsContains(tags), mock.AnythingOfType("string"))
 	return okCall && notOkCalls
 }
 
 // AssertMetric allows to assert a metric was emitted with given parameters.
 // Additional tags over the ones specified don't make it fail
 func (m *MockSender) AssertMetric(t *testing.T, method string, metric string, value float64, hostname string, tags []string) bool {
-	return m.Mock.AssertCalled(t, method, metric, value, hostname, AssertTagsContains(tags))
+	return m.Mock.AssertCalled(t, method, metric, value, hostname, MatchTagsContains(tags))
 }
 
 // AssertMetricInRange allows to assert a metric was emitted with given parameters, with a value in a given range.
 // Additional tags over the ones specified don't make it fail
 func (m *MockSender) AssertMetricInRange(t *testing.T, method string, metric string, min float64, max float64, hostname string, tags []string) bool {
-	return m.Mock.AssertCalled(t, method, metric, AssertFloatInRange(min, max), hostname, AssertTagsContains(tags))
+	return m.Mock.AssertCalled(t, method, metric, AssertFloatInRange(min, max), hostname, MatchTagsContains(tags))
 }
 
 // AssertMetricTaggedWith allows to assert a metric was emitted with given tags, value and hostname not tested.
 // Additional tags over the ones specified don't make it fail
 func (m *MockSender) AssertMetricTaggedWith(t *testing.T, method string, metric string, tags []string) bool {
-	return m.Mock.AssertCalled(t, method, metric, mock.AnythingOfType("float64"), mock.AnythingOfType("string"), AssertTagsContains(tags))
+	return m.Mock.AssertCalled(t, method, metric, mock.AnythingOfType("float64"), mock.AnythingOfType("string"), MatchTagsContains(tags))
 }
 
 // AssertMetricNotTaggedWith allows to assert tags were never emitted for a metric.
@@ -46,30 +46,17 @@ func (m *MockSender) AssertMetricNotTaggedWith(t *testing.T, method string, metr
 	return m.Mock.AssertCalled(t, method, metric, mock.AnythingOfType("float64"), mock.AnythingOfType("string"), AssertTagsNotContains(tags))
 }
 
-// Return a bool value if all the elt of expected are inside the actual array
-func expectedInActual(expected, actual []string) bool {
-	expectedCount := 0
-	for _, e := range expected {
-		for _, a := range actual {
-			if e == a {
-				expectedCount++
-				break
-			}
-		}
-	}
-	return len(expected) == expectedCount
-}
-
 // AnythingBut match everything except the argument
+// It builds a mock.argumentMatcher
 func AnythingBut(expected interface{}) interface{} {
 	return mock.MatchedBy(func(actual interface{}) bool {
 		return !assert.ObjectsAreEqualValues(expected, actual)
 	})
 }
 
-// AssertTagsContains is a mock.argumentMatcher builder to be used in asserts.
+// MatchTagsContains is a mock.argumentMatcher builder to be used in asserts.
 // It allows to check if tags are emitted, ignoring unexpected ones and order.
-func AssertTagsContains(expected []string) interface{} {
+func MatchTagsContains(expected []string) interface{} {
 	return mock.MatchedBy(func(actual []string) bool {
 		return expectedInActual(expected, actual)
 	})
@@ -89,4 +76,18 @@ func AssertFloatInRange(min float64, max float64) interface{} {
 	return mock.MatchedBy(func(actual float64) bool {
 		return actual >= min && actual <= max
 	})
+}
+
+// Return a bool value if all the elements of expected are inside the actual array
+func expectedInActual(expected, actual []string) bool {
+	expectedCount := 0
+	for _, e := range expected {
+		for _, a := range actual {
+			if e == a {
+				expectedCount++
+				break
+			}
+		}
+	}
+	return len(expected) == expectedCount
 }
