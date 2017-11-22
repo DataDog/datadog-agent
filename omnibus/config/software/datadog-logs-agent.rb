fb1f87e078c64f36a7080b4d6263680fd63af9d0
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2017-11-22 19:34:32 +0100

    [omnibus] Make 4xx & 5xx http statuses fail the build (#835)
    When downloading the process agent/logs agent binaries.

--- omnibus/config/software/datadog-logs-agent.rb
+++ omnibus/config/software/datadog-logs-agent.rb
@@ -10,7 +10,7 @@ build do
   logs_agent_version = "alpha"
   binary = "logagent"
   url = "https://s3.amazonaws.com/public.binaries.sheepdog.datad0g.com/agent/#{logs_agent_version}/linux-amd64/#{binary}"
-  command "curl #{url} -o #{binary}"
+  command "curl -f #{url} -o #{binary}"
   command "chmod +x #{binary}"
   command "mv #{binary} #{install_dir}/bin/agent/logs-agent"
 end
