ecbd742bfaa3a1cc5cedb2ef7aad768382d675ba
Haissam Kaj <haissam.kaj@gmail.com>
2017-11-23 18:55:01 +0100

    [AD] First pass at ECS listener

--- pkg/collector/listeners/types.go
+++ pkg/collector/listeners/types.go
@@ -7,6 +7,10 @@ package listeners
 
 import log "github.com/cihub/seelog"
 
+const (
+	identifierLabel string = "io.datadog.check.id"
+)
+
 // ID is the representation of the unique ID of a Service
 type ID string
 
