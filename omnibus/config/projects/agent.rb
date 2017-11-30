9e88ab9672a18ab4c7d70a6fa491648707400d4e
Derek Brown <derek.brown@datadoghq.com>
2017-11-29 22:23:59 -0800

    Test additional signfiles in omnibus-ruby

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -88,6 +88,7 @@ package :msi do
   wix_candle_extension 'WixUtilExtension'
   wix_light_extension 'WixUtilExtension'
   extra_package_dir "#{Omnibus::Config.source_dir()}\\etc\\datadog-agent\\extra_package_files"
+  additional_sign_files ["#{Omnibus::Config.source_dir()}/datadog-agent/src/github.com/DataDog/datadog-agent/bin/agent/agent.exe"]
 #  extra_package_files File.absolute_path("..\\..\\etc\\datadog-agent\\extra_package_files")
   if ENV['SIGN_WINDOWS']
     signing_identity "ECCDAE36FDCB654D2CBAB3E8975AA55469F96E4C", machine_store: true, algorithm: "SHA256"
