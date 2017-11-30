617fa5028a47fe6134c58db1a6cdd8a175a1786b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-11-30 17:45:24 +0100

    [protobuf] ship protobuf shared lib for python deps
    [omnibus] bump protobuf to 3.5.1
    
    [omnibus] install protobuf wheel
    
    [protobuf] static compile should be explicit
    
    [protobuf] adding CXXFLAGS to environment
    
    [omnibus] workaround for windows protobuf build issues

--- omnibus/config/software/protobuf-py.rb
+++ omnibus/config/software/protobuf-py.rb
@@ -1,6 +1,12 @@
-name "protobuf-py"
-default_version "3.1.0"
-rhel_pip_version = "3.1.0.post1"
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https:#www.datadoghq.com/).
+# Copyright 2018 Datadog, Inc.
+#
+require './lib/ostools.rb'
+
+name "protobuf-py-cpp"
+default_version "3.5.1"
 
 dependency "python"
 dependency "setuptools"
@@ -8,43 +14,41 @@ dependency "pip"
 dependency "six"
 
 source :url => "https://github.com/google/protobuf/releases/download/v#{version}/protobuf-python-#{version}.tar.gz",
-       :sha256 => "0bc10bfd00a9614fae58c86c21fbcf339790e48accf6d45f098034de985f5405"
+       :sha256 => "13d3c15ebfad8c28bee203dd4a0f6e600d2a7d2243bac8b5d0e517466500fcae"
 
 relative_path "protobuf-#{version}/python"
 
-env = {}
+env = {
+  "CFLAGS" => "-fPIC",
+  "CXXFLAGS" => "-fPIC",
+}
 
 if ohai["platform_family"] == "mac_os_x"
   env["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
 end
 
 build do
-  ship_license "https://raw.githubusercontent.com/google/protobuf/3.1.x/LICENSE"
+  ship_license "https://raw.githubusercontent.com/google/protobuf/3.5.x/LICENSE"
 
-  # Note: RHEL5 is equipped with gcc4.1 that is not supported by Protobuf (it actually crashes during the build)
-  # so we use the official package from PyPI and skip the CPP extension for the time being.
-  if ohai["platform_family"] == "debian"
+  if windows?
+    pip "install protobuf==#{version}"
+  else
     # C++ runtime
     command ["cd .. && ./configure",
                 "--prefix=#{install_dir}/embedded",
-                "--enable-static=no",
-                "--without-zlib"].join(" ")
+                "--without-zlib"].join(" "), :env => env
 
     # You might want to temporarily uncomment the following line to check build sanity (e.g. when upgrading the
     # library) but there's no need to perform the check every time.
     # command "cd .. && make check"
     command "cd .. && make -j #{workers}"
-    command "cd .. && make install"
 
-    # Python lib
-    command "#{install_dir}/embedded/bin/python setup.py build --cpp_implementation", :env => env
+    command ["#{install_dir}/embedded/bin/python",
+             "setup.py",
+             "build",
+             "--cpp_implementation",
+             "--compile_static_extension"].join(" "), :env => env
     command "#{install_dir}/embedded/bin/python setup.py test --cpp_implementation", :env => env
     pip "install . --install-option=\"--cpp_implementation\""
-
-    # We don't need protoc anymore
-    delete "#{install_dir}/embedded/lib/libprotoc.*"
-    delete "#{install_dir}/embedded/bin/protoc"
-  else
-    pip "install protobuf==#{rhel_pip_version}"
   end
 end
