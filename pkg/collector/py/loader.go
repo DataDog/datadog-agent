b81f42e28886d4e96f2a47d2ddb4a331f90f9531
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-12-07 19:28:19 +0100

    [omni][wheels] if so-specified build the wheels
    [omnibus] set hashes for python requirements
    
    [py][loader] fix datadog wheels namespace
    
    [omnibus] datadog-checks-base is the name of the base checks integration
    
    [omnibus][integrations] fixing merge collateral
    
    [integrations] no need to ship check_requirements.txt

--- pkg/collector/py/loader.go
+++ pkg/collector/py/loader.go
@@ -54,7 +54,7 @@ func NewPythonCheckLoader() (*PythonCheckLoader, error) {
 func (cl *PythonCheckLoader) Load(config check.Config) ([]check.Check, error) {
 	checks := []check.Check{}
 	moduleName := config.Name
-	whlModuleName := fmt.Sprintf("datadog.%s", config.Name)
+	whlModuleName := fmt.Sprintf("datadog_checks.%s", config.Name)
 
 	// Looking for wheels first
 	modules := []string{whlModuleName, moduleName}
