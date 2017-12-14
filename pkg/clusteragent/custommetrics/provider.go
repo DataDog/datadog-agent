00e266068db241750a58a8d1b3a515d1ec65643e
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2017-12-14 19:53:59 +0100

    wip

--- pkg/clusteragent/custommetrics/provider.go
+++ pkg/clusteragent/custommetrics/provider.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	apimeta "k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
