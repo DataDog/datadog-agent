fc52f94eb87ddd773400931af8a39865f2bcc305
Haissam Kaj <haissam.kaj@gmail.com>
2017-12-15 03:24:03 +0100

    implement MetricBySelector, make error more descriptive

--- pkg/clusteragent/custommetrics/provider.go
+++ pkg/clusteragent/custommetrics/provider.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/metrics/pkg/apis/custom_metrics"
 
+	"github.com/golang/glog"
 	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/provider"
 )
 
@@ -34,39 +35,39 @@ func (p *datadogProvider) metricFor(value int64, groupResource schema.GroupResou
 	}, nil
 }
 
-// func (p *datadogProvider) metricsFor(totalValue int64, groupResource schema.GroupResource, metricName string, list runtime.Object) (*custom_metrics.MetricValueList, error) {
-// 	if !apimeta.IsListType(list) {
-// 		return nil, fmt.Errorf("returned object was not a list")
-// 	}
+func (p *datadogProvider) metricsFor(totalValue int64, groupResource schema.GroupResource, metricName string, list runtime.Object) (*custom_metrics.MetricValueList, error) {
+	if !apimeta.IsListType(list) {
+		return nil, fmt.Errorf("returned object was not a list")
+	}
 
-// 	res := make([]custom_metrics.MetricValue, 0)
+	res := make([]custom_metrics.MetricValue, 0)
 
-// 	err := apimeta.EachListItem(list, func(item runtime.Object) error {
-// 		objMeta := item.(metav1.Object)
-// 		value, err := p.metricFor(0, groupResource, objMeta.GetNamespace(), objMeta.GetName(), metricName)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		res = append(res, *value)
+	err := apimeta.EachListItem(list, func(item runtime.Object) error {
+		objMeta := item.(metav1.Object)
+		value, err := p.metricFor(0, groupResource, objMeta.GetNamespace(), objMeta.GetName(), metricName)
+		if err != nil {
+			return err
+		}
+		res = append(res, *value)
 
-// 		return nil
-// 	})
-// 	if err != nil {
-// 		return nil, err
-// 	}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
 
-// 	for i := range res {
-// 		res[i].Value = *resource.NewMilliQuantity(100*totalValue/int64(len(res)), resource.DecimalSI)
-// 	}
+	for i := range res {
+		res[i].Value = *resource.NewMilliQuantity(100*totalValue/int64(len(res)), resource.DecimalSI)
+	}
 
-// 	//return p.metricFor(value, groupResource, "", name, metricName)
-// 	return &custom_metrics.MetricValueList{
-// 		Items: res,
-// 	}, nil
-// }
+	//return p.metricFor(value, groupResource, "", name, metricName)
+	return &custom_metrics.MetricValueList{
+		Items: res,
+	}, nil
+}
 
 func (p *datadogProvider) GetRootScopedMetricByName(groupResource schema.GroupResource, name string, metricName string) (*custom_metrics.MetricValue, error) {
-	return nil, fmt.Errorf("Not Implemented")
+	return nil, fmt.Errorf("Not Implemented - RootScopedByName")
 }
 
 func (p *datadogProvider) GetRootScopedMetricBySelector(groupResource schema.GroupResource, selector labels.Selector, metricName string) (*custom_metrics.MetricValueList, error) {
@@ -95,7 +96,7 @@ func (p *datadogProvider) GetRootScopedMetricBySelector(groupResource schema.Gro
 	// 	return nil, err
 	// }
 	// return p.metricsFor(totalValue, groupResource, metricName, matchingObjectsRaw)
-	return nil, fmt.Errorf("Not Implemented")
+	return nil, fmt.Errorf("Not Implemented - RootScopedBySelector")
 }
 
 func (p *datadogProvider) GetNamespacedMetricByName(groupResource schema.GroupResource, namespace string, name string, metricName string) (*custom_metrics.MetricValue, error) {
@@ -103,31 +104,31 @@ func (p *datadogProvider) GetNamespacedMetricByName(groupResource schema.GroupRe
 }
 
 func (p *datadogProvider) GetNamespacedMetricBySelector(groupResource schema.GroupResource, namespace string, selector labels.Selector, metricName string) (*custom_metrics.MetricValueList, error) {
-	// // construct a client to list the names of objects matching the label selector
-	// client, err := p.client.ClientForGroupVersionResource(groupResource.WithVersion(""))
-	// if err != nil {
-	// 	glog.Errorf("unable to construct dynamic client to list matching resource names: %v", err)
-	// 	// don't leak implementation details to the user
-	// 	return nil, apierr.NewInternalError(fmt.Errorf("unable to list matching resources"))
-	// }
+	// construct a client to list the names of objects matching the label selector
+	client, err := p.client.ClientForGroupVersionResource(groupResource.WithVersion(""))
+	if err != nil {
+		glog.Errorf("unable to construct dynamic client to list matching resource names: %v", err)
+		// don't leak implementation details to the user
+		return nil, apierr.NewInternalError(fmt.Errorf("unable to list matching resources"))
+	}
 
-	// totalValue, err := p.valueFor(groupResource, metricName, true)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	totalValue, err := p.valueFor(groupResource, metricName, true)
+	if err != nil {
+		return nil, err
+	}
 
-	// // we can construct a this APIResource ourself, since the dynamic client only uses Name and Namespaced
-	// apiRes := &metav1.APIResource{
-	// 	Name:       groupResource.Resource,
-	// 	Namespaced: true,
-	// }
+	// we can construct a this APIResource ourself, since the dynamic client only uses Name and Namespaced
+	apiRes := &metav1.APIResource{
+		Name:       groupResource.Resource,
+		Namespaced: true,
+	}
 
-	// matchingObjectsRaw, err := client.Resource(apiRes, namespace).
-	// 	List(metav1.ListOptions{LabelSelector: selector.String()})
-	// if err != nil {
-	// 	return nil, err
-	// }
-	return nil, fmt.Errorf("Not Implemented")
+	matchingObjectsRaw, err := client.Resource(apiRes, namespace).
+		List(metav1.ListOptions{LabelSelector: selector.String()})
+	if err != nil {
+		return nil, err
+	}
+	return p.metricsFor(10, groupResource, name, metricName)
 }
 
 func (p *datadogProvider) ListAllMetrics() []provider.MetricInfo {
