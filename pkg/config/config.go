6527c8f8680e4b26426c7107e0aa99cc84ce08c6
Xavier Vello <xavier.vello@datadoghq.com>
2017-12-20 10:57:20 +0100

    add bindenv

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -177,12 +177,14 @@ func init() {
 	Datadog.BindEnv("kubernetes_kubelet_host")
 	Datadog.BindEnv("kubernetes_http_kubelet_port")
 	Datadog.BindEnv("kubernetes_https_kubelet_port")
+
 	Datadog.BindEnv("forwarder_timeout")
 	Datadog.BindEnv("forwarder_retry_queue_max_size")
 	Datadog.BindEnv("cloud_foundry")
 	Datadog.BindEnv("bosh_id")
 	Datadog.BindEnv("histogram_aggregates")
 	Datadog.BindEnv("histogram_percentiles")
+	Datadog.BindEnv("kubernetes_kubeconfig_path")
 
 	// Logs
 	BindEnvAndSetDefault("log_enabled", false)
