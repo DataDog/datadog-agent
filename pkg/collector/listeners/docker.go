a3426113e7f779ff692ca36af7d03f225ff34ee5
Xavier Vello <xavier.vello@datadoghq.com>
2017-12-27 13:55:38 +0100

    [docker] move event collection logic to dockerutil (#974)

--- pkg/collector/listeners/docker.go
+++ pkg/collector/listeners/docker.go
@@ -15,17 +15,13 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
+
+	log "github.com/cihub/seelog"
+	"github.com/docker/docker/api/types"
 
 	"github.com/DataDog/datadog-agent/cmd/agent/common/signals"
 	"github.com/DataDog/datadog-agent/pkg/tagger"
 	"github.com/DataDog/datadog-agent/pkg/util/docker"
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/events"
-	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker/client"
-
-	log "github.com/cihub/seelog"
 )
 
 const (
@@ -37,7 +33,6 @@ const (
 // It also holds a cache of services that the ConfigResolver can query to
 // match templates against.
 type DockerListener struct {
-	Client     *client.Client
 	dockerUtil *docker.DockerUtil
 	services   map[ID]Service
 	newService chan<- Service
@@ -62,16 +57,11 @@ func init() {
 // NewDockerListener creates a client connection to Docker and instantiate a DockerListener with it
 // TODO: TLS support
 func NewDockerListener() (ServiceListener, error) {
-	c, err := docker.ConnectToDocker()
-	if err != nil {
-		return nil, fmt.Errorf("Failed to connect to Docker, auto discovery will not work: %s", err)
-	}
 	d, err := docker.GetDockerUtil()
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to Docker, auto discovery will not work: %s", err)
 	}
 	return &DockerListener{
-		Client:     c,
 		dockerUtil: d,
 		services:   make(map[ID]Service),
 		stop:       make(chan bool),
@@ -87,28 +77,24 @@ func (l *DockerListener) Listen(newSvc chan<- Service, delSvc chan<- Service) {
 	// process containers that might be already running
 	l.init()
 
-	// filters only match start/stop container events
-	filters := filters.NewArgs()
-	filters.Add("type", "container")
-	filters.Add("event", "start")
-	filters.Add("event", "die")
-	eventOptions := types.EventsOptions{
-		Since:   fmt.Sprintf("%d", time.Now().Unix()),
-		Filters: filters,
+	messages, errs, err := l.dockerUtil.SubscribeToContainerEvents("DockerListener")
+	if err != nil {
+		log.Errorf("can't listen to docker events: %v", err)
+		signals.ErrorStopper <- true
+		return
 	}
-
-	messages, errs := l.Client.Events(context.Background(), eventOptions)
 
 	go func() {
 		for {
 			select {
 			case <-l.stop:
+				l.dockerUtil.UnsubscribeFromContainerEvents("DockerListener")
 				return
 			case msg := <-messages:
 				l.processEvent(msg)
 			case err := <-errs:
 				if err != nil && err != io.EOF {
-					log.Errorf("Docker listener error: %v", err)
+					log.Errorf("docker listener error: %v", err)
 					signals.ErrorStopper <- true
 				}
 				return
@@ -129,7 +115,7 @@ func (l *DockerListener) init() {
 	l.m.Lock()
 	defer l.m.Unlock()
 
-	containers, err := l.Client.ContainerList(context.Background(), types.ContainerListOptions{})
+	containers, err := l.dockerUtil.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
 		log.Errorf("Couldn't retrieve container list - %s", err)
 	}
@@ -172,10 +158,10 @@ func (l *DockerListener) GetServices() map[ID]Service {
 	return ret
 }
 
-// processEvent takes a Docker Message, tries to find a service linked to it, and
+// processEvent takes a ContainerEvent, tries to find a service linked to it, and
 // figure out if the ConfigResolver could be interested to inspect it.
-func (l *DockerListener) processEvent(e events.Message) {
-	cID := ID(e.Actor.ID)
+func (l *DockerListener) processEvent(e *docker.ContainerEvent) {
+	cID := ID(e.ContainerID)
 
 	l.m.RLock()
 	_, found := l.services[cID]
