f97f06f3592f903f80f4fd00a8fe69cc6cc470b5
JulienBalestra <julien.balestra@datadoghq.com>
2018-01-02 14:08:33 +0100

    autoconfig: reorganize the auto listener, separate docker specifics

--- cmd/agent/common/auto_listener_nodocker.go
+++ cmd/agent/common/auto_listener_nodocker.go
@@ -3,16 +3,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2017 Datadog, Inc.
 
-// +build docker
+// +build !docker
 
 package common
 
-import (
-	"github.com/DataDog/datadog-agent/pkg/util/docker"
-)
-
 // isDockerRunning check if docker is running
 func isDockerRunning() bool {
-	_, err := docker.GetDockerUtil()
-	return err == nil
+	return false
 }
