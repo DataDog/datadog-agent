7bacd0e4795ee95f0d787a1c7dfe9d376747357d
Derek Brown <derek.brown@datadoghq.com>
2018-01-16 16:48:43 -0800

    Go fmt

--- cmd/agent/app/dependent_services_windows.go
+++ cmd/agent/app/dependent_services_windows.go
@@ -35,11 +35,11 @@ var subservices = []Servicedef{
 		serviceInit: apmInit,
 	},
 	{
-	name:        "process",
-	configKey:   "process_agent_enabled",
-	serviceName: "datadog-process-agent",
-	serviceInit: processInit,
-}}
+		name:        "process",
+		configKey:   "process_agent_enabled",
+		serviceName: "datadog-process-agent",
+		serviceInit: processInit,
+	}}
 
 func apmInit() error {
 	traceAgentConfPath := filepath.Join(common.DefaultConfPath, "trace-agent.conf")
