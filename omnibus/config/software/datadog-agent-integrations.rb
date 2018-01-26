21f9d6d3afa7fd7ca7d8e60fe34e2ad3b52f0d9e
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2018-01-26 18:52:05 +0100

    Revert "Temporary revert of wheels build (#1096)"
    Let's use the wheels-based build again now that `beta.9` is out!
    
    This reverts commit 10f64a4accef17fda04c6b3a1161c3ed815a8039.

--- omnibus/config/software/datadog-agent-integrations.rb
+++ omnibus/config/software/datadog-agent-integrations.rb
@@ -23,10 +23,12 @@ default_version integrations_core_version
 
 
 blacklist = [
+  'datadog-checks-base',  # namespacing package for wheels (NOT AN INTEGRATION)
   'agent_metrics',
   'docker_daemon',
   'kubernetes',
   'ntp',  # provided as a go check by the core agent
+  'sqlserver',
   'vsphere',
 ]
 
@@ -46,13 +48,51 @@ build do
   # Copy the checks and generate the global requirements file
   block do
     all_reqs_file = File.open("#{project_dir}/check_requirements.txt", 'w+')
+    # Manually add "core" dependencies that are not listed in the checks requirements
+    # FIX THIS these dependencies have to be grabbed from somewhere
+    all_reqs_file.puts "requests==2.11.1 --hash=sha256:545c4855cd9d7c12671444326337013766f4eea6068c3f0307fb2dc2696d580e"
+    all_reqs_file.puts "pympler==0.5 --hash=sha256:7d16c4285f01dcc647f69fb6ed4635788abc7a7cb7caa0065d763f4ce3d21c0f"
+    all_reqs_file.puts "wheel==0.30.0 --hash=sha256:e721e53864f084f956f40f96124a74da0631ac13fbbd1ba99e8e2b5e9cafdf64"\
+        " --hash=sha256:9515fe0a94e823fd90b08d22de45d7bde57c90edce705b22f5e1ecf7e1b653c8"
+
+    all_reqs_file.close
+
+    # Install all the requirements
+    if windows?
+      pip_args = "install  -r #{project_dir}/check_requirements.txt"
+      command "#{windows_safe_path(install_dir)}\\embedded\\scripts\\pip.exe #{pip_args}"
+    else
+      build_env = {
+        "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+        "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
+      }
+      pip "install -r #{project_dir}/check_requirements.txt", :env => build_env
+    end
+
+    if windows?
+      build_args = "wheel --no-deps ."
+      command "#{windows_safe_path(install_dir)}\\embedded\\scripts\\pip.exe #{build_args}", :cwd => "#{project_dir}\\datadog-checks-base"
+      Dir.glob("#{project_dir}\\datadog-base\\*.whl").each do |wheel_path|
+        whl_file = wheel_path.split('/').last
+        install_args = "install #{whl_file}"
+        command "#{windows_safe_path(install_dir)}\\embedded\\scripts\\pip.exe #{install_args}", :cwd => "#{project_dir}\\datadog-checks-base"
+      end
+    else
+      build_env = {
+        "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+        "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
+      }
+      pip "wheel --no-deps .", :env => build_env, :cwd => "#{project_dir}/datadog-checks-base"
+      pip "install *.whl", :env => build_env, :cwd => "#{project_dir}/datadog-checks-base"
+    end
 
     Dir.glob("#{project_dir}/*").each do |check_dir|
       check = check_dir.split('/').last
 
-      next if blacklist.include? check
-      # Check the manifest to be sure that this check is enabled on this system
-      # or skip this iteration
+      next if !File.directory?("#{check_dir}") || blacklist.include?(check)
+
+      # If there is no manifest file, then we should assume the folder does not
+      # contain a working check and move onto the next
       manifest_file_path = "#{check_dir}/manifest.json"
 
       # If there is no manifest file, then we should assume the folder does not
@@ -62,13 +102,7 @@ build do
       manifest = JSON.parse(File.read(manifest_file_path))
       manifest['supported_os'].include?(os) || next
 
-      # Copy the checks over
-      if File.exist? "#{check_dir}/check.py"
-        copy "#{check_dir}/check.py", "#{checks_dir}/#{check}.py"
-      end
-
       check_conf_dir = "#{conf_dir}/#{check}.d"
-
       mkdir check_conf_dir unless File.exist? check_conf_dir
 
       # For each conf file, if it already exists, that means the `datadog-agent` software def
@@ -99,30 +133,23 @@ build do
         end
       end
 
-      next unless File.exist?("#{check_dir}/requirements.txt") && !manifest['use_omnibus_reqs']
-
-      reqs = File.open("#{check_dir}/requirements.txt", 'r').read
-      reqs.each_line do |line|
-        all_reqs_file.puts line if line[0] != '#'
+      File.file?("#{check_dir}/setup.py") || next
+      if windows?
+        build_args = "wheel --no-deps ."
+        command "#{windows_safe_path(install_dir)}\\embedded\\scripts\\pip.exe #{build_args}", :cwd => "#{project_dir}\\#{check}"
+        Dir.glob("#{project_dir}\\#{check}\\*.whl").each do |wheel_path|
+          whl_file = wheel_path.split('/').last
+          install_args = "install #{whl_file}"
+          command "#{windows_safe_path(install_dir)}\\embedded\\scripts\\pip.exe #{install_args}", :cwd => "#{project_dir}\\#{check}"
+        end
+      else
+        build_env = {
+          "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+          "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
+        }
+        pip "wheel --no-deps .", :env => build_env, :cwd => "#{project_dir}/#{check}"
+        pip "install *.whl", :env => build_env, :cwd => "#{project_dir}/#{check}"
       end
     end
-
-    # Manually add "core" dependencies that are not listed in the checks requirements
-    all_reqs_file.puts "requests==2.11.1"
-    all_reqs_file.puts "pympler==0.5"
-
-    all_reqs_file.close
-  end
-
-  # Install all the requirements
-  if windows?
-    pip_args = "install  -r #{project_dir}/check_requirements.txt"
-    command "#{windows_safe_path(install_dir)}\\embedded\\scripts\\pip.exe #{pip_args}"
-  else
-    build_env = {
-      "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-      "PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}",
-    }
-    pip "install -r #{project_dir}/check_requirements.txt", :env => build_env
   end
 end
