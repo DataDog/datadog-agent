2e96fc183af1ba6803530858bf7a2112b764f0e4
charlyF <charly@datadoghq.com>
2018-01-26 21:10:35 +0100

    adding multi pod/svc/nodes case to unit tests

--- pkg/util/kubernetes/apiserver/services_test.go
+++ pkg/util/kubernetes/apiserver/services_test.go
@@ -4,9 +4,11 @@
 // Copyright 2018 Datadog, Inc.
 
 // +build kubeapiserver
+
 package apiserver
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/ericchiang/k8s/api/v1"
@@ -24,36 +26,41 @@ type svcStruct struct {
 	podIps  []string
 }
 
-func createSvcList(nodeName string, svc svcStruct) v1.EndpointsList {
+func toPtr(str string) *string {
+	strToptr := str
+	return &strToptr
+}
+
+func createSvcList(nodeName string, svcs []svcStruct) v1.EndpointsList {
 	var list v1.EndpointsList
-	var endpoints v1.Endpoints
-	var endpointsSubset v1.EndpointSubset
-	var edpt v1.EndpointAddress
+	for _, svc := range svcs {
+		var endpoints v1.Endpoints
+		var endpointsSubset v1.EndpointSubset
+		endpoints.Metadata = &metav1.ObjectMeta{}
+		endpoints.Subsets = append(endpoints.Subsets, &endpointsSubset)
+		endpoints.Metadata.Name = toPtr(svc.svcName)
 
-	endpoints.Metadata = &metav1.ObjectMeta{}
-
-	endpoints.Subsets = append(endpoints.Subsets, &endpointsSubset)
-
-	endpoints.Metadata.Name = &svc.svcName
-	edpt.NodeName = &nodeName
-
-	for _, e := range svc.podIps {
-		edpt.Ip = &e
-		endpointsSubset.Addresses = append(endpointsSubset.Addresses, &edpt)
+		for _, e := range svc.podIps {
+			var edpt v1.EndpointAddress
+			edpt.NodeName = &nodeName
+			edpt.Ip = toPtr(e)
+			endpointsSubset.Addresses = append(endpointsSubset.Addresses, &edpt)
+		}
+		list.Items = append(list.Items, &endpoints)
 	}
-
-	list.Items = append(list.Items, &endpoints)
 	return list
 }
 
 func createPodList(listPodStructs []podStruct) v1.PodList {
 	var podlist v1.PodList
-	var pod v1.Pod
-	pod.Status = &v1.PodStatus{}
-	pod.Metadata = &metav1.ObjectMeta{}
 	for _, ps := range listPodStructs {
-		pod.Status.PodIP = &ps.ip
-		pod.Metadata.Name = &ps.name
+		var pod v1.Pod
+		pod.Status = &v1.PodStatus{}
+		pod.Metadata = &metav1.ObjectMeta{}
+		pod.Status.PodIP = toPtr(ps.ip)
+		pod.Metadata.Name = toPtr(ps.name)
+		fmt.Printf("\n ps.name is %s, toPtr ps.name is %q and &ps.name is %q \n\n", ps.name, toPtr(ps.name), &ps.name)
+		//fmt.Printf("current pod added is %s and pod list is %q \n", ps.name, podlist.Items)
 		podlist.Items = append(podlist.Items, &pod)
 	}
 
@@ -73,50 +80,84 @@ func TestMapServices(t *testing.T) {
 
 	node1 := createNode("firstNode")
 	pod1 := podStruct{
-		ip:   "1.2.3.4",
-		name: "foo",
+		ip:   "1.1.1.1",
+		name: "pod1_name",
 	}
-	svc := svcStruct{
+	svc1 := svcStruct{
 		svcName: "svc1",
-		podIps: []string{
-			"1.2.3.4",
-		},
+		podIps:  []string{"1.1.1.1"},
 	}
 	k8PodList1 := createPodList([]podStruct{pod1})
-	k8EpList := createSvcList(*node1.Metadata.Name, svc)
+	k8EpList := createSvcList(*node1.Metadata.Name, []svcStruct{svc1})
 
 	smb.mapServices(*node1.Metadata.Name, k8PodList1, k8EpList)
+
 	res := make(map[string][]string)
-	res[pod1.name] = []string{svc.svcName}
+	res[pod1.name] = []string{svc1.svcName}
+
 	require.Equal(t, res, smb.PodNameToServices)
 
 	// Test 3 nodes, 5 pods, 4 services
-	// smb := newServiceMapperBundle()
+	smb1 := newServiceMapperBundle()
 
-	// node1 := createNode("firstNode")
-	// node2 := createNode("firstNode")
-	// node2 := createNode("firstNode")
-	// pod1 := podStruct{
-	// 	ip:   "1.2.3.4",
-	// 	name: "foo",
-	// }
-	// svc := svcStruct{
-	// 	svcName: "svc1",
-	// 	podIps: []string{
-	// 		"1.2.3.4",
-	// 	},
-	// }
-	// k8PodList1 := createPodList([]podStruct{pod1})
-	// k8EpList := createSvcList(*node1.Metadata.Name, svc)
+	node2 := createNode("secondNode")
+	pod2 := podStruct{
+		ip:   "2.2.2.2",
+		name: "pod2_name",
+	}
+	pod3 := podStruct{
+		ip:   "3.3.3.3",
+		name: "pod3_name",
+	}
+	pod4 := podStruct{
+		ip:   "4.4.4.4",
+		name: "pod4_name",
+	}
+	pod5 := podStruct{
+		ip:   "5.5.5.5",
+		name: "pod5_name",
+	}
+	svc2 := svcStruct{
+		svcName: "svc2",
+		podIps: []string{
+			"2.2.2.2",
+		},
+	}
+	svc3 := svcStruct{
+		svcName: "svc3",
+		podIps: []string{
+			"2.2.2.2",
+			"5.5.5.5",
+			"1.1.1.1",
+		},
+	}
+	svc4 := svcStruct{
+		svcName: "svc4",
+		podIps: []string{
+			"2.2.2.2",
+			"3.3.3.3",
+		},
+	}
+	k8PodList2 := createPodList([]podStruct{pod1, pod2, pod3, pod4})
+	k8EpList2 := createSvcList(*node2.Metadata.Name, []svcStruct{svc1, svc3, svc4})
 
-	// node2 := createNode("secondNode")
-	// pod2 := podStruct{
-	// 	ip: '0.0.0.0',
-	// 	name: 'bar'
-	// }
-	// pod3 := podStruct{
-	// 	ip: '0.1.0.1',
-	// 	name: 'baz'
-	// }
-	// k8PodList2 := createPodList([]podStruct{pod2,pod3})
+	fmt.Printf("input is pod list %q and ep list %q", k8PodList2, k8EpList2)
+	smb1.mapServices(*node2.Metadata.Name, k8PodList2, k8EpList2) // Simulate that we evaluate the node2
+
+	res2 := make(map[string][]string)
+	res2[pod1.name] = []string{svc1.svcName, svc3.svcName}
+	res2[pod2.name] = []string{svc3.svcName, svc4.svcName} // No SV2 as it's on pod2
+	res2[pod3.name] = []string{svc4.svcName}
+	fmt.Printf("smb is %q", smb1.PodNameToServices)
+	require.Equal(t, res2, smb1.PodNameToServices) // Though pod4 and sv4 are being evaluated, as none of the svcs point to pod4, it won't be in the list.
+
+	// We check that evaluating new services/pods on a given SMB (that would be gotten from the cache) maps as expected.
+	k8PodList3 := createPodList([]podStruct{pod1, pod2, pod3, pod4, pod5})
+	k8EpLis3 := createSvcList(*node2.Metadata.Name, []svcStruct{svc1, svc2, svc3, svc4})
+
+	res2[pod5.name] = []string{svc3.svcName}
+	res2[pod2.name] = []string{svc2.svcName, svc3.svcName, svc4.svcName} // SV2 as it's on pod2
+	smb1.mapServices(*node2.Metadata.Name, k8PodList3, k8EpLis3)
+	fmt.Printf("smb is %q", smb1.PodNameToServices)
+	require.Equal(t, res2, smb1.PodNameToServices) // We can imagine a case
 }
