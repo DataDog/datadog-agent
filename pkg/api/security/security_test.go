eca255a50b2b0209ae8c3f8c8e48526d30bed9c1
Julien Balestra <julien.balestra@gmail.com>
2018-01-29 14:09:13 +0100

    security/token: only maintain windows and others, const for token len (#1126)

---
+++ pkg/api/security/security_test.go
@@ -0,0 +1,42 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+// +build !windows
+
+package security
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+)
+
+func TestFetchAuthTokenValidGen(t *testing.T) {
+	testDir, err := ioutil.TempDir("", "fake-datadog-etc-")
+	require.Nil(t, err, fmt.Sprintf("%v", err))
+	defer os.RemoveAll(testDir)
+
+	f, err := ioutil.TempFile(testDir, "fake-datadog-yaml-")
+	require.Nil(t, err, fmt.Errorf("%v", err))
+	defer os.Remove(f.Name())
+
+	config.Datadog.SetConfigFile(f.Name())
+	expectTokenPath := path.Join(testDir, "auth_token")
+	defer os.Remove(expectTokenPath)
+
+	config.Datadog.Set("auth_token", "")
+	token, err := FetchAuthToken()
+	require.Nil(t, err, fmt.Sprintf("%v", err))
+	assert.True(t, len(token) > authTokenMinimalLen, fmt.Sprintf("%d", len(token)))
+	_, err = os.Stat(expectTokenPath)
+	require.Nil(t, err)
+}
