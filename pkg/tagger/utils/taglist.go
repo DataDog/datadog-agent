091474e997e82692e70f175d380466989b868a68
JulienBalestra <julien.balestra@datadoghq.com>
2018-02-01 15:29:34 +0100

    taglist: improve the tag list to support multiple tags with the same name but different values

--- pkg/tagger/utils/taglist.go
+++ pkg/tagger/utils/taglist.go
@@ -13,31 +13,31 @@ import (
 // TagList allows collector to incremental build a tag list
 // then export it easily to []string format
 type TagList struct {
-	lowCardTags  map[string]string
-	highCardTags map[string]string
+	lowCardTags  map[string]bool
+	highCardTags map[string]bool
 }
 
 // NewTagList creates a new object ready to use
 func NewTagList() *TagList {
 	return &TagList{
-		lowCardTags:  make(map[string]string),
-		highCardTags: make(map[string]string),
+		lowCardTags:  make(map[string]bool),
+		highCardTags: make(map[string]bool),
 	}
 }
 
-// AddHigh adds a new high cardinality tag to the list, or replace if name already exists.
+// AddHigh adds a new high cardinality tag to the map, or replace if already exists.
 // It will skip empty values/names, so it's safe to use without verifying the value is not empty.
 func (l *TagList) AddHigh(name string, value string) {
-	if len(name) > 0 && len(value) > 0 {
-		l.highCardTags[name] = value
+	if name != "" && value != "" {
+		l.highCardTags[fmt.Sprintf("%s:%s", name, value)] = true
 	}
 }
 
-// AddLow adds a new low cardinality tag to the list, or replace if name already exists.
+// AddLow adds a new low cardinality tag to the list, or replace if already exists.
 // It will skip empty values/names, so it's safe to use without verifying the value is not empty.
 func (l *TagList) AddLow(name string, value string) {
-	if len(name) > 0 && len(value) > 0 {
-		l.lowCardTags[name] = value
+	if name != "" && value != "" {
+		l.lowCardTags[fmt.Sprintf("%s:%s", name, value)] = true
 	}
 }
 
@@ -58,13 +58,13 @@ func (l *TagList) Compute() ([]string, []string) {
 	high := make([]string, len(l.highCardTags))
 
 	index := 0
-	for k, v := range l.lowCardTags {
-		low[index] = fmt.Sprintf("%s:%s", k, v)
+	for tag := range l.lowCardTags {
+		low[index] = tag
 		index++
 	}
 	index = 0
-	for k, v := range l.highCardTags {
-		high[index] = fmt.Sprintf("%s:%s", k, v)
+	for tag := range l.highCardTags {
+		high[index] = tag
 		index++
 	}
 	return low, high
