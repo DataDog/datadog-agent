b183421ef109b321668584012bce57f3b976db2b
Arthur Bellal <arthur.bellal@datadoghq.com>
2018-02-13 14:27:19 +0100

    remove jmx file logging

--- pkg/collector/corechecks/embed/jmx.go
+++ pkg/collector/corechecks/embed/jmx.go
@@ -288,7 +288,6 @@ func (c *JMXCheck) start() error {
 		"--ipc_port", fmt.Sprintf("%v", config.Datadog.GetInt("cmd_port")),
 		"--check_period", fmt.Sprintf("%v", int(check.DefaultCheckInterval/time.Millisecond)), // Period of the main loop of jmxfetch in ms
 		"--log_level", jmxLogLevel,
-		"--log_location", path.Join(here, "dist", "jmx", "jmxfetch.log"), // FIXME : Path of the log file. At some point we should have a `run` folder
 		"--reporter", reporter, // Reporter to use
 		jmxCollectCommand, // Name of the command
 	)
