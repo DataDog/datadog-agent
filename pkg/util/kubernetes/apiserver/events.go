99054440d48b3fd7d30746ec06cd7140ea2aeb76
charlyF <charly@datadoghq.com>
2018-02-14 18:34:07 -0500

    reducing log level to Trace

--- pkg/util/kubernetes/apiserver/events.go
+++ pkg/util/kubernetes/apiserver/events.go
@@ -36,7 +36,7 @@ func (c *APIClient) LatestEvents(since string) ([]*v1.Event, []*v1.Event, string
 	// If `since` is "" strconv.Atoi(*latestResVersion) below will panic as we evaluate the error.
 	// One could chose to use "" instead of 0 to not query the API Server cache.
 	// We decide to only rely on the cache as it avoids crawling everything from the API Server at once.
-	log.Debugf("since value is %q", since)
+	log.Tracef("since value is %q", since)
 	resVersionCached, err := strconv.Atoi(since)
 	if err != nil {
 		log.Errorf("The cached event token could not be parsed: %s, pulling events from the API server's cache", err)
@@ -66,15 +66,13 @@ func (c *APIClient) LatestEvents(since string) ([]*v1.Event, []*v1.Event, string
 	for {
 		meta, event, err := watcher.Next()
 		if err != nil {
-			// We sometimes face a protobuf unmarshal issue, it will happen if there is no event to collect.
+			// We sometimes face a protobuf unmarshal issue, it will happen if there is no recent events to collect.
 			// Or if the resVersion is not available in the API Server's cache.
 			// We need to reset the resVersion to seamlessly continue watching.
-			// The resVersion forced to "0" is dealt with in the check to avoid collecting duplicates.
 			if strings.Contains(err.Error(), "illegal wireType") {
-				log.Debugf("Protobuf error, no recent events to collect: %s", err) // To move to Tracef
+				log.Tracef("Protobuf error, no recent events to collect: %s", err)
 				*latestResVersion = "0"
 				continue
-				// break or continue ?
 			}
 			if err != context.Canceled && err != io.EOF && !strings.Contains(err.Error(), "illegal wireType") {
 				log.Debugf("Stopping event collection, got error: %s", err)
