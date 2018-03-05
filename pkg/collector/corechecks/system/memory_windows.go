ed5f61aef70f7f442031addfb4fa759f69d47c71
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-03-05 13:07:44 -0500

    Updates Windows memory metrics (#1397)
    * Splits Memory check out to separate Windows as not all metrics are available

---
+++ pkg/collector/corechecks/system/memory_windows.go
@@ -0,0 +1,51 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+// +build windows
+
+package system
+
+import (
+	"fmt"
+
+	log "github.com/cihub/seelog"
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator"
+)
+
+// Run executes the check
+func (c *MemoryCheck) Run() error {
+	sender, err := aggregator.GetSender(c.ID())
+	if err != nil {
+		return err
+	}
+
+	v, errVirt := virtualMemory()
+	if errVirt == nil {
+		sender.Gauge("system.mem.total", float64(v.Total)/mbSize, "", nil)
+		sender.Gauge("system.mem.free", float64(v.Available)/mbSize, "", nil)
+		sender.Gauge("system.mem.used", float64(v.Total-v.Available)/mbSize, "", nil)
+		sender.Gauge("system.mem.pct_usable", float64(100-v.UsedPercent)/100, "", nil)
+	} else {
+		log.Errorf("system.MemoryCheck: could not retrieve virtual memory stats: %s", errVirt)
+	}
+
+	s, errSwap := swapMemory()
+	if errSwap == nil {
+		sender.Gauge("system.swap.total", float64(s.Total)/mbSize, "", nil)
+		sender.Gauge("system.swap.free", float64(s.Free)/mbSize, "", nil)
+		sender.Gauge("system.swap.used", float64(s.Used)/mbSize, "", nil)
+		sender.Gauge("system.swap.pct_free", float64(100-s.UsedPercent)/100, "", nil)
+	} else {
+		log.Errorf("system.MemoryCheck: could not retrieve swap memory stats: %s", errSwap)
+	}
+
+	if errVirt != nil && errSwap != nil {
+		return fmt.Errorf("failed to gather any memory information")
+	}
+
+	sender.Commit()
+	return nil
+}
---
+++ pkg/collector/corechecks/system/memory_windows_test.go
@@ -0,0 +1,64 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+// +build windows
+
+package system
+
+import (
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/stretchr/testify/require"
+)
+
+func VirtualMemory() (*mem.VirtualMemoryStat, error) {
+	return &mem.VirtualMemoryStat{
+		Total:       12345667890,
+		Available:   234567890,
+		Used:        10000000000,
+		UsedPercent: 81,
+	}, nil
+}
+
+func SwapMemory() (*mem.SwapMemoryStat, error) {
+	return &mem.SwapMemoryStat{
+		Total:       100000,
+		Used:        40000,
+		Free:        60000,
+		UsedPercent: 40,
+		Sin:         21,
+		Sout:        22,
+	}, nil
+}
+
+func TestMemoryCheckWindows(t *testing.T) {
+	virtualMemory = VirtualMemory
+	swapMemory = SwapMemory
+	memCheck := new(MemoryCheck)
+
+	mock := mocksender.NewMockSender(memCheck.ID())
+
+	runtimeOS = "linux"
+
+	mock.On("Gauge", "system.mem.free", 234567890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.used", 12111100000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.total", 12345667890.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.mem.pct_usable", 0.19, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.total", 100000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.free", 60000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.used", 40000.0/mbSize, "", []string(nil)).Return().Times(1)
+	mock.On("Gauge", "system.swap.pct_free", 0.6, "", []string(nil)).Return().Times(1)
+
+	mock.On("Commit").Return().Times(1)
+
+	err := memCheck.Run()
+	require.Nil(t, err)
+
+	mock.AssertExpectations(t)
+	mock.AssertNumberOfCalls(t, "Gauge", 8)
+	mock.AssertNumberOfCalls(t, "Commit", 1)
+}
