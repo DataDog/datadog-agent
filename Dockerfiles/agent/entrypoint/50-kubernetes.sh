85f47197e17d0210dde26d4f974c27c2778c4139
Xavier Vello <xavier.vello@datadoghq.com>
2018-03-05 16:48:00 +0100

    make docker integration optional in the docker image (#1371)

--- Dockerfiles/agent/entrypoint/50-kubernetes.sh
+++ Dockerfiles/agent/entrypoint/50-kubernetes.sh
@@ -7,21 +7,21 @@ fi
 # Set a default config for Kubernetes if found
 # Don't override /etc/datadog-agent/datadog.yaml if it exists
 
-cd /etc/datadog-agent/
-
-if [[ ! -e datadog.yaml ]]; then
-    ln -s datadog-kubernetes.yaml datadog.yaml
+if [[ ! -e /etc/datadog-agent/datadog.yaml ]]; then
+    ln -s  /etc/datadog-agent/datadog-kubernetes.yaml \
+           /etc/datadog-agent/datadog.yaml
 fi
 
 # Enable kubernetes integrations (don't fail if integration absent)
 
-cd /etc/datadog-agent/conf.d
-
-mv kubelet.d/conf.yaml.example kubelet.d/conf.yaml.default || true
+mv /etc/datadog-agent/conf.d/kubelet.d/conf.yaml.example \
+   /etc/datadog-agent/conf.d/kubelet.d/conf.yaml.default || true
 
 # The apiserver check requires leader election to be enabled
+
 if [[ "$DD_LEADER_ELECTION" ]]; then
-    mv kubernetes_apiserver.d/conf.yaml.example kubernetes_apiserver.d/conf.yaml.default || true
+    mv /etc/datadog-agent/conf.d/kubernetes_apiserver.d/conf.yaml.example \
+       /etc/datadog-agent/conf.d/kubernetes_apiserver.d/conf.yaml.default || true
 else
     echo "Disabling the apiserver check as leader election is disabled"
 fi
