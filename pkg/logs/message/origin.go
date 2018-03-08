5a42ea44aa2a2d2139ccf1cc04df5cdfd07c600f
Alexandre Jacquemot <alexandre.jacquemot@datadoghq.com>
2018-03-08 15:13:53 +0100

    [Logs]: Fixed issue with missing tags when collecting logs from docker container (#1420)

--- pkg/logs/message/origin.go
+++ pkg/logs/message/origin.go
@@ -49,11 +49,17 @@ func (o *Origin) TagsPayload() []byte {
 	if o.LogSource.Config.SourceCategory != "" {
 		tagsPayload = append(tagsPayload, []byte("[dd ddsourcecategory=\""+o.LogSource.Config.SourceCategory+"\"]")...)
 	}
+	var tags string
 	if o.LogSource.Config.Tags != "" {
-		tags := o.LogSource.Config.Tags
-		if len(o.tags) > 0 {
-			tags = tags + "," + strings.Join(o.tags, ",")
+		tags = o.LogSource.Config.Tags
+	}
+	if len(o.tags) > 0 {
+		if tags != "" {
+			tags = tags + ","
 		}
+		tags = tags + strings.Join(o.tags, ",")
+	}
+	if tags != "" {
 		tagsPayload = append(tagsPayload, []byte("[dd ddtags=\""+tags+"\"]")...)
 	}
 	if len(tagsPayload) == 0 {
