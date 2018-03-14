099bbfe1f2311f3bbf810416b8f5b96fdd902b9e
Ofek Lev <ofekmeister@gmail.com>
2018-03-14 17:19:47 -0400

    more testing

--- pkg/forwarder/backoff_test.go
+++ pkg/forwarder/backoff_test.go
@@ -6,11 +6,36 @@
 package forwarder
 
 import (
+	"math"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func TestMinBackoffFactorValid(t *testing.T) {
 	assert.True(t, minBackoffFactor >= 2)
+}
+
+func TestRandomBetween(t *testing.T) {
+	getRandomMinMax := func() (float64, float64) {
+		a := float64(rand.Intn(10))
+		b := float64(rand.Intn(10))
+		min := math.Min(a, b)
+		max := math.Max(a, b)
+		return min, max
+	}
+
+	for i := 1; i < 100; i++ {
+		min, max := getRandomMinMax()
+		between := randomBetween(min, max)
+
+		assert.True(t, min <= between)
+		assert.True(t, max > between)
+	}
 }
