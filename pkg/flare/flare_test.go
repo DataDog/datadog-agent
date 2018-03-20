25fa4f883f87f9621aab2fbae3606954fbcf26b7
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-20 15:26:40 -0400

    adds better flare test

--- pkg/flare/flare_test.go
+++ pkg/flare/flare_test.go
@@ -6,10 +6,14 @@
 package flare
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/DataDog/datadog-agent/cmd/agent/common"
 	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/version"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,4 +23,34 @@ func TestMkURL(t *testing.T) {
 	config.Datadog.Set("api_key", "123456")
 	assert.Equal(t, "https://example.com/support/flare/999?api_key=123456", mkURL("999"))
 	assert.Equal(t, "https://example.com/support/flare?api_key=123456", mkURL(""))
+}
+
+func TestFlareHasRightForm(t *testing.T) {
+	var lastRequest *http.Request
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		io.WriteString(w, "{}")
+		lastRequest = r
+	}))
+	defer ts.Close()
+
+	ddURL := ts.URL
+
+	config.Datadog.Set("dd_url", ddURL)
+
+	archivePath := "./test/blank.zip"
+	caseID := "12345"
+	email := "dev@datadoghq.com"
+
+	_, err := SendFlare(archivePath, caseID, email)
+
+	assert.Nil(t, err)
+
+	av, _ := version.New(version.AgentVersion, version.Commit)
+
+	assert.Equal(t, caseID, lastRequest.FormValue("case_id"))
+	assert.Equal(t, email, lastRequest.FormValue("email"))
+	assert.Equal(t, av.String(), lastRequest.FormValue("agent_version"))
+
 }
