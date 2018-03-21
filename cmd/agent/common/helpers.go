c0a2de31c386a63d88035011c864baf20e8920ff
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-21 17:39:43 -0400

    allow the config path to be set by environment variable

--- cmd/agent/common/helpers.go
+++ cmd/agent/common/helpers.go
@@ -7,6 +7,7 @@ package common
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/DataDog/datadog-agent/pkg/config"
@@ -14,6 +15,19 @@ import (
 
 // SetupConfig fires up the configuration system
 func SetupConfig(confFilePath string) error {
+	// Grab the environment variable
+	envConfigPath := os.Getenv("DD_CONFIG_PATH")
+
+	// Check the environment config path first,
+	// the confFilePath from the cli should override
+	// so don't add it to the config paths until the second step
+	if len(envConfigPath) != 0 {
+		// If they set a config file directly, let's try to honor that
+		if strings.HasSuffix(envConfigPath, ".yaml") {
+			config.Datadog.SetConfigFile(envConfigPath)
+		}
+	}
+
 	// set the paths where a config file is expected
 	if len(confFilePath) != 0 {
 		// if the configuration file path was supplied on the command line,
@@ -24,6 +38,14 @@ func SetupConfig(confFilePath string) error {
 			config.Datadog.SetConfigFile(confFilePath)
 		}
 	}
+
+	// set the paths where a config file is expected
+	if len(envConfigPath) != 0 {
+		// if the configuration file path was supplied on the command line,
+		// add that first so it's first in line
+		config.Datadog.AddConfigPath(envConfigPath)
+	}
+
 	config.Datadog.AddConfigPath(DefaultConfPath)
 
 	// load the configuration
