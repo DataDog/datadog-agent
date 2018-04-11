e712f9cb9fe638afcaca1c651cb42f61d3405503
Antoine Pouille <antoine.pouille@datadoghq.com>
2018-04-11 18:39:28 +0200

    Change dependancies + build tags

--- pkg/util/kubernetes/hostinfo/host_alias.go
+++ pkg/util/kubernetes/hostinfo/host_alias.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2018 Datadog, Inc.
 
-// +build kubelet,kubeapiserver
+// +build kubelet
 
 package hostinfo
 
@@ -11,16 +11,14 @@ import (
 	"fmt"
 
 	"github.com/DataDog/datadog-agent/pkg/util"
-	"github.com/DataDog/datadog-agent/pkg/util/hostname"
+	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/kubelet"
 )
 
 // GetHostAlias uses the "kubelet" hostname provider to fetch the kubernetes alias
 func GetHostAlias() (string, error) {
-	if getKubeletHostname, found := hostname.ProviderCatalog["kubelet"]; found {
-		name, err := getKubeletHostname("")
-		if err == nil && util.ValidHostname(name) == nil {
-			return name, nil
-		}
+	name, err := kubelet.HostnameProvider("")
+	if err == nil && util.ValidHostname(name) == nil {
+		return name, nil
 	}
 	return "", fmt.Errorf("Couldn't extract a host alias from the kubelet")
 }
