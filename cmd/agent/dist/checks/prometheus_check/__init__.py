b4388e3867d835e660941630bfcb38014361a5af
Ofek Lev <ofekmeister@gmail.com>
2018-05-16 19:38:32 -0400

    reuse python code from datadog_checks_base

--- cmd/agent/dist/checks/prometheus_check/__init__.py
+++ cmd/agent/dist/checks/prometheus_check/__init__.py
@@ -1,6 +1,4 @@
 # (C) Datadog, Inc. 2016
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-
-from prometheus_check import *
-from . functions import parse_metric_family  # noqa: F401
+from datadog_checks.checks.prometheus import PrometheusCheck
