93f0badaad9ba69d8b3ddb49f25947700f5a334e
JulienBalestra <julien.balestra@datadoghq.com>
2018-05-17 13:54:04 +0200

    Display the status of the units

--- test/e2e/scripts/run-instance/10-pupernetes-ready.sh
+++ test/e2e/scripts/run-instance/10-pupernetes-ready.sh
@@ -4,5 +4,10 @@ set -x
 
 until curl -sf http://127.0.0.1:8989/ready --connect-timeout 1 -w '\n'
 do
+    for unit in pupernetes p8s-kubelet p8s-etcd
+    do
+        systemctl status ${unit}.service --no-pager
+        echo "---"
+    done
     sleep 10
 done
