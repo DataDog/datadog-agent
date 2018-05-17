0ffaa4d6d5a63735e6374f10ce49af300d3ac726
Maxime Mouial <maxime.mouial@gmail.com>
2018-05-17 17:03:55 -0400

    Allow users to use a secrets manager to store conf passwords
    User can now specify an external command to fetch secrets. Encrypted
    secrets are then replace in configurations.

---
+++ pkg/secrets/secrets.go
@@ -0,0 +1,169 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+// +build !windows
+
+package secrets
+
+import (
+	"fmt"
+	"strings"
+
+	log "github.com/cihub/seelog"
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+)
+
+var secretCache map[string]string
+
+func init() {
+	secretCache = make(map[string]string)
+}
+
+type walkerCallback func(string) (string, error)
+
+func walkSlice(data []interface{}, callback walkerCallback) error {
+	for idx, k := range data {
+		switch v := k.(type) {
+		case string:
+			newValue, err := callback(v)
+			if err != nil {
+				return err
+			}
+			data[idx] = newValue
+		case map[interface{}]interface{}:
+			if err := walkHash(v, callback); err != nil {
+				return err
+			}
+		case []interface{}:
+			if err := walkSlice(v, callback); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func walkHash(data map[interface{}]interface{}, callback walkerCallback) error {
+	for k := range data {
+		switch v := data[k].(type) {
+		case string:
+			newValue, err := callback(v)
+			if err != nil {
+				return err
+			}
+			data[k] = newValue
+		case map[interface{}]interface{}:
+			if err := walkHash(v, callback); err != nil {
+				return err
+			}
+		case []interface{}:
+			if err := walkSlice(v, callback); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// walk will go through loaded yaml and call callback on every strings allowing
+// the callback to overwrite the string value
+func walk(data *interface{}, callback walkerCallback) error {
+	switch v := (*data).(type) {
+	case string:
+		newValue, err := callback(v)
+		if err != nil {
+			return err
+		}
+		*data = newValue
+	case map[interface{}]interface{}:
+		return walkHash(v, callback)
+	case []interface{}:
+		return walkSlice(v, callback)
+	}
+	return nil
+}
+
+func isEnc(str string) (bool, string) {
+	// trimming space and tabs
+	str = strings.Trim(str, " 	")
+	if strings.HasPrefix(str, "ENC[") && strings.HasSuffix(str, "]") {
+		return true, str[4 : len(str)-1]
+	}
+	return false, ""
+}
+
+// testing purpose
+var secretFetcher = fetchSecret
+
+// Decrypt replaces all encrypted secrets in data by executing
+// "secret_backend_command" once if all secrets aren't present in the cache.
+func Decrypt(data []byte) ([]byte, error) {
+	if data == nil || config.Datadog.GetString("secret_backend_command") == "" {
+		return data, nil
+	}
+
+	var config interface{}
+	err := yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, fmt.Errorf("could not Unmarshal config: %s", err)
+	}
+
+	// First we collect all new handles in the config
+	newHandles := []string{}
+	haveSecret := false
+	err = walk(&config, func(str string) (string, error) {
+		if ok, handle := isEnc(str); ok {
+			haveSecret = true
+			// Check if we already know this secret
+			if secret, ok := secretCache[handle]; ok {
+				log.Debugf("Secret '%s' was retrieved from cache", handle)
+				return secret, nil
+			}
+			newHandles = append(newHandles, handle)
+		}
+		return str, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// the configuration does not contain any secrets
+	if !haveSecret {
+		return data, nil
+	}
+
+	// check if any new secrets need to be fetch
+	if len(newHandles) != 0 {
+		secrets, err := secretFetcher(newHandles)
+		if err != nil {
+			return nil, err
+		}
+
+		// Replace all new encrypted secrets in the config
+		err = walk(&config, func(str string) (string, error) {
+			if ok, handle := isEnc(str); ok {
+				if secret, ok := secrets[handle]; ok {
+					log.Debugf("Secret '%s' was retrieved from executable", handle)
+					return secret, nil
+				}
+				// This should never happen since fetchSecret will return an error
+				// if not every handles have been fetched.
+				return str, fmt.Errorf("unknown secret '%s'", handle)
+			}
+			return str, nil
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	finalConfig, err := yaml.Marshal(config)
+	if err != nil {
+		return nil, fmt.Errorf("could not Marshal config after replacing encrypted secrets: %s", err)
+	}
+	return finalConfig, nil
+}
---
+++ pkg/secrets/test/timeout.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while [ true ]
+do
+	sleep 1
+done
