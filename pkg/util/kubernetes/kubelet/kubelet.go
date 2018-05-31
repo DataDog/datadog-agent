0e2b37b5837e7a913e73039839cc3c7116a06211
Charly Fontaine <charly@datadoghq.com>
2018-05-31 15:36:59 -0400

    Broader tolerance for client errors leading to a fallback on http (#1720)
    * broader tolerance for client errors leading to a fallback on http

--- pkg/util/kubernetes/kubelet/kubelet.go
+++ pkg/util/kubernetes/kubelet/kubelet.go
@@ -411,10 +411,10 @@ func (ku *KubeUtil) setupKubeletApiEndpoint() error {
 			log.Debugf("Kubelet endpoint is: %s", ku.kubeletApiEndpoint)
 			return nil
 		}
-		if code != http.StatusUnauthorized {
+		if code < 400 && code >= 500 {
 			return fmt.Errorf("unexpected status code %d on endpoint %s%s", code, ku.kubeletApiEndpoint, kubeletPodPath)
 		}
-		log.Warn("Failed to securely reach the kubelet over HTTPS. Trying a non secure connection over HTTP. We highly recommend configuring TLS to access the kubelet")
+		log.Warnf("Failed to securely reach the kubelet over HTTPS, received a status %d. Trying a non secure connection over HTTP. We highly recommend configuring TLS to access the kubelet", code)
 	}
 	log.Debugf("Cannot query %s%s: %s", ku.kubeletApiEndpoint, kubeletPodPath, httpsUrlErr)
 
