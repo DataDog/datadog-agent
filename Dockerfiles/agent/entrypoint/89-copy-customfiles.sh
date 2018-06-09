170630aa693446388e87ce9e999efcee7f694f52
int <kattu0426@gmail.com>
2018-06-10 00:16:14 +0900

    feat: change custom config file to expand environment when copying

--- Dockerfiles/agent/entrypoint/89-copy-customfiles.sh
+++ Dockerfiles/agent/entrypoint/89-copy-customfiles.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Expand environment variables
+for file in /conf.d/*.yaml.tpl;
+do
+  conf_file=`echo $file | sed 's/\.tpl//'`
+  envsubst < $file > $conf_file
+done
 # Copy the custom checks and confs in the /etc/datadog-agent folder
 find /conf.d -name '*.yaml' -exec cp --parents -fv {} /etc/datadog-agent/ \;
 find /checks.d -name '*.py' -exec cp --parents -fv {} /etc/datadog-agent/ \;
