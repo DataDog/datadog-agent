8ccba80742b7972980970fb599366ac3d7936d92
Arthur Bellal <arthur.bellal@datadoghq.com>
2018-06-13 12:43:33 +0200

    fix

--- pkg/util/hostname.go
+++ pkg/util/hostname.go
@@ -134,7 +134,7 @@ func GetHostname() (string, error) {
 		}
 		h, err := os.Hostname()
 		if err == nil && h != name {
-			log.Warnf("DEPRECATION NOTICE: The agent resolved your hostname as %s. However starting from version 6.4, it will be resolved as %s by default. To enable the behavior of 6.4+, please enable the `hostname_fqdn` flag in the configuration.")
+			log.Warnf("DEPRECATION NOTICE: The agent resolved your hostname as %s. However starting from version 6.4, it will be resolved as %s by default. To enable the behavior of 6.4+, please enable the `hostname_fqdn` flag in the configuration.", h, name)
 		}
 	} else {
 		log.Debug("Unable to get FQDN from system: ", err)
