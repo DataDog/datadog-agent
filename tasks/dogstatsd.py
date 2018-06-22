a8639504a1f26f68ddfc13988f979da3484447d7
Xavier Vello <xavier.vello@datadoghq.com>
2018-06-22 17:55:46 +0200

    Fix dogstatsd static build with go 1.10 (#1873)
    * make dsd-statc static again, add test
    
    * [invoke][dsd6] address PR feedback - no stripping, larger binary limit

--- tasks/dogstatsd.py
+++ tasks/dogstatsd.py
@@ -45,6 +45,7 @@ def build(ctx, rebuild=False, race=False, static=False, build_include=None,
     if static:
         bin_path = STATIC_BIN_PATH
 
+    # NOTE: consider stripping symbols to reduce binary size 
     cmd = "go build {race_opt} {build_type} -tags '{build_tags}' -o {bin_name} "
     cmd += "-gcflags=\"{gcflags}\" -ldflags=\"{ldflags}\" {REPO_PATH}/cmd/dogstatsd/"
     args = {
