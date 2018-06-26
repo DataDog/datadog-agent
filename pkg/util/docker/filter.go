426d49474dd2c0d977ff30eb74d94c7a2f0a0ccb
Xavier Vello <xavier.vello@datadoghq.com>
2018-06-26 11:18:43 -0400

    Add ECS pause containers to the default docker exclusion list

--- pkg/util/docker/filter.go
+++ pkg/util/docker/filter.go
@@ -25,6 +25,7 @@ const (
 	pauseContainerGCR        = `image:(.*)gcr\.io(/google_containers/|/)pause(.*)`
 	pauseContainerOpenshift  = "image:openshift/origin-pod"
 	pauseContainerKubernetes = "image:kubernetes/pause"
+	pauseContainerECS        = "image:amazon/amazon-ecs-pause"
 	// pauseContainerAzure regex matches:
 	// - k8s-gcrio.azureedge.net/pause-amd64
 	// - gcrio.azureedge.net/google_containers/pause-amd64
@@ -92,7 +93,13 @@ func NewFilterFromConfig() (*Filter, error) {
 	blacklist := config.Datadog.GetStringSlice("ac_exclude")
 
 	if config.Datadog.GetBool("exclude_pause_container") {
-		blacklist = append(blacklist, pauseContainerGCR, pauseContainerOpenshift, pauseContainerKubernetes, pauseContainerAzure)
+		blacklist = append(blacklist,
+			pauseContainerGCR,
+			pauseContainerOpenshift,
+			pauseContainerKubernetes,
+			pauseContainerAzure,
+			pauseContainerECS,
+		)
 	}
 	return NewFilter(whitelist, blacklist)
 }
