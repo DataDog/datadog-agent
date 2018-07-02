e52bc8110c797fc1b1a54515d127d23282daff36
JulienBalestra <julien.balestra@datadoghq.com>
2018-07-02 17:32:41 +0200

    pupernetes: upgrade to 0.6.0

--- test/e2e/scripts/setup-instance/01-ignition.sh
+++ test/e2e/scripts/setup-instance/01-ignition.sh
@@ -46,7 +46,7 @@ tee ignition.json << EOF
       {
         "enabled": true,
         "name": "pupernetes.service",
-        "contents": "[Unit]\nDescription=Run pupernetes\nRequires=setup-pupernetes.service docker.service\nAfter=setup-pupernetes.service docker.service\n\n[Service]\nEnvironment=SUDO_USER=core\nExecStart=/opt/bin/pupernetes daemon run /opt/sandbox --hyperkube-version 1.10.1 --kubectl-link /opt/bin/kubectl -v 5 --run-timeout 48h\nRestart=on-failure\nRestartSec=5\nType=notify\n\n[Install]\nWantedBy=multi-user.target\n"
+        "contents": "[Unit]\nDescription=Run pupernetes\nRequires=setup-pupernetes.service docker.service\nAfter=setup-pupernetes.service docker.service\n\n[Service]\nEnvironment=SUDO_USER=core\nExecStart=/opt/bin/pupernetes daemon run /opt/sandbox --kubectl-link /opt/bin/kubectl -v 5 --run-timeout 48h\nRestart=on-failure\nRestartSec=5\nType=notify\n\n[Install]\nWantedBy=multi-user.target\n"
       },
       {
         "name": "terminate.service",
@@ -73,7 +73,7 @@ tee ignition.json << EOF
         "path": "/opt/bin/setup-pupernetes",
         "mode": 320,
         "contents": {
-          "source": "data:,%23%21%2Fbin%2Fbash%20-ex%0Acurl%20-Lf%20https%3A%2F%2Fgithub.com%2FDataDog%2Fpupernetes%2Freleases%2Fdownload%2Fv0.5.0%2Fpupernetes%20-o%20%2Fopt%2Fbin%2Fpupernetes%0Asha512sum%20-c%20%2Fopt%2Fbin%2Fpupernetes.sha512sum%0Achmod%20%2Bx%20%2Fopt%2Fbin%2Fpupernetes%0A"
+          "source": "data:,%23%21%2Fbin%2Fbash%20-ex%0Acurl%20-Lf%20https%3A%2F%2Fgithub.com%2FDataDog%2Fpupernetes%2Freleases%2Fdownload%2Fv0.6.0%2Fpupernetes%20-o%20%2Fopt%2Fbin%2Fpupernetes%0Asha512sum%20-c%20%2Fopt%2Fbin%2Fpupernetes.sha512sum%0Achmod%20%2Bx%20%2Fopt%2Fbin%2Fpupernetes%0A"
         },
         "filesystem": "root"
       },
@@ -81,17 +81,9 @@ tee ignition.json << EOF
         "path": "/opt/bin/pupernetes.sha512sum",
         "mode": 256,
         "contents": {
-          "source": "data:,3aa9d450349183b312a53e7280a095eb6e1508d429bf32c991e95d58690a576d1bc9cadafe5cec8674d17a91c247f1de96fd80f74c0608d6043f3fab5cb71677%20%2Fopt%2Fbin%2Fpupernetes%0A"
+          "source": "data:,47257210de7941a2242a1e7f17d5c5a45442cda3bc4be876023a7bd1932025705679a4eec6244d264d364b4b7496ab4544023d1cb5e381588c24286dbe5fb8e2%20%20.%2Fpupernetes%0A"
         },
         "filesystem": "root"
-      },
-      {
-        "path": "/home/core/.kube/config",
-        "filesystem": "root",
-        "user": {
-          "name": "core"
-        },
-        "mode": 420
       }
     ]
   },
