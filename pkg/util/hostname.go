5a0e92e30f24a798aedd40bb581266f6f69a865b
Arthur <arthur.bellal@datadoghq.com>
2018-07-19 18:07:36 +0200

    Update hostname.go

--- pkg/util/hostname.go
+++ pkg/util/hostname.go
@@ -169,7 +169,7 @@ func GetHostname() (string, error) {
 		}
 	}
 
-	// REMOVEME: This should be removed in 6.4
+	// REMOVEME: This should be removed in 6.5
 	h, err := os.Hostname()
 	if err == nil && !config.Datadog.GetBool("hostname_fqdn") && hostName == h && h != fqdn {
 		log.Warnf("DEPRECATION NOTICE: The agent resolved your hostname as '%s'. However starting from version 6.5, it will be resolved as '%s' by default. To enable the behavior of 6.5+, please enable the `hostname_fqdn` flag in the configuration. For more information: https://dtdg.co/flag-hostname-fqdn", h, fqdn)
