240bfc207a9a3875b8c6735c030962f75bd64d60
Derek Brown <derek.brown@datadoghq.com>
2018-08-02 11:48:55 -0700

    Add circle check that requires a label of `team/*` on the PR before (#2041)
    * Add circle check that requires a label of `team/*` on the PR before
    accepting for merge.  This simplifies test assignment for releases,
    since multiple teams check into this repo
    
    * Add add'l comment
    
    * Change the name of the job back to `reno_linting`, even though it's
    doing more than reno.  The github config expects the name
    `reno_linting`, and it doesnt' seem worth it to change just for this.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -7,7 +7,7 @@ from invoke import Collection
 from . import agent, android, benchmarks, docker, dogstatsd, pylauncher, cluster_agent, systray, release
 
 from .go import fmt, lint, vet, cyclo, ineffassign, misspell, deps, reset
-from .test import test, integration_tests, version, lint_releasenote, lint_filenames, e2e_tests
+from .test import test, integration_tests, version, lint_teamassignment, lint_releasenote, lint_filenames, e2e_tests
 from .build_tags import audit_tag_impact
 
 # the root namespace
@@ -25,6 +25,7 @@ ns.add_task(integration_tests)
 ns.add_task(deps)
 ns.add_task(reset)
 ns.add_task(version)
+ns.add_task(lint_teamassignment)
 ns.add_task(lint_releasenote)
 ns.add_task(lint_filenames)
 ns.add_task(audit_tag_impact)
