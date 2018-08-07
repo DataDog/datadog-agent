4ac33586d8cc6e2209c9cd04bd095821ee2ac072
Tristan Michelet <tristan@datadoghq.com>
2018-08-07 09:54:23 +0200

    [Logs] Docker - properly handle restart of docker containers monitored (#2116)
    * [Logs] Docker - properly handle restart of docker containers monitored
    
    Previously, we would tail from beginning at each restart, causing
    duplicates of logs
    
    * move release note to proper place...
    
    * rename reno

--- pkg/logs/input/docker/tailer_test.go
+++ pkg/logs/input/docker/tailer_test.go
@@ -9,15 +9,32 @@ package docker
 
 import (
 	"testing"
+	"time"
 
+	"github.com/DataDog/datadog-agent/pkg/logs/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTailerNextLogSinceDate(t *testing.T) {
 	tailer := &Tailer{}
-	assert.Equal(t, "2008-01-12T01:01:01.000000001Z", tailer.nextLogSinceDate("2008-01-12T01:01:01.000000000Z"))
-	assert.Equal(t, "2008-01-12T01:01:01.anything", tailer.nextLogSinceDate("2008-01-12T01:01:01.anything"))
-	assert.Equal(t, "", tailer.nextLogSinceDate(""))
+	var date time.Time
+	var err error
+	date, err = tailer.nextLogSinceDate("2008-01-12T01:01:01.000000000Z")
+	assert.Equal(t, "2008-01-12T01:01:01.000000001Z", date.Format(config.DateFormat))
+	assert.Nil(t, err)
+
+	_, err = tailer.nextLogSinceDate("2008-01-12T01:01:01.anything")
+	assert.NotNil(t, err)
+
+	_, err = tailer.nextLogSinceDate("")
+	assert.NotNil(t, err)
+}
+
+func TestTailerNextLogDate(t *testing.T) {
+	tailer := &Tailer{}
+	assert.Equal(t, "2008-01-12T01:01:01.000000001Z", tailer.nextLogDate("2008-01-12T01:01:01.000000000Z", true))
+	assert.Equal(t, "0001-01-01T00:00:00.000000000Z", tailer.nextLogDate("", true))
+	assert.NotEqual(t, "0001-01-01T00:00:00.000000000Z", tailer.nextLogDate("", false))
 }
 
 func TestTailerIdentifier(t *testing.T) {
