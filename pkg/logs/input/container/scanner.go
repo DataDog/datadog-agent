c80131913b54fdbbd164b5baa198fc608fcb27b7
Alexandre Jacquemot <alexandre.jacquemot@datadoghq.com>
2018-08-10 14:25:38 +0200

    Added a comment to explain how to enable the kubernetes integration and listed its limitations

--- pkg/logs/input/container/scanner.go
+++ pkg/logs/input/container/scanner.go
@@ -14,12 +14,19 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/logs/restart"
 )
 
-// NewScanner returns a new container scanner.
+// NewScanner returns a new container scanner,
+// by default it returns the docker scanner.
+//
+// When running in kubernetes, the kubernetes integration can be enabled either setting:
+// - the environment variable `DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL` to true
+// - the datadog.yaml configuration parameter `logs_config.container_collect_all` to true
+// For now, it's not possible to enable the kubernetes integration using an integration file and
+// it's not possible to define pod or container filters either.
+// However, pod annotations are supported to enrich and process logs but not for discovery purpose.
+// The current implementation of the kubernetes integration is still a work in progress and will be improved in the future.
 func NewScanner(sources *config.LogSources, pp pipeline.Provider, auditor *auditor.Auditor) restart.Restartable {
 	if config.LogsAgent.GetBool("logs_config.container_collect_all") {
 		if scanner, err := kubernetes.NewScanner(sources); err == nil {
-			// For now, avoid manually scanning docker containers when in a
-			// kubernetes environment, and rely on Kubernetes API.
 			return scanner
 		}
 	}
