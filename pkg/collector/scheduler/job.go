18a50135a06278e7a69c01c1a8b8297bab3681df
Arthur <arthur.bellal@datadoghq.com>
2018-08-27 18:24:12 +0200

    remove actions

--- pkg/collector/scheduler/job.go
+++ pkg/collector/scheduler/job.go
@@ -166,7 +166,7 @@ func (jq *jobQueue) process(out chan<- check.Check) bool {
 		log.Tracef("Bucket ticked... current index: %v", jq.currentBucketIdx)
 		jq.mu.Lock()
 		if !jq.lastTick.Equal(time.Time{}) && t.After(jq.lastTick.Add(2*time.Second)) {
-			log.Debugf("Previous bucket took over %v to schedule. Next checks will be running behind the schedule. Please disable checks that are taking too long to run or consider adding more runners.", t.Sub(jq.lastTick))
+			log.Debugf("Previous bucket took over %v to schedule. Next checks will be running behind the schedule.", t.Sub(jq.lastTick))
 		}
 		jq.lastTick = t
 		bucket := jq.buckets[jq.currentBucketIdx]
