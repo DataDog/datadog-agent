9bfd0de9700d5fcec32ad64fc495e804644977aa
Sami Tabet <sami.tabet@datadoghq.com>
2018-08-27 15:11:58 -0400

    update tests and benchmarks to use display name instead of name

--- pkg/procmatch/procmatch_test.go
+++ pkg/procmatch/procmatch_test.go
@@ -22,25 +22,25 @@ func TestMatchIntegration(t *testing.T) {
 		integration string
 	}{
 		{"java org.elasticsearch.bootstrap.Elasticsearch ‑Xms28000m ‑Xmx28000m ‑XX:+UseCompressedOops ‑Djna.tmpdir=/tmp/elasticsearch/jna ‑XX:+UseConcMarkSweepGC ‑XX:CMSInitiatingOccupancyFraction=75 ‑XX:+UseCMSInitiatingOccupancyOnly ‑XX:+DisableExplicitGC ‑XX:+AlwaysPreTouch ‑server ‑Xss1m ‑Djava.awt.headless=true ‑Dfile.encoding=UTF-8 ‑Djna.nosys=true ‑Djdk.io.permissionsUseCanonicalPath=true ‑Dio.netty.noUnsafe=true ‑Dio.netty.noKeySetOptimization=true ‑Dio.netty.recycler.maxCapacityPerThread=0 ‑Dlog4j.shutdownHookEnabled=false ‑Dlog4j2.disable.jmx=true ‑Dlog4j.skipJansi=true ‑Des.path.home=/usr/share/elasticsearch ‑Des.path.conf=/config ‑cp /usr/share/elasticsearch/lib/* ‑p /var/run/elasticsearch.pid ‑Epath.logs=/logs ‑Epath.data=/data ",
-			"elastic"},
+			"Elasticsearch"},
 		{"gunicorn: master [mcnulty]",
-			"gunicorn"},
+			"Gunicorn"},
 		{"java kafka.Kafka /usr/local/kafka/config/server.properties ‑Xmx4G ‑Xms4G ‑server ‑XX:+UseCompressedOops ‑XX:PermSize=48m ‑XX:MaxPermSize=48m ‑XX:+UseG1GC ‑XX:MaxGCPauseMillis=20 ‑XX:InitiatingHeapOccupancyPercent=35 ‑Djava.awt.headless=true ‑Xloggc:/mnt/log/kafka/kafkaServer-gc.log ‑verbose:gc ‑XX:+PrintGCDetails ‑XX:+PrintGCDateStamps ‑XX:+PrintGCTimeStamps ‑Dcom.sun.management.jmxremote ‑Dcom.sun.management.jmxremote.authenticate=false ‑Dcom.sun.management.jmxremote.ssl=false ‑Dcom.sun.management.jmxremote.port=9999",
-			"kafka"},
+			"Kafka"},
 		{"haproxy ‑p /run/haproxy.pid ‑db ‑f /usr/local/etc/haproxy/haproxy.cfg ‑Ds",
-			"haproxy"},
+			"HAProxy"},
 		{"mongod ‑-config /config/mongodb.conf",
-			"mongo"},
+			"MongoDB"},
 		{"java -Xmx4000m -Xms4000m -XX:ReservedCodeCacheSize=256m -port 9999 kafka.Kafka",
-			"kafka"},
+			"Kafka"},
 		{"java -Xmx4000m -Xms4000m -XX:ReservedCodeCacheSize=256m -port 9999 kafka.Kafka",
-			"kafka"},
+			"Kafka"},
 		{"/usr/local/bin/consul agent -config-dir /etc/consul.d",
-			"consul"},
+			"Consul"},
 		{"/usr/bin/python /usr/local/bin/supervisord -c /etc/supervisord.conf",
-			"supervisord"},
+			"Supervisord"},
 		{"/usr/sbin/pgbouncer -d /etc/pgbouncer/pgbouncer.ini",
-			"pgbouncer"},
+			"PGBouncer"},
 	}
 
 	for _, c := range cases {
@@ -55,11 +55,11 @@ func TestOverlappingSignatures(t *testing.T) {
 		integration string
 	}{
 		{"java org.elasticsearch.bootstrap.Elasticsearch -p=mypath",
-			"elastic"},
+			"Elasticsearch"},
 		{"java org.elasticsearch.bootstrap.Elasticsearch",
-			"elastic"},
+			"Elasticsearch"},
 		{"java ***** kafka.kafka",
-			"kafka"},
+			"Kafka"},
 	}
 
 	for _, c := range cases {
