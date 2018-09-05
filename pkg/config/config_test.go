5d707b565b973ece4de45dfa538f281394f72b6a
Olivier Vielpeau <olivier.vielpeau@datadoghq.com>
2018-09-05 15:09:05 +0200

    [config] Handle `app.datadoghq.eu` similarly to `app.datadoghq.com`
    The `app.datad0g.com`/`app.datadoghq.com` `dd_url`s are "re-written" so that
    the Agent actually sends to versioned endpoints.
    
    Make similar transformations to `app.datad0g.eu`/`app.datadoghq.eu`
    `dd_url`s.

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -153,6 +153,10 @@ func TestAddAgentVersionToDomain(t *testing.T) {
 	require.Nil(t, err)
 	assert.Equal(t, "https://"+getDomainPrefix("flare")+".datadoghq.com", newURL)
 
+	newURL, err = AddAgentVersionToDomain("https://app.datadoghq.eu", "app")
+	require.Nil(t, err)
+	assert.Equal(t, "https://"+getDomainPrefix("app")+".datadoghq.eu", newURL)
+
 	newURL, err = AddAgentVersionToDomain("https://app.myproxy.com", "app")
 	require.Nil(t, err)
 	assert.Equal(t, "https://app.myproxy.com", newURL)
