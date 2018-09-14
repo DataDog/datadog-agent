4365c1f53f9aca656c47c63eb90c67dadf5c182d
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-09-14 03:04:29 +0200

    [gohai][make] trying to fix ldflags build issue

--- pkg/gohai/make.go
+++ pkg/gohai/make.go
@@ -26,15 +26,20 @@ func main() {
 	if len(os.Args) > 1 {
 		gobin = os.Args[1]
 	}
-
-	date := time.Now().Format(time.UnixDate)
+	date := time.Now().Format(time.RFC3339)
 	commit := commandOutput("git", "rev-parse", "--short", "HEAD")
 	branch := commandOutput("git", "rev-parse", "--abbrev-ref", "HEAD")
-	go_version := commandOutput(gobin, "version")
+	version := commandOutput(gobin, "version")
 
-	ldflags := fmt.Sprintf("\"-X main.buildDate='%s' -X=main.gitCommit='%s' -X main.gitBranch='%s' -X main.goVersion='%s'\"", date, commit, branch, go_version)
+	// expected go_version output: go version go1.9.2 darwin/amd64
+	versionRune := []rune(strings.Split(version, " ")[2])
+	goVersion := string(versionRune[2:])
+
+	var ldflags string
+	ldflags = fmt.Sprintf("-X main.buildDate=%s -X main.gitCommit=%s -X main.gitBranch=%s -X main.goVersion=%s", date, commit, branch, goVersion)
 
 	cmd := exec.Command(gobin, []string{"build", "-a", "-ldflags", ldflags}...)
+	cmd.Env = os.Environ()
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
