b8b175d144e8ecdc5a2450cc557e4d6ec4324339
Sami Tabet <sami.tabet@datadoghq.com>
2018-09-21 11:42:11 -0400

    Add windows pollProcesses implementation

--- pkg/autodiscovery/procdiscovery/process_windows.go
+++ pkg/autodiscovery/procdiscovery/process_windows.go
@@ -2,9 +2,33 @@
 
 package procdiscovery
 
-import "errors"
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-agent/pkg/util/winutil/pdhutil"
+)
 
 // pollProcesses retrieves processes running on the host
 func pollProcesses() ([]process, error) {
-	return nil, errors.New("Not implemented yet on windows")
+	var counter *pdhutil.PdhCounterSet
+	var err error
+
+	counter, err = pdhutil.GetCounterSet("Process", "ID Process", "", nil)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't get counter set: %s", err)
+	}
+
+	values, err := counter.GetAllValues()
+	if err != nil {
+		return nil, fmt.Errorf("couldn't get counter values: %s", err)
+	}
+
+	pl := make([]process, 0, len(values))
+
+	// Not really the process CLI but it seems there is no way to get it using PDH atm
+	for name, pid := range values {
+		pl = append(pl, process{cmd: name, pid: int32(pid)})
+	}
+
+	return pl, nil
 }
