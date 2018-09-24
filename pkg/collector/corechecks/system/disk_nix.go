b064836a7d73bf4f93ab410b78007d6f7a1f3b56
Rémi Calixte <remi.calixte@datadoghq.com>
2018-09-24 14:19:01 -0400

    Remove "percent" metric

--- pkg/collector/corechecks/system/disk_nix.go
+++ pkg/collector/corechecks/system/disk_nix.go
@@ -119,17 +119,15 @@ func (c *DiskCheck) sendPartitionMetrics(sender aggregator.Sender, usage *disk.U
 	sender.Gauge(fmt.Sprintf(diskMetric, "total"), float64(usage.Total)/1024, "", tags)
 	sender.Gauge(fmt.Sprintf(diskMetric, "used"), float64(usage.Used)/1024, "", tags)
 	sender.Gauge(fmt.Sprintf(diskMetric, "free"), float64(usage.Free)/1024, "", tags)
+	// FIXME: 6.x, use percent, a lot more logical than in_use
 	sender.Gauge(fmt.Sprintf(diskMetric, "in_use"), usage.UsedPercent/100, "", tags)
-	// Use percent, a lot more logical than in_use
-	sender.Gauge(fmt.Sprintf(diskMetric, "used.percent"), usage.UsedPercent, "", tags)
 
 	// Inodes metrics
 	sender.Gauge(fmt.Sprintf(inodeMetric, "total"), float64(usage.InodesTotal), "", tags)
 	sender.Gauge(fmt.Sprintf(inodeMetric, "used"), float64(usage.InodesUsed), "", tags)
 	sender.Gauge(fmt.Sprintf(inodeMetric, "free"), float64(usage.InodesFree), "", tags)
+	// FIXME: 6.x, use percent, a lot more logical than in_use
 	sender.Gauge(fmt.Sprintf(inodeMetric, "in_use"), usage.InodesUsedPercent/100, "", tags)
-	// Use percent, a lot more logical than in_use
-	sender.Gauge(fmt.Sprintf(inodeMetric, "used.percent"), usage.InodesUsedPercent, "", tags)
 
 }
 
