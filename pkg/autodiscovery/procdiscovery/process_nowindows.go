e5ac147402d82b4a390cd7c3ffc19c8e26ca84cc
Sami Tabet <sami.tabet@datadoghq.com>
2018-09-26 18:41:33 -0400

    Avoid detecting integrations on docker containers on linux

--- pkg/autodiscovery/procdiscovery/process_nowindows.go
+++ pkg/autodiscovery/procdiscovery/process_nowindows.go
@@ -3,7 +3,11 @@
 package procdiscovery
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"path"
+	"strconv"
 
 	goproc "github.com/shirou/gopsutil/process"
 )
@@ -23,8 +27,31 @@ func pollProcesses() ([]process, error) {
 		if err != nil {
 			continue
 		}
-		pl = append(pl, process{pid: p.Pid, cmd: cmd})
+
+		// Do not retrieve processes running inside containers
+		if is, err := isContainer(p.Pid); !is && err == nil {
+			pl = append(pl, process{pid: p.Pid, cmd: cmd})
+		}
 	}
 
 	return pl, nil
+}
+
+// Hold cgroup names used by common containers systems
+var cgroupNames = [][]byte{[]byte("docker")}
+
+func isContainer(pid int32) (bool, error) {
+	// Read the /proc/$PID/cgroup file to retrieve cgroups
+	raw, err := ioutil.ReadFile(path.Join("/proc", strconv.Itoa(int(pid)), "cgroup"))
+	if err != nil {
+		return false, fmt.Errorf("error reading cgroup file for process with pid %d", pid)
+	}
+
+	for _, n := range cgroupNames {
+		if bytes.Contains(raw, n) {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
