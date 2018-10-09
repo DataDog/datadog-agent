c42d91216f62738c6083a760232dcb0c2c0f49e4
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2018-10-09 17:24:15 +0200

    Enable add systemd flag to the centos build (#2397)
    ### What does this PR do?
    
    This PR enables systemd build on the default source build of Centos
    
    ### Motivation
    
    We want to enable the journald integration on Centos 7. For this, we need to enable the build flag systemd on Centos
    
    
    ### Note: 
    
    The build succeeded https://gitlab.ddbuild.io/DataDog/datadog-agent/pipelines/715202
    And the binary worked on 
    - Centos 6
    - Centos 7 (with systemd)
    - Amazon Linux 
    - Amazon Linux 2 (with systemd)
    
    
    ![image](https://user-images.githubusercontent.com/6985756/46364108-1aeccd00-c675-11e8-8485-b6fc49ec3131.png)
    It worked on the image built by https://github.com/DataDog/datadog-agent-builders/pull/68

--- tasks/build_tags.py
+++ tasks/build_tags.py
@@ -37,8 +37,15 @@ LINUX_ONLY_TAGS = [
     "kubeapiserver",
 ]
 
-DEBIAN_ONLY_TAGS = [
+REDHAT_AND_DEBIAN_ONLY_TAGS = [
     "systemd",
+]
+
+REDHAT_AND_DEBIAN_DIST = [
+    'debian',
+    'ubuntu',
+    'centos',
+    'redhat'
 ]
 
 
@@ -57,8 +64,8 @@ def get_default_build_tags(puppy=False):
 
     # remove all tags that are only available on debian distributions
     distname = platform.linux_distribution()[0].lower()
-    if distname not in ['debian', 'ubuntu']:
-        exclude = exclude + DEBIAN_ONLY_TAGS
+    if distname not in REDHAT_AND_DEBIAN_DIST:
+        exclude = exclude + REDHAT_AND_DEBIAN_ONLY_TAGS
 
     return get_build_tags(include, exclude)
 
