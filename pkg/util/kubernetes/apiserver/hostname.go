c875f3122e9c3cbbc4e31868fc96e8697e4c9e19
Xavier Vello <xavier.vello@datadoghq.com>
2018-10-10 15:25:37 +0200

    Add kube_apiserver hostname provider for the cluster-agent (#2433)

---
+++ pkg/util/kubernetes/apiserver/hostname.go
@@ -0,0 +1,44 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+// +build kubeapiserver
+
+package apiserver
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/apiserver/common"
+	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/clustername"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+// HostnameProvider retrieves the hostname from the apiserver, assuming our hostname
+// is the pod name. It connects to the apiserver, and returns the node name where
+// our pod is scheduled.
+// Tested in the TestHostnameProvider integration test
+func HostnameProvider(_ string) (string, error) {
+	c, err := GetAPIClient()
+	if err != nil {
+		return "", fmt.Errorf("could not connect to the apiserver: %s", err)
+	}
+	podName, err := os.Hostname()
+	if err != nil {
+		return "", fmt.Errorf("could not fetch our hostname: %s", err)
+	}
+
+	nodeName, err := c.GetNodeForPod(common.GetMyNamespace(), podName)
+	if err != nil {
+		return "", fmt.Errorf("could not fetch the host nodename from the apiserver: %s", err)
+	}
+	clusterName := clustername.GetClusterName()
+	if clusterName == "" {
+		log.Debugf("Now using plain kubernetes nodename as an alias: no cluster name was set and none could be autodiscovered")
+		return nodeName, nil
+	} else {
+		return (nodeName + "-" + clusterName), nil
+	}
+}
