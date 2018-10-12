6b40c0ccb6a415980937702ce53f2deac7b7ff1c
Arthur Bellal <arthur.bellal@datadoghq.com>
2018-10-12 18:25:19 +0200

    add a few coms

--- pkg/util/common.go
+++ pkg/util/common.go
@@ -199,14 +199,19 @@ func CreateHTTPTransport() *http.Transport {
 		tlsConfig.MinVersion = tls.VersionTLS12
 	}
 
+	// Most of the following timeouts are a copy of Golang http.DefaultTransport
+	// They are mostly used to act as safeguards in case we forget to add a general
+	// timeout to our http clients.
 	transport := &http.Transport{
 		TLSClientConfig: tlsConfig,
 		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
+			Timeout: 30 * time.Second,
+			// Enables TCP keepalives to detect broken connections
 			KeepAlive: 30 * time.Second,
 		}).DialContext,
-		MaxIdleConns:          100,
-		MaxIdleConnsPerHost:   5,
+		MaxIdleConns:        100,
+		MaxIdleConnsPerHost: 5,
+		// This parameter is set to avoid connections sitting idle in the pool indefinitely
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
