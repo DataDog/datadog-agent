1e3a942091ca41b1f9ec9ff48b1b1692c295b519
Maxime Mouial <maxime.mouial@gmail.com>
2018-10-16 14:59:06 -0400

    Add check for milestone
    This is mandatory in order to script the testing phase of our release

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -7,7 +7,7 @@ from invoke import Collection
 from . import agent, android, benchmarks, customaction, docker, dogstatsd, pylauncher, cluster_agent, systray, release
 
 from .go import fmt, lint, vet, cyclo, ineffassign, misspell, deps, lint_licenses, reset
-from .test import test, integration_tests, version, lint_teamassignment, lint_releasenote, lint_filenames, e2e_tests
+from .test import test, integration_tests, version, lint_teamassignment, lint_releasenote, lint_milestone, lint_filenames, e2e_tests
 from .build_tags import audit_tag_impact
 
 # the root namespace
@@ -28,6 +28,7 @@ ns.add_task(reset)
 ns.add_task(version)
 ns.add_task(lint_teamassignment)
 ns.add_task(lint_releasenote)
+ns.add_task(lint_milestone)
 ns.add_task(lint_filenames)
 ns.add_task(audit_tag_impact)
 ns.add_task(e2e_tests)
