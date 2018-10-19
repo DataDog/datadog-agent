9b1bb6d4b09515217ab390fd8b239526b51f5175
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2018-10-19 21:30:27 +0200

    Compile the process agent from source within omnibus

--- omnibus/config/software/datadog-process-agent.rb
+++ omnibus/config/software/datadog-process-agent.rb
@@ -3,9 +3,12 @@
 # This product includes software developed at Datadog (https:#www.datadoghq.com/).
 # Copyright 2018 Datadog, Inc.
 
-name "datadog-process-agent"
-always_build true
 require "./lib/ostools.rb"
+require 'pathname'
+
+name "datadog-process-agent"
+
+dependency "datadog-agent"
 
 process_agent_version = ENV['PROCESS_AGENT_VERSION']
 if process_agent_version.nil? || process_agent_version.empty?
@@ -13,21 +16,51 @@ if process_agent_version.nil? || process_agent_version.empty?
 end
 default_version process_agent_version
 
+source git: 'https://github.com/DataDog/datadog-process-agent.git'
+relative_path 'src/github.com/DataDog/datadog-process-agent'
+
+if windows?
+  process_agent_binary = "process-agent.exe"
+else
+  process_agent_binary = "process-agent"
+end
+
 build do
+  ship_license "https://raw.githubusercontent.com/DataDog/datadog-process-agent/#{version}/LICENSE"
+  # set GOPATH on the omnibus source dir for this software
+  gopath = Pathname.new(project_dir) + '../../../..'
   if windows?
-    binary = "process-agent-windows-#{version}.exe"
-    target_binary = "process-agent.exe"
-    url = "https://s3.amazonaws.com/datad0g-process-agent/#{binary}"
-    curl_cmd = "powershell -Command wget -OutFile #{binary} #{url}"
-    command curl_cmd
-    command "mv #{binary} #{install_dir}/bin/agent/#{target_binary}"
+    env = {
+      # Trace agent uses GNU make to build.  Some of the input to gnu make
+      # needs the path with `\` as separators, some needs `/`.  Provide both,
+      # and let the makefile sort it out (ugh)
+
+      # also on windows don't modify the path.  Modifying the path here mixes
+      # `/` with `\` in the PATH variable, which confuses the make (and sub-processes)
+      # below.  When properly configured the path on the windows box is sufficient.
+      'GOPATH' => "#{windows_safe_path(gopath.to_path)}",
+    }
   else
-    binary = "process-agent-amd64-#{version}"
-    target_binary = "process-agent"
-    url = "https://s3.amazonaws.com/datad0g-process-agent/#{binary}"
-    curl_cmd = "curl -f #{url} -o #{binary}"
-    command curl_cmd
-    command "chmod +x #{binary}"
-    command "mv #{binary} #{install_dir}/embedded/bin/#{target_binary}"
+    env = {
+      'GOPATH' => gopath.to_path,
+      'PATH' => "#{gopath.to_path}/bin:#{ENV['PATH']}",
+    }
+  end
+
+  block do
+    # defer compilation step in a block to allow getting the project's build version, which is populated
+    # only once the software that the project takes its version from (i.e. `datadog-agent`) has finished building
+    env['PROCESS_AGENT_VERSION'] = project.build_version.gsub(/[^0-9\.]/, '') # used by gorake.rb in the process-agent, only keep digits and dots
+
+    # build process-agent
+    command "rake deps", :env => env
+    command "rake build", :env => env
+
+    # copy binary
+    if windows?
+      copy "#{project_dir}/#{process_agent_binary}", "#{install_dir}/bin/agent"
+    else
+      copy "#{project_dir}/#{process_agent_binary}", "#{install_dir}/embedded/bin"
+    end
   end
 end
