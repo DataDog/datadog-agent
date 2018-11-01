78ac73863f9556fffca21daba16ce23d4ebeb02a
Bram Schuur <bramschuur@gmail.com>
2018-11-01 12:15:28 +0100

    Patch package info

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -8,17 +8,17 @@ require "./lib/ostools.rb"
 name 'agent'
 package_name 'stackstate-agent'
 
-homepage 'http://www.datadoghq.com'
+homepage 'http://www.stackstate.com'
 
 if ohai['platform'] == "windows"
   # Note: this is the path used by Omnibus to build the agent, the final install
   # dir will be determined by the Windows installer. This path must not contain
   # spaces because Omnibus doesn't quote the Git commands it launches.
   install_dir "C:/opt/stackstate-agent/"
-  maintainer 'Datadog Inc.' # Windows doesn't want our e-mail address :(
+  maintainer 'StackState B.V.' # Windows doesn't want our e-mail address :(
 else
   install_dir '/opt/stackstate-agent'
-  maintainer 'Datadog Packages <package@datadoghq.com>'
+  maintainer 'StackState <info@stackstate.com>'
 end
 
 build_version do
@@ -28,14 +28,14 @@ end
 
 build_iteration 1
 
-description 'Datadog Monitoring Agent
- The Datadog Monitoring Agent is a lightweight process that monitors system
- processes and services, and sends information back to your Datadog account.
+description 'StackState Monitoring Agent
+ The StackState Monitoring Agent is a lightweight process that monitors system
+ processes and services, and sends information back to your StackState account.
  .
  This package installs and runs the advanced Agent daemon, which queues and
  forwards metrics from your applications as well as system services.
  .
- See http://www.datadoghq.com/ for more information
+ See http://www.stackstate.com/ for more information
 '
 
 # ------------------------------------
@@ -53,7 +53,7 @@ end
 
 # .rpm specific flags
 package :rpm do
-  vendor 'StackState <package@stackstate.com>'
+  vendor 'StackState <info@stackstate.com>'
   epoch 1
   dist_tag ''
   license 'Apache License Version 2.0'
