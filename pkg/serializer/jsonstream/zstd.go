3e3d7aa117c82da11824840cf2674569a6cb3139
Xavier Vello <xavier.vello@datadoghq.com>
2018-11-08 18:38:22 +0100

    add chunk type to wrap payload start/end

--- pkg/serializer/jsonstream/zstd.go
+++ pkg/serializer/jsonstream/zstd.go
@@ -8,6 +8,10 @@
 package jsonstream
 
 import (
+	"bytes"
+
+	"github.com/DataDog/zstd"
+
 	"github.com/DataDog/datadog-agent/pkg/forwarder"
 	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
 )
@@ -21,7 +25,48 @@ const (
 var maxPayloadSize = 2 * 1024 * 1024
 var maxUncompressedSize = 40 * 1024 * 1024
 
+type chunk struct {
+	compressed bytes.Buffer
+	writer     *zstd.Writer
+}
+
+func startChunk(header []byte) (*chunk, error) {
+	c := &chunk{}
+	c.writer = zstd.NewWriter(c.compressed)
+	c.writer.Write(header)
+	return c
+}
+
+func (c *chunk) close(footer []byte) ([]byte, error) {
+	_, err := c.writer.Write(fooler)
+	if err != nil {
+		return nil, err
+	}
+	err := c.writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return c.compressed.Bytes(), nil
+}
+
+func (c *chunk) remainingSpace() int {
+	return maxPayloadSize - c.compressed.Len()
+}
+
 // Payloads serializes a metadata payload and sends it to the forwarder
 func Payloads(m marshaler.StreamMarshaler) (forwarder.Payloads, error) {
-	return nil, nil
+	var output forwarder.Payloads
+
+	chunk, err := startChunk(m.MarshalHeaders())
+	if err != nil {
+		return nil, err
+	}
+
+	for item := range m.Items() {
+		var inputBuffer bytes.Buffer
+
+	}
+
+	return output, nil
 }
