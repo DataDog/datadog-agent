c1c8d2c38bf79c2adbc1bc1c9daf9ac7538b366f
Arthur Bellal <arthur.bellal@datadoghq.com>
2018-11-09 14:48:49 +0100

    port splitter test to show our 👏 high 👏 quality 👏 compatibility 👏

--- pkg/serializer/jsonstream/zlib_test.go
+++ pkg/serializer/jsonstream/zlib_test.go
@@ -6,3 +6,79 @@
 //+build zlib
 
 package jsonstream
+
+import (
+	"bytes"
+	"compress/zlib"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/metrics"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSplitPayloadsSeries(t *testing.T) {
+	testSeries := metrics.Series{}
+	for i := 0; i < 30000; i++ {
+		point := metrics.Serie{
+			Points: []metrics.Point{
+				{Ts: 12345.0, Value: float64(21.21)},
+				{Ts: 67890.0, Value: float64(12.12)},
+				{Ts: 2222.0, Value: float64(22.12)},
+				{Ts: 333.0, Value: float64(32.12)},
+				{Ts: 444444.0, Value: float64(42.12)},
+				{Ts: 882787.0, Value: float64(52.12)},
+				{Ts: 99990.0, Value: float64(62.12)},
+				{Ts: 121212.0, Value: float64(72.12)},
+				{Ts: 222227.0, Value: float64(82.12)},
+				{Ts: 808080.0, Value: float64(92.12)},
+				{Ts: 9090.0, Value: float64(13.12)},
+			},
+			MType:    metrics.APIGaugeType,
+			Name:     fmt.Sprintf("test.metrics%d", i),
+			Interval: 1,
+			Host:     "localHost",
+			Tags:     []string{"tag1", "tag2:yes"},
+		}
+		testSeries = append(testSeries, &point)
+	}
+
+	originalLength := len(testSeries)
+	payloads, err := Payloads(testSeries)
+	require.Nil(t, err)
+	var splitSeries = []metrics.Series{}
+	for _, compressedPayload := range payloads {
+		payload, err := decompressPayload(*compressedPayload)
+
+		var s = map[string]metrics.Series{}
+		err = json.Unmarshal(payload, &s)
+		require.Nil(t, err)
+		splitSeries = append(splitSeries, s["series"])
+	}
+
+	unrolledSeries := metrics.Series{}
+	for _, series := range splitSeries {
+		for _, s := range series {
+			unrolledSeries = append(unrolledSeries, s)
+		}
+	}
+
+	newLength := len(unrolledSeries)
+	require.Equal(t, originalLength, newLength)
+}
+
+func decompressPayload(payload []byte) ([]byte, error) {
+	r, err := zlib.NewReader(bytes.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	dst, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	return dst, nil
+}
