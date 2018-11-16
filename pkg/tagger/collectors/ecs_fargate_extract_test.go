6ed5efda54d831c16b2d0bcab0a079ebd7923081
Xavier Vello <xavier.vello@datadoghq.com>
2018-11-16 11:39:46 +0100

    add test case for 1.0 fargate API, ported from integrations-core

--- pkg/tagger/collectors/ecs_fargate_extract_test.go
+++ pkg/tagger/collectors/ecs_fargate_extract_test.go
@@ -117,6 +117,86 @@ func TestParseMetadata(t *testing.T) {
 	assert.Len(t, updates, 3)
 }
 
+func TestParseMetadataV10(t *testing.T) {
+	raw, err := ioutil.ReadFile("./testdata/fargate_meta_v1.0.json")
+	require.NoError(t, err)
+	var meta ecs.TaskMetadata
+	err = json.Unmarshal(raw, &meta)
+	require.NoError(t, err)
+	require.Len(t, meta.Containers, 3)
+
+	collector := &ECSFargateCollector{}
+	collector.expire, err = taggerutil.NewExpire(ecsFargateExpireFreq)
+	require.NoError(t, err)
+
+	expectedUpdates := []*TagInfo{
+		{
+			Source: "ecs_fargate",
+			Entity: "docker://e8d4a9a20a0d931f8f632ec166b3f71a6ff00450aa7e99607f650e586df7d068",
+			LowCardTags: []string{
+				"docker_image:datadog/docker-dd-agent:latest",
+				"image_name:datadog/docker-dd-agent",
+				"short_image:docker-dd-agent",
+				"image_tag:latest",
+				"cluster_name:pierrem-test-fargate",
+				"task_family:redis-datadog",
+				"task_version:1",
+				"ecs_container_name:dd-agent",
+			},
+			HighCardTags: []string{
+				"container_id:e8d4a9a20a0d931f8f632ec166b3f71a6ff00450aa7e99607f650e586df7d068",
+				"container_name:ecs-redis-datadog-1-dd-agent-8085fa82d1d3ada5a601",
+				"task_arn:arn:aws:ecs:eu-west-1:172597598159:task/648ca535-cbe0-4de7-b102-28e50b81e888",
+			},
+			DeleteEntity: false,
+		},
+		{
+			Source: "ecs_fargate",
+			Entity: "docker://c912d0f0f204360ee90ce67c0d083c3514975f149b854f38a48deac611e82e48",
+			LowCardTags: []string{
+				"docker_image:redis:latest",
+				"image_name:redis",
+				"short_image:redis",
+				"image_tag:latest",
+				"cluster_name:pierrem-test-fargate",
+				"task_family:redis-datadog",
+				"task_version:1",
+				"ecs_container_name:redis",
+			},
+			HighCardTags: []string{
+				"container_id:c912d0f0f204360ee90ce67c0d083c3514975f149b854f38a48deac611e82e48",
+				"container_name:ecs-redis-datadog-1-redis-ce99d29f8ce998ed4a00",
+				"task_arn:arn:aws:ecs:eu-west-1:172597598159:task/648ca535-cbe0-4de7-b102-28e50b81e888",
+			},
+			DeleteEntity: false,
+		},
+		{
+			Source: "ecs_fargate",
+			Entity: "docker://39e13ccc425e7777187a603fe33f466a18515030707c4063de1dc1b63d14d411",
+			LowCardTags: []string{
+				"docker_image:amazon/amazon-ecs-pause:0.1.0",
+				"image_name:amazon/amazon-ecs-pause",
+				"short_image:amazon-ecs-pause",
+				"image_tag:0.1.0",
+				"cluster_name:pierrem-test-fargate",
+				"task_family:redis-datadog",
+				"task_version:1",
+				"ecs_container_name:~internal~ecs~pause",
+			},
+			HighCardTags: []string{
+				"container_id:39e13ccc425e7777187a603fe33f466a18515030707c4063de1dc1b63d14d411",
+				"container_name:ecs-redis-datadog-1-internalecspause-a2df9cefc2938ec19e01",
+				"task_arn:arn:aws:ecs:eu-west-1:172597598159:task/648ca535-cbe0-4de7-b102-28e50b81e888",
+			},
+			DeleteEntity: false,
+		},
+	}
+
+	updates, err := collector.parseMetadata(meta, false)
+	assert.NoError(t, err)
+	assertTagInfoListEqual(t, expectedUpdates, updates)
+}
+
 func TestParseExpires(t *testing.T) {
 	collector := &ECSFargateCollector{}
 
