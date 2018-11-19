509e4e57705910e0badbeea0d54486e2de6c681f
Arthur <bellal.arthur@gmail.com>
2018-11-19 11:12:05 +0100

    use netcgo resolver by default on linux (#2538)

--- tasks/build_tags.py
+++ tasks/build_tags.py
@@ -37,6 +37,9 @@ LINUX_ONLY_TAGS = [
     "kubelet",
     "kubeapiserver",
     "cri",
+
+    # Force the use of the CGO resolver. This will also have the effect of making the binary non-static
+    "netcgo",
 ]
 
 REDHAT_AND_DEBIAN_ONLY_TAGS = [
