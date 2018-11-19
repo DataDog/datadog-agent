e1a393b47f44af86bf3f6231572c2db6f1493e0a
Xavier Vello <xavier.vello@datadoghq.com>
2018-11-19 10:56:57 +0100

    disable cluster checks in cluster-agent 1.1.x

--- tasks/cluster_agent.py
+++ tasks/cluster_agent.py
@@ -20,7 +20,7 @@ BIN_PATH = os.path.join(".", "bin", "datadog-cluster-agent")
 AGENT_TAG = "datadog/cluster_agent:master"
 DEFAULT_BUILD_TAGS = [
     "kubeapiserver",
-    "clusterchecks",
+    # "clusterchecks",
 ]
 
 
