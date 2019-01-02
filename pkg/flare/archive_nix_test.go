e46e2bffda5af2c764d0a094f5d3f0213b779ea0
Pierre <mfpierre@gmail.com>
2019-01-02 10:53:21 +0100

    Update new test with mock

--- pkg/flare/archive_nix_test.go
+++ pkg/flare/archive_nix_test.go
@@ -25,8 +25,9 @@ func TestPermsFile(t *testing.T) {
 	assert := assert.New(t)
 
 	common.SetupConfig("./test")
-	config.Datadog.Set("confd_path", "./test/confd")
-	config.Datadog.Set("log_file", "./test/logs/agent.log")
+	mockConfig := config.Mock()
+	mockConfig.Set("confd_path", "./test/confd")
+	mockConfig.Set("log_file", "./test/logs/agent.log")
 	zipFilePath := getArchivePath()
 	filePath, err := createArchive(zipFilePath, true, SearchPaths{}, "")
 	defer os.Remove(zipFilePath)
