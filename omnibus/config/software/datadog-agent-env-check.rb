8259aa83e4bd20aa1a89b3afdce785c2f26a1118
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2019-01-02 18:56:01 +0100

    Run pip check during the omnibus build to ensure a clean env (#2830)

---
+++ omnibus/config/software/datadog-agent-env-check.rb
@@ -0,0 +1,13 @@
+name 'datadog-agent-env-check'
+
+description "Execute pip check on the python environment of the agent to make sure everything is compatible"
+
+# Run the check after all the definitions touching the python environment of the agent.
+dependency "datadog-pip"
+dependency "datadog-agent"
+dependency "datadog-agent-integrations"
+
+build do
+    # Run pip check to make sure the agent's python environment is clean, all the dependencies are compatible
+    pip "check"
+end
