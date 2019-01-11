1ef3c7a38b9fae0f08a05d7a228b521fa83a71ef
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-01-11 15:16:53 -0500

    Remove beta notice on integration command

--- cmd/agent/app/integrations.go
+++ cmd/agent/app/integrations.go
@@ -124,7 +124,7 @@ func init() {
 
 var tufCmd = &cobra.Command{
 	Use:   "integration [command]",
-	Short: "Datadog integration manager (Beta feature)",
+	Short: "Datadog integration manager",
 	Long:  ``,
 }
 
