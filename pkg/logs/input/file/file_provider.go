ee726983d614353d66410bf095d54b7c1a71c3fc
Alexandre Chaintreuil <alexandre.chaintreuil@datadoghq.com>
2019-01-18 11:43:41 +0100

    Update comments

--- pkg/logs/input/file/file_provider.go
+++ pkg/logs/input/file/file_provider.go
@@ -52,7 +52,7 @@ func NewProvider(filesLimit int) *Provider {
 // FilesToTail returns all the Files matching paths in sources,
 // it cannot return more than filesLimit Files.
 // For now, there is no way to prioritize specific Files over others,
-// they are just returned in alphabetical order
+// they are just returned in reverse lexicographical order, see `searchFiles`
 func (p *Provider) FilesToTail(sources []*config.LogSource) []*File {
 	var filesToTail []*File
 	shouldLogErrors := p.shouldLogErrors
@@ -137,6 +137,7 @@ func (p *Provider) searchFiles(pattern string, source *config.LogSource) ([]*Fil
 	// Here, we reverse paths to have stable sort keep reverse lexicographical order w.r.t dir names. Example:
 	// [/tmp/1/2017.log, /tmp/1/2018.log, /tmp/2/2018.log] becomes [/tmp/2/2018.log, /tmp/1/2018.log, /tmp/1/2017.log]
 	// then kept as is by the sort below.
+	// https://github.com/golang/go/wiki/SliceTricks#reversing
 	for i := len(paths)/2 - 1; i >= 0; i-- {
 		opp := len(paths) - 1 - i
 		paths[i], paths[opp] = paths[opp], paths[i]
