1520f9f7d9c0519ba963fd45ed342519f1ddc0c1
Massimiliano Pippi <mpippi@gmail.com>
2019-01-28 14:16:11 +0100

    test extending for Python2

--- test/two/two.go
+++ test/two/two.go
@@ -3,6 +3,11 @@ package two
 // #cgo CFLAGS: -I../../include
 // #cgo LDFLAGS: -L../../six/ -ldatadog-agent-six -ldl
 // #include <datadog_agent_six.h>
+//
+// extern six_pyobject_t *printFoo();
+// static void goSetupMyModule(six_t *six) {
+//     add_module_func_noargs(six, "my_module", "print_foo", printFoo);
+// }
 import "C"
 
 import (
@@ -10,6 +15,8 @@ import (
 
 	common "../common"
 )
+
+var six *C.six_t
 
 func get2() *C.six_t {
 	six := C.make2()
@@ -56,5 +63,46 @@ func runString(code string) (string, error) {
 		return "", fmt.Errorf("`run_simple_string` errored")
 	}
 
+	return string(output), err
+}
+
+//export printFoo
+func printFoo() *C.six_pyobject_t {
+	fmt.Println("I'm extending Python!")
+	return C.get_none(six)
+}
+
+func extend() (string, error) {
+	var err error
+	six = C.make2()
+
+	// cb := printFoo
+	C.goSetupMyModule(six)
+	// C.add_module_func_noargs(six, C.CString("my_module"), C.CString("print_foo"), C.goPrintFoo)
+	C.init(six, nil)
+
+	code := C.CString(`
+try:
+	import my_module
+	my_module.print_foo()
+except Exception as e:
+	print(e)
+`)
+	var ret bool
+	var output []byte
+	output, err = common.Capture(func() {
+		ret = C.run_simple_string(six, code) == 0
+	})
+
+	if err != nil {
+		C.destroy2(six)
+		return "", err
+	}
+
+	if !ret {
+		return "", fmt.Errorf("`run_simple_string` errored")
+	}
+
+	C.destroy2(six)
 	return string(output), err
 }
