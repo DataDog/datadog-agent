cbda0aa79c1a2f272f16fe30cee936746d83e386
Massimiliano Pippi <mpippi@gmail.com>
2019-02-04 10:18:34 +0100

    added make clang-format target

---
+++ cmake/clang-format.cmake
@@ -0,0 +1,21 @@
+# Get all project files
+file(GLOB_RECURSE
+     ALL_SOURCE_FILES
+     *.[ch]pp *.[ch]xx *.cc *.h
+    )
+
+# Adding clang-format target if executable is found
+find_program(
+    CLANG_FORMAT
+    NAMES "clang-format"
+    PATHS "/usr/local/bin"
+    )
+if(CLANG_FORMAT)
+  add_custom_target(
+    clang-format
+    COMMAND ${CLANG_FORMAT}
+    -i
+    -style=file
+    ${ALL_SOURCE_FILES}
+    )
+endif()
