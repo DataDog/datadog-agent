111ed9c0296fc44f26d15c0eec4ffb2349787af6
Ofek Lev <ofek.lev@datadoghq.com>
2019-02-08 11:15:16 -0500

    Update datadog-agent-integrations.rb

--- omnibus/config/software/datadog-agent-integrations.rb
+++ omnibus/config/software/datadog-agent-integrations.rb
@@ -42,13 +42,16 @@ blacklist = [
   'datadog_checks_base',           # namespacing package for wheels (NOT AN INTEGRATION)
   'datadog_checks_dev',            # Development package, (NOT AN INTEGRATION)
   'datadog_checks_tests_helper',   # Testing and Development package, (NOT AN INTEGRATION)
-  'aerospike',                     # Temporarily blacklist Aerospike until builder supports new dependency
   'agent_metrics',
   'docker_daemon',
   'kubernetes',
   'ntp',                           # provided as a go check by the core agent
   'openstack_controller',          # Check currently under active development and in beta
 ]
+
+if suse?
+  blacklist.push('aerospike')  # Temporarily blacklist Aerospike until builder supports new dependency
+end
 
 core_constraints_file = 'core_constraints.txt'
 agent_requirements_file = 'agent_requirements.txt'
