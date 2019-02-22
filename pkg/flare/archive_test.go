4686ab15a11aa2e1d3906ee2e11748c1c5062f5b
Arthur Bellal <arthur.bellal@datadoghq.com>
2019-02-22 13:57:11 +0100

    ensure flare directory name is valid

--- pkg/flare/archive_test.go
+++ pkg/flare/archive_test.go
@@ -179,3 +179,38 @@ func TestIncludeConfigFiles(t *testing.T) {
 	assert.True(yml, "test.yml should've been included")
 	assert.True(yaml, "test.yaml should've been included")
 }
+
+func TestCleanDirectoryName(t *testing.T) {
+	insaneHostname := `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+	<title>404 - File or directory not found.</title>
+	<style type="text/css">
+	<!--
+	body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}
+	fieldset{padding:0 15px 10px 15px;}
+	h1{font-size:2.4em;margin:0;color:#FFF;}
+	h2{font-size:1.7em;margin:0;color:#CC0000;}
+	h3{font-size:1.2em;margin:10px 0 0 0;color:#000000;}
+	background-color:#555555;}
+	.content-container{background:#FFF;width:96%;margin-top:8px;padding:10px;position:relative;}
+	-->
+	</style>
+	</head>
+	<body>
+	<div id="header"><h1>Server Error</h1></div>
+	<div id="content">
+	<div class="content-container"><fieldset>
+	<h2>404 - File or directory not found.</h2>
+	<h3>The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h3>
+	</fieldset></div>
+	</div>
+	</body>
+	</html>`
+
+	cleanedHostname := cleanDirectoryName(insaneHostname)
+
+	assert.Len(t, cleanedHostname, directoryNameMaxSize)
+	assert.True(t, !directoryNameFilter.MatchString(cleanedHostname))
+}
