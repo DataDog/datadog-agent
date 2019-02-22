3ebd27e6d2d92d0ab3e88b03876ba5b98d3eeb19
Gabriel Aszalos <gabriel.aszalos@datadoghq.com>
2019-02-22 10:15:35 +0100

    pkg/trace/sampler: add regression test for catalog race
    This change adds a test for a race that occurred as a regression in
    6.9.0 where the receiver of (*serviceKeyCatalog).ratesByService was of
    value type (not pointer).
    
    It also fixes wrong ordering in a mutex.

--- pkg/trace/sampler/catalog.go
+++ pkg/trace/sampler/catalog.go
@@ -30,8 +30,8 @@ func (cat *serviceKeyCatalog) register(svcSig ServiceSignature) Signature {
 // the signatures.
 func (cat *serviceKeyCatalog) ratesByService(rates map[Signature]float64, totalScore float64) map[ServiceSignature]float64 {
 	rbs := make(map[ServiceSignature]float64, len(rates)+1)
-	defer cat.mu.Unlock()
 	cat.mu.Lock()
+	defer cat.mu.Unlock()
 	for key, sig := range cat.lookup {
 		if rate, ok := rates[sig]; ok {
 			rbs[key] = rate
