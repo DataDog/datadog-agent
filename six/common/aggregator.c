10a922de88a954ce38324a2b08b34482f5157df2
Massimiliano Pippi <mpippi@gmail.com>
2019-02-26 09:23:57 +0100

    submit_service_check builtin

--- six/common/aggregator.c
+++ six/common/aggregator.c
@@ -11,6 +11,7 @@
 
 // these must be set by the Agent
 static cb_submit_metric_t cb_submit_metric = NULL;
+static cb_submit_service_check_t cb_submit_service_check = NULL;
 
 // forward declarations
 static PyObject *submit_metric(PyObject *self, PyObject *args);
@@ -18,14 +19,10 @@ static PyObject *submit_service_check(PyObject *self, PyObject *args);
 static PyObject *submit_event(PyObject *self, PyObject *args);
 void add_constants(PyObject *);
 
-// module object storage (Python2)
-static PyObject *module;
-
 static PyMethodDef methods[] = {
-    { "submit_metric", (PyCFunction)submit_metric, METH_VARARGS, "Submit metrics to the aggregator." },
-    { "submit_service_check", (PyCFunction)submit_service_check, METH_VARARGS,
-      "Submit service checks to the aggregator." },
-    { "submit_event", (PyCFunction)submit_event, METH_VARARGS, "Submit events to the aggregator." },
+    { "submit_metric", (PyCFunction)submit_metric, METH_VARARGS, "Submit metrics." },
+    { "submit_service_check", (PyCFunction)submit_service_check, METH_VARARGS, "Submit service checks." },
+    { "submit_event", (PyCFunction)submit_event, METH_VARARGS, "Submit events." },
     { NULL, NULL } // guards
 };
 
@@ -40,6 +37,9 @@ PyMODINIT_FUNC PyInit_aggregator(void) {
 #endif
 
 #ifdef DATADOG_AGENT_TWO
+// module object storage
+static PyObject *module;
+
 void Py2_init_aggregator() {
     module = Py_InitModule(MODULE_NAME, methods);
     add_constants(module);
@@ -60,6 +60,10 @@ void _set_submit_metric_cb(cb_submit_metric_t cb) {
     cb_submit_metric = cb;
 }
 
+void _set_submit_service_check_cb(cb_submit_service_check_t cb) {
+    cb_submit_service_check = cb;
+}
+
 static PyObject *submit_metric(PyObject *self, PyObject *args) {
     // callback must be set
     assert(cb_submit_metric != NULL);
@@ -75,54 +79,77 @@ static PyObject *submit_metric(PyObject *self, PyObject *args) {
     int mt;
     float value;
 
-    PyGILState_STATE gstate;
-    gstate = PyGILState_Ensure();
-
     // Python call: aggregator.submit_metric(self, check_id, aggregator.metric_type.GAUGE, name, value, tags, hostname)
     if (!PyArg_ParseTuple(args, "OsisfOs", &check, &check_id, &mt, &name, &value, &py_tags, &hostname)) {
         goto done;
     }
 
-    // convert tags to an array of char*
-    Py_ssize_t len = 0;
-    if (py_tags != NULL && PySequence_Check(py_tags)) {
-        len = PySequence_Length(py_tags);
-        if (len) {
-            py_tags_list = PySequence_Fast(py_tags, err);
-            if (py_tags_list == NULL) {
-                goto done;
-            }
+    int len = PySequence_Length(py_tags);
+    if (len) {
+        py_tags_list = PySequence_Fast(py_tags, err); // new reference
+        if (py_tags_list == NULL) {
+            goto done;
+        }
 
-            tags = malloc(len * sizeof(char *));
-
-            for (int i = 0; i < len; i++) {
-                PyObject *item = PySequence_Fast_GET_ITEM(py_tags_list, i); // `item` is borrowed, no need to decref
-                char *str = as_string(item);
-                // skip if not a string
-                if (str == NULL) {
-                    continue;
-                }
-                tags[i] = str;
-            }
+        tags = malloc(len * sizeof(char *));
+        for (int i = 0; i < len; i++) {
+            // `item` is borrowed, no need to decref
+            PyObject *item = PySequence_Fast_GET_ITEM(py_tags_list, i);
+            tags[i] = as_string(item);
         }
     }
 
     cb_submit_metric(check_id, mt, name, value, tags, len, hostname);
 
 done:
-    if (err != NULL) {
-        free(err);
-    }
+    free(err);
     Py_XDECREF(py_tags_list);
-    PyGILState_Release(gstate);
-
     return Py_None;
 }
 
 static PyObject *submit_service_check(PyObject *self, PyObject *args) {
-    /*FIXME*/
-    return NULL;
+    // callback must be set
+    assert(cb_submit_service_check != NULL);
+
+    PyObject *check = NULL; // borrowed
+    PyObject *py_tags = NULL; // borrowed
+    PyObject *py_tags_list = NULL; // new reference
+    char *err = NULL;
+    char *name = NULL;
+    int status;
+    char *hostname = NULL;
+    char *message = NULL;
+    char *check_id = NULL;
+    char **tags = NULL;
+
+    // aggregator.submit_service_check(self, check_id, name, status, tags, hostname, message)
+    if (!PyArg_ParseTuple(args, "OssiOss", &check, &check_id, &name, &status, &py_tags, &hostname, &message)) {
+        goto done;
+    }
+
+    int len = PySequence_Length(py_tags);
+    if (len) {
+        py_tags_list = PySequence_Fast(py_tags, err); // new reference
+        if (py_tags_list == NULL) {
+            goto done;
+        }
+
+        tags = malloc(len * sizeof(char *));
+        for (int i = 0; i < len; i++) {
+            // `item` is borrowed, no need to decref
+            PyObject *item = PySequence_Fast_GET_ITEM(py_tags_list, i);
+            tags[i] = as_string(item);
+        }
+    }
+
+    cb_submit_service_check(check_id, name, status, tags, len, hostname, message);
+
+done:
+    free(err);
+    Py_XDECREF(py_tags_list);
+    return Py_None;
 }
+
 static PyObject *submit_event(PyObject *self, PyObject *args) {
     /*FIXME*/
     return NULL;
