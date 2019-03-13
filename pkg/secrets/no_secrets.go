f8219dc3c4fb5554d5e9600fedf091e87220cca7
Derek Brown <derek.brown@datadoghq.com>
2019-03-13 08:31:20 -0700

    fix linting

--- pkg/secrets/no_secrets.go
+++ pkg/secrets/no_secrets.go
@@ -23,5 +23,5 @@ func Decrypt(data []byte, origin string) ([]byte, error) {
 
 // GetDebugInfo exposes debug informations about secrets to be included in a flare
 func GetDebugInfo() (*SecretInfo, error) {
-	return nil, fmt.Errorf("Secret feature is not available in this version of the agent.")
+	return nil, fmt.Errorf("Secret feature is not available in this version of the agent")
 }
