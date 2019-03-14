b08c653a603f103439f73dafb9987a83f377ea84
Gaetan Deputier <48209202+gaetan-deputier@users.noreply.github.com>
2019-03-14 17:02:43 +0100

    [Logs]: Update sender config to ignore empty logs_dd_url configuration coming from agent migrations (#3149)
    * [Logs]: Update sender config to ignore empty logs_dd_url configuration
    coming from agent migrations

--- pkg/logs/sender/config_test.go
+++ pkg/logs/sender/config_test.go
@@ -128,6 +128,23 @@ func (suite *ConfigTestSuite) TestBuildEndpointsShouldFailWithInvalidOverride() 
 	}
 }
 
+//When migrating the agent v5 to v6, logs_dd_url is set to empty. Default to the dd_url/site already set instead.
+func (suite *ConfigTestSuite) TestBuildEndpointsShouldSucceedWhenMigratingToAgentV6() {
+	suite.config.Set("logs_config.logs_dd_url", "")
+	endpoints, err := BuildEndpoints()
+	suite.Nil(err)
+	suite.Equal("agent-intake.logs.datadoghq.com", endpoints.Main.Host)
+	suite.Equal(10516, endpoints.Main.Port)
+}
+
+func (suite *ConfigTestSuite) TestIsSetAndNotEmpty() {
+	suite.config.Set("bob", "vanilla")
+	suite.config.Set("empty", "")
+	suite.True(isSetAndNotEmpty(suite.config, "bob"))
+	suite.False(isSetAndNotEmpty(suite.config, "empty"))
+	suite.False(isSetAndNotEmpty(suite.config, "wassup"))
+}
+
 func TestConfigTestSuite(t *testing.T) {
 	suite.Run(t, new(ConfigTestSuite))
 }
