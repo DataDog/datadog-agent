457374f21c32f93a9c465135344538236017b89e
Derek Brown <derek.brown@datadoghq.com>
2019-04-17 09:34:11 -0700

    Build six package on windows
    Builds windows package, py2 at least working
    
    fix appveyor (maybe)
    fix upgrade (moved agent binary dir)
    
    Remove redundant file
    
    Add creation of bin/agent
    
    Fix build
    
    Change default extract to seven_zip
    
    Fix agent path in kitchen tests

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -14,6 +14,8 @@ if ohai['platform'] == "windows"
   # dir will be determined by the Windows installer. This path must not contain
   # spaces because Omnibus doesn't quote the Git commands it launches.
   install_dir "C:/opt/datadog-agent/"
+  python_2_embedded "#{install_dir}/embedded2"
+  python_3_embedded "#{install_dir}/embedded3"
   maintainer 'Datadog Inc.' # Windows doesn't want our e-mail address :(
 else
   install_dir '/opt/datadog-agent'
@@ -116,7 +118,12 @@ dependency 'datadog-agent-prepare'
 dependency 'datadog-agent'
 
 # Additional software
-dependency 'cacerts'
+if windows?
+  dependency 'cacerts_py2'
+  dependency 'cacerts_py3'
+else
+  dependency 'cacerts'
+end
 dependency 'datadog-a7'
 dependency 'datadog-agent-integrations'
 if osx?
