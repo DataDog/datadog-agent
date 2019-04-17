6f3fd6eac71e99c8a2e483bef865b12359154d89
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2019-04-17 11:43:31 +0200

    shorten if statement

--- pkg/util/docker/containers.go
+++ pkg/util/docker/containers.go
@@ -189,7 +189,7 @@ func parseContainerHealth(status string) string {
 func parseContainerNetworkAddresses(ports []types.Port, netSettings *types.SummaryNetworkSettings, container string) []containers.NetworkAddress {
 	addrList := []containers.NetworkAddress{}
 	tempAddrList := []containers.NetworkAddress{}
-	if netSettings == nil || netSettings.Networks == nil || len(netSettings.Networks) == 0 {
+	if netSettings == nil || len(netSettings.Networks) == 0 {
 		log.Debugf("No network settings available from docker")
 		return addrList
 	}
