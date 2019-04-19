ea8d820f66468e3639853f2f354b5755ee3720a7
Maxime Mouial <maxime.mouial@gmail.com>
2019-04-19 14:42:07 -0600

    Remove 'GetPythonInterpreterMemoryUsage' from the 'python' package
    Since Pympler segfautl under Python3, memory usage is only available
    when compile with the 'cpython' build flag (until we found a replacement
    for Pympler).

---
+++ cmd/agent/api/agent/python_status.go
@@ -0,0 +1,23 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+// Package agent implements the api endpoints for the `/agent` prefix.
+// This group of endpoints is meant to provide high-level functionalities
+// at the agent level.
+
+// +build !cpython
+
+package agent
+
+import (
+	"net/http"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+func getPythonStatus(w http.ResponseWriter, r *http.Request) {
+	log.Warnf("Error: python stats is only available when compiled with cpython\n")
+	w.Write([]byte("{}")) // return empty json
+}
