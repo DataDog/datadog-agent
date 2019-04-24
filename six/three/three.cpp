51309a40f47ac1a2d334a92a093ff57777edd946
maxime mouial <hush-hush@users.noreply.github.com>
2019-04-24 04:45:42 -0600

    Check malloc return value (#3359)

--- six/three/three.cpp
+++ six/three/three.cpp
@@ -324,6 +324,12 @@ char **Three::getCheckWarnings(SixPyObject *check)
 
     Py_ssize_t numWarnings = PyList_Size(warns_list);
     char **warnings = (char **)malloc(sizeof(*warnings) * (numWarnings + 1));
+    if (!warnings) {
+        Py_XDECREF(warns_list);
+        setError("could not allocate memory to get warnings: ");
+        return NULL;
+    }
+
     warnings[numWarnings] = NULL;
 
     for (Py_ssize_t idx = 0; idx < numWarnings; idx++) {
