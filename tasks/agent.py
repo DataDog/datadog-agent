320717e82d97a8246cbe9f3f9f7b3d4eedd423ae
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2019-04-29 16:07:10 +0200

    Make sha1 length 8 instead of 7

--- tasks/agent.py
+++ tasks/agent.py
@@ -413,7 +413,7 @@ def clean(ctx):
 
 
 @task
-def version(ctx, url_safe=False, git_sha_length=7):
+def version(ctx, url_safe=False, git_sha_length=8):
     """
     Get the agent version.
     url_safe: get the version that is able to be addressed as a url
