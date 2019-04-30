5691b7caf2665de408831ec146bc18e34d09071f
Albert Vaca <albert.vaca@datadoghq.com>
2019-04-30 15:25:10 +0200

    Added keys from datadog-process-agent

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -390,7 +390,8 @@ func initConfig(config Config) {
 	config.BindEnvAndSetDefault("cluster_checks.cluster_tag_name", "cluster_name")
 	config.BindEnvAndSetDefault("cluster_checks.extra_tags", []string{})
 
-	//Declare other keys that don't have a default/env var
+	// Declare other keys that don't have a default/env var.
+	// Mostly, keys we use IsSet() on, because IsSet always returns true if a key has a default.
 	config.SetKnown("metadata_providers")
 	config.SetKnown("config_providers")
 	config.SetKnown("proxy.http")
@@ -406,7 +407,6 @@ func initConfig(config Config) {
 	config.SetKnown("process_config.intervals.container")
 	config.SetKnown("process_config.intervals.container_realtime")
 	config.SetKnown("process_config.dd_agent_bin")
-	config.SetKnown("process_config.max_proc_fds")
 	config.SetKnown("apm_config.receiver_port")
 	config.SetKnown("apm_config.env")
 	config.SetKnown("apm_config.apm_non_local_traffic")
@@ -415,6 +415,13 @@ func initConfig(config Config) {
 	config.SetKnown("apm_config.max_traces_per_second")
 	config.SetKnown("clustername")
 	config.SetKnown("listeners")
+	config.SetKnown("process.strip_proc_arguments")
+	config.SetKnown("process_config.windows.args_refresh_interval")
+	config.SetKnown("process_config.windows.add_new_args")
+	config.SetKnown("process.additional_endpoints")
+	config.SetKnown("additional_endpoints")
+	config.SetKnown("process.container_source")
+	config.SetKnown("process.intervals.connections")
 
 	setAssetFs(config)
 }
