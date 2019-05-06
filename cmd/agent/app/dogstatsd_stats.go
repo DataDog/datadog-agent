ef5ccc8693d7089002cfa22a4f7b49aa504ed798
Rémy Mathieu <remy.mathieu@datadoghq.com>
2019-05-06 14:33:42 +0200

    cmd/dogstatsd-stats: ask confirmation before overwriting the output file (#3302)
    * cmd/dogstatsd-stats: ask confirmation before overwriting the output file.
    * Add release note.
    * cmd/dogstatsd-stats: early return if not writing the output in a file.

--- cmd/agent/app/dogstatsd_stats.go
+++ cmd/agent/app/dogstatsd_stats.go
@@ -10,11 +10,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/DataDog/datadog-agent/cmd/agent/common"
 	"github.com/DataDog/datadog-agent/pkg/api/util"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/dogstatsd"
+	"github.com/DataDog/datadog-agent/pkg/util/input"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -94,14 +96,23 @@ func requestDogstatsdStats() error {
 		}
 	}
 
-	if dsdStatsFilePath != "" {
-		if err := ioutil.WriteFile(dsdStatsFilePath, []byte(s), 0644); err != nil {
-			fmt.Println("Error while writing the output:", err)
-		} else {
-			fmt.Println("Dogstatsd stats written in:", dsdStatsFilePath)
-		}
-	} else {
+	if dsdStatsFilePath == "" {
 		fmt.Println(s)
+		return nil
+	}
+
+	// if the file is already existing, ask for a confirmation.
+	if _, err := os.Stat(dsdStatsFilePath); err == nil {
+		if !input.AskForConfirmation(fmt.Sprintf("'%s' existing, do you wan't to overwrite it? [Y/N]", dsdStatsFilePath)) {
+			fmt.Println("Canceling.")
+			return nil
+		}
+	}
+
+	if err := ioutil.WriteFile(dsdStatsFilePath, []byte(s), 0644); err != nil {
+		fmt.Println("Error while writing the file (is the location writable by the dd-agent user?):", err)
+	} else {
+		fmt.Println("Dogstatsd stats written in:", dsdStatsFilePath)
 	}
 
 	return nil
