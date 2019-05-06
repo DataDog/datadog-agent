f199e3198aa59657f578f968542a355043689bab
Maxime Mouial <maxime.mouial@gmail.com>
2019-05-06 17:11:34 -0600

    TEST

--- omnibus/config/software/datadog-agent-six.rb
+++ omnibus/config/software/datadog-agent-six.rb
@@ -17,6 +17,7 @@ if ohai["platform"] != "windows"
         "Python3_ROOT_DIR" => "#{install_dir}/embedded",
         "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
     }
+    env = with_embedded_path(env)
 
     command "inv -e six.build --install-prefix \"#{install_dir}/embedded\" --cmake-options '-DCMAKE_CXX_FLAGS:=\"-D_GLIBCXX_USE_CXX11_ABI=0\" -DCMAKE_FIND_FRAMEWORK:STRING=NEVER'", :env => env
     command "inv -e six.install"
@@ -28,6 +29,7 @@ else
         "Python3_ROOT_DIR" => "#{windows_safe_path(python_3_embedded)}",
         "CMAKE_INSTALL_PREFIX" => "#{windows_safe_path(python_2_embedded)}"
     }
+    env = with_embedded_path(env)
 
     command "inv -e six.build --install-prefix \"#{windows_safe_path(python_2_embedded)}\" --cmake-options '-G \"Unix Makefiles\"", :env => env
     command "mv bin/*.dll  #{Omnibus::Config.source_dir()}/datadog-agent/src/github.com/DataDog/datadog-agent/bin/agent/"
