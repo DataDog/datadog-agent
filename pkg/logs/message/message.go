410273efec81793b66651764ceb607f59ba86311
Ting Tu <ting.tu@datadoghq.com>
2019-05-16 16:29:39 +0200

    Fix formating issue

--- pkg/logs/message/message.go
+++ pkg/logs/message/message.go
@@ -43,6 +43,7 @@ func New(content []byte, origin *Origin, status string, timestamp string, rawDat
 		status: status,
 	}
 }
+
 // GetStatus gets the status of the message if status is not set, StatusInfo will be returned.
 func (m *Message) GetStatus() string {
 	if m.status == "" {
