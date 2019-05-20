a646c5f1d91bfe2cb5f7962a6a85c1a641d66c9e
Derek Brown <derek.brown@datadoghq.com>
2019-05-20 01:10:11 -0700

    Fixes assumption that group name for "Performance Monitor Users" is (#3495)
    always in English; look up based on the well known SID

--- omnibus/resources/agent/msi/cal/userrights.cpp
+++ omnibus/resources/agent/msi/cal/userrights.cpp
@@ -51,6 +51,56 @@ cleanAndFail:
 	}
 	return NULL;
 }
+
+bool GetNameForSid(LPCWSTR host, PSID sid, std::wstring& namestr) 
+{
+    wchar_t * name = NULL;
+    DWORD cchName = 0;
+    LPWSTR refDomain = NULL;
+    DWORD cchRefDomain = 0;
+    SID_NAME_USE use;
+    std::string narrowdomain;
+    BOOL success = false;
+    BOOL bRet = LookupAccountSid(host, sid, name, &cchName, refDomain, &cchRefDomain, &use);
+    if (bRet) {
+        // this should *never* happen, because we didn't pass in a buffer large enough for
+        // the sid or the domain name.
+        WcaLog(LOGMSG_STANDARD, "Unexpected success looking up account sid");
+        return false;
+    }
+    DWORD err = GetLastError();
+    if (ERROR_INSUFFICIENT_BUFFER != err) {
+        WcaLog(LOGMSG_STANDARD, "Unexpected failure looking up account sid %d", err);
+        // we don't know what happened
+        return false;
+    }
+    name = (wchar_t*) new wchar_t[cchName];
+    ZeroMemory(name, cchName * sizeof(wchar_t));
+
+    refDomain = new wchar_t[cchRefDomain + 1];
+    ZeroMemory(refDomain, (cchRefDomain + 1) * sizeof(wchar_t));
+
+    // try it again
+    bRet = LookupAccountSid(host, sid, name, &cchName, refDomain, &cchRefDomain, &use);
+    if (!bRet) {
+        WcaLog(LOGMSG_STANDARD, "Failed to lookup account name %d", GetLastError());
+        goto cleanAndDone;
+    }
+    success = true;
+    toMbcs(narrowdomain, refDomain);
+    WcaLog(LOGMSG_STANDARD, "Got account from sid from %s\n", narrowdomain.c_str());
+    namestr = name;
+
+cleanAndDone:
+    if (name) {
+        delete[](wchar_t*)name;
+    }
+    if (refDomain) {
+        delete[] refDomain;
+    }
+    return success;
+}
+
 bool RemovePrivileges(PSID AccountSID, LSA_HANDLE PolicyHandle, LPCWSTR rightToAdd)
 {
 	LSA_UNICODE_STRING lucPrivilege;
