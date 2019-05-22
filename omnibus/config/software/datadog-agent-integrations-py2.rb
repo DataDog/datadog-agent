d7e0d703a48bf9366b301cc94e49a9fbc1c5c6af
Massimiliano Pippi <mpippi@gmail.com>
2019-05-22 14:20:29 +0200

    windows path definition

--- omnibus/config/software/datadog-agent-integrations-py2.rb
+++ omnibus/config/software/datadog-agent-integrations-py2.rb
@@ -245,7 +245,7 @@ build do
 
     # Patch applies to only one file: set it explicitly as a target, no need for -p
     if windows?
-      patch :source => "create-regex-at-runtime.patch", :target => "#{windows_safe_path(python_2_embedded)}\\Lib\\site-packages\\yaml\\reader.py"
+      patch :source => "create-regex-at-runtime.patch", :target => "#{windows_safe_path(python_2_embedded)}/Lib/site-packages/yaml/reader.py"
     else
       patch :source => "create-regex-at-runtime.patch", :target => "#{install_dir}/embedded/lib/python2.7/site-packages/yaml/reader.py"
     end
