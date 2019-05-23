72efe63d37f5af6f261ddf21185e950ce98bd008
Ting Tu <ting.tu@datadoghq.com>
2019-05-23 12:08:14 +0200

    Fix bug last multi-line log not being collected

--- pkg/logs/decoder/decoder.go
+++ pkg/logs/decoder/decoder.go
@@ -7,7 +7,6 @@ package decoder
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/DataDog/datadog-agent/pkg/logs/config"
 	"github.com/DataDog/datadog-agent/pkg/logs/parser"
 )
@@ -132,6 +131,5 @@ func (d *Decoder) sendLine() {
 	content := make([]byte, d.lineBuffer.Len())
 	copy(content, d.lineBuffer.Bytes())
 	d.lineBuffer.Reset()
-	fmt.Printf("sendLine from decoder: %v\n", string(content))
 	d.lineHandler.Handle(content)
 }
