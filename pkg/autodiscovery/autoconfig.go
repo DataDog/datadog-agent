3ee40997414b726e7935f9de52ae019c61d32c9c
Pierre Margueritte <mfpierre@gmail.com>
2019-05-31 11:52:03 +0200

    Fix configcheck crash when agent not ready (#3602)

--- pkg/autodiscovery/autoconfig.go
+++ pkg/autodiscovery/autoconfig.go
@@ -541,6 +541,10 @@ func (ac *AutoConfig) resolveTemplateForService(tpl integration.Config, svc list
 
 // GetLoadedConfigs returns configs loaded
 func (ac *AutoConfig) GetLoadedConfigs() map[string]integration.Config {
+	if ac.store == nil {
+		log.Error("Autoconfig store not initialized")
+		return map[string]integration.Config{}
+	}
 	return ac.store.getLoadedConfigs()
 }
 
