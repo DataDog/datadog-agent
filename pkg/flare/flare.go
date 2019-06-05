776a3b3bca88b908083189af4893b50ff626d5b5
Albert Vaca <albert.vaca@datadoghq.com>
2019-06-05 19:40:49 +0200

    Handle redirects when uploading a flare (#3632)
    * Handle redirects when uploading a flare
    
    * Refactor to remove goto
    
    * For syntax

--- pkg/flare/flare.go
+++ pkg/flare/flare.go
@@ -31,6 +31,26 @@ type flareResponse struct {
 
 // SendFlareWithHostname sends a flare with a set hostname
 func SendFlareWithHostname(archivePath string, caseID string, email string, hostname string) (string, error) {
+	r, err := readAndPostFlareFileWithRedirects(archivePath, caseID, email, hostname)
+	return analyzeResponse(r, err)
+}
+
+func readAndPostFlareFileWithRedirects(archivePath string, caseID string, email string, hostname string) (*http.Response, error) {
+	// Handle redirects manually. Go's http.Client doesn't know how to do it when it can't seek
+	// back to the beginning of the body. Since we are using a pipe, seeking isn't possible.
+	// Re-sending a POST is only legal for status 307, so we only need to check for that code.
+	var url = mkURL(caseID)
+	for redirectHops := 0; ; redirectHops++ {
+		r, err := readAndPostFlareFile(url, archivePath, caseID, email, hostname)
+		if r != nil && r.StatusCode == 307 && redirectHops < 5 {
+			url = r.Header.Get("Location")
+		} else {
+			return r, err
+		}
+	}
+}
+
+func readAndPostFlareFile(url string, archivePath string, caseID string, email string, hostname string) (*http.Response, error) {
 	bodyReader, bodyWriter := io.Pipe()
 	defer bodyReader.Close()
 	writer := multipart.NewWriter(bodyWriter)
@@ -72,10 +92,9 @@ func SendFlareWithHostname(archivePath string, caseID string, email string, host
 
 	}()
 
-	var url = mkURL(caseID)
 	request, err := http.NewRequest("POST", url, bodyReader)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	request.Header.Set("Content-Type", writer.FormDataContentType())
 
@@ -84,9 +103,7 @@ func SendFlareWithHostname(archivePath string, caseID string, email string, host
 	request.ContentLength = -1
 
 	client := mkHTTPClient()
-	r, err := client.Do(request)
-
-	return analyzeResponse(r, err)
+	return client.Do(request)
 }
 
 // SendFlare will send a flare and grab the local hostname
