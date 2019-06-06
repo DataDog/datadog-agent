d4b584b5d69220a3aa73ac2b0a83da07021fa6b2
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2019-06-06 17:22:34 +0200

    cleaning up

--- tasks/docker.py
+++ tasks/docker.py
@@ -147,15 +147,6 @@ def publish(ctx, src, dst, signed_pull=False, signed_push=False):
 @task
 def delete(ctx, org, image, tag, token):
     print("Deleting {org}/{image}:{tag}".format(org=org, image=image, tag=tag))
-
-    # FIXME: remove these asserts and debugging lines after testing
-    assert image == "agent-dev" # assert we don't do break things when testing
-    assert tag != "latest" # assert we don't do break things when testing
-
-    # debugging
-    print("Using the following command")
-    print("curl 'https://hub.docker.com/v2/repositories/{org}/{image}/tags/{tag}/' -X DELETE -H 'Authorization: JWT {token}'".format(org=org, image=image, tag=tag, token=token))
-
     ctx.run("curl 'https://hub.docker.com/v2/repositories/{org}/{image}/tags/{tag}/' -X DELETE -H 'Authorization: JWT {token}'".format(org=org, image=image, tag=tag, token=token))
 
 @task
