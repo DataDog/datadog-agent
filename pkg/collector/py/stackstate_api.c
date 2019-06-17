d9a0dc3a90d8bad3d2f58d1e8c4c2eefeeeaf90e
Bram Schuur <bramschuur@gmail.com>
2019-06-17 10:58:26 +0200

    Rename stackstate to topology api

--- pkg/collector/py/stackstate_api.c
+++ pkg/collector/py/stackstate_api.c
@@ -1,8 +1,3 @@
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License Version 2.0.
-// This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2019 Datadog, Inc.
-
 // +build cpython
 
 #include "stackstate_api.h"
@@ -87,11 +82,11 @@ static PyObject *submit_stop_snapshot(PyObject *self, PyObject *args) {
     return SubmitStopSnapshot(check, check_id, instance_key);
 }
 
-static PyMethodDef StackStateMethods[] = {
-  {"submit_component", (PyCFunction)submit_component, METH_VARARGS, "Submit a component to the stackstate api."},
-  {"submit_relation", (PyCFunction)submit_relation, METH_VARARGS, "Submit a relation to the stackstate api."},
-  {"submit_start_snapshot", (PyCFunction)submit_start_snapshot, METH_VARARGS, "Submit a snapshot start to the stackstate api."},
-  {"submit_stop_snapshot", (PyCFunction)submit_stop_snapshot, METH_VARARGS, "Submit a snapshot stop to the stackstate api."},
+static PyMethodDef TopologyMethods[] = {
+  {"submit_component", (PyCFunction)submit_component, METH_VARARGS, "Submit a component to the topology api."},
+  {"submit_relation", (PyCFunction)submit_relation, METH_VARARGS, "Submit a relation to the topology api."},
+  {"submit_start_snapshot", (PyCFunction)submit_start_snapshot, METH_VARARGS, "Submit a snapshot start to the topology api."},
+  {"submit_stop_snapshot", (PyCFunction)submit_stop_snapshot, METH_VARARGS, "Submit a snapshot stop to the topology api."},
   {NULL, NULL}  // guards
 };
 
@@ -100,7 +95,7 @@ void initstackstate()
   PyGILState_STATE gstate;
   gstate = PyGILState_Ensure();
 
-  PyObject *m = Py_InitModule("stackstate", StackStateMethods);
+  PyObject *m = Py_InitModule("topology", TopologyMethods);
 
   PyGILState_Release(gstate);
 }
--- pkg/collector/py/tests/testtopology.py
+++ pkg/collector/py/tests/testtopology.py
@@ -3,9 +3,8 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2016-2019 Datadog, Inc.
 
-from datetime import datetime
 from checks import AgentCheck
-import stackstate
+import topology
 
 # [BS] TODO: move this to a separate repo
 class StackStateAgentCheck(AgentCheck):
@@ -17,21 +16,21 @@ class StackStateAgentCheck(AgentCheck):
         # [BS] TODO: Catch invalid parameters (input sanitization) (e.g. types of parameters, of instance_key)
         if data is None:
             data = {}
-        stackstate.submit_component(self, self.check_id, self.get_instance_key(), id, type, data)
+        topology.submit_component(self, self.check_id, self.get_instance_key(), id, type, data)
 
     def relation(self, source, target, type, data):
         # [BS] TODO: Catch invalid parameters (input sanitization) (e.g. types of parameters, of instance_key)
         if data is None:
             data = {}
-        stackstate.submit_relation(self, self.check_id, self.get_instance_key(), source, target, type, data)
+        topology.submit_relation(self, self.check_id, self.get_instance_key(), source, target, type, data)
 
     def start_snapshot(self):
         # [BS] TODO: Catch invalid parameters (input sanitization) (e.g. instance_key)
-        stackstate.submit_start_snapshot(self, self.check_id, self.get_instance_key())
+        topology.submit_start_snapshot(self, self.check_id, self.get_instance_key())
 
     def stop_snapshot(self):
         # [BS] TODO: Catch invalid parameters (input sanitization) (e.g. instance_key)
-        stackstate.submit_stop_snapshot(self, self.check_id, self.get_instance_key())
+        topology.submit_stop_snapshot(self, self.check_id, self.get_instance_key())
 
 class TestComponentCheck(StackStateAgentCheck):
     def get_instance_key(self):
