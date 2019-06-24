8c9fc58f768e84b087c9fe3b2ca0832ca9b81684
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2019-06-24 18:07:33 +0200

    remove unnecessary quotes

--- test/e2e/scripts/generate-parameters.sh
+++ test/e2e/scripts/generate-parameters.sh
@@ -70,7 +70,7 @@ generate_parameters()
     
     # Rendering namespace
     echo 'Info: Parameters merged, rendering namespace and saving file...'
-    NAMESPACE_TEMPLATE_VAR="'{{ namespace }}'"
+    NAMESPACE_TEMPLATE_VAR="{{ namespace }}"
     sed -e "s/$NAMESPACE_TEMPLATE_VAR/$NAMESPACE/g" $TMP_YAML_PATH > $OUTPUT_YAML_FILE
     echo "Info: Generated parameters, yaml file saved: $OUTPUT_YAML_FILE"
     
