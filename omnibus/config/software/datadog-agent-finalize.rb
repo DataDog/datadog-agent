46fd4569e0a97f3769139a123074a59b443d88a9
Remy Mathieu <remy.mathieu@datadoghq.com>
2019-06-28 10:42:43 +0200

    Store the files installed by the package to later automatically remove all the *others*.

--- omnibus/config/software/datadog-agent-finalize.rb
+++ omnibus/config/software/datadog-agent-finalize.rb
@@ -98,6 +98,11 @@ build do
             command "echo '# DO NOT REMOVE/MODIFY - used by package removal tasks' > #{install_dir}/embedded/.py_compiled_files.txt"
             command "find #{install_dir}/embedded '(' -name '*.pyc' -o -name '*.pyo' ')' -type f -delete -print >> #{install_dir}/embedded/.py_compiled_files.txt"
 
+            # The prerm and preinst scripts of the package will use this list to detect which files
+            # have been setup by the installer, this way, on removal, we'll be able to delete only files
+            # which have not been created by the package.
+            command "find #{install_dir}/embedded/lib/python2.7/site-packages ! -name 'datadog_checks*' -type d -name 'datadog_*' >> #{install_dir}/embedded/.installed_by_pkg.txt
+
             # Setup pip aliases: `/opt/datadog-agent/embedded/bin/pip` will default to `pip2`
             if with_python_runtime? "2"
                 delete "#{install_dir}/embedded/bin/pip"
