fbaf4bda8d888ea32f5be6475d1040051d19343e
Alexandre Jacquemot <alexandre.jacquemot@datadoghq.com>
2019-07-08 18:38:42 +0200

    Fixed several bugs in the decoder (#3676)
    * Make sure the matcher is not exposed publicly
    
    * Updated the comments for the line handler
    
    * Refactored single line handler and added more comments
    
    * Fixed bug where we would commit next log timestamp to auditor
    
    * Removed the line buffer, refactored the multiline handler code and fixed a wrong accounting of raw length
    
    * Added missing tests decoding logic for kubernetes and docker
    
    * Updated the comments for multiline handler
    
    * Fixed wrong formatting of docker logs when being collected on Kubernetes from /var/log/pods (#3655)

--- pkg/logs/decoder/line_handler.go
+++ pkg/logs/decoder/line_handler.go
@@ -14,17 +14,25 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
-// TRUNCATED is the warning we add at the beginning or/and at the end of a truncated message
-var TRUNCATED = []byte("...TRUNCATED...")
+// truncatedFlag is the flag that is added at the beginning
+// or/and at the end of every trucated lines.
+var truncatedFlag = []byte("...TRUNCATED...")
 
-// LineHandler handles byte slices to form line output
+// escapedLineFeed is used to escape new line character
+// for multiline message.
+// New line character needs to be escaped because they are used
+// as delimiter for transport.
+var escapedLineFeed = []byte(`\n`)
+
+// LineHandler handles raw lines to form structured lines
 type LineHandler interface {
 	Handle(content []byte)
 	Start()
 	Stop()
 }
 
-// SingleLineHandler creates and forward outputs to outputChan from single-lines
+// SingleLineHandler takes care of tracking the line length
+// and truncating them when they are too long.
 type SingleLineHandler struct {
 	lineChan       chan []byte
 	outputChan     chan *Output
@@ -33,7 +41,7 @@ type SingleLineHandler struct {
 	lineLimit      int
 }
 
-// NewSingleLineHandler returns a new SingleLineHandler
+// NewSingleLineHandler returns a new SingleLineHandler.
 func NewSingleLineHandler(outputChan chan *Output, parser parser.Parser, lineLimit int) *SingleLineHandler {
 	return &SingleLineHandler{
 		lineChan:   make(chan []byte),
@@ -43,23 +51,22 @@ func NewSingleLineHandler(outputChan chan *Output, parser parser.Parser, lineLim
 	}
 }
 
-// Handle trims leading and trailing whitespaces from content,
-// and sends it as a new Line to lineChan.
+// Handle puts all new lines into a channel for later processing.
 func (h *SingleLineHandler) Handle(content []byte) {
 	h.lineChan <- content
 }
 
-// Stop stops the handler from processing new lines
+// Stop stops the handler.
 func (h *SingleLineHandler) Stop() {
 	close(h.lineChan)
 }
 
-// Start starts the handler
+// Start starts the handler.
 func (h *SingleLineHandler) Start() {
 	go h.run()
 }
 
-// run consumes lines from lineChan to process them
+// run consumes new lines and processes them.
 func (h *SingleLineHandler) run() {
 	for line := range h.lineChan {
 		h.process(line)
@@ -67,102 +74,107 @@ func (h *SingleLineHandler) run() {
 	close(h.outputChan)
 }
 
-// process creates outputs from lines and forwards them to outputChan
-// When lines are too long, they are truncated
+// process transforms a raw line into a structured line,
+// it guarantees that the content of the line won't exceed
+// the limit and that the length of the line is properly tracked
+// so that the agent restarts tailing from the right place.
 func (h *SingleLineHandler) process(line []byte) {
-	lineLen := len(line)
-	line = bytes.TrimSpace(line)
-	if len(line) == 0 {
+	isTruncated := h.shouldTruncate
+	h.shouldTruncate = false
+
+	rawLen := len(line)
+	if rawLen < h.lineLimit {
+		// lines are delimited on '\n' character when bellow the limit
+		// so we need to make sure it's properly accounted
+		rawLen++
+	}
+
+	content, status, timestamp, err := h.parser.Parse(line)
+	if err != nil {
+		log.Debug(err)
+	}
+	content = bytes.TrimSpace(content)
+	if len(content) == 0 {
+		// don't send empty lines
 		return
 	}
 
-	var content []byte
-	if h.shouldTruncate {
-		// add TRUNCATED at the beginning of content
-		content = append(TRUNCATED, line...)
-		h.shouldTruncate = false
-	} else {
-		// keep content the same
-		content = line
+	if isTruncated {
+		// the previous line has been truncated because it was too long,
+		// the new line is just a remainder,
+		// adding the truncated flag at the beginning of the content
+		content = append(truncatedFlag, content...)
 	}
 
-	if lineLen < h.lineLimit {
-		// send content
-		// add 1 to take into account '\n' that we didn't include in content
-		output, status, timestamp, err := h.parser.Parse(content)
-		if err != nil {
-			log.Debug(err)
-		}
-		if len(output) > 0 {
-			h.outputChan <- NewOutput(output, status, lineLen+1, timestamp)
-		}
+	if len(content) < h.lineLimit {
+		h.outputChan <- NewOutput(content, status, rawLen, timestamp)
 	} else {
-		// add TRUNCATED at the end of content and send it
-		content := append(content, TRUNCATED...)
-		output, status, timestamp, err := h.parser.Parse(content)
-		if err != nil {
-			log.Debug(err)
-		}
-		if len(output) > 0 {
-			h.outputChan <- NewOutput(output, status, lineLen, timestamp)
-			h.shouldTruncate = true
-		}
+		// the line is too long, it needs to be cut off and send,
+		// adding the truncated flag the end of the content
+		content = append(content, truncatedFlag...)
+		h.outputChan <- NewOutput(content, status, rawLen, timestamp)
+		// make sure the following part of the line will be cut off as well
+		h.shouldTruncate = true
 	}
 }
 
-// defaultFlushTimeout represents the time we want to wait before flushing lineBuffer
-// when no more line is received
+// defaultFlushTimeout represents the time after which a multiline
+// will be be considered as complete.
 const defaultFlushTimeout = 1000 * time.Millisecond
 
-// MultiLineHandler reads lines from lineChan and uses lineBuffer to send them
-// when a new line matches with re or flushTimer is fired
+// MultiLineHandler makes sure that multiple lines from a same content
+// are properly put together.
 type MultiLineHandler struct {
-	lineChan          chan []byte
-	outputChan        chan *Output
-	lineBuffer        *LineBuffer
-	lastSeenTimestamp string
-	newContentRe      *regexp.Regexp
-	flushTimeout      time.Duration
-	parser            parser.Parser
-	lineLimit         int
+	lineChan       chan []byte
+	outputChan     chan *Output
+	parser         parser.Parser
+	newContentRe   *regexp.Regexp
+	buffer         *bytes.Buffer
+	flushTimeout   time.Duration
+	lineLimit      int
+	shouldTruncate bool
+	linesLen       int
+	status         string
+	timestamp      string
 }
 
-// NewMultiLineHandler returns a new MultiLineHandler
+// NewMultiLineHandler returns a new MultiLineHandler.
 func NewMultiLineHandler(outputChan chan *Output, newContentRe *regexp.Regexp, flushTimeout time.Duration, parser parser.Parser, lineLimit int) *MultiLineHandler {
 	return &MultiLineHandler{
 		lineChan:     make(chan []byte),
 		outputChan:   outputChan,
-		lineBuffer:   NewLineBuffer(),
-		newContentRe: newContentRe,
-		flushTimeout: flushTimeout,
 		parser:       parser,
+		newContentRe: newContentRe,
+		buffer:       bytes.NewBuffer(nil),
+		flushTimeout: flushTimeout,
 		lineLimit:    lineLimit,
 	}
 }
 
-// Handle forward lines to lineChan to process them
+// Handle forward lines to lineChan to process them.
 func (h *MultiLineHandler) Handle(content []byte) {
 	h.lineChan <- content
 }
 
-// Stop stops the lineHandler from processing lines
+// Stop stops the handler.
 func (h *MultiLineHandler) Stop() {
 	close(h.lineChan)
 }
 
-// Start starts the handler
+// Start starts the handler.
 func (h *MultiLineHandler) Start() {
 	go h.run()
 }
 
-// run processes new lines from lineChan and flushes the buffer when the timeout expires
+// run processes new lines from the channel and makes sur the content is properly sent when
+// it stayed for too long in the buffer.
 func (h *MultiLineHandler) run() {
 	flushTimer := time.NewTimer(h.flushTimeout)
 	defer func() {
 		flushTimer.Stop()
-		// last send before closing the channel to flush lineBuffer. This can occur when
-		// container stops before meeting sendContent condition.
-		h.sendContent()
+		// make sure the content stored in the buffer gets sent,
+		// this can happen when the stop is called in between two timer ticks.
+		h.sendBuffer()
 		close(h.outputChan)
 	}()
 	for {
@@ -174,9 +186,9 @@ func (h *MultiLineHandler) run() {
 			}
 			// process the new line and restart the timeout
 			if !flushTimer.Stop() {
-				// stop doesn't not prevent a tick from the Timer if it happens at the same time
-				// we read from the timer channel to prevent an incorrect read
-				// in <-flushTimer.C in the case below
+				// timer stop doesn't not prevent the timer to tick,
+				// makes sure the event is consumed to avoid sending
+				// just one piece of the content.
 				select {
 				case <-flushTimer.C:
 				default:
@@ -185,63 +197,84 @@ func (h *MultiLineHandler) run() {
 			h.process(line)
 			flushTimer.Reset(h.flushTimeout)
 		case <-flushTimer.C:
-			// the timout expired, the content is ready to be sent
-			h.sendContent()
+			// no line has been collected since a while,
+			// the content is supposed to be complete.
+			h.sendBuffer()
 		}
 	}
 }
 
-// process accumulates lines in lineBuffer and flushes lineBuffer when a new line matches with newContentRe
-// When lines are too long, they are truncated
+// process aggregates multiple lines to form a full multiline message,
+// it stops when a line matches with the new content regular expression.
+// It also makes sure that the content will never exceed the limit
+// and that the length of the lines is properly tracked
+// so that the agent restarts tailing from the right place.
 func (h *MultiLineHandler) process(line []byte) {
-	unwrappedLine, _, timestamp, err := h.parser.Parse(line)
-	h.lastSeenTimestamp = timestamp
+	content, status, timestamp, err := h.parser.Parse(line)
 	if err != nil {
 		log.Debug(err)
 	}
-	if h.newContentRe.Match(unwrappedLine) {
-		// send content from lineBuffer
-		h.sendContent()
+
+	if h.newContentRe.Match(content) {
+		// the current line is part of a new message,
+		// send the buffer
+		h.sendBuffer()
 	}
-	if !h.lineBuffer.IsEmpty() {
-		// unwrap all the following lines
-		line = unwrappedLine
-		// add '\n' to content in lineBuffer
-		h.lineBuffer.AddEndOfLine()
+
+	isTruncated := h.shouldTruncate
+	h.shouldTruncate = false
+
+	rawLen := len(line)
+	if rawLen < h.lineLimit {
+		// lines are delimited on '\n' character when bellow the limit
+		// so we need to make sure it's properly accounted
+		rawLen++
 	}
-	// NOTES: this check takes into account the length of "...TRUNCATED..."
-	// which in some scenario is outputting an ending message with
-	// ...TRUNCATED... as only content, see unit test line_handler_test.go/TestMultiLineHandler
-	if len(line)+h.lineBuffer.Length() < h.lineLimit {
-		// add line to content in lineBuffer
-		h.lineBuffer.Add(line)
-	} else {
-		// add line and truncate and flush content in lineBuffer
-		h.lineBuffer.AddIncompleteLine(line)
-		h.lineBuffer.AddTruncate(line)
-		// send content from lineBuffer
-		h.sendContent()
-		// truncate next content
-		h.lineBuffer.AddTruncate(line)
+
+	// track the raw data length and the timestamp so that the agent tails
+	// from the right place at restart
+	h.linesLen += rawLen
+	h.timestamp = timestamp
+	h.status = status
+
+	if h.buffer.Len() > 0 {
+		// the buffer already contains some data which means that
+		// the current line is not the first line of the message
+		h.buffer.Write(escapedLineFeed)
+	}
+
+	if isTruncated {
+		// the previous line has been truncated because it was too long,
+		// the new line is just a remainder,
+		// adding the truncated flag at the beginning of the content
+		h.buffer.Write(truncatedFlag)
+	}
+
+	h.buffer.Write(content)
+
+	if h.buffer.Len() >= h.lineLimit {
+		// the multiline message is too long, it needs to be cut off and send,
+		// adding the truncated flag the end of the content
+		h.buffer.Write(truncatedFlag)
+		h.sendBuffer()
+		h.shouldTruncate = true
 	}
 }
 
-// sendContent forwards the content from lineBuffer to outputChan
-func (h *MultiLineHandler) sendContent() {
-	defer h.lineBuffer.Reset()
-	content, rawDataLen := h.lineBuffer.Content()
-	content = bytes.TrimSpace(content)
+// sendBuffer forwards the content stored in the buffer
+// to the output channel.
+func (h *MultiLineHandler) sendBuffer() {
+	defer func() {
+		h.buffer.Reset()
+		h.linesLen = 0
+		h.shouldTruncate = false
+	}()
+
+	data := bytes.TrimSpace(h.buffer.Bytes())
+	content := make([]byte, len(data))
+	copy(content, data)
+
 	if len(content) > 0 {
-		output, status, _, err := h.parser.Parse(content)
-		if err != nil {
-			log.Debug(err)
-		}
-		if len(output) > 0 {
-			// The output.Timestamp filled by the Parse function is the ts of the first
-			// log line, in order to be useful to setLastSince function, we need to replace
-			// it with the ts of the last log line. Note: this timestamp is NOT used for stamp
-			// the log record, it's ONLY used to recover well when tailing back the container.
-			h.outputChan <- NewOutput(output, status, rawDataLen, h.lastSeenTimestamp)
-		}
+		h.outputChan <- NewOutput(content, h.status, h.linesLen, h.timestamp)
 	}
 }
