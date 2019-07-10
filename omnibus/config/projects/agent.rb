db3fa3915b4a632d01012e368887d31304a2511d
Derek Brown <derek.brown@datadoghq.com>
2019-07-09 23:25:00 -0700

    Adds ability to sign using pfx (pkcs12) cert file rather than require cert be added to cert store

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -94,6 +94,9 @@ package :msi do
   if ENV['SIGN_WINDOWS']
     signing_identity "ECCDAE36FDCB654D2CBAB3E8975AA55469F96E4C", machine_store: true, algorithm: "SHA256"
   end
+  if ENV['SIGN_PFX']
+    signing_identity_file "#{ENV['SIGN_PFX']}", password: "#{ENV['SIGN_PFX_PW']}", algorithm: "SHA256"
+  end
   parameters({
     'InstallDir' => install_dir,
     'InstallFiles' => "#{Omnibus::Config.source_dir()}/datadog-agent/dd-agent/packaging/datadog-agent/win32/install_files",
