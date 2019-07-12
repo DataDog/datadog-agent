18882ff18356f074dd86923bdb508efb7f2c7be1
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2019-07-12 16:22:20 -0400

    all: add copyright headers (#3869)
    This commit is the result of running the following program:
    
    ```
    if err := filepath.Walk("./pkg/trace", func(path string, info os.FileInfo, err error) error {
          if err != nil {
    	      return err
          }
          if filepath.Ext(path) != ".go" || info.IsDir() {
    	      return nil
          }
          log.Println("Writing", path)
          slurp, err := ioutil.ReadFile(path)
          if err != nil {
    	      return err
          }
          return ioutil.WriteFile(path, append([]byte(copyrightText), slurp...), 0666)
    }); err != nil {
          log.Fatal(err)
    }
    ```

--- pkg/trace/pb/doc.go
+++ pkg/trace/pb/doc.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
 // Package pb contains the data structures used by the trace agent to communicate
 // with tracers and the Datadog API. Note that the "//go:generate" directives from this
 // package were removed because the generated files were manually edited to create
