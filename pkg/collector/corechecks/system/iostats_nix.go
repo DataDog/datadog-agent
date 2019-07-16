b0ec4010adabe0c75c36deefbb46547a7ee12b70
Maxime Mouial <maxime.mouial@gmail.com>
2019-07-16 11:09:34 -0600

    Handle 32bits overflow on 64bits system
    Some value in /proc/diskstats overflow at maxUInt32 even when running on
    a 64bit system.
    
    We now detect if the value overflowed at the maxUInt32 mark or
    maxUInt64. This fixes spikes in metrics when the values wrap back to 0.
    
    This also fixes the values being offset by 1 when they were wrapping.

--- pkg/collector/corechecks/system/iostats_nix.go
+++ pkg/collector/corechecks/system/iostats_nix.go
@@ -48,14 +48,21 @@ func roundFloat(val float64) float64 {
 	return math.Round(val*100) / 100
 }
 
-// Can't use values from math.MaxUint* because C size for longs changes on 32/64 bit machines
-const maxLong = int64(^uint(0) >> 1)
+// We can't use values from math.MaxUint* because C size for longs changes on 32/64 bit machines
+const maxULong = uint64(^uint(0)) // local max value for an uint (uint64 or uint32)
+const maxULong32 = math.MaxUint32
 
 // Compute the increment between two iostats values, taking into account they can overflow
-func incrementWithOverflow(currentValue, lastValue uint64, maxValue int64) int64 {
+func incrementWithOverflow(currentValue, lastValue uint64) int64 {
 	ret := int64(currentValue - lastValue)
 	if ret < 0 {
-		ret = ret + maxValue
+		// even on 64bit machines some values overflow at max32bit (like WeightedIO)
+		maxValue := maxULong
+		if lastValue <= maxULong32 {
+			maxValue = maxULong32
+		}
+		// +1 is for the overflow that pushed the value from its max to 0
+		ret = int64(currentValue + (maxValue - lastValue) + 1)
 	}
 	return ret
 }
@@ -112,40 +119,40 @@ func (c *IOCheck) nixIO() error {
 		}
 
 		// computing kB/s
-		rkbs := float64(incrementWithOverflow(ioStats.ReadBytes, lastIOStats.ReadBytes, maxLong)) / kB / deltaSecond
-		wkbs := float64(incrementWithOverflow(ioStats.WriteBytes, lastIOStats.WriteBytes, maxLong)) / kB / deltaSecond
-		avgqusz := float64(incrementWithOverflow(ioStats.WeightedIO, lastIOStats.WeightedIO, maxLong)) / kB / deltaSecond
+		rkbs := float64(incrementWithOverflow(ioStats.ReadBytes, lastIOStats.ReadBytes)) / kB / deltaSecond
+		wkbs := float64(incrementWithOverflow(ioStats.WriteBytes, lastIOStats.WriteBytes)) / kB / deltaSecond
+		avgqusz := float64(incrementWithOverflow(ioStats.WeightedIO, lastIOStats.WeightedIO)) / kB / deltaSecond
 
 		rAwait := 0.0
 		wAwait := 0.0
-		diffNRIO := float64(incrementWithOverflow(ioStats.ReadCount, lastIOStats.ReadCount, maxLong))
-		diffNWIO := float64(incrementWithOverflow(ioStats.WriteCount, lastIOStats.WriteCount, maxLong))
+		diffNRIO := float64(incrementWithOverflow(ioStats.ReadCount, lastIOStats.ReadCount))
+		diffNWIO := float64(incrementWithOverflow(ioStats.WriteCount, lastIOStats.WriteCount))
 		if diffNRIO != 0 {
 			//Note we use math.MaxUint32 because this value is always 32-bit, even on 64 bit machines
-			rAwait = float64(incrementWithOverflow(ioStats.ReadTime, lastIOStats.ReadTime, math.MaxUint32)) / diffNRIO
+			rAwait = float64(incrementWithOverflow(ioStats.ReadTime, lastIOStats.ReadTime)) / diffNRIO
 		}
 		if diffNWIO != 0 {
 			//Note we use math.MaxUint32 because this value is always 32-bit, even on 64 bit machines
-			wAwait = float64(incrementWithOverflow(ioStats.WriteTime, lastIOStats.WriteTime, math.MaxUint32)) / diffNWIO
+			wAwait = float64(incrementWithOverflow(ioStats.WriteTime, lastIOStats.WriteTime)) / diffNWIO
 		}
 
 		avgrqsz := 0.0
 		aWait := 0.0
 		diffNIO := diffNRIO + diffNWIO
 		if diffNIO != 0 {
-			avgrqsz = float64((incrementWithOverflow(ioStats.ReadBytes, lastIOStats.ReadBytes, maxLong)+
-				incrementWithOverflow(ioStats.WriteBytes, lastIOStats.WriteBytes, maxLong))/SectorSize) / diffNIO
+			avgrqsz = float64((incrementWithOverflow(ioStats.ReadBytes, lastIOStats.ReadBytes)+
+				incrementWithOverflow(ioStats.WriteBytes, lastIOStats.WriteBytes))/SectorSize) / diffNIO
 			//Note we use math.MaxUint32 because these values are always 32-bit, even on 64 bit machines
 			aWait = float64(
-				incrementWithOverflow(ioStats.ReadTime, lastIOStats.ReadTime, math.MaxUint32)+
-					incrementWithOverflow(ioStats.WriteTime, lastIOStats.WriteTime, math.MaxUint32)) / diffNIO
+				incrementWithOverflow(ioStats.ReadTime, lastIOStats.ReadTime)+
+					incrementWithOverflow(ioStats.WriteTime, lastIOStats.WriteTime)) / diffNIO
 		}
 
 		// we are aligning ourselves with the metric reported by
 		// sysstat, so itv is a time interval in 1/100th of a second
 		itv := delta / 10
 		tput := diffNIO * 100 / itv
-		util := float64(incrementWithOverflow(ioStats.IoTime, lastIOStats.IoTime, maxLong)) / itv * 100
+		util := float64(incrementWithOverflow(ioStats.IoTime, lastIOStats.IoTime)) / itv * 100
 		svctime := 0.0
 		if tput != 0 {
 			svctime = util / tput
