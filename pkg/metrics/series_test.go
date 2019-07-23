89088b401f9cce4a3e73b51005132d23fd7b8081
Olivier Gaça <olivier.gaca@datadoghq.com>
2019-07-23 14:53:04 +0200

    Uncomment TestPayloadsSeries

--- pkg/metrics/series_test.go
+++ pkg/metrics/series_test.go
@@ -6,17 +6,21 @@
 package metrics
 
 import (
+	"bytes"
+	"compress/zlib"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"testing"
 
-	"github.com/json-iterator/go"
+	jsoniter "github.com/json-iterator/go"
 
+	agentpayload "github.com/DataDog/agent-payload/gogen"
+	"github.com/DataDog/datadog-agent/pkg/forwarder"
+	"github.com/DataDog/datadog-agent/pkg/serializer/jsonstream"
 	"github.com/gogo/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	agentpayload "github.com/DataDog/agent-payload/gogen"
 )
 
 func TestMarshalSeries(t *testing.T) {
@@ -378,100 +382,114 @@ func TestDescribeItem(t *testing.T) {
 	assert.Equal(t, "out of range", desc2)
 }
 
-// // test taken from the spliter
-// func TestPayloadsSeries(t *testing.T) {
-// 	testSeries := metrics.Series{}
-// 	for i := 0; i < 30000; i++ {
-// 		point := metrics.Serie{
-// 			Points: []metrics.Point{
-// 				{Ts: 12345.0, Value: float64(21.21)},
-// 				{Ts: 67890.0, Value: float64(12.12)},
-// 				{Ts: 2222.0, Value: float64(22.12)},
-// 				{Ts: 333.0, Value: float64(32.12)},
-// 				{Ts: 444444.0, Value: float64(42.12)},
-// 				{Ts: 882787.0, Value: float64(52.12)},
-// 				{Ts: 99990.0, Value: float64(62.12)},
-// 				{Ts: 121212.0, Value: float64(72.12)},
-// 				{Ts: 222227.0, Value: float64(82.12)},
-// 				{Ts: 808080.0, Value: float64(92.12)},
-// 				{Ts: 9090.0, Value: float64(13.12)},
-// 			},
-// 			MType:    metrics.APIGaugeType,
-// 			Name:     fmt.Sprintf("test.metrics%d", i),
-// 			Interval: 1,
-// 			Host:     "localHost",
-// 			Tags:     []string{"tag1", "tag2:yes"},
-// 		}
-// 		testSeries = append(testSeries, &point)
-// 	}
+// test taken from the spliter
+func TestPayloadsSeries(t *testing.T) {
+	testSeries := Series{}
+	for i := 0; i < 30000; i++ {
+		point := Serie{
+			Points: []Point{
+				{Ts: 12345.0, Value: float64(21.21)},
+				{Ts: 67890.0, Value: float64(12.12)},
+				{Ts: 2222.0, Value: float64(22.12)},
+				{Ts: 333.0, Value: float64(32.12)},
+				{Ts: 444444.0, Value: float64(42.12)},
+				{Ts: 882787.0, Value: float64(52.12)},
+				{Ts: 99990.0, Value: float64(62.12)},
+				{Ts: 121212.0, Value: float64(72.12)},
+				{Ts: 222227.0, Value: float64(82.12)},
+				{Ts: 808080.0, Value: float64(92.12)},
+				{Ts: 9090.0, Value: float64(13.12)},
+			},
+			MType:    APIGaugeType,
+			Name:     fmt.Sprintf("test.metrics%d", i),
+			Interval: 1,
+			Host:     "localHost",
+			Tags:     []string{"tag1", "tag2:yes"},
+		}
+		testSeries = append(testSeries, &point)
+	}
 
-// 	originalLength := len(testSeries)
-// 	builder := NewPayloadBuilder()
-// 	payloads, err := builder.Build(testSeries)
-// 	require.Nil(t, err)
-// 	var splitSeries = []metrics.Series{}
-// 	for _, compressedPayload := range payloads {
-// 		payload, err := decompressPayload(*compressedPayload)
-// 		require.NoError(t, err)
+	originalLength := len(testSeries)
+	builder := jsonstream.NewPayloadBuilder()
+	payloads, err := builder.Build(testSeries)
+	require.Nil(t, err)
+	var splitSeries = []Series{}
+	for _, compressedPayload := range payloads {
+		payload, err := decompressPayload(*compressedPayload)
+		require.NoError(t, err)
 
-// 		var s = map[string]metrics.Series{}
-// 		err = json.Unmarshal(payload, &s)
-// 		require.NoError(t, err)
-// 		splitSeries = append(splitSeries, s["series"])
-// 	}
+		var s = map[string]Series{}
+		err = json.Unmarshal(payload, &s)
+		require.NoError(t, err)
+		splitSeries = append(splitSeries, s["series"])
+	}
 
-// 	unrolledSeries := metrics.Series{}
-// 	for _, series := range splitSeries {
-// 		for _, s := range series {
-// 			unrolledSeries = append(unrolledSeries, s)
-// 		}
-// 	}
+	unrolledSeries := Series{}
+	for _, series := range splitSeries {
+		for _, s := range series {
+			unrolledSeries = append(unrolledSeries, s)
+		}
+	}
 
-// 	newLength := len(unrolledSeries)
-// 	require.Equal(t, originalLength, newLength)
-// }
+	newLength := len(unrolledSeries)
+	require.Equal(t, originalLength, newLength)
+}
 
-// var result forwarder.Payloads
+var result forwarder.Payloads
 
-// func BenchmarkPayloadsSeries(b *testing.B) {
-// 	testSeries := metrics.Series{}
-// 	for i := 0; i < 400000; i++ {
-// 		point := metrics.Serie{
-// 			Points: []metrics.Point{
-// 				{Ts: 12345.0, Value: 1.2 * float64(i)},
-// 			},
-// 			MType:    metrics.APIGaugeType,
-// 			Name:     fmt.Sprintf("test.metrics%d", i),
-// 			Interval: 1,
-// 			Host:     "localHost",
-// 			Tags:     []string{"tag1", "tag2:yes"},
-// 		}
-// 		testSeries = append(testSeries, &point)
-// 	}
+func BenchmarkPayloadsSeries(b *testing.B) {
+	testSeries := Series{}
+	for i := 0; i < 400000; i++ {
+		point := Serie{
+			Points: []Point{
+				{Ts: 12345.0, Value: 1.2 * float64(i)},
+			},
+			MType:    APIGaugeType,
+			Name:     fmt.Sprintf("test.metrics%d", i),
+			Interval: 1,
+			Host:     "localHost",
+			Tags:     []string{"tag1", "tag2:yes"},
+		}
+		testSeries = append(testSeries, &point)
+	}
 
-// 	var r forwarder.Payloads
-// 	builder := NewPayloadBuilder()
-// 	for n := 0; n < b.N; n++ {
-// 		// always record the result of Payloads to prevent
-// 		// the compiler eliminating the function call.
-// 		r, _ = builder.Build(testSeries)
-// 	}
-// 	// ensure we actually had to split
-// 	if len(r) != 13 {
-// 		panic(fmt.Sprintf("expecting two payloads, got %d", len(r)))
-// 	}
-// 	// test the compressed size
-// 	var compressedSize int
-// 	for _, p := range r {
-// 		if p == nil {
-// 			continue
-// 		}
-// 		compressedSize += len([]byte(*p))
-// 	}
-// 	if compressedSize > 3000000 {
-// 		panic(fmt.Sprintf("expecting no more than 3 MB, got %d", compressedSize))
-// 	}
-// 	// always store the result to a package level variable
-// 	// so the compiler cannot eliminate the Benchmark itself.
-// 	result = r
-// }
+	var r forwarder.Payloads
+	builder := jsonstream.NewPayloadBuilder()
+	for n := 0; n < b.N; n++ {
+		// always record the result of Payloads to prevent
+		// the compiler eliminating the function call.
+		r, _ = builder.Build(testSeries)
+	}
+	// ensure we actually had to split
+	if len(r) != 13 {
+		panic(fmt.Sprintf("expecting two payloads, got %d", len(r)))
+	}
+	// test the compressed size
+	var compressedSize int
+	for _, p := range r {
+		if p == nil {
+			continue
+		}
+		compressedSize += len([]byte(*p))
+	}
+	if compressedSize > 3000000 {
+		panic(fmt.Sprintf("expecting no more than 3 MB, got %d", compressedSize))
+	}
+	// always store the result to a package level variable
+	// so the compiler cannot eliminate the Benchmark itself.
+	result = r
+}
+
+func decompressPayload(payload []byte) ([]byte, error) {
+	r, err := zlib.NewReader(bytes.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	dst, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	return dst, nil
+}
