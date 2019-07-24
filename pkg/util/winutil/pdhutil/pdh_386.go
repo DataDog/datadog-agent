6658a8b11d166ba74082550570a5d021f7800a4d
Derek Brown <derek.brown@datadoghq.com>
2019-07-24 15:02:00 -0700

    Fix padding for long countervalue

--- pkg/util/winutil/pdhutil/pdh_386.go
+++ pkg/util/winutil/pdhutil/pdh_386.go
@@ -23,5 +23,6 @@ type PDH_FMT_COUNTERVALUE_LARGE struct {
 // Union specialization for long values
 type PDH_FMT_COUNTERVALUE_LONG struct {
 	CStatus   uint32
+	floatpadding uint32
 	LongValue int32
 }
