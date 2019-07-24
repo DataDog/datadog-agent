953e203f417efb0ecbe8f971388d097eb79c8337
Bram Schuur <bramschuur@gmail.com>
2019-07-24 11:47:38 +0200

    Use topologyInstance datatype

--- pkg/collector/py/tests/testtopology.py
+++ pkg/collector/py/tests/testtopology.py
@@ -3,32 +3,32 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2016-2019 Datadog, Inc.
 
-from checks import AgentCheck
+from checks import AgentCheck, TopologyInstance
 
 class TestComponentCheck(AgentCheck):
     def get_instance_key(self, instance):
-        return { "type": "type", "url": "url" }
+        return TopologyInstance("type", "url")
 
     def check(self, instance):
         self.component("myid", "mytype", { "key": "value", "intlist": [1], "emptykey": None, "nestedobject": { "nestedkey": "nestedValue" }})
 
 class TestRelationCheck(AgentCheck):
     def get_instance_key(self, instance):
-        return { "type": "type", "url": "url" }
+        return TopologyInstance("type", "url")
 
     def check(self, instance):
         self.relation("source", "target", "mytype", { "key": "value", "intlist": [1], "emptykey": None, "nestedobject": { "nestedkey": "nestedValue" }})
 
 class TestStartSnapshotCheck(AgentCheck):
     def get_instance_key(self, instance):
-        return { "type": "type", "url": "url" }
+        return TopologyInstance("type", "url")
 
     def check(self, instance):
         self.start_snapshot()
 
 class TestStopSnapshotCheck(AgentCheck):
     def get_instance_key(self, instance):
-        return { "type": "type", "url": "url" }
+        return TopologyInstance("type", "url")
 
     def check(self, instance):
         self.stop_snapshot()
