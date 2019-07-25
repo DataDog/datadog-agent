ad27263c58855131ccd70d6d9ade940044644a50
johnnygladwin1991 <39700147+johnnygladwin1991@users.noreply.github.com>
2019-07-25 22:54:54 +0530

    Push `latest` tag for all StackState Agent docker images  (#102)
    * added push to latest tag for docker image official releases

--- omnibus/package-scripts/publish_image.sh
+++ omnibus/package-scripts/publish_image.sh
@@ -5,6 +5,7 @@ set -xe
 IMAGE_TAG=$1
 IMAGE_REPO=$2
 ARTIFACT_PATH=$3
+PUSH_LATEST="${4:-false}"
 
 echo $IMAGE_TAG
 echo $IMAGE_REPO
@@ -13,7 +14,13 @@ echo $ARTIFACT_PATH
 cp $ARTIFACT_PATH/*.deb Dockerfiles/agent
 
 docker build -t stackstate/${IMAGE_REPO}:${IMAGE_TAG} Dockerfiles/agent
-docker tag stackstate/${IMAGE_REPO}:${IMAGE_TAG} stackstate/${IMAGE_REPO}:latest
+
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push stackstate/${IMAGE_REPO}:${IMAGE_TAG}
+
+if ["$PUSH_LATEST= true" ]; then
+    docker tag stackstate/${IMAGE_REPO}:${IMAGE_TAG} stackstate/${IMAGE_REPO}:latest
+    echo 'Pushing release to latest'
+    docker push stackstate/${IMAGE_REPO}:latest
+fi
