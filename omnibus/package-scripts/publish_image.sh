a3bc28edf8c5bb9a68a6c109f6cf27197fd4cff9
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2019-08-02 01:59:47 +0200

    feature(cluster-agent): [STAC-5008] turn to publish_image.sh (adjusting it) instead of inv because cannot be used in the docker image

--- omnibus/package-scripts/publish_image.sh
+++ omnibus/package-scripts/publish_image.sh
@@ -4,22 +4,18 @@ set -xe
 
 IMAGE_TAG=$1
 IMAGE_REPO=$2
-ARTIFACT_PATH=$3
+DOCKERFILE_PATH=$3
 PUSH_LATEST="${4:-false}"
 
 echo $IMAGE_TAG
 echo $IMAGE_REPO
-echo $ARTIFACT_PATH
+echo $DOCKERFILE_PATH
 
-cp $ARTIFACT_PATH/*.deb Dockerfiles/agent
-
-docker build -t stackstate/${IMAGE_REPO}:${IMAGE_TAG} Dockerfiles/agent
-
-
+docker build -t stackstate/${IMAGE_REPO}:${IMAGE_TAG} $DOCKERFILE_PATH
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push stackstate/${IMAGE_REPO}:${IMAGE_TAG}
 
-if ["$PUSH_LATEST= true" ]; then
+if [ "$PUSH_LATEST" = "true" ]; then
     docker tag stackstate/${IMAGE_REPO}:${IMAGE_TAG} stackstate/${IMAGE_REPO}:latest
     echo 'Pushing release to latest'
     docker push stackstate/${IMAGE_REPO}:latest
