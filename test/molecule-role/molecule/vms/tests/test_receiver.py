a6700bce4cdce9cf184716b5c354fd66c613296b
Zandre Witte <zandre.witte@gmail.com>
2019-08-06 09:59:56 +0200

    vm test fixes

--- test/molecule-role/molecule/vms/tests/test_receiver.py
+++ test/molecule-role/molecule/vms/tests/test_receiver.py
@@ -242,7 +242,7 @@ def test_host_metrics(host):
 
         # Memory
         assert_metric("system.mem.total", lambda v: v > 900.0, lambda v: v > 900.0, lambda v: v > 2000.0)
-        assert_metric("system.mem.usable", lambda v: 1000.0 > v > 300.0, lambda v: 1000.0 > v > 300.0, lambda v: 1500.0 > v > 400.0)
+        assert_metric("system.mem.usable", lambda v: 1000.0 > v > 300.0, lambda v: 1000.0 > v > 300.0, lambda v: 1500.0 > v > 300.0)
         assert_metric("system.mem.pct_usable", lambda v: 1.0 > v > 0.3, lambda v: 1.0 > v > 0.3, lambda v: 1.0 > v > 0.2)
 
         # Load - only linux
