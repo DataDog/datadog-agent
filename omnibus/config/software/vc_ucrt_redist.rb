c78246d6f88a97685b4b28ea9010f0f5e1410cd4
Derek Brown <derek.brown@datadoghq.com>
2019-08-07 07:42:50 -0700

    Review feedback

--- omnibus/config/software/vc_ucrt_redist.rb
+++ omnibus/config/software/vc_ucrt_redist.rb
@@ -8,13 +8,13 @@ if windows_arch_i386?
   source :url => "https://s3.amazonaws.com/dd-agent-omnibus/msvc_ucrt_runtime_x86.zip",
          :sha256 => "7f93f888b3f99f890557a7361381b10552022744422e482510c24f25cf09191d",
          :extract => :seven_zip
-end
 
-build do
-  # Because python is built with really old (VS2008) visual c++, and with the CRT
-  # as a DLL, we need to redistribute the CRT DLLS.  We (now) need the DLLS in
-  # both embedded and dist, as we have executables in each of those directories
-  # that require them.
-  command "XCOPY /YEH .\\*.* \"#{windows_safe_path(python_3_embedded)}\" /IR"
+  build do
+    # Because python is built with really old (VS2008) visual c++, and with the CRT
+    # as a DLL, we need to redistribute the CRT DLLS.  We (now) need the DLLS in
+    # both embedded and dist, as we have executables in each of those directories
+    # that require them.
+    command "XCOPY /YEH .\\*.* \"#{windows_safe_path(python_3_embedded)}\" /IR"
 
+  end
 end
