cc3cb9ea3c501d6f60edb8f8328e693e789758eb
Kylian Serrania <kylian.serrania@datadoghq.com>
2019-08-08 19:01:17 +0200

    Test order change

--- test/kitchen/test/integration/common/rspec/spec_helper.rb
+++ test/kitchen/test/integration/common/rspec/spec_helper.rb
@@ -239,9 +239,9 @@ shared_examples_for 'Agent' do
   it_behaves_like 'a running Agent with no errors'
   it_behaves_like 'a running Agent with APM'
   it_behaves_like 'a running Agent with APM manually disabled'
+  it_behaves_like 'an Agent with python3 enabled'
   it_behaves_like 'an Agent that stops'
   it_behaves_like 'an Agent that restarts'
-  it_behaves_like 'an Agent with python3 enabled'
   it_behaves_like 'an Agent that is removed'
 end
 
