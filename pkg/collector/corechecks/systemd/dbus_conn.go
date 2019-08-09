a5554c76c6b78f3a5a27b46d2990a49d52a4c192
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-08-09 11:02:05 +0200

    Update license info

--- pkg/collector/corechecks/systemd/dbus_conn.go
+++ pkg/collector/corechecks/systemd/dbus_conn.go
@@ -1,23 +1,13 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
+// This file includes software developed at CoreOS, Inc (http://www.coreos.com/).
+
 // Copyright 2016-2019 Datadog, Inc.
+// Copyright 2015 CoreOS, Inc.
 //
-// This file includes software developed at CoreOS, Inc.
-// (http://www.coreos.com/).
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
+// Use of this source code is governed by Apache License 2.0
+// license that can be found here: https://github.com/coreos/go-systemd/blob/master/LICENSE
 
 // +build systemd
 
