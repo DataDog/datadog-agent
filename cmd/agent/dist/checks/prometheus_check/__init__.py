abfaf78940a721626251a6d7328b0ca9dd3037fc
Kylian Serrania <kylian.serrania@datadoghq.com>
2019-08-13 18:09:00 +0200

    Clean up cmd/agent/dist/checks folder

--- cmd/agent/dist/checks/prometheus_check/__init__.py
+++
@@ -1,6 +0,0 @@
-# (C) Datadog, Inc. 2016
-# All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
-from datadog_checks.checks.prometheus import (
-    PrometheusCheck, PrometheusFormat, UnknownFormatError
-)
