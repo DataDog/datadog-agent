b3252dc06e7aa542b7c2992e4d1c39ae775de84f
shangwang <shang-wang@users.noreply.github.com>
2019-08-13 10:11:00 -0400

    [process-agent] update process-agent check so it could start by datadog-agent in macOS (#3968)
    * update process-agent check to make sure it could auto start in macOS
    
    * remove unused config
    
    * update build task to copy process_agent.yaml.default file
    
    * remove process_agent.yaml.default on linux and windows build
    
    * added release note

--- tasks/agent.py
+++ tasks/agent.py
@@ -25,6 +25,7 @@ BIN_PATH = os.path.join(".", "bin", "agent")
 AGENT_TAG = "datadog/agent:master"
 DEFAULT_BUILD_TAGS = [
     "apm",
+    "process",
     "consul",
     "containerd",
     "python",
@@ -199,6 +200,8 @@ def refresh_assets(ctx, build_tags, development=True, puppy=False):
         copy_tree("./cmd/agent/dist/conf.d/{}.d/".format(check), check_dir)
     if "apm" in build_tags:
         shutil.copy("./cmd/agent/dist/conf.d/apm.yaml.default", os.path.join(dist_folder, "conf.d/apm.yaml.default"))
+    if "process" in build_tags:
+        shutil.copy("./cmd/agent/dist/conf.d/process_agent.yaml.default", os.path.join(dist_folder, "conf.d/process_agent.yaml.default"))
 
     copy_tree("./pkg/status/dist/", dist_folder)
     copy_tree("./cmd/agent/gui/views", os.path.join(dist_folder, "views"))
