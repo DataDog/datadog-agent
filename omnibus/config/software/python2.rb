c377f34a212187082b6a7ad28ab9ae4e28f9736a
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2019-08-22 13:41:53 +0200

    Revert "[omnibus][python2] shared build (#3554)" (#4057)
    This reverts commit 8a83c799136b0d3ca925b5770cbdc1330f42ec8a.

--- omnibus/config/software/python2.rb
+++ omnibus/config/software/python2.rb
@@ -48,8 +48,7 @@ if ohai["platform"] != "windows"
                           "CC=clang",
                           "MACOSX_DEPLOYMENT_TARGET=10.12")
   elsif linux?
-    python_configure.push("--enable-unicode=ucs4",
-                          "--enable-shared")
+    python_configure.push("--enable-unicode=ucs4")
   end
 
   build do
