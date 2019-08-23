d21ae6340aed10a5428867a84bf09d42468d6432
Albert Vaca <albert.vaca@datadoghq.com>
2019-08-23 12:41:34 +0200

    Build python 3 by default

--- tasks/rtloader.py
+++ tasks/rtloader.py
@@ -39,7 +39,7 @@ def build(ctx, install_prefix=None, python_runtimes=None, cmake_options='', arch
 
     cmake_args = cmake_options + " -DBUILD_DEMO:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH={}".format(install_prefix or dev_path)
 
-    python_runtimes = python_runtimes or os.environ.get("PYTHON_RUNTIMES") or "2"
+    python_runtimes = python_runtimes or os.environ.get("PYTHON_RUNTIMES") or "3"
     python_runtimes = python_runtimes.split(',')
 
     settings = {
