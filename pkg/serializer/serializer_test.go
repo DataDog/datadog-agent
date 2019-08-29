637b9ca13c55a6336c94da52f6a392f0081ad27f
Olivier Gaça <olivier.gaca@datadoghq.com>
2019-08-29 15:18:20 +0200

    Fix TestSendV1ServiceChecks

--- pkg/serializer/serializer_test.go
+++ pkg/serializer/serializer_test.go
@@ -199,7 +199,7 @@ func TestSendV1ServiceChecks(t *testing.T) {
 	f.On("SubmitV1CheckRuns", jsonPayloads, jsonExtraHeadersWithCompression).Return(nil).Times(1)
 
 	s := NewSerializer(f)
-
+	s.enableServiceChecksJSONStream = false
 	payload := &testPayload{}
 	err := s.SendServiceChecks(payload)
 	require.Nil(t, err)
