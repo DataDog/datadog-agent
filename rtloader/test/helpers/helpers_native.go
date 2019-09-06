5140ac1eeaa3c12a95121100a0619ebb3d45ac1b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2019-09-06 20:25:28 +0200

    [rtloader][test] more helpers for memory tracking

--- rtloader/test/helpers/helpers_native.go
+++ rtloader/test/helpers/helpers_native.go
@@ -1,5 +1,12 @@
 package helpers
 
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
 /*
 #cgo CFLAGS: -I../../include -I../../common
 #cgo !windows LDFLAGS: -L../../rtloader/ -ldatadog-agent-rtloader -ldl
@@ -19,4 +26,24 @@ import "C"
 // InitMemoryTracker initializes RTLoader memory tracking
 func InitMemoryTracker() {
 	C.initTestMemoryTracker()
+}
+
+// ResetMemoryStats resets allocations and frees counters to zero
+func TrackedCString(str string) unsafe.Pointer {
+	cstr := C.CString(str)
+	Allocations.Add(1)
+
+	return unsafe.Pointer(cstr)
+}
+
+// ResetMemoryStats resets allocations and frees counters to zero
+func ResetMemoryStats() {
+	Allocations.Set(0)
+	Frees.Set(0)
+}
+
+// AssertMemoryUsage makes sure the allocations and frees match
+func AssertMemoryUsage(t *testing.T) {
+	assert.Equal(t, Allocations.Value(), Frees.Value(),
+		"Number of allocations doesn't match number of frees")
 }
