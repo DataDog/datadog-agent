eab12211b879ddb940d25a3e532866d8b1682935
Julien Lebot <julien.lebot@datadoghq.com>
2019-09-11 14:42:04 +0200

    Move CommonConfigure after BuildID so that CheckBase's Tags are set on the correct sender.

--- pkg/collector/corechecks/net/ntp.go
+++ pkg/collector/corechecks/net/ntp.go
@@ -127,12 +127,8 @@ func (c *ntpConfig) parse(data []byte, initData []byte, getLocalServers func() (
 
 // Configure configure the data from the yaml
 func (c *NTPCheck) Configure(data integration.Data, initConfig integration.Data, source string) error {
-	err := c.CommonConfigure(data, source)
-	if err != nil {
-		return err
-	}
 	cfg := new(ntpConfig)
-	err = cfg.parse(data, initConfig, getLocalDefinedNTPServers)
+	err := cfg.parse(data, initConfig, getLocalDefinedNTPServers)
 	if err != nil {
 		log.Errorf("Error parsing configuration file: %s", err)
 		return err
@@ -140,6 +136,11 @@ func (c *NTPCheck) Configure(data integration.Data, initConfig integration.Data,
 
 	c.BuildID(data, initConfig)
 	c.cfg = cfg
+
+	err = c.CommonConfigure(data, source)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
