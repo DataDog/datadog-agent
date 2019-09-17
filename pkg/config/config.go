9622448ac05ae4ab2ba3d32c98f7a52436e0332b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2019-09-17 14:54:47 +0200

    [config] default python should be python3

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -131,7 +131,7 @@ func initConfig(config Config) {
 	config.BindEnvAndSetDefault("bind_host", "localhost")
 	config.BindEnvAndSetDefault("health_port", int64(0))
 	config.BindEnvAndSetDefault("disable_py3_validation", false)
-	config.BindEnvAndSetDefault("python_version", "2")
+	config.BindEnvAndSetDefault("python_version", "3")
 	// Debugging + C-land crash feature flags
 	config.BindEnvAndSetDefault("c_stacktrace_collection", false)
 	config.BindEnvAndSetDefault("c_core_dump", false)
