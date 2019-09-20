445a01ffe7a2c56fe2cfdc42c21960fef0c5ba99
Julien Lebot <julien.lebot@datadoghq.com>
2019-09-20 19:24:54 +0200

    Removed full access permissions to install dir

--- omnibus/resources/agent/msi/cal/CustomAction.cpp
+++ omnibus/resources/agent/msi/cal/CustomAction.cpp
@@ -244,8 +244,6 @@ extern "C" UINT __stdcall FinalizeInstall(MSIHANDLE hInstall) {
     }
     er = addDdUserPermsToFile(data, programdataroot);
     WcaLog(LOGMSG_STANDARD, "%d setting programdata dir perms", er);
-    er = addDdUserPermsToFile(data, installdir);
-    WcaLog(LOGMSG_STANDARD, "%d setting installdir dir perms", er);
 
     er = addDdUserPermsToFile(data, logfilename);
     WcaLog(LOGMSG_STANDARD, "%d setting log file perms", er);
