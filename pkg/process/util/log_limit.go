240bb18e05cda4d0d877ecfa5523d5411e3e98fb
Lee Avital <lee.avital@datadoghq.com>
2019-09-20 17:24:22 -0400

    Add tests

--- pkg/process/util/log_limit.go
+++ pkg/process/util/log_limit.go
@@ -1,43 +1,68 @@
 package util
 
 import (
+	"sync/atomic"
 	"time"
 )
 
 // LogLimit is a utility that can be used to avoid logging noisily
 type LogLimit struct {
-	firstN   int
-	interval time.Duration
-	next     time.Time
+	// n is the times remaining that the LogLimit will return true for ShouldLog.
+	// we repeatedly add 1 to it.
+	n int32
+
+	// reset will be channel that belongs to the ticket
+	ticker *time.Ticker
+	exit   chan struct{}
 }
 
 // NewLogLimit creates a LogLimit where shouldLog will return
 // true the first N times it is called, and will return true once every
 // interval thereafter.
 func NewLogLimit(n int, interval time.Duration) *LogLimit {
-	return &LogLimit{
-		firstN:   n,
-		interval: interval,
-		next:     time.Now().Add(interval),
+	l := &LogLimit{
+		n: int32(n),
+
+		// exit and ticker must be different channels
+		// becaues Stopping a ticker will not close the ticker channel,
+		// and we will otherwise leak memory
+		ticker: time.NewTicker(interval),
+		exit:   make(chan struct{}),
 	}
+
+	go l.resetLoop()
+	return l
 }
 
 // ShouldLog returns true if the caller should log
 func (l *LogLimit) ShouldLog() bool {
-	return l.shouldLogTime(time.Now())
-
-}
-
-func (l *LogLimit) shouldLogTime(now time.Time) bool {
-	if l.firstN > 0 {
-		l.firstN--
-		return true
-	}
-
-	if now.After(l.next) {
-		l.next = now.Add(l.interval)
+	if l.n > 0 {
+		l.n--
 		return true
 	}
 
 	return false
+}
+
+// Close will stop the underlying ticker
+func (l *LogLimit) Close() {
+	l.ticker.Stop()
+	close(l.exit)
+}
+
+func (l *LogLimit) resetLoop() {
+	for {
+		select {
+		case <-l.ticker.C:
+			l.resetCounter()
+		case <-l.exit:
+			return
+		}
+	}
+}
+
+func (l *LogLimit) resetCounter() {
+	// c.n == 0, it means we have gotten through the first few logs, and after ticker.T we should
+	// do another log
+	atomic.CompareAndSwapInt32(&l.n, 0, 1)
 }
--- pkg/process/util/log_limit_test.go
+++ pkg/process/util/log_limit_test.go
@@ -9,16 +9,18 @@ import (
 
 func TestLogLimit(t *testing.T) {
 	l := NewLogLimit(10, time.Hour)
-
-	now := time.Now()
+	defer l.Close()
 
 	for i := 0; i < 10; i++ {
-		assert.True(t, l.shouldLogTime(now))
+		// this reset will not have any effect because we haven't logged 10 times yet
+		l.resetCounter()
+		assert.True(t, l.ShouldLog())
 	}
 
-	assert.False(t, l.shouldLogTime(now))
-	assert.False(t, l.shouldLogTime(now.Add(time.Minute*20)))
+	assert.False(t, l.ShouldLog())
+	assert.False(t, l.ShouldLog())
 
-	assert.True(t, l.shouldLogTime(now.Add(time.Minute*61)))
-	assert.False(t, l.shouldLogTime(now.Add(time.Minute*61)))
+	l.resetCounter()
+	assert.True(t, l.ShouldLog())
+	assert.False(t, l.ShouldLog())
 }
