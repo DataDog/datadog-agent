0b44329fe75091988ddb1021f70bd6bed4b2e78b
Xavier Lucas <xavier.lucas@datadoghq.com>
2019-09-27 14:34:37 +0200

    Use OpenJDK11 in the docker image (#4217)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -337,6 +337,7 @@ func initConfig(config Config) {
 	// JMXFetch
 	config.BindEnvAndSetDefault("jmx_custom_jars", []string{})
 	config.BindEnvAndSetDefault("jmx_use_cgroup_memory_limit", false)
+	config.BindEnvAndSetDefault("jmx_use_container_support", false)
 	config.BindEnvAndSetDefault("jmx_max_restarts", int64(3))
 	config.BindEnvAndSetDefault("jmx_restart_interval", int64(5))
 	config.BindEnvAndSetDefault("jmx_thread_pool_size", 3)
