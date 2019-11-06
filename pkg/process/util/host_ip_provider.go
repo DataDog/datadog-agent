7c30b982740d8bac928e6f48086c90278c82dab9
Lee Avital <lee.avital@datadoghq.com>
2019-11-06 13:54:01 -0500

    More stuff

--- pkg/process/util/host_ip_provider.go
+++ pkg/process/util/host_ip_provider.go
@@ -1,6 +1,9 @@
 package util
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/util/cache"
 	"github.com/DataDog/datadog-agent/pkg/util/ec2"
@@ -12,20 +15,18 @@ import (
 func GetDockerHostIP() []string {
 	cacheKey := cache.BuildAgentKey("hostIPs")
 	if cachedIPs, found := cache.Cache.Get(cacheKey); found {
-		return cachedIPs
+		return cachedIPs.([]string)
 	}
 
 	ips := getDockerHostIPUncached()
 	if len(ips) == 0 {
 		log.Warnf("could not get host IP")
 	}
-	cache.Cache.Set(cacheKey, ips)
+	cache.Cache.Set(cacheKey, ips, time.Hour*2)
 	return ips
 }
 
 func getDockerHostIPUncached() []string {
-	var lastErr error
-
 	for _, attempt := range []struct {
 		name     string
 		provider func() ([]string, error)
@@ -34,9 +35,8 @@ func getDockerHostIPUncached() []string {
 		{"ec2 metadata api", ec2.GetLocalIPv4},
 	} {
 		log.Debugf("attempting to get host ip from source: %s", attempt.name)
-		ips, err = attempt.provider()
+		ips, err := attempt.provider()
 		if err != nil {
-			lastErr = err
 			log.Debugf("could not deduce host IP from source: %s", err)
 		} else {
 			return ips
@@ -46,9 +46,9 @@ func getDockerHostIPUncached() []string {
 }
 
 func getHostIPFFromConfig() ([]string, error) {
-	hostIPs := config.DataDog.GetString("process_agent_config.host_ips")
+	hostIPs := config.Datadog.GetStringSlice("process_agent_config.host_ips")
 	if len(hostIPs) == 0 {
 		return nil, fmt.Errorf("no host IPs configured")
 	}
-	return hostIPs
+	return hostIPs, nil
 }
