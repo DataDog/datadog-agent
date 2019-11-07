2521489291fabd991f8ef289690c2d0651365672
Pedro Lambert <pedro.lambert@datadoghq.com>
2019-11-07 14:55:24 -0500

    Add function for getting Host IP when running in host network mode (#4414)
    * Add `DefaultHostAddrs` function
    
    This function will be used to determine the IP of containers when running on
    host network mode.

--- pkg/util/docker/network.go
+++ pkg/util/docker/network.go
@@ -145,6 +145,60 @@ func resolveDockerNetworks(containerNetworks map[string][]dockerNetwork) {
 
 // DefaultGateway returns the default Docker gateway.
 func DefaultGateway() (net.IP, error) {
+	fields, err := defaultGatewayFields()
+	if err != nil || len(fields) < 3 {
+		return nil, err
+	}
+
+	ipInt, err := strconv.ParseUint(fields[2], 16, 32)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse ip %s from route file: %s", fields[2], err)
+	}
+	ip := make(net.IP, 4)
+	binary.LittleEndian.PutUint32(ip, uint32(ipInt))
+	return ip, nil
+}
+
+// DefaultHostIPs returns the IP addresses bound to the default network interface.
+// The default network interface is the one connected to the network gateway, and it is determined
+// by parsing the routing table file in the proc file system.
+func DefaultHostIPs() ([]string, error) {
+	fields, err := defaultGatewayFields()
+	if err != nil {
+		return nil, err
+	}
+	if len(fields) == 0 {
+		return nil, fmt.Errorf("missing interface information from routing file")
+	}
+	iface, err := net.InterfaceByName(fields[0])
+	if err != nil {
+		return nil, err
+	}
+
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return nil, err
+	}
+
+	ips := make([]string, len(addrs))
+	for i, addr := range addrs {
+		// Translate CIDR blocks into IPs, if necessary
+		ips[i] = strings.Split(addr.String(), "/")[0]
+	}
+
+	return ips, nil
+}
+
+// defaultGatewayFields extracts the fields associated to the interface connected
+// to the network gateway from the linux routing table. As an example, for the given file in /proc/net/routes:
+//
+// Iface   Destination  Gateway   Flags  RefCnt  Use  Metric  Mask      MTU  Window  IRTT
+// enp0s3  00000000     0202000A  0003   0       0    0       00000000  0    0       0
+// enp0s3  0002000A     00000000  0001   0       0    0       00FFFFFF  0    0       0
+//
+// The returned value would be ["enp0s3","00000000","0202000A","0003","0","0","0","00000000","0","0","0"]
+//
+func defaultGatewayFields() ([]string, error) {
 	procRoot := config.Datadog.GetString("proc_root")
 	netRouteFile := filepath.Join(procRoot, "net", "route")
 	f, err := os.Open(netRouteFile)
@@ -158,20 +212,15 @@ func DefaultGateway() (net.IP, error) {
 	}
 	defer f.Close()
 
-	ip := make(net.IP, 4)
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		fields := strings.Fields(scanner.Text())
-		if len(fields) >= 3 && fields[1] == "00000000" {
-			ipInt, err := strconv.ParseUint(fields[2], 16, 32)
-			if err != nil {
-				return nil, fmt.Errorf("unable to parse ip %s, from %s: %s", fields[2], netRouteFile, err)
-			}
-			binary.LittleEndian.PutUint32(ip, uint32(ipInt))
-			break
+		if len(fields) >= 1 && fields[1] == "00000000" {
+			return fields, nil
 		}
 	}
-	return ip, nil
+
+	return nil, fmt.Errorf("couldn't retrieve default gateway information")
 }
 
 // GetAgentContainerNetworkMode provides the network mode of the Agent container
