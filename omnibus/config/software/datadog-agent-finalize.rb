0e2e4332f86cfa8c23963a3f96b43f93ac9e8e4b
Kylian Serrania <kylian.serrania@datadoghq.com>
2019-11-18 20:41:51 +0100

    SUSE sysvinit support (#4436)
    * SUSE sysvinit support
    
    Adds SysVInit service files to support SUSE 11.
    
    * Add release note
    
    * Add comments in package scripts and do not use glob to remove
    
    * Add kitchen tests for SUSE 11
    
    * Indentation and minor consistency changes accross files
    
    * Redundant check removed
    
    * Double hashbang
    
    * Revert "Add kitchen tests for SUSE 11"
    
    This reverts commit 8e5c3779b7b571c71b65ab7de3c5ab08e58eb455.

--- omnibus/config/software/datadog-agent-finalize.rb
+++ omnibus/config/software/datadog-agent-finalize.rb
@@ -78,6 +78,12 @@ build do
                 move "#{install_dir}/scripts/datadog-agent-trace", "/etc/init.d"
                 move "#{install_dir}/scripts/datadog-agent-process", "/etc/init.d"
             end
+            if suse?
+                mkdir "/etc/init.d"
+                move "#{install_dir}/scripts/datadog-agent", "/etc/init.d"
+                move "#{install_dir}/scripts/datadog-agent-trace", "/etc/init.d"
+                move "#{install_dir}/scripts/datadog-agent-process", "/etc/init.d"
+            end
             mkdir systemd_directory
             move "#{install_dir}/scripts/datadog-agent.service", systemd_directory
             move "#{install_dir}/scripts/datadog-agent-trace.service", systemd_directory
