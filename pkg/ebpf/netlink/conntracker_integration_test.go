8961d4ae2c9fb908e3d1a8f2906c58b55315d23b
Lee Avital <lee.avital@datadoghq.com>
2019-11-25 18:09:53 -0500

    Add an integration test for conntracker.go

---
+++ pkg/ebpf/netlink/conntracker_integration_test.go
@@ -0,0 +1,118 @@
+// +build linux_bpf
+
+package netlink
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+	"testing"
+
+	"github.com/DataDog/agent-payload/process"
+	"github.com/DataDog/datadog-agent/pkg/process/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConntracker(t *testing.T) {
+	cmd := exec.Command("testdata/setup_dnat.sh")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Errorf("setup command output: %s", string(out))
+	}
+
+	ct, err := NewConntracker("/proc", 100, 100)
+	require.NoError(t, err)
+	defer ct.Close()
+
+	<-startServerTCP(t, "1.1.1.1:5432")
+	<-startServerUDP(t, net.ParseIP("1.1.1.1"), 5432)
+
+	localAddr := pingTCP(t, "2.2.2.2:5432").(*net.TCPAddr)
+
+	trans := ct.GetTranslationForConn(util.AddressFromNetIP(localAddr.IP), uint16(localAddr.Port), process.ConnectionType_tcp)
+	require.NotNil(t, trans)
+	assert.Equal(t, util.AddressFromString("1.1.1.1"), trans.ReplSrcIP)
+
+	localAddrUDP := pingUDP(t, net.ParseIP("2.2.2.2"), 5432).(*net.UDPAddr)
+	trans = ct.GetTranslationForConn(util.AddressFromNetIP(localAddrUDP.IP), uint16(localAddrUDP.Port), process.ConnectionType_udp)
+	assert.Equal(t, util.AddressFromString("1.1.1.1"), trans.ReplSrcIP)
+
+	defer teardown(t)
+}
+
+func startServerTCP(t *testing.T, addr string) <-chan struct{} {
+	ch := make(chan struct{}, 1)
+
+	l, err := net.Listen("tcp", addr)
+	require.NoError(t, err)
+	go func() {
+		defer l.Close()
+
+		ch <- struct{}{}
+
+		conn, err := l.Accept()
+		require.NoError(t, err)
+
+		conn.Write([]byte("hello"))
+		conn.Close()
+	}()
+
+	return ch
+}
+
+func startServerUDP(t *testing.T, ip net.IP, port int) <-chan struct{} {
+	ch := make(chan struct{}, 1)
+
+	addr := &net.UDPAddr{
+		IP:   ip,
+		Port: port,
+	}
+
+	l, err := net.ListenUDP("udp", addr)
+	require.NoError(t, err)
+	go func() {
+		defer l.Close()
+		ch <- struct{}{}
+
+		bs := make([]byte, 10)
+		_, err := l.Read(bs)
+		require.NoError(t, err)
+	}()
+
+	return ch
+}
+
+func pingTCP(t *testing.T, addr string) net.Addr {
+	conn, err := net.Dial("tcp", addr)
+	require.NoError(t, err)
+
+	_, err = conn.Write([]byte("ping"))
+	require.NoError(t, err)
+	bs := make([]byte, 10)
+	_, err = conn.Read(bs)
+	require.NoError(t, err)
+
+	return conn.LocalAddr()
+}
+
+func pingUDP(t *testing.T, ip net.IP, port int) net.Addr {
+	addr := &net.UDPAddr{
+		IP:   ip,
+		Port: port,
+	}
+	conn, err := net.DialUDP("udp", nil, addr)
+	require.NoError(t, err)
+
+	_, err = conn.Write([]byte("ping"))
+	require.NoError(t, err)
+
+	return conn.LocalAddr()
+}
+
+func teardown(t *testing.T) {
+	cmd := exec.Command("testdata/teardown_dnat.sh")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		fmt.Printf("teardown command output: %s", string(out))
+		t.Errorf("error tearing down: %s", err)
+	}
+}
