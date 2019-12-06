ddbe9f630897e0bab78a4f0b66688af883a93249
Jahanzeb Khan <jahanzeb.khan@datadoghq.com>
2019-12-05 21:08:56 -0500

    [apm-stats] no longer ignoring childless spans for apm sublayer metrics and stats to fix inaccuracies

--- pkg/trace/stats/subtrace.go
+++ pkg/trace/stats/subtrace.go
@@ -70,7 +70,7 @@ func ExtractTopLevelSubtraces(t pb.Trace, root *pb.Span) []Subtrace {
 	for current := next.Pop(); current != nil; current = next.Pop() {
 		// We do not extract subtraces for toplevel spans that have no children
 		// since these are not interresting
-		if traceutil.HasTopLevel(current.Span) && len(childrenMap[current.Span.SpanID]) > 0 {
+		if traceutil.HasTopLevel(current.Span) {
 			current.Ancestors = append(current.Ancestors, current.Span)
 		}
 		visited[current.Span] = true
