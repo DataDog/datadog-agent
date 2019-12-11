2f8b8078e2c231f975a80306e867e56922ffb271
celene <celene@datadoghq.com>
2019-12-11 15:59:46 +0000

    remove unused fn

--- pkg/util/clusteragent/clusteragent.go
+++ pkg/util/clusteragent/clusteragent.go
@@ -375,19 +375,3 @@ func (c *DCAClient) GetKubernetesMetadataNames(nodeName, ns, podName string) ([]
 
 	return metadataNames, nil
 }
-
-func getLocalIP() string {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return ""
-	}
-	for _, address := range addrs {
-		// check the address type and if it is not a loopback the display it
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String()
-			}
-		}
-	}
-	return ""
-}
