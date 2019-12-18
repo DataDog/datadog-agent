ddc1df458b6f75e762cacbc975f69a1bd25df907
Albert Vaca <albert.vaca@datadoghq.com>
2019-12-18 16:56:57 +0100

    install_script: warn if version not specified (#4438)
    * install_script: warn if version not specified
    
    Since we will install Agent 6 by default.
    
    * Print warning in yellow
    
    Co-Authored-By: Olivier Vielpeau <olivielpeau@users.noreply.github.com>

--- cmd/agent/install_script.sh
+++ cmd/agent/install_script.sh
@@ -86,6 +86,8 @@ if [ -n "$DD_AGENT_MAJOR_VERSION" ]; then
     exit 1;
   fi
   dd_agent_major_version=$DD_AGENT_MAJOR_VERSION
+else
+  echo -e "\033[33mWarning: DD_AGENT_MAJOR_VERSION not set. Installing Agent version 6 by default.\033[0m"
 fi
 
 dd_agent_dist_channel=stable
