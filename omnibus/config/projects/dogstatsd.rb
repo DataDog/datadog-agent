be4919760d94e3a2609772c93bd5c9b8a300b0ec
db <derek.brown@datadoghq.com>
2019-12-27 11:42:25 -0800

    More review feedback I missed earlier

--- omnibus/config/projects/dogstatsd.rb
+++ omnibus/config/projects/dogstatsd.rb
@@ -82,14 +82,10 @@ package :msi do
   end
   wix_candle_extension 'WixUtilExtension'
   wix_light_extension 'WixUtilExtension'
-  #extra_package_dir "#{Omnibus::Config.source_dir()}\\etc\\datadog-agent\\extra_package_files"
 
   additional_sign_files [
       "#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\agent\\dogstatsd.exe"
     ]
-  #if ENV['SIGN_WINDOWS']
-  #  signing_identity "ECCDAE36FDCB654D2CBAB3E8975AA55469F96E4C", machine_store: true, algorithm: "SHA256"
-  #end
   if ENV['SIGN_PFX']
     signing_identity_file "#{ENV['SIGN_PFX']}", password: "#{ENV['SIGN_PFX_PW']}", algorithm: "SHA256"
   end
