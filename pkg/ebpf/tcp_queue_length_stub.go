fd6433fdbb27017de4c9a870950af34e654cbb76
Lénaïc Huard <lenaic.huard@datadoghq.com>
2020-01-06 14:06:18 +0100

    fixup! Add stub

--- pkg/ebpf/tcp_queue_length_stub.go
+++ pkg/ebpf/tcp_queue_length_stub.go
@@ -2,18 +2,23 @@
 
 package ebpf
 
+// TCPQueueLengthTracer is not implemented on non-linux systems
 type TCPQueueLengthTracer struct{}
 
+// NewTCPQUeueLengthTracer is not implemented on non-linux systems
 func NewTCPQueueLengthTracer() (*TCPQueueLengthTracer, error) {
 	return nil, ErrNotImplemented
 }
 
+// Close is not implemented on non-linux systems
 func (t *TCPQueueLengthTracer) Close() {}
 
+// Get is not implemented on non-linux systems
 func (t *TCPQueueLengthTracer) Get() []Stats {
 	return nil
 }
 
+// GetAndFlush is not implemented on non-linux systems
 func (t *TCPQueueLengthTracer) GetAndFlush() []Stats {
 	return nil
 }
