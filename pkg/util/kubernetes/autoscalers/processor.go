dadfe2df09c78fd16f60d94e6ed2838927dccd5f
CharlyF <charly@datadoghq.com>
2020-01-13 18:23:48 -0500

    removing sorting for review purposes

--- pkg/util/kubernetes/autoscalers/processor.go
+++ pkg/util/kubernetes/autoscalers/processor.go
@@ -34,12 +34,6 @@ const (
 	maxCharactersPerChunk = 7000
 )
 
-type ByLen []string
-
-func (a ByLen) Len() int           { return len(a) }
-func (a ByLen) Less(i, j int) bool { return len(a[i]) > len(a[j]) }
-func (a ByLen) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-
 type DatadogClient interface {
 	QueryMetrics(from, to int64, query string) ([]datadog.Series, error)
 	GetRateLimitStats() map[string]datadog.RateLimit
@@ -158,9 +152,6 @@ func makeChunks(batch []string) (chunks [][]string) {
 	// uriLenght is used to avoid making a query that goes beyond the maximum URI size.
 	var uriLenght int
 	var tempBucket []string
-
-	// sort the batch by length of the element, from longest to shortest
-	sort.Sort(ByLen(batch))
 
 	for _, val := range batch {
 
