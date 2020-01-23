ae13101f769be59161a1da70ac742e8ede566f51
Olivier Gaça <olivier.gaca@datadoghq.com>
2020-01-23 17:52:24 -0500

    Add a warning for ec2_use_windows_prefix_detection

--- pkg/util/hostname.go
+++ pkg/util/hostname.go
@@ -228,6 +228,11 @@ func GetHostnameData() (HostnameData, error) {
 	// and the hostname is one of the default ones
 	if getEC2Hostname, found := hostname.ProviderCatalog["ec2"]; found {
 		log.Debug("GetHostname trying EC2 metadata...")
+		if !ecs.IsECSInstance() && ec2.IsWindowsDefaultHostname(hostName) && !config.Datadog.GetBool("ec2_use_windows_prefix_detection") {
+			// REMOVEME: This should be removed when the default `ec2_use_windows_prefix_detection` is set to true
+			log.Warn("You may want to use the EC2 instance-id for the intake hostname." +
+				" For more information: https://dtdg.co/ec2-use-win-prefix-detection")
+		}
 		if ecs.IsECSInstance() || ec2.IsDefaultHostname(hostName) {
 			instanceID, err := getEC2Hostname()
 			if err == nil {
