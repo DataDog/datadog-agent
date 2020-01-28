054c9743dba0e0dff9c4e02634fa4c76d64dbe58
Remy Mathieu <remy.mathieu@datadoghq.com>
2020-01-28 10:18:45 +0100

    omnibus/python3: update wheel dep to 0.34.1

--- omnibus/config/software/datadog-agent-integrations-py3.rb
+++ omnibus/config/software/datadog-agent-integrations-py3.rb
@@ -100,7 +100,7 @@ build do
     # Prepare the build env, these dependencies are only needed to build and
     # install the core integrations.
     #
-    command "#{pip} install wheel==0.30.0"
+    command "#{pip} install wheel==0.34.1"
     command "#{pip} install pip-tools==4.2.0"
     uninstall_buildtime_deps = ['rtloader', 'click', 'first', 'pip-tools']
     nix_build_env = {
