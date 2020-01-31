5957d8ef5a081091f3aca5225830a8795d3436c2
Lénaïc Huard <lenaic.huard@datadoghq.com>
2020-01-31 07:12:39 +0100

    Fix missing zlib dependency for puppy

--- omnibus/config/software/libelf.rb
+++ omnibus/config/software/libelf.rb
@@ -11,6 +11,8 @@ version '0.178' do
          sha512: '356656ad0db8f6877b461de1a11280de16a9cc5d8dde4381a938a212e828e32755135e5e3171d311c4c9297b728fbd98123048e2e8fbf7fe7de68976a2daabe5'
 end
 
+dependency 'zlib'
+
 relative_path "elfutils-#{version}"
 
 build do
