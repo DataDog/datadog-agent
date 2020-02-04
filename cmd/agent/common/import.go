9e8614a3e5b460f24f819ea972be7554ecfe7333
Pierre Rognant <pierre.rognant@datadoghq.com>
2020-02-04 11:00:48 +0100

    Make the relocation more generic/reusable

--- cmd/agent/common/import.go
+++ cmd/agent/common/import.go
@@ -9,7 +9,6 @@ package common
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"os/user"
@@ -23,6 +22,9 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/config/legacy"
 )
+
+// TransformationFunc type represents transformation applicable to byte slices
+type TransformationFunc func(rawData []byte) ([]byte, error)
 
 // ImportConfig imports the agent5 configuration into the agent6 yaml config
 func ImportConfig(oldConfigDir string, newConfigDir string, force bool) error {
@@ -111,6 +113,8 @@ func ImportConfig(oldConfigDir string, newConfigDir string, force bool) error {
 		}
 	}
 
+	tr := []TransformationFunc{relocateMinCollectionInterval}
+
 	for _, f := range files {
 		if f.IsDir() || filepath.Ext(f.Name()) != cfgExt {
 			continue
@@ -141,11 +145,8 @@ func ImportConfig(oldConfigDir string, newConfigDir string, force bool) error {
 			continue
 		}
 
-		if err := copyFile(src, dst, force); err != nil {
+		if err := copyFile(src, dst, force, tr); err != nil {
 			return fmt.Errorf("unable to copy %s to %s: %v", src, dst, err)
-		}
-		if err := relocateMinCollectionIntervalInFile(dst); err != nil {
-			return fmt.Errorf("error %v", err)
 		}
 
 		fmt.Fprintln(
@@ -179,7 +180,7 @@ func ImportConfig(oldConfigDir string, newConfigDir string, force bool) error {
 		src := filepath.Join(oldConfigDir, "conf.d", "auto_conf", f.Name())
 		dst := filepath.Join(newConfigDir, "conf.d", checkName+dirExt, "auto_conf"+cfgExt)
 
-		if err := copyFile(src, dst, force); err != nil {
+		if err := copyFile(src, dst, force, tr); err != nil {
 			fmt.Fprintf(os.Stderr, "unable to copy %s to %s: %v\n", src, dst, err)
 			continue
 		}
@@ -218,8 +219,8 @@ func ImportConfig(oldConfigDir string, newConfigDir string, force bool) error {
 	return nil
 }
 
-// Copy the src file to dst. File attributes won't be copied.
-func copyFile(src, dst string, overwrite bool) error {
+// Copy the src file to dst. File attributes won't be copied. Apply all TransformationFunc while copying.
+func copyFile(src, dst string, overwrite bool, transformations []TransformationFunc) error {
 	// if the file exists check whether we can overwrite
 	if _, err := os.Stat(dst); !os.IsNotExist(err) {
 		if overwrite {
@@ -233,28 +234,25 @@ func copyFile(src, dst string, overwrite bool) error {
 		}
 	}
 
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
 	// Create necessary destination directories
-	err = os.MkdirAll(filepath.Dir(dst), 0750)
+	err := os.MkdirAll(filepath.Dir(dst), 0750)
 	if err != nil {
 		return err
 	}
 
-	out, err := os.Create(dst)
+	data, err := ioutil.ReadFile(src)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to read file %s : %s", src, err)
 	}
-	defer out.Close()
 
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return err
+	for _, transformation := range transformations {
+		data, err = transformation(data)
+		if err != nil {
+			return fmt.Errorf("unable to convert file %s : %s", src, err)
+		}
 	}
+
+	ioutil.WriteFile(dst, data, 0640)
 
 	ddGroup, errGroup := user.LookupGroup("dd-agent")
 	ddUser, errUser := user.LookupId("dd-agent")
@@ -272,18 +270,18 @@ func copyFile(src, dst string, overwrite bool) error {
 			return fmt.Errorf("Couldn't convert dd-agent user ID: %s into an int: %s", ddUser.Uid, err)
 		}
 
-		err = out.Chown(ddUID, ddGID)
+		err = os.Chown(dst, ddUID, ddGID)
 		if err != nil {
 			return fmt.Errorf("Couldn't change the file permissions for this check. Error: %s", err)
 		}
 	}
 
-	err = out.Chmod(0640)
+	err = os.Chmod(dst, 0640)
 	if err != nil {
 		return err
 	}
 
-	return out.Close()
+	return nil
 }
 
 // configTraceAgent extracts trace-agent specific info and dump to its own config file
@@ -302,18 +300,6 @@ func configTraceAgent(datadogConfPath, traceAgentConfPath string, overwrite bool
 	}
 
 	return legacy.ImportTraceAgentConfig(datadogConfPath, traceAgentConfPath)
-}
-
-func relocateMinCollectionIntervalInFile(file string) error {
-	data, err := ioutil.ReadFile(file)
-	if err != nil {
-		return err
-	}
-	output, err := relocateMinCollectionInterval(data)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(file, output, 0640)
 }
 
 func relocateMinCollectionInterval(rawData []byte) ([]byte, error) {
