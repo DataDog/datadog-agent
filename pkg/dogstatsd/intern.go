11be9fc737ff0a167703732565be8677339f4908
Remy Mathieu <remy.mathieu@datadoghq.com>
2020-02-05 16:49:48 +0100

    dogstatsd/intern: comments.

--- pkg/dogstatsd/intern.go
+++ pkg/dogstatsd/intern.go
@@ -1,5 +1,8 @@
 package dogstatsd
 
+// stringInterner is a string cache providing a longer life for strings,
+// helping to avoid GC runs because they're re-used many times instead of
+// created every time.
 type stringInterner struct {
 	strings map[string]string
 	maxSize int
@@ -12,6 +15,10 @@ func newStringInterner(maxSize int) *stringInterner {
 	}
 }
 
+// LoadOrStore always returns the string from the cache, adding it into the
+// cache if needed.
+// If we need to store a new entry and the cache is at its maximum capacity,
+// it is reset.
 func (i *stringInterner) LoadOrStore(key []byte) string {
 	if s, found := i.strings[string(key)]; found {
 		return s
