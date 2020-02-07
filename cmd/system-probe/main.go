43b83bf3901bc815099e4b1320f98fed5cc97947
Spencer Brown <spencer.brown@datadoghq.com>
2020-02-07 17:40:56 -0500

    Add windows pipe and reset filepath for linux

--- cmd/system-probe/main.go
+++ cmd/system-probe/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	// Parse flags
-	flag.StringVar(&opts.configPath, "config", "c:\\programdata\\datadog\\system-probe.yaml", "Path to system-probe config formatted as YAML")
+	flag.StringVar(&opts.configPath, "config", "/etc/datadog-agent/system-probe.yaml", "Path to system-probe config formatted as YAML")
 	flag.StringVar(&opts.pidFilePath, "pid", "", "Path to set pidfile for process")
 	flag.BoolVar(&opts.version, "version", false, "Print the version and exit")
 
