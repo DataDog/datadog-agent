DataDog/datadog-agent

commit ae3bb2291f7a0223bbc7c6ec87f914f8c976d956

Make the log agent retry to connect to docker rather than giving up (#4536)
This is to handle the case where the agent is started while the docker daemon is stopped
and the docker daemon is started later while the datadog agent is already running.

pkg/logs/input/container/launcher.go
@@ -21,7 +21,8 @@ import (
 // By default returns a docker launcher if the docker socket is mounted and fallback to
 // a kubernetes launcher if '/var/log/pods' is mounted ; this behaviour is reversed when
 // collectFromFiles is enabled.
-// Returns a noop launcher if none of those volumes are mounted.
+// If none of those volumes are mounted, returns a lazy docker launcher with a retrier to handle the cases
+// where docker is started after the agent.
 func NewLauncher(collectAll bool, collectFromFiles bool, sources *config.LogSources, services *service.Services, pipelineProvider pipeline.Provider, registry auditor.Registry) restart.Restartable {
 	var (
 		launcher restart.Restartable
@@ -36,14 +37,14 @@ func NewLauncher(collectAll bool, collectFromFiles bool, sources *config.LogSour
 		}
 		log.Infof("Could not setup the kubernetes launcher: %v", err)
 
-		launcher, err = docker.NewLauncher(sources, services, pipelineProvider, registry)
+		launcher, err = docker.NewLauncher(sources, services, pipelineProvider, registry, false)
 		if err == nil {
 			log.Info("Docker launcher initialized")
 			return launcher
 		}
 		log.Infof("Could not setup the docker launcher: %v", err)
 	} else {
-		launcher, err = docker.NewLauncher(sources, services, pipelineProvider, registry)
+		launcher, err = docker.NewLauncher(sources, services, pipelineProvider, registry, false)
 		if err == nil {
 			log.Info("Docker launcher initialized")
 			return launcher
@@ -58,7 +59,13 @@ func NewLauncher(collectAll bool, collectFromFiles bool, sources *config.LogSour
 		log.Infof("Could not setup the kubernetes launcher: %v", err)
 	}
 
-	log.Infof("Container logs won't be collected")
+	launcher, err = docker.NewLauncher(sources, services, pipelineProvider, registry, true)
+	if err != nil {
+		log.Warnf("Could not setup the docker launcher: %v. Will not be able to collect container logs", err)
+		return nil
+	}
 
-	return NewNoopLauncher()
+	log.Infof("Container logs won't be collected unless a docker daemon is eventually started")
+
+	return launcher
 }

pkg/logs/input/container/noop.go
@@ -1,24 +1,0 @@
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License Version 2.0.
-// This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2019 Datadog, Inc.
-
-package container
-
-import (
-	"github.com/DataDog/datadog-agent/pkg/logs/restart"
-)
-
-// noopLauncher does nothing.
-type noopLauncher struct{}
-
-// NewNoopLauncher returns a new noopLauncher.
-func NewNoopLauncher() restart.Restartable {
-	return &noopLauncher{}
-}
-
-// Start does nothing.
-func (l *noopLauncher) Start() {}
-
-// Stop does nothing.
-func (l *noopLauncher) Stop() {}

pkg/logs/input/docker/launcher.go
@@ -8,6 +8,7 @@
 package docker
 
 import (
+	"math"
 	"sync"
 	"time"
 
@@ -20,6 +21,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/logs/service"
 	"github.com/DataDog/datadog-agent/pkg/tagger"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/pkg/util/retry"
 )
 
 const (
@@ -29,6 +31,7 @@ const (
 
 // A Launcher starts and stops new tailers for every new containers discovered by autodiscovery.
 type Launcher struct {
+	initRetry          retry.Retrier
 	pipelineProvider   pipeline.Provider
 	addedSources       chan *config.LogSource
 	removedSources     chan *config.LogSource
@@ -43,10 +46,11 @@ type Launcher struct {
 	erroredContainerID chan string
 	lock               *sync.Mutex
 	collectAllSource   *config.LogSource
+	dockerRetrierStop  chan struct{}
 }
 
 // NewLauncher returns a new launcher
-func NewLauncher(sources *config.LogSources, services *service.Services, pipelineProvider pipeline.Provider, registry auditor.Registry) (*Launcher, error) {
+func NewLauncher(sources *config.LogSources, services *service.Services, pipelineProvider pipeline.Provider, registry auditor.Registry, shouldRetry bool) (*Launcher, error) {
 	launcher := &Launcher{
 		pipelineProvider:   pipelineProvider,
 		tailers:            make(map[string]*Tailer),
@@ -55,13 +59,13 @@ func NewLauncher(sources *config.LogSources, services *service.Services, pipelin
 		stop:               make(chan struct{}),
 		erroredContainerID: make(chan string),
 		lock:               &sync.Mutex{},
+		dockerRetrierStop:  make(chan struct{}, 1),
 	}
-	err := launcher.setup()
-	if err != nil {
+
+	if err := launcher.retrySetupInBackground(shouldRetry); err != nil {
 		return nil, err
 	}
-	// Sources and services are added after the setup to avoid creating
-	// a channel that will lock the scheduler in case of setup failure
+
 	// FIXME(achntrl): Find a better way of choosing the right launcher
 	// between Docker and Kubernetes
 	launcher.addedSources = sources.GetAddedForType(config.DockerType)
@@ -85,6 +89,39 @@ func (l *Launcher) setup() error {
 	return nil
 }
 
+func (l *Launcher) retrySetupInBackground(shouldRetry bool) error {
+	var retryStrategy retry.Strategy
+	if shouldRetry {
+		retryStrategy = retry.RetryCount
+	} else {
+		retryStrategy = retry.OneTry
+	}
+	l.initRetry.SetupRetrier(&retry.Config{
+		Name:          "docker Launcher setup",
+		AttemptMethod: func() error { return l.setup() },
+		Strategy:      retryStrategy,
+		RetryCount:    math.MaxInt32,
+		RetryDelay:    30 * time.Second,
+	})
+
+	if err := l.initRetry.TriggerRetry(); err != nil && err.RetryStatus == retry.PermaFail {
+		return err
+	}
+
+	go func() {
+		for l.initRetry.RetryStatus() == retry.FailWillRetry {
+			select {
+			case <-time.After(time.Until(l.initRetry.NextRetry())):
+				l.initRetry.TriggerRetry()
+			case <-l.dockerRetrierStop:
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
 // Start starts the Launcher
 func (l *Launcher) Start() {
 	go l.run()
@@ -93,6 +130,7 @@ func (l *Launcher) Start() {
 // Stop stops the Launcher and its tailers in parallel,
 // this call returns only when all the tailers are stopped.
 func (l *Launcher) Stop() {
+	l.dockerRetrierStop <- struct{}{}
 	l.stop <- struct{}{}
 	stopper := restart.NewParallelStopper()
 	for _, tailer := range l.tailers {

pkg/logs/input/docker/launcher_nodocker.go
@@ -18,7 +18,7 @@ import (
 type Launcher struct{}
 
 // NewLauncher returns a new Launcher
-func NewLauncher(sources *config.LogSources, services *service.Services, pipelineProvider pipeline.Provider, registry auditor.Registry) (*Launcher, error) {
+func NewLauncher(sources *config.LogSources, services *service.Services, pipelineProvider pipeline.Provider, registry auditor.Registry, shouldRetry bool) (*Launcher, error) {
 	return &Launcher{}, nil
 }
 

releasenotes/notes/collect_logs_docker_started_after_agent-7ab9653c7d0e9f08.yaml
@@ -1,0 +1,12 @@
+# Each section from every releasenote are combined when the
+# CHANGELOG.rst is rendered. So the text needs to be worded so that
+# it does not depend on any information only available in another
+# section. This may mean repeating some details, but each section
+# must be readable independently of the other.
+#
+# Each section note must be formatted as reStructuredText.
+---
+fixes:
+  - |
+    Make the log agent continuously retry to connect to docker rather than giving up when docker is not running when the agent is started.
+    This is to handle the case where the agent is started while the docker daemon is stopped and the docker daemon is started later while the datadog agent is already running.

commit b95977bf74ad79e36246e4b9327e12fd128760d8

Fix panic in the agent when log collection is enabled on ECS Fargate (#4839)
Commit ae3bb229 introduced a potential panic in the agent:
```
panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x60 pc=0x1a7453c]
goroutine 158 [running]:
github.com/DataDog/datadog-agent/vendor/github.com/docker/docker/client.(*Client).ContainerList(0x0, 0x3b05100, 0xc0000c4010, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xc0004a70e0, ...)
	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/vendor/github.com/docker/docker/client/container_list.go:38 +0x38c
github.com/DataDog/datadog-agent/pkg/logs/input/docker.GetContainer(0x0, 0xc0002f2d29, 0x40, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, ...)
	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/logs/input/docker/client.go:72 +0x19f
github.com/DataDog/datadog-agent/pkg/logs/input/docker.(*Launcher).run(0xc0004905a0)
	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/logs/input/docker/launcher.go:150 +0x738
created by github.com/DataDog/datadog-agent/pkg/logs/input/docker.(*Launcher).Start
	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/logs/input/docker/launcher.go:127 +0x3f
AGENT EXITED WITH CODE 2, SIGNAL 0, KILLING CONTAINER
```

The initial goal of this commit was to allow the datadog agent to start while
the docker daemon was not reachable and then to reconnect to docker as soon as
it becomes available in order to collect docker container logs without having
to restart the agent.

This was done by allowing the docker log input processor to start even if the
initial attempt to connect to the docker daemon failed. New attempts to connect
to docker were regularly done in background until it succeeds.

A nil pointer dereference on `l.cli` could happen in

pkg/logs/input/container/launcher.go
@@ -62,7 +62,7 @@ func NewLauncher(collectAll bool, collectFromFiles bool, sources *config.LogSour
 	launcher, err = docker.NewLauncher(sources, services, pipelineProvider, registry, true)
 	if err != nil {
 		log.Warnf("Could not setup the docker launcher: %v. Will not be able to collect container logs", err)
-		return nil
+		return NewNoopLauncher()
 	}
 
 	log.Infof("Container logs won't be collected unless a docker daemon is eventually started")

pkg/logs/input/container/noop.go
@@ -1,0 +1,24 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package container
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/logs/restart"
+)
+
+// noopLauncher does nothing.
+type noopLauncher struct{}
+
+// NewNoopLauncher returns a new noopLauncher.
+func NewNoopLauncher() restart.Restartable {
+	return &noopLauncher{}
+}
+
+// Start does nothing.
+func (l *noopLauncher) Start() {}
+
+// Stop does nothing.
+func (l *noopLauncher) Stop() {}

pkg/logs/input/docker/launcher.go
@@ -31,7 +31,7 @@ const (
 
 // A Launcher starts and stops new tailers for every new containers discovered by autodiscovery.
 type Launcher struct {
-	initRetry          retry.Retrier
+	setupRetrier       retry.Retrier
 	pipelineProvider   pipeline.Provider
 	addedSources       chan *config.LogSource
 	removedSources     chan *config.LogSource
@@ -46,7 +46,6 @@ type Launcher struct {
 	erroredContainerID chan string
 	lock               *sync.Mutex
 	collectAllSource   *config.LogSource
-	dockerRetrierStop  chan struct{}
 }
 
 // NewLauncher returns a new launcher
@@ -59,10 +58,9 @@ func NewLauncher(sources *config.LogSources, services *service.Services, pipelin
 		stop:               make(chan struct{}),
 		erroredContainerID: make(chan string),
 		lock:               &sync.Mutex{},
-		dockerRetrierStop:  make(chan struct{}, 1),
 	}
 
-	if err := launcher.retrySetupInBackground(shouldRetry); err != nil {
+	if err := launcher.retrySetup(shouldRetry); err != nil {
 		return nil, err
 	}
 
@@ -73,6 +71,15 @@ func NewLauncher(sources *config.LogSources, services *service.Services, pipelin
 	launcher.addedServices = services.GetAddedServicesForType(config.DockerType)
 	launcher.removedServices = services.GetRemovedServicesForType(config.DockerType)
 	return launcher, nil
+}
+
+func (l *Launcher) getCli() (*client.Client, error) {
+	if err := l.setupRetrier.TriggerRetry(); err != nil {
+		log.Debugf("docker setup error: %v", err)
+		return nil, err
+	}
+
+	return l.cli, nil
 }
 
 // setup initializes the docker client and the tagger,
@@ -89,35 +96,28 @@ func (l *Launcher) setup() error {
 	return nil
 }
 
-func (l *Launcher) retrySetupInBackground(shouldRetry bool) error {
+func (l *Launcher) retrySetup(shouldRetry bool) error {
 	var retryStrategy retry.Strategy
 	if shouldRetry {
 		retryStrategy = retry.RetryCount
 	} else {
 		retryStrategy = retry.OneTry
 	}
-	l.initRetry.SetupRetrier(&retry.Config{
+	l.setupRetrier.SetupRetrier(&retry.Config{
 		Name:          "docker Launcher setup",
 		AttemptMethod: func() error { return l.setup() },
 		Strategy:      retryStrategy,
 		RetryCount:    math.MaxInt32,
-		RetryDelay:    30 * time.Second,
+		// We use a low RetryDelay value here because retries will be attempted on-demand
+		// inside Launcher.run() when a new service or source is received and these
+		// events are generated by auto-discovery form the informations gathered from docker
+		// So, if such events are generated, we will most probably succeed in connecting to docker.
+		RetryDelay: 1 * time.Second,
 	})
 
-	if err := l.initRetry.TriggerRetry(); err != nil && err.RetryStatus == retry.PermaFail {
+	if err := l.setupRetrier.TriggerRetry(); err != nil && err.RetryStatus == retry.PermaFail {
 		return err
 	}
-
-	go func() {
-		for l.initRetry.RetryStatus() == retry.FailWillRetry {
-			select {
-			case <-time.After(time.Until(l.initRetry.NextRetry())):
-				l.initRetry.TriggerRetry()
-			case <-l.dockerRetrierStop:
-				return
-			}
-		}
-	}()
 
 	return nil
 }
@@ -130,7 +130,6 @@ func (l *Launcher) Start() {
 // Stop stops the Launcher and its tailers in parallel,
 // this call returns only when all the tailers are stopped.
 func (l *Launcher) Stop() {
-	l.dockerRetrierStop <- struct{}{}
 	l.stop <- struct{}{}
 	stopper := restart.NewParallelStopper()
 	for _, tailer := range l.tailers {
@@ -147,7 +146,12 @@ func (l *Launcher) run() {
 		select {
 		case service := <-l.addedServices:
 			// detected a new container running on the host,
-			dockerContainer, err := GetContainer(l.cli, service.Identifier)
+			cli, err := l.getCli()
+			if err != nil {
+				log.Warnf("Could not use docker client, logs for container %s won’t be collected: %v", service.Identifier, err)
+				continue
+			}
+			dockerContainer, err := GetContainer(cli, service.Identifier)
 			if err != nil {
 				log.Warnf("Could not find container with id: %v", err)
 				continue
@@ -239,7 +243,12 @@ func (l *Launcher) startTailer(container *Container, source *config.LogSource) {
 
 	// overridenSource == source if the containerCollectAll option is not activated or the container has AD labels
 	overridenSource := l.overrideSource(container, source)
-	tailer := NewTailer(l.cli, containerID, overridenSource, l.pipelineProvider.NextPipelineChan(), l.erroredContainerID)
+	cli, err := l.getCli()
+	if err != nil {
+		log.Warnf("Could not use docker client, logs for container %s won’t be collected: %v", containerID, err)
+		return
+	}
+	tailer := NewTailer(cli, containerID, overridenSource, l.pipelineProvider.NextPipelineChan(), l.erroredContainerID)
 
 	// compute the offset to prevent from missing or duplicating logs
 	since, err := Since(l.registry, tailer.Identifier(), container.service.CreationTime)
@@ -286,7 +295,14 @@ func (l *Launcher) restartTailer(containerID string) {
 		l.removeTailer(containerID)
 	}
 
-	tailer := NewTailer(l.cli, containerID, source, l.pipelineProvider.NextPipelineChan(), l.erroredContainerID)
+	cli, err := l.getCli()
+	if err != nil {
+		// This cannot happen since, if we have a tailer to restart, it means that we created
+		// it earlier and we couldn't have created it if the docker client wasn't initialized.
+		log.Warnf("Could not use docker client, logs for container %s won’t be collected: %v", containerID, err)
+		return
+	}
+	tailer := NewTailer(cli, containerID, source, l.pipelineProvider.NextPipelineChan(), l.erroredContainerID)
 
 	// compute the offset to prevent from missing or duplicating logs
 	since, err := Since(l.registry, tailer.Identifier(), service.Before)

releasenotes/notes/fix_panic_in_docker_logs_collection-39b10045685fde2c.yaml
@@ -1,0 +1,14 @@
+# Each section from every releasenote are combined when the
+# CHANGELOG.rst is rendered. So the text needs to be worded so that
+# it does not depend on any information only available in another
+# section. This may mean repeating some details, but each section
+# must be readable independently of the other.
+#
+# Each section note must be formatted as reStructuredText.
+---
+fixes:
+  - |
+    Fix a panic in the log agent when the auto-discovery reports new containers to monitor
+    and the agent fails to connect to the docker daemon.
+    The main setup where this happened is on ECS Fargate where the ECS auto-discovery is watching
+    for new containers and the docker socket is not available from the datadog agent.