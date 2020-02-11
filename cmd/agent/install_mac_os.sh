a58f3b0f834a73f46a9d87449c6626142efb7b89
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-02-11 10:37:26 +0100

    Revert change to the install script

--- cmd/agent/install_mac_os.sh
+++ cmd/agent/install_mac_os.sh
@@ -7,7 +7,7 @@
 set -e
 logfile=ddagent-install.log
 dmg_file=/tmp/datadog-agent.dmg
-dmg_base_url="https://dd-agent.s3.amazonaws.com"
+dmg_base_url="https://s3.amazonaws.com/dd-agent"
 
 dd_upgrade=
 if [ -n "$DD_UPGRADE" ]; then
