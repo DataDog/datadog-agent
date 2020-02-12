bb7faec5ca9f5f37a197a4bb28595e2722afeba5
Spencer Brown <spencer.brown@datadoghq.com>
2020-02-12 12:00:22 -0500

    Finalize setup of TCP server running

--- pkg/process/net/network_windows.go
+++ pkg/process/net/network_windows.go
@@ -1,16 +1,3 @@
 // +build windows
 
 package net
-
-import (
-	"net"
-
-	"github.com/Datadog/datadog-agent/pkg/process/config"
-)
-
-func NewListener(cfg *config.AgentConfig) (*net.Listener, error) {
-	conn, err := net.Listen("tcp", "localhost:3333")
-	defer conn.Close()
-
-	return conn
-}
--- pkg/process/net/windows_pipe.go
+++ pkg/process/net/windows_pipe.go
@@ -4,6 +4,8 @@ package net
 
 import (
 	"net"
+	"github.com/DataDog/datadog-agent/pkg/process/config"
+
 )
 
 // UDSListener (Unix Domain Socket Listener)
@@ -12,6 +14,16 @@ type WindowsPipeListener struct {
 	pipePath string
 }
 
-func NewPipeListener(cfg *config.AgentConfig) (*WindowsPipeListener, error) {
+func NewListener(cfg *config.AgentConfig) (*WindowsPipeListener, error) {
+	l, err := net.Listen("tcp", "localhost:3333")
+	return &WindowsPipeListener{l, "path"}, err
+}
 
+func (wp *WindowsPipeListener) GetListener() net.Listener {
+	return wp.conn
+}
+
+// Stop closes the UDSListener connection and stops listening
+func (wp *WindowsPipeListener) Stop() {
+	wp.conn.Close()
 }
