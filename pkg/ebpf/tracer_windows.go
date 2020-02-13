3fb8c47849b8fe6903aa192dd4d877da7748139c
Spencer Brown <spencer.brown@datadoghq.com>
2020-02-13 11:10:43 -0500

    Have windows tracer return mock connection

--- pkg/ebpf/tracer_windows.go
+++ pkg/ebpf/tracer_windows.go
@@ -3,6 +3,7 @@
 package ebpf
 
 import (
+	"github.com/DataDog/datadog-agent/pkg/process/util"
 	"sync"
 )
 
@@ -46,7 +47,27 @@ func NewTracer(config *Config) (*Tracer, error) {
 func (t *Tracer) Stop() {}
 
 func (t *Tracer) GetActiveConnections(_ string) (*Connections, error) {
-	return nil, ErrNotImplemented
+	return &Connections{
+		DNS: map[util.Address][]string{
+			util.AddressFromString("127.0.0.1"): []string{"localhost"},
+		},
+		Conns: []ConnectionStats{
+			ConnectionStats{
+				Source: util.AddressFromString("127.0.0.1"),
+				Dest:   util.AddressFromString("127.0.0.1"),
+				SPort:  35673,
+				DPort:  8000,
+				Type:   TCP,
+			},
+		},
+	}, nil
+}
+
+// getConnections returns all of the active connections in the ebpf maps along with the latest timestamp.  It takes
+// a reusable buffer for appending the active connections so that this doesn't continuously allocate
+func (t *Tracer) getConnections(active []ConnectionStats) ([]ConnectionStats, uint64, error) {
+	return nil, 0, ErrNotImplemented
+
 }
 
 func (t *Tracer) GetStats() (map[string]interface{}, error) {
