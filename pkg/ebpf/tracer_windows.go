bad84733ae37b842f21808ede81681495d5f07e5
Spencer Brown <spencer.brown@datadoghq.com>
2020-02-14 13:59:39 -0500

    Save various tracer_windows file changes

--- pkg/ebpf/tracer_windows.go
+++ pkg/ebpf/tracer_windows.go
@@ -3,11 +3,44 @@
 package ebpf
 
 import (
+	"C"
 	"github.com/DataDog/datadog-agent/pkg/process/util"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"sync"
+	"syscall"
+	"os"
+	"unsafe"
+)
+
+/*
+#cgo LDFLAGS: -luser32
+#include <windows.h>
+#include <stdint.h>
+*/
+var (
+	kernel32    = syscall.MustLoadDLL("kernel32.dll")
+	CreateFile  = kernel32.MustFindProc("CreateFileW")
+	CloseHandle = kernel32.MustFindProc("CloseHandle")
 )
 
 type Tracer struct {
 	config *Config
+}
+
+type I2c_client struct {
+	f *os.File
+}
+
+func Open(fname string) (c *I2c_client, err error) {
+	f, err := os.OpenFile(fname , syscall.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return &I2c_client{f: f}, nil
+}
+
+func (c *I2c_client) Close() (err error) {
+	return c.f.Close()
 }
 
 func NewTracer(config *Config) (*Tracer, error) {
@@ -16,7 +49,81 @@ func NewTracer(config *Config) (*Tracer, error) {
 
 func (t *Tracer) Stop() {}
 
+
+/*
+WINDOWS:
+CreateFileW(
+  LPCWSTR               lpFileName,
+  DWORD                 dwDesiredAccess,
+  DWORD                 dwShareMode,
+  LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+  DWORD                 dwCreationDisposition,
+  DWORD                 dwFlagsAndAttributes,
+  HANDLE                hTemplateFile
+);
+*/
+
+func open(path string) (syscall.Handle, error) {
+	p, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return syscall.InvalidHandle, err
+	}
+	r, _, err := CreateFile.Call(uintptr(unsafe.Pointer(p)),
+		syscall.GENERIC_READ,
+		syscall.FILE_SHARE_READ|syscall.FILE_SHARE_WRITE,
+		0,
+		syscall.OPEN_EXISTING,
+		syscall.FILE_ATTRIBUTE_NORMAL,
+		0)
+	h := syscall.Handle(r)
+	if h == syscall.InvalidHandle {
+		return h, err
+	}
+	return h, nil
+}
+
+func close(handle syscall.Handle) error {
+	r, _, err := CloseHandle.Call(uintptr(handle))
+	if r == 0 {
+		return err
+	}
+	return nil
+}
+
+/*
+func Encode(s string) C.LPCWSTR {
+	wstr := utf16.Encode([]rune(s))
+
+	p := C.calloc(C.size_t(len(wstr)+1), C.sizeof_uint16_t)
+	pp := (*[1 << 30]uint16)(p)
+	copy(pp[:], wstr)
+
+	return (C.LPCWSTR)(p)
+}*/
+
 func (t *Tracer) GetActiveConnections(_ string) (*Connections, error) {
+
+	// p, err := Encode("\\\\.\\ddfilter") // Note the subtle change here
+	blah := C.ulong(3)
+	log.Infof("%d", blah)
+
+	h, err := open("\\\\.\\ddfilter")
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		log.Errorf("open: %v", err)
+	}
+	err = close(h)
+	if err != nil {
+		log.Errorf("close: %v", err)
+	}
+	err = close(h)
+	if err != nil {
+		log.Errorf("second close: %v", err)
+	}
+
 	return &Connections{
 		DNS: map[util.Address][]string{
 			util.AddressFromString("127.0.0.1"): []string{"localhost"},
