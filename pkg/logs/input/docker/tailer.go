82d867158f1cf2c8bdd6da52efb889a42ddd5f6c
Lénaïc Huard <L3n41c@users.noreply.github.com>
2020-02-19 08:20:25 +0100

    Make the docker log processor use the global DockerUtil rather than its own docker client (#4852)
    The package `pkg/logs/input/docker` used to have its own docker client.
    This could create some unwanted behaviour in some edge cases.
    
    One example is if the docker client used by the docker auto-discovery listener
    is successfully connected to the docker daemon, it can start sending "new
    docker source" and "new docker service" events to the log package.
    If the docker client used by the log package is not connected to the docker
    daemon, the "new docker source" and "new docker service" will not be properly
    processed.
    
    This change removes the docker client of the log package so that there's a
    single docker client.
    This will fix the behaviour in the above described edge case.

--- pkg/logs/input/docker/tailer.go
+++ pkg/logs/input/docker/tailer.go
@@ -24,7 +24,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/logs/tag"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
 )
 
 const defaultSleepDuration = 1 * time.Second
@@ -42,7 +41,7 @@ type Tailer struct {
 	outputChan  chan *message.Message
 	decoder     *decoder.Decoder
 	reader      *safeReader
-	cli         dockerContainerLogInterface
+	dockerutil  dockerContainerLogInterface
 	source      *config.LogSource
 	tagProvider tag.Provider
 
@@ -58,14 +57,14 @@ type Tailer struct {
 }
 
 // NewTailer returns a new Tailer
-func NewTailer(cli *client.Client, containerID string, source *config.LogSource, outputChan chan *message.Message, erroredContainerID chan string) *Tailer {
+func NewTailer(cli *dockerutil.DockerUtil, containerID string, source *config.LogSource, outputChan chan *message.Message, erroredContainerID chan string) *Tailer {
 	return &Tailer{
 		ContainerID:        containerID,
 		outputChan:         outputChan,
 		decoder:            InitializeDecoder(source, containerID),
 		source:             source,
 		tagProvider:        tag.NewProvider(dockerutil.ContainerIDToTaggerEntityName(containerID)),
-		cli:                cli,
+		dockerutil:         cli,
 		readTimeout:        defaultReadTimeout,
 		sleepDuration:      defaultSleepDuration,
 		stop:               make(chan struct{}, 1),
@@ -135,7 +134,7 @@ func (t *Tailer) setupReader() error {
 		Since:      t.getLastSince(),
 	}
 	ctx, cancelFunc := context.WithCancel(context.Background())
-	reader, err := t.cli.ContainerLogs(ctx, t.ContainerID, options)
+	reader, err := t.dockerutil.ContainerLogs(ctx, t.ContainerID, options)
 	t.reader.setUnsafeReader(reader)
 	t.cancelFunc = cancelFunc
 
