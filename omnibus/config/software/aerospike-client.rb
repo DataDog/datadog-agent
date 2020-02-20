ab0892f2bd808ece814c167ae4137a14345376de
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-02-20 18:50:32 +0100

    [omnibus] Fix aerospike_client software definition and cleanup aerospike build (#4955)
    Uses EXT_CFLAGS instead of CFLAGS during aerospike C client build.
    Builds the C client outside of the install_dir to avoid shipping it (it's not needed at runtime).
    Uses omnibus' git fetcher feature.

--- omnibus/config/software/aerospike-client.rb
+++ omnibus/config/software/aerospike-client.rb
@@ -5,20 +5,21 @@ name "aerospike-client"
 # https://github.com/aerospike/aerospike-client-python/blob/3.10.0/setup.py#L32-L33
 default_version "4.6.10"
 
+source git: "git://github.com/aerospike/aerospike-client-c.git",
+       submodules: true
+
 build do
   # The binary wheels on PyPI are not yet compatible with OpenSSL 1.1.0+, see:
   # https://github.com/aerospike/aerospike-client-python/issues/214#issuecomment-385451007
   # https://github.com/aerospike/aerospike-client-python/issues/227#issuecomment-423220411
-  command "git clone https://github.com/aerospike/aerospike-client-c.git #{install_dir}/embedded/lib/aerospike"
-  command "cd #{install_dir}/embedded/lib/aerospike && git checkout #{version}"
-  command "cd #{install_dir}/embedded/lib/aerospike && git submodule update --init"
 
   env = {
     "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "EXT_CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
     "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
   }
 
-  command "cd #{install_dir}/embedded/lib/aerospike && make clean", :env => env
-  command "cd #{install_dir}/embedded/lib/aerospike && make", :env => env
+  command "make clean", :env => env
+  # make fails if used with multiple parallel jobs when building the aerospike client
+  command "make", :env => env
 end
