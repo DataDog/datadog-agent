f3afcefd19594bbc59fd050937969fd3d1997650
Jahanzeb Khan <jahanzeb.khan@datadoghq.com>
2020-02-20 10:07:58 -0500

    [apm] now ignoring sublayer metrics for only childless db spans

--- pkg/trace/stats/subtrace.go
+++ pkg/trace/stats/subtrace.go
@@ -71,7 +71,8 @@ func ExtractTopLevelSubtraces(t pb.Trace, root *pb.Span) []Subtrace {
 		// not computing sublayer metrics for db spans for now because they usually don't have children
 		// and they increase the number of metrics computed by much more
 		// TODO[jahanzebk]: stop ignoring DB spans
-		if traceutil.HasTopLevel(current.Span) {
+		if traceutil.HasTopLevel(current.Span) &&
+			!(traceutil.SpanTypeIsDB(current.Span.Type) && len(childrenMap[current.Span.SpanID]) == 0) {
 			current.Ancestors = append(current.Ancestors, current.Span)
 		}
 		visited[current.Span] = true
