7f74cb3be2a2d873131f5ab86289a8e805de407e
Albert Vaca <albert.vaca@datadoghq.com>
2020-02-22 01:23:19 +0100

    Let pipelines run for only A6 or A7

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -23,7 +23,7 @@ from . import (agent,
 
 
 from .go import fmt, lint, vet, cyclo, golangci_lint, deps, lint_licenses, reset, generate
-from .test import test, integration_tests, lint_teamassignment, lint_releasenote, lint_milestone, lint_filenames, e2e_tests, make_kitchen_gitlab_yml
+from .test import test, integration_tests, lint_teamassignment, lint_releasenote, lint_milestone, lint_filenames, e2e_tests, make_kitchen_gitlab_yml, check_gitlab_broken_dependencies
 from .build_tags import audit_tag_impact
 
 # the root namespace
@@ -47,6 +47,7 @@ ns.add_task(lint_filenames)
 ns.add_task(audit_tag_impact)
 ns.add_task(e2e_tests)
 ns.add_task(make_kitchen_gitlab_yml)
+ns.add_task(check_gitlab_broken_dependencies)
 ns.add_task(generate)
 
 # add namespaced tasks to the root
