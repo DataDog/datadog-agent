c2abfe07a931e7a2742a80495993e8d97b934ed4
Spencer Brown <spencer.brown@datadoghq.com>
2020-02-27 08:59:15 -0500

    Refactor comments

--- pkg/ebpf/utils_windows.go
+++ pkg/ebpf/utils_windows.go
@@ -7,8 +7,8 @@ func verifyOSVersion(kernelCode uint32, platform string, exclusionList []string)
 	return true, ""
 }
 
-// TODO determine if we will keep this function. Also, add an error that will not cause failure to set up system probe
 // CurrentKernelVersion returns the current kernel version of the OS
 func CurrentKernelVersion() (uint32, error) {
 	return 1, nil
 }
+// TODO determine if we will keep this function^. Also, add an error that will not cause failure to set up system probe
