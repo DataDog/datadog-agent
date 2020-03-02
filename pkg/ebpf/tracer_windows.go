0af6d7e5821f67e8dbfbcfde887e02810704788a
Spencer Brown <spencer.brown@datadoghq.com>
2020-03-02 18:30:01 -0500

    Move polling IO stats to GetStats function and clean tracer_windows code

--- pkg/ebpf/tracer_windows.go
+++ pkg/ebpf/tracer_windows.go
@@ -3,17 +3,77 @@
 package ebpf
 
 /*
+typedef struct in6_addr {
+  union {
+    unsigned char Byte[16];
+    unsigned short Word[8];
+  } U;
+} IN6_ADDR;
+
+struct in_addr {
+  union {
+    struct {
+      unsigned char S_b1;
+      unsigned char S_b2;
+      unsigned char S_b3;
+      unsigned char S_b4;
+    } S_un_b;
+    struct {
+      unsigned short S_w1;
+      unsigned short S_w2;
+    } S_un_w;
+    unsigned long S_addr;
+  } S_un;
+} IN_ADDR;
+
+typedef struct _filterAddress
+{
+// _filterAddress defines an address to be matched, if supplied.
+// it can be ipv4 or ipv6 but not both.
+// supplying 0 for the address family means _any_ address (v4 or v6)
+	unsigned short af; //! AF_INET, AF_INET6 or 0
+	union
+	{
+		struct in6_addr         V6_address;
+		struct in_addr          V4_address;
+	}u;
+	unsigned short mask; // number of mask bits.
+} FILTER_ADDRESS;
+
+typedef enum
+{
+	DIRECTION_INBOUND = 0,
+	DIRECTION_OUTBOUND = 1
+} FILTER_DIRECTION;
+
+typedef struct _filterDefinition
+{
+	unsigned long Size;         //! size of this structure
+
+//  if supplied, the source and destination address must have the same address family.
+//  if both source and destination are applied, then the match for this filter
+//  is a logical AND, i.e. the source and destination both match.
+	unsigned short Af;     //! address family to filter
+
+	FILTER_ADDRESS  SourceAddress;
+	FILTER_ADDRESS  DestAddress;
+	unsigned short SourcePort;
+	unsigned short DestinationPort;
+	unsigned short Protocol;
+	FILTER_DIRECTION    Direction;
+} FILTER_DEFINITION;
+
 typedef struct _stats
 {
-    volatile long Read_calls;		//! number of read calls to the driver
-    volatile long Read_bytes;
-    volatile long Read_calls_outstanding;
-    volatile long Read_calls_completed;
-    volatile long Read_calls_cancelled;
-    volatile long Read_packets_skipped;
-    volatile long Write_calls;	//! number of write calls to the driver
-    volatile long Write_bytes;
-    volatile long Ioctl_calls;	//! number of ioctl calls to the driver
+    long Read_calls;		//! number of read calls to the driver
+    long Read_bytes;
+    long Read_calls_outstanding;
+    long Read_calls_completed;
+    long Read_calls_cancelled;
+    long Read_packets_skipped;
+    long Write_calls;	//! number of write calls to the driver
+    long Write_bytes;
+    long Ioctl_calls;	//! number of ioctl calls to the driver
 } STATS;
 
 typedef struct driver_stats
@@ -24,10 +84,19 @@ typedef struct driver_stats
 */
 import "C"
 import (
+	"fmt"
 	"github.com/DataDog/datadog-agent/pkg/process/util"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 	"syscall"
 	"unsafe"
+)
+
+const (
+	DRIVERFILE                            = "\\\\.\\ddfilter"
+	DDFILTER_IOCTL_GETSTATS               = 0x801
+	DDFILTER_IOCTL_SIMULATE_COMPLETE_READ = 0x802
+	DDFILTER_IOCTL_SET_FILTER             = 0x803
+	NETWORK_DEVICE_TYPE_CTL_CODE          = 0x00000012
 )
 
 var (
@@ -38,18 +107,25 @@ var (
 
 // Tracer struct for tracking network state and connections
 type Tracer struct {
-	config *Config
+	config       *Config
+	DriverHandle syscall.Handle
 }
 
 // NewTracer returns an initialized tracer struct
 func NewTracer(config *Config) (*Tracer, error) {
-	return &Tracer{}, nil
+	handle, err := openDriverFile(DRIVERFILE)
+	if err != nil {
+		return nil, fmt.Errorf("%s : %s", "Could not open driver file", err)
+	}
+	return &Tracer{
+		DriverHandle: handle,
+	}, nil
 }
 
 // Stop function stops running tracer
 func (t *Tracer) Stop() {}
 
-func open(path string) (syscall.Handle, error) {
+func openDriverFile(path string) (syscall.Handle, error) {
 	p, err := syscall.UTF16PtrFromString(path)
 	if err != nil {
 		return syscall.InvalidHandle, err
@@ -72,7 +148,6 @@ func open(path string) (syscall.Handle, error) {
 }
 
 func GetIoCompletionPort(handleFile syscall.Handle) (syscall.Handle, error) {
-	// logger.Print("creating io completion port")
 	iocpHandle, err := syscall.CreateIoCompletionPort(handleFile, 0, 0, 0)
 	if err != nil {
 		return syscall.Handle(0), err
@@ -80,7 +155,7 @@ func GetIoCompletionPort(handleFile syscall.Handle) (syscall.Handle, error) {
 	return iocpHandle, nil
 }
 
-func close(handle syscall.Handle) error {
+func closeDriverFile(handle syscall.Handle) error {
 	r, _, err := CloseHandle.Call(uintptr(handle))
 	if r == 0 {
 		return err
@@ -104,56 +179,6 @@ func createFilterDefinition(family uint16, direction FilterDirection, dPort uint
 
 // GetActiveConnections returns all active connections
 func (t *Tracer) GetActiveConnections(_ string) (*Connections, error) {
-
-	log.Info("GetActiveConnections Called")
-	h, err := open("\\\\.\\ddfilter")
-	if err != nil {
-		panic(err)
-	}
-
-	var (
-		bytesReturned uint32
-		stats C.struct_driver_stats
-	)
-	rdbbuf := make([]byte, C.sizeof_struct_driver_stats)
-	ioctlcd := ctl_code(0x00000012, 0x801, uint32(0), uint32(0))
-	err = syscall.DeviceIoControl(h, ioctlcd, nil, 0, &rdbbuf[0], uint32(len(rdbbuf)), &bytesReturned, nil)
-	if err != nil {
-		log.Errorf("Error from DeviceIoControl: %v", err)
-	}
-
-	log.Infof("Total bytes returned: %d\n", bytesReturned)
-	log.Infof("Printing rdbbuf: %+v\n", rdbbuf)
-	stats = *(*C.struct_driver_stats)(unsafe.Pointer(&rdbbuf[0]))
-
-	log.Infof("Printing stats: %+v\n", stats)
-
-	if err != nil {
-		log.Errorf("Error reading: %v", err)
-	}
-
-	//log.Infof("Trying to print pointer thingy: %+v\n", *(*C.struct_driver_stats)(unsafe.Pointer(&rdbbuf[0])))
-
-	//AF_INET is defined as 2
-	// fdOutbound := createFilterDefinition(2, DIRECTIONOUTBOUND, 80)
-	// fdInbound := createFilterDefinition(2, DIRECTIONINBOUND, 80)
-	// ioctlcd = ctl_code(0x00000012, 0x803, uint32(0), uint32(0))
-	// err = syscall.DeviceIoControl(h, ioctlcd, (*byte)(unsafe.Pointer(&fdOutbound)), uint32(unsafe.Sizeof(FilterDefinition{})), nil, 0, nil, nil)
-
-	// if err != nil {
-	// 	log.Errorf("Close: %v", err)
-	// }
-
-	// var buf = make([]byte, 256)
-	// bufSize := uint32(256)
-	// syscall.ReadFile(h, buf, &bufSize,nil)
-	// log.Infof("Total bytes in buf: %d\n", len(buf))
-
-	// err = close(h)
-	// if err != nil {
-	//	log.Errorf("close: %v", err)
-	// }
-
 	return &Connections{
 		DNS: map[util.Address][]string{
 			util.AddressFromString("127.0.0.1"): {"localhost"},
@@ -178,7 +203,42 @@ func (t *Tracer) getConnections(active []ConnectionStats) ([]ConnectionStats, ui
 
 // GetStats returns a map of statistics about the current tracer's internal state
 func (t *Tracer) GetStats() (map[string]interface{}, error) {
-	return nil, ErrNotImplemented
+	var (
+		bytesReturned uint32
+		stats         C.struct_driver_stats
+		statbuf       = make([]byte, C.sizeof_struct_driver_stats)
+		ioctlcd       = ctl_code(NETWORK_DEVICE_TYPE_CTL_CODE, DDFILTER_IOCTL_GETSTATS, uint32(0), uint32(0))
+	)
+
+	err := syscall.DeviceIoControl(t.DriverHandle, ioctlcd, nil, 0, &statbuf[0], uint32(len(statbuf)), &bytesReturned, nil)
+	if err != nil {
+		log.Errorf("Error reading stats with DeviceIoControl: %v", err)
+	}
+
+	stats = *(*C.struct_driver_stats)(unsafe.Pointer(&statbuf[0]))
+	return map[string]interface{}{
+		"BytesReturned" : bytesReturned,
+		"TotalStats": map[string]C.long{
+			"Read_calls":             stats.Total.Read_calls,
+			"Read_bytes":             stats.Total.Read_bytes,
+			"Read_calls_outstanding": stats.Total.Read_calls_outstanding, // Skipped connections (e.g. Local DNS requests)
+			"Read_calls_cancelled":   stats.Total.Read_calls_cancelled,
+			"Read_packets_skipped":   stats.Total.Read_packets_skipped,
+			"Write_calls":            stats.Total.Write_calls,
+			"Write_bytes":            stats.Total.Write_bytes,
+			"Ioctl_calls":            stats.Total.Ioctl_calls,
+		},
+		"HandleStats": map[string]C.long{
+			"Read_calls":             stats.Handle.Read_calls,
+			"Read_bytes":             stats.Handle.Read_bytes,
+			"Read_calls_outstanding": stats.Handle.Read_calls_outstanding, // Skipped connections (e.g. Local DNS requests)
+			"Read_calls_cancelled":   stats.Handle.Read_calls_cancelled,
+			"Read_packets_skipped":   stats.Handle.Read_packets_skipped,
+			"Write_calls":            stats.Handle.Write_calls,
+			"Write_bytes":            stats.Handle.Write_bytes,
+			"Ioctl_calls":            stats.Handle.Ioctl_calls,
+		},
+	}, nil
 }
 
 // DebugNetworkState returns a map with the current tracer's internal state, for debugging
@@ -190,28 +250,6 @@ func (t *Tracer) DebugNetworkState(clientID string) (map[string]interface{}, err
 func (t *Tracer) DebugNetworkMaps() (*Connections, error) {
 	return nil, ErrNotImplemented
 }
-
-/*
-	if err != nil {
-		log.Errorf("open: %v", err)
-	}
-
-	log.Info("Calling getiocompletionport")
-	iocp, err := GetIoCompletionPort(h)
-	overlapped := &syscall.Overlapped{
-		Internal:     0,
-		InternalHigh: 0,
-		Offset:       0,
-		OffsetHigh:   0,
-		HEvent:       0,
-	}
-	bytes := uint32(0)
-	key := uint32(0)
-	log.Info("Calling getqueuedcompletionstatus")
-	cs := syscall.GetQueuedCompletionStatus(iocp, &bytes, &key, &overlapped, 5)
-	log.Info(cs)
-	log.Infof("received %d bytes\n", bytes)
-*/
 
 // CurrentKernelVersion is not implemented on this OS for Tracer
 func CurrentKernelVersion() (uint32, error) {
