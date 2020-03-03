a3fa3d56b6216f469b3a9893496d3c5c8089ed8a
Albert Vaca <albert.vaca@datadoghq.com>
2020-03-03 21:10:50 +0100

    Install based on ZIP so we can support Nano in the future

---
+++ Dockerfiles/agent/install.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = "Stop"
+
+Expand-Archive datadog-agent-7-latest.amd64.zip
+Remove-Item datadog-agent-7-latest.amd64.zip
+
+Get-ChildItem -Path datadog-agent-7-* | Rename-Item -NewName "Datadog Agent"
+
+New-Item -ItemType directory -Path "C:/Program Files/Datadog"
+Move-Item "Datadog Agent" "C:/Program Files/Datadog/"
+
+New-Item -ItemType directory -Path 'C:/ProgramData/Datadog' 
+Move-Item "C:/Program Files/Datadog/Datadog Agent/EXAMPLECONFSLOCATION" "C:/ProgramData/Datadog/conf.d"
