2ddbe5c4187e564c7905bc2fb1bdff2a06c7cb64
Spencer Brown <spencer.brown@datadoghq.com>
2020-03-03 15:15:32 -0500

    Set up pulling of driver stats in the windows tracer

--- pkg/ebpf/common.go
+++ pkg/ebpf/common.go
@@ -150,3 +150,27 @@ func isRHELOrCentOS() (bool, error) {
 func isPre410Kernel(currentKernelCode uint32) bool {
 	return currentKernelCode < stringToKernelCode("4.1.0")
 }
+
+// snakeToCapInitialCamel converts a snake case to Camel case with capital initial
+func snakeToCapInitialCamel(s string) string {
+	n := ""
+	capNext := true
+	for _, v := range s {
+		if v >= 'A' && v <= 'Z' {
+			n += string(v)
+		}
+		if v >= 'a' && v <= 'z' {
+			if capNext {
+				n += strings.ToUpper(string(v))
+			} else {
+				n += string(v)
+			}
+		}
+		if v == '_' {
+			capNext = true
+		} else {
+			capNext = false
+		}
+	}
+	return n
+}
--- pkg/ebpf/tracer_windows.go
+++ pkg/ebpf/tracer_windows.go
@@ -2,22 +2,148 @@
 
 package ebpf
 
+/*
+typedef struct _stats
+{
+    long Read_calls;		//! number of read calls to the driver
+    long Read_bytes;
+    long Read_calls_outstanding;
+    long Read_calls_completed;
+    long Read_calls_cancelled;
+    long Read_packets_skipped;
+    long Write_calls;	//! number of write calls to the driver
+    long Write_bytes;
+    long Ioctl_calls;	//! number of ioctl calls to the driver
+} STATS;
+
+typedef struct driver_stats
+{
+    STATS		Total;		//! stats since the driver was started
+    STATS		Handle;		//! stats for the file handle in question
+} DRIVER_STATS;
+*/
+import "C"
 import (
+	"expvar"
+	"fmt"
+	"syscall"
+	"time"
+	"unsafe"
+
 	"github.com/DataDog/datadog-agent/pkg/process/util"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
+
+const (
+	DRIVERFILE = `\\.\ddfilter`
+
+	// https://github.com/DataDog/datadog-windows-filter/blob/master/include/ddfilterapi.h
+	DDFILTER_IOCTL_GETSTATS               = 0x801
+	DDFILTER_IOCTL_SIMULATE_COMPLETE_READ = 0x802
+	DDFILTER_IOCTL_SET_FILTER             = 0x803
+
+	// https://docs.microsoft.com/en-us/windows-hardware/drivers/kernel/specifying-device-types
+	NETWORK_DEVICE_TYPE_CTL_CODE = 0x00000012
+)
+
+var (
+	kernel32        = syscall.MustLoadDLL("kernel32.dll")
+	CreateFile      = kernel32.MustFindProc("CreateFileW")
+	CloseHandle     = kernel32.MustFindProc("CloseHandle")
+	expvarEndpoints map[string]*expvar.Map
+	expvarTypes     = []string{"driver_total_stats", "driver_handle_stats"}
+)
+
+func init() {
+	expvarEndpoints = make(map[string]*expvar.Map, len(expvarTypes))
+	for _, name := range expvarTypes {
+		expvarEndpoints[name] = expvar.NewMap(name)
+	}
+}
 
 // Tracer struct for tracking network state and connections
 type Tracer struct {
-	config *Config
+	config       *Config
+	driverHandle syscall.Handle
 }
 
 // NewTracer returns an initialized tracer struct
 func NewTracer(config *Config) (*Tracer, error) {
-	return &Tracer{}, nil
+	handle, err := openDriverFile(DRIVERFILE)
+	if err != nil {
+		return nil, fmt.Errorf("%s : %s", "Could not create driver handle", err)
+	}
+	tr := &Tracer{
+		driverHandle: handle,
+	}
+	go tr.expvarStats()
+	return tr, nil
+}
+
+func (t *Tracer) expvarStats() {
+	ticker := time.NewTicker(5 * time.Second)
+	// starts running the body immediately instead waiting for the first tick
+	for ; true; <-ticker.C {
+		stats, err := t.GetStats()
+		if err != nil {
+			continue
+		}
+
+		for name, stat := range stats {
+			for metric, val := range stat.(map[string]int64) {
+				currVal := &expvar.Int{}
+				currVal.Set(val)
+				expvarEndpoints[name].Set(snakeToCapInitialCamel(metric), currVal)
+			}
+		}
+	}
 }
 
 // Stop function stops running tracer
 func (t *Tracer) Stop() {}
+
+func openDriverFile(path string) (syscall.Handle, error) {
+	p, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return syscall.InvalidHandle, err
+	}
+	log.Debug("Creating Driver File...")
+	r, _, err := CreateFile.Call(uintptr(unsafe.Pointer(p)),
+		syscall.GENERIC_READ,
+		syscall.FILE_SHARE_READ|syscall.FILE_SHARE_WRITE,
+		0,
+		syscall.OPEN_EXISTING,
+		syscall.FILE_FLAG_OVERLAPPED,
+		0)
+	log.Debug("Creating Driver Handle...")
+	h := syscall.Handle(r)
+	if h == syscall.InvalidHandle {
+		return h, err
+	}
+	log.Info("Connected to driver and handle created")
+	return h, nil
+}
+
+func closeDriverFile(handle syscall.Handle) error {
+	r, _, err := CloseHandle.Call(uintptr(handle))
+	if r == 0 {
+		return err
+	}
+	return nil
+}
+
+func getIoCompletionPort(handleFile syscall.Handle) (syscall.Handle, error) {
+	iocpHandle, err := syscall.CreateIoCompletionPort(handleFile, 0, 0, 0)
+	if err != nil {
+		return syscall.Handle(0), err
+	}
+	return iocpHandle, nil
+}
+
+// Creates the IOCTLCode to be passed for DeviceIoControl syscall
+func ctl_code(device_type, function, method, access uint32) uint32 {
+	return (device_type << 16) | (access << 14) | (function << 2) | method
+}
 
 // GetActiveConnections returns all active connections
 func (t *Tracer) GetActiveConnections(_ string) (*Connections, error) {
@@ -45,7 +171,46 @@ func (t *Tracer) getConnections(active []ConnectionStats) ([]ConnectionStats, ui
 
 // GetStats returns a map of statistics about the current tracer's internal state
 func (t *Tracer) GetStats() (map[string]interface{}, error) {
-	return nil, ErrNotImplemented
+	if t.driverHandle == syscall.InvalidHandle {
+		return nil, fmt.Errorf("Problem with handle cannot get stats.")
+	}
+
+	var (
+		bytesReturned uint32
+		stats         C.struct_driver_stats
+		statbuf       = make([]byte, C.sizeof_struct_driver_stats)
+		ioctlcd       = ctl_code(NETWORK_DEVICE_TYPE_CTL_CODE, DDFILTER_IOCTL_GETSTATS, uint32(0), uint32(0))
+	)
+
+	err := syscall.DeviceIoControl(t.driverHandle, ioctlcd, nil, 0, &statbuf[0], uint32(len(statbuf)), &bytesReturned, nil)
+	if err != nil {
+		log.Errorf("Error reading Stats with DeviceIoControl: %v", err)
+	}
+
+	stats = *(*C.struct_driver_stats)(unsafe.Pointer(&statbuf[0]))
+
+	return map[string]interface{}{
+		"driver_total_stats": map[string]int64{
+			"read_calls":             int64(stats.Total.Read_calls),
+			"read_bytes":             int64(stats.Total.Read_bytes),
+			"read_calls_outstanding": int64(stats.Total.Read_calls_outstanding),
+			"read_calls_cancelled":   int64(stats.Total.Read_calls_cancelled),
+			"read_packets_skipped":   int64(stats.Total.Read_packets_skipped),
+			"write_calls":            int64(stats.Total.Write_calls),
+			"write_bytes":            int64(stats.Total.Write_bytes),
+			"ioctl_calls":            int64(stats.Total.Ioctl_calls),
+		},
+		"driver_handle_stats": map[string]int64{
+			"read_calls":             int64(stats.Handle.Read_calls),
+			"read_bytes":             int64(stats.Handle.Read_bytes),
+			"read_calls_outstanding": int64(stats.Handle.Read_calls_outstanding),
+			"read_calls_cancelled":   int64(stats.Handle.Read_calls_cancelled),
+			"read_packets_skipped":   int64(stats.Handle.Read_packets_skipped),
+			"write_calls":            int64(stats.Handle.Write_calls),
+			"write_bytes":            int64(stats.Handle.Write_bytes),
+			"ioctl_calls":            int64(stats.Handle.Ioctl_calls),
+		},
+	}, nil
 }
 
 // DebugNetworkState returns a map with the current tracer's internal state, for debugging
