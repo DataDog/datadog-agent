fcc15df6c8ac4cf3a094711edbe1136a10b6772d
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2020-03-05 14:17:54 +0100

    Update pkg/autodiscovery/providers/cloudfoundry.go
    Co-Authored-By: Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>

--- pkg/autodiscovery/providers/cloudfoundry.go
+++ pkg/autodiscovery/providers/cloudfoundry.go
@@ -53,7 +53,7 @@ func (cf CloudFoundryConfigProvider) IsUpToDate() (bool, error) {
 
 // Collect collects AD config templates from all relevant BBS API information
 func (cf CloudFoundryConfigProvider) Collect() ([]integration.Config, error) {
-	log.Debugf("Collecting configs via the CloudFoundryProvider")
+	log.Debug("Collecting configs via the CloudFoundryProvider")
 	cf.lastCollected = time.Now()
 	allActualLRPs, desiredLRPs := cf.bbsCache.GetAllLRPs()
 	allConfigs := []integration.Config{}
