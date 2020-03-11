46d950e5a3c73418bbfdb5bb1f291a6d01f1fc8c
Albert Vaca <albert.vaca@datadoghq.com>
2020-03-11 15:38:46 +0100

    Path doesn't persist outside of script :(

--- Dockerfiles/agent/install.ps1
+++ Dockerfiles/agent/install.ps1
@@ -6,8 +6,6 @@ if ("$env:WITH_JMX" -ne "false") {
     Expand-Archive -Path jre-11.0.6.zip -DestinationPath C:/
     Remove-Item jre-11.0.6.zip
     Move-Item C:/jdk-11.0.6+10-jre/ C:/java
-    setx PATH "$env:PATH;C:\java\bin"
-    java -version
 }
 
 Expand-Archive datadog-agent-7-latest.amd64.zip
