DataDog/datadog-agent

commit 43ec23761449e5cd30f004a58a0450d418921b43

Config change at runtime (#4840)
Implement a generic way to add runtime configurable setting.
It includes the ability to change log_level at runtime using
the aforementioned work.

cmd/agent/app/config.go
@@ -6,8 +6,10 @@
 package app
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"html"
 
 	"github.com/DataDog/datadog-agent/cmd/agent/common"
 	"github.com/DataDog/datadog-agent/pkg/api/util"
@@ -18,42 +20,68 @@ import (
 
 func init() {
 	AgentCmd.AddCommand(configCommand)
+	configCommand.AddCommand(listRuntimeCommand)
+	configCommand.AddCommand(setCommand)
+	configCommand.AddCommand(getCommand)
 }
 
-var configCommand = &cobra.Command{
-	Use:   "config",
-	Short: "Print the runtime configuration of a running agent",
-	Long:  ``,
-	RunE: func(cmd *cobra.Command, args []string) error {
+var (
+	configCommand = &cobra.Command{
+		Use:   "config",
+		Short: "Print the runtime configuration of a running agent",
+		Long:  ``,
+		RunE:  showRuntimeConfiguration,
+	}
+	listRuntimeCommand = &cobra.Command{
+		Use:   "list-runtime",
+		Short: "List settings that can be changed at runtime",
+		Long:  ``,
+		RunE:  listRuntimeConfigurableValue,
+	}
+	setCommand = &cobra.Command{
+		Use:   "set [setting] [value]",
+		Short: "Set, for the current runtime, the value of a given configuration setting",
+		Long:  ``,
+		RunE:  setConfigValue,
+	}
+	getCommand = &cobra.Command{
+		Use:   "get [setting]",
+		Short: "Get, for the current runtime, the value of a given configuration setting",
+		Long:  ``,
+		RunE:  getConfigValue,
+	}
+	agentConfigURLPath = "/agent/config"
+	listRuntimeURLPath = agentConfigURLPath + "/list-runtime"
+)
 
-		if flagNoColor {
-			color.NoColor = true
-		}
+func showRuntimeConfiguration(cmd *cobra.Command, args []string) error {
+	if flagNoColor {
+		color.NoColor = true
+	}
 
-		err := common.SetupConfigWithoutSecrets(confFilePath, "")
-		if err != nil {
-			return fmt.Errorf("unable to set up global agent configuration: %v", err)
-		}
+	err := common.SetupConfigWithoutSecrets(confFilePath, "")
+	if err != nil {
+		return fmt.Errorf("unable to set up global agent configuration: %v", err)
+	}
 
-		err = config.SetupLogger(loggerName, config.GetEnv("DD_LOG_LEVEL", "off"), "", "", false, true, false)
-		if err != nil {
-			fmt.Printf("Cannot setup logger, exiting: %v\n", err)
-			return err
-		}
+	err = config.SetupLogger(loggerName, config.GetEnv("DD_LOG_LEVEL", "off"), "", "", false, true, false)
+	if err != nil {
+		fmt.Printf("Cannot setup logger, exiting: %v\n", err)
+		return err
+	}
 
-		err = util.SetAuthToken()
-		if err != nil {
-			return err
-		}
+	err = util.SetAuthToken()
+	if err != nil {
+		return err
+	}
 
-		runtimeConfig, err := requestConfig()
-		if err != nil {
-			return err
-		}
+	runtimeConfig, err := requestConfig()
+	if err != nil {
+		return err
+	}
 
-		fmt.Println(runtimeConfig)
-		return nil
-	},
+	fmt.Println(runtimeConfig)
+	return nil
 }
 
 func requestConfig() (string, error) {
@@ -62,7 +90,7 @@ func requestConfig() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	apiConfigURL := fmt.Sprintf("https://%v:%v/agent/config", ipcAddress, config.Datadog.GetInt("cmd_port"))
+	apiConfigURL := fmt.Sprintf("https://%v:%v"+agentConfigURLPath, ipcAddress, config.Datadog.GetInt("cmd_port"))
 
 	r, err := util.DoGet(c, apiConfigURL)
 	if err != nil {
@@ -77,4 +105,103 @@ func requestConfig() (string, error) {
 	}
 
 	return string(r), nil
+}
+
+func listRuntimeConfigurableValue(cmd *cobra.Command, args []string) error {
+	err := util.SetAuthToken()
+	if err != nil {
+		return err
+	}
+	c := util.GetClient(false)
+	ipcAddress, err := config.GetIPCAddress()
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("https://%v:%v"+listRuntimeURLPath, ipcAddress, config.Datadog.GetInt("cmd_port"))
+	r, err := util.DoGet(c, url)
+	if err != nil {
+		var errMap = make(map[string]string)
+		json.Unmarshal(r, &errMap)
+		// If the error has been marshalled into a json object, check it and return it properly
+		if e, found := errMap["error"]; found {
+			return fmt.Errorf(e)
+		}
+		return err
+	}
+	var settings = make(map[string]string)
+	err = json.Unmarshal(r, &settings)
+	if err != nil {
+		return err
+	}
+	fmt.Println("=== Settings that can be changed at runtime ===")
+	for setting, desc := range settings {
+		fmt.Printf("%s:\t\t\t%s\n", setting, desc)
+	}
+	return nil
+}
+
+func setConfigValue(cmd *cobra.Command, args []string) error {
+	if len(args) != 2 {
+		return fmt.Errorf("Exactly two parameters are required: the setting name and its value")
+	}
+	err := util.SetAuthToken()
+	if err != nil {
+		return err
+	}
+	c := util.GetClient(false)
+	ipcAddress, err := config.GetIPCAddress()
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("https://%v:%v"+agentConfigURLPath+"/%v", ipcAddress, config.Datadog.GetInt("cmd_port"), args[0])
+	body := fmt.Sprintf("value=%s", html.EscapeString(args[1]))
+	r, err := util.DoPost(c, url, "application/x-www-form-urlencoded", bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		var errMap = make(map[string]string)
+		json.Unmarshal(r, &errMap)
+		// If the error has been marshalled into a json object, check it and return it properly
+		if e, found := errMap["error"]; found {
+			return fmt.Errorf(e)
+		}
+		return err
+	}
+	fmt.Printf("Configuration setting %s is now set to: %s\n", args[0], args[1])
+	return nil
+}
+
+func getConfigValue(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("A single setting name must be specified")
+	}
+	err := util.SetAuthToken()
+	if err != nil {
+		return err
+	}
+	c := util.GetClient(false)
+	ipcAddress, err := config.GetIPCAddress()
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("https://%v:%v"+agentConfigURLPath+"/%v", ipcAddress, config.Datadog.GetInt("cmd_port"), args[0])
+	r, err := util.DoGet(c, url)
+	if err != nil {
+		var errMap = make(map[string]string)
+		json.Unmarshal(r, &errMap)
+		// If the error has been marshalled into a json object, check it and return it properly
+		if e, found := errMap["error"]; found {
+			return fmt.Errorf(e)
+		}
+		return err
+	}
+
+	var setting = make(map[string]interface{})
+	err = json.Unmarshal(r, &setting)
+	if err != nil {
+		return err
+	}
+	if value, found := setting["value"]; found {
+		fmt.Printf("%s is set to: %s\n", args[0], value)
+		return nil
+	}
+	return fmt.Errorf("unable to get value for this setting: %v", args[0])
 }

pkg/config/log.go
@@ -75,6 +75,10 @@ func SetupLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, sys
 	var syslog bool
 	var useTLS bool
 
+	if _, found := seelog.LogLevelFromString(logLevel); !found {
+		return fmt.Errorf("unknown log level: %s", logLevel)
+	}
+
 	if syslogURI != "" { // non-blank uri enables syslog
 		syslog = true
 
@@ -97,13 +101,12 @@ func SetupLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, sys
 		seelogLogLevel = "warn"
 	}
 
-	configTemplate := fmt.Sprintf(`<seelog minlevel="%s">`, seelogLogLevel)
-
 	formatID := "common"
 	if jsonFormat {
 		formatID = "json"
 	}
 
+	configTemplate := `<seelog>`
 	configTemplate += fmt.Sprintf(`<outputs formatid="%s">`, formatID)
 
 	if logToConsole {
@@ -232,7 +235,7 @@ func getSyslogConnection(uri *url.URL, secure bool) (net.Conn, error) {
 	} else {
 		switch uri.Scheme {
 		case "unix", "unixgram":
-			fmt.Printf("Trying to connecto to: %s", uri.Path)
+			fmt.Printf("Trying to connect to: %s", uri.Path)
 			for _, netName := range localNetNames {
 				conn, err = net.Dial(netName, uri.Path)
 				if err == nil {

pkg/config/runtime_setting_log_level.go
@@ -1,0 +1,39 @@
+/// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package config
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+// logLevelRuntimeSetting wraps operations to change log level at runtime
+type logLevelRuntimeSetting string
+
+func (l logLevelRuntimeSetting) Description() string {
+	return "Set/get the log level, valid values are: trace, debug, info, warn, error, critical and off"
+}
+
+func (l logLevelRuntimeSetting) Name() string {
+	return string(l)
+}
+
+func (l logLevelRuntimeSetting) Get() (interface{}, error) {
+	level, err := log.GetLogLevel()
+	if err != nil {
+		return "", err
+	}
+	return level.String(), nil
+}
+
+func (l logLevelRuntimeSetting) Set(v interface{}) error {
+	logLevel := v.(string)
+	err := log.ChangeLogLevel(logLevel)
+	if err != nil {
+		return err
+	}
+	Datadog.Set("log_level", logLevel)
+	return nil
+}

pkg/util/log/log.go
@@ -556,3 +556,12 @@ func GetLogLevel() (seelog.LogLevel, error) {
 	// need to return something, just set to Info (expected default)
 	return seelog.InfoLvl, errors.New("cannot get loglevel: logger not initialized")
 }
+
+// ChangeLogLevel changes the current log level, valide levels are trace, debug, info, warn, error, critical and off
+func ChangeLogLevel(level string) error {
+	if logger != nil {
+		return logger.changeLogLevel(level)
+	}
+	// need to return something, just set to Info (expected default)
+	return errors.New("cannot set loglevel: logger not initialized")
+}

pkg/config/runtime_settings_test.go
@@ -1,0 +1,90 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package config
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/cihub/seelog"
+	"github.com/stretchr/testify/assert"
+)
+
+type runtimeTestSetting struct {
+	value int
+}
+
+func (t *runtimeTestSetting) Name() string {
+	return "name"
+}
+
+func (t *runtimeTestSetting) Description() string {
+	return "desc"
+}
+
+func (t *runtimeTestSetting) Get() (interface{}, error) {
+	return t.value, nil
+}
+
+func (t *runtimeTestSetting) Set(v interface{}) error {
+	t.value = v.(int)
+	return nil
+}
+
+func cleanRuntimeSetting() {
+	runtimeSettings = make(map[string]RuntimeSetting)
+}
+
+func TestRuntimeSettings(t *testing.T) {
+	cleanRuntimeSetting()
+	runtimeSetting := runtimeTestSetting{1}
+
+	err := registerRuntimeSetting(&runtimeSetting)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(RuntimeSettings()))
+
+	v, err := GetRuntimeSetting(runtimeSetting.Name())
+	assert.Nil(t, err)
+	assert.Equal(t, runtimeSetting.value, v)
+
+	err = SetRuntimeSetting(runtimeSetting.Name(), 123)
+	assert.Nil(t, err)
+
+	v, err = GetRuntimeSetting(runtimeSetting.Name())
+	assert.Nil(t, err)
+	assert.Equal(t, 123, v)
+
+	err = registerRuntimeSetting(&runtimeSetting)
+	assert.NotNil(t, err)
+	assert.Equal(t, "duplicated settings detected", err.Error())
+}
+
+func TestLogLevel(t *testing.T) {
+	cleanRuntimeSetting()
+	setupConf()
+	l, err := seelog.LoggerFromWriterWithMinLevelAndFormat(ioutil.Discard, seelog.DebugLvl, "[%LEVEL] %FuncShort: %Msg")
+	assert.Nil(t, err)
+	log.SetupDatadogLogger(l, "debug")
+
+	ll := logLevelRuntimeSetting("log_level")
+	assert.Equal(t, "log_level", ll.Name())
+
+	err = ll.Set("off")
+	assert.Nil(t, err)
+
+	v, err := ll.Get()
+	assert.Equal(t, "off", v)
+	assert.Nil(t, err)
+
+	err = ll.Set("invalid")
+	assert.NotNil(t, err)
+	assert.Equal(t, "bad log level", err.Error())
+
+	v, err = ll.Get()
+	assert.Equal(t, "off", v)
+	assert.Nil(t, err)
+}

pkg/config/config.go
@@ -119,6 +119,8 @@ func init() {
 	Datadog = NewConfig("datadog", "DD", strings.NewReplacer(".", "_"))
 	// Configuration defaults
 	initConfig(Datadog)
+	// Configuration settings that can be changed at runtime
+	initRuntimeSettings()
 }
 
 // initConfig initializes the config defaults on a config

commit 656c274769a3ae8a284d569caa262f79ed1a334d

Rebuild seelog logger when changing log level (#5102)
This commit fixes the bug introduced by #4840 that led to systematic trace log level in
dependencies using seelog. Log level change at runtime now completely rebuilds a new 
seelog logger each time the log level is modified.
The seelog configuration is now wrapped into its own package.
This PR also includes fixes for the new list-runtime, get and set commands when a
non-default config directory/file is defined as the config needs to be initialised before 
util.SetAuthToken is called so it can read the token file from the proper location.

cmd/agent/app/config.go
@@ -54,7 +54,7 @@ var (
 	listRuntimeURLPath = agentConfigURLPath + "/list-runtime"
 )
 
-func showRuntimeConfiguration(cmd *cobra.Command, args []string) error {
+func setupConfig() error {
 	if flagNoColor {
 		color.NoColor = true
 	}
@@ -70,7 +70,11 @@ func showRuntimeConfiguration(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	err = util.SetAuthToken()
+	return util.SetAuthToken()
+}
+
+func showRuntimeConfiguration(cmd *cobra.Command, args []string) error {
+	err := setupConfig()
 	if err != nil {
 		return err
 	}
@@ -108,7 +112,7 @@ func requestConfig() (string, error) {
 }
 
 func listRuntimeConfigurableValue(cmd *cobra.Command, args []string) error {
-	err := util.SetAuthToken()
+	err := setupConfig()
 	if err != nil {
 		return err
 	}
@@ -144,7 +148,7 @@ func setConfigValue(cmd *cobra.Command, args []string) error {
 	if len(args) != 2 {
 		return fmt.Errorf("Exactly two parameters are required: the setting name and its value")
 	}
-	err := util.SetAuthToken()
+	err := setupConfig()
 	if err != nil {
 		return err
 	}
@@ -173,7 +177,7 @@ func getConfigValue(cmd *cobra.Command, args []string) error {
 	if len(args) != 1 {
 		return fmt.Errorf("A single setting name must be specified")
 	}
-	err := util.SetAuthToken()
+	err := setupConfig()
 	if err != nil {
 		return err
 	}

pkg/config/log.go
@@ -16,6 +16,7 @@ import (
 	"strconv"
 	"strings"
 
+	seelogCfg "github.com/DataDog/datadog-agent/pkg/config/seelog"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 	"github.com/cihub/seelog"
 )
@@ -27,8 +28,10 @@ const logDateFormat = "2006-01-02 15:04:05 MST" // see time.Format for format sy
 
 var syslogTLSConfig *tls.Config
 
-// BuildCommonFormat returns the log common format seelog string
-func BuildCommonFormat(loggerName LoggerName) string {
+var seelogConfig *seelogCfg.Config
+
+// buildCommonFormat returns the log common format seelog string
+func buildCommonFormat(loggerName LoggerName) string {
 	return fmt.Sprintf("%%Date(%s) | %s | %%LEVEL | (%%ShortFilePath:%%Line in %%FuncShort) | %%Msg%%n", logDateFormat, loggerName)
 }
 
@@ -38,10 +41,10 @@ func createQuoteMsgFormatter(params string) seelog.FormatterFunc {
 	}
 }
 
-// BuildJSONFormat returns the log JSON format seelog string
-func BuildJSONFormat(loggerName LoggerName) string {
+// buildJSONFormat returns the log JSON format seelog string
+func buildJSONFormat(loggerName LoggerName) string {
 	seelog.RegisterCustomFormatter("QuoteMsg", createQuoteMsgFormatter)
-	return fmt.Sprintf("{&quot;agent&quot;:&quot;%s&quot;,&quot;time&quot;:&quot;%%Date(%s)&quot;,&quot;level&quot;:&quot;%%LEVEL&quot;,&quot;file&quot;:&quot;%%ShortFilePath&quot;,&quot;line&quot;:&quot;%%Line&quot;,&quot;func&quot;:&quot;%%FuncShort&quot;,&quot;msg&quot;:%%QuoteMsg}%%n", strings.ToLower(string(loggerName)), logDateFormat)
+	return fmt.Sprintf(`{"agent":"%s","time":"%%Date(%s)","level":"%%LEVEL","file":"%%ShortFilePath","line":"%%Line","func":"%%FuncShort","msg":%%QuoteMsg}%%n`, strings.ToLower(string(loggerName)), logDateFormat)
 }
 
 func getSyslogTLSKeyPair() (*tls.Certificate, error) {
@@ -72,9 +75,6 @@ func getSyslogTLSKeyPair() (*tls.Certificate, error) {
 // a non empty syslogURI will enable syslog, and format them following RFC 5424 if specified
 // you can also specify to log to the console and in JSON format
 func SetupLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, syslogRFC, logToConsole, jsonFormat bool) error {
-	var syslog bool
-	var useTLS bool
-
 	seelogLogLevel := strings.ToLower(logLevel)
 	if seelogLogLevel == "warning" { // Common gotcha when used to agent5
 		seelogLogLevel = "warn"
@@ -84,14 +84,21 @@ func SetupLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, sys
 		return fmt.Errorf("unknown log level: %s", seelogLogLevel)
 	}
 
-	if syslogURI != "" { // non-blank uri enables syslog
-		syslog = true
+	formatID := "common"
+	if jsonFormat {
+		formatID = "json"
+	}
 
+	seelogConfig = seelogCfg.NewSeelogConfig(string(loggerName), seelogLogLevel, formatID, buildJSONFormat(loggerName), buildCommonFormat(loggerName), syslogRFC)
+	seelogConfig.EnableConsoleLog(logToConsole)
+	seelogConfig.EnableFileLogging(logFile, Datadog.GetSizeInBytes("log_file_max_size"), uint(Datadog.GetInt("log_file_max_rolls")))
+
+	if syslogURI != "" { // non-blank uri enables syslog
 		syslogTLSKeyPair, err := getSyslogTLSKeyPair()
 		if err != nil {
 			return err
 		}
-
+		var useTLS bool
 		if syslogTLSKeyPair != nil {
 			useTLS = true
 			syslogTLSConfig = &tls.Config{
@@ -99,57 +106,19 @@ func SetupLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, sys
 				InsecureSkipVerify: Datadog.GetBool("syslog_tls_verify"),
 			}
 		}
+		seelogConfig.ConfigureSyslog(syslogURI, useTLS)
 	}
 
-	formatID := "common"
-	if jsonFormat {
-		formatID = "json"
+	configTemplate, err := seelogConfig.Render()
+	if err != nil {
+		return err
 	}
-
-	configTemplate := `<seelog>`
-	configTemplate += fmt.Sprintf(`<outputs formatid="%s">`, formatID)
-
-	if logToConsole {
-		configTemplate += `<console />`
-	}
-	if logFile != "" {
-		configTemplate += fmt.Sprintf(`<rollingfile type="size" filename="%s" maxsize="%d" maxrolls="%d" />`, logFile, Datadog.GetSizeInBytes("log_file_max_size"), Datadog.GetInt("log_file_max_rolls"))
-	}
-	if syslog {
-		var syslogTemplate string
-		if syslogURI != "" {
-			syslogTemplate = fmt.Sprintf(
-				`<custom name="syslog" formatid="syslog-%s" data-uri="%s" data-tls="%v" />`,
-				formatID,
-				syslogURI,
-				useTLS,
-			)
-		} else {
-			syslogTemplate = fmt.Sprintf(`<custom name="syslog" formatid="syslog-%s" />`, formatID)
-		}
-		configTemplate += syslogTemplate
-	}
-
-	configTemplate += fmt.Sprintf(`</outputs>
-	<formats>
-		<format id="json" format="%s"/>
-		<format id="common" format="%s"/>
-		<format id="syslog-json" format="%%CustomSyslogHeader(20,`+strconv.FormatBool(syslogRFC)+`){&quot;agent&quot;:&quot;%s&quot;,&quot;level&quot;:&quot;%%LEVEL&quot;,&quot;relfile&quot;:&quot;%%ShortFilePath&quot;,&quot;line&quot;:&quot;%%Line&quot;,&quot;msg&quot;:&quot;%%Msg&quot;}%%n"/>
-		<format id="syslog-common" format="%%CustomSyslogHeader(20,`+strconv.FormatBool(syslogRFC)+`) %s | %%LEVEL | (%%ShortFilePath:%%Line in %%FuncShort) | %%Msg%%n" />
-	</formats>
-</seelog>`,
-		BuildJSONFormat(loggerName),
-		BuildCommonFormat(loggerName),
-		strings.ToLower(string(loggerName)),
-		loggerName,
-	)
 
 	logger, err := seelog.LoggerFromConfigAsString(configTemplate)
 	if err != nil {
 		return err
 	}
 	seelog.ReplaceLogger(logger)
-
 	log.SetupDatadogLogger(logger, seelogLogLevel)
 	log.AddStrippedKeys(Datadog.GetStringSlice("flare_stripped_keys"))
 	return nil
@@ -348,6 +317,24 @@ func extractShortPathFromFullPath(fullPath string) string {
 	// ie DataDog/datadog-agent/ or DataDog/datadog-process-agent/
 	slices := strings.Split(fullPath, "-agent/")
 	return slices[len(slices)-1]
+}
+
+func changeLogLevel(level string) error {
+	// We create a new logger to propagate the new log level everywhere seelog is used (including dependencies)
+	seelogConfig.SetLogLevel(level)
+	configTemplate, err := seelogConfig.Render()
+	if err != nil {
+		return err
+	}
+
+	logger, err := seelog.LoggerFromConfigAsString(configTemplate)
+	if err != nil {
+		return err
+	}
+	seelog.ReplaceLogger(logger)
+
+	// We wire the new logger with the Datadog logic
+	return log.ChangeLogLevel(logger, level)
 }
 
 func init() {

pkg/config/runtime_setting_log_level.go
@@ -30,7 +30,7 @@ func (l logLevelRuntimeSetting) Get() (interface{}, error) {
 
 func (l logLevelRuntimeSetting) Set(v interface{}) error {
 	logLevel := v.(string)
-	err := log.ChangeLogLevel(logLevel)
+	err := changeLogLevel(logLevel)
 	if err != nil {
 		return err
 	}

pkg/config/seelog/seelog_config.go
@@ -1,0 +1,99 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package seelog
+
+import (
+	"bytes"
+	"html/template"
+	"strings"
+	"sync"
+)
+
+// Config abstracts seelog XML configuration definition
+type Config struct {
+	settings map[string]interface{}
+	sync.Mutex
+}
+
+const seelogConfigurationTemplate = `
+<seelog minlevel="{{.logLevel}}">
+	<outputs formatid="{{.format}}">
+		{{if .consoleLoggingEnabled}}<console />{{end}}
+		{{if .logfile              }}<rollingfile type="size" filename="{{.logfile}}" maxsize="{{.maxsize}}" maxrolls="{{.maxrolls}}" />{{end}}
+		{{if .syslogURI            }}<custom name="syslog" formatid="syslog-{{.format}}" data-uri="{{.syslogURI}}" data-tls="{{.syslogUseTLS}}" />{{end}}
+	</outputs>
+	<formats>
+		<format id="json"          format="{{.jsonFormat}}"/>
+		<format id="common"        format="{{.commonFormat}}"/>
+		<format id="syslog-json"   format="%CustomSyslogHeader(20,{{.syslogRFC}}) {{getJSONSyslogFormat .loggerName}}"/>
+		<format id="syslog-common" format="%CustomSyslogHeader(20,{{.syslogRFC}}) {{.loggerName}} | %LEVEL | (%ShortFilePath:%Line in %FuncShort) | %Msg%n" />
+	</formats>
+</seelog>`
+
+func (c *Config) setValue(k string, v interface{}) {
+	c.Lock()
+	defer c.Unlock()
+	c.settings[k] = v
+}
+
+// Render generates a string containing a valid seelog XML configuration
+func (c *Config) Render() (string, error) {
+	c.Lock()
+	defer c.Unlock()
+	funcMap := template.FuncMap{
+		// This function will be called by the html/template engine that will perform HTML escaping of quotes characters in the output string
+		"getJSONSyslogFormat": func(name string) string {
+			return `{"agent":"` + strings.ToLower(name) + `","level":"%LEVEL","relfile":"%ShortFilePath","line":"%Line","msg":"%Msg"}%n`
+		},
+	}
+	tmpl, err := template.New("seelog_config").Funcs(funcMap).Parse(seelogConfigurationTemplate)
+	if err != nil {
+		return "", err
+	}
+	b := &bytes.Buffer{}
+	err = tmpl.Execute(b, c.settings)
+	return b.String(), err
+}
+
+// EnableConsoleLog sets enable or disable console logging depending on the parameter value
+func (c *Config) EnableConsoleLog(v bool) {
+	c.setValue("consoleLoggingEnabled", v)
+}
+
+// SetLogLevel configures the loglevel
+func (c *Config) SetLogLevel(l string) {
+	c.setValue("logLevel", l)
+}
+
+// EnableFileLogging enables and configures file logging if the filename is not empty
+func (c *Config) EnableFileLogging(f string, maxsize, maxrolls uint) {
+	c.Lock()
+	defer c.Unlock()
+	c.settings["logfile"] = f
+	c.settings["maxsize"] = maxsize
+	c.settings["maxrolls"] = maxrolls
+}
+
+// ConfigureSyslog enables and configures syslog if the syslogURI it not an empty string
+func (c *Config) ConfigureSyslog(syslogURI string, usetTLS bool) {
+	c.Lock()
+	defer c.Unlock()
+	c.settings["syslogURI"] = template.URL(syslogURI)
+	c.settings["syslogUseTLS"] = usetTLS
+
+}
+
+// NewSeelogConfig returns a SeelogConfig filled with correct parameters
+func NewSeelogConfig(name, level, format, jsonFormat, commonFormat string, syslogRFC bool) *Config {
+	c := &Config{settings: make(map[string]interface{})}
+	c.settings["loggerName"] = name
+	c.settings["format"] = format
+	c.settings["syslogRFC"] = syslogRFC
+	c.settings["jsonFormat"] = jsonFormat
+	c.settings["commonFormat"] = commonFormat
+	c.settings["logLevel"] = level
+	return c
+}

pkg/util/log/log.go
@@ -557,11 +557,24 @@ func GetLogLevel() (seelog.LogLevel, error) {
 	return seelog.InfoLvl, errors.New("cannot get loglevel: logger not initialized")
 }
 
-// ChangeLogLevel changes the current log level, valide levels are trace, debug, info, warn, error, critical and off
-func ChangeLogLevel(level string) error {
-	if logger != nil {
-		return logger.changeLogLevel(level)
+// ChangeLogLevel changes the current log level, valide levels are trace, debug,
+// info, warn, error, critical and off, it requires a new seelog logger because
+// an existing one cannot be updated
+func ChangeLogLevel(l seelog.LoggerInterface, level string) error {
+	if logger != nil && logger.inner != nil {
+		err := logger.changeLogLevel(level)
+		if err != nil {
+			return err
+		}
+		// See detailed explanation in SetupDatadogLogger(...)
+		err = l.SetAdditionalStackDepth(defaultStackDepth)
+		if err != nil {
+			return err
+		}
+
+		logger.replaceInnerLogger(l)
+		return nil
 	}
 	// need to return something, just set to Info (expected default)
-	return errors.New("cannot set loglevel: logger not initialized")
+	return errors.New("cannot change loglevel: logger not initialized")
 }

pkg/config/log_test.go
@@ -10,6 +10,7 @@ import (
 	"bytes"
 	"testing"
 
+	seelogCfg "github.com/DataDog/datadog-agent/pkg/config/seelog"
 	"github.com/cihub/seelog"
 	"github.com/stretchr/testify/assert"
 )
@@ -25,6 +26,19 @@ func TestExtractShortPathFromFullPath(t *testing.T) {
 	assert.Equal(t, "main.go", extractShortPathFromFullPath("main.go"))
 	// process agent
 	assert.Equal(t, "cmd/agent/collector.go", extractShortPathFromFullPath("/home/jenkins/workspace/process-agent-build-ddagent/go/src/github.com/DataDog/datadog-process-agent/cmd/agent/collector.go"))
+}
+
+func TestSeelogConfig(t *testing.T) {
+	cfg := seelogCfg.NewSeelogConfig("TEST", "off", "common", "", "", false)
+	cfg.EnableConsoleLog(true)
+	cfg.EnableFileLogging("/dev/null", 123, 456)
+
+	seelogConfigStr, err := cfg.Render()
+	assert.Nil(t, err)
+
+	logger, err := seelog.LoggerFromConfigAsString(seelogConfigStr)
+	assert.Nil(t, err)
+	assert.NotNil(t, logger)
 }
 
 func benchmarkLogFormat(logFormat string, b *testing.B) {

pkg/config/runtime_settings_test.go
@@ -6,11 +6,8 @@
 package config
 
 import (
-	"io/ioutil"
 	"testing"
 
-	"github.com/DataDog/datadog-agent/pkg/util/log"
-	"github.com/cihub/seelog"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -66,14 +63,12 @@ func TestRuntimeSettings(t *testing.T) {
 func TestLogLevel(t *testing.T) {
 	cleanRuntimeSetting()
 	setupConf()
-	l, err := seelog.LoggerFromWriterWithMinLevelAndFormat(ioutil.Discard, seelog.DebugLvl, "[%LEVEL] %FuncShort: %Msg")
-	assert.Nil(t, err)
-	log.SetupDatadogLogger(l, "debug")
+	SetupLogger("TEST", "debug", "", "", true, true, true)
 
 	ll := logLevelRuntimeSetting("log_level")
 	assert.Equal(t, "log_level", ll.Name())
 
-	err = ll.Set("off")
+	err := ll.Set("off")
 	assert.Nil(t, err)
 
 	v, err := ll.Get()
@@ -82,7 +77,7 @@ func TestLogLevel(t *testing.T) {
 
 	err = ll.Set("invalid")
 	assert.NotNil(t, err)
-	assert.Equal(t, "bad log level", err.Error())
+	assert.Equal(t, "declared minlevel not found: invalid", err.Error())
 
 	v, err = ll.Get()
 	assert.Equal(t, "off", v)