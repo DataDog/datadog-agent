555cb6732ca465280b39f4fe08900d9c81aa98b4
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2020-03-19 09:45:44 +0100

    Fix a flake in tests for Cloud Foundry listener (#5121)

--- pkg/autodiscovery/listeners/cloudfoundry.go
+++ pkg/autodiscovery/listeners/cloudfoundry.go
@@ -31,6 +31,7 @@ type CloudFoundryListener struct {
 	delService    chan<- Service
 	services      map[string]Service // maps ADIdentifiers to services
 	stop          chan bool
+	refreshCount  int64
 	refreshTicker *time.Ticker
 	bbsCache      cloudfoundry.BBSCacheI
 }
@@ -84,6 +85,7 @@ func (l *CloudFoundryListener) refreshServices(firstRun bool) {
 	// make sure that we can't have two simultaneous runs of this function
 	l.Lock()
 	defer l.Unlock()
+	l.refreshCount++
 	allActualLRPs, desiredLRPs := l.bbsCache.GetAllLRPs()
 
 	// if not found and running, add it
