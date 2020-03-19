d1ea61b1f2097ac227ab7310fb0437c28f6449d0
Remy Mathieu <remy.mathieu@datadoghq.com>
2020-03-19 10:00:12 +0100

    [dogstatsd+config] comment on the config field.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -300,6 +300,7 @@ func initConfig(config Config) {
 	config.BindEnvAndSetDefault("dogstatsd_string_interner_size", 4096)
 	// Enable check for Entity-ID presence when enriching Dogstatsd metrics with tags
 	config.BindEnvAndSetDefault("dogstatsd_entity_id_precedence", false)
+	// Sends Dogstatsd parse errors to the Debug level instead of the Error level
 	config.BindEnvAndSetDefault("dogstatsd_disable_verbose_logs", false)
 	config.SetKnown("dogstatsd_mapper_profiles")
 
