7b24cc8ac188feaa18772249a6389d4fea9596e3
Marco Costa <marco.costa@datadoghq.com>
2020-03-24 11:39:35 -0400

    Update cmd/cluster-agent-cloudfoundry/app/app.go
    Co-Authored-By: Gabriel Aszalos <gabriel.aszalos@gmail.com>

--- cmd/cluster-agent-cloudfoundry/app/app.go
+++ cmd/cluster-agent-cloudfoundry/app/app.go
@@ -204,8 +204,7 @@ func run(cmd *cobra.Command, args []string) error {
 
 	if config.Datadog.GetBool("cluster_agent.admission_controller.enabled") {
 		log.Info("Starting admission controller server")
-
-		if err = admissioncontroller.StartServer(); err != nil {
+		if err := admissioncontroller.StartServer(); err != nil {
 			return log.Errorf("Error while starting admission controller server, exiting: %v", err)
 		}
 	}
