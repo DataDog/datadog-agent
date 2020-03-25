3032eb14a1108dd8f2a80598143fbc7cef96fe38
Julien Lebot <julien.lebot@datadoghq.com>
2020-03-25 14:08:22 +0100

    Fix powershell reference

--- tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
+++ tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
@@ -1,2 +1,2 @@
-agentVersion=(inv agent.version) | Select-String -Pattern "\d+.\d+.\d+" | ForEach-Object{$_.Matches[0].Value}
+$agentVersion=(inv agent.version) | Select-String -Pattern "\d+.\d+.\d+" | ForEach-Object{$_.Matches[0].Value}
 choco pack --version=$agentVersion --out=c:\mnt\build-out c:\mnt\chocolatey\datadog-agent.nuspec
