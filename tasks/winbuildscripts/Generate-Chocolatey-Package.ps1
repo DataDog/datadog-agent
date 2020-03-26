31c0c096ce0877c0df33131431c97e53ffd5c066
Julien Lebot <julien.lebot@datadoghq.com>
2020-03-26 17:17:03 +0100

    Create output directory if not exists

--- tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
+++ tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
@@ -1,2 +1,9 @@
+$outputDirectory = "c:\mnt\build-out"
 $agentVersion=(inv agent.version) | Select-String -Pattern "\d+.\d+.\d+" | ForEach-Object{$_.Matches[0].Value}
-choco pack --version=$agentVersion --out=c:\mnt\build-out c:\mnt\chocolatey\datadog-agent.nuspec
+Write-Host "Generating Chocolatey package for $agentVersion in $outputDirectory"
+
+if (!(Test-Path $outputDirectory)){
+    New-Item -ItemType Directory -Path $outputDirectory
+}
+
+choco pack --version=$agentVersion --out=$outputDirectory c:\mnt\chocolatey\datadog-agent.nuspec
