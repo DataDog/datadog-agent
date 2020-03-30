5b54ea0a8f40234e01a377086653794dc693dd4d
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-03-30 10:38:22 +0200

    filtered

--- omnibus/config/software/datadog-agent-integrations-py3.rb
+++ omnibus/config/software/datadog-agent-integrations-py3.rb
@@ -94,7 +94,7 @@ end
 
 final_constraints_file = 'final_constraints-py3.txt'
 agent_requirements_file = 'agent_requirements-py3.txt'
-filtered_agent_requirements_txt = 'agent_requirements-py3.in'
+filtered_agent_requirements_txt = 'agent_requirements-py3-filtered.txt'
 
 if windows?
   agent_requirements_txt = 'agent_requirements-windows-py3.txt'
@@ -184,10 +184,14 @@ build do
       command "#{python} -m pip install --no-deps  #{windows_safe_path(project_dir)}\\datadog_checks_base"
       command "#{python} -m pip install --no-deps  #{windows_safe_path(project_dir)}\\datadog_checks_downloader --install-option=\"--install-scripts=#{windows_safe_path(install_dir)}/bin\""
       command "cp #{static_reqs_out_file} #{windows_safe_path(install_dir)}\\#{agent_requirements_file}"
+      command "mkdir -p #{ENV['OMNIBUS_PACKAGE_DIR']}"
+      command "cp #{static_reqs_out_file} #{ENV['OMNIBUS_PACKAGE_DIR']}/"
     else
       command "#{pip} install --no-deps .", :env => nix_build_env, :cwd => "#{project_dir}/datadog_checks_base"
       command "#{pip} install --no-deps .", :env => nix_build_env, :cwd => "#{project_dir}/datadog_checks_downloader"
       command "cp #{static_reqs_out_file} #{install_dir}/#{agent_requirements_file}"
+      command "mkdir -p #{ENV['OMNIBUS_PACKAGE_DIR']}"
+      command "cp #{static_reqs_out_file} #{ENV['OMNIBUS_PACKAGE_DIR']}/"
     end
 
     # From now on we don't need piptools anymore, uninstall its deps so we don't include them in the final artifact
