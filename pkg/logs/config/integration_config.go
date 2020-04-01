c917f085d2caa5a436feacbc93db924655d052fd
Pierre Rognant <59561183+prognant@users.noreply.github.com>
2020-04-01 18:31:02 +0200

    Add the ability to tail log file from the beginning (#5091)
    Log integration configs now accept a new parameters named `start_position`.
    If it is set to `beginning` then new log file will be read from the beginning.
    If it is set to `end` then new log file will be read from the end (current
    behaviour). If a file is known then tailing will continue from the previous
    known location, else the `start_position` setting will be honoured.

--- pkg/logs/config/integration_config.go
+++ pkg/logs/config/integration_config.go
@@ -7,6 +7,7 @@ package config
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Logs source types
@@ -27,7 +28,8 @@ type LogsConfig struct {
 	Port int    // Network
 	Path string // File, Journald
 
-	ExcludePaths []string `mapstructure:"exclude_paths" json:"exclude_paths"` // File
+	ExcludePaths []string `mapstructure:"exclude_paths" json:"exclude_paths"`   // File
+	TailingMode  string   `mapstructure:"start_position" json:"start_position"` // File
 
 	IncludeUnits  []string `mapstructure:"include_units" json:"include_units"`   // Journald
 	ExcludeUnits  []string `mapstructure:"exclude_units" json:"exclude_units"`   // Journald
@@ -48,6 +50,47 @@ type LogsConfig struct {
 	ProcessingRules []*ProcessingRule `mapstructure:"log_processing_rules" json:"log_processing_rules"`
 }
 
+// TailingMode type
+type TailingMode uint8
+
+// Tailing Modes
+const (
+	ForceBeginning = iota
+	ForceEnd
+	Beginning
+	End
+)
+
+var tailingModeTuples = []struct {
+	s string
+	m TailingMode
+}{
+	{"forceBeginning", ForceBeginning},
+	{"forceEnd", ForceEnd},
+	{"beginning", Beginning},
+	{"end", End},
+}
+
+// TailingModeFromString parses a string and returns a corresponding tailing mode, default to End if not found
+func TailingModeFromString(mode string) (TailingMode, bool) {
+	for _, t := range tailingModeTuples {
+		if t.s == mode {
+			return t.m, true
+		}
+	}
+	return End, false
+}
+
+// TailingModeToString returns seelog string representation for a specified tailing mode. Returns "" for invalid tailing mode.
+func (mode TailingMode) String() string {
+	for _, t := range tailingModeTuples {
+		if t.m == mode {
+			return t.s
+		}
+	}
+	return ""
+}
+
 // Validate returns an error if the config is misconfigured
 func (c *LogsConfig) Validate() error {
 	switch {
@@ -56,8 +99,14 @@ func (c *LogsConfig) Validate() error {
 		// an autodiscovery label because so we must override it at some point,
 		// this check is mostly used for sanity purposed to detect an override miss.
 		return fmt.Errorf("a config must have a type")
-	case c.Type == FileType && c.Path == "":
-		return fmt.Errorf("file source must have a path")
+	case c.Type == FileType:
+		if c.Path == "" {
+			return fmt.Errorf("file source must have a path")
+		}
+		err := c.validateTailingMode()
+		if err != nil {
+			return err
+		}
 	case c.Type == TCPType && c.Port == 0:
 		return fmt.Errorf("tcp source must have a port")
 	case c.Type == UDPType && c.Port == 0:
@@ -68,4 +117,20 @@ func (c *LogsConfig) Validate() error {
 		return err
 	}
 	return CompileProcessingRules(c.ProcessingRules)
+}
+
+func (c *LogsConfig) validateTailingMode() error {
+	mode, found := TailingModeFromString(c.TailingMode)
+	if !found && c.TailingMode != "" {
+		return fmt.Errorf("invalid tailing mode '%v' for %v", mode, c.Path)
+	}
+	if ContainsWildcard(c.Path) && (mode == Beginning || mode == ForceBeginning) {
+		return fmt.Errorf("tailing from the beginning is not supported for wildcard path %v", c.Path)
+	}
+	return nil
+}
+
+// ContainsWildcard returns true if the path contains any wildcard character
+func ContainsWildcard(path string) bool {
+	return strings.ContainsAny(path, "*?[")
 }
