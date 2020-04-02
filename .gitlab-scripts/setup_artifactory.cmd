bfcdccc0505f3f412b8e83ae57f7b1355650e37e
vishal kumar <vk7282@gmail.com>
2020-04-03 00:00:13 +0530

    fix build

--- .gitlab-scripts/setup_artifactory.cmd
+++ .gitlab-scripts/setup_artifactory.cmd
@@ -1,4 +1,1 @@
-## python artifactory dependency
-mkdir %APPDATA%\pip
-echo "[global]" > %APPDATA%\pip\pip.ini
-echo "extra-index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@$ARTIFACTORY_URL/simple" >> %APPDATA%\pip\pip.ini
+if exist %APPDATA%\pip rd /s/q %APPDATA%\pip
