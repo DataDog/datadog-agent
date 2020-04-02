dba1bd0cedd53a9e15d9e1b921c9eb428058c3dd
Remy Mathieu <remy.mathieu@datadoghq.com>
2020-04-02 21:44:57 +0200

    [telemetry+dogstatsd] avoid heap escape in telemetry.Counter
    The `...string` syntax used in the telemetry pkg escapes the strings on
    the heap. Because it is in a hot path, it's important having access to
    methods not creating any heap escape.
    
    This PR is adding those methods to the `telemetry.Counter` type. This
    will reduce the total garbage memory generated by the Dogstatsd `parseMetricMessage`
    and help reduce memory allocation during the spike of metrics sent to Dogstatsd.

--- pkg/telemetry/prom_counter.go
+++ pkg/telemetry/prom_counter.go
@@ -14,17 +14,38 @@ type promCounter struct {
 	pc *prometheus.CounterVec
 }
 
-// Add adds the given value to the counter for the given tags.
+// Add adds the given value to the counter with the given tags value.
 func (c *promCounter) Add(value float64, tagsValue ...string) {
 	c.pc.WithLabelValues(tagsValue...).Add(value)
 }
 
-// Inc increments the counter for the given tags.
+// AddWithTags adds the given value to the counter with the given tags.
+// Even if less convenient, this signature could be used in hot path
+// instead of Add(float64, ...string) to avoid escaping the parameters on the heap.
+func (c *promCounter) AddWithTags(value float64, tags map[string]string) {
+	c.pc.With(tags).Add(value)
+}
+
+// Inc increments the counter with the given tags value.
 func (c *promCounter) Inc(tagsValue ...string) {
 	c.pc.WithLabelValues(tagsValue...).Inc()
 }
 
-// Delete deletes the value for the counter with the given tags.
+// IncWithTags increments the counter with the given tags.
+// Even if less convenient, this signature could be used in hot path
+// instead of Inc(...string) to avoid escaping the parameters on the heap.
+func (c *promCounter) IncWithTags(tags map[string]string) {
+	c.pc.With(tags).Inc()
+}
+
+// Delete deletes the value for the counter with the given tags value.
 func (c *promCounter) Delete(tagsValue ...string) {
 	c.pc.DeleteLabelValues(tagsValue...)
+}
+
+// DeleteWithTags deletes the value for the counter with the given tags.
+// Even if less convenient, this signature could be used in hot path
+// instead of Delete(...string) to avoid escaping the parameters on the heap.
+func (c *promCounter) DeleteWithTags(tags map[string]string) {
+	c.pc.Delete(tags)
 }
