f79cc35dafce871336ecca542521dda169463a21
vishal kumar <vk7282@gmail.com>
2020-04-02 21:54:06 +0530

    window support for custom artifactory

---
+++ .gitlab-scripts/setup_artifactory.cmd
@@ -0,0 +1,4 @@
+## python artifactory dependency
+mkdir %APPDATA%\pip
+echo "[global]" > %APPDATA%\pip\pip.ini
+echo "extra-index-url = https://$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD@$ARTIFACTORY_URL/simple" >> %APPDATA%\pip\pip.ini
