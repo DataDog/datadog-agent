9289c4104848eac76b7f4739ee838a76f989d0cf
Spencer Brown <spencer.brown@datadoghq.com>
2020-04-03 17:06:56 -0400

    Update licesnses and error message

--- pkg/status/status_system_probe_unsupported.go
+++ pkg/status/status_system_probe_unsupported.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
 // +build zlib
 
 package status
@@ -8,6 +13,6 @@ import (
 
 func getSystemProbeStats() map[string]interface{} {
 	return map[string]interface{}{
-		"Errors": fmt.Sprintf("System Probe Unsupported"),
+		"Errors": fmt.Sprintf("System Probe is not supported when built without zstd support"),
 	}
 }
