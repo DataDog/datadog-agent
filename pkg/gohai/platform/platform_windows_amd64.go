43780cd5e237ad6249a3f7dfd5ea0017e0a35653
Derek Brown <derek.brown@datadoghq.com>
2020-04-16 13:22:58 -0700

    Add additional windows information so that we can use gohai instead of (#74)
    * Add additional windows information so that we can use gohai instead of
    psutil for getting windows system & processor information
    
    * Remove empty line
    `
    
    * Run gofmt
    
    * Fix backwards check for error
    
    Co-authored-by: Derek Brown <db@dereks-mbp.browns.local>

--- pkg/gohai/platform/platform_windows_amd64.go
+++ pkg/gohai/platform/platform_windows_amd64.go
@@ -13,6 +13,15 @@ type WKSTA_INFO_100 struct {
 	wki100_ver_minor    uint32
 }
 
+type SERVER_INFO_101 struct {
+	sv101_platform_id   uint32
+	sv101_name          string
+	sv101_version_major uint32
+	sv101_version_minor uint32
+	sv101_type          uint32
+	sv101_comment       string
+}
+
 func byteArrayToWksaInfo(data []byte) (info WKSTA_INFO_100) {
 	info.wki100_platform_id = binary.LittleEndian.Uint32(data)
 
@@ -42,5 +51,24 @@ func platGetVersion(outdata *byte) (maj uint64, min uint64, err error) {
 	info = byteArrayToWksaInfo(dataptr)
 	maj = uint64(info.wki100_ver_major)
 	min = uint64(info.wki100_ver_minor)
+	return
+}
+
+func platGetServerInfo(data *byte) (si101 SERVER_INFO_101) {
+	var outdata []byte
+	outdata = (*[40]byte)(unsafe.Pointer(data))[:]
+	si101.sv101_platform_id = binary.LittleEndian.Uint32(outdata)
+
+	// due to 64 bit packing, skip 8 bytes to get to the name string
+	//stringptr := *(*[]uint16)(unsafe.Pointer(uintptr(binary.LittleEndian.Uint64(outdata[8:]))))
+	//si101.sv101_name = convert_windows_string(stringptr)
+
+	si101.sv101_version_major = binary.LittleEndian.Uint32(outdata[16:])
+	si101.sv101_version_minor = binary.LittleEndian.Uint32(outdata[20:])
+	si101.sv101_type = binary.LittleEndian.Uint32(outdata[24:])
+
+	// again skip 4 more for byte packing, so start at 32
+	//stringptr = (*[]uint16)(unsafe.Pointer(uintptr(binary.LittleEndian.Uint32(outdata[32:]))))
+	//si101.sv101_comment = convert_windows_string(*stringptr)
 	return
 }
