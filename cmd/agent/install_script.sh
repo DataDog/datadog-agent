85e7c2584e47880ede704e67623d73073a607a17
Karim Bogtob <karim.bogtob@datadoghq.com>
2020-04-20 15:38:08 +0200

    Update install-script to add install_info

--- cmd/agent/install_script.sh
+++ cmd/agent/install_script.sh
@@ -6,6 +6,7 @@
 # using the package manager and Datadog repositories.
 
 set -e
+install_script_version=7.19.0
 logfile="ddagent-install.log"
 
 LEGACY_ETCDIR="/etc/dd-agent"
@@ -342,6 +343,14 @@ else
   $sudo_cmd chown dd-agent:dd-agent $CONF
   $sudo_cmd chmod 640 $CONF
 fi
+
+# Creating or overriding the install information
+echo "---
+install_method:
+  name: install_script
+  tool: install_script
+  version: $install_script_version
+" > $ETCDIR/install_info
 
 # On SUSE 11, sudo service datadog-agent start fails (because /sbin is not in a base user's path)
 # However, sudo /sbin/service datadog-agent does work.
