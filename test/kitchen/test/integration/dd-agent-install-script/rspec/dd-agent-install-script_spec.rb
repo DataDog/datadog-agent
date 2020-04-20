1afd0cc1f06b50a5d95d8ad259e46b1681eae652
Karim Bogtob <karim.bogtob@datadoghq.com>
2020-04-20 16:09:28 +0200

    Add kitchen tests for install info

--- test/kitchen/test/integration/dd-agent-install-script/rspec/dd-agent-install-script_spec.rb
+++ test/kitchen/test/integration/dd-agent-install-script/rspec/dd-agent-install-script_spec.rb
@@ -20,4 +20,26 @@ describe 'dd-agent-installation-script' do
       expect(config['site']).to eq 'datadoghq.eu'
     end
   end
+
+  context 'when testing the install infos' do
+    let(:install_info_path) do
+      if os == :windows
+        conf_path = "#{ENV['ProgramData']}\\Datadog\\install_info"
+      else
+        conf_path = '/etc/datadog-agent/install_info'
+      end
+    end
+
+    let(:install_info) do
+      YAML.load_file(install_info_path)
+    end
+
+    it 'adds an install_info' do
+      expect(install_info['install_method']).to match(
+        'name' => 'install_script',
+        'tool' => 'install_script',
+        'version' => /^\d+\.\d+\.\d+$/
+      )
+    end
+  end
 end
