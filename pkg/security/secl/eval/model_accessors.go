4250e17906b7d4e6975e4acbec9f95773354dcab
Sylvain Afchain <safchain@gmail.com>
2020-04-22 18:46:16 +0200

    add partial eval and test

--- pkg/security/secl/eval/model_accessors.go
+++ pkg/security/secl/eval/model_accessors.go
@@ -16,97 +16,97 @@ func GetAccessor(key string) (interface{}, []string, error) {
 	case "container.id":
 
 		return &StringEvaluator{
-			Eval:       func(ctx *Context) string { return ctx.Event.Container.ID },
-			DebugEval:  func(ctx *Context) string { return ctx.Event.Container.ID },
-			ModelField: "Container.ID",
+			Eval:      func(ctx *Context) string { return ctx.Event.Container.ID },
+			DebugEval: func(ctx *Context) string { return ctx.Event.Container.ID },
+			Field:     key,
 		}, []string{"container"}, nil
 
 	case "open.filename":
 
 		return &StringEvaluator{
-			Eval:       func(ctx *Context) string { return ctx.Event.Open.Filename },
-			DebugEval:  func(ctx *Context) string { return ctx.Event.Open.Filename },
-			ModelField: "Open.Filename",
+			Eval:      func(ctx *Context) string { return ctx.Event.Open.Filename },
+			DebugEval: func(ctx *Context) string { return ctx.Event.Open.Filename },
+			Field:     key,
 		}, []string{"fs"}, nil
 
 	case "open.flags":
 
 		return &IntEvaluator{
-			Eval:       func(ctx *Context) int { return int(ctx.Event.Open.Flags) },
-			DebugEval:  func(ctx *Context) int { return int(ctx.Event.Open.Flags) },
-			ModelField: "Open.Flags",
+			Eval:      func(ctx *Context) int { return int(ctx.Event.Open.Flags) },
+			DebugEval: func(ctx *Context) int { return int(ctx.Event.Open.Flags) },
+			Field:     key,
 		}, []string{}, nil
 
 	case "open.mode":
 
 		return &IntEvaluator{
-			Eval:       func(ctx *Context) int { return int(ctx.Event.Open.Mode) },
-			DebugEval:  func(ctx *Context) int { return int(ctx.Event.Open.Mode) },
-			ModelField: "Open.Mode",
+			Eval:      func(ctx *Context) int { return int(ctx.Event.Open.Mode) },
+			DebugEval: func(ctx *Context) int { return int(ctx.Event.Open.Mode) },
+			Field:     key,
 		}, []string{}, nil
 
 	case "process.gid":
 
 		return &IntEvaluator{
-			Eval:       func(ctx *Context) int { return int(ctx.Event.Process.GID) },
-			DebugEval:  func(ctx *Context) int { return int(ctx.Event.Process.GID) },
-			ModelField: "Process.GID",
+			Eval:      func(ctx *Context) int { return int(ctx.Event.Process.GID) },
+			DebugEval: func(ctx *Context) int { return int(ctx.Event.Process.GID) },
+			Field:     key,
 		}, []string{}, nil
 
 	case "process.name":
 
 		return &StringEvaluator{
-			Eval:       func(ctx *Context) string { return ctx.Event.Process.Name },
-			DebugEval:  func(ctx *Context) string { return ctx.Event.Process.Name },
-			ModelField: "Process.Name",
+			Eval:      func(ctx *Context) string { return ctx.Event.Process.Name },
+			DebugEval: func(ctx *Context) string { return ctx.Event.Process.Name },
+			Field:     key,
 		}, []string{"process"}, nil
 
 	case "process.pid":
 
 		return &IntEvaluator{
-			Eval:       func(ctx *Context) int { return int(ctx.Event.Process.PID) },
-			DebugEval:  func(ctx *Context) int { return int(ctx.Event.Process.PID) },
-			ModelField: "Process.PID",
+			Eval:      func(ctx *Context) int { return int(ctx.Event.Process.PID) },
+			DebugEval: func(ctx *Context) int { return int(ctx.Event.Process.PID) },
+			Field:     key,
 		}, []string{}, nil
 
 	case "process.uid":
 
 		return &IntEvaluator{
-			Eval:       func(ctx *Context) int { return int(ctx.Event.Process.UID) },
-			DebugEval:  func(ctx *Context) int { return int(ctx.Event.Process.UID) },
-			ModelField: "Process.UID",
+			Eval:      func(ctx *Context) int { return int(ctx.Event.Process.UID) },
+			DebugEval: func(ctx *Context) int { return int(ctx.Event.Process.UID) },
+			Field:     key,
 		}, []string{}, nil
 
 	case "rename.newname":
 
 		return &StringEvaluator{
-			Eval:       func(ctx *Context) string { return ctx.Event.Rename.NewName },
-			DebugEval:  func(ctx *Context) string { return ctx.Event.Rename.NewName },
-			ModelField: "Rename.NewName",
+			Eval:      func(ctx *Context) string { return ctx.Event.Rename.NewName },
+			DebugEval: func(ctx *Context) string { return ctx.Event.Rename.NewName },
+			Field:     key,
 		}, []string{"fs"}, nil
 
 	case "rename.oldname":
 
 		return &StringEvaluator{
-			Eval:       func(ctx *Context) string { return ctx.Event.Rename.OldName },
-			DebugEval:  func(ctx *Context) string { return ctx.Event.Rename.OldName },
-			ModelField: "Rename.OldName",
+			Eval:      func(ctx *Context) string { return ctx.Event.Rename.OldName },
+			DebugEval: func(ctx *Context) string { return ctx.Event.Rename.OldName },
+			Field:     key,
 		}, []string{"fs"}, nil
 
 	case "syscall":
 
 		return &StringEvaluator{
-			Eval:       func(ctx *Context) string { return ctx.Event.Syscall },
-			DebugEval:  func(ctx *Context) string { return ctx.Event.Syscall },
-			ModelField: "Syscall",
+			Eval:      func(ctx *Context) string { return ctx.Event.Syscall },
+			DebugEval: func(ctx *Context) string { return ctx.Event.Syscall },
+			Field:     key,
 		}, []string{}, nil
 
 	case "unlink.filename":
 
 		return &StringEvaluator{
-			Eval:       func(ctx *Context) string { return ctx.Event.Unlink.Filename },
-			DebugEval:  func(ctx *Context) string { return ctx.Event.Unlink.Filename },
-			ModelField: "Unlink.Filename",
+			Eval:      func(ctx *Context) string { return ctx.Event.Unlink.Filename },
+			DebugEval: func(ctx *Context) string { return ctx.Event.Unlink.Filename },
+			Field:     key,
 		}, []string{"fs"}, nil
 
 	}
