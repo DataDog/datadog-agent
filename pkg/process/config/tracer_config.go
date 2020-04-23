971b1cb09e6a014fdb869d8c127b1b9d26cab9e1
Pedro Lambert <pedrolambert@gmail.com>
2020-04-23 11:58:46 -0400

    [system-probe] Change ENOBUF param name and default value

--- pkg/process/config/tracer_config.go
+++ pkg/process/config/tracer_config.go
@@ -52,7 +52,7 @@ func SysProbeConfigFromConfig(cfg *AgentConfig) *ebpf.Config {
 	tracerConfig.ProcRoot = util.GetProcRoot()
 	tracerConfig.BPFDebug = cfg.SysProbeBPFDebug
 	tracerConfig.EnableConntrack = cfg.EnableConntrack
-	tracerConfig.EnableENOBUFS = cfg.EnableENOBUFS
+	tracerConfig.ConntrackIgnoreENOBUFS = cfg.ConntrackIgnoreENOBUFS
 	tracerConfig.ConntrackMaxStateSize = cfg.ConntrackMaxStateSize
 	tracerConfig.DebugPort = cfg.SystemProbeDebugPort
 
