2c49f14da8806cfc2f43ded29fc6ed97301905b3
Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>
2020-05-14 12:34:11 +0200

    improve aks clustername detection (#5495)

--- pkg/util/azure/azure.go
+++ pkg/util/azure/azure.go
@@ -6,7 +6,6 @@
 package azure
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -46,7 +45,8 @@ func GetHostAlias() (string, error) {
 	return res, nil
 }
 
-// GetClusterName returns the name of the cluster containing the current VM
+// GetClusterName returns the name of the cluster containing the current VM by parsing the resource group name.
+// It expects the resource group name to have the format (MC|mc)_resource-group_cluster-name_zone
 func GetClusterName() (string, error) {
 	if !config.IsCloudProviderEnabled(CloudProviderName) {
 		return "", fmt.Errorf("cloud provider is disabled by configuration")
@@ -57,12 +57,11 @@ func GetClusterName() (string, error) {
 	}
 
 	splitAll := strings.Split(all, "_")
-	if len(splitAll) < 4 || splitAll[0] != "MC" {
-		return "", errors.New("cannot parse the clustername from metadata")
+	if len(splitAll) < 4 || strings.ToLower(splitAll[0]) != "mc" {
+		return "", fmt.Errorf("cannot parse the clustername from resource group name: %s", all)
 	}
 
-	clusterName := splitAll[len(splitAll)-2]
-	return clusterName, nil
+	return splitAll[len(splitAll)-2], nil
 }
 
 func getResponseWithMaxLength(endpoint string, maxLength int) (string, error) {
