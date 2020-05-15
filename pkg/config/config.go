a1c6babcab31b27d7559be906b61886ffb8a5d6e
Olivier Vielpeau <olivielpeau@users.noreply.github.com>
2020-05-15 23:54:12 +0200

    [forwarder] Allow configuring the api key validation interval (#5543)
    And log at warning level if an api key is invalid

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -34,6 +34,8 @@ const (
 	DefaultNumWorkers = 4
 	// MaxNumWorkers maximum number of workers for our check runner
 	MaxNumWorkers = 25
+	// DefaultAPIKeyValidationInterval is the default interval of api key validation checks
+	DefaultAPIKeyValidationInterval = 60
 
 	// DefaultForwarderRecoveryInterval is the default recovery interval,
 	// also used if the user-provided value is invalid.
@@ -292,7 +294,8 @@ func initConfig(config Config) {
 	config.BindEnvAndSetDefault("additional_endpoints", map[string][]string{})
 	config.BindEnvAndSetDefault("forwarder_timeout", 20)
 	config.BindEnvAndSetDefault("forwarder_retry_queue_max_size", 30)
-	config.BindEnvAndSetDefault("forwarder_connection_reset_interval", 0) // in seconds, 0 means disabled
+	config.BindEnvAndSetDefault("forwarder_connection_reset_interval", 0)                                // in seconds, 0 means disabled
+	config.BindEnvAndSetDefault("forwarder_apikey_validation_interval", DefaultAPIKeyValidationInterval) // in minutes
 	config.BindEnvAndSetDefault("forwarder_num_workers", 1)
 	config.BindEnvAndSetDefault("forwarder_stop_timeout", 2)
 	// Forwarder retry settings
