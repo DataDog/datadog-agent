f320c231c0761e0222fde12a959b897cdb0483b0
Ivan Ilichev <ivan.ilichev@datadoghq.com>
2020-05-18 23:11:47 -0400

    Implement file owner user/group lookup and reporting

--- pkg/compliance/checks/file_check_test.go
+++ pkg/compliance/checks/file_check_test.go
@@ -12,30 +12,75 @@ import (
 )
 
 func TestFileCheck(t *testing.T) {
-	reporter := &compliance.MockReporter{}
-
-	fc := &fileCheck{
-		baseCheck: newTestBaseCheck(reporter),
-		File: &compliance.File{
-			Path: "./testdata/644.dat",
-			Report: compliance.Report{
-				{
-					Property: "permissions",
-					Kind:     "attribute",
+	tests := []struct {
+		name       string
+		file       *compliance.File
+		expectedKV compliance.KV
+	}{
+		{
+			name: "permissions",
+			file: &compliance.File{
+				Path: "./testdata/file/644.dat",
+				Report: compliance.Report{
+					{
+						Property: "permissions",
+						Kind:     "attribute",
+					},
 				},
+			},
+			expectedKV: compliance.KV{
+				"permissions": "644",
+			},
+		},
+		{
+			name: "owner nobody:nobody",
+			file: &compliance.File{
+				Path: "./testdata/file/nobody-nobody.dat",
+				Report: compliance.Report{
+					{
+						Property: "owner",
+						Kind:     "attribute",
+					},
+				},
+			},
+			expectedKV: compliance.KV{
+				"owner": "nobody:nobody",
+			},
+		},
+		{
+			name: "owner 2048:2048",
+			file: &compliance.File{
+				Path: "./testdata/file/2048-2048.dat",
+				Report: compliance.Report{
+					{
+						Property: "owner",
+						Kind:     "attribute",
+					},
+				},
+			},
+			expectedKV: compliance.KV{
+				"owner": "2048:2048",
 			},
 		},
 	}
-	reporter.On(
-		"Report",
-		newTestRuleEvent(
-			nil,
-			compliance.KV{
-				"permissions": "644",
-			},
-		),
-	).Once()
 
-	err := fc.Run()
-	assert.NoError(t, err)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			reporter := &compliance.MockReporter{}
+			fc := &fileCheck{
+				baseCheck: newTestBaseCheck(reporter),
+				File:      test.file,
+			}
+			reporter.On(
+				"Report",
+				newTestRuleEvent(
+					nil,
+					test.expectedKV,
+				),
+			).Once()
+
+			err := fc.Run()
+			assert.NoError(t, err)
+		})
+	}
 }
