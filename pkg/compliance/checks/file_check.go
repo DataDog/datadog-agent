d156a2a93d8e6709701d18cb1f2f06fd5718df13
Ivan Ilichev <ivan.ilichev@datadoghq.com>
2020-05-18 23:11:47 -0400

    Implement file owner user/group lookup and reporting

--- pkg/compliance/checks/file_check.go
+++ pkg/compliance/checks/file_check.go
@@ -6,11 +6,23 @@
 package checks
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"os/user"
+	"strconv"
+	"syscall"
 
 	"github.com/DataDog/datadog-agent/pkg/compliance"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+var (
+	// ErrPropertyKindNotSupported is returned for property kinds not supported by the check
+	ErrPropertyKindNotSupported = errors.New("property kind not supported")
+
+	// ErrPropertyNotSupported is returned for properties not supported by the check
+	ErrPropertyNotSupported = errors.New("property not supported")
 )
 
 type fileCheck struct {
@@ -31,16 +43,69 @@ func (c *fileCheck) Run() error {
 
 func (c *fileCheck) reportFile(filePath string) error {
 	kv := compliance.KV{}
+	var v string
+
 	fi, err := os.Stat(filePath)
 	if err != nil {
 		return log.Errorf("failed to stat %s", filePath)
 	}
 
 	for _, field := range c.File.Report {
-		if field.Property == "permissions" {
-			kv["permissions"] = fmt.Sprintf("%3o", fi.Mode()&os.ModePerm)
+
+		key := field.As
+
+		if field.Value != "" {
+			if key == "" {
+				// TODO: error here
+				continue
+			}
+
+			kv[key] = field.Value
+			continue
 		}
+
+		switch field.Kind {
+		case compliance.PropertyKindAttribute:
+			v, err = c.getAttribute(fi, field.Property)
+		case compliance.PropertyKindJSONPath:
+			v, err = c.getJSONPathValue(filePath, field.Property)
+		default:
+			return ErrPropertyKindNotSupported
+		}
+
+		if key == "" {
+			key = field.Property
+		}
+
+		kv[key] = v
 	}
 	c.report(nil, kv)
 	return nil
+}
+
+func (c *fileCheck) getAttribute(fi os.FileInfo, property string) (string, error) {
+	switch property {
+	case "permissions":
+		return fmt.Sprintf("%3o", fi.Mode()&os.ModePerm), nil
+	case "owner":
+		if statt, ok := fi.Sys().(*syscall.Stat_t); ok {
+			var (
+				u = strconv.Itoa(int(statt.Gid))
+				g = strconv.Itoa(int(statt.Uid))
+			)
+			if group, err := user.LookupGroupId(g); err == nil {
+				g = group.Name
+			}
+			if user, err := user.LookupId(u); err == nil {
+				u = user.Username
+			}
+			return fmt.Sprintf("%s:%s", u, g), nil
+		}
+	}
+	return "", ErrPropertyNotSupported
+}
+
+func (c *fileCheck) getJSONPathValue(filePath string, jsonPath string) (string, error) {
+	// TODO: implement
+	return "", nil
 }
