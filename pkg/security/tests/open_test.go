8752e031374f5d507ad087c9beaad95f851b2b48
Sylvain Baubeau <lebauce@gmail.com>
2020-05-22 18:56:38 +0200

    Add benchmarks on open

--- pkg/security/tests/open_test.go
+++ pkg/security/tests/open_test.go
@@ -1,7 +1,10 @@
 package tests
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"path"
 	"syscall"
 	"testing"
 
@@ -27,7 +30,7 @@ func TestOpen(t *testing.T) {
 
 	fd, _, errno := syscall.Syscall(syscall.SYS_OPENAT, 0, uintptr(testFilePtr), syscall.O_CREAT)
 	if errno != 0 {
-		t.Fatal(err)
+		t.Fatal(error(errno))
 	}
 	defer syscall.Close(int(fd))
 	defer os.Remove(testFile)
@@ -44,4 +47,97 @@ func TestOpen(t *testing.T) {
 			t.Errorf("expected open mode O_CREAT, got %d", flags)
 		}
 	}
+}
+
+func benchmarkOpenSameFile(b *testing.B, rules ...*policy.RuleDefinition) {
+	test, err := newSimpleTest(nil, rules)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer test.Close()
+
+	testFile, _, err := test.Path("benchtest")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		fd, err := syscall.Open(testFile, syscall.O_CREAT, 0777)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		if err := syscall.Close(fd); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkOpen(b *testing.B) {
+	rule := &policy.RuleDefinition{
+		ID:         "test-rule",
+		Expression: `open.filename == "{{.Root}}/donotmatch"`,
+	}
+
+	benchmarkOpenSameFile(b, rule)
+}
+
+func BenchmarkOpenNoKprobe(b *testing.B) {
+	benchmarkOpenSameFile(b)
+}
+
+func createFolder(current string, filesPerFolder, maxDepth int) error {
+	os.MkdirAll(current, 0777)
+
+	for i := 0; i < filesPerFolder; i++ {
+		f, err := os.Create(path.Join(current, fmt.Sprintf("file%d", i)))
+		if err != nil {
+			return err
+		}
+		if err := f.Close(); err != nil {
+			return err
+		}
+	}
+
+	if maxDepth > 0 {
+		if err := createFolder(path.Join(current, fmt.Sprintf("dir%d", maxDepth)), filesPerFolder, maxDepth-1); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func benchmarkFind(b *testing.B, filesPerFolder, maxDepth int, rules ...*policy.RuleDefinition) {
+	test, err := newSimpleTest(nil, rules)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer test.Close()
+
+	if err := createFolder(test.Root(), filesPerFolder, maxDepth); err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		findCmd := exec.Command("/usr/bin/find", test.Root())
+		if err := findCmd.Run(); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkFind(b *testing.B) {
+	benchmarkFind(b, 128, 8, &policy.RuleDefinition{
+		ID:         "test-rule",
+		Expression: `open.filename == "{{.Root}}/donotmatch"`,
+	})
+}
+
+func BenchmarkFindNoKprobe(b *testing.B) {
+	benchmarkFind(b, 128, 8)
 }
