22723bacef91b8823a26e515bdfc0de83d5fc9c6
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2020-06-03 15:15:51 +0200

    add admission controller status

---
+++ pkg/clusteragent/admission/status_no_apiserver.go
@@ -0,0 +1,15 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+// +build !kubeapiserver
+
+package admission
+
+// GetStatus returns status info for the secret and webhook controllers.
+func GetStatus(apiCl interface{}) map[string]interface{} {
+	status := make(map[string]interface{})
+	status["Disabled"] = "The admission controller is not compiled-in"
+	return status
+}
