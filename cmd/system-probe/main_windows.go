49a6be99f3a1bbf3729e7d09da2719a2279efc20
Spencer Brown <spencer.brown@datadoghq.com>
2020-06-16 13:11:17 -0400

    Make channel declaratino inline

--- cmd/system-probe/main_windows.go
+++ cmd/system-probe/main_windows.go
@@ -12,9 +12,8 @@ func main() {
 	flag.StringVar(&opts.pidFilePath, "pid", "", "Path to set pidfile for process")
 	flag.BoolVar(&opts.version, "version", false, "Print the version and exit")
 	flag.Parse()
-	exit := make(chan struct{})
 
-	runAgent(exit)
+	runAgent(make(chan struct{}))
 }
 
 func runCheck(cfg *config.AgentConfig) {
