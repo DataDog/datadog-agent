f69a7d35330c563e9cad4c5b8865a357a87cd0dc
Piotr WOLSKI <piotr.wolski42@gmail.com>
2020-07-20 18:42:45 -0400

    pkg/trace/stats: improve sublayer metrics computation (#5962)
    * [apm][agent] Improve sublayer metrics computation
    
    * [apm] Integrate sublayers computation in the trace agent
    
    * [apm][trace agent] Update comment
    
    * [apm] Update default max spans for sublayers calculator
    
    * [apm][sublayer metrics] clearer code
    
    * [apm][sublayers] Rename opened to open
    
    * [apm][sublayer metrics] Update comment
    
    * [apm][sublayers] Format code
    
    * [apm][sublayer metrics] Rename imports
    
    * [apm][sublayer metrics] Update comment
    
    Co-authored-by: Gabriel Aszalos <gabriel.aszalos@gmail.com>
    
    * [apm][sublayer metrics] Rename maxSpans to capacity
    
    * [apm][sublayers] Clearer code
    
    * [apm][sublayers] Call computeExecDurations method
    
    * [apm][sublayers] Update comments
    
    * [apm][sublayer metrics] Update comments
    
    * [apm][sublayers] Rename benchamrking function
    
    * [apm][sublayers] Init active spans correctly
    
    * [apm][sublayers] Remove unused parentIdx array
    
    * [apm][sublayers] Typo in comment
    
    * [apm][sublayers] Optimize execution time computation
    
    * [apm][sublayers] Put activate and deactivate functions on spanState
    
    * [apm][sublayers] Round instead of casting
    
    * [apm][sublayers] Merge clear and resize
    
    * [apm][sublayers] Update comments
    
    * [apm][sublayers] Rename traceExecDuration to execTime
    
    * [apm][sublayers] Clarify comments
    
    Co-authored-by: Gabriel Aszalos <gabriel.aszalos@gmail.com>

--- pkg/trace/agent/agent.go
+++ pkg/trace/agent/agent.go
@@ -26,9 +26,11 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
-// tagContainersTags specifies the name of the tag which holds key/value
-// pairs representing information about the container (Docker, EC2, etc).
-const tagContainersTags = "_dd.tags.container"
+const (
+	// tagContainersTags specifies the name of the tag which holds key/value
+	// pairs representing information about the container (Docker, EC2, etc).
+	tagContainersTags = "_dd.tags.container"
+)
 
 // Agent struct holds all the sub-routines structs and make the data flow between them
 type Agent struct {
@@ -110,13 +112,14 @@ func (a *Agent) Run() {
 }
 
 func (a *Agent) work() {
+	sublayerCalculator := stats.NewSublayerCalculator()
 	for {
 		select {
 		case t, ok := <-a.In:
 			if !ok {
 				return
 			}
-			a.Process(t)
+			a.Process(t, sublayerCalculator)
 		}
 	}
 
@@ -145,7 +148,7 @@ func (a *Agent) loop() {
 
 // Process is the default work unit that receives a trace, transforms it and
 // passes it downstream.
-func (a *Agent) Process(t *api.Trace) {
+func (a *Agent) Process(t *api.Trace, sublayerCalculator *stats.SublayerCalculator) {
 	if len(t.Spans) == 0 {
 		log.Debugf("Skipping received empty trace")
 		return
@@ -203,7 +206,7 @@ func (a *Agent) Process(t *api.Trace) {
 
 	subtraces := stats.ExtractSubtraces(t.Spans, root)
 	for _, subtrace := range subtraces {
-		subtraceSublayers := stats.ComputeSublayers(subtrace.Trace)
+		subtraceSublayers := sublayerCalculator.ComputeSublayers(subtrace.Trace)
 		pt.Sublayers[subtrace.Root] = subtraceSublayers
 		if sampled {
 			stats.SetSublayersOnSpan(subtrace.Root, subtraceSublayers)
