de934623e921ef264a15feb9d1602d4bc6023566
Albert Vaca <albert.vaca@datadoghq.com>
2020-07-20 13:17:51 +0200

    [kitchen] Add custom recipe to resize the root partition on RHEL
    Should fix the root partition not being large enough to install and then
    upgrade the Agent.

---
+++ test/kitchen/site-cookbooks/dd-agent-rhel-workaround/metadata.rb
@@ -0,0 +1,5 @@
+name             "dd-agent-rhel-workaround"
+maintainer       "Datadog"
+description      "Works around the root partition not being large enough to install and upgrade the Agent"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
