694d7426551b37588754d7c72dd8dbc8d1e1439e
Sylvain Baubeau <lebauce@gmail.com>
2020-07-24 19:12:32 +0200

    Add code headers for all runtime security agent files

--- pkg/security/probe/model.go
+++ pkg/security/probe/model.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
 // +build linux_bpf
 
 //go:generate go run github.com/DataDog/datadog-agent/pkg/security/secl/generators/accessors -tags linux_bpf -output model_accessors.go
