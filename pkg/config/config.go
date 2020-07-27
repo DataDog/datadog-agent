2b1181057bec1a4da3410ffacb6315c27b422926
Sylvain Baubeau <lebauce@gmail.com>
2020-07-27 10:26:56 +0200

    Move configuration path to /etc/datadog/runtime-agent.d

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -737,8 +737,8 @@ func InitConfig(config Config) {
 	// Datadog security agent (runtime)
 	config.BindEnvAndSetDefault("runtime_security_config.enabled", false)
 	config.BindEnvAndSetDefault("runtime_security_config.debug", false)
-	config.BindEnvAndSetDefault("runtime_security_config.policies.dir", "")
-	config.BindEnvAndSetDefault("runtime_security_config.socket", "")
+	config.BindEnvAndSetDefault("runtime_security_config.policies.dir", "/etc/datadog-agent/runtime-security.d")
+	config.BindEnvAndSetDefault("runtime_security_config.socket", "/opt/datadog-agent/run/runtime-security.sock")
 	config.BindEnvAndSetDefault("runtime_security_config.enable_kernel_filters", true)
 	config.BindEnvAndSetDefault("runtime_security_config.syscall_monitor.enabled", false)
 
