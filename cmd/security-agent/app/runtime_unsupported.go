f9f4adcbdec854cfa9c5981a40d2f4871ef04e7b
Sylvain Baubeau <lebauce@gmail.com>
2020-07-28 11:51:34 +0200

    Merge DDClient logic into the runtime security agent

--- cmd/security-agent/app/runtime_unsupported.go
+++ cmd/security-agent/app/runtime_unsupported.go
@@ -11,11 +11,13 @@ import (
 	"errors"
 
 	coreconfig "github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/logs/client"
+	"github.com/DataDog/datadog-agent/pkg/logs/config"
 	"github.com/DataDog/datadog-agent/pkg/logs/restart"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
-func startRuntimeSecurity(stopper restart.Stopper) error {
+func startRuntimeSecurity(hostname string, endpoints *config.Endpoints, context *client.DestinationsContext, stopper restart.Stopper) error {
 	enabled := coreconfig.Datadog.GetBool("runtime_security_config.enabled")
 	if !enabled {
 		log.Info("Datadog runtime security agent disabled by config")
