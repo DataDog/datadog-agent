fca51a54b9ae80a1bfc5df417541f0773edaeae6
Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>
2020-07-30 12:14:31 +0200

    more ksm v2 metric transformers (#6025)

--- pkg/collector/corechecks/cluster/kubernetes_state_transformers.go
+++ pkg/collector/corechecks/cluster/kubernetes_state_transformers.go
@@ -26,7 +26,6 @@ type metricTransformerFunc = func(aggregator.Sender, string, ksmstore.DDMetric, 
 var (
 	// metricTransformers contains KSM metric names and their corresponding transformer functions
 	// These metrics require more than a name translation to generate Datadog metrics, as opposed to the metrics in metricNamesMapper
-	// TODO: implement the metric transformers of these metrics and unit test them
 	// For reference see METRIC_TRANSFORMERS in KSM check V1
 	metricTransformers = map[string]metricTransformerFunc{
 		"kube_pod_status_phase":                       podPhaseTransformer,
@@ -37,14 +36,108 @@ var (
 		"kube_job_failed":                             jobFailedTransformer,
 		"kube_job_status_failed":                      jobStatusFailedTransformer,
 		"kube_job_status_succeeded":                   jobStatusSucceededTransformer,
-		"kube_node_status_condition":                  func(s aggregator.Sender, name string, metric ksmstore.DDMetric, tags []string) {},
-		"kube_node_spec_unschedulable":                func(s aggregator.Sender, name string, metric ksmstore.DDMetric, tags []string) {},
+		"kube_node_status_condition":                  nodeConditionTransformer,
+		"kube_node_spec_unschedulable":                nodeUnschedulableTransformer,
 		"kube_resourcequota":                          resourcequotaTransformer,
-		"kube_limitrange":                             func(s aggregator.Sender, name string, metric ksmstore.DDMetric, tags []string) {},
+		"kube_limitrange":                             limitrangeTransformer,
 		"kube_persistentvolume_status_phase":          pvPhaseTransformer,
 		"kube_service_spec_type":                      serviceTypeTransformer,
 	}
 )
+
+// nodeConditionTransformer generates service checks based on the metric kube_node_status_condition
+// It also submit the metric kubernetes_state.node.by_condition
+func nodeConditionTransformer(s aggregator.Sender, name string, metric ksmstore.DDMetric, tags []string) {
+	if metric.Val != 1.0 {
+		// Only consider active metrics
+		return
+	}
+
+	condition, found := metric.Labels["condition"]
+	if !found {
+		log.Debugf("Couldn't find 'condition' label, ignoring service check for metric '%s'", name)
+		return
+	}
+
+	s.Gauge(ksmMetricPrefix+"node.by_condition", metric.Val, "", tags)
+	statusLabel, found := metric.Labels["status"]
+	if !found {
+		log.Debugf("Couldn't find 'status' label, ignoring service check for metric '%s'", name)
+		return
+	}
+
+	serviceCheckName := ""
+	var eventStatus metrics.ServiceCheckStatus
+	switch condition {
+	case "Ready":
+		serviceCheckName = ksmMetricPrefix + "node.ready"
+		eventStatus = statusForCondition(statusLabel, true)
+	case "OutOfDisk":
+		serviceCheckName = ksmMetricPrefix + "node.out_of_disk"
+		eventStatus = statusForCondition(statusLabel, false)
+	case "DiskPressure":
+		serviceCheckName = ksmMetricPrefix + "node.disk_pressure"
+		eventStatus = statusForCondition(statusLabel, false)
+	case "NetworkUnavailable":
+		serviceCheckName = ksmMetricPrefix + "node.network_unavailable"
+		eventStatus = statusForCondition(statusLabel, false)
+	case "MemoryPressure":
+		serviceCheckName = ksmMetricPrefix + "node.memory_pressure"
+		eventStatus = statusForCondition(statusLabel, false)
+	default:
+		log.Debugf("Invalid 'condition' label '%s', ignoring service check for metric '%s'", condition, name)
+		return
+	}
+
+	node, found := metric.Labels["node"]
+	if !found {
+		log.Debugf("Couldn't find 'node' label, ignoring service check for metric '%s'", name)
+		return
+	}
+
+	message := fmt.Sprintf("%s is currently reporting %s = %s", node, condition, statusLabel)
+	s.ServiceCheck(serviceCheckName, eventStatus, "", tags, message)
+}
+
+// statusForCondition returns the right service check status based on the KSM label 'status'
+// and the nature of the event whether a positive event or not
+// e.g Ready is a positive event while MemoryPressure is not
+func statusForCondition(status string, positiveEvent bool) metrics.ServiceCheckStatus {
+	switch status {
+	case "true":
+		if positiveEvent {
+			return metrics.ServiceCheckOK
+		}
+		return metrics.ServiceCheckCritical
+	case "false":
+		if positiveEvent {
+			return metrics.ServiceCheckCritical
+		}
+		return metrics.ServiceCheckOK
+	case "unknown":
+		return metrics.ServiceCheckUnknown
+	default:
+		log.Debugf("Unknown 'status' label: '%s'", status)
+		return metrics.ServiceCheckUnknown
+	}
+}
+
+// nodeUnschedulableTransformer reports whether a node can schedule new pods
+// It adds a tag 'status' that can be either 'schedulable' or 'unschedulable' and always report the metric value '1'
+func nodeUnschedulableTransformer(s aggregator.Sender, name string, metric ksmstore.DDMetric, tags []string) {
+	status := ""
+	switch metric.Val {
+	case 0.0:
+		status = "schedulable"
+	case 1.0:
+		status = "unschedulable"
+	default:
+		log.Debugf("Invalid metric value '%v', ignoring metric '%s'", metric.Val, name)
+		return
+	}
+	tags = append(tags, "status:"+status)
+	s.Gauge(ksmMetricPrefix+"node.status", 1, "", tags)
+}
 
 // podPhaseTransformer sends status phase metrics for pods, the tag phase has the pod status
 func podPhaseTransformer(s aggregator.Sender, name string, metric ksmstore.DDMetric, tags []string) {
@@ -183,6 +276,36 @@ func resourcequotaTransformer(s aggregator.Sender, name string, metric ksmstore.
 		quotaType = "limit"
 	}
 	metricName := ksmMetricPrefix + fmt.Sprintf("resourcequota.%s.%s", resource, quotaType)
+	s.Gauge(metricName, metric.Val, "", tags)
+}
+
+// constraintsMapper is used by the kube_limitrange metric transformer
+var constraintsMapper = map[string]string{
+	"min":                  "min",
+	"max":                  "max",
+	"default":              "default",
+	"defaultRequest":       "default_request",
+	"maxLimitRequestRatio": "max_limit_request_ratio",
+}
+
+// limitrangeTransformer generates dedicated metrics per resource per type from the kube_limitrange metric
+func limitrangeTransformer(s aggregator.Sender, name string, metric ksmstore.DDMetric, tags []string) {
+	constraintLabel, found := metric.Labels["constraint"]
+	if !found {
+		log.Debugf("Couldn't find 'constraint' label, ignoring metric '%s'", name)
+		return
+	}
+	constraint, found := constraintsMapper[constraintLabel]
+	if !found {
+		log.Debugf("Constraint '%s' unsupported for metric '%s'", constraint, name)
+		return
+	}
+	resource, found := metric.Labels["resource"]
+	if !found {
+		log.Debugf("Couldn't find 'resource' label, ignoring metric '%s'", name)
+		return
+	}
+	metricName := ksmMetricPrefix + fmt.Sprintf("limitrange.%s.%s", resource, constraint)
 	s.Gauge(metricName, metric.Val, "", tags)
 }
 
--- pkg/collector/corechecks/cluster/kubernetes_state_transformers_test.go
+++ pkg/collector/corechecks/cluster/kubernetes_state_transformers_test.go
@@ -947,3 +947,536 @@ func Test_containerTerminatedReasonTransformer(t *testing.T) {
 		})
 	}
 }
+
+func Test_limitrangeTransformer(t *testing.T) {
+	type args struct {
+		name   string
+		metric ksmstore.DDMetric
+		tags   []string
+	}
+	type metricsExpected struct {
+		val  float64
+		name string
+		tags []string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected *metricsExpected
+	}{
+		{
+			name: "nominal case",
+			args: args{
+				name: "kube_limitrange",
+				metric: ksmstore.DDMetric{
+					Val: 0.1,
+					Labels: map[string]string{
+						"constraint": "defaultRequest",
+						"limitrange": "limits",
+						"resource":   "cpu",
+					},
+				},
+				tags: []string{"constraint:default_request", "limitrange:limits", "resource:cpu"},
+			},
+			expected: &metricsExpected{
+				name: "kubernetes_state.limitrange.cpu.default_request",
+				val:  0.1,
+				tags: []string{"constraint:default_request", "limitrange:limits", "resource:cpu"},
+			},
+		},
+		{
+			name: "no constraint label",
+			args: args{
+				name: "kube_limitrange",
+				metric: ksmstore.DDMetric{
+					Val: 0.1,
+					Labels: map[string]string{
+						"limitrange": "limits",
+						"resource":   "cpu",
+					},
+				},
+				tags: []string{"limitrange:limits", "resource:cpu"},
+			},
+			expected: nil,
+		},
+		{
+			name: "invalid constraint label",
+			args: args{
+				name: "kube_limitrange",
+				metric: ksmstore.DDMetric{
+					Val: 0.1,
+					Labels: map[string]string{
+						"constraint": "foo",
+						"limitrange": "limits",
+						"resource":   "cpu",
+					},
+				},
+				tags: []string{"constraint:foo", "limitrange:limits", "resource:cpu"},
+			},
+			expected: nil,
+		},
+		{
+			name: "no resource label",
+			args: args{
+				name: "kube_limitrange",
+				metric: ksmstore.DDMetric{
+					Val: 0.1,
+					Labels: map[string]string{
+						"constraint": "defaultRequest",
+						"limitrange": "limits",
+					},
+				},
+				tags: []string{"constraint:default_request", "limitrange:limits"},
+			},
+			expected: nil,
+		},
+	}
+	for _, tt := range tests {
+		s := mocksender.NewMockSender("ksm")
+		s.SetupAcceptAll()
+		t.Run(tt.name, func(t *testing.T) {
+			limitrangeTransformer(s, tt.args.name, tt.args.metric, tt.args.tags)
+			if tt.expected != nil {
+				s.AssertMetric(t, "Gauge", tt.expected.name, tt.expected.val, "", tt.expected.tags)
+				s.AssertNumberOfCalls(t, "Gauge", 1)
+			} else {
+				s.AssertNotCalled(t, "Gauge")
+			}
+		})
+	}
+}
+
+func Test_nodeUnschedulableTransformer(t *testing.T) {
+	type args struct {
+		name   string
+		metric ksmstore.DDMetric
+		tags   []string
+	}
+	type metricsExpected struct {
+		val  float64
+		name string
+		tags []string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected *metricsExpected
+	}{
+		{
+			name: "schedulable",
+			args: args{
+				name: "kube_node_spec_unschedulable",
+				metric: ksmstore.DDMetric{
+					Val: 0.0,
+					Labels: map[string]string{
+						"node": "foo",
+					},
+				},
+				tags: []string{"node:foo"},
+			},
+			expected: &metricsExpected{
+				name: "kubernetes_state.node.status",
+				val:  1.0,
+				tags: []string{"node:foo", "status:schedulable"},
+			},
+		},
+		{
+			name: "unschedulable",
+			args: args{
+				name: "kube_node_spec_unschedulable",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node": "foo",
+					},
+				},
+				tags: []string{"node:foo"},
+			},
+			expected: &metricsExpected{
+				name: "kubernetes_state.node.status",
+				val:  1.0,
+				tags: []string{"node:foo", "status:unschedulable"},
+			},
+		},
+		{
+			name: "invalid",
+			args: args{
+				name: "kube_node_spec_unschedulable",
+				metric: ksmstore.DDMetric{
+					Val: 2.0,
+					Labels: map[string]string{
+						"node": "foo",
+					},
+				},
+				tags: []string{"node:foo"},
+			},
+			expected: nil,
+		},
+	}
+	for _, tt := range tests {
+		s := mocksender.NewMockSender("ksm")
+		s.SetupAcceptAll()
+		t.Run(tt.name, func(t *testing.T) {
+			nodeUnschedulableTransformer(s, tt.args.name, tt.args.metric, tt.args.tags)
+			if tt.expected != nil {
+				s.AssertMetric(t, "Gauge", tt.expected.name, tt.expected.val, "", tt.expected.tags)
+				s.AssertNumberOfCalls(t, "Gauge", 1)
+			} else {
+				s.AssertNotCalled(t, "Gauge")
+			}
+		})
+	}
+}
+
+func Test_nodeConditionTransformer(t *testing.T) {
+	type args struct {
+		name   string
+		metric ksmstore.DDMetric
+		tags   []string
+	}
+	type serviceCheck struct {
+		name    string
+		status  metrics.ServiceCheckStatus
+		tags    []string
+		message string
+	}
+	type metric struct {
+		val  float64
+		name string
+		tags []string
+	}
+	tests := []struct {
+		name                 string
+		args                 args
+		expectedServiceCheck *serviceCheck
+		expectedMetric       *metric
+	}{
+		{
+			name: "Ready",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "Ready",
+						"status":    "true",
+					},
+				},
+				tags: []string{"node:foo", "condition:Ready", "status:true"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.ready",
+				tags:    []string{"node:foo", "condition:Ready", "status:true"},
+				status:  metrics.ServiceCheckOK,
+				message: "foo is currently reporting Ready = true",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:Ready", "status:true"},
+			},
+		},
+		{
+			name: "Not Ready",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "Ready",
+						"status":    "false",
+					},
+				},
+				tags: []string{"node:foo", "condition:Ready", "status:false"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.ready",
+				tags:    []string{"node:foo", "condition:Ready", "status:false"},
+				status:  metrics.ServiceCheckCritical,
+				message: "foo is currently reporting Ready = false",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:Ready", "status:false"},
+			},
+		},
+		{
+			name: "Unknown Readiness",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "Ready",
+						"status":    "unknown",
+					},
+				},
+				tags: []string{"node:foo", "condition:Ready", "status:unknown"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.ready",
+				tags:    []string{"node:foo", "condition:Ready", "status:unknown"},
+				status:  metrics.ServiceCheckUnknown,
+				message: "foo is currently reporting Ready = unknown",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:Ready", "status:unknown"},
+			},
+		},
+		{
+			name: "Zero metric value",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 0.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "Ready",
+						"status":    "true",
+					},
+				},
+				tags: []string{"node:foo", "condition:Ready", "status:true"},
+			},
+			expectedServiceCheck: nil,
+			expectedMetric:       nil,
+		},
+		{
+			name: "Invalid condition label",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "foo",
+						"status":    "unknown",
+					},
+				},
+				tags: []string{"node:foo", "condition:foo", "status:unknown"},
+			},
+			expectedServiceCheck: nil,
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:foo", "status:unknown"},
+			},
+		},
+		{
+			name: "Missing condition label",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":   "foo",
+						"status": "unknown",
+					},
+				},
+				tags: []string{"node:foo", "status:unknown"},
+			},
+			expectedServiceCheck: nil,
+			expectedMetric:       nil,
+		},
+		{
+			name: "Invalid status label",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "Ready",
+						"status":    "foo",
+					},
+				},
+				tags: []string{"node:foo", "condition:Ready", "status:foo"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.ready",
+				tags:    []string{"node:foo", "condition:Ready", "status:foo"},
+				status:  metrics.ServiceCheckUnknown,
+				message: "foo is currently reporting Ready = foo",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:Ready", "status:foo"},
+			},
+		},
+		{
+			name: "Missing status label",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "Ready",
+					},
+				},
+				tags: []string{"node:foo", "condition:Ready"},
+			},
+			expectedServiceCheck: nil,
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:Ready"},
+			},
+		},
+		{
+			name: "Not OutOfDisk",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "OutOfDisk",
+						"status":    "false",
+					},
+				},
+				tags: []string{"node:foo", "condition:OutOfDisk", "status:false"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.out_of_disk",
+				tags:    []string{"node:foo", "condition:OutOfDisk", "status:false"},
+				status:  metrics.ServiceCheckOK,
+				message: "foo is currently reporting OutOfDisk = false",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:OutOfDisk", "status:false"},
+			},
+		},
+		{
+			name: "OutOfDisk",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "OutOfDisk",
+						"status":    "true",
+					},
+				},
+				tags: []string{"node:foo", "condition:OutOfDisk", "status:true"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.out_of_disk",
+				tags:    []string{"node:foo", "condition:OutOfDisk", "status:true"},
+				status:  metrics.ServiceCheckCritical,
+				message: "foo is currently reporting OutOfDisk = true",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:OutOfDisk", "status:true"},
+			},
+		},
+		{
+			name: "DiskPressure",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "DiskPressure",
+						"status":    "true",
+					},
+				},
+				tags: []string{"node:foo", "condition:DiskPressure", "status:true"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.disk_pressure",
+				tags:    []string{"node:foo", "condition:DiskPressure", "status:true"},
+				status:  metrics.ServiceCheckCritical,
+				message: "foo is currently reporting DiskPressure = true",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:DiskPressure", "status:true"},
+			},
+		},
+		{
+			name: "NetworkUnavailable",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "NetworkUnavailable",
+						"status":    "true",
+					},
+				},
+				tags: []string{"node:foo", "condition:NetworkUnavailable", "status:true"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.network_unavailable",
+				tags:    []string{"node:foo", "condition:NetworkUnavailable", "status:true"},
+				status:  metrics.ServiceCheckCritical,
+				message: "foo is currently reporting NetworkUnavailable = true",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:NetworkUnavailable", "status:true"},
+			},
+		},
+		{
+			name: "MemoryPressure",
+			args: args{
+				name: "kube_node_status_condition",
+				metric: ksmstore.DDMetric{
+					Val: 1.0,
+					Labels: map[string]string{
+						"node":      "foo",
+						"condition": "MemoryPressure",
+						"status":    "true",
+					},
+				},
+				tags: []string{"node:foo", "condition:MemoryPressure", "status:true"},
+			},
+			expectedServiceCheck: &serviceCheck{
+				name:    "kubernetes_state.node.memory_pressure",
+				tags:    []string{"node:foo", "condition:MemoryPressure", "status:true"},
+				status:  metrics.ServiceCheckCritical,
+				message: "foo is currently reporting MemoryPressure = true",
+			},
+			expectedMetric: &metric{
+				name: "kubernetes_state.node.by_condition",
+				val:  1.0,
+				tags: []string{"node:foo", "condition:MemoryPressure", "status:true"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		s := mocksender.NewMockSender("ksm")
+		s.SetupAcceptAll()
+		t.Run(tt.name, func(t *testing.T) {
+			nodeConditionTransformer(s, tt.args.name, tt.args.metric, tt.args.tags)
+			if tt.expectedServiceCheck != nil {
+				s.AssertServiceCheck(t, tt.expectedServiceCheck.name, tt.expectedServiceCheck.status, "", tt.expectedServiceCheck.tags, tt.expectedServiceCheck.message)
+				s.AssertNumberOfCalls(t, "ServiceCheck", 1)
+			} else {
+				s.AssertNotCalled(t, "ServiceCheck")
+			}
+			if tt.expectedMetric != nil {
+				s.AssertMetric(t, "Gauge", tt.expectedMetric.name, tt.expectedMetric.val, "", tt.expectedMetric.tags)
+				s.AssertNumberOfCalls(t, "Gauge", 1)
+			} else {
+				s.AssertNotCalled(t, "Gauge")
+			}
+		})
+	}
+}
