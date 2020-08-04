62ff6cbcfd0630e2eba800ea4fb3d6d4fdd502fb
Bryce Kahle <bryce.kahle@datadoghq.com>
2020-08-04 13:20:59 -0400

    Handle SIGPIPE again
    This code was removed in #5200 but was serving a valuable purpose. If we do not handle the SIGPIPE signal, then the agent will be abruptly terminated if journald crashes.

--- pkg/process/util/signal_nowindows.go
+++ pkg/process/util/signal_nowindows.go
@@ -13,7 +13,7 @@ import (
 // HandleSignals tells us whether we should exit.
 func HandleSignals(exit chan struct{}) {
 	sigIn := make(chan os.Signal, 100)
-	signal.Notify(sigIn, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigIn, syscall.SIGINT, syscall.SIGTERM, syscall.SIGPIPE)
 	// unix only in all likelihood; but we don't care.
 	for sig := range sigIn {
 		switch sig {
@@ -21,6 +21,12 @@ func HandleSignals(exit chan struct{}) {
 			log.Criticalf("Caught signal '%s'; terminating.", sig)
 			close(exit)
 			return
+		case syscall.SIGPIPE:
+			// By default systemd redirects the stdout to journald. When journald is stopped or crashes we receive a SIGPIPE signal.
+			// Go ignores SIGPIPE signals unless it is when stderr or stdout is closed, in this case the agent is stopped.
+			// We never want the agent to stop upon receiving SIGPIPE, so we intercept the SIGPIPE signals and just discard them.
+			// See https://golang.org/pkg/os/signal/#hdr-SIGPIPE
+			continue
 		}
 	}
 }
