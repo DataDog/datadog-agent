4c3f570d56630e963524e0e73309d49bfba4e999
Bryce Kahle <bryce.kahle@datadoghq.com>
2020-08-06 12:18:58 -0400

    Ensure we don't attempt to send to closed channels in PerfHandler

--- pkg/ebpf/bytecode/perf.go
+++ pkg/ebpf/bytecode/perf.go
@@ -12,6 +12,7 @@ type PerfHandler struct {
 	ClosedChannel chan []byte
 	LostChannel   chan uint64
 	once          sync.Once
+	closed        bool
 }
 
 func NewPerfHandler(closedChannelSize int) *PerfHandler {
@@ -22,15 +23,22 @@ func NewPerfHandler(closedChannelSize int) *PerfHandler {
 }
 
 func (c *PerfHandler) dataHandler(CPU int, batchData []byte, perfMap *manager.PerfMap, manager *manager.Manager) {
+	if c.closed {
+		return
+	}
 	c.ClosedChannel <- batchData
 }
 
 func (c *PerfHandler) lostHandler(CPU int, lostCount uint64, perfMap *manager.PerfMap, manager *manager.Manager) {
+	if c.closed {
+		return
+	}
 	c.LostChannel <- lostCount
 }
 
 func (c *PerfHandler) Stop() {
 	c.once.Do(func() {
+		c.closed = true
 		close(c.ClosedChannel)
 		close(c.LostChannel)
 	})
