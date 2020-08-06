754dc6cb8bd84e75a595cf6d38b77be34890b2d4
Mehedi Bakht <mehedi.bakht@datadoghq.com>
2020-08-06 14:40:06 -0500

    Update version logic to match vX.Y.Z version in agent-payload

--- tasks/utils.py
+++ tasks/utils.py
@@ -143,13 +143,14 @@ def get_payload_version():
                 continue
             pkgname = whitespace_split[0]
             if pkgname == "github.com/DataDog/agent-payload":
-                comment_split = line.split("//")
+                # Example of line: github.com/DataDog/agent-payload v4.40.0+incompatible
+                comment_split = re.split(r'[ +]', line)
                 if len(comment_split) < 2:
                     raise Exception(
                         "Versioning of agent-payload in go.mod has changed, the version logic needs to be updated"
                     )
                 version = comment_split[1].strip()
-                if not re.search(r"^\d+(\.\d+){2}$", version):
+                if not re.search(r"^v\d+(\.\d+){2}$", version):
                     raise Exception("Version of agent-payload in go.mod is invalid: '{}'".format(version))
                 return version
 
