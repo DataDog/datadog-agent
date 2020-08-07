45fcafe2a180efff96631dc66f25c0cf4bb73603
Mehedi Bakht <mehedi.bakht@datadoghq.com>
2020-08-07 14:47:26 -0500

    [system-probe] Add stats for DNS successes, errors, and queries (#6130)
    * Add stats for DNS successes, errors, and queries
    
    * undo unrelated change
    
    * undo more unrelated changes
    
    * Store DNS telemetry till last tick, show last tick time
    
    * Fix unit test
    
    * Not rely on ticks
    
    * add timestamp in DNS stats
    
    * Update test

--- pkg/network/dns_snooper.go
+++ pkg/network/dns_snooper.go
@@ -44,6 +44,11 @@ type SocketFilterSnooper struct {
 	polls          int64
 	decodingErrors int64
 	truncatedPkts  int64
+
+	// DNS telemetry, values calculated *till* the last tick in pollStats
+	queries   int64
+	successes int64
+	errors    int64
 }
 
 // NewSocketFilterSnooper returns a new SocketFilterSnooper
@@ -123,7 +128,10 @@ func (s *SocketFilterSnooper) GetStats() map[string]int64 {
 	stats["packets_dropped"] = atomic.LoadInt64(&s.dropped)
 	stats["decoding_errors"] = atomic.LoadInt64(&s.decodingErrors)
 	stats["truncated_packets"] = atomic.LoadInt64(&s.truncatedPkts)
-
+	stats["queries"] = atomic.LoadInt64(&s.queries)
+	stats["successes"] = atomic.LoadInt64(&s.successes)
+	stats["errors"] = atomic.LoadInt64(&s.errors)
+	stats["timestamp_micro_secs"] = time.Now().UnixNano() / 1000
 	return stats
 }
 
@@ -165,6 +173,11 @@ func (s *SocketFilterSnooper) processPacket(data []byte) {
 
 	if pktInfo.pktType == SuccessfulResponse {
 		s.cache.Add(t, time.Now())
+		atomic.AddInt64(&s.successes, 1)
+	} else if pktInfo.pktType == FailedResponse {
+		atomic.AddInt64(&s.errors, 1)
+	} else {
+		atomic.AddInt64(&s.queries, 1)
 	}
 }
 
