b8789ee25ff1a54aea6b897bbf58dbceb82038c7
Lénaïc Huard <lenaic.huard@datadoghq.com>
2020-08-11 10:12:07 +0200

    fixup! [e2e] Upgrade argo

--- test/e2e/scripts/run-instance/21-argo-setup.sh
+++ test/e2e/scripts/run-instance/21-argo-setup.sh
@@ -14,7 +14,24 @@ done
 set -e
 
 kubectl create ns argo
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/master/manifests/install.yaml
+#kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/master/manifests/install.yaml
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/install.yaml
+
+# TODO use a more restrictive SA
+kubectl apply -f - << EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: argo-admin
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: default
+  namespace: default
+EOF
 
 set +e
 
