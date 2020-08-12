4cb38092fb30db08e9ac9ac6f647be511c4d18aa
Lénaïc Huard <lenaic.huard@datadoghq.com>
2020-08-12 18:21:48 +0200

    Fix tests

--- rtloader/test/uutil/uutil_test.go
+++ rtloader/test/uutil/uutil_test.go
@@ -47,7 +47,8 @@ func TestSubprocessOutputWrongArg(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if out != "TypeError: get_subprocess_output() missing required argument 'command' (pos 1)" {
+	if out != "TypeError: get_subprocess_output() missing required argument 'command' (pos 1)" && // Python 3
+		out != "TypeError: Required argument 'command' (pos 1) not found" { // Python 2
 		t.Errorf("Unexpected printed value: '%s'", out)
 	}
 
