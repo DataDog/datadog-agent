ced51a617e1e6a6d9d64cb2e6848f4ebd04718c6
Spencer Brown <spencer.brown@datadoghq.com>
2020-08-13 14:20:27 -0400

    Add dummy values

--- pkg/ebpf/utils_windows.go
+++ pkg/ebpf/utils_windows.go
@@ -6,7 +6,7 @@ package ebpf
 // IsTracerSupportedByOS returns whether or not the current kernel version supports tracer functionality
 // along with some context on why it's not supported
 func IsTracerSupportedByOS(exclusionList []string) (bool, string) {
-	return verifyOSVersion(currentKernelCode, platform, exclusionList)
+	return verifyOSVersion(0, "", nil)
 }
 
 // TODO Determine which windows versions we will support, and potentially remove kernelCode from parameters list
