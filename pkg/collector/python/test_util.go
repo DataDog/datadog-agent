3da9173c7eeb63872e346b562f332425f6bb19c2
Lénaïc Huard <lenaic.huard@datadoghq.com>
2020-08-14 14:49:58 +0200

    Fix Windows

--- pkg/collector/python/test_util.go
+++ pkg/collector/python/test_util.go
@@ -76,18 +76,3 @@ func testGetSubprocessOutputError(t *testing.T) {
 	assert.NotEqual(t, C.int(0), cRetCode)
 	assert.Nil(t, exception)
 }
-
-func testGetSubprocessOutputEnv(t *testing.T) {
-	var argv []*C.char = []*C.char{C.CString("bash"), C.CString("-c"), C.CString("echo $BAZ"), nil}
-	var env []*C.char = []*C.char{C.CString("FOO=BAR"), C.CString("BAZ=QUX"), nil}
-	var cStdout *C.char
-	var cStderr *C.char
-	var cRetCode C.int
-	var exception *C.char
-
-	GetSubprocessOutput(&argv[0], &env[0], &cStdout, &cStderr, &cRetCode, &exception)
-	assert.Equal(t, "QUX\n", C.GoString(cStdout))
-	assert.Equal(t, "", C.GoString(cStderr))
-	assert.Equal(t, C.int(0), cRetCode)
-	assert.Nil(t, exception)
-}
