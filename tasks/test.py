c519a861df1affb951721f23436e2b80767d07b5
Ofek Lev <ofekmeister@gmail.com>
2020-09-01 12:15:27 -0400

    fix test logger when printing non-ascii

--- tasks/test.py
+++ tasks/test.py
@@ -41,6 +41,13 @@ DEFAULT_TEST_TARGETS = [
 ]
 
 
+def ensure_bytes(s):
+    if not isinstance(s, bytes):
+        return s.encode('utf-8')
+
+    return s
+
+
 @task()
 def test(
     ctx,
@@ -405,7 +412,8 @@ class TestProfiler:
 
     def write(self, txt):
         # Output to stdout
-        sys.stdout.write(txt)
+        # NOTE: write to underlying stream on Python 3 to avoid unicode issues when default encoding is not UTF-8
+        getattr(sys.stdout, 'buffer', sys.stdout).write(ensure_bytes(txt))
         # Extract the run time
         for result in self.parser.finditer(txt):
             self.times.append((result.group(1), float(result.group(2))))
