7154173334c5f7b2803f3c2e4a41136b067151b5
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-09-09 21:18:05 +0200

    Fix default security-agent logs file (#6335) (#6349)
    Backport of #6335.
    Fix default security-agent logs file.
    
    Co-authored-by: Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -749,6 +749,7 @@ func InitConfig(config Config) {
 	// Datadog security agent (common)
 	config.BindEnvAndSetDefault("security_agent.cmd_port", 5010)
 	config.BindEnvAndSetDefault("security_agent.expvar_port", 5011)
+	config.BindEnvAndSetDefault("security_agent.log_file", defaultSecurityAgentLogFile)
 
 	// Datadog security agent (compliance)
 	config.BindEnvAndSetDefault("compliance_config.enabled", false)
