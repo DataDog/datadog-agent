902a01b9596d6d9e619d541701bdb181dfb2bf98
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-09-17 10:10:06 +0200

    [omnibus, MacOS] Blacklist snowflake on Python 2

--- omnibus/config/software/datadog-agent-integrations-py2.rb
+++ omnibus/config/software/datadog-agent-integrations-py2.rb
@@ -78,8 +78,17 @@ if osx?
   # binaries were built with a MacOS SDK lower than 10.9.
   # This can be removed once a new lxml version with binaries built with a newer SDK is available.
   blacklist_packages.push(/^lxml==/)
+
   # Blacklist ibm_was, which depends on lxml
   blacklist_folders.push('ibm_was')
+
+  # Blacklist ijson as it fails MacOS notarization: the _yajl2.so binary inside was built with a
+  # MacOS SDK lower than 10.9. The Python 3 counterpart of the same package is not affected (it
+  # doesn't ship this file).
+  blacklist_packages.push(/^ijson==/)
+
+  # Blacklist snowflake, which depends on lxml
+  blacklist_folders.push('snowflake')
 
   # Blacklist aerospike, new version 3.10 is not supported on MacOS yet
   blacklist_folders.push('aerospike')
