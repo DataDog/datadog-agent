58ad7e50aa3bb985c7ff62002dac5f07c499780a
Berzan Yildiz <17531789+bmikaili@users.noreply.github.com>
2020-09-18 11:50:51 +0200

    Add 7th alibaba server

--- pkg/util/alibaba/alibaba.go
+++ pkg/util/alibaba/alibaba.go
@@ -50,7 +50,7 @@ func GetNTPHosts() []string {
 	if IsRunningOn() {
 		return []string{
 			"ntp.aliyun.com", "ntp1.aliyun.com", "ntp2.aliyun.com", "ntp3.aliyun.com",
-			"ntp4.aliyun.com", "ntp5.aliyun.com", "ntp6.aliyun.com",
+			"ntp4.aliyun.com", "ntp5.aliyun.com", "ntp6.aliyun.com", "ntp7.aliyun.com",
 		}
 	}
 
