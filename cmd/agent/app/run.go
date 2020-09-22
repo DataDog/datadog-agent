c849d51a6fbf7e110e099de6b9ed8e9b4a30954f
Julien Lebot <julien.lebot@datadoghq.com>
2020-09-22 15:09:23 +0200

    Register jetson check

--- cmd/agent/app/run.go
+++ cmd/agent/app/run.go
@@ -52,6 +52,7 @@ import (
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/embed"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/net"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/system"
+	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/system/gpu/nvidia"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/systemd"
 
 	// register metadata providers
