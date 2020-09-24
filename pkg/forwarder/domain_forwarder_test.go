20a31541302204e1b09e2d8478d839e9ddea84df
Olivier G <olivier.gaca@datadoghq.com>
2020-09-24 09:36:47 +0200

    Fix race condition.

--- pkg/forwarder/domain_forwarder_test.go
+++ pkg/forwarder/domain_forwarder_test.go
@@ -243,12 +243,13 @@ func TestDomainForwarderRetryQueueAllPayloadsMaxSize(t *testing.T) {
 	forwarder.blockedList.close("blocked")
 	forwarder.blockedList.errorPerEndpoint["blocked"].until = time.Now().Add(1 * time.Minute)
 	defer forwarder.Stop(true)
-	forwarder.Start()
 
 	// Disable flush
 	flushIntervalOld := flushInterval
 	defer func() { flushInterval = flushIntervalOld }()
 	flushInterval = 10 * time.Minute
+
+	forwarder.Start()
 
 	for _, payloadSize := range []int{4, 3, 2, 1} {
 		tr := newTestTransaction()
