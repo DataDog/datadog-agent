da7b6fc3550ea377c44d713bdb8e091c7d3c0c83
Mike Zhu <66324146+mikezhu-dd@users.noreply.github.com>
2020-09-30 20:05:56 -0400

    Update kitchen tests to test SLES 15 SP2 (#6481)
    * Update kitchen tests to test SLES 15 SP2.
    
    * Fix dogstatsd testing install script
    
    * Update kitchen tests to test SLES 15 SP2.
    
    Co-authored-by: Kylian Serrania <kylian.serrania@datadoghq.com>

--- test/kitchen/site-cookbooks/dd-agent-step-by-step/recipes/default.rb
+++ test/kitchen/site-cookbooks/dd-agent-step-by-step/recipes/default.rb
@@ -73,7 +73,7 @@ when 'suse'
       sudo rpm --import /tmp/DATADOG_RPM_KEY_E09422B3.public
       sudo rpm --import https://yum.datadoghq.com/DATADOG_RPM_KEY.public
       sudo rpm --import https://yum.datadoghq.com/DATADOG_RPM_KEY_E09422B3.public
-      sudo zypper --non-interactive --no-gpg-check refresh datadog
+      sudo zypper --non-interactive --no-gpg-checks refresh datadog
       sudo zypper --non-interactive install #{node['dd-agent-step-by-step']['package_name']}
     EOF
   end
