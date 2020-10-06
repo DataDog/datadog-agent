cc17effc1967544a36c9700ddc0a775d48b32060
Nam Nguyen <nam.nguyen@datadoghq.com>
2020-10-06 16:16:40 +0200

    [orchestrator] add orchestratorinformation for DCA output (#6447)
    * [orchestrator] add orchestrator code for DCA output
    
    This PR adds a orchestrator dedicated section for the DCA status output. Additionally, on each run the agent caches the last information it has collected about each resource in a cache, which is then shown in the status output.

--- pkg/clusteragent/orchestrator/transformer.go
+++ pkg/clusteragent/orchestrator/transformer.go
@@ -13,6 +13,7 @@ import (
 
 	model "github.com/DataDog/agent-payload/process"
 	"github.com/DataDog/datadog-agent/pkg/orchestrator"
+
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
