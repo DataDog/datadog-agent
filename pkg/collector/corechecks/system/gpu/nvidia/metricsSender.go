a40e6e2bf1e666445aab2997d771837cfe311ce9
Julien Lebot <julien.lebot@datadoghq.com>
2020-10-08 12:56:46 +0200

    Move everything under jetson folder, add license header, add conditional compiling flag on all files

--- pkg/collector/corechecks/system/gpu/nvidia/metricsSender.go
+++
@@ -1,8 +0,0 @@
-package nvidia
-
-import "github.com/DataDog/datadog-agent/pkg/aggregator"
-
-type metricsSender interface {
-	Init() error
-	SendMetrics(sender aggregator.Sender, field string) error
-}
