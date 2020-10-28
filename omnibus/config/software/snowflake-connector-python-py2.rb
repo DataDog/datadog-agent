e7efb08f94f1a43d90813779bdea68247ef5bedd
ChristineTChen <christine.chen@datadoghq.com>
2020-10-28 17:24:05 -0400

    move software from omnibus-software to datadog-agent

---
+++ omnibus/config/software/snowflake-connector-python-py2.rb
@@ -0,0 +1,16 @@
+name "snowflake-connector-python"
+
+default_version "2.1.3"
+
+dependency "pip2"
+dependency "cython"
+
+source url: "https://github.com/snowflakedb/snowflake-connector-python/archive/v#{version}.tar.gz",
+       sha256: "855ffb93a09c3cd994dab8af7c87a46038bbba103928c5948a0edcd2500f4e1a",
+       extract: :seven_zip
+
+build do
+  ship_license "https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v#{version}/LICENSE.txt"
+  patch source: "snowflake-connector-python-cryptography.patch", target: "setup.py", plevel: 0
+  command "#{install_dir}/embedded/bin/pip2 install ."
+end
