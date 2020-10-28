35b88df07f776f250e7eb98f591b66c1e8095381
Pierre Rognant <pierre.rognant@datadoghq.com>
2020-10-28 15:39:23 +0100

    Adjust logger name

--- cmd/agent/app/app.go
+++ cmd/agent/app/app.go
@@ -39,7 +39,7 @@ monitoring and performance data.`,
 
 // loggerName is the name of the core agent logger
 const loggerName config.LoggerName = "CORE"
-const jmxLoggerName config.LoggerName = "JMX"
+const jmxLoggerName config.LoggerName = "JMXFETCH"
 
 func init() {
 	AgentCmd.PersistentFlags().StringVarP(&confFilePath, "cfgpath", "c", "", "path to directory containing datadog.yaml")
--- pkg/config/log.go
+++ pkg/config/log.go
@@ -53,7 +53,7 @@ func createQuoteMsgFormatter(params string) seelog.FormatterFunc {
 
 // buildCommonFormat returns the log common format seelog string
 func buildCommonFormat(loggerName LoggerName) string {
-	if loggerName == "JMX" {
+	if loggerName == "JMXFETCH" {
 		return `%Msg%n`
 	}
 	return fmt.Sprintf("%%Date(%s) | %s | %%LEVEL | (%%ShortFilePath:%%Line in %%FuncShort) | %%ExtraTextContext%%Msg%%n", getLogDateFormat(), loggerName)
@@ -62,7 +62,7 @@ func buildCommonFormat(loggerName LoggerName) string {
 // buildJSONFormat returns the log JSON format seelog string
 func buildJSONFormat(loggerName LoggerName) string {
 	seelog.RegisterCustomFormatter("QuoteMsg", createQuoteMsgFormatter) //nolint:errcheck
-	if loggerName == "JMX" {
+	if loggerName == "JMXFETCH" {
 		return `{"msg":%QuoteMsg}%n`
 	}
 	return fmt.Sprintf(`{"agent":"%s","time":"%%Date(%s)","level":"%%LEVEL","file":"%%ShortFilePath","line":"%%Line","func":"%%FuncShort","msg":%%QuoteMsg%%ExtraJSONContext}%%n`, strings.ToLower(string(loggerName)), getLogDateFormat())
