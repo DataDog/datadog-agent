DataDog/datadog-agent

commit b7467cc7b50bcad34b64268b25d3ba1dfeef4079

pkg/trace/config: move env. var registrations to shared config pkg (#6347)
This change moves the loading of environment variables into the main agent config package.

Whenever a file was loaded with the `pkg/config` package outside the `trace-agent` (e.g. agent CLI, flares, etc), it would lack environment variables. The reason for this was partly lack of functionality in the Viper dependency and also support for Agent 5 (and the way the `pkg/config/legacy` package was working).

Support for Agent 5 is no longer needed in this repository so it is safe to do it right now.

pkg/trace/config/apply.go
@@ -207,17 +207,17 @@ func (c *AgentConfig) applyDatadogConfig() error {
 	if config.Datadog.IsSet("apm_config.max_traces_per_second") {
 		c.MaxTPS = config.Datadog.GetFloat64("apm_config.max_traces_per_second")
 	}
-	if config.Datadog.IsSet("apm_config.ignore_resources") {
-		c.Ignore["resource"] = config.Datadog.GetStringSlice("apm_config.ignore_resources")
+	if k := "apm_config.ignore_resources"; config.Datadog.IsSet(k) {
+		c.Ignore["resource"] = config.Datadog.GetStringSlice(k)
 	}
 	if k := "apm_config.max_payload_size"; config.Datadog.IsSet(k) {
 		c.MaxRequestBytes = config.Datadog.GetInt64(k)
 	}
-
-	if config.Datadog.IsSet("apm_config.replace_tags") {
+	if k := "apm_config.replace_tags"; config.Datadog.IsSet(k) {
 		rt := make([]*ReplaceRule, 0)
-		err := config.Datadog.UnmarshalKey("apm_config.replace_tags", &rt)
-		if err == nil {
+		if err := config.Datadog.UnmarshalKey(k, &rt); err != nil {
+			log.Errorf("Bad format for %q it should be of the form '[{\"name\": \"tag_name\",\"pattern\":\"pattern\",\"repl\":\"replace_str\"}]', error: %v", "apm_config.replace_tags", err)
+		} else {
 			err := compileReplaceRules(rt)
 			if err != nil {
 				osutil.Exitf("replace_tags: %s", err)
@@ -281,22 +281,19 @@ func (c *AgentConfig) applyDatadogConfig() error {
 		}
 	}
 	// undocumeted
-	if config.Datadog.IsSet("apm_config.analyzed_spans") {
-		rateBySpan := make(map[string]float64)
-		if err := config.Datadog.UnmarshalKey("apm_config.analyzed_spans", &rateBySpan); err != nil {
-			return err
-		}
-		for key, rate := range rateBySpan {
+	if k := "apm_config.analyzed_spans"; config.Datadog.IsSet(k) {
+		for key, rate := range config.Datadog.GetStringMap("apm_config.analyzed_spans") {
 			serviceName, operationName, err := parseServiceAndOp(key)
 			if err != nil {
 				log.Errorf("Error parsing names: %v", err)
 				continue
 			}
-
-			if _, ok := c.AnalyzedSpansByService[serviceName]; !ok {
-				c.AnalyzedSpansByService[serviceName] = make(map[string]float64)
+			if floatrate, ok := rate.(float64); ok {
+				if _, ok := c.AnalyzedSpansByService[serviceName]; !ok {
+					c.AnalyzedSpansByService[serviceName] = make(map[string]float64)
+				}
+				c.AnalyzedSpansByService[serviceName][operationName] = floatrate
 			}
-			c.AnalyzedSpansByService[serviceName][operationName] = rate
 		}
 	}
 

cmd/agent/app/status.go
@@ -11,8 +11,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/DataDog/datadog-agent/cmd/agent/common"
@@ -138,13 +136,6 @@ func requestStatus() error {
 // key with an explanation.
 func getAPMStatus() map[string]interface{} {
 	port := 8126
-	// TODO(gbbr): This should be handled by the shared config package once
-	// we migrate APM env. vars there.
-	if p, ok := os.LookupEnv("DD_APM_RECEIVER_PORT"); ok {
-		if v, err := strconv.Atoi(p); err == nil {
-			port = v
-		}
-	}
 	if config.Datadog.IsSet("apm_config.receiver_port") {
 		port = config.Datadog.GetInt("apm_config.receiver_port")
 	}

go.mod
@@ -26,7 +26,7 @@ replace (
 	github.com/mholt/archiver => github.com/mholt/archiver v2.0.1-0.20171012052341-26cf5bb32d07+incompatible
 	github.com/prometheus/client_golang => github.com/prometheus/client_golang v0.9.2
 	github.com/spf13/cast => github.com/DataDog/cast v1.3.1-0.20190301154711-1ee8c8bd14a3
-	github.com/spf13/viper => github.com/DataDog/viper v1.7.1
+	github.com/spf13/viper => github.com/DataDog/viper v1.8.0
 	github.com/ugorji/go => github.com/ugorji/go v1.1.7
 )
 

go.sum
@@ -104,6 +104,8 @@ github.com/DataDog/mmh3 v0.0.0-20200316233529-f5b682d8c981 h1:UcKqIrOowv2PjTkOC2
 github.com/DataDog/mmh3 v0.0.0-20200316233529-f5b682d8c981/go.mod h1:SvsjzyJlSg0rKsqYgdcFxeEVflx3ZNAyFfkUHP0TxXg=
 github.com/DataDog/viper v1.7.1 h1:yyn+WujSB5rnBXHm7VzilXK8gN7qB4TLN1KU90RZVd8=
 github.com/DataDog/viper v1.7.1/go.mod h1:Gx7+/WONkbQIh3ac52KqcFTzipQ1OYHhCQGWAgHlzpc=
+github.com/DataDog/viper v1.8.0 h1:+0s6jzOzQEpMCW+c4bkirz+4vNoWm4jmiQNmKfO2p38=
+github.com/DataDog/viper v1.8.0/go.mod h1:Gx7+/WONkbQIh3ac52KqcFTzipQ1OYHhCQGWAgHlzpc=
 github.com/DataDog/watermarkpodautoscaler v0.1.0 h1:EK8mxbhGt+jNRu/q8fdGi05aX7buVSQqONWxS/DnzXM=
 github.com/DataDog/watermarkpodautoscaler v0.1.0/go.mod h1:JHOlfw4/9Ng1io9QDotxKggdFEYFd2Au9cj+lXzQSRA=
 github.com/DataDog/zstd v0.0.0-20160706220725-2bf71ec48360 h1:CiuXIvblnzlQEnFP5tvrzcONd0AZpOuqzFg+bkCq95U=

pkg/config/apm.go
@@ -1,0 +1,130 @@
+package config
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+func setupAPM(config Config) {
+	config.SetKnown("apm_config.obfuscation.elasticsearch.enabled")
+	config.SetKnown("apm_config.obfuscation.elasticsearch.keep_values")
+	config.SetKnown("apm_config.obfuscation.mongodb.enabled")
+	config.SetKnown("apm_config.obfuscation.mongodb.keep_values")
+	config.SetKnown("apm_config.obfuscation.http.remove_query_string")
+	config.SetKnown("apm_config.obfuscation.http.remove_paths_with_digits")
+	config.SetKnown("apm_config.obfuscation.remove_stack_traces")
+	config.SetKnown("apm_config.obfuscation.redis.enabled")
+	config.SetKnown("apm_config.obfuscation.memcached.enabled")
+	config.SetKnown("apm_config.extra_sample_rate")
+	config.SetKnown("apm_config.dd_agent_bin")
+	config.SetKnown("apm_config.trace_writer.connection_limit")
+	config.SetKnown("apm_config.trace_writer.queue_size")
+	config.SetKnown("apm_config.service_writer.connection_limit")
+	config.SetKnown("apm_config.service_writer.queue_size")
+	config.SetKnown("apm_config.stats_writer.connection_limit")
+	config.SetKnown("apm_config.stats_writer.queue_size")
+	config.SetKnown("apm_config.analyzed_rate_by_service.*")
+	config.SetKnown("apm_config.log_throttling")
+	config.SetKnown("apm_config.bucket_size_seconds")
+	config.SetKnown("apm_config.watchdog_check_delay")
+
+	if runtime.GOARCH == "386" && runtime.GOOS == "windows" {
+		// on Windows-32 bit, the trace agent isn't installed.  Set the default to disabled
+		// so that there aren't messages in the log about failing to start.
+		config.BindEnvAndSetDefault("apm_config.enabled", false, "DD_APM_ENABLED")
+	} else {
+		config.BindEnvAndSetDefault("apm_config.enabled", true, "DD_APM_ENABLED")
+	}
+
+	config.BindEnvAndSetDefault("apm_config.receiver_port", 8126, "DD_APM_RECEIVER_PORT", "DD_RECEIVER_PORT")
+
+	config.BindEnv("apm_config.receiver_timeout", "DD_APM_RECEIVER_TIMEOUT")                             //nolint:errcheck
+	config.BindEnv("apm_config.max_payload_size", "DD_APM_MAX_PAYLOAD_SIZE")                             //nolint:errcheck
+	config.BindEnv("apm_config.log_file", "DD_APM_LOG_FILE")                                             //nolint:errcheck
+	config.BindEnv("apm_config.max_events_per_second", "DD_APM_MAX_EPS", "DD_MAX_EPS")                   //nolint:errcheck
+	config.BindEnv("apm_config.max_traces_per_second", "DD_APM_MAX_TPS", "DD_MAX_TPS")                   //nolint:errcheck
+	config.BindEnv("apm_config.max_memory", "DD_APM_MAX_MEMORY")                                         //nolint:errcheck
+	config.BindEnv("apm_config.max_cpu_percent", "DD_APM_MAX_CPU_PERCENT")                               //nolint:errcheck
+	config.BindEnv("apm_config.env", "DD_APM_ENV")                                                       //nolint:errcheck
+	config.BindEnv("apm_config.apm_non_local_traffic", "DD_APM_NON_LOCAL_TRAFFIC")                       //nolint:errcheck
+	config.BindEnv("apm_config.apm_dd_url", "DD_APM_DD_URL")                                             //nolint:errcheck
+	config.BindEnv("apm_config.connection_limit", "DD_APM_CONNECTION_LIMIT", "DD_CONNECTION_LIMIT")      //nolint:errcheck
+	config.BindEnv("apm_config.connection_reset_interval", "DD_APM_CONNECTION_RESET_INTERVAL")           //nolint:errcheck
+	config.BindEnv("apm_config.profiling_dd_url", "DD_APM_PROFILING_DD_URL")                             //nolint:errcheck
+	config.BindEnv("apm_config.profiling_additional_endpoints", "DD_APM_PROFILING_ADDITIONAL_ENDPOINTS") //nolint:errcheck
+	config.BindEnv("apm_config.additional_endpoints", "DD_APM_ADDITIONAL_ENDPOINTS")                     //nolint:errcheck
+	config.BindEnv("apm_config.replace_tags", "DD_APM_REPLACE_TAGS")                                     //nolint:errcheck
+	config.BindEnv("apm_config.analyzed_spans", "DD_APM_ANALYZED_SPANS")                                 //nolint:errcheck
+	config.BindEnv("apm_config.ignore_resources", "DD_APM_IGNORE_RESOURCES", "DD_IGNORE_RESOURCE")       //nolint:errcheck
+	config.BindEnv("apm_config.receiver_socket", "DD_APM_RECEIVER_SOCKET")                               //nolint:errcheck
+
+	config.SetEnvKeyTransformer("apm_config.ignore_resources", func(in string) interface{} {
+		r, err := splitCSVString(in, ',')
+		if err != nil {
+			log.Warnf(`"apm_config.ignore_resources" can not be parsed: %v`, err)
+			return []string{}
+		}
+		return r
+	})
+
+	config.SetEnvKeyTransformer("apm_config.replace_tags", func(in string) interface{} {
+		var out []map[string]string
+		if err := json.Unmarshal([]byte(in), &out); err != nil {
+			log.Warnf(`"apm_config.replace_tags" can not be parsed: %v`, err)
+		}
+		return out
+	})
+
+	config.SetEnvKeyTransformer("apm_config.analyzed_spans", func(in string) interface{} {
+		out, err := parseAnalyzedSpans(in)
+		if err != nil {
+			log.Errorf(`Bad format for "apm_config.analyzed_spans" it should be of the form \"service_name|operation_name=rate,other_service|other_operation=rate\", error: %v`, err)
+		}
+		return out
+	})
+}
+
+func splitCSVString(s string, sep rune) ([]string, error) {
+	r := csv.NewReader(strings.NewReader(s))
+	r.TrimLeadingSpace = true
+	r.LazyQuotes = true
+	r.Comma = sep
+
+	return r.Read()
+}
+
+func parseNameAndRate(token string) (string, float64, error) {
+	parts := strings.Split(token, "=")
+	if len(parts) != 2 {
+		return "", 0, fmt.Errorf("Bad format")
+	}
+	rate, err := strconv.ParseFloat(parts[1], 64)
+	if err != nil {
+		return "", 0, fmt.Errorf("Unabled to parse rate")
+	}
+	return parts[0], rate, nil
+}
+
+// parseAnalyzedSpans parses the env string to extract a map of spans to be analyzed by service and operation.
+// the format is: service_name|operation_name=rate,other_service|other_operation=rate
+func parseAnalyzedSpans(env string) (analyzedSpans map[string]interface{}, err error) {
+	analyzedSpans = make(map[string]interface{})
+	if env == "" {
+		return
+	}
+	tokens := strings.Split(env, ",")
+	for _, token := range tokens {
+		name, rate, err := parseNameAndRate(token)
+		if err != nil {
+			return nil, err
+		}
+		analyzedSpans[name] = rate
+	}
+	return
+}

pkg/config/config.go
@@ -14,7 +14,6 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"runtime"
 	"strings"
 	"time"
 
@@ -494,18 +493,6 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("profiling.enabled", false)
 	config.BindEnv("profiling.profile_dd_url", "") //nolint:errcheck
 
-	// Trace agent
-	// Note that trace-agent environment variables are parsed in pkg/trace/config/env.go
-	// since some of them require custom parsing algorithms. DO NOT add environment variable
-	// bindings here, add them there instead.
-	if runtime.GOARCH == "386" && runtime.GOOS == "windows" {
-		// on Windows-32 bit, the trace agent isn't installed.  Set the default to disabled
-		// so that there aren't messages in the log about failing to start.
-		config.BindEnvAndSetDefault("apm_config.enabled", false)
-	} else {
-		config.BindEnvAndSetDefault("apm_config.enabled", true)
-	}
-
 	// Process agent
 	config.SetDefault("process_config.enabled", "false")
 	// process_config.enabled is only used on Windows by the core agent to start the process agent service.
@@ -710,50 +697,6 @@ func InitConfig(config Config) {
 	// Network
 	config.BindEnv("network.id") //nolint:errcheck
 
-	// APM
-	config.SetKnown("apm_config.enabled")
-	config.SetKnown("apm_config.env")
-	config.SetKnown("apm_config.additional_endpoints.*")
-	config.SetKnown("apm_config.apm_non_local_traffic")
-	config.SetKnown("apm_config.max_traces_per_second")
-	config.SetKnown("apm_config.max_memory")
-	config.SetKnown("apm_config.log_file")
-	config.SetKnown("apm_config.apm_dd_url")
-	config.SetKnown("apm_config.profiling_dd_url")
-	config.SetKnown("apm_config.profiling_additional_endpoints.*")
-	config.SetKnown("apm_config.max_cpu_percent")
-	config.SetKnown("apm_config.receiver_port")
-	config.SetKnown("apm_config.receiver_socket")
-	config.SetKnown("apm_config.connection_limit")
-	config.SetKnown("apm_config.ignore_resources")
-	config.SetKnown("apm_config.replace_tags")
-	config.SetKnown("apm_config.obfuscation.elasticsearch.enabled")
-	config.SetKnown("apm_config.obfuscation.elasticsearch.keep_values")
-	config.SetKnown("apm_config.obfuscation.mongodb.enabled")
-	config.SetKnown("apm_config.obfuscation.mongodb.keep_values")
-	config.SetKnown("apm_config.obfuscation.http.remove_query_string")
-	config.SetKnown("apm_config.obfuscation.http.remove_paths_with_digits")
-	config.SetKnown("apm_config.obfuscation.remove_stack_traces")
-	config.SetKnown("apm_config.obfuscation.redis.enabled")
-	config.SetKnown("apm_config.obfuscation.memcached.enabled")
-	config.SetKnown("apm_config.extra_sample_rate")
-	config.SetKnown("apm_config.dd_agent_bin")
-	config.SetKnown("apm_config.max_events_per_second")
-	config.SetKnown("apm_config.trace_writer.connection_limit")
-	config.SetKnown("apm_config.trace_writer.queue_size")
-	config.SetKnown("apm_config.service_writer.connection_limit")
-	config.SetKnown("apm_config.service_writer.queue_size")
-	config.SetKnown("apm_config.stats_writer.connection_limit")
-	config.SetKnown("apm_config.stats_writer.queue_size")
-	config.SetKnown("apm_config.connection_reset_interval") // in seconds
-	config.SetKnown("apm_config.analyzed_rate_by_service.*")
-	config.SetKnown("apm_config.analyzed_spans.*")
-	config.SetKnown("apm_config.log_throttling")
-	config.SetKnown("apm_config.bucket_size_seconds")
-	config.SetKnown("apm_config.receiver_timeout")
-	config.SetKnown("apm_config.watchdog_check_delay")
-	config.SetKnown("apm_config.max_payload_size")
-
 	// inventories
 	config.BindEnvAndSetDefault("inventories_enabled", true)
 	config.BindEnvAndSetDefault("inventories_max_interval", 600) // 10min
@@ -785,6 +728,7 @@ func InitConfig(config Config) {
 	config.SetKnown("cmd.check.fullsketches")
 
 	setAssetFs(config)
+	setupAPM(config)
 }
 
 var (

pkg/config/types.go
@@ -47,6 +47,7 @@ type Config interface {
 	SetEnvPrefix(in string)
 	BindEnv(input ...string) error
 	SetEnvKeyReplacer(r *strings.Replacer)
+	SetEnvKeyTransformer(key string, fn func(string) interface{})
 
 	UnmarshalKey(key string, rawVal interface{}, opts ...viper.DecoderConfigOption) error
 	Unmarshal(rawVal interface{}) error
@@ -77,8 +78,11 @@ type Config interface {
 	// API not implemented by viper.Viper and that have proven useful for our config usage
 
 	// BindEnvAndSetDefault sets the default value for a config parameter and adds an env binding
-	// in one call, used for most config options
-	BindEnvAndSetDefault(key string, val interface{})
+	// in one call, used for most config options.
+	//
+	// If env is provided, it will override the name of the environment variable used for this
+	// config key
+	BindEnvAndSetDefault(key string, val interface{}, env ...string)
 	// GetEnvVars returns a list of the non-sensitive env vars that the config supports
 	GetEnvVars() []string
 }

pkg/config/viper.go
@@ -57,6 +57,14 @@ func (c *safeConfig) GetKnownKeys() map[string]interface{} {
 	return c.Viper.GetKnownKeys()
 }
 
+// SetEnvKeyTransformer allows defining a transformer function which decides
+// how an environment variables value gets assigned to key.
+func (c *safeConfig) SetEnvKeyTransformer(key string, fn func(string) interface{}) {
+	c.Lock()
+	defer c.Unlock()
+	c.Viper.SetEnvKeyTransformer(key, fn)
+}
+
 // SetFs wraps Viper for concurrent access
 func (c *safeConfig) SetFs(fs afero.Fs) {
 	c.Lock()
@@ -348,9 +356,9 @@ func (c *safeConfig) GetEnvVars() []string {
 }
 
 // BindEnvAndSetDefault implements the Config interface
-func (c *safeConfig) BindEnvAndSetDefault(key string, val interface{}) {
+func (c *safeConfig) BindEnvAndSetDefault(key string, val interface{}, env ...string) {
 	c.SetDefault(key, val)
-	c.BindEnv(key) //nolint:errcheck
+	c.BindEnv(append([]string{key}, env...)...) //nolint:errcheck
 }
 
 // NewConfig returns a new Config object.

commit 9e1e21b45153c7f69bad9370e875e38ca61988da

pkg/trace/config: ensure apm_config.analyzed_spans allows non-float64 (#6631)
This change ensures that the `apm_config.analyzed_spans` values allow
non-float64 numeric values. It fixes a bug introduced by #6347

pkg/trace/config/apply.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -288,7 +289,9 @@ func (c *AgentConfig) applyDatadogConfig() error {
 				log.Errorf("Error parsing names: %v", err)
 				continue
 			}
-			if floatrate, ok := rate.(float64); ok {
+			if floatrate, err := toFloat64(rate); err != nil {
+				log.Errorf("Invalid value for apm_config.analyzed_spans: %v", err)
+			} else {
 				if _, ok := c.AnalyzedSpansByService[serviceName]; !ok {
 					c.AnalyzedSpansByService[serviceName] = make(map[string]float64)
 				}
@@ -404,4 +407,33 @@ func splitString(s string, sep rune) ([]string, error) {
 	r.Comma = sep
 
 	return r.Read()
+}
+
+func toFloat64(val interface{}) (float64, error) {
+	switch v := val.(type) {
+	case float64:
+		return v, nil
+	case float32:
+		return float64(v), nil
+	case int:
+		return float64(v), nil
+	case int32:
+		return float64(v), nil
+	case int64:
+		return float64(v), nil
+	case uint:
+		return float64(v), nil
+	case uint32:
+		return float64(v), nil
+	case uint64:
+		return float64(v), nil
+	case string:
+		f, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return 0, err
+		}
+		return f, nil
+	default:
+		return 0, fmt.Errorf("%v can not be converted to float64", val)
+	}
 }

releasenotes/notes/apm-analyzed-spans-float64-bug-ed3f1011ef393f06.yaml
@@ -1,0 +1,12 @@
+# Each section from every releasenote are combined when the
+# CHANGELOG.rst is rendered. So the text needs to be worded so that
+# it does not depend on any information only available in another
+# section. This may mean repeating some details, but each section
+# must be readable independently of the other.
+#
+# Each section note must be formatted as reStructuredText.
+---
+fixes:
+  - |
+    APM: Fix a bug where non-float64 numeric values in apm_config.analyzed_spans
+    would disable this functionality.