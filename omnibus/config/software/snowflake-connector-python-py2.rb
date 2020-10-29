bb026d31cff661a4a1f40cd48f0f70b70940941b
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 19:06:54 -0400

    Use version instead of default ver

--- omnibus/config/software/snowflake-connector-python-py2.rb
+++ omnibus/config/software/snowflake-connector-python-py2.rb
@@ -4,8 +4,7 @@ dependency "pip2"
 
 dependency "snowflake-connector-python"
 
-default_version "2.1.3"
-relative_path "snowflake-connector-python-#{version}"
+version "2.1.3"
 
 build do
   command "#{install_dir}/embedded/bin/pip2 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-#{version}"
