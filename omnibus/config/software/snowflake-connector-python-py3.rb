49e6396bfaf630a9c655ff89cc07eed8ff1f736a
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 19:05:59 -0400

    Add missing default version

--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -4,6 +4,8 @@ dependency "pip3"
 
 dependency "snowflake-connector-python"
 
+default_version "2.1.3"
+
 build do
   command "#{install_dir}/embedded/bin/pip3 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-#{version}"
 end
