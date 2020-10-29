365e51f6c3f4d728e687dd2dae016957a0adf2bb
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 18:20:57 -0400

    Fix py ver

--- omnibus/config/software/snowflake-connector-python-py2.rb
+++ omnibus/config/software/snowflake-connector-python-py2.rb
@@ -8,5 +8,5 @@ default_version "2.1.3"
 relative_path "snowflake-connector-python-#{version}"
 
 build do
-  command "#{install_dir}/embedded/bin/pip3 install #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-#{version}"
+  command "#{install_dir}/embedded/bin/pip2 install #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-#{version}"
 end
--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -1,9 +1,9 @@
-name "snowflake-connector-python-py2"
+name "snowflake-connector-python-py3"
 
-dependency "pip2"
+dependency "pip3"
 
 dependency "snowflake-connector-python"
 
 build do
-  command "cd #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3 && #{install_dir}/embedded/bin/pip2 install ."
+  command "cd #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3 && #{install_dir}/embedded/bin/pip3 install ."
 end
