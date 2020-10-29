d9dafff7ad24b5a8e11511cdbb927a55e23812df
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 12:48:56 -0400

    Move patch

--- omnibus/config/software/snowflake-connector-python-py2.rb
+++ omnibus/config/software/snowflake-connector-python-py2.rb
@@ -5,7 +5,5 @@ dependency "pip2"
 dependency "snowflake-connector-python"
 
 build do
-  ship_license "https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v#{version}/LICENSE.txt"
-  patch :source => "snowflake-connector-python-cryptography.patch", :target => "setup.py"
   command "#{install_dir}/embedded/bin/pip2 install ."
 end
--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -5,7 +5,5 @@ dependency "pip3"
 dependency "snowflake-connector-python"
 
 build do
-  ship_license "https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v#{version}/LICENSE.txt"
-  patch :source => "snowflake-connector-python-cryptography.patch", :target => "setup.py"
   command "#{install_dir}/embedded/bin/pip3 install ."
 end
