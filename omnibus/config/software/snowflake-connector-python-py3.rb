bc5318e7166211938f034588d4cfbec800c5e5e6
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 18:38:12 -0400

    Add --no-deps

--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -5,5 +5,5 @@ dependency "pip3"
 dependency "snowflake-connector-python"
 
 build do
-  command "cd #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3 && #{install_dir}/embedded/bin/pip3 install ."
+  command "#{install_dir}/embedded/bin/pip3 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-#{version}"
 end
