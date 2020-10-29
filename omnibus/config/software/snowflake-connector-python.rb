e8d896ce8d9ed933a22eb954578f93e23d1c08c9
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 13:10:51 -0400

    Add missing build

--- omnibus/config/software/snowflake-connector-python.rb
+++ omnibus/config/software/snowflake-connector-python.rb
@@ -9,5 +9,7 @@ source :url => "https://github.com/snowflakedb/snowflake-connector-python/archiv
        :sha256 => "855ffb93a09c3cd994dab8af7c87a46038bbba103928c5948a0edcd2500f4e1a",
        :extract => :seven_zip
 
-ship_license "https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v#{version}/LICENSE.txt"
-patch :source => "snowflake-connector-python-cryptography.patch", :target => "setup.py"
+build do
+    ship_license "https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v#{version}/LICENSE.txt"
+    patch :source => "snowflake-connector-python-cryptography.patch", :target => "setup.py"
+end
