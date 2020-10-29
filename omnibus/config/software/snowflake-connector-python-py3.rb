2d7d3a345df6e696eef4674009bdd99bd9b97947
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 13:53:27 -0400

    Pass relative path

--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -4,6 +4,8 @@ dependency "pip3"
 
 dependency "snowflake-connector-python"
 
+relative_path "snowflake-connector-python-#{version}"
+
 build do
   command "#{install_dir}/embedded/bin/pip3 install ."
 end
