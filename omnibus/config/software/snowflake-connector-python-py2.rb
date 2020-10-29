6c71df95df1ac06f7d85460f4510aa419ad800c1
ChristineTChen <christine.chen@datadoghq.com>
2020-10-29 19:28:03 -0400

    Hardcode version

--- omnibus/config/software/snowflake-connector-python-py2.rb
+++ omnibus/config/software/snowflake-connector-python-py2.rb
@@ -7,5 +7,5 @@ dependency "snowflake-connector-python"
 version "2.1.3"
 
 build do
-  command "#{install_dir}/embedded/bin/pip2 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-#{version}"
+  command "#{install_dir}/embedded/bin/pip2 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3"
 end
--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -4,8 +4,6 @@ dependency "pip3"
 
 dependency "snowflake-connector-python"
 
-version "2.1.3"
-
 build do
-  command "#{install_dir}/embedded/bin/pip3 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-#{version}"
+  command "#{install_dir}/embedded/bin/pip3 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3"
 end
