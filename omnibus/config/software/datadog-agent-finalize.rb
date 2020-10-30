9fad553192d6381e1acc40a2bbbe00f17a61ff41
Guillaume Valadon <guillaume.valadon@datadoghq.com>
2020-10-30 22:45:28 +0100

    Add security-agent from default-activated agents on host installs
    This partially reverts commit 2b906d40c0f6f9cbc52b39b9c05fd215fb971fbb

--- omnibus/config/software/datadog-agent-finalize.rb
+++ omnibus/config/software/datadog-agent-finalize.rb
@@ -107,12 +107,14 @@ build do
                 move "#{install_dir}/scripts/datadog-agent", "/etc/init.d"
                 move "#{install_dir}/scripts/datadog-agent-trace", "/etc/init.d"
                 move "#{install_dir}/scripts/datadog-agent-process", "/etc/init.d"
+                move "#{install_dir}/scripts/datadog-agent-security", "/etc/init.d"
             end
             if suse?
                 mkdir "/etc/init.d"
                 move "#{install_dir}/scripts/datadog-agent", "/etc/init.d"
                 move "#{install_dir}/scripts/datadog-agent-trace", "/etc/init.d"
                 move "#{install_dir}/scripts/datadog-agent-process", "/etc/init.d"
+                move "#{install_dir}/scripts/datadog-agent-security", "/etc/init.d"
             end
             mkdir systemd_directory
             move "#{install_dir}/scripts/datadog-agent.service", systemd_directory
