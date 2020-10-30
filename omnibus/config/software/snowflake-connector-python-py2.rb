59e9abb94de8a04219784ea4ac9ab3141f6aaa3d
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-10-30 10:51:50 +0100

    Install snowflake-connector-python with deps

--- omnibus/config/software/snowflake-connector-python-py2.rb
+++ omnibus/config/software/snowflake-connector-python-py2.rb
@@ -5,5 +5,5 @@ dependency "pip2"
 dependency "snowflake-connector-python"
 
 build do
-  command "#{install_dir}/embedded/bin/pip2 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3"
+  command "#{install_dir}/embedded/bin/pip2 install #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3"
 end
--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -4,5 +4,5 @@ dependency "pip3"
 dependency "snowflake-connector-python"
 
 build do
-  command "#{install_dir}/embedded/bin/pip3 install --no-deps #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3"
+  command "#{install_dir}/embedded/bin/pip3 install #{Omnibus::Config.source_dir()}/snowflake-connector-python/snowflake-connector-python-2.1.3"
 end
