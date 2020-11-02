03836ef4600b12aa256f14277f312c7498b2024f
Afchain <safchain@gmail.com>
2020-11-02 16:40:04 +0100

    Do not random path_id to limit LRU exhausting

--- pkg/security/tests/dentry_test.go
+++ pkg/security/tests/dentry_test.go
@@ -23,7 +23,7 @@ func TestDentryRename(t *testing.T) {
 		Expression: `rename.old.filename in ["{{.Root}}/test-rename", "{{.Root}}/test2-rename"]`,
 	}
 
-	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: false})
+	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: true})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -78,7 +78,7 @@ func TestDentryRenameFolder(t *testing.T) {
 		Expression: `open.basename == "test-rename" && (open.flags & O_CREAT) > 0`,
 	}
 
-	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: false})
+	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: true})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -107,8 +107,6 @@ func TestDentryRenameFolder(t *testing.T) {
 		testFile.Close()
 
 		event, _, err := test.GetEvent()
-		t.Logf("#%d Event: %+v", i, event)
-
 		if err != nil {
 			t.Error(err)
 		} else {
@@ -140,7 +138,7 @@ func TestDentryUnlink(t *testing.T) {
 		Expression: `unlink.filename =~ "{{.Root}}/test-unlink-*"`,
 	}
 
-	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: false})
+	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: true})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -182,7 +180,7 @@ func TestDentryRmdir(t *testing.T) {
 		Expression: `rmdir.filename =~ "{{.Root}}/test-rmdir-*"`,
 	}
 
-	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: false})
+	test, err := newTestModule(nil, []*rules.RuleDefinition{rule}, testOpts{enableFilters: true})
 	if err != nil {
 		t.Fatal(err)
 	}
