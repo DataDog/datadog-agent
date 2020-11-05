DataDog/datadog-agent

commit 74c5a909cca90ddd76f7dfb408b186eb7b8bfa7b

[logging] Makes jmxfetch use its own logger (#5671)

pkg/util/log/log.go
@@ -17,7 +17,8 @@ import (
 )
 
 var (
-	logger *DatadogLogger
+	logger    *DatadogLogger
+	jmxLogger *DatadogLogger
 
 	// This buffer holds log lines sent to the logger before its
 	// initialization. Even if initializing the logger is one of the first
@@ -40,10 +41,20 @@ type DatadogLogger struct {
 	contextLock sync.Mutex
 }
 
-// SetupDatadogLogger configure logger singleton with seelog interface
-func SetupDatadogLogger(l seelog.LoggerInterface, level string) {
-	logger = &DatadogLogger{
-		inner: l,
+// SetupLogger setup agent wide logger
+func SetupLogger(i seelog.LoggerInterface, level string) {
+	logger = setupCommonLogger(i, level)
+}
+
+// SetupJMXLogger setup JMXfetch specific logger
+func SetupJMXLogger(i seelog.LoggerInterface, level string) {
+	jmxLogger = setupCommonLogger(i, level)
+}
+
+func setupCommonLogger(i seelog.LoggerInterface, level string) *DatadogLogger {
+
+	l := &DatadogLogger{
+		inner: i,
 		extra: make(map[string]seelog.LoggerInterface),
 	}
 
@@ -51,7 +62,7 @@ func SetupDatadogLogger(l seelog.LoggerInterface, level string) {
 	if !ok {
 		lvl = seelog.InfoLvl
 	}
-	logger.level = lvl
+	l.level = lvl
 
 	// We're not going to call DatadogLogger directly, but using the
 	// exported functions, that will give us two frames in the stack
@@ -60,7 +71,7 @@ func SetupDatadogLogger(l seelog.LoggerInterface, level string) {
 	// The fact we need a constant "additional depth" means some
 	// theoretical refactor to avoid duplication in the functions
 	// below cannot be performed.
-	logger.inner.SetAdditionalStackDepth(defaultStackDepth) //nolint:errcheck
+	l.inner.SetAdditionalStackDepth(defaultStackDepth) //nolint:errcheck
 
 	// Flushing logs since the logger is now initialized
 	bufferMutex.Lock()
@@ -70,6 +81,7 @@ func SetupDatadogLogger(l seelog.LoggerInterface, level string) {
 		logLine()
 	}
 	logsBuffer = []func(){}
+	return l
 }
 
 func addLogToBuffer(logHandle func()) {
@@ -178,7 +190,6 @@ func (sw *DatadogLogger) info(s string) {
 
 	scrubbed := sw.scrub(s)
 	sw.inner.Info(scrubbed)
-
 	for _, l := range sw.extra {
 		l.Info(scrubbed)
 	}
@@ -507,10 +518,23 @@ func ErrorStackDepth(depth int, v ...interface{}) error {
 	}, true, v...)
 }
 
+// JMXError Logs for JMX check
+func JMXError(v ...interface{}) error {
+	return logWithError(seelog.ErrorLvl, func() { JMXError(v...) }, jmxLogger.error, true, v...)
+}
+
+//JMXInfo Logs
+func JMXInfo(v ...interface{}) {
+	log(seelog.InfoLvl, func() { JMXInfo(v...) }, jmxLogger.info, v...)
+}
+
 // Flush flushes the underlying inner log
 func Flush() {
 	if logger != nil && logger.inner != nil {
 		logger.inner.Flush()
+	}
+	if jmxLogger != nil && jmxLogger.inner != nil {
+		jmxLogger.inner.Flush()
 	}
 }
 
@@ -561,7 +585,7 @@ func ChangeLogLevel(l seelog.LoggerInterface, level string) error {
 		if err != nil {
 			return err
 		}
-		// See detailed explanation in SetupDatadogLogger(...)
+		// See detailed explanation in SetupLogger(...)
 		err = l.SetAdditionalStackDepth(defaultStackDepth)
 		if err != nil {
 			return err

pkg/config/log.go
@@ -36,6 +36,7 @@ const (
 var syslogTLSConfig *tls.Config
 
 var seelogConfig *seelogCfg.Config
+var jmxSeelogConfig *seelogCfg.Config
 
 func getLogDateFormat() string {
 	if Datadog.GetBool("log_format_rfc3339") {
@@ -52,12 +53,18 @@ func createQuoteMsgFormatter(params string) seelog.FormatterFunc {
 
 // buildCommonFormat returns the log common format seelog string
 func buildCommonFormat(loggerName LoggerName) string {
+	if loggerName == "JMXFETCH" {
+		return `%Msg%n`
+	}
 	return fmt.Sprintf("%%Date(%s) | %s | %%LEVEL | (%%ShortFilePath:%%Line in %%FuncShort) | %%ExtraTextContext%%Msg%%n", getLogDateFormat(), loggerName)
 }
 
 // buildJSONFormat returns the log JSON format seelog string
 func buildJSONFormat(loggerName LoggerName) string {
 	seelog.RegisterCustomFormatter("QuoteMsg", createQuoteMsgFormatter) //nolint:errcheck
+	if loggerName == "JMXFETCH" {
+		return `{"msg":%QuoteMsg}%n`
+	}
 	return fmt.Sprintf(`{"agent":"%s","time":"%%Date(%s)","level":"%%LEVEL","file":"%%ShortFilePath","line":"%%Line","func":"%%FuncShort","msg":%%QuoteMsg%%ExtraJSONContext}%%n`, strings.ToLower(string(loggerName)), getLogDateFormat())
 }
 
@@ -93,20 +100,48 @@ func SetupLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, sys
 	if err != nil {
 		return err
 	}
+	seelogConfig, err = buildLoggerConfig(loggerName, seelogLogLevel, logFile, syslogURI, syslogRFC, logToConsole, jsonFormat)
+	if err != nil {
+		return err
+	}
+	loggerInterface, err := GenerateLoggerInterface(seelogConfig)
+	log.SetupLogger(loggerInterface, seelogLogLevel)
+	log.AddStrippedKeys(Datadog.GetStringSlice("flare_stripped_keys"))
+	return err
+}
 
+// SetupJMXLogger sets up a logger with JMX logger name and log level
+// if a non empty logFile is provided, it will also log to the file
+// a non empty syslogURI will enable syslog, and format them following RFC 5424 if specified
+// you can also specify to log to the console and in JSON format
+func SetupJMXLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, syslogRFC, logToConsole, jsonFormat bool) error {
+	seelogLogLevel, err := validateLogLevel(logLevel)
+	if err != nil {
+		return err
+	}
+	jmxSeelogConfig, err = buildLoggerConfig(loggerName, seelogLogLevel, logFile, syslogURI, syslogRFC, logToConsole, jsonFormat)
+	if err != nil {
+		return err
+	}
+	jmxLoggerInterface, err := GenerateLoggerInterface(jmxSeelogConfig)
+	log.SetupJMXLogger(jmxLoggerInterface, seelogLogLevel)
+	return err
+}
+
+func buildLoggerConfig(loggerName LoggerName, seelogLogLevel, logFile, syslogURI string, syslogRFC, logToConsole, jsonFormat bool) (*seelogCfg.Config, error) {
 	formatID := "common"
 	if jsonFormat {
 		formatID = "json"
 	}
 
-	seelogConfig = seelogCfg.NewSeelogConfig(string(loggerName), seelogLogLevel, formatID, buildJSONFormat(loggerName), buildCommonFormat(loggerName), syslogRFC)
-	seelogConfig.EnableConsoleLog(logToConsole)
-	seelogConfig.EnableFileLogging(logFile, Datadog.GetSizeInBytes("log_file_max_size"), uint(Datadog.GetInt("log_file_max_rolls")))
+	config := seelogCfg.NewSeelogConfig(string(loggerName), seelogLogLevel, formatID, buildJSONFormat(loggerName), buildCommonFormat(loggerName), syslogRFC)
+	config.EnableConsoleLog(logToConsole)
+	config.EnableFileLogging(logFile, Datadog.GetSizeInBytes("log_file_max_size"), uint(Datadog.GetInt("log_file_max_rolls")))
 
 	if syslogURI != "" { // non-blank uri enables syslog
 		syslogTLSKeyPair, err := getSyslogTLSKeyPair()
 		if err != nil {
-			return err
+			return nil, err
 		}
 		var useTLS bool
 		if syslogTLSKeyPair != nil {
@@ -116,22 +151,24 @@ func SetupLogger(loggerName LoggerName, logLevel, logFile, syslogURI string, sys
 				InsecureSkipVerify: Datadog.GetBool("syslog_tls_verify"),
 			}
 		}
-		seelogConfig.ConfigureSyslog(syslogURI, useTLS)
+		config.ConfigureSyslog(syslogURI, useTLS)
+	}
+	return config, nil
+}
+
+//GenerateLoggerInterface return a logger Interface from a log config
+func GenerateLoggerInterface(logConfig *seelogCfg.Config) (seelog.LoggerInterface, error) {
+	configTemplate, err := logConfig.Render()
+	if err != nil {
+		return nil, err
 	}
 
-	configTemplate, err := seelogConfig.Render()
+	loggerInterface, err := seelog.LoggerFromConfigAsString(configTemplate)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	logger, err := seelog.LoggerFromConfigAsString(configTemplate)
-	if err != nil {
-		return err
-	}
-	seelog.ReplaceLogger(logger) //nolint:errcheck
-	log.SetupDatadogLogger(logger, seelogLogLevel)
-	log.AddStrippedKeys(Datadog.GetStringSlice("flare_stripped_keys"))
-	return nil
+	return loggerInterface, err
 }
 
 // ErrorLogWriter is a Writer that logs all written messages with the global seelog logger

cmd/agent/api/agent/agent.go
@@ -101,9 +101,12 @@ func makeFlare(w http.ResponseWriter, r *http.Request) {
 	if logFile == "" {
 		logFile = common.DefaultLogFile
 	}
-
+	jmxLogFile := config.Datadog.GetString("jmx_log_file")
+	if jmxLogFile == "" {
+		jmxLogFile = common.DefaultJmxLogFile
+	}
 	log.Infof("Making a flare")
-	filePath, err := flare.CreateArchive(false, common.GetDistPath(), common.PyChecksPath, logFile, profile)
+	filePath, err := flare.CreateArchive(false, common.GetDistPath(), common.PyChecksPath, []string{logFile, jmxLogFile}, profile)
 	if err != nil || filePath == "" {
 		if err != nil {
 			log.Errorf("The flare failed to be created: %s", err)

cmd/agent/app/app.go
@@ -39,6 +39,7 @@ monitoring and performance data.`,
 
 // loggerName is the name of the core agent logger
 const loggerName config.LoggerName = "CORE"
+const jmxLoggerName config.LoggerName = "JMXFETCH"
 
 func init() {
 	AgentCmd.PersistentFlags().StringVarP(&confFilePath, "cfgpath", "c", "", "path to directory containing datadog.yaml")

cmd/agent/app/flare.go
@@ -108,6 +108,11 @@ func makeFlare(caseID string) error {
 	if logFile == "" {
 		logFile = common.DefaultLogFile
 	}
+	jmxLogFile := config.Datadog.GetString("jmx_log_file")
+	if jmxLogFile == "" {
+		jmxLogFile = common.DefaultJmxLogFile
+	}
+	logFiles := []string{logFile, jmxLogFile}
 	var (
 		profile flare.ProfileData
 		err     error
@@ -124,9 +129,9 @@ func makeFlare(caseID string) error {
 
 	var filePath string
 	if forceLocal {
-		filePath, err = createArchive(logFile, profile)
+		filePath, err = createArchive(logFiles, profile)
 	} else {
-		filePath, err = requestArchive(logFile, profile)
+		filePath, err = requestArchive(logFiles, profile)
 	}
 
 	if err != nil {
@@ -156,14 +161,14 @@ func makeFlare(caseID string) error {
 	return nil
 }
 
-func requestArchive(logFile string, pdata flare.ProfileData) (string, error) {
+func requestArchive(logFiles []string, pdata flare.ProfileData) (string, error) {
 	fmt.Fprintln(color.Output, color.BlueString("Asking the agent to build the flare archive."))
 	var e error
 	c := util.GetClient(false) // FIX: get certificates right then make this true
 	ipcAddress, err := config.GetIPCAddress()
 	if err != nil {
 		fmt.Fprintln(color.Output, color.RedString(fmt.Sprintf("Error getting IPC address for the agent: %s", err)))
-		return createArchive(logFile, pdata)
+		return createArchive(logFiles, pdata)
 	}
 
 	urlstr := fmt.Sprintf("https://%v:%v/agent/flare", ipcAddress, config.Datadog.GetInt("cmd_port"))
@@ -172,7 +177,7 @@ func requestArchive(logFile string, pdata flare.ProfileData) (string, error) {
 	e = util.SetAuthToken()
 	if e != nil {
 		fmt.Fprintln(color.Output, color.RedString(fmt.Sprintf("Error: %s", e)))
-		return createArchive(logFile, pdata)
+		return createArchive(logFiles, pdata)
 	}
 
 	p, err := json.Marshal(pdata)
@@ -188,14 +193,14 @@ func requestArchive(logFile string, pdata flare.ProfileData) (string, error) {
 		} else {
 			fmt.Fprintln(color.Output, color.RedString("The agent was unable to make the flare. (is it running?)"))
 		}
-		return createArchive(logFile, pdata)
+		return createArchive(logFiles, pdata)
 	}
 	return string(r), nil
 }
 
-func createArchive(logFile string, pdata flare.ProfileData) (string, error) {
+func createArchive(logFiles []string, pdata flare.ProfileData) (string, error) {
 	fmt.Fprintln(color.Output, color.YellowString("Initiating flare locally."))
-	filePath, e := flare.CreateArchive(true, common.GetDistPath(), common.PyChecksPath, logFile, pdata)
+	filePath, e := flare.CreateArchive(true, common.GetDistPath(), common.PyChecksPath, logFiles, pdata)
 	if e != nil {
 		fmt.Printf("The flare zipfile failed to be created: %s\n", e)
 		return "", e

cmd/agent/app/run.go
@@ -153,9 +153,15 @@ func StartAgent() error {
 			logFile = common.DefaultLogFile
 		}
 
+		jmxLogFile := config.Datadog.GetString("jmx_log_file")
+		if jmxLogFile == "" {
+			jmxLogFile = common.DefaultJmxLogFile
+		}
+
 		if config.Datadog.GetBool("disable_file_logging") {
 			// this will prevent any logging on file
 			logFile = ""
+			jmxLogFile = ""
 		}
 
 		err = config.SetupLogger(
@@ -167,6 +173,20 @@ func StartAgent() error {
 			config.Datadog.GetBool("log_to_console"),
 			config.Datadog.GetBool("log_format_json"),
 		)
+
+		//Setup JMX logger
+		if err == nil {
+			err = config.SetupJMXLogger(
+				jmxLoggerName,
+				config.Datadog.GetString("log_level"),
+				jmxLogFile,
+				syslogURI,
+				config.Datadog.GetBool("syslog_rfc"),
+				config.Datadog.GetBool("log_to_console"),
+				config.Datadog.GetBool("log_format_json"),
+			)
+		}
+
 	} else {
 		err = config.SetupLogger(
 			loggerName,
@@ -177,6 +197,19 @@ func StartAgent() error {
 			true,  // always log to console
 			false, // not in json
 		)
+
+		//Setup JMX logger
+		if err == nil {
+			err = config.SetupJMXLogger(
+				jmxLoggerName,
+				config.Datadog.GetString("log_level"),
+				"", // no log file on android
+				"", // no syslog on android,
+				false,
+				true,  // always log to console
+				false, // not in json
+			)
+		}
 	}
 	if err != nil {
 		return fmt.Errorf("Error while setting up logging, exiting: %v", err)

cmd/agent/common/common_android.go
@@ -16,6 +16,8 @@ const (
 	DefaultLogFile = "/var/log/datadog/agent.log"
 	// DefaultDCALogFile points to the log file that will be used if not configured
 	DefaultDCALogFile = "/var/log/datadog/cluster-agent.log"
+	//DefaultJmxLogFile points to the jmx fetch log file that will be used if not configured
+	DefaultJmxLogFile = "/var/log/datadog/jmxfetch.log"
 )
 
 var (

cmd/agent/common/common_darwin.go
@@ -16,6 +16,8 @@ const (
 	DefaultLogFile = "/var/log/datadog/agent.log"
 	// DefaultDCALogFile points to the log file that will be used if not configured
 	DefaultDCALogFile = "/var/log/datadog/cluster-agent.log"
+	//DefaultJmxLogFile points to the jmx fetch log file that will be used if not configured
+	DefaultJmxLogFile = "/var/log/datadog/jmxfetch.log"
 )
 
 var (

commit 15256c00ec30c75679c6623488f8b57637f7a440

[logging] Fix logger initialization
Was broken by #5671

pkg/util/log/log.go
@@ -43,6 +43,7 @@ type DatadogLogger struct {
 
 // SetupLogger setup agent wide logger
 func SetupLogger(i seelog.LoggerInterface, level string) {
+	seelog.ReplaceLogger(i)
 	logger = setupCommonLogger(i, level)
 }
 