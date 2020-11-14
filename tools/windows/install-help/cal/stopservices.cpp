DataDog/datadog-agent

commit 24b2a69e7bdb242ba62e7db8aee3f4ddb5b39883

When upgrading from the NPM beta to 7.24 (which doesn't include npm), (#6694)
upgrade was leaving a service configured, pointing to a binary that no
longer exists.

tools/windows/install-help/cal/stopservices.cpp
@@ -887,6 +887,7 @@ int verifyServices(CustomActionData& data)
     // Get a handle to the SCM database. 
 #ifdef __REGISTER_ALL_SERVICES
   #define NUM_SERVICES 4
+  #define SYSPROBE_INDEX 3
     serviceDef services[NUM_SERVICES] = {
         serviceDef(agentService.c_str(), L"Datadog Agent", L"Send metrics to Datadog",
                    agent_exe.c_str(),
@@ -934,6 +935,16 @@ int verifyServices(CustomActionData& data)
         if (retval != 0) {
             WcaLog(LOGMSG_STANDARD, "Failed to verify service %d %d 0x%x, rolling back", i, retval, retval);
             break;
+        }
+    }
+    if(!data.installSysprobe()) {
+        retval = services[SYSPROBE_INDEX].destroy(hScManager);
+        if(0 == retval) {
+            WcaLog(LOGMSG_STANDARD, "Removed system probe service");
+        } else if ( ERROR_SERVICE_DOES_NOT_EXIST == retval ) {
+            WcaLog(LOGMSG_STANDARD, "system probe not present");
+        } else {
+            WcaLog(LOGMSG_STANDARD, "Error removing system probe service %d", retval);
         }
     }
     WcaLog(LOGMSG_STANDARD, "done updating services");

commit 90e456ef4d874e8fbda6d317a4ec56c94cbeaddc

Fixes bug introduced by #6694.  Install would fail when upgrading from an install that didn't already have system-probe

tools/windows/install-help/cal/stopservices.cpp
@@ -946,6 +946,9 @@ int verifyServices(CustomActionData& data)
         } else {
             WcaLog(LOGMSG_STANDARD, "Error removing system probe service %d", retval);
         }
+        // reset retval to zero.  If we were unable to remove the system-probe service,
+        // and it's not present anyway, don't cause the entire install to fail
+        retval = 0;
     }
     WcaLog(LOGMSG_STANDARD, "done updating services");
    