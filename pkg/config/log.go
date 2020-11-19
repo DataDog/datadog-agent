67576230870c09bc1c58d42cfe67cb0c5002e56c
Remy Mathieu <remy.mathieu@datadoghq.com>
2020-11-19 15:31:10 +0100

    config: fix log conflict while adding the serverless format.

--- pkg/config/log.go
+++ pkg/config/log.go
@@ -51,23 +51,6 @@ func createQuoteMsgFormatter(params string) seelog.FormatterFunc {
 	}
 }
 
-// buildCommonFormat returns the log common format seelog string
-func buildCommonFormat(loggerName LoggerName) string {
-	if loggerName == "JMXFETCH" {
-		return `%Msg%n`
-	}
-	return fmt.Sprintf("%%Date(%s) | %s | %%LEVEL | (%%ShortFilePath:%%Line in %%FuncShort) | %%ExtraTextContext%%Msg%%n", getLogDateFormat(), loggerName)
-}
-
-// buildJSONFormat returns the log JSON format seelog string
-func buildJSONFormat(loggerName LoggerName) string {
-	seelog.RegisterCustomFormatter("QuoteMsg", createQuoteMsgFormatter) //nolint:errcheck
-	if loggerName == "JMXFETCH" {
-		return `{"msg":%QuoteMsg}%n`
-	}
-	return fmt.Sprintf(`{"agent":"%s","time":"%%Date(%s)","level":"%%LEVEL","file":"%%ShortFilePath","line":"%%Line","func":"%%FuncShort","msg":%%QuoteMsg%%ExtraJSONContext}%%n`, strings.ToLower(string(loggerName)), getLogDateFormat())
-}
-
 func getSyslogTLSKeyPair() (*tls.Certificate, error) {
 	var syslogTLSKeyPair *tls.Certificate
 	if Datadog.IsSet("syslog_pem") && Datadog.IsSet("syslog_key") {
