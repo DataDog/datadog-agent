dce38d3ff3794a87fa24ca48e798d2643a17ffae
Bryce Kahle <bryce.kahle@datadoghq.com>
2020-12-04 12:48:08 -0500

    Improve TestEbpfBytesCorrect

--- pkg/ebpf/bytecode/ebpf_bytes_match_test.go
+++ pkg/ebpf/bytecode/ebpf_bytes_match_test.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path"
-	"path/filepath"
-	"runtime"
 	"testing"
 
 	"github.com/DataDog/datadog-agent/pkg/ebpf/bytecode/bindata"
@@ -15,24 +13,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func curDir() string {
-	_, file, _, _ := runtime.Caller(0)
-	return filepath.Dir(file)
-}
-
 func TestEbpfBytesCorrect(t *testing.T) {
-	dir := filepath.Join(curDir(), "build")
+	bpfDir := "build"
 	for _, filename := range bindata.AssetNames() {
-		bs, err := ioutil.ReadFile(path.Join(dir, filename))
-		require.NoError(t, err)
+		t.Run(filename, func(t *testing.T) {
+			bs, err := ioutil.ReadFile(path.Join(bpfDir, filename))
+			require.NoError(t, err)
 
-		actualReader, err := GetReader(dir, filename)
-		require.NoError(t, err)
-		defer actualReader.Close()
+			actualReader, err := GetReader(bpfDir, filename)
+			require.NoError(t, err)
+			defer actualReader.Close()
 
-		actual, err := ioutil.ReadAll(actualReader)
-		require.NoError(t, err)
-
-		assert.Equal(t, bs, actual, fmt.Sprintf("on-disk file %s and bundled content are different", filename))
+			actual, err := ioutil.ReadAll(actualReader)
+			require.NoError(t, err)
+			assert.Equal(t, bs, actual, fmt.Sprintf("on-disk file %s and bundled content are different", filename))
+		})
 	}
 }
