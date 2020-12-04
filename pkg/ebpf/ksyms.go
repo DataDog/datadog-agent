fdbcf08ed9cc6a67cf4fcdc75e0be4121663ea52
Pedro Lambert <pedrolambert@gmail.com>
2020-12-04 13:16:24 -0500

    [networks] Improve kallsym lookup

--- pkg/ebpf/ksyms.go
+++ pkg/ebpf/ksyms.go
@@ -3,18 +3,20 @@ package ebpf
 import (
 	"bufio"
 	"os"
-	"strings"
+	"sort"
 
+	"github.com/DataDog/datadog-agent/pkg/process/util"
 	"github.com/pkg/errors"
 )
 
 // VerifyKernelFuncs ensures all kernel functions exist in ksyms located at provided path.
 func VerifyKernelFuncs(path string, requiredKernelFuncs []string) ([]string, error) {
 	// Will hold the found functions
-	found := make(map[string]bool, len(requiredKernelFuncs))
-	for _, f := range requiredKernelFuncs {
-		found[f] = false
+	missing := make(util.SSBytes, len(requiredKernelFuncs))
+	for i, f := range requiredKernelFuncs {
+		missing[i] = []byte(f)
 	}
+	sort.Sort(missing)
 
 	f, err := os.Open(path)
 	if err != nil {
@@ -23,26 +25,19 @@ func VerifyKernelFuncs(path string, requiredKernelFuncs []string) ([]string, err
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-
-	for scanner.Scan() {
-		fields := strings.Fields(scanner.Text())
-		if len(fields) < 3 {
-			continue
-		}
-
-		name := fields[2]
-		if _, ok := found[name]; ok {
-			found[name] = true
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() && len(missing) > 0 {
+		if i := missing.Search(scanner.Bytes()); i < len(missing) {
+			missing[0], missing[i] = missing[i], missing[0]
+			missing = missing[1:]
+			sort.Sort(missing)
 		}
 	}
 
-	var missing []string
-	for probe, b := range found {
-		if !b {
-			missing = append(missing, probe)
-		}
+	missingStrs := make([]string, len(missing))
+	for i := range missing {
+		missingStrs[i] = string(missing[i])
 	}
 
-	return missing, nil
+	return missingStrs, nil
 }
