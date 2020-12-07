8b34cca1fe9852f2944a39a34a41830135aa4e67
Vyacheslav <git@voronenko.info>
2020-12-07 10:29:19 +0100

    Stac 11513 windows 2012 (#243)
    * chore(tests) windows 2012 experiment
    * chore(molecule) vm scenario - W12 does not have option for ssh
    * improve(vms) centos6 officially deprecated
    * improve(tests) PS3 compatible way to check installed package
    * windows-16 is default for vm test

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -124,7 +124,7 @@ dependency 'datadog-agent'
 # Additional software
 dependency 'pip'
 dependency 'stackstate-agent-integrations'
-dependency 'datadog-a7'
+#dependency 'datadog-a7'
 dependency 'datadog-agent-env-check'
 dependency 'jmxfetch'
 
