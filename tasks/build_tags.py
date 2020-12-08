a4e51223ced4378e4a8bd6f047f04fa921342411
Bryce Kahle <bryce.kahle@datadoghq.com>
2020-12-08 12:56:18 -0500

    security-agent doesn't need linux_bpf tag

--- tasks/build_tags.py
+++ tasks/build_tags.py
@@ -83,7 +83,7 @@ IOT_AGENT_TAGS = set(["jetson", "systemd", "zlib",])
 PROCESS_AGENT_TAGS = AGENT_TAGS.union(set(["clusterchecks", "fargateprocess", "orchestrator",]))
 
 # SECURITY_AGENT_TAGS lists the tags necessary to build the security agent
-SECURITY_AGENT_TAGS = set(["netcgo", "secrets", "docker", "kubeapiserver", "kubelet", "linux_bpf",])
+SECURITY_AGENT_TAGS = set(["netcgo", "secrets", "docker", "kubeapiserver", "kubelet",])
 
 # PROCESS_AGENT_TAGS lists the tags necessary to build system-probe
 SYSTEM_PROBE_TAGS = AGENT_TAGS.union(set(["clusterchecks", "linux_bpf",]))
