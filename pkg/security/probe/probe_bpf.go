47e5abe22d7090e861306975ada8b89e31c573cf
Gui774ume <guillaume.fournier@datadoghq.com>
2020-12-27 19:33:40 +0100

    lock process resolver before computing cache size

--- pkg/security/probe/probe_bpf.go
+++ pkg/security/probe/probe_bpf.go
@@ -222,7 +222,7 @@ func (p *Probe) SendStats(statsdClient *statsd.Client) error {
 		}
 	}
 
-	if err := statsdClient.Gauge(MetricPrefix+".process_resolver.cache_size", float64(len(p.resolvers.ProcessResolver.entryCache)), []string{}, 1.0); err != nil {
+	if err := statsdClient.Gauge(MetricPrefix+".process_resolver.cache_size", p.resolvers.ProcessResolver.GetCacheSize(), []string{}, 1.0); err != nil {
 		return errors.Wrap(err, "failed to send process_resolver cache_size metric")
 	}
 	return nil
