b025b41d79a1ba5a7628c5c5c61e881dfe467c7a
Shang Wang <shang.wang@datadoghq.com>
2021-01-06 16:58:04 -0500

    nit

--- pkg/process/procutil/process_linux.go
+++ pkg/process/procutil/process_linux.go
@@ -102,15 +102,18 @@ func (p *Probe) StatsForPIDs(pids []int32, now time.Time) (map[int32]*Stats, err
 		statusInfo := p.parseStatus(pathForPID)
 		ioInfo := p.parseIO(pathForPID)
 		statInfo := p.parseStat(pathForPID, pid, now)
+		memInfoEx := p.parseStatm(pathForPID)
+
 		statsByPID[pid] = &Stats{
-			CreateTime:  statInfo.createTime,    // /proc/{pid}/{stat}
-			Nice:        statInfo.nice,          // /proc/{pid}/{stat}
-			CPUTime:     statInfo.cpuStat,       // /proc/{pid}/{stat}
-			MemInfo:     statusInfo.memInfo,     // /proc/{pid}/status
-			MemInfoEx:   &MemoryInfoExStat{},    // not needed currently so not collected to reduce workload
-			CtxSwitches: statusInfo.ctxSwitches, // /proc/{pid}/status
-			NumThreads:  statusInfo.numThreads,  // /proc/{pid}/status
-			IOStat:      ioInfo,                 // /proc/{pid}/io, requires permission checks
+			CreateTime:  statInfo.createTime,      // /proc/[pid]/stat
+			Nice:        statInfo.nice,            // /proc/[pid]/stat
+			OpenFdCount: p.getFDCount(pathForPID), // /proc/[pid]/fd, requires permission checks
+			CPUTime:     statInfo.cpuStat,         // /proc/[pid]/stat
+			MemInfo:     statusInfo.memInfo,       // /proc/[pid]/status
+			MemInfoEx:   memInfoEx,                // /proc/[pid]/statm
+			CtxSwitches: statusInfo.ctxSwitches,   // /proc/[pid]/status
+			NumThreads:  statusInfo.numThreads,    // /proc/[pid]/status
+			IOStat:      ioInfo,                   // /proc/[pid]/io, requires permission checks
 		}
 	}
 	return statsByPID, nil
@@ -144,26 +147,26 @@ func (p *Probe) ProcessesByPID(now time.Time) (map[int32]*Process, error) {
 		memInfoEx := p.parseStatm(pathForPID)
 
 		procsByPID[pid] = &Process{
-			Pid:     pid,                                       // /proc/{pid}
-			Ppid:    statInfo.ppid,                             // /proc/{pid}/{stat}
-			Cmdline: cmdline,                                   // /proc/{pid}/cmdline
-			Name:    statusInfo.name,                           // /proc/{pid}/status
-			Status:  statusInfo.status,                         // /proc/{pid}/status
-			Uids:    statusInfo.uids,                           // /proc/{pid}/status
-			Gids:    statusInfo.gids,                           // /proc/{pid}/status
-			Cwd:     p.getLinkWithAuthCheck(pathForPID, "cwd"), // /proc/{pid}/cwd, requires permission checks
-			Exe:     p.getLinkWithAuthCheck(pathForPID, "exe"), // /proc/{pid}/exe, requires permission checks
-			NsPid:   statusInfo.nspid,                          // /proc/{pid}/status
+			Pid:     pid,                                       // /proc/[pid]
+			Ppid:    statInfo.ppid,                             // /proc/[pid]/stat
+			Cmdline: cmdline,                                   // /proc/[pid]/cmdline
+			Name:    statusInfo.name,                           // /proc/[pid]/status
+			Status:  statusInfo.status,                         // /proc/[pid]/status
+			Uids:    statusInfo.uids,                           // /proc/[pid]/status
+			Gids:    statusInfo.gids,                           // /proc/[pid]/status
+			Cwd:     p.getLinkWithAuthCheck(pathForPID, "cwd"), // /proc/[pid]/cwd, requires permission checks
+			Exe:     p.getLinkWithAuthCheck(pathForPID, "exe"), // /proc/[pid]/exe, requires permission checks
+			NsPid:   statusInfo.nspid,                          // /proc/[pid]/status
 			Stats: &Stats{
-				CreateTime:  statInfo.createTime,      // /proc/{pid}/{stat}
-				Nice:        statInfo.nice,            // /proc/{pid}/{stat}
-				OpenFdCount: p.getFDCount(pathForPID), // /proc/{pid}/fd, requires permission checks
-				CPUTime:     statInfo.cpuStat,         // /proc/{pid}/{stat}
-				MemInfo:     statusInfo.memInfo,       // /proc/{pid}/status
-				MemInfoEx:   memInfoEx,                // /proc/{pid}/statm
-				CtxSwitches: statusInfo.ctxSwitches,   // /proc/{pid}/status
-				NumThreads:  statusInfo.numThreads,    // /proc/{pid}/status
-				IOStat:      ioInfo,                   // /proc/{pid}/io, requires permission checks
+				CreateTime:  statInfo.createTime,      // /proc/[pid]/stat
+				Nice:        statInfo.nice,            // /proc/[pid]/stat
+				OpenFdCount: p.getFDCount(pathForPID), // /proc/[pid]/fd, requires permission checks
+				CPUTime:     statInfo.cpuStat,         // /proc/[pid]/stat
+				MemInfo:     statusInfo.memInfo,       // /proc/[pid]/status
+				MemInfoEx:   memInfoEx,                // /proc/[pid]/statm
+				CtxSwitches: statusInfo.ctxSwitches,   // /proc/[pid]/status
+				NumThreads:  statusInfo.numThreads,    // /proc/[pid]/status
+				IOStat:      ioInfo,                   // /proc/[pid]/io, requires permission checks
 			},
 		}
 	}
