DataDog/datadog-agent

commit 3d8635424b48630e87943ce5ecc4f6bc52927435

Check ListenAndServe return status (#6998)
- Remove the nolint annotation and print the error if any.
- Log port on the error message

cmd/process-agent/main_common.go
@@ -194,7 +194,10 @@ func runAgent(exit chan struct{}) {
 		if ddconfig.Datadog.GetBool("telemetry.enabled") {
 			http.Handle("/telemetry", telemetry.Handler())
 		}
-		http.ListenAndServe(fmt.Sprintf("localhost:%d", cfg.ProcessExpVarPort), nil) //nolint:errcheck
+		err := http.ListenAndServe(fmt.Sprintf("localhost:%d", cfg.ProcessExpVarPort), nil)
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+		}
 	}()
 
 	cl, err := NewCollector(cfg)

cmd/agent/app/run.go
@@ -235,7 +235,12 @@ func StartAgent() error {
 	if config.Datadog.GetBool("telemetry.enabled") {
 		http.Handle("/telemetry", telemetry.Handler())
 	}
-	go http.ListenAndServe("127.0.0.1:"+port, http.DefaultServeMux) //nolint:errcheck
+	go func() {
+		err := http.ListenAndServe("127.0.0.1:"+port, http.DefaultServeMux)
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+		}
+	}()
 
 	// Setup healthcheck port
 	var healthPort = config.Datadog.GetInt("health_port")

cmd/cluster-agent/main.go
@@ -35,7 +35,13 @@ func main() {
 
 	// Expose the registered metrics via HTTP.
 	http.Handle("/metrics", telemetry.Handler())
-	go http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", config.Datadog.GetInt("metrics_port")), nil) //nolint:errcheck
+	go func() {
+		port := config.Datadog.GetInt("metrics_port")
+		err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), nil)
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+		}
+	}()
 
 	if err := app.ClusterAgentCmd.Execute(); err != nil {
 		log.Error(err)

cmd/dogstatsd/main_nix.go
@@ -24,9 +24,13 @@ func main() {
 	flavor.SetFlavor(flavor.Dogstatsd)
 
 	// go_expvar server
-	go http.ListenAndServe( //nolint:errcheck
-		fmt.Sprintf("127.0.0.1:%d", config.Datadog.GetInt("dogstatsd_stats_port")),
-		http.DefaultServeMux)
+	go func() {
+		port := config.Datadog.GetInt("dogstatsd_stats_port")
+		err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), http.DefaultServeMux)
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+		}
+	}()
 
 	if err := dogstatsdCmd.Execute(); err != nil {
 		log.Error(err)

cmd/dogstatsd/main_windows.go
@@ -69,9 +69,13 @@ func main() {
 	config.Datadog.AddConfigPath(DefaultConfPath)
 
 	// go_expvar server
-	go http.ListenAndServe(
-		fmt.Sprintf("127.0.0.1:%d", config.Datadog.GetInt("dogstatsd_stats_port")),
-		http.DefaultServeMux)
+	go func() {
+		port := config.Datadog.GetInt("dogstatsd_stats_port")
+		err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), http.DefaultServeMux)
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+		}
+	}()
 
 	isIntSess, err := svc.IsAnInteractiveSession()
 	if err != nil {

cmd/security-agent/app/app.go
@@ -213,7 +213,12 @@ func start(cmd *cobra.Command, args []string) error {
 	if coreconfig.Datadog.GetBool("telemetry.enabled") {
 		http.Handle("/telemetry", telemetry.Handler())
 	}
-	go http.ListenAndServe("127.0.0.1:"+port, http.DefaultServeMux) //nolint:errcheck
+	go func() {
+		err := http.ListenAndServe("127.0.0.1:"+port, http.DefaultServeMux)
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+		}
+	}()
 
 	// get hostname
 	// FIXME: use gRPC cross-agent communication API to retrieve hostname

cmd/serverless/main.go
@@ -111,9 +111,13 @@ func main() {
 	flavor.SetFlavor(flavor.ServerlessAgent)
 
 	// go_expvar server // TODO(remy): shouldn't we remove that for the serverless agent?
-	go http.ListenAndServe( //nolint:errcheck
-		fmt.Sprintf("127.0.0.1:%d", config.Datadog.GetInt("dogstatsd_stats_port")),
-		http.DefaultServeMux)
+	go func() {
+		port := config.Datadog.GetInt("dogstatsd_stats_port")
+		err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), http.DefaultServeMux)
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+		}
+	}()
 
 	// if not command has been provided, run the agent
 	if len(os.Args) == 1 {

commit 3238c08cdcff56c9466ae0a805ac09ce4585cf54

Fix build broken in #6998
There wasn't a `port` variable in this case

cmd/process-agent/main_common.go
@@ -196,7 +196,7 @@ func runAgent(exit chan struct{}) {
 		}
 		err := http.ListenAndServe(fmt.Sprintf("localhost:%d", cfg.ProcessExpVarPort), nil)
 		if err != nil && err != http.ErrServerClosed {
-			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+			log.Errorf("Error creating expvar server on port %v: %v", cfg.ProcessExpVarPort, err)
 		}
 	}()
 