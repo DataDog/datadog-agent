85b793b8a8944ef54cc0e3a2ac0c84582ded0b49
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2021-01-14 15:56:57 +0100

    Fix typo in omnibus project configs

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -64,7 +64,7 @@ package :deb do
   section 'utils'
   priority 'extra'
   if ENV.has_key?('DEB_SIGNING_PASSPHRASE') and not ENV['DEB_SIGNING_PASSPHRASE'].empty?
-    signing_passphrase "#{ENV['DEB_SIGNING_PASSHPRASE']}"
+    signing_passphrase "#{ENV['DEB_SIGNING_PASSPHRASE']}"
     if ENV.has_key?('DEB_GPG_KEY_NAME') and not ENV['DEB_GPG_KEY_NAME'].empty?
       gpg_key_name "#{ENV['DEB_GPG_KEY_NAME']}"
     end
