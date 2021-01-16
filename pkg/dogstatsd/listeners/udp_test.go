DataDog/datadog-agent

commit 0afb48b173d4d5f51249d4381bd7a94afacb64b8

Allow user to overwrite the socket read buffer size for dogstatsd

pkg/dogstatsd/listeners/udp.go
@@ -45,6 +45,12 @@ func NewUDPListener(packetOut chan *Packet, packetPool *PacketPool) (*UDPListene
 
 	conn, err = net.ListenPacket("udp", url)
 
+	if rcvbuf := config.Datadog.GetInt("dogstatsd_so_rcvbuf"); rcvbuf != 0 {
+		if err := conn.(*net.UDPConn).SetReadBuffer(rcvbuf); err != nil {
+			return nil, fmt.Errorf("could not set socket rcvbuf: %s", err)
+		}
+	}
+
 	if err != nil {
 		return nil, fmt.Errorf("can't listen: %s", err)
 	}

pkg/config/config.go
@@ -168,6 +168,7 @@ func init() {
 	Datadog.SetDefault("dogstatsd_stats_buffer", 10)
 	Datadog.SetDefault("dogstatsd_expiry_seconds", 300)
 	Datadog.SetDefault("dogstatsd_origin_detection", false) // Only supported for socket traffic
+	Datadog.SetDefault("dogstatsd_so_rcvbuf", 0)
 	Datadog.SetDefault("statsd_forward_host", "")
 	Datadog.SetDefault("statsd_forward_port", 0)
 	BindEnvAndSetDefault("statsd_metric_namespace", "")
@@ -264,6 +265,7 @@ func init() {
 	Datadog.BindEnv("dogstatsd_stats_port")
 	Datadog.BindEnv("dogstatsd_non_local_traffic")
 	Datadog.BindEnv("dogstatsd_origin_detection")
+	Datadog.BindEnv("dogstatsd_so_rcvbuf")
 	Datadog.BindEnv("check_runners")
 
 	Datadog.BindEnv("log_file")

pkg/config/config_template.yaml
@@ -182,6 +182,13 @@ api_key:
 # The port for the go_expvar server
 # dogstatsd_stats_port: 5000
 #
+# The number of bytes allocated to dogstatsd's socket receive buffer (POSIX
+# system only). By default, this value is set by the system. If you need to
+# increase the size of this buffer but keep the OS default value the same, you
+# can set dogstatsd's receive buffer size here. The maximum accepted value
+# might change depending on the OS.
+# dogstatsd_so_rcvbuf:
+#
 # If you want to forward every packet received by the dogstatsd server
 # to another statsd server, uncomment these lines.
 # WARNING: Make sure that forwarded packets are regular statsd packets and not "dogstatsd" packets,

pkg/dogstatsd/listeners/uds_common.go
@@ -63,6 +63,12 @@ func NewUDSListener(packetOut chan *Packet, packetPool *PacketPool) (*UDSListene
 		}
 	}
 
+	if rcvbuf := config.Datadog.GetInt("dogstatsd_so_rcvbuf"); rcvbuf != 0 {
+		if err := conn.SetReadBuffer(rcvbuf); err != nil {
+			return nil, fmt.Errorf("could not set socket rcvbuf: %s", err)
+		}
+	}
+
 	listener := &UDSListener{
 		OriginDetection: originDection,
 		packetOut:       packetOut,

releasenotes/notes/add-so_rcvbuf-to-dogstatsd-c4ba178747057dc4.yaml
@@ -1,0 +1,5 @@
+---
+features:
+  - |
+    Add an option to set the read buffer size for dogstatsd socker on POSIX
+    system (SO_RCVBUF).

commit 93696352451682dd715efeb535487c503d93cd26

dogstatsd: Fix error handling in ListenUDP()
Fix error handling in ListenUDP that was broken by commit 0afb48b, which
added socket read buffer updating before the error check:

  2020-11-19 02:33:54 UTC | CORE | INFO | (cmd/agent/app/run.go:360 in StopAgent) | See ya!
  panic: runtime error: invalid memory address or nil pointer dereference
  [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x2c8925c]

  goroutine 1 [running]:
  github.com/DataDog/datadog-agent/pkg/dogstatsd/listeners.NewUDPListener(0xc00098f380, 0xc0007d3e60, 0xe, 0x1fbd, 0x0)
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/dogstatsd/listeners/udp.go:68 +0x5fc
  github.com/DataDog/datadog-agent/pkg/dogstatsd.NewServer(0xc0008485a0, 0x3a1af38, 0xd, 0x1)
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/dogstatsd/server.go:158 +0x1100
  github.com/DataDog/datadog-agent/cmd/agent/app.StartAgent(0xc000000008, 0x3b17538)
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/app/run.go:275 +0x126e
  github.com/DataDog/datadog-agent/cmd/agent/app.run(0x5627f00, 0x580e2f0, 0x0, 0x0, 0x0, 0x0)
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/app/run.go:122 +0x1de
  github.com/spf13/cobra.(*Command).execute(0x5627f00, 0x580e2f0, 0x0, 0x0, 0x5627f00, 0x580e2f0)
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/vendor/github.com/spf13/cobra/command.go:842 +0x460
  github.com/spf13/cobra.(*Command).ExecuteC(0x56232e0, 0x0, 0x4f180, 0xc0000e2058)
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/vendor/github.com/spf13/cobra/command.go:950 +0x349
  github.com/spf13/cobra.(*Command).Execute(...)
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/vendor/github.com/spf13/cobra/command.go:887
  main.main()
  	/.omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/main.go:18 +0x2d

Fixes #6803

pkg/dogstatsd/listeners/udp.go
@@ -63,15 +63,14 @@ func NewUDPListener(packetOut chan Packets, sharedPacketPool *PacketPool) (*UDPL
 		return nil, fmt.Errorf("could not resolve udp addr: %s", err)
 	}
 	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("can't listen: %s", err)
+	}
 
 	if rcvbuf := config.Datadog.GetInt("dogstatsd_so_rcvbuf"); rcvbuf != 0 {
 		if err := conn.SetReadBuffer(rcvbuf); err != nil {
 			return nil, fmt.Errorf("could not set socket rcvbuf: %s", err)
 		}
-	}
-
-	if err != nil {
-		return nil, fmt.Errorf("can't listen: %s", err)
 	}
 
 	bufferSize := config.Datadog.GetInt("dogstatsd_buffer_size")

releasenotes/notes/dogstatsd-Fix-error-handling-in-ListenUDP-288a788984dd1750.yaml
@@ -1,0 +1,11 @@
+# Each section from every release note are combined when the
+# CHANGELOG.rst is rendered. So the text needs to be worded so that
+# it does not depend on any information only available in another
+# section. This may mean repeating some details, but each section
+# must be readable independently of the other.
+#
+# Each section note must be formatted as reStructuredText.
+---
+fixes:
+  - |
+    Fix agent panic when UDP port is busy and dogstatsd_so_rcvbuf is configured.

pkg/dogstatsd/listeners/udp_test.go
@@ -141,6 +141,23 @@ func TestUDPReceive(t *testing.T) {
 	}
 }
 
+// Reproducer for https://github.com/DataDog/datadog-agent/issues/6803
+func TestNewUDPListenerWhenBusyWithSoRcvBufSet(t *testing.T) {
+	port, err := getAvailableUDPPort()
+	assert.Nil(t, err)
+	address, _ := net.ResolveUDPAddr("udp", fmt.Sprintf("127.0.0.1:%d", port))
+	conn, err := net.ListenUDP("udp", address)
+	assert.NotNil(t, conn)
+	assert.Nil(t, err)
+	defer conn.Close()
+	config.Datadog.SetDefault("dogstatsd_so_rcvbuf", 1)
+	config.Datadog.SetDefault("dogstatsd_port", port)
+	config.Datadog.SetDefault("dogstatsd_non_local_traffic", false)
+	s, err := NewUDPListener(nil, packetPoolUDP)
+	assert.Nil(t, s)
+	assert.NotNil(t, err)
+}
+
 // getAvailableUDPPort requests a random port number and makes sure it is available
 func getAvailableUDPPort() (int, error) {
 	conn, err := net.ListenPacket("udp", ":0")