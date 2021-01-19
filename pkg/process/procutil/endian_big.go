ca75d93c415ee42fdea9a390468b8bafcd13e1ce
Shang Wang <shang.wang@datadoghq.com>
2021-01-19 17:10:32 -0500

    update build tags and add comment

--- pkg/process/procutil/endian_big.go
+++ pkg/process/procutil/endian_big.go
@@ -1,4 +1,8 @@
-// +build ppc64 s390x mips mips64
+// this is copied from https://golang.org/src/syscall/endian_little.go,
+// so far procutil is used only for linux so we have "linux" build tag for each architecture,
+// otherwise the compilation on some platforms will get "`isBigEndian` is unused (deadcode)" error
+
+// +build linux,ppc64 linux,s390x linux,mips linux,mips64
 
 package procutil
 
--- pkg/process/procutil/endian_little.go
+++ pkg/process/procutil/endian_little.go
@@ -1,4 +1,8 @@
-// +build 386 amd64 arm arm64 ppc64le mips64le mipsle riscv64 wasm
+// this is copied from https://golang.org/src/syscall/endian_little.go,
+// so far procutil is used only for linux so we have "linux" build tag for each architecture,
+// otherwise the compilation on some platforms will get "`isBigEndian` is unused (deadcode)" error
+
+// +build linux,386 linux,amd64 linux,arm linux,arm64 linux,ppc64le linux,mips64le linux,mipsle linux,riscv64 linux,wasm
 
 package procutil
 
