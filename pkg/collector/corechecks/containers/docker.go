9dcabee83b8bf200480125755e57c3a0d80cd654
Pierre Rognant <pierre.rognant@datadoghq.com>
2021-01-21 11:27:01 +0100

    dep cleanup

--- pkg/collector/corechecks/containers/docker.go
+++ pkg/collector/corechecks/containers/docker.go
@@ -14,8 +14,6 @@ import (
 	"strings"
 	"time"
 
-	yaml "gopkg.in/yaml.v2"
-
 	"github.com/DataDog/datadog-agent/pkg/aggregator"
 	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
 	"github.com/DataDog/datadog-agent/pkg/collector/check"
