e186d32c903cda3a2286862239467e2653e9fc0c
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-01-26 11:28:52 +0100

    Add comment for getColumnValues

--- pkg/collector/corechecks/snmp/values.go
+++ pkg/collector/corechecks/snmp/values.go
@@ -19,6 +19,10 @@ func (v *resultValueStore) getScalarValue(oid string) (snmpValueType, error) {
 	return value, nil
 }
 
+// getColumnValues look for oid in resultValueStore and returns a map[<fullIndex>]snmpValueType
+// where `fullIndex` refer to the entire index part of the instance OID.
+// For example if the row oid (instance oid) is `1.3.6.1.4.1.1.2.3.10.11.12`,
+// the column oid is `1.3.6.1.4.1.1.2.3`, the fullIndex is `10.11.12`.
 func (v *resultValueStore) getColumnValues(oid string) (map[string]snmpValueType, error) {
 	values, ok := v.columnValues[oid]
 	if !ok {
