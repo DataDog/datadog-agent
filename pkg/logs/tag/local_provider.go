f3c978327f0d3058c33c318efd022c5738e53429
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-02-01 16:35:43 +0100

    [logs] noopprovider is now local provider (#7316)
    * [logs] noopprovider is now local provider
    
    * [logs] remove unwanted logging
    
    * [logs] update copyrights
    
    * [logs][test] fixing docker tailer_test

---
+++ pkg/logs/tag/local_provider.go
@@ -0,0 +1,54 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2021 Datadog, Inc.
+
+package tag
+
+import (
+	"sync"
+	"time"
+
+	coreConfig "github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/logs/config"
+	"github.com/DataDog/datadog-agent/pkg/metadata/host"
+)
+
+type localProvider struct {
+	tags                 []string
+	expectedTags         []string
+	expectedTagsDeadline time.Time
+	sync.RWMutex
+}
+
+// NewLocalProvider returns a new local Provider.
+func NewLocalProvider(t []string) Provider {
+	p := &localProvider{
+		tags: t,
+	}
+
+	if config.IsExpectedTagsSet() {
+		p.expectedTags = append(p.tags, host.GetHostTags(true).System...)
+		p.expectedTagsDeadline = coreConfig.StartTime.Add(coreConfig.Datadog.GetDuration("logs_config.expected_tags_duration"))
+
+		// reset submitExpectedTags after deadline elapsed
+		go func() {
+			<-time.After(time.Until(p.expectedTagsDeadline))
+
+			p.Lock()
+			defer p.Unlock()
+			p.expectedTags = p.tags
+		}()
+	}
+
+	return p
+}
+
+// GetTags returns an empty list of tags.
+func (p *localProvider) GetTags() []string {
+
+	p.RLock()
+	defer p.RUnlock()
+
+	return p.expectedTags
+}
--- pkg/logs/tag/provider_test.go
+++ pkg/logs/tag/provider_test.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-2020 Datadog, Inc.
+// Copyright 2016-2021 Datadog, Inc.
 
 package tag
 
@@ -17,21 +17,30 @@ import (
 func TestProviderExpectedTags(t *testing.T) {
 	mockConfig := config.Mock()
 
+	startTime := config.StartTime
+	config.StartTime = time.Now()
+	defer func() {
+		config.StartTime = startTime
+	}()
+
 	tags := []string{"tag1:value1", "tag2", "tag3"}
 
 	mockConfig.Set("tags", tags)
+	defer mockConfig.Set("tags", nil)
+
 	// Setting a test-friendly value for the deadline
 	mockConfig.Set("logs_config.expected_tags_duration", "5s")
-	defer mockConfig.Set("tags", nil)
-	defer mockConfig.Set("expected_tags_duration", 0)
+	defer mockConfig.Set("logs_config.expected_tags_duration", 0)
 
 	p := NewProvider("foo")
 	pp := p.(*provider)
 
 	// Is provider expected?
 	d := mockConfig.GetDuration("logs_config.expected_tags_duration")
-	assert.InDelta(t, time.Now().Add(d).Unix(), pp.expectedTagsDeadline.Unix(), 1)
-	assert.True(t, pp.submitExpectedTags)
+	l := pp.localTagProvider
+	ll := l.(*localProvider)
+
+	assert.InDelta(t, config.StartTime.Add(d).Unix(), ll.expectedTagsDeadline.Unix(), 1)
 
 	tt := pp.GetTags()
 	sort.Strings(tags)
@@ -39,11 +48,7 @@ func TestProviderExpectedTags(t *testing.T) {
 	assert.Equal(t, tags, tt)
 
 	// let the deadline expire + a little grace period
-	<-time.After(time.Until(pp.expectedTagsDeadline.Add(2 * time.Second)))
+	<-time.After(time.Until(ll.expectedTagsDeadline.Add(2 * time.Second)))
 
-	pp.Lock()
-	assert.False(t, pp.submitExpectedTags)
-	pp.Unlock()
 	assert.Equal(t, []string{}, pp.GetTags())
-
 }
