f08e284ed0e525071fa52a18701eef1121144005
Julien Lebot <julien.ar.lebot@gmail.com>
2021-02-05 15:38:56 +0100

    Copy unit test binary to output and run it

--- tasks/winbuildscripts/unittests.ps1
+++ tasks/winbuildscripts/unittests.ps1
@@ -73,3 +73,11 @@ if($err -ne 0){
     Write-Host -ForegroundColor Red "custom action test failed $err"
     [Environment]::Exit($err)
 }
+
+& $Env:BUILD_ROOT\bin\agent\customaction-tests.exe
+$err = $LASTEXITCODE
+Write-Host Test result is $err
+if($err -ne 0){
+    Write-Host -ForegroundColor Red "test failed $err"
+    [Environment]::Exit($err)
+}
