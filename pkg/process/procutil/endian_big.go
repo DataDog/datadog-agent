633dacf27a5c83415c7c07c4a44b39347bc4d06c
Shang Wang <shang.wang@datadoghq.com>
2021-02-05 16:23:31 -0500

    add copyright

--- pkg/process/procutil/endian_big.go
+++ pkg/process/procutil/endian_big.go
@@ -1,6 +1,6 @@
-// this is copied from https://golang.org/src/syscall/endian_little.go,
-// so far procutil is used only for linux so we have "linux" build tag for each architecture,
-// otherwise the compilation on some platforms will get "`isBigEndian` is unused (deadcode)" error
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 // +build linux,ppc64 linux,s390x linux,mips linux,mips64
 
--- pkg/process/procutil/endian_little.go
+++ pkg/process/procutil/endian_little.go
@@ -1,6 +1,6 @@
-// this is copied from https://golang.org/src/syscall/endian_little.go,
-// so far procutil is used only for linux so we have "linux" build tag for each architecture,
-// otherwise the compilation on some platforms will get "`isBigEndian` is unused (deadcode)" error
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 // +build linux,386 linux,amd64 linux,arm linux,arm64 linux,ppc64le linux,mips64le linux,mipsle linux,riscv64 linux,wasm
 
