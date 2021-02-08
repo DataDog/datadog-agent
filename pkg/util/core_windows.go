cb0f99fb364f2ffdc44d18338cb14a2d485d2204
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-02-08 17:38:28 +0100

    Add error message on Windows

--- pkg/util/core_windows.go
+++ pkg/util/core_windows.go
@@ -5,7 +5,9 @@
 
 package util
 
+import "fmt"
+
 // SetupCoreDump enables core dumps and sets the core dump size limit based on configuration
 func SetupCoredump() error {
-	return nil
+	return fmt.Errorf("Not supported on Windows")
 }
