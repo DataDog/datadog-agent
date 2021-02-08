6705f31f559df17c1bd79f9875d3d44b86d534bc
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-02-08 17:57:34 +0100

    Fix typo

--- pkg/util/core_windows.go
+++ pkg/util/core_windows.go
@@ -8,6 +8,6 @@ package util
 import "fmt"
 
 // SetupCoreDump enables core dumps and sets the core dump size limit based on configuration
-func SetupCoredump() error {
+func SetupCoreDump() error {
 	return fmt.Errorf("Not supported on Windows")
 }
