3200f2e0d075594400d7030582e1decaafc43936
Sylvain Baubeau <lebauce@gmail.com>
2021-02-08 16:48:35 +0100

    [runtime-security] allow building functional tests in static mode (#7317)
    * Allow using pkg/ebpf without linking against llvm
    
    * Allow building tests in static mode

--- pkg/ebpf/bytecode/runtime/runtime-security.go
+++ pkg/ebpf/bytecode/runtime/runtime-security.go
@@ -3,8 +3,4 @@
 
 package runtime
 
-import (
-	"github.com/DataDog/datadog-agent/pkg/ebpf"
-)
-
-var RuntimeSecurity = ebpf.NewRuntimeAsset("runtime-security.c", "5ac39a4d437faa3d983b245fea60200f229cf3a750e68c314bf265c07ceac6fb")
+var RuntimeSecurity = NewRuntimeAsset("runtime-security.c", "5ac39a4d437faa3d983b245fea60200f229cf3a750e68c314bf265c07ceac6fb")
