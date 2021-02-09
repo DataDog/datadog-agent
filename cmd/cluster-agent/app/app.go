8eee7f164962897ba4f8f979775e0d8ad5adb6f6
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-02-09 15:02:24 +0100

    Add option to cluster agent

--- cmd/cluster-agent/app/app.go
+++ cmd/cluster-agent/app/app.go
@@ -142,6 +142,11 @@ func start(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
+	// setup core dump as soon as possible
+	if err := util.SetupCoreDump(); err != nil {
+		log.Infof("Can't setup core dumps: %v, core dumps might not be available after a crash", err)
+	}
+
 	if !config.Datadog.IsSet("api_key") {
 		log.Critical("no API key configured, exiting")
 		return nil
