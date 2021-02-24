543111af0a9e930fcc1c6466bf0ae43ddbd7779b
Shang Wang <shang.wang@datadoghq.com>
2021-02-24 15:42:49 -0500

    update test

--- pkg/process/encoding/encoding_test.go
+++ pkg/process/encoding/encoding_test.go
@@ -11,48 +11,56 @@ import (
 )
 
 func TestSerialization(t *testing.T) {
-	origin := map[int32]*procutil.StatsWithPerm{
-		1: {
-			OpenFdCount: 1,
-			IOStat: &procutil.IOCountersStat{
-				ReadCount:  1,
-				WriteCount: 2,
-				ReadBytes:  3,
-				WriteBytes: 4,
+	tcs := []struct {
+		origin map[int32]*procutil.StatsWithPerm
+		exp    *model.ProcStatsWithPermByPID
+	}{
+		{
+			origin: map[int32]*procutil.StatsWithPerm{
+				1: {
+					OpenFdCount: 1,
+					IOStat: &procutil.IOCountersStat{
+						ReadCount:  1,
+						WriteCount: 2,
+						ReadBytes:  3,
+						WriteBytes: 4,
+					},
+				},
+			},
+			exp: &model.ProcStatsWithPermByPID{
+				StatsByPID: map[int32]*model.ProcStatsWithPerm{
+					1: {
+						OpenFDCount: 1,
+						ReadCount:   1,
+						WriteCount:  2,
+						ReadBytes:   3,
+						WriteBytes:  4,
+					},
+				},
 			},
 		},
-	}
-	exp := &model.ProcStatsWithPermByPID{
-		StatsByPID: map[int32]*model.ProcStatsWithPerm{
-			1: {
-				OpenFDCount: 1,
-				ReadCount:   1,
-				WriteCount:  2,
-				ReadBytes:   3,
-				WriteBytes:  4,
+		{
+			origin: map[int32]*procutil.StatsWithPerm{
+				1: {
+					OpenFdCount: 2,
+					IOStat: &procutil.IOCountersStat{
+						ReadCount:  4,
+						WriteCount: 2,
+						ReadBytes:  5,
+						WriteBytes: 8,
+					},
+				},
 			},
-		},
-	}
-
-	origin2 := map[int32]*procutil.StatsWithPerm{
-		1: {
-			OpenFdCount: 2,
-			IOStat: &procutil.IOCountersStat{
-				ReadCount:  4,
-				WriteCount: 2,
-				ReadBytes:  5,
-				WriteBytes: 8,
-			},
-		},
-	}
-	exp2 := &model.ProcStatsWithPermByPID{
-		StatsByPID: map[int32]*model.ProcStatsWithPerm{
-			1: {
-				OpenFDCount: 2,
-				ReadCount:   4,
-				WriteCount:  2,
-				ReadBytes:   5,
-				WriteBytes:  8,
+			exp: &model.ProcStatsWithPermByPID{
+				StatsByPID: map[int32]*model.ProcStatsWithPerm{
+					1: {
+						OpenFDCount: 2,
+						ReadCount:   4,
+						WriteCount:  2,
+						ReadBytes:   5,
+						WriteBytes:  8,
+					},
+				},
 			},
 		},
 	}
@@ -60,50 +68,59 @@ func TestSerialization(t *testing.T) {
 	t.Run("requesting application/json serialization", func(t *testing.T) {
 		marshaler := GetMarshaler("application/json")
 		assert.Equal(t, "application/json", marshaler.ContentType())
-		blob, err := marshaler.Marshal(origin)
-		require.NoError(t, err)
-
 		unmarshaler := GetUnmarshaler("application/json")
-		result, err := unmarshaler.Unmarshal(blob)
-		require.NoError(t, err)
-		assert.Equal(t, exp, result)
+
+		for _, tc := range tcs {
+			blob, err := marshaler.Marshal(tc.origin)
+			require.NoError(t, err)
+
+			result, err := unmarshaler.Unmarshal(blob)
+			require.NoError(t, err)
+			assert.Equal(t, tc.exp, result)
+		}
 	})
 
 	t.Run("requesting empty marshaler name serialization", func(t *testing.T) {
 		marshaler := GetMarshaler("")
 		// in case we request empty serialization type, default to application/json
 		assert.Equal(t, "application/json", marshaler.ContentType())
-		blob, err := marshaler.Marshal(origin2)
-		require.NoError(t, err)
-
 		unmarshaler := GetUnmarshaler("application/json")
-		result, err := unmarshaler.Unmarshal(blob)
-		require.NoError(t, err)
-		assert.Equal(t, exp2, result)
+
+		for _, tc := range tcs {
+			blob, err := marshaler.Marshal(tc.origin)
+			require.NoError(t, err)
+
+			result, err := unmarshaler.Unmarshal(blob)
+			require.NoError(t, err)
+			assert.Equal(t, tc.exp, result)
+		}
 	})
 
 	t.Run("requesting application/protobuf serialization", func(t *testing.T) {
 		marshaler := GetMarshaler("application/protobuf")
 		assert.Equal(t, "application/protobuf", marshaler.ContentType())
-
-		blob, err := marshaler.Marshal(origin)
-		require.NoError(t, err)
-
 		unmarshaler := GetUnmarshaler("application/protobuf")
-		result, err := unmarshaler.Unmarshal(blob)
-		require.NoError(t, err)
-		assert.Equal(t, exp, result)
+
+		for _, tc := range tcs {
+			blob, err := marshaler.Marshal(tc.origin)
+			require.NoError(t, err)
+
+			result, err := unmarshaler.Unmarshal(blob)
+			require.NoError(t, err)
+			assert.Equal(t, tc.exp, result)
+		}
+
 	})
 
 	t.Run("protobuf serializing empty input", func(t *testing.T) {
 		marshaler := GetMarshaler("application/protobuf")
 		assert.Equal(t, "application/protobuf", marshaler.ContentType())
+		unmarshaler := GetUnmarshaler("application/protobuf")
 
 		var empty map[int32]*procutil.StatsWithPerm
 		blob, err := marshaler.Marshal(empty)
 		require.NoError(t, err)
 
-		unmarshaler := GetUnmarshaler("application/protobuf")
 		result, err := unmarshaler.Unmarshal(blob)
 		require.NoError(t, err)
 		assert.EqualValues(t, &model.ProcStatsWithPermByPID{}, result)
