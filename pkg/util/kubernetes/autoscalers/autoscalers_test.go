24347f970a49596959a00d63397d4fb6090cea22
Florent Delannoy <florent.delannoy@gmail.com>
2021-02-26 16:27:21 +0000

    [cluster-agent] Validate external metrics
    Add a validation step before accepting metrics set in HPAs.
    
    This ensures that no obviously-broken metric is accepted and goes on to
    break the whole metrics gathering process.
    
    Fixes #5282
    Replaces #5305

--- pkg/util/kubernetes/autoscalers/autoscalers_test.go
+++ pkg/util/kubernetes/autoscalers/autoscalers_test.go
@@ -329,6 +329,72 @@ func TestInspect(t *testing.T) {
 			},
 			[]custommetrics.ExternalMetricValue{},
 		},
+		"skip invalid metric names": {
+			&autoscalingv2.HorizontalPodAutoscaler{
+				Spec: autoscalingv2.HorizontalPodAutoscalerSpec{
+					Metrics: []autoscalingv2.MetricSpec{
+						{
+							Type: autoscalingv2.ExternalMetricSourceType,
+							External: &autoscalingv2.ExternalMetricSource{
+								MetricName: "valid_name",
+							},
+						},
+						{
+							Type: autoscalingv2.ExternalMetricSourceType,
+							External: &autoscalingv2.ExternalMetricSource{
+								MetricName: "valid_name.with_dots.and_underscores.AndUppercaseLetters",
+							},
+						},
+						{
+							Type: autoscalingv2.ExternalMetricSourceType,
+							External: &autoscalingv2.ExternalMetricSource{
+								MetricName: "0_invalid_name_must_start_with_letter",
+							},
+						},
+						{
+							Type: autoscalingv2.ExternalMetricSourceType,
+							External: &autoscalingv2.ExternalMetricSource{
+								MetricName: "spaces are invalid",
+							},
+						},
+						{
+							Type: autoscalingv2.ExternalMetricSourceType,
+							External: &autoscalingv2.ExternalMetricSource{
+								MetricName: "utf8_invalid_🤷",
+							},
+						},
+						{
+							Type: autoscalingv2.ExternalMetricSourceType,
+							External: &autoscalingv2.ExternalMetricSource{
+								MetricName: "over_200_characters_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding_padding",
+							},
+						},
+					},
+				},
+			},
+			[]custommetrics.ExternalMetricValue{
+				{
+					MetricName: "valid_name",
+					Ref: custommetrics.ObjectReference{
+						Type: "horizontal",
+					},
+					Labels:    nil,
+					Timestamp: 0,
+					Value:     0,
+					Valid:     false,
+				},
+				{
+					MetricName: "valid_name.with_dots.and_underscores.AndUppercaseLetters",
+					Ref: custommetrics.ObjectReference{
+						Type: "horizontal",
+					},
+					Labels:    nil,
+					Timestamp: 0,
+					Value:     0,
+					Valid:     false,
+				},
+			},
+		},
 		"upper cases handled": {
 			&autoscalingv2.HorizontalPodAutoscaler{
 				Spec: autoscalingv2.HorizontalPodAutoscalerSpec{
