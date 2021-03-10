296c9275c1931be6b55a03dfaf41960ecbddd876
Lénaïc Huard <lenaic.huard@datadoghq.com>
2021-03-10 15:32:48 +0100

    Use `/debug/stats` instead of `/status` to check `system-probe` connectivity

--- cmd/system-probe/main.go
+++ cmd/system-probe/main.go
@@ -143,8 +143,6 @@ func runAgent(exit <-chan struct{}) {
 		cleanupAndExit(1)
 	}
 
-	httpMux.HandleFunc("/status", func(w http.ResponseWriter, req *http.Request) {})
-
 	// Register stats endpoint
 	httpMux.HandleFunc("/debug/stats", func(w http.ResponseWriter, req *http.Request) {
 		stats := loader.GetStats()
