b2e8fb40a261d37aae1d1c3ca1bb2ce5c879984c
vishal kumar <vishal.kumar@xebia.com>
2021-03-19 14:46:24 +0530

    fix swarm molecule

--- test/molecule-role/molecule/swarm/tests/test_docker_swarm_integration.py
+++ test/molecule-role/molecule/swarm/tests/test_docker_swarm_integration.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 
 import testinfra.utils.ansible_runner
 
@@ -17,7 +18,6 @@ def test_hosts_file(host):
 
 
 def test_docker_swarm_metrics(host):
-    hostname = host.ansible.get_variables()["inventory_hostname"]
     url = "http://localhost:7070/api/topic/sts_multi_metrics?limit=1000"
 
     def wait_for_metrics():
@@ -40,3 +40,103 @@ def test_docker_swarm_metrics(host):
         assert all([expectedMetric for expectedMetric in expected if expectedMetric in get_keys()])
 
     util.wait_until(wait_for_metrics, 180, 3)
+
+
+def test_docker_swarm_topology(host):
+
+    def assert_topology():
+        topo_url = "http://localhost:7070/api/topic/sts_topo_docker-swarm_agents?limit=1500"
+        data = host.check_output('curl "{}"'.format(topo_url))
+        json_data = json.loads(data)
+        with open("./topic-agent-integration-sample-topo-agent-integrations.json", 'w') as f:
+            json.dump(json_data, f, indent=4)
+
+        components = [
+            {
+                "assertion": "Should find the nginx swarm service component",
+                "type": "swarm-service",
+                "external_id": lambda e_id: re.compile(
+                    r"urn:swarm-service:/.*\..*\..*\..*").findall(e_id),
+                "data": lambda d: (
+                    d["name"] == "nginx" and
+                    d["image"].startsWith("nginx:latest@") and
+                    "spec" in d and
+                    "global" in d["spec"]["Mode"]
+                )
+            },
+            {
+                "assertion": "Should find the agent swarm service component",
+                "type": "swarm-service",
+                "external_id": lambda e_id: re.compile(
+                    r"urn:swarm-service:/.*\..*\..*\..*").findall(e_id),
+                "data": lambda d: (
+                    d["name"] == "agent_stackstate-agent" and
+                    d["image"].startsWith("stackstate/stackstate-agent-2-test:{}@".format(os.environ['STACKSTATE_BRANCH'])) and
+                    "spec" in d and
+                    "Replicated" in d["spec"]["Mode"] and
+                    d["spec"]["Mode"]["Replicated"]["Replicas"] == 1
+                )
+            },
+            {
+                "assertion": "Should find the receiver service component",
+                "type": "swarm-service",
+                "external_id": lambda e_id: re.compile(
+                    r"urn:swarm-service:/.*\..*\..*\..*").findall(e_id),
+                "data": lambda d: (
+                    d["name"] == "agent_receiver" and
+                    d["image"].startsWith("stackstate/stackstate-receiver:{}@".format(os.environ['STACKSTATE_BRANCH'])) and
+                    "spec" in d and
+                    "Replicated" in d["spec"]["Mode"] and
+                    d["spec"]["Mode"]["Replicated"]["Replicas"] == 1
+                )
+            },
+            {
+                "assertion": "Should find the topic-api swarm service component",
+                "type": "swarm-service",
+                "external_id": lambda e_id: re.compile(
+                    r"urn:swarm-service:/.*\..*\..*\..*").findall(e_id),
+                "data": lambda d: (
+                    d["name"] == "agent_topic-api" and
+                    d["image"].startsWith("stackstate/stackstate-topic-api:{}@".format(os.environ['STACKSTATE_BRANCH'])) and
+                    "spec" in d and
+                    "Replicated" in d["spec"]["Mode"] and
+                    d["spec"]["Mode"]["Replicated"]["Replicas"] == 1
+                )
+            },
+            {
+                "assertion": "Should find the kafka swarm service component",
+                "type": "swarm-service",
+                "external_id": lambda e_id: re.compile(
+                    r"urn:swarm-service:/.*\..*\..*\..*").findall(e_id),
+                "data": lambda d: (
+                    d["name"] == "agent_kafka" and
+                    d["image"].startsWith("wurstmeister/kafka:2.12-2.3.1@") and
+                    "spec" in d and
+                    "Replicated" in d["spec"]["Mode"] and
+                    d["spec"]["Mode"]["Replicated"]["Replicas"] == 1
+                )
+            },
+            {
+                "assertion": "Should find the zookeeper swarm service component",
+                "type": "swarm-service",
+                "external_id": lambda e_id: re.compile(
+                    r"urn:swarm-service:/.*\..*\..*\..*").findall(e_id),
+                "data": lambda d: (
+                    d["name"] == "agent_zookeeper" and
+                    d["image"].startsWith("wurstmeister/zookeeper:latest@") and
+                    "spec" in d and
+                    "Replicated" in d["spec"]["Mode"] and
+                    d["spec"]["Mode"]["Replicated"]["Replicas"] == 1
+                )
+            }
+        ]
+        for c in components:
+            print("Running assertion for: " + c["assertion"])
+            assert util.component_data(
+                json_data=json_data,
+                type_name=c["type"],
+                external_id_assert_fn=c["external_id"],
+                data_assert_fn=c["data"],
+            ) is not None
+
+    util.wait_until(assert_topology, 30, 3)
