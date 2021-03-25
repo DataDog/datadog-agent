01c42a0d6572d97b9b818bd1c0eb4ff5427cd029
Julien Lebot <julien.lebot@datadoghq.com>
2021-03-25 15:44:31 +0100

    Add ec2_use_windows_prefix_detection (#7598)

--- tools/windows/install-help/cal/customaction-tests/ReplaceYamlProperties_Tests.cpp
+++ tools/windows/install-help/cal/customaction-tests/ReplaceYamlProperties_Tests.cpp
@@ -19,8 +19,7 @@ TEST_F(ReplaceYamlPropertiesTests, When_APIKEY_Present_Replace_It)
 {
     value_map values = {{L"APIKEY", L"1234567890"}};
     std::vector<std::wstring> failedToReplace;
-    std::wstring result = replace_yaml_properties(
-LR"(
+    std::wstring result = replace_yaml_properties(LR"(
 ## @param api_key - string - required
 ## The Datadog API key to associate your Agent's data with your organization.
 ## Create a new API key here: https://app.datadoghq.com/account/settings
@@ -28,8 +27,7 @@ LR"(
 api_key:)",
                                                   propertyRetriever(values), &failedToReplace);
 
-    EXPECT_EQ(result,
-LR"(
+    EXPECT_EQ(result, LR"(
 ## @param api_key - string - required
 ## The Datadog API key to associate your Agent's data with your organization.
 ## Create a new API key here: https://app.datadoghq.com/account/settings
@@ -54,4 +52,27 @@ random_prop: true
 )");
     EXPECT_EQ(failedToReplace.size(), 1);
     EXPECT_STREQ(failedToReplace[0].c_str(), L"APIKEY");
+}
+
+TEST_F(ReplaceYamlPropertiesTests, When_EC2_USE_WINDOWS_PREFIX_DETECTION_Add_It)
+{
+    value_map values = {{L"EC2_USE_WINDOWS_PREFIX_DETECTION", L"true"}};
+    std::wstring result = replace_yaml_properties(LR"()", propertyRetriever(values));
+
+    EXPECT_EQ(result, LR"(
+ec2_use_windows_prefix_detection: true
+)");
+}
+
+TEST_F(ReplaceYamlPropertiesTests, When_EC2_USE_WINDOWS_PREFIX_DETECTION_Already_Exists_Dont_Duplicate_it)
+{
+    value_map values = {{L"EC2_USE_WINDOWS_PREFIX_DETECTION", L"true"}};
+    std::wstring result = replace_yaml_properties(LR"(
+ec2_use_windows_prefix_detection: false
+)",
+                                                  propertyRetriever(values));
+
+    EXPECT_EQ(result, LR"(
+ec2_use_windows_prefix_detection: true
+)");
 }
