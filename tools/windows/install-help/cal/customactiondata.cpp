038a5dc74a5ecfe5708e36ca8d739b6b3808e400
Julien Lebot <julien.lebot@datadoghq.com>
2021-03-26 17:30:44 +0100

    Parse MSI properties without needing a semicolon (#7601)

--- tools/windows/install-help/cal/customactiondata.cpp
+++ tools/windows/install-help/cal/customactiondata.cpp
@@ -34,24 +34,25 @@ bool CustomActionData::init(const std::wstring &data)
         return false;
     }
 
-    auto start = data.begin();
-    auto end = data.end();
-    std::wregex re(L"((\\w+)=(.+)?;\\s*\r?\n)");
-    std::match_results<decltype(start)> results;
-    while (std::regex_search(start, end, results, re))
-    {
-        auto propertyValue = results[3].str();
-        propertyValue.erase(propertyValue.begin(), std::find_if(propertyValue.begin(), propertyValue.end(), [](int ch)
-        {
-            return !std::isspace(ch);
-        }));
-        if (propertyValue.length() > 0)
-        {
-            values[results[2]] = propertyValue;
-        }
-        start += results.position() + results.length();
-    }
+    std::wstringstream ss(data);
+    std::wstring token;
 
+    while (std::getline(ss, token))
+    {
+        // 'token' contains "<key>=<value>"
+        std::wstringstream instream(token);
+        std::wstring key, val;
+        if (std::getline(instream, key, L'='))
+        {
+            trim_string(key);
+            std::getline(instream, val);
+            trim_string(val);
+            if (!key.empty() && !val.empty())
+            {
+                this->values[key] = val;
+            }
+        }
+    }
     return parseUsernameData() && parseSysprobeData();
 }
 
