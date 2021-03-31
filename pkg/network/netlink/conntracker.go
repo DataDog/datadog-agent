de320c6ed7889b8490171b3665758b811b3bdacd
Hasan Mahmood <hmahmood@users.noreply.github.com>
2021-03-31 14:12:06 -0500

    [system-probe] Count conntrack cache registers correctly (#7774)

--- pkg/network/netlink/conntracker.go
+++ pkg/network/netlink/conntracker.go
@@ -72,6 +72,7 @@ type realConntracker struct {
 		registersTotalTime   int64
 		unregisters          int64
 		unregistersTotalTime int64
+		evicts               int64
 	}
 }
 
@@ -160,19 +161,28 @@ func (ctr *realConntracker) GetStats() map[string]int64 {
 		"orphan_size": int64(orphanSize),
 	}
 
-	if ctr.stats.gets != 0 {
-		m["gets_total"] = ctr.stats.gets
-		m["nanoseconds_per_get"] = ctr.stats.getTimeTotal / ctr.stats.gets
+	gets := atomic.LoadInt64(&ctr.stats.gets)
+	if gets != 0 {
+		getTimeTotal := atomic.LoadInt64(&ctr.stats.getTimeTotal)
+		m["gets_total"] = gets
+		m["nanoseconds_per_get"] = getTimeTotal / gets
 	}
-	if ctr.stats.registers != 0 {
-		m["registers_total"] = ctr.stats.registers
-		m["registers_dropped"] = ctr.stats.registersDropped
-		m["nanoseconds_per_register"] = ctr.stats.registersTotalTime / ctr.stats.registers
+
+	registers := atomic.LoadInt64(&ctr.stats.registers)
+	if registers != 0 {
+		registersTotalTime := atomic.LoadInt64(&ctr.stats.registersTotalTime)
+		m["registers_total"] = registers
+		m["nanoseconds_per_register"] = registersTotalTime / registers
 	}
-	if ctr.stats.unregisters != 0 {
-		m["unregisters_total"] = ctr.stats.unregisters
-		m["nanoseconds_per_unregister"] = ctr.stats.unregistersTotalTime / ctr.stats.unregisters
+	m["registers_dropped"] = atomic.LoadInt64(&ctr.stats.registersDropped)
+
+	unregisters := atomic.LoadInt64(&ctr.stats.unregisters)
+	if unregisters != 0 {
+		unregisterTotalTime := atomic.LoadInt64(&ctr.stats.unregistersTotalTime)
+		m["unregisters_total"] = unregisters
+		m["nanoseconds_per_unregister"] = unregisterTotalTime / unregisters
 	}
+	m["evicts_total"] = atomic.LoadInt64(&ctr.stats.evicts)
 
 	// Merge telemetry from the consumer
 	for k, v := range ctr.consumer.GetStats() {
@@ -217,7 +227,9 @@ func (ctr *realConntracker) loadInitialState(events <-chan Event) {
 				continue
 			}
 
-			ctr.cache.Add(c, false)
+			evicts := ctr.cache.Add(c, false)
+			atomic.AddInt64(&ctr.stats.registers, 1)
+			atomic.AddInt64(&ctr.stats.evicts, int64(evicts))
 		}
 	}
 }
@@ -232,15 +244,15 @@ func (ctr *realConntracker) register(c Con) int {
 	}
 
 	then := time.Now().UnixNano()
-	defer func() {
-		atomic.AddInt64(&ctr.stats.registers, 2)
-		atomic.AddInt64(&ctr.stats.registersTotalTime, time.Now().UnixNano()-then)
-	}()
 
 	ctr.Lock()
 	defer ctr.Unlock()
 
-	ctr.cache.Add(c, true)
+	evicts := ctr.cache.Add(c, true)
+
+	atomic.AddInt64(&ctr.stats.registers, 1)
+	atomic.AddInt64(&ctr.stats.evicts, int64(evicts))
+	atomic.AddInt64(&ctr.stats.registersTotalTime, time.Now().UnixNano()-then)
 
 	return 0
 }
@@ -323,7 +335,7 @@ func (cc *conntrackCache) Remove(k connKey) bool {
 	return cc.cache.Remove(k)
 }
 
-func (cc *conntrackCache) Add(c Con, orphan bool) {
+func (cc *conntrackCache) Add(c Con, orphan bool) (evicts int) {
 	registerTuple := func(keyTuple, transTuple *ct.IPTuple) {
 		key, ok := formatKey(keyTuple)
 		if !ok {
@@ -350,13 +362,20 @@ func (cc *conntrackCache) Add(c Con, orphan bool) {
 			})
 		}
 
-		cc.cache.Add(key, t)
+		if cc.cache.Add(key, t) {
+			evicts++
+		}
 	}
 
 	log.Tracef("%s", c)
 
 	registerTuple(c.Origin, c.Reply)
 	registerTuple(c.Reply, c.Origin)
+	return
+}
+
+func (cc *conntrackCache) Len() int {
+	return cc.cache.Len()
 }
 
 func (cc *conntrackCache) removeOrphans(now time.Time) (removed int64) {
