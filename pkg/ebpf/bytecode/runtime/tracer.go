4c4bc0e575d795e18f957bd9938c4e5af4554f3a
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2021-03-31 17:02:09 -0500

    Update tracer.go

--- pkg/ebpf/bytecode/runtime/tracer.go
+++ pkg/ebpf/bytecode/runtime/tracer.go
@@ -3,4 +3,4 @@
 
 package runtime
 
-var Tracer = NewRuntimeAsset("tracer.c", "f2aaadb168adc8cf8e51595718b8bec6ab7e64a88805f3b6f12ee1794da9e1a3")
+var Tracer = NewRuntimeAsset("tracer.c", "0a5dc6cf27f78cc2d842308f4a0342f48cba6456c86565f1f15e61f657db4bcb")
