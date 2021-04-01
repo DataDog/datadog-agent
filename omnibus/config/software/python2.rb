9decfe0415cbc87f68faa7364bfa32b6bd617b1d
Julien Lebot <julien.ar.lebot@gmail.com>
2021-04-01 11:51:57 +0200

    Revert using x64 instead of amd64

--- omnibus/config/software/python2.rb
+++ omnibus/config/software/python2.rb
@@ -85,7 +85,7 @@ else
            :sha256 => "c8309b3351610a7159e91e55f09f7341bc3bbdd67d2a5e3049a9d1157e5a9110",
            :extract => :seven_zip
   else
-    source :url => "https://dd-ci-persistent-artefacts-build-stable.s3.amazonaws.com/python-windows-#{version}-x64.zip",
+    source :url => "https://dd-ci-persistent-artefacts-build-stable.s3.amazonaws.com/python-windows-#{version}-amd64.zip",
          :sha256 => "7989b2efe6106a3df82c47d403dbb166db6d4040f3654871323df7e724a9fdd2",
          :extract => :seven_zip
   end
