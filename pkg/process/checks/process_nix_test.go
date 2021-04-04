b450c2ae2e05fe6db7feb3fa6ce5f47f484d993d
William Yu <william.yu@datadoghq.com>
2021-04-04 15:41:33 -0400

    Update process chunking tests.

--- pkg/process/checks/process_nix_test.go
+++ pkg/process/checks/process_nix_test.go
@@ -3,7 +3,9 @@
 package checks
 
 import (
+	"fmt"
 	"regexp"
+	"sort"
 	"testing"
 	"time"
 
@@ -16,90 +18,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/containers"
 	"github.com/DataDog/gopsutil/cpu"
 )
-
-// TestRandomizeMessage generates some processes and containers, then do a deep dive on return messages and make sure the chunk logic holds
-func TestRandomizeMessages(t *testing.T) {
-	for i, tc := range []struct {
-		testName                                string
-		pCount, cCount, cProcs, maxSize, chunks int
-		containerHostType                       model.ContainerHostType
-	}{
-		{
-			testName:          "no-containers",
-			pCount:            100,
-			cCount:            0,
-			cProcs:            0,
-			maxSize:           30,
-			chunks:            4,
-			containerHostType: model.ContainerHostType_fargateECS,
-		},
-		{
-			testName:          "no-processes",
-			pCount:            0,
-			cCount:            30,
-			cProcs:            0,
-			maxSize:           10,
-			chunks:            1,
-			containerHostType: model.ContainerHostType_fargateEKS,
-		},
-		{
-			testName:          "container-process-mixed-1",
-			pCount:            100,
-			cCount:            30,
-			cProcs:            60,
-			maxSize:           30,
-			chunks:            3,
-			containerHostType: model.ContainerHostType_notSpecified,
-		},
-		{
-			testName: "container-process-mixed-2",
-			pCount:   100,
-			cCount:   10,
-			cProcs:   60,
-			maxSize:  10,
-			chunks:   5,
-		},
-		{
-			testName: "container-process-mixed-3",
-			pCount:   100,
-			cCount:   100,
-			cProcs:   100,
-			maxSize:  10,
-			chunks:   1,
-		},
-		{
-			testName: "container-process-mixed-4",
-			pCount:   100,
-			cCount:   17,
-			cProcs:   78,
-			maxSize:  10,
-			chunks:   4,
-		},
-	} {
-
-		t.Run(tc.testName, func(t *testing.T) {
-			procs, ctrs := procCtrGenerator(tc.pCount, tc.cCount, tc.cProcs)
-			procsByPid := procsToHash(procs)
-			networks := make(map[int32][]*model.Connection)
-
-			lastRun := time.Now().Add(-5 * time.Second)
-			syst1, syst2 := cpu.TimesStat{}, cpu.TimesStat{}
-			cfg := config.NewDefaultAgentConfig(false)
-			sysInfo := &model.SystemInfo{}
-			lastCtrRates := util.ExtractContainerRateMetric(ctrs)
-
-			cfg.MaxPerMessage = tc.maxSize
-			cfg.ContainerHostType = tc.containerHostType
-			processes := fmtProcesses(cfg, procsByPid, procsByPid, containersByPid(ctrs), syst2, syst1, lastRun, networks)
-			containers := fmtContainers(ctrs, lastCtrRates, lastRun)
-			messages, totalProcs, totalContainers := createProcCtrMessages(processes, containers, cfg, sysInfo, int32(i), "nid")
-
-			assert.Equal(t, totalProcs, tc.pCount)
-			assert.Equal(t, totalContainers, tc.cCount)
-			procMsgsVerification(t, messages, ctrs, procs, tc.maxSize, cfg)
-		})
-	}
-}
 
 // TestBasicProcessMessages tests basic cases for creating payloads by hard-coded scenarios
 func TestBasicProcessMessages(t *testing.T) {
@@ -178,13 +96,24 @@ func TestBasicProcessMessages(t *testing.T) {
 			totalContainers: 1,
 		},
 		{
+			testName:        "container processes chunked",
+			cur:             map[int32]*procutil.Process{p[2].Pid: p[2], p[3].Pid: p[3], p[4].Pid: p[4]},
+			last:            map[int32]*procutil.Process{p[2].Pid: p[2], p[3].Pid: p[3], p[4].Pid: p[4]},
+			maxSize:         1,
+			containers:      []*containers.Container{c[0], c[1]},
+			blacklist:       []string{},
+			expectedChunks:  3,
+			totalProcs:      3,
+			totalContainers: 2,
+		},
+		{
 			testName:        "no non-container processes",
 			cur:             map[int32]*procutil.Process{p[0].Pid: p[0], p[1].Pid: p[1], p[2].Pid: p[2]},
 			last:            map[int32]*procutil.Process{p[0].Pid: p[0], p[1].Pid: p[1], p[2].Pid: p[2]},
 			maxSize:         1,
 			containers:      []*containers.Container{c[0], c[1]},
 			blacklist:       []string{},
-			expectedChunks:  1,
+			expectedChunks:  2,
 			totalProcs:      3,
 			totalContainers: 2,
 		},
@@ -218,4 +147,223 @@ func TestBasicProcessMessages(t *testing.T) {
 			assert.Equal(t, tc.totalContainers, totalContainers)
 		})
 	}
+}
+
+type ctrProc struct {
+	ctrId   string
+	pCounts int
+}
+
+// TestContainerProcessChunking generates processes and containers and tests that they are properly chunked
+func TestContainerProcessChunking(t *testing.T) {
+	for i, tc := range []struct {
+		testName                            string
+		ctrProcs                            []ctrProc
+		expectedBatches                     []map[string]int
+		expectedCtrCount, expectedProcCount int
+		maxSize                             int
+		containerHostType                   model.ContainerHostType
+	}{
+		{
+			testName: "no containers",
+			ctrProcs: []ctrProc{
+				{ctrId: "", pCounts: 3},
+			},
+			expectedBatches: []map[string]int{
+				{"": 3},
+			},
+			expectedProcCount: 3,
+			maxSize:           10,
+			containerHostType: model.ContainerHostType_notSpecified,
+		},
+		{
+			testName: "no containers processes are chunked",
+			ctrProcs: []ctrProc{
+				{ctrId: "", pCounts: 8},
+			},
+			expectedBatches: []map[string]int{
+				{"": 2},
+				{"": 2},
+				{"": 2},
+				{"": 2},
+			},
+
+			expectedProcCount: 8,
+			maxSize:           2,
+			containerHostType: model.ContainerHostType_notSpecified,
+		},
+		{
+			testName: "container process mixed 1",
+			ctrProcs: []ctrProc{
+				{ctrId: "", pCounts: 3},
+				{ctrId: "1", pCounts: 4},
+			},
+			expectedBatches: []map[string]int{
+				{"": 3},
+				{"1": 4},
+			},
+			expectedCtrCount:  1,
+			expectedProcCount: 7,
+			maxSize:           10,
+			containerHostType: model.ContainerHostType_notSpecified,
+		},
+		{
+			testName: "container process mixed 1",
+			ctrProcs: []ctrProc{
+				{ctrId: "1", pCounts: 5},
+				{ctrId: "2", pCounts: 4},
+				{ctrId: "3", pCounts: 1},
+				{ctrId: "4", pCounts: 1},
+				{ctrId: "5", pCounts: 4},
+				{ctrId: "6", pCounts: 2},
+				{ctrId: "7", pCounts: 9},
+			},
+			expectedBatches: []map[string]int{
+				{"1": 5, "2": 4, "3": 1},
+				{"4": 1, "5": 4, "6": 2},
+				{"7": 9},
+			},
+			expectedCtrCount:  7,
+			expectedProcCount: 26,
+			maxSize:           10,
+			containerHostType: model.ContainerHostType_notSpecified,
+		},
+		{
+			testName: "container process mixed 3",
+			ctrProcs: []ctrProc{
+				{ctrId: "", pCounts: 3},
+				// container pid counts - [11, 4, 8, 1, 4, 2, 9]
+				{ctrId: "1", pCounts: 11},
+				{ctrId: "2", pCounts: 4},
+				{ctrId: "3", pCounts: 8},
+				{ctrId: "4", pCounts: 1},
+				{ctrId: "5", pCounts: 4},
+				{ctrId: "6", pCounts: 2},
+				{ctrId: "7", pCounts: 9},
+			},
+			expectedBatches: []map[string]int{
+				{"": 3},
+				{"1": 11},
+				{"2": 4},
+				{"3": 8, "4": 1},
+				{"5": 4, "6": 2},
+				{"7": 9},
+			},
+			expectedCtrCount:  7,
+			expectedProcCount: 42,
+			maxSize:           10,
+			containerHostType: model.ContainerHostType_notSpecified,
+		},
+	} {
+		t.Run(tc.testName, func(t *testing.T) {
+			procs, ctrs := generateCtrProcs(tc.ctrProcs)
+			procsByPid := procsToHash(procs)
+
+			lastRun := time.Now().Add(-5 * time.Second)
+			syst1, syst2 := cpu.TimesStat{}, cpu.TimesStat{}
+			cfg := config.NewDefaultAgentConfig(false)
+			sysInfo := &model.SystemInfo{}
+			lastCtrRates := util.ExtractContainerRateMetric(ctrs)
+			cfg.MaxPerMessage = tc.maxSize
+			cfg.ContainerHostType = tc.containerHostType
+
+			processes := fmtProcesses(cfg, procsByPid, procsByPid, ctrIDForPID(ctrs), syst2, syst1, lastRun)
+			containers := fmtContainers(ctrs, lastCtrRates, lastRun)
+			messages, totalProcs, totalContainers := createProcCtrMessages(processes, containers, cfg, sysInfo, int32(i), "nid")
+
+			assert.Equal(t, tc.expectedProcCount, totalProcs)
+			assert.Equal(t, tc.expectedCtrCount, totalContainers)
+
+			// sort messages
+			sortMsgs(messages)
+			verifyBatchedMsgs(t, cfg, tc.expectedBatches, messages)
+		})
+	}
+}
+
+//sortMsgs sorts the CollectorProc messages so they can be validated
+func sortMsgs(m []model.MessageBody) {
+	// sort the processes and containers of each message
+	for i := range m {
+		payload := m[i].(*model.CollectorProc)
+		sort.SliceStable(payload.Containers, func(i, j int) bool {
+			return payload.Containers[i].Id < payload.Containers[j].Id
+		})
+		sort.SliceStable(payload.Processes, func(i, j int) bool {
+			return payload.Processes[i].Pid < payload.Processes[j].Pid
+		})
+	}
+
+	// sort all the messages by containers then by PIDs
+	sort.SliceStable(m, func(i, j int) bool {
+		cpI := m[i].(*model.CollectorProc)
+		cpJ := m[j].(*model.CollectorProc)
+
+		for k := range cpI.Containers {
+			if cpJ.Containers == nil {
+				return false
+			}
+			if cpI.Containers[k].Id < cpJ.Containers[k].Id {
+				return true
+			}
+			if cpI.Containers[k].Id > cpJ.Containers[k].Id {
+				return false
+			}
+		}
+		for k := range cpI.Processes {
+			if cpJ.Processes == nil {
+				return false
+			}
+			if cpI.Processes[k].Pid < cpJ.Processes[k].Pid {
+				return true
+			}
+			if cpI.Processes[k].Pid > cpJ.Processes[k].Pid {
+				return false
+			}
+		}
+		return true
+	})
+}
+
+func verifyBatchedMsgs(t *testing.T, cfg *config.AgentConfig, expected []map[string]int, msgs []model.MessageBody) {
+	assert := assert.New(t)
+
+	assert.Equal(len(expected), len(msgs), "Number of messages created")
+
+	for i, msg := range msgs {
+		payload := msg.(*model.CollectorProc)
+
+		assert.Equal(cfg.ContainerHostType, payload.ContainerHostType)
+
+		var actualCtrPIDCounts = map[string]int{}
+
+		// verify number of processes for each container
+		for _, proc := range payload.Processes {
+			actualCtrPIDCounts[proc.ContainerId] += 1
+		}
+
+		assert.EqualValues(expected[i], actualCtrPIDCounts)
+	}
+}
+
+// generateCtrProcs generates groups of processes for linking with containers
+func generateCtrProcs(ctrProcs []ctrProc) ([]*procutil.Process, []*containers.Container) {
+	var procs []*procutil.Process
+	var ctrs []*containers.Container
+	pid := 1
+
+	for _, ctrProc := range ctrProcs {
+		ctr := makeContainer(ctrProc.ctrId)
+		if ctr.ID != emptyCtrID {
+			ctrs = append(ctrs, ctr)
+		}
+
+		for i := 0; i < ctrProc.pCounts; i++ {
+			proc := makeProcess(int32(pid), fmt.Sprintf("cmd %d", pid))
+			ctr.Pids = append(ctr.Pids, proc.Pid)
+			procs = append(procs, proc)
+			pid++
+		}
+	}
+	return procs, ctrs
 }
