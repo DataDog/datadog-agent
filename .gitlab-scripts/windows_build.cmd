7889179d74d2e6f9803c95d91e88c82b4ee03793
Vyacheslav Voronenko <git@voronenko.info>
2021-04-14 22:50:22 +0200

    workaround for a scenario when venv exists, but empty for some reason

--- .gitlab-scripts/windows_build.cmd
+++ .gitlab-scripts/windows_build.cmd
@@ -2,8 +2,6 @@ REM set WIN_CI_PROJECT_DIR=%CD%
 REM set WORKON_HOME=%WIN_CI_PROJECT_DIR%
 
 
-echo call %WORKON_HOME%\venv\Scripts\activate.bat
-call "%WORKON_HOME%\venv\Scripts\activate.bat"
 call ridk enable
 call "%VCINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
 
@@ -14,4 +12,8 @@ echo git config --global user.email "gitlab@runner.some"
 git config --global user.email "gitlab@runner.some"
 echo git config --global user.name "Gitlab runner"
 git config --global user.name "Gitlab runner"
+
+echo call %WORKON_HOME%\venv\Scripts\activate.bat
+call "%WORKON_HOME%\venv\Scripts\activate.bat"
+
 inv -e agent.omnibus-build --skip-sign --log-level debug --skip-deps
