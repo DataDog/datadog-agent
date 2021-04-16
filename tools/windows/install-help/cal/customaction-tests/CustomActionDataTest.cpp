2d19a514445c9cf40f0c40069c4e2cdd5030c197
Julien Lebot <julien.lebot@datadoghq.com>
2021-04-16 17:09:15 +0200

    [Windows] Run custom action unit tests in CI (#7374)

---
+++ tools/windows/install-help/cal/customaction-tests/CustomActionDataTest.cpp
@@ -0,0 +1,11 @@
+#include "customaction-tests.h"
+#include <Msi.h>
+
+#include "CustomActionDataTest.h"
+#include "strings.h"
+
+void CustomActionDataTest::SetUp()
+{
+    propertyDDAgentUserName = L"DDAGENTUSER_NAME";
+    propertyDDAgentUserPassword = L"DDAGENTUSER_PASSWORD";
+}
