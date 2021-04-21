93778707bdaa001b9900e75d7b92fc06c94dd1e3
Moises Botarro <moises.bottaro.silva@gmail.com>
2021-04-21 12:56:04 -0300

    fix DefaultSysProbeConfPath comment on macOS

--- cmd/process-agent/flags/flags_darwin.go
+++ cmd/process-agent/flags/flags_darwin.go
@@ -5,6 +5,6 @@ package flags
 const (
 	// DefaultConfPath points to the location of datadog.yaml
 	DefaultConfPath = "/opt/datadog-agent/etc/datadog.yaml"
-	// system-probe is not yet supported on darwin
+	// DefaultSysProbeConfPath is set to empty since system-probe is not yet supported on darwin
 	DefaultSysProbeConfPath = ""
 )
