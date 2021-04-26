418c53eb645f06f0f2d81e3125b99a18fedf7535
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2021-04-26 17:42:53 +0000

    [jmx] update comment
    Remove mention of initial start attempt, because it doesn't happen in
    the same loop where the limiter is called. Correct the statement about
    when the first negative check might happen.

--- pkg/jmxfetch/limiter.go
+++ pkg/jmxfetch/limiter.go
@@ -31,11 +31,9 @@ func (r *restartLimiter) canRestart(now time.Time) bool {
 	r.stopTimes[r.idx] = now
 	oldestIdx := (r.idx + r.maxRestarts + 1) % r.maxRestarts
 
-	// Please note that the zero value for `time.Time` is `0001-01-01 00:00:00 +0000 UTC`
-	// therefore for the first iteration (the initial launch attempt), the interval will
-	// always be biger than ival (jmx_restart_interval). In fact, this sub operation with
-	// stopTimes here will only start yielding values potentially <= ival _after_ the first
-	// maxRestarts attempts, which is fine and consistent.
+	// Please note that the zero value for `time.Time` is `0001-01-01 00:00:00+0000 UTC`
+	// The sub operation with stopTimes here will only start yielding values potentially
+	// <= ival _after_ the first maxRestarts-1 attempts.
 	restartTooSoon := r.stopTimes[r.idx].Sub(r.stopTimes[oldestIdx]).Seconds() <= r.interval
 
 	r.idx = (r.idx + 1) % r.maxRestarts
