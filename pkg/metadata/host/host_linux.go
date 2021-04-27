09d79d7c5e3a15fd23bfc3df3b7a243e96af4bda
db <derek.brown@datadoghq.com>
2021-04-27 14:32:35 -0700

    use configuration option for procfs path if supplied

--- pkg/metadata/host/host_linux.go
+++ pkg/metadata/host/host_linux.go
@@ -6,13 +6,19 @@
 package host
 
 import (
+	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/network/config/sysctl"
 )
 
 // GetNetworkInfo returns host specific network configuration.
 // At this time, only information queried is the ephemeral port range
 func GetNetworkInfo() (*NetworkInfo, error) {
-	intpair := sysctl.NewIntPair("/proc", "net/ipv4/ip_local_port_range", 0)
+	procfsPath := "/proc"
+	if config.Datadog.IsSet("procfs_path") {
+		procfsPath = config.Datadog.GetString("procfs_path")
+	}
+
+	intpair := sysctl.NewIntPair(procfsPath, "net/ipv4/ip_local_port_range", 0)
 	low, hi, err := intpair.Get()
 	if nil != err {
 		return nil, err
