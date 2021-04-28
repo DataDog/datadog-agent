b21e50b825c3fed453eb6b75b800b09b89ea42b4
Brian Floersch <brian.floersch@datadoghq.com>
2021-04-28 13:41:25 -0400

    added flush flag

--- pkg/aggregator/check_sampler.go
+++ pkg/aggregator/check_sampler.go
@@ -90,12 +90,16 @@ func (cs *CheckSampler) addBucket(bucket *metrics.HistogramBucket) {
 	if bucket.Monotonic {
 		lastBucketValue, bucketFound := cs.lastBucketValue[contextKey]
 		rawValue := bucket.Value
-		if bucketFound {
-			cs.lastSeenBucket[contextKey] = time.Now()
-			bucket.Value = rawValue - lastBucketValue
-		}
-		cs.lastBucketValue[contextKey] = rawValue
+
 		cs.lastSeenBucket[contextKey] = time.Now()
+		cs.lastBucketValue[contextKey] = rawValue
+
+		// Return early so we don't report the first raw value instead of the delta which will cause spikes
+		if !bucketFound && !bucket.FlushFirstValue {
+			return
+		}
+
+		bucket.Value = rawValue - lastBucketValue
 	}
 
 	if bucket.Value < 0 {
