834cb483782118802d9f702a4f29a25d639fb6fa
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-04-30 10:49:00 +0200

    [kitchen] Fix resource group name collisions in CI (#7979)
    Adds a special variable for kitchen tests to prevent different types of tests conflicting with each other.
    Renames the test suites with shorter names.

--- test/kitchen/test/integration/dd-agent-upgrade-x64/rspec/dd-agent-upgrade_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'the upgraded agent' do
-  include_examples 'Agent install'
-  include_examples 'Agent behavior'
-  it_behaves_like 'an upgraded Agent with the expected version'
-
-  include_examples 'Agent uninstall'
-end
