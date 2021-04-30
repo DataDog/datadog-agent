67792f2feb3dea1b1d6a03dcb949f479d573f809
Julien Lebot <julien.ar.lebot@gmail.com>
2021-04-30 15:53:59 +0200

    Save customaction PDB

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -108,6 +108,7 @@ build do
   else
     copy 'bin/agent/ddtray.exe', "#{install_dir}/bin/agent"
     copy 'bin/agent/dist', "#{install_dir}/bin/agent"
+    copy 'bin/agent/customaction.pdb', "#{Omnibus::Config.package_dir()}"
   end
 
   block do
