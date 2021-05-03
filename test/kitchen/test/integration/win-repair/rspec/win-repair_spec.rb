615343ec973d6fd3ac6f9a2d61a5533c6e49ffdd
Julien Lebot <julien.ar.lebot@gmail.com>
2021-05-03 16:03:02 +0200

    Use new naming convention

--- test/kitchen/test/integration/win-repair/rspec/win-repair_spec.rb
+++ test/kitchen/test/integration/win-repair/rspec/win-repair_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'dd-agent-repair' do
+describe 'win-repair' do
     it_behaves_like 'an installed Agent'
     it_behaves_like 'a running Agent with no errors'
   end
