bd0d117bd874b960612084ac1d811adf35c193bd
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2021-05-10 16:49:47 +0200

    [DCA][Admission] Support namespace selector fallback

--- pkg/clusteragent/admission/util_test.go
+++ pkg/clusteragent/admission/util_test.go
@@ -8,14 +8,17 @@
 package admission
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/util/cache"
 
 	"github.com/stretchr/testify/assert"
 	admiv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/version"
+	fakediscovery "k8s.io/client-go/discovery/fake"
+	fakeclientset "k8s.io/client-go/kubernetes/fake"
 )
 
 func Test_getWebhookSkeleton(t *testing.T) {
@@ -93,11 +96,17 @@ func Test_getWebhookSkeleton(t *testing.T) {
 }
 
 func Test_generateWebhooks(t *testing.T) {
+	client := fakeclientset.NewSimpleClientset()
+	fakeDiscovery, ok := client.Discovery().(*fakediscovery.FakeDiscovery)
+	assert.True(t, ok)
+
 	mockConfig := config.Mock()
 	tests := []struct {
-		name        string
-		setupConfig func()
-		want        func() []admiv1beta1.MutatingWebhook
+		name          string
+		setupConfig   func()
+		serverVersion *version.Info
+		want          func() []admiv1beta1.MutatingWebhook
+		wantErr       bool
 	}{
 		{
 			name: "config injection, mutate all",
@@ -119,6 +128,7 @@ func Test_generateWebhooks(t *testing.T) {
 				}
 				return []admiv1beta1.MutatingWebhook{webhook}
 			},
+			wantErr: false,
 		},
 		{
 			name: "config injection, mutate labelled",
@@ -136,6 +146,7 @@ func Test_generateWebhooks(t *testing.T) {
 				}
 				return []admiv1beta1.MutatingWebhook{webhook}
 			},
+			wantErr: false,
 		},
 		{
 			name: "tags injection, mutate all",
@@ -157,6 +168,7 @@ func Test_generateWebhooks(t *testing.T) {
 				}
 				return []admiv1beta1.MutatingWebhook{webhook}
 			},
+			wantErr: false,
 		},
 		{
 			name: "tags injection, mutate labelled",
@@ -174,6 +186,7 @@ func Test_generateWebhooks(t *testing.T) {
 				}
 				return []admiv1beta1.MutatingWebhook{webhook}
 			},
+			wantErr: false,
 		},
 		{
 			name: "config and tags injection, mutate labelled",
@@ -196,6 +209,7 @@ func Test_generateWebhooks(t *testing.T) {
 				}
 				return []admiv1beta1.MutatingWebhook{webhookConfig, webhookTags}
 			},
+			wantErr: false,
 		},
 		{
 			name: "config and tags injection, mutate all",
@@ -227,14 +241,164 @@ func Test_generateWebhooks(t *testing.T) {
 				}
 				return []admiv1beta1.MutatingWebhook{webhookConfig, webhookTags}
 			},
+			wantErr: false,
+		},
+		{
+			name: "namespace selector enabled, old cluster version",
+			setupConfig: func() {
+				mockConfig.Set("admission_controller.mutate_unlabelled", false)
+				mockConfig.Set("admission_controller.inject_config.enabled", true)
+				mockConfig.Set("admission_controller.inject_tags.enabled", true)
+				mockConfig.Set("admission_controller.namespace_selector_fallback", true)
+			},
+			serverVersion: &version.Info{Major: "1", Minor: "14+"},
+			want: func() []admiv1beta1.MutatingWebhook {
+				webhookConfig := getWebhookSkeleton("config", "/injectconfig")
+				webhookConfig.NamespaceSelector = &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"admission.datadoghq.com/enabled": "true",
+					},
+				}
+				webhookTags := getWebhookSkeleton("tags", "/injecttags")
+				webhookTags.NamespaceSelector = &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"admission.datadoghq.com/enabled": "true",
+					},
+				}
+				return []admiv1beta1.MutatingWebhook{webhookConfig, webhookTags}
+			},
+			wantErr: false,
+		},
+		{
+			name: "namespace selector enabled, new cluster version",
+			setupConfig: func() {
+				mockConfig.Set("admission_controller.mutate_unlabelled", false)
+				mockConfig.Set("admission_controller.inject_config.enabled", true)
+				mockConfig.Set("admission_controller.inject_tags.enabled", true)
+				mockConfig.Set("admission_controller.namespace_selector_fallback", true)
+			},
+			serverVersion: &version.Info{Major: "1", Minor: "19+"},
+			want: func() []admiv1beta1.MutatingWebhook {
+				webhookConfig := getWebhookSkeleton("config", "/injectconfig")
+				webhookConfig.ObjectSelector = &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"admission.datadoghq.com/enabled": "true",
+					},
+				}
+				webhookTags := getWebhookSkeleton("tags", "/injecttags")
+				webhookTags.ObjectSelector = &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"admission.datadoghq.com/enabled": "true",
+					},
+				}
+				return []admiv1beta1.MutatingWebhook{webhookConfig, webhookTags}
+			},
+			wantErr: false,
+		},
+		{
+			name: "namespace selector enabled, invalid version",
+			setupConfig: func() {
+				mockConfig.Set("admission_controller.mutate_unlabelled", false)
+				mockConfig.Set("admission_controller.inject_config.enabled", true)
+				mockConfig.Set("admission_controller.inject_tags.enabled", true)
+				mockConfig.Set("admission_controller.namespace_selector_fallback", true)
+			},
+			serverVersion: &version.Info{Major: "1", Minor: "foo"},
+			want:          func() []admiv1beta1.MutatingWebhook { return []admiv1beta1.MutatingWebhook{} },
+			wantErr:       true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.setupConfig()
-			if got := generateWebhooks(); !reflect.DeepEqual(got, tt.want()) {
-				t.Errorf("generateWebhooks() = %v, want %v", got, tt.want())
-			}
+			fakeDiscovery.FakedServerVersion = tt.serverVersion
+			cache.Cache.Flush()
+			got, err := generateWebhooks(fakeDiscovery)
+			assert.Equal(t, tt.wantErr, err != nil)
+			assert.EqualValues(t, tt.want(), got)
+		})
+	}
+}
+
+func Test_shouldFallback(t *testing.T) {
+	tests := []struct {
+		name    string
+		v       *version.Info
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "v1.10 => fallback",
+			v:       &version.Info{Major: "1", Minor: "10"},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "v1.11 => fallback",
+			v:       &version.Info{Major: "1", Minor: "11"},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "v1.12 => fallback",
+			v:       &version.Info{Major: "1", Minor: "12"},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "v1.13 => fallback",
+			v:       &version.Info{Major: "1", Minor: "13"},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "v1.14 => fallback",
+			v:       &version.Info{Major: "1", Minor: "14"},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name:    "v1.15 => no fallback",
+			v:       &version.Info{Major: "1", Minor: "15+"},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name:    "v1.9 => no fallback",
+			v:       &version.Info{Major: "1", Minor: "9"},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name:    "unsupported major #1",
+			v:       &version.Info{Major: "0", Minor: "14"},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name:    "unsupported major #2",
+			v:       &version.Info{Major: "2", Minor: "14"},
+			want:    false,
+			wantErr: false,
+		},
+		{
+			name:    "invalid minor",
+			v:       &version.Info{Major: "1", Minor: "foo"},
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name:    "custom minor",
+			v:       &version.Info{Major: "1", Minor: "10+"},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := shouldFallback(tt.v)
+			assert.Equal(t, tt.wantErr, err != nil)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
