fe252a55b1d910085efbd611e9177faabc7b254e
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-05-14 12:05:27 +0200

    [runtime-security] add json schema validation for chown syscall

--- pkg/security/tests/schemas.go
+++ pkg/security/tests/schemas.go
@@ -109,3 +109,24 @@ func validateChmodSchema(t *testing.T, event *sprobe.Event) bool {
 
 	return true
 }
+
+func validateChownSchema(t *testing.T, event *sprobe.Event) bool {
+	fs := NewAssetFileSystem()
+
+	documentLoader := gojsonschema.NewStringLoader(event.String())
+	schemaLoader := gojsonschema.NewReferenceLoaderFileSystem("file:///chown.schema.json", fs)
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !result.Valid() {
+		for _, desc := range result.Errors() {
+			t.Errorf("%s", desc)
+		}
+		return false
+	}
+
+	return true
+}
