b5b7f73a53fa071255f64563ed183e1b450b65a4
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-05-17 17:25:40 -0400

    Fix test utils

--- pkg/logs/status/test_utils.go
+++ pkg/logs/status/test_utils.go
@@ -13,6 +13,6 @@ import (
 // InitStatus initialize a status builder
 func InitStatus(sources *config.LogSources) {
 	var isRunning int32 = 1
-	endpoints, _ := config.BuildEndpoints(config.HTTPConnectivityFailure)
+	endpoints, _ := config.BuildEndpoints(config.HTTPConnectivityFailure, false)
 	Init(&isRunning, endpoints, sources, metrics.LogsExpvars)
 }
