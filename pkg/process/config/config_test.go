fdf646f9c3c1241b541e3e8154f7e51dc9facb30
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-05-20 14:07:49 -0500

    [protobuf] fix a couple packages left behind

--- pkg/process/config/config_test.go
+++ pkg/process/config/config_test.go
@@ -16,10 +16,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/DataDog/datadog-agent/cmd/agent/api/pb"
-	"github.com/DataDog/datadog-agent/cmd/agent/api/pb/mocks"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/process/procutil"
+	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo"
+	"github.com/DataDog/datadog-agent/pkg/proto/pbgo/mocks"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
