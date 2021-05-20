cb7c6ed6e2c4a3128ec064a5fe24e851d41f6df4
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-05-20 09:51:05 +0200

    Add network config warning (#8100)

--- pkg/collector/corechecks/snmp/config.go
+++ pkg/collector/corechecks/snmp/config.go
@@ -41,6 +41,10 @@ type snmpInstanceConfig struct {
 	Profile          string            `yaml:"profile"`
 	UseGlobalMetrics bool              `yaml:"use_global_metrics"`
 	ExtraTags        string            `yaml:"extra_tags"` // comma separated tags
+
+	// `network` config is only available in Python SNMP integration
+	// it's added here to raise warning if used with corecheck SNMP integration
+	Network string `yaml:"network"`
 }
 
 type snmpConfig struct {
@@ -147,6 +151,10 @@ func buildConfig(rawInstance integration.Data, rawInitConfig integration.Data) (
 	c.port = uint16(instance.Port)
 	if instance.ExtraTags != "" {
 		c.extraTags = strings.Split(instance.ExtraTags, ",")
+	}
+
+	if instance.Network != "" {
+		log.Warnf("`network` config is not available for corecheck SNMP integration to use autodiscovery. Agent `snmp_listener` config can be used instead: https://docs.datadoghq.com/network_monitoring/devices/setup?tab=snmpv2#autodiscovery")
 	}
 
 	if c.ipAddress == "" {
