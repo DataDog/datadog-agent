f19d8f8f6d868df18a93c0532da90c6c28a616f3
Bram Schuur <bschuur@stackstate.com>
2021-05-20 10:35:13 +0200

    STAC-12961: Add omitempty to expiry

--- pkg/health/start_snapshot_metadata.go
+++ pkg/health/start_snapshot_metadata.go
@@ -3,5 +3,5 @@ package health
 // StartSnapshotMetadata is a representation of 'start' for health synchronization
 type StartSnapshotMetadata struct {
 	RepeatIntervalS int `json:"repeat_interval_s"`
-	ExpiryIntervalS int `json:"expiry_interval_s"`
+	ExpiryIntervalS int `json:"expiry_interval_s,omitempty"`
 }
