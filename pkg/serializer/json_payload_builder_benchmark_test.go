98253e1d17b514608f5985ab64bc3dff44b3fed8
Brian Floersch <brian.floersch@datadoghq.com>
2021-05-21 15:16:35 -0400

    added normal benchmarks

--- pkg/serializer/json_payload_builder_benchmark_test.go
+++ pkg/serializer/json_payload_builder_benchmark_test.go
@@ -4,13 +4,14 @@ package serializer
 
 import (
 	"fmt"
+	"testing"
 	"time"
 
 	"github.com/DataDog/datadog-agent/pkg/metrics"
 	"github.com/DataDog/datadog-agent/pkg/serializer/stream"
 )
 
-func benchmarkJSONPayloadBuilderThroughput(points int, items int, tags int, runs int) { //nolint:unuse
+func generateData(points int, items int, tags int) metrics.Series {
 	series := metrics.Series{}
 	for i := 0; i < items; i++ {
 		series = append(series, &metrics.Serie{
@@ -34,6 +35,23 @@ func benchmarkJSONPayloadBuilderThroughput(points int, items int, tags int, runs
 			}(),
 		})
 	}
+	return series
+}
+
+func benchmarkJSONPayloadBuilderUsage(b *testing.B, points int, items int, tags int) {
+
+	series := generateData(points, items, tags)
+	payloadBuilder := stream.NewJSONPayloadBuilder(true)
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		payloadBuilder.Build(series)
+	}
+}
+
+func benchmarkJSONPayloadBuilderThroughput(points int, items int, tags int, runs int) { //nolint:unuse
+	series := generateData(points, items, tags)
 	json, _ := series.MarshalJSON()
 	initialSize := len(json)
 	metricsCount := len(series)
@@ -87,3 +105,60 @@ func benchmarkJSONPayloadBuilderThroughput(points int, items int, tags int, runs
 // 	benchmarkJSONPayloadBuilderThroughput(4, 500000, 1, 1)
 // 	benchmarkJSONPayloadBuilderThroughput(4, 500000, 10, 1)
 // }
+
+func BenchmarkJSONPayloadBuilderThroughputPoints0(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 0, 100, 1)
+}
+func BenchmarkJSONPayloadBuilderThroughputPoints1(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 1)
+}
+func BenchmarkJSONPayloadBuilderThroughputPoints2(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 2, 100, 1)
+}
+func BenchmarkJSONPayloadBuilderThroughputPoints5(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 5, 100, 1)
+}
+func BenchmarkJSONPayloadBuilderThroughputPoints10(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 10, 100, 1)
+}
+func BenchmarkJSONPayloadBuilderThroughputPoints100(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 100, 100, 1)
+}
+
+func BenchmarkJSONPayloadBuilderTags0(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 0)
+}
+func BenchmarkJSONPayloadBuilderTags1(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 1)
+}
+func BenchmarkJSONPayloadBuilderTags2(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 2)
+}
+func BenchmarkJSONPayloadBuilderTags5(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 5)
+}
+func BenchmarkJSONPayloadBuilderTags10(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 10)
+}
+func BenchmarkJSONPayloadBuilderTags100(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 100)
+}
+
+func BenchmarkJSONPayloadBuilderItems1(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 1, 1)
+}
+func BenchmarkJSONPayloadBuilderItems10(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 10, 1)
+}
+func BenchmarkJSONPayloadBuilderItems100(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100, 1)
+}
+func BenchmarkJSONPayloadBuilderItems1000(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 1000, 1)
+}
+func BenchmarkJSONPayloadBuilderItems10000(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 10000, 1)
+}
+func BenchmarkJSONPayloadBuilderItems100000(b *testing.B) {
+	benchmarkJSONPayloadBuilderUsage(b, 1, 100000, 1)
+}
