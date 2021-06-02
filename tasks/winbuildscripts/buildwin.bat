3bcc4581e904d9b7e659ac408dd31dda35e24c51
Mike Zhu <mike.zhu@datadoghq.com>
2021-06-01 17:07:19 -0700

    Support in-house CPython
    This commit creates a fake VCForPython 13.2 and point it to the local VS 2019 toolchain.

--- tasks/winbuildscripts/buildwin.bat
+++ tasks/winbuildscripts/buildwin.bat
@@ -2,6 +2,13 @@ if not exist c:\mnt\ goto nomntdir
 
 @echo c:\mnt found, continuing
 
+REM
+REM This is a tweak for the in-house CPython build which requests a weird compiler version.
+REM The registry keys will redirect the nonexisting v13.2 compiler to the local VS 2019 toolchain.
+REM
+reg add "HKEY_CURRENT_USER\Software\Microsoft\DevDiv\VCForPython\13.2" /v InstallDir     /t REG_SZ    /d C:\devtools\vstudio\VC\Auxiliary\Build\ /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\DevDiv\VCForPython\13.2" /v StartMenuItems /t REG_DWORD /d 1                                       /f
+
 mkdir \dev\go\src\github.com\DataDog\datadog-agent
 if not exist \dev\go\src\github.com\DataDog\datadog-agent exit /b 2
 cd \dev\go\src\github.com\DataDog\datadog-agent || exit /b 3
