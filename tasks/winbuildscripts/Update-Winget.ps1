3a3fe1e2d868ff3b2485df6d2edcf6507cd4a90b
Julien Lebot <julien.ar.lebot@gmail.com>
2021-06-04 14:35:26 +0200

    Add gitlab job

---
+++ tasks/winbuildscripts/Update-Winget.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'Stop';
+Set-Location c:\mnt
+
+Invoke-WebRequest https://aka.ms/wingetcreate/latest -OutFile wingetcreate.exe
+
+# Install dev tools, including invoke
+pip3 install -r requirements.txt
+
+$rawAgentVersion = (inv agent.version)
+$releasePattern = "(\d+\.\d+\.\d+)"
+if ($rawAgentVersion -match $releasePattern) {
+} else {
+    Write-Host "Unsupported agent version '$rawAgentVersion', aborting"
+    exit 1
+}
+Write-Host ("Updating Winget {0}" -f $env:WINGET_GITHUB_ACCESS_TOKEN)
+exit 0
+wingetcreate.exe update --id "Datadog.Agent" --url "https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli-$($agentVersion).msi" --version $($agentVersion) --token $env:WINGET_GITHUB_ACCESS_TOKEN
