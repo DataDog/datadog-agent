69e318daa3c9c316f23f9bf022cc102a0dcea134
Julien Lebot <julien.lebot@datadoghq.com>
2021-06-04 14:50:23 +0200

    Update Update-Winget.ps1

--- tasks/winbuildscripts/Update-Winget.ps1
+++ tasks/winbuildscripts/Update-Winget.ps1
@@ -13,6 +13,5 @@ if ($rawAgentVersion -match $releasePattern) {
     Write-Host "Unsupported agent version '$rawAgentVersion', aborting"
     exit 1
 }
-Write-Host ("Updating Winget {0}" -f $env:WINGET_GITHUB_ACCESS_TOKEN)
-exit 0
+Write-Host ("Updating Winget {0}" -f $rawAgentVersion)
 wingetcreate.exe update --id "Datadog.Agent" --url "https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli-$($agentVersion).msi" --version $($agentVersion) --token $env:WINGET_GITHUB_ACCESS_TOKEN
