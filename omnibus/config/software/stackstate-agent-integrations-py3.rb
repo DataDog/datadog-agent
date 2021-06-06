873a80a236b443cea54db4fa02197e3ee62ddcbb
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2021-06-06 11:58:30 +0200

    rollback integrations py3 recipe (not sure why we cannot use newer pip-tools in linux)

--- omnibus/config/software/stackstate-agent-integrations-py3.rb
+++ omnibus/config/software/stackstate-agent-integrations-py3.rb
@@ -127,7 +127,7 @@ build do
     # install the core integrations.
     #
     command "#{pip} install wheel==0.34.1"
-    command "#{pip} install pip-tools==5.4.0"
+
     uninstall_buildtime_deps = ['rtloader', 'click', 'first', 'pip-tools']
     nix_build_env = {
       "CFLAGS" => "-I#{install_dir}/embedded/include -I/opt/mqm/inc",
@@ -144,9 +144,11 @@ build do
     # want to filter out things before installing.
     #
     if windows?
+      command "#{pip} install pip-tools==5.4.0"
       static_reqs_in_file = "#{windows_safe_path(project_dir)}\\stackstate_checks_base\\stackstate_checks\\base\\data\\#{agent_requirements_in}"
       static_reqs_out_file = "#{windows_safe_path(project_dir)}\\#{filtered_agent_requirements_in}"
     else
+      command "#{pip} install pip-tools==4.2.0"
       static_reqs_in_file = "#{project_dir}/stackstate_checks_base/stackstate_checks/base/data/#{agent_requirements_in}"
       static_reqs_out_file = "#{project_dir}/#{filtered_agent_requirements_in}"
     end
