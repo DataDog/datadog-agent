44e19f664db9718b02a1506ce65a1f13011ed995
Bohdan Lytvynov <bohdan@canva.com>
2021-06-06 13:15:21 +1000

    Prevent agressive tagger retry by keeping failed fetch in cache

--- pkg/tagger/local/tagger.go
+++ pkg/tagger/local/tagger.go
@@ -250,17 +250,16 @@ IterCollectors:
 		log.Debugf("cache miss for %s, collecting tags for %s", name, entity)
 
 		cacheMiss := false
-		skipCache := false
+		var expiryDate *time.Time
 		low, orch, high, err := collector.Fetch(entity)
 		switch {
 		case errors.IsNotFound(err):
 			log.Debugf("entity %s not found in %s, skipping: %v", entity, name, err)
 			cacheMiss = true
-		case errors.IsPartial(err):
-			skipCache = true
 		case err != nil:
 			log.Warnf("error collecting from %s: %s", name, err)
-			continue // don't store empty tags, retry next time
+			expireAfter := time.Now().Add(1 * time.Second)
+			expiryDate = &expireAfter
 		}
 
 		tagArrays = append(tagArrays, low)
@@ -280,7 +279,8 @@ IterCollectors:
 				OrchestratorCardTags: orch,
 				HighCardTags:         high,
 				CacheMiss:            cacheMiss,
-				SkipCache:            skipCache,
+				//set expiry date for cache of failed Fetch
+				ExpiryDate: expiryDate,
 			},
 		})
 	}
