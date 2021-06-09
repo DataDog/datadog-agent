fc1cea7e3ec74705566810d4b49be0f453e982de
zandrewitte <zandre.witte@gmail.com>
2021-06-09 10:45:55 +0200

    try

--- omnibus/config/software/datadog-agent-finalize.rb
+++ omnibus/config/software/datadog-agent-finalize.rb
@@ -21,13 +21,13 @@ build do
             conf_dir_root = "#{Omnibus::Config.source_dir()}/etc/stackstate-agent"
             conf_dir = "#{conf_dir_root}/extra_package_files/EXAMPLECONFSLOCATION"
             if Dir.exists?(conf_dir)
-                command "dir #{conf_dir}"
+                command "dir /q /s /t:c #{conf_dir}"
                 delete "#{conf_dir}"
-                command "dir #{conf_dir_root}/extra_package_files/"
+                command "dir /q /s /t:c #{conf_dir_root}/extra_package_files"
             end
             mkdir conf_dir
             move "#{install_dir}/etc/stackstate-agent/stackstate.yaml.example", conf_dir_root, :force=>true
-            move "#{install_dir}/etc/stackstate-agent/conf.d/*", conf_dir, :force=>true
+            move "#{install_dir}/etc/stackstate-agent/conf.d/*", conf_dir#, :force=>true
             delete "#{install_dir}/bin/agent/agent.exe"
             # TODO why does this get generated at all
             delete "#{install_dir}/bin/agent/agent.exe~"
@@ -44,13 +44,13 @@ build do
             # remove the config files for the subservices; they'll be started
             # based on the config file
             command "echo pre-delete-conf-dir"
-            command "dir #{conf_dir}"
+            command "dir /q /s /t:c #{conf_dir}"
             delete "#{conf_dir}/apm.yaml.default"
             delete "#{conf_dir}/process_agent.yaml.default"
             # load isn't supported by windows
             delete "#{conf_dir}/load.d"
             command "echo post-delete-conf-dir"
-            command "dir #{conf_dir}"
+            command "dir /q /s /t:c #{conf_dir}"
 
             # cleanup clutter
             delete "#{install_dir}/etc"
