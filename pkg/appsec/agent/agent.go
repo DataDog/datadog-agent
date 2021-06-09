7b0dc8ebe6e603408d85669d35159d70b0807642
Julio Guerra <julio@datadog.com>
2021-06-09 15:39:58 +0200

    appsec/agent: add the event pipeline

--- pkg/appsec/agent/agent.go
+++ pkg/appsec/agent/agent.go
@@ -2,12 +2,13 @@ package agent
 
 import (
 	"context"
+	"errors"
 
 	agenttypes "github.com/DataDog/datadog-agent/pkg/appsec/agent/types"
 	"github.com/DataDog/datadog-agent/pkg/appsec/api/http"
 	"github.com/DataDog/datadog-agent/pkg/appsec/backend"
 	"github.com/DataDog/datadog-agent/pkg/appsec/batch"
-	"github.com/DataDog/datadog-agent/pkg/trace/config"
+	appsecconfig "github.com/DataDog/datadog-agent/pkg/appsec/config"
 )
 
 type Agent struct {
@@ -18,26 +19,37 @@ type Agent struct {
 	eventsChan agenttypes.RawJSONEventsChan
 	eventBatch *batch.EventBatch
 
-	backendClient backend.Client
+	backendClient *backend.Client
 }
 
-func NewAgent(conf *config.AgentConfig) *Agent {
+var ErrAgentDisabled = errors.New(`AppSec agent disabled. Set the environment variable
+DD_APPSEC_ENABLED=true or add "appsec_config.enabled: true" entry
+to your datadog.yaml.`)
+
+func NewAgent(cfg *appsecconfig.Config) (*Agent, error) {
+	if !cfg.Enabled {
+		return nil, ErrAgentDisabled
+	}
 	// TODO(julio): check the channel length with proper load testing
 	eventsChan := make(agenttypes.RawJSONEventsChan, 1000)
 
-	return &Agent{
-		httpSrv:    http.NewServer(conf, eventsChan),
-		eventBatch: batch.NewEventBatch(10),
+	backendClient, err := backend.NewClient(cfg.BackendAPIKey, cfg.BackendAPIBaseURL, cfg.BackendAPIProxy)
+	if err != nil {
+		return nil, err
 	}
+
+	return &Agent{
+		httpSrv:       http.NewServer(cfg, eventsChan),
+		eventBatch:    batch.NewEventBatch(10),
+		backendClient: backendClient,
+	}, nil
 }
 
 func (a *Agent) Start(ctx context.Context) {
+	// Start the event pipeline
 	a.startEventPipeline(ctx)
-
 	// Serve the agent API
 	a.serve()
-
-	<-ctx.Done()
 }
 
 func (a *Agent) serve() {
@@ -46,7 +58,7 @@ func (a *Agent) serve() {
 
 func (a *Agent) startEventPipeline(ctx context.Context) {
 	// Send event batches to Datadog's backend
-	go a.backendClient.SendEventsFrom(ctx, a.eventBatch.Chan(), a.eventBatch.Put)
+	go a.backendClient.EventService().SendBatchesFrom(ctx, a.eventBatch.Chan(), a.eventBatch.Put)
 	// Batch the events sent to the agent API
 	go a.eventBatch.AppendFrom(ctx, a.eventsChan)
 }
