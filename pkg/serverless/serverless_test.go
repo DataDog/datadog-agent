2d9b096d9b26bba74bf08a42cbc0e99acb03f42c
maxday <maxime.david@datadoghq.com>
2021-06-10 15:28:38 -0400

    fix test

--- pkg/serverless/serverless_test.go
+++ pkg/serverless/serverless_test.go
@@ -41,7 +41,6 @@ func TestHandleInvocationShouldSetExtraTags(t *testing.T) {
 		"a3:valuea3",
 		"a4:valuea4",
 		"a_maj:valueamaj",
-		"account_id:123456789012",
 		"aws_account:123456789012",
 		"function_arn:arn:aws:lambda:us-east-1:123456789012:function:my-function",
 		"functionname:my-function",
