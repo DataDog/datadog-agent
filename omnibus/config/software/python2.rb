73999a0a934ca90f40f7954eaf25b6106dfa5088
Julien Lebot <julien.ar.lebot@gmail.com>
2021-06-14 12:07:18 +0200

    Test custom python 2 x64

--- omnibus/config/software/python2.rb
+++ omnibus/config/software/python2.rb
@@ -90,8 +90,8 @@ else
            :sha256 => "c8309b3351610a7159e91e55f09f7341bc3bbdd67d2a5e3049a9d1157e5a9110",
            :extract => :seven_zip
   else
-    source :url => "https://dd-agent-omnibus.s3.amazonaws.com/python-windows-#{version}-amd64.zip",
-         :sha256 => "7989b2efe6106a3df82c47d403dbb166db6d4040f3654871323df7e724a9fdd2",
+    source :url => "https://dd-agent-omnibus.s3.amazonaws.com/python-windows-#{version}-x64.zip",
+         :sha256 => "8E9D8F94F42F25AA7C19DF4254FC866E18EF98FBD56D5629F364B65567001ABB",
          :extract => :seven_zip
   end
   build do
