7ac1aa6efb007de523b94243c4e2866da2db18fe
Jesse Gumz <jesse.gumz@datadoghq.com>
2021-06-15 12:52:03 -0400

    pkg/trace: Add pkg/trace/flags/features package and update HasFeature calls

--- pkg/trace/test/testutil/features.go
+++ pkg/trace/test/testutil/features.go
@@ -3,14 +3,14 @@ package testutil
 import (
 	"os"
 
-	"github.com/DataDog/datadog-agent/pkg/trace/config"
+	"github.com/DataDog/datadog-agent/pkg/trace/flags/features"
 )
 
 // WithFeatures sets the given list of comma-separated features as active and
 // returns a function which resets the features to their previous state.
 func WithFeatures(feats string) (undo func()) {
-	config.SetFeatures(feats)
+	features.SetFeatures(feats)
 	return func() {
-		config.SetFeatures(os.Getenv("DD_APM_FEATURES"))
+		features.SetFeatures(os.Getenv("DD_APM_FEATURES"))
 	}
 }
