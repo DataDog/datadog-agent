de7c1a215755f9b516d0c2ff5a79be3e0a341011
Jesse Gumz <jdgumz@users.noreply.github.com>
2021-06-17 12:32:52 -0400

    pkg/trace: Normalize default env (#8417)
    * pkg/trace: Add pkg/trace/flags/features package and update HasFeature calls
    
    * pkg/trace/config: Remove old features API in pkg/trace/config, add env normalization to config loading
    
    * pkg/trace/config: Update test data and condense test logic
    
    * pkg/trace/api: Replace old config.Features() call with new features.Features() call
    
    * pkg/trace: Move features package to be under config, rename function calls for brevity
    
    * pkg/trace/config: Rename test files and modify/expand test coverage
    
    * pkg/trace/config: Add doc comment for features package
    
    * pkg/trace: Add release note
    
    * pkg/trace/config/features: Remove newline after package doc

--- pkg/trace/test/testutil/features.go
+++ pkg/trace/test/testutil/features.go
@@ -3,14 +3,14 @@ package testutil
 import (
 	"os"
 
-	"github.com/DataDog/datadog-agent/pkg/trace/config"
+	"github.com/DataDog/datadog-agent/pkg/trace/config/features"
 )
 
 // WithFeatures sets the given list of comma-separated features as active and
 // returns a function which resets the features to their previous state.
 func WithFeatures(feats string) (undo func()) {
-	config.SetFeatures(feats)
+	features.Set(feats)
 	return func() {
-		config.SetFeatures(os.Getenv("DD_APM_FEATURES"))
+		features.Set(os.Getenv("DD_APM_FEATURES"))
 	}
 }
