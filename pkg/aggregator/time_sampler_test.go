9e3c8bba3003283b97434d2fe93bc5e2cd348af4
Olivier G <52180542+ogaca-dd@users.noreply.github.com>
2021-06-22 10:55:16 +0200

    Fix check sampler (#8392)
    * Revert "Revert Expire context check sampler [8122] (#8335)"
    * Set `check_sampler_bucket_commits_count_expiry` to `2`

--- pkg/aggregator/time_sampler_test.go
+++ pkg/aggregator/time_sampler_test.go
@@ -251,13 +251,13 @@ func TestCounterExpirySeconds(t *testing.T) {
 	// Counter2 should still report
 	assert.Equal(t, 1, len(series))
 	assert.Equal(t, 1, len(sampler.counterLastSampledByContext))
-	assert.Equal(t, 2, len(sampler.contextResolver.contextsByKey))
+	assert.Equal(t, 2, len(sampler.contextResolver.resolver.contextsByKey))
 
 	series, _ = sampler.flush(1800.0)
 	// Everything stopped reporting and is expired
 	assert.Equal(t, 0, len(series))
 	assert.Equal(t, 0, len(sampler.counterLastSampledByContext))
-	assert.Equal(t, 0, len(sampler.contextResolver.contextsByKey))
+	assert.Equal(t, 0, len(sampler.contextResolver.resolver.contextsByKey))
 }
 
 func TestSketch(t *testing.T) {
