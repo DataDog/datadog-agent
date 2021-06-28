1e8525b3c3ae3dbeddadc9569e79f1e49dd500ad
Julio Guerra <julio@datadog.com>
2021-06-28 11:10:59 +0200

    trace/api/info: add /appsec to the list of endpoints

--- pkg/trace/api/info_test.go
+++ pkg/trace/api/info_test.go
@@ -112,7 +112,8 @@ func TestInfoHandler(t *testing.T) {
 		"/v0.4/services",
 		"/v0.5/traces",
 		"/profiling/v1/input",
-		"/v0.6/stats"
+		"/v0.6/stats",
+		"/appsec"
 	],
 	"feature_flags": [
 		"feature_flag"
