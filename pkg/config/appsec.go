d0b26e5192d5616d2e678a81cb843c309b07203c
Julio Guerra <julio@datadog.com>
2021-06-30 16:46:12 +0200

    config/appsec: remove unecessary calls to SetKnown

--- pkg/config/appsec.go
+++ pkg/config/appsec.go
@@ -2,8 +2,6 @@ package config
 
 // setupAppSec initializes the configuration values of the appsec agent.
 func setupAppSec(cfg Config) {
-	cfg.SetKnown("appsec_config.enabled")
-	cfg.SetKnown("appsec_config.appsec_dd_url")
 	cfg.BindEnvAndSetDefault("appsec_config.enabled", true, "DD_APPSEC_ENABLED")
 	cfg.BindEnvAndSetDefault("appsec_config.appsec_dd_url", "", "DD_APPSEC_DD_URL")
 }
