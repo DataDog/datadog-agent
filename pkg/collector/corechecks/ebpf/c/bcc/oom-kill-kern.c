e160b90933d014178c9c8fa25be05e00b1566ae1
Lénaïc Huard <lenaic.huard@datadoghq.com>
2021-07-01 16:43:09 +0200

    [oom-kill check] Support kernel older than 4.10

--- pkg/collector/corechecks/ebpf/c/bcc/oom-kill-kern.c
+++ pkg/collector/corechecks/ebpf/c/bcc/oom-kill-kern.c
@@ -10,7 +10,16 @@
  */
 BPF_HASH(oomStats, u32, struct oom_stats);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,10,0)
 int kprobe__oom_kill_process(struct pt_regs *ctx, struct oom_control *oc, const char *message) {
+
+#else
+int kprobe__oom_kill_process(struct pt_regs *ctx, struct oom_control *oc, struct task_struct *p,
+                             unsigned int points, unsigned long totalpages,
+                             const char *message) {
+
+#endif
+
     struct oom_stats zero = {};
     u32 pid = bpf_get_current_pid_tgid() >> 32;
 
@@ -20,18 +29,21 @@ int kprobe__oom_kill_process(struct pt_regs *ctx, struct oom_control *oc, const 
     // From bpf-common.h
     get_cgroup_name(s->cgroup_name, sizeof(s->cgroup_name));
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,10,0)
     struct task_struct *p = oc->chosen;
-    unsigned long totalpages;
+    s->pages = oc->totalpages;
+#else
+    s->pages = totalpages;
+#endif
 
     s->pid = pid;
     s->tpid = p->pid;
     bpf_get_current_comm(&s->fcomm, sizeof(s->fcomm));
     bpf_probe_read_str(&s->tcomm, sizeof(s->tcomm), p->comm);
-    s->pages = oc->totalpages;
 
-    #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
-      s->memcg_oom = oc->memcg != NULL ? 1 : 0;
-    #endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
+    s->memcg_oom = oc->memcg != NULL ? 1 : 0;
+#endif
 
     return 0;
 }
