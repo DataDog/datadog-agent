adab11a7c99628c634a26a9f4bc28b50c19f7bf7
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-07-08 10:09:24 +0200

    Update batch size to 5

--- pkg/collector/corechecks/snmp/config.go
+++ pkg/collector/corechecks/snmp/config.go
@@ -13,11 +13,10 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
-// Python snmp integration is using oid_batch_size=10 as default.
 // Using high oid batch size might lead to snmp calls timing out.
-// For some devices, the default oid_batch_size of 10 might be high (leads to timeouts),
-// and require manual setting oid_batch_size to a lower value e.g. 1 or 5
-var defaultOidBatchSize = 10
+// For some devices, the default oid_batch_size of 5 might be high (leads to timeouts),
+// and require manual setting oid_batch_size to a lower value.
+var defaultOidBatchSize = 5
 
 var defaultPort = uint16(161)
 var defaultRetries = 3
