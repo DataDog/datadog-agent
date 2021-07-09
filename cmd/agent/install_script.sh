b7aaea60cc505bf67c22f132de47da6a3006337b
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2021-07-09 13:44:31 +0200

    Better detect if systemd is the init system
    Not only check if it's installed, but also that it's in use as the init
    process. Some systems have systemd installed as a dependency but it's not
    in use.

--- cmd/agent/install_script.sh
+++ cmd/agent/install_script.sh
@@ -586,8 +586,8 @@ restart_cmd="$sudo_cmd $service_cmd datadog-agent restart"
 stop_instructions="$sudo_cmd $service_cmd datadog-agent stop"
 start_instructions="$sudo_cmd $service_cmd datadog-agent start"
 
-if command -v systemctl 2>&1; then
-  # Use systemd if systemctl binary exists
+if [[ `ps --no-headers -o comm 1 2>&1` == "systemd" ]] && command -v systemctl 2>&1; then
+  # Use systemd if systemctl binary exists and systemd is the init process
   restart_cmd="$sudo_cmd systemctl restart datadog-agent.service"
   stop_instructions="$sudo_cmd systemctl stop datadog-agent"
   start_instructions="$sudo_cmd systemctl start datadog-agent"
