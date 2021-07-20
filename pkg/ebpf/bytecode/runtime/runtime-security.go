a1c973240f08847ca86be688bec1c74525417dd2
Sylvain Afchain <safchain@gmail.com>
2021-07-20 17:13:30 +0200

    Fix eRPC in-kernel execution check

--- pkg/ebpf/bytecode/runtime/runtime-security.go
+++ pkg/ebpf/bytecode/runtime/runtime-security.go
@@ -3,4 +3,4 @@
 
 package runtime
 
-var RuntimeSecurity = NewRuntimeAsset("runtime-security.c", "914da4e15bce516f74935f8ef8e75deb3aaf7258fcd7933fe2804f1ec99e5356")
+var RuntimeSecurity = NewRuntimeAsset("runtime-security.c", "452b177a5ecf8b491a2e5e9d1417a38bb75b137bc04f71da1241001a9f58551d")
--- pkg/security/ebpf/c/erpc.h
+++ pkg/security/ebpf/c/erpc.h
@@ -85,7 +85,7 @@ int __attribute__((always_inline)) is_eprc_request(struct pt_regs *ctx) {
 int __attribute__((always_inline)) handle_erpc_request(struct pt_regs *ctx) {
     void *req = (void *)PT_REGS_PARM4(ctx);
 
-    u8 op;
+    u8 op = 0;
     int ret = bpf_probe_read(&op, sizeof(op), req);
     if (ret < 0) {
         return 0;
