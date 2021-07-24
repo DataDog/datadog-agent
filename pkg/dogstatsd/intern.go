ae43f886eb4e09b048924156e15ee9a86520cf71
Carlo Alberto Ferraris <cafxx@strayorange.com>
2021-07-24 20:14:16 +0900

    Update intern.go

--- pkg/dogstatsd/intern.go
+++ pkg/dogstatsd/intern.go
@@ -21,6 +21,7 @@ var (
 type stringInterner struct {
 	strings map[string]string
 	maxSize int
+	calls uint64
 	// telemetry
 	tlmEnabled bool
 }
@@ -36,8 +37,24 @@ func newStringInterner(maxSize int) *stringInterner {
 // LoadOrStore always returns the string from the cache, adding it into the
 // cache if needed.
 // If we need to store a new entry and the cache is at its maximum capacity,
-// it is reset.
+// an existing entry is randomly dropped.
 func (i *stringInterner) LoadOrStore(key []byte) string {
+	// Drop one random entry every dropInterval calls to LoadOrStore. This
+	// aims at ensuring that entries are eventually dropped even if no new
+	// entries are added.
+	// TODO: Use a LRU/LFU policy (possibly approximated via sampling).
+	i.calls++
+	if i.calls % dropInterval == 0 {
+		for k := range i.strings {
+			delete(i.strings, k)
+			break
+		}
+	}
+	
+	if len(key) == 0 {
+		return ""
+	}
+		
 	// here is the string interner trick: the map lookup using
 	// string(key) doesn't actually allocate a string, but is
 	// returning the string value -> no new heap allocation
@@ -46,13 +63,17 @@ func (i *stringInterner) LoadOrStore(key []byte) string {
 	if s, found := i.strings[string(key)]; found {
 		return s
 	}
+	
+	// If adding a new entry would bring us over the maxSize limit, randomly drop one entry.
+	// TODO: Use a LRU/LFU policy (possibly approximated via sampling).
 	if len(i.strings) >= i.maxSize {
-		i.strings = make(map[string]string)
-		log.Debug("clearing the string interner cache")
-		if i.tlmEnabled {
-			tlmSIResets.Inc()
+		for k := range i.strings {
+			delete(i.strings, k)
+			break
 		}
 	}
+	
+	// Add the new entry.
 	s := string(key)
 	i.strings[s] = s
 	return s
