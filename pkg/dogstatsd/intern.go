595a30361edcbe1ad31f19df841182ba6eba3a56
Carlo Alberto Ferraris <cafxx@strayorange.com>
2021-07-24 23:06:14 +0000

    LRU policy for the string interner

--- pkg/dogstatsd/intern.go
+++ pkg/dogstatsd/intern.go
@@ -28,22 +28,32 @@ const (
 	// This ensures that eventually old entries are evicted.
 	// It is recommended, for performance, to use a number that is power-of-2.
 	dropInterval = 256
+	// dropSample controls how many entries are sampled, at most, to pick the least
+	// recently used when an entry needs to be dropped.
+	// Higher numbers lower the chances of recently-accessed entries to be dropped,
+	// at the expense of higher CPU usage.
+	dropSample = 3
 )
 
 // stringInterner is a string cache providing a longer life for strings,
 // helping to avoid GC runs because they're re-used many times instead of
 // created every time.
 type stringInterner struct {
-	strings map[string]string
+	strings map[string]*stringEntry
 	maxSize int
-	calls   uint64
+	calls   uint
 	// telemetry
 	tlmEnabled bool
 }
 
+type stringEntry struct {
+	str        string
+	lastAccess uint
+}
+
 func newStringInterner(maxSize int) *stringInterner {
 	return &stringInterner{
-		strings:    make(map[string]string),
+		strings:    make(map[string]*stringEntry),
 		maxSize:    maxSize,
 		tlmEnabled: telemetry_utils.IsEnabled(),
 	}
@@ -61,21 +71,10 @@ func (i *stringInterner) LoadOrStore(key []byte) string {
 	// Drop one random entry every dropInterval calls to LoadOrStore. This
 	// aims at ensuring that entries are eventually dropped even if no new
 	// entries are added.
-	// TODO: Use a LRU/LFU policy (possibly approximated via sampling).
 	i.calls++
 	if i.calls%dropInterval == 0 {
-		for k := range i.strings {
-			if k == string(key) { // Temp string: should not allocate
-				// Avoid removing this entry in case it's exactly the one
-				// that we need below.
-				continue
-			}
-			delete(i.strings, k)
-			if i.tlmEnabled {
-				tlmSIEntries.Dec()
-			}
-			break // Drop a single entry.
-		}
+		// Drop a old entry, avoding the one we are going to need below.
+		i.dropOldEntry(key)
 	}
 
 	// Silly case: it's pointless to use/lookup an entry for this.
@@ -91,31 +90,51 @@ func (i *stringInterner) LoadOrStore(key []byte) string {
 	// returning the string value -> no new heap allocation
 	// for this string.
 	// See https://github.com/golang/go/commit/f5f5a8b6209f84961687d993b93ea0d397f5d5bf
-	if s, found := i.strings[string(key)]; found {
+	if entry, found := i.strings[string(key)]; found {
+		entry.lastAccess = i.calls
 		if i.tlmEnabled {
 			tlmSIHits.Inc()
 		}
-		return s
+		return entry.str
 	}
 
 	// If adding a new entry would bring us over the maxSize limit, randomly drop one entry.
-	// TODO: Use a LRU/LFU policy (possibly approximated via sampling).
 	if len(i.strings) >= i.maxSize {
-		for k := range i.strings {
-			delete(i.strings, k)
-			if i.tlmEnabled {
-				tlmSIEntries.Dec()
-			}
-			break // Drop a single entry.
-		}
+		i.dropOldEntry(nil)
 	}
 
 	// Add the new entry.
 	s := string(key)
-	i.strings[s] = s
+	i.strings[s] = &stringEntry{str: s, lastAccess: i.calls}
 	if i.tlmEnabled {
 		tlmSIEntries.Inc()
 	}
 
 	return s
+}
+
+func (i *stringInterner) dropOldEntry(excludedKey []byte) {
+	var victim *stringEntry
+	sample := dropSample
+	for _, entry := range i.strings {
+		if string(excludedKey) == entry.str { // Does not allocate.
+			// We do not consider this entry for deletion.
+			continue
+		}
+		if sample <= 0 {
+			break
+		}
+		sample--
+		if victim == nil || victim.lastAccess > entry.lastAccess {
+			// Either we have no victim yet, or the current victim has been
+			// accessed more recently than the current entry.
+			victim = entry
+		}
+	}
+	if victim != nil {
+		delete(i.strings, victim.str)
+		if i.tlmEnabled {
+			tlmSIEntries.Dec()
+		}
+	}
 }
