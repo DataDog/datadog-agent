badd7ea6665503ee1ddcaae38d5eb724443b385e
Corrina Sivak <corrina.sivak@datadoghq.com>
2021-07-26 14:06:19 -0400

    Added setup config, (support for other runtime values included in the box!) :)

--- cmd/process-agent/main.go
+++ cmd/process-agent/main.go
@@ -4,10 +4,41 @@ package main
 
 import (
 	"flag"
+	"github.com/DataDog/datadog-agent/cmd/agent/common"
+	"log"
 	_ "net/http/pprof"
+	"strings"
 
 	"github.com/DataDog/datadog-agent/cmd/process-agent/flags"
 )
+
+func setupConfig() {
+	verb, key, value :=
+		strings.ToLower(flag.Arg(1)),
+		strings.ToLower(flag.Arg(2)),
+		flag.Arg(3)
+
+	settingsClient, err := common.NewSettingsClient()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if verb == "get" {
+		get, err := settingsClient.Get(key)
+		if err == nil {
+			log.Println(get)
+		}
+	} else if verb == "set" {
+		_, err = settingsClient.Set(key, value)
+	} else {
+		log.Println("Usage: process-agent config {get setting, set setting value}")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
 
 func main() {
 	ignore := ""
@@ -24,6 +55,11 @@ func main() {
 	flag.StringVar(&opts.check, "check", "", "Run a specific check and print the results. Choose from: process, connections, realtime")
 	flag.StringVar(&opts.logLevel, "logLevel", "", "Set the log level for a running process agent.")
 	flag.Parse()
+
+	if flag.Arg(0) == "config" {
+		setupConfig()
+		return
+	}
 
 	exit := make(chan struct{})
 
