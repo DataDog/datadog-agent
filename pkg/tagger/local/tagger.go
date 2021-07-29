07e8d3acd0beed2ca9ef4ef1f357d2f45717749f
Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>
2021-07-29 18:14:37 +0200

    Revert Do not spawn tagger pull in a background goroutine (#8736) (#8740)

--- pkg/tagger/local/tagger.go
+++ pkg/tagger/local/tagger.go
@@ -124,9 +124,9 @@ func (t *Tagger) run() error {
 		case msg := <-t.infoIn:
 			t.store.processTagInfo(msg)
 		case <-t.retryTicker.C:
-			t.startCollectors(ctx)
+			go t.startCollectors(ctx)
 		case <-t.pullTicker.C:
-			t.pull(ctx)
+			go t.pull(ctx)
 		case <-t.pruneTicker.C:
 			t.store.prune()
 		case <-t.telemetryTicker.C:
