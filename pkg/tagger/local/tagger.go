57821733620249a8a0265944713a6199045a2001
Julio Greff <julio.greff@datadoghq.com>
2021-08-02 15:39:22 +0200

    [Tagger] Better telemetry for queries and fetches
    * Add a `status` tag to the `tagger.queries` metric showing whether the
    query succeeded, found no tags, or was called with an empty ID.
    
    * Add a `tagger.fetches` metric that counts the number of fetches caused
    by queries. This gives us better visibility into the amount of cache
    misses in the tagger, when they error out, and when they find no tags.

--- pkg/tagger/local/tagger.go
+++ pkg/tagger/local/tagger.go
@@ -241,17 +241,18 @@ func (t *Tagger) Stop() error {
 
 // getTags returns a read only list of tags for a given entity.
 func (t *Tagger) getTags(entity string, cardinality collectors.TagCardinality) ([]string, error) {
-	telemetry.Queries.Inc(collectors.TagCardinalityToString(cardinality))
-
 	if entity == "" {
+		telemetry.Queries.Inc(collectors.TagCardinalityToString(cardinality), telemetry.QueryEmptyEntityID)
 		return nil, fmt.Errorf("empty entity ID")
 	}
+
 	cachedTags, sources := t.store.lookup(entity, cardinality)
 
 	if len(sources) == len(t.fetchers) {
-		// All sources sent data to cache
+		telemetry.Queries.Inc(collectors.TagCardinalityToString(cardinality), telemetry.QuerySuccess)
 		return cachedTags, nil
 	}
+
 	// Else, partial cache miss, query missing data
 	// TODO: get logging on that to make sure we should optimize
 	tagArrays := [][]string{cachedTags}
@@ -269,17 +270,23 @@ IterCollectors:
 
 		var cacheMiss bool
 		var expiryDate time.Time
+
 		low, orch, high, err := collector.Fetch(context.TODO(), entity)
+
 		switch {
 		case errors.IsNotFound(err):
 			log.Debugf("entity %s not found in %s, skipping: %v", entity, name, err)
+			telemetry.Fetches.Inc(name, telemetry.FetchNotFound)
 
 			cacheMiss = true
 			expiryDate = time.Now().Add(notFoundTTL)
 		case err != nil:
 			log.Warnf("error collecting from %s: %s", name, err)
+			telemetry.Fetches.Inc(name, telemetry.FetchError)
 
 			expiryDate = time.Now().Add(errTTL)
+		default:
+			telemetry.Fetches.Inc(name, telemetry.FetchSuccess)
 		}
 
 		tagArrays = append(tagArrays, low)
@@ -305,7 +312,15 @@ IterCollectors:
 	}
 	t.RUnlock()
 
-	return utils.ConcatenateTags(tagArrays), nil
+	tags := utils.ConcatenateTags(tagArrays)
+
+	if len(tags) > 0 {
+		telemetry.Queries.Inc(collectors.TagCardinalityToString(cardinality), telemetry.QuerySuccess)
+	} else {
+		telemetry.Queries.Inc(collectors.TagCardinalityToString(cardinality), telemetry.QueryEmptyTags)
+	}
+
+	return tags, nil
 }
 
 // TagBuilder appends tags for a given entity from the tagger to the TagsBuilder
