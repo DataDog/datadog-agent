fe0c41e2971105d621f42e4c6d4eece103115c9d
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-08-04 14:21:51 +0200

    revert

--- pkg/config/autodiscovery/autodiscovery.go
+++ pkg/config/autodiscovery/autodiscovery.go
@@ -66,10 +66,5 @@ func DiscoverComponentsFromEnv() ([]config.ConfigurationProviders, []config.List
 		log.Info("Adding ECS Fargate autodiscovery provider and listener from environment")
 	}
 
-	if config.IsFeaturePresent(config.Ndm) {
-		detectedListeners = append(detectedListeners, config.Listeners{Name: "snmp"})
-		log.Info("Adding SNMP listener from environment")
-	}
-
 	return detectedProviders, detectedListeners
 }
