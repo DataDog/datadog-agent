40d2097d40a89ecbe6a94a91469db9f5a78743d9
Derek Brown <derek.brown@datadoghq.com>
2021-08-09 09:55:18 -0700

    add some helpful driver status to flare
    Fix testing error
    
    Use new(er) built-in shutil functions instead of rolling our own.  Also
    allows getting rid of vet-busting side-code
    
    fix missing return

--- pkg/flare/archive.go
+++ pkg/flare/archive.go
@@ -302,6 +302,10 @@ func createArchive(confSearchPaths SearchPaths, local bool, zipFilePath string, 
 	if err != nil {
 		log.Errorf("Could not export Windows event logs: %s", err)
 	}
+	err = zipServiceStatus(tempDir, hostname)
+	if err != nil {
+		log.Errorf("Could not export Windows driver status: %s", err)
+	}
 
 	// force a log flush before zipping them
 	log.Flush()
