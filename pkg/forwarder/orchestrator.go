c5dbeca1212368604ba3ec65a537f9cee64c01a2
nam <nam.nguyen@datadoghq.com>
2021-08-10 11:55:39 +0200

    alternative scope

--- pkg/forwarder/orchestrator.go
+++ pkg/forwarder/orchestrator.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-2021 Datadog, Inc.
 
+// +build kubelet,orchestrator
+
 package forwarder
 
 import (
