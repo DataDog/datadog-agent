9983f67510d273f0f6d471eef8881aab0c8faf8e
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2021-08-11 17:54:09 +0200

    [so/testing] add testutils/procfs directory image of /proc for testing purpose
    Remove local /proc test as CI may not have awk, egrep tools installed

--- pkg/network/so/so_test.go
+++ pkg/network/so/so_test.go
@@ -1,107 +1,44 @@
 package so
 
 import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"os"
-	"os/exec"
 	"strconv"
 	"testing"
 
+	"github.com/DataDog/datadog-agent/pkg/network/so/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
-func procBashReadPidMapsPathname(t *testing.T, pid int, cmdlineExtra ...string) []string {
-	cmdline := fmt.Sprintf("awk '{print $6}' /proc/%d/maps | sort -u | grep -v '^\\['", pid)
-	if len(cmdlineExtra) > 0 {
-		cmdline = cmdline + cmdlineExtra[0]
-	}
-	cmd := exec.Command("bash", "-c", cmdline)
-	out, err := cmd.Output()
-	if err != nil {
-		t.Fatalf("exec Command failed '%s' %v", cmd.String(), err)
-	}
+func TestForeachPIDNoFilter(t *testing.T) {
+	for _, p := range testutils.ProcFS {
+		t.Log("testing PID :", p.Pid)
 
-	libs := []string{}
-	scanner := bufio.NewScanner(bytes.NewBuffer(out))
-	for scanner.Scan() {
-		line := scanner.Text()
-		if len(line) > 0 {
-			libs = append(libs, scanner.Text())
+		libs := Find("testutils/procfs/"+strconv.Itoa(p.Pid), nil)
+		libPathname := []string{}
+		for _, lib := range libs {
+			libPathname = append(libPathname, lib.Pathname)
 		}
+
+		expected := p.Libraries
+		assert.ElementsMatch(t, expected, libPathname)
 	}
-	if err := scanner.Err(); err != nil {
-		t.Fatal("reading output Command error", err)
-	}
-	return libs
 }
 
-func TestProcSelfNoFilter(t *testing.T) {
-	pid := os.Getpid()
-	pidStr := strconv.Itoa(pid)
-	libs := Find("/proc/"+pidStr, nil)
+func TestForeachPIDAllLibrariesFilter(t *testing.T) {
+	for _, p := range testutils.ProcFS {
+		t.Log("testing PID :", p.Pid)
 
-	libPathname := []string{}
-	for _, lib := range libs {
-		libPathname = append(libPathname, lib.Pathname)
-	}
+		libs := Find("testutils/procfs/"+strconv.Itoa(p.Pid), AllLibraries)
+		libPathname := []string{}
+		for _, lib := range libs {
+			libPathname = append(libPathname, lib.Pathname)
+		}
 
-	expected := procBashReadPidMapsPathname(t, pid)
-	assert.ElementsMatch(t, expected, libPathname)
-}
-
-func TestProcSelfFilterAllLibraries(t *testing.T) {
-	pid := os.Getpid()
-	pidStr := strconv.Itoa(pid)
-	libs := Find("/proc/"+pidStr, AllLibraries)
-
-	libPathname := []string{}
-	for _, lib := range libs {
-		libPathname = append(libPathname, lib.Pathname)
-	}
-
-	expected := procBashReadPidMapsPathname(t, pid, fmt.Sprintf(" | egrep '%s'", AllLibraries.String()))
-
-	assert.ElementsMatch(t, expected, libPathname)
-}
-
-func TestProcSelfvsAllPidsNoFilter(t *testing.T) {
-	pid := os.Getpid()
-	pidStr := strconv.Itoa(pid)
-
-	libs := Find("/proc", nil)
-
-	libPathname := []string{}
-	for _, lib := range libs {
-		for _, p := range lib.PidsPath {
-			if p == "/proc/"+pidStr {
-				libPathname = append(libPathname, lib.Pathname)
+		expected := []string{}
+		for _, lib := range p.Libraries {
+			if AllLibraries.MatchString(lib) {
+				expected = append(expected, lib)
 			}
 		}
+		assert.ElementsMatch(t, expected, libPathname)
 	}
-
-	expected := procBashReadPidMapsPathname(t, pid)
-
-	assert.ElementsMatch(t, expected, libPathname)
-}
-
-func TestProcSelfvsAllPidsAllLibrariesFilter(t *testing.T) {
-	pid := os.Getpid()
-	pidStr := strconv.Itoa(pid)
-
-	libs := Find("/proc", AllLibraries)
-
-	libPathname := []string{}
-	for _, lib := range libs {
-		for _, p := range lib.PidsPath {
-			if p == "/proc/"+pidStr {
-				libPathname = append(libPathname, lib.Pathname)
-			}
-		}
-	}
-
-	expected := procBashReadPidMapsPathname(t, pid, fmt.Sprintf(" | egrep '%s'", AllLibraries.String()))
-
-	assert.ElementsMatch(t, expected, libPathname)
 }
