9f6922012743de3e4d007897b92a7ec43368e97b
Corrina Sivak <corrina.sivak@datadoghq.com>
2021-08-12 16:30:29 -0400

    rough guestimate of what new command structure looks like

--- cmd/process-agent/main.go
+++ cmd/process-agent/main.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/DataDog/datadog-agent/cmd/agent/common/commands"
 	"github.com/DataDog/datadog-agent/cmd/process-agent/flags"
@@ -15,15 +14,37 @@ import (
 )
 
 var (
-	RootCmd = &cobra.Command{
+	rootCmd = &cobra.Command{
 		Run: func(_ *cobra.Command, _ []string) {
-			exit := make(chan struct{})
+			startAgent()
+		},
+		Long: "The Datadog Agent faithfully collects information about running processes " +
+			"and sends them to Datadog on your behalf",
+	}
 
-			// Invoke the Agent
-			runAgent(exit)
+	configCmd = commands.Config(setupConfigClient)
+	checkCmd  = &cobra.Command{
+		Use:       "check {process|connections|realtime}",
+		Args:      cobra.ExactValidArgs(1),
+		ValidArgs: []string{"process", "connections", "realtime"},
+		Run: func(_ *cobra.Command, args []string) {
+			opts.check = args[1]
+			startAgent()
+		},
+	}
+	infoCmd = &cobra.Command{
+		Use: "info",
+		Run: func(_ *cobra.Command, args []string) {
+			opts.info = true
+			startAgent()
 		},
 	}
 )
+
+func startAgent() {
+	exit := make(chan struct{})
+	runAgent(exit)
+}
 
 func setupConfigClient() (settings.Client, error) {
 	cfg := config.NewDefaultAgentConfig(false)
@@ -36,24 +57,26 @@ func setupConfigClient() (settings.Client, error) {
 
 func init() {
 	ignore := ""
-	flag.StringVar(&opts.configPath, "config", flags.DefaultConfPath, "Path to datadog.yaml config")
-	flag.StringVar(&ignore, "ddconfig", "", "[deprecated] Path to dd-agent config")
 
 	if flags.DefaultSysProbeConfPath != "" {
-		flag.StringVar(&opts.sysProbeConfigPath, "sysprobe-config", flags.DefaultSysProbeConfPath, "Path to system-probe.yaml config")
+		rootCmd.PersistentFlags().StringVar(&opts.sysProbeConfigPath, "sysprobe-config", flags.DefaultSysProbeConfPath, "Path to system-probe.yaml config")
 	}
 
-	flag.StringVar(&opts.pidfilePath, "pid", "", "Path to set pidfile for process")
-	flag.BoolVar(&opts.info, "info", false, "Show info about running process agent and exit")
-	flag.BoolVar(&opts.version, "version", false, "Print the version and exit")
-	flag.StringVar(&opts.check, "check", "", "Run a specific check and print the results. Choose from: process, connections, realtime")
-	flag.Parse()
+	rootCmd.LocalFlags().StringVar(&opts.pidfilePath, "pid", "", "Path to set pidfile for process")
+	rootCmd.LocalFlags().BoolVar(&opts.version, "version", false, "Print the version and exit")
 
-	RootCmd.AddCommand(commands.Config(setupConfigClient))
+	// Deprecated Commands
+	rootCmd.LocalFlags().StringVar(&opts.check, "check", "", "[deprecated] Run a specific check and print the results. Choose from: process, connections, realtime")
+	rootCmd.LocalFlags().StringVar(&opts.configPath, "config", flags.DefaultConfPath, "[deprecated] Path to datadog.yaml config")
+	rootCmd.LocalFlags().StringVar(&ignore, "ddconfig", "", "[deprecated] Path to dd-agent config")
+	rootCmd.LocalFlags().BoolVar(&opts.info, "info", false, "[deprecated] Show info about running process agent and exit")
+	rootCmd.PersistentFlags().StringVar(&opts.configPath, "cfgPath", flags.DefaultConfPath, "Path to datadog.yaml config")
+
+	rootCmd.AddCommand(configCmd, checkCmd, infoCmd)
 }
 
 func main() {
-	err := RootCmd.Execute()
+	err := rootCmd.Execute()
 	if err != nil {
 		fmt.Println(err)
 	}
