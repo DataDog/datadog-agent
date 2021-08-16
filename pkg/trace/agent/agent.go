ac068e83f93650872f54c067d251ed117e8306ff
Oskar Jung <ojung@users.noreply.github.com>
2021-08-16 12:39:07 +0200

    pkg/trace/api: add live debugger proxy endpoint (#8814)
    Add a proxy to forward live debugger requests (apm_config.debugger_dd_url)

--- pkg/trace/agent/agent.go
+++ pkg/trace/agent/agent.go
@@ -24,6 +24,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/trace/stats"
 	"github.com/DataDog/datadog-agent/pkg/trace/traceutil"
 	"github.com/DataDog/datadog-agent/pkg/trace/writer"
+	"github.com/DataDog/datadog-agent/pkg/util/fargate"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
@@ -295,7 +296,7 @@ func (a *Agent) Process(p *api.Payload) {
 	}
 	if len(envtraces) > 0 {
 		in := stats.Input{Traces: envtraces}
-		if !features.Has("disable_cid_stats") && a.conf.IsFargate {
+		if !features.Has("disable_cid_stats") && a.conf.FargateOrchestrator != fargate.Unknown {
 			// only allow the ContainerID stats dimension if we're in a Fargate instance
 			// and it's not prohibited by the disable_cid_stats feature flag.
 			in.ContainerID = p.ContainerID
@@ -307,7 +308,7 @@ func (a *Agent) Process(p *api.Payload) {
 var _ api.StatsProcessor = (*Agent)(nil)
 
 func (a *Agent) processStats(in pb.ClientStatsPayload, lang, tracerVersion string) pb.ClientStatsPayload {
-	if features.Has("disable_cid_stats") || !a.conf.IsFargate {
+	if features.Has("disable_cid_stats") || a.conf.FargateOrchestrator == fargate.Unknown {
 		// this functionality is disabled by the disable_cid_stats feature flag
 		// or we're not in a Fargate instance.
 		in.ContainerID = ""
