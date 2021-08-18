1fe49abfd5d3588bd3c51495927dd9804a11f53d
Srdjan Grubor <sgnn7@sgnn7.org>
2021-08-18 16:31:19 -0500

    [pkg/collector/worker] Added UtilizationTracker class
    This class can use the SlidingWindow and internal time tracking to
    properly evaluate the utilization stats and to publish them to expvars.

---
+++ pkg/collector/worker/utilization_tracker.go
@@ -0,0 +1,187 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package worker
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/collector/runner/expvars"
+	"github.com/DataDog/datadog-agent/pkg/util"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+const (
+	UtilizationWarningThreshold = 0.99
+)
+
+// utilizationTracker is the object that polls and evaluates utilization
+// statistics which then get published to expvars.
+type utilizationTracker struct {
+	sync.Mutex
+
+	workerName string
+	started    bool
+	stopped    bool
+
+	utilizationStats util.SlidingWindow
+	pollingFunc      util.PollingFunc
+	statsUpdateFunc  util.CallbackFunc
+
+	busyDuration time.Duration
+	checkStart   time.Time
+	windowStart  time.Time
+}
+
+// UtilizationTracker is the interface that encapsulates the API around the
+// utilizationTracker object.
+type UtilizationTracker interface {
+	Start() error
+	Stop() error
+
+	CheckStarted()
+	CheckFinished()
+}
+
+// NewUtilizationTracker instantiates and configures a utilization tracker that
+// calculates the values and publishes them to expvars
+func NewUtilizationTracker(
+	workerName string,
+	windowSize time.Duration,
+	pollingInterval time.Duration,
+) (UtilizationTracker, error) {
+	sw, err := util.NewSlidingWindow(windowSize, pollingInterval)
+	if err != nil {
+		return nil, err
+	}
+
+	ut := &utilizationTracker{
+		workerName:       workerName,
+		utilizationStats: sw,
+
+		busyDuration: time.Duration(0),
+		checkStart:   time.Time{},
+		windowStart:  time.Now(),
+	}
+
+	ut.pollingFunc = func() float64 {
+		ut.Lock()
+		defer ut.Unlock()
+
+		currentTime := time.Now()
+
+		if !ut.checkStart.IsZero() {
+			duration := currentTime.Sub(ut.checkStart)
+			ut.busyDuration += duration
+			ut.checkStart = currentTime
+		}
+
+		pollingWindowDuration := currentTime.Sub(ut.windowStart)
+		ut.windowStart = currentTime
+
+		utilization := float64(ut.busyDuration) / float64(pollingWindowDuration)
+		ut.busyDuration = time.Duration(0)
+
+		return utilization
+	}
+
+	ut.statsUpdateFunc = func(sw util.SlidingWindow) {
+		utilization := sw.Average()
+
+		if utilization > UtilizationWarningThreshold {
+			log.Warnf(
+				"Worker '%s' utilization level (%.2f) is above the predefined threshold (%.2f)",
+				workerName,
+				utilization,
+				UtilizationWarningThreshold,
+			)
+		}
+
+		expvars.SetWorkerStats(workerName, &expvars.WorkerStats{
+			Utilization: utilization,
+		})
+	}
+
+	return ut, nil
+}
+
+// Start activates the polling ticker for collection of data
+func (ut *utilizationTracker) Start() error {
+	ut.Lock()
+	defer ut.Unlock()
+
+	if ut.started {
+		return fmt.Errorf("Attempted to use UtilizationTracker.Start() when the tracker was already started")
+	}
+
+	if ut.stopped {
+		return fmt.Errorf("Attempted to use UtilizationTracker.Start() when the tracker after it was stopped")
+	}
+
+	// Initialize the worker expvar
+	expvars.SetWorkerStats(
+		ut.workerName,
+		&expvars.WorkerStats{
+			Utilization: 0,
+		},
+	)
+
+	// Start the ticker
+	err := ut.utilizationStats.Start(ut.pollingFunc, ut.statsUpdateFunc)
+	if err != nil {
+		return err
+	}
+
+	ut.started = true
+
+	return nil
+}
+
+// Stop should be invoked when a worker is about to exit
+// so that we can remove the instance's expvars
+func (ut *utilizationTracker) Stop() error {
+	ut.Lock()
+	defer ut.Unlock()
+
+	if !ut.started {
+		return fmt.Errorf("Attempted to use UtilizationTracker.Stop() when the tracker was never started")
+	}
+
+	if ut.stopped {
+		return fmt.Errorf("Attempted to use UtilizationTracker.Stop() when the tracker was already stopped")
+	}
+
+	ut.stopped = true
+
+	ut.utilizationStats.Stop()
+	expvars.DeleteWorkerStats(ut.workerName)
+
+	return nil
+}
+
+// CheckStarted should be invoked when a worker's check is about to
+// run so that we can track the start time and the utilization
+func (ut *utilizationTracker) CheckStarted() {
+	ut.Lock()
+
+	ut.checkStart = time.Now()
+
+	ut.Unlock()
+}
+
+// CheckFinished should be invoked when a worker's check is complete
+// so that we can calculate the utilization of the linked worker
+func (ut *utilizationTracker) CheckFinished() {
+	ut.Lock()
+
+	duration := time.Now().Sub(ut.checkStart)
+	ut.busyDuration += duration
+
+	ut.checkStart = time.Time{}
+
+	ut.Unlock()
+}
---
+++ pkg/collector/worker/utilization_tracker_test.go
@@ -0,0 +1,244 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package worker
+
+import (
+	"expvar"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/DataDog/datadog-agent/pkg/collector/runner/expvars"
+)
+
+// Helpers
+
+// getWorkerUtilizationExpvar returns the utilization as presented by expvars
+// for a particular named worker. Since we use this in `worker_test` too, the
+// method is public
+func getWorkerUtilizationExpvar(t *testing.T, name string) float64 {
+	runnerMapExpvar := expvar.Get("runner")
+	require.NotNil(t, runnerMapExpvar)
+
+	workersExpvar := runnerMapExpvar.(*expvar.Map).Get("Workers")
+	require.NotNil(t, workersExpvar)
+
+	instancesExpvar := workersExpvar.(*expvar.Map).Get("Instances")
+	require.NotNil(t, instancesExpvar)
+
+	workerStatsExpvar := instancesExpvar.(*expvar.Map).Get(name)
+	require.NotNil(t, workerStatsExpvar)
+
+	workerStats := workerStatsExpvar.(*expvars.WorkerStats)
+	require.NotNil(t, workerStats)
+
+	return workerStats.Utilization
+}
+
+func newTracker(t *testing.T) UtilizationTracker {
+	ut, err := NewUtilizationTracker("worker", 50*time.Millisecond, 10*time.Millisecond)
+	require.Nil(t, err)
+	AssertAsyncWorkerCount(t, 0)
+
+	return ut
+}
+
+// Tests
+
+func TestUtilizationTracker(t *testing.T) {
+	ut := newTracker(t)
+
+	require.NoError(t, ut.Start())
+	defer func() {
+		ut.Stop()
+		AssertAsyncWorkerCount(t, 0)
+	}()
+
+	AssertAsyncWorkerCount(t, 1)
+
+	// Initially and after some time without any checks running, the utilization
+	// should be a constant zero value
+	require.Equal(t, 0.0, getWorkerUtilizationExpvar(t, "worker"))
+
+	time.Sleep(60 * time.Millisecond)
+	require.Equal(t, 0.0, getWorkerUtilizationExpvar(t, "worker"))
+
+	// Ramp up the expected utilization
+	ut.CheckStarted()
+
+	time.Sleep(25 * time.Millisecond)
+	require.True(t, getWorkerUtilizationExpvar(t, "worker") > 0)
+	require.True(t, getWorkerUtilizationExpvar(t, "worker") < 1)
+
+	time.Sleep(50 * time.Millisecond)
+	require.Equal(t, 1.0, getWorkerUtilizationExpvar(t, "worker"))
+
+	// Ramp down the expected utilization
+	ut.CheckFinished()
+
+	time.Sleep(25 * time.Millisecond)
+	require.True(t, getWorkerUtilizationExpvar(t, "worker") > 0)
+	require.True(t, getWorkerUtilizationExpvar(t, "worker") < 1)
+
+	time.Sleep(50 * time.Millisecond)
+	require.Equal(t, 0.0, getWorkerUtilizationExpvar(t, "worker"))
+}
+
+func TestUtilizationTrackerStart(t *testing.T) {
+	ut := newTracker(t)
+
+	require.NoError(t, ut.Start())
+	defer func() {
+		ut.Stop()
+		AssertAsyncWorkerCount(t, 0)
+	}()
+
+	AssertAsyncWorkerCount(t, 1)
+	require.Equal(t, 0.0, getWorkerUtilizationExpvar(t, "worker"))
+
+	// Check that on consecutive calls we don't break
+	require.Error(t, ut.Start())
+	require.Error(t, ut.Start())
+
+	AssertAsyncWorkerCount(t, 1)
+}
+
+func TestUtilizationTrackerStop(t *testing.T) {
+	ut := newTracker(t)
+
+	// If we haven't started yet, stopping should throw an error
+	require.Error(t, ut.Stop())
+
+	require.NoError(t, ut.Start())
+	defer func() {
+		ut.Stop()
+		AssertAsyncWorkerCount(t, 0)
+	}()
+
+	AssertAsyncWorkerCount(t, 1)
+	require.Equal(t, 0.0, getWorkerUtilizationExpvar(t, "worker"))
+
+	require.NoError(t, ut.Stop())
+	AssertAsyncWorkerCount(t, 0)
+
+	// Check that on consecutive calls we don't break
+	require.Error(t, ut.Stop())
+	require.Error(t, ut.Stop())
+	AssertAsyncWorkerCount(t, 0)
+
+	// A stopped tracker should not be able to start again
+	require.Error(t, ut.Start())
+}
+
+func TestUtilizationTrackerCheckLifecycle(t *testing.T) {
+	ut, err := NewUtilizationTracker("worker", 50*time.Millisecond, 10*time.Millisecond)
+	require.Nil(t, err)
+	AssertAsyncWorkerCount(t, 0)
+
+	require.NoError(t, ut.Start())
+	defer func() {
+		ut.Stop()
+		AssertAsyncWorkerCount(t, 0)
+	}()
+
+	// No tasks should equal no utilization
+	time.Sleep(50 * time.Millisecond)
+	AssertAsyncWorkerCount(t, 1)
+	require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 0, 0)
+
+	for idx := 0; idx < 10; idx++ {
+		// Ramp up utilization
+		ut.CheckStarted()
+
+		time.Sleep(25 * time.Millisecond)
+		AssertAsyncWorkerCount(t, 1)
+		require.True(t, getWorkerUtilizationExpvar(t, "worker") > 0.1)
+		require.True(t, getWorkerUtilizationExpvar(t, "worker") < 0.9)
+
+		time.Sleep(50 * time.Millisecond)
+		AssertAsyncWorkerCount(t, 1)
+		require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 1, 0.05)
+
+		// Ramp down utilization
+		ut.CheckFinished()
+
+		time.Sleep(25 * time.Millisecond)
+		AssertAsyncWorkerCount(t, 1)
+		require.True(t, getWorkerUtilizationExpvar(t, "worker") > 0.1)
+		require.True(t, getWorkerUtilizationExpvar(t, "worker") < 0.9)
+
+		time.Sleep(50 * time.Millisecond)
+		AssertAsyncWorkerCount(t, 1)
+		require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 0, 0.05)
+	}
+}
+
+func TestUtilizationTrackerAccuracy(t *testing.T) {
+	ut, err := NewUtilizationTracker("worker", 500*time.Millisecond, 20*time.Millisecond)
+	require.Nil(t, err)
+	AssertAsyncWorkerCount(t, 0)
+
+	require.NoError(t, ut.Start())
+	defer func() {
+		ut.Stop()
+		AssertAsyncWorkerCount(t, 0)
+	}()
+
+	require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 0, 0)
+
+	go func() {
+		// This should provide about 30% utilization
+		for {
+			totalMs := rand.Int31n(100)
+			runtimeMs := (totalMs * 30) / 100
+
+			ut.CheckStarted()
+			runtimeDuration := time.Duration(time.Duration(runtimeMs) * time.Millisecond)
+			time.Sleep(runtimeDuration)
+
+			ut.CheckFinished()
+			idleDuration := time.Duration(time.Duration(totalMs-runtimeMs) * time.Millisecond)
+			time.Sleep(idleDuration)
+		}
+	}()
+
+	for checkIdx := 0; checkIdx < 10; checkIdx++ {
+		time.Sleep(100 * time.Millisecond)
+		require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 0.3, 0.2)
+	}
+
+	require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 0.3, 0.03)
+}
+
+func TestUtilizationTrackerLongTaskAccuracy(t *testing.T) {
+	var previousUtilization, currentUtilization float64
+
+	ut, err := NewUtilizationTracker("worker", 1*time.Second, 50*time.Millisecond)
+	require.Nil(t, err)
+	AssertAsyncWorkerCount(t, 0)
+
+	require.NoError(t, ut.Start())
+	defer func() {
+		ut.Stop()
+		AssertAsyncWorkerCount(t, 0)
+	}()
+
+	require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 0, 0)
+
+	go ut.CheckStarted()
+
+	for checkIdx := 0; checkIdx < 10; checkIdx++ {
+		time.Sleep(100 * time.Millisecond)
+
+		currentUtilization = getWorkerUtilizationExpvar(t, "worker")
+		require.True(t, getWorkerUtilizationExpvar(t, "worker") > previousUtilization)
+		previousUtilization = currentUtilization
+	}
+
+	require.InDelta(t, getWorkerUtilizationExpvar(t, "worker"), 1.0, 0.05)
+}
