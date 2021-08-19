de1c2b4d3655ad07f52aa5495b3ee8e5f994f4a5
Rémy Mathieu <remy.mathieu@datadoghq.com>
2021-08-19 17:57:51 +0200

    [aggregator/ckey] fix collision unit test broken after a merge into `main`. (#8939)

--- pkg/aggregator/ckey/tests/collisions_test.go
+++ pkg/aggregator/ckey/tests/collisions_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/DataDog/datadog-agent/pkg/aggregator/ckey"
+	"github.com/DataDog/datadog-agent/pkg/util"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,7 +31,7 @@ func TestCollisions(t *testing.T) {
 		metricName := parts[0]
 		tagList := parts[1]
 		tags := strings.Split(tagList, " ")
-		ck := generator.Generate(metricName, host, tags)
+		ck := generator.Generate(metricName, host, util.NewTagsBuilderFromSlice(tags))
 		if v, exists := cache[ck]; exists {
 			assert.Fail("A collision happened:", v, "and", line)
 		} else {
