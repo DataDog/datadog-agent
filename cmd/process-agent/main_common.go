ea398932835e58c228b601de1b2ad5703430d957
Corrina Sivak <6798352+just-chillin@users.noreply.github.com>
2021-08-25 09:37:23 -0400

    Silenced config client and nil logs (#8982)
    * Silenced config client and nil logs
    
    * Explicitly ignored returned error on line 388
    
    * Explicitly ignored returned error
    
    * Removed dependency on rtloader
    
    * Removed initRuntimeSettings from default agent config since the config does not need to know about runtime settings.
    
    * Exported config.LoadConfigIfExists so that we can take in a directory containing datadog.yaml instead of only a path to the file itself.
    
    * Added extra detail for registerRuntimeSetting log and moved initRuntimeSettings to after the log is set up

--- cmd/process-agent/main_common.go
+++ cmd/process-agent/main_common.go
@@ -74,8 +74,15 @@ var (
 )
 
 func getSettingsClient() (settings.Client, error) {
-	// Set up the config in case the cmd_port was specified
-	_, err := config.NewAgentConfig(loggerName, opts.configPath, opts.sysProbeConfigPath)
+	// Set up the config so we can get the port later
+	// We set this up differently from the main process-agent because this way is quieter
+	cfg := config.NewDefaultAgentConfig(false)
+	if opts.configPath != "" {
+		if err := config.LoadConfigIfExists(opts.configPath); err != nil {
+			return nil, err
+		}
+	}
+	err := cfg.LoadProcessYamlConfig(opts.configPath)
 	if err != nil {
 		return nil, err
 	}
