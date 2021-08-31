865235ca08e6598eab7b08f4c8d710392101aca3
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-08-31 18:54:35 +0200

    [runtime-security] freeze kernel version to base syscall generator on

--- pkg/security/probe/syscalls.go
+++ pkg/security/probe/syscalls.go
@@ -5,8 +5,8 @@
 
 // +build linux
 
-//go:generate go run github.com/DataDog/datadog-agent/pkg/security/probe/syscall_table_generator -table-url https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/entry/syscalls/syscall_64.tbl -output syscalls_linux_amd64.go -output-string syscalls_string_linux_amd64.go -abis common,64
-//go:generate go run github.com/DataDog/datadog-agent/pkg/security/probe/syscall_table_generator -table-url https://raw.githubusercontent.com/torvalds/linux/master/arch/arm/tools/syscall.tbl -output syscalls_linux_arm64.go -output-string syscalls_string_linux_arm64.go -abis common,oabi,eabi
+//go:generate go run github.com/DataDog/datadog-agent/pkg/security/probe/syscall_table_generator -table-url https://raw.githubusercontent.com/torvalds/linux/v5.14/arch/x86/entry/syscalls/syscall_64.tbl -output syscalls_linux_amd64.go -output-string syscalls_string_linux_amd64.go -abis common,64
+//go:generate go run github.com/DataDog/datadog-agent/pkg/security/probe/syscall_table_generator -table-url https://raw.githubusercontent.com/torvalds/linux/v5.14/arch/arm/tools/syscall.tbl -output syscalls_linux_arm64.go -output-string syscalls_string_linux_arm64.go -abis common,oabi,eabi
 
 package probe
 
