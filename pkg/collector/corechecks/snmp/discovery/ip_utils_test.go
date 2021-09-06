3e57abb324a87fdba6ca2b3084216ebaa63bab3d
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-09-06 10:49:53 +0200

    Fix lint

--- pkg/collector/corechecks/snmp/discovery/ip_utils_test.go
+++ pkg/collector/corechecks/snmp/discovery/ip_utils_test.go
@@ -8,28 +8,28 @@ import (
 
 func Test_incrementIP(t *testing.T) {
 	tests := []struct {
-		name string
-		ip net.IP
+		name           string
+		ip             net.IP
 		expectedNextIP net.IP
 	}{
 		{
-			name: "next ip from 0",
-			ip: net.IPv4(127, 0, 0, 0),
+			name:           "next ip from 0",
+			ip:             net.IPv4(127, 0, 0, 0),
 			expectedNextIP: net.IPv4(127, 0, 0, 1),
 		},
 		{
-			name: "next ip from 2",
-			ip: net.IPv4(127, 0, 1, 2),
+			name:           "next ip from 2",
+			ip:             net.IPv4(127, 0, 1, 2),
 			expectedNextIP: net.IPv4(127, 0, 1, 3),
 		},
 		{
-			name: "next ip 255 to 0",
-			ip: net.IPv4(127, 0, 1, 255),
+			name:           "next ip 255 to 0",
+			ip:             net.IPv4(127, 0, 1, 255),
 			expectedNextIP: net.IPv4(127, 0, 2, 0),
 		},
 		{
-			name: "next ip multiple digit change",
-			ip: net.IPv4(10, 10, 255, 255),
+			name:           "next ip multiple digit change",
+			ip:             net.IPv4(10, 10, 255, 255),
 			expectedNextIP: net.IPv4(10, 11, 0, 0),
 		},
 	}
