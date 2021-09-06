5aa0b05b6b1de07f279ea1591b4bed3c66f4b1ba
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-09-06 17:23:01 +0200

    Update snowflake patch

--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -2,27 +2,25 @@ name "snowflake-connector-python-py3"
 
 dependency "pip3"
 
-default_version "2.1.3"
+default_version "2.6.0"
 
-source :url => "https://github.com/snowflakedb/snowflake-connector-python/archive/v#{version}.tar.gz",
-       :sha256 => "855ffb93a09c3cd994dab8af7c87a46038bbba103928c5948a0edcd2500f4e1a",
+
+source :url => "https://github.com/snowflakedb/snowflake-connector-python/archive/refs/tags/v#{version}.tar.gz",
+       :sha256 => "31764cb1ee30a575d8b15aa3af1df8795d30838f",
        :extract => :seven_zip
 
 relative_path "snowflake-connector-python-#{version}"
 
 build do
+  # This introduces a pyarrow dependency that is not needed for the agent and fails to build on SUSE
+  delete "pyproject.toml"
+
   if windows?
     pip = "#{windows_safe_path(python_3_embedded)}\\Scripts\\pip.exe"
-
-    # HACK: replace misbehaving symlinks by real copies of what they
-    # linked.
-    delete "connector_python2"
-    delete "connector_python3"
-    copy "test", "connector_python2"
-    copy "test", "connector_python3"
   else
     pip = "#{install_dir}/embedded/bin/pip3"
   end
+
 
   ship_license "https://raw.githubusercontent.com/snowflakedb/snowflake-connector-python/v#{version}/LICENSE.txt"
   patch :source => "dependencies.patch", :target => "setup.py"
