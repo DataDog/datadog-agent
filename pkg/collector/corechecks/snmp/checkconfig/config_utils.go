7d81e83f0217ed39b22b4753cc574c5befd60ffc
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-09-10 17:08:51 +0200

    Add comment for subnetTagPrefix exclusion

--- pkg/collector/corechecks/snmp/checkconfig/config_utils.go
+++ pkg/collector/corechecks/snmp/checkconfig/config_utils.go
@@ -12,9 +12,14 @@ func buildDeviceID(origTags []string) (string, []string) {
 	h := fnv.New64()
 	var tags []string
 	for _, tag := range origTags {
+
+		// When using snmp_listener, the `autodiscovery_subnet` tag is passed
+		// via autodiscovery template. We are ignoring it to avoid deviceID
+		// being changed when we change subnet e.g. 10.10.0.0/29 -> 10.10.0.0/30
 		if strings.HasPrefix(tag, subnetTagPrefix+":") {
 			continue
 		}
+
 		tags = append(tags, tag)
 	}
 	tags = util.SortUniqInPlace(tags)
