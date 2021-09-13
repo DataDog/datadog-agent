8606b7fe703cd6bb1144b9874a6b9428fb95d6df
Alexander Hasselbach <agasselbakh@stackstate.com>
2021-09-13 13:40:12 +0200

    publish docker images to both docker.io and quay repositories STAC-14134

--- omnibus/package-scripts/publish_image.sh
+++ omnibus/package-scripts/publish_image.sh
@@ -6,19 +6,29 @@ IMAGE_TAG="${1}"
 IMAGE_REPO="${2}"
 DOCKERFILE_PATH="${3}"
 EXTRA_TAG="${4}"
-REGISTRY="${5:-docker.io}"
-ORGANIZATION="${6:-stackstate}"
+REGISTRY_DOCKERHUB="docker.io"
+REGISTRY_QUAY="quay.io"
+ORGANIZATION="stackstate"
 
-echo "${IMAGE_TAG}"
-echo "${IMAGE_REPO}"
-echo "${DOCKERFILE_PATH}"
+echo "IMAGE_TAG=${IMAGE_TAG}"
+echo "IMAGE_REPO=${IMAGE_REPO}"
+echo "DOCKERFILE_PATH=${DOCKERFILE_PATH}"
+
+BUILD_TAG="${IMAGE_REPO}:${IMAGE_TAG}"
 
 docker login -u "${docker_user}" -p "${docker_password}" "${REGISTRY}"
-docker build -t "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}" "${DOCKERFILE_PATH}"
-docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
+docker login -u "${quay_user}" -p "${quay_user}" "${REGISTRY_QUAY}"
 
-if [ -n "$EXTRA_TAG" ]; then
-    docker tag "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}" "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
-    echo "Pushing release to ${EXTRA_TAG}"
-    docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
-fi
+docker build -t "${BUILD_TAG}" "${DOCKERFILE_PATH}"
+
+for REGISTRY in "${REGISTRY_DOCKERHUB}" "${REGISTRY_QUAY}"; do
+    docker tag "${BUILD_TAG}" "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
+    docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
+
+    if [ -n "$EXTRA_TAG" ]; then
+        docker tag "${BUILD_TAG}" "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+        echo "Pushing release to ${EXTRA_TAG}"
+
+        docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+    fi
+done
