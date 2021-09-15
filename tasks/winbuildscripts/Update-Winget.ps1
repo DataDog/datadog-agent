e4b8081b41af6228a2f2d43ced393c187ec006a2
Julien Lebot <julien.lebot@datadoghq.com>
2021-09-15 22:25:21 +0200

    Fixed logs

--- tasks/winbuildscripts/Update-Winget.ps1
+++ tasks/winbuildscripts/Update-Winget.ps1
@@ -13,11 +13,11 @@ if ($m) {
     $agentVersion = $m.Groups[1].Value
     $rc = $m.Groups[3].Value
 } else {
-    Write-Host "Unsupported agent version '$agentVersion', aborting"
+    Write-Host "Unsupported agent version '$rawAgentVersion', aborting"
     exit 1
 }
 if ($rc) {
-    Write-Host ("Updating Winget manifest for Agent version ${agentVersion}-${rc}")
+    Write-Host ("Updating Winget manifest for Agent version ${agentVersion}-rc.${rc}")
     .\wingetcreate.exe update --urls "https://s3.amazonaws.com/dd-agent-mstesting/builds/tagged/datadog-agent-${agentVersion}-rc.${rc}-1-x86_64.msi" --version "${agentVersion}-rc.${rc}" --submit --token "${env:WINGET_GITHUB_ACCESS_TOKEN}" "Datadog.Agent"
 } else {
     Write-Host ("Updating Winget manifest for Agent version ${agentVersion}")
