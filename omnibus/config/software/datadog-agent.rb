4a22f2f9273b2e5d91eec48ab54b08216f04e5b4
Julien Lebot <julien.ar.lebot@gmail.com>
2021-09-17 14:39:57 +0200

    Use wildcard

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -110,7 +110,7 @@ build do
     copy 'bin/agent/ddtray.exe', "#{install_dir}/bin/agent"
     copy 'bin/agent/dist', "#{install_dir}/bin/agent"
     mkdir Omnibus::Config.package_dir() unless Dir.exists?(Omnibus::Config.package_dir())
-    copy 'bin/agent/customaction.pdb', "#{Omnibus::Config.package_dir()}/"
+    copy 'bin/agent/customaction*.pdb', "#{Omnibus::Config.package_dir()}/"
   end
 
   block do
