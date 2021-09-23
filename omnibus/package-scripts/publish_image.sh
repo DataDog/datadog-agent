5bd2340fc8894127194d19ac9e9cea7236159ce0
Louis Parkin <11991882+LouisParkin@users.noreply.github.com>
2021-09-23 13:04:59 +0200

    STAC-13434 Added anchore scanning of master and tags. (#371)
    Experimental license scanning.
    
    Co-authored-by: Zandré Witte <zandre.witte@gmail.com>

--- omnibus/package-scripts/publish_image.sh
+++ omnibus/package-scripts/publish_image.sh
@@ -13,12 +13,20 @@ echo "${IMAGE_TAG}"
 echo "${IMAGE_REPO}"
 echo "${DOCKERFILE_PATH}"
 
+DOCKER_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
+
 docker login -u "${docker_user}" -p "${docker_password}" "${REGISTRY}"
-docker build -t "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}" "${DOCKERFILE_PATH}"
-docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}"
+docker build -t "${DOCKER_TAG}" "${DOCKERFILE_PATH}"
+docker push "${DOCKER_TAG}"
 
 if [ -n "$EXTRA_TAG" ]; then
-    docker tag "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${IMAGE_TAG}" "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+    DOCKER_EXTRA_TAG="${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+    docker tag "${DOCKER_TAG}" "${DOCKER_EXTRA_TAG}"
     echo "Pushing release to ${EXTRA_TAG}"
-    docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE_REPO}:${EXTRA_TAG}"
+    docker push "${DOCKER_EXTRA_TAG}"
+fi
+
+if [ ! -z "${CI_COMMIT_TAG}" ] || [ "${CI_COMMIT_BRANCH}" = "master" ]; then
+    echo "Scanning image ${DOCKER_TAG} for vulnerabilities"
+    ./anchore.sh -n 0 -i "${DOCKER_TAG}"
 fi
