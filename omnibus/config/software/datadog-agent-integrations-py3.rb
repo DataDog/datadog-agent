4572c8aa4bbd9f7eb4ea863781ca529680094118
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2021-09-24 11:13:52 +0200

    Fix wheel search glob, implement caching for python 2 integrations

--- omnibus/config/software/datadog-agent-integrations-py3.rb
+++ omnibus/config/software/datadog-agent-integrations-py3.rb
@@ -337,7 +337,7 @@ build do
           copy profiles, "#{check_conf_dir}/"
         end
 
-        cached_wheel_glob = Dir.glob(File.join(cached_wheels_dir, "datadog_#{check}-*.whl"))
+        cached_wheel_glob = Dir.glob(File.join(cached_wheels_dir.gsub("\\", "/"), "datadog_#{check}-*.whl"))
         if cached_wheel_glob.length == 1
           wheel_path = windows_safe_path(cached_wheel_glob[0])
           command "#{pip} install --no-deps --no-index #{wheel_path}"
