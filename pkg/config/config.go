b7080cd17a9ae41481ef3f530228677876f0891a
pducolin <paola.ducolin@datadoghq.com>
2021-09-30 13:21:57 +0200

    [corecheck/snmp] add snmp_listener.namespace to known config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -546,6 +546,7 @@ func InitConfig(config Config) {
 	config.SetKnown("snmp_listener.configs")
 	config.SetKnown("snmp_listener.loader")
 	config.SetKnown("snmp_listener.min_collection_interval")
+	config.SetKnown("snmp_listener.namespace")
 
 	config.BindEnvAndSetDefault("snmp_traps_enabled", false)
 	config.BindEnvAndSetDefault("snmp_traps_config.port", 162)
