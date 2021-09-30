b5f28bbb7a1c1b6ccc7a55871854495542879057
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-09-30 16:21:58 +0200

    [pkg/quantile] Fix lint errors

--- pkg/quantile/test_helper.go
+++ pkg/quantile/test_helper.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build test
 // +build test
 
 package quantile
@@ -34,7 +35,7 @@ func SketchesApproxEqual(exp, act *Sketch, e float64) bool {
 		return false
 	}
 
-	if exp.Basic.Cnt != exp.Basic.Cnt {
+	if exp.Basic.Cnt != act.Basic.Cnt {
 		return false
 	}
 
@@ -57,8 +58,4 @@ func SketchesApproxEqual(exp, act *Sketch, e float64) bool {
 	}
 
 	return true
-}
-
-type tHelper interface {
-	Helper()
 }
