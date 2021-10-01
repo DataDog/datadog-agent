8fec801657ca2858e4e2a6fdd40947a50e1c391b
Vyacheslav Voronenko <git@voronenko.info>
2021-10-01 20:21:54 +0200

    temporary workaround for LE root cert for openssl 1.0.1

--- omnibus/config/software/libelf.rb
+++ omnibus/config/software/libelf.rb
@@ -9,7 +9,6 @@ default_version '0.178'
 version '0.178' do
   source url: 'https://sourceware.org/elfutils/ftp/0.178/elfutils-0.178.tar.bz2',
          sha512: '356656ad0db8f6877b461de1a11280de16a9cc5d8dde4381a938a212e828e32755135e5e3171d311c4c9297b728fbd98123048e2e8fbf7fe7de68976a2daabe5'
-         options: {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}  # Workaround LE root cert.
 end
 
 dependency 'zlib'
