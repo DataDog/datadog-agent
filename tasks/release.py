587b3d75e100d15500db37e3ebd93c237e1310d5
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-10-04 17:26:48 +0200

    Add skip-qa label

--- tasks/release.py
+++ tasks/release.py
@@ -1221,7 +1221,7 @@ Make sure that milestone is open before trying again.""".format(
     updated_pr = github.update_pr(
         pull_number=pr["number"],
         milestone_number=milestone["number"],
-        labels=["changelog/no-changelog", "team/agent-platform", "team/agent-core"],
+        labels=["changelog/no-changelog", "qa/skip-qa", "team/agent-platform", "team/agent-core"],
     )
 
     if not updated_pr or not updated_pr.get("number") or not updated_pr.get("html_url"):
