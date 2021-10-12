DataDog/datadog-agent

commit 8a4b773f1676120525e2688bb60d8e70a093cbac

[containerd] Add support for Windows (#9296)
* [build-tags] Remove containerd from linux-only list

* [containerd] Parse Windows metrics

* [go.mod] Run "go mod tidy"

* [config/containers] Don't register cri on Windows

* [containers/providers] Expose GetNumFileDescriptors

* Add release note

* Run "inv generate-licenses"

* [containerd] Report Windows metrics

* [containerd] Refactor to avoid extra conditionals

pkg/collector/corechecks/containers/containerd/containerd.go
@@ -12,11 +12,11 @@ import (
 	"strings"
 	"time"
 
+	wstats "github.com/Microsoft/hcsshim/cmd/containerd-shim-runhcs-v1/stats"
 	v1 "github.com/containerd/cgroups/stats/v1"
-	containerdTypes "github.com/containerd/containerd/api/types"
+	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/containers"
 	"github.com/containerd/typeurl"
-	"github.com/gogo/protobuf/types"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"gopkg.in/yaml.v2"
 
@@ -31,7 +31,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/tagger/collectors"
 	cutil "github.com/DataDog/datadog-agent/pkg/util/containerd"
 	ddContainers "github.com/DataDog/datadog-agent/pkg/util/containers"
-	cgroup "github.com/DataDog/datadog-agent/pkg/util/containers/providers/cgroup"
+	"github.com/DataDog/datadog-agent/pkg/util/containers/providers"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 	"github.com/DataDog/datadog-agent/pkg/util/system"
 )
@@ -187,18 +187,6 @@ func computeMetrics(sender aggregator.Sender, cu cutil.ContainerdItf, fil *ddCon
 			continue
 		}
 
-		metricTask, errTask := cu.TaskMetrics(ctn)
-		if errTask != nil {
-			log.Tracef("Could not retrieve metrics from task %s: %s", ctn.ID()[:12], errTask.Error())
-			continue
-		}
-
-		metrics, err := convertTasktoMetrics(metricTask)
-		if err != nil {
-			log.Errorf("Could not process the metrics from %s: %v", ctn.ID(), err.Error())
-			continue
-		}
-
 		tags, err := collectTags(info)
 		if err != nil {
 			log.Errorf("Could not collect tags for container %s: %s", ctn.ID()[:12], err)
@@ -214,25 +202,29 @@ func computeMetrics(sender aggregator.Sender, cu cutil.ContainerdItf, fil *ddCon
 
 		currentTime := time.Now()
 		computeUptime(sender, info, currentTime, tags)
-		computeMem(sender, metrics.Memory, tags)
 
-		ociSpec, err := cu.Spec(ctn)
+		metricTask, errTask := cu.TaskMetrics(ctn)
+		if errTask != nil {
+			log.Tracef("Could not retrieve metrics from task %s: %s", ctn.ID()[:12], errTask.Error())
+			continue
+		}
+
+		anyMetrics, err := typeurl.UnmarshalAny(metricTask.Data)
 		if err != nil {
-			log.Warnf("Could not retrieve OCI Spec from: %s: %v", ctn.ID(), err)
+			log.Errorf("Can't convert the metrics data from %s", ctn.ID())
+			continue
 		}
 
-		var cpuLimits *specs.LinuxCPU
-		if ociSpec != nil && ociSpec.Linux != nil && ociSpec.Linux.Resources != nil {
-			cpuLimits = ociSpec.Linux.Resources.CPU
-		}
-		computeCPU(sender, metrics.CPU, cpuLimits, info.CreatedAt, currentTime, tags)
-
-		if metrics.Blkio.Size() > 0 {
-			computeBlkio(sender, metrics.Blkio, tags)
-		}
-
-		if len(metrics.Hugetlb) > 0 {
-			computeHugetlb(sender, metrics.Hugetlb, tags)
+		switch metricsVal := anyMetrics.(type) {
+		case *v1.Metrics:
+			computeLinuxSpecificMetrics(metricsVal, sender, cu, ctn, info.CreatedAt, currentTime, tags)
+		case *wstats.Statistics:
+			if windowsMetrics := metricsVal.GetWindows(); windowsMetrics != nil {
+				computeWindowsSpecificMetrics(windowsMetrics, sender, tags)
+			}
+		default:
+			log.Errorf("Can't convert the metrics data from %s", ctn.ID())
+			continue
 		}
 
 		size, err := cu.ImageSize(ctn)
@@ -251,7 +243,7 @@ func computeMetrics(sender aggregator.Sender, cu cutil.ContainerdItf, fil *ddCon
 		fileDescCount := 0
 		for _, p := range processes {
 			pid := p.Pid
-			fdCount, err := cgroup.GetFileDescriptorLen(int(pid))
+			fdCount, err := providers.ContainerImpl().GetNumFileDescriptors(int(pid))
 			if err != nil {
 				log.Debugf("Failed to get file desc length for pid %d, container %s: %s", pid, ctn.ID()[:12], err)
 				continue
@@ -270,16 +262,33 @@ func isExcluded(ctn containers.Container, fil *ddContainers.Filter) bool {
 	return fil.IsExcluded("", ctn.Image, ctn.Labels["io.kubernetes.pod.namespace"])
 }
 
-func convertTasktoMetrics(metricTask *containerdTypes.Metric) (*v1.Metrics, error) {
-	metricAny, err := typeurl.UnmarshalAny(&types.Any{
-		TypeUrl: metricTask.Data.TypeUrl,
-		Value:   metricTask.Data.Value,
-	})
+func computeLinuxSpecificMetrics(metrics *v1.Metrics, sender aggregator.Sender, cu cutil.ContainerdItf, ctn containerd.Container, createdAt time.Time, currentTime time.Time, tags []string) {
+	computeMemLinux(sender, metrics.Memory, tags)
+
+	ociSpec, err := cu.Spec(ctn)
 	if err != nil {
-		log.Errorf(err.Error())
-		return nil, err
+		log.Warnf("Could not retrieve OCI Spec from: %s: %v", ctn.ID(), err)
 	}
-	return metricAny.(*v1.Metrics), nil
+
+	var cpuLimits *specs.LinuxCPU
+	if ociSpec != nil && ociSpec.Linux != nil && ociSpec.Linux.Resources != nil {
+		cpuLimits = ociSpec.Linux.Resources.CPU
+	}
+	computeCPULinux(sender, metrics.CPU, cpuLimits, createdAt, currentTime, tags)
+
+	if metrics.Blkio.Size() > 0 {
+		computeBlkio(sender, metrics.Blkio, tags)
+	}
+
+	if len(metrics.Hugetlb) > 0 {
+		computeHugetlb(sender, metrics.Hugetlb, tags)
+	}
+}
+
+func computeWindowsSpecificMetrics(windowsStats *wstats.WindowsContainerStatistics, sender aggregator.Sender, tags []string) {
+	computeCPUWindows(sender, windowsStats.Processor, tags)
+	computeMemWindows(sender, windowsStats.Memory, tags)
+	computeStorageWindows(sender, windowsStats.Storage, tags)
 }
 
 // TODO when creating a dedicated collector for the tagger, unify the local tagging logic and the Tagger.
@@ -316,7 +325,7 @@ func computeUptime(sender aggregator.Sender, ctn containers.Container, currentTi
 	}
 }
 
-func computeMem(sender aggregator.Sender, mem *v1.MemoryStat, tags []string) {
+func computeMemLinux(sender aggregator.Sender, mem *v1.MemoryStat, tags []string) {
 	if mem == nil {
 		return
 	}
@@ -346,7 +355,17 @@ func parseAndSubmitMem(metricName string, sender aggregator.Sender, stat *v1.Mem
 	sender.Gauge(fmt.Sprintf("%s.max", metricName), float64(stat.Max), "", tags)
 }
 
-func computeCPU(sender aggregator.Sender, cpu *v1.CPUStat, cpuLimits *specs.LinuxCPU, startTime, currentTime time.Time, tags []string) {
+func computeMemWindows(sender aggregator.Sender, memStats *wstats.WindowsContainerMemoryStatistics, tags []string) {
+	if memStats == nil {
+		return
+	}
+
+	sender.Gauge("containerd.mem.commit", float64(memStats.MemoryUsageCommitBytes), "", tags)
+	sender.Gauge("containerd.mem.commit_peak", float64(memStats.MemoryUsageCommitPeakBytes), "", tags)
+	sender.Gauge("containerd.mem.private_working_set", float64(memStats.MemoryUsagePrivateWorkingSetBytes), "", tags)
+}
+
+func computeCPULinux(sender aggregator.Sender, cpu *v1.CPUStat, cpuLimits *specs.LinuxCPU, startTime, currentTime time.Time, tags []string) {
 	if cpu == nil || cpu.Usage == nil {
 		return
 	}
@@ -368,6 +387,16 @@ func computeCPU(sender aggregator.Sender, cpu *v1.CPUStat, cpuLimits *specs.Linu
 		}
 		sender.Rate("containerd.cpu.limit", cpuLimitPct*timeDiff, "", tags)
 	}
+}
+
+func computeCPUWindows(sender aggregator.Sender, cpuStats *wstats.WindowsContainerProcessorStatistics, tags []string) {
+	if cpuStats == nil {
+		return
+	}
+
+	sender.Rate("containerd.cpu.total", float64(cpuStats.TotalRuntimeNS), "", tags)
+	sender.Rate("containerd.cpu.system", float64(cpuStats.RuntimeKernelNS), "", tags)
+	sender.Rate("containerd.cpu.user", float64(cpuStats.RuntimeUserNS), "", tags)
 }
 
 func computeBlkio(sender aggregator.Sender, blkio *v1.BlkIOStat, tags []string) {
@@ -404,4 +433,13 @@ func parseAndSubmitBlkio(metricName string, sender aggregator.Sender, list []*v1
 
 		sender.Rate(metricName, float64(m.Value), "", deviceTags)
 	}
+}
+
+func computeStorageWindows(sender aggregator.Sender, storageStats *wstats.WindowsContainerStorageStatistics, tags []string) {
+	if storageStats == nil {
+		return
+	}
+
+	sender.Rate("containerd.storage.read", float64(storageStats.ReadSizeBytes), "", tags)
+	sender.Rate("containerd.storage.write", float64(storageStats.WriteSizeBytes), "", tags)
 }

go.mod
@@ -68,6 +68,7 @@ require (
 	github.com/Masterminds/semver v1.5.0
 	github.com/Masterminds/sprig v2.22.0+incompatible
 	github.com/Microsoft/go-winio v0.4.17
+	github.com/Microsoft/hcsshim v0.8.18
 	github.com/alecthomas/jsonschema v0.0.0-20210526225647-edb03dcab7bc
 	github.com/alecthomas/participle v0.7.1
 	github.com/alecthomas/repr v0.0.0-20181024024818-d37bc2a10ba1

pkg/collector/corechecks/containers/containerd/containerd_test.go
@@ -8,16 +8,13 @@
 package containerd
 
 import (
-	"encoding/json"
 	"sort"
 	"testing"
 	"time"
 
+	wstats "github.com/Microsoft/hcsshim/cmd/containerd-shim-runhcs-v1/stats"
 	v1 "github.com/containerd/cgroups/stats/v1"
-	"github.com/containerd/containerd/api/types"
 	"github.com/containerd/containerd/containers"
-	"github.com/containerd/typeurl"
-	prototypes "github.com/gogo/protobuf/types"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -183,7 +180,7 @@ func TestComputeEvents(t *testing.T) {
 	}
 }
 
-func TestComputeCPU(t *testing.T) {
+func TestComputeCPULinux(t *testing.T) {
 	containerdCheck := &ContainerdCheck{
 		instance:  &ContainerdConfig{},
 		CheckBase: corechecks.NewCheckBase("containerd"),
@@ -249,7 +246,7 @@ func TestComputeCPU(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			computeCPU(mocked, test.cpu, test.cpuLimit, test.startTime, test.currentTime, []string{})
+			computeCPULinux(mocked, test.cpu, test.cpuLimit, test.startTime, test.currentTime, []string{})
 			for name, val := range test.expected {
 				mocked.AssertMetric(t, "Rate", name, val, "", []string{})
 			}
@@ -257,7 +254,58 @@ func TestComputeCPU(t *testing.T) {
 	}
 }
 
-func TestComputeMem(t *testing.T) {
+func TestComputeCPUWindows(t *testing.T) {
+	containerdCheck := &ContainerdCheck{
+		instance:  &ContainerdConfig{},
+		CheckBase: corechecks.NewCheckBase("containerd"),
+	}
+	mocked := mocksender.NewMockSender(containerdCheck.ID())
+	mocked.SetupAcceptAll()
+
+	tests := []struct {
+		name                    string
+		cpuStats                *wstats.WindowsContainerProcessorStatistics
+		tags                    []string
+		expectedMetricsReported map[string]float64
+	}{
+		{
+			name: "CPU stats not nil",
+			cpuStats: &wstats.WindowsContainerProcessorStatistics{
+				TotalRuntimeNS:  50,
+				RuntimeUserNS:   10,
+				RuntimeKernelNS: 40,
+			},
+			tags: []string{"foo:bar"},
+			expectedMetricsReported: map[string]float64{
+				"containerd.cpu.total":  50,
+				"containerd.cpu.user":   10,
+				"containerd.cpu.system": 40,
+			},
+		},
+		{
+			name:                    "CPU stats is nil",
+			cpuStats:                nil,
+			tags:                    []string{"foo:bar"},
+			expectedMetricsReported: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			computeCPUWindows(mocked, test.cpuStats, test.tags)
+
+			if test.cpuStats == nil {
+				mocked.AssertNotCalled(t, "Rate")
+			} else {
+				for metricName, metricVal := range test.expectedMetricsReported {
+					mocked.AssertMetric(t, "Rate", metricName, metricVal, "", test.tags)
+				}
+			}
+		})
+	}
+}
+
+func TestComputeMemLinux(t *testing.T) {
 	containerdCheck := &ContainerdCheck{
 		instance:  &ContainerdConfig{},
 		CheckBase: corechecks.NewCheckBase("containerd"),
@@ -344,9 +392,109 @@ func TestComputeMem(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			computeMem(mocked, test.mem, []string{})
+			computeMemLinux(mocked, test.mem, []string{})
 			for name, val := range test.expected {
 				mocked.AssertMetric(t, "Gauge", name, val, "", []string{})
+			}
+		})
+	}
+}
+
+func TestComputeMemWindows(t *testing.T) {
+	containerdCheck := &ContainerdCheck{
+		instance:  &ContainerdConfig{},
+		CheckBase: corechecks.NewCheckBase("containerd"),
+	}
+	mocked := mocksender.NewMockSender(containerdCheck.ID())
+	mocked.SetupAcceptAll()
+
+	tests := []struct {
+		name                    string
+		memStats                *wstats.WindowsContainerMemoryStatistics
+		tags                    []string
+		expectedMetricsReported map[string]float64
+	}{
+		{
+			name: "Memory stats not nil",
+			memStats: &wstats.WindowsContainerMemoryStatistics{
+				MemoryUsageCommitBytes:            1024,
+				MemoryUsageCommitPeakBytes:        2048,
+				MemoryUsagePrivateWorkingSetBytes: 512,
+			},
+			tags: []string{"foo:bar"},
+			expectedMetricsReported: map[string]float64{
+				"containerd.mem.commit":              1024,
+				"containerd.mem.commit_peak":         2048,
+				"containerd.mem.private_working_set": 512,
+			},
+		},
+		{
+			name:                    "Memory stats is nil",
+			memStats:                nil,
+			tags:                    []string{"foo:bar"},
+			expectedMetricsReported: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			computeMemWindows(mocked, test.memStats, test.tags)
+
+			if test.memStats == nil {
+				mocked.AssertNotCalled(t, "Gauge")
+			} else {
+				for metricName, metricVal := range test.expectedMetricsReported {
+					mocked.AssertMetric(t, "Gauge", metricName, metricVal, "", test.tags)
+				}
+			}
+		})
+	}
+}
+
+func TestComputeStorageWindows(t *testing.T) {
+	containerdCheck := &ContainerdCheck{
+		instance:  &ContainerdConfig{},
+		CheckBase: corechecks.NewCheckBase("containerd"),
+	}
+	mocked := mocksender.NewMockSender(containerdCheck.ID())
+	mocked.SetupAcceptAll()
+
+	tests := []struct {
+		name                    string
+		storageStats            *wstats.WindowsContainerStorageStatistics
+		tags                    []string
+		expectedMetricsReported map[string]float64
+	}{
+		{
+			name: "Storage stats not nil",
+			storageStats: &wstats.WindowsContainerStorageStatistics{
+				ReadSizeBytes:  256,
+				WriteSizeBytes: 128,
+			},
+			tags: []string{"foo:bar"},
+			expectedMetricsReported: map[string]float64{
+				"containerd.storage.read":  256,
+				"containerd.storage.write": 128,
+			},
+		},
+		{
+			name:                    "Storage stats is nil",
+			storageStats:            nil,
+			tags:                    []string{"foo:bar"},
+			expectedMetricsReported: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			computeStorageWindows(mocked, test.storageStats, test.tags)
+
+			if test.storageStats == nil {
+				mocked.AssertNotCalled(t, "Rate")
+			} else {
+				for metricName, metricVal := range test.expectedMetricsReported {
+					mocked.AssertMetric(t, "Rate", metricName, metricVal, "", test.tags)
+				}
 			}
 		})
 	}
@@ -389,62 +537,6 @@ func TestComputeUptime(t *testing.T) {
 			computeUptime(mocked, test.ctn, currentTime, []string{})
 			for name, val := range test.expected {
 				mocked.AssertMetric(t, "Gauge", name, val, "", []string{})
-			}
-		})
-	}
-}
-
-// TestConvertTaskToMetrics checks the convertTasktoMetrics
-func TestConvertTaskToMetrics(t *testing.T) {
-	typeurl.Register(&v1.Metrics{}, "io.containerd.cgroups.v1.Metrics") // Need to register the type to be used in UnmarshalAny later on.
-
-	tests := []struct {
-		name     string
-		typeURL  string
-		values   v1.Metrics
-		error    string
-		expected *v1.Metrics
-	}{
-		{
-			"unregistered type",
-			"io.containerd.cgroups.v1.Doge",
-			v1.Metrics{},
-			"type with url io.containerd.cgroups.v1.Doge: not found",
-			nil,
-		},
-		{
-			"missing values",
-			"io.containerd.cgroups.v1.Metrics",
-			v1.Metrics{},
-			"",
-			&v1.Metrics{},
-		},
-		{
-			"fully functional",
-			"io.containerd.cgroups.v1.Metrics",
-			v1.Metrics{Memory: &v1.MemoryStat{Cache: 100}},
-			"",
-			&v1.Metrics{
-				Memory: &v1.MemoryStat{
-					Cache: 100,
-				},
-			},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			typeURL := test.typeURL
-			jsonValue, _ := json.Marshal(test.values)
-			metric := &types.Metric{
-				Data: &prototypes.Any{
-					TypeUrl: typeURL,
-					Value:   jsonValue,
-				},
-			}
-			m, e := convertTasktoMetrics(metric)
-			require.Equal(t, test.expected, m)
-			if e != nil {
-				require.Equal(t, e.Error(), test.error)
 			}
 		})
 	}

pkg/config/environment_containers.go
@@ -120,10 +120,7 @@ func detectContainerd(features FeatureMap) {
 	}
 
 	if criSocket != "" {
-		// Containerd support was historically meant for K8S
-		// However, containerd is now used standalone elsewhere.
-		// TODO: Consider having a dedicated setting for containerd standalone
-		if IsKubernetes() {
+		if isCriSupported() {
 			features[Cri] = struct{}{}
 		}
 
@@ -131,6 +128,14 @@ func detectContainerd(features FeatureMap) {
 			features[Containerd] = struct{}{}
 		}
 	}
+}
+
+func isCriSupported() bool {
+	// Containerd support was historically meant for K8S
+	// However, containerd is now used standalone elsewhere.
+	// TODO: Consider having a dedicated setting for containerd standalone
+	// Also, cri is not enabled on Windows (check build_tags.py).
+	return IsKubernetes() && runtime.GOOS != "windows"
 }
 
 func detectFargate(features FeatureMap) {

pkg/util/containers/providers/cgroup/provider.go
@@ -202,6 +202,12 @@ func (mp *provider) GetDefaultHostIPs() ([]string, error) {
 	return defaultHostIPs()
 }
 
+// GetNumFileDescriptors returns the number of open file descriptors for a given
+// pid
+func (mp *provider) GetNumFileDescriptors(pid int) (int, error) {
+	return GetFileDescriptorLen(pid)
+}
+
 func (mp *provider) getCgroup(containerID string) (*ContainerCgroup, error) {
 	mp.lock.RLock()
 	defer mp.lock.RUnlock()

pkg/util/containers/providers/mock/mock.go
@@ -75,3 +75,8 @@ func (f FakeContainerImpl) GetContainerLimits(containerID string) (*metrics.Cont
 func (f FakeContainerImpl) GetNetworkMetrics(containerID string, networks map[string]string) (metrics.ContainerNetStats, error) {
 	return nil, nil
 }
+
+// GetNumFileDescriptors mocks the GetNumFileDescriptors interface method
+func (f FakeContainerImpl) GetNumFileDescriptors(pid int) (int, error) {
+	return 0, nil
+}

pkg/util/containers/providers/windows/provider.go
@@ -322,6 +322,12 @@ func (mp *provider) GetDefaultHostIPs() ([]string, error) {
 	return []string{fields[3]}, nil
 }
 
+// GetNumFileDescriptors returns the number of open file descriptors for a given
+// pid
+func (mp *provider) GetNumFileDescriptors(pid int) (int, error) {
+	return 0, fmt.Errorf("not supported on windows")
+}
+
 // Output from route print 0.0.0.0:
 //
 // λ route print 0.0.0.0

pkg/util/containers/types.go
@@ -110,6 +110,7 @@ type ContainerImplementation interface {
 	ContainerIDForPID(pid int) (string, error)
 	GetDefaultGateway() (net.IP, error)
 	GetDefaultHostIPs() ([]string, error)
+	GetNumFileDescriptors(pid int) (int, error)
 
 	metrics.ContainerMetricsProvider
 }

commit 71df6de806155f6cb7be3cb593785142e583e98d

[containerd] Avoid calling the tagger for stopped tasks
This fixes a regression introduced in #9296, by basically reapplying the
changes in #8520, plus a comment to make it more obvious why the code is
structured in this way.

pkg/collector/corechecks/containers/containerd/containerd.go
@@ -183,7 +183,19 @@ func computeMetrics(sender aggregator.Sender, cu cutil.ContainerdItf, fil *ddCon
 			log.Errorf("Could not retrieve the metadata of the container: %s", ctn.ID()[:12])
 			continue
 		}
+
 		if isExcluded(info, fil) {
+			continue
+		}
+
+		// we need a running task before calling tagger.Tag. if the
+		// task isn't running, no tags will ever be returned, and it
+		// will cause a lot of unnecessary and expensive cache misses
+		// in the tagger. see 6e2ee06db6e4394b728d527a596d4c3e3393054a
+		// for more context.
+		metricTask, errTask := cu.TaskMetrics(ctn)
+		if errTask != nil {
+			log.Tracef("Could not retrieve metrics from task %s: %s", ctn.ID()[:12], errTask.Error())
 			continue
 		}
 
@@ -202,12 +214,6 @@ func computeMetrics(sender aggregator.Sender, cu cutil.ContainerdItf, fil *ddCon
 
 		currentTime := time.Now()
 		computeUptime(sender, info, currentTime, tags)
-
-		metricTask, errTask := cu.TaskMetrics(ctn)
-		if errTask != nil {
-			log.Tracef("Could not retrieve metrics from task %s: %s", ctn.ID()[:12], errTask.Error())
-			continue
-		}
 
 		anyMetrics, err := typeurl.UnmarshalAny(metricTask.Data)
 		if err != nil {