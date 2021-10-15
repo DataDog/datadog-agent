4e90e6c5baa0a5ca1a0f70d181af41483fa5f9b5
Julien Lebot <julien.lebot@datadoghq.com>
2021-10-15 15:23:16 +0200

    [Windows][Installer] Only create datadog.yaml on fresh installs (#9504)

--- tasks/winbuildscripts/unittests.ps1
+++ tasks/winbuildscripts/unittests.ps1
@@ -25,7 +25,11 @@ if ($Env:TARGET_ARCH -eq "x86") {
 }
 
 mkdir  .\bin\agent
-& inv -e customaction.build --arch=$archflag
+if ($Env:DEBUG_CUSTOMACTION) {
+    & inv -e customaction.build --arch=$archflag --debug
+} else {
+    & inv -e customaction.build --arch=$archflag
+}
 
 # Generate the datadog.yaml config file to be used in integration tests
 & inv -e generate-config --build-type="agent-py2py3" --output-file="./datadog.yaml"
