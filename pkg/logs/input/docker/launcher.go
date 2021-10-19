4f63274038167c7487aa44e0126d42cd6b27fba0
Brian Floersch <brian.floersch@datadoghq.com>
2021-10-19 14:38:00 -0400

    change logs_config.docker_container_use_file default to true (#9445)
    Change logs_config.docker_container_use_file default to true. This will improve the performance and reliability of agents collecting logs from docker when the files are available and accessibly by the agent. (Followup from: #7109)

--- pkg/logs/input/docker/launcher.go
+++ pkg/logs/input/docker/launcher.go
@@ -354,6 +354,8 @@ func (l *Launcher) scheduleFileSource(container *Container, source *config.LogSo
 	}
 	// fileSource is a new source using the original source as its parent
 	fileSource := l.getFileSource(container, source)
+	fileSource.source.ParentSource.HideFromStatus()
+
 	// Keep source for later unscheduling
 	l.fileSourcesByContainer[containerID] = fileSource
 	l.sources.AddSource(fileSource.source)
