12263efe96b70286315b34e768ccb28794f460f8
Pedro Lambert <pedro.lambert@datadoghq.com>
2021-10-20 09:24:07 -0400

    [networks] Fix eBPF program clone (#9586)
    * [networks] Fix eBPF program clone
    
    * [networks] Fix program termination

--- pkg/network/http/ebpf_ssl.go
+++ pkg/network/http/ebpf_ssl.go
@@ -39,9 +39,6 @@ const (
 	// probe used for streaming shared library events
 	doSysOpen    = "kprobe/do_sys_open"
 	doSysOpenRet = "kretprobe/do_sys_open"
-
-	// UID used to create the base probes
-	baseUID = "base"
 )
 
 type openSSLProgram struct {
@@ -88,17 +85,6 @@ func (o *openSSLProgram) ConfigureManager(m *manager.Manager) {
 			&manager.Probe{Section: doSysOpen, KProbeMaxActive: maxActive},
 			&manager.Probe{Section: doSysOpenRet, KProbeMaxActive: maxActive},
 		)
-	}
-
-	// Load SSL & Crypto "base" probes
-	var extraProbes []string
-	extraProbes = append(extraProbes, sslProbes...)
-	extraProbes = append(extraProbes, cryptoProbes...)
-	for _, sec := range extraProbes {
-		m.Probes = append(m.Probes, &manager.Probe{
-			Section: sec,
-			UID:     baseUID,
-		})
 	}
 }
 
@@ -187,7 +173,7 @@ func addHooks(m *manager.Manager, probes []string) func(string) error {
 				UID:        uid,
 			}
 
-			err := m.AddHook(baseUID, newProbe)
+			err := m.AddHook("", newProbe)
 			if err != nil {
 				return err
 			}
@@ -205,7 +191,12 @@ func removeHooks(m *manager.Manager, probes []string) func(string) error {
 			if !found {
 				continue
 			}
-			p.Detach()
+
+			program := p.Program()
+			m.DetachHook(sec, uid)
+			if program != nil {
+				program.Close()
+			}
 		}
 
 		return nil
