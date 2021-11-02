85e9dd2cbe443e675887fc8a4e238737119d7607
Mehedi Bakht <mehedi.bakht@datadoghq.com>
2021-11-02 10:40:01 -0500

    [networks] replace GetDNSNamesV2 (#9722)
    * replace GetDNSNamesV2
    
    * handle additional parameter

--- pkg/process/checks/net_test.go
+++ pkg/process/checks/net_test.go
@@ -249,7 +249,7 @@ func TestNetworkConnectionBatchingWithDomainsByQueryType(t *testing.T) {
 		connections := c.(*model.CollectorConnections)
 		total += len(connections.Connections)
 
-		domaindb := connections.GetDNSNamesV2()
+		domaindb, _ := connections.GetDNSNames()
 
 		// verify nothing was put in the DnsStatsByDomain bucket by mistake
 		assert.Equal(t, len(connections.Connections[0].DnsStatsByDomain), 0)
@@ -368,7 +368,7 @@ func TestNetworkConnectionBatchingWithDomains(t *testing.T) {
 		connections := c.(*model.CollectorConnections)
 		total += len(connections.Connections)
 
-		domaindb := connections.GetDNSNamesV2()
+		domaindb, _ := connections.GetDNSNames()
 
 		// verify nothing was put in the DnsStatsByDomain bucket by mistake
 		assert.Equal(t, len(connections.Connections[0].DnsStatsByDomain), 0)
