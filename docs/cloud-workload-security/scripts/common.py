e5a845353fc1b4d348716575654a5a910a606425
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-11-04 18:19:27 +0100

    [CWS] extract templating to common function

---
+++ docs/cloud-workload-security/scripts/common.py
@@ -0,0 +1,12 @@
+import jinja2
+import os
+
+def fill_template(template_name, **kwargs):
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
+        autoescape=jinja2.select_autoescape(),
+        trim_blocks=True,
+    )
+    templ = env.get_template(template_name)
+    return templ.render(**kwargs)
+
