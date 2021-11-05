3f2fa645c03fa1330c7bd47afc1ebd273a2401e9
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-11-05 16:23:24 -0400

    Rm clientReadyTimeout

--- pkg/serverless/serverless.go
+++ pkg/serverless/serverless.go
@@ -32,9 +32,7 @@ const (
 	headerExtID      string = "Lambda-Extension-Identifier"
 	headerExtErrType string = "Lambda-Extension-Function-Error-Type"
 
-	requestTimeout     time.Duration = 5 * time.Second
-	clientReadyTimeout time.Duration = 2 * time.Second
-
+	requestTimeout      time.Duration = 5 * time.Second
 	safetyBufferTimeout time.Duration = 20 * time.Millisecond
 
 	// FatalNoAPIKey is the error reported to the AWS Extension environment when
