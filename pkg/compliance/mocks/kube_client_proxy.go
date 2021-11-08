784cb460839fe38f4ebd9c70474d6b8e571ba86e
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-11-08 22:16:03 +0100

    [CWS] fix linting errors

--- pkg/compliance/mocks/kube_client_proxy.go
+++ pkg/compliance/mocks/kube_client_proxy.go
@@ -6,15 +6,18 @@ import (
 	"k8s.io/client-go/dynamic/fake"
 )
 
+// KubeClientProxy is a proxy that maps methods to either a mock or a fake client
 type KubeClientProxy struct {
 	FakeClient *fake.FakeDynamicClient
 	MockClient KubeClient
 }
 
+// Resource maps the call to the underlying fake client
 func (p *KubeClientProxy) Resource(resource schema.GroupVersionResource) dynamic.NamespaceableResourceInterface {
 	return p.FakeClient.Resource(resource)
 }
 
+// ClusterID maps the call to the underlying mock
 func (p *KubeClientProxy) ClusterID() (string, error) {
 	return p.MockClient.ClusterID()
 }
