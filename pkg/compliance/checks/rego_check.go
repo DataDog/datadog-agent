a1c09f02f017b788e59f3cc3ea9583a747f79bce
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-11-09 16:42:49 +0100

    [CSPM] add warn log when failing to resolve input

--- pkg/compliance/checks/rego_check.go
+++ pkg/compliance/checks/rego_check.go
@@ -170,6 +170,7 @@ func (r *regoCheck) buildNormalInput(env env.Env) (eval.RegoInputMap, error) {
 
 		resolved, err := resolve(ctx, env, r.ruleID, input.ResourceCommon)
 		if err != nil {
+			log.Warnf("failed to resolve input: %v", err)
 			continue
 		}
 
