c0f26578424078925494fc58a14163dd6f5c594a
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2021-11-11 13:27:11 -0500

    [logs-agent] Use workloadmeta instead of AD to learn about new containers (#9803)
    * Add subscription support to pkg/workloadmeta/testing.Store
    * Use workloadmeta for logs-agent containers
    
    This switches from listening for AD notifications of containers
    starting/stopping to use the workloadmeta store instead.  This store is
    the "new way", and also gives information on the container runtime.

--- pkg/logs/logs.go
+++ pkg/logs/logs.go
@@ -16,6 +16,7 @@ import (
 	coreConfig "github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/logs/client/http"
 	"github.com/DataDog/datadog-agent/pkg/logs/metrics"
+	"github.com/DataDog/datadog-agent/pkg/workloadmeta"
 
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 
@@ -79,8 +80,9 @@ func start(getAC func() *autodiscovery.AutoConfig, serverless bool, logsChan cha
 	sources := config.NewLogSources()
 	services := service.NewServices()
 
-	// setup the config scheduler
-	scheduler.CreateScheduler(sources, services)
+	// setup the config scheduler; this will be added to the AD meta-scheduler
+	// later during agent startup, in common.LoadComponents
+	scheduler.CreateScheduler(sources, services, workloadmeta.GetGlobalStore())
 
 	// setup the server config
 	endpoints, err := buildEndpoints(serverless)
