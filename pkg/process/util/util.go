051e90fa23b0eeb05da54e68ea1c856af5673be2
Pedro Lambert <pedrolambert@gmail.com>
2021-11-12 15:30:44 -0500

    [networks] Add missing build tag

--- pkg/process/util/util.go
+++ pkg/process/util/util.go
@@ -123,17 +123,6 @@ func GetSysRoot() string {
 	return "/sys"
 }
 
-// GetRootNSPID returns the current PID from the root namespace
-func GetRootNSPID() (int, error) {
-	pidPath := filepath.Join(GetProcRoot(), "self")
-	pidStr, err := os.Readlink(pidPath)
-	if err != nil {
-		return 0, err
-	}
-
-	return strconv.Atoi(pidStr)
-}
-
 // WithAllProcs will execute `fn` for every pid under procRoot. `fn` is
 // passed the `pid`. If `fn` returns an error the iteration aborts,
 // returning the last error returned from `fn`.
