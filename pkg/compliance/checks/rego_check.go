1edb5d0f8d775b79d7afbfe1ffe1bcdb20dcbb8d
Paul Cacheux <paul.cacheux@datadoghq.com>
2021-11-12 10:11:23 +0100

    [CSPM] fix dump rego input feature when the dump file is present but empty (#9867)

--- pkg/compliance/checks/rego_check.go
+++ pkg/compliance/checks/rego_check.go
@@ -355,8 +355,10 @@ func dumpInputToFile(ruleID, path string, input interface{}) error {
 	currentData := make(map[string]interface{})
 	currentContent, err := ioutil.ReadFile(path)
 	if err == nil {
-		if err := json.Unmarshal(currentContent, &currentData); err != nil {
-			return err
+		if len(currentContent) != 0 {
+			if err := json.Unmarshal(currentContent, &currentData); err != nil {
+				return err
+			}
 		}
 	}
 
