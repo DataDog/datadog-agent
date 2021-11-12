516f80548deccb19a46fc0fa19496ba8f5b6b460
Pedro Lambert <pedro.lambert@datadoghq.com>
2021-11-12 12:04:05 -0500

    [networks] Enable user-defined rules for processing HTTP paths (#9866)
    * [networks] Add `HTTPReplaceRules` configuration field
    
    * [networks] Apply rules to HTTP path
    
    * [networks] Allow rules to be chained together
    
    * [networks] Distinguish between `rejected` and `dropped` requests

--- pkg/config/system_probe.go
+++ pkg/config/system_probe.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"encoding/json"
 	"strings"
 	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 const (
@@ -120,6 +123,15 @@ func InitSystemProbeConfig(cfg Config) {
 	cfg.BindEnv(join(netNS, "enable_http_monitoring"), "DD_SYSTEM_PROBE_NETWORK_ENABLE_HTTP_MONITORING")
 	cfg.BindEnv(join(netNS, "enable_https_monitoring"), "DD_SYSTEM_PROBE_NETWORK_ENABLE_HTTPS_MONITORING")
 	cfg.BindEnvAndSetDefault(join(netNS, "enable_gateway_lookup"), true, "DD_SYSTEM_PROBE_NETWORK_ENABLE_GATEWAY_LOOKUP")
+	httpRules := join(netNS, "http_replace_rules")
+	cfg.BindEnv(httpRules, "DD_SYSTEM_PROBE_NETWORK_HTTP_REPLACE_RULES")
+	cfg.SetEnvKeyTransformer(httpRules, func(in string) interface{} {
+		var out []map[string]string
+		if err := json.Unmarshal([]byte(in), &out); err != nil {
+			log.Warnf(`%q can not be parsed: %v`, httpRules, err)
+		}
+		return out
+	})
 
 	// list of DNS query types to be recorded
 	cfg.BindEnvAndSetDefault(join(netNS, "dns_recorded_query_types"), []string{})
