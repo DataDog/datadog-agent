1fcae39291d60a73bc6b3e35f4fa9584163c15ea
Lénaïc Huard <L3n41c@users.noreply.github.com>
2021-11-15 16:14:16 +0100

    Revert "[tagger] Enable pull collection for ECS collector (#9294)" (#9902)
    * Revert "[tagger] Enable pull collection for ECS collector (#9294)"
    
    This reverts commit 0f6a8fdfaf15c8126e7959d589442724e86030bf.
    
    * Add a release note
    
    * Re-introduce FetchOnlyCollection for ECS collector
    
    This is a partial revert of #9312 to facilitate the revert of #9294.
    
    Co-authored-by: Julio Greff <julio.greff@datadoghq.com>

--- pkg/tagger/collectors/ecs_main.go
+++ pkg/tagger/collectors/ecs_main.go
@@ -68,7 +68,7 @@ func (c *ECSCollector) Detect(ctx context.Context, out chan<- []*TagInfo) (Colle
 
 	c.clusterName = instance.Cluster
 
-	return PullCollection, nil
+	return FetchOnlyCollection, nil
 }
 
 // Fetch fetches ECS tags
@@ -86,15 +86,20 @@ func (c *ECSCollector) Fetch(ctx context.Context, entity string) ([]string, []st
 	var updates []*TagInfo
 
 	if config.Datadog.GetBool("ecs_collect_resource_tags_ec2") && ecsutil.HasEC2ResourceTags() {
-		updates, err = c.parseTasks(ctx, tasks, addTagsForContainer)
+		updates, err = c.parseTasks(ctx, tasks, cID, addTagsForContainer)
 	} else {
-		updates, err = c.parseTasks(ctx, tasks)
+		updates, err = c.parseTasks(ctx, tasks, cID)
 	}
 	if err != nil {
 		return []string{}, []string{}, []string{}, err
 	}
 
 	c.infoOut <- updates
+
+	expires := c.expire.ComputeExpires()
+	if len(expires) > 0 {
+		c.infoOut <- expires
+	}
 
 	for _, info := range updates {
 		if info.Entity == entity {
@@ -115,34 +120,6 @@ func (c *ECSCollector) Fetch(ctx context.Context, entity string) ([]string, []st
 
 	// container not found in updates
 	return []string{}, []string{}, []string{}, errors.NewNotFound(entity)
-}
-
-// Pull fetches ECS tags for all tasks in the current node
-func (c *ECSCollector) Pull(ctx context.Context) error {
-	tasks, err := c.metaV1.GetTasks(ctx)
-	if err != nil {
-		return err
-	}
-
-	var updates []*TagInfo
-
-	if config.Datadog.GetBool("ecs_collect_resource_tags_ec2") && ecsutil.HasEC2ResourceTags() {
-		updates, err = c.parseTasks(ctx, tasks, addTagsForContainer)
-	} else {
-		updates, err = c.parseTasks(ctx, tasks)
-	}
-	if err != nil {
-		return err
-	}
-
-	c.infoOut <- updates
-
-	expires := c.expire.ComputeExpires()
-	if len(expires) > 0 {
-		c.infoOut <- expires
-	}
-
-	return nil
 }
 
 func addTagsForContainer(ctx context.Context, containerID string, tags *utils.TagList) error {
