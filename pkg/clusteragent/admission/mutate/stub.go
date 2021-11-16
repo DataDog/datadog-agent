6c3b8efc4c05ab2134b23dd33657de02b6d5a6b3
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2021-11-16 16:34:09 +0000

    add stub.go files to all empty packages

---
+++ pkg/clusteragent/admission/mutate/stub.go
@@ -0,0 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// stub file to ensure go can read the package even when the build flags eliminate all files
+
+package mutate
