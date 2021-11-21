aff0141248db2a30d443c57a5143a68a162d8470
Julien Lebot <julien.lebot@datadoghq.com>
2021-11-21 18:37:37 +0100

    Rewrite decompression code in C++
    (cherry picked from commit fc0ec8f4f009f16333601aa0b5df19153068b4b5)

--- tools/windows/install-help/cal/decompress.cpp
+++ tools/windows/install-help/cal/decompress.cpp
@@ -1,108 +1,132 @@
-#include "stdafx.h"
 #include "decompress.h"
-#include <fstream>
-#include <sstream>
+#include "stdafx.h"
 #include <archive.h>
 #include <archive_entry.h>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 
-static la_ssize_t copy_data(struct archive *ar, struct archive *aw)
+/// <summary>
+/// Wrapper class around libarchive's API and resources.
+/// This class ensures that all resources allocated are properly cleaned up.
+/// </summary>
+class DecompressionContext
 {
-    const void *buff;
-    size_t size;
-    la_int64_t offset;
+  private:
+    struct archive *_archive;
+    struct archive *_extractor;
 
-    for (;;)
+  public:
+    DecompressionContext(std::filesystem::path const &archivePath)
     {
-        la_ssize_t r = archive_read_data_block(ar, &buff, &size, &offset);
-        if (r == ARCHIVE_EOF)
+        _archive = archive_read_new();
+        archive_read_support_compression_lzma(_archive);
+        archive_read_support_format_7zip(_archive);
+
+        const int flags = ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_PERM | ARCHIVE_EXTRACT_ACL | ARCHIVE_EXTRACT_FFLAGS;
+        _extractor = archive_write_disk_new();
+        archive_write_disk_set_options(_extractor, flags);
+        archive_write_disk_set_standard_lookup(_extractor);
+
+        // Note, the construct ".string().c_str()" is ok since the temporary string
+        // will be copied internally by archive_entry_set_pathname.
+        if (archive_read_open_filename(_archive, archivePath.string().c_str(), 10240) != ARCHIVE_OK)
         {
-            return ARCHIVE_OK;
-        }
-        if (r < ARCHIVE_OK)
-        {
-            return r;
-        }
-        r = archive_write_data_block(aw, buff, size, offset);
-        if (r < ARCHIVE_OK)
-        {
-            return r;
+            throw std::runtime_error("archive_read_open_filename");
         }
     }
-}
+
+    struct archive_entry *read_next_header()
+    {
+        struct archive_entry *entry;
+        la_ssize_t r = archive_read_next_header(_archive, &entry);
+        if (r == ARCHIVE_EOF)
+        {
+            return nullptr;
+        }
+        if (r < ARCHIVE_OK)
+        {
+            throw std::runtime_error(archive_error_string(_archive));
+        }
+        return entry;
+    }
+
+    void write_header(struct archive_entry *entry)
+    {
+        if (archive_write_header(_extractor, entry) < ARCHIVE_OK)
+        {
+            throw std::runtime_error(archive_error_string(_extractor));
+        }
+    }
+
+    void write_finish_entry()
+    {
+        if (archive_write_finish_entry(_extractor) < ARCHIVE_OK)
+        {
+            throw std::runtime_error(archive_error_string(_extractor));
+        }
+    }
+
+    void copy_data()
+    {
+        const void *buff;
+        size_t size;
+        la_int64_t offset;
+
+        for (;;)
+        {
+            if (archive_read_data_block(_archive, &buff, &size, &offset) < ARCHIVE_OK)
+            {
+                throw std::runtime_error(archive_error_string(_extractor));
+            }
+            if (archive_write_data_block(_extractor, buff, size, offset) < ARCHIVE_OK)
+            {
+                throw std::runtime_error(archive_error_string(_extractor));
+            }
+        }
+    }
+
+    ~DecompressionContext()
+    {
+        archive_read_close(_archive);
+        archive_read_free(_archive);
+
+        archive_write_close(_extractor);
+        archive_write_free(_extractor);
+    }
+};
 
 int decompress_archive(std::filesystem::path const &archivePath, std::filesystem::path const &destinationFolder)
 {
-    struct archive *a = archive_read_new();
-    archive_read_support_compression_lzma(a);
-    archive_read_support_format_7zip(a);
-
-    const int flags = ARCHIVE_EXTRACT_TIME | ARCHIVE_EXTRACT_PERM | ARCHIVE_EXTRACT_ACL | ARCHIVE_EXTRACT_FFLAGS;
-    struct archive *ext = archive_write_disk_new();
-    archive_write_disk_set_options(ext, flags);
-    archive_write_disk_set_standard_lookup(ext);
-
-    la_ssize_t r;
-    // Note, the construct ".string().c_str()" is ok since the temporary string
-    // will be copied internally by archive_read_open_filename.
-    if ((r = archive_read_open_filename(a, archivePath.string().c_str(), 10240)))
+    try
     {
+        DecompressionContext context(archivePath);
+        for (;;)
+        {
+            struct archive_entry *entry = context.read_next_header();
+            if (entry == nullptr)
+            {
+                return 0;
+            }
+
+            std::filesystem::path destFilepath(destinationFolder / archive_entry_pathname(entry));
+            // Note, the construct ".string().c_str()" is ok since the temporary string
+            // will be copied internally by archive_entry_set_pathname.
+            archive_entry_set_pathname(entry, destFilepath.string().c_str());
+
+            context.write_header(entry);
+            if (archive_entry_size(entry) > 0)
+            {
+                context.copy_data();
+            }
+            context.write_finish_entry();
+        }
+    }
+    catch (const std::exception &e)
+    {
+        WcaLog(LOGMSG_STANDARD, "Extracting archive failed (archive_read_next_header): %s\n", e.what());
         return 1;
     }
-
-    for (;;)
-    {
-        struct archive_entry *entry;
-        r = archive_read_next_header(a, &entry);
-        if (r == ARCHIVE_EOF)
-        {
-            break;
-        }
-        if (r < ARCHIVE_OK)
-        {
-            WcaLog(LOGMSG_STANDARD, "Extracting archive failed (archive_read_next_header): %s\n", archive_error_string(a));
-        }
-        if (r < ARCHIVE_WARN)
-        {
-            return 1;
-        }
-        std::filesystem::path destFilepath(destinationFolder / archive_entry_pathname(entry));
-        // Note, the construct ".string().c_str()" is ok since the temporary string
-        // will be copied internally by archive_entry_set_pathname.
-        archive_entry_set_pathname(entry, destFilepath.string().c_str());
-
-        r = archive_write_header(ext, entry);
-        if (r < ARCHIVE_OK)
-        {
-            WcaLog(LOGMSG_STANDARD, "Extracting archive failed (archive_write_header): %s\n", archive_error_string(ext));
-        }
-        else if (archive_entry_size(entry) > 0)
-        {
-            r = copy_data(a, ext);
-            if (r < ARCHIVE_OK)
-            {
-                WcaLog(LOGMSG_STANDARD, "Extracting archive failed (copy_data): %s\n", archive_error_string(ext));
-            }
-            if (r < ARCHIVE_WARN)
-            {
-                return 1;
-            }
-        }
-        r = archive_write_finish_entry(ext);
-        if (r < ARCHIVE_OK)
-        {
-            WcaLog(LOGMSG_STANDARD, "Extracting archive failed (archive_write_finish_entry): %s\n", archive_error_string(ext));
-        }
-        if (r < ARCHIVE_WARN)
-        {
-            return 1;
-        }
-    }
-
-    archive_read_close(a);
-    archive_read_free(a);
-
-    archive_write_close(ext);
-    archive_write_free(ext);
 
     return 0;
 }
