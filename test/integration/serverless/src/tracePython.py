c03fc48263e56491fe0d67f8e4198ded0559b199
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-11-24 17:29:27 -0500

    Black

--- test/integration/serverless/src/tracePython.py
+++ test/integration/serverless/src/tracePython.py
@@ -2,12 +2,10 @@
 import time
 from ddtrace import tracer
 
+
 def simple_test(event, context):
     # submit a custom span
     with tracer.trace("integration-test"):
         time.sleep(0.1)
 
-    return {
-        "statusCode": 200,
-        "body": "ok"
-    }
+    return {"statusCode": 200, "body": "ok"}
