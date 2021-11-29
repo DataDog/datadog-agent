898de25b23cbe95ac701b6963b9478ad7710dc56
Kangyi LI <kangyi.li@datadoghq.com>
2021-11-29 16:00:05 +0100

    Update orchestrator.go

--- pkg/collector/corechecks/cluster/orchestrator/orchestrator.go
+++ pkg/collector/corechecks/cluster/orchestrator/orchestrator.go
@@ -3,7 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-2021 Datadog, Inc.
 
-//go:build kubeapiserver && orchestrator
 // +build kubeapiserver,orchestrator
 
 package orchestrator
