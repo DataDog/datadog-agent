53a07b84f859e161469f9b397edb75ea942ce36c
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-12-01 14:41:11 +0100

    test enrich

--- pkg/collector/corechecks/snmp/checkconfig/config_validate_enrich.go
+++ pkg/collector/corechecks/snmp/checkconfig/config_validate_enrich.go
@@ -90,18 +90,6 @@ func validateEnrichMetadata(metadata MetadataConfig) []string {
 					continue
 				}
 				field := res.Fields[fieldName]
-				//if field.Symbol.OID != "" {
-				//	field.Symbols = append([]SymbolConfig{field.Symbol}, field.Symbols...)
-				//	field.Symbol = SymbolConfig{}
-				//}
-				// TODO: TEST ME
-				//if field.Value == "" && len(field.Symbols) == 0 {
-				//	errors = append(errors, fmt.Sprintf("field `%s`: value, symbol or symbols must be defined", fieldName))
-				//}
-				//// TODO: TEST ME
-				//if field.Value != "" && len(field.Symbols) > 0 {
-				//	errors = append(errors, fmt.Sprintf("field `%s`: value symbol or symbols cannot be both defined", fieldName))
-				//}
 				// TODO: TEST ME
 				for i := range field.Symbols {
 					errors = append(errors, validateEnrichSymbol(&field.Symbols[i])...)
