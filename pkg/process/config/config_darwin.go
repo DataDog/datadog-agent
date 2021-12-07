8eba4c3bca7b647157931f416fd554c880d799f1
Corrina Sivak <corrina.sivak@datadoghq.com>
2021-12-07 14:25:34 -0500

    Removed dead code in config_darwin.go

--- pkg/process/config/config_darwin.go
+++ pkg/process/config/config_darwin.go
@@ -5,18 +5,5 @@
 
 package config
 
-import (
-	"fmt"
-	"path/filepath"
-)
-
 // defaultSystemProbeAddress is the default unix socket path to be used for connecting to the system probe
 const defaultSystemProbeAddress = "/opt/datadog-agent/run/sysprobe.sock"
-
-// ValidateSysprobeSocket validates that the sysprobe socket config option is of the correct format.
-func ValidateSysprobeSocket(sockPath string) error {
-	if !filepath.IsAbs(sockPath) {
-		return fmt.Errorf("socket path must be an absolute file path")
-	}
-	return nil
-}
