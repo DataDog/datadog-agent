155b1bc21db57e01f30a72ea96aae93ee2c20eec
Sylvain Afchain <safchain@gmail.com>
2021-12-09 09:56:09 +0100

    Fix root path symlink resolution

--- pkg/security/probe/symlink_resolver.go
+++ pkg/security/probe/symlink_resolver.go
@@ -64,13 +64,16 @@ func (s *SymlinkResolver) UpdateSymlinks(root string) {
 		if err != nil {
 			continue
 		}
-		dest = strings.TrimPrefix(dest, root)
+
+		if root != "/" {
+			dest = strings.TrimPrefix(dest, root)
+		}
 
 		if s.added[dest] {
 			continue
 		}
 
-		seclog.Tracef("symlink resolved %s(%s) => %s\n", path, root, dest)
+		seclog.Tracef("symlink resolved %s(%s) => %s => %+v\n", path, root, dest, targets)
 
 		s.Lock()
 		for _, target := range targets {
