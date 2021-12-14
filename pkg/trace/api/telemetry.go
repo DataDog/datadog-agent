325e19cdcb5783a24fb1b96475ef4d59a60b3168
Pawel Chojnacki <pawelchcki@gmail.com>
2021-12-14 13:56:36 +0100

    rearange methods in telemetry.go

--- pkg/trace/api/telemetry.go
+++ pkg/trace/api/telemetry.go
@@ -33,58 +33,6 @@ type telemetryMultiTransport struct {
 	Endpoints []*config.Endpoint
 }
 
-func (m *telemetryMultiTransport) roundTrip(req *http.Request, endpoint *config.Endpoint) (*http.Response, error) {
-	tags := []string{
-		fmt.Sprintf("endpoint:%s", endpoint.Host),
-	}
-	defer func(now time.Time) {
-		metrics.Timing("datadog.trace_agent.telemetry_proxy.roundtrip_ms", time.Since(now), tags, 1)
-	}(time.Now())
-
-	req.Host = endpoint.Host
-	req.URL.Host = endpoint.Host
-	req.URL.Scheme = "https"
-	req.Header.Set("DD-API-KEY", endpoint.APIKey)
-
-	resp, err := m.Transport.RoundTrip(req)
-	if err != nil {
-		metrics.Count("datadog.trace_agent.telemetry_proxy.roundtrip.errors", 1, tags, 1)
-	}
-	return resp, err
-}
-
-// RoundTrip sends request first to MainTarget, then sends a copy of main request to every configurged
-// AdditionalTargets receiptient.
-//
-// All requests will be sent irregardless of any errors
-// If any request fails, the error will be logged. Only main target's
-// error will be propagated via return value
-func (m *telemetryMultiTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	if len(m.Endpoints) == 1 {
-		return m.roundTrip(req, m.Endpoints[0])
-	}
-	slurp, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		return nil, err
-	}
-	newreq := req.Clone(req.Context())
-	newreq.Body = ioutil.NopCloser(bytes.NewReader(slurp))
-	// despite the number of endpoints, we always return the response of the first
-	rresp, rerr := m.roundTrip(newreq, m.Endpoints[0])
-	for _, endpoint := range m.Endpoints[1:] {
-		newreq := req.Clone(req.Context())
-		newreq.Body = ioutil.NopCloser(bytes.NewReader(slurp))
-		if resp, err := m.roundTrip(newreq, endpoint); err == nil {
-			// we discard responses for all subsequent requests
-			io.Copy(ioutil.Discard, resp.Body)
-			resp.Body.Close()
-		} else {
-			log.Error(err)
-		}
-	}
-	return rresp, rerr
-}
-
 // telemetryProxyHandler parses returns a new HTTP handler which will proxy requests to the configured intakes.
 // If the main intake URL can not be computed because of config, the returned handler will always
 // return http.StatusInternalServerError along with a clarification.
@@ -133,4 +81,56 @@ func (r *HTTPReceiver) telemetryProxyHandler() http.Handler {
 		ErrorLog:  logger,
 		Transport: &transport,
 	}
+}
+
+// RoundTrip sends request first to Endpoint[0], then sends a copy of main request to every configurged
+// additional endpoint.
+//
+// All requests will be sent irregardless of any errors
+// If any request fails, the error will be logged. Only main target's
+// error will be propagated via return value
+func (m *telemetryMultiTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if len(m.Endpoints) == 1 {
+		return m.roundTrip(req, m.Endpoints[0])
+	}
+	slurp, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return nil, err
+	}
+	newreq := req.Clone(req.Context())
+	newreq.Body = ioutil.NopCloser(bytes.NewReader(slurp))
+	// despite the number of endpoints, we always return the response of the first
+	rresp, rerr := m.roundTrip(newreq, m.Endpoints[0])
+	for _, endpoint := range m.Endpoints[1:] {
+		newreq := req.Clone(req.Context())
+		newreq.Body = ioutil.NopCloser(bytes.NewReader(slurp))
+		if resp, err := m.roundTrip(newreq, endpoint); err == nil {
+			// we discard responses for all subsequent requests
+			io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		} else {
+			log.Error(err)
+		}
+	}
+	return rresp, rerr
+}
+
+func (m *telemetryMultiTransport) roundTrip(req *http.Request, endpoint *config.Endpoint) (*http.Response, error) {
+	tags := []string{
+		fmt.Sprintf("endpoint:%s", endpoint.Host),
+	}
+	defer func(now time.Time) {
+		metrics.Timing("datadog.trace_agent.telemetry_proxy.roundtrip_ms", time.Since(now), tags, 1)
+	}(time.Now())
+
+	req.Host = endpoint.Host
+	req.URL.Host = endpoint.Host
+	req.URL.Scheme = "https"
+	req.Header.Set("DD-API-KEY", endpoint.APIKey)
+
+	resp, err := m.Transport.RoundTrip(req)
+	if err != nil {
+		metrics.Count("datadog.trace_agent.telemetry_proxy.roundtrip.errors", 1, tags, 1)
+	}
+	return resp, err
 }
