182472b1e807258428426ef12fdc7bb56a8fe400
Derek Brown <derek.brown@datadoghq.com>
2021-12-17 13:01:54 -0800

    [windows] [npm] Switch to release-signed driver which contains fix to test. (#9630)
    * Switch to testsigned driver which contains fix to test.
    Enable TestUDPSendAndReceive test
    
    * update to release-signed build
    
    * Release Note Nit
    
    Co-authored-by: Austin Lai <76412946+alai97@users.noreply.github.com>

--- pkg/network/tracer/tracer_test.go
+++ pkg/network/tracer/tracer_test.go
@@ -433,7 +433,6 @@ func TestTCPCollectionDisabled(t *testing.T) {
 
 func TestUDPSendAndReceive(t *testing.T) {
 	// incoming.MonotonicSentBytes is 0, when it should be 512
-	skipIfWindows(t)
 
 	// Enable BPF-based system probe
 	cfg := testConfig()
