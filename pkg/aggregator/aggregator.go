c90d7fd95b5a72f294defe3200b85641147e6d45
Vickenty Fesunov <vickenty@users.noreply.github.com>
2021-12-22 12:55:13 +0100

    De-duplicate tags between contexts (#10132)
    * Add tags store package
    
    * Add TagsKey
    
    Modify context key generator to return tags hash along with the full
    context key.
    
    * Pass tags store to context resolver
    
    Create an instance of the store during aggregator initialization, and
    thread it through time and check samplers to underlying context
    resolvers.
    
    Tags store has a setting to be disabled, so add it as well.
    
    * Use tags store when creating contexts
    
    Change Context to return tags via a getter, because we're no store
    them directly as a field.
    
    * Update tests for tags store
    
    Run each sampler test twice, with and without store enabled.
    
    * Improve tags store comments
    
    * Remove outdated remark
    
    * Fix minSize
    
    * Remove useless indirection

--- pkg/aggregator/aggregator.go
+++ pkg/aggregator/aggregator.go
@@ -12,6 +12,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/DataDog/datadog-agent/pkg/aggregator/tags"
 	"github.com/DataDog/datadog-agent/pkg/epforwarder"
 	"github.com/DataDog/datadog-agent/pkg/logs/message"
 	"github.com/DataDog/datadog-agent/pkg/serializer/split"
@@ -204,6 +205,7 @@ type BufferedAggregator struct {
 	MetricSamplePool *metrics.MetricSamplePool
 
 	statsdSampler          TimeSampler
+	tagsStore              *tags.Store
 	checkSamplers          map[check.ID]*CheckSampler
 	serviceChecks          metrics.ServiceChecks
 	events                 metrics.Events
@@ -242,6 +244,8 @@ func NewBufferedAggregator(s serializer.MetricSerializer, eventPlatformForwarder
 		agentName = flavor.HerokuAgent
 	}
 
+	tagsStore := tags.NewStore(config.Datadog.GetBool("aggregator_use_tags_store"), "aggregator")
+
 	aggregator := &BufferedAggregator{
 		bufferedMetricIn:       make(chan []metrics.MetricSample, bufferSize),
 		bufferedMetricInWithTs: make(chan []metrics.MetricSample, bufferSize),
@@ -260,7 +264,8 @@ func NewBufferedAggregator(s serializer.MetricSerializer, eventPlatformForwarder
 
 		MetricSamplePool: metrics.NewMetricSamplePool(MetricSamplePoolBatchSize),
 
-		statsdSampler:           *NewTimeSampler(bucketSize),
+		tagsStore:               tagsStore,
+		statsdSampler:           *NewTimeSampler(bucketSize, tagsStore),
 		checkSamplers:           make(map[check.ID]*CheckSampler),
 		flushInterval:           flushInterval,
 		serializer:              s,
@@ -352,6 +357,7 @@ func (agg *BufferedAggregator) registerSender(id check.ID) error {
 		config.Datadog.GetInt("check_sampler_bucket_commits_count_expiry"),
 		config.Datadog.GetBool("check_sampler_expire_metrics"),
 		config.Datadog.GetDuration("check_sampler_stateful_metric_expiration_time"),
+		agg.tagsStore,
 	)
 	return nil
 }
@@ -728,6 +734,12 @@ func (agg *BufferedAggregator) run() {
 		case <-agg.TickerChan:
 			start := time.Now()
 			agg.Flush(start, false)
+
+			// Do this here, rather than in the Flush():
+			// - make sure Shrink doesn't happen concurrently with sample processing.
+			// - we don't need to Shrink() on stop
+			agg.tagsStore.Shrink()
+
 			addFlushTime("MainFlushTime", int64(time.Since(start)))
 			aggregatorNumberOfFlush.Add(1)
 			aggregatorEventPlatformErrorLogged = false
