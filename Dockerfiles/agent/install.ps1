aa65c2648f62d86cff2863776733af1e75cd16a7
Julien Lebot <julien.ar.lebot@gmail.com>
2021-12-22 16:42:52 +0100

    Use multi stage setup

--- Dockerfiles/agent/install.ps1
+++ Dockerfiles/agent/install.ps1
@@ -32,31 +32,6 @@ if ("$env:WITH_JMX" -ne "false") {
     $Env:Path="$Env:Path;C:/java/bin"
 }
 
-Expand-Archive datadog-agent-latest.amd64.zip
-Remove-Item datadog-agent-latest.amd64.zip
-
-Get-ChildItem -Path datadog-agent-* | Rename-Item -NewName "Datadog Agent"
-
-New-Item -ItemType directory -Path "C:/Program Files/Datadog"
-Move-Item "Datadog Agent" "C:/Program Files/Datadog/"
-
-#   Install 7zip module
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Set-PSRepository -Name 'PSGallery' -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
-Install-Module -Name 7Zip4PowerShell -Force
-
-#   Extract embbeded3.7z
-Expand-7Zip -ArchiveFileName "C:/Program Files/Datadog/Datadog Agent/embedded3.7z" -TargetPath "C:/Program Files/Datadog/Datadog Agent"
-Remove-Item "C:/Program Files/Datadog/Datadog Agent/embedded3.7z"
-
-If (Test-Path -Path "C:/Program Files/Datadog/Datadog Agent/embedded2.7z") {
-  Expand-7Zip -ArchiveFileName "C:/Program Files/Datadog/Datadog Agent/embedded2.7z" -TargetPath "C:/Program Files/Datadog/Datadog Agent"
-  Remove-Item "C:/Program Files/Datadog/Datadog Agent/embedded2.7z"
-}
-
-New-Item -ItemType directory -Path 'C:/ProgramData/Datadog'
-Move-Item "C:/Program Files/Datadog/Datadog Agent/EXAMPLECONFSLOCATION" "C:/ProgramData/Datadog/conf.d"
-
 $services = [ordered]@{
   "datadogagent" = "C:\Program Files\Datadog\Datadog Agent\bin\agent.exe",@()
   "datadog-process-agent" = "C:\Program Files\Datadog\Datadog Agent\bin\agent\process-agent.exe",@("datadogagent")
@@ -66,7 +41,6 @@ $services = [ordered]@{
 foreach ($s in $services.Keys) {
     Install-Service -SvcName $s -BinPath $services[$s][0] $services[$s][1]
 }
-
 
 # Allow to run agent binaries as `agent`
 setx /m PATH "$Env:Path;C:/Program Files/Datadog/Datadog Agent/bin;C:/Program Files/Datadog/Datadog Agent/bin/agent"
