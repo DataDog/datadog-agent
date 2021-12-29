0f7df55e4faaf117390f8167190c65efcc3b05e7
Sylvain Baubeau <lebauce@gmail.com>
2021-12-29 10:52:39 +0100

    Do not ignore last remaining character in variable interpolation

--- pkg/security/secl/compiler/eval/eval_test.go
+++ pkg/security/secl/compiler/eval/eval_test.go
@@ -343,6 +343,7 @@ func TestVariables(t *testing.T) {
 		Expected bool
 	}{
 		{Expr: `process.name == "/proc/${pid}/maps/${str}"`, Expected: true},
+		{Expr: `process.name == "/proc/${pid}/maps/${str}3"`, Expected: false},
 		{Expr: `process.name == "/proc/${pid}/maps/${str"`, Expected: false},
 		{Expr: `process.name == "/proc/${pid/maps/${str"`, Expected: false},
 		{Expr: `process.pid == ${pid}`, Expected: true},
