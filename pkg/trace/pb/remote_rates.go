8a279ec931b12f412ec249e866d4449a72ad5b66
raphael.gavache <raphael.gavache@datadoghq.com>
2022-01-06 13:29:45 +0100

    Fix

--- pkg/trace/pb/remote_rates.go
+++ pkg/trace/pb/remote_rates.go
@@ -2,11 +2,14 @@ package pb
 
 // TargetTPS contains the targeted traces per second the agent should try to sample for a particular service and env
 type TargetTPS struct {
-	Service   string  `msgpack:"0"`
-	Env       string  `msgpack:"1"`
-	Value     float64 `msgpack:"2"`
-	Rank      uint32  `msgpack:"3"`
-	Mechanism uint32  `msgpack:"4"`
+	Service string  `msgpack:"0"`
+	Env     string  `msgpack:"1"`
+	Value   float64 `msgpack:"2"`
+	// Rank is the rank associated to this TargetTPS. Lower ranks of a same (env, service) are discarded
+	// in favor of the highest rank.
+	Rank uint32 `msgpack:"3"`
+	// Mechanism is the identifier of the mechanism that generated this TargetTPS
+	Mechanism uint32 `msgpack:"4"`
 }
 
 // APMSampling is the list of target tps
