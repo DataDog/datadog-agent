4d3aa3ad39a16e998add5b95177e770177261f07
Olivier G <olivier.gaca@datadoghq.com>
2022-01-07 11:41:22 +0100

    Introduce `NewCompositeTags`

---
+++ pkg/metrics/composite_tags_test.go
@@ -0,0 +1,94 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// +build test
+
+package metrics
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompositeTagsConstructors(t *testing.T) {
+	tags1 := NewCompositeTags([]string{"tag1"}, []string{"tag2"})
+	tags2 := CompositeTagsFromSlice([]string{"tag1", "tag2"})
+	tags3 := CombineCompositeTagsAndSlice(CompositeTagsFromSlice([]string{"tag1"}), []string{"tag2"})
+
+	AssertCompositeTagsEqual(t, tags1, tags2)
+	AssertCompositeTagsEqual(t, tags2, tags3)
+}
+
+func TestCompositeTagsCombineCompositeTagsAndSlice(t *testing.T) {
+	// Make sure there is no reallocation later
+	tags := make([]string, 1, 10)
+	tags[0] = "tag1"
+	tags4 := NewCompositeTags(tags, []string{"tag4"})
+	tags2 := CombineCompositeTagsAndSlice(tags4, []string{"tag2"})
+	tags3 := CombineCompositeTagsAndSlice(tags4, []string{"tag3"})
+
+	assert.Equal(t, "tag1,tag2,tag4", tags2.Join(","))
+	assert.Equal(t, "tag1,tag3,tag4", tags3.Join(","))
+}
+
+func TestCompositeTagsForEach(t *testing.T) {
+	compositeTags := NewCompositeTags([]string{"tag1"}, []string{"tag2"})
+	var tags []string
+	compositeTags.ForEach(func(tag string) {
+		tags = append(tags, tag)
+	})
+	expectedTags := []string{"tag1", "tag2"}
+	r := require.New(t)
+	r.EqualValues(expectedTags, tags)
+
+	tags = nil
+	r.NoError(compositeTags.ForEachErr(func(tag string) error {
+		tags = append(tags, tag)
+		return nil
+	}))
+	require.EqualValues(t, expectedTags, tags)
+
+	r.Error(compositeTags.ForEachErr(func(tag string) error {
+		return errors.New("error")
+	}))
+}
+
+func TestCompositeTagsFind(t *testing.T) {
+	compositeTags := NewCompositeTags([]string{"tag1"}, []string{"tag2"})
+	r := require.New(t)
+	r.True(compositeTags.Find(func(tag string) bool { return tag == "tag1" }))
+	r.True(compositeTags.Find(func(tag string) bool { return tag == "tag2" }))
+	r.False(compositeTags.Find(func(tag string) bool { return tag == "tag3" }))
+}
+
+func TestCompositeTagsLen(t *testing.T) {
+	r := require.New(t)
+	r.Equal(2, NewCompositeTags([]string{"tag1"}, []string{"tag2"}).Len())
+	r.Equal(1, NewCompositeTags([]string{"tag1"}, []string{}).Len())
+	r.Equal(1, NewCompositeTags([]string{}, []string{"tag1"}).Len())
+}
+
+func TestCompositeTagsJoin(t *testing.T) {
+	tags := NewCompositeTags([]string{"tag1"}, []string{"tag2"})
+	require.Equal(t, "tag1, tag2", tags.Join(", "))
+
+	tags = CompositeTagsFromSlice([]string{"tag1", "tag2"})
+	require.Equal(t, "tag1, tag2", tags.Join(", "))
+}
+
+func TestCompositeTagsMarshalJSON(t *testing.T) {
+	r := require.New(t)
+	tags := NewCompositeTags([]string{"tag1"}, []string{"tag2"})
+
+	bytes, err := json.Marshal(tags)
+	r.NoError(err)
+	var newTags *CompositeTags
+	r.NoError(json.Unmarshal(bytes, &newTags))
+	require.Equal(t, tags.Join(", "), newTags.Join(", "))
+}
