36da6e2ecc14f602c955c05ede86f0eda00aee33
Lénaïc Huard <L3n41c@users.noreply.github.com>
2022-01-11 16:22:20 +0100

    Fix panic when parsing the `prometheus_scrape.checks` config parameter (#10498)
    ```
    panic: runtime error: invalid memory address or nil pointer dereference
    [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x957e3c]
    
    goroutine 1 [running]:
    github.com/DataDog/viper.(*Viper).find(0x400000f320, 0x2e260d1, 0x18, 0x18, 0x40009dc058)
    	/go/pkg/mod/github.com/!data!dog/viper@v1.9.0/viper.go:1053 +0x86c
    github.com/DataDog/viper.(*Viper).GetE(0x400000f320, 0x2e260d1, 0x18, 0x2751ca0, 0x4000fd2f78, 0x97b804, 0x39a5093d00000000)
    	/go/pkg/mod/github.com/!data!dog/viper@v1.9.0/viper.go:616 +0x54
    github.com/DataDog/viper.(*Viper).Get(...)
    	/go/pkg/mod/github.com/!data!dog/viper@v1.9.0/viper.go:608
    github.com/DataDog/viper.(*Viper).UnmarshalKey(0x400000f320, 0x2e260d1, 0x18, 0x26ecb60, 0x4000ca6030, 0x0, 0x0, 0x0, 0x4000fd2ff8, 0x18bc55c)
    	/go/pkg/mod/github.com/!data!dog/viper@v1.9.0/viper.go:847 +0x38
    github.com/DataDog/datadog-agent/pkg/config.(*safeConfig).UnmarshalKey(0x40001d6040, 0x2e260d1, 0x18, 0x26ecb60, 0x4000ca6030, 0x0, 0x0, 0x0, 0x0, 0x0)
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/config/viper.go:318 +0xa8
    github.com/DataDog/datadog-agent/pkg/autodiscovery/providers.getPrometheusConfigs(0xffffffffffffffff, 0x0, 0x1, 0x6, 0x400019ff18)
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/autodiscovery/providers/prometheus_common.go:18 +0x90
    github.com/DataDog/datadog-agent/pkg/autodiscovery/providers.NewPrometheusPodsConfigProvider(0x4000c5a240, 0x40005dea20, 0x2e07252, 0xf, 0x400019ff98)
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/pkg/autodiscovery/providers/prometheus_pods.go:31 +0x20
    github.com/DataDog/datadog-agent/cmd/agent/common.setupAutoDiscovery(0x4000803290, 0x3, 0x3, 0x4000866440, 0x28)
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/common/autodiscovery.go:81 +0x5e8
    github.com/DataDog/datadog-agent/cmd/agent/common.LoadComponents(0x2e26d90, 0x19)
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/common/loader.go:65 +0x290
    github.com/DataDog/datadog-agent/cmd/agent/app.StartAgent(0x4000000008, 0x3153488)
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/app/run.go:434 +0xec4
    github.com/DataDog/datadog-agent/cmd/agent/app.run(0x4b5aa60, 0x52139b8, 0x0, 0x0, 0x0, 0x0)
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/app/run.go:149 +0x174
    github.com/spf13/cobra.(*Command).execute(0x4b5aa60, 0x52139b8, 0x0, 0x0, 0x4b5aa60, 0x52139b8)
    	/go/pkg/mod/github.com/spf13/cobra@v1.2.1/command.go:856 +0x320
    github.com/spf13/cobra.(*Command).ExecuteC(0x4b57ae0, 0x4495cc, 0xffffffff00000000, 0x4000000180)
    	/go/pkg/mod/github.com/spf13/cobra@v1.2.1/command.go:974 +0x274
    github.com/spf13/cobra.(*Command).Execute(...)
    	/go/pkg/mod/github.com/spf13/cobra@v1.2.1/command.go:902
    main.main()
    	/omnibus/src/datadog-agent/src/github.com/DataDog/datadog-agent/cmd/agent/main.go:18 +0x30
    ```

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -198,6 +198,21 @@ const (
 	Metrics DataType = "metrics"
 )
 
+// prometheusScrapeChecksTransformer is a trampoline function that delays the
+// resolution of `PrometheusScrapeChecksTransformer` from `InitConfig` (invoked
+// from an `init()` function to `cobra.(*Command).Execute` (invoked from `main.main`)
+//
+// Without it, the issue is that `config.PrometheusScrapeChecksTransformer` would be:
+// * written from an `init` function from `pkg/autodiscovery/common/types` and
+// * read from an `init` function here.
+// This would result in an undefined behaviour
+//
+// With this intermediate function, it’s read from `cobra.(*Command).Execute`
+// which is called from `main.main` which is guaranteed to be called after all `init`.
+func prometheusScrapeChecksTransformer(s string) interface{} {
+	return PrometheusScrapeChecksTransformer(s)
+}
+
 func init() {
 	osinit()
 	// Configure Datadog global configuration
@@ -596,7 +611,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("prometheus_scrape.enabled", false)           // Enables the prometheus config provider
 	config.BindEnvAndSetDefault("prometheus_scrape.service_endpoints", false) // Enables Service Endpoints checks in the prometheus config provider
 	config.BindEnv("prometheus_scrape.checks")                                // Defines any extra prometheus/openmetrics check configurations to be handled by the prometheus config provider
-	config.SetEnvKeyTransformer("prometheus_scrape.checks", PrometheusScrapeChecksTransformer)
+	config.SetEnvKeyTransformer("prometheus_scrape.checks", prometheusScrapeChecksTransformer)
 	config.BindEnvAndSetDefault("prometheus_scrape.version", 1) // Version of the openmetrics check to be scheduled by the Prometheus auto-discovery
 
 	// SNMP
