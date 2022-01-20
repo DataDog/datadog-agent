5aa31772481feeaa7ba184f60ad5cbba049c6e1f
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-01-20 18:33:14 +0100

    [CWS] use syscall tester on arm64

--- pkg/security/tests/chown32_test.go
+++ pkg/security/tests/chown32_test.go
@@ -50,11 +50,7 @@ func TestChown32(t *testing.T) {
 
 	syscallTester, err := loadSyscallTester(t, test, "syscall_x86_tester")
 	if err != nil {
-		if _, ok := err.(ErrUnsupportedArch); ok {
-			t.Skip(err)
-		} else {
-			t.Fatal(err)
-		}
+		t.Fatal(err)
 	}
 
 	prevUID := 98
--- pkg/security/tests/syscall_tester.go
+++ pkg/security/tests/syscall_tester.go
@@ -9,7 +9,6 @@
 package tests
 
 import (
-	"bytes"
 	"embed"
 	"fmt"
 	"os"
@@ -19,16 +18,6 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// ErrUnsupportedArch is the error returned for unsupported architectures
-type ErrUnsupportedArch struct {
-	arch string
-}
-
-// Error returns the error string
-func (ua ErrUnsupportedArch) Error() string {
-	return fmt.Sprintf("unsupported_arch: %s", ua.arch)
-}
-
 //go:embed syscall_tester/bin
 var syscallTesterFS embed.FS
 
@@ -36,11 +25,6 @@ func loadSyscallTester(t *testing.T, test *testModule, binary string) (string, e
 	var uname unix.Utsname
 	if err := unix.Uname(&uname); err != nil {
 		return "", fmt.Errorf("couldn't resolve arch: %s", err)
-	}
-
-	switch string(uname.Machine[:bytes.IndexByte(uname.Machine[:], 0)]) {
-	case "aarch64":
-		return "", ErrUnsupportedArch{arch: "aarch64"}
 	}
 
 	testerBin, err := syscallTesterFS.ReadFile(fmt.Sprintf("syscall_tester/bin/%s", binary))
