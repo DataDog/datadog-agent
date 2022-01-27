6dc9217330bbe079119c8f1d8d3325cca9bc51dd
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-01-27 18:56:25 +0000

    fix inconsitent go build lines, preferring +build

--- pkg/util/containers/v2/metrics/docker/collector.go
+++ pkg/util/containers/v2/metrics/docker/collector.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2021-present Datadog, Inc.
 
-//go:build docker && (linux || windows)
+//go:build (docker && linux) || (docker && windows)
 // +build docker,linux docker,windows
 
 package docker
