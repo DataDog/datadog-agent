43b8347a15a34374adccc8d0e69d85e680e3c822
Harvinder Ghotra <ghotra.harvinder@gmail.com>
2022-01-27 18:47:47 -0500

    Move test over to serverless package

--- pkg/serverless/trace/span_modifer_test.go
+++ pkg/serverless/trace/span_modifer_test.go
@@ -49,3 +49,36 @@ func TestServerlessServiceRewrite(t *testing.T) {
 	}
 	assert.Equal(t, "myTestService", span.Service)
 }
+
+func TestInferredSpanFunctionTagFiltering(t *testing.T) {
+	cfg := config.New()
+	cfg.GlobalTags = map[string]string{"some": "tag", "function_arn": "arn:aws:foo:bar:baz"}
+	cfg.Endpoints[0].APIKey = "test"
+	ctx, cancel := context.WithCancel(context.Background())
+	agnt := agent.NewAgent(ctx, cfg)
+	spanModifier := &spanModifier{
+		tags: cfg.GlobalTags,
+	}
+	agnt.ModifySpan = spanModifier.ModifySpan
+	defer cancel()
+
+	tp := testutil.TracerPayloadWithChunk(testutil.RandomTraceChunk(2, 1))
+	tp.Chunks[0].Spans[0].Meta["_inferred_span.tag_source"] = "self"
+	tp.Chunks[0].Spans[1].Meta["_dd_origin"] = "lambda"
+	go agnt.Process(&api.Payload{
+		TracerPayload: tp,
+		Source:        agnt.Receiver.Stats.GetTagStats(info.Tags{}),
+	})
+	timeout := time.After(2 * time.Second)
+	select {
+	case ss := <-agnt.TraceWriter.In:
+		tp = ss.TracerPayload
+	case <-timeout:
+		t.Fatal("timed out")
+	}
+
+	_, lambdaSpanHasGlobalTags := tp.Chunks[0].Spans[1].GetMeta()["function_arn"]
+	assert.True(t, lambdaSpanHasGlobalTags, "The regular span should get global tags")
+	_, tagOriginSelfSpanHasGlobalTags := tp.Chunks[0].Spans[0].GetMeta()["function_arn"]
+	assert.False(t, tagOriginSelfSpanHasGlobalTags, "A span with meta._inferred_span.tag_origin = self should not get global tags")
+}
