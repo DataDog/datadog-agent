fc37bdd247d9147ad1d7953df4004c9a5963c912
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-01-27 09:21:08 -0500

    Support a max point count in serializing series (#10522)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -429,6 +429,7 @@ func InitConfig(config Config) {
 	// Warning: do not change the two following values. Your payloads will get dropped by Datadog's intake.
 	config.BindEnvAndSetDefault("serializer_max_payload_size", 2*megaByte+megaByte/2)
 	config.BindEnvAndSetDefault("serializer_max_uncompressed_payload_size", 4*megaByte)
+	config.BindEnvAndSetDefault("serializer_max_series_points_per_payload", 100000)
 
 	config.BindEnvAndSetDefault("use_v2_api.series", false)
 	// Serializer: allow user to blacklist any kind of payload to be sent
