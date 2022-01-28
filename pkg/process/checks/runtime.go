3dc72698409d06ff82770d898c8a2cfa3456e993
Moises Botarro <moises.bottaro.silva@gmail.com>
2022-01-28 19:56:30 +0100

    fix typo

--- pkg/process/checks/runtime.go
+++ pkg/process/checks/runtime.go
@@ -3,7 +3,7 @@ package checks
 import ddconfig "github.com/DataDog/datadog-agent/pkg/config"
 
 var (
-	// MaxBatchSize is the max number of items (processes, containers, processs-discoveries) in a RunResult
+	// MaxBatchSize is the max number of items (processes, containers, process-discoveries) in a RunResult
 	MaxBatchSize = ddconfig.DefaultProcessMaxPerMessage
 
 	// MaxCtrProcsBatchSize is the maximum number of processes belonging to a container per message
