d383e4152aac0440a41b25e4fc18d87bdd54f96a
Corrina Sivak <corrina.sivak@datadoghq.com>
2022-01-31 12:57:51 -0500

    Lowercase `enabled` to fix any capitalization errors

--- pkg/process/config/yaml_config.go
+++ pkg/process/config/yaml_config.go
@@ -73,7 +73,7 @@ func (a *AgentConfig) LoadProcessYamlConfig(path string) error {
 		//   If "false" (the default) we will only collect containers.
 		//   If "true" we will collect containers and processes.
 		//   If "disabled" the agent will be disabled altogether and won't start.
-		enabled := config.Datadog.GetString(k)
+		enabled := strings.ToLower(config.Datadog.GetString(k))
 		ok, err := isAffirmative(enabled)
 		if ok {
 			a.Enabled, a.EnabledChecks = true, processChecks
