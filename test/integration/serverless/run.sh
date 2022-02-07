205641aa744490f5c9eb39a8c8c4c946d9580e9a
Nick Hinsch <nick.hinsch@datadoghq.com>
2022-02-07 17:30:36 -0500

    Fix regex

--- test/integration/serverless/run.sh
+++ test/integration/serverless/run.sh
@@ -224,7 +224,8 @@ for function_name in "${all_functions[@]}"; do
                 # remove configuration log line from dd-trace-go
                 grep -v "DATADOG TRACER CONFIGURATION" |
                 perl -p -e "s/(timestamp\":)[0-9]{13}/\1TIMESTAMP/g" |
-                perl -p -e "s/\d{4}[\-/]\d{2}[\-/]\d{2}\s\d{2}:\d{2}:\d{2}/\1TIMESTAMP/g" |
+                perl -p -e "s/\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}/\1TIMESTAMP/g" |
+                perl -p -e "s/\d{4}/\d{2}/\d{2}\s\d{2}:\d{2}:\d{2}/\1TIMESTAMP/g" |
                 perl -p -e "s/(\"REPORT |START |END ).*/\1XXX\"}}/g" |
                 perl -p -e "s/(,\"request_id\":\")[a-zA-Z0-9\-,]+\"//g" |
                 perl -p -e "s/$stage/STAGE/g" |
