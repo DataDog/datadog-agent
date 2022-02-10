c7d78a97365332b8e07734c09220f3868f3f319e
Kylian Serrania <kylian.serrania@datadoghq.com>
2022-02-10 13:31:41 +0100

    Lint fix

--- tasks/build_tags.py
+++ tasks/build_tags.py
@@ -93,7 +93,9 @@ DOGSTATSD_TAGS = {"containerd", "docker", "kubelet", "podman", "secrets", "zlib"
 IOT_AGENT_TAGS = {"jetson", "otlp", "systemd", "zlib"}
 
 # PROCESS_AGENT_TAGS lists the tags necessary to build the process-agent
-PROCESS_AGENT_TAGS = AGENT_TAGS.union({"clusterchecks", "fargateprocess", "orchestrator"}).difference({"otlp", "python"})
+PROCESS_AGENT_TAGS = AGENT_TAGS.union({"clusterchecks", "fargateprocess", "orchestrator"}).difference(
+    {"otlp", "python"}
+)
 
 # PROCESS_AGENT_HEROKU_TAGS lists the tags necessary to build the process-agent for Heroku
 PROCESS_AGENT_HEROKU_TAGS = PROCESS_AGENT_TAGS.difference(
