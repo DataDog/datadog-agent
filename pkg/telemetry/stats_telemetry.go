83777a196940a2856eaeaeaf462e1c2d0b84e9ac
Brian Floersch <brian.floersch@datadoghq.com>
2022-02-11 15:56:14 -0500

    linter

--- pkg/telemetry/stats_telemetry.go
+++ pkg/telemetry/stats_telemetry.go
@@ -7,31 +7,32 @@ type StatsTelemetryHandler interface {
 	Count(metric string, value float64, hostname string, tags []string)
 }
 
-type statsTelemetryProvider struct {
-	sync.RWMutex
+// StatsTelemetryProvider handles stats telemetry and passes it on to a handler
+type StatsTelemetryProvider struct {
 	handler StatsTelemetryHandler
+	m       sync.RWMutex
 }
 
 var (
-	statsProvider = &statsTelemetryProvider{}
+	statsProvider = &StatsTelemetryProvider{}
 )
 
 // RegisterStatsHandler regsiters a handler to send the stats metrics
 func RegisterStatsHandler(handler StatsTelemetryHandler) {
-	statsProvider.Lock()
-	defer statsProvider.Unlock()
+	statsProvider.m.Lock()
+	defer statsProvider.m.Unlock()
 	statsProvider.handler = handler
 }
 
-// StatsTelemetryProvider gets an instance of the current stats telemetry provider
-func StatsTelemetryProvider() *statsTelemetryProvider {
+// GetStatsTelemetryProvider gets an instance of the current stats telemetry provider
+func GetStatsTelemetryProvider() *StatsTelemetryProvider {
 	return statsProvider
 }
 
 // Count reports a count metric to the handler
-func (s *statsTelemetryProvider) Count(metric string, value float64, tags []string) {
-	s.RLock()
-	defer s.RUnlock()
+func (s *StatsTelemetryProvider) Count(metric string, value float64, tags []string) {
+	s.m.RLock()
+	defer s.m.RUnlock()
 	if s.handler == nil {
 		return
 	}
