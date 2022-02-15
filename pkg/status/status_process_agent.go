df865bf7ca1bcdeef94764857492fdc5331440fc
Moisés Botarro <45740044+mbotarro@users.noreply.github.com>
2022-02-15 17:31:31 +0100

    [process-agent] Refactor status build and add it to core agent (#10886)
    * refactor status command in process-agent; add process-agent status output to core agent status command

---
+++ pkg/status/status_process_agent.go
@@ -0,0 +1,41 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package status
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/DataDog/datadog-agent/cmd/process-agent/api"
+	apiutil "github.com/DataDog/datadog-agent/pkg/api/util"
+)
+
+// GetProcessAgentStatus fetches the process-agent status from the process-agent API server
+func GetProcessAgentStatus() map[string]interface{} {
+	httpClient := apiutil.GetClient(false)
+
+	s := make(map[string]interface{})
+	addressPort, err := api.GetAPIAddressPort()
+	if err != nil {
+		s["error"] = fmt.Sprintf("%v", err.Error())
+		return s
+	}
+
+	statusEndpoint := fmt.Sprintf("http://%s/agent/status", addressPort)
+	b, err := apiutil.DoGet(httpClient, statusEndpoint)
+	if err != nil {
+		s["error"] = fmt.Sprintf("%v", err.Error())
+		return s
+	}
+
+	err = json.Unmarshal(b, &s)
+	if err != nil {
+		s["error"] = fmt.Sprintf("%v", err.Error())
+		return s
+	}
+
+	return s
+}
