da16155988dc8ad803ee522367e36845e3ac31d3
Arthur Bellal <arthur.bellal@datadoghq.com>
2022-02-16 22:12:07 +0100

    (rcm-88) receive and send back a "client_state" controlled by the backend (#10869)
    * (rcm-88) receive and send back a "client_state" controlled by the backend
    
    * re-generate protos
    
    * add testing
    
    * licenses
    
    * update tooling
    
    * tidy

--- pkg/config/remote/uptane/client.go
+++ pkg/config/remote/uptane/client.go
@@ -175,6 +175,13 @@ func (c *Client) State() (State, error) {
 	return s, nil
 }
 
+// TargetsCustom returns the current targets custom of this uptane client
+func (c *Client) TargetsCustom() ([]byte, error) {
+	c.Lock()
+	defer c.Unlock()
+	return c.directorLocalStore.GetMetaCustom(metaTargets)
+}
+
 // DirectorRoot returns a director root
 func (c *Client) DirectorRoot(version uint64) ([]byte, error) {
 	c.Lock()
