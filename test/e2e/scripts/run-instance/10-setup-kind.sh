75fed65af345400bebb815f13612c6aab0d24126
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2022-02-22 17:16:58 +0100

    Do not download kubectl nor argo each run

--- test/e2e/scripts/run-instance/10-setup-kind.sh
+++ test/e2e/scripts/run-instance/10-setup-kind.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+download_and_install_kubectl() {
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install kubectl /usr/local/bin/kubectl
+}
+
 printf '=%.0s' {0..79} ; echo
 set -x
 
@@ -11,11 +16,21 @@ then
     exit 1
 fi
 
-KIND_VERSION=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/kubernetes-sigs/kind/releases | jq -r '.[0].name')
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install kubectl /usr/local/bin/kubectl
-curl -Lo ./kind "https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-linux-amd64"
+# if kubctl is not here, download it
+if [[ ! -f ./kubectl ]]; then
+    download_and_install_kubectl
+else
+    # else, download the SHA256 of the wanted version
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    # And if it differs, force the download again
+    if ! echo "$(<kubectl.sha256)  kubectl" | sha256sum --check ; then
+        echo "SHA256 of kubectl differs, downloading it again"
+        download_and_install_kubectl
+    fi
+fi
+
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/kubernetes-sigs/kind/releases | jq -r '.[0].name')/kind-linux-amd64"
 sudo install kind /usr/local/bin/kind
 
 sudo usermod -a -G docker core
