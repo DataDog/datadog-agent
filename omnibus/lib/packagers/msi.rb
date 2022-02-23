f2eb59de3c74c59ab03b01f7d6837f0107120e48
Julien Lebot <julien.lebot@datadoghq.com>
2022-02-23 12:55:17 +0100

    Override install_dir from base packager, cleanup code and add comments

--- omnibus/lib/packagers/msi.rb
+++ omnibus/lib/packagers/msi.rb
@@ -3,35 +3,42 @@ require "./lib/ostools.rb"
 module Omnibus
   class Packager::MSI
 
-    def safe_install_dir
-      "#{windows_safe_path(staging_dir)}\\install_dir"
+    # Override the default install dir to place the install files in
+    # the staging directory. That way we can modify it as we want and it won't
+    # impact other packagers, and Omnibus will take care of cleaning it for us after the build.
+    def install_dir
+      "#{staging_dir}\\install_dir"
+    end
+
+    def generate_embedded_archive(version)
+      safe_embedded_path = windows_safe_path(install_dir, "embedded#{version}")
+      safe_embedded_archive_path = windows_safe_path(install_dir, "embedded#{version}.7z")
+
+      shellout!(
+        <<-EOH.split.join(" ").squeeze(" ").strip
+          7z a -mx=5 -ms=on #{safe_embedded_archive_path} #{safe_embedded_path}
+      EOH
+      )
+      FileUtils.rm_rf "#{safe_embedded_path}"
     end
 
     def heat_command
+      safe_source_install_dir = windows_safe_path(project.install_dir)
+      safe_install_dir = windows_safe_path(install_dir)
 
       # Create a copy of the install directory
-      FileUtils.copy_entry "#{windows_safe_path(project.install_dir)}", safe_install_dir
+      FileUtils.copy_entry safe_source_install_dir, safe_install_dir
 
       # Create the embedded zips and delete their folders
-      shellout!(
-        <<-EOH.split.join(" ").squeeze(" ").strip
-          7z a -mx=5 -ms=on #{windows_safe_path(safe_install_dir)}\\embedded3.7z #{windows_safe_path(safe_install_dir)}\\embedded3
-        EOH
-      )
-      FileUtils.rmdir "#{safe_install_dir}\\embedded3"
+      generate_embedded_archive(3)
 
       if with_python_runtime? "2"
-        shellout!(
-          <<-EOH.split.join(" ").squeeze(" ").strip
-            7z a -mx=5 -ms=on #{windows_safe_path(safe_install_dir)}\\embedded2.7z #{windows_safe_path(safe_install_dir)}\\embedded2
-          EOH
-        )
-        FileUtils.rmdir "#{safe_install_dir}\\embedded2"
+        generate_embedded_archive(2)
       end
 
       # Return this heat command that points to our safe_install_dir
       <<-EOH.split.join(" ").squeeze(" ").strip
-          heat.exe dir "#{windows_safe_path(safe_install_dir)}"
+          heat.exe dir "#{safe_install_dir}"
             -nologo -srd -sreg -gg -cg ProjectDir
             -dr PROJECTLOCATION
             -var "var.ProjectSourceDir"
