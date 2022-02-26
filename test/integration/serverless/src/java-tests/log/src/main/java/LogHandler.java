88451129bd15dbea72ccecbe4ff16c57e552d14d
David Lee <david.lee@datadoghq.com>
2022-02-26 18:16:03 -0500

    Update LogHandler.java

--- test/integration/serverless/src/java-tests/log/src/main/java/LogHandler.java
+++ test/integration/serverless/src/java-tests/log/src/main/java/LogHandler.java
@@ -8,21 +8,29 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2ProxyRequestEven
 import com.datadoghq.datadog_lambda_java.DDLambda;
 
 public class LogHandler implements RequestHandler<APIGatewayV2ProxyRequestEvent, Map<String, Object>>{
-    public Map<String, Object> handleRequest(APIGatewayV2ProxyRequestEvent request, Context context) throws InterruptedException{
+    public Map<String, Object> handleRequest(APIGatewayV2ProxyRequestEvent request, Context context) {
         DDLambda ddl = new DDLambda(context); //Required to initialize the trace
 		Map<String, Object> res = new LinkedHashMap();
 		res.put("statusCode", 200);
 		res.put("body", "ok");
 
-		Thread.sleep(250);
+		sleepHelper();
 		System.out.println("XXX Log 0 XXX");
-		Thread.sleep(250);
+		sleepHelper();
 		System.out.println("XXX Log 1 XXX");
-		Thread.sleep(250);
+		sleepHelper();
 		System.out.println("XXX Log 2 XXX");
-		Thread.sleep(250);
+		sleepHelper();
 
 		ddl.finish();
 		return res;
     }
+
+	private void sleepHelper() {
+		try {
+			Thread.sleep(250);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
