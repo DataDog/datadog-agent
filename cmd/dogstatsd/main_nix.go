52fe20a8074a25415e78cc8917897d8e6cc5c9f0
keisku <keisuke.umegaki@datadoghq.com>
2022-03-02 22:17:12 +0900

    shutdown expvar server of dogstatd

--- cmd/dogstatsd/main_nix.go
+++ cmd/dogstatsd/main_nix.go
@@ -9,6 +9,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
@@ -25,16 +26,28 @@ func main() {
 	flavor.SetFlavor(flavor.Dogstatsd)
 
 	// go_expvar server
+	port := config.Datadog.GetInt("dogstatsd_stats_port")
+	s := &http.Server{
+		Addr:    fmt.Sprintf("127.0.0.1:%d", port),
+		Handler: http.DefaultServeMux,
+	}
+	defer func() {
+		if err := s.Shutdown(context.Background()); err != nil {
+			log.Errorf("Error shutting down dogstatsd stats server on port %d: %s", port, err)
+		}
+	}()
 	go func() {
-		port := config.Datadog.GetInt("dogstatsd_stats_port")
-		err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), http.DefaultServeMux)
+		err := s.ListenAndServe()
 		if err != nil && err != http.ErrServerClosed {
-			log.Errorf("Error creating expvar server on port %v: %v", port, err)
+			log.Errorf("Error creating dogstatsd stats server on port %v: %v", port, err)
 		}
 	}()
 
 	if err := dogstatsdCmd.Execute(); err != nil {
 		log.Error(err)
-		os.Exit(-1)
+		// os.Exit() must be called last because it terminates immediately;
+		// deferred functions are not run. Deferred function calls are executed
+		// in Last In First Out order after the surrounding function returns.
+		defer os.Exit(-1)
 	}
 }
