0c49f98516dc24ad50f9ab4d8257a666a2fa06d1
Kylian Serrania <kylian.serrania@datadoghq.com>
2022-03-03 16:09:29 +0100

    Test conversion to Sketch

--- pkg/quantile/ddsketch.go
+++ pkg/quantile/ddsketch.go
@@ -8,6 +8,7 @@ package quantile
 import (
 	"fmt"
 	"math"
+	"sort"
 
 	"github.com/DataDog/datadog-agent/pkg/quantile/summary"
 	"github.com/DataDog/sketches-go/ddsketch"
@@ -29,7 +30,7 @@ func convertToCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*dd
 	negativeStore := store.NewDenseStore()
 
 	gamma := c.gamma.v
-	offset := float64(c.norm.bias) + 0.5
+	offset := float64(c.norm.bias)
 	newMapping, err := mapping.NewLogarithmicMappingWithGamma(gamma, offset)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't create LogarithmicMapping for DDSketch: %v", err)
@@ -37,6 +38,40 @@ func convertToCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*dd
 
 	newSketch := inputSketch.ChangeMapping(newMapping, positiveStore, negativeStore, 1.0)
 	return newSketch, nil
+}
+
+type floatKeyCount struct {
+	k int
+	c float64
+}
+
+func keyCountsFromFloatKeyCounts(floatKeyCounts []floatKeyCount) []KeyCount {
+	keyCounts := make([]KeyCount, 0, len(floatKeyCounts))
+
+	sort.Slice(floatKeyCounts, func(i, j int) bool {
+		return floatKeyCounts[i].k < floatKeyCounts[j].k
+	})
+
+	leftoverCount := 0.0
+	for _, fkc := range floatKeyCounts {
+		key := fkc.k
+		count := fkc.c
+
+		// Add leftovers from previous bucket, and compute leftovers
+		// for the next bucket
+		count += leftoverCount
+		uintCount := uint(count)
+		leftoverCount = count - float64(uintCount)
+
+		keyCounts = append(keyCounts, KeyCount{k: Key(key), n: uintCount})
+	}
+
+	if leftoverCount >= 0.5 {
+		lastIndex := len(keyCounts) - 1
+		keyCounts[lastIndex] = KeyCount{k: keyCounts[lastIndex].k, n: keyCounts[lastIndex].n + 1}
+	}
+
+	return keyCounts
 }
 
 func fromCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*Sketch, error) {
@@ -50,7 +85,7 @@ func fromCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*Sketch,
 
 	var loopErr error
 
-	keyCounts := make([]KeyCount, 0, defaultBinListSize)
+	floatKeyCounts := make([]floatKeyCount, 0, defaultBinListSize)
 	inputSketch.ForEach(func(value, count float64) bool {
 		if count <= 0 {
 			loopErr = fmt.Errorf("negative counts are not supported: got %f", count)
@@ -67,7 +102,12 @@ func fromCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*Sketch,
 			value = -value
 		}
 
-		index := inputSketch.Index(value)
+		var index int
+		if value == 0 {
+			index = 0
+		} else {
+			index = inputSketch.Index(value)
+		}
 
 		if index >= maxIndex {
 			loopErr = fmt.Errorf("index value %d exceeds the maximum supported index value (%d)", index, maxIndex)
@@ -82,8 +122,7 @@ func fromCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*Sketch,
 			return false
 		}
 
-		// Should we really round the values with uint()?
-		keyCounts = append(keyCounts, KeyCount{k: Key(sign * index), n: uint(count)})
+		floatKeyCounts = append(floatKeyCounts, floatKeyCount{k: sign * index, c: count})
 		return false
 	})
 
@@ -92,13 +131,19 @@ func fromCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*Sketch,
 	}
 
 	// Finally, add the 0 key
-	keyCounts = append(keyCounts, KeyCount{k: Key(0), n: uint(zeroes)})
+	floatKeyCounts = append(floatKeyCounts, floatKeyCount{k: 0, c: zeroes})
+
+	keyCounts := keyCountsFromFloatKeyCounts(floatKeyCounts)
 
 	// Populate sparseStore object with the collected keyCounts
 	sparseStore.insertCounts(c, keyCounts)
 
 	// Create summary object
-	cnt := int64(inputSketch.GetCount())
+	// Calculate the total count that was inserted in the Sketch
+	var cnt uint
+	for _, v := range keyCounts {
+		cnt += v.n
+	}
 	sum := inputSketch.GetSum()
 	avg := sum / float64(cnt)
 	max, err := inputSketch.GetValueAtQuantile(1.0)
@@ -112,7 +157,7 @@ func fromCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*Sketch,
 	}
 
 	summary := summary.Summary{
-		Cnt: cnt,
+		Cnt: int64(cnt),
 		Sum: sum,
 		Avg: avg,
 		Max: max,
@@ -125,7 +170,6 @@ func fromCompatibleDDSketch(c *Config, inputSketch *ddsketch.DDSketch) (*Sketch,
 	}
 
 	return outputSketch, nil
-
 }
 
 // FromDDSketch converts a DDSketch into a Sketch
--- pkg/quantile/ddsketch_test.go
+++ pkg/quantile/ddsketch_test.go
@@ -23,7 +23,7 @@ func generateDDSketch(quantile func(float64) float64, N, M int) *ddsketch.DDSket
 	return sketch
 }
 
-func TestGenerateDDSketch(t *testing.T) {
+func TestConvertToCompatibleDDSketch(t *testing.T) {
 	N := 1_000
 	M := 50
 
@@ -49,7 +49,7 @@ func TestGenerateDDSketch(t *testing.T) {
 				b := float64(N)
 				alpha := 12.0 / math.Pow(b-a, 3)
 				beta := (b + a) / 2.0
-				return alpha / 3 * (math.Pow(x-beta, 3) + math.Pow(beta - a, 3))
+				return alpha / 3 * (math.Pow(x-beta, 3) + math.Pow(beta-a, 3))
 			},
 			quantile: func(y float64) float64 {
 				a := 0.0
@@ -60,10 +60,10 @@ func TestGenerateDDSketch(t *testing.T) {
 				// golang's math.Pow doesn't like negative numbers as the first argument
 				// (it will return Nan), even though cubic roots of negative numbers are defined.
 				sign := 1.0
-				if 3 / alpha * y - math.Pow(beta - a, 3) < 0 {
+				if 3/alpha*y-math.Pow(beta-a, 3) < 0 {
 					sign = -1.0
 				}
-				return beta + sign * math.Pow(sign * (3 / alpha * y - math.Pow(beta - a, 3)), 1.0/3.0)
+				return beta + sign*math.Pow(sign*(3/alpha*y-math.Pow(beta-a, 3)), 1.0/3.0)
 			},
 		},
 	}
@@ -111,11 +111,104 @@ func TestGenerateDDSketch(t *testing.T) {
 					// and the actual value (ie. the quantile value in the output sketch)
 					// should have a relative error that's less than the sum of the relative errors
 					// of each sketch.
-					sketch.RelativeAccuracy() + convertedSketch.RelativeAccuracy(),
+					sketch.RelativeAccuracy()+convertedSketch.RelativeAccuracy(),
 					fmt.Sprintf("error too high for p%d", i),
 				)
 			}
 
+		})
+	}
+}
+
+func TestFromCompatibleDDSketch(t *testing.T) {
+	N := 1_000
+	M := 50
+
+	tests := []struct {
+		// distribution name
+		name string
+		// the cumulative distribution function (within [0,N])
+		cdf func(x float64) float64
+		// the quantile function (within [0,1])
+		quantile func(x float64) float64
+	}{
+		{
+			// https://en.wikipedia.org/wiki/Continuous_uniform_distribution
+			name:     "Uniform distribution (a=0,b=N)",
+			cdf:      func(x float64) float64 { return x / float64(N) },
+			quantile: func(y float64) float64 { return y * float64(N) },
+		},
+		{
+			// https://en.wikipedia.org/wiki/U-quadratic_distribution
+			name: "U-quadratic distribution (a=0,b=N)",
+			cdf: func(x float64) float64 {
+				a := 0.0
+				b := float64(N)
+				alpha := 12.0 / math.Pow(b-a, 3)
+				beta := (b + a) / 2.0
+				return alpha / 3 * (math.Pow(x-beta, 3) + math.Pow(beta-a, 3))
+			},
+			quantile: func(y float64) float64 {
+				a := 0.0
+				b := float64(N)
+				alpha := 12.0 / math.Pow(b-a, 3)
+				beta := (b + a) / 2.0
+
+				// golang's math.Pow doesn't like negative numbers as the first argument
+				// (it will return NaN), even though cubic roots of negative numbers are defined.
+				sign := 1.0
+				if 3/alpha*y-math.Pow(beta-a, 3) < 0 {
+					sign = -1.0
+				}
+				return beta + sign*math.Pow(sign*(3/alpha*y-math.Pow(beta-a, 3)), 1.0/3.0)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			sketch := generateDDSketch(test.quantile, 100, M)
+
+			// Check the quantiles are approximately correct
+			for i := 1; i <= 100; i++ {
+				q := (float64(i)) / 100.0
+				expectedValue := test.quantile(q)
+
+				quantileValue, err := sketch.GetValueAtQuantile(q)
+				assert.NoError(t, err)
+				assert.InEpsilon(t,
+					// test that the quantile value returned by the sketch is vithin the relative accuracy
+					// of the expected value
+					expectedValue,
+					quantileValue,
+					sketch.RelativeAccuracy(),
+					fmt.Sprintf("error too high for p%d", i),
+				)
+			}
+
+			sketchConfig := Default()
+			convertedSketch, err := convertToCompatibleDDSketch(sketchConfig, sketch)
+			assert.NoError(t, err)
+
+			outputSketch, err := fromCompatibleDDSketch(sketchConfig, convertedSketch)
+			assert.NoError(t, err)
+
+			// Check the quantiles are approximately correct
+			for i := 1; i <= 100; i++ {
+				q := (float64(i)) / 100.0
+				expectedValue, err := convertedSketch.GetValueAtQuantile(q)
+				assert.NoError(t, err)
+
+				quantileValue := outputSketch.Quantile(sketchConfig, q)
+				assert.InEpsilon(t,
+					// test that the quantile value returned by the sketch is vithin the relative accuracy
+					// of the expected value
+					expectedValue,
+					quantileValue,
+					0.01,
+					fmt.Sprintf("error too high for p%d", i),
+				)
+			}
 		})
 	}
 }
