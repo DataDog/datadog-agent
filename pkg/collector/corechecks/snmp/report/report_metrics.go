f74dec19d35cdf30ab2a7e496c8019c755912818
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-03-03 09:36:15 +0100

    Remove copyStrings that are not needed anymore

--- pkg/collector/corechecks/snmp/report/report_metrics.go
+++ pkg/collector/corechecks/snmp/report/report_metrics.go
@@ -145,26 +145,22 @@ func (ms *MetricSender) sendMetric(metricName string, value valuestore.ResultVal
 
 // Gauge wraps Sender.Gauge
 func (ms *MetricSender) Gauge(metric string, value float64, tags []string) {
-	// we need copy tags before using Sender due to https://github.com/DataDog/datadog-agent/issues/7159
-	ms.sender.Gauge(metric, value, ms.hostname, common.CopyStrings(tags))
+	ms.sender.Gauge(metric, value, ms.hostname, tags)
 }
 
 // Rate wraps Sender.Rate
 func (ms *MetricSender) Rate(metric string, value float64, tags []string) {
-	// we need copy tags before using Sender due to https://github.com/DataDog/datadog-agent/issues/7159
-	ms.sender.Rate(metric, value, ms.hostname, common.CopyStrings(tags))
+	ms.sender.Rate(metric, value, ms.hostname, tags)
 }
 
 // MonotonicCount wraps Sender.MonotonicCount
 func (ms *MetricSender) MonotonicCount(metric string, value float64, tags []string) {
-	// we need copy tags before using Sender due to https://github.com/DataDog/datadog-agent/issues/7159
-	ms.sender.MonotonicCount(metric, value, ms.hostname, common.CopyStrings(tags))
+	ms.sender.MonotonicCount(metric, value, ms.hostname, tags)
 }
 
 // ServiceCheck wraps Sender.ServiceCheck
 func (ms *MetricSender) ServiceCheck(checkName string, status metrics.ServiceCheckStatus, tags []string, message string) {
-	// we need copy tags before using Sender due to https://github.com/DataDog/datadog-agent/issues/7159
-	ms.sender.ServiceCheck(checkName, status, ms.hostname, common.CopyStrings(tags), message)
+	ms.sender.ServiceCheck(checkName, status, ms.hostname, tags, message)
 }
 
 // GetSubmittedMetrics returns submitted metrics count
