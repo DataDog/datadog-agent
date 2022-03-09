b7be79b0bada1071ebe1da5ffd8addca732a3ce4
Jordan Storms <jordan.storms@datadoghq.com>
2022-03-09 11:09:42 -0500

    net6.0

--- test/integration/serverless/build_csharp_functions.sh
+++ test/integration/serverless/build_csharp_functions.sh
@@ -4,7 +4,7 @@ echo "Building C# Lambda functions for $ARCHITECTURE architecture"
 cd src/csharp-tests
 dotnet restore --verbosity quiet
 set +e #set this so we don't exit if the tools are already installed
-dotnet tool install -g Amazon.Lambda.Tools --framework netcoreapp3.1 --verbosity quiet
+dotnet tool install -g Amazon.Lambda.Tools --framework net6.0 --verbosity quiet
 set -e
 
 if [ $ARCHITECTURE == "arm64" ]; then
@@ -14,4 +14,4 @@ else
     CONVERTED_ARCH="x86_64"
 fi
 
-dotnet lambda package --configuration Release --framework netcoreapp3.1 --verbosity quiet --output-package bin/Release/netcoreapp3.1/handler.zip --function-architecture $CONVERTED_ARCH
+dotnet lambda package --configuration Release --framework net6.0 --verbosity quiet --output-package bin/Release/net6.0/handler.zip --function-architecture $CONVERTED_ARCH
