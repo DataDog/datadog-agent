716e1953c695ebff0f7f88799ba4bb80a617ce60
Bryce Kahle <bryce.kahle@datadoghq.com>
2022-03-10 10:09:06 -0800

    Add comment

--- pkg/ebpf/c/kconfig.h
+++ pkg/ebpf/c/kconfig.h
@@ -2,6 +2,8 @@
 #define __KCONFIG_H
 
 #include <linux/kconfig.h>
+// include asm/compiler.h to fix `error: expected string literal in 'asm'` compilation error coming from mte-kasan.h
+// this was fixed in https://github.com/torvalds/linux/commit/b859ebedd1e730bbda69142fca87af4e712649a1
 #include <asm/compiler.h>
 
 #endif
