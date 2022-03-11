7a3e70b03179e1dfe0a5a7b77bdcaae33e9526bc
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-03-11 15:18:25 +0100

    Add desc

--- pkg/snmp/constants.go
+++ pkg/snmp/constants.go
@@ -1,4 +1,8 @@
 package snmp
 
 // DeviceReachableGetNextOid is used in getNext call to check if the device is reachable
+// GETNEXT 1.0 should be able to fetch the first available SNMP OID.
+// There is no need to handle top node other than iso(1) since it the only valid SNMP tree starting point.
+// Other top nodes like ccitt(0) and joint(2) do not pertain to SNMP.
+// Source: https://docstore.mik.ua/orelly/networking_2ndEd/snmp/ch02_03.htm
 const DeviceReachableGetNextOid = "1.0"
