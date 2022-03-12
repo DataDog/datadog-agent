efb50374b954c26724a166f950ffd2c1251e868c
Logan Rohloff <logan@rapdev.io>
2022-03-12 15:50:41 -0500

    testing keyvault.go

--- cmd/secret-backend/backend/azure/keyvault.go
+++ cmd/secret-backend/backend/azure/keyvault.go
@@ -1,9 +1,20 @@
 package azure
 
-type AwsKeyVaultBackendConfig struct {
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/2016-10-01/keyvault"
+	"github.com/mitchellh/mapstructure"
+	"github.com/rapdev-io/datadog-secret-backend/secret"
+	log "github.com/sirupsen/logrus"
+)
+
+type AzureKeyVaultBackendConfig struct {
 	AzureSession AzureSessionBackendConfig   `mapstructure:"azure_session"`
 	BackendType  string                      `mapstructure:"backend_type"`
 	KeyVaultURL  string                      `mapstructure:"keyvaulturl"`
+	SecretId     string                      `mapstructure:"secret_id"`
 }
 
 type AzureKeyVaultBackend struct {
@@ -12,3 +23,58 @@ type AzureKeyVaultBackend struct {
 	Secret    map[string]string
 }
 
+func NewAzureKeyVaultBackendConfig(backendId string, bc map[string]interface{}) (*AzureKeyVaultBackend, error) {
+	backendConfig := AzureKeyVaultBackendConfig{}
+	err := mapstructure.Decode(bc, &backendConfig)
+	if err != nil {
+		log.WithError(err).Error("failed to map backend configuration")
+		return nil, err
+	}
+
+	cfg, err := NewAzureConfigFromBackendConfig(backendId, backendConfig.AzureSession)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"backend_id": backendId,
+		}).WithError(err).Error("failed to initialize Azure session")
+		return nil, err
+	}
+	client := keyvault.New()
+	client.Authorizer = cfg
+
+	out, err := client.GetSecret(context.Background(), AzureKeyVaultBackendConfig.KeyVaultURL, AzureKeyVaultBackendConfig.SecretId)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"backend_id": backendId,
+			"backend_type": backendConfig.BackendType,
+			"secret_id": backendConfig.SecretId,
+			"keyvaulturl": backendConfig.KeyVaultURL
+		}).WithError(err).Error("failed to retrieve secret value")
+		return nil, err
+	}
+
+	secretValue := make(map[string]string, 0)
+	if err := json.Unmarshal([]byte(*out.Value), &secretValue); err != nil {
+		log.WithFields(log.Fields{
+			"backend_id": backendId,
+			"backend_type": backendConfig.BackendType,
+			"secret_id": backendConfig.SecretId,
+			"keyvaulturl": backendConfig.KeyVaultURL
+		}).WithError(err).Error("failed to retrieve secret value")
+		return nil, err
+	}
+}
+
+func (b *AzureKeyVaultBackend) GetSecretOutput(secretKey string) secret.SecretOutput {
+	if val, ok := b.Secret[secretKey]; ok {
+		return secret.SecretOutput{Value &val, Error: nil}
+	}
+	es := errors.New("backend does not provide secret key").Error()
+	
+	log.WithFields(log.Fields{
+		"backend_id": backendId,
+		"backend_type": backendConfig.BackendType,
+		"secret_id": backendConfig.SecretId,
+		"keyvaulturl": backendConfig.KeyVaultURL
+	}).Error("backend does not provide secret key")
+	return secret.SecretOutput{Value: nil, Error: &es}
+}
--- cmd/secret-backend/go.mod
+++ cmd/secret-backend/go.mod
@@ -33,10 +33,13 @@ require (
 )
 
 require (
+	github.com/Azure/azure-sdk-for-go v62.1.0+incompatible // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
 	github.com/Azure/go-autorest/autorest/adal v0.9.11 // indirect
 	github.com/Azure/go-autorest/autorest/azure/cli v0.4.2 // indirect
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
 	github.com/Azure/go-autorest/logger v0.2.0 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/dimchansky/utfbom v1.1.1 // indirect
