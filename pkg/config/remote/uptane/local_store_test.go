bb438b9ee82cce7073f60d409f4f4903339e453e
Carlos Roman <carlos.roman@datadoghq.com>
2022-03-17 16:37:53 +0000

    [tasks] Updating copyright exlusion list.

--- pkg/config/remote/uptane/local_store_test.go
+++ pkg/config/remote/uptane/local_store_test.go
@@ -1,9 +1,9 @@
-package uptane
-
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-present Datadog, Inc.
+// Copyright 2021-present Datadog, Inc.
+
+package uptane
 
 import (
 	"encoding/json"
