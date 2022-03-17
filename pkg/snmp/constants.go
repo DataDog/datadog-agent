f214570f8954ad061df1a9fb230c905e2f69f2b3
Carlos Roman <carlos.roman@datadoghq.com>
2022-03-17 16:37:53 +0000

    [tasks] Updating copyright exlusion list.

--- pkg/snmp/constants.go
+++ pkg/snmp/constants.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022-present Datadog, Inc.
+
 package snmp
 
 // DeviceReachableGetNextOid is used in getNext call to check if the device is reachable
