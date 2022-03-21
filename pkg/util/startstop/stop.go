ad7cb805280b801961d5a0e8597f4c85b3215d3e
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-03-21 11:02:46 -0400

    Move 'restart' utility out of pkg/logs (#11285)
    * Move pkg/logs/restart to pkg/util/startstop
    
    This package is so useful that it's used in several places in the
    codebase unrelated to the logs agent, so pkg/util is a better place for
    it.
    
    This also renames `Restartable` to `StartStoppable`, since the interface
    does not relate to restarting.

--- pkg/util/startstop/stop.go
+++ pkg/util/startstop/stop.go
@@ -3,10 +3,15 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package restart
+package startstop
 
-// Restartable represents a startable and stopable object
-type Restartable interface {
-	Startable
+// Stoppable represents a stoppable object
+type Stoppable interface {
+	Stop()
+}
+
+// Stopper stops a group of stoppable objects from a data pipeline
+type Stopper interface {
 	Stoppable
+	Add(components ...Stoppable)
 }
