e3a68208509570678593bd0cb8d550ed0dabe526
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2022-03-25 11:24:59 +0100

    [pkg/otlp] Deprecate `metrics.send_monotonic_counter` in favor of `metrics.sums.cumulative_monotonic_mode` (#11406)
    * [pkg/otlp] Deprecate `metrics.send_monotonic_counter` in favor of `metrics.sums.cumulative_monotonic_mode`
    
    * Update configuration template and add env variable
    
    * Delete renames.dat
    
    * Address review comments
    
    This does the same changes as open-telemetry/opentelemetry-collector-contrib@027a481f84f9b8204993c2a280145c189b640936

--- pkg/config/otlp.go
+++ pkg/config/otlp.go
@@ -162,4 +162,5 @@ func setupOTLPEnvironmentVariables(config Config) {
 	config.BindEnv(OTLPSection + ".metrics.tag_cardinality")
 	config.BindEnv(OTLPSection + ".metrics.histograms.mode")
 	config.BindEnv(OTLPSection + ".metrics.histograms.send_count_sum_metrics")
+	config.BindEnv(OTLPSection + ".metrics.sums.cumulative_monotonic_mode")
 }
