8364fced29bb3bb6b9b0b3a1b5fb16e266e035d7
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2022-04-05 14:32:23 +0200

    add common.SetupInternalProfiling()

--- cmd/cluster-agent/app/app.go
+++ cmd/cluster-agent/app/app.go
@@ -39,7 +39,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/clusteragent/clusterchecks"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/config/resolver"
-	"github.com/DataDog/datadog-agent/pkg/config/settings"
 	"github.com/DataDog/datadog-agent/pkg/forwarder"
 	"github.com/DataDog/datadog-agent/pkg/serializer"
 	"github.com/DataDog/datadog-agent/pkg/status/health"
@@ -159,24 +158,7 @@ func start(cmd *cobra.Command, args []string) error {
 	}
 
 	// Setup Internal Profiling
-	if v := config.Datadog.GetInt("internal_profiling.block_profile_rate"); v > 0 {
-		if err := settings.SetRuntimeSetting("runtime_block_profile_rate", v); err != nil {
-			log.Errorf("Error setting block profile rate: %v", err)
-		}
-	}
-
-	if v := config.Datadog.GetInt("internal_profiling.mutex_profile_fraction"); v > 0 {
-		if err := settings.SetRuntimeSetting("runtime_mutex_profile_fraction", v); err != nil {
-			log.Errorf("Error mutex profile fraction: %v", err)
-		}
-	}
-
-	if config.Datadog.GetBool("internal_profiling.enabled") {
-		err := settings.SetRuntimeSetting("internal_profiling", true)
-		if err != nil {
-			log.Errorf("Error starting profiler: %v", err)
-		}
-	}
+	common.SetupInternalProfiling()
 
 	if !config.Datadog.IsSet("api_key") {
 		log.Critical("no API key configured, exiting")
