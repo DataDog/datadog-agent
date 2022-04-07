ed3c2a25f416a1fb6bf31d0d1a3577732918b640
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2022-04-08 01:09:10 +0200

    Fix dependency of deb packages on datadog-signing-keys (#11600)

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -53,7 +53,7 @@ else
   end
 
   if debian?
-    runtime_recommended_dependency 'datadog-signing-keys (>= 1.1.0)'
+    runtime_recommended_dependency 'datadog-signing-keys (>= 1:1.1.0)'
   end
 
   if osx?
