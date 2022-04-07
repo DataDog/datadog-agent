6a82ceb0326bce7953a05b3920076d201cdaa2a3
Vincent Boulineau <vincent.boulineau@datadoghq.com>
2022-04-07 10:21:13 +0200

    Remove YAML Marshal/Unmarshal in AD config poller flow

--- pkg/autodiscovery/config_poller.go
+++ pkg/autodiscovery/config_poller.go
@@ -20,7 +20,7 @@ import (
 // `ConfigProvider` and whether it should be polled or not.
 type configPoller struct {
 	provider     providers.ConfigProvider
-	configs      []integration.Config
+	configs      map[uint64]*integration.Config
 	canPoll      bool
 	isPolling    bool
 	pollInterval time.Duration
@@ -31,20 +31,10 @@ type configPoller struct {
 func newConfigPoller(provider providers.ConfigProvider, canPoll bool, interval time.Duration) *configPoller {
 	return &configPoller{
 		provider:     provider,
-		configs:      []integration.Config{},
+		configs:      make(map[uint64]*integration.Config),
 		canPoll:      canPoll,
 		pollInterval: interval,
 	}
-}
-
-// contains checks if the providerDescriptor contains the Config passed
-func (pd *configPoller) contains(c *integration.Config) bool {
-	for _, config := range pd.configs {
-		if config.Equal(c) {
-			return true
-		}
-	}
-	return false
 }
 
 // stop stops the provider descriptor if it's polling
@@ -116,30 +106,47 @@ func (pd *configPoller) poll(ac *AutoConfig) {
 	}
 }
 
+func (pd *configPoller) overwriteConfigs(configs []integration.Config) {
+	fetchedMap := make(map[uint64]*integration.Config, len(configs))
+	for _, c := range configs {
+		cHash := c.FastDigest()
+		fetchedMap[cHash] = &c
+	}
+	pd.configs = fetchedMap
+}
+
 // collect is just a convenient wrapper to fetch configurations from a provider and
 // see what changed from the last time we called Collect().
 func (pd *configPoller) collect(ctx context.Context) ([]integration.Config, []integration.Config) {
-	var newConf []integration.Config
-	var removedConf []integration.Config
-	old := pd.configs
-
 	fetched, err := pd.provider.Collect(ctx)
 	if err != nil {
 		log.Errorf("Unable to collect configurations from provider %s: %s", pd.provider, err)
 		return nil, nil
 	}
 
-	for _, c := range fetched {
-		if !pd.contains(&c) {
+	return pd.storeAndDiffConfigs(fetched)
+}
+
+func (pd *configPoller) storeAndDiffConfigs(configs []integration.Config) ([]integration.Config, []integration.Config) {
+	var newConf []integration.Config
+	var removedConf []integration.Config
+
+	// We allocate a new map. We could do without it with a bit more processing
+	// but it allows to free some memory if number of collected configs varies a lot
+	fetchedMap := make(map[uint64]*integration.Config, len(configs))
+	for _, c := range configs {
+		cHash := c.FastDigest()
+		if _, found := pd.configs[cHash]; found {
+			delete(pd.configs, cHash)
+		} else {
 			newConf = append(newConf, c)
 		}
 	}
 
-	pd.configs = fetched
-	for _, c := range old {
-		if !pd.contains(&c) {
-			removedConf = append(removedConf, c)
-		}
+	for _, c := range pd.configs {
+		removedConf = append(removedConf, *c)
 	}
+	pd.configs = fetchedMap
+
 	return newConf, removedConf
 }
