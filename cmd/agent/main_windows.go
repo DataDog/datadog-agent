b08dd571eb6b0ec27e821f1c0f3a8a6f1e809300
Olivier G <olivier.gaca@datadoghq.com>
2022-04-11 12:15:51 +0200

    Write new logs to debug a Windows service timeout issue.

--- cmd/agent/main_windows.go
+++ cmd/agent/main_windows.go
@@ -21,22 +21,54 @@ import (
 	"golang.org/x/sys/windows/svc"
 )
 
+var logFile *os.File
+
+func writeLogFile(s string) {
+	if logFile != nil {
+		logFile.WriteString(s + "\n")
+		logFile.Sync()
+	}
+}
+
+func init() {
+	var err error
+	logFile, err = os.OpenFile(`C:\ProgramData\Datadog\logs\main.txt`, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
+	fmt.Println(err)
+	writeLogFile("Init")
+}
+
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			writeLogFile("Recovered")
+			writeLogFile(fmt.Sprintf("%v", r))
+		}
+		logFile.Close()
+	}()
+	writeLogFile("Main")
 	common.EnableLoggingToFile()
+	writeLogFile("EnableLoggingToFile")
 	// if command line arguments are supplied, even in a non interactive session,
 	// then just execute that.  Used when the service is executing the executable,
 	// for instance to trigger a restart.
 	if len(os.Args) == 1 {
+		writeLogFile("len(os.Args) == 1")
 		isIntSess, err := svc.IsAnInteractiveSession()
+		writeLogFile("IsAnInteractiveSession")
 		if err != nil {
 			fmt.Printf("failed to determine if we are running in an interactive session: %v\n", err)
 		}
 		if !isIntSess {
+			writeLogFile("!isIntSess")
 			common.EnableLoggingToFile()
+			writeLogFile("EnableLoggingToFile")
 			service.RunService(false)
+			writeLogFile("RunService")
 			return
 		}
+		writeLogFile("return")
 	}
+	writeLogFile("No service")
 	defer log.Flush()
 
 	// Invoke the Agent
