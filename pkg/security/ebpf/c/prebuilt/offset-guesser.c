f7ec5fe26a5dab4656dfda2adee1b1202338081c
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-04-12 16:11:03 +0200

    [CWS] remove useless includes for offset guesser

--- pkg/security/ebpf/c/prebuilt/offset-guesser.c
+++ pkg/security/ebpf/c/prebuilt/offset-guesser.c
@@ -5,18 +5,6 @@
 #include <linux/types.h>
 #include <linux/version.h>
 #include <linux/bpf.h>
-#include <linux/filter.h>
-#include <uapi/asm-generic/mman-common.h>
-#include <linux/pipe_fs_i.h>
-#include <linux/nsproxy.h>
-
-#include <net/sock.h>
-#include <net/netfilter/nf_conntrack.h>
-#include <net/netfilter/nf_nat.h>
-#include <uapi/linux/ip.h>
-#include <uapi/linux/ipv6.h>
-#include <uapi/linux/udp.h>
-#include <uapi/linux/tcp.h>
 
 #include "bpf_helpers.h"
 #include "constants.h"
