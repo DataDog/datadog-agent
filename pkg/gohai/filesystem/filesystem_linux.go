8b7e33136a8cd0fac64e513f259c82d2be4bc6ae
Julien Lebot <julien.lebot@datadoghq.com>
2022-04-12 16:46:46 +0200

    Update expectedLength for Darwin (#94)

--- pkg/gohai/filesystem/filesystem_linux.go
+++ pkg/gohai/filesystem/filesystem_linux.go
@@ -3,10 +3,10 @@ package filesystem
 var dfOptions = []string{"-l"}
 var expectedLength = 6
 
-func updatefileSystemInfo(values []string) map[string]string {
+func updatefileSystemInfo(values []string) (map[string]string, error) {
 	return map[string]string{
 		"name":       values[0],
 		"kb_size":    values[1],
 		"mounted_on": values[5],
-	}
+	}, nil
 }
