74bb899117dde7bedbfafa401fe21a650c1f6a94
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-04-27 17:19:52 +0000

    Add Dump methods for various structs
    These methods are often helpful in tracing the handling of
    integration.Configs through the logs agent.

--- pkg/logs/config/source.go
+++ pkg/logs/config/source.go
@@ -6,6 +6,8 @@
 package config
 
 import (
+	"fmt"
+	"strings"
 	"sync"
 	"time"
 
@@ -144,4 +146,30 @@ func (s *LogSource) IsHiddenFromStatus() bool {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	return s.hiddenFromStatus
+}
+
+// Dump provides a multi-line dump of the LogSource contents, for debugging purposes
+func (s *LogSource) Dump() string {
+	if s == nil {
+		return "&LogSource(nil)"
+	}
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	var b strings.Builder
+	fmt.Fprintf(&b, "&LogsSource @ %p = {\n", s)
+	fmt.Fprintf(&b, "\tName: %#v,\n", s.Name)
+	fmt.Fprintf(&b, "\tConfig: %s,\n", strings.ReplaceAll(s.Config.Dump(), "\n", "\n\t"))
+	fmt.Fprintf(&b, "\tStatus: %s,\n", strings.ReplaceAll(s.Status.Dump(), "\n", "\n\t"))
+	fmt.Fprintf(&b, "\tinputs: %#v,\n", s.inputs)
+	fmt.Fprintf(&b, "\tMessages: %#v,\n", s.Messages.GetMessages())
+	fmt.Fprintf(&b, "\tsourceType: %#v,\n", s.sourceType)
+	fmt.Fprintf(&b, "\tinfo: %#v,\n", s.info)
+	fmt.Fprintf(&b, "\tparentSource: %p,\n", s.ParentSource)
+	fmt.Fprintf(&b, "\tLatencyStats: %#v,\n", s.LatencyStats)
+	fmt.Fprintf(&b, "\tBytesRead: %d,\n", s.BytesRead.Load())
+	fmt.Fprintf(&b, "\thiddenFromStatus: %t,\n", s.hiddenFromStatus)
+	fmt.Fprintf(&b, "}")
+	return b.String()
 }
