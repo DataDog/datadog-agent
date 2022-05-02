8d02a6cc7d0c4ae91f434a4a1b9b507c448af214
diana.shevchenko <shevchenko.diana26@gmail.com>
2022-05-02 15:24:06 +0200

    changed the test

--- cmd/trace-agent/test/testsuite/config_set_test.go
+++ cmd/trace-agent/test/testsuite/config_set_test.go
@@ -6,55 +6,36 @@
 package testsuite
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
-	"net/http/httptest"
+	"net/http"
 	"testing"
 
-	"github.com/DataDog/datadog-agent/cmd/trace-agent/config"
-	coreconfig "github.com/DataDog/datadog-agent/pkg/config"
-	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/cmd/trace-agent/test"
 
 	"github.com/cihub/seelog"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestConfigSetHandler(t *testing.T) {
-	coreconfig.SetupLogger("", seelog.InfoStr, "", "", false, true, false)
-	var b bytes.Buffer
-	f := bufio.NewWriter(&b)
-	l, _ := seelog.LoggerFromWriterWithMinLevel(f, seelog.InfoLvl)
-	log.RegisterAdditionalLogger("buffer", l)
+	var r test.Runner
+	if err := r.Start(); err != nil {
+		t.Fatal(err)
+	}
+	if err := r.RunAgent([]byte("log_level: info")); err != nil {
+		t.Fatal(err)
+	}
 
-	t.Run("warn", func(t *testing.T) {
-		h := config.SetHandler()
-		rec := httptest.NewRecorder()
-		h.ServeHTTP(rec, httptest.NewRequest("POST", fmt.Sprintf("/?log_level=%s", seelog.WarnStr), nil))
-		log.Warn("should be logged")
-		f.Flush()
+	defer r.KillAgent()
+	logstr := r.AgentLog()
+	assert.NotContains(t, logstr, "| DEBUG |")
+	assert.Contains(t, logstr, "| INFO |")
 
-		currentLvl, err := log.GetLogLevel()
-		assert.Nil(t, err)
-		assert.Equal(t, seelog.WarnStr, coreconfig.Datadog.Get("log_level"))
-		assert.Equal(t, seelog.WarnStr, currentLvl.String())
-		assert.NotContains(t, b.String(), fmt.Sprintf("Switched log level to %s", seelog.WarnStr))
-		assert.Contains(t, b.String(), "should be logged")
-		assert.Equal(t, 200, rec.Code)
-	})
+	resp, err := r.DoReq(fmt.Sprintf("config/set?log_level=%s", seelog.WarnStr), http.MethodPost, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	t.Run("debug", func(t *testing.T) {
-		b.Reset()
-		h := config.SetHandler()
-		rec := httptest.NewRecorder()
-		h.ServeHTTP(rec, httptest.NewRequest("POST", fmt.Sprintf("/?log_level=%s", seelog.DebugStr), nil))
-		f.Flush()
-
-		currentLvl, err := log.GetLogLevel()
-		assert.Nil(t, err)
-		assert.Equal(t, seelog.DebugStr, coreconfig.Datadog.Get("log_level"))
-		assert.Equal(t, seelog.DebugStr, currentLvl.String())
-		assert.Contains(t, b.String(), fmt.Sprintf("Switched log level to %s", seelog.DebugStr))
-		assert.Equal(t, 200, rec.Code)
-	})
+	logstr = r.AgentLog()
+	assert.NotContains(t, logstr, "Switched log level to")
+	assert.Equal(t, 200, resp.StatusCode)
 }
