0f73a78606b3b8909e869057076caea7a978c9f1
Diana Shevchenko <40775148+dianashevchenko@users.noreply.github.com>
2022-05-02 21:32:26 +0200

    trace-agent: change configuration settings (log_level) at runtime (#11608)
    * trace/agent: added endpoint to change configuration options
    Fixes #6962
    
    * trace-agent: removed unused debug and LogLevel fields

--- pkg/trace/api/api.go
+++ pkg/trace/api/api.go
@@ -72,7 +72,6 @@ type HTTPReceiver struct {
 	statsProcessor StatsProcessor
 	appsecHandler  http.Handler
 
-	debug               bool
 	rateLimiterResponse int // HTTP status code when refusing
 
 	wg   sync.WaitGroup // waits for all requests to be processed
@@ -99,7 +98,6 @@ func NewHTTPReceiver(conf *config.AgentConfig, dynConf *sampler.DynamicConfig, o
 		dynConf:        dynConf,
 		appsecHandler:  appsecHandler,
 
-		debug:               strings.ToLower(conf.LogLevel) == "debug",
 		rateLimiterResponse: rateLimiterResponse,
 
 		exit: make(chan struct{}),
@@ -138,7 +136,6 @@ func (r *HTTPReceiver) Start() {
 		log.Debug("HTTP receiver disabled by config (apm_config.receiver_port: 0).")
 		return
 	}
-	mux := r.buildMux()
 
 	timeout := 5 * time.Second
 	if r.conf.ReceiverTimeout > 0 {
@@ -149,7 +146,7 @@ func (r *HTTPReceiver) Start() {
 		ReadTimeout:  timeout,
 		WriteTimeout: timeout,
 		ErrorLog:     stdlog.New(httpLogger, "http.Server: ", 0),
-		Handler:      mux,
+		Handler:      r.buildMux(),
 	}
 
 	addr := fmt.Sprintf("%s:%d", r.conf.ReceiverHost, r.conf.ReceiverPort)
