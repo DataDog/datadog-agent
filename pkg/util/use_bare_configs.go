25453012396bcdfe21542bf72fb36fb27c18edd8
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-05-03 16:12:17 +0000

    remove ForceCcaInAD

--- pkg/util/use_bare_configs.go
+++ pkg/util/use_bare_configs.go
@@ -7,25 +7,9 @@ package util
 
 import "github.com/DataDog/datadog-agent/pkg/config"
 
-// set in testing to force the result of CcaInAD
-var forcedCcaInAD *bool
-
-// ForceCcaInAD forces a choice for CcaInAD, and returns
-// a function to revert the change, suitable for use in a `defer`.
-func ForceCcaInAD(use bool) func() {
-	forcedCcaInAD = &use
-	return func() {
-		forcedCcaInAD = nil
-	}
-}
-
 // CcaInAD returns the value of the logs_config.cca_in_ad
 // feature flag.  This is temporary, as this functionality will eventually be
 // the only option.
 func CcaInAD() bool {
-	if forcedCcaInAD != nil {
-		return *forcedCcaInAD
-	}
-
 	return config.Datadog.GetBool("logs_config.cca_in_ad")
 }
