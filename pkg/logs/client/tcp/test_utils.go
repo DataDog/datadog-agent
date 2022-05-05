5107eeee7a38d884f7e5b4b5f7d7faf80a21a3c8
Brian Floersch <brian.floersch@datadoghq.com>
2022-05-05 15:33:52 -0400

    fix tests

--- pkg/logs/client/tcp/test_utils.go
+++ pkg/logs/client/tcp/test_utils.go
@@ -26,7 +26,8 @@ func AddrToHostPort(remoteAddr net.Addr) (string, int) {
 // AddrToEndPoint creates an EndPoint from an Addr.
 func AddrToEndPoint(addr net.Addr) config.Endpoint {
 	host, port := AddrToHostPort(addr)
-	return config.Endpoint{Host: host, Port: port, IsReliable: true}
+	isReliable := true
+	return config.Endpoint{Host: host, Port: port, IsReliable: &isReliable}
 }
 
 // AddrToDestination creates a Destination from an Addr
