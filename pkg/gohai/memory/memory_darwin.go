d9f572512c45465cd3e2bfb8179fde1bf27ed5d4
Maxime mouial <maxime.mouial@gmail.com>
2022-05-06 15:50:46 +0200

    Update Memory API to use typed fields
    Memory now has a 'Get' method that returns an initialized Memory stuct.
    
    This also aligns the units for the values returned. Legacy code from
    gohai returns memory in:
    - byte for Windows
    - mix of byte and MB for OSX
    - KB on linux
    
    'Get' being a new method we can align this behavior to return bytes
    everywhere without breaking backward compatibility

--- pkg/gohai/memory/memory_darwin.go
+++ pkg/gohai/memory/memory_darwin.go
@@ -1,8 +1,10 @@
 package memory
 
 import (
+	"fmt"
 	"os/exec"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -21,4 +23,32 @@ func getMemoryInfo() (memoryInfo map[string]string, err error) {
 	}
 
 	return
+}
+
+func getMemoryInfoByte() (uint64, uint64, []string, error) {
+	memInfo, err := getMemoryInfo()
+	var mem, swap uint64
+	warnings := []string{}
+
+	// mem is already in bytes but `swap_total` use the format "5120,00M"
+	if v, ok := memInfo["swap_total"]; ok {
+		idx := strings.IndexAny(v, ",.") // depending on the locale either a comma or dot is used
+		swapTotal, e := strconv.ParseUint(v[0:idx], 10, 64)
+		if e == nil {
+			swap = swapTotal * 1024 * 1024 // swapTotal is in mb
+		} else {
+			warnings = append(warnings, fmt.Sprintf("could not parse swap size: %s", e))
+		}
+	}
+
+	if v, ok := memInfo["total"]; ok {
+		t, e := strconv.ParseUint(v, 10, 64)
+		if e == nil {
+			mem = t // mem is returned in bytes
+		} else {
+			warnings = append(warnings, fmt.Sprintf("could not parse memory size: %s", e))
+		}
+	}
+
+	return mem, swap, warnings, err
 }
