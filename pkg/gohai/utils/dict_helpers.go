07e1bf71fc8c2a5bd5aa03ea5a7f9ace4583adf6
Maxime mouial <maxime.mouial@gmail.com>
2022-05-06 15:52:36 +0200

    Update Cpu API to use typed fields
    Cpu now has a 'Get' method that returns an initialized Cpu stuct.

--- pkg/gohai/utils/dict_helpers.go
+++ pkg/gohai/utils/dict_helpers.go
@@ -1,6 +1,9 @@
 package utils
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 // GetString returns the dict[key] or an empty string if the entry doesn't exists
 func GetString(dict map[string]string, key string) string {
@@ -23,24 +26,26 @@ func GetStringInterface(dict map[string]interface{}, key string) string {
 }
 
 // GetUint64 returns the dict[key] or an empty uint64 if the entry doesn't exists
-func GetUint64(dict map[string]string, key string) uint64 {
+func GetUint64(dict map[string]string, key string, warnings *[]string) uint64 {
 	if v, ok := dict[key]; ok {
 		num, err := strconv.ParseUint(v, 10, 64)
 		if err == nil {
 			return num
 		}
+		*warnings = append(*warnings, fmt.Sprintf("could not parse '%s' from %s: %s", v, key, err))
 		return 0
 	}
 	return 0
 }
 
 // GetFloat64 returns the dict[key] or an empty uint64 if the entry doesn't exists
-func GetFloat64(dict map[string]string, key string) float64 {
+func GetFloat64(dict map[string]string, key string, warnings *[]string) float64 {
 	if v, ok := dict[key]; ok {
 		num, err := strconv.ParseFloat(v, 10)
 		if err == nil {
 			return num
 		}
+		*warnings = append(*warnings, fmt.Sprintf("could not parse '%s' from %s: %s", v, key, err))
 		return 0
 	}
 	return 0
