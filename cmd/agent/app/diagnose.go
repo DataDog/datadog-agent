aa04471ceca2388710fe4df6feb958df8ebb8fdc
Nicolas Guerguadj <35628945+Kaderinho@users.noreply.github.com>
2022-05-12 18:06:54 +0200

    Add 'diagnose datadog-connectivity' subcommand (#11913)
    * feat(diagnose): add empty new diagnose subcommand 'datadog-connectivity'
    
    Create a new 'datadog-connectivity' which will be used to diagnose connectivity issues
    between the Agent and Datadog endpoints. This command is empty for now, but will be implemented
    in futures PRs.
    
    There is also a new subcommand 'metadata-availability' which is doing the same job as the 'diagnose'
    command. The 'diagnose' command might evolve in the future to run both 'datadog-connectivity' and
    'metadata-availability' checks.
    
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@epita.fr>
    
    Co-authored-by: Vickenty Fesunov <vickenty@users.noreply.github.com>

--- cmd/agent/app/diagnose.go
+++ cmd/agent/app/diagnose.go
@@ -16,18 +16,39 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	diagnoseCommand = &cobra.Command{
+		Use:   "diagnose",
+		Short: "Check availability of cloud provider metadata endpoints",
+		Long:  ``,
+		RunE:  doDiagnoseMetadataAvailability,
+	}
+
+	diagnoseMetadataAvailabilityCommand = &cobra.Command{
+		Use:   "metadata-availability",
+		Short: "Check availability of cloud provider metadata endpoints",
+		Long:  ``,
+		RunE:  doDiagnoseMetadataAvailability,
+	}
+
+	diagnoseDatadogConnectivityCommand = &cobra.Command{
+		Use:    "datadog-connectivity",
+		Short:  "Check connectivity between your system and Datadog endpoints",
+		Long:   ``,
+		Hidden: true,
+		RunE:   doDiagnoseDatadogConnectivity,
+	}
+)
+
 func init() {
+
+	diagnoseCommand.AddCommand(diagnoseMetadataAvailabilityCommand)
+	diagnoseCommand.AddCommand(diagnoseDatadogConnectivityCommand)
+
 	AgentCmd.AddCommand(diagnoseCommand)
 }
 
-var diagnoseCommand = &cobra.Command{
-	Use:   "diagnose",
-	Short: "Execute some connectivity diagnosis on your system",
-	Long:  ``,
-	RunE:  doDiagnose,
-}
-
-func doDiagnose(cmd *cobra.Command, args []string) error {
+func doDiagnoseMetadataAvailability(cmd *cobra.Command, args []string) error {
 	// Global config setup
 	err := common.SetupConfig(confFilePath)
 	if err != nil {
@@ -48,8 +69,12 @@ func doDiagnose(cmd *cobra.Command, args []string) error {
 		config.Datadog.GetBool("log_format_json"),
 	)
 	if err != nil {
-		return fmt.Errorf("Error while setting up logging, exiting: %v", err)
+		return fmt.Errorf("error while setting up logging, exiting: %v", err)
 	}
 
 	return diagnose.RunAll(color.Output)
+}
+
+func doDiagnoseDatadogConnectivity(cmd *cobra.Command, args []string) error {
+	return fmt.Errorf("this command is not implemented yet")
 }
