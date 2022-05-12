88aa8b634b9849d38fae88bcbf5134b8a84cbbe1
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2022-05-12 16:33:24 -0500

    Fix kitchen test failures for system-probe

--- pkg/ebpf/bytecode/runtime/tracer.go
+++ pkg/ebpf/bytecode/runtime/tracer.go
@@ -4,4 +4,4 @@
 
 package runtime
 
-var Tracer = NewRuntimeAsset("tracer.c", "b3805b8e6c64fda74e5f31ad0c4c3a22864ee2a3230633bb7d13e753046081db")
+var Tracer = NewRuntimeAsset("tracer.c", "d944dec80b0daa2777115b11a1243bb6f7418af08ce8b6b2fdd6a53e4a9e0004")
--- pkg/network/ebpf/c/tags.h
+++ pkg/network/ebpf/c/tags.h
@@ -10,11 +10,4 @@ static __always_inline void add_tags_stats(conn_stats_ts_t *stats, __u64 tags) {
     stats->tags |= tags;
 }
 
-static __always_inline void add_tags_tuple(conn_tuple_t *t, __u64 tags) {
-    conn_stats_ts_t *stats = get_conn_stats(t);
-    if (!stats) {
-        return;
-    }
-    add_tags_stats(stats, tags);
-}
 #endif
