7f17a0609a77dd8d07110e0d95eac8b8fffeb12e
Nicolas Guerguadj <nicolas.guerguadj@epita.fr>
2022-05-16 14:03:26 +0200

    chore(diagnose): add copyright, package and struct comments
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@epita.fr>

--- pkg/diagnose/connectivity/endpoint_info.go
+++ pkg/diagnose/connectivity/endpoint_info.go
@@ -1,3 +1,12 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package connectivity contains logic for connectivity troubleshooting between the Agent
+// and Datadog endpoints. It uses HTTP request to contact different endpoints and displays
+// some results depending on endpoints responses, if any, and aims to imitate the Forwarder
+// behavior in order to get a more relevant troubleshooting.
 package connectivity
 
 import (
@@ -5,11 +14,23 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/forwarder/transaction"
 )
 
+// EndpointInfo is a value object that contains all the information we need to
+// contact an endpoint to troubleshoot connectivity issues.
+// It can be seen as a very lightweight version of transaction.HTTPTransaction.
+// One EndpointInfo should be define for each endpoint we want to troubleshoot.
 type EndpointInfo struct {
-	endpoint            transaction.Endpoint
-	payload             []byte
-	method              string
-	apiKeyInQueryString bool
+	// Endpoint is the API Endpoint we want to contact.
+	Endpoint transaction.Endpoint
+
+	// Method is the HTTP request method we want to send to the endpoint.
+	Method string
+
+	// Payload is the HTTP request body we want to send to the endpoint.
+	Payload []byte
+
+	// ApiKeyInQueryString is set to true if the API Key has to be in the query string
+	// i.e. https://domain/endpoint?api_key=***************************XXXXX
+	ApiKeyInQueryString bool
 }
 
 var (
@@ -18,8 +39,8 @@ var (
 	emptyPayload = []byte("")
 
 	// TODO : add more endpoints info and a filtering function to only keep used endpoints
-	V1SeriesEndpointInfo   = EndpointInfo{endpoints.V1SeriesEndpoint, emptyPayload, "POST", apiKeyInQueryString}
-	V1ValidateEndpointInfo = EndpointInfo{endpoints.V1ValidateEndpoint, emptyPayload, "GET", apiKeyInQueryString}
+	V1SeriesEndpointInfo   = EndpointInfo{endpoints.V1SeriesEndpoint, "POST", emptyPayload, apiKeyInQueryString}
+	V1ValidateEndpointInfo = EndpointInfo{endpoints.V1ValidateEndpoint, "GET", emptyPayload, apiKeyInQueryString}
 
 	endpointsInfo = []EndpointInfo{V1SeriesEndpointInfo, V1ValidateEndpointInfo}
 )
--- pkg/diagnose/connectivity/trace_request.go
+++ pkg/diagnose/connectivity/trace_request.go
@@ -1,3 +1,11 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package connectivity contains logic for connectivity troubleshooting between the Agent
+// and Datadog endpoints. It uses HTTP request to contact different endpoints and displays
+// some results depending on endpoints responses, if any.
 package connectivity
 
 import (
@@ -48,7 +56,7 @@ func sendRequestToAllEndpointOfADomain(client *http.Client, domainResolver resol
 	for _, apiKey := range domainResolver.GetAPIKeys() {
 
 		for _, endpointInfo := range endpointsInfo {
-			domain, _ := domainResolver.Resolve(endpointInfo.endpoint)
+			domain, _ := domainResolver.Resolve(endpointInfo.Endpoint)
 
 			// Create the endpoint URL and send the request
 			url := createEndpointURL(domain, apiKey, endpointInfo)
@@ -59,9 +67,9 @@ func sendRequestToAllEndpointOfADomain(client *http.Client, domainResolver resol
 
 func createEndpointURL(domain string, apiKey string, endpointInfo EndpointInfo) string {
 
-	url := domain + endpointInfo.endpoint.Route
+	url := domain + endpointInfo.Endpoint.Route
 
-	if endpointInfo.apiKeyInQueryString {
+	if endpointInfo.ApiKeyInQueryString {
 		url = fmt.Sprintf("%s?api_key=%s", url, apiKey)
 	}
 
@@ -71,11 +79,11 @@ func createEndpointURL(domain string, apiKey string, endpointInfo EndpointInfo) 
 func sendHTTPRequestToUrl(client *http.Client, url string, info EndpointInfo) {
 	logURL := scrubber.ScrubLine(url)
 
-	fmt.Printf("======== '%v' ========\n", logURL)
+	fmt.Printf("\n======== '%v' ========\n", logURL)
 
 	// Create a request for the backend
-	reader := bytes.NewReader(info.payload)
-	req, err := http.NewRequest(info.method, url, reader)
+	reader := bytes.NewReader(info.Payload)
+	req, err := http.NewRequest(info.Method, url, reader)
 
 	if err != nil {
 		log.Errorf("Could not create request for transaction to invalid URL '%v' : %v", logURL, scrubber.ScrubLine(err.Error()))
@@ -103,6 +111,6 @@ func sendHTTPRequestToUrl(client *http.Client, url string, info EndpointInfo) {
 		//fmt.Printf("Endpoint '%v' answers with status code %v\n", logURL, resp.StatusCode)
 		fmt.Printf("Received response : '%v'\n", string(body))
 	}
-	fmt.Printf("Received status code %v from the endpoint ====> %v\n\n", resp.StatusCode, statusString)
+	fmt.Printf("Received status code %v from the endpoint ====> %v\n", resp.StatusCode, statusString)
 
 }
