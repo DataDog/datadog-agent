dbc20f584466d0dc410e72ea6a22ffbfbc11df95
Gui774ume <guillaume.fournier@datadoghq.com>
2022-05-16 10:20:37 +0200

    fix io_uring ci tests

--- pkg/security/tests/rmdir_test.go
+++ pkg/security/tests/rmdir_test.go
@@ -131,7 +131,7 @@ func TestRmdir(t *testing.T) {
 			result := <-ch
 			ret, err := result.ReturnInt()
 			if err != nil {
-				if err == syscall.EBADF {
+				if err == syscall.EBADF || err == syscall.EINVAL {
 					return ErrSkipTest{"unlinkat not supported by io_uring"}
 				}
 				return err
