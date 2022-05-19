1dcff751337d4f10d2ffce72bcf6e8355705bc36
Sylvain Baubeau <lebauce@gmail.com>
2022-05-19 11:09:18 +0000

    Allow chaining commands in syscall-tester

--- pkg/security/tests/syscall_tester/c/syscall_tester.c
+++ pkg/security/tests/syscall_tester/c/syscall_tester.c
@@ -163,20 +163,25 @@ void sig_handler(int signum){
     exit(0);
 }
 
-int test_signal_sigusr(int sig) {
-    pid_t child = fork();
-    if (child == 0) {
-        signal(sig, sig_handler);
-        sleep(60);
-
-        return EXIT_SUCCESS;
+int test_signal_sigusr(int child, int sig) {
+    int do_fork = child != 0;
+    if (do_fork) {
+        child = fork();
+        if (child == 0) {
+            sleep(5);
+            return EXIT_SUCCESS;
+        }
     }
 
     int ret = kill(child, sig);
     if (ret < 0) {
         return ret;
     }
-    return wait(NULL);
+
+    if (do_fork)
+        wait(NULL);
+
+    return ret;
 }
 
 int test_signal_eperm(void) {
@@ -202,10 +207,19 @@ int test_signal(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
+    int pid = 0;
+    if (argc > 2) {
+        pid = atoi(argv[2]);
+        if (pid < 1) {
+            fprintf(stderr, "invalid pid: %s\n", argv[2]);
+            return EXIT_FAILURE;
+        }
+    }
+
     if (!strcmp(argv[1], "sigusr"))
-        return test_signal_sigusr(SIGUSR1);
-    if (!strcmp(argv[1], "sigusr2"))
-        return test_signal_sigusr(SIGUSR2);
+        return test_signal_sigusr(pid, SIGUSR1);
+    else if (!strcmp(argv[1], "sigusr2"))
+        return test_signal_sigusr(pid, SIGUSR2);
     else if (!strcmp(argv[1], "eperm"))
         return test_signal_eperm();
     fprintf(stderr, "%s: Unknown argument: %s.\n", __FUNCTION__, argv[1]);
