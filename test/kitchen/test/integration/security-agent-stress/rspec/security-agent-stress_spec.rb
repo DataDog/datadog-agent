1137b7c9eedaaa4abcd4b03daa12dd9fc9d5489e
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-05-25 10:17:54 +0200

    Remove debug logs

--- test/kitchen/test/integration/security-agent-stress/rspec/security-agent-stress_spec.rb
+++ test/kitchen/test/integration/security-agent-stress/rspec/security-agent-stress_spec.rb
@@ -1,9 +1,6 @@
 print `cat /etc/os-release`
 print `uname -a`
 
-print `ls /sys/devices/system/cpu`
-print `cat /sys/devices/system/cpu/nohz_full`
-
 describe 'successfully run stress test against master' do
   it 'displays PASS and returns 0' do
     output = `sudo /tmp/security-agent/stresssuite -test.v -duration 120 1>&2`
