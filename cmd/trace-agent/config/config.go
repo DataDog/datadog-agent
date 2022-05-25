46bb28ef947b9cf8fad41db884bf1df44ffb7811
andrew.glaude <andrew.glaude@datadoghq.com>
2022-05-25 15:43:57 -0400

    trace-agent/config: proxy config should be loaded after file so it is not ignored

--- cmd/trace-agent/config/config.go
+++ cmd/trace-agent/config/config.go
@@ -62,9 +62,6 @@ func prepareConfig(path string) (*config.AgentConfig, error) {
 	cfg.LogFilePath = DefaultLogFilePath
 	cfg.DDAgentBin = defaultDDAgentBin
 	cfg.AgentVersion = version.AgentVersion
-	if p := coreconfig.GetProxies(); p != nil {
-		cfg.Proxy = httputils.GetProxyTransportFunc(p)
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	orch := fargate.GetOrchestrator(ctx)
 	cancel()
@@ -75,6 +72,9 @@ func prepareConfig(path string) (*config.AgentConfig, error) {
 	coreconfig.Datadog.SetConfigFile(path)
 	if _, err := coreconfig.Load(); err != nil {
 		return cfg, err
+	}
+	if p := coreconfig.GetProxies(); p != nil {
+		cfg.Proxy = httputils.GetProxyTransportFunc(p)
 	}
 	cfg.ConfigPath = path
 	if coreconfig.Datadog.GetBool("remote_configuration.enabled") && coreconfig.Datadog.GetBool("remote_configuration.apm_sampling.enabled") {
