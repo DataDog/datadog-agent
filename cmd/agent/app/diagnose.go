15a2e681fe9a2390ff8ece0ba73daac570764f78
Nicolas Guerguadj <35628945+Kaderinho@users.noreply.github.com>
2022-05-30 15:45:45 +0200

    send http request to endpoint to verify connectivity issue (#12050)
    * feat(diagnose): send HTTP request to the endpoint and displays the response body
    
    * feat(diagnose): create EndpointInfo struct to store information on how to contact a endpoint
    
    This struct contains information about method and payload to use for a specific endpoint and also
    if we have to add the api_key in the query string.
    
    This will be useful to easily define new connectivity checks just by giving an endpoint, a method and a payload
    
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@epita.fr>

--- cmd/agent/app/diagnose.go
+++ cmd/agent/app/diagnose.go
@@ -11,6 +11,7 @@ import (
 	"github.com/DataDog/datadog-agent/cmd/agent/common"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/diagnose"
+	"github.com/DataDog/datadog-agent/pkg/diagnose/connectivity"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -49,16 +50,33 @@ func init() {
 }
 
 func doDiagnoseMetadataAvailability(cmd *cobra.Command, args []string) error {
-	// Global config setup
-	err := common.SetupConfig(confFilePath)
-	if err != nil {
-		return fmt.Errorf("unable to set up global agent configuration: %v", err)
+	if err := configAndLogSetup(); err != nil {
+		return err
 	}
 
 	if flagNoColor {
 		color.NoColor = true
 	}
 
+	return diagnose.RunAll(color.Output)
+}
+
+func doDiagnoseDatadogConnectivity(cmd *cobra.Command, args []string) error {
+	if err := configAndLogSetup(); err != nil {
+		return err
+	}
+
+	return connectivity.RunDatadogConnectivityDiagnose()
+}
+
+func configAndLogSetup() error {
+	// Global config setup
+	err := common.SetupConfig(confFilePath)
+	if err != nil {
+		return fmt.Errorf("unable to set up global agent configuration: %v", err)
+	}
+
+	// log level is always off since this might be use by other agent to get the hostname
 	err = config.SetupLogger(
 		loggerName,
 		config.Datadog.GetString("log_level"),
@@ -68,13 +86,10 @@ func doDiagnoseMetadataAvailability(cmd *cobra.Command, args []string) error {
 		config.Datadog.GetBool("log_to_console"),
 		config.Datadog.GetBool("log_format_json"),
 	)
+
 	if err != nil {
 		return fmt.Errorf("error while setting up logging, exiting: %v", err)
 	}
 
-	return diagnose.RunAll(color.Output)
-}
-
-func doDiagnoseDatadogConnectivity(cmd *cobra.Command, args []string) error {
-	return fmt.Errorf("this command is not implemented yet")
+	return nil
 }
