484710a06ee813fba0618646eb5bba904d0b25de
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-05-31 15:32:41 +0200

    update ndmflow endpoint

--- pkg/epforwarder/epforwarder.go
+++ pkg/epforwarder/epforwarder.go
@@ -90,7 +90,7 @@ var passthroughPipelineDescs = []passthroughPipelineDesc{
 	{
 		eventType:                     EventTypeNetworkDevicesNetFlow,
 		endpointsConfigPrefix:         "network_devices.netflow.forwarder.",
-		hostnameEndpointPrefix:        "event-platform-intake.", // TODO: Update to ndmflow-intake
+		hostnameEndpointPrefix:        "ndmflow-intake.",
 		intakeTrackType:               "ndmflow",
 		defaultBatchMaxConcurrentSend: 10,
 		defaultBatchMaxContentSize:    20e6,  // max 20Mb uncompressed size per payload
