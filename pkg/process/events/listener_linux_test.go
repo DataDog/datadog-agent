12ec1b6cac12669829bd5515f35b27c4b3d9c4f6
Moises Botarro <moises.bottaro.silva@gmail.com>
2022-06-02 19:13:58 +0200

    add missing license header; add build tags to test file

--- pkg/process/events/listener_linux_test.go
+++ pkg/process/events/listener_linux_test.go
@@ -1,3 +1,11 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// Copyright 2016-present Datadog, Inc.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+
+//go:build linux
+// +build linux
+
 package events
 
 import (
