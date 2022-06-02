e78d90bd6c6a2f58e4b313525679c6a25ea84832
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2022-06-02 22:30:49 +0200

    Fix broken test

--- pkg/trace/api/info_test.go
+++ pkg/trace/api/info_test.go
@@ -128,7 +128,7 @@ func TestInfoHandler(t *testing.T) {
 		"/v0.6/stats",
 		"/v0.1/pipeline_stats",
 		"/appsec/proxy/",
-		"/evpIntakeProxy/v1",
+		"/evpIntakeProxy/v1/",
 		"/debugger/v1/input"
 	],
 	"feature_flags": [
@@ -188,7 +188,7 @@ func TestInfoHandler(t *testing.T) {
 		"/v0.6/stats",
 		"/v0.1/pipeline_stats",
 		"/appsec/proxy/",
-		"/evpIntakeProxy/v1",
+		"/evpIntakeProxy/v1/",
 		"/debugger/v1/input"
 	],
 	"feature_flags": [
