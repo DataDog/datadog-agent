956f4a6090ae714d8a86ada1fbd9d7d9d518257d
Kyle Ames <kyle.ames@datadoghq.com>
2022-06-02 15:25:32 -0400

    Validate the repository's current state in basic update tests

--- pkg/remoteconfig/repository_test.go
+++ pkg/remoteconfig/repository_test.go
@@ -42,7 +42,7 @@ func TestUpdateNewConfig(t *testing.T) {
 
 	file := newCWSDDFile()
 	path, hashes, data := addCWSDDFile("test", 1, file, ta.targets)
-
+	ta.targets.Version = 1
 	b := signTargets(ta.key, ta.targets)
 
 	update := Update{
@@ -51,21 +51,79 @@ func TestUpdateNewConfig(t *testing.T) {
 		TargetFiles:   map[string][]byte{path: data},
 		ClientConfigs: []string{path},
 	}
-
 	updatedProducts, err := ta.repository.Update(update)
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(updatedProducts))
 	assert.Contains(t, updatedProducts, ProductCWSDD)
+
 	assert.Equal(t, 0, len(ta.repository.APMConfigs()))
 	assert.Equal(t, 1, len(ta.repository.CWSDDConfigs()))
 
 	storedFile, ok := ta.repository.CWSDDConfigs()[path]
 	assert.True(t, ok)
-
 	assert.Equal(t, file, storedFile.Config)
 	assert.EqualValues(t, 1, storedFile.Metadata.Version)
 	assert.Equal(t, "test", storedFile.Metadata.ID)
 	assertHashesEqual(t, hashes, storedFile.Metadata.Hashes)
 	assert.Equal(t, ProductCWSDD, storedFile.Metadata.Product)
 	assert.EqualValues(t, len(data), storedFile.Metadata.RawLength)
+
+	state, err := ta.repository.CurrentState()
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(state.Configs))
+	assert.Equal(t, 1, len(state.CachedFiles))
+	assert.EqualValues(t, 1, state.TargetsVersion)
+	assert.EqualValues(t, 1, state.RootsVersion)
+	configState := state.Configs[0]
+	assert.Equal(t, ProductCWSDD, configState.Product)
+	assert.Equal(t, "test", configState.ID)
+	assert.EqualValues(t, 1, configState.Version)
+	cached := state.CachedFiles[0]
+	assert.Equal(t, path, cached.Path)
+	assert.EqualValues(t, len(data), cached.Length)
+	assertHashesEqual(t, hashes, cached.Hashes)
+}
+
+func TestUpdateNewConfigThenRemove(t *testing.T) {
+	ta := newTestArtifacts()
+
+	file := newCWSDDFile()
+	path, _, data := addCWSDDFile("test", 1, file, ta.targets)
+	b := signTargets(ta.key, ta.targets)
+
+	update := Update{
+		TUFRoots:      make([][]byte, 0),
+		TUFTargets:    b,
+		TargetFiles:   map[string][]byte{path: data},
+		ClientConfigs: []string{path},
+	}
+	_, err := ta.repository.Update(update)
+	assert.Nil(t, err)
+
+	// We test this exact update in another test, so we'll just carry on here.
+	// TODO: Make it possible to create the repository at this valid state just to
+	// clean up tests if we accidentally introduce a bug that breaks this first update.
+
+	delete(ta.targets.Targets, path)
+	ta.targets.Version = 2
+	b = signTargets(ta.key, ta.targets)
+	removalUpdate := Update{
+		TUFRoots:      make([][]byte, 0),
+		TUFTargets:    b,
+		TargetFiles:   make(map[string][]byte),
+		ClientConfigs: make([]string, 0),
+	}
+	updatedProducts, err := ta.repository.Update(removalUpdate)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(updatedProducts), "An empty update shouldn't indicate any updated products")
+	assert.Equal(t, 0, len(ta.repository.APMConfigs()), "An empty update shoudldn't add any APM configs")
+	assert.Equal(t, 0, len(ta.repository.CWSDDConfigs()), "An empty update shouldn't add any CWSDD configs")
+
+	state, err := ta.repository.CurrentState()
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(state.Configs))
+	assert.Equal(t, 0, len(state.CachedFiles))
+	assert.EqualValues(t, 2, state.TargetsVersion)
+	assert.EqualValues(t, 1, state.RootsVersion)
+
 }
