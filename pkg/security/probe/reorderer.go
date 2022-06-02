7e717239101dd6ffc22a0db8f876e8c26b4ac292
Sylvain Afchain <safchain@gmail.com>
2022-06-02 12:33:57 +0000

    shrink

--- pkg/security/probe/reorderer.go
+++ pkg/security/probe/reorderer.go
@@ -121,7 +121,7 @@ func (h *reOrdererHeap) enqueue(cpu uint64, data []byte, tm uint64, generation u
 	h.up(node, len(h.heap)-1, metric)
 }
 
-func (h *reOrdererHeap) dequeue(handler func(cpu uint64, data []byte), generation uint64, metric *ReOrdererMetric) {
+func (h *reOrdererHeap) dequeue(handler func(cpu uint64, data []byte), generation uint64, metric *ReOrdererMetric, opts *ReOrdererOpts) {
 	var n, i int
 	var node *reOrdererNode
 
@@ -141,23 +141,29 @@ func (h *reOrdererHeap) dequeue(handler func(cpu uint64, data []byte), generatio
 
 		h.heap[i] = nil
 
-		heap := make([]*reOrdererNode, i)
-		copy(heap, h.heap[0:i])
-		h.heap = heap
-
 		metric.TotalOp++
 		handler(node.cpu, node.data)
 
 		h.pool.free(node)
+
+		// shrink
+		if cap(h.heap)-len(h.heap) > opts.HeapShrinkDelta {
+			heap := make([]*reOrdererNode, i)
+			copy(heap, h.heap[0:i])
+			h.heap = heap
+		} else {
+			h.heap = h.heap[0:i]
+		}
 	}
 }
 
 // ReOrdererOpts options to pass when creating a new instance of ReOrderer
 type ReOrdererOpts struct {
-	QueueSize  uint64        // size of the chan where the perf data are pushed
-	Rate       time.Duration // delay between two time based iterations
-	Retention  uint64        // bucket to keep before dequeueing
-	MetricRate time.Duration // delay between two metric samples
+	QueueSize       uint64        // size of the chan where the perf data are pushed
+	Rate            time.Duration // delay between two time based iterations
+	Retention       uint64        // bucket to keep before dequeueing
+	MetricRate      time.Duration // delay between two metric samples
+	HeapShrinkDelta int           // delta between cap and len between releasing heap array
 }
 
 func (r *ReOrdererMetric) zero() {
@@ -220,13 +226,12 @@ func (r *ReOrderer) Start(wg *sync.WaitGroup) {
 			} else {
 				r.heap.enqueue(cpu, data, tm, r.generation, &r.metric)
 			}
-			r.heap.dequeue(r.handler, r.generation-r.opts.Retention, &r.metric)
-
+			r.heap.dequeue(r.handler, r.generation-r.opts.Retention, &r.metric, &r.opts)
 		case <-flushTicker.C:
 			r.generation++
 
 			// force dequeue of a generation in case of low event rate
-			r.heap.dequeue(r.handler, r.generation-r.opts.Retention, &r.metric)
+			r.heap.dequeue(r.handler, r.generation-r.opts.Retention, &r.metric, &r.opts)
 		case <-metricTicker.C:
 			r.metric.QueueSize = r.heap.len()
 
