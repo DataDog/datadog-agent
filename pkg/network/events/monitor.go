3dbf3f58a5e399cd99ea54e92cfa331768c7d3cb
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2022-06-03 18:38:14 -0500

    Add missing files

---
+++ pkg/network/events/monitor.go
@@ -0,0 +1,89 @@
+package events
+
+import (
+	"sync"
+	"sync/atomic"
+
+	sprobe "github.com/DataDog/datadog-agent/pkg/security/probe"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/rules"
+)
+
+var theMonitor atomic.Value
+var once sync.Once
+var initErr error
+
+// Init initializes the events package
+func Init() error {
+	once.Do(func() {
+		var m *eventMonitor
+		m, initErr = newEventMonitor()
+		if initErr == nil {
+			theMonitor.Store(m)
+		}
+	})
+
+	return initErr
+}
+
+// HandlerFunc is the prototype for an event handler callback for process events
+type HandlerFunc func(*model.ProcessCacheEntry)
+
+func RegisterHandler(handler HandlerFunc) {
+	m := theMonitor.Load().(*eventMonitor)
+	m.RegisterHandler(handler)
+}
+
+type eventHandlerWrapper struct{}
+
+func (h *eventHandlerWrapper) HandleEvent(ev *sprobe.Event) {
+	m := theMonitor.Load()
+	if m != nil {
+		m.(*eventMonitor).HandleEvent(ev)
+	}
+}
+
+func (e *eventHandlerWrapper) HandleCustomEvent(rule *rules.Rule, event *sprobe.CustomEvent) {
+	m := theMonitor.Load()
+	if m != nil {
+		m.(*eventMonitor).HandleCustomEvent(rule, event)
+	}
+}
+
+var _eventHandlerWrapper = &eventHandlerWrapper{}
+
+// Handler returns an event handler to handle events from the runtime security module
+func Handler() sprobe.EventHandler {
+	return _eventHandlerWrapper
+}
+
+type eventMonitor struct {
+	handlers []HandlerFunc
+}
+
+func newEventMonitor() (*eventMonitor, error) {
+	return &eventMonitor{}, nil
+}
+
+func (e *eventMonitor) HandleEvent(ev *sprobe.Event) {
+	_ = ev.ResolveProcessEnvp(&ev.ProcessContext.Process)
+
+	entry := ev.ResolveProcessCacheEntry()
+	if entry == nil {
+		return
+	}
+
+	for _, h := range e.handlers {
+		h(entry)
+	}
+
+}
+
+func (e *eventMonitor) HandleCustomEvent(rule *rules.Rule, event *sprobe.CustomEvent) {
+}
+
+func (e *eventMonitor) RegisterHandler(handler HandlerFunc) {
+	if handler != nil {
+		e.handlers = append(e.handlers, handler)
+	}
+}
---
+++ pkg/network/tracer/process_cache.go
@@ -0,0 +1,108 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build linux_bpf
+// +build linux_bpf
+
+package tracer
+
+import (
+	"strings"
+
+	smodel "github.com/DataDog/datadog-agent/pkg/security/secl/model"
+	lru "github.com/hashicorp/golang-lru"
+)
+
+var filteredEnvs = []string{
+	"DD_ENV",
+	"DD_VERSION",
+	"DD_SERVICE",
+}
+
+// Process stores process info
+type Process struct {
+	Pid  uint32
+	Envs map[string]string
+}
+
+type processCache struct {
+	cache *lru.Cache
+}
+
+func newProcessCache(maxProcs int) (*processCache, error) {
+	cache, err := lru.New(maxProcs)
+	if err != nil {
+		return nil, err
+	}
+
+	return &processCache{cache: cache}, nil
+}
+
+func (pc *processCache) handleProcessEvent(entry *smodel.ProcessCacheEntry) {
+	var envs map[string]string
+	if entry.EnvsEntry != nil {
+		for _, v := range entry.EnvsEntry.Values {
+			kv := strings.SplitN(v, "=", 2)
+			k := kv[0]
+			found := len(filteredEnvs) == 0
+			for _, r := range filteredEnvs {
+				if k == r {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+
+			v = ""
+			if len(kv) > 1 {
+				v = kv[1]
+			}
+
+			if envs == nil {
+				envs = make(map[string]string)
+			}
+			envs[k] = v
+			if len(filteredEnvs) > 0 && len(filteredEnvs) == len(envs) {
+				break
+			}
+		}
+	}
+
+	if len(envs) == 0 && len(filteredEnvs) > 0 {
+		return
+	}
+
+	pc.cache.Add(entry.Pid, &Process{Pid: entry.Pid, Envs: envs})
+}
+
+// Process looks up a process by PID
+func (pc *processCache) Process(pid uint32) (*Process, bool) {
+	p, ok := pc.cache.Get(pid)
+	if ok {
+		return p.(*Process), true
+	}
+
+	return nil, false
+}
+
+func (pc *processCache) dump() (interface{}, error) {
+	keys := pc.cache.Keys()
+	if len(keys) == 0 {
+		return []interface{}{}, nil
+	}
+
+	var res []interface{}
+	for _, k := range keys {
+		p, ok := pc.cache.Peek(k)
+		if !ok {
+			continue
+		}
+		res = append(res, *(p.(*Process)))
+	}
+
+	return res, nil
+}
