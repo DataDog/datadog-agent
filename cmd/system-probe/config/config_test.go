55366d584dc8cd108ae54055f03b8e28020acf3a
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2022-06-07 17:31:58 -0500

    Fix lint errors

--- cmd/system-probe/config/config_test.go
+++ cmd/system-probe/config/config_test.go
@@ -13,8 +13,8 @@ import (
 	"testing"
 
 	"github.com/DataDog/datadog-agent/pkg/config"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func newConfig() {
--- pkg/network/events/monitor.go
+++ pkg/network/events/monitor.go
@@ -3,8 +3,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build linux_bpf
-// +build linux_bpf
+//go:build linux
+// +build linux
+
 package events
 
 import (
