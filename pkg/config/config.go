17727a458f86a0214e92d116c9627b352b5b6c37
Sylvain Afchain <safchain@gmail.com>
2022-06-07 08:29:36 +0000

    [CWS] Enable DNS detections by default (#12223)
    * Enable DNS detections by default
    
    * Bump security-agent-policies to v0.26.2
    
    (cherry picked from commit 2c3b55f600ca831ead3cf7a3d8c3d5da49360f33)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1044,7 +1044,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("runtime_security_config.activity_dump.cgroup_dump_timeout", 30)
 	config.BindEnvAndSetDefault("runtime_security_config.activity_dump.cgroup_wait_list_size", 10)
 	config.BindEnvAndSetDefault("runtime_security_config.activity_dump.cgroup_output_directory", "")
-	config.BindEnvAndSetDefault("runtime_security_config.network.enabled", false)
+	config.BindEnvAndSetDefault("runtime_security_config.network.enabled", true)
 	config.BindEnvAndSetDefault("runtime_security_config.network.lazy_interface_prefixes", []string{})
 	config.BindEnvAndSetDefault("runtime_security_config.remote_configuration.enabled", false)
 
