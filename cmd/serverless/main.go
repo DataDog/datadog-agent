988df22371368af2eb5be55ba40d374ddea9d74a
Ivan Topolcic <ivan.topolcic@datadoghq.com>
2022-06-08 13:33:43 -0400

    Correctly decrypt keys

--- cmd/serverless/main.go
+++ cmd/serverless/main.go
@@ -163,7 +163,8 @@ func runAgent(stopCh chan struct{}) (serverlessDaemon *daemon.Daemon, err error)
 		}
 		envKey := tokens[0]
 		envVal := tokens[1]
-		if strings.HasSuffix(envVar, kmsKeySuffix) {
+		if strings.HasSuffix(envKey, kmsKeySuffix) {
+			log.Debugf("Decrypting %v", envVar)
 			secretVal, err := readAPIKeyFromKMS(envVal)
 			if err != nil {
 				log.Debugf("Couldn't read API key from KMS: %v", err)
@@ -171,7 +172,8 @@ func runAgent(stopCh chan struct{}) (serverlessDaemon *daemon.Daemon, err error)
 			}
 			os.Setenv(strings.TrimSuffix(envKey, kmsKeySuffix), secretVal)
 		}
-		if strings.HasSuffix(envVar, secretArnSuffix) {
+		if strings.HasSuffix(envKey, secretArnSuffix) {
+			log.Debugf("Retrieving %v from secrets manager", envVar)
 			secretVal, err := readAPIKeyFromSecretsManager(envVal)
 			if err != nil {
 				log.Debugf("Couldn't read API key from KMS: %v", err)
