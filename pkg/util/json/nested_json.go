9aeffe22d09db07661b3e7b83b6612691030dbf9
Ivan Topolcic <ivan.topolcic@datadoghq.com>
2022-06-14 12:06:26 -0400

    Formatting

--- pkg/util/json/nested_json.go
+++ pkg/util/json/nested_json.go
@@ -4,16 +4,16 @@ package json
 // where each value is another depth level in the map.
 // Returns nil if the map doesn't contain the nested key.
 func GetNestedValue(inputMap map[string]interface{}, keys ...string) interface{} {
-	if val, exists := inputMap[keys[0]]; !exists {
+	val, exists := inputMap[keys[0]]
+	if !exists {
 		return nil
-	} else {
-		if len(keys) == 1 {
-			return val
-		}
-		innerMap, ok := val.(map[string]interface{})
-		if !ok {
-			return nil
-		}
-		return GetNestedValue(innerMap, keys[1:]...)
 	}
+	if len(keys) == 1 {
+		return val
+	}
+	innerMap, ok := val.(map[string]interface{})
+	if !ok {
+		return nil
+	}
+	return GetNestedValue(innerMap, keys[1:]...)
 }
