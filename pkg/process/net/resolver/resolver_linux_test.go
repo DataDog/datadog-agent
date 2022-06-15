aa1a161bad895e353d72b945e5796cac1c6266dc
Bryce Kahle <bryce.kahle@datadoghq.com>
2022-06-15 15:13:31 +0200

    Only run resolver test that requires root if root (#12399)

--- pkg/process/net/resolver/resolver_linux_test.go
+++ pkg/process/net/resolver/resolver_linux_test.go
@@ -6,6 +6,7 @@
 package resolver
 
 import (
+	"os"
 	"testing"
 
 	model "github.com/DataDog/agent-payload/v5/process"
@@ -104,15 +105,14 @@ func TestLocalResolver(t *testing.T) {
 }
 
 func TestResolveLoopbackConnections(t *testing.T) {
-	rootNs, err := util.GetNetNsInoFromPid("/proc", 1)
-	require.NoError(t, err)
-
-	tests := []struct {
+	type resolveTest struct {
 		name            string
 		conn            *model.Connection
 		expectedLaddrID string
 		expectedRaddrID string
-	}{
+	}
+
+	tests := []resolveTest{
 		{
 			name: "raddr resolution with nat",
 			conn: &model.Connection{
@@ -314,30 +314,6 @@ func TestResolveLoopbackConnections(t *testing.T) {
 			expectedRaddrID: "",
 		},
 		{
-			name: "cross namespace with dnat to loopback",
-			conn: &model.Connection{
-				Pid:   21,
-				NetNS: rootNs,
-				Laddr: &model.Addr{
-					Ip:   "127.0.0.1",
-					Port: 8181,
-				},
-				Raddr: &model.Addr{
-					Ip:   "10.10.10.10",
-					Port: 22222,
-				},
-				Direction: model.ConnectionDirection_outgoing,
-				IpTranslation: &model.IPTranslation{
-					ReplDstIP:   "169.254.169.254",
-					ReplDstPort: 80,
-					ReplSrcIP:   "10.10.10.10",
-					ReplSrcPort: 22222,
-				},
-			},
-			expectedLaddrID: "foo21",
-			expectedRaddrID: "foo20",
-		},
-		{
 			name: "zero src netns failed resolution",
 			conn: &model.Connection{
 				Pid:   22,
@@ -373,6 +349,35 @@ func TestResolveLoopbackConnections(t *testing.T) {
 			expectedLaddrID: "foo21",
 			expectedRaddrID: "", // should NOT resolve to foo22
 		},
+	}
+
+	if os.Getuid() == 0 {
+		rootNs, err := util.GetNetNsInoFromPid("/proc", 1)
+		require.NoError(t, err)
+		tests = append(tests, resolveTest{
+			name: "cross namespace with dnat to loopback",
+			conn: &model.Connection{
+				Pid:   21,
+				NetNS: rootNs,
+				Laddr: &model.Addr{
+					Ip:   "127.0.0.1",
+					Port: 8181,
+				},
+				Raddr: &model.Addr{
+					Ip:   "10.10.10.10",
+					Port: 22222,
+				},
+				Direction: model.ConnectionDirection_outgoing,
+				IpTranslation: &model.IPTranslation{
+					ReplDstIP:   "169.254.169.254",
+					ReplDstPort: 80,
+					ReplSrcIP:   "10.10.10.10",
+					ReplSrcPort: 22222,
+				},
+			},
+			expectedLaddrID: "foo21",
+			expectedRaddrID: "foo20",
+		})
 	}
 
 	resolver := &LocalResolver{}
