308cae6077800920930dfb7a7d86a7a22b62f67b
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-06-15 16:09:18 +0200

    [CWS] add utility to parse agent version semver

---
+++ pkg/security/agent/utils.go
@@ -0,0 +1,21 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package agent
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/version"
+	"github.com/Masterminds/semver"
+)
+
+// GetAgentSemverVersion returns the agent version as a semver version
+func GetAgentSemverVersion() (*semver.Version, error) {
+	av, err := version.Agent()
+	if err != nil {
+		return nil, err
+	}
+
+	return semver.NewVersion(av.GetNumberAndPre())
+}
