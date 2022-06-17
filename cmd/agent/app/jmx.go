a6549c517d97aa1cccc349f0bb2ce7fa585f0ab0
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-06-17 13:50:35 +0000

    use common.WaitForConfigsFromAD in 'agent jmx', too

--- cmd/agent/app/jmx.go
+++ cmd/agent/app/jmx.go
@@ -107,7 +107,7 @@ var (
 func init() {
 	jmxCmd.PersistentFlags().StringVarP(&jmxLogLevel, "log-level", "l", "", "set the log level (default 'debug') (deprecated, use the env var DD_LOG_LEVEL instead)")
 	jmxCmd.PersistentFlags().UintVarP(&discoveryTimeout, "discovery-timeout", "", 5, "max retry duration until Autodiscovery resolves the check template (in seconds)")
-	jmxCmd.PersistentFlags().UintVarP(&discoveryRetryInterval, "discovery-retry-interval", "", 1, "duration between retries until Autodiscovery resolves the check template (in seconds)")
+	jmxCmd.PersistentFlags().UintVarP(&discoveryRetryInterval, "discovery-retry-interval", "", 1, "(unused)")
 	jmxCmd.PersistentFlags().UintVarP(&discoveryMinInstances, "discovery-min-instances", "", 1, "minimum number of config instances to be discovered before running the check(s)")
 
 	// attach list and collect commands to jmx command
@@ -182,20 +182,22 @@ func runJmxCommandConsole(command string) error {
 
 	common.LoadComponents(context.Background(), config.Datadog.GetString("confd_path"))
 
-	// Set up check collector
-	common.AC.AddScheduler("check", collector.InitCheckScheduler(common.Coll), true)
-	common.Coll.Start()
-
 	if discoveryRetryInterval > discoveryTimeout {
 		fmt.Println("The discovery retry interval", discoveryRetryInterval, "is higher than the discovery timeout", discoveryTimeout)
 		fmt.Println("Setting the discovery retry interval to", discoveryTimeout)
 		discoveryRetryInterval = discoveryTimeout
 	}
 
-	// Note: when no checks are selected, cliSelectedChecks will be the empty slice and thus common.SelectedCheckMatcherBuilder
-	//       will return false, leading WaitForConfigs to timeout before returning all AD configs.
-	allConfigs := common.WaitForConfigs(time.Duration(discoveryRetryInterval)*time.Second, time.Duration(discoveryTimeout)*time.Second,
-		common.SelectedCheckMatcherBuilder(cliSelectedChecks, discoveryMinInstances))
+	// Create the CheckScheduler, but do not attach it to
+	// AutoDiscovery.  NOTE: we do not start common.Coll, either.
+	collector.InitCheckScheduler(common.Coll)
+
+	// Note: when no checks are selected, cliSelectedChecks will be the empty slice and thus
+	//       WaitForConfigsFromAD will timeout and return no AD configs.
+	waitCtx, cancelTimeout := context.WithTimeout(
+		context.Background(), time.Duration(discoveryTimeout)*time.Second)
+	allConfigs := common.WaitForConfigsFromAD(waitCtx, cliSelectedChecks, int(discoveryMinInstances))
+	cancelTimeout()
 
 	err = standalone.ExecJMXCommandConsole(command, cliSelectedChecks, logLevel, allConfigs)
 
