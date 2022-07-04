a64cdf48fd5301d6dd088c11702f848a348ea7b8
truthbk <truthbk@gmail.com>
2022-07-04 16:52:27 +0200

    [scrubber] string(b) check unnecessary

--- pkg/util/scrubber/scrubber.go
+++ pkg/util/scrubber/scrubber.go
@@ -118,7 +118,7 @@ func (c *Scrubber) scrubReader(file io.Reader) ([]byte, error) {
 	first := true
 	for scanner.Scan() {
 		b := scanner.Bytes()
-		if blankRegex.Match(b) || string(b) == "" {
+		if blankRegex.Match(b) {
 			cleanedFile = append(cleanedFile, byte('\n'))
 		} else if !commentRegex.Match(b) {
 			b = c.scrub(b, c.singleLineReplacers)
