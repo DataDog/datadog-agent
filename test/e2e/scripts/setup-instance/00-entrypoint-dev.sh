67bbf597f0ddbe3e4eece590844e038b1496dcb2
Lénaïc Huard <lenaic.huard@datadoghq.com>
2022-07-06 15:21:40 +0200

    Fix

--- test/e2e/scripts/setup-instance/00-entrypoint-dev.sh
+++ test/e2e/scripts/setup-instance/00-entrypoint-dev.sh
@@ -55,8 +55,6 @@ tee specification.json << EOF
 }
 EOF
 
-CI_COMMIT_SHORT_SHA=${CI_COMMIT_SHORT_SHA:-$(git describe --tags --always --dirty --match 7.\*)}
-CI_PIPELINE_ID=${CI_PIPELINE_ID:-unknown}
-CI_JOB_ID=${CI_JOB_ID:-unknown}
+export CI_COMMIT_SHORT_SHA=${CI_COMMIT_SHORT_SHA:-$(git describe --tags --always --dirty --match 7.\*)}
 
 exec ./02-ec2.sh
