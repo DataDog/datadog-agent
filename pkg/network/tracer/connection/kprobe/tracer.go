09734094c053eb3d7c19cb435efbf4d2c20a0b5c
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-07-11 19:58:27 +0000

    use go.uber.org/atomic in tracer/connection/kprobe

--- pkg/network/tracer/connection/kprobe/tracer.go
+++ pkg/network/tracer/connection/kprobe/tracer.go
@@ -12,7 +12,6 @@ import (
 	"errors"
 	"fmt"
 	"math"
-	"sync/atomic"
 	"unsafe"
 
 	ddebpf "github.com/DataDog/datadog-agent/pkg/ebpf"
@@ -27,6 +26,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 	manager "github.com/DataDog/ebpf-manager"
 	"github.com/cilium/ebpf"
+	"go.uber.org/atomic"
 	"golang.org/x/sys/unix"
 )
 
@@ -46,15 +46,15 @@ type kprobeTracer struct {
 	// tcp_close events
 	closeConsumer *tcpCloseConsumer
 
-	pidCollisions int64
+	pidCollisions *atomic.Int64
 	removeTuple   *netebpf.ConnTuple
 
 	telemetry telemetry
 }
 
 type telemetry struct {
-	tcpConns4, tcpConns6 int64 `stats:"atomic"`
-	udpConns4, udpConns6 int64 `stats:"atomic"`
+	tcpConns4, tcpConns6 *atomic.Int64 `stats:""`
+	udpConns4, udpConns6 *atomic.Int64 `stats:""`
 
 	reporter stats.Reporter
 }
@@ -151,6 +151,7 @@ func New(config *config.Config, constants []manager.ConstantEditor) (connection.
 		m:             m,
 		config:        config,
 		closeConsumer: closeConsumer,
+		pidCollisions: atomic.NewInt64(0),
 		removeTuple:   &netebpf.ConnTuple{},
 	}
 
@@ -166,6 +167,10 @@ func New(config *config.Config, constants []manager.ConstantEditor) (connection.
 		return nil, fmt.Errorf("error retrieving the bpf %s map: %s", probes.TcpStatsMap, err)
 	}
 
+	tr.telemetry.tcpConns4 = atomic.NewInt64(0)
+	tr.telemetry.udpConns4 = atomic.NewInt64(0)
+	tr.telemetry.tcpConns6 = atomic.NewInt64(0)
+	tr.telemetry.udpConns6 = atomic.NewInt64(0)
 	tr.telemetry.reporter, err = stats.NewReporter(&tr.telemetry)
 	if err != nil {
 		return nil, fmt.Errorf("error creating stats reporter: %w", err)
@@ -248,10 +253,10 @@ func (t *kprobeTracer) GetConnections(buffer *network.ConnectionBuffer, filter f
 }
 
 func (t *telemetry) assign(other telemetry) {
-	atomic.StoreInt64(&t.tcpConns4, other.tcpConns4)
-	atomic.StoreInt64(&t.tcpConns6, other.tcpConns6)
-	atomic.StoreInt64(&t.udpConns4, other.udpConns4)
-	atomic.StoreInt64(&t.udpConns6, other.udpConns6)
+	t.tcpConns4.Store(other.tcpConns4.Load())
+	t.tcpConns6.Store(other.tcpConns6.Load())
+	t.udpConns4.Store(other.udpConns4.Load())
+	t.udpConns6.Store(other.udpConns6.Load())
 }
 
 func (t *telemetry) addConnection(conn *network.ConnectionStats) {
@@ -259,15 +264,15 @@ func (t *telemetry) addConnection(conn *network.ConnectionStats) {
 	switch conn.Family {
 	case network.AFINET6:
 		if isTcp {
-			atomic.AddInt64(&t.tcpConns6, 1)
+			t.tcpConns6.Inc()
 		} else {
-			atomic.AddInt64(&t.udpConns6, 1)
+			t.udpConns6.Inc()
 		}
 	case network.AFINET:
 		if isTcp {
-			atomic.AddInt64(&t.tcpConns4, 1)
+			t.tcpConns4.Inc()
 		} else {
-			atomic.AddInt64(&t.udpConns4, 1)
+			t.udpConns4.Inc()
 		}
 	}
 }
@@ -277,15 +282,15 @@ func (t *telemetry) removeConnection(conn *network.ConnectionStats) {
 	switch conn.Family {
 	case network.AFINET6:
 		if isTcp {
-			atomic.AddInt64(&t.tcpConns6, -1)
+			t.tcpConns6.Dec()
 		} else {
-			atomic.AddInt64(&t.udpConns6, -1)
+			t.udpConns6.Dec()
 		}
 	case network.AFINET:
 		if isTcp {
-			atomic.AddInt64(&t.tcpConns4, -1)
+			t.tcpConns4.Dec()
 		} else {
-			atomic.AddInt64(&t.udpConns4, -1)
+			t.udpConns4.Dec()
 		}
 	}
 }
@@ -349,7 +354,7 @@ func (t *kprobeTracer) GetTelemetry() map[string]int64 {
 	}
 
 	closeStats := t.closeConsumer.GetStats()
-	pidCollisions := atomic.LoadInt64(&t.pidCollisions)
+	pidCollisions := t.pidCollisions.Load()
 
 	stats := map[string]int64{
 		"closed_conn_polling_lost":     closeStats[perfLostStat],
@@ -440,7 +445,7 @@ func (t *kprobeTracer) getTCPStats(stats *netebpf.TCPStats, tuple *netebpf.ConnT
 	if err == nil {
 		// This is required to avoid (over)reporting retransmits for connections sharing the same socket.
 		if _, reported := seen[*tuple]; reported {
-			atomic.AddInt64(&t.pidCollisions, 1)
+			t.pidCollisions.Inc()
 			stats.Retransmits = 0
 		} else {
 			seen[*tuple] = struct{}{}
