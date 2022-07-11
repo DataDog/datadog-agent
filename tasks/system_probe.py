e0468e06d9f58ebd04c38da75a7129dff0f7002b
Hasan Mahmood <hmahmood@users.noreply.github.com>
2022-07-11 13:33:07 -0500

    [system-probe] Do not use `tcp_cleanup_rbuf` for tcp receive tracking (#12660)

--- tasks/system_probe.py
+++ tasks/system_probe.py
@@ -129,6 +129,7 @@ def test(
     windows=is_windows,
     parallel_build=True,
     failfast=False,
+    kernel_release=None,
 ):
     """
     Run tests on eBPF parts
@@ -148,7 +149,7 @@ def test(
         clang_tidy(ctx)
 
     if not skip_object_files and not windows:
-        build_object_files(ctx, parallel_build=parallel_build)
+        build_object_files(ctx, parallel_build=parallel_build, kernel_release=kernel_release)
 
     build_tags = [NPM_TAG]
     if not windows:
@@ -177,7 +178,7 @@ def test(
 
 
 @task
-def kitchen_prepare(ctx, windows=is_windows):
+def kitchen_prepare(ctx, windows=is_windows, kernel_release=None):
     """
     Compile test suite for kitchen
     """
@@ -223,6 +224,7 @@ def kitchen_prepare(ctx, windows=is_windows):
             skip_linters=True,
             bundle_ebpf=False,
             output_path=os.path.join(target_path, target_bin),
+            kernel_release=kernel_release,
         )
 
         # copy ancillary data, if applicable
