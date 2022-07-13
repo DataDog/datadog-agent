e4fecd933a5d6e6cd0de32a353f694dd5c0abd4f
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2022-07-13 09:54:08 +0200

    fixup! use nil body for api key validation endpoint to make it work across all sites (#12629)

--- pkg/network/ebpf/c/runtime/tracer.c
+++ pkg/network/ebpf/c/runtime/tracer.c
@@ -456,6 +456,36 @@ int kprobe__tcp_set_state(struct pt_regs* ctx) {
     return 0;
 }
 
+SEC("kretprobe/tcp_connect")
+int kretprobe__tcp_connect(struct pt_regs* ctx) {
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    log_debug("kretprobe/tcp_connect: tgid: %u, pid: %u\n", pid_tgid >> 32, pid_tgid & 0xFFFFFFFF);
+
+    // We only trace if the connection worked and is established
+    int rc = PT_REGS_RC(ctx);
+    if (rc != 0) {
+        return 0;
+    }
+
+    struct sock **sk = bpf_map_lookup_elem(&tcp_connect_sock, &pid_tgid);
+    if (!sk) {
+        return 0;
+    }
+
+    conn_tuple_t t = {};
+    if (!read_conn_tuple(&t, *sk, pid_tgid, CONN_TYPE_TCP)) {
+        return 0;
+    }
+    handle_tcp_stats(&t, *sk, TCP_ESTABLISHED);
+    handle_message(&t, 0, 0, CONN_DIRECTION_OUTGOING, 0, 0, PACKET_COUNT_NONE);
+
+    log_debug("kretprobe/tcp_connect: netns: %u, sport: %u, dport: %u\n", t.netns, t.sport, t.dport);
+
+    bpf_map_delete_elem(&tcp_connect_sock, &pid_tgid);
+
+    return 0;
+}
+
 SEC("kretprobe/inet_csk_accept")
 int kretprobe__inet_csk_accept(struct pt_regs* ctx) {
 
