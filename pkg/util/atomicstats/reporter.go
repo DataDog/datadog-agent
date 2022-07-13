e1a7106f8dc561401ff54a91ecf611caaeb10545
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-07-13 19:04:54 +0000

    Refactor atomicstats to use getter functions
    This makes the Report function a little faster, and reduces the number
    of places that the supported types are enumerated.

--- pkg/util/atomicstats/reporter.go
+++ pkg/util/atomicstats/reporter.go
@@ -55,8 +55,16 @@ type Reporter struct {
 	// structType is the type of value this Reporter reports on
 	structType reflect.Type
 
-	// stats maps field names to the struct index containing their value
-	stats map[string]int
+	// fields maps field names to getters and setters for that field
+	fields map[string]field
+}
+
+type field struct {
+	// idx is the index of the field in the struct
+	idx int
+
+	// get will get the value from the field
+	get func(f reflect.Value) interface{}
 }
 
 // NewReporter creates a new Reporter to represent the type of the argument.
@@ -69,6 +77,8 @@ func NewReporter(v interface{}) Reporter {
 	ptrType := reflect.TypeOf(v)
 
 	if ptrType.Kind() != reflect.Ptr || ptrType.Elem().Kind() != reflect.Struct {
+		// errors here are programming (type) errors that cannot be caught at compile time,
+		// and thus are fatal
 		panic("NewReporter expects a pointer to a struct")
 	}
 
@@ -76,17 +86,19 @@ func NewReporter(v interface{}) Reporter {
 
 	reporter := Reporter{
 		structType: structType,
-		stats:      map[string]int{},
+		fields:     map[string]field{},
 	}
 
-	for f := 0; f < structType.NumField(); f++ {
-		field := structType.Field(f)
-		if _, ok := field.Tag.Lookup(statsTag); ok {
-			if !isTypeSupported(field) {
-				panic(fmt.Errorf("unsupported type %+v for field %s", field.Type.Kind(), field.Name))
+	for idx := 0; idx < structType.NumField(); idx++ {
+		fieldType := structType.Field(idx)
+		if _, ok := fieldType.Tag.Lookup(statsTag); ok {
+			fld, err := getFieldFor(fieldType.Type)
+			if err != nil {
+				panic(err.Error())
 			}
 
-			reporter.stats[toSnakeCase(field.Name)] = f
+			fld.idx = idx
+			reporter.fields[toSnakeCase(fieldType.Name)] = fld
 		}
 	}
 
@@ -98,7 +110,7 @@ func NewReporter(v interface{}) Reporter {
 //
 // The value _must_ have the same type as the value passed to NewReporter.
 func (r *Reporter) Report(v interface{}) map[string]interface{} {
-	stats := make(map[string]interface{}, len(r.stats))
+	stats := make(map[string]interface{}, len(r.fields))
 
 	value := reflect.ValueOf(v)
 	r.checkType(value)
@@ -106,74 +118,10 @@ func (r *Reporter) Report(v interface{}) map[string]interface{} {
 	// dereference the pointer
 	value = value.Elem()
 
-	for name, idx := range r.stats {
-		var v interface{}
-		f := value.Field(idx)
-
-		switch f.Kind() {
-		case reflect.Int:
-			v = int(f.Int())
-		case reflect.Int8:
-			v = int8(f.Int())
-		case reflect.Int16:
-			v = int16(f.Int())
-		case reflect.Int32:
-			v = int32(f.Int())
-		case reflect.Int64:
-			v = int64(f.Int()) //nolint:unconvert
-		case reflect.Uint:
-			v = uint(f.Uint())
-		case reflect.Uint8:
-			v = uint8(f.Uint())
-		case reflect.Uint16:
-			v = uint16(f.Uint())
-		case reflect.Uint32:
-			v = uint32(f.Uint())
-		case reflect.Uint64:
-			v = uint64(f.Uint()) //nolint:unconvert
-		case reflect.Uintptr:
-			v = *(*uintptr)(unsafe.Pointer(f.UnsafeAddr()))
-		case reflect.Ptr:
-			// This is a "trick" to hide the fact that f is unexported from f.Interface,
-			// which (unlike f.Int etc, above) will panic for unexported fields
-			f = reflect.NewAt(f.Type(), unsafe.Pointer(f.UnsafeAddr())).Elem()
-
-			referentType := qualifiedTypeName(f.Elem().Type())
-			switch referentType {
-			case "go.uber.org/atomic.Bool":
-				v = f.Interface().(*atomic.Bool).Load()
-			case "go.uber.org/atomic.Duration":
-				v = f.Interface().(*atomic.Duration).Load()
-			case "go.uber.org/atomic.Error":
-				v = f.Interface().(*atomic.Error).Load()
-			case "go.uber.org/atomic.Float64":
-				v = f.Interface().(*atomic.Float64).Load()
-			case "go.uber.org/atomic.Int32":
-				v = f.Interface().(*atomic.Int32).Load()
-			case "go.uber.org/atomic.Int64":
-				v = f.Interface().(*atomic.Int64).Load()
-			case "go.uber.org/atomic.String":
-				v = f.Interface().(*atomic.String).Load()
-			case "go.uber.org/atomic.Time":
-				v = f.Interface().(*atomic.Time).Load()
-			case "go.uber.org/atomic.Uint32":
-				v = f.Interface().(*atomic.Uint32).Load()
-			case "go.uber.org/atomic.Uint64":
-				v = f.Interface().(*atomic.Uint64).Load()
-			case "go.uber.org/atomic.Uintptr":
-				v = f.Interface().(*atomic.Uintptr).Load()
-			case "go.uber.org/atomic.UnsafePointer":
-				v = f.Interface().(*atomic.UnsafePointer).Load()
-			case "go.uber.org/atomic.Value":
-				v = f.Interface().(*atomic.Value).Load()
-			default:
-				panic("Unrecognized pointer to %s" + referentType)
-			}
-		default:
-			panic(fmt.Sprintf("unrecognized kind %#v for field %s", f.Kind(), name))
-		}
-		stats[name] = v
+	for name, fld := range r.fields {
+		stats[name] = fld.get(value.Field(fld.idx))
 	}
+
 	return stats
 }
 
@@ -184,43 +132,149 @@ func (r *Reporter) checkType(value reflect.Value) {
 	}
 }
 
-func isTypeSupported(f reflect.StructField) bool {
-	switch f.Type.Kind() {
-	case
-		reflect.Int,
-		reflect.Int8,
-		reflect.Int16,
-		reflect.Int32,
-		reflect.Int64,
-		reflect.Uint,
-		reflect.Uint8,
-		reflect.Uint16,
-		reflect.Uint32,
-		reflect.Uint64,
-		reflect.Uintptr:
-		return true
+func getFieldFor(fieldType reflect.Type) (field, error) {
+	switch fieldType.Kind() {
+	case reflect.Int:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return int(v.Int())
+			}}, nil
+	case reflect.Int8:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return int8(v.Int())
+			}}, nil
+	case reflect.Int16:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return int16(v.Int())
+			}}, nil
+	case reflect.Int32:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return int32(v.Int())
+			}}, nil
+	case reflect.Int64:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return int64(v.Int()) //nolint:unconvert
+			}}, nil
+	case reflect.Uint:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return uint(v.Uint())
+			}}, nil
+	case reflect.Uint8:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return uint8(v.Uint())
+			}}, nil
+	case reflect.Uint16:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return uint16(v.Uint())
+			}}, nil
+	case reflect.Uint32:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return uint32(v.Uint())
+			}}, nil
+	case reflect.Uint64:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return uint64(v.Uint()) //nolint:unconvert
+			}}, nil
+	case reflect.Uintptr:
+		return field{
+			get: func(v reflect.Value) interface{} {
+				return *(*uintptr)(unsafe.Pointer(v.UnsafeAddr()))
+			}}, nil
 	case reflect.Ptr:
-		referentType := qualifiedTypeName(f.Type.Elem())
+		referentType := qualifiedTypeName(fieldType.Elem())
 		switch referentType {
-		case
-			"go.uber.org/atomic.Bool",
-			"go.uber.org/atomic.Duration",
-			"go.uber.org/atomic.Error",
-			"go.uber.org/atomic.Float64",
-			"go.uber.org/atomic.Int32",
-			"go.uber.org/atomic.Int64",
-			"go.uber.org/atomic.String",
-			"go.uber.org/atomic.Time",
-			"go.uber.org/atomic.Uint32",
-			"go.uber.org/atomic.Uint64",
-			"go.uber.org/atomic.Uintptr",
-			"go.uber.org/atomic.UnsafePointer",
-			"go.uber.org/atomic.Value":
-			return true
+		case "go.uber.org/atomic.Bool":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Bool).Load()
+				}}, nil
+		case "go.uber.org/atomic.Duration":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Duration).Load()
+				}}, nil
+		case "go.uber.org/atomic.Error":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Error).Load()
+				}}, nil
+		case "go.uber.org/atomic.Float64":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Float64).Load()
+				}}, nil
+		case "go.uber.org/atomic.Int32":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Int32).Load()
+				}}, nil
+		case "go.uber.org/atomic.Int64":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Int64).Load()
+				}}, nil
+		case "go.uber.org/atomic.String":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.String).Load()
+				}}, nil
+		case "go.uber.org/atomic.Time":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Time).Load()
+				}}, nil
+		case "go.uber.org/atomic.Uint32":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Uint32).Load()
+				}}, nil
+		case "go.uber.org/atomic.Uint64":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Uint64).Load()
+				}}, nil
+		case "go.uber.org/atomic.Uintptr":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Uintptr).Load()
+				}}, nil
+		case "go.uber.org/atomic.UnsafePointer":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.UnsafePointer).Load()
+				}}, nil
+		case "go.uber.org/atomic.Value":
+			return field{
+				get: func(v reflect.Value) interface{} {
+					return getPointer(v).(*atomic.Value).Load()
+				}}, nil
 		}
+
+		// NOTE: if adding a type here, also update the doc comment for the Reporter type
 	}
 
-	return false
+	// none of the cases above matched..
+	return field{}, fmt.Errorf("Unrecognized field type %s", fieldType)
+}
+
+// getPointer returns an interface{} representing the data to which pointer v refers.
+func getPointer(v reflect.Value) interface{} {
+	// This is a "trick" to hide the fact that f is unexported from f.Interface,
+	// which (unlike f.Int etc, above) will panic for unexported fields
+	v = reflect.NewAt(v.Type(), unsafe.Pointer(v.UnsafeAddr()))
+	// evaluate v
+	v = v.Elem()
+	// convert to interface{}
+	return v.Interface()
 }
 
 func qualifiedTypeName(ty reflect.Type) string {
