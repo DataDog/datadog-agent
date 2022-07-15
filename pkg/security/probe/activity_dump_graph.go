7be8153107ee5b84a90751c590b799513411c4d5
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-07-15 15:01:56 +0200

    CWS: fix race on activity dump tags (#12765)
    * CWS: fix race on activity dump tags
    
    * CWS: preallocate tags slice

--- pkg/security/probe/activity_dump_graph.go
+++ pkg/security/probe/activity_dump_graph.go
@@ -97,12 +97,12 @@ func (ad *ActivityDump) EncodeDOT() (*bytes.Buffer, error) {
 	ad.Lock()
 	defer ad.Unlock()
 
-	title := fmt.Sprintf("%s: %s", ad.DumpMetadata.Name, ad.GetSelectorStr())
+	title := fmt.Sprintf("%s: %s", ad.DumpMetadata.Name, ad.getSelectorStr())
 	data := ad.prepareGraphData(title)
 	t := template.Must(template.New("tmpl").Parse(GraphTemplate))
 	raw := bytes.NewBuffer(nil)
 	if err := t.Execute(raw, data); err != nil {
-		return nil, fmt.Errorf("couldn't encode %s in %s: %w", ad.GetSelectorStr(), dump.DOT, err)
+		return nil, fmt.Errorf("couldn't encode %s in %s: %w", ad.getSelectorStr(), dump.DOT, err)
 	}
 	return raw, nil
 }
