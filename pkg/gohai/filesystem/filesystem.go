5122de720787965bb0a7053daa7c00e74e85609f
Dustin J. Mitchell <dustin@v.igoro.us>
2022-07-18 09:08:25 -0400

    Be more robust to failures in `df` (#110)

--- pkg/gohai/filesystem/filesystem.go
+++ pkg/gohai/filesystem/filesystem.go
@@ -4,6 +4,7 @@ package filesystem
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os/exec"
 	"regexp"
@@ -13,35 +14,55 @@ import (
 	"github.com/hashicorp/go-multierror"
 )
 
+var dfCommand = "df"
+var dfTimeout = 2 * time.Second
+
 func getFileSystemInfo() (interface{}, error) {
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), dfTimeout)
 	defer cancel()
 
 	/* Grab filesystem data from df	*/
-	cmd := exec.CommandContext(ctx, "df", dfOptions...)
+	cmd := exec.CommandContext(ctx, dfCommand, dfOptions...)
 
-	out, err := cmd.Output()
-	if err != nil {
-		return nil, fmt.Errorf("df failed to collect filesystem data: %s", err)
-	}
+	out, execErr := cmd.Output()
+	var parseErr error
+	var result []interface{}
 	if out != nil {
-		return parseDfOutput(string(out))
+		result, parseErr = parseDfOutput(string(out))
 	}
-	return nil, fmt.Errorf("df failed to collect filesystem data")
+
+	// if we managed to get _any_ data, just use it, ignoring other errors
+	if result != nil && len(result) != 0 {
+		return result, nil
+	}
+
+	// otherwise, prefer the parse error, as it is probably more detailed
+	err := execErr
+	if parseErr != nil {
+		err = parseErr
+	}
+	if err == nil {
+		err = errors.New("unknown error")
+	}
+	return nil, fmt.Errorf("df failed to collect filesystem data: %s", parseErr)
 }
 
-func parseDfOutput(out string) (interface{}, error) {
+func parseDfOutput(out string) ([]interface{}, error) {
 	var aggregateError error
 	lines := strings.Split(out, "\n")
-	var fileSystemInfo = make([]interface{}, len(lines)-2)
-	for i, line := range lines[1:] {
+	if len(lines) < 2 {
+		return nil, errors.New("no output")
+	}
+	var fileSystemInfo = make([]interface{}, 0, len(lines)-2)
+	for _, line := range lines[1:] {
 		values := regexp.MustCompile(`\s+`).Split(line, -1)
 		if len(values) >= expectedLength {
-			var err error
-			fileSystemInfo[i], err = updatefileSystemInfo(values)
+			info, err := updatefileSystemInfo(values)
 			if err != nil {
 				aggregateError = multierror.Append(aggregateError, err)
+			} else {
+				fileSystemInfo = append(fileSystemInfo, info)
 			}
 		}
 	}
