a46b4e351f8244162f6ee90eb5daa8eb55925cd5
Olivier G <olivier.gaca@datadoghq.com>
2022-07-19 19:59:14 +0200

    Add trace init

---
+++ pkg/traceinit/traceinit.go
@@ -0,0 +1,23 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package traceinit
+
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+var initTraceFile *os.File
+var initTraceFileOnce sync.Once
+
+func TraceFunction(text string) {
+	initTraceFileOnce.Do(func() {
+		initTraceFile, _ = os.OpenFile("C:\\ProgramData\\Datadog\\logs\\traceinit.log", os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	})
+	initTraceFile.WriteString(fmt.Sprintf("%v\n", text))
+	initTraceFile.Sync()
+}
