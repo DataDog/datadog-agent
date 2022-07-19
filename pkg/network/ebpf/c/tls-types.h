224f698987a8b37ed6b04929c684b925a97f2fca
Usama Saqib <usama.saqib@datadoghq.com>
2022-07-19 14:55:43 +0200

    code restructuring

--- pkg/network/ebpf/c/tls-types.h
+++ pkg/network/ebpf/c/tls-types.h
@@ -2,12 +2,25 @@
 #define __TLS_TYPES_H
 
 #include "tracer.h"
+#include "classifier.h"
 
 typedef struct {
     __u8 app;
     __u16 version;
     __u16 length;
-} tls_header_t;
+} tls_record_t;
+
+// handshake types
+#define CLIENT_HELLO 1
+#define SERVER_HELLO 2
+#define CERTIFICATE 11
+
+typedef struct {
+    __u8 handshake_type;
+    // the length of the handshake
+    // is given in a field of 24 bytes.
+    u32 length;
+} tls_handshake_t __attribute__((aligned(8)));
 
 #define TLS_HEADER_SIZE 5
 
@@ -18,19 +31,33 @@ typedef struct {
 #define TLS_VERSION12 0x0303
 #define TLS_VERSION13 0x0304
 
+#define TLS_CHANGE_CIPHER 0x14
 #define TLS_ALERT 0x15
 #define TLS_HANDSHAKE 0x16
 #define TLS_APPLICATION_DATA 0x17
+// For tls 1.0, 1.1 and 1.3 maximum allowed size of the TLS fragment
+// is 2^14. However, for tls 1.2 maximum size is (2^14)+1024.
+#define MAX_TLS_FRAGMENT_LENGTH ((1<<14)+1024)
 
 #define TLS_MAX_PACKET_CLASSIFIER 10
 
+// The different states of the tls connection we observe
+#define STATE_HELLO_CLIENT (1)
+#define STATE_HELLO_SERVER (1<<1)
+#define STATE_SHARE_CERTIFICATE (1<<2)
+#define STATE_APPLICATION_DATA (1<<3)
+
 /* packets here is used as guard for miss classification */
 typedef struct {
-    conn_tuple_t tup;
-    __u8 isTLS;
+    cnx_info_t info;
     __u8 packets;
-    __u8 handshake_done;
-    __u8 __padding;
-} tls_session_t;
+    __u8 state;
+    __u16 version;
+    __u16 cipher_suite;
+} tls_session_t __attribute__((aligned(8)));
+
+typedef union {
+	tls_session_t tls;
+} session_t;
 
 #endif
