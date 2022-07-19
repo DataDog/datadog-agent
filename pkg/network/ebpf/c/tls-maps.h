a002aee103c25df84bbbd47d707682040a23ca49
Usama Saqib <usama.saqib@datadoghq.com>
2022-07-19 14:55:43 +0200

    code restructuring

--- pkg/network/ebpf/c/tls-maps.h
+++ pkg/network/ebpf/c/tls-maps.h
@@ -6,10 +6,10 @@
 #include "tls-types.h"
 
 /* This map is used to keep track of in-flight TLS transactions for each TCP connection */
-struct bpf_map_def SEC("maps/tls_in_flight") tls_in_flight = {
+struct bpf_map_def SEC("maps/proto_in_flight") proto_in_flight = {
     .type = BPF_MAP_TYPE_HASH,
     .key_size = sizeof(conn_tuple_t),
-    .value_size = sizeof(tls_session_t),
+    .value_size = sizeof(session_t),
     .max_entries = 1, // This will get overridden at runtime using max_tracked_connections
     .pinning = 0,
     .namespace = "",
--- pkg/network/ebpf/c/tls.h
+++ pkg/network/ebpf/c/tls.h
@@ -1,6 +1,7 @@
 #ifndef __TLS_H
 #define __TLS_H
 
+#include "classifier.h"
 #include "tracer.h"
 #include "tags.h"
 #include "tls-types.h"
@@ -8,46 +9,86 @@
 #include "port_range.h"
 #include "http.h"
 #include "classifier-telemetry.h"
+#include "bpf_endian.h"
 
 #include <uapi/linux/ptrace.h>
 
-static __always_inline void tls_cleanup(conn_tuple_t *tup) {
-    bpf_map_delete_elem(&tls_in_flight, tup);
+static __always_inline int is_valid_tls_app(u8 app) {
+    return (app == TLS_CHANGE_CIPHER) || (app == TLS_ALERT) || (app == TLS_HANDSHAKE) || (app == TLS_APPLICATION_DATA);
 }
 
-static __always_inline int isTLS(tls_header_t *hdr, struct __sk_buff* skb, u32 offset) {
-    if (skb->len - offset < TLS_HEADER_SIZE) {
+static __always_inline int is_valid_tls_version(__u16 version) {
+    return (version == SSL_VERSION20) || (version == SSL_VERSION30) || (version == TLS_VERSION10) || (version == TLS_VERSION11) || (version == TLS_VERSION12) || (version == TLS_VERSION13);
+}
+
+static __always_inline int sane_payload_length(__u8 app, __u16 tls_len, __u16 skb_len) {
+    if (app != TLS_APPLICATION_DATA)
+        return 1;
+
+    if (skb_len > tls_len)
         return 0;
-    }
+
+    return 1;
+}
+
+static __always_inline int is_tls(struct __sk_buff* skb, skb_info_t* skb_info) {
+    if (skb->len - skb_info.data_off <= 0)
+        return 0;
+
+    if (skb->len - offset < TLS_HEADER_SIZE)
+        return 0;
 
     __u8 app = load_byte(skb, offset);
-    if ((app != TLS_HANDSHAKE) &&
-        (app != TLS_APPLICATION_DATA)) {
-            return 0;
-    }
-    hdr->app = app;
+    if (!is_valid_tls_app(app))
+        return 0;
 
     __u16 version = load_half(skb, offset + 1);
-    if ((version != SSL_VERSION20) &&
-        (version != SSL_VERSION30) &&
-        (version != TLS_VERSION10) &&
-        (version != TLS_VERSION11) &&
-        (version != TLS_VERSION12) &&
-        (version != TLS_VERSION13)) {
-            return 0;
-    }
-    hdr->version = version;
+    if (!is_valid_tls_version(version))
+        return 0;
 
     __u16 length = load_half(skb, offset + 3);
-    hdr->length = length;
-    __u16 skblen = skb->len - offset - TLS_HEADER_SIZE;
-    if (skblen < length) {
+    if (length > MAX_TLS_FRAGMENT_LENGTH)
         return 0;
-    }
-    if (skblen == length) {
-        return 1;
-    }
+
+    if (!sane_payload_length(app, length, skb->len))
+        return 0;
+
     return 1;
+}
+
+static __always_inline void parse_tls_server_hello(tls_session_t* tls, struct __sk_buff *skb, u32 offset) {
+    tls->cipher_suite = load_half(skb, offset+45);
+    // TODO: parse extensions to find if tls 1.3
+}
+
+static __always_inline void handle_tls_handshake(tls_session_t* tls, struct __sk_buff* skb, u32 offset) {
+    __u8 handshake = load_byte(skb, offset+5);
+
+    if (handshake == SERVER_HELLO) {
+        tls->state |= STATE_HELLO_SERVER;
+        parse_tls_server_hello(tls, skb, offset);
+    } else if (handshake == CLIENT_HELLO) {
+        tls->state |= STATE_HELLO_CLIENT;
+    } else if (handshake == CERTIFICATE) {
+        tls->state |= STATE_SHARE_CERTIFICATE;
+    }
+}
+
+static __always_inline void transition_session_state(tls_session_t* tls, struct __sk_buff* skb, u32 offset) {
+    tls_record_t record;
+
+    // read the tls record
+    record.app = load_byte(skb, offset);
+    record.version = load_half(skb, offset+1);
+    record.length = __bpf_ntohs(load_half(skb, offset+3));
+    if (skb->len < ((record.length + sizeof(tls_record_t)) + offset))
+        return;
+
+    if (record.app == TLS_HANDSHAKE) {
+        handle_tls_handshake(tls, skb, offset);
+    } else if (record.app == TLS_APPLICATION_DATA) {
+        tls->state |= STATE_APPLICATION_DATA;
+    }
 }
 
 /*
@@ -57,47 +98,36 @@ static __always_inline int isTLS(tls_header_t *hdr, struct __sk_buff* skb, u32 o
  */
 SEC("socket/proto_tls")
 int socket__proto_tls(struct __sk_buff* skb) {
-    skb_info_t skb_info;
-    conn_tuple_t tup;
-    __builtin_memset(&tup, 0, sizeof(tup));
-    if (!read_conn_tuple_skb(skb, &skb_info, &tup)) {
+    proto_args_t *args;
+    skb_info_t *skb_info;
+    conn_tuple_t *tup;
+
+    u32 cpu = bpf_get_smp_processor_id();
+    args = bpf_map_lookup_elem(&proto_args, &cpu);
+    if (args == NULL)
         return 0;
-    }
-    if (skb->len - skb_info.data_off == 0) {
-        return 0;
-    }
-    normalize_tuple(&tup);
+
+    skb_info = &args->skb_info;
+    tup = &args->tup;
 
     tls_session_t *tls = NULL;
     tls_session_t new_entry = { 0 };
-    bpf_map_update_elem(&tls_in_flight, &tup, &new_entry, BPF_NOEXIST);
-    tls = bpf_map_lookup_elem(&tls_in_flight, &tup);
-    if (tls == NULL) {
+    bpf_map_update_elem(&proto_in_flight, tup, &new_entry, BPF_NOEXIST);
+    tls = bpf_map_lookup_elem(&proto_in_flight, tup);
+    if (tls == NULL)
         return 0;
-    }
+
     /* cnx classified or not */
     if (tls->packets > TLS_MAX_PACKET_CLASSIFIER) {
-        return 0;
-    }
-    if (tls->isTLS == 1 && tls->handshake_done == 1) {
+        tls->info.failed = 1;
         return 0;
     }
     tls->packets++;
 
-    /* check packet content */
-    tls_header_t tls_hdr;
-    if (!isTLS(&tls_hdr, skb, skb_info.data_off)) {
-        return 0;
-    }
+    transition_session_state(tls, skb, skb_info->data_off);
 
-    /* we got TLS */
-    if (tls_hdr.app == TLS_APPLICATION_DATA) {
-        tls->handshake_done = 1;
-        add_tags_tuple(&tup, TLS);
-        increment_classifier_telemetry_count(tls_flow_classified);
-    }
-    __builtin_memcpy(&tls->tup, &tup, sizeof(conn_tuple_t));
-    tls->isTLS = 1;
+    if (tls->state & STATE_APPLICATION_DATA)
+        tls->info.done = 1;
 
     return 0;
 }
