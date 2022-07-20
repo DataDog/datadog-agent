94a419a913ec2cdf23e298e121b3ec7c77c5e9f8
Guillaume Fournier <36961134+Gui774ume@users.noreply.github.com>
2022-07-20 11:32:39 +0200

    count syscall event only for new syscalls (#12806)

--- pkg/security/probe/activity_dump.go
+++ pkg/security/probe/activity_dump.go
@@ -1248,6 +1248,7 @@ func (pan *ProcessActivityNode) InsertBindEvent(evt *model.BindEvent) bool {
 
 // InsertSyscalls inserts the syscall of the process in the dump
 func (pan *ProcessActivityNode) InsertSyscalls(e *model.SyscallsEvent) bool {
+	var hasNewSyscalls bool
 newSyscallLoop:
 	for _, newSyscall := range e.Syscalls {
 		for _, existingSyscall := range pan.Syscalls {
@@ -1257,8 +1258,9 @@ newSyscallLoop:
 		}
 
 		pan.Syscalls = append(pan.Syscalls, int(newSyscall))
+		hasNewSyscalls = true
 	}
-	return true
+	return hasNewSyscalls
 }
 
 // FileActivityNode holds a tree representation of a list of files
