ac464ad82d09a0fcd8ba0d8bacab4669f82855c1
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2022-07-25 17:03:58 +0200

    fixup! [system-probe] report open connections on client-side before data transmission

--- pkg/network/tracer/tracer_test.go
+++ pkg/network/tracer/tracer_test.go
@@ -528,6 +528,7 @@ func TestTCPConnsReported(t *testing.T) {
 	c, err := net.DialTimeout("tcp", server.address, 50*time.Millisecond)
 	require.NoError(t, err)
 	defer c.Close()
+	<-processedChan
 
 	// Test
 	initTracerState(t, tr)
