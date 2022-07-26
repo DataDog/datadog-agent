f99ee90db0fad591d39e19166d9f16057a6a53bb
Usama Saqib <usamasaqib.96@live.com>
2022-07-26 11:17:44 +0200

    classifying tls connection even when handshake is missed

--- pkg/ebpf/bytecode/runtime/classifier.go
+++ pkg/ebpf/bytecode/runtime/classifier.go
@@ -4,4 +4,4 @@
 
 package runtime
 
-var Classifier = NewRuntimeAsset("classifier.c", "be43f15cc03e270437163cca6d5d03cfa3a1b69ab129690352794bcbef6f5151")
+var Classifier = NewRuntimeAsset("classifier.c", "687977e4699aa03238d3df1fae456706178c6fa00586269761dddda27b9f68b0")
