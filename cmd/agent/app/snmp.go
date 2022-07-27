8da36a1353e3be309d46fbefb2b40bc232eaac3a
NouhaManai96 <nouha.manai@yahoo.fr>
2022-07-27 16:20:08 +0200

    remove default context name for v3

--- cmd/agent/app/snmp.go
+++ cmd/agent/app/snmp.go
@@ -30,7 +30,7 @@ const (
 	defaultAuthKey       = ""
 	defaultPrivProtocol  = ""
 	defaultPrivKey       = ""
-	defaultContext       = "public"
+	defaultContext       = ""
 	defaultSecurityLevel = ""
 
 	// general communication options
