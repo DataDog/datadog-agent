7449175b3a1738ae90a43505dee28298b7c0ef23
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-07-27 16:49:40 +0200

    Sync tracer integrity check (#12889)

--- pkg/ebpf/bytecode/runtime/tracer.go
+++ pkg/ebpf/bytecode/runtime/tracer.go
@@ -4,4 +4,4 @@
 
 package runtime
 
-var Tracer = NewRuntimeAsset("tracer.c", "153de00b40562f1110207a519706e87fbb3a0c5abb557fa2765b5d6cb5ed6c34")
+var Tracer = NewRuntimeAsset("tracer.c", "6c49784e1b7d8c3f0970dcbfad775e7695e3c019f0e831a8e9bbfa7d9f00d3f2")
