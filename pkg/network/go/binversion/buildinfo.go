ec153799dab566f6a26015bbe8a410fa6a0abc80
Einat Stern <einat.stern@datadoghq.com>
2022-07-28 09:50:03 +0300

    rebase & linting

--- pkg/network/go/binversion/buildinfo.go
+++ pkg/network/go/binversion/buildinfo.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022-present Datadog, Inc.
+//
 // This package contains the `debug/buildinfo` package from Go 1.18,
 // adapted to work in Go 1.17 and modified to only include ELF parsing.
 //
