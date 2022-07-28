a5f2217adf0be0d183325f52f00de6e1d34c2849
Einat Stern <einat.stern@datadoghq.com>
2022-07-28 11:02:42 +0300

    generated files

--- pkg/network/http/gotls/lookup/luts.go
+++ pkg/network/http/gotls/lookup/luts.go
@@ -24,7 +24,7 @@ func GetWriteParams(version goversion.GoVersion, goarch string) ([]bininspect.Pa
 		}
 		return nil, fmt.Errorf("unsupported version go%d.%d.%d (min supported: go%d.%d.%d)", version.Major, version.Minor, version.Rev, 1, 13, 0)
 	case "arm64":
-		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: -1}) {
+		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: 0}) {
 			return []bininspect.ParameterMetadata{{TotalSize: 8, Kind: 0x16, Pieces: []bininspect.ParameterPiece{{Size: 0, InReg: true, StackOffset: 0, Register: 0}}}, {TotalSize: 24, Kind: 0x17, Pieces: []bininspect.ParameterPiece{{Size: 8, InReg: true, StackOffset: 0, Register: 1}, {Size: 8, InReg: true, StackOffset: 0, Register: 2}, {Size: 8, InReg: true, StackOffset: 0, Register: 3}}}}, nil
 		}
 		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 13, Rev: 0}) {
@@ -40,7 +40,7 @@ func GetWriteParams(version goversion.GoVersion, goarch string) ([]bininspect.Pa
 func GetReadParams(version goversion.GoVersion, goarch string) ([]bininspect.ParameterMetadata, error) {
 	switch goarch {
 	case "amd64":
-		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: -1}) {
+		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: 0}) {
 			return []bininspect.ParameterMetadata{{TotalSize: 8, Kind: 0x16, Pieces: []bininspect.ParameterPiece{{Size: 0, InReg: true, StackOffset: 0, Register: 0}}}, {TotalSize: 24, Kind: 0x17, Pieces: []bininspect.ParameterPiece{{Size: 8, InReg: true, StackOffset: 0, Register: 3}, {Size: 8, InReg: true, StackOffset: 0, Register: 2}, {Size: 8, InReg: true, StackOffset: 0, Register: 5}}}}, nil
 		}
 		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 17, Rev: 0}) {
@@ -54,7 +54,7 @@ func GetReadParams(version goversion.GoVersion, goarch string) ([]bininspect.Par
 		}
 		return nil, fmt.Errorf("unsupported version go%d.%d.%d (min supported: go%d.%d.%d)", version.Major, version.Minor, version.Rev, 1, 13, 0)
 	case "arm64":
-		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: -1}) {
+		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: 0}) {
 			return []bininspect.ParameterMetadata{{TotalSize: 8, Kind: 0x16, Pieces: []bininspect.ParameterPiece{{Size: 0, InReg: true, StackOffset: 0, Register: 0}}}, {TotalSize: 24, Kind: 0x17, Pieces: []bininspect.ParameterPiece{{Size: 8, InReg: true, StackOffset: 0, Register: 1}, {Size: 8, InReg: true, StackOffset: 0, Register: 2}, {Size: 8, InReg: true, StackOffset: 0, Register: 3}}}}, nil
 		}
 		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 17, Rev: 0}) {
@@ -84,7 +84,7 @@ func GetCloseParams(version goversion.GoVersion, goarch string) ([]bininspect.Pa
 		}
 		return nil, fmt.Errorf("unsupported version go%d.%d.%d (min supported: go%d.%d.%d)", version.Major, version.Minor, version.Rev, 1, 13, 0)
 	case "arm64":
-		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: -1}) {
+		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 18, Rev: 0}) {
 			return []bininspect.ParameterMetadata{{TotalSize: 8, Kind: 0x16, Pieces: []bininspect.ParameterPiece{{Size: 0, InReg: true, StackOffset: 0, Register: 0}}}}, nil
 		}
 		if version.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 13, Rev: 0}) {
