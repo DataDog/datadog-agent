c118754fbf0e63bd8bb15ca07a98915b4e137fc0
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-07-29 20:07:52 +0000

    Instantiate test cases in release_qa.py

--- tools/agent_QA/release_qa.py
+++ tools/agent_QA/release_qa.py
@@ -40,40 +40,74 @@ version = sys.argv[1]
 board = setup(version)
 
 # Test that apply to all host platforms
-xplatHostTests = [TailFile, TailFileWildcard, TailFileStartPosition]
+xplatHostTests = [
+    TailFile(),
+    TailFileWildcard(),
+    TailFileStartPosition(),
+]
 
 misc = board.add_list("Misc (any platform)")
 Suite(
-    LinuxConfig(), [TailFileMultiLine, TailFileUTF16, TailTCPUDP, EndpointTests, DualShipping, Serverless, StreamLogs]
+    LinuxConfig(),
+    [
+        TailFileMultiLine(),
+        TailFileUTF16(),
+        TailTCPUDP(),
+        EndpointTests(),
+        DualShipping(),
+        Serverless(),
+        StreamLogs(),
+    ],
 ).build(misc.add_card)
 
 kube = board.add_list("Kubernetes")
 Suite(
-    LinuxConfig(), [K8CollectAllDocker, K8DockerContainerLabels, K8CollectAll, K8PodAnnotation, K8FileTailingAnnotation]
+    LinuxConfig(),
+    [
+        K8CollectAllDocker(),
+        K8DockerContainerLabels(),
+        K8CollectAll(),
+        K8PodAnnotation(),
+        K8FileTailingAnnotation(),
+    ],
 ).build(kube.add_card)
 
 containers = board.add_list("Container Runtimes")
 Suite(
     LinuxConfig(),
     [
-        ContainerTailJounald,
-        ContainerCollectAll,
-        AgentUsesAdLabels,
-        DockerMaxFile,
-        DockerFileTailingAD,
-        DockerFileTail,
-        PodmanFileTail,
-        PodmanSocketTail,
+        ContainerTailJounald(),
+        ContainerCollectAll(),
+        AgentUsesAdLabels(),
+        DockerMaxFile(),
+        DockerFileTailingAD(),
+        DockerFileTail(),
+        PodmanFileTail(),
+        PodmanSocketTail(),
     ],
 ).build(containers.add_card)
 
 windows = board.add_list("Windows")
-Suite(WindowsConfig(), xplatHostTests + [TestEventLog]).build(windows.add_card)
+Suite(
+    WindowsConfig(),
+    xplatHostTests
+    + [
+        TestEventLog(),
+    ],
+).build(windows.add_card)
 
 mac = board.add_list("Mac")
 Suite(MacConfig(), xplatHostTests + []).build(mac.add_card)
 
 linux = board.add_list("Linux")
-Suite(LinuxConfig(), xplatHostTests + [TailJounald, TailJournaldStartPosition, SNMPTraps]).build(linux.add_card)
+Suite(
+    LinuxConfig(),
+    xplatHostTests
+    + [
+        TailJounald(),
+        TailJournaldStartPosition(),
+        SNMPTraps(),
+    ],
+).build(linux.add_card)
 
 finish(board)
