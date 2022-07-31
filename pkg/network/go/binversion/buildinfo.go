de8b68eada1f30047e52c878601d2c44f07dd26e
Einat Stern <einat.stern@datadoghq.com>
2022-07-31 11:44:19 +0300

    go flags

--- pkg/network/go/binversion/buildinfo.go
+++ pkg/network/go/binversion/buildinfo.go
@@ -23,6 +23,9 @@
 // Build information is available for the currently running binary in
 // runtime/debug.ReadBuildInfo.
 
+//go:build linux_bpf
+// +build linux_bpf
+
 package binversion
 
 import (
