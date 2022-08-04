86f6e6a9cbe6ebb5320c77f2e60f01c33d288ac9
William Yu <william.yu@datadoghq.com>
2022-08-04 15:15:15 -0400

    make ec2 setup reusable

--- test/new-e2e/processes/docker_test.go
+++ test/new-e2e/processes/docker_test.go
@@ -3,147 +3,131 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package containers
+package processes
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"golang.org/x/crypto/ssh"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/utils/clients"
-	"github.com/DataDog/datadog-agent/test/new-e2e/utils/credentials"
-	"github.com/DataDog/datadog-agent/test/new-e2e/utils/infra"
+	"github.com/DataDog/datadog-api-client-go/v2/api/common"
+	"github.com/DataDog/datadog-api-client-go/v2/api/v2/datadog"
 
-	"github.com/vboulineau/pulumi-definitions/aws"
-	"github.com/vboulineau/pulumi-definitions/aws/ec2/ec2"
-
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
-
-const initScript = `#!/bin/bash
-
-set -ex
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt -y update && apt -y install docker.io
-`
 
 const (
 	containerName = "dd-agent"
 	agentImage    = "public.ecr.aws/datadog/agent:7"
 )
 
-type EC2TestSuite struct {
+type DockerTestSuite struct {
 	suite.Suite
 
-	ddAPIKey   string
-	instanceIP string
-
-	stack     *infra.Stack
-	sshClient *ssh.Client
+	ec2           *EC2TestEnv
+	ddAPIClient   *common.APIClient
+	procAPIClient *datadog.ProcessesApi
 }
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
-func TestEC2TestSuite(t *testing.T) {
-	suite.Run(t, new(EC2TestSuite))
+func TestDockerTestSuite(t *testing.T) {
+	suite.Run(t, new(DockerTestSuite))
 }
 
-func (s *EC2TestSuite) SetupSuite() {
-	config := auto.ConfigMap{}
-	env := aws.NewSandboxEnvironment(config)
-	credentialsManager := credentials.NewManager()
+func (s *DockerTestSuite) SetupSuite() {
+	var err error
 
-	// Retrieving necessary secrets
-	sshKey, err := credentialsManager.GetCredential(credentials.AWSSSMStore, "agent.ci.awssandbox.ssh")
-	require.NoError(s.T(), err)
-
-	s.ddAPIKey, err = credentialsManager.GetCredential(credentials.AWSSSMStore, "agent.ci.dev.apikey")
-	require.NoError(s.T(), err)
-
-	s.stack, err = infra.NewStack(context.Background(),
-		"ci",
-		"ci-agent-docker-vm-hello-world",
-		config,
-		func(ctx *pulumi.Context) error {
-			instance, err := ec2.CreateEC2Instance(ctx, env, "process-agent-ci-docker", "", "amd64",
-				"t3.large", "agent-ci-sandbox", initScript)
-			if err != nil {
-				return err
-			}
-
-			ctx.Export("private-ip", instance.PrivateIp)
-			return nil
-		})
-	require.NoError(s.T(), err)
-	require.NotNil(s.T(), s.stack)
-
-	result, err := s.stack.Up(context.Background())
-	require.NoError(s.T(), err)
-
-	output, found := result.Outputs["private-ip"]
-	require.True(s.T(), found)
-	s.instanceIP = output.Value.(string)
-
-	s.sshClient, _, err = clients.GetSSHClient("ubuntu", fmt.Sprintf("%s:%d", s.instanceIP, 22), sshKey, 2*time.Second, 30)
+	s.ec2, err = NewEC2TestEnv("process-agent-docker-test")
 	require.NoError(s.T(), err)
 
 	s.waitForDocker()
+
+	configuration := common.NewConfiguration()
+	s.ddAPIClient = common.NewAPIClient(configuration)
+	s.procAPIClient = datadog.NewProcessesApi(s.ddAPIClient)
 }
 
-func (s *EC2TestSuite) TearDownSuite() {
-	s.T().Log("tear down stack")
-	_ = s.sshClient.Close()
-	_ = s.stack.Down(context.Background())
+func (s *DockerTestSuite) TearDownSuite() {
+	s.ec2.Close()
 }
 
-func (s *EC2TestSuite) TearDownTest() {
+func (s *DockerTestSuite) TearDownTest() {
 	s.T().Log("terminating docker containers")
-	stdout, err := clients.ExecuteCommand(s.sshClient, "sudo docker kill $(sudo docker ps -aq)")
+	stdout, err := clients.ExecuteCommand(s.ec2.sshClient, "sudo docker kill $(sudo docker ps -aq)")
 	if err != nil {
 		s.T().Logf("error terminating docker containers. stdout:%s, err: %v", stdout, err)
 	}
 
-	stdout, err = clients.ExecuteCommand(s.sshClient, "sudo docker rm $(sudo docker ps -aq)")
+	stdout, err = clients.ExecuteCommand(s.ec2.sshClient, "sudo docker rm $(sudo docker ps -aq)")
 	if err != nil {
 		s.T().Logf("error removing docker containers. stdout:%s, err: %v", stdout, err)
 	}
 	s.T().Log("docker containers terminated")
 }
 
-func (s *EC2TestSuite) TestDockerAgent() {
-	//docker run -d
-	// -v /var/run/docker.sock:/var/run/docker.sock:ro \
-	// -v /proc/:/host/proc/:ro \
-	// -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
-	// -v /etc/datadog-agent/datadog.yaml:/etc/datadog-agent/datadog.yaml \
-	// --name datadog-agent \
-	// -e DD_API_KEY=$API_KEY \
-	//$AGENT_TAG
+func (s *DockerTestSuite) TestDockerAgent() {
+	// TODO:
+	//  - setup a custom hostname with DD_HOSTNAME
+	//  - Enable process-agent
+	//  - Add public API check for processes and containers
 
-	s.T().Log("run datadog agent")
+	hostName := createHostName(s.T().Name())
+
+	s.T().Logf("start the datadog agent with hostname: %s", hostName)
 	// run the agent container on the VM
-	stdout, err := clients.ExecuteCommand(s.sshClient, fmt.Sprintf("sudo docker run -d --cgroupns host"+
+	stdout, err := clients.ExecuteCommand(s.ec2.sshClient, fmt.Sprintf("sudo docker run -d --cgroupns host"+
 		" --name %s"+
 		" -v /var/run/docker.sock:/var/run/docker.sock:ro"+
 		" -v /proc/:/host/proc/:ro"+
 		" -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro"+
 		" -v /dd/config/:/etc/datadog-agent/"+
-		" -e DD_API_KEY=%s %s", containerName, s.ddAPIKey, agentImage))
+		" -e DD_PROCESS_AGENT_ENABLED=true"+
+		" -e DD_HOSTNAME=%s"+
+		" -e DD_API_KEY=%s %s", containerName, hostName, s.ec2.ddAPIKey, agentImage))
 	s.T().Log(stdout)
 	require.NoError(s.T(), err)
 
+	s.waitForProcessAgent()
+
 	// run "echo hello world" on the agent's container
-	stdout, err = clients.ExecuteCommand(s.sshClient, "sudo docker exec dd-agent sh -c \"echo hello world\"")
+	stdout, err = clients.ExecuteCommand(s.ec2.sshClient, "sudo docker exec dd-agent sh -c \"echo hello world\"")
 	s.T().Log(stdout)
 	require.NoError(s.T(), err)
+
+	ctx := common.NewDefaultContext(context.Background())
+	assert.Eventually(s.T(), func() bool {
+		resp, _, err := s.procAPIClient.ListProcesses(ctx, *datadog.NewListProcessesOptionalParameters().
+			//WithSearch("process-agent").
+			WithTags(fmt.Sprintf("host:%s", hostName)).
+			WithPageLimit(20),
+		)
+		if err != nil {
+			s.T().Logf("Error when calling `ProcessesApi.ListProcesses`: %v\n", err)
+			return false
+		}
+		if procs, ok := resp.GetDataOk(); ok && procs != nil {
+			if len(*procs) > 0 {
+				s.T().Log("summaries")
+				for _, summary := range *procs {
+					s.T().Logf("%v", summary)
+				}
+
+				responseContent, _ := json.MarshalIndent(resp, "", "  ")
+				s.T().Logf("Response from `ProcessesApi.ListProcesses`:\n%s\n", responseContent)
+
+				return true
+			}
+		}
+		return false
+	}, 10*time.Minute, 1*time.Second)
+
 }
 
 // TODO: create config helper
@@ -151,18 +135,35 @@ func (s *EC2TestSuite) TestDockerAgent() {
 // TODO: update hosts values on a per test basis
 
 // waitForDocker waits for the docker daemon to startup
-func (s *EC2TestSuite) waitForDocker() {
+func (s *DockerTestSuite) waitForDocker() {
 	var stdout string
 	require.Eventuallyf(s.T(), func() bool {
-		stdout, _ := clients.ExecuteCommand(s.sshClient, "systemctl is-active docker")
+		stdout, _ := clients.ExecuteCommand(s.ec2.sshClient, "systemctl is-active docker")
 		return !strings.Contains(stdout, "inactive")
 	}, 2*time.Minute, 500*time.Millisecond, "docker service is not running")
 	s.T().Logf("docker service is active: %s", stdout)
 
 	require.Eventuallyf(s.T(), func() bool {
-		stdout, _ = clients.ExecuteCommand(s.sshClient, "sudo docker info")
+		stdout, _ = clients.ExecuteCommand(s.ec2.sshClient, "sudo docker info")
 		return !strings.Contains(stdout, "docker: command not found")
 	}, 2*time.Minute, 500*time.Millisecond, "docker is not running")
 
 	s.T().Logf("docker is running: %s", stdout)
+}
+
+func (s *DockerTestSuite) waitForProcessAgent() {
+	var stdout string
+	require.Eventuallyf(s.T(), func() bool {
+		// Get process log
+		stdout, _ = clients.ExecuteCommand(s.ec2.sshClient, "sudo docker logs dd-agent | grep PROCESS")
+		return strings.Contains(stdout, "Starting process-agent for")
+	}, 2*time.Minute, 500*time.Millisecond, "process-agent is not running")
+
+	s.T().Log("process-agent is running")
+}
+
+func createHostName(testName string) string {
+	sl := strings.Split(testName, "/")
+	hostName := fmt.Sprintf("%s-%d", sl[len(sl)-1], time.Now().UnixMilli())
+	return hostName
 }
---
+++ test/new-e2e/processes/ec2_env.go
@@ -0,0 +1,119 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package processes
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"os"
+	"time"
+
+	"github.com/DataDog/datadog-agent/test/new-e2e/utils/clients"
+	"github.com/DataDog/datadog-agent/test/new-e2e/utils/credentials"
+	"github.com/DataDog/datadog-agent/test/new-e2e/utils/infra"
+	"github.com/vboulineau/pulumi-definitions/aws/ec2/ec2"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/vboulineau/pulumi-definitions/aws"
+)
+
+const initScript = `#!/bin/bash
+
+set -ex
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt -y update && apt -y install docker.io
+`
+
+// EC2TestEnv provides an ec2 test environment
+type EC2TestEnv struct {
+	ddAPIKey   string
+	instanceIP string
+
+	sshClient *ssh.Client
+	stack     *infra.Stack
+}
+
+// NewEC2TestEnv creates an EC2TestEnv in the aws sandbox env
+func NewEC2TestEnv(name string) (*EC2TestEnv, error) {
+	ec2TestEnv := &EC2TestEnv{}
+	config := auto.ConfigMap{}
+	env := aws.NewSandboxEnvironment(config)
+	credentialsManager := credentials.NewManager()
+
+	// Retrieving necessary secrets
+	sshKey, err := credentialsManager.GetCredential(credentials.AWSSSMStore, "agent.ci.awssandbox.ssh")
+	if err != nil {
+		return nil, err
+	}
+
+	if ec2TestEnv.ddAPIKey, err = credentialsManager.GetCredential(credentials.AWSSSMStore, "agent.ci.dev.apikey"); err != nil {
+		return nil, err
+	}
+
+	ddAPPKey, err := credentialsManager.GetCredential(credentials.AWSSSMStore, "agent.ci.dev.appkey")
+	if err != nil {
+		return nil, err
+	}
+
+	ec2TestEnv.stack, err = infra.NewStack(context.Background(),
+		"ci",
+		fmt.Sprintf("ci-agent-%s", name),
+		config,
+		func(ctx *pulumi.Context) error {
+			instance, err := ec2.CreateEC2Instance(ctx, env, name, "", "amd64", "t3.large",
+				"agent-ci-sandbox", initScript)
+			if err != nil {
+				return err
+			}
+
+			ctx.Export("private-ip", instance.PrivateIp)
+			return nil
+		})
+	if err != nil {
+		return nil, err
+	}
+	if ec2TestEnv.stack == nil {
+		return nil, errors.New("the ec2 stack is nil")
+	}
+
+	result, err := ec2TestEnv.stack.Up(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	output, found := result.Outputs["private-ip"]
+	if !found {
+		return nil, errors.New("unable to find the ec2 host ip")
+	}
+	ec2TestEnv.instanceIP = output.Value.(string)
+
+	if ec2TestEnv.sshClient, _, err = clients.GetSSHClient("ubuntu", fmt.Sprintf("%s:%d", ec2TestEnv.instanceIP, 22),
+		sshKey, 2*time.Second, 30); err != nil {
+		return nil, err
+	}
+
+	if err = os.Setenv("DD_API_KEY", ec2TestEnv.ddAPIKey); err != nil {
+		return nil, err
+	}
+	if err = os.Setenv("DD_APP_KEY", ddAPPKey); err != nil {
+		return nil, err
+	}
+
+	return ec2TestEnv, nil
+}
+
+// Close performs cleanup and destroys the ec2 stack
+func (e *EC2TestEnv) Close() {
+	_ = os.Unsetenv("DD_API_KEY")
+	_ = os.Unsetenv("DD_APP_KEY")
+	_ = e.sshClient.Close()
+	_ = e.stack.Down(context.Background())
+}
