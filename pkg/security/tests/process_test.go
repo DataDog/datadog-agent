9f25bb2d70c6722c49cba6930ea7ff292ed4df4f
Celia Yuen <celia.yuen@datadoghq.com>
2022-08-04 23:05:45 -0500

    rename test

--- pkg/security/tests/process_test.go
+++ pkg/security/tests/process_test.go
@@ -1525,7 +1525,7 @@ func TestProcessBusybox(t *testing.T) {
 	})
 }
 
-func TestProcessIsPython(t *testing.T) {
+func TestProcessIdentifyInterpreter(t *testing.T) {
 
 	tests := []struct {
 		name            string
