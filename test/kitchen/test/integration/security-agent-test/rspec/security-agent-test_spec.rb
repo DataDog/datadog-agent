b2a0f3085615d0ff10b81ed47c2669ae27404617
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-08 12:12:55 +0200

    CWS: changing describe and it values

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -8,7 +8,7 @@ Dir.glob('/tmp/security-agent/pkg/ebpf/bytecode/build/*.o').each do |f|
 end
 
 describe 'successfully run functional test' do
-  it 'displays PASS and returns 0' do
+  it 'successfully runs' do
     output = `DD_TESTS_RUNTIME_COMPILED=1 DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/pkg/ebpf/bytecode/build sudo -E /tmp/security-agent/testsuite -test.v -status-metrics 1>&2`
     retval = $?
     expect(retval).to eq(0)
@@ -17,7 +17,7 @@ end
 
 if File.readlines("/etc/os-release").grep(/SUSE/).size == 0 and ! File.exists?('/etc/rhsm')
   describe 'successfully run functional test inside a container' do
-    it 'displays PASS and returns 0' do
+    it 'successfully runs' do
       output = `sudo docker exec -e DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/pkg/ebpf/bytecode/build -ti docker-testsuite /tmp/security-agent/testsuite -test.v -status-metrics -env docker 1>&2`
       retval = $?
       expect(retval).to eq(0)
