981f76a237432e4f1c808e88f083b4a5176100e0
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-08 13:19:12 +0200

    CWS: use KernelOut in security agent test

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'kernel_out_spec_helper'
 require 'open3'
 
 GOLANG_TEST_FAILURE = /FAIL:/
@@ -7,19 +7,19 @@ def check_output(output, wait_thr)
   test_failures = []
 
   output.each_line do |line|
-    puts line.strip
-    test_failures << line.strip if line =~ GOLANG_TEST_FAILURE
+    puts KernelOut.format(line.strip)
+    test_failures << KernelOut.format(line.strip) if line =~ GOLANG_TEST_FAILURE
   end
 
   if test_failures.empty? && !wait_thr.value.success?
-    test_failures << "Test command exited with status (#{wait_thr.value.exitstatus}) but no failures were captured."
+    test_failures << KernelOut.format("Test command exited with status (#{wait_thr.value.exitstatus}) but no failures were captured.")
   end
 
   test_failures
 end
 
-print `cat /etc/os-release`
-print `uname -a`
+print KernelOut.format(`cat /etc/os-release`)
+print KernelOut.format(`uname -a`)
 
 Dir.glob('/tmp/security-agent/pkg/ebpf/bytecode/build/*.o').each do |f|
   FileUtils.chmod 0644, f, :verbose => true
@@ -34,8 +34,8 @@ describe 'successfully run functional test' do
   end
 end
 
-if File.readlines("/etc/os-release").grep(/SUSE/).size == 0 and ! File.exists?('/etc/rhsm')
-  describe 'successfully run functional test inside a container' do
+if File.readlines("/etc/os-release").grep(/SUSE/).size == 0 and !File.exists?('/etc/rhsm')
+  describe 'functional test running inside a container' do
     it 'successfully runs' do
       Open3.popen2e("sudo", "docker", "exec", "-e", "DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/pkg/ebpf/bytecode/build", "-ti", "docker-testsuite", "/tmp/security-agent/testsuite", "-test.v", "-status-metrics", "--env", "docker") do |_, output, wait_thr|
         test_failures = check_output(output, wait_thr)
