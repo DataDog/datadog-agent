bd4c2cb22e1065a808b58cd1ae1b948bf0e91fe1
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-08 12:21:52 +0200

    CWS: use popen2e for output and display test failures

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
+require 'open3'
+
+def check_output(output, wait_thr)
+  test_failures = []
+
+  output.each_line do |line|
+    puts line.strip
+    test_failures << line.strip if line =~ GOLANG_TEST_FAILURE
+  end
+
+  if test_failures.empty? && !wait_thr.value.success?
+    test_failures << "Test command exited with status (#{wait_thr.value.exitstatus}) but no failures were captured."
+  end
+
+  test_failures
+end
 
 print `cat /etc/os-release`
 print `uname -a`
@@ -9,18 +25,20 @@ end
 
 describe 'successfully run functional test' do
   it 'successfully runs' do
-    output = `DD_TESTS_RUNTIME_COMPILED=1 DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/pkg/ebpf/bytecode/build sudo -E /tmp/security-agent/testsuite -test.v -status-metrics 1>&2`
-    retval = $?
-    expect(retval).to eq(0)
+    Open3.popen2e({"DD_TESTS_RUNTIME_COMPILED"=>1, "DD_SYSTEM_PROBE_BPF_DIR"=>"/tmp/security-agent/pkg/ebpf/bytecode/build"}, "sudo", "-E", "/tmp/security-agent/testsuite", "-test.v", "-status-metrics") do |_, output, wait_thr|
+      test_failures = check_output(output, wait_thr)
+      expect(test_failures).to be_empty, test_failures.join("\n")
+    end
   end
 end
 
 if File.readlines("/etc/os-release").grep(/SUSE/).size == 0 and ! File.exists?('/etc/rhsm')
   describe 'successfully run functional test inside a container' do
     it 'successfully runs' do
-      output = `sudo docker exec -e DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/pkg/ebpf/bytecode/build -ti docker-testsuite /tmp/security-agent/testsuite -test.v -status-metrics -env docker 1>&2`
-      retval = $?
-      expect(retval).to eq(0)
+      Open3.popen2e("sudo", "docker", "exec", "-e", "DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/pkg/ebpf/bytecode/build", "-ti", "docker-testsuite", "/tmp/security-agent/testsuite", "-test.v", "-status-metrics", "--env", "docker") do |_, output, wait_thr|
+        test_failures = check_output(output, wait_thr)
+        expect(test_failures).to be_empty, test_failures.join("\n")
+      end
     end
   end
 end
