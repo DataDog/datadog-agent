f6b3ad49a6a5cb0a9d7917b3bf263dcfcf18d060
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-18 18:05:35 +0200

    fix linter issue

--- cmd/security-agent/app/runtime.go
+++ cmd/security-agent/app/runtime.go
@@ -658,16 +658,15 @@ func checkPoliciesInner(dir string) error {
 		return err
 	}
 
-	filters := make([]rules.RuleFilter, 0, 1)
 	agentVersionFilter, err := rules.NewAgentVersionFilter(agentVersion)
 	if err != nil {
 		return fmt.Errorf("failed to create agent version filter: %w", err)
-	} else {
-		filters = append(filters, agentVersionFilter)
 	}
 
 	loaderOpts := rules.PolicyLoaderOpts{
-		RuleFilters: filters,
+		RuleFilters: []rules.RuleFilter{
+			agentVersionFilter,
+		},
 	}
 
 	provider, err := rules.NewPoliciesDirProvider(cfg.PoliciesDir, false)
