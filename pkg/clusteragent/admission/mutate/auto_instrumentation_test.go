a26024f7da5e3005c158406c6d5a9da448347868
Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>
2022-08-19 10:44:00 +0200

    [admission][auto-instrumentation] Support Python (#13109)

--- pkg/clusteragent/admission/mutate/auto_instrumentation_test.go
+++ pkg/clusteragent/admission/mutate/auto_instrumentation_test.go
@@ -17,44 +17,79 @@ import (
 
 func TestInjectAutoInstruConfig(t *testing.T) {
 	tests := []struct {
-		name     string
-		pod      *corev1.Pod
-		language string
-		image    string
-		wantErr  bool
+		name           string
+		pod            *corev1.Pod
+		lang           language
+		image          string
+		expectedEnvKey string
+		expectedEnvVal string
+		wantErr        bool
 	}{
 		{
-			name:     "nominal case: java",
-			pod:      fakePod("java-pod"),
-			language: "java",
-			image:    "gcr.io/datadoghq/dd-lib-java-init:v1",
-			wantErr:  false,
+			name:           "nominal case: java",
+			pod:            fakePod("java-pod"),
+			lang:           "java",
+			image:          "gcr.io/datadoghq/dd-lib-java-init:v1",
+			expectedEnvKey: "JAVA_TOOL_OPTIONS",
+			expectedEnvVal: " -javaagent:/datadog-lib/dd-java-agent.jar",
+			wantErr:        false,
 		},
 		{
-			name:     "nominal case: js",
-			pod:      fakePod("js-pod"),
-			language: "js",
-			image:    "gcr.io/datadoghq/dd-lib-js-init:v1",
-			wantErr:  false,
+			name:           "JAVA_TOOL_OPTIONS not empty",
+			pod:            fakePodWithEnvValue("java-pod", "JAVA_TOOL_OPTIONS", "predefined"),
+			lang:           "java",
+			image:          "gcr.io/datadoghq/dd-lib-java-init:v1",
+			expectedEnvKey: "JAVA_TOOL_OPTIONS",
+			expectedEnvVal: "predefined -javaagent:/datadog-lib/dd-java-agent.jar",
+			wantErr:        false,
+		},
+		{
+			name:           "nominal case: js",
+			pod:            fakePod("js-pod"),
+			lang:           "js",
+			image:          "gcr.io/datadoghq/dd-lib-js-init:v1",
+			expectedEnvKey: "NODE_OPTIONS",
+			expectedEnvVal: " --require=/datadog-lib/node_modules/dd-trace/init",
+			wantErr:        false,
+		},
+		{
+			name:           "NODE_OPTIONS not empty",
+			pod:            fakePodWithEnvValue("js-pod", "NODE_OPTIONS", "predefined"),
+			lang:           "js",
+			image:          "gcr.io/datadoghq/dd-lib-js-init:v1",
+			expectedEnvKey: "NODE_OPTIONS",
+			expectedEnvVal: "predefined --require=/datadog-lib/node_modules/dd-trace/init",
+			wantErr:        false,
+		},
+		{
+			name:           "nominal case: python",
+			pod:            fakePod("python-pod"),
+			lang:           "python",
+			image:          "gcr.io/datadoghq/dd-lib-python-init:v1",
+			expectedEnvKey: "PYTHONPATH",
+			expectedEnvVal: "/datadog-lib/",
+			wantErr:        false,
+		},
+		{
+			name:           "PYTHONPATH not empty",
+			pod:            fakePodWithEnvValue("python-pod", "PYTHONPATH", "predefined"),
+			lang:           "python",
+			image:          "gcr.io/datadoghq/dd-lib-python-init:v1",
+			expectedEnvKey: "PYTHONPATH",
+			expectedEnvVal: "/datadog-lib/:predefined",
+			wantErr:        false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := injectAutoInstruConfig(tt.pod, tt.language, tt.image)
+			err := injectAutoInstruConfig(tt.pod, tt.lang, tt.image)
 			require.False(t, (err != nil) != tt.wantErr)
-			switch tt.language {
-			case "java":
-				assertLibConfig(t, tt.pod, tt.image, "JAVA_TOOL_OPTIONS", " -javaagent:/datadog-lib/dd-java-agent.jar", []string{"sh", "copy-lib.sh", "/datadog-lib"})
-			case "js":
-				assertLibConfig(t, tt.pod, tt.image, "NODE_OPTIONS", " --require=/datadog-lib/node_modules/dd-trace/init", []string{"sh", "copy-lib.sh", "/datadog-lib"})
-			default:
-				t.Fatalf("Unknown language %q", tt.language)
-			}
+			assertLibConfig(t, tt.pod, tt.image, tt.expectedEnvKey, tt.expectedEnvVal)
 		})
 	}
 }
 
-func assertLibConfig(t *testing.T, pod *corev1.Pod, image, envKey, envVal string, cmd []string) {
+func assertLibConfig(t *testing.T, pod *corev1.Pod, image, envKey, envVal string) {
 	// Empty dir volume
 	volumeFound := false
 	for _, volume := range pod.Spec.Volumes {
@@ -69,9 +104,9 @@ func assertLibConfig(t *testing.T, pod *corev1.Pod, image, envKey, envVal string
 	// Init container
 	initContainerFound := false
 	for _, container := range pod.Spec.InitContainers {
-		if container.Name == "datadog-tracer-init" {
+		if container.Name == "datadog-lib-init" {
 			require.Equal(t, image, container.Image)
-			require.Equal(t, cmd, container.Command)
+			require.Equal(t, []string{"sh", "copy-lib.sh", "/datadog-lib"}, container.Command)
 			require.Equal(t, "datadog-auto-instrumentation", container.VolumeMounts[0].Name)
 			require.Equal(t, "/datadog-lib", container.VolumeMounts[0].MountPath)
 			initContainerFound = true
@@ -87,7 +122,7 @@ func assertLibConfig(t *testing.T, pod *corev1.Pod, image, envKey, envVal string
 	envFound := false
 	for _, env := range container.Env {
 		if env.Name == envKey {
-			require.Contains(t, envVal, env.Value)
+			require.Equal(t, envVal, env.Value)
 			envFound = true
 			break
 		}
@@ -100,7 +135,7 @@ func TestExtractLibInfo(t *testing.T) {
 		name                 string
 		pod                  *corev1.Pod
 		containerRegistry    string
-		expectedLangauge     string
+		expectedLangauge     language
 		expectedImage        string
 		expectedShouldInject bool
 	}{
@@ -118,6 +153,14 @@ func TestExtractLibInfo(t *testing.T) {
 			containerRegistry:    "registry",
 			expectedLangauge:     "js",
 			expectedImage:        "registry/dd-lib-js-init:v1",
+			expectedShouldInject: true,
+		},
+		{
+			name:                 "python",
+			pod:                  fakePodWithAnnotation("admission.datadoghq.com/python-lib.version", "v1"),
+			containerRegistry:    "registry",
+			expectedLangauge:     "python",
+			expectedImage:        "registry/dd-lib-python-init:v1",
 			expectedShouldInject: true,
 		},
 		{
@@ -139,8 +182,8 @@ func TestExtractLibInfo(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			language, image, shouldInject := extractLibInfo(tt.pod, tt.containerRegistry)
-			require.Equal(t, tt.expectedLangauge, language)
+			lang, image, shouldInject := extractLibInfo(tt.pod, tt.containerRegistry)
+			require.Equal(t, tt.expectedLangauge, lang)
 			require.Equal(t, tt.expectedImage, image)
 			require.Equal(t, tt.expectedShouldInject, shouldInject)
 		})
