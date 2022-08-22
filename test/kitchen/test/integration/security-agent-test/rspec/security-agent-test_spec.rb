783605b62954730b9893942c2c21b4822873b40a
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-22 20:54:49 +0200

    change describe post rebase

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -25,7 +25,7 @@ Dir.glob('/tmp/security-agent/pkg/ebpf/bytecode/build/*.o').each do |f|
   FileUtils.chmod 0644, f, :verbose => true
 end
 
-describe 'successfully run functional test' do
+describe 'functional test running directly on host' do
   it 'successfully runs' do
     Open3.popen2e({"DD_TESTS_RUNTIME_COMPILED"=>1, "DD_SYSTEM_PROBE_BPF_DIR"=>"/tmp/security-agent/pkg/ebpf/bytecode/build"}, "sudo", "-E", "/tmp/security-agent/testsuite", "-test.v", "-status-metrics") do |_, output, wait_thr|
       test_failures = check_output(output, wait_thr)
