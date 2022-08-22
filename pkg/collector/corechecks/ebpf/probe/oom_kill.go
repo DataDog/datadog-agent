b710e71ea63ef147e63d339c09dbe6170b1d6b88
Usama Saqib <usamasaqib.96@live.com>
2022-08-22 17:07:32 +0200

    Explicitly pass debug flag to runtime compiler (#13191)
    * explicitly pass debug flag to runtime compiled object files
    
    * passing debug flag to oom_kill and tcp_queue_length files

--- pkg/collector/corechecks/ebpf/probe/oom_kill.go
+++ pkg/collector/corechecks/ebpf/probe/oom_kill.go
@@ -41,7 +41,7 @@ type OOMKillProbe struct {
 }
 
 func NewOOMKillProbe(cfg *ebpf.Config) (*OOMKillProbe, error) {
-	compiledOutput, err := runtime.OomKill.Compile(cfg, nil, statsd.Client)
+	compiledOutput, err := runtime.OomKill.Compile(cfg, []string{"-g"}, statsd.Client)
 	if err != nil {
 		return nil, err
 	}
