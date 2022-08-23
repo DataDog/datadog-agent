5fd97618eaf5170496f017e10cac05d3ea7754d5
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-23 14:24:07 +0200

    fix again

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 print `cat /etc/os-release`
 print `uname -a`
 
-Dir.glob('/tmp/security-agent/ebpf_bytecode/build/*.o').each do |f|
+Dir.glob('/tmp/security-agent/ebpf_bytecode/*.o').each do |f|
   FileUtils.chmod 0644, f, :verbose => true
 end
 
 describe 'successfully run functional test' do
   it 'displays PASS and returns 0' do
-    output = `DD_TESTS_RUNTIME_COMPILED=1 DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/ebpf_bytecode/build sudo -E /tmp/security-agent/testsuite -test.v -status-metrics 1>&2`
+    output = `DD_TESTS_RUNTIME_COMPILED=1 DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/ebpf_bytecode/ sudo -E /tmp/security-agent/testsuite -test.v -status-metrics 1>&2`
     retval = $?
     expect(retval).to eq(0)
   end
@@ -18,7 +18,7 @@ end
 if File.readlines("/etc/os-release").grep(/SUSE/).size == 0 and ! File.exists?('/etc/rhsm')
   describe 'successfully run functional test inside a container' do
     it 'displays PASS and returns 0' do
-      output = `sudo docker exec -e DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/ebpf_bytecode/build -ti docker-testsuite /tmp/security-agent/testsuite -test.v -status-metrics -env docker 1>&2`
+      output = `sudo docker exec -e DD_SYSTEM_PROBE_BPF_DIR=/tmp/security-agent/ebpf_bytecode/ -ti docker-testsuite /tmp/security-agent/testsuite -test.v -status-metrics -env docker 1>&2`
       retval = $?
       expect(retval).to eq(0)
     end
