9ee01b3ab567bdb26f318f78891ab38524db9955
Paul Coignet <paul.coignet@datadoghq.com>
2022-08-29 17:39:29 +0200

    Strip from file

--- pkg/flare/remote_config.go
+++ pkg/flare/remote_config.go
@@ -7,7 +7,6 @@ package flare
 
 import (
 	"crypto/sha256"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -41,33 +40,17 @@ func zipRemoteConfigDB(tempDir, hostname string) error {
 	defer dstDB.Close()
 
 	err = dstDB.Update(func(tx *bbolt.Tx) error {
-		var targetFilesBuckets [][]byte
-		err := tx.ForEach(func(name []byte, b *bbolt.Bucket) error {
+		return tx.ForEach(func(name []byte, b *bbolt.Bucket) error {
 			if strings.HasSuffix(string(name), "_targets") {
-				newBucket, err := tx.CreateBucket([]byte(fmt.Sprintf("%s_hashed", string(name))))
-				if err != nil {
-					return err
-				}
 				cursor := b.Cursor()
 				for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
-					if err := newBucket.Put(k, hashedRCTargets(v)); err != nil {
+					if err := b.Put(k, hashedRCTargets(v)); err != nil {
 						return err
 					}
 				}
-				targetFilesBuckets = append(targetFilesBuckets, name)
 			}
 			return nil
 		})
-		if err != nil {
-			return err
-		}
-		// Delete the buckets with target files content
-		for _, name := range targetFilesBuckets {
-			if err := tx.DeleteBucket(name); err != nil {
-				return err
-			}
-		}
-		return nil
 	})
 	if err != nil {
 		// Prevent from having a clear db here
