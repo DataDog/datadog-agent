7691c7cec9c529d15ef8140c493fc25cef541b7e
Paul Coignet <paul.coignet@datadoghq.com>
2022-08-29 11:32:07 +0200

    Hash target files

--- pkg/flare/remote_config.go
+++ pkg/flare/remote_config.go
@@ -6,14 +6,65 @@
 package flare
 
 import (
+	"crypto/sha256"
+	"fmt"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/util"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"go.etcd.io/bbolt"
 )
 
+func hashedRCTargets(raw []byte) []byte {
+	hash := sha256.Sum256(raw)
+	return hash[:]
+}
+
 func zipRemoteConfigDB(tempDir, hostname string) error {
-	srcPath := filepath.Join(config.Datadog.GetString("run_path"), "remote-config.db")
 	dstPath := filepath.Join(tempDir, hostname, "remote-config.db")
-	return util.CopyFileAll(srcPath, dstPath)
+	srcPath := filepath.Join(config.Datadog.GetString("run_path"), "remote-config.db")
+
+	err := util.CopyFileAll(srcPath, dstPath)
+	if err != nil {
+		// Prevent from having a clear db here
+		os.Remove(dstPath)
+		return err
+	}
+
+	dstDB, err := bbolt.Open(dstPath, 0600, &bbolt.Options{})
+	if err != nil {
+		os.Remove(dstPath)
+		return err
+	}
+	defer dstDB.Close()
+
+	err = dstDB.Update(func(tx *bbolt.Tx) error {
+		return tx.ForEach(func(name []byte, b *bbolt.Bucket) error {
+			if strings.HasSuffix(string(name), "_targets") {
+				log.Errorf("replacing targets")
+				newBucket, err := tx.CreateBucket([]byte(fmt.Sprintf("%s_hashed", string(name))))
+				if err != nil {
+					return err
+				}
+				cursor := b.Cursor()
+				for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
+					if err := newBucket.Put(k, hashedRCTargets(v)); err != nil {
+						return err
+					}
+				}
+				b.DeleteBucket(name)
+			}
+			return nil
+		})
+	})
+	if err != nil {
+		// Prevent from having a clear db here
+		os.Remove(dstPath)
+		return err
+	}
+
+	return nil
 }
