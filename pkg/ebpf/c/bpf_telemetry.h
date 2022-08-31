546a39469b198116ea538c3f9d59f2474693e75f
Usama Saqib <usamasaqib.96@live.com>
2022-08-31 12:09:18 +0200

    recording telemetry for each errno

--- pkg/ebpf/c/bpf_telemetry.h
+++ pkg/ebpf/c/bpf_telemetry.h
@@ -1,6 +1,7 @@
 #ifndef BPF_TELEMETRY
 #define BPF_TELEMETRY
 
+#include <uapi/asm-generic/errno-base.h>
 #include "bpf_helpers.h"
 #include "map-defs.h"
 #include "defs.h"
@@ -12,36 +13,50 @@
 #define read_indx 1
 #define read_user_indx 2
 #define read_kernel_indx 3
-#define MAX_ERR_TELEMETRY 4
+#define MAX_ERR_TELEMETRY read_kernel_indx
+
+#define MAX_ERRNO (ERANGE + 1)
+#define ERRNO_SLOTS MAX_ERR_TELEMETRY *MAX_ERRNO
 
 struct map_err_telemetry {
-    unsigned int err_count[MAX_ERR_TELEMETRY];
+    unsigned int err_count[ERRNO_SLOTS];
 };
 
 BPF_HASH_MAP(map_err_telemetry_map, unsigned long, struct map_err_telemetry, 128)
 
-#define map_op_with_telemetry(fn, map, args...)                                                                                          \
+#define helper_with_telemetry(fn, map, args...)                                                                                          \
     do {                                                                                                                                 \
-        if (fn(&map, args) < 0) {                                                                                                        \
+        int helper_indx = -1;                                                                                                            \
+        int errno_ret, errno_slot;                                                                                                       \
+        errno_ret = fn(&map, args);                                                                                                      \
+        if (errno_ret < 0) {                                                                                                             \
             unsigned long err_telemetry_key;                                                                                             \
             LOAD_CONSTANT(MK_MAP_KEY(map), err_telemetry_key);                                                                           \
             struct map_err_telemetry *entry =                                                                                            \
                 bpf_map_lookup_elem(&map_err_telemetry_map, &err_telemetry_key);                                                         \
             if (entry) {                                                                                                                 \
                 if ((unsigned long)fn == BPF_FUNC_map_update_elem) {                                                                     \
-                    __sync_fetch_and_add(&entry->err_count[update_indx], 1);                                                             \
+                    helper_indx = update_indx;                                                                                           \
                 } else if ((unsigned long)fn == BPF_FUNC_probe_read) {                                                                   \
-                    __sync_fetch_and_add(&entry->err_count[read_indx], 1);                                                               \
+                    helper_indx = read_indx;                                                                                             \
                 } else if (((unsigned long)fn == BPF_FUNC_probe_read_user) || ((unsigned long)fn == BPF_FUNC_probe_read_user_str)) {     \
-                    __sync_fetch_and_add(&entry->err_count[read_user_indx], 1);                                                          \
+                    helper_indx = read_user_indx;                                                                                        \
                 } else if (((unsigned long)fn == BPF_FUNC_probe_read_kernel) || ((unsigned long)fn == BPF_FUNC_probe_read_kernel_str)) { \
-                    __sync_fetch_and_add(&entry->err_count[read_kernel_indx], 1);                                                        \
+                    helper_indx = read_kernel_indx;                                                                                      \
+                }                                                                                                                        \
+                if (errno_ret >= MAX_ERRNO) {                                                                                            \
+                    errno_slot = MAX_ERRNO - 1;                                                                                          \
+                } else {                                                                                                                 \
+                    errno_slot = errno_ret - 1;                                                                                          \
+                }                                                                                                                        \
+                if ((helper_indx >= 0) && (errno_slot >= 0)) {                                                                           \
+                    __sync_fetch_and_add(&entry->err_count[(helper_indx * MAX_ERRNO) + errno_slot], 1);                                  \
                 }                                                                                                                        \
             }                                                                                                                            \
         }                                                                                                                                \
     } while (0)
 
 #define bpf_map_update_elem(map, key, val, flags) \
-    map_op_with_telemetry(_bpf_map_update_elem, map, key, val, flags)
+    helper_with_telemetry(_bpf_map_update_elem, map, key, val, flags)
 
 #endif
