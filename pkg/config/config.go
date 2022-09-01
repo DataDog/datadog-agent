47d1940973b9c5a2c273b8915731c62c63df6e7d
Xavier Lucas <xavier.lucas@datadoghq.com>
2022-09-01 16:29:45 +0200

    Kubernetes resource discovery (#13228)
    Collector discovery in the orchestrator check

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1005,6 +1005,7 @@ func InitConfig(config Config) {
 	// this option will potentially impact the CPU usage of the agent
 	config.BindEnvAndSetDefault("orchestrator_explorer.container_scrubbing.enabled", true)
 	config.BindEnvAndSetDefault("orchestrator_explorer.custom_sensitive_words", []string{})
+	config.BindEnvAndSetDefault("orchestrator_explorer.collector_discovery.enabled", true)
 	config.BindEnv("orchestrator_explorer.max_per_message")
 	config.BindEnv("orchestrator_explorer.orchestrator_dd_url")
 	config.BindEnv("orchestrator_explorer.orchestrator_additional_endpoints")
