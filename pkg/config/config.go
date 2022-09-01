25125f738dd8855f993e318f363a6e8a118a3537
Dylan Yang <dylan.yang@datadoghq.com>
2022-09-01 10:36:36 -0400

    [serverless][SLS-2333] Downgrade datadog.yaml config warning when agent is running in Lambda/Serverless (#13121)
    downgrade config file log when agent is running in Lambda

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1377,6 +1377,10 @@ func load(config Config, origin string, loadSecret bool) (*Warnings, error) {
 	}()
 
 	if err := config.ReadInConfig(); err != nil {
+		if IsServerless() {
+			log.Debug("No config file detected, using environment variable based configuration only")
+			return &warnings, nil
+		}
 		if errors.Is(err, os.ErrPermission) {
 			log.Warnf("Error loading config: %v (check config file permissions for dd-agent user)", err)
 		} else {
