af0e1b143006411335666a9e9613f3f461f5bce5
Derek Brown <derek.brown@datadoghq.com>
2022-09-05 16:22:23 -0700

    WIP. Begin work on remove hard-coded build path. Hard-coded build path is an artifact of previous Go versions, where datadog-agent had to be within GOPATH.  It no longer needs to be.
    This change should be a <noop>, but opens opportunities for mounting
    different directories in a build container for ongoing development.
    
    Fix some places where merge conflicts were resolved incorrectly.
    
    Fix some places where the execution would occur on the mounted
    filesystem rather than in the container.
    
    Fix using global environment variables when local script variables were
    sufficient.
    
    Update lint scripts to also not require hard-coded directories.
    
    change `lint.ps1` to not use environment variable for variable not needed outside
    of script.
    
    fix expansion of modcache files
    
    By popular demand, delete the modcache tar files even though they're in
    the container.
    
    Hopefully final feature creep.
    Check to see if GOMODCACHE is set.  If not, don't bother
    with the expansion because it will take up a lot of time and space
    for nothing.
    
    And then, use that variable as the target for the unzip rather than
    unzip to hard-coded location.  If GOMODCACHE was pointed elsewhere,
    the expansion would also be wasted.

--- tasks/winbuildscripts/extract-modcache.bat
+++ tasks/winbuildscripts/extract-modcache.bat
@@ -1,10 +1,33 @@
-if exist c:\mnt\modcache.tar.gz (
-    @echo Extracting modcache
-    Powershell -C "7z x c:\mnt\modcache.tar.gz -oc:\mnt"
-    Powershell -C "7z x c:\mnt\modcache.tar -oc:\modcache"
-    del /f c:\mnt\modcache.tar.gz
-    del /f c:\mnt\modcache.tar
+REM don't let variables escape
+@setlocal
+set DEFAULT_MODCACHE_ROOT="c:\mnt"
+set MODCACHE_ROOT=%DEFAULT_MODCACHE_ROOT%
+if not "%1" == "" set MODCACHE_ROOT=%1
+
+set MODCACHE_GZ_FILE="%MODCACHE_ROOT%\modcache.tar.gz"
+set MODCACHE_TAR_FILE="%MODCACHE_ROOT%\modcache.tar"
+
+if "%GOMODCACHE%" == "" (
+    @echo GOMODCACHE environment variable not set, skipping expansion of mod cache files
+    goto :endofscript
+)
+if exist %MODCACHE_GZ_FILE% (
+    @echo Extracting modcache file %MODCACHE_GZ_FILE%
+    Powershell -C "7z x %MODCACHE_GZ_FILE% -o%MODCACHE_ROOT%
+    Powershell -C "7z x %MODCACHE_TAR_FILE% -o%GOMODCACHE%"
     @echo Modcache extracted
 ) else (
     @echo modcache.tar.gz not found, dependencies will be downloaded
 )
+
+:endofscript
+if exist %MODCACHE_GZ_FILE% (
+    @echo deleting modcache tar.gz %MODCACHE_GZ_FILE%
+    del /f %MODCACHE_GZ_FILE%
+)
+if exist %MODCACHE_TAR_FILE% (
+    @echo deleting modcache tar %MODCACHE_TAR_FILE%
+    del /f %MODCACHE_TAR_FILE%
+)
+
+@endlocal
