c647a42c244ca6298ba8bb331b769aff4fd96b29
Usama Saqib <usamasaqib.96@live.com>
2022-09-06 15:19:12 +0200

    no telemetry for 4.4

--- pkg/ebpf/c/bpf_telemetry.h
+++ pkg/ebpf/c/bpf_telemetry.h
@@ -6,7 +6,10 @@
 #include "map-defs.h"
 #include "defs.h"
 
+// The telemetry functions with fail on kernel 4.4, due to
+// reasons described here: https://github.com/DataDog/datadog-agent/blob/main/pkg/network/ebpf/c/http.h#L74
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0) || RHEL_MAJOR == 7
+
 #define IS_PROBE_READ(fn) \
     ((((unsigned long)fn) == BPF_FUNC_probe_read) || (((unsigned long)fn) == BPF_FUNC_probe_read_str))
 #else
@@ -27,6 +30,8 @@
 #define STR(x) #x
 #define MK_KEY(key) STR(key##_telemetry_key)
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)
+
 BPF_HASH_MAP(map_err_telemetry_map, unsigned long, map_err_telemetry_t, 128)
 BPF_HASH_MAP(helper_err_telemetry_map, unsigned long, helper_err_telemetry_t, 256)
 
@@ -37,48 +42,57 @@ BPF_HASH_MAP(helper_err_telemetry_map, unsigned long, helper_err_telemetry_t, 25
         if (errno_ret < 0) {                                                     \
             unsigned long err_telemetry_key;                                     \
             LOAD_CONSTANT(MK_KEY(map), err_telemetry_key);                       \
-            map_err_telemetry_t *entry =                                    \
+            map_err_telemetry_t *entry =                                         \
                 bpf_map_lookup_elem(&map_err_telemetry_map, &err_telemetry_key); \
             if (entry) {                                                         \
-                errno_slot = errno_ret * -1; \
-               if (errno_slot >= T_MAX_ERRNO) { \
-                   errno_slot = T_MAX_ERRNO - 1; \
-               } \
-               errno_slot &= (T_MAX_ERRNO - 1); \
-               __sync_fetch_and_add(&entry->err_count[errno_slot], 1);      \
+                errno_slot = errno_ret * -1;                                     \
+                if (errno_slot >= T_MAX_ERRNO) {                                 \
+                    errno_slot = T_MAX_ERRNO - 1;                                \
+                }                                                                \
+                errno_slot &= (T_MAX_ERRNO - 1);                                 \
+                __sync_fetch_and_add(&entry->err_count[errno_slot], 1);          \
             }                                                                    \
         }                                                                        \
     } while (0)
 
-#define helper_with_telemetry(fn, errno_ret, dst, sz, src)                                              \
-    do {                                                                                                \
-        int helper_indx = -1;                                                                           \
-        int errno_slot;                                                                                 \
-        errno_ret = fn(dst, sz, src);                                                                   \
-        if (errno_ret < 0) {                                                                            \
-            unsigned long telemetry_program_id; \
-            LOAD_CONSTANT("telemetry_program_id_key", telemetry_program_id); \
-            helper_err_telemetry_t *entry =                                                        \
-                bpf_map_lookup_elem(&helper_err_telemetry_map, &telemetry_program_id);                                  \
-            if (entry) {                                                                                \
-                if (IS_PROBE_READ(fn)) {                                                                \
-                    helper_indx = read_indx;                                                            \
-                } else if (IS_PROBE_READ_USER(fn)) {                                                    \
-                    helper_indx = read_user_indx;                                                       \
-                } else if (IS_PROBE_READ_KERNEL(fn)) {                                                  \
-                    helper_indx = read_kernel_indx;                                                     \
-                }                                                                                       \
-                errno_slot = errno_ret * -1; \
-                if (errno_slot >= T_MAX_ERRNO) {                                                           \
+#define helper_with_telemetry(fn, errno_ret, dst, sz, src)                                                \
+    do {                                                                                                  \
+        int helper_indx = -1;                                                                             \
+        int errno_slot;                                                                                   \
+        errno_ret = fn(dst, sz, src);                                                                     \
+        if (errno_ret < 0) {                                                                              \
+            unsigned long telemetry_program_id;                                                           \
+            LOAD_CONSTANT("telemetry_program_id_key", telemetry_program_id);                              \
+            helper_err_telemetry_t *entry =                                                               \
+                bpf_map_lookup_elem(&helper_err_telemetry_map, &telemetry_program_id);                    \
+            if (entry) {                                                                                  \
+                if (IS_PROBE_READ(fn)) {                                                                  \
+                    helper_indx = read_indx;                                                              \
+                } else if (IS_PROBE_READ_USER(fn)) {                                                      \
+                    helper_indx = read_user_indx;                                                         \
+                } else if (IS_PROBE_READ_KERNEL(fn)) {                                                    \
+                    helper_indx = read_kernel_indx;                                                       \
+                }                                                                                         \
+                errno_slot = errno_ret * -1;                                                              \
+                if (errno_slot >= T_MAX_ERRNO) {                                                          \
                     errno_slot = T_MAX_ERRNO - 1;                                                         \
-                }                                                                                       \
-                errno_slot &= (T_MAX_ERRNO - 1); \
-                if (helper_indx >= 0) {                                          \
+                }                                                                                         \
+                errno_slot &= (T_MAX_ERRNO - 1);                                                          \
+                if (helper_indx >= 0) {                                                                   \
                     __sync_fetch_and_add(&entry->err_count[(helper_indx * T_MAX_ERRNO) + errno_slot], 1); \
-                }                                                                                       \
-            }                                                                                           \
-        }                                                                                               \
+                }                                                                                         \
+            }                                                                                             \
+        }                                                                                                 \
     } while (0)
+
+#else
+#define map_update_with_telemetry(fn, map, args...) \
+    fn(&map, args)
+
+#define helper_with_telemetry(fn, errno_ret, dst, sz, src) \
+    errno_ret = fn(dst, sz, src)
+
+#endif // LINUX_VERSION_CODE >= 4.14.0
 
 #define MAP_UPDATE(map, key, val, flags) \
     map_update_with_telemetry(bpf_map_update_elem, map, key, val, flags)
@@ -101,4 +115,4 @@ BPF_HASH_MAP(helper_err_telemetry_map, unsigned long, helper_err_telemetry_t, 25
 #define PROBE_READ_KERNEL_STR(dst, sz, src, errno_ret) \
     helper_with_telemetry(bpf_probe_read_kernel_str, errno_ret, dst, sz, src)
 
-#endif
+#endif // BPF_TELEMETRY_H
