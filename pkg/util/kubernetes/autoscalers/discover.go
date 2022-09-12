b4ff0377b5161e0bf4bbef114549ed9df728414a
Julio Greff <julio.greff@datadoghq.com>
2022-09-12 17:10:45 +0200

    [external metrics] Support HPA v2beta2, v2
    Adds support for v2beta2 and v2 HorizontalPodAutoscalers in the external
    metrics server, effectively adding support for Kubernetes 1.25+, where
    support for v2beta1 has been removed.

---
+++ pkg/util/kubernetes/autoscalers/discover.go
@@ -0,0 +1,42 @@
+package autoscalers
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	autoscalingGroup = "autoscaling"
+	hpaResource      = "horizontalpodautoscalers"
+)
+
+func DiscoverHPAGroupVersionResource(client kubernetes.Interface) (schema.GroupVersionResource, error) {
+	groups, _, err := client.Discovery().ServerGroupsAndResources()
+	if err != nil {
+		if !discovery.IsGroupDiscoveryFailedError(err) {
+			return schema.GroupVersionResource{}, err
+		} else {
+			for group, apiGroupErr := range err.(*discovery.ErrGroupDiscoveryFailed).Groups {
+				log.Warnf("unable to perform resource discovery for group %s: %s", group, apiGroupErr)
+			}
+		}
+	}
+
+	for _, group := range groups {
+		if group.Name != autoscalingGroup {
+			continue
+		}
+
+		return schema.GroupVersionResource{
+			Group:    autoscalingGroup,
+			Version:  group.PreferredVersion.Version,
+			Resource: hpaResource,
+		}, nil
+	}
+
+	return schema.GroupVersionResource{}, fmt.Errorf("cannot find group %q", autoscalingGroup)
+}
