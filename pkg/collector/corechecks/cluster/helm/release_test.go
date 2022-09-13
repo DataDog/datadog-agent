63d897c5b1a3a36240a558507b0f0e4d2f0f3088
David Ortiz <david.ortiz@datadoghq.com>
2022-09-13 12:00:28 +0200

    [corechecks/helm] Add option to set config values as tags (#13451)
    * [corechecks/helm] Add func to get map value with dot-separated key
    
    * [corechecks/helm/release] Add func to get config value
    
    * [corechecks/helm] Add option to set config values as tags
    
    * Add release note
    
    * [corechecks/helm] Store tags to avoid unnecessary recalculations

---
+++ pkg/collector/corechecks/cluster/helm/release_test.go
@@ -0,0 +1,78 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build kubeapiserver
+// +build kubeapiserver
+
+package helm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetConfigValue(t *testing.T) {
+	testRelease := release{
+		Name: "test release",
+		Chart: &chart{
+			Values: map[string]interface{}{
+				"agents": map[string]interface{}{
+					"image": map[string]string{
+						"tag":        "7.39.0",
+						"repository": "datadog/agent", // not overridden
+					},
+				},
+			},
+		},
+		Config: map[string]interface{}{
+			"agents": map[string]interface{}{
+				"image": map[string]string{
+					"tag": "latest", // overrides default value
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		name            string
+		rel             release
+		dotSeparatedKey string
+		expectedValue   string
+		expectsError    bool
+	}{
+		{
+			name:            "config param has been set",
+			rel:             testRelease,
+			dotSeparatedKey: "agents.image.tag",
+			expectedValue:   "latest",
+		},
+		{
+			name:            "config param has a default value in the chart",
+			rel:             testRelease,
+			dotSeparatedKey: "agents.image.repository",
+			expectedValue:   "datadog/agent",
+		},
+		{
+			name:            "config param not set",
+			rel:             testRelease,
+			dotSeparatedKey: "agents.abc",
+			expectsError:    true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := test.rel.getConfigValue(test.dotSeparatedKey)
+
+			if test.expectsError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, test.expectedValue, result)
+			}
+		})
+	}
+}
