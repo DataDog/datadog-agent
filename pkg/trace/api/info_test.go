c39eac34f005191e214f644e8c5ab781a04d4524
Kyle Nusbaum <kyle@datadog.com>
2022-09-19 13:45:37 -0500

    pkg/trace/api: update info endpoint test to check only keys.
    The tests for the info endpoint over-specify the output, making them fail
    and need to be updated whenever anything changes, rather than when a new
    key is added or removed.
    
    This change ensures that the exact keys are present in the output, rather
    than checking specific values.
    
    Fixes #13569

--- pkg/trace/api/info_test.go
+++ pkg/trace/api/info_test.go
@@ -6,6 +6,9 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http/httptest"
 	"net/url"
@@ -17,6 +20,43 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/trace/config"
 	"github.com/DataDog/datadog-agent/pkg/trace/testutil"
 )
+
+func ensureKeys(expect, result map[string]interface{}, prefix string) error {
+	for k, ev := range expect {
+		rv, ok := result[k]
+		if !ok {
+			if prefix != "" {
+				k = prefix + "." + k
+			}
+			return fmt.Errorf("Expected key %s, but it is not present in the output.\n", k)
+		}
+
+		if em, ok := ev.(map[string]interface{}); ok {
+			rm, ok := rv.(map[string]interface{})
+			if !ok {
+				return fmt.Errorf("Expected key %s to be a map, but it is '%#v'.\n", k, rv)
+			}
+			if prefix != "" {
+				prefix = prefix + "." + k
+			} else {
+				prefix = k
+			}
+			if err := ensureKeys(em, rm, prefix); err != nil {
+				return err
+			}
+		}
+	}
+	for k, _ := range result {
+		_, ok := expect[k]
+		if !ok {
+			if prefix != "" {
+				k = prefix + "." + k
+				return fmt.Errorf("Found key %s, but it is not expected in the output. If you've added a new key to the /info endpoint, please add it to the tests.\n", k)
+			}
+		}
+	}
+	return nil
+}
 
 // TestInfoHandler ensures that the keys returned by the /info handler do not
 // change from one release to another to ensure consistency. Tracing clients
@@ -109,146 +149,56 @@ func TestInfoHandler(t *testing.T) {
 		},
 	}
 
-	var testCases = []struct {
-		name     string
-		expected string
-	}{
-		{
-			name: "default",
-			expected: `{
-	"version": "0.99.0",
-	"git_commit": "fab047e10",
-	"endpoints": [
-		"/v0.3/traces",
-		"/v0.3/services",
-		"/v0.4/traces",
-		"/v0.4/services",
-		"/v0.5/traces",
-		"/v0.7/traces",
-		"/profiling/v1/input",
-		"/telemetry/proxy/",
-		"/v0.6/stats",
-		"/v0.1/pipeline_stats",
-		"/evp_proxy/v1/",
-		"/evp_proxy/v2/",
-		"/debugger/v1/input"
-		"/dogstatsd/v1/proxy"
-	],
-	"feature_flags": [
-		"feature_flag"
-	],
-	"client_drop_p0s": true,
-	"span_meta_structs": true,
-	"long_running_spans": true,
-	"config": {
-		"default_env": "prod",
-		"target_tps": 11,
-		"max_eps": 12,
-		"receiver_port": 8111,
-		"receiver_socket": "/sock/path",
-		"connection_limit": 12,
-		"receiver_timeout": 100,
-		"max_request_bytes": 123,
-		"statsd_port": 123,
-		"max_memory": 1000000,
-		"max_cpu": 12345,
-		"analyzed_spans_by_service": {
-			"X": {
-				"Y": 2.4
-			}
-		},
-		"obfuscation": {
-			"elastic_search": true,
-			"mongo": true,
-			"sql_exec_plan": true,
-			"sql_exec_plan_normalize": true,
-			"http": {
-				"remove_query_string": true,
-				"remove_path_digits": true
+	expectedKeys := map[string]interface{}{
+		"version":            nil,
+		"git_commit":         nil,
+		"endpoints":          nil,
+		"feature_flags":      nil,
+		"client_drop_p0s":    nil,
+		"span_meta_structs":  nil,
+		"long_running_spans": nil,
+		"config": map[string]interface{}{
+			"default_env":               nil,
+			"target_tps":                nil,
+			"max_eps":                   nil,
+			"receiver_port":             nil,
+			"receiver_socket":           nil,
+			"connection_limit":          nil,
+			"receiver_timeout":          nil,
+			"max_request_bytes":         nil,
+			"statsd_port":               nil,
+			"max_memory":                nil,
+			"max_cpu":                   nil,
+			"analyzed_spans_by_service": nil,
+			"obfuscation": map[string]interface{}{
+				"elastic_search":          nil,
+				"mongo":                   nil,
+				"sql_exec_plan":           nil,
+				"sql_exec_plan_normalize": nil,
+				"http": map[string]interface{}{
+					"remove_query_string": nil,
+					"remove_path_digits":  nil,
+				},
+				"remove_stack_traces": nil,
+				"redis":               nil,
+				"memcached":           nil,
 			},
-			"remove_stack_traces": false,
-			"redis": true,
-			"memcached": false
-		}
-	}
-}`,
-		},
-		{
-			name: "debug",
-			expected: `{
-	"version": "0.99.0",
-	"git_commit": "fab047e10",
-	"endpoints": [
-		"/v0.3/traces",
-		"/v0.3/services",
-		"/v0.4/traces",
-		"/v0.4/services",
-		"/v0.5/traces",
-		"/v0.7/traces",
-		"/profiling/v1/input",
-		"/telemetry/proxy/",
-		"/v0.6/stats",
-		"/v0.1/pipeline_stats",
-		"/evp_proxy/v1/",
-		"/evp_proxy/v2/",
-		"/evp_proxy/v3/",
-		"/debugger/v1/input"
-		"/dogstatsd/v1/proxy"
-	],
-	"feature_flags": [
-		"feature_flag"
-	],
-	"client_drop_p0s": true,
-	"span_meta_structs": true,
-	"long_running_spans": true,
-	"config": {
-		"default_env": "prod",
-		"target_tps": 11,
-		"max_eps": 12,
-		"receiver_port": 8111,
-		"receiver_socket": "/sock/path",
-		"connection_limit": 12,
-		"receiver_timeout": 100,
-		"max_request_bytes": 123,
-		"statsd_port": 123,
-		"max_memory": 1000000,
-		"max_cpu": 12345,
-		"analyzed_spans_by_service": {
-			"X": {
-				"Y": 2.4
-			}
-		},
-		"obfuscation": {
-			"elastic_search": true,
-			"mongo": true,
-			"sql_exec_plan": true,
-			"sql_exec_plan_normalize": true,
-			"http": {
-				"remove_query_string": true,
-				"remove_path_digits": true
-			},
-			"remove_stack_traces": false,
-			"redis": true,
-			"memcached": false
-		}
-	}
-}`,
 		},
 	}
-	for _, tt := range testCases {
-		t.Run(tt.name, func(t *testing.T) {
-			rcv := newTestReceiverFromConfig(conf)
-			defer testutil.WithFeatures("feature_flag")()
-			_, h := rcv.makeInfoHandler()
-			rec := httptest.NewRecorder()
-			req := httptest.NewRequest("GET", "/info", nil)
-			h.ServeHTTP(rec, req)
-			assert.Equal(t, tt.expected, rec.Body.String())
-			if rec.Body.String() != tt.expected {
-				t.Fatalf("Output of /info has changed. Changing the keys "+
-					"is not allowed because the client rely on them and "+
-					"is considered a breaking change:\n\n%v", rec.Body.String())
-			}
-		})
+
+	rcv := newTestReceiverFromConfig(conf)
+	defer testutil.WithFeatures("feature_flag")()
+	_, h := rcv.makeInfoHandler()
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/info", nil)
+	h.ServeHTTP(rec, req)
+	var m map[string]interface{}
+	b, err := ioutil.ReadAll(rec.Body)
+	if !assert.NoError(t, err) {
+		return
 	}
+	if !assert.NoError(t, json.Unmarshal(b, &m)) {
+		return
+	}
+	assert.NoError(t, ensureKeys(expectedKeys, m, ""))
 }
