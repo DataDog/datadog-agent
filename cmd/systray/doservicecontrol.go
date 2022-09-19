6e859f65bf57901fc882a511ded82a5d61f03de7
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-09-19 17:22:49 +0000

    whoops, those are calling the service-control functions

--- cmd/systray/doservicecontrol.go
+++ cmd/systray/doservicecontrol.go
@@ -8,26 +8,26 @@
 package main
 
 import (
-	runcmd "github.com/DataDog/datadog-agent/cmd/agent/subcommands/run"
+	controlservicecmd "github.com/DataDog/datadog-agent/cmd/agent/subcommands/controlsvc"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 func onRestart() {
-	if err := runcmd.StopService(config.ServiceName, true); err == nil {
-		runcmd.StartService(nil, nil)
+	if err := controlservicecmd.StopService(config.ServiceName, true); err == nil {
+		controlservicecmd.StartService(nil, nil)
 	}
 
 }
 func onStart() {
-	if err := runcmd.StartService(nil, nil); err != nil {
+	if err := controlservicecmd.StartService(nil, nil); err != nil {
 		log.Warnf("Failed to start datadog service %v", err)
 	}
 
 }
 
 func onStop() {
-	if err := runcmd.StopService(config.ServiceName, true); err != nil {
+	if err := controlservicecmd.StopService(config.ServiceName, true); err != nil {
 		log.Warnf("Failed to stop datadog service %v", err)
 	}
 
