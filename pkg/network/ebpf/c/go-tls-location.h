982fddf49dce70f8424fa6d8868efb1dbe6172c4
Einat Stern <einat.stern@datadoghq.com>
2022-09-19 11:36:33 +0300

    test

--- pkg/network/ebpf/c/go-tls-location.h
+++ pkg/network/ebpf/c/go-tls-location.h
@@ -137,7 +137,7 @@ static __always_inline int read_location(struct pt_regs* ctx, location_t* loc, s
     }
 
     if (loc->in_register) {
-        if (size != sizeof(void *)) {
+        if (size != REG_SIZE) {
             return 1;
         }
 
