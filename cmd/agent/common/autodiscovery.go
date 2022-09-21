3c1810b3f39b4666b8ff2d0d4045f23c45553224
Julio Greff <julio.greff@datadoghq.com>
2022-09-21 16:13:28 +0200

    [AD] Disable kubelet, container providers if CCA in AD
    This prevents older agent configs that specifically enable the kubelet
    and container (or docker as it used to be known) from having those
    providers running alongside the new kubecontainer provider, when CCA in
    AD is enabled (which at the time of writing is enabled by default).

--- cmd/agent/common/autodiscovery.go
+++ cmd/agent/common/autodiscovery.go
@@ -17,6 +17,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/autodiscovery/scheduler"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	confad "github.com/DataDog/datadog-agent/pkg/config/autodiscovery"
+	"github.com/DataDog/datadog-agent/pkg/util"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
@@ -28,6 +29,8 @@ var (
 		"kubelet":   {"container": struct{}{}},
 		"container": {"kubelet": struct{}{}},
 	}
+
+	incompatibleProviders = []string{"kubelet", "container", "docker"}
 )
 
 func setupAutoDiscovery(confSearchPaths []string, metaScheduler *scheduler.MetaScheduler) *autodiscovery.AutoConfig {
@@ -64,13 +67,28 @@ func setupAutoDiscovery(confSearchPaths []string, metaScheduler *scheduler.MetaS
 			}
 		}
 
-		// The "docker" config provider was replaced with the "container" one
-		// that supports Docker, but also other runtimes. We need this
-		// conversion to avoid breaking configs that included "docker".
-		if options, found := uniqueConfigProviders["docker"]; found {
-			delete(uniqueConfigProviders, "docker")
-			options.Name = names.Container
-			uniqueConfigProviders["container"] = options
+		ccaInAD := util.CcaInAD()
+		if ccaInAD {
+			var enableContainerProvider bool
+			for _, p := range incompatibleProviders {
+				if _, found := uniqueConfigProviders[p]; found {
+					enableContainerProvider = true
+					delete(uniqueConfigProviders, p)
+				}
+			}
+
+			if enableContainerProvider {
+				uniqueConfigProviders[names.KubeContainer] = config.ConfigurationProviders{Name: names.KubeContainer}
+			}
+		} else {
+			// The "docker" config provider was replaced with the "container" one
+			// that supports Docker, but also other runtimes. We need this
+			// conversion to avoid breaking configs that included "docker".
+			if options, found := uniqueConfigProviders["docker"]; found {
+				delete(uniqueConfigProviders, "docker")
+				options.Name = names.Container
+				uniqueConfigProviders["container"] = options
+			}
 		}
 
 		for _, provider := range extraConfigProviders {
