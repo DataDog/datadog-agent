326dae6e071a80431aa3a44aa672e4072d74b397
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2022-09-22 09:25:45 -0400

    add benchmarks

--- pkg/logs/internal/processor/processor_test.go
+++ pkg/logs/internal/processor/processor_test.go
@@ -70,6 +70,58 @@ func TestReplaceTraceID(t *testing.T) {
 	}
 }
 
+// Avoid benchmark compiler optimisations, see:
+// https://dave.cheney.net/2013/06/30/how-to-write-benchmarks-in-go#compiler-optimisation
+var result1, result2 []byte
+
+func BenchmarkReplaceTraceID(b *testing.B) {
+	for name, in := range map[string]string{
+		"regular":  "log message without a date or any sort of trace id contained within it",
+		"short":    "log message with a weird trace id contained at the end that is not long enough trace_id=12387987ds",
+		"uint64":   "log message with a weird trace id contained at the end that is not long enough trace_id=123879879 span_id=123 other_field=3",
+		"128bit":   "log message with a weird trace id contained at the end that is not long enough trace_id=1b8c31d6d2fa97a97d0763ab555c1d19",
+		"128bit/2": "log message with a weird trace id contained at the end that is not long enough trace_id=1b8c31d6d2fa97a97d0763ab555c1d19 span_id=123 other_field=3",
+	} {
+		content := []byte(in)
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				result1 = replaceTraceID(content)
+			}
+		})
+	}
+}
+
+func BenchmarkApplyRedactingRules(b *testing.B) {
+	source := &sources.LogSource{Config: &config.LogsConfig{}}
+	p := &Processor{
+		processingRules: []*config.ProcessingRule{
+			newProcessingRule(config.MaskSequences, "world", "Matt"),
+		},
+	}
+	base := "this is a log message that doesn't report anything interesting but does say hello to Matt without any trace ids or anything special !"
+	content := []byte(base)
+	msg := newMessage([]byte(content), source, "")
+	b.Run("off", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, result2 = p.applyRedactingRules(msg)
+		}
+	})
+	p.traceIDRule = true
+	for name, in := range map[string]string{
+		"on":     base,
+		"uint64": "this is a log message that doesn't report anything interesting but does say hello to Matt with an old styl dd.trace_id=12387889321 !",
+		"128bit": "this is a log message that doesn't report anything interesting but does say hello to Matt with a trace_id=1b8c31d6d2fa97a97d0763ab555c1d19 !",
+	} {
+		content := []byte(in)
+		msg := newMessage([]byte(content), source, "")
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, result2 = p.applyRedactingRules(msg)
+			}
+		})
+	}
+}
+
 func TestInclusion(t *testing.T) {
 	p := &Processor{processingRules: []*config.ProcessingRule{newProcessingRule("include_at_match", "", "world")}}
 
