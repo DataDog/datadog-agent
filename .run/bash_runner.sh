59c2884d23e0911887487a93e59a844e21dd351e
Daniel Lavie <daniel.lavie@datadoghq.com>
2022-09-28 17:14:58 +0300

    Added ignore list for the local environment variables

--- .run/bash_runner.sh
+++ .run/bash_runner.sh
@@ -38,6 +38,10 @@ else
   remote_env_array_as_grep_patterns=()
   for env in "${remote_env_array[@]}"; do remote_env_array_as_grep_patterns+=(-e "$env"); done
 
+  # Ignore local environment variables that could cause problem in the remote machine
+  ENV_IGNORE_LIST=("TMPDIR" "GOPRIVATE")
+  for env in "${ENV_IGNORE_LIST[@]}"; do remote_env_array_as_grep_patterns+=(-e "$env"); done
+
   # Finally create the environment variable to inject list in the format that works with sh `ssh` command
   env_variables_to_inject=$(env | grep -v -w "${remote_env_array_as_grep_patterns[@]}" | tr '\n' ' ')
   # shellcheck disable=SC2002
