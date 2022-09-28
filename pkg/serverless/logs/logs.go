73c074f9ecc896c5ffd567ee2db289d66938addf
Rey Abolofia <rey.abolofia@datadoghq.com>
2022-09-28 14:54:50 -0700

    Use platform.runtimeDone for the "END RequestId:" message.

--- pkg/serverless/logs/logs.go
+++ pkg/serverless/logs/logs.go
@@ -74,8 +74,6 @@ const (
 
 	// logTypePlatformStart is used for the log message about the platform starting
 	logTypePlatformStart = "platform.start"
-	// logTypePlatformEnd is used for the log message about the platform shutting down
-	logTypePlatformEnd = "platform.end"
 	// logTypePlatformReport is used for the log messages containing a report of the last invocation.
 	logTypePlatformReport = "platform.report"
 	// logTypePlatformLogsDropped is used when AWS has dropped logs because we were unable to consume them fast enough.
@@ -130,7 +128,7 @@ func (l *logMessage) UnmarshalJSON(data []byte) error {
 	case logTypeFunction, logTypeExtension:
 		l.logType = typ
 		l.stringRecord = j["record"].(string)
-	case logTypePlatformStart, logTypePlatformEnd, logTypePlatformReport, logTypePlatformRuntimeDone:
+	case logTypePlatformStart, logTypePlatformReport, logTypePlatformRuntimeDone:
 		l.logType = typ
 		if objectRecord, ok := j["record"].(map[string]interface{}); ok {
 			// all of these have the requestId
@@ -146,10 +144,6 @@ func (l *logMessage) UnmarshalJSON(data []byte) error {
 				l.stringRecord = fmt.Sprintf("START RequestId: %s Version: %s",
 					l.objectRecord.requestID,
 					l.objectRecord.startLogItem.version,
-				)
-			case logTypePlatformEnd:
-				l.stringRecord = fmt.Sprintf("END RequestId: %s",
-					l.objectRecord.requestID,
 				)
 			case logTypePlatformReport:
 				if metrics, ok := objectRecord["metrics"].(map[string]interface{}); ok {
@@ -172,6 +166,8 @@ func (l *logMessage) UnmarshalJSON(data []byte) error {
 				} else {
 					log.Error("LogMessage.UnmarshalJSON: can't read the metrics object")
 				}
+			case logTypePlatformRuntimeDone:
+				l.stringRecord = fmt.Sprintf("END RequestId: %s", l.objectRecord.requestID)
 			}
 		} else {
 			log.Error("LogMessage.UnmarshalJSON: can't read the record object")
