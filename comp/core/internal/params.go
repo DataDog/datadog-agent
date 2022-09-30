2ebfef05a526b36baf62bffaeded3f53fba15f0a
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-09-30 19:57:34 +0000

    Remove support for Android (comp/core changes)

--- comp/core/internal/params.go
+++ comp/core/internal/params.go
@@ -107,9 +107,6 @@ func (params BundleParams) LogForOneShot(loggerName, level string, overrideFromE
 // The log file is set based on the logFileConfig config parameter,
 // or disabled if `disable_file_logging` is set.
 //
-// On Android, the configuration is much like a one-shot app, except that
-// the log level is set based on the `log_level` config param.
-//
 // On platforms which support it, syslog is enabled if `log_to_syslog` is set,
 // using `syslog_uri` or defaulting to "unixgram:///dev/log" if that is empty.
 // The `syslog_rfc` config parameter determines whether this produces 5424-compliant
@@ -119,46 +116,43 @@ func (params BundleParams) LogForOneShot(loggerName, level string, overrideFromE
 // as JSON if `log_format_json` is set.
 func (params BundleParams) LogForDaemon(loggerName, logFileConfig, defaultLogFile string) BundleParams {
 	params.LoggerName = loggerName
-	if runtime.GOOS == "android" {
-		// no log file or syslog on android, and always log to console, not in JSON
-		params.LogLevelFn = func(g configGetter) string { return g.GetString("log_level") }
-		params.LogFileFn = func(configGetter) string { return "" }
-		params.LogSyslogURIFn = func(configGetter) string { return "" }
-		params.LogSyslogRFCFn = func(configGetter) bool { return false }
-		params.LogToConsoleFn = func(configGetter) bool { return true }
-		params.LogFormatJSONFn = func(configGetter) bool { return false }
-	} else {
-		params.LogLevelFn = func(g configGetter) string { return g.GetString("log_level") }
-		params.LogFileFn = func(g configGetter) string {
-			if g.GetBool("disable_file_logging") {
-				return ""
-			}
-			logFile := g.GetString(logFileConfig)
-			if logFile == "" {
-				logFile = defaultLogFile
-			}
-			return logFile
+	params.LogLevelFn = func(g configGetter) string { return g.GetString("log_level") }
+	params.LogFileFn = func(g configGetter) string {
+		if g.GetBool("disable_file_logging") {
+			return ""
 		}
-		params.LogSyslogURIFn = func(g configGetter) string {
-			if runtime.GOOS == "windows" {
-				return "" // syslog not supported on Windows
-			}
-			enabled := g.GetBool("log_to_syslog")
-			uri := g.GetString("syslog_uri")
-
-			if !enabled {
-				return ""
-			}
-
-			if uri == "" {
-				return "unixgram:///dev/log"
-			}
-
-			return uri
+		logFile := g.GetString(logFileConfig)
+		if logFile == "" {
+			logFile = defaultLogFile
 		}
-		params.LogSyslogRFCFn = func(g configGetter) bool { return g.GetBool("syslog_rfc") }
-		params.LogToConsoleFn = func(g configGetter) bool { return g.GetBool("log_to_console") }
-		params.LogFormatJSONFn = func(g configGetter) bool { return g.GetBool("log_format_json") }
+		return logFile
 	}
+	params.LogSyslogURIFn = func(g configGetter) string {
+		if runtime.GOOS == "windows" {
+			return "" // syslog not supported on Windows
+		}
+		enabled := g.GetBool("log_to_syslog")
+		uri := g.GetString("syslog_uri")
+
+		if !enabled {
+			return ""
+		}
+
+		if uri == "" {
+			return "unixgram:///dev/log"
+		}
+
+		return uri
+	}
+	params.LogSyslogRFCFn = func(g configGetter) bool { return g.GetBool("syslog_rfc") }
+	params.LogToConsoleFn = func(g configGetter) bool { return g.GetBool("log_to_console") }
+	params.LogFormatJSONFn = func(g configGetter) bool { return g.GetBool("log_format_json") }
+	return params
+}
+
+// LogToFile modifies the parameters to set the destination log file, overriding any
+// previous logfile parameter.
+func (params BundleParams) LogToFile(logFile string) BundleParams {
+	params.LogFileFn = func(configGetter) string { return logFile }
 	return params
 }
