54d2e282abe49af8c9536409be4d2e6c26fc10c2
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2022-10-03 18:51:17 -0500

    Fixes for running locally

--- tasks/system_probe.py
+++ tasks/system_probe.py
@@ -575,7 +575,7 @@ def kitchen_test(ctx, target=None, provider="virtualbox"):
 
 @task
 def kitchen_genconfig(
-    ctx, ssh_key, platform, osversions, image_size=None, provider="azure", arch=None, azure_sub_id=None
+        ctx, platform, osversions, ssh_key=None, image_size=None, provider="azure", arch=None, azure_sub_id=None
 ):
     if not arch:
         arch = CURRENT_ARCH
@@ -596,9 +596,10 @@ def kitchen_genconfig(
     if azure_sub_id is None and provider == "azure":
         raise Exit("azure subscription id must be specified with --azure-sub-id")
 
-    env = {
-        "KITCHEN_RSA_SSH_KEY_PATH": ssh_key,
-    }
+    env = {}
+    if ssh_key:
+        env["KITCHEN_RSA_SSH_KEY_PATH"] = ssh_key
+
     if azure_sub_id:
         env["AZURE_SUBSCRIPTION_ID"] = azure_sub_id
 
