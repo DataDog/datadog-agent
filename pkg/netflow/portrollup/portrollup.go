5cb069592f70f223657582539169eea2ab0316f0
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-10-05 07:33:15 +0200

    Fix buildStoreKey

--- pkg/netflow/portrollup/portrollup.go
+++ pkg/netflow/portrollup/portrollup.go
@@ -161,5 +161,7 @@ func (prs *EndpointPairPortRollupStore) GetCurStoreLen() int {
 }
 
 func buildStoreKey(sourceAddr []byte, destAddr []byte, endpointT endpointType, port uint16) string {
-	return string(sourceAddr) + string(destAddr) + string([]byte{byte(int(endpointT))}) + string([]byte{byte(int(port))})
+	var portPart1, portPart2 = uint8(port >> 8), uint8(port & 0xff)
+	return string(sourceAddr) + string(destAddr) + string([]byte{byte(endpointT)}) + string([]byte{portPart1}) + string([]byte{portPart2})
+
 }
