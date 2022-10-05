dbb716e3474df7f8847160766e5170a0f1ccb436
Duong (Yoon) <47346352+DDuongNguyen@users.noreply.github.com>
2022-10-05 09:22:04 -0400

    adding NoProxyNonexactMatchExplicitlySet to ProxyMeta payload within Host Payload to identify if customer did explicitly use/change the param no_proxy_nonexact_match within agent config. (#13642)
    Adding an attribute of NoProxyNonexactMatchExplicitlySet to the ProxyMeta payload which ultimately get added to the host meta payload.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -319,7 +319,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("tracemalloc_whitelist", "") // deprecated
 	config.BindEnvAndSetDefault("tracemalloc_blacklist", "") // deprecated
 	config.BindEnvAndSetDefault("run_path", defaultRunPath)
-	config.BindEnvAndSetDefault("no_proxy_nonexact_match", false)
+	config.BindEnv("no_proxy_nonexact_match")
 
 	// Python 3 linter timeout, in seconds
 	// NOTE: linter is notoriously slow, in the absence of a better solution we
