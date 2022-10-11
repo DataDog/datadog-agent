da58c54252cb907d03c27062d8916616dafc670f
Julien Lebot <julien.lebot@datadoghq.com>
2022-10-11 16:56:23 +0200

    Add apm_dd_url test

--- tools/windows/DatadogAgentInstaller/CustomActions.Tests/WriteConfigUnitTests.cs
+++ tools/windows/DatadogAgentInstaller/CustomActions.Tests/WriteConfigUnitTests.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
 using AutoFixture.Xunit2;
 using Datadog.CustomActions;
 using Moq;
 using Xunit;
 using YamlDotNet.RepresentationModel;
+using ISession = Datadog.CustomActions.ISession;
 
 namespace CustomActions.Tests
 {
@@ -75,6 +75,29 @@ random_property: test
                 .ToYaml()
                 .Should()
                 .NotHaveKey(key);
+        }
+
+        [Fact]
+        public void Should_Always_Set_apm_dd_url()
+        {
+            var datadogYaml = @"
+# apm_config:
+
+  # enabled: true
+
+  # apm_dd_url: <ENDPOINT>:<PORT>
+";
+            var sessionMock = new Mock<ISession>();
+            sessionMock.Setup(session => session["TRACE_DD_URL"]).Returns("https://trace.someurl.datadoghq.com");
+            var resultingYaml = ConfigCustomActions.ReplaceProperties(datadogYaml, sessionMock.Object)
+                .ToYaml();
+            resultingYaml
+                .Should()
+                .HaveKey("apm_config.apm_dd_url")
+                .And.HaveValue("https://trace.someurl.datadoghq.com");
+            resultingYaml
+                .Should()
+                .NotHaveKey("apm_config.enabled");
 
         }
     }
