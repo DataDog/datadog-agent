b16d3f2d3ac230bb7de51d0eb839d667443da553
Lénaïc Huard <lenaic.huard@datadoghq.com>
2022-10-11 10:21:52 +0200

    Container check: ignore aberrant values for `container.memory.rss`

--- pkg/collector/corechecks/containers/generic/processor.go
+++ pkg/collector/corechecks/containers/generic/processor.go
@@ -160,7 +160,10 @@ func (p *Processor) processContainer(sender aggregator.Sender, tags []string, co
 		p.sendMetric(sender.Gauge, "container.memory.kernel", containerStats.Memory.KernelMemory, tags)
 		p.sendMetric(sender.Gauge, "container.memory.limit", containerStats.Memory.Limit, tags)
 		p.sendMetric(sender.Gauge, "container.memory.soft_limit", containerStats.Memory.Softlimit, tags)
-		p.sendMetric(sender.Gauge, "container.memory.rss", containerStats.Memory.RSS, tags)
+		// Filter out aberant values
+		if *containerStats.Memory.RSS < 1<<63 {
+			p.sendMetric(sender.Gauge, "container.memory.rss", containerStats.Memory.RSS, tags)
+		}
 		p.sendMetric(sender.Gauge, "container.memory.cache", containerStats.Memory.Cache, tags)
 		p.sendMetric(sender.Gauge, "container.memory.swap", containerStats.Memory.Swap, tags)
 		p.sendMetric(sender.Gauge, "container.memory.oom_events", containerStats.Memory.OOMEvents, tags)
