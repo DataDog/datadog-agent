52dc252887b88956203bc0d883f42747e32c98a7
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-10-12 14:00:38 +0200

    dbg only docker tests

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -25,10 +25,6 @@ print KernelOut.format(`uname -a`)
 
 describe 'functional test running directly on host' do
   it 'successfully runs' do
-    Open3.popen2e({"DD_TESTS_RUNTIME_COMPILED"=>"1", "DD_SYSTEM_PROBE_BPF_DIR"=>"/tmp/security-agent/ebpf_bytecode"}, "sudo", "-E", "/tmp/security-agent/testsuite", "-test.v", "-status-metrics") do |_, output, wait_thr|
-      test_failures = check_output(output, wait_thr, "h")
-      expect(test_failures).to be_empty, test_failures.join("\n")
-    end
   end
 end
 
