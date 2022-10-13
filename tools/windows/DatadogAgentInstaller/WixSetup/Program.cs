d3038d9b286a5af9c4bbc481f121f5bc3bf1d788
Julien Lebot <julien.lebot@datadoghq.com>
2022-10-13 15:52:55 +0200

    Remove warning

--- tools/windows/DatadogAgentInstaller/WixSetup/Program.cs
+++ tools/windows/DatadogAgentInstaller/WixSetup/Program.cs
@@ -9,6 +9,8 @@ namespace WixSetup
         {
             var installer = new AgentInstaller();
             Compiler.LightOptions += "-sval -reusecab -cc \"cabcache\"";
+            // ServiceConfig functionality is documented in the Windows Installer SDK to "not [work] as expected." Consider replacing ServiceConfig with the WixUtilExtension ServiceConfig element.
+            Compiler.CandleOptions += "-sw1150";
             installer.ConfigureProject().BuildMsi();
         }
     }
