e4b7edc54382e57e499f19c233668a78de234f51
Daniel Lavie <daniel.lavie@datadoghq.com>
2022-10-19 12:27:05 +0300

    WIP - getting to kafka process, can try to parse the protocol now

--- pkg/network/ebpf/c/kafka/socket-filter-approach/kafka-types.h
+++ pkg/network/ebpf/c/kafka/socket-filter-approach/kafka-types.h
@@ -5,6 +5,7 @@
 
 //// This determines the size of the payload fragment that is captured for each HTTP request
 //#define HTTP_BUFFER_SIZE (8 * 20)
+#define KAFKA_BUFFER_SIZE (8 * 20)
 //// This controls the number of HTTP transactions read from userspace at a time
 //#define HTTP_BATCH_SIZE 15
 //// HTTP_BATCH_PAGES controls how many `http_batch_t` instances exist for each CPU core
@@ -48,27 +49,29 @@
 //    __u32 page_num;
 //} http_batch_key_t;
 //
-//// HTTP transaction information associated to a certain socket (tuple_t)
-//typedef struct {
-//    conn_tuple_t tup;
-//    __u64 request_started;
-//    __u8  request_method;
-//    __u16 response_status_code;
-//    __u64 response_last_seen;
-//    char request_fragment[HTTP_BUFFER_SIZE] __attribute__ ((aligned (8)));
-//
-//    // this field is used exclusively in the kernel side to prevent a TCP segment
-//    // to be processed twice in the context of localhost traffic. The field will
-//    // be populated with the "original" (pre-normalization) source port number of
-//    // the TCP segment containing the beginning of a given HTTP request
-//    __u16 owned_by_src_port;
-//
-//    // this field is used to disambiguate segments in the context of keep-alives
-//    // we populate it with the TCP seq number of the request and then the response segments
-//    __u32 tcp_seq;
-//
-//    __u64 tags;
+// HTTP transaction information associated to a certain socket (tuple_t)
+// Kafka transaction information associated to a certain socket (tuple_t)
+typedef struct {
+    conn_tuple_t tup;
+    __u64 request_started;
+    __u8  request_method;
+    __u16 response_status_code;
+    __u64 response_last_seen;
+    char request_fragment[KAFKA_BUFFER_SIZE] __attribute__ ((aligned (8)));
+
+    // this field is used exclusively in the kernel side to prevent a TCP segment
+    // to be processed twice in the context of localhost traffic. The field will
+    // be populated with the "original" (pre-normalization) source port number of
+    // the TCP segment containing the beginning of a given HTTP request
+    __u16 owned_by_src_port;
+
+    // this field is used to disambiguate segments in the context of keep-alives
+    // we populate it with the TCP seq number of the request and then the response segments
+    __u32 tcp_seq;
+
+    __u64 tags;
 //} http_transaction_t;
+} kafka_transaction_t;
 //
 //typedef struct {
 //    // idx is a monotonic counter used for uniquely determinng a batch within a CPU core
