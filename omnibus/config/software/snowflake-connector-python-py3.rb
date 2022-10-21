66ff01fa3b54e03fa783dc0c043f6df2e32de985
Kylian Serrania <kylian.serrania@datadoghq.com>
2022-10-21 15:31:59 +0200

    [omnibus] Add explanation for snowflake-connector-python software definition (#13984)
    Following investigations in #13497, add explanations for why we need the snowflake-connector-python-py3 software definition.

--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -1,3 +1,10 @@
+# We need to build the snowflake-connector-python wheel separately because this is the only way to build
+# it on CentOS 6.
+# manylinux2014 wheels (min. requirement: glibc 2.17) do not support CentOS 6 (glibc 2.12). Therefore, when
+# installed by pip on CentOS 6, the wheel is manually compiled. This fails because of a pyarrow build dependency
+# that fails to build (defined in pyproject.toml), hence the need to have a separate software definition where we
+# modify the wheel build.
+
 name "snowflake-connector-python-py3"
 default_version "2.7.12"
 
@@ -13,7 +20,7 @@ build do
   license "Apache-2.0"
   license_file "./LICENSE.txt"
 
-  # This introduces a pyarrow dependency that is not needed for the agent and fails to build on SUSE
+  # This introduces a pyarrow dependency that is not needed for the agent and fails to build on the CentOS 6 builder.
   delete "pyproject.toml"
 
   if windows?
