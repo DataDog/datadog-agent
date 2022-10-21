7534e854a1fb8f69c0fcb8fad027d5ef9fc22661
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-10-21 10:05:36 -0400

    Generate CODEOWNERS and comp/README.md for components (#13878)
    Co-authored-by: Pablo Baeyens <pablo.baeyens@datadoghq.com>

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -28,6 +28,7 @@ from . import (
     vscode,
 )
 from .build_tags import audit_tag_impact, print_default_build_tags
+from .components import lint_components
 from .fuzz import fuzz
 from .go import (
     check_mod_tidy,
@@ -68,6 +69,7 @@ ns.add_task(deps)
 ns.add_task(deps_vendored)
 ns.add_task(lint_licenses)
 ns.add_task(generate_licenses)
+ns.add_task(lint_components)
 ns.add_task(generate_protobuf)
 ns.add_task(reset)
 ns.add_task(lint_copyrights),
