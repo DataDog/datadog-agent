05a56cef9e638c5d8f86c640b6c5715e3c6f72db
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 18:34:46 +0000

    Adding tarfile member sanitization to extractall()

--- tasks/libs/junit_upload.py
+++ tasks/libs/junit_upload.py
@@ -105,7 +105,26 @@ def junit_upload_from_tgz(junit_tgz, codeowners_path=".github/CODEOWNERS"):
     with tempfile.TemporaryDirectory() as unpack_dir:
         # unpack all files from archive
         with tarfile.open(junit_tgz) as tgz:
-            tgz.extractall(path=unpack_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tgz, path=unpack_dir)
         # read additional tags
         with open(os.path.join(unpack_dir, TAGS_FILE_NAME)) as tf:
             tags = tf.read().split()
