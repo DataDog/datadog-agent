04c34c37293b1679994b86f5693df210fff044e6
Sylvain Baubeau <lebauce@gmail.com>
2022-10-25 13:59:29 +0200

    [CWS] Fix VERSION_ID check on RHEL 7 (#14034)
    (cherry picked from commit 2c022f73fd2743cdc190d3dd4045aa2789c64615)

--- pkg/security/ebpf/kernel/kernel.go
+++ pkg/security/ebpf/kernel/kernel.go
@@ -205,7 +205,7 @@ func (k *Version) UbuntuKernelVersion() *kernel.UbuntuKernelVersion {
 
 // IsRH7Kernel returns whether the kernel is a rh7 kernel
 func (k *Version) IsRH7Kernel() bool {
-	return (k.OsRelease["ID"] == "centos" || k.OsRelease["ID"] == "rhel") && k.OsRelease["VERSION_ID"] == "7"
+	return (k.OsRelease["ID"] == "centos" || k.OsRelease["ID"] == "rhel") && strings.HasPrefix(k.OsRelease["VERSION_ID"], "7")
 }
 
 // IsRH8Kernel returns whether the kernel is a rh8 kernel
