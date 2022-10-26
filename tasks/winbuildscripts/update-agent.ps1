6e1ef02cc4327fd78324ebefc412094a3fc8f729
Branden Clark <branden.clark@datadoghq.com>
2022-10-26 18:03:37 -0400

    Delete update-agent.ps1

--- tasks/winbuildscripts/update-agent.ps1
+++
@@ -1,4 +0,0 @@
-#Stop-Service -Name datadogagent -Force -Verbose
-#cp \\tsclient\C\Users\branden.clark\projects\datadog-agent-wsl\datadog-agent\bin\agent\agent.exe "$env:PROGRAMFILES\Datadog\Datadog Agent\bin\agent.exe"
-#Start-Service -Name datadogagent
-dir \\tsclient\C\Users
