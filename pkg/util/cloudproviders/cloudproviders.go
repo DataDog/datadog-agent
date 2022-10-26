TrellixVulnTeam/datadog-agent_6PO2

Adding cloud provider identifiers for ec2
In some setup the agent can't reliably detect if it's running on a cloud
provider. Identifiers are hints for the backend to try to link Agent
host to cloud providers resources.

For now only EC2 is supported, in a very minimal way.

pkg/util/cloudproviders/cloudproviders.go
@@ -33,6 +33,11 @@ type cloudProviderDetector struct {
 	callback func(context.Context) bool
 }
 
+type cloudProviderHintDetector struct {
+	name     string
+	callback func(context.Context) map[string]string
+}
+
 // DetectCloudProvider detects the cloud provider where the agent is running in order:
 func DetectCloudProvider(ctx context.Context) {
 	detectors := []cloudProviderDetector{
@@ -54,6 +59,30 @@ func DetectCloudProvider(ctx context.Context) {
 		}
 	}
 	log.Info("No cloud provider detected")
+
+	// At this point we haven't found any cloud provider. Some cloud provider aren't metadata endpoint aren't
+	// reliable and some customer setup prevent us from detecting cloud providers. This can prevent the backend from
+	// associating the hostname found by the agent to a known host in the backend.
+	//
+	// In those cases, we gather as much data, or hint, as possible on the cloud providers. This will help corrolate
+	// hosts in the backend.
+
+	// not all cloud providers provides hints yet.
+	identifiersDetectors := []cloudProviderDetector{
+		{name: ec2.CloudProviderName, callback: ec2.IsRunningOnHints},
+	}
+
+	cloudIdentifiers := map[string]string{}
+	for _, cloudDetector := range identifiersDetectors {
+		for identifier, value := range cloudDetector.callback(ctx) {
+			cloudIdentifiers[identifier] = value
+			log.Infof("Identifier for cloud provider %s detected: '%s' is '%s'", cloudDetector.name, identifier, value)
+		}
+	}
+
+	if len(cloudIdentifiers) != 0 {
+		inventories.SetHostMetadata(inventories.HostCloudIdentifiers, cloudIdentifiers)
+	}
 }
 
 type cloudProviderNTPDetector struct {

DataDog/datadog-agent

pkg/util/cloudproviders/cloudproviders.go
@@ -33,6 +33,11 @@ type cloudProviderDetector struct {
 	callback func(context.Context) bool
 }
 
+type cloudProviderIdentifiersDetector struct {
+	name     string
+	callback func(context.Context) map[string]string
+}
+
 // DetectCloudProvider detects the cloud provider where the agent is running in order:
 func DetectCloudProvider(ctx context.Context) {
 	detectors := []cloudProviderDetector{
@@ -54,6 +59,30 @@ func DetectCloudProvider(ctx context.Context) {
 		}
 	}
 	log.Info("No cloud provider detected")
+
+	// At this point we haven't found any cloud provider. Some cloud provider aren't metadata endpoint aren't
+	// reliable and some customer setup prevent us from detecting cloud providers. This can prevent the backend from
+	// associating the hostname found by the agent to a known host in the backend.
+	//
+	// In those cases, we gather as much data, or hint, as possible on the cloud providers. This will help corrolate
+	// hosts in the backend.
+
+	// not all cloud providers provides hints yet.
+	identifiersDetectors := []cloudProviderIdentifiersDetector{
+		{name: ec2.CloudProviderName, callback: ec2.CollectIdentifiers},
+	}
+
+	cloudIdentifiers := map[string]string{}
+	for _, cloudDetector := range identifiersDetectors {
+		for identifier, value := range cloudDetector.callback(ctx) {
+			cloudIdentifiers[identifier] = value
+			log.Infof("Identifier for cloud provider %s detected: '%s' is '%s'", cloudDetector.name, identifier, value)
+		}
+	}
+
+	if len(cloudIdentifiers) != 0 {
+		inventories.SetHostMetadata(inventories.HostCloudIdentifiers, cloudIdentifiers)
+	}
 }
 
 type cloudProviderNTPDetector struct {