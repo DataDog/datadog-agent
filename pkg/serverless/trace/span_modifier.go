cb9d37d6d0d4552abb04774d12acd7f09d6c9cc0
AJ Stuyvenberg <astuyve@gmail.com>
2022-11-08 11:10:24 -0500

    feat: Move coldstart span creation out of conditional where span tag is missing

--- pkg/serverless/trace/span_modifier.go
+++ pkg/serverless/trace/span_modifier.go
@@ -22,9 +22,11 @@ type spanModifier struct {
 
 // Process applies extra logic to the given span
 func (s *spanModifier) ModifySpan(span *pb.Span) {
-	if span.Service == "aws.lambda" && s.tags["service"] != "" {
+	if span.Service == "aws.lambda" {
 		// service name could be incorrectly set to 'aws.lambda' in datadog lambda libraries
-		span.Service = s.tags["service"]
+		if s.tags["service"] != "" {
+			span.Service = s.tags["service"]
+		}
 		if s.coldStartSpanCreator != nil {
 			s.coldStartSpanCreator.create(span)
 		}
