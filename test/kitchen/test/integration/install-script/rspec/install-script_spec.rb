e2988db2adfffa52892f2b1ab3af109fa9439c3d
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2022-11-11 09:26:11 +0100

    Fix one more tool_version occurence

--- test/kitchen/test/integration/install-script/rspec/install-script_spec.rb
+++ test/kitchen/test/integration/install-script/rspec/install-script_spec.rb
@@ -55,7 +55,7 @@ shared_examples_for 'Dogstatsd installed by the install script' do
     it 'adds an install_info' do
       expect(install_info['install_method']).to match(
         'tool' => 'install_script',
-        'tool_version' => 'install_script',
+        'tool_version' => /^install_script_agent\d+$/,
         'installer_version' => /^install_script-\d+\.\d+\.\d+(.post)?$/
       )
     end
