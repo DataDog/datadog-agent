14ede02d92b81cddd108416e3b84257dfe0ab9df
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-11-16 17:10:04 +0100

    [netflow] Try fix flaky test `TestAggragator` (#14346)

--- pkg/netflow/flowaggregator/aggregator.go
+++ pkg/netflow/flowaggregator/aggregator.go
@@ -135,15 +135,20 @@ func (agg *FlowAggregator) flush() int {
 		agg.sendFlows(flowsToFlush)
 	}
 
-	agg.flushedFlowCount.Add(uint64(len(flowsToFlush)))
+	flushCount := len(flowsToFlush)
+
 	agg.sender.MonotonicCount("datadog.netflow.aggregator.hash_collisions", float64(agg.flowAcc.hashCollisionFlowCount.Load()), "", nil)
 	agg.sender.MonotonicCount("datadog.netflow.aggregator.flows_received", float64(agg.receivedFlowCount.Load()), "", nil)
-	agg.sender.MonotonicCount("datadog.netflow.aggregator.flows_flushed", float64(agg.flushedFlowCount.Load()), "", nil)
+	agg.sender.Count("datadog.netflow.aggregator.flows_flushed", float64(flushCount), "", nil)
 	agg.sender.Gauge("datadog.netflow.aggregator.flows_contexts", float64(flowsContexts), "", nil)
 	agg.sender.Gauge("datadog.netflow.aggregator.port_rollup.current_store_size", float64(agg.flowAcc.portRollup.GetCurrentStoreSize()), "", nil)
 	agg.sender.Gauge("datadog.netflow.aggregator.port_rollup.new_store_size", float64(agg.flowAcc.portRollup.GetNewStoreSize()), "", nil)
 	agg.sender.Gauge("datadog.netflow.aggregator.input_buffer.capacity", float64(cap(agg.flowIn)), "", nil)
 	agg.sender.Gauge("datadog.netflow.aggregator.input_buffer.length", float64(len(agg.flowIn)), "", nil)
+
+	// We increase `flushedFlowCount` at the end to be sure that the metrics are submitted before hand.
+	// Tests will wait for `flushedFlowCount` to be increased before asserting the metrics.
+	agg.flushedFlowCount.Add(uint64(flushCount))
 
 	return len(flowsToFlush)
 }
