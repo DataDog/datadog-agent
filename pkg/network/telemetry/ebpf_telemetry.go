af1b231f83909d01e8650623c688b42a0a6f5233
Usama Saqib <usamasaqib.96@live.com>
2022-11-18 10:57:55 +0100

    continue for undefinedProbes loop as well

--- pkg/network/telemetry/ebpf_telemetry.go
+++ pkg/network/telemetry/ebpf_telemetry.go
@@ -273,7 +273,7 @@ func getAllProgramSpecs(m *manager.Manager, undefinedProbes []manager.ProbeIdent
 			return nil, err
 		}
 		if !present {
-			return nil, fmt.Errorf("could not find ProgramSpec for probe %v", id)
+			continue
 		}
 
 		specs = append(specs, s...)
