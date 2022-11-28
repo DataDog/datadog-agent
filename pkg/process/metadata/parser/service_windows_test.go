0ad287ffa50b2cc4e0d03dc0da952e80fa1d88f2
William Yu <william.yu@datadoghq.com>
2022-11-28 17:16:16 -0500

    add windows test

---
+++ pkg/process/metadata/parser/service_windows_test.go
@@ -0,0 +1,51 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build windows
+// +build windows
+
+package parser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	ddconfig "github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/process/procutil"
+)
+
+func TestWindowsExtractServiceMetadata(t *testing.T) {
+	tests := []struct {
+		name               string
+		cmdline            []string
+		expectedServiceTag string
+	}{
+		{
+			name: "CDPSvc",
+			cmdline: []string{
+				"C:\\Windows\\system32\\svchost.exe", "-k", "LocalService", "-p", "-s", "CDPSvc",
+			},
+			expectedServiceTag: "service:CDPSvc",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockConfig := ddconfig.Mock(t)
+			mockConfig.Set("service_monitoring_config.process_service_inference.enabled", true)
+
+			proc := procutil.Process{
+				Pid:     1,
+				Cmdline: tt.cmdline,
+			}
+			procsByPid := map[int32]*procutil.Process{proc.Pid: &proc}
+
+			se := NewServiceExtractor()
+			se.Extract(procsByPid)
+			assert.Equal(t, tt.expectedServiceTag, se.GetServiceTag(proc.Pid))
+		})
+	}
+}
