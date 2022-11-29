3d34147aa1f63b3af3f6118faf8b40090c2a6763
nam <nam.nguyen@datadoghq.com>
2022-11-29 14:57:17 +0100

    [orchestration] detect ust for cluster level resources for orchestrator explorer

---
+++ pkg/collector/corechecks/cluster/orchestrator/transformers/ust_test.go
@@ -0,0 +1,39 @@
+package transformers
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/util/kubernetes"
+)
+
+func TestRetrieveUST(t *testing.T) {
+	cfg := config.Mock(t)
+	cfg.Set("env", "staging")
+	cfg.Set("version", "not-applied")
+
+	tests := []struct {
+		name   string
+		labels map[string]string
+		want   []string
+	}{
+		{
+			name:   "label contains ust, labels ust takes precedence",
+			labels: map[string]string{kubernetes.EnvTagLabelKey: "prod", kubernetes.VersionTagLabelKey: "123", kubernetes.ServiceTagLabelKey: "app"},
+			want:   []string{"env:prod", "version:123", "service:app"},
+		},
+		{
+			name:   "label does not contain env, takes from config",
+			labels: map[string]string{},
+			want:   []string{"env:staging"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := retrieveUST(tt.labels); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("retrieveUST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
