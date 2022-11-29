ea4cab26ea0ceecf9c6eb3912755fdc6487976e1
Nicolas Vivet <nicolas.vivet@datadoghq.com>
2022-11-29 13:19:49 +0100

    envoy support

--- pkg/trace/appsec/envoy.go
+++ pkg/trace/appsec/envoy.go
@@ -7,12 +7,17 @@ package appsec
 
 import (
 	"context"
+	"fmt"
+	"net/url"
+	"strings"
 
 	"github.com/DataDog/datadog-agent/pkg/trace/log"
 	waf "github.com/DataDog/go-libddwaf"
+	//envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoy_service_auth_v3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
 	"google.golang.org/genproto/googleapis/rpc/code"
 	"google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type server struct {
@@ -27,6 +32,46 @@ func NewEnvoyAuthorizationServer(wafHandle *waf.Handle) envoy_service_auth_v3.Au
 	return &server{
 		wafHandle: wafHandle,
 	}
+}
+
+func reqToClientIp(req *envoy_service_auth_v3.CheckRequest) string {
+	sockAddr := req.Attributes.Source.Address.GetSocketAddress()
+	if sockAddr == nil {
+		log.Warnf("appsec envoy authorization api: unsupported source: %v", req.Attributes.Source)
+		return ""
+	}
+	// TODO support for client IP detection
+	peerIp := sockAddr.Address
+	return peerIp
+}
+
+func reqToAddrs(req *envoy_service_auth_v3.CheckRequest) map[string]interface{} {
+	httpReq := req.Attributes.Request.Http
+	httpPathIdx := strings.Index(httpReq.Path, "?")
+	if httpPathIdx == -1 {
+		httpPathIdx = len(httpReq.Path)
+	}
+	httpUrl := url.URL{
+		Scheme:   httpReq.Scheme,
+		Host:     httpReq.Host,
+		Path:     httpReq.Path[:httpPathIdx],
+		RawQuery: httpReq.Path[httpPathIdx:],
+		Fragment: httpReq.Fragment,
+	}
+	httpHeaders := map[string]string{}
+	for key, val := range httpReq.Headers {
+		if len(key) == 0 || key[0] == ':' || key == "cookie" {
+			continue
+		}
+		httpHeaders[key] = val
+	}
+	addresses := map[string]interface{}{
+		"http.client_ip":                    reqToClientIp(req),
+		"server.request.method":             httpReq.Method,
+		"server.request.uri.raw":            httpUrl.String(),
+		"server.request.headers.no_cookies": httpHeaders,
+	}
+	return addresses
 }
 
 // Check implements authorization's Check interface which performs authorization check based on the
@@ -45,20 +90,23 @@ func (s *server) Check(ctx context.Context, req *envoy_service_auth_v3.CheckRequ
 	}
 	defer wafCtx.Close()
 
-	// TODO: create all the sec addresses out of CheckRequest
-	httpReq := req.Attributes.Request.Http
-	addresses := map[string]interface{}{
-		"server.request.headers.no_cookies": httpReq.Headers,
-	}
-
-	log.Debug("appsec: envoy auth api: running the security rules against %v", addresses)
+	addresses := reqToAddrs(req)
+	log.Debugf("appsec: envoy auth api: running the security rules against %v", addresses)
 	matches, actions, err := wafCtx.Run(addresses, defaultWAFTimeout)
 	if err != nil && err != waf.ErrTimeout {
 		log.Errorf("appsec: unexpected waf execution error: %v", err)
 	}
-	log.Debug("appsec: envoy auth api: matches=%s actions=%v", string(matches), actions)
+	log.Debugf("appsec: envoy auth api: matches=%s actions=%v", string(matches), actions)
 
-	// TODO: create appsec span tags
+	if len(matches) > 0 {
+		meta := &structpb.Struct{Fields: make(map[string]*structpb.Value)}
+		meta.Fields["appsec"] = structpb.NewStringValue(fmt.Sprintf("{\"event\":true,\"triggers\":%s}", matches))
+		meta.Fields["origin"] = structpb.NewStringValue("appsec")
+		okResponse.DynamicMetadata = meta
+		//okResponse.HttpResponse.OkHttpResponse = envoy_service_auth_v3.OkHttpResponse{Headers: []*envoy_config_core_v3.HeaderValueOption{
+		//	{Header: envoy_config_core_v3.HeaderValue{Key: "x-datadog-sampling-priority", Value: "1"}},
+		//}}
+	}
 
 	return okResponse, nil
 }
