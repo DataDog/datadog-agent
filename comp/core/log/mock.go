68df3c652b5ee8000a2ca35ddaf367cbc709aac7
Lucas Liseth <lucas.liseth@datadoghq.com>
2022-11-30 11:01:36 -0500

    Fixed incorrect function parameter

--- comp/core/log/mock.go
+++ comp/core/log/mock.go
@@ -45,7 +45,7 @@ func newMockLogger(t testing.TB, lc fx.Lifecycle) (Component, error) {
 	}})
 
 	// install the logger into pkg/util/log
-	log.SetupLogger(iface, "trace")
+	log.SetupLogger(iface, "trace", nil)
 
 	return &logger{}, nil
 }
