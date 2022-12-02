b41e5b384838e7c57954fa9b77dc42f0a7e10bb8
Andrew Glaude <andrew.glaude@datadoghq.com>
2022-12-02 12:52:54 -0500

    Need to change to original branch to access scripts again

--- .gitlab/benchmarks/scripts/run-benchmarks.sh
+++ .gitlab/benchmarks/scripts/run-benchmarks.sh
@@ -7,3 +7,5 @@ inv trace-agent.benchmarks --output="${ARTIFACTS_DIR}/pr_bench.txt" --bench="Ben
 
 git checkout main
 inv trace-agent.benchmarks --output="${ARTIFACTS_DIR}/main_bench.txt" --bench="BenchmarkAgentTraceProcessing$"
+
+git checkout "${CI_COMMIT_REF_NAME}" # (Only needed while these changes aren't merged to main)
