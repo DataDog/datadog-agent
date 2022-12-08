aec3099393656e2228982f1b86432c07555f6770
Ivan Ilichev <ivan@ivanilichev.com>
2022-12-08 08:24:53 -0500

    Simplify config subcommand per feedback from @ogaca-dd

--- cmd/process-agent/app/subcommands/config/config.go
+++ cmd/process-agent/app/subcommands/config/config.go
@@ -25,71 +25,52 @@ func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 		Short: "Print the runtime configuration of a running agent",
 		Long:  ``,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			c, err := getClient(globalParams)
-			if err != nil {
-				return err
-			}
-
-			return showRuntimeConfiguration(c, cmd, args)
+			return showRuntimeConfiguration(globalParams)
 		},
 	}
 
-	cmd.AddCommand(listRuntime(globalParams))
-	cmd.AddCommand(set(globalParams))
-	cmd.AddCommand(get(globalParams))
+	cmd.AddCommand(
+		&cobra.Command{
+			Use:   "list-runtime",
+			Short: "List settings that can be changed at runtime",
+			Long:  ``,
+			RunE: func(cmd *cobra.Command, args []string) error {
+				return listRuntimeConfigurableValue(globalParams)
+			},
+		},
+	)
+
+	cmd.AddCommand(
+		&cobra.Command{
+			Use:   "set [setting] [value]",
+			Short: "Set, for the current runtime, the value of a given configuration setting",
+			Long:  ``,
+			RunE: func(cmd *cobra.Command, args []string) error {
+				return setConfigValue(globalParams, args)
+			},
+		},
+	)
+	cmd.AddCommand(
+		&cobra.Command{
+			Use:   "get [setting]",
+			Short: "Get, for the current runtime, the value of a given configuration setting",
+			Long:  ``,
+			RunE: func(cmd *cobra.Command, args []string) error {
+				return getConfigValue(globalParams, args)
+			},
+		},
+	)
+
 	return []*cobra.Command{cmd}
 }
 
-// listRuntime returns a cobra command to list the settings that can be changed at runtime.
-func listRuntime(globalParams *command.GlobalParams) *cobra.Command {
-	return &cobra.Command{
-		Use:   "list-runtime",
-		Short: "List settings that can be changed at runtime",
-		Long:  ``,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			c, err := getClient(globalParams)
-			if err != nil {
-				return err
-			}
-			return listRuntimeConfigurableValue(c, cmd, args)
-		},
+func showRuntimeConfiguration(globalParams *command.GlobalParams) error {
+	c, err := getClient(globalParams)
+	if err != nil {
+		return err
 	}
-}
 
-// set returns a cobra command to set a config value at runtime.
-func set(globalParams *command.GlobalParams) *cobra.Command {
-	return &cobra.Command{
-		Use:   "set [setting] [value]",
-		Short: "Set, for the current runtime, the value of a given configuration setting",
-		Long:  ``,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			c, err := getClient(globalParams)
-			if err != nil {
-				return err
-			}
-			return setConfigValue(c, cmd, args)
-		},
-	}
-}
-
-// get returns a cobra command to get a runtime config value.
-func get(globalParams *command.GlobalParams) *cobra.Command {
-	return &cobra.Command{
-		Use:   "get [setting]",
-		Short: "Get, for the current runtime, the value of a given configuration setting",
-		Long:  ``,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			c, err := getClient(globalParams)
-			if err != nil {
-				return err
-			}
-			return getConfigValue(c, cmd, args)
-		},
-	}
-}
-
-func showRuntimeConfiguration(client settings.Client, cmd *cobra.Command, args []string) error {
-	runtimeConfig, err := client.FullConfig()
+	runtimeConfig, err := c.FullConfig()
 	if err != nil {
 		return err
 	}
@@ -99,7 +80,12 @@ func showRuntimeConfiguration(client settings.Client, cmd *cobra.Command, args [
 	return nil
 }
 
-func listRuntimeConfigurableValue(c settings.Client, cmd *cobra.Command, args []string) error {
+func listRuntimeConfigurableValue(globalParams *command.GlobalParams) error {
+	c, err := getClient(globalParams)
+	if err != nil {
+		return err
+	}
+
 	settingsList, err := c.List()
 	if err != nil {
 		return err
@@ -115,7 +101,12 @@ func listRuntimeConfigurableValue(c settings.Client, cmd *cobra.Command, args []
 	return nil
 }
 
-func setConfigValue(c settings.Client, cmd *cobra.Command, args []string) error {
+func setConfigValue(globalParams *command.GlobalParams, args []string) error {
+	c, err := getClient(globalParams)
+	if err != nil {
+		return err
+	}
+
 	if len(args) != 2 {
 		return fmt.Errorf("exactly two parameters are required: the setting name and its value")
 	}
@@ -134,7 +125,12 @@ func setConfigValue(c settings.Client, cmd *cobra.Command, args []string) error 
 	return nil
 }
 
-func getConfigValue(c settings.Client, cmd *cobra.Command, args []string) error {
+func getConfigValue(globalParams *command.GlobalParams, args []string) error {
+	c, err := getClient(globalParams)
+	if err != nil {
+		return err
+	}
+
 	if len(args) != 1 {
 		return fmt.Errorf("a single setting name must be specified")
 	}
