bd23bc9f4fbc7e67b3f446572e56104eb023f4e0
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2022-12-07 20:15:56 -0800

    Check for bpftool compatability outside of generate_minimized_btfs

--- tasks/system_probe.py
+++ tasks/system_probe.py
@@ -1167,7 +1167,15 @@ def kitchen_prepare_btfs(ctx, files_dir, arch=CURRENT_ARCH):
         ctx.run(f"mkdir {btf_dir}/kitchen-btfs-{arch} && " +
                 f"tar xf {btf_dir}/kitchen-btfs-{arch}.tar.xz -C {btf_dir}/kitchen-btfs-{arch}")
 
+    can_minimize = True
     try:
+        check_for_bpftool_compatability(ctx)
+    except:
+        print("Cannot minimize BTFs: preparing kitchen environment with full sized BTFs instead. " +
+              "In order to minimize BTFs, you will need to have bpftool version 6 or higher.")
+        can_minimize = False
+
+    if can_minimize:
         co_re_programs = glob.glob("/opt/datadog-agent/embedded/share/system-probe/ebpf/co-re/*.o")
         generate_minimized_btfs(
             ctx,
@@ -1179,10 +1187,7 @@ def kitchen_prepare_btfs(ctx, files_dir, arch=CURRENT_ARCH):
         ctx.run(f"cd {btf_dir}/minimized-btfs && " +
                 f"tar -cJf minimized-btfs.tar.xz * && " +
                 f"mv minimized-btfs.tar.xz {files_dir}")
-    except:
-        print("Cannot minimize BTFs: preparing kitchen environment with full sized BTFs instead. " +
-              "In order to minimize BTFs, you will need to have bpftool version 6 or higher.")
-
+    else:
         ctx.run(f"cp {btf_dir}/kitchen-btfs-{arch}.tar.xz {files_dir}/minimized-btfs.tar.xz")
 
 
@@ -1204,8 +1209,6 @@ def generate_minimized_btfs(
     if input_bpf_programs == "":
         ctx.run(f"mkdir -p {output_dir}/dummy_data")
         return
-
-    check_for_bpftool_compatability(ctx)
 
     ctx.run(f"mkdir -p {output_dir}")
     for root, dirs, files in os.walk(source_dir):
