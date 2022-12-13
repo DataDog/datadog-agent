0c5b0c9a8f222c498f0bcc5f2b56ef8742986f71
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2022-12-13 15:42:13 +0000

    Move all memory tracking to the same file

--- rtloader/three/three_mem.cpp
+++ rtloader/three/three_mem.cpp
@@ -4,6 +4,83 @@
 // (https://www.datadoghq.com/).
 // Copyright 2019-present Datadog, Inc.
 
+// Tracking allocations in Python interpreter.
+//
+// See https://docs.python.org/3/c-api/memory.html#customize-memory-allocators
+//
+// Python allocates memory using two mechanisms: pymalloc for small,
+// short lived allocations, which constitute the bulk of memory usage,
+// and RAW allocator for larger chunks of memory. We don't track two
+// types of allocations separately, as the distinction exists largely
+// inside Python implementation, out of reach for both users and
+// module authors.
+
+#include "three.h"
+
+#if __linux__ || _WIN32
+#    include <malloc.h>
+#elif __APPLE__ || __FreeBSD__
+#    include <malloc/malloc.h>
+#endif
+
+void Three::initPymemStats()
+{
+    PyObject_GetArenaAllocator(&_pymallocPrev);
+    PyObjectArenaAllocator alloc{
+        .ctx = static_cast<void *>(this),
+        .alloc = Three::pymallocAllocCb,
+        .free = Three::pymallocFreeCb,
+    };
+    PyObject_SetArenaAllocator(&alloc);
+
+    PyMemAllocatorEx alloc_raw{
+        .ctx = static_cast<void *>(this),
+        .malloc = Three::pyrawMallocCb,
+        .calloc = Three::pyrawCallocCb,
+        .realloc = Three::pyrawReallocCb,
+        .free = Three::pyrawFreeCb,
+    };
+    PyMem_SetAllocator(PYMEM_DOMAIN_RAW, &alloc_raw);
+}
+
+void Three::getPymemStats(pymem_stats_t &s)
+{
+    s.inuse = _pymemInuse;
+    s.alloc = _pymemAlloc;
+}
+
+// Tracking allocations by Pymalloc. Pymalloc is the optimized
+// allocator used for small-sized allocations in OBJ and MEM
+// domains. These functions track amount of memory requested by the
+// allocator from the OS, not how much is actually used by currently
+// reachable python objects (IOW, pymalloc keeps some unused memory
+// around internally to speed up allocations).
+void *Three::pymallocAlloc(size_t size)
+{
+    void *ptr = _pymallocPrev.alloc(_pymallocPrev.ctx, size);
+    if (ptr != NULL) {
+        _pymemInuse += size;
+        _pymemAlloc += size;
+    }
+    return ptr;
+}
+
+void Three::pymallocFree(void *ptr, size_t size)
+{
+    _pymallocPrev.free(_pymallocPrev.ctx, ptr, size);
+    _pymemInuse -= size;
+}
+
+void *Three::pymallocAllocCb(void *ctx, size_t size)
+{
+    return static_cast<Three *>(ctx)->pymallocAlloc(size);
+}
+
+void Three::pymallocFreeCb(void *ctx, void *ptr, size_t size)
+{
+    static_cast<Three *>(ctx)->pymallocFree(ptr, size);
+}
+
 // Tracking allocations in RAW python domain. This avoids the need to
 // track individual pointers by using non-standard functions present
 // on all supported platforms that return allocation size for a given
@@ -13,16 +90,6 @@
 // top of the built-in python allocator, to be sure that our pointers
 // come from malloc and not some other kind of allocator, and are
 // compatible with malloc_usable_size.
-
-// See https://docs.python.org/3/c-api/memory.html#customize-memory-allocators
-
-#include "three.h"
-
-#if __linux__ || _WIN32
-#    include <malloc.h>
-#elif __APPLE__ || __FreeBSD__
-#    include <malloc/malloc.h>
-#endif
 
 static size_t pyrawAllocSize(void *ptr)
 {
