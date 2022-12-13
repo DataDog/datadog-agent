19b7ba0d4133ccf71fd41033d668648be9047fca
Adam Karpowich <adam.karpowich@datadoghq.com>
2022-12-13 17:36:16 -0500

    [NET-2891] fix probe definitions

--- pkg/network/ebpf/probes/probes.go
+++ pkg/network/ebpf/probes/probes.go
@@ -115,11 +115,11 @@ const (
 
 	// TCPRetransmit traces the params for the tcp_retransmit_skb() system call
 	TCPRetransmit ProbeName = "kprobe/tcp_retransmit_skb"
+	// TCPRetransmitPre470 traces the return value for the tcp_retransmit_skb() system call on kernel version < 4.7
+	TCPRetransmitPre470 ProbeName = "kprobe/tcp_retransmit_skb/pre_4_7_0"
 
 	// TCPRetransmitRet traces the return value for the tcp_retransmit_skb() system call
 	TCPRetransmitRet ProbeName = "kretprobe/tcp_retransmit_skb"
-	// TCPRetransmitPre470Ret traces the return value for the tcp_retransmit_skb() system call on kernel version < 4.7
-	TCPRetransmitPre470Ret ProbeName = "kretprobe/tcp_retransmit_skb/pre_4_7_0"
 
 	// InetCskAcceptReturn traces the return value for the inet_csk_accept syscall
 	InetCskAcceptReturn ProbeName = "kretprobe/inet_csk_accept"
