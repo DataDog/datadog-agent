995c86d7d9686b0acf49503a2eb42473003a86c2
amit.slavin <amit.slavin@datadoghq.com>
2022-12-15 17:25:11 +0200

    fixed event common protocol type number

--- pkg/network/event_common.go
+++ pkg/network/event_common.go
@@ -26,7 +26,7 @@ const (
 	ProtocolHTTP
 	ProtocolHTTP2
 	ProtocolTLS
-	ProtocolAMQP
+	ProtocolAMQP = 8
 	MaxProtocols
 )
 
@@ -286,8 +286,9 @@ func (c ConnectionStats) IsExpired(now uint64, timeout uint64) bool {
 // ByteKey returns a unique key for this connection represented as a byte slice
 // It's as following:
 //
-//     4B      2B      2B     .5B     .5B      4/16B        4/16B   = 17/41B
-//    32b     16b     16b      4b      4b     32/128b      32/128b
+//	 4B      2B      2B     .5B     .5B      4/16B        4/16B   = 17/41B
+//	32b     16b     16b      4b      4b     32/128b      32/128b
+//
 // |  PID  | SPORT | DPORT | Family | Type |  SrcAddr  |  DestAddr
 func (c ConnectionStats) ByteKey(buf []byte) []byte {
 	return generateConnectionKey(c, buf, false)
