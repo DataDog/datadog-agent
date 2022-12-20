40f593f6f3c25e4eac38f12e31c5a0bb4055df25
modernplumbing <modernplumbing9@gmail.com>
2022-12-20 12:13:05 -0500

    moving root command
    subcommand

--- cmd/security-agent/command/command.go
+++ cmd/security-agent/command/command.go
@@ -6,7 +6,13 @@
 package command
 
 import (
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"path"
+
+	"github.com/DataDog/datadog-agent/cmd/agent/common"
+	"github.com/DataDog/datadog-agent/cmd/security-agent/flags"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 type GlobalParams struct {
@@ -16,3 +22,41 @@ type GlobalParams struct {
 type SubcommandFactory func(globalParams *GlobalParams) []*cobra.Command
 
 const LoggerName = "SECURITY"
+
+var defaultSecurityAgentConfigFilePaths = []string{
+	path.Join(common.DefaultConfPath, "datadog.yaml"),
+	path.Join(common.DefaultConfPath, "security-agent.yaml"),
+}
+
+// MakeCommand makes the top-level Cobra command for this command.
+func MakeCommand(subcommandFactories []SubcommandFactory) *cobra.Command {
+	var globalParams GlobalParams
+	var flagNoColor bool
+
+	SecurityAgentCmd := &cobra.Command{
+		Use:   "datadog-security-agent [command]",
+		Short: "Datadog Security Agent at your service.",
+		Long: `
+Datadog Security Agent takes care of running compliance and security checks.`,
+		SilenceUsage: true, // don't print usage on errors
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if flagNoColor {
+				color.NoColor = true
+			}
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			log.Flush()
+		},
+	}
+
+	SecurityAgentCmd.PersistentFlags().StringArrayVarP(&globalParams.ConfigFilePaths, flags.CfgPath, "c", defaultSecurityAgentConfigFilePaths, "paths to yaml configuration files")
+	SecurityAgentCmd.PersistentFlags().BoolVarP(&flagNoColor, flags.NoColor, "n", false, "disable color output")
+
+	for _, factory := range subcommandFactories {
+		for _, subcmd := range factory(&globalParams) {
+			SecurityAgentCmd.AddCommand(subcmd)
+		}
+	}
+
+	return SecurityAgentCmd
+}
