9472f21e391d731799e2a84a31a72ec8be0fa2c0
Vincent Boulineau <vincent.boulineau@datadoghq.com>
2022-12-21 17:31:13 +0100

    Revert "Replace timestamp by increasing id to avoid configVersion matching different config changed in the same second"
    This reverts commit f8e097de2aa3322670fcc6a6c8cfc5c1ed9d6239.

--- pkg/clusteragent/clusterchecks/dispatcher_nodes.go
+++ pkg/clusteragent/clusterchecks/dispatcher_nodes.go
@@ -33,7 +33,7 @@ func (d *dispatcher) getClusterCheckConfigs(nodeName string) ([]integration.Conf
 
 	node.RLock()
 	defer node.RUnlock()
-	return makeConfigArray(node.digestToConfig), node.configVersion.Load(), nil
+	return makeConfigArray(node.digestToConfig), node.lastConfigChange, nil
 }
 
 // processNodeStatus keeps the node's status in the store, and returns true
@@ -53,7 +53,7 @@ func (d *dispatcher) processNodeStatus(nodeName, clientIP string, status types.N
 	node.lastStatus = status
 	node.heartbeat = timestampNow()
 
-	if node.configVersion.Load() == status.LastChange {
+	if node.lastConfigChange == status.LastChange {
 		// Node-agent is up to date
 		return true, nil
 	}
