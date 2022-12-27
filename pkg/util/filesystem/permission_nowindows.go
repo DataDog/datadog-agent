d215901981054a9d922cc3f2773919ac1a9b8e0c
Maxime mouial <maxime.mouial@gmail.com>
2022-12-27 16:39:11 +0100

    Restrict flare file from being accessible by other users on Unix

--- pkg/util/filesystem/permission_nowindows.go
+++ pkg/util/filesystem/permission_nowindows.go
@@ -2,6 +2,7 @@
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
+
 //go:build !windows
 // +build !windows
 
@@ -26,7 +27,8 @@ func NewPermission() (*Permission, error) {
 	return &Permission{}, nil
 }
 
-// RestrictAccessToUser restricts the access to a file to the current user and its group
+// RestrictAccessToUser sets the file user and group to the same as 'dd-agent' user. If the function fails to lookup
+// "dd-agent" user it return nil immediately.
 func (p *Permission) RestrictAccessToUser(path string) error {
 	usr, err := user.Lookup("dd-agent")
 	if err != nil {
@@ -53,4 +55,19 @@ func (p *Permission) RestrictAccessToUser(path string) error {
 	}
 
 	return nil
+}
+
+// RemoveAccessToOtherUsers on Unix this calls RestrictAccessToUser and then removes all access to the file for 'group'
+// and 'other'
+func (p *Permission) RemoveAccessToOtherUsers(path string) error {
+	// We first try to set other and group to "dd-agent" when possible
+	_ = p.RestrictAccessToUser(path)
+
+	fperm, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	// We keep the original 'user' rights but set 'group' and 'use' to zero.
+	newPerm := fperm.Mode().Perm() & 0700
+	return os.Chmod(path, fs.FileMode(newPerm))
 }
