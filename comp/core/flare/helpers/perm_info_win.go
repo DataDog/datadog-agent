613131d3ddc67541959bdb8ec6e9745605d822ce
Len Gamburg <len.gamburg@datadoghq.com>
2022-12-27 15:18:30 -0500

    Add to permissions.log windows permissions

--- comp/core/flare/helpers/perm_info_win.go
+++ comp/core/flare/helpers/perm_info_win.go
@@ -8,13 +8,116 @@
 
 package helpers
 
-// filePermsInfo represents file rights on windows.
-type filePermsInfo struct{}
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
-func (p permissionsInfos) add(filePath string) error {
-	return nil
+	"golang.org/x/sys/windows"
+)
+
+// filePermsInfo represents file rights on windows.
+type filePermsInfo struct {
+	path   string
+	mode   string
+	icacls string
+	getacl string
+	err    error
 }
 
+func run_cmd(cmd *exec.Cmd) string {
+	stdout := bytes.Buffer{}
+	stderr := bytes.Buffer{}
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Sprintf("Error calling '%s': %s", cmd.String(), err)
+	}
+
+	output := fmt.Sprintf("Cmd: %s\n%s", cmd.String(), stdout.String())
+	if stderr.Len() != 0 {
+		output += fmt.Sprintf("\n%s", stderr.String())
+	}
+
+	return output
+}
+
+func (p permissionsInfos) add(filePath string) {
+	info := filePermsInfo{
+		path: filePath,
+	}
+	p[filePath] = &info
+
+	// Get basic information
+	fi, err := os.Stat(filePath)
+	if err != nil {
+		info.err = fmt.Errorf("could not find file %s: %s", filePath, err)
+		return
+	}
+	info.mode = fi.Mode().String()
+
+	// Run icacls.exe
+	sysDir, err := windows.GetSystemDirectory()
+	if err != nil {
+		info.icacls = fmt.Sprintf("Error: cannot locate icacls.exe %s", err)
+	} else {
+		execPath := fmt.Sprintf("%s\\icacls.exe", sysDir)
+		// Direct path argument will be quoted by the exec.Command https://pkg.go.dev/os/exec#Command
+		info.icacls = run_cmd(exec.Command(execPath, filePath))
+	}
+
+	// Run Powershell get-acl (to add complimentary information like owner)
+	ps, err := exec.LookPath("powershell.exe")
+	if err != nil {
+		info.getacl = fmt.Sprintf("Error: cannot locate powershell.exe %s", err)
+	} else {
+		// Indirect path argument seems to be not quoted
+		filePathQuoted := fmt.Sprintf("\"%s\"", filePath)
+		info.getacl = run_cmd(exec.Command(ps, "get-acl", "-Path", filePathQuoted, "|", "fl"))
+	}
+}
+
+// Commit resolves the infos of every stacked files in the map
+// and then writes the permissions.log file on the filesystem.
 func (p permissionsInfos) commit() ([]byte, error) {
-	return nil, nil
+	f := &bytes.Buffer{}
+
+	sep := strings.Repeat("-", 48) + "\n"
+
+	// write each file permissions infos
+	for _, info := range p {
+		if _, err := f.Write([]byte(sep)); err != nil {
+			return nil, err
+		}
+
+		// Print error and try next item
+		if info.err != nil {
+			_, err := f.Write([]byte(
+				fmt.Sprintf("File: %s\n\nError:%s\n\n",
+					info.path,
+					info.err.Error(),
+				)))
+			if err != nil {
+				return f.Bytes(), err
+			}
+		} else {
+			// ... or actual permissions
+			_, err := f.Write([]byte(
+				fmt.Sprintf("File: %s\n\nMode:%s\n\n%s\n%s\n",
+					info.path,
+					info.mode,
+					info.icacls,
+					info.getacl,
+				)))
+			if err != nil {
+				return f.Bytes(), err
+			}
+		}
+	}
+
+	return f.Bytes(), nil
 }
--- comp/core/flare/helpers/perm_info_win_test.go
+++ comp/core/flare/helpers/perm_info_win_test.go
@@ -9,18 +9,85 @@
 package helpers
 
 import (
+	"os"
+	"path/filepath"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func createPermsTestFile(t *testing.T) (string, string, string) {
+	tempDir := t.TempDir()
+
+	f1 := filepath.Join(tempDir, "file 1")
+	f2 := filepath.Join(tempDir, "file2")
+	f3 := filepath.Join(tempDir, "file3")
+
+	os.WriteFile(f1, nil, 0666)
+	os.WriteFile(f2, nil, 0444)
+	return f1, f2, f3
+}
 
 // Test that addParentPerms does not loop on Windows.
 func TestPermissionsInfosAddWindows(t *testing.T) {
 	permsInfos := make(permissionsInfos)
-	expectedParentPerms := map[string]filePermsInfo{}
 
 	// Basic Case
 	path := "C:\\a\\b\\c\\d"
-	permsInfos.Add(path)
-	assert.NotContains(t, permsInfos, path)
+	permsInfos.add(path)
+	assert.Contains(t, permsInfos, path)
+	assert.True(t, len(permsInfos) == 1)
+}
+
+func TestPermsFileAdd(t *testing.T) {
+	f1, f2, f3 := createPermsTestFile(t)
+
+	pi := permissionsInfos{}
+
+	pi.add(f1)
+	pi.add(f2)
+	pi.add(f3)
+
+	require.Len(t, pi, 3)
+
+	assert.Equal(t, f1, pi[f1].path)
+	assert.Equal(t, "-rw-rw-rw-", pi[f1].mode)
+
+	assert.Equal(t, f2, pi[f2].path)
+	assert.Equal(t, "-r--r--r--", pi[f2].mode)
+
+	assert.Equal(t, f3, pi[f3].path)
+	assert.Empty(t, pi[f3].mode)
+	assert.NotNil(t, pi[f3].err)
+}
+
+func TestPermsFileCommit(t *testing.T) {
+	f1, f2, f3 := createPermsTestFile(t)
+
+	pi := permissionsInfos{}
+	pi.add(f1)
+	pi.add(f2)
+	pi.add(f3)
+
+	// No error
+	b, err := pi.commit()
+	require.NoError(t, err)
+
+	// All files are reportes
+	res, _ := regexp.Match("\nFile:.+file 1\n", b)
+	assert.True(t, res)
+	res, _ = regexp.Match("\nFile:.+file2\n", b)
+	assert.True(t, res)
+	res, _ = regexp.Match("\nFile:.+file3\n", b)
+	assert.True(t, res)
+
+	// Success and failiures are recorded
+	res, _ = regexp.Match("\nSuccessfully processed 1 files", b)
+	assert.True(t, res)
+	res, _ = regexp.Match("\nOwner  :", b)
+	assert.True(t, res)
+	res, _ = regexp.Match("\nError:could not find file.+file3", b)
+	assert.True(t, res)
 }
