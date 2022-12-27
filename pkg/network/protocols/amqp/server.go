60c8b264d0b44588275ff227b34be50815157365
Guy Arbitman <guy20495@gmail.com>
2022-12-27 21:23:00 +0200

    Added pattern scanner

--- pkg/network/protocols/amqp/server.go
+++ pkg/network/protocols/amqp/server.go
@@ -7,37 +7,21 @@ package amqp
 
 import (
 	"fmt"
-	"github.com/DataDog/datadog-agent/pkg/network/protocols/http/testutil"
-	"github.com/stretchr/testify/require"
 	"os/exec"
-	"strings"
+	"regexp"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/DataDog/datadog-agent/pkg/network/protocols/http/testutil"
+	protocolsUtils "github.com/DataDog/datadog-agent/pkg/network/protocols/testutil"
 )
 
 const (
 	User = "guest"
 	Pass = "guest"
 )
-
-type logWriter struct {
-	port         string
-	callbackChan chan struct{}
-	ignore       bool
-}
-
-func (l *logWriter) Write(p []byte) (n int, err error) {
-	if l.ignore {
-		return len(p), nil
-	}
-	e := fmt.Sprintf("started TCP listener on [::]:%s", l.port)
-	if strings.Contains(string(p), e) {
-		l.callbackChan <- struct{}{}
-		l.ignore = true
-	}
-
-	return len(p), nil
-}
 
 func RunAmqpServer(t *testing.T, serverAddr, serverPort string) {
 	t.Helper()
@@ -50,28 +34,23 @@ func RunAmqpServer(t *testing.T, serverAddr, serverPort string) {
 	}
 	dir, _ := testutil.CurDir()
 	cmd := exec.Command("docker-compose", "-f", dir+"/testdata/docker-compose.yml", "up")
-	writer := &logWriter{
-		port:         serverPort,
-		callbackChan: make(chan struct{}, 1),
-		ignore:       false,
-	}
-	cmd.Stdout = writer
+	patternScanner := protocolsUtils.NewScanner(regexp.MustCompile(fmt.Sprintf(".*started TCP listener on .*%s.*", serverPort)), make(chan struct{}, 1))
+
+	cmd.Stdout = patternScanner
 	cmd.Env = append(cmd.Env, env...)
 	go func() {
 		require.NoErrorf(t, cmd.Run(), "could not start amqp with docker-compose")
 	}()
 
-	cleanup := func() {
+	t.Cleanup(func() {
 		c := exec.Command("docker-compose", "-f", dir+"/testdata/docker-compose.yml", "down", "--remove-orphans")
 		c.Env = append(c.Env, env...)
 		_ = c.Run()
-	}
-
-	t.Cleanup(cleanup)
+	})
 
 	for {
 		select {
-		case <-writer.callbackChan:
+		case <-patternScanner.DoneChan:
 			fmt.Println("amqp server is ready")
 			return
 		case <-time.After(time.Second * 30):
---
+++ pkg/network/protocols/testutil/patternscanner.go
@@ -0,0 +1,47 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package testutil
+
+import (
+	"regexp"
+	"sync"
+)
+
+type PatternScanner struct {
+	// The log pattern to match on
+	pattern *regexp.Regexp
+	// Once we've found the correct log, we should notify the caller.
+	DoneChan chan struct{}
+	// A sync.Once instance to ensure we notify the caller only once, and stop the operation.
+	stopOnce sync.Once
+	// A helper to spare redundant calls to the analyzer once we've found the relevant log.
+	stopped bool
+}
+
+func NewScanner(pattern *regexp.Regexp, doneChan chan struct{}) *PatternScanner {
+	return &PatternScanner{
+		pattern:  pattern,
+		DoneChan: doneChan,
+		stopOnce: sync.Once{},
+		stopped:  false,
+	}
+}
+
+// Write implemented io.Writer to be used as a callback for log/string writing.
+// Once we find a match in for the given pattern, we notify the caller.
+func (ps *PatternScanner) Write(p []byte) (n int, err error) {
+	n = len(p)
+	err = nil
+
+	if !ps.stopped && ps.pattern.Match(p) {
+		ps.stopOnce.Do(func() {
+			ps.DoneChan <- struct{}{}
+			ps.stopped = true
+		})
+	}
+
+	return
+}
