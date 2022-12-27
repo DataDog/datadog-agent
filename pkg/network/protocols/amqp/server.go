8c3652516606971ba32900483ee24ded1cfd752e
Guy Arbitman <guy20495@gmail.com>
2022-12-27 21:08:36 +0200

    Added debug log 3

--- pkg/network/protocols/amqp/server.go
+++ pkg/network/protocols/amqp/server.go
@@ -6,19 +6,38 @@
 package amqp
 
 import (
-	"os"
-	"os/exec"
-	"testing"
-	"time"
-
+	"fmt"
 	"github.com/DataDog/datadog-agent/pkg/network/protocols/http/testutil"
 	"github.com/stretchr/testify/require"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
 )
 
 const (
 	User = "guest"
 	Pass = "guest"
 )
+
+type logWriter struct {
+	port         string
+	callbackChan chan struct{}
+	ignore       bool
+}
+
+func (l *logWriter) Write(p []byte) (n int, err error) {
+	if l.ignore {
+		return len(p), nil
+	}
+	e := fmt.Sprintf("started TCP listener on [::]:%s", l.port)
+	if strings.Contains(string(p), e) {
+		l.callbackChan <- struct{}{}
+		l.ignore = true
+	}
+
+	return len(p), nil
+}
 
 func RunAmqpServer(t *testing.T, serverAddr, serverPort string) {
 	t.Helper()
@@ -31,17 +50,32 @@ func RunAmqpServer(t *testing.T, serverAddr, serverPort string) {
 	}
 	dir, _ := testutil.CurDir()
 	cmd := exec.Command("docker-compose", "-f", dir+"/testdata/docker-compose.yml", "up")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stdout
+	writer := &logWriter{
+		port:         serverPort,
+		callbackChan: make(chan struct{}, 1),
+		ignore:       false,
+	}
+	cmd.Stdout = writer
 	cmd.Env = append(cmd.Env, env...)
 	go func() {
 		require.NoErrorf(t, cmd.Run(), "could not start amqp with docker-compose")
 	}()
 
-	time.Sleep(5 * time.Second)
-	t.Cleanup(func() {
+	cleanup := func() {
 		c := exec.Command("docker-compose", "-f", dir+"/testdata/docker-compose.yml", "down", "--remove-orphans")
 		c.Env = append(c.Env, env...)
 		_ = c.Run()
-	})
+	}
+
+	t.Cleanup(cleanup)
+
+	for {
+		select {
+		case <-writer.callbackChan:
+			fmt.Println("amqp server is ready")
+			return
+		case <-time.After(time.Second * 30):
+			t.Fatal("failed to start amqp server")
+		}
+	}
 }
