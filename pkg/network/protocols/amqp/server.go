2d8ef0b5d2874c9a9e0e211854719d658c5dbcef
Guy Arbitman <guy20495@gmail.com>
2022-12-28 11:36:47 +0200

    usm: protocols: Refactored server creation (#14869)

--- pkg/network/protocols/amqp/server.go
+++ pkg/network/protocols/amqp/server.go
@@ -7,12 +7,8 @@ package amqp
 
 import (
 	"fmt"
-	"os/exec"
 	"regexp"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/require"
 
 	"github.com/DataDog/datadog-agent/pkg/network/protocols/http/testutil"
 	protocolsUtils "github.com/DataDog/datadog-agent/pkg/network/protocols/testutil"
@@ -24,37 +20,14 @@ const (
 )
 
 func RunAmqpServer(t *testing.T, serverAddr, serverPort string) {
-	t.Helper()
-
 	env := []string{
 		"AMQP_ADDR=" + serverAddr,
 		"AMQP_PORT=" + serverPort,
 		"USER=" + User,
 		"PASS=" + Pass,
 	}
+
+	t.Helper()
 	dir, _ := testutil.CurDir()
-	cmd := exec.Command("docker-compose", "-f", dir+"/testdata/docker-compose.yml", "up")
-	patternScanner := protocolsUtils.NewScanner(regexp.MustCompile(fmt.Sprintf(".*started TCP listener on .*%s.*", serverPort)), make(chan struct{}, 1))
-
-	cmd.Stdout = patternScanner
-	cmd.Env = append(cmd.Env, env...)
-	go func() {
-		require.NoErrorf(t, cmd.Run(), "could not start amqp with docker-compose")
-	}()
-
-	t.Cleanup(func() {
-		c := exec.Command("docker-compose", "-f", dir+"/testdata/docker-compose.yml", "down", "--remove-orphans")
-		c.Env = append(c.Env, env...)
-		_ = c.Run()
-	})
-
-	for {
-		select {
-		case <-patternScanner.DoneChan:
-			fmt.Println("amqp server is ready")
-			return
-		case <-time.After(time.Second * 30):
-			t.Fatal("failed to start amqp server")
-		}
-	}
+	protocolsUtils.RunDockerServer(t, "amqp", dir+"/testdata/docker-compose.yml", env, regexp.MustCompile(fmt.Sprintf(".*started TCP listener on .*%s.*", serverPort)))
 }
