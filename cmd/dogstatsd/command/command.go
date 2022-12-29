3881bb2620caee9c04593a18f9d850b3d009b384
Olivier G <olivier.gaca@datadoghq.com>
2022-12-29 10:21:56 +0100

    Fix path for go generate

--- cmd/dogstatsd/command/command.go
+++ cmd/dogstatsd/command/command.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:generate go run ../../pkg/config/render_config.go dogstatsd ../../pkg/config/config_template.yaml ./dist/dogstatsd.yaml
+//go:generate go run ../../../pkg/config/render_config.go dogstatsd ../../../pkg/config/config_template.yaml ./dist/dogstatsd.yaml
 
 package command
 
