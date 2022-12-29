dbeae77874f419604dbaeed035aaa97ad56e5de8
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2022-12-29 22:01:22 +0100

    Fix bad certificate on gmplib.org

--- omnibus/config/software/gmp.rb
+++ omnibus/config/software/gmp.rb
@@ -25,7 +25,7 @@ skip_transitive_dependency_licensing true
 
 version("6.2.1") { source sha256: "fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2" }
 
-source url: "https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz"
+source url: "https://ftp.dimensiondata.com/mirrors/ftp.gnu.org/gmp/gmp-6.2.1.tar.xz"
 
 relative_path "#{name}-#{version}"
 
