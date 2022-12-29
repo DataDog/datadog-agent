e1c07fa52b0847a76f82cd6fd26633ca4b8eebdb
modernplumbing <modernplumbing9@gmail.com>
2022-12-29 13:43:14 -0500

    fixing test post bundle breaking into config and log components

---
+++ comp/core/config/params_test.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"github.com/DataDog/datadog-agent/cmd/agent/common"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewSecurityAgentParams(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		check func(params Params)
+	}{
+		{
+			name:  "no security agent config file paths",
+			input: []string{},
+			check: func(params Params) {
+				require.Equal(t, "", params.confFilePath)
+				require.Equal(t, []string{}, params.securityAgentConfigFilePaths, "Security Agent Config File Paths not matching")
+			},
+		},
+		{
+			name:  "1 security agent config file path",
+			input: []string{"/etc/datadog-agent/security-agent.yaml"},
+			check: func(params Params) {
+				require.Equal(t, "/etc/datadog-agent/security-agent.yaml", params.confFilePath)
+				require.Equal(t, []string{"/etc/datadog-agent/security-agent.yaml"}, params.securityAgentConfigFilePaths, "Security Agent config file paths not matching")
+			},
+		},
+		{
+			name:  "more than 1 security agent config file paths",
+			input: []string{"/etc/datadog-agent/security-agent.yaml", "/etc/datadog-agent/other.yaml"},
+			check: func(params Params) {
+				require.Equal(t, "/etc/datadog-agent/security-agent.yaml", params.confFilePath)
+				require.Equal(t, []string{"/etc/datadog-agent/security-agent.yaml"}, params.securityAgentConfigFilePaths, "Security Agent config file paths not matching")
+			},
+		},
+	}
+
+	for _, test := range tests {
+		configComponentParams := NewSecurityAgentParams(test.input)
+
+		require.Equal(t, true, configComponentParams.configLoadSecurityAgent, "configLoadSecurityAgent values not matching")
+		require.Equal(t, common.DefaultConfPath, configComponentParams.defaultConfPath, "defaultConfPath values not matching")
+		require.Equal(t, true, configComponentParams.configLoadSecrets, "configLoadSecrets values not matching")
+		require.Equal(t, false, configComponentParams.configMissingOK, "configMissingOK values not matching")
+	}
+}
