c2a19659c58f91efc681ce9330f03e6e5be67803
DanielLavie <daniel.lavie@datadoghq.com>
2023-01-02 14:50:09 +0200

    WIP

--- pkg/network/ebpf/c/protocols/protocol-classification-defs.h
+++ pkg/network/ebpf/c/protocols/protocol-classification-defs.h
@@ -12,7 +12,7 @@
 // Represents the max buffer size required to classify protocols .
 // We need to round it to be multiplication of 16 since we are reading blocks of 16 bytes in read_into_buffer_skb_all_kernels.
 // ATM, it is HTTP2_MARKER_SIZE + 8 bytes for padding,
-#define CLASSIFICATION_MAX_BUFFER (HTTP2_MARKER_SIZE + 8)
+#define CLASSIFICATION_MAX_BUFFER (8 * 40) // 320
 
 // The enum below represents all different protocols we know to classify.
 // We set the size of the enum to be 8 bits, by adding max value (max uint8 which is 255) and
@@ -24,6 +24,7 @@ typedef enum {
     PROTOCOL_HTTP,
     PROTOCOL_HTTP2,
     PROTOCOL_TLS,
+    PROTOCOL_KAFKA,
     PROTOCOL_MONGO = 6,
     PROTOCOL_AMQP = 8,
     PROTOCOL_REDIS = 9,
