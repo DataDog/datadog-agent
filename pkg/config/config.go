80f8b45971f6c15becce8dc7b30292f12cb28252
Carlos Roman <carlos.roman@datadoghq.com>
2023-01-03 11:55:49 +0000

    Adding config option to disable delta profiles when profiling the Agent

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -821,6 +821,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("internal_profiling.block_profile_rate", 0)
 	config.BindEnvAndSetDefault("internal_profiling.mutex_profile_fraction", 0)
 	config.BindEnvAndSetDefault("internal_profiling.enable_goroutine_stacktraces", false)
+	config.BindEnvAndSetDefault("internal_profiling.delta_profiles", true)
 
 	config.BindEnvAndSetDefault("internal_profiling.capture_all_allocations", false)
 
