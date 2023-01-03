53745b4d09194c9d01f6df0f45db79edc4d51621
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-01-03 16:29:53 +0100

    [corechecks/snmp] Add IP Addresses to NDM Metadata interfaces (IPv4) (#14823)

--- pkg/collector/corechecks/snmp/internal/report/report_device_metadata_test.go
+++ pkg/collector/corechecks/snmp/internal/report/report_device_metadata_test.go
@@ -406,6 +406,10 @@ func Test_batchPayloads(t *testing.T) {
 	for i := 0; i < 350; i++ {
 		interfaces = append(interfaces, metadata.InterfaceMetadata{DeviceID: deviceID, Index: int32(i)})
 	}
+	var ipAddresses []metadata.IPAddressMetadata
+	for i := 0; i < 100; i++ {
+		ipAddresses = append(ipAddresses, metadata.IPAddressMetadata{InterfaceID: deviceID + ":1", IPAddress: "1.2.3.4", Prefixlen: 24})
+	}
 	var topologyLinks []metadata.TopologyLinkMetadata
 	for i := 0; i < 100; i++ {
 		topologyLinks = append(topologyLinks, metadata.TopologyLinkMetadata{
@@ -413,9 +417,9 @@ func Test_batchPayloads(t *testing.T) {
 			Remote: &metadata.TopologyLinkSide{Interface: &metadata.TopologyLinkInterface{ID: "b"}},
 		})
 	}
-	payloads := batchPayloads("my-ns", "127.0.0.0/30", collectTime, 100, device, interfaces, topologyLinks)
+	payloads := batchPayloads("my-ns", "127.0.0.0/30", collectTime, 100, device, interfaces, ipAddresses, topologyLinks)
 
-	assert.Equal(t, 5, len(payloads))
+	assert.Equal(t, 6, len(payloads))
 
 	assert.Equal(t, "my-ns", payloads[0].Namespace)
 	assert.Equal(t, "127.0.0.0/30", payloads[0].Subnet)
@@ -436,12 +440,18 @@ func Test_batchPayloads(t *testing.T) {
 
 	assert.Equal(t, 0, len(payloads[3].Devices))
 	assert.Equal(t, 51, len(payloads[3].Interfaces))
-	assert.Equal(t, 49, len(payloads[3].Links))
+	assert.Equal(t, 49, len(payloads[3].IPAddresses))
 	assert.Equal(t, interfaces[299:350], payloads[3].Interfaces)
-	assert.Equal(t, topologyLinks[:49], payloads[3].Links)
+	assert.Equal(t, ipAddresses[:49], payloads[3].IPAddresses)
 
 	assert.Equal(t, 0, len(payloads[4].Devices))
-	assert.Equal(t, 0, len(payloads[4].Interfaces))
-	assert.Equal(t, 51, len(payloads[4].Links))
-	assert.Equal(t, topologyLinks[49:100], payloads[4].Links)
+	assert.Equal(t, 51, len(payloads[4].IPAddresses))
+	assert.Equal(t, 49, len(payloads[4].Links))
+	assert.Equal(t, ipAddresses[49:], payloads[4].IPAddresses)
+	assert.Equal(t, topologyLinks[:49], payloads[4].Links)
+
+	assert.Equal(t, 0, len(payloads[5].Devices))
+	assert.Equal(t, 0, len(payloads[5].Interfaces))
+	assert.Equal(t, 51, len(payloads[5].Links))
+	assert.Equal(t, topologyLinks[49:100], payloads[5].Links)
 }
