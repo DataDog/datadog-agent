f13e112869de5d2072a3b41ce8103b05ce01566b
Florian Veaux <florian.veaux@datadoghq.com>
2023-01-06 10:36:27 +0100

    NDM: Add snmp.device.[un]reachable metrics (#14649)
    * NDM: Add snmp.device_up metric
    
    * Address review
    
    * update reno
    
    * Address review
    
    * fix import

--- pkg/collector/corechecks/snmp/internal/devicecheck/devicecheck.go
+++ pkg/collector/corechecks/snmp/internal/devicecheck/devicecheck.go
@@ -32,9 +32,11 @@ import (
 )
 
 const (
-	snmpLoaderTag        = "loader:core"
-	serviceCheckName     = "snmp.can_check"
-	deviceHostnamePrefix = "device:"
+	snmpLoaderTag           = "loader:core"
+	serviceCheckName        = "snmp.can_check"
+	deviceReachableMetric   = "snmp.device.reachable"
+	deviceUnreachableMetric = "snmp.device.unreachable"
+	deviceHostnamePrefix    = "device:"
 )
 
 // define timeNow as variable to make it possible to mock it during test
@@ -114,6 +116,9 @@ func (d *DeviceCheck) Run(collectionTime time.Time) error {
 		tags = append(tags, dynamicTags...)
 		d.sender.ServiceCheck(serviceCheckName, metrics.ServiceCheckOK, tags, "")
 	}
+	d.sender.Gauge(deviceReachableMetric, common.BoolToFloat64(deviceReachable), tags)
+	d.sender.Gauge(deviceUnreachableMetric, common.BoolToFloat64(!deviceReachable), tags)
+
 	if values != nil {
 		d.sender.ReportMetrics(d.config.Metrics, values, tags)
 	}
