87acdb745f5da734926009e51f928be944849e61
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2023-01-06 13:57:26 -0600

    Use bpf_helper_exists to call bpf_get_prandom_u32

--- pkg/network/ebpf/c/cookie.h
+++ pkg/network/ebpf/c/cookie.h
@@ -2,14 +2,17 @@
 #define __COOKIE_H__
 
 #include "ktypes.h"
+#include "bpf_core_read.h"
 
 static __always_inline u32 get_sk_cookie(struct sock *sk) {
-#ifdef COMPILE_CORE
-    return bpf_get_prandom_u32();
-#else
+#if defined(COMPILE_CORE) || defined(COMPILE_RUNTIME)
+    if (bpf_helper_exists(BPF_FUNC_get_prandom_u32)) {
+        return bpf_get_prandom_u32();
+    }
+#endif
+
     u64 t = bpf_ktime_get_ns();
     return (u32)((u64)sk ^ t);
-#endif
 }
 
 #endif // __COOKIE_H__
