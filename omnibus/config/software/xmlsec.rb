a834414cac599dd15643fa5bff58ce9b6134c2ca
Sylvain Baubeau <lebauce@gmail.com>
2023-01-07 00:28:31 +0100

    Make xmlsec depend on libgcrypt and gnutls

--- omnibus/config/software/xmlsec.rb
+++ omnibus/config/software/xmlsec.rb
@@ -25,6 +25,8 @@ dependency "libxml2"
 dependency "libxslt"
 dependency "openssl"
 dependency "libtool"
+dependency "libgcrypt"
+dependency "gnutls"
 
 version("1.2.37") { source sha256: "5f8dfbcb6d1e56bddd0b5ec2e00a3d0ca5342a9f57c24dffde5c796b2be2871c" }
 
