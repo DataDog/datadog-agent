7c74f4277072fbabb9e5157f5f7eb534173b34db
Yoann Ghigoff <yoann.ghigoff@datadoghq.com>
2023-01-09 17:58:17 +0100

    [CWS] restore SECL documentation generation (#14993)

--- pkg/security/secl/model/model.go
+++ pkg/security/secl/model/model.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:generate go run github.com/DataDog/datadog-agent/pkg/security/secl/compiler/generators/accessors -output accessors.go -field-handlers field_handlers.go
+//go:generate go run github.com/DataDog/datadog-agent/pkg/security/secl/compiler/generators/accessors -output accessors.go -field-handlers field_handlers.go -doc ../../../../docs/cloud-workload-security/secl.json
 
 package model
 
