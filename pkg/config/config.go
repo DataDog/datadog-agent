3e5de5b57dd323be9043ed609fa49c4e69a49534
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2023-01-09 13:47:06 +0100

    [CSPM] remove the hostSelector field not used anymore (#14770)
    * [CSPM] remove the hostSelector field not used anymore
    
    In a more global effort to remove the internal compliance DSL after
    our move to rego, this commit removes one field where it is still
    being used.
    
    The hostSelector field has been put in place in order to make sure
    we only run specific rules on hosts that match, in particular for
    k8s nodes. However, the rule were not used anymore since the hosts
    "master" labels are not properly set. We rely other side effects
    (like process and file existence) to avoid running some rules on
    bad nodes.
    
    * [CSPM] remove k8s nodeLabels retrieval from compliance rules execution
    
    Now that hostSelector fields have been removed, fetching the k8s node labels
    is not required anymore and completely useless.
    
    This PR just remove the nodeLabels fetching and all the subsequent
    dependencies.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1111,7 +1111,6 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("compliance_config.run_path", defaultRunPath)
 	config.BindEnv("compliance_config.run_commands_as")
 	bindEnvAndSetLogsConfigKeys(config, "compliance_config.endpoints.")
-	config.BindEnvAndSetDefault("compliance_config.ignore_host_selectors", true)
 	config.BindEnvAndSetDefault("compliance_config.opa.metrics.enabled", false)
 
 	// Datadog security agent (runtime)
