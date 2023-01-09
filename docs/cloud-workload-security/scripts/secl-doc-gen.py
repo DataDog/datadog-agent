88a574b7cec16b4dae4f7782782d361dfab2e8c9
YoannGh <yoann.ghigoff@datadoghq.com>
2023-01-09 17:16:02 +0100

    [CWS] introduce SECLDoc and add secl field examples to documentation

--- docs/cloud-workload-security/scripts/secl-doc-gen.py
+++ docs/cloud-workload-security/scripts/secl-doc-gen.py
@@ -9,10 +9,8 @@ import common
 @dataclass
 class EventTypeProperty:
     name: str
-    datatype: str
     definition: str
-    constants: str
-
+    doc_link: str
 
 @dataclass
 class EventType:
@@ -25,6 +23,22 @@ class EventType:
 
 
 @dataclass
+class Example:
+    expression: str
+    description: str
+
+@dataclass
+class PropertyDocumentation:
+    name: str
+    link: str
+    datatype: str
+    definition: str
+    prefixes: List[str]
+    constants: str
+    constants_link: str
+    examples: List[Example]
+
+@dataclass
 class Constant:
     name: str
     architecture: str
@@ -33,8 +47,10 @@ class Constant:
 @dataclass
 class Constants:
     name: str
+    link: str
     definition: str
     all: List[Constant]
+
 
 
 def build_event_types(top_node):
@@ -44,19 +60,24 @@ def build_event_types(top_node):
             et["name"], et["type"], et["definition"], et["from_agent_version"], et["experimental"], []
         )
         for p in et["properties"]:
-            try:
-                prop = EventTypeProperty(p["name"], p["type"], p["definition"], p["constants"])
-            except KeyError:
-                prop = EventTypeProperty(p["name"], p["type"], p["definition"], "none")
+            prop = EventTypeProperty(p["name"], p["definition"], p["property_doc_link"])
             event_type.properties.append(prop)
         output.append(event_type)
     return output
 
+def build_properties_doc(top_node):
+    output = []
+    for property in top_node["properties_doc"]:
+        property_doc = PropertyDocumentation(property["name"], property["link"], property["type"], property["definition"], property["prefixes"], property["constants"], property["constants_link"], [])
+        for exp in property["examples"]:
+            property_doc.examples.append(Example(exp["expression"], exp["description"]))
+        output.append(property_doc)
+    return output
 
 def build_constants(top_node):
     output = []
     for cs in top_node["constants"]:
-        constants = Constants(cs["name"], cs["description"], [])
+        constants = Constants(cs["name"], cs["link"], cs["description"], [])
         for c in cs["all"]:
             constants.all.append(Constant(c["name"], c["architecture"]))
         output.append(constants)
@@ -75,8 +96,9 @@ if __name__ == "__main__":
     secl_json_file.close()
 
     event_types = build_event_types(json_top_node)
+    properties_doc_list = build_properties_doc(json_top_node)
     constants_list = build_constants(json_top_node)
 
     output_file = open(args.output, "w")
-    print(common.fill_template(args.template, event_types=event_types, constants_list=constants_list), file=output_file)
+    print(common.fill_template(args.template, event_types=event_types, constants_list=constants_list, properties_doc_list=properties_doc_list), file=output_file)
     output_file.close()
