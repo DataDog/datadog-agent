255da921b27629d8b9496d0601615ac57651e36b
Julien Lebot <julien.lebot@datadoghq.com>
2023-01-11 18:33:24 +0100

    Fix stop service

--- test/kitchen/site-cookbooks/dd-agent-install/recipes/_stop_windows_agent.rb
+++ test/kitchen/site-cookbooks/dd-agent-install/recipes/_stop_windows_agent.rb
@@ -6,7 +6,22 @@
 
 powershell_script 'stop-datadog-agent' do
   code <<-EOH
-    sc.exe query "#{node['dd-agent-install']['agent_name']}"
-    sc.exe stop "#{node['dd-agent-install']['agent_name']}"
+    $serviceName = "#{node['dd-agent-install']['agent_name']}"
+    sc.exe query $serviceName;
+    $agentService = Get-Service -Name $serviceName
+    if ($agentService.Status -eq "running")
+    {
+      Get-Service -CN .
+      | Where-Object { $_.status -eq 'running' -and $_.Name -eq "#{node['dd-agent-install']['agent_name']}" }
+      | ForEach-Object
+      {
+        foreach($s in $_.DependentServices | Where-Object { $_.status -eq 'running' })
+        {
+          Write-Host "Stopping " + $s.name;
+          sc.exe stop $s.name
+        }
+      }
+      sc.exe stop $serviceName
+    }
   EOH
 end
