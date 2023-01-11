ee4f3590974c311da6bf484e2055c8f1cd2486fe
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2023-01-10 18:30:37 -0600

    Use bpf_get_prandom_u32 only on runtime compiled and kernel >= 4.9

--- pkg/network/ebpf/c/cookie.h
+++ pkg/network/ebpf/c/cookie.h
@@ -5,7 +5,12 @@
 #include "bpf_core_read.h"
 
 static __always_inline u32 get_sk_cookie(struct sock *sk) {
+#if defined(RUNTIME_COMPILED) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
     return bpf_get_prandom_u32();
+#endif
+
+    u64 t = bpf_ktime_get_ns();
+    return (u32)((u64)sk ^ t);
 }
 
 #endif // __COOKIE_H__
