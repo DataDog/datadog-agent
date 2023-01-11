5d81187377157d12bfa5fd9b322c8a983dd09391
Julien Lebot <julien.lebot@datadoghq.com>
2023-01-11 18:36:36 +0100

    Simplify code

--- test/kitchen/site-cookbooks/dd-agent-install/recipes/_stop_windows_agent.rb
+++ test/kitchen/site-cookbooks/dd-agent-install/recipes/_stop_windows_agent.rb
@@ -8,18 +8,13 @@ powershell_script 'stop-datadog-agent' do
   code <<-EOH
     $serviceName = "#{node['dd-agent-install']['agent_name']}"
     sc.exe query $serviceName;
-    $agentService = Get-Service -Name $serviceName
+    $agentService = Get-Service -Name $serviceName;
     if ($agentService.Status -eq "running")
     {
-      Get-Service -CN .
-      | Where-Object { $_.status -eq 'running' -and $_.Name -eq "#{node['dd-agent-install']['agent_name']}" }
-      | ForEach-Object
+      foreach($dependentService in $agentService.DependentServices | Where-Object { $_.status -eq 'running' })
       {
-        foreach($s in $_.DependentServices | Where-Object { $_.status -eq 'running' })
-        {
-          Write-Host "Stopping " + $s.name;
-          sc.exe stop $s.name
-        }
+        Write-Host "Stopping " + $dependentService.name;
+        sc.exe stop $dependentService.name
       }
       sc.exe stop $serviceName
     }
