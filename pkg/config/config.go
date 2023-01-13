a8451fae658e7ea1f58127f0c1d37941867945f3
Branden Clark <branden.clark@datadoghq.com>
2023-01-13 11:53:34 -0500

    Migrate systray to an fx.App (#14985)
    Deprecate single-dash args and add double-dash args
    
    Move code from cmd/systray to comp/systray
    
    Update UAC manifest to requireAdministrator
    
    Fix log file and add `system_tray.log_file` configuration option.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1237,6 +1237,9 @@ func InitConfig(config Config) {
 	bindVectorOptions(config, Metrics)
 	bindVectorOptions(config, Logs)
 
+	// Datadog Agent Manager System Tray
+	config.BindEnvAndSetDefault("system_tray.log_file", "")
+
 	setupAPM(config)
 	SetupOTLP(config)
 	setupProcesses(config)
