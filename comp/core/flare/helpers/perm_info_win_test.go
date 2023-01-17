5869941cc1e6f8c552b38cc454f04b51cfc5a1f7
Len Gamburg <len.gamburg@datadoghq.com>
2023-01-17 09:00:29 -0500

    Adjust unit tests for switch to native permission API call implementation

--- comp/core/flare/helpers/perm_info_win_test.go
+++ comp/core/flare/helpers/perm_info_win_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func createPermsTestFile(t *testing.T) (string, string, string, string) {
+func createPermsTestFile(t *testing.T) (string, string, string) {
 	tempDir := t.TempDir()
 
 	f1 := filepath.Join(tempDir, "file1")
@@ -28,17 +28,41 @@ func createPermsTestFile(t *testing.T) (string, string, string, string) {
 	os.WriteFile(f1, nil, 0666)
 	os.WriteFile(f2, nil, 0444)
 
-	return tempDir, f1, f2, f3
+	return f1, f2, f3
+}
+
+func TestPermsFileAdd(t *testing.T) {
+	f1, f2, f3 := createPermsTestFile(t)
+
+	pi := permissionsInfos{}
+	pi.add(f1)
+	pi.add(f2)
+	pi.add(f3)
+
+	require.Len(t, pi, 3)
+
+	assert.Equal(t, f1, pi[f1].path)
+	assert.Equal(t, "-rw-rw-rw-", pi[f1].mode)
+	assert.NotNil(t, pi[f1].acls)
+	assert.LessOrEqual(t, 1, len(pi[f1].acls))
+	assert.NotEmpty(t, pi[f1].acls[0].userName)
+	assert.NotEmpty(t, pi[f1].acls[0].accessMask)
+
+	assert.Equal(t, f2, pi[f2].path)
+	assert.Equal(t, "-r--r--r--", pi[f2].mode)
+	assert.NotNil(t, pi[f2].acls)
+	assert.LessOrEqual(t, 1, len(pi[f2].acls))
+	assert.NotEmpty(t, pi[f2].acls[0].userName)
+	assert.NotEmpty(t, pi[f2].acls[0].accessMask)
+
+	assert.Equal(t, f3, pi[f3].path)
+	assert.Empty(t, pi[f3].mode)
+	assert.NotNil(t, pi[f3].err)
+	assert.Nil(t, pi[f3].acls)
 }
 
 func TestPermsFileCommit(t *testing.T) {
-	tempDir, f1, f2, f3 := createPermsTestFile(t)
-
-	// Set current directory to temp (and restore) because
-	// filepath.Dir(config.Datadog.ConfigFileUsed()) returns "."
-	curDir, _ := os.Getwd()
-	os.Chdir(tempDir)
-	defer os.Chdir(curDir)
+	f1, f2, f3 := createPermsTestFile(t)
 
 	pi := permissionsInfos{}
 	pi.add(f1)
@@ -49,10 +73,10 @@ func TestPermsFileCommit(t *testing.T) {
 	b, err := pi.commit()
 	require.NoError(t, err)
 
-	res, _ := regexp.Match("\nC:\\\\.+\\\\file1 ", b)
+	res, _ := regexp.Match("\nFile: C:\\\\.+\\\\file1\n", b)
 	assert.True(t, res)
-	res, _ = regexp.Match("\nC:\\\\.+\\\\file2 ", b)
+	res, _ = regexp.Match("\nFile: C:\\\\.+\\\\file2\n", b)
 	assert.True(t, res)
-	res, _ = regexp.Match("\nSuccessfully processed 3 files", b)
+	res, _ = regexp.Match("\ncould not stat file.+C:\\\\.+\\\\file3", b)
 	assert.True(t, res)
 }
