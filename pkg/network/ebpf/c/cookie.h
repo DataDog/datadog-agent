343f64def0cb4bbe7905da58e31de334bc6b9494
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2023-01-17 12:47:07 -0600

    Correct condition

--- pkg/network/ebpf/c/cookie.h
+++ pkg/network/ebpf/c/cookie.h
@@ -9,12 +9,12 @@
 static __always_inline u32 get_sk_cookie(struct sock *sk) {
 #if defined(COMPILE_RUNTIME) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
     return bpf_get_prandom_u32();
-#endif
-
+#else
     __u64 t = bpf_ktime_get_ns();
     __u64 _sk = 0;
     bpf_probe_read_with_telemetry(&_sk, sizeof(_sk), &sk);
     return (u32)(_sk ^ t);
+#endif
 }
 
 #endif // __COOKIE_H__
