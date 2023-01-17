e0d4212eaa8ddd072964f98c572c93195738f3c2
Val Pliskin <valeri.pliskin@datadoghq.com>
2023-01-17 18:06:49 +0200

    - added ioctl kprobe to ebpf_javatls subprogram

--- pkg/network/ebpf/c/protocols/java-tls-erpc.h
+++ pkg/network/ebpf/c/protocols/java-tls-erpc.h
@@ -4,6 +4,7 @@
 #include "bpf_helpers.h"
 #include "tracer.h"
 #include "tags-types.h"
+#include "https.h"
 
 #define USM_IOCTL_ID 0xda7ad09
 
@@ -12,6 +13,7 @@ enum erpc_message_type {
     CLOSE_CONNECTION
 };
 
+static __always_inline void https_process(conn_tuple_t *t, void *buffer, size_t len, __u64 tags);
 
 int __attribute__((always_inline)) handle_request(conn_tuple_t* connection, void *data) {
     const bool val = true;
@@ -19,30 +21,35 @@ int __attribute__((always_inline)) handle_request(conn_tuple_t* connection, void
 
     //read the actual length of the message (limited by HTTP_BUFFER_SIZE)
     if (0 != bpf_probe_read_user(&bytes_read, sizeof(bytes_read), data)){
-        u64 pid_tgid = bpf_get_current_pid_tgid();
-        u64 pid = pid_tgid >> 32;\
-        if (pid){}
-        log_debug("[java-tls-handle_request] failed reading message length location for pid %d\n", pid);
+        #ifdef DEBUG
+            u64 pid_tgid = bpf_get_current_pid_tgid();
+            u64 pid = pid_tgid >> 32;
+            log_debug("[java-tls-handle_request] failed reading message length location for pid %d\n", pid);
+        #endif
+
         return 1;
     }
-    bpf_map_update_elem(&java_tls_connections, &connection, &val, BPF_ANY);
-    https_process(connection, data+sizeof(bytes_read), bytes_read, JAVA_TLS);
+    //register the connection in our map
+    bpf_map_update_elem(&java_tls_connections, connection, &val, BPF_ANY);
+    log_debug("[java-tls-handle_request] got request to handle, size: %d\n", bytes_read);
+    https_process(connection, data, bytes_read, JAVA_TLS);
     return 0;
 }
 
 void __attribute__((always_inline)) handle_close_connection(conn_tuple_t* connection, void *data) {
-    void *exists = bpf_map_lookup_elem(&java_tls_connections, &connection);
+    void *exists = bpf_map_lookup_elem(&java_tls_connections, connection);
     // if the connection exists in our map, finalize it and remove from the map
     // otherwise just ignore
     if (exists != NULL){
         https_finish(connection);
-        bpf_map_delete_elem(&java_tls_connections,&connection);
+        bpf_map_delete_elem(&java_tls_connections,connection);
     }
 }
 
 int __attribute__((always_inline)) is_usm_erpc_request(struct pt_regs *ctx) {
-    u32 cmd = PT_REGS_PARM3(ctx);
+    u32 cmd = PT_REGS_PARM2(ctx);
     if (cmd != USM_IOCTL_ID) {
+        log_debug("[is_usm_erpc_request] bad request %d\n", cmd);
         return 0;
     }
 
@@ -50,10 +57,13 @@ int __attribute__((always_inline)) is_usm_erpc_request(struct pt_regs *ctx) {
 }
 
 int __attribute__((always_inline)) handle_erpc_request(struct pt_regs *ctx) {
-    u64 pid_tgid = bpf_get_current_pid_tgid();
-    u64 pid = pid_tgid >> 32;
-    if (pid){}
-    void *req = (void *)PT_REGS_PARM4(ctx);
+    #ifdef DEBUG
+        u64 pid_tgid = bpf_get_current_pid_tgid();
+        u64 pid = pid_tgid >> 32;
+    #else
+        __maybe_unused u64 pid=0;
+    #endif
+    void *req = (void *)PT_REGS_PARM3(ctx);
 
     u8 op = 0;
     if (0 != bpf_probe_read_user(&op, sizeof(op), req)){
