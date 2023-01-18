1113f732c18e8e7987a0dd99a4ca84d036925ba9
Guy Arbitman <guy20495@gmail.com>
2023-01-18 19:51:15 +0200

    usm: classification: Fixed AMQP publish flakiness (#15071)
    * usm: classification: Fixed AMQP publish flakiness
    
    Publish has no response, so added classification by close-reply and close-ok messages.
    
    * Moved closing of the client into the teardown
    
    * Fixed test error
    
    * Removed wrong bpf debug setup

--- pkg/network/tracer/tracer_classification_test.go
+++ pkg/network/tracer/tracer_classification_test.go
@@ -277,6 +277,7 @@ func testProtocolClassification(t *testing.T, cfg *config.Config, clientHost, ta
 				serverPort:       "5672",
 				clientDialer:     defaultDialer,
 				expectedProtocol: network.ProtocolAMQP,
+				extras:           make(map[string]interface{}),
 			},
 			preTracerSetup: func(t *testing.T, ctx testContext) {
 				host, port, _ := net.SplitHostPort(ctx.serverAddress)
@@ -287,7 +288,11 @@ func testProtocolClassification(t *testing.T, cfg *config.Config, clientHost, ta
 					ServerAddress: ctx.serverAddress,
 				})
 				require.NoError(t, err)
-				defer client.Terminate()
+				ctx.extras["client"] = client
+			},
+			teardown: func(t *testing.T, ctx testContext) {
+				client := ctx.extras["client"].(*amqp.Client)
+				client.Terminate()
 			},
 			shouldSkip: composeSkips(skipIfNotLinux, skipIfUsingNAT),
 			validation: validateProtocolConnection,
@@ -312,9 +317,11 @@ func testProtocolClassification(t *testing.T, cfg *config.Config, clientHost, ta
 			},
 			postTracerSetup: func(t *testing.T, ctx testContext) {
 				client := ctx.extras["client"].(*amqp.Client)
-				defer client.Terminate()
-
 				require.NoError(t, client.DeclareQueue("test", client.PublishChannel))
+			},
+			teardown: func(t *testing.T, ctx testContext) {
+				client := ctx.extras["client"].(*amqp.Client)
+				client.Terminate()
 			},
 			shouldSkip: composeSkips(skipIfNotLinux, skipIfUsingNAT),
 			validation: validateProtocolConnection,
@@ -340,9 +347,11 @@ func testProtocolClassification(t *testing.T, cfg *config.Config, clientHost, ta
 			},
 			postTracerSetup: func(t *testing.T, ctx testContext) {
 				client := ctx.extras["client"].(*amqp.Client)
-				defer client.Terminate()
-
 				require.NoError(t, client.Publish("test", "my msg"))
+			},
+			teardown: func(t *testing.T, ctx testContext) {
+				client := ctx.extras["client"].(*amqp.Client)
+				client.Terminate()
 			},
 			shouldSkip: composeSkips(skipIfNotLinux, skipIfUsingNAT),
 			validation: validateProtocolConnection,
@@ -370,11 +379,13 @@ func testProtocolClassification(t *testing.T, cfg *config.Config, clientHost, ta
 			},
 			postTracerSetup: func(t *testing.T, ctx testContext) {
 				client := ctx.extras["client"].(*amqp.Client)
-				defer client.Terminate()
-
 				res, err := client.Consume("test", 1)
 				require.NoError(t, err)
 				require.Equal(t, []string{"my msg"}, res)
+			},
+			teardown: func(t *testing.T, ctx testContext) {
+				client := ctx.extras["client"].(*amqp.Client)
+				client.Terminate()
 			},
 			shouldSkip: composeSkips(skipIfNotLinux, skipIfUsingNAT),
 			validation: validateProtocolConnection,
