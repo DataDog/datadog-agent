75215b2b3abbf96cf7840b00f0ae8cb299417310
Julien Lebot <julien.lebot@datadoghq.com>
2023-01-20 15:15:26 +0100

    Use Wixsharp for the Windows Installer (#13459)

--- test/kitchen/site-cookbooks/dd-agent-install/recipes/_stop_windows_agent.rb
+++ test/kitchen/site-cookbooks/dd-agent-install/recipes/_stop_windows_agent.rb
@@ -8,15 +8,6 @@ powershell_script 'stop-datadog-agent' do
   code <<-EOH
     $serviceName = "#{node['dd-agent-install']['agent_name']}"
     sc.exe query $serviceName;
-    $agentService = Get-Service -Name $serviceName;
-    if ($agentService.Status -eq "running")
-    {
-      foreach($dependentService in $agentService.DependentServices | Where-Object { $_.status -eq 'running' })
-      {
-        Write-Host "Stopping " + $dependentService.name;
-        sc.exe stop $dependentService.name
-      }
-      sc.exe stop $serviceName
-    }
+    Stop-Service -force $serviceName
   EOH
 end
