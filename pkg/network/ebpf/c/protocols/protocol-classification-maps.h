67fd057f91f7ec2ce7a39c3be29cc5f7816e43b2
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-01-20 10:50:02 +0100

    movemap-conn_tuple_to_socket_skb_conn_tuple-rightplace

--- pkg/network/ebpf/c/protocols/protocol-classification-maps.h
+++ pkg/network/ebpf/c/protocols/protocol-classification-maps.h
@@ -9,16 +9,6 @@
 // connection. Assumption: each connection has a single protocol.
 BPF_HASH_MAP(connection_protocol, conn_tuple_t, protocol_t, 0)
 
-// Maps skb connection tuple to socket connection tuple.
-// On ingress, skb connection tuple is pre NAT, and socket connection tuple is post NAT, and on egress, the opposite.
-// We track the lifecycle of socket using tracepoint net/net_dev_queue.
-// Some protocol can be classified in a single direction (for example HTTP/2 can be classified only by the first 24 bytes
-// sent on the hand shake), and if we have NAT, then the conn tuple we extract from sk_buff will be different than the
-// one we extract from the sock object, and then we are not able to correctly classify those protocols.
-// To overcome those problems, we save two maps that translates from conn tuple of sk_buff to conn tuple of sock* and vice
-// versa (the vice versa is used for cleanup purposes).
-BPF_HASH_MAP(conn_tuple_to_socket_skb_conn_tuple, conn_tuple_t, conn_tuple_t, 0)
-
 // Maps a connection tuple to latest tcp segment we've processed. Helps to detect same packets that travels multiple
 // interfaces or retransmissions.
 BPF_HASH_MAP(connection_states, conn_tuple_t, u32, 0)
--- pkg/network/ebpf/c/tracer-maps.h
+++ pkg/network/ebpf/c/tracer-maps.h
@@ -92,4 +92,14 @@ BPF_HASH_MAP(do_sendfile_args, __u64, struct sock *, 1024)
 // corresponding kretprobes
 BPF_HASH_MAP(ip_make_skb_args, __u64, ip_make_skb_args_t, 1024)
 
+// Maps skb connection tuple to socket connection tuple.
+// On ingress, skb connection tuple is pre NAT, and socket connection tuple is post NAT, and on egress, the opposite.
+// We track the lifecycle of socket using tracepoint net/net_dev_queue.
+// Some protocol can be classified in a single direction (for example HTTP/2 can be classified only by the first 24 bytes
+// sent on the hand shake), and if we have NAT, then the conn tuple we extract from sk_buff will be different than the
+// one we extract from the sock object, and then we are not able to correctly classify those protocols.
+// To overcome those problems, we save two maps that translates from conn tuple of sk_buff to conn tuple of sock* and vice
+// versa (the vice versa is used for cleanup purposes).
+BPF_HASH_MAP(conn_tuple_to_socket_skb_conn_tuple, conn_tuple_t, conn_tuple_t, 0)
+
 #endif
