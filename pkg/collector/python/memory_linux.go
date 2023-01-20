7450bd3c29735f6d383b6ab637209ce923159b36
Brian Floersch <brian.floersch@datadoghq.com>
2023-01-20 09:55:37 -0500

    add linux build tag

--- pkg/collector/python/memory_linux.go
+++ pkg/collector/python/memory_linux.go
@@ -3,8 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build python
-// +build python
+//go:build python && linux
+// +build python,linux
 
 package python
 
