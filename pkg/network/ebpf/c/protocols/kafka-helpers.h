255e5bfa633d1200067e059229d5010cdc288dba
DanielLavie <daniel.lavie@datadoghq.com>
2023-01-30 14:52:02 +0200

    WIP - trying to understand the offset initialization behaviour

--- pkg/network/ebpf/c/protocols/kafka-helpers.h
+++ pkg/network/ebpf/c/protocols/kafka-helpers.h
@@ -37,8 +37,9 @@ static __always_inline bool is_kafka(const char* buf, __u32 buf_size) {
     kafka_context.buffer = buf;
     kafka_context.buffer_size = MIN(buf_size, CLASSIFICATION_MAX_BUFFER);
     fill_kafka_header(&kafka_context);
+    log_debug("kafka: kafka_context->offset: %u\n", kafka_context.offset);
 
-    return is_kafka_request_header(&kafka_context) || is_kafka_request(&kafka_context);
+    return is_kafka_request_header(&kafka_context) && is_kafka_request(&kafka_context);
 }
 
 static __always_inline bool is_kafka_request_header(kafka_context_t* kafka_context) {
