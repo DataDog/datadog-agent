f96673aca759b03b72a86c6d9b9c298f7e8eabdd
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2023-01-31 09:03:26 +0100

    apply rule filtering only if we have filters define on the rule

--- pkg/compliance/checks/builder.go
+++ pkg/compliance/checks/builder.go
@@ -439,18 +439,20 @@ func (b *builder) GetCheckStatus() compliance.CheckStatusList {
 }
 
 func (b *builder) checkFromRegoRule(meta *compliance.SuiteMeta, rule *compliance.RegoRule) (compliance.Check, error) {
-	ruleFilterModel := module.NewRuleFilterModel()
-	seclRuleFilter := rules.NewSECLRuleFilter(ruleFilterModel)
+	if len(rule.Filters) > 0 {
+		ruleFilterModel := module.NewRuleFilterModel()
+		seclRuleFilter := rules.NewSECLRuleFilter(ruleFilterModel)
 
-	accepted, err := seclRuleFilter.IsRuleAccepted(&rules.RuleDefinition{
-		Filters: rule.Filters,
-	})
-	if err != nil {
-		log.Errorf("failed to apply rule filters: %s", err)
-	}
-	if !accepted {
-		log.Infof("rule %s skipped - not matching constraints", rule.ID)
-		return nil, ErrRuleDoesNotApply
+		accepted, err := seclRuleFilter.IsRuleAccepted(&rules.RuleDefinition{
+			Filters: rule.Filters,
+		})
+		if err != nil {
+			log.Errorf("failed to apply rule filters: %s", err)
+		}
+		if !accepted {
+			log.Infof("rule %s skipped - not matching constraints", rule.ID)
+			return nil, ErrRuleDoesNotApply
+		}
 	}
 
 	ruleScope, err := getRuleScope(meta, rule.Scope)
