cf58c75231b283fad1e4f7c16c20ff9f0a227c50
Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>
2023-02-01 11:21:23 +0100

    pkg/clusteragent/admission/patch: make file provider ready for e2e testing (#15221)
    * pkg/clusteragent/admission/patch: make file provider ready for e2e testing

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1025,7 +1025,8 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.endpoint", "/injectlib")
 	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.container_registry", "gcr.io/datadoghq")
 	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.patcher.enabled", false)
-	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.patcher.fallback_to_file_provider", false) // to be enabled only in e2e tests
+	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.patcher.fallback_to_file_provider", false)                                // to be enabled only in e2e tests
+	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.patcher.file_provider_path", "/etc/datadog-agent/patch/auto-instru.json") // to be used only in e2e tests
 
 	// Telemetry
 	// Enable telemetry metrics on the internals of the Agent.
