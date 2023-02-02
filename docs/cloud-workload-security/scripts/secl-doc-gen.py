6db9f7c664b3951b4ad1c7d6c4a48f32426aa2f7
modernplumbing <modernplumbing9@gmail.com>
2023-02-02 10:21:17 -0500

    ran black

--- docs/cloud-workload-security/scripts/secl-doc-gen.py
+++ docs/cloud-workload-security/scripts/secl-doc-gen.py
@@ -12,6 +12,7 @@ class EventTypeProperty:
     definition: str
     doc_link: str
 
+
 @dataclass
 class EventType:
     name: str
@@ -27,6 +28,7 @@ class Example:
     expression: str
     description: str
 
+
 @dataclass
 class PropertyDocumentation:
     name: str
@@ -37,6 +39,7 @@ class PropertyDocumentation:
     constants: str
     constants_link: str
     examples: List[Example]
+
 
 @dataclass
 class Constant:
@@ -52,7 +55,6 @@ class Constants:
     all: List[Constant]
 
 
-
 def build_event_types(top_node):
     output = []
     for et in top_node["event_types"]:
@@ -65,14 +67,25 @@ def build_event_types(top_node):
         output.append(event_type)
     return output
 
+
 def build_properties_doc(top_node):
     output = []
     for property in top_node["properties_doc"]:
-        property_doc = PropertyDocumentation(property["name"], property["link"], property["type"], property["definition"], property["prefixes"], property["constants"], property["constants_link"], [])
+        property_doc = PropertyDocumentation(
+            property["name"],
+            property["link"],
+            property["type"],
+            property["definition"],
+            property["prefixes"],
+            property["constants"],
+            property["constants_link"],
+            [],
+        )
         for exp in property["examples"]:
             property_doc.examples.append(Example(exp["expression"], exp["description"]))
         output.append(property_doc)
     return output
+
 
 def build_constants(top_node):
     output = []
@@ -100,5 +113,13 @@ if __name__ == "__main__":
     constants_list = build_constants(json_top_node)
 
     output_file = open(args.output, "w")
-    print(common.fill_template(args.template, event_types=event_types, constants_list=constants_list, properties_doc_list=properties_doc_list), file=output_file)
+    print(
+        common.fill_template(
+            args.template,
+            event_types=event_types,
+            constants_list=constants_list,
+            properties_doc_list=properties_doc_list,
+        ),
+        file=output_file,
+    )
     output_file.close()
