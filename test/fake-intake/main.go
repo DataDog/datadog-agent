57f8b7c0e17835c6824e943a907c1692dffde84a
pducolin <paola.ducolin@datadoghq.com>
2023-02-03 11:20:30 +0100

    [fakeintake] remove not required done channel

--- test/fake-intake/main.go
+++ test/fake-intake/main.go
@@ -17,23 +17,18 @@ import (
 func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
-	done := make(chan bool, 1)
 
 	fmt.Println("⌛️ Starting fake intake")
 	fi := fakeintake.NewFakeIntake()
 	fmt.Println("🏃 Fake intake running")
 
-	go func() {
-		<-sigs
-		fmt.Println("Stopping fake intake")
-		err := fi.Stop()
-		if err != nil {
-			fmt.Println("Error stopping fake intake, ", err)
-		}
-		done <- true
-	}()
+	<-sigs
+	fmt.Println("Stopping fake intake")
+	err := fi.Stop()
+	if err != nil {
+		fmt.Println("Error stopping fake intake, ", err)
+	}
 
-	<-done
 	fmt.Println("Fake intake is stopped")
 	fmt.Println("👋 Bye bye")
 }
