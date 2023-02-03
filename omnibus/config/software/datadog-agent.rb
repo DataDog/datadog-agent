c8a977daf39b165614c9115c2d495b3aa021280a
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2023-02-03 08:46:30 +0100

    [omnibus] Deactivate openscap build against arm7l machine

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -14,7 +14,7 @@ dependency "python3" if with_python_runtime? "3"
 dependency "libarchive" if windows?
 dependency "yaml-cpp" if windows?
 
-dependency "openscap" if linux?
+dependency "openscap" if linux? and !arm7l?
 
 source path: '..'
 relative_path 'src/github.com/DataDog/datadog-agent'
