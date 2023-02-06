2377d274e6a8d148250ce240f66198ed3dde0597
David Ortiz <david.ortiz@datadoghq.com>
2023-02-06 19:15:13 +0100

    [corechecks/{containerimage,sbom}] Refactor tests to be table-driven

--- pkg/collector/corechecks/sbom/processor_test.go
+++ pkg/collector/corechecks/sbom/processor_test.go
@@ -19,52 +19,79 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
-	"github.com/DataDog/datadog-agent/pkg/collector/check"
 	"github.com/DataDog/datadog-agent/pkg/util/pointer"
 	"github.com/DataDog/datadog-agent/pkg/workloadmeta"
 )
 
 func TestProcessEvents(t *testing.T) {
-	var SBOMsSent = atomic.NewInt32(0)
-
-	sender := mocksender.NewMockSender(check.ID(""))
-	sender.On("SBOM", mock.Anything, mock.Anything).Return().Run(func(_ mock.Arguments) {
-		SBOMsSent.Inc()
-	})
-
-	// Define a max size of 1 for the queue. With a size > 1, it's difficult to
-	// control the number of events sent on each call.
-	p := newProcessor(sender, 1, 50*time.Millisecond)
-
 	sbomGenerationTime := time.Now()
 
-	p.processEvents(workloadmeta.EventBundle{
-		Events: []workloadmeta.Event{
-			{
-				Type: workloadmeta.EventTypeSet,
-				Entity: &workloadmeta.ContainerImageMetadata{
-					EntityID: workloadmeta.EntityID{
-						Kind: workloadmeta.KindContainerImageMetadata,
-						ID:   "sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+	tests := []struct {
+		name          string
+		inputEvents   []workloadmeta.Event
+		expectedSBOMs []*model.SBOMEntity
+	}{
+		{
+			name: "standard case",
+			inputEvents: []workloadmeta.Event{
+				{
+					Type: workloadmeta.EventTypeSet,
+					Entity: &workloadmeta.ContainerImageMetadata{
+						EntityID: workloadmeta.EntityID{
+							Kind: workloadmeta.KindContainerImageMetadata,
+							ID:   "sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+						},
+						RepoTags: []string{
+							"datadog/agent:7-rc",
+							"datadog/agent:7.41.1-rc.1",
+							"gcr.io/datadoghq/agent:7-rc",
+							"gcr.io/datadoghq/agent:7.41.1-rc.1",
+							"public.ecr.aws/datadog/agent:7-rc",
+							"public.ecr.aws/datadog/agent:7.41.1-rc.1",
+						},
+						RepoDigests: []string{
+							"datadog/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
+							"gcr.io/datadoghq/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
+							"public.ecr.aws/datadog/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
+						},
+						SBOM: &workloadmeta.SBOM{
+							CycloneDXBOM: &cyclonedx.BOM{
+								SpecVersion: cyclonedx.SpecVersion1_4,
+								Version:     42,
+								Components: &[]cyclonedx.Component{
+									{
+										Name: "Foo",
+									},
+									{
+										Name: "Bar",
+									},
+									{
+										Name: "Baz",
+									},
+								},
+							},
+							GenerationTime:     sbomGenerationTime,
+							GenerationDuration: 10 * time.Second,
+						},
 					},
-					RepoTags: []string{
-						"datadog/agent:7-rc",
-						"datadog/agent:7.41.1-rc.1",
-						"gcr.io/datadoghq/agent:7-rc",
-						"gcr.io/datadoghq/agent:7.41.1-rc.1",
-						"public.ecr.aws/datadog/agent:7-rc",
-						"public.ecr.aws/datadog/agent:7.41.1-rc.1",
+				},
+			},
+			expectedSBOMs: []*model.SBOMEntity{
+				{
+					Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
+					Id:   "datadog/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+					Tags: []string{
+						"7-rc",
+						"7.41.1-rc.1",
 					},
-					RepoDigests: []string{
-						"datadog/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
-						"gcr.io/datadoghq/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
-						"public.ecr.aws/datadog/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
-					},
-					SBOM: &workloadmeta.SBOM{
-						CycloneDXBOM: &cyclonedx.BOM{
-							SpecVersion: cyclonedx.SpecVersion1_4,
-							Version:     42,
-							Components: &[]cyclonedx.Component{
+					InUse:              true,
+					GeneratedAt:        timestamppb.New(sbomGenerationTime),
+					GenerationDuration: durationpb.New(10 * time.Second),
+					Sbom: &model.SBOMEntity_Cyclonedx{
+						Cyclonedx: &cyclonedx_v1_4.Bom{
+							SpecVersion: "1.4",
+							Version:     pointer.Ptr(int32(42)),
+							Components: []*cyclonedx_v1_4.Component{
 								{
 									Name: "Foo",
 								},
@@ -76,161 +103,23 @@ func TestProcessEvents(t *testing.T) {
 								},
 							},
 						},
-						GenerationTime:     sbomGenerationTime,
-						GenerationDuration: 10 * time.Second,
 					},
 				},
-			},
-		},
-		Ch: make(chan struct{}),
-	})
-
-	expectedSBOMs := []*model.SBOMEntity{
-		{
-			Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
-			Id:   "datadog/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
-			Tags: []string{
-				"7-rc",
-				"7.41.1-rc.1",
-			},
-			InUse:              true,
-			GeneratedAt:        timestamppb.New(sbomGenerationTime),
-			GenerationDuration: durationpb.New(10 * time.Second),
-			Sbom: &model.SBOMEntity_Cyclonedx{
-				Cyclonedx: &cyclonedx_v1_4.Bom{
-					SpecVersion: "1.4",
-					Version:     pointer.Ptr(int32(42)),
-					Components: []*cyclonedx_v1_4.Component{
-						{
-							Name: "Foo",
-						},
-						{
-							Name: "Bar",
-						},
-						{
-							Name: "Baz",
-						},
+				{
+					Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
+					Id:   "gcr.io/datadoghq/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+					Tags: []string{
+						"7-rc",
+						"7.41.1-rc.1",
 					},
-				},
-			},
-		},
-		{
-			Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
-			Id:   "gcr.io/datadoghq/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
-			Tags: []string{
-				"7-rc",
-				"7.41.1-rc.1",
-			},
-			InUse:              true,
-			GeneratedAt:        timestamppb.New(sbomGenerationTime),
-			GenerationDuration: durationpb.New(10 * time.Second),
-			Sbom: &model.SBOMEntity_Cyclonedx{
-				Cyclonedx: &cyclonedx_v1_4.Bom{
-					SpecVersion: "1.4",
-					Version:     pointer.Ptr(int32(42)),
-					Components: []*cyclonedx_v1_4.Component{
-						{
-							Name: "Foo",
-						},
-						{
-							Name: "Bar",
-						},
-						{
-							Name: "Baz",
-						},
-					},
-				},
-			},
-		},
-		{
-			Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
-			Id:   "public.ecr.aws/datadog/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
-			Tags: []string{
-				"7-rc",
-				"7.41.1-rc.1",
-			},
-			InUse:              true,
-			GeneratedAt:        timestamppb.New(sbomGenerationTime),
-			GenerationDuration: durationpb.New(10 * time.Second),
-			Sbom: &model.SBOMEntity_Cyclonedx{
-				Cyclonedx: &cyclonedx_v1_4.Bom{
-					SpecVersion: "1.4",
-					Version:     pointer.Ptr(int32(42)),
-					Components: []*cyclonedx_v1_4.Component{
-						{
-							Name: "Foo",
-						},
-						{
-							Name: "Bar",
-						},
-						{
-							Name: "Baz",
-						},
-					},
-				},
-			},
-		},
-	}
-
-	p.stop()
-
-	// The queue is processing the events in a different go routine and might
-	// need some time
-	assert.Eventually(t, func() bool {
-		return SBOMsSent.Load() == int32(len(expectedSBOMs))
-	}, 1*time.Second, 5*time.Millisecond)
-
-	for _, expectedSBOM := range expectedSBOMs {
-		sender.AssertSBOM(t, []model.SBOMPayload{
-			{
-				Version:  1,
-				Source:   &sourceAgent,
-				Entities: []*model.SBOMEntity{expectedSBOM},
-			},
-		})
-	}
-}
-
-func TestProcessEvents_repoTagWithNoMatchingRepoDigest(t *testing.T) {
-	// In containerd some images are created without a repo digest, and it's
-	// also possible to remove repo digests manually. To test that scenario, in
-	// this test, we define an image with 2 repo tags: one for the gcr.io
-	// registry and another for the public.ecr.aws registry, but there's only
-	// one repo digest.
-	// We expect to send 2 events, one for each registry.
-
-	var SBOMsSent = atomic.NewInt32(0)
-
-	sender := mocksender.NewMockSender("")
-	sender.On("SBOM", mock.Anything, mock.Anything).Return().Run(func(_ mock.Arguments) {
-		SBOMsSent.Inc()
-	})
-	p := newProcessor(sender, 1, 50*time.Millisecond)
-
-	sbomGenerationTime := time.Now()
-
-	p.processEvents(workloadmeta.EventBundle{
-		Events: []workloadmeta.Event{
-			{
-				Type: workloadmeta.EventTypeSet,
-				Entity: &workloadmeta.ContainerImageMetadata{
-					EntityID: workloadmeta.EntityID{
-						Kind: workloadmeta.KindContainerImageMetadata,
-						ID:   "sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
-					},
-					RepoTags: []string{
-						"gcr.io/datadoghq/agent:7-rc",
-						"public.ecr.aws/datadog/agent:7-rc",
-					},
-					RepoDigests: []string{
-						// Notice that there's a repo tag for gcr.io, but no repo digest.
-						"public.ecr.aws/datadog/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
-					},
-					SBOM: &workloadmeta.SBOM{
-						CycloneDXBOM: &cyclonedx.BOM{
-							SpecVersion: cyclonedx.SpecVersion1_4,
-							Version:     42,
-							Components: &[]cyclonedx.Component{
+					InUse:              true,
+					GeneratedAt:        timestamppb.New(sbomGenerationTime),
+					GenerationDuration: durationpb.New(10 * time.Second),
+					Sbom: &model.SBOMEntity_Cyclonedx{
+						Cyclonedx: &cyclonedx_v1_4.Bom{
+							SpecVersion: "1.4",
+							Version:     pointer.Ptr(int32(42)),
+							Components: []*cyclonedx_v1_4.Component{
 								{
 									Name: "Foo",
 								},
@@ -242,65 +131,136 @@ func TestProcessEvents_repoTagWithNoMatchingRepoDigest(t *testing.T) {
 								},
 							},
 						},
-						GenerationTime:     sbomGenerationTime,
-						GenerationDuration: 10 * time.Second,
 					},
 				},
-			},
-		},
-		Ch: make(chan struct{}),
-	})
-
-	expectedSBOMs := []*model.SBOMEntity{
-		{
-			Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
-			Id:   "public.ecr.aws/datadog/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
-			Tags: []string{
-				"7-rc",
-			},
-			InUse:              true,
-			GeneratedAt:        timestamppb.New(sbomGenerationTime),
-			GenerationDuration: durationpb.New(10 * time.Second),
-			Sbom: &model.SBOMEntity_Cyclonedx{
-				Cyclonedx: &cyclonedx_v1_4.Bom{
-					SpecVersion: "1.4",
-					Version:     pointer.Ptr(int32(42)),
-					Components: []*cyclonedx_v1_4.Component{
-						{
-							Name: "Foo",
-						},
-						{
-							Name: "Bar",
-						},
-						{
-							Name: "Baz",
+				{
+					Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
+					Id:   "public.ecr.aws/datadog/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+					Tags: []string{
+						"7-rc",
+						"7.41.1-rc.1",
+					},
+					InUse:              true,
+					GeneratedAt:        timestamppb.New(sbomGenerationTime),
+					GenerationDuration: durationpb.New(10 * time.Second),
+					Sbom: &model.SBOMEntity_Cyclonedx{
+						Cyclonedx: &cyclonedx_v1_4.Bom{
+							SpecVersion: "1.4",
+							Version:     pointer.Ptr(int32(42)),
+							Components: []*cyclonedx_v1_4.Component{
+								{
+									Name: "Foo",
+								},
+								{
+									Name: "Bar",
+								},
+								{
+									Name: "Baz",
+								},
+							},
 						},
 					},
 				},
 			},
 		},
 		{
-			Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
-			Id:   "gcr.io/datadoghq/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
-			Tags: []string{
-				"7-rc",
+			// In containerd some images are created without a repo digest, and it's
+			// also possible to remove repo digests manually. To test that scenario, in
+			// this test, we define an image with 2 repo tags: one for the gcr.io
+			// registry and another for the public.ecr.aws registry, but there's only
+			// one repo digest.
+			// We expect to send 2 events, one for each registry.
+			name: "repo tag with no matching repo digest",
+			inputEvents: []workloadmeta.Event{
+				{
+					Type: workloadmeta.EventTypeSet,
+					Entity: &workloadmeta.ContainerImageMetadata{
+						EntityID: workloadmeta.EntityID{
+							Kind: workloadmeta.KindContainerImageMetadata,
+							ID:   "sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+						},
+						RepoTags: []string{
+							"gcr.io/datadoghq/agent:7-rc",
+							"public.ecr.aws/datadog/agent:7-rc",
+						},
+						RepoDigests: []string{
+							// Notice that there's a repo tag for gcr.io, but no repo digest.
+							"public.ecr.aws/datadog/agent@sha256:052f1fdf4f9a7117d36a1838ab60782829947683007c34b69d4991576375c409",
+						},
+						SBOM: &workloadmeta.SBOM{
+							CycloneDXBOM: &cyclonedx.BOM{
+								SpecVersion: cyclonedx.SpecVersion1_4,
+								Version:     42,
+								Components: &[]cyclonedx.Component{
+									{
+										Name: "Foo",
+									},
+									{
+										Name: "Bar",
+									},
+									{
+										Name: "Baz",
+									},
+								},
+							},
+							GenerationTime:     sbomGenerationTime,
+							GenerationDuration: 10 * time.Second,
+						},
+					},
+				},
 			},
-			InUse:              true,
-			GeneratedAt:        timestamppb.New(sbomGenerationTime),
-			GenerationDuration: durationpb.New(10 * time.Second),
-			Sbom: &model.SBOMEntity_Cyclonedx{
-				Cyclonedx: &cyclonedx_v1_4.Bom{
-					SpecVersion: "1.4",
-					Version:     pointer.Ptr(int32(42)),
-					Components: []*cyclonedx_v1_4.Component{
-						{
-							Name: "Foo",
+			expectedSBOMs: []*model.SBOMEntity{
+				{
+					Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
+					Id:   "public.ecr.aws/datadog/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+					Tags: []string{
+						"7-rc",
+					},
+					InUse:              true,
+					GeneratedAt:        timestamppb.New(sbomGenerationTime),
+					GenerationDuration: durationpb.New(10 * time.Second),
+					Sbom: &model.SBOMEntity_Cyclonedx{
+						Cyclonedx: &cyclonedx_v1_4.Bom{
+							SpecVersion: "1.4",
+							Version:     pointer.Ptr(int32(42)),
+							Components: []*cyclonedx_v1_4.Component{
+								{
+									Name: "Foo",
+								},
+								{
+									Name: "Bar",
+								},
+								{
+									Name: "Baz",
+								},
+							},
 						},
-						{
-							Name: "Bar",
-						},
-						{
-							Name: "Baz",
+					},
+				},
+				{
+					Type: model.SBOMSourceType_CONTAINER_IMAGE_LAYERS,
+					Id:   "gcr.io/datadoghq/agent@sha256:9634b84c45c6ad220c3d0d2305aaa5523e47d6d43649c9bbeda46ff010b4aacd",
+					Tags: []string{
+						"7-rc",
+					},
+					InUse:              true,
+					GeneratedAt:        timestamppb.New(sbomGenerationTime),
+					GenerationDuration: durationpb.New(10 * time.Second),
+					Sbom: &model.SBOMEntity_Cyclonedx{
+						Cyclonedx: &cyclonedx_v1_4.Bom{
+							SpecVersion: "1.4",
+							Version:     pointer.Ptr(int32(42)),
+							Components: []*cyclonedx_v1_4.Component{
+								{
+									Name: "Foo",
+								},
+								{
+									Name: "Bar",
+								},
+								{
+									Name: "Baz",
+								},
+							},
 						},
 					},
 				},
@@ -308,21 +268,41 @@ func TestProcessEvents_repoTagWithNoMatchingRepoDigest(t *testing.T) {
 		},
 	}
 
-	p.stop()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var SBOMsSent = atomic.NewInt32(0)
 
-	// The queue is processing the events in a different go routine and might
-	// need some time
-	assert.Eventually(t, func() bool {
-		return SBOMsSent.Load() == int32(len(expectedSBOMs))
-	}, 1*time.Second, 5*time.Millisecond)
+			sender := mocksender.NewMockSender("")
+			sender.On("SBOM", mock.Anything, mock.Anything).Return().Run(func(_ mock.Arguments) {
+				SBOMsSent.Inc()
+			})
 
-	for _, expectedSBOM := range expectedSBOMs {
-		sender.AssertSBOM(t, []model.SBOMPayload{
-			{
-				Version:  1,
-				Source:   &sourceAgent,
-				Entities: []*model.SBOMEntity{expectedSBOM},
-			},
+			// Define a max size of 1 for the queue. With a size > 1, it's difficult to
+			// control the number of events sent on each call.
+			p := newProcessor(sender, 1, 50*time.Millisecond)
+
+			p.processEvents(workloadmeta.EventBundle{
+				Events: test.inputEvents,
+				Ch:     make(chan struct{}),
+			})
+
+			p.stop()
+
+			// The queue is processing the events in a different go routine and might
+			// need some time
+			assert.Eventually(t, func() bool {
+				return SBOMsSent.Load() == int32(len(test.expectedSBOMs))
+			}, 1*time.Second, 5*time.Millisecond)
+
+			for _, expectedSBOM := range test.expectedSBOMs {
+				sender.AssertSBOM(t, []model.SBOMPayload{
+					{
+						Version:  1,
+						Source:   &sourceAgent,
+						Entities: []*model.SBOMEntity{expectedSBOM},
+					},
+				})
+			}
 		})
 	}
 }
