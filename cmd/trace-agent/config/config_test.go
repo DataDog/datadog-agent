fccea11d8c990f7cd5b48e2c1dba26b617b5573f
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2023-02-08 17:09:11 +0200

    pkg/trace/api: add probabilistic sampling for OTLP (#15352)
    This change introduces a new probabilistic sampler as part of the OTLP Ingest in the trace-agent. This sampler allows the user to control ingestion reliably in a distributed system, considering that the same percentage is used in all agents.
    
    Use the following configuration:
    ```yaml
    otlp_config:
      traces:
        probabilistic_sampler:
          # @param sampling_percentage - number - optional - default: 100
          # @env DD_OTLP_CONFIG_TRACES_PROBABILISTIC_SAMPLER_SAMPLING_PERCENTAGE - number - optional - default: 100
          # Percentage of traces to ingest (0 100]. Invalid values (<= 0 || > 100) are disconsidered
          # and the default is used. If incoming spans have a sampling.priority set by the user, it 
          # will be followed and the sampling percentage will be overridden.
          sampling_percentage: 100
    ```

--- cmd/trace-agent/config/config_test.go
+++ cmd/trace-agent/config/config_test.go
@@ -438,6 +438,7 @@ func TestFullYamlConfig(t *testing.T) {
 	assert.True(c.LogThrottling)
 	assert.True(c.OTLPReceiver.SpanNameAsResourceName)
 	assert.Equal(map[string]string{"a": "b", "and:colons": "in:values", "c": "d", "with.dots": "in.side"}, c.OTLPReceiver.SpanNameRemappings)
+	assert.Equal(88.4, c.OTLPReceiver.ProbabilisticSampling)
 
 	noProxy := true
 	if _, ok := os.LookupEnv("NO_PROXY"); ok {
@@ -745,6 +746,16 @@ func TestLoadEnv(t *testing.T) {
 		cfg, err := LoadConfigFile("./testdata/undocumented.yaml")
 		assert.NoError(err)
 		assert.Equal("0.0.0.0", cfg.ReceiverHost)
+	})
+
+	env = "DD_OTLP_CONFIG_TRACES_PROBABILISTIC_SAMPLER_SAMPLING_PERCENTAGE"
+	t.Run(env, func(t *testing.T) {
+		defer cleanConfig()()
+		assert := assert.New(t)
+		t.Setenv(env, "12.3")
+		cfg, err := LoadConfigFile("./testdata/undocumented.yaml")
+		assert.NoError(err)
+		assert.Equal(12.3, cfg.OTLPReceiver.ProbabilisticSampling)
 	})
 
 	for _, envKey := range []string{
