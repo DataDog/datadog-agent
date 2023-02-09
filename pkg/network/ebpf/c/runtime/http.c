bc35b1ad44b48d5d77a0cc950f01ed0f5e5551dd
Guy Arbitman <guy20495@gmail.com>
2023-02-09 15:58:34 +0200

    Fixed runtime

--- pkg/network/ebpf/c/runtime/http.c
+++ pkg/network/ebpf/c/runtime/http.c
@@ -136,6 +136,7 @@ int tracepoint__net__netif_receive_skb(struct pt_regs* ctx) {
     // flush batch to userspace
     // because perf events can't be sent from socket filter programs
     http_flush_batch(ctx);
+    http2_flush_batch(ctx);
     return 0;
 }
 
