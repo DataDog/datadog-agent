6efc0f79180c1fd227c368bae503f4540b54d996
David du Colombier <djc@datadoghq.com>
2023-02-10 14:20:07 +0100

    [omnibus] Add OpenSCAP and its dependencies
    OpenSCSAP is an implementation of the SCAP standards.
    It provides tools and libraries to handle SCAP documents.
    
    This change adds the following software definitions:
    
     - apt
     - attr
     - elfutils
     - file
     - glib
     - gmp
     - gnutls
     - libacl
     - libdb
     - libgcrypt
     - libgpg-error
     - liblz4
     - libselinux
     - libsepol
     - libtasn1
     - libxxhash
     - lua
     - m4
     - nettle
     - openscap
     - pcre2
     - popt
     - rpm
     - sqlite
     - util-linux
     - xmlsec
     - zstd
    
    Some patches were required:
    
     - apt/cmake-bindir.patch: allow to update BIN_DIR CMake CACHE entry
     - apt/disable_arch_check.patch: disable architecture check
     - apt/disable_systemd.patch: remove dependency on udev and systemd
     - apt/no_doc.patch: don't install documentation
     - apt/triehash.patch: add the triehash tool, needed at build time
     - libselinux/fcntl_o_path.patch: fix build with old glibc
     - libselinux/ln_no_relative.patch: don't use relative symlink on installed libraries
     - libsepol/ln_no_relative.patch: don't use relative symlink on installed libraries
     - lua/nodoc.patch: don't install documentation
     - nettle/install_to_lib.patch: omit lib directory suffix
     - rpm/0001-Include-fcntl.patch: fix build
    
    OpenSCAP includes a number of fixes, backported from maint-1.3
    branch and also some fixes which are currently being reviewed.
    
    We added -I. before -I#{install_dir}/embedded/include in CGO_CFLAGS,
    so the Go zstd package use its own zstd.h instead of using the one
    provided by the newly added zstd software.
    
    This change is the result of a collaborative work between
    Sylvain Baubeau, Pierre Guilleminot and David du Colombier.
    
    Co-authored-by: Sylvain Baubeau <lebauce@gmail.com>
    Co-authored-by: Pierre Guilleminot <pierre.guilleminot@datadoghq.com>

--- omnibus/config/software/freetds.rb
+++ omnibus/config/software/freetds.rb
@@ -5,6 +5,8 @@ version "1.1.36" do
   source sha256: "1c306e658e10a325eefddfd662cec3a6d9065fe61c515f26d4f1fb6c4c62405d"
 end
 
+ship_source_offer true
+
 source url: "https://www.freetds.org/files/stable/freetds-#{version}.tar.gz"
 
 relative_path "freetds-#{version}"
