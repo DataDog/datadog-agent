dccd27c7341018463edce0a2d0facce855d9598a
Celia Yuen <celia.yuen@datadoghq.com>
2023-02-17 12:05:54 -0500

    fix stress and functional test recipes

--- test/kitchen/site-cookbooks/dd-security-agent-check/recipes/default.rb
+++ test/kitchen/site-cookbooks/dd-security-agent-check/recipes/default.rb
@@ -8,9 +8,3 @@
 directory "#{node['common']['work_dir']}/tests" do
   recursive true
 end
-
-include_recipe "::functional-tests"
-
-if node['platform_family'] != 'windows'
-  include_recipe "::stress-tests"
-end
--- test/kitchen/site-cookbooks/dd-security-agent-check/recipes/stress-tests.rb
+++ test/kitchen/site-cookbooks/dd-security-agent-check/recipes/stress-tests.rb
@@ -5,14 +5,16 @@
 # Copyright (C) 2020-present Datadog
 #
 
-cookbook_file "#{node['common']['work_dir']}/tests/stresssuite" do
-  source "tests/stresssuite"
-  mode '755'
-end
+if node['platform_family'] != 'windows'
+  cookbook_file "#{node['common']['work_dir']}/tests/stresssuite" do
+    source "tests/stresssuite"
+    mode '755'
+  end
 
-['polkit', 'unattended-upgrades', 'snapd', 'cron', 'walinuxagent',
- 'multipathd', 'rsyslog', 'atd', 'chronyd', 'hv-kvp-daemon'].each do |s|
-  service s do
-      action :stop
+  ['polkit', 'unattended-upgrades', 'snapd', 'cron', 'walinuxagent',
+   'multipathd', 'rsyslog', 'atd', 'chronyd', 'hv-kvp-daemon'].each do |s|
+    service s do
+        action :stop
+    end
   end
 end
