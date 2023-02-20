17401d858c85087044185bd8920f995af2caa823
DanielLavie <daniel.lavie@datadoghq.com>
2023-02-20 14:33:50 +0200

    Mor resilient sanity test + lots of produces test

--- pkg/network/protocols/http/kafka_monitor_test.go
+++ pkg/network/protocols/http/kafka_monitor_test.go
@@ -98,11 +98,13 @@ func TestSanity(t *testing.T) {
 	numberOfProduceRequests := 0
 	numberOfFetchRequests := 0
 	for kafkaKey, kafkaStat := range kafkaStats {
-		// When the ctxTimeout is configured with 10 seconds, we get 2 fetches from this client
+		require.Equal(t, defaultTopicName, kafkaKey.TopicName)
 		if kafkaKey.RequestAPIKey == kafka.ProduceAPIKey {
+			require.Equal(t, uint16(8), kafkaKey.RequestVersion)
 			numberOfProduceRequests += kafkaStat.Count
 		} else if kafkaKey.RequestAPIKey == kafka.FetchAPIKey {
 			numberOfFetchRequests += kafkaStat.Count
+			require.Equal(t, uint16(11), kafkaKey.RequestVersion)
 		} else {
 			require.FailNow(t, "Expecting only produce or fetch kafka requests")
 		}
@@ -193,8 +195,64 @@ func TestProduceClientIdEmptyString(t *testing.T) {
 		if kafkaKey.RequestAPIKey != kafka.ProduceAPIKey {
 			require.FailNow(t, "Expecting only produce requests")
 		}
-		produceRequestsCount := kafkaStat.Count
-		kafkaStatIsOK := produceRequestsCount == 1
-		require.True(t, kafkaStatIsOK, "Number of produce kafka requests: %d", produceRequestsCount)
+		require.Equal(t, 1, kafkaStat.Count)
+	}
+}
+
+func TestManyProduceRequests(t *testing.T) {
+	skipTestIfKernelNotSupported(t)
+
+	kafka.RunKafkaServer(t, "127.0.0.1", "9092")
+
+	cfg := config.New()
+	cfg.BPFDebug = true
+	// We don't have a way of enabling kafka without http at the moment
+	cfg.EnableHTTPMonitoring = true
+	monitor, err := NewMonitor(cfg, nil, nil, nil)
+	require.NoError(t, err)
+	err = monitor.Start()
+	require.NoError(t, err)
+	defer monitor.Stop()
+
+	seeds := []string{"localhost:9092"}
+	client, err := kgo.NewClient(
+		kgo.SeedBrokers(seeds...),
+		kgo.DefaultProduceTopic(defaultTopicName),
+		kgo.MaxVersions(kversion.V2_5_0()),
+	)
+	require.NoError(t, err)
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	err = client.Ping(ctxTimeout)
+	cancel()
+	defer client.Close()
+	require.NoError(t, err)
+
+	// Create the topic
+	adminClient := kadm.NewClient(client)
+	ctxTimeout, cancel = context.WithTimeout(context.Background(), time.Second*5)
+	_, err = adminClient.CreateTopics(ctxTimeout, 1, 1, nil, defaultTopicName)
+	cancel()
+	require.NoError(t, err)
+
+	numberOfIterations := 1000
+	for i := 1; i <= numberOfIterations; i++ {
+		record := &kgo.Record{Topic: defaultTopicName, Value: []byte("Hello Kafka!")}
+		ctxTimeout, cancel = context.WithTimeout(context.Background(), time.Second*5)
+		err = client.ProduceSync(ctxTimeout, record).FirstErr()
+		cancel()
+		require.NoError(t, err, "record had a produce error while synchronously producing")
+	}
+
+	// Wait for the kafka monitor to process the Kafka traffic
+	time.Sleep(time.Second * 2)
+
+	kafkaStats := monitor.GetKafkaStats()
+	// We expect 2 occurrences for each connection as we are working with a docker for now
+	require.Equal(t, 2, len(kafkaStats))
+	for kafkaKey, kafkaStat := range kafkaStats {
+		if kafkaKey.RequestAPIKey != kafka.ProduceAPIKey {
+			require.FailNow(t, "Expecting only produce requests")
+		}
+		require.Equal(t, numberOfIterations, kafkaStat.Count)
 	}
 }
