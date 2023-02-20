36df3e03ec2957207528accffca7260a6edd7a0d
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2023-02-20 20:23:18 +0100

    on-cpu

--- pkg/collector/corechecks/oracle/activity.go
+++ pkg/collector/corechecks/oracle/activity.go
@@ -60,32 +60,34 @@ const ACTIVITY_QUERY = `SELECT
     wait_class,
 	sql_text
 FROM sys.dd_session
-WHERE module != 'datadog agent' and action != 'session sampling' or module is null or action is null`
+WHERE 
+	( module != 'datadog agent' and action != 'session sampling' or module is null or action is null) 
+	AND NOT (state = 'WAITING' and wait_class = 'Idle')`
 
 type OracleActivityRow struct {
-	SessionID             int64  `db:"SID" json:"sid,omitempty"`
-	SessionSerial         int64  `db:"SERIAL#" json:"serial,omitempty"`
-	Username              string `db:"USERNAME" json:"username,omitempty"`
-	OsUser                string `db:"OSUSER" json:"os_user,omitempty"`
-	Process               *int64 `db:"PROCESS" json:"process,omitempty"`
-	Machine               string `db:"MACHINE" json:"machine,omitempty"`
-	Program               string `db:"PROGRAM" json:"program,omitempty"`
-	Type                  string `db:"TYPE" json:"type,omitempty"`
-	SqlID                 string `db:"SQL_ID" json:"sql_id,omitempty"`
-	SqlPlanHashValue      int64  `db:"SQL_PLAN_HASH_VALUE" json:"sql_plan_hash_value,omitempty"`
-	SqlExecStart          string `db:"SQL_EXEC_START" json:"sql_exec_start,omitempty"`
-	Module                string `db:"MODULE" json:"module,omitempty"`
-	Action                string `db:"ACTION" json:"action,omitempty"`
-	ClientInfo            string `db:"CLIENT_INFO" json:"client_info,omitempty"`
-	LogonTime             string `db:"LOGON_TIME" json:"logon_time,omitempty"`
-	ClientIdentifier      string `db:"CLIENT_IDENTIFIER" json:"client_identifier,omitempty"`
-	BlockingInstance      *int64 `db:"BLOCKING_INSTANCE" json:"blocking_instance,omitempty"`
-	BlockingSession       *int64 `db:"BLOCKING_SESSION" json:"blocking_session,omitempty"`
-	FinalBlockingInstance *int64 `db:"FINAL_BLOCKING_INSTANCE" json:"final_blocking_instance,omitempty"`
-	FinalBlockingSession  *int64 `db:"FINAL_BLOCKING_SESSION" json:"final_blocking_session,omitempty"`
-	Event                 string `db:"EVENT" json:"event,omitempty"`
-	WaitClass             string `db:"WAIT_CLASS" json:"wait_class,omitempty"`
-	SqlText               string `db:"SQL_TEXT" json:"sql_text,omitempty"`
+	SessionID             int64   `db:"SID" json:"sid,omitempty"`
+	SessionSerial         int64   `db:"SERIAL#" json:"serial,omitempty"`
+	Username              *string `db:"USERNAME" json:"username,omitempty"`
+	OsUser                *string `db:"OSUSER" json:"os_user,omitempty"`
+	Process               *string `db:"PROCESS" json:"process,omitempty"`
+	Machine               *string `db:"MACHINE" json:"machine,omitempty"`
+	Program               *string `db:"PROGRAM" json:"program,omitempty"`
+	Type                  *string `db:"TYPE" json:"type,omitempty"`
+	SqlID                 *string `db:"SQL_ID" json:"sql_id,omitempty"`
+	SqlPlanHashValue      *int64  `db:"SQL_PLAN_HASH_VALUE" json:"sql_plan_hash_value,omitempty"`
+	SqlExecStart          *string `db:"SQL_EXEC_START" json:"sql_exec_start,omitempty"`
+	Module                *string `db:"MODULE" json:"module,omitempty"`
+	Action                *string `db:"ACTION" json:"action,omitempty"`
+	ClientInfo            *string `db:"CLIENT_INFO" json:"client_info,omitempty"`
+	LogonTime             *string `db:"LOGON_TIME" json:"logon_time,omitempty"`
+	ClientIdentifier      *string `db:"CLIENT_IDENTIFIER" json:"client_identifier,omitempty"`
+	BlockingInstance      *int64  `db:"BLOCKING_INSTANCE" json:"blocking_instance,omitempty"`
+	BlockingSession       *int64  `db:"BLOCKING_SESSION" json:"blocking_session,omitempty"`
+	FinalBlockingInstance *int64  `db:"FINAL_BLOCKING_INSTANCE" json:"final_blocking_instance,omitempty"`
+	FinalBlockingSession  *int64  `db:"FINAL_BLOCKING_SESSION" json:"final_blocking_session,omitempty"`
+	Event                 *string `db:"EVENT" json:"event,omitempty"`
+	WaitClass             *string `db:"WAIT_CLASS" json:"wait_class,omitempty"`
+	SqlText               *string `db:"SQL_TEXT" json:"sql_text,omitempty"`
 }
 
 // Metadata contains the metadata fields common to all events processed
