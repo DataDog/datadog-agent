80ba09aa3653d1bfd99f70e99b2530efd006d621
Florent Clarret <florent.clarret@datadoghq.com>
2023-02-22 14:21:52 +0100

    add missing env

--- omnibus/config/software/librdkafka.rb
+++ omnibus/config/software/librdkafka.rb
@@ -13,7 +13,13 @@ build do
   license_file "https://raw.githubusercontent.com/confluentinc/librdkafka/master/LICENSE"
 
   # https://github.com/confluentinc/librdkafka#build-from-source
-  command "./configure --prefix=#{install_dir}/embedded"
+  env = {
+    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+  }
+
+  command "./configure --prefix=#{install_dir}/embedded", :env => env
   command "make", :env => { "LD_RUN_PATH" => "#{install_dir}/embedded/lib" }
   command "make install", :env => { "LD_RUN_PATH" => "#{install_dir}/embedded/lib" }
 
