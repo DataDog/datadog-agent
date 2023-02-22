2d3ef4abb325e8c59271c369c86579b9cb6aafde
Florent Clarret <florent.clarret@datadoghq.com>
2023-02-22 13:42:54 +0100

    add stf flag

--- omnibus/config/software/confluent-kafka-python.rb
+++ omnibus/config/software/confluent-kafka-python.rb
@@ -22,5 +22,5 @@ build do
     pip = "#{install_dir}/embedded/bin/pip3"
   end
 
-  command "#{pip} install --no-binary confluent-kafka ."
+  command "#{pip} install -std=gnu99 --no-binary confluent-kafka ."
 end
