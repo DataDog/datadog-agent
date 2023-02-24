94ffc21ebeac23604ce0e268db1145003e2c2ba7
Julien Lebot <julien.lebot@datadoghq.com>
2023-02-24 15:44:11 +0100

    Rename component names in the Custom Setup dialog to be consistent with the current installer

--- tools/windows/DatadogAgentInstaller/WixSetup/Datadog/AgentFeatures.cs
+++ tools/windows/DatadogAgentInstaller/WixSetup/Datadog/AgentFeatures.cs
@@ -4,14 +4,17 @@ namespace WixSetup.Datadog
 {
     public class AgentFeatures
     {
+        public const string MainApplicationName = "Datadog Agent";
+        public const string NpmFeatureName = "Network Performance Monitoring";
+
         public Feature MainApplication { get; }
 
         public Feature Npm { get; }
 
         public AgentFeatures()
         {
-            Npm = new Feature("NPM", description: "Network Performance Monitoring", isEnabled: false, allowChange: true, configurableDir: "PROJECTLOCATION");
-            MainApplication = new Feature("MainApplication", description: "Datadog Agent", isEnabled: true, allowChange: false, configurableDir: "PROJECTLOCATION");
+            Npm = new Feature(NpmFeatureName, description: NpmFeatureName, isEnabled: false, allowChange: true, configurableDir: "PROJECTLOCATION");
+            MainApplication = new Feature(MainApplicationName, description: MainApplicationName, isEnabled: true, allowChange: false, configurableDir: "PROJECTLOCATION");
             MainApplication.Children.Add(Npm);
         }
     }
