79c28addc23e35cd67771ba39b7df336d6f0ed99
AJ Stuyvenberg <astuyve@gmail.com>
2023-02-24 10:54:13 -0500

    feat: Honor enhanced metrics env var
    fix: Use constant instead of passing a string

--- pkg/serverless/metrics/enhanced_metrics.go
+++ pkg/serverless/metrics/enhanced_metrics.go
@@ -7,6 +7,7 @@ package metrics
 
 import (
 	"math"
+	"os"
 	"strings"
 	"time"
 
@@ -41,6 +42,7 @@ const (
 	// ErrorsMetric is the name of the errors enhanced Lambda metric
 	ErrorsMetric      = "aws.lambda.enhanced.errors"
 	invocationsMetric = "aws.lambda.enhanced.invocations"
+	enhancedMetricsEnvVar = "DD_ENHANCED_METRICS"
 )
 
 func getOutOfMemorySubstrings() []string {
@@ -223,6 +225,10 @@ func SendInvocationEnhancedMetric(tags []string, demux aggregator.Demultiplexer)
 
 // incrementEnhancedMetric sends an enhanced metric with a value of 1 to the metrics channel
 func incrementEnhancedMetric(name string, tags []string, timestamp float64, demux aggregator.Demultiplexer) {
+	// TODO - pass config here, instead of directly looking up var
+	if (strings.ToLower(os.Getenv(enhancedMetricsEnvVar)) == "false") {
+	    return
+	}
 	demux.AggregateSample(metrics.MetricSample{
 		Name:       name,
 		Value:      1.0,
