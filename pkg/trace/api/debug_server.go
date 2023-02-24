95a41e1703765a15defe0cd9e17863edfcf6f6f4
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-02-24 15:05:33 -0800

    Disable debug trace agent server in serverless.

--- pkg/trace/api/debug_server.go
+++ pkg/trace/api/debug_server.go
@@ -3,6 +3,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
+//go:build !serverless
+// +build !serverless
+
 package api
 
 import (
