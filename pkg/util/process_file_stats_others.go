851c9f20aec7c777ebc25f83e151bf062db6ed49
morgan-wang <96082814+mwdd146980@users.noreply.github.com>
2023-02-24 15:08:36 -0500

    copying over ErrNotImplemented into process_file_stats_others.go

--- pkg/util/process_file_stats_others.go
+++ pkg/util/process_file_stats_others.go
@@ -8,6 +8,13 @@
 
 package util
 
+import "errors"
+
+// ErrNotImplemented is the "not implemented" error given by `gopsutil` when an
+// OS doesn't support and API. Unfortunately it's in an internal package so
+// we can't import it so we'll copy it here.
+var ErrNotImplemented = errors.New("not implemented yet")
+
 func GetProcessFileStats() (*ProcessFileStats, error) {
-	return nil, util.ErrNotImplemented
+	return nil, ErrNotImplemented
 }
