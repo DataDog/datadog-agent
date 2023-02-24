f5f3a4b65f56ead33bb40dd42418a6528ca52f78
Florent Clarret <florent.clarret@datadoghq.com>
2023-02-24 16:34:31 +0100

    remove the std flag

--- omnibus/config/software/confluent-kafka-python.rb
+++ omnibus/config/software/confluent-kafka-python.rb
@@ -16,7 +16,7 @@ build do
   license_file "./LICENSE.txt"
 
   build_env = {
-    "CFLAGS" => "-I#{install_dir}/embedded/include -std=c99"
+    "CFLAGS" => "-I#{install_dir}/embedded/include"
   }
 
   if windows?
