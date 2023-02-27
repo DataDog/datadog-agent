5f0d25451518b86e278507fbf2cbd9ff76daba4a
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-02-27 14:14:21 +0100

    extract flags and options parsing to secl lib

---
+++ pkg/security/secl/args/parse.go
@@ -0,0 +1,63 @@
+package args
+
+import (
+	"strings"
+
+	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
+)
+
+func ParseProcessFlags(args []string) []string {
+	flags := make([]string, 0)
+	for _, arg := range args {
+		if len(arg) > 1 && arg[0] == '-' {
+			isFlag := true
+			name := arg[1:]
+			if len(name) >= 1 && name[0] == '-' {
+				name = name[1:]
+				isFlag = false
+			}
+
+			isOption := false
+			for _, r := range name {
+				isFlag = isFlag && model.IsAlphaNumeric(r)
+				isOption = isOption || r == '='
+			}
+
+			if len(name) > 0 {
+				if isFlag {
+					for _, r := range name {
+						flags = append(flags, string(r))
+					}
+				}
+				if !isOption && len(name) > 1 {
+					flags = append(flags, name)
+				}
+			}
+		}
+	}
+	return flags
+}
+
+func ParseProcessOptions(args []string) []string {
+	options := make([]string, 0)
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		if len(arg) > 1 && arg[0] == '-' {
+			name := arg[1:]
+			if len(name) >= 1 && name[0] == '-' {
+				name = name[1:]
+			}
+			if len(name) > 0 && model.IsAlphaNumeric(rune(name[0])) {
+				if index := strings.IndexRune(name, '='); index == -1 {
+					if i < len(args)-1 && (len(args[i+1]) == 0 || args[i+1][0] != '-') {
+						options = append(options, name+"="+args[i+1])
+						i++
+					}
+				} else {
+					options = append(options, name)
+				}
+			}
+		}
+	}
+	return options
+}
