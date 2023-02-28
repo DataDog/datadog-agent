1ca257c9aa9063408f09ba4b355c6c6798340a1b
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2023-02-27 18:16:49 -0600

    Add test for offset guessing

---
+++ pkg/network/ebpf/c/runtime/offsetguess-test.c
@@ -0,0 +1,141 @@
+#include "kconfig.h"
+#include "bpf_tracing.h"
+#include "map-defs.h"
+
+#include <net/net_namespace.h>
+#include <net/sock.h>
+#include <net/inet_sock.h>
+#include <net/flow.h>
+#include <linux/tcp.h>
+
+typedef enum {
+    OFFSET_SADDR = 0,
+    OFFSET_DADDR,
+    OFFSET_SPORT,
+    OFFSET_DPORT,
+    OFFSET_NETNS,
+    OFFSET_INO,
+    OFFSET_FAMILY,
+    OFFSET_RTT,
+    OFFSET_RTTVAR,
+    OFFSET_DADDR_IPV6,
+    OFFSET_SADDR_FL4,
+    OFFSET_DADDR_FL4,
+    OFFSET_SPORT_FL4,
+    OFFSET_DPORT_FL4,
+    OFFSET_SADDR_FL6,
+    OFFSET_DADDR_FL6,
+    OFFSET_SPORT_FL6,
+    OFFSET_DPORT_FL6,
+    OFFSET_SOCKET_SK,
+    OFFSET_SK_BUFF_SOCK,
+    OFFSET_SK_BUFF_TRANSPORT_HEADER,
+    OFFSET_SK_BUFF_HEAD,
+} offset_t;
+
+BPF_HASH_MAP(offsets, offset_t, u64, 1024)
+
+SEC("kprobe/tcp_getsockopt")
+int kprobe__tcp_getsockopt(struct pt_regs* ctx) {
+    u64 offset = 0;
+    offset_t o = OFFSET_SADDR;
+    offset = offsetof(struct sock, sk_rcv_saddr);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_DADDR;
+    offset = offsetof(struct sock, sk_daddr);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_FAMILY;
+    offset = offsetof(struct sock, sk_family);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SPORT;
+    offset = offsetof(struct inet_sock, inet_sport);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_DPORT;
+    offset = offsetof(struct sock, sk_dport);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+#ifdef CONFIG_NET_NS
+    o = OFFSET_NETNS;
+    offset = offsetof(struct sock, sk_net);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+    o = OFFSET_INO;
+#if defined(_LINUX_NS_COMMON_H)
+    offset = offsetof(struct net, ns);
+    offset += offsetof(struct ns_common, inum);
+#else
+    offset = offsetof(struct net, proc_inum);
+#endif
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+#endif
+
+    o = OFFSET_RTT;
+    offset = offsetof(struct tcp_sock, srtt_us);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_RTTVAR;
+    offset = offsetof(struct tcp_sock, mdev_us);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_DADDR_IPV6;
+    offset = offsetof(struct sock, sk_v6_daddr);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SADDR_FL4;
+    offset = offsetof(struct flowi4, saddr);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_DADDR_FL4;
+    offset = offsetof(struct flowi4, daddr);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SPORT_FL4;
+    offset = offsetof(struct flowi4, fl4_sport);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_DPORT_FL4;
+    offset = offsetof(struct flowi4, fl4_dport);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SADDR_FL6;
+    offset = offsetof(struct flowi6, saddr);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_DADDR_FL6;
+    offset = offsetof(struct flowi6, daddr);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SPORT_FL6;
+    offset = offsetof(struct flowi6, fl6_sport);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_DPORT_FL6;
+    offset = offsetof(struct flowi6, fl6_dport);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SOCKET_SK;
+    offset = offsetof(struct socket, sk);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SK_BUFF_SOCK;
+    offset = offsetof(struct sk_buff, sk);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SK_BUFF_TRANSPORT_HEADER;
+    offset = offsetof(struct sk_buff, network_header) - 2;
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    o = OFFSET_SK_BUFF_HEAD;
+    offset = offsetof(struct sk_buff, head);
+    bpf_map_update_elem(&offsets, &o, &offset, BPF_ANY);
+
+    return 0;
+}
+
+// This number will be interpreted by elf-loader to set the current running kernel version
+__u32 _version SEC("version") = 0xFFFFFFFE; // NOLINT(bugprone-reserved-identifier)
+
+char _license[] SEC("license") = "GPL"; // NOLINT(bugprone-reserved-identifier)
---
+++ pkg/network/tracer/offsetguess_test.go
@@ -0,0 +1,179 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build linux_bpf
+// +build linux_bpf
+
+package tracer
+
+import (
+	"math"
+	"net"
+	"testing"
+	"unsafe"
+
+	"github.com/cilium/ebpf"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/unix"
+
+	"github.com/DataDog/datadog-agent/pkg/ebpf/bytecode/runtime"
+	netebpf "github.com/DataDog/datadog-agent/pkg/network/ebpf"
+	"github.com/DataDog/datadog-agent/pkg/process/statsd"
+	manager "github.com/DataDog/ebpf-manager"
+)
+
+//go:generate go run ../../../pkg/ebpf/include_headers.go ../../../pkg/network/ebpf/c/runtime/offsetguess-test.c ../../../pkg/ebpf/bytecode/build/runtime/offsetguess-test.c ../../../pkg/ebpf/c ../../../pkg/ebpf/c/protocols ../../../pkg/network/ebpf/c/runtime ../../../pkg/network/ebpf/c
+//go:generate go run ../../../pkg/ebpf/bytecode/runtime/integrity.go ../../../pkg/ebpf/bytecode/build/runtime/offsetguess-test.c ../../../pkg/ebpf/bytecode/runtime/offsetguess.go runtime
+
+const (
+	offsetSaddr int = iota
+	offsetDaddr
+	offsetSport
+	offsetDport
+	offsetNetns
+	offsetIno
+	offsetFamily
+	offsetRtt
+	offsetRttVar
+	offsetDaddrIpv6
+	offsetSaddrFl4
+	offsetDaddrFl4
+	offsetSportFl4
+	offsetDportFl4
+	offsetSaddrFl6
+	offsetDaddrFl6
+	offsetSportFl6
+	offsetDportFl6
+	offsetSocketSk
+	offsetSkBuffSock
+	offsetSkBuffTransportHeader
+	offsetSkBuffHead
+	offsetMax
+)
+
+func TestOffsetGuess(t *testing.T) {
+	cfg := testConfig()
+	if !cfg.EnableRuntimeCompiler {
+		t.Skip("runtime compilation is not enabled")
+	}
+
+	offsetBuf, err := netebpf.ReadOffsetBPFModule(cfg.BPFDir, cfg.BPFDebug)
+	require.NoError(t, err, "could not read offset bpf module")
+	t.Cleanup(func() { offsetBuf.Close() })
+	_consts, err := runOffsetGuessing(cfg, offsetBuf)
+	require.NoError(t, err)
+	consts := map[int]uint64{}
+	for _, c := range _consts {
+		value := c.Value.(uint64)
+		switch c.Name {
+		case "offset_saddr":
+			consts[offsetSaddr] = value
+		case "offset_daddr":
+			consts[offsetDaddr] = value
+		case "offset_sport":
+			consts[offsetSport] = value
+		case "offset_dport":
+			consts[offsetDport] = value
+		case "offset_netns":
+			consts[offsetNetns] = value
+		case "offset_ino":
+			consts[offsetIno] = value
+		case "offset_family":
+			consts[offsetFamily] = value
+		case "offset_rtt":
+			consts[offsetRtt] = value
+		case "offset_rtt_var":
+			consts[offsetRttVar] = value
+		case "offset_daddr_ipv6":
+			consts[offsetDaddrIpv6] = value
+		case "offset_saddr_fl4":
+			consts[offsetSaddrFl4] = value
+		case "offset_daddr_fl4":
+			consts[offsetDaddrFl4] = value
+		case "offset_sport_fl4":
+			consts[offsetSportFl4] = value
+		case "offset_dport_fl4":
+			consts[offsetDportFl4] = value
+		case "offset_saddr_fl6":
+			consts[offsetSaddrFl6] = value
+		case "offset_daddr_fl6":
+			consts[offsetDaddrFl6] = value
+		case "offset_sport_fl6":
+			consts[offsetSportFl6] = value
+		case "offset_dport_fl6":
+			consts[offsetDportFl6] = value
+		case "offset_socket_sk":
+			consts[offsetSocketSk] = value
+		case "offset_sk_buff_sock":
+			consts[offsetSkBuffSock] = value
+		case "offset_sk_buff_transport_header":
+			consts[offsetSkBuffTransportHeader] = value
+		case "offset_sk_buff_head":
+			consts[offsetSkBuffHead] = value
+		}
+	}
+
+	buf, err := runtime.OffsetguessTest.Compile(&cfg.Config, getCFlags(cfg), statsd.Client)
+	require.NoError(t, err)
+	defer buf.Close()
+
+	mgr := &manager.Manager{
+		Maps: []*manager.Map{
+			{Name: "offsets"},
+		},
+		PerfMaps: []*manager.PerfMap{},
+		Probes: []*manager.Probe{
+			{
+				ProbeIdentificationPair: manager.ProbeIdentificationPair{
+					EBPFFuncName: "kprobe__tcp_getsockopt",
+					UID:          "offsetguess",
+				},
+			},
+		},
+	}
+
+	opts := manager.Options{
+		// Extend RLIMIT_MEMLOCK (8) size
+		// On some systems, the default for RLIMIT_MEMLOCK may be as low as 64 bytes.
+		// This will result in an EPERM (Operation not permitted) error, when trying to create an eBPF map
+		// using bpf(2) with BPF_MAP_CREATE.
+		//
+		// We are setting the limit to infinity until we have a better handle on the true requirements.
+		RLimit: &unix.Rlimit{
+			Cur: math.MaxUint64,
+			Max: math.MaxUint64,
+		},
+		MapEditors: make(map[string]*ebpf.Map),
+	}
+
+	require.NoError(t, mgr.InitWithOptions(buf, opts))
+	require.NoError(t, mgr.Start())
+	t.Cleanup(func() { mgr.Stop(manager.CleanAll) })
+
+	server := NewTCPServer(func(c net.Conn) {})
+	require.NoError(t, server.Run())
+	t.Cleanup(func() { server.Shutdown() })
+
+	c, err := net.Dial("tcp4", server.address)
+	require.NoError(t, err)
+	t.Cleanup(func() { c.Close() })
+
+	f, err := c.(*net.TCPConn).File()
+	require.NoError(t, err)
+	t.Cleanup(func() { f.Close() })
+	_, err = unix.GetsockoptByte(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_INFO)
+	require.NoError(t, err)
+
+	mp, _, err := mgr.GetMap("offsets")
+	require.NoError(t, err)
+
+	for o := offsetSaddr; o < offsetMax; o++ {
+		var offset uint64
+		var name int = o
+		require.NoError(t, mp.Lookup(unsafe.Pointer(&name), unsafe.Pointer(&offset)))
+		assert.Equal(t, offset, consts[o], "unexpected offset for %d", o)
+	}
+}
