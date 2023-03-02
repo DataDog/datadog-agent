3dc1041f8d876a4e8f7670706ef24325f2790f9c
Sylvain Baubeau <lebauce@gmail.com>
2023-03-02 03:08:10 +0100

    Fix copyright

--- pkg/util/trivy/report.go
+++ pkg/util/trivy/report.go
@@ -1,3 +1,13 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Imported from https://github.com/aquasecurity/trivy/blob/main/pkg/fanal/image/daemon/image.go
+
+//go:build trivy
+// +build trivy
+
 package trivy
 
 import (
