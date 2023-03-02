7fa7ee8de75e802180bb27ac569892db59bf9088
Julien Lebot <julien.lebot@datadoghq.com>
2023-03-02 12:37:55 +0100

    Use a different property for debugging properties before install

--- tools/windows/DatadogAgentInstaller/WixSetup/Datadog/AgentInstaller.cs
+++ tools/windows/DatadogAgentInstaller/WixSetup/Datadog/AgentInstaller.cs
@@ -228,7 +228,7 @@ namespace WixSetup.Datadog
 
             project.ResolveWildCards(pruneEmptyDirectories: true);
 
-#if DEBUG
+#if DEBUG_PROPERTIES
             project.BeforeInstall += args =>
             {
                 var installed = args.Session.Property("Installed");
