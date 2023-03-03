99c7f30c1d736912c142fe9e4d4b741aab44b5fd
AliDatadog <125997632+AliDatadog@users.noreply.github.com>
2023-03-03 10:16:30 +0100

    use log.Printf instead of Println
    Co-authored-by: Lénaïc Huard <L3n41c@users.noreply.github.com>

--- test/benchmarks/kubernetes_state/main.go
+++ test/benchmarks/kubernetes_state/main.go
@@ -233,7 +233,7 @@ func main() {
 	fmt.Printf("KSMCheck.Run() returned %v in %s\n", err, elapsed)
 
 	if err = file.Close(); err != nil {
-		log.Println("failed to close \"cpuprofile.pprof\": %v\n", err)
+		log.Printf("failed to close \"cpuprofile.pprof\": %v\n", err)
 		return
 	}
 }
