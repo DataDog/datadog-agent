4aed3e60930cbc87aa22e9c9a70e758cf03c02e2
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-03-03 21:52:00 +0100

    windowsfixup

--- pkg/network/encoding/encoding_test.go
+++ pkg/network/encoding/encoding_test.go
@@ -138,6 +138,20 @@ func getExpectedConnections(encodedWithQueryType bool, httpOutBlob []byte) *mode
 		},
 		Tags: network.GetStaticTags(tagOpenSSL | tagTLS),
 	}
+	// fixup Protocol stack as on windows
+	// we don't have tags mechanism inserting TLS protocol on protocol stack
+	if runtime.GOOS == "windows" {
+		for _, c := range out.Conns {
+			stack := []model.ProtocolType{}
+			for _, p := range c.Protocol.Stack {
+				if p == model.ProtocolType_protocolTLS {
+					continue
+				}
+				stack = append(stack, p)
+			}
+			c.Protocol.Stack = stack
+		}
+	}
 	sort.Strings(out.Tags)
 	if runtime.GOOS == "linux" {
 		out.Conns[1].Tags = []uint32{0, 1}
