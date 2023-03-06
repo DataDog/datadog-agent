31b3b8e8e22ab65233c3d2daf61eeff7da88cc67
AliDatadog <ali.benabdallah@datadoghq.com>
2023-03-06 15:24:11 +0000

    linting

--- Dockerfiles/agent/test_gbi.py
+++ Dockerfiles/agent/test_gbi.py
@@ -6,36 +6,42 @@ import subprocess
 import unittest
 import yaml
 
-CIS_FILE='/tmp/cis.yaml'
-COMMAND_TEST='commandTests'
-FILE_EXISTENCE_TESTS='fileExistenceTests'
+CIS_FILE = '/tmp/cis.yaml'
+COMMAND_TEST = 'commandTests'
+FILE_EXISTENCE_TESTS = 'fileExistenceTests'
+
 
 def create_tests(func_name, file, testCategory):
     def decorator(cls):
         func = getattr(cls, func_name)
         with open(file, 'r') as f:
             data = yaml.safe_load(f)[testCategory]
-            for i, params in enumerate(data):
+            for _, params in enumerate(data):
+
                 def tmp(params=params):
-                    def wrapper(self, *args, **kwargs):
-                        return func(self, **params)      
+                    def wrapper(self):
+                        return func(self, **params)
+
                     return wrapper
+
                 test_name = 'test_' + params['name'].replace(" ", "_").lower()
                 setattr(cls, test_name, tmp())
         setattr(cls, func_name, None)
         return cls
+
     return decorator
+
 
 @create_tests('test_file_existence', CIS_FILE, FILE_EXISTENCE_TESTS)
 @create_tests('test_commands', CIS_FILE, COMMAND_TEST)
 class TestGoldenBaseImage(unittest.TestCase):
-    def test_file_existence(self, name, path, shouldExist):
-        if shouldExist: 
+    def test_file_existence(self, path, shouldExist, **kwargs):
+        if shouldExist:
             self.assertTrue(os.path.isfile(path), path + " should be present")
-        else: 
+        else:
             self.assertFalse(os.path.isfile(path), path + " should NOT be present")
 
-    def test_commands(self, name, command, args, exitCode = 0, expectedOutput = None):
+    def test_commands(self, command, args, exitCode=0, expectedOutput=None, **kwargs):
         full_command = [command] + args
         p = subprocess.run(full_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         self.assertEqual(exitCode, p.returncode)
@@ -43,6 +49,7 @@ class TestGoldenBaseImage(unittest.TestCase):
             expectedOutput = ''.join(expectedOutput)
             output = p.stdout.decode().strip()
             self.assertEqual(expectedOutput, output)
+
 
 if __name__ == "__main__":
     unittest.main()
