03f11d7af2a246de5fab0b7b0c961dd422dfea34
grantseltzer <grantseltzer@gmail.com>
2023-03-08 13:08:25 -0800

    Start of dynamic instrumentation system probe module
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

---
+++ pkg/dynamic-instrumentation/doc.go
@@ -0,0 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package dynamicinstrumentation encapsulates a system-probe module which uses uprobes and bpf
+// to exfiltrate data from running processes
+package dynamicinstrumentation
