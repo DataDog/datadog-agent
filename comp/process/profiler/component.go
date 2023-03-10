43dacf805a29da730dac9b46ca5938ac0900b2fb
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-03-10 11:40:16 -0500

    Fix linter errors

--- comp/process/profiler/component.go
+++ comp/process/profiler/component.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-present Datadog, Inc.
+// Copyright 2023-present Datadog, Inc.
 
 // Package profiler implements a component to handle starting and stopping the internal profiler.
 package profiler
