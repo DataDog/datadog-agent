8d8d274f623c71bdf85b3372d5bdf8761d45e188
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-03-13 15:12:49 +0100

    Add copyright

--- comp/process/hostinfo/component.go
+++ comp/process/hostinfo/component.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023-present Datadog, Inc.
+
 // Package hostinfo wraps the hostinfo inside a component. This is useful because it is relied on by other components.
 package hostinfo
 
