a0a2a25cf86daaa75d408aa2b7915b28833c4236
Brian Floersch <brian.floersch@datadoghq.com>
2023-03-15 13:29:25 -0400

    Revert file

--- pkg/network/protocols/http/debugging/debugging.go
+++ pkg/network/protocols/http/debugging/debugging.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package serverDebugging
+package debugging
 
 import (
 	"github.com/DataDog/sketches-go/ddsketch"
