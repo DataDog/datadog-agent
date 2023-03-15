df1ea7ffd33be0dd23ef9df7d7411f3375f39709
AliDatadog <ali.benabdallah@datadoghq.com>
2023-03-15 15:52:34 +0000

    remove trivy update introduced by merging with master

--- pkg/util/trivy/trivy.go
+++ pkg/util/trivy/trivy.go
@@ -80,7 +80,7 @@ func DefaultCollectorConfig(enabledAnalyzers []string, cacheLocation string) Col
 		ClearCacheOnClose: true,
 	}
 
-	collectorConfig.CacheProvider = cacheProvider(cacheLocation, false)
+	collectorConfig.CacheProvider = cacheProvider(cacheLocation, true)
 
 	if len(enabledAnalyzers) == 1 && enabledAnalyzers[0] == OSAnalyzers {
 		collectorConfig.ArtifactOption.OnlyDirs = []string{"etc", "var/lib/dpkg", "var/lib/rpm", "lib/apk"}
