e3e64bcb0e3bcf3f1b8fdfec28814414db18b4aa
Steven Blumenthal <steven.blumenthal@datadoghq.com>
2023-03-17 08:58:46 -0400

    Check if pod FinishedAt field is non-zero before displaying

--- pkg/workloadmeta/types.go
+++ pkg/workloadmeta/types.go
@@ -532,7 +532,9 @@ func (p KubernetesPod) String(verbose bool) string {
 		_, _ = fmt.Fprintln(&sb, "PVCs:", sliceToString(p.PersistentVolumeClaimNames))
 		_, _ = fmt.Fprintln(&sb, "Kube Services:", sliceToString(p.KubeServices))
 		_, _ = fmt.Fprintln(&sb, "Namespace Labels:", mapToString(p.NamespaceLabels))
-		_, _ = fmt.Fprintln(&sb, "Finished At:", p.FinishedAt)
+		if !p.FinishedAt.IsZero() {
+			_, _ = fmt.Fprintln(&sb, "Finished At:", p.FinishedAt)
+		}
 	}
 
 	return sb.String()
