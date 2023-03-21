1400b5c4eee69d2d6a6610a1b6dd9089b38626af
Brian Floersch <brian.floersch@datadoghq.com>
2023-03-21 14:31:59 -0400

    PR feedback + missing docs

--- comp/dogstatsd/replay/component.go
+++ comp/dogstatsd/replay/component.go
@@ -18,12 +18,29 @@ import (
 
 // Component is the component type.
 type Component interface {
+	// TODO: (components) we should remove the configure method once Dogsatasd's lifecycle is managed by FX (start/stop)
+	// Because Dogstatsd is failable at runtime, we can't yet rely on fx to configure and start up the replay feature
+	// since it has no way of knowing if dogstatsd succeeded to start.
 	Configure() error
+
+	// IsOngoing returns whether a capture is ongoing for this TrafficCapture instance.
 	IsOngoing() bool
+
+	// Start starts a TrafficCapture and returns an error in the event of an issue.
 	Start(p string, d time.Duration, compressed bool) (string, error)
+
+	// Stop stops an ongoing TrafficCapture.
 	Stop()
+
+	// TODO: (components) pool manager should be injected as a component in the future.
+	// RegisterSharedPoolManager registers the shared pool manager with the TrafficCapture.
 	RegisterSharedPoolManager(p *packets.PoolManager) error
+
+	// TODO: (components) pool manager should be injected as a component in the future.
+	// RegisterOOBPoolManager registers the OOB shared pool manager with the TrafficCapture.f
 	RegisterOOBPoolManager(p *packets.PoolManager) error
+
+	// Enqueue enqueues a capture buffer so it's written to file.
 	Enqueue(msg *CaptureBuffer) bool
 }
 
