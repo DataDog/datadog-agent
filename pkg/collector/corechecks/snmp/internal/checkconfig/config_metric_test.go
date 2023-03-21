47cd41aa465fb376c2a4d10d50588661d23156e7
Thibaud Cheruy <thibaud.cheruy@datadoghq.com>
2023-03-21 10:19:33 +0100

    Implement SNMP profile metric tag value mapping

--- pkg/collector/corechecks/snmp/internal/checkconfig/config_metric_test.go
+++ pkg/collector/corechecks/snmp/internal/checkconfig/config_metric_test.go
@@ -43,3 +43,54 @@ func Test_normalizeRegexReplaceValue(t *testing.T) {
 		})
 	}
 }
+
+func Test_getMappedValue(t *testing.T) {
+	tests := []struct {
+		val                 string
+		mapping             map[string]string
+		expectedMappedValue string
+		expectedError       string
+	}{
+		{
+			"1",
+			map[string]string{
+				"1": "one",
+			},
+			"one",
+			"",
+		},
+		{
+			"2",
+			map[string]string{
+				"1": "one",
+				"2": "two",
+			},
+			"two",
+			"",
+		},
+		{
+			"3",
+			map[string]string{
+				"1": "one",
+				"2": "two",
+			},
+			"",
+			"mapping for `3` does not exist. mapping=`map[1:one 2:two]`",
+		},
+		{
+			"4",
+			map[string]string{},
+			"4",
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.val, func(t *testing.T) {
+			mappedValue, err := GetMappedValue(tt.val, tt.mapping)
+			assert.Equal(t, tt.expectedMappedValue, mappedValue)
+			if tt.expectedError != "" {
+				assert.EqualError(t, err, tt.expectedError)
+			}
+		})
+	}
+}
