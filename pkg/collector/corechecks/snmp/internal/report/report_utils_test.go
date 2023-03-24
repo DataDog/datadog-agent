3187a9f5e7f6144bfd3c2dc06c54faba3d5059b4
NouhaManai <nouha.manai@yahoo.fr>
2023-03-24 16:21:39 +0100

    remove prefix when formatting

--- pkg/collector/corechecks/snmp/internal/report/report_utils_test.go
+++ pkg/collector/corechecks/snmp/internal/report/report_utils_test.go
@@ -811,4 +811,5 @@ func Test_getIpAddressFromHexDecimal(t *testing.T) {
 	assert.Equal(t, "10.128.27.197", getIpAddressFromHexDecimal("0A 80 1B C5"))
 	assert.Equal(t, "10.128.27.197", getIpAddressFromHexDecimal("0A801BC5"))
 	assert.Equal(t, "10.128.27.197", getIpAddressFromHexDecimal("10.128.27.197"))
+	assert.Equal(t, "10.5.14.134", getIpAddressFromHexDecimal("0x0a050e86"))
 }
