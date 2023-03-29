355b11a31a93832759c94de8e3d626c94c23edef
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-03-29 10:02:35 -0400

    Clean up

--- omnibus/config/software/confluent-kafka-python.rb
+++ omnibus/config/software/confluent-kafka-python.rb
@@ -4,7 +4,6 @@ name "confluent-kafka-python"
 default_version "2.0.2"
 
 dependency "pip3"
-dependency "librdkafka"
 
 source :url => "https://github.com/confluentinc/confluent-kafka-python/archive/refs/tags/v#{version}.tar.gz",
        :sha256 => "137c82d23e931e03a2803569d51bb025a7b9a364818b08d1664800abaaa5cc63",
@@ -17,15 +16,11 @@ build do
   license_file "./LICENSE.txt"
 
   if windows?
-    librd_dir = "c:\\librdkafka-redist"
-    build_env = {
-        "INCLUDE" => "#{librd_dir}\\librdkafka.redist.#{version}\\build\\native\\include",
-        "LIB" => "#{librd_dir}\\librdkafka.redist.#{version}\\build\\native\\lib\\win\\x64\\win-x64-Release\\v142"
-    }
-    
     pip = "#{windows_safe_path(python_3_embedded)}\\Scripts\\pip.exe"
     command "#{pip} install confluent-kafka", :env => build_env
   else
+    dependency "librdkafka"
+    
     build_env = {
       "CFLAGS" => "-I#{install_dir}/embedded/include -std=c99"
     }
