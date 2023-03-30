0cf26ccae53c74aecc795cef8e1c20b940614c75
Jesse Gumz <jdgumz@users.noreply.github.com>
2023-03-30 15:06:03 -0700

    Update cmd/trace-agent/config/config.go
    Co-authored-by: Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>

--- cmd/trace-agent/config/config.go
+++ cmd/trace-agent/config/config.go
@@ -193,12 +193,8 @@ func applyDatadogConfig(c *config.AgentConfig) error {
 	if coreconfig.Datadog.IsSet("apm_config.connection_limit") {
 		c.ConnectionLimit = coreconfig.Datadog.GetInt("apm_config.connection_limit")
 	}
-	if coreconfig.Datadog.IsSet("apm_config.peer_service_stats_aggregation") {
-		c.PeerServiceStatsAggregation = coreconfig.Datadog.GetBool("apm_config.peer_service_stats_aggregation")
-	}
-	if coreconfig.Datadog.IsSet("apm_config.compute_stats_by_span_kind") {
-		c.ComputeStatsBySpanKind = coreconfig.Datadog.GetBool("apm_config.compute_stats_by_span_kind")
-	}
+	c.PeerServiceStatsAggregation = coreconfig.Datadog.GetBool("apm_config.peer_service_stats_aggregation")
+	c.ComputeStatsBySpanKind = coreconfig.Datadog.GetBool("apm_config.compute_stats_by_span_kind")
 	if coreconfig.Datadog.IsSet("apm_config.extra_sample_rate") {
 		c.ExtraSampleRate = coreconfig.Datadog.GetFloat64("apm_config.extra_sample_rate")
 	}
