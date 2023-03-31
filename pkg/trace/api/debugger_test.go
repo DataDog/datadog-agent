c8ee554b99e13333e340836feaf4950857cadf67
Oskar Jung <oskar.jung@datadoghq.com>
2023-03-31 16:35:37 +0200

    Use assert instead of t.Fatal

--- pkg/trace/api/debugger_test.go
+++ pkg/trace/api/debugger_test.go
@@ -22,44 +22,28 @@ import (
 func TestDebuggerProxy(t *testing.T) {
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		slurp, err := io.ReadAll(req.Body)
-		_ = req.Body.Close()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if body := string(slurp); body != "body" {
-			t.Fatalf("invalid request body: %q", body)
-		}
-		if v := req.Header.Get("DD-API-KEY"); v != "123" {
-			t.Fatalf("got invalid API key: %q", v)
-		}
-		if query := req.URL.RawQuery; query != "ddtags=key%3Aval" {
-			t.Fatalf("got invalid query params: %q", query)
-		}
+		assert.NoError(t, err)
+		err = req.Body.Close()
+		assert.NoError(t, err)
+		body := string(slurp)
+		assert.Equal(t, "body", string(slurp), "invalid request body: %q", body)
+		assert.Equal(t, "123", req.Header.Get("DD-API-KEY"), "got invalid API key: %q", req.Header.Get("DD-API-KEY"))
+		assert.Equal(t, "ddtags=key%3Aval", req.URL.RawQuery, "got invalid query params: %q", req.URL.Query())
 		_, err = w.Write([]byte("OK"))
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 	}))
 	u, err := url.Parse(srv.URL)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	req, err := http.NewRequest("POST", "dummy.com/path", strings.NewReader("body"))
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.NoError(t, err)
 	rec := httptest.NewRecorder()
 	c := &traceconfig.AgentConfig{}
 	newDebuggerProxy(c, []*url.URL{u}, []string{"123"}, "key:val").ServeHTTP(rec, req)
 	result := rec.Result()
 	slurp, err := io.ReadAll(result.Body)
 	result.Body.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if string(slurp) != "OK" {
-		t.Fatal("did not proxy")
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, "OK", string(slurp), "did not proxy")
 }
 
 func TestDebuggerProxyHandler(t *testing.T) {
@@ -67,76 +51,54 @@ func TestDebuggerProxyHandler(t *testing.T) {
 		var called bool
 		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			ddtags := req.URL.Query().Get("ddtags")
-			if strings.Contains(ddtags, "orchestrator") {
-				t.Fatalf("ddtags should not contain orchestrator: %v", ddtags)
-			}
+			assert.False(t, strings.Contains(ddtags, "orchestrator"), "ddtags should not contain orchestrator: %v", ddtags)
 			for _, tag := range []string{"host", "default_env", "agent_version"} {
-				if !strings.Contains(ddtags, tag) {
-					t.Fatalf("ddtags should contain %s", tag)
-				}
+				assert.True(t, strings.Contains(ddtags, tag), "ddtags should contain %s", tag)
 			}
 			called = true
 		}))
 		req, err := http.NewRequest("POST", "/some/path", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 		conf := getConf()
 		conf.Hostname = "myhost"
 		conf.DebuggerProxy.DDURL = srv.URL
 		receiver := newTestReceiverFromConfig(conf)
 		receiver.debuggerProxyHandler().ServeHTTP(httptest.NewRecorder(), req)
-		if !called {
-			t.Fatal("request not proxied")
-		}
+		assert.True(t, called, "request not proxied")
 	})
 
 	t.Run("ok_fargate", func(t *testing.T) {
 		var called bool
 		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			ddtags := req.URL.Query().Get("ddtags")
-			if !strings.Contains(ddtags, "orchestrator:fargate_orchestrator") {
-				t.Fatalf("invalid ddtags, fargate env should contain '%s' tag: %q", "orchestrator", ddtags)
-			}
+			assert.True(t, strings.Contains(ddtags, "orchestrator"), "ddtags must contain orchestrator: %v", ddtags)
 			called = true
 		}))
 		req, err := http.NewRequest("POST", "/some/path", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 		conf := newTestReceiverConfig()
 		conf.Hostname = "myhost"
 		conf.FargateOrchestrator = "orchestrator"
 		conf.DebuggerProxy.DDURL = srv.URL
 		receiver := newTestReceiverFromConfig(conf)
 		receiver.debuggerProxyHandler().ServeHTTP(httptest.NewRecorder(), req)
-		if !called {
-			t.Fatal("request not proxied")
-		}
+		assert.True(t, called, "request not proxied")
 	})
 
 	t.Run("error", func(t *testing.T) {
 		req, err := http.NewRequest("POST", "/some/path", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 		rec := httptest.NewRecorder()
 		conf := newTestReceiverConfig()
 		conf.Site = "asd:\r\n"
 		r := newTestReceiverFromConfig(conf)
 		r.debuggerProxyHandler().ServeHTTP(rec, req)
 		res := rec.Result()
-		if res.StatusCode != http.StatusInternalServerError {
-			t.Fatalf("invalid response: %s", res.Status)
-		}
+		assert.Equal(t, http.StatusInternalServerError, res.StatusCode, "invalid response: %s", res.Status)
 		slurp, err := io.ReadAll(res.Body)
 		res.Body.Close()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !strings.Contains(string(slurp), "error parsing debugger intake URL") {
-			t.Fatalf("invalid message: %q", slurp)
-		}
+		assert.NoError(t, err)
+		assert.Contains(t, string(slurp), "error parsing debugger intake URL", "invalid message: %q", string(slurp))
 	})
 
 	t.Run("ok_additional_endpoints", func(t *testing.T) {
@@ -145,12 +107,7 @@ func TestDebuggerProxyHandler(t *testing.T) {
 		conf := getConf()
 		for i := 0; i < numEndpoints; i++ {
 			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-				ddtags := req.URL.Query().Get("ddtags")
-				for _, tag := range []string{"host", "default_env", "agent_version"} {
-					if !strings.Contains(ddtags, tag) {
-						t.Fatalf("ddtags should contain %s", tag)
-					}
-				}
+				assert.Equal(t, "ddtags=host%3Amyhost%2Cdefault_env%3Atest%2Cagent_version%3Av1", req.URL.RawQuery, "got invalid query params: %q", req.URL.Query())
 				numCalls.Add(1)
 			}))
 			if i == 0 {
@@ -160,16 +117,12 @@ func TestDebuggerProxyHandler(t *testing.T) {
 			conf.DebuggerProxy.AdditionalEndpoints[srv.URL] = []string{"foo"}
 		}
 		req, err := http.NewRequest("POST", "/some/path", strings.NewReader("body"))
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 		receiver := newTestReceiverFromConfig(conf)
 		receiver.debuggerProxyHandler().ServeHTTP(httptest.NewRecorder(), req)
 		var expected atomic.Int32
 		expected.Store(int32(numEndpoints))
-		if numCalls != expected {
-			t.Fatalf("requests not proxied, expected %d calls, got %d", numEndpoints, numCalls)
-		}
+		assert.Equal(t, expected, numCalls, "requests not proxied, expected %d calls, got %d", numEndpoints, numCalls)
 	})
 
 	t.Run("error_additional_endpoints_main_endpoint_error", func(t *testing.T) {
@@ -192,9 +145,7 @@ func TestDebuggerProxyHandler(t *testing.T) {
 			conf.DebuggerProxy.AdditionalEndpoints[srv.URL] = []string{"foo"}
 		}
 		req, err := http.NewRequest("POST", "/some/path", strings.NewReader("body"))
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 		receiver := newTestReceiverFromConfig(conf)
 		recorder := httptest.NewRecorder()
 		receiver.debuggerProxyHandler().ServeHTTP(recorder, req)
@@ -223,9 +174,7 @@ func TestDebuggerProxyHandler(t *testing.T) {
 			conf.DebuggerProxy.AdditionalEndpoints[srv.URL] = []string{"foo"}
 		}
 		req, err := http.NewRequest("POST", "/some/path", strings.NewReader("body"))
-		if err != nil {
-			t.Fatal(err)
-		}
+		assert.NoError(t, err)
 		receiver := newTestReceiverFromConfig(conf)
 		recorder := httptest.NewRecorder()
 		receiver.debuggerProxyHandler().ServeHTTP(recorder, req)
