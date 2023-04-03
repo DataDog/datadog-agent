2a25544145302eb4a1de9141178731097a0fcbf2
AliDatadog <ali.benabdallah@datadoghq.com>
2023-04-03 12:06:07 +0000

    put back build flags

--- pkg/util/trivy/db.go
+++ pkg/util/trivy/db.go
@@ -3,6 +3,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build trivy
+// +build trivy
+
 package trivy
 
 import (
