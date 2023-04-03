a9916aeba2bec476d597febd24f72a5463eb15ad
usamasaqib <usamasaqib.96@live.com>
2023-04-03 15:14:44 +0200

    flush buffer to file

--- test/new-e2e/system-probe/system-probe-test-env.go
+++ test/new-e2e/system-probe/system-probe-test-env.go
@@ -64,9 +64,9 @@ func outputsToFile(output auto.OutputMap) error {
 	w := bufio.NewWriter(f)
 
 	for key, value := range output {
-		fmt.Printf("writing: %s: %s\n", key, value.Value.(string))
 		fmt.Fprintf(w, "%s: %s\n", key, value.Value.(string))
 	}
+	w.Flush()
 
 	return nil
 }
