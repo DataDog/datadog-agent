85751162c0224b728b69e2f001a38cebe93e133f
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-04-05 18:10:06 -0400

    Fix comma

--- omnibus/config/software/lmdb.rb
+++ omnibus/config/software/lmdb.rb
@@ -1,8 +1,8 @@
 name "lmdb"
 default_version "0.9.29"
 
-source :sha512 => "f75d5751ed97c1b7f982379988719f566efbf8df2d4c8894764f49c2eb926e3918844bc1e7d88e8b278e1c949ad75940f2404816ce345e74cf94d36645143b05"
-       :url => "https://src.fedoraproject.org/repo/pkgs/lmdb/LMDB_#{version}.tar.gz/sha512/#{sha512}/LMDB_#{version}.tar.gz"
+source :sha512 => "f75d5751ed97c1b7f982379988719f566efbf8df2d4c8894764f49c2eb926e3918844bc1e7d88e8b278e1c949ad75940f2404816ce345e74cf94d36645143b05",
+       :url => "https://src.fedoraproject.org/repo/pkgs/lmdb/LMDB_#{version}.tar.gz/sha512/f75d5751ed97c1b7f982379988719f566efbf8df2d4c8894764f49c2eb926e3918844bc1e7d88e8b278e1c949ad75940f2404816ce345e74cf94d36645143b05/LMDB_#{version}.tar.gz"
 
 relative_path "lmdb-#{version}"
 
