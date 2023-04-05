80e56e32fdea27841c326c086be6a6d7f75b125f
Olivier G <52180542+ogaca-dd@users.noreply.github.com>
2023-04-05 18:17:52 +0200

    Move mock config overrides to MockParam (#16386)

--- comp/core/log/logger_test.go
+++ comp/core/log/logger_test.go
@@ -17,7 +17,6 @@ import (
 func TestLogging(t *testing.T) {
 	log := fxutil.Test[Component](t, fx.Options(
 		fx.Supply(LogForOneShot("TEST", "debug", false)),
-		fx.Supply(config.Params{}),
 		config.MockModule,
 		Module,
 	))
