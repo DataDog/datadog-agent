14ea53d0b6dd026231a9f4e85001018676c2ae0d
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-04-06 09:37:47 -0400

    Make lmdb from source

--- omnibus/config/software/lmdb.rb
+++ omnibus/config/software/lmdb.rb
@@ -1,8 +1,8 @@
 name "lmdb"
-default_version "0.9.29"
+default_version "0.9.22"
 
-source :sha512 => "f75d5751ed97c1b7f982379988719f566efbf8df2d4c8894764f49c2eb926e3918844bc1e7d88e8b278e1c949ad75940f2404816ce345e74cf94d36645143b05",
-       :url => "https://src.fedoraproject.org/repo/pkgs/lmdb/LMDB_#{version}.tar.gz/sha512/f75d5751ed97c1b7f982379988719f566efbf8df2d4c8894764f49c2eb926e3918844bc1e7d88e8b278e1c949ad75940f2404816ce345e74cf94d36645143b05/LMDB_#{version}.tar.gz",
+source :url => "https://github.com/LMDB/lmdb/archive/LMDB_#{version}.tar.gz",
+       :md5 => "64c6132f481281b7b2ad746ecbfb8423",
        :extract => :seven_zip
 
 relative_path "lmdb-#{version}"
@@ -16,11 +16,8 @@ build do
         "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
     }
 
-    configure_command = ["./configure",
-                          "--prefix=#{install_dir}/embedded"]  
-  
-    command configure_command.join(" "), env: env
     command "make", :env => env
+    command "sed -i 's| liblmdb.a||' Makefile" :env => env
     command "make install", :env => env
 
 end
