d45434b7ebae96392be1a2e24721f7d815712c13
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2023-04-06 11:57:26 +0200

    chore(ci): add gohai to module list

--- tasks/modules.py
+++ tasks/modules.py
@@ -128,6 +128,7 @@ DEFAULT_MODULES = {
     "test/new-e2e": GoModule("test/new-e2e", condition=lambda: False, should_tag=False),
     "test/fakeintake": GoModule("test/fakeintake", independent=True, should_tag=False),
     "pkg/obfuscate": GoModule("pkg/obfuscate", independent=True),
+    "pkg/gohai": GoModule("pkg/gohai", independent=True),
     "pkg/trace": GoModule("pkg/trace", independent=True),
     "pkg/security/secl": GoModule("pkg/security/secl", independent=True),
     "pkg/remoteconfig/state": GoModule("pkg/remoteconfig/state", independent=True),
