4224ad1871617c059675a40126a29d350a0ae51a
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-04-06 10:52:13 -0400

    Try different relative_path

--- omnibus/config/software/lmdb.rb
+++ omnibus/config/software/lmdb.rb
@@ -5,7 +5,7 @@ source :url => "https://github.com/LMDB/lmdb/archive/LMDB_#{version}.tar.gz",
        :sha256 => "22054926b426c66d8f2bc22071365df6e35f3aacf19ad943bc6167d4cae3bebb",
        :extract => :seven_zip
 
-relative_path "lmdb-#{version}"
+relative_path "libraries/liblmdb"
 
 build do
     license ""
@@ -16,6 +16,7 @@ build do
         "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
     }
 
+    # https://www.linuxfromscratch.org/blfs/view/8.3/server/lmdb.html
     command "make", :env => env
     command "sed -i 's| liblmdb.a||' Makefile", :env => env
     command "make install", :env => env
