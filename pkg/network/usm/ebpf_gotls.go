f428cc081bf069beab0932385cfe4565834484b6
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-04-06 20:56:40 +0200

    gotls-unregisterProcess

--- pkg/network/usm/ebpf_gotls.go
+++ pkg/network/usm/ebpf_gotls.go
@@ -396,13 +396,15 @@ func (p *GoTLSProgram) registerProcess(binID binaryID, pid pid, mTime syscall.Ti
 }
 
 func (p *GoTLSProgram) unregisterProcess(pid pid) {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-
+	p.lock.RLock()
 	binID, found := p.processes[pid]
+	p.lock.RUnlock()
 	if !found {
 		return
 	}
+
+	p.lock.Lock()
+	defer p.lock.Unlock()
 	delete(p.processes, pid)
 
 	bin, found := p.binaries[binID]
