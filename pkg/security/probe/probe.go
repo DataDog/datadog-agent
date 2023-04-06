e7bcc802734e323f4253df07757fc5410fbc047e
Celia Yuen <celia.yuen@datadoghq.com>
2023-04-06 10:39:55 -0400

    multipe rulesets

--- pkg/security/probe/probe.go
+++ pkg/security/probe/probe.go
@@ -1043,8 +1043,7 @@ func (p *Probe) validEventTypeForConfig(eventType string) bool {
 	return true
 }
 
-// updateProbes applies the loaded set of rules and returns a report
-// of the applied approvers for it.
+// updateProbes applies the loaded set of rules and returns a report of the applied approvers for it. TODO(Celia): I don't think it returns a report of approvers anymore
 func (p *Probe) updateProbes(ruleEventTypes []eval.EventType) error {
 	// event types enabled either by event handlers or by rules
 	eventTypes := append([]eval.EventType{}, defaultEventTypes...)
@@ -1233,8 +1232,8 @@ func (p *Probe) GetDebugStats() map[string]interface{} {
 	return debug
 }
 
-// NewRuleSet returns a new rule set
-func (p *Probe) NewRuleSet(eventTypeEnabled map[eval.EventType]bool) *rules.RuleSet {
+// EvaluationSet returns a new evaluation set
+func (p *Probe) EvaluationSet(eventTypeEnabled map[eval.EventType]bool) *rules.EvaluationSet {
 	ruleOpts, evalOpts := rules.NewEvalOpts(eventTypeEnabled)
 
 	ruleOpts.WithLogger(seclog.DefaultLogger)
@@ -1247,7 +1246,10 @@ func (p *Probe) NewRuleSet(eventTypeEnabled map[eval.EventType]bool) *rules.Rule
 		}
 	}
 
-	return rules.NewRuleSet(NewModel(p), eventCtor, ruleOpts, evalOpts)
+	defaultRuleSet := rules.NewRuleSet(NewModel(p), eventCtor, ruleOpts, evalOpts)
+	threatScoreRuleSet := rules.NewRuleSet(NewModel(p), eventCtor, ruleOpts, evalOpts)
+
+	return rules.NewEvaluationSet(defaultRuleSet, threatScoreRuleSet)
 }
 
 // QueuedNetworkDeviceError is used to indicate that the new network device was queued until its namespace handle is
@@ -1349,7 +1351,7 @@ func (p *Probe) applyDefaultFilterPolicies() {
 }
 
 // ApplyRuleSet setup the filters for the provided set of rules and returns the policy report.
-func (p *Probe) ApplyRuleSet(rs *rules.RuleSet) (*kfilters.ApplyRuleSetReport, error) {
+func (p *Probe) ApplyRuleSet(rs *rules.RuleSet, isThreatScore bool) (*kfilters.ApplyRuleSetReport, error) {
 	ars, err := kfilters.NewApplyRuleSetReport(p.Config.Probe, rs)
 	if err != nil {
 		return nil, err
