3773641c803f535aba2a91df364e748fb9bc8b10
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-07 11:45:33 +0200

    black format

--- tasks/build_tags.py
+++ tasks/build_tags.py
@@ -40,7 +40,7 @@ ALL_TAGS = {
     "secrets",
     "systemd",
     "trivy",
-    "vtprotocodec", # used by system-probe to gate the registration of the vtprotobuf gRPC codec
+    "vtprotocodec",  # used by system-probe to gate the registration of the vtprotobuf gRPC codec
     "zk",
     "zlib",
     "test",  # used for unit-tests
@@ -118,7 +118,9 @@ PROCESS_AGENT_HEROKU_TAGS = PROCESS_AGENT_TAGS.difference(
 SECURITY_AGENT_TAGS = {"netcgo", "secrets", "docker", "containerd", "kubeapiserver", "kubelet", "podman", "zlib"}
 
 # SYSTEM_PROBE_TAGS lists the tags necessary to build system-probe
-SYSTEM_PROBE_TAGS = AGENT_TAGS.union({"clusterchecks", "linux_bpf", "npm", "vtprotocodec"}).difference({"python", "trivy"})
+SYSTEM_PROBE_TAGS = AGENT_TAGS.union({"clusterchecks", "linux_bpf", "npm", "vtprotocodec"}).difference(
+    {"python", "trivy"}
+)
 
 # TRACE_AGENT_TAGS lists the tags that have to be added when the trace-agent
 TRACE_AGENT_TAGS = {"docker", "containerd", "kubeapiserver", "kubelet", "otlp", "netcgo", "podman", "secrets"}
