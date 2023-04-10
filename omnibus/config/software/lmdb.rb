dd8bc5c961501a5804743a355e74dad5e4c85fde
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-04-10 18:17:06 -0400

    Make first and then copy

--- omnibus/config/software/lmdb.rb
+++ omnibus/config/software/lmdb.rb
@@ -10,21 +10,23 @@ relative_path "lmdb-LMDB_#{version}/libraries/liblmdb"
 build do
     license "OpenLDAP Public License"
     license_file "https://raw.githubusercontent.com/LMDB/lmdb/LMDB_#{version}/libraries/liblmdb/COPYRIGHT"
-    # env = {
-    #     "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    #     "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    #     "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-    # }
+    env = {
+        "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+        "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+        "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+    }
 
     # # https://www.linuxfromscratch.org/blfs/view/8.3/server/lmdb.html
-    # command "make", :env => env
-    # command "sed -i 's| liblmdb.a||' Makefile", :env => env
+    command "make", :env => env
+    command "sed -i 's| liblmdb.a||' Makefile", :env => env
     # command "make install", :env => env
-    # command "mkdir #{install_dir}/embedded/lib/liblmdb"
 
     # We have to manually move the files into the correct directories because the Makefile for lmdb hardcodes the install directory to `/usr/local`, although we need this to be `#{install_dir}/embedded`
-    command "cp /omnibus/src/lmdb/libraries/liblmdb/liblmdb.a #{install_dir}/embedded/lib/"
-    command "cp /omnibus/src/lmdb/libraries/liblmdb/lmdb.h #{install_dir}/embedded/include/"
-    command "cp /omnibus/src/lmdb/libraries/liblmdb/mdb_stat /omnibus/src/lmdb/libraries/liblmdb/mdb_copy /omnibus/src/lmdb/libraries/liblmdb/mdb_dump /omnibus/src/lmdb/libraries/liblmdb/mdb_load #{install_dir}/embedded/bin/"
+    copy "liblmdb.a" "#{install_dir}/embedded/lib/"
+    copy "lmdb.h" "#{install_dir}/embedded/include/"
+    copy "mdb_stat" "#{install_dir}/embedded/bin/"
+    copy "mdb_copy" "#{install_dir}/embedded/bin/"
+    copy "mdb_dump" "#{install_dir}/embedded/bin/"
+    copy "mdb_load" "#{install_dir}/embedded/bin/"
 
 end
