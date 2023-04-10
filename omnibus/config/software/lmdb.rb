80c657fafedd56bd36c946ae6580c3b90510a2b9
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-04-10 15:33:12 -0400

    Copy lmdb files manually

--- omnibus/config/software/lmdb.rb
+++ omnibus/config/software/lmdb.rb
@@ -10,17 +10,24 @@ relative_path "lmdb-LMDB_#{version}/libraries/liblmdb"
 build do
     license "OpenLDAP Public License"
     license_file "https://raw.githubusercontent.com/LMDB/lmdb/LMDB_#{version}/libraries/liblmdb/COPYRIGHT"
-    env = {
-        "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-        "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-        "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-    }
+    # env = {
+    #     "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    #     "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    #     "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+    # }
 
-    command "./configure --prefix=#{install_dir}/embedded", :env => env
-    # https://www.linuxfromscratch.org/blfs/view/8.3/server/lmdb.html
-    command "make", :env => env
-    command "sed -i 's| liblmdb.a||' Makefile", :env => env
-    command "make install", :env => env
+    # # https://www.linuxfromscratch.org/blfs/view/8.3/server/lmdb.html
+    # command "make", :env => env
+    # command "sed -i 's| liblmdb.a||' Makefile", :env => env
+    # command "make install", :env => env
+    # command "mkdir #{install_dir}/embedded/lib/liblmdb"
+
+    # We have to manually move the files into the correct directories because the Makefile for lmdb hardcodes the install directory to `/usr/local`, although we need this to be `#{install_dir}/embedded`
+    command "cp liblmdb.so liblmdb.a #{install_dir}/embedded/lib/"
+    command "cp lmdb.h #{install_dir}/embedded/include/"
+    command "cp mdb_stat mdb_copy mdb_dump mdb_load #{install_dir}/embedded/bin/"
+
+
 
 end
   
