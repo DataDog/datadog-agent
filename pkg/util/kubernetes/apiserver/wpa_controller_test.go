5babefa32d263b1ee7a182293087977dd41e6d40
Lénaïc Huard <lenaic.huard@datadoghq.com>
2023-04-11 14:39:51 +0200

    fixup! Ignore race in `pkg/util/kubernetes/apiserver` tests

--- pkg/util/kubernetes/apiserver/wpa_controller_test.go
+++ pkg/util/kubernetes/apiserver/wpa_controller_test.go
@@ -3,8 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build kubeapiserver
-// +build kubeapiserver
+//go:build !race && kubeapiserver
+// +build !race,kubeapiserver
 
 package apiserver
 
