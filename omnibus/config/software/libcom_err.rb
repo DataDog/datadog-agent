be549ebab5476c4f58444ad86a16c3a5038d9cc5
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-04-11 11:43:27 -0400

    Try only libcom_err

---
+++ omnibus/config/software/libcom_err.rb
@@ -0,0 +1,35 @@
+name "libcom_err"
+default_version "1.47.0"
+
+# We will download and build e2fsprogs, then move libcom_err.so files into the correct library
+source :url => "https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v#{version}/e2fsprogs-#{version}.tar.gz",
+       :sha256 => "0b4fe723d779b0927fb83c9ae709bc7b40f66d7df36433bef143e41c54257084",
+       :extract => :seven_zip
+
+relative_path "e2fsprogs-#{version}"
+
+build do
+  license = "MIT"
+  license_file = "https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git/tree/lib/et/com_err.c"
+
+  env = {
+    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+  }
+
+  # For libcom_err, we need to build e2fsprogs (since libcom_err is a subpackage of it), 
+  # and manually move the contents of libcom_err into the Agent
+  # Build e2fsprogs in a temp directory
+  command "./configure --prefix=#{install_dir}/embedded/temp_dir", :env => env
+  command "make", :env => env
+  command "make install", :env => env
+
+  # Move libcom_err files directly
+  copy "libcom_err.a" "#{install_dir}/embedded/lib/"
+  copy "et/comm_err.h" "#{install_dir}/embedded/include/"
+  copy "bin/compile_et" "#{install_dir}/embedded/bin/"
+
+  # Remove the temp_dir
+  command "rm -rf #{install_dir}/embedded/temp_dir"
+end
