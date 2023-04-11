95db1809d93b87a5dc0f43a91ad91635ecf4ebcc
Pawel Chojnacki <pawelchcki@gmail.com>
2023-04-11 16:03:07 +0200

    cleanup

--- test/benchmarks/apm_scripts/ben-runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-fetch_data_layer_from_registry.py registry.ddbuild.io ci/relenv-microbenchmarking-platform/ben-runner latest-nightly benrunner.tar.gz
-tar xf benrunner.tar.gz 
-
-./ben-runner -l debug execute -f ./test/benchmarks/apm_scripts/latency.yaml
