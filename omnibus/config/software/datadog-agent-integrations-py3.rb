6e366b5e6925506d773142591a3f0cf8d0d388fa
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-04-12 16:50:08 -0400

    Remove extra spacing

--- omnibus/config/software/datadog-agent-integrations-py3.rb
+++ omnibus/config/software/datadog-agent-integrations-py3.rb
@@ -15,7 +15,6 @@ dependency 'setuptools3'
 dependency 'snowflake-connector-python-py3'
 dependency 'confluent-kafka-python'
 
-
 if arm?
   # psycopg2 doesn't come with pre-built wheel on the arm architecture.
   # to compile from source, it requires the `pg_config` executable present on the $PATH
