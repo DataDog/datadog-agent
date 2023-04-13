cf485ce40cd75ce8b574add5d91dab3206e6fc50
Derek Brown <derek.brown@datadoghq.com>
2023-04-13 13:27:05 -0700

    [Windows] Remove normalization from http tracking (#16220)
    * [windows] remove normalizing of http connections
    
    On Windows, removes the notion of "normalized" windows connections.
    Stores all connections by local/remote addr, which allows them to
    be accurately matched to the tcp connection in the connections object.
    
    Resolves problem of localhost connections not being properly reported.
    
    add corresponding driver
    
    add release not
    
    remove no longer necessary files/functions
    
    update to driver that's present
    
    fix missed import (linux)
    
    Fix tests.
    
    One of the tests is fixed in a way that should net
    be merged
    
    Fix more tests
    
    fix missed release.json entry
    
    * Fix final test.
    Remove NATing portion of test since we don't do that on Windows,
    anyway
    
    * update to tagged testsigned driver for signing
    
    * Make corresponding fix to http2 tests
    
    * update to release-signed version of driver
    
    * Complete rebase
    
    * Split out key tests between windows & linux since the behavior is now
    different
    
    * fix linux side missed in previous commit

--- pkg/network/event_common_linux.go
+++ pkg/network/event_common_linux.go
@@ -8,6 +8,10 @@
 
 package network
 
+import (
+	"github.com/DataDog/datadog-agent/pkg/network/types"
+)
+
 // Sub returns s-other.
 //
 // This implementation is different from the implementation on
@@ -50,4 +54,30 @@ func (s StatCounters) Sub(other StatCounters) (sc StatCounters, underflow bool) 
 	}
 
 	return sc, false
+}
+
+// ConnectionKeysFromConnectionStats constructs connection key using the underlying raw connection stats object, which is produced by the tracer.
+// Each ConnectionStats object contains both the source and destination addresses, as well as an IPTranslation object that stores the original addresses in the event that the connection is NAT'd.
+// This function generates all relevant combinations of connection keys: [(source, dest), (dest, source), (NAT'd source, NAT'd dest), (NAT'd dest, NAT'd source)].
+// This is necessary to handle all possible scenarios for connections originating from the USM module (i.e., whether they are NAT'd or not, and whether they use TLS).
+func ConnectionKeysFromConnectionStats(connectionStats ConnectionStats) []types.ConnectionKey {
+
+	// USM data is always indexed as (client, server), but we don't know which is the remote
+	// and which is the local address. To account for this, we'll construct 2 possible
+	// connection keys and check for both of them in the aggregations map.
+	connectionKeys := []types.ConnectionKey{
+		types.NewConnectionKey(connectionStats.Source, connectionStats.Dest, connectionStats.SPort, connectionStats.DPort),
+		types.NewConnectionKey(connectionStats.Dest, connectionStats.Source, connectionStats.DPort, connectionStats.SPort),
+	}
+
+	// if IPTranslation is not nil, at least one of the sides has a translation, thus we need to add translated addresses.
+	if connectionStats.IPTranslation != nil {
+		localAddress, localPort := GetNATLocalAddress(connectionStats)
+		remoteAddress, remotePort := GetNATRemoteAddress(connectionStats)
+		connectionKeys = append(connectionKeys,
+			types.NewConnectionKey(localAddress, remoteAddress, localPort, remotePort),
+			types.NewConnectionKey(remoteAddress, localAddress, remotePort, localPort))
+	}
+
+	return connectionKeys
 }
