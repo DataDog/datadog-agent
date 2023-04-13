DataDog/datadog-agent

commit 7dc5730ef935f2c4671893fdf9bfaa4e73259734

[CWS] Add security profile anomaly detection and event filtering (#16385)

pkg/security/probe/probe.go
@@ -22,6 +22,7 @@ import (
 	"github.com/hashicorp/go-multierror"
 	easyjson "github.com/mailru/easyjson"
 	"github.com/moby/sys/mountinfo"
+	"go.uber.org/atomic"
 	"golang.org/x/exp/slices"
 	"golang.org/x/sys/unix"
 	"golang.org/x/time/rate"
@@ -38,6 +39,7 @@ import (
 	kernel "github.com/DataDog/datadog-agent/pkg/security/ebpf/kernel"
 	"github.com/DataDog/datadog-agent/pkg/security/ebpf/probes"
 	"github.com/DataDog/datadog-agent/pkg/security/events"
+	"github.com/DataDog/datadog-agent/pkg/security/metrics"
 	pconfig "github.com/DataDog/datadog-agent/pkg/security/probe/config"
 	"github.com/DataDog/datadog-agent/pkg/security/probe/constantfetch"
 	"github.com/DataDog/datadog-agent/pkg/security/probe/erpc"
@@ -108,8 +110,9 @@ type Probe struct {
 	wg             sync.WaitGroup
 
 	// Events section
-	eventHandlers       [model.MaxAllEventType][]EventHandler
-	customEventHandlers [model.MaxAllEventType][]CustomEventHandler
+	eventHandlers        [model.MaxAllEventType][]EventHandler
+	customEventHandlers  [model.MaxAllEventType][]CustomEventHandler
+	anomalyDetectionSent map[model.EventType]*atomic.Uint64
 
 	// internals
 	monitor       *Monitor
@@ -359,12 +362,31 @@ func (p *Probe) AddCustomEventHandler(eventType model.EventType, handler CustomE
 	return nil
 }
 
+func (p *Probe) SendAnomalyDetection(event *model.Event) {
+	evtType := event.GetEventType()
+	if evtType != model.DNSEventType &&
+		evtType != model.ExecEventType {
+		return // at least, files, bind, fork and exit
+	}
+
+	p.DispatchCustomEvent(
+		events.NewCustomRule(events.AnomalyDetectionRuleID),
+		events.NewCustomEvent(event.GetEventType(), p.EventMarshallerCtor(event)),
+	)
+	p.anomalyDetectionSent[evtType].Inc()
+}
+
 // DispatchEvent sends an event to the probe event handler
 func (p *Probe) DispatchEvent(event *model.Event) {
 	traceEvent("Dispatching event %s", func() ([]byte, model.EventType, error) {
 		eventJSON, err := serializers.MarshalEvent(event, p.resolvers)
 		return eventJSON, event.GetEventType(), err
 	})
+
+	// filter out event if already present on a profile
+	if p.Config.RuntimeSecurity.SecurityProfileEnabled {
+		p.monitor.securityProfileManager.LookupEventOnProfiles(event)
+	}
 
 	// send wildcard first
 	for _, handler := range p.eventHandlers[model.UnknownEventType] {
@@ -376,8 +398,15 @@ func (p *Probe) DispatchEvent(event *model.Event) {
 		handler.HandleEvent(event)
 	}
 
-	// Process after evaluation because some monitors need the DentryResolver to have been called first.
-	p.monitor.ProcessEvent(event)
+	if event.SecurityProfileContext.Status.IsEnabled(model.AnomalyDetection) && !event.IsInProfile() && event.GetEventType() != model.SyscallsEventType {
+		p.SendAnomalyDetection(event)
+	}
+
+	// if a profile is already present for this event, dont even try to add it to a dump
+	if !event.HasProfile() {
+		// Process after evaluation because some monitors need the DentryResolver to have been called first.
+		p.monitor.ProcessEvent(event)
+	}
 }
 
 // DispatchCustomEvent sends a custom event to the probe event handler
@@ -418,6 +447,15 @@ func traceEvent(fmt string, marshaller func() ([]byte, model.EventType, error)) 
 // SendStats sends statistics about the probe to Datadog
 func (p *Probe) SendStats() error {
 	p.resolvers.TCResolver.SendTCProgramsStats(p.StatsdClient)
+
+	for evtType, count := range p.anomalyDetectionSent {
+		tags := []string{fmt.Sprintf("event_type:%s", evtType)}
+		if value := count.Swap(0); value > 0 {
+			if err := p.StatsdClient.Count(metrics.MetricSecurityProfileAnomalyDetectionSent, int64(value), tags, 1.0); err != nil {
+				return fmt.Errorf("couldn't send MetricSecurityProfileAnomalyDetectionSent metric: %w", err)
+			}
+		}
+	}
 
 	return p.monitor.SendStats()
 }
@@ -883,11 +921,11 @@ func (p *Probe) handleEvent(CPU int, data []byte) {
 	// anomaly detection events
 	if model.IsAnomalyDetectionEvent(event.GetType()) {
 		p.monitor.securityProfileManager.FillProfileContextFromContainerID(event.ProcessContext.ContainerID, &event.SecurityProfileContext)
-
 		p.DispatchCustomEvent(
 			events.NewCustomRule(events.AnomalyDetectionRuleID),
 			events.NewCustomEventLazy(event.GetEventType(), p.EventMarshallerCtor(event)),
 		)
+		p.anomalyDetectionSent[event.GetEventType()].Inc()
 	}
 
 	// flush exited process
@@ -1361,6 +1399,10 @@ func NewProbe(config *config.Config, opts Opts) (*Probe, error) {
 		discarderRateLimiter: rate.NewLimiter(rate.Every(time.Second/5), 100),
 		isRuntimeDiscarded:   !opts.DontDiscardRuntime,
 		event:                &model.Event{},
+		anomalyDetectionSent: make(map[model.EventType]*atomic.Uint64),
+	}
+	for i := model.EventType(0); i < model.MaxKernelEventType; i++ {
+		p.anomalyDetectionSent[i] = atomic.NewUint64(0)
 	}
 
 	if err := p.detectKernelVersion(); err != nil {

pkg/security/metrics/metrics.go
@@ -255,6 +255,12 @@ var (
 	// MetricSecurityProfileCacheMiss is the name of the metric used to report the count of Security Profile cache misses
 	// Tags: -
 	MetricSecurityProfileCacheMiss = newRuntimeMetric(".security_profile.cache.miss")
+	// MetricSecurityProfileAnomalyDetection
+	// Tags: - event_type
+	MetricSecurityProfileAnomalyDetectionSent = newRuntimeMetric(".security_profile.anomaly_detection.sent")
+	// MetricSecurityProfileEventFilteringNoProfile
+	// Tags: - event_type, in_profile ('true' or 'false')
+	MetricSecurityProfileEventFiltering = newRuntimeMetric(".security_profile.evaluation.hit")
 
 	// Namespace resolver metrics
 

pkg/security/module/cws.go
@@ -414,7 +414,9 @@ func (c *CWSConsumer) HandleEvent(event *model.Event) {
 	}
 
 	if ruleSet := c.GetRuleSet(); ruleSet != nil {
-		ruleSet.Evaluate(event)
+		if (event.SecurityProfileContext.Status.IsEnabled(model.AutoSuppression) && event.IsInProfile()) || !ruleSet.Evaluate(event) {
+			ruleSet.EvaluateDiscarders(event)
+		}
 	}
 }
 

pkg/security/probe/discarders_test.go
@@ -377,7 +377,9 @@ func TestIsDiscarderOverride(t *testing.T) {
 	event.SetFieldValue("unlink.file.path", "/var/log/httpd")
 	event.SetFieldValue("process.file.path", "/bin/touch")
 
-	rs.Evaluate(event)
+	if !rs.Evaluate(event) {
+		rs.EvaluateDiscarders(event)
+	}
 
 	if listener.fields["process.file.path"] > 0 {
 		t.Error("shouldn't get a discarder")
@@ -385,7 +387,9 @@ func TestIsDiscarderOverride(t *testing.T) {
 
 	event.SetFieldValue("process.file.path", "/bin/cat")
 
-	rs.Evaluate(event)
+	if !rs.Evaluate(event) {
+		rs.EvaluateDiscarders(event)
+	}
 
 	if listener.fields["process.file.path"] == 0 {
 		t.Error("should get a discarder")

pkg/security/probe/field_handlers.go
@@ -15,6 +15,8 @@ import (
 	"syscall"
 	"time"
 
+	sprocess "github.com/DataDog/datadog-agent/pkg/security/resolvers/process"
+
 	"github.com/DataDog/datadog-agent/pkg/security/resolvers"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/args"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
@@ -183,7 +185,7 @@ func (fh *FieldHandlers) ResolveProcessArgs(ev *model.Event, process *model.Proc
 
 // ResolveProcessArgv resolves the args of the event as an array
 func (fh *FieldHandlers) ResolveProcessArgv(ev *model.Event, process *model.Process) []string {
-	argv, _ := fh.resolvers.ProcessResolver.GetProcessArgv(process)
+	argv, _ := sprocess.GetProcessArgv(process)
 	return argv
 }
 
@@ -195,7 +197,7 @@ func (fh *FieldHandlers) ResolveProcessEnvp(ev *model.Event, process *model.Proc
 
 // ResolveProcessArgsTruncated returns whether the args are truncated
 func (fh *FieldHandlers) ResolveProcessArgsTruncated(ev *model.Event, process *model.Process) bool {
-	_, truncated := fh.resolvers.ProcessResolver.GetProcessArgv(process)
+	_, truncated := sprocess.GetProcessArgv(process)
 	return truncated
 }
 

pkg/security/resolvers/cgroup/model/model.go
@@ -9,6 +9,7 @@
 package model
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 
@@ -25,11 +26,16 @@ type WorkloadSelector struct {
 }
 
 // NewWorkloadSelector returns an initialized instance of a WorkloadSelector
-func NewWorkloadSelector(image string, tag string) WorkloadSelector {
+func NewWorkloadSelector(image string, tag string) (WorkloadSelector, error) {
+	if image == "" {
+		return WorkloadSelector{}, errors.New("no image name provided")
+	} else if tag == "" {
+		tag = "latest"
+	}
 	return WorkloadSelector{
 		Image: image,
 		Tag:   tag,
-	}
+	}, nil
 }
 
 // IsEmpty returns true if the selector is set

pkg/security/resolvers/process/resolver.go
@@ -852,7 +852,7 @@ func (p *Resolver) SetProcessArgs(pce *model.ProcessCacheEntry) {
 }
 
 // GetProcessArgv returns the args of the event as an array
-func (p *Resolver) GetProcessArgv(pr *model.Process) ([]string, bool) {
+func GetProcessArgv(pr *model.Process) ([]string, bool) {
 	if pr.ArgsEntry == nil {
 		return nil, false
 	}
@@ -885,7 +885,7 @@ func (p *Resolver) GetProcessScrubbedArgv(pr *model.Process) ([]string, bool) {
 		return pr.ScrubbedArgv, pr.ScrubbedArgsTruncated
 	}
 
-	argv, truncated := p.GetProcessArgv(pr)
+	argv, truncated := GetProcessArgv(pr)
 
 	if p.scrubber != nil {
 		argv, _ = p.scrubber.ScrubCommand(argv)

pkg/security/secl/model/model.go
@@ -150,10 +150,47 @@ type ContainerContext struct {
 	Tags      []string `field:"tags,handler:ResolveContainerTags,opts:skip_ad,weight:9999"` // SECLDoc[tags] Definition:`Tags of the container`
 }
 
+type Status uint32
+
+const (
+	// AnomalyDetection will trigger alerts each time an event is not part of the profile
+	AnomalyDetection Status = 1 << iota
+	// AutoSuppression will suppress any signal to events present on the profile
+	AutoSuppression
+	// WorkloadHardening will kill the process that triggered anomaly detection
+	WorkloadHardening
+)
+
+func (s Status) IsEnabled(option Status) bool {
+	return (s & option) != 0
+}
+
+func (s Status) String() string {
+	var options []string
+	if s.IsEnabled(AnomalyDetection) {
+		options = append(options, "anomaly_detection")
+	}
+	if s.IsEnabled(AutoSuppression) {
+		options = append(options, "auto_suppression")
+	}
+	if s.IsEnabled(WorkloadHardening) {
+		options = append(options, "workload_hardening")
+	}
+
+	var res string
+	for _, option := range options {
+		if len(res) > 0 {
+			res += ","
+		}
+		res += option
+	}
+	return res
+}
+
 // SecurityProfileContext holds the security context of the profile
 type SecurityProfileContext struct {
 	Name    string   `field:"name"`    // SECLDoc[name] Definition:`Name of the security profile`
-	Status  string   `field:"status"`  // SECLDoc[status] Definition:`Status of the security profile`
+	Status  Status   `field:"status"`  // SECLDoc[status] Definition:`Status of the security profile`
 	Version string   `field:"version"` // SECLDoc[version] Definition:`Version of the security profile`
 	Tags    []string `field:"tags"`    // SECLDoc[tags] Definition:`Tags of the security profile`
 }
@@ -283,6 +320,26 @@ func (e *Event) IsSavedByActivityDumps() bool {
 // IsSavedByActivityDumps return whether AD sample
 func (e *Event) IsActivityDumpSample() bool {
 	return e.Flags&EventFlagsActivityDumpSample > 0
+}
+
+// IsInProfile return true if the event was fount in the profile
+func (e *Event) IsInProfile() bool {
+	return e.Flags&EventFlagsSecurityProfileInProfile > 0
+}
+
+// AddToFlags adds a flag to the event
+func (e *Event) AddToFlags(flag uint32) {
+	e.Flags |= flag
+}
+
+// RemoveFromFlags remove a flag to the event
+func (e *Event) RemoveFromFlags(flag uint32) {
+	e.Flags ^= flag
+}
+
+// HasProfile returns true if we found a profile for that event
+func (e *Event) HasProfile() bool {
+	return e.SecurityProfileContext.Name != ""
 }
 
 // GetType returns the event type

commit 2e3fec6705988a4ba7f9d78054737906e23b67f3

Fix CI test from a regression introduced with #16385

pkg/security/probe/probe.go
@@ -371,7 +371,7 @@ func (p *Probe) SendAnomalyDetection(event *model.Event) {
 
 	p.DispatchCustomEvent(
 		events.NewCustomRule(events.AnomalyDetectionRuleID),
-		events.NewCustomEvent(event.GetEventType(), p.EventMarshallerCtor(event)),
+		events.NewCustomEventLazy(event.GetEventType(), p.EventMarshallerCtor(event)),
 	)
 	p.anomalyDetectionSent[evtType].Inc()
 }