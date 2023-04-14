abc7c39b57cc7ef85fd05fb6663b4575db2c3ccc
AliDatadog <ali.benabdallah@datadoghq.com>
2023-04-14 16:14:07 +0000

    revert changes for check_disk_usage and min_available_disk 2

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1098,8 +1098,6 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("sbom.custom_cache_max_disk_size", 1000*1000*10) // used by custom cache: max disk space used by cached objects. Not equal to max disk usage
 	config.BindEnvAndSetDefault("sbom.custom_cache_max_cache_entries", 1000)     // used by custom cache keys stored in memory
 	config.BindEnvAndSetDefault("sbom.cache_clean_interval", "30m")              // used by custom cache.
-	config.BindEnvAndSetDefault("sbom.check_disk_usage", true)
-	config.BindEnvAndSetDefault("sbom.min_available_disk", "1Gb")
 
 	// Orchestrator Explorer - process agent
 	// DEPRECATED in favor of `orchestrator_explorer.orchestrator_dd_url` setting. If both are set `orchestrator_explorer.orchestrator_dd_url` will take precedence.
@@ -1127,6 +1125,8 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("container_image_collection.sbom.scan_interval", 0)    // Integer seconds
 	config.BindEnvAndSetDefault("container_image_collection.sbom.scan_timeout", 10*60) // Integer seconds
 	config.BindEnvAndSetDefault("container_image_collection.sbom.analyzers", []string{"os"})
+	config.BindEnvAndSetDefault("container_image_collection.sbom.check_disk_usage", true)
+	config.BindEnvAndSetDefault("container_image_collection.sbom.min_available_disk", "1Gb")
 
 	// Datadog security agent (common)
 	config.BindEnvAndSetDefault("security_agent.cmd_port", 5010)
