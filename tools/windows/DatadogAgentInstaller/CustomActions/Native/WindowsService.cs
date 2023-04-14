fe580856aa7eebcf225c6a2e434f314a0504a605
Julien Lebot <julien.lebot@datadoghq.com>
2023-04-14 14:27:36 +0200

    Re-implement start type since it does not exists in .Net 4.5

--- tools/windows/DatadogAgentInstaller/CustomActions/Native/WindowsService.cs
+++ tools/windows/DatadogAgentInstaller/CustomActions/Native/WindowsService.cs
@@ -1,5 +1,8 @@
+using System.Security.AccessControl;
+using System;
 using System.ServiceProcess;
 using Datadog.CustomActions.Interfaces;
+using Microsoft.Win32;
 
 namespace Datadog.CustomActions.Native
 {
@@ -18,6 +21,23 @@ namespace Datadog.CustomActions.Native
 
         public ServiceControllerStatus Status => _service.Status;
 
-        public ServiceStartMode StartType => _service.StartType;
+        public ServiceStartMode StartType
+        {
+            get
+            {
+                using var key = Registry.LocalMachine.OpenSubKey($@"SYSTEM\CurrentControlSet\Services\{_service.ServiceName}", RegistryKeyPermissionCheck.ReadSubTree, RegistryRights.ReadKey);
+                if (key == null)
+                {
+                    throw new ArgumentException("Invalid request of StartType for non-existent service {_service.ServiceName}");
+                }
+
+                if (Enum.TryParse<ServiceStartMode>(key.GetValue("Start").ToString(), out var serviceStartMode))
+                {
+                    return serviceStartMode;
+                }
+
+                throw new Exception($"Unexpected Start value for service {_service.ServiceName}");
+            }
+        }
     }
 }
