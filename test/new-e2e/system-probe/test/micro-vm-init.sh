fb570e1ac024346cb46f41be9c7af0f491469b29
usamasaqib <usamasaqib.96@live.com>
2023-04-14 16:41:31 +0200

    run init script

---
+++ test/new-e2e/system-probe/test/micro-vm-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo xtrace
+
+ARCH=$1
+DEPENDENCIES=dependencies-$ARCH.tar.gz
+
+mv /opt/kernel-version-testing/$DEPENDENCIES /$DEPENDENCIES
+tar xzvf $DEPENDENCIES
+
+systemctl start docker
+
+# Add provisioning steps here
+
+# VM provisioning end
+
+go version
