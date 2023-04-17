ed08fcbd8b44fac8fb0e89f233daffb7557350c7
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-17 21:39:26 +0200

    working unix solution

--- pkg/security/secl/model/field_handlers_unix.go
+++ pkg/security/secl/model/field_handlers_unix.go
@@ -1,5 +1,5 @@
-//go:build linux
-// +build linux
+//go:build unix
+// +build unix
 
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
