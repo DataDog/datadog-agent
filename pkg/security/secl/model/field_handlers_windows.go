7588f216258459ad9001443fc0467d768a523d2b
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-17 21:40:18 +0200

    fix windows field handlers

--- pkg/security/secl/model/field_handlers_windows.go
+++ pkg/security/secl/model/field_handlers_windows.go
@@ -1,5 +1,5 @@
-//go:build !linux
-// +build !linux
+//go:build windows
+// +build windows
 
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
