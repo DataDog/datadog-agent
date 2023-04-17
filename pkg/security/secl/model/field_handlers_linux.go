1f6c93c0671a9fec65d831f42f2ee432023a7ac3
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-17 11:03:00 +0200

    fix build tags

--- pkg/security/secl/model/field_handlers_linux.go
+++ pkg/security/secl/model/field_handlers_linux.go
@@ -1,7 +1,10 @@
+//go:build linux
+// +build linux
+
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2022-present Datadog, Inc.// +build linux
+// Copyright 2022-present Datadog, Inc.
 // Code generated - DO NOT EDIT.
 package model
 
