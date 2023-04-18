97b0ed0097ba986a5cec9b45952f372de99e69c0
Guy Arbitman <guy20495@gmail.com>
2023-04-18 10:24:51 +0300

    usm: sowatcher: Fixed leak
    We registered paths by the number of mappings, but cleanning them by the number of processes.
    A single process might have multiple mappings of the same so, which makes those numbers different

---
+++ pkg/network/protocols/http/testutil/testdata/sowatcher_client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	done := make(chan bool, 1)
+
+	fds := make([]*os.File, len(os.Args)-1)
+	defer func() {
+		for _, fd := range fds {
+			_ = fd.Close()
+		}
+	}()
+	for _, path := range os.Args[1:] {
+		fd, err := os.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		fds = append(fds, fd)
+	}
+
+	go func() {
+		<-sigs
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
