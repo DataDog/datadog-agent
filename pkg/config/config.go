b8d42c215f0b509d3a3ccf66bff99da7cc1833d3
AliDatadog <ali.benabdallah@datadoghq.com>
2023-04-19 14:48:30 +0200

    fix lint

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1097,7 +1097,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("sbom.use_custom_cache", false)
 	config.BindEnvAndSetDefault("sbom.custom_cache_max_disk_size", 1000*1000*100) // used by custom cache: max disk space used by cached objects. Not equal to max disk usage
 	config.BindEnvAndSetDefault("sbom.custom_cache_max_cache_entries", 10000)     // used by custom cache keys stored in memory
-	config.BindEnvAndSetDefault("sbom.cache_clean_interval", "30m")              // used by custom cache.
+	config.BindEnvAndSetDefault("sbom.cache_clean_interval", "30m")               // used by custom cache.
 
 	// Orchestrator Explorer - process agent
 	// DEPRECATED in favor of `orchestrator_explorer.orchestrator_dd_url` setting. If both are set `orchestrator_explorer.orchestrator_dd_url` will take precedence.
