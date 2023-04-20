43576a9dc6e359640754682628cbe8e3038e49f4
Maxime mouial <maxime.mouial@gmail.com>
2023-04-20 14:17:07 +0200

    Send 'agent_checks' payload 1 min after startup
    The 'agent_checks' payload contains the status of every checks run by
    the agent. It's sent every 10 min. We no longer wait 10 min for the first
    payload to be sent. Instead we send the first payload after 1 min and
    every 10 min after that.

--- pkg/metadata/types.go
+++ pkg/metadata/types.go
@@ -7,6 +7,7 @@ package metadata
 
 import (
 	"context"
+	"time"
 
 	"github.com/DataDog/datadog-agent/pkg/serializer"
 )
@@ -18,9 +19,17 @@ type Collector interface {
 	Send(ctx context.Context, s serializer.MetricSerializer) error
 }
 
-// CollectorWithInit is an optional interface that collectors that need to be
+// CollectorWithInit is an optional interface for collectors that need to be
 // initialized can implement. If implemented, the Init method will be called
 // when the collector is scheduled
 type CollectorWithInit interface {
 	Init() error
+}
+
+// CollectorWithFirstRun is an optional interface for collectors that want to send their first payload after a different
+// interval. This allows collectors to send a payload shortly after the Agent starts.
+type CollectorWithFirstRun interface {
+	// FirstRunInterval returns the interval after which to send the first payload. This allows the collector to
+	// send a payload shortly after Agent startup.
+	FirstRunInterval() time.Duration
 }
