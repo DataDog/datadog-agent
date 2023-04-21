3177f8400d3aa1ed160fe9d1c5388db4166ceae1
Derek Brown <derek.brown@datadoghq.com>
2023-04-21 09:03:04 -0700

    Add check for running under emulation on arm (#165)
    * Add check for running under emulation on arm
    
    * add additional constant

--- pkg/gohai/platform/platform_windows.go
+++ pkg/gohai/platform/platform_windows.go
@@ -36,6 +36,8 @@ var (
 	ntdll                              = windows.NewLazyDLL("Ntdll.dll")
 	procRtlGetVersion                  = ntdll.NewProc("RtlGetVersion")
 	winbrand                           = windows.NewLazyDLL("winbrand.dll")
+	kernel32                           = windows.NewLazyDLL("kernel32.dll")
+	procIsWow64Process2                = kernel32.NewProc("IsWow64Process2")
 	ERROR_SUCESS         syscall.Errno = 0
 )
 
@@ -73,6 +75,41 @@ const (
 	SV_TYPE_LOCAL_LIST_ONLY   = uint32(0x40000000) /* Return local list only */
 	SV_TYPE_DOMAIN_ENUM       = uint32(0x80000000)
 	SV_TYPE_ALL               = uint32(0xFFFFFFFF) /* handy for NetServerEnum2 */
+)
+
+const (
+	IMAGE_FILE_MACHINE_UNKNOWN     = uint16(0x0)
+	IMAGE_FILE_MACHINE_TARGET_HOST = uint16(0x0001) // Useful for indicating we want to interact with the host and not a WoW guest.  Win10/2016 and above only
+	IMAGE_FILE_MACHINE_I386        = uint16(0x014c) // Intel 386.
+	IMAGE_FILE_MACHINE_R3000       = uint16(0x0162) // MIPS little-endian, = uint16(0x160 big-endian
+	IMAGE_FILE_MACHINE_R4000       = uint16(0x0166) // MIPS little-endian
+	IMAGE_FILE_MACHINE_R10000      = uint16(0x0168) // MIPS little-endian
+	IMAGE_FILE_MACHINE_WCEMIPSV2   = uint16(0x0169) // MIPS little-endian WCE v2
+	IMAGE_FILE_MACHINE_ALPHA       = uint16(0x0184) // Alpha_AXP
+	IMAGE_FILE_MACHINE_SH3         = uint16(0x01a2) // SH3 little-endian
+	IMAGE_FILE_MACHINE_SH3DSP      = uint16(0x01a3)
+	IMAGE_FILE_MACHINE_SH3E        = uint16(0x01a4) // SH3E little-endian
+	IMAGE_FILE_MACHINE_SH4         = uint16(0x01a6) // SH4 little-endian
+	IMAGE_FILE_MACHINE_SH5         = uint16(0x01a8) // SH5
+	IMAGE_FILE_MACHINE_ARM         = uint16(0x01c0) // ARM Little-Endian
+	IMAGE_FILE_MACHINE_THUMB       = uint16(0x01c2) // ARM Thumb/Thumb-2 Little-Endian
+	IMAGE_FILE_MACHINE_ARMNT       = uint16(0x01c4) // ARM Thumb-2 Little-Endian
+	IMAGE_FILE_MACHINE_AM33        = uint16(0x01d3)
+	IMAGE_FILE_MACHINE_POWERPC     = uint16(0x01F0) // IBM PowerPC Little-Endian
+	IMAGE_FILE_MACHINE_POWERPCFP   = uint16(0x01f1)
+	IMAGE_FILE_MACHINE_IA64        = uint16(0x0200) // Intel 64
+	IMAGE_FILE_MACHINE_MIPS16      = uint16(0x0266) // MIPS
+	IMAGE_FILE_MACHINE_ALPHA64     = uint16(0x0284) // ALPHA64
+	IMAGE_FILE_MACHINE_MIPSFPU     = uint16(0x0366) // MIPS
+	IMAGE_FILE_MACHINE_MIPSFPU16   = uint16(0x0466) // MIPS
+	IMAGE_FILE_MACHINE_AXP64       = IMAGE_FILE_MACHINE_ALPHA64
+	IMAGE_FILE_MACHINE_TRICORE     = uint16(0x0520) // Infineon
+	IMAGE_FILE_MACHINE_CEF         = uint16(0x0CEF)
+	IMAGE_FILE_MACHINE_EBC         = uint16(0x0EBC) // EFI Byte Code
+	IMAGE_FILE_MACHINE_AMD64       = uint16(0x8664) // AMD64 (K8)
+	IMAGE_FILE_MACHINE_M32R        = uint16(0x9041) // M32R little-endian
+	IMAGE_FILE_MACHINE_ARM64       = uint16(0xAA64) // ARM64 Little-Endian
+	IMAGE_FILE_MACHINE_CEE         = uint16(0xC0EE)
 )
 const registryHive = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"
 const productNameKey = "ProductName"
@@ -174,6 +211,41 @@ func fetchWindowsVersion() (major uint64, minor uint64, build uint64, err error)
 	return
 }
 
+// check to see if we're running on syswow64 on another architecture
+// (specifically arm)
+// the function we're going to use (IsWow64Process2) isn't available prior
+// to win10/2016.  Fail gracefully, and assume we're not on wow in that
+// case
+
+func getNativeArchInfo() string {
+	nativearch := "x86_64"
+	if runtime.GOARCH == "amd64" {
+		nativearch = "x86_64"
+	} else {
+		nativearch = runtime.GOARCH
+	}
+	var err error
+	if err = kernel32.Load(); err == nil {
+		if err = procIsWow64Process2.Find(); err == nil {
+			var pmachine uint16
+			var pnative uint16
+			h := windows.CurrentProcess()
+			b, _, _ := procIsWow64Process2.Call(uintptr(h), uintptr(unsafe.Pointer(&pmachine)), uintptr(unsafe.Pointer(&pnative)))
+			if b != uintptr(0) {
+				// check to see the native processor type.
+				switch pnative {
+				case IMAGE_FILE_MACHINE_AMD64:
+					// it's already set to this
+					nativearch = "x86_64"
+				case IMAGE_FILE_MACHINE_ARM64:
+					nativearch = "ARM64"
+				}
+			}
+		}
+	}
+	return nativearch
+}
+
 // GetArchInfo() returns basic host architecture information
 func GetArchInfo() (systemInfo map[string]string, err error) {
 	systemInfo = map[string]string{}
@@ -183,11 +255,7 @@ func GetArchInfo() (systemInfo map[string]string, err error) {
 		systemInfo["hostname"] = hostname
 	}
 
-	if runtime.GOARCH == "amd64" {
-		systemInfo["machine"] = "x86_64"
-	} else {
-		systemInfo["machine"] = runtime.GOARCH
-	}
+	systemInfo["machine"] = getNativeArchInfo()
 
 	osDescription, err := fetchOsDescription()
 	if err == nil {
