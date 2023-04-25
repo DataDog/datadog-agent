e203005f003d1008650e2dd9d3e22109e3b5d4d1
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-04-25 10:16:05 +0200

    [CWS] fix build tags generation in accessors/field handlers generation code (#16654)
    * fix generation of build tags after copyright and code generated line
    
    * cleanup copyrights ignores

--- pkg/security/secl/model/field_handlers_unix.go
+++ pkg/security/secl/model/field_handlers_unix.go
@@ -1,11 +1,12 @@
-//go:build unix
-// +build unix
-
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2022-present Datadog, Inc.
 // Code generated - DO NOT EDIT.
+
+//go:build unix
+// +build unix
+
 package model
 
 // ResolveFields resolves all the fields associate to the event type. Context fields are automatically resolved.
--- pkg/security/secl/model/field_handlers_windows.go
+++ pkg/security/secl/model/field_handlers_windows.go
@@ -1,11 +1,12 @@
-//go:build windows
-// +build windows
-
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2022-present Datadog, Inc.
 // Code generated - DO NOT EDIT.
+
+//go:build windows
+// +build windows
+
 package model
 
 // ResolveFields resolves all the fields associate to the event type. Context fields are automatically resolved.
