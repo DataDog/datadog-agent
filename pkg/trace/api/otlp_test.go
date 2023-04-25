5d147b490729abebc25cc383a8cdce1b547d3476
Gabriel Aszalos <gabriel.aszalos@gmail.com>
2023-04-25 16:39:01 +0200

    pkg/trace/api: fix flaky TestFlatten test (#16515)

--- pkg/trace/api/otlp_test.go
+++ pkg/trace/api/otlp_test.go
@@ -1329,8 +1329,12 @@ func TestOTLPConvertSpan(t *testing.T) {
 }
 
 func TestFlatten(t *testing.T) {
-	assert.Equal(t, flatten(map[string]string{"a": "b", "c": "d"}).String(), "a:b,c:d")
-	assert.Equal(t, flatten(map[string]string{"x": "y"}).String(), "x:y")
+	for _, tt := range []map[string]string{
+		map[string]string{"a": "b", "c": "d"},
+		map[string]string{"x": "y"},
+	} {
+		assert.Equal(t, unflatten(flatten(tt).String()), tt)
+	}
 	assert.Equal(t, flatten(map[string]string{}).String(), "")
 	assert.Equal(t, flatten(nil).String(), "")
 }
