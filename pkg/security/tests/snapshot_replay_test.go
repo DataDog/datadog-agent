e1de8ee3b5c15e8bbfd5be59ea4e440cf2684a05
Momar TOURÉ <momar.toure@datadoghq.com>
2023-04-25 18:59:26 +0200

    Writing tests

---
+++ pkg/security/tests/snapshot_replay_test.go
@@ -0,0 +1,60 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build functionaltests
+// +build functionaltests
+
+package tests
+
+import (
+	"fmt"
+	// "github.com/DataDog/datadog-agent/pkg/security/secl/model"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/rules"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"os/exec"
+	"testing"
+)
+
+func TestSnapshotReplay(t *testing.T) {
+	ncExec := which(t, "nc")
+	cmd := exec.Command(ncExec, "-l", "4242")
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer cmd.Process.Kill()
+
+	ruleDef := &rules.RuleDefinition{
+		ID:         "test_rule",
+		Expression: fmt.Sprintf(`exec.comm in ["socat", "dig", "nslookup", "host", ~"netcat*", ~"nc*", "ncat"] `),
+	}
+
+	test, err := newTestModule(t, nil, []*rules.RuleDefinition{ruleDef}, testOpts{})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer test.Close()
+
+	t.Run("snapshot-replay", func(t *testing.T) {
+		// Check if the process is present in the process resolver's entrycache
+
+		found := false
+		for _, entry := range test.probe.GetResolvers().ProcessResolver.GetEntryCache() {
+			if entry.ProcessContext.Process.Comm == "nc.openbsd" {
+				found = true
+			}
+		}
+		assert.Equal(t, found, true, "ProcessEntryCache found")
+
+		// test.RegisterRuleEventHandler(func(event *model.Event, rule *rules.Rule) {
+		// 	fmt.Println("----------YOYOYOYO----------------------------")
+		// 	assertTriggeredRule(t, rule, "test_rule")
+		// })
+	})
+
+}
