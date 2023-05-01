29ecb5d4e669784ae6f22ead356c2da9b742ab8a
amit.slavin <amit.slavin@datadoghq.com>
2023-05-01 17:07:45 +0300

    added ut to test empty path

--- pkg/network/protocols/http/incomplete_stats_test.go
+++ pkg/network/protocols/http/incomplete_stats_test.go
@@ -111,4 +111,21 @@ func TestHTTP2Path(t *testing.T) {
 		assert.Nil(t, path)
 	})
 
+	t.Run("empty path", func(t *testing.T) {
+		// create a buffer to store the encoded data
+		var buf []byte
+		var arr [30]uint8
+		str := ""
+
+		buf = hpack.AppendHuffmanString(buf, str)
+
+		request := &EbpfHttp2Tx{
+			Request_path: arr,
+			Path_size:    21,
+		}
+		outBuf := make([]byte, 28)
+
+		path, _ := request.Path(outBuf)
+		assert.Nil(t, path)
+	})
 }
