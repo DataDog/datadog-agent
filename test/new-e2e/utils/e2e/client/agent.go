48c3f42ea9a2afb5fc04f8eb303ff1b9cb315585
pducolin <paola.ducolin@datadoghq.com>
2023-05-03 18:30:59 +0200

    [agent] add helper to wait for agent is ready

--- test/new-e2e/utils/e2e/client/agent.go
+++ test/new-e2e/utils/e2e/client/agent.go
@@ -6,7 +6,12 @@
 package client
 
 import (
+	"errors"
+	"regexp"
+	"time"
+
 	"github.com/DataDog/test-infra-definitions/datadog/agent"
+	"github.com/cenkalti/backoff"
 )
 
 var _ stackInitializer = (*Agent)(nil)
@@ -30,6 +35,56 @@ func (agent *Agent) init(auth *Authentification, data *agent.ClientData) error {
 	return err
 }
 
-func (agent *Agent) Status() (string, error) {
-	return agent.sshClient.Execute("sudo datadog-agent status")
+func (agent *Agent) Version() (string, error) {
+	return agent.sshClient.Execute("datadog-agent version")
+}
+
+type Status struct {
+	rawString string
+}
+
+func NewStatus(s string) *Status {
+	return &Status{rawString: s}
+}
+
+func (agent *Agent) Status() (*Status, error) {
+	s, err := agent.sshClient.Execute("sudo datadog-agent status")
+	if err != nil {
+		return nil, err
+	}
+	return NewStatus(s), nil
+}
+
+// IsReady true if status contains a valid version
+func (s *Status) IsReady() (bool, error) {
+	return regexp.MatchString("={15}\nAgent \\(v7\\.\\d{2}\\..*\n={15}", s.rawString)
+}
+
+// IsReady runs status command and returns true if the status is ready
+// Use this to wait for agent to be ready before running any command
+func (a *Agent) IsReady() (bool, error) {
+	status, err := a.Status()
+	if err != nil {
+		return false, err
+	}
+	return status.IsReady()
+}
+
+// WaitForReady blocks up for timeout waiting for agent to be ready
+// Retries every 100 ms up to timeout
+// Returns error on failure
+func (a *Agent) WaitForReady(timeout time.Duration) error {
+	interval := 100 * time.Millisecond
+	maxRetries := timeout.Milliseconds() / interval.Milliseconds()
+	err := backoff.Retry(func() error {
+		isReady, err := a.IsReady()
+		if err != nil {
+			return err
+		}
+		if !isReady {
+			return errors.New("agent not ready")
+		}
+		return nil
+	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(interval), uint64(maxRetries)))
+	return err
 }
