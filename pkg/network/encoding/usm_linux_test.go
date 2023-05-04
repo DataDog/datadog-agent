c92a1b2f2b60365e453edde5a0724c75d24f1a8d
Pedro Lambert <pedrolambert@gmail.com>
2023-05-04 13:16:44 -0400

    Add test case for NAT

--- pkg/network/encoding/usm_linux_test.go
+++ pkg/network/encoding/usm_linux_test.go
@@ -51,4 +51,45 @@ func TestUSMLookup(t *testing.T) {
 		assert.Equal(t, val, USMLookup(c2, data))
 	})
 
+	t.Run("with NAT", func(t *testing.T) {
+		key := types.NewConnectionKey(
+			util.AddressFromString("3.3.3.3"),
+			util.AddressFromString("4.4.4.4"),
+			50000,
+			8080,
+		)
+
+		val := new(USMConnectionData[struct{}, any])
+		data := make(map[types.ConnectionKey]*USMConnectionData[struct{}, any])
+		data[key] = val
+
+		// Assert that c1 and c2 (which are symmetrical) "link" to the same aggregation
+		c1 := network.ConnectionStats{
+			Source: util.AddressFromString("1.1.1.1"),
+			Dest:   util.AddressFromString("2.2.2.2"),
+			SPort:  60000,
+			DPort:  80,
+			IPTranslation: &network.IPTranslation{
+				ReplSrcIP:   util.AddressFromString("3.3.3.3"),
+				ReplDstIP:   util.AddressFromString("4.4.4.4"),
+				ReplSrcPort: 50000,
+				ReplDstPort: 8080,
+			},
+		}
+		assert.Equal(t, val, USMLookup(c1, data))
+
+		c2 := network.ConnectionStats{
+			Source: util.AddressFromString("2.2.2.2"),
+			Dest:   util.AddressFromString("1.1.1.1"),
+			SPort:  80,
+			DPort:  60000,
+			IPTranslation: &network.IPTranslation{
+				ReplSrcIP:   util.AddressFromString("4.4.4.4"),
+				ReplDstIP:   util.AddressFromString("3.3.3.3"),
+				ReplSrcPort: 8080,
+				ReplDstPort: 50000,
+			},
+		}
+		assert.Equal(t, val, USMLookup(c2, data))
+	})
 }
