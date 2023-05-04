7b9a40dacde26c0c14ea60422b11334cc55c304d
amit.slavin <amit.slavin@datadoghq.com>
2023-05-04 11:16:52 +0300

    added ut's for the change

--- pkg/network/protocols/http/incomplete_stats_test.go
+++ pkg/network/protocols/http/incomplete_stats_test.go
@@ -9,6 +9,7 @@
 package http
 
 import (
+	"golang.org/x/net/http2/hpack"
 	"testing"
 	"time"
 
@@ -16,6 +17,12 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/DataDog/datadog-agent/pkg/network/config"
+)
+
+const (
+	http2MaxPathLen     = 30
+	compressedPathLen   = 21
+	decompressedPathLen = 28
 )
 
 func TestOrphanEntries(t *testing.T) {
@@ -67,5 +74,45 @@ func TestOrphanEntries(t *testing.T) {
 		now = now.Add(35 * time.Second)
 		_ = buffer.Flush(now)
 		assert.True(t, len(buffer.data) == 0)
+	})
+}
+
+func TestHTTP2Path(t *testing.T) {
+	t.Run("validate http2 path backslash", func(t *testing.T) {
+		// create a buffer to store the encoded data
+		paths := []string{"/hello.HelloService/SayHello", "hello.HelloService/SayHello"}
+		results := []bool{true, false}
+		pathsResults := []string{"/hello.HelloService/SayHello", ""}
+
+		for index, currentString := range paths {
+			var buf []byte
+			var arr [http2MaxPathLen]uint8
+			buf = hpack.AppendHuffmanString(buf, currentString)
+			copy(arr[:], buf[:30])
+
+			request := &ebpfHttp2Tx{
+				Request_path: arr,
+				Path_size:    compressedPathLen,
+			}
+
+			outBuf := make([]byte, decompressedPathLen)
+
+			path, ok := request.Path(outBuf)
+			assert.Equal(t, results[index], ok)
+			assert.Equal(t, pathsResults[index], string(path))
+		}
+
+	})
+
+	t.Run("empty path", func(t *testing.T) {
+		request := &ebpfHttp2Tx{
+			Request_path: [http2MaxPathLen]uint8{},
+			Path_size:    compressedPathLen,
+		}
+		outBuf := make([]byte, decompressedPathLen)
+
+		path, ok := request.Path(outBuf)
+		assert.Equal(t, ok, false)
+		assert.Nil(t, path)
 	})
 }
