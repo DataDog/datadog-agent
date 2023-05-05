83cb13f4c9aa047a55b3508e906e212b3732d58a
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-05-05 15:54:12 +0200

    Add note

--- pkg/netflow/server_test.go
+++ pkg/netflow/server_test.go
@@ -145,8 +145,6 @@ network_devices:
 	require.NoError(t, err, "cannot start Netflow Server")
 	assert.NotNil(t, server)
 
-	// Send netflowV5Data twice to test aggregator
-	// Flows will have 2x bytes/packets after aggregation
 	time.Sleep(100 * time.Millisecond) // wait to make sure goflow listener is started before sending
 
 	err = testutil.SendUDPPacket(port, testutil.SFlow5Payload)
