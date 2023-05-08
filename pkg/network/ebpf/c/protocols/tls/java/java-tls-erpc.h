b5f8b6b866eed9d0f8666be475dc12e9256a9d02
Val Pliskin <valeri.pliskin@datadoghq.com>
2023-05-08 16:16:46 +0300

    refactored names and cleaned debug logs

--- pkg/network/ebpf/c/protocols/tls/java/java-tls-erpc.h
+++ pkg/network/ebpf/c/protocols/tls/java/java-tls-erpc.h
@@ -19,112 +19,148 @@
       u8 data[len];
   }
 */
-static int __always_inline handle_request(struct pt_regs *ctx, conn_tuple_t* connection, void *data) {
+static int __always_inline handle_sync_payload(struct pt_regs *ctx, void *data) {
+    // get connection tuple
+    conn_tuple_t connection = {0};
     const bool val = true;
     u32 bytes_read = 0;
+    void* bufferPtr = data;
+
+    //read the connection tuple from the ioctl buffer
+    if (0 != bpf_probe_read_user(&connection, sizeof(conn_tuple_t), bufferPtr)){
+        log_debug("[handle_sync_payload] failed to parse connection info\n");
+        return 1;
+    }
+    normalize_tuple(&connection);
+    bufferPtr+=sizeof(conn_tuple_t);
+
     // read the actual length of the message (limited by HTTP_BUFFER_SIZE)
-    if (0 != bpf_probe_read_user(&bytes_read, sizeof(bytes_read), data)){
+    if (0 != bpf_probe_read_user(&bytes_read, sizeof(bytes_read), bufferPtr)){
 #ifdef DEBUG
         u64 pid_tgid = bpf_get_current_pid_tgid();
         u64 pid = pid_tgid >> 32;
-        log_debug("[java-tls-handle_request] failed reading message length location for pid %d\n", pid);
+        log_debug("[handle_sync_payload] failed reading message length location for pid %d\n", pid);
 #endif
         return 1;
     }
-
-
+    bufferPtr+=sizeof(bytes_read);
 
     // register the connection in our map
-    bpf_map_update_with_telemetry(java_tls_connections, connection, &val, BPF_ANY);
-    log_debug("[java-tls-handle_request] handling tls request of size: %d\n", bytes_read);
-    https_process(connection, data+sizeof(bytes_read), bytes_read, JAVA_TLS);
+    bpf_map_update_with_telemetry(java_tls_connections, &connection, &val, BPF_ANY);
+    log_debug("[handle_sync_payload] handling tls request of size: %d for connection src addr: %llx; dst address %llx\n",
+                bytes_read, connection.saddr_l, connection.daddr_l);
+    https_process(&connection, bufferPtr, bytes_read, JAVA_TLS);
     http_batch_flush(ctx);
     return 0;
 }
 
-static void __always_inline handle_close_connection(conn_tuple_t* connection) {
-    void *exists = bpf_map_lookup_elem(&java_tls_connections, connection);
+static int __always_inline handle_close_connection(void *data) {
+
+    //read the connection tuple from the ioctl buffer
+    conn_tuple_t connection = {0};
+    if (0 != bpf_probe_read_user(&connection, sizeof(conn_tuple_t), data)){
+        log_debug("[java_tls_handle_close] failed to parse connection info\n");
+        return 1;
+    }
+    normalize_tuple(&connection);
+
+    void *exists = bpf_map_lookup_elem(&java_tls_connections, &connection);
     // if the connection exists in our map, finalize it and remove from the map
     // otherwise just ignore
     if (exists != NULL){
-        https_finish(connection);
-        log_debug("[java-tls-handle_request] removing connection from the map %llx\n", connection->daddr_h);
-        bpf_map_delete_elem(&java_tls_connections, connection);
+        https_finish(&connection);
+        bpf_map_delete_elem(&java_tls_connections, &connection);
     }
-}
-
-static void __always_inline handle_hostname(conn_tuple_t* connection, void *data) {
-
-    peer_domain_port_t peer_domain;
-    bpf_memset(&peer_domain, 0, sizeof(peer_domain_port_t));
-    u64 pid_tgid = bpf_get_current_pid_tgid();
-    peer_domain.pid = pid_tgid >> 32;
-    peer_domain.port = connection->dport;
-
-
-    //read the actual domain
-    if (0 != bpf_probe_read_user(&peer_domain.domain_name, MAX_DOMAIN_NAME_LENGTH, data)){
-        log_debug("[java-tls-handle_hostname] failed reading hostname location for pid %d\n", peer_domain.pid);
-        return;
-    }
-
-    // register the connection in domain_to_conn_tuple map
-    bpf_map_update_with_telemetry(conn_tuple_by_java_peer, &peer_domain, connection, BPF_ANY);
-
-    log_debug("[java-tls-handle_hostname] created map entry for pid %d hostname %s port: %d\n", peer_domain.pid, peer_domain.domain_name, peer_domain.port);
-}
-
-static int __always_inline handle_plain(struct pt_regs *ctx, conn_tuple_t* connection, void *data) {
-    const bool val = true;
-    u32 bytes_read = 0;
-
-
-    log_debug("[java-tls-handle_plain] starting");
-    // Get the buffer the hostname will be read into from a per-cpu array map.
-    // Meant to avoid hitting the stack size limit of 512 bytes
-    const u32 key = 0;
-    peer_domain_port_t* peer_domain = bpf_map_lookup_elem(&java_tls_hostname, &key);
-    if (peer_domain == NULL) {
-        log_debug("[java-tls-handle_plain] could not get peer domain buffer from map");
-        return 1;
-    }
-
-    bpf_memset(peer_domain, 0, sizeof(peer_domain_port_t));
-    u64 pid_tgid = bpf_get_current_pid_tgid();
-    peer_domain->pid = pid_tgid >> 32;
-    peer_domain->port = connection->dport;
-
-    //read the actual domain
-    if (0 != bpf_probe_read_user(&peer_domain->domain_name, MAX_DOMAIN_NAME_LENGTH, data)){
-        log_debug("[java-tls-handle_plain] failed reading hostname location for pid %d\n", peer_domain->pid);
-        return 1;
-    }
-
-    // get connection tuple
-    conn_tuple_t * actual_connection = bpf_map_lookup_elem(&conn_tuple_by_java_peer, peer_domain);
-    if (!actual_connection) {
-        log_debug("[java-tls-handle_plain] connection not found, pid: %d; hostname: %s; peer port: %d\n",
-         peer_domain->pid, peer_domain->domain_name, peer_domain->port);
-        return 1;
-    }
-
-     log_debug("[java-tls-handle_plain] found correlation conn src port: %d dst port: %d\n",
-             actual_connection->sport, actual_connection->dport);
-
-    // read the actual length of the message (limited by HTTP_BUFFER_SIZE)
-    if (0 != bpf_probe_read_user(&bytes_read, sizeof(bytes_read), data+MAX_DOMAIN_NAME_LENGTH)){
-        log_debug("[java-tls-handle_plain] failed reading message length location for pid %d\n", peer_domain->pid);
-        return 1;
-    }
-
-    // register the connection in our map
-    bpf_map_update_with_telemetry(java_tls_connections, actual_connection, &val, BPF_ANY);
-    log_debug("[java-tls-handle_plain] handling tls request of size: %d\n", bytes_read);
-    https_process(actual_connection, data+sizeof(bytes_read)+MAX_DOMAIN_NAME_LENGTH, bytes_read, JAVA_TLS);
-    http_batch_flush(ctx);
     return 0;
 }
 
+static int __always_inline handle_connection_by_peer(void *data) {
+
+    connection_by_peer_key_t peer_key ={0};
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    peer_key.pid = pid_tgid >> 32;
+    void* bufferPtr = data;
+
+    //read the connection tuple from the ioctl buffer
+    conn_tuple_t connection = {0};
+    if (0 != bpf_probe_read_user(&connection, sizeof(conn_tuple_t), bufferPtr)){
+        log_debug("[handle_connection_by_peer] failed to parse connection info for pid: %d\n", peer_key.pid);
+        return 1;
+    }
+    normalize_tuple(&connection);
+    bufferPtr+=sizeof(conn_tuple_t);
+
+    //read the host tuple (peer domain string and peer port)
+    if (0 != bpf_probe_read_user(&peer_key.peer, sizeof(peer_t), bufferPtr)){
+        log_debug("[handle_connection_by_peer] failed reading hostname location for pid %d\n", peer_key.pid);
+        return 1;
+    }
+
+    // register the connection in domain_to_conn_tuple map
+    bpf_map_update_with_telemetry(java_conn_tuple_by_peer, &peer_key, &connection, BPF_ANY);
+
+    log_debug("[handle_connection_by_peer] created map entry for pid %d domain %s port: %d\n",
+                peer_key.pid, peer_key.peer.domain, peer_key.peer.port);
+    return 0;
+}
+
+static int __always_inline handle_async_payload(struct pt_regs *ctx, void *data) {
+    const bool val = true;
+    u32 bytes_read = 0;
+    void* bufferPtr = data;
+
+    // Get the buffer the hostname will be read into from a per-cpu array map.
+    // Meant to avoid hitting the stack size limit of 512 bytes
+    const u32 key = 0;
+    connection_by_peer_key_t* peer_key = bpf_map_lookup_elem(&java_tls_peer, &key);
+    if (peer_key == NULL) {
+        log_debug("[handle_async_payload] could not get peer host buffer from map");
+        return 1;
+    }
+
+    bpf_memset(peer_key, 0, sizeof(connection_by_peer_key_t));
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    peer_key->pid = pid_tgid >> 32;
+
+    //read the host tuple (peer domain string and peer port)
+    if (0 != bpf_probe_read_user(&peer_key->peer, sizeof(peer_t), bufferPtr)){
+        log_debug("[java_tls_handle_plain] failed reading hostname location for pid %d\n", peer_key->pid);
+        return 1;
+    }
+    bufferPtr+=sizeof(peer_t);
+    log_debug("[handle_async_payload] pid: %d; peer domain: %s; peer port: %d\n",
+         peer_key->pid,
+         peer_key->peer.domain,
+         peer_key->peer.port);
+
+    //get connection tuple
+    conn_tuple_t * actual_connection = bpf_map_lookup_elem(&java_conn_tuple_by_peer, peer_key);
+    if (!actual_connection) {
+        log_debug("[handle_async_payload] couldn't correlate connection\n");
+        return 1;
+    }
+
+     log_debug("[handle_async_payload] found correlation conn src port: %d dst port: %d\n",
+             actual_connection->sport,
+             actual_connection->dport);
+
+    // read the actual length of the message (limited by HTTP_BUFFER_SIZE)
+    if (0 != bpf_probe_read_user(&bytes_read, sizeof(bytes_read), bufferPtr)){
+        log_debug("[handle_async_payload] failed reading message length location for pid %d\n", peer_key->pid);
+        return 1;
+    }
+    bufferPtr+=sizeof(bytes_read);
+
+    // register the connection in our map
+    bpf_map_update_with_telemetry(java_tls_connections, actual_connection, &val, BPF_ANY);
+    log_debug("[handle_async_payload] handling tls request of size: %d for connection src addr: %llx; dst address %llx\n",
+                bytes_read,
+                actual_connection->saddr_l,
+                actual_connection->daddr_l);
+    https_process(actual_connection, bufferPtr, bytes_read, JAVA_TLS);
+    http_batch_flush(ctx);
+    return 0;
+}
 
 static int __always_inline is_usm_erpc_request(struct pt_regs *ctx) {
     u32 cmd = PT_REGS_PARM3(ctx);
@@ -135,7 +171,7 @@ static int __always_inline is_usm_erpc_request(struct pt_regs *ctx) {
   handle_erpc_request ioctl request format :
 
   struct {
-      u8           operation;  // REQUEST, CLOSE_CONNECTION
+      u8           operation;  // REQUEST, CLOSE_CONNECTION, HOST, PLAIN
       conn_tuple_t connection; // connection tuple
       u8           data[];     // payload data
   }
@@ -149,34 +185,23 @@ static int __always_inline handle_erpc_request(struct pt_regs *ctx) {
 
     u8 op = 0;
     if (0 != bpf_probe_read_user(&op, sizeof(op), req)){
-        log_debug("[java-tls-handle_erpc_request] failed to parse opcode of java tls erpc request for: pid %d\n", pid);
+        log_debug("[java_tls_handle_erpc_request] failed to parse opcode of java tls erpc request for: pid %d\n", pid);
         return 1;
     }
 
-    // get connection tuple
-    conn_tuple_t connection = {0};
-    if (0 != bpf_probe_read_user(&connection, sizeof(conn_tuple_t), req+sizeof(op))){
-        log_debug("[java-tls-handle_erpc_request] failed to parse connection info of java tls erpc request %x for: pid %d\n", op, pid);
-        return 1;
-    }
-
-    normalize_tuple(&connection);
-
-    void *data = req + sizeof(op) + sizeof(conn_tuple_t);
-    log_debug("[java-tls-handle_erpc_request] received %d op\n", op);
+    void *data = req + sizeof(op);
+    log_debug("[java_tls_handle_erpc_request] received %d op\n", op);
     switch (op) {
-    case REQUEST:
-        return handle_request(ctx, &connection, data);
+    case SYNCHRONOUS_PAYLOAD:
+        return handle_sync_payload(ctx, data);
     case CLOSE_CONNECTION:
-        handle_close_connection(&connection);
-        return 0;
-    case HOSTNAME:
-        handle_hostname(&connection, data);
-        return 0;
-    case PLAIN:
-        return handle_plain(ctx, &connection, data);
+        return handle_close_connection(data);
+    case CONNECTION_BY_PEER:
+        return handle_connection_by_peer(data);
+    case ASYNC_PAYLOAD:
+        return handle_async_payload(ctx, data);
     default:
-        log_debug("[java-tls-handle_erpc_request] got unsupported erpc request %x for: pid %d\n",op, pid);
+        log_debug("[java_tls_handle_erpc_request] got unsupported erpc request %x for: pid %d\n",op, pid);
     }
 
     return 0;
--- pkg/network/ebpf/c/protocols/tls/java/java-tls-types.h
+++ pkg/network/ebpf/c/protocols/tls/java/java-tls-types.h
@@ -1,27 +1,27 @@
 #ifndef __JAVA_TLS_TYPES_H
 #define __JAVA_TLS_TYPES_H
 
-#include "ktypes.h"\
+#include "ktypes.h"
 
 #define MAX_DOMAIN_NAME_LENGTH 64
 
 
 enum erpc_message_type {
-    REQUEST,
+    SYNCHRONOUS_PAYLOAD,
     CLOSE_CONNECTION,
-    HOSTNAME,
-    PLAIN,
+    CONNECTION_BY_PEER,
+    ASYNC_PAYLOAD,
 };
 
 typedef struct{
     __u16 port;
-    char domain_name[MAX_DOMAIN_NAME_LENGTH]; //__attribute__ ((aligned (8)));
-} host_message_t;
+    char domain[MAX_DOMAIN_NAME_LENGTH]; //__attribute__ ((aligned (8)));
+} peer_t;
 
 typedef struct{
     __u32 pid;
-    host_message_t host;
-} connection_by_host_key_t;
+    peer_t peer;
+} connection_by_peer_key_t;
 
 
 #endif //__JAVA_TLS_TYPES_H
