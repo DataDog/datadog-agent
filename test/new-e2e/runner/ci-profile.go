62795ccb6adbf1a6af5802cb614ac9ea46e49729
Fanny Jiang <fanny.jiang@datadoghq.com>
2023-05-10 09:49:52 -0400

    fix curly braces

--- test/new-e2e/runner/ci-profile.go
+++ test/new-e2e/runner/ci-profile.go
@@ -49,11 +49,12 @@ func NewCIProfile() (Profile, error) {
 		return nil, fmt.Errorf("unable to compute name prefix, missing variables pipeline id: %s, project id: %s", pipelineID, projectID)
 	}
 
-  store := parameters.NewEnvStore(EnvPrefix)
-  
-  ciEnvironments := strings.Split(os.Getenv("CI_ENV_NAMES"), " ")
+	store := parameters.NewEnvStore(EnvPrefix)
+
+	ciEnvironments := strings.Split(os.Getenv("CI_ENV_NAMES"), " ")
 	if len(ciEnvironments) == 0 {
 		ciEnvironments = defaultCIEnvs
+	}
 
 	return ciProfile{
 		baseProfile: newProfile("e2eci", ciEnvironments, store, &secretStore),
