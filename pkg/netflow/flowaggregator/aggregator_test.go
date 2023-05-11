7dc3cc49b8bd054020cd45c21fca9ce1d9d113dc
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-05-11 11:31:55 +0200

    Rename processor.flows to processor.processed

--- pkg/netflow/flowaggregator/aggregator_test.go
+++ pkg/netflow/flowaggregator/aggregator_test.go
@@ -299,7 +299,7 @@ func TestAggregator_withMockPayload(t *testing.T) {
 	sender.AssertMetric(t, "Count", "datadog.netflow.aggregator.sequence.delta", 6, "", []string{"exporter_ip:127.0.0.1", "device_namespace:default", "flow_type:netflow5"})
 	sender.AssertMetric(t, "Gauge", "datadog.netflow.aggregator.sequence.last", 6, "", []string{"exporter_ip:127.0.0.1", "device_namespace:default", "flow_type:netflow5"})
 	sender.AssertMetric(t, "MonotonicCount", "datadog.netflow.decoder.messages", 1, "", []string{"collector_type:netflow5", "worker:0"})
-	sender.AssertMetric(t, "MonotonicCount", "datadog.netflow.processor.flows", 1, "", []string{"exporter_ip:127.0.0.1", "version:5", "flow_protocol:netflow"})
+	sender.AssertMetric(t, "MonotonicCount", "datadog.netflow.processor.processed", 1, "", []string{"exporter_ip:127.0.0.1", "version:5", "flow_protocol:netflow"})
 	sender.AssertMetric(t, "MonotonicCount", "datadog.netflow.processor.flowsets", 6, "", []string{"exporter_ip:127.0.0.1", "type:data_flow_set", "version:5", "flow_protocol:netflow"})
 	sender.AssertMetric(t, "MonotonicCount", "datadog.netflow.traffic.bytes", 312, "", []string{"listener_port:52056", "exporter_ip:127.0.0.1", "collector_type:netflow5"})
 	sender.AssertMetric(t, "MonotonicCount", "datadog.netflow.traffic.packets", 1, "", []string{"listener_port:52056", "exporter_ip:127.0.0.1", "collector_type:netflow5"})
