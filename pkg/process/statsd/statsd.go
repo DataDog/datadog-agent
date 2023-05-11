cc91868e2663e9069ac127503b89cbf47482d729
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-05-11 15:31:44 +0200

    allow `statsd.Configure` to look into env variables for statsd URL

--- pkg/process/statsd/statsd.go
+++ pkg/process/statsd/statsd.go
@@ -7,6 +7,7 @@ package statsd
 
 import (
 	"net"
+	"os"
 	"strconv"
 
 	"github.com/DataDog/datadog-go/v5/statsd"
@@ -17,9 +18,17 @@ import (
 var Client *statsd.Client
 
 // Configure creates a statsd client from a dogweb.ini style config file and set it to the global Statsd.
-func Configure(host string, port int) error {
-	url := net.JoinHostPort(host, strconv.Itoa(port))
-	client, err := statsd.New(url)
+func Configure(host string, port int, lookInEnv bool) error {
+	var statsdAddr string
+	if lookInEnv {
+		statsdAddr = os.Getenv("STATSD_URL")
+	}
+
+	if statsdAddr == "" {
+		statsdAddr = net.JoinHostPort(host, strconv.Itoa(port))
+	}
+
+	client, err := statsd.New(statsdAddr)
 	if err != nil {
 		return err
 	}
