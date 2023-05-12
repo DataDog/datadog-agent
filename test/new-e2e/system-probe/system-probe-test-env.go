b8a2260bf2d1c61803b1a5ba1d7d91648d339f43
usamasaqib <usamasaqib.96@live.com>
2023-05-12 18:48:06 +0200

    fix command provider init

--- test/new-e2e/system-probe/system-probe-test-env.go
+++ test/new-e2e/system-probe/system-probe-test-env.go
@@ -20,6 +20,7 @@ import (
 	"github.com/DataDog/test-infra-definitions/resources/aws"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/microVMs/microvms"
 
+	pulumiCommand "github.com/pulumi/pulumi-command/sdk/go/command"
 	"github.com/pulumi/pulumi-command/sdk/go/command/remote"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -128,7 +129,7 @@ func NewTestEnv(name, x86InstanceType, armInstanceType string, opts *SystemProbe
 		var depends []pulumi.Resource
 		osCommand := command.NewUnixOSCommand()
 
-		commandProvider, err = command.NewProvider(ctx, "command-provider", &command.ProviderArgs{})
+		commandProvider, err := pulumiCommand.NewProvider(ctx, "command-provider", &command.ProviderArgs{})
 		if err != nil {
 			return fmt.Errorf("failed to get command provider: %w", err)
 		}
