8b1b6772a536b8d5e9dd252783a6900e77b4cb48
Pedro Lambert <pedrolambert@gmail.com>
2023-05-16 11:54:35 -0400

    Improve how limit is enforced on `incompleteBuffer`

--- pkg/network/protocols/http/incomplete_stats_test.go
+++ pkg/network/protocols/http/incomplete_stats_test.go
@@ -9,9 +9,10 @@
 package http
 
 import (
-	"golang.org/x/net/http2/hpack"
 	"testing"
 	"time"
+
+	"golang.org/x/net/http2/hpack"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -75,6 +76,38 @@ func TestOrphanEntries(t *testing.T) {
 		_ = buffer.Flush(now)
 		assert.True(t, len(buffer.data) == 0)
 	})
+}
+
+func TestBufferLimit(t *testing.T) {
+	now := time.Now()
+	tel, err := NewTelemetry()
+	require.NoError(t, err)
+
+	buffer := newIncompleteBuffer(config.New(), tel)
+
+	// Attempt to insert more data then allowed
+	// Since all incomplete parts share the same tuple, this will generate
+	// *one* map key, with many "parts" appended to it
+	//
+	// We're asserting here that our buffer counts the total number of entries
+	// included in the nested structures, and not only the map keys
+	for i := 0; i < 2*buffer.maxEntries; i++ {
+		request := &EbpfHttpTx{
+			Request_fragment: requestFragment([]byte("GET /foo/bar")),
+			Request_started:  uint64(now.UnixNano()),
+		}
+		request.Tup.Sport = 60000
+		buffer.Add(request)
+	}
+
+	// Count total entries
+	total := 0
+	for _, parts := range buffer.data {
+		total += len(parts.requests) + len(parts.responses)
+	}
+
+	// Assert that buffer honored the max number of entries allowed
+	assert.Equal(t, buffer.maxEntries, total)
 }
 
 func TestHTTP2Path(t *testing.T) {
