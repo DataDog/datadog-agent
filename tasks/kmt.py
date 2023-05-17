86a5a4bb4c04a938007a04d8357ee5212eb6aa2a
usamasaqib <usamasaqib.96@live.com>
2023-05-17 13:17:26 +0200

    fix init task

--- tasks/kmt.py
+++ tasks/kmt.py
@@ -1,4 +1,5 @@
 import os
+import getpass
 import itertools
 import filecmp
 import math
@@ -17,6 +18,8 @@ KMT_STACKS_DIR = os.path.join(KMT_DIR, "stacks")
 KMT_PACKAGES_DIR = os.path.join(KMT_DIR, "kernel-packages")
 KMT_BACKUP_DIR = os.path.join(KMT_DIR, "backups")
 KMT_LIBVIRT_DIR = os.path.join(KMT_DIR, "libvirt")
+KMT_SHARED_DIR = os.path.join("/", "opt", "kernel-version-testing")
+KMT_KHEADERS_DIR = os.path.join("/", "opt", "kernel-version-testing", "kernel-headers")
 
 VMCONFIG = "vm-config.json"
 
@@ -88,6 +91,56 @@ images_path = {
     "buster": "file:///home/kernel-version-testing/rootfs/buster.qcow2.amd64-DEV",
 }
 
+priv_key = """-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAACFwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAgEA7QX+iYc1CmxNdIbr6r0+kD+hvzX6IiSjMOmD9qL5R4MJw7/Kc01A
+e+JN7wrF7Mpj/HTC8Tv11TpMBBCBnJumps2reZgOhWLPFmwIoY1pbt+SkRAjOlmwSs8MWW
+wom1Rw45h6VtCW2TfiQKSsr6HeVJzXQeNwRApCO3mMDSDjJrGZft8Xnn054e9A70fEX3II
+Mi4CeTY1Y5Dy6E4MNumzgSiq/F6Ok+eyZBtR11tXT5MkL40U3dm8xMxW3sYg4G66Y4yVWA
+/AZJHa30IM18d0XDzXf5trCZP31NptP8nisVqB0hQJ5331NNJzQfhjm1u4v2BAowulALZv
++FUPOGemYevKOl26vsPj6050E43t2wbKog/fbVyaTnjZjuhY+oyFsCohdmKYafx48E+80R
+G8/4H6vIaWalUG5XC1ftR60m8Ehzd/eadWc9CasnEi0NahJZQD0ba30FH3vmvOBcd6Ya8j
+uVqS8XTXwcUXWJV3DI3G8YbDziKYDipquwkGh6qGtj7wWJxvfFA9esy6zW3xlPxd0/7e1W
+/rw8exAJjv/PI/5fxa6KL41r62SELKgcIYEfgFHi2dvX1Iktnw8u3uPHgl/6YgSio0m3+v
+G0MDpWe//QMzQ1HCbyH8sgb8YhXgxRGtNROE+2LmhaRtEuZUEueN/0sJ+eZMvN12SjbNAW
+sAAAdQ2GtkLdhrZC0AAAAHc3NoLXJzYQAAAgEA7QX+iYc1CmxNdIbr6r0+kD+hvzX6IiSj
+MOmD9qL5R4MJw7/Kc01Ae+JN7wrF7Mpj/HTC8Tv11TpMBBCBnJumps2reZgOhWLPFmwIoY
+1pbt+SkRAjOlmwSs8MWWwom1Rw45h6VtCW2TfiQKSsr6HeVJzXQeNwRApCO3mMDSDjJrGZ
+ft8Xnn054e9A70fEX3IIMi4CeTY1Y5Dy6E4MNumzgSiq/F6Ok+eyZBtR11tXT5MkL40U3d
+m8xMxW3sYg4G66Y4yVWA/AZJHa30IM18d0XDzXf5trCZP31NptP8nisVqB0hQJ5331NNJz
+Qfhjm1u4v2BAowulALZv+FUPOGemYevKOl26vsPj6050E43t2wbKog/fbVyaTnjZjuhY+o
+yFsCohdmKYafx48E+80RG8/4H6vIaWalUG5XC1ftR60m8Ehzd/eadWc9CasnEi0NahJZQD
+0ba30FH3vmvOBcd6Ya8juVqS8XTXwcUXWJV3DI3G8YbDziKYDipquwkGh6qGtj7wWJxvfF
+A9esy6zW3xlPxd0/7e1W/rw8exAJjv/PI/5fxa6KL41r62SELKgcIYEfgFHi2dvX1Iktnw
+8u3uPHgl/6YgSio0m3+vG0MDpWe//QMzQ1HCbyH8sgb8YhXgxRGtNROE+2LmhaRtEuZUEu
+eN/0sJ+eZMvN12SjbNAWsAAAADAQABAAACAAVKVSzhYDHFSqRuQ/DEAQGyzVKinUpKzcTQ
+W8flScQYfwOn/3O7z8FvjAbEXJOVO3MW3zq+eF6T8ZpEw8NEKvtLa3m/GVIo/YGYZiN9i1
+LUa/NrFUrH6Go2eLgp9KQSV+y0julYbz/M8AUVx93OROXFlGr5SIpGhuoRWoZB65bhSuza
+hOWno76+mpETijctu1Ri04NzO/DUn8PsDgsGTQ9RT9hGDXQ1iKMCFoZ7Ycxw9q67Bla1B/
+IYRlvRAG7/sI8x1ivNOjPkdBhlvsyjl7A4NyUk7mp3hvPMOJR1RAuzfxmVyeqEwmtsMRdk
+OGfKhvMxbktVWUZoJ3hbktDAslxUBPHflUjA2i+R2aKaG90Ha9hOInzFGXgI7wiC5ZilnQ
+1iOVT9xIV/RKgII7w/JAiuDXgQDp3RQH7QEBZ+WQ96iTLw4aLYaWklJWvyLBTfbvOwK3VD
+Dh8xmBnA9gKYHdyFgH8OHn0j1CynkfuEEmhzw3Y2IM+hb1joTya1CnitS4y94fWxnqG0RO
+che1e64KDc/QBoCH/ZGAQxGgruLjGR/xteLNl+ENjxkGbaPV9N9o4reKOgIDw8zKB06eaB
+WAqrDIN47/legYrUBbbqOXk3tpbo45+tvjw+3Za/HNuDbs0tBsbBZSLzp0Xt4FN02rTvYR
+V6+i3oIS4c8ZDJm7u1AAABAQCsg0ynvvNFXJIy0+xmrcwEjk0S5AUxea8GfRK8bGcLmnjE
+4OGioJ9Fo6oXoZzC366od+c8XBn0oyIH23cNzz4wq5tgyxQPZm5Tix6FORKTvUhTVsSJ7Q
+fKA5C+0OqZ930U3168cwx812xWJMY6T3v5QBzfvtXW1BSLEwH9zcb7x9RvDFPfQ1wDotLy
+J0GIT69fk+RNcF0b4CjXAekQdOZ0EO3LsjqhMirY73rBKvWXFgQeDVrEPcOE0I/yNvUpjA
+3JFteeRaE0HG+4aIwNTQ00IGGs/TshFNt2HldgBNvXht7D1AEGYnIYeYAedHiVYLAtdsEH
+W/3O5nlrwK7Keye1AAABAQDxMrYChpvPGmlzNMjzjJO7Kl3FXkgWspd90gybUz28pgcVF8
+0zevOg+/TyAzQCHuGgQ0FyG7cGAqCqu3jmWQrvD9PDJGgbOb/K5qCfhPCCe0Tif6ql6PdB
+I1NRqxtiUlGs8yYIxWJs0zmFjJnuHkR/OJg3qYlzOYI4UCoySktnKfiisGCIDXF/q0EOrk
+gQXbLSmOfhCcrNp75GiJTJ9Bgc+V85NCLbL0aTTSBEMz74ONj4/z2rxf31o+2Dig3G2yrm
+ddjS2kRKkNxrq9lxOm9e288G6yT9s/YZaxSRX1bsoW4y88t1Zrod0luuFztMrFIu5nrm6K
+nH9Jkqmy7J4OcnAAABAQD7kbKw7iY1HQmYIhLIWc3TwTdbQQvxsl0X3mqmQghna9SPmyFc
+Jw3QZ5Db7zk6UhT3VEffeGjnLo80TKejCAVZNdu0dTe3PpHl7Xs1IRZajc+/DcVyMhJbEc
+Ku31pHRnozPTDrZxu+vyG5su5/G6/QKwX9O2/oFqVOnEtTqP8QQfIGVRG3i+ZuKQAcGHwG
+zFgWBFTT+4oJSC8pMAQQfY5rrUSFr3Zg/EBhk/XeBmIxo5iyOkZtpCHNuGbqNYteNNsM8y
+a1eAv3AZrgqk0eQl1XapooMMSY5mKjxJKscqthce9uvVnWPWVSI9moPKH6gaZ6336UhFzz
+3VDkRuwEid4dAAAAFnJvb3RAaXAtMTcyLTI5LTE4NS0yMjgBAgME
+-----END OPENSSH PRIVATE KEY-----
+"""
 
 def is_root():
     return os.getuid() == 0
@@ -101,6 +154,12 @@ def vm_config_exists(stack):
     return os.path.exists(f"{KMT_STACKS_DIR}/{stack}/{VMCONFIG}")
 
 
+def gen_ssh_key(ctx):
+    with open(f"{KMT_DIR}/ddvm_rsa", "w") as f:
+        f.write(priv_key)
+
+    ctx.run(f"chmod 400 {KMT_DIR}/ddvm_rsa")
+
 @task
 def init(ctx):
     sudo = "sudo" if not is_root() else ""
@@ -110,10 +169,20 @@ def init(ctx):
     ctx.run(f"{sudo} install -d -m 0755 -g libvirt -o $(getent passwd 1000 | cut -d ':' -f 1) {KMT_STACKS_DIR}")
     ctx.run(f"{sudo} install -d -m 0755 -g libvirt -o $(getent passwd 1000 | cut -d ':' -f 1) {KMT_LIBVIRT_DIR}")
     ctx.run(f"{sudo} install -d -m 0755 -g libvirt -o $(getent passwd 1000 | cut -d ':' -f 1) {KMT_ROOTFS_DIR}")
-    # download dependencies
+    ctx.run(f"{sudo} install -d -m 0755 -g libvirt -o $(getent passwd 1000 | cut -d ':' -f 1) {KMT_SHARED_DIR}")
+    ctx.run(f"{sudo} install -d -m 0755 -g libvirt -o $(getent passwd 1000 | cut -d ':' -f 1) {KMT_KHEADERS_DIR}")
 
+    # fix libvirt conf
+    user = getpass.getuser()
+    ctx.run(f"{sudo} sed --in-place 's/#security_driver = \"selinux\"/security_driver = \"none\"/' /etc/libvirt/qemu.conf")
+    ctx.run(f"{sudo} sed --in-place 's/#user = \"root\"/user = \"{user}\"/' /etc/libvirt/qemu.conf")
+    ctx.run(f"{sudo} sed --in-place 's/#group = \"root\"/group = \"kvm\"/' /etc/libvirt/qemu.conf")
+    ctx.run(f"{sudo} systemctl restart libvirtd.service")
+
+    # download dependencies
     download_kernel_packages(ctx)
     download_rootfs(ctx)
+    gen_ssh_key(ctx)
 
 
 def resource_in_stack(stack, resource):
@@ -525,6 +594,7 @@ def download_kernel_packages(ctx, revert=False):
     kernel_packages_sum = f"kernel-packages-{arch}.sum"
     kernel_packages_tar = f"kernel-packages-{arch}.tar"
 
+    sudo = "sudo" if not is_root() else ""
     # download kernel packages
     res = ctx.run(
         f"wget -q https://dd-agent-omnibus.s3.amazonaws.com/kernel-version-testing/{kernel_packages_tar} -O {KMT_PACKAGES_DIR}/{kernel_packages_tar}",
@@ -552,11 +622,24 @@ def download_kernel_packages(ctx, revert=False):
         raise Exit("Failed to extract kernel packages")
 
     # set permissions
-    res = ctx.run(f"find {KMT_PACKAGES_DIR} -name kernel-v* -type d -exec chmod 0766 {{}}/* \;")
+    packages = glob(f"{KMT_PACKAGES_DIR}/kernel-v*")
+    for pkg in packages:
+        if not os.path.isdir(pkg):
+            continue
+        # set package dir as rwx for all
+        os.chmod(pkg, 0o766)
+        files = glob(f"{pkg}/*")
+        for f in files:
+            if not os.path.isdir(f):
+                # set all files to rw for all
+                os.chmod(f, 0o666)
+
+    # copy headers
+    res = ctx.run(f"find {KMT_PACKAGES_DIR} -name 'linux-image-*' -type f | xargs -i cp {{}} {KMT_KHEADERS_DIR} && find {KMT_PACKAGES_DIR} -name 'linux-headers-*' -type f | xargs -i cp {{}} {KMT_KHEADERS_DIR}")
     if not res.ok:
         if revert:
             revert_kernel_packages(ctx)
-        raise Exit("Failed to set permissions to kernel packages")
+        raise Exit(f"failed to copy kernel headers to shared dir {KMT_KHEADERS_DIR}")
 
 
 def update_kernel_packages(ctx):
@@ -698,29 +781,8 @@ def revert_resources(ctx):
     print("[+] Reverted successfully")
 
 
-'''
-aws-vault exec sandbox-account-admin -- pulumi up \
-    -c scenario=aws/microvms \
-    -c ddinfra:aws/defaultKeyPairName=usama-saqib-datadog-aws-2 \
-    -c ddinfra:aws/defaultPrivateKeyPath=$HOME/.ssh/usama-saqib-datadog-aws-2.pem \
-    -c ddinfra:aws/defaultPrivateKeyPassword=\
-    -c ddinfra:env=aws/sandbox \
-    -c ddinfra:aws/defaultARMInstanceType=m6g.metal \
-    -c ddinfra:aws/defaultInstanceType=i3.metal \
-    -c ddinfra:aws/defaultInstanceStorageSize=500 \
-    -c microvm:microVMConfigFile="/home/usama.saqib/go/github.com/DataDog/test-infra-definitions/aws/scenarios/microVMs/sample-vm-config.json" \
-    -c microvm:workingDir="/home/kernel-version-testing" \
-    -c microvm:provision=false \
-    -c microvm:x86AmiID="ami-0584a00dd384af6ab" \
-    -c microvm:arm64AmiID="ami-0a5c054df5931fbfc" \
-    -C ./aws/scenarios/microVMs \
-    -s usama-saqib
-
-'''
-
-
 @task
-def launch_stack(ctx, stack, test_infra_definitions="../test-infra-definitions", ssh_key="", x86_ami="", arm64_ami=""):
+def launch_stack(ctx, stack, ssh_key="", x86_ami="ami-0584a00dd384af6ab", arm_ami="ami-0a5c054df5931fbfc"):
     if not stack_exists(stack):
         raise Exit(f"Stack {stack} does not exist. Please create with 'inv kmt.stack-create --stack=<name>'")
 
@@ -730,6 +792,9 @@ def launch_stack(ctx, stack, test_infra_definitions="../test-infra-definitions",
     if not os.path.exists(test_infra_definitions):
         raise Exit("'test-infra-definitions' repository required to launc VMs")
 
+    vm_config = f"{KMT_STACKS_DIR}/{stack}/{VMCONFIG}"
+    micro_vm_scenario = "../test-infra-definitions/aws/scenarios/microVMs"
+
     pulumi_cmd = [
         "aws-vault exec sandbox-account-admin",
         "--",
@@ -737,17 +802,22 @@ def launch_stack(ctx, stack, test_infra_definitions="../test-infra-definitions",
         "up",
         "-c scenario=aws/microvms",
         "-c ddinfra:aws/defaultKeyPairName=",
-        "-c ddinfra:aws/defaultPrivateKeyPath=",
-        "-c ddinfra:aws/defaultPrivateKeyPassword=",
         "-c ddinfra:env=aws/sandbox",
         "-c ddinfra:aws/defaultARMInstanceType=m6g.metal",
         "-c ddinfra:aws/defaultInstanceType=i3.metal",
         "-c ddinfra:aws/defaultInstanceStorageSize=500",
-        "-c microvm:microVMConfigFile=\"/home/usama.saqib/go/github.com/DataDog/test-infra-definitions/aws/scenarios/microVMs/sample-vm-config.json\"",
-        "-c microvm:workingDir=",
+        f"-c microvm:microVMConfigFile={vm_config}",
+        "-c microvm:workingDir={KMT_DIR}",
         "-c microvm:provision=false",
-        "-c microvm:x86AmiID=ami-0584a00dd384af6ab",
-        "-c microvm:arm64AmiID=ami-0a5c054df5931fbfc",
-        "-C ./aws/scenarios/microVMs",
-        "-s usama-saqib",
+        "-c microvm:x86AmiID={x86_ami}",
+        "-c microvm:arm64AmiID={arm_ami}",
+        f"-C {micro_vm_scenario}",
+        "-s {stack}",
     ]
+
+    if not os.path.exists(micro_vm_scenario):
+        raise Exit(f"Could not find scenario directory at {micro_vm_scenario}")
+    
+    print("Run this ->")
+    print(' '.join(pulumi_cmd))
+
