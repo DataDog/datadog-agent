7db99eeb6da1120229db225ebe94461eaca537b2
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-05-19 16:37:00 -0400

    use enums in tests

--- pkg/languagedetection/detector_test.go
+++ pkg/languagedetection/detector_test.go
@@ -15,33 +15,34 @@ func TestLanguageFromCommandline(t *testing.T) {
 	for _, tc := range []struct {
 		name     string
 		cmdline  []string
-		expected string
+		expected LanguageName
 		error    bool
 	}{
 		{
 			name:     "python3",
 			cmdline:  []string{"/opt/python/2.7.11/bin/python2.7", "/opt/foo/bar/baz", "--config=asdf"},
-			expected: "python",
+			expected: python,
 		},
 		{
 			name:     "java",
 			cmdline:  []string{"/usr/bin/java", "-Xfoo=true", "org.elasticsearch.bootstrap.Elasticsearch"},
-			expected: "java",
+			expected: java,
 		},
 		{
-			name:    "unknown",
-			cmdline: []string{"mine-bitcoins", "--all"},
-			error:   true,
+			name:     "unknown",
+			cmdline:  []string{"mine-bitcoins", "--all"},
+			expected: unknown,
+			error:    true,
 		},
 		{
 			name:     "python with space and special chars in path",
 			cmdline:  []string{"//..//path/\"\\ to/python", "asdf"},
-			expected: "python",
+			expected: python,
 		},
 		{
 			name:     "args in first element",
 			cmdline:  []string{"/usr/bin/python myapp.py --config=/etc/mycfg.yaml"},
-			expected: "python",
+			expected: python,
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
