be1901e9ceb1a2df97ca9aa469f09d49e2157e89
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-05-19 16:16:45 -0400

    Don't use logger component

--- pkg/languagedetection/detector.go
+++ pkg/languagedetection/detector.go
@@ -9,8 +9,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/DataDog/datadog-agent/comp/core/log"
 	"github.com/DataDog/datadog-agent/pkg/process/procutil"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 type LanguageName string
@@ -41,12 +41,12 @@ func languageNameFromCommandLine(cmdline []string) (LanguageName, error) {
 	return unknown, fmt.Errorf("unknown executable: %s", exe)
 }
 
-func DetectLanguage(logger log.Component, procs []procutil.Process) []*Language {
+func DetectLanguage(procs []procutil.Process) []*Language {
 	langs := make([]*Language, len(procs))
 	for i, proc := range procs {
 		languageName, err := languageNameFromCommandLine(proc.Cmdline)
 		if err == nil {
-			logger.Trace("detected languageName:", languageName)
+			log.Trace("detected languageName:", languageName)
 		}
 		langs[i] = &Language{Name: languageName}
 	}
