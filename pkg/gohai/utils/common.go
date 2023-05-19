71ed38fcadfb82062787a71c12e00e2b35f35748
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2023-05-19 18:19:21 +0200

    feat(gohai): implement AsJSON and Initialize

---
+++ pkg/gohai/utils/common.go
@@ -0,0 +1,249 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"runtime"
+	"strings"
+)
+
+var ErrNoFieldCollected = errors.New("no field was collected")
+var ErrArgNotStruct = errors.New("argument is not a struct")
+
+// NotCollectedError represents a value which is not collected on a specific OS / Arch.
+type NotCollectedError struct {
+	PkgName   string
+	ValueName string
+}
+
+// NewNotCollectedError return a new NotCollectedError
+func NewNotCollectedError(pkgName, valueName string) *NotCollectedError {
+	return &NotCollectedError{
+		pkgName, valueName,
+	}
+}
+
+func (err *NotCollectedError) Error() string {
+	var valuePath string
+	if err.PkgName == "" {
+		valuePath = err.ValueName
+	} else {
+		valuePath = fmt.Sprintf("%s.%s", err.PkgName, err.ValueName)
+	}
+	return fmt.Sprintf("%s is not collected on %s %s", valuePath, runtime.GOOS, runtime.GOARCH)
+}
+
+// fieldIsValue returns whether the field has type Value[T] for some T.
+//
+// It actually checks whether the type has the correct name, and has functions Value and NewErrorValue
+// with correct number and types of argument and output
+func fieldIsValue(fieldTy reflect.StructField) bool {
+	// check that the type is Value[T] for some T
+	// cannot really do better than checking the name as long as
+	// https://github.com/golang/go/issues/54393 is not implemented
+	if !strings.HasPrefix(fieldTy.Type.Name(), "Value[") {
+		return false
+	}
+
+	// check that a pointer to the field has a Value method
+	valueMethod, ok := reflect.PtrTo(fieldTy.Type).MethodByName("Value")
+	if !ok || valueMethod.Type.NumIn() != 1 || valueMethod.Type.NumOut() != 2 {
+		return false
+	}
+
+	// check that the second return value is not an error
+	reflErrorInterface := reflect.TypeOf((*error)(nil)).Elem()
+	secondRetType := valueMethod.Type.Out(1)
+	if secondRetType.Kind() != reflect.Interface || !secondRetType.Implements(reflErrorInterface) {
+		return false
+	}
+
+	// check that the type has a NewErrorValue method
+	newErrorValueMethod, ok := fieldTy.Type.MethodByName("NewErrorValue")
+	if !ok || newErrorValueMethod.Type.NumIn() != 2 || newErrorValueMethod.Type.NumOut() != 1 {
+		return false
+	}
+
+	// check that the returned value has the same type as the field
+	if newErrorValueMethod.Type.Out(0) != fieldTy.Type {
+		return false
+	}
+
+	return true
+}
+
+// fieldIsExportable checks if a field can be exported by AsJSON.
+//
+// A field can be exported if it has type Value, it is exported by the struct and has a json tag.
+// The function returns whether a field can be exported as well as its json tag if so.
+func fieldIsExportable(fieldTy reflect.StructField) (string, bool) {
+	// check if field has type Value
+	if !fieldIsValue(fieldTy) {
+		return "", false
+	}
+
+	// check if field is exported
+	if !fieldTy.IsExported() {
+		return "", false
+	}
+
+	// check if field has a json tag
+	fieldName, ok := fieldTy.Tag.Lookup("json")
+	if !ok {
+		return "", false
+	}
+
+	return fieldName, true
+}
+
+// renderValue converts the given value to a string, and return a boolean indicating
+// whether it succeeded
+func renderValue(value reflect.Value) (string, bool) {
+	var rendered string
+	switch value.Kind() {
+	// case reflect.Bool:
+	case reflect.Int:
+		rendered = fmt.Sprintf("%d", value.Interface().(int))
+	case reflect.Int8:
+		rendered = fmt.Sprintf("%d", value.Interface().(int8))
+	case reflect.Int16:
+		rendered = fmt.Sprintf("%d", value.Interface().(int16))
+	case reflect.Int32:
+		rendered = fmt.Sprintf("%d", value.Interface().(int32))
+	case reflect.Int64:
+		rendered = fmt.Sprintf("%d", value.Interface().(int64))
+	case reflect.Uint:
+		rendered = fmt.Sprintf("%d", value.Interface().(uint))
+	case reflect.Uint8:
+		rendered = fmt.Sprintf("%d", value.Interface().(uint8))
+	case reflect.Uint16:
+		rendered = fmt.Sprintf("%d", value.Interface().(uint16))
+	case reflect.Uint32:
+		rendered = fmt.Sprintf("%d", value.Interface().(uint32))
+	case reflect.Uint64:
+		rendered = fmt.Sprintf("%d", value.Interface().(uint64))
+	// case reflect.Uintptr:
+	case reflect.Float32:
+		rendered = fmt.Sprintf("%f", value.Interface().(float32))
+	case reflect.Float64:
+		rendered = fmt.Sprintf("%f", value.Interface().(float64))
+	// case reflect.Complex64:
+	// case reflect.Complex128:
+	// case reflect.Array:
+	// case reflect.Chan:
+	// case reflect.Func:
+	// case reflect.Interface:
+	// case reflect.Map:
+	// case reflect.Pointer:
+	// case reflect.Slice:
+	case reflect.String:
+		rendered = value.String()
+	// case reflect.Struct:
+	// case reflect.UnsafePointer:
+	default:
+		return "", false
+	}
+
+	return rendered, true
+}
+
+// AsJSON takes an Info struct and returns a marshal-able object representing the fields of the struct,
+// the lists of errors for fields which could not be collected, and an error if it failed or if no
+// information was collected in the Info struct.
+//
+// If useDefault is true, fields which failed to be collected will be included in the marshal-able object
+// as their default value, otherwise they are ignored.
+//
+// If the error is non-nil, the first two parameters are unspecified.
+//
+// Fields which are not exported, don't have a json tag or are not of type Value[T] are ignored.
+func AsJSON[T any](info *T, useDefault bool) (interface{}, []error, error) {
+	reflVal := reflect.ValueOf(info).Elem()
+	reflType := reflect.TypeOf(info).Elem()
+
+	// info has to be a struct
+	if reflVal.Kind() != reflect.Struct {
+		return nil, nil, ErrArgNotStruct
+	}
+
+	values := make(map[string]interface{})
+	errors := []error{}
+
+	for i := 0; i < reflVal.NumField(); i++ {
+		fieldName, isExportable := fieldIsExportable(reflType.Field(i))
+		if !isExportable {
+			continue
+		}
+
+		// Value is a method on *Value[T] so we get a pointer to the value
+		fieldPtr := reflVal.Field(i).Addr()
+		valueMethod, _ := fieldPtr.Type().MethodByName("Value")
+		ret := valueMethod.Func.Call([]reflect.Value{fieldPtr})
+		retValue := ret[0]
+		if ret[1].Interface() != nil {
+			err := ret[1].Interface().(error)
+			errors = append(errors, err)
+			if !useDefault {
+				continue
+			}
+
+			// use the default value of the type
+			retValue = reflect.Zero(retValue.Type())
+		}
+
+		renderedValue, ok := renderValue(retValue)
+		if !ok {
+			continue
+		}
+
+		values[fieldName] = renderedValue
+	}
+
+	if len(values) == 0 {
+		return nil, errors, ErrNoFieldCollected
+	}
+
+	return values, errors, nil
+}
+
+// GetPkgName returns the package name from the given package path
+func GetPkgName(pkgPath string) string {
+	fmt.Fprintln(os.Stderr, pkgPath)
+	pkg := strings.Split(pkgPath, "/")
+	if len(pkg) == 0 {
+		return pkgPath
+	}
+	return pkg[len(pkg)-1]
+}
+
+// Initialize sets the value of exportable fields to a NotCollectedError.
+//
+// A field is exportable if it has type Value, has a json tag and is exported by the struct.
+func Initialize[T any](info *T) error {
+	reflVal := reflect.ValueOf(info).Elem()
+	reflType := reflect.TypeOf(info).Elem()
+	pkgName := GetPkgName(reflType.PkgPath())
+
+	// info has to be a struct
+	if reflVal.Kind() != reflect.Struct {
+		return ErrArgNotStruct
+	}
+
+	for i := 0; i < reflVal.NumField(); i++ {
+		fieldName, isExportable := fieldIsExportable(reflType.Field(i))
+		if !isExportable {
+			continue
+		}
+
+		err := reflect.ValueOf(NewNotCollectedError(pkgName, fieldName))
+		fieldVal := reflVal.Field(i)
+		newErrorValueMethod, _ := fieldVal.Type().MethodByName("NewErrorValue")
+		ret := newErrorValueMethod.Func.Call([]reflect.Value{fieldVal, err})
+
+		fieldVal.Set(ret[0])
+	}
+
+	return nil
+}
---
+++ pkg/gohai/utils/common_test.go
@@ -0,0 +1,223 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestAsJSONTypes tests that each supported type can appear properly in the output
+func TestAsJSONTypes(t *testing.T) {
+	info := &struct {
+		SomeInt     Value[int]     `json:"my_int"`
+		SomeInt8    Value[int8]    `json:"my_int8"`
+		SomeInt16   Value[int16]   `json:"my_int16"`
+		SomeInt32   Value[int32]   `json:"my_int32"`
+		SomeInt64   Value[int64]   `json:"my_int64"`
+		SomeUint    Value[uint]    `json:"my_uint"`
+		SomeUint8   Value[uint8]   `json:"my_uint8"`
+		SomeUint16  Value[uint16]  `json:"my_uint16"`
+		SomeUint32  Value[uint32]  `json:"my_uint32"`
+		SomeUint64  Value[uint64]  `json:"my_uint64"`
+		SomeFloat32 Value[float32] `json:"my_float32"`
+		SomeFloat64 Value[float64] `json:"my_float64"`
+	}{
+		SomeInt:     NewValue(1),
+		SomeInt8:    NewValue[int8](2),
+		SomeInt16:   NewValue[int16](3),
+		SomeInt32:   NewValue[int32](4),
+		SomeInt64:   NewValue[int64](5),
+		SomeUint:    NewValue[uint](6),
+		SomeUint8:   NewValue[uint8](7),
+		SomeUint16:  NewValue[uint16](8),
+		SomeUint32:  NewValue[uint32](9),
+		SomeUint64:  NewValue[uint64](10),
+		SomeFloat32: NewValue[float32](32.),
+		SomeFloat64: NewValue(64.),
+	}
+
+	marshallable, warns, err := AsJSON(info, false)
+	require.NoError(t, err)
+	require.Empty(t, warns)
+
+	marshalled, err := json.Marshal(marshallable)
+	require.NoError(t, err)
+
+	// use format specifier for floats to make sure the formatting is the same
+	expected := fmt.Sprintf(`{
+		"my_int": "1",
+		"my_int8": "2",
+		"my_int16": "3",
+		"my_int32": "4",
+		"my_int64": "5",
+		"my_uint": "6",
+		"my_uint8": "7",
+		"my_uint16": "8",
+		"my_uint32": "9",
+		"my_uint64": "10",
+		"my_float32": "%f",
+		"my_float64": "%f"
+	}`, float32(32.), 64.)
+	require.JSONEq(t, expected, string(marshalled))
+}
+
+func TestAsJSONEmpty(t *testing.T) {
+	info := &struct{}{}
+	_, _, err := AsJSON(info, false)
+	require.ErrorIs(t, err, ErrNoFieldCollected)
+
+	_, _, err = AsJSON(info, true)
+	require.ErrorIs(t, err, ErrNoFieldCollected)
+}
+
+// TestAsJsonInvalidParam tests that using something other than struct returns an error
+func TestAsJsonInvalidParam(t *testing.T) {
+	var err error
+	// function
+	f := func() {}
+	_, _, err = AsJSON(&f, false)
+	require.ErrorIs(t, err, ErrArgNotStruct)
+	// slice
+	_, _, err = AsJSON(&[]int{1}, false)
+	require.ErrorIs(t, err, ErrArgNotStruct)
+	// array
+	_, _, err = AsJSON(&[1]int{1}, false)
+	require.ErrorIs(t, err, ErrArgNotStruct)
+	// basic type (bool)
+	b := true
+	_, _, err = AsJSON(&b, false)
+	require.ErrorIs(t, err, ErrArgNotStruct)
+	// pointer
+	p := &struct{}{}
+	_, _, err = AsJSON(&p, false)
+	require.ErrorIs(t, err, ErrArgNotStruct)
+}
+
+// TestAsJsonOmit checks that fields which should be omitted are
+// - type is not Value
+// - no json tag
+// - not exported
+// - Value is an error
+// - Value type is not handled (struct, ptr, bool, ...)
+func TestAsJsonOmit(t *testing.T) {
+	myerr := errors.New("this is an error")
+	info := &struct {
+		NotValue    int `json:"not_value"`
+		NoTag       Value[int]
+		notExported Value[int]      `json:"not_exported"`
+		ValueError  Value[int]      `json:"value_error"`
+		ValueStruct Value[struct{}] `json:"value_struct"`
+		ValuePtr    Value[*int]     `json:"value_ptr"`
+		ValueBool   Value[bool]     `json:"value_bool"`
+	}{
+		NotValue:    1,
+		NoTag:       NewValue(2),
+		notExported: NewValue(3),
+		ValueError:  NewErrorValue[int](myerr),
+		ValueStruct: NewValue(struct{}{}),
+		ValuePtr:    NewValue[*int](nil),
+		ValueBool:   NewValue(true),
+	}
+
+	_, _, err := AsJSON(info, false)
+	require.ErrorIs(t, err, ErrNoFieldCollected)
+
+	marshallable, warns, err := AsJSON(info, true)
+	require.NoError(t, err)
+	require.ElementsMatch(t, []error{myerr}, warns)
+
+	marshalled, err := json.Marshal(marshallable)
+	require.NoError(t, err)
+	expected := `{ "value_error": "0" }`
+	require.JSONEq(t, expected, string(marshalled))
+}
+
+func TestAsJsonWarns(t *testing.T) {
+	errs := []error{
+		errors.New("this is the first error"),
+		errors.New("this is the second error"),
+		errors.New("this is the third error"),
+		errors.New("this is the fourth error"),
+	}
+	info := &struct {
+		FieldOne   Value[int] `json:"field_one"`
+		FieldTwo   Value[int] `json:"field_two"`
+		FieldThree Value[int] `json:"field_three"`
+		FieldFour  Value[int] `json:"field_four"`
+		FieldFive  Value[int] `json:"field_five"`
+	}{
+		FieldOne:   NewErrorValue[int](errs[0]),
+		FieldTwo:   NewErrorValue[int](errs[1]),
+		FieldThree: NewErrorValue[int](errs[2]),
+		FieldFour:  NewErrorValue[int](errs[3]),
+		FieldFive:  NewValue(1),
+	}
+
+	marshallable, warns, err := AsJSON(info, false)
+	require.NoError(t, err)
+	require.ElementsMatch(t, errs, warns)
+
+	marshalled, err := json.Marshal(marshallable)
+	require.NoError(t, err)
+
+	expected := `{
+		"field_five": "1"
+	}`
+	require.JSONEq(t, expected, string(marshalled))
+
+	marshallable, warns, err = AsJSON(info, true)
+	require.NoError(t, err)
+	require.ElementsMatch(t, errs, warns)
+
+	marshalled, err = json.Marshal(marshallable)
+	require.NoError(t, err)
+
+	expected = `{
+		"field_one": "0",
+		"field_two": "0",
+		"field_three": "0",
+		"field_four": "0",
+		"field_five": "1"
+	}`
+	require.JSONEq(t, expected, string(marshalled))
+}
+
+func TestGetPkgName(t *testing.T) {
+	require.Equal(t, "", GetPkgName(""))
+
+	longPkgPath := "github.com/DataDog/datadog-agent/pkg/gohai/utils"
+	require.Equal(t, "utils", GetPkgName(longPkgPath))
+}
+
+func TestInitializeOmit(t *testing.T) {
+	info := &struct {
+		NotValue    int `json:"not_value"`
+		NoTag       Value[int]
+		notExported Value[int] `json:"not_exported"`
+	}{}
+
+	err := Initialize(info)
+	require.NoError(t, err)
+	// check that Initialize did not initialize any of the fields
+	require.Zero(t, info.NotValue)
+	require.Zero(t, info.NoTag)
+	require.Zero(t, info.notExported)
+}
+
+func TestInitialize(t *testing.T) {
+	info := &struct {
+		SomeInt Value[int] `json:"some_int"`
+	}{}
+	err := Initialize(info)
+	require.NoError(t, err)
+
+	_, err = info.SomeInt.Value()
+	var targetErr *NotCollectedError
+	require.ErrorAs(t, err, &targetErr)
+	// the struct is not declared so its package path is empty
+	require.Equal(t, "", targetErr.PkgName)
+	require.Equal(t, "some_int", targetErr.ValueName)
+}
