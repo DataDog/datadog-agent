2758ed2ff2486dcc21269b903d9d2d330e955d9f
usamasaqib <usamasaqib.96@live.com>
2023-05-22 19:23:31 +0200

    fix delete path of junit and testjson package

--- test/new-e2e/system-probe/test/micro-vm-init.sh
+++ test/new-e2e/system-probe/test/micro-vm-init.sh
@@ -17,8 +17,8 @@ find $KITCHEN_DOCKERS -maxdepth 1 -type f -exec docker load -i {} \;
 
 # Start tests
 IP=$(ip -f inet addr show $(ip route get $(getent ahosts google.com | awk '{print $1; exit}') | grep -Po '(?<=(dev ))(\S+)') | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
-rm -f /opt/kernel-version-testing/testjson-$IP.tar.gz
-rm -f /opt/kernel-version-testing/junit-$IP.tar.gz
+rm -f /testjson-$IP.tar.gz
+rm -f /junit-$IP.tar.gz
 
 CODE=0
 /system-probe-test_spec || CODE=$?
