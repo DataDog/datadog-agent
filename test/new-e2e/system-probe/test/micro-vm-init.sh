ed6af1d68bf8daf38da4ab1bfed181729ee2e7c0
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-05-23 15:13:18 -0700

    Simplify micro-vm-init.sh IP address command

--- test/new-e2e/system-probe/test/micro-vm-init.sh
+++ test/new-e2e/system-probe/test/micro-vm-init.sh
@@ -7,7 +7,7 @@ KITCHEN_DOCKERS=/kitchen-docker
 
 # Add provisioning steps here !
 ## Set go version correctly
-eval $(gimme $GOVERSION)
+eval $(gimme "$GOVERSION")
 ## Start docker
 systemctl start docker
 ## Load docker images
@@ -16,14 +16,14 @@ find $KITCHEN_DOCKERS -maxdepth 1 -type f -exec docker load -i {} \;
 # VM provisioning end !
 
 # Start tests
-IP=$(ip -f inet addr show $(ip route get $(getent ahosts google.com | awk '{print $1; exit}') | grep -Po '(?<=(dev ))(\S+)') | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
-rm -f /testjson-$IP.tar.gz
-rm -f /junit-$IP.tar.gz
+IP=$(ip route get 8.8.8.8 | grep -Po '(?<=(src ))(\S+)')
+rm -f "/testjson-$IP.tar.gz"
+rm -f "/junit-$IP.tar.gz"
 
 CODE=0
 /system-probe-test_spec || CODE=$?
 
-tar czvf /testjson-$IP.tar.gz /testjson
-tar czvf /junit-$IP.tar.gz /junit
+tar czvf "/testjson-$IP.tar.gz" /testjson
+tar czvf "/junit-$IP.tar.gz" /junit
 
 exit $CODE
