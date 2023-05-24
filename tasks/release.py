6bfa54efd93c84a25c02355bbf2414f97ae86fc9
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2023-05-24 11:17:53 +0200

    Merge some checks and move to utils to reuse lately (#17261)

--- tasks/release.py
+++ tasks/release.py
@@ -17,12 +17,11 @@ from invoke.exceptions import Exit
 from .libs.common.color import color_message
 from .libs.common.github_api import GithubAPI, get_github_token
 from .libs.common.gitlab import Gitlab, get_gitlab_token
-from .libs.common.remote_api import APIError
 from .libs.common.user_interactions import yes_no_question
 from .libs.version import Version
 from .modules import DEFAULT_MODULES
 from .pipeline import run
-from .utils import DEFAULT_BRANCH, get_version, nightly_entry_for, release_entry_for
+from .utils import DEFAULT_BRANCH, check_clean_branch_state, get_version, nightly_entry_for, release_entry_for
 
 # Generic version regex. Aims to match:
 # - X.Y.Z
@@ -936,41 +935,6 @@ def check_base_branch(branch, release_version):
     return branch == DEFAULT_BRANCH or branch == release_version.branch()
 
 
-def check_uncommitted_changes(ctx):
-    """
-    Checks if there are uncommitted changes in the local git repository.
-    """
-    modified_files = ctx.run("git --no-pager diff --name-only HEAD | wc -l", hide=True).stdout.strip()
-
-    # Return True if at least one file has uncommitted changes.
-    return modified_files != "0"
-
-
-def check_local_branch(ctx, branch):
-    """
-    Checks if the given branch exists locally
-    """
-    matching_branch = ctx.run(f"git --no-pager branch --list {branch} | wc -l", hide=True).stdout.strip()
-
-    # Return True if a branch is returned by git branch --list
-    return matching_branch != "0"
-
-
-def check_upstream_branch(github, branch):
-    """
-    Checks if the given branch already exists in the upstream repository
-    """
-    try:
-        github_branch = github.get_branch(branch)
-    except APIError as e:
-        if e.status_code == 404:
-            return False
-        raise e
-
-    # Return True if the branch exists
-    return github_branch and github_branch.get('name', False)
-
-
 def parse_major_versions(major_versions):
     return sorted(int(x) for x in major_versions.split(","))
 
@@ -1092,41 +1056,15 @@ def create_rc(ctx, major_versions="6,7", patch_version=False, upstream="origin")
     print(color_message("Checking repository state", "bold"))
     ctx.run("git fetch")
 
-    if check_uncommitted_changes(ctx):
-        raise Exit(
-            color_message(
-                "There are uncomitted changes in your repository. Please commit or stash them before trying again.",
-                "red",
-            ),
-            code=1,
-        )
-
     # Check that the current and update branches are valid
     current_branch = ctx.run("git rev-parse --abbrev-ref HEAD", hide=True).stdout.strip()
     update_branch = f"release/{new_highest_version}"
 
+    check_clean_branch_state(ctx, github, update_branch)
     if not check_base_branch(current_branch, new_highest_version):
         raise Exit(
             color_message(
                 f"The branch you are on is neither {DEFAULT_BRANCH} or the correct release branch ({new_highest_version.branch()}). Aborting.",
-                "red",
-            ),
-            code=1,
-        )
-
-    if check_local_branch(ctx, update_branch):
-        raise Exit(
-            color_message(
-                f"The branch {update_branch} already exists locally. Please remove it before trying again.",
-                "red",
-            ),
-            code=1,
-        )
-
-    if check_upstream_branch(github, update_branch):
-        raise Exit(
-            color_message(
-                f"The branch {update_branch} already exists upstream. Please remove it before trying again.",
                 "red",
             ),
             code=1,
@@ -1403,14 +1341,8 @@ def unfreeze(ctx, base_directory="~/dd", major_versions="6,7", upstream="origin"
     print(color_message("Checking repository state", "bold"))
     ctx.run("git fetch")
 
-    if check_uncommitted_changes(ctx):
-        raise Exit(
-            color_message(
-                "There are uncomitted changes in your repository. Please commit or stash them before trying again.",
-                "red",
-            ),
-            code=1,
-        )
+    github = GithubAPI(repository=REPOSITORY_NAME, api_token=get_github_token())
+    check_clean_branch_state(ctx, github, release_branch)
 
     if not yes_no_question(
         f"This task will create new branches with the name '{release_branch}' in repositories: {', '.join(UNFREEZE_REPOS)}. Is this OK?",
