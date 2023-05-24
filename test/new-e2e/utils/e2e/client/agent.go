af8f2a294c486a4f4d10b32ed24fe6e55fa41349
Olivier G <olivier.gaca@datadoghq.com>
2023-05-24 19:33:16 +0200

    Add documentation for e2e.Suite

--- test/new-e2e/utils/e2e/client/agent.go
+++ test/new-e2e/utils/e2e/client/agent.go
@@ -17,7 +17,9 @@ import (
 
 var _ clientService[agent.ClientData] = (*Agent)(nil)
 
-// A client Agent that is connected to an agent.Installer defined in test-infra-definition.
+// A client Agent that is connected to an [agent.Installer].
+//
+// [agent.Installer]: https://pkg.go.dev/github.com/DataDog/test-infra-definitions@main/components/datadog/agent#Installer
 type Agent struct {
 	*UpResultDeserializer[agent.ClientData]
 	*vmClient
@@ -62,22 +64,22 @@ func (agent *Agent) Status() *Status {
 	return newStatus(agent.vmClient.Execute("sudo datadog-agent status"))
 }
 
-// IsReady runs status command and returns true if the agent is ready
-// Use this to wait for agent to be ready before running any command
+// IsReady runs status command and returns true if the agent is ready.
+// Use this to wait for agent to be ready before running any command.
 func (a *Agent) IsReady() (bool, error) {
 	return a.Status().isReady()
 }
 
-// WaitForReady blocks up for one minute waiting for agent to be ready
-// Retries every 100 ms up to one minute
-// Returns error on failure
+// WaitForReady blocks up for one minute waiting for agent to be ready.
+// Retries every 100 ms up to one minute.
+// Returns error on failure.
 func (a *Agent) WaitForReady() error {
 	return a.WaitForReadyTimeout(1 * time.Minute)
 }
 
-// WaitForReady blocks up for timeout waiting for agent to be ready
-// Retries every 100 ms up to timeout
-// Returns error on failure
+// WaitForReady blocks up for timeout waiting for agent to be ready.
+// Retries every 100 ms up to timeout.
+// Returns error on failure.
 func (a *Agent) WaitForReadyTimeout(timeout time.Duration) error {
 	interval := 100 * time.Millisecond
 	maxRetries := timeout.Milliseconds() / interval.Milliseconds()
