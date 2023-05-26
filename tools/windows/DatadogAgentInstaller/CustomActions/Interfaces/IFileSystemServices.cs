6c379358dc6dc15b5f6fd2179183f352f02a56eb
Branden Clark <branden.clark@datadoghq.com>
2023-05-26 19:42:13 -0400

    add wrapper for file/dir access operations

---
+++ tools/windows/DatadogAgentInstaller/CustomActions/Interfaces/IFileSystemServices.cs
@@ -0,0 +1,14 @@
+using System.Security.AccessControl;
+
+namespace Datadog.CustomActions.Interfaces
+{
+    public interface IFileSystemServices
+    {
+        bool Exists(string path);
+        bool IsFile(string path);
+        bool IsDirectory(string path);
+        FileSystemSecurity GetAccessControl(string path, AccessControlSections includeSections);
+        FileSystemSecurity GetAccessControl(string path);
+        void SetAccessControl(string path, FileSystemSecurity fileSecurity);
+    }
+}
