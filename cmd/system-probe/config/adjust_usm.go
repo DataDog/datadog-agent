DataDog/datadog-agent

commit 7a865f735b2aca5fbbcd92a095494c524c05e865

Add structured way to adjust raw system-probe config (#17188)
* Add structured way to adjust raw system-probe config

* Reword max logs

* Add namespace function comments

* Fix tests

* Use uint32 for MaxTrackedConnections and MaxClosedConnectionsBuffered

* Add maximums for uint32 values

* Add defaults for collect_tcp/udp_v4/v6

* Fix panic from double channel close

* Change deprecation logs to WARN

* Add docs

* Fix error about missing config

* Formatting

* Fix darwin sysprobe config

* Ensure adjustments are made before using config

* change maxRouteCacheSize to uint32

* log deprecation message if oldkey is set, regardless of newkey

* Skip config tests on darwin

* Allow external adjustment and only do once

* automatically adjust for ebpf, network, and security config

* remove more config init

* undo formatting-only changes

* Fix test case

* enable NPM if network_events is true for test

* Fix closed_connection_flush_threshold validation

cmd/system-probe/config/adjust_usm.go
@@ -1,0 +1,51 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+const (
+	maxHTTPFrag = 160
+)
+
+func adjustUSM(cfg config.Config) {
+	deprecateBool(cfg, netNS("enable_http_monitoring"), smNS("enable_http_monitoring"))
+
+	if cfg.GetBool(dsmNS("enabled")) {
+		// DSM infers USM
+		cfg.Set(smNS("enabled"), true)
+	}
+
+	if cfg.GetBool(smNS("enabled")) {
+		// USM infers HTTP
+		cfg.Set(smNS("enable_http_monitoring"), true)
+		applyDefault(cfg, netNS("enable_https_monitoring"), true)
+		applyDefault(cfg, spNS("enable_runtime_compiler"), true)
+		applyDefault(cfg, spNS("enable_kernel_header_download"), true)
+	}
+
+	if cfg.GetBool(smNS("process_service_inference", "enabled")) &&
+		!cfg.GetBool(smNS("enabled")) &&
+		!cfg.GetBool(dsmNS("enabled")) {
+		log.Info("universal service monitoring and data streams monitoring are disabled, disabling process service inference")
+		cfg.Set(smNS("process_service_inference", "enabled"), false)
+	}
+
+	validateInt(cfg, netNS("http_notification_threshold"), cfg.GetInt(spNS("max_tracked_connections"))/2, func(v int) error {
+		limit := cfg.GetInt(netNS("max_tracked_http_connections"))
+		if v >= limit {
+			return fmt.Errorf("notification threshold %d set higher than tracked connections %d", v, limit)
+		}
+		return nil
+	})
+
+	limitMaxInt64(cfg, netNS("http_max_request_fragment"), maxHTTPFrag)
+}

pkg/network/config/config_test.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build linux || windows
+
 package config
 
 import (
@@ -489,15 +491,14 @@ func TestMaxClosedConnectionsBuffered(t *testing.T) {
 	t.Run("value set", func(t *testing.T) {
 		newConfig(t)
 		t.Setenv("DD_SYSTEM_PROBE_CONFIG_MAX_CLOSED_CONNECTIONS_BUFFERED", fmt.Sprintf("%d", maxTrackedConnections-1))
-
 		cfg := New()
-		require.Equal(t, int(maxTrackedConnections-1), cfg.MaxClosedConnectionsBuffered)
+		require.Equal(t, maxTrackedConnections-1, cfg.MaxClosedConnectionsBuffered)
 	})
 
 	t.Run("value not set", func(t *testing.T) {
 		newConfig(t)
 		cfg := New()
-		require.Equal(t, int(cfg.MaxTrackedConnections), cfg.MaxClosedConnectionsBuffered)
+		require.Equal(t, cfg.MaxTrackedConnections, cfg.MaxClosedConnectionsBuffered)
 	})
 }
 
@@ -505,7 +506,6 @@ func TestMaxHTTPStatsBuffered(t *testing.T) {
 	t.Run("value set through env var", func(t *testing.T) {
 		newConfig(t)
 		t.Setenv("DD_SYSTEM_PROBE_NETWORK_MAX_HTTP_STATS_BUFFERED", "50000")
-
 		cfg := New()
 		assert.Equal(t, 50000, cfg.MaxHTTPStatsBuffered)
 	})
@@ -516,7 +516,6 @@ func TestMaxHTTPStatsBuffered(t *testing.T) {
 network_config:
   max_http_stats_buffered: 30000
 `)
-
 		assert.Equal(t, 30000, cfg.MaxHTTPStatsBuffered)
 	})
 }

cmd/system-probe/config/config_test.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build linux || windows
+
 package config
 
 import (
@@ -51,13 +53,15 @@ func TestEventMonitor(t *testing.T) {
 		{cws: true, fim: true, process_events: true, network_events: true, enabled: true},
 	} {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Logf("%+v\n", tc)
 			t.Setenv("DD_RUNTIME_SECURITY_CONFIG_ENABLED", strconv.FormatBool(tc.cws))
 			t.Setenv("DD_RUNTIME_SECURITY_CONFIG_FIM_ENABLED", strconv.FormatBool(tc.fim))
 			t.Setenv("DD_SYSTEM_PROBE_EVENT_MONITORING_PROCESS_ENABLED", strconv.FormatBool(tc.process_events))
 			t.Setenv("DD_SYSTEM_PROBE_EVENT_MONITORING_NETWORK_PROCESS_ENABLED", strconv.FormatBool(tc.network_events))
+			t.Setenv("DD_SYSTEM_PROBE_NETWORK_ENABLED", strconv.FormatBool(tc.network_events))
 
-			cfg, err := New("")
-			t.Log(cfg)
+			cfg, err := New("/doesnotexist")
+			t.Logf("%+v\n", cfg)
 			require.NoError(t, err)
 			assert.Equal(t, tc.enabled, cfg.ModuleIsEnabled(EventMonitorModule))
 		})

.github/CODEOWNERS
@@ -135,6 +135,9 @@
 /cmd/serverless/                        @DataDog/serverless
 /cmd/serverless-init/                   @DataDog/serverless
 /cmd/system-probe/                      @DataDog/ebpf-platform
+/cmd/system-probe/config/adjust_npm.go  @DataDog/ebpf-platform @DataDog/Networks
+/cmd/system-probe/config/adjust_usm.go  @DataDog/ebpf-platform @DataDog/universal-service-monitoring
+/cmd/system-probe/config/adjust_security.go  @DataDog/ebpf-platform @DataDog/agent-security
 /cmd/system-probe/modules/network_tracer*   @DataDog/Networks
 /cmd/system-probe/modules/oom_kill_probe*   @DataDog/container-integrations
 /cmd/system-probe/modules/process*      @DataDog/processes

cmd/system-probe/config/adjust.go
@@ -1,0 +1,153 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package config
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+var adjustMtx sync.Mutex
+
+// Adjust makes changes to the raw config based on deprecations and inferences.
+func Adjust(cfg config.Config) {
+	if IsAdjusted(cfg) {
+		return
+	}
+	adjustMtx.Lock()
+	defer adjustMtx.Unlock()
+
+	deprecateString(cfg, spNS("log_level"), "log_level")
+	deprecateString(cfg, spNS("log_file"), "log_file")
+
+	usmEnabled := cfg.GetBool(smNS("enabled"))
+	dsmEnabled := cfg.GetBool(dsmNS("enabled"))
+	// this check must come first, so we can accurately tell if system_probe was explicitly enabled
+	if cfg.GetBool(spNS("enabled")) &&
+		!cfg.IsSet(netNS("enabled")) &&
+		!usmEnabled &&
+		!dsmEnabled {
+		// This case exists to preserve backwards compatibility. If system_probe_config.enabled is explicitly set to true, and there is no network_config block,
+		// enable the connections/network check.
+		log.Warn(deprecationMessage(spNS("enabled"), netNS("enabled")))
+		// ensure others can key off of this single config value for NPM status
+		cfg.Set(netNS("enabled"), true)
+	}
+
+	validateString(cfg, spNS("sysprobe_socket"), defaultSystemProbeAddress, ValidateSocketAddress)
+
+	adjustNetwork(cfg)
+	adjustUSM(cfg)
+	adjustSecurity(cfg)
+
+	cfg.Set(spNS("adjusted"), true)
+}
+
+// IsAdjusted returns whether the configuration has already been adjusted by Adjust
+func IsAdjusted(cfg config.Config) bool {
+	adjustMtx.Lock()
+	defer adjustMtx.Unlock()
+	return cfg.GetBool(spNS("adjusted"))
+}
+
+// validateString validates the string configuration value at `key` using a custom provided function `valFn`.
+// If `key` is not set or `valFn` returns an error, the `defaultVal` is used instead.
+func validateString(cfg config.Config, key string, defaultVal string, valFn func(string) error) {
+	if cfg.IsSet(key) {
+		if err := valFn(cfg.GetString(key)); err != nil {
+			log.Errorf("error validating `%s`: %s, using default value of `%s`", key, err, defaultVal)
+			cfg.Set(key, defaultVal)
+		}
+	} else {
+		cfg.Set(key, defaultVal)
+	}
+}
+
+// validateInt validates the int configuration value at `key` using a custom provided function `valFn`.
+// If `key` is not set or `valFn` returns an error, the `defaultVal` is used instead.
+func validateInt(cfg config.Config, key string, defaultVal int, valFn func(int) error) {
+	if cfg.IsSet(key) {
+		if err := valFn(cfg.GetInt(key)); err != nil {
+			log.Errorf("error validating `%s`: %s, using default value of `%d`", key, err, defaultVal)
+			cfg.Set(key, defaultVal)
+		}
+	} else {
+		cfg.Set(key, defaultVal)
+	}
+}
+
+// validateInt64 validates the int64 configuration value at `key` using a custom provided function `valFn`.
+// If `key` is not set or `valFn` returns an error, the `defaultVal` is used instead.
+func validateInt64(cfg config.Config, key string, defaultVal int64, valFn func(int64) error) {
+	if cfg.IsSet(key) {
+		if err := valFn(cfg.GetInt64(key)); err != nil {
+			log.Errorf("error validating `%s`: %s. using default value of `%d`", key, err, defaultVal)
+			cfg.Set(key, defaultVal)
+		}
+	} else {
+		cfg.Set(key, defaultVal)
+	}
+}
+
+// applyDefault sets configuration `key` to `defaultVal` only if not previously set.
+func applyDefault(cfg config.Config, key string, defaultVal interface{}) {
+	if !cfg.IsSet(key) {
+		cfg.Set(key, defaultVal)
+	}
+}
+
+// deprecateBool logs a deprecation message if `oldkey` is used.
+// It sets `newkey` to the value obtained from `getFn`, but only if `oldkey` is set and `newkey` is not set.
+func deprecateBool(cfg config.Config, oldkey string, newkey string) {
+	deprecateCustom(cfg, oldkey, newkey, func(cfg config.Config) interface{} {
+		return cfg.GetBool(oldkey)
+	})
+}
+
+// deprecateString logs a deprecation message if `oldkey` is used.
+// It sets `newkey` to the value obtained from `getFn`, but only if `oldkey` is set and `newkey` is not set.
+func deprecateString(cfg config.Config, oldkey string, newkey string) {
+	deprecateCustom(cfg, oldkey, newkey, func(cfg config.Config) interface{} {
+		return cfg.GetString(oldkey)
+	})
+}
+
+// deprecateCustom logs a deprecation message if `oldkey` is used.
+// It sets `newkey` to the value obtained from `getFn`, but only if `oldkey` is set and `newkey` is not set.
+func deprecateCustom(cfg config.Config, oldkey string, newkey string, getFn func(config.Config) interface{}) {
+	if cfg.IsSet(oldkey) {
+		log.Warn(deprecationMessage(oldkey, newkey))
+		if !cfg.IsSet(newkey) {
+			cfg.Set(newkey, getFn(cfg))
+		}
+	}
+}
+
+// deprecationMessage returns the standard deprecation message
+func deprecationMessage(oldkey, newkey string) string {
+	return fmt.Sprintf("configuration key `%s` is deprecated, use `%s` instead", oldkey, newkey)
+}
+
+// limitMaxInt logs a warning and sets `key` to `max` if the value exceeds `max`.
+func limitMaxInt(cfg config.Config, key string, max int) {
+	val := cfg.GetInt(key)
+	if val > max {
+		log.Warnf("configuration key `%s` was set to `%d`, using maximum value `%d` instead", key, val, max)
+		cfg.Set(key, max)
+	}
+}
+
+// limitMaxInt64 logs a warning and sets `key` to `max` if the value exceeds `max`.
+func limitMaxInt64(cfg config.Config, key string, max int64) {
+	val := cfg.GetInt64(key)
+	if val > max {
+		log.Warnf("configuration key `%s` was set to `%d`, using maximum value `%d` instead", key, val, max)
+		cfg.Set(key, max)
+	}
+}

cmd/system-probe/config/adjust_npm.go
@@ -1,0 +1,79 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package config
+
+import (
+	"fmt"
+	"math"
+	"runtime"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/util/kernel"
+)
+
+const (
+	maxConnsMessageBatchSize     = 1000
+	maxOffsetThreshold           = 3000
+	defaultMaxProcessesTracked   = 1024
+	defaultMaxTrackedConnections = 65536
+)
+
+func adjustNetwork(cfg config.Config) {
+	limitMaxInt(cfg, spNS("max_conns_per_message"), maxConnsMessageBatchSize)
+
+	if cfg.GetBool(spNS("disable_tcp")) {
+		cfg.Set(netNS("collect_tcp_v4"), false)
+		cfg.Set(netNS("collect_tcp_v6"), false)
+	}
+	if cfg.GetBool(spNS("disable_udp")) {
+		cfg.Set(netNS("collect_udp_v4"), false)
+		cfg.Set(netNS("collect_udp_v6"), false)
+	}
+	if cfg.GetBool(spNS("disable_ipv6")) || !kernel.IsIPv6Enabled() {
+		cfg.Set(netNS("collect_tcp_v6"), false)
+		cfg.Set(netNS("collect_udp_v6"), false)
+	}
+
+	if runtime.GOOS == "windows" {
+		validateInt(cfg, spNS("closed_connection_flush_threshold"), 0, func(v int) error {
+			if v != 0 && v < 1024 {
+				return fmt.Errorf("closed connection notification threshold set to invalid value %d. resetting to default", v)
+			}
+			return nil
+		})
+	}
+
+	validateInt64(cfg, spNS("max_tracked_connections"), defaultMaxTrackedConnections, func(v int64) error {
+		if v <= 0 {
+			return fmt.Errorf("must be a positive value")
+		}
+		return nil
+	})
+	limitMaxInt64(cfg, spNS("max_tracked_connections"), math.MaxUint32)
+	// make sure max_closed_connections_buffered is equal to max_tracked_connections,
+	// if the former is not set. this helps with lowering or eliminating dropped
+	// closed connections in environments with mostly short-lived connections
+	validateInt64(cfg, spNS("max_closed_connections_buffered"), cfg.GetInt64(spNS("max_tracked_connections")), func(v int64) error {
+		if v <= 0 {
+			return fmt.Errorf("must be a positive value")
+		}
+		return nil
+	})
+	limitMaxInt64(cfg, spNS("max_closed_connections_buffered"), math.MaxUint32)
+
+	limitMaxInt(cfg, spNS("offset_guess_threshold"), maxOffsetThreshold)
+
+	if !cfg.GetBool(netNS("enable_root_netns")) {
+		cfg.Set(spNS("enable_conntrack_all_namespaces"), false)
+	}
+
+	validateInt(cfg, evNS("network_process", "max_processes_tracked"), defaultMaxProcessesTracked, func(v int) error {
+		if v <= 0 {
+			return fmt.Errorf("`%d` is 0 or less", v)
+		}
+		return nil
+	})
+}

cmd/system-probe/config/adjust_security.go
@@ -1,0 +1,30 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package config
+
+import (
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+)
+
+func adjustSecurity(cfg config.Config) {
+	deprecateCustom(cfg, secNS("activity_dump.cgroup_dump_timeout"), secNS("activity_dump.dump_duration"), func(cfg config.Config) interface{} {
+		// convert old minutes int value to time.Duration
+		return time.Duration(cfg.GetInt(secNS("activity_dump.cgroup_dump_timeout"))) * time.Minute
+	})
+
+	if cfg.GetBool(secNS("enabled")) {
+		// if runtime is enabled then we force fim
+		cfg.Set(secNS("fim_enabled"), true)
+	} else {
+		// if runtime is disabled then we force disable activity dumps
+		cfg.Set(secNS("activity_dump.enabled"), false)
+	}
+
+	// further adjustments done in RuntimeSecurityConfig.sanitize
+	// because it requires access to security packages
+}

cmd/system-probe/config/config.go
@@ -16,7 +16,6 @@ import (
 	"github.com/DataDog/viper"
 
 	aconfig "github.com/DataDog/datadog-agent/pkg/config"
-	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 // ModuleName is a typed alias for string, used only for module names
@@ -25,14 +24,6 @@ type ModuleName string
 const (
 	// Namespace is the top-level configuration key that all system-probe settings are nested underneath
 	Namespace = "system_probe_config"
-	spNS      = Namespace
-	netNS     = "network_config"
-	smNS      = "service_monitoring_config"
-	dsmNS     = "data_streams_config"
-	diNS      = "dynamic_instrumentation"
-
-	defaultConnsMessageBatchSize = 600
-	maxConnsMessageBatchSize     = 1000
 )
 
 // system-probe module names
@@ -45,10 +36,6 @@ const (
 	EventMonitorModule           ModuleName = "event_monitor"
 	DynamicInstrumentationModule ModuleName = "dynamic_instrumentation"
 )
-
-func key(pieces ...string) string {
-	return strings.Join(pieces, ".")
-}
 
 // Config represents the configuration options for the system-probe
 type Config struct {
@@ -82,6 +69,11 @@ func NewCustom(configPath string, loadSecrets bool) (*Config, error) {
 }
 
 func newSysprobeConfig(configPath string, loadSecrets bool) (*Config, error) {
+	// System probe is not supported on darwin, so we should fail gracefully in this case.
+	if runtime.GOOS == "darwin" {
+		return &Config{}, nil
+	}
+
 	aconfig.SystemProbe.SetConfigName("system-probe")
 	// set the paths where a config file is expected
 	if len(configPath) != 0 {
@@ -99,15 +91,6 @@ func newSysprobeConfig(configPath string, loadSecrets bool) (*Config, error) {
 	// load the configuration
 	_, err := aconfig.LoadCustom(aconfig.SystemProbe, "system-probe", loadSecrets, aconfig.Datadog.GetEnvVars())
 	if err != nil {
-		// System probe is not supported on darwin, so we should fail gracefully in this case.
-		if runtime.GOOS != "darwin" {
-			if errors.Is(err, os.ErrPermission) {
-				log.Warnf("Error loading config: %v (check config file permissions for dd-agent user)", err)
-			} else {
-				log.Warnf("Error loading config: %v", err)
-			}
-		}
-
 		var e viper.ConfigFileNotFoundError
 		if errors.As(err, &e) || errors.Is(err, os.ErrNotExist) {
 			// do nothing, we can ignore a missing system-probe.yaml config file
@@ -127,102 +110,55 @@ func newSysprobeConfig(configPath string, loadSecrets bool) (*Config, error) {
 
 func load() (*Config, error) {
 	cfg := aconfig.SystemProbe
+	Adjust(cfg)
 
 	c := &Config{
-		Enabled:             cfg.GetBool(key(spNS, "enabled")),
+		Enabled:             cfg.GetBool(spNS("enabled")),
 		EnabledModules:      make(map[ModuleName]struct{}),
-		ExternalSystemProbe: cfg.GetBool(key(spNS, "external")),
+		ExternalSystemProbe: cfg.GetBool(spNS("external")),
 
-		SocketAddress:      cfg.GetString(key(spNS, "sysprobe_socket")),
-		MaxConnsPerMessage: cfg.GetInt(key(spNS, "max_conns_per_message")),
+		SocketAddress:      cfg.GetString(spNS("sysprobe_socket")),
+		MaxConnsPerMessage: cfg.GetInt(spNS("max_conns_per_message")),
 
 		LogFile:          cfg.GetString("log_file"),
 		LogLevel:         cfg.GetString("log_level"),
-		DebugPort:        cfg.GetInt(key(spNS, "debug_port")),
-		TelemetryEnabled: cfg.GetBool(key(spNS, "telemetry_enabled")),
+		DebugPort:        cfg.GetInt(spNS("debug_port")),
+		TelemetryEnabled: cfg.GetBool(spNS("telemetry_enabled")),
 
 		StatsdHost: aconfig.GetBindHost(),
 		StatsdPort: cfg.GetInt("dogstatsd_port"),
 	}
 
-	// backwards compatible log settings
-	if !cfg.IsSet("log_level") && cfg.IsSet(key(spNS, "log_level")) {
-		c.LogLevel = cfg.GetString(key(spNS, "log_level"))
-		cfg.Set("log_level", c.LogLevel)
-	}
-	if !cfg.IsSet("log_file") && cfg.IsSet(key(spNS, "log_file")) {
-		c.LogFile = cfg.GetString(key(spNS, "log_file"))
-		cfg.Set("log_file", c.LogFile)
-	}
-
-	handleBackwardCompatibilityForUsmConfig(cfg)
-
-	if c.MaxConnsPerMessage > maxConnsMessageBatchSize {
-		log.Warn("Overriding the configured connections count per message limit because it exceeds maximum")
-		c.MaxConnsPerMessage = defaultConnsMessageBatchSize
-		cfg.Set(key(spNS, "max_conns_per_message"), c.MaxConnsPerMessage)
-	}
-
-	// this check must come first, so we can accurately tell if system_probe was explicitly enabled
-	npmEnabled := cfg.GetBool("network_config.enabled")
-	usmEnabled := cfg.GetBool(key(smNS, "enabled"))
-	dsmEnabled := cfg.GetBool(key(dsmNS, "enabled"))
-
-	if c.Enabled && !cfg.IsSet("network_config.enabled") && !usmEnabled && !dsmEnabled {
-		// This case exists to preserve backwards compatibility. If system_probe_config.enabled is explicitly set to true, and there is no network_config block,
-		// enable the connections/network check.
-		log.Info("`system_probe_config.enabled` is deprecated, enable NPM with `network_config.enabled` instead")
-		// ensure others can key off of this single config value for NPM status
-		cfg.Set("network_config.enabled", true)
-		npmEnabled = true
-	}
+	npmEnabled := cfg.GetBool(netNS("enabled"))
+	usmEnabled := cfg.GetBool(smNS("enabled"))
+	dsmEnabled := cfg.GetBool(dsmNS("enabled"))
 
 	if npmEnabled || usmEnabled || dsmEnabled {
 		c.EnabledModules[NetworkTracerModule] = struct{}{}
 	}
-	if cfg.GetBool(key(spNS, "enable_tcp_queue_length")) {
+	if cfg.GetBool(spNS("enable_tcp_queue_length")) {
 		c.EnabledModules[TCPQueueLengthTracerModule] = struct{}{}
 	}
-	if cfg.GetBool(key(spNS, "enable_oom_kill")) {
+	if cfg.GetBool(spNS("enable_oom_kill")) {
 		c.EnabledModules[OOMKillProbeModule] = struct{}{}
 	}
 
-	if cfg.GetBool("runtime_security_config.enabled") ||
-		cfg.GetBool("runtime_security_config.fim_enabled") ||
-		cfg.GetBool("event_monitoring_config.process.enabled") ||
-		(c.ModuleIsEnabled(NetworkTracerModule) && cfg.GetBool("event_monitoring_config.network_process.enabled")) {
+	if cfg.GetBool(secNS("enabled")) ||
+		cfg.GetBool(secNS("fim_enabled")) ||
+		cfg.GetBool(evNS("process.enabled")) ||
+		(c.ModuleIsEnabled(NetworkTracerModule) && cfg.GetBool(evNS("network_process.enabled"))) {
 		c.EnabledModules[EventMonitorModule] = struct{}{}
 	}
-	if cfg.GetBool(key(spNS, "process_config.enabled")) {
+	if cfg.GetBool(spNS("process_config.enabled")) {
 		c.EnabledModules[ProcessModule] = struct{}{}
 	}
-
-	if cfg.GetBool(key(diNS, "enabled")) {
+	if cfg.GetBool(diNS("enabled")) {
 		c.EnabledModules[DynamicInstrumentationModule] = struct{}{}
 	}
 
-	if len(c.EnabledModules) > 0 {
-		c.Enabled = true
-		if err := ValidateSocketAddress(c.SocketAddress); err != nil {
-			log.Errorf("Could not parse %s.sysprobe_socket: %s", spNS, err)
-			c.SocketAddress = defaultSystemProbeAddress
-		}
-	} else {
-		c.Enabled = false
-		c.SocketAddress = ""
-	}
-
-	cfg.Set(key(spNS, "sysprobe_socket"), c.SocketAddress)
-	cfg.Set(key(spNS, "enabled"), c.Enabled)
-
-	if cfg.GetBool(key(smNS, "process_service_inference", "enabled")) {
-		if !usmEnabled && !dsmEnabled {
-			log.Info("Both service monitoring and data streams monitoring are disabled, disabling process service inference")
-			cfg.Set(key(smNS, "process_service_inference", "enabled"), false)
-		} else {
-			log.Info("process service inference is enabled")
-		}
-	}
+	c.Enabled = len(c.EnabledModules) > 0
+	// only allowed raw config adjustments here, otherwise use Adjust function
+	cfg.Set(spNS("enabled"), c.Enabled)
 
 	return c, nil
 }
@@ -233,12 +169,7 @@ func (c Config) ModuleIsEnabled(modName ModuleName) bool {
 	return ok
 }
 
-// SetupOptionalDatadogConfig loads the datadog.yaml config file but will not fail on a missing file
-func SetupOptionalDatadogConfig() error {
-	return SetupOptionalDatadogConfigWithDir(defaultConfigDir, "")
-}
-
-// SetupOptionalDatadogConfig loads the datadog.yaml config file from a given config directory but will not fail on a missing file
+// SetupOptionalDatadogConfigWithDir loads the datadog.yaml config file from a given config directory but will not fail on a missing file
 func SetupOptionalDatadogConfigWithDir(configDir, configFile string) error {
 	aconfig.Datadog.AddConfigPath(configDir)
 	if configFile != "" {
@@ -262,17 +193,4 @@ func SetupOptionalDatadogConfigWithDir(configDir, configFile string) error {
 		return err
 	}
 	return nil
-}
-
-func handleBackwardCompatibilityForUsmConfig(cfg aconfig.Config) {
-	// This code block handles the backward compatibility logic for the enable_http_monitoring configuration value
-	deprecatedEnableHttpMonitoringKey := key(netNS, "enable_http_monitoring")
-	if cfg.IsSet(deprecatedEnableHttpMonitoringKey) {
-		enableHttpMonitoringKey := key(smNS, "enable_http_monitoring")
-		log.Infof("%q is deprecated, use %q instead",
-			deprecatedEnableHttpMonitoringKey, enableHttpMonitoringKey)
-		if !cfg.IsSet(enableHttpMonitoringKey) {
-			cfg.Set(enableHttpMonitoringKey, cfg.GetBool(deprecatedEnableHttpMonitoringKey))
-		}
-	}
 }

commit 4f9b0d741cc3d5629dcba436779545b73f17053a

usm: Fixed a bug introduced in #17188

cmd/system-probe/config/adjust_usm.go
@@ -39,7 +39,7 @@ func adjustUSM(cfg config.Config) {
 		cfg.Set(smNS("process_service_inference", "enabled"), false)
 	}
 
-	validateInt(cfg, netNS("http_notification_threshold"), cfg.GetInt(spNS("max_tracked_connections"))/2, func(v int) error {
+	validateInt(cfg, netNS("http_notification_threshold"), cfg.GetInt(netNS("max_tracked_http_connections"))/2, func(v int) error {
 		limit := cfg.GetInt(netNS("max_tracked_http_connections"))
 		if v >= limit {
 			return fmt.Errorf("notification threshold %d set higher than tracked connections %d", v, limit)

pkg/network/config/config_test.go
@@ -485,6 +485,61 @@ func TestHTTPReplaceRules(t *testing.T) {
 	})
 }
 
+func TestHTTPNotificationThreshold(t *testing.T) {
+	t.Run("via YAML", func(t *testing.T) {
+		newConfig(t)
+		_, err := sysconfig.New("./testdata/TestDDSystemProbeConfig-HTTPNotificationThreshold.yaml")
+		require.NoError(t, err)
+		cfg := New()
+
+		require.Equal(t, cfg.HTTPNotificationThreshold, int64(100))
+	})
+
+	t.Run("via ENV variable", func(t *testing.T) {
+		newConfig(t)
+		t.Setenv("DD_NETWORK_CONFIG_HTTP_NOTIFICATION_THRESHOLD", "100")
+
+		cfg := New()
+
+		require.Equal(t, cfg.HTTPNotificationThreshold, int64(100))
+	})
+
+	t.Run("Not enabled", func(t *testing.T) {
+		newConfig(t)
+		cfg := New()
+		// Default value.
+		require.Equal(t, cfg.HTTPNotificationThreshold, int64(512))
+	})
+}
+
+// Testing we're not exceeding the limit for http_notification_threshold.
+func TestHTTPNotificationThresholdOverLimit(t *testing.T) {
+	t.Run("via YAML", func(t *testing.T) {
+		newConfig(t)
+		_, err := sysconfig.New("./testdata/TestDDSystemProbeConfig-HTTPNotificationThresholdOverLimit.yaml")
+		require.NoError(t, err)
+		cfg := New()
+
+		require.Equal(t, cfg.HTTPNotificationThreshold, int64(512))
+	})
+
+	t.Run("via ENV variable", func(t *testing.T) {
+		newConfig(t)
+		t.Setenv("DD_NETWORK_CONFIG_HTTP_NOTIFICATION_THRESHOLD", "2000")
+
+		cfg := New()
+
+		require.Equal(t, cfg.HTTPNotificationThreshold, int64(512))
+	})
+
+	t.Run("Not enabled", func(t *testing.T) {
+		newConfig(t)
+		cfg := New()
+		// Default value.
+		require.Equal(t, cfg.HTTPNotificationThreshold, int64(512))
+	})
+}
+
 func TestMaxClosedConnectionsBuffered(t *testing.T) {
 	maxTrackedConnections := New().MaxTrackedConnections
 

pkg/network/config/testdata/TestDDSystemProbeConfig-HTTPNotificationThreshold.yaml
@@ -1,0 +1,2 @@
+network_config:
+  http_notification_threshold: 100

pkg/network/config/testdata/TestDDSystemProbeConfig-HTTPNotificationThresholdOverLimit.yaml
@@ -1,0 +1,2 @@
+network_config:
+  http_notification_threshold: 2000