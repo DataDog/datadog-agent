2fd5e29e9530ebd457d4788d3c73f789976a71a4
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2023-05-30 11:33:50 +0200

    both metrics

--- pkg/collector/corechecks/oracle-dbm/sysmetrics.go
+++ pkg/collector/corechecks/oracle-dbm/sysmetrics.go
@@ -12,7 +12,10 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/collector/corechecks/oracle-dbm/common"
 )
 
-const SYSMETRICS_QUERY = "SELECT metric_name, value, name pdb_name FROM v$con_sysmetric s, v$containers c WHERE s.con_id = c.con_id(+)"
+const SYSMETRICS_QUERY = `SELECT metric_name, value, name pdb_name 
+  FROM %s s, v$containers c 
+  WHERE s.con_id = c.con_id(+)
+  ORDER BY begin_time DESC`
 
 type SysmetricsRowDB struct {
 	MetricName string         `db:"METRIC_NAME"`
@@ -20,48 +23,69 @@ type SysmetricsRowDB struct {
 	PdbName    sql.NullString `db:"PDB_NAME"`
 }
 
+type sysMetricsDefinition struct {
+	DDmetric               string
+	DBM                    bool
+	seenInContainerMetrics bool
+}
+
 func (c *Check) SysMetrics() error {
-	SYSMETRICS_COLS := map[string]string{
-		"Buffer Cache Hit Ratio":          "buffer_cachehit_ratio",
-		"Cursor Cache Hit Ratio":          "cursor_cachehit_ratio",
-		"Library Cache Hit Ratio":         "library_cachehit_ratio",
-		"Shared Pool Free %":              "shared_pool_free",
-		"Physical Reads Per Sec":          "physical_reads",
-		"Physical Writes Per Sec":         "physical_writes",
-		"Enqueue Timeouts Per Sec":        "enqueue_timeouts",
-		"GC CR Block Received Per Second": "gc_cr_block_received",
-		"Global Cache Blocks Corrupted":   "cache_blocks_corrupt",
-		"Global Cache Blocks Lost":        "cache_blocks_lost",
-		"Logons Per Sec":                  "logons",
-		"Average Active Sessions":         "active_sessions",
-		"Long Table Scans Per Sec":        "long_table_scans",
-		"SQL Service Response Time":       "service_response_time",
-		"User Rollbacks Per Sec":          "user_rollbacks",
-		"Total Sorts Per User Call":       "sorts_per_user_call",
-		"Rows Per Sort":                   "rows_per_sort",
-		"Disk Sort Per Sec":               "disk_sorts",
-		"Memory Sorts Ratio":              "memory_sorts_ratio",
-		"Database Wait Time Ratio":        "database_wait_time_ratio",
-		"Session Limit %":                 "session_limit_usage",
-		"Session Count":                   "session_count",
-		"Temp Space Used":                 "temp_space_used",
+	SYSMETRICS_COLS := map[string]sysMetricsDefinition{
+		"Buffer Cache Hit Ratio":          {DDmetric: "buffer_cachehit_ratio"},
+		"Cursor Cache Hit Ratio":          {DDmetric: "cursor_cachehit_ratio"},
+		"Library Cache Hit Ratio":         {DDmetric: "library_cachehit_ratio"},
+		"Shared Pool Free %":              {DDmetric: "shared_pool_free"},
+		"Physical Reads Per Sec":          {DDmetric: "physical_reads"},
+		"Physical Writes Per Sec":         {DDmetric: "physical_writes"},
+		"Enqueue Timeouts Per Sec":        {DDmetric: "enqueue_timeouts"},
+		"GC CR Block Received Per Second": {DDmetric: "gc_cr_block_received"},
+		"Global Cache Blocks Corrupted":   {DDmetric: "cache_blocks_corrupt"},
+		"Global Cache Blocks Lost":        {DDmetric: "cache_blocks_lost"},
+		"Logons Per Sec":                  {DDmetric: "logons"},
+		"Average Active Sessions":         {DDmetric: "active_sessions"},
+		"Long Table Scans Per Sec":        {DDmetric: "long_table_scans"},
+		"SQL Service Response Time":       {DDmetric: "service_response_time"},
+		"User Rollbacks Per Sec":          {DDmetric: "user_rollbacks"},
+		"Total Sorts Per User Call":       {DDmetric: "sorts_per_user_call"},
+		"Rows Per Sort":                   {DDmetric: "rows_per_sort"},
+		"Disk Sort Per Sec":               {DDmetric: "disk_sorts"},
+		"Memory Sorts Ratio":              {DDmetric: "memory_sorts_ratio"},
+		"Database Wait Time Ratio":        {DDmetric: "database_wait_time_ratio"},
+		"Session Limit %":                 {DDmetric: "session_limit_usage"},
+		"Session Count":                   {DDmetric: "session_count"},
+		"Temp Space Used":                 {DDmetric: "temp_space_used"},
 	}
 
-	metricRows := []SysmetricsRowDB{}
-	err := c.db.Select(&metricRows, SYSMETRICS_QUERY)
-
-	if err != nil {
-		return fmt.Errorf("failed to collect sysmetrics: %w", err)
-	}
 	sender, err := c.GetSender()
 	if err != nil {
 		return fmt.Errorf("failed to initialize sender: %w", err)
 	}
+
+	metricRows := []SysmetricsRowDB{}
+	err = c.db.Select(&metricRows, fmt.Sprintf(SYSMETRICS_QUERY, "v$con_sysmetric"))
+	if err != nil {
+		return fmt.Errorf("failed to collect container sysmetrics: %w", err)
+	}
+	seenInContainerMetrics := make(map[string]bool)
 	for _, r := range metricRows {
 		if metric, ok := SYSMETRICS_COLS[r.MetricName]; ok {
-			sender.Gauge(fmt.Sprintf("%s.%s", common.IntegrationName, metric), r.Value, "", appendPDBTag(c.tags, r.PdbName))
+			sender.Gauge(fmt.Sprintf("%s.%s", common.IntegrationName, metric.DDmetric), r.Value, "", appendPDBTag(c.tags, r.PdbName))
+			seenInContainerMetrics[r.MetricName] = true
 		}
 	}
+
+	err = c.db.Select(&metricRows, fmt.Sprintf(SYSMETRICS_QUERY, "v$sysmetric"))
+	if err != nil {
+		return fmt.Errorf("failed to collect sysmetrics: %w", err)
+	}
+	for _, r := range metricRows {
+		if metric, ok := SYSMETRICS_COLS[r.MetricName]; ok {
+			if _, ok := seenInContainerMetrics[r.MetricName]; !ok {
+				sender.Gauge(fmt.Sprintf("%s.%s", common.IntegrationName, metric.DDmetric), r.Value, "", appendPDBTag(c.tags, r.PdbName))
+			}
+		}
+	}
+
 	sender.Commit()
 	return nil
 }
