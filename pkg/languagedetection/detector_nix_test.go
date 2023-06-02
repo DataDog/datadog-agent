5e567ab2ac92207a207ffa1f8db46e15483a842f
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-06-02 17:42:31 -0400

    Fix import cycle

--- pkg/languagedetection/detector_nix_test.go
+++ pkg/languagedetection/detector_nix_test.go
@@ -12,12 +12,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/DataDog/datadog-agent/pkg/process/procutil"
 )
 
-func makeProcess(cmdline []string) *procutil.Process {
-	return &procutil.Process{
+func makeProcess(cmdline []string) *Process {
+	return &Process{
 		Pid:     rand.Int31(),
 		Cmdline: cmdline,
 	}
@@ -157,7 +155,7 @@ func BenchmarkDetectLanguage(b *testing.B) {
 		{"javap", "-c", "MyClass"},
 	}
 
-	var procs []*procutil.Process
+	var procs []*Process
 	for _, command := range commands {
 		procs = append(procs, makeProcess(command))
 	}
