05db9466999e329adc50cdb8ac907b57814f2bc9
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-06-02 17:05:03 -0400

    Add extractor tests

---
+++ pkg/process/metadata/workloadmeta/extractor_test.go
@@ -0,0 +1,76 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package workloadmeta
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/DataDog/datadog-agent/pkg/languagedetection"
+	"github.com/DataDog/datadog-agent/pkg/process/procutil"
+)
+
+const (
+	Pid1 = 1000
+	Pid2 = 1001
+)
+
+func TestExtractor(t *testing.T) {
+	wlmExtractor := NewWorkloadMetaExtractor()
+
+	type testCase struct {
+		name     string
+		procs    map[int32]*procutil.Process
+		expected map[int32]*languagedetection.Language
+	}
+	for _, tc := range []testCase{
+		{
+			name: "java & python",
+			procs: map[int32]*procutil.Process{
+				Pid1: {
+					Pid:     Pid1,
+					Cmdline: []string{"java", "TestClass"},
+				},
+				Pid2: {
+					Pid:     Pid2,
+					Cmdline: []string{"python", "main.py"},
+				},
+			},
+			expected: map[int32]*languagedetection.Language{
+				Pid1: {Name: languagedetection.Java},
+				Pid2: {Name: languagedetection.Python},
+			},
+		},
+		{
+			// This TC is an edge case where the pid in the map doesn't match the pid in the process.
+			// In this case we should not panic.
+			name: "mismatched pid",
+			procs: map[int32]*procutil.Process{
+				Pid1: {
+					Pid:     Pid2,
+					Cmdline: []string{"dotnet"},
+				},
+			},
+			expected: map[int32]*languagedetection.Language{
+				Pid1: nil,
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			wlmExtractor.Extract(tc.procs)
+			for pid, lang := range tc.expected {
+				proc := tc.procs[pid]
+				if lang == nil {
+					assert.Nil(t, proc.Language)
+					continue
+				}
+
+				assert.Equal(t, lang.Name, proc.Language.Name)
+			}
+		})
+	}
+}
