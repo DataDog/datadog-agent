16eec81595df2f306bb16fc5cf7966f69630831c
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2023-06-05 10:39:43 -0400

    Try typo

--- omnibus/config/software/datadog-agent-integrations-py2.rb
+++ omnibus/config/software/datadog-agent-integrations-py2.rb
@@ -163,9 +163,7 @@ build do
     nix_specific_build_env = {
       "aerospike" => nix_build_env.merge({"EXT_CFLAGS" => nix_build_env["CFLAGS"] + " -std=gnu99"}),
     }
-    win_specific_build_env = {
-      "aerospike" => nix_build_env.merge({"EXT_CFLAGS" => nix_build_env["CFLAGS"] + " -std=gnu99"}),
-    }
+    win_specific_build_env = {}
 
 
     # On Linux & Windows, specify the C99 standard explicitly to avoid issues while building some
