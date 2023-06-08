ca4f7108c51bf1af8d154f5c89a20b991263b0c3
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2023-06-08 18:52:08 +0200

    [CWS] get rid of invalidate_dentry

--- pkg/security/probe/perf_buffer_monitor.go
+++ pkg/security/probe/perf_buffer_monitor.go
@@ -83,8 +83,6 @@ type PerfBufferMonitor struct {
 
 	// lastTimestamp is used to track the timestamp of the last event retrieved from the perf map
 	lastTimestamp uint64
-	// shouldBumpGeneration is used to track if the dentry cache generations should be bumped
-	shouldBumpGeneration *atomic.Bool
 
 	// call that can be used to get notify when events are lost
 	onEventLost func(perfMapName string, perEvent map[string]uint64)
@@ -116,8 +114,6 @@ func NewPerfBufferMonitor(p *Probe, onEventLost func(perfMapName string, perEven
 		kernelStats:       make(map[string][][model.MaxKernelEventType]PerfMapStats),
 		readLostEvents:    make(map[string][]*atomic.Uint64),
 		sortingErrorStats: make(map[string][model.MaxKernelEventType]*atomic.Int64),
-
-		shouldBumpGeneration: atomic.NewBool(false),
 
 		onEventLost: onEventLost,
 	}
@@ -388,7 +384,6 @@ func (pbm *PerfBufferMonitor) CountEvent(eventType model.EventType, timestamp ui
 	// check event order
 	if timestamp < pbm.lastTimestamp && pbm.lastTimestamp != 0 {
 		pbm.sortingErrorStats[mapName][eventType].Inc()
-		pbm.shouldBumpGeneration.Store(true)
 	} else {
 		pbm.lastTimestamp = timestamp
 	}
@@ -534,11 +529,6 @@ func (pbm *PerfBufferMonitor) collectAndSendKernelStats(client statsd.ClientInte
 				}
 				if tmpCount = pbm.swapKernelLostCount(evtType, perfMapName, cpu, stats.Lost.Load()); tmpCount <= stats.Lost.Load() {
 					stats.Lost.Sub(tmpCount)
-
-					// purge dentry resolver generation if needed
-					if evtType == model.FileRenameEventType || evtType == model.FileUnlinkEventType || evtType == model.FileRmdirEventType {
-						pbm.shouldBumpGeneration.Store(true)
-					}
 				}
 
 				if err := pbm.sendKernelStats(client, stats, tags); err != nil {
@@ -568,7 +558,9 @@ func (pbm *PerfBufferMonitor) collectAndSendKernelStats(client statsd.ClientInte
 
 		// send an alert if events were lost
 		if total > 0 {
-			pbm.onEventLost(perfMapName, perEvent)
+			if pbm.onEventLost != nil {
+				pbm.onEventLost(perfMapName, perEvent)
+			}
 		}
 	}
 	return nil
@@ -600,10 +592,6 @@ func (pbm *PerfBufferMonitor) sendKernelStats(client statsd.ClientInterface, sta
 func (pbm *PerfBufferMonitor) SendStats() error {
 	if err := pbm.collectAndSendKernelStats(pbm.statsdClient); err != nil {
 		return err
-	}
-
-	if pbm.shouldBumpGeneration.Swap(false) {
-		pbm.probe.resolvers.DentryResolver.BumpCacheGenerations()
 	}
 
 	if err := pbm.sendEventsAndBytesReadStats(pbm.statsdClient); err != nil {
