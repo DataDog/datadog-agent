cd91b27c156eafb56d283b683c0b20fab348c0b4
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-06-09 14:20:24 -0400

    support windows as well

--- tasks/winbuildscripts/extract-modcache.bat
+++ tasks/winbuildscripts/extract-modcache.bat
@@ -1,10 +1,10 @@
-if exist c:\mnt\modcache.tar.gz (
+if exist c:\mnt\modcache.tar.zst (
     @echo Extracting modcache
-    Powershell -C "7z x c:\mnt\modcache.tar.gz -oc:\mnt"
+    Powershell -C "7z x c:\mnt\modcache.tar.zst -oc:\mnt"
     Powershell -C "7z x c:\mnt\modcache.tar -oc:\modcache"
-    del /f c:\mnt\modcache.tar.gz
+    del /f c:\mnt\modcache.tar.zst
     del /f c:\mnt\modcache.tar
     @echo Modcache extracted
 ) else (
-    @echo modcache.tar.gz not found, dependencies will be downloaded
+    @echo modcache.tar.zst not found, dependencies will be downloaded
 )
