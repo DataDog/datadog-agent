9196d7d1718107b7c414affdb30a13b5dfab7046
Rey Abolofia <rey.abolofia@datadoghq.com>
2023-06-09 15:17:08 -0700

    Linting.

--- test/integration/serverless/src/otlpPython.py
+++ test/integration/serverless/src/otlpPython.py
@@ -5,6 +5,7 @@ from opentelemetry import trace
 
 tracer = trace.get_tracer(__name__)
 
+
 def simple_test(event, context):
     with tracer.start_as_current_span('my-function'):
         time.sleep(0.1)
