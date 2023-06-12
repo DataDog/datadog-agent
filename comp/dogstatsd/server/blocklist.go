090c8f4559654d1c1cf60868e6bb71113da3770e
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2023-06-12 15:40:17 +0000

    Use binary search for metrics blocklist

--- comp/dogstatsd/server/blocklist.go
+++ comp/dogstatsd/server/blocklist.go
@@ -6,6 +6,7 @@
 package server
 
 import (
+	"sort"
 	"strings"
 )
 
@@ -15,6 +16,26 @@ type blocklist struct {
 }
 
 func newBlocklist(data []string, matchPrefix bool) blocklist {
+	sort.Strings(data)
+
+	if matchPrefix && len(data) > 0 {
+		// Make sure that elements identify unique prefixes.
+		i := 0
+		for j := 1; j < len(data); j++ {
+			if strings.HasPrefix(data[j], data[i]) {
+				continue
+			}
+			i++
+			data[i] = data[j]
+		}
+
+		data = data[:i+1]
+	}
+
+	// Invariants for data:
+	// For all i, j such that i < j, data[i] < data[j].
+	// for all i, j such that i != j, !HasPrefix(data[i], data[j]).
+
 	return blocklist{
 		data:        data,
 		matchPrefix: matchPrefix,
@@ -22,18 +43,30 @@ func newBlocklist(data []string, matchPrefix bool) blocklist {
 }
 
 func (b *blocklist) test(name string) bool {
-	if b.matchPrefix {
-		for _, item := range b.data {
-			if strings.HasPrefix(name, item) {
-				return true
-			}
-		}
-	} else {
-		for _, item := range b.data {
-			if name == item {
-				return true
-			}
-		}
+	if len(b.data) == 0 {
+		return false
+	}
+
+	i := sort.SearchStrings(b.data, name)
+
+	// SearchStrings returns an index such that either:
+	// - data[i] == name
+	// - data[i-1] < name (if i > 0) && data[i] > name (if i < len(b.data))
+	//
+	// If for some j, data[j] is a prefix of name, then:
+	//
+	// - j < i, because any prefix of a string is less than string itself,
+	//
+	// - if j < i - 1, then strings in range [j+1, i-1] would have
+	// data[j] as a prefix, which is impossible by construction of
+	// data.
+	//
+	// Thus j must be i - 1.
+	if b.matchPrefix && i > 0 && strings.HasPrefix(name, b.data[i-1]) {
+		return true
+	}
+	if i < len(b.data) {
+		return name == b.data[i]
 	}
 
 	return false
