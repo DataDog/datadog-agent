a99909c32e2a9e0178f80fe0938a2e8b0b1fefb4
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2023-06-13 11:47:13 +0200

    add more details in case of test failure

--- pkg/compliance/tests/process_test.go
+++ pkg/compliance/tests/process_test.go
@@ -9,6 +9,7 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"os"
@@ -204,17 +205,18 @@ valid(p) {
 }
 
 findings[f] {
-	count([p | p := input.process[_]; valid(p)]) == 2
+	c := count([p | p := input.process[_]; valid(p)])
 	f := dd.passed_finding(
 		"sleep",
 		"sleep",
-		{},
+		{ "c": c },
 	)
 }
 `, envFoo).
 		AssertPassedEvent(func(t *testing.T, evt *compliance.CheckEvent) {
 			assert.Equal(t, "sleep", evt.ResourceID)
 			assert.Equal(t, "sleep", evt.ResourceType)
+			assert.Equal(t, json.Number("2"), evt.Data["c"])
 		})
 }
 
