3ff9e750f60b3527d729ad90c4e8df103cc359b5
YoannGh <yoann.ghigoff@datadoghq.com>
2023-06-14 15:06:21 +0200

    fix attached probes

--- pkg/security/probe/probe_linux.go
+++ pkg/security/probe/probe_linux.go
@@ -78,7 +78,7 @@ var (
 	defaultEventTypes = []eval.EventType{
 		model.ForkEventType.String(),
 		model.ExecEventType.String(),
-		model.ExecEventType.String(),
+		model.ExitEventType.String(),
 	}
 )
 
@@ -289,6 +289,10 @@ func (p *Probe) Setup() error {
 
 	p.applyDefaultFilterPolicies()
 
+	if err := p.updateProbes(defaultEventTypes, true); err != nil {
+		return err
+	}
+
 	p.profileManagers.Start(p.ctx, &p.wg)
 
 	return nil
@@ -296,15 +300,7 @@ func (p *Probe) Setup() error {
 
 // Start processing events
 func (p *Probe) Start() error {
-	if err := p.eventStream.Start(&p.wg); err != nil {
-		return err
-	}
-
-	return p.updateProbes([]eval.EventType{
-		model.ForkEventType.String(),
-		model.ExecEventType.String(),
-		model.ExecEventType.String(),
-	})
+	return p.eventStream.Start(&p.wg)
 }
 
 func (p *Probe) PlaySnapshot() {
@@ -1053,7 +1049,7 @@ func (p *Probe) validEventTypeForConfig(eventType string) bool {
 
 // updateProbes applies the loaded set of rules and returns a report
 // of the applied approvers for it.
-func (p *Probe) updateProbes(ruleEventTypes []eval.EventType) error {
+func (p *Probe) updateProbes(ruleEventTypes []eval.EventType, useSnapshotProbes bool) error {
 	// event types enabled either by event handlers or by rules
 	eventTypes := append([]eval.EventType{}, defaultEventTypes...)
 	eventTypes = append(eventTypes, ruleEventTypes...)
@@ -1070,6 +1066,10 @@ func (p *Probe) updateProbes(ruleEventTypes []eval.EventType) error {
 	}
 
 	var activatedProbes []manager.ProbesSelector
+
+	if useSnapshotProbes {
+		activatedProbes = append(activatedProbes, probes.SnapshotSelectors...)
+	}
 
 	// extract probe to activate per the event types
 	for eventType, selectors := range probes.GetSelectorsPerEventType() {
@@ -1390,7 +1390,7 @@ func (p *Probe) ApplyRuleSet(rs *rules.RuleSet) (*kfilters.ApplyRuleSetReport, e
 		return nil, fmt.Errorf("failed to flush discarders: %w", err)
 	}
 
-	if err := p.updateProbes(rs.GetEventTypes()); err != nil {
+	if err := p.updateProbes(rs.GetEventTypes(), false); err != nil {
 		return nil, fmt.Errorf("failed to select probes: %w", err)
 	}
 
