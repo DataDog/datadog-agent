DataDog/datadog-agent

commit fd550c67d6265a7431d6bcacdd48a1f8870db24a

pkg/trace: Add config file support for apm features (#15904)
* Making apm.config_features configurable from datadog.yaml

* Fixed tests

* Removed superfluous print statements

* Removed superfluous comments

* Added release note

* Update releasenotes/notes/apm-feature-flags-as-config-06145e28554f3f37.yaml

Co-authored-by: Ursula Chen <58821586+urseberry@users.noreply.github.com>

* gofmt'd files that were failing linter tests

* Ran gofmt on files that were failing linting tests

* Ran gofmt again on files failing linter tests

* Update pkg/config/config_template.yaml

Co-authored-by: Katie Hockman <katie@hockman.dev>

* changes requested by PR review

---------

Co-authored-by: Ursula Chen <58821586+urseberry@users.noreply.github.com>
Co-authored-by: Katie Hockman <katie@hockman.dev>

cmd/trace-agent/config/config.go
@@ -29,7 +29,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/tagger"
 	"github.com/DataDog/datadog-agent/pkg/tagger/collectors"
 	"github.com/DataDog/datadog-agent/pkg/trace/config"
-	"github.com/DataDog/datadog-agent/pkg/trace/config/features"
 	"github.com/DataDog/datadog-agent/pkg/trace/traceutil"
 	"github.com/DataDog/datadog-agent/pkg/util/fargate"
 	"github.com/DataDog/datadog-agent/pkg/util/grpc"
@@ -221,6 +220,16 @@ func applyDatadogConfig(c *config.AgentConfig) error {
 
 	if coreconfig.Datadog.IsSet("apm_config.max_remote_traces_per_second") {
 		c.MaxRemoteTPS = coreconfig.Datadog.GetFloat64("apm_config.max_remote_traces_per_second")
+	}
+	if k := "apm_config.features"; coreconfig.Datadog.IsSet(k) {
+		feats := coreconfig.Datadog.GetStringSlice(k)
+		for _, f := range feats {
+			c.Features[f] = struct{}{}
+		}
+		if c.HasFeature("big_resource") {
+			c.MaxResourceLen = 15_000
+		}
+		log.Debug("Found APM feature flags: %v", c.Features)
 	}
 
 	if k := "apm_config.ignore_resources"; coreconfig.Datadog.IsSet(k) {
@@ -586,7 +595,7 @@ func acquireHostname(c *config.AgentConfig) error {
 	if err != nil {
 		return err
 	}
-	if features.Has("disable_empty_hostname") && reply.Hostname == "" {
+	if c.HasFeature("disable_empty_hostname") && reply.Hostname == "" {
 		log.Debugf("Acquired empty hostname from gRPC but it's disallowed.")
 		return errors.New("empty hostname disallowed")
 	}
@@ -605,7 +614,7 @@ func acquireHostnameFallback(c *config.AgentConfig) error {
 	cmd.Stdout = &out
 	err := cmd.Run()
 	c.Hostname = strings.TrimSpace(out.String())
-	if emptyDisallowed := features.Has("disable_empty_hostname") && c.Hostname == ""; err != nil || emptyDisallowed {
+	if emptyDisallowed := c.HasFeature("disable_empty_hostname") && c.Hostname == ""; err != nil || emptyDisallowed {
 		if emptyDisallowed {
 			log.Debugf("Core agent returned empty hostname but is disallowed by disable_empty_hostname feature flag. Falling back to os.Hostname.")
 		}

pkg/config/apm.go
@@ -52,6 +52,7 @@ func setupAPM(config Config) {
 	config.SetKnown("apm_config.bucket_size_seconds")
 	config.SetKnown("apm_config.watchdog_check_delay")
 	config.SetKnown("apm_config.sync_flushing")
+	config.SetKnown("apm_config.features")
 
 	bindVectorOptions(config, Traces)
 
@@ -106,6 +107,8 @@ func setupAPM(config Config) {
 	config.BindEnv("apm_config.obfuscation.credit_cards.enabled", "DD_APM_OBFUSCATION_CREDIT_CARDS_ENABLED")
 	config.BindEnv("apm_config.obfuscation.credit_cards.luhn", "DD_APM_OBFUSCATION_CREDIT_CARDS_LUHN")
 	config.BindEnvAndSetDefault("apm_config.debug.port", 5012, "DD_APM_DEBUG_PORT")
+	config.BindEnv("apm_config.features", "DD_APM_FEATURES")
+	config.SetEnvKeyTransformer("apm_config.features", parseKVList("apm_config.features"))
 
 	config.SetEnvKeyTransformer("apm_config.ignore_resources", func(in string) interface{} {
 		r, err := splitCSVString(in, ',')

cmd/trace-agent/config/config_test.go
@@ -24,7 +24,6 @@ import (
 
 	coreconfig "github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/trace/config"
-	"github.com/DataDog/datadog-agent/pkg/trace/config/features"
 )
 
 func TestMain(m *testing.M) {
@@ -326,10 +325,7 @@ func TestConfigHostname(t *testing.T) {
 		})
 
 		t.Run("empty+disallowed", func(t *testing.T) {
-			features.Set("disable_empty_hostname")
-			defer func() { features.Set(os.Getenv("DD_APM_FEATURES")) }()
-
-			cfg := config.AgentConfig{DDAgentBin: makeProgram("", 0)}
+			cfg := config.AgentConfig{DDAgentBin: makeProgram("", 0), Features: map[string]struct{}{"disable_empty_hostname": {}}}
 			defer os.Remove(cfg.DDAgentBin)
 			assert.NoError(t, acquireHostnameFallback(&cfg))
 			assert.Equal(t, "fallback.host", cfg.Hostname)

pkg/trace/config/config.go
@@ -18,7 +18,6 @@ import (
 
 	"github.com/DataDog/datadog-agent/pkg/obfuscate"
 	"github.com/DataDog/datadog-agent/pkg/remoteconfig/state"
-	"github.com/DataDog/datadog-agent/pkg/trace/config/features"
 	"github.com/DataDog/datadog-agent/pkg/trace/log"
 )
 
@@ -117,14 +116,14 @@ type ObfuscationConfig struct {
 }
 
 // Export returns an obfuscate.Config matching o.
-func (o *ObfuscationConfig) Export() obfuscate.Config {
+func (o *ObfuscationConfig) Export(conf *AgentConfig) obfuscate.Config {
 	return obfuscate.Config{
 		SQL: obfuscate.SQLConfig{
-			TableNames:       features.Has("table_names"),
-			ReplaceDigits:    features.Has("quantize_sql_tables") || features.Has("replace_sql_digits"),
-			KeepSQLAlias:     features.Has("keep_sql_alias"),
-			DollarQuotedFunc: features.Has("dollar_quoted_func"),
-			Cache:            features.Has("sql_cache"),
+			TableNames:       conf.HasFeature("table_names"),
+			ReplaceDigits:    conf.HasFeature("quantize_sql_tables") || conf.HasFeature("replace_sql_digits"),
+			KeepSQLAlias:     conf.HasFeature("keep_sql_alias"),
+			DollarQuotedFunc: conf.HasFeature("dollar_quoted_func"),
+			Cache:            conf.HasFeature("sql_cache"),
 		},
 		ES: obfuscate.JSONConfig{
 			Enabled:            o.ES.Enabled,
@@ -290,6 +289,8 @@ type DebuggerProxyConfig struct {
 // It is exposed with expvar, so make sure to exclude any sensible field
 // from JSON encoding. Use New() to create an instance.
 type AgentConfig struct {
+	Features map[string]struct{}
+
 	Enabled      bool
 	AgentVersion string
 	GitCommit    string
@@ -385,6 +386,9 @@ type AgentConfig struct {
 
 	// Obfuscation holds sensitive data obufscator's configuration.
 	Obfuscation *ObfuscationConfig
+
+	// MaxResourceLen the maximum length the resource can have
+	MaxResourceLen int
 
 	// RequireTags specifies a list of tags which must be present on the root span in order for a trace to be accepted.
 	RequireTags []*Tag
@@ -492,6 +496,7 @@ func New() *AgentConfig {
 		AnalyzedRateByServiceLegacy: make(map[string]float64),
 		AnalyzedSpansByService:      make(map[string]map[string]float64),
 		Obfuscation:                 &ObfuscationConfig{},
+		MaxResourceLen:              5000,
 
 		GlobalTags: make(map[string]string),
 
@@ -507,6 +512,8 @@ func New() *AgentConfig {
 		},
 
 		InAzureAppServices: inAzureAppServices(os.Getenv),
+
+		Features: make(map[string]struct{}),
 	}
 }
 
@@ -551,6 +558,19 @@ func (c *AgentConfig) NewHTTPTransport() *http.Transport {
 		ExpectContinueTimeout: 1 * time.Second,
 	}
 	return transport
+}
+
+func (c *AgentConfig) HasFeature(feat string) bool {
+	_, ok := c.Features[feat]
+	return ok
+}
+
+func (c *AgentConfig) AllFeatures() []string {
+	feats := []string{}
+	for feat := range c.Features {
+		feats = append(feats, feat)
+	}
+	return feats
 }
 
 func inAzureAppServices(getenv func(string) string) bool {

cmd/trace-agent/remote_config_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/proto/pbgo"
 	"github.com/DataDog/datadog-agent/pkg/trace/api"
 	"github.com/DataDog/datadog-agent/pkg/trace/config"
-	"github.com/DataDog/datadog-agent/pkg/trace/config/features"
 	"github.com/DataDog/datadog-agent/pkg/trace/sampler"
 	"github.com/DataDog/datadog-agent/pkg/trace/telemetry"
 
@@ -26,8 +25,6 @@ import (
 )
 
 func TestConfigEndpoint(t *testing.T) {
-	defer func(old string) { features.Set(old) }(strings.Join(features.All(), ","))
-
 	var tcs = []struct {
 		name               string
 		reqBody            string

pkg/config/config_template.yaml
@@ -1222,6 +1222,14 @@ api_key:
   #
   # connection_limit: 2000
 
+  ## @param features - list of strings - optional
+  ## @env DD_APM_FEATURES - comma separated list of strings - optional
+  ## Configure additional beta APM features.
+  ## The list of items available under apm_config.features is not guaranteed to persist across versions; 
+  ## a feature may eventually be promoted to its own configuration option on the agent, or dropped entirely.
+  #
+  # features: ["error_rare_sample_tracer_drop","table_names","component2name","sql_cache"]
+
   {{- if .InternalProfiling -}}
   ## @param profiling - custom object - optional
   ## Enter specific configurations for internal profiling.

pkg/trace/agent/agent.go
@@ -16,7 +16,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/obfuscate"
 	"github.com/DataDog/datadog-agent/pkg/trace/api"
 	"github.com/DataDog/datadog-agent/pkg/trace/config"
-	"github.com/DataDog/datadog-agent/pkg/trace/config/features"
 	"github.com/DataDog/datadog-agent/pkg/trace/event"
 	"github.com/DataDog/datadog-agent/pkg/trace/filters"
 	"github.com/DataDog/datadog-agent/pkg/trace/info"
@@ -89,8 +88,7 @@ func NewAgent(ctx context.Context, conf *config.AgentConfig, telemetryCollector 
 	dynConf := sampler.NewDynamicConfig()
 	in := make(chan *api.Payload, 1000)
 	statsChan := make(chan pb.StatsPayload, 100)
-
-	oconf := conf.Obfuscation.Export()
+	oconf := conf.Obfuscation.Export(conf)
 	if oconf.Statsd == nil {
 		oconf.Statsd = metrics.Client
 	}
@@ -254,7 +252,7 @@ func (a *Agent) Process(p *api.Payload) {
 
 		tracen := int64(len(chunk.Spans))
 		ts.SpansReceived.Add(tracen)
-		err := normalizeTrace(p.Source, chunk.Spans)
+		err := a.normalizeTrace(p.Source, chunk.Spans)
 		if err != nil {
 			log.Debugf("Dropping invalid trace: %s", err)
 			ts.SpansDropped.Add(tracen)
@@ -294,7 +292,7 @@ func (a *Agent) Process(p *api.Payload) {
 				a.ModifySpan(chunk, span)
 			}
 			a.obfuscateSpan(span)
-			Truncate(span)
+			a.Truncate(span)
 			if p.ClientComputedTopLevel {
 				traceutil.UpdateTracerTopLevel(span)
 			}
@@ -409,8 +407,8 @@ func (a *Agent) discardSpans(p *api.Payload) {
 }
 
 func (a *Agent) processStats(in pb.ClientStatsPayload, lang, tracerVersion string) pb.ClientStatsPayload {
-	enableContainers := features.Has("enable_cid_stats") || (a.conf.FargateOrchestrator != config.OrchestratorUnknown)
-	if !enableContainers || features.Has("disable_cid_stats") {
+	enableContainers := a.conf.HasFeature("enable_cid_stats") || (a.conf.FargateOrchestrator != config.OrchestratorUnknown)
+	if !enableContainers || a.conf.HasFeature("disable_cid_stats") {
 		// only allow the ContainerID stats dimension if we're in a Fargate instance or it's
 		// been explicitly enabled and it's not prohibited by the disable_cid_stats feature flag.
 		in.ContainerID = ""
@@ -429,7 +427,7 @@ func (a *Agent) processStats(in pb.ClientStatsPayload, lang, tracerVersion strin
 	for i, group := range in.Stats {
 		n := 0
 		for _, b := range group.Stats {
-			normalizeStatsGroup(&b, lang)
+			a.normalizeStatsGroup(&b, lang)
 			if !a.Blacklister.AllowsStat(&b) {
 				continue
 			}
@@ -486,8 +484,7 @@ func (a *Agent) sample(now time.Time, ts *info.TagStats, pt traceutil.ProcessedT
 	} else {
 		ts.TracesPriorityNone.Inc()
 	}
-
-	if features.Has("error_rare_sample_tracer_drop") {
+	if a.conf.HasFeature("error_rare_sample_tracer_drop") {
 		if isManualUserDrop(priority, pt) {
 			return 0, false, nil
 		}

pkg/trace/agent/agent_test.go
@@ -25,7 +25,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/obfuscate"
 	"github.com/DataDog/datadog-agent/pkg/trace/api"
 	"github.com/DataDog/datadog-agent/pkg/trace/config"
-	"github.com/DataDog/datadog-agent/pkg/trace/config/features"
 	"github.com/DataDog/datadog-agent/pkg/trace/event"
 	"github.com/DataDog/datadog-agent/pkg/trace/filters"
 	"github.com/DataDog/datadog-agent/pkg/trace/info"
@@ -595,8 +594,8 @@ func TestConcentratorInput(t *testing.T) {
 
 	for _, tc := range tts {
 		t.Run(tc.name, func(t *testing.T) {
-			defer testutil.WithFeatures(tc.features)()
 			cfg := config.New()
+			cfg.Features[tc.features] = struct{}{}
 			cfg.Endpoints[0].APIKey = "test"
 			if tc.withFargate {
 				cfg.FargateOrchestrator = config.OrchestratorECS
@@ -948,7 +947,7 @@ func TestSampling(t *testing.T) {
 }
 
 func TestSample(t *testing.T) {
-	cfg := &config.AgentConfig{TargetTPS: 5, ErrorTPS: 10}
+	cfg := &config.AgentConfig{TargetTPS: 5, ErrorTPS: 10, Features: make(map[string]struct{})}
 	a := &Agent{
 		NoPrioritySampler: sampler.NewNoPrioritySampler(cfg),
 		ErrorsSampler:     sampler.NewErrorsSampler(cfg),
@@ -1008,9 +1007,8 @@ func TestSample(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			_, keep, _ := a.sample(time.Now(), info.NewReceiverStats().GetTagStats(info.Tags{}), tt.trace)
 			assert.Equal(t, tt.sampledNoFeature, keep)
-
-			features.Set("error_rare_sample_tracer_drop")
-			defer features.Set("")
+			cfg.Features["error_rare_sample_tracer_drop"] = struct{}{}
+			defer delete(cfg.Features, "error_rare_sample_tracer_drop")
 			_, keep, _ = a.sample(time.Now(), info.NewReceiverStats().GetTagStats(info.Tags{}), tt.trace)
 			assert.Equal(t, tt.sampledWithFeature, keep)
 		})
@@ -1299,8 +1297,9 @@ func runTraceProcessingBenchmark(b *testing.B, c *config.AgentConfig) {
 // Mimics behaviour of agent Process function
 func formatTrace(t pb.Trace) pb.Trace {
 	for _, span := range t {
-		(&Agent{obfuscator: obfuscate.NewObfuscator(obfuscate.Config{})}).obfuscateSpan(span)
-		Truncate(span)
+		a := &Agent{obfuscator: obfuscate.NewObfuscator(obfuscate.Config{}), conf: config.New()}
+		a.obfuscateSpan(span)
+		a.Truncate(span)
 	}
 	return t
 }
@@ -1530,7 +1529,7 @@ func TestConvertStats(t *testing.T) {
 		Blacklister: filters.NewBlacklister([]string{"blocked_resource"}),
 		obfuscator:  obfuscate.NewObfuscator(obfuscate.Config{}),
 		Replacer:    filters.NewReplacer([]*config.ReplaceRule{{Name: "http.status_code", Pattern: "400", Re: regexp.MustCompile("400"), Repl: "200"}}),
-		conf:        &config.AgentConfig{DefaultEnv: "agent_env", Hostname: "agent_hostname"},
+		conf:        &config.AgentConfig{DefaultEnv: "agent_env", Hostname: "agent_hostname", MaxResourceLen: 5000},
 	}
 	for _, testCase := range testCases {
 		out := a.processStats(testCase.in, testCase.lang, testCase.tracerVersion)

commit a931b697193d7c840020620abc59d04de36ebc9b

config/apm: fix parsing DD_APM_FEATURES (#17630)
Support either "," or " " as separator when parsing the value of DD_APM_FEATURES. It fixes a regression introduced in #15904 which changed the separator from comma to space. This was a breaking change. From 7.44 to 7.46 using a space as separator was suggested ad a workaround, this PR ensures we don't break compatibility again. We now support either space or comma.

cmd/trace-agent/config/config.go
@@ -235,7 +235,7 @@ func applyDatadogConfig(c *config.AgentConfig) error {
 		if c.HasFeature("big_resource") {
 			c.MaxResourceLen = 15_000
 		}
-		log.Debug("Found APM feature flags: %v", c.Features)
+		log.Infof("Found APM feature flags: %s", feats)
 	}
 
 	if k := "apm_config.ignore_resources"; coreconfig.Datadog.IsSet(k) {

pkg/config/apm.go
@@ -114,7 +114,21 @@ func setupAPM(config Config) {
 	config.BindEnv("apm_config.obfuscation.credit_cards.luhn", "DD_APM_OBFUSCATION_CREDIT_CARDS_LUHN")
 	config.BindEnvAndSetDefault("apm_config.debug.port", 5012, "DD_APM_DEBUG_PORT")
 	config.BindEnv("apm_config.features", "DD_APM_FEATURES")
-	config.SetEnvKeyTransformer("apm_config.features", parseKVList("apm_config.features"))
+	config.SetEnvKeyTransformer("apm_config.features", func(s string) interface{} {
+		// Either commas or spaces can be used as separators.
+		// Comma takes precedence as it was the only supported separator in the past.
+		// Mixing separators is not supported.
+		var res []string
+		if strings.ContainsRune(s, ',') {
+			res = strings.Split(s, ",")
+		} else {
+			res = strings.Split(s, " ")
+		}
+		for i, v := range res {
+			res[i] = strings.TrimSpace(v)
+		}
+		return res
+	})
 
 	config.SetEnvKeyTransformer("apm_config.ignore_resources", func(in string) interface{} {
 		r, err := splitCSVString(in, ',')

releasenotes/notes/apm-features-fix-c1c0adac4f3f4edf.yaml
@@ -1,0 +1,14 @@
+# Each section from every release note are combined when the
+# CHANGELOG.rst is rendered. So the text needs to be worded so that
+# it does not depend on any information only available in another
+# section. This may mean repeating some details, but each section
+# must be readable independently of the other.
+#
+# Each section note must be formatted as reStructuredText.
+---
+fixes:
+  - |
+    APM: Fix a bug introduced in Agent versions 7.44 and 6.44 that
+    changed the expected strings separator from comma to space when
+    multiple features are defined in DD_APM_FEATURES.
+    Now either separator can be used (for example, DD_APM_FEATURES="feat1,feat2" or DD_APM_FEATURES="feat1 feat2").

cmd/trace-agent/config/config_test.go
@@ -1075,6 +1075,29 @@ func TestLoadEnv(t *testing.T) {
 			t.Fatalf("Failed to process env var %s, expected %v and got %v", env, expected, actual)
 		}
 	})
+
+	env = "DD_APM_FEATURES"
+	t.Run(env, func(t *testing.T) {
+		assert := func(in string, expected []string) {
+			defer cleanConfig()()
+			t.Setenv(env, in)
+			_, err := LoadConfigFile("./testdata/full.yaml")
+			assert.NoError(t, err)
+			assert.Equal(t, expected, coreconfig.Datadog.GetStringSlice("apm_config.features"))
+		}
+		cases := map[string][]string{
+			"":                      nil,
+			"feat1":                 {"feat1"},
+			"feat1,feat2,feat3":     {"feat1", "feat2", "feat3"},
+			"feat1 feat2 feat3":     {"feat1", "feat2", "feat3"},
+			"feat1,feat2 feat3":     {"feat1", "feat2 feat3"},    // mixing separators is not supported, comma wins
+			"feat1, feat2, feat3":   {"feat1", "feat2", "feat3"}, // trim whitespaces
+			"feat1 , feat2 , feat3": {"feat1", "feat2", "feat3"}, // trim whitespaces
+		}
+		for in, expected := range cases {
+			assert(in, expected)
+		}
+	})
 }
 
 func TestFargateConfig(t *testing.T) {