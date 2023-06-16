3ecf4e8d8973e2bef317d8cf9930c04a9f41e72a
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2023-06-16 18:13:46 +0200

    Save modcache in container and not shared folder

--- tasks/winbuildscripts/extract-modcache.bat
+++ tasks/winbuildscripts/extract-modcache.bat
@@ -1,10 +1,10 @@
-if exist c:\mnt\modcache.tar.gz (
+if exist c:\mnt\modcache_%CI_PIPELINE_ID%.tar.gz (
     @echo Extracting modcache
-    Powershell -C "7z x c:\mnt\modcache.tar.gz -oc:\mnt"
-    Powershell -C "7z x c:\mnt\modcache.tar -oc:\modcache"
-    del /f c:\mnt\modcache.tar.gz
-    del /f c:\mnt\modcache.tar
+    Powershell -C "7z x c:\mnt\modcache_%CI_PIPELINE_ID%.tar.gz -oc:\mnt"
+    Powershell -C "7z x c:\mnt\modcache_%CI_PIPELINE_ID%.tar -oc:\modcache"
+    del /f c:\mnt\modcache_%CI_PIPELINE_ID%.tar.gz
+    del /f c:\mnt\modcache_%CI_PIPELINE_ID%.tar
     @echo Modcache extracted
 ) else (
-    @echo modcache.tar.gz not found, dependencies will be downloaded
+    @echo modcache_%CI_PIPELINE_ID%.tar.gz not found, dependencies will be downloaded
 )
--- tasks/winbuildscripts/extract-tools-modcache.bat
+++ tasks/winbuildscripts/extract-tools-modcache.bat
@@ -1,14 +1,14 @@
-if exist c:\mnt\modcache_tools.tar.gz (
+if exist c:\mnt\modcache_tools_%CI_PIPELINE_ID%.tar.gz (
     @echo Extracting modcache_tools
-    Powershell -C "7z x c:\mnt\modcache_tools.tar.gz -oc:\mnt"
+    Powershell -C "7z x c:\mnt\modcache_tools_%CI_PIPELINE_ID%.tar.gz -oc:\mnt"
     REM Use -aoa to allow overwriting existing files
     REM This shouldn't have any negative impact: since modules are
     REM stored per version and hash, files that get replaced will
     REM get replaced by the same files
-    Powershell -C "7z x c:\mnt\modcache_tools.tar -oc:\modcache -aoa"
-    del /f c:\mnt\modcache_tools.tar.gz
-    del /f c:\mnt\modcache_tools.tar
+    Powershell -C "7z x c:\mnt\modcache_tools_%CI_PIPELINE_ID%.tar -oc:\modcache -aoa"
+    del /f c:\mnt\modcache_tools_%CI_PIPELINE_ID%.tar.gz
+    del /f c:\mnt\modcache_tools_%CI_PIPELINE_ID%.tar
     @echo modcache_tools extracted
 ) else (
-    @echo modcache_tools.tar.gz not found, tooling dependencies will be downloaded
+    @echo modcache_tools_%CI_PIPELINE_ID%.tar.gz not found, tooling dependencies will be downloaded
 )
