58a224d15da3f9f9bb975a304c6a87072e386142
Usama Saqib <usamasaqib.96@live.com>
2023-06-19 15:32:48 +0200

    Destroy stack without needing to deploy first (#17685)
    * Destroy stack without needing to deploy first
    
    * use shorthand for initialization

--- test/new-e2e/utils/infra/stack_manager.go
+++ test/new-e2e/utils/infra/stack_manager.go
@@ -122,7 +122,25 @@ func (sm *StackManager) DeleteStack(ctx context.Context, name string) error {
 	sm.lock.Lock()
 	defer sm.lock.Unlock()
 
-	return sm.deleteStack(ctx, name, sm.stacks[name])
+	stack, ok := sm.stacks[name]
+	if !ok {
+		// Build configuration from profile
+		profile := runner.GetProfile()
+		stackName := buildStackName(profile.NamePrefix(), name)
+		workspace, err := buildWorkspace(ctx, profile, stackName, func(ctx *pulumi.Context) error { return nil })
+		if err != nil {
+			return err
+		}
+
+		newStack, err := auto.SelectStack(ctx, stackName, workspace)
+		if err != nil {
+			return err
+		}
+
+		stack = &newStack
+	}
+
+	return sm.deleteStack(ctx, name, stack)
 }
 
 func (sm *StackManager) Cleanup(ctx context.Context) []error {
