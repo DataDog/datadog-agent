49902f1cc8dc64063b57438e08f8786cf5f39844
usamasaqib <usamasaqib.96@live.com>
2023-06-20 12:08:08 +0200

    upload dependencies as they become available

--- test/new-e2e/system-probe/test/micro-vm-init.sh
+++ test/new-e2e/system-probe/test/micro-vm-init.sh
@@ -13,6 +13,14 @@ systemctl start docker
 ## Load docker images
 find $KITCHEN_DOCKERS -maxdepth 1 -type f -exec docker load -i {} \;
 
+## setup system-probe-tests
+cp /opt/kernel-version-testing/tests-$ARCH.tar.gz /
+tar xzvf /tests-$ARCH.tar.gz --strip-components=1
+## setup minimized btfs
+cp /opt/kernel-version-testing/btfs-$ARCH.tar.gz /
+tar xzvf /btfs-$ARCH.tar.gz --strip-components=1
+
+
 # VM provisioning end !
 
 # Start tests
