7862e64ea46ff25137854d1b9892f74c3384e2e6
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2023-06-22 09:37:31 +0200

    Move modcache before extraction in windows runner to prevent conflict access to the shared repository

--- tasks/winbuildscripts/extract-modcache.bat
+++ tasks/winbuildscripts/extract-modcache.bat
@@ -1,9 +1,18 @@
 if exist c:\mnt\modcache.tar.gz (
-    @echo Extracting modcache
-    Powershell -C "7z x c:\mnt\modcache.tar.gz -oc:\mnt"
-    Powershell -C "7z x c:\mnt\modcache.tar -oc:\modcache"
-    del /f c:\mnt\modcache.tar.gz
-    del /f c:\mnt\modcache.tar
+    @echo Extracting modcache in dedicated repository
+    if not exist c:\cache (
+        mkdir "c:\cache"
+    )
+    move "c:\mnt\modcache.tar.gz" "c:\cache\modcache.tar.gz"
+    if errorlevel 1 (
+        @echo Failed to move modcache
+    ) else (
+        @echo Successfully moved modcache
+    )
+    Powershell -C "7z x c:\cache\modcache.tar.gz -oc:\cache"
+    Powershell -C "7z x c:\cache\modcache.tar -oc:\modcache"
+    del /f c:\cache\modcache.tar.gz
+    del /f c:\cache\modcache.tar
     @echo Modcache extracted
 ) else (
     @echo modcache.tar.gz not found, dependencies will be downloaded
--- tasks/winbuildscripts/extract-tools-modcache.bat
+++ tasks/winbuildscripts/extract-tools-modcache.bat
@@ -1,13 +1,22 @@
 if exist c:\mnt\modcache_tools.tar.gz (
-    @echo Extracting modcache_tools
-    Powershell -C "7z x c:\mnt\modcache_tools.tar.gz -oc:\mnt"
+    @echo Extracting modcache_tools in a dedicated repository
+    if not exist c:\cache (
+        mkdir "c:\cache"
+    )
+    move "c:\mnt\modcache.tar.gz" "c:\cache\modcache.tar.gz"
+    if errorlevel 1 (
+        @echo Failed to move modcache
+    ) else (
+        @echo Successfully moved modcache
+    )
+    Powershell -C "7z x c:\cache\modcache_tools.tar.gz -oc:\cache"
     REM Use -aoa to allow overwriting existing files
     REM This shouldn't have any negative impact: since modules are
     REM stored per version and hash, files that get replaced will
     REM get replaced by the same files
-    Powershell -C "7z x c:\mnt\modcache_tools.tar -oc:\modcache -aoa"
-    del /f c:\mnt\modcache_tools.tar.gz
-    del /f c:\mnt\modcache_tools.tar
+    Powershell -C "7z x c:\cache\modcache_tools.tar -oc:\modcache -aoa"
+    del /f c:\cache\modcache_tools.tar.gz
+    del /f c:\cache\modcache_tools.tar
     @echo modcache_tools extracted
 ) else (
     @echo modcache_tools.tar.gz not found, tooling dependencies will be downloaded
