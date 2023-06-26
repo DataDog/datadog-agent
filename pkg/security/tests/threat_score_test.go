30836d5b49122eb3ff3405f034e011084459119c
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-06-26 11:14:00 +0200

    remove useless `defer os.RemoveAll`

--- pkg/security/tests/threat_score_test.go
+++ pkg/security/tests/threat_score_test.go
@@ -59,7 +59,7 @@ func TestActivityDumpsThreatScore(t *testing.T) {
 	}
 
 	outputDir := t.TempDir()
-	defer os.RemoveAll(outputDir)
+
 	expectedFormats := []string{"json", "protobuf"}
 	testActivityDumpTracedEventTypes := []string{"exec", "open", "syscalls", "dns", "bind"}
 	test, err := newTestModule(t, nil, rules, testOpts{
