da444f1220f741c347bbe7c6cacc2fb974d9d485
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-06-26 20:52:47 +0200

    [USM] fix http_allow_packet() (#17800)

--- pkg/network/ebpf/c/protocols/http/http.h
+++ pkg/network/ebpf/c/protocols/http/http.h
@@ -166,7 +166,7 @@ static __always_inline bool http_allow_packet(http_transaction_t *http, struct _
         return false;
     }
     bool empty_payload = skb_info->data_off == skb->len;
-    if (empty_payload || is_fully_classified(stack) || is_protocol_layer_known(stack, LAYER_ENCRYPTION)) {
+    if (empty_payload || is_protocol_layer_known(stack, LAYER_ENCRYPTION)) {
         // if the payload data is empty or encrypted packet, we only
         // process it if the packet represents a TCP termination
         return skb_info->tcp_flags&(TCPHDR_FIN|TCPHDR_RST);
