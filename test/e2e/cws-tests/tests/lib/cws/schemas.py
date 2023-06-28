cf93ef83f2a88efb50f89a77f755994960e8db44
Momar TOURÉ <momar.toure@datadoghq.com>
2023-06-28 17:55:25 +0200

    Fix datetime validation

--- test/e2e/cws-tests/tests/lib/cws/schemas.py
+++ test/e2e/cws-tests/tests/lib/cws/schemas.py
@@ -1,6 +1,7 @@
 import json
 import os
-from jsonschema import RefResolver, Draft7Validator
+from jsonschema import RefResolver, Draft7Validator, validators
+from datetime import datetime
 
 class JsonSchemaValidator:
     def __init__(self):
@@ -10,15 +11,20 @@ class JsonSchemaValidator:
             if filename.endswith('.json'):
                 with open(os.path.join(self.schema_directory, filename)) as file:
                     schema = json.load(file)
-                    print(schema['$id'])
                     # Add each schema to the store using its 'id' as key
                     self.schema_store[schema['$id']] = schema
 
         # Create a resolver that uses the schema store for resolving references
         self.resolver = RefResolver(base_uri='', referrer=None, store=self.schema_store)
 
+    def is_datetime(self,checker, inst):
+        return isinstance(inst, datetime)
+
     def validate_json_data(self, schema_filename, json_data):
-        validator = Draft7Validator(self.schema_store[schema_filename], resolver=self.resolver)
+        date_check = Draft7Validator.TYPE_CHECKER.redefine('datetime', self.is_datetime)
+        CustomValidator = validators.extend(Draft7Validator, type_checker=date_check)
+        validator = CustomValidator(self.schema_store[schema_filename], resolver=self.resolver)
+        
         # Validate the instance
         validator.validate(json_data)
 
