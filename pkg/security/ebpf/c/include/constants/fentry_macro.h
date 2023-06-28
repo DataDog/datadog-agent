59e4f4dfabb4842d24ce563a313f15e213efbab2
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-06-28 10:29:52 +0200

    add more ctx macros

--- pkg/security/ebpf/c/include/constants/fentry_macro.h
+++ pkg/security/ebpf/c/include/constants/fentry_macro.h
@@ -5,13 +5,15 @@
 
 #define HOOK_ENTRY(func_name) SEC("fentry/" func_name)
 typedef unsigned long long ctx_t;
-#define CTX_PARM1(ctx) (void *)(ctx[0])
+#define CTX_PARM1(ctx) (u64)(ctx[0])
+#define CTX_PARM2(ctx) (u64)(ctx[1])
 
 #else
 
 #define HOOK_ENTRY(func_name) SEC("kprobe/" func_name)
 typedef struct pt_regs ctx_t;
 #define CTX_PARM1(ctx) PT_REGS_PARM1(ctx)
+#define CTX_PARM2(ctx) PT_REGS_PARM2(ctx)
 
 #endif
 
