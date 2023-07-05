b1e676431474e9d00bf018a57fbcfcff5414c20f
Ivan Topolcic <ivan.topolcic@datadoghq.com>
2023-07-05 10:51:05 -0400

    Change InitFn to void return type

--- cmd/serverless-init/initcontainer/instrument.go
+++ cmd/serverless-init/initcontainer/instrument.go
@@ -12,39 +12,31 @@ import (
 // tracer
 type Tracer struct {
 	FsPath string
-	InitFn func() error
+	InitFn func()
 }
 
-func instrumentNode() error {
+func instrumentNode() {
 	currNodePath := os.Getenv("NODE_PATH")
 	os.Setenv("NODE_PATH", addToString(currNodePath, ":", "/dd_tracer/node/"))
 
 	currNodeOptions := os.Getenv("NODE_OPTIONS")
 	os.Setenv("NODE_OPTIONS", addToString(currNodeOptions, " ", "--require dd-trace/init"))
-
-	return nil
 }
 
-func instrumentJava() error {
+func instrumentJava() {
 	currJavaToolOptions := os.Getenv("JAVA_TOOL_OPTIONS")
 	os.Setenv("JAVA_TOOL_OPTIONS", addToString(currJavaToolOptions, " ", "-javaagent:/dd_tracer/java/dd-java-agent.jar"))
-
-	return nil
 }
 
-func instrumentDotnet() error {
+func instrumentDotnet() {
 	os.Setenv("CORECLR_ENABLE_PROFILING", "1")
 	os.Setenv("CORECLR_PROFILER", "{846F5F1C-F9AE-4B07-969E-05C26BC060D8}")
 	os.Setenv("CORECLR_PROFILER_PATH", "/dd_tracer/dotnet/Datadog.Trace.ClrProfiler.Native.so")
 	os.Setenv("DD_DOTNET_TRACER_HOME", "/dd_tracer/dotnet/")
-
-	return nil
 }
 
-func instrumentPython() error {
+func instrumentPython() {
 	os.Setenv("PYTHONPATH", addToString(os.Getenv("PYTHONPATH"), ":", "/dd_tracer/python/"))
-
-	return nil
 }
 
 // AutoInstrumentTracer searches the filesystem for a trace library, and
