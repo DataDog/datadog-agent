576b27223045108388849cbb7a719206f3f9fd10
usamasaqib <usamasaqib.96@live.com>
2023-07-06 15:37:40 +0200

    upload dependencies as they become ready

--- test/new-e2e/system-probe/test/micro-vm-init.sh
+++ test/new-e2e/system-probe/test/micro-vm-init.sh
@@ -4,6 +4,7 @@ set -eo xtrace
 
 GOVERSION=$1
 RETRY_COUNT=$2
+ARCH=$3
 KITCHEN_DOCKERS=/kitchen-docker
 
 # Add provisioning steps here !
@@ -13,6 +14,15 @@ eval $(gimme "$GOVERSION")
 systemctl start docker
 ## Load docker images
 find $KITCHEN_DOCKERS -maxdepth 1 -type f -exec docker load -i {} \;
+
+# TEMP: bring remanining dependencies
+BTFS=btfs-$ARCH.tar.gz
+TESTS=tests-$ARCH.tar.gz
+cd /
+cp /opt/kernel-version-testing/$BTFS /
+tar xzvf $BTFS --strip-components=1
+cp /opt/kernel-version-testing/$TESTS /
+tar xzvf $TESTS --strip-components=1
 
 # VM provisioning end !
 
