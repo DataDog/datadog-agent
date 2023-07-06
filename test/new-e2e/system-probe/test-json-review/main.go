9c4641a63576dac53b9564e9253f834c7d96e52a
usamasaqib <usamasaqib.96@live.com>
2023-07-06 18:06:11 +0200

    close json file

--- test/new-e2e/system-probe/test-json-review/main.go
+++ test/new-e2e/system-probe/test-json-review/main.go
@@ -62,6 +62,7 @@ func reviewTests(jsonFile string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("open %s: %s", jsonFile, err)
 	}
+	defer jf.Close()
 
 	scanner := bufio.NewScanner(jf)
 	testResults := make(map[string]testEvent)
