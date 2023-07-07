c545aa6bbc08bd85c235020799662debcba3fecc
Celia Yuen <celia.yuen@datadoghq.com>
2023-07-07 15:00:06 -0400

    Rename AnomalyDetectionRateLimiterDuration to ...Period

--- pkg/security/events/rate_limiter.go
+++ pkg/security/events/rate_limiter.go
@@ -135,10 +135,10 @@ func (rl *RateLimiter) applyBaseLimitersFromDefault(limiters map[string]Limiter)
 		limiters[id] = limiter
 	}
 
-	limiter, err := NewAnomalyDetectionLimiter(rl.config.AnomalyDetectionRateLimiterNumKeys, rl.config.AnomalyDetectionRateLimiterNumEventsAllowed, rl.config.AnomalyDetectionRateLimiterDuration)
+	limiter, err := NewAnomalyDetectionLimiter(rl.config.AnomalyDetectionRateLimiterNumKeys, rl.config.AnomalyDetectionRateLimiterNumEventsAllowed, rl.config.AnomalyDetectionRateLimiterPeriod)
 	if err != nil {
 		// should never happen, fallback to std limiter
-		limiters[AnomalyDetectionRuleID] = NewStdLimiter(rate.Every(rl.config.AnomalyDetectionRateLimiterDuration), rl.config.AnomalyDetectionRateLimiterNumEventsAllowed)
+		limiters[AnomalyDetectionRuleID] = NewStdLimiter(rate.Every(rl.config.AnomalyDetectionRateLimiterPeriod), rl.config.AnomalyDetectionRateLimiterNumEventsAllowed)
 	} else {
 		limiters[AnomalyDetectionRuleID] = limiter
 	}
