3806726b257993ee1a148712b0b6d1aaed317966
Baptiste Foy <baptiste.foy@datadoghq.com>
2023-07-07 10:16:56 +0200

    upgrade(remote-config): Disable Remote Config for FED customers (#17967)
    * upgrade(remote-config): Disable Remote Config for FED customers
    
    * fix(config): Make IsRemoteConfigEnabled compatible with the config component

--- cmd/cluster-agent/subcommands/start/command.go
+++ cmd/cluster-agent/subcommands/start/command.go
@@ -150,7 +150,7 @@ func start(log log.Component, config config.Component, forwarder defaultforwarde
 
 	// Initialize remote configuration
 	var rcClient *remote.Client
-	if pkgconfig.Datadog.GetBool("remote_configuration.enabled") {
+	if pkgconfig.IsRemoteConfigEnabled(pkgconfig.Datadog) {
 		var err error
 		rcClient, err = initializeRemoteConfig(mainCtx)
 		if err != nil {
