2a80819893916d8aa80caef4efd134fe1d2ad40d
Steven Blumenthal <steven.blumenthal@datadoghq.com>
2023-07-11 11:11:05 -0400

    Remove unused factors variable from pod/provider.go

--- pkg/collector/corechecks/containers/kubelet/provider/pod/provider.go
+++ pkg/collector/corechecks/containers/kubelet/provider/pod/provider.go
@@ -28,26 +28,6 @@ import (
 )
 
 var (
-	// Suffixes per:
-	// https://github.com/kubernetes/kubernetes/blob/8fd414537b5143ab039cb910590237cabf4af783/pkg/api/resource/suffix.go#L108
-	factors = map[string]float64{
-		"n":  float64(1) / (1000 * 1000 * 1000),
-		"u":  float64(1) / (1000 * 1000),
-		"m":  float64(1) / 1000,
-		"k":  1000,
-		"M":  1000 * 1000,
-		"G":  1000 * 1000 * 1000,
-		"T":  1000 * 1000 * 1000 * 1000,
-		"P":  1000 * 1000 * 1000 * 1000 * 1000,
-		"E":  1000 * 1000 * 1000 * 1000 * 1000 * 1000,
-		"Ki": 1024,
-		"Mi": 1024 * 1024,
-		"Gi": 1024 * 1024 * 1024,
-		"Ti": 1024 * 1024 * 1024 * 1024,
-		"Pi": 1024 * 1024 * 1024 * 1024 * 1024,
-		"Ei": 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
-	}
-
 	includeContainerStateReason = map[string][]string{
 		"waiting": {
 			"errimagepull",
