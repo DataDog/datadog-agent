64dbdc925e8dc8f42ca77e2693f160f761acf190
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-07-11 14:08:15 -0400

    I actually wanted select not if :)

--- pkg/process/metadata/workloadmeta/grpc.go
+++ pkg/process/metadata/workloadmeta/grpc.go
@@ -128,8 +128,11 @@ func (l *GRPCServer) StreamEntities(_ *pbgo.ProcessStreamEntitiesRequest, out pb
 	for {
 		select {
 		case diff := <-l.extractor.ProcessCacheDiff():
-			if _, ok := <-streamCtx.Done(); !ok { // Ensure that if streamCtx.Done() is closed, we always choose that path.
+			// Ensure that if streamCtx.Done() is closed, we always choose that path.
+			select {
+			case <-streamCtx.Done():
 				return DuplicateConnectionErr
+			default:
 			}
 
 			// Do not send diff if it has the same or older version of the cache snapshot sent on the connection creation
