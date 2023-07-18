117e923556628ef7f651056e9b440e68077c6c9d
Florent Clarret <florent.clarret@datadoghq.com>
2023-07-18 16:40:03 +0200

    use PyPi instead of github

--- omnibus/config/software/oracledb-py3.rb
+++ omnibus/config/software/oracledb-py3.rb
@@ -3,8 +3,10 @@ default_version "1.3.2"
 
 dependency "pip3"
 
-source :url => "https://github.com/oracle/python-oracledb/archive/refs/tags/v#{version}.tar.gz",
-       :sha256 => "0a8a6bd2aacf7db0f26b0cb9991316f3e02f4bf671c67ca38b0baa9fd6fbb21f",
+# The github repository contains a submodule which is not shipped with their released file.
+# Grabbing the tar from PyPi instead
+source :url => "https://files.pythonhosted.org/packages/05/18/516f38a55c99d8ac417a817afff88f484300ce7e4d94058055af1f1461e5/oracledb-#{version}.tar.gz",
+       :sha256 => "bb3c391c167b5778ddb15a7538a2b36db5c9b88a50c86c61781ca9ff302bb643",
        :extract => :seven_zip
 
 relative_path "python-oracledb-#{version}"
