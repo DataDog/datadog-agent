1b7bd5d10e79dd3072472c186f5cf52789209019
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2023-07-19 11:14:58 +0200

    simplify helper functions on darwin

--- pkg/gohai/cpu/cpu_darwin.go
+++ pkg/gohai/cpu/cpu_darwin.go
@@ -12,30 +12,30 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func getSysctl[U any, V any](call func(string) (U, error), cast func(U) V, key string) (V, error) {
+func getSysctl[U any, V any](call func(string) (U, error), cast func(U) V, key string) utils.Value[V] {
 	value, err := call(key)
 	// sysctl returns ENOENT when the key doesn't exists on the device
 	// eg. on ARM, the frequency and the family don't exist
 	if err == unix.ENOENT {
 		err = utils.ErrNotCollectable
 	}
-	return cast(value), err
+	return utils.NewValueFrom(cast(value), err)
 }
 
 // type returned by sysctl is string, stored as string
-func getSysctlString(key string) (string, error) {
+func getSysctlString(key string) utils.Value[string] {
 	castFun := func(val string) string { return val }
 	return getSysctl(unix.Sysctl, castFun, key)
 }
 
 // type returned by sysctl is uint32, stored as string
-func getSysctlInt32String(key string) (string, error) {
+func getSysctlInt32String(key string) utils.Value[string] {
 	castFun := func(val uint32) string { return fmt.Sprintf("%d", val) }
 	return getSysctl(unix.SysctlUint32, castFun, key)
 }
 
 // type returned by sysctl is uint32, stored as uint64
-func getSysctlInt32Int64(key string) (uint64, error) {
+func getSysctlInt32Int64(key string) utils.Value[uint64] {
 	castFun := func(val uint32) uint64 { return uint64(val) }
 	return getSysctl(unix.SysctlUint32, castFun, key)
 }
@@ -52,15 +52,15 @@ func getCPUInfo() *Info {
 
 	// use `man 3 sysctl` to see the type returned when using each option
 
-	cpuInfo.VendorID = utils.NewValueFrom(getSysctlString("machdep.cpu.vendor"))
-	cpuInfo.ModelName = utils.NewValueFrom(getSysctlString("machdep.cpu.brand_string"))
+	cpuInfo.VendorID = getSysctlString("machdep.cpu.vendor")
+	cpuInfo.ModelName = getSysctlString("machdep.cpu.brand_string")
 
-	cpuInfo.Family = utils.NewValueFrom(getSysctlInt32String("machdep.cpu.family"))
-	cpuInfo.Model = utils.NewValueFrom(getSysctlInt32String("machdep.cpu.model"))
-	cpuInfo.Stepping = utils.NewValueFrom(getSysctlInt32String("machdep.cpu.stepping"))
+	cpuInfo.Family = getSysctlInt32String("machdep.cpu.family")
+	cpuInfo.Model = getSysctlInt32String("machdep.cpu.model")
+	cpuInfo.Stepping = getSysctlInt32String("machdep.cpu.stepping")
 
-	cpuInfo.CPUCores = utils.NewValueFrom(getSysctlInt32Int64("hw.physicalcpu"))
-	cpuInfo.CPULogicalProcessors = utils.NewValueFrom(getSysctlInt32Int64("hw.logicalcpu"))
+	cpuInfo.CPUCores = getSysctlInt32Int64("hw.physicalcpu")
+	cpuInfo.CPULogicalProcessors = getSysctlInt32Int64("hw.logicalcpu")
 
 	// mhz is returned in hz but stored in mhz so we use a specific cast function
 	mhzCast := func(value uint64) float64 {
@@ -68,7 +68,7 @@ func getCPUInfo() *Info {
 	}
 	// unix.SysctlUint64 takes extra arguments so we have to use a wrapper
 	sysctlUint64 := func(key string) (uint64, error) { return unix.SysctlUint64(key) }
-	cpuInfo.Mhz = utils.NewValueFrom(getSysctl(sysctlUint64, mhzCast, "hw.cpufrequency"))
+	cpuInfo.Mhz = getSysctl(sysctlUint64, mhzCast, "hw.cpufrequency")
 
 	return cpuInfo
 }
