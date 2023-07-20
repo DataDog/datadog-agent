9e2a675b74c2dbde2c524d71ca8931b94e036cc4
Adam Karpowich <adam.karpowich@datadoghq.com>
2023-07-20 15:31:37 -0400

    Revert "return byteslice to pool as is"
    This reverts commit 29bc463c6f243481d417c1d6d2de975cbbdbc881.

--- cmd/system-probe/modules/network_tracer.go
+++ cmd/system-probe/modules/network_tracer.go
@@ -295,7 +295,7 @@ func writeConnections(w http.ResponseWriter, marshaler encoding.Marshaler, cs *n
 
 	w.Header().Set("Content-type", marshaler.ContentType())
 	w.Write(buf) //nolint:errcheck
-	pool.Put(bs)
+	pool.Put(bs[:0])
 	log.Tracef("/connections: %d connections, %d bytes", len(cs.Conns), len(buf))
 }
 
