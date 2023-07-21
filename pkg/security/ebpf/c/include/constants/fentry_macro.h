efc82a7b1b020078f4a2e383d670b4be04f130de
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-07-21 11:37:50 +0200

    add new `HOOK_EXIT` macro

--- pkg/security/ebpf/c/include/constants/fentry_macro.h
+++ pkg/security/ebpf/c/include/constants/fentry_macro.h
@@ -4,6 +4,7 @@
 #ifdef USE_FENTRY
 
 #define HOOK_ENTRY(func_name) SEC("fentry/" func_name)
+#define HOOK_EXIT(func_name) SEC("fexit/" func_name)
 typedef unsigned long long ctx_t;
 #define CTX_PARM1(ctx) (u64)(ctx[0])
 #define CTX_PARM2(ctx) (u64)(ctx[1])
@@ -12,6 +13,7 @@ typedef unsigned long long ctx_t;
 #else
 
 #define HOOK_ENTRY(func_name) SEC("kprobe/" func_name)
+#define HOOK_EXIT(func_name) SEC("kretprobe/" func_name)
 typedef struct pt_regs ctx_t;
 #define CTX_PARM1(ctx) PT_REGS_PARM1(ctx)
 #define CTX_PARM2(ctx) PT_REGS_PARM2(ctx)
