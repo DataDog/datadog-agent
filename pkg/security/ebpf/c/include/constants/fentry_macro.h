3c5223da7e50f679a9dc8da9c0bb1f1a3321b5d4
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-07-21 11:40:07 +0200

    convert `io_ring_ctx_alloc`

--- pkg/security/ebpf/c/include/constants/fentry_macro.h
+++ pkg/security/ebpf/c/include/constants/fentry_macro.h
@@ -10,6 +10,8 @@ typedef unsigned long long ctx_t;
 #define CTX_PARM2(ctx) (u64)(ctx[1])
 #define CTX_PARM3(ctx) (u64)(ctx[2])
 
+#define CTX_PARMRET(ctx, argc) (u64)(ctx[argc+1])
+
 #else
 
 #define HOOK_ENTRY(func_name) SEC("kprobe/" func_name)
@@ -18,6 +20,8 @@ typedef struct pt_regs ctx_t;
 #define CTX_PARM1(ctx) PT_REGS_PARM1(ctx)
 #define CTX_PARM2(ctx) PT_REGS_PARM2(ctx)
 #define CTX_PARM3(ctx) PT_REGS_PARM3(ctx)
+
+#define CTX_PARMRET(ctx, _argc) PT_REGS_RC(ctx)
 
 #endif
 
