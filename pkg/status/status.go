20fdd8b087a5015ab2c8ce2b2fb878e37b14a7c8
Paul <paul.coignet@datadoghq.com>
2023-07-25 11:22:48 +0200

    Apply suggestions from code review
    Co-authored-by: Arthur Bellal <arthur.bellal@datadoghq.com>

--- pkg/status/status.go
+++ pkg/status/status.go
@@ -311,11 +311,11 @@ func getRemoteConfigStatus() map[string]interface{} {
 		json.Unmarshal([]byte(remoteConfigStatusJSON), &status) //nolint:errcheck
 	} else {
 		if !config.Datadog.GetBool("remote_configuration.enabled") {
-			status["disabledReason"] = "`remote_configuration.enabled` is set to false"
+			status["disabledReason"] = "it is explicitly disabled in the agent configuration. (`remote_configuration.enabled: false`)"
 		} else if config.Datadog.GetBool("fips.enabled") {
-			status["disabledReason"] = "`fips.enabled` is set to true"
+			status["disabledReason"] = "it is not supported when FIPS is enabled. (`fips.enabled: true`)"
 		} else if config.Datadog.GetString("site") == "ddog-gov.com" {
-			status["disabledReason"] = "the agent runs on govcloud"
+			status["disabledReason"] = "it is not supported on GovCloud. (`site: \"ddog-gov.com\"`)"
 		}
 	}
 
