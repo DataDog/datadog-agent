7206a8e1bf84db51db2a34d31ac3b511e3ebd640
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-07-26 13:56:34 +0200

    more cleanup

--- pkg/security/ebpf/c/include/constants/syscall_macro.h
+++ pkg/security/ebpf/c/include/constants/syscall_macro.h
@@ -86,20 +86,24 @@
 #define __SC_PASS(t, a) a
 
 #define SYSCALL_PREFIX "sys"
+#define KPROBE_CTX_TYPE struct pt_regs
+#define KRETPROBE_CTX_TYPE struct pt_regs
+#define FENTRY_CTX_TYPE ctx_t
+#define FEXIT_CTX_TYPE ctx_t
 
 #define SYSCALL_ABI_HOOKx(x,word_size,type,TYPE,prefix,syscall,suffix,...) \
-    int __attribute__((always_inline)) type##__##sys##syscall(void *ctx __JOIN(x,__SC_DECL,__VA_ARGS__)); \
+    int __attribute__((always_inline)) type##__##sys##syscall(TYPE##_CTX_TYPE *ctx __JOIN(x,__SC_DECL,__VA_ARGS__)); \
     SEC(#type "/" SYSCALL##word_size##_PREFIX #prefix SYSCALL_PREFIX #syscall #suffix) \
-    int type##__ ##word_size##_##prefix ##sys##syscall##suffix(void *ctx) { \
+    int type##__ ##word_size##_##prefix ##sys##syscall##suffix(TYPE##_CTX_TYPE *ctx) { \
         SYSCALL_##TYPE##_PROLOG(x,__SC_##word_size##_PARAM,syscall,__VA_ARGS__) \
         return type##__sys##syscall(ctx __JOIN(x,__SC_PASS,__VA_ARGS__)); \
     }
 
-#define SYSCALL_HOOK_COMMON(x,type,syscall,...) int __attribute__((always_inline)) type##__sys##syscall(void *ctx __JOIN(x,__SC_DECL,__VA_ARGS__))
+#define SYSCALL_HOOK_COMMON(x,type,TYPE,syscall,...) int __attribute__((always_inline)) type##__sys##syscall(TYPE##_CTX_TYPE *ctx __JOIN(x,__SC_DECL,__VA_ARGS__))
 #define SYSCALL_KRETPROBE_PROLOG(...)
 
 #define SYSCALL_FENTRY_PROLOG(x,m,syscall,...) \
-  struct pt_regs *rctx = (struct pt_regs *) ((ctx_t *)ctx)[0]; \
+  struct pt_regs *rctx = (struct pt_regs *) (ctx)[0]; \
   if (!rctx) return 0; \
   __MAP(x,m,__VA_ARGS__)
 
@@ -107,23 +111,23 @@
   #define __SC_64_PARAM(n, t, a) t a; bpf_probe_read(&a, sizeof(t), (void*) &SYSCALL64_PT_REGS_PARM##n(rctx));
   #define __SC_32_PARAM(n, t, a) t a; bpf_probe_read(&a, sizeof(t), (void*) &SYSCALL32_PT_REGS_PARM##n(rctx));
   #define SYSCALL_KPROBE_PROLOG(x,m,syscall,...) \
-    struct pt_regs *rctx = (struct pt_regs *) PT_REGS_PARM1((struct pt_regs *)ctx); \
+    struct pt_regs *rctx = (struct pt_regs *) PT_REGS_PARM1(ctx); \
     if (!rctx) return 0; \
     __MAP(x,m,__VA_ARGS__)
   #define SYSCALL_HOOKx(x,type,TYPE,prefix,name,...) \
     SYSCALL_ABI_HOOKx(x,32,type,TYPE,prefix,name,,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,,name,,__VA_ARGS__) \
-    SYSCALL_HOOK_COMMON(x,type,name,__VA_ARGS__)
+    SYSCALL_HOOK_COMMON(x,type,TYPE,name,__VA_ARGS__)
   #define SYSCALL_COMPAT_HOOKx(x,type,TYPE,name,...) \
     SYSCALL_ABI_HOOKx(x,32,type,TYPE,compat_,name,,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,,name,,__VA_ARGS__) \
-    SYSCALL_HOOK_COMMON(x,type,name,__VA_ARGS__)
+    SYSCALL_HOOK_COMMON(x,type,TYPE,name,__VA_ARGS__)
   #define SYSCALL_COMPAT_TIME_HOOKx(x,type,TYPE,name,...) \
     SYSCALL_ABI_HOOKx(x,32,type,TYPE,compat_,name,,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,32,type,TYPE,,name,_time32,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,,name,,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,,name,_time32,__VA_ARGS__) \
-    SYSCALL_HOOK_COMMON(x,type,name,__VA_ARGS__)
+    SYSCALL_HOOK_COMMON(x,type,TYPE,name,__VA_ARGS__)
 #else
   #define __SC_64_PARAM(n, t, a) t a = (t) SYSCALL64_PT_REGS_PARM##n(rctx);
   #define __SC_32_PARAM(n, t, a) t a = (t) SYSCALL32_PT_REGS_PARM##n(rctx);
@@ -134,15 +138,15 @@
   #define SYSCALL_HOOKx(x,type,TYPE,prefix,name,...) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,compat_,name,,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,,name,,__VA_ARGS__) \
-    SYSCALL_HOOK_COMMON(x,type,name,__VA_ARGS__)
+    SYSCALL_HOOK_COMMON(x,type,TYPE,name,__VA_ARGS__)
   #define SYSCALL_COMPAT_HOOKx(x,type,TYPE,name,...) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,compat_,name,,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,,name,,__VA_ARGS__) \
-    SYSCALL_HOOK_COMMON(x,type,name,__VA_ARGS__)
+    SYSCALL_HOOK_COMMON(x,type,TYPE,name,__VA_ARGS__)
   #define SYSCALL_COMPAT_TIME_HOOKx(x,type,TYPE,name,...) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,compat_,name,,__VA_ARGS__) \
     SYSCALL_ABI_HOOKx(x,64,type,TYPE,,name,,__VA_ARGS__) \
-    SYSCALL_HOOK_COMMON(x,type,name,__VA_ARGS__)
+    SYSCALL_HOOK_COMMON(x,type,TYPE,name,__VA_ARGS__)
 #endif
 
 #define SYSCALL_KPROBE0(name, ...) SYSCALL_HOOKx(0,kprobe,KPROBE,,_##name,__VA_ARGS__)
