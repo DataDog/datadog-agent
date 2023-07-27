e9820d793f3f092067f6e4e295eb8b147b628966
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2023-07-27 15:27:37 +0200

    move back tests in filesystem_nix_test.go

--- pkg/gohai/filesystem/filesystem_nix_test.go
+++ pkg/gohai/filesystem/filesystem_nix_test.go
@@ -10,6 +10,7 @@ package filesystem
 
 import (
 	"math/rand"
+	"strconv"
 	"testing"
 
 	"github.com/moby/sys/mountinfo"
@@ -21,6 +22,41 @@ import (
 
 // Used for dynamic test field value generation
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func TestNixCollect(t *testing.T) {
+	mountsObj, err := new(FileSystem).Collect()
+	require.NoError(t, err)
+
+	mounts, ok := mountsObj.([]interface{})
+	require.True(t, ok, "Could not cast %+v to []interface{}", mountsObj)
+
+	require.Greater(t, len(mounts), 0)
+
+	for _, mountObj := range mounts {
+		mount := mountObj.(map[string]string)
+		assert.NotEmpty(t, mount["name"])
+
+		assert.NotEmpty(t, mount["kb_size"])
+		sizeKB, err := strconv.Atoi(mount["kb_size"])
+		require.NoError(t, err)
+		assert.GreaterOrEqual(t, sizeKB, 0)
+
+		assert.NotEmpty(t, mount["mounted_on"])
+	}
+}
+
+func TestNixGet(t *testing.T) {
+	mounts, err := new(FileSystem).Get()
+	require.NoError(t, err)
+
+	require.Greater(t, len(mounts), 0)
+
+	for _, mount := range mounts {
+		assert.NotEmpty(t, mount.Name)
+		assert.GreaterOrEqual(t, mount.SizeKB, uint64(0))
+		assert.NotEmpty(t, mount.MountedOn)
+	}
+}
 
 func TestNixFSTypeFiltering(t *testing.T) {
 
--- pkg/gohai/filesystem/filesystem_test.go
+++ pkg/gohai/filesystem/filesystem_test.go
@@ -6,52 +6,15 @@ package filesystem
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCollect(t *testing.T) {
-	mountsObj, err := new(FileSystem).Collect()
-	require.NoError(t, err)
-
-	mounts, ok := mountsObj.([]interface{})
-	require.True(t, ok, "Could not cast %+v to []interface{}", mountsObj)
-
-	require.Greater(t, len(mounts), 0)
-
-	for _, mountObj := range mounts {
-		mount := mountObj.(map[string]string)
-		assert.NotEmpty(t, mount["name"])
-
-		assert.NotEmpty(t, mount["kb_size"])
-		sizeKB, err := strconv.Atoi(mount["kb_size"])
-		require.NoError(t, err)
-		assert.GreaterOrEqual(t, sizeKB, 0)
-
-		assert.NotEmpty(t, mount["mounted_on"])
-	}
-}
-
-func TestGet(t *testing.T) {
-	mounts, err := new(FileSystem).Get()
-	require.NoError(t, err)
-
-	require.Greater(t, len(mounts), 0)
-
-	for _, mount := range mounts {
-		assert.NotEmpty(t, mount.Name)
-		assert.GreaterOrEqual(t, mount.SizeKB, uint64(0))
-		assert.NotEmpty(t, mount.MountedOn)
-	}
-}
-
 func TestGetTimeout(t *testing.T) {
 	prevTimeout := timeout
-	timeout = time.Microsecond
+	timeout = time.Nanosecond
 	defer func() {
 		timeout = prevTimeout
 	}()
