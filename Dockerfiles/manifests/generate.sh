e997cc30b67949e7485a4d9ffc270a3c0384955c
David Ortiz <david.ortiz@datadoghq.com>
2023-07-31 11:31:31 +0200

    [Dockerfiles/manifests/generate.sh] Update to kubernetes 1.24

--- Dockerfiles/manifests/generate.sh
+++ Dockerfiles/manifests/generate.sh
@@ -131,7 +131,7 @@ for values in "$TMPDIR"/values-*.yaml; do
     rm -rf "${type:?}"
     mkdir "${type:?}"
 
-    helm template --kube-version 1.21 --namespace default datadog "${HELM_DATADOG_CHART:-datadog/datadog}" --set datadog.apiKey=DUMMY_API_KEY --values "$values" --output-dir "$TMPDIR/generated_$type"
+    helm template --kube-version 1.24 --namespace default datadog "${HELM_DATADOG_CHART:-datadog/datadog}" --set datadog.apiKey=DUMMY_API_KEY --values "$values" --output-dir "$TMPDIR/generated_$type"
     for file in "$TMPDIR/generated_$type"/datadog/templates/*.yaml; do
         # Skip files containing only comments like `containers-common-env.yaml`
         if [[ "$(yq eval '. | length' "$file")" == 0 ]]; then
