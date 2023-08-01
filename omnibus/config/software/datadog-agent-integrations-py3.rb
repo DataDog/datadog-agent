071fde955798eb1ac0369123b34c54b8d4755312
Jo-Ann Meunier <jojo.meunier@datadoghq.com>
2023-08-01 11:31:00 -0400

    test installing psycogp[c] package

--- omnibus/config/software/datadog-agent-integrations-py3.rb
+++ omnibus/config/software/datadog-agent-integrations-py3.rb
@@ -16,9 +16,6 @@ dependency 'snowflake-connector-python-py3'
 dependency 'confluent-kafka-python'
 
 if arm?
-  # psycopg2 doesn't come with pre-built wheel on the arm architecture.
-  # to compile from source, it requires the `pg_config` executable present on the $PATH
-  dependency 'postgresql'
   # same with libffi to build the cffi wheel
   dependency 'libffi'
   # same with libxml2 and libxslt to build the lxml wheel
@@ -31,6 +28,9 @@ if osx?
 end
 
 if linux?
+  # psycopg2 & psycopg3 don't come with pre-built wheel on linux.
+  # to compile from source, it requires the `pg_config` executable present on the $PATH
+  dependency 'postgresql'
   # add nfsiostat script
   dependency 'unixodbc'
   dependency 'freetds'  # needed for SQL Server integration
