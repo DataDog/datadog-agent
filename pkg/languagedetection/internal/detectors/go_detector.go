d22dcab38c9565448d6f8e3fba9da8fc39327328
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-08-01 13:39:30 -0400

    Address some comments

--- pkg/languagedetection/internal/detectors/go_detector.go
+++ pkg/languagedetection/internal/detectors/go_detector.go
@@ -10,22 +10,26 @@ package detectors
 import (
 	"debug/elf"
 	"fmt"
+	"path"
+	"strconv"
 
 	dderrors "github.com/DataDog/datadog-agent/pkg/errors"
 	"github.com/DataDog/datadog-agent/pkg/languagedetection/languagemodels"
 	"github.com/DataDog/datadog-agent/pkg/network/go/binversion"
+	"github.com/DataDog/datadog-agent/pkg/process/util"
 )
 
-type GoDetector struct{}
+type GoDetector struct {
+	hostProc string
+}
 
 // DetectLanguage allows for detecting if a process is a go process, and its version.
 // Note that currently the GoDetector only returns non-retriable errors since in all cases we will not be able to detect the language.
 // Scenarios in which we can return an error:
-//   - Invalid permissions. The system-probe is not running as root, or the container does not have CAP_PTRACE.
 //   - Program exits early, and we fail to call `elf.Open`. Note that in the future it may be possible to lock the directory using a system call.
 //   - Program is not a go binary, or has build tags stripped out. In this case we return a `dderrors.NotFound`.
-func (GoDetector) DetectLanguage(pid int) (languagemodels.Language, error) {
-	exePath := getExePath(pid)
+func (d *GoDetector) DetectLanguage(pid int) (languagemodels.Language, error) {
+	exePath := d.getHostProc(pid)
 
 	bin, err := elf.Open(exePath)
 	if err != nil {
@@ -42,4 +46,12 @@ func (GoDetector) DetectLanguage(pid int) (languagemodels.Language, error) {
 		Name:    languagemodels.Go,
 		Version: vers,
 	}, nil
+}
+
+func (d *GoDetector) getHostProc(pid int) string {
+	if d.hostProc == "" {
+		d.hostProc = util.HostProc()
+	}
+
+	return path.Join(d.hostProc, strconv.Itoa(pid), "exe")
 }
