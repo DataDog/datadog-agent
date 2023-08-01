3aa324d646817d5ac20638feca98295974a56ae8
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-08-01 17:47:08 +0200

    Should make retry mechanism working when no artifacts are found

--- tasks/libs/github_actions_tools.py
+++ tasks/libs/github_actions_tools.py
@@ -165,7 +165,7 @@ def download_artifacts(run_id, destination="."):
     print(color_message(f"Downloading artifacts for run {run_id} to {destination}", "blue"))
     github_workflows = create_or_refresh_macos_build_github_workflows()
     run_artifacts = github_workflows.workflow_run_artifacts(run_id)
-    if len(run_artifacts) == 0:
+    if run_artifacts["total_count"] == 0:
         raise ConnectionError
 
     print("Found the following artifacts: ", run_artifacts)
