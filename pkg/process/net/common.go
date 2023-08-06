3fdbdf77d1597dc89d096ca82aadb51cbed67acf
amit.slavin <amit.slavin@datadoghq.com>
2023-08-06 09:56:01 +0300

    fix common

--- pkg/process/net/common.go
+++ pkg/process/net/common.go
@@ -24,9 +24,8 @@ import (
 	netEncoding "github.com/DataDog/datadog-agent/pkg/network/encoding"
 	procEncoding "github.com/DataDog/datadog-agent/pkg/process/encoding"
 	reqEncoding "github.com/DataDog/datadog-agent/pkg/process/encoding/request"
-	pbgo "github.com/DataDog/datadog-agent/pkg/proto/pbgo/process"
 	"github.com/DataDog/datadog-agent/pkg/proto/connectionserver"
-	"github.com/DataDog/datadog-agent/pkg/proto/pbgo"
+	pbgo "github.com/DataDog/datadog-agent/pkg/proto/pbgo/process"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 	"github.com/DataDog/datadog-agent/pkg/util/retry"
 )
