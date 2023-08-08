36ca2e65f2d50035f2130b932f9bed9072bcb491
David Lee <david.lee@datadoghq.com>
2023-08-08 11:54:42 -0700

    Update log.go

--- cmd/serverless-init/log/log.go
+++ cmd/serverless-init/log/log.go
@@ -115,7 +115,7 @@ func (cw *CustomWriter) writeWithMaxBufferSize(p []byte, maxBufferSize int) (n i
 	// Prevent buffer overflow, flush the buffer if writing the current chunk
 	// will exceed maxBufferSize
 	if cw.LineBuffer.Len()+len(p) > maxBufferSize {
-		log.Errorf("Log buffer exceeds %d kb. Flushing log buffer\n", maxBufferSize)
+		log.Errorf("Log buffer exceeds %d kb. Flushing log buffer", maxBufferSize)
 		Write(cw.LogConfig, getByteArrayClone(cw.LineBuffer.Bytes()), cw.IsError)
 		cw.LineBuffer.Reset()
 	}
