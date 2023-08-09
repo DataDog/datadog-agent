5a9a09aefe0f51bc9a8044496cfaf10e511a5a97
Branden Clark <branden.clark@datadoghq.com>
2023-08-08 23:25:17 -0400

    use common Windows Service code in dogstatsd

--- cmd/dogstatsd/main_windows.go
+++ cmd/dogstatsd/main_windows.go
@@ -7,11 +7,9 @@ package main
 
 import (
 	"context"
-	"fmt"
 	_ "net/http/pprof"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/DataDog/datadog-agent/cmd/dogstatsd/command"
 	"github.com/DataDog/datadog-agent/cmd/dogstatsd/subcommands/start"
@@ -23,14 +21,10 @@ import (
 	pkglog "github.com/DataDog/datadog-agent/pkg/util/log"
 
 	"github.com/DataDog/datadog-agent/pkg/util/winutil"
-
-	"golang.org/x/sys/windows/svc"
-	"golang.org/x/sys/windows/svc/debug"
-	"golang.org/x/sys/windows/svc/eventlog"
+	"github.com/DataDog/datadog-agent/pkg/util/winutil/servicemain"
 )
 
 var (
-	elog           debug.Log
 	defaultLogFile = "c:\\programdata\\datadog\\logs\\dogstatsd.log"
 
 	// DefaultConfPath points to the folder containing datadog.yaml
@@ -50,38 +44,38 @@ func init() {
 // ServiceName is the name of the service in service control manager
 const ServiceName = "dogstatsd"
 
-// EnableLoggingToFile -- set up logging to file
-
 func main() {
 	// set the Agent flavor
 	flavor.SetFlavor(flavor.Dogstatsd)
 
-	isIntSess, err := svc.IsAnInteractiveSession()
-	if err != nil {
-		fmt.Printf("failed to determine if we are running in an interactive session: %v\n", err)
-	}
-	if !isIntSess {
-		runService(false)
+	if servicemain.RunningAsWindowsService() {
+		servicemain.Run(&service{})
 		return
 	}
 	defer pkglog.Flush()
 
-	if err = command.MakeRootCommand(defaultLogFile).Execute(); err != nil {
+	if err := command.MakeRootCommand(defaultLogFile).Execute(); err != nil {
 		pkglog.Error(err)
 		os.Exit(-1)
 	}
 }
 
-type myservice struct{}
+type service struct{}
 
-func (m *myservice) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (ssec bool, errno uint32) {
-	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown
-	changes <- svc.Status{State: svc.StartPending}
-	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+func (s *service) Name() string {
+	return ServiceName
+}
 
+func (s *service) Init() error {
+	// Nothing to do, kept empty for compatibility with previous implementation.
+	return nil
+}
+
+func (s *service) Run(ctx context.Context) error {
 	pkglog.Infof("Service control function")
 
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(ctx)
+
 	cliParams := &start.CLIParams{}
 	components := &start.DogstatsdComponents{}
 
@@ -96,69 +90,14 @@ func (m *myservice) Execute(args []string, r <-chan svc.ChangeRequest, changes c
 
 	if err != nil {
 		pkglog.Errorf("Failed to start agent %v", err)
-		elog.Error(0xc0000008, err.Error())
-		errno = 1 // indicates non-successful return from handler.
 		start.StopAgent(cancel, components)
-		changes <- svc.Status{State: svc.Stopped}
-		return
+		return err
 	}
-	elog.Info(0x40000003, ServiceName)
 
-loop:
-	for {
-		select {
-		case c := <-r:
-			switch c.Cmd {
-			case svc.Interrogate:
-				changes <- c.CurrentStatus
-				// Testing deadlock from https://code.google.com/p/winsvc/issues/detail?id=4
-				time.Sleep(100 * time.Millisecond)
-				changes <- c.CurrentStatus
-			case svc.Stop:
-				pkglog.Info("Received stop message from service control manager")
-				elog.Info(0x4000000b, ServiceName)
-				break loop
-			case svc.PreShutdown:
-				pkglog.Info("Received pre-shutdown message from service control manager")
-				elog.Info(0x4000000d, ServiceName)
-				break loop
-			case svc.Shutdown:
-				pkglog.Info("Received shutdown message from service control manager")
-				elog.Info(0x4000000c, ServiceName)
-				break loop
-			default:
-				pkglog.Warnf("unexpected control request #%d", c)
-				elog.Warning(0xc0000005, fmt.Sprint(c.Cmd))
-			}
-		}
-	}
-	elog.Info(0x40000006, ServiceName)
+	// Wait for stop signal
+	<-ctx.Done()
+
 	pkglog.Infof("Initiating service shutdown")
-	changes <- svc.Status{State: svc.StopPending}
 	start.StopAgent(cancel, components)
-	changes <- svc.Status{State: svc.Stopped}
-	return
-}
-
-func runService(isDebug bool) {
-	var err error
-	if isDebug {
-		elog = debug.New(ServiceName)
-	} else {
-		elog, err = eventlog.Open(ServiceName)
-		if err != nil {
-			return
-		}
-	}
-	defer elog.Close()
-
-	elog.Info(0x40000007, ServiceName)
-	run := svc.Run
-
-	err = run(ServiceName, &myservice{})
-	if err != nil {
-		elog.Error(0xc0000008, err.Error())
-		return
-	}
-	elog.Info(0x40000004, ServiceName)
+	return nil
 }
