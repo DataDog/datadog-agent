a80e74ff7dd84e84ec93485f41c8b56bfd99f440
Dylan Yang <dylan.yang@datadoghq.com>
2023-08-10 16:38:17 -0400

    add unit tests

--- pkg/serverless/executioncontext/executioncontext_test.go
+++ pkg/serverless/executioncontext/executioncontext_test.go
@@ -74,6 +74,17 @@ func TestSetArnFromExtensionResponse(t *testing.T) {
 	assert.Equal("arn:aws:lambda:us-east-1:123456789012:function:my-super-function", ec.arn)
 }
 
+func TestSetArnFromExtensionResponse(t *testing.T) {
+	assert := assert.New(t)
+
+	testArn := "arn:aws:lambda:us-east-1:123456789012:function:MY-SUPER-function"
+
+	ec := ExecutionContext{}
+	ec.SetArnFromExtensionResponse(testArn)
+
+	assert.Equal("arn:aws:lambda:us-east-1:123456789012:function:my-super-function", ec.arn)
+}
+
 func TestUpdateFromStartLog(t *testing.T) {
 	assert := assert.New(t)
 
