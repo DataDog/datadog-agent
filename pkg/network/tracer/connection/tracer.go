4e2bd8041c7fb8a26946d307b33aeaf4a3d939c8
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-08-16 14:58:38 -0700

    Fix manager mutation on fallback

--- pkg/network/tracer/connection/tracer.go
+++ pkg/network/tracer/connection/tracer.go
@@ -24,6 +24,8 @@ import (
 	"go.uber.org/atomic"
 	"golang.org/x/sys/unix"
 
+	manager "github.com/DataDog/ebpf-manager"
+
 	"github.com/DataDog/datadog-agent/pkg/collector/corechecks/ebpf/probe/ebpfcheck"
 	ddebpf "github.com/DataDog/datadog-agent/pkg/ebpf"
 	"github.com/DataDog/datadog-agent/pkg/network"
@@ -37,7 +39,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/process/util"
 	"github.com/DataDog/datadog-agent/pkg/telemetry"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
-	manager "github.com/DataDog/ebpf-manager"
 )
 
 // TracerType is the type of the underlying tracer
@@ -199,10 +200,7 @@ func NewTracer(config *config.Config, bpfTelemetry *nettelemetry.EBPFTelemetry) 
 		closedChannelSize = config.ClosedChannelSize
 	}
 	perfHandlerTCP := ddebpf.NewPerfHandler(closedChannelSize)
-	m := &manager.Manager{
-		DumpHandler: dumpMapsHandler,
-	}
-
+	var m *manager.Manager
 	var tracerType TracerType = TracerTypeFentry
 	var closeTracerFn func()
 	closeTracerFn, err := fentry.LoadTracer(config, m, mgrOptions, perfHandlerTCP)
@@ -215,10 +213,11 @@ func NewTracer(config *config.Config, bpfTelemetry *nettelemetry.EBPFTelemetry) 
 		// load the kprobe tracer
 		log.Info("fentry tracer not supported, falling back to kprobe tracer")
 		var kprobeTracerType kprobe.TracerType
-		closeTracerFn, kprobeTracerType, err = kprobe.LoadTracer(config, m, mgrOptions, perfHandlerTCP)
+		m, closeTracerFn, kprobeTracerType, err = kprobe.LoadTracer(config, mgrOptions, perfHandlerTCP)
 		if err != nil {
 			return nil, err
 		}
+		m.DumpHandler = dumpMapsHandler
 		tracerType = TracerType(kprobeTracerType)
 	}
 	ebpfcheck.AddNameMappings(m, "npm_tracer")
