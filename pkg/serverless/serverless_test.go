214614fe7d38b546f318cf9ca2e92b7310bfc6ec
David Lee <david.lee@datadoghq.com>
2023-08-21 08:19:06 -0700

    remove unit test

--- pkg/serverless/serverless_test.go
+++ pkg/serverless/serverless_test.go
@@ -9,15 +9,12 @@ import (
 	"fmt"
 	"os"
 	"runtime/debug"
-	"sort"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/DataDog/datadog-agent/pkg/serverless/daemon"
-	"github.com/DataDog/datadog-agent/pkg/serverless/flush"
-	"github.com/DataDog/datadog-agent/pkg/serverless/tags"
 )
 
 func TestMain(m *testing.M) {
@@ -25,47 +22,6 @@ func TestMain(m *testing.M) {
 	daemon.ShutdownDelay = 0
 	defer func() { daemon.ShutdownDelay = origShutdownDelay }()
 	os.Exit(m.Run())
-}
-
-func TestHandleInvocationShouldSetExtraTags(t *testing.T) {
-	d := daemon.StartDaemon("http://localhost:8124")
-	defer d.Stop()
-
-	// force daemon not to wait for flush at end of handleInvocation
-	d.SetFlushStrategy(flush.NewPeriodically(time.Second))
-	d.UseAdaptiveFlush(false)
-
-	// deadline = current time + 5s
-	deadlineMs := (time.Now().UnixNano())/1000000 + 5000
-
-	// setting DD_TAGS and DD_EXTRA_TAGS
-	t.Setenv("DD_TAGS", "a1:valueA1,a2:valueA2,A_MAJ:valueAMaj")
-	t.Setenv("DD_EXTRA_TAGS", "a3:valueA3 a4:valueA4")
-
-	callInvocationHandler(d, "arn:aws:lambda:us-east-1:123456789012:function:my-function", deadlineMs, 0, "myRequestID", handleInvocation)
-	architecture := fmt.Sprintf("architecture:%s", tags.ResolveRuntimeArch())
-
-	assert.Equal(t, 14, len(d.ExtraTags.Tags))
-
-	sort.Strings(d.ExtraTags.Tags)
-	assert.Equal(t, "a1:valuea1", d.ExtraTags.Tags[0])
-	assert.Equal(t, "a2:valuea2", d.ExtraTags.Tags[1])
-	assert.Equal(t, "a3:valuea3", d.ExtraTags.Tags[2])
-	assert.Equal(t, "a4:valuea4", d.ExtraTags.Tags[3])
-	assert.Equal(t, "a_maj:valueamaj", d.ExtraTags.Tags[4])
-	assert.Equal(t, "account_id:123456789012", d.ExtraTags.Tags[5])
-	assert.Equal(t, architecture, d.ExtraTags.Tags[6])
-	assert.Equal(t, "aws_account:123456789012", d.ExtraTags.Tags[7])
-	assert.Equal(t, "dd_extension_version:xxx", d.ExtraTags.Tags[8])
-	assert.Equal(t, "function_arn:arn:aws:lambda:us-east-1:123456789012:function:my-function", d.ExtraTags.Tags[9])
-	assert.Equal(t, "functionname:my-function", d.ExtraTags.Tags[10])
-	assert.Equal(t, "region:us-east-1", d.ExtraTags.Tags[11])
-	assert.Equal(t, "resource:my-function", d.ExtraTags.Tags[12])
-	assert.True(t, d.ExtraTags.Tags[13] == "runtime:unknown" || d.ExtraTags.Tags[13] == "runtime:provided.al2")
-
-	ecs := d.ExecutionContext.GetCurrentState()
-	assert.Equal(t, "arn:aws:lambda:us-east-1:123456789012:function:my-function", ecs.ARN)
-	assert.Equal(t, "myRequestID", ecs.LastRequestID)
 }
 
 func TestHandleInvocationShouldNotSIGSEGVWhenTimedOut(t *testing.T) {
