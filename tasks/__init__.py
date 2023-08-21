f998a443b4fef0a66920ade8d920fbf4ef28167a
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2023-08-21 16:22:55 -0400

    chore: Remove old invoke workaround

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -138,9 +138,6 @@ ns.add_collection(kmt)
 ns.configure(
     {
         'run': {
-            # workaround waiting for a fix being merged on Invoke,
-            # see https://github.com/pyinvoke/invoke/pull/407
-            'shell': os.environ.get('COMSPEC', os.environ.get('SHELL')),
             # this should stay, set the encoding explicitly so invoke doesn't
             # freak out if a command outputs unicode chars.
             'encoding': 'utf-8',
