837dbd95b9a51b5e19023a108bdeae06b2538605
ganeshkumarsv <53483484+ganeshkumarsv@users.noreply.github.com>
2023-08-22 15:54:49 -0400

    Update dependency-review.py

--- .github/dependency-review.py
+++ .github/dependency-review.py
@@ -35,8 +35,8 @@ for entry in REPOS:
     for vulnerability in vulnerabilities:
         output += f"\tSeverity: {vulnerability['severity']}\n"
         output += f"\tAdvisory Summary: {vulnerability['advisory_summary']}\n"
+        output += f"\tCVE ID: {CVE_INFO['cve_id']}\n"
         output += f"\tAdvisory URL: {vulnerability['advisory_url']}\n\n"
-        output += f"CVE ID: {CVE_INFO['cve_id']}\n"
 
 print(output)
 
