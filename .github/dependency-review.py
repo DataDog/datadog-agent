067ce1901209548f4884164a9cf18c70e39ee770
ganeshkumarsv <53483484+ganeshkumarsv@users.noreply.github.com>
2023-08-23 06:11:33 -0400

    Update dependency-review.py

--- .github/dependency-review.py
+++ .github/dependency-review.py
@@ -4,10 +4,8 @@ import requests
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 ORG_NAME = "datadog"
 
-API_URL = f"https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/issues/{os.getenv('INPUT_PULL_REQUEST')}/comments"
-
 DEPENDENCY_REVIEW_RESPONSE = requests.get(
-    f"https://api.github.com/repos/datadog/datadog-agent/dependency-graph/compare/0984805db2b6ef8dd83ab3482de654440dfd10a4...2e8e7359a0f831b78e081327e6c4967dd31f465e",
+    f"https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/dependency-graph/compare/{os.getenv('BASE_REF')}...{os.getenv('HEAD_REF')}",
     headers={"Authorization": f"Bearer {GITHUB_TOKEN}"}
 ).json()
 print(DEPENDENCY_REVIEW_RESPONSE)
