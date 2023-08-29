24055fe4b5f112b48aa5fa61dc37bbc56a1e9056
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2023-08-29 17:02:29 +0200

    Refactor platforms argument

--- tasks/need_fixing_linters/need_fixing_linters.py
+++ tasks/need_fixing_linters/need_fixing_linters.py
@@ -9,7 +9,7 @@ from .golangci_lint_parser import filter_lints, display_result, merge_results
 
 FIRST_COMMIT_HASH = "52a313fe7f5e8e16d487bc5dc770038bc234608b"
 # See https://go.dev/doc/install/source#environment for all available combinations of GOOS x GOARCH.
-CI_TESTED_OS_AND_ARCH = {"linux": ["amd64", "arm64"], "windows": ["amd64"], "darwin": ["amd64", "arm64"]}
+CI_TESTED_OS_AND_ARCH = ["linux,arm64", "linux,amd64", "windows,amd64", "darwin,amd64", "darwin,arm64"]
 
 def check_if_team_exists(team: str):
     """
@@ -22,19 +22,19 @@ def check_if_team_exists(team: str):
     else:
         print("[WARNING] No team entered. Displaying linters errors for all teams.\n")
 
-@task
-def need_fixing_linters(ctx, filter_team: str=None, from_commit_hash: str=FIRST_COMMIT_HASH, filter_linters: str="revive", show_output=False, tested_os_and_arch=CI_TESTED_OS_AND_ARCH):
+@task(iterable=['tested_os_and_arch'])
+def need_fixing_linters(ctx, filter_team: str=None, from_commit_hash: str=FIRST_COMMIT_HASH, filter_linters: str="revive", show_output=False, platforms=None):
     """
     This function displays the list of files that need fixing for a specific team and for specific linters.
 
-        Example: inv need-fixing-linters --filter-team "@DataDog/agent-platform" --filter-linters "revive"
+        Example: inv need-fixing-linters --filter-team "@DataDog/agent-platform" --filter-linters "revive" --platforms "linux,amd64" --platforms "linux,arm64"
 
         Parameters:
             team (str): keep only the files owned by a team. These are Github team names from the GITHUB_SLACK_MAP variable.
             from_commit_hash (str): the linters will run on all commit after this hash. Set on the first commit on the repo by default.
             filter_linters (str): comma-separated string of the linters you want to keep in the output. By default no filter applied.
             show_output (bool): show output of the raw linter run.
-            tested_os_and_arch (dict): dict of the OS and their arch on which the linter will run
+            platforms (list): list of comma-separated OS,arch on which the linter will run.
 
     """
     check_if_team_exists(filter_team)
@@ -43,11 +43,11 @@ def need_fixing_linters(ctx, filter_team: str=None, from_commit_hash: str=FIRST_
 
     # Run the linters for every OS x Arch and merge the results
     results_per_os_x_arch = dict()
-    for tested_os, tested_archs_list in tested_os_and_arch.items():
-        for tested_arch in tested_archs_list:
-            env = {"GOOS": tested_os, "GOARCH": tested_arch}
-            print(f"Running linters for {tested_os}_{tested_arch}...")
-            results_per_os_x_arch[f"{tested_os}_{tested_arch}"] = ctx.run(command, env=env, warn=True, hide=not show_output).stdout
+    platforms = list(map(lambda x: x.split(','), platforms if platforms else CI_TESTED_OS_AND_ARCH))
+    for tested_os, tested_arch in platforms:
+        env = {"GOOS": tested_os, "GOARCH": tested_arch}
+        print(f"Running linters for {tested_os}_{tested_arch}...")
+        results_per_os_x_arch[f"{tested_os}_{tested_arch}"] = ctx.run(command, env=env, warn=True, hide=not show_output).stdout
     merged_results = merge_results(results_per_os_x_arch)
 
     # Filter the results by filtering with filter_team and filter_linters.
