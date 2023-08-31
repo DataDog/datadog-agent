6b23facb1689dce3038d04d595e5d82effbbd52e
Julien Lebot <julien.lebot@datadoghq.com>
2023-08-31 21:48:31 +0200

    Also inspect System-Probe

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -397,6 +397,7 @@ if windows?
   inspect_binary("#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\agent.exe", &raise_if_forbidden_symbol_found)
   inspect_binary("#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\trace-agent.exe", &raise_if_forbidden_symbol_found)
   inspect_binary("#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\process-agent.exe", &raise_if_forbidden_symbol_found)
+  inspect_binary("#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\agent\\system-probe.exe", &raise_if_forbidden_symbol_found)
 
   #
   # For Windows build, files need to be stripped must be specified here.
