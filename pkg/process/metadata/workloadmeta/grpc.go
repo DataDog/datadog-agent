1d232bc8ebc37ff1cfd12c0c93a0c60c3f526917
Corrina Sivak <corrina.sivak@datadoghq.com>
2023-08-31 15:10:29 -0400

    Add warning for if the stream is closed because of an unexpected error

--- pkg/process/metadata/workloadmeta/grpc.go
+++ pkg/process/metadata/workloadmeta/grpc.go
@@ -172,6 +172,10 @@ func (l *GRPCServer) StreamEntities(_ *pbgo.ProcessStreamEntitiesRequest, out pb
 			}
 
 		case <-out.Context().Done():
+			err := out.Context().Err()
+			if err != nil {
+				log.Warn("The workloadmeta grpc stream was closed: %v", err)
+			}
 			return nil
 		case <-streamCtx.Done():
 			return DuplicateConnectionErr
