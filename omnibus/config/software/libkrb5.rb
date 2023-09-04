d8d9c0d00fdf6dc8a79bec77a1d99578416a506d
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-09-04 16:24:30 +0200

    omnibus: libkrb5: use autotools wrapper

--- omnibus/config/software/libkrb5.rb
+++ omnibus/config/software/libkrb5.rb
@@ -1,3 +1,5 @@
+require './lib/autotools.rb'
+
 name "libkrb5"
 default_version "1.20.1"
 
@@ -21,20 +23,13 @@ build do
   license "BSD-style"
   license_file "https://raw.githubusercontent.com/krb5/krb5/master/NOTICE"
 
-  cmd = ["./configure",
-         "--without-keyutils", # this would require additional deps/system deps, disable it
-         "--without-system-verto", # do not prefer libverto from the system, if installed
-         "--without-libedit", # we don't want to link with libraries outside of the install dir
-         "--disable-static",
-         "--prefix=#{install_dir}/embedded"].join(" ")
-  env = {
-    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-    "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
-  }
-  command cmd, :env => env
-  command "make -j #{workers}", :env => { "LD_RUN_PATH" => "#{install_dir}/embedded/lib" }
-  command "make install", :env => { "LD_RUN_PATH" => "#{install_dir}/embedded/lib" }
+  build_with_autotools({
+    configure_opts: [
+      "--without-keyutils", # this would require additional deps/system deps, disable it
+      "--without-system-verto", # do not prefer libverto from the system, if installed
+      "--without-libedit", # we don't want to link with libraries outside of the install dir
+    ]
+  })
 
   # FIXME: CONDA libs appear to confuse the health checker - manually checked file
   # are properly linked. Must whitelist for build to succeed.
