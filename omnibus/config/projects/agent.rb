48488c12ae930402f3d726d9218260a767e639c8
Julien Lebot <julien.lebot@datadoghq.com>
2023-09-05 12:43:41 +0200

    Strip the system-probe binary on Windows (#19200)

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -391,6 +391,7 @@ if windows?
   windows_symbol_stripping_file "#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\agent\\process-agent.exe"
   windows_symbol_stripping_file "#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\agent\\trace-agent.exe"
   windows_symbol_stripping_file "#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\agent\\agent.exe"
+  windows_symbol_stripping_file "#{Omnibus::Config.source_dir()}\\datadog-agent\\src\\github.com\\DataDog\\datadog-agent\\bin\\agent\\system-probe.exe"
 end
 
 if linux? or windows?
