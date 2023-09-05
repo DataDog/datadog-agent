233ac892c9c52c9d8352fe8ee58f4efab347f494
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2023-09-05 15:45:17 +0200

    [CSPM] Revive lint of pkg/compliance

--- pkg/compliance/tools/k8s_types_generator/main.go
+++ pkg/compliance/tools/k8s_types_generator/main.go
@@ -3,6 +3,9 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package main is the entrypoint of the compliance k8s_types_generator tool
+// that is responsible for generating various configuration types of
+// Kubernetes components.
 package main
 
 import (
@@ -141,6 +144,7 @@ const preamble = `// Unless explicitly stated otherwise all files in this reposi
 // !!!
 // This is a generated file: regenerate with go run ./pkg/compliance/tools/k8s_types_generator.go
 // !!!
+//revive:disable
 package k8sconfig
 
 import (
@@ -391,9 +395,8 @@ func printKomponentCode(komp *komponent) string {
 		case "*K8sConfigFileMeta":
 			if komp.name == "kubelet" && c.flagName == "config" {
 				return fmt.Sprintf("res.%s = l.loadKubeletConfigFileMeta(%s)", toGoField(c.flagName), v)
-			} else {
-				return fmt.Sprintf("res.%s = l.loadConfigFileMeta(%s)", toGoField(c.flagName), v)
 			}
+			return fmt.Sprintf("res.%s = l.loadConfigFileMeta(%s)", toGoField(c.flagName), v)
 		case "*K8sKubeletConfigFileMeta":
 			return fmt.Sprintf("res.%s = l.loadKubeletConfigFileMeta(%s)", toGoField(c.flagName), v)
 		case "*K8sAdmissionConfigFileMeta":
@@ -445,11 +448,11 @@ func downloadEtcdAndExtractFlags(componentVersion string) *komponent {
 	const componentName = "etcd"
 	componentBin := path.Join(bindir, fmt.Sprintf("%s-%s", componentName, componentVersion))
 	componentTar := path.Join(bindir, fmt.Sprintf("%s-%s.tar.gz", componentName, componentVersion))
-	componentUrl := fmt.Sprintf("https://github.com/etcd-io/etcd/releases/download/%s/etcd-%s-linux-%s.tar.gz",
+	componentURL := fmt.Sprintf("https://github.com/etcd-io/etcd/releases/download/%s/etcd-%s-linux-%s.tar.gz",
 		componentVersion, componentVersion, arch)
 	if _, err := os.Stat(componentBin); os.IsNotExist(err) {
-		fmt.Fprintf(os.Stderr, "downloading %s into %s...", componentUrl, componentBin)
-		if err := download(componentUrl, componentTar); err != nil {
+		fmt.Fprintf(os.Stderr, "downloading %s into %s...", componentURL, componentBin)
+		if err := download(componentURL, componentTar); err != nil {
 			log.Fatal(err)
 		}
 		t, err := os.Open(componentTar)
@@ -514,11 +517,11 @@ func downloadEtcdAndExtractFlags(componentVersion string) *komponent {
 
 func downloadKubeComponentAndExtractFlags(componentName, componentVersion string) *komponent {
 	componentBin := path.Join(bindir, fmt.Sprintf("%s-%s", componentName, componentVersion))
-	componentUrl := fmt.Sprintf("https://dl.k8s.io/%s/bin/linux/%s/%s",
+	componentURL := fmt.Sprintf("https://dl.k8s.io/%s/bin/linux/%s/%s",
 		componentVersion, arch, componentName)
 	if _, err := os.Stat(componentBin); os.IsNotExist(err) {
-		fmt.Fprintf(os.Stderr, "downloading %s into %s...", componentUrl, componentBin)
-		if err := download(componentUrl, componentBin); err != nil {
+		fmt.Fprintf(os.Stderr, "downloading %s into %s...", componentURL, componentBin)
+		if err := download(componentURL, componentBin); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Fprintf(os.Stderr, "ok\n")
