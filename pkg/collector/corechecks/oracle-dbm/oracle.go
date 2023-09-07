4337f172f3abccab60f482a4f11b9927f96b4c53
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2023-09-07 17:20:44 +0200

    constants

--- pkg/collector/corechecks/oracle-dbm/oracle.go
+++ pkg/collector/corechecks/oracle-dbm/oracle.go
@@ -33,6 +33,18 @@ var MAX_OPEN_CONNECTIONS = 10
 var DEFAULT_SQL_TRACED_RUNS = 10
 var DB_TIMEOUT = "20000"
 
+const (
+	// SQL_FULLTEXT size in V$SQL
+	MaxSQLFullTextVSQL = 4000
+
+	/*
+	 * SQL_FULLTEXT size in V$SQLSTATS. The column is defined as VARCHAR2(4000)
+	 * but due to the Oracle 27760729 : V$SQLSTAT.SQL_FULLTEXT DOES NOT SHOW COMPLETE SQL STMT
+	 * it contains only the first 1000 characters
+	 */
+	MaxSQLFulltextVSQLStats = 1000
+)
+
 // The structure is filled by activity sampling and serves as a filter for query metrics
 type StatementsFilter struct {
 	SQLIDs                  map[string]int
