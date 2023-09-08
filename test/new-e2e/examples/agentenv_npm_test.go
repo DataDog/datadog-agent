8d2b5d12e0fee845544f467bc989a5056fc9cfc3
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-09-08 17:13:28 +0200

    test/new-e2e/examples/agentenv_npm_test.go

--- test/new-e2e/examples/agentenv_npm_test.go
+++ test/new-e2e/examples/agentenv_npm_test.go
@@ -11,29 +11,41 @@ import (
 	"time"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e"
+	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
 	"github.com/cenkalti/backoff/v4"
 	"github.com/stretchr/testify/require"
 )
+
+const NPMsystemProbeConfig = `
+network_config:
+  enabled: true
+`
 
 type vmSuiteEx6 struct {
 	e2e.Suite[e2e.FakeIntakeEnv]
 }
 
 func TestVMSuiteEx6(t *testing.T) {
-	e2e.Run(t, &vmSuiteEx6{}, e2e.FakeIntakeStackDef(nil))
+	e2e.Run(t, &vmSuiteEx6{}, e2e.FakeIntakeStackDef(nil, agentparams.WithSystemProbeConfig(NPMsystemProbeConfig)))
 }
 
-func (v *vmSuiteEx6) Test1_FakeIntakeReceivesMetrics() {
+func (v *vmSuiteEx6) Test1_FakeIntakeNPM() {
 	t := v.T()
+
 	err := backoff.Retry(func() error {
-		metricNames, err := v.Env().Fakeintake.GetConnections()
+
+		v.Env().VM.Execute("curl http://httpbin.org/anything")
+
+		hostnameNetID, err := v.Env().Fakeintake.GetConnectionsNames()
 		if err != nil {
 			return err
 		}
-		if len(metricNames) == 0 {
-			return errors.New("no metrics yet")
+		if len(hostnameNetID) == 0 {
+			return errors.New("no connections yet")
 		}
+
+		t.Logf("hostname+networkID %v seen connections", hostnameNetID)
 		return nil
-	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(500*time.Millisecond), 60))
+	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(time.Second), 60))
 	require.NoError(t, err)
 }
