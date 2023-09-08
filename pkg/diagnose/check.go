ebedabd67bbdaa786dc3eae706d6047550aebe8b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2023-09-08 03:05:12 +0200

    [comp] workloadmeta: adopt new component for pkg/diagnose

--- pkg/diagnose/check.go
+++ pkg/diagnose/check.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/DataDog/datadog-agent/cmd/agent/common"
 	"github.com/DataDog/datadog-agent/comp/aggregator/diagnosesendermanager"
+	"github.com/DataDog/datadog-agent/comp/core/workloadmeta"
 	"github.com/DataDog/datadog-agent/pkg/aggregator/sender"
 	"github.com/DataDog/datadog-agent/pkg/collector"
 	"github.com/DataDog/datadog-agent/pkg/collector/check"
@@ -98,7 +99,8 @@ func diagnoseChecksInCLIProcess(diagCfg diagnosis.Config, senderManager diagnose
 	}
 
 	// Initializing the aggregator with a flush interval of 0 (to disable the flush goroutines)
-	common.LoadComponents(context.Background(), senderManagerInstance, pkgconfig.Datadog.GetString("confd_path"))
+	wmeta := workloadmeta.GetGlobalStore()
+	common.LoadComponents(context.Background(), senderManagerInstance, wmeta, pkgconfig.Datadog.GetString("confd_path"))
 	common.AC.LoadAndRun(context.Background())
 
 	// Create the CheckScheduler, but do not attach it to
