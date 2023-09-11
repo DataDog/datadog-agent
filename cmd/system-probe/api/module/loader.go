337d140cb93db9c61bd82fdf18bd2c83201ac784
Tristan Blake <tristan.blake@datadoghq.com>
2023-09-11 10:25:30 -0400

    register liveness in network tracer module

--- cmd/system-probe/api/module/loader.go
+++ cmd/system-probe/api/module/loader.go
@@ -18,7 +18,6 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/DataDog/datadog-agent/cmd/system-probe/config"
-	"github.com/DataDog/datadog-agent/pkg/status/health"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
@@ -204,18 +203,11 @@ func updateStats() {
 	then := time.Now()
 	now := time.Now()
 	ticker := time.NewTicker(15 * time.Second)
-	health := health.RegisterLiveness("system-probe")
-
-	log.Info("system probe: health check initialized successfully")
 
 	for {
 		l.Lock()
 		if l.closed {
 			l.Unlock()
-			err := health.Deregister()
-			if err != nil {
-				log.Warnf("error de-registering health check: %s", err)
-			}
 			return
 		}
 
@@ -232,7 +224,6 @@ func updateStats() {
 		l.stats["uptime"] = now.Sub(start).String()
 		l.Unlock()
 
-		<-health.C
 		then = now
 		now = <-ticker.C
 	}
