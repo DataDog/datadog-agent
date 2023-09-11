7227719a8dfb646729f4cb22f0c5c78b44f94750
Josh Lineaweaver <josh.lineaweaver@datadoghq.com>
2023-09-11 14:35:01 -0600

    Add package comment for stub

--- pkg/collector/corechecks/orchestrator/pod/stub.go
+++ pkg/collector/corechecks/orchestrator/pod/stub.go
@@ -3,4 +3,5 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package pod is used for the orchestrator pod check
 package pod
