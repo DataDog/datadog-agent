924d2c22eae3bbd07997f3f80daff708101f0c6f
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-09-11 17:54:50 +0200

    add comments

--- pkg/security/utils/lru_interner.go
+++ pkg/security/utils/lru_interner.go
@@ -11,11 +11,14 @@ import (
 	"github.com/hashicorp/golang-lru/v2/simplelru"
 )
 
+// LRUStringInterner is a best-effort LRU-based string deduplicator
 type LRUStringInterner struct {
 	sync.Mutex
 	store *simplelru.LRU[string, string]
 }
 
+// NewLRUStringInterner returns a new LRUStringInterner, with the cache size provided
+// if the cache size is negative this function will panic
 func NewLRUStringInterner(size int) *LRUStringInterner {
 	store, err := simplelru.NewLRU[string, string](size, nil)
 	if err != nil {
@@ -27,6 +30,7 @@ func NewLRUStringInterner(size int) *LRUStringInterner {
 	}
 }
 
+// Deduplicate returns a possibly de-duplicated string
 func (si *LRUStringInterner) Deduplicate(value string) string {
 	si.Lock()
 	defer si.Unlock()
@@ -43,6 +47,7 @@ func (si *LRUStringInterner) deduplicateUnsafe(value string) string {
 	return value
 }
 
+// DeduplicateSlice returns a possibly de-duplicated string slice
 func (si *LRUStringInterner) DeduplicateSlice(values []string) {
 	si.Lock()
 	defer si.Unlock()
