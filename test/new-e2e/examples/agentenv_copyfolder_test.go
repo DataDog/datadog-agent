f3f3cfc1859d5a33bf0e1dc4cbbe7e1b5c423097
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-09-11 16:46:53 +0200

    Edit copy example to match ex 6

--- test/new-e2e/examples/agentenv_copyfolder_test.go
+++ test/new-e2e/examples/agentenv_copyfolder_test.go
@@ -6,23 +6,27 @@
 package examples
 
 import (
+	"os"
 	"testing"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e"
+	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/vm/ec2os"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/vm/ec2params"
 	"github.com/stretchr/testify/require"
 )
 
-type vmSuiteCopy struct {
-	e2e.Suite[e2e.VMEnv]
+type agentSuiteEx6 struct {
+	e2e.Suite[e2e.AgentEnv]
 }
 
-func TestVMSuiteCopy(t *testing.T) {
-	e2e.Run(t, &vmSuiteCopy{}, e2e.EC2VMStackDef(ec2params.WithOS(ec2os.UbuntuOS)))
+func TestAgentSuiteEx6(t *testing.T) {
+	file, err := os.ReadFile("test-folder/file-0")
+	require.NoError(t, err)
+	e2e.Run(t, &agentSuiteEx6{}, e2e.AgentStackDef([]ec2params.Option{ec2params.WithOS(ec2os.UbuntuOS)}, agentparams.WithFile("/home/ubuntu/testfilewithfile", string(file), false)))
 }
 
-func (v *vmSuiteCopy) TestCopy() {
+func (v *agentSuiteEx6) TestCopy() {
 
 	v.Env().VM.CopyFolder("test-folder", "test")
 	v.Env().VM.CopyFile("test-folder/file-0", "copied-file")
@@ -30,8 +34,10 @@ func (v *vmSuiteCopy) TestCopy() {
 	output0 := v.Env().VM.Execute("cat test/file-0")
 	output1 := v.Env().VM.Execute("cat test/folder-1/file-1")
 	output2 := v.Env().VM.Execute("cat copied-file")
+	output3 := v.Env().VM.Execute("cat testfilewithfile")
 
 	require.Equal(v.T(), "This is a test file 0\n", output0)
 	require.Equal(v.T(), "This is a test file 1\n", output1)
 	require.Equal(v.T(), "This is a test file 0\n", output2)
+	require.Equal(v.T(), "This is a test file 0\n", output3)
 }
