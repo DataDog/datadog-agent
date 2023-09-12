1fa14694c42227fdedb9bf5b22a6fe51f768127e
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-09-12 12:06:50 +0200

    Last fix to example 6

--- test/new-e2e/examples/agentenv_copyfolder_test.go
+++ test/new-e2e/examples/agentenv_copyfolder_test.go
@@ -29,27 +29,28 @@ func TestAgentSuiteEx6(t *testing.T) {
 func (v *agentSuiteEx6) TestCopy() {
 
 	testFolder := path.Join(os.TempDir(), "test-folder")
-	err := createFolders(testFolder)
+	err := createFoldersEx6(testFolder)
 	require.NoError(v.T(), err)
 
-	file, err := os.ReadFile(path.Join(testFolder, "file-0"))
+	file, err := os.ReadFile(path.Join(testFolder, "hosts"))
 	require.NoError(v.T(), err)
-	v.UpdateEnv(e2e.AgentStackDef([]ec2params.Option{ec2params.WithOS(ec2os.UbuntuOS)}, agentparams.WithFile("/home/ubuntu/testfilewithfile", string(file), false)))
+	v.UpdateEnv(e2e.AgentStackDef([]ec2params.Option{ec2params.WithOS(ec2os.UbuntuOS)}, agentparams.WithFile("/etc/hosts", string(file), true)))
+
 	v.Env().VM.CopyFolder(path.Join(testFolder), "test")
 	v.Env().VM.CopyFile(path.Join(testFolder, "file-0"), "copied-file")
 
 	output0 := v.Env().VM.Execute("cat test/file-0")
 	output1 := v.Env().VM.Execute("cat test/folder-1/file-1")
 	output2 := v.Env().VM.Execute("cat copied-file")
-	output3 := v.Env().VM.Execute("cat testfilewithfile")
+	output3 := v.Env().VM.Execute("cat /etc/hosts")
 
 	require.Equal(v.T(), "This is a test file 0", output0)
 	require.Equal(v.T(), "This is a test file 1", output1)
 	require.Equal(v.T(), "This is a test file 0", output2)
-	require.Equal(v.T(), "This is a test file 0", output3)
+	require.Equal(v.T(), "127.0.0.1			localhost\n127.0.0.1           agentvm\n", output3)
 }
 
-func createFolders(folder string) error {
+func createFoldersEx6(folder string) error {
 
 	err := os.MkdirAll(folder, 0755)
 	if err != nil {
@@ -65,6 +66,11 @@ func createFolders(folder string) error {
 		return err
 	}
 	err = os.WriteFile(path.Join(folder, "folder-1", "file-1"), []byte("This is a test file 1"), 0655)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(path.Join(folder, "hosts"), []byte("127.0.0.1			localhost\n127.0.0.1           agentvm\n"), 0655)
 	if err != nil {
 		return err
 	}
