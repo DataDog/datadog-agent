dd650e74c2f697099b2342c24d2bca8dc457fd3f
yoon nguyen <duong.nguyen@datadoghq.com>
2023-09-12 13:41:47 +0200

    update

--- test/new-e2e/log-agent/file_tailing_test.go
+++ test/new-e2e/log-agent/file_tailing_test.go
@@ -11,15 +11,10 @@ import (
 
 	fi "github.com/DataDog/datadog-agent/test/fakeintake/client"
 	"github.com/DataDog/datadog-agent/test/new-e2e/utils/e2e"
-	"github.com/DataDog/datadog-agent/test/new-e2e/utils/e2e/client"
 	"github.com/DataDog/datadog-agent/test/new-e2e/utils/e2e/params"
-	"github.com/DataDog/test-infra-definitions/components/datadog/agent"
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
-	"github.com/DataDog/test-infra-definitions/scenarios/aws"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/vm/ec2params"
-	"github.com/DataDog/test-infra-definitions/scenarios/aws/vm/ec2vm"
 	"github.com/cenkalti/backoff/v4"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/stretchr/testify/require"
 )
 
@@ -37,40 +32,8 @@ func logsExampleStackDef(vmParams []ec2params.Option, agentParams ...agentparams
     service: hello
     source: custom_log
 `
-	return e2e.EnvFactoryStackDef(
-		// This function creates the environment, including the virtual machine and agent installer
-		func(ctx *pulumi.Context) (*e2e.FakeIntakeEnv, error) {
-			// Create a new EC2 virtual machine, default Ubuntu OS ( WithOS(os.UbuntuOS, os.AMD64Arch)
-			vm, err := ec2vm.NewEc2VM(ctx, vmParams...)
-			if err != nil {
-				return nil, err
-			}
+	return e2e.FakeIntakeStackDef(nil, agentparams.WithLogs(), agentparams.WithIntegration("custom_logs.d", config))
 
-			// Create a new fake intake exporter.
-			fakeintakeExporter, err := aws.NewEcsFakeintake(vm.GetAwsEnvironment())
-			if err != nil {
-				return nil, err
-			}
-
-			// Define agent parameters, including integration with custom_logs logs.
-			agentParams = append(agentParams, agentparams.WithFakeintake(fakeintakeExporter))
-			agentParams = append(agentParams, agentparams.WithLogs())
-			agentParams = append(agentParams, agentparams.WithIntegration("custom_logs.d", config))
-
-			// Create a new agent installer.
-			installer, err := agent.NewInstaller(vm, agentParams...)
-			if err != nil {
-				return nil, err
-			}
-
-			// Return the environment setup.
-			return &e2e.FakeIntakeEnv{
-				VM:         client.NewVM(vm),
-				Agent:      client.NewAgent(installer),
-				Fakeintake: client.NewFakeintake(fakeintakeExporter),
-			}, nil
-		},
-	)
 }
 
 // TestE2EVMFakeintakeSuite runs the end-to-end test suite for the log agent with virtual machine and fake intake.
@@ -78,20 +41,18 @@ func TestE2EVMFakeintakeSuite(t *testing.T) {
 	e2e.Run(t, &vmFakeintakeSuite{}, logsExampleStackDef(nil), params.WithDevMode())
 }
 
-// TestLogs executes a three-part test of the log agent's interaction with the fake intake system.
-func (s *vmFakeintakeSuite) TestLogs() {
+func (s *vmFakeintakeSuite) TestLogCollection() {
 	t := s.T()
 	fakeintake := s.Env().Fakeintake
+	fakeintake.FlushServerAndResetAggregators()
+	s.cleanUp()
+	s.Env().VM.Execute("sudo touch /var/log/hello-world.log")
 
-	// part 1: no logs
+	// part 1: check for no logs
 	// This part verifies that no logs are received initially.
 	err := backoff.Retry(
 
 		func() error {
-			s.Env().VM.ExecuteWithError("sudo rm -f /var/log/hello-world.log")
-			s.Env().VM.ExecuteWithError("sudo rm -f /var/log/hello-world.log.old")
-			s.Env().VM.ExecuteWithError("sudo touch /var/log/hello-world.log")
-
 			logs, err := fakeintake.FilterLogs("hello")
 			if err != nil {
 				return err
@@ -102,20 +63,14 @@ func (s *vmFakeintakeSuite) TestLogs() {
 				return errors.New(lm)
 			}
 			return nil
-		}, backoff.WithMaxRetries(backoff.NewConstantBackOff(1*time.Second), 120))
+		}, backoff.WithMaxRetries(backoff.NewConstantBackOff(1*time.Second), 100))
 	require.NoError(t, err)
 
 	// part 2: generate logs
 	// This part generates a test log and verifies that it was created successfully.
 
-	var str string
-	for i := 0; i < 100; i++ {
-		str += "hello-world"
-	}
-
 	s.Env().VM.Execute("sudo chmod 777 /var/log/hello-world.log")
-	generateLog := fmt.Sprintf("echo %s > /var/log/hello-world.log", str)
-	_, err = s.Env().VM.ExecuteWithError(generateLog)
+	_, err = s.Env().VM.ExecuteWithError(generateLog("hello-world"))
 	require.NoError(t, err)
 
 	// part 3: there should be logs
@@ -153,80 +108,105 @@ func (s *vmFakeintakeSuite) TestLogs() {
 			return fmt.Errorf("received %v logs with 'hello-world', expecting 1", len(logs))
 		}
 
-		// Part 4: Block permission and check the Agent status
-		_, err = s.Env().VM.ExecuteWithError("sudo chmod 000 /var/log/hello-world.log")
-		require.NoError(t, err)
+		return nil
+	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(1*time.Second), 120))
+	require.NoError(t, err)
+}
 
-		// Sleep 10s to make sure agent status for the log file is updated
-		time.Sleep(10 * time.Second)
+func (s *vmFakeintakeSuite) TestLogPermission() {
+	t := s.T()
 
+	// Part 4: Block permission and check the Agent status
+	s.Env().VM.Execute("sudo chmod 000 /var/log/hello-world.log")
+	// require.NoError(t, err)
+
+	erro := backoff.Retry(func() error {
 		// Check the Agent status
-		statusOutput, err := s.Env().VM.ExecuteWithError("datadog-agent status | grep -A 10 'custom_logs'")
-		if err != nil || !strings.Contains(statusOutput, "Status: OK") {
-			if strings.Contains(statusOutput, "permission denied") {
-				t.Log("Log file is inaccessible due to permission issues.")
-			} else {
-				t.Log("Log file is inaccessible.")
-			}
+
+		statusOutput := s.Env().VM.Execute("sudo datadog-agent status | grep -A 10 'custom_logs'")
+
+		// Check if the status indicates that the log file is accessible
+		if strings.Contains(statusOutput, "Status: OK") {
+			t.Logf("Log file is unexpectedly accessible.")
+			return errors.New("log file is unexpectedly accessible")
+		} else if strings.Contains(statusOutput, "permission denied") {
+			// This is the expected behavior when the log file is inaccessible
+			t.Logf("Log file correctly inaccessible.")
 		} else {
-			t.Error("Expected the log file to be inaccessible, but agent status says log status is OK.")
+			// If the status is neither "OK" nor "permission denied", log the unexpected status for debugging
+			t.Logf("Unexpected agent status: \n%s", statusOutput)
+			return errors.New("unexpected agent status")
 		}
 
 		// Part 5: Restore permissions
-		_, err = s.Env().VM.ExecuteWithError("sudo chmod 777 /var/log/hello-world.log")
-		require.NoError(t, err)
+		s.Env().VM.Execute("sudo chmod 777 /var/log/hello-world.log")
 
 		// Part 6: Stop the agent, generate new logs, start the agent
-		_, err = s.Env().VM.ExecuteWithError("sudo service datadog-agent stop")
-		require.NoError(t, err)
-		time.Sleep(5 * time.Second)
+		s.Env().VM.Execute("sudo service datadog-agent stop")
 
-		_, err = s.Env().VM.ExecuteWithError(generateLog) // Appending logs this time.
-		require.NoError(t, err)
+		s.Env().VM.Execute(generateLog("hello-world")) // Appending logs this time.
 
-		_, err = s.Env().VM.ExecuteWithError("sudo service datadog-agent start")
-		require.NoError(t, err)
+		s.Env().VM.Execute("sudo service datadog-agent start")
 
-		// Part 7: Rotate the log file and check if the agent is tailing the new log file.
-
-		// Rotate the log file
-		_, err = s.Env().VM.ExecuteWithError("sudo mv /var/log/hello-world.log /var/log/hello-world.log.old && sudo touch /var/log/hello-world.log")
-		require.NoError(t, err)
-
-		// Verify the old log file's existence after rotation
-		_, err = s.Env().VM.ExecuteWithError("ls /var/log/hello-world.log.old")
-		require.NoError(t, err, "Old log file missing after rotation")
-
-		// Grant new log file permission
-		_, err = s.Env().VM.ExecuteWithError("sudo chmod 777 /var/log/hello-world.log")
-		require.NoError(t, err)
-
-		// Check if agent is tailing new log file via agent status
-		time.Sleep(10 * time.Second) // Making sure agent has time to start
-		newStatusOutput, err := s.Env().VM.ExecuteWithError("datadog-agent status | grep -A 10 'custom_logs'")
-		require.NoError(t, err)
-		if !strings.Contains(newStatusOutput, "Path: /var/log/hello-world.log") {
-			t.Error("The agent is not tailing the expected log file.")
+		// Check the Agent status
+		statusOutput = s.Env().VM.Execute("sudo datadog-agent status | grep -A 10 'custom_logs'")
+		if strings.Contains(statusOutput, "Status: OK") {
+			if strings.Contains(statusOutput, "permission denied") {
+				t.Errorf("Expecting log file to be accessible, received: %s", statusOutput)
+			}
+		} else {
+			t.Log("Agent is collecting logs as expected")
 		}
-
-		// Generate new log
-		newLogContent := "hello-world-new-content"
-		generateNewLog := fmt.Sprintf("echo %s > /var/log/hello-world.log", newLogContent)
-		_, err = s.Env().VM.ExecuteWithError(generateNewLog)
-		require.NoError(t, err)
-
-		// Verify log file's content
-		logs, err = fakeintake.FilterLogs("hello", fi.WithMessageContaining(newLogContent))
-		if err != nil {
-			return err
-		}
-		if len(logs) != 1 {
-			return fmt.Errorf("received %v logs with 'hello-world-new-content', expecting 1", len(logs))
-		}
-		require.NoError(t, err)
-
 		return nil
-	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(1*time.Second), 60))
+	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(1*time.Microsecond), 120))
+	require.NoError(t, erro)
+}
 
+func (s *vmFakeintakeSuite) TestLogRotation() {
+	t := s.T()
+	fakeintake := s.Env().Fakeintake
+
+	// Part 7: Rotate the log file and check if the agent is tailing the new log file.
+
+	// Rotate the log file
+	_, err := s.Env().VM.ExecuteWithError("sudo mv /var/log/hello-world.log /var/log/hello-world.log.old && sudo touch /var/log/hello-world.log")
 	require.NoError(t, err)
+
+	// Verify the old log file's existence after rotation
+	_, err = s.Env().VM.ExecuteWithError("ls /var/log/hello-world.log.old")
+	require.NoError(t, err, "Old log file missing after rotation")
+
+	// Grant new log file permission
+	_, err = s.Env().VM.ExecuteWithError("sudo chmod 777 /var/log/hello-world.log")
+	require.NoError(t, err)
+
+	// Check if agent is tailing new log file via agent status
+	time.Sleep(3000 * time.Millisecond)
+	newStatusOutput, err := s.Env().VM.ExecuteWithError("sudo datadog-agent status | grep -A 10 'custom_logs'")
+	require.NoError(t, err)
+	if !strings.Contains(newStatusOutput, "Path: /var/log/hello-world.log") {
+		t.Error("The agent is not tailing the expected log file.")
+	}
+
+	// Verify log file's content
+	s.Env().VM.Execute(generateLog("hello-world-new-content"))
+	time.Sleep(10000 * time.Millisecond)
+
+	logs, err := fakeintake.FilterLogs("hello", fi.WithMessageContaining("hello-world-new-content"))
+	require.NoError(t, err)
+	require.NotEqual(t, 0, len(logs), "received %v logs with content: %s, expecting 1 ", len(logs), logs)
+
+	//clean up
+	s.cleanUp()
+}
+
+func generateLog(content string) string {
+	return fmt.Sprintf("echo %s > /var/log/hello-world.log", strings.Repeat(content, 10))
+}
+
+func (s *vmFakeintakeSuite) cleanUp() {
+	s.Env().VM.Execute("sudo rm -f /var/log/hello-world.log")
+	s.Env().VM.Execute("sudo rm -f /var/log/hello-world.log.old")
+	time.Sleep(3000 * time.Millisecond)
+	s.T().Logf("Cleaning up log files")
 }
