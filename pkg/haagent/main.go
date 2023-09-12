5be192c14a8c9db2ffd60797fff87c3a10351462
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-09-12 02:02:35 +0200

    use hostname

--- pkg/haagent/main.go
+++ pkg/haagent/main.go
@@ -2,9 +2,12 @@ package haagent
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
+	coreconfig "github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/util/hostname"
 	"net/http"
 	"os"
 	"os/signal"
@@ -106,6 +109,67 @@ func join(joinAddr, raftAddr, nodeID string) error {
 	return nil
 }
 
+type HaAgentConfig struct {
+	NodeName string `mapstructure:"node_name"` // TODO: use agent hostname instead?
+	HttpAddr string `mapstructure:"http_addr"`
+	RaftAddr string `mapstructure:"raft_addr"`
+	JoinAddr string `mapstructure:"join_addr"`
+}
+
 func StartRaft() {
-	log.Infof("Start Raft")
+	log.Infof("[HA Agent] Start Raft")
+	var haAgentConfig HaAgentConfig
+	if err := coreconfig.Datadog.UnmarshalKey("ha_agent", &haAgentConfig); err != nil {
+		log.Errorf("unmarshall config: %s", err)
+		return
+	}
+	log.Infof("[HA Agent] Config: %+v", haAgentConfig)
+
+	agentHostname, err := hostname.Get(context.TODO())
+	if err != nil {
+		log.Errorf("Error getting the hostname: %v", err)
+		return
+	}
+	log.Infof("[HA Agent] hostname: %+v", agentHostname)
+
+	// Ensure Raft storage exists.
+	//raftDir := flag.Arg(0)
+	//if raftDir == "" {
+	//	log.Errorf("No Raft storage directory specified")
+	//	return
+	//}
+	//if err := os.MkdirAll(raftDir, 0700); err != nil {
+	//	log.Errorf("failed to create path for Raft storage: %s", err.Error())
+	//	return
+	//}
+
+	//s := store.New(inmem)
+	//s.RaftDir = raftDir
+	//s.RaftBind = raftAddr
+	//if err := s.Open(joinAddr == "", nodeID); err != nil {
+	//	log.Errorf("failed to open store: %s", err.Error())
+	//	return
+	//}
+	//
+	//h := httpd.New(httpAddr, s)
+	//if err := h.Start(); err != nil {
+	//	log.Errorf("failed to start HTTP service: %s", err.Error())
+	//	return
+	//}
+	//
+	//// If join was specified, make the join request.
+	//if joinAddr != "" {
+	//	if err := join(joinAddr, raftAddr, nodeID); err != nil {
+	//		log.Errorf("failed to join node at %s: %s", joinAddr, err.Error())
+	//		return
+	//	}
+	//}
+	//
+	//// We're up and running!
+	//log.Infof("hraftd started successfully, listening on http://%s", httpAddr)
+	//
+	//terminate := make(chan os.Signal, 1)
+	//signal.Notify(terminate, os.Interrupt)
+	//<-terminate
+	//log.Infof("hraftd exiting")
 }
