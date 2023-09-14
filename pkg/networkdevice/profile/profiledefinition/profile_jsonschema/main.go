ec16f1a459506143be6438ce7eef2f2a200330de
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-09-14 17:12:42 +0200

    generate

--- pkg/networkdevice/profile/profiledefinition/profile_jsonschema/main.go
+++ pkg/networkdevice/profile/profiledefinition/profile_jsonschema/main.go
@@ -9,41 +9,15 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"github.com/DataDog/datadog-agent/pkg/networkdevice/profile/profiledefinition"
-	"github.com/invopop/jsonschema"
 	"os"
-	"strings"
+
+	"github.com/invopop/jsonschema"
+
+	"github.com/DataDog/datadog-agent/pkg/networkdevice/profile/profiledefinition"
 )
 
-// import (
-//
-//	"encoding/json"
-//	"flag"
-//	"github.com/DataDog/datadog-agent/pkg/security/serializers"
-//	"github.com/DataDog/datadog-agent/pkg/security/utils"
-//	"os"
-//	"reflect"
-//	"strings"
-//	"time"
-//
-//	"github.com/invopop/jsonschema"
-//
-// )
 func generateBackendJSON(output string) error {
-	reflector := jsonschema.Reflector{
-		ExpandedStruct: true,
-		DoNotReference: false,
-		//Mapper:         jsonTypeMapper,
-		//Namer: jsonTypeNamer,
-	}
-
-	if err := reflector.AddGoComments("github.com/DataDog/datadog-agent/pkg/networkdevice/profile/profiledefinition", "./"); err != nil {
-		return err
-	}
-	reflector.CommentMap = cleanupEasyjson(reflector.CommentMap)
-
-	schema := reflector.Reflect(&profiledefinition.DeviceProfileRcConfig{})
-
+	schema := jsonschema.Reflect(&profiledefinition.DeviceProfileRcConfig{})
 	schemaJSON, err := json.MarshalIndent(schema, "", "  ")
 	if err != nil {
 		return err
@@ -52,35 +26,6 @@ func generateBackendJSON(output string) error {
 	return os.WriteFile(output, schemaJSON, 0664)
 }
 
-//func jsonTypeMapper(ty reflect.Type) *jsonschema.Schema {
-//	if ty == reflect.TypeOf(utils.EasyjsonTime{}) {
-//		schema := jsonschema.Reflect(time.Time{})
-//		schema.Version = ""
-//		return schema
-//	}
-//	return nil
-//}
-
-func cleanupEasyjson(commentMap map[string]string) map[string]string {
-	res := make(map[string]string, len(commentMap))
-	for name, comment := range commentMap {
-		cleaned := strings.TrimSpace(comment)
-		cleaned = strings.TrimSuffix(cleaned, "easyjson:json")
-		res[name] = strings.TrimSpace(cleaned)
-	}
-	return res
-}
-
-//	func jsonTypeNamer(ty reflect.Type) string {
-//		const selinuxPrefix = "selinux"
-//
-//		base := strings.TrimSuffix(ty.Name(), "Serializer")
-//		if strings.HasPrefix(base, selinuxPrefix) {
-//			return "SELinux" + strings.TrimPrefix(base, selinuxPrefix)
-//		}
-//
-//		return base
-//	}
 func main() {
 	var (
 		output string
