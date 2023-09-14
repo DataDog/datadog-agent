a2e64c6dde1a88cb3ad69c5b88b94d4756e6750b
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2023-09-14 10:22:04 +0200

    [Oracle] Add network disruption handling (#19432)
    * add refused connection handling
    
    * relnote
    
    * linter
    
    * Update releasenotes/notes/oracle-handling-refused-connection-8606661daa27952c.yaml
    
    Co-authored-by: May Lee <mayl@alumni.cmu.edu>
    
    ---------
    
    Co-authored-by: May Lee <mayl@alumni.cmu.edu>

--- pkg/collector/corechecks/oracle-dbm/connection_handling.go
+++ pkg/collector/corechecks/oracle-dbm/connection_handling.go
@@ -61,10 +61,12 @@ func (c *Check) Connect() (*sqlx.DB, error) {
 
 	db, err := sqlx.Open(oracleDriver, connStr)
 	if err != nil {
+		_, err := handleRefusedConnection(c, db, err)
 		return nil, fmt.Errorf("failed to connect to oracle instance: %w", err)
 	}
 	err = db.Ping()
 	if err != nil {
+		_, err := handleRefusedConnection(c, db, err)
 		return nil, fmt.Errorf("failed to ping oracle instance: %w", err)
 	}
 
