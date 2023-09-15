ba295c95ec7da5569d6f10a8d9561614a18001ad
Minyi Zhu <minyi.zhu@datadoghq.com>
2023-09-15 18:23:24 -0400

    fix linter

--- pkg/collector/corechecks/containers/kubelet/provider/summary/provider.go
+++ pkg/collector/corechecks/containers/kubelet/provider/summary/provider.go
@@ -54,7 +54,7 @@ func NewProvider(filter *containers.Filter,
 	}
 
 	rateFilterList := []*regexp.Regexp{
-		regexp.MustCompile("diskio.io_service_bytes.stats.total"),
+		regexp.MustCompile("diskio[.]io_service_bytes[.]stats[.]total"),
 		regexp.MustCompile("network[.].._bytes"),
 		regexp.MustCompile("cpu[.].*[.]total"),
 	} //default enabled_rates
