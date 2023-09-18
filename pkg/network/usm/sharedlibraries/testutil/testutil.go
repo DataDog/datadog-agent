ed8f2d8d093f65be92b7e68da32e69110e7a0f0b
Pedro Lambert <pedro.lambert@datadoghq.com>
2023-09-18 12:50:40 -0400

    [usm] Add debugging endpoint for listing USM traced programs (#19473)

--- pkg/network/usm/sharedlibraries/testutil/testutil.go
+++ pkg/network/usm/sharedlibraries/testutil/testutil.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build linux_bpf && test
+//go:build linux && test
 
 package testutil
 
