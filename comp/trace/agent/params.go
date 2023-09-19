e8c296a4d09de108bbf02629af14c283036eae27
Dario Castañé <dario.castane@datadoghq.com>
2023-09-19 16:05:41 +0200

    comp/trace: add missing files

---
+++ comp/trace/agent/params.go
@@ -0,0 +1,15 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023-present Datadog, Inc.
+
+package agent
+
+type Params struct {
+	// PIDFilePath contains the value of the --pidfile flag.
+	PIDFilePath string
+	// CPUProfile contains the value for the --cpu-profile flag.
+	CPUProfile string
+	// MemProfile contains the value for the --mem-profile flag.
+	MemProfile string
+}
