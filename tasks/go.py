ffd4b796927601f9262239a256afb1ab64a4bc01
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-09-20 17:06:29 +0200

    Use context managers to monitor durations (#19556)
    * tasks: utils: add a stop_watch context manager
    
    Which replicates the timed helper, but doesn't print the duration itself
    
    * tasks: agent: use stop_watch helper to measure durations
    
    * tasks: go: use utils.timed to measure tasks duration
    
    * tasks: merge stop_watch into timed context manager

--- tasks/go.py
+++ tasks/go.py
@@ -2,7 +2,6 @@
 Golang related tasks go here
 """
 
-import datetime
 import glob
 import os
 import shutil
@@ -15,7 +14,7 @@ from invoke.exceptions import Exit
 from .build_tags import ALL_TAGS, UNIT_TEST_TAGS, get_default_build_tags
 from .licenses import get_licenses_list
 from .modules import DEFAULT_MODULES, generate_dummy_package
-from .utils import get_build_flags
+from .utils import get_build_flags, timed
 
 GOOS_MAPPING = {
     "win32": "windows",
@@ -95,13 +94,11 @@ def deps(ctx, verbose=False):
     """
 
     print("downloading dependencies")
-    start = datetime.datetime.now()
-    verbosity = ' -x' if verbose else ''
-    for mod in DEFAULT_MODULES.values():
-        with ctx.cd(mod.full_path()):
-            ctx.run(f"go mod download{verbosity}")
-    dep_done = datetime.datetime.now()
-    print(f"go mod download, elapsed: {dep_done - start}")
+    with timed("go mod download"):
+        verbosity = ' -x' if verbose else ''
+        for mod in DEFAULT_MODULES.values():
+            with ctx.cd(mod.full_path()):
+                ctx.run(f"go mod download{verbosity}")
 
 
 @task
@@ -111,26 +108,23 @@ def deps_vendored(ctx, verbose=False):
     """
 
     print("vendoring dependencies")
-    start = datetime.datetime.now()
-    verbosity = ' -v' if verbose else ''
+    with timed("go mod vendor"):
+        verbosity = ' -v' if verbose else ''
 
-    ctx.run(f"go mod vendor{verbosity}")
-    ctx.run(f"go mod tidy{verbosity} -compat=1.17")
+        ctx.run(f"go mod vendor{verbosity}")
+        ctx.run(f"go mod tidy{verbosity} -compat=1.17")
 
-    # "go mod vendor" doesn't copy files that aren't in a package: https://github.com/golang/go/issues/26366
-    # This breaks when deps include other files that are needed (eg: .java files from gomobile): https://github.com/golang/go/issues/43736
-    # For this reason, we need to use a 3rd party tool to copy these files.
-    # We won't need this if/when we change to non-vendored modules
-    ctx.run(f'modvendor -copy="**/*.c **/*.h **/*.proto **/*.java"{verbosity}')
+        # "go mod vendor" doesn't copy files that aren't in a package: https://github.com/golang/go/issues/26366
+        # This breaks when deps include other files that are needed (eg: .java files from gomobile): https://github.com/golang/go/issues/43736
+        # For this reason, we need to use a 3rd party tool to copy these files.
+        # We won't need this if/when we change to non-vendored modules
+        ctx.run(f'modvendor -copy="**/*.c **/*.h **/*.proto **/*.java"{verbosity}')
 
-    # If github.com/DataDog/datadog-agent gets vendored too - nuke it
-    # This may happen because of the introduction of nested modules
-    if os.path.exists('vendor/github.com/DataDog/datadog-agent'):
-        print("Removing vendored github.com/DataDog/datadog-agent")
-        shutil.rmtree('vendor/github.com/DataDog/datadog-agent')
-
-    dep_done = datetime.datetime.now()
-    print(f"go mod vendor, elapsed: {dep_done - start}")
+        # If github.com/DataDog/datadog-agent gets vendored too - nuke it
+        # This may happen because of the introduction of nested modules
+        if os.path.exists('vendor/github.com/DataDog/datadog-agent'):
+            print("Removing vendored github.com/DataDog/datadog-agent")
+            shutil.rmtree('vendor/github.com/DataDog/datadog-agent')
 
 
 @task
