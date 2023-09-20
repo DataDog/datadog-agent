c9a5730fbf9476444ee3289557e90e70a7def697
Dario Castañé <dario.castane@datadoghq.com>
2023-09-20 14:39:23 +0200

    comp/trace: revert year update

--- comp/trace/config/component_mock.go
+++ comp/trace/config/component_mock.go
@@ -1,7 +1,7 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2023-present Datadog, Inc.
+// Copyright 2016-present Datadog, Inc.
 
 // Package config implements a component to handle agent configuration.  This
 // component temporarily wraps pkg/config.
@@ -20,8 +20,9 @@
 package config
 
 import (
-	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"go.uber.org/fx"
+
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
 // Mock implements mock-specific methods.
