5ef71dae2b6bcf3d4166b17592f6fcdcabb0cc88
Adam Karpowich <adam.karpowich@datadoghq.com>
2023-09-22 14:17:56 -0400

    Revert "use raw git instead of gh"
    This reverts commit 34b6e96e8013e9b521f7a7483a7a77688321cd1e.

--- .gitlab/scripts/generate_changelog.sh
+++ .gitlab/scripts/generate_changelog.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Get the value of the Git tag "stripe_staging"
-PREV_AGENT_SHA=$(git rev-list -n 1 'stripe_staging')
+PREV_AGENT_SHA=$(git tag -l 'stripe_staging' | tail -n 1)
 
 if [ -z "$PREV_AGENT_SHA" ]; then
     echo "Git tag 'stripe_staging' not found"
@@ -21,14 +21,13 @@ unique_emails=""
 
 for commit_hash in $(git rev-list "$PREV_AGENT_SHA".."$CI_COMMIT_SHORT_SHA")
 do
-  # Get the commit message
-  commit_message=$(git log --format="%s" -n 1 "$commit_hash")
-
   # Get the author's email from Git log
   author_email=$(git log -n 1 --pretty=format:"%ae" "$commit_hash")
 
-  # Append commit message and author to changelog
-  changelog+="Commit: $commit_message\nAuthor: $author_email\n"
+  # Fetch PR information using 'gh'
+  pr_info=$(gh search prs "$commit_hash" --repo 'DataDog/datadog-agent' --label 'component/system-probe' --merged --json 'title,url,author,number' --template "{{range .}}{{printf \"%v %v %v %v\" .title \"$author_email\" .author.login .url}}{{end}}")
+  # Append PR info to changelog
+  changelog+="$pr_info\n"
 
   # Store unique email addresses in the list
   if [ -n "$author_email" ]; then
@@ -45,3 +44,4 @@ pwd
 
 echo -e "$changelog" > changelog.txt
 echo -e "$unique_emails" > unique_emails.txt
+
