ddf7c9f7af6503b75c9beed0ceef37a10cbbb57f
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-09-22 17:41:52 +0200

    Rename folder and function and fix go test args

--- tasks/new_e2e_tests.py
+++ tasks/new_e2e_tests.py
@@ -91,10 +91,10 @@ def run(
     cmd = f'gotestsum --format {gotestsum_format} '
     cmd += '{junit_file_flag} --packages="{packages}" -- -ldflags="-X {REPO_PATH}/test/new-e2e/containers.GitCommit={commit}" {verbose} -mod={go_mod} -vet=off -timeout {timeout} -tags {go_build_tags} {nocache} {run} {skip} {coverage_opt} {test_run_arg}'
     if osversion != "" or platform != "" or cws_supported_osversion != "":
-        cmd += "-args "
-        cmd += "-osversion {osversion}" if osversion else ""
-        cmd += "-platform {platform}" if platform else ""
-        cmd += "cws-supported-osversion {cws_supported_osversion}" if cws_supported_osversion else ""
+        cmd += "-args"
+        cmd += " -osversion {osversion}" if osversion else ""
+        cmd += " -platform {platform}" if platform else ""
+        cmd += " -cws-supported-osversion {cws_supported_osversion}" if cws_supported_osversion else ""
 
     args = {
         "go_mod": "mod",
