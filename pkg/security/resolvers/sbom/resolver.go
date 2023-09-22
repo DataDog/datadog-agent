1871eb8192b65efe73ccce9b81e7529b8495aa90
YoannGh <yoann.ghigoff@datadoghq.com>
2023-09-22 12:24:43 +0200

    increase retry delay

--- pkg/security/resolvers/sbom/resolver.go
+++ pkg/security/resolvers/sbom/resolver.go
@@ -203,7 +203,7 @@ func (r *Resolver) Start(ctx context.Context) {
 			case sbom := <-r.scannerChan:
 				if err := retry.Do(func() error {
 					return r.analyzeWorkload(sbom)
-				}, retry.Attempts(maxSBOMGenerationRetries), retry.Delay(20*time.Millisecond)); err != nil {
+				}, retry.Attempts(maxSBOMGenerationRetries), retry.Delay(200*time.Millisecond)); err != nil {
 					seclog.Errorf(err.Error())
 				}
 			}
