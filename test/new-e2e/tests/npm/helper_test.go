12ccf37021bda084f5aa78a1d56be960b53893d1
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-09-24 18:30:11 +0200

    LastTcpEstablished

--- test/new-e2e/tests/npm/helper_test.go
+++ test/new-e2e/tests/npm/helper_test.go
@@ -84,6 +84,7 @@ func validateUDPConnection(t *testing.T, c *agentmodel.Connection) {
 	require.Zero(t, c.Rtt, "Rtt != 0")
 	require.Zero(t, c.RttVar, "RttVar != 0")
 	require.Zero(t, c.LastRetransmits, "LastRetransmits != 0")
+	require.Zero(t, c.LastTcpEstablished, "LastTcpEstablished != 0")
 
 	// we can this only for UDP connection as there are no empty payload packets
 	// technically possible but in reality no UDP protocol implement that
