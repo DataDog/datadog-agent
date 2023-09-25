2c880aaf881ff61661bbaa9f688859884c26aae9
Stanley Liu <stanley.liu@datadoghq.com>
2023-09-25 15:26:49 -0400

    Fix detectContainerFeatures

--- pkg/conf/env/environment_nocontainers.go
+++ pkg/conf/env/environment_nocontainers.go
@@ -7,10 +7,12 @@
 
 package env
 
+import "github.com/DataDog/datadog-agent/pkg/conf"
+
 // IsAnyContainerFeaturePresent checks if any of known container features is present
 func IsAnyContainerFeaturePresent() bool {
 	return false
 }
 
-func detectContainerFeatures(features FeatureMap) {
+func detectContainerFeatures(features FeatureMap, cfg conf.ConfigReader) {
 }
