6804efa4b7344bf1e6f7f170216e12fb6f6d0331
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-09-25 16:48:23 +0200

    add test

--- pkg/networkdevice/profile/profiledefinition/profile_definition_test.go
+++ pkg/networkdevice/profile/profiledefinition/profile_definition_test.go
@@ -7,69 +7,139 @@ import (
 )
 
 func TestNewProfileDefinition(t *testing.T) {
-	// language=json
-	rcConfig := []byte(`
-{
-	"profile_definition": {
-		"name": "a-profile",
-		"metrics": [
+	tests := []struct {
+		name            string
+		inputJsonConfig string
+		expectedProfile DeviceProfileRcConfig
+	}{
+		{
+			name: "metric_tag mapping",
+			inputJsonConfig: `
 			{
-				"symbols": [
-					{
-						"OID": "1.2.3",
-						"name": "aSymbol"
-					}
-				],
-				"metric_tags": [
-					{
-						"tag": "a-tag",
-						"column": {
-							"OID": "1.2.3",
-							"name": "aSymbol"
-						},
-						"mapping": [
-							{
-								"key": "1",
-								"value": "aa"
-							},
-							{
-								"key": "2",
-								"value": "bb"
-							}
-						]
-					}
-				]
-			}
-		]
-	}
-}`)
-
-	profile := DeviceProfileRcConfig{}
-
-	err := json.Unmarshal(rcConfig, &profile)
-	assert.NoError(t, err)
-
-	expectedProfile := DeviceProfileRcConfig{
-		Profile: ProfileDefinition{
-			Name: "a-profile",
-			Metrics: []MetricsConfig{
-				{
-					Symbols: []SymbolConfig{
+				"profile_definition": {
+					"name": "a-profile",
+					"metrics": [
 						{
-							OID:  "1.2.3",
-							Name: "aSymbol",
+							"symbols": [
+								{
+									"OID": "1.2.3",
+									"name": "aSymbol"
+								}
+							],
+							"metric_tags": [
+								{
+									"tag": "a-tag",
+									"column": {
+										"OID": "1.2.3",
+										"name": "aSymbol"
+									},
+									"mapping": [
+										{
+											"key": "1",
+											"value": "aa"
+										},
+										{
+											"key": "2",
+											"value": "bb"
+										}
+									]
+								}
+							]
+						}
+					]
+				}
+			}`,
+			expectedProfile: DeviceProfileRcConfig{
+				Profile: ProfileDefinition{
+					Name: "a-profile",
+					Metrics: []MetricsConfig{
+						{
+							Symbols: []SymbolConfig{
+								{
+									OID:  "1.2.3",
+									Name: "aSymbol",
+								},
+							},
+							MetricTags: MetricTagConfigList{
+								{
+									Column: SymbolConfig{
+										OID:  "1.2.3",
+										Name: "aSymbol",
+									},
+									Tag: "a-tag",
+									Mapping: map[string]string{
+										"1": "aa",
+										"2": "bb",
+									},
+								},
+							},
 						},
 					},
-					MetricTags: MetricTagConfigList{
+				},
+			},
+		},
+		{
+			name: "metric_tag match tags",
+			inputJsonConfig: `
+			{
+				"profile_definition": {
+					"name": "a-profile",
+					"metrics": [
 						{
-							Column: SymbolConfig{
-								OID:  "1.2.3",
-								Name: "aSymbol",
+							"symbols": [
+								{
+									"OID": "1.2.3",
+									"name": "aSymbol"
+								}
+							],
+							"metric_tags": [
+								{
+									"tag": "a-tag",
+									"column": {
+										"OID": "1.2.3",
+										"name": "aSymbol"
+									},
+									"match": "(\\d)(\\d)",
+									"tags": [
+										{
+											"key": "foo",
+											"value": "$1"
+										},
+										{
+											"key": "bar",
+											"value": "$2"
+										}
+									]
+								}
+							]
+						}
+					]
+				}
+			}`,
+			expectedProfile: DeviceProfileRcConfig{
+				Profile: ProfileDefinition{
+					Name: "a-profile",
+					Metrics: []MetricsConfig{
+						{
+							Symbols: []SymbolConfig{
+								{
+									OID:  "1.2.3",
+									Name: "aSymbol",
+								},
 							},
-							Tag: "a-tag",
-							Mapping: map[string]string{
-								"1": "aa",
-								"2": "bb",
+							MetricTags: MetricTagConfigList{
+								{
+									Column: SymbolConfig{
+										OID:  "1.2.3",
+										Name: "aSymbol",
+									},
+									Tag:   "a-tag",
+									Match: "(\\d)(\\d)",
+									Tags: map[string]string{
+										"foo": "$1",
+										"bar": "$2",
+									},
+								},
 							},
 						},
 					},
@@ -77,5 +147,14 @@ func TestNewProfileDefinition(t *testing.T) {
 			},
 		},
 	}
-	assert.Equal(t, expectedProfile, profile)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actualProfile := DeviceProfileRcConfig{}
+			err := json.Unmarshal([]byte(tt.inputJsonConfig), &actualProfile)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.expectedProfile, actualProfile)
+		})
+	}
 }
