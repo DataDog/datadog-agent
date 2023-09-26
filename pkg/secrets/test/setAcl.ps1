fb4f53073bb0a441aafa2c336aa902fae2d89fa6
Len Gamburg <len.gamburg@datadoghq.com>
2023-09-25 22:00:20 -0400

    Testing with longer timeout and single secrets package

--- pkg/secrets/test/setAcl.ps1
+++ pkg/secrets/test/setAcl.ps1
@@ -43,7 +43,7 @@ if ($removeLocalSystem -eq $True) {
 
 # adding ACL for current user
 if ($addDDUser -eq $True) {
-    $ddCurrentUser = $Env:ComputerName + "\" + $Env:UserName
+    $ddCurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
     $ddAcl = New-Object  system.security.accesscontrol.filesystemaccessrule($ddCurrentUser, "FullControl","Allow")
     $acl.SetAccessRule($ddAcl)
 }
