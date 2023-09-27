2647e69fcb9cfae31bce0f05ad685416c58ff052
Daniel Lepage <daniel.lepage@datadoghq.com>
2023-09-27 15:00:58 -0400

    Add mock to ndmtmp/sender.

--- comp/ndmtmp/sender/component.go
+++ comp/ndmtmp/sender/component.go
@@ -7,9 +7,14 @@
 package sender
 
 import (
+	"testing"
+	"time"
+
 	"go.uber.org/fx"
 
 	"github.com/DataDog/datadog-agent/pkg/aggregator/sender"
+	"github.com/DataDog/datadog-agent/pkg/metrics/event"
+	"github.com/DataDog/datadog-agent/pkg/metrics/servicecheck"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
@@ -23,4 +28,29 @@ type Component interface {
 // Module defines the fx options for this component.
 var Module = fxutil.Component(
 	fx.Provide(getDefaultSender),
+)
+
+// MockComponent is an interface satisfied by mocksender.MockSender.
+type MockComponent interface {
+	Component
+	AssertServiceCheck(t *testing.T, checkName string, status servicecheck.ServiceCheckStatus, hostname string, tags []string, message string) bool
+	AssertMetric(t *testing.T, method string, metric string, value float64, hostname string, tags []string) bool
+	AssertMonotonicCount(t *testing.T, method string, metric string, value float64, hostname string, tags []string, flushFirstValue bool) bool
+	AssertHistogramBucket(t *testing.T, method string, metric string, value int64, lowerBound float64, upperBound float64, monotonic bool, hostname string, tags []string, flushFirstValue bool) bool
+	AssertMetricInRange(t *testing.T, method string, metric string, min float64, max float64, hostname string, tags []string) bool
+	AssertMetricTaggedWith(t *testing.T, method string, metric string, tags []string) bool
+	AssertMetricNotTaggedWith(t *testing.T, method string, metric string, tags []string) bool
+	AssertEvent(t *testing.T, expectedEvent event.Event, allowedDelta time.Duration) bool
+	AssertEventPlatformEvent(t *testing.T, expectedRawEvent []byte, expectedEventType string) bool
+	AssertEventMissing(t *testing.T, expectedEvent event.Event, allowedDelta time.Duration) bool
+}
+
+// MockModule provides a MockSender as the sender Component.
+var MockModule = fxutil.Component(
+	fx.Provide(
+		newMockSender,
+		func(s MockComponent) Component {
+			return s
+		},
+	),
 )
--- comp/ndmtmp/sender/sender.go
+++ comp/ndmtmp/sender/sender.go
@@ -6,8 +6,17 @@
 // Package sender exposes a Sender for netflow.
 package sender
 
-import "github.com/DataDog/datadog-agent/comp/ndmtmp/aggregator"
+import (
+	"github.com/DataDog/datadog-agent/comp/ndmtmp/aggregator"
+	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
+)
 
 func getDefaultSender(agg aggregator.Component) (Component, error) {
 	return agg.GetDefaultSender()
+}
+
+func newMockSender() MockComponent {
+	mockSender := mocksender.NewMockSender("mock-sender")
+	mockSender.SetupAcceptAll()
+	return mockSender
 }
