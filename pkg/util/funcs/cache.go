94c83910ed03e4c78b9259875637356a21fa10fd
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-09-27 10:45:17 -0700

    Add package comment

--- pkg/util/funcs/cache.go
+++ pkg/util/funcs/cache.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package funcs provides utilities for functions, such as caching and memoization.
 package funcs
 
 import "sync"
