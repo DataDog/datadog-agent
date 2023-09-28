0ec8a30db31b3e2fe6e8c3d72b9d5e6eba50b004
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2023-09-28 18:18:03 +0200

    formatter

--- pkg/collector/corechecks/oracle-dbm/init.go
+++ pkg/collector/corechecks/oracle-dbm/init.go
@@ -51,11 +51,11 @@ func (c *Check) init() error {
 		return fmt.Errorf("%s failed to query v$instance: %w", c.logPrompt, err)
 	}
 	c.dbVersion = i.VersionFull
-	if isDbVersionGreaterOrEqualThan(c, "18"){
+	if isDbVersionGreaterOrEqualThan(c, "18") {
 		err = getWrapper(c, &c.dbVersion, "SELECT /* DD */ version_full FROM v$instance")
 		if err != nil {
 			return fmt.Errorf("%s failed to query full version: %w", c.logPrompt, err)
-		}	
+		}
 	}
 
 	if c.config.ReportedHostname != "" {
