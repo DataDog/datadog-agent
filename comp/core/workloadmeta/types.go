46fc822ccae564d27d397974f33149d29c26964b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2023-09-28 15:55:28 +0200

    [workloadmeta] fixing misspell lint

--- comp/core/workloadmeta/types.go
+++ comp/core/workloadmeta/types.go
@@ -469,7 +469,7 @@ type ContainerSecurityContext struct {
 	SeccompProfile *SeccompProfile
 }
 
-// Capabilities is the capabilities a certain Conatiner security context is capable of
+// Capabilities is the capabilities a certain Container security context is capable of
 type Capabilities struct {
 	Add  []string
 	Drop []string
