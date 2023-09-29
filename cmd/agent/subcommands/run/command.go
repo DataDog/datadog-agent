13978036af7ee2b3c6d498587a5b825b691b3952
Joshua Lineaweaver <JLineaweaver@gmail.com>
2023-09-29 08:05:39 -0600

    [orchestrator] Move the pod check to corechecks (#18740)
    * [orchestrator] Move the pod check to corechecks
    
    We want to move the pod check away from the process agent into the
    cluster agent. This will allow us to match all of the other k8s
    corechecks.

--- cmd/agent/subcommands/run/command.go
+++ cmd/agent/subcommands/run/command.go
@@ -103,6 +103,7 @@ import (
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/net"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/nvidia/jetson"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/oracle-dbm"
+	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/orchestrator/pod"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/sbom"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/snmp"
 	_ "github.com/DataDog/datadog-agent/pkg/collector/corechecks/system/cpu"
