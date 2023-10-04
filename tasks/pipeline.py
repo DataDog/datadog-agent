05e97cff02df2776fbec59c0f05bee53e3146fe3
Adam Karpowich <adam.karpowich@datadoghq.com>
2023-10-04 18:02:52 -0400

    run formatter

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -495,9 +495,12 @@ def is_system_probe(owners, files):
 
 @task
 def changelog(ctx, new_commit_sha):
-    old_commit_sha = ctx.run("aws ssm get-parameter --region us-east-1 --name "
-                             "ci.datadog-agent.gitlab_changelog_commit_sha --with-decryption --query "
-                             "\"Parameter.Value\" --out text", hide=False).stdout.strip()
+    old_commit_sha = ctx.run(
+        "aws ssm get-parameter --region us-east-1 --name "
+        "ci.datadog-agent.gitlab_changelog_commit_sha --with-decryption --query "
+        "\"Parameter.Value\" --out text",
+        hide=False,
+    ).stdout.strip()
     print(f"Generating changelog for commit range {old_commit_sha} to {new_commit_sha}")
     commits = ctx.run(f"git log {old_commit_sha}..{new_commit_sha} --pretty=format:%h", hide=True).stdout.split("\n")
     owners = read_owners(".github/CODEOWNERS")
@@ -516,17 +519,16 @@ def changelog(ctx, new_commit_sha):
                 messages.append(f"<{url}|{title}>")
 
     slack_message = (
-            f"The nightly deployment is rolling out to Staging :siren: \n"
-            f"Changelog for commit range: `{old_commit_sha}` to `{new_commit_sha}`\n"
-            + "\n".join(messages)
-            + "\n:wave: Authors, please check relevant "
-              "<https://ddstaging.datadoghq.com/dashboard/kfn-zy2-t98|dashboards> for issues"
+        f"The nightly deployment is rolling out to Staging :siren: \n"
+        f"Changelog for commit range: `{old_commit_sha}` to `{new_commit_sha}`\n"
+        + "\n".join(messages)
+        + "\n:wave: Authors, please check relevant "
+        "<https://ddstaging.datadoghq.com/dashboard/kfn-zy2-t98|dashboards> for issues"
     )
     print(f"tagging {new_commit_sha}")
     # ctx.run(f"aws ssm put-parameter --name ci.datadog-agent.gitlab_changelog_commit_sha --value \"{new_git_sha}\" "
     #         "--type \"SecureString\" --region us-east-1", hide=False)
     send_slack_message("system-probe-ops", slack_message)
-
 
 
 @task
