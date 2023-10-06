02e4e9a5e889b6ba01168b7fcbc074189accecab
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-10-06 18:26:36 +0200

    fix lint

--- pkg/networkdevice/profile/profiledefinition/bundle.go
+++ pkg/networkdevice/profile/profiledefinition/bundle.go
@@ -1,9 +1,17 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023-present Datadog, Inc.
+
 package profiledefinition
 
+// ProfileSource is the type used to indicate the source (e.g. custom or default) of the profile
 type ProfileSource string
 
 const (
-	SourceCustom  ProfileSource = "custom"
+	// SourceCustom enum for custom profile source
+	SourceCustom ProfileSource = "custom"
+	// SourceDefault enum for default profile source
 	SourceDefault ProfileSource = "default"
 )
 
