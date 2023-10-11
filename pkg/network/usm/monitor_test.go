ad5511fc461f13fd331764c3eab6359ef47e5802
Guy Arbitman <guy20495@gmail.com>
2023-10-11 08:28:50 +0300

    usm: Do not allocate buffers for unused response bodies

--- pkg/network/usm/monitor_test.go
+++ pkg/network/usm/monitor_test.go
@@ -130,7 +130,7 @@ func (s *HTTPTestSuite) TestHTTPMonitorCaptureRequestMultipleTimes() {
 				resp, err := client.Do(req)
 				require.NoError(t, err)
 				// Have to read the response body to ensure the client will be able to properly close the connection.
-				io.ReadAll(resp.Body)
+				io.Copy(io.Discard, resp.Body)
 				resp.Body.Close()
 			}
 			srvDoneFn()
