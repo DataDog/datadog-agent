df64a377e895fe2dbca5ee4c79ab8a7b02c20769
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2023-10-11 10:59:41 -0700

    lint ding
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

--- pkg/metrics/context_metrics_flusher.go
+++ pkg/metrics/context_metrics_flusher.go
@@ -42,8 +42,12 @@ func (f *ContextMetricsFlusher) Append(bucketTimestamp float64, contextMetrics C
 // `callback`. Any errors encountered flushing the Metric instances are returned,
 // but such errors do not interrupt the flushing operation.
 func (f *ContextMetricsFlusher) FlushAndClear(callback func([]*Serie)) map[ckey.ContextKey][]error {
-	errors := make(map[ckey.ContextKey][]error)
-	var series []*Serie
+	// NOTE in practice this function and its call-chain are allocation
+	// hotspots in Agent when counter metrics are dominate. We pre-allocate
+	// these structures to arbitrary, smallish sizes to avoid needing to
+	// grow from 0-size.
+	series := make([]*Serie, 0, 32)
+	errors := make(map[ckey.ContextKey]error, 32)
 
 	contextMetricsCollection := make([]ContextMetrics, 0, len(f.metrics))
 	for _, m := range f.metrics {
@@ -55,7 +59,7 @@ func (f *ContextMetricsFlusher) FlushAndClear(callback func([]*Serie)) map[ckey.
 	aggregateContextMetricsByContextKey(
 		contextMetricsCollection,
 		func(contextKey ckey.ContextKey, m Metric, contextMetricIndex int) {
-			series = flushToSeries(
+			flushToSeries(
 				contextKey,
 				m,
 				f.metrics[contextMetricIndex].bucketTimestamp,
