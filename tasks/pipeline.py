7b7dbdcdfd3dab2e705edf3bbe314f79a6495c8b
Kylian Serrania <kylian.serrania@datadoghq.com>
2023-10-12 00:57:02 +0200

    [invoke] Revamp notify task to separate mandatory and optional jobs

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -34,7 +34,7 @@ from .libs.pipeline_tools import (
     trigger_agent_pipeline,
     wait_for_pipeline,
 )
-from .libs.types import SlackMessage, TeamMessage
+from .libs.types import FailedJobs, SlackMessage, TeamMessage
 from .utils import (
     DEFAULT_BRANCH,
     GITHUB_REPO_NAME,
@@ -376,7 +376,7 @@ Please check for typos in the JOBOWNERS file and/or add them to the Github <-> S
 """
 
 
-def generate_failure_messages(project_name, failed_jobs):
+def generate_failure_messages(project_name: str, failed_jobs: FailedJobs) -> dict[str, SlackMessage]:
     all_teams = "@DataDog/agent-all"
 
     # Generate messages for each team
@@ -594,7 +594,7 @@ def notify(_, notification_type="merge", print_to_stdout=False):
 
     # From the job failures, set whether the pipeline succeeded or failed and craft the
     # base message that will be sent.
-    if failed_jobs:  # At least one job failed
+    if failed_jobs.all_mandatory_failures():  # At least one mandatory job failed
         header_icon = ":host-red:"
         state = "failed"
         coda = "If there is something wrong with the notification please contact #agent-platform"
