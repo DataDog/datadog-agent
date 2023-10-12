6d4cf92287777ede628bf7c766270bea1d8646a1
Scott Opell <me@scottopell.com>
2023-10-12 19:48:12 +0000

    Adds some new dsd telemetry

--- comp/dogstatsd/server/server.go
+++ comp/dogstatsd/server/server.go
@@ -51,6 +51,9 @@ var (
 	tlmProcessedOk    = tlmProcessed.WithValues("metrics", "ok", "")
 	tlmProcessedError = tlmProcessed.WithValues("metrics", "error", "")
 
+	tlmTotalPackets  = telemetry.NewSimpleCounter("dogstatsd", "packets", "Count of packets processed by DogStatsD server")
+	tlmTotalMessages = telemetry.NewCounter("dogstatsd", "messages", []string{"message_type"}, "Count of raw dogstatsd messages")
+
 	// while we try to add the origin tag in the tlmProcessed metric, we want to
 	// avoid having it growing indefinitely, hence this safeguard to limit the
 	// size of this cache for long-running agent or environment with a lot of
@@ -594,6 +597,7 @@ func (s *server) parsePackets(batcher *batcher, parser *parser, packets []*packe
 					s.errLog("Dogstatsd: error parsing service check '%q': %s", message, err)
 					continue
 				}
+				tlmTotalMessages.IncWithTags(map[string]string{"message_type": "service_checks"})
 				batcher.appendServiceCheck(serviceCheck)
 			case eventType:
 				event, err := s.parseEventMessage(parser, message, packet.Origin)
@@ -601,6 +605,7 @@ func (s *server) parsePackets(batcher *batcher, parser *parser, packets []*packe
 					s.errLog("Dogstatsd: error parsing event '%q': %s", message, err)
 					continue
 				}
+				tlmTotalMessages.IncWithTags(map[string]string{"message_type": "events"})
 				batcher.appendEvent(event)
 			case metricSampleType:
 				var err error
@@ -612,6 +617,7 @@ func (s *server) parsePackets(batcher *batcher, parser *parser, packets []*packe
 					s.errLog("Dogstatsd: error parsing metric message '%q': %s", message, err)
 					continue
 				}
+				tlmTotalMessages.IncWithTags(map[string]string{"message_type": "metrics"})
 
 				for idx := range samples {
 					s.Debug.StoreMetricStats(samples[idx])
@@ -630,6 +636,7 @@ func (s *server) parsePackets(batcher *batcher, parser *parser, packets []*packe
 					}
 				}
 			}
+			tlmTotalPackets.Inc()
 		}
 		s.sharedPacketPoolManager.Put(packet)
 	}
@@ -694,7 +701,7 @@ func (s *server) parseMetricMessage(metricSamples []metrics.MetricSample, parser
 		dogstatsdMetricParseErrors.Add(1)
 		errorCnt.Inc()
 		return metricSamples, err
-	}
+	} // candidate to instrument # of dogstatsd messages
 
 	if s.mapper != nil {
 		mapResult := s.mapper.Map(sample.name)
