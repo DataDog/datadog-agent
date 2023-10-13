a5a14326e406fdc60f31c329a50c302acf9ca076
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-10-13 12:08:48 +0200

    move SetConfdPathAndCleanProfiles

---
+++ pkg/collector/corechecks/snmp/internal/profile/testing_utils.go
@@ -0,0 +1,27 @@
+package checkconfig
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/collector/corechecks/snmp/internal/profile"
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"os"
+	"path/filepath"
+)
+
+// SetConfdPathAndCleanProfiles is used for testing only
+func SetConfdPathAndCleanProfiles() {
+	profile.SetGlobalProfileConfigMap(nil) // make sure from the new confd path will be reloaded
+	file, _ := filepath.Abs(filepath.Join(".", "test", "conf.d"))
+	if !pathExists(file) {
+		file, _ = filepath.Abs(filepath.Join("..", "test", "conf.d"))
+	}
+	if !pathExists(file) {
+		file, _ = filepath.Abs(filepath.Join(".", "internal", "test", "conf.d"))
+	}
+	config.Datadog.Set("confd_path", file)
+}
+
+// pathExists returns true if the given path exists
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
