6ba212dcd5f37a3dc5775590174375918eada3bb
Olivier G <olivier.gaca@datadoghq.com>
2023-10-16 16:58:38 +0200

    Fix Windows build

--- cmd/dogstatsd/main_windows.go
+++ cmd/dogstatsd/main_windows.go
@@ -88,7 +88,7 @@ func (s *service) Run(ctx context.Context) error {
 			}
 			defer start.StopAgent(cancel, components)
 
-			err := start.RunDogstatsd(ctx, cliParams, config, log, params, components, demux)
+			err := start.RunDogstatsd(ctx, cliParams, config, log, params, components, demultiplexer)
 			if err != nil {
 				log.Errorf("Failed to start agent %v", err)
 				return err
