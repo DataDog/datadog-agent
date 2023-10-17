cd97bbdf85acd1e0afcf7a100345a17ffa0abb95
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2023-10-17 10:13:43 +0200

    [CWS] scrub windows cmd line

--- pkg/security/resolvers/process/resolver_windows.go
+++ pkg/security/resolvers/process/resolver_windows.go
@@ -159,6 +159,20 @@ func (p *Resolver) GetEnvp(pr *model.Process) []string {
 	return pr.Envp
 }
 
+// GetProcessCmdLineScrubbed returns the scrubbed cmdline
+func (p *Resolver) GetProcessCmdLineScrubbed(pr *model.Process) []string {
+	if pr.ScrubbedCmdLineResolved {
+		return pr.CmdLine
+	}
+
+	if p.scrubber != nil && len(pr.CmdLine) > 0 {
+		// replace with the scrubbed version
+		pr.CmdLine, _ = p.scrubber.ScrubCommand(pr.CmdLine)
+	}
+
+	return pr.CmdLine
+}
+
 // getCacheSize returns the cache size of the process resolver
 func (p *Resolver) getCacheSize() float64 {
 	p.RLock()
