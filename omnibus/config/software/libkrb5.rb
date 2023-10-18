025b927df2103668b273cc660c8411e4722eec3a
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-10-18 09:33:34 +0200

    omnibus: krb5: remove binaries

--- omnibus/config/software/libkrb5.rb
+++ omnibus/config/software/libkrb5.rb
@@ -36,4 +36,21 @@ build do
   # are properly linked. Must whitelist for build to succeed.
   whitelist_file "#{install_dir}/embedded/lib/krb5/plugins/tls/k5tls.so"
   whitelist_file "#{install_dir}/embedded/lib/krb5/plugins/preauth/pkinit.so"
+
+  delete "#{install_dir}/embedded/bin/compile_et"
+  delete "#{install_dir}/embedded/bin/gss-client"
+  delete "#{install_dir}/embedded/bin/k5srvutil"
+  delete "#{install_dir}/embedded/bin/kadmin"
+  delete "#{install_dir}/embedded/bin/kdestroy"
+  delete "#{install_dir}/embedded/bin/kinit"
+  delete "#{install_dir}/embedded/bin/klist"
+  delete "#{install_dir}/embedded/bin/kpasswd"
+  delete "#{install_dir}/embedded/bin/krb5-config"
+  delete "#{install_dir}/embedded/bin/ksu"
+  delete "#{install_dir}/embedded/bin/kswitch"
+  delete "#{install_dir}/embedded/bin/ktutil"
+  delete "#{install_dir}/embedded/bin/kvno"
+  delete "#{install_dir}/embedded/bin/sclient"
+  delete "#{install_dir}/embedded/bin/sim_client"
+  delete "#{install_dir}/embedded/bin/uuclient"
 end
