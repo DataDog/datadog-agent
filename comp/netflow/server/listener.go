93ed93790c6c6311291518c4854aa25706ed6fc6
atukic-dd <amir.tukic@datadoghq.com>
2023-10-19 15:07:34 +0200

    Status shows number of flows received

--- comp/netflow/server/listener.go
+++ comp/netflow/server/listener.go
@@ -7,6 +7,7 @@ package server
 
 import (
 	"github.com/DataDog/datadog-agent/comp/core/log"
+	"github.com/DataDog/datadog-agent/comp/netflow/common"
 	"github.com/DataDog/datadog-agent/comp/netflow/config"
 	"github.com/DataDog/datadog-agent/comp/netflow/flowaggregator"
 	"github.com/DataDog/datadog-agent/comp/netflow/goflowlib"
@@ -17,18 +18,31 @@ type netflowListener struct {
 	flowState *goflowlib.FlowStateWrapper
 	config    config.ListenerConfig
 	error     *atomic.String
+	flowCount *atomic.Int64
+}
+
+func (l *netflowListener) listen(inputChan <-chan *common.Flow, flowAgg *flowaggregator.FlowAggregator) {
+	for flow := range inputChan {
+		l.flowCount.Add(1)
+		flowAgg.GetFlowInChan() <- flow
+	}
 }
 
 func startFlowListener(listenerConfig config.ListenerConfig, flowAgg *flowaggregator.FlowAggregator, logger log.Component) (*netflowListener, error) {
 	atomicErr := atomic.NewString("")
+	flowCount := atomic.NewInt64(0)
+	inputChan := make(chan *common.Flow)
 
-	flowState, err := goflowlib.StartFlowRoutine(listenerConfig.FlowType, listenerConfig.BindHost, listenerConfig.Port, listenerConfig.Workers, listenerConfig.Namespace, flowAgg.GetFlowInChan(), logger, atomicErr)
+	flowState, err := goflowlib.StartFlowRoutine(listenerConfig.FlowType, listenerConfig.BindHost, listenerConfig.Port, listenerConfig.Workers, listenerConfig.Namespace, inputChan, logger, atomicErr)
 
 	listener := &netflowListener{
 		flowState: flowState,
 		config:    listenerConfig,
 		error:     atomicErr,
+		flowCount: flowCount,
 	}
+
+	go listener.listen(inputChan, flowAgg)
 
 	return listener, err
 }
