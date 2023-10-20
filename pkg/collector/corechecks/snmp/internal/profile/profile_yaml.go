3d8fa7ccb222b02223e2870c1491b44f322f3219
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-10-20 16:29:33 +0200

    add sysobjectid_resolver

--- pkg/collector/corechecks/snmp/internal/profile/profile_yaml.go
+++ pkg/collector/corechecks/snmp/internal/profile/profile_yaml.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"sync"
 
@@ -198,51 +197,4 @@ func resolveProfileDefinitionPath(definitionFile string) string {
 func getProfileConfdRoot(profileFolderName string) string {
 	confdPath := config.Datadog.GetString("confd_path")
 	return filepath.Join(confdPath, "snmp.d", profileFolderName)
-}
-
-func getMostSpecificOid(oids []string) (string, error) {
-	var mostSpecificParts []int
-	var mostSpecificOid string
-
-	if len(oids) == 0 {
-		return "", fmt.Errorf("cannot get most specific oid from empty list of oids")
-	}
-
-	for _, oid := range oids {
-		parts, err := getOidPatternSpecificity(oid)
-		if err != nil {
-			return "", err
-		}
-		if len(parts) > len(mostSpecificParts) {
-			mostSpecificParts = parts
-			mostSpecificOid = oid
-			continue
-		}
-		if len(parts) == len(mostSpecificParts) {
-			for i := range mostSpecificParts {
-				if parts[i] > mostSpecificParts[i] {
-					mostSpecificParts = parts
-					mostSpecificOid = oid
-				}
-			}
-		}
-	}
-	return mostSpecificOid, nil
-}
-
-func getOidPatternSpecificity(pattern string) ([]int, error) {
-	wildcardKey := -1
-	var parts []int
-	for _, part := range strings.Split(strings.TrimLeft(pattern, "."), ".") {
-		if part == "*" {
-			parts = append(parts, wildcardKey)
-		} else {
-			intPart, err := strconv.Atoi(part)
-			if err != nil {
-				return nil, fmt.Errorf("error parsing part `%s` for pattern `%s`: %v", part, pattern, err)
-			}
-			parts = append(parts, intPart)
-		}
-	}
-	return parts, nil
 }
--- pkg/collector/corechecks/snmp/internal/profile/profile_yaml_test.go
+++ pkg/collector/corechecks/snmp/internal/profile/profile_yaml_test.go
@@ -51,65 +51,6 @@ func Test_getDefaultProfilesDefinitionFiles(t *testing.T) {
 	assert.Equal(t, expectedProfileConfig, actualProfileConfig)
 }
 
-func Test_getMostSpecificOid(t *testing.T) {
-	tests := []struct {
-		name           string
-		oids           []string
-		expectedOid    string
-		expectedErrror error
-	}{
-		{
-			"one",
-			[]string{"1.2.3.4"},
-			"1.2.3.4",
-			nil,
-		},
-		{
-			"error on empty oids",
-			[]string{},
-			"",
-			fmt.Errorf("cannot get most specific oid from empty list of oids"),
-		},
-		{
-			"error on parsing",
-			[]string{"a.1.2.3"},
-			"",
-			fmt.Errorf("error parsing part `a` for pattern `a.1.2.3`: strconv.Atoi: parsing \"a\": invalid syntax"),
-		},
-		{
-			"most lengthy",
-			[]string{"1.3.4", "1.3.4.1.2"},
-			"1.3.4.1.2",
-			nil,
-		},
-		{
-			"wild card 1",
-			[]string{"1.3.4.*", "1.3.4.1"},
-			"1.3.4.1",
-			nil,
-		},
-		{
-			"wild card 2",
-			[]string{"1.3.4.1", "1.3.4.*"},
-			"1.3.4.1",
-			nil,
-		},
-		{
-			"sample oids",
-			[]string{"1.3.6.1.4.1.3375.2.1.3.4.43", "1.3.6.1.4.1.8072.3.2.10"},
-			"1.3.6.1.4.1.3375.2.1.3.4.43",
-			nil,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			oid, err := getMostSpecificOid(tt.oids)
-			assert.Equal(t, tt.expectedErrror, err)
-			assert.Equal(t, tt.expectedOid, oid)
-		})
-	}
-}
-
 func Test_resolveProfileDefinitionPath(t *testing.T) {
 	defaultTestConfdPath, _ := filepath.Abs(filepath.Join("..", "test", "user_profiles.d"))
 	config.Datadog.Set("confd_path", defaultTestConfdPath)
