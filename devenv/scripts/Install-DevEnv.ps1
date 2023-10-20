9607028c65ee66eeb7913e3dc48dadc27e2911b9
Dustin Long <me@dustmop.io>
2023-10-20 13:31:39 -0400

    Upgrade go version to 1.20.10 (#20288)
    * Upgrade go version from 1.20.8 to 1.20.10
    
    * Use agent-buildimages main ci image
    
    ---------
    
    Co-authored-by: Jeremy Hanna <jeremy.hanna@datadoghq.com>

--- devenv/scripts/Install-DevEnv.ps1
+++ devenv/scripts/Install-DevEnv.ps1
@@ -45,11 +45,11 @@ Write-Host -ForegroundColor Yellow -BackgroundColor DarkGreen '- Installing Gola
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-Write-Host -ForegroundColor Green "Installing go 1.20.8"
+Write-Host -ForegroundColor Green "Installing go 1.20.10"
 
-$gozip = "https://dl.google.com/go/go1.20.8.windows-amd64.zip"
+$gozip = "https://dl.google.com/go/go1.20.10.windows-amd64.zip"
 if ($Env:TARGET_ARCH -eq "x86") {
-    $gozip = "https://dl.google.com/go/go1.20.8.windows-386.zip"
+    $gozip = "https://dl.google.com/go/go1.20.10.windows-386.zip"
 }
 
 $out = 'c:\go.zip'
