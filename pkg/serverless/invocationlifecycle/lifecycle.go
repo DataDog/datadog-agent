a29ef9adac12d8f276c50af252754ac0b8f03e33
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2023-10-25 16:35:20 -0400

    fix

--- pkg/serverless/invocationlifecycle/lifecycle.go
+++ pkg/serverless/invocationlifecycle/lifecycle.go
@@ -94,7 +94,6 @@ func (lp *LifecycleProcessor) OnInvokeStart(startDetails *InvocationStartDetails
 	}
 
 	eventType := trigger.GetEventType(lowercaseEventPayload)
-	log.Debugf("[lifecycle] Inferred Event Type : %v", eventType)
 	if eventType == trigger.Unknown {
 		log.Debugf("[lifecycle] Failed to extract event type")
 	}
