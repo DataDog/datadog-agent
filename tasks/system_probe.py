a7845f0157ffa2b2e276f2b0614a7f0ba782e26e
usamasaqib <usamasaqib.96@live.com>
2023-10-25 15:08:02 +0200

    address linter errors

--- tasks/system_probe.py
+++ tasks/system_probe.py
@@ -1610,10 +1610,10 @@ def save_test_dockers(ctx, output_dir, arch, windows=is_windows, use_crane=False
 
     # only download images not present in preprepared vm disk
     resp = requests.get('https://dd-agent-omnibus.s3.amazonaws.com/kernel-version-testing/rootfs/docker.ls')
-    docker_ls = set([line for line in resp.text.split('\n') if line.strip()])
+    docker_ls = {line for line in resp.text.split('\n') if line.strip()}
 
     images = _test_docker_image_list()
-    for image in (images - docker_ls):
+    for image in images - docker_ls:
         output_path = image.translate(str.maketrans('', '', string.punctuation))
         output_file = f"{os.path.join(output_dir, output_path)}.tar"
         if use_crane:
