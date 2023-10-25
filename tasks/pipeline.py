aeffe1bb4294cf70d86929be53ad7abf1e6bbd00
Adam Karpowich <adam.karpowich@datadoghq.com>
2023-10-25 17:25:37 -0400

    remove unncessary query params

--- tasks/pipeline.py
+++ tasks/pipeline.py
@@ -545,11 +545,10 @@ def changelog(ctx, new_commit_sha):
     if messages:
         slack_message += (
             "\n".join(messages) + "\n:wave: Authors, please check the "
-            "<https://ddstaging.datadoghq.com/dashboard/kfn-zy2-t98?refresh_mode=sliding&tpl_var_cluster_name%5B0%5D"
-                                  "=stripe&tpl_var_cluster_name%5B1%5D=muk&tpl_var_cluster_name%5B2%5D=snowver"
+            "<https://ddstaging.datadoghq.com/dashboard/kfn-zy2-t98?tpl_var_cluster_name%5B0%5D=stripe"
+                                  "&tpl_var_cluster_name%5B1%5D=muk&tpl_var_cluster_name%5B2%5D=snowver"
                                   "&tpl_var_cluster_name%5B3%5D=chillpenguin&tpl_var_cluster_name%5B4%5D=diglet"
-                                  "&tpl_var_cluster_name%5B5%5D=lagaffe&from_ts=1698254375766&to_ts=1698268775766"
-                                  "&live=true|dashboard> for issues"
+                                  "&tpl_var_cluster_name%5B5%5D=lagaffe|dashboard> for issues"
         )
     else:
         slack_message += "No new System Probe related commits in this release :cricket:"
