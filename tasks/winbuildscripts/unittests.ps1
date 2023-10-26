4b8a4550908f11b04dd6d7f63ab9294526842580
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2023-10-26 11:05:01 +0200

    Remove PyYAML override in the windows job

--- tasks/winbuildscripts/unittests.ps1
+++ tasks/winbuildscripts/unittests.ps1
@@ -13,8 +13,6 @@ if ($Env:TARGET_ARCH -eq "x64") {
 $UT_BUILD_ROOT=(Get-Location).Path
 $Env:PATH="$UT_BUILD_ROOT\dev\lib;$Env:GOPATH\bin;$Env:Python3_ROOT_DIR;$Env:Python3_ROOT_DIR\Scripts;$Env:PATH"
 
-& $Env:Python3_ROOT_DIR\python.exe -m pip install PyYAML==5.3.1
-
 & inv -e invoke-unit-tests
 
 if ($LASTEXITCODE -ne 0) {
