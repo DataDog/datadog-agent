4f906aea5de340486f2f50a4c1ec4e98e5e47d59
amitslavin <108348428+amitslavin@users.noreply.github.com>
2023-10-26 16:27:11 +0300

    usm: support RST stream (#20390)
    * support RST stream in HTTP/2 decoding

--- pkg/network/usm/monitor_test.go
+++ pkg/network/usm/monitor_test.go
@@ -568,7 +568,7 @@ func (s *USMHTTP2Suite) TestSimpleHTTP2() {
 					Path:   http.Path{Content: http.Interner.GetString("/")},
 					Method: http.MethodPost,
 				}: {
-					lower: 999,
+					lower: 1000,
 					upper: 1000,
 				},
 			},
@@ -590,7 +590,7 @@ func (s *USMHTTP2Suite) TestSimpleHTTP2() {
 					Path:   http.Path{Content: http.Interner.GetString("/index.html")},
 					Method: http.MethodPost,
 				}: {
-					lower: 999,
+					lower: 1000,
 					upper: 1000,
 				},
 			},
@@ -608,7 +608,7 @@ func (s *USMHTTP2Suite) TestSimpleHTTP2() {
 				tt.runClients(t, clientCount)
 
 				res := make(map[http.Key]int)
-				require.Eventually(t, func() bool {
+				assert.Eventually(t, func() bool {
 					stats := monitor.GetProtocolStats()
 					http2Stats, ok := stats[protocols.HTTP2]
 					if !ok {
@@ -646,6 +646,14 @@ func (s *USMHTTP2Suite) TestSimpleHTTP2() {
 
 					return true
 				}, time.Second*5, time.Millisecond*100, "%v != %v", res, tt.expectedEndpoints)
+				if t.Failed() {
+					o, err := monitor.DumpMaps("http2_in_flight")
+					if err != nil {
+						t.Logf("failed dumping http2_in_flight: %s", err)
+					} else {
+						t.Log(o)
+					}
+				}
 			})
 		}
 	}
