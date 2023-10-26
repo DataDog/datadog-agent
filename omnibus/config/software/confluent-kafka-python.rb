e403ee071dae79a1a465935bf814458676e3cccc
Vivekanand Ilango <vivekanand.ilango@datadoghq.com>
2023-10-26 11:27:55 +0200

    Bump librdkafka and confluent-kafka to v2.3.0

--- omnibus/config/software/confluent-kafka-python.rb
+++ omnibus/config/software/confluent-kafka-python.rb
@@ -1,12 +1,12 @@
 # https://github.com/confluentinc/confluent-kafka-python/blob/master/INSTALL.md#install-from-source
 
 name "confluent-kafka-python"
-default_version "2.2.0"
+default_version "2.3.0"
 
 dependency "pip3"
 
 source :url => "https://github.com/confluentinc/confluent-kafka-python/archive/refs/tags/v#{version}.tar.gz",
-       :sha256 => "ee099702bd5fccd3ce4916658fed4c7ef28cb22e111defb843d27633100ff065",
+       :sha256 => "6be601e34073e7e8df883eff4540c2ed08c2f79578002774212df51c3018bd7d",
        :extract => :seven_zip
 
 relative_path "confluent-kafka-python-#{version}"
