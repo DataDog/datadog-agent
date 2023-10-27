9ec307b2782c041dafdc0667db7fa8ec11573a1a
Yoann Ghigoff <yoann.ghigoff@datadoghq.com>
2023-10-27 14:50:19 +0200

    [CWS] fix rule engine/policy monitor data race (#20472)
    * [CWS] fix engine/policy monitor data race
    
    * relax lock release condition

--- pkg/security/rules/policy_monitor.go
+++ pkg/security/rules/policy_monitor.go
@@ -75,6 +75,19 @@ func (p *PolicyMonitor) SetPolicies(policies []*rules.Policy, mErrs *multierror.
 	}
 }
 
+// ReportHeartbeatEvent sends HeartbeatEvents reporting the current set of policies
+func (p *PolicyMonitor) ReportHeartbeatEvent(sender events.EventSender) {
+	log.Infof("send heartbeat")
+
+	p.RLock()
+	rule, events := NewHeartbeatEvents(p.policies)
+	p.RUnlock()
+
+	for _, event := range events {
+		sender.SendEvent(rule, event, nil, "")
+	}
+}
+
 // Start the monitor
 func (p *PolicyMonitor) Start(ctx context.Context) {
 	go func() {
@@ -145,16 +158,6 @@ func ReportRuleSetLoaded(sender events.EventSender, statsdClient statsd.ClientIn
 	}
 
 	sender.SendEvent(rule, event, nil, "")
-}
-
-// ReportHeartbeatEvent periodically reports to Datadog that
-func ReportHeartbeatEvent(sender events.EventSender, policies map[string]Policy) {
-	log.Infof("send heartbeat")
-
-	rule, events := NewHeartbeatEvents(policies)
-	for _, event := range events {
-		sender.SendEvent(rule, event, nil, "")
-	}
 }
 
 // RuleState defines a loaded rule
