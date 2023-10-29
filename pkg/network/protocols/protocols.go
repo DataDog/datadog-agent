0bff5f1081aa57586b5058f4b79c9192bf2d4bbe
Guy Arbitman <guy20495@gmail.com>
2023-10-29 11:40:06 +0200

    usm: protocols: Introduce a new method - IsBuildModeSupported. (#20488)
    The new method tells if the protocol is supported by a given protocol. This is preliminary change for migrating gotls into a protocol.
    All protocols besides go-tls are supported by all build modes. Currently the method is not being called, as we need to do a further change
    before calling this method

--- pkg/network/protocols/protocols.go
+++ pkg/network/protocols/protocols.go
@@ -14,6 +14,7 @@ import (
 	"github.com/cilium/ebpf"
 
 	"github.com/DataDog/datadog-agent/pkg/network/config"
+	"github.com/DataDog/datadog-agent/pkg/network/usm/buildmode"
 )
 
 // Programs maps used for tail calls
@@ -57,6 +58,9 @@ type Protocol interface {
 	// GetStats returns the latest monitoring stats from a protocol
 	// implementation.
 	GetStats() *ProtocolStats
+
+	// IsBuildModeSupported return true is the given build mode is supported by this protocol.
+	IsBuildModeSupported(buildmode.Type) bool
 }
 
 // ProtocolStats is a "tuple" struct that represents monitoring data from a
--- pkg/network/usm/ebpf_main.go
+++ pkg/network/usm/ebpf_main.go
@@ -30,6 +30,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/network/protocols/http"
 	errtelemetry "github.com/DataDog/datadog-agent/pkg/network/telemetry"
 	"github.com/DataDog/datadog-agent/pkg/network/tracer/offsetguess"
+	"github.com/DataDog/datadog-agent/pkg/network/usm/buildmode"
 	"github.com/DataDog/datadog-agent/pkg/network/usm/utils"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
@@ -61,7 +62,7 @@ type ebpfProgram struct {
 
 	// Used for connection_protocol data expiration
 	mapCleaner *ddebpf.MapCleaner
-	buildMode  buildMode
+	buildMode  buildmode.Type
 }
 
 type probeResolver interface {
@@ -90,20 +91,9 @@ type probeResolver interface {
 	GetAllUndefinedProbes() []manager.ProbeIdentificationPair
 }
 
-type buildMode string
-
-const (
-	// Prebuilt mode
-	Prebuilt buildMode = "prebuilt"
-	// RuntimeCompiled mode
-	RuntimeCompiled buildMode = "runtime-compilation"
-	// CORE mode
-	CORE buildMode = "CO-RE"
-)
-
 type subprogram interface {
 	Name() string
-	IsBuildModeSupported(buildMode) bool
+	IsBuildModeSupported(buildmode.Type) bool
 	ConfigureManager(*errtelemetry.Manager)
 	ConfigureOptions(*manager.Options)
 	Start()
@@ -184,7 +174,7 @@ func (e *ebpfProgram) Init() error {
 	if e.cfg.EnableCORE {
 		err = e.initCORE()
 		if err == nil {
-			e.buildMode = CORE
+			e.buildMode = buildmode.CORE
 			return nil
 		}
 
@@ -197,7 +187,7 @@ func (e *ebpfProgram) Init() error {
 	if e.cfg.EnableRuntimeCompiler || (err != nil && e.cfg.AllowRuntimeCompiledFallback) {
 		err = e.initRuntimeCompiler()
 		if err == nil {
-			e.buildMode = RuntimeCompiled
+			e.buildMode = buildmode.RuntimeCompiled
 			return nil
 		}
 
@@ -209,7 +199,7 @@ func (e *ebpfProgram) Init() error {
 
 	err = e.initPrebuilt()
 	if err == nil {
-		e.buildMode = Prebuilt
+		e.buildMode = buildmode.Prebuilt
 	}
 	return err
 }
