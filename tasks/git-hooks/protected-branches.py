fbaa9460e73419294f0a2982a91abec424f49887
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2023-10-31 09:09:32 +0100

    apply review comments

--- tasks/git-hooks/protected-branches.py
+++ tasks/git-hooks/protected-branches.py
@@ -8,14 +8,14 @@ import sys
 def main():
     try:
         local_branch = subprocess.check_output('git rev-parse --abbrev-ref HEAD', shell=True).decode('utf-8').strip()
-        print(local_branch)
         if local_branch == 'main':
             print("You're about to commit on main, are you sure this is what you want?")
             sys.exit(1)
         if re.fullmatch(r'^[0-9]+\.[0-9]+\.x$', local_branch):
             print("You're about to commit on a release branch, are you sure this is what you want?")
             sys.exit(1)
-    except OSError:
+    except OSError as e:
+        print(e)
         sys.exit(1)
 
 
