3d22da4d4384a04fa1b88057b3e8a98f95804909
Sylvain Baubeau <sylvain.baubeau@datadoghq.com>
2023-10-31 11:06:45 +0100

    [CWS] Add rule kill action (#20004)
    * Allow chaining commands in syscall-tester
    
    * New 'kill' action that allows sending a signal when a rule is triggered
    
    * Use a context.Context to run syscall_tester
    
    * Add TestKillAction functional test
    
    * Send signal from eBPF raw_syscalls tracepoint
    
    * Only use bpf_send_signal on supported kernels
    
    * Remove unused functions from syscall_tester
    
    * Add release note
    
    * Defaults to SIGTERM signal for 'kill' action
    
    * Fix testsuite bind mount in docker tests invoke task

--- pkg/security/secl/rules/policy.go
+++ pkg/security/secl/rules/policy.go
@@ -8,10 +8,11 @@ package rules
 
 import (
 	"fmt"
+	"io"
+
 	"github.com/DataDog/datadog-agent/pkg/security/secl/validators"
 	"github.com/hashicorp/go-multierror"
 	"gopkg.in/yaml.v2"
-	"io"
 )
 
 // PolicyDef represents a policy file definition
