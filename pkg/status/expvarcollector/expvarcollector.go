b77b1c3994978364390a32cdcd661f57af1d2c65
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-11-02 11:22:08 +0100

    fix lint issues

--- pkg/status/expvarcollector/expvarcollector.go
+++ pkg/status/expvarcollector/expvarcollector.go
@@ -3,14 +3,18 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package expvarcollector allows external pacjages to register the expvar that wants to report on for agent status command.
 package expvarcollector
 
 var expvarRegistry = map[string]func() (interface{}, error){}
 
+// RegisterExpvarReport allow components to register the function to collect
+// expvar variables.
 func RegisterExpvarReport(key string, report func() (interface{}, error)) {
 	expvarRegistry[key] = report
 }
 
+// Report iterates over the registered collect function and append the result into the stats map
 func Report(stats map[string]interface{}) (map[string]interface{}, []error) {
 	errors := []error{}
 	for key, report := range expvarRegistry {
