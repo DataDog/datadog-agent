5b762b324782a742664c06c7ef4d38cd26f8b4d3
Guy Arbitman <guy20495@gmail.com>
2023-11-02 09:27:02 +0200

    looking for max headers to filter in a single frame

--- pkg/network/ebpf/c/protocols/http2/decoding-defs.h
+++ pkg/network/ebpf/c/protocols/http2/decoding-defs.h
@@ -5,14 +5,14 @@
 
 #include "protocols/http2/defs.h"
 
-#define HTTP2_FRAMES_PER_TAIL_CALL 4
+#define HTTP2_FRAMES_PER_TAIL_CALL 1
 // Maximum number of frames to be processed in a single TCP packet. That's also the number of tail calls we'll have.
 // NOTE: we may need to revisit this const if we need to capture more connections.
 #define HTTP2_MAX_FRAMES_ITERATIONS 30
 #define HTTP2_MAX_FRAMES_TO_FILTER  100
 
 // A limit of max headers which we process in the request/response.
-#define HTTP2_MAX_HEADERS_COUNT_FOR_FILTERING 25
+#define HTTP2_MAX_HEADERS_COUNT_FOR_FILTERING 156
 
 // Per request or response we have fewer headers than HTTP2_MAX_HEADERS_COUNT_FOR_FILTERING that are interesting us.
 // For request - those are method, path, and soon to be content type. For response - status code.
