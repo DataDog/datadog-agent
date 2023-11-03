34dd14e4576be843c8568a561040208448e94cde
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-11-03 17:09:04 -0400

    add unixodbc as dependency

--- omnibus/config/software/msodbcsql18.rb
+++ omnibus/config/software/msodbcsql18.rb
@@ -1,6 +1,8 @@
 name "msodbcsql18"
 default_version "18.3.2.1-1"
 
+dependency "unixodbc"
+
 source url: "https://packages.microsoft.com/debian/12/prod/pool/main/m/msodbcsql18/#{name}_#{version}_amd64.deb"
 source sha256: "ae8eea58236e46c3f4eae05823cf7f0531ac58f12d90bc24245830b847c052ee"
 
