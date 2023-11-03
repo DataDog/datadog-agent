78ee5b46450f82c031937eb685bc2fde05263973
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-11-03 13:57:29 -0400

    add msodbc driver 18

---
+++ omnibus/config/software/msodbcsql18.rb
@@ -0,0 +1,14 @@
+name "msodbcsq18"
+default_version "18.3.2.1-1"
+
+source url: "https://packages.microsoft.com/debian/12/prod/pool/main/m/msodbcsql18/#{name}_#{version}_amd64.deb"
+
+relative_path "msodbcsql18-#{version}"
+
+build do
+    if debian_target? && !arm_target?
+        command "mkdir -p #{install_dir}/embedded/msodbcsql"
+        command "dpkg-deb -R #{project_dir}/#{name}_#{version}_amd64.deb #{project_dir}/#{relative_path}"
+        command "mv #{project_dir}/#{relative_path}/* #{install_dir}/embedded/msodbcsql18/"
+    end
+end
