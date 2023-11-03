acdc0db11cf71cc82e5d308083f4cd0f8c8dbe0a
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2023-11-03 15:05:38 +0100

    add ebpfless model

--- pkg/security/secl/model/model_unix.go
+++ pkg/security/secl/model/model_unix.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build unix
+//go:build unix && !ebpfless
 
 //go:generate go run github.com/DataDog/datadog-agent/pkg/security/secl/compiler/generators/accessors -tags "unix && !ebpfless" -types-file model.go -output accessors_unix.go -field-handlers field_handlers_unix.go -doc ../../../../docs/cloud-workload-security/secl.json -field-accessors-output field_accessors_unix.go
 
