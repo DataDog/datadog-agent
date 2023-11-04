4b8498e8d9b039eb7a6d93365d4c6268911a5533
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2023-11-04 13:14:57 +0100

    add minimal tests for cloudfoundry_container collector

---
+++ pkg/workloadmeta/collectors/internal/cloudfoundry/cf_container/cloudfoundry_container_test.go
@@ -0,0 +1,52 @@
+package cloudfoundry_container
+
+import (
+	"context"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/workloadmeta"
+	"github.com/stretchr/testify/assert"
+)
+
+type fakeWorkloadmetaStore struct {
+	workloadmeta.Store
+	notifiedEvents []workloadmeta.CollectorEvent
+}
+
+func (store *fakeWorkloadmetaStore) Notify(events []workloadmeta.CollectorEvent) {
+	store.notifiedEvents = append(store.notifiedEvents, events...)
+}
+
+func TestStart(t *testing.T) {
+
+	workloadmetaStore := fakeWorkloadmetaStore{}
+	c := collector{
+		store: &workloadmetaStore,
+	}
+
+	err := c.Start(context.TODO(), &workloadmetaStore)
+	assert.Error(t, err)
+}
+
+func TestPull(t *testing.T) {
+	workloadmetaStore := fakeWorkloadmetaStore{}
+	fakeNodeName := "fake-hostname"
+
+	c := collector{
+		store:    &workloadmetaStore,
+		nodeName: fakeNodeName,
+	}
+
+	err := c.Pull(context.TODO())
+	assert.NoError(t, err)
+	assert.NotEmpty(t, workloadmetaStore.notifiedEvents)
+
+	event0 := workloadmetaStore.notifiedEvents[0]
+
+	assert.Equal(t, event0.Type, workloadmeta.EventTypeSet)
+	assert.Equal(t, event0.Source, workloadmeta.SourceClusterOrchestrator)
+
+	containerEntity, ok := event0.Entity.(*workloadmeta.Container)
+	assert.True(t, ok)
+	assert.Equal(t, containerEntity.ID, fakeNodeName)
+}
