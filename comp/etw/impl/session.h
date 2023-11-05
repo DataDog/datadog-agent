423dcf0f771a87a0de919224b1a7fa624044123f
Julien Lebot <julien.lebot@datadoghq.com>
2023-11-05 23:24:54 +0100

    Move C code into C file

--- comp/etw/impl/session.h
+++ comp/etw/impl/session.h
@@ -9,28 +9,7 @@
 #define EVENT_FILTER_TYPE_EVENT_ID          (0x80000200)
 #define EVENT_FILTER_TYPE_PID               (0x80000004)
 
-// This constant defines the maximum number of filter types supported.
-#define MAX_FILTER_SUPPORTED                2
-
-extern void etwCallbackC(PEVENT_RECORD);
-
-static void WINAPI RecordEventCallback(PEVENT_RECORD event)
-{
-    etwCallbackC(event);
-}
-
-static TRACEHANDLE DDStartTracing(LPWSTR name, uintptr_t context)
-{
-    EVENT_TRACE_LOGFILEW trace = {0};
-    trace.LoggerName = name;
-    trace.Context = (void*)context;
-    trace.ProcessTraceMode = PROCESS_TRACE_MODE_REAL_TIME | PROCESS_TRACE_MODE_EVENT_RECORD;
-    trace.EventRecordCallback = RecordEventCallback;
-
-    return OpenTraceW(&trace);
-}
-
-static ULONG DDEnableTrace(
+ULONG DDEnableTrace(
     TRACEHANDLE TraceHandle,
     LPCGUID     ProviderId,
     ULONG       ControlCode,
@@ -40,32 +19,5 @@ static ULONG DDEnableTrace(
     ULONG       Timeout,
     ULONGLONG*  PIDs,
     ULONG       PIDCount
-)
-{
-    EVENT_FILTER_DESCRIPTOR eventFilterDescriptors[MAX_FILTER_SUPPORTED];
-
-    ENABLE_TRACE_PARAMETERS enableParameters = { 0 };
-    enableParameters.Version = ENABLE_TRACE_PARAMETERS_VERSION_2;
-    enableParameters.EnableFilterDesc = &eventFilterDescriptors[0];
-    enableParameters.FilterDescCount = 0;
-
-    if (PIDCount > 0)
-    {
-        eventFilterDescriptors[0].Ptr  = (ULONGLONG)PIDs;
-        eventFilterDescriptors[0].Size = (ULONG)(sizeof(PIDCount) * PIDCount);
-        eventFilterDescriptors[0].Type = EVENT_FILTER_TYPE_PID;
-
-        enableParameters.FilterDescCount++;
-    }
-
-    return EnableTraceEx2(
-        TraceHandle,
-        ProviderId,
-        ControlCode,
-        Level,
-        MatchAnyKeyword,
-        MatchAllKeyword,
-        Timeout,
-        &enableParameters
-    );
-}
+);
+TRACEHANDLE DDStartTracing(LPWSTR name, uintptr_t context);
