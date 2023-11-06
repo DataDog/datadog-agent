c90afdccb9ebe5a8563d302b25075d639421ae8d
goldsky4 <53126800+goldsky4@users.noreply.github.com>
2023-11-06 08:45:14 -0500

    Update kubernetes_state_transformers.go

--- pkg/collector/corechecks/cluster/ksm/kubernetes_state_transformers.go
+++ pkg/collector/corechecks/cluster/ksm/kubernetes_state_transformers.go
@@ -538,7 +538,7 @@ func serviceTypeTransformer(s sender.Sender, name string, metric ksmstore.DDMetr
 
 // removeSecretTransformer removes the secret tag from the kube_ingress_tls metric
 func removeSecretTransformer(s sender.Sender, _ string, metric ksmstore.DDMetric, hostname string, tags []string, _ time.Time) {
-	if len(tags)>0{
+	if len(tags)>0 {
 		for i, s := range tags {
 			if strings.Contains(s, "secret:") {
 				tags[i] = ""
