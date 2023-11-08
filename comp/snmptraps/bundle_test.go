6f9cef5eca0953a04f705090796441661c567001
Daniel Lepage <daniel.lepage@datadoghq.com>
2023-11-08 16:04:28 -0500

    Fix lint.

--- comp/snmptraps/bundle_test.go
+++ comp/snmptraps/bundle_test.go
@@ -11,29 +11,11 @@ import (
 	"github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/comp/core/hostname/hostnameimpl"
 	"github.com/DataDog/datadog-agent/comp/core/log"
-	trapsconfig "github.com/DataDog/datadog-agent/comp/snmptraps/config"
-	"github.com/DataDog/datadog-agent/comp/snmptraps/formatter"
-	"github.com/DataDog/datadog-agent/comp/snmptraps/forwarder"
-	"github.com/DataDog/datadog-agent/comp/snmptraps/listener"
-	"github.com/DataDog/datadog-agent/comp/snmptraps/oidresolver"
-	trapsserver "github.com/DataDog/datadog-agent/comp/snmptraps/server"
-	"github.com/DataDog/datadog-agent/comp/snmptraps/status"
 	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
 	"github.com/DataDog/datadog-agent/pkg/aggregator/sender"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"go.uber.org/fx"
 )
-
-type deps struct {
-	fx.In
-	Config    trapsconfig.Component
-	Formatter formatter.Component
-	Forwarder forwarder.Component
-	Listener  listener.Component
-	Resolver  oidresolver.Component
-	Server    trapsserver.Component
-	Status    status.Component
-}
 
 func TestBundleDependencies(t *testing.T) {
 	fxutil.TestBundle(t, Bundle,
