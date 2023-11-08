9f1bdb8a6f38dbcb3924be0f222eb3a9c9e5f378
Julien Lebot <julien.lebot@datadoghq.com>
2023-11-08 10:49:33 +0100

    Refactor integration logs sender to use a different field in the logs payload

--- comp/checks/winregistry/impl/integration_logs_registry_delegate.go
+++ comp/checks/winregistry/impl/integration_logs_registry_delegate.go
@@ -28,26 +28,41 @@ const (
 	keyDeleted = "key_deleted"
 )
 
-func (i *integrationLogsRegistryDelegate) sendLog(payload message.StructuredContent, status string) {
+// logEvent struct defines the extra attributes that are sent along with the log message
+type logEvent struct {
+	keyPath   string `yaml:"key_path"`
+	eventType string `yaml:"event_type"`
+
+	// process_id, process_name and thread_id will be coming in a future version.
+}
+
+type valueChangedLogEvent struct {
+	logEvent
+	oldValue interface{} `yaml:"old_value"`
+	newValue interface{} `yaml:"new_value"`
+}
+
+func (i *integrationLogsRegistryDelegate) sendLog(status string, payload message.BasicStructuredContent) {
 	if i.logsComponent == nil || i.muted {
 		return
 	}
-	msg := message.NewStructuredMessage(payload, i.origin, status, time.Now().UnixNano())
+	msg := message.NewStructuredMessage(&payload, i.origin, status, time.Now().UnixNano())
 	i.logsComponent.GetPipelineProvider().NextPipelineChan() <- msg
 }
 
-func getPayload(m, eventType string, regKeyCfg registryKey) message.BasicStructuredContent {
+type logEventType interface {
+	logEvent | valueChangedLogEvent
+}
+
+func getPayload[L logEventType](m string, logEventFn func(*L)) message.BasicStructuredContent {
 	payload := message.BasicStructuredContent{
 		Data: make(map[string]interface{}),
 	}
 	payload.Data["message"] = m
-	payload.Data["type"] = "windows_registry_event"
-	payload.Data["key_path"] = regKeyCfg.originalKeyPath
-	payload.Data["event_type"] = eventType
+	var logEvent L
+	logEventFn(&logEvent)
+	payload.Data[checkName] = logEvent
 
-	//payload.Data["process_id"] = coming in a future version
-	//payload.Data["process_name"] = coming in a future version
-	//payload.Data["thread_id"] = coming in a future version
 	return payload
 }
 
@@ -55,66 +70,60 @@ func getKeyName(valueName string, regKeyCfg registryKey) string {
 	return fmt.Sprintf("%s\\%s", regKeyCfg.originalKeyPath, valueName)
 }
 
-func (i *integrationLogsRegistryDelegate) processValue(valueName string, val interface{}, regKeyCfg registryKey) {
+func (i *integrationLogsRegistryDelegate) processValue(valueName, originalVal string, val interface{}, regKeyCfg registryKey) {
 	keyName := getKeyName(valueName, regKeyCfg)
 	cachedVal, ok := i.valueMap[keyName]
+	var log, eventType string
+
 	if !ok {
-		p := getPayload(fmt.Sprintf("value %s = '%v'", keyName, val), keyCreated, regKeyCfg)
-		p.Data["old_value"] = nil
-		p.Data["new_value"] = val
-		i.sendLog(&p, "info")
-		i.valueMap[keyName] = val
-	} else {
-		if cachedVal != val {
-			p := getPayload(fmt.Sprintf("value %s changed from '%v' to '%v'", keyName, cachedVal, val), keyChanged, regKeyCfg)
-			p.Data["old_value"] = cachedVal
-			p.Data["new_value"] = val
-			i.sendLog(&p, "info")
-			i.valueMap[keyName] = val
+		eventType = keyCreated
+		if originalVal != "" {
+			log = fmt.Sprintf("value %s = '%v' ('%v')", keyName, val, originalVal)
+		} else {
+			log = fmt.Sprintf("value %s = '%v'", keyName, val)
 		}
+	} else if cachedVal != val {
+		eventType = keyChanged
+		if originalVal != "" {
+			log = fmt.Sprintf("value %s changed from '%v' to '%v' ('%v')", keyName, cachedVal, val, originalVal)
+		} else {
+			log = fmt.Sprintf("value %s changed from '%v' to '%v'", keyName, cachedVal, val)
+		}
+	}
+
+	if !ok || cachedVal != val {
+		i.sendLog("info", getPayload[valueChangedLogEvent](log, func(e *valueChangedLogEvent) {
+			e.keyPath = keyName
+			e.eventType = eventType
+			e.oldValue = nil
+			e.newValue = val
+		}))
+		i.valueMap[keyName] = val
 	}
 }
 
 func (i *integrationLogsRegistryDelegate) onSendNumber(valueName string, val float64, regKeyCfg registryKey, _ registryValueCfg) {
-	i.processValue(valueName, val, regKeyCfg)
+	i.processValue(valueName, "", val, regKeyCfg)
 }
 
 func (i *integrationLogsRegistryDelegate) onSendMappedNumber(valueName string, originalVal string, mappedVal float64, regKeyCfg registryKey, _ registryValueCfg) {
-	keyName := getKeyName(valueName, regKeyCfg)
-	cachedVal, ok := i.valueMap[keyName]
-	if !ok {
-		p := getPayload(fmt.Sprintf("value %s = '%v' ('%v')", keyName, mappedVal, originalVal), keyCreated, regKeyCfg)
-		p.Data["old_value"] = nil
-		p.Data["new_value"] = mappedVal
-		i.sendLog(&p, "info")
-		i.valueMap[keyName] = mappedVal
-	} else {
-		if cachedVal != mappedVal {
-			p := getPayload(fmt.Sprintf("value %s changed from '%v' to '%v' ('%v')", keyName, cachedVal, mappedVal, originalVal), keyChanged, regKeyCfg)
-			p.Data["old_value"] = cachedVal
-			p.Data["new_value"] = mappedVal
-			i.sendLog(&p, "info")
-			i.valueMap[keyName] = mappedVal
-		}
-	}
+	i.processValue(valueName, originalVal, mappedVal, regKeyCfg)
 }
 
 func (i *integrationLogsRegistryDelegate) onNoMappingFound(valueName string, val string, regKeyCfg registryKey, _ registryValueCfg) {
-	i.processValue(valueName, val, regKeyCfg)
+	i.processValue(valueName, "", val, regKeyCfg)
 }
 
 func (i *integrationLogsRegistryDelegate) onMissing(valueName string, regKeyCfg registryKey, _ registryValueCfg, _ error) {
-	if i.logsComponent == nil {
-		return
-	}
-
 	keyName := getKeyName(valueName, regKeyCfg)
 	cachedVal, ok := i.valueMap[keyName]
 	if ok {
-		p := getPayload(fmt.Sprintf("value %s ('%v') was deleted", keyName, cachedVal), keyDeleted, regKeyCfg)
-		p.Data["old_value"] = cachedVal
-		p.Data["new_value"] = nil
-		i.sendLog(&p, "info")
+		i.sendLog("info", getPayload[valueChangedLogEvent](fmt.Sprintf("value %s ('%v') was deleted", keyName, cachedVal), func(e *valueChangedLogEvent) {
+			e.keyPath = keyName
+			e.eventType = keyDeleted
+			e.oldValue = cachedVal
+			e.newValue = nil
+		}))
 		delete(i.valueMap, keyName)
 	}
 }
