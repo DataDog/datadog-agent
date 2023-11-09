16bffeb1ba5a581038503fa290a25551d36185a9
AliDatadog <ali.benabdallah@datadoghq.com>
2023-11-09 16:44:21 +0100

    remove clusterchecks build tag from process-agent and system probe

--- tasks/build_tags.py
+++ tasks/build_tags.py
@@ -108,7 +108,7 @@ DOGSTATSD_TAGS = {"containerd", "docker", "kubelet", "podman", "secrets", "zlib"
 IOT_AGENT_TAGS = {"jetson", "otlp", "systemd", "zlib"}
 
 # PROCESS_AGENT_TAGS lists the tags necessary to build the process-agent
-PROCESS_AGENT_TAGS = AGENT_TAGS.union({"clusterchecks", "fargateprocess", "orchestrator"}).difference(
+PROCESS_AGENT_TAGS = AGENT_TAGS.union({"fargateprocess", "orchestrator"}).difference(
     {"otlp", "python", "trivy"}
 )
 
@@ -124,7 +124,7 @@ SECURITY_AGENT_TAGS = {"netcgo", "secrets", "docker", "containerd", "kubeapiserv
 SERVERLESS_TAGS = {"serverless", "otlp"}
 
 # SYSTEM_PROBE_TAGS lists the tags necessary to build system-probe
-SYSTEM_PROBE_TAGS = AGENT_TAGS.union({"clusterchecks", "linux_bpf", "npm"}).difference({"python", "systemd"})
+SYSTEM_PROBE_TAGS = AGENT_TAGS.union({"linux_bpf", "npm"}).difference({"python", "systemd"})
 
 # TRACE_AGENT_TAGS lists the tags that have to be added when the trace-agent
 TRACE_AGENT_TAGS = {"docker", "containerd", "kubeapiserver", "kubelet", "otlp", "netcgo", "podman", "secrets"}
