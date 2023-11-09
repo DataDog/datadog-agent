3354cca245eeb8cd37d91caba98ddc672969736b
Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>
2023-11-09 15:02:01 +0100

    feat: create api server minimal API
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>

---
+++ comp/core/api/apiimpl/api.go
@@ -0,0 +1,27 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023-present Datadog, Inc.
+
+// Package apiimpl ... /* TODO: detailed doc comment for the component */
+package apiimpl
+
+import (
+	"go.uber.org/fx"
+
+	"github.com/DataDog/datadog-agent/comp/core/api"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+)
+
+// Module defines the fx options for this component.
+var Module = fxutil.Component(
+	fx.Provide(newAPIServer),
+)
+
+type apiServer struct{}
+
+var _ api.Component = (*apiServer)(nil)
+
+func newAPIServer() api.Component {
+	return &apiServer{}
+}
