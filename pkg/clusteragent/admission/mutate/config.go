38122c8f4045afe2bd45f2803644115557ec441d
Liliya Belaus <liliya.belaus@datadoghq.com>
2023-11-10 12:36:01 -0500

    Fix lint issues

--- pkg/clusteragent/admission/mutate/config.go
+++ pkg/clusteragent/admission/mutate/config.go
@@ -35,7 +35,7 @@ const (
 	dogstatsdURLEnvVarName               = "DD_DOGSTATSD_URL"
 	instrumentationInstallTypeEnvVarName = "DD_INSTRUMENTATION_INSTALL_TYPE"
 	instrumentationInstallTimeEnvVarName = "DD_INSTRUMENTATION_INSTALL_TIME"
-	instrumentationInstallIdEnvVarName   = "DD_INSTRUMENTATION_INSTALL_ID"
+	instrumentationInstallIDEnvVarName   = "DD_INSTRUMENTATION_INSTALL_ID"
 
 	// Config injection modes
 	hostIP  = "hostip"
@@ -142,7 +142,7 @@ func injectConfig(pod *corev1.Pod, _ string, _ dynamic.Interface) error {
 
 	injectedEntity = injectEnv(pod, ddEntityIDEnvVar)
 
-	// Inject env variables used for Onboarding KPIs propogation
+	// Inject env variables used for Onboarding KPIs propagation
 	if isApmInstrumentationEnabled(pod.Namespace) {
 		// if Single Step Instrumentation is enabled, inject DD_INSTRUMENTATION_INSTALL_TYPE:k8s_single_step
 		_ = injectEnv(pod, singleStepInstrumentationInstallTypeEnvVar)
@@ -154,8 +154,8 @@ func injectConfig(pod *corev1.Pod, _ string, _ dynamic.Interface) error {
 	_ = injectEnv(pod, instrumentationInstallTimeEnvVar)
 	// inject DD_INSTRUMENTATION_INSTALL_ID with UUID created during the Agent install time
 	instrumentationInstallIdEnvVar := corev1.EnvVar{
-		Name:  instrumentationInstallIdEnvVarName,
-		Value: os.Getenv(instrumentationInstallIdEnvVarName),
+		Name:  instrumentationInstallIDEnvVarName,
+		Value: os.Getenv(instrumentationInstallIDEnvVarName),
 	}
 	_ = injectEnv(pod, instrumentationInstallIdEnvVar)
 
