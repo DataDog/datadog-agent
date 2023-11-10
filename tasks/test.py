eea9e1ab8dcd88d5d904d24c91e848f753f84cce
Alexandre Menasria <47357713+amenasria@users.noreply.github.com>
2023-11-10 14:41:25 +0100

    [CI] Remove `--skip-linters` flag from the test invoke task (#20568)

--- tasks/test.py
+++ tasks/test.py
@@ -512,6 +512,24 @@ def process_module_results(module_results: Dict[str, Dict[str, List[ModuleResult
     return success
 
 
+def deprecating_skip_linters_flag(skip_linters):
+    """
+    We're deprecating the --skip-linters flag in the test invoke task
+
+    Displays a warning when user is running inv -e test --skip-linters
+    Also displays the command the user should run to
+    """
+    if skip_linters:
+        deprecation_msg = """Warning: the --skip-linters is deprecated for the test invoke task.
+Feel free to remove the flag when running inv -e test.
+"""
+    else:
+        deprecation_msg = """Warning: the linters were removed from the test invoke task.
+If you want to run the linters, please run inv -e lint-go instead.
+"""
+    print(deprecation_msg, file=sys.stderr)
+
+
 def sanitize_env_vars():
     """
     Sanitizes environment variables
@@ -562,8 +580,6 @@ def test(
 
     If no module or target is set the tests are run against all modules and targets.
 
-    Also runs linters on the same modules / targets, except if the --skip-linters option is passed.
-
     Example invokation:
         inv test --targets=./pkg/collector/check,./pkg/aggregator --race
         inv test --module=. --race
@@ -573,20 +589,7 @@ def test(
 
     sanitize_env_vars()
 
-    # Run linters first
-
-    if not skip_linters:
-        modules_results_per_phase["lint"] = run_lint_go(
-            ctx=ctx,
-            module=module,
-            targets=targets,
-            flavors=flavors,
-            build_include=build_include,
-            build_exclude=build_exclude,
-            rtloader_root=rtloader_root,
-            arch=arch,
-            cpus=cpus,
-        )
+    deprecating_skip_linters_flag(skip_linters)
 
     modules, flavors = process_input_args(module, targets, flavors)
 
@@ -697,10 +700,7 @@ def test(
     success = process_module_results(modules_results_per_phase)
 
     if success:
-        if skip_linters:
-            print(color_message("All tests passed", "green"))
-        else:
-            print(color_message("All tests and linters passed", "green"))
+        print(color_message("All tests passed", "green"))
     else:
         # Exit if any of the modules failed on any phase
         raise Exit(code=1)
