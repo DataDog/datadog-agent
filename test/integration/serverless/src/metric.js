ed54b08136335d64416cf282039870951cb91f1f
Romain Marcadier <romain@datadoghq.com>
2023-11-10 18:09:44 +0100

    fix: appsec-node serverless integration test failure
    Somehow this is not registering a trace anymore. Trying to set it to use
    the same function code as the `trace-node` test, as this appears to be
    reliably getting a trace registered.

--- test/integration/serverless/src/metric.js
+++ test/integration/serverless/src/metric.js
@@ -13,16 +13,6 @@ async function myHandler(event, context) {
   };
 }
 
-async function appSecHandler(event, context) {
-  return {
-    statusCode: 200,
-    body: 'ok',
-    headers: {
-      'Content-Encoding': 'text/plain',
-    },
-  };
-}
-
 async function myTimeoutHandler(event, context) {
   if (shouldSendMetric) {
     sendDistributionMetric("serverless.lambda-extension.integration-test.count", 1);
