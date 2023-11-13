9b5f51a9d41f2ffa5849ed929877f4515abde41c
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-11-13 12:01:00 -0500

    build for arm

--- omnibus/config/software/msodbcsql18.rb
+++ omnibus/config/software/msodbcsql18.rb
@@ -3,30 +3,54 @@ default_version "18.3.2.1-1"
 
 dependency "unixodbc"
 
+skip_transitive_dependency_licensing true
+ship_source_offer true
+
 # Dynamically build source url and sha256 based on the linux platform and architecture
 # The source url and sha256 are taken from the official Microsoft ODBC Driver for SQL Server page:
-
 if debian_target?
-  source url: "https://packages.microsoft.com/debian/12/prod/pool/main/m/msodbcsql18/#{name}_#{version}_amd64.deb"
-  source sha256: "ae8eea58236e46c3f4eae05823cf7f0531ac58f12d90bc24245830b847c052ee"
+  if arm_target?
+    # Debian ARM64 build
+    package_name = "#{name}_#{version}_arm64.deb"
+    source url: "https://packages.microsoft.com/debian/12/prod/pool/main/m/msodbcsql18/#{package_name}"
+    source sha256: "d9bb2d2e165e9d86f6b5de75b2baa24c9f0f25107471bcf82254d27f5dafff30"
+  else
+    # Debian AMD64 build
+    package_name = "#{name}_#{version}_amd64.deb"
+    source url: "https://packages.microsoft.com/debian/12/prod/pool/main/m/msodbcsql18/#{package_name}"
+    source sha256: "ae8eea58236e46c3f4eae05823cf7f0531ac58f12d90bc24245830b847c052ee"
+  end
 elsif redhat_target?
-  source url: "https://packages.microsoft.com/rhel/8/prod/Packages/m/#{name}-#{version}.x86_64.rpm"
-  source sha256: "ecd8e148138ee72a452a5357e380580e2c19219c5424f4ac9350cbdf3217fad1"
+  if arm_target?
+    # RHEL ARM64 build
+    package_name = "#{name}-#{version}.aarch64.rpm"
+    source url: "https://packages.microsoft.com/rhel/8/prod/Packages/m/#{package_name}"
+    source sha256: "27746644aaf1e681ae3e721d8d292b74bbe3fc046292873a816cbda8f4fc4309"
+  else
+    # RHEL AMD64 build
+    package_name = "#{name}-#{version}.x86_64.rpm"
+    source url: "https://packages.microsoft.com/rhel/8/prod/Packages/m/#{package_name}"
+    source sha256: "ecd8e148138ee72a452a5357e380580e2c19219c5424f4ac9350cbdf3217fad1"
+  end
 else
-  source url: "https://packages.microsoft.com/sles/15/prod/Packages/m/#{name}-#{version}.x86_64.rpm"
+  # SUSE AMD64 build (ARM64 not supported)
+  package_name = "#{name}-#{version}.x86_64.rpm"
+  source url: "https://packages.microsoft.com/sles/15/prod/Packages/m/#{package_name}"
   source sha256: "e8b753f8730681d9308f5e3e9e2bde4e169d5e598e322a7d6e31860b73af55e6"
 end
 
 relative_path "msodbcsql18-#{version}"
 
 build do
-  if !arm_target?
+  # Support for ARM64 is only available on Debian and RHEL. SUSE is not supported.
+  # ARMv7 is not supported on any platform.
+  if !(arm_target? && suse_target?) && !arm7l_target?
     command "mkdir -p #{project_dir}/#{relative_path}"
     command "mkdir -p #{install_dir}/embedded/msodbcsql/lib"
     if debian_target?
-      command "dpkg-deb -R #{project_dir}/#{name}-#{name}_#{version}_amd64.deb #{project_dir}/#{relative_path}"
+      command "dpkg-deb -R #{project_dir}/#{name}-#{package_name} #{project_dir}/#{relative_path}"
     else
-      command "rpm2cpio #{project_dir}/#{name}-#{name}-#{version}.x86_64.rpm | (cd #{project_dir}/#{relative_path} && cpio -idmv)"
+      command "rpm2cpio #{project_dir}/#{name}-#{package_name} | (cd #{project_dir}/#{relative_path} && cpio -idmv)"
     end
     # Fix rpath first
     command "patchelf --force-rpath --set-rpath '#{install_dir}/embedded/lib' '#{project_dir}/#{relative_path}/opt/microsoft/msodbcsql18/lib64/libmsodbcsql-18.3.so.2.1'"
