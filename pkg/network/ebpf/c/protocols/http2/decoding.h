025ab444b0ed00a5a6c912c0001acca44568caf2
amit.slavin <amit.slavin@datadoghq.com>
2023-11-15 13:39:35 +0200

    increased HTTP2_MAX_FRAMES_ITERATIONS from 40 to 60

--- pkg/network/ebpf/c/protocols/http2/decoding.h
+++ pkg/network/ebpf/c/protocols/http2/decoding.h
@@ -545,15 +545,12 @@ static __always_inline __u8 find_relevant_headers(struct __sk_buff *skb, skb_inf
         // https://datatracker.ietf.org/doc/html/rfc7540#section-6.2 for headers frame.
         is_headers_or_rst_frame = current_frame.type == kHeadersFrame || current_frame.type == kRSTStreamFrame;
         is_data_end_of_stream = ((current_frame.flags & HTTP2_END_OF_STREAM) == HTTP2_END_OF_STREAM) && (current_frame.type == kDataFrame);
-        if (is_headers_or_rst_frame || is_data_end_of_stream) {
-            if (interesting_frame_index < HTTP2_MAX_FRAMES_ITERATIONS) {
-                frames_array[interesting_frame_index].frame = current_frame;
-                frames_array[interesting_frame_index].offset = skb_info->data_off;
-                interesting_frame_index++;
-            } else {
-                passed_max_interesting_frames = true;
-            }
+        if (interesting_frame_index < HTTP2_MAX_FRAMES_ITERATIONS && (is_headers_or_rst_frame || is_data_end_of_stream)) {
+            frames_array[interesting_frame_index].frame = current_frame;
+            frames_array[interesting_frame_index].offset = skb_info->data_off;
+            interesting_frame_index++;
         }
+        passed_max_interesting_frames |= ((is_headers_or_rst_frame || is_data_end_of_stream) && interesting_frame_index >= HTTP2_MAX_FRAMES_ITERATIONS);
         skb_info->data_off += current_frame.length;
     }
 
