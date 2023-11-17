01963766e7c625c58b63db1017e44c25d0e3c5a8
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2023-11-17 17:24:38 +0100

    [side-scanner] Avoid recursion in ebsfile

--- cmd/side-scanner/trivy_ebsfile.go
+++ cmd/side-scanner/trivy_ebsfile.go
@@ -12,17 +12,18 @@ type EBSClientWithWalk struct {
 }
 
 func (e EBSClientWithWalk) WalkSnapshotBlocks(ctx context.Context, input *ebs.ListSnapshotBlocksInput, table map[int32]string) (*ebs.ListSnapshotBlocksOutput, map[int32]string, error) {
-	output, err := e.ListSnapshotBlocks(ctx, input)
-	if err != nil {
-		return nil, nil, err
-	}
-	for _, block := range output.Blocks {
-		table[*block.BlockIndex] = *block.BlockToken
-	}
-	if output.NextToken != nil {
+	for {
+		output, err := e.ListSnapshotBlocks(ctx, input)
+		if err != nil {
+			return nil, nil, err
+		}
+		for _, block := range output.Blocks {
+			table[*block.BlockIndex] = *block.BlockToken
+		}
+		output.VolumeSize = aws.Int64(*output.VolumeSize << 30)
+		if output.NextToken == nil {
+			return output, table, nil
+		}
 		input.NextToken = output.NextToken
-		return e.WalkSnapshotBlocks(ctx, input, table)
 	}
-	output.VolumeSize = aws.Int64(*output.VolumeSize << 30)
-	return output, table, nil
 }
