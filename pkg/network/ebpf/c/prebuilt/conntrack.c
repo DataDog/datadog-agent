b591266430e6193f1306a8dbaa50403bd473ae33
Adam Karpowich <adam.karpowich@datadoghq.com>
2023-11-20 16:33:04 -0500

    remove use of status in fill_info

--- pkg/network/ebpf/c/prebuilt/conntrack.c
+++ pkg/network/ebpf/c/prebuilt/conntrack.c
@@ -44,12 +44,7 @@ int kprobe_ctnetlink_fill_info(struct pt_regs* ctx) {
 
     struct nf_conn *ct = (struct nf_conn*)PT_REGS_PARM5(ctx);
 
-    u32 status = ct_status(ct);
-    if (!(status&IPS_CONFIRMED) || !(status&IPS_NAT_MASK)) {
-        return 0;
-    }
-
-    log_debug("kprobe/ctnetlink_fill_info: netns: %u, status: %x", get_netns(ct), status);
+    log_debug("kprobe/ctnetlink_fill_info: netns: %u", get_netns(ct));
 
     conntrack_tuple_t orig = {}, reply = {};
     if (nf_conn_to_conntrack_tuples(ct, &orig, &reply) != 0) {
