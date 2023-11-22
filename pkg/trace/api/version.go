631af562b65136c49c5cbacf46df17bf5efa3719
Francisco Paulo <123977764+fpddog@users.noreply.github.com>
2023-11-22 14:58:43 +0000

    Updating v07 payload references as well.

--- pkg/trace/api/version.go
+++ pkg/trace/api/version.go
@@ -113,7 +113,7 @@ const (
 	//
 	// Request: Tracer Payload.
 	// 	Content-Type: application/msgpack
-	// 	Payload: TracerPayload (pkg/trace/pb/tracer_payload.proto)
+	// 	Payload: TracerPayload (pkg/proto/datadog/trace/tracer_payload.proto)
 	//
 	// Response: Service sampling rates (see description in v04).
 	//
