40990effc3bc634c40c8417430e2756fa0449b17
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-11-22 11:45:15 +0100

    Update invoke task for agent version tests

--- tasks/new_e2e_tests.py
+++ tasks/new_e2e_tests.py
@@ -46,6 +46,7 @@ def run(
     platform="",
     arch="",
     flavor="",
+    major_version="",
     cws_supported_osversion="",
     keep_stacks=False,
     cache=False,
@@ -92,7 +93,7 @@ def run(
         test_run_arg = f"-run {test_run_name}"
 
     cmd = f'gotestsum --format {gotestsum_format} '
-    cmd += '{junit_file_flag} --packages="{packages}" -- -ldflags="-X {REPO_PATH}/test/new-e2e/tests/containers.GitCommit={commit}" {verbose} -mod={go_mod} -vet=off -timeout {timeout} -tags {go_build_tags} {nocache} {run} {skip} {coverage_opt} {test_run_arg} -args {osversion} {platform} {arch} {flavor} {cws_supported_osversion} {keep_stacks}'
+    cmd += '{junit_file_flag} --packages="{packages}" -- -ldflags="-X {REPO_PATH}/test/new-e2e/tests/containers.GitCommit={commit}" {verbose} -mod={go_mod} -vet=off -timeout {timeout} -tags {go_build_tags} {nocache} {run} {skip} {coverage_opt} {test_run_arg} -args {osversion} {platform} {major_version} {arch} {flavor} {cws_supported_osversion} {keep_stacks}'
 
     args = {
         "go_mod": "mod",
@@ -109,6 +110,7 @@ def run(
         "platform": f"-platform {platform}" if platform else '',
         "arch": f"-arch {arch}" if arch else '',
         "flavor": f"-flavor {flavor}" if flavor else '',
+        "major_version": f"-major-version {major_version}" if major_version else '',
         "cws_supported_osversion": f"-cws-supported-osversion {cws_supported_osversion}"
         if cws_supported_osversion
         else '',
