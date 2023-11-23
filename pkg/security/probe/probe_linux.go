b58ec432a89113b39c02d1f186afb57ef005da7e
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2023-11-23 10:03:05 +0100

    add common probe

---
+++ pkg/security/probe/probe_linux.go
@@ -0,0 +1,43 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package probe holds probe related files
+package probe
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/security/config"
+)
+
+// NewProbe instantiates a new runtime security agent probe
+func NewProbe(config *config.Config, opts Opts) (*Probe, error) {
+	opts.normalize()
+
+	p := &Probe{
+		Opts:         opts,
+		Config:       config,
+		StatsdClient: opts.StatsdClient,
+	}
+
+	if opts.EBPFLessEnabled {
+		pp, err := NewEBPFLessProbe(p, config, opts)
+		if err != nil {
+			return nil, err
+		}
+		p.PlatformProbe = pp
+	} else {
+		pp, err := NewEBPFProbe(p, config, opts)
+		if err != nil {
+			return nil, err
+		}
+		p.PlatformProbe = pp
+	}
+
+	p.event = p.PlatformProbe.NewEvent()
+
+	// be sure to zero the probe event before everything else
+	p.zeroEvent()
+
+	return p, nil
+}
