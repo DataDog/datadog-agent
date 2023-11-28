ba9be00cf54cb7fc4a5c848150a86f044693e4c1
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-11-28 15:20:26 +0100

    allow system probe to build and load config on darwin

--- cmd/system-probe/config/config.go
+++ cmd/system-probe/config/config.go
@@ -48,11 +48,6 @@ func New(configPath string) (*types.Config, error) {
 }
 
 func newSysprobeConfig(configPath string) (*types.Config, error) {
-	// System probe is not supported on darwin, so we should fail gracefully in this case.
-	if runtime.GOOS == "darwin" {
-		return &types.Config{}, nil
-	}
-
 	aconfig.SystemProbe.SetConfigName("system-probe")
 	// set the paths where a config file is expected
 	if len(configPath) != 0 {
