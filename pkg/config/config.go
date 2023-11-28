69606a1c7172dd25f8e53e10fa0483c16beaace8
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2023-11-28 10:51:33 +0100

    disable new endpoint by default

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -248,7 +248,7 @@ func InitConfig(config Config) {
 	config.BindEnvAndSetDefault("cmd_host", "localhost")
 	config.BindEnvAndSetDefault("cmd_port", 5001)
 	config.BindEnvAndSetDefault("agent_ipc_host", "localhost")
-	config.BindEnvAndSetDefault("agent_ipc_port", 5004)
+	config.BindEnvAndSetDefault("agent_ipc_port", 0)
 	config.BindEnvAndSetDefault("default_integration_http_timeout", 9)
 	config.BindEnvAndSetDefault("integration_tracing", false)
 	config.BindEnvAndSetDefault("integration_tracing_exhaustive", false)
