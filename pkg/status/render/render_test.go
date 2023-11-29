8155a18be67570e934bdf0bfaee6ed8f566bc521
Gustavo Caso <gustavo.caso@datadoghq.com>
2023-11-29 12:52:48 +0100

    add test for process agent status output

--- pkg/status/render/render_test.go
+++ pkg/status/render/render_test.go
@@ -106,3 +106,34 @@ func TestFormatSecurityAgentStatus(t *testing.T) {
 		assert.NotContains(t, actual, statusRenderErrors)
 	})
 }
+
+func TestFormatProcessAgentStatus(t *testing.T) {
+	originalTZ := os.Getenv("TZ")
+	os.Setenv("TZ", "UTC")
+	defer func() {
+		os.Setenv("TZ", originalTZ)
+	}()
+	agentJSON, err := os.ReadFile("fixtures/process_agent_status.json")
+	require.NoError(t, err)
+	agentText, err := os.ReadFile("fixtures/process_agent_status.text")
+	require.NoError(t, err)
+	const statusRenderErrors = "Status render errors"
+
+	t.Run("render errors", func(t *testing.T) {
+		actual, err := FormatProcessAgentStatus([]byte{})
+		require.NoError(t, err)
+		assert.Contains(t, actual, statusRenderErrors)
+	})
+
+	t.Run("no render errors", func(t *testing.T) {
+		actual, err := FormatProcessAgentStatus(agentJSON)
+		require.NoError(t, err)
+
+		// We replace windows line break by linux so the tests pass on every OS
+		result := strings.Replace(string(agentText), "\r\n", "\n", -1)
+		actual = strings.Replace(actual, "\r\n", "\n", -1)
+
+		assert.Equal(t, actual, result)
+		assert.NotContains(t, actual, statusRenderErrors)
+	})
+}
