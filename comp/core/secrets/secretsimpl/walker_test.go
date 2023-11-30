06c6bce960cdbe3b06da0157ffb441afef9ac863
Maxime mouial <maxime.mouial@gmail.com>
2023-11-30 18:19:15 +0100

    Fix secrets resolution with a callback
    When resolving secrets from the datadog configuration we only want to
    update known settings.

--- comp/core/secrets/secretsimpl/walker_test.go
+++ comp/core/secrets/secretsimpl/walker_test.go
@@ -16,6 +16,20 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
+var yamlConf = []byte(`
+slice:
+  - "ENC 1"
+  - ["ENC test1", test2]
+  - 123
+hash:
+  a: test3
+  b: "ENC 2"
+  c: 456
+  slice:
+    - ENC test4
+    - test5
+`)
+
 func TestWalkerError(t *testing.T) {
 	var config interface{}
 	err := yaml.Unmarshal(testYamlHash, &config)
@@ -29,29 +43,6 @@ func TestWalkerError(t *testing.T) {
 
 	err = w.walk(&config)
 	assert.NotNil(t, err)
-}
-
-func TestWalkerSimple(t *testing.T) {
-	var config interface{}
-	err := yaml.Unmarshal([]byte("test"), &config)
-	require.NoError(t, err)
-
-	stringsCollected := []string{}
-
-	w := walker{
-		resolver: func(_ []string, str string) (string, error) {
-			stringsCollected = append(stringsCollected, str)
-			return str + "_verified", nil
-		},
-	}
-	err = w.walk(&config)
-	require.NoError(t, err)
-
-	assert.Equal(t, []string{"test"}, stringsCollected)
-
-	updatedConf, err := yaml.Marshal(config)
-	require.NoError(t, err)
-	assert.Equal(t, string("test_verified\n"), string(updatedConf))
 }
 
 func TestWalkerComplex(t *testing.T) {
@@ -86,20 +77,6 @@ func TestWalkerComplex(t *testing.T) {
 }
 
 func TestWalkerNotify(t *testing.T) {
-	yamlConf := []byte(`
-slice:
-  - "ENC 1"
-  - ["ENC test1", test2]
-  - 123
-hash:
-  a: test3
-  b: "ENC 2"
-  c: 456
-  slice:
-    - ENC test4
-    - test5
-`)
-
 	notification := map[string]any{}
 	w := walker{
 		resolver: func(_ []string, value string) (string, error) {
@@ -108,8 +85,9 @@ hash:
 			}
 			return value, nil
 		},
-		notifier: func(yamlPath []string, value any) {
+		notifier: func(yamlPath []string, value any) bool {
 			notification[strings.Join(yamlPath, ".")] = value
+			return true
 		},
 	}
 
@@ -131,4 +109,48 @@ hash:
 		},
 	}
 	assert.Equal(t, expected, notification)
+}
+
+func TestWalkerNotifyReset(t *testing.T) {
+	w := walker{
+		resolver: func(_ []string, value string) (string, error) {
+			if strings.HasPrefix(value, "ENC ") {
+				return value[4:] + "_verified", nil
+			}
+			return value, nil
+		},
+		notifier: func(yamlPath []string, value any) bool {
+			// always refuse
+			return false
+		},
+	}
+
+	var config interface{}
+	err := yaml.Unmarshal(yamlConf, &config)
+	require.NoError(t, err)
+
+	err = w.walk(&config)
+	require.NoError(t, err)
+
+	// checking if the internal state has been reset in case every single notification has been refused
+	assert.False(t, w.notificationPending)
+}
+
+func TestWalkerNoNotifyOnError(t *testing.T) {
+	w := walker{
+		resolver: func(_ []string, value string) (string, error) {
+			return "", fmt.Errorf("some error")
+		},
+		notifier: func(yamlPath []string, value any) bool {
+			assert.Fail(t, "no notification should be sent upon error from the resolver")
+			return false
+		},
+	}
+
+	var config interface{}
+	err := yaml.Unmarshal(yamlConf, &config)
+	require.NoError(t, err)
+
+	err = w.walk(&config)
+	require.Error(t, err)
 }
