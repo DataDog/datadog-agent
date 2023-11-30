3cfbd46eed3e056e31aab55a7de2130001f7443c
Duncan Harvey <duncan.harvey@datadoghq.com>
2023-11-30 16:18:03 -0500

    [SVLS-4179] Combines unit tests into table test

--- cmd/serverless/main_test.go
+++ cmd/serverless/main_test.go
@@ -18,48 +18,37 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/serverless/daemon"
 )
 
-func TestDaemonStopOnSIGINT(t *testing.T) {
+func TestDaemonStopOnTerminationSignals(t *testing.T) {
 	stopCh := make(chan struct{})
-	signalCh := make(chan os.Signal, 1)
-
 	serverlessDaemon := daemon.StartDaemon("http://localhost:8124")
-	go handleTerminationSignals(serverlessDaemon, stopCh, func(c chan<- os.Signal, sig ...os.Signal) {
-		c <- syscall.SIGINT
-	})
 
-	signalCh <- syscall.SIGINT
+	testCases := []struct {
+		name   string
+		signal syscall.Signal
+	}{
+		{
+			name:   "WaitForStopOnSIGINT",
+			signal: syscall.SIGINT,
+		},
+		{
+			name:   "WaitForStopOnSIGTERM",
+			signal: syscall.SIGTERM,
+		},
+	}
 
-	// Use t.Run with a timeout to allow the goroutine to finish
-	t.Run("WaitForStop", func(t *testing.T) {
-		select {
-		// Expected behavior, the daemon should be stopped
-		case <-stopCh:
-			assert.Equal(t, true, serverlessDaemon.Stopped)
-		case <-time.After(1000 * time.Millisecond):
-			t.Error("Timeout waiting for daemon to stop")
-		}
-	})
-}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			go handleTerminationSignals(serverlessDaemon, stopCh, func(c chan<- os.Signal, sig ...os.Signal) {
+				c <- tc.signal
+			})
 
-func TestDaemonStopOnSIGTERM(t *testing.T) {
-	stopCh := make(chan struct{})
-	signalCh := make(chan os.Signal, 1)
-
-	serverlessDaemon := daemon.StartDaemon("http://localhost:8124")
-	go handleTerminationSignals(serverlessDaemon, stopCh, func(c chan<- os.Signal, sig ...os.Signal) {
-		c <- syscall.SIGINT
-	})
-
-	signalCh <- syscall.SIGTERM
-
-	// Use t.Run with a timeout to allow the goroutine to finish
-	t.Run("WaitForStop", func(t *testing.T) {
-		select {
-		// Expected behavior, the daemon should be stopped
-		case <-stopCh:
-			assert.Equal(t, true, serverlessDaemon.Stopped)
-		case <-time.After(1000 * time.Millisecond):
-			t.Error("Timeout waiting for daemon to stop")
-		}
-	})
+			select {
+			// Expected behavior, the daemon should be stopped
+			case <-stopCh:
+				assert.Equal(t, true, serverlessDaemon.Stopped)
+			case <-time.After(1000 * time.Millisecond):
+				t.Error("Timeout waiting for daemon to stop")
+			}
+		})
+	}
 }
