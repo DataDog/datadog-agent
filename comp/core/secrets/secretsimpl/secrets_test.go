25640774a2304882f6bac1e6760d0a9fb6b0c810
Maxime mouial <maxime.mouial@gmail.com>
2023-11-30 18:20:59 +0100

    Revert "Revert "Adding a callback based method to the secrets component" (#21207)"
    This reverts commit 1c1373ff0f76c6f86f41a8aaa8341f79b5ef6cb1.

--- comp/core/secrets/secretsimpl/secrets_test.go
+++ comp/core/secrets/secretsimpl/secrets_test.go
@@ -8,11 +8,11 @@ package secretsimpl
 import (
 	"fmt"
 	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	yaml "gopkg.in/yaml.v2"
 )
 
 var (
@@ -52,7 +52,7 @@ instances:
   user: test2
 `)
 
-	testConfDecrypted = `instances:
+	testConfResolveed = `instances:
 - password: password1
   user: test
 - password: password2
@@ -80,7 +80,7 @@ keys_with_dash_string_value:
   foo: "-"
 `)
 
-	testConfDecryptedDash = `keys_with_dash_string_value:
+	testConfResolveedDash = `keys_with_dash_string_value:
   foo: '-'
 some_encoded_password: password1
 `
@@ -97,7 +97,7 @@ some_encoded_password: password1
 some_encoded_password: ENC[pass1]
 `)
 
-	testConfDecryptedMultiline = `some_encoded_password: |
+	testConfResolveedMultiline = `some_encoded_password: |
   password1
 `
 	testConfMultilineOrigin = handleToContext{
@@ -115,7 +115,7 @@ some:
     data: ENC[pass1]
 `)
 
-	testConfDecryptedNested = `some:
+	testConfResolveedNested = `some:
   encoded:
     data: password1
 `
@@ -124,6 +124,35 @@ some:
 			{
 				origin:   "test",
 				yamlPath: "some/encoded/data",
+			},
+		},
+	}
+
+	testConfNestedMultiple = []byte(`---
+top_level: ENC[pass1]
+some:
+  second_level: ENC[pass2]
+  encoded:
+    third_level: ENC[pass3]
+`)
+
+	testConfNestedOriginMultiple = handleToContext{
+		"pass1": []secretContext{
+			{
+				origin:   "test",
+				yamlPath: "top_level",
+			},
+		},
+		"pass2": []secretContext{
+			{
+				origin:   "test",
+				yamlPath: "some/second_level",
+			},
+		},
+		"pass3": []secretContext{
+			{
+				origin:   "test",
+				yamlPath: "some/encoded/third_level",
 			},
 		},
 	}
@@ -157,77 +186,19 @@ func TestIsEnc(t *testing.T) {
 	assert.Equal(t, "test", secret)
 }
 
-func TestWalkerError(t *testing.T) {
-	var config interface{}
-	err := yaml.Unmarshal(testYamlHash, &config)
-	require.NoError(t, err)
-
-	err = walk(&config, nil, func([]string, string) (string, error) {
-		return "", fmt.Errorf("some error")
-	})
-	assert.NotNil(t, err)
-}
-
-func TestWalkerSimple(t *testing.T) {
-	var config interface{}
-	err := yaml.Unmarshal([]byte("test"), &config)
-	require.NoError(t, err)
-
-	stringsCollected := []string{}
-	err = walk(&config, nil, func(_ []string, str string) (string, error) {
-		stringsCollected = append(stringsCollected, str)
-		return str + "_verified", nil
-	})
-	require.NoError(t, err)
-
-	assert.Equal(t, []string{"test"}, stringsCollected)
-
-	updatedConf, err := yaml.Marshal(config)
-	require.NoError(t, err)
-	assert.Equal(t, string("test_verified\n"), string(updatedConf))
-}
-
-func TestWalkerComplex(t *testing.T) {
-	var config interface{}
-	err := yaml.Unmarshal(testYamlHash, &config)
-	require.NoError(t, err)
-
-	stringsCollected := []string{}
-	err = walk(&config, nil, func(_ []string, str string) (string, error) {
-		stringsCollected = append(stringsCollected, str)
-		return str + "_verified", nil
-	})
-	require.NoError(t, err)
-
-	sort.Strings(stringsCollected)
-	assert.Equal(t, []string{
-		"1",
-		"2",
-		"test1",
-		"test2",
-		"test3",
-		"test4",
-		"test5",
-	}, stringsCollected)
-
-	updatedConf, err := yaml.Marshal(config)
-	require.NoError(t, err)
-	assert.Equal(t, string(testYamlHashUpdated), string(updatedConf))
-}
-
-func TestDecryptNoCommand(t *testing.T) {
+func TestResolveNoCommand(t *testing.T) {
 	resolver := newEnabledSecretResolver()
 	resolver.fetchHookFunc = func(secrets []string) (map[string]string, error) {
 		return nil, fmt.Errorf("some error")
 	}
 
 	// since we didn't set any command this should return without any error
-	resConf, err := resolver.Decrypt(testConf, "test")
+	resConf, err := resolver.Resolve(testConf, "test")
 	require.NoError(t, err)
 	assert.Equal(t, testConf, resConf)
 }
 
-func TestDecryptSecretError(t *testing.T) {
+func TestResolveSecretError(t *testing.T) {
 	resolver := newEnabledSecretResolver()
 	resolver.backendCommand = "some_command"
 
@@ -235,15 +206,15 @@ func TestDecryptSecretError(t *testing.T) {
 		return nil, fmt.Errorf("some error")
 	}
 
-	_, err := resolver.Decrypt(testConf, "test")
+	_, err := resolver.Resolve(testConf, "test")
 	require.NotNil(t, err)
 }
 
-func TestDecrypt(t *testing.T) {
+func TestResolve(t *testing.T) {
 	type testCase struct {
 		name                 string
 		testConf             []byte
-		decryptedConf        string
+		resolveedConf        string
 		expectedSecretOrigin handleToContext
 		expectedScrubbedKey  []string
 		secretFetchCB        func([]string) (map[string]string, error)
@@ -253,13 +224,13 @@ func TestDecrypt(t *testing.T) {
 	currentTest := t
 	testCases := []testCase{
 		{
-			// TestDecryptSecretStringMapStringWithDashValue checks that a nested string config value
+			// TestResolveSecretStringMapStringWithDashValue checks that a nested string config value
 			// that can be interpreted as YAML (such as a "-") is not interpreted as YAML by the secrets
 			// decryption logic, but is left unchanged as a string instead.
 			// See https://github.com/DataDog/datadog-agent/pull/6586 for details.
 			name:                 "map with dash value",
 			testConf:             testConfDash,
-			decryptedConf:        testConfDecryptedDash,
+			resolveedConf:        testConfResolveedDash,
 			expectedSecretOrigin: testConfDashOrigin,
 			expectedScrubbedKey:  []string{"some_encoded_password"},
 			secretFetchCB: func(secrets []string) (map[string]string, error) {
@@ -275,7 +246,7 @@ func TestDecrypt(t *testing.T) {
 		{
 			name:                 "multiline",
 			testConf:             testConfMultiline,
-			decryptedConf:        testConfDecryptedMultiline,
+			resolveedConf:        testConfResolveedMultiline,
 			expectedSecretOrigin: testConfMultilineOrigin,
 			expectedScrubbedKey:  []string{"some_encoded_password"},
 			secretFetchCB: func(secrets []string) (map[string]string, error) {
@@ -291,7 +262,7 @@ func TestDecrypt(t *testing.T) {
 		{
 			name:                 "nested",
 			testConf:             testConfNested,
-			decryptedConf:        testConfDecryptedNested,
+			resolveedConf:        testConfResolveedNested,
 			expectedSecretOrigin: testConfNestedOrigin,
 			expectedScrubbedKey:  []string{"data"},
 			secretFetchCB: func(secrets []string) (map[string]string, error) {
@@ -307,7 +278,7 @@ func TestDecrypt(t *testing.T) {
 		{
 			name:                 "no cache",
 			testConf:             testConf,
-			decryptedConf:        testConfDecrypted,
+			resolveedConf:        testConfResolveed,
 			expectedSecretOrigin: testConfOrigin,
 			expectedScrubbedKey:  []string{"password", "password"},
 			secretFetchCB: func(secrets []string) (map[string]string, error) {
@@ -326,7 +297,7 @@ func TestDecrypt(t *testing.T) {
 		{
 			name:                 "partial cache",
 			testConf:             testConf,
-			decryptedConf:        testConfDecrypted,
+			resolveedConf:        testConfResolveed,
 			expectedSecretOrigin: testConfOrigin,
 			expectedScrubbedKey:  []string{"password", "password"},
 			secretCache:          map[string]string{"pass1": "password1"},
@@ -344,7 +315,7 @@ func TestDecrypt(t *testing.T) {
 		{
 			name:                 "full cache",
 			testConf:             testConf,
-			decryptedConf:        testConfDecrypted,
+			resolveedConf:        testConfResolveed,
 			expectedSecretOrigin: testConfOrigin,
 			expectedScrubbedKey:  []string{"password", "password"},
 			secretCache:          map[string]string{"pass1": "password1", "pass2": "password2"},
@@ -368,12 +339,56 @@ func TestDecrypt(t *testing.T) {
 			scrubbedKey := []string{}
 			resolver.scrubHookFunc = func(k []string) { scrubbedKey = append(scrubbedKey, k[0]) }
 
-			newConf, err := resolver.Decrypt(tc.testConf, "test")
+			newConf, err := resolver.Resolve(tc.testConf, "test")
 			require.NoError(t, err)
 
-			assert.Equal(t, tc.decryptedConf, string(newConf))
+			assert.Equal(t, tc.resolveedConf, string(newConf))
 			assert.Equal(t, tc.expectedSecretOrigin, resolver.origin)
 			assert.Equal(t, tc.expectedScrubbedKey, scrubbedKey)
 		})
 	}
+}
+
+func TestResolveWithCallback(t *testing.T) {
+	testConf := testConfNestedMultiple
+
+	resolver := newEnabledSecretResolver()
+	resolver.backendCommand = "some_command"
+	resolver.cache = map[string]string{"pass3": "password3"}
+
+	resolver.fetchHookFunc = func(secrets []string) (map[string]string, error) {
+		return map[string]string{
+			"pass1": "password1",
+			"pass2": "password2",
+		}, nil
+	}
+
+	topLevelResolved := 0
+	secondLevelResolved := 0
+	thirdLevelResolved := 0
+	err := resolver.ResolveWithCallback(
+		testConf,
+		"test",
+		func(yamlPath []string, value any) {
+			switch strings.Join(yamlPath, "/") {
+			case "top_level":
+				assert.Equal(t, "password1", value)
+				topLevelResolved++
+			case "some/second_level":
+				assert.Equal(t, "password2", value)
+				secondLevelResolved++
+			case "some/encoded/third_level":
+				assert.Equal(t, "password3", value)
+				thirdLevelResolved++
+			default:
+				assert.Fail(t, "unknown yaml path: %s", yamlPath)
+			}
+		},
+	)
+	require.NoError(t, err)
+	assert.Equal(t, 1, topLevelResolved, "'top_level' secret was not resolved or resolved multiple times")
+	assert.Equal(t, 1, secondLevelResolved, "'second_level' secret was not resolved or resolved multiple times")
+	assert.Equal(t, 1, thirdLevelResolved, "'third_level' secret was not resolved or resolved multiple times")
+
+	assert.Equal(t, testConfNestedOriginMultiple, resolver.origin)
 }
