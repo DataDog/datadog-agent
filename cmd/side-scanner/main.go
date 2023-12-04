a6312d1fc70565551f0ada9e8645da3501788ad6
David du Colombier <djc@datadoghq.com>
2023-12-04 10:57:53 +0100

    [side-scanner] Fix "error attaching volume" log message

--- cmd/side-scanner/main.go
+++ cmd/side-scanner/main.go
@@ -2057,9 +2057,9 @@ func shareAndAttachSnapshot(ctx context.Context, scan *scanTask, snapshotARN arn
 			Device:     aws.String(device),
 		})
 		if err == nil {
-			log.Debugf("error attaching volume %q into device %q", *volume.VolumeId, device)
 			break
 		}
+		log.Debugf("error attaching volume %q into device %q", *volume.VolumeId, device)
 		if !sleepCtx(ctx, 1*time.Second) {
 			break
 		}
