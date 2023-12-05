73c41d9cb8597e8fbdf0978f68989c428db23739
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-12-05 13:28:17 -0800

    only linux build tag to prevent import bloat

--- pkg/ebpf/perf_metrics_nonlinux.go
+++ pkg/ebpf/perf_metrics_nonlinux.go
@@ -3,12 +3,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build !linux_bpf
+//go:build !linux
 
 package ebpf
 
 import (
-	manager "github.com/DataDog/ebpf-manager"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -16,12 +15,3 @@ import (
 func NewPerfUsageCollector() prometheus.Collector {
 	return nil
 }
-
-// ReportPerfMapTelemetry starts reporting the telemetry for the provided PerfMap
-func ReportPerfMapTelemetry(_ *manager.PerfMap) {}
-
-// ReportRingBufferTelemetry starts reporting the telemetry for the provided RingBuffer
-func ReportRingBufferTelemetry(_ *manager.RingBuffer) {}
-
-// UnregisterTelemetry unregisters the PerfMap and RingBuffers from telemetry
-func UnregisterTelemetry(_ *manager.Manager) {}
