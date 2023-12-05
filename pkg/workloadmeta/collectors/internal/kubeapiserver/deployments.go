171df5221846d294a8e5de36e7c7702bf4c7d0a2
Adel Haj Hassan <41540817+adel121@users.noreply.github.com>
2023-12-05 23:16:36 +0100

    Change language annotation format for language detection (#21069)
    Change language annotation format for language detection

--- pkg/workloadmeta/collectors/internal/kubeapiserver/deployments.go
+++ pkg/workloadmeta/collectors/internal/kubeapiserver/deployments.go
@@ -10,7 +10,6 @@ package kubeapiserver
 
 import (
 	"context"
-	"regexp"
 	"strings"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -21,11 +20,11 @@ import (
 	"k8s.io/client-go/tools/cache"
 
 	"github.com/DataDog/datadog-agent/pkg/languagedetection/languagemodels"
+	languagedetectionUtil "github.com/DataDog/datadog-agent/pkg/languagedetection/util"
+
 	ddkube "github.com/DataDog/datadog-agent/pkg/util/kubernetes"
 	"github.com/DataDog/datadog-agent/pkg/workloadmeta"
 )
-
-var re = regexp.MustCompile(`apm\.datadoghq\.com\/(init)?\.?(.+?)\.languages`)
 
 // deploymentFilter filters out deployments that can't be used for unified service tagging or process language detection
 type deploymentFilter struct{}
@@ -92,16 +91,14 @@ func (p deploymentParser) Parse(obj interface{}) workloadmeta.Entity {
 	containerLanguages := make(map[string][]languagemodels.Language)
 
 	for annotation, languages := range deployment.Annotations {
-		// find a match
-		matches := re.FindStringSubmatch(annotation)
-		if len(matches) != 3 {
-			continue
-		}
-		// matches[1] matches "init"
-		if matches[1] != "" {
-			updateContainerLanguageMap(initContainerLanguages, matches[2], languages)
-		} else {
-			updateContainerLanguageMap(containerLanguages, matches[2], languages)
+
+		containerName, isInitContainer := languagedetectionUtil.ExtractContainerFromAnnotationKey(annotation)
+		if containerName != "" {
+			if isInitContainer {
+				updateContainerLanguageMap(initContainerLanguages, containerName, languages)
+			} else {
+				updateContainerLanguageMap(containerLanguages, containerName, languages)
+			}
 		}
 	}
 
