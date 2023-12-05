fca5c9947cc09407b8bdad2c23aab7ad843d5333
Bryce Kahle <bryce.kahle@datadoghq.com>
2023-12-05 13:17:08 -0800

    add missing stubs

--- pkg/ebpf/perf_metrics_nonlinux.go
+++ pkg/ebpf/perf_metrics_nonlinux.go
@@ -7,9 +7,21 @@
 
 package ebpf
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	manager "github.com/DataDog/ebpf-manager"
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 // NewPerfUsageCollector returns nil
 func NewPerfUsageCollector() prometheus.Collector {
 	return nil
 }
+
+// ReportPerfMapTelemetry starts reporting the telemetry for the provided PerfMap
+func ReportPerfMapTelemetry(_ *manager.PerfMap) {}
+
+// ReportRingBufferTelemetry starts reporting the telemetry for the provided RingBuffer
+func ReportRingBufferTelemetry(_ *manager.RingBuffer) {}
+
+// UnregisterTelemetry unregisters the PerfMap and RingBuffers from telemetry
+func UnregisterTelemetry(_ *manager.Manager) {}
