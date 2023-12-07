35eb862ce5477dd34a55d1b51a256fa925fc2865
Dan Lepage <140522866+dplepage-dd@users.noreply.github.com>
2023-12-07 11:21:27 -0500

    [SNMP] Run traps server as a separate `fx.App` (#20807)
    * Remove server implementation components from bundle, provide them to subapp in server.
    * Restore 'error' field in traps status.

--- comp/snmptraps/server/serverimpl/server.go
+++ comp/snmptraps/server/serverimpl/server.go
@@ -7,9 +7,24 @@
 package serverimpl
 
 import (
+	"context"
+
+	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer"
+	"github.com/DataDog/datadog-agent/comp/core/config"
+	"github.com/DataDog/datadog-agent/comp/core/hostname"
+	"github.com/DataDog/datadog-agent/comp/core/log"
+
+	trapsconfig "github.com/DataDog/datadog-agent/comp/snmptraps/config"
+	"github.com/DataDog/datadog-agent/comp/snmptraps/config/configimpl"
+	"github.com/DataDog/datadog-agent/comp/snmptraps/formatter/formatterimpl"
 	"github.com/DataDog/datadog-agent/comp/snmptraps/forwarder"
+	"github.com/DataDog/datadog-agent/comp/snmptraps/forwarder/forwarderimpl"
 	"github.com/DataDog/datadog-agent/comp/snmptraps/listener"
+	"github.com/DataDog/datadog-agent/comp/snmptraps/listener/listenerimpl"
+	"github.com/DataDog/datadog-agent/comp/snmptraps/oidresolver/oidresolverimpl"
 	"github.com/DataDog/datadog-agent/comp/snmptraps/server"
+	"github.com/DataDog/datadog-agent/comp/snmptraps/status"
+	"github.com/DataDog/datadog-agent/comp/snmptraps/status/statusimpl"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"go.uber.org/fx"
 )
@@ -19,20 +34,87 @@ var Module = fxutil.Component(
 	fx.Provide(newServer),
 )
 
-// Server manages an SNMP trap listener.
-type Server struct {
-	listener  listener.Component
-	forwarder forwarder.Component
+type dependencies struct {
+	fx.In
+	Conf      config.Component
+	HNService hostname.Component
+	Demux     demultiplexer.Component
+	Logger    log.Component
 }
 
-// newServer configures a netflow server.
-// Since the listener and forwarder are already registered with the lifecycle
-// tracker, this component really just exists so that there's one component to
-// register instead of requiring users to remember to depend on both the
-// listener and the forwarder.
-func newServer(l listener.Component, f forwarder.Component) server.Component {
-	return &Server{
-		listener:  l,
-		forwarder: f,
+// injections bundles the injectables passed from the main app to the subapp.
+type injections struct {
+	fx.Out
+	Conf      config.Component
+	HNService hostname.Component
+	Demux     demultiplexer.Component
+	Logger    log.Component
+	Status    status.Component
+}
+
+// TrapsServer implements the SNMP traps service.
+type TrapsServer struct {
+	app     *fx.App
+	running bool
+	stat    status.Component
+}
+
+// Running indicates whether the traps server is currently running.
+func (w *TrapsServer) Running() bool {
+	return w.running
+}
+
+// Error reports any error from server initialization/startup.
+func (w *TrapsServer) Error() error {
+	if w.stat == nil {
+		return nil
 	}
+	return w.stat.GetStartError()
+}
+
+// newServer creates a new traps server, registering it with the fx lifecycle
+// system if traps are enabled.
+func newServer(lc fx.Lifecycle, deps dependencies) server.Component {
+	if !trapsconfig.IsEnabled(deps.Conf) {
+		return &TrapsServer{running: false}
+	}
+	stat := statusimpl.New()
+	app := fx.New(
+		fx.Supply(injections{
+			Conf:      deps.Conf,
+			HNService: deps.HNService,
+			Demux:     deps.Demux,
+			Logger:    deps.Logger,
+			Status:    stat,
+		}),
+		configimpl.Module,
+		formatterimpl.Module,
+		forwarderimpl.Module,
+		listenerimpl.Module,
+		oidresolverimpl.Module,
+		fx.Invoke(func(_ forwarder.Component, _ listener.Component) {}),
+	)
+	server := &TrapsServer{app: app, stat: stat}
+	if err := app.Err(); err != nil {
+		deps.Logger.Errorf("Failed to initialize snmp-traps server: %s", err)
+		server.stat.SetStartError(err)
+		return server
+	}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			err := app.Start(ctx)
+			if err != nil {
+				deps.Logger.Errorf("Failed to start snmp-traps server: %s", err)
+				server.stat.SetStartError(err)
+			} else {
+				server.running = true
+			}
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			server.running = false
+			return app.Stop(ctx)
+		},
+	})
+	return server
 }
