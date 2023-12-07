105d5a6518510be1263f5b29974bb5a814e2348e
Dmytro Yurchenko <dmytro.yurchenko@datadoghq.com>
2023-12-07 15:12:01 +0100

    Update microbenchmark scripts

--- test/benchmarks/apm_scripts/run-benchmarks.sh
+++ test/benchmarks/apm_scripts/run-benchmarks.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-ARTIFACTS_DIR="/artifacts/${CI_JOB_ID}"
-mkdir -p "${ARTIFACTS_DIR}"
-
-inv trace-agent.benchmarks --output="${ARTIFACTS_DIR}/pr_bench.txt" --bench="BenchmarkAgentTraceProcessing$"
-
+inv trace-agent.benchmarks --output="$ARTIFACTS_DIR/pr_bench.txt" --bench="BenchmarkAgentTraceProcessing$"
 git checkout main
-inv trace-agent.benchmarks --output="${ARTIFACTS_DIR}/main_bench.txt" --bench="BenchmarkAgentTraceProcessing$"
 
+inv trace-agent.benchmarks --output="$ARTIFACTS_DIR/main_bench.txt" --bench="BenchmarkAgentTraceProcessing$"
 git checkout "${CI_COMMIT_REF_NAME}" # (Only needed while these changes aren't merged to main)
