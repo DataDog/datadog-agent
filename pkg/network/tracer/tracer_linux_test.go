da3ca0f003f16157b915d71d086660f0990b9572
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2023-12-07 14:04:40 -0600

    Fix TestBlockingReadCounts
    * make socket blocking
    * use syscall.RawConn to ensure socket fd is valid for socket operations

--- pkg/network/tracer/tracer_linux_test.go
+++ pkg/network/tracer/tracer_linux_test.go
@@ -1729,22 +1729,38 @@ func (s *TracerSuite) TestBlockingReadCounts() {
 	require.NoError(t, err)
 	defer c.Close()
 
-	f, err := c.(*net.TCPConn).File()
-	require.NoError(t, err)
+	rawConn, err := c.(syscall.Conn).SyscallConn()
+	require.NoError(t, err, "error getting raw conn")
 
-	fd := int(f.Fd())
+	// set the socket to blocking as the MSG_WAITALL
+	// option used later on for reads only works for
+	// blocking sockets
+	// also set a timeout on the reads to not wait
+	// forever
+	rawConn.Control(func(fd uintptr) {
+		err = syscall.SetNonblock(int(fd), false)
+		require.NoError(t, err, "could not set socket to blocking")
+		err = syscall.SetsockoptTimeval(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVTIMEO, &syscall.Timeval{Sec: 5})
+		require.NoError(t, err, "could not set read timeout on socket")
+	})
 
 	read := 0
 	buf := make([]byte, 6)
-	require.EventuallyWithT(t, func(c *assert.CollectT) {
-		n, _, err := syscall.Recvfrom(fd, buf[read:], syscall.MSG_WAITALL)
-		if !assert.NoError(c, err) {
+	require.EventuallyWithT(t, func(collect *assert.CollectT) {
+		var n int
+		readErr := rawConn.Read(func(fd uintptr) bool {
+			n, _, err = syscall.Recvfrom(int(fd), buf[read:], syscall.MSG_WAITALL)
+			return true
+		})
+
+		if !assert.NoError(collect, err, "error reading from connection") ||
+			!assert.NoError(collect, readErr, "error from raw conn") {
 			return
 		}
 
 		read += n
 		t.Logf("read %d", read)
-		assert.Equal(c, 6, read)
+		assert.Equal(collect, 6, read)
 	}, 10*time.Second, 100*time.Millisecond, "failed to get required bytes")
 
 	var conn *network.ConnectionStats
