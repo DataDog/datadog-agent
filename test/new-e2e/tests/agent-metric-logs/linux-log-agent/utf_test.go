ff7db9e1a3dc4f1dfc2a59cd60f30a56ed2861bf
raymond zhao <raymond.zhao@datadoghq.com>
2023-12-11 15:46:49 -0500

    utf tailing test

--- test/new-e2e/tests/agent-metric-logs/linux-log-agent/utf_test.go
+++ test/new-e2e/tests/agent-metric-logs/linux-log-agent/utf_test.go
@@ -14,153 +14,102 @@ import (
 
 	fi "github.com/DataDog/datadog-agent/test/fakeintake/client"
 	e2e "github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e/params"
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
 	"github.com/stretchr/testify/assert"
 )
 
-//go:embed log-config/utf-log-config.yaml
+//go:embed log-config/utf-16-le-log-config.yaml
 var utfLittleEndianLogConfig []byte
 
-//go:embed log-config/utf-be-log-config.yaml
+//go:embed log-config/utf-16-be-log-config.yaml
 var utfBigEndianLogConfig []byte
 
-// LinuxVMFakeintakeSuite defines a test suite for the log agent interacting with a virtual machine and fake intake.
-type utfSuite struct {
+var service string = "utfservice"
+
+// UTF defines a test suite for the log agent interacting with a virtual machine and fake intake.
+type UtfSuite struct {
 	e2e.Suite[e2e.FakeIntakeEnv]
 	DevMode bool
 }
 
 // TestE2EVMFakeintakeSuite runs the E2E test suite for the log agent with a VM and fake intake.
 func TestUtfSuite(t *testing.T) {
-	s := &utfSuite{}
+	s := &UtfSuite{}
 	_, s.DevMode = os.LookupEnv("TESTS_E2E_DEVMODE")
-
-	e2e.Run(t, s, e2e.FakeIntakeStackDef())
+	e2e.Run(t, s, e2e.FakeIntakeStackDef(), params.WithDevMode())
 }
 
-func (s *utfSuite) TestUtfTailing() {
+func (s *UtfSuite) generateUtfLog(endianness, content string) error {
+	utfLogGenerationCommand := fmt.Sprintf(`sudo python3 -c "f = open('/var/log/hello-world-utf.log', 'ab'); t = '%s\n'.encode('utf-16-%s'); f.write(t); f.close()"`, content, endianness)
+	_, err := s.Env().VM.ExecuteWithError(utfLogGenerationCommand)
+	return err
+}
+
+func (s *UtfSuite) BeforeTest(_, _ string) {
+	// Create a new log file
+	s.Env().VM.Execute("sudo touch /var/log/hello-world-utf.log")
+	s.Env().VM.Execute("sudo chmod +r /var/log/hello-world-utf.log")
+}
+
+func (s *UtfSuite) TestUtfTailing() {
 	// Run test cases
 	s.Run("UtfLittleEndianCollection", s.UtfLittleEndianCollection)
-	// s.Run("UtfBigEndianCollection", s.UtfBigEndianCollection)
+	s.Run("UtfBigEndianCollection", s.UtfBigEndianCollection)
 }
 
-func (s *utfSuite) UtfBigEndianCollection() {
+func (s *UtfSuite) UtfBigEndianCollection() {
 	s.UpdateEnv(e2e.FakeIntakeStackDef(
 		e2e.WithAgentParams(
 			agentparams.WithLogs(),
 			agentparams.WithIntegration("custom_logs.d", string(utfBigEndianLogConfig)))))
 	t := s.T()
-	fakeintake := s.Env().Fakeintake
+	intake := s.Env().Fakeintake
 
-	// Create a new log file with permissionn inaccessable to the agent
-	s.Env().VM.Execute("sudo touch /var/log/hello-world-utf.log")
-	s.Env().VM.Execute("sudo chmod 666 /var/log/hello-world-utf.log")
+	// generate utf-16-be log
+	content := "big endian sample log"
+	err := s.generateUtfLog("be", content)
+	assert.NoErrorf(t, err, "Unable to generate utf-16-be log, err: %s.", err)
 
-	// Part 1: Ensure no logs are present in fakeintake
+	// check that intake has utf-16-be encoded log
 	s.EventuallyWithT(func(c *assert.CollectT) {
-		logs, err := fakeintake.FilterLogs("utfservice")
-		if !assert.NoError(c, err, "Unable to filter logs by the service 'hello'.") {
-			return
-		}
-		// If logs are found, print their content for debugging
-		if !assert.Empty(c, logs, "Logs were found when none were expected.") {
-			cat, _ := s.Env().VM.ExecuteWithError("cat /var/log/hello-world-utf.log")
-			t.Logf("Logs detected when none were expected: %v", cat)
-		}
-	}, 2*time.Minute, 10*time.Second)
-
-	// send utf16 log
-	utfLogCommand := `python3 -c "f = open('/var/log/hello-world-utf.log', 'ab'); t = 'big endian log\n'.encode('utf-16-be'); f.write(t); f.close()"`
-	s.Env().VM.Execute(utfLogCommand)
-
-	expectLogs := true
-	service := "utfservice"
-
-	content := "big endian log"
-	// check that intake has log
-	s.EventuallyWithT(func(c *assert.CollectT) {
-		names, err := fakeintake.GetLogServiceNames()
-		if !assert.NoErrorf(c, err, "Error found: %s", err) {
-			return
-		}
-		fmt.Println(names)
-		logs, err := fakeintake.FilterLogs(service, fi.WithMessageMatching(content))
-		intakeLogs := logsToString(logs)
+		logs, err := intake.FilterLogs(service, fi.WithMessageMatching(content))
 		assert.NoErrorf(c, err, "Error found: %s", err)
-		if expectLogs {
-			t.Logf("Logs with content: '%s' service: %s found", content, names)
-			fmt.Println(intakeLogs, "rz")
-			assert.NotEmpty(c, logs, "Expected at least 1 log with content: '%s', but received %s logs.", content, intakeLogs)
-		} else {
-			t.Logf("No logs with content: '%s' service: %s found as expected", content, names)
-			assert.Empty(c, logs, "No logs with content: '%s' is expected to be found from service instead found: %s", content, intakeLogs)
-		}
-		fmt.Println(len(intakeLogs), "len utf-16-le")
-		fmt.Println(intakeLogs)
-
-		logs, _ = fakeintake.FilterLogs(service)
-		fmt.Println(len(logs), "total", logs)
+		intakeLogs := logsToString(logs)
+		assert.NotEmpty(c, logs, "Expected at least 1 log with content: '%s', but received %s logs.", content, intakeLogs)
+		t.Logf(intakeLogs)
 	}, 2*time.Minute, 10*time.Second)
 
 	s.EventuallyWithT(func(c *assert.CollectT) {
 		err := s.Env().Fakeintake.FlushServerAndResetAggregators()
 		assert.NoErrorf(c, err, "Having issue flushing server and resetting aggregators, retrying...")
 	}, 5*time.Minute, 10*time.Second)
-
-	s.Env().VM.Execute("sudo rm -f /var/log/hello-world-utf.log")
 }
 
-func (s *utfSuite) UtfLittleEndianCollection() {
+func (s *UtfSuite) UtfLittleEndianCollection() {
 	s.UpdateEnv(e2e.FakeIntakeStackDef(
 		e2e.WithAgentParams(
 			agentparams.WithLogs(),
 			agentparams.WithIntegration("custom_logs.d", string(utfLittleEndianLogConfig)))))
 	t := s.T()
-	fakeintake := s.Env().Fakeintake
+	intake := s.Env().Fakeintake
 
-	// Create a new log file with permissionn inaccessable to the agent
-	s.Env().VM.Execute("sudo touch /var/log/hello-world-utf.log")
-	s.Env().VM.Execute("sudo chmod 666 /var/log/hello-world-utf.log")
+	// generate utf-16-le log
+	content := "little endian sample log"
+	err := s.generateUtfLog("le", content)
+	assert.NoErrorf(t, err, "Unable to generate utf-16-le log, err: %s.", err)
 
-	// send utf16 log
-	utfLogCommand := `python3 -c "f = open('/var/log/hello-world-utf.log', 'ab'); t = 'This is just sample text encoded in utf-16\n'.encode('utf-16'); f.write(t); f.close()"`
-	s.Env().VM.Execute(utfLogCommand)
-
-	expectLogs := true
-	service := "utfservice"
-
-	content := "This is just sample text encoded in utf-16"
-	// check that intake has log
+	// check that intake has utf-16-le encoded log
 	s.EventuallyWithT(func(c *assert.CollectT) {
-		names, err := fakeintake.GetLogServiceNames()
-		if !assert.NoErrorf(c, err, "Error found: %s", err) {
-			return
-		}
-		fmt.Println(names)
-		logs, err := fakeintake.FilterLogs(service, fi.WithMessageMatching(content))
+		logs, err := intake.FilterLogs(service, fi.WithMessageMatching(content))
 		intakeLogs := logsToString(logs)
 		assert.NoErrorf(c, err, "Error found: %s", err)
-		if expectLogs {
-			t.Logf("Logs with content: '%s' service: %s found", content, names)
-			fmt.Println(intakeLogs, "rz")
-			assert.NotEmpty(c, logs, "Expected at least 1 log with content: '%s', but received %s logs.", content, intakeLogs)
-		} else {
-			t.Logf("No logs with content: '%s' service: %s found as expected", content, names)
-			assert.Empty(c, logs, "No logs with content: '%s' is expected to be found from service instead found: %s", content, intakeLogs)
-		}
-		fmt.Println(len(intakeLogs), "len utf-16-le")
-		fmt.Println(intakeLogs)
-
-		logs, _ = fakeintake.FilterLogs(service)
-		fmt.Println(len(logs), "total", logs)
+		assert.NotEmpty(c, logs, "Expected at least 1 log with content: '%s', but received %s logs.", content, intakeLogs)
+		t.Logf(intakeLogs)
 	}, 2*time.Minute, 10*time.Second)
 
-	fmt.Println("reached here")
 	s.EventuallyWithT(func(c *assert.CollectT) {
-		fmt.Println("flushing fake intake and resetting aggregator...")
 		err := s.Env().Fakeintake.FlushServerAndResetAggregators()
 		assert.NoErrorf(c, err, "Having issue flushing server and resetting aggregators, retrying...")
 	}, 5*time.Minute, 10*time.Second)
-
-	s.Env().VM.Execute("sudo rm -f /var/log/hello-world-utf.log")
 }
