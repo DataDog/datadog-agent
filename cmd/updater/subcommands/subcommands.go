9e325f1b54d6e54cc962b80416a5989ad1ad5527
Arthur Bellal <arthur.bellal@datadoghq.com>
2023-12-11 23:29:07 +0100

    scafhold updater run

--- cmd/updater/subcommands/subcommands.go
+++ cmd/updater/subcommands/subcommands.go
@@ -3,14 +3,18 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package subcommands contains the updater subcommands
 package subcommands
 
 import (
 	"github.com/DataDog/datadog-agent/cmd/updater/command"
+	"github.com/DataDog/datadog-agent/cmd/updater/subcommands/run"
 )
 
 // UpdaterSubcommands returns SubcommandFactories for the subcommands
 // supported with the current build flags.
 func UpdaterSubcommands() []command.SubcommandFactory {
-	return []command.SubcommandFactory{}
+	return []command.SubcommandFactory{
+		run.Commands,
+	}
 }
