e48df7d70a22bc0bef3ef5d95475f3d749ba2da3
pducolin <paola.ducolin@datadoghq.com>
2023-12-12 16:21:55 +0100

    [e2e] migrate snmp e2e test to TestSuite

--- test/new-e2e/tests/ndm/snmp/snmp_test.go
+++ test/new-e2e/tests/ndm/snmp/snmp_test.go
@@ -7,30 +7,24 @@
 package snmp
 
 import (
-	"context"
 	"embed"
-	"errors"
 	"path"
+	"testing"
+	"time"
 
-	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/infra"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e/client"
 	"github.com/DataDog/test-infra-definitions/components/datadog/agent"
 	"github.com/DataDog/test-infra-definitions/components/datadog/dockeragentparams"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/fakeintake/fakeintakeparams"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/vm/ec2os"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/vm/ec2params"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/vm/ec2vm"
+	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
-
-// TestEnv implements a test environment for NDM. Deprecated, should port to TestSuite
-type TestEnv struct {
-	context context.Context
-	name    string
-
-	InstanceIP  string
-	StackOutput auto.UpResult
-}
 
 //go:embed compose/snmpCompose.yaml
 var snmpCompose string
@@ -42,96 +36,79 @@ const (
 	composeDataPath = "compose/data"
 )
 
-// NewTestEnv creates a new test environment for NDM. Deprecated, should port to NDM
-func NewTestEnv() (*TestEnv, error) {
-	snmpTestEnv := &TestEnv{
-		context: context.Background(),
-		name:    "snmp-agent",
-	}
-
-	stackManager := infra.GetStackManager()
-
-	_, upResult, err := stackManager.GetStack(snmpTestEnv.context, snmpTestEnv.name, nil, func(ctx *pulumi.Context) error {
+// snmpDockerStackDef defines a stack with a docker agent on an AmazonLinuxDockerOS VM
+// with snmpsim installed and configured with snmp recordings
+func snmpDockerStackDef() *e2e.StackDefinition[e2e.DockerEnv] {
+	return e2e.EnvFactoryStackDef(func(ctx *pulumi.Context) (*e2e.DockerEnv, error) {
 		// setup VM
-		vm, err := ec2vm.NewUnixEc2VM(ctx)
+		vm, err := ec2vm.NewUnixEc2VM(ctx, ec2params.WithOS(ec2os.AmazonLinuxDockerOS))
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		fakeintakeExporter, err := aws.NewEcsFakeintake(vm.GetAwsEnvironment(), fakeintakeparams.WithoutLoadBalancer())
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		filemanager := vm.GetFileManager()
 		// upload snmpsim data files
 		createDataDirCommand, dataPath, err := filemanager.TempDirectory("data")
 		if err != nil {
-			return err
+			return nil, err
 		}
 		dataFiles, err := loadDataFileNames()
 		if err != nil {
-			return err
+			return nil, err
 		}
+
 		fileCommands := []pulumi.Resource{}
 		for _, fileName := range dataFiles {
 			fileContent, err := dataFolder.ReadFile(path.Join(composeDataPath, fileName))
 			if err != nil {
-				return err
+				return nil, err
 			}
 			dontUseSudo := false
 			fileCommand, err := filemanager.CopyInlineFile(pulumi.String(fileContent), path.Join(dataPath, fileName), dontUseSudo,
 				pulumi.DependsOn([]pulumi.Resource{createDataDirCommand}))
 			if err != nil {
-				return err
+				return nil, err
 			}
 			fileCommands = append(fileCommands, fileCommand)
 		}
 
 		createConfigDirCommand, configPath, err := filemanager.TempDirectory("config")
 		if err != nil {
-			return err
+			return nil, err
 		}
 		// edit snmp config file
 		dontUseSudo := false
 		configCommand, err := filemanager.CopyInlineFile(pulumi.String(snmpConfig), path.Join(configPath, "snmp.yaml"), dontUseSudo,
 			pulumi.DependsOn([]pulumi.Resource{createConfigDirCommand}))
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		// install agent and snmpsim on docker
 		envVars := pulumi.StringMap{"DATA_DIR": pulumi.String(dataPath), "CONFIG_DIR": pulumi.String(configPath)}
 		composeDependencies := []pulumi.Resource{createDataDirCommand, configCommand}
 		composeDependencies = append(composeDependencies, fileCommands...)
-		_, err = agent.NewDaemon(
+		docker, err := agent.NewDaemon(
 			vm,
 			dockeragentparams.WithFakeintake(fakeintakeExporter),
 			dockeragentparams.WithExtraComposeManifest("snmpsim", snmpCompose),
 			dockeragentparams.WithEnvironmentVariables(envVars),
 			dockeragentparams.WithPulumiDependsOn(pulumi.DependsOn(composeDependencies)),
 		)
-		return err
-	}, false)
-	if err != nil {
-		return nil, err
-	}
-
-	snmpTestEnv.StackOutput = upResult
-
-	output, found := upResult.Outputs["instance-ip"]
-
-	if !found {
-		return nil, errors.New("unable to find host ip")
-	}
-	snmpTestEnv.InstanceIP = output.Value.(string)
-
-	return snmpTestEnv, nil
-}
-
-// Destroy delete the NDM stack. Deprecated, should port to NDM
-func (testEnv *TestEnv) Destroy() error {
-	return infra.GetStackManager().DeleteStack(testEnv.context, testEnv.name)
+		if err != nil {
+			return nil, err
+		}
+		return &e2e.DockerEnv{
+			Docker:     client.NewDocker(docker),
+			VM:         client.NewPulumiStackVM(vm),
+			Fakeintake: client.NewFakeintake(fakeintakeExporter),
+		}, nil
+	})
 }
 
 //go:embed compose/data
@@ -146,4 +123,24 @@ func loadDataFileNames() (out []string, err error) {
 		out = append(out, f.Name())
 	}
 	return out, nil
+}
+
+type snmpDockerSuite struct {
+	e2e.Suite[e2e.DockerEnv]
+}
+
+// TestSnmpSuite runs the snmp e2e suite
+func TestSnmpSuite(t *testing.T) {
+	e2e.Run(t, &snmpDockerSuite{}, snmpDockerStackDef())
+}
+
+// TestSnmp tests that the snmpsim container is running and that the agent container
+// is sending snmp metrics to the fakeintake
+func (s *snmpDockerSuite) TestSnmp() {
+	fakeintake := s.Env().Fakeintake
+	s.EventuallyWithT(func(c *assert.CollectT) {
+		metrics, err := fakeintake.GetMetricNames()
+		assert.NoError(c, err)
+		assert.Contains(c, metrics, "snmp.sysUpTimeInstance", "metrics %v doesn't contain snmp.sysUpTimeInstance", metrics)
+	}, 5*time.Minute, 10*time.Second)
 }
