8662b4559a5049e6b0fc04e5780b7a10c695608a
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-12-13 08:59:57 +0100

    PoC of direct python extraction

--- tools/windows/DatadogAgentInstaller/CustomActions/PythonDistributionCustomAction.cs
+++ tools/windows/DatadogAgentInstaller/CustomActions/PythonDistributionCustomAction.cs
@@ -1,6 +1,7 @@
 using Microsoft.Deployment.WindowsInstaller;
 using System;
 using System.IO;
+using System.IO.Pipes;
 using Datadog.CustomActions.Extensions;
 using System.Text;
 using Datadog.CustomActions.Interfaces;
@@ -36,26 +37,25 @@ namespace Datadog.CustomActions
         static void Decompress(ISession session, string compressedFileName)
         {
             var decoder = new SevenZip.Compression.LZMA.Decoder();
+            var outputPath = Path.GetDirectoryName(Path.GetFullPath(compressedFileName));
+
             using (var inStream = File.OpenRead(compressedFileName))
+            using (var outStream = new AnonymousPipeServerStream())
             {
-                using (var outStream = File.Create($"{compressedFileName}.tar"))
+                var reader = new BinaryReader(inStream, Encoding.UTF8);
+                // Properties of the stream are encoded on 5 bytes
+                var props = reader.ReadBytes(5);
+                decoder.SetDecoderProperties(props);
+                var length = reader.ReadInt64();
+                decoder.Code(inStream, outStream, inStream.Length, length, null);
+
+                using (var tarStream = new AnonymousPipeClientStream(outStream.GetClientHandleAsString()))
+                using (var tarArchive = TarArchive.CreateInputTarArchive(tarStream, Encoding.UTF8))
                 {
-                    var reader = new BinaryReader(inStream, Encoding.UTF8);
-                    // Properties of the stream are encoded on 5 bytes
-                    var props = reader.ReadBytes(5);
-                    decoder.SetDecoderProperties(props);
-                    var length = reader.ReadInt64();
-                    decoder.Code(inStream, outStream, inStream.Length, length, null);
-                    outStream.Flush();
+                    tarArchive.ExtractContents(outputPath);
                 }
             }
-            var outputPath = Path.GetDirectoryName(Path.GetFullPath(compressedFileName));
-            using (var inStream = File.OpenRead($"{compressedFileName}.tar"))
-            using (var tarArchive = TarArchive.CreateInputTarArchive(inStream, Encoding.UTF8))
-            {
-                tarArchive.ExtractContents(outputPath);
-            }
-            File.Delete($"{compressedFileName}.tar");
+
             File.Delete($"{compressedFileName}");
         }
 
