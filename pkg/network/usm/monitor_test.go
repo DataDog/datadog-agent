89c3cc6e93dde250abb3d7fe1f31f60a46a7a0a4
amit.slavin <amit.slavin@datadoghq.com>
2023-12-19 17:56:27 +0200

    removed doc that is redundant

--- pkg/network/usm/monitor_test.go
+++ pkg/network/usm/monitor_test.go
@@ -666,8 +666,6 @@ func (s *USMHTTP2Suite) TestHTTP2ManyDifferentPaths() {
 
 func getExpectedOutcomeForPathWithRepeatedChars() map[http.Key]captureRange {
 	expected := make(map[http.Key]captureRange)
-	// The path `/a` and `/aa` are not being encoded with Huffman, and that's an edge case for our http2 monitoring for the moment,
-	// thus we're starting with `/aaa` and above.
 	for i := 1; i < 100; i++ {
 		expected[http.Key{
 			Path: http.Path{
@@ -743,7 +741,6 @@ func (s *USMHTTP2Suite) TestSimpleHTTP2() {
 			runClients: func(t *testing.T, clientsCount int) {
 				clients := getClientsArray(t, clientsCount)
 
-				// currently we have a bug with paths which are not Huffman encoded, therefor I am skipping them by string length 3.
 				for i := 1; i < 100; i++ {
 					path := strings.Repeat("a", i)
 					client := clients[getClientsIndex(i, clientsCount)]
