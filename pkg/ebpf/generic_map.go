2d8e30c52d22bda19cc50fc6265a586b41155548
Guillermo Julián <guillermo.julian@datadoghq.com>
2023-12-28 05:41:00 -0800

    Implement generic map

--- pkg/ebpf/generic_map.go
+++ pkg/ebpf/generic_map.go
@@ -6,23 +6,83 @@
 package ebpf
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
+	"sync"
 	"unsafe"
 
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 	manager "github.com/DataDog/ebpf-manager"
 	"github.com/cilium/ebpf"
 )
 
+var (
+	batchAPISupportedOnce sync.Once
+	batchAPISupported     bool
+)
+
+// BatchAPISupported returns true if the kernel supports the batch API for maps
+func BatchAPISupported() bool {
+	batchAPISupportedOnce.Do(func() {
+		// Do feature detection directly instead of based on kernel versions for more accuracy
+		m, err := ebpf.NewMap(&ebpf.MapSpec{
+			Type:       ebpf.Hash,
+			KeySize:    4,
+			ValueSize:  4,
+			MaxEntries: 10,
+		})
+		if err != nil {
+			log.Warnf("Failed to create map for batch API test: %v, will mark batch API as unsupported", err)
+			batchAPISupported = false
+			return
+		}
+
+		keys := make([]uint32, 1)
+		values := make([]uint32, 1)
+
+		// Do a batch update, check the result.
+		// We do an update instead of a lookup because it's more reliable for detection
+		_, err = m.BatchUpdate(keys, values, nil)
+		batchAPISupported = err == nil
+	})
+	return batchAPISupported
+}
+
+// GenericMap is a wrapper around ebpf.Map that allows to use generic types.
+// Also includes support for batch iterations
 type GenericMap[K interface{}, V interface{}] struct {
 	m *ebpf.Map
 }
 
+// NewGenericMap creates a new GenericMap with the given spec. Key and Value sizes are automatically
+// inferred from the types of K and V.
+func NewGenericMap[K interface{}, V interface{}](spec *ebpf.MapSpec) (*GenericMap[K, V], error) {
+	// Automatic inference of sizes. We assume that K/V are simple types that
+	// can be instantiated with no arguments
+	var kval K
+	var vval V
+	spec.KeySize = uint32(reflect.TypeOf(kval).Size())
+	spec.ValueSize = uint32(reflect.TypeOf(vval).Size())
+
+	m, err := ebpf.NewMap(spec)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GenericMap[K, V]{
+		m: m,
+	}, nil
+}
+
+// Map creates a new GenericMap from an existing ebpf.Map
 func Map[K interface{}, V interface{}](m *ebpf.Map) *GenericMap[K, V] {
 	return &GenericMap[K, V]{
 		m: m,
 	}
 }
 
+// GetMap gets the generic map with the given name from the manager
 func GetMap[K interface{}, V interface{}](mgr *manager.Manager, name string) (*GenericMap[K, V], error) {
 	m, _, err := mgr.GetMap(name)
 	if err != nil {
@@ -34,40 +94,138 @@ func GetMap[K interface{}, V interface{}](mgr *manager.Manager, name string) (*G
 	return Map[K, V](m), nil
 }
 
+// Map returns the underlying ebpf.Map
 func (g *GenericMap[K, V]) Map() *ebpf.Map {
 	return g.m
 }
 
-func (g *GenericMap[K, V]) Iterate() *GenericMapIterator[K, V] {
-	return &GenericMapIterator[K, V]{
-		it: g.m.Iterate(),
-	}
+// IteratorOptions are options for the Iterate method
+type IteratorOptions struct {
+	BatchSize       int  // Number of items to fetch per batch. If 0, use default value (100)
+	ForceSingleItem bool // Force the use of the single item iterator even if the batch API is supported
 }
 
+// Put inserts a new key/value pair in the map. If the key already exists, the value is updated
 func (g *GenericMap[K, V]) Put(key *K, value *V) error {
 	return g.Update(key, value, ebpf.UpdateAny)
 }
 
+// Update updates the value of an existing key in the map. If the key doesn't exist, it returns an error
 func (g *GenericMap[K, V]) Update(key *K, value *V, flags ebpf.MapUpdateFlags) error {
 	return g.m.Update(unsafe.Pointer(key), unsafe.Pointer(value), flags)
 }
 
+// Lookup looks up a key in the map and returns the value. If the key doesn't exist, it returns an error
 func (g *GenericMap[K, V]) Lookup(key *K, valueOut *V) error {
 	return g.m.Lookup(unsafe.Pointer(key), unsafe.Pointer(valueOut))
 }
 
+// Delete deletes a key from the map. If the key doesn't exist, it returns an error
 func (g *GenericMap[K, V]) Delete(key *K) error {
 	return g.m.Delete(unsafe.Pointer(key))
 }
 
-type GenericMapIterator[K interface{}, V interface{}] struct {
+// GenericMapIterator is an interface for iterating over a GenericMap
+type GenericMapIterator[K interface{}, V interface{}] interface {
+	// Next fills K and V with the next key/value pair in the map. It returns false if there are no more elements
+	Next(key *K, value *V) bool
+
+	// Err returns the last error that happened during iteration.
+	Err() error
+}
+
+// TODO: This is copied from pkg/collector/corechecks/ebpf/probe/probe.go temporarily
+// I feel like this should be in a generic package (ebpf-manager maybe?) but I'm not sure,
+// so I'm leaving it here for now until PR review
+func isPerCPU(typ ebpf.MapType) bool {
+	switch typ {
+	case ebpf.PerCPUHash, ebpf.PerCPUArray, ebpf.LRUCPUHash:
+		return true
+	}
+	return false
+}
+
+// Iterate returns an iterator for the map, which transparenlty chooses between batch and single item
+// iterations.
+func (g *GenericMap[K, V]) Iterate(itops IteratorOptions) GenericMapIterator[K, V] {
+	if itops.BatchSize == 0 {
+		itops.BatchSize = 100 // Default value for batch sizes. Possibly needs more testing to find an optimal default
+	}
+
+	if BatchAPISupported() && !isPerCPU(g.m.Type()) && !itops.ForceSingleItem {
+		return &genericMapBatchIterator[K, V]{
+			m:         g.m,
+			batchSize: itops.BatchSize,
+			keys:      make([]K, itops.BatchSize),
+			values:    make([]V, itops.BatchSize),
+		}
+	}
+
+	return &genericMapItemIterator[K, V]{
+		it: g.m.Iterate(),
+	}
+}
+
+type genericMapItemIterator[K interface{}, V interface{}] struct {
 	it *ebpf.MapIterator
 }
 
-func (g *GenericMapIterator[K, V]) Next(key *K, value *V) bool {
+func (g *genericMapItemIterator[K, V]) Next(key *K, value *V) bool {
 	return g.it.Next(unsafe.Pointer(key), unsafe.Pointer(value))
 }
 
-func (g *GenericMapIterator[K, V]) Err() error {
+func (g *genericMapItemIterator[K, V]) Err() error {
 	return g.it.Err()
+}
+
+type genericMapBatchIterator[K interface{}, V interface{}] struct {
+	m                *ebpf.Map
+	batchSize        int
+	cursor           ebpf.BatchCursor
+	keys             []K
+	values           []V
+	currentBatchSize int
+	inBatchIndex     int
+	err              error
+	totalCount       int
+	lastBatch        bool
+}
+
+func (g *genericMapBatchIterator[K, V]) Next(key *K, value *V) bool {
+	// Safety check to avoid an infinite loop
+	if g.totalCount >= int(g.m.MaxEntries()) {
+		return false
+	}
+
+	// We have finished all the values in the current batch (or there wasn't any batch
+	// to begin with with g.currentBatchSize == 0), so we need to fetch the next batch
+	if g.inBatchIndex >= g.currentBatchSize {
+		if g.lastBatch {
+			return false
+		}
+
+		g.currentBatchSize, g.err = g.m.BatchLookup(&g.cursor, g.keys, g.values, nil)
+		g.inBatchIndex = 0
+		if g.err != nil && errors.Is(g.err, ebpf.ErrKeyNotExist) {
+			// The lookup API returns ErrKeyNotExist when this is the last batch,
+			// even when partial results are returned. We need to mark this so that
+			// we don't try to fetch another batch when this one is finished
+			g.lastBatch = true
+		} else if g.err != nil {
+			return false
+		}
+	}
+
+	// At this point we know for sure that keys/values are populated with values
+	// from a previous call to BatchLookup.
+	*key = g.keys[g.inBatchIndex]
+	*value = g.values[g.inBatchIndex]
+	g.inBatchIndex++
+	g.totalCount++
+
+	return true
+}
+
+func (g *genericMapBatchIterator[K, V]) Err() error {
+	return g.err
 }
---
+++ pkg/ebpf/generic_map_test.go
@@ -0,0 +1,263 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build linux_bpf
+
+package ebpf
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/util/kernel"
+	"github.com/cilium/ebpf"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBatchAPISupported(t *testing.T) {
+	// Batch API is supported on kernels >= 5.6, so make sure that in those cases
+	// it returns true
+	kernelVersion, err := kernel.HostVersion()
+	require.NoError(t, err)
+
+	if kernelVersion <= kernel.VersionCode(5, 6, 0) {
+		require.False(t, BatchAPISupported())
+	} else {
+		require.True(t, BatchAPISupported())
+	}
+}
+
+func TestSingleItemIter(t *testing.T) {
+	m, err := NewGenericMap[uint32, uint32](&ebpf.MapSpec{
+		Type:       ebpf.Hash,
+		MaxEntries: 10,
+	})
+	require.NoError(t, err)
+
+	numsToPut := []uint32{1, 2, 3, 4, 5}
+	for _, num := range numsToPut {
+		require.NoError(t, m.Put(&num, &num))
+	}
+
+	var k uint32
+	var v uint32
+	numElements := 0
+	foundElements := make(map[uint32]bool)
+
+	it := m.Iterate(IteratorOptions{ForceSingleItem: true})
+	require.NotNil(t, it)
+	require.IsType(t, &genericMapItemIterator[uint32, uint32]{}, it)
+	for it.Next(&k, &v) {
+		numElements++
+		foundElements[k] = true
+	}
+
+	require.Equal(t, len(numsToPut), numElements)
+	for _, num := range numsToPut {
+		require.True(t, foundElements[num])
+	}
+}
+
+func TestBatchIter(t *testing.T) {
+	if !BatchAPISupported() {
+		t.Skip("Batch API not supported")
+	}
+
+	m, err := NewGenericMap[uint32, uint32](&ebpf.MapSpec{
+		Type:       ebpf.Hash,
+		MaxEntries: 100,
+	})
+	require.NoError(t, err)
+
+	numsToPut := uint32(50)
+	for i := uint32(0); i < numsToPut; i++ {
+		require.NoError(t, m.Put(&i, &i))
+	}
+
+	var k uint32
+	var v uint32
+	numElements := uint32(0)
+	foundElements := make(map[uint32]bool)
+
+	it := m.Iterate(IteratorOptions{BatchSize: 10})
+	require.NotNil(t, it)
+	require.IsType(t, &genericMapBatchIterator[uint32, uint32]{}, it)
+	for it.Next(&k, &v) {
+		numElements++
+		foundElements[k] = true
+	}
+
+	require.Equal(t, numsToPut, numElements)
+	for i := uint32(0); i < numsToPut; i++ {
+		require.True(t, foundElements[i])
+	}
+}
+
+func TestBatchIterLessItemsThanBatchSize(t *testing.T) {
+	if !BatchAPISupported() {
+		t.Skip("Batch API not supported")
+	}
+
+	m, err := NewGenericMap[uint32, uint32](&ebpf.MapSpec{
+		Type:       ebpf.Hash,
+		MaxEntries: 100,
+	})
+	require.NoError(t, err)
+
+	numsToPut := uint32(5)
+	for i := uint32(0); i < numsToPut; i++ {
+		require.NoError(t, m.Put(&i, &i))
+	}
+
+	var k uint32
+	var v uint32
+	numElements := uint32(0)
+	foundElements := make(map[uint32]bool)
+
+	it := m.Iterate(IteratorOptions{BatchSize: 10})
+	require.NotNil(t, it)
+	require.IsType(t, &genericMapBatchIterator[uint32, uint32]{}, it)
+	for it.Next(&k, &v) {
+		numElements++
+		foundElements[k] = true
+	}
+
+	require.Equal(t, numsToPut, numElements)
+	for i := uint32(0); i < numsToPut; i++ {
+		require.True(t, foundElements[i])
+	}
+}
+
+func TestBatchIterWhileUpdated(t *testing.T) {
+	if !BatchAPISupported() {
+		t.Skip("Batch API not supported")
+	}
+
+	maxEntries := 50
+	m, err := NewGenericMap[uint32, uint32](&ebpf.MapSpec{
+		Type:       ebpf.Hash,
+		MaxEntries: uint32(maxEntries),
+	})
+	require.NoError(t, err)
+
+	numsToPut := uint32(50)
+	for i := uint32(0); i < numsToPut; i++ {
+		require.NoError(t, m.Put(&i, &i))
+	}
+
+	var k uint32
+	var v uint32
+	numElements := uint32(0)
+	foundElements := make(map[uint32]bool)
+	updateEachElements := 25
+	updatesDone := 0
+
+	it := m.Iterate(IteratorOptions{BatchSize: 10})
+	require.NotNil(t, it)
+	require.IsType(t, &genericMapBatchIterator[uint32, uint32]{}, it)
+	for it.Next(&k, &v) {
+		numElements++
+		foundElements[k] = true
+
+		// Not recommended! But helps us simulate the case where the map is updated
+		// as we iterate over it. We are not concerned with correctness here but we
+		// want to make sure that the iterator doesn't crash or run into an infinite
+		// loop
+		if numElements%uint32(updateEachElements) == 0 {
+			for i := uint32(0); i < numsToPut; i++ {
+				oldKey := i + uint32(updatesDone)*10
+				newKey := i + uint32(updatesDone+1)*10
+				require.NoError(t, m.Delete(&oldKey))
+				require.NoError(t, m.Put(&newKey, &newKey))
+			}
+
+			updatesDone++
+		}
+
+		require.LessOrEqual(t, numElements, uint32(maxEntries))
+	}
+
+	// Again, just concerned with exiting the loop and not correctness
+	require.LessOrEqual(t, numElements, uint32(maxEntries))
+}
+
+func TestBatchIterAllocsPerRun(t *testing.T) {
+	if !BatchAPISupported() {
+		t.Skip("Batch API not supported")
+	}
+
+	m, err := NewGenericMap[uint32, uint32](&ebpf.MapSpec{
+		Type:       ebpf.Hash,
+		MaxEntries: 10000,
+	})
+	require.NoError(t, err)
+
+	numsToPut := uint32(5000)
+	for i := uint32(0); i < numsToPut; i++ {
+		require.NoError(t, m.Put(&i, &i))
+	}
+
+	var k uint32
+	var v uint32
+	batchSize := 100
+
+	allocs := testing.AllocsPerRun(10, func() {
+		numElements := uint32(0)
+		it := m.Iterate(IteratorOptions{BatchSize: batchSize})
+		for it.Next(&k, &v) {
+			numElements++
+		}
+		require.Equal(t, numsToPut, numElements)
+	})
+
+	numBatches := int(numsToPut) / batchSize
+	expectedAllocs := float64(numBatches * 2) // 2 allocations per batch because ebpf has two buffers to allocate on each batch...
+	expectedAllocs *= 1.1                     // ...and we add a 10% margin to account for any other allocations that might happen
+
+	require.LessOrEqual(t, allocs, expectedAllocs)
+}
+
+func BenchmarkIterate(b *testing.B) {
+	setupAndBenchmark := func(b *testing.B, forceSingleItem bool, maxEntries int, numEntries int, batchSize int) {
+		m, err := NewGenericMap[uint32, uint32](&ebpf.MapSpec{
+			Type:       ebpf.Hash,
+			MaxEntries: uint32(maxEntries),
+		})
+		require.NoError(b, err)
+
+		for i := uint32(0); i < uint32(numEntries); i++ {
+			require.NoError(b, m.Put(&i, &i))
+		}
+
+		var benchName string
+		if forceSingleItem {
+			benchName = fmt.Sprintf("BenchmarkIterateSingleItem-%dentries-%dbatch", numEntries, batchSize)
+		} else {
+			benchName = fmt.Sprintf("BenchmarkIterateBatch-%dentries-%dbatch", numEntries, batchSize)
+		}
+
+		b.Run(benchName, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				var k uint32
+				var v uint32
+
+				it := m.Iterate(IteratorOptions{BatchSize: batchSize, ForceSingleItem: forceSingleItem})
+				for it.Next(&k, &v) {
+				}
+			}
+		})
+	}
+
+	batchSizes := []int{5, 10, 20, 50, 100}
+	for _, batchSize := range batchSizes {
+		for _, numEntries := range []int{100, 1000, 10000} {
+			if BatchAPISupported() {
+				setupAndBenchmark(b, false, numEntries, numEntries, batchSize)
+			}
+
+			setupAndBenchmark(b, true, numEntries, numEntries, batchSize)
+		}
+	}
+}
