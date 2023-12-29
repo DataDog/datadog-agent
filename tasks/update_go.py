c47331c6c5d8fa68703c3c8aa87295fbe83d6eb9
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2023-12-29 16:20:42 +0100

    fix: specify encoding for windows

--- tasks/update_go.py
+++ tasks/update_go.py
@@ -130,7 +130,7 @@ def _update_file(warn: bool, path: str, pattern: str, replace: str, expected_mat
     # newline='' keeps the file's newline character(s)
     # meaning it keeps '\n' for most files and '\r\n' for windows specific files
 
-    with open(path, "r", newline='') as reader:
+    with open(path, "r", newline='', encoding='utf-8') as reader:
         content = reader.read()
 
     if dry_run:
