6b3acf4d8173469ea65135adb40c6cbc48f43b82
Kevin Fairise <kevin.fairise@datadoghq.com>
2023-12-29 09:44:03 +0100

    Factorize code

--- test/new-e2e/pkg/utils/e2e/client/vm_client.go
+++ test/new-e2e/pkg/utils/e2e/client/vm_client.go
@@ -35,80 +35,32 @@ type VMClient struct {
 // NewVMClient creates a new instance of VMClient.
 func NewVMClient(t *testing.T, connection *utils.Connection, osType componentos.Type) (*VMClient, error) {
 	t.Logf("connecting to remote VM at %s:%s", connection.User, connection.Host)
-	var privateSSHKey, privateKeyPassphraseBytes []byte
-
-	privateKeyPath, err := runner.GetProfile().ParamStore().GetWithDefault(parameters.PrivateKeyPath, "")
-	if err != nil {
-		return nil, err
-	}
-
-	if privateKeyPath != "" {
-		privateSSHKey, err = os.ReadFile(privateKeyPath)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	privateKeyPassphrase, err := runner.GetProfile().SecretStore().GetWithDefault(parameters.PrivateKeyPassword, "")
-	if err != nil {
-		return nil, err
-	}
-	if privateKeyPassphrase != "" {
-		privateKeyPassphraseBytes = []byte(privateKeyPassphrase)
-	}
-
-	client, _, err := clients.GetSSHClient(
-		connection.User,
-		fmt.Sprintf("%s:%d", connection.Host, 22),
-		privateSSHKey,
-		privateKeyPassphraseBytes,
-		2*time.Second, 5)
-	return &VMClient{
-		client:     client,
+	vmClient := &VMClient{
 		connection: connection,
 		osType:     osType,
 		t:          t,
-	}, err
+	}
+
+	err := vmClient.connect()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return vmClient, nil
 }
 
 // ReconnectSSH recreate the SSH connection to the VM. Should be used only after VM reboot to restore the SSH connection.
 func (vmClient *VMClient) ReconnectSSH() error {
-	var privateSSHKey, privateKeyPassphraseBytes []byte
-
-	privateKeyPath, err := runner.GetProfile().ParamStore().GetWithDefault(parameters.PrivateKeyPath, "")
-	if err != nil {
-		return err
-	}
-
-	if privateKeyPath != "" {
-		privateSSHKey, err = os.ReadFile(privateKeyPath)
-		if err != nil {
-			return err
-		}
-	}
-
-	privateKeyPassphrase, err := runner.GetProfile().SecretStore().GetWithDefault(parameters.PrivateKeyPassword, "")
-	if err != nil {
-		return err
-	}
-	if privateKeyPassphrase != "" {
-		privateKeyPassphraseBytes = []byte(privateKeyPassphrase)
-	}
-
-	client, _, err := clients.GetSSHClient(
-		vmClient.connection.User,
-		fmt.Sprintf("%s:%d", vmClient.connection.Host, 22),
-		privateSSHKey,
-		privateKeyPassphraseBytes,
-		5*time.Second, 60)
-	if err != nil {
-		return err
-	}
-
 	if vmClient.client != nil {
 		vmClient.client.Close()
 	}
-	vmClient.client = client
+
+	err := vmClient.connect()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -224,6 +176,44 @@ func (vmClient *VMClient) setEnvVariables(command string, envVar executeparams.E
 	}
 	return cmd
 
+}
+
+func (vmClient *VMClient) connect() error {
+	var privateSSHKey, privateKeyPassphraseBytes []byte
+
+	privateKeyPath, err := runner.GetProfile().ParamStore().GetWithDefault(parameters.PrivateKeyPath, "")
+	if err != nil {
+		return err
+	}
+
+	if privateKeyPath != "" {
+		privateSSHKey, err = os.ReadFile(privateKeyPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	privateKeyPassphrase, err := runner.GetProfile().SecretStore().GetWithDefault(parameters.PrivateKeyPassword, "")
+	if err != nil {
+		return err
+	}
+	if privateKeyPassphrase != "" {
+		privateKeyPassphraseBytes = []byte(privateKeyPassphrase)
+	}
+
+	client, _, err := clients.GetSSHClient(
+		vmClient.connection.User,
+		fmt.Sprintf("%s:%d", vmClient.connection.Host, 22),
+		privateSSHKey,
+		privateKeyPassphraseBytes,
+		2*time.Second, 5)
+	if err != nil {
+		return err
+	}
+
+	vmClient.client = client
+
+	return nil
 }
 
 // GetOSType returns the operating system type of the VMClient instance.
