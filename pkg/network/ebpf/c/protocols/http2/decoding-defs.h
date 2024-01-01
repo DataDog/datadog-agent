9e38d3c2d49de007047b8a12bc87a0317c42a4d4
amit.slavin <amit.slavin@datadoghq.com>
2024-01-01 18:19:23 +0200

    Increment the number of interesting frames

--- pkg/network/ebpf/c/protocols/http2/decoding-defs.h
+++ pkg/network/ebpf/c/protocols/http2/decoding-defs.h
@@ -9,7 +9,7 @@
 #define HTTP2_MAX_FRAMES_FOR_EOS_PARSER_PER_TAIL_CALL 200
 // Represents the maximum number of tail calls to process EOS frames.
 // Currently we have up to 120 frames in a packet, thus 1 tail call is enough.
-#define HTTP2_MAX_TAIL_CALLS_FOR_EOS_PARSER 1
+#define HTTP2_MAX_TAIL_CALLS_FOR_EOS_PARSER 2
 #define HTTP2_MAX_FRAMES_FOR_EOS_PARSER (HTTP2_MAX_FRAMES_FOR_EOS_PARSER_PER_TAIL_CALL * HTTP2_MAX_TAIL_CALLS_FOR_EOS_PARSER)
 
 // Represents the maximum number of frames we'll process in a single tail call in `handle_headers_frames` program.
@@ -24,7 +24,7 @@
 // The number of frames to parse is determined by HTTP2_MAX_FRAMES_ITERATIONS, resulting in a total defined as:
 // HTTP2_MAX_FRAMES_ITERATIONS * HTTP2_MAX_TAIL_CALLS_FOR_FRAMES_FILTER (for tail call 0-1)
 #define HTTP2_MAX_TAIL_CALLS_FOR_FRAMES_FILTER 1
-#define HTTP2_MAX_FRAMES_TO_FILTER  120
+#define HTTP2_MAX_FRAMES_TO_FILTER  240
 
 // A limit of max headers which we process in the request/response.
 #define HTTP2_MAX_HEADERS_COUNT_FOR_FILTERING 25
