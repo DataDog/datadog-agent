a327c2ea10017756f73f88ba2e7149fc6d2c95b8
usamasaqib <usama.saqib@datadoghq.com>
2024-01-03 17:44:00 +0100

    improve first build detection

--- tasks/kmt.py
+++ tasks/kmt.py
@@ -244,9 +244,6 @@ def full_arch(arch):
     return arch
 
 
-def first_build(ctx):
-    pass
-
 @task
 def prepare(ctx, vms, stack=None, arch=None, ssh_key="", rebuild_deps=False, packages="", verbose=False):
     stack = check_and_get_stack(stack)
@@ -267,7 +264,7 @@ def prepare(ctx, vms, stack=None, arch=None, ssh_key="", rebuild_deps=False, pac
     domains = build_target_domains(ctx, stack, vms, ssh_key, verbose)
 
     constrain_pkgs=""
-    if not rebuild_deps:
+    if not rebuild_deps or (not os.path.isfile(f"kmt-deps/{stack}/dependencies-{arch}.tar.gz")):
         constrain_pkgs = f"--packages={packages}"
 
     docker_exec(
