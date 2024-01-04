9359a97efa1e2d9557d2c7028a95a222b4a48da2
Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
2024-01-04 11:01:57 +0100

    fix python linter comment

--- tasks/vscode.py
+++ tasks/vscode.py
@@ -128,19 +128,19 @@ def setup_devcontainer(
                     "--build-tags",
                     local_build_tags,
                     "--config",
-                    "/workspaces/datadog-agent/.golangci.yml"
+                    "/workspaces/datadog-agent/.golangci.yml",
                 ],
                 "[go]": {
                     "editor.formatOnSave": True,
                 },
-                "gopls": {
-                    "formatting.local": "github.com/DataDog/datadog-agent"
-                }
+                "gopls": {"formatting.local": "github.com/DataDog/datadog-agent"},
             },
             "extensions": ["golang.Go"],
         }
     }
-    devcontainer["postStartCommand"] = "git config --global --add safe.directory /workspaces/datadog-agent && invoke install-tools && invoke deps"
+    devcontainer[
+        "postStartCommand"
+    ] = "git config --global --add safe.directory /workspaces/datadog-agent && invoke install-tools && invoke deps"
 
     with open(fullpath, "w") as sf:
         json.dump(devcontainer, sf, indent=4, sort_keys=False, separators=(',', ': '))
