2b8647eebf13ff4d01541e60e09f4ec05290d6fc
Corentin Chary <corentin.chary@datadoghq.com>
2024-01-05 15:22:53 +0100

    dogstatsd/aggregator: add more memory telemetry (#21247)
    dogstatsd/aggregator: add more memory telemetry

--- comp/dogstatsd/server/server_worker.go
+++ comp/dogstatsd/server/server_worker.go
@@ -6,6 +6,7 @@
 package server
 
 import (
+	"github.com/DataDog/datadog-agent/comp/dogstatsd/packets"
 	"github.com/DataDog/datadog-agent/pkg/aggregator"
 	"github.com/DataDog/datadog-agent/pkg/metrics"
 )
@@ -55,11 +56,12 @@ func (w *worker) run() {
 		case <-w.server.health.C:
 		case <-w.server.serverlessFlushChan:
 			w.batcher.flush()
-		case packets := <-w.server.packetsIn:
+		case ps := <-w.server.packetsIn:
+			packets.TelemetryUntrackPackets(ps)
 			w.samples = w.samples[0:0]
 			// we return the samples in case the slice was extended
 			// when parsing the packets
-			w.samples = w.server.parsePackets(w.batcher, w.parser, packets, w.samples)
+			w.samples = w.server.parsePackets(w.batcher, w.parser, ps, w.samples)
 		}
 
 	}
