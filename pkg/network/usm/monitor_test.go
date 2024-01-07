6adcdf828ce2f520556e3465723f78c63f5bdcb8
amit.slavin <amit.slavin@datadoghq.com>
2024-01-07 17:17:20 +0200

    fixed cr notes

--- pkg/network/usm/monitor_test.go
+++ pkg/network/usm/monitor_test.go
@@ -964,7 +964,7 @@ var (
 		0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00,
 	}
 
-	// http2 request to path /aaa
+	// http2 request to path /aaa with index 5
 	request = []byte{
 		0x00, 0x00, 0x37, 0x01, 0x04, 0x00, 0x00, 0x00, 0x03, 0x41, 0x8a, 0x08, 0x9d, 0x5c, 0x0b, 0x81,
 		0x70, 0xdc, 0x78, 0x0f, 0x0b, 0x83, 0x45, 0x83, 0x60, 0x63, 0x1f, 0x86, 0x5f, 0x8b, 0x1d, 0x75,
@@ -972,11 +972,12 @@ var (
 		0xab, 0x7a, 0x8d, 0xc4, 0x75, 0xa7, 0x4a, 0x6b, 0x58, 0x94, 0x18, 0xb5, 0x25, 0x81, 0x2e, 0x0f,
 	}
 
-	emptyRequest = []byte{
-		0x00, 0x00, 0x33, 0x01, 0x04, 0x00, 0x00, 0x00, 0x03, 0x41, 0x8a, 0x08, 0x9d, 0x5c, 0x0b, 0x81,
-		0x70, 0xdc, 0x78, 0x0f, 0x0b, 0x83, 0x84, 0x86, 0x5f, 0x8b, 0x1d, 0x75, 0xd0, 0x62, 0x0d, 0x26,
-		0x3d, 0x4c, 0x74, 0x41, 0xea, 0x5c, 0x01, 0x34, 0x50, 0x83, 0x9b, 0xd9, 0xab, 0x7a, 0x8d, 0xc4,
-		0x75, 0xa7, 0x4a, 0x6b, 0x58, 0x94, 0x18, 0xb5, 0x25, 0x81, 0x2e, 0x0f,
+	// http2 request to path /aaa with index 4 (same as above but with different index changed from 0x45 to 0x44)
+	requestWithIndex4 = []byte{
+		0x00, 0x00, 0x37, 0x01, 0x04, 0x00, 0x00, 0x00, 0x03, 0x41, 0x8a, 0x08, 0x9d, 0x5c, 0x0b, 0x81,
+		0x70, 0xdc, 0x78, 0x0f, 0x0b, 0x83, 0x44, 0x83, 0x60, 0x63, 0x1f, 0x86, 0x5f, 0x8b, 0x1d, 0x75,
+		0xd0, 0x62, 0x0d, 0x26, 0x3d, 0x4c, 0x74, 0x41, 0xea, 0x5c, 0x01, 0x34, 0x50, 0x83, 0x9b, 0xd9,
+		0xab, 0x7a, 0x8d, 0xc4, 0x75, 0xa7, 0x4a, 0x6b, 0x58, 0x94, 0x18, 0xb5, 0x25, 0x81, 0x2e, 0x0f,
 	}
 
 	// data frame & end of stream
@@ -985,22 +986,10 @@ var (
 	}
 )
 
-// createRawEmptyRequestFrame creates a raw request frame with the given streamID and path index 4 ("/").
-func createRawEmptyRequestFrame(streamID int) []byte {
-	binary.BigEndian.PutUint16(emptyRequest[7:9], uint16(streamID))
-	return emptyRequest
-}
-
-// createRawRequestFrame creates a raw request frame with the given streamID.
-func createRawRequestFrame(streamID int) []byte {
-	binary.BigEndian.PutUint16(request[7:9], uint16(streamID))
-	return request
-}
-
-// createRawRequestFrame creates a raw request frame with the given streamID.
-func createRawDataFrame(streamID int) []byte {
-	binary.BigEndian.PutUint16(dataFrame[7:9], uint16(streamID))
-	return dataFrame
+// changeStreamIDInFrame changes the streamID in the given frame to the given streamID.
+func changeStreamIDInFrame(buf []byte, streamID int) []byte {
+	binary.BigEndian.PutUint16(buf[7:9], uint16(streamID))
+	return buf
 }
 
 func (s *USMHTTP2Suite) TestRawTraffic() {
@@ -1014,7 +1003,7 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 		name                  string
 		numberOfSettingFrames int
 		numberOfRequestFrames int
-		isEmptyPath           bool
+		requestBuffer         []byte
 		expectedEndpoints     map[http.Key]int
 	}{
 		{
@@ -1023,6 +1012,7 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 			// a single program.
 			numberOfSettingFrames: 100,
 			numberOfRequestFrames: 1,
+			requestBuffer:         request,
 			expectedEndpoints: map[http.Key]int{
 				{
 					Path:   http.Path{Content: http.Interner.GetString("/aaa")},
@@ -1036,6 +1026,7 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 			// the filtering of subsequent frames will continue using tail calls.
 			numberOfSettingFrames: 130,
 			numberOfRequestFrames: 1,
+			requestBuffer:         request,
 			expectedEndpoints: map[http.Key]int{
 				{
 					Path:   http.Path{Content: http.Interner.GetString("/aaa")},
@@ -1049,6 +1040,7 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 			// for 2 filter_frames we do not use more than two tail calls.
 			numberOfSettingFrames: 250,
 			numberOfRequestFrames: 1,
+			requestBuffer:         request,
 			expectedEndpoints:     nil,
 		},
 		{
@@ -1057,6 +1049,7 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 			// determines the maximum number of "interesting frames" we can process.
 			numberOfSettingFrames: 0,
 			numberOfRequestFrames: 120,
+			requestBuffer:         request,
 			expectedEndpoints: map[http.Key]int{
 				{
 					Path:   http.Path{Content: http.Interner.GetString("/aaa")},
@@ -1070,6 +1063,7 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 			// and validate that we cannot handle more than that limit.
 			numberOfSettingFrames: 0,
 			numberOfRequestFrames: 130,
+			requestBuffer:         request,
 			expectedEndpoints: map[http.Key]int{
 				{
 					Path:   http.Path{Content: http.Interner.GetString("/aaa")},
@@ -1082,10 +1076,10 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 			// The purpose of this test is to verify our ability to identify paths with index 4.
 			numberOfSettingFrames: 0,
 			numberOfRequestFrames: 5,
-			isEmptyPath:           true,
+			requestBuffer:         requestWithIndex4,
 			expectedEndpoints: map[http.Key]int{
 				{
-					Path:   http.Path{Content: http.Interner.GetString("/")},
+					Path:   http.Path{Content: http.Interner.GetString("/aaa")},
 					Method: http.MethodPost,
 				}: 5,
 			},
@@ -1114,20 +1108,11 @@ func (s *USMHTTP2Suite) TestRawTraffic() {
 				reqInput = append(reqInput, settingsFrame...)
 			}
 
-			if tt.isEmptyPath {
-				// we have to use negative numbers for the stream id.
-				for i := 0; i < tt.numberOfRequestFrames; i++ {
-					streamID := 2*i + 1
-					reqInput = append(reqInput, createRawEmptyRequestFrame(streamID)...)
-					reqInput = append(reqInput, createRawDataFrame(streamID)...)
-				}
-			} else {
-				// we have to use negative numbers for the stream id.
-				for i := 0; i < tt.numberOfRequestFrames; i++ {
-					streamID := 2*i + 1
-					reqInput = append(reqInput, createRawRequestFrame(streamID)...)
-					reqInput = append(reqInput, createRawDataFrame(streamID)...)
-				}
+			// we have to use negative numbers for the stream id.
+			for i := 0; i < tt.numberOfRequestFrames; i++ {
+				streamID := 2*i + 1
+				reqInput = append(reqInput, changeStreamIDInFrame(tt.requestBuffer, streamID)...)
+				reqInput = append(reqInput, changeStreamIDInFrame(dataFrame, streamID)...)
 			}
 
 			// Sending the repeated settings with request.
