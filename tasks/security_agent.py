bf1082994a54056879b77137d08c8961d36eccf8
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2024-01-08 14:27:36 +0100

    Rework the ebpfless syscalls handlers and add mkdir/mkdirat/utime/utimes/utimensat/futimesat syscalls.

--- tasks/security_agent.py
+++ tasks/security_agent.py
@@ -218,7 +218,7 @@ def run_ebpfless_functional_tests(ctx, cws_instrumentation, testsuite, verbose=F
         "verbose_opt": "-test.v" if verbose else "",
         "testflags": testflags,
         "path": os.environ['PATH'],
-        "tests": "-test.run '^(TestOpen|TestProcess|TimestampVariable|KillAction|TestRmdir|TestUnlink|TestRename)'",
+        "tests": "-test.run '^(TestOpen|TestProcess|TimestampVariable|KillAction|TestRmdir|TestUnlink|TestRename|TestMkdir|TestUtimes)'",
     }
 
     ctx.run(cmd.format(**args))
