b7e8ea4a8c120a53722245a6524c58af60c38145
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2024-01-09 14:03:32 +0100

    Add gocovmerge to the test go dependencies

--- tasks/go_test.py
+++ tasks/go_test.py
@@ -91,6 +91,7 @@ TOOL_LIST = [
     'github.com/stormcat24/protodep',
     'gotest.tools/gotestsum',
     'github.com/vektra/mockery/v2',
+    'github.com/wadey/gocovmerge',
 ]
 
 TOOL_LIST_PROTO = [
