3a2053575b57463616e1dfd382ece7828a6b8e1b
Scott Opell <me@scottopell.com>
2024-01-09 14:30:28 -0500

    Adds a comment referencing a future golang feature that will simplify interning

--- comp/dogstatsd/server/intern.go
+++ comp/dogstatsd/server/intern.go
@@ -38,6 +38,17 @@ var (
 // stringInterner is a string cache providing a longer life for strings,
 // helping to avoid GC runs because they're re-used many times instead of
 // created every time.
+//
+// The current interning strategy is fairly simple, but can require manual
+// adjustments of the `maxSize` to improve performance, which is not ideal.
+
+// However the current strategy works well enough, and there is an
+// accepted go proposal to offer an "interning" mechanism from the
+// go runtime directly.
+
+// Once this is available, the interner design should be re-visited to
+// take advantage of the new "Unique" api that is proposed below.
+// ref: https://github.com/golang/go/issues/62483
 type stringInterner struct {
 	strings map[string]string
 	maxSize int
