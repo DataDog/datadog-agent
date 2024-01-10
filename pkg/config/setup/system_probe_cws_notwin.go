f45ab0cbe91eda11094a55e0fe86c343ad3e1aca
Arthur Bellal <arthur.bellal@datadoghq.com>
2024-01-10 10:52:31 +0100

    missing install path

--- pkg/config/setup/system_probe_cws_notwin.go
+++ pkg/config/setup/system_probe_cws_notwin.go
@@ -15,5 +15,5 @@ import (
 
 func platformCWSConfig(cfg pkgconfigmodel.Config) {
 	cfg.BindEnvAndSetDefault("runtime_security_config.policies.dir", DefaultRuntimePoliciesDir)
-	cfg.BindEnvAndSetDefault("runtime_security_config.socket", filepath.Join("run/runtime-security.sock"))
+	cfg.BindEnvAndSetDefault("runtime_security_config.socket", filepath.Join(InstallPath, "run/runtime-security.sock"))
 }
