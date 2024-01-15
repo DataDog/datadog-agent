0436703a44c051cdb0c52b139013b0fa2dec35a7
Olivier G <olivier.gaca@datadoghq.com>
2024-01-15 15:37:03 +0100

    Move getSharedFxOption to command.go (no code change)

--- cmd/agent/subcommands/run/command.go
+++ cmd/agent/subcommands/run/command.go
@@ -57,3 +57,78 @@ func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 
 	return []*cobra.Command{startCmd, runCmd}
 }
+
+func getSharedFxOption() fx.Option {
+	return fx.Options(
+		fx.Supply(flare.NewParams(
+			path.GetDistPath(),
+			path.PyChecksPath,
+			path.DefaultLogFile,
+			path.DefaultJmxLogFile,
+			path.DefaultDogstatsDLogFile,
+		)),
+		flare.Module(),
+		core.Bundle(),
+		fx.Supply(dogstatsdServer.Params{
+			Serverless: false,
+		}),
+		forwarder.Bundle(),
+		fx.Provide(func(config config.Component, log log.Component) defaultforwarder.Params {
+			params := defaultforwarder.NewParams(config, log)
+			// Enable core agent specific features like persistence-to-disk
+			params.Options.EnabledFeatures = defaultforwarder.SetFeature(params.Options.EnabledFeatures, defaultforwarder.CoreFeatures)
+			return params
+		}),
+
+		// workloadmeta setup
+		collectors.GetCatalog(),
+		fx.Provide(defaults.DefaultParams),
+		workloadmeta.Module(),
+		apiimpl.Module(),
+
+		dogstatsd.Bundle(),
+		otelcol.Bundle(),
+		rcclient.Module(),
+
+		// TODO: (components) - some parts of the agent (such as the logs agent) implicitly depend on the global state
+		// set up by LoadComponents. In order for components to use lifecycle hooks that also depend on this global state, we
+		// have to ensure this code gets run first. Once the common package is made into a component, this can be removed.
+		//
+		// Workloadmeta component needs to be initialized before this hook is executed, and thus is included
+		// in the function args to order the execution. This pattern might be worth revising because it is
+		// error prone.
+		fx.Invoke(func(lc fx.Lifecycle, demultiplexer demultiplexer.Component, _ workloadmeta.Component, secretResolver secrets.Component) {
+			lc.Append(fx.Hook{
+				OnStart: func(ctx context.Context) error {
+
+					// create and setup the Autoconfig instance
+					common.LoadComponents(demultiplexer, secretResolver, pkgconfig.Datadog.GetString("confd_path"))
+					return nil
+				},
+			})
+		}),
+		logs.Bundle(),
+		langDetectionClimpl.Module(),
+		metadata.Bundle(),
+		// injecting the aggregator demultiplexer to FX until we migrate it to a proper component. This allows
+		// other already migrated components to request it.
+		fx.Provide(func(config config.Component) demultiplexerimpl.Params {
+			params := demultiplexerimpl.NewDefaultParams()
+			params.EnableNoAggregationPipeline = config.GetBool("dogstatsd_no_aggregation_pipeline")
+			return params
+		}),
+		demultiplexerimpl.Module(),
+		orchestratorForwarderImpl.Module(),
+		fx.Supply(orchestratorForwarderImpl.NewDefaultParams()),
+		// injecting the shared Serializer to FX until we migrate it to a prpoper component. This allows other
+		// already migrated components to request it.
+		fx.Provide(func(demuxInstance demultiplexer.Component) serializer.MetricSerializer {
+			return demuxInstance.Serializer()
+		}),
+		ndmtmp.Bundle(),
+		netflow.Bundle(),
+		fx.Provide(func(demultiplexer demultiplexer.Component) optional.Option[collector.Collector] {
+			return optional.NewOption(common.LoadCollector(demultiplexer))
+		}),
+	)
+}
