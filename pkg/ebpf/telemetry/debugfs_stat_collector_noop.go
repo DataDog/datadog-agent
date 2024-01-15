5caf1af138b39c7efb0e23281cdfb98293c5c655
Valeri Pliskin <valeri.pliskin@datadoghq.com>
2024-01-15 15:00:28 +0200

    - fixed revive linter - aligned files names

--- pkg/ebpf/telemetry/debugfs_stat_collector_noop.go
+++ pkg/ebpf/telemetry/debugfs_stat_collector_noop.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package telemetry provides types and functions for kernel telemetry collected by eBPF programs.
 package telemetry
 
 import "github.com/prometheus/client_golang/prometheus"
