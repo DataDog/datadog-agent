d7f90ed2a2001ca75d0c2759f5fc1bde5030d575
pducolin <paola.ducolin@datadoghq.com>
2024-01-17 17:35:54 +0100

    [flake] update package name

--- pkg/util/testutil/flake/flake.go
+++ pkg/util/testutil/flake/flake.go
@@ -3,13 +3,10 @@
 // This product includes software developed at Datadog (https://www.Datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-// Package flake provides a [testing.TB](https://pkg.go.dev/testing#TB) implementation that support flaky tests.
-// Flaky tests are tests that are not reliable and may fail for various reasons.
-// Wrap your *testing.T with [flake.FlakyTesting] to allow flaky tests to fail or skip them.
-// Use flags `allow-flake-failure` and `skip-flake` to control the behavior, or set the environment variables `ALLOW_FLAKE_FAILURE` and `SKIP_FLAKE`.
-// If both `allow-flake-failure` and `skip-flake` are set, `skip-flake` takes precedence.
+// Package flake marks an instance of [testing.TB](https://pkg.go.dev/testing#TB) as flake.
+// Use [flake.Mark] to mark a known flake test.
+// Use `skip-flake` to control the behavior, or set the environment variable `SKIP_FLAKE`.
 // Flags take precedence over environment variables.
-// Subtests from [testing.T.Run](https://pkg.go.dev/testing#T.Run) should be wrapped with [flake.Wrap](https://pkg.go.dev/github.com/DataDog/datadog-agent/pkg/util/testutil/flake#Wrap) to inherit the behavior.
 package flake
 
 import (
@@ -25,7 +22,7 @@ var skipFlake = flag.Bool("skip-flake", false, "skip tests labeled as flakes")
 
 // Mark test as a known flaky.
 // If any of skip-flake flag or GO_TEST_SKIP_FLAKE environment variable is set, the test will be skipped.
-// Otherwise test will be retried on failure.
+// Otherwise test will be marked as known flake through a special message on tests output.
 func Mark(t testing.TB) {
 	t.Helper()
 	if shouldSkipFlake() {
