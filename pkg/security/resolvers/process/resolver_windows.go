3064f21a96405b1f7c9403422a8414649e2d3699
Momar TOURÉ <momar.toure@datadoghq.com>
2024-01-17 15:55:45 +0100

    FIx error handling

--- pkg/security/resolvers/process/resolver_windows.go
+++ pkg/security/resolvers/process/resolver_windows.go
@@ -103,13 +103,13 @@ func (p *Resolver) AddNewEntry(pid uint32, ppid uint32, file string, commandLine
 	processHandle, err := procutil.OpenProcessHandle(int32(pid))
 	if err != nil {
 		log.Debugf("Couldn't get the process handle %v ", err)
-
-	}
-	username, err := procutil.GetUsernameForProcess(processHandle)
-	if err != nil {
-		log.Debugf("Couldn't get process username %v %v", pid, err)
 	} else {
-		e.Process.User = username
+		username, err := procutil.GetUsernameForProcess(processHandle)
+		if err != nil {
+			log.Debugf("Couldn't get process username %v %v", pid, err)
+		} else {
+			e.Process.User = username
+		}
 	}
 
 	e.Process.CmdLine = utils.NormalizePath(commandLine)
