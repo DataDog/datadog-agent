47015e670c62b239b84e0dc925979f866f210461
usamasaqib <usama.saqib@datadoghq.com>
2024-01-17 08:52:59 +0100

    use t.TempDir helper

--- pkg/network/protocols/tls/java/jattach_test.go
+++ pkg/network/protocols/tls/java/jattach_test.go
@@ -109,7 +109,7 @@ func TestInject(t *testing.T) {
 func TestInjectInReadOnlyFS(t *testing.T) {
 	curDir, err := os.Getwd()
 	require.NoError(t, err)
-	rodir := filepath.Join("/", "tmp", "rodir")
+	rodir := filepath.Join(t.TempDir(), "rodir")
 
 	err = os.Mkdir(rodir, 0766)
 	require.NoError(t, err)
