8b59018d2b62c451d298dd9ce208cb962e454232
David du Colombier <djc@datadoghq.com>
2024-01-18 21:09:59 +0100

    [agentless-scanner] Bump agentless-scanner package version

--- cmd/agentless-scanner/userdata.sh
+++ cmd/agentless-scanner/userdata.sh
@@ -18,7 +18,7 @@ echo "options nbd nbds_max=128" > /etc/modprobe.d/nbd.conf
 echo "agentless-scanning-${SCANNER_NAME}" > /etc/hostname
 
 # Install the agent
-DD_API_KEY="${DD_API_KEY}" DD_SITE="datad0g.com" DD_HOSTNAME="agentless-scanning-${SCANNER_NAME}" DD_REPO_URL="datad0g.com" DD_AGENT_DIST_CHANNEL="beta" DD_AGENT_MINOR_VERSION="50.0~agentless~scanner~2024010901" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+DD_API_KEY="${DD_API_KEY}" DD_SITE="datad0g.com" DD_HOSTNAME="agentless-scanning-${SCANNER_NAME}" DD_REPO_URL="datad0g.com" DD_AGENT_DIST_CHANNEL="beta" DD_AGENT_MINOR_VERSION="50.0~agentless~scanner~2024011701" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 
 cat << EOF > /etc/datadog-agent/datadog.yaml
 hostname: agentless-scanning-${SCANNER_NAME}
