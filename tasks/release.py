1326a93988b2569a132ec65b790b4a17dfe2ee15
Kacper <89013263+kacper-murzyn@users.noreply.github.com>
2024-01-18 10:22:50 +0100

    Kacper murzyn/build links job (#22105)
    Kacper murzyn/build links job

--- tasks/release.py
+++ tasks/release.py
@@ -1231,6 +1231,7 @@ def build_rc(ctx, major_versions="6,7", patch_version=False, k8s_deployments=Fal
         major_versions=major_versions,
         repo_branch="beta",
         deploy=True,
+        rc_build=True,
         rc_k8s_deployments=k8s_deployments,
     )
 
@@ -1478,6 +1479,8 @@ def update_build_links(_ctx, new_version):
     for key in patterns:
         body = body.replace(key, patterns[key])
 
+    print(color_message(f"Updating QA Build links page with {new_version}", "bold"))
+
     try:
         confluence.update_page(BUILD_LINKS_PAGE_ID, title, body=body)
     except ApiError as e:
@@ -1488,6 +1491,7 @@ def update_build_links(_ctx, new_version):
             ),
             code=1,
         )
+    print(color_message("Build links page updated", "green"))
 
 
 def _create_build_links_patterns(current_version, new_version):
