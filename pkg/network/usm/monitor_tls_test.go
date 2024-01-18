1f4434ab95ff5b90b7469830d60a16a7487c64bd
Guy Arbitman <guy20495@gmail.com>
2024-01-18 16:04:48 +0200

    usm: tls: tests: Skip not supported environments

--- pkg/network/usm/monitor_tls_test.go
+++ pkg/network/usm/monitor_tls_test.go
@@ -46,6 +46,9 @@ type tlsSuite struct {
 
 func TestTLSSuite(t *testing.T) {
 	ebpftest.TestBuildModes(t, []ebpftest.BuildMode{ebpftest.Prebuilt, ebpftest.RuntimeCompiled, ebpftest.CORE}, "", func(t *testing.T) {
+		if !http.TLSSupported(config.New()) {
+			t.Skip("TLS not supported for this setup")
+		}
 		suite.Run(t, new(tlsSuite))
 	})
 }
