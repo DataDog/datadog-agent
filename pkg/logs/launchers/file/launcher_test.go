041a294010119e9e827e211e435f4ff32cb72024
Lucas Liseth <36653792+soberpeach@users.noreply.github.com>
2024-01-18 19:38:27 +0100

    Add Logs File Permissions to Flare (#22010)
    * Testing provides struct
    
    * Logs file permissions added to the flare
    
    * Added methods to get all files attempted to tail
    
    * Add logs files permissions to flare
    
    * Fix linter errors
    
    * Fix failing test
    
    * Add tests for flare controller
    
    * Fix failing FillFlare test
    
    * Remove unnecessary comment
    
    * Updated failing test
    
    * Add release note
    
    * Implement comment suggestions
    
    * Update releasenotes/notes/add-logs-file-permissions-to-flare-ac908b09b95a5313.yaml
    
    * Add newlines to log permissions file
    
    * Added timer to prevent function call from blocking flare
    
    ---------
    
    Co-authored-by: cecilia saixue watt <cecilia.watt@datadoghq.com>

--- pkg/logs/launchers/file/launcher_test.go
+++ pkg/logs/launchers/file/launcher_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/DataDog/datadog-agent/comp/logs/agent/config"
+	flareController "github.com/DataDog/datadog-agent/comp/logs/agent/flare"
 	pkgConfig "github.com/DataDog/datadog-agent/pkg/config"
 	auditor "github.com/DataDog/datadog-agent/pkg/logs/auditor/mock"
 	"github.com/DataDog/datadog-agent/pkg/logs/internal/util"
@@ -29,6 +30,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/logs/status"
 	"github.com/DataDog/datadog-agent/pkg/logs/tailers"
 	filetailer "github.com/DataDog/datadog-agent/pkg/logs/tailers/file"
+
 	//nolint:revive // TODO(AML) Fix revive linter
 	tailer "github.com/DataDog/datadog-agent/pkg/logs/tailers/file"
 )
@@ -69,7 +71,8 @@ func (suite *LauncherTestSuite) SetupTest() {
 	suite.openFilesLimit = 100
 	suite.source = sources.NewLogSource("", &config.LogsConfig{Type: config.FileType, Identifier: suite.configID, Path: suite.testPath})
 	sleepDuration := 20 * time.Millisecond
-	suite.s = NewLauncher(suite.openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+	fc := flareController.NewFlareController()
+	suite.s = NewLauncher(suite.openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 	suite.s.pipelineProvider = suite.pipelineProvider
 	suite.s.registry = auditor.NewRegistry()
 	suite.s.activeSources = append(suite.s.activeSources, suite.source)
@@ -226,7 +229,8 @@ func TestLauncherScanStartNewTailer(t *testing.T) {
 		path = fmt.Sprintf("%s/*.log", testDir)
 		openFilesLimit := 2
 		sleepDuration := 20 * time.Millisecond
-		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+		fc := flareController.NewFlareController()
+		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 		launcher.pipelineProvider = mock.NewMockProvider()
 		launcher.registry = auditor.NewRegistry()
 		outputChan := launcher.pipelineProvider.NextPipelineChan()
@@ -264,7 +268,8 @@ func TestLauncherWithConcurrentContainerTailer(t *testing.T) {
 	// create launcher
 	openFilesLimit := 3
 	sleepDuration := 20 * time.Millisecond
-	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+	fc := flareController.NewFlareController()
+	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 	launcher.pipelineProvider = mock.NewMockProvider()
 	launcher.registry = auditor.NewRegistry()
 	outputChan := launcher.pipelineProvider.NextPipelineChan()
@@ -311,7 +316,8 @@ func TestLauncherTailFromTheBeginning(t *testing.T) {
 	// create launcher
 	openFilesLimit := 3
 	sleepDuration := 20 * time.Millisecond
-	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+	fc := flareController.NewFlareController()
+	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 	launcher.pipelineProvider = mock.NewMockProvider()
 	launcher.registry = auditor.NewRegistry()
 	outputChan := launcher.pipelineProvider.NextPipelineChan()
@@ -377,7 +383,8 @@ func TestLauncherScanWithTooManyFiles(t *testing.T) {
 	path = fmt.Sprintf("%s/*.log", testDir)
 	openFilesLimit := 2
 	sleepDuration := 20 * time.Millisecond
-	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+	fc := flareController.NewFlareController()
+	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 	launcher.pipelineProvider = mock.NewMockProvider()
 	launcher.registry = auditor.NewRegistry()
 	source := sources.NewLogSource("", &config.LogsConfig{Type: config.FileType, Path: path})
@@ -406,7 +413,8 @@ func TestLauncherUpdatesSourceForExistingTailer(t *testing.T) {
 	os.Create(path)
 	openFilesLimit := 2
 	sleepDuration := 20 * time.Millisecond
-	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+	fc := flareController.NewFlareController()
+	launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 	launcher.pipelineProvider = mock.NewMockProvider()
 	launcher.registry = auditor.NewRegistry()
 
@@ -460,6 +468,7 @@ func TestLauncherScanRecentFilesWithRemoval(t *testing.T) {
 			stop:                   make(chan struct{}),
 			validatePodContainerID: false,
 			scanPeriod:             10 * time.Second,
+			flarecontroller:        flareController.NewFlareController(),
 		}
 		launcher.pipelineProvider = mock.NewMockProvider()
 		launcher.registry = auditor.NewRegistry()
@@ -515,7 +524,8 @@ func TestLauncherScanRecentFilesWithNewFiles(t *testing.T) {
 
 	createLauncher := func() *Launcher {
 		sleepDuration := 20 * time.Millisecond
-		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_modification_time")
+		fc := flareController.NewFlareController()
+		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_modification_time", fc)
 		launcher.pipelineProvider = mock.NewMockProvider()
 		launcher.registry = auditor.NewRegistry()
 		logDirectory := fmt.Sprintf("%s/*.log", testDir)
@@ -575,7 +585,8 @@ func TestLauncherFileRotation(t *testing.T) {
 
 	createLauncher := func() *Launcher {
 		sleepDuration := 20 * time.Millisecond
-		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+		fc := flareController.NewFlareController()
+		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 		launcher.pipelineProvider = mock.NewMockProvider()
 		launcher.registry = auditor.NewRegistry()
 		logDirectory := fmt.Sprintf("%s/*.log", testDir)
@@ -633,7 +644,8 @@ func TestLauncherFileDetectionSingleScan(t *testing.T) {
 
 	createLauncher := func() *Launcher {
 		sleepDuration := 20 * time.Millisecond
-		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name")
+		fc := flareController.NewFlareController()
+		launcher := NewLauncher(openFilesLimit, sleepDuration, false, 10*time.Second, "by_name", fc)
 		launcher.pipelineProvider = mock.NewMockProvider()
 		launcher.registry = auditor.NewRegistry()
 		logDirectory := fmt.Sprintf("%s/*.log", testDir)
