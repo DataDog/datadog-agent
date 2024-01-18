42032d82b65e41d5684727e02260e413d7fdcab9
Guy Arbitman <guy20495@gmail.com>
2024-01-18 17:07:37 +0200

    Fixed a CR comment

--- pkg/network/ebpf/c/protocols/classification/dispatcher-helpers.h
+++ pkg/network/ebpf/c/protocols/classification/dispatcher-helpers.h
@@ -124,10 +124,9 @@ static __always_inline void protocol_dispatcher_entrypoint(struct __sk_buff *skb
     stack->flags |= FLAG_USM_ENABLED;
 
     protocol_t cur_fragment_protocol = get_protocol_from_stack(stack, LAYER_APPLICATION);
-    protocol_t encryption_layer = get_protocol_from_stack(stack, LAYER_ENCRYPTION);
     if (tcp_termination) {
         dispatcher_delete_protocol_stack(&skb_tup, stack);
-    } else if (encryption_layer == PROTOCOL_TLS) {
+    } else if (is_protocol_layer_known(stack, LAYER_ENCRYPTION)) {
         // If we have a TLS connection and we're not in the middle of a TCP termination, we can skip the packet.
         return;
     }
