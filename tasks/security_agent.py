5e875c7d8b027005a38042b5a657ab80db423c6e
Derek Brown <derek.brown@datadoghq.com>
2024-01-19 10:55:04 -0800

    Add skeleton, see if it will run in CI

--- tasks/security_agent.py
+++ tasks/security_agent.py
@@ -43,9 +43,12 @@ BIN_DIR = os.path.join(".", "bin")
 BIN_PATH = os.path.join(BIN_DIR, "security-agent", bin_name("security-agent"))
 CI_PROJECT_DIR = os.environ.get("CI_PROJECT_DIR", ".")
 KITCHEN_DIR = os.getenv('DD_AGENT_TESTING_DIR') or os.path.normpath(os.path.join(os.getcwd(), "test", "kitchen"))
+#if is_windows:
+#  KITCHEN_ARTIFACT_DIR = os.path.join(KITCHEN_DIR, "site-cookbooks", "dd-security-agent-check", "files", "default")
+#else:
 KITCHEN_ARTIFACT_DIR = os.path.join(KITCHEN_DIR, "site-cookbooks", "dd-security-agent-check", "files")
 STRESS_TEST_SUITE = "stresssuite"
-is_windows = sys.platform == "win32"
+
 
 @task(iterable=["build_tags"])
 def build(
@@ -841,7 +844,7 @@ def kitchen_prepare(ctx, windows=is_windows, skip_linters=False):
     out_binary = "testsuite"
     if windows:
         out_binary = "testsuite.exe"
-        
+
     testsuite_out_path = os.path.join(KITCHEN_ARTIFACT_DIR, "tests", out_binary)
     build_functional_tests(
         ctx,
