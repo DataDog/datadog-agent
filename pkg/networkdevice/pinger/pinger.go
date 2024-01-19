539ac96c09f17397bac2f717d1bf2299dbf832d5
Ken Schneider <ken.schneider@datadoghq.com>
2024-01-19 12:14:03 -0500

    add copyright notice

--- pkg/networkdevice/pinger/pinger.go
+++ pkg/networkdevice/pinger/pinger.go
@@ -1,3 +1,8 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
 // Package pinger implements ICMP ping functionality for the agent
 package pinger
 
--- pkg/networkdevice/pinger/pinger_windows.go
+++ pkg/networkdevice/pinger/pinger_windows.go
@@ -1,5 +1,10 @@
 //go:build windows
 
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
 package pinger
 
 // WindowsPinger is a structure for pinging
