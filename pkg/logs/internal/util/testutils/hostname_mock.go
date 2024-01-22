d0ed207e2051e2cb5bdb0dfdffbaf7388403a39b
Stanley Liu <stanley.liu@datadoghq.com>
2024-01-22 14:05:48 -0500

    Add package comment

--- pkg/logs/internal/util/testutils/hostname_mock.go
+++ pkg/logs/internal/util/testutils/hostname_mock.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
+// Package testutils provides util test setup methods for pkg/logs
 package testutils
 
 import (
