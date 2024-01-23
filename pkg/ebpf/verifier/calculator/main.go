dc883efc6e6c4e5db89b3d5f12f6a32f27987f91
usamasaqib <usama.saqib@datadoghq.com>
2024-01-23 08:27:14 +0100

    add main comment

--- pkg/ebpf/verifier/calculator/main.go
+++ pkg/ebpf/verifier/calculator/main.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package main exercises BuildVerifierStats and outputs the result as a JSON to stdout
 package main
 
 import (
