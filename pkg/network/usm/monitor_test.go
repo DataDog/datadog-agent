05429d13d7603f63b5843d25c6adbe1d31ed0f90
Guy Arbitman <guy20495@gmail.com>
2024-01-23 22:45:07 +0200

    usm: http: tests: Remove redundant scenarios
    The removed scenarios are covered by other tests

--- pkg/network/usm/monitor_test.go
+++ pkg/network/usm/monitor_test.go
@@ -251,7 +251,6 @@ func (s *HTTPTestSuite) TestHTTPMonitorLoadWithIncompleteBuffers() {
 
 func (s *HTTPTestSuite) TestHTTPMonitorIntegrationWithResponseBody() {
 	t := s.T()
-	targetAddr := "localhost:8080"
 	serverAddr := "localhost:8080"
 
 	tests := []struct {
@@ -271,16 +270,8 @@ func (s *HTTPTestSuite) TestHTTPMonitorIntegrationWithResponseBody() {
 			requestBodySize: 10 * kb,
 		},
 		{
-			name:            "100kb body",
-			requestBodySize: 100 * kb,
-		},
-		{
 			name:            "500kb body",
 			requestBodySize: 500 * kb,
-		},
-		{
-			name:            "2mb body",
-			requestBodySize: 2 * mb,
 		},
 		{
 			name:            "10mb body",
@@ -295,7 +286,7 @@ func (s *HTTPTestSuite) TestHTTPMonitorIntegrationWithResponseBody() {
 			})
 			t.Cleanup(srvDoneFn)
 
-			requestFn := requestGenerator(t, targetAddr, bytes.Repeat([]byte("a"), tt.requestBodySize))
+			requestFn := requestGenerator(t, serverAddr, bytes.Repeat([]byte("a"), tt.requestBodySize))
 			var requests []*nethttp.Request
 			for i := 0; i < 100; i++ {
 				requests = append(requests, requestFn())
