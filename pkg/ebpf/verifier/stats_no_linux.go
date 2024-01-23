4c8279562e1e6b9cf1e101dae20adcc7cff2c913
usamasaqib <usama.saqib@datadoghq.com>
2024-01-23 08:49:08 +0100

    add stats_no_linux.go file

---
+++ pkg/ebpf/verifier/stats_no_linux.go
@@ -0,0 +1,16 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build !linux_bpf
+
+// Package verifier is responsible for exposing information the verifier provides
+// for any loaded eBPF program
+package verifier
+
+import "fmt"
+
+func BuildVerifierStats(objectFiles []string) (map[string]*Statistics, error) {
+	return nil, fmt.Errorf("not implemented")
+}
