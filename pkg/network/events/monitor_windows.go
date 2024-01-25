0f875fe53f3241014dcd89368be3974824f48a9f
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2024-01-25 14:02:08 -0600

    Add package comment

--- pkg/network/events/monitor_windows.go
+++ pkg/network/events/monitor_windows.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//nolint:revive // TODO(NET) Fix revive linter
+// Package events handles process events
 package events
 
 // Initialized returns true if Init() has been called successfully
