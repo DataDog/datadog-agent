d0e1ab04072545defc7c8708bc3ea530b1ba8b95
Branden Clark <branden.clark@datadoghq.com>
2024-01-29 14:31:49 -0500

    Revert "unmark test that is no longer flaky"
    This reverts commit 315e64f874398114332739f49ca788c03e4c59f3.

--- test/new-e2e/tests/windows/install-test/installtester.go
+++ test/new-e2e/tests/windows/install-test/installtester.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/DataDog/datadog-agent/pkg/util/testutil/flake"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/components"
 	"github.com/DataDog/datadog-agent/test/new-e2e/tests/agent-platform/common"
 	windows "github.com/DataDog/datadog-agent/test/new-e2e/tests/windows"
@@ -217,6 +218,12 @@ func (t *Tester) testDoesNotChangeSystemFiles(tt *testing.T) bool {
 		output, err := t.host.Execute(cmd)
 		require.NoError(tt, err, "should compare system files")
 		output = strings.TrimSpace(output)
+		// Log the output if it's not empty so we have a record of what changed.
+		// Normally require.Empty would log this but flake.Mark might Skip the test before its called.
+		if output != "" {
+			tt.Logf("should not remove system files: %s", output)
+		}
+		flake.Mark(tt)
 		require.Empty(tt, output, "should not remove system files")
 	})
 }
