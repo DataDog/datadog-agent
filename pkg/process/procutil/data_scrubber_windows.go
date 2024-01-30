46aab920f84f78abdbf4812d94d8fa5fd3a30c2a
daniel calderon <daniel.calderon@datadoghq.com>
2024-01-30 16:15:39 -0500

    Updating removing regex import from data_scrubber_windows

--- pkg/process/procutil/data_scrubber_windows.go
+++ pkg/process/procutil/data_scrubber_windows.go
@@ -14,37 +14,36 @@ import (
 
 func (ds *DataScrubber) stripArguments(cmdline []string) []string {
 
-	winDotExec := []string{".com",".exe",".bat",".cmd",".vbs", ".vbe",".js",".jse",".wsf",".wsh",".psc1"}
+	winDotExec := []string{".com", ".exe", ".bat", ".cmd", ".vbs", ".vbe", ".js", ".jse", ".wsf", ".wsh", ".psc1"}
 
 	if len(cmdline) > 0 {
 
-		if len(cmdline) > 1  {
+		if len(cmdline) > 1 {
 
 			cmdline = []string{strings.Split(cmdline[0], ",")[0]}
 			return cmdline
 
-		}else{ 
+		} else {
 
-			strCmdline := strings.Join(cmdline,"")
+			strCmdline := strings.Join(cmdline, "")
 
 			validCmdline := validWindowsPrefix(strCmdline)
 
 			i := extensionParser(validCmdline, winDotExec)
 
 			strippedcmdline := validCmdline[:i+4]
-		
+
 			slicedCmdline := []string{}
-		
+
 			cmdline := append(slicedCmdline, strippedcmdline)
-		
-			return cmdline					
-		}	
+
+			return cmdline
+		}
 	}
 	return cmdline
 }
 
-
-func validWindowsPrefix(cmdline string, ) string {
+func validWindowsPrefix(cmdline string) string {
 
 	for _, c := range cmdline {
 		if unicode.IsLetter(c) {
@@ -59,7 +58,7 @@ func validWindowsPrefix(cmdline string, ) string {
 
 }
 
-func extensionParser (validCmdline string, winDotExec []string) int {
+func extensionParser(validCmdline string, winDotExec []string) int {
 
 	var i int
 
@@ -68,7 +67,7 @@ func extensionParser (validCmdline string, winDotExec []string) int {
 		if strings.Contains(validCmdline, c) {
 			i := strings.Index(validCmdline, c)
 			return i
-		} 
 		}
+	}
 	return i
 }
--- pkg/process/procutil/data_scrubber_windows_test.go
+++ pkg/process/procutil/data_scrubber_windows_test.go
@@ -16,20 +16,20 @@ import (
 func TestStripArguments(t *testing.T) {
 
 	cases := []struct {
-		cmdline      []string
-		striplessCmdline []string   
-	}{	
-	{[]string{"C:\\Program Files\\Datadog\\agent.com"}, []string{"C:\\Program Files\\Datadog\\agent.com"}},
-	{[]string{"C:\\Program Files\\Datadog\\agent.exe check, process"}, []string{"C:\\Program Files\\Datadog\\agent.exe"}},
-	{[]string{"C:\\Program Files\\Datadog\\agent.bat", "check", "process"}, []string{"C:\\Program Files\\Datadog\\agent.bat"}},
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.cmd\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.cmd"}},	
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.vbs\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.vbs"}},	
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.vbe\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.vbe"}},	
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.js\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.js"}},	
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.jse\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.jse"}},	
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.wsf\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.wsf"}},	
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.wsh\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.wsh"}},	
-	{[]string{"\"C:\\Program Files\\Datadog\\agent.psc1\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.psc1"}},	
+		cmdline          []string
+		striplessCmdline []string
+	}{
+		{[]string{"C:\\Program Files\\Datadog\\agent.com"}, []string{"C:\\Program Files\\Datadog\\agent.com"}},
+		{[]string{"C:\\Program Files\\Datadog\\agent.exe check, process"}, []string{"C:\\Program Files\\Datadog\\agent.exe"}},
+		{[]string{"C:\\Program Files\\Datadog\\agent.bat", "check", "process"}, []string{"C:\\Program Files\\Datadog\\agent.bat"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.cmd\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.cmd"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.vbs\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.vbs"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.vbe\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.vbe"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.js\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.js"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.jse\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.jse"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.wsf\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.wsf"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.wsh\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.wsh"}},
+		{[]string{"\"C:\\Program Files\\Datadog\\agent.psc1\\\" check process"}, []string{"C:\\Program Files\\Datadog\\agent.psc1"}},
 	}
 	scrubber := setupDataScrubber(t)
 	scrubber.StripAllArguments = true
@@ -39,4 +39,4 @@ func TestStripArguments(t *testing.T) {
 		cases[i].cmdline = scrubber.stripArguments(cmdline)
 		assert.Equal(t, cases[i].noArgsCmdline, cases[i].cmdline)
 	}
-}	
+}
