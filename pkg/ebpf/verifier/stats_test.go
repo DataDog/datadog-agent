83ce3d32aaba34f88cf4bd1814563834747012bc
usamasaqib <usama.saqib@datadoghq.com>
2024-01-31 11:32:10 +0100

    fix import

--- pkg/ebpf/verifier/stats_test.go
+++ pkg/ebpf/verifier/stats_test.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"testing"
 
+	ddebpf "github.com/DataDog/datadog-agent/pkg/ebpf"
 	"github.com/DataDog/datadog-agent/pkg/util/kernel"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 
