1b6d84c1c8776a7aaada5abc38def32dcbd786e9
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-01-31 15:52:32 +0100

    WIP

--- cmd/serverless/main.go
+++ cmd/serverless/main.go
@@ -10,6 +10,7 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"strconv"
 	"sync"
 	"syscall"
 	"time"
@@ -17,12 +18,25 @@ import (
 	logConfig "github.com/DataDog/datadog-agent/comp/logs/agent/config"
 	"github.com/DataDog/datadog-agent/pkg/config"
 	"github.com/DataDog/datadog-agent/pkg/config/model"
+	configUtils "github.com/DataDog/datadog-agent/pkg/config/utils"
+	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/trace"
+	"github.com/DataDog/datadog-agent/pkg/serverless"
 	"github.com/DataDog/datadog-agent/pkg/serverless/apikey"
+	"github.com/DataDog/datadog-agent/pkg/serverless/appsec"
+	appsecConfig "github.com/DataDog/datadog-agent/pkg/serverless/appsec/config"
+	"github.com/DataDog/datadog-agent/pkg/serverless/appsec/httpsec"
+	"github.com/DataDog/datadog-agent/pkg/serverless/daemon"
 	"github.com/DataDog/datadog-agent/pkg/serverless/debug"
+	"github.com/DataDog/datadog-agent/pkg/serverless/flush"
+	"github.com/DataDog/datadog-agent/pkg/serverless/invocationlifecycle"
 	serverlessLogs "github.com/DataDog/datadog-agent/pkg/serverless/logs"
 	"github.com/DataDog/datadog-agent/pkg/serverless/metrics"
 	"github.com/DataDog/datadog-agent/pkg/serverless/otlp"
+	"github.com/DataDog/datadog-agent/pkg/serverless/proxy"
+	"github.com/DataDog/datadog-agent/pkg/serverless/random"
 	"github.com/DataDog/datadog-agent/pkg/serverless/registration"
+	"github.com/DataDog/datadog-agent/pkg/serverless/trace"
+	"github.com/DataDog/datadog-agent/pkg/serverless/trace/inferredspan"
 	"github.com/DataDog/datadog-agent/pkg/util/flavor"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
@@ -127,19 +141,19 @@ func runAgent() {
 	}
 
 	// immediately starts the communication server
-	// serverlessDaemon := daemon.StartDaemon(httpServerAddr)
-	// serverlessDaemon.ExecutionContext.SetInitializationTime(startTime)
-	// err = serverlessDaemon.ExecutionContext.RestoreCurrentStateFromFile()
-	// if err != nil {
-	// 	log.Debug("Unable to restore the state from file")
-	// } else {
-	// 	serverlessDaemon.StartLogCollection()
-	// }
+	serverlessDaemon := daemon.StartDaemon(httpServerAddr)
+	serverlessDaemon.ExecutionContext.SetInitializationTime(startTime)
+	err = serverlessDaemon.ExecutionContext.RestoreCurrentStateFromFile()
+	if err != nil {
+		log.Debug("Unable to restore the state from file")
+	} else {
+		serverlessDaemon.StartLogCollection()
+	}
 	// serverless parts
 	// ----------------
 
 	// extension registration
-	serverlessID, _, err := registration.RegisterExtension(extensionRegistrationRoute, extensionRegistrationTimeout)
+	serverlessID, functionArn, err := registration.RegisterExtension(extensionRegistrationRoute, extensionRegistrationTimeout)
 	if err != nil {
 		// at this point, we were not even able to register, thus, we don't have
 		// any ID assigned, thus, we can't report an error to the init error route
@@ -147,46 +161,46 @@ func runAgent() {
 		log.Errorf("Can't register as a serverless agent: %s", err)
 		return
 	}
-	// if len(functionArn) > 0 {
-	// 	serverlessDaemon.ExecutionContext.SetArnFromExtensionResponse(string(functionArn))
-	// }
+	if len(functionArn) > 0 {
+		serverlessDaemon.ExecutionContext.SetArnFromExtensionResponse(string(functionArn))
+	}
 
 	// adaptive flush configuration
-	// if v, exists := os.LookupEnv(flushStrategyEnvVar); exists {
-	// 	if flushStrategy, err := flush.StrategyFromString(v); err != nil {
-	// 		log.Debugf("Invalid flush strategy %s, will use adaptive flush instead. Err: %s", v, err)
-	// 	} else {
-	// 		// serverlessDaemon.UseAdaptiveFlush(false) // we're forcing the flush strategy, we won't be using the adaptive flush
-	// 		// serverlessDaemon.SetFlushStrategy(flushStrategy)
-	// 	}
-	// } else {
-	// 	// serverlessDaemon.UseAdaptiveFlush(true) // already initialized to true, but let's be explicit just in case
-	// }
+	if v, exists := os.LookupEnv(flushStrategyEnvVar); exists {
+		if flushStrategy, err := flush.StrategyFromString(v); err != nil {
+			log.Debugf("Invalid flush strategy %s, will use adaptive flush instead. Err: %s", v, err)
+		} else {
+			serverlessDaemon.UseAdaptiveFlush(false) // we're forcing the flush strategy, we won't be using the adaptive flush
+			serverlessDaemon.SetFlushStrategy(flushStrategy)
+		}
+	} else {
+		serverlessDaemon.UseAdaptiveFlush(true) // already initialized to true, but let's be explicit just in case
+	}
 
 	logChannel := make(chan *logConfig.ChannelMessage)
 	// Channels for ColdStartCreator
-	// lambdaSpanChan := make(chan *pb.Span)
-	// lambdaInitMetricChan := make(chan *serverlessLogs.LambdaInitMetric)
+	lambdaSpanChan := make(chan *pb.Span)
+	lambdaInitMetricChan := make(chan *serverlessLogs.LambdaInitMetric)
 	//nolint:revive // TODO(SERV) Fix revive linter
-	// coldStartSpanId := random.Random.Uint64()
+	coldStartSpanId := random.Random.Uint64()
 	metricAgent := &metrics.ServerlessMetricAgent{
 		SketchesBucketOffset: time.Second * 10,
 	}
-	metricAgent.Start(5*time.Second, &metrics.MetricConfig{}, &metrics.MetricDogStatsD{})
-	// serverlessDaemon.SetStatsdServer(metricAgent)
-	// serverlessDaemon.SetupLogCollectionHandler(logsAPICollectionRoute, logChannel, config.Datadog.GetBool("serverless.logs_enabled"), config.Datadog.GetBool("enhanced_metrics"), lambdaInitMetricChan)
+	metricAgent.Start(daemon.FlushTimeout, &metrics.MetricConfig{}, &metrics.MetricDogStatsD{})
+	serverlessDaemon.SetStatsdServer(metricAgent)
+	serverlessDaemon.SetupLogCollectionHandler(logsAPICollectionRoute, logChannel, config.Datadog.GetBool("serverless.logs_enabled"), config.Datadog.GetBool("enhanced_metrics"), lambdaInitMetricChan)
 
 	// Concurrently start heavyweight features
 	var wg sync.WaitGroup
 
 	// starts trace agent
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	traceAgent := &trace.ServerlessTraceAgent{}
-	// 	traceAgent.Start(config.Datadog.GetBool("apm_config.enabled"), &trace.LoadConfig{Path: datadogConfigPath}, lambdaSpanChan, coldStartSpanId)
-	// 	serverlessDaemon.SetTraceAgent(traceAgent)
-	// }()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		traceAgent := &trace.ServerlessTraceAgent{}
+		traceAgent.Start(config.Datadog.GetBool("apm_config.enabled"), &trace.LoadConfig{Path: datadogConfigPath}, lambdaSpanChan, coldStartSpanId)
+		serverlessDaemon.SetTraceAgent(traceAgent)
+	}()
 
 	// starts otlp agent
 	wg.Add(1)
@@ -198,14 +212,14 @@ func runAgent() {
 		}
 		otlpAgent := otlp.NewServerlessOTLPAgent(metricAgent.Demux.Serializer())
 		otlpAgent.Start()
-		// serverlessDaemon.SetOTLPAgent(otlpAgent)
+		serverlessDaemon.SetOTLPAgent(otlpAgent)
 	}()
 
 	// enable telemetry collection
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		if os.Getenv("DD_LOCAL_TEST") == "true" || os.Getenv("DD_LOCAL_TEST") == "1" {
+		if os.Getenv(daemon.LocalTestEnvVar) == "true" || os.Getenv(daemon.LocalTestEnvVar) == "1" {
 			log.Debug("Running in local test mode. Telemetry collection HTTP route won't be enabled")
 			return
 		}
@@ -227,94 +241,93 @@ func runAgent() {
 		if logRegistrationError != nil {
 			log.Error("Can't subscribe to logs:", logRegistrationError)
 		} else {
-			_, err := serverlessLogs.SetupLogAgent(logChannel, "AWS Logs", "lambda")
+			logsAgent, err := serverlessLogs.SetupLogAgent(logChannel, "AWS Logs", "lambda")
 			if err != nil {
 				log.Errorf("Error setting up the logs agent: %s", err)
 			}
-			// serverlessDaemon.SetLogsAgent(logsAgent)
+			serverlessDaemon.SetLogsAgent(logsAgent)
 		}
 	}()
 
 	// start appsec
-	// var appsecProxyProcessor *httpsec.ProxyLifecycleProcessor
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	var err error
-	// 	appsecProxyProcessor, err = appsec.New(serverlessDaemon.MetricAgent.Demux)
-	// 	if err != nil {
-	// 		log.Error("appsec: could not start: ", err)
-	// 	}
-	// }()
+	var appsecProxyProcessor *httpsec.ProxyLifecycleProcessor
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		var err error
+		appsecProxyProcessor, err = appsec.New(serverlessDaemon.MetricAgent.Demux)
+		if err != nil {
+			log.Error("appsec: could not start: ", err)
+		}
+	}()
 
 	wg.Wait()
 
-	// coldStartSpanCreator := &trace.ColdStartSpanCreator{
-	// 	LambdaSpanChan:       lambdaSpanChan,
-	// 	LambdaInitMetricChan: lambdaInitMetricChan,
-	// 	TraceAgent:           serverlessDaemon.TraceAgent,
-	// 	StopChan:             make(chan struct{}),
-	// 	ColdStartSpanId:      coldStartSpanId,
-	// }
+	coldStartSpanCreator := &trace.ColdStartSpanCreator{
+		LambdaSpanChan:       lambdaSpanChan,
+		LambdaInitMetricChan: lambdaInitMetricChan,
+		TraceAgent:           serverlessDaemon.TraceAgent,
+		StopChan:             make(chan struct{}),
+		ColdStartSpanId:      coldStartSpanId,
+	}
 
-	// log.Debug("Starting ColdStartSpanCreator")
-	// coldStartSpanCreator.Run()
-	// log.Debug("Setting ColdStartSpanCreator on Daemon")
-	// serverlessDaemon.SetColdStartSpanCreator(coldStartSpanCreator)
+	log.Debug("Starting ColdStartSpanCreator")
+	coldStartSpanCreator.Run()
+	log.Debug("Setting ColdStartSpanCreator on Daemon")
+	serverlessDaemon.SetColdStartSpanCreator(coldStartSpanCreator)
 
-	// ta := serverlessDaemon.TraceAgent.Get()
-	// if ta == nil {
-	// 	log.Error("Unexpected nil instance of the trace-agent")
-	// 	return
-	// }
+	ta := serverlessDaemon.TraceAgent.Get()
+	if ta == nil {
+		log.Error("Unexpected nil instance of the trace-agent")
+		return
+	}
 
-	// // set up invocation processor in the serverless Daemon to be used for the proxy and/or lifecycle API
-	// serverlessDaemon.InvocationProcessor = &invocationlifecycle.LifecycleProcessor{
-	// 	ExtraTags:            serverlessDaemon.ExtraTags,
-	// 	Demux:                serverlessDaemon.MetricAgent.Demux,
-	// 	ProcessTrace:         ta.Process,
-	// 	DetectLambdaLibrary:  func() bool { return serverlessDaemon.LambdaLibraryDetected },
-	// 	InferredSpansEnabled: inferredspan.IsInferredSpansEnabled(),
-	// }
+	// set up invocation processor in the serverless Daemon to be used for the proxy and/or lifecycle API
+	serverlessDaemon.InvocationProcessor = &invocationlifecycle.LifecycleProcessor{
+		ExtraTags:            serverlessDaemon.ExtraTags,
+		Demux:                serverlessDaemon.MetricAgent.Demux,
+		ProcessTrace:         ta.Process,
+		DetectLambdaLibrary:  func() bool { return serverlessDaemon.LambdaLibraryDetected },
+		InferredSpansEnabled: inferredspan.IsInferredSpansEnabled(),
+	}
 
-	// if appsecProxyProcessor != nil {
-	// AppSec runs as a Runtime API proxy. The reverse proxy was already
-	// started by appsec.New(). A span modifier needs to be added in order
-	// to detect the finished request spans and run the complete AppSec
-	// monitoring logic, and ultimately adding the AppSec events to them.
-	// ta.ModifySpan = appsecProxyProcessor.WrapSpanModifier(serverlessDaemon.ExecutionContext, ta.ModifySpan)
-	// Set the default rate limiting to approach 1 trace/min in live circumstances to limit non ASM related traces as much as possible.
-	// This limit is decided in the Standalone ASM Billing RFC and ensures reducing non ASM-related trace throughput
-	// while keeping billing and service catalog running correctly.
-	// In case of ASM event, the trace priority will be set to manual keep
-	// if appsecConfig.IsStandalone() {
-	// 	ta.PrioritySampler.UpdateTargetTPS(1. / 120)
-	// }
-	// } else
-	// if enabled, _ := strconv.ParseBool(os.Getenv("DD_EXPERIMENTAL_ENABLE_PROXY")); enabled {
-	// 	// start the experimental proxy if enabled
-	// 	log.Debug("Starting the experimental runtime api proxy")
-	// 	proxy.Start(
-	// 		"127.0.0.1:9000",
-	// 		"127.0.0.1:9001",
-	// 		serverlessDaemon.InvocationProcessor,
-	// 	)
-	// }
+	if appsecProxyProcessor != nil {
+		// AppSec runs as a Runtime API proxy. The reverse proxy was already
+		// started by appsec.New(). A span modifier needs to be added in order
+		// to detect the finished request spans and run the complete AppSec
+		// monitoring logic, and ultimately adding the AppSec events to them.
+		ta.ModifySpan = appsecProxyProcessor.WrapSpanModifier(serverlessDaemon.ExecutionContext, ta.ModifySpan)
+		// Set the default rate limiting to approach 1 trace/min in live circumstances to limit non ASM related traces as much as possible.
+		// This limit is decided in the Standalone ASM Billing RFC and ensures reducing non ASM-related trace throughput
+		// while keeping billing and service catalog running correctly.
+		// In case of ASM event, the trace priority will be set to manual keep
+		if appsecConfig.IsStandalone() {
+			ta.PrioritySampler.UpdateTargetTPS(1. / 120)
+		}
+	} else if enabled, _ := strconv.ParseBool(os.Getenv("DD_EXPERIMENTAL_ENABLE_PROXY")); enabled {
+		// start the experimental proxy if enabled
+		log.Debug("Starting the experimental runtime api proxy")
+		proxy.Start(
+			"127.0.0.1:9000",
+			"127.0.0.1:9001",
+			serverlessDaemon.InvocationProcessor,
+		)
+	}
 
-	// serverlessDaemon.ComputeGlobalTags(configUtils.GetConfiguredTags(config.Datadog, true))
+	serverlessDaemon.ComputeGlobalTags(configUtils.GetConfiguredTags(config.Datadog, true))
 
 	// run the invocation loop in a routine
 	// we don't want to start this mainloop before because once we're waiting on
 	// the invocation route, we can't report init errors anymore.
-	// go func() {
-	// 	for {
-	// 		if err := serverless.WaitForNextInvocation(stopCh, serverlessDaemon, serverlessID); err != nil {
-	// 			log.Error(err)
-	// 		}
-	// 	}
-	// }()
+	go func() {
+		for {
+			if err := serverless.WaitForNextInvocation(stopCh, serverlessDaemon, serverlessID); err != nil {
+				log.Error(err)
+			}
+		}
+	}()
 
-	go handleTerminationSignals(stopCh, signal.Notify)
+	go handleTerminationSignals(serverlessDaemon, stopCh, signal.Notify)
 
 	// this log line is used for performance checks during CI
 	// please be careful before modifying/removing it
@@ -328,11 +341,11 @@ func runAgent() {
 
 // handleTerminationSignals handles OS termination signals.
 // If a specified signal is received the serverless agent stops.
-func handleTerminationSignals(stopCh chan struct{}, notify func(c chan<- os.Signal, sig ...os.Signal)) {
+func handleTerminationSignals(serverlessDaemon *daemon.Daemon, stopCh chan struct{}, notify func(c chan<- os.Signal, sig ...os.Signal)) {
 	signalCh := make(chan os.Signal, 1)
 	notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
 	signo := <-signalCh
 	log.Infof("Received signal '%s', shutting down...", signo)
-	// serverlessDaemon.Stop()
+	serverlessDaemon.Stop()
 	stopCh <- struct{}{}
 }
