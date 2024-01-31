f5aa38c07db8dac36783f012f1ac3ff1a3f6c066
timothyalexandersoftware <148588980+timothyalexandersoftware@users.noreply.github.com>
2024-01-31 17:08:49 -0500

    [RC-1453] fx-ify the remote config service component (#22444)
    [RC-1453] fx-ify the remote config service component

--- cmd/agent/subcommands/jmx/command.go
+++ cmd/agent/subcommands/jmx/command.go
@@ -10,6 +10,8 @@ package jmx
 import (
 	"context"
 	"fmt"
+	"github.com/DataDog/datadog-agent/comp/remote-config/rcservice"
+	"github.com/DataDog/datadog-agent/pkg/util/optional"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -138,6 +140,7 @@ func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 			fx.Provide(func() demultiplexer.Component { return nil }),
 			fx.Provide(func() inventorychecks.Component { return nil }),
 			fx.Provide(func() packagesigning.Component { return nil }),
+			fx.Provide(func() optional.Option[rcservice.Component] { return optional.NewNoneOption[rcservice.Component]() }),
 			fx.Provide(func() status.Component { return nil }),
 			fx.Provide(tagger.NewTaggerParamsForCoreAgent),
 			tagger.Module(),
