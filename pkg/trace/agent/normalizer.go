8d93eac938e713229fcdb599d92dd82ec499a4a4
Munir Abdinur <munir.abdinur@datadoghq.com>
2024-02-01 16:36:33 -0500

    do not ignore error

--- pkg/trace/agent/normalizer.go
+++ pkg/trace/agent/normalizer.go
@@ -162,7 +162,10 @@ func (a *Agent) normalize(ts *info.TagStats, s *pb.Span) error {
 	if len(s.SpanLinks) > 0 {
 		for _, link := range s.SpanLinks {
 			if val, ok := link.Attributes["link.name"]; ok {
-				link.Attributes["link.name"], _ = traceutil.NormalizeName(val)
+				link.Attributes["link.name"], err = traceutil.NormalizeName(val)
+				if err != nil {
+                                 	log.Debugf("Fixing malformed trace. 'link.name' attribute in span link is invalid (reason=%q), setting link.Attributes["link.name"]=%s", err, link.Attributes["link.name"])
+				}
 			}
 		}
 	}
