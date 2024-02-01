66ed93095b6bc4b461007e6eeb269229960d27a2
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2024-02-01 15:12:43 +0100

    goembeddockercompose

--- test/new-e2e/tests/npm/dockercompose_httpbin.go
+++ test/new-e2e/tests/npm/dockercompose_httpbin.go
@@ -1,31 +1,18 @@
 package npm
 
 import (
+	_ "embed"
+
 	"github.com/DataDog/test-infra-definitions/components/docker"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"gopkg.in/yaml.v2"
 )
 
-func dockerHTTPBinCompose() docker.ComposeInlineManifest {
-	httpbinManifestContent := pulumi.All().ApplyT(func(args []interface{}) (string, error) {
-		agentManifest := docker.ComposeManifest{
-			Version: "3.9",
-			Services: map[string]docker.ComposeManifestService{
-				"httpbin": {
-					Privileged:    true,
-					Image:         "mccutchen/go-httpbin",
-					ContainerName: "httpbin",
-					Pid:           "host",
-					Ports:         []string{"80:8080/tcp"},
-				},
-			},
-		}
-		data, err := yaml.Marshal(agentManifest)
-		return string(data), err
-	}).(pulumi.StringOutput)
+//go:embed "config/dockercompose_httpbin.yaml"
+var dockerHTTPBinComposeYaml string
 
+func dockerHTTPBinCompose() docker.ComposeInlineManifest {
 	return docker.ComposeInlineManifest{
 		Name:    "httpbin",
-		Content: httpbinManifestContent,
+		Content: pulumi.String(dockerHTTPBinComposeYaml),
 	}
 }
