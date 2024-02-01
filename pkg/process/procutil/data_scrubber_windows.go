b59a7c182fc52d2c9d116c81140958bb59067013
daniel calderon <daniel.calderon@datadoghq.com>
2024-02-01 01:15:33 -0500

    all unit tests passing

--- pkg/process/procutil/data_scrubber_windows.go
+++ pkg/process/procutil/data_scrubber_windows.go
@@ -14,7 +14,7 @@ import (
 // Extensions in which windows defaults the exec commands. Additional extensions can be included. 
 var (
 	winDotExec= []string{
-		".com ",".exe ",".bat ",".cmd ",".vbs ", ".vbe ",".js ",".jse ",".wsf ",".wsh ",".psc1 "}
+		".com",".exe",".bat",".cmd",".vbs", ".vbe",".js",".jse",".wsf",".wsh",".psc1", ".ps1"} 
 )
 
 // stripWindowsArgs removes the arguments of the commands if they identify any of the file extensions that windows defaults to.
@@ -23,51 +23,56 @@ var (
 func (ds *DataScrubber) stripArguments(cmdline []string)[]string {
 	
 	argLength := len(cmdline)
-
-	winDotExec := []string{".com",".exe",".bat",".cmd",".vbs", ".vbe",".js",".jse",".wsf",".wsh",".psc1", ".ps1"} 
 	
 		strCmdline := (cmdline[0] + " ")
 	
 	// case 1: Uses extensionParser() to format and search the command. 
 		if argLength == 1 && !strings.HasPrefix(strCmdline, "\"") {
-			cmdline := extensionParser(strCmdline, winDotExec)
-	
+			strippedCmdline := extensionParser(strCmdline, winDotExec)
+			cmdline = cleanUp(strippedCmdline)
 			return cmdline
+		}
 		}
 	// case 2: Uses quotesFinder() to search for any existing pair of double quotes ("") existing in the string as characters and return the content between them. 
 		if argLength == 1 && strings.HasPrefix(strCmdline, "\""){
-			cmdline := []string{quotesFinder(strCmdline)}
-	
+			strippedCmdline := quotesFinder(strCmdline)
+			cmdline = cleanUp(strippedCmdline)
 			return cmdline
 	
 		}
 	// case 3: Process a cmdline with multiple strings and use extensionParser() to format and search the command. 
 		if 	argLength > 1 && !strings.HasPrefix(strCmdline, "\""){
-			cmdline := extensionParser(strCmdline, winDotExec)
-	
+			strippedCmdline := extensionParser(strCmdline, winDotExec)
+			cmdline = cleanUp(strippedCmdline)
 			return cmdline
 					
 		}
+	
 		return cmdline
 	}	
+	
+	func cleanUp(strippedCmdline string)[]string{
+		cmdline := []string{strings.TrimSuffix(strippedCmdline," ")}
+		return cmdline
+	}
 
 	// Iterate through the cmdline to identify any match with any item of winDotExec[n] and remove the characters after any occurrence.
 	
-	func extensionParser (cmdline string, winDotExec []string) []string{
-	
+	func extensionParser (cmdline string, winDotExec []string) string{
+
 		var i int
 	
-		strippedcmdline := []string{}
+		var processedCmdline string
 	
 		for _, c := range winDotExec {
 			if i = strings.Index(cmdline, c+" " ); i != -1 {
-				strippedcmdline = []string{cmdline[:i+len(c)]}
-				return strippedcmdline
+				processedCmdline = cmdline[:i+len(c)]
+				return processedCmdline
 				} else{
-				strippedcmdline = []string{strings.Split(cmdline, c)[0]}
+					processedCmdline = strings.Split(cmdline, c)[0]
 				}
 			}
-		return strippedcmdline
+		return processedCmdline
 	}
 	
 	func quotesFinder(cmdline string)string{
