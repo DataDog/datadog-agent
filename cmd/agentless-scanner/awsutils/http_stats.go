21aad5d4b0be45ce3f6f6a05954e39dc637233c0
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2024-02-02 21:31:40 +0100

    [agentless-scanner] split: move aws config in dedicated awsutils package

---
+++ cmd/agentless-scanner/awsutils/http_stats.go
@@ -0,0 +1,220 @@
+package awsutils
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+
+	ddogstatsd "github.com/DataDog/datadog-go/v5/statsd"
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
+)
+
+type roundtrip struct {
+	transport *http.Transport
+	region    string
+	limiter   *Limiter
+	role      arn.ARN
+	statsd    *ddogstatsd.Client
+	tags      []string
+}
+
+func newHTTPClientWithStats(region string, assumedRole *arn.ARN, statsd *ddogstatsd.Client, limiter *Limiter, tags []string) *http.Client {
+	rt := &roundtrip{
+		region:  region,
+		limiter: limiter,
+		tags:    tags,
+		statsd:  statsd,
+		transport: &http.Transport{
+			DisableKeepAlives:   false,
+			IdleConnTimeout:     10 * time.Second,
+			MaxIdleConns:        500,
+			MaxConnsPerHost:     500,
+			MaxIdleConnsPerHost: 500,
+			TLSHandshakeTimeout: 5 * time.Second,
+		},
+	}
+	if assumedRole != nil {
+		rt.role = *assumedRole
+	}
+	return &http.Client{
+		Timeout:   10 * time.Minute,
+		Transport: rt,
+	}
+}
+
+var (
+	ebsGetBlockReg      = regexp.MustCompile("^/snapshots/(snap-[a-f0-9]+)/blocks/([0-9]+)$")
+	ebsListBlocksReg    = regexp.MustCompile("^/snapshots/(snap-[a-f0-9]+)/blocks$")
+	ebsChangedBlocksReg = regexp.MustCompile("^/snapshots/(snap-[a-f0-9]+)/changedblocks$")
+)
+
+func (rt *roundtrip) getAction(req *http.Request) (service, action string, error error) {
+	host := req.URL.Host
+	if strings.HasSuffix(host, ".amazonaws.com") {
+		switch {
+		// STS (sts.(region.)?amazonaws.com)
+		case strings.HasPrefix(host, "sts."):
+			return "sts", "getcalleridentity", nil
+
+		// Lambda (lambda.(region.)?amazonaws.com)
+		case strings.HasPrefix(host, "lambda."):
+			return "lambda", "getfunction", nil
+
+		case strings.HasPrefix(host, "ebs."):
+			if req.Method == http.MethodGet && ebsGetBlockReg.MatchString(req.URL.Path) {
+				return "ebs", "getblock", nil
+			}
+			if req.Method == http.MethodGet && ebsListBlocksReg.MatchString(req.URL.Path) {
+				return "ebs", "listblocks", nil
+			}
+			if req.Method == http.MethodGet && ebsChangedBlocksReg.MatchString(req.URL.Path) {
+				return "ebs", "changedblocks", nil
+			}
+			return "ebs", "unknown", nil
+
+		// EC2 (ec2.(region.)?amazonaws.com): https://docs.aws.amazon.com/AWSEC2/latest/APIReference/Using_Endpoints.html
+		case strings.HasPrefix(host, "ec2."):
+			if req.Method == http.MethodPost && req.Body != nil {
+				defer req.Body.Close()
+				body, err := io.ReadAll(req.Body)
+				if err != nil {
+					return
+				}
+				req.Body = io.NopCloser(bytes.NewReader(body))
+				form, err := url.ParseQuery(string(body))
+				if err == nil {
+					if action := form.Get("Action"); action != "" {
+						return "ec2", strings.ToLower(action), nil
+					}
+					return "ec2", "unknown", nil
+				}
+			} else {
+				form := req.URL.Query()
+				if action := form.Get("Action"); action != "" {
+					return "ec2", strings.ToLower(action), nil
+				}
+				return "ec2", "unknown", nil
+			}
+		case strings.Contains(host, ".s3.") || strings.Contains(host, ".s3-"):
+			return "s3", "unknown", nil
+		}
+	} else if host == "169.254.169.254" {
+		return "imds", "unknown", nil
+	}
+	return "unknown", "unknown", nil
+}
+
+func (rt *roundtrip) RoundTrip(req *http.Request) (*http.Response, error) {
+	startTime := time.Now()
+	service, action, err := rt.getAction(req)
+	if err != nil {
+		return nil, err
+	}
+	limiter := rt.limiter.Get(rt.role.AccountID, rt.region, service, action)
+	throttled100 := false
+	throttled1000 := false
+	throttled5000 := false
+	if limiter != nil {
+		r := limiter.Reserve()
+		if !r.OK() {
+			panic("unexpected limiter with a zero burst")
+		}
+		if delay := r.Delay(); delay > 0 {
+			throttled100 = delay > 100*time.Millisecond
+			throttled1000 = delay > 1000*time.Millisecond
+			throttled5000 = delay > 5000*time.Millisecond
+			select {
+			case <-time.After(delay):
+			case <-req.Context().Done():
+				return nil, req.Context().Err()
+			}
+		}
+	}
+	tags := append(rt.tags,
+		fmt.Sprintf("aws_region:%s", rt.region),
+		fmt.Sprintf("aws_assumed_role:%s", rt.role.Resource),
+		fmt.Sprintf("aws_account_id:%s", rt.role.AccountID),
+		fmt.Sprintf("aws_service:%s", service),
+		fmt.Sprintf("aws_action:%s_%s", service, action),
+		fmt.Sprintf("aws_throttled_100:%t", throttled100),
+		fmt.Sprintf("aws_throttled_1000:%t", throttled1000),
+		fmt.Sprintf("aws_throttled_5000:%t", throttled5000),
+	)
+	if err := rt.statsd.Incr("datadog.agentless_scanner.aws.requests", tags, 1.0); err != nil {
+		log.Warnf("failed to send metric: %v", err)
+	}
+	resp, err := rt.transport.RoundTrip(req)
+	duration := float64(time.Since(startTime).Milliseconds())
+	defer func() {
+		if err := rt.statsd.Histogram("datadog.agentless_scanner.aws.responses", duration, tags, 0.2); err != nil {
+			log.Warnf("failed to send metric: %v", err)
+		}
+	}()
+	if err != nil {
+		if err == context.Canceled {
+			tags = append(tags, "aws_statuscode:ctx_canceled")
+		} else if err == context.DeadlineExceeded {
+			tags = append(tags, "aws_statuscode:ctx_deadline_exceeded")
+		} else {
+			tags = append(tags, "aws_statuscode:unknown_error")
+		}
+		return nil, err
+	}
+
+	tags = append(tags, fmt.Sprintf("aws_statuscode:%d", resp.StatusCode))
+	if resp.StatusCode >= 400 {
+		switch {
+		case service == "ec2" && resp.Header.Get("Content-Type") == "text/xml;charset=UTF-8":
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return resp, err
+			}
+			resp.Body = io.NopCloser(bytes.NewReader(body))
+			var ec2Error struct {
+				XMLName   xml.Name `xml:"Response"`
+				RequestID string   `xml:"RequestID"`
+				Errors    []struct {
+					Code    string `xml:"Code"`
+					Message string `xml:"Message"`
+				} `xml:"Errors>Error"`
+			}
+			if errx := xml.Unmarshal(body, &ec2Error); errx == nil {
+				for _, errv := range ec2Error.Errors {
+					tags = append(tags, fmt.Sprintf("aws_ec2_errorcode:%s", strings.ToLower(errv.Code)))
+				}
+			}
+		case service == "ebs" && resp.Header.Get("Content-Type") == "application/json":
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return resp, err
+			}
+			resp.Body = io.NopCloser(bytes.NewReader(body))
+			// {"Message":"The snapshot 'snap-00000' does not exist.","Reason":"SNAPSHOT_NOT_FOUND"}
+			var ebsError struct {
+				Reason string `json:"Reason"`
+			}
+			if errx := json.Unmarshal(body, &ebsError); errx == nil {
+				tags = append(tags, fmt.Sprintf("aws_ebs_errorcode:%s", strings.ToLower(ebsError.Reason)))
+			}
+		}
+	}
+	if contentLength, err := strconv.Atoi(resp.Header.Get("Content-Length")); err == nil {
+		if err := rt.statsd.Histogram("datadog.agentless_scanner.responses.size", float64(contentLength), tags, 0.2); err != nil {
+			log.Warnf("failed to send metric: %v", err)
+		}
+	}
+	return resp, nil
+}
