31f05b9157509a51c109257858964d1c6869c40a
Alex Lopez <alex.lopez@datadoghq.com>
2024-02-06 15:13:45 +0100

    Remove some of the `dependency` statements for deps we already fetch

--- omnibus/config/software/datadog-agent-integrations-py3-dependencies.rb
+++ omnibus/config/software/datadog-agent-integrations-py3-dependencies.rb
@@ -13,25 +13,16 @@ if arm_target?
   dependency 'libxslt'
 end
 
-if osx_target?
-  dependency 'unixodbc'
-end
-
 if linux_target?
-  # add nfsiostat script
   dependency 'unixodbc'
   dependency 'freetds'  # needed for SQL Server integration
   unless heroku_target?
     dependency 'msodbcsql18' # needed for SQL Server integration
   end
+  # add nfsiostat script
   dependency 'nfsiostat'
   # add libkrb5 for all integrations supporting kerberos auth with `requests-kerberos`
   dependency 'libkrb5'
   # needed for glusterfs
   dependency 'gstatus'
-end
-
-if linux_target?
-  # We need to use cython<3.0.0 to build oracledb
-  dependency 'oracledb-py3'
 end
