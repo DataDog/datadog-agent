0d15fae8f6ffbf0454471071ce4066f601029220
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2024-02-06 17:03:36 +0100

    ec2VMContainerizedSuite-suitetests

--- test/new-e2e/tests/npm/ec2_1host_test.go
+++ test/new-e2e/tests/npm/ec2_1host_test.go
@@ -99,93 +99,85 @@ func (v *ec2VMSuite) SetupSuite() {
 // BeforeTest will be called before each test
 func (v *ec2VMSuite) BeforeTest(suiteName, testName string) {
 	v.BaseSuite.BeforeTest(suiteName, testName)
-	v.beforeTest(suiteName, testName)
-}
 
-func (v *ec2VMSuite) beforeTest(_, _ string) {
 	// default is to reset the current state of the fakeintake aggregators
 	if !v.BaseSuite.IsDevMode() {
 		v.Env().FakeIntake.Client().FlushServerAndResetAggregators()
 	}
 }
 
-// TestFakeIntakeNPM Validate the agent can communicate with the (fake) backend and send connections every 30 seconds
+// TestFakeIntakeNPM_HostRequests Validate the agent can communicate with the (fake) backend and send connections every 30 seconds
 // 2 tests generate the request on the host and on docker
 //   - looking for 1 host to send CollectorConnections payload to the fakeintake
 //   - looking for 3 payloads and check if the last 2 have a span of 30s +/- 500ms
-func (v *ec2VMSuite) TestFakeIntakeNPM() {
-	vt := v.T()
+func (v *ec2VMSuite) TestFakeIntakeNPM_HostRequests() {
 	testURL := "http://" + v.Env().HTTPBinHost.Address + "/"
-	vt.Run("host_requests", func(t *testing.T) {
-		v.BaseSuite.SetT(t)
-		v.beforeTest("TestEC2VMSuite", t.Name()) // workaround as suite doesn't call BeforeTest before each sub tests
 
-		// generate a connection
-		v.Env().RemoteHost.MustExecute("curl " + testURL)
+	// generate a connection
+	v.Env().RemoteHost.MustExecute("curl " + testURL)
 
-		test1HostFakeIntakeNPM(&v.BaseSuite, v.Env().FakeIntake)
-	})
-	vt.Run("docker_requests", func(t *testing.T) {
-		v.BaseSuite.SetT(t)
-		v.beforeTest("TestEC2VMSuite", t.Name()) // workaround as suite doesn't call BeforeTest before each sub tests
-
-		// generate a connection
-		v.Env().RemoteHost.MustExecute("docker run curlimages/curl curl " + testURL)
-
-		test1HostFakeIntakeNPM(&v.BaseSuite, v.Env().FakeIntake)
-	})
+	test1HostFakeIntakeNPM(&v.BaseSuite, v.Env().FakeIntake)
 }
 
-// TestFakeIntakeNPM_600cnx_bucket Validate the agent can communicate with the (fake) backend and send connections
+// TestFakeIntakeNPM_DockerRequests Validate the agent can communicate with the (fake) backend and send connections every 30 seconds
+// 2 tests generate the request on the host and on docker
+//   - looking for 1 host to send CollectorConnections payload to the fakeintake
+//   - looking for 3 payloads and check if the last 2 have a span of 30s +/- 500ms
+func (v *ec2VMSuite) TestFakeIntakeNPM_DockerRequests() {
+	testURL := "http://" + v.Env().HTTPBinHost.Address + "/"
+
+	// generate a connection
+	v.Env().RemoteHost.MustExecute("docker run curlimages/curl curl " + testURL)
+
+	test1HostFakeIntakeNPM(&v.BaseSuite, v.Env().FakeIntake)
+}
+
+// TestFakeIntakeNPM600cnxBucket_HostRequests Validate the agent can communicate with the (fake) backend and send connections
 // every 30 seconds with a maximum of 600 connections per payloads, if more another payload will follow.
 //   - looking for 1 host to send CollectorConnections payload to the fakeintake
 //   - looking for n payloads and check if the last 2 have a maximum span of 100ms
-func (v *ec2VMSuite) TestFakeIntakeNPM_600cnx_bucket() {
-	vt := v.T()
+func (v *ec2VMSuite) TestFakeIntakeNPM600cnxBucket_HostRequests() {
 	testURL := "http://" + v.Env().HTTPBinHost.Address + "/"
-	vt.Run("host_requests", func(t *testing.T) {
-		v.BaseSuite.SetT(t)
-		v.beforeTest("TestEC2VMSuite", t.Name()) // workaround as suite doesn't call BeforeTest before each sub tests
 
-		// generate connections
-		v.Env().RemoteHost.MustExecute("ab -n 600 -c 600 " + testURL)
+	// generate connections
+	v.Env().RemoteHost.MustExecute("ab -n 600 -c 600 " + testURL)
 
-		test1HostFakeIntakeNPM600cnxBucket(&v.BaseSuite, v.Env().FakeIntake)
-	})
-	vt.Run("docker_requests", func(t *testing.T) {
-		v.BaseSuite.SetT(t)
-		v.beforeTest("TestEC2VMSuite", t.Name()) // workaround as suite doesn't call BeforeTest before each sub tests
-
-		// generate connections
-		v.Env().RemoteHost.MustExecute("docker run devth/alpine-bench -n 600 -c 600 " + testURL)
-
-		test1HostFakeIntakeNPM600cnxBucket(&v.BaseSuite, v.Env().FakeIntake)
-	})
+	test1HostFakeIntakeNPM600cnxBucket(&v.BaseSuite, v.Env().FakeIntake)
 }
 
-// TestFakeIntakeNPM_TCP_UDP_DNS validate we received tcp, udp, and DNS connections
-// with some basic checks, like IPs/Ports present, DNS query has been captured, ...
-func (v *ec2VMSuite) TestFakeIntakeNPM_TCP_UDP_DNS() {
-	vt := v.T()
+// TestFakeIntakeNPM600cnxBucket_DockerRequests Validate the agent can communicate with the (fake) backend and send connections
+// every 30 seconds with a maximum of 600 connections per payloads, if more another payload will follow.
+//   - looking for 1 host to send CollectorConnections payload to the fakeintake
+//   - looking for n payloads and check if the last 2 have a maximum span of 100ms
+func (v *ec2VMSuite) TestFakeIntakeNPM600cnxBucket_DockerRequests() {
 	testURL := "http://" + v.Env().HTTPBinHost.Address + "/"
-	vt.Run("host_requests", func(t *testing.T) {
-		v.BaseSuite.SetT(t)
-		v.beforeTest("TestEC2VMSuite", t.Name()) // workaround as suite doesn't call BeforeTest before each sub tests
 
-		// generate connections
-		v.Env().RemoteHost.MustExecute("curl " + testURL)
-		v.Env().RemoteHost.MustExecute("dig @8.8.8.8 www.google.ch")
+	// generate connections
+	v.Env().RemoteHost.MustExecute("docker run devth/alpine-bench -n 600 -c 600 " + testURL)
 
-		test1HostFakeIntakeNPMTCPUDPDNS(&v.BaseSuite, v.Env().FakeIntake)
-	})
-	vt.Run("docker_requests", func(t *testing.T) {
-		v.BaseSuite.SetT(t)
-		v.beforeTest("TestEC2VMSuite", t.Name()) // workaround as suite doesn't call BeforeTest before each sub tests
+	test1HostFakeIntakeNPM600cnxBucket(&v.BaseSuite, v.Env().FakeIntake)
+}
 
-		// generate connections
-		v.Env().RemoteHost.MustExecute("docker run curlimages/curl curl " + testURL)
-		v.Env().RemoteHost.MustExecute("docker run makocchi/alpine-dig dig @8.8.8.8 www.google.ch")
+// TestFakeIntakeNPM_TCP_UDP_DNS_HostRequests validate we received tcp, udp, and DNS connections
+// with some basic checks, like IPs/Ports present, DNS query has been captured, ...
+func (v *ec2VMSuite) TestFakeIntakeNPM_TCP_UDP_DNS_HostRequests() {
+	testURL := "http://" + v.Env().HTTPBinHost.Address + "/"
 
-		test1HostFakeIntakeNPMTCPUDPDNS(&v.BaseSuite, v.Env().FakeIntake)
-	})
+	// generate connections
+	v.Env().RemoteHost.MustExecute("curl " + testURL)
+	v.Env().RemoteHost.MustExecute("dig @8.8.8.8 www.google.ch")
+
+	test1HostFakeIntakeNPMTCPUDPDNS(&v.BaseSuite, v.Env().FakeIntake)
+}
+
+// TestFakeIntakeNPM_TCP_UDP_DNS_DockerRequests validate we received tcp, udp, and DNS connections
+// with some basic checks, like IPs/Ports present, DNS query has been captured, ...
+func (v *ec2VMSuite) TestFakeIntakeNPM_TCP_UDP_DNS_DockerRequests() {
+	testURL := "http://" + v.Env().HTTPBinHost.Address + "/"
+
+	// generate connections
+	v.Env().RemoteHost.MustExecute("docker run curlimages/curl curl " + testURL)
+	v.Env().RemoteHost.MustExecute("docker run makocchi/alpine-dig dig @8.8.8.8 www.google.ch")
+
+	test1HostFakeIntakeNPMTCPUDPDNS(&v.BaseSuite, v.Env().FakeIntake)
 }
