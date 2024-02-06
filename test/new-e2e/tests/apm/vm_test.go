689ef65a6308a1e427a80897102c891c725530bc
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2024-02-06 14:53:18 +0100

    unskip

--- test/new-e2e/tests/apm/vm_test.go
+++ test/new-e2e/tests/apm/vm_test.go
@@ -34,9 +34,6 @@ func vmSuiteOpts(tr transport, opts ...awshost.ProvisionerOption) []e2e.SuiteOpt
 
 // TestVMFakeintakeSuiteUDS runs basic Trace Agent tests over the UDS transport
 func TestVMFakeintakeSuiteUDS(t *testing.T) {
-	// FIXME: fix flakiness reaching the agent then enable the test
-	t.Skip("Skipping flaky test - incident-25117 AIT-9390")
-
 	cfg := `
 apm_config.enabled: true
 apm_config.receiver_socket: /var/run/datadog/apm.socket
@@ -51,9 +48,6 @@ apm_config.receiver_socket: /var/run/datadog/apm.socket
 
 // TestVMFakeintakeSuiteTCP runs basic Trace Agent tests over the TCP transport
 func TestVMFakeintakeSuiteTCP(t *testing.T) {
-	// FIXME: fix flakiness reaching the agent then enable the test
-	t.Skip("Skipping flaky test - incident-25117 AIT-9390")
-
 	cfg := `
 apm_config.enabled: true
 `
