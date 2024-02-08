56a8bd00c9c6778e1fff0a9e2f2b6698111ad998
Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>
2024-02-08 14:59:35 +0100

    feat: run health e2e tests on Windows
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>

--- test/new-e2e/tests/agent-subcommands/health/health_win_test.go
+++ test/new-e2e/tests/agent-subcommands/health/health_win_test.go
@@ -3,51 +3,29 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package agentsubcommands
+package health
 
 import (
 	"testing"
-	"time"
 
 	"github.com/DataDog/datadog-agent/test/fakeintake/api"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
-	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
-
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
-
-	"github.com/cenkalti/backoff"
+	"github.com/DataDog/test-infra-definitions/components/os"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
 	"github.com/stretchr/testify/assert"
 )
 
-type subcommandWithFakeIntakeSuite struct {
-	e2e.BaseSuite[environments.Host]
+type windowsHealthSuite struct {
+	baseHealthSuite
 }
 
-func TestSubcommandSuite(t *testing.T) {
-	e2e.Run(t, &subcommandWithFakeIntakeSuite{}, e2e.WithProvisioner(awshost.Provisioner()))
+func TestWindowsHealthSuite(t *testing.T) {
+	e2e.Run(t, &windowsHealthSuite{}, e2e.WithProvisioner(awshost.Provisioner(awshost.WithEC2InstanceOptions(ec2.WithOS(os.WindowsDefault)))))
 }
 
-// section contains the content status of a specific section (e.g. Forwarder)
-
-func (v *subcommandWithFakeIntakeSuite) TestDefaultInstallHealthy() {
-	interval := 1 * time.Second
-
-	var output string
-	var err error
-	err = backoff.Retry(func() error {
-		output, err = v.Env().Agent.Client.Health()
-		if err != nil {
-			return err
-		}
-		return nil
-	}, backoff.WithMaxRetries(backoff.NewConstantBackOff(interval), uint64(15)))
-
-	assert.NoError(v.T(), err)
-	assert.Contains(v.T(), output, "Agent health: PASS")
-}
-
-func (v *subcommandWithFakeIntakeSuite) TestDefaultInstallUnhealthy() {
+func (v *windowsHealthSuite) TestDefaultInstallUnhealthy() {
 	// the fakeintake says that any API key is invalid by sending a 403 code
 	override := api.ResponseOverride{
 		Endpoint:    "/api/v1/validate",
@@ -56,11 +34,9 @@ func (v *subcommandWithFakeIntakeSuite) TestDefaultInstallUnhealthy() {
 		Body:        []byte("invalid API key"),
 	}
 	v.Env().FakeIntake.Client().ConfigureOverride(override)
-
 	// restart the agent, which validates the key using the fakeintake at startup
-	v.UpdateEnv(awshost.Provisioner(
-		awshost.WithAgentOptions(agentparams.WithAgentConfig("log_level: info\n")),
-	))
+	v.UpdateEnv(awshost.Provisioner(awshost.WithEC2InstanceOptions(ec2.WithOS(os.WindowsDefault)),
+		awshost.WithAgentOptions(agentparams.WithAgentConfig("log_level: info\n"))))
 
 	// agent should be unhealthy because the key is invalid
 	_, err := v.Env().Agent.Client.Health()
