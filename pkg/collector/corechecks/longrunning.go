c24b744afc6939c97b5c8f7da4082ccfd9a92ed8
AliDatadog <ali.benabdallah@datadoghq.com>
2024-02-08 16:03:22 +0100

    remove todo comments

--- pkg/collector/corechecks/longrunning.go
+++ pkg/collector/corechecks/longrunning.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2017-present Datadog, Inc.
 
-//nolint:revive // TODO(AML) Fix revive linter
+//nolint:revive
 package corechecks
 
 import (
