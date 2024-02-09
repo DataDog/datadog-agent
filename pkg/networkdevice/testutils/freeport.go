db99d3b4d0fb13c21914edd84d71e080575fa640
Florian Veaux <florian.veaux@datadoghq.com>
2024-02-09 14:05:28 +0100

    NDM: Update GetFreePort

--- pkg/networkdevice/testutils/freeport.go
+++ pkg/networkdevice/testutils/freeport.go
@@ -12,42 +12,21 @@ import (
 	"sync"
 )
 
-var (
-	globalMutex sync.Mutex // Prevent the util function to be called concurrently
-	usedPorts   = make(map[uint16]bool)
-)
-
-// GetFreePort finds a free port to use for testing.
-// Borrowed from: https://github.com/phayes/freeport/blame/master/freeport.go#L8-L20
 func GetFreePort() (uint16, error) {
-	globalMutex.Lock()
-	defer globalMutex.Unlock()
-
-	var lastError error
-	for retries := 0; retries < 5; retries++ {
-		port, err := getSingleFreePort()
-		if err != nil {
-			lastError = err
-			continue
-		}
-		if _, ok := usedPorts[port]; !ok {
-			usedPorts[port] = true
-			return port, nil
-		}
-	}
-	return 0, lastError
-}
-
-func getSingleFreePort() (uint16, error) {
-	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	conn, err := net.ListenPacket("udp", ":0")
 	if err != nil {
-		return 0, err
+		return -1, fmt.Errorf("can't find an available udp port: %s", err)
+	}
+	defer conn.Close()
+
+	_, portString, err := net.SplitHostPort(conn.LocalAddr().String())
+	if err != nil {
+		return -1, fmt.Errorf("can't find an available udp port: %s", err)
+	}
+	portInt, err := strconv.Atoi(portString)
+	if err != nil {
+		return -1, fmt.Errorf("can't convert udp port: %s", err)
 	}
 
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return 0, err
-	}
-	defer l.Close()
-	return uint16(l.Addr().(*net.TCPAddr).Port), nil
+	return portInt, nil
 }
