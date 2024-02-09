f58d73192067cfdac1525f053a232745e214bdb3
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-02-09 13:54:21 +0100

    add new util function to get the keys of a map

--- pkg/security/secl/compiler/eval/utils.go
+++ pkg/security/secl/compiler/eval/utils.go
@@ -55,3 +55,11 @@ func IntToIP(ipInt *big.Int, bits int) net.IP {
 	}
 	return ret
 }
+
+func KeysOfMap[M ~map[K]V, K comparable, V any](m M) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
+}
