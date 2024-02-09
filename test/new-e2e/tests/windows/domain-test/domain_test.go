092e72e35424c74d8d7f40e318dc1f5d585a4589
Julien Lebot <julien.lebot@datadoghq.com>
2024-02-09 01:00:36 +0100

    Refactor test for easier legibility and creation of multiple suites

--- test/new-e2e/tests/windows/domain-test/domain_test.go
+++ test/new-e2e/tests/windows/domain-test/domain_test.go
@@ -2,94 +2,87 @@ package domain
 
 import (
 	"fmt"
-	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/components"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
-	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner"
 	"github.com/DataDog/datadog-agent/test/new-e2e/tests/windows"
-	ad "github.com/DataDog/datadog-agent/test/new-e2e/tests/windows/active_directory"
-	windowsAgent "github.com/DataDog/datadog-agent/test/new-e2e/tests/windows/agent"
+	"github.com/DataDog/datadog-agent/test/new-e2e/tests/windows/common/active_directory"
 	"github.com/stretchr/testify/assert"
-	"net"
-	"net/url"
-	"path/filepath"
-	"strings"
 	"testing"
 	"time"
 )
 
-type testDomainSuite struct {
-	e2e.BaseSuite[ad.ActiveDirectoryEnv]
+const (
+	TestDomain   = "datadogqalab.com"
+	TestUser     = "TestUser"
+	TestPassword = "Test1234#"
+)
 
-	agentPackage *windowsAgent.Package
-	majorVersion string
+func TestInstallsOnDomainController(t *testing.T) {
+	suites := []e2e.Suite[active_directory.ActiveDirectoryEnv]{
+		&testInstallSuite{},
+		&testUpgradeSuite{},
+	}
+
+	for _, suite := range suites {
+		e2e.Run(t, suite, e2e.WithProvisioner(active_directory.Provisioner(
+			active_directory.WithActiveDirectoryOptions(
+				active_directory.WithDomainName(TestDomain),
+				active_directory.WithDomainPassword(TestPassword),
+				active_directory.WithDomainUser(TestUser, TestPassword),
+			))))
+	}
 }
 
-func TestOnDomainController(t *testing.T) {
-	agentPackage, err := windowsAgent.GetPackageFromEnv()
-	if err != nil {
-		t.Fatalf("failed to get MSI URL from env: %v", err)
-	}
-	t.Logf("Using Agent: %#v", agentPackage)
-	majorVersion := strings.Split(agentPackage.Version, ".")[0]
-
-	e2e.Run(t, &testDomainSuite{
-		agentPackage: agentPackage,
-		majorVersion: majorVersion,
-	}, e2e.WithProvisioner(ad.Provisioner(
-		ad.WithActiveDirectoryOptions(
-			ad.WithDomainName("datadogqalab.com"),
-			ad.WithDomainPassword("TestPassword1234#"),
-			ad.WithDomainUser("TestUser", "TestPassword1234#"),
-		))))
+type testInstallSuite struct {
+	windows.BaseAgentInstallerSuite
 }
 
-func installAgentPackage(host *components.RemoteHost, agentPackage *windowsAgent.Package, args string, logfile string) (string, error) {
-	remoteMSIPath, err := windows.GetTemporaryFile(host)
-	if err != nil {
-		return "", err
-	}
-	err = windows.PutOrDownloadFile(host, agentPackage.URL, remoteMSIPath)
-	if err != nil {
-		return "", err
-	}
-	return remoteMSIPath, windows.InstallMSI(host, remoteMSIPath, args, logfile)
+func (suite *testInstallSuite) TestGivenDomainUserCanInstallAgent() {
+	host := suite.Env().DomainControllerHost
+
+	_, err := suite.InstallAgent(host,
+		windows.WithPackage(suite.AgentPackage),
+		windows.WithAgentUser(fmt.Sprintf("%s\\%s", TestDomain, TestUser)),
+		windows.WithAgentUserPassword(fmt.Sprintf("\"%s\"", TestPassword)),
+		windows.WithValidApiKey(),
+		windows.WithFakeIntake(suite.Env().FakeIntake),
+		windows.WithInstallLogFile("TC-INS-DC-006_install.log"))
+
+	suite.Require().NoError(err, "should succeed to install Agent on a Domain Controller with a valid domain account & password")
+	suite.EventuallyWithT(func(c *assert.CollectT) {
+		metricNames, err := suite.Env().FakeIntake.Client().GetMetricNames()
+		assert.NoError(c, err)
+		assert.Greater(c, len(metricNames), 0)
+	}, 5*time.Minute, 10*time.Second)
 }
 
-// hostIPFromURL extracts the host from the given URL and returns any IP associated to that host
-// or an error
-func hostIPFromURL(fakeintakeURL string) (string, error) {
-	parsed, err := url.Parse(fakeintakeURL)
-	if err != nil {
-		return "", err
-	}
-
-	host := parsed.Hostname()
-	ips, err := net.LookupIP(host)
-	if err != nil {
-		return "", err
-	}
-
-	if len(ips) == 0 {
-		return "", fmt.Errorf("no ip for host %s", host)
-	}
-
-	// return any valid ip
-	return ips[0].String(), nil
+type testUpgradeSuite struct {
+	windows.BaseAgentInstallerSuite
 }
 
-func (suite *testDomainSuite) TestGivenDomainUserCanInstallAgent() {
-	outputDir, err := runner.GetTestOutputDir(runner.GetProfile(), suite.T())
-	suite.Require().NoError(err, "should get output dir")
-	suite.T().Logf("Output dir: %s", outputDir)
+func (suite *testUpgradeSuite) TestGivenDomainUserCanUpgradeAgent() {
 
 	host := suite.Env().DomainControllerHost
 
-	fakeIntakeIP, err := hostIPFromURL(suite.Env().FakeIntake.URL)
-	cmdline := fmt.Sprintf("SITE=%s DDAGENTUSER_NAME=datadogqalab.com\\DatadogTestUser DDAGENTUSER_PASSWORD=\"TestPassword1234#\"", fakeIntakeIP)
-	suite.T().Logf("Using command line %s", cmdline)
-	_, err = installAgentPackage(host, suite.agentPackage, cmdline, filepath.Join(outputDir, "tc_ins_dc_006_install.log"))
-	suite.Require().NoError(err, "should succeed to install Agent on a Domain Controller with a valid domain account & password")
+	_, err := suite.InstallAgent(host,
+		windows.WithLastStablePackage(),
+		windows.WithAgentUser(fmt.Sprintf("%s\\%s", TestDomain, TestUser)),
+		windows.WithAgentUserPassword(fmt.Sprintf("\"%s\"", TestPassword)),
+		windows.WithValidApiKey(),
+		windows.WithFakeIntake(suite.Env().FakeIntake),
+		windows.WithInstallLogFile("TC-UPG-DC-001_install_last_stable.log"))
 
+	suite.Require().NoError(err, "should succeed to install Agent on a Domain Controller with a valid domain account & password")
+	suite.EventuallyWithT(func(c *assert.CollectT) {
+		metricNames, err := suite.Env().FakeIntake.Client().GetMetricNames()
+		assert.NoError(c, err)
+		assert.Greater(c, len(metricNames), 0)
+	}, 5*time.Minute, 10*time.Second)
+
+	_, err = suite.InstallAgent(host,
+		windows.WithPackage(suite.AgentPackage),
+		windows.WithInstallLogFile("TC-UPG-DC-001_upgrade.log"))
+
+	suite.Require().NoError(err, "should succeed to upgrade an Agent on a Domain Controller")
 	suite.EventuallyWithT(func(c *assert.CollectT) {
 		metricNames, err := suite.Env().FakeIntake.Client().GetMetricNames()
 		assert.NoError(c, err)
