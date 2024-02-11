082a3305bc9d57aac3289baaa36bd959c262fa07
Guy Arbitman <guy20495@gmail.com>
2024-02-11 10:22:02 +0200

    usm: http2: Introduce dedicated incomplete mechanism
    The HTTP2 monitoring does not have a logic of attaching request-reponses, as we get the full stream from the kernel.
    Thus, the dedicated mechanism keeps incomplete requests until those become complete

---
+++ pkg/network/protocols/http2/incomplete_stats.go
@@ -0,0 +1,82 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022-present Datadog, Inc.
+
+//go:build linux_bpf
+
+package http2
+
+import (
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/network/config"
+	"github.com/DataDog/datadog-agent/pkg/network/protocols/http"
+)
+
+const (
+	defaultMinAge = 30 * time.Second
+)
+
+// incompleteBuffer is responsible for buffering incomplete transactions
+type incompleteBuffer struct {
+	data       []*EbpfTx
+	maxEntries int
+	minAgeNano int64
+}
+
+// NewIncompleteBuffer returns a new incompleteBuffer.
+func NewIncompleteBuffer(c *config.Config) http.IncompleteBuffer {
+	return &incompleteBuffer{
+		data:       make([]*EbpfTx, 0),
+		maxEntries: c.MaxHTTPStatsBuffered,
+		minAgeNano: defaultMinAge.Nanoseconds(),
+	}
+}
+
+// Add adds a transaction to the buffer.
+func (b *incompleteBuffer) Add(tx http.Transaction) {
+	if len(b.data) >= b.maxEntries {
+		return
+	}
+
+	// Copy underlying EbpfTx value.
+	ebpfTX, ok := tx.(*EbpfTx)
+	if !ok {
+		// should never happen
+		return
+	}
+
+	ebpfTxCopy := new(EbpfTx)
+	*ebpfTxCopy = *ebpfTX
+
+	b.data = append(b.data, ebpfTxCopy)
+}
+
+// Flush flushes the buffer and returns the joined transactions.
+func (b *incompleteBuffer) Flush(now time.Time) []http.Transaction {
+	var (
+		joined   []http.Transaction
+		previous = b.data
+		nowUnix  = now.UnixNano()
+	)
+
+	b.data = make([]*EbpfTx, 0)
+	for _, entry := range previous {
+		// now that we have finished matching requests and responses
+		// we check if we should keep orphan requests a little longer
+		if !entry.Incomplete() {
+			joined = append(joined, entry)
+		} else if b.shouldKeep(entry, nowUnix) {
+			b.data = append(b.data, entry)
+		}
+	}
+
+	return joined
+}
+
+func (b *incompleteBuffer) shouldKeep(tx http.Transaction, now int64) bool {
+	then := int64(tx.RequestStarted())
+	diff := now - then
+	return diff < b.minAgeNano
+}
