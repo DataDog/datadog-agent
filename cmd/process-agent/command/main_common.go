ebd15e1b471fbbdc958c94d753f45f43904ac63b
Daniel Tafoya <daniel.tafoya@datadoghq.com>
2024-02-12 12:36:40 -0500

    Fix issues

--- cmd/process-agent/command/main_common.go
+++ cmd/process-agent/command/main_common.go
@@ -207,11 +207,6 @@ func runApp(ctx context.Context, globalParams *GlobalParams) error {
 		return err
 	}
 
-	if appInitDeps.Config.GetBool("process_config.run_in_core_agent.enabled") {
-		log.Info("Running in core agent mode, exiting...")
-		return nil
-	}
-
 	// Look to see if any checks are enabled, if not, return since the agent doesn't need to be enabled.
 	if !shouldEnableProcessAgent(appInitDeps.Checks, appInitDeps.Config) {
 		log.Infof(agent6DisabledMessage)
