7b32474d220df54ab94986c29cf516d40667a2e0
Amit Slavin <108348428+amitslavin@users.noreply.github.com>
2024-02-12 15:16:34 +0200

    [USM] Add telemetry for interesting frame split on multiple TCP payloads (#22758)
    [USM] Add telemetry for interesting frame split on multiple TCP payloads

--- pkg/network/protocols/http2/model_linux.go
+++ pkg/network/protocols/http2/model_linux.go
@@ -417,6 +417,7 @@ HTTP2Telemetry{
 	"literal values exceed message count": %d,
 	"messages with more frames than we can filter": %d,
 	"messages with more interesting frames than we can process": %d,
+	"fragmented frame count": %d,
 	"path headers length distribution": {
 		"in range [0, 120)": %d,
 		"in range [120, 130)": %d,
@@ -428,7 +429,7 @@ HTTP2Telemetry{
 		"in range [180, infinity)": %d
 	}
 }`, t.Request_seen, t.Response_seen, t.End_of_stream, t.End_of_stream_rst, t.Literal_value_exceeds_frame,
-		t.Exceeding_max_frames_to_filter, t.Exceeding_max_interesting_frames, t.Path_size_bucket[0], t.Path_size_bucket[1],
+		t.Exceeding_max_frames_to_filter, t.Exceeding_max_interesting_frames, t.Fragmented_frame_count, t.Path_size_bucket[0], t.Path_size_bucket[1],
 		t.Path_size_bucket[2], t.Path_size_bucket[3], t.Path_size_bucket[4], t.Path_size_bucket[5], t.Path_size_bucket[6],
 		t.Path_size_bucket[7])
 }
