2dfb80556b48fa531338950bff90f86129d23f59
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-02-13 13:28:53 +0100

    Add some test cases

--- tasks/unit-tests/go_tests.py
+++ tasks/unit-tests/go_tests.py
@@ -4,9 +4,11 @@ from tasks.go_test import find_impacted_packages
 
 
 class TestUtils(unittest.TestCase):
-    def test_find_impacted_packages(self):
+    def test_impacted_packages_1(self):
         dependencies = {
-            "pkg1": ["pkg2", "pkg3"],
+            "pkg1": [
+                "pkg2",
+            ],
             "pkg2": ["pkg3"],
             "pkg3": [],
         }
@@ -14,12 +16,32 @@ class TestUtils(unittest.TestCase):
         expected_impacted_packages = {"pkg1", "pkg2", "pkg3"}
         self.assertEqual(find_impacted_packages(dependencies, changed_files), expected_impacted_packages)
 
-    def test_impacted_packages(self):
+    def test_impacted_packages_2(self):
         dependencies = {
-            "pkg1": ["pkg3"],
+            "pkg1": ["pkg2", "pkg3"],
+            "pkg2": ["pkg4"],
+            "pkg3": ["pkg4"],
+            "pkg4": [],
+        }
+        changed_files = {"pkg1"}
+        expected_impacted_packages = {"pkg1", "pkg2", "pkg3", "pkg4"}
+        self.assertEqual(find_impacted_packages(dependencies, changed_files), expected_impacted_packages)
+
+    def test_impacted_packages_3(self):
+        dependencies = {
+            "pkg1": ["pkg2"],
+            "pkg2": ["pkg1"],
+        }
+        changed_files = {"pkg1"}
+        expected_impacted_packages = {"pkg1", "pkg2"}
+        self.assertEqual(find_impacted_packages(dependencies, changed_files), expected_impacted_packages)
+
+    def test_impacted_packages_4(self):
+        dependencies = {
+            "pkg1": ["pkg2"],
             "pkg2": ["pkg3"],
             "pkg3": [],
         }
-        changed_files = {"pkg1"}
-        expected_impacted_packages = {"pkg1", "pkg3"}
+        changed_files = {"pkg3"}
+        expected_impacted_packages = {"pkg3"}
         self.assertEqual(find_impacted_packages(dependencies, changed_files), expected_impacted_packages)
