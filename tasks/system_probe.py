08b7d5245bdaef483cf7515cc89adfa5ee8f9d33
Derek Brown <derek.brown@datadoghq.com>
2024-02-12 22:34:27 -0800

    pylint

--- tasks/system_probe.py
+++ tasks/system_probe.py
@@ -866,7 +866,7 @@ def kitchen_prepare(ctx, kernel_release=None, ci=False, packages=""):
     # test/kitchen/site-cookbooks/dd-system-probe-check/files/default/tests/pkg/ebpf/testsuite
     # test/kitchen/site-cookbooks/dd-system-probe-check/files/default/tests/pkg/ebpf/bytecode/testsuite
     for i, pkg in enumerate(target_packages):
-        regexstr = "^{}.".format(re.escape(os.getcwd()))
+        regexstr = f"^{re.escape(os.getcwd())}."
         target_path = os.path.join(KITCHEN_ARTIFACT_DIR, re.sub(regexstr, "", pkg))
         target_bin = "testsuite"
         if is_windows:
