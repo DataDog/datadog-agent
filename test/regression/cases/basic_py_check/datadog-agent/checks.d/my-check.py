253e02a2fb579902389f6b54ed11d6e08f19b49f
Scott Opell <me@scottopell.com>
2024-02-14 13:23:25 -0500

    Prefixes unused var with underscore

--- test/regression/cases/basic_py_check/datadog-agent/checks.d/my-check.py
+++ test/regression/cases/basic_py_check/datadog-agent/checks.d/my-check.py
@@ -1,5 +1,5 @@
 from datadog_checks.checks import AgentCheck
 
 class MyCheck(AgentCheck):
-    def check(self, instance):
+    def check(self, _instance):
         self.gauge('hello.world', 1.23, tags=['foo:bar'])
