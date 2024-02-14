f2462dab21d467e786f7009043a4d279126597c0
Arthur Bellal <arthur.bellal@datadoghq.com>
2024-02-14 10:24:06 +0100

    use default log file

--- cmd/updater/command/command.go
+++ cmd/updater/command/command.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	pkgconfig "github.com/DataDog/datadog-agent/pkg/config"
 )
 
 // common constants for all the updater subcommands.
@@ -53,7 +54,7 @@ Datadog Updater updates your agents based on requests received from the Datadog 
 		SilenceUsage: true,
 	}
 
-	agentCmd.PersistentFlags().StringVarP(&globalParams.ConfFilePath, "cfgpath", "c", "", "path to directory containing updater.yaml")
+	agentCmd.PersistentFlags().StringVarP(&globalParams.ConfFilePath, "cfgpath", "c", pkgconfig.DefaultUpdaterLogFile, "path to directory containing updater.yaml")
 	agentCmd.PersistentFlags().StringVarP(&globalParams.Package, "package", "P", "", "package to update")
 	agentCmd.PersistentFlags().StringVarP(&globalParams.RepositoriesDir, "repositories", "d", "/opt/datadog-packages", "path to directory containing repositories")
 	_ = agentCmd.MarkFlagRequired("package")
