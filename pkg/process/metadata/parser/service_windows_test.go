034b66ae075530c358ec7b6e6a8fb18dd9146691
Levin Sanchez <94712128+levin0710@users.noreply.github.com>
2024-02-16 09:07:03 -0500

    Update service_windows_test.go

--- pkg/process/metadata/parser/service_windows_test.go
+++ pkg/process/metadata/parser/service_windows_test.go
@@ -57,7 +57,7 @@ func TestWindowsExtractServiceMetadata(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			enabled := true
-			useWindowsServiceName := true
+			useWindowsServiceName := false
 			proc := procutil.Process{
 				Pid:     1,
 				Cmdline: tt.cmdline,
