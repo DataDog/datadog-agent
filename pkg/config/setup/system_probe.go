340f481b96b86301c916f0f368cd4843c022fbd4
amit.slavin <amit.slavin@datadoghq.com>
2024-02-18 13:37:20 +0200

    fixed cfg

--- pkg/config/setup/system_probe.go
+++ pkg/config/setup/system_probe.go
@@ -303,8 +303,6 @@ func InitSystemProbeConfig(cfg pkgconfigmodel.Config) {
 
 	cfg.BindEnvAndSetDefault(join(smNS, "http2_dynamic_table_map_cleaner_interval_seconds"), 5)
 
-	cfg.BindEnvAndSetDefault(join(smNS, "http2_dynamic_table_map_cleaner_interval_seconds"), 30)
-
 	// Default value (300) is set in `adjustUSM`, to avoid having "deprecation warning", due to the default value.
 	cfg.BindEnv(join(spNS, "http_map_cleaner_interval_in_s"))
 	cfg.BindEnv(join(smNS, "http_map_cleaner_interval_in_s"))
