56abc1639d6877355571dcddbd3445c3bc3e9393
Raphael Gavache <raphael.gavache@datadoghq.com>
2024-02-20 13:35:41 +0100

    test

--- test/new-e2e/tests/updater/linux_test.go
+++ test/new-e2e/tests/updater/linux_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
+	"github.com/DataDog/test-infra-definitions/components/os"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
 
 	"github.com/stretchr/testify/require"
 )
@@ -20,9 +22,12 @@ type vmUpdaterSuite struct {
 }
 
 func TestUpdaterSuite(t *testing.T) {
-	e2e.Run(t, &vmUpdaterSuite{}, e2e.WithProvisioner(awshost.ProvisionerNoFakeIntake(awshost.WithUpdater())))
+	e2e.Run(t, &vmUpdaterSuite{}, e2e.WithProvisioner(awshost.ProvisionerNoFakeIntake(
+		awshost.WithUpdater(),
+		awshost.WithEC2InstanceOptions(ec2.WithOSArch(os.DebianDefault, os.ARM64Arch)),
+	)))
 }
 
 func (v *vmUpdaterSuite) TestUpdater() {
-	require.Equal(v.T(), "aarch64", v.Env().RemoteHost.MustExecute("uname -m"))
+	require.Equal(v.T(), "arm64", v.Env().RemoteHost.MustExecute("uname -m"))
 }
