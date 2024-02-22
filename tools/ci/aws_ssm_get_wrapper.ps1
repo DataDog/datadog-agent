89c74248092d86438763c2df177ee6fc0c368319
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-02-22 14:20:24 +0100

    Remove useless block in powershell script

--- tools/ci/aws_ssm_get_wrapper.ps1
+++ tools/ci/aws_ssm_get_wrapper.ps1
@@ -5,13 +5,6 @@ param (
 $retryCount = 0
 $maxRetries = 5
 
-if ($env:TraceLevel -eq '1') {
-    $script:originalDebugPreference = $DebugPreference
-    $DebugPreference = 'SilentlyContinue'
-    $script:enableDebug = { $DebugPreference = 'Continue' }
-    Register-EngineEvent PowerShell.Exiting -Action $enableDebug -SupportEvent
-}
-
 while ($retryCount -lt $maxRetries) {
     $result = (aws ssm get-parameter --region us-east-1 --name $parameterName --with-decryption --query "Parameter.Value" --output text)
 
