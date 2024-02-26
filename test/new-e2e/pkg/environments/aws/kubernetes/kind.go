e9f27c88554f124cea8ca7ddcb5159ff9ee0e770
Kevin Fairise <kevin.fairise@datadoghq.com>
2024-02-26 16:31:26 +0100

    Fix package name

--- test/new-e2e/pkg/environments/aws/kubernetes/kind.go
+++ test/new-e2e/pkg/environments/aws/kubernetes/kind.go
@@ -3,8 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-// Package kubernetes contains the provisioner for the Kubernetes based environments
-package kubernetes
+// Package awskubernetes contains the provisioner for the Kubernetes based environments
+package awskubernetes
 
 import (
 	"fmt"
