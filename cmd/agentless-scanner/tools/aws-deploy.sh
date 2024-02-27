37606c36bb9d6defd44bd5e297102c0b534b9d8d
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2024-02-27 12:34:55 +0100

    [agentless-scanner] AWS: cloudformation update with delegate roles

--- cmd/agentless-scanner/tools/aws-deploy.sh
+++ cmd/agentless-scanner/tools/aws-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Usage:
 #
 #   ssh-keygen -f ~/.ssh/sidescanner -t ed25519 -C "sidescannerdebug@datadoghq.com"
@@ -35,206 +36,19 @@ if [ -z "${STACK_SECURITY_GROUP}" ]; then
   exit 1
 fi
 
-if [ -z "${STACK_INSTANCE_TYPE}" ]; then
-  STACK_INSTANCE_TYPE="t4g.medium"
-fi
 BASEDIR=$(dirname "$0")
 
 # Stack meta
 STACK_NAME="DatadogAgentlessScanner-${STACK_OWNER}"
 STACK_AWS_REGION="us-east-1"
 STACK_PUBLIC_KEY=$(cat "$HOME/.ssh/sidescanner.pub")
-STACK_INSTALL_SH=$(base64 -w 0 < "${BASEDIR}/userdata.sh")
-STACK_USER_DATA=$(cat <<EOF | base64 -w 0
-#!/bin/bash
-echo "$STACK_INSTALL_SH" | base64 -d > userdata.sh
-chmod +x userdata.sh
-DD_API_KEY="$STACK_API_KEY" DD_API_KEY_DUAL="$STACK_API_KEY_DUAL" ./userdata.sh "${STACK_OWNER}"
-EOF
-)
-STACK_TEMPLATE=$(cat <<EOF
-AWSTemplateFormatVersion: '2010-09-09'
-Description: Create an EC2 instance in an isolated VPC for Datadog AgentlessScanner
+STACK_TEMPLATE=$(cat "$BASEDIR/aws-cf.yaml")
 
-Parameters:
-  DatadogAgentlessScannerAMIId:
-    Type: 'AWS::SSM::Parameter::Value<AWS::EC2::Image::Id>'
-    Default: '/aws/service/canonical/ubuntu/server/22.04/stable/current/arm64/hvm/ebs-gp2/ami-id'
 
-  DatadogAgentlessScannerInstanceType:
-    Type: String
-    Default: '${STACK_INSTANCE_TYPE}'
-
-Resources:
-  DatadogAgentlessScannerKeyPair:
-    Type: AWS::EC2::KeyPair
-    Properties:
-      KeyName: ${STACK_NAME}SSHKeyPair
-      PublicKeyMaterial: "${STACK_PUBLIC_KEY}"
-
-  DatadogAgentlessScannerAccessRole:
-    Type: AWS::IAM::Role
-    Properties:
-      AssumeRolePolicyDocument:
-        Version: '2012-10-17'
-        Statement:
-          - Effect: Allow
-            Principal:
-              Service:
-                - ec2.amazonaws.com
-            Action:
-              - sts:AssumeRole
-
-  DatadogAgentlessScannerAccessPolicy:
-    Type: AWS::IAM::Policy
-    Properties:
-      PolicyName: ${STACK_NAME}AccessPolicy
-      Roles:
-        - !Ref DatadogAgentlessScannerAccessRole
-      PolicyDocument:
-        Version: '2012-10-17'
-        Statement:
-        - Action: ec2:CreateTags
-          Condition:
-            StringEquals:
-              ec2:CreateAction:
-              - CreateSnapshot
-              - CreateVolume
-              - CopySnapshot
-          Effect: Allow
-          Resource:
-          - arn:aws:ec2:*:*:volume/*
-          - arn:aws:ec2:*:*:snapshot/*
-          Sid: DatadogAgentlessScannerResourceTagging
-        - Action: ec2:CreateSnapshot
-          Condition:
-            StringNotEquals:
-              aws:ResourceTag/DatadogAgentlessScanner: 'false'
-          Effect: Allow
-          Resource: arn:aws:ec2:*:*:volume/*
-          Sid: DatadogAgentlessScannerVolumeSnapshotCreation
-        - Action: ec2:CreateSnapshot
-          Condition:
-            ForAllValues:StringLike:
-              aws:TagKeys: DatadogAgentlessScanner*
-            StringEquals:
-              aws:RequestTag/DatadogAgentlessScanner: 'true'
-          Effect: Allow
-          Resource: arn:aws:ec2:*:*:snapshot/*
-          Sid: DatadogAgentlessScannerSnapshotCreation
-        - Action:
-          - ec2:ModifySnapshotAttribute
-          - ec2:DescribeSnapshotAttribute
-          - ec2:DeleteSnapshot
-          - ebs:ListSnapshotBlocks
-          - ebs:ListChangedBlocks
-          - ebs:GetSnapshotBlock
-          Condition:
-            StringEquals:
-              aws:ResourceTag/DatadogAgentlessScanner: 'true'
-          Effect: Allow
-          Resource: arn:aws:ec2:*:*:snapshot/*
-          Sid: DatadogAgentlessScannerSnapshotAccessAndCleanup
-        - Action: ec2:DescribeSnapshots
-          Effect: Allow
-          Resource: "*"
-          Sid: DatadogAgentlessScannerDescribeSnapshots
-        - Action: ec2:CreateVolume
-          Condition:
-            ForAllValues:StringLike:
-              aws:TagKeys: DatadogAgentlessScanner*
-            StringEquals:
-              aws:RequestTag/DatadogAgentlessScanner: 'true'
-          Effect: Allow
-          Resource: arn:aws:ec2:*:*:volume/*
-          Sid: DatadogAgentlessScannerVolumeCreation
-        - Action:
-          - ec2:DetachVolume
-          - ec2:AttachVolume
-          Condition:
-            StringEquals:
-              aws:ResourceTag/DatadogAgentlessScanner: 'true'
-          Effect: Allow
-          Resource: arn:aws:ec2:*:*:instance/*
-          Sid: DatadogAgentlessScannerVolumeAttachToInstance
-        - Action:
-          - ec2:DetachVolume
-          - ec2:DeleteVolume
-          - ec2:AttachVolume
-          Condition:
-            StringEquals:
-              aws:ResourceTag/DatadogAgentlessScanner: 'true'
-          Effect: Allow
-          Resource: arn:aws:ec2:*:*:volume/*
-          Sid: DatadogAgentlessScannerVolumeAttachAndDelete
-        - Action: lambda:GetFunction
-          Effect: Allow
-          Resource: arn:aws:lambda:*:*:function:*
-          Sid: GetLambdaDetails
-        - Action: lambda:ListFunctions
-          Effect: Allow
-          Resource: '*'
-          Sid: ListLambdas
-        - Action: ec2:DescribeInstances
-          Effect: Allow
-          Resource: '*'
-          Sid: DatadogAgentlessScannerOfflineModeLambdas
-        - Action: ec2:DescribeRegions
-          Effect: Allow
-          Resource: '*'
-          Sid: DatadogAgentlessScannerOfflineModeRegions
-        - Action: ec2:DescribeVolumes
-          Effect: Allow
-          Resource: '*'
-          Sid: DatadogAgentlessScannerDescribeVolumes
-        - Action: ec2:DescribeImages
-          Effect: Allow
-          Resource: '*'
-          Sid: DatadogAgentlessScannerDescribeImages
-        - Action: ec2:CopySnapshot
-          Effect: Allow
-          Condition:
-            ForAllValues:StringLike:
-              aws:TagKeys: DatadogAgentlessScanner*
-            StringEquals:
-              aws:RequestTag/DatadogAgentlessScanner: 'true'
-          Resource: arn:aws:ec2:*:*:snapshot/*
-          Sid: DatadogAgentlessScannerCopySnapshot
-
-  DatadogAgentlessScannerInstanceProfile:
-    Type: AWS::IAM::InstanceProfile
-    Properties:
-      Path: /
-      Roles:
-        - !Ref DatadogAgentlessScannerAccessRole
-
-  DatadogAgentlessScannerInstance:
-    Type: AWS::EC2::Instance
-    Properties:
-      InstanceType: !Ref DatadogAgentlessScannerInstanceType
-      KeyName: !Ref DatadogAgentlessScannerKeyPair
-      IamInstanceProfile: !Ref DatadogAgentlessScannerInstanceProfile
-      BlockDeviceMappings:
-      - DeviceName: /dev/sda1
-        Ebs:
-          DeleteOnTermination: true
-          Encrypted: true
-          VolumeSize: 30
-          VolumeType: gp2
-      UserData: ${STACK_USER_DATA}
-      SecurityGroupIds:
-        - ${STACK_SECURITY_GROUP}
-      SubnetId: ${STACK_SUBNET_ID}
-      ImageId: !Ref DatadogAgentlessScannerAMIId
-      Tags:
-        - Key: Name
-          Value: ${STACK_NAME}RootInstance
-        - Key: DatadogAgentlessScanner
-          Value: "true"
-        - Key: please_keep_my_resource
-          Value: "true"
-EOF
-)
+printf "validating template %s..." "${STACK_NAME}"
+aws cloudformation validate-template \
+  --template-body "$STACK_TEMPLATE"
+printf "\n"
 
 printf "deleting stack %s..." "$STACK_NAME"
 aws cloudformation delete-stack --stack-name "$STACK_NAME" --region "$STACK_AWS_REGION"
@@ -247,6 +61,12 @@ STACK_ARN=$(aws cloudformation create-stack \
   --region "$STACK_AWS_REGION" \
   --template-body "$STACK_TEMPLATE" \
   --capabilities CAPABILITY_NAMED_IAM \
+  --parameters \
+    "ParameterKey=DatadogAPIKey,ParameterValue=${STACK_API_KEY}" \
+    "ParameterKey=DatadogScannerOwner,ParameterValue=${STACK_OWNER}" \
+    "ParameterKey=DatadogPublicKeyMaterial,ParameterValue=${STACK_PUBLIC_KEY}" \
+    "ParameterKey=DatadogSubnetId,ParameterValue=${STACK_SUBNET_ID}" \
+    "ParameterKey=DatadogSecurityGroup,ParameterValue=${STACK_SECURITY_GROUP}" \
   --query 'StackId' \
   --output text)
 
