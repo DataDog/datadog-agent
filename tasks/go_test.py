5044377708f61be9dbaf317369923a4f94bfff90
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2024-02-28 16:04:07 +0100

    Windows fix

--- tasks/go_test.py
+++ tasks/go_test.py
@@ -392,7 +392,7 @@ def test(
             # The '{{{{' is necessary to escape the f-string first and the .format() second. It's needed for the Powershell 'If' loop.
             coverage_script_template = f"""$tempFile = (".\\{TMP_PROFILE_COV_PREFIX}." + ([guid]::NewGuid().ToString().Replace("-", "").Substring(0, 10)))
 $packages = if ($args.count -eq 0) {{{{"{{packages}}"}}}} else {"{{$args}}"}
-go test {gobuild_flags} {govet_flags} {gotest_flags} -json -coverprofile="$tempFile" $packages
+go test "($args | select -skip 1)" -json -coverprofile="$tempFile" {{packages}}
 exit $LASTEXITCODE"""
         else:
             coverage_script_template = f"""#!/usr/bin/env bash
