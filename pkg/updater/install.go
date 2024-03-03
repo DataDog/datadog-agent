7510679f583bfbae9dcc6513380be9b61157a62d
Arthur <bellal.arthur@gmail.com>
2024-03-03 21:53:16 +0100

    (fleet) first service management interation for the operator
    This PR makes the operator start / stop experiments of the agent by
    sending signals to systemd through writing at specific paths.
    
    This is a v0 and will soon be replaced by generic support. We are still
    adding it to complete an end-to-end working version.
    
    commit-id:f478e83a

--- pkg/updater/install.go
+++ pkg/updater/install.go
@@ -8,6 +8,8 @@ package updater
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	oci "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/types"
@@ -18,6 +20,10 @@ import (
 const (
 	datadogPackageLayerMediaType types.MediaType = "application/vnd.datadog.package.layer.v1.tar+zstd"
 	datadogPackageMaxSize                        = 3 << 30 // 3GiB
+
+	updaterSystemdCommandsPath                = "/opt/datadog/updater/systemd_commands"
+	updaterSystemdCommandsStartExperimentPath = updaterSystemdCommandsPath + "/start_experiment"
+	updaterSystemdCommandsStopExperimentPath  = updaterSystemdCommandsPath + "/stop_experiment"
 )
 
 type installer struct {
@@ -57,7 +63,11 @@ func (i *installer) installExperiment(pkg string, version string, image oci.Imag
 	if err != nil {
 		return fmt.Errorf("could not get repository: %w", err)
 	}
-	return repository.SetExperiment(version, tmpDir)
+	err = repository.SetExperiment(version, tmpDir)
+	if err != nil {
+		return fmt.Errorf("could not set experiment: %w", err)
+	}
+	return i.startExperiment(pkg)
 }
 
 func (i *installer) promoteExperiment(pkg string) error {
@@ -65,7 +75,11 @@ func (i *installer) promoteExperiment(pkg string) error {
 	if err != nil {
 		return fmt.Errorf("could not get repository: %w", err)
 	}
-	return repository.PromoteExperiment()
+	err = repository.PromoteExperiment()
+	if err != nil {
+		return fmt.Errorf("could not promote experiment: %w", err)
+	}
+	return i.stopExperiment(pkg)
 }
 
 func (i *installer) uninstallExperiment(pkg string) error {
@@ -73,7 +87,37 @@ func (i *installer) uninstallExperiment(pkg string) error {
 	if err != nil {
 		return fmt.Errorf("could not get repository: %w", err)
 	}
-	return repository.DeleteExperiment()
+	err = repository.DeleteExperiment()
+	if err != nil {
+		return fmt.Errorf("could not delete experiment: %w", err)
+	}
+	return i.stopExperiment(pkg)
+}
+
+func (i *installer) startExperiment(pkg string) error {
+	// TODO(arthur): currently we only support the datadog-agent package
+	if pkg != "datadog-agent" {
+		return nil
+	}
+	err := os.MkdirAll(updaterSystemdCommandsPath, 0755)
+	if err != nil {
+		return fmt.Errorf("could not create systemd commands directory: %w", err)
+	}
+	content := []byte(strconv.FormatInt(time.Now().UTC().UnixNano(), 10))
+	return os.WriteFile(updaterSystemdCommandsStartExperimentPath, content, 0644)
+}
+
+func (i *installer) stopExperiment(pkg string) error {
+	// TODO(arthur): currently we only support the datadog-agent package
+	if pkg != "datadog-agent" {
+		return nil
+	}
+	err := os.MkdirAll(updaterSystemdCommandsPath, 0755)
+	if err != nil {
+		return fmt.Errorf("could not create systemd commands directory: %w", err)
+	}
+	content := []byte(strconv.FormatInt(time.Now().UTC().UnixNano(), 10))
+	return os.WriteFile(updaterSystemdCommandsStopExperimentPath, content, 0644)
 }
 
 func extractPackageLayers(image oci.Image, dir string) error {
