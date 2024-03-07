19666c971e77888893d35f9fa94747e380f875cb
raymond zhao <raymond.zhao@datadoghq.com>
2024-03-07 11:29:49 -0500

    Add new YYYY-MM-DD HH-MM-SS automultiline pattern

--- pkg/logs/internal/decoder/auto_multiline_handler.go
+++ pkg/logs/internal/decoder/auto_multiline_handler.go
@@ -244,4 +244,6 @@ var formatsToTry = []*regexp.Regexp{
 	regexp.MustCompile(`^[A-Za-z_]+ \d+, \d+ \d+:\d+:\d+ (AM|PM)`),
 	// 2021-01-31 - with stricter matching around the months/days
 	regexp.MustCompile(`^\d{4}-(1[012]|0?[1-9])-([12][0-9]|3[01]|0?[1-9])`),
+	// 2023/02/20 14:33:24
+	regexp.MustCompile(`^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}`),
 }
