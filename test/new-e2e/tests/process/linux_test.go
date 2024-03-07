900ac52d171b84bb983bd6c1fbc24296953a3256
Daniel Tafoya <daniel.tafoya@datadoghq.com>
2024-03-07 15:10:55 -0500

    lint

--- test/new-e2e/tests/process/linux_test.go
+++ test/new-e2e/tests/process/linux_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
 
 	"github.com/DataDog/datadog-agent/test/fakeintake/aggregator"
-	fi "github.com/DataDog/datadog-agent/test/fakeintake/client"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
