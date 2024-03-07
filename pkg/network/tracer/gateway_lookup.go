64863d0e402f117a93f160ce8e0fff2b94efba99
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2024-03-07 12:46:50 -0600

    Add tag for timeout

--- pkg/network/tracer/gateway_lookup.go
+++ pkg/network/tracer/gateway_lookup.go
@@ -44,7 +44,7 @@ var gatewayLookupTelemetry = struct {
 	telemetry.NewStatCounterWrapper(gatewayLookupModuleName, "subnet_cache_misses", []string{}, "Counter measuring the number of subnet cache misses"),
 	telemetry.NewStatCounterWrapper(gatewayLookupModuleName, "subnet_cache_lookups", []string{}, "Counter measuring the number of subnet cache lookups"),
 	telemetry.NewStatCounterWrapper(gatewayLookupModuleName, "subnet_lookups", []string{}, "Counter measuring the number of subnet lookups"),
-	telemetry.NewStatCounterWrapper(gatewayLookupModuleName, "subnet_lookup_errors", []string{}, "Counter measuring the number of subnet lookup errors"),
+	telemetry.NewStatCounterWrapper(gatewayLookupModuleName, "subnet_lookup_errors", []string{"reason"}, "Counter measuring the number of subnet lookup errors"),
 }
 
 type gatewayLookup struct {
@@ -149,7 +149,6 @@ func (g *gatewayLookup) Lookup(cs *network.ConnectionStats) *network.Via {
 
 			gatewayLookupTelemetry.subnetLookups.Inc()
 			if s, err = g.subnetForHwAddrFunc(ifi.HardwareAddr); err != nil {
-				gatewayLookupTelemetry.subnetLookupErrors.Inc()
 				log.Debugf("error getting subnet info for interface index %d: %s", r.IfIndex, err)
 
 				// cache an empty result so that we don't keep hitting the
@@ -157,8 +156,10 @@ func (g *gatewayLookup) Lookup(cs *network.ConnectionStats) *network.Via {
 				if errors.IsTimeout(err) {
 					// retry after a minute if we timed out
 					g.subnetCache.Add(r.IfIndex, time.Now().Add(time.Minute))
+					gatewayLookupTelemetry.subnetLookupErrors.Inc("timeout")
 				} else {
 					g.subnetCache.Add(r.IfIndex, nil)
+					gatewayLookupTelemetry.subnetLookupErrors.Inc()
 				}
 				gatewayLookupTelemetry.subnetCacheSize.Inc()
 				return err
