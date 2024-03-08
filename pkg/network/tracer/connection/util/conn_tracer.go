03a48a5dddc61c985fbe903297db0147d99d6eef
Adam Karpowich <adam.karpowich@datadoghq.com>
2024-03-08 11:41:45 -0500

    fix linter issues

--- pkg/network/tracer/connection/util/conn_tracer.go
+++ pkg/network/tracer/connection/util/conn_tracer.go
@@ -16,7 +16,6 @@ import (
 	"github.com/cilium/ebpf/asm"
 
 	"github.com/DataDog/datadog-agent/pkg/ebpf"
-	ddebpf "github.com/DataDog/datadog-agent/pkg/ebpf"
 	ebpftelemetry "github.com/DataDog/datadog-agent/pkg/ebpf/telemetry"
 	"github.com/DataDog/datadog-agent/pkg/network/config"
 	"github.com/DataDog/datadog-agent/pkg/network/ebpf/probes"
@@ -24,9 +23,9 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
-// ComputeDefaultClosedConnRingBufferSize is the default buffer size of the ring buffer for closed connection events.
+// computeDefaultClosedConnRingBufferSize is the default buffer size of the ring buffer for closed connection events.
 // Must be a power of 2 and a multiple of the page size
-func ComputeDefaultClosedConnRingBufferSize() int {
+func computeDefaultClosedConnRingBufferSize() int {
 	numCPUs, err := utils.NumCPU()
 	if err != nil {
 		numCPUs = 1
@@ -44,10 +43,11 @@ func computeDefaultClosedConnPerfBufferSize() int {
 	return 8 * os.Getpagesize()
 }
 
+// EnableRingbuffersViaMapEditor sets up the ring buffer for closed connection events via a map editor
 func EnableRingbuffersViaMapEditor(mgrOpts *manager.Options) {
 	mgrOpts.MapSpecEditors[probes.ConnCloseEventMap] = manager.MapSpecEditor{
 		Type:       cebpf.RingBuf,
-		MaxEntries: uint32(ComputeDefaultClosedConnRingBufferSize()),
+		MaxEntries: uint32(computeDefaultClosedConnRingBufferSize()),
 		KeySize:    0,
 		ValueSize:  0,
 		EditorFlag: manager.EditType | manager.EditMaxEntries | manager.EditKeyValue,
@@ -55,7 +55,7 @@ func EnableRingbuffersViaMapEditor(mgrOpts *manager.Options) {
 }
 
 // SetupClosedConnHandler sets up the closed connection event handler
-func SetupClosedConnHandler(connCloseEventHandler ebpf.EventHandler, mgr *ddebpf.Manager, cfg *config.Config) {
+func SetupClosedConnHandler(connCloseEventHandler ebpf.EventHandler, mgr *ebpf.Manager, cfg *config.Config) {
 	switch handler := connCloseEventHandler.(type) {
 	case *ebpf.RingBufferHandler:
 		options := manager.RingBufferOptions{
@@ -63,7 +63,7 @@ func SetupClosedConnHandler(connCloseEventHandler ebpf.EventHandler, mgr *ddebpf
 			RecordHandler:    handler.RecordHandler,
 			TelemetryEnabled: cfg.InternalTelemetryEnabled,
 			// RingBufferSize is not used yet by the manager, we use a map editor to set it in the tracer
-			RingBufferSize: ComputeDefaultClosedConnRingBufferSize(),
+			RingBufferSize: computeDefaultClosedConnRingBufferSize(),
 		}
 		rb := &manager.RingBuffer{
 			Map:               manager.Map{Name: probes.ConnCloseEventMap},
