ee6d7eec560b850d02731423b9e14c5d6fa1cf48
louis-cqrl <93274433+louis-cqrl@users.noreply.github.com>
2024-03-08 19:03:16 +0100

    [ASCII-1227] Add init to loger to avoid bufferrisation  (#23399)
    * feat(pkg/util/log/): Create log_init.go to initialize logger before running tests
    
    * fix(pkg/util/log/log.go): Re organize Lock and Unlock logic in 'ChangeLogLevel' and in 'ReplaceLogger'
    
    * fix(pkg/util/log/log.go): delete Lock status in subfunction 'func (sw *DatadogLogger) shouldLog(level seelog.LogLevel) bool'
    
    * fix(pkg/util/log/log.go): delete Lock status in multiple subfunctions to put it on upperfunctions'
    
    * fix(pkg/process/checks/host_info_test.go): Fix test due to init logger
    
    * fix(pkg/process/checks/host_info_test.go): lint correction:
    
    * Fix: Lint correction
    
    * test add init fine for logger
    
    * fix commit history
    
    * fix(pkg/util/log/log_init.go): linter
    
    * Fix(pkg/util/log/log.go): fix version
    
    * fix(pkg/util/log/log*.go): Clean comments, reduce lock time if possible
    
    * fix(pkg/util/log/log.go): Clean comments
    
    * fix(pkg/util/log/log.go): lint of flush function
    
    * fix(pkg/util/log/log.go): unreachable unlock
    
    * fix(pkg/util/log/log.go): linter
    
    * Fix(pkg/util/log/log.go): fix version
    
    * Fix(pkg/util/log/log.go): remove comment

--- pkg/process/checks/host_info_test.go
+++ pkg/process/checks/host_info_test.go
@@ -141,6 +141,6 @@ func fakeExecCommand(command string, args ...string) *exec.Cmd {
 	cs := []string{"-test.run=TestGetHostnameShellCmd", "--", command}
 	cs = append(cs, args...)
 	cmd := exec.Command(os.Args[0], cs...)
-	cmd.Env = []string{"GO_TEST_PROCESS=1"}
+	cmd.Env = []string{"GO_TEST_PROCESS=1", "DD_LOG_LEVEL=info"} // Set LOG LEVEL to info
 	return cmd
 }
