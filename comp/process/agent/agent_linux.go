e169962379f2d0d7d491780984e2a2a5de56c1a5
Daniel Tafoya <daniel.tafoya@datadoghq.com>
2024-03-11 10:59:16 -0400

    Add logging

--- comp/process/agent/agent_linux.go
+++ comp/process/agent/agent_linux.go
@@ -37,7 +37,7 @@ func Enabled(config config.Component, checkComponents []types.CheckComponent, lo
 	case flavor.ProcessAgent:
 		if npmEnabled {
 			if runInCoreAgent {
-				log.Warn("Network Performance Monitoring is not supported in the core agent. " +
+				log.Info("Network Performance Monitoring is not supported in the core agent. " +
 					"The process-agent will be enabled as a standalone agent")
 			}
 			return true
@@ -51,11 +51,10 @@ func Enabled(config config.Component, checkComponents []types.CheckComponent, lo
 	case flavor.DefaultAgent:
 		if npmEnabled {
 			if runInCoreAgent {
-				log.Error("Network Performance Monitoring is not supported in the core agent. " +
-					"Please ensure the process-agent is enabled as a standalone agent to collect " +
-					"process, container and network performance metrics.")
+				log.Info("Network Performance Monitoring is not supported in the core agent. " +
+					"The process-agent will be enabled as a standalone agent to collect network performance metrics.")
 			}
-			return false
+			return true
 		}
 		return runInCoreAgent
 	default:
