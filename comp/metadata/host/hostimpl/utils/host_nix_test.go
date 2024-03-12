08caf3d8cba39de298c714fff66b89aa2e526147
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2024-03-12 10:55:55 +0100

    fix: remove previous assertion

--- comp/metadata/host/hostimpl/utils/host_nix_test.go
+++ comp/metadata/host/hostimpl/utils/host_nix_test.go
@@ -29,7 +29,6 @@ func TestGetHostInfo(t *testing.T) {
 	// boot time can be computed dynamically using uptime on some platforms, in which case
 	// there can be an off-by-one error
 	assert.InDelta(t, expected.BootTime, info.BootTime, 1.5)
-	assert.Equal(t, expected.BootTime, info.BootTime)
 	assert.Equal(t, expected.HostID, info.HostID)
 	assert.Equal(t, expected.Hostname, info.Hostname)
 	assert.Equal(t, expected.KernelArch, info.KernelArch)
