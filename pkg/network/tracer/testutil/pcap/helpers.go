4f949c136f15257b09faa4eb1524345a08c78418
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-03-13 09:32:50 +0000

    fix linter

--- pkg/network/tracer/testutil/pcap/helpers.go
+++ pkg/network/tracer/testutil/pcap/helpers.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// GetPacketSourceFromPCAP returns a gopacket PacketSource for the given PCAP file.
 func GetPacketSourceFromPCAP(t *testing.T, path string) *gopacket.PacketSource {
 	t.Helper()
 
