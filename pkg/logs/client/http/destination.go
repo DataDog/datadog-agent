79c95b2a2491685130cbe0d4b59adf44b3338546
AliDatadog <ali.benabdallah@datadoghq.com>
2024-03-13 12:34:21 +0100

    remove debuglog for req

--- pkg/logs/client/http/destination.go
+++ pkg/logs/client/http/destination.go
@@ -307,7 +307,6 @@ func (d *Destination) unconditionalSend(payload *message.Payload) (err error) {
 		log.Debugf("Server closed or terminated the connection after serving the request with err %v", err)
 		return err
 	}
-	log.Infof("debuglog req %+v", req)
 	log.Infof("debuglog resp %+v", resp)
 
 	metrics.DestinationHttpRespByStatusAndUrl.Add(strconv.Itoa(resp.StatusCode), 1)
