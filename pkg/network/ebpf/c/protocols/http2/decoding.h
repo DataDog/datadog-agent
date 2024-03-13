d168fcddc83c2c104f2278707a97bfef5b24e507
amit.slavin <amit.slavin@datadoghq.com>
2024-03-13 17:15:55 +0200

    fix size check

--- pkg/network/ebpf/c/protocols/http2/decoding.h
+++ pkg/network/ebpf/c/protocols/http2/decoding.h
@@ -568,7 +568,7 @@ static __always_inline bool find_relevant_frames(struct __sk_buff *skb, skb_info
             iteration_value->frames_count++;
         }
 
-        if (current_frame.length > 16384 ){
+        if (current_frame.length >= 16384 ){
             __sync_fetch_and_add(&http2_tel->large_payload, 1);
         }
         skb_info->data_off += current_frame.length;
@@ -667,7 +667,7 @@ int socket__http2_handle_first_frame(struct __sk_buff *skb) {
         iteration_value->frames_count = 1;
     }
 
-    if (current_frame.length > 16384 ){
+    if (current_frame.length >= 16384 ){
         __sync_fetch_and_add(&http2_tel->large_payload, 1);
     }
     dispatcher_args_copy.skb_info.data_off += current_frame.length;
