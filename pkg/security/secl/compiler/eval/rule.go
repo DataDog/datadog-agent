9494898bdd8ea319af2c0f91316f82ec080a19b5
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-03-13 19:30:15 +0100

    reduce allocation related to `ErrFieldNotFound`

--- pkg/security/secl/compiler/eval/rule.go
+++ pkg/security/secl/compiler/eval/rule.go
@@ -7,7 +7,6 @@
 package eval
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 	"slices"
@@ -69,6 +68,12 @@ func NewRule(id string, expression string, opts *Opts, tags ...string) *Rule {
 	}
 }
 
+// IsPartialAvailable checks if partial have been generated for the given Field
+func (r *RuleEvaluator) IsPartialAvailable(field Field) bool {
+	_, exists := r.partialEvals[field]
+	return exists
+}
+
 // PartialEval partially evaluation of the Rule with the given Field.
 func (r *RuleEvaluator) PartialEval(ctx *Context, field Field) (bool, error) {
 	eval, ok := r.partialEvals[field]
@@ -103,19 +108,13 @@ func (r *Rule) GetFieldValues(field Field) []FieldValue {
 
 // PartialEval - Partial evaluation with the given Field
 func (r *Rule) PartialEval(ctx *Context, field Field) (bool, error) {
-	result, err := r.evaluator.PartialEval(ctx, field)
-	if err == nil {
-		return result, nil
-	}
-
-	var errNotFound *ErrFieldNotFound
-	if errors.As(err, &errNotFound) {
-		if err = r.genPartials(field); err != nil {
+	if !r.evaluator.IsPartialAvailable(field) {
+		if err := r.genPartials(field); err != nil {
 			return false, err
 		}
-		result, err = r.evaluator.PartialEval(ctx, field)
 	}
-	return result, err
+
+	return r.evaluator.PartialEval(ctx, field)
 }
 
 // GetPartialEval - Returns the Partial RuleEvaluator for the given Field
