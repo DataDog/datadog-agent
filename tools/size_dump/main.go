2145c002e4da780cd5857fd5774dfd2e96fa4be5
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2024-03-13 13:02:07 +0100

    [tools] size_dump: adding correct licensing information

--- tools/size_dump/main.go
+++ tools/size_dump/main.go
@@ -1,3 +1,11 @@
+// Software (c) by Dylan Reimerink
+//
+// This software is licensed under a
+// Creative Commons Attribution-ShareAlike 4.0 International License.
+//
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+
 package main
 
 import (
