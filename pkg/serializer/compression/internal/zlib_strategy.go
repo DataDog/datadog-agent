f9920452e5efec610cea1fba7343b8a4ed1edfc3
raymond zhao <raymond.zhao@datadoghq.com>
2024-03-15 10:44:10 -0400

    Add build tag to strategies

--- pkg/serializer/compression/internal/zlib_strategy.go
+++ pkg/serializer/compression/internal/zlib_strategy.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build zlib
+
 // Package compression provides a set of functions for compressing with zlib / zstd
 package compression
 
