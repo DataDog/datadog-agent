f4a8a9ef132e8ff3e8fc06bab5c1c8274e6482e6
Guy Arbitman <guy20495@gmail.com>
2024-03-15 15:32:46 +0200

    Increase USM timeout in kmt

--- test/new-e2e/system-probe/test-runner/main.go
+++ test/new-e2e/system-probe/test-runner/main.go
@@ -58,7 +58,7 @@ var baseEnv = []string{
 var timeouts = map[*regexp.Regexp]time.Duration{
 	regexp.MustCompile("pkg/network/protocols/http$"): 15 * time.Minute,
 	regexp.MustCompile("pkg/network/tracer$"):         55 * time.Minute,
-	regexp.MustCompile("pkg/network/usm$"):            30 * time.Minute,
+	regexp.MustCompile("pkg/network/usm$"):            55 * time.Minute,
 	regexp.MustCompile("pkg/security/.*"):             30 * time.Minute,
 }
 
