e63838c55b53bf917c18784b09d22d06a8f09230
Minyi Zhu <minyi.zhu@datadoghq.com>
2024-03-18 15:38:16 -0400

    config store

--- comp/core/autodiscovery/scheduler/meta.go
+++ comp/core/autodiscovery/scheduler/meta.go
@@ -6,8 +6,11 @@
 package scheduler
 
 import (
+	"context"
 	"sync"
+	"time"
 
+	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/configstore"
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/integration"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
@@ -17,20 +20,88 @@ type MetaScheduler struct {
 	// m protects all fields in this struct.
 	m sync.Mutex
 
-	// scheduledConfigs contains the set of configs that have been scheduled
-	// via the metascheduler, but not subsequently unscheduled.
-	scheduledConfigs map[string]integration.Config
-
 	// activeSchedulers is the set of schedulers currently subscribed to configs.
 	activeSchedulers map[string]Scheduler
+
+	// configStore contains the set of configs that have been scheduled and to be scheduled
+	configStore *configstore.ConfigStore
+
+	scheduledEventCh chan scheduledEvent
+
+	stopCh chan bool
+
+	started bool
 }
+
+type scheduledEvent struct {
+	configs   []integration.Config
+	eventType configstore.EventType
+}
+
+const (
+	scheduledEventChBufferSize = 50
+	reTryInterval              = 30 * time.Second
+)
 
 // NewMetaScheduler inits a meta scheduler
 func NewMetaScheduler() *MetaScheduler {
-	return &MetaScheduler{
-		scheduledConfigs: make(map[string]integration.Config),
+	metaScheduler := MetaScheduler{
 		activeSchedulers: make(map[string]Scheduler),
+		configStore:      configstore.NewConfigStore(),
+		scheduledEventCh: make(chan scheduledEvent, scheduledEventChBufferSize),
+		stopCh:           make(chan bool),
+		started:          false,
 	}
+	metaScheduler.start()
+	return &metaScheduler
+}
+
+func (ms *MetaScheduler) start() {
+	ms.m.Lock()
+	if ms.started {
+		return
+	}
+	ms.m.Unlock()
+	ms.started = true
+	go func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		for {
+			select {
+
+			case ev := <-ms.scheduledEventCh:
+				ms.configStore.Push(ev.configs, ev.eventType)
+				ms.processQueue()
+
+			case <-ms.stopCh:
+				ms.started = false
+				return
+
+			case <-ctx.Done():
+				ms.started = false
+				return
+			}
+		}
+	}()
+	go func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		ticker := time.NewTicker(reTryInterval)
+		for {
+			select {
+			case <-ticker.C:
+				ms.processQueue()
+
+			case <-ms.stopCh:
+				ms.started = false
+				return
+
+			case <-ctx.Done():
+				ms.started = false
+				return
+			}
+		}
+	}()
 }
 
 // Register a new scheduler to receive configurations.
@@ -50,10 +121,7 @@ func (ms *MetaScheduler) Register(name string, s Scheduler, replayConfigs bool) 
 	// this occurs under the protection of `ms.m`, so no config may be double-
 	// scheduled or missed in this process.
 	if replayConfigs {
-		configs := make([]integration.Config, 0, len(ms.scheduledConfigs))
-		for _, config := range ms.scheduledConfigs {
-			configs = append(configs, config)
-		}
+		configs := ms.configStore.List()
 		s.Schedule(configs)
 	}
 }
@@ -71,27 +139,32 @@ func (ms *MetaScheduler) Deregister(name string) {
 
 // Schedule schedules configs to all registered schedulers
 func (ms *MetaScheduler) Schedule(configs []integration.Config) {
-	ms.m.Lock()
-	defer ms.m.Unlock()
 	for _, config := range configs {
 		log.Tracef("Scheduling %s\n", config.Dump(false))
-		ms.scheduledConfigs[config.Digest()] = config
 	}
-	for _, scheduler := range ms.activeSchedulers {
-		scheduler.Schedule(configs)
+	ms.scheduledEventCh <- scheduledEvent{
+		configs:   configs,
+		eventType: configstore.SCHEDULE_TASK,
 	}
 }
 
 // Unschedule unschedules configs to all registered schedulers
 func (ms *MetaScheduler) Unschedule(configs []integration.Config) {
-	ms.m.Lock()
-	defer ms.m.Unlock()
+
 	for _, config := range configs {
 		log.Tracef("Unscheduling %s\n", config.Dump(false))
-		delete(ms.scheduledConfigs, config.Digest())
 	}
+	ms.scheduledEventCh <- scheduledEvent{
+		configs:   configs,
+		eventType: configstore.UNSCHEDULE_TASK,
+	}
+}
+
+func (ms *MetaScheduler) processQueue() {
+	ms.m.Lock()
+	defer ms.m.Unlock()
 	for _, scheduler := range ms.activeSchedulers {
-		scheduler.Unschedule(configs)
+		ms.configStore.HandleEvents(scheduler.Schedule, scheduler.Unschedule)
 	}
 }
 
@@ -99,7 +172,17 @@ func (ms *MetaScheduler) Unschedule(configs []integration.Config) {
 func (ms *MetaScheduler) Stop() {
 	ms.m.Lock()
 	defer ms.m.Unlock()
+	ms.stopCh <- true
+	ms.configStore.Stop()
 	for _, scheduler := range ms.activeSchedulers {
 		scheduler.Stop()
 	}
+}
+
+// PurgeStoreEvents purges task events in the config store
+// Used for testing purposes
+func (ms *MetaScheduler) PurgeStoreEvents() {
+	ms.m.Lock()
+	defer ms.m.Unlock()
+	ms.configStore.PurgeEvents()
 }
--- comp/core/autodiscovery/scheduler/meta_test.go
+++ comp/core/autodiscovery/scheduler/meta_test.go
@@ -7,6 +7,7 @@ package scheduler
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -53,32 +54,38 @@ func TestMetaScheduler(t *testing.T) {
 	c1 := makeConfig("one")
 	c2 := makeConfig("two")
 	ms.Schedule([]integration.Config{c1, c2})
-
+	time.Sleep(10 * time.Millisecond)
 	// register a scheduler and see that it gets caught up
 	s1 := &scheduler{}
 	ms.Register("s1", s1, true)
 	require.ElementsMatch(t, []event{{true, "one"}, {true, "two"}}, s1.events)
+	ms.PurgeStoreEvents()
 	s1.reset()
 
 	// remove one of those configs and add another
 	ms.Unschedule([]integration.Config{c1})
 	c3 := makeConfig("three")
 	ms.Schedule([]integration.Config{c3})
-
 	// check s1 was informed about those in order
+	time.Sleep(10 * time.Millisecond)
 	require.Equal(t, []event{{false, "one"}, {true, "three"}}, s1.events)
+	ms.PurgeStoreEvents()
 	s1.reset()
 
 	// subscribe a new scheduler and see that it does not get c1
 	s2 := &scheduler{}
 	ms.Register("s2", s2, true)
+	time.Sleep(10 * time.Millisecond)
 	require.ElementsMatch(t, []event{{true, "two"}, {true, "three"}}, s2.events)
+	ms.PurgeStoreEvents()
 	s2.reset()
 
 	// unsubscribe s1 and see that it no longer gets events
 	ms.Deregister("s1")
 	ms.Unschedule([]integration.Config{c2})
+	time.Sleep(10 * time.Millisecond)
 	require.Equal(t, []event{}, s1.events)
+	ms.PurgeStoreEvents()
 	s1.reset()
 	require.Equal(t, []event{{false, "two"}}, s2.events)
 	s2.reset()
@@ -87,5 +94,6 @@ func TestMetaScheduler(t *testing.T) {
 	s3 := &scheduler{}
 	ms.Register("s3", s3, false)
 	require.ElementsMatch(t, []event{}, s3.events)
+	ms.Stop()
 	s3.reset()
 }
