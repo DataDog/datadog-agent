369deafcdf6f56806bec4b203b4f61f8d5da420a
Raymond Zhao <35050708+rayz@users.noreply.github.com>
2024-03-19 13:56:52 -0400

    [AMLII-434] Allow metric payloads to be compressed with zstd (#23501)
    Co-authored-by: carlosroman <carlos.roman@datadoghq.com>

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -73,6 +73,9 @@ const (
 	// DefaultRuntimePoliciesDir is the default policies directory used by the runtime security module
 	DefaultRuntimePoliciesDir = "/etc/datadog-agent/runtime-security.d"
 
+	// DefaultCompressorKind is the default compressor. Options available are 'zlib' and 'zstd'
+	DefaultCompressorKind = "zlib"
+
 	// DefaultLogsSenderBackoffFactor is the default logs sender backoff randomness factor
 	DefaultLogsSenderBackoffFactor = 2.0
 
@@ -441,6 +444,7 @@ func InitConfig(config pkgconfigmodel.Config) {
 	config.BindEnvAndSetDefault("serializer_max_series_points_per_payload", 10000)
 	config.BindEnvAndSetDefault("serializer_max_series_payload_size", 512000)
 	config.BindEnvAndSetDefault("serializer_max_series_uncompressed_payload_size", 5242880)
+	config.BindEnvAndSetDefault("serializer_compressor_kind", DefaultCompressorKind)
 
 	config.BindEnvAndSetDefault("use_v2_api.series", true)
 	// Serializer: allow user to blacklist any kind of payload to be sent
