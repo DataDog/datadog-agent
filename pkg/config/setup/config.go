6b9ac46f82516203137e053e1e1d59a14390ca09
maxime mouial <hush-hush@users.noreply.github.com>
2024-03-19 12:47:32 +0100

    Removing Endpoint from the public config API (#23722)
    This reduces the API from the config package and prevent other packages
    from using it for other fields by mistake.

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -176,14 +176,6 @@ type MetricMapping struct {
 	Tags      map[string]string `mapstructure:"tags" json:"tags" yaml:"tags"`
 }
 
-// Endpoint represent a datadog endpoint
-type Endpoint struct {
-	Site   string `mapstructure:"site" json:"site" yaml:"site"`
-	URL    string `mapstructure:"url" json:"url" yaml:"url"`
-	APIKey string `mapstructure:"api_key" json:"api_key" yaml:"api_key"`
-	APPKey string `mapstructure:"app_key" json:"app_key" yaml:"app_key"`
-}
-
 // DataType represent the generic data type (e.g. metrics, logs) that can be sent by the Agent
 type DataType string
 
