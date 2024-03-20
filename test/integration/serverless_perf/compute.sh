fdbeb5d0832de90b89362ecb417fe8d51aca0205
Duncan Harvey <35278470+duncanpharvey@users.noreply.github.com>
2024-03-20 16:19:20 -0400

    [SVLS-4629] Reduce Flakiness of Cold Start Performance Integration Test (#23928)
    * Compare median start time against threshold for serverless cold start integration test
    
    * Structure log messages from serverless cold start job
    
    * Add fields to log
    
    * Add message when median exceeds the threshold

--- test/integration/serverless_perf/compute.sh
+++ test/integration/serverless_perf/compute.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-STARTUP_TIME_THREESHOLD=20
-TOTAL_THREESHOLD=$((STARTUP_TIME_THREESHOLD*10))
+STARTUP_TIME_THRESHOLD=20
 
-totalMs=0
+calculate_median() {
+    local sorted=($(printf "%s\n" "${@}" | sort -n))
+
+    local len=${#sorted[@]}
+    local middle=$((len / 2))
+
+    if ((len % 2 == 0)); then
+        local median=$(((${sorted[middle - 1]} + ${sorted[middle]}) / 2))
+    else
+        local median=${sorted[middle]}
+    fi
+
+    echo "$median"
+}
+
+log() {
+    echo "Job Name=$CI_JOB_NAME | Pipeline Id=$CI_PIPELINE_ID | Job Id=$CI_JOB_ID | Branch Name=$CI_COMMIT_BRANCH | $1"
+}
+
+startupTimes=()
 
 # loop 10 times to incur no false positive/negative alarms
 for i in {1..10}
@@ -12,15 +30,17 @@ do
     dockerId=$(docker run -d datadogci/lambda-extension)
     sleep 10
     numberOfMillisecs=$(docker logs "$dockerId" | grep 'ready in' | grep -Eo '[0-9]{1,4}' | tail -3 | head -1)
-    totalMs=$((totalMs+numberOfMillisecs))
-    echo "Iteration $i - Statup time = $numberOfMillisecs"
+    startupTimes+=($numberOfMillisecs)
+    log "Iteration=$i | Startup Time=$numberOfMillisecs"
 done
 
-echo "Total computed : $totalMs"
-echo "Threshold : $TOTAL_THREESHOLD"
+medianMs=$(calculate_median "${startupTimes[@]}")
 
-# check whether or not the total duration exceeds the threshold
-if (( totalMs > TOTAL_THREESHOLD )); then
+log "Median=$medianMs | Threshold=$STARTUP_TIME_THRESHOLD"
+
+# check whether or not the median duration exceeds the threshold
+if (( medianMs > STARTUP_TIME_THRESHOLD )); then
+    echo "Median startup time above threshold"
     exit 1
 fi
 
