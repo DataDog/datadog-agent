752243444757b53deb61dfa4b444b5c5fe0a08df
Vincent Boulineau <58430298+vboulineau@users.noreply.github.com>
2024-03-22 18:10:18 +0100

    Move external/custom metrics in a dedicated pkg in cluster agent (#23712)

--- pkg/clusteragent/autoscaling/externalmetrics/autoscaler_watcher.go
+++ pkg/clusteragent/autoscaling/externalmetrics/autoscaler_watcher.go
@@ -26,7 +26,7 @@ import (
 
 	"github.com/DataDog/watermarkpodautoscaler/api/v1alpha1"
 
-	"github.com/DataDog/datadog-agent/pkg/clusteragent/externalmetrics/model"
+	"github.com/DataDog/datadog-agent/pkg/clusteragent/autoscaling/externalmetrics/model"
 	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/apiserver"
 	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/autoscalers"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
