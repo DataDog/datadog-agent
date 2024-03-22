e45450a4848b2fc859517cc5f9a107995baee78f
Usama Saqib <usamasaqib.96@live.com>
2024-03-22 15:00:13 +0100

    Allow users to skip provisioning microvms (#23976)
    * explicitly specify if microvms should be provisioned
    
    * remove debug print
    
    * lint-python
    
    * update test-infra-definitions
    
    * fix args type to list
    
    * python-lint

--- tasks/kmt.py
+++ tasks/kmt.py
@@ -184,9 +184,14 @@ def gen_config_from_ci_pipeline(
 
 @task
 def launch_stack(
-    ctx: Context, stack: Optional[str] = None, ssh_key="", x86_ami=X86_AMI_ID_SANDBOX, arm_ami=ARM_AMI_ID_SANDBOX
+    ctx: Context,
+    stack: Optional[str] = None,
+    ssh_key: str = "",
+    x86_ami: str = X86_AMI_ID_SANDBOX,
+    arm_ami: str = ARM_AMI_ID_SANDBOX,
+    provision_microvms: bool = True,
 ):
-    stacks.launch_stack(ctx, stack, ssh_key, x86_ami, arm_ami)
+    stacks.launch_stack(ctx, stack, ssh_key, x86_ami, arm_ami, provision_microvms)
 
 
 @task
