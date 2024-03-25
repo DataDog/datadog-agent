8b9fa6a4c6382d9d9322cbd60a3ebdb8d607b007
Maxime Riaud <maxime.riaud@datadoghq.com>
2024-03-25 16:59:24 +0100

    create new runsimesettings comp and move current implementation files in it

---
+++ comp/agent/runtimesettings/runtimesettingsimpl/runtimesettings.go
@@ -0,0 +1,19 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+package runtimesettingsimpl
+
+import (
+	"go.uber.org/fx"
+
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+)
+
+// Module defines the fx options for this component.
+func Module() fxutil.Module {
+	return fxutil.Component(
+		fx.Provide( /* TODO: provide Component constructor */ ),
+	)
+}
