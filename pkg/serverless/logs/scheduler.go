7628f16639246e3ab5d8b4dc23f943f26dfcf8b8
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-03-25 15:16:15 -0400

    remove extra space for diff

--- pkg/serverless/logs/scheduler.go
+++ pkg/serverless/logs/scheduler.go
@@ -28,7 +28,6 @@ func SetupLogAgent(logChannel chan *config.ChannelMessage, sourceName string, so
 
 	logsScheduler = channel.NewScheduler(sourceName, source, logChannel)
 	agent.AddScheduler(logsScheduler)
-
 	return agent, nil
 }
 
