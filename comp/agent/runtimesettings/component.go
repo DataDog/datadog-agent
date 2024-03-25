247827275a61aa97eb63d089e7b55d17db626e11
Maxime Riaud <maxime.riaud@datadoghq.com>
2024-03-25 16:59:24 +0100

    create new runsimesettings comp and move current implementation files in it

---
+++ comp/agent/runtimesettings/component.go
@@ -0,0 +1,14 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+// Package runtimesettings ... /* TODO: detailed doc comment for the component */
+package runtimesettings
+
+// team: /* TODO: add team name */
+
+// Component is the component type.
+type Component interface {
+	/* TODO: define Component interface */
+}
