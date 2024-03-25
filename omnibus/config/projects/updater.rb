a4edc0b437d3fdb48ed69f2d616f49646d33b7dc
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-03-25 14:55:37 +0100

    Revert "Split remote updater build & packaging (#23968)" (#24060)
    This reverts commit 4cf32147d359f48ae1c6234bf8d85aa5041affb1.

--- omnibus/config/projects/updater.rb
+++ omnibus/config/projects/updater.rb
@@ -17,10 +17,33 @@ INSTALL_DIR = '/opt/datadog/updater'
 
 install_dir INSTALL_DIR
 
-if ENV.has_key?("OMNIBUS_WORKERS_OVERRIDE")
-  COMPRESSION_THREADS = ENV["OMNIBUS_WORKERS_OVERRIDE"].to_i
+if redhat_target? || suse_target?
+  maintainer 'Datadog, Inc <package@datadoghq.com>'
+
+  # NOTE: with script dependencies, we only care about preinst/postinst/posttrans,
+  # because these would be used in a kickstart during package installation phase.
+  # All of the packages that we depend on in prerm/postrm scripts always have to be
+  # installed on all distros that we support, so we don't have to depend on them
+  # explicitly.
+
+  # postinst and posttrans scripts use a subset of preinst script deps, so we don't
+  # have to list them, because they'll already be there because of preinst
+  runtime_script_dependency :pre, "coreutils"
+  runtime_script_dependency :pre, "findutils"
+  runtime_script_dependency :pre, "grep"
+  if redhat_target?
+    runtime_script_dependency :pre, "glibc-common"
+    runtime_script_dependency :pre, "shadow-utils"
+  else
+    runtime_script_dependency :pre, "glibc"
+    runtime_script_dependency :pre, "shadow"
+  end
 else
-  COMPRESSION_THREADS = 1
+  maintainer 'Datadog Packages <package@datadoghq.com>'
+end
+
+if debian_target?
+  runtime_recommended_dependency 'datadog-signing-keys (>= 1:1.3.1)'
 end
 
 # build_version is computed by an invoke command/function.
@@ -42,13 +65,21 @@ description 'Datadog Updater
 
 # .deb specific flags
 package :deb do
-  skip_packager true
+  vendor 'Datadog <package@datadoghq.com>'
+  epoch 1
+  license 'Apache License Version 2.0'
+  section 'utils'
+  priority 'extra'
+  if ENV.has_key?('DEB_SIGNING_PASSPHRASE') and not ENV['DEB_SIGNING_PASSPHRASE'].empty?
+    signing_passphrase "#{ENV['DEB_SIGNING_PASSPHRASE']}"
+    if ENV.has_key?('DEB_GPG_KEY_NAME') and not ENV['DEB_GPG_KEY_NAME'].empty?
+      gpg_key_name "#{ENV['DEB_GPG_KEY_NAME']}"
+    end
+  end
 end
 
 package :xz do
-  skip_packager false
-  compression_threads COMPRESSION_THREADS
-  compression_level 5
+  skip_packager true
 end
 
 # ------------------------------------
