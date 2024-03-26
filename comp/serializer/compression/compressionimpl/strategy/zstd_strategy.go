6fc514ce4eb6688ce6f7092c7567ca80c711f60d
Brian Floersch <brian.floersch@datadoghq.com>
2024-03-26 13:18:21 -0400

    remove strategy build tags

--- comp/serializer/compression/compressionimpl/strategy/zstd_strategy.go
+++ comp/serializer/compression/compressionimpl/strategy/zstd_strategy.go
@@ -3,8 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build zstd
-
 // Package strategy provides a set of functions for compressing with zlib / zstd
 package strategy
 
