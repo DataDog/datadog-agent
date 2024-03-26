22694abc12307750ef5ebe325455799e4ae14e76
Thibaud Cheruy <thibaud.cheruy@datadoghq.com>
2024-03-26 11:32:28 +0100

    Add mock Cisco SDWAN API server

--- pkg/collector/corechecks/network-devices/cisco-sdwan/client/request.go
+++ pkg/collector/corechecks/network-devices/cisco-sdwan/client/request.go
@@ -82,7 +82,7 @@ func (client *Client) get(endpoint string, params map[string]string) ([]byte, er
 		}
 	}
 
-	return nil, fmt.Errorf("http responsed with %d code", statusCode)
+	return nil, fmt.Errorf("%s http responsed with %d code", endpoint, statusCode)
 }
 
 // get wraps client.get with generic type content and unmarshalling (methods can't use generics)
