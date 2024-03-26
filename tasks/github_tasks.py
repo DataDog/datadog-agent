daafd125e46d24d971ad1d007dc40bd6f36451d3
Kylian Serrania <kylian.serrania@datadoghq.com>
2024-03-26 16:10:07 +0100

    [invoke] Add tasks to use CODEOWNERS files

--- tasks/github_tasks.py
+++ tasks/github_tasks.py
@@ -18,7 +18,7 @@ from tasks.libs.github_actions_tools import (
     trigger_macos_workflow,
 )
 from tasks.libs.junit_upload_core import repack_macos_junit_tar
-from tasks.libs.pipeline_notifications import read_owners
+from tasks.libs.owners.parsing import read_owners
 from tasks.release import _get_release_json_value
 
 
