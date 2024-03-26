bf6f5f707c0020de43cd264a2b6eb7f5ecb8b7c6
usamasaqib <usama.saqib@datadoghq.com>
2024-03-26 15:50:14 +0100

    fix out of bound memory access of map value

--- pkg/ebpf/c/bpf_telemetry.h
+++ pkg/ebpf/c/bpf_telemetry.h
@@ -21,24 +21,25 @@ BPF_ARRAY_MAP(bpf_instrumentation_map, instrumentation_blob_t, 1);
 #define STR(x) #x
 #define MK_MAP_INDX(key) STR(key##_telemetry_key)
 
-#define map_update_with_telemetry(fn, map, args...)                     \
-    ({                                                                  \
-        long errno_ret;                                                 \
-        errno_ret = fn(&map, args);                                     \
-        unsigned long map_index;                                        \
-        LOAD_CONSTANT(MK_MAP_INDX(map), map_index);                     \
-        if (errno_ret < 0 && map_index > 0){                            \
-            instrumentation_blob_t *tb = FETCH_TELEMETRY_BLOB();        \
-            if (tb) {                                                   \
-                long error = errno_ret * -1;                            \
-                if (error >= T_MAX_ERRNO) {                             \
-                    error = T_MAX_ERRNO - 1;                            \
-                }                                                       \
-                error &= (T_MAX_ERRNO - 1);                             \
-                __sync_fetch_and_add(&tb->map_err_telemetry[map_index].err_count[error], 1); \
-            }                                                           \
-        }                                                               \
-        errno_ret;                                                      \
+#define map_update_with_telemetry(fn, map, args...)                                 \
+    ({                                                                              \
+        long errno_ret;                                                             \
+        errno_ret = fn(&map, args);                                                 \
+        unsigned long map_index;                                                    \
+        LOAD_CONSTANT(MK_MAP_INDX(map), map_index);                                 \
+        if (errno_ret < 0 && map_index > 0){                                        \
+            instrumentation_blob_t *tb = FETCH_TELEMETRY_BLOB();                    \
+            if (tb) {                                                               \
+                long error = errno_ret * -1;                                        \
+                long offset = (sizeof(map_err_telemetry_t) * map_index) +  \
+                    (error * sizeof(unsigned long));                                \
+                if (offset < (sizeof(instrumentation_blob_t) - 8)) {                \
+                    void *target = (void *)tb + offset;                             \
+                    __sync_fetch_and_add((unsigned long *)target, 1);               \
+                }                                                                   \
+            }                                                                       \
+        }                                                                           \
+        errno_ret;                                                                  \
     })
 
 #define MK_FN_INDX(fn) FN_INDX_##fn
@@ -51,29 +52,25 @@ BPF_ARRAY_MAP(bpf_instrumentation_map, instrumentation_blob_t, 1);
 #define FN_INDX_bpf_skb_load_bytes skb_load_bytes
 #define FN_INDX_bpf_perf_event_output perf_event_output
 
-#define helper_with_telemetry(fn, ...)                                                          \
-    ({                                                                                          \
-        long errno_ret = fn(__VA_ARGS__);                                                       \
-        unsigned long telemetry_program_id;                                                     \
-        LOAD_CONSTANT("telemetry_program_id_key", telemetry_program_id);                        \
-        if (errno_ret < 0 && telemetry_program_id > 0) {                                        \
-            instrumentation_blob_t *tb = FETCH_TELEMETRY_BLOB();       \
-            if (tb) {                                                                        \
+#define helper_with_telemetry(fn, ...)                                                              \
+    ({                                                                                              \
+        long errno_ret = fn(__VA_ARGS__);                                                           \
+        unsigned long telemetry_program_id;                                                         \
+        LOAD_CONSTANT("telemetry_program_id_key", telemetry_program_id);                            \
+        if (errno_ret < 0 && telemetry_program_id > 0) {                                            \
+            instrumentation_blob_t *tb = FETCH_TELEMETRY_BLOB();                                    \
+            if (tb) {                                                                               \
                 int helper_indx = MK_FN_INDX(fn);                                                   \
-                long errno_slot = errno_ret * -1;                                                    \
-                if (errno_slot >= T_MAX_ERRNO) {                                                \
-                    errno_slot = T_MAX_ERRNO - 1;                                               \
-                    /* This is duplicated below because on clang 14.0.6 the compiler
-                     * concludes that this if-check will always force errno_slot in range
-                     * (0, T_MAX_ERRNO-1], and removes the bounds check, causing the verifier
-                     * to trip. Duplicating this check forces clang not to omit the check */    \
-                    errno_slot &= (T_MAX_ERRNO - 1);                                            \
-                }                                                                               \
-                errno_slot &= (T_MAX_ERRNO - 1);                                                \
-                __sync_fetch_and_add(&tb->helper_err_telemetry[telemetry_program_id].err_count[(helper_indx * T_MAX_ERRNO) + errno_slot], 1); \
-            }                                                                                   \
-        }                                                                                       \
-        errno_ret;                                                                              \
+                long errno_slot = errno_ret * -1;                                                   \
+                long offset = (sizeof(helper_err_telemetry_t) * telemetry_program_id) +             \
+                    (((helper_indx * T_MAX_ERRNO)+errno_slot)*sizeof(unsigned long));               \
+                if (offset < (sizeof(instrumentation_blob_t) - 8)) {                                \
+                    void *target = (void *)tb + offset;                                             \
+                    __sync_fetch_and_add((unsigned long *)target, 1);                               \
+                }                                                                                   \
+            }                                                                                       \
+        }                                                                                           \
+        errno_ret;                                                                                  \
     })
 // If instrumentation is not enabled do not waste instructions
 #else
