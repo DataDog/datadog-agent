bbcf34530e8590408a13aa25d1b136693361072e
Maxime Riaud <maxime.riaud@datadoghq.com>
2024-03-27 15:54:18 +0100

    creating autoexit agent component

---
+++ comp/agent/autoexit/component.go
@@ -0,0 +1,13 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+// Package autoexit lets setup automatic shutdown mechanism if necessary
+package autoexit
+
+// team: agent-shared-components
+
+// Component is the component type.
+type Component interface {
+}
