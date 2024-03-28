97db361b3aa62bbddb8b03344b53a106b2426e97
AliDatadog <ali.benabdallah@datadoghq.com>
2024-03-28 18:28:52 +0100

    add tests for config parsing

---
+++ pkg/collector/corechecks/sbom/check_test.go
@@ -0,0 +1,93 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022-present Datadog, Inc.
+
+//go:build test && trivy
+
+package sbom
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigParsing(t *testing.T) {
+	for _, tt := range []struct {
+		name     string
+		raw      string
+		expected Config
+	}{
+		{
+			name: "default values",
+			raw:  ``,
+			expected: Config{
+				ChunkSize:                       1,
+				NewSBOMMaxLatencySeconds:        30,
+				ContainerPeriodicRefreshSeconds: 3600,
+				HostPeriodicRefreshSeconds:      3600,
+				HostHeartbeatValiditySeconds:    3600 * 24,
+			},
+		},
+		{
+			name: "custom values",
+			raw: `
+chunk_size: 10
+new_sbom_max_latency_seconds: 120
+periodic_refresh_seconds: 3600
+host_periodic_refresh_seconds: 7200
+host_heartbeat_validity_seconds: 86400
+`,
+			expected: Config{
+				ChunkSize:                       10,
+				NewSBOMMaxLatencySeconds:        120,
+				ContainerPeriodicRefreshSeconds: 3600,
+				HostPeriodicRefreshSeconds:      7200,
+				HostHeartbeatValiditySeconds:    86400,
+			},
+		},
+		{
+			name: "invalid values",
+			raw: `
+chunk_size: -10
+new_sbom_max_latency_seconds: -120
+periodic_refresh_seconds: -3600
+host_periodic_refresh_seconds: -7200
+host_heartbeat_validity_seconds: -86400
+`,
+			expected: Config{
+				ChunkSize:                       1,
+				NewSBOMMaxLatencySeconds:        1,
+				ContainerPeriodicRefreshSeconds: 60,
+				HostPeriodicRefreshSeconds:      60,
+				HostHeartbeatValiditySeconds:    60,
+			},
+		},
+		{
+			name: "exceeding max values",
+			raw: `
+chunk_size: 1000
+new_sbom_max_latency_seconds: 10000
+periodic_refresh_seconds: 1000000
+host_periodic_refresh_seconds: 1000000
+host_heartbeat_validity_seconds: 1000000
+`,
+
+			expected: Config{
+				ChunkSize:                       100,
+				NewSBOMMaxLatencySeconds:        300,
+				ContainerPeriodicRefreshSeconds: 604800,
+				HostPeriodicRefreshSeconds:      604800,
+				HostHeartbeatValiditySeconds:    604800,
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			var got Config
+			err := got.Parse([]byte(tt.raw))
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
+}
