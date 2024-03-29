4f935ea3f68faf6510cb1a2f03c1acefab17991f
Duncan Harvey <duncan.harvey@datadoghq.com>
2024-03-29 18:59:30 -0400

    exclude extension and lambda layer logs for serverless integration tests

--- test/integration/serverless/log_normalize.py
+++ test/integration/serverless/log_normalize.py
@@ -43,6 +43,16 @@ def normalize_logs(stage, aws_account_id):
         # should be investigated
         "TIMESTAMP UTC | DD_EXTENSION | ERROR | could not forward the request context canceled",
         "TIMESTAMP http: proxy error: context canceled",
+        # Extension logs
+        "TIMESTAMP UTC | DD_EXTENSION | DEBUG |",
+        "TIMESTAMP UTC | DD_EXTENSION | INFO |",
+        "TIMESTAMP UTC | DD_EXTENSION | WARN |",
+        "[datadog-wrapper]",
+        # Layer logs
+        "[DEBUG]", # Python
+        "[DD_TRACE_DOTNET]", # .NET
+        "\"status\":\"debug\"", # Go, Node
+        "[dd.trace TIMESTAMP]" # Java
     )
 
     def rm_extra_items_key(log):
@@ -61,6 +71,7 @@ def normalize_logs(stage, aws_account_id):
         exclude(r'BEGINLOG'),
         exclude(r'ENDLOG'),
         replace(r'("timestamp":\s*?)\d{13}', r'\1"XXX"'),
+        replace(r'\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}:\d{3}\s\+\d{4}', 'TIMESTAMP'),
         replace(r'\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}:\d{3}', 'TIMESTAMP'),
         replace(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z', 'TIMESTAMP'),
         replace(r'\d{4}\/\d{2}\/\d{2}\s\d{2}:\d{2}:\d{2}', 'TIMESTAMP'),
@@ -100,6 +111,7 @@ def normalize_traces(stage, aws_account_id):
         exclude(r'"_dd.install.time":"[0-9]+",'),
         exclude(r'"_dd.install.type":"[a-zA-Z0-9_\-]+",'),
         exclude(r'"_dd.p.tid":"[0-9a-fA-F]+",'),
+        exclude(r'"_dd.tracer_hostname":"\d{1,3}(?:.\d{1,3}){3}"+,'),
         replace(r'(ts":)[0-9]{10}', r'\1XXX'),
         replace(r'((startTime|endTime|traceID|trace_id|span_id|parent_id|start|system.pid)":)[0-9]+', r'\1null'),
         replace(r'((tracer_version|language_version)":)["a-zA-Z0-9~\-\.\_]+', r'\1null'),
@@ -114,7 +126,6 @@ def normalize_traces(stage, aws_account_id):
         replace(r'("otel.trace_id":")[a-zA-Z0-9]+"', r'\1null"'),
         replace(r'("faas.execution":")[a-zA-Z0-9-]+"', r'\1null"'),
         replace(r'("faas.instance":")[a-zA-Z0-9-/]+\[\$LATEST\][a-zA-Z0-9]+"', r'\1null"'),
-        replace(r'("_dd.tracer_hostname":)"\d{1,3}(?:.\d{1,3}){3}"+', r'\1"<redacted>"'),
         replace(stage, 'XXXXXX'),
         replace(aws_account_id, '############'),
         exclude(r'[ ]$'),
