7d8f96c417eec1939c9711b8f3cc0b7cf7a56df8
Dinesh Gurumurthy <dinesh.gurumurthy@datadoghq.com>
2024-03-29 19:28:15 +0000

    revert un-needed changes

--- cmd/trace-agent/subcommands/params.go
+++ cmd/trace-agent/subcommands/params.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-// Package subcommands contains the subcommands of the otel-agent.
+// Package subcommands contains the subcommands of the trace-agent.
 package subcommands
 
 // GlobalParams contains the values of agent-global Cobra flags.
@@ -11,7 +11,6 @@ package subcommands
 // A pointer to this type is passed to SubcommandFactory's, but its contents
 // are not valid until Cobra calls the subcommand's Run or RunE function.
 type GlobalParams struct {
-	// ConfPath is OTel collector configuration file path
 	ConfPath   string
 	ConfigName string
 	LoggerName string
