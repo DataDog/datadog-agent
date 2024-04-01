b8f18202003d9aedf67ab9c9ae7608d8e81120d4
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-04-01 13:35:14 -0400

    add same launchers as core agent

--- comp/logs/agent/agentimpl/agent_serverless_init.go
+++ comp/logs/agent/agentimpl/agent_serverless_init.go
@@ -16,9 +16,15 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/logs/diagnostic"
 	"github.com/DataDog/datadog-agent/pkg/logs/launchers"
 	"github.com/DataDog/datadog-agent/pkg/logs/launchers/channel"
+	"github.com/DataDog/datadog-agent/pkg/logs/launchers/container"
+	filelauncher "github.com/DataDog/datadog-agent/pkg/logs/launchers/file"
+	"github.com/DataDog/datadog-agent/pkg/logs/launchers/journald"
+	"github.com/DataDog/datadog-agent/pkg/logs/launchers/listener"
+	"github.com/DataDog/datadog-agent/pkg/logs/launchers/windowsevent"
 	"github.com/DataDog/datadog-agent/pkg/logs/pipeline"
 	"github.com/DataDog/datadog-agent/pkg/logs/schedulers"
 	"github.com/DataDog/datadog-agent/pkg/status/health"
+	"time"
 	"github.com/DataDog/datadog-agent/pkg/util/optional"
 )
 
@@ -47,6 +53,16 @@ func (a *logAgent) SetupPipeline(
 	// setup the sole launcher for this agent
 	lnchrs := launchers.NewLaunchers(a.sources, pipelineProvider, a.auditor, a.tracker)
 	lnchrs.AddLauncher(channel.NewLauncher())
+	lnchrs.AddLauncher(filelauncher.NewLauncher(
+		a.config.GetInt("logs_config.open_files_limit"),
+		filelauncher.DefaultSleepDuration,
+		a.config.GetBool("logs_config.validate_pod_container_id"),
+		time.Duration(a.config.GetFloat64("logs_config.file_scan_period")*float64(time.Second)),
+		a.config.GetString("logs_config.file_wildcard_selection_mode"), a.flarecontroller))
+	lnchrs.AddLauncher(listener.NewLauncher(a.config.GetInt("logs_config.frame_size")))
+	lnchrs.AddLauncher(journald.NewLauncher(a.flarecontroller))
+	lnchrs.AddLauncher(windowsevent.NewLauncher())
+	lnchrs.AddLauncher(container.NewLauncher(a.sources))
 
 	a.schedulers = schedulers.NewSchedulers(a.sources, a.services)
 	a.destinationsCtx = destinationsCtx
