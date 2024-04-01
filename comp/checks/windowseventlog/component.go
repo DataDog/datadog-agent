483a0a5c0c9e5673e8b9cbd2908a41c46ac5e5a9
Branden Clark <branden.clark@datadoghq.com>
2024-04-01 11:23:41 -0400

    comp docs

--- comp/checks/windowseventlog/component.go
+++ comp/checks/windowseventlog/component.go
@@ -3,12 +3,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
-// Package windowseventlog ... /* TODO: detailed doc comment for the component */
+// Package windowseventlog defines the Windows Event Log check component
 package windowseventlog
 
 // team: windows-agent
 
 // Component is the component type.
 type Component interface {
-	/* TODO: define Component interface */
 }
--- comp/checks/windowseventlog/windowseventlogimpl/windows_event_log.go
+++ comp/checks/windowseventlog/windowseventlogimpl/windows_event_log.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
+// Package windowseventlogimpl provides the Windows Event Log check component
 package windowseventlogimpl
 
 import (
