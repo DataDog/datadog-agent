c7af8c3c2e940e1fc47c00e42b7dcc7908e45985
Maxime Riaud <maxime.riaud@datadoghq.com>
2024-04-02 15:14:25 +0200

    wrap old impl into comp

--- comp/agent/autoexit/autoexitimpl/autoexit.go
+++ comp/agent/autoexit/autoexitimpl/autoexit.go
@@ -9,12 +9,37 @@ package autoexitimpl
 import (
 	"go.uber.org/fx"
 
+	"github.com/DataDog/datadog-agent/cmd/manager"
+	"github.com/DataDog/datadog-agent/comp/agent/autoexit"
+	"github.com/DataDog/datadog-agent/comp/core/config"
+	pkgcommon "github.com/DataDog/datadog-agent/pkg/util/common"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
 // Module defines the fx options for this component.
 func Module() fxutil.Module {
 	return fxutil.Component(
-		fx.Provide( /* TODO: provide Component constructor */ ),
+		fx.Provide(newAutoExit),
 	)
+}
+
+type dependencies struct {
+	fx.In
+
+	Config config.Component
+	Lc     fx.Lifecycle
+}
+
+// ExitDetector is common interface for shutdown mechanisms
+type ExitDetector interface {
+	check() bool
+}
+
+// ConfigureAutoExit starts automatic shutdown mechanism if necessary
+func newAutoExit(deps dependencies) (autoexit.Component, error) {
+
+	ctx, _ := pkgcommon.GetMainCtxCancel()
+	err := manager.ConfigureAutoExit(ctx, deps.Config)
+
+	return struct{}{}, err
 }
