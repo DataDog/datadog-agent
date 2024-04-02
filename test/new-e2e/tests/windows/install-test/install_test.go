b4d09fb9730e658deaf331f8ce5c674c72f739d3
Branden Clark <branden.clark@datadoghq.com>
2024-04-02 17:50:56 -0400

    Add E2E tests for Windows Installer agent config related options (#23558)
    * add installopts test
    
    * switch expected/actual
    
    * label assertions
    
    * move config read into helper
    
    * add all/no subservices test
    
    * remove config after uninstall
    
    * check service status
    
    * add comment about system-probe
    
    * comment
    
    * add to CI
    
    * remove kitchen tests
    
    * fix ci run
    
    * try without forward slash
    
    * review comment, skip uninstall tests for config tests
    
    * split test into separate jobs
    
    * embed test-infra installparams
    
    * include embedded args
    
    * fix msi log file option

--- test/new-e2e/tests/windows/install-test/install_test.go
+++ test/new-e2e/tests/windows/install-test/install_test.go
@@ -10,14 +10,17 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/components"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
-
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner"
+	"github.com/DataDog/datadog-agent/test/new-e2e/tests/agent-platform/common"
+	boundport "github.com/DataDog/datadog-agent/test/new-e2e/tests/agent-platform/common/bound-port"
 	"github.com/DataDog/datadog-agent/test/new-e2e/tests/windows"
 	windowsCommon "github.com/DataDog/datadog-agent/test/new-e2e/tests/windows/common"
 	windowsAgent "github.com/DataDog/datadog-agent/test/new-e2e/tests/windows/common/agent"
@@ -25,6 +28,9 @@ import (
 	componentos "github.com/DataDog/test-infra-definitions/components/os"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
 
+	"gopkg.in/yaml.v3"
+
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"testing"
@@ -125,6 +131,58 @@ func (is *agentMSISuite) AfterTest(suiteName, testName string) {
 			}
 		}
 	}
+}
+
+// cleanupAgent fully removes the agent from the VM, MSI, config, agent user, etc,
+// to create a clean slate for the next test to run on the same host/stack.
+func (is *agentMSISuite) cleanupAgent() {
+	host := is.Env().RemoteHost
+	t := is.T()
+
+	// The uninstaller removes the registry keys that point to the install parameters,
+	// so we collect them prior to uninstalling and then use defer to perform the actions.
+
+	// remove the agent user
+	_, agentUserName, err := windowsAgent.GetAgentUserFromRegistry(host)
+	if err == nil {
+		defer func() {
+			t.Logf("Removing agent user: %s", agentUserName)
+			err = windowsCommon.RemoveLocalUser(host, agentUserName)
+			require.NoError(t, err)
+		}()
+	}
+
+	// remove the agent config
+	configDir, err := windowsAgent.GetConfigRootFromRegistry(host)
+	if err == nil {
+		defer func() {
+			t.Logf("Removing agent configuration files: %s", configDir)
+			err = host.RemoveAll(configDir)
+			require.NoError(t, err)
+		}()
+	}
+
+	// uninstall the agent
+	_, err = windowsAgent.GetDatadogAgentProductCode(host)
+	if err == nil {
+		defer func() {
+			t.Logf("Uninstalling Datadog Agent")
+			err = windowsAgent.UninstallAgent(host, filepath.Join(is.OutputDir, "uninstall.log"))
+			require.NoError(t, err)
+		}()
+	}
+}
+
+// cleanupOnSuccessInDevMode runs clean tasks on the host when running in DevMode. This makes it
+// easier to run subsequent tests on the same host without having to manually clean up.
+// This is not necessary outside of DevMode because the VM is destroyed after each test run.
+// Cleanup is not run if the test failed, to allow for manual inspection of the VM.
+func (is *agentMSISuite) cleanupOnSuccessInDevMode() {
+	if !is.IsDevMode() || is.T().Failed() {
+		return
+	}
+	is.T().Log("Running DevMode cleanup tasks")
+	is.cleanupAgent()
 }
 
 func (is *agentMSISuite) TestInstall() {
@@ -307,8 +365,226 @@ func (is *agentMSISuite) TestAgentUser() {
 	}
 }
 
+// TC-INS-003
+// tests that the installer options are set correctly in the agent config.
+// This test toes the line between testing the installer and the agent. The installer
+// already has unit-test coverage for the config replacement, so it is somewhat redundant to
+// test it here.
+// TODO: It would be better for the cmd_port binding test to be done by a regular Agent E2E test.
+func (is *agentMSISuite) TestInstallOpts() {
+	vm := is.Env().RemoteHost
+	is.prepareHost()
+
+	cmdPort := 4999
+
+	installOpts := []windowsAgent.InstallAgentOption{
+		windowsAgent.WithAPIKey("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
+		windowsAgent.WithPackage(is.AgentPackage),
+		windowsAgent.WithInstallLogFile(filepath.Join(is.OutputDir, "install.log")),
+		windowsAgent.WithTags("k1:v1,k2:v2"),
+		windowsAgent.WithHostname("win-installopts"),
+		windowsAgent.WithCmdPort(fmt.Sprintf("%d", cmdPort)),
+		windowsAgent.WithProxyHost("proxy.foo.com"),
+		windowsAgent.WithProxyPort("1234"),
+		windowsAgent.WithProxyUser("puser"),
+		windowsAgent.WithProxyPassword("ppass"),
+		windowsAgent.WithSite("eu"),
+		windowsAgent.WithDdURL("https://someurl.datadoghq.com"),
+		windowsAgent.WithLogsDdURL("https://logs.someurl.datadoghq.com"),
+		windowsAgent.WithProcessDdURL("https://process.someurl.datadoghq.com"),
+		windowsAgent.WithTraceDdURL("https://trace.someurl.datadoghq.com"),
+	}
+
+	_ = is.installAgentPackage(vm, is.AgentPackage, installOpts...)
+
+	// read the config file and check the options
+	confYaml, err := is.readAgentConfig(vm)
+	is.Require().NoError(err)
+
+	assert.Contains(is.T(), confYaml, "hostname")
+	assert.Equal(is.T(), "win-installopts", confYaml["hostname"], "hostname should match")
+	assert.Contains(is.T(), confYaml, "api_key")
+	assert.Equal(is.T(), "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", confYaml["api_key"], "api_key should match")
+	assert.Contains(is.T(), confYaml, "tags")
+	assert.ElementsMatch(is.T(), []string{"k1:v1", "k2:v2"}, confYaml["tags"], "tags should match")
+	assert.Contains(is.T(), confYaml, "cmd_port")
+	assert.Equal(is.T(), cmdPort, confYaml["cmd_port"], "cmd_port should match")
+	assert.Contains(is.T(), confYaml, "site")
+	assert.Equal(is.T(), "eu", confYaml["site"], "site should match")
+	assert.Contains(is.T(), confYaml, "dd_url")
+	assert.Equal(is.T(), "https://someurl.datadoghq.com", confYaml["dd_url"], "dd_url should match")
+
+	if assert.Contains(is.T(), confYaml, "proxy") {
+		// https proxy conf does use http:// in the URL
+		// https://docs.datadoghq.com/agent/configuration/proxy/?tab=windows
+		proxyConf := confYaml["proxy"].(map[string]interface{})
+		assert.Contains(is.T(), proxyConf, "https")
+		assert.Equal(is.T(), "http://puser:ppass@proxy.foo.com:1234/", proxyConf["https"], "https proxy should match")
+		assert.Contains(is.T(), proxyConf, "http")
+		assert.Equal(is.T(), "http://puser:ppass@proxy.foo.com:1234/", proxyConf["http"], "http proxy should match")
+	}
+
+	if assert.Contains(is.T(), confYaml, "logs_config") {
+		logsConf := confYaml["logs_config"].(map[string]interface{})
+		assert.Contains(is.T(), logsConf, "logs_dd_url")
+		assert.Equal(is.T(), "https://logs.someurl.datadoghq.com", logsConf["logs_dd_url"], "logs_dd_url should match")
+	}
+
+	if assert.Contains(is.T(), confYaml, "process_config") {
+		processConf := confYaml["process_config"].(map[string]interface{})
+		assert.Contains(is.T(), processConf, "process_dd_url")
+		assert.Equal(is.T(), "https://process.someurl.datadoghq.com", processConf["process_dd_url"], "process_dd_url should match")
+	}
+
+	if assert.Contains(is.T(), confYaml, "apm_config") {
+		apmConf := confYaml["apm_config"].(map[string]interface{})
+		assert.Contains(is.T(), apmConf, "apm_dd_url")
+		assert.Equal(is.T(), "https://trace.someurl.datadoghq.com", apmConf["apm_dd_url"], "apm_dd_url should match")
+	}
+
+	// check that agent is listening on the new bound port
+	var boundPort boundport.BoundPort
+	is.Require().EventuallyWithTf(func(c *assert.CollectT) {
+		pid, err := windowsCommon.GetServicePID(vm, "DatadogAgent")
+		if !assert.NoError(c, err) {
+			return
+		}
+		boundPort, err = common.GetBoundPort(vm, cmdPort)
+		if !assert.NoError(c, err) {
+			return
+		}
+		if !assert.NotNil(c, boundPort, "port %d should be bound", cmdPort) {
+			return
+		}
+		assert.Equalf(c, pid, boundPort.PID(), "port %d should be bound by the agent", cmdPort)
+	}, 1*time.Minute, 500*time.Millisecond, "port %d should be bound by the agent", cmdPort)
+	is.Require().EqualValues("127.0.0.1", boundPort.LocalAddress(), "agent should only be listening locally")
+
+	is.cleanupOnSuccessInDevMode()
+}
+
+// TestSubServicesOpts tests that the agent installer can configure the subservices.
+// TODO: Once E2E's Agent interface supports providing MSI installer options these tests
+// should be moved to regular Agent E2E tests for each subservice.
+func (is *agentMSISuite) TestSubServicesOpts() {
+	vm := is.Env().RemoteHost
+	is.prepareHost()
+
+	tcs := []struct {
+		testname string
+		// it's surprising but we do not have an installer option for enabling NPM/system-probe.
+		logsEnabled    bool
+		processEnabled bool
+		apmEnabled     bool
+	}{
+		// TC-INS-004
+		{"all-subservices", true, true, true},
+		// TC-INS-005
+		{"no-subservices", false, false, false},
+	}
+	for _, tc := range tcs {
+		if !is.Run(tc.testname, func() {
+
+			installOpts := []windowsAgent.InstallAgentOption{
+				windowsAgent.WithLogsEnabled(strconv.FormatBool(tc.logsEnabled)),
+				// set both process agent options so we can check if process-agent is running or not
+				windowsAgent.WithProcessEnabled(strconv.FormatBool(tc.processEnabled)),
+				windowsAgent.WithProcessDiscoveryEnabled(strconv.FormatBool(tc.processEnabled)),
+				windowsAgent.WithAPMEnabled(strconv.FormatBool(tc.apmEnabled)),
+			}
+			_ = is.installAgentPackage(vm, is.AgentPackage, installOpts...)
+
+			// read the config file and check the options
+			confYaml, err := is.readAgentConfig(vm)
+			is.Require().NoError(err)
+
+			assert.Contains(is.T(), confYaml, "logs_enabled", "logs_enabled should be present in the config")
+			assert.Equal(is.T(), tc.logsEnabled, confYaml["logs_enabled"], "logs_enabled should match")
+
+			if assert.Contains(is.T(), confYaml, "process_config", "process_config should be present in the config") {
+				processConf := confYaml["process_config"].(map[string]interface{})
+				if assert.Contains(is.T(), processConf, "process_collection", "process_collection should be present in process_config") {
+					processCollectionConf := processConf["process_collection"].(map[string]interface{})
+					assert.Contains(is.T(), processCollectionConf, "enabled", "enabled should be present in process_collection")
+					assert.Equal(is.T(), tc.processEnabled, processCollectionConf["enabled"], "process_collection enabled should match")
+				}
+				if assert.Contains(is.T(), processConf, "process_discovery", "process_discovery should be present in process_config") {
+					processDiscoveryConf := processConf["process_discovery"].(map[string]interface{})
+					assert.Contains(is.T(), processDiscoveryConf, "enabled", "enabled should be present in process_discovery")
+					assert.Equal(is.T(), tc.processEnabled, processDiscoveryConf["enabled"], "process_discovery enabled should match")
+				}
+			}
+
+			if assert.Contains(is.T(), confYaml, "apm_config", "apm_config should be present in the config") {
+				apmConf := confYaml["apm_config"].(map[string]interface{})
+				assert.Contains(is.T(), apmConf, "enabled", "enabled should be present in apm_config")
+				assert.Equal(is.T(), tc.apmEnabled, apmConf["enabled"], "apm_config enabled should match")
+			}
+
+			tcs := []struct {
+				serviceName string
+				enabled     bool
+			}{
+				// NOTE: Even with processEnabled=false the Agent will start process-agent because container_collection is
+				//       enabled by default. We do not have an installer option to control this process-agent setting.
+				//       However, process-agent will exit soon after starting because there's no container environment installed
+				//       and the other options are disabled.
+				{"datadog-process-agent", tc.processEnabled},
+				{"datadog-trace-agent", tc.apmEnabled},
+			}
+			for _, tc := range tcs {
+				assert.EventuallyWithT(is.T(), func(c *assert.CollectT) {
+					status, err := windowsCommon.GetServiceStatus(vm, tc.serviceName)
+					require.NoError(c, err)
+					if tc.enabled {
+						assert.Equal(c, "Running", status, "%s should be running", tc.serviceName)
+					} else {
+						assert.Equal(c, "Stopped", status, "%s should be stopped", tc.serviceName)
+					}
+				}, 1*time.Minute, 1*time.Second, "%s should be in the expected state", tc.serviceName)
+			}
+		}) {
+			is.T().FailNow()
+		}
+		// clean the host between test runs
+		is.cleanupOnSuccessInDevMode()
+	}
+}
+
+func (is *agentMSISuite) readYamlConfig(host *components.RemoteHost, path string) (map[string]any, error) {
+	config, err := host.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	confYaml := make(map[string]any)
+	err = yaml.Unmarshal(config, &confYaml)
+	if err != nil {
+		return nil, err
+	}
+	return confYaml, nil
+}
+
+func (is *agentMSISuite) readAgentConfig(host *components.RemoteHost) (map[string]any, error) {
+	confDir, err := windowsAgent.GetConfigRootFromRegistry(host)
+	if err != nil {
+		return nil, err
+	}
+	configFilePath := filepath.Join(confDir, "datadog.yaml")
+	return is.readYamlConfig(host, configFilePath)
+}
+
 func (is *agentMSISuite) uninstallAgentAndRunUninstallTests(t *Tester) bool {
 	return is.T().Run("uninstall the agent", func(tt *testing.T) {
+		// Get config dir from registry before uninstalling
+		configDir, err := windowsAgent.GetConfigRootFromRegistry(t.host)
+		require.NoError(tt, err)
+		tt.Cleanup(func() {
+			// remove the agent config for a cleaner uninstall
+			tt.Logf("Removing agent configuration files")
+			err = t.host.RemoveAll(configDir)
+			require.NoError(tt, err)
+		})
+
 		if !tt.Run("uninstall", func(tt *testing.T) {
 			err := windowsAgent.UninstallAgent(t.host, filepath.Join(is.OutputDir, "uninstall.log"))
 			require.NoError(tt, err, "should uninstall the agent")
