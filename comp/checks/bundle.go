78afef8aa2d663406eaf92f039864a4c58839a24
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2024-04-03 16:44:44 +0200

    chore: fix comp ownership (#24342)

--- comp/checks/bundle.go
+++ comp/checks/bundle.go
@@ -15,7 +15,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
-// team: agent-shared-components
+// team: agent-metrics-logs
 
 // Bundle defines the fx options for this bundle.
 func Bundle() fxutil.BundleOptions {
