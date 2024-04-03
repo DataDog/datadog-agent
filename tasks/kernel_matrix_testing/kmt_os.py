0dfd5e11da58a8a3b4fdf4390e62d122d4766be6
Usama Saqib <usamasaqib.96@live.com>
2024-04-03 16:28:16 +0200

    Fix nfs initialization for KMT on linux (#24335)
    * fix nfs init on linux
    
    * export NFS share for linux hosts
    
    * fix linter error

--- tasks/kernel_matrix_testing/kmt_os.py
+++ tasks/kernel_matrix_testing/kmt_os.py
@@ -66,6 +66,13 @@ class Linux:
         ctx.run(f"{sudo} sed --in-place 's/#user = \"root\"/user = \"{user}\"/' {Linux.qemu_conf}")
         ctx.run(f"{sudo} sed --in-place 's/#group = \"root\"/group = \"kvm\"/' {Linux.qemu_conf}")
 
+        with open("/etc/exports") as f:
+            if "/opt/kernel-version-testing 100.0.0.0/8(ro,no_root_squash,no_subtree_check)" not in f.read():
+                ctx.run(
+                    f"{sudo} sh -c 'echo \"/opt/kernel-version-testing 100.0.0.0/8(ro,no_root_squash,no_subtree_check)\" >> /etc/exports'"
+                )
+                ctx.run(f"{sudo} exportfs -a")
+
         Linux.restart_libvirtd(ctx, sudo)
 
 
