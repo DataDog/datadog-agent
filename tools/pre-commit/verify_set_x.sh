265661cd7d9377cab58dacb8ce53dbe181ed6bbf
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-04-04 11:52:07 +0200

    [precommit-setx] Updated regex and script

--- tools/pre-commit/verify_set_x.sh
+++ tools/pre-commit/verify_set_x.sh
@@ -6,7 +6,8 @@ files="$(git grep -rn --color=always 'set -.*x' -- ':*.sh' ':*/Dockerfile' ':*.y
 
 if [ -n "$files" ]; then
     echo "$files" >& 2
-    echo -e '\nerror: No script should include `set -x`' >& 2
+    echo >& 2
+    echo 'error: No script should include "set -x"' >& 2
     exit 1
 else
     exit 0
