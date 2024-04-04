26bd4940bca265f9fc96ed7910766da3a9a29fdb
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-04-04 11:50:29 +0200

    [precommit-setx] Updated regex

--- tools/pre-commit/verify_set_x.sh
+++ tools/pre-commit/verify_set_x.sh
@@ -2,7 +2,7 @@
 
 # Verify no set -x is used in this repository
 
-files="$(git grep -rn --color=always 'set -x' -- ':*.sh' ':*/Dockerfile' ':*.yaml' ":(exclude)$0" ':(exclude).pre-commit-config.yaml')"
+files="$(git grep -rn --color=always 'set -.*x' -- ':*.sh' ':*/Dockerfile' ':*.yaml' ":(exclude)$0" ':(exclude).pre-commit-config.yaml')"
 
 if [ -n "$files" ]; then
     echo "$files" >& 2
