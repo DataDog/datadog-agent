f5aea2ba29b10ef92a9fce52307b1eefcb7a6d94
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2024-04-05 19:10:41 +0200

    [CSPM] switch to NullAuditor in the event forwarder (#24402)
    * [CSPM] switch to NullAuditor in the event forwarder
    
    * remove unused config params

--- cmd/security-agent/subcommands/check/command.go
+++ cmd/security-agent/subcommands/check/command.go
@@ -220,7 +220,7 @@ func RunCheck(log log.Component, config config.Component, _ secrets.Component, s
 		}
 	}
 	if checkArgs.report {
-		if err := reportComplianceEvents(log, config, events); err != nil {
+		if err := reportComplianceEvents(log, events); err != nil {
 			log.Error(err)
 			return err
 		}
@@ -243,17 +243,16 @@ func dumpComplianceEvents(reportFile string, events []*compliance.CheckEvent) er
 	return nil
 }
 
-func reportComplianceEvents(log log.Component, config config.Component, events []*compliance.CheckEvent) error {
+func reportComplianceEvents(log log.Component, events []*compliance.CheckEvent) error {
 	hostnameDetected, err := utils.GetHostnameWithContextAndFallback(context.Background())
 	if err != nil {
 		return log.Errorf("Error while getting hostname, exiting: %v", err)
 	}
-	runPath := config.GetString("compliance_config.run_path")
 	endpoints, context, err := common.NewLogContextCompliance()
 	if err != nil {
 		return fmt.Errorf("reporter: could not reate log context for compliance: %w", err)
 	}
-	reporter := compliance.NewLogReporter(hostnameDetected, "compliance-agent", "compliance", runPath, endpoints, context)
+	reporter := compliance.NewLogReporter(hostnameDetected, "compliance-agent", "compliance", endpoints, context)
 	defer reporter.Stop()
 	for _, event := range events {
 		reporter.ReportEvent(event)
