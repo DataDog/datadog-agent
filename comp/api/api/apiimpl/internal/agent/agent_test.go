4f9d8fa34d9517988377f101e5daf7901e927751
Mister Yo (Maxime Riaud) <65339037+misteriaud@users.noreply.github.com>
2024-04-05 17:00:37 +0200

    [ASCII-1159] Migrate GUI to component implementation (#23791)
    * create gui comp boilerplate and move old gui implementation into it
    
    * update gui implementation to match the component architecture
    
    * update agent.build invoke task with the new gui assets location
    
    * update agent run subcommand to use gui component
    
    * update api component to use GUI comp
    
    * update check subcommand and jmx related files to use newly create GUI component
    
    * fix unused parameters errors
    
    * update apiMock struct to match api.Component interface
    
    * update gui unit tests to match new GUI implementation (eg: use of config comp)
    
    * update getConfigSetting_test: explicitly set tested config parameter to not rely on default config settings
    
    * Adding GUI to the Component README and CODEOWNERS file
    
    * fix forgotten error handling in gui component constructor
    
    * Providing None option GUI component instance to JMX
    
    * fix forgotten gui comp parameter for multiple jmx related functions
    
    * add documentation to the GUI component
    
    * fix typo agent-shared-componentS
    
    * use a cleaner way to provide the GUI component constructor
    
    * update Windows run command to use GUI component
    
    * update check subcommand to use GUI component, by providing an empty None instance of GUI
    
    * change a little bit implementation of GUI following Kaderinho's review
    
    * add gui to comp/api deps
    
    * revert to unlinked agentHandler and checkHandler function, passing deps by parameters
    
    * reorder gui implementation
    
    * remove gui instance from ExecJMXCommandConsole function
    
    * use fx.Supply rather than fx.Provide
    
    * conditionnaly register CSRF token endpoint depending on the existence of gui instance
    
    * update gui init and lifecycle hooks to not stop the application if the GUI fail
    
    * revert conditionnal registering of api endpoint due to change of behaviour in case of unexisting GUI
    
    * remove error return in the newGui function prototype
    
    * remove error return in newGui function prototype
    
    * fix apiimpl comp (missing gui parameter)

--- comp/api/api/apiimpl/internal/agent/agent_test.go
+++ comp/api/api/apiimpl/internal/agent/agent_test.go
@@ -14,6 +14,9 @@ import (
 	"testing"
 
 	// component dependencies
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer"
 	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/demultiplexerimpl"
 	"github.com/DataDog/datadog-agent/comp/collector/collector"
@@ -22,6 +25,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/comp/core/flare"
 	"github.com/DataDog/datadog-agent/comp/core/flare/flareimpl"
+	"github.com/DataDog/datadog-agent/comp/core/gui"
 	"github.com/DataDog/datadog-agent/comp/core/hostname/hostnameinterface"
 	"github.com/DataDog/datadog-agent/comp/core/log/logimpl"
 	"github.com/DataDog/datadog-agent/comp/core/secrets"
@@ -45,8 +49,6 @@ import (
 	"github.com/DataDog/datadog-agent/comp/metadata/inventoryhost/inventoryhostimpl"
 	"github.com/DataDog/datadog-agent/comp/metadata/packagesigning"
 	"github.com/DataDog/datadog-agent/comp/metadata/packagesigning/packagesigningimpl"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	// package dependencies
 	"github.com/DataDog/datadog-agent/pkg/aggregator"
@@ -78,6 +80,7 @@ type handlerdeps struct {
 	Collector             optional.Option[collector.Component]
 	EventPlatformReceiver eventplatformreceiver.Component
 	Ac                    autodiscovery.Mock
+	Gui                   optional.Option[gui.Component]
 }
 
 func getComponentDeps(t *testing.T) handlerdeps {
@@ -115,6 +118,7 @@ func getComponentDeps(t *testing.T) handlerdeps {
 			fx.Supply(autodiscoveryimpl.MockParams{Scheduler: nil}),
 			autodiscoveryimpl.MockModule(),
 		),
+		fx.Supply(optional.NewNoneOption[gui.Component]()),
 	)
 }
 
@@ -142,6 +146,7 @@ func setupRoutes(t *testing.T) *mux.Router {
 		deps.Collector,
 		deps.EventPlatformReceiver,
 		deps.Ac,
+		deps.Gui,
 	)
 
 	return router
--- comp/core/gui/guiimpl/platform_darwin.go
+++ comp/core/gui/guiimpl/platform_darwin.go
@@ -3,9 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build freebsd || netbsd || openbsd || solaris || dragonfly || linux
-
-package gui
+package guiimpl
 
 import (
 	"fmt"
