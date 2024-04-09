83bed83e954ab4fc2ed70c15e1594d7cbebba045
AliDatadog <ali.benabdallah@datadoghq.com>
2024-04-09 15:39:35 +0200

    add build flag for trivy

--- pkg/sbom/scanner/scanner_test.go
+++ pkg/sbom/scanner/scanner_test.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
-//go:build test
+//go:build test && trivy
 
 // Package scanner holds scanner related files
 package scanner
