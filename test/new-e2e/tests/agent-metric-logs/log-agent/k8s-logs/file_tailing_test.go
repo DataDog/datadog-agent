13cc8c1115b2507235a69b4ffaeef6b942e473cf
Lucas Liseth <lucas+github@lucasliseth.com>
2024-04-09 11:05:56 -0400

    Added long line test

--- test/new-e2e/tests/agent-metric-logs/log-agent/k8s-logs/file_tailing_test.go
+++ test/new-e2e/tests/agent-metric-logs/log-agent/k8s-logs/file_tailing_test.go
@@ -7,6 +7,8 @@ package k8sfiletailing
 
 import (
 	"context"
+	"io"
+	"os"
 	"slices"
 	"testing"
 	"time"
@@ -76,4 +78,56 @@ func (v *myKindSuite) TestSingleLog() {
 		}
 
 	}, 1*time.Minute, 10*time.Second)
+}
+
+func (v *myKindSuite) TestCorruptedLog() {
+	var backOffLimit int32 = 4
+	file, err := os.Open("long_line_log.txt")
+	assert.NoError(v.T(), err, "Could not open long line file.")
+	io.ReadAll(file)
+
+	b, err := io.ReadAll(file)
+
+	defer func() {
+		err := file.Close()
+		assert.NoError(v.T(), err, "Could not close file.")
+	}()
+
+	jobSpcec := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "long-line-job",
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:    "long-line-job",
+							Image:   "ubuntu",
+							Command: []string{"echo", string(b)},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyNever,
+				},
+			},
+			BackoffLimit: &backOffLimit,
+		},
+	}
+
+	_, err = v.Env().KubernetesCluster.Client().BatchV1().Jobs("default").Create(context.TODO(), jobSpcec, metav1.CreateOptions{})
+	assert.NoError(v.T(), err, "Could not properly start job")
+
+	v.EventuallyWithT(func(c *assert.CollectT) {
+		logsServiceNames, err := v.Env().FakeIntake.Client().GetLogServiceNames()
+		assert.NoError(c, err, "Error starting job")
+		assert.Contains(c, logsServiceNames, "ubuntu", "Ubuntu service not found")
+
+		if slices.Contains(logsServiceNames, "ubuntu") {
+			filteredLogs, err := v.Env().FakeIntake.Client().FilterLogs("ubuntu")
+			assert.NoError(c, err, "Error filtering logs")
+			assert.Equal(c, filteredLogs[0].Message, string(b), "Test log doesn't match")
+		}
+
+	}, 2*time.Minute, 10*time.Second)
 }
