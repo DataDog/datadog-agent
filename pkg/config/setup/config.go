87a9a884349d782bbfa4e6e1ac250ae656ef69e9
louis-cqrl <93274433+louis-cqrl@users.noreply.github.com>
2024-04-10 16:50:49 +0200

    Upgrade/rename setting 'flare_stripped_keys' (#24425)
    * Add scrubber.additional_keys parameter to config_template.yaml
    
    * fix linter
    
    * fix(pkg/config/logs/log.go): add all scrubbed element
    
    * Refactor config_template.yaml to use scrubber.additional_keys instead of flare_stripped_keys
    
    * fix release notes
    
    * Refactor log configuration and scrubber functions and add tests
    
    * Add ENV variable binding for flare_stripped_keys and scrubber.additional_keys
    
    * Add MergeAdditionalKeysToScrubber function description
    
    * revert changes
    
    * Refactor mergeAdditionalKeysToScrubber function and update test cases
    
    * Update config_template.yaml to include scrubbing setting more detail
    
    * Log message for flare_stripped_keys deprecated
    
    * Update pkg/config/config_template.yaml
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * Update releasenotes/notes/rename-flare_stripped_keys-setting-fcb082f6398759fb.yaml
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * Update pkg/config/config_template.yaml
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * Update pkg/config/config_template.yaml
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * Update pkg/config/config_template.yaml
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * Update config_template.yaml
    
    * Update releasenotes/notes/rename-flare_stripped_keys-setting-fcb082f6398759fb.yaml
    
    Co-authored-by: maxime mouial <hush-hush@users.noreply.github.com>
    
    * Update scrubber definition in config_template.yaml and remove unnecessary code in log.go
    
    * Fix bug in mergeAdditionalKeysToScrubber function
    
    * Refactor log configuration and test code***
    
    ***Add stripped keys to scrubber***
    
    ***Rename flare_stripped_keys setting
    
    * fix test for the logger
    
    * Update dependencies in go.mod file
    
    * Update dependencies in go.mod file
    
    * Update module dependencies and remove unused imports
    
    * Refactor test assertion in log_test.go
    
    * revert file useless change
    
    * Fix indent
    
    ---------
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    Co-authored-by: maxime mouial <hush-hush@users.noreply.github.com>

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -382,6 +382,7 @@ func InitConfig(config pkgconfigmodel.Config) {
 
 	// Yaml keys which values are stripped from flare
 	config.BindEnvAndSetDefault("flare_stripped_keys", []string{})
+	config.BindEnvAndSetDefault("scrubber.additional_keys", []string{})
 
 	// Agent GUI access port
 	config.BindEnvAndSetDefault("GUI_port", defaultGuiPort)
