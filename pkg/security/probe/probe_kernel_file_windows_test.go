0913d862f15062a5921664422a7f0753d36123de
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-04-10 17:14:46 +0200

    [CWS] fix usage of `assert.Eventually` with selects in windows tests

--- pkg/security/probe/probe_kernel_file_windows_test.go
+++ pkg/security/probe/probe_kernel_file_windows_test.go
@@ -165,8 +165,9 @@ func testSimpleCreate(t *testing.T, et *etwTester, testfilename string) {
 		select {
 		case <-et.loopExited:
 			return true
+		default:
+			return false
 		}
-		return false
 	}, 4*time.Second, 250*time.Millisecond, "did not get notification")
 
 	stopLoop(et, &wg)
@@ -234,8 +235,9 @@ func testFileOpen(t *testing.T, et *etwTester, testfilename string) {
 		select {
 		case <-et.loopExited:
 			return true
+		default:
+			return false
 		}
-		return false
 	}, 4*time.Second, 250*time.Millisecond, "did not get notification")
 
 	stopLoop(et, &wg)
