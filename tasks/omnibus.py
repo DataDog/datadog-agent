097c1de5f82dbfdb63e96295039ca8aa4cc3cd9e
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-04-10 16:44:47 +0200

    move non-task functions to tasks.common.libs

--- tasks/omnibus.py
+++ tasks/omnibus.py
@@ -1,13 +1,11 @@
-import json
 import os
 import sys
-from datetime import datetime
 
-import requests
 from invoke import task
 
 from tasks.flavor import AgentFlavor
 from tasks.go import deps
+from tasks.libs.common.omnibus import send_build_metrics, should_retry_bundle_install
 from tasks.libs.common.utils import get_version, load_release_versions, timed
 from tasks.ssm import get_pfx_pass, get_signing_cert
 
@@ -64,17 +62,9 @@ def bundle_install_omnibus(ctx, gem_path=None, env=None, max_try=2):
             res = ctx.run(cmd, env=env, warn=True)
             if res.ok:
                 return
-            if not _should_retry_bundle_install(res):
+            if not should_retry_bundle_install(res):
                 return
             print(f"Retrying bundle install, attempt {trial + 1}/{max_try}")
-
-
-def _should_retry_bundle_install(res):
-    # We sometimes get a Net::HTTPNotFound error when fetching the
-    # license-scout gem. This is a transient error, so we retry the bundle install
-    if "Net::HTTPNotFound:" in res.stderr:
-        return True
-    return False
 
 
 def get_omnibus_env(
@@ -149,104 +139,6 @@ def get_omnibus_env(
         env['PACKAGE_ARCH'] = os.environ.get('PACKAGE_ARCH')
 
     return env
-
-
-def _send_build_metrics(ctx, overall_duration):
-    # We only want to generate those metrics from the CI
-    if sys.platform == 'win32':
-        src_dir = "C:/buildroot/datadog-agent"
-        aws_cmd = "aws.cmd"
-    else:
-        src_dir = os.environ.get('CI_PROJECT_DIR')
-        aws_cmd = "aws"
-    job_name = os.environ.get('CI_JOB_NAME_SLUG')
-    branch = os.environ.get('CI_COMMIT_REF_NAME')
-    pipeline_id = os.environ.get('CI_PIPELINE_ID')
-    if not job_name or not branch or not src_dir or not pipeline_id:
-        print(
-            '''Missing required environment variables, this is probably not a CI job.
-                  skipping sending build metrics'''
-        )
-        return
-
-    series = []
-    timestamp = int(datetime.now().timestamp())
-    with open(f'{src_dir}/omnibus/pkg/build-summary.json') as summary_json:
-        j = json.load(summary_json)
-        # Various software build durations are all sent as the `datadog.agent.build.duration` metric
-        # with a specific tag for each software.
-        for software, metrics in j['build'].items():
-            series.append(
-                {
-                    'metric': 'datadog.agent.build.duration',
-                    'points': [{'timestamp': timestamp, 'value': metrics['build_duration']}],
-                    'tags': [
-                        f'software:{software}',
-                        f'cached:{metrics["cached"]}',
-                        f'job:{job_name}',
-                        f'branch:{branch}',
-                        f'pipeline:{pipeline_id}',
-                    ],
-                    'unit': 'seconds',
-                    'type': 0,
-                }
-            )
-        # We also provide the total duration for the omnibus build as a separate metric
-        series.append(
-            {
-                'metric': 'datadog.agent.build.total',
-                'points': [{'timestamp': timestamp, 'value': overall_duration}],
-                'tags': [
-                    f'job:{job_name}',
-                    f'branch:{branch}',
-                    f'pipeline:{pipeline_id}',
-                ],
-                'unit': 'seconds',
-                'type': 0,
-            }
-        )
-        # Stripping might not always be enabled so we conditionally read the metric
-        if "strip" in j:
-            series.append(
-                {
-                    'metric': 'datadog.agent.build.strip',
-                    'points': [{'timestamp': timestamp, 'value': j['strip']}],
-                    'tags': [
-                        f'job:{job_name}',
-                        f'branch:{branch}',
-                        f'pipeline:{pipeline_id}',
-                    ],
-                    'unit': 'seconds',
-                    'type': 0,
-                }
-            )
-        # And all packagers duration as another separated metric
-        for packager, duration in j['packaging'].items():
-            series.append(
-                {
-                    'metric': 'datadog.agent.package.duration',
-                    'points': [{'timestamp': timestamp, 'value': duration}],
-                    'tags': [
-                        f'job:{job_name}',
-                        f'branch:{branch}',
-                        f'packager:{packager}',
-                        f'pipeline:{pipeline_id}',
-                    ],
-                    'unit': 'seconds',
-                    'type': 0,
-                }
-            )
-    dd_api_key = ctx.run(
-        f'{aws_cmd} ssm get-parameter --region us-east-1 --name {os.environ["API_KEY_ORG2_SSM_NAME"]} --with-decryption --query "Parameter.Value" --out text',
-        hide=True,
-    ).stdout.strip()
-    headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'DD-API-KEY': dd_api_key}
-    r = requests.post("https://api.datadoghq.com/api/v2/series", json={'series': series}, headers=headers)
-    if r.ok:
-        print('Successfully sent build metrics to DataDog')
-    else:
-        print(f'Failed to send build metrics to DataDog: {r.status_code}')
-        print(r.text)
 
 
 # hardened-runtime needs to be set to False to build on MacOS < 10.13.6, as the -o runtime option is not supported.
@@ -347,7 +239,7 @@ def build(
         print(f"Deps:    {deps_elapsed.duration}")
     print(f"Bundle:  {bundle_elapsed.duration}")
     print(f"Omnibus: {omnibus_elapsed.duration}")
-    _send_build_metrics(ctx, omnibus_elapsed.duration)
+    send_build_metrics(ctx, omnibus_elapsed.duration)
 
 
 @task
