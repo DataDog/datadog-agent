96ecd4e5c5a04fe8a96444c6aba347c87af4cf41
Florent Clarret <florent.clarret@datadoghq.com>
2024-04-11 16:01:14 +0200

    address

--- tasks/devcontainer.py
+++ tasks/devcontainer.py
@@ -15,53 +15,9 @@ from libs.common.color import color_message
 from tasks.build_tags import build_tags, filter_incompatible_tags, get_build_tags, get_default_build_tags
 from tasks.flavor import AgentFlavor
 
-VSCODE_DIR = ".vscode"
-VSCODE_FILE = "settings.json"
 DEVCONTAINER_DIR = ".devcontainer"
 DEVCONTAINER_FILE = "devcontainer.json"
 DEVCONTAINER_NAME = "datadog_agent_devcontainer"
-
-
-@task
-def set_buildtags(
-    _,
-    target="agent",
-    build_include=None,
-    build_exclude=None,
-    flavor=AgentFlavor.base.name,
-    arch='x64',
-):
-    """
-    Modifies devcontainer settings file for this project to include correct build tags
-    """
-    flavor = AgentFlavor[flavor]
-
-    if target not in build_tags[flavor]:
-        print("Must choose a valid target.  Valid targets are: \n")
-        print(f'{", ".join(build_tags[flavor].keys())} \n')
-        return
-
-    build_include = (
-        get_default_build_tags(build=target, arch=arch, flavor=flavor)
-        if build_include is None
-        else filter_incompatible_tags(build_include.split(","), arch=arch)
-    )
-    build_exclude = [] if build_exclude is None else build_exclude.split(",")
-    use_tags = get_build_tags(build_include, build_exclude)
-
-    if not os.path.exists(VSCODE_DIR):
-        os.makedirs(VSCODE_DIR)
-
-    settings = {}
-    fullpath = os.path.join(VSCODE_DIR, VSCODE_FILE)
-    if os.path.exists(fullpath):
-        with open(fullpath, "r") as sf:
-            settings = json.load(sf, object_pairs_hook=OrderedDict)
-
-    settings["go.buildTags"] = ",".join(use_tags)
-
-    with open(fullpath, "w") as sf:
-        json.dump(settings, sf, indent=4, sort_keys=False, separators=(',', ': '))
 
 
 @task
--- tasks/vscode.py
+++ tasks/vscode.py
@@ -3,10 +3,18 @@ vscode namespaced tags
 
 Helpers for getting vscode set up nicely
 """
+import json
+import os
+from typing import OrderedDict
+
 from invoke import task
 from libs.common.color import color_message
 
+from tasks.build_tags import build_tags, filter_incompatible_tags, get_build_tags, get_default_build_tags
 from tasks.flavor import AgentFlavor
+
+VSCODE_DIR = ".vscode"
+VSCODE_FILE = "settings.json"
 
 
 @task
@@ -21,18 +29,34 @@ def set_buildtags(
     """
     Modifies vscode settings file for this project to include correct build tags
     """
-    from tasks.devcontainer import set_buildtags
+    flavor = AgentFlavor[flavor]
 
-    print(color_message('This command is deprecated, please use `devcontainer.set_buildtags` instead', "orange"))
-    print("Running `devcontainer.set_buildtags`...")
-    set_buildtags(
-        _,
-        target=target,
-        build_include=build_include,
-        build_exclude=build_exclude,
-        flavor=flavor,
-        arch=arch,
+    if target not in build_tags[flavor]:
+        print("Must choose a valid target.  Valid targets are: \n")
+        print(f'{", ".join(build_tags[flavor].keys())} \n')
+        return
+
+    build_include = (
+        get_default_build_tags(build=target, arch=arch, flavor=flavor)
+        if build_include is None
+        else filter_incompatible_tags(build_include.split(","), arch=arch)
     )
+    build_exclude = [] if build_exclude is None else build_exclude.split(",")
+    use_tags = get_build_tags(build_include, build_exclude)
+
+    if not os.path.exists(VSCODE_DIR):
+        os.makedirs(VSCODE_DIR)
+
+    settings = {}
+    fullpath = os.path.join(VSCODE_DIR, VSCODE_FILE)
+    if os.path.exists(fullpath):
+        with open(fullpath, "r") as sf:
+            settings = json.load(sf, object_pairs_hook=OrderedDict)
+
+    settings["go.buildTags"] = ",".join(use_tags)
+
+    with open(fullpath, "w") as sf:
+        json.dump(settings, sf, indent=4, sort_keys=False, separators=(',', ': '))
 
 
 @task
