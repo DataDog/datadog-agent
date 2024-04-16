a60ac718597de46a09adde3955b1aa43bdbc3c14
Lucas Liseth <lucas+github@lucasliseth.com>
2024-04-16 13:33:22 -0400

    Add test to ensure non-annotated pod isn't detected

--- test/new-e2e/tests/agent-metric-logs/log-agent/k8s-logs/file_tailing_cca_off_test.go
+++ test/new-e2e/tests/agent-metric-logs/log-agent/k8s-logs/file_tailing_cca_off_test.go
@@ -6,7 +6,7 @@ package k8sfiletailing
 
 import (
 	"context"
-	"fmt"
+	"slices"
 	"testing"
 	"time"
 
@@ -29,17 +29,55 @@ type myKindSuite2 struct {
 
 func TestMyKindSuite2(t *testing.T) {
 	helmValues := `
-    datadog:
-      kubelet:
-        tlsVerify: false
-      clusterName: "random-cluster-name"
-      envDict:
-        DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL: "true"
-    agents:
-      useHostNetwork: true
-      `
+datadog:
+  envDict:
+    DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL: "false"
+  kubelet:
+    tlsVerify: false
+  clusterName: "random-cluster-name"
+  agents:
+    useHostNetwork: true
+`
 
 	e2e.Run(t, &myKindSuite2{}, e2e.WithProvisioner(customkind.Provisioner(customkind.WithAgentOptions(kubernetesagentparams.WithHelmValues(helmValues)))))
+}
+
+func (v *myKindSuite2) TestCCAOff() {
+	v.Env().FakeIntake.Client().FlushServerAndResetAggregators()
+	var backOffLimit int32 = 4
+	testLogMessage := "Test pod"
+
+	jobSpcec := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cca-off-job",
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:    "cca-off-job",
+							Image:   "ubuntu",
+							Command: []string{"echo", testLogMessage},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyNever,
+				},
+			},
+			BackoffLimit: &backOffLimit,
+		},
+	}
+
+	_, err := v.Env().KubernetesCluster.Client().BatchV1().Jobs("default").Create(context.TODO(), jobSpcec, metav1.CreateOptions{})
+	assert.NoError(v.T(), err, "Could not start job")
+
+	v.EventuallyWithT(func(c *assert.CollectT) {
+		logsServiceNames, err := v.Env().FakeIntake.Client().GetLogServiceNames()
+		assert.NoError(c, err, "Error starting job")
+		assert.NotContains(c, logsServiceNames, "ubuntu", "Ubuntu service found with autodiscovery off")
+	}, 1*time.Minute, 10*time.Second)
+
 }
 
 func (v *myKindSuite2) TestADAnnotations() {
@@ -51,12 +89,14 @@ func (v *myKindSuite2) TestADAnnotations() {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "annotations-job",
 			Namespace: "default",
-			Annotations: map[string]string{
-				"ad.datadoghq.com/annotations-job.logs": "[{\"source\": \"test-container\"}]",
-			},
 		},
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"ad.datadoghq.com/annotations-job.logs": "[{\"source\": \"test-container\"}]",
+					},
+				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
@@ -77,8 +117,13 @@ func (v *myKindSuite2) TestADAnnotations() {
 
 	v.EventuallyWithT(func(c *assert.CollectT) {
 		logsServiceNames, err := v.Env().FakeIntake.Client().GetLogServiceNames()
-		fmt.Println(logsServiceNames)
 		assert.NoError(c, err, "Error starting job")
 		assert.Contains(c, logsServiceNames, "ubuntu", "Ubuntu service not found")
+
+		if slices.Contains(logsServiceNames, "ubuntu") {
+			filteredLogs, err := v.Env().FakeIntake.Client().FilterLogs("ubuntu")
+			assert.NoError(c, err, "Error filtering logs")
+			assert.Equal(c, testLogMessage, filteredLogs[0].Message, "Test log doesn't match")
+		}
 	}, 1*time.Minute, 10*time.Second)
 }
