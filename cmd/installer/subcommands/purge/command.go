85b00935524940545c52a420e24ec60fe2656419
paullegranddc <82819397+paullegranddc@users.noreply.github.com>
2024-04-17 23:04:10 +0200

    [installer] fix purge logging and telemetry (#24782)
    * * Inject logger and telemetry client in purge command
    * Propagate context and errors trhough purge command
    
    * fix tests

--- cmd/installer/subcommands/purge/command.go
+++ cmd/installer/subcommands/purge/command.go
@@ -7,12 +7,21 @@
 package purge
 
 import (
+	"context"
+
 	"github.com/spf13/cobra"
 	"go.uber.org/fx"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 
 	"github.com/DataDog/datadog-agent/cmd/installer/command"
 	"github.com/DataDog/datadog-agent/comp/core"
+	"github.com/DataDog/datadog-agent/comp/core/config"
+	"github.com/DataDog/datadog-agent/comp/core/log"
 	"github.com/DataDog/datadog-agent/comp/core/log/logimpl"
+	"github.com/DataDog/datadog-agent/comp/core/secrets"
+	"github.com/DataDog/datadog-agent/comp/core/sysprobeconfig/sysprobeconfigimpl"
+	"github.com/DataDog/datadog-agent/comp/updater/telemetry"
+	"github.com/DataDog/datadog-agent/comp/updater/telemetry/telemetryimpl"
 	"github.com/DataDog/datadog-agent/pkg/installer"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
@@ -30,7 +39,8 @@ func Commands(global *command.GlobalParams) []*cobra.Command {
 		Short: "Purge installer packages",
 		Long:  ``,
 		RunE: func(_ *cobra.Command, _ []string) error {
-			return purgeFxWrapper(&cliParams{
+			ctx := context.Background()
+			return purgeFxWrapper(ctx, &cliParams{
 				GlobalParams: *global,
 				pkg:          pkg,
 			})
@@ -40,21 +50,29 @@ func Commands(global *command.GlobalParams) []*cobra.Command {
 	return []*cobra.Command{purgeCmd}
 }
 
-func purgeFxWrapper(params *cliParams) error {
+func purgeFxWrapper(ctx context.Context, params *cliParams) error {
 	return fxutil.OneShot(purge,
+		fx.Provide(func() context.Context { return ctx }),
 		fx.Supply(params),
 		fx.Supply(core.BundleParams{
-			LogParams: logimpl.ForOneShot("INSTALLER", "info", true),
+			ConfigParams:         config.NewAgentParams(params.GlobalParams.ConfFilePath),
+			SecretParams:         secrets.NewEnabledParams(),
+			SysprobeConfigParams: sysprobeconfigimpl.NewParams(),
+			LogParams:            logimpl.ForOneShot("INSTALLER", "info", true),
 		}),
 		core.Bundle(),
+		telemetryimpl.Module(),
 	)
 }
 
-func purge(params *cliParams) error {
+func purge(ctx context.Context, params *cliParams, _ log.Component, _ telemetry.Component) (err error) {
+	span, ctx := tracer.StartSpanFromContext(ctx, "cmd/purge")
+	defer func() { span.Finish(tracer.WithError(err)) }()
+
+	span.SetTag("params.pkg", params.pkg)
+
 	if params.pkg != "" {
-		installer.PurgePackage(params.pkg)
-	} else {
-		installer.Purge()
+		return installer.PurgePackage(ctx, params.pkg)
 	}
-	return nil
+	return installer.Purge(ctx)
 }
