53362c6529a62c9d60b46e3bdd827b26e68f7b4a
Florent Clarret <florent.clarret@datadoghq.com>
2024-04-17 10:32:51 +0200

    future

--- rtloader/demo/main.py
+++ rtloader/demo/main.py
@@ -2,6 +2,8 @@
 # under the Apache License Version 2.0.
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2019-present Datadog, Inc.
+from __future__ import print_function  # fmt: off
+
 
 import aggregator
 import tagger
