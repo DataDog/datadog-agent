1b8e0884ce2acef0600e0528518af1b593318496
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-04-19 14:01:56 +0200

    update tests

--- tasks/unit-tests/pipeline_tests.py
+++ tasks/unit-tests/pipeline_tests.py
@@ -3,7 +3,7 @@ import unittest
 
 import yaml
 
-from tasks.libs.ciproviders.gitlab_api import update_gitlab_config
+from tasks.libs.ciproviders.gitlab_api import update_gitlab_config, ReferenceTag
 from tasks.libs.ciproviders.circleci import update_circleci_config
 
 
@@ -15,16 +15,51 @@ class TestUpdateGitlabCI(unittest.TestCase):
         subprocess.run(f"git checkout -- {self.gitlabci_file} {self.erroneous_file}".split())
         return super().tearDown()
 
-    def test_nominal(self):
-        update_gitlab_config(self.gitlabci_file, "1mageV3rsi0n", test_version=True)
+    def test_all_images(self):
+        modified = update_gitlab_config(self.gitlabci_file, "1mageV3rsi0n", test_version=True)
+        yaml.SafeLoader.add_constructor(ReferenceTag.yaml_tag, ReferenceTag.from_yaml)
         with open(self.gitlabci_file) as gl:
             gitlab_ci = yaml.safe_load(gl)
+        self.assertEqual(13, sum(1 for _ in modified))
+        self.assertEqual(13, sum(1 for v in gitlab_ci["variables"].values() if v == "\"_test_only\""))
+        self.assertEqual(13, sum(1 for v in gitlab_ci["variables"].values() if v == "ImageV3rsi0n"))
+        self.assertEqual(
+            0, sum(1 for k, v in gitlab_ci["variables"].items() if k.endswith("SUFFIX") and v == "_test_only")
+        )
+
+    def test_one_image(self):
+        modified = update_gitlab_config(self.gitlabci_file, "1mageV3rsi0n", "deb_x64")
+        yaml.SafeLoader.add_constructor(ReferenceTag.yaml_tag, ReferenceTag.from_yaml)
+        with open(self.gitlabci_file) as gl:
+            gitlab_ci = yaml.safe_load(gl)
+        self.assertEqual(13, sum(1 for _ in modified))
+        for variable, value in gitlab_ci["variables"].items():
+            if variable.endswith("_SUFFIX"):
+                self.assertEqual("_test_only", value)
+
+    def test_several_images(self):
+        modified = update_gitlab_config(self.gitlabci_file, "1mageV3rsi0n", "deb,rpm,suse")
+        yaml.SafeLoader.add_constructor(ReferenceTag.yaml_tag, ReferenceTag.from_yaml)
+        with open(self.gitlabci_file) as gl:
+            gitlab_ci = yaml.safe_load(gl)
+        self.assertEqual(13, sum(1 for _ in modified))
+        for variable, value in gitlab_ci["variables"].items():
+            if variable.endswith("_SUFFIX"):
+                self.assertEqual("_test_only", value)
+
+    def test_multimatch(self):
+        modified = update_gitlab_config(self.gitlabci_file, "1mageV3rsi0n", "x64")
+        yaml.SafeLoader.add_constructor(ReferenceTag.yaml_tag, ReferenceTag.from_yaml)
+        with open(self.gitlabci_file) as gl:
+            gitlab_ci = yaml.safe_load(gl)
+        self.assertEqual(13, sum(1 for _ in modified))
         for variable, value in gitlab_ci["variables"].items():
             if variable.endswith("_SUFFIX"):
                 self.assertEqual("_test_only", value)
 
     def test_update_no_test(self):
-        update_gitlab_config(self.gitlabci_file, "1mageV3rsi0n", test_version=False)
+        update_gitlab_config(self.gitlabci_file, "1mageV3rsi0n")
+        yaml.SafeLoader.add_constructor(ReferenceTag.yaml_tag, ReferenceTag.from_yaml)
         with open(self.gitlabci_file) as gl:
             gitlab_ci = yaml.safe_load(gl)
         for variable, value in gitlab_ci["variables"].items():
