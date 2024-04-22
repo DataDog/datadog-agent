11683e279d52335090028a2c708e0bbfb0868c46
Guy Arbitman <guy20495@gmail.com>
2024-04-22 09:26:59 +0300

    usm: Simplify *_allow_packet functions
    Removed redundant check for TCP, as it is being done in the dispacther beforehand.
    Used is_payload_empty method to determine if a packet is empty, instead of wrongly
    using skb->len.

--- pkg/network/ebpf/c/protocols/kafka/kafka-parsing.h
+++ pkg/network/ebpf/c/protocols/kafka/kafka-parsing.h
@@ -8,7 +8,7 @@
 #include "protocols/kafka/usm-events.h"
 
 // forward declaration
-static __always_inline bool kafka_allow_packet(kafka_transaction_t *kafka, struct __sk_buff* skb, skb_info_t *skb_info);
+static __always_inline bool kafka_allow_packet(skb_info_t *skb_info);
 static __always_inline bool kafka_process(kafka_transaction_t *kafka_transaction, struct __sk_buff* skb, __u32 offset);
 
 // A template for verifying a given buffer is composed of the characters [a-z], [A-Z], [0-9], ".", "_", or "-".
@@ -45,7 +45,7 @@ int socket__kafka_filter(struct __sk_buff* skb) {
         return 0;
     }
 
-    if (!kafka_allow_packet(kafka, skb, &skb_info)) {
+    if (!kafka_allow_packet(&skb_info)) {
         return 0;
     }
     normalize_tuple(&kafka->tup);
@@ -179,17 +179,10 @@ static __always_inline bool kafka_process(kafka_transaction_t *kafka_transaction
 
 // this function is called by the socket-filter program to decide whether or not we should inspect
 // the contents of a certain packet, in order to avoid the cost of processing packets that are not
-// of interest such as empty ACKs, UDP data or encrypted traffic.
-static __always_inline bool kafka_allow_packet(kafka_transaction_t *kafka, struct __sk_buff* skb, skb_info_t *skb_info) {
-    // we're only interested in TCP traffic
-    if (!(kafka->tup.metadata&CONN_TYPE_TCP)) {
-        return false;
-    }
-
-    // if payload data is empty or if this is an encrypted packet, we only
-    // process it if the packet represents a TCP termination
-    bool empty_payload = skb_info->data_off == skb->len;
-    if (empty_payload) {
+// of interest such as empty ACKs, or encrypted traffic.
+static __always_inline bool kafka_allow_packet(skb_info_t *skb_info) {
+    // if payload data is empty, we only process it if the packet represents a TCP termination
+    if (is_payload_empty(skb_info)) {
         return skb_info->tcp_flags&(TCPHDR_FIN|TCPHDR_RST);
     }
 
