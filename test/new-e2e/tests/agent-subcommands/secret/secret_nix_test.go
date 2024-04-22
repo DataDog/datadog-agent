9b70cd0790c814265de56f884d04a5cf8c05d2e7
Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>
2024-04-22 15:19:28 +0200

    test: add e2e tests for config-refresh on *nix
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>

--- test/new-e2e/tests/agent-subcommands/secret/secret_nix_test.go
+++ test/new-e2e/tests/agent-subcommands/secret/secret_nix_test.go
@@ -7,14 +7,17 @@ package secret
 
 import (
 	"testing"
+	"time"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e/client/agentclient"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/secrets"
 
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type linuxSecretSuite struct {
@@ -73,4 +76,38 @@ host_aliases:
 	assert.Contains(v.T(), output, "- 'alias_secret':\n\tused in 'datadog.yaml' configuration in entry 'host_aliases/0'")
 	// assert we don't output the resolved secret
 	assert.NotContains(v.T(), output, "a_super_secret_string")
+}
+
+func (v *linuxSecretSuite) TestAgentConfigRefresh() {
+	config := `secret_backend_command: /tmp/secret.py
+secret_backend_arguments:
+  - /tmp
+api_key: ENC[api_key]
+`
+
+	secretClient := secrets.NewSecretClient(v.T(), v.Env().RemoteHost, "/tmp")
+	secretClient.SetSecret("api_key", "abcdefghijklmnopqrstuvwxyz123456")
+
+	v.UpdateEnv(
+		awshost.ProvisionerNoFakeIntake(
+			awshost.WithAgentOptions(
+				secrets.WithUnixSecretSetupScript("/tmp/secret.py", false),
+				agentparams.WithSkipAPIKeyInConfig(),
+				agentparams.WithAgentConfig(config),
+			),
+		),
+	)
+
+	status := v.Env().Agent.Client.Status()
+	assert.Contains(v.T(), status.Content, "API key ending with 23456")
+
+	secretClient.SetSecret("api_key", "123456abcdefghijklmnopqrstuvwxyz")
+
+	secretRefreshOutput := v.Env().Agent.Client.Secret(agentclient.WithArgs([]string{"refresh"}))
+	require.Contains(v.T(), secretRefreshOutput, "api_key")
+
+	status = v.Env().Agent.Client.Status()
+	assert.EventuallyWithT(v.T(), func(t *assert.CollectT) {
+		assert.Contains(t, status.Content, "API key ending with vwxyz")
+	}, 1*time.Minute, 10*time.Second)
 }
