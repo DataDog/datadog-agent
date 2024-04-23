33ae9b84e35d5fe376f7d45ae5cab479bf71877f
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-04-23 12:52:14 -0400

    fix test

--- cmd/serverless-init/main_test.go
+++ cmd/serverless-init/main_test.go
@@ -36,7 +36,7 @@ func TestTagsSetup(t *testing.T) {
 
 	allTags := append(ddTags, ddExtraTags...)
 
-	_, _, traceAgent, metricAgent, _ := setup("", nil, nil)
+	_, _, traceAgent, metricAgent, _ := setup()
 	defer traceAgent.Stop()
 	defer metricAgent.Stop()
 	assert.Subset(t, metricAgent.GetExtraTags(), allTags)
