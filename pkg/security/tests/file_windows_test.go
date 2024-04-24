8f544c98dda9f8cde6da920c1e6cc1ccc57d12cc
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-04-24 19:30:51 +0200

    [CWS] implement rename event on windows (#25068)
    * default return false fix
    
    * very basic rename state tracking
    
    * generated rename fields
    
    * add test for new rename feature
    
    * fix rename not found in windows model
    
    * clean up a bit where the rename state is tracked
    
    * add release note
    
    * fix discarders

--- pkg/security/tests/file_windows_test.go
+++ pkg/security/tests/file_windows_test.go
@@ -64,6 +64,43 @@ func TestBasicFileTest(t *testing.T) {
 
 }
 
+func TestRenameFileEvent(t *testing.T) {
+	// ebpftest.LogLevel(t, "info")
+	cfn := &rules.RuleDefinition{
+		ID:         "test_rename_file",
+		Expression: `rename.file.name =~ "test.bad" && rename.file.path =~ "\Device\*\Temp\**"`,
+	}
+	opts := testOpts{
+		enableFIM: true,
+	}
+	test, err := newTestModule(t, nil, []*rules.RuleDefinition{cfn}, withStaticOpts(opts))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer test.Close()
+	// this is kinda hokey.  ETW (which is what FIM is based on) takes an indeterminant amount of time to start up.
+	// so wait around for it to start
+	time.Sleep(5 * time.Second)
+
+	os.MkdirAll("C:\\Temp", 0755)
+	f, err := os.Create("C:\\Temp\\test.bad")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	test.Run(t, "rename", func(t *testing.T, kind wrapperType, cmdFunc func(cmd string, args []string, envs []string) *exec.Cmd) {
+		test.WaitSignal(t, func() error {
+			return os.Rename("C:\\Temp\\test.bad", "C:\\Temp\\test.good")
+		}, test.validateFileEvent(t, noWrapperType, func(event *model.Event, rule *rules.Rule) {
+			assertFieldEqualCaseInsensitve(t, event, "rename.file.name", "test.bad", event, "rename.file.name file didn't match")
+			assertFieldEqualCaseInsensitve(t, event, "rename.file.destination.name", "test.good", event, "rename.file.destination.name file didn't match")
+		}))
+	})
+}
+
 func (tm *testModule) validateFileEvent(tb *testing.T, kind wrapperType, validate func(event *model.Event, rule *rules.Rule)) func(event *model.Event, rule *rules.Rule) {
 	return func(event *model.Event, rule *rules.Rule) {
 		validate(event, rule)
