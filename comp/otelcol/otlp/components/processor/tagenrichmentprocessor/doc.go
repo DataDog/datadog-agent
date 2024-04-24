f893b14407d809af2b2126e3a84cbdc8ed02cd5d
Dino Oliva <dinooliva@users.noreply.github.com>
2024-04-24 12:02:22 -0700

    Add Datadog license to all new files.

--- comp/otelcol/otlp/components/processor/tagenrichmentprocessor/doc.go
+++ comp/otelcol/otlp/components/processor/tagenrichmentprocessor/doc.go
@@ -1,7 +1,7 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-//go:generate mdatagen metadata.yaml
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
 
 // Package tagenrichmentprocessor implements a processor for augmenting
 // tags.
--- comp/otelcol/otlp/components/processor/tagenrichmentprocessor/factory.go
+++ comp/otelcol/otlp/components/processor/tagenrichmentprocessor/factory.go
@@ -1,5 +1,7 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
 
 package tagenrichmentprocessor
 
