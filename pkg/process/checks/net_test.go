7b92da988b6c2be60acf78d3b93f202ad8a44097
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-04-24 10:30:51 +0200

    testme

--- pkg/process/checks/net_test.go
+++ pkg/process/checks/net_test.go
@@ -697,6 +697,7 @@ func TestConvertAndEnrichWithServiceTags(t *testing.T) {
 }
 
 func TestConnectionsCheck_scheduleNetworkPath(t *testing.T) {
+	// TODO: TESTME
 	//conns := makeConnections(4)
 	//conns[0].Raddr.Ip = "127.0.0.1"
 
