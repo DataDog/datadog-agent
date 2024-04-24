22c484a45db6fafad71994a2d1d299b46b28a7af
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-04-24 11:06:02 +0200

    bump google.golang.org/protobuf/cmd/protoc-gen-go to v1.33.0

--- tasks/security_agent.py
+++ tasks/security_agent.py
@@ -752,7 +752,7 @@ def generate_btfhub_constants(ctx, archive_path, force_refresh=False):
 def generate_cws_proto(ctx):
     with tempfile.TemporaryDirectory() as temp_gobin:
         with environ({"GOBIN": temp_gobin}):
-            ctx.run("go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.32.0")
+            ctx.run("go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.33.0")
             ctx.run("go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@v0.6.0")
             ctx.run("go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3.0")
 
