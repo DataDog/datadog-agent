0781648e5e3c0e4bed03a14493bc3fc3a821fade
Raphael Gavache <raphael.gavache@datadoghq.com>
2024-04-25 14:42:39 +0200

    fix chars

--- cmd/installer/subcommands/installer/command.go
+++ cmd/installer/subcommands/installer/command.go
@@ -202,7 +202,7 @@ func installExperimentCommand() *cobra.Command {
 		GroupID: "installer",
 		Args:    cobra.ExactArgs(1),
 		RunE: func(_ *cobra.Command, args []string) (err error) {
-			i := newInstallerCmd("install-experiment")
+			i := newInstallerCmd("install_experiment")
 			defer func() { i.Stop(err) }()
 			i.span.SetTag("params.url", args[0])
 			return i.InstallExperiment(i.ctx, args[0])
@@ -218,7 +218,7 @@ func removeExperimentCommand() *cobra.Command {
 		GroupID: "installer",
 		Args:    cobra.ExactArgs(1),
 		RunE: func(_ *cobra.Command, args []string) (err error) {
-			i := newInstallerCmd("remove-experiment")
+			i := newInstallerCmd("remove_experiment")
 			defer func() { i.Stop(err) }()
 			i.span.SetTag("params.package", args[0])
 			return i.RemoveExperiment(i.ctx, args[0])
@@ -234,7 +234,7 @@ func promoteExperimentCommand() *cobra.Command {
 		GroupID: "installer",
 		Args:    cobra.ExactArgs(1),
 		RunE: func(_ *cobra.Command, args []string) (err error) {
-			i := newInstallerCmd("promote-experiment")
+			i := newInstallerCmd("promote_experiment")
 			defer func() { i.Stop(err) }()
 			i.span.SetTag("params.package", args[0])
 			return i.PromoteExperiment(i.ctx, args[0])
@@ -250,7 +250,7 @@ func garbageCollectCommand() *cobra.Command {
 		GroupID: "installer",
 		Args:    cobra.NoArgs,
 		RunE: func(_ *cobra.Command, _ []string) (err error) {
-			i := newInstallerCmd("garbage-collect")
+			i := newInstallerCmd("garbage_collect")
 			defer func() { i.Stop(err) }()
 			return i.GarbageCollect(i.ctx)
 		},
