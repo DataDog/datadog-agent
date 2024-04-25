dfc57b88a7f6f53cbee20f4bb769030704140e80
Lucas Liseth <lucas+github@lucasliseth.com>
2024-04-25 11:55:18 -0400

    Renamed test suites and added container collect all test

--- test/new-e2e/tests/agent-metric-logs/log-agent/k8s-logs/file_tailing_cca_off_test.go
+++ test/new-e2e/tests/agent-metric-logs/log-agent/k8s-logs/file_tailing_cca_off_test.go
@@ -8,7 +8,6 @@ package k8sfiletailing
 import (
 	"context"
 	"fmt"
-	"os"
 	"slices"
 	"testing"
 	"time"
@@ -18,35 +17,42 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/DataDog/test-infra-definitions/components/datadog/kubernetesagentparams"
+
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	customkind "github.com/DataDog/datadog-agent/test/new-e2e/tests/agent-metric-logs/customkind"
 )
 
-type k8sSuite struct {
+type k8sCCAOffSuite struct {
 	e2e.BaseSuite[environments.Kubernetes]
 }
 
-func TestK8sSuite(t *testing.T) {
-	e2e.Run(t, &k8sSuite{}, e2e.WithProvisioner(customkind.Provisioner()))
+func TestK8sCCAOff(t *testing.T) {
+	e2e.Run(t, &k8sCCAOffSuite{}, e2e.WithProvisioner(customkind.Provisioner(customkind.WithAgentOptions(kubernetesagentparams.WithoutLogsContainerCollectAll()))))
 }
 
-func (v *k8sSuite) TestSingleLogAndMetadata() {
+func (v *k8sCCAOffSuite) TestADAnnotations() {
 	v.Env().FakeIntake.Client().FlushServerAndResetAggregators()
 	var backOffLimit int32 = 4
-	testLogMessage := "Test log message"
+	testLogMessage := "Annotations pod"
 
 	jobSpcec := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "job-1",
+			Name:      "annotations-job",
 			Namespace: "default",
 		},
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"ad.datadoghq.com/annotations-job.logs": "[{\"source\": \"test-container\"}]",
+					},
+				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name:    "query-job-1",
+							Name:    "annotations-job",
 							Image:   "ubuntu",
 							Command: []string{"echo", testLogMessage},
 						},
@@ -59,7 +65,7 @@ func (v *k8sSuite) TestSingleLogAndMetadata() {
 	}
 
 	_, err := v.Env().KubernetesCluster.Client().BatchV1().Jobs("default").Create(context.TODO(), jobSpcec, metav1.CreateOptions{})
-	assert.NoError(v.T(), err, "Could not properly start job")
+	assert.NoError(v.T(), err, "Could not start autodiscovery job")
 
 	v.EventuallyWithT(func(c *assert.CollectT) {
 		logsServiceNames, err := v.Env().FakeIntake.Client().GetLogServiceNames()
@@ -70,25 +76,18 @@ func (v *k8sSuite) TestSingleLogAndMetadata() {
 			filteredLogs, err := v.Env().FakeIntake.Client().FilterLogs("ubuntu")
 			assert.NoError(c, err, "Error filtering logs")
 			assert.Equal(c, testLogMessage, filteredLogs[0].Message, "Test log doesn't match")
-
-			// Check container metatdata
-			assert.Equal(c, filteredLogs[0].Service, "ubuntu", "Could not find service")
-			assert.NotNil(c, filteredLogs[0].HostName, "Hostname not found")
-			assert.NotNil(c, filteredLogs[0].Tags, "Log tags not found")
 		}
-
 	}, 1*time.Minute, 10*time.Second)
 }
 
-func (v *k8sSuite) TestLongLogLine() {
+func (v *k8sCCAOffSuite) TestCCAOff() {
 	v.Env().FakeIntake.Client().FlushServerAndResetAggregators()
 	var backOffLimit int32 = 4
-	file, err := os.ReadFile("long_line_log.txt")
-	assert.NoError(v.T(), err, "Could not open long line file.")
+	testLogMessage := "Test pod"
 
 	jobSpcec := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "long-line-job",
+			Name:      "cca-off-job",
 			Namespace: "default",
 		},
 		Spec: batchv1.JobSpec{
@@ -96,63 +95,8 @@ func (v *k8sSuite) TestLongLogLine() {
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
-							Name:    "long-line-job",
+							Name:    "cca-off-job",
 							Image:   "ubuntu",
-							Command: []string{"echo", string(file)},
-						},
-					},
-					RestartPolicy: corev1.RestartPolicyNever,
-				},
-			},
-			BackoffLimit: &backOffLimit,
-		},
-	}
-
-	_, err = v.Env().KubernetesCluster.Client().BatchV1().Jobs("default").Create(context.TODO(), jobSpcec, metav1.CreateOptions{})
-	assert.NoError(v.T(), err, "Could not properly start job")
-
-	v.EventuallyWithT(func(c *assert.CollectT) {
-		logsServiceNames, err := v.Env().FakeIntake.Client().GetLogServiceNames()
-		assert.NoError(c, err, "Error starting job")
-		assert.Contains(c, logsServiceNames, "ubuntu", "Ubuntu service not found")
-
-		if slices.Contains(logsServiceNames, "ubuntu") {
-			filteredLogs, err := v.Env().FakeIntake.Client().FilterLogs("ubuntu")
-			assert.NoError(c, err, "Error filtering logs")
-			assert.Equal(c, string(file), fmt.Sprintf("%s%s", filteredLogs[0].Message, "\n"), "Test log doesn't match")
-		}
-
-	}, 1*time.Minute, 10*time.Second)
-}
-
-func (v *k8sSuite) TestContainerExclude() {
-	v.Env().FakeIntake.Client().FlushServerAndResetAggregators()
-
-	// We're testing exclusion via namespace, so we have to create a new namespace
-	namespaceName := "exclude-namespace"
-	namespace := &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: namespaceName,
-		},
-	}
-	_, err := v.Env().KubernetesCluster.Client().CoreV1().Namespaces().Create(context.TODO(), namespace, metav1.CreateOptions{})
-	assert.NoError(v.T(), err, "Could not create namespace")
-
-	var backOffLimit int32 = 4
-	testLogMessage := "Test log message here"
-
-	jobSpcec := &batchv1.Job{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "exclude-job",
-			Namespace: namespaceName,
-		},
-		Spec: batchv1.JobSpec{
-			Template: corev1.PodTemplateSpec{
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:    "exclude-job",
-							Image:   "alpine",
 							Command: []string{"echo", testLogMessage},
 						},
 					},
@@ -163,12 +107,21 @@ func (v *k8sSuite) TestContainerExclude() {
 		},
 	}
 
-	_, err = v.Env().KubernetesCluster.Client().BatchV1().Jobs(namespaceName).Create(context.TODO(), jobSpcec, metav1.CreateOptions{})
-	assert.NoError(v.T(), err, "Could not properly start job")
+	_, err := v.Env().KubernetesCluster.Client().BatchV1().Jobs("default").Create(context.TODO(), jobSpcec, metav1.CreateOptions{})
+	assert.NoError(v.T(), err, "Could not start job")
 
 	v.EventuallyWithT(func(c *assert.CollectT) {
 		logsServiceNames, err := v.Env().FakeIntake.Client().GetLogServiceNames()
 		assert.NoError(c, err, "Error starting job")
-		assert.NotContains(c, logsServiceNames, "alpine", "Alpine service found after excluded")
+		// assert.NotContains(c, logsServiceNames, "ubuntu", "Ubuntu service found with container collect all off")
+
+		if slices.Contains(logsServiceNames, "ubuntu") {
+			filteredLogs, err := v.Env().FakeIntake.Client().FilterLogs("ubuntu")
+			assert.NoError(c, err, "Error filtering logs")
+			fmt.Println("LOGS here:")
+			fmt.Printf("%s\n", filteredLogs[0].Message)
+			assert.Nil(c, filteredLogs, "Filtered logs is not nil")
+		}
+
 	}, 1*time.Minute, 10*time.Second)
 }
