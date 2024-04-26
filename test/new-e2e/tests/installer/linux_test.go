b4dea00fdb39c71e71d3e12cc3d5c2ee78d7f078
Arthur Bellal <arthur.bellal@datadoghq.com>
2024-04-27 00:15:35 +0200

    (fleet) fix remote updates permissions (#25170)
    * (fleet) fix remote updates permissions
    
    Couple fixes to allow the deamon to write runtime files.
    
    * tests
    
    * fx fun
    
    * fix tests
    
    * lol

--- test/new-e2e/tests/installer/linux_test.go
+++ test/new-e2e/tests/installer/linux_test.go
@@ -31,11 +31,12 @@ import (
 )
 
 const (
-	confDir          = "/etc/datadog-agent"
-	logDir           = "/var/log/datadog"
-	locksDir         = "/var/run/datadog-packages"
-	packagesDir      = "/opt/datadog-packages"
-	bootInstallerDir = "/opt/datadog-installer"
+	confDir               = "/etc/datadog-agent"
+	logDir                = "/var/log/datadog"
+	stableInstallerRunDir = "/opt/datadog-packages/datadog-installer/stable/run"
+	locksDir              = "/var/run/datadog-packages"
+	packagesDir           = "/opt/datadog-packages"
+	bootInstallerDir      = "/opt/datadog-installer"
 )
 
 type installerSuite struct {
@@ -152,6 +153,11 @@ func (v *installerSuite) TestInstallerDirs() {
 	for _, dir := range []string{packagesDir, bootInstallerDir} {
 		require.Equal(v.T(), "root\n", host.MustExecute(`stat -c "%U" `+dir))
 		require.Equal(v.T(), "root\n", host.MustExecute(`stat -c "%G" `+dir))
+	}
+	for _, dir := range []string{stableInstallerRunDir} {
+		require.Equal(v.T(), "dd-agent\n", host.MustExecute(`stat -c "%U" `+dir))
+		require.Equal(v.T(), "dd-agent\n", host.MustExecute(`stat -c "%G" `+dir))
+
 	}
 	require.Equal(v.T(), "drwxrwxrwx\n", host.MustExecute(`stat -c "%A" `+locksDir))
 	require.Equal(v.T(), "drwxr-xr-x\n", host.MustExecute(`stat -c "%A" `+packagesDir))
