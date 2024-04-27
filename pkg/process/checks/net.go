63bc6ab413e47f7360730cc2c707d977f64e0cf1
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-04-27 15:42:46 +0200

    shouldScheduleNetworkPathForConn

--- pkg/process/checks/net.go
+++ pkg/process/checks/net.go
@@ -530,7 +530,7 @@ func (c *ConnectionsCheck) scheduleNetworkPath(conns []*model.Connection) {
 	// TODO: TESTME
 	for _, conn := range conns {
 		// Only process outgoing traffic
-		if conn.Direction != model.ConnectionDirection_outgoing {
+		if !c.shouldScheduleNetworkPathForConn(conn) {
 			// TODO: TESTME
 			continue
 		}
@@ -548,4 +548,19 @@ func (c *ConnectionsCheck) scheduleNetworkPath(conns []*model.Connection) {
 
 	scheduleDuration := time.Since(startTime)
 	statsd.Client.Gauge("datadog.network_path.connections_check.schedule_duration", scheduleDuration.Seconds(), nil, 1) //nolint:errcheck
+}
+
+func (c *ConnectionsCheck) shouldScheduleNetworkPathForConn(conn *model.Connection) bool {
+	// TODO: TESTME
+	if conn.Direction != model.ConnectionDirection_outgoing {
+		return false
+	}
+	// Skip IPs
+	// 169.254.169.254 is used in AWS, Azure, GCP and other cloud computing platforms to host instance metadata service.
+	// TODO: Should we skip all 169.254.0.0/16 (dynamically configured link-local addresses)
+	if conn.Raddr.Ip == "169.254.169.254" {
+		return false
+	}
+
+	return true
 }
