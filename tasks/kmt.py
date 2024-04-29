bb10de9121b323b6bf9673a500253d59e0d08c7a
usamasaqib <usama.saqib@datadoghq.com>
2024-04-29 16:13:20 +0200

    package security

--- tasks/kmt.py
+++ tasks/kmt.py
@@ -558,11 +558,11 @@ def kmt_secagent_prepare(
         bundle_ebpf=False,
         race=True,
         debug=True,
-        output=f"{kmt_paths.secagent_tests}/pkg/security-agent/testsuite",
+        output=f"{kmt_paths.secagent_tests}/pkg/security/testsuite",
         skip_linters=True,
         skip_object_files=True,
     )
-    build_stress_tests(ctx, output=f"{kmt_paths.secagent_tests}/pkg/security-agent/stresssuite", skip_linters=True)
+    build_stress_tests(ctx, output=f"{kmt_paths.secagent_tests}/pkg/security/stresssuite", skip_linters=True)
 
     go_path = "go"
     go_root = os.getenv("GOROOT")
