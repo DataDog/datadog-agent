3e653a396bfc610e657e0995c496fd2754e60f3d
AliDatadog <ali.benabdallah@datadoghq.com>
2024-04-29 14:26:29 +0200

    merge dogstatsd tests together

--- test/new-e2e/tests/containers/k8s_test.go
+++ test/new-e2e/tests/containers/k8s_test.go
@@ -595,25 +595,17 @@ func (suite *k8sSuite) TestCPU() {
 	})
 }
 
-func (suite *k8sSuite) TestDogstatsdInAgent() {
-	// Test with UDS
-	suite.testDogstatsdContainerID(kubeNamespaceDogstatsWorkload, kubeDeploymentDogstatsdUDS)
-	// Test with UDP + Origin detection
-	suite.testDogstatsdContainerID(kubeNamespaceDogstatsWorkload, kubeDeploymentDogstatsdUDPOrigin)
-	// Test with UDP + DD_ENTITY_ID with container name injected
-	suite.testDogstatsdContainerID(kubeNamespaceDogstatsWorkload, kubeDeploymentDogstatsdUDPOriginContNameInjected)
-	// Test with UDP + DD_ENTITY_ID
-	suite.testDogstatsdPodUID(kubeNamespaceDogstatsWorkload)
-}
-
-func (suite *k8sSuite) TestDogstatsdStandalone() {
-	// Test with UDS
-	suite.testDogstatsdContainerID(kubeNamespaceDogstatsStandaloneWorkload, kubeDeploymentDogstatsdUDS)
-	// Dogstatsd standalone does not support origin detection
-	// Test with UDP + DD_ENTITY_ID
-	suite.testDogstatsdPodUID(kubeNamespaceDogstatsWorkload)
-	// Test with UDP + DD_ENTITY_ID with container name injected
-	suite.testDogstatsdContainerID(kubeNamespaceDogstatsWorkload, kubeDeploymentDogstatsdUDPOriginContNameInjected)
+func (suite *k8sSuite) TestDogstatsd() {
+	for _, ns := range []string{kubeNamespaceDogstatsWorkload, kubeNamespaceDogstatsStandaloneWorkload} {
+		// Test with UDS
+		suite.testDogstatsdContainerID(ns, kubeDeploymentDogstatsdUDS)
+		// Test with UDP + Origin detection
+		suite.testDogstatsdContainerID(ns, kubeDeploymentDogstatsdUDPOrigin)
+		// Test with UDP + DD_ENTITY_ID with container name injected
+		suite.testDogstatsdContainerID(ns, kubeDeploymentDogstatsdUDPOriginContNameInjected)
+		// Test with UDP + DD_ENTITY_ID
+		suite.testDogstatsdPodUID(ns)
+	}
 }
 
 func (suite *k8sSuite) testDogstatsdPodUID(kubeNamespace string) {
