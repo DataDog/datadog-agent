3015e1f08637bb216ada9ed660cb5da919be7772
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2024-04-29 15:29:15 +0200

    Add unit-tests

--- tasks/custom_task/custom_task_test.py
+++ tasks/custom_task/custom_task_test.py
@@ -1,23 +1,103 @@
-import inspect
-from invoke import Task
-import invoke
+import unittest.mock
+from invoke import task, Context
+from datetime import datetime
 import unittest
-
-expected_content = """    def __call__(self, *args: Any, **kwargs: Any) -> T:
-        # Guard against calling tasks with no context.
-        if not isinstance(args[0], Context):
-            err = "Task expected a Context as its first arg, got {} instead!"
-            # TODO: raise a custom subclass _of_ TypeError instead
-            raise TypeError(err.format(type(args[0])))
-        result = self.body(*args, **kwargs)
-        self.times_called += 1
-        return result
-"""
+from tasks.custom_task.custom_task import log_invoke_task
 
 
-class TestTaskCall(unittest.TestCase):
-    def test_task_call_content(self):
-        self.assertEqual(inspect.getsource(Task.__call__), expected_content)
+@task
+def test_task(_):
+    """
+    Dummy task for testing purposes.
+    """
+    return "Nice task"
 
-    def test_invoke_version(self):
-        self.assertEqual(invoke.__version__, '2.2.0')
+
+test_task.__module__ = "my_module"
+
+
+@task
+def test_task_with_error(_):
+    """
+    Dummy task returning an error for testing purposes.
+    """
+    raise TypeError("Oh no this is not a good type !")
+
+
+test_task_with_error.__module__ = "my_broken_module"
+
+
+class TestInvokeTaskCustomCall(unittest.TestCase):
+    """
+    Testing the __call__ method (overridden with custom__call__) log generation.
+    """
+
+    @unittest.mock.patch('tasks.custom_task.custom_task.datetime')
+    @unittest.mock.patch('tasks.custom_task.custom_task.perf_counter', side_effect=[1, 1.0123456, 10, 15])
+    @unittest.mock.patch('tasks.custom_task.custom_task.getuser', side_effect=['john.doe', 'alex.smith'])
+    @unittest.mock.patch("tasks.custom_task.custom_task.logging.info")
+    def test_custom__call__(self, mock_logging, _getuser, _perf_counter, mock_datetime):
+        """
+        Testing the __call__ method (overridden with custom__call__) log generation.
+        """
+        ctx = Context()
+        mock_datetime.now.return_value = datetime(2024, 4, 29, 14, 5, 30, 367616)
+
+        # Testing the context manager with a successful task.
+        expected_test_log = {
+            "name": "test_task",
+            "module": "my_module",
+            "datetime": "2024-04-29 14:05:30",
+            "duration": 0.0123,
+            "user": "john.doe",
+            "result": None,
+        }
+        test_task.__call__(ctx)
+        mock_logging.assert_called_once_with(expected_test_log)
+
+        # Testing the context manager with a failing task.
+        expected_test_error_log = {
+            "name": "test_task_with_error",
+            "module": "my_broken_module",
+            "datetime": "2024-04-29 14:05:30",
+            "duration": 5.0000,
+            "user": "alex.smith",
+            "result": unittest.mock.ANY,
+        }
+        with self.assertRaises(TypeError):
+            test_task_with_error.__call__(ctx)
+
+        ## Check that the logger was called with the expected arguments.
+        mock_logging.assert_called_with(expected_test_error_log)
+
+        ## Check that the traceback is returned in the log.
+        expected_test_error_result = mock_logging.call_args_list[1].args[0]['result']
+        self.assertIn('Traceback (most recent call last)', expected_test_error_result)
+        self.assertIn('TypeError', expected_test_error_result)
+        self.assertIn('Oh no this is not a good type !', expected_test_error_result)
+
+
+class TestLogInvokeTask(unittest.TestCase):
+    """
+    Testing the log_invoke_task function.
+    """
+
+    @unittest.mock.patch('tasks.custom_task.custom_task.getuser', side_effect=['john.smith'])
+    @unittest.mock.patch("tasks.custom_task.custom_task.logging.info")
+    def test_log_invoke_task(self, mock_logging, _getuser):
+        """
+        Testing the log_invoke_task function.
+        """
+        log_invoke_task(
+            name="tname", module="mname", task_datetime="2024-01-29 07:50:11", duration=3.1, task_result="result"
+        )
+        mock_logging.assert_called_once_with(
+            {
+                "name": "tname",
+                "module": "mname",
+                "datetime": "2024-01-29 07:50:11",
+                "duration": 3.1,
+                "user": "john.smith",
+                "result": "result",
+            }
+        )
