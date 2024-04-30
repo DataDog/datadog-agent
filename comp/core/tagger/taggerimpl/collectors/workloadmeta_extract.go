e6dfd53ae067960804846d5f120a7aae7e02593d
Jennifer Chen <jennifer.chen@datadoghq.com>
2024-04-30 11:37:41 -0400

    Add namespace info to workloadmeta

--- comp/core/tagger/taggerimpl/collectors/workloadmeta_extract.go
+++ comp/core/tagger/taggerimpl/collectors/workloadmeta_extract.go
@@ -137,7 +137,7 @@ func (c *WorkloadMetaCollector) processEvents(evBundle workloadmeta.EventBundle)
 			case workloadmeta.KindKubernetesNode:
 				tagInfos = append(tagInfos, c.handleKubeNode(ev)...)
 			case workloadmeta.KindKubernetesNamespace:
-				// tagInfos = append(tagInfos, c.handleKubeNamespace(ev)...) No tags for now
+				tagInfos = append(tagInfos, c.handleKubeNamespace(ev)...)
 			case workloadmeta.KindECSTask:
 				tagInfos = append(tagInfos, c.handleECSTask(ev)...)
 			case workloadmeta.KindContainerImageMetadata:
@@ -427,6 +427,28 @@ func (c *WorkloadMetaCollector) handleKubeNode(ev workloadmeta.Event) []*types.T
 		{
 			Source:               nodeSource,
 			Entity:               buildTaggerEntityID(node.EntityID),
+			HighCardTags:         high,
+			OrchestratorCardTags: orch,
+			LowCardTags:          low,
+			StandardTags:         standard,
+		},
+	}
+
+	return tagInfos
+}
+
+func (c *WorkloadMetaCollector) handleKubeNamespace(ev workloadmeta.Event) []*types.TagInfo {
+	namespace := ev.Entity.(*workloadmeta.KubernetesNamespace)
+
+	tags := utils.NewTagList()
+
+	// Add tags for namespace here
+
+	low, orch, high, standard := tags.Compute()
+	tagInfos := []*types.TagInfo{
+		{
+			Source:               nodeSource,
+			Entity:               buildTaggerEntityID(namespace.EntityID),
 			HighCardTags:         high,
 			OrchestratorCardTags: orch,
 			LowCardTags:          low,
