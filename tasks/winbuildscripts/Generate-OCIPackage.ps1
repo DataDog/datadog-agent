d7e0759143eaf792654a83dd2160bf94167a189e
Julien Lebot <julien.lebot@datadoghq.com>
2024-04-30 15:11:29 +0200

    Generate package in pipeline

--- tasks/winbuildscripts/Generate-OCIPackage.ps1
+++ tasks/winbuildscripts/Generate-OCIPackage.ps1
@@ -1,5 +1,16 @@
-Write-HOst "Downloading datadog-package.exe"
-(New-Object System.Net.WebClient).DownloadFile("https://dd-agent-omnibus.s3.amazonaws.com/datadog-package.exe", "C:\\tools\\datadog-package.exe")
+if (-not (Test-Path C:\tools\datadog-package.exe)) {
+    Write-Host "Downloading datadog-package.exe"
+    (New-Object System.Net.WebClient).DownloadFile("https://dd-agent-omnibus.s3.amazonaws.com/datadog-package.exe", "C:\\tools\\datadog-package.exe")
+}
 $rawAgentVersion = "{0}-1" -f (inv agent.version --url-safe --major-version 7)
 Write-Host "Detected agent version ${rawAgentVersion}"
-& C:\\tools\\datadog-package.exe create --package datadog-agent --version $rawAgentVersion c:\buildroot\datadog-agent\omnibus\pkg\
+
+$packageName = "datadog-agent-${rawAgentVersion}-windows-amd64.tar"
+
+if (Test-Path .\omnibus\pkg\$packageName) {
+    Remove-Item .\omnibus\pkg\$packageName
+}
+
+# The argument --archive-path ".\omnibus\pkg\datadog-agent-${version}.tar.gz" is currently broken and has no effects
+& C:\tools\datadog-package.exe create --package datadog-agent --os windows --arch amd64 --archive --version $rawAgentVersion .\omnibus\pkg\
+Copy-Item datadog-agent-${rawAgentVersion}-windows-amd64.tar .\omnibus\pkg\
