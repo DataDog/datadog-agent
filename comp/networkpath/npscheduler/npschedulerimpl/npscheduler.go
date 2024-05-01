935c7579dbc545f6d8483a97fcba771af691b9c7
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-05-01 22:50:21 +0200

    comment

--- comp/networkpath/npscheduler/npschedulerimpl/npscheduler.go
+++ comp/networkpath/npscheduler/npschedulerimpl/npscheduler.go
@@ -228,7 +228,9 @@ func (s *npSchedulerImpl) flushLoop() {
 }
 
 func (s *npSchedulerImpl) flush() {
+	// TODO: Remove workers metric?
 	statsd.Client.Gauge("datadog.network_path.scheduler.workers", float64(s.workers), []string{}, 1) //nolint:errcheck
+
 	flowsContexts := s.pathtestStore.getPathtestContextCount()
 	statsd.Client.Gauge("datadog.network_path.scheduler.pathtest_store_size", float64(flowsContexts), []string{}, 1) //nolint:errcheck
 	flushTime := s.TimeNowFunction()
