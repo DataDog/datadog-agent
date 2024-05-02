41c5e318e832541532c08c9bf06c950958df502d
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-05-02 14:12:01 +0200

    codereview: use variables and fix improper rebase

--- tasks/winbuildscripts/unittests.ps1
+++ tasks/winbuildscripts/unittests.ps1
@@ -62,8 +62,8 @@ if($err -ne 0){
 }
 
 $ErrorActionPreference = "Continue" # Ignore upload errors now, until we change the logic to ignore empty files in the upload script
-$Env:DATADOG_API_KEY=$(& "c:\mnt\tools\ci\aws_ssm_get_wrapper.ps1" $Env:API_KEY_ORG2_SSM_NAME)
-Get-ChildItem -Path "c:\mnt" -Filter "junit-*.tgz" -Recurse | ForEach-Object {
+$Env:DATADOG_API_KEY=$(& "$UT_BUILD_ROOT\tools\ci\aws_ssm_get_wrapper.ps1" $Env:API_KEY_ORG2_SSM_NAME)
+Get-ChildItem -Path "$UT_BUILD_ROOT" -Filter "junit-*.tgz" -Recurse | ForEach-Object {
     $outputFilePath = "upload_output.txt"
     inv -e junit-upload --tgz-path $_.FullName > $outputFilePath
 }
