d146e67e17f369982df2bddfdfd8b6ae75075ca1
raymond zhao <raymond.zhao@datadoghq.com>
2024-05-03 11:03:16 -0400

    Fix import

--- comp/dogstatsd/server/server_test.go
+++ comp/dogstatsd/server/server_test.go
@@ -37,6 +37,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/metrics"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"github.com/DataDog/datadog-agent/pkg/util/optional"
+	"github.com/DataDog/datadog-agent/pkg/util/testutil/flake"
 )
 
 type serverDeps struct {
