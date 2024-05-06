583c160f61b25f5cbe68107b178c501ad47cef6c
Raphael Gavache <raphael.gavache@datadoghq.com>
2024-05-06 12:06:59 +0200

    Update pkg/fleet/installer/service/apm_inject.go
    Co-authored-by: Arthur Bellal <arthur.bellal@datadoghq.com>

--- pkg/fleet/installer/service/apm_inject.go
+++ pkg/fleet/installer/service/apm_inject.go
@@ -76,7 +76,7 @@ type apmInjectorInstaller struct {
 }
 
 // Setup sets up the APM injector
-func (a *apmInjectorInstaller) Setup(ctx context.Context) error {
+func (a *apmInjectorInstaller) Setup(ctx context.Context) (err error) {
 	var err error
 	var rollbackAgentConfig, rollbackLDPreload, rollbackDockerConfig func() error
 	defer func() {
