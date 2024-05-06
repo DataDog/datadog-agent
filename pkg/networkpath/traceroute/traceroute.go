a25bd831f14cc9aa9a9573f66f207a33e82ee008
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-05-06 10:46:52 +0200

    move payload

--- pkg/networkpath/traceroute/traceroute.go
+++ pkg/networkpath/traceroute/traceroute.go
@@ -8,8 +8,6 @@ package traceroute
 
 import (
 	"context"
-
-	"github.com/DataDog/datadog-agent/pkg/network"
 )
 
 type (
@@ -31,42 +29,5 @@ type (
 	// traceroutes for the Network Path integration
 	Traceroute interface {
 		Run(context.Context) (NetworkPath, error)
-	}
-
-	// NetworkPathHop encapsulates the data for a single
-	// hop within a path
-	NetworkPathHop struct {
-		TTL       int     `json:"ttl"`
-		IPAddress string  `json:"ip_address"`
-		Hostname  string  `json:"hostname"`
-		RTT       float64 `json:"rtt"`
-		Success   bool    `json:"success"`
-	}
-
-	// NetworkPathSource encapsulates information
-	// about the source of a path
-	NetworkPathSource struct {
-		Hostname  string       `json:"hostname"`
-		Via       *network.Via `json:"via"`
-		NetworkID string       `json:"network_id"` // Today this will be a VPC ID since we only resolve AWS resources
-	}
-
-	// NetworkPathDestination encapsulates information
-	// about the destination of a path
-	NetworkPathDestination struct {
-		Hostname  string `json:"hostname"`
-		IPAddress string `json:"ip_address"`
-	}
-
-	// NetworkPath encapsulates data that defines a
-	// path between two hosts as mapped by the agent
-	NetworkPath struct {
-		Timestamp   int64                  `json:"timestamp"`
-		Namespace   string                 `json:"namespace"` // namespace used to resolve NDM resources
-		PathID      string                 `json:"path_id"`
-		Source      NetworkPathSource      `json:"source"`
-		Destination NetworkPathDestination `json:"destination"`
-		Hops        []NetworkPathHop       `json:"hops"`
-		Tags        []string               `json:"tags"`
 	}
 )
