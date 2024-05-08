be9587b5a9d4a973f8691bbb255f18878f33d705
Yakov Shapiro <yakov.shapiro@datadoghq.com>
2024-05-08 14:07:35 -0400

    Report single-step agent installations on Docker

---
+++ comp/trace/config/install_signature_test.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInferInstallTypeFromEnvironment(t *testing.T) {
+	t.Run("Non-dockerized agent", func(t *testing.T) {
+		assert.Equal(t, defaultInstallType, inferInstallTypeFromEnvironment())
+	})
+
+	t.Run("Dockerized manual instrumentation", func(t *testing.T) {
+		_ = os.Setenv("DOCKER_DD_AGENT", "true")
+		defer os.Unsetenv("DOCKER_DD_AGENT")
+		assert.Equal(t, defaultDockerInstallType, inferInstallTypeFromEnvironment())
+	})
+
+	t.Run("Dockerized single-step instrumentation", func(t *testing.T) {
+		_ = os.Setenv("DOCKER_DD_AGENT", "true")
+		_ = os.Setenv("DD_APM_ENABLED", "true")
+		defer os.Unsetenv("DOCKER_DD_AGENT")
+		defer os.Unsetenv("DD_APM_ENABLED")
+		assert.Equal(t, dockerSingleStepInstallType, inferInstallTypeFromEnvironment())
+	})
+
+	t.Run("Non-standard environment variable values", func(t *testing.T) {
+		_ = os.Setenv("DOCKER_DD_AGENT", "yes")
+		_ = os.Setenv("DD_APM_ENABLED", "sure")
+		defer os.Unsetenv("DOCKER_DD_AGENT")
+		defer os.Unsetenv("DD_APM_ENABLED")
+		assert.Equal(t, dockerSingleStepInstallType, inferInstallTypeFromEnvironment())
+	})
+}
