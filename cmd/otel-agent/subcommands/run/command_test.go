dd0a71e87db34a4c1e29063373d665ea246bb77f
Dinesh Gurumurthy <dinesh.gurumurthy@datadoghq.com>
2024-05-09 05:34:19 -0400

    Update OTel Agent to use Collector config

--- cmd/otel-agent/subcommands/run/command_test.go
+++ cmd/otel-agent/subcommands/run/command_test.go
@@ -10,13 +10,18 @@ import (
 	"testing"
 
 	"github.com/DataDog/datadog-agent/cmd/otel-agent/subcommands"
+	compdef "github.com/DataDog/datadog-agent/comp/def"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"go.uber.org/fx"
 )
 
 func TestFxRun(t *testing.T) {
 	fxutil.TestRun(t, func() error {
 		ctx := context.Background()
 		cliParams := &subcommands.GlobalParams{}
-		return runOTelAgentCommand(ctx, cliParams)
+		return runOTelAgentCommand(ctx, cliParams, fx.Provide(compdef.NewTestLifecycle), fx.Provide(func(t *compdef.TestLifecycle) compdef.Lifecycle {
+			return t
+		}),
+		)
 	})
 }
