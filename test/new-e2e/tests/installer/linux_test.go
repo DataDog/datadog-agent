2f3cb2534b61c810623e4e786f5abf0e7a194b60
Arthur Bellal <arthur.bellal@datadoghq.com>
2024-05-11 00:18:36 +0200

    quick fix

--- test/new-e2e/tests/installer/linux_test.go
+++ test/new-e2e/tests/installer/linux_test.go
@@ -108,6 +108,7 @@ func TestSuseARM(t *testing.T) {
 }
 
 func (v *installerSuite) bootstrap(remoteUpdatesEnabled bool) {
+	v.Env().RemoteHost.MustExecute("sudo rm /opt/datadog-installer/run/installer-hash || true")
 	v.Env().RemoteHost.MustExecute(
 		"sudo -E datadog-bootstrap bootstrap",
 		client.WithEnvVariables(client.EnvVar{
@@ -238,7 +239,6 @@ func (v *installerSuite) TestUninstall() {
 	}
 
 	for _, assertion := range installAssertions {
-		v.T().Logf("assertion: %s", assertion)
 		_ = host.MustExecute(assertion)
 	}
 	switch v.packageManager {
