9ffcb14307c1579fa731b07adc0be31755ee84b1
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-05-10 14:32:49 -0400

    fix health probe merge

--- cmd/serverless-init/main.go
+++ cmd/serverless-init/main.go
@@ -19,8 +19,8 @@ import (
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery"
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/autodiscoveryimpl"
 	coreconfig "github.com/DataDog/datadog-agent/comp/core/config"
-	"github.com/DataDog/datadog-agent/comp/core/healthprobe"
-	"github.com/DataDog/datadog-agent/comp/core/healthprobe/healthprobeimpl"
+	healthprobeDef "github.com/DataDog/datadog-agent/comp/core/healthprobe/def"
+	healthprobeFx "github.com/DataDog/datadog-agent/comp/core/healthprobe/fx"
 	"github.com/DataDog/datadog-agent/comp/core/log/logimpl"
 	"github.com/DataDog/datadog-agent/comp/core/secrets"
 	"github.com/DataDog/datadog-agent/comp/core/tagger"
@@ -61,14 +61,14 @@ func main() {
 		run,
 		autodiscoveryimpl.Module(),
 		workloadmeta.Module(),
-		fx.Provide(func(config coreconfig.Component) healthprobe.Options {
-			return healthprobe.Options{
+		fx.Provide(func(config coreconfig.Component) healthprobeDef.Options {
+			return healthprobeDef.Options{
 				Port:           config.GetInt("health_port"),
 				LogsGoroutines: config.GetBool("log_all_goroutines_when_unhealthy"),
 			}
 		}),
 		taggerimpl.Module(),
-		healthprobeimpl.Module(),
+		healthprobeFx.Module(),
 		fx.Supply(workloadmeta.NewParams()),
 		fx.Supply(tagger.NewTaggerParams()),
 		fx.Supply(core.BundleParams{
@@ -85,7 +85,7 @@ func main() {
 }
 
 // removing these unused dependencies will cause silent crash due to fx framework
-func run(_ secrets.Component, _ autodiscovery.Component, _ healthprobe.Component) {
+func run(_ secrets.Component, _ autodiscovery.Component, _ healthprobeDef.Component) {
 	cloudService, logConfig, traceAgent, metricAgent, logsAgent := setup(modeConf)
 
 	modeConf.Runner(logConfig)
