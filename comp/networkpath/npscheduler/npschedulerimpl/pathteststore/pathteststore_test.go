9659cb7f56f60798c21c591920bb0b16223b662f
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-05-12 17:30:36 +0200

    fix test

--- comp/networkpath/npscheduler/npschedulerimpl/pathteststore/pathteststore_test.go
+++ comp/networkpath/npscheduler/npschedulerimpl/pathteststore/pathteststore_test.go
@@ -34,23 +34,23 @@ func Test_pathtestStore_add(t *testing.T) {
 	logger := fxutil.Test[log.Component](t, logimpl.MockModule())
 
 	// GIVEN
-	store := newPathtestStore(common.DefaultFlushTickerInterval, 10*time.Minute, 1*time.Minute, logger)
+	store := NewPathtestStore(common.DefaultFlushTickerInterval, 10*time.Minute, 1*time.Minute, logger)
 
 	// WHEN
 	pt1 := &common.Pathtest{Hostname: "host1", Port: 53}
 	pt2 := &common.Pathtest{Hostname: "host2", Port: 53}
 	pt3 := &common.Pathtest{Hostname: "host3", Port: 53}
-	store.add(pt1)
-	store.add(pt2)
-	store.add(pt3)
+	store.Add(pt1)
+	store.Add(pt2)
+	store.Add(pt3)
 
 	// THEN
 	assert.Equal(t, 3, len(store.pathtestContexts))
 
-	pt1Ctx := store.pathtestContexts[pt1.getHash()]
-	pt2Ctx := store.pathtestContexts[pt2.getHash()]
-	assert.Equal(t, *pt1, *pt1Ctx.pathtest)
-	assert.Equal(t, *pt2, *pt2Ctx.pathtest)
+	pt1Ctx := store.pathtestContexts[pt1.GetHash()]
+	pt2Ctx := store.pathtestContexts[pt2.GetHash()]
+	assert.Equal(t, *pt1, *pt1Ctx.Pathtest)
+	assert.Equal(t, *pt2, *pt2Ctx.Pathtest)
 }
 
 func Test_pathtestStore_flush(t *testing.T) {
@@ -61,16 +61,16 @@ func Test_pathtestStore_flush(t *testing.T) {
 	runInterval := 1 * time.Minute
 
 	// GIVEN
-	store := newPathtestStore(flushTickerInterval, runDurationFromDisc, runInterval, logger)
+	store := NewPathtestStore(flushTickerInterval, runDurationFromDisc, runInterval, logger)
 
 	// WHEN
 	pt := &common.Pathtest{Hostname: "host1", Port: 53}
-	store.add(pt)
+	store.Add(pt)
 
 	// THEN
 	assert.Equal(t, 1, len(store.pathtestContexts))
 
-	ptCtx := store.pathtestContexts[pt.getHash()]
+	ptCtx := store.pathtestContexts[pt.GetHash()]
 
 	assert.Equal(t, MockTimeNow(), ptCtx.nextRunTime)
 	assert.Equal(t, MockTimeNow().Add(runDurationFromDisc), ptCtx.runUntilTime)
@@ -79,24 +79,24 @@ func Test_pathtestStore_flush(t *testing.T) {
 	flushTime1 := MockTimeNow().Add(10 * time.Second)
 	setMockTimeNow(flushTime1)
 	// TODO: check flush results
-	store.flush()
-	ptCtx = store.pathtestContexts[pt.getHash()]
+	store.Flush()
+	ptCtx = store.pathtestContexts[pt.GetHash()]
 	assert.Equal(t, MockTimeNow().Add(store.pathtestInterval), ptCtx.nextRunTime)
 	assert.Equal(t, MockTimeNow().Add(runDurationFromDisc), ptCtx.runUntilTime)
 
 	// skip flush if nextRunTime is not reached yet
 	flushTime2 := MockTimeNow().Add(20 * time.Second)
 	setMockTimeNow(flushTime2)
-	store.flush()
-	ptCtx = store.pathtestContexts[pt.getHash()]
+	store.Flush()
+	ptCtx = store.pathtestContexts[pt.GetHash()]
 	assert.Equal(t, MockTimeNow().Add(store.pathtestInterval), ptCtx.nextRunTime)
 	assert.Equal(t, MockTimeNow().Add(runDurationFromDisc), ptCtx.runUntilTime)
 
 	// test flush, it should increment nextRunTime
 	flushTime3 := MockTimeNow().Add(70 * time.Second)
 	setMockTimeNow(flushTime3)
-	store.flush()
-	ptCtx = store.pathtestContexts[pt.getHash()]
+	store.Flush()
+	ptCtx = store.pathtestContexts[pt.GetHash()]
 	assert.Equal(t, MockTimeNow().Add(store.pathtestInterval*2), ptCtx.nextRunTime)
 	assert.Equal(t, MockTimeNow().Add(runDurationFromDisc), ptCtx.runUntilTime)
 
@@ -104,30 +104,30 @@ func Test_pathtestStore_flush(t *testing.T) {
 	// it should reset runUntilTime
 	flushTime4 := MockTimeNow().Add(80 * time.Second)
 	setMockTimeNow(flushTime4)
-	store.add(pt)
-	ptCtx = store.pathtestContexts[pt.getHash()]
+	store.Add(pt)
+	ptCtx = store.pathtestContexts[pt.GetHash()]
 	assert.Equal(t, MockTimeNow().Add(store.pathtestInterval*2), ptCtx.nextRunTime)
 	assert.Equal(t, MockTimeNow().Add(runDurationFromDisc+80*time.Second), ptCtx.runUntilTime)
 
 	// test flush, it should increment nextRunTime
 	flushTime5 := MockTimeNow().Add(120 * time.Second)
 	setMockTimeNow(flushTime5)
-	store.flush()
-	ptCtx = store.pathtestContexts[pt.getHash()]
+	store.Flush()
+	ptCtx = store.pathtestContexts[pt.GetHash()]
 	assert.Equal(t, MockTimeNow().Add(store.pathtestInterval*3), ptCtx.nextRunTime)
 	assert.Equal(t, MockTimeNow().Add(runDurationFromDisc+80*time.Second), ptCtx.runUntilTime)
 
 	// test flush before runUntilTime, it should NOT delete Pathtest entry
 	flushTime6 := MockTimeNow().Add((600 + 70) * time.Second)
 	setMockTimeNow(flushTime6)
-	store.flush()
-	ptCtx = store.pathtestContexts[pt.getHash()]
+	store.Flush()
+	ptCtx = store.pathtestContexts[pt.GetHash()]
 	assert.Equal(t, MockTimeNow().Add(store.pathtestInterval*4), ptCtx.nextRunTime)
 	assert.Equal(t, MockTimeNow().Add(runDurationFromDisc+80*time.Second), ptCtx.runUntilTime)
 
 	// test flush after runUntilTime, it should delete Pathtest entry
 	flushTime7 := MockTimeNow().Add((600 + 90) * time.Second)
 	setMockTimeNow(flushTime7)
-	store.flush()
+	store.Flush()
 	assert.Equal(t, 0, len(store.pathtestContexts))
 }
