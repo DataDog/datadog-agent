800c0065a165613b72639b4d98e036757f91bb81
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-05-13 18:22:10 +0200

    refactor

--- comp/networkpath/npscheduler/npschedulerimpl/npscheduler_test.go
+++ comp/networkpath/npscheduler/npschedulerimpl/npscheduler_test.go
@@ -15,14 +15,8 @@ import (
 	"time"
 
 	model "github.com/DataDog/agent-payload/v5/process"
-	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/demultiplexerimpl"
-	"github.com/DataDog/datadog-agent/comp/core"
-	"github.com/DataDog/datadog-agent/comp/core/sysprobeconfig/sysprobeconfigimpl"
-	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/eventplatform"
 	"github.com/DataDog/datadog-agent/comp/forwarder/eventplatform/eventplatformimpl"
-	"github.com/DataDog/datadog-agent/comp/ndmtmp/forwarder/forwarderimpl"
-	"github.com/DataDog/datadog-agent/comp/networkpath/npscheduler"
 	"github.com/DataDog/datadog-agent/comp/networkpath/npscheduler/npschedulerimpl/common"
 	"github.com/DataDog/datadog-agent/pkg/logs/message"
 	"github.com/DataDog/datadog-agent/pkg/networkpath/payload"
@@ -32,47 +26,7 @@ import (
 	"github.com/cihub/seelog"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.uber.org/fx"
-	"go.uber.org/fx/fxtest"
 )
-
-// testOptions is a fx collection of common dependencies for all tests
-var testOptions = fx.Options(
-	Module(),
-	forwarderimpl.MockModule(),
-	demultiplexerimpl.MockModule(),
-	defaultforwarder.MockModule(),
-	core.MockBundle(),
-	eventplatformimpl.MockModule(),
-)
-
-func newTestNpScheduler(t *testing.T, sysConfigs map[string]any) (*fxtest.App, *npSchedulerImpl) {
-	var component npscheduler.Component
-	app := fxtest.New(t, fx.Options(
-		testOptions,
-		fx.Supply(fx.Annotate(t, fx.As(new(testing.TB)))),
-		fx.Replace(sysprobeconfigimpl.MockParams{Overrides: sysConfigs}),
-		fx.Populate(&component),
-	))
-	npScheduler := component.(*npSchedulerImpl)
-
-	require.NotNil(t, npScheduler)
-	require.NotNil(t, app)
-	return app, npScheduler
-}
-
-func createConns(numberOfConns int) []*model.Connection {
-	var conns []*model.Connection
-	for i := 0; i < numberOfConns; i++ {
-		conns = append(conns, &model.Connection{
-			Laddr:     &model.Addr{Ip: fmt.Sprintf("127.0.0.%d", i), Port: int32(30000)},
-			Raddr:     &model.Addr{Ip: fmt.Sprintf("127.0.1.%d", i), Port: int32(80)},
-			Direction: model.ConnectionDirection_outgoing,
-		})
-	}
-	return conns
-}
 
 func Test_NpScheduler_StartAndStop(t *testing.T) {
 	sysConfigs := map[string]any{
@@ -188,7 +142,7 @@ func Test_NpScheduler_runningAndProcessing(t *testing.T) {
 	}
 	npScheduler.ScheduleConns(conns)
 
-	waitForProcessedPathtests(npScheduler, 1)
+	waitForProcessedPathtests(npScheduler, 5*time.Second, 1)
 
 	app.RequireStop()
 }
---
+++ comp/networkpath/npscheduler/npschedulerimpl/npscheduler_testutils.go
@@ -0,0 +1,78 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+//go:build test
+
+package npschedulerimpl
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	model "github.com/DataDog/agent-payload/v5/process"
+	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/demultiplexerimpl"
+	"github.com/DataDog/datadog-agent/comp/core"
+	"github.com/DataDog/datadog-agent/comp/core/sysprobeconfig/sysprobeconfigimpl"
+	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
+	"github.com/DataDog/datadog-agent/comp/forwarder/eventplatform/eventplatformimpl"
+	"github.com/DataDog/datadog-agent/comp/ndmtmp/forwarder/forwarderimpl"
+	"github.com/DataDog/datadog-agent/comp/networkpath/npscheduler"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
+)
+
+// testOptions is a fx collection of common dependencies for all tests
+var testOptions = fx.Options(
+	Module(),
+	forwarderimpl.MockModule(),
+	demultiplexerimpl.MockModule(),
+	defaultforwarder.MockModule(),
+	core.MockBundle(),
+	eventplatformimpl.MockModule(),
+)
+
+func newTestNpScheduler(t *testing.T, sysConfigs map[string]any) (*fxtest.App, *npSchedulerImpl) {
+	var component npscheduler.Component
+	app := fxtest.New(t, fx.Options(
+		testOptions,
+		fx.Supply(fx.Annotate(t, fx.As(new(testing.TB)))),
+		fx.Replace(sysprobeconfigimpl.MockParams{Overrides: sysConfigs}),
+		fx.Populate(&component),
+	))
+	npScheduler := component.(*npSchedulerImpl)
+
+	require.NotNil(t, npScheduler)
+	require.NotNil(t, app)
+	return app, npScheduler
+}
+
+func createConns(numberOfConns int) []*model.Connection {
+	var conns []*model.Connection
+	for i := 0; i < numberOfConns; i++ {
+		conns = append(conns, &model.Connection{
+			Laddr:     &model.Addr{Ip: fmt.Sprintf("127.0.0.%d", i), Port: int32(30000)},
+			Raddr:     &model.Addr{Ip: fmt.Sprintf("127.0.1.%d", i), Port: int32(80)},
+			Direction: model.ConnectionDirection_outgoing,
+		})
+	}
+	return conns
+}
+
+func waitForProcessedPathtests(npScheduler *npSchedulerImpl, timeout time.Duration, processecCount uint64) {
+	timeoutChan := time.After(timeout)
+	tick := time.Tick(100 * time.Millisecond)
+	for {
+		select {
+		case <-timeoutChan:
+			return
+		case <-tick:
+			if npScheduler.processedCount.Load() >= processecCount {
+				return
+			}
+		}
+	}
+}
