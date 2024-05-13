5a48ec37cda3a8abe4d5d5a5a3cf6d98ec371662
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-05-13 12:27:49 +0200

    [CWS] rework mmap hook + rhel 9.3 fix (#25518)
    * [CWS] mmap hooks: use `security_mmap_file` instead of `fget`
    
    * remove `HOOK_SYSCALL_EXIT` on mmap and convert this to a tracepoint hook
    
    * simplify types
    
    * fix mmap types
    
    * read directy into syscall cache
    
    * manual offset

--- pkg/security/ebpf/probes/event_types.go
+++ pkg/security/ebpf/probes/event_types.go
@@ -373,10 +373,10 @@ func GetSelectorsPerEventType(fentry bool) map[eval.EventType][]manager.ProbesSe
 
 		// List of probes required to capture mmap events
 		"mmap": {
-			&manager.BestEffort{Selectors: ExpandSyscallProbesSelector(SecurityAgentUID, "mmap", fentry, Exit)},
 			&manager.AllOf{Selectors: []manager.ProbesSelector{
 				&manager.ProbeSelector{ProbeIdentificationPair: manager.ProbeIdentificationPair{UID: SecurityAgentUID, EBPFFuncName: "tracepoint_syscalls_sys_enter_mmap"}},
-				kretprobeOrFexit("fget"),
+				&manager.ProbeSelector{ProbeIdentificationPair: manager.ProbeIdentificationPair{UID: SecurityAgentUID, EBPFFuncName: "tracepoint_syscalls_sys_exit_mmap"}},
+				kprobeOrFentry("security_mmap_file"),
 			}}},
 
 		// List of probes required to capture mprotect events
