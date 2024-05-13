1a2a8946ee8b21b1e5e9944e180acaf8fe783a6a
Julien Lebot <julien.lebot@datadoghq.com>
2024-05-13 13:54:26 +0200

    progress

--- tasks/winbuildscripts/Generate-OCIPackage.ps1
+++ tasks/winbuildscripts/Generate-OCIPackage.ps1
@@ -11,6 +11,13 @@ if (Test-Path .\omnibus\pkg\$packageName) {
     Remove-Item .\omnibus\pkg\$packageName
 }
 
+# datadog-package takes a folder as input and will package everything in that
+# So we make a dummy folder with a symlink to the installer
+Remove-Item -Recurse -Force C:\oci-pkg -ErrorAction SilentlyContinue
+New-Item C:\oci-pkg
+New-Item -ItemType SymbolicLink -Path C:\oci-pkg\datadog-agent-${rawAgentVersion}-1-x86_64.msi -Target .\omnibus\pkg\datadog-agent-*${rawAgentVersion}-1-x86_64.msi
+
 # The argument --archive-path ".\omnibus\pkg\datadog-agent-${version}.tar.gz" is currently broken and has no effects
-& C:\tools\datadog-package.exe create --package datadog-agent --os windows --arch amd64 --archive --version $rawAgentVersion .\omnibus\pkg\
-Copy-Item datadog-agent-${rawAgentVersion}-windows-amd64.tar .\omnibus\pkg\
+& C:\tools\datadog-package.exe create --package datadog-agent --os windows --arch amd64 --archive --version $rawAgentVersion C:\oci-pkg
+
+Move-Item datadog-agent-${rawAgentVersion}-windows-amd64.tar .\omnibus\pkg\datadog-agent-${rawAgentVersion}-windows-amd64.oci.tar
