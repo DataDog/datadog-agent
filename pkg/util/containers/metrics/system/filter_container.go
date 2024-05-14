1619b898226f27a5da9c2b58978ab988713dd436
AliDatadog <ali.benabdallah@datadoghq.com>
2024-05-14 14:30:40 +0200

    Move trie to its own package

--- pkg/util/containers/metrics/system/filter_container.go
+++ pkg/util/containers/metrics/system/filter_container.go
@@ -13,108 +13,20 @@ import (
 	"github.com/DataDog/datadog-agent/comp/core/workloadmeta"
 	"github.com/DataDog/datadog-agent/pkg/util/cgroups"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/pkg/util/trie"
 )
-
-type trieNode struct {
-	children map[rune]*trieNode
-	cid      string
-}
-
-func newTrieNode() *trieNode {
-	return &trieNode{
-		children: make(map[rune]*trieNode),
-	}
-}
-
-type suffixTrie struct {
-	root *trieNode
-}
-
-func newSuffixTrie() *suffixTrie {
-	return &suffixTrie{
-		root: newTrieNode(),
-	}
-}
-
-func reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
-// Delete deletes a suffix from the suffixTrie
-func (t *suffixTrie) Delete(suffix string) {
-	reversedSuffix := reverse(suffix)
-	t.delete(t.root, reversedSuffix, 0)
-}
-
-func (t *suffixTrie) delete(node *trieNode, suffix string, depth int) bool {
-	if node == nil {
-		return false
-	}
-
-	if len(suffix) == depth {
-		node.cid = ""
-		return len(node.children) == 0
-	}
-
-	char := rune(suffix[depth])
-	if nextNode, ok := node.children[char]; ok {
-		if t.delete(nextNode, suffix, depth+1) {
-			delete(node.children, char)
-			return len(node.children) == 0 && node.cid == ""
-		}
-	}
-	return false
-}
-
-// Insert adds a new suffix to the trie
-func (t *suffixTrie) Insert(suffix, cid string) {
-	reversedSuffix := reverse(suffix)
-	node := t.root
-
-	for _, char := range reversedSuffix {
-		if _, ok := node.children[char]; !ok {
-			node.children[char] = newTrieNode()
-		}
-		node = node.children[char]
-	}
-
-	node.cid = cid
-}
-
-// Get returns the container id for a given suffix
-func (t *suffixTrie) Get(text string) string {
-	reversedText := reverse(text)
-	node := t.root
-
-	for _, char := range reversedText {
-		if node.cid != "" {
-			return node.cid
-		}
-		if next, ok := node.children[char]; ok {
-			node = next
-		} else {
-			return ""
-		}
-	}
-
-	return node.cid
-}
 
 type containerFilter struct {
 	wlm workloadmeta.Component
 
 	mutex sync.RWMutex
-	trie  *suffixTrie
+	trie  *trie.SuffixTrie[string]
 }
 
 // newContainerFilter returns a new container filter
 func newContainerFilter(wlm workloadmeta.Component) *containerFilter {
 	cf := &containerFilter{
-		trie: newSuffixTrie(),
+		trie: trie.NewSuffixTrie[string](),
 		wlm:  wlm,
 	}
 	return cf
@@ -150,7 +62,8 @@ func (cf *containerFilter) handleEvent(ev workloadmeta.Event) {
 	switch ev.Type {
 	case workloadmeta.EventTypeSet:
 		if cont.CgroupPath != "" {
-			cf.trie.Insert(cont.CgroupPath, cont.ID)
+			cid := cont.ID
+			cf.trie.Insert(cont.CgroupPath, &cid)
 		}
 	case workloadmeta.EventTypeUnset:
 		cf.trie.Delete(cont.CgroupPath)
@@ -165,7 +78,10 @@ func (cf *containerFilter) ContainerFilter(path, name string) (string, error) {
 		return res, nil
 	}
 	cf.mutex.RLock()
-	res := cf.trie.Get(path)
+	res, ok := cf.trie.Get(path)
 	cf.mutex.RUnlock()
-	return res, nil
+	if !ok || res == nil {
+		return "", nil
+	}
+	return *res, nil
 }
--- pkg/util/containers/metrics/system/filter_container_test.go
+++ pkg/util/containers/metrics/system/filter_container_test.go
@@ -21,47 +21,6 @@ import (
 	"go.uber.org/fx"
 )
 
-func TestSuffixTrieInsertAndGet(t *testing.T) {
-	trie := newSuffixTrie()
-	cid := "kubelet-kubepods-burstable-pod99dcb84d2a34f7e338778606703258c4.slice/cri-containerd-ec9ea0ad54dd0d96142d5dbe11eb3f1509e12ba9af739620c7b5ad377ce94602"
-	cgroupPath := "/host/sys/fs/cgroup/kubelet.slice/kubelet-kubepods.slice/kubelet-kubepods-burstable.slice/kubelet-kubepods-burstable-pod99dcb84d2a34f7e338778606703258c4.slice/cri-containerd-ec9ea0ad54dd0d96142d5dbe11eb3f1509e12ba9af739620c7b5ad377ce94602.scope"
-	trie.Insert(cgroupPath, cid)
-
-	assert.Equal(t, cid, trie.Get(cgroupPath), "should return correct container id")
-	assert.Equal(t, "", trie.Get("unknown/path"), "should return empty if path is unknown")
-}
-
-func TestSuffixTrieDelete_EmptyKey(t *testing.T) {
-	trie := newSuffixTrie()
-	trie.Delete("")
-	trie.Insert("abc", "value")
-	assert.Equal(t, trie.Get("abc"), "value", "Deleting empty key should allow to insert keys")
-}
-
-func TestSuffixTrieDelete_NonExistentKey(t *testing.T) {
-	trie := newSuffixTrie()
-	trie.Insert("path/to/container", "container123")
-	trie.Delete("nonexistent/key")
-	assert.Equal(t, "container123", trie.Get("path/to/container"), "Existing key unaffected by deleting non-existent key")
-}
-
-func TestSuffixTrieDelete_RootNode(t *testing.T) {
-	trie := newSuffixTrie()
-	trie.Insert("", "rootValue")
-	trie.Delete("")
-	assert.Equal(t, "", trie.Get(""), "Deleting root node should remove the value")
-}
-
-func TestSuffixTrieDelete_PartiallyMatchingKey(t *testing.T) {
-	trie := newSuffixTrie()
-	trie.Insert("path/to/one", "value1")
-	trie.Insert("path/to/one/two", "value2")
-	trie.Insert("path/to/one/two/three", "value3")
-	trie.Delete("path/to/one/two")
-	assert.Equal(t, "value1", trie.Get("path/to/one"), "Existing shorter keys should be unaffected")
-	assert.Equal(t, "value3", trie.Get("path/to/one/two/three"), "Existing longer keys should be unaffected")
-}
-
 func TestHandleSetEvent(t *testing.T) {
 	for _, tt := range []struct {
 		name               string
