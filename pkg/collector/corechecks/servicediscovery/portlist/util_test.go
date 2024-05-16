f32dde6d4ff780ac52f0e19c6cd6b50122069d46
Rodrigo Arguello <rodrigo.arguello@datadoghq.com>
2024-05-16 15:25:10 +0200

    use correct copyright header

--- pkg/collector/corechecks/servicediscovery/portlist/util_test.go
+++ pkg/collector/corechecks/servicediscovery/portlist/util_test.go
@@ -1,3 +1,8 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2014-present Datadog, Inc.
+
 package portlist
 
 import (
