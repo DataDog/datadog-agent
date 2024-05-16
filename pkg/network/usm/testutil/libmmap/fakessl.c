4612cc179eba81f85ad012fbd3ac70350039b862
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2024-05-16 13:06:43 +0200

    Format fakessl.c with clang-format

--- pkg/network/usm/testutil/libmmap/fakessl.c
+++ pkg/network/usm/testutil/libmmap/fakessl.c
@@ -7,12 +7,21 @@
 
 // Dummy library with same functions as OpenSSL.
 
-void SSL_read_ex(void) {}
-void SSL_write_ex(void) {}
-void SSL_do_handshake(void) {}
-void SSL_connect(void) {}
-void SSL_set_bio(void) {}
-void SSL_set_fd(void) {}
-void SSL_read(void) {}
-void SSL_write(void) {}
-void SSL_shutdown(void) {}
+void SSL_read_ex(void) {
+}
+void SSL_write_ex(void) {
+}
+void SSL_do_handshake(void) {
+}
+void SSL_connect(void) {
+}
+void SSL_set_bio(void) {
+}
+void SSL_set_fd(void) {
+}
+void SSL_read(void) {
+}
+void SSL_write(void) {
+}
+void SSL_shutdown(void) {
+}
