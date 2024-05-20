426ab2233d577801aa176ef2545163ca872dfe5d
Yang Song <yang.song@datadoghq.com>
2024-05-20 11:11:13 -0400

    Fix pgproto3 Query

--- pkg/network/usm/postgres_monitor_test.go
+++ pkg/network/usm/postgres_monitor_test.go
@@ -310,7 +310,8 @@ func (s *postgresProtocolParsingSuite) TestCleanupEBPFEntriesOnTermination() {
 	// Encoding a dummy query.
 	output := make([]byte, 0)
 	query := pgproto3.Query{String: "SELECT * FROM dummy"}
-	output = query.Encode(output)
+	output, err = query.Encode(output)
+	require.NoError(t, err)
 
 	// Connecting to the server
 	client, err := net.Dial("tcp", serverAddress)
