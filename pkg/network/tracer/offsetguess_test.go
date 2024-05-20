be83b768ba62df2666189e85cf6e12262f66d458
Adam Karpowich <adam.karpowich@datadoghq.com>
2024-05-20 10:24:48 -0400

    add back log

--- pkg/network/tracer/offsetguess_test.go
+++ pkg/network/tracer/offsetguess_test.go
@@ -304,6 +304,7 @@ func testOffsetGuess(t *testing.T) {
 		var name offsetT = o
 		require.NoError(t, mp.Lookup(&name, &offset))
 		assert.Equal(t, offset, consts[o], "unexpected offset for %s", o)
+		t.Logf("offset %s expected: %d guessed: %d", o, offset, consts[o])
 	}
 }
 
