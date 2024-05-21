a67308134e727126f039c4d779db03219c77143f
Adam Karpowich <adam.karpowich@datadoghq.com>
2024-05-20 23:18:45 -0400

    remove sprint

--- pkg/network/tracer/tracer_test.go
+++ pkg/network/tracer/tracer_test.go
@@ -1425,7 +1425,7 @@ func setupDropTrafficRule(tb testing.TB) (ns string) {
 		nettestutil.IptablesRestore(tb, state)
 	})
 	cmds := []string{
-		fmt.Sprint("iptables -A OUTPUT -p tcp -d 127.0.0.1 --dport 10000 -j DROP"),
+		"iptables -A OUTPUT -p tcp -d 127.0.0.1 --dport 10000 -j DROP",
 	}
 	nettestutil.RunCommands(tb, cmds, false)
 	return
