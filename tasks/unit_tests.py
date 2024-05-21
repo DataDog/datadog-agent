887c3ac4c2bf594afd6dc8fd0b6cf88512ba5fde
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-05-21 10:57:37 +0200

    feat(ci): Wrap the sys.executable to handle potential spaces in the name

--- tasks/unit_tests.py
+++ tasks/unit_tests.py
@@ -8,4 +8,4 @@ def invoke_unit_tests(ctx):
     """
     Run the unit tests on the invoke tasks
     """
-    ctx.run(f"{sys.executable} -m unittest discover -s tasks -p '*_tests.py'", env={"GITLAB_TOKEN": "fake_token"})
+    ctx.run(f"'{sys.executable}' -m unittest discover -s tasks -p '*_tests.py'", env={"GITLAB_TOKEN": "fake_token"})
