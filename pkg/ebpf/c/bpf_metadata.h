480130296453fe2de8745011dcccfffc32ddb3fd
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-05-22 10:30:27 +0200

    Change variable name to avoid conflicts

--- pkg/ebpf/c/bpf_metadata.h
+++ pkg/ebpf/c/bpf_metadata.h
@@ -2,11 +2,11 @@
 #define __BPF_METADATA_H__
 
 #if defined(__x86_64__) || defined(__TARGET_ARCH_x86)
-char _arch[] __attribute__((section("dd_metadata"), used)) = "<arch:amd64>";
+char __dd_metadata_arch[] __attribute__((section("dd_metadata"), used)) = "<arch:amd64>";
 #elif defined(__aarch64__) || defined(__TARGET_ARCH_arm64)
-char _arch[] __attribute__((section("dd_metadata"), used)) = "<arch:arm64>";
+char __dd_metadata_arch[] __attribute__((section("dd_metadata"), used)) = "<arch:arm64>";
 #else
-char _arch[] __attribute__((section("dd_metadata"), used)) = "<arch:unset>";
+char __dd_metadata_arch[] __attribute__((section("dd_metadata"), used)) = "<arch:unset>";
 #endif
 
 #endif
