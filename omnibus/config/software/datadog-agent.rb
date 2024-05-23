7865d2de5e0253437ac2d73109d25fd345cd15ec
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-05-23 10:44:03 +0200

    move the init script in theiw own software definition
    This dill simplify generating what's needed during the packaging stage
    instead of generating everything and removing files during the packaging

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -34,7 +34,6 @@ build do
 
   # set GOPATH on the omnibus source dir for this software
   gopath = Pathname.new(project_dir) + '../../../..'
-  etc_dir = "/etc/datadog-agent"
   if windows_target?
     env = {
         'GOPATH' => gopath.to_path,
@@ -191,90 +190,6 @@ build do
       command "inv agent.generate-config --build-type apm-injection --output-file ./bin/agent/dist/apm-inject.yaml", :env => env
       move 'bin/agent/dist/apm-inject.yaml', "#{conf_dir}/apm-inject.yaml.example"
     end
-  end
-  if linux_target?
-    if debian_target?
-      erb source: "upstart_debian.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_debian.process.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-process.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_debian.sysprobe.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-sysprobe.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_debian.trace.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-trace.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_debian.security.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-security.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "sysvinit_debian.erb",
-          dest: "#{install_dir}/scripts/datadog-agent",
-          mode: 0755,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "sysvinit_debian.process.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-process",
-          mode: 0755,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "sysvinit_debian.trace.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-trace",
-          mode: 0755,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "sysvinit_debian.security.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-security",
-          mode: 0755,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-    elsif redhat_target? || suse_target?
-      # Ship a different upstart job definition on RHEL to accommodate the old
-      # version of upstart (0.6.5) that RHEL 6 provides.
-      erb source: "upstart_redhat.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_redhat.process.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-process.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_redhat.sysprobe.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-sysprobe.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_redhat.trace.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-trace.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-      erb source: "upstart_redhat.security.conf.erb",
-          dest: "#{install_dir}/scripts/datadog-agent-security.conf",
-          mode: 0644,
-          vars: { install_dir: install_dir, etc_dir: etc_dir }
-    end
-
-    erb source: "systemd.service.erb",
-        dest: "#{install_dir}/scripts/datadog-agent.service",
-        mode: 0644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
-    erb source: "systemd.process.service.erb",
-        dest: "#{install_dir}/scripts/datadog-agent-process.service",
-        mode: 0644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
-    erb source: "systemd.sysprobe.service.erb",
-        dest: "#{install_dir}/scripts/datadog-agent-sysprobe.service",
-        mode: 0644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
-    erb source: "systemd.trace.service.erb",
-        dest: "#{install_dir}/scripts/datadog-agent-trace.service",
-        mode: 0644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
-    erb source: "systemd.security.service.erb",
-        dest: "#{install_dir}/scripts/datadog-agent-security.service",
-        mode: 0644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
   end
 
   if osx_target?
---
+++ omnibus/config/software/init-scripts-agent.rb
@@ -0,0 +1,110 @@
+name 'init-scripts-agent'
+
+description "Generate and configure init scripts packaging"
+
+always_build true
+
+build do
+  if linux_target?
+    etc_dir = "/etc/datadog-agent"
+    mkdir "/etc/init"
+    if debian_target?
+      # sysvinit support for debian only for now
+      mkdir "/etc/init.d"
+
+      # debian recommends using a different directory for systemd unit files
+      systemd_directory = "/lib/systemd/system"
+      erb source: "upstart_debian.conf.erb",
+          dest: "/etc/init/datadog-agent.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_debian.process.conf.erb",
+          dest: "/etc/init/datadog-agent-process.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_debian.sysprobe.conf.erb",
+          dest: "/etc/init/datadog-agent-sysprobe.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_debian.trace.conf.erb",
+          dest: "/etc/init/datadog-agent-trace.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_debian.security.conf.erb",
+          dest: "/etc/init/datadog-agent-security.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "sysvinit_debian.erb",
+          dest: "/etc/init.d/datadog-agent",
+          mode: 0755,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "sysvinit_debian.process.erb",
+          dest: "/etc/init.d/datadog-agent-process",
+          mode: 0755,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "sysvinit_debian.trace.erb",
+          dest: "/etc/init.d/datadog-agent-trace",
+          mode: 0755,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "sysvinit_debian.security.erb",
+          dest: "/etc/init.d/datadog-agent-security",
+          mode: 0755,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+    elsif redhat_target? || suse_target?
+      systemd_directory = "/usr/lib/systemd/system"
+      # Ship a different upstart job definition on RHEL to accommodate the old
+      # version of upstart (0.6.5) that RHEL 6 provides.
+      erb source: "upstart_redhat.conf.erb",
+          dest: "/etc/init/datadog-agent.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_redhat.process.conf.erb",
+          dest: "/etc/init/datadog-agent-process.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_redhat.sysprobe.conf.erb",
+          dest: "/etc/init/datadog-agent-sysprobe.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_redhat.trace.conf.erb",
+          dest: "/etc/init/datadog-agent-trace.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+      erb source: "upstart_redhat.security.conf.erb",
+          dest: "/etc/init/datadog-agent-security.conf",
+          mode: 0644,
+          vars: { install_dir: install_dir, etc_dir: etc_dir }
+    end
+    project.extra_package_file '/etc/init/datadog-agent.conf'
+    project.extra_package_file '/etc/init/datadog-agent-process.conf'
+    project.extra_package_file '/etc/init/datadog-agent-sysprobe.conf'
+    project.extra_package_file '/etc/init/datadog-agent-trace.conf'
+    project.extra_package_file '/etc/init/datadog-agent-security.conf'
+
+    erb source: "systemd.service.erb",
+        dest: "#{systemd_directory}/datadog-agent.service",
+        mode: 0644,
+        vars: { install_dir: install_dir, etc_dir: etc_dir }
+    erb source: "systemd.process.service.erb",
+        dest: "#{systemd_directory}/datadog-agent-process.service",
+        mode: 0644,
+        vars: { install_dir: install_dir, etc_dir: etc_dir }
+    erb source: "systemd.sysprobe.service.erb",
+        dest: "#{systemd_directory}/datadog-agent-sysprobe.service",
+        mode: 0644,
+        vars: { install_dir: install_dir, etc_dir: etc_dir }
+    erb source: "systemd.trace.service.erb",
+        dest: "#{systemd_directory}/datadog-agent-trace.service",
+        mode: 0644,
+        vars: { install_dir: install_dir, etc_dir: etc_dir }
+    erb source: "systemd.security.service.erb",
+        dest: "#{systemd_directory}/datadog-agent-security.service",
+        mode: 0644,
+        vars: { install_dir: install_dir, etc_dir: etc_dir }
+  end
+  project.extra_package_file "#{systemd_directory}/datadog-agent.service"
+  project.extra_package_file "#{systemd_directory}/datadog-agent-process.service"
+  project.extra_package_file "#{systemd_directory}/datadog-agent-sysprobe.service"
+  project.extra_package_file "#{systemd_directory}/datadog-agent-trace.service"
+  project.extra_package_file "#{systemd_directory}/datadog-agent-security.service"
+end
