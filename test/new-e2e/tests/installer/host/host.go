09a230fcd8491713072f03157a7bd4166de65032
Arthur Bellal <arthur.bellal@datadoghq.com>
2024-05-23 09:54:36 +0200

    (fleet) skip re-installation of packages that have already been installed (#25818)
    * (fleet) skip re-installation of packages that have already been installed
    
    * remove old code, e2e
    
    * missing sudo
    
    * remove check that is no longer needed
    
    * .

--- test/new-e2e/tests/installer/host/host.go
+++ test/new-e2e/tests/installer/host/host.go
@@ -79,6 +79,12 @@ func (h *Host) ReadFile(path string) ([]byte, error) {
 	return h.remote.ReadFile(path)
 }
 
+// DeletePath deletes a path on the host.
+func (h *Host) DeletePath(path string) {
+	h.remote.MustExecute(fmt.Sprintf("sudo ls %s", path))
+	h.remote.MustExecute(fmt.Sprintf("sudo rm -rf %s", path))
+}
+
 // WaitForUnitActive waits for a systemd unit to be active
 func (h *Host) WaitForUnitActive(units ...string) {
 	for _, unit := range units {
