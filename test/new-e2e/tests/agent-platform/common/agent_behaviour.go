77a7ca8cac85505e5abe6d3d7a103c9628a4a928
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-05-24 17:32:36 +0200

    Skip test if readelf is not available

--- test/new-e2e/tests/agent-platform/common/agent_behaviour.go
+++ test/new-e2e/tests/agent-platform/common/agent_behaviour.go
@@ -351,6 +351,7 @@ func CheckSystemProbeBehavior(t *testing.T, client *TestClient) {
 		_, err = client.Host.Execute("command -v readelf")
 		if err != nil {
 			t.Skip("readelf is not available on the host")
+			return
 		}
 
 		hostArch, err := client.Host.Execute("uname -m")
