e5a142d8eb3399ae97fc2f741e19b4028b5f6648
Julien Lebot <julien.lebot@datadoghq.com>
2024-05-24 10:19:14 +0200

    Only include windows registry on windows

--- pkg/fleet/internal/winregistry/winregistry.go
+++ pkg/fleet/internal/winregistry/winregistry.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build windows
+
 // Package winregistry provides helper functions for interacting with the Windows Registry
 package winregistry
 
