8e4391bc55921bbd0e69dafb8b5da1be2e1cc979
Jeremy Hanna <jeremy-hanna@users.noreply.github.com>
2024-05-24 09:20:20 -0400

    [ASCII-1434]  Migrate /dogstats* and eventplatformreceiver endpoints to providers (#25367)
    Co-authored-by: GustavoCaso <gustavo.caso@datadoghq.com>

---
+++ comp/aggregator/demultiplexerendpoint/mock/mock.go
@@ -0,0 +1,36 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023-present Datadog, Inc.
+
+//go:build test
+
+// Package demultiplexerendpointmock provides a mock component for demultiplexerendpoint
+package demultiplexerendpointmock
+
+import (
+	"net/http"
+
+	"github.com/DataDog/datadog-agent/comp/api/api"
+)
+
+type mock struct {
+}
+
+// ServeHTTP is a simple mocked http.Handler function
+func (m *mock) handlerFunc(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("OK"))
+}
+
+// Provides is the mock component output
+type Provides struct {
+	Endpoint api.AgentEndpointProvider
+}
+
+// NewMock creates a new mock component
+func NewMock() Provides {
+	instance := &mock{}
+	return Provides{
+		Endpoint: api.NewAgentEndpointProvider(instance.handlerFunc, "/dogstatsd-contexts-dump", "POST"),
+	}
+}
