0f74e6c7454d42bef8e6ae24608a81841a57bbcc
Derek Brown <derek.brown@datadoghq.com>
2024-05-24 11:26:54 -0700

    review feedback

--- comp/etw/impl/session.c
+++ comp/etw/impl/session.c
@@ -46,8 +46,8 @@ ULONG DDEnableTrace(
     int eventFilterDescriptorIndex = 0;
     ULONG ret = 0;
 
-    EVENT_FILTER_EVENT_ID * enabledFilters = NULL;
-    EVENT_FILTER_EVENT_ID * disabledFilters = NULL;
+    PEVENT_FILTER_EVENT_ID  enabledFilters = NULL;
+    PEVENT_FILTER_EVENT_ID  disabledFilters = NULL;
     if (PIDCount > 0)
     {
         eventFilterDescriptors[eventFilterDescriptorIndex].Ptr  = (ULONGLONG)PIDs;
@@ -60,7 +60,7 @@ ULONG DDEnableTrace(
 
     if (enableFilterIDCount > 0)
     {
-        ULONG size = sizeof(EVENT_FILTER_EVENT_ID) + (sizeof(USHORT) * enableFilterIDCount);
+        ULONG size = sizeof(EVENT_FILTER_EVENT_ID) + (sizeof(enableFilterIDs[0]) * enableFilterIDCount);
         enabledFilters = (EVENT_FILTER_EVENT_ID*)malloc(size);
 
         enabledFilters->FilterIn = TRUE;
@@ -79,7 +79,7 @@ ULONG DDEnableTrace(
 
     if (disableFilterIDCount > 0)
     {
-        ULONG size = sizeof(EVENT_FILTER_EVENT_ID) + (sizeof(USHORT) * disableFilterIDCount);
+        ULONG size = sizeof(EVENT_FILTER_EVENT_ID) + (sizeof(enableFilterIDs[0]) * disableFilterIDCount);
         disabledFilters = (EVENT_FILTER_EVENT_ID*)malloc(size);
 
         disabledFilters->FilterIn = FALSE;
