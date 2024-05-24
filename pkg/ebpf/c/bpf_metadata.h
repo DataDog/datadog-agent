d4cd323488d37babb65e77084850a9bbc8ac42ef
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-05-24 12:43:21 +0200

    Fix arch name to match uname

--- pkg/ebpf/c/bpf_metadata.h
+++ pkg/ebpf/c/bpf_metadata.h
@@ -8,7 +8,7 @@
 
 #ifndef __CGO__ // Do not include metadata in CGo builds, as it causes duplicate symbols
 #if defined(__x86_64__) || defined(__TARGET_ARCH_x86)
-char __dd_metadata_arch[] __attribute__((section("dd_metadata"), used)) = "<arch:amd64>";
+char __dd_metadata_arch[] __attribute__((section("dd_metadata"), used)) = "<arch:x86_64>";
 #elif defined(__aarch64__) || defined(__TARGET_ARCH_arm64)
 char __dd_metadata_arch[] __attribute__((section("dd_metadata"), used)) = "<arch:arm64>";
 #else
