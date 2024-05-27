e29622d1d30287c09ab91c5780c48ad3987a40df
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-05-27 15:31:27 +0200

    add test for workloadURL

--- cmd/security-agent/subcommands/workloadlist/command_test.go
+++ cmd/security-agent/subcommands/workloadlist/command_test.go
@@ -9,7 +9,10 @@ import (
 	"testing"
 
 	"github.com/DataDog/datadog-agent/cmd/security-agent/command"
+	"github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx"
 )
 
 func TestWorkloadListCommand(t *testing.T) {
@@ -19,4 +22,29 @@ func TestWorkloadListCommand(t *testing.T) {
 		workloadList,
 		func() {},
 	)
+}
+
+func TestWorkloadURL(t *testing.T) {
+	cfg := fxutil.Test[config.Component](t, fx.Options(
+		config.MockModule(),
+		// fx.Replace(config.MockParams{Overrides: overrides}),
+	))
+
+	expected := "https://localhost:5010/agent/workload-list?verbose=true"
+	got, err := workloadURL(cfg, true)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, got)
+
+	cfg = fxutil.Test[config.Component](t, fx.Options(
+		config.MockModule(),
+		fx.Replace(config.MockParams{Overrides: map[string]interface{}{
+			"security_agent.cmd_port": 1234,
+			"cmd_host":                "127.0.0.1",
+		}}),
+	))
+
+	expected = "https://127.0.0.1:1234/agent/workload-list"
+	got, err = workloadURL(cfg, false)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, got)
 }
