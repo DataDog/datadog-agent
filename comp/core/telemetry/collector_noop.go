e2b33ddbf72797cd5e281eab35a905b92efde59d
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2024-05-29 09:52:04 -0400

    Revert "remove serverless build tag types"
    This reverts commit d8ebc910f469dc401949abef094723b16350f528.

--- comp/core/telemetry/collector_noop.go
+++ comp/core/telemetry/collector_noop.go
@@ -3,11 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
-//go:build !serverless
+//go:build serverless
 
 package telemetry
 
-import "github.com/prometheus/client_golang/prometheus"
-
-// Collector is an alias to prometheus.Collector
-type Collector = prometheus.Collector
+type Collector interface{}
