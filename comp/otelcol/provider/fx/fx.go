fa528d2c9437b97745835bea8e5425fda02fec3f
mackjmr <mackenzie.jomard@datadoghq.com>
2024-05-29 15:25:41 +0200

    add package comment

--- comp/otelcol/provider/fx/fx.go
+++ comp/otelcol/provider/fx/fx.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
+// Package providerfx provides fx access for the provider component
 package providerfx
 
 import (
