65cccf20d553082bef7dedcf2de36075104e1634
Guillermo Julián <gjulianm@users.noreply.github.com>
2024-05-30 17:56:21 +0200

    [EBPF] Test correct packaging of eBPF files (#25727)

--- pkg/ebpf/c/kconfig.h
+++ pkg/ebpf/c/kconfig.h
@@ -1,6 +1,8 @@
 #ifndef __KCONFIG_H
 #define __KCONFIG_H
 
+#include "bpf_metadata.h"
+
 #include <linux/kconfig.h>
 // include asm/compiler.h to fix `error: expected string literal in 'asm'` compilation error coming from mte-kasan.h
 // this was fixed in https://github.com/torvalds/linux/commit/b859ebedd1e730bbda69142fca87af4e712649a1
