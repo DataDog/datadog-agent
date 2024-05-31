703cf0b39cf38bd9090fc3c5066cc7522c15764e
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2024-05-31 18:37:39 +0200

    NPM testE2E WKit use S3 bucket for external tools (#26193)

--- test/new-e2e/tests/npm/ec2_1host_wkit_test.go
+++ test/new-e2e/tests/npm/ec2_1host_wkit_test.go
@@ -32,8 +32,8 @@ func TestEC2VMWKitSuite(t *testing.T) {
 func (v *ec2VMWKitSuite) SetupSuite() {
 	v.BaseSuite.SetupSuite()
 
-	v.Env().RemoteHost.MustExecute("Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))")
-	v.Env().RemoteHost.MustExecute("C:\\ProgramData\\chocolatey\\choco install /y apache-httpd --params '\"/noService\"'")
+	v.Env().RemoteHost.MustExecute("Invoke-WebRequest -UseBasicParsing http://s3.amazonaws.com/dd-agent-mstesting/windows/pvt/nplanel/httpd-2.4.59-240404-win64-VS17.zip -OutFile httpd.zip")
+	v.Env().RemoteHost.MustExecute("Expand-Archive httpd.zip")
 }
 
 // BeforeTest will be called before each test
@@ -75,7 +75,7 @@ func (v *ec2VMWKitSuite) TestFakeIntakeNPM600cnxBucket_HostRequests() {
 	testURL := "http://" + v.Env().HTTPBinHost.Address + "/"
 
 	// generate connections
-	v.Env().RemoteHost.MustExecute("C:\\Users\\Administrator\\AppData\\Roaming\\Apache24\\bin\\ab.exe -n 600 -c 600 " + testURL)
+	v.Env().RemoteHost.MustExecute("C:\\Users\\Administrator\\httpd\\Apache24\\bin\\ab.exe -n 600 -c 600 " + testURL)
 
 	test1HostFakeIntakeNPM600cnxBucket(&v.BaseSuite, v.Env().FakeIntake)
 }
