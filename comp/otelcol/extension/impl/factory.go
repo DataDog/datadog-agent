557ff17b22b8036fd043b36cb29ba25f45cf64d1
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2024-06-03 15:22:56 +0200

    [otel-agent][extension] more extension work

--- comp/otelcol/extension/impl/factory.go
+++ comp/otelcol/extension/impl/factory.go
@@ -2,13 +2,13 @@ package impl
 
 import (
 	"context"
+	"fmt"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/extension"
 
 	"github.com/DataDog/datadog-agent/comp/otelcol/extension/impl/internal/metadata"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/common/localhostgate"
 )
 
 const (
@@ -28,12 +28,12 @@ func NewFactory() extension.Factory {
 func createDefaultConfig() component.Config {
 	return &Config{
 		HTTPConfig: &confighttp.ServerConfig{
-			Endpoint: localhostgate.EndpointForPort(defaultHTTPPort),
+			Endpoint: fmt.Sprintf("localhost:%d", defaultHTTPPort),
 		},
 	}
 }
 
 // Create the extension instance
 func createExtension(ctx context.Context, set extension.CreateSettings, cfg component.Config) (extension.Extension, error) {
-	return newDDHTTPExtension(ctx, cfg.(*Config), set.TelemetrySettings), nil
+	return newDDHTTPExtension(ctx, cfg.(*Config), set.TelemetrySettings)
 }
