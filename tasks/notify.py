b79d4a241010dc2fdbde9dd87908b85a49ddc001
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-06-06 14:27:35 +0200

    Update slack [skip ci]

--- tasks/notify.py
+++ tasks/notify.py
@@ -483,7 +483,7 @@ def send_failure_summary_notification(
         args = os.environ["ARGS"]
         args = json.loads(args)
         jobs = args['jobs']
-        allowed_to_fail = not args['allowedToFail']
+        allowed_to_fail = args['allowedToFail']
 
     # List of (job_name, failure_count) ordered by failure_count
     stats = sorted(
@@ -523,16 +523,22 @@ def send_failure_summary_notification(
         timestamp_start = int((datetime.now() - delta).timestamp() * 1000)
         timestamp_end = int(datetime.now().timestamp() * 1000)
 
-        header = f'*{period} Job Failure Report*\nTO: *{channel}*\nThese jobs{you_own} had the most failures in the last {duration}:'
+        header = f'{period} Job Failure Report'
+        # TODO
+        description = f'TO: *{channel}*\nThese jobs{you_own} had the most failures in the last {duration}:'
         footer = f'{expected_to_fail}. Click <https://app.datadoghq.com/ci/pipeline-executions?query=ci_level%3Ajob%20env%3Aprod%20%40git.repository.id%3A%22gitlab.ddbuild.io%2FDataDog%2Fdatadog-agent%22%20%40ci.pipeline.name%3A%22DataDog%2Fdatadog-agent%22%20%40ci.provider.instance%3Agitlab-ci%20%40git.branch%3Amain%20%40ci.status%3Aerror%20%40gitlab.pipeline_source%3A%28push%20OR%20schedule%29%20{not_allowed_query}%40ci.allowed_to_fail%3Atrue&agg_m=count&agg_m_source=base&agg_q=%40ci.job.name&start={timestamp_start}&end={timestamp_end}&agg_q_source=base&agg_t=count&fromUser=false&index=cipipeline&sort_m=count&sort_m_source=base&sort_t=count&top_n=25&top_o=top&viz=toplist&x_missing=true&paused=false|here> for more details.{flaky_tests}'
-        messages = [header, '\n'.join(message), footer]
+
+        blocks = [
+            {'type': 'header', 'text': {'type': 'plain_text', 'text': header}},
+            {'type': 'section', 'text': {'type': 'mrkdwn', 'text': '\n'.join(message)}},
+            {'type': 'section', 'text': {'type': 'mrkdwn', 'text': footer}},
+        ]
 
         # Send message
         client = WebClient(os.environ["SLACK_API_TOKEN"])
-        # TODO : Updated tests
-        for message in messages:
-            # client.chat_postMessage(channel=channel, text=message)
-            client.chat_postMessage(channel="#celian-tests", text=message)
+        # TODO : Update tests
+        # client.chat_postMessage(channel=channel, blocks=blocks)
+        client.chat_postMessage(channel="#celian-tests", blocks=blocks)
 
     for channel, stat in team_stats.items():
         send_summary(channel, stat)
