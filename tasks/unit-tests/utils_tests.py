43a776ff6b1e171fed81c6a77df997e89ece73ff
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-06-10 18:04:19 +0200

    add tests

--- tasks/unit-tests/utils_tests.py
+++ tasks/unit-tests/utils_tests.py
@@ -88,11 +88,12 @@ class TestQueryVersion(unittest.TestCase):
         self.assertEqual(g, "315e3a2")
 
     @patch.dict(os.environ, {"BUCKET_BRANCH": "nightly"}, clear=True)
+    @patch("tasks.libs.common.utils.get_git_branch_name", new=MagicMock(return_value="main"))
     def test_on_nightly_bucket(self):
         major_version = "7"
         c = MockContext(
             run={
-                rf"git tag --list | grep -E '^{major_version}\.[0-9]+\.[0-9]+(-rc.*|-devel.*)?$' | sort -rV | head -1": Result(
+                rf"git tag --list --merged main | grep -E '^{major_version}\.[0-9]+\.[0-9]+(-rc.*|-devel.*)?$' | sort -rV | head -1": Result(
                     "7.55.0-devel"
                 ),
                 'git describe --tags --candidates=50 --match "7.55.0-devel" --abbrev=7': Result(
@@ -106,11 +107,12 @@ class TestQueryVersion(unittest.TestCase):
         self.assertEqual(c, 543)
         self.assertEqual(g, "315e3a2")
 
+    @patch("tasks.libs.common.utils.get_git_branch_name", new=MagicMock(return_value="7.55.x"))
     def test_on_release(self):
         major_version = "7"
         c = MockContext(
             run={
-                rf"git tag --list | grep -E '^{major_version}\.[0-9]+\.[0-9]+(-rc.*|-devel.*)?$' | sort -rV | head -1": Result(
+                rf"git tag --list --merged 7.55.x | grep -E '^{major_version}\.[0-9]+\.[0-9]+(-rc.*|-devel.*)?$' | sort -rV | head -1": Result(
                     "7.55.0-devel"
                 ),
                 'git describe --tags --candidates=50 --match "7.55.0-devel" --abbrev=7': Result(
