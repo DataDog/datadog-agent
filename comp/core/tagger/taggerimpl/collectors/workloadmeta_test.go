fba01b2d9310c6a4c6022501af231cef12b239bf
Wassim DHIF <wassim.dhif@datadoghq.com>
2024-06-10 11:52:33 +0200

    chore(opentelemetry): add malformed OTEL_RESOURCE_ATTRIBUTES unit test
    Signed-off-by: Wassim DHIF <wassim.dhif@datadoghq.com>

--- comp/core/tagger/taggerimpl/collectors/workloadmeta_test.go
+++ comp/core/tagger/taggerimpl/collectors/workloadmeta_test.go
@@ -1129,6 +1129,41 @@ func TestHandleContainer(t *testing.T) {
 			},
 		},
 		{
+			name: "tags from environment with malformed OTEL_RESOURCE_ATTRIBUTES",
+			container: workloadmeta.Container{
+				EntityID: entityID,
+				EntityMeta: workloadmeta.EntityMeta{
+					Name: containerName,
+				},
+				EnvVars: map[string]string{
+					// env as tags
+					"TEAM": "container-integrations",
+					"TIER": "node",
+
+					// otel standard tags
+					"OTEL_RESOURCE_ATTRIBUTES": fmt.Sprintf("service.name=,  =  , =%s", env),
+				},
+			},
+			envAsTags: map[string]string{
+				"team": "owner_team",
+			},
+			expected: []*types.TagInfo{
+				{
+					Source: containerSource,
+					Entity: taggerEntityID,
+					HighCardTags: []string{
+						fmt.Sprintf("container_name:%s", containerName),
+						fmt.Sprintf("container_id:%s", entityID.ID),
+					},
+					OrchestratorCardTags: []string{},
+					LowCardTags: []string{
+						"owner_team:container-integrations",
+					},
+					StandardTags: []string{},
+				},
+			},
+		},
+		{
 			name: "tags from labels",
 			container: workloadmeta.Container{
 				EntityID: entityID,
