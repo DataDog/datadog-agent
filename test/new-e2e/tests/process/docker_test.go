45691d566cee91e1dc2ff193ed55d9f50dc1989b
William Yu <william.yu@datadoghq.com>
2024-06-10 16:35:08 -0400

    update tests

--- test/new-e2e/tests/process/docker_test.go
+++ test/new-e2e/tests/process/docker_test.go
@@ -31,7 +31,7 @@ func TestDockerTestSuite(t *testing.T) {
 		dockeragentparams.WithAgentServiceEnvVariable("DD_PROCESS_CONFIG_CONTAINER_COLLECTION_ENABLED", pulumi.StringPtr("false")),
 		dockeragentparams.WithAgentServiceEnvVariable("DD_PROCESS_CONFIG_PROCESS_DISCOVERY_ENABLED", pulumi.StringPtr("false")),
 
-		dockeragentparams.WithExtraComposeManifest("stress", pulumi.String(stressCompose)),
+		dockeragentparams.WithExtraComposeManifest("fakeProcess", pulumi.String(fakeProcessCompose)),
 	}
 
 	options := []e2e.SuiteOption{
@@ -65,9 +65,8 @@ func (s *dockerTestSuite) TestDockerProcessCheck() {
 		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
 	}, 2*time.Minute, 10*time.Second)
 
-	assertProcessCollected(t, payloads, false, "stress")
-
-	// TODO: assert containers collected
+	assertProcessCollected(t, payloads, false, "dd")
+	assertContainersCollected(t, payloads, []string{"fake-process"})
 }
 
 func (s *dockerTestSuite) TestProcessDiscoveryCheck() {
@@ -77,7 +76,7 @@ func (s *dockerTestSuite) TestProcessDiscoveryCheck() {
 		dockeragentparams.WithAgentServiceEnvVariable("DD_PROCESS_CONFIG_CONTAINER_COLLECTION_ENABLED", pulumi.StringPtr("false")),
 		dockeragentparams.WithAgentServiceEnvVariable("DD_PROCESS_CONFIG_PROCESS_DISCOVERY_ENABLED", pulumi.StringPtr("true")),
 
-		dockeragentparams.WithExtraComposeManifest("stress", pulumi.String(stressCompose)),
+		dockeragentparams.WithExtraComposeManifest("fakeProcess", pulumi.String(fakeProcessCompose)),
 	}
 
 	s.UpdateEnv(awsdocker.Provisioner(awsdocker.WithAgentOptions(agentOpts...)))
@@ -94,131 +93,90 @@ func (s *dockerTestSuite) TestProcessDiscoveryCheck() {
 		assert.NotEmpty(c, payloads, "no process discovery payloads returned")
 	}, 2*time.Minute, 10*time.Second)
 
-	assertProcessDiscoveryCollected(t, payloads, "stress")
+	assertProcessDiscoveryCollected(t, payloads, "dd")
 }
 
-//
-//func (s *dockerTestSuite) TestProcessCheckWithIO() {
-//	t := s.T()
-//	s.UpdateEnv(awshost.Provisioner(awshost.WithAgentOptions(agentparams.WithAgentConfig(processCheckConfigStr), agentparams.WithSystemProbeConfig(systemProbeConfigStr))))
-//
-//	// Flush fake intake to remove payloads that won't have IO stats
-//	s.Env().FakeIntake.Client().FlushServerAndResetAggregators()
-//
-//	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
-//		assertRunningChecks(collect, s.Env().RemoteHost, []string{"process", "rtprocess"}, true, "sudo datadog-agent status --json")
-//	}, 1*time.Minute, 5*time.Second)
-//
-//	var payloads []*aggregator.ProcessPayload
-//	assert.EventuallyWithT(t, func(c *assert.CollectT) {
-//		var err error
-//		payloads, err = s.Env().FakeIntake.Client().GetProcesses()
-//		assert.NoError(c, err, "failed to get process payloads from fakeintake")
-//
-//		// Wait for two payloads, as processes must be detected in two check runs to be returned
-//		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
-//	}, 2*time.Minute, 10*time.Second)
-//
-//	assertProcessCollected(t, payloads, true, "stress")
-//}
-//
-//func (s *dockerTestSuite) TestProcessChecksInCoreAgent() {
-//	t := s.T()
-//	s.UpdateEnv(awshost.Provisioner(awshost.WithAgentOptions(agentparams.WithAgentConfig(processCheckInCoreAgentConfigStr))))
-//
-//	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
-//		assertRunningChecks(collect, s.Env().RemoteHost, []string{}, false, "sudo datadog-agent status --json")
-//	}, 1*time.Minute, 5*time.Second)
-//
-//	// Verify that the process agent is not running
-//	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
-//		status := s.Env().RemoteHost.MustExecute("/opt/datadog-agent/embedded/bin/process-agent status")
-//		assert.Contains(t, status, "The Process Agent is not running")
-//	}, 1*time.Minute, 5*time.Second)
-//
-//	// Flush fake intake to remove any payloads which may have
-//	s.Env().FakeIntake.Client().FlushServerAndResetAggregators()
-//
-//	var payloads []*aggregator.ProcessPayload
-//	assert.EventuallyWithT(t, func(c *assert.CollectT) {
-//		var err error
-//		payloads, err = s.Env().FakeIntake.Client().GetProcesses()
-//		assert.NoError(c, err, "failed to get process payloads from fakeintake")
-//
-//		// Wait for two payloads, as processes must be detected in two check runs to be returned
-//		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
-//	}, 2*time.Minute, 10*time.Second)
-//
-//	assertProcessCollected(t, payloads, false, "stress")
-//
-//	// check that the process agent is not collected as it should not be running
-//	requireProcessNotCollected(t, payloads, "process-agent")
-//}
-//
-//func (s *dockerTestSuite) TestProcessChecksInCoreAgentWithNPM() {
-//	t := s.T()
-//	s.UpdateEnv(awshost.Provisioner(awshost.WithAgentOptions(agentparams.WithAgentConfig(processCheckInCoreAgentConfigStr), agentparams.WithSystemProbeConfig(systemProbeNPMConfigStr))))
-//
-//	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
-//		assertRunningChecks(collect, s.Env().RemoteHost, []string{"connections"}, false, "sudo datadog-agent status --json")
-//	}, 1*time.Minute, 5*time.Second)
-//
-//	// Flush fake intake to remove any payloads which may have
-//	s.Env().FakeIntake.Client().FlushServerAndResetAggregators()
-//
-//	var payloads []*aggregator.ProcessPayload
-//	assert.EventuallyWithT(t, func(c *assert.CollectT) {
-//		var err error
-//		payloads, err = s.Env().FakeIntake.Client().GetProcesses()
-//		assert.NoError(c, err, "failed to get process payloads from fakeintake")
-//
-//		// Wait for two payloads, as processes must be detected in two check runs to be returned
-//		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
-//	}, 2*time.Minute, 10*time.Second)
-//
-//	assertProcessCollected(t, payloads, false, "stress")
-//}
-//
-//func (s *dockerTestSuite) TestProcessChecksWithNPM() {
-//	t := s.T()
-//	s.UpdateEnv(awshost.Provisioner(awshost.WithAgentOptions(agentparams.WithAgentConfig(processCheckConfigStr), agentparams.WithSystemProbeConfig(systemProbeNPMConfigStr))))
-//
-//	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
-//		assertRunningChecks(collect, s.Env().RemoteHost, []string{"process", "rtprocess", "connections"}, false, "sudo datadog-agent status --json")
-//	}, 1*time.Minute, 5*time.Second)
-//
-//	// Flush fake intake to remove any payloads which may have
-//	s.Env().FakeIntake.Client().FlushServerAndResetAggregators()
-//
-//	var payloads []*aggregator.ProcessPayload
-//	assert.EventuallyWithT(t, func(c *assert.CollectT) {
-//		var err error
-//		payloads, err = s.Env().FakeIntake.Client().GetProcesses()
-//		assert.NoError(c, err, "failed to get process payloads from fakeintake")
-//
-//		// Wait for two payloads, as processes must be detected in two check runs to be returned
-//		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
-//	}, 2*time.Minute, 10*time.Second)
-//
-//	assertProcessCollected(t, payloads, false, "stress")
-//}
-//
-//func (s *dockerTestSuite) TestManualProcessCheck() {
-//	check := s.Env().RemoteHost.MustExecute("sudo /opt/datadog-agent/embedded/bin/process-agent check process --json")
-//
-//	assertManualProcessCheck(s.T(), check, false, "stress")
-//}
-//
-//func (s *dockerTestSuite) TestManualProcessDiscoveryCheck() {
-//	check := s.Env().RemoteHost.MustExecute("sudo /opt/datadog-agent/embedded/bin/process-agent check process_discovery --json")
-//
-//	assertManualProcessDiscoveryCheck(s.T(), check, "stress")
-//}
-//
-//func (s *dockerTestSuite) TestManualProcessCheckWithIO() {
-//	s.UpdateEnv(awshost.Provisioner(awshost.WithAgentOptions(agentparams.WithAgentConfig(processCheckConfigStr), agentparams.WithSystemProbeConfig(systemProbeConfigStr))))
-//
-//	check := s.Env().RemoteHost.MustExecute("sudo /opt/datadog-agent/embedded/bin/process-agent check process --json")
-//
-//	assertManualProcessCheck(s.T(), check, true, "stress")
-//}
+func (s *dockerTestSuite) TestProcessCheckWithIO() {
+	t := s.T()
+	agentOpts := []dockeragentparams.Option{
+		dockeragentparams.WithAgentServiceEnvVariable("DD_PROCESS_CONFIG_PROCESS_COLLECTION_ENABLED", pulumi.StringPtr("true")),
+		dockeragentparams.WithAgentServiceEnvVariable("DD_SYSTEM_PROBE_PROCESS_ENABLED", pulumi.StringPtr("true")),
+	}
+	s.UpdateEnv(awsdocker.Provisioner(awsdocker.WithAgentOptions(agentOpts...)))
+
+	// Flush fake intake to remove payloads that won't have IO stats
+	s.Env().FakeIntake.Client().FlushServerAndResetAggregators()
+
+	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
+		assertRunningChecks(collect, s.Env().Agent.Client, []string{"process", "rtprocess"}, true)
+	}, 1*time.Minute, 5*time.Second)
+
+	var payloads []*aggregator.ProcessPayload
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		var err error
+		payloads, err = s.Env().FakeIntake.Client().GetProcesses()
+		assert.NoError(c, err, "failed to get process payloads from fakeintake")
+
+		// Wait for two payloads, as processes must be detected in two check runs to be returned
+		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
+	}, 2*time.Minute, 10*time.Second)
+
+	assertProcessCollected(t, payloads, true, "dd")
+}
+
+func (s *dockerTestSuite) TestProcessChecksWithNPM() {
+	t := s.T()
+	agentOpts := []dockeragentparams.Option{
+		dockeragentparams.WithAgentServiceEnvVariable("DD_PROCESS_CONFIG_PROCESS_COLLECTION_ENABLED", pulumi.StringPtr("true")),
+		dockeragentparams.WithAgentServiceEnvVariable("DD_SYSTEM_PROBE_NETWORK_ENABLED", pulumi.StringPtr("true")),
+		dockeragentparams.WithExtraComposeManifest("fakeProcess", pulumi.String(fakeProcessCompose)),
+	}
+	s.UpdateEnv(awsdocker.Provisioner(awsdocker.WithAgentOptions(agentOpts...)))
+
+	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
+		assertRunningChecks(collect, s.Env().Agent.Client, []string{"process", "rtprocess", "connections"}, false)
+	}, 1*time.Minute, 5*time.Second)
+
+	// Flush fake intake to ensure a clean slate
+	s.Env().FakeIntake.Client().FlushServerAndResetAggregators()
+
+	var payloads []*aggregator.ProcessPayload
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		var err error
+		payloads, err = s.Env().FakeIntake.Client().GetProcesses()
+		assert.NoError(c, err, "failed to get process payloads from fakeintake")
+
+		// Wait for two payloads, as processes must be detected in two check runs to be returned
+		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
+	}, 2*time.Minute, 10*time.Second)
+
+	assertProcessCollected(t, payloads, false, "dd")
+	assertContainersCollected(t, payloads, []string{"fake-process"})
+}
+
+func (s *dockerTestSuite) TestManualProcessCheck() {
+	check := s.Env().Docker.Client.ExecuteCommand(s.Env().Agent.ContainerName,
+		"process-agent", "check", "process", "--json")
+
+	assertManualProcessCheck(s.T(), check, false, "dd", "fake-process")
+}
+
+func (s *dockerTestSuite) TestManualProcessDiscoveryCheck() {
+	check := s.Env().Docker.Client.ExecuteCommand(s.Env().Agent.ContainerName,
+		"process-agent", "check", "process_discovery", "--json")
+
+	assertManualProcessDiscoveryCheck(s.T(), check, "dd")
+}
+
+func (s *dockerTestSuite) TestManualProcessCheckWithIO() {
+	agentOpts := []dockeragentparams.Option{
+		dockeragentparams.WithAgentServiceEnvVariable("DD_PROCESS_CONFIG_PROCESS_COLLECTION_ENABLED", pulumi.StringPtr("true")),
+		dockeragentparams.WithAgentServiceEnvVariable("DD_SYSTEM_PROBE_PROCESS_ENABLED", pulumi.StringPtr("true")),
+	}
+	s.UpdateEnv(awsdocker.Provisioner(awsdocker.WithAgentOptions(agentOpts...)))
+
+	check := s.Env().Docker.Client.ExecuteCommand(s.Env().Agent.ContainerName,
+		"process-agent", "check", "process", "--json")
+
+	assertManualProcessCheck(s.T(), check, true, "dd")
+}
