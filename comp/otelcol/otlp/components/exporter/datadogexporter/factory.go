e2b59f586e74c5d6f419bf43e43863846283a8d3
Yang Song <yang.song@datadoghq.com>
2024-06-10 14:30:32 -0400

    [OASIS-12] Allow serializerexporter to consume APM stats

--- comp/otelcol/otlp/components/exporter/datadogexporter/factory.go
+++ comp/otelcol/otlp/components/exporter/datadogexporter/factory.go
@@ -8,6 +8,8 @@ package datadogexporter
 
 import (
 	"context"
+	"fmt"
+	"runtime"
 	"sync"
 	"time"
 
@@ -16,7 +18,9 @@ import (
 	"github.com/DataDog/datadog-agent/comp/otelcol/otlp/components/exporter/logsagentexporter"
 	"github.com/DataDog/datadog-agent/comp/otelcol/otlp/components/exporter/serializerexporter"
 	"github.com/DataDog/datadog-agent/pkg/logs/message"
+	tracepb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/trace"
 	"github.com/DataDog/datadog-agent/pkg/serializer"
+
 	"github.com/DataDog/opentelemetry-mapping-go/pkg/otlp/attributes"
 	otlpmetrics "github.com/DataDog/opentelemetry-mapping-go/pkg/otlp/metrics"
 	"go.opentelemetry.io/collector/component"
@@ -26,6 +30,7 @@ import (
 	"go.opentelemetry.io/collector/exporter"
 	"go.opentelemetry.io/collector/exporter/exporterhelper"
 	"go.opentelemetry.io/collector/featuregate"
+	"google.golang.org/protobuf/proto"
 
 	"go.uber.org/zap"
 )
@@ -39,6 +44,8 @@ type factory struct {
 	s         serializer.MetricSerializer
 	logsAgent logsagentpipeline.Component
 	h         hostnameinterface.Component
+
+	wg sync.WaitGroup // waits for agent to exit
 }
 
 func (f *factory) AttributesTranslator(set component.TelemetrySettings) (*attributes.Translator, error) {
@@ -169,7 +176,10 @@ func (f *factory) createMetricsExporter(
 	c component.Config,
 ) (exporter.Metrics, error) {
 	cfg := checkAndCastConfig(c, set.Logger)
-	sf := serializerexporter.NewFactory(f.s, &tagEnricher{}, f.h.Get)
+	statsIn := make(chan []byte, 1000)
+	statsv := set.BuildInfo.Command + set.BuildInfo.Version
+	f.consumeStatsPayload(ctx, statsIn, statsv, fmt.Sprintf("datadogexporter-%s-%s", set.BuildInfo.Command, set.BuildInfo.Version), set.Logger)
+	sf := serializerexporter.NewFactory(f.s, &tagEnricher{}, f.h.Get, statsIn)
 	ex := &serializerexporter.ExporterConfig{
 		Metrics: cfg.Metrics,
 		TimeoutSettings: exporterhelper.TimeoutSettings{
@@ -178,6 +188,38 @@ func (f *factory) createMetricsExporter(
 		QueueSettings: cfg.QueueSettings,
 	}
 	return sf.CreateMetricsExporter(ctx, set, ex)
+}
+
+func (f *factory) consumeStatsPayload(ctx context.Context, statsIn <-chan []byte, tracerVersion string, agentVersion string, logger *zap.Logger) {
+	for i := 0; i < runtime.NumCPU(); i++ {
+		f.wg.Add(1)
+		go func() {
+			defer f.wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case msg := <-statsIn:
+					sp := &tracepb.StatsPayload{}
+
+					err := proto.Unmarshal(msg, sp)
+					if err != nil {
+						logger.Error("failed to unmarshal stats payload", zap.Error(err))
+						continue
+					}
+					for _, csp := range sp.Stats {
+						if csp.TracerVersion == "" {
+							csp.TracerVersion = tracerVersion
+						}
+					}
+					// The DD Connector doesn't set the agent version, so we'll set it here
+					sp.AgentVersion = agentVersion
+
+					// TODO(OASIS-12): send StatsPayload with trace agent
+				}
+			}
+		}()
+	}
 }
 
 // createLogsExporter creates a logs exporter based on the config.
