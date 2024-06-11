df5262f9fba6784cbcae048a3858e50252bc8274
Baptiste Foy <baptiste.foy@datadoghq.com>
2024-06-11 11:59:23 +0200

    upgrade(installer): Make installer extract tmp dir owned by dd-agent

--- pkg/fleet/installer/service/datadog_installer.go
+++ pkg/fleet/installer/service/datadog_installer.go
@@ -96,6 +96,10 @@ func SetupInstaller(ctx context.Context) (err error) {
 	if err != nil {
 		return fmt.Errorf("error changing owner of /var/run/datadog-installer: %w", err)
 	}
+	err = os.Chown("/opt/datadog-installer/tmp", ddAgentUID, ddAgentGID)
+	if err != nil {
+		return fmt.Errorf("failed to change owner of temporary directory: %w", err)
+	}
 
 	// Create installer path symlink
 	err = os.Symlink("/opt/datadog-packages/datadog-installer/stable/bin/installer/installer", "/usr/bin/datadog-installer")
