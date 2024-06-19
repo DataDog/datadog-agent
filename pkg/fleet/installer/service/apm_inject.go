9b99ef437a92d2a889bf875ae58b26a08a837df8
Baptiste Foy <baptiste.foy@datadoghq.com>
2024-06-19 17:59:06 +0200

    fix(installer): Few fixes for injector installation (#26876)

--- pkg/fleet/installer/service/apm_inject.go
+++ pkg/fleet/installer/service/apm_inject.go
@@ -141,7 +141,7 @@ func (a *apmInjectorInstaller) Setup(ctx context.Context) error {
 		return err
 	}
 
-	// /var/log/datadog is created by default with datadog-installer install
+	// Create mandatory dirs
 	err = os.Mkdir("/var/log/datadog/dotnet", 0777)
 	if err != nil && !os.IsExist(err) {
 		return fmt.Errorf("error creating /var/log/datadog/dotnet: %w", err)
@@ -150,6 +150,10 @@ func (a *apmInjectorInstaller) Setup(ctx context.Context) error {
 	err = os.Chmod("/var/log/datadog/dotnet", 0777)
 	if err != nil {
 		return fmt.Errorf("error changing permissions on /var/log/datadog/dotnet: %w", err)
+	}
+	err = os.Mkdir("/etc/datadog-agent/inject", 0755)
+	if err != nil && !os.IsExist(err) {
+		return fmt.Errorf("error creating /etc/datadog-agent/inject: %w", err)
 	}
 
 	err = a.addInstrumentScripts(ctx)
