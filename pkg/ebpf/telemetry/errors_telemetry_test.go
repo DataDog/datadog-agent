dfe1738111b0105290c7873669a6fc89fb20ba9f
usamasaqib <usamasaqib.96@live.com>
2024-06-20 17:29:36 +0200

    remove unused test helpers

--- pkg/ebpf/telemetry/errors_telemetry_test.go
+++ pkg/ebpf/telemetry/errors_telemetry_test.go
@@ -8,7 +8,6 @@ import (
 	"golang.org/x/sys/unix"
 
 	"github.com/DataDog/datadog-agent/pkg/ebpf/bytecode"
-	"github.com/DataDog/datadog-agent/pkg/util/log"
 	manager "github.com/DataDog/ebpf-manager"
 	"github.com/prometheus/client_golang/prometheus"
 	dto "github.com/prometheus/client_model/go"
@@ -38,54 +37,6 @@ var m = &manager.Manager{
 			Name: "suppress_map",
 		},
 	},
-}
-
-func getMapsTelemetry(e *EBPFTelemetry) map[string]interface{} {
-	t := make(map[string]interface{})
-	if e.mapErrMap == nil {
-		return t
-	}
-
-	var val mapErrTelemetry
-	for m, k := range e.mapKeys {
-		err := e.mapErrMap.Lookup(&k, &val)
-		if err != nil {
-			log.Debugf("failed to get telemetry for map:key %s:%d\n", m, k)
-			continue
-		}
-		if count := getErrCount(val.Count[:]); len(count) > 0 {
-			t[m] = count
-		}
-	}
-	return t
-}
-
-func getHelpersTelemetry(e *EBPFTelemetry) map[string]interface{} {
-	helperTelemMap := make(map[string]interface{})
-	if e.helperErrMap == nil {
-		return helperTelemMap
-	}
-
-	var val helperErrTelemetry
-	for probeName, k := range e.probeKeys {
-		err := e.helperErrMap.Lookup(&k, &val)
-		if err != nil {
-			log.Debugf("failed to get telemetry for map:key %s:%d\n", probeName, k)
-			continue
-		}
-
-		t := make(map[string]interface{})
-		for indx, helperName := range helperNames {
-			base := maxErrno * indx
-			if count := getErrCount(val.Count[base : base+maxErrno]); len(count) > 0 {
-				t[helperName] = count
-			}
-		}
-		if len(t) > 0 {
-			helperTelemMap[probeName] = t
-		}
-	}
-	return helperTelemMap
 }
 
 func triggerTestAndGetTelemetry(t *testing.T) []prometheus.Metric {
