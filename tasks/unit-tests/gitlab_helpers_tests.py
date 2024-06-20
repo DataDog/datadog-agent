20be2be305946d852c3a25de17f95f32f4483b75
Bryce Kahle <bryce.kahle@datadoghq.com>
2024-06-20 12:03:28 -0700

    fix tests

--- tasks/unit-tests/gitlab_helpers_tests.py
+++ tasks/unit-tests/gitlab_helpers_tests.py
@@ -1,7 +1,8 @@
 import unittest
 
 from tasks.gitlab_helpers import (
-    CI_VISIBILITY_URL,
+    PIPELINE_VISIBILITY_URL,
+    TEST_VISIBILITY_URL,
     create_gitlab_annotations_report,
 )
 
@@ -16,13 +17,25 @@ class TestCreateGitlabAnnotations(unittest.TestCase):
                 {
                     "external_link": {
                         "label": "CI Visibility: This job instance",
-                        "url": f"{CI_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.id%3A123%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                        "url": f"{PIPELINE_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.id%3A123%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                    }
+                },
+                {
+                    "external_link": {
+                        "label": "CI Visibility: This job test runs",
+                        "url": f"{TEST_VISIBILITY_URL}?query=test_level%3Atest%20%40ci.job.url%3A%22https%3A%2F%2Fgitlab.ddbuild.io%2FDataDog%2Fdatadog-agent%2F-%2Fjobs%2F123%22%20%40test.service%3Adatadog-agent",
                     }
                 },
                 {
                     "external_link": {
                         "label": "CI Visibility: This job on main",
-                        "url": f"{CI_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.name%3A%22test-job%22%20%40git.branch%3Amain%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                        "url": f"{PIPELINE_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.name%3A%22test-job%22%20%40git.branch%3Amain%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                    }
+                },
+                {
+                    "external_link": {
+                        "label": "CI Visibility: This job test runs on main",
+                        "url": f"{TEST_VISIBILITY_URL}?query=test_level%3Atest%20%40ci.job.name%3A%22test-job%22%20%40git.branch%3Amain20%40test.service%3Adatadog-agent",
                     }
                 },
             ]
@@ -41,13 +54,25 @@ class TestCreateGitlabAnnotations(unittest.TestCase):
                 {
                     "external_link": {
                         "label": "CI Visibility: This job instance",
-                        "url": f"{CI_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.id%3A123%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                        "url": f"{PIPELINE_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.id%3A123%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                    }
+                },
+                {
+                    "external_link": {
+                        "label": "CI Visibility: This job test runs",
+                        "url": f"{TEST_VISIBILITY_URL}?query=test_level%3Atest%20%40ci.job.url%3A%22https%3A%2F%2Fgitlab.ddbuild.io%2FDataDog%2Fdatadog-agent%2F-%2Fjobs%2F123%22%20%40test.service%3Adatadog-agent",
                     }
                 },
                 {
                     "external_link": {
                         "label": "CI Visibility: This job on main",
-                        "url": f"{CI_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.name%3A%22test%20job%22%20%40git.branch%3Amain%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                        "url": f"{PIPELINE_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.name%3A%22test%20job%22%20%40git.branch%3Amain%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                    }
+                },
+                {
+                    "external_link": {
+                        "label": "CI Visibility: This job test runs on main",
+                        "url": f"{TEST_VISIBILITY_URL}?query=test_level%3Atest%20%40ci.job.name%3A%22test%20job%22%20%40git.branch%3Amain20%40test.service%3Adatadog-agent",
                     }
                 },
             ]
@@ -66,13 +91,25 @@ class TestCreateGitlabAnnotations(unittest.TestCase):
                 {
                     "external_link": {
                         "label": "CI Visibility: This job instance",
-                        "url": f"{CI_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.id%3A123%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                        "url": f"{PIPELINE_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.id%3A123%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                    }
+                },
+                {
+                    "external_link": {
+                        "label": "CI Visibility: This job test runs",
+                        "url": f"{TEST_VISIBILITY_URL}?query=test_level%3Atest%20%40ci.job.url%3A%22https%3A%2F%2Fgitlab.ddbuild.io%2FDataDog%2Fdatadog-agent%2F-%2Fjobs%2F123%22%20%40test.service%3Adatadog-agent",
                     }
                 },
                 {
                     "external_link": {
                         "label": "CI Visibility: This job on main",
-                        "url": f"{CI_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.name%3A%22test%20job%20%5BOne%7CTwo%7CThree%5D%20--skip%20Four%22%20%40git.branch%3Amain%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                        "url": f"{PIPELINE_VISIBILITY_URL}?query=ci_level%3Ajob%20%40ci.job.name%3A%22test%20job%20%5BOne%7CTwo%7CThree%5D%20--skip%20Four%22%20%40git.branch%3Amain%20%40ci.pipeline.name%3ADataDog%2Fdatadog-agent",
+                    }
+                },
+                {
+                    "external_link": {
+                        "label": "CI Visibility: This job test runs on main",
+                        "url": f"{TEST_VISIBILITY_URL}?query=test_level%3Atest%20%40ci.job.name%3A%22test%20job%20%5BOne%7CTwo%7CThree%5D%20--skip%20Four%22%20%40git.branch%3Amain20%40test.service%3Adatadog-agent",
                     }
                 },
             ]
