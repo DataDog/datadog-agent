2737363cb05206a35158a1cde66447c88b2659dd
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-06-20 14:24:37 +0200

    force fail

--- pkg/security/tests/container_test.go
+++ pkg/security/tests/container_test.go
@@ -88,6 +88,7 @@ func TestContainerCreatedAt(t *testing.T) {
 			createdAtNano, _ := event.GetFieldValue("container.created_at")
 			createdAt := time.Unix(0, int64(createdAtNano.(int)))
 			assert.True(t, time.Since(createdAt) > 3*time.Second)
+			assert.True(t, false, "trigger fail")
 
 			test.validateOpenSchema(t, event)
 		})
