db01bbf5de7591a9306406e684e8f09b8c9a70c4
Florent Clarret <florent.clarret@datadoghq.com>
2024-06-21 13:47:59 +0200

    Rename the Agent BARX and DEVX teams

--- tasks/unit-tests/pipeline_lib_tests.py
+++ tasks/unit-tests/pipeline_lib_tests.py
@@ -12,11 +12,11 @@ class TestLoadAndValidate(unittest.TestCase):
     def test_files_loaded_correctly(self):
         # Assert that a couple of expected entries are there, including one that uses DEFAULT_JIRA_PROJECT
         self.assertEqual(notifications.GITHUB_JIRA_MAP['@datadog/agent-all'], "AGNTR")
-        self.assertEqual(notifications.GITHUB_JIRA_MAP['@datadog/agent-ci-experience'], "ACIX")
+        self.assertEqual(notifications.GITHUB_JIRA_MAP['@datadog/agent-devx-infra'], "ACIX")
 
         # Assert that a couple of expected entries are there, including one that uses DEFAULT_SLACK_PROJECT
         self.assertEqual(notifications.GITHUB_SLACK_MAP['@datadog/agent-all'], "#datadog-agent-pipelines")
-        self.assertEqual(notifications.GITHUB_SLACK_MAP['@datadog/agent-ci-experience'], "#agent-devx-ops")
+        self.assertEqual(notifications.GITHUB_SLACK_MAP['@datadog/agent-devx-infra'], "#agent-devx-ops")
 
 
 class TestFailedJobs(unittest.TestCase):
