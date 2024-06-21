54023f62f31dabd5eef335b67bb50a2d45989eb3
William Yu <william.yu@datadoghq.com>
2024-06-21 12:01:56 -0400

    add checks for the running agent

--- test/new-e2e/tests/process/k8s_test.go
+++ test/new-e2e/tests/process/k8s_test.go
@@ -9,8 +9,10 @@ import (
 	"bytes"
 	"context"
 	_ "embed"
+	"encoding/json"
 	"testing"
 	"text/template"
+	"time"
 
 	"github.com/DataDog/test-infra-definitions/common/config"
 	"github.com/DataDog/test-infra-definitions/components/datadog/apps/cpustress"
@@ -23,6 +25,7 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeClient "k8s.io/client-go/kubernetes"
 
+	"github.com/DataDog/datadog-agent/test/fakeintake/aggregator"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awskubernetes "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/kubernetes"
@@ -76,6 +79,61 @@ func TestK8sTestSuite(t *testing.T) {
 	e2e.Run(t, &K8sSuite{}, options...)
 }
 
+func (s *K8sSuite) TestProcessCheck() {
+	t := s.T()
+
+	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
+		status := s.getAgentStatus()
+		assert.ElementsMatch(t, []string{"process", "rtprocess"}, status.ProcessAgentStatus.Expvars.Map.EnabledChecks)
+	}, 2*time.Minute, 5*time.Second)
+
+	var payloads []*aggregator.ProcessPayload
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		var err error
+		payloads, err = s.Env().FakeIntake.Client().GetProcesses()
+		assert.NoError(c, err, "failed to get process payloads from fakeintake")
+
+		// Wait for two payloads, as processes must be detected in two check runs to be returned
+		assert.GreaterOrEqual(c, len(payloads), 2, "fewer than 2 payloads returned")
+	}, 2*time.Minute, 10*time.Second)
+
+	assertProcessCollected(t, payloads, false, "stress-ng-cpu [run]")
+	assertContainersCollected(t, payloads, []string{"stress-ng"})
+}
+
+func (s *K8sSuite) TestProcessDiscoveryCheck() {
+	s.T().Skip("WIP: test is failing as process collection is still enabled with 'DD_PROCESS_AGENT_ENABLED=true'." +
+		"The bug appears to be in test-infra-definitions and it's default helm values taking precedence")
+	t := s.T()
+	helmValues, err := createHelmValues(helmConfig{
+		ProcessAgentEnabled:        true,
+		ProcessDiscoveryCollection: true,
+	})
+	require.NoError(t, err)
+
+	s.UpdateEnv(awskubernetes.KindProvisioner(
+		awskubernetes.WithWorkloadApp(func(e config.Env, kubeProvider *kubernetes.Provider) (*kubeComp.Workload, error) {
+			return cpustress.K8sAppDefinition(e, kubeProvider, "workload-stress")
+		}),
+		awskubernetes.WithAgentOptions(kubernetesagentparams.WithHelmValues(helmValues)),
+	))
+
+	assert.EventuallyWithT(t, func(collect *assert.CollectT) {
+		status := s.getAgentStatus()
+		assert.ElementsMatch(t, []string{"process_discovery"}, status.ProcessAgentStatus.Expvars.Map.EnabledChecks)
+	}, 2*time.Minute, 5*time.Second)
+
+	var payloads []*aggregator.ProcessDiscoveryPayload
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		var err error
+		payloads, err = s.Env().FakeIntake.Client().GetProcessDiscoveries()
+		assert.NoError(c, err, "failed to get process discovery payloads from fakeintake")
+		assert.NotEmpty(c, payloads, "no process discovery payloads returned")
+	}, 2*time.Minute, 10*time.Second)
+
+	assertProcessDiscoveryCollected(t, payloads, "stress-ng-cpu [run]")
+}
+
 func (s *K8sSuite) TestManualProcessCheck() {
 	agent := getAgentPod(s.T(), s.Env().KubernetesCluster.Client())
 
@@ -115,6 +173,24 @@ func (s *K8sSuite) TestManualContainerCheck() {
 	assert.Empty(s.T(), stderr)
 
 	assertManualContainerCheck(s.T(), stdout, "stress-ng")
+}
+
+func (s *K8sSuite) getAgentStatus() AgentStatus {
+	t := s.T()
+	agent := getAgentPod(t, s.Env().KubernetesCluster.Client())
+
+	stdout, stderr, err := s.Env().KubernetesCluster.KubernetesClient.
+		PodExec(agent.Namespace, agent.Name, "process-agent",
+			[]string{"bash", "-c", "DD_LOG_LEVEL=OFF agent status --json"})
+	require.NoError(t, err)
+	assert.Empty(t, stderr)
+	assert.NotNil(t, stdout, "failed to get agent status")
+
+	var statusMap AgentStatus
+	err = json.Unmarshal([]byte(stdout), &statusMap)
+	assert.NoError(t, err, "failed to unmarshal agent status")
+
+	return statusMap
 }
 
 func getAgentPod(t *testing.T, client kubeClient.Interface) corev1.Pod {
