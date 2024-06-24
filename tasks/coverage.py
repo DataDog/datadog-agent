105a4f106721846bcede27cbdf247705bd3d4dee
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2024-06-24 11:50:23 +0200

    [CI] Create its own gitlab section for Coverage upload/download

--- tasks/coverage.py
+++ tasks/coverage.py
@@ -119,11 +119,16 @@ def upload_to_codecov(
     distro_tag = get_distro()
     codecov_binary = "codecov" if platform.system() != "Windows" else "codecov.exe"
 
-    with gitlab_section("Upload coverage reports to Codecov", collapsed=True):
+    coverage_cache_section = (
+        "Applying missing coverage cache from S3." if pull_coverage_cache else "Uploading coverage files to S3."
+    )
+    with gitlab_section(coverage_cache_section, collapsed=True):
         if pull_coverage_cache:
             apply_missing_coverage(ctx, from_commit_sha=get_main_parent_commit(ctx), debug=debug)
         if push_coverage_cache:
             upload_coverage_to_s3(ctx)
+
+    with gitlab_section("Upload coverage reports to Codecov", collapsed=True):
         ctx.run(f"{codecov_binary} -f {PROFILE_COV} -F {distro_tag}", warn=True)
 
 
