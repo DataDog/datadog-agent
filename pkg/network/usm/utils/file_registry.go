1b75ddb813650506f80539f25f5a72cb200a3415
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-06-24 08:10:04 -0700

    usm: gotls manual attach: do not consider an already register binary as an error

--- pkg/network/usm/utils/file_registry.go
+++ pkg/network/usm/utils/file_registry.go
@@ -107,7 +107,7 @@ var (
 	errCallbackIsMissing       = errors.New("activationCB and deactivationCB must be both non-nil")
 	errAlreadyStopped          = errors.New("registry already stopped")
 	errPathIsBlocked           = errors.New("path is blocked")
-	errPathIsAlreadyRegistered = errors.New("path is already registered")
+	ErrPathIsAlreadyRegistered = errors.New("path is already registered")
 )
 
 // Register inserts or updates a new file registration within to the `FileRegistry`;
@@ -149,7 +149,7 @@ func (r *FileRegistry) Register(namespacedPath string, pid uint32, activationCB,
 			r.byPID[pid][pathID] = struct{}{}
 		}
 		r.telemetry.fileAlreadyRegistered.Add(1)
-		return errPathIsAlreadyRegistered
+		return ErrPathIsAlreadyRegistered
 	}
 
 	if err := activationCB(path); err != nil {
