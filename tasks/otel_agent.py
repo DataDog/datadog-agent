ee53eb24a053b4fe144d6b24e936aa2bd63a4b0a
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2024-06-25 20:43:40 +0200

    [tasks] adding otel-agent.image-build to otel tasks
    [dockerfiles] otel: update collector section to otelcollector

--- tasks/otel_agent.py
+++ tasks/otel_agent.py
@@ -2,11 +2,16 @@ import os
 import shutil
 
 from invoke import task
+from invoke.exceptions import Exit
 
 from tasks.libs.common.utils import REPO_PATH, bin_name
 
+BIN_NAME = "otel-agent"
+CFG_NAME = "otel-config.yaml"
 BIN_DIR = os.path.join(".", "bin", "otel-agent")
 BIN_PATH = os.path.join(BIN_DIR, bin_name("otel-agent"))
+OT_AGENT_IMAGE_NAME = "datadog/agent"
+OT_AGENT_TAG = "main-ot"
 
 
 @task
@@ -31,3 +36,33 @@ def build(ctx):
     os.mkdir(dist_folder)
 
     shutil.copy("./cmd/otel-agent/dist/otel-config.yaml", os.path.join(dist_folder, "otel-config.yaml"))
+
+
+@task
+def image_build(ctx, arch='amd64', base_version='latest', tag=OT_AGENT_TAG, push=False):
+    """
+    Build the otel agent container image
+    """
+
+    build_context = os.path.join("Dockerfiles", "agent")
+    dockerfile = os.path.join("Dockerfiles", "agent-ot", "Dockerfile")
+
+    otel_binary = os.path.join(BIN_DIR, BIN_NAME)
+    config_file = os.path.join(BIN_DIR, "dist", CFG_NAME)
+
+    if (not os.path.exists(otel_binary)) or (not os.path.exists(config_file)):
+        print("Please run otel-agent.build")
+        raise Exit(code=1)
+
+    shutil.copy2(otel_binary, build_context)
+    shutil.copy2(config_file, build_context)
+
+    common_build_opts = (
+        f"-t {OT_AGENT_IMAGE_NAME}:{tag} -f {dockerfile} --build-arg=\"BASE_IMAGE_DD_VERSION={base_version}\""
+    )
+    ctx.run(f"docker build {common_build_opts} --platform linux/{arch} {build_context}")
+    if push:
+        ctx.run(f"docker push {tag}")
+
+    os.remove(os.path.join(build_context, BIN_NAME))
+    os.remove(os.path.join(build_context, CFG_NAME))
