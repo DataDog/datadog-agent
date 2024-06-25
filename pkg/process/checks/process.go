66dfd0c8a1a8ae880c9bc3c7ccbd2c17d28062a6
Robert Li <robert.li@datadoghq.com>
2024-06-25 13:11:03 -0400

    Update comment about short-lived processes (#27052)

--- pkg/process/checks/process.go
+++ pkg/process/checks/process.go
@@ -618,7 +618,8 @@ func skipProcess(
 	}
 	if _, ok := lastProcs[fp.Pid]; !ok {
 		// Skipping any processes that didn't exist in the previous run.
-		// This means short-lived processes (<2s) will never be captured.
+		// The check runs every 10 seconds by default, so this means
+		// processes that live less than 20 seconds may not be captured.
 		return true
 	}
 	// Skipping zombie processes (defined in docs as Status = "Z") if the config
