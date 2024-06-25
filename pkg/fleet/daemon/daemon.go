cf12a6708d556a29f79efe1c27500386346b55a0
Paul Coignet <paul.coignet@datadoghq.com>
2024-06-25 13:57:20 +0200

    Fix auto-inject status

--- pkg/fleet/daemon/daemon.go
+++ pkg/fleet/daemon/daemon.go
@@ -120,8 +120,9 @@ func (d *daemonImpl) GetAPMInjectionStatus() (status APMInjectionStatus, err err
 	// Host is instrumented if the ld.so.preload file contains the apm injector
 	ldPreloadContent, err := os.ReadFile("/etc/ld.so.preload")
 	if err != nil {
-		return status, fmt.Errorf("could not read /etc/ld.so.preload: %w", err)
+		return status, nil
 	}
+	status.LdPreloadAvailable = true
 	if bytes.Contains(ldPreloadContent, []byte("/opt/datadog-packages/datadog-apm-inject/stable/inject")) {
 		status.HostInstrumented = true
 	}
