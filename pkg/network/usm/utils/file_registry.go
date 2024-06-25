4e87cacaa629b372472cc36ba36c18ebabaddf61
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-06-25 06:44:31 -0700

    fix linter

--- pkg/network/usm/utils/file_registry.go
+++ pkg/network/usm/utils/file_registry.go
@@ -102,6 +102,7 @@ func NewFileRegistry(programName string) *FileRegistry {
 	return r
 }
 
+//revive:disable:exported
 var (
 	errPidIsNotRegistered      = errors.New("pid is not registered")
 	errCallbackIsMissing       = errors.New("activationCB and deactivationCB must be both non-nil")
@@ -109,6 +110,8 @@ var (
 	errPathIsBlocked           = errors.New("path is blocked")
 	ErrPathIsAlreadyRegistered = errors.New("path is already registered")
 )
+
+//revive:enable
 
 // Register inserts or updates a new file registration within to the `FileRegistry`;
 //
