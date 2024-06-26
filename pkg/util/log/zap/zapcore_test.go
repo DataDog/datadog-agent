05d4660555e4d66ec573dfb63ceac3fe5481c195
Dustin Long <dustin.long@datadoghq.com>
2024-06-26 17:21:38 -0400

    Use placeholder so that we dont care where or how the test is run

--- pkg/util/log/zap/zapcore_test.go
+++ pkg/util/log/zap/zapcore_test.go
@@ -9,7 +9,6 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"os/exec"
 	"strings"
 	"testing"
 
@@ -38,31 +37,12 @@ func createExtraTextContext(string) seelog.FormatterFunc {
 	}
 }
 
-func parseShortFilePath(_ string) seelog.FormatterFunc {
-	return func(message string, level seelog.LogLevel, context seelog.LogContextInterface) interface{} {
-		return extractShortPathFromFullPath(context.FullPath())
+// Instead of using the real filepath, use a placeholder, in order to not
+// need to rely on where this test is being run.
+func replaceFilePathForTest(_ string) seelog.FormatterFunc {
+	return func(_ string, _ seelog.LogLevel, _ seelog.LogContextInterface) interface{} {
+		return "test/zapcore_test.go"
 	}
-}
-
-func extractShortPathFromFullPath(fullPath string) string {
-	// Ask git for the root of the repository. This handles cases where
-	// the repository's directory name does not end in "datadog-agent",
-	// for example, when using git worktrees.
-	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
-	stdout, err := cmd.CombinedOutput()
-	if err == nil {
-		rootpath := strings.TrimSpace(string(stdout))
-		if !strings.HasSuffix(rootpath, "/") {
-			rootpath = rootpath + "/"
-		}
-		if strings.HasPrefix(fullPath, rootpath) {
-			return strings.TrimPrefix(fullPath, rootpath)
-		}
-	}
-	// We want to trim the part containing the path of the project
-	// ie DataDog/datadog-agent/
-	slices := strings.Split(fullPath, "datadog-agent/")
-	return slices[len(slices)-1]
 }
 
 func TestZapBasicLogging(t *testing.T) {
@@ -77,31 +57,31 @@ func TestZapBasicLogging(t *testing.T) {
 			desc:    "Debug (no fields, debug level)",
 			log:     func(l *zap.Logger) { l.Debug("Simple message") },
 			level:   "debug",
-			message: "[DEBUG] | pkg/util/log/zap/zapcore_test.go | Simple message",
+			message: "[DEBUG] | test/zapcore_test.go | Simple message",
 		},
 		{
 			desc:    "Info (no fields, debug level)",
 			log:     func(l *zap.Logger) { l.Info("Simple message") },
 			level:   "debug",
-			message: "[INFO] | pkg/util/log/zap/zapcore_test.go | Simple message",
+			message: "[INFO] | test/zapcore_test.go | Simple message",
 		},
 		{
 			desc:    "Warn (no fields, debug level)",
 			log:     func(l *zap.Logger) { l.Warn("Simple message") },
 			level:   "debug",
-			message: "[WARN] | pkg/util/log/zap/zapcore_test.go | Simple message",
+			message: "[WARN] | test/zapcore_test.go | Simple message",
 		},
 		{
 			desc:    "Error (no fields, debug level)",
 			log:     func(l *zap.Logger) { l.Error("Simple message") },
 			level:   "debug",
-			message: "[ERROR] | pkg/util/log/zap/zapcore_test.go | Simple message",
+			message: "[ERROR] | test/zapcore_test.go | Simple message",
 		},
 		{
 			desc:    "DPanic (no fields, debug level)",
 			log:     func(l *zap.Logger) { l.DPanic("Development panic") },
 			level:   "debug",
-			message: "[CRITICAL] | pkg/util/log/zap/zapcore_test.go | Development panic",
+			message: "[CRITICAL] | test/zapcore_test.go | Development panic",
 		},
 		{
 			desc: "Error level",
@@ -117,13 +97,13 @@ func TestZapBasicLogging(t *testing.T) {
 			desc:    "Info (fields)",
 			log:     func(l *zap.Logger) { l.Info("Fields", zap.Int("int", 1), zap.String("key", "val")) },
 			level:   "debug",
-			message: "[INFO] | pkg/util/log/zap/zapcore_test.go | int:1, key:val | Fields",
+			message: "[INFO] | test/zapcore_test.go | int:1, key:val | Fields",
 		},
 		{
 			desc:    "Error (fields)",
 			log:     func(l *zap.Logger) { l.Error("Fields", zap.Error(fmt.Errorf("an error"))) },
 			level:   "debug",
-			message: "[ERROR] | pkg/util/log/zap/zapcore_test.go | error:an error | Fields",
+			message: "[ERROR] | test/zapcore_test.go | error:an error | Fields",
 		},
 		{
 			desc: "With (using original)",
@@ -132,7 +112,7 @@ func TestZapBasicLogging(t *testing.T) {
 				l.Info("Fields", zap.Bool("bool", true))
 			},
 			level:   "debug",
-			message: "[INFO] | pkg/util/log/zap/zapcore_test.go | bool:true | Fields",
+			message: "[INFO] | test/zapcore_test.go | bool:true | Fields",
 		},
 		{
 			desc: "With (using new)",
@@ -141,13 +121,13 @@ func TestZapBasicLogging(t *testing.T) {
 				extra.Info("Fields", zap.Bool("bool", true))
 			},
 			level:   "debug",
-			message: "[INFO] | pkg/util/log/zap/zapcore_test.go | int:1, bool:true | Fields",
+			message: "[INFO] | test/zapcore_test.go | int:1, bool:true | Fields",
 		},
 		{
 			desc:    "Namespace",
 			log:     func(l *zap.Logger) { l.Info("Fields", zap.Namespace("ns"), zap.Int("int", 1)) },
 			level:   "debug",
-			message: "[INFO] | pkg/util/log/zap/zapcore_test.go | ns/int:1 | Fields",
+			message: "[INFO] | test/zapcore_test.go | ns/int:1 | Fields",
 		},
 	}
 
@@ -156,7 +136,7 @@ func TestZapBasicLogging(t *testing.T) {
 			var b bytes.Buffer
 			w := bufio.NewWriter(&b)
 			seelog.RegisterCustomFormatter("ExtraTextContext", createExtraTextContext)
-			seelog.RegisterCustomFormatter("ShortFilePath", parseShortFilePath)
+			seelog.RegisterCustomFormatter("ShortFilePath", replaceFilePathForTest)
 			l, err := seelog.LoggerFromWriterWithMinLevelAndFormat(w, seelog.DebugLvl, "[%LEVEL] | %ShortFilePath | %ExtraTextContext%Msg")
 			require.NoError(t, err)
 			log.SetupLogger(l, testInstance.level)
