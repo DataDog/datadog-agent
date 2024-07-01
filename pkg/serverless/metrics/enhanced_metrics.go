f799ab9e3836332367930c28487438db42339c41
Tal Usvyatsky <tal.usvyatsky@datadoghq.com>
2024-07-01 09:55:25 -0400

    cleanup

--- pkg/serverless/metrics/enhanced_metrics.go
+++ pkg/serverless/metrics/enhanced_metrics.go
@@ -26,7 +26,7 @@ const (
 	armLambdaPricePerGbSecond = 0.0000133334
 	msToSec                   = 0.001
 
-	//tmp path
+	// tmp directory path
 	tmpPath = "/tmp/"
 
 	// Enhanced metrics
@@ -514,9 +514,10 @@ func SendTmpEnhancedMetrics(sendMetrics chan bool, tags []string, metricAgent *S
 		return
 	}
 
-	bsize, blocks, bavail, err := statfs()
+	bsize, blocks, bavail, err := statfs(tmpPath)
 	if err != nil {
 		log.Debug("Could not emit tmp enhanced metrics")
+		return
 	}
 	tmpMax := blocks * bsize
 	tmpUsed := bsize * (blocks - bavail)
@@ -535,7 +536,11 @@ func SendTmpEnhancedMetrics(sendMetrics chan bool, tags []string, metricAgent *S
 			})
 			return
 		case <-ticker.C:
-			bsize, blocks, bavail, err = statfs()
+			bsize, blocks, bavail, err = statfs(tmpPath)
+			if err != nil {
+				log.Debug("Could not emit tmp enhanced metrics")
+				return
+			}
 			tmpUsed = math.Max(tmpUsed, bsize*(blocks-bavail))
 		}
 	}
