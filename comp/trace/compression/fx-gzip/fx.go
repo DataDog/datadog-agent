a50464648f30cfff9fa146d4bf740cde775839d2
Iñigo Lopez de Heredia <inigo.lopezdeheredia@datadoghq.com>
2024-07-03 14:13:08 +0200

    add fx folders to compression comp

---
+++ comp/trace/compression/fx-gzip/fx.go
@@ -0,0 +1,16 @@
+package fxzstd
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+
+	implgzip "github.com/DataDog/datadog-agent/comp/trace/compression/impl-gzip"
+)
+
+// Module specifies the compression module.
+func Module() fxutil.Module {
+	return fxutil.Component(
+		fxutil.ProvideComponentConstructor(
+			implgzip.NewComponent,
+		),
+	)
+}
---
+++ comp/trace/compression/fx-zstd/fx.go
@@ -0,0 +1,16 @@
+package fxzstd
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+
+	implzstd "github.com/DataDog/datadog-agent/comp/trace/compression/impl-zstd"
+)
+
+// Module specifies the compression module.
+func Module() fxutil.Module {
+	return fxutil.Component(
+		fxutil.ProvideComponentConstructor(
+			implzstd.NewComponent,
+		),
+	)
+}
