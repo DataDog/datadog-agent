fc6287ff84e20e4e65d6224ec3ab60c01c4de0ce
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-07-04 13:57:43 +0200

    add the right build tags to run security tests

--- pkg/security/tests/testopts.go
+++ pkg/security/tests/testopts.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build test || functionaltests || stresstests
+//go:build functionaltests || stresstests
 
 // Package tests holds tests related files
 package tests
