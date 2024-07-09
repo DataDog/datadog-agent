f7585ffd40d99303228872b12906beb409b3f187
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-07-09 11:47:06 +0200

    ensure components tests test that we returns errors

--- tasks/unit-tests/components_tests.py
+++ tasks/unit-tests/components_tests.py
@@ -8,13 +8,8 @@ from tasks import components
 
 class TestComponents(unittest.TestCase):
     def setUp(self):
-        # Add the classic component (used by tests) to the allowlist
-        classicComp = 'comp/classic/classicimpl'
-        if classicComp not in components.components_classic_style:
-            components.components_classic_style.append(classicComp)
         # Create a temporary directory of the source directory to allow
         # tests to modify the source files
-        # TODO: Add tests that modify source files to ensure that errors work
         self.tmpdir = tempfile.mkdtemp()
         shutil.copytree('tasks/unit-tests/testdata/components_src/comp', os.path.join(self.tmpdir, 'comp'))
         # Preserve currenty directory, change to temp directory
@@ -35,10 +30,28 @@ class TestComponents(unittest.TestCase):
         results = components.get_components_and_bundles()
         bundles = results[0]
         comps = results[1]
+        ok = results[2]
         self.assertEqual([], bundles)
         self.assertEqual(3, len(comps))
         self.assertEqual('comp/classic', comps[0].path)
         self.assertEqual('comp/multiple', comps[1].path)
         self.assertEqual('comp/newstyle', comps[2].path)
+        self.assertEqual(ok, False)
+
+        # Add the classic component (used by tests) to the allowlist
+        # To validate that calling get_components_and_bundles does not return an error
+        classicComp = 'comp/classic/classicimpl'
+        if classicComp not in components.components_classic_style:
+            components.components_classic_style.append(classicComp)
+
+        results = components.get_components_and_bundles()
+        bundles = results[0]
+        comps = results[1]
+        ok = results[2]
+        self.assertEqual([], bundles)
+        self.assertEqual(3, len(comps))
+        self.assertEqual(ok, True)
+
+        components.components_classic_style.remove(classicComp)
 
     # TODO: more tests
