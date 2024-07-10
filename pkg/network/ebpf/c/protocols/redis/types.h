611e5f252fccf4bcb11b00326b6368357bbeaf2c
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-07-10 02:57:04 -0700

    usm: ebpf: add redis decoding skeleton

---
+++ pkg/network/ebpf/c/protocols/redis/types.h
@@ -0,0 +1,19 @@
+#ifndef __REDIS_TYPES_H
+#define __REDIS_TYPES_H
+
+#include "conn_tuple.h"
+
+// Controls the number of Redis transactions read from userspace at a time.
+#define REDIS_BATCH_SIZE 25
+
+// Redis in-flight transaction info
+typedef struct {
+} redis_transaction_t;
+
+// The struct we send to userspace, containing the connection tuple and the transaction information.
+typedef struct {
+    conn_tuple_t tuple;
+    redis_transaction_t tx;
+} redis_event_t;
+
+#endif /* __REDIS_TYPES_H */
