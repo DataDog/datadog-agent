4af451892f010c4d47c2bb913d0cb0882cd1ef72
gui774ume <gui774ume.fournier@gmail.com>
2024-07-10 13:50:12 +0200

    [CSM] Hook ftruncate

--- pkg/security/ebpf/probes/open.go
+++ pkg/security/ebpf/probes/open.go
@@ -15,7 +15,13 @@ func getOpenProbes(fentry bool) []*manager.Probe {
 		{
 			ProbeIdentificationPair: manager.ProbeIdentificationPair{
 				UID:          SecurityAgentUID,
-				EBPFFuncName: "hook_vfs_truncate",
+				EBPFFuncName: "hook_security_path_truncate",
+			},
+		},
+		{
+			ProbeIdentificationPair: manager.ProbeIdentificationPair{
+				UID:          SecurityAgentUID,
+				EBPFFuncName: "hook_security_file_truncate",
 			},
 		},
 		{
@@ -79,6 +85,12 @@ func getOpenProbes(fentry bool) []*manager.Probe {
 			UID: SecurityAgentUID,
 		},
 		SyscallFuncName: "truncate",
+	}, fentry, EntryAndExit, true)...)
+	openProbes = append(openProbes, ExpandSyscallProbes(&manager.Probe{
+		ProbeIdentificationPair: manager.ProbeIdentificationPair{
+			UID: SecurityAgentUID,
+		},
+		SyscallFuncName: "ftruncate",
 	}, fentry, EntryAndExit, true)...)
 	openProbes = append(openProbes, ExpandSyscallProbes(&manager.Probe{
 		ProbeIdentificationPair: manager.ProbeIdentificationPair{
