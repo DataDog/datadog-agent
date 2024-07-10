43b2b5731eca47c026dd59a3f7b289379db4ce29
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-07-10 18:25:49 +0200

    [activate-change-path-linter] Added stderr

--- tasks/linter.py
+++ tasks/linter.py
@@ -567,12 +567,13 @@ def job_change_path(ctx, job_files=None):
                 color_message(
                     'warning: The following tests do not contain required change paths rule but are allowed:',
                     Color.ORANGE,
-                )
+                ),
+                file=sys.stderr,
             )
             for filepath, tests in tests_without_change_path_allowed.items():
                 print(f"- {color_message(filepath, 'bold')}: {', '.join(tests)}", file=sys.stderr)
-            print(color_message('warning: End of allow-listed jobs', Color.ORANGE))
-            print()
+            print(color_message('warning: End of allow-listed jobs', Color.ORANGE), file=sys.stderr)
+            print(file=sys.stderr)
 
     if len(tests_without_change_path) != 0:
         print(color_message("error: Tests without required change paths rule:", "red"), file=sys.stderr)
