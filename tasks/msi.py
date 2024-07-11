79bc03ea54920e570add6658867533ee685de25c
Julien Lebot <julien.lebot@datadoghq.com>
2024-07-11 15:33:44 +0200

    Add missing PACKAGE_VERSION env var

--- tasks/msi.py
+++ tasks/msi.py
@@ -312,6 +312,9 @@ def build_installer(ctx, vstudio_root=None, arch="x64", debug=False):
     Build the MSI installer for the agent
     """
     env = {}
+    env['PACKAGE_VERSION'] = get_version(
+        ctx, include_git=True, url_safe=True, major_version=7, include_pipeline_id=True
+    )
     env['NUGET_PACKAGES_DIR'] = f'{NUGET_PACKAGES_DIR}'
     env['AGENT_INSTALLER_OUTPUT_DIR'] = f'{BUILD_OUTPUT_DIR}'
     configuration = _msbuild_configuration(debug=debug)
