f611907261dc4efeb9d7818cb31845185e9bd243
Josh Lineaweaver <josh.lineaweaver@datadoghq.com>
2024-07-11 07:32:10 -0600

    Add comment

--- pkg/aggregator/orchestrator.go
+++ pkg/aggregator/orchestrator.go
@@ -5,4 +5,6 @@ package aggregator
 // Orchestrator Explorer is enabled by default but
 // the forwarder is only created if the orchestrator
 // build tag exists
+
+// OrchestratorForwarderSupport shows if the orchestrator build tag is enabled
 const OrchestratorForwarderSupport = true
