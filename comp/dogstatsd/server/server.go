c4bfec1de5c5c5c110277c817115066010e6cc6e
Iñigo López de Heredia <inigo.lheredia@gmail.com>
2024-07-12 12:55:31 +0200

    Use errors.Is instead of os.IsNotExist
    Co-authored-by: Gustavo Caso <gustavo.caso@datadoghq.com>

--- comp/dogstatsd/server/server.go
+++ comp/dogstatsd/server/server.go
@@ -373,7 +373,7 @@ func (s *server) start(context.Context) error {
 	}
 
 	if len(socketPath) > 0 {
-		if _, err := os.Stat(filepath.Dir(socketPath)); !os.IsNotExist(err) {
+		if _, err := os.Stat(filepath.Dir(socketPath)); !errors.Is(err, fs.ErrNotExist) {
 			unixListener, err := listeners.NewUDSDatagramListener(packetsChannel, sharedPacketPoolManager, sharedUDSOobPoolManager, s.config, s.tCapture, s.wmeta, s.pidMap, s.listernersTelemetry, s.packetsTelemetry, s.telemetry)
 			if err != nil {
 				s.log.Errorf("Can't init UDS listener on path %s: %s", socketPath, err.Error())
