ef384ce737fd996002eaddfb6f82ed3fc52da846
amit.slavin <amit.slavin@datadoghq.com>
2024-07-14 15:52:05 +0300

    reduce the batch size

--- pkg/network/ebpf/c/protocols/postgres/types.h
+++ pkg/network/ebpf/c/protocols/postgres/types.h
@@ -4,7 +4,7 @@
 #include "conn_tuple.h"
 
 // Controls the number of Postgres transactions read from userspace at a time.
-#define POSTGRES_BATCH_SIZE 15
+#define POSTGRES_BATCH_SIZE 14
 
 // Maximum length of postgres query to send to userspace.
 #define POSTGRES_BUFFER_SIZE 200
