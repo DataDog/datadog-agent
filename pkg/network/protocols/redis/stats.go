20f028e869fee0263cab6f2797132d6fa5f5f3e7
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-07-15 16:17:54 +0200

    usm: redis: add redis protobuf marshalling

--- pkg/network/protocols/redis/stats.go
+++ pkg/network/protocols/redis/stats.go
@@ -7,6 +7,7 @@ package redis
 
 import (
 	"github.com/DataDog/datadog-agent/pkg/network/types"
+	"github.com/DataDog/datadog-agent/pkg/process/util"
 )
 
 // Key is an identifier for a group of Redis transactions
@@ -14,7 +15,17 @@ type Key struct {
 	types.ConnectionKey
 }
 
+// NewKey creates a new redis key
+func NewKey(saddr, daddr util.Address, sport, dport uint16) Key {
+	return Key{
+		ConnectionKey: types.NewConnectionKey(saddr, daddr, sport, dport),
+	}
+}
+
 // RequestStat represents a group of Redis transactions.
 type RequestStat struct{}
 
+
+// CombineWith merges the data in 2 RequestStats objects
+// newStats is kept as it is, while the method receiver gets mutated
 func (r *RequestStat) CombineWith(*RequestStat) {}
