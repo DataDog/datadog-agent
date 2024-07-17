1ec5649b01362ccfddd82e2f7b7109c63cf75fd3
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-07-17 11:06:37 +0200

    [USM] ebpf: compute batch sizes based on the batch buffer size

--- pkg/network/ebpf/c/protocols/http/usm-events.h
+++ pkg/network/ebpf/c/protocols/http/usm-events.h
@@ -4,6 +4,9 @@
 #include "protocols/events.h"
 #include "protocols/http/types.h"
 
+// This controls the number of HTTP transactions read from userspace at a time
+#define HTTP_BATCH_SIZE (MAX_BATCH_SIZE(http_event_t))
+
 USM_EVENTS_INIT(http, http_event_t, HTTP_BATCH_SIZE);
 
 #endif
