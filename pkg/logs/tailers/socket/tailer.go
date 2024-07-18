5abff90e0767f255576c055b1aba4bfce786bd10
andrewqian2001datadog <andrew.qian@datadoghq.com>
2024-07-18 13:34:12 -0400

    WIP

--- pkg/logs/tailers/socket/tailer.go
+++ pkg/logs/tailers/socket/tailer.go
@@ -7,6 +7,7 @@
 package socket
 
 import (
+	"fmt"
 	"io"
 	"net"
 
@@ -14,6 +15,7 @@ import (
 
 	"github.com/DataDog/datadog-agent/pkg/logs/internal/decoder"
 	"github.com/DataDog/datadog-agent/pkg/logs/internal/parsers/noop"
+	"github.com/DataDog/datadog-agent/pkg/logs/internal/tag"
 	"github.com/DataDog/datadog-agent/pkg/logs/message"
 	"github.com/DataDog/datadog-agent/pkg/logs/sources"
 	status "github.com/DataDog/datadog-agent/pkg/logs/status/utils"
@@ -22,22 +24,25 @@ import (
 // Tailer reads data from a net.Conn.  It uses a `read` callback to be generic
 // over types of connections.
 type Tailer struct {
-	source     *sources.LogSource
-	Conn       net.Conn
-	outputChan chan *message.Message
-	read       func(*Tailer) ([]byte, error)
-	decoder    *decoder.Decoder
-	stop       chan struct{}
-	done       chan struct{}
+	source      *sources.LogSource
+	Conn        net.Conn
+	tagProvider tag.Provider
+	outputChan  chan *message.Message
+	read        func(*Tailer) ([]byte, error)
+	decoder     *decoder.Decoder
+	stop        chan struct{}
+	done        chan struct{}
 }
 
 // NewTailer returns a new Tailer
 func NewTailer(source *sources.LogSource, conn net.Conn, outputChan chan *message.Message, read func(*Tailer) ([]byte, error)) *Tailer {
+	tagProvider := tag.NewLocalProvider([]string{})
 	return &Tailer{
-		source:     source,
-		Conn:       conn,
-		outputChan: outputChan,
-		read:       read,
+		source:      source,
+		Conn:        conn,
+		tagProvider: tagProvider,
+		outputChan:  outputChan,
+		read:        read,
 		// tailer info is currently unused for this tailer type.
 		decoder: decoder.InitializeDecoder(sources.NewReplaceableSource(source), noop.New(), status.NewInfoRegistry()),
 		stop:    make(chan struct{}, 1),
@@ -65,16 +70,12 @@ func (t *Tailer) forwardMessages() {
 		// the decoder has successfully been flushed
 		t.done <- struct{}{}
 	}()
-	log.Debug("andrewqian", t.source.Config.Port)
-	log.Debug("andrewqian2", t.source.Config.Tags)
 	for output := range t.decoder.OutputChan {
 		if len(output.GetContent()) > 0 {
-			t.outputChan <- message.NewMessageWithSource(output.GetContent(), message.StatusInfo, t.source, output.IngestionTimestamp)
-			log.Debug("andrewqian3", t.outputChan)
-			log.Debug("------------------------------------------------------------")
-			log.Debug("andrewqian4", t)
-			log.Debug("------------------------------------------------------------")
-
+			origin := message.NewOrigin(t.source)
+			source_host_tag := fmt.Sprintf("source_host:%d", t.source.Config.Port)
+			origin.SetTags(append(t.source.Config.Tags, source_host_tag))
+			t.outputChan <- message.NewMessage(output.GetContent(), origin, output.Status, output.IngestionTimestamp)
 		}
 	}
 }
