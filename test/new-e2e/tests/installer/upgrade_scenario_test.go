de869476d8cc14d15a0b37e6fe4d8ce3281e0dc6
Erwann <erwann.masson@datadoghq.com>
2024-07-18 14:37:08 +0200

    update latest version used

--- test/new-e2e/tests/installer/upgrade_scenario_test.go
+++ test/new-e2e/tests/installer/upgrade_scenario_test.go
@@ -83,7 +83,7 @@ const (
 	previousAgentImageVersion = "7.54.0-1"
 	latestAgentImageVersion   = "7.54.1-1"
 
-	latestInstallerImageVersion   = "7.55.0-installer-0.4.2-1"
+	latestInstallerImageVersion   = "7.56.0-installer-0.4.5-1"
 	previousInstallerImageVersion = "7.55.0-installer-0.4.1-1"
 )
 
