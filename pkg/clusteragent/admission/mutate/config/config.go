cb45c95b59432795a14e1cfc1dd005c9e988a093
Minyi Zhu <minyi.zhu@datadoghq.com>
2024-07-25 15:37:23 -0400

    revert

--- pkg/clusteragent/admission/mutate/config/config.go
+++ pkg/clusteragent/admission/mutate/config/config.go
@@ -14,7 +14,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/samber/lo"
 	admiv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -94,15 +93,9 @@ var (
 	}
 )
 
-// conf is the configuration for the webhook
-type conf struct {
-	injectContName bool
-}
-
 // Webhook is the webhook that injects DD_AGENT_HOST and DD_ENTITY_ID into a pod
 type Webhook struct {
 	name            string
-	config          conf
 	isEnabled       bool
 	endpoint        string
 	resources       []string
@@ -115,10 +108,7 @@ type Webhook struct {
 // NewWebhook returns a new Webhook
 func NewWebhook(wmeta workloadmeta.Component, injectionFilter common.InjectionFilter) *Webhook {
 	return &Webhook{
-		name: webhookName,
-		config: conf{
-			injectContName: config.Datadog().GetBool("admission_controller.inject_config.inject_container_name"),
-		},
+		name:            webhookName,
 		isEnabled:       config.Datadog().GetBool("admission_controller.inject_config.enabled"),
 		endpoint:        config.Datadog().GetString("admission_controller.inject_config.endpoint"),
 		resources:       []string{"pods"},
@@ -204,12 +194,7 @@ func (w *Webhook) inject(pod *corev1.Pod, _ string, _ dynamic.Interface) (bool, 
 		return false, errors.New(metrics.InvalidInput)
 	}
 
-	// Inject DD_ENTITY_ID
-	if w.config.injectContName {
-		injectedEntity = injectFullIdentity(pod)
-	} else {
-		injectedEntity = common.InjectEnv(pod, defaultDdEntityIDEnvVar)
-	}
+	injectedEntity = common.InjectEnv(pod, defaultDdEntityIDEnvVar)
 
 	// Inject External Data Environment Variable
 	injectedExternalEnv = injectExternalDataEnvVar(pod)
@@ -275,56 +260,6 @@ func injectExternalDataEnvVar(pod *corev1.Pod) (injected bool) {
 		injected = true
 	}
 
-	return injected
-}
-
-func injectIdentityInContainer(container *corev1.Container, prefix, podStr string) bool {
-	if container == nil {
-		_ = log.Errorf("Cannot inject identity into nil container")
-		return false
-	}
-	// Do not override DD_ENTITY_ID if it's already set
-	if lo.ContainsBy(container.Env, func(v corev1.EnvVar) bool { return v.Name == ddEntityIDEnvVarName }) {
-		log.Debugf("Ignoring container '%s' in pod %s: env var '%s' already exist", container.Name, podStr, ddEntityIDEnvVarName)
-		return false
-	}
-
-	// We can and should override DD_INTERNAL_* variables if they are already set
-	container.Env = lo.Filter(container.Env, func(v corev1.EnvVar, _ int) bool { return v.Name != podUIDEnvVarName })
-	addedEnv := []corev1.EnvVar{
-		// DD_INTERNAL_POD_UID must precede DD_ENTITY_ID to be referenced in the latter.
-		// See https://kubernetes.io/docs/tasks/inject-data-application/define-interdependent-environment-variables/
-		{
-			Name: podUIDEnvVarName,
-			ValueFrom: &corev1.EnvVarSource{
-				FieldRef: &corev1.ObjectFieldSelector{
-					FieldPath: "metadata.uid",
-				},
-			},
-		},
-		{
-			Name:  ddEntityIDEnvVarName,
-			Value: fmt.Sprintf("en-%s$(%s)/%s", prefix, podUIDEnvVarName, container.Name),
-		},
-	}
-
-	// prepend rather than append so that our new vars precede container vars in the final list, so that they
-	// can be referenced in other env vars downstream.  (see:  Kubernetes dependent environment variables.)
-	container.Env = append(addedEnv, container.Env...)
-	return true
-}
-
-// injectFullIdentity injects `DD_INTERNAL_CONTAINER_NAME`, `DD_INTERNAL_POD_UID` and `DD_ENTITY_ID`
-// as `en-(init.)$(DD_INTERNAL_POD_UID)/$(DD_INTERNAL_CONTAINER_NAME)`.
-func injectFullIdentity(pod *corev1.Pod) bool {
-	injected := false
-	podStr := common.PodString(pod)
-	for i := range pod.Spec.Containers {
-		injected = injectIdentityInContainer(&pod.Spec.Containers[i], "", podStr) || injected
-	}
-	for i := range pod.Spec.InitContainers {
-		injected = injectIdentityInContainer(&pod.Spec.InitContainers[i], "init.", podStr) || injected
-	}
 	return injected
 }
 
