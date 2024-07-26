c88c64975f363eb2c2dc4ce8dc9e3746e3e0a8a2
Julien Lebot <julien.lebot@datadoghq.com>
2024-07-26 11:23:33 +0200

    Fix missing override

--- test/new-e2e/tests/installer/package_definitions.go
+++ test/new-e2e/tests/installer/package_definitions.go
@@ -8,6 +8,8 @@ package installer
 
 import (
 	"fmt"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner/parameters"
 	e2eos "github.com/DataDog/test-infra-definitions/components/os"
 	"os"
 	"strings"
@@ -44,12 +46,11 @@ var packagesConfig = []testPackageConfig{
 func installScriptPackageManagerEnv(env map[string]string, arch e2eos.Architecture) {
 	apiKey := os.Getenv("DD_API_KEY")
 	if apiKey == "" {
-		// Doesn't seem to work
-		// var err error
-		// apiKey, err = runner.GetProfile().SecretStore().Get(parameters.APIKey)
-		// if apiKey == "" || err != nil {
-		apiKey = "deadbeefdeadbeefdeadbeefdeadbeef"
-		// }
+		var err error
+		apiKey, err = runner.GetProfile().SecretStore().Get(parameters.APIKey)
+		if apiKey == "" || err != nil {
+			apiKey = "deadbeefdeadbeefdeadbeefdeadbeef"
+		}
 	}
 	env["DD_API_KEY"] = apiKey
 	env["DD_SITE"] = "datadoghq.com"
