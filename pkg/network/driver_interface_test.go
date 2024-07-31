5dbc9de6e2f128488bb944026ab05a0812fe283b
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-07-31 18:18:29 +0200

    continue fixing linter issues

--- pkg/network/driver_interface_test.go
+++ pkg/network/driver_interface_test.go
@@ -69,7 +69,7 @@ func TestConnectionStatsInfiniteLoop(t *testing.T) {
 	activeBuf := NewConnectionBuffer(startSize, minSize)
 	closedBuf := NewConnectionBuffer(startSize, minSize)
 
-	di, err := NewDriverInterface(config.New(), func(_ uint32, _ driver.HandleType, telemetry telemetry.Component) (driver.Handle, error) {
+	di, err := NewDriverInterface(config.New(), func(_ uint32, _ driver.HandleType, _ telemetry.Component) (driver.Handle, error) {
 		return &TestDriverHandleInfiniteLoop{t: t}, nil
 	}, nil)
 	require.NoError(t, err, "Failed to create new driver interface")
