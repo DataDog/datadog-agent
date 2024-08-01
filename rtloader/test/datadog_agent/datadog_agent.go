c5279b5ef46245440a59180c228b03821c744804
Natasha Dada <natasha.dada@datadoghq.com>
2024-08-01 13:08:38 -0400

    clean up

--- rtloader/test/datadog_agent/datadog_agent.go
+++ rtloader/test/datadog_agent/datadog_agent.go
@@ -29,7 +29,6 @@ extern void doLog(char*, int);
 extern void getClustername(char **);
 extern void getConfig(char *, char **);
 extern void getHostname(char **);
-extern void getHostTags(char **);
 extern bool getTracemallocEnabled();
 extern void getVersion(char **);
 extern void headers(char **);
@@ -49,7 +48,6 @@ static void initDatadogAgentTests(rtloader_t *rtloader) {
    set_get_clustername_cb(rtloader, getClustername);
    set_get_config_cb(rtloader, getConfig);
    set_get_hostname_cb(rtloader, getHostname);
-   set_get_host_tags_cb(rtloader, getHostTags);
    set_tracemalloc_enabled_cb(rtloader, getTracemallocEnabled);
    set_get_version_cb(rtloader, getVersion);
    set_headers_cb(rtloader, headers);
