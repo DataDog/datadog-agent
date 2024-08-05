880b8abeab8213bd1cc30036a68f1c87dca6dbe0
Florent Clarret <florent.clarret@datadoghq.com>
2024-08-05 09:16:28 +0200

    add retry

---
+++ tasks/libs/common/docker_hub.py
@@ -0,0 +1,16 @@
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry  # type: ignore
+
+
+def get_latest_version(repository, namespace="datadog") -> str:
+    session = requests.session()
+    retries = Retry(total=3, backoff_factor=1)
+    session.mount('https://', HTTPAdapter(max_retries=retries))
+
+    response = session.get(f"https://hub.docker.com/v2/namespaces/{namespace}/repositories/{repository}/tags")
+    response.raise_for_status()
+
+    return sorted(
+        [result["name"] for result in response.json()["results"] if result["name"] != "latest"], reverse=True
+    )[0]
