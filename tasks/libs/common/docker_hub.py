4a40123311a55e063dde12e4dceb56ee56dca65b
Florent Clarret <florent.clarret@datadoghq.com>
2024-08-05 11:39:51 +0200

    address

--- tasks/libs/common/docker_hub.py
+++ tasks/libs/common/docker_hub.py
@@ -1,4 +1,5 @@
 import requests
+import semver
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry  # type: ignore
 
@@ -11,6 +12,13 @@ def get_latest_version(repository: str, namespace: str = "datadog") -> str:
     response = session.get(f"https://hub.docker.com/v2/namespaces/{namespace}/repositories/{repository}/tags")
     response.raise_for_status()
 
-    return sorted(
-        [result["name"] for result in response.json()["results"] if result["name"] != "latest"], reverse=True
-    )[0]
+    latest_release = semver.VersionInfo(0)
+
+    for version in response.json()["results"]:
+        name = version["name"]
+        if semver.VersionInfo.isvalid(name):
+            current_version = semver.VersionInfo.parse(name)
+            if current_version > latest_release:
+                latest_release = current_version
+
+    return str(latest_release)
