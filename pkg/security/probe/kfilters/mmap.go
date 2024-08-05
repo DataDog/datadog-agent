a42ffe80bcbdc5f7082c9e9899b6198729f7364b
Sylvain Afchain <safchain@gmail.com>
2024-08-05 14:08:46 +0200

    [CWS] make approver/kfilter naming a bit more consistent (#28167)

--- pkg/security/probe/kfilters/mmap.go
+++ pkg/security/probe/kfilters/mmap.go
@@ -32,33 +32,30 @@ var mmapCapabilities = Capabilities{
 	},
 }
 
-func mmapOnNewApprovers(approvers rules.Approvers) (ActiveApprovers, error) {
-	mmapApprovers, err := onNewBasenameApprovers(model.MMapEventType, "file", approvers)
+func mmapKFilters(approvers rules.Approvers) (ActiveKFilters, error) {
+	mmapKFilters, err := getBasenameKFilters(model.MMapEventType, "file", approvers)
 	if err != nil {
 		return nil, err
 	}
 
 	for field, values := range approvers {
 		switch field {
-		case "mmap.file.name", "mmap.file.path": // already handled by onNewBasenameApprovers
+		case "mmap.file.name", "mmap.file.path": // already handled by getBasenameKFilters
 		case "mmap.flags":
-			var approver activeApprover
-			approver, err = approveFlags("mmap_flags_approvers", intValues[int32](values)...)
+			kfilter, err := getFlagsKFilters("mmap_flags_approvers", intValues[int32](values)...)
 			if err != nil {
 				return nil, err
 			}
-			mmapApprovers = append(mmapApprovers, approver)
+			mmapKFilters = append(mmapKFilters, kfilter)
 		case "mmap.protection":
-			var approver activeApprover
-			approver, err = approveFlags("mmap_protection_approvers", intValues[int32](values)...)
+			kfilter, err := getFlagsKFilters("mmap_protection_approvers", intValues[int32](values)...)
 			if err != nil {
 				return nil, err
 			}
-			mmapApprovers = append(mmapApprovers, approver)
-
+			mmapKFilters = append(mmapKFilters, kfilter)
 		default:
 			return nil, fmt.Errorf("unknown field '%s'", field)
 		}
 	}
-	return newActiveKFilters(mmapApprovers...), nil
+	return newActiveKFilters(mmapKFilters...), nil
 }
