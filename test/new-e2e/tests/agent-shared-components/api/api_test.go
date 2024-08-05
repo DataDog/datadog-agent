fec763d0c58f3432a33a89605a8a9ec94e69e6c0
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2024-08-05 09:44:01 +0200

    small test

--- test/new-e2e/tests/agent-shared-components/api/api_test.go
+++ test/new-e2e/tests/agent-shared-components/api/api_test.go
@@ -468,17 +468,60 @@ func (v *apiSuite) TestDefaultAgentAPIEndpoints() {
 			},
 			expectedCode: 200,
 			assert: func(ct *assert.CollectT, e agentEndpointInfo, resp *http.Response) {
-				type Diagnose struct {
-					Suite string `json:"SuiteName"`
+				// Diagnosis contains the results of the diagnosis
+				type Diagnosis struct {
+					// --------------------------
+					// required fields
+
+					// run-time (pass, fail etc)
+					Result Result `json:"result"`
+					// static-time (meta typically)
+					Name string `json:"name"`
+					// run-time (actual diagnosis consumable by a user)
+					Diagnosis string `json:"diagnosis"`
+
+					// --------------------------
+					// optional fields
+
+					// static-time (meta typically)
+					Category string `json:"category,omitempty"`
+					// static-time (meta typically, description of what being tested)
+					Description string `json:"description,omitempty"`
+					// run-time (what can be done of what docs need to be consulted to address the issue)
+					Remediation string `json:"remediation,omitempty"`
+					// run-time
+					RawError string `json:"rawerror,omitempty"`
 				}
-				var have []Diagnose
+
+				// DiagnoseResult contains the results of the diagnose command
+				type DiagnoseResult struct {
+					Diagnoses []Diagnoses `json:"runs"`
+					Summary   Counters    `json:"summary"`
+				}
+
+				// Diagnoses is a collection of Diagnosis
+				type Diagnoses struct {
+					SuiteName      string      `json:"suite_name"`
+					SuiteDiagnoses []Diagnosis `json:"diagnoses"`
+				}
+
+				// Counters contains the count of the diagnosis results
+				type Counters struct {
+					Total         int `json:"total,omitempty"`
+					Success       int `json:"success,omitempty"`
+					Fail          int `json:"fail,omitempty"`
+					Warnings      int `json:"warnings,omitempty"`
+					UnexpectedErr int `json:"unexpected_error,omitempty"`
+				}
+				var have DiagnoseResult
 
 				body, err := io.ReadAll(resp.Body)
 				assert.NoError(ct, err, "failed to read body from request")
 
 				err = json.Unmarshal(body, &have)
 				assert.NoError(ct, err)
-				assert.NotZero(ct, len(have), "%s %s returned: %s, expected diagnose suites to be present", e.method, e.endpoint, body)
+				assert.NotNil(ct, have)
+				assert.NotZero(ct, len(have.Diagnoses), "%s %s returned: %s, expected diagnose suites to be present", e.method, e.endpoint, body)
 			},
 		},
 	}
