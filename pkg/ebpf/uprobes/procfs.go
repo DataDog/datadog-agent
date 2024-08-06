bd9c22a04f97b2620691e17b750082ec3b4a2ce5
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-08-06 17:07:41 +0200

    Remove unused code

--- pkg/ebpf/uprobes/procfs.go
+++ pkg/ebpf/uprobes/procfs.go
@@ -15,8 +15,6 @@ import (
 	"strconv"
 	"sync"
 	"time"
-
-	ddsync "github.com/DataDog/datadog-agent/pkg/util/sync"
 )
 
 const procFSUpdateTimeout = 10 * time.Millisecond
@@ -118,61 +116,4 @@ func (p *ProcInfo) Comm() (string, error) {
 	}
 
 	return p.comm, nil
-}
-
-var readBufferPool = ddsync.NewSlicePool[byte](128, 128)
-
-func (p *ProcInfo) readCmdline(cmdlineFile string) (string, error) {
-	file, err := os.Open(cmdlineFile)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	bufferPtr := readBufferPool.Get()
-	defer readBufferPool.Put(bufferPtr)
-
-	buffer := *bufferPtr
-	n, _ := file.Read(buffer)
-	if n == 0 {
-		return "", nil
-	}
-
-	buffer = buffer[:n]
-	i := bytes.Index(buffer, []byte{0})
-	if i == -1 {
-		return "", nil
-	}
-
-	return string(buffer[:i]), nil
-}
-
-func (p *ProcInfo) FindCmdlineWord(wordEnd []byte) (string, error) {
-	f, err := waitUntilSucceeds(p, "cmdline", os.Open)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	// From here on we shouldn't allocate for the common case
-	// (eg., a process is *not* the pattern)
-	bufferPtr := readBufferPool.Get()
-	defer func() {
-		readBufferPool.Put(bufferPtr)
-	}()
-
-	buffer := *bufferPtr
-	n, _ := f.Read(buffer)
-	if n == 0 {
-		return "", nil
-	}
-
-	buffer = buffer[:n]
-	i := bytes.Index(buffer, wordEnd)
-	if i < 0 {
-		return "", nil
-	}
-
-	executable := buffer[:i+len(wordEnd)]
-	return string(executable), nil
 }
