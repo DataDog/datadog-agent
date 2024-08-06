4982bee137e4cb87eb24cb02ecd22d19e498c05c
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-08-06 12:24:45 +0200

    Remove debug print

--- pkg/ebpf/uprobes/attacher.go
+++ pkg/ebpf/uprobes/attacher.go
@@ -408,7 +408,6 @@ func (ua *UprobeAttacher) Sync(trackDeletions bool) error {
 	var deletionCandidates map[uint32]struct{}
 	if trackDeletions {
 		deletionCandidates = ua.fileRegistry.GetRegisteredProcesses()
-		fmt.Println("deletionCandidates", deletionCandidates)
 	}
 	thisPID, err := kernel.RootNSPID()
 	if err != nil {
