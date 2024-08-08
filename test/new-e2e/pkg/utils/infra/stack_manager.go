4eb3679267974f4633e8d90dd2f4e2a8a5819c43
Florent Clarret <florent.clarret@datadoghq.com>
2024-08-08 12:37:06 +0000

    Mark AWS errors as internal errors when retrieving values in SSM (#28307)

--- test/new-e2e/pkg/utils/infra/stack_manager.go
+++ test/new-e2e/pkg/utils/infra/stack_manager.go
@@ -10,6 +10,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/common"
 	"io"
 	"os"
 	"runtime"
@@ -50,19 +51,6 @@ var (
 	stackManager     *StackManager
 	initStackManager sync.Once
 )
-
-type internalError struct {
-	err error
-}
-
-func (i internalError) Error() string {
-	return fmt.Sprintf("E2E INTERNAL ERROR: %v", i.err)
-}
-
-func (i internalError) Is(target error) bool {
-	_, ok := target.(internalError)
-	return ok
-}
 
 // StackManager handles
 type StackManager struct {
@@ -129,7 +117,7 @@ func newStackManager() (*StackManager, error) {
 func (sm *StackManager) GetStack(ctx context.Context, name string, config runner.ConfigMap, deployFunc pulumi.RunFunc, failOnMissing bool) (_ *auto.Stack, _ auto.UpResult, err error) {
 	defer func() {
 		if err != nil {
-			err = internalError{err}
+			err = common.InternalError{Err: err}
 		}
 	}()
 
@@ -216,7 +204,7 @@ func WithCancelTimeout(cancelTimeout time.Duration) GetStackOption {
 func (sm *StackManager) GetStackNoDeleteOnFailure(ctx context.Context, name string, deployFunc pulumi.RunFunc, options ...GetStackOption) (_ *auto.Stack, _ auto.UpResult, err error) {
 	defer func() {
 		if err != nil {
-			err = internalError{err}
+			err = common.InternalError{Err: err}
 		}
 	}()
 
@@ -227,7 +215,7 @@ func (sm *StackManager) GetStackNoDeleteOnFailure(ctx context.Context, name stri
 func (sm *StackManager) DeleteStack(ctx context.Context, name string, logWriter io.Writer) (err error) {
 	defer func() {
 		if err != nil {
-			err = internalError{err}
+			err = common.InternalError{Err: err}
 		}
 	}()
 
@@ -257,7 +245,7 @@ func (sm *StackManager) DeleteStack(ctx context.Context, name string, logWriter 
 func (sm *StackManager) ForceRemoveStackConfiguration(ctx context.Context, name string) (err error) {
 	defer func() {
 		if err != nil {
-			err = internalError{err}
+			err = common.InternalError{Err: err}
 		}
 	}()
 
@@ -278,7 +266,7 @@ func (sm *StackManager) Cleanup(ctx context.Context) []error {
 	sm.stacks.Range(func(stackID string, stack *auto.Stack) {
 		err := sm.deleteStack(ctx, stackID, stack, nil, nil)
 		if err != nil {
-			errors = append(errors, internalError{err})
+			errors = append(errors, common.InternalError{Err: err})
 		}
 	})
 
@@ -573,7 +561,7 @@ func sendEventToDatadog(sender datadogEventSender, title string, message string,
 func (sm *StackManager) GetPulumiStackName(name string) (_ string, err error) {
 	defer func() {
 		if err != nil {
-			err = internalError{err}
+			err = common.InternalError{Err: err}
 		}
 	}()
 
