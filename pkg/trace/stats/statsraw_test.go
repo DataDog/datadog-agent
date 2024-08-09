DataDog/datadog-agent

commit 0427ab5d790fcca60ae04aa23f19c18fbaa39099

APM: pull span stats concentration into separate struct (#28107)

pkg/trace/stats/span_concentrator.go
@@ -1,0 +1,147 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package stats
+
+import (
+	"sync"
+	"time"
+
+	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/trace"
+	"github.com/DataDog/datadog-agent/pkg/trace/log"
+	"github.com/DataDog/datadog-agent/pkg/trace/traceutil"
+)
+
+// SpanConcentratorConfig exposes configuration options for a SpanConcentrator
+type SpanConcentratorConfig struct {
+	// ComputeStatsBySpanKind enables/disables the computing of stats based on a span's `span.kind` field
+	ComputeStatsBySpanKind bool
+	// BucketInterval the size of our pre-aggregation per bucket
+	BucketInterval int64
+	// PeerTags additional tags to use for peer entity stats aggregation, nil if disabled
+	PeerTags []string
+}
+
+// SpanConcentrator produces time bucketed statistics from a stream of raw spans.
+type SpanConcentrator struct {
+	computeStatsBySpanKind bool
+	// bucket duration in nanoseconds
+	bsize int64
+	// Timestamp of the oldest time bucket for which we allow data.
+	// Any ingested stats older than it get added to this bucket.
+	oldestTs int64
+	// bufferLen is the number of 10s stats bucket we keep in memory before flushing them.
+	// It means that we can compute stats only for the last `bufferLen * bsize` and that we
+	// wait such time before flushing the stats.
+	// This only applies to past buckets. Stats buckets in the future are allowed with no restriction.
+	bufferLen   int
+	peerTagKeys []string // keys for supplementary tags that describe peer.service entities, nil if disabled
+
+	// mu protects the buckets field
+	mu      sync.Mutex
+	buckets map[int64]*RawBucket
+}
+
+// NewSpanConcentrator builds a new SpanConcentrator object
+func NewSpanConcentrator(cfg *SpanConcentratorConfig, now time.Time) *SpanConcentrator {
+	sc := &SpanConcentrator{
+		computeStatsBySpanKind: false,
+		bsize:                  cfg.BucketInterval,
+		oldestTs:               alignTs(now.UnixNano(), cfg.BucketInterval),
+		bufferLen:              defaultBufferLen,
+		mu:                     sync.Mutex{},
+		buckets:                make(map[int64]*RawBucket),
+		peerTagKeys:            cfg.PeerTags,
+	}
+	return sc
+}
+
+func (sc *SpanConcentrator) addSpan(s *pb.Span, aggKey PayloadAggregationKey, containerID string, containerTags []string, origin string, weight float64) {
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+	isTop := traceutil.HasTopLevel(s)
+	eligibleSpanKind := sc.computeStatsBySpanKind && computeStatsForSpanKind(s)
+	if !(isTop || traceutil.IsMeasured(s) || eligibleSpanKind) {
+		return
+	}
+	if traceutil.IsPartialSnapshot(s) {
+		return
+	}
+	end := s.Start + s.Duration
+	btime := end - end%sc.bsize
+
+	// If too far in the past, count in the oldest-allowed time bucket instead.
+	if btime < sc.oldestTs {
+		btime = sc.oldestTs
+	}
+
+	b, ok := sc.buckets[btime]
+	if !ok {
+		b = NewRawBucket(uint64(btime), uint64(sc.bsize))
+		if containerID != "" && len(containerTags) > 0 {
+			b.containerTagsByID[containerID] = containerTags
+		}
+		sc.buckets[btime] = b
+	}
+	b.HandleSpan(s, weight, isTop, origin, aggKey, sc.peerTagKeys)
+}
+
+// AddSpan to the SpanConcentrator, appending the new data to the appropriate internal bucket.
+func (sc *SpanConcentrator) AddSpan(s *pb.Span, aggKey PayloadAggregationKey, containerID string, containerTags []string, origin string) {
+	sc.addSpan(s, aggKey, containerID, containerTags, origin, 1)
+}
+
+// Flush deletes and returns complete ClientStatsPayloads.
+// The force boolean guarantees flushing all buckets if set to true.
+func (sc *SpanConcentrator) Flush(now int64, force bool) []*pb.ClientStatsPayload {
+	m := make(map[PayloadAggregationKey][]*pb.ClientStatsBucket)
+	containerTagsByID := make(map[string][]string)
+
+	sc.mu.Lock()
+	for ts, srb := range sc.buckets {
+		// Always keep `bufferLen` buckets (default is 2: current + previous one).
+		// This is a trade-off: we accept slightly late traces (clock skew and stuff)
+		// but we delay flushing by at most `bufferLen` buckets.
+		//
+		// This delay might result in not flushing stats payload (data loss)
+		// if the agent stops while the latest buckets aren't old enough to be flushed.
+		// The "force" boolean skips the delay and flushes all buckets, typically on agent shutdown.
+		if !force && ts > now-int64(sc.bufferLen)*sc.bsize {
+			log.Tracef("Bucket %d is not old enough to be flushed, keeping it", ts)
+			continue
+		}
+		log.Debugf("Flushing bucket %d", ts)
+		for k, b := range srb.Export() {
+			m[k] = append(m[k], b)
+			if ctags, ok := srb.containerTagsByID[k.ContainerID]; ok {
+				containerTagsByID[k.ContainerID] = ctags
+			}
+		}
+		delete(sc.buckets, ts)
+	}
+	// After flushing, update the oldest timestamp allowed to prevent having stats for
+	// an already-flushed bucket.
+	newOldestTs := alignTs(now, sc.bsize) - int64(sc.bufferLen-1)*sc.bsize
+	if newOldestTs > sc.oldestTs {
+		log.Debugf("Update oldestTs to %d", newOldestTs)
+		sc.oldestTs = newOldestTs
+	}
+	sc.mu.Unlock()
+	sb := make([]*pb.ClientStatsPayload, 0, len(m))
+	for k, s := range m {
+		p := &pb.ClientStatsPayload{
+			Env:          k.Env,
+			Hostname:     k.Hostname,
+			ContainerID:  k.ContainerID,
+			Version:      k.Version,
+			GitCommitSha: k.GitCommitSha,
+			ImageTag:     k.ImageTag,
+			Stats:        s,
+			Tags:         containerTagsByID[k.ContainerID],
+		}
+		sb = append(sb, p)
+	}
+	return sb
+}

comp/trace/config/setup.go
@@ -17,8 +17,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/DataDog/opentelemetry-mapping-go/pkg/otlp/attributes"
 	"go.opentelemetry.io/collector/component/componenttest"
+
+	"github.com/DataDog/opentelemetry-mapping-go/pkg/otlp/attributes"
 
 	corecompcfg "github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/comp/core/tagger"

pkg/trace/stats/concentrator.go
@@ -36,47 +36,34 @@ type Writer interface {
 type Concentrator struct {
 	Writer Writer
 
+	spanConcentrator *SpanConcentrator
 	// bucket duration in nanoseconds
-	bsize int64
-	// Timestamp of the oldest time bucket for which we allow data.
-	// Any ingested stats older than it get added to this bucket.
-	oldestTs int64
-	// bufferLen is the number of 10s stats bucket we keep in memory before flushing them.
-	// It means that we can compute stats only for the last `bufferLen * bsize` and that we
-	// wait such time before flushing the stats.
-	// This only applies to past buckets. Stats buckets in the future are allowed with no restriction.
-	bufferLen              int
-	exit                   chan struct{}
-	exitWG                 sync.WaitGroup
-	buckets                map[int64]*RawBucket // buckets used to aggregate stats per timestamp
-	mu                     sync.Mutex
-	agentEnv               string
-	agentHostname          string
-	agentVersion           string
-	computeStatsBySpanKind bool     // flag to enable computation of stats through checking the span.kind field
-	peerTagKeys            []string // keys for supplementary tags that describe peer.service entities. This should remain unset if peer tag aggregation is not enabled.
-	statsd                 statsd.ClientInterface
+	bsize         int64
+	exit          chan struct{}
+	exitWG        sync.WaitGroup
+	agentEnv      string
+	agentHostname string
+	agentVersion  string
+	statsd        statsd.ClientInterface
 }
 
 // NewConcentrator initializes a new concentrator ready to be started
 func NewConcentrator(conf *config.AgentConfig, writer Writer, now time.Time, statsd statsd.ClientInterface) *Concentrator {
 	bsize := conf.BucketInterval.Nanoseconds()
+	sc := NewSpanConcentrator(&SpanConcentratorConfig{
+		ComputeStatsBySpanKind: conf.ComputeStatsBySpanKind,
+		BucketInterval:         bsize,
+		PeerTags:               conf.ConfiguredPeerTags(),
+	}, now)
 	c := Concentrator{
-		bsize:   bsize,
-		buckets: make(map[int64]*RawBucket),
-		// At start, only allow stats for the current time bucket. Ensure we don't
-		// override buckets which could have been sent before an Agent restart.
-		oldestTs: alignTs(now.UnixNano(), bsize),
-		// TODO: Move to configuration.
-		bufferLen:              defaultBufferLen,
-		Writer:                 writer,
-		exit:                   make(chan struct{}),
-		agentEnv:               conf.DefaultEnv,
-		agentHostname:          conf.Hostname,
-		agentVersion:           conf.AgentVersion,
-		peerTagKeys:            conf.ConfiguredPeerTags(),
-		computeStatsBySpanKind: conf.ComputeStatsBySpanKind,
-		statsd:                 statsd,
+		spanConcentrator: sc,
+		Writer:           writer,
+		exit:             make(chan struct{}),
+		agentEnv:         conf.DefaultEnv,
+		agentHostname:    conf.Hostname,
+		agentVersion:     conf.AgentVersion,
+		statsd:           statsd,
+		bsize:            bsize,
 	}
 	return &c
 }
@@ -155,8 +142,6 @@ func NewStatsInput(numChunks int, containerID string, clientComputedStats bool, 
 
 // Add applies the given input to the concentrator.
 func (c *Concentrator) Add(t Input) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	for _, trace := range t.Traces {
 		c.addNow(&trace, t.ContainerID, t.ContainerTags)
 	}
@@ -183,31 +168,7 @@ func (c *Concentrator) addNow(pt *traceutil.ProcessedTrace, containerID string, 
 		ImageTag:     pt.ImageTag,
 	}
 	for _, s := range pt.TraceChunk.Spans {
-		isTop := traceutil.HasTopLevel(s)
-		eligibleSpanKind := c.computeStatsBySpanKind && computeStatsForSpanKind(s)
-		if !(isTop || traceutil.IsMeasured(s) || eligibleSpanKind) {
-			continue
-		}
-		if traceutil.IsPartialSnapshot(s) {
-			continue
-		}
-		end := s.Start + s.Duration
-		btime := end - end%c.bsize
-
-		// If too far in the past, count in the oldest-allowed time bucket instead.
-		if btime < c.oldestTs {
-			btime = c.oldestTs
-		}
-
-		b, ok := c.buckets[btime]
-		if !ok {
-			b = NewRawBucket(uint64(btime), uint64(c.bsize))
-			if containerID != "" && len(containerTags) > 0 {
-				b.containerTagsByID[containerID] = containerTags
-			}
-			c.buckets[btime] = b
-		}
-		b.HandleSpan(s, weight, isTop, pt.TraceChunk.Origin, aggKey, c.peerTagKeys)
+		c.spanConcentrator.addSpan(s, aggKey, containerID, containerTags, pt.TraceChunk.Origin, weight)
 	}
 }
 
@@ -218,54 +179,7 @@ func (c *Concentrator) Flush(force bool) *pb.StatsPayload {
 }
 
 func (c *Concentrator) flushNow(now int64, force bool) *pb.StatsPayload {
-	m := make(map[PayloadAggregationKey][]*pb.ClientStatsBucket)
-	containerTagsByID := make(map[string][]string)
-
-	c.mu.Lock()
-	for ts, srb := range c.buckets {
-		// Always keep `bufferLen` buckets (default is 2: current + previous one).
-		// This is a trade-off: we accept slightly late traces (clock skew and stuff)
-		// but we delay flushing by at most `bufferLen` buckets.
-		//
-		// This delay might result in not flushing stats payload (data loss)
-		// if the agent stops while the latest buckets aren't old enough to be flushed.
-		// The "force" boolean skips the delay and flushes all buckets, typically on agent shutdown.
-		if !force && ts > now-int64(c.bufferLen)*c.bsize {
-			log.Tracef("Bucket %d is not old enough to be flushed, keeping it", ts)
-			continue
-		}
-		log.Debugf("Flushing bucket %d", ts)
-		for k, b := range srb.Export() {
-			m[k] = append(m[k], b)
-			if ctags, ok := srb.containerTagsByID[k.ContainerID]; ok {
-				containerTagsByID[k.ContainerID] = ctags
-			}
-		}
-		delete(c.buckets, ts)
-	}
-	// After flushing, update the oldest timestamp allowed to prevent having stats for
-	// an already-flushed bucket.
-	newOldestTs := alignTs(now, c.bsize) - int64(c.bufferLen-1)*c.bsize
-	if newOldestTs > c.oldestTs {
-		log.Debugf("Update oldestTs to %d", newOldestTs)
-		c.oldestTs = newOldestTs
-	}
-	c.mu.Unlock()
-	sb := make([]*pb.ClientStatsPayload, 0, len(m))
-	for k, s := range m {
-		p := &pb.ClientStatsPayload{
-			Env:          k.Env,
-			Hostname:     k.Hostname,
-			ContainerID:  k.ContainerID,
-			Version:      k.Version,
-			GitCommitSha: k.GitCommitSha,
-			ImageTag:     k.ImageTag,
-			Stats:        s,
-			Tags:         containerTagsByID[k.ContainerID],
-		}
-		sb = append(sb, p)
-	}
-
+	sb := c.spanConcentrator.Flush(now, force)
 	return &pb.StatsPayload{Stats: sb, AgentHostname: c.agentHostname, AgentEnv: c.agentEnv, AgentVersion: c.agentVersion}
 }
 

pkg/trace/stats/concentrator_test.go
@@ -114,7 +114,7 @@ func TestNewConcentratorPeerTags(t *testing.T) {
 			Hostname:       "hostname",
 		}
 		c := NewConcentrator(&cfg, nil, time.Now(), statsd)
-		assert.Nil(c.peerTagKeys)
+		assert.Nil(c.spanConcentrator.peerTagKeys)
 	})
 	t.Run("with peer tags", func(t *testing.T) {
 		assert := assert.New(t)
@@ -127,7 +127,7 @@ func TestNewConcentratorPeerTags(t *testing.T) {
 			PeerTags:            []string{"zz_tag"},
 		}
 		c := NewConcentrator(&cfg, nil, time.Now(), statsd)
-		assert.Equal(cfg.ConfiguredPeerTags(), c.peerTagKeys)
+		assert.Equal(cfg.ConfiguredPeerTags(), c.spanConcentrator.peerTagKeys)
 	})
 }
 
@@ -144,7 +144,7 @@ func TestTracerHostname(t *testing.T) {
 	c := NewTestConcentrator(now)
 	c.addNow(testTrace, "", nil)
 
-	stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+	stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 	assert.Equal("tracer-hostname", stats.Stats[0].Hostname)
 }
 
@@ -173,7 +173,7 @@ func TestConcentratorOldestTs(t *testing.T) {
 		c := NewTestConcentrator(now)
 		c.addNow(testTrace, "", nil)
 
-		for i := 0; i < c.bufferLen; i++ {
+		for i := 0; i < c.spanConcentrator.bufferLen; i++ {
 			stats := c.flushNow(flushTime, false)
 			if !assert.Equal(0, len(stats.Stats), "We should get exactly 0 Bucket") {
 				t.FailNow()
@@ -208,10 +208,10 @@ func TestConcentratorOldestTs(t *testing.T) {
 	t.Run("hot", func(t *testing.T) {
 		flushTime := now.UnixNano()
 		c := NewTestConcentrator(now)
-		c.oldestTs = alignTs(flushTime, c.bsize) - int64(c.bufferLen-1)*c.bsize
+		c.spanConcentrator.oldestTs = alignTs(flushTime, c.bsize) - int64(c.spanConcentrator.bufferLen-1)*c.bsize
 		c.addNow(testTrace, "", nil)
 
-		for i := 0; i < c.bufferLen-1; i++ {
+		for i := 0; i < c.spanConcentrator.bufferLen-1; i++ {
 			stats := c.flushNow(flushTime, false)
 			if !assert.Equal(0, len(stats.Stats), "We should get exactly 0 Bucket") {
 				t.FailNow()
@@ -275,7 +275,7 @@ func TestConcentratorStatsTotals(t *testing.T) {
 
 	// update oldestTs as it running for quite some time, to avoid the fact that at startup
 	// it only allows recent stats.
-	c.oldestTs = alignedNow - int64(c.bufferLen)*c.bsize
+	c.spanConcentrator.oldestTs = alignedNow - int64(c.spanConcentrator.bufferLen)*c.bsize
 
 	// Build that simply have spans spread over time windows.
 	spans := []*pb.Span{
@@ -299,7 +299,7 @@ func TestConcentratorStatsTotals(t *testing.T) {
 		var topLevelHits uint64
 
 		flushTime := now.UnixNano()
-		for i := 0; i <= c.bufferLen; i++ {
+		for i := 0; i <= c.spanConcentrator.bufferLen; i++ {
 			stats := c.flushNow(flushTime, false)
 
 			if len(stats.Stats) == 0 {
@@ -331,7 +331,7 @@ func TestConcentratorStatsCounts(t *testing.T) {
 
 	// update oldestTs as it running for quite some time, to avoid the fact that at startup
 	// it only allows recent stats.
-	c.oldestTs = alignedNow - int64(c.bufferLen)*c.bsize
+	c.spanConcentrator.oldestTs = alignedNow - int64(c.spanConcentrator.bufferLen)*c.bsize
 
 	// Build a trace with stats which should cover 3 time buckets.
 	spans := []*pb.Span{
@@ -498,12 +498,12 @@ func TestConcentratorStatsCounts(t *testing.T) {
 
 	// flush every testBucketInterval
 	flushTime := now.UnixNano()
-	for i := 0; i <= c.bufferLen+2; i++ {
+	for i := 0; i <= c.spanConcentrator.bufferLen+2; i++ {
 		t.Run(fmt.Sprintf("flush-%d", i), func(t *testing.T) {
 			assert := assert.New(t)
 			stats := c.flushNow(flushTime, false)
 
-			expectedFlushedTs := alignTs(flushTime, c.bsize) - int64(c.bufferLen)*testBucketInterval
+			expectedFlushedTs := alignTs(flushTime, c.bsize) - int64(c.spanConcentrator.bufferLen)*testBucketInterval
 			if len(expectedCountValByKeyByTime[expectedFlushedTs]) == 0 {
 				// That's a flush for which we expect no data
 				return
@@ -542,7 +542,7 @@ func TestRootTag(t *testing.T) {
 	traceutil.ComputeTopLevel(spans)
 	testTrace := toProcessedTrace(spans, "none", "", "", "", "")
 	c := NewTestConcentrator(now)
-	c.computeStatsBySpanKind = true
+	c.spanConcentrator.computeStatsBySpanKind = true
 	c.addNow(testTrace, "", nil)
 
 	expected := []*pb.ClientGroupedStats{
@@ -582,7 +582,7 @@ func TestRootTag(t *testing.T) {
 		},
 	}
 
-	stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+	stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 	assertCountsEqual(t, expected, stats.Stats[0].Stats[0].Stats)
 }
 
@@ -592,7 +592,7 @@ func generateDistribution(t *testing.T, now time.Time, generator func(i int) int
 	alignedNow := alignTs(now.UnixNano(), c.bsize)
 	// update oldestTs as it running for quite some time, to avoid the fact that at startup
 	// it only allows recent stats.
-	c.oldestTs = alignedNow - int64(c.bufferLen)*c.bsize
+	c.spanConcentrator.oldestTs = alignedNow - int64(c.spanConcentrator.bufferLen)*c.bsize
 	// Build a trace with stats representing the distribution given by the generator
 	spans := []*pb.Span{}
 	for i := 0; i < 100; i++ {
@@ -600,7 +600,7 @@ func generateDistribution(t *testing.T, now time.Time, generator func(i int) int
 	}
 	traceutil.ComputeTopLevel(spans)
 	c.addNow(toProcessedTrace(spans, "none", "", "", "", ""), "", nil)
-	stats := c.flushNow(now.UnixNano()+c.bsize*int64(c.bufferLen), false)
+	stats := c.flushNow(now.UnixNano()+c.bsize*int64(c.spanConcentrator.bufferLen), false)
 	expectedFlushedTs := alignedNow
 	assert.Len(stats.Stats, 1)
 	assert.Len(stats.Stats[0].Stats, 1)
@@ -653,7 +653,7 @@ func TestIgnoresPartialSpans(t *testing.T) {
 	c := NewTestConcentrator(now)
 	c.addNow(testTrace, "", nil)
 
-	stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+	stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 	assert.Empty(stats.GetStats())
 }
 
@@ -667,19 +667,19 @@ func TestForceFlush(t *testing.T) {
 	c := NewTestConcentrator(now)
 	c.addNow(testTrace, "", nil)
 
-	assert.Len(c.buckets, 1)
+	assert.Len(c.spanConcentrator.buckets, 1)
 
 	// ts=0 so that flushNow always considers buckets not old enough to be flushed
 	ts := int64(0)
 
 	// Without force flush, flushNow should skip the bucket
 	stats := c.flushNow(ts, false)
-	assert.Len(c.buckets, 1)
+	assert.Len(c.spanConcentrator.buckets, 1)
 	assert.Len(stats.GetStats(), 0)
 
 	// With force flush, flushNow should flush buckets regardless of the age
 	stats = c.flushNow(ts, true)
-	assert.Len(c.buckets, 0)
+	assert.Len(c.spanConcentrator.buckets, 0)
 	assert.Len(stats.GetStats(), 1)
 }
 
@@ -732,7 +732,7 @@ func TestPeerTags(t *testing.T) {
 		testTrace := toProcessedTrace(spans, "none", "", "", "", "")
 		c := NewTestConcentrator(now)
 		c.addNow(testTrace, "", nil)
-		stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+		stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 		assert.Len(stats.Stats[0].Stats[0].Stats, 2)
 		for _, st := range stats.Stats[0].Stats[0].Stats {
 			assert.Nil(st.PeerTags)
@@ -743,9 +743,9 @@ func TestPeerTags(t *testing.T) {
 		traceutil.ComputeTopLevel(spans)
 		testTrace := toProcessedTrace(spans, "none", "", "", "", "")
 		c := NewTestConcentrator(now)
-		c.peerTagKeys = []string{"db.instance", "db.system", "peer.service"}
+		c.spanConcentrator.peerTagKeys = []string{"db.instance", "db.system", "peer.service"}
 		c.addNow(testTrace, "", nil)
-		stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+		stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 		assert.Len(stats.Stats[0].Stats[0].Stats, 2)
 		for _, st := range stats.Stats[0].Stats[0].Stats {
 			if st.Name == "postgres.query" {
@@ -808,7 +808,7 @@ func TestComputeStatsThroughSpanKindCheck(t *testing.T) {
 		testTrace := toProcessedTrace(spans, "none", "", "", "", "")
 		c := NewTestConcentrator(now)
 		c.addNow(testTrace, "", nil)
-		stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+		stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 		assert.Len(stats.Stats[0].Stats[0].Stats, 3)
 		opNames := make(map[string]struct{}, 3)
 		for _, s := range stats.Stats {
@@ -825,9 +825,9 @@ func TestComputeStatsThroughSpanKindCheck(t *testing.T) {
 		traceutil.ComputeTopLevel(spans)
 		testTrace := toProcessedTrace(spans, "none", "", "", "", "")
 		c := NewTestConcentrator(now)
-		c.computeStatsBySpanKind = true
+		c.spanConcentrator.computeStatsBySpanKind = true
 		c.addNow(testTrace, "", nil)
-		stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+		stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 		assert.Len(stats.Stats[0].Stats[0].Stats, 4)
 		opNames := make(map[string]struct{}, 4)
 		for _, s := range stats.Stats {
@@ -869,7 +869,7 @@ func TestVersionData(t *testing.T) {
 	testTrace := toProcessedTrace(spans, "none", "", "v1.0.1", "abc", "abc123")
 	c := NewTestConcentrator(now)
 	c.addNow(testTrace, "", nil)
-	stats := c.flushNow(now.UnixNano()+int64(c.bufferLen)*testBucketInterval, false)
+	stats := c.flushNow(now.UnixNano()+int64(c.spanConcentrator.bufferLen)*testBucketInterval, false)
 	assert.Len(stats.Stats[0].Stats[0].Stats, 2)
 	for _, st := range stats.Stats {
 		assert.Equal("v1.0.1", st.Version)

commit 56fdca0e1cbf711290c86f6efb45321a1f3685ed

[apm] fix broken compute_stats_by_span_kind
Fixes a bug introduced in https://github.com/DataDog/datadog-agent/pull/28107 where the `apm_config.compute_stats_by_span_kind` config setting was not being propagated all the way to the new `SpanConcentrator`.

The reason this wasn't caught by the unit tests is that all of the tests were marking the tests spans as `_dd.measured:1` which meant that stats would be calculated regardless of `span.kind`.

pkg/trace/stats/span_concentrator.go
@@ -106,7 +106,7 @@ type SpanConcentrator struct {
 // NewSpanConcentrator builds a new SpanConcentrator object
 func NewSpanConcentrator(cfg *SpanConcentratorConfig, now time.Time) *SpanConcentrator {
 	sc := &SpanConcentrator{
-		computeStatsBySpanKind: false,
+		computeStatsBySpanKind: cfg.ComputeStatsBySpanKind,
 		bsize:                  cfg.BucketInterval,
 		oldestTs:               alignTs(now.UnixNano(), cfg.BucketInterval),
 		bufferLen:              defaultBufferLen,

pkg/trace/stats/statsraw_test.go
@@ -64,6 +64,23 @@ func TestGrainWithPeerTags(t *testing.T) {
 			},
 		}, aggr)
 	})
+	t.Run("computeBySpanKind config", func(t *testing.T) {
+		for _, spanKindEnabled := range []bool{true, false} {
+			t.Run(fmt.Sprintf("%t", spanKindEnabled), func(t *testing.T) {
+				assert := assert.New(t)
+				sci := NewSpanConcentrator(&SpanConcentratorConfig{
+					ComputeStatsBySpanKind: spanKindEnabled,
+					BucketInterval:         (time.Duration(10) * time.Second).Nanoseconds(),
+				}, time.Now().Add(-time.Minute))
+				s, _ := sci.NewStatSpan("thing", "yo", "other", "", 0, 0, 0, 0, map[string]string{"span.kind": "client", "server.address": "foo"}, nil, []string{"_dd.base_service", "server.address"})
+				if spanKindEnabled {
+					assert.Equal([]string{"server.address:foo"}, s.matchingPeerTags)
+				} else {
+					assert.Nil(s)
+				}
+			})
+		}
+	})
 	t.Run("service override", func(t *testing.T) {
 		for _, spanKind := range []string{"client", "internal"} {
 			t.Run(spanKind, func(t *testing.T) {