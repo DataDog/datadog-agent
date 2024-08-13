f91c74f1324132b9e50a8701463e6e9a07be938b
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-08-13 12:40:25 +0200

    apply feedback to Go linter

--- pkg/linters/components/pkgconfigusage/pkgconfigusage.go
+++ pkg/linters/components/pkgconfigusage/pkgconfigusage.go
@@ -7,7 +7,6 @@
 package pkgconfigusage
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/golangci/plugin-module-register/register"
@@ -34,7 +33,7 @@ func (f *pkgconfigUsagePlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
 	return []*analysis.Analyzer{
 		{
 			Name: "pkgconfigusage",
-			Doc:  "ensure pkg/config is not used inside comp folder",
+			Doc:  "ensure github.com/DataDog/datadog-agent/pkg/config is not used inside comp folder",
 			Run:  f.run,
 		},
 	}, nil
@@ -48,25 +47,23 @@ func (f *pkgconfigUsagePlugin) GetLoadMode() string {
 func (f *pkgconfigUsagePlugin) run(pass *analysis.Pass) (interface{}, error) {
 	for _, file := range pass.Files {
 
-		if file.Imports == nil {
+		if !strings.HasPrefix(pass.Pkg.Path(), componentPath) {
 			continue
 		}
 
-		if strings.Contains(pass.Pkg.Path(), componentPath) {
-			for _, imp := range file.Imports {
-				if imp.Path.Value == fmt.Sprintf("\"%s\"", "github.com/DataDog/datadog-agent/pkg/config") {
-					pass.Report(analysis.Diagnostic{
-						Pos:      imp.Pos(),
-						End:      imp.End(),
-						Category: "components",
-						Message:  "pkg/config should not be used inside comp folder",
-						SuggestedFixes: []analysis.SuggestedFix{
-							{
-								Message: "Use the config component instead. Normally you can declare the confg component as part of your component dependencies.",
-							},
+		for _, imp := range file.Imports {
+			if imp.Path.Value == `"github.com/DataDog/datadog-agent/pkg/config"` {
+				pass.Report(analysis.Diagnostic{
+					Pos:      imp.Pos(),
+					End:      imp.End(),
+					Category: "components",
+					Message:  "github.com/DataDog/datadog-agent/pkg/config should not be used inside comp folder",
+					SuggestedFixes: []analysis.SuggestedFix{
+						{
+							Message: "Use the config component instead, by declaring it as part of your component's dependencies.",
 						},
-					})
-				}
+					},
+				})
 			}
 		}
 	}
