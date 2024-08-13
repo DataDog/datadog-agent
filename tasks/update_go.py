a4e551fa3b26a99788898641e5197e278b1b9e8c
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2024-08-13 19:08:52 +0200

    docs: update Go version in public doc

--- tasks/update_go.py
+++ tasks/update_go.py
@@ -30,6 +30,7 @@ GO_VERSION_REFERENCES: list[tuple[str, str, str, bool]] = [
     ("./test/fakeintake/docs/README.md", "[Golang ", "]", False),
     ("./cmd/process-agent/README.md", "`go >= ", "`", False),
     ("./pkg/logs/launchers/windowsevent/README.md", "install go ", "+,", False),
+    ("./docs/public/setup.md", "version `", "` or higher", True),
 ]
 
 PATTERN_MAJOR_MINOR = r'1\.\d+'
