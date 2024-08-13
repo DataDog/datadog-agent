90aa27e21d5cbead2abe470ae3c8224b4ff877c0
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-08-13 11:17:26 +0000

    Add dummy gpu check

---
+++ pkg/collector/corechecks/gpu/gpu.go
@@ -0,0 +1,104 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package gpu defines the agent corecheck for
+// the GPU integration
+package gpu
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v2"
+
+	sysconfig "github.com/DataDog/datadog-agent/cmd/system-probe/config"
+	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/integration"
+	telemetryComp "github.com/DataDog/datadog-agent/comp/core/telemetry"
+	"github.com/DataDog/datadog-agent/pkg/aggregator/sender"
+	"github.com/DataDog/datadog-agent/pkg/collector/check"
+	core "github.com/DataDog/datadog-agent/pkg/collector/corechecks"
+	"github.com/DataDog/datadog-agent/pkg/collector/corechecks/gpu/model"
+	ddconfig "github.com/DataDog/datadog-agent/pkg/config"
+	processnet "github.com/DataDog/datadog-agent/pkg/process/net"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/pkg/util/optional"
+)
+
+// CheckName defines the name of the
+// GPU check
+const CheckName = "gpu"
+
+type CheckConfig struct {
+}
+
+// Check doesn't need additional fields
+type Check struct {
+	core.CheckBase
+	config        *CheckConfig
+	sysProbeUtil  *processnet.RemoteSysProbeUtil
+	telemetryComp telemetryComp.Component
+}
+
+// Factory creates a new check factory
+func Factory() optional.Option[func() check.Check] {
+	return optional.NewOption(newCheck)
+}
+
+func newCheck() check.Check {
+	return &Check{
+		CheckBase: core.NewCheckBase(CheckName),
+		config:    &CheckConfig{},
+	}
+}
+
+// Parse parses the check configuration
+func (c *CheckConfig) Parse(data []byte) error {
+	return yaml.Unmarshal(data, c)
+}
+
+// Configure parses the check configuration and init the check
+func (m *Check) Configure(senderManager sender.SenderManager, _ uint64, config, initConfig integration.Data, source string) error {
+	if err := m.CommonConfigure(senderManager, initConfig, config, source); err != nil {
+		return err
+	}
+	if err := m.config.Parse(config); err != nil {
+		return fmt.Errorf("ebpf check config: %s", err)
+	}
+	if err := processnet.CheckPath(ddconfig.SystemProbe().GetString("system_probe_config.sysprobe_socket")); err != nil {
+		return fmt.Errorf("sysprobe socket: %s", err)
+	}
+
+	return nil
+}
+
+// Run executes the check
+func (m *Check) Run() error {
+	if m.sysProbeUtil == nil {
+		var err error
+		m.sysProbeUtil, err = processnet.GetRemoteSystemProbeUtil(
+			ddconfig.SystemProbe().GetString("system_probe_config.sysprobe_socket"),
+		)
+		if err != nil {
+			return fmt.Errorf("sysprobe connection: %s", err)
+		}
+	}
+
+	data, err := m.sysProbeUtil.GetCheck(sysconfig.GPUMonitoringModule)
+	if err != nil {
+		return fmt.Errorf("get ebpf check: %s", err)
+	}
+
+	sender, err := m.GetSender()
+	if err != nil {
+		return fmt.Errorf("get metric sender: %s", err)
+	}
+
+	stats, ok := data.(model.GPUStats)
+	if !ok {
+		return log.Errorf("ebpf check raw data has incorrect type: %T", stats)
+	}
+
+	sender.Commit()
+	return nil
+}
