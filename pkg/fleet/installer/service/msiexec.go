0da57092b146890f3462466749a1b3594cf38b58
Julien Lebot <julien.lebot@datadoghq.com>
2024-08-14 19:04:49 +0200

    Fix remove product

--- pkg/fleet/installer/service/msiexec.go
+++ pkg/fleet/installer/service/msiexec.go
@@ -10,6 +10,7 @@ package service
 import (
 	"fmt"
 	"github.com/DataDog/datadog-agent/pkg/fleet/internal/paths"
+	"golang.org/x/sys/windows/registry"
 	"os/exec"
 	"path/filepath"
 )
@@ -30,17 +31,67 @@ func msiexec(target, product, operation string, args []string) (err error) {
 	return cmd.Run()
 }
 
+// Product represents a software from the Windows Registry
+type Product struct {
+	// Code is the software product code
+	Code string
+	// UninstallString is the string that can be executed to uninstall the software. May be empty.
+	UninstallString string
+}
+
+func findProductCode(name string) (error, *Product) {
+	rootPath := "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+	reg, err := registry.OpenKey(registry.LOCAL_MACHINE, rootPath, registry.ENUMERATE_SUB_KEYS)
+	if err != nil {
+		return err, nil
+	}
+	defer reg.Close()
+	keys, err := reg.ReadSubKeyNames(0)
+	if err != nil {
+		return err, nil
+	}
+	for _, key := range keys {
+		err, product := processKey(rootPath, key, name)
+		if err == nil && product != nil {
+			return nil, product
+		}
+	}
+	return nil, nil
+}
+
+func processKey(rootPath, key, name string) (error, *Product) {
+	subkey, err := registry.OpenKey(registry.LOCAL_MACHINE, rootPath+"\\"+key, registry.QUERY_VALUE)
+	if err != nil {
+		return err, nil
+	}
+	defer subkey.Close()
+
+	displayName, _, err := subkey.GetStringValue("DisplayName")
+	if err == nil && displayName == name {
+		product := &Product{}
+		product.UninstallString, _, _ = subkey.GetStringValue("UninstallString")
+		product.Code = key
+		return nil, product
+	}
+
+	return nil, nil
+}
+
 // removeProduct uses the registry to try and find a product and use msiexec to remove it.
 // It is different from msiexec in that it uses the registry and not the stable/experiment path on disk to
 // uninstall the product.
 // This is needed because in certain circumstances the installer database stored in the stable/experiment paths does not
 // reflect the installed version, and using those installers can lead to undefined behavior (either failure to uninstall,
 // or weird bugs from uninstalling a product with an installer from a different version).
-func removeProduct(product string) (err error) {
-	return exec.Command("powershell", fmt.Sprintf(`{
-	$installerList = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object {$_.DisplayName -like '%s'}
-	if (($installerList | measure).Count -gt 0) {
-		msiexec /x $installerList.PSChildName /qn MSIFASTINSTALL=7
+func removeProduct(productName string) error {
+	err, product := findProductCode(productName)
+	if err != nil {
+		return fmt.Errorf("error trying to find product %s: %w", productName, err)
 	}
-}`, product)).Run()
+	if product != nil {
+		cmd := exec.Command("msiexec", "/x", product.Code, "/qn", "MSIFASTINSTALL=7")
+		return cmd.Run()
+	} else {
+		return fmt.Errorf("product %s not found", productName)
+	}
 }
