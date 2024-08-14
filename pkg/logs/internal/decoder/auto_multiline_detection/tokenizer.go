5253bed46f30825906ffd129995047f5c4ac586e
Brian Floersch <brian.floersch@datadoghq.com>
2024-08-14 09:27:56 -0400

    PR feedback
    rename `set` -> `sequence`

--- pkg/logs/internal/decoder/auto_multiline_detection/tokenizer.go
+++ pkg/logs/internal/decoder/auto_multiline_detection/tokenizer.go
@@ -377,17 +377,17 @@ func tokensToString(tokens []Token) string {
 	return str
 }
 
-// isMatch compares two sets of tokens and returns true if they match.
+// isMatch compares two sequences of tokens and returns true if they match.
 // if the token strings are different lengths, the shortest string is used for comparison.
-func isMatch(setA []Token, setB []Token, thresh float64) bool {
-	count := len(setA)
-	if len(setB) < count {
-		count = len(setB)
+func isMatch(seqA []Token, seqB []Token, thresh float64) bool {
+	count := len(seqA)
+	if len(seqB) < count {
+		count = len(seqB)
 	}
 
 	match := 0
 	for i := 0; i < count; i++ {
-		if setA[i] == setB[i] {
+		if seqA[i] == seqB[i] {
 			match++
 		}
 	}
