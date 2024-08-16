616ea32ccc42a1d0393a3576a6fab545659006ec
Brian Floersch <brian.floersch@datadoghq.com>
2024-08-16 13:16:03 -0400

    spelling

--- pkg/logs/schedulers/ad/scheduler.go
+++ pkg/logs/schedulers/ad/scheduler.go
@@ -200,8 +200,8 @@ func CreateSources(config integration.Config) ([]*sourcesPkg.LogSource, error) {
 			// a config defined in a container label or a pod annotation does not always contain a type,
 			// override it here to ensure that the config won't be dropped at validation.
 			skipOverrideType := (cfg.Type == logsConfig.FileType || cfg.Type == logsConfig.TCPType || cfg.Type == logsConfig.UDPType || cfg.Type == logsConfig.IntegrationType)
-			sikpOverrideProvider := (config.Provider == names.Kubernetes || config.Provider == names.Container || config.Provider == names.KubeContainer || config.Provider == logsConfig.FileType)
-			shouldOverrideType := !(skipOverrideType && sikpOverrideProvider)
+			skipOverrideProvider := (config.Provider == names.Kubernetes || config.Provider == names.Container || config.Provider == names.KubeContainer || config.Provider == logsConfig.FileType)
+			shouldOverrideType := !(skipOverrideType && skipOverrideProvider)
 
 			// cfg.Type is not overwritten as tailing a file, socket, or integration from a Docker or Kubernetes AD configuration
 			// is explicitly supported (other combinations may be supported later)
