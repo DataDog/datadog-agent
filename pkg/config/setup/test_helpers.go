a0ab2778b5ee49cdb4893a9cbe422cc6d4ba0d89
Maxime mouial <maxime.mouial@gmail.com>
2024-08-19 12:44:04 +0200

    Remove public test helper Conf and ConfFromYaml
    These 2 helpers were not using the official configuration mock which
    duplicate the mock logic and create yet another way to use configuration
    in tests. Removing them align the code base on a single way of getting
    the config.

--- pkg/config/setup/test_helpers.go
+++ pkg/config/setup/test_helpers.go
@@ -8,27 +8,14 @@
 package setup
 
 import (
-	"bytes"
-	"log"
 	"strings"
 
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
 )
 
-// Conf generates and returns a new configuration
-func Conf() pkgconfigmodel.Config {
+// newTestConf generates and returns a new configuration
+func newTestConf() pkgconfigmodel.Config {
 	conf := pkgconfigmodel.NewConfig("datadog", "DD", strings.NewReplacer(".", "_"))
 	InitConfig(conf)
-	return conf
-}
-
-// ConfFromYAML generates a configuration from the given yaml config
-func ConfFromYAML(yamlConfig string) pkgconfigmodel.Config {
-	conf := Conf()
-	conf.SetConfigType("yaml")
-	e := conf.ReadConfig(bytes.NewBuffer([]byte(yamlConfig)))
-	if e != nil {
-		log.Println(e)
-	}
 	return conf
 }
