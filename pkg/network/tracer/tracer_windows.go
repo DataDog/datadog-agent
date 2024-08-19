6d31d0764ef15d933ecae7bd2e367c2c0eb45dc3
amit.slavin <amit.slavin@datadoghq.com>
2024-08-19 18:13:36 +0300

    removed MaxPostgresTelemetryBuffer from windows

--- pkg/network/tracer/tracer_windows.go
+++ pkg/network/tracer/tracer_windows.go
@@ -84,7 +84,6 @@ func NewTracer(config *config.Config, telemetry telemetry.Component) (*Tracer, e
 		config.MaxHTTPStatsBuffered,
 		config.MaxKafkaStatsBuffered,
 		config.MaxPostgresStatsBuffered,
-		config.MaxPostgresTelemetryBuffer,
 		config.MaxRedisStatsBuffered,
 		config.EnableNPMConnectionRollup,
 		config.EnableProcessEventMonitoring,
