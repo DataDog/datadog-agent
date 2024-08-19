cc7a427829105ca996ee2d69f7f956f0ee72dd6c
Hasan Mahmood <6599778+hmahmood@users.noreply.github.com>
2024-08-19 10:12:16 -0500

    Add config to enable/disable ebpf conntracker (#28447)

--- pkg/config/setup/system_probe.go
+++ pkg/config/setup/system_probe.go
@@ -206,6 +206,7 @@ func InitSystemProbeConfig(cfg pkgconfigmodel.Config) {
 	cfg.BindEnvAndSetDefault(join(netNS, "ignore_conntrack_init_failure"), false, "DD_SYSTEM_PROBE_NETWORK_IGNORE_CONNTRACK_INIT_FAILURE")
 	cfg.BindEnvAndSetDefault(join(netNS, "conntrack_init_timeout"), 10*time.Second)
 	cfg.BindEnvAndSetDefault(join(netNS, "allow_netlink_conntracker_fallback"), true)
+	cfg.BindEnvAndSetDefault(join(netNS, "enable_ebpf_conntracker"), true)
 
 	cfg.BindEnvAndSetDefault(join(spNS, "source_excludes"), map[string][]string{})
 	cfg.BindEnvAndSetDefault(join(spNS, "dest_excludes"), map[string][]string{})
