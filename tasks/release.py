5d0e52d9eaa2af2ece4dbaa949d5f72ed787e692
Florent Clarret <florent.clarret@datadoghq.com>
2024-08-20 13:02:56 +0200

    Update tasks/release.py
    Co-authored-by: pducolin <45568537+pducolin@users.noreply.github.com>

--- tasks/release.py
+++ tasks/release.py
@@ -885,7 +885,7 @@ def get_active_release_branch(_):
 @task
 def get_active_release_branches(_):
     """
-    Determine what is the current active release branches for the Agent.
+    Determine what are the current active release branches for the Agent.
     """
     gh = GithubAPI()
     print(json.dumps([branch.name for branch in gh.latest_unreleased_release_branches()]))
