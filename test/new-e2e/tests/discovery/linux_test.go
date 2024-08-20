71f242065e7bad2a33e9a0305f27a892c0650284
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2024-08-20 13:38:43 +0200

    discovery: e2e: Remove dev mode options

--- test/new-e2e/tests/discovery/linux_test.go
+++ test/new-e2e/tests/discovery/linux_test.go
@@ -42,8 +42,6 @@ func TestLinuxTestSuite(t *testing.T) {
 	}
 	options := []e2e.SuiteOption{
 		e2e.WithProvisioner(awshost.Provisioner(awshost.WithAgentOptions(agentParams...))),
-		e2e.WithDevMode(),
-		e2e.WithSkipDeleteOnFailure(),
 	}
 	e2e.Run(t, &linuxTestSuite{}, options...)
 }
