9d2755e7d1becfe78ca0df24528cfeade560cb5c
Dylan Yang <dylan.yang@datadoghq.com>
2024-08-21 12:33:18 -0400

    test

--- test/integration/serverless/build_go_functions.sh
+++ test/integration/serverless/build_go_functions.sh
@@ -4,5 +4,5 @@ echo "Building Go functions for $ARCHITECTURE architecture"
 go_test_dirs=("metric" "log" "timeout" "trace" "appsec")
 cd src
 for go_dir in "${go_test_dirs[@]}"; do
-    env GOOS=linux CGO_ENABLED=0 GOARCH=$ARCHITECTURE go build -ldflags="-s -w" -o bin/"$go_dir" go-tests/"$go_dir"/main.go
+    env GOOS=linux GOARCH=$ARCHITECTURE go build -ldflags="-s -w" -o bin/"$go_dir" go-tests/"$go_dir"/main.go
 done
