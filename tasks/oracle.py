921925d6c105a99453ad188f3ba3a5c3e999def6
Seth Samuel <seth.samuel@datadoghq.com>
2024-08-22 18:33:47 +0000

    Docker?

--- tasks/oracle.py
+++ tasks/oracle.py
@@ -35,12 +35,9 @@ def test(ctx, verbose=False) -> None:
             raise Exit(message='docker failed to start', code=1)
 
     try:
-        ctx.run("docker ps")
         os.environ["ORACLE_TEST_PORT"] = "1521"
-        os.environ["ORACLE_TEST_SERVER"] = ctx.run(
-            "docker inspect  -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' compose-oracle-1", hide=True
-        ).stdout.strip()
-        ctx.run("echo port=$ORACLE_TEST_PORT server=$ORACLE_TEST_SERVER")
+        if os.environ.get("CI"):
+            os.environ["ORACLE_TEST_SERVER"] = "docker"
         with ctx.cd("pkg/collector/corechecks/oracle"):
             print("Running tests...")
             go_flags = " -v" if verbose else ""
