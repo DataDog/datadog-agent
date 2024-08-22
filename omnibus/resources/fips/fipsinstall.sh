11b5d12c50b7869448fb56b5f71ad21478918506
Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>
2024-08-22 12:40:00 +0200

    feat: run fipsinstall.sh after installing package and before running container cmd
    Signed-off-by: Nicolas Guerguadj <nicolas.guerguadj@datadoghq.com>

--- omnibus/resources/fips/fipsinstall.sh
+++ omnibus/resources/fips/fipsinstall.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# The OpenSSL security policy states:
+# "The Module shall have the self-tests run, and the Module config file output generated on each
+# platform where it is intended to be used. The Module config file output data shall not be copied from
+# one machine to another."
+# This script aims to run self-tests and generate `fipsmodule.cnf.`
+# Because the provided `openssl.cnf` references to `fipsmodule.cnf` which is not yet created, we first create it
+# as `openssl.cnf.tmp` and then move it to its final name `openssl.cnf` when `fipsmodule.cnf` has been created
+
+# TODO: template INSTALL_DIR fipsinstall.sh.erb
 set -euo pipefail
 
 INSTALL_DIR="/opt/datadog-agent/embedded"
