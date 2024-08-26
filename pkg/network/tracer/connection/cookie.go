df6f57389305446335ee6e7df907f34ec3d48ec8
Hasan Mahmood <hasan.mahmood@datadoghq.com>
2024-08-26 09:52:47 -0500

    Remove npm tag

--- pkg/network/tracer/connection/cookie.go
+++ pkg/network/tracer/connection/cookie.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
-//go:build linux && npm
+//go:build linux
 
 package connection
 
