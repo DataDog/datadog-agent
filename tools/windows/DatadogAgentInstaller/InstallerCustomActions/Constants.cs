1a6d318dec34d9215ce6698f521d506c467c5598
Branden Clark <branden.clark@datadoghq.com>
2024-08-26 18:49:55 -0400

    remove unused constants

--- tools/windows/DatadogAgentInstaller/InstallerCustomActions/Constants.cs
+++ tools/windows/DatadogAgentInstaller/InstallerCustomActions/Constants.cs
@@ -3,14 +3,6 @@ namespace Datadog.InstallerCustomActions
 {
     public class Constants
     {
-        public const string AgentServiceName = "datadogagent";
-        public const string TraceAgentServiceName = "datadog-trace-agent";
-        public const string ProcessAgentServiceName = "datadog-process-agent";
-        public const string SystemProbeServiceName = "datadog-system-probe";
-        public const string SecurityAgentServiceName = "datadog-security-agent";
-        public const string NpmServiceName = "ddnpm";
-        public const string ProcmonServiceName = "ddprocmon";
-
         // Key under HKLM that contains our options
         public const string DatadogInstallerRegistryKey = @"Software\Datadog\Datadog Installer";
     }
