97a81aaa491ef7c32fd1782c6375548491271293
Branden Clark <branden.clark@datadoghq.com>
2024-08-26 15:25:04 -0400

    add installer rollback tests

---
+++ test/new-e2e/tests/installer/windows/suites/installer-package/base_suite.go
@@ -0,0 +1,69 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package installertests implements E2E tests for the Datadog installer package on Windows
+package installertests
+
+import (
+	"embed"
+	installerwindows "github.com/DataDog/datadog-agent/test/new-e2e/tests/installer/windows"
+	"github.com/DataDog/datadog-agent/test/new-e2e/tests/windows/common"
+	"github.com/DataDog/datadog-agent/test/new-e2e/tests/windows/common/agent"
+)
+
+//go:embed fixtures/sample_config
+var fixturesFS embed.FS
+
+// baseInstallerSuite is the base test suite for tests of the installer MSI
+type baseInstallerSuite struct {
+	installerwindows.BaseInstallerSuite
+}
+
+func (s *baseInstallerSuite) freshInstall() {
+	// Arrange
+
+	// Act
+	s.Require().NoError(s.Installer().Install())
+
+	// Assert
+	s.assertInstalled()
+	s.Require().Host(s.Env().RemoteHost).
+		HasAService(installerwindows.ServiceName).
+		// the service cannot start because of the missing API key
+		WithStatus("Stopped")
+}
+
+func (s *baseInstallerSuite) startServiceWithConfigFile() {
+	// Arrange
+	s.Env().RemoteHost.CopyFileFromFS(fixturesFS, "fixtures/sample_config", installerwindows.ConfigPath)
+
+	// Act
+	s.Require().NoError(common.StartService(s.Env().RemoteHost, installerwindows.ServiceName))
+
+	// Assert
+	s.Require().Host(s.Env().RemoteHost).
+		HasAService(installerwindows.ServiceName).
+		WithStatus("Running")
+}
+
+func (s *baseInstallerSuite) assertInstalled() {
+	s.Require().Host(s.Env().RemoteHost).
+		HasBinary(installerwindows.BinaryPath).
+		WithSignature(agent.GetCodeSignatureThumbprints()).
+		WithVersionMatchPredicate(func(version string) {
+			s.Require().NotEmpty(version)
+		}).
+		HasAService(installerwindows.ServiceName).
+		WithIdentity(common.GetIdentityForSID(common.LocalSystemSID)).
+		HasRegistryKey(installerwindows.RegistryKeyPath).
+		WithValueEqual("installedUser", agent.DefaultAgentUserName)
+}
+
+func (s *baseInstallerSuite) assertUninstalled() {
+	s.Require().Host(s.Env().RemoteHost).
+		NoFileExists(installerwindows.BinaryPath).
+		HasNoService(installerwindows.ServiceName).
+		HasNoRegistryKey(installerwindows.RegistryKeyPath)
+}
---
+++ test/new-e2e/tests/installer/windows/suites/installer-package/rollback_test.go
@@ -0,0 +1,55 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package installertests implements E2E tests for the Datadog installer package on Windows
+package installertests
+
+import (
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
+	awsHostWindows "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host/windows"
+	installerwindows "github.com/DataDog/datadog-agent/test/new-e2e/tests/installer/windows"
+	"testing"
+)
+
+type testInstallerRollbackSuite struct {
+	baseInstallerSuite
+}
+
+// TestInstaller tests the installation of the Datadog installer on a system.
+func TestInstallerRollback(t *testing.T) {
+	e2e.Run(t, &testInstallerRollbackSuite{}, e2e.WithProvisioner(awsHostWindows.ProvisionerNoAgentNoFakeIntake()))
+}
+
+// TestInstalls tests installing and uninstalling the latest version of the Datadog installer from the pipeline.
+func (s *testInstallerRollbackSuite) TestInstallerRollback() {
+	s.Run("Fresh install rollback", s.installRollback)
+	s.Run("Fresh install", s.freshInstall)
+	s.Run("uninstall rollback", s.uninstallRollback)
+	s.Run("Start service with a configuration file", s.startServiceWithConfigFile)
+}
+
+// installRollback
+func (s *testInstallerRollbackSuite) installRollback() {
+	// Arrange
+
+	// Act
+	msiErr := s.Installer().Install(installerwindows.WithMSIArg("WIXFAILWHENDEFERRED=1"))
+	s.Require().Error(msiErr)
+
+	// Assert
+	s.assertUninstalled()
+}
+
+// uninstallRollback
+func (s *testInstallerRollbackSuite) uninstallRollback() {
+	// Arrange
+
+	// Act
+	msiErr := s.Installer().Uninstall(installerwindows.WithMSIArg("WIXFAILWHENDEFERRED=1"))
+	s.Require().Error(msiErr)
+
+	// Assert
+	s.assertInstalled()
+}
