e568382a5df07dd0c0cb3a7ac6d9c9560015a7a2
Dinesh Gurumurthy <dinesh.gurumurthy@datadoghq.com>
2024-08-27 18:21:03 +0530

    remove un-needed changes

--- comp/otelcol/otlp/components/exporter/datadogexporter/config.go
+++ comp/otelcol/otlp/components/exporter/datadogexporter/config.go
@@ -82,22 +82,6 @@ type TracesConfig struct {
 	// The default value is `true`.
 	ComputeTopLevelBySpanKind bool `mapstructure:"compute_top_level_by_span_kind"`
 
-	// If set to true, enables aggregation of peer related tags (e.g., `peer.service`, `db.instance`, etc.) in the datadog exporter.
-	// If disabled, aggregated trace stats will not include these tags as dimensions on trace metrics.
-	// For the best experience with peer tags, Datadog also recommends enabling `compute_stats_by_span_kind`.
-	// If you are using an OTel tracer, it's best to have both enabled because client/producer spans with relevant peer tags
-	// may not be marked by the datadog exporter as top-level spans.
-	// If enabling both causes the datadog exporter to consume too many resources, try disabling `compute_stats_by_span_kind` first.
-	// A high cardinality of peer tags or APM resources can also contribute to higher CPU and memory consumption.
-	// You can check for the cardinality of these fields by making trace search queries in the Datadog UI.
-	// The default list of peer tags can be found in https://github.com/DataDog/datadog-agent/blob/main/pkg/trace/stats/concentrator.go.
-	PeerTagsAggregation bool `mapstructure:"peer_tags_aggregation"`
-
-	// [BETA] Optional list of supplementary peer tags that go beyond the defaults. The Datadog backend validates all tags
-	// and will drop ones that are unapproved. The default set of peer tags can be found at
-	// https://github.com/DataDog/datadog-agent/blob/505170c4ac8c3cbff1a61cf5f84b28d835c91058/pkg/trace/stats/concentrator.go#L55.
-	PeerTags []string `mapstructure:"peer_tags"`
-
 	// TraceBuffer specifies the number of Datadog Agent TracerPayloads to buffer before dropping.
 	// The default value is 0, meaning the Datadog Agent TracerPayloads are unbuffered.
 	TraceBuffer int `mapstructure:"trace_buffer"`
