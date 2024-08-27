535d2267bc48890a001ef7a68c1eaf28106a76bd
Guy Arbitman <guy20495@gmail.com>
2024-08-27 12:09:15 +0300

    service discovery: add build tag

--- pkg/collector/corechecks/servicediscovery/language/language.go
+++ pkg/collector/corechecks/servicediscovery/language/language.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build linux
+
 // Package language provides functionality to detect the programming language for a given process.
 package language
 
