35fb46bc7a5858d6e3f205d8c1219c48e0f45ff1
mackjmr <mackenzie.jomard@datadoghq.com>
2024-08-28 17:12:50 +0200

    fix lint

--- comp/otelcol/ddflareextension/impl/config.go
+++ comp/otelcol/ddflareextension/impl/config.go
@@ -58,10 +58,11 @@ func (c *Config) Unmarshal(conf *confmap.Conf) error {
 	return nil
 }
 
-func zPagesExtractEndpoint(c *confmap.Conf) (string, bool, error) {
-	endpoint, err := regularStringEndpointExtractor(c)
-	return endpoint, true, err
-}
+// todo: uncomment once zpages data is re-added to flare
+// func zPagesExtractEndpoint(c *confmap.Conf) (string, bool, error) {
+// 	endpoint, err := regularStringEndpointExtractor(c)
+// 	return endpoint, true, err
+// }
 
 func pprofExtractEndpoint(c *confmap.Conf) (string, bool, error) {
 	endpoint, err := regularStringEndpointExtractor(c)
