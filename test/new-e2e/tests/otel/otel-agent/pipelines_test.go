a05f98d6627ca60fa0dbdfaca7dbf141421b3cf8
Stanley Liu <stanley.liu@datadoghq.com>
2024-08-28 14:49:42 -0400

    Fix import

--- test/new-e2e/tests/otel/otel-agent/pipelines_test.go
+++ test/new-e2e/tests/otel/otel-agent/pipelines_test.go
@@ -28,6 +28,7 @@ import (
 	fakeintake "github.com/DataDog/datadog-agent/test/fakeintake/client"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
+	awskubernetes "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/kubernetes"
 )
 
 const (
