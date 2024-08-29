5e9785a5c5c51a1bb0fe44d828d2d28d311b2805
Daniel Tafoya <daniel.tafoya@datadoghq.com>
2024-08-29 13:52:00 -0400

    Mark flaky test

--- test/new-e2e/tests/process/k8s_test.go
+++ test/new-e2e/tests/process/k8s_test.go
@@ -195,6 +195,10 @@ func (s *K8sSuite) TestProcessCheckInCoreAgent() {
 
 func (s *K8sSuite) TestProcessCheckInCoreAgentWithNPM() {
 	t := s.T()
+	// PROCS-4327: The process-agent container either fails to start or
+	// does not seem to run the connections check as expected.
+	flake.Mark(t)
+
 	helmValues, err := createHelmValues(helmConfig{
 		ProcessCollection:            true,
 		RunInCoreAgent:               true,
@@ -213,6 +217,11 @@ func (s *K8sSuite) TestProcessCheckInCoreAgentWithNPM() {
 		status := k8sAgentStatus(t, s.Env().KubernetesCluster)
 		assert.ElementsMatch(t, []string{"process", "rtprocess"}, status.ProcessComponentStatus.Expvars.Map.EnabledChecks)
 		assert.ElementsMatch(t, []string{"connections"}, status.ProcessAgentStatus.Expvars.Map.EnabledChecks)
+
+		if t.Failed() {
+			t.LogF("status: %+v\n", status)
+		}
+
 	}, 2*time.Minute, 5*time.Second)
 
 	var payloads []*aggregator.ProcessPayload
