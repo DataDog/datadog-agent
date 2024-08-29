edb7a091ee65a98d13b6b9d284dfdb283ae10251
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2024-08-29 09:26:59 +0200

    binspect: Fix EOF handling
    If a symbol to be found has its name as the last string in the string
    table, and the string table is at the end of the file, and the symbol's
    length is less than the maximum length of the symbols we are searching
    for, the code fails to find this symbol, since ReadAt() returns EOF and
    a size lesser than the size of the preallocated array.
    
    To fix this, ignore EOF and instead use the return value of ReadAt() to
    known how much to process.  This works if ReadAt() returns lesser than
    the prealloc buffer's size, and also handles the case when ReadAt()
    returns 0 (with err = EOF).

--- pkg/network/go/bininspect/symbols.go
+++ pkg/network/go/bininspect/symbols.go
@@ -32,8 +32,8 @@ const (
 // redundant allocations. We get it as a parameter and not putting it as a global, to be thread safe among concurrent
 // and parallel calls.
 func getSymbolNameByEntry(sectionReader io.ReaderAt, startPos, minLength int, preAllocatedBuf []byte) int {
-	_, err := sectionReader.ReadAt(preAllocatedBuf, int64(startPos))
-	if err != nil {
+	readBytes, err := sectionReader.ReadAt(preAllocatedBuf, int64(startPos))
+	if err != nil && err != io.EOF {
 		return -1
 	}
 
@@ -42,7 +42,7 @@ func getSymbolNameByEntry(sectionReader io.ReaderAt, startPos, minLength int, pr
 	// If we didn't find null there, then it is not a symbol we're looking for.
 	foundNull := false
 	nullIndex := minLength
-	for ; nullIndex < len(preAllocatedBuf); nullIndex++ {
+	for ; nullIndex < readBytes; nullIndex++ {
 		if preAllocatedBuf[nullIndex] == 0 {
 			foundNull = true
 			break
