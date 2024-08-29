33043f6b54bc07ea753cad9dab24fd74f9441be8
grantseltzer <grantseltzer@gmail.com>
2024-08-29 08:57:32 -0500

    normalize name of file
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/codegen/compile.go
+++ pkg/dynamicinstrumentation/codegen/compile.go
@@ -2,5 +2,5 @@
 
 package codegen
 
-//go:generate $GOPATH/bin/include_headers pkg/dynamicinstrumentation/codegen/c/dynamicinstrumentation.c pkg/ebpf/bytecode/build/runtime/dynamicinstrumentation_template.c pkg/ebpf/c
+//go:generate $GOPATH/bin/include_headers pkg/dynamicinstrumentation/codegen/c/dynamicinstrumentation.c pkg/ebpf/bytecode/build/runtime/dynamicinstrumentation.c pkg/ebpf/c
 //go:generate $GOPATH/bin/integrity pkg/dynamicinstrumentation/codegen/c/dynamicinstrumentation.c pkg/ebpf/bytecode/runtime/dynamicinstrumentation.go runtime
