0bd29f793535c974f5adb716617459b3d732f0ad
Seth Samuel <seth.samuel@datadoghq.com>
2024-08-29 12:33:02 -0400

    Postgres telemetry

---
+++ pkg/postgres/telemetry.go
@@ -0,0 +1,17 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build postgres
+
+package postgres
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/telemetry"
+)
+
+var (
+	// TlmPostgresActivityLatency is the time for the activity gathering to complete
+	TlmPostgresActivityLatency = telemetry.NewHistogram("postgres", "activity_latency", nil, "Histogram of activity query latency in ms", []float64{10, 25, 50, 75, 100, 250, 500, 1000, 10000})
+)
