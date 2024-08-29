70870f0a34d0225ad65287bbb2e2c5c9084076e8
jackgopack4 <john.peterson@datadoghq.com>
2024-08-29 15:32:07 -0400

    remove comment failing linter

--- comp/otelcol/otlp/components/processor/infraattributesprocessor/taggerclient.go
+++ comp/otelcol/otlp/components/processor/infraattributesprocessor/taggerclient.go
@@ -3,7 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-// client for using tagger interface
 package infraattributesprocessor
 
 import "github.com/DataDog/datadog-agent/comp/core/tagger/types"
--- comp/otelcol/otlp/components/processor/infraattributesprocessor/taggerclient_test.go
+++ comp/otelcol/otlp/components/processor/infraattributesprocessor/taggerclient_test.go
@@ -3,7 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-// helper methods for testing using tagger interface
 package infraattributesprocessor
 
 import (
