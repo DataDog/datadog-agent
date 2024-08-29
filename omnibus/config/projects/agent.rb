8a1fd084dcc512dcb6b65e687b2bd4e024579d48
Alex Lopez <alex.lopez@datadoghq.com>
2024-08-29 11:50:21 +0200

    Test against cmake update

--- omnibus/config/projects/agent.rb
+++ omnibus/config/projects/agent.rb
@@ -231,14 +231,15 @@ if do_build
     dependency 'datadog-agent-mac-app'
   end
 
-  if with_python_runtime? "2"
-    dependency 'pylint2'
-    dependency 'datadog-agent-integrations-py2'
-  end
+  # Integrations is for now broken and we want to test the cmake change
+  # if with_python_runtime? "2"
+  #   dependency 'pylint2'
+  #   dependency 'datadog-agent-integrations-py2'
+  # end
 
-  if with_python_runtime? "3"
-    dependency 'datadog-agent-integrations-py3'
-  end
+  # if with_python_runtime? "3"
+  #   dependency 'datadog-agent-integrations-py3'
+  # end
 
   if linux_target?
     dependency 'datadog-security-agent-policies'
