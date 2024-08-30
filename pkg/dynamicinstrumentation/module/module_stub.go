90cbe8d759c82b3ab4156bd42893e5423943aee2
grantseltzer <grantseltzer@gmail.com>
2024-08-30 12:11:57 -0500

    Add back interface required function
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/module/module_stub.go
+++ pkg/dynamicinstrumentation/module/module_stub.go
@@ -7,6 +7,10 @@
 
 package module
 
+import (
+	"github.com/DataDog/datadog-agent/cmd/system-probe/api/module"
+)
+
 //nolint:revive // TODO(DEBUG) Fix revive linter
 type Module struct {
 }
@@ -24,7 +28,7 @@ func (m *Module) GetStats() map[string]interface{} {
 	return nil
 }
 
-// //nolint:revive // TODO(DEBUG) Fix revive linter
-// func (m *Module) Register(_ *module.Router) error {
-// 	return nil
-// }
+//nolint:revive // TODO(DEBUG) Fix revive linter
+func (m *Module) Register(_ *module.Router) error {
+	return nil
+}
