8db7fc7d04fd724152ab0d7baf27e1b514bdae1a
jackgopack4 <john.peterson@datadoghq.com>
2024-08-30 12:55:45 -0400

    add doc.go files

---
+++ pkg/util/ecs/metadata/v3or4/doc.go
@@ -0,0 +1,9 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022-present Datadog, Inc.
+
+//go:build docker
+
+// Package v3or4 provides an ECS client for the version v3 and v4 of the API.
+package v3or4
