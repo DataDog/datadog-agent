073f80bab12a056ef5f975100e831c77fb718c01
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-08-30 10:29:39 +0200

    discovery: fallback on binary name if service name detection fails

--- pkg/collector/corechecks/servicediscovery/usm/service_test.go
+++ pkg/collector/corechecks/servicediscovery/usm/service_test.go
@@ -191,7 +191,7 @@ func TestExtractServiceMetadata(t *testing.T) {
 				"--",
 				"/somewhere/index.js",
 			},
-			expectedServiceTag: "",
+			expectedServiceTag: "node",
 		},
 		{
 			name: "node js with a broken package.json",
@@ -199,7 +199,7 @@ func TestExtractServiceMetadata(t *testing.T) {
 				"/usr/bin/node",
 				"./testdata/inner/index.js",
 			},
-			expectedServiceTag: "",
+			expectedServiceTag: "node",
 		},
 		{
 			name: "node js with a valid package.json",
@@ -385,6 +385,7 @@ func TestExtractServiceMetadata(t *testing.T) {
 			cmdline: []string{
 				"/usr/bin/dotnet", "run", "--project", "./projects/proj1/proj1.csproj",
 			},
+			expectedServiceTag: "dotnet",
 		},
 		{
 			name: "PHP Laravel",
