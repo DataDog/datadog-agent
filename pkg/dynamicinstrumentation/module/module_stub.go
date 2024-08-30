654e7757808c30764e9b3ef7697f8c16ac6a112b
grantseltzer <grantseltzer@gmail.com>
2024-08-30 12:09:16 -0500

    Attempt to fix build tags
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/module/module_stub.go
+++ pkg/dynamicinstrumentation/module/module_stub.go
@@ -7,10 +7,6 @@
 
 package module
 
-import (
-	"github.com/DataDog/datadog-agent/cmd/system-probe/api/module"
-)
-
 //nolint:revive // TODO(DEBUG) Fix revive linter
 type Module struct {
 }
@@ -28,7 +24,7 @@ func (m *Module) GetStats() map[string]interface{} {
 	return nil
 }
 
-//nolint:revive // TODO(DEBUG) Fix revive linter
-func (m *Module) Register(_ *module.Router) error {
-	return nil
-}
+// //nolint:revive // TODO(DEBUG) Fix revive linter
+// func (m *Module) Register(_ *module.Router) error {
+// 	return nil
+// }
