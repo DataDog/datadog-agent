31b859d4b027a669559edf9a87dc650aee4e6561
jackgopack4 <john.peterson@datadoghq.com>
2024-08-30 16:56:23 -0400

    remove deprecated options from docker api

--- pkg/compliance/resolver.go
+++ pkg/compliance/resolver.go
@@ -28,7 +28,8 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/version"
 	"github.com/DataDog/datadog-go/v5/statsd"
 
-	dockertypes "github.com/docker/docker/api/types"
+	dockerimagetypes "github.com/docker/docker/api/types/image"
+	dockernetworktypes "github.com/docker/docker/api/types/network"
 	docker "github.com/docker/docker/client"
 
 	"github.com/shirou/gopsutil/v3/process"
@@ -564,7 +565,7 @@ func (r *defaultResolver) resolveDocker(ctx context.Context, spec InputSpecDocke
 	var resolved []interface{}
 	switch spec.Kind {
 	case "image":
-		list, err := cl.ImageList(ctx, dockertypes.ImageListOptions{All: true})
+		list, err := cl.ImageList(ctx, dockerimagetypes.ListOptions{All: true})
 		if err != nil {
 			return nil, err
 		}
@@ -597,7 +598,7 @@ func (r *defaultResolver) resolveDocker(ctx context.Context, spec InputSpecDocke
 			})
 		}
 	case "network":
-		networks, err := cl.NetworkList(ctx, dockertypes.NetworkListOptions{})
+		networks, err := cl.NetworkList(ctx, dockernetworktypes.ListOptions{})
 		if err != nil {
 			return nil, err
 		}
