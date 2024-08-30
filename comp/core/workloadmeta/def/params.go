1d4f299567617343268e48964ab2221c9d95daf0
jackgopack4 <john.peterson@datadoghq.com>
2024-08-30 14:34:50 -0400

    replace workloadmeta package name

--- comp/core/workloadmeta/def/params.go
+++ comp/core/workloadmeta/def/params.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
-package workloadmetadef
+package workloadmeta
 
 // Params provides the kind of agent we're instantiating workloadmeta for
 type Params struct {
