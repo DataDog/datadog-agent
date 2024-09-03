18f95dfd50e091a91bbe259b924180c631c1c7e3
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-09-03 11:57:03 +0200

    Improve retry logic and send events on failures

--- test/new-e2e/system-probe/connector/metric/metric.go
+++ test/new-e2e/system-probe/connector/metric/metric.go
@@ -15,6 +15,7 @@ import (
 	"os"
 
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV2"
 )
 
@@ -30,6 +31,29 @@ func SubmitExecutionMetric(metricBody datadogV2.MetricPayload) error {
 	apiClient := datadog.NewAPIClient(configuration)
 	api := datadogV2.NewMetricsApi(apiClient)
 	resp, r, err := api.SubmitMetrics(ctx, metricBody, *datadogV2.NewSubmitMetricsOptionalParameters())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		return fmt.Errorf("error when calling `MetricsApi.SubmitMetrics`: %v", err)
+	}
+
+	responseContent, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Fprintf(os.Stdout, "Response from `MetricsApi.SubmitMetrics`:\n%s\n", responseContent)
+
+	return nil
+}
+
+func SubmitExecutionEvent(eventBody datadogV1.EventCreateRequest) error {
+	if _, ok := os.LookupEnv("DD_API_KEY"); !ok {
+		fmt.Fprintf(os.Stderr, "skipping sending metric because DD_API_KEY not present")
+		return nil
+	}
+
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV1.NewEventsApi(apiClient)
+	resp, r, err := api.CreateEvent(ctx, eventBody)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
