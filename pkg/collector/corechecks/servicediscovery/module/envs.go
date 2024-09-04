7c52b261885672e9962a474fd606af4476cdef45
Raphael Gavache <raphael.gavache@datadoghq.com>
2024-09-04 15:40:34 +0200

    read inject process metadata

--- pkg/collector/corechecks/servicediscovery/module/envs.go
+++ pkg/collector/corechecks/servicediscovery/module/envs.go
@@ -15,39 +15,22 @@ import (
 	"strings"
 
 	"github.com/DataDog/datadog-agent/pkg/util/kernel"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 	"github.com/shirou/gopsutil/v3/process"
 )
 
 const (
-	// injectorMemFdName is the name the injector (Datadog/auto_inject) uses.
-	injectorMemFdName = "dd_environ"
-	injectorMemFdPath = "/memfd:" + injectorMemFdName + " (deleted)"
+	injectorMemFdName = "dd_process_inject_info.msgpack"
+	injectorMemFdPath = "/memfd:" + injectorMemFdName
 
 	// memFdMaxSize is used to limit the amount of data we read from the memfd.
 	// This is for safety to limit our memory usage in the case of a corrupt
 	// file.
-	memFdMaxSize = 4096
+	// matches limit in the [auto injector](https://github.com/DataDog/auto_inject/blob/5ae819d01d8625c24dcf45b8fef32a7d94927d13/librouter.c#L52)
+	memFdMaxSize = 65536
 )
 
-// readEnvsFile reads the env file created by the auto injector. The file
-// contains the variables in a format similar to /proc/$PID/environ: ENV=VAL,
-// separated by \000.
-func readEnvsFile(path string) ([]string, error) {
-	reader, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer reader.Close()
-
-	data, err := io.ReadAll(io.LimitReader(reader, memFdMaxSize))
-	if err != nil {
-		return nil, err
-	}
-
-	return strings.Split(string(data), "\000"), nil
-}
-
-// getInjectedEnvs gets environment variables injected by the auto injector, if
+// getInjectionMeta gets metadata from auto injector injection, if
 // present. The auto injector creates a memfd file with a specific name into which
 // it writes the environment variables. In order to find the correct file, we
 // need to iterate the list of files (named after file descriptor numbers) in
@@ -61,55 +44,102 @@ func readEnvsFile(path string) ([]string, error) {
 // lrwx------ 1 foo foo 64 Aug 13 14:24 2 -> /dev/pts/6
 // lrwx------ 1 foo foo 64 Aug 13 14:24 3 -> '/memfd:dd_environ (deleted)'
 // ```
-func getInjectedEnvs(proc *process.Process) []string {
-	fdsPath := kernel.HostProc(strconv.Itoa(int(proc.Pid)), "fd")
-	entries, err := os.ReadDir(fdsPath)
+func getInjectionMeta(proc *process.Process) (*InjectedProcess, bool) {
+	path, found := findInjectorFile(proc)
+	if !found {
+		return nil, false
+	}
+	injectionMeta, err := extractInjectionMeta(path)
 	if err != nil {
-		return nil
+		log.Warnf("error extracting injected envs: %s", err)
+		return nil, false
 	}
+	return injectionMeta, true
 
-	for _, entry := range entries {
-		path := filepath.Join(fdsPath, entry.Name())
-		name, err := os.Readlink(path)
-		if err != nil {
-			continue
-		}
-
-		if name != injectorMemFdPath {
-			continue
-		}
-
-		envs, _ := readEnvsFile(path)
-		return envs
-	}
-
-	return nil
 }
 
-// envsToMap splits a list of strings containing environment variables of the
-// format NAME=VAL to a map.
-func envsToMap(envs ...string) map[string]string {
-	envMap := make(map[string]string, len(envs))
-	for _, env := range envs {
-		name, val, found := strings.Cut(env, "=")
-		if !found {
-			continue
-		}
+func extractInjectionMeta(path string) (*InjectedProcess, error) {
+	reader, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
 
-		envMap[name] = val
+	data, err := io.ReadAll(io.LimitReader(reader, memFdMaxSize))
+	if err != nil || len(data) == memFdMaxSize {
+		return nil, err
 	}
 
-	return envMap
+	var injectedProc InjectedProcess
+	_, err = injectedProc.UnmarshalMsg(data)
+	if err != nil {
+		return nil, err
+	}
+	return &injectedProc, nil
+}
+
+func findInjectorFile(proc *process.Process) (string, bool) {
+	fdsPath := kernel.HostProc(strconv.Itoa(int(proc.Pid)), "fd")
+	// quick path, the shadow file is the first opened file by the process
+	// unless there are inherited fds
+	path := filepath.Join(fdsPath, "3")
+	if isInjectorFile(path) {
+		return path, true
+	}
+	entries, err := os.ReadDir(fdsPath)
+	if err != nil {
+		return "", false
+	}
+	for _, entry := range entries {
+		switch entry.Name() {
+		case "0", "1", "2", "3":
+			continue
+		default:
+			path := filepath.Join(fdsPath, entry.Name())
+			if isInjectorFile(path) {
+				return path, true
+			}
+		}
+	}
+	return "", false
+}
+
+func isInjectorFile(path string) bool {
+	name, err := os.Readlink(path)
+	if err != nil {
+		return false
+	}
+	return strings.HasPrefix(name, injectorMemFdPath)
+}
+
+// addEnvToMap splits a list of strings containing environment variables of the
+// format NAME=VAL to a map.
+func addEnvToMap(env string, envs map[string]string) {
+	name, val, found := strings.Cut(env, "=")
+	if !found {
+		return
+	}
+	envs[name] = val
+	return
 }
 
 // getEnvs gets the environment variables for the process, both the initial
 // ones, and if present, the ones injected via the auto injector.
 func getEnvs(proc *process.Process) (map[string]string, error) {
-	envs, err := proc.Environ()
+	procEnvs, err := proc.Environ()
 	if err != nil {
 		return nil, err
 	}
-
-	envs = append(envs, getInjectedEnvs(proc)...)
-	return envsToMap(envs...), nil
+	envs := make(map[string]string, len(procEnvs))
+	for _, env := range procEnvs {
+		addEnvToMap(env, envs)
+	}
+	injectionMeta, ok := getInjectionMeta(proc)
+	if !ok {
+		return envs, nil
+	}
+	for _, env := range injectionMeta.InjectedEnv {
+		addEnvToMap(string(env), envs)
+	}
+	return envs, nil
 }
