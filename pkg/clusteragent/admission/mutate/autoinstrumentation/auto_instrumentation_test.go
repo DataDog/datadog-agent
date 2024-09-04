385b43c2a8c74bf6b05b48916cfa61fdd1618fca
David Ortiz <david.ortiz@datadoghq.com>
2024-09-04 11:25:18 +0200

    [clusteragent/admission/mutate] Mark injected volumes as safe to evict

--- pkg/clusteragent/admission/mutate/autoinstrumentation/auto_instrumentation_test.go
+++ pkg/clusteragent/admission/mutate/autoinstrumentation/auto_instrumentation_test.go
@@ -262,6 +262,10 @@ func TestInjectAutoInstruConfigV2(t *testing.T) {
 			require.Equal(t, etcVolume.Name, tt.pod.Spec.Volumes[1].Name,
 				"expected datadog-etc volume to be injected")
 
+			volumesMarkedAsSafeToEvict := strings.Split(tt.pod.Annotations[common.K8sAutoscalerSafeToEvictVolumesAnnotation], ",")
+			require.Contains(t, volumesMarkedAsSafeToEvict, volumeName, "expected volume %s to be marked as safe to evict", volumeName)
+			require.Contains(t, volumesMarkedAsSafeToEvict, etcVolume.Name, "expected volume %s to be marked as safe to evict", etcVolume.Name)
+
 			require.Equal(t, len(tt.libInfo.libs)+1, len(tt.pod.Spec.InitContainers),
 				"expected there to be one more than the number of libs to inject for init containers")
 
