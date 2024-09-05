73f6821e928efd73e96c0a099fcc0814085a4d81
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2024-09-05 12:53:44 +0200

    catching exception at the top level code

--- omnibus/python-scripts/postinst.py
+++ omnibus/python-scripts/postinst.py
@@ -10,28 +10,32 @@ import os
 import sys
 import packages
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: postinst.py <INSTALL_DIR>")
-        sys.exit(1)
-    install_directory = sys.argv[1]
-    if os.path.exists(install_directory):
-        postinst_python_installed_packages_file = packages.postinst_python_installed_packages_file(install_directory)
-        packages.create_python_installed_packages_file(postinst_python_installed_packages_file)
-        ENV_VAR = 'INSTALL_PYTHON_THIRD_PARTY_DEPS'
-        if ENV_VAR in os.environ:
-            env_var_value = os.environ[ENV_VAR].lower() in ['true', '1', 't', 'yes', 'y']
-            if env_var_value:
-                diff_python_installed_packages_file = packages.diff_python_installed_packages_file(install_directory)
-                if os.path.exists(diff_python_installed_packages_file):
-                    packages.install_diff_packages_file(f"{install_directory}/embedded/bin/pip", diff_python_installed_packages_file)
-                    packages.cleanup_files(diff_python_installed_packages_file)
+def main():
+    try:
+        if len(sys.argv) != 2:
+            print("Usage: postinst.py <INSTALL_DIR>")
+        install_directory = sys.argv[1]
+        if os.path.exists(install_directory):
+            postinst_python_installed_packages_file = packages.postinst_python_installed_packages_file(install_directory)
+            packages.create_python_installed_packages_file(postinst_python_installed_packages_file)
+            env_var = 'INSTALL_PYTHON_THIRD_PARTY_DEPS'
+            if env_var in os.environ:
+                env_var_value = os.environ[env_var].lower() in ['true', '1', 't', 'yes', 'y']
+                if env_var_value:
+                    diff_python_installed_packages_file = packages.diff_python_installed_packages_file(install_directory)
+                    if os.path.exists(diff_python_installed_packages_file):
+                        packages.install_diff_packages_file(f"{install_directory}/embedded/bin/pip", diff_python_installed_packages_file)
+                        packages.cleanup_files(diff_python_installed_packages_file)
+                    else:
+                        print(f"File {diff_python_installed_packages_file} does not exist.")
                 else:
-                    print(f"File {diff_python_installed_packages_file} does not exist.")
+                    print(f"{env_var} is set to: {env_var_value}")
             else:
-                print(f"{ENV_VAR} is set to: {env_var_value}")
+                print(f"{env_var} is not set.")
         else:
-            print(f"{ENV_VAR} is not set.")
-    else:
-        print(f"Directory {install_directory} does not exist.")
-        sys.exit(1)
+            print(f"Directory {install_directory} does not exist.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
