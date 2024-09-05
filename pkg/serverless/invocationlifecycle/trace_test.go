d8a0cb7c49384ee9515dc4974d91b503bf984ec9
chris.agocs <chris.agocs@datadoghq.com>
2024-09-05 13:40:01 -0700

    Make trace_test use anonymous structs

--- pkg/serverless/invocationlifecycle/trace_test.go
+++ pkg/serverless/invocationlifecycle/trace_test.go
@@ -102,22 +102,17 @@ func TestStartExecutionSpan(t *testing.T) {
 	reqHeadersWithCtx.Set("traceparent", "00-00000000000000000000000000000006-0000000000000006-01")
 
 	stepFunctionEvent := events.StepFunctionPayload{
-		Execution: events.StepFunctionExecution{
-			ID:           "arn:aws:states:us-east-1:425362996713:execution:agocsTestSF:aa6c9316-713a-41d4-9c30-61131716744f",
-			Input:        interface{}(nil),
-			StartTime:    "2024-07-30T20:46:20.777Z",
-			Name:         "aa6c9316-713a-41d4-9c30-61131716744f",
-			RoleArn:      "arn:aws:iam::425362996713:role/test-serverless-stepfunctions-dev-AgocsTestSFRole-tRkeFXScjyk4",
-			RedriveCount: 0,
+		Execution: struct {
+			ID string
+		}{
+			ID: "arn:aws:states:us-east-1:425362996713:execution:agocsTestSF:aa6c9316-713a-41d4-9c30-61131716744f",
 		},
-		StateMachine: events.StepFunctionStateMachine{
-			ID:   "arn:aws:states:us-east-1:425362996713:stateMachine:agocsTestSF",
-			Name: "agocsTestSF",
-		},
-		State: events.StepFunctionState{
+		State: struct {
+			Name        string
+			EnteredTime string
+		}{
 			Name:        "agocsTest1",
 			EnteredTime: "2024-07-30T20:46:20.824Z",
-			RetryCount:  0,
 		},
 	}
 
@@ -757,22 +752,17 @@ func TestEndExecutionSpanWithStepFunctions(t *testing.T) {
 	}
 
 	stepFunctionEvent := events.StepFunctionPayload{
-		Execution: events.StepFunctionExecution{
-			ID:           "arn:aws:states:us-east-1:425362996713:execution:agocsTestSF:aa6c9316-713a-41d4-9c30-61131716744f",
-			Input:        interface{}(nil),
-			StartTime:    "2024-07-30T20:46:20.777Z",
-			Name:         "aa6c9316-713a-41d4-9c30-61131716744f",
-			RoleArn:      "arn:aws:iam::425362996713:role/test-serverless-stepfunctions-dev-AgocsTestSFRole-tRkeFXScjyk4",
-			RedriveCount: 0,
-		},
-		StateMachine: events.StepFunctionStateMachine{
-			ID:   "arn:aws:states:us-east-1:425362996713:stateMachine:agocsTestSF",
-			Name: "agocsTestSF",
-		},
-		State: events.StepFunctionState{
+		Execution: struct{ ID string }(struct {
+			ID string `json:"id"`
+		}{
+			ID: "arn:aws:states:us-east-1:425362996713:execution:agocsTestSF:aa6c9316-713a-41d4-9c30-61131716744f",
+		}),
+		State: struct {
+			Name        string
+			EnteredTime string
+		}{
 			Name:        "agocsTest1",
 			EnteredTime: "2024-07-30T20:46:20.824Z",
-			RetryCount:  0,
 		},
 	}
 
