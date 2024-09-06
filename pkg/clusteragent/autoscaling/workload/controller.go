b680558ac724520d7a1eb8b4e4e60b77c9b04928
Jennifer Chen <jennifer.chen@datadoghq.com>
2024-09-06 15:18:30 -0400

    Add check for 100 DPAs to validation logic

--- pkg/clusteragent/autoscaling/workload/controller.go
+++ pkg/clusteragent/autoscaling/workload/controller.go
@@ -285,7 +285,7 @@ func (c *Controller) syncPodAutoscaler(ctx context.Context, key, ns, name string
 	podAutoscalerInternal.SetError(nil)
 
 	// Validate autoscaler requirements
-	validationErr := c.validateAutoscaler(podAutoscaler)
+	validationErr := c.validateAutoscaler(key, podAutoscaler)
 	if validationErr != nil {
 		podAutoscalerInternal.SetError(validationErr)
 		return autoscaling.NoRequeue, c.updateAutoscalerStatusAndUnlock(ctx, key, ns, name, validationErr, podAutoscalerInternal, podAutoscaler)
@@ -396,7 +396,11 @@ func (c *Controller) deletePodAutoscaler(ns, name string) error {
 	return nil
 }
 
-func (c *Controller) validateAutoscaler(podAutoscaler *datadoghq.DatadogPodAutoscaler) error {
+func (c *Controller) validateAutoscaler(key string, podAutoscaler *datadoghq.DatadogPodAutoscaler) error {
+	// Check that we are within the limit of 100 DatadogPodAutoscalers
+	if !c.hashHeap.Exists(key) {
+		return fmt.Errorf("Autoscaler disabled as maximum number per cluster reached (%d).", maxDatadogPodAutoscalerObjects)
+	}
 
 	// Check that targetRef is not set to the cluster agent
 	clusterAgentPodName, err := common.GetSelfPodName()
