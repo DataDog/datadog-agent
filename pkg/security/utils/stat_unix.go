d51f5b9948600c9b7a1b1ce867ee7c0153d1eca6
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-09-06 18:51:44 +0200

    fix build tag

--- pkg/security/utils/stat_unix.go
+++ pkg/security/utils/stat_unix.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build unix
+
 // Package utils holds utils related files
 package utils
 
