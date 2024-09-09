95046d0f3cb6a689b66b2a74c0287e418d8e26ae
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-09-09 11:42:01 +0200

    improved version of `HostProc`

--- pkg/util/kernel/fs.go
+++ pkg/util/kernel/fs.go
@@ -9,9 +9,11 @@ package kernel
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/moby/sys/mountinfo"
 
@@ -62,7 +64,47 @@ var SysFSRoot = funcs.MemoizeNoError(func() string {
 
 // hostProcInternal is the testable/benchmarkable version of HostProc
 func hostProcInternal(procFsRoot string, combineWith ...string) string {
-	return filepath.Join(procFsRoot, filepath.Join(combineWith...))
+	const sepLen = len(string(filepath.Separator))
+
+	// pre-compute the total size of the string to avoid reallocations
+	size := len(procFsRoot)
+	allEmpty := true
+	for _, c := range combineWith {
+		if len(c) > math.MaxInt-sepLen {
+			panic("int overflow in HostProc")
+		}
+		toadd := sepLen + len(c)
+
+		if size > math.MaxInt-toadd {
+			panic("int overflow in HostProc")
+		}
+		size += toadd
+
+		if c != "" {
+			allEmpty = false
+		}
+	}
+
+	// early escape to return "" if procFsRoot is empty and all combineWith are empty
+	// this is to match the behavior of filepath.Join
+	if procFsRoot == "" && allEmpty {
+		return ""
+	}
+
+	var builder strings.Builder
+	builder.Grow(size)
+
+	builder.WriteString(procFsRoot)
+	for i, c := range combineWith {
+		// the goal is not add the separator at the very beginning if procFsRoot is empty because we want to
+		// get a relative path, again the goal is to match the behavior of filepath.Join
+		if i != 0 || procFsRoot != "" {
+			builder.WriteRune(filepath.Separator)
+		}
+		builder.WriteString(c)
+	}
+
+	return filepath.Clean(builder.String())
 }
 
 // HostProc returns the location of a host's procfs. This can and will be
