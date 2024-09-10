fd5486a8c4f445f40470d57f193db90f850bb93e
Guillermo Julián <gjulianm@users.noreply.github.com>
2024-09-10 20:33:10 +0200

    [EBPF] Replace generated pointers to structs with uintptr (#29221)

--- pkg/ebpf/cgo/genpost.go
+++ pkg/ebpf/cgo/genpost.go
@@ -44,6 +44,10 @@ func main() {
 	convertInt8ArrayToByteArrayRegex := regexp.MustCompile(`(` + strings.Join(int8variableNames, "|") + `)(\s+)\[(\d+)\]u?int8`)
 	b = convertInt8ArrayToByteArrayRegex.ReplaceAll(b, []byte("$1$2[$3]byte"))
 
+	// Convert generated pointers to CGo structs to uintptr
+	convertPointerToUint64Regex := regexp.MustCompile(`\*_Ctype_struct_(\w+)`)
+	b = convertPointerToUint64Regex.ReplaceAll(b, []byte("uintptr"))
+
 	b, err = format.Source(b)
 	if err != nil {
 		log.Fatal(err)
