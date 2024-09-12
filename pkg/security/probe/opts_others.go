41ffcc75e146fd2e34b2548b6899d1721d2a5b4b
Sylvain Afchain <safchain@gmail.com>
2024-09-12 13:22:09 +0200

    [CWS] do not eval event originating from system-probe itself (#29275)

--- pkg/security/probe/opts_others.go
+++ pkg/security/probe/opts_others.go
@@ -3,29 +3,13 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build windows
+//go:build !linux && !windows
 
 // Package probe holds probe related files
 package probe
-
-import (
-	"github.com/DataDog/datadog-go/v5/statsd"
-)
 
 // Opts defines some probe options
 type Opts struct {
 	// DontDiscardRuntime do not discard the runtime. Mostly used by functional tests
 	DontDiscardRuntime bool
-
-	// StatsdClient to be used for probe stats
-	StatsdClient statsd.ClientInterface
-
-	// this option for test purposes only; should never be true in main code
-	disableProcmon bool
-}
-
-func (o *Opts) normalize() {
-	if o.StatsdClient == nil {
-		o.StatsdClient = &statsd.NoOpClient{}
-	}
 }
