f334930e0beb26b433502ad1778371da4b454216
nina9753 <nina.rei@datadoghq.com>
2024-09-12 10:57:28 -0400

    Update serverless-init Cloud Run service to support cloud run functions via cloud functions

--- cmd/serverless-init/cloudservice/cloudrun_test.go
+++ cmd/serverless-init/cloudservice/cloudrun_test.go
@@ -78,3 +78,45 @@ func TestGetCloudRunTagsWithEnvironmentVariables(t *testing.T) {
 		"_dd.origin":    "cloudrun",
 	}, tags)
 }
+
+func TestGetCloudRunFunctionTagsWithEnvironmentVariables(t *testing.T) {
+	service := &CloudRun{}
+
+	metadataHelperFunc = func(*helper.GCPConfig) *helper.GCPMetadata {
+		return &helper.GCPMetadata{
+			ContainerID: &helper.Info{
+				TagName: "container_id",
+				Value:   "test_container",
+			},
+			Region: &helper.Info{
+				TagName: "region",
+				Value:   "test_region",
+			},
+			ProjectID: &helper.Info{
+				TagName: "project_id",
+				Value:   "test_project",
+			},
+		}
+	}
+
+	t.Setenv("K_SERVICE", "test_service")
+	t.Setenv("K_REVISION", "test_revision")
+	t.Setenv("K_CONFIGURATION", "test_config")
+	t.Setenv("FUNCTION_SIGNATURE_TYPE", "test_signature")
+	t.Setenv("FUNCTION_TARGET", "test_target")
+
+	tags := service.GetTags()
+
+	assert.Equal(t, map[string]string{
+		"container_id":            "test_container",
+		"region":                  "test_region",
+		"origin":                  "cloudfunction",
+		"project_id":              "test_project",
+		"service_name":            "test_service",
+		"revision_name":           "test_revision",
+		"configuration_name":      "test_config",
+		"_dd.origin":              "cloudfunction",
+		"function_target":         "test_target",
+		"function_signature_type": "test_signature",
+	}, tags)
+}
