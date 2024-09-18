d49c475b8b2e41b8b0dadbc705e762a2278bf384
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2024-09-18 11:46:36 +0200

    Add unit test helpers and missing tests

--- pkg/security/process_list/process_resolver/process_resolver_test.go
+++ pkg/security/process_list/process_resolver/process_resolver_test.go
@@ -16,12 +16,17 @@ import (
 	cgroupModel "github.com/DataDog/datadog-agent/pkg/security/resolvers/cgroup/model"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/slices"
 )
 
-func newFakeExecEvent() *model.Event {
+func newFakeExecEvent(ppid, pid, tid int) *model.Event {
 	e := model.NewFakeEvent()
 	e.Type = uint32(model.ExecEventType)
 	e.ProcessContext = &model.ProcessContext{}
+	e.ProcessContext.PPid = uint32(ppid)
+	e.ProcessContext.Pid = uint32(pid)
+	e.ProcessContext.Tid = uint32(tid)
+	e.ProcessContext.ForkTime = time.Now()
 	return e
 }
 
@@ -53,6 +58,90 @@ func (ts *testStats) ValidateCounters(t *testing.T, pl *processlist.ProcessList)
 	assert.Equal(t, int(ts.CurrentExecNodes), pl.GetExecCacheSize())
 }
 
+func checkParentality(pl *processlist.ProcessList, pc *ProcessResolver, parent, child *model.Event) bool {
+	// first, get cached processes
+	cachedProcessParent := pl.GetCacheProcess(pc.GetProcessCacheKey(&parent.ProcessContext.Process))
+	cachedProcessChild := pl.GetCacheProcess(pc.GetProcessCacheKey(&child.ProcessContext.Process))
+	if cachedProcessParent == nil || cachedProcessChild == nil {
+		return false
+	}
+	// then, ensure child is part of parent children
+	if !slices.ContainsFunc(cachedProcessParent.Children, func(c *processlist.ProcessNode) bool {
+		return pc.ProcessMatches(cachedProcessChild, c)
+	}) {
+		return false
+	}
+
+	// validate process / exec links
+
+	// 1/ for parent
+	cachedExecParent := pl.GetCacheExec(pc.GetExecCacheKey(&parent.ProcessContext.Process))
+	if cachedExecParent == nil {
+		return false
+	}
+	if !slices.ContainsFunc(cachedProcessParent.PossibleExecs, func(e *processlist.ExecNode) bool {
+		return pc.ExecMatches(e, cachedExecParent)
+	}) {
+		return false
+	}
+	if cachedExecParent.ProcessLink != cachedProcessParent {
+		return false
+	}
+
+	// 1/ for child
+	cachedExecChild := pl.GetCacheExec(pc.GetExecCacheKey(&child.ProcessContext.Process))
+	if cachedExecChild == nil {
+		return false
+	}
+	if !slices.ContainsFunc(cachedProcessChild.PossibleExecs, func(e *processlist.ExecNode) bool {
+		return pc.ExecMatches(e, cachedExecChild)
+	}) {
+		return false
+	}
+	if cachedExecChild.ProcessLink != cachedProcessChild {
+		return false
+	}
+	return true
+}
+
+func isProcessAndExecPresent(pl *processlist.ProcessList, pc *ProcessResolver, event *model.Event) bool {
+	// first, get cached process
+	cachedProcess := pl.GetCacheProcess(pc.GetProcessCacheKey(&event.ProcessContext.Process))
+	if cachedProcess == nil {
+		return false
+	}
+
+	// validate process / exec links
+	cachedExec := pl.GetCacheExec(pc.GetExecCacheKey(&event.ProcessContext.Process))
+	if cachedExec == nil {
+		return false
+	}
+	if !slices.ContainsFunc(cachedProcess.PossibleExecs, func(e *processlist.ExecNode) bool {
+		return pc.ExecMatches(e, cachedExec)
+	}) {
+		return false
+	}
+	if cachedExec.ProcessLink != cachedProcess {
+		return false
+	}
+	return true
+}
+
+func isProcessOrExecPresent(pl *processlist.ProcessList, pc *ProcessResolver, event *model.Event) bool {
+	// first, check process presence
+	cachedProcess := pl.GetCacheProcess(pc.GetProcessCacheKey(&event.ProcessContext.Process))
+	if cachedProcess != nil {
+		return true
+	}
+
+	// then exec
+	cachedExec := pl.GetCacheExec(pc.GetExecCacheKey(&event.ProcessContext.Process))
+	if cachedExec != nil {
+		return true
+	}
+	return false
+}
+
 func TestFork1st(t *testing.T) {
 	pc := NewProcessResolver()
 	processList := processlist.NewProcessList(cgroupModel.WorkloadSelector{Image: "*", Tag: "*"},
@@ -60,59 +149,30 @@ func TestFork1st(t *testing.T) {
 	stats := testStats{}
 
 	// parent
-	parent := newFakeExecEvent()
-	parent.ProcessContext.Pid = 1
-	parent.ProcessContext.Tid = 1
-	parent.ProcessContext.ForkTime = time.Now()
+	parent := newFakeExecEvent(0, 1, 1)
 	new, err := processList.Insert(parent, true, "")
 	if err != nil {
 		t.Fatal(err)
 	}
-	stats.AddProcess(1)
 	assert.Equal(t, true, new)
-	assert.Equal(t, nil, err)
-	cachedParent := processList.GetCacheExec(pc.GetExecCacheKey(&parent.ProcessContext.Process))
-	if cachedParent == nil {
+	stats.AddProcess(1)
+	stats.ValidateCounters(t, processList)
+	if !isProcessAndExecPresent(processList, pc, parent) {
 		t.Fatal("didn't found cached parent")
 	}
-	stats.ValidateCounters(t, processList)
 
 	// parent
 	//     \ child
-	child := newFakeExecEvent()
-	child.ProcessContext.PPid = 1
-	child.ProcessContext.Pid = 2
-	child.ProcessContext.Tid = 2
-	child.ProcessContext.ForkTime = time.Now()
+	child := newFakeExecEvent(1, 2, 2)
 	new, err = processList.Insert(child, true, "")
 	if err != nil {
 		t.Fatal(err)
 	}
 	assert.Equal(t, true, new)
-	assert.Equal(t, nil, err)
 	stats.AddProcess(1)
-	cachedChild := processList.GetCacheExec(pc.GetExecCacheKey(&child.ProcessContext.Process))
-	if cachedChild == nil {
-		t.Fatal("didn't found cached child")
-	}
-	assert.Equal(t, child.ProcessContext.Process, cachedChild.Process)
 	stats.ValidateCounters(t, processList)
-	assert.Equal(t, cachedParent.ProcessLink, cachedChild.ProcessLink.GetCurrentParent())
-	childs := cachedParent.ProcessLink.GetChildren()
-	if childs == nil {
-		t.Fatal("no childs returned")
-	} else if len(*childs) != 1 {
-		t.Error("not only 1 child")
-	}
-	found := false
-	for _, child := range *childs {
-		if child.CurrentExec == cachedChild {
-			found = true
-			break
-		}
-	}
-	if !found {
-		t.Error("child not found in cached parent")
+	if checkParentality(processList, pc, parent, child) == false {
+		t.Fatal("parent / child paternality not found")
 	}
 
 	// parent
@@ -122,22 +182,37 @@ func TestFork1st(t *testing.T) {
 		t.Fatal(err)
 	}
 	assert.Equal(t, true, deleted)
-	assert.Equal(t, nil, err)
 	stats.DeleteProcess(1)
-	cachedParent = processList.GetCacheExec(pc.GetExecCacheKey(&parent.ProcessContext.Process))
-	if cachedParent == nil {
+	stats.ValidateCounters(t, processList)
+	if !isProcessAndExecPresent(processList, pc, parent) {
 		t.Fatal("didn't found cached parent")
 	}
-	stats.ValidateCounters(t, processList)
+	if isProcessOrExecPresent(processList, pc, child) {
+		t.Fatal("child still present")
+	}
 
 	// nothing
 	deleted, err = processList.DeleteProcess(pc.GetProcessCacheKey(&parent.ProcessContext.Process), "")
 	assert.Equal(t, true, deleted)
-	assert.Equal(t, nil, err)
 	stats.DeleteProcess(1)
-	cachedParent = processList.GetCacheExec(pc.GetExecCacheKey(&parent.ProcessContext.Process))
-	if cachedParent != nil {
+	stats.ValidateCounters(t, processList)
+	if isProcessOrExecPresent(processList, pc, parent) {
 		t.Fatal("parent still present")
 	}
-	stats.ValidateCounters(t, processList)
 }
+
+//
+// TODO: tests from pkg/security/resolvers/process/resolver_test.go to add:
+//
+
+func TestFork2nd(t *testing.T)            {}
+func TestForkExec(t *testing.T)           {}
+func TestForkExecExec(t *testing.T)       {}
+func TestOrphanExec(t *testing.T)         {}
+func TestForkReuse(t *testing.T)          {}
+func TestForkForkExec(t *testing.T)       {}
+func TestExecBomb(t *testing.T)           {}
+func TestExecLostFork(t *testing.T)       {}
+func TestExecLostExec(t *testing.T)       {}
+func TestIsExecExecRuntime(t *testing.T)  {}
+func TestIsExecExecSnapshot(t *testing.T) {}
