c136371c927d7a074603ee2d23a42df72faf1f14
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2024-09-19 17:25:18 +0200

    test update go version

--- tasks/go.py
+++ tasks/go.py
@@ -446,7 +446,7 @@ def tidy(ctx):
 @task
 def check_go_version(ctx):
     go_version_output = ctx.run('go version')
-    # result is like "go version go1.22.6 linux/amd64"
+    # result is like "go version go1.22.7 linux/amd64"
     running_go_version = go_version_output.stdout.split(' ')[2]
 
     with open(".go-version") as f:
