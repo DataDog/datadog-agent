a8c4973aa502d1d4e0b1b6a5fec4031da8e915f7
grantseltzer <grantseltzer@gmail.com>
2024-09-19 16:40:58 -0500

    Add end to end tests
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

---
+++ pkg/dynamicinstrumentation/testutil/e2e_test.go
@@ -0,0 +1,176 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// //go:build linux_bpf && test
+
+package testutil
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"reflect"
+	"strings"
+	"testing"
+	"text/template"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/dynamicinstrumentation"
+	"github.com/DataDog/datadog-agent/pkg/dynamicinstrumentation/diconfig"
+	"github.com/DataDog/datadog-agent/pkg/dynamicinstrumentation/ditypes"
+	"github.com/kr/pretty"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGoDI(t *testing.T) {
+	sampleServicePath := BuildSampleService(t)
+	cmd := exec.Command(sampleServicePath)
+	cmd.Env = []string{
+		"DD_DYNAMIC_INSTRUMENTATION_ENABLED=true",
+		"DD_SERVICE=go-di-sample-service",
+		"DD_DYNAMIC_INSTRUMENTATION_OFFLINE=true",
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	require.NoError(t, cmd.Start())
+	t.Cleanup(func() {
+		log.Println(cmd.Process.Kill())
+	})
+
+	eventOutputWriter := &eventOutputTestWriter{
+		t: t,
+	}
+
+	opts := &dynamicinstrumentation.DIOptions{
+		RateLimitPerProbePerSecond: 0.0,
+		ReaderWriterOptions: dynamicinstrumentation.ReaderWriterOptions{
+			CustomReaderWriters: true,
+			SnapshotWriter:      eventOutputWriter,
+			DiagnosticWriter:    os.Stderr,
+		},
+	}
+
+	var (
+		GoDI *dynamicinstrumentation.GoDI
+		err  error
+	)
+
+	GoDI, err = dynamicinstrumentation.RunDynamicInstrumentation(opts)
+	require.NoError(t, err)
+
+	// Give DI time to start up
+	time.Sleep(time.Second * 5)
+
+	cm, ok := GoDI.ConfigManager.(*diconfig.ReaderConfigManager)
+	if !ok {
+		t.Error("Config manager is of wrong type")
+	}
+
+	cfgTemplate, err := template.New("config_template").Parse(configTemplateText)
+	require.NoError(t, err)
+
+	b := []byte{}
+	var buf *bytes.Buffer
+	for function, expectedCaptureValue := range expectedCaptures {
+		// Generate config for this function
+		buf = bytes.NewBuffer(b)
+		functionWithoutPackagePrefix, _ := strings.CutPrefix(function, "main.")
+		err = cfgTemplate.Execute(buf, configDataType{functionWithoutPackagePrefix})
+		require.NoError(t, err)
+		eventOutputWriter.doCompare = false
+		eventOutputWriter.expectedResult = expectedCaptureValue
+
+		// Read the configuration via the config manager
+		_, err := cm.ConfigReader.Read(buf.Bytes())
+		time.Sleep(time.Second * 5)
+		if err != nil {
+			t.Errorf("could not read new configuration: %s", err)
+		}
+
+		fmt.Printf("\n\n")
+	}
+}
+
+type eventOutputTestWriter struct {
+	t              *testing.T
+	doCompare      bool
+	expectedResult map[string]*ditypes.CapturedValue
+}
+
+func (e *eventOutputTestWriter) Write(p []byte) (n int, err error) {
+	var snapshot ditypes.SnapshotUpload
+	if err := json.Unmarshal(p, &snapshot); err != nil {
+		e.t.Error("failed to unmarshal snapshot", err)
+	}
+
+	funcName := snapshot.Debugger.ProbeInSnapshot.Type + "." + snapshot.Debugger.ProbeInSnapshot.Method
+	actual := snapshot.Debugger.Captures.Entry.Arguments
+	scrubPointerValues(actual)
+	if !reflect.DeepEqual(e.expectedResult, actual) {
+		e.t.Error("Unexpected ", funcName)
+		pretty.Log(actual)
+	}
+
+	return len(p), nil
+}
+
+func scrubPointerValues(captures ditypes.CapturedValueMap) {
+	for _, v := range captures {
+		scrubPointerValue(v)
+	}
+}
+
+func scrubPointerValue(capture *ditypes.CapturedValue) {
+	if capture.Type == "ptr" {
+		capture.Value = nil
+	}
+	scrubPointerValues(capture.Fields)
+}
+
+type configDataType struct{ FunctionName string }
+
+var configTemplateText = `
+{
+    "go-di-sample-service": {
+        "e504163d-f367-4522-8905-fe8bc34eb975": {
+            "id": "e504163d-f367-4522-8905-fe8bc34eb975",
+            "version": 0,
+            "type": "LOG_PROBE",
+            "language": "go",
+            "where": {
+                "typeName": "main",
+                "methodName": "{{.FunctionName}}"
+            },
+            "tags": [],
+            "template": "Executed main.{{.FunctionName}}, it took {@duration}ms",
+            "segments": [
+                {
+                "str": "Executed main.{{.FunctionName}}, it took "
+                },
+                {
+                "dsl": "@duration",
+                "json": {
+                    "ref": "@duration"
+                }
+                },
+                {
+                "str": "ms"
+                }
+            ],
+            "captureSnapshot": false,
+            "capture": {
+                "maxReferenceDepth": 3
+            },
+            "sampling": {
+                "snapshotsPerSecond": 5000
+            },
+            "evaluateAt": "EXIT"
+        }
+    }
+}
+`
---
+++ pkg/dynamicinstrumentation/testutil/fixtures.go
@@ -0,0 +1,265 @@
+package testutil
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/dynamicinstrumentation/ditypes"
+)
+
+type funcName = string
+type fixtures = map[funcName]ditypes.CapturedValueMap
+type fieldMap = map[string]*ditypes.CapturedValue
+
+func strPtr(s string) *string { return &s }
+func capturedValue(t string, v string) *ditypes.CapturedValue {
+	return &ditypes.CapturedValue{Type: t, Value: strPtr(v)}
+}
+
+var basicCaptures = fixtures{
+	"main.test_single_bool":   {"x": capturedValue("bool", "true")},
+	"main.test_single_int":    {"x": capturedValue("int", "-1512")},
+	"main.test_single_uint":   {"x": capturedValue("uint", "1512")},
+	"main.test_single_int8":   {"x": capturedValue("int8", "-8")},
+	"main.test_single_int16":  {"x": capturedValue("int16", "-16")},
+	"main.test_single_int32":  {"x": capturedValue("int32", "-32")},
+	"main.test_single_int64":  {"x": capturedValue("int64", "-64")},
+	"main.test_single_uint8":  {"x": capturedValue("uint8", "8")},
+	"main.test_single_uint16": {"x": capturedValue("uint16", "16")},
+	"main.test_single_uint32": {"x": capturedValue("uint32", "32")},
+	"main.test_single_uint64": {"x": capturedValue("uint64", "64")},
+	"main.test_single_byte":   {"x": capturedValue("uint8", "97")},
+	"main.test_single_rune":   {"x": capturedValue("int32", "ă")},
+
+	// everything with float errors out
+	// "main.test_single_float32": {"x": capturedValue("float", "1.32")},
+	// "main.test_single_float64": {"x": capturedValue("float", "-1.646464")},
+}
+
+var multiParamCaptures = fixtures{
+	"main.test_multiple_simple_params": {
+		"a": capturedValue("bool", "false"),
+		"b": capturedValue("uint8", "42"),
+		"c": capturedValue("int32", "122"),
+		"d": capturedValue("uint", "1337"),
+		"e": capturedValue("string", "xyz"),
+	},
+	"main.test_multiple_composite_params": {
+		"a": {Type: "array", Fields: fieldMap{
+			"a_0": capturedValue("string", "one"),
+			"a_1": capturedValue("string", "two"),
+			"a_2": capturedValue("string", "three"),
+		}},
+		"b": {Type: "struct", Fields: fieldMap{
+			"aBool":   capturedValue("bool", "false"),
+			"aString": capturedValue("string", ""),
+			"aNumber": capturedValue("int", "0"),
+			"nested": {Type: "struct", Fields: fieldMap{
+				"anotherInt":    capturedValue("int", "0"),
+				"anotherString": capturedValue("string", ""),
+			}},
+		}},
+		"c": {Type: "slice", Fields: fieldMap{
+			"c_0": capturedValue("uint", "24"),
+			"c_1": capturedValue("uint", "42"),
+		}},
+		"d": {Type: "map", Fields: fieldMap{
+			"foo": capturedValue("string", "bar"),
+		}},
+		"e": {Type: "slice", Fields: fieldMap{
+			"e_0": {Type: "struct", Fields: fieldMap{
+				"anotherInt":    capturedValue("int", "42"),
+				"anotherString": capturedValue("string", "ftwo"),
+			}},
+			"e_1": {Type: "struct", Fields: fieldMap{
+				"anotherInt":    capturedValue("int", "24"),
+				"anotherString": capturedValue("string", "tfour"),
+			}},
+		}},
+	},
+	"main.test_combined_byte": {
+		"w": capturedValue("uint8", "2"),
+		"x": capturedValue("uint8", "3"),
+		"y": capturedValue("uint8", "3.0"),
+	},
+}
+
+var stringCaptures = fixtures{
+	"main.test_single_string": {"x": capturedValue("string", "abc")},
+}
+
+var arrayCaptures = fixtures{
+	"main.test_byte_array": {"x": {Type: "array", Fields: fieldMap{
+		"[2]uint8[0]": capturedValue("uint8", "1"),
+		"[2]uint8[1]": capturedValue("uint8", "1"),
+	}}},
+	"main.test_rune_array": {"x": {Type: "array", Fields: fieldMap{
+		"[2]int32[0]": capturedValue("int32", "1"),
+		"[2]int32[1]": capturedValue("int32", "2"),
+	}}},
+	"main.test_string_array": {"x": {Type: "array", Fields: fieldMap{
+		"[2]string[0]": capturedValue("string", "one"),
+		"[2]string[1]": capturedValue("string", "two"),
+	}}},
+	"main.test_int_array": {"x": {Type: "array", Fields: fieldMap{
+		"[2]int[0]": capturedValue("int", "1"),
+		"[2]int[1]": capturedValue("int", "2"),
+	}}},
+	"main.test_int8_array": {"x": {Type: "array", Fields: fieldMap{
+		"[2]int8[0]": capturedValue("int8", "1"),
+		"[2]int8[1]": capturedValue("int8", "2"),
+	}}},
+	"main.test_uint_array": {"x": {Type: "array", Fields: fieldMap{
+		"[2]uint[0]": capturedValue("uint", "1"),
+		"[2]uint[1]": capturedValue("uint", "2"),
+	}}},
+	"main.test_array_of_arrays": {"a": {Type: "array", Fields: fieldMap{
+		"[2][2]int[0]": {Type: "array", Fields: fieldMap{
+			"[2]int[0]": capturedValue("int", "1"),
+			"[2]int[1]": capturedValue("int", "2"),
+		}},
+		"[2][2]int[1]": {Type: "array", Fields: fieldMap{
+			"[2]int[0]": capturedValue("int", "3"),
+			"[2]int[1]": capturedValue("int", "4"),
+		}},
+	}}},
+	"main.test_array_of_structs": {"a": {Type: "array", Fields: fieldMap{
+		"[2]main.nestedStruct[0]": {Type: "struct", Fields: fieldMap{
+			"anotherInt":    capturedValue("int", "42"),
+			"anotherString": capturedValue("string", "foo"),
+		}},
+		"[2]main.nestedStruct[1]": {Type: "struct", Fields: fieldMap{
+			"anotherInt":    capturedValue("int", "24"),
+			"anotherString": capturedValue("string", "bar"),
+		}},
+	}}},
+	"main.test_array_of_arrays_of_arrays": {"b": {Type: "array", Fields: fieldMap{
+		"[2][2][2]int[0]": {Type: "array", Fields: fieldMap{
+			"[2][2]int[0]": {Type: "array", Fields: fieldMap{
+				"[2]int[0]": capturedValue("int", "1"),
+				"[2]int[1]": capturedValue("int", "2"),
+			}},
+			"[2][2]int[1]": {Type: "array", Fields: fieldMap{
+				"[2]int[0]": capturedValue("int", "3"),
+				"[2]int[1]": capturedValue("int", "4"),
+			}},
+		}},
+		"[2][2][2]int[1]": {Type: "array", Fields: fieldMap{
+			"[2][2]int[0]": {Type: "array", Fields: fieldMap{
+				"[2]int[0]": capturedValue("int", "5"),
+				"[2]int[1]": capturedValue("int", "6"),
+			}},
+			"[2][2]int[1]": {Type: "array", Fields: fieldMap{
+				"[2]int[0]": capturedValue("int", "7"),
+				"[2]int[1]": capturedValue("int", "8"),
+			}},
+		}},
+	}}},
+}
+
+var structCaptures = fixtures{
+	"main.test_string_struct": {"t": {Type: "struct", Fields: fieldMap{
+		"a": capturedValue("string", "a"),
+		"b": capturedValue("string", "bb"),
+		"c": capturedValue("string", "ccc"),
+	}}},
+	"main.test_lots_of_fields": {"l": {Type: "struct", Fields: fieldMap{
+		"a": capturedValue("uint8", "1"),
+		"b": capturedValue("uint8", "2"),
+		"c": capturedValue("uint8", "3"),
+		"d": capturedValue("uint8", "4"),
+		"e": capturedValue("uint8", "5"),
+		"f": capturedValue("uint8", "6"),
+		"g": capturedValue("uint8", "7"),
+		"h": capturedValue("uint8", "8"),
+		"i": capturedValue("uint8", "9"),
+		"j": capturedValue("uint8", "10"),
+		"k": capturedValue("uint8", "11"),
+		"l": capturedValue("uint8", "12"),
+		"m": capturedValue("uint8", "13"),
+		"n": capturedValue("uint8", "14"),
+		"o": capturedValue("uint8", "15"),
+		"p": capturedValue("uint8", "16"),
+		"q": capturedValue("uint8", "17"),
+		"r": capturedValue("uint8", "18"),
+		"s": capturedValue("uint8", "19"),
+		"t": capturedValue("uint8", "20"),
+		"u": capturedValue("CutFieldLimit", "reached field limit"),
+		"v": capturedValue("CutFieldLimit", "reached field limit"),
+		"w": capturedValue("CutFieldLimit", "reached field limit"),
+		"x": capturedValue("CutFieldLimit", "reached field limit"),
+		"y": capturedValue("CutFieldLimit", "reached field limit"),
+		"z": capturedValue("CutFieldLimit", "reached field limit"),
+	}}},
+	"main.test_nonembedded_struct": {"x": {Type: "struct", Fields: fieldMap{
+		"aBool":  capturedValue("bool", "true"),
+		"aInt":   capturedValue("int", "1"),
+		"aInt16": capturedValue("int16", "2"),
+	}}},
+	"main.test_struct_pointer": {"x": {Type: "ptr", Fields: fieldMap{
+		"": {
+			Type: "struct", Fields: fieldMap{
+				"aBool":  capturedValue("bool", "true"),
+				"aInt":   capturedValue("int", "1"),
+				"aInt16": capturedValue("int16", "2"),
+			}},
+	}}},
+	"main.test_multiple_embedded_struct": {"b": {Type: "struct", Fields: fieldMap{
+		"aBool":  capturedValue("bool", "true"),
+		"aInt16": capturedValue("int16", "42"),
+		"aInt32": capturedValue("int32", "31"),
+		"nested": {Type: "struct", Fields: fieldMap{
+			"aBool":   capturedValue("bool", "true"),
+			"aString": capturedValue("string", "one"),
+			"aNumber": capturedValue("int", "2"),
+			"nested": {Type: "struct", Fields: fieldMap{
+				"anotherInt":    capturedValue("int", "3"),
+				"anotherString": capturedValue("string", "four"),
+			}},
+		}},
+	}}},
+}
+
+// TODO: this doesn't work yet:
+// could not determine locations of variables from debug information could not inspect param "x" on function: no location field in parameter entry
+var genericCaptures = fixtures{
+	"main.TypeWithGenerics[go.shape.string].Guess": {"value": capturedValue("string", "generics work")},
+}
+
+// TODO: check how map entries should be represented, likely that entries have key / value pair fields
+// instead of having the keys hardcoded as string field names
+// maps are no supported at the moment so this fails anyway
+var mapCaptures = fixtures{
+	"main.test_map_string_to_int": {"m": {Type: "map", Fields: fieldMap{
+		"foo": capturedValue("int", "1"),
+		"bar": capturedValue("int", "2"),
+	}}},
+	"main.test_map_string_to_struct": {"m": {Type: "map", Fields: fieldMap{
+		"foo": {Type: "struct", Fields: fieldMap{
+			"anotherInt":    capturedValue("int", "3"),
+			"anotherString": capturedValue("string", "four"),
+		}},
+		"bar": {Type: "struct", Fields: fieldMap{
+			"anotherInt":    capturedValue("int", "3"),
+			"anotherString": capturedValue("string", "four"),
+		}},
+	}}},
+}
+
+// mergeMaps combines multiple fixture maps into a single map
+func mergeMaps(maps ...fixtures) fixtures {
+	result := make(fixtures)
+	for _, m := range maps {
+		for k, v := range m {
+			result[k] = v
+		}
+	}
+	return result
+}
+
+var expectedCaptures = mergeMaps(
+	basicCaptures,
+	stringCaptures,
+	arrayCaptures,
+	structCaptures,
+	mapCaptures,
+	genericCaptures,
+	multiParamCaptures,
+)
