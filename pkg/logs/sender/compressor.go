f7293332b13c6be4495b3aed7e1b23f2cf6ca70f
Stephen Wakely <fungus.humungus@gmail.com>
2024-09-20 15:47:02 -0400

    Remove unnecessary metrics.
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- pkg/logs/sender/compressor.go
+++ pkg/logs/sender/compressor.go
@@ -1,22 +1,13 @@
 package sender
 
 import (
-	"github.com/DataDog/datadog-agent/pkg/telemetry"
 	"github.com/DataDog/datadog-agent/comp/serializer/compression"
 )
 
+// TODO: This may not be needed.
 type Compressor struct {
 	compression compression.Component
 }
-
-var (
-	tlmTotalPayloads = telemetry.NewCounter("logscompressor", "total_payloads",
-		nil, "Total payloads in the compressor serializer")
-	tlmBytesIn = telemetry.NewCounter("logscompressor", "bytes_in",
-		nil, "Count of bytes entering the compressor serializer")
-	tlmBytesOut = telemetry.NewCounter("logscompressor", "bytes_out",
-		nil, "Count of bytes out the compressor serializer")
-)
 
 func NewCompressor(compression compression.Component) *Compressor {
 	return &Compressor{
@@ -29,18 +20,5 @@ func (c *Compressor) name() string {
 }
 
 func (c *Compressor) encode(payload []byte) ([]byte, error) {
-	uncompressedSize := len(payload)
-
-	payload, error := c.compression.Compress(payload)
-	if error != nil {
-		return nil, error
-	}
-
-	compressedSize := len(payload)
-
-	tlmTotalPayloads.Add(1)
-	tlmBytesIn.Add(float64(uncompressedSize))
-	tlmBytesOut.Add(float64(compressedSize))
-
-	return payload, nil
+	return c.compression.Compress(payload)
 }
