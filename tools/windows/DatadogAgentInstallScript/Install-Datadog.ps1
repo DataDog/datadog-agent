d562397a1fe985261f94508eb9ca587244af8792
Julien Lebot <julien.lebot@datadoghq.com>
2024-09-23 17:16:22 +0200

    Support writing to config file from Install-Script

--- tools/windows/DatadogAgentInstallScript/Install-Datadog.ps1
+++ tools/windows/DatadogAgentInstallScript/Install-Datadog.ps1
@@ -4,16 +4,39 @@
 #>
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 param(
-    # The URL to download the installer from
+    # The URL where to download the installer
     [Parameter(Mandatory = $false)]
     [string]
-    $ddInstallerUrl = $env:DD_INSTALLER_URL
+    $ddInstallerUrl = $env:DD_INSTALLER_URL,
+
+    # Whether or not to enable remote updates
+    [Parameter(Mandatory = $false)]
+    [string]
+    $ddRemoteUpdates = $env:DD_REMOTE_UPDATES
 )
 
+function Update-ConfigFile($regex, $replacement) {
+   $configFile = Join-Path (Get-ItemPropertyValue -Path "HKLM:\\SOFTWARE\\Datadog\\Datadog Agent" -Name "ConfigRoot") "datadog.yaml"
+   if (-Not $configFile) {
+      $configFile = "C:\\ProgramData\\datadog.yaml"
+   }
+   if  (-Not Test-Path $configFile) {
+      Write-Warning "datadog.yaml doesn't exist"
+      return
+   }
+   (Get-Content $configFile) -replace $regex, $replacement Out-File configFile
+}
+
 Write-Host "Welcome to the Datadog Install Script"
+
+# Set some defaults if not provided
 if (-Not $ddInstallerUrl) {
    # Replace with https://s3.amazonaws.com/ddagent-windows-stable/datadog-installer-x86_64.exe when ready
    $ddInstallerUrl = "https://s3.amazonaws.com/dd-agent-omnibus/datadog-installer-x86_64.exe"
+}
+
+if (-Not $ddRemoteUpdates) {
+   $ddRemoteUpdates = "false"
 }
 
 $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -49,8 +72,32 @@ Write-Host ("Downloading installer from {0}" -f $ddInstallerUrl)
 
 # If not set the `default-packages` won't contain the Datadog Agent
 $env:DD_INSTALLER_DEFAULT_PKG_INSTALL_DATADOG_AGENT="True"
+
 Write-Host "Starting bootstrap process"
 & $installer bootstrap
 Write-Host "Bootstrap execution done"
+
+if ($env:DD_API_KEY) {
+   Write-Host "Writing DD_API_KEY"
+   Update-ConfigFile("^[ #]*api_key:.*",  ("api_key: {0}" -f $env:DD_API_KEY))
+}
+
+if ($env:DD_SITE) {
+   Write-Host "Writing DD_SITE"
+   Update-ConfigFile("^[ #]*site:.*",  ("site: {0}" -f $env:DD_SITE))
+}
+
+if ($env:DD_URL) {
+   Write-Host "Writing DD_URL"
+   Update-ConfigFile("^[ #]*dd_url:.*",  ("dd_url: {0}" -f $env:DD_URL))
+}
+
+if ($ddRemoteUpdates) {
+   Write-Host "Writing DD_REMOTE_UPDATES"
+   Update-ConfigFile("^[ #]*remote_updates:.*",  ("remote_updates: {0}" -f $ddRemoteUpdates.ToLower()))
+}
+
+Write-Host "Cleaning up..."
 Remove-Item -Force $installer
-Write-Host "Datadog Install Script finished"
+
+Write-Host "Datadog Install Script finished!"
