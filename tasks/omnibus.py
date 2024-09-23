de23014dda3714c0f5887eea9b9b4b347a6dcd4d
Pythyu <antoine.zellmeyer@datadoghq.com>
2024-09-23 13:21:06 +0200

    format(ruff): omnibus.py

--- tasks/omnibus.py
+++ tasks/omnibus.py
@@ -393,7 +393,7 @@ def rpath_edit(ctx, install_path, target_rpath_dd_folder, platform="linux"):
     # Since bash and sh don't support **/* we are using glob
     # to get all datadog files inside of one `file --mime-type` command
     datadog_files = " ".join(glob.glob(f"{install_path}/**/*", recursive=True))
-   # with open("/tmp/ddargs", "w") as f:
+    # with open("/tmp/ddargs", "w") as f:
     #    f.write(" ".join(datadog_files))
     files = ctx.run(f"echo \"{datadog_files}\" | xargs file --mime-type", hide=True).stdout
 
