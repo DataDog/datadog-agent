eb64087956cd7231e16ec24bf49483d39021c074
Branden Clark <branden.clark@datadoghq.com>
2024-09-24 14:50:26 -0400

    Revert "add optional version param"
    This reverts commit 14ad18d8f0241b408525c8a9229c2c0244c88576.

--- tasks/winbuildscripts/Generate-OCIPackage.ps1
+++ tasks/winbuildscripts/Generate-OCIPackage.ps1
@@ -2,23 +2,19 @@ Param(
     [Parameter(Mandatory=$true,Position=0)]
     [ValidateSet("datadog-agent", "datadog-installer")]
     [String]
-    $package,
-    [Parameter(Mandatory=$false)]
-    [String]
-    $Version
+    $package
 )
 
-if (-not $Version) {
-    $Version = "{0}-1" -f (inv agent.version --url-safe --major-version 7)
-}
 $omnibusOutput = "$($Env:REPO_ROOT)\omnibus\pkg\"
 
 if (-not (Test-Path C:\tools\datadog-package.exe)) {
     Write-Host "Downloading datadog-package.exe"
     (New-Object System.Net.WebClient).DownloadFile("https://dd-agent-omnibus.s3.amazonaws.com/datadog-package.exe", "C:\\tools\\datadog-package.exe")
 }
+$rawAgentVersion = "{0}-1" -f (inv agent.version --url-safe --major-version 7)
+Write-Host "Detected agent version ${rawAgentVersion}"
 
-$packageName = "${package}-${Version}-windows-amd64.oci.tar"
+$packageName = "${package}-${rawAgentVersion}-windows-amd64.oci.tar"
 
 if (Test-Path $omnibusOutput\$packageName) {
     Remove-Item $omnibusOutput\$packageName
@@ -27,9 +23,9 @@ if (Test-Path $omnibusOutput\$packageName) {
 # datadog-package takes a folder as input and will package everything in that, so copy the msi to its own folder
 Remove-Item -Recurse -Force C:\oci-pkg -ErrorAction SilentlyContinue
 New-Item -ItemType Directory C:\oci-pkg
-Copy-Item (Get-ChildItem $omnibusOutput\${package}-${Version}-x86_64.msi).FullName -Destination C:\oci-pkg\${package}-${Version}-x86_64.msi
+Copy-Item (Get-ChildItem $omnibusOutput\${package}-${rawAgentVersion}-x86_64.msi).FullName -Destination C:\oci-pkg\${package}-${rawAgentVersion}-x86_64.msi
 
 # The argument --archive-path ".\omnibus\pkg\datadog-agent-${version}.tar.gz" is currently broken and has no effects
-& C:\tools\datadog-package.exe create --package $package --os windows --arch amd64 --archive --version $Version C:\oci-pkg
+& C:\tools\datadog-package.exe create --package $package --os windows --arch amd64 --archive --version $rawAgentVersion C:\oci-pkg
 
-Move-Item ${package}-${Version}-windows-amd64.tar $omnibusOutput\$packageName
+Move-Item ${package}-${rawAgentVersion}-windows-amd64.tar $omnibusOutput\$packageName
