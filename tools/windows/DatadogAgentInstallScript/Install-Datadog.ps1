9fd4ec75ab965168e42079cf3fa476299dcfde9c
Julien Lebot <julien.lebot@datadoghq.com>
2024-09-24 15:50:57 +0200

    Update install script

--- tools/windows/DatadogAgentInstallScript/Install-Datadog.ps1
+++ tools/windows/DatadogAgentInstallScript/Install-Datadog.ps1
@@ -20,17 +20,17 @@ param(
    $ddAgentMinorVersion = $env:DD_AGENT_MINOR_VERSION
 )
 
-$SCRIPT_VERSION="1.0.0"
-$SUCCESS=0
-$GENERAL_ERROR_CODE=1
+$SCRIPT_VERSION = "1.0.0"
+$SUCCESS = 0
+$GENERAL_ERROR_CODE = 1
 
 # ExitCodeException can be used to report failures from executables that set $LASTEXITCODE
 class ExitCodeException : Exception {
-    [string] $LastExitCode
+   [string] $LastExitCode
 
-    ExitCodeException($message, $lastExitCode) : base($message) {
-        $this.LastExitCode = $lastExitCode
-    }
+   ExitCodeException($message, $lastExitCode) : base($message) {
+      $this.LastExitCode = $lastExitCode
+   }
 }
 
 function Update-ConfigFile($regex, $replacement) {
@@ -38,61 +38,100 @@ function Update-ConfigFile($regex, $replacement) {
    if (-Not $configFile) {
       $configFile = "C:\\ProgramData\\datadog.yaml"
    }
-   if  (-Not (Test-Path $configFile)) {
-      Write-Warning "datadog.yaml doesn't exist"
-      return $GENERAL_ERROR_CODE
+   if (-Not (Test-Path $configFile)) {
+      throw "datadog.yaml doesn't exist"
    }
    (Get-Content $configFile) -replace $regex, $replacement | Out-File configFile
-   return $SUCCESS
 }
 
-function Report-Telemetry($payload) {
+function Send-Telemetry($payload) {
    $telemetryUrl = "https://instrumentation-telemetry-intake.datadoghq.com/api/v2/apmtelemetry"
    if ($env:DD_SITE -eq "ddog-gov.com" -or -Not ($env:DD_API_KEY)) {
-         return
+      return
    }
 
    if ($env:DD_SITE) {
-      $telemetryUrl = ("https://instrumentation-telemetry-intake.${0}/api/v2/apmtelemetry" -f $env:DD_SITE)
+      $telemetryUrl = "https://instrumentation-telemetry-intake.${env:DD_SITE}/api/v2/apmtelemetry"
    }
-
-   Invoke-WebRequest -Uri $telemetryUrl -Method POST -Body $payload -ContentType "application/json" -Headers @{"DD-Api-Key"=$env:DD_API_KEY}
+   $requestHeaders = @{
+      "DD-Api-Key"   = $env:DD_API_KEY
+      "Content-Type" = "application/json"
+   }
+   $result = Invoke-WebRequest -Uri $telemetryUrl -Method POST -Body $payload -Headers $requestHeaders
+   Write-Host "Sending telemetry: $($result.StatusCode)"
 }
 
-function Report-Error($errorMessage, $errorCode) {
-    Write-Warning (@'
+function Show-Error($errorMessage, $errorCode) {
+   # Report as warning for prettier output and not having to deal with script termination
+   Write-Warning @"
     Datadog Install script failed:
 
-      {0}
-      {1}
+    Error message: $($errorMessage)
+    Error code: $($errorCode)
 
-'@ -f $errorMessage, $errorCode)
+"@
 
-    $agentVersion = "7.x"
-    if ($ddAgentMinorVersion) {
-        $agentVersion = ("7.{0}" -f $ddAgentMinorVersion)
-    }
-    Report-Telemetry (@'
+   $agentVersion = "7.x"
+   if ($ddAgentMinorVersion) {
+      $agentVersion = "7.${ddAgentMinorVersion}"
+   }
+   $errorMessage = ($errorMessage -replace '"', '_' -replace '\n', ' ' -replace '\r', ' ')
+
+   Send-Telemetry @"
 {
-   \"request_type\": \"apm-onboarding-event\",
-   \"api_version\": \"v1\",
-   \"payload\": {
-       \"event_name\": \"agent.installation.error\",
-       \"tags\": {
-           \"install_id\": \"{0}\",
-           \"install_type\": \"windows_powershell\",
-           \"install_time\": \"{1}\"
-           \"agent_platform\": \"windows\",
-           \"agent_version\": \"{2}\",
-           \"script_version\": \"{3}\"
+   "request_type": "apm-onboarding-event",
+   "api_version": "v1",
+   "payload": {
+       "event_name": "agent.installation.error",
+       "tags": {
+           "install_id": "$(New-Guid)",
+           "install_type": "windows_powershell",
+           "install_time": "$([DateTimeOffset]::Now.ToUnixTimeSeconds())"
+           "agent_platform": "windows",
+           "agent_version: "$($agentVersion)",
+           "script_version": "$($SCRIPT_VERSION)"
        },
-       \"error\": {
-          \"code\": {4},
-          \"message\": \"{5}\"
+       "error": {
+          "code": "$($errorCode)",
+          "message": "$($errorMessage)"
        }
    }
 }
-'@ -f (New-Guid).ToString(), [DateTimeOffset]::Now.ToUnixTimeSeconds(), $agentVersion, $SCRIPT_VERSION, $errorCode, ($errorMessage -replace '"', '_' -replace '\n', ' ' -replace '\r', ' '))
+"@
+}
+
+function Start-ProcessWithOutput {
+   param ([string]$Path, [string[]]$ArgumentList)
+   $psi = New-object System.Diagnostics.ProcessStartInfo 
+   $psi.CreateNoWindow = $true 
+   $psi.UseShellExecute = $false 
+   $psi.RedirectStandardOutput = $true 
+   $psi.RedirectStandardError = $true 
+   $psi.FileName = $Path
+   if ($ArgumentList.Count -gt 0) {
+      $psi.Arguments = $ArgumentList
+   }
+   $process = New-Object System.Diagnostics.Process 
+   $process.StartInfo = $psi
+   $stdout = Register-ObjectEvent -InputObject $process -EventName 'OutputDataReceived'`
+      -Action {
+      if (![String]::IsNullOrEmpty($EventArgs.Data)) {
+         Write-Host $EventArgs.Data
+      }
+   }
+   $stderr = Register-ObjectEvent -InputObject $process -EventName 'ErrorDataReceived' `
+      -Action {
+      if (![String]::IsNullOrEmpty($EventArgs.Data)) {
+         Write-Warning $EventArgs.Data
+      }
+   }
+   [void]$process.Start()
+   $process.BeginOutputReadLine()
+   $process.BeginErrorReadLine()
+   $process.WaitForExit()
+   Unregister-Event -SourceIdentifier $stdout.Name
+   Unregister-Event -SourceIdentifier $stderr.Name
+   return $process.ExitCode
 }
 
 Write-Host "Welcome to the Datadog Install Script"
@@ -110,14 +149,11 @@ if (-Not $ddRemoteUpdates) {
 $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $myWindowsPrincipal = new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
 $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-if ($myWindowsPrincipal.IsInRole($adminRole))
-{
+if ($myWindowsPrincipal.IsInRole($adminRole)) {
    # We are running "as Administrator"
    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-   clear-host
 }
-else
-{
+else {
    # We are not running "as Administrator
    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
@@ -127,7 +163,7 @@ else
 }
 
 try {
-   # Powershell does not enabled TLS 1.2 by default, & we want it enabled for faster downloads
+   # Powershell does not enable TLS 1.2 by default, & we want it enabled for faster downloads
    Write-Host "Forcing web requests to TLS v1.2"
    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
@@ -140,65 +176,73 @@ try {
    [System.Net.WebClient]::new().DownloadFile($ddInstallerUrl, $installer)
 
    # If not set the `default-packages` won't contain the Datadog Agent
-   $env:DD_INSTALLER_DEFAULT_PKG_INSTALL_DATADOG_AGENT="True"
+   $env:DD_INSTALLER_DEFAULT_PKG_INSTALL_DATADOG_AGENT = "True"
 
    Write-Host "Starting bootstrap process"
-   & $installer bootstrap
-   if ($LASTEXITCODE -ne 0)  {
-      throw [ExitCodeException]::new("Bootstrap failed", $LASTEXITCODE)
+   $result = Start-ProcessWithOutput -Path $installer -ArgumentList "bootstrap"
+   if ($result -ne 0) {
+      # bootstrap only fails if it fails to install to install the Datadog Installer, so it's possible the Agent was not  installed
+      throw [ExitCodeException]::new("Bootstrap failed", $result)
    }
    Write-Host "Bootstrap execution done"
 
+   if (-Not (Test-Path "HKLM:\\SOFTWARE\\Datadog\\Datadog Agent")) {
+      throw "Agent is not installed"
+   }
+
    if ($env:DD_API_KEY) {
       Write-Host "Writing DD_API_KEY"
-      if (Update-ConfigFile("^[ #]*api_key:.*",  ("api_key: {0}" -f $env:DD_API_KEY)) -ne $SUCCESS) {
+      if (Update-ConfigFile "^[ #]*api_key:.*" ("api_key: {0}" -f $env:DD_API_KEY) -ne $SUCCESS) {
          throw "Writing DD_API_KEY failed"
       }
    }
 
    if ($env:DD_SITE) {
       Write-Host "Writing DD_SITE"
-      if (Update-ConfigFile("^[ #]*site:.*",  ("site: {0}" -f $env:DD_SITE)) -ne $SUCCESS) {
+      if (Update-ConfigFile "^[ #]*site:.*" ("site: {0}" -f $env:DD_SITE) -ne $SUCCESS) {
          throw "Writing DD_SITE failed"
       }
    }
 
    if ($env:DD_URL) {
       Write-Host "Writing DD_URL"
-      if (Update-ConfigFile("^[ #]*dd_url:.*",  ("dd_url: {0}" -f $env:DD_URL)) -ne $SUCCESS) {
+      if (Update-ConfigFile "^[ #]*dd_url:.*" ("dd_url: {0}" -f $env:DD_URL) -ne $SUCCESS) {
          throw "Writing DD_URL failed"
       }
    }
 
    if ($ddRemoteUpdates) {
       Write-Host "Writing DD_REMOTE_UPDATES"
-      if (Update-ConfigFile("^[ #]*remote_updates:.*",  ("remote_updates: {0}" -f $ddRemoteUpdates.ToLower())) -ne $SUCCESS) {
+      if (Update-ConfigFile "^[ #]*remote_updates:.*" ("remote_updates: {0}" -f $ddRemoteUpdates.ToLower()) -ne $SUCCESS) {
          throw "Writing DD_REMOTE_UPDATES failed"
       }
    }
-   Report-Telemetry (@'
+   Send-Telemetry @"
 {
-   \"request_type\": \"apm-onboarding-event\",
-   \"api_version\": \"v1\",
-   \"payload\": {
-      \"event_name\": \"agent.installation.success\",
-      \"tags\": {
-         \"install_id\": \"{0}\",
-         \"install_type\": \"windows_powershell\",
-         \"install_time\": \"{1}\"
-         \"agent_platform\": \"windows\",
-         \"agent_version\": \"{2}\",
-         \"script_version\": \"{3}\"
-      }
+   "request_type": "apm-onboarding-event",
+   "api_version": "v1",
+   "payload": {
+       "event_name": "agent.installation.success",
+       "tags": {
+           "install_id": "$(New-Guid)",
+           "install_type": "windows_powershell",
+           "install_time": "$([DateTimeOffset]::Now.ToUnixTimeSeconds())"
+           "agent_platform": "windows",
+           "agent_version: "$($agentVersion)",
+           "script_version": "$($SCRIPT_VERSION)"
+       }
    }
 }
-'@ -f (New-Guid).ToString(), [DateTimeOffset]::Now.ToUnixTimeSeconds(), $agentVersion, $SCRIPT_VERSION)
+"@
 
-} catch [ExitCodeException] {
-   Report-Error($_.Exception.Message, $_.Exception.LastExitCode)
-} catch {
-   Report-Error($_.Exception.Message, $GENERAL_ERROR_CODE)
-} finally {
+}
+catch [ExitCodeException] {
+   Show-Error $_.Exception.Message $_.Exception.LastExitCode
+}
+catch {
+   Show-Error $_.Exception.Message $GENERAL_ERROR_CODE
+}
+finally {
    Write-Host "Cleaning up..."
    Remove-Item -Force $installer
 }
