caeed0c4297bfdae57270bab7b1f5b21195dd0c3
Célian Raimbault <161456554+CelianR@users.noreply.github.com>
2024-09-27 10:49:43 -0400

    Update tasks/jira.py
    Co-authored-by: Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>

--- tasks/jira.py
+++ tasks/jira.py
@@ -72,7 +72,7 @@ def get_jira():
     return jira
 
 
-def close_failing_test_issue(jira, issue_key: str, verbose_test: str):
+def close_jira_issue(jira, issue_key: str, verbose_test: str):
     print('Marking as done issue', issue_key, 'for test', verbose_test)
 
     jira.issue_add_comment(issue_key, 'Marking this issue as done since the test is not failing anymore')
