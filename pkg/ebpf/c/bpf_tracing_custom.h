c0eb0864f24dac1780b55cc07b5c2f5080888625
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-09-27 06:41:20 -0700

    Fix ARM eBPF build

--- pkg/ebpf/c/bpf_tracing_custom.h
+++ pkg/ebpf/c/bpf_tracing_custom.h
@@ -11,11 +11,11 @@
         p;                                                                   \
     })
 
-#define PT_REGS_USER_STACK_PARM(x, n)                                             \
-    ({                                                                       \
-        __u64 p = 0;                                                         \
+#define PT_REGS_USER_STACK_PARM(x, n)                                      \
+    ({                                                                     \
+        __u64 p = 0;                                                       \
         bpf_probe_read_user(&p, sizeof(p), ((__u64 *)x->__PT_SP_REG) + n); \
-        p;                                                                   \
+        p;                                                                 \
     })
 
 #define PT_REGS_PARM7(x) PT_REGS_STACK_PARM(x, 1)
@@ -38,11 +38,11 @@
         p;                                                                  \
     })
 
-#define PT_REGS_USER_STACK_PARM(x, n)                                            \
-    ({                                                                      \
-        unsigned long p = 0;                                                \
+#define PT_REGS_USER_STACK_PARM(x, n)                                     \
+    ({                                                                    \
+        unsigned long p = 0;                                              \
         bpf_probe_read_USER(&p, sizeof(p), ((unsigned long *)x->sp) + n); \
-        p;                                                                  \
+        p;                                                                \
     })
 
 #define PT_REGS_PARM7(x) (__PT_REGS_CAST(x)->regs[6])
@@ -51,6 +51,9 @@
 #define PT_REGS_PARM10(x) PT_REGS_STACK_PARM(__PT_REGS_CAST(x), 1)
 #define PT_REGS_PARM7_CORE(x) BPF_CORE_READ(__PT_REGS_CAST(x), regs[6])
 #define PT_REGS_PARM8_CORE(x) BPF_CORE_READ(__PT_REGS_CAST(x), regs[7])
+
+#define PT_REGS_USER_PARM7(x) PT_REGS_PARM7(x)
+#define PT_REGS_USER_PARM8(x) PT_REGS_PARM8(x)
 
 #define PT_REGS_USER_PARM9(x) PT_REGS_STACK_PARM(__PT_REGS_CAST(x), 0)
 #define PT_REGS_USER_PARM10(x) PT_REGS_STACK_PARM(__PT_REGS_CAST(x), 1)
