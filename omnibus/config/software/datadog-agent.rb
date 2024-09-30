be943b313cff134a9caa65298ade26d2aa1291cd
Alex Lopez <alex.lopez@datadoghq.com>
2024-09-30 09:30:59 +0200

    Add jemalloc as a dependency to the datadog-agent

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -16,6 +16,9 @@ dependency "python3" if with_python_runtime? "3"
 
 dependency "openscap" if linux_target? and !arm7l_target? and !heroku_target? # Security-agent dependency, not needed for Heroku
 
+# Alternative memory allocator
+dependency "libjemalloc" if linux_target?
+
 dependency 'agent-dependencies'
 dependency 'datadog-agent-dependencies'
 
