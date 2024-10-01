113dd2bfbedfd595ebb30474f2b9388e06456d53
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-01 03:55:43 -0700

    Add sample program testing

--- pkg/gpu/testutil/testdata/cudasample.c
+++ pkg/gpu/testutil/testdata/cudasample.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
 
 typedef struct {
     uint32_t x, y, z;
@@ -21,7 +23,25 @@ cudaError_t cudaFree(void *devPtr) {
     return 0;
 }
 
+cudaError_t cudaStreamSynchronize(cudaStream_t stream) {
+    return 0;
+}
+
 int main(int argc, char **argv) {
-    printf("Hello, World!\n");
+    cudaStream_t stream = 30;
+
+    // Give time for the eBPF program to load
+    sleep(5);
+
+    cudaLaunchKernel((void *)0x1234, (dim3){ 1, 2, 3 }, (dim3){ 4, 5, 6 }, NULL, 10, stream);
+    void *ptr;
+    cudaMalloc(&ptr, 100);
+    cudaFree(ptr);
+    cudaStreamSynchronize(stream);
+
+    while (1) {
+        sleep(10);
+    }
+
     return 0;
 }
