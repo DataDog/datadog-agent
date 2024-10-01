035ef565a40023a44212899b7a42b14f0599e98c
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-01 11:03:08 +0200

    Add sample program

---
+++ pkg/gpu/ebpf/testdata/cudasample.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    uint32_t x, y, z;
+} dim3;
+
+typedef int cudaError_t;
+typedef uint64_t cudaStream_t;
+
+cudaError_t cudaLaunchKernel(const void *func, dim3 gridDim, dim3 blockDim, void **args, size_t sharedMem, cudaStream_t stream) {
+    return 0;
+}
+
+cudaError_t cudaMalloc(void **devPtr, size_t size) {
+    *devPtr = (void *)0xdeadbeef;
+    return 0;
+}
+
+cudaError_t cudaFree(void *devPtr) {
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    printf("Hello, World!\n");
+    return 0;
+}
