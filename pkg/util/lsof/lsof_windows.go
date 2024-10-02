e6073839f17857dbb3b278ad7180ab15ec49cc41
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2024-10-02 10:26:54 +0200

    feat: list agent open files in flares on Windows

--- pkg/util/lsof/lsof_windows.go
+++ pkg/util/lsof/lsof_windows.go
@@ -5,6 +5,102 @@
 
 package lsof
 
-func openFiles(_ int) (Files, error) {
-	return nil, ErrNotImplemented
+import (
+	"fmt"
+	"os"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+func openFiles(pid int) (Files, error) {
+	handle, err := windows.OpenProcess(windows.STANDARD_RIGHTS_ALL|windows.SPECIFIC_RIGHTS_ALL, false, uint32(pid))
+	if err != nil {
+		return nil, err
+	}
+
+	ofl := &openFilesLister{
+		windows.EnumProcessModules,
+		windows.GetModuleFileNameEx,
+		os.Stat,
+	}
+
+	files, err := ofl.getDLLFiles(handle)
+
+	return files, err
+}
+
+type openFilesLister struct {
+	EnumProcessModules  func(windows.Handle, *windows.Handle, uint32, *uint32) error
+	GetModuleFileNameEx func(windows.Handle, windows.Handle, *uint16, uint32) error
+	Stat                func(string) (os.FileInfo, error)
+}
+
+// getDLLFiles returns the list of File for the DLLs opened by the process
+func (ofl *openFilesLister) getDLLFiles(process windows.Handle) (Files, error) {
+	hModules, err := ofl.listOpenDLL(process)
+	if err != nil {
+		return nil, err
+	}
+
+	var files Files
+	for _, hModule := range hModules {
+		file, err := ofl.getDLLFile(process, hModule)
+		if err != nil {
+			continue
+		}
+		files = append(files, file)
+	}
+
+	return files, nil
+}
+
+func (ofl *openFilesLister) getDLLFile(process windows.Handle, hModule windows.Handle) (File, error) {
+	// get DLL file path
+	modPath, err := ofl.getModulePath(process, hModule)
+	if err != nil {
+		return File{}, err
+	}
+
+	// try to get some permissions and file size
+	filePerms := "<unknown>"
+	var fileSize int64 = -1
+	fileInfo, err := ofl.Stat(modPath)
+	if err == nil {
+		fileSize = fileInfo.Size()
+		filePerms = fileInfo.Mode().Perm().String()
+	}
+
+	file := File{
+		Fd:       fmt.Sprintf("%d", hModule),
+		Type:     "DLL",
+		FilePerm: filePerms,
+		Size:     fileSize,
+		Name:     modPath,
+	}
+	return file, nil
+}
+
+// listOpenDLL returns the list of DLLs opened by the process
+func (ofl *openFilesLister) listOpenDLL(process windows.Handle) ([]windows.Handle, error) {
+	var hModules [1024]windows.Handle
+	var cbNeeded uint32
+
+	if err := ofl.EnumProcessModules(process, &hModules[0], uint32(unsafe.Sizeof(hModules)), &cbNeeded); err != nil {
+		return nil, err
+	}
+
+	nModules := int(cbNeeded / uint32(unsafe.Sizeof(hModules[0])))
+	return hModules[:nModules], nil
+}
+
+// getModulePath returns the path to the given module
+func (ofl *openFilesLister) getModulePath(process windows.Handle, module windows.Handle) (string, error) {
+	var modName [windows.MAX_PATH]uint16
+	if err := ofl.GetModuleFileNameEx(process, module, &modName[0], windows.MAX_PATH); err != nil {
+		return "", err
+	}
+
+	modPath := windows.UTF16ToString(modName[:])
+	return modPath, nil
 }
---
+++ pkg/util/lsof/lsof_windows_test.go
@@ -0,0 +1,255 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package lsof
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"testing"
+	"unicode/utf16"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/maps"
+	"golang.org/x/sys/windows"
+)
+
+func TestOpenFiles(t *testing.T) {
+	pid := os.Getpid()
+
+	files, err := openFiles(pid)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, files)
+}
+
+func TestGetDLLFiles(t *testing.T) {
+	var procHandle windows.Handle = 42
+	modules := []windows.Handle{1, 2, 3}
+	paths := map[windows.Handle]string{
+		1: "C:/some/path",
+		2: "C:/some/other/path",
+	}
+	stats := map[string]fs.FileInfo{
+		paths[1]: &mockFileInfo{
+			mode: 0400,
+			size: 10,
+		},
+	}
+
+	ofl := &openFilesLister{
+		EnumProcessModules: func(proc windows.Handle, handles *windows.Handle, _ uint32, size *uint32) error {
+			require.EqualValues(t, procHandle, proc)
+
+			copySliceToBuff(modules, handles)
+			*size = uint32(len(modules)) * uint32(unsafe.Sizeof(procHandle))
+			return nil
+		},
+		GetModuleFileNameEx: func(process, module windows.Handle, buff *uint16, _ uint32) error {
+			require.Equal(t, procHandle, process)
+			require.Contains(t, modules, module)
+
+			path, ok := paths[module]
+			if ok {
+				writeStringToUTF16Buffer(path, buff)
+				return nil
+			}
+
+			return errors.New("some error")
+		},
+		Stat: func(s string) (os.FileInfo, error) {
+			require.Contains(t, maps.Values(paths), s)
+			stat, ok := stats[s]
+			if ok {
+				return stat, nil
+			}
+			return nil, errors.New("some error")
+		},
+	}
+
+	expected := Files{
+		{
+			Fd:       "1",
+			Type:     "DLL",
+			FilePerm: "-r--------",
+			Size:     10,
+			Name:     "C:/some/path",
+		},
+		{
+			Fd:       "2",
+			Type:     "DLL",
+			FilePerm: "<unknown>",
+			Size:     -1,
+			Name:     "C:/some/other/path",
+		},
+	}
+
+	files, err := ofl.getDLLFiles(procHandle)
+	require.NoError(t, err)
+	require.ElementsMatch(t, expected, files)
+}
+
+func TestGetDLLFilesError(t *testing.T) {
+	expectedErr := errors.New("some error")
+	ofl := &openFilesLister{
+		EnumProcessModules: func(_ windows.Handle, _ *windows.Handle, _ uint32, _ *uint32) error {
+			return expectedErr
+		},
+	}
+	_, err := ofl.getDLLFiles(0)
+	require.ErrorIs(t, err, expectedErr)
+}
+
+func TestGetDLLFile(t *testing.T) {
+	t.Run("success with stat", func(t *testing.T) {
+		var expectedProc windows.Handle = 42
+		var expectedModule windows.Handle = 43
+		var expectedSize int64 = 10
+		path := "C:/some/path"
+
+		ofl := &openFilesLister{
+			GetModuleFileNameEx: func(process, module windows.Handle, buff *uint16, _ uint32) error {
+				require.Equal(t, expectedProc, process)
+				require.Equal(t, expectedModule, module)
+				writeStringToUTF16Buffer(path, buff)
+				return nil
+			},
+			Stat: func(s string) (os.FileInfo, error) {
+				require.Equal(t, path, s)
+				return &mockFileInfo{
+					mode: 0400,
+					size: expectedSize,
+				}, nil
+			},
+		}
+		file, err := ofl.getDLLFile(expectedProc, expectedModule)
+		require.NoError(t, err)
+		expected := File{
+			Fd:       fmt.Sprintf("%d", expectedModule),
+			FilePerm: "-r--------",
+			Name:     path,
+			Size:     expectedSize,
+			Type:     "DLL",
+		}
+		require.Equal(t, expected, file)
+	})
+
+	t.Run("success without stat", func(t *testing.T) {
+		someError := errors.New("some error")
+		path := "C:/some/path"
+		ofl := &openFilesLister{
+			GetModuleFileNameEx: func(_, _ windows.Handle, buff *uint16, _ uint32) error {
+				writeStringToUTF16Buffer(path, buff)
+				return nil
+			},
+			Stat: func(s string) (os.FileInfo, error) {
+				require.Equal(t, path, s)
+				return nil, someError
+			},
+		}
+
+		file, err := ofl.getDLLFile(42, 43)
+		require.NoError(t, err)
+		assert.EqualValues(t, -1, file.Size)
+		assert.Equal(t, "<unknown>", file.FilePerm)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		someError := errors.New("some error")
+		ofl := &openFilesLister{
+			GetModuleFileNameEx: func(_, _ windows.Handle, _ *uint16, _ uint32) error {
+				return someError
+			},
+		}
+		_, err := ofl.getDLLFile(42, 43)
+		require.ErrorIs(t, err, someError)
+	})
+}
+
+func TestListOpenDLL(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		var procHandle windows.Handle = 42
+		expected := []windows.Handle{1, 2}
+
+		ofl := &openFilesLister{
+			EnumProcessModules: func(proc windows.Handle, handles *windows.Handle, _ uint32, size *uint32) error {
+				require.EqualValues(t, procHandle, proc)
+
+				copySliceToBuff(expected, handles)
+				*size = uint32(len(expected)) * uint32(unsafe.Sizeof(procHandle))
+				return nil
+			},
+		}
+		handles, err := ofl.listOpenDLL(procHandle)
+		require.NoError(t, err)
+		require.Equal(t, expected, handles)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		someError := errors.New("some error")
+
+		ofl := &openFilesLister{
+			EnumProcessModules: func(_ windows.Handle, _ *windows.Handle, _ uint32, _ *uint32) error {
+				return someError
+			},
+		}
+		_, err := ofl.listOpenDLL(0)
+		require.ErrorIs(t, err, someError)
+	})
+}
+
+func TestGetModulePath(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		var expectedProc windows.Handle = 42
+		var expectedModule windows.Handle = 43
+		name := "C:/some/path"
+
+		ofl := &openFilesLister{
+			GetModuleFileNameEx: func(proc windows.Handle, module windows.Handle, buff *uint16, _ uint32) error {
+				require.Equal(t, expectedProc, proc)
+				require.Equal(t, expectedModule, module)
+				writeStringToUTF16Buffer(name, buff)
+				return nil
+			},
+		}
+
+		path, err := ofl.getModulePath(expectedProc, expectedModule)
+		require.NoError(t, err)
+		require.Equal(t, name, path)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		someError := errors.New("some error")
+		ofl := &openFilesLister{
+			GetModuleFileNameEx: func(_ windows.Handle, _ windows.Handle, _ *uint16, _ uint32) error {
+				return someError
+			},
+		}
+
+		_, err := ofl.getModulePath(0, 0)
+		require.ErrorIs(t, err, someError)
+	})
+}
+
+// this function assumes the buffer is big enough
+func copySliceToBuff[T any](elems []T, buff *T) {
+	if len(elems) == 0 {
+		return
+	}
+
+	for i, v := range elems {
+		*(*T)(unsafe.Pointer(uintptr(unsafe.Pointer(buff)) + uintptr(i)*unsafe.Sizeof(elems[0]))) = v
+	}
+}
+
+// this function assumes the buffer is big enough
+func writeStringToUTF16Buffer(str string, buff *uint16) {
+	nameUTF16Encoded := utf16.Encode([]rune(str))
+	copySliceToBuff(nameUTF16Encoded, buff)
+}
