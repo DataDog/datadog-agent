982604ab3adf34401e3bc4d5017484a03a096232
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-10-02 22:02:53 +0200

    working tests

--- test/new-e2e/tests/agent-platform/upgrade/persistent_integrations_test.go
+++ test/new-e2e/tests/agent-platform/upgrade/persistent_integrations_test.go
@@ -6,6 +6,7 @@
 package upgrade
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"testing"
@@ -25,28 +26,46 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TODO: This is a temporary workaround to test the upgrade of the persistent integrations
+// Since we need the previous agent version to have produced these files
+// We will mock these files for now since this is the first release that will generate them
+// Once we have a release that generates these files we can remove this workaround and stop mocking these files
+
+//go:embed fixtures/diff_python_installed_packages.txt
+var diffPythonInstalledPackages string
+
+//go:embed fixtures/postinst_python_installed_packages.txt
+var postinstPythonInstalledPackages string
+
+//go:embed fixtures/requirements-agent-release.txt
+var requirementsAgentRelease string
+
 type persistentIntegrationsSuite struct {
 	e2e.BaseSuite[environments.Host]
-	srcPipelineID string
-	dstPipelineID string
-	apiKey        string
-	flavor        componentos.Flavor
-	arch          componentos.Architecture
+	pipelineID string
+	apiKey     string
+	flavor     componentos.Flavor
+	arch       componentos.Architecture
+}
+
+var filesToMock = map[string]string{
+	"/opt/datadog-agent/.diff_python_installed_packages.txt":     diffPythonInstalledPackages,
+	"/opt/datadog-agent/.postinst_python_installed_packages.txt": postinstPythonInstalledPackages,
+	"/opt/datadog-agent/requirements-agent-release.txt":          requirementsAgentRelease,
 }
 
 func TestPersistentIntegrationsSuite(t *testing.T) {
-	srcPipelineID := os.Getenv("SRC_AGENT_PIPELINE_ID")
-	dstpipelineID := os.Getenv("DST_AGENT_PIPELINE_ID")
+	pipelineID := os.Getenv("SRC_AGENT_PIPELINE_ID")
 	apiKey := os.Getenv("DD_API_KEY")
 
 	oses := []componentos.Descriptor{
 		componentos.Ubuntu2204,
-		// componentos.Debian12,
-		// componentos.RedHat9,
+		componentos.Debian12,
+		componentos.RedHat9,
 	}
 
 	archs := []componentos.Architecture{
-		// componentos.AMD64Arch,
+		componentos.AMD64Arch,
 		componentos.ARM64Arch,
 	}
 
@@ -60,7 +79,7 @@ func TestPersistentIntegrationsSuite(t *testing.T) {
 				tt.Logf("Testing %s-%s", os, arch)
 
 				e2e.Run(tt,
-					&persistentIntegrationsSuite{srcPipelineID: srcPipelineID, dstPipelineID: dstpipelineID, apiKey: apiKey, flavor: os.Flavor, arch: arch},
+					&persistentIntegrationsSuite{pipelineID: pipelineID, apiKey: apiKey, flavor: os.Flavor, arch: arch},
 					e2e.WithProvisioner(awshost.ProvisionerNoAgentNoFakeIntake(awshost.WithEC2InstanceOptions(ec2.WithOSArch(os, arch)))),
 					e2e.WithStackName(fmt.Sprintf("upgrade-persistent-integrations-test-%s-%s", os.Flavor, arch)),
 				)
@@ -69,7 +88,7 @@ func TestPersistentIntegrationsSuite(t *testing.T) {
 	}
 }
 
-func (v *persistentIntegrationsSuite) TestNVMLIntegrationPersists() {
+func (v *persistentIntegrationsSuite) TestIntegrationPersistsWithFileFlag() {
 	host := v.Env().RemoteHost
 	fileManager := filemanager.NewUnix(host)
 	agentClient, err := client.NewHostAgentClient(v, host.HostOutput, false)
@@ -81,7 +100,7 @@ func (v *persistentIntegrationsSuite) TestNVMLIntegrationPersists() {
 	var stdout string
 
 	// Install the agent
-	install.Unix(v.T(), client, installparams.WithFlavor("datadog-agent"), installparams.WithAPIKey(v.apiKey), installparams.WithPipelineID(v.srcPipelineID), installparams.WithArch(string(v.arch)), installparams.WithInstallPythonThirdPartyDeps(true))
+	install.Unix(v.T(), client, installparams.WithMajorVersion("7"), installparams.WithFlavor("datadog-agent"), installparams.WithAPIKey(v.apiKey), installparams.WithArch(string(v.arch)), installparams.WithInstallPythonThirdPartyDeps(true))
 
 	// Check Agent version
 	agentVersion := v.Env().RemoteHost.MustExecute("sudo runuser -u dd-agent -- datadog-agent version")
@@ -99,12 +118,20 @@ func (v *persistentIntegrationsSuite) TestNVMLIntegrationPersists() {
 	stdout = v.Env().RemoteHost.MustExecute("sudo runuser -u dd-agent -- datadog-agent integration freeze")
 	v.Require().Contains(stdout, "datadog-nvml==1.0.0")
 
+	// Prepare the mocked files
+	for dest, data := range filesToMock {
+		v.Env().RemoteHost.MustExecute(fmt.Sprintf("sudo bash -c \"echo '%s' > %s\"", data, dest))
+	}
+
+	// Add flag file to persist the integrations
+	v.Env().RemoteHost.MustExecute("sudo touch	/opt/datadog-agent/.install_python_third_party_deps")
+
 	// Unset/Reset sticky bit on /tmp to allow the agent to write the error log
 	defer v.Env().RemoteHost.MustExecute("sudo chmod +t /tmp")
 	v.Env().RemoteHost.MustExecute("sudo chmod -t /tmp")
 
 	// Upgrade the agent with the package from the pipeline:
-	install.Unix(v.T(), client, installparams.WithPipelineID(v.dstPipelineID), installparams.WithAPIKey(v.apiKey), installparams.WithArch(string(v.arch)), installparams.WithFlavor("datadog-agent"), installparams.WithInstallPythonThirdPartyDeps(true))
+	install.Unix(v.T(), client, installparams.WithPipelineID(v.pipelineID), installparams.WithAPIKey(v.apiKey), installparams.WithArch(string(v.arch)), installparams.WithFlavor("datadog-agent"), installparams.WithInstallPythonThirdPartyDeps(true))
 
 	// Check New Agent version is different from the old one
 	newAgentVersion := v.Env().RemoteHost.MustExecute("sudo runuser -u dd-agent -- datadog-agent version")
