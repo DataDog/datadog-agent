645903a1811be6914dfb2281e49c7d18afcd05ae
grantseltzer <grantseltzer@gmail.com>
2024-10-02 22:25:51 -0500

    Write test to print output of bininspect
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/diconfig/binary_inspection_test.go
+++ pkg/dynamicinstrumentation/diconfig/binary_inspection_test.go
@@ -6,3 +6,94 @@
 //go:build linux_bpf
 
 package diconfig
+
+import (
+	"debug/elf"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/dynamicinstrumentation/testutil"
+	"github.com/DataDog/datadog-agent/pkg/network/go/bininspect"
+	"github.com/kr/pretty"
+)
+
+func TestBinaryInspection(t *testing.T) {
+
+	testFunctions := []string{
+		"github.com/DataDog/datadog-agent/pkg/dynamicinstrumentation/testutil/sample.test_struct",
+	}
+
+	curDir, err := pwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	binPath, err := testutil.BuildGoBinaryWrapper(curDir, "../testutil/sample/sample_service")
+	if err != nil {
+		t.Error(err)
+	}
+
+	f, err := elf.Open(binPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	result, err := bininspect.InspectWithDWARF(f, testFunctions, nil)
+	if err != nil {
+		t.Error(">", err)
+	}
+
+	for _, funcMetadata := range result.Functions {
+		for paramName, paramMeta := range funcMetadata.Parameters {
+			for _, piece := range paramMeta.Pieces {
+				pretty.Log(paramName, piece)
+			}
+		}
+	}
+}
+
+// pwd returns the current directory of the caller.
+func pwd() (string, error) {
+	_, file, _, ok := runtime.Caller(1)
+	if !ok {
+		return "", fmt.Errorf("unable to get current file build path")
+	}
+
+	buildDir := filepath.Dir(file)
+
+	// build relative path from base of repo
+	buildRoot := rootDir(buildDir)
+	relPath, err := filepath.Rel(buildRoot, buildDir)
+	if err != nil {
+		return "", err
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	curRoot := rootDir(cwd)
+
+	return filepath.Join(curRoot, relPath), nil
+}
+
+// rootDir returns the base repository directory, just before `pkg`.
+// If `pkg` is not found, the dir provided is returned.
+func rootDir(dir string) string {
+	pkgIndex := -1
+	parts := strings.Split(dir, string(filepath.Separator))
+	for i, d := range parts {
+		if d == "pkg" {
+			pkgIndex = i
+			break
+		}
+	}
+	if pkgIndex == -1 {
+		return dir
+	}
+	return strings.Join(parts[:pkgIndex], string(filepath.Separator))
+}
