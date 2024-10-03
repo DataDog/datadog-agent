fe53c23e58efaa5e316621414f0d4a2b3b648b87
andrewqian2001datadog <andrew.qian@datadoghq.com>
2024-10-03 17:01:13 -0400

    add test checking for duplicate tags

--- pkg/logs/tailers/socket/tailer_test.go
+++ pkg/logs/tailers/socket/tailer_test.go
@@ -58,20 +58,20 @@ func TestReadShouldFailWithError(t *testing.T) {
 	tailer.Stop()
 }
 
-func TestDuplicateTags(t *testing.T) {
+func TestAvoidDuplicateTags(t *testing.T) {
 	msgChan := make(chan *message.Message)
 	r, w := net.Pipe()
-	// Create a log source with a sample configuration, if needed
-	logSource := sources.NewLogSource("test-source", &config.LogsConfig{})
+	logsConfig := &config.LogsConfig{
+		Tags: []string{"wack:tag", "test:tag2"},
+	}
+	logSource := sources.NewLogSource("test-source", logsConfig)
 
-	// Define the read function to append tags to the message
 	read := func(tailer *Tailer) ([]byte, string, error) {
 		inBuf := make([]byte, 4096)
 		n, err := tailer.Conn.Read(inBuf)
 		if err != nil {
 			return nil, "", err
 		}
-		// Append tags to the message based on your logic
 		return inBuf[:n], "", nil
 	}
 
@@ -80,25 +80,12 @@ func TestDuplicateTags(t *testing.T) {
 
 	var msg *message.Message
 
-	// should receive and decode one message
 	w.Write([]byte("foo\n"))
 	msg = <-msgChan
-	// Adding tags to the message
-	msg.ParsingExtra.Tags = append(msg.ParsingExtra.Tags, "test_tag:tag1")
-	assert.Equal(t, "foo", string(msg.GetContent()))
-	assert.Contains(t, msg.ParsingExtra.Tags, "test_tag:tag1")
 
-	// should receive and decode two messages
-	w.Write([]byte("bar\nboo\n"))
-	msg = <-msgChan
-	msg.ParsingExtra.Tags = append(msg.ParsingExtra.Tags, "test_tag:tag2")
-	assert.Equal(t, "bar", string(msg.GetContent()))
-	assert.Contains(t, msg.ParsingExtra.Tags, "test_tag:tag2")
-
-	msg = <-msgChan
-	msg.ParsingExtra.Tags = append(msg.ParsingExtra.Tags, "test_tag:tag3")
-	assert.Equal(t, "boo", string(msg.GetContent()))
-	assert.Contains(t, msg.ParsingExtra.Tags, "test_tag:tag3")
+	// Getting tags from Origin adds tags from the log config, we want to ensure that the tags
+	// are not added before that
+	assert.Equal(t, msg.Tags(), []string{})
 
 	tailer.Stop()
 }
