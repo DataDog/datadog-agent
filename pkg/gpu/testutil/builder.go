475caebe609c445bf2f2d54defdadd23d17d9029
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-03 15:12:48 +0000

    Fix linter

--- pkg/gpu/testutil/builder.go
+++ pkg/gpu/testutil/builder.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
+// package testutil contains helpers to build sample C binaries for testing.
 package testutil
 
 import (
