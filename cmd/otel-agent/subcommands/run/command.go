7c5196267427bd4399121f448c33dfd16b701ce8
Yang Song <yang.song@datadoghq.com>
2024-10-04 16:58:16 -0400

    Fix test

--- cmd/otel-agent/subcommands/run/command.go
+++ cmd/otel-agent/subcommands/run/command.go
@@ -100,7 +100,7 @@ func runOTelAgentCommand(ctx context.Context, params *subcommands.GlobalParams, 
 	if err == agentConfig.ErrNoDDExporter {
 		return fxutil.Run(
 			fx.Supply(uris),
-			fx.Supply(optional.NewNoneOption[coreconfig.Component]),
+			fx.Supply(optional.NewNoneOption[coreconfig.Component]()),
 			converterfx.Module(),
 			fx.Provide(func(cp converter.Component) confmap.Converter {
 				return cp
