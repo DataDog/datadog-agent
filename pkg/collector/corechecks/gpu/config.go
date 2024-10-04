f187e4e64b510d99aa09739d2977645bd637bfe8
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-04 12:12:25 +0200

    Linter fix

--- pkg/collector/corechecks/gpu/config.go
+++ pkg/collector/corechecks/gpu/config.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
+// Package gpu defines the agent corecheck for
+// the GPU integration
 package gpu
 
 // CheckName defines the name of the
