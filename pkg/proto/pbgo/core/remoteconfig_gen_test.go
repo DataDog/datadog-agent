1361a00270ca65600fef841af891135624374648
Tim Alexander <tim.alexander@datadoghq.com>
2024-10-04 08:38:31 -0400

    Add support for datadog-operator RC client type

--- pkg/proto/pbgo/core/remoteconfig_gen_test.go
+++ pkg/proto/pbgo/core/remoteconfig_gen_test.go
@@ -240,6 +240,64 @@ func BenchmarkUnmarshalClientGetConfigsResponse(b *testing.B) {
 	}
 }
 
+func TestMarshalUnmarshalClientOperator(t *testing.T) {
+	v := ClientOperator{}
+	bts, err := v.MarshalMsg(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	left, err := v.UnmarshalMsg(bts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(left) > 0 {
+		t.Errorf("%d bytes left over after UnmarshalMsg(): %q", len(left), left)
+	}
+
+	left, err = msgp.Skip(bts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(left) > 0 {
+		t.Errorf("%d bytes left over after Skip(): %q", len(left), left)
+	}
+}
+
+func BenchmarkMarshalMsgClientOperator(b *testing.B) {
+	v := ClientOperator{}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		v.MarshalMsg(nil)
+	}
+}
+
+func BenchmarkAppendMsgClientOperator(b *testing.B) {
+	v := ClientOperator{}
+	bts := make([]byte, 0, v.Msgsize())
+	bts, _ = v.MarshalMsg(bts[0:0])
+	b.SetBytes(int64(len(bts)))
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bts, _ = v.MarshalMsg(bts[0:0])
+	}
+}
+
+func BenchmarkUnmarshalClientOperator(b *testing.B) {
+	v := ClientOperator{}
+	bts, _ := v.MarshalMsg(nil)
+	b.ReportAllocs()
+	b.SetBytes(int64(len(bts)))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := v.UnmarshalMsg(bts)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
 func TestMarshalUnmarshalClientState(t *testing.T) {
 	v := ClientState{}
 	bts, err := v.MarshalMsg(nil)
