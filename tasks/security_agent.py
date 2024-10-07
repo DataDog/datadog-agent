926eb293cc31903bb540546b140b934a17413311
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-10-07 19:54:07 +0200

    [CWS] make sure the seclwin stays in sync by adding a CI check

--- tasks/security_agent.py
+++ tasks/security_agent.py
@@ -779,6 +779,7 @@ def go_generate_check(ctx):
         [cws_go_generate],
         [generate_cws_documentation],
         [gen_mocks],
+        [sync_secl_win_pkg],
     ]
     failing_tasks = []
 
