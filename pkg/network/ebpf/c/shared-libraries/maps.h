168da32108b744fe9f1c8a095336f039e2edc2df
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-09 09:28:41 -0700

    Documentation in eBPF probe

--- pkg/network/ebpf/c/shared-libraries/maps.h
+++ pkg/network/ebpf/c/shared-libraries/maps.h
@@ -6,7 +6,11 @@
 
 BPF_LRU_MAP(open_at_args, __u64, lib_path_t, 1024)
 
-/* This map used for notifying userspace of a shared library being loaded */
+/*
+ * These maps are used for notifying userspace of a shared library being loaded
+ * There is one for each library set, so that userspace isn't overwhelmed with
+ * events for libraries it doesn't care about.
+ */
 BPF_PERF_EVENT_ARRAY_MAP(crypto_shared_libraries, __u32)
 BPF_PERF_EVENT_ARRAY_MAP(gpu_shared_libraries, __u32)
 
--- pkg/network/ebpf/c/shared-libraries/probes.h
+++ pkg/network/ebpf/c/shared-libraries/probes.h
@@ -86,6 +86,7 @@ static __always_inline void do_sys_open_helper_exit(exit_sys_ctx *args) {
     LOAD_CONSTANT("crypto_libset_enabled", crypto_libset_enabled);
 
     if (crypto_libset_enabled && (match6chars(0, 'l', 'i', 'b', 's', 's', 'l') || match6chars(0, 'c', 'r', 'y', 'p', 't', 'o') || match6chars(0, 'g', 'n', 'u', 't', 'l', 's'))) {
+        // Only get the CPU if we're going to use it.
         u32 cpu = bpf_get_smp_processor_id();
         bpf_perf_event_output((void *)args, &crypto_shared_libraries, cpu, path, sizeof(lib_path_t));
     }
