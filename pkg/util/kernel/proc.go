7a52a4f8c9388aaf4c1c3e0fdc29049f2e4e3a0b
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-10 10:51:46 +0000

    Add tests for ReadProcessMemMaps

--- pkg/util/kernel/proc.go
+++ pkg/util/kernel/proc.go
@@ -10,6 +10,7 @@ package kernel
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -54,54 +55,64 @@ func WithAllProcs(procRoot string, fn func(int) error) error {
 	return nil
 }
 
+// ProcMapEntry represents an entry of the /proc/X/maps file
 type ProcMapEntry struct {
 	Start   uint64
 	End     uint64
 	Mode    string
 	Offset  uint64
-	MajorId uint64
-	MinorId uint64
+	MajorID uint64
+	MinorID uint64
 	Inode   uint64
 	Path    string
 	Deleted bool
 }
 
-type ProcMapEntries struct {
-	Entries []ProcMapEntry
-}
+// ProcMapEntries is a collection of ProcMapEntry objects sorted by start address
+type ProcMapEntries []ProcMapEntry
 
+// Insert will insert the entry in the correct position in the slice based on its start address
 func (p *ProcMapEntries) Insert(entry ProcMapEntry) {
-	for i, other := range p.Entries {
-		if entry.End < other.Start {
-			p.Entries = append(append(p.Entries[:i], entry), p.Entries[i:]...)
+	for i, other := range *p {
+		if entry.Start < other.Start {
+			if i == 0 {
+				*p = append([]ProcMapEntry{entry}, *p...)
+				return
+			}
+
+			*p = append(append((*p)[:i-1], entry), (*p)[i:]...)
 			return
 		}
 	}
 
-	p.Entries = append(p.Entries, entry)
+	*p = append(*p, entry)
 }
 
+// FindEntryForAddress will return the entry that contains the given address
 func (p *ProcMapEntries) FindEntryForAddress(addr uint64) *ProcMapEntry {
 	// Probably could be made more efficient, but for a first implementation it should be fine
-	for i := range p.Entries {
-		if addr >= p.Entries[i].Start && addr < p.Entries[i].End {
-			return &p.Entries[i]
+	for i := range *p {
+		if addr >= (*p)[i].Start && addr < (*p)[i].End {
+			return &(*p)[i]
 		}
 	}
 
 	return nil
 }
 
-func ReadProcessMemMaps(pid int, procRoot string) (ProcMapEntries, error) {
-	mapsPath := filepath.Join(procRoot, strconv.Itoa(pid), "maps")
-	mapsFile, err := os.Open(mapsPath)
-	if err != nil {
-		return ProcMapEntries{}, fmt.Errorf("cannot open %s: %w", mapsPath, err)
-	}
-	defer mapsFile.Close()
+// readProcessMemMapsFromBuffer reads the content of a /proc/PID/maps file from a buffer
+// Format reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/fs/proc/task_mmu.c#n304
+// see show_vma_header_prefix function
+func readProcessMemMapsFromBuffer(buffer io.Reader) (ProcMapEntries, error) {
+	scanner := bufio.NewScanner(buffer)
 
 	var entries ProcMapEntries
-	scanner := bufio.NewScanner(bufio.NewReader(mapsFile))
+	var err error
+
+	// For clarity of the arguments to ParseUint
+	const formatHex, formatDec = 16, 10
+	const width64, width16 = 64, 16
+
 	for scanner.Scan() {
 		var entry ProcMapEntry
 		line := scanner.Text()
@@ -114,38 +125,38 @@ func ReadProcessMemMaps(pid int, procRoot string) (ProcMapEntries, error) {
 		addrFields := strings.Split(fields[0], "-")
 		majminFields := strings.Split(fields[3], ":")
 		if len(addrFields) != 2 || len(majminFields) != 2 {
-			return ProcMapEntries{}, fmt.Errorf("invalid line in %s: %s", mapsPath, line)
+			return nil, fmt.Errorf("invalid line (line=%s)", line)
 		}
 
-		entry.Start, err = strconv.ParseUint(addrFields[0], 16, 64)
+		entry.Start, err = strconv.ParseUint(addrFields[0], formatHex, width64)
 		if err != nil {
-			return ProcMapEntries{}, fmt.Errorf("invalid start address in %s: %s", mapsPath, line)
+			return nil, fmt.Errorf("invalid start address (line=%s)", line)
 		}
 
-		entry.End, err = strconv.ParseUint(addrFields[1], 16, 64)
+		entry.End, err = strconv.ParseUint(addrFields[1], formatHex, width64)
 		if err != nil {
-			return ProcMapEntries{}, fmt.Errorf("invalid end address in %s: %s", mapsPath, line)
+			return nil, fmt.Errorf("invalid end address (line=%s)", line)
 		}
 
 		entry.Mode = fields[1]
-		entry.Offset, err = strconv.ParseUint(fields[2], 16, 64)
+		entry.Offset, err = strconv.ParseUint(fields[2], formatHex, width64)
 		if err != nil {
-			return ProcMapEntries{}, fmt.Errorf("invalid offset in %s: %s", mapsPath, line)
+			return nil, fmt.Errorf("invalid offset (line=%s)", line)
 		}
 
-		entry.MajorId, err = strconv.ParseUint(majminFields[0], 10, 16)
+		entry.MajorID, err = strconv.ParseUint(majminFields[0], formatHex, width16)
 		if err != nil {
-			return ProcMapEntries{}, fmt.Errorf("invalid major id in %s: %s", mapsPath, line)
+			return nil, fmt.Errorf("invalid major id (line=%s)", line)
 		}
 
-		entry.MinorId, err = strconv.ParseUint(majminFields[1], 10, 16)
+		entry.MinorID, err = strconv.ParseUint(majminFields[1], formatHex, width16)
 		if err != nil {
-			return ProcMapEntries{}, fmt.Errorf("invalid minor id in %s: %s", mapsPath, line)
+			return nil, fmt.Errorf("invalid minor id (line=%s)", line)
 		}
 
-		entry.Inode, err = strconv.ParseUint(fields[4], 10, 64)
+		entry.Inode, err = strconv.ParseUint(fields[4], formatDec, width64)
 		if err != nil {
-			return ProcMapEntries{}, fmt.Errorf("invalid inode in %s: %s", mapsPath, line)
+			return nil, fmt.Errorf("invalid inode (line=%s)", line)
 		}
 
 		if len(fields) >= 6 {
@@ -157,6 +168,22 @@ func ReadProcessMemMaps(pid int, procRoot string) (ProcMapEntries, error) {
 		}
 
 		entries.Insert(entry)
+	}
+
+	return entries, nil
+}
+
+func ReadProcessMemMaps(pid int, procRoot string) (ProcMapEntries, error) {
+	mapsPath := filepath.Join(procRoot, strconv.Itoa(pid), "maps")
+	mapsFile, err := os.Open(mapsPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open %s: %w", mapsPath, err)
+	}
+	defer mapsFile.Close()
+
+	entries, err := readProcessMemMapsFromBuffer(bufio.NewReader(mapsFile))
+	if err != nil {
+		return nil, fmt.Errorf("cannot read maps from %s: %w", mapsPath, err)
 	}
 
 	return entries, nil
