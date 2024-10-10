5b55239b87b50cd0e0002835d7d104586eee6a56
Derek Brown <derek.brown@datadoghq.com>
2024-10-09 18:26:53 -0700

    refactor as requested in review

--- test/new-e2e/tests/windows/common/version.go
+++ test/new-e2e/tests/windows/common/version.go
@@ -14,55 +14,68 @@ import (
 )
 
 // GetFileVersion gets the file version information from the versioninfo resource
-// of the specified file.  It returns as a single string
-func GetFileVersion(host *components.RemoteHost, remoteFileName string) (string, error) {
+// of the specified file.  It returns as a slice of strings, each element being part
+// of the version (major, minor, patch, build)
+//
+// // for the remote file, it uses the powershell version information command
+func GetFileVersion(host *components.RemoteHost, remoteFileName string) ([]string, error) {
 	pscommand := fmt.Sprintf(`(Get-Item "%s").VersionInfo.FileVersion`, remoteFileName)
 	remoteversion, err := host.Execute(pscommand)
 	if err != nil {
-		return "", fmt.Errorf("failed to get version: %w", err)
+		return nil, fmt.Errorf("failed to get version: %w", err)
 	}
-	return strings.TrimSpace(remoteversion), nil
+	return strings.Split(strings.TrimSpace(remoteversion), "."), nil
 }
 
-// VerifyVersion takes a filename and an expected version and determines
-// if the file matches the expected version.
+// GetExpectedVersion gets the expected version from the most recent tag
+// returns in the form of slice of strings, each element being a part of the version
+// (major, minor, patch)
 //
 // expectedVersion can be empty; if so, then it uses `git` to determine the
 // expected version from the most recent tag.  It also then assumes that
 // git is in the path
-//
-// for the remote file, it uses the powershell version information command
-func VerifyVersion(host *components.RemoteHost, remoteFileName, expectedVersion string) error {
-
+func GetExpectedVersion(expectedVersion string) ([]string, error) {
 	if expectedVersion == "" {
-
 		// this needs to be executed locally
 		output, err := exec.Command("git", "describe", "--tags", "--candidates=50", "--match", "[0-9]*", "--abbrev=7").Output()
 		if err != nil {
-			return fmt.Errorf("failed to get git version: %w", err)
+			return nil, fmt.Errorf("failed to get git version: %w", err)
 		}
 		// we only want the M.m.p version number, so up to the first `-` if it exists
 		expectedVersion = strings.TrimSpace(string(output))
-		dashindex := strings.Index(expectedVersion, "-")
-		if dashindex > 1 {
-			expectedVersion = expectedVersion[:dashindex]
-		}
 	}
+	dashindex := strings.Index(expectedVersion, "-")
+	if dashindex > 1 {
+		expectedVersion = expectedVersion[:dashindex]
+	}
+
 	expectedparts := strings.Split(expectedVersion, ".")
 	fmt.Printf("Looking for version %v\n", expectedparts)
 	if len(expectedparts) != 3 {
-		return fmt.Errorf("expected version must be in the form M.m.p.b")
+		return nil, fmt.Errorf("expected version must be in the form M.m.p.")
+	}
+	return expectedparts, nil
+}
+
+// VerifyVersion takes a filename and an expected version and determines
+// if the file matches the expected version.
+
+func VerifyVersion(host *components.RemoteHost, remoteFileName, expectedVersion string) error {
+
+	expectedparts, err := GetExpectedVersion(expectedVersion)
+	if err != nil {
+		return fmt.Errorf("failed to get expected version: %w", err)
 	}
 
-	remoteversion, err := GetFileVersion(host, remoteFileName)
+	remoteversionparts, err := GetFileVersion(host, remoteFileName)
 	if err != nil {
 		return fmt.Errorf("failed to get version: %w", err)
 	}
-	remoteversionparts := strings.Split(strings.TrimSpace(remoteversion), ".")
 	fmt.Printf("Found version %v\n", remoteversionparts)
+
 	for idx, v := range expectedparts {
 		if v != remoteversionparts[idx] {
-			return fmt.Errorf("expected version %v, got %v", expectedVersion, remoteversion)
+			return fmt.Errorf("expected version %v, got %v", expectedVersion, remoteversionparts)
 		}
 	}
 	return nil
