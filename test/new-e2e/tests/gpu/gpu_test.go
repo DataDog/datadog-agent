c76e185d9213eb41422e534752a27c00f3edf202
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-11 16:05:22 +0200

    Add simple test based on docker image

--- test/new-e2e/tests/gpu/gpu_test.go
+++ test/new-e2e/tests/gpu/gpu_test.go
@@ -7,9 +7,16 @@ package gpu
 
 import (
 	"flag"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
+
+	"github.com/DataDog/datadog-agent/test/fakeintake/client"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
@@ -22,9 +29,31 @@ type gpuSuite struct {
 	e2e.BaseSuite[environments.Host]
 }
 
+const defaultGpuCheckConfig = `
+init_config:
+  min_collection_interval: 5
+
+instances:
+  - {}
+`
+
+const defaultSysprobeConfig = `
+gpu_monitoring:
+  enabled: true
+`
+
+const vectorAddDockerImg = "nvcr.io/nvidia/k8s/cuda-sample:vectoradd-cuda10.2"
+
 // TestGPUSuite runs tests for the VM interface to ensure its implementation is correct.
 func TestGPUSuite(t *testing.T) {
-	provisioner := awshost.Provisioner(awshost.WithEC2InstanceOptions(ec2.WithInstanceType("g4dn.xlarge")))
+	provisioner := awshost.Provisioner(
+		awshost.WithEC2InstanceOptions(ec2.WithInstanceType("g4dn.xlarge")),
+		awshost.WithAgentOptions(
+			agentparams.WithIntegration("gpu", defaultGpuCheckConfig),
+			agentparams.WithSystemProbeConfig(defaultSysprobeConfig),
+		),
+	)
+
 	suiteParams := []e2e.SuiteOption{e2e.WithProvisioner(provisioner)}
 	if *devMode {
 		suiteParams = append(suiteParams, e2e.WithDevMode())
@@ -33,10 +62,25 @@ func TestGPUSuite(t *testing.T) {
 	e2e.Run(t, &gpuSuite{}, suiteParams...)
 }
 
-func (v *gpuSuite) TestExecute() {
+func (v *gpuSuite) SetupSuite() {
+	v.BaseSuite.SetupSuite()
+
+	v.Env().RemoteHost.MustExecute(fmt.Sprintf("docker pull %s", vectorAddDockerImg))
+}
+
+func (v *gpuSuite) TestVectorAddProgramDetected() {
 	vm := v.Env().RemoteHost
 
-	out, err := vm.Execute("whoami")
+	out, err := vm.Execute(fmt.Sprintf("docker run --rm --gpus all %s", vectorAddDockerImg))
 	v.Require().NoError(err)
 	v.Require().NotEmpty(out)
+
+	v.EventuallyWithT(func(c *assert.CollectT) {
+		metricNames := []string{"gpu.memory", "gpu.utilization", "gpu.max_memory"}
+		for _, metricName := range metricNames {
+			metrics, err := v.Env().FakeIntake.Client().FilterMetrics(metricName, client.WithMetricValueHigherThan(0))
+			assert.NoError(c, err)
+			assert.Greater(c, len(metrics), 0, "no '%s' with value higher than 0 yet", metricName)
+		}
+	}, 5*time.Minute, 10*time.Second)
 }
