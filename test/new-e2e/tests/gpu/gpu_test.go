9a4420b5635b9858114b961a18b78d9697aef686
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-14 16:07:44 +0200

    Fix test

--- test/new-e2e/tests/gpu/gpu_test.go
+++ test/new-e2e/tests/gpu/gpu_test.go
@@ -6,6 +6,7 @@
 package gpu
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"testing"
@@ -13,14 +14,17 @@ import (
 
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
+	"github.com/DataDog/test-infra-definitions/components/os"
 
 	"github.com/DataDog/datadog-agent/test/fakeintake/client"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e/client/agentclient"
 )
 
 var devMode = flag.Bool("devmode", false, "enable dev mode")
@@ -43,15 +47,20 @@ gpu_monitoring:
 `
 
 const vectorAddDockerImg = "nvcr.io/nvidia/k8s/cuda-sample:vectoradd-cuda10.2"
+const gpuEnabledAMI = "ami-0f71e237bb2ba34be" // Ubuntu 22.04 with GPU drivers
 
 // TestGPUSuite runs tests for the VM interface to ensure its implementation is correct.
 func TestGPUSuite(t *testing.T) {
 	provisioner := awshost.Provisioner(
-		awshost.WithEC2InstanceOptions(ec2.WithInstanceType("g4dn.xlarge")),
+		awshost.WithEC2InstanceOptions(
+			ec2.WithInstanceType("g4dn.xlarge"),
+			ec2.WithAMI(gpuEnabledAMI, os.Ubuntu2204, os.AMD64Arch),
+		),
 		awshost.WithAgentOptions(
-			agentparams.WithIntegration("gpu", defaultGpuCheckConfig),
+			agentparams.WithIntegration("gpu.d", defaultGpuCheckConfig),
 			agentparams.WithSystemProbeConfig(defaultSysprobeConfig),
 		),
+		awshost.WithDocker(),
 	)
 
 	suiteParams := []e2e.SuiteOption{e2e.WithProvisioner(provisioner)}
@@ -68,10 +77,40 @@ func (v *gpuSuite) SetupSuite() {
 	v.Env().RemoteHost.MustExecute(fmt.Sprintf("docker pull %s", vectorAddDockerImg))
 }
 
+// TODO: Extract this to common package? service_discovery uses it too
+type checkStatus struct {
+	CheckID           string `json:"CheckID"`
+	CheckName         string `json:"CheckName"`
+	CheckConfigSource string `json:"CheckConfigSource"`
+	ExecutionTimes    []int  `json:"ExecutionTimes"`
+	LastError         string `json:"LastError"`
+}
+
+type runnerStats struct {
+	Checks map[string]checkStatus `json:"Checks"`
+}
+
+type collectorStatus struct {
+	RunnerStats runnerStats `json:"runnerStats"`
+}
+
+func (v *gpuSuite) TestGPUCheckIsEnabled() {
+	statusOutput := v.Env().Agent.Client.Status(agentclient.WithArgs([]string{"collector", "--json"}))
+
+	var status collectorStatus
+	err := json.Unmarshal([]byte(statusOutput.Content), &status)
+	v.Require().NoError(err, "failed to unmarshal agent status")
+	v.Require().Contains(status.RunnerStats.Checks, "gpu")
+
+	gpuCheckStatus := status.RunnerStats.Checks["gpu"]
+	v.Require().Equal(gpuCheckStatus.LastError, "")
+
+}
+
 func (v *gpuSuite) TestVectorAddProgramDetected() {
 	vm := v.Env().RemoteHost
-
 	out, err := vm.Execute(fmt.Sprintf("docker run --rm --gpus all %s", vectorAddDockerImg))
+	require.NoError(v.T(), err)
 	v.Require().NoError(err)
 	v.Require().NotEmpty(out)
 
