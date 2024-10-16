16c93f9def8d74b214027dc6670f530203426ccc
Aaron Rhodes <aaron.rhodes@rapdev.io>
2024-10-16 10:18:25 -0400

    [NDM] Fix for ignored_ip_addresses in autodiscovery

--- pkg/config/autodiscovery/autodiscovery_test.go
+++ pkg/config/autodiscovery/autodiscovery_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
+	snmplistener "github.com/DataDog/datadog-agent/pkg/snmp"
 )
 
 func TestDiscoverComponentsFromConfigForSnmp(t *testing.T) {
@@ -45,4 +46,26 @@ snmp_listener:
 	assert.NoError(t, err)
 	_, configListeners = DiscoverComponentsFromConfig()
 	assert.Empty(t, len(configListeners))
+}
+
+func TestNewListenerConfigForSnmp(t *testing.T) {
+	pkgconfigsetup.Datadog().SetConfigType("yaml")
+
+	err := pkgconfigsetup.Datadog().ReadConfig(strings.NewReader(`
+network_devices:
+  autodiscovery:
+    configs:
+      - network_address: 127.0.0.1/30
+        ignored_ip_addresses:
+          - 127.0.0.2
+`))
+	assert.NoError(t, err)
+
+	snmpConfig, err := snmplistener.NewListenerConfig()
+	assert.NoError(t, err)
+
+	assert.Len(t, snmpConfig.Configs, 1)
+	assert.Equal(t, "127.0.0.1/30", snmpConfig.Configs[0].Network)
+	assert.Equal(t, 1, len(snmpConfig.Configs[0].IgnoredIPAddresses))
+	assert.Contains(t, snmpConfig.Configs[0].IgnoredIPAddresses, "127.0.0.2")
 }
