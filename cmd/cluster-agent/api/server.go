61bdcf0481dafd8e98350ed44e961c0a96374508
adel121 <adel.hajhassan@datadoghq.com>
2024-10-16 19:19:21 +0200

    Cut tagger stream event responses into chunks of 4MB max size each

--- cmd/cluster-agent/api/server.go
+++ cmd/cluster-agent/api/server.go
@@ -123,14 +123,18 @@ func StartServer(ctx context.Context, w workloadmeta.Component, taggerComp tagge
 		return struct{}{}, nil
 	})
 
+	const maxMessageSize = 4 * 1024 * 1024 // 4 MB
+
 	opts := []grpc.ServerOption{
 		grpc.StreamInterceptor(grpc_auth.StreamServerInterceptor(authInterceptor)),
 		grpc.UnaryInterceptor(grpc_auth.UnaryServerInterceptor(authInterceptor)),
+		grpc.MaxSendMsgSize(maxMessageSize),
+		grpc.MaxRecvMsgSize(maxMessageSize),
 	}
 
 	grpcSrv := grpc.NewServer(opts...)
 	pb.RegisterAgentSecureServer(grpcSrv, &serverSecure{
-		taggerServer: taggerserver.NewServer(taggerComp),
+		taggerServer: taggerserver.NewServer(taggerComp, maxMessageSize),
 	})
 
 	timeout := pkgconfigsetup.Datadog().GetDuration("cluster_agent.server.idle_timeout_seconds") * time.Second
