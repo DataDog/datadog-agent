29f09bbac41c4ada95dc5c9df3071dc8be88af21
Stuart Geipel <stuart.geipel@datadoghq.com>
2024-10-17 10:16:09 -0700

    add documentation

--- pkg/network/tracer/tracer_dump_conntrack.go
+++ pkg/network/tracer/tracer_dump_conntrack.go
@@ -19,13 +19,19 @@ import (
 
 // DebugConntrackTable contains conntrack table data used for debugging NAT
 type DebugConntrackTable struct {
+	// Kind indicates if it is a table from conntrack/cached or conntrack/host, and whether
+	// conntrack/cached was using ebpf or not.
 	Kind    string
 	RootNS  uint32
 	Entries map[uint32][]netlink.DebugConntrackEntry
+	// IsTruncated indicates we closed the netlink socket early - some clients have tens of thousands of
+	// connections and if netlink takes too long, we just return partial results.
+	IsTruncated bool
 }
 
 // WriteTo dumps the conntrack table in the style of `conntrack -L`.
 // It sorts the output so that equivalent tables should result in the same text.
+// If the output would result in more than maxEntries connections, it skips outputting the rest.
 func (table *DebugConntrackTable) WriteTo(w io.Writer, maxEntries int) error {
 	_, err := fmt.Fprintf(w, "conntrack dump, kind=%s rootNS=%d\n", table.Kind, table.RootNS)
 	if err != nil {
@@ -48,6 +54,11 @@ func (table *DebugConntrackTable) WriteTo(w io.Writer, maxEntries int) error {
 	_, err = fmt.Fprintf(w, "totalEntries=%d%s", totalEntries, suffix)
 	if err != nil {
 		return err
+	}
+
+	// in this case the table itself is incomplete due to closing the netlink socket part-way
+	if table.IsTruncated {
+		_, err = fmt.Fprintf(w, "results truncated due to netlink response timeout, some entries may be missing")
 	}
 
 	// used to stop writing once we reach maxEntries
