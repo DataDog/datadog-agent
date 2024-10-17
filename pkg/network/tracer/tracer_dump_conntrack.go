0899f04f88e8b51b1cc6b044b3b8608f5743ecda
Stuart Geipel <stuart.geipel@datadoghq.com>
2024-10-17 10:45:56 -0700

    change message, add test

--- pkg/network/tracer/tracer_dump_conntrack.go
+++ pkg/network/tracer/tracer_dump_conntrack.go
@@ -58,7 +58,7 @@ func (table *DebugConntrackTable) WriteTo(w io.Writer, maxEntries int) error {
 
 	// in this case the table itself is incomplete due to closing the netlink socket part-way
 	if table.IsTruncated {
-		_, err = fmt.Fprintf(w, "results truncated due to netlink response timeout, some entries may be missing")
+		_, err = fmt.Fprintln(w, "netlink table truncated due to response timeout, some entries may be missing")
 	}
 
 	// used to stop writing once we reach maxEntries
