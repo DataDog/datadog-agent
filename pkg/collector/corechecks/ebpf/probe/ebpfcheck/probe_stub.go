9d7cc928d956f67bfcf1b053cd32e345b87404be
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-17 16:43:50 +0200

    Introduce system context and probe dependencies

--- pkg/collector/corechecks/ebpf/probe/ebpfcheck/probe_stub.go
+++ pkg/collector/corechecks/ebpf/probe/ebpfcheck/probe_stub.go
@@ -9,9 +9,16 @@
 package ebpfcheck
 
 import (
+	"github.com/DataDog/datadog-agent/comp/core/telemetry"
 	"github.com/DataDog/datadog-agent/pkg/collector/corechecks/ebpf/probe/ebpfcheck/model"
 	"github.com/DataDog/datadog-agent/pkg/ebpf"
 )
+
+// ProbeDependencies holds the dependencies for the probe
+type ProbeDependencies struct {
+	Telemetry telemetry.Component
+	NvmlLib   any // use any as the type to avoid importing NVML
+}
 
 // Probe is not implemented on non-linux systems
 type Probe struct{}
