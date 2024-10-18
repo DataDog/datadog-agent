6b7022872212d29f8f0b72cd6a6834d03bc26b7e
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-10-18 15:10:46 +0200

    CR: add unit tests

---
+++ pkg/collector/corechecks/servicediscovery/usm/ruby_test.go
@@ -0,0 +1,48 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build !windows
+
+package usm
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateNameFromRailsApplicationRb(t *testing.T) {
+	tests := []struct {
+		name     string
+		path     string
+		expected string
+	}{
+		{
+			name:     "name is found",
+			path:     "./testdata/application.rb",
+			expected: "rails_hello",
+		},
+		{
+			name:     "name not found",
+			path:     "./testdata/application_invalid.rb",
+			expected: "",
+		},
+	}
+	full, err := filepath.Abs("testdata/root")
+	require.NoError(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			instance := &railsDetector{ctx: DetectionContext{
+				fs:         NewSubDirFS(full),
+				contextMap: make(DetectorContextMap),
+			}}
+			value, ok := instance.findRailsApplicationName(tt.path)
+			assert.Equal(t, len(tt.expected) > 0, ok)
+			assert.Equal(t, tt.expected, railsUnderscore(value))
+		})
+	}
+}
