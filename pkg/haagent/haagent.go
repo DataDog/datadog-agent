dcce2d663758fc548f2b28365a4b88033904d63a
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-10-21 17:45:24 +0200

    simplify

--- pkg/haagent/haagent.go
+++ pkg/haagent/haagent.go
@@ -8,42 +8,30 @@
 package haagent
 
 import (
-	"github.com/DataDog/datadog-agent/pkg/collector/check"
-	"github.com/DataDog/datadog-agent/pkg/collector/worker"
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
-	"github.com/DataDog/datadog-agent/pkg/util/log"
 	"go.uber.org/atomic"
 )
 
-// TODO: SHOULD BE A COMPONENT WITH STATE
+// TODO: Should be converted into a fx component
 
-var runtimeRole = atomic.NewString("")
+var roleStore = atomic.NewString("")
 
 func IsEnabled() bool {
 	return pkgconfigsetup.Datadog().GetBool("failover.enabled")
 }
 
 func IsPrimary() bool {
-	currentRole := pkgconfigsetup.Datadog().GetString("failover.role")
-	runtRole := runtimeRole.Load()
-	if runtRole != "" {
-		currentRole = runtRole
+	return GetRole() == "primary"
+}
+
+func GetRole() string {
+	role := roleStore.Load()
+	if role != "" {
+		return role
 	}
-	log.Infof("[IsPrimary] currentRole: %v", currentRole)
-	return currentRole == "primary"
+	return pkgconfigsetup.Datadog().GetString("failover.role")
 }
 
 func SetRole(role string) {
-	runtimeRole.Store(role)
-}
-
-func ShouldRunForCheck(check check.Check, checkLogger worker.CheckLogger) bool {
-	// TODO: handle check name generically
-	if IsEnabled() && check.IsHACheck() {
-		isPrimary := IsPrimary()
-		checkLogger.Debug("Check skipped Agent check is not run since agent is not primary")
-		return isPrimary
-	}
-
-	return true
+	roleStore.Store(role)
 }
