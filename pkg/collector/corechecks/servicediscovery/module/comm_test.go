d111f5e52fe79f4a4b29952ff99c0f8f526f0e48
yuri.lipnesh <yuri.lipnesh@datadoghq.com>
2024-10-22 07:31:58 -0400

    [usm] service discovery, comm_test.go +go:build test && linux_bpf to fix linter error.

--- pkg/collector/corechecks/servicediscovery/module/comm_test.go
+++ pkg/collector/corechecks/servicediscovery/module/comm_test.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
-//go:build test && linux
+//go:build test && linux_bpf
 
 package module
 
