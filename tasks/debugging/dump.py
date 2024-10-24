6a5a98f9dd6919bf9d82a167ddcac4608aad1741
Branden Clark <branden.clark@datadoghq.com>
2024-10-24 14:31:39 -0400

    add platform support to package job symbol fetch

--- tasks/debugging/dump.py
+++ tasks/debugging/dump.py
@@ -113,7 +113,7 @@ def debug_job_dump(ctx, job_id):
     if not syms:
         print("No symbols found")
         return
-    syms = find_symbol_files(syms)
+    syms = find_symbol_files_for_platform(ca.target_platform, syms)
     if not syms:
         print("No symbols found")
         return
@@ -156,7 +156,7 @@ def get_job_dump(ctx, job_id, with_symbols=False):
             print("No symbols found")
             return
         print("Symbols:")
-        for symbol_file in find_symbol_files(syms):
+        for symbol_file in find_symbol_files_for_platform(ca.target_platform, syms):
             print('\t', Path(symbol_file).resolve())
 
 
@@ -193,29 +193,38 @@ def get_symbols_for_job_id(ca: CrashAnalyzer, job_id: str) -> Path | None:
     project_id = ca.active_project.name
     # check if we already have the symbols for this job
     artifact = ca.artifact_store.get(project_id, job_id)
+    syms = None
     if artifact and artifact.version:
-        version = artifact.version
-        syms = ca.symbol_store.get(version, ca.target_platform, ca.target_arch)
-    else:
-        # Need to get the symbols from the package build job in the pipeline
-        package_job_id = get_package_job_id(ca.active_project, job_id)
-        if not package_job_id:
-            raise Exception(f"Could not find package job for job {job_id}")
-        package_artifacts = get_or_fetch_artifacts(ca.artifact_store, ca.active_project, package_job_id)
-        for path in find_debug_zip(package_artifacts.get()):
-            debug_zip = Path(path)
-            version = debug_zip.name.removesuffix('.debug.zip')
-            # add a version ref so we can look it up faster next time
-            package_artifacts.version = version
-            if not artifact:
-                artifact = ca.artifact_store.add(project_id, job_id)
-            artifact.version = version
-            # add the symbols to the symbol store
-            syms = ca.symbol_store.get(version, ca.target_platform, ca.target_arch)
-            if not syms:
-                with tempfile.TemporaryDirectory() as tmp_dir:
-                    _windows_extract_agent_symbols(debug_zip, tmp_dir)
-                    syms = ca.symbol_store.add(version, ca.target_platform, ca.target_arch, tmp_dir)
+        syms = ca.symbol_store.get(artifact.version, ca.target_platform, ca.target_arch)
+        if syms:
+            return syms
+    # Need to get the symbols from the package build job in the pipeline
+    package_job_id = get_package_job_id(ca.active_project, job_id, ca.target_platform, ca.target_arch)
+    if not package_job_id:
+        raise Exception(f"Could not find package job for job {job_id}")
+    package_artifacts = get_or_fetch_artifacts(ca.artifact_store, ca.active_project, package_job_id)
+    archives = find_platform_debug_artifacts(ca.target_platform, package_artifacts.get())
+    # TODO: hacky way to get the version from the archive name
+    version = Path(archives[0]).name
+    for s in ['.debug.zip', '.tar.xz', '-amd64', '-x86_64', '-arm64', '-1']:
+        version = version.removesuffix(s)
+    for p in ['datadog-agent-dbg-', 'datadog-agent-']:
+        version = version.removeprefix(p)
+    # add a version ref so we can look it up faster next time
+    package_artifacts.version = version
+    if not artifact:
+        artifact = ca.artifact_store.add(project_id, job_id)
+    artifact.version = version
+    syms = ca.symbol_store.get(version, ca.target_platform, ca.target_arch)
+    if not syms:
+        # add the symbols to the symbol store
+        for path in archives:
+            with tempfile.TemporaryDirectory() as tmp_dir:
+                if ca.target_platform == 'windows':
+                    _windows_extract_agent_symbols(path, tmp_dir)
+                elif ca.target_platform in ['suse', 'linux']:
+                    _linux_extract_agent_symbols(path, tmp_dir)
+                syms = ca.symbol_store.add(version, ca.target_platform, ca.target_arch, tmp_dir)
 
     return syms
 
@@ -243,10 +252,10 @@ def _suse_get_debug_symbols_for_version(version: str, arch: str, output_dir: Pat
     url = f'{base}stable/{major_version}/{arch}/datadog-agent-dbg-{version}-1.{arch}.rpm'
     print(f"Downloading symbols for {version} from {url}")
     with download_to_tempfile(url) as rpm_path:
-        _suse_extract_agent_symbols(rpm_path, output_dir)
+        _suse_extract_agent_symbols_from_rpm(rpm_path, output_dir)
 
 
-def _suse_extract_agent_symbols(rpm_path: Path | str, output_dir: Path | str) -> None:
+def _suse_extract_agent_symbols_from_rpm(rpm_path: Path | str, output_dir: Path | str) -> None:
     assert shutil.which('rpm2cpio'), "rpm2cpio is required to extract symbols from RPMs"
     assert shutil.which('cpio'), "cpio is required to extract symbols from RPMs"
     with tempfile.TemporaryDirectory() as tmp_dir:
@@ -254,6 +263,13 @@ def _suse_extract_agent_symbols(rpm_path: Path | str, output_dir: Path | str) ->
         agent_root = 'opt/datadog-agent/.debug/opt/datadog-agent'
         os.system(f'rpm2cpio {rpm_path} | cpio -idm -D {tmp_dir}')
         shutil.copytree(f"{tmp_dir}/{agent_root}", output_dir, dirs_exist_ok=True)
+
+
+def _linux_extract_agent_symbols(archive_path: Path | str, output_dir: Path | str) -> None:
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        shutil.unpack_archive(archive_path, tmp_dir)
+        agent_root = Path(tmp_dir) / 'opt/datadog-agent/.debug/opt/datadog-agent'
+        shutil.copytree(agent_root, output_dir, dirs_exist_ok=True)
 
 
 def _windows_extract_agent_symbols(zip_path: Path | str, output_dir: Path | str) -> None:
@@ -301,20 +317,39 @@ def find_dmp_files(output_dir: Path | str) -> list[str]:
     return list(glob.glob(f"{output_dir}/**/*.dmp", recursive=True))
 
 
-def find_debug_zip(output_dir: Path | str) -> list[str]:
-    return list(glob.glob(f"{output_dir}/**/*.debug.zip", recursive=True))
+def find_symbol_files_for_platform(platform: str, output_dir: Path | str) -> list[str]:
+    if platform == 'windows':
+        return list(glob.glob(f"{output_dir}/**/*.exe.debug", recursive=True))
+    elif platform in ['suse', 'linux']:
+        return list(glob.glob(f"{output_dir}/**/*.dbg", recursive=True))
+    else:
+        raise NotImplementedError(f"Unsupported platform {platform}")
 
 
-def find_symbol_files(output_dir: Path | str) -> list[str]:
-    return list(glob.glob(f"{output_dir}/**/*.exe.debug", recursive=True))
+def find_platform_debug_artifacts(platform: str, path: Path | str) -> list[str]:
+    if platform == 'windows':
+        return list(glob.glob(f"{path}/**/*.debug.zip", recursive=True))
+    elif platform in ['suse', 'linux']:
+        return list(glob.glob(f"{path}/**/datadog-agent-dbg-*.tar.xz", recursive=True))
+    else:
+        raise NotImplementedError(f"Unsupported platform {platform}")
 
 
-def get_package_job_id(project: Project, job_id: str, package_job_name=None) -> str | None:
+def get_package_job_id(project: Project, job_id: str, platform: str, arch: str) -> str | None:
     """
     Get the package job ID for the pipeline of the given job.
     """
-    if package_job_name is None:
+    if platform == 'windows':
         package_job_name = "windows_msi_and_bosh_zip_x64-a7"
+    elif platform in ['suse', 'linux']:
+        if arch == 'x86_64':
+            package_job_name = "datadog-agent-7-x64"
+        elif arch == 'arm64':
+            package_job_name = "datadog-agent-7-arm64"
+        else:
+            raise NotImplementedError(f"Unsupported arch {arch}")
+    else:
+        raise NotImplementedError(f"Unsupported platform {platform}")
 
     job = project.jobs.get(job_id)
     pipeline_id = str(job.pipeline["id"])
