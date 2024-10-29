1165d41bf0d6ca3df7f8ff74e2a0c1c7faeafedd
Guillermo Julián <guillermo.julian@datadoghq.com>
2024-10-29 15:26:59 +0100

    Fix build tags

--- pkg/eventmonitor/consumers/testutil/testutil.go
+++ pkg/eventmonitor/consumers/testutil/testutil.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
-//go:build (linux || windows) && test
+//go:build linux && test
 
 // Package testutil provides utilities for using the event monitor in tests
 package testutil
