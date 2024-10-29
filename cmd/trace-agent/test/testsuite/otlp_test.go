32e6f2692b8520f0b7d7f42cf7a16d7accd473fc
Florent Clarret <florent.clarret@datadoghq.com>
2024-10-29 08:04:20 +0100

    Revert "Enable peer tags aggregation by default (#30232)" (#30570)

--- cmd/trace-agent/test/testsuite/otlp_test.go
+++ cmd/trace-agent/test/testsuite/otlp_test.go
@@ -313,7 +313,6 @@ otlp_config:
       endpoint: 0.0.0.0:5111
 apm_config:
   env: my-env
-  compute_stats_by_span_kind: false
 `, port)
 		if err := r.RunAgent([]byte(c)); err != nil {
 			t.Fatal(err)
