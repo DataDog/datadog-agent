edd9fdabbd88fe2436b5178e223b4b1822940590
Maxime mouial <maxime.mouial@gmail.com>
2024-10-30 14:12:50 +0100

    Adding Copyright header to all files

--- cmd/secret-backend/backend/error.go
+++ cmd/secret-backend/backend/error.go
@@ -1,3 +1,9 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the BSD 3-Clause License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+// Copyright (c) 2021, RapDev.IO
+
 package backend
 
 import "github.com/rapdev-io/datadog-secret-backend/secret"
