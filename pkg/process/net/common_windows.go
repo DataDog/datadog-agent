3bd69f4f385615b97881595fbec66c664d135509
Stuart Geipel <stuart.geipel@datadoghq.com>
2024-10-31 08:03:19 -0700

    remove constant

--- pkg/process/net/common_windows.go
+++ pkg/process/net/common_windows.go
@@ -39,8 +39,6 @@ const (
 	conntrackCachedURL = "http://localhost:3333/" + string(sysconfig.NetworkTracerModule) + "/debug/conntrack/cached"
 	// conntrackHostURL is not used on Windows, the value is added to avoid a compilation error
 	conntrackHostURL = "http://localhost:3333/" + string(sysconfig.NetworkTracerModule) + "/debug/conntrack/host"
-	// ebpfBtfLoaderURL is not used on Windows, the value is added to avoid a compilation error
-	ebpfBTFLoaderURL = ""
 
 	// SystemProbePipeName is the production named pipe for system probe
 	SystemProbePipeName = `\\.\pipe\dd_system_probe`
