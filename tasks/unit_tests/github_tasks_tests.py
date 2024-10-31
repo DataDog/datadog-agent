90def8f031937fef0c28b24427fd7e96184396ce
pducolin <paola.ducolin@datadoghq.com>
2024-10-31 16:14:22 +0100

    [task] add test for send_pr_merge_event

--- tasks/unit_tests/github_tasks_tests.py
+++ tasks/unit_tests/github_tasks_tests.py
@@ -2,19 +2,22 @@ from __future__ import annotations
 
 import unittest
 from dataclasses import dataclass
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from invoke.context import Context
 
 import tasks
-from tasks.github_tasks import assign_team_label, extract_test_qa_description
+from tasks.github_tasks import Exit, assign_team_label, extract_test_qa_description, pr_merge_dd_event_sender
 
 
 class GithubAPIMock:
-    def __init__(self, pr_labels: list[str], pr_files: list[str]) -> None:
-        self.pr_labels = pr_labels
-        self.pr_files = pr_files
+    from github.PullRequest import PullRequest
+
+    def __init__(self, pr_labels: list[str] = None, pr_files: list[str] = None, pr: PullRequest = None) -> None:
+        self.pr_labels = pr_labels or []
+        self.pr_files = pr_files or []
         self.assigned_labels = []
+        self.pr = pr
 
     def get_pr_files(self, *_args, **_kwargs):
         return self.pr_files
@@ -24,6 +27,9 @@ class GithubAPIMock:
 
     def add_pr_label(self, _pr_id, label_name):
         self.assigned_labels.append(label_name)
+
+    def get_pr(self, _pr_id):
+        return self.pr
 
 
 class TestAssignTeamLabelMock(unittest.TestCase):
@@ -42,7 +48,7 @@ class TestAssignTeamLabelMock(unittest.TestCase):
             patch.object(tasks.github_tasks, '_get_team_labels') as team_labels_mock,
         ):
             gh = GithubAPIMock(
-                pr_labels or [],
+                pr_labels,
                 changed_files,
             )
             gh_mock.return_value = gh
@@ -118,13 +124,13 @@ class TestAssignTeamLabelMock(unittest.TestCase):
 class TestExtractQADescriptionFromPR(unittest.TestCase):
     def test_extract_qa_description(self):
         @dataclass
-        class TestCase:
+        class TestExtractBodyCase:
             name: str
             body: str
             expected: str
 
-        testcases: list[TestCase] = [
-            TestCase(
+        testcases: list[TestExtractBodyCase] = [
+            TestExtractBodyCase(
                 name="Single line",
                 body="""### What does this PR do?
 
@@ -138,7 +144,7 @@ I added one test
 """,
                 expected="I added one test",
             ),
-            TestCase(
+            TestExtractBodyCase(
                 name="Multi line",
                 body="""### What does this PR do?
 
@@ -154,7 +160,7 @@ and one e2e test
                 expected="""I added one unit test
 and one e2e test""",
             ),
-            TestCase(
+            TestExtractBodyCase(
                 name="Empty description",
                 body="""### What does this PR do?
 
@@ -168,7 +174,7 @@ and one e2e test""",
 """,
                 expected="",
             ),
-            TestCase(
+            TestExtractBodyCase(
                 name="Multiline with subheaders",
                 body="""### What does this PR do?
 
@@ -213,3 +219,168 @@ Pay attentions to this""",
         for tc in testcases:
             qa_description = extract_test_qa_description(tc.body)
             self.assertEqual(qa_description, tc.expected, f"Test case: {tc.name}")
+
+
+class TestPRMergeDDEVentSender(unittest.TestCase):
+    def test_send_pr_merge_event(self):
+        @dataclass
+        class TestSendPRMergeEventCase:
+            name: str
+            labels: list[str]
+            merged: bool
+            number: int
+            login: str
+            body: str
+            ref: str
+            repo_full_name: str
+            expected_text: str
+            expected_tags: list[str]
+            dry_run: bool = False
+            expected_error: str = None
+
+        testcases: list[TestSendPRMergeEventCase] = [
+            TestSendPRMergeEventCase(
+                name="PR merged, qa/done label, with description",
+                labels=["qa/done"],
+                merged=True,
+                number=123,
+                login="testuser",
+                body="""
+### Describe how to test/QA your changes
+This is covered by unit tests
+### Possible Drawbacks / Trade-offs""",
+                ref="main",
+                repo_full_name="testorg/testrepo",
+                expected_text="PR #123 merged to main at testorg/testrepo by testuser with QA description [This is covered by unit tests]",
+                expected_tags=[
+                    "repo:testorg/testrepo",
+                    "pr_id:123",
+                    "author:testuser",
+                    "qa_label:qa/done",
+                ],
+                expected_error=None,
+            ),
+            TestSendPRMergeEventCase(
+                name="PR not merged",
+                labels=[],
+                merged=False,
+                number=123,
+                login="testuser",
+                body="",
+                ref="main",
+                repo_full_name="testorg/testrepo",
+                expected_text="",
+                expected_tags=[],
+                expected_error="PR #123 is not merged yet",
+            ),
+            TestSendPRMergeEventCase(
+                name="PR merged, qa/no-code-change label, no description, team label",
+                labels=["qa/no-code-change"],
+                merged=True,
+                number=123,
+                login="testuser",
+                body="""
+### Describe how to test/QA your changes
+
+### Possible Drawbacks / Trade-offs""",
+                ref="main",
+                repo_full_name="testorg/testrepo",
+                expected_text="PR #123 merged to main at testorg/testrepo by testuser with QA description []",
+                expected_tags=[
+                    "repo:testorg/testrepo",
+                    "pr_id:123",
+                    "author:testuser",
+                    "qa_label:qa/no-code-change",
+                    "qa_description:missing",
+                ],
+                expected_error=None,
+            ),
+            TestSendPRMergeEventCase(
+                name="PR merged, no qa label, with description",
+                labels=["team/team-a"],
+                merged=True,
+                number=123,
+                login="testuser",
+                body="""
+### Describe how to test/QA your changes
+You should do
+#### Step 1
+Create an ubuntu VM
+#### Step 2
+Install the agent
+#### Step 3
+Send some logs
+#### Step 4
+Check the logs in DD
+### Possible Drawbacks / Trade-offs""",
+                ref="main",
+                repo_full_name="testorg/testrepo",
+                expected_text="""PR #123 merged to main at testorg/testrepo by testuser with QA description [You should do
+#### Step 1
+Create an ubuntu VM
+#### Step 2
+Install the agent
+#### Step 3
+Send some logs
+#### Step 4
+Check the logs in DD]""",
+                expected_tags=[
+                    "repo:testorg/testrepo",
+                    "pr_id:123",
+                    "author:testuser",
+                    "qa_label:missing",
+                    "team:team-a",
+                ],
+                expected_error=None,
+            ),
+        ]
+
+        for tc in testcases:
+            with (
+                patch('tasks.libs.ciproviders.github_api.GithubAPI') as gh_mock,
+                patch('tasks.github_tasks.send_event') as send_event_mock,
+            ):
+                from github.NamedUser import NamedUser
+                from github.PullRequestPart import PullRequestPart
+                from github.Repository import Repository
+
+                repo_mock = unittest.mock.Mock(spec=Repository)
+                repo_mock.full_name = tc.repo_full_name
+
+                base_mock = unittest.mock.Mock(spec=PullRequestPart)
+                base_mock.ref = tc.ref
+                base_mock.repo = repo_mock
+
+                user_mock = Mock(spec=NamedUser)
+                user_mock.login = tc.login
+
+                pr_mock = unittest.mock.Mock(spec=GithubAPIMock.PullRequest)
+                pr_mock.number = tc.number
+                pr_mock.user = user_mock
+                pr_mock.body = tc.body
+                pr_mock.base = base_mock
+                pr_mock.merged = tc.merged
+
+                gh = GithubAPIMock(
+                    pr_labels=tc.labels,
+                    pr=pr_mock,
+                )
+                gh_mock.return_value = gh
+
+                send_event_mock.return_value = None
+
+                # with self.assertRaises(Exit) as exit_mock:
+                try:
+                    pr_merge_dd_event_sender(Context(), pr_id=tc.number, dry_run=tc.dry_run)
+                except Exit as exception:
+                    if tc.expected_error:
+                        send_event_mock.assert_not_called()
+                        self.assertEqual(exception.code, 1, f"Test case: {tc.name}")
+                        self.assertEqual(exception.message, tc.expected_error, f"Test case: {tc.name}")
+                        continue
+                    self.fail(f"Test case: {tc.name} should not have raised an error")
+
+                if tc.expected_error:
+                    self.fail(f"Test case: {tc.name} should have raised an error")
+
+                send_event_mock.assert_called_once_with(title="PR merged", text=tc.expected_text, tags=tc.expected_tags)
