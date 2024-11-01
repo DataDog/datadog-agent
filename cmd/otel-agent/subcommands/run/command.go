382cc0f589bd389e7ebfe5fd22146e8e5a438827
Yang Song <yang.song@datadoghq.com>
2024-11-01 10:03:30 -0400

    Remove DetectFeatures

--- cmd/otel-agent/subcommands/run/command.go
+++ cmd/otel-agent/subcommands/run/command.go
@@ -100,9 +100,8 @@ func runOTelAgentCommand(ctx context.Context, params *subcommands.GlobalParams, 
 	if err == agentConfig.ErrNoDDExporter {
 		return fxutil.Run(
 			fx.Supply(uris),
-			fx.Provide(func() (coreconfig.Component, error) {
-				pkgconfigenv.DetectFeatures(acfg)
-				return acfg, nil
+			fx.Provide(func() coreconfig.Component {
+				return acfg
 			}),
 			converterfx.Module(),
 			fx.Provide(func(cp converter.Component) confmap.Converter {
