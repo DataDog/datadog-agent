b2b934a6b9bafe3157759bae7b234100fb8c36b1
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-11-04 23:11:07 +0100

    clean up

--- pkg/haagent/config.go
+++ pkg/haagent/config.go
@@ -2,23 +2,10 @@ package haagent
 
 import pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
 
-func getHAIntegrations() []string {
-	return pkgconfigsetup.Datadog().GetStringSlice("ha_agent.integrations")
-}
-
 func IsEnabled() bool {
 	return pkgconfigsetup.Datadog().GetBool("ha_agent.enabled")
 }
 
 func GetGroup() string {
 	return pkgconfigsetup.Datadog().GetString("ha_agent.group")
-}
-
-func IsHAIntegration(integrationName string) bool {
-	for _, name := range getHAIntegrations() {
-		if name == integrationName {
-			return true
-		}
-	}
-	return false
 }
