8648fb48578f7fdd180142f9aa57edc05bf83f35
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-11-04 23:10:28 +0100

    clean up

--- pkg/haagent/payload.go
+++ pkg/haagent/payload.go
@@ -1,10 +1,5 @@
 package haagent
 
-type Integration struct {
-	ID   string `json:"id"`
-	Type string `json:"type"`
-}
-
 type Payload struct {
 	PrimaryAgent        string `json:"primary_agent"`
 	ExpirationTimestamp int64  `json:"expiration_timestamp"`
