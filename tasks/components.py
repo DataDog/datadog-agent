a1f30e19de33741173e831089397e24b872031dd
Len Gamburg <len.gamburg@datadoghq.com>
2024-11-06 15:38:09 -0500

    Debug component lint S

--- tasks/components.py
+++ tasks/components.py
@@ -546,6 +546,12 @@ def lint_components(_, fix=False):
             f.write(codeowners)
     elif current != codeowners:
         print(f"** {filename} differs")
+
+        print(f"** CURRENT")
+        print(f"{current}")
+        print(f"** CODDEOWNERS")
+        print(f"{codeowners}")
+
         ok = False
         fixable = True
 
