6a0855420c25a2e8d5402e0025bfff346fdc9970
Raphael Gavache <raphael.gavache@datadoghq.com>
2024-11-08 13:52:59 +0100

    Update setup.go

--- pkg/fleet/installer/setup.go
+++ pkg/fleet/installer/setup.go
@@ -26,7 +26,7 @@ func Setup(ctx context.Context, env *env.Env) error {
 	for _, url := range defaultPackages {
 		err = cmd.Install(ctx, url, nil)
 		if err != nil {
-			return fmt.Errorf("failed to install package %s: %v\n", url, err)
+			return fmt.Errorf("failed to install package %s: %w", url, err)
 		}
 	}
 	return nil
