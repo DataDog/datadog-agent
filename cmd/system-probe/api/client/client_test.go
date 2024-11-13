81162a9a1eeddad3cacf14258f9c548321444ade
Bryce Kahle <bryce.kahle@datadoghq.com>
2024-11-13 11:27:46 -0800

    add test for GetCheck

--- cmd/system-probe/api/client/client_test.go
+++ cmd/system-probe/api/client/client_test.go
@@ -6,9 +6,14 @@
 package client
 
 import (
+	"context"
+	"net"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestConstructURL(t *testing.T) {
@@ -20,4 +25,29 @@ func TestConstructURL(t *testing.T) {
 
 	u = constructURL("zzzz", "asdf")
 	assert.Equal(t, "http://sysprobe/zzzz/asdf", u)
+}
+
+func TestGetCheck(t *testing.T) {
+	type testData struct {
+		Str string
+		Num int
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/test/check" {
+			_, _ = w.Write([]byte(`{"Str": "asdf", "Num": 42}`))
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
+	t.Cleanup(server.Close)
+
+	client := &http.Client{Transport: &http.Transport{DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+		return net.Dial("tcp", server.Listener.Addr().String())
+	}}}
+
+	resp, err := GetCheck[testData](client, "test")
+	require.NoError(t, err)
+	assert.Equal(t, "asdf", resp.Str)
+	assert.Equal(t, 42, resp.Num)
 }
