8c7f6babb14535aeddea1b3dbf262b3a546621ed
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-11-14 16:27:39 +0100

    [gomodules-legacy] Added legacy_go_mod_version

--- tasks/libs/common/gomodules.py
+++ tasks/libs/common/gomodules.py
@@ -151,6 +151,7 @@ class GoModule:
     lint_targets: list[str] | None = None
     # Whether the module is an otel dependency or not
     used_by_otel: bool = False
+    legacy_go_mod_version: bool | None = None
 
     @staticmethod
     def from_dict(path: str, data: dict[str, object]) -> GoModule:
@@ -165,6 +166,7 @@ class GoModule:
             importable=data.get("importable", default["importable"]),
             independent=data.get("independent", default["independent"]),
             used_by_otel=data.get("used_by_otel", default["used_by_otel"]),
+            legacy_go_mod_version=data.get("legacy_go_mod_version", default["legacy_go_mod_version"]),
         )
 
     @staticmethod
@@ -197,6 +199,7 @@ class GoModule:
             "importable": self.importable,
             "independent": self.independent,
             "used_by_otel": self.used_by_otel,
+            "legacy_go_mod_version": self.legacy_go_mod_version,
         }
 
         if remove_path:
