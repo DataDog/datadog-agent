9ef5cdddebfe7ceb12a95a8f2cedb5d70bb78eef
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2024-11-18 13:40:19 +0100

    Revert "Sync health test from main"
    This reverts commit e59e8e829911974698c5f5c351ff1c4bcfa38815.

--- test/new-e2e/tests/agent-subcommands/health/health_win_test.go
+++ test/new-e2e/tests/agent-subcommands/health/health_win_test.go
@@ -8,11 +8,13 @@ package health
 import (
 	"testing"
 
-	"github.com/DataDog/test-infra-definitions/components/os"
-	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
-
+	"github.com/DataDog/datadog-agent/test/fakeintake/api"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
+	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
+	"github.com/DataDog/test-infra-definitions/components/os"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
+	"github.com/stretchr/testify/assert"
 )
 
 type windowsHealthSuite struct {
@@ -20,9 +22,27 @@ type windowsHealthSuite struct {
 }
 
 func TestWindowsHealthSuite(t *testing.T) {
-	t.Parallel()
-	suite := &windowsHealthSuite{baseHealthSuite{descriptor: os.WindowsDefault}}
-	e2e.Run(t, suite, e2e.WithProvisioner(awshost.Provisioner(
-		awshost.WithEC2InstanceOptions(ec2.WithOS(suite.descriptor)),
-	)))
+	e2e.Run(t, &windowsHealthSuite{}, e2e.WithProvisioner(awshost.Provisioner(awshost.WithEC2InstanceOptions(ec2.WithOS(os.WindowsDefault)))))
+}
+
+func (v *windowsHealthSuite) TestDefaultInstallUnhealthy() {
+	// the fakeintake says that any API key is invalid by sending a 403 code
+	override := api.ResponseOverride{
+		Endpoint:    "/api/v1/validate",
+		StatusCode:  403,
+		ContentType: "text/plain",
+		Body:        []byte("invalid API key"),
+	}
+	v.Env().FakeIntake.Client().ConfigureOverride(override)
+	// restart the agent, which validates the key using the fakeintake at startup
+	v.UpdateEnv(awshost.Provisioner(awshost.WithEC2InstanceOptions(ec2.WithOS(os.WindowsDefault)),
+		awshost.WithAgentOptions(agentparams.WithAgentConfig("log_level: info\n"))))
+
+	// agent should be unhealthy because the key is invalid
+	_, err := v.Env().Agent.Client.Health()
+	if err == nil {
+		assert.Fail(v.T(), "agent expected to be unhealthy, but no error found!")
+		return
+	}
+	assert.Contains(v.T(), err.Error(), "Agent health: FAIL")
 }
