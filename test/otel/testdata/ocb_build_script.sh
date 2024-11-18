b136eb3b12095d3b3681809fb8c6ad0a3c4a927a
jackgopack4 <john.peterson@datadoghq.com>
2024-11-18 17:49:51 +0000

    update datadog_otel_components_ocb_build gitlab test and underlying scripts

--- test/otel/testdata/ocb_build_script.sh
+++ test/otel/testdata/ocb_build_script.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-ARCH=$(uname -m)
-if [ "$ARCH" = "x86_64" ]; then
-    ARCH="amd64"
-elif [ "$ARCH" = "aarch64" ]; then
-    ARCH="arm64"
-fi
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-OCB_URL="https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/cmd%2Fbuilder%2Fv0.113.0/ocb_0.113.0_${OS}_${ARCH}"
 mkdir -p /tmp/otel-ci
-cp ./test/otel/testdata/* /tmp/otel-ci/
-wget -O /tmp/otel-ci/ocb "$OCB_URL"
+cp ./test/otel/testdata/builder-config.yaml /tmp/otel-ci/
+cp ./test/otel/testdata/collector-config.yaml /tmp/otel-ci/
+
+# TODO: Pin OCB to v0.114.0 once we upgrade collector dependencies to v0.114.0
+# OCB_VERSION="0.113.0"
+# CGO_ENABLED=0 go install -trimpath -ldflags="-s -w" go.opentelemetry.io/collector/cmd/builder@v${OCB_VERSION}
+# mv "$(go env GOPATH)/bin/builder" /tmp/otel-ci/ocb
+
+# TODO: remove this once we upgrade collector dependencies to v0.114.0
+# clone collector repo and build cmd/builder from source
+git clone --depth 1 https://github.com/open-telemetry/opentelemetry-collector.git /tmp/otel-ci/opentelemetry-collector
+cwd=$(pwd)
+cd "/tmp/otel-ci/opentelemetry-collector/cmd/builder" || (echo "failed to change to ocb source dir" && exit 1)
+CGO_ENABLED=0 go build -o /tmp/otel-ci/ocb -trimpath -ldflags "-s -w" . > "${cwd}/go-install-ocb.log" 2>&1 || (echo "failed to build ocb" && exit 1)
+cd "$cwd" || (echo "failed to change back to original dir" && exit 1)
+
 chmod +x /tmp/otel-ci/ocb
 
 /tmp/otel-ci/ocb --config=/tmp/otel-ci/builder-config.yaml > ocb-output.log 2>&1
-grep -q 'Compiled' ocb-output.log || (echo "OCB failed to compile" && exit 1)
-grep -q '{"binary": "/tmp/otel-ci/otelcol-custom/otelcol-custom"}' ocb-output.log || (echo "OCB failed to compile" && exit 1)
+grep -q 'Compiled' ocb-output.log || (echo "OCB failed to build custom collector" && exit 1)
+grep -q '{"binary": "/tmp/otel-ci/otelcol-custom/otelcol-custom"}' ocb-output.log || (echo "OCB failed to build custom collector" && exit 1)
 
 /tmp/otel-ci/otelcol-custom/otelcol-custom --config /tmp/otel-ci/collector-config.yaml > otelcol-custom.log 2>&1 &
 OTELCOL_PID=$!
@@ -59,6 +65,9 @@ retry_grep 'Everything is ready. Begin running and processing data.' otelcol-cus
 
 retry_curl https://localhost:7777 > flare-info.log 2>&1
 grep -q '"provided_configuration": ""' flare-info.log || (echo "provided config is not empty" && kill $OTELCOL_PID && exit 1)
-grep -q 'extensions:\\n  - ddflare\\n' flare-info.log || (echo "ddflare extension should be enabled" && kill $OTELCOL_PID && exit 1)
+grep -q 'ddflare/dd-autoconfigured' flare-info.log || (echo "ddflare extension should be enabled" && kill $OTELCOL_PID && exit 1)
+grep -q 'health_check/dd-autoconfigured' flare-info.log || (echo "health_check extension should be enabled" && kill $OTELCOL_PID && exit 1)
+grep -q 'pprof/dd-autoconfigured' flare-info.log || (echo "pprof extension should be enabled" && kill $OTELCOL_PID && exit 1)
+grep -q 'zpages/dd-autoconfigured' flare-info.log || (echo "zpages extension should be enabled" && kill $OTELCOL_PID && exit 1)
 kill $OTELCOL_PID
 echo "OCB build script completed successfully"
