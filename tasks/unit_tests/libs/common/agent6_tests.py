63761562cb298ddcf0f01d7750cedc948db79a72
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-11-18 16:49:41 +0100

    [rfc-update-tasks-a6-a7] unit-tests: Added agent 6 tests

--- tasks/unit_tests/libs/common/agent6_tests.py
+++ tasks/unit_tests/libs/common/agent6_tests.py
@@ -1,8 +1,11 @@
+import os
 import unittest
 
 from invoke import Context
 
-from tasks.libs.common.agent6 import agent_context, is_agent6
+from tasks.libs.common.agent6 import agent_context, is_agent6, prepare
+from tasks.libs.common.git import get_default_branch
+from tasks.libs.common.gomodules import get_default_modules
 
 
 def get_ctx():
@@ -10,6 +13,11 @@ def get_ctx():
 
 
 class TestAgent6(unittest.TestCase):
+    def setUp(self):
+        # Pull only once
+        prepare(get_ctx())
+        os.environ['AGENT6_NO_PULL'] = '1'
+
     def test_context_is_agent6_true(self):
         with agent_context(get_ctx(), 6):
             self.assertTrue(is_agent6())
@@ -17,3 +25,42 @@ class TestAgent6(unittest.TestCase):
     def test_context_is_agent6_false(self):
         with agent_context(get_ctx(), 7):
             self.assertFalse(is_agent6())
+
+    def test_context_nested(self):
+        with agent_context(get_ctx(), 6):
+            with agent_context(get_ctx(), 6):
+                self.assertTrue(is_agent6())
+            self.assertTrue(is_agent6())
+
+    def test_context_pwd(self):
+        ctx = get_ctx()
+
+        with agent_context(ctx, 7):
+            pwd7 = ctx.run('pwd').stdout
+
+        with agent_context(ctx, 6):
+            pwd6 = ctx.run('pwd').stdout
+
+        self.assertNotEqual(pwd6, pwd7)
+
+    def test_context_modules(self):
+        ctx = get_ctx()
+
+        with agent_context(ctx, 7):
+            modules7 = get_default_modules()
+
+        with agent_context(ctx, 6):
+            modules6 = get_default_modules()
+
+        self.assertNotEqual(set(modules6.keys()), set(modules7.keys()))
+
+    def test_context_branch(self):
+        ctx = get_ctx()
+
+        with agent_context(ctx, 7):
+            branch7 = get_default_branch()
+
+        with agent_context(ctx, 6):
+            branch6 = get_default_branch()
+
+        self.assertNotEqual(branch6, branch7)
