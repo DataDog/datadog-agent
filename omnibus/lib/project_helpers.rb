db52d64330b6a3e4c62622deafccb57ef7d322bc
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-11-19 10:10:43 +0100

    fix inverted logic

--- omnibus/lib/project_helpers.rb
+++ omnibus/lib/project_helpers.rb
@@ -7,6 +7,6 @@ require './lib/ostools.rb'
 def sysprobe_enabled?()
   # This doesn't account for Windows special case which build system probe as part of the
   # agent build process
-  !heroku_target? && linux_target? && ENV.fetch('SYSTEM_PROBE_BIN', '').empty?
+  !heroku_target? && linux_target? && !ENV.fetch('SYSTEM_PROBE_BIN', '').empty?
 end
 
