ad92114763c9136fd08b23f7aea5870145de44ae
Lucas Liseth <lucas.liseth@datadoghq.com>
2024-11-20 16:38:46 -0500

    Treat fakeintake as a black box

--- test/new-e2e/tests/agent-metrics-logs/log-agent/linux-log/integrations/fixtures/maxSize.py
+++ test/new-e2e/tests/agent-metrics-logs/log-agent/linux-log/integrations/fixtures/maxSize.py
@@ -1,14 +1,26 @@
+import uuid
+
 from datadog_checks.base import AgentCheck
 
 
 class HelloCheck(AgentCheck):
+    def __init__(self, name, init_config, instances):
+        super().__init__(name, init_config, instances)
+        # Initialize a local variable to simulate a counter
+        self.counter = 0
+
     def check(self, instance):
+        self.counter = self.counter + 1
         data = {}
+
+        uuid_str = str(uuid.uuid4())
         # Create a 1 MB string
-        string_one_mb = "a" * 1024 * 1024
-        # Remove 16 characters to account for the json the launcher adds and the
-        # extra newline character
-        string_one_mb = string_one_mb[:-15]
-        data['message'] = string_one_mb
+        total_size = 1024 * 256
+        # Remove 37 characters to account for the json characters, tags, and
+        # newline character the launcher adds to the log
+        padding_size = total_size - len(uuid_str) - 37
+        log_str = uuid_str + ('a' * padding_size)
+        data['message'] = log_str
 
         self.send_log(data)
+        self.monotonic_count("rotate_logs_sent", self.counter, tags=["foo:bar"])
--- test/new-e2e/tests/agent-metrics-logs/log-agent/linux-log/integrations/logs_test.go
+++ test/new-e2e/tests/agent-metrics-logs/log-agent/linux-log/integrations/logs_test.go
@@ -7,9 +7,7 @@ package integrationslogs
 
 import (
 	_ "embed"
-	"strings"
 	"testing"
-	"time"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
@@ -41,7 +39,7 @@ func TestIntegrationsLogsSuite(t *testing.T) {
 		e2e.WithProvisioner(awshost.Provisioner(awshost.WithAgentOptions(
 			agentparams.WithLogs(),
 			// set the integration log file max size to 1MB
-			agentparams.WithAgentConfig("logs_config.integrations_logs_files_max_size: 2"),
+			agentparams.WithAgentConfig("logs_config.integrations_logs_files_max_size: 1"),
 			agentparams.WithFile("/etc/datadog-agent/checks.d/writeTenLogs.py", writeTenLogsCheck, true),
 			agentparams.WithFile("/etc/datadog-agent/conf.d/writeTenLogs.yaml", writeTenLogsConfig, true),
 			agentparams.WithFile("/etc/datadog-agent/checks.d/maxSize.py", maxSizeCheck, true),
@@ -56,32 +54,23 @@ func (v *IntegrationsLogsSuite) TestWriteTenLogsCheck() {
 	utils.CheckLogsExpected(v.T(), v.Env().FakeIntake, "ten_logs_service", "Custom log message", []string{"env:dev", "bar:foo"})
 }
 
-// TestIntegrationLogFileMaxSize ensures integration log files don't exceed the max file size
-func (v *IntegrationsLogsSuite) TestIntegrationLogFileMaxSize() {
+// TestIntegrationLogFileRotation ensures integration log files don't exceed the max file size
+func (v *IntegrationsLogsSuite) TestIntegrationLogFileRotation() {
 	// Since it's not yet possible to write to the integration log file by calling
-	// the agent check command, we can test if the file size limits are being
-	// respected using the following method:
-	// 1. Wait until the integration log file reaches the maximum allowable size
-	// (it won't be deleted until the next log that exceeds the maximum allowable
-	// size is written).
-	// 2. Check immediately that on the subsequent write, the file is smaller than
-	// in step 1, indicating the log file has been deleted and remade, and thus
-	// respects the set size.
-	// 3. After the file sizes are confirmed, check to make sure fakeIntake still receives logs
-	v.EventuallyWithT(func(c *assert.CollectT) {
-		output := v.Env().RemoteHost.MustExecute("sudo cat /opt/datadog-agent/run/integrations/maxSize*.log")
-		integrationLogFileSize := len(output)
-		assert.Equal(c, 2*1024*1024, integrationLogFileSize)
-	}, 1*time.Minute, 5*time.Second)
+	// the agent check command, we can test if the file rotation works using the following method:
+	// 1. Send logs until to the logs from integrations launcher until their
+	// cumulative size is greater than integration_logs_files_max_size (for logs
+	// of size 256kb, this will be four logs given the max size of 1mb)
+	// 2. Check that the logs received from fakeIntake are unique by checking the
+	// UUID and at the same time ensure monotonic_count is equal to 1 (indicating
+	// a 1:1 correlation between a log and metric)
 
-	v.EventuallyWithT(func(c *assert.CollectT) {
-		output := v.Env().RemoteHost.MustExecute("sudo cat /opt/datadog-agent/run/integrations/maxSize*.log")
-		integrationLogFileSize := len(output)
-		assert.Equal(c, 1*1024*1024, integrationLogFileSize)
-	}, 1*time.Minute, 5*time.Second)
-
-	stringSize := 1024 * 1024
-	logString := strings.Repeat("a", stringSize-15)
-
-	utils.CheckLogsExpected(v.T(), v.Env().FakeIntake, "max_size_service", logString, []string{})
+	for i := 0; i < 5; i++ {
+		utils.CheckLogsExpected(v.T(), v.Env().FakeIntake, "max_size_service", ".*aaaaaaaaaaa.*", []string{})
+		metrics, err := v.Env().FakeIntake.Client().FilterMetrics("rotate_logs_sent")
+		assert.NoError(v.T(), err)
+		points := metrics[len(metrics)-1].Points
+		point := metrics[len(metrics)-1].Points[len(points)-1].Value
+		assert.Equal(v.T(), 1.0, point)
+	}
 }
