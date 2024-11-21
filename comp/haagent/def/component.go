c7da32da0ffebf6f8f08db0ecfb9c7ef62d6575a
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-11-21 09:18:47 +0100

    update team

--- comp/haagent/def/component.go
+++ comp/haagent/def/component.go
@@ -6,7 +6,7 @@
 // Package haagent handles states for HA Agent feature.
 package haagent
 
-// team: network-device-monitoring remote-config fleet
+// team: network-device-monitoring
 
 // Component is the component type.
 type Component interface {
