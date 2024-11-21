12322575ee7aba0b89c77daf4d16cae7ed17243e
Minyi Zhu <minyi.zhu@datadoghq.com>
2024-11-21 14:04:20 -0500

    remove cmd/cluster-agent in darwin build

--- cmd/cluster-agent/command/command.go
+++ cmd/cluster-agent/command/command.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build !windows && kubeapiserver
+//go:build !darwin && !windows && kubeapiserver
 
 // Package command implements the top-level `cluster-agent` binary, including its subcommands.
 package command
--- cmd/cluster-agent/subcommands/workloadlist/command.go
+++ cmd/cluster-agent/subcommands/workloadlist/command.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build !windows && kubeapiserver
+//go:build !darwin && !windows && kubeapiserver
 
 // Package workloadlist implements 'cluster-agent workloadlist'.
 package workloadlist
