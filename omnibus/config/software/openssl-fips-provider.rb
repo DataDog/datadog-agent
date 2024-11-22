051b7dc6abc0bb41e8adc573536c67497a0b4d68
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-11-22 14:38:58 +0100

    WIP fips module for windows

--- omnibus/config/software/openssl-fips-provider.rb
+++ omnibus/config/software/openssl-fips-provider.rb
@@ -22,19 +22,25 @@ source url: "https://www.openssl.org/source/#{OPENSSL_FIPS_MODULE_FILENAME}",
 relative_path "openssl-#{OPENSSL_FIPS_MODULE_VERSION}"
 
 build do
+    prefix = if windows_target? then "perl.exe" else "" end
+    platform = if windows_target? then "mingw64" else "" end
     # Exact build steps from security policy:
     # https://csrc.nist.gov/CSRC/media/projects/cryptographic-module-validation-program/documents/security-policies/140sp4282.pdf
     #
     # ---------------- DO NOT MODIFY LINES BELOW HERE ----------------
-    command "./Configure enable-fips"
+    command "#{prefix} ./Configure enable-fips #{platform}"
 
-    command "make"
-    command "make install"
+    command "make V=1"
+    command "make install V=1"
     # ---------------- DO NOT MODIFY LINES ABOVE HERE ----------------
 
     mkdir "#{install_dir}/embedded/ssl"
     mkdir "#{install_dir}/embedded/lib/ossl-modules"
-    copy "/usr/local/lib*/ossl-modules/fips.so", "#{install_dir}/embedded/lib/ossl-modules/fips.so"
+    if linux_target?
+      copy "/usr/local/lib*/ossl-modules/fips.so", "#{install_dir}/embedded/lib/ossl-modules/fips.so"
+    elsif windows_target?
+      copy "/usr/local/lib*/ossl-modules/fips.so", "#{install_dir}/embedded/lib/ossl-modules/fips.so"
+    end
 
     erb source: "openssl.cnf.erb",
         dest: "#{install_dir}/embedded/ssl/openssl.cnf.tmp",
