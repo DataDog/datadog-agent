14ad3d7021b8cff1cd8f12eeeaa897f7e695553b
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-11-25 15:22:13 +0100

    [rfc-update-tasks-a6-a7] Added various git commands to worktree and skip_checkout option

--- tasks/worktree.py
+++ tasks/worktree.py
@@ -1,6 +1,7 @@
 from invoke import task
+from invoke.exceptions import Exit
 
-from tasks.libs.common.worktree import agent_context, enter_env, init_env, remove_env
+from tasks.libs.common.worktree import WORKTREE_DIRECTORY, agent_context, enter_env, init_env, remove_env
 
 
 @task
@@ -18,19 +19,53 @@ def remove(ctx):
 
 
 @task
-def run(ctx, branch: str, command: str):
+def status(ctx):
+    """Displays the status of the worktree environment."""
+
+    if not WORKTREE_DIRECTORY.is_dir():
+        raise Exit('No worktree environment found.')
+
+    ctx.run(f"git -C '{WORKTREE_DIRECTORY}' status", pty=True)
+
+
+@task
+def switch(ctx, ref):
+    """Changes the worktree environment to the specified ref.
+
+    Note:
+        This won't pull.
+    """
+
+    if not WORKTREE_DIRECTORY.is_dir():
+        raise Exit('No worktree environment found.')
+
+    ctx.run(f"git -C '{WORKTREE_DIRECTORY}' checkout '{ref}'", pty=True)
+
+
+@task
+def pull(ctx):
+    """Pulls the worktree environment."""
+
+    if not WORKTREE_DIRECTORY.is_dir():
+        raise Exit('No worktree environment found.')
+
+    ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull", pty=True)
+
+
+@task
+def run(ctx, branch: str, command: str, skip_checkout: bool = False):
     """Runs a command in the target worktree environment.
 
     Usage:
         $ inv worktree.run 6.53.x "head CHANGELOG.rst"  # Displays the changelog of the target branch
     """
 
-    with agent_context(ctx, branch):
+    with agent_context(ctx, branch, skip_checkout=skip_checkout):
         ctx.run(command)
 
 
 @task
-def invoke(ctx, branch: str):
+def invoke(ctx, branch: str, skip_checkout: bool = False):
     """Enters the worktree environment in order to invoke tasks in this context.
 
     Note:
@@ -41,4 +76,4 @@ def invoke(ctx, branch: str):
     """
 
     # The tasks running after this one will be using the agent 6 environment
-    enter_env(ctx, branch)
+    enter_env(ctx, branch, skip_checkout=skip_checkout)
