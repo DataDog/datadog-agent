c40760a8aa965cb70f0b488fcc793b5ed8bf1227
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-11-26 12:47:22 +0100

    [ha-agent] Consume RC HA_AGENT

--- comp/haagent/impl/haagent.go
+++ comp/haagent/impl/haagent.go
@@ -7,8 +7,10 @@ package haagentimpl
 
 import (
 	"context"
+	"encoding/json"
 
 	log "github.com/DataDog/datadog-agent/comp/core/log/def"
+	"github.com/DataDog/datadog-agent/pkg/remoteconfig/state"
 	"github.com/DataDog/datadog-agent/pkg/util/hostname"
 	"go.uber.org/atomic"
 )
@@ -46,4 +48,30 @@ func (h *haAgentImpl) SetLeader(leaderAgentHostname string) {
 		return
 	}
 	h.isLeader.Store(agentHostname == leaderAgentHostname)
+}
+
+func (h *haAgentImpl) onHaAgentUpdate(updates map[string]state.RawConfig, applyStateCallback func(string, state.ApplyStatus)) {
+	h.log.Debugf("Updates received: count=%d", len(updates))
+
+	for configPath, rawConfig := range updates {
+		h.log.Debugf("Received config %s: %s", configPath, string(rawConfig.Config))
+		haAgentMsg := haAgentConfig{}
+		err := json.Unmarshal(rawConfig.Config, &haAgentMsg)
+		if err != nil {
+			h.log.Warnf("Skipping invalid HA_AGENT update %s: %v", configPath, err)
+			applyStateCallback(configPath, state.ApplyStatus{
+				State: state.ApplyStateError,
+				Error: "error unmarshalling payload",
+			})
+			continue
+		}
+
+		h.SetLeader(haAgentMsg.Leader)
+
+		h.log.Debugf("Processed config %s: %v", configPath, haAgentMsg)
+
+		applyStateCallback(configPath, state.ApplyStatus{
+			State: state.ApplyStateUnacknowledged,
+		})
+	}
 }
--- comp/haagent/impl/haagent_test.go
+++ comp/haagent/impl/haagent_test.go
@@ -8,8 +8,15 @@ package haagentimpl
 import (
 	"testing"
 
+	"github.com/DataDog/datadog-agent/comp/core/config"
+	logmock "github.com/DataDog/datadog-agent/comp/core/log/mock"
+	"github.com/DataDog/datadog-agent/pkg/remoteconfig/state"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx"
 )
+
+var testConfigID = "datadog/2/HA_AGENT/group-62345762794c0c0b/65f17d667fb50f8ae28a3c858bdb1be9ea994f20249c119e007c520ac115c807"
 
 func Test_Enabled(t *testing.T) {
 	tests := []struct {
@@ -34,7 +41,7 @@ func Test_Enabled(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			haAgent := newTestHaAgentComponent(t, tt.configs)
+			haAgent := newTestHaAgentComponent(t, tt.configs).Comp
 			assert.Equal(t, tt.expectedEnabled, haAgent.Enabled())
 		})
 	}
@@ -44,7 +51,7 @@ func Test_GetGroup(t *testing.T) {
 	agentConfigs := map[string]interface{}{
 		"ha_agent.group": "my-group-01",
 	}
-	haAgent := newTestHaAgentComponent(t, agentConfigs)
+	haAgent := newTestHaAgentComponent(t, agentConfigs).Comp
 	assert.Equal(t, "my-group-01", haAgent.GetGroup())
 }
 
@@ -52,11 +59,99 @@ func Test_IsLeader_SetLeader(t *testing.T) {
 	agentConfigs := map[string]interface{}{
 		"hostname": "my-agent-hostname",
 	}
-	haAgent := newTestHaAgentComponent(t, agentConfigs)
+	haAgent := newTestHaAgentComponent(t, agentConfigs).Comp
 
 	haAgent.SetLeader("another-agent")
 	assert.False(t, haAgent.IsLeader())
 
 	haAgent.SetLeader("my-agent-hostname")
 	assert.True(t, haAgent.IsLeader())
+}
+
+func Test_RCListener(t *testing.T) {
+	tests := []struct {
+		name             string
+		configs          map[string]interface{}
+		expectRCListener bool
+	}{
+		{
+			name: "enabled",
+			configs: map[string]interface{}{
+				"ha_agent.enabled": true,
+			},
+			expectRCListener: true,
+		},
+		{
+			name: "disabled",
+			configs: map[string]interface{}{
+				"ha_agent.enabled": false,
+			},
+			expectRCListener: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			provides := newTestHaAgentComponent(t, tt.configs)
+			if tt.expectRCListener {
+				assert.NotNil(t, provides.RCListener.ListenerProvider)
+			} else {
+				assert.Nil(t, provides.RCListener.ListenerProvider)
+			}
+		})
+	}
+}
+
+func Test_haAgentImpl_onHaAgentUpdate(t *testing.T) {
+
+	tests := []struct {
+		name                string
+		updates             map[string]state.RawConfig
+		expectedApplyID     string
+		expectedApplyStatus state.ApplyStatus
+	}{
+		{
+			name: "successful update",
+			updates: map[string]state.RawConfig{
+				testConfigID: {Config: []byte(`{"group":"group01","leader":"ha-agent1"}`)},
+			},
+			expectedApplyID: testConfigID,
+			expectedApplyStatus: state.ApplyStatus{
+				State: state.ApplyStateUnacknowledged,
+			},
+		},
+		{
+			name: "invalid payload",
+			updates: map[string]state.RawConfig{
+				testConfigID: {Config: []byte(`invalid-json`)},
+			},
+			expectedApplyID: testConfigID,
+			expectedApplyStatus: state.ApplyStatus{
+				State: state.ApplyStateError,
+				Error: "error unmarshalling payload",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			agentConfigs := map[string]interface{}{
+				"hostname": "my-agent-hostname",
+			}
+			agentConfigComponent := fxutil.Test[config.Component](t, fx.Options(
+				config.MockModule(),
+				fx.Replace(config.MockParams{Overrides: agentConfigs}),
+			))
+
+			h := newHaAgentImpl(logmock.New(t), newHaAgentConfigs(agentConfigComponent))
+
+			var applyID string
+			var applyStatus state.ApplyStatus
+			applyFunc := func(id string, status state.ApplyStatus) {
+				applyID = id
+				applyStatus = status
+			}
+			h.onHaAgentUpdate(tt.updates, applyFunc)
+			assert.Equal(t, tt.expectedApplyID, applyID)
+			assert.Equal(t, tt.expectedApplyStatus, applyStatus)
+		})
+	}
 }
