6a5471aa65c1d1944cedfed3bb6ef048122d40df
pducolin <paola.ducolin@datadoghq.com>
2024-11-26 17:28:57 +0100

    [e2e] remove resource name from local kind cluster call

--- test/new-e2e/pkg/environments/local/kubernetes/kind.go
+++ test/new-e2e/pkg/environments/local/kubernetes/kind.go
@@ -131,7 +131,7 @@ func KindRunFunc(ctx *pulumi.Context, env *environments.Kubernetes, params *Prov
 		return err
 	}
 
-	kindCluster, err := kubeComp.NewLocalKindCluster(&localEnv, localEnv.CommonNamer().ResourceName("kind"), params.name, localEnv.KubernetesVersion())
+	kindCluster, err := kubeComp.NewLocalKindCluster(&localEnv, params.name, localEnv.KubernetesVersion())
 	if err != nil {
 		return err
 	}
