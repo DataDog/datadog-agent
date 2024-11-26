bd0ce8d6cc13afb88ffb70a9ee292f3d9c4e21e6
Ryan Hall <ryan.hall@datadoghq.com>
2024-11-26 09:04:31 -0500

    AMLII-2127 - Enable requesting flares with profiles via Remote Config
    Add a new flare provider that generates profile data. Provider is defaulted to disabled.
    
    Optionally enable profiler provider in the RC agent task
    
    Shift existing cli flare generation to call the profiler directly

---
+++ comp/core/profiler/def/component.go
@@ -0,0 +1,16 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+// Package profiler provides a flare folder containing the output of various agent's pprof servers
+package profiler
+
+// team: agent-shared-components
+
+import "github.com/DataDog/datadog-agent/comp/core/flare"
+
+// Component is the component type
+type Component interface {
+	ReadProfileData(seconds int, logFunc func(log string, params ...interface{}) error) (flare.ProfileData, error)
+}
