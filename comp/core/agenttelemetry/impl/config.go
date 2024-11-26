c2f04c28ee78301cdbde7a3f65da2278d5e37a8b
Brian Floersch <brian.floersch@datadoghq.com>
2024-11-26 13:22:57 -0500

    tabs -> spaces

--- comp/core/agenttelemetry/impl/config.go
+++ comp/core/agenttelemetry/impl/config.go
@@ -188,7 +188,7 @@ var defaultProfiles = `
         - name: logs.dropped
         - name: logs.encoded_bytes_sent
         - name: logs.processed
-		- name: logs.sender_latency
+        - name: logs.sender_latency
         - name: logs.sent
         - name: point.sent
         - name: point.dropped
