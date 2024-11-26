dada6cbb431637c499d1407f1c43bb5a7b70baaf
Jeremy Hanna <jeremy.hanna@datadoghq.com>
2024-11-25 22:25:16 -0500

    Move msgo toolchain env setup for fips build into omnibus

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -30,8 +30,14 @@ always_build true
 build do
   license :project_license
 
+  bundled_agents = []
+  if heroku_target?
+    bundled_agents = ["process-agent"]
+  end
+
   # set GOPATH on the omnibus source dir for this software
   gopath = Pathname.new(project_dir) + '../../../..'
+  msgoroot = "/usr/local/msgo"
   flavor_arg = ENV['AGENT_FLAVOR']
   fips_arg = fips_mode? ? "--fips-mode" : ""
   if windows_target?
@@ -58,6 +64,13 @@ build do
 
   # include embedded path (mostly for `pkg-config` binary)
   env = with_standard_compiler_flags(with_embedded_path(env))
+
+  # Use msgo toolchain when fips mode is enabled
+  if fips_mode? && !windows_target?
+    env["GOROOT"] = msgoroot
+    env["PATH"] = "#{msgoroot}/bin:#{env['PATH']}"
+  end
+
   default_install_dir = "/opt/datadog-agent"
   if Omnibus::Config.host_distribution == "ociru"
     default_install_dir = "#{install_dir}"
@@ -79,15 +92,16 @@ build do
     command "inv -e rtloader.clean"
     command "inv -e rtloader.make --install-prefix \"#{install_dir}/embedded\" --cmake-options '-DCMAKE_CXX_FLAGS:=\"-D_GLIBCXX_USE_CXX11_ABI=0\" -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_FIND_FRAMEWORK:STRING=NEVER -DPython3_EXECUTABLE=#{install_dir}/embedded/bin/python3'", :env => env
     command "inv -e rtloader.install"
+    bundle_arg = bundled_agents ? bundled_agents.map { |k| "--bundle #{k}" }.join(" ") : "--bundle agent"
 
     include_sds = ""
     if linux_target?
         include_sds = "--include-sds" # we only support SDS on Linux targets for now
     end
-    command "inv -e agent.build --exclude-rtloader #{include_sds} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{default_install_dir}/embedded --python-home-2=#{default_install_dir}/embedded --python-home-3=#{default_install_dir}/embedded --flavor #{flavor_arg}", env: env
+    command "inv -e agent.build --exclude-rtloader #{include_sds} --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{default_install_dir}/embedded --python-home-2=#{default_install_dir}/embedded --python-home-3=#{default_install_dir}/embedded --flavor #{flavor_arg} #{bundle_arg}", env: env
 
     if heroku_target?
-      command "inv -e agent.build --exclude-rtloader --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded --python-home-2=#{install_dir}/embedded --python-home-3=#{install_dir}/embedded --flavor #{flavor_arg} --agent-bin=bin/agent/core-agent", env: env
+      command "inv -e agent.build --exclude-rtloader --major-version #{major_version_arg} --rebuild --no-development --install-path=#{install_dir} --embedded-path=#{install_dir}/embedded --python-home-2=#{install_dir}/embedded --python-home-3=#{install_dir}/embedded --flavor #{flavor_arg} --agent-bin=bin/agent/core-agent --bundle agent", env: env
     end
   end
 
@@ -116,8 +130,10 @@ build do
     mkdir Omnibus::Config.package_dir() unless Dir.exists?(Omnibus::Config.package_dir())
   end
 
-  platform = windows_arch_i386? ? "x86" : "x64"
-  command "invoke trace-agent.build --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg}", :env => env
+  if not bundled_agents.include? "trace-agent"
+    platform = windows_arch_i386? ? "x86" : "x64"
+    command "invoke trace-agent.build --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg}", :env => env
+  end
 
   if windows_target?
     copy 'bin/trace-agent/trace-agent.exe', "#{install_dir}/bin/agent"
@@ -126,7 +142,9 @@ build do
   end
 
   # Process agent
-  command "invoke -e process-agent.build --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg}", :env => env
+  if not bundled_agents.include? "process-agent"
+    command "invoke -e process-agent.build --install-path=#{install_dir} --major-version #{major_version_arg} --flavor #{flavor_arg}", :env => env
+  end
 
   if windows_target?
     copy 'bin/process-agent/process-agent.exe', "#{install_dir}/bin/agent"
@@ -137,10 +155,12 @@ build do
   # System-probe
   sysprobe_support = (not heroku_target?) && (linux_target? || (windows_target? && do_windows_sysprobe != ""))
   if sysprobe_support
-    if windows_target?
-      command "invoke -e system-probe.build #{fips_arg}", env: env
-    elsif linux_target?
-      command "invoke -e system-probe.build-sysprobe-binary #{fips_arg} --install-path=#{install_dir}", env: env
+    if not bundled_agents.include? "system-probe"
+      if windows_target?
+        command "invoke -e system-probe.build #{fips_arg}", env: env
+      elsif linux_target?
+        command "invoke -e system-probe.build-sysprobe-binary #{fips_arg} --install-path=#{install_dir}", env: env
+      end
     end
 
     if windows_target?
@@ -161,7 +181,9 @@ build do
   # Security agent
   secagent_support = (not heroku_target?) and (not windows_target? or (ENV['WINDOWS_DDPROCMON_DRIVER'] and not ENV['WINDOWS_DDPROCMON_DRIVER'].empty?))
   if secagent_support
-    command "invoke -e security-agent.build #{fips_arg} --install-path=#{install_dir} --major-version #{major_version_arg}", :env => env
+    if not bundled_agents.include? "security-agent"
+      command "invoke -e security-agent.build #{fips_arg} --install-path=#{install_dir} --major-version #{major_version_arg}", :env => env
+    end
     if windows_target?
       copy 'bin/security-agent/security-agent.exe', "#{install_dir}/bin/agent"
     else
@@ -177,7 +199,7 @@ build do
     copy 'bin/cws-instrumentation/cws-instrumentation', "#{install_dir}/embedded/bin"
   end
 
-  # OTel agent
+  # OTel agent - can never be bundled
   if ot_target?
     unless windows_target?
       command "invoke -e otel-agent.build", :env => env
