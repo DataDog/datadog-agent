ef07f7f26c3ab96754394a1c2aae8d024792df4f
jackgopack4 <john.peterson@datadoghq.com>
2024-11-26 20:08:48 +0000

    clean up folder after run

--- test/otel/testdata/ocb_build_script.sh
+++ test/otel/testdata/ocb_build_script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
+OTELCOL_PID=0
 
 mkdir -p /tmp/otel-ci
+trap 'rm -rf /tmp/otel-ci && kill $OTELCOL_PID' EXIT
 cp ./test/otel/testdata/builder-config.yaml /tmp/otel-ci/
 cp ./test/otel/testdata/collector-config.yaml /tmp/otel-ci/
 cp ./tools/ci/retry.sh /tmp/otel-ci/
@@ -30,13 +33,13 @@ grep -q '{"binary": "/tmp/otel-ci/otelcol-custom/otelcol-custom"}' ocb-output.lo
 
 /tmp/otel-ci/otelcol-custom/otelcol-custom --config /tmp/otel-ci/collector-config.yaml > otelcol-custom.log 2>&1 &
 OTELCOL_PID=$!
-/tmp/otel-ci/retry.sh grep -q 'Everything is ready. Begin running and processing data.' otelcol-custom.log || (echo "Failed to start otelcol-custom" && kill $OTELCOL_PID && exit 1)
+/tmp/otel-ci/retry.sh grep -q 'Everything is ready. Begin running and processing data.' otelcol-custom.log || (echo "Failed to start otelcol-custom" && exit 1)
 
 /tmp/otel-ci/retry.sh curl -k https://localhost:7777 > flare-info.log 2>&1 
-grep -q '"provided_configuration": ""' flare-info.log || (echo "provided config is not empty" && kill $OTELCOL_PID && exit 1)
-grep -q 'ddflare/dd-autoconfigured' flare-info.log || (echo "ddflare extension should be enabled" && kill $OTELCOL_PID && exit 1)
-grep -q 'health_check/dd-autoconfigured' flare-info.log || (echo "health_check extension should be enabled" && kill $OTELCOL_PID && exit 1)
-grep -q 'pprof/dd-autoconfigured' flare-info.log || (echo "pprof extension should be enabled" && kill $OTELCOL_PID && exit 1)
-grep -q 'zpages/dd-autoconfigured' flare-info.log || (echo "zpages extension should be enabled" && kill $OTELCOL_PID && exit 1)
-kill $OTELCOL_PID
+grep -q '"provided_configuration": ""' flare-info.log || (echo "provided config is not empty" && exit 1)
+grep -q 'ddflare/dd-autoconfigured' flare-info.log || (echo "ddflare extension should be enabled" && exit 1)
+grep -q 'health_check/dd-autoconfigured' flare-info.log || (echo "health_check extension should be enabled" && exit 1)
+grep -q 'pprof/dd-autoconfigured' flare-info.log || (echo "pprof extension should be enabled" && exit 1)
+grep -q 'zpages/dd-autoconfigured' flare-info.log || (echo "zpages extension should be enabled" && exit 1)
+
 echo "OCB build script completed successfully"
