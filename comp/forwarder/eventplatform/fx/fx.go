037568723e7d0c8389f2d30747d01435761cfe10
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-11-26 10:37:24 +0100

    use fxutil.ProvideComponentConstructor for eventplatform fx package

--- comp/forwarder/eventplatform/fx/fx.go
+++ comp/forwarder/eventplatform/fx/fx.go
@@ -7,13 +7,11 @@
 package fx
 
 import (
-	"go.uber.org/fx"
-
 	eventplatformimpl "github.com/DataDog/datadog-agent/comp/forwarder/eventplatform/impl"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
 // Module defines the fx options for this component.
 func Module() fxutil.Module {
-	return fxutil.Component(fx.Provide(eventplatformimpl.NewComponent))
+	return fxutil.Component(fxutil.ProvideComponentConstructor(eventplatformimpl.NewComponent))
 }
