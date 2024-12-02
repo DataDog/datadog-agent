c832d5f63d80dfaf32d83b521ac67f83b8969f7d
Maxime Riaud <maxime.riaud@datadoghq.com>
2024-12-02 16:34:06 +0100

    update the authtoken component to provide client and server tls config based on the certificate/key stored on the disk

--- comp/api/authtoken/fetchonlyimpl/authtoken.go
+++ comp/api/authtoken/fetchonlyimpl/authtoken.go
@@ -8,7 +8,14 @@
 package fetchonlyimpl
 
 import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"time"
+
 	"go.uber.org/fx"
+
+	"github.com/cenkalti/backoff"
 
 	"github.com/DataDog/datadog-agent/comp/api/authtoken"
 	"github.com/DataDog/datadog-agent/comp/core/config"
@@ -22,13 +29,20 @@ func Module() fxutil.Module {
 	return fxutil.Component(
 		fx.Provide(newAuthToken),
 		fxutil.ProvideOptional[authtoken.Component](),
+
+		// Force the component to be constructed to run the periodic auth_token fetch.
+		// This approach is used because the coreAgent is currently responsible for auth_token generation,
+		// but there is no defined startup sequence between Agent processes.
+		// Some functions depend on the auth_token being initialized to work correctly.
+		// Running a background routine that periodically fetches the auth_token until it is retrieved
+		// helps reduce the number of cases where it is used uninitialized.
+		fx.Invoke(func(_ authtoken.Component) {}),
 	)
 }
 
 type authToken struct {
-	log  log.Component
-	conf config.Component
-
+	log         log.Component
+	conf        config.Component
 	tokenLoaded bool
 }
 
@@ -39,26 +53,96 @@ type dependencies struct {
 
 	Log  log.Component
 	Conf config.Component
+	Lc   fx.Lifecycle
 }
 
 func newAuthToken(deps dependencies) authtoken.Component {
-	return &authToken{
+
+	// Create a ticker that triggers every 5 seconds.
+	expBackoff := backoff.NewExponentialBackOff()
+	expBackoff.InitialInterval = 2 * time.Second
+	expBackoff.MaxInterval = 60 * time.Second
+	expBackoff.MaxElapsedTime = 5 * time.Minute
+	expBackoff.Reset()
+	ticker := backoff.NewTicker(expBackoff)
+
+	// Create a channel to signal the goroutine to stop.
+	stopChan := make(chan struct{})
+
+	comp := authToken{
 		log:  deps.Log,
 		conf: deps.Conf,
 	}
+
+	deps.Lc.Append(fx.Hook{
+		OnStart: func(_ context.Context) error {
+			deps.Log.Debugf("starting auth_token periodic fetch until getting it")
+			go func() {
+				for {
+					select {
+					case <-ticker.C:
+						err := util.SetAuthToken(deps.Conf)
+						if err == nil {
+							deps.Log.Infof("auth_token have been initialized")
+							comp.tokenLoaded = true
+							return
+						}
+						deps.Log.Infof("auth_token not initialized yet: %v", err.Error())
+					case <-stopChan:
+						return
+					}
+				}
+			}()
+			return nil
+		},
+		OnStop: func(_ context.Context) error {
+			ticker.Stop()
+			close(stopChan)
+			return nil
+		},
+	})
+
+	return &comp
 }
 
-// Get returns the session token
-func (at *authToken) Get() string {
+func (at *authToken) setToken() error {
 	if !at.tokenLoaded {
 		// We try to load the auth_token until we succeed since it might be created at some point by another
 		// process.
 		if err := util.SetAuthToken(at.conf); err != nil {
-			at.log.Debugf("could not load auth_token: %s", err)
-			return ""
+			return fmt.Errorf("could not load auth_token: %s", err)
 		}
 		at.tokenLoaded = true
 	}
+	return nil
+}
+
+// Get returns the session token
+func (at *authToken) Get() string {
+	if err := at.setToken(); err != nil {
+		at.log.Debugf(err.Error())
+		return ""
+	}
 
 	return util.GetAuthToken()
+}
+
+// GetTLSClientConfig return a TLS configuration with the IPC certificate for http.Client
+func (at *authToken) GetTLSClientConfig() *tls.Config {
+	if err := at.setToken(); err != nil {
+		at.log.Debugf(err.Error())
+		return nil
+	}
+
+	return util.GetTLSClientConfig()
+}
+
+// GetTLSServerConfig return a TLS configuration with the IPC certificate for http.Server
+func (at *authToken) GetTLSServerConfig() *tls.Config {
+	if err := at.setToken(); err != nil {
+		at.log.Debugf(err.Error())
+		return nil
+	}
+
+	return util.GetTLSServerConfig()
 }
--- comp/api/authtoken/fetchonlyimpl/authtoken_test.go
+++ comp/api/authtoken/fetchonlyimpl/authtoken_test.go
@@ -9,12 +9,17 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
-	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/fx"
 
+	"github.com/DataDog/datadog-agent/pkg/api/security/cert"
+	"github.com/DataDog/datadog-agent/pkg/api/util"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+
+	"github.com/DataDog/datadog-agent/comp/api/authtoken"
 	"github.com/DataDog/datadog-agent/comp/core/config"
 	log "github.com/DataDog/datadog-agent/comp/core/log/def"
 	logmock "github.com/DataDog/datadog-agent/comp/core/log/mock"
@@ -23,6 +28,7 @@ import (
 func TestGet(t *testing.T) {
 	dir := t.TempDir()
 	authPath := filepath.Join(dir, "auth_token")
+	var cfg config.Component
 	overrides := map[string]any{
 		"auth_token_file_path": authPath,
 	}
@@ -32,6 +38,7 @@ func TestGet(t *testing.T) {
 			t,
 			fx.Provide(func() log.Component { return logmock.New(t) }),
 			config.MockModule(),
+			fx.Populate(&cfg),
 			fx.Replace(config.MockParams{Overrides: overrides}),
 		),
 	).(*authToken)
@@ -42,7 +49,50 @@ func TestGet(t *testing.T) {
 	err := os.WriteFile(authPath, []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), 0777)
 	require.NoError(t, err)
 
+	// Should be empty because the cert/key weren't generated yet
+	assert.Empty(t, comp.Get())
+	assert.False(t, comp.tokenLoaded)
+
+	// generating IPC cert/key files
+	_, _, err = cert.CreateOrFetchAgentIPCCert(cfg)
+	require.NoError(t, err)
+
 	assert.Equal(t, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", comp.Get())
 	assert.True(t, comp.tokenLoaded)
 
+}
+
+func TestBackgroundFetcher(t *testing.T) {
+	dir := t.TempDir()
+	authPath := filepath.Join(dir, "auth_token")
+	var cfg config.Component
+	overrides := map[string]any{
+		"auth_token_file_path": authPath,
+	}
+
+	comp := fxutil.Test[authtoken.Component](
+		t,
+		fx.Provide(func() log.Component { return logmock.New(t) }),
+		config.MockModule(),
+		fx.Populate(&cfg),
+		fx.Replace(config.MockParams{Overrides: overrides}),
+		Module(),
+	)
+
+	// Simulate external auth_token creation
+	go func() {
+		time.Sleep(time.Second * 5)
+
+		err := os.WriteFile(authPath, []byte("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), 0777)
+		require.NoError(t, err)
+
+		// generating IPC cert/key files
+		_, _, err = cert.CreateOrFetchAgentIPCCert(cfg)
+		require.NoError(t, err)
+	}()
+
+	assert.EventuallyWithT(t, func(c *assert.CollectT) {
+		assert.True(c, util.IsInitialized())
+		assert.Equal(c, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", comp.Get())
+	}, 20*time.Second, 1*time.Second)
 }
