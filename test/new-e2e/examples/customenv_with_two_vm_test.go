c48c368e0241dc7d0f9b7f93f24eb4a5808e555a
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-12-02 21:38:26 +0100

    revert test/new-e2e/examples/customenv_with_two_vm_test.go

--- test/new-e2e/examples/customenv_with_two_vm_test.go
+++ test/new-e2e/examples/customenv_with_two_vm_test.go
@@ -10,22 +10,17 @@ import (
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/components"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
-	"github.com/DataDog/test-infra-definitions/components/datadog/agent"
-	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
+
 	"github.com/DataDog/test-infra-definitions/components/os"
 	"github.com/DataDog/test-infra-definitions/resources/aws"
 	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
-	"github.com/DataDog/test-infra-definitions/scenarios/aws/fakeintake"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type multiVMEnv struct {
-	MainVM     *components.RemoteHost
-	AppVM      *components.RemoteHost
-	MainAgent  *components.RemoteHostAgent
-	AppAgent   *components.RemoteHostAgent
-	FakeIntake *components.FakeIntake
+	MainVM *components.RemoteHost
+	AppVM  *components.RemoteHost
 }
 
 func multiVMEnvProvisioner() e2e.PulumiEnvRunFunc[multiVMEnv] {
@@ -41,42 +36,12 @@ func multiVMEnvProvisioner() e2e.PulumiEnvRunFunc[multiVMEnv] {
 		}
 		main.Export(ctx, &env.MainVM.HostOutput)
 
-		fakeIntake, err := fakeintake.NewECSFargateInstance(awsEnv, params.name, params.fakeintakeOptions...)
-		if err != nil {
-			return err
-		}
-		err = fakeIntake.Export(ctx, &env.FakeIntake.FakeintakeOutput)
-		if err != nil {
-			return err
-		}
-
-		agent1, err := agent.NewHostAgent(&awsEnv, main,
-			// DUALSHIP: change datadogYaml
-			//
-			agentparams.WithFakeintake(fakeIntake),
-			agentparams.WithAgentConfig(datadogYaml),
-			agentparams.WithFakeintake(datadogYaml),
-		)
-		if err != nil {
-			return err
-		}
-		agent1.Export(ctx, &env.MainAgent.HostAgentOutput)
-
 		app, err := ec2.NewVM(awsEnv, "app", ec2.WithOS(os.AmazonLinuxDefault))
 		if err != nil {
 			return err
 		}
 		app.Export(ctx, &env.AppVM.HostOutput)
 
-		agent2, err := agent.NewHostAgent(&awsEnv, main,
-			agentparams.WithFakeintake(datadogYaml),
-		)
-		if err != nil {
-			return err
-		}
-		agent2.Export(ctx, &env.AppAgent.HostAgentOutput)
-
-		// Need to install Agent (2 agent client)
 		return nil
 	}
 }
@@ -91,15 +56,7 @@ func TestMultiVMSuite(t *testing.T) {
 
 func (v *multiVMSuite) TestItIsExpectedOS() {
 	res := v.Env().MainVM.MustExecute("cat /etc/os-release")
-	res := v.Env().MainVM.MustExecute("systemctl stop datadog-agent")
-	res := v.Env().MainAgent.Client
 	v.Assert().Contains(res, "Ubuntu")
 	res = v.Env().AppVM.MustExecute("cat /etc/os-release")
 	v.Assert().Contains(res, "Amazon Linux")
-
-	fakeClient := v.Env().FakeIntake.Client()
-
-	// assert via backend
-	// idea:
-	//   - dual ship to take intake (used by containers)
 }
