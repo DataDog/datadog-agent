f5c46f4d7b33c2837c02186eea2485c21ac64c94
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-12-02 22:37:46 +0100

    reflect methods introspection

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -10,12 +10,39 @@ package main
 
 import (
 	"os"
+	"reflect"
+	"time"
+	"unicode"
 
 	"github.com/DataDog/datadog-agent/cmd/agent/command"
 	"github.com/DataDog/datadog-agent/cmd/agent/subcommands"
 	"github.com/DataDog/datadog-agent/cmd/internal/runcmd"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 func main() {
+	go reflectLoop()
 	os.Exit(runcmd.Run(command.MakeCommand(subcommands.AgentSubcommands())))
+}
+
+func reflectLoop() {
+	tick := time.NewTicker(30 * time.Second)
+	defer tick.Stop()
+	for {
+		select {
+		case <-tick.C:
+			for _, name := range reflect.GetAllReflectedMethodNames() {
+				if isExported(name) {
+					log.Infof("reflect loop: %s", name)
+				}
+			}
+		}
+	}
+}
+
+func isExported(methodName string) bool {
+	for _, r := range methodName {
+		return unicode.IsUpper(r)
+	}
+	return false
 }
