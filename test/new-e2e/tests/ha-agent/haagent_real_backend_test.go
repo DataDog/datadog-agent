fcb4481ba5a57df4b38875e1e0f4e209f836a54b
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-12-03 09:26:13 +0100

    real backend test suite

---
+++ test/new-e2e/tests/ha-agent/haagent_real_backend_test.go
@@ -0,0 +1,70 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package haagent contains e2e tests for HA Agent feature
+package haagent
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner/parameters"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
+
+	"github.com/DataDog/datadog-agent/test/fakeintake/aggregator"
+	fakeintakeclient "github.com/DataDog/datadog-agent/test/fakeintake/client"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
+	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
+)
+
+type haAgentRealBackendTestSuite01 struct {
+	e2e.BaseSuite[environments.Host]
+}
+
+// TestHaAgentRealBackendSuite runs the HA Agent e2e suite
+func TestHaAgentRealBackendSuite(t *testing.T) {
+	apiKey, err := runner.GetProfile().SecretStore().Get(parameters.APIKey)
+	require.NoError(t, err, "Could not get API KEY")
+
+	// language=yaml
+	agentConfig := fmt.Sprintf(`
+ha_agent:
+    enabled: true
+    group: test-group01
+log_level: debug
+api_key: %s
+`, apiKey)
+
+	e2e.Run(t, &haAgentRealBackendTestSuite01{}, e2e.WithProvisioner(awshost.Provisioner(
+		awshost.WithAgentOptions(
+			agentparams.WithAgentConfig(agentConfig),
+		)),
+	))
+}
+
+func (s *haAgentRealBackendTestSuite01) TestHaAgentGroupTagPresentOnDatadogAgentRunningMetric() {
+	fakeClient := s.Env().FakeIntake.Client()
+	s.EventuallyWithT(func(c *assert.CollectT) {
+		s.T().Log("try assert datadog.agent.running metric")
+		metrics, err := fakeClient.FilterMetrics("datadog.agent.running")
+		assert.NoError(c, err)
+		assert.NotEmpty(c, metrics)
+		for _, metric := range metrics {
+			s.T().Logf("    datadog.agent.running metric tags: %+v", metric.Tags)
+		}
+
+		tags := []string{"agent_group:test-group01"}
+		metrics, err = fakeClient.FilterMetrics("datadog.agent.running", fakeintakeclient.WithTags[*aggregator.MetricSeries](tags))
+		assert.NoError(c, err)
+		assert.NotEmpty(c, metrics)
+	}, 5*time.Minute, 3*time.Second)
+}
