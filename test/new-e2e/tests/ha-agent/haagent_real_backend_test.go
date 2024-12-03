170c89a4c1e4a7a6440b13087d1cd32a6b0eb968
Alexandre Yang <alexandre.yang@datadoghq.com>
2024-12-03 11:02:50 +0100

    working test

--- test/new-e2e/tests/ha-agent/haagent_real_backend_test.go
+++ test/new-e2e/tests/ha-agent/haagent_real_backend_test.go
@@ -9,62 +9,80 @@ package haagent
 import (
 	_ "embed"
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/runner/parameters"
+	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
-
-	"github.com/DataDog/datadog-agent/test/fakeintake/aggregator"
-	fakeintakeclient "github.com/DataDog/datadog-agent/test/fakeintake/client"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
 )
 
-type haAgentRealBackendTestSuite01 struct {
+type haAgentRealBackendTestSuite11 struct {
 	e2e.BaseSuite[environments.Host]
 }
 
 // TestHaAgentRealBackendSuite runs the HA Agent e2e suite
 func TestHaAgentRealBackendSuite(t *testing.T) {
-	apiKey, err := runner.GetProfile().SecretStore().Get(parameters.APIKey)
-	require.NoError(t, err, "Could not get API KEY")
+	apiKey := os.Getenv("DD_API_KEY") // Used for local testing
+	if apiKey == "" {
+		apiKeyFromSecreteStore, err := runner.GetProfile().SecretStore().Get(parameters.APIKey)
+		require.NoError(t, err, "Could not get API KEY")
+		apiKey = apiKeyFromSecreteStore
+	}
+
+	if apiKey != "" {
+		t.Logf("API_KEY: %s", apiKey[len(apiKey)-4:])
+	} else {
+		t.Logf("API_KEY empty")
+	}
 
 	// language=yaml
 	agentConfig := fmt.Sprintf(`
 ha_agent:
     enabled: true
-    group: test-group01
+    group: test-group-%s
 log_level: debug
 api_key: %s
-`, apiKey)
+site: datad0g.com
+`, uuid.New().String(), apiKey)
 
-	e2e.Run(t, &haAgentRealBackendTestSuite01{}, e2e.WithProvisioner(awshost.Provisioner(
+	// language=yaml
+	snmpIntegration := `
+init_config:
+  loader: core
+  use_device_id_as_hostname: true
+instances:
+- ip_address: '1.2.3.4'
+  community_string: 'sample-string'
+`
+
+	e2e.Run(t, &haAgentRealBackendTestSuite11{}, e2e.WithProvisioner(awshost.Provisioner(
 		awshost.WithAgentOptions(
 			agentparams.WithAgentConfig(agentConfig),
-		)),
+			agentparams.WithSkipAPIKeyInConfig(),
+			agentparams.WithIntegration("snmp.d", snmpIntegration),
+		),
+		awshost.WithoutFakeIntake(),
+	),
 	))
 }
 
-func (s *haAgentRealBackendTestSuite01) TestHaAgentGroupTagPresentOnDatadogAgentRunningMetric() {
-	fakeClient := s.Env().FakeIntake.Client()
+func (s *haAgentRealBackendTestSuite11) TestSnmpCheckIsRunningOnLeaderAgent() {
 	s.EventuallyWithT(func(c *assert.CollectT) {
-		s.T().Log("try assert datadog.agent.running metric")
-		metrics, err := fakeClient.FilterMetrics("datadog.agent.running")
-		assert.NoError(c, err)
-		assert.NotEmpty(c, metrics)
-		for _, metric := range metrics {
-			s.T().Logf("    datadog.agent.running metric tags: %+v", metric.Tags)
+		s.T().Log("try assert snmp check is running")
+		output, err := s.Env().RemoteHost.Execute("cat /var/log/datadog/agent.log")
+		if !assert.NoError(c, err) {
+			return
 		}
-
-		tags := []string{"agent_group:test-group01"}
-		metrics, err = fakeClient.FilterMetrics("datadog.agent.running", fakeintakeclient.WithTags[*aggregator.MetricSeries](tags))
-		assert.NoError(c, err)
-		assert.NotEmpty(c, metrics)
+		assert.Contains(c, output, "check:snmp | Check is an HA integration and current agent is not leader, skipping execution...")
+		assert.Contains(c, output, "check:snmp | Running check...")
 	}, 5*time.Minute, 3*time.Second)
 }
