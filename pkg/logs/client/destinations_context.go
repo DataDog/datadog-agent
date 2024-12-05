6039f93726a60b1cac028395d2e62cb40b0676d9
Remy Mathieu <remy.mathieu@datadoghq.com>
2024-12-05 14:49:22 +0100

    logs: use `ConnectDone` instead of `TLSHandshakeDone`

--- pkg/logs/client/destinations_context.go
+++ pkg/logs/client/destinations_context.go
@@ -7,7 +7,6 @@ package client
 
 import (
 	"context"
-	"crypto/tls"
 	"net/http/httptrace"
 	"sync"
 
@@ -59,7 +58,7 @@ func (dc *DestinationsContext) Context() context.Context {
 // GetClientTrace is an httptrace.ClientTrace instance that traces the events within HTTP client requests.
 func GetClientTrace() *httptrace.ClientTrace {
 	return &httptrace.ClientTrace{
-		TLSHandshakeDone: func(_ tls.ConnectionState, _ error) {
+		ConnectDone: func(_ string, _ string, _ error) {
 			metrics.TlmConnectionsOpened.Inc()
 		},
 	}
