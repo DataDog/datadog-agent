724c15683f49f78f4cc2c2823437e7e3950c0f17
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2024-12-05 16:05:10 +0100

    Create IntegrationTestsConfig and IntegrationTest dataclasses

--- tasks/gointegrationtest.py
+++ tasks/gointegrationtest.py
@@ -1,12 +1,86 @@
 import os
 import sys
 import traceback
+from dataclasses import dataclass
 
 from invoke import task
 from invoke.exceptions import Exit
 
 from tasks.build_tags import get_default_build_tags
 from tasks.libs.common.utils import TestsNotSupportedError, gitlab_section
+
+
+@dataclass
+class IntegrationTest:
+    """
+    Integration tests
+    """
+
+    prefix: str
+    dir: str = None
+    extra_args: str = None
+
+
+@dataclass
+class IntegrationTestsConfig:
+    """
+    Integration tests configuration
+    """
+
+    name: str
+    go_build_tags: list[str]
+    tests: list[IntegrationTest]
+    env: dict[str, str] = None
+
+
+CORE_AGENT_LINUX = IntegrationTestsConfig(
+    name="Core Agent Linux",
+    go_build_tags=get_default_build_tags(build="test"),
+    tests=[
+        IntegrationTest(prefix="./test/integration/config_providers/..."),
+        IntegrationTest(prefix="./test/integration/corechecks/..."),
+        IntegrationTest(prefix="./test/integration/listeners/..."),
+        IntegrationTest(prefix="./test/integration/util/kubelet/..."),
+    ],
+)
+
+CORE_AGENT_WINDOWS = IntegrationTestsConfig(
+    name="Core Agent Windows",
+    go_build_tags=get_default_build_tags(build="test"),
+    tests=[
+        # Run eventlog tests with the Windows API, which depend on the EventLog service
+        IntegrationTest(dir="./pkg/util/winutil/", prefix="./eventlog/...", extra_args="-evtapi Windows"),
+        # Run eventlog tailer tests with the Windows API, which depend on the EventLog service
+        IntegrationTest(dir=".", prefix="./pkg/logs/tailers/windowsevent/...", extra_args="-evtapi Windows"),
+        # Run eventlog check tests with the Windows API, which depend on the EventLog service
+        # Don't include submodules, since the `-evtapi` flag is not defined in them
+        IntegrationTest(
+            dir=".", prefix="./comp/checks/windowseventlog/windowseventlogimpl/check", extra_args="-evtapi Windows"
+        ),
+    ],
+)
+
+DOGSTATSD = IntegrationTestsConfig(
+    name="DogStatsD",
+    go_build_tags=get_default_build_tags(build="test"),
+    tests=[IntegrationTest(prefix="./test/integration/dogstatsd/...")],
+)
+
+CLUSTER_AGENT = IntegrationTestsConfig(
+    name="Cluster Agent",
+    go_build_tags=get_default_build_tags(build="cluster-agent") + ["docker", "test"],
+    tests=[
+        IntegrationTest(prefix="./test/integration/util/kube_apiserver"),
+        IntegrationTest(prefix="./test/integration/util/leaderelection"),
+    ],
+)
+
+TRACE_AGENT = IntegrationTestsConfig(
+    name="Trace Agent",
+    go_build_tags=get_default_build_tags(build="test"),
+    tests=[IntegrationTest(prefix="./cmd/trace-agent/test/testsuite/...")],
+    env={"INTEGRATION": "yes"},
+)
 
 
 def containerized_integration_tests(
