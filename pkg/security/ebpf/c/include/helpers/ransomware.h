1507bab05af6a19647e7ea2ee92d8fe4acf624dd
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2024-12-06 15:09:03 +0100

    some fixes and adjustments

--- pkg/security/ebpf/c/include/helpers/ransomware.h
+++ pkg/security/ebpf/c/include/helpers/ransomware.h
@@ -7,8 +7,8 @@
 #include "constants/macros.h"
 
 #define RANSOMWARE_SCORE_NEW_FILE 1
-#define RANSOMWARE_SCORE_UNLINK 10
-#define RANSOMWARE_SCORE_RENAME 10
+#define RANSOMWARE_SCORE_UNLINK 5
+#define RANSOMWARE_SCORE_RENAME 5
 #define RANSOMWARE_SCORE_URANDOM 100
 #define RANSOMWARE_SCORE_KILL 100
 
@@ -16,15 +16,16 @@
 #define RANSOMWARE_THRESHOLD_SCORE 500
 
 __attribute__((always_inline)) void ransomware_cleanup(u32 pid) {
-    bpf_map_lookup_elem(&ransomware_score, &pid);
+    bpf_map_delete_elem(&ransomware_score, &pid);
     return;
 }
 
-__attribute__((always_inline)) void reset_score(struct ransomware_score_t *rs) {
+__attribute__((always_inline)) void reset_score(struct ransomware_score_t *rs, u64 ts) {
     if (!rs) {
         return; // should not happen
     }
     __builtin_memset(rs, 0, sizeof(*rs));
+    rs->first_syscall = ts;
     return;
 }
 
@@ -38,11 +39,16 @@ __attribute__((always_inline)) void compute_score(ctx_t *ctx, struct ransomware_
     score += rs->kill * RANSOMWARE_SCORE_KILL;
 
     if (score < RANSOMWARE_THRESHOLD_SCORE) {
-        /* bpf_printk("-- new score: %u", score); */
         return;
     }
 
     u64 diff_time = rs->last_syscall - rs->first_syscall;
+    if (diff_time > RANSOMWARE_WATCH_PERIOD_NS) {
+        // if it took more than 1sec to reach the threshold, reset and continue
+        reset_score(rs, rs->last_syscall);
+        return;
+    }
+
     bpf_printk("== THRESHOLD REACHED with score: %u in %u.%u seconds", score, NS_TO_SEC(diff_time), diff_time%1000000000);
     bpf_printk("  new_files: %u", rs->new_file);
     bpf_printk("  unlinks:   %u", rs->unlink);
@@ -59,11 +65,8 @@ __attribute__((always_inline)) void compute_score(ctx_t *ctx, struct ransomware_
         .kill = rs->kill,
         .score = score,
     };
-
     /* rs->already_notified = 1; */
-    u64 last = rs->last_syscall; //DEBUG, TODO: REDO ^
-    reset_score(rs); //DEBUG, TODO: REDO ^
-    rs->first_syscall = last; //DEBUG, TODO: REDO ^
+    reset_score(rs, rs->last_syscall); //DEBUG, TODO: REDO ^?
 
     struct proc_cache_t *entry = fill_process_context(&event.process);
     fill_container_context(entry, &event.container);
@@ -74,11 +77,12 @@ __attribute__((always_inline)) void compute_score(ctx_t *ctx, struct ransomware_
 
 __attribute__((always_inline)) struct ransomware_score_t* ransomware_get_score() {
     u32 pid = bpf_get_current_pid_tgid() >> 32;
+    u64 now = bpf_ktime_get_ns();
 
     struct ransomware_score_t *score = bpf_map_lookup_elem(&ransomware_score, &pid);
     if (!score) {
         struct ransomware_score_t s;
-        reset_score(&s);
+        reset_score(&s, now);
         if (bpf_map_update_elem(&ransomware_score, &pid, &s, BPF_ANY) < 0) {
             bpf_printk("ransomware_get_score failed to update elem");
             return NULL;
@@ -88,10 +92,8 @@ __attribute__((always_inline)) struct ransomware_score_t* ransomware_get_score()
         return NULL;
     }
 
-    u64 now = bpf_ktime_get_ns();
     if (score->last_syscall + RANSOMWARE_WATCH_PERIOD_NS < now) {
-        reset_score(score);
-        score->first_syscall = now;
+        reset_score(score, now);
     }
     score->last_syscall = now;
     return score;
@@ -103,7 +105,6 @@ __attribute__((always_inline)) void ransomware_score_unlink(ctx_t *ctx) {
     if (!score) {
         return;
     }
-    /* bpf_printk("++ unlink"); */
     score->unlink++;
     compute_score(ctx, score);
     return;
@@ -114,7 +115,6 @@ __attribute__((always_inline)) void ransomware_score_rename(ctx_t *ctx) {
     if (!score) {
         return;
     }
-    /* bpf_printk("++ rename"); */
     score->rename++;
     compute_score(ctx, score);
     return;
@@ -129,7 +129,6 @@ __attribute__((always_inline)) void ransomware_score_open(ctx_t *ctx, int flags)
     if (!score) {
         return;
     }
-    /* bpf_printk("++ open new file"); */
     score->new_file++;
     compute_score(ctx, score);
     return;
@@ -144,7 +143,6 @@ __attribute__((always_inline)) void ransomware_score_kill(ctx_t *ctx, int sig) {
     if (!score) {
         return;
     }
-    /* bpf_printk("++ kill"); */
     score->kill++;
     compute_score(ctx, score);
     return;
