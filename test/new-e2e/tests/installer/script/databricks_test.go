41138d9db409b710bbaf66ff73659f8cb26dcfc1
Raphael Gavache <raphael.gavache@datadoghq.com>
2024-12-10 13:17:29 +0100

    first test

--- test/new-e2e/tests/installer/script/databricks_test.go
+++ test/new-e2e/tests/installer/script/databricks_test.go
@@ -5,8 +5,30 @@
 
 package installscript
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
 
-func TestDJMTest(t *testing.T) {
-	t.Fatal("not implemented")
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
+	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/host"
+	osdesc "github.com/DataDog/test-infra-definitions/components/os"
+	"github.com/DataDog/test-infra-definitions/scenarios/aws/ec2"
+)
+
+type vmUpdaterSuite struct {
+	commitHash string
+	e2e.BaseSuite[environments.Host]
+}
+
+func (s *vmUpdaterSuite) TestInstallScript() {
+	url := fmt.Sprintf("https://installtesting.datad0g.com/%s/scripts/install-databricks.sh", s.commitHash)
+	s.Env().RemoteHost.MustExecute(fmt.Sprintf("curl -L %s > install_script; export DD_INSTALLER_REGISTRY_URL_INSTALLER_PACKAGE=installtesting.datad0g.com; sudo -E bash install_script", url))
+}
+
+func TestUpdaterSuite(t *testing.T) {
+	e2e.Run(t, &vmUpdaterSuite{commitHash: os.Getenv("CI_COMMIT_SHA")}, e2e.WithProvisioner(awshost.ProvisionerNoFakeIntake(
+		awshost.WithEC2InstanceOptions(ec2.WithOSArch(osdesc.UbuntuDefault, osdesc.ARM64Arch)),
+	)))
 }
