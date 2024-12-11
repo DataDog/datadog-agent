965e710210ef3591e207bf768ef8b85b2465053a
Paul Cacheux <paul.cacheux@datadoghq.com>
2024-12-11 13:33:14 +0100

    workloadmeta: create remote only catalog

--- comp/core/workloadmeta/collectors/catalog-remote/options.go
+++ comp/core/workloadmeta/collectors/catalog-remote/options.go
@@ -3,8 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build remotewmonly
-
 // Package collectors is a wrapper that loads the available workloadmeta
 // collectors. It exists as a shorthand for importing all packages manually in
 // all of the agents.
@@ -14,14 +12,30 @@ import (
 	"go.uber.org/fx"
 
 	remoteworkloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/collectors/internal/remote/workloadmeta"
+	workloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/def"
+	"github.com/DataDog/datadog-agent/pkg/util/flavor"
 )
-
-// TODO: (components) Move remote-only to its own catalog, similar to how catalog-less works
-// Depend on this catalog-remote using fx, instead of build tags
 
 func getCollectorOptions() []fx.Option {
 	return []fx.Option{
 		remoteworkloadmeta.GetFxOptions(),
 		remoteWorkloadmetaParams(),
 	}
+}
+
+func remoteWorkloadmetaParams() fx.Option {
+	var filter *workloadmeta.Filter // Nil filter accepts everything
+
+	// Security Agent is only interested in containers
+	// TODO: (components) create a Catalog component, the implementation used by
+	// security-agent can use this filter, instead of needing to chekc agent.flavor
+	if flavor.GetFlavor() == flavor.SecurityAgent {
+		filter = workloadmeta.NewFilterBuilder().AddKind(workloadmeta.KindContainer).Build()
+	}
+
+	return fx.Provide(func() remoteworkloadmeta.Params {
+		return remoteworkloadmeta.Params{
+			Filter: filter,
+		}
+	})
 }
