0aee941c42fab28450e898d5fc75ddef7afc56ff
Branden Clark <branden.clark@datadoghq.com>
2024-12-11 16:27:11 -0500

    reorg choco files to prep for fips flavor
    Build package in temp dir to avoid modifying source

--- tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
+++ tasks/winbuildscripts/Generate-Chocolatey-Package.ps1
@@ -18,7 +18,8 @@ $env:chocolateyUseWindowsCompression = 'true'; Invoke-Expression ((New-Object Sy
 # Install dev tools, including invoke
 pip3 install -r requirements.txt
 
-$outputDirectory = "c:\mnt\build-out"
+$repoRoot = "C:\mnt"
+$outputDirectory = "$repoRoot\build-out"
 $rawAgentVersion = (inv agent.version --url-safe --major-version 7)
 $copyright = "Datadog {0}" -f (Get-Date).Year
 
@@ -26,72 +27,85 @@ $releasePattern = "(\d+\.\d+\.\d+)"
 $releaseCandidatePattern = "(\d+\.\d+\.\d+)-rc\.(\d+)"
 $develPattern = "(\d+\.\d+\.\d+)-devel\.git\.\d+\.(.+)"
 
-$nuspecFile = "c:\mnt\chocolatey\datadog-agent-online.nuspec"
-$licensePath = "c:\mnt\chocolatey\tools-online\LICENSE.txt"
-$installScript = "c:\mnt\chocolatey\tools-online\chocolateyinstall.ps1"
-
-if ($installMethod -eq "offline") {
-    $nuspecFile = "c:\mnt\chocolatey\datadog-agent-offline.nuspec"
-    $licensePath = "c:\mnt\chocolatey\tools-offline\LICENSE.txt"
+# Build the package in a temporary directory
+# Some of the build steps modify the package source, so we don't want to do this in the source directory
+$buildTempDir = [System.IO.Path]::GetTempPath() + "\datadog-choco-build"
+if (Test-Path $buildTempDir) {
+    Remove-Item -Recurse -Force $buildTempDir
 }
+New-Item -ItemType Directory -Path $buildTempDir | Out-Null
+Push-Location -Path $buildTempDir
+try {
+    # For historical reasons, use a different artifact name for the datadog-agent flavor
+    # See agent-release-mangement for more details
+    $artifactName = "ddagent-cli"
+    $packageSource = "$repoRoot\chocolatey\datadog-agent\$installMethod"
+    $nuspecFile = "datadog-agent-$installMethod.nuspec"
 
-if ($rawAgentVersion -match $releaseCandidatePattern) {
-    $agentVersionMatches = $rawAgentVersion | Select-String -Pattern $releaseCandidatePattern
-    $agentVersion = "{0}-rc-{1}" -f $agentVersionMatches.Matches.Groups[1], $agentVersionMatches.Matches.Groups[2].Value
-    # We don't have release notes for RCs but this way the user can always see what commits are included in this RC
-    $releaseNotes = "https://github.com/DataDog/datadog-agent/releases/tag/{0}-rc.{1}" -f $agentVersionMatches.Matches.Groups[1], $agentVersionMatches.Matches.Groups[2]
-    $url = "https://s3.amazonaws.com/dd-agent-mstesting/builds/beta/ddagent-cli-$($agentVersionMatches.Matches.Groups[1])-rc.$($agentVersionMatches.Matches.Groups[2]).msi"
-} elseif ($rawAgentVersion -match $develPattern) {
-    if ($installMethod -eq "online") {
-        # We don't publish online chocolatey packages for dev branches, error out
-        Write-Host "Chocolatey packages are not built for dev branches aborting"
-        exit 2
-    }
-    $agentVersionMatches = $rawAgentVersion | Select-String -Pattern $develPattern
-    $agentVersion = "{0}-devel-{1}" -f $agentVersionMatches.Matches.Groups[1], $agentVersionMatches.Matches.Groups[2].Value
-    # We don't have release notes for devel, so point it to the generic url
-    $releaseNotes = "https://github.com/DataDog/datadog-agent/releases"
-} elseif ($rawAgentVersion -match $releasePattern) {
-    $agentVersionMatches = $rawAgentVersion | Select-String -Pattern $releasePattern
-    $agentVersion = $agentVersionMatches.Matches.Groups[1].Value
-    $releaseNotes = "https://github.com/DataDog/datadog-agent/releases/tag/$agentVersion"
-    $url = "https://s3.amazonaws.com/ddagent-windows-stable/ddagent-cli-$($agentVersion).msi"
-} else {
-    Write-Host "Unknown agent version '$rawAgentVersion', aborting"
-    exit 3
-}
+    # These files/directories are referenced in the nuspec file
+    $licensePath = "tools\LICENSE.txt"
+    $installScript = "tools\chocolateyinstall.ps1"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DataDog/datadog-agent/main/LICENSE" -OutFile $licensePath
+    # Copy package source to build temp dir
+    Copy-Item -Recurse -Force -Path $packageSource\* -Destination $buildTempDir
+    Copy-Item -Force -Path $repoRoot\LICENSE -Destination $licensePath
 
-Write-Host "Generating Chocolatey $installMethod package version $agentVersion in $outputDirectory"
-
-if (!(Test-Path $outputDirectory)) {
-    New-Item -ItemType Directory -Path $outputDirectory
-}
-
-if ($installMethod -eq "online") {
-    try {
-        $tempMsi = Join-Path -Path "$msiDirectory" "datadog-agent-$rawAgentVersion-1-x86_64.msi"
-        if (!(Test-Path $tempMsi)) {
-            Write-Host "Error: Could not find MSI file in $tempMsi"
-            Get-ChildItem "$msiDirectory"
-            exit 1 
+    # Generate URLs based on the Agent version
+    if ($rawAgentVersion -match $releaseCandidatePattern) {
+        $agentVersionMatches = $rawAgentVersion | Select-String -Pattern $releaseCandidatePattern
+        $agentVersion = "{0}-rc-{1}" -f $agentVersionMatches.Matches.Groups[1], $agentVersionMatches.Matches.Groups[2].Value
+        # We don't have release notes for RCs but this way the user can always see what commits are included in this RC
+        $releaseNotes = "https://github.com/DataDog/datadog-agent/releases/tag/{0}-rc.{1}" -f $agentVersionMatches.Matches.Groups[1], $agentVersionMatches.Matches.Groups[2]
+        $url = "https://s3.amazonaws.com/dd-agent-mstesting/builds/beta/$artifactName-$($agentVersionMatches.Matches.Groups[1])-rc.$($agentVersionMatches.Matches.Groups[2]).msi"
+    } elseif ($rawAgentVersion -match $develPattern) {
+        if ($installMethod -eq "online") {
+            # We don't publish online chocolatey packages for dev branches, error out
+            Write-Host "Chocolatey packages are not built for dev branches aborting"
+            exit 2
         }
-        $checksum = (Get-FileHash $tempMsi -Algorithm SHA256).Hash
-    } 
-    catch {
-        Write-Host "Error: Could not generate checksum for package $($tempMsi): $($_)"
-        exit 4
+        $agentVersionMatches = $rawAgentVersion | Select-String -Pattern $develPattern
+        $agentVersion = "{0}-devel-{1}" -f $agentVersionMatches.Matches.Groups[1], $agentVersionMatches.Matches.Groups[2].Value
+        # We don't have release notes for devel, so point it to the generic url
+        $releaseNotes = "https://github.com/DataDog/datadog-agent/releases"
+    } elseif ($rawAgentVersion -match $releasePattern) {
+        $agentVersionMatches = $rawAgentVersion | Select-String -Pattern $releasePattern
+        $agentVersion = $agentVersionMatches.Matches.Groups[1].Value
+        $releaseNotes = "https://github.com/DataDog/datadog-agent/releases/tag/$agentVersion"
+        $url = "https://s3.amazonaws.com/ddagent-windows-stable/$artifactName-$($agentVersion).msi"
+    } else {
+        Write-Host "Unknown agent version '$rawAgentVersion', aborting"
+        exit 3
     }
-    # Set the $url in the install script
-    (Get-Content $installScript).replace('$__url_from_ci__', '"' +  $url  + '"').replace('$__checksum_from_ci__', '"' +  $checksum  + '"') | Set-Content $installScript
+
+    Write-Host "Generating Chocolatey $installMethod package $flavor version $agentVersion in $(Get-Location)"
+
+    # Template the install script with the URL and checksum
+    if ($installMethod -eq "online") {
+        try {
+            $tempMsi = Join-Path -Path "$msiDirectory" "$flavor-$rawAgentVersion-1-x86_64.msi"
+            if (!(Test-Path $tempMsi)) {
+                Write-Host "Error: Could not find MSI file in $tempMsi"
+                Get-ChildItem "$msiDirectory"
+                exit 1
+            }
+            $checksum = (Get-FileHash $tempMsi -Algorithm SHA256).Hash
+        }
+        catch {
+            Write-Host "Error: Could not generate checksum for package $($tempMsi): $($_)"
+            exit 4
+        }
+        # Set the $url in the install script
+        (Get-Content $installScript).replace('$__url_from_ci__', '"' +  $url  + '"').replace('$__checksum_from_ci__', '"' +  $checksum  + '"') | Set-Content $installScript
+    }
+
+    Write-Host "Generated nupsec file:"
+    Write-Host (Get-Content $installScript | Out-String)
+
+    if (!(Test-Path $outputDirectory)) {
+        New-Item -ItemType Directory -Path $outputDirectory
+    }
+    Write-Host choco pack --out=$outputDirectory $nuspecFile --version $agentVersion release_notes=$releaseNotes copyright=$copyright
+    choco pack --out=$outputDirectory $nuspecFile --version $agentVersion release_notes=$releaseNotes copyright=$copyright
+} finally {
+    Pop-Location
 }
-
-Write-Host "Generated nupsec file:"
-Write-Host (Get-Content $installScript | Out-String)
-
-Write-Host choco pack --out=$outputDirectory $nuspecFile --version $agentVersion release_notes=$releaseNotes copyright=$copyright
-choco pack --out=$outputDirectory $nuspecFile --version $agentVersion release_notes=$releaseNotes copyright=$copyright
-
-# restore installScript (useful for local testing/deployment)
-git checkout $installScript
