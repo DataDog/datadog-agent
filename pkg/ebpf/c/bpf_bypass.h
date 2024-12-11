be901bcd6bfe59c2576143d614e76c321ec871c7
usamasaqib <usamasaqib.96@live.com>
2024-12-11 17:00:07 +0100

    cleanup backslashes

--- pkg/ebpf/c/bpf_bypass.h
+++ pkg/ebpf/c/bpf_bypass.h
@@ -9,55 +9,55 @@
 BPF_ARRAY_MAP(program_bypassed, u32, 1)
 
 // instruct clang that r0-r5 are clobbered, because we are going to make a helper call
-#define CHECK_BPF_PROGRAM_BYPASSED() \
-    unsigned long bypass_program; \
+#define CHECK_BPF_PROGRAM_BYPASSED()    \
+    unsigned long bypass_program;       \
     asm("%0 = " "bypass_program" " ll" : "=r"(bypass_program) :: "memory");
 
 #define DO_BYPASS CHECK_BPF_PROGRAM_BYPASSED
 
-#define DO_WITH1(a) \
+#define DO_WITH1(a)                             \
     DO_##a()
 
-#define DO_WITH2(a, b) \
-        DO_WITH1(a) \
-        DO_WITH1(b) \
+#define DO_WITH2(a, b)                          \
+        DO_WITH1(a)                             \
+        DO_WITH1(b)                             \
 
-#define DO_WITHx(x,...) \
-        DO_WITH##x(__VA_ARGS__) \
+#define DO_WITHx(x,...)                         \
+        DO_WITH##x(__VA_ARGS__)                 \
 
-#define _WITH(x,...) \
+#define _WITH(x,...)                            \
         DO_WITHx(x, __VA_ARGS__)
 
-#define WITH(...) \
+#define WITH(...)                               \
         _WITH(NARGS(__VA_ARGS__), __VA_ARGS__)
 
-#define BPF_KPROBE_MODIFY(preamble, name, args...) \
-    name(struct pt_regs *ctx);						    \
+#define BPF_KPROBE_MODIFY(preamble, name, args...)              \
+    name(struct pt_regs *ctx);						            \
     static __always_inline typeof(name(0))					    \
     ____##name(struct pt_regs *ctx, ##args);				    \
     typeof(name(0)) name(struct pt_regs *ctx)				    \
-    {									    \
-        preamble; \
-    	_Pragma("GCC diagnostic push")					    \
-    	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"")		    \
-    	return ____##name(___bpf_kprobe_args(args));			    \
-    	_Pragma("GCC diagnostic pop")					    \
-    }									    \
+    {									                        \
+        preamble;                                               \
+    	_Pragma("GCC diagnostic push")					        \
+    	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"")  \
+    	return ____##name(___bpf_kprobe_args(args));			\
+    	_Pragma("GCC diagnostic pop")					        \
+    }									                        \
     static __always_inline typeof(name(0))					    \
     ____##name(struct pt_regs *ctx, ##args)
 
-#define BPF_KRETPROBE_MODIFY(preamble, name, args...) \
-    name(struct pt_regs *ctx);						    \
+#define BPF_KRETPROBE_MODIFY(preamble, name, args...)           \
+    name(struct pt_regs *ctx);						            \
     static __always_inline typeof(name(0))					    \
     ____##name(struct pt_regs *ctx, ##args);				    \
     typeof(name(0)) name(struct pt_regs *ctx)				    \
-    {									    \
-        preamble; \
-    	_Pragma("GCC diagnostic push")					    \
-    	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"")		    \
-    	return ____##name(___bpf_kretprobe_args(args));			    \
-    	_Pragma("GCC diagnostic pop")					    \
-    }									    \
+    {									                        \
+        preamble;                                               \
+    	_Pragma("GCC diagnostic push")					        \
+    	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"")  \
+    	return ____##name(___bpf_kretprobe_args(args));			\
+    	_Pragma("GCC diagnostic pop")					        \
+    }									                        \
     static __always_inline typeof(name(0)) ____##name(struct pt_regs *ctx, ##args)
 
 
