87b69020183a3c1b5610c82a86ba1e4410c4d17a
Sylvain Baubeau <sylvain.baubeau@datadoghq.com>
2024-12-11 20:56:49 +0100

    Resolve context using pid for CgroupTracing events

--- pkg/security/secl/model/model_unix.go
+++ pkg/security/secl/model/model_unix.go
@@ -624,6 +624,7 @@ type CgroupTracingEvent struct {
 	ContainerContext ContainerContext
 	CGroupContext    CGroupContext
 	Config           ActivityDumpLoadConfig
+	Pid              uint32
 	ConfigCookie     uint64
 }
 
