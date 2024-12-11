4526f49444f86ff7e8ec63cc2e76c017b61b9a77
usamasaqib <usamasaqib.96@live.com>
2024-12-11 15:10:32 +0100

    simplify preamble macros

--- pkg/ebpf/c/bpf_bypass.h
+++ pkg/ebpf/c/bpf_bypass.h
@@ -15,47 +15,29 @@ BPF_ARRAY_MAP(program_bypassed, u32, 1)
 
 #define DO_BYPASS CHECK_BPF_PROGRAM_BYPASSED
 
-#define DO_WITH1(name, a) \
-static __always_inline void ____bpf_preamble__##name##__##a() { \
-    DO_##a() \
-}\
+#define DO_WITH1(a) \
+    DO_##a()
 
-#define DO_WITH2(name, a, b) \
-        DO_WITH1(name, a) \
-        DO_WITH1(name, b) \
+#define DO_WITH2(a, b) \
+        DO_WITH1(a) \
+        DO_WITH1(b) \
 
-#define DO_WITH_CALL1(name, a) \
-        static __always_inline void ____bpf_preamble__##name() { \
-            ____bpf_preamble__##name##__##a(); \
-        }
+#define DO_WITHx(x,...) \
+        DO_WITH##x(__VA_ARGS__) \
 
-#define DO_WITH_CALL2(name, a, b) \
-        static __always_inline void ____bpf_preamble__##name() { \
-            ____bpf_preamble__##name##__##a(); \
-            ____bpf_preamble__##name##__##b(); \
-        }
-
-#define DO_WITHx(name, x,...) \
-        DO_WITH##x(name, __VA_ARGS__) \
-        DO_WITH_CALL##x(name, __VA_ARGS__)
-
-#define _WITH(name, x,...) \
-        DO_WITHx(name, x, __VA_ARGS__)
-
-#define WITH_PREAMBLE(name, ...) \
-        _WITH(name, nargs(__VA_ARGS__), __VA_ARGS__)
+#define _WITH(x,...) \
+        DO_WITHx(x, __VA_ARGS__)
 
 #define WITH(...) \
-    __VA_ARGS__
+        _WITH(nargs(__VA_ARGS__), __VA_ARGS__)
 
 #define BPF_KPROBE_INSTR(preamble, name, args...) \
     name(struct pt_regs *ctx);						    \
-    WITH_PREAMBLE(name, preamble) \
     static __always_inline typeof(name(0))					    \
     ____##name(struct pt_regs *ctx, ##args);				    \
     typeof(name(0)) name(struct pt_regs *ctx)				    \
     {									    \
-        ____bpf_preamble__##name(); \
+        preamble; \
     	_Pragma("GCC diagnostic push")					    \
     	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"")		    \
     	return ____##name(___bpf_kprobe_args(args));			    \
@@ -66,12 +48,11 @@ static __always_inline void ____bpf_preamble__##name##__##a() { \
 
 #define BPF_KRETPROBE_INSTR(preamble, name, args...) \
     name(struct pt_regs *ctx);						    \
-    WITH_PREAMBLE(name, preamble) \
     static __always_inline typeof(name(0))					    \
     ____##name(struct pt_regs *ctx, ##args);				    \
     typeof(name(0)) name(struct pt_regs *ctx)				    \
     {									    \
-        ____bpf_preamble__##name(); \
+        preamble; \
     	_Pragma("GCC diagnostic push")					    \
     	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"")		    \
     	return ____##name(___bpf_kretprobe_args(args));			    \
