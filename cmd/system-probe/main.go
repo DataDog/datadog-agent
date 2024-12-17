975100341b18885283192b00517243d65506a652
Lénaïc Huard <lenaic.huard@datadoghq.com>
2024-12-17 18:32:43 +0100

    Reduce agent RSS

--- cmd/system-probe/main.go
+++ cmd/system-probe/main.go
@@ -11,12 +11,15 @@ package main
 import (
 	"os"
 
+	"github.com/DataDog/datadog-agent/cmd/internal/rssshrinker"
 	"github.com/DataDog/datadog-agent/cmd/internal/runcmd"
 	"github.com/DataDog/datadog-agent/cmd/system-probe/command"
 	"github.com/DataDog/datadog-agent/cmd/system-probe/subcommands"
 )
 
 func main() {
+	rssshrinker.Setup()
+
 	rootCmd := command.MakeCommand(subcommands.SysprobeSubcommands())
 	command.SetDefaultCommandIfNonePresent(rootCmd)
 	os.Exit(runcmd.Run(rootCmd))
