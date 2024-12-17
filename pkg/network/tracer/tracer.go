45fe447cc66b7d9dd34c49983e3a005a021538e9
yuri.lipnesh <yuri.lipnesh@datadoghq.com>
2024-12-17 16:53:56 -0500

    [usm] reliability, preparation for pool of objects, add Tracer.ReleaseProtocolStats()

--- pkg/network/tracer/tracer.go
+++ pkg/network/tracer/tracer.go
@@ -460,6 +460,12 @@ func (t *Tracer) GetActiveConnections(clientID string) (*network.Connections, er
 	return conns, nil
 }
 
+// ReleaseProtocolStats releases usm stats objects.
+func (t *Tracer) ReleaseProtocolStats(_ *network.Connections) {
+	t.bufferLock.Lock()
+	defer t.bufferLock.Unlock()
+}
+
 // RegisterClient registers a clientID with the tracer
 func (t *Tracer) RegisterClient(clientID string) error {
 	t.state.RegisterClient(clientID)
--- pkg/network/tracer/tracer_windows.go
+++ pkg/network/tracer/tracer_windows.go
@@ -251,6 +251,10 @@ func (t *Tracer) GetActiveConnections(clientID string) (*network.Connections, er
 	return conns, nil
 }
 
+// ReleaseProtocolStats releases usm stats objects.
+func (t *Tracer) ReleaseProtocolStats(_ *network.Connections) {
+}
+
 // RegisterClient registers the client
 func (t *Tracer) RegisterClient(clientID string) error {
 	t.state.RegisterClient(clientID)
