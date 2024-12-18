05375e21612c68db05d3eb087ab8988b4cf8eb28
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2024-12-18 17:13:45 +0100

    restore previous build tag

--- pkg/collector/corechecks/system/cpu/cpu/cpu.go
+++ pkg/collector/corechecks/system/cpu/cpu/cpu.go
@@ -2,7 +2,7 @@
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
-//go:build linux || darwin
+//go:build !windows
 
 //nolint:revive // TODO(PLINT) Fix revive linter
 package cpu
