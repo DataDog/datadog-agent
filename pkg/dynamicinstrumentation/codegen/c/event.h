ad4a27ee07375c07a33eb43b84666d5c12b75012
grantseltzer <grantseltzer@gmail.com>
2024-12-19 16:01:00 -0600

    Add more comments
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/codegen/c/event.h
+++ pkg/dynamicinstrumentation/codegen/c/event.h
@@ -12,12 +12,12 @@ struct event {
 // expression_context contains state that is meant to be shared across location expressions
 // during execution of the full bpf program.
 struct expression_context {
-    __u64 *output_offset;
-    __u8 *stack_counter;
+    __u64 *output_offset; // current offset within the output buffer to write to
+    __u8 *stack_counter;  // current size of the bpf parameter stack, used for emptying stack
     struct pt_regs *ctx;
-    struct event *event;
-    __u64 *temp_storage;
-    char *zero_string;
+    struct event *event;  // output event allocated on ringbuffer
+    __u64 *temp_storage;  // temporary storage array on heap used by some location expressions
+    char *zero_string;    // array of zero's used to zero out buffers
 };
 
 #endif
