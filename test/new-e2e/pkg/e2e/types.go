56677e57449cf6d22d11f8beb91fd8128d085f1b
pducolin <paola.ducolin@datadoghq.com>
2024-12-20 12:42:03 +0100

    [e2e] remove RawResources, moved to provisioners

--- test/new-e2e/pkg/e2e/types.go
+++ test/new-e2e/pkg/e2e/types.go
@@ -4,13 +4,3 @@
 // Copyright 2016-present Datadog, Inc.
 
 package e2e
-
-// RawResources is the common types returned by provisioners
-type RawResources map[string][]byte
-
-// Merge merges two RawResources maps
-func (rr RawResources) Merge(in RawResources) {
-	for k, v := range in {
-		rr[k] = v
-	}
-}
