146d2f393a9f32df2401f33e783530a4d683b53b
grantseltzer <grantseltzer@gmail.com>
2024-12-20 10:10:45 -0600

    Use typedefs for struct types
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/codegen/c/event.h
+++ pkg/dynamicinstrumentation/codegen/c/event.h
@@ -4,20 +4,20 @@
 #include "ktypes.h"
 #include "macros.h"
 
-struct event {
+typedef struct event {
     struct base_event base;
     char output[PARAM_BUFFER_SIZE];
-};
+} event_t;
 
 // expression_context contains state that is meant to be shared across location expressions
 // during execution of the full bpf program.
-struct expression_context {
+typedef struct expression_context {
     __u64 *output_offset; // current offset within the output buffer to write to
     __u8 *stack_counter;  // current size of the bpf parameter stack, used for emptying stack
     struct pt_regs *ctx;
-    struct event *event;  // output event allocated on ringbuffer
+    event_t *event;  // output event allocated on ringbuffer
     __u64 *temp_storage;  // temporary storage array on heap used by some location expressions
     char *zero_string;    // array of zero's used to zero out buffers
-};
+} expression_context_t;
 
 #endif
