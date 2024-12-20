b2520c59b02850db5cf8f35ac14be60476eafb05
grantseltzer <grantseltzer@gmail.com>
2024-12-19 21:55:46 -0600

    Define new map macro for stack, including change for whether or not to include key type. Use macros in DI
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/codegen/c/maps.h
+++ pkg/dynamicinstrumentation/codegen/c/maps.h
@@ -1,6 +1,8 @@
 #ifndef DI_MAPS_H
 #define DI_MAPS_H
 
+#include "map-defs.h"
+
 // The events map is the ringbuffer used for communicating events from
 // bpf to user space.
 struct {
@@ -10,40 +12,20 @@ struct {
 
 // The zeroval map is used to have pre-zero'd data which bpf code can
 // use to zero out event buffers (similar to memset, but verifier friendly).
-struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(key_size, sizeof(__u32));
-    __uint(value_size, sizeof(char[PARAM_BUFFER_SIZE]));
-    __uint(max_entries, 1);
-} zeroval SEC(".maps");
-
-// The param_stack map is used as a stack for the location expressions
-// to operate on values and addresses.
-struct {
-    __uint(type, BPF_MAP_TYPE_STACK);
-    __uint(max_entries, 2048);
-    __type(value, __u64);
-} param_stack SEC(".maps");
+BPF_ARRAY_MAP(zeroval, char[PARAM_BUFFER_SIZE], 1);
 
 // The temp_storage_array map is used as a temporary location in memory
 // not on the bpf stack that location expressions can use for temporarily
 // caching data while they operate on it without worrying about exceeding
 // the 512 byte bpf stack limit.
-struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __uint(max_entries, 1);
-    __type(key, __u32);
-    __type(value, __u64[4000]);
-} temp_storage_array SEC(".maps");
+BPF_PERCPU_ARRAY_MAP(temp_storage_array, __u64[4000], 1);
 
 // The collection_limits map is used for setting the known length limit
 // of collections such as slices so that they can later be referenced
 // when reading the values in the collection.
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 1024);
-    __type(key, char[6]);
-    __type(value, __u16);
-} collection_limits SEC(".maps");
+BPF_HASH_MAP(collection_limits, char[6], __u16, 1024);
 
+// The param_stack map is used as a stack for the location expressions
+// to operate on values and addresses.
+BPF_STACK_MAP(param_stack, __u64, 2048);
 #endif
