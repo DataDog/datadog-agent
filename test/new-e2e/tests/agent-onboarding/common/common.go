ec7a6efaab5cf07d7bbe3ff7c52f412ff3d45505
Fanny Jiang <fanny.jiang@datadoghq.com>
2024-12-24 13:17:55 -0500

    fix ci

--- test/new-e2e/tests/agent-onboarding/common/common.go
+++ test/new-e2e/tests/agent-onboarding/common/common.go
@@ -3,9 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build e2e
-// +build e2e
-
 package common
 
 import (
