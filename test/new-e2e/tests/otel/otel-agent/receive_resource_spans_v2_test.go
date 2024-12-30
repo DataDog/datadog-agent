52e0b8b7ee86ce51c0d1965f57e3dbd6b6175642
Florent Clarret <florent.clarret@datadoghq.com>
2024-12-30 17:31:39 +0000

    Mark some otel tests flaky (#32558)
    (cherry picked from commit 20c010fda49b74b0c99a64b8f50e5219061008d8)

--- test/new-e2e/tests/otel/otel-agent/receive_resource_spans_v2_test.go
+++ test/new-e2e/tests/otel/otel-agent/receive_resource_spans_v2_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/DataDog/test-infra-definitions/components/datadog/kubernetesagentparams"
 
+	"github.com/DataDog/datadog-agent/pkg/util/testutil/flake"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awskubernetes "github.com/DataDog/datadog-agent/test/new-e2e/pkg/provisioners/aws/kubernetes"
@@ -21,6 +22,7 @@ type otelAgentSpanReceiverV2TestSuite struct {
 }
 
 func TestOTelAgentSpanReceiverV2(t *testing.T) {
+	flake.Mark(t) // incident-33599
 	values := `
 datadog:
   logs:
