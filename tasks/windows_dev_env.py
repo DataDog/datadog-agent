fcd39e2e4e443371650e2e6dbc48234b6fea8580
pducolin <paola.ducolin@datadoghq.com>
2024-12-31 17:47:05 +0100

    [task] add task to start a windows dev environment and keep remote code in sync with local code

---
+++ tasks/windows_dev_env.py
@@ -0,0 +1,139 @@
+"""
+Create a remote Windows development environment and keep it in sync with local changes.
+"""
+
+import os
+import shutil
+import time
+from datetime import timedelta
+
+from invoke.context import Context
+from invoke.tasks import task
+
+AMI_WINDOWS_DEV_2022 = "ami-09b68440cb06b26d6"
+
+
+@task(
+    help={
+        'name': 'Override the default name of the development environment (windows-dev-env).',
+    },
+)
+def start(
+    ctx: Context,
+    name: str = "windows-dev-env",
+):
+    """
+    Create a remote Windows development environment and keep it in sync with local changes.
+    """
+    _start_windows_dev_env(ctx, name)
+
+
+@task(
+    help={
+        'name': 'Override the default name of the development environment (windows-dev-env).',
+    },
+)
+def stop(
+    ctx: Context,
+    name: str = "windows-dev-env",
+):
+    """
+    Removes a remote Windows development environment.
+    """
+    _stop_windows_dev_env(ctx, name)
+
+
+def _start_windows_dev_env(ctx, name: str = "windows-dev-env"):
+    start_time = time.time()
+    # lazy load watchdog to avoid import error on the CI
+    from watchdog.events import FileSystemEvent, FileSystemEventHandler
+    from watchdog.observers import Observer
+
+    class DDAgentEventHandler(FileSystemEventHandler):
+        def __init__(self, handler):
+            self.handler = handler
+
+        def on_any_event(self, event: FileSystemEvent) -> None:  # noqa # called by watchdog callback
+            self.handler(event.src_path)
+
+    # Ensure `test-infra-definitions` is cloned.
+    if not os.path.isdir('../test-infra-definitions'):
+        with ctx.cd('..'):
+            ctx.run("git clone git@github.com:DataDog/test-infra-definitions.git")
+            with ctx.cd('test-infra-definitions'):
+                # setup test-infra-definitions
+                ctx.run("python3 -m pip install -r requirements.txt")
+                ctx.run("inv setup")
+    if shutil.which("rsync") is None:
+        raise Exception(
+            "rsync is not installed. Please install rsync by running `brew install rsync` on macOS and try again."
+        )
+    # Create the Windows development environment.
+    host = ""
+    with ctx.cd('../test-infra-definitions'):
+        result = ctx.run(
+            f"inv aws.create-vm --ami-id={AMI_WINDOWS_DEV_2022} --os-family=windows --architecture=x86_64 --no-install-agent --stack-name={name} --no-interactive"
+        )
+        if result is None or not result:
+            raise Exception("Failed to create the Windows development environment.")
+        # Find pulumi output in stdout
+        connection_message_prefix = "You can run the following command to connect to the host `ssh "
+        connection_message_suffix = "`."
+
+        pulumi_output_start_index = result.stdout.find(connection_message_prefix)
+        if pulumi_output_start_index == -1:
+            raise Exception("Failed to find pulumi output in stdout.")
+        pulumi_output_end_index = result.stdout.find(connection_message_suffix, pulumi_output_start_index)
+        if pulumi_output_end_index == -1:
+            raise Exception("Failed to find pulumi output end in stdout.")
+        connection_message = result.stdout[
+            pulumi_output_start_index + len(connection_message_prefix) : pulumi_output_end_index
+        ]
+        print(f"Pulumi output:\n============\n{connection_message}\n============\n")
+        # extract username and address from connection message
+        host = connection_message.split(" ")[0]
+
+    # add host to known hosts
+    ctx.run(f"ssh-keyscan -H {host} >> ~/.ssh/known_hosts")
+    # sync local changes to the remote Windows development environment
+    rsync_command = f"rsync -avzrcIR --delete --rsync-path='C:\\cygwin\\bin\\rsync.exe' --filter=':- .gitignore' --exclude /.git/ . {host}:/cygdrive/c/mnt/datadog-agent/"
+    print("Syncing changes to the remote Windows development environment...")
+    ctx.run(rsync_command)
+    # print the time taken to start the dev env
+    elapsed_time = time.time() - start_time
+    print("♻️ Windows dev env started in", timedelta(seconds=elapsed_time))
+
+    # start file watcher and run rsync on changes
+    def on_change(path):
+        current_dir = os.getcwd()
+        relative_path = os.path.relpath(path, current_dir)
+        if relative_path.startswith(".git/"):
+            # ignore changes in .git directory
+            return
+        res = ctx.run(f"git check-ignore {relative_path} --quiet", warn=True, hide=True)
+        if res is not None and res.exited == 0:
+            # ignore changes in git ignored files
+            # see https://git-scm.com/docs/git-check-ignore#_exit_status
+            return
+        print("Syncing changes to the remote Windows development environment...")
+        ctx.run(rsync_command)
+
+    event_handler = DDAgentEventHandler(handler=on_change)
+    observer = Observer()
+    observer.schedule(event_handler, ".", recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    finally:
+        observer.join()
+    print("♻️ Windows dev env sync stopped")
+    print("Start it again with `inv windows_dev_env.start`")
+    print("Destroy the Windows dev env with `inv windows-dev-env.stop`")
+
+
+def _stop_windows_dev_env(ctx, name: str = "windows-dev-env"):
+    with ctx.cd('../test-infra-definitions'):
+        ctx.run(f"inv aws.destroy-vm --stack-name={name}")
