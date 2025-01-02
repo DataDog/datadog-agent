bf94af7e5ad6d91173a22b478bfe6fc0d51bb4d8
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-01-02 17:44:48 +0100

    improve test with chmod + read only open

--- pkg/security/tests/fim_test.go
+++ pkg/security/tests/fim_test.go
@@ -40,6 +40,7 @@ func TestFIMOpen(t *testing.T) {
 	}
 	defer os.Remove(testFile)
 
+	// open test
 	test.WaitSignal(t, func() error {
 		f, err := os.Create(testFile)
 		if err != nil {
@@ -51,5 +52,26 @@ func TestFIMOpen(t *testing.T) {
 		assertTriggeredRule(t, rule, "__fim_expanded_open__test_fim_rule")
 		assert.Equal(t, rule.Def.ID, "test_fim_rule")
 		assertInode(t, event.Open.File.Inode, getInode(t, testFile))
+	})
+
+	// chmod test
+	test.WaitSignal(t, func() error {
+		return os.Chmod(testFile, 0o777)
+	}, func(event *model.Event, rule *rules.Rule) {
+		assert.Equal(t, "chmod", event.GetType(), "wrong event type")
+		assertTriggeredRule(t, rule, "__fim_expanded_chmod__test_fim_rule")
+		assert.Equal(t, rule.Def.ID, "test_fim_rule")
+		assertInode(t, event.Chmod.File.Inode, getInode(t, testFile))
+	})
+
+	// open but read only
+	_ = test.GetSignal(t, func() error {
+		f, err := os.Open(testFile)
+		if err != nil {
+			return err
+		}
+		return f.Close()
+	}, func(_ *model.Event, _ *rules.Rule) {
+		t.Error("Event received (rule is in write only mode, and the open is read only)")
 	})
 }
