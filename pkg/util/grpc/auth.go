7292624f0546df49ddf2e2a27312a62d7814e468
Maxime Riaud <maxime.riaud@datadoghq.com>
2025-01-02 15:46:29 -0600

    updating grpc pkg to use pkg/api/security/auth utils

--- pkg/util/grpc/auth.go
+++ pkg/util/grpc/auth.go
@@ -7,70 +7,96 @@ package grpc
 
 import (
 	"context"
-	"crypto/subtle"
-	"errors"
-	"fmt"
 
-	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 
-	grpccontext "github.com/DataDog/datadog-agent/pkg/util/grpc/context"
+	"github.com/DataDog/datadog-agent/pkg/api/security/auth"
 )
 
-// verifierFunc receives the token passed in the request headers, and returns
-// arbitrary information about the token to be stored in the request context,
-// or an error if the token is not valid.
-type verifierFunc func(string) (interface{}, error)
-
-// AuthInterceptor is a gRPC interceptor that extracts an auth token from the
-// request headers, and validates it using the provided func.
-func AuthInterceptor(verifier verifierFunc) grpc_auth.AuthFunc {
-	return func(ctx context.Context) (context.Context, error) {
-		token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
-		if err != nil {
-			return nil, err
+// UnaryServerInterceptor validates the signature
+func GetUnaryServerInterceptor(a auth.Authorizer) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		// Retrieve metadata
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, status.Errorf(codes.Unauthenticated, "missing metadata")
 		}
 
-		tokenInfo, err := verifier(token)
+		err := a.VerifyGRPC(info.FullMethod, md)
+
 		if err != nil {
-			return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %v", err)
+			return nil, status.Error(codes.Unauthenticated, err.Error())
 		}
 
-		return context.WithValue(ctx, grpccontext.ContextKeyTokenInfoID, tokenInfo), nil
+		return handler(ctx, req)
 	}
 }
 
-// StaticAuthInterceptor is a gRPC interceptor that extracts an auth token from the request headers, and validates it
-// using the given token.
-func StaticAuthInterceptor(token string) grpc_auth.AuthFunc {
-	return AuthInterceptor(func(reqToken string) (interface{}, error) {
-		if subtle.ConstantTimeCompare([]byte(reqToken), []byte(token)) == 0 {
-			return struct{}{}, errors.New("invalid session token")
+// GetStreamServerInterceptor validates the signature for streaming RPCs
+func GetStreamServerInterceptor(a auth.Authorizer) grpc.StreamServerInterceptor {
+	return func(
+		srv interface{},
+		ss grpc.ServerStream,
+		info *grpc.StreamServerInfo,
+		handler grpc.StreamHandler,
+	) error {
+		// Retrieve metadata
+		md, ok := metadata.FromIncomingContext(ss.Context())
+		if !ok {
+			return status.Errorf(codes.Unauthenticated, "missing metadata")
 		}
 
-		return struct{}{}, nil
-	})
+		err := a.VerifyGRPC(info.FullMethod, md)
+		if err != nil {
+			return status.Error(codes.Unauthenticated, err.Error())
+		}
+
+		return handler(srv, ss)
+	}
 }
 
-type bearerTokenAuth struct {
-	token string
-}
+// UnaryInterceptor adds the signature to the request
+func GetUnaryClientInterceptor(a auth.Authorizer) grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req interface{},
+		reply interface{},
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
 
-func (b bearerTokenAuth) GetRequestMetadata(_ context.Context, _ ...string) (map[string]string, error) {
-	return map[string]string{
-		"authorization": fmt.Sprintf("Bearer %s", b.token),
-	}, nil
-}
+		// // Serialize the request body
+		// body, err := proto.Marshal(req.(proto.Message))
+		// if err != nil {
+		// 	return err
+		// }
 
-func (b bearerTokenAuth) RequireTransportSecurity() bool {
-	return true
-}
+		md, ok := metadata.FromOutgoingContext(ctx)
+		if !ok {
+			md = make(metadata.MD)
+		}
 
-// NewBearerTokenAuth creates a set of per-RPC credentials that uses a bearer token for authentication/authorization.
-//
-// This credentials implementation requires the connection to be secure (i.e. using TLS).
-func NewBearerTokenAuth(token string) credentials.PerRPCCredentials {
-	return bearerTokenAuth{token: token}
+		// Generate the signature
+		err := a.SignGRPC(method, md)
+		if err != nil {
+			return err
+		}
+
+		// Add signature and timestamp to metadata
+		// md.Append(headers)
+		ctx = metadata.NewOutgoingContext(ctx, md)
+
+		// Invoke the RPC
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
 }
