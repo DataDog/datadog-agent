41e840d5a6f3c6e2d5ebb250c91b7ad546368928
Ankit Patel <8731662+ankitpatel96@users.noreply.github.com>
2025-01-06 19:28:15 -0500

    port nocgo strategy to component

--- comp/serializer/compression/impl-zstd-nocgo/zstd_nocgo_strategy.go
+++ comp/serializer/compression/impl-zstd-nocgo/zstd_nocgo_strategy.go
@@ -8,8 +8,12 @@ package zstdimpl
 
 import (
 	"bytes"
+	"os"
+	"strconv"
+	"sync"
 
-	"github.com/DataDog/zstd"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/klauspost/compress/zstd"
 
 	compression "github.com/DataDog/datadog-agent/comp/serializer/compression/def"
 )
@@ -24,33 +28,63 @@ type Provides struct {
 	Comp compression.Component
 }
 
-// ZstdNoCgoStrategy is the strategy for when serializer_compressor_kind is zstd
+var globalencoder *zstd.Encoder
+var mutex sync.Mutex
+
+// ZstdNoCgoStrategy can be manually selected via component - it's not used by any selector / config option
 type ZstdNoCgoStrategy struct {
-	level int
+	level   int
+	encoder *zstd.Encoder
 }
 
 // NewComponent returns a new ZstdNoCgoStrategy
 func NewComponent(reqs Requires) Provides {
+	log.Debugf("Compressing native zstd at level %d", reqs.Level)
+
+	mutex.Lock()
+	if globalencoder == nil {
+		conc, err := strconv.Atoi(os.Getenv("ZSTD_NOCGO_CONCURRENCY"))
+		if err != nil {
+			conc = 1
+		}
+
+		window, err := strconv.Atoi(os.Getenv("ZSTD_NOCGO_WINDOW"))
+		if err != nil {
+			window = 1 << 15
+		}
+
+		log.Debugf("native zstd concurrency %d", conc)
+		log.Debugf("native zstd window size %d", window)
+		globalencoder, _ = zstd.NewWriter(nil,
+			zstd.WithEncoderLevel(zstd.EncoderLevelFromZstd(reqs.Level)),
+			zstd.WithEncoderConcurrency(conc),
+			zstd.WithLowerEncoderMem(true),
+			zstd.WithWindowSize(window))
+	}
+	mutex.Unlock()
+
 	return Provides{
 		Comp: &ZstdNoCgoStrategy{
-			level: reqs.Level,
+			level:   reqs.Level,
+			encoder: globalencoder,
 		},
 	}
 }
 
 // Compress will compress the data with zstd
 func (s *ZstdNoCgoStrategy) Compress(src []byte) ([]byte, error) {
-	return zstd.CompressLevel(nil, src, s.level)
+	return s.encoder.EncodeAll(src, nil), nil
 }
 
 // Decompress will decompress the data with zstd
 func (s *ZstdNoCgoStrategy) Decompress(src []byte) ([]byte, error) {
-	return zstd.Decompress(nil, src)
+	decoder, _ := zstd.NewReader(nil)
+	return decoder.DecodeAll(src, nil)
 }
 
 // CompressBound returns the worst case size needed for a destination buffer when using zstd
 func (s *ZstdNoCgoStrategy) CompressBound(sourceLen int) int {
-	return zstd.CompressBound(sourceLen)
+	return s.encoder.MaxEncodedSize(sourceLen)
 }
 
 // ContentEncoding returns the content encoding value for zstd
@@ -60,5 +94,6 @@ func (s *ZstdNoCgoStrategy) ContentEncoding() string {
 
 // NewStreamCompressor returns a new zstd Writer
 func (s *ZstdNoCgoStrategy) NewStreamCompressor(output *bytes.Buffer) compression.StreamCompressor {
-	return zstd.NewWriterLevel(output, s.level)
+	writer, _ := zstd.NewWriter(output, zstd.WithEncoderLevel(zstd.EncoderLevelFromZstd(s.level)))
+	return writer
 }
