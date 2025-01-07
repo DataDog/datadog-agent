1315c47b3d7be8b2271a8db6745bf5da28646fa3
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-01-07 10:31:50 +0100

    sign more files

--- omnibus/config/software/openssl-fips-provider.rb
+++ omnibus/config/software/openssl-fips-provider.rb
@@ -56,6 +56,9 @@ build do
       copy "/usr/local/lib*/ossl-modules/fips.so", "#{dest}/lib/ossl-modules/fips.so"
     elsif windows_target?
       copy "providers/fips.dll", "#{dest}/lib/ossl-modules/fips.dll"
+      if windows_signing_enabled?
+        sign_file "#{dest}/lib/ossl-modules/fips.dll"
+      end
     end
 
     erb source: "openssl.cnf.erb",
