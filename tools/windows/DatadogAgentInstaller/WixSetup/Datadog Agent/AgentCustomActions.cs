8d63f07f90094dee35c4bc41e423ba2c3ed9dcf1
Jack Phillips <jack.phillips@datadoghq.com>
2025-01-08 14:36:49 -0500

    update task run properties

--- tools/windows/DatadogAgentInstaller/WixSetup/Datadog Agent/AgentCustomActions.cs
+++ tools/windows/DatadogAgentInstaller/WixSetup/Datadog Agent/AgentCustomActions.cs
@@ -299,7 +299,7 @@ namespace WixSetup.Datadog_Agent
                     Return.ignore,
                     When.Before,
                     Step.RemoveFiles,
-                    Conditions.RemovingForUpgrade | Conditions.Maintenance | Conditions.Uninstalling
+                    Conditions.RemovingForUpgrade | Conditions.Maintenance
                 )
             {
                 Execute = Execute.deferred,
