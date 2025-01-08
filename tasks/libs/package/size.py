63bcdbf953ceba958ae1e84c047ef85d84ac59cf
Marethyu <45374460+Pythyu@users.noreply.github.com>
2025-01-08 14:39:29 +0100

    Reduce tolerance of all package size increase threshold (#32733)

--- tasks/libs/package/size.py
+++ tasks/libs/package/size.py
@@ -37,22 +37,22 @@ SCANNED_BINARIES = {
 # The below template contains the relative increase threshold for each package type
 PACKAGE_SIZE_TEMPLATE = {
     'amd64': {
-        'datadog-agent': {'deb': int(140e6)},
-        'datadog-iot-agent': {'deb': int(10e6)},
-        'datadog-dogstatsd': {'deb': int(10e6)},
-        'datadog-heroku-agent': {'deb': int(70e6)},
+        'datadog-agent': {'deb': int(5e5)},
+        'datadog-iot-agent': {'deb': int(5e5)},
+        'datadog-dogstatsd': {'deb': int(5e5)},
+        'datadog-heroku-agent': {'deb': int(5e5)},
     },
     'x86_64': {
-        'datadog-agent': {'rpm': int(140e6), 'suse': int(140e6)},
-        'datadog-iot-agent': {'rpm': int(10e6), 'suse': int(10e6)},
-        'datadog-dogstatsd': {'rpm': int(10e6), 'suse': int(10e6)},
+        'datadog-agent': {'rpm': int(5e5), 'suse': int(5e5)},
+        'datadog-iot-agent': {'rpm': int(5e5), 'suse': int(5e5)},
+        'datadog-dogstatsd': {'rpm': int(5e5), 'suse': int(5e5)},
     },
     'arm64': {
-        'datadog-agent': {'deb': int(140e6)},
-        'datadog-iot-agent': {'deb': int(10e6)},
-        'datadog-dogstatsd': {'deb': int(10e6)},
+        'datadog-agent': {'deb': int(5e5)},
+        'datadog-iot-agent': {'deb': int(5e5)},
+        'datadog-dogstatsd': {'deb': int(5e5)},
     },
-    'aarch64': {'datadog-agent': {'rpm': int(140e6)}, 'datadog-iot-agent': {'rpm': int(10e6)}},
+    'aarch64': {'datadog-agent': {'rpm': int(5e5)}, 'datadog-iot-agent': {'rpm': int(5e5)}},
 }
 
 
