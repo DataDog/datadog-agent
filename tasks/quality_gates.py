8613a05a5540879d2d638fda9be4e72c792a44d7
Pythyu <antoine.zellmeyer@datadoghq.com>
2025-01-13 17:15:07 +0100

    feat(quality_gates): better visual report on gates

--- tasks/quality_gates.py
+++ tasks/quality_gates.py
@@ -1,12 +1,15 @@
+import traceback
+
 import yaml
 from invoke import task
-import traceback
+
+from tasks.libs.common.color import color_message
 
 FAIL_CHAR = "❌"
 SUCCESS_CHAR = "✅"
 
 
-def generate_pr_comment(finalState, gateStates):
+def display_pr_comment(ctx, finalState, gateStates):
     body_head = f"""
     <details>
     <summary><h1>
@@ -22,6 +25,28 @@ def generate_pr_comment(finalState, gateStates):
     </details>
     """
     return body_head + body_gates + body_feet
+    # pr_commenter(ctx, title="Uncompressed package size comparison", body=body)
+
+
+def _print_quality_gates_report(gateStates):
+    print(color_message("======== Static Quality Gates Report ========", "magenta"))
+    for gate in sorted(gateStates, key=lambda x: x["error_type"] is not None):
+        if gate["error_type"] is None:
+            print(color_message(f"Gate {gate['name']} succeeded ✅", "green"))
+        elif gate["error_type"] == "AssertionError":
+            print(
+                color_message(
+                    f"Gate {gate['name']} failed ❌ because of the following assertion failures :\n{gate['error_message']}",
+                    "red",
+                )
+            )
+        else:
+            print(
+                color_message(
+                    f"Gate {gate['name']} failed ❌ with the following stack trace :\n{gate['error_message']}",
+                    "red",
+                )
+            )
 
 
 @task
@@ -41,13 +66,20 @@ def parse_and_trigger_gates(ctx, config_path="test/static/static_quality_gates.y
         try:
             getattr(quality_gates_mod, gate).entrypoint(**gateInputs)
             print(f"Gate {gate} succeeded !")
-            gateStates.append({"name": gate, "state": True})
-        except Exception:
-            print(f"Gate {gate} failed with the following stack trace :")
-            print(traceback.format_exc())
+            gateStates.append({"name": gate, "state": True, "error_type": None, "error_message": None})
+        except AssertionError as e:
+            print(f"Gate {gate} failed ! (AssertionError)")
             finalState = False
-            gateStates.append({"name": gate, "state": False})
+            gateStates.append({"name": gate, "state": False, "error_type": "AssertionError", "error_message": str(e)})
+        except Exception:
+            print(f"Gate {gate} failed ! (StackTrace)")
+            finalState = False
+            gateStates.append(
+                {"name": gate, "state": False, "error_type": "StackTrace", "error_message": traceback.format_exc()}
+            )
     if not finalState:
         ctx.run("datadog-ci tag --level job --tags static_quality_gates:\"failed\"")
     else:
         ctx.run("datadog-ci tag --level job --tags static_quality_gates:\"passed\"")
+
+    _print_quality_gates_report(gateStates)
