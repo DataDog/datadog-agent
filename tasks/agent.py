d059daa1fa9287ed6ff5f253d3b76433563fc68e
Atsushi Watanabe <atsushi.w@ieee.org>
2025-01-14 14:25:44 +0900

    Add option to build on non-glibc env
    Add an build option (`--non-glibc`) to build the agent on
    non-glibc environment like musl libc.
    The option disables system-probe gpu module and
    corechecks gpu collector using `github.com/NVIDIA/go-nvml`
    which depends on a glibc-extended definition.

--- tasks/agent.py
+++ tasks/agent.py
@@ -146,6 +146,7 @@ def build(
     bundle_ebpf=False,
     agent_bin=None,
     run_on=None,  # noqa: U100, F841. Used by the run_on_devcontainer decorator
+    glibc=True,
 ):
     """
     Build the agent. If the bits to include in the build are not specified,
@@ -216,6 +217,9 @@ def build(
 
             all_tags |= set(build_tags)
         build_tags = list(all_tags)
+
+    if not glibc:
+        build_tags = list(set(build_tags).difference({"nvml"}))
 
     cmd = "go build -mod={go_mod} {race_opt} {build_type} -tags \"{go_build_tags}\" "
 
