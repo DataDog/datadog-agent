421121a536fb1a8776ecb4c64dc06d8266fa5f1c
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-01-14 13:47:03 +0100

    Mark some otel tests flaky (#32558) (#32948)
    Co-authored-by: Florent Clarret <florent.clarret@datadoghq.com>

--- test/new-e2e/tests/otel/otel-agent/infraattributes_test.go
+++ test/new-e2e/tests/otel/otel-agent/infraattributes_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/DataDog/test-infra-definitions/components/datadog/kubernetesagentparams"
 
 	"github.com/DataDog/datadog-agent/comp/core/tagger/types"
+	"github.com/DataDog/datadog-agent/pkg/util/testutil/flake"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
 	awskubernetes "github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments/aws/kubernetes"
@@ -27,6 +28,7 @@ type iaTestSuite struct {
 var iaConfig string
 
 func TestOTelAgentIA(t *testing.T) {
+	flake.Mark(t) // incident-33599
 	values := `
 datadog:
   logs:
