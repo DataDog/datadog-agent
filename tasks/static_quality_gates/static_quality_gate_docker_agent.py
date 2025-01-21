61ceca171aaf467e6f5ab28437a1d56641df9e14
Pythyu <antoine.zellmeyer@datadoghq.com>
2025-01-21 18:10:38 +0100

    fix(docker): fix docker image on disk size calculation

--- tasks/static_quality_gates/static_quality_gate_docker_agent.py
+++ tasks/static_quality_gates/static_quality_gate_docker_agent.py
@@ -4,6 +4,26 @@ from tasks.libs.common.color import color_message
 from tasks.static_quality_gates.lib.gates_lib import argument_extractor
 
 
+def calculate_image_on_disk_size(ctx, url):
+    # Pull image locally to get on disk size
+    ctx.run(f"crane pull {url} output.tar")
+    ctx.run("tar -xf output.tar")
+    image_content = ctx.run("tar -tvf output.tar | awk -F' ' '{print $5; print $9}'").stdout.splitlines()
+    total_size = 0
+    image_tar_gz = None
+    for k, line in enumerate(image_content):
+        if k % 2 == 0:
+            if "tar.gz" in image_content[k + 1]:
+                image_tar_gz = image_content[k + 1]
+            else:
+                total_size += int(line)
+
+    if image_tar_gz:
+        total_size += int(ctx.run(f"tar -xf {image_tar_gz} --to-stdout | wc -c").stdout)
+
+    return total_size
+
+
 def entrypoint(**kwargs):
     arguments = argument_extractor(kwargs, max_on_wire_size=None, max_on_disk_size=None, ctx=None, metricHandler=None)
     ctx = arguments.ctx
@@ -35,10 +55,8 @@ def entrypoint(**kwargs):
         + " | grep size | awk -F ':' '{sum+=$NF} END {print sum}'"
     )
     image_on_wire_size = int(image_on_wire_size.stdout)
-    # Pull image locally to get on disk size
-    ctx.run(f"crane pull {url} output.tar")
-    image_on_disk_size = ctx.run("tar -xf output.tar --to-stdout | wc -c")
-    image_on_disk_size = int(image_on_disk_size.stdout)
+    # Calculate image on disk size
+    image_on_disk_size = calculate_image_on_disk_size(ctx, url)
 
     metricHandler.register_metric("static_quality_gate_docker_agent", "current_on_wire_size", image_on_wire_size)
     metricHandler.register_metric("static_quality_gate_docker_agent", "current_on_disk_size", image_on_disk_size)
@@ -68,7 +86,7 @@ def entrypoint(**kwargs):
     else:
         print(
             color_message(
-                f"image_on_disk_size <= max_on_wire_size, ({image_on_disk_size}) <= ({max_on_disk_size})",
+                f"image_on_disk_size <= max_on_disk_size, ({image_on_disk_size}) <= ({max_on_disk_size})",
                 "green",
             )
         )
