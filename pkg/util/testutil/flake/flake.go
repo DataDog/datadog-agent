8b4021eca4b77345e93c077b739623dfb9964665
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-01-22 13:58:36 +0100

    [rfc-flake-logs-2-adxt-861] Added mark on log

--- pkg/util/testutil/flake/flake.go
+++ pkg/util/testutil/flake/flake.go
@@ -12,13 +12,18 @@ package flake
 import (
 	"flag"
 	"os"
+	"runtime"
 	"strconv"
+	"strings"
 	"testing"
+
+	"gopkg.in/yaml.v3"
 )
 
 const flakyTestMessage = "flakytest: this is a known flaky test"
 
 var skipFlake = flag.Bool("skip-flake", false, "skip tests labeled as flakes")
+var flakyPatternsConfig = flag.String("flaky-patterns-config", "/tmp/e2e-flaky-patterns.yaml", "path to the flaky patterns configuration file that will be created when MarkOnLog is used")
 
 // Mark test as a known flaky.
 // If any of skip-flake flag or GO_TEST_SKIP_FLAKE environment variable is set, the test will be skipped.
@@ -30,6 +35,114 @@ func Mark(t testing.TB) {
 		return
 	}
 	t.Log(flakyTestMessage)
+}
+
+// func readFlakyPatternsConfig(path string) {
+
+// r, err := os.Open(*flakyPatternsConfig)
+// if err != nil {
+// 	t.Fatalf("failed to open flaky patterns config file: %v", err)
+// 	return
+// }
+// dec := yaml.NewDecoder(r)
+// // package -> []test -> {test: name, on-log: pattern}
+// pkgToTests := make(map[string][]map[string]interface{})
+// if err := dec.Decode(&pkgToTests); err != nil {
+// 	t.Errorf("unmarshal: %w", err)
+// 	return
+// }
+// println("Parsed")
+// }
+
+// func writeFlakyPatternsConfig(path string) {
+
+// MarkOnLog marks the test as flaky when the `pattern` regular expression is found in its logs.
+func MarkOnLog(t testing.TB, pattern string) {
+	t.Helper()
+	if *flakyPatternsConfig == "" {
+		t.Fatal("flaky-patterns-config flag is required when using MarkOnLog")
+		return
+	}
+
+	// TODO: Lock
+
+	// TODO A: Useful ?
+	println(t.Name())
+	t.Log("NOTE: This test will be marked flaky if the following pattern is found in the logs:", pattern)
+
+	// Add the pattern to the yaml config
+
+	// TODO: Lock file (multithread)
+	// TODO: Not created case
+	f, err := os.OpenFile(*flakyPatternsConfig, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		t.Fatalf("failed to open flaky patterns config file: %v", err)
+		return
+	}
+	defer f.Close()
+
+	dec := yaml.NewDecoder(f)
+	flakyConfig := make(map[string]interface{})
+	err = dec.Decode(&flakyConfig)
+	if err != nil {
+		t.Fatalf("failed to decode flaky patterns config file: %v", err)
+		return
+	}
+
+	// Get the test function package which is the topmost function in the stack that is part of the datadog-agent package
+	fullPackageName := ""
+	for i := 0; i < 42; i++ {
+		pc, _, _, ok := runtime.Caller(i)
+		if !ok {
+			// Top of the stack
+			break
+		}
+		fullname := runtime.FuncForPC(pc).Name()
+		if strings.Contains(fullname, "datadog-agent") {
+			fullPackageName = fullname
+		}
+	}
+
+	// TODO A: Is it the same with subtests?
+	if fullPackageName == "" {
+		t.Fatalf("failed to fetch e2e test function information")
+		return
+	}
+
+	// println("Caller: " + e2eCaller)
+
+	// // TODO: How to get it properly ? If MarkOnLog is used in a subfunction, it doesn't work
+	// // Get package name
+	// pc, _, _, ok := runtime.Caller(1)
+	// if !ok {
+	// 	t.Fatalf("failed to get caller information")
+	// 	return
+	// }
+	// fullname := runtime.FuncForPC(pc).Name()
+	// TODO: Windows
+	prefix := "github.com/DataDog/datadog-agent/"
+	fullPackageName = strings.TrimPrefix(fullPackageName, prefix)
+	nameParts := strings.Split(fullPackageName, ".")
+	packageName := nameParts[0]
+	println(packageName)
+	println(t.Name())
+
+	entry := make(map[string]interface{})
+	entry["test"] = t.Name()
+	entry["on-log"] = pattern
+	flakyConfig[packageName] = entry
+
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		t.Fatalf("failed to seek flaky patterns config file: %v", err)
+		return
+	}
+	encoder := yaml.NewEncoder(f)
+	err = encoder.Encode(flakyConfig)
+	if err != nil {
+		t.Fatalf("failed to encode flaky patterns config file: %v", err)
+		return
+	}
 }
 
 func shouldSkipFlake() bool {
