2e6c6595aa51589ccfae4bb0bce48fa58c913a45
Remy Mathieu <remy.mathieu@datadoghq.com>
2025-01-22 14:49:31 +0100

    this change isn't necessary anymore

--- pkg/logs/pipeline/provider_test.go
+++ pkg/logs/pipeline/provider_test.go
@@ -6,7 +6,6 @@
 package pipeline
 
 import (
-	"net"
 	"testing"
 	"time"
 
@@ -15,50 +14,24 @@ import (
 
 	"github.com/DataDog/datadog-agent/comp/logs/agent/config"
 	compressionfx "github.com/DataDog/datadog-agent/comp/serializer/logscompression/fx-mock"
-	configmock "github.com/DataDog/datadog-agent/pkg/config/mock"
 	"github.com/DataDog/datadog-agent/pkg/logs/auditor"
-	"github.com/DataDog/datadog-agent/pkg/logs/client"
-	"github.com/DataDog/datadog-agent/pkg/logs/client/mock"
-	"github.com/DataDog/datadog-agent/pkg/logs/client/tcp"
-	"github.com/DataDog/datadog-agent/pkg/logs/message"
-	"github.com/DataDog/datadog-agent/pkg/logs/metrics"
-	"github.com/DataDog/datadog-agent/pkg/logs/sender"
-	"github.com/DataDog/datadog-agent/pkg/logs/status/statusinterface"
 	"github.com/DataDog/datadog-agent/pkg/status/health"
 )
 
 type ProviderTestSuite struct {
 	suite.Suite
-	p          *provider
-	a          *auditor.RegistryAuditor
-	fakeIntake net.Listener
-}
-
-func newTestSender(suite *ProviderTestSuite) *sender.Sender {
-	input := make(chan *message.Payload, 1)
-
-	destinationsCtx := client.NewDestinationsContext()
-	destinationsCtx.Start()
-
-	destination := tcp.AddrToDestination(suite.fakeIntake.Addr(), destinationsCtx, statusinterface.NewStatusProviderMock())
-	destinations := client.NewDestinations([]client.Destination{destination}, nil)
-
-	cfg := configmock.New(suite.T())
-	sender := sender.NewSender(cfg, input, auditor.NewNullAuditor(), destinations, 0, nil, nil, metrics.NewNoopPipelineMonitor(""))
-	return sender
+	p *provider
+	a *auditor.RegistryAuditor
 }
 
 func (suite *ProviderTestSuite) SetupTest() {
-	suite.fakeIntake = mock.NewMockLogsIntake(suite.T())
 	suite.a = auditor.New("", auditor.DefaultRegistryFilename, time.Hour, health.RegisterLiveness("fake"))
-	sender := newTestSender(suite)
 	suite.p = &provider{
 		numberOfPipelines:    3,
 		auditor:              suite.a,
 		pipelines:            []*Pipeline{},
 		endpoints:            config.NewEndpoints(config.Endpoint{}, nil, true, false),
 		currentPipelineIndex: atomic.NewUint32(0),
-		sender:               sender,
 		compression:          compressionfx.NewMockCompressor(),
 	}
 }
