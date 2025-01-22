db9b94ed11cccc06536a2bc13211184286d0bac2
Yang Song <yang.song@datadoghq.com>
2025-01-22 09:37:01 -0500

    try fix versions_equal

--- tasks/collector.py
+++ tasks/collector.py
@@ -98,6 +98,10 @@ def find_matching_components(manifest, components_to_match: dict, present: bool)
 
 
 def versions_equal(version1, version2):
+    idx = version1.find("/")
+    if idx != -1:
+        # version may be in the format of "v1.xx.0/v0.yyy.0"
+        version1 = version1[idx+1:]
     # strip leading 'v' if present
     if version1.startswith("v"):
         version1 = version1[1:]
