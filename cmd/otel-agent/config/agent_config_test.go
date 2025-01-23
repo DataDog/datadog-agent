5c7f971b4b9da6382b01e3f80a78dddbb2de674c
Ibraheem Aboulnaga <ibraheem.aboulnaga@datadoghq.com>
2025-01-23 15:09:53 -0500

    rebase

--- cmd/otel-agent/config/agent_config_test.go
+++ cmd/otel-agent/config/agent_config_test.go
@@ -80,7 +80,7 @@ func (suite *ConfigTestSuite) TestAgentConfigDefaults() {
 	assert.Equal(t, "https://trace.agent.datadoghq.com", c.Get("apm_config.apm_dd_url"))
 	assert.Equal(t, false, c.Get("apm_config.receiver_enabled"))
 	assert.Equal(t, false, c.Get("otlp_config.traces.span_name_as_resource_name"))
-	assert.Equal(t, []string{"enable_receive_resource_spans_v2", "enable_otlp_compute_top_level_by_span_kind"},
+	assert.Equal(t, []string{"enable_otlp_compute_top_level_by_span_kind"},
 		c.Get("apm_config.features"))
 }
 
@@ -104,7 +104,7 @@ func (suite *ConfigTestSuite) TestOperationAndResourceNameV2FeatureGate() {
 	assert.Equal(t, "https://trace.agent.datadoghq.com", c.Get("apm_config.apm_dd_url"))
 	assert.Equal(t, false, c.Get("apm_config.receiver_enabled"))
 	assert.Equal(t, false, c.Get("otlp_config.traces.span_name_as_resource_name"))
-	assert.Equal(t, []string{"enable_otlp_compute_top_level_by_span_kind"},
+	assert.Equal(t, []string{"enable_operation_and_resource_name_logic_v2", "enable_otlp_compute_top_level_by_span_kind"},
 		c.Get("apm_config.features"))
 }
 
