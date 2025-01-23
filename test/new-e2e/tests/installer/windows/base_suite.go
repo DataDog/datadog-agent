ee316713ddb15ba4702b4399aff5f226e27fe350
julien.lebot@datadoghq.com <julien.lebot@datadoghq.com>
2025-01-23 10:58:39 +0100

    Add missing comment package

--- test/new-e2e/tests/installer/windows/base_suite.go
+++ test/new-e2e/tests/installer/windows/base_suite.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package installer contains code for the E2E tests for the Datadog installer on Windows
 package installer
 
 import (
