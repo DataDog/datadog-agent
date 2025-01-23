9f5d71fc6ef510e23cfeeb8b19c22ef2f7fce4fb
Ibraheem Aboulnaga <ibraheem.aboulnaga@datadoghq.com>
2025-01-23 14:48:44 -0500

    Change ReceiveResourceSpansV2 to opt out

--- test/new-e2e/tests/otel/otlp-ingest/receive_resource_spans_v2_test.go
+++ test/new-e2e/tests/otel/otlp-ingest/receive_resource_spans_v2_test.go
@@ -33,12 +33,6 @@ datadog:
   logs:
     containerCollectAll: false
     containerCollectUsingFiles: false
-agents:
-  containers:
-    traceAgent:
-      env:
-        - name: DD_APM_FEATURES
-          value: 'enable_receive_resource_spans_v2'
 `
 	t.Parallel()
 	e2e.Run(t, &otlpIngestSpanReceiverV2TestSuite{}, e2e.WithProvisioner(awskubernetes.KindProvisioner(awskubernetes.WithAgentOptions(kubernetesagentparams.WithHelmValues(values)))))
