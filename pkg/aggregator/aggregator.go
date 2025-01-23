e66ec05cf9464793acf9125fe2bd794cc63c093e
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-01-23 10:04:28 +0100

    add config_id tag

--- pkg/aggregator/aggregator.go
+++ pkg/aggregator/aggregator.go
@@ -618,7 +618,10 @@ func (agg *BufferedAggregator) appendDefaultSeries(start time.Time, series metri
 	})
 
 	if agg.haAgent.Enabled() {
-		haAgentTags := append(agg.tags(true), "ha_agent_state:"+string(agg.haAgent.GetState()))
+		haAgentTags := append(agg.tags(false),
+			"config_id:"+agg.configID,
+			"ha_agent_state:"+string(agg.haAgent.GetState()),
+		)
 		// Send along a metric to show if HA Agent is running with ha_agent_state tag.
 		series.Append(&metrics.Serie{
 			Name:           fmt.Sprintf("datadog.%s.ha_agent.running", agg.agentName),
