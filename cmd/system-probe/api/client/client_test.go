ad028d7d03013092af55d62a80320265dd02cd57
Valeri Pliskin <valeri.pliskin@datadoghq.com>
2025-01-23 12:59:26 +0000

    added telemetry to the sysprobe remote client added UTs to test the counters

--- cmd/system-probe/api/client/client_test.go
+++ cmd/system-probe/api/client/client_test.go
@@ -27,15 +27,31 @@ func TestConstructURL(t *testing.T) {
 	assert.Equal(t, "http://sysprobe/zzzz/asdf", u)
 }
 
+type expectedTelemetryValues struct {
+	failedRequests     float64
+	failedResponses    float64
+	responseErrors     float64
+	malformedResponses float64
+}
+
+func validateTelemetry(t *testing.T, module string, expected expectedTelemetryValues) {
+	assert.Equal(t, expected.failedRequests, checkTelemetry.failedRequests.WithValues(module).Get(), "wrong failedRequest counter value")
+	assert.Equal(t, expected.failedResponses, checkTelemetry.failedResponses.WithValues(module).Get(), "wrong failedResponses counter value")
+	assert.Equal(t, expected.responseErrors, checkTelemetry.responseErrors.WithValues(module).Get(), "wrong responseErrors counter value")
+	assert.Equal(t, expected.malformedResponses, checkTelemetry.malformedResponses.WithValues(module).Get(), "wrong malformedResponses counter value")
+}
+
 func TestGetCheck(t *testing.T) {
 	type testData struct {
-		Str string
-		Num int
+		Str string `json:"Str"`
+		Num int    `json:"Num"`
 	}
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/test/check" {
 			_, _ = w.Write([]byte(`{"Str": "asdf", "Num": 42}`))
+		} else if r.URL.Path == "/malformed/check" {
+			_, _ = w.Write([]byte("1"))
 		} else {
 			w.WriteHeader(http.StatusNotFound)
 		}
@@ -46,8 +62,23 @@ func TestGetCheck(t *testing.T) {
 		return net.Dial("tcp", server.Listener.Addr().String())
 	}}}
 
+	//test happy flow
 	resp, err := GetCheck[testData](client, "test")
 	require.NoError(t, err)
 	assert.Equal(t, "asdf", resp.Str)
 	assert.Equal(t, 42, resp.Num)
+	validateTelemetry(t, "test", expectedTelemetryValues{0, 0, 0, 0})
+
+	//test responseError counter
+	resp, err = GetCheck[testData](client, "foo")
+	validateTelemetry(t, "foo", expectedTelemetryValues{0, 0, 1, 0})
+
+	//test malformedResponses counter
+	resp, err = GetCheck[testData](client, "malformed")
+	validateTelemetry(t, "malformed", expectedTelemetryValues{0, 0, 0, 1})
+
+	//test failedRequests counter
+	server.Close()
+	resp, err = GetCheck[testData](client, "test")
+	validateTelemetry(t, "test", expectedTelemetryValues{1, 0, 0, 0})
 }
