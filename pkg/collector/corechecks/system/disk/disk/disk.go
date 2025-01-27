280d939d26387a901dde4f2c1500ac98cbbcadc4
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2025-01-27 12:25:27 +0100

    add 'device_blacklist' deprecation message

--- pkg/collector/corechecks/system/disk/disk/disk.go
+++ pkg/collector/corechecks/system/disk/disk/disk.go
@@ -83,6 +83,7 @@ func (c *Check) diskConfigure(data integration.Data, initConfig integration.Data
 		"file_system_whitelist": "file_system_include",
 		"file_system_blacklist": "file_system_exclude",
 		"device_whitelist":      "device_include",
+		"device_blacklist":      "device_exclude",
 	}
 	for oldKey, newKey := range deprecationsInstanceConf {
 		if _, exists := unmarshalledInstanceConfig[oldKey]; exists {
