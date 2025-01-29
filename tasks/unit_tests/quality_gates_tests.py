081fad1d58a2c05961b4f3693a5021db0e28e994
Pythyu <antoine.zellmeyer@datadoghq.com>
2025-01-29 16:21:45 +0100

    WIP

--- tasks/unit_tests/quality_gates_tests.py
+++ tasks/unit_tests/quality_gates_tests.py
@@ -1,10 +1,11 @@
 import unittest
 from unittest.mock import ANY, MagicMock, patch
 
-from invoke import MockContext
+from invoke import Context, MockContext, Result
 
 from tasks.quality_gates import display_pr_comment
 from tasks.static_quality_gates.lib.gates_lib import GateMetricHandler
+from tasks.static_quality_gates.static_quality_gate_docker_agent_amd64 import calculate_image_on_disk_size
 
 
 class TestQualityGatesPrMessage(unittest.TestCase):
@@ -130,3 +131,31 @@ class TestQualityGatesPrMessage(unittest.TestCase):
             title='Static quality checks ❌',
             body='Please find below the results from static quality gates\n### Error\n\n|Result|Quality gate|On disk size|On disk size limit|On wire size|On wire size limit|\n|----|----|----|----|----|----|\n|❌|gateA|DataNotFound|DataNotFound|DataNotFound|DataNotFound|\n<details>\n<summary>Gate failure full details</summary>\n\n|Quality gate|Error type|Error message|\n|----|---|--------|\n|gateA|AssertionError|some_msg_A|\n\n</details>\n\n\n',
         )
+
+
+class DynamicMockContext:
+    def __init__(self, actual_context, mock_context):
+        self.actual_context = actual_context
+        self.mock_context = mock_context
+
+    def run(self, *args, **kwargs):
+        try:
+            self.mock_context.run(*args, **kwargs)
+        except NotImplementedError:
+            self.actual_context.run(*args, **kwargs)
+
+
+class TestOnDiskImageSizeCalculation(unittest.TestCase):
+    def test_compute_image_size(self):
+        actualContext = Context()
+        c = MockContext(
+            run={
+                'crane pull some_url output.tar"': Result('Done'),
+            }
+        )
+        actualContext.run("cd tasks/unit_tests/testdata/fake_agent_image/with_tar_gz_archive/")
+        context = DynamicMockContext(actual_context=actualContext, mock_context=c)
+        calculate_image_on_disk_size(context, "some_url")
+
+    def test_metadata_only(self):
+        pass
