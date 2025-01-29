301a8ab491d43a52ebe2f35da5ba99073658a56a
Branden Clark <branden.clark@datadoghq.com>
2025-01-29 15:56:37 -0500

    run same tests on Windows and Linux

--- test/new-e2e/tests/fips-compliance/fips_ciphers_win_test.go
+++ test/new-e2e/tests/fips-compliance/fips_ciphers_win_test.go
@@ -10,8 +10,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/provisioners"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/utils/e2e/client"
@@ -30,7 +28,6 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -44,7 +41,7 @@ type multiVMEnv struct {
 }
 
 type fipsServerWinSuite struct {
-	e2e.BaseSuite[multiVMEnv]
+	fipsServerSuite[multiVMEnv]
 }
 
 func TestFIPSCiphersWindowsSuite(t *testing.T) {
@@ -60,8 +57,20 @@ func TestFIPSCiphersWindowsSuite(t *testing.T) {
 func (s *fipsServerWinSuite) SetupSuite() {
 	s.BaseSuite.SetupSuite()
 
-	// Enable FIPS mode
-	err := windowsCommon.EnableFIPSMode(s.Env().WindowsVM)
+	agentHost := s.Env().WindowsVM
+	dockerHost := s.Env().LinuxDockerVM
+
+	// supply workers to base fipsServerSuite
+	composeFilePath := "/tmp/docker-compose.yaml"
+	s.fipsServer = newFIPSServer(dockerHost, composeFilePath)
+	s.generateTestTraffic = s.generateTraffic
+
+	// Write docker-compose.yaml to disk
+	_, err := dockerHost.WriteFile(composeFilePath, dockerFipsServerCompose)
+	require.NoError(s.T(), err)
+
+	// Enable FIPS mode for OS
+	err = windowsCommon.EnableFIPSMode(agentHost)
 	require.NoError(s.T(), err)
 
 	// Install FIPS Agent
@@ -73,97 +82,55 @@ func (s *fipsServerWinSuite) SetupSuite() {
 	}
 	agentPackage, err := windowsAgent.GetPackageFromEnv()
 	require.NoError(s.T(), err)
+	s.T().Logf("Using Agent: %#v", agentPackage)
 	logFile := filepath.Join(s.SessionOutputDir(), "install.log")
-	_, err = windowsAgent.InstallAgent(s.Env().WindowsVM,
+	_, err = windowsAgent.InstallAgent(agentHost,
 		windowsAgent.WithPackage(agentPackage),
 		windowsAgent.WithInstallLogFile(logFile))
 	require.NoError(s.T(), err)
 }
 
-func (s *fipsServerWinSuite) fipsServerUp(tc cipherTestCase) {
-	// Write docker-compose.yaml to disk
-	host := s.Env().LinuxDockerVM
-	_, err := host.WriteFile("/tmp/docker-compose.yaml", dockerFipsServerCompose)
-	require.NoError(s.T(), err)
-
-	// stop currently running server, so we can reset logs+env
-	s.fipsServerDown()
-
-	// start datadog/appd-fips-server with env vars from the test case
-	envVars := map[string]string{
-		"CERT": tc.cert,
-	}
-	if tc.cipher != "" {
-		envVars["CIPHER"] = fmt.Sprintf("-c %s", tc.cipher)
-	}
-	if tc.tlsMax != "" {
-		envVars["TLS_MAX"] = fmt.Sprintf("--tls-max %s", tc.tlsMax)
-	}
-
-	cmd := "docker-compose -f /tmp/docker-compose.yaml up --detach --wait --timeout 300"
-	_, err = host.Execute(cmd, client.WithEnvVariables(envVars))
-	require.NoError(s.T(), err)
-
-	// Wait for container to start and ensure it's a fresh instance
-	require.EventuallyWithT(s.T(), func(t *assert.CollectT) {
-		serverLogs, _ := host.Execute("docker logs dd-fips-server")
-		assert.Contains(t, serverLogs, "Server Starting...", "Server should start")
-		assert.Equal(t, 1, strings.Count(serverLogs, "Server Starting..."), "Server should start only once, logs from previous runs should not be present")
-	}, 60*time.Second, 5*time.Second)
-}
-
-func (s *fipsServerWinSuite) fipsServerDown() {
-	host := s.Env().LinuxDockerVM
-	_, err := host.Execute("docker-compose -f /tmp/docker-compose.yaml down")
-	require.NoError(s.T(), err)
-}
-
-func (s *fipsServerWinSuite) TestFIPSCiphers() {
-	host := s.Env().WindowsVM
+func (s *fipsServerWinSuite) generateTraffic() {
+	agentHost := s.Env().WindowsVM
 	dockerHost := s.Env().LinuxDockerVM
-
-	for _, tc := range testcases {
-		s.Run(fmt.Sprintf("FIPS enabled testing '%v -c %v' (should connect %v)", tc.cert, tc.cipher, tc.want), func() {
-			// Start the fips-server and waits for it to be ready
-			s.fipsServerUp(tc)
-			s.T().Cleanup(func() {
-				s.fipsServerDown()
-			})
-
-			agentEnv := map[string]string{
-				// datadog/apps-fips-server creates self-signed certed
-				"DD_SKIP_SSL_VALIDATION": "true",
-				// point diagnose command at datadog/apps-fips-server container
-				"DD_DD_URL": fmt.Sprintf(`https://%s:443`, dockerHost.HostOutput.Address),
-			}
-			agent := `C:\Program Files\Datadog\Datadog Agent\bin\agent.exe`
-			cmd := fmt.Sprintf(`& '%s' diagnose --include connectivity-datadog-core-endpoints --local`, agent)
-			host.Execute(cmd, client.WithEnvVariables(agentEnv))
-
-			serverLogs := dockerHost.MustExecute("docker logs dd-fips-server")
-			if tc.want {
-				assert.Contains(s.T(), serverLogs, fmt.Sprintf("Negotiated cipher suite: %s", tc.cipher))
-			} else {
-				assert.Contains(s.T(), serverLogs, "no cipher suite supported by both client and server")
-			}
-		})
+	agentEnv := map[string]string{
+		// datadog/apps-fips-server creates self-signed cert
+		"DD_SKIP_SSL_VALIDATION": "true",
+		// point diagnose command at datadog/apps-fips-server container
+		"DD_DD_URL": fmt.Sprintf(`https://%s:443`, dockerHost.HostOutput.Address),
 	}
+	agent := `C:\Program Files\Datadog\Datadog Agent\bin\agent.exe`
+	cmd := fmt.Sprintf(`& '%s' diagnose --include connectivity-datadog-core-endpoints --local`, agent)
+	agentHost.Execute(cmd, client.WithEnvVariables(agentEnv))
 }
 
 // multiVMEnvProvisioner provisions a Windows VM and a Linux Docker VM
 //
 // This allows us to run the datadog/apps-fips-server container on the Linux VM,
-// and query it from the Agent running on the Windows VM.
+// and query it from the Agent running on the Windows VM. This is different from
+// the Linux test which runs the Agent and fips-server on the same host with environments.DockerHost.
+// This means the DockerHost provisioner args are not usable in this test, and this test isn't using any
+// special params right now so we didn't copy over the param code.
 //
-// Why do we do it this way?
-//   - The fips_ciphers_nix_test.go uses test-infra DockerHost which doesn't support Windows.
-//   - The fips-server is a go binary, so it might be portable to Windows, but TBD.
-//   - The datadog/apps-fips-server container is only built for Linux, but Windows
-//     Server / Docker EE do NOT support Linux containers.
+// Why use a custom provisioner?
+//   - fips_ciphers_nix_test.go uses test-infra DockerHost which doesn't support Windows.
+//   - E2E environments/provisioners are not flexible/combineable, so we must create one from scratch to customize the env
+//
+// Can fips-server run on Windows?
+//   - The datadog/apps-fips-server container is only built for Linux
+//   - it's a go binary, so it might be portable to Windows / Windows container
+//
+// Can we run the Linux container on Windows?
+//   - Windows Server / Docker EE do not support Linux containers
+//   - AWS only has built-in Windows Server AMIs, not clients, so its more effort to use
+//     a custom AMI and then install Docker Desktop to get Linux container support.
 //   - AWS only supports nested virtualization on metal instances, so that's a hurdle to using WSL2,
 //     plus would also have to do the WSL2 and docker setup manually.
-//   - E2E environments/provisioners are not flexible, so we must create our own to customize the env
-//   - E2E environments/provisioners are not reusable, so we have to copy/paste the DockerHost setup
+//
+// Long term vision / ideal scenario:
+//   - port fips-server to Windows and create a Windows container image
+//   - Add Windows support to test-infra DockerHost
+//   - Use DockerHost env in this test
 func multiVMEnvProvisioner() provisioners.PulumiEnvRunFunc[multiVMEnv] {
 	return func(ctx *pulumi.Context, env *multiVMEnv) error {
 		awsEnv, err := aws.NewEnvironment(ctx)
