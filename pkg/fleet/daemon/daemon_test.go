0f368d2c15eef41096a5347b0660241a1440c4e4
Julien Lebot <julien.lebot@datadoghq.com>
2025-01-31 15:08:27 +0100

    Fix daemon tests

--- pkg/fleet/daemon/daemon_test.go
+++ pkg/fleet/daemon/daemon_test.go
@@ -59,7 +59,12 @@ func (m *testPackageManager) ConfigStates() (map[string]repository.State, error)
 	return args.Get(0).(map[string]repository.State), args.Error(1)
 }
 
-func (m *testPackageManager) Install(ctx context.Context, url string, installArgs []string, _ bool) error {
+func (m *testPackageManager) Install(ctx context.Context, url string, installArgs []string) error {
+	args := m.Called(ctx, url, installArgs)
+	return args.Error(0)
+}
+
+func (m *testPackageManager) ForceInstall(ctx context.Context, url string, installArgs []string) error {
 	args := m.Called(ctx, url, installArgs)
 	return args.Error(0)
 }
