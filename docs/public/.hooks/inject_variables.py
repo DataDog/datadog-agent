203d066634ad907ed42a5d54c7bdfa7c1ec15a00
Ofek Lev <ofekmeister@gmail.com>
2025-02-01 00:17:45 -0500

    refactor

--- docs/public/.hooks/inject_variables.py
+++ docs/public/.hooks/inject_variables.py
@@ -7,17 +7,20 @@ from markdown.preprocessors import Preprocessor
 @cache
 def variable_replacements():
     return {
-        "<<<GO_VERSION>>>": get_go_version(),
+        f"<<<{variable}>>>": replacement
+        for variable, replacement in (
+            ("GO_VERSION", get_go_version()),
+        )
     }
 
 
 def get_go_version():
-    return Path('.go-version').read_text(encoding="utf-8")
+    return Path(".go-version").read_text(encoding="utf-8").strip()
 
 
 class VariableInjectionPreprocessor(Preprocessor):
     def run(self, lines):  # noqa: PLR6301
-        markdown = '\n'.join(lines)
+        markdown = "\n".join(lines)
         for variable, replacement in variable_replacements().items():
             markdown = markdown.replace(variable, replacement)
 
