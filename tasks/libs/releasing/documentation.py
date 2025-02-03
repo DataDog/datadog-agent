ea457b85eb670822ca63b701e76050f46922761b
Cédric Van Rompay <cedric.vanrompay@datadoghq.com>
2025-02-03 17:40:50 +0100

    remove duplicate entry in documentation.py

--- tasks/libs/releasing/documentation.py
+++ tasks/libs/releasing/documentation.py
@@ -9,7 +9,6 @@ SPACE_KEY = "agent"
 NON_RELEASING_TEAMS = {
     'telemetry-and-analytics',
     'documentation',
-    'sdlc-security',
     'single-machine-performance',
     'agent-all',
     'apm-core-reliability-and-performance',
