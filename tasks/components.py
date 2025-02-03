d1e127fc2e1af03eb9f381c6784dab055084891b
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-02-03 10:39:15 +0100

    remove exception tasks/components.py

--- tasks/components.py
+++ tasks/components.py
@@ -117,7 +117,6 @@ components_classic_style = [
     'comp/metadata/inventorychecks/inventorychecksimpl',
     'comp/metadata/inventoryhost/inventoryhostimpl',
     'comp/metadata/inventoryotel/inventoryotelimpl',
-    'comp/metadata/haagent/haagentimpl',
     'comp/metadata/packagesigning/packagesigningimpl',
     'comp/metadata/resources/resourcesimpl',
     'comp/metadata/runner/runnerimpl',
