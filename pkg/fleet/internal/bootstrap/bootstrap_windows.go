b84c4b93a7e94d4d2c7e80846ed5115e69ad9121
Julien Lebot <julien.lebot@datadoghq.com>
2025-02-04 15:17:50 +0100

    Remove comment

--- pkg/fleet/internal/bootstrap/bootstrap_windows.go
+++ pkg/fleet/internal/bootstrap/bootstrap_windows.go
@@ -43,8 +43,6 @@ func install(ctx context.Context, env *env.Env, url string, experiment bool) err
 	if experiment {
 		return cmd.InstallExperiment(ctx, url)
 	}
-
-	// true = force install, even if the version appears to be already installed.
 	return cmd.ForceInstall(ctx, url, nil)
 }
 
