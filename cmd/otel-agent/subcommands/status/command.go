6d64d8dc3ca223ea3199c24c880f93bdfb93fb18
Stanley Liu <stanley.liu@datadoghq.com>
2025-02-06 14:52:25 -0500

    Add authtoken component and fix fx

--- cmd/otel-agent/subcommands/status/command.go
+++ cmd/otel-agent/subcommands/status/command.go
@@ -7,19 +7,22 @@
 package status
 
 import (
-	"context"
 	"os"
 
 	"github.com/spf13/cobra"
 	"go.uber.org/fx"
 
-	agentConfig "github.com/DataDog/datadog-agent/cmd/otel-agent/config"
 	"github.com/DataDog/datadog-agent/cmd/otel-agent/subcommands"
+	"github.com/DataDog/datadog-agent/comp/api/authtoken/fetchonlyimpl"
 	coreconfig "github.com/DataDog/datadog-agent/comp/core/config"
+	log "github.com/DataDog/datadog-agent/comp/core/log/def"
+	logfx "github.com/DataDog/datadog-agent/comp/core/log/fx"
+	"github.com/DataDog/datadog-agent/comp/core/secrets"
+	"github.com/DataDog/datadog-agent/comp/core/secrets/secretsimpl"
 	status "github.com/DataDog/datadog-agent/comp/otelcol/status/def"
 	otelagentStatusfx "github.com/DataDog/datadog-agent/comp/otelcol/status/fx"
-	pkgconfigenv "github.com/DataDog/datadog-agent/pkg/config/env"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"github.com/DataDog/datadog-agent/pkg/util/option"
 )
 
 type dependencies struct {
@@ -30,7 +33,7 @@ type dependencies struct {
 
 const headerText = "==========\nOTel Agent\n==========\n"
 
-// MakeCommand returns a `version` command to be used by agent binaries.
+// MakeCommand returns a `status` command to be used by agent binaries.
 func MakeCommand(globalConfGetter func() *subcommands.GlobalParams) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "status",
@@ -38,20 +41,16 @@ func MakeCommand(globalConfGetter func() *subcommands.GlobalParams) *cobra.Comma
 		Long:  ``,
 		RunE: func(*cobra.Command, []string) error {
 			globalParams := globalConfGetter()
-			acfg, err := agentConfig.NewConfigComponent(context.Background(), globalParams.CoreConfPath, globalParams.ConfPaths)
-			if err != nil && err != agentConfig.ErrNoDDExporter {
-				return err
-			}
-			uris := append(globalParams.ConfPaths, globalParams.Sets...)
 			return fxutil.OneShot(
 				runStatus,
-				fx.Supply(uris),
-				fx.Provide(func() (coreconfig.Component, error) {
-					if err != agentConfig.ErrNoDDExporter {
-						pkgconfigenv.DetectFeatures(acfg)
-					}
-					return acfg, nil
-				}),
+				fx.Supply(coreconfig.NewAgentParams(globalParams.CoreConfPath, coreconfig.WithExtraConfFiles(globalParams.ConfPaths))),
+				fx.Supply(option.None[secrets.Component]()),
+				fx.Supply(secrets.NewEnabledParams()),
+				fx.Supply(log.ForOneShot(globalParams.LoggerName, "off", true)),
+				coreconfig.Module(),
+				secretsimpl.Module(),
+				logfx.Module(),
+				fetchonlyimpl.Module(),
 				otelagentStatusfx.Module(),
 			)
 		},
--- comp/otelcol/status/impl/status.go
+++ comp/otelcol/status/impl/status.go
@@ -15,6 +15,7 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/DataDog/datadog-agent/comp/api/authtoken"
 	"github.com/DataDog/datadog-agent/comp/core/config"
 	statusComponent "github.com/DataDog/datadog-agent/comp/core/status"
 	ddflareextension "github.com/DataDog/datadog-agent/comp/otelcol/ddflareextension/def"
@@ -28,7 +29,8 @@ var templatesFS embed.FS
 
 // Requires defines the dependencies of the status component.
 type Requires struct {
-	Config config.Component
+	Config    config.Component
+	Authtoken authtoken.Component
 }
 
 // Provides contains components provided by status constructor.
@@ -40,6 +42,7 @@ type Provides struct {
 type statusProvider struct {
 	Config         config.Component
 	client         *http.Client
+	authToken      authtoken.Component
 	receiverStatus map[string]interface{}
 	exporterStatus map[string]interface{}
 }
@@ -66,8 +69,9 @@ type prometheusRuntimeConfig struct {
 // NewComponent creates a new status component.
 func NewComponent(reqs Requires) Provides {
 	comp := statusProvider{
-		Config: reqs.Config,
-		client: apiutil.GetClient(false),
+		Config:    reqs.Config,
+		client:    apiutil.GetClient(false),
+		authToken: reqs.Authtoken,
 		receiverStatus: map[string]interface{}{
 			"spans":           0.0,
 			"metrics":         0.0,
@@ -183,7 +187,11 @@ func (s statusProvider) populatePrometheusStatus(prometheusURL string) error {
 
 func (s statusProvider) populateStatus() map[string]interface{} {
 	extensionURL := s.Config.GetString("otelcollector.extension_url")
-	resp, err := apiutil.DoGet(s.client, extensionURL, apiutil.CloseConnection)
+	options := apiutil.ReqOptions{
+		Conn:      apiutil.CloseConnection,
+		Authtoken: s.authToken.Get(),
+	}
+	resp, err := apiutil.DoGetWithOptions(s.client, extensionURL, &options)
 	if err != nil {
 		return map[string]interface{}{
 			"url":   extensionURL,
