46f401ab935b53dfc4f3d7130749a403c4bb34a7
Pythyu <antoine.zellmeyer@datadoghq.com>
2025-02-07 12:17:02 +0100

    fix(gate): wrong gate parameter

--- tasks/static_quality_gates/static_quality_gate_docker_cluster_agent_arm64.py
+++ tasks/static_quality_gates/static_quality_gate_docker_cluster_agent_arm64.py
@@ -3,5 +3,5 @@ from tasks.static_quality_gates.lib.docker_agent_lib import generic_docker_agent
 
 def entrypoint(**kwargs):
     generic_docker_agent_quality_gate(
-        gate_name="static_quality_gate_docker_dogstatsd_arm64.", arch="amd64", flavor="dogstatsd", **kwargs
+        gate_name="static_quality_gate_docker_cluster_agent_arm64", arch="arm64", flavor="cluster-agent-nightly", **kwargs
     )
