351a7cccac976946e10283629e2b7fc8cc1f84d2
Mark Spicer <mark.spicer@datadoghq.com>
2025-02-07 08:01:28 -0500

    fix(ssi): remove newly added config variables (#33803)

--- pkg/clusteragent/admission/mutate/autoinstrumentation/filter.go
+++ pkg/clusteragent/admission/mutate/autoinstrumentation/filter.go
@@ -12,23 +12,10 @@ import (
 	mutatecommon "github.com/DataDog/datadog-agent/pkg/clusteragent/admission/mutate/common"
 )
 
-// FilterConfig provides a struct to configure the filter.
-type FilterConfig struct {
-	enabled            bool
-	enabledNamespaces  []string
-	disabledNamespaces []string
-}
-
-// NewFilterConfig creates a new FilterConfig from the datadogConfig.
-func NewFilterConfig(datadogConfig config.Component) *FilterConfig {
-	return &FilterConfig{
-		enabled:            datadogConfig.GetBool("apm_config.instrumentation.enabled"),
-		enabledNamespaces:  datadogConfig.GetStringSlice("apm_config.instrumentation.enabled_namespaces"),
-		disabledNamespaces: datadogConfig.GetStringSlice("apm_config.instrumentation.disabled_namespaces"),
-	}
-}
-
 // NewFilter creates a new MutationFilter from the provided FilterConfig.
-func NewFilter(cfg *FilterConfig) (mutatecommon.MutationFilter, error) {
-	return mutatecommon.NewDefaulFilter(cfg.enabled, cfg.enabledNamespaces, cfg.disabledNamespaces)
+func NewFilter(datadogConfig config.Component) (mutatecommon.MutationFilter, error) {
+	enabled := datadogConfig.GetBool("apm_config.instrumentation.enabled")
+	enabledNamespaces := datadogConfig.GetStringSlice("apm_config.instrumentation.enabled_namespaces")
+	disabledNamespaces := datadogConfig.GetStringSlice("apm_config.instrumentation.disabled_namespaces")
+	return mutatecommon.NewDefaultFilter(enabled, enabledNamespaces, disabledNamespaces)
 }
