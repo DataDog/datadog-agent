e91801e5c9e519f59bae8c109f2897f216c4f3d7
Thibaud Cheruy <thibaud.cheruy@datadoghq.com>
2025-02-07 12:29:46 +0100

    lint

--- comp/netflow/goflowlib/logger.go
+++ comp/netflow/goflowlib/logger.go
@@ -15,26 +15,32 @@ type GoflowLoggerAdapter struct {
 	log.Component
 }
 
+// Printf logs the given formatted arguments at the info level
 func (g *GoflowLoggerAdapter) Printf(format string, params ...interface{}) {
 	g.Infof(format, params...)
 }
 
+// Errorf logs the given formatted arguments at the error level
 func (g *GoflowLoggerAdapter) Errorf(format string, params ...interface{}) {
 	g.Component.Errorf(format, params...)
 }
 
+// Error logs the given arguments, separated by spaces, at the error level
 func (g *GoflowLoggerAdapter) Error(params ...interface{}) {
 	g.Component.Error(params...)
 }
 
+// Warnf logs the given formatted arguments at the warn level
 func (g *GoflowLoggerAdapter) Warnf(format string, params ...interface{}) {
 	g.Component.Warnf(format, params...)
 }
 
+// Warn logs the given arguments, separated by spaces, at the warn level
 func (g *GoflowLoggerAdapter) Warn(params ...interface{}) {
 	g.Component.Warn(params...)
 }
 
+// Fatalf logs the given formatted arguments at the critical level
 func (g *GoflowLoggerAdapter) Fatalf(format string, params ...interface{}) {
 	g.Criticalf(format, params...)
 }
