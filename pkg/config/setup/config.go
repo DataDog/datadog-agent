8d8e3dd7e7f75b51ce16b5ddfc2100295be84f07
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2025-02-10 11:25:38 +0100

    feat(config.go): turn default value of 'ec2_imdsv2_transition_payload_enabled' to true

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -567,7 +567,7 @@ func InitConfig(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("collect_ec2_tags", false)
 	config.BindEnvAndSetDefault("collect_ec2_tags_use_imds", false)
 	config.BindEnvAndSetDefault("exclude_ec2_tags", []string{})
-	config.BindEnvAndSetDefault("ec2_imdsv2_transition_payload_enabled", false)
+	config.BindEnvAndSetDefault("ec2_imdsv2_transition_payload_enabled", true)
 
 	// ECS
 	config.BindEnvAndSetDefault("ecs_agent_url", "") // Will be autodetected
