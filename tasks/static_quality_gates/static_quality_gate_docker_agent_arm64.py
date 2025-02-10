9895bca163210bf45623300690a01753bfcec484
Marethyu <45374460+Pythyu@users.noreply.github.com>
2025-02-10 09:57:38 +0100

    Add jmx, dogstatsd, cluster-agent and arm64 images static quality gates (#33754)
    Co-authored-by: Célian Raimbault <161456554+CelianR@users.noreply.github.com>

--- tasks/static_quality_gates/static_quality_gate_docker_agent_arm64.py
+++ tasks/static_quality_gates/static_quality_gate_docker_agent_arm64.py
@@ -2,4 +2,4 @@ from tasks.static_quality_gates.lib.docker_agent_lib import generic_docker_agent
 
 
 def entrypoint(**kwargs):
-    generic_docker_agent_quality_gate(gate_name="static_quality_gate_docker_agent_amd64", arch="amd64", **kwargs)
+    generic_docker_agent_quality_gate(gate_name="static_quality_gate_docker_agent_arm64", arch="arm64", **kwargs)
