8000ab80d76d3186d4095f36569c72d5c62cdd14
Pythyu <antoine.zellmeyer@datadoghq.com>
2025-02-11 11:48:17 +0100

    feat(rpm): add rpm2cpi to docker image

--- tasks/static_quality_gates/static_quality_gate_dogstatsd_deb_amd64.py
+++ tasks/static_quality_gates/static_quality_gate_dogstatsd_deb_amd64.py
@@ -3,5 +3,5 @@ from tasks.static_quality_gates.lib.package_agent_lib import generic_package_age
 
 def entrypoint(**kwargs):
     generic_package_agent_quality_gate(
-        "static_quality_gate_dogstatsd_deb_amd64", "amd64", "debian", "dogstatsd", **kwargs
+        "static_quality_gate_dogstatsd_deb_amd64", "amd64", "debian", "datadog-dogstatsd", **kwargs
     )
--- tasks/static_quality_gates/static_quality_gate_dogstatsd_deb_arm64.py
+++ tasks/static_quality_gates/static_quality_gate_dogstatsd_deb_arm64.py
@@ -3,5 +3,5 @@ from tasks.static_quality_gates.lib.package_agent_lib import generic_package_age
 
 def entrypoint(**kwargs):
     generic_package_agent_quality_gate(
-        "static_quality_gate_dogstatsd_deb_arm64", "arm64", "debian", "dogstatsd", **kwargs
+        "static_quality_gate_dogstatsd_deb_arm64", "arm64", "debian", "datadog-dogstatsd", **kwargs
     )
