3bd2ecc4cc94f37a403c178dd3d1e0476416586a
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-02-11 09:58:39 +0100

    [flake-marker-output-category] unit tests: Updated

--- tasks/unit_tests/e2e_testing_tests.py
+++ tasks/unit_tests/e2e_testing_tests.py
@@ -47,3 +47,39 @@ class TestE2ETesting(unittest.TestCase):
                 "TestGetPayloadContainerizedWithDocker0",
             },
         )
+
+    @patch("tasks.new_e2e_tests.pretty_print_test_logs")
+    @patch("tasks.libs.common.utils.running_in_ci", new=MagicMock(return_value=True))
+    def test_pretty_print_inner_depth1(self, p=None):
+        flakes_file = "tasks/unit_tests/testdata/flakes_1.yaml"
+        path = "tasks/unit_tests/testdata/test_output_inner.json"
+
+        pretty_print_logs(path, post_process_output(path), flakes_files=[flakes_file], test_depth=1)
+
+        # Successful / non flaky, successful / flaky
+        self.assertEqual(p.call_count, 2)
+        args1 = p.call_args_list[0][0][0]
+        args2 = p.call_args_list[1][0][0]
+
+        # TestParent contains both flaky and not flaky tests
+        self.assertEqual({name for (_, name) in args1.keys()}, {"TestParent"})
+        self.assertEqual({name for (_, name) in args2.keys()}, {"TestParent", "TestParentFlaky"})
+
+    @patch("tasks.new_e2e_tests.pretty_print_test_logs")
+    @patch("tasks.libs.common.utils.running_in_ci", new=MagicMock(return_value=True))
+    def test_pretty_print_inner_depth2(self, p=None):
+        flakes_file = "tasks/unit_tests/testdata/flakes_1.yaml"
+        path = "tasks/unit_tests/testdata/test_output_inner.json"
+
+        pretty_print_logs(path, post_process_output(path), flakes_files=[flakes_file], test_depth=2)
+
+        # Successful / non flaky, successful / flaky
+        self.assertEqual(p.call_count, 2)
+        args1 = p.call_args_list[0][0][0]
+        args2 = p.call_args_list[1][0][0]
+
+        # Both have at least one test with this state
+        self.assertEqual({name for (_, name) in args1.keys()}, {"TestParent", "TestParent/Child"})
+        self.assertEqual(
+            {name for (_, name) in args2.keys()}, {"TestParentFlaky", "TestParentFlaky/Child", "TestParent/Child2"}
+        )
