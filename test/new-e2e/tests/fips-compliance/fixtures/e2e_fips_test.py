e09055e485cf508e53a73a3c0ce6a1c1b1513544
David Kirov <david.kirov@datadoghq.com>
2025-02-12 16:12:20 +0100

    Add Python libraries to Windows FIPS tests

--- test/new-e2e/tests/fips-compliance/fixtures/e2e_fips_test.py
+++ test/new-e2e/tests/fips-compliance/fixtures/e2e_fips_test.py
@@ -1,6 +1,8 @@
 import _hashlib
+import ssl
 import win32api
 from checks import AgentCheck
+from cryptography.hazmat.backends import default_backend
 
 
 class FIPSModeCheck(AgentCheck):
@@ -10,6 +12,8 @@ class FIPSModeCheck(AgentCheck):
         # it doesn't mean that FIPS mode is operating correctly, so we check
         # that the FIPS provider DLL is loaded as well
         self.gauge('e2e.fips_dll_loaded', _is_fips_dll_loaded())
+        self.gauge('e2e.fips_cryptography', _is_cryptography_fips())
+        self.gauge('e2e.fips_ssl', _is_ssl_fips())
 
 
 def _is_fips_dll_loaded():
@@ -19,3 +23,23 @@ def _is_fips_dll_loaded():
     except Exception:
         handle = 0
     return handle != 0
+
+
+def _is_cryptography_fips():
+    try:
+        default_backend()._enable_fips()
+        return 1
+    except Exception:
+        return 0
+
+
+def _is_ssl_fips():
+    # we expect ssl to throw an exception in FIPS mode whenever we try to set
+    # the context cipher list to "MD5"
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    try:
+        ctx.set_ciphers("MD5")
+        return 0
+    except ssl.SSLError:
+        return 1
+
