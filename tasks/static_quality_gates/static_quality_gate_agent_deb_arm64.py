9969044b652faa1c0821ce02d9ad5eb1d145b030
Marethyu <45374460+Pythyu@users.noreply.github.com>
2025-02-13 10:32:43 +0100

    New package static quality gates (#33878)

--- tasks/static_quality_gates/static_quality_gate_agent_deb_arm64.py
+++ tasks/static_quality_gates/static_quality_gate_agent_deb_arm64.py
@@ -3,5 +3,5 @@ from tasks.static_quality_gates.lib.package_agent_lib import generic_package_age
 
 def entrypoint(**kwargs):
     generic_package_agent_quality_gate(
-        "static_quality_gate_agent_deb_amd64", "amd64", "debian", "datadog-agent", **kwargs
+        "static_quality_gate_agent_deb_arm64", "arm64", "debian", "datadog-agent", **kwargs
     )
