f9f4489d3b7b1ef9fff366923ccc5b42c4a886e8
Arthur Bellal <arthur.bellal@datadoghq.com>
2025-02-17 21:02:44 +0100

    new script architecture

--- pkg/fleet/installer/setup/install.sh
+++ pkg/fleet/installer/setup/install.sh
@@ -1,54 +1,57 @@
 #!/bin/bash
-# Installer for Datadog (www.datadoghq.com).
-# Copyright 2016-present Datadog, Inc.
-#
 set -e
 
-if [ "$(uname -s)" != "Linux" ] || { [ "$(uname -m)" != "x86_64" ] && [ "$(uname -m)" != "aarch64" ]; }; then
-  echo "This installer only supports linux running on amd64 or arm64." >&2
+os=$(uname -s)
+arch=$(uname -m)
+
+if [[ "$os" != "Linux" || ("$arch" != "x86_64" && "$arch" != "aarch64") ]]; then
+  echo "This installer only supports Linux running on amd64 or arm64." >&2
   exit 1
 fi
 
+if ! command -v sha256sum >/dev/null || ! (command -v curl >/dev/null || command -v wget >/dev/null); then
+  echo "This installer requires sha256sum and either curl or wget to be installed." >&2
+  exit 1
+fi
+
+case "$arch" in
+x86_64)
+  installer_url="LINUX_INSTALLER_URL_AMD64"
+  installer_sha256="LINUX_INSTALLER_URL_AMD64_SHA256"
+  ;;
+aarch64)
+  installer_url="LINUX_INSTALLER_URL_ARM64"
+  installer_sha256="LINUX_INSTALLER_URL_ARM64_SHA256"
+  ;;
+esac
+
 tmp_dir="/opt/datadog-packages/tmp"
-downloader_path="${tmp_dir}/download-installer"
+tmp_bin="${tmp_dir}/installer"
 
-install() {
-  if [ "$UID" == "0" ]; then
-    sudo_cmd=''
-    sudo_cmd_with_envs=''
-  else
-    sudo_cmd='sudo'
-    sudo_cmd_with_envs='sudo -E'
-  fi
+if ((UID == 0)); then
+  sudo_cmd=()
+  sudo_env_cmd=()
+else
+  sudo_cmd=(sudo)
+  sudo_env_cmd=(sudo -E)
+fi
 
-  $sudo_cmd mkdir -p "${tmp_dir}"
-  case "$(uname -m)" in
-  x86_64)
-    write_installer_amd64 "$sudo_cmd" "$downloader_path"
-    ;;
-  aarch64)
-    write_installer_arm64 "$sudo_cmd" "$downloader_path"
-    ;;
-  esac
-  $sudo_cmd chmod +x "${downloader_path}"
-  echo "Starting the Datadog installer..."
-  $sudo_cmd_with_envs "${downloader_path}" "$@"
-  $sudo_cmd rm -f "${downloader_path}"
-}
+"${sudo_cmd[@]}" mkdir -p "$tmp_dir"
 
-# Embedded binaries used to install Datadog.
-# Source: https://github.com/DataDog/datadog-agent/tree/INSTALLER_COMMIT/pkg/fleet/installer
-# DO NOT EDIT THIS SECTION MANUALLY.
-write_installer_amd64() {
-  local sudo_cmd=$1
-  local path=$2
-  base64 -d <<<"DOWNLOADER_BIN_LINUX_AMD64" | $sudo_cmd tee "${path}" >/dev/null
-}
-write_installer_arm64() {
-  local sudo_cmd=$1
-  local path=$2
-  base64 -d <<<"DOWNLOADER_BIN_LINUX_ARM64" | $sudo_cmd tee "${path}" >/dev/null
-}
+echo "Downloading the Datadog installer..."
+if command -v curl >/dev/null; then
+  curl -L --retry 3 -o "$tmp_bin" "$installer_url"
+else
+  wget --tries=3 -O "$tmp_bin" "$installer_url"
+fi
 
-install "$@"
+echo "Verifying installer integrity..."
+sha256sum -c <<<"$installer_sha256  $tmp_bin"
+
+echo "Starting the Datadog installer..."
+"${sudo_cmd[@]}" chmod +x "$tmp_bin"
+"${sudo_env_cmd[@]}" "$tmp_bin" "$@"
+
+"${sudo_cmd[@]}" rm -f "$tmp_bin"
+
 exit 0
