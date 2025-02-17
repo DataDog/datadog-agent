46a9c050637923c9950423251bb9626b00bb89db
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-02-17 18:54:05 +0100

    Fix tests

--- pkg/collector/corechecks/gpu/model/model.go
+++ pkg/collector/corechecks/gpu/model/model.go
@@ -9,10 +9,16 @@ package model
 
 // MemoryMetrics contains the memory stats for a given memory type
 type MemoryMetrics struct {
-	CurrentBytes           uint64  `json:"current_bytes"`
-	MaxBytes               uint64  `json:"max_bytes"`
+	// CurrentBytes is the amount of memory that is allocated when the stats are generated.
+	CurrentBytes uint64 `json:"current_bytes"`
+
+	// MaxBytes is the maximum amount of memory that has been allocated in the interval.
+	// While it's not reported to the backend, we leave it as it's useful for debugging
+	// and unit testing.
+	MaxBytes uint64 `json:"max_bytes"`
+
+	// CurrentBytesPercentage is the percentage (0-1) of the current memory usage compared to the total memory available.
 	CurrentBytesPercentage float64 `json:"current_bytes_percentage"`
-	MaxBytesPercentage     float64 `json:"max_bytes_percentage"`
 }
 
 // UtilizationMetrics contains the GPU stats for a given device and process
