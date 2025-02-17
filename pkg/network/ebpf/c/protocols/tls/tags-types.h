8cd52c990e4bc15da4e4fac6f26ef28417787a2e
Luca Abbati <luca.abbati@datadoghq.com>
2025-02-17 12:31:41 +0000

    Add debug info as http tags

--- pkg/network/ebpf/c/protocols/tls/tags-types.h
+++ pkg/network/ebpf/c/protocols/tls/tags-types.h
@@ -2,14 +2,17 @@
 #define __TAGS_TYPES_H
 
 // static tags limited to 64 tags per unique connection
-enum static_tags {
+enum static_tags
+{
     NO_TAGS = 0,
-    LIBGNUTLS = (1<<0),
-    LIBSSL = (1<<1),
-    GO = (1<<2),
-    CONN_TLS = (1<<3),
-    ISTIO = (1<<4),
-    NODEJS = (1<<5),
+    LIBGNUTLS = (1 << 0),
+    LIBSSL = (1 << 1),
+    GO = (1 << 2),
+    CONN_TLS = (1 << 3),
+    ISTIO = (1 << 4),
+    NODEJS = (1 << 5),
+    DEBUG_MISBHEAVIOR_NO_SKB_INFO = (1 << 6),
+    DEBUG_MISBHEAVIOR_YES_SKB_INFO = (1 << 7),
 };
 
 #endif
