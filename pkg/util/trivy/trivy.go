c40f7d14835a04d679dd1f6332485857e68f6c4f
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-02-18 20:06:23 +0100

    trivy: skip download of java database (#34149)

--- pkg/util/trivy/trivy.go
+++ pkg/util/trivy/trivy.go
@@ -177,9 +177,6 @@ func DefaultDisabledCollectors(enabledAnalyzers []string) []analyzer.Type {
 		analyzer.TypeRpmArchive,
 	)
 
-	// FIXME: the java analyzer requires some javadb, let's skip it for now
-	disabledAnalyzers = append(disabledAnalyzers, analyzer.TypeJar, analyzer.TypeGradleLock, analyzer.TypePom, analyzer.TypeSbtLock)
-
 	return disabledAnalyzers
 }
 
