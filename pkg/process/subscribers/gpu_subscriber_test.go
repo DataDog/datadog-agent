bcb3b8d9601f5093d4b5b3b453c54afd0aa5a630
Stephanie Wei <72533858+Stephanie0829@users.noreply.github.com>
2025-02-18 04:19:33 +0000

    Use Run and Stop to test GPU detection

--- pkg/process/subscribers/gpu_subscriber_test.go
+++ pkg/process/subscribers/gpu_subscriber_test.go
@@ -8,6 +8,7 @@ package subscribers
 import (
 	"context"
 	"testing"
+	"time"
 
 	"go.uber.org/fx"
 
@@ -20,67 +21,128 @@ import (
 	fxutil "github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
-func TestProcessEvents(t *testing.T) {
+func TestGPUDetection(t *testing.T) {
 	tests := []struct {
 		name                string
-		events              workloadmeta.EventBundle
+		events              []workloadmeta.CollectorEvent
 		expectedGPUDetected bool
 	}{
 		{
 			name:                "no events",
-			events:              workloadmeta.EventBundle{},
+			events:              []workloadmeta.CollectorEvent{},
 			expectedGPUDetected: false,
 		},
 		{
 			name: "non GPU event",
-			events: workloadmeta.EventBundle{
-				Events: []workloadmeta.Event{
-					{Entity: &workloadmeta.Container{}},
+			events: []workloadmeta.CollectorEvent{
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.Container{
+						EntityID: workloadmeta.EntityID{
+							ID:   "123",
+							Kind: workloadmeta.KindContainer,
+						},
+					},
 				},
 			},
 			expectedGPUDetected: false,
 		},
 		{
 			name: "one GPU event",
-			events: workloadmeta.EventBundle{
-				Events: []workloadmeta.Event{
-					{Entity: &workloadmeta.GPU{}},
+			events: []workloadmeta.CollectorEvent{
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.GPU{
+						EntityID: workloadmeta.EntityID{
+							ID:   "1",
+							Kind: workloadmeta.KindGPU,
+						},
+					},
 				},
 			},
 			expectedGPUDetected: true,
 		},
 		{
 			name: "multiple GPU events",
-			events: workloadmeta.EventBundle{
-				Events: []workloadmeta.Event{
-					{Entity: &workloadmeta.GPU{}},
-					{Entity: &workloadmeta.GPU{}},
+			events: []workloadmeta.CollectorEvent{
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.GPU{
+						EntityID: workloadmeta.EntityID{
+							ID:   "1",
+							Kind: workloadmeta.KindGPU,
+						},
+					},
+				},
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.GPU{
+						EntityID: workloadmeta.EntityID{
+							ID:   "2",
+							Kind: workloadmeta.KindGPU,
+						},
+					},
 				},
 			},
 			expectedGPUDetected: true,
 		},
 		{
 			name: "False detection bool overwritten",
-			events: workloadmeta.EventBundle{
-				Events: []workloadmeta.Event{
-					{Entity: &workloadmeta.Container{}},
-					{Entity: &workloadmeta.GPU{}},
+			events: []workloadmeta.CollectorEvent{
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.Container{
+						EntityID: workloadmeta.EntityID{
+							ID:   "123",
+							Kind: workloadmeta.KindContainer,
+						},
+					},
+				},
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.GPU{
+						EntityID: workloadmeta.EntityID{
+							ID:   "1",
+							Kind: workloadmeta.KindGPU,
+						},
+					},
 				},
 			},
 			expectedGPUDetected: true,
 		},
 		{
 			name: "True detection bool not overwritten",
-			events: workloadmeta.EventBundle{
-				Events: []workloadmeta.Event{
-					{Entity: &workloadmeta.GPU{}},
-					{Entity: &workloadmeta.Container{}},
+			events: []workloadmeta.CollectorEvent{
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.GPU{
+						EntityID: workloadmeta.EntityID{
+							ID:   "1",
+							Kind: workloadmeta.KindGPU,
+						},
+					},
+				},
+				{
+					Type:   workloadmeta.EventTypeSet,
+					Source: workloadmeta.SourceRuntime,
+					Entity: &workloadmeta.Container{
+						EntityID: workloadmeta.EntityID{
+							ID:   "123",
+							Kind: workloadmeta.KindContainer,
+						},
+					},
 				},
 			},
 			expectedGPUDetected: true,
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mockWmeta := fxutil.Test[workloadmetamock.Mock](t, fx.Options(
@@ -90,9 +152,16 @@ func TestProcessEvents(t *testing.T) {
 			))
 
 			gpuDetector := NewGPUSubscriber(mockWmeta)
-			gpuDetector.processEvents(tt.events)
+			go gpuDetector.Run()
+			defer gpuDetector.Stop()
 
-			assert.Equal(t, tt.expectedGPUDetected, gpuDetector.IsGPUDetected())
+			// Notify subscribers of events
+			mockWmeta.Notify(tt.events)
+
+			// Eventually, GPU detector should finish processing all events
+			assert.Eventually(t, func() bool {
+				return assert.Equal(t, tt.expectedGPUDetected, gpuDetector.IsGPUDetected())
+			}, 1*time.Second, 100*time.Millisecond)
 		})
 	}
 }
