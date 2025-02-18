c430943f3772a52172707cc4154c2ebdf8c92041
David Kirov <david.kirov@datadoghq.com>
2025-02-18 15:25:34 +0100

    Add logging to FIPS tests

--- test/new-e2e/tests/fips-compliance/fixtures/e2e_fips_test.py
+++ test/new-e2e/tests/fips-compliance/fixtures/e2e_fips_test.py
@@ -14,34 +14,37 @@ class FIPSModeCheck(AgentCheck):
         # it doesn't mean that FIPS mode is operating correctly, so we check
         # that the FIPS provider DLL is loaded as well
         if sys.platform == "win32":
-            self.gauge('e2e.fips_dll_loaded', _is_fips_dll_loaded())
-        self.gauge('e2e.fips_cryptography', _is_cryptography_fips())
-        self.gauge('e2e.fips_ssl', _is_ssl_fips())
+            self.gauge('e2e.fips_dll_loaded', _is_fips_dll_loaded(self.log))
+        self.gauge('e2e.fips_cryptography', _is_cryptography_fips(self.log))
+        self.gauge('e2e.fips_ssl', _is_ssl_fips(self.log))
 
 
-def _is_fips_dll_loaded():
+def _is_fips_dll_loaded(logger):
     # the module is loaded on demand, import _hashlib is enough to load itf
     try:
         handle = win32api.GetModuleHandle("fips.dll")
-    except Exception:
+    except Exception as e:
+        logger.error(e, exc_info=True)
         handle = 0
     return handle != 0
 
 
-def _is_cryptography_fips():
+def _is_cryptography_fips(logger):
     try:
         default_backend()._enable_fips()
         return 1
-    except Exception:
+    except Exception as e:
+        logger.error(e, exc_info=True)
         return 0
 
 
-def _is_ssl_fips():
+def _is_ssl_fips(logger):
     # we expect ssl to throw an exception in FIPS mode whenever we try to set
     # the context cipher list to "MD5"
     ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     try:
         ctx.set_ciphers("MD5")
         return 0
-    except ssl.SSLError:
+    except ssl.SSLError as e:
+        logger.error(e, exc_info=True)
         return 1
