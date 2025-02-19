7eeb009f819efeb72407eb98cb17b4434a15aed7
Jean-Edouard Dupau <jeanedouard.dupau@datadoghq.com>
2025-02-19 15:22:18 +0100

    update rtloader to add  getAttrBool method

--- rtloader/test/rtloader/rtloader_test.go
+++ rtloader/test/rtloader/rtloader_test.go
@@ -258,3 +258,57 @@ with open(r'%s', 'w') as f:
 	// Check for leaks
 	helpers.AssertMemoryUsage(t)
 }
+
+func TestGetAttrBool(t *testing.T) {
+	// Reset memory counters
+	helpers.ResetMemoryStats()
+
+	tests := []struct {
+		name    string
+		code    string
+		success bool
+		value   bool
+	}{
+
+		{
+			name: "Test with a True attribute",
+			code: `import fake_check
+fake_check.foo = True`,
+			success: true,
+			value:   true,
+		}, {
+			name: "Test with a False attribute",
+			code: `import fake_check
+fake_check.foo = False`,
+			success: true,
+			value:   false,
+		}, {
+			name: "Test with a non-bool attribute",
+			code: `import fake_check
+fake_check.foo = "bar"`,
+			success: false,
+			value:   false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, err := runString(tt.code); err != nil {
+				t.Fatalf("`TestGetAttrBool` error resetting was_canceled: %v", err)
+			}
+
+			res, err := getFakeModuleWithBool()
+
+			if tt.success && err != nil {
+				t.Fatal(err)
+			}
+
+			if res != tt.value {
+				t.Fatalf("Expected %v, got %v", tt.value, res)
+			}
+		})
+	}
+
+	// Check for leaks
+	helpers.AssertMemoryUsage(t)
+}
