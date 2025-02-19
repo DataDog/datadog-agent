24177a37a7eb86718fd9a264244df55929da6076
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-02-19 05:19:47 -0800

    Better visualization of events

--- pkg/gpu/testutil/events.go
+++ pkg/gpu/testutil/events.go
@@ -11,7 +11,9 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/DataDog/datadog-agent/pkg/gpu/ebpf"
 )
@@ -45,11 +47,20 @@ func parseCompleteEvent(eventType ebpf.CudaEventType, data []byte) (any, error) 
 	}
 }
 
-// ParseEventsFile reads a file containing JSON-encoded events recorded with the
+// EventsCollection represents a collection of recorded CUDA events from the system-probe
+// module, as returned by the `gpu/debug/collect-events` endpoint. This struct encapsulates methods to
+// parse, manipulate and display the events.
+type EventCollection struct {
+	Events       []any
+	firstKtimeNs uint64
+	lastKtimeNs  uint64
+}
+
+// NewEventCollection reads a file containing JSON-encoded events recorded with the
 // `gpu/debug/collect-events` endpoint from system-probe (in a format of one per
 // line) and returns a slice of events.
-func ParseEventsFile(path string) ([]any, error) {
-	var events []any
+func NewEventCollection(path string) (*EventCollection, error) {
+	coll := &EventCollection{}
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -76,59 +87,134 @@ func ParseEventsFile(path string) ([]any, error) {
 			return nil, fmt.Errorf("cannot parse line %d: %v", lineNumber, err)
 		}
 
-		events = append(events, completeEvent)
-	}
-
-	return events, nil
-}
-
-// GetEventTimestamp returns the timestamp of a gpuebpf event by casting it to the
-// appropriate type and extracting the timestamp field. Not the best way to do this but
-// considering it's just testutil code, it works well enough rather than modifying the ebpf package
-func GetEventTimestamp(ev any) uint64 {
-	switch e := ev.(type) {
-	case *ebpf.CudaKernelLaunch:
-		return e.Header.Ktime_ns
-	case *ebpf.CudaMemEvent:
-		return e.Header.Ktime_ns
-	case *ebpf.CudaSync:
-		return e.Header.Ktime_ns
-	case *ebpf.CudaSetDeviceEvent:
-		return e.Header.Ktime_ns
-	default:
-		return 0
-	}
-}
-
-// EventToString converts an event to a human-readable string based on its type
-func EventToString(ev any, lastTimestamp uint64) (string, error) {
-	switch e := ev.(type) {
-	case *ebpf.CudaKernelLaunch:
-		return fmt.Sprintf("[%s] kernel launch addr 0x%X", headerToString(&e.Header, lastTimestamp), e.Kernel_addr), nil
-	case *ebpf.CudaMemEvent:
-		memName := "allocation"
-		if ebpf.CudaMemEventType(e.Type) == ebpf.CudaMemFree {
-			memName = "free"
+		if len(coll.Events) == 0 {
+			coll.firstKtimeNs = event.Header.Ktime_ns
 		}
-		return fmt.Sprintf("[%s] memory %s addr 0x%X size %d", headerToString(&e.Header, lastTimestamp), memName, e.Addr, e.Size), nil
-	case *ebpf.CudaSync:
-		return fmt.Sprintf("[%s] sync event", headerToString(&e.Header, lastTimestamp)), nil
-	case *ebpf.CudaSetDeviceEvent:
-		return fmt.Sprintf("[%s] set device event device %d", headerToString(&e.Header, lastTimestamp), e.Device), nil
-	default:
-		return "", fmt.Errorf("unsupported event type: %T", e)
+		coll.lastKtimeNs = event.Header.Ktime_ns
+
+		coll.Events = append(coll.Events, completeEvent)
 	}
+
+	return coll, nil
 }
 
-func headerToString(header *ebpf.CudaEventHeader, lastTimestamp uint64) string {
+// headerToString converts a CUDA event header to a human-readable string, including relative time
+// from the previous event if it's not zero
+func (c *EventCollection) headerToString(header *ebpf.CudaEventHeader, prevKtimeNs uint64) string {
 	pid := header.Pid_tgid >> 32
 	tid := header.Pid_tgid & 0xffffffff
 
 	var diffStr string
-	if lastTimestamp != 0 {
-		diff := header.Ktime_ns - lastTimestamp
+	if prevKtimeNs != 0 {
+		diff := header.Ktime_ns - prevKtimeNs
 		diffStr = fmt.Sprintf(" (%+6.3fms)", float64(diff)/1e6)
 	}
 
-	return fmt.Sprintf("PID/TID: %d/%d | STR: %d | T %d%s", pid, tid, header.Stream_id, header.Ktime_ns, diffStr)
+	// Output timestamps relative to the start of the first event, it makes it easier to understand
+	tsMsec := float64(header.Ktime_ns-c.firstKtimeNs) / 1e6
+
+	return fmt.Sprintf("PID/TID: %d/%d | STR: %d | T %6.3f%s", pid, tid, header.Stream_id, tsMsec, diffStr)
+}
+
+func (c *EventCollection) OutputEvents(writer io.Writer) error {
+	prevKtimeNs := uint64(0)
+
+	// output events by groups for each PID for the summary later
+	groupers := make(map[uint64]*eventGrouper)
+
+	for i, ev := range c.Events {
+		var header *ebpf.CudaEventHeader
+		var evStr string
+		switch e := ev.(type) {
+		case *ebpf.CudaKernelLaunch:
+			header = &e.Header
+			evStr = fmt.Sprintf("kernel launch addr 0x%X", e.Kernel_addr)
+		case *ebpf.CudaMemEvent:
+			memName := "allocation"
+			if ebpf.CudaMemEventType(e.Type) == ebpf.CudaMemFree {
+				memName = "free"
+			}
+			header = &e.Header
+			evStr = fmt.Sprintf("memory %s addr 0x%X size %d", memName, e.Addr, e.Size)
+		case *ebpf.CudaSync:
+			header = &e.Header
+			evStr = fmt.Sprintf("sync event")
+		case *ebpf.CudaSetDeviceEvent:
+			header = &e.Header
+			evStr = fmt.Sprintf("set device event device %d", e.Device)
+		default:
+			return fmt.Errorf("%d: unsupported event type: %T", i, e)
+		}
+
+		headerStr := c.headerToString(header, prevKtimeNs)
+		prevKtimeNs = header.Ktime_ns
+
+		fmt.Fprintf(writer, "%d: [%s] %s\n", i, headerStr, evStr)
+
+		tsMsec := float64(header.Ktime_ns-c.firstKtimeNs) / 1e6
+
+		pid := header.Pid_tgid >> 32
+		if _, ok := groupers[pid]; !ok {
+			groupers[pid] = &eventGrouper{}
+		}
+		groupers[pid].addEvent(ebpf.CudaEventType(header.Type), tsMsec, i)
+	}
+
+	// flush the last value
+	for _, grouper := range groupers {
+		grouper.flushCurrent(0)
+	}
+
+	fmt.Fprintf(writer, "\n\nGrouped events:\n")
+
+	for pid, grouper := range groupers {
+		fmt.Fprintf(writer, "\n=== PID: %d\n", pid)
+
+		for _, group := range grouper.groups {
+			fmt.Fprintf(writer, "%s\n", group)
+		}
+	}
+
+	return nil
+}
+
+// eventGrouper is just a small helper function to have a running count of subsequent events of the same type
+type eventGrouper struct {
+	groups            []string
+	currentEventType  ebpf.CudaEventType
+	currentEventCount int
+	firstTimestampMs  float64
+	lastTimestampMs   float64
+
+	firstEventId int
+	lastEventId  int
+}
+
+// flushCurrent flushes the current event count to the groups slice
+func (g *eventGrouper) flushCurrent(nextTsMs float64) {
+	if g.currentEventCount > 0 {
+		typeStr := strings.Replace(g.currentEventType.String(), "CudaEventType", "", 1)
+		g.groups = append(g.groups, fmt.Sprintf("%13s x %3d: from %.3f to %.3f (lasts %.3fms) | IDs %d -> %d", typeStr, g.currentEventCount, g.firstTimestampMs, g.lastTimestampMs, g.lastTimestampMs-g.firstTimestampMs, g.firstEventId, g.lastEventId))
+
+		// if the last group was too long ago, print a "INACTIVE" group
+		timeSinceLast := nextTsMs - g.lastTimestampMs
+		if g.lastTimestampMs > 0 && timeSinceLast > 1 {
+			g.groups = append(g.groups, fmt.Sprintf("%19s: from %.3f to %.3f (lasts %.3fms)", "== INACTIVE == ", g.lastTimestampMs, nextTsMs, timeSinceLast))
+		}
+	}
+}
+
+// addEvent adds an event to the current group, or starts a new one if the event type is different
+func (g *eventGrouper) addEvent(eventType ebpf.CudaEventType, tsMsec float64, evNumber int) {
+	if g.currentEventType != eventType {
+		g.flushCurrent(tsMsec)
+		g.currentEventType = eventType
+		g.currentEventCount = 0
+		g.firstTimestampMs = tsMsec
+		g.firstEventId = evNumber
+	}
+
+	g.currentEventCount++
+	g.lastTimestampMs = tsMsec
+	g.lastEventId = evNumber
 }
