5727606ae2b9115dbaa76f2bc67f9dfcecc7ce74
Sylvain Baubeau <sylvain.baubeau@datadoghq.com>
2025-02-20 14:17:33 +0100

    Merge #34230: [CWS] Improve variables output in security-agent status

--- pkg/security/module/server.go
+++ pkg/security/module/server.go
@@ -33,7 +33,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/security/probe/selftests"
 	"github.com/DataDog/datadog-agent/pkg/security/proto/api"
 	"github.com/DataDog/datadog-agent/pkg/security/rules/monitor"
-	"github.com/DataDog/datadog-agent/pkg/security/secl/compiler/eval"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/rules"
 	"github.com/DataDog/datadog-agent/pkg/security/seclog"
@@ -546,43 +545,9 @@ func (a *APIServer) ApplyPolicyStates(policies []*monitor.PolicyState) {
 	}
 }
 
-// GetSECLVariables sets the secl variables for the apiClient
-func (a *APIServer) GetSECLVariables() []*api.SECLVariableState {
-	ruleset := a.cwsConsumer.ruleEngine.GetRuleSet()
-	if ruleset == nil {
-		return nil
-	}
-	var seclVariables = []*api.SECLVariableState{}
-	for name, value := range ruleset.GetVariables() {
-		if strings.HasPrefix(name, "process.") {
-			a.probe.Walk(func(entry *model.ProcessCacheEntry) {
-				entry.Retain()
-				defer entry.Release()
-				event := a.probe.PlatformProbe.NewEvent()
-				event.ProcessCacheEntry = entry
-				ctx := eval.NewContext(event)
-				scopedName := strings.Replace(name, "process.", fmt.Sprintf("process_%d.", entry.Pid), 1)
-				v := value.(eval.ScopedVariable).GetValue(ctx)
-				seclVariables = append(seclVariables,
-					&api.SECLVariableState{
-						Name:  scopedName,
-						Value: fmt.Sprintf("%v", v),
-					})
-			})
-		} else if strings.HasPrefix(name, "container.") {
-			continue // skip container variables for now
-		} else { // global variables
-			seclVariables = append(seclVariables,
-				&api.SECLVariableState{
-					Name:  name,
-					Value: fmt.Sprintf("%v", value.(eval.Variable).GetValue()),
-				})
-
-		}
-
-	}
-
-	return seclVariables
+// GetSECLVariables returns the SECL variables and their value
+func (a *APIServer) GetSECLVariables() map[string]*api.SECLVariableState {
+	return a.cwsConsumer.ruleEngine.GetSECLVariables()
 }
 
 // Stop stops the API server
