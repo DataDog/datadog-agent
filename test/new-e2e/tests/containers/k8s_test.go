27facce6322543be6cefd30002e637868b928e47
Justin Lesko <justin.lesko@datadoghq.com>
2025-02-24 16:17:00 -0500

    Add checks for static tags from env and instance config

--- test/new-e2e/tests/containers/k8s_test.go
+++ test/new-e2e/tests/containers/k8s_test.go
@@ -504,6 +504,8 @@ func (suite *k8sSuite) TestNginx() {
 				`^team:contp$`,
 				`^mail:team-container-platform@datadoghq.com$`,
 				`^sub-team:contint$`,
+				`^example-instance-tag$`,
+				`^example-env-tag$`,
 			},
 			Value: &testMetricExpectValueArgs{
 				Max: 5,
@@ -600,6 +602,8 @@ func (suite *k8sSuite) TestRedis() {
 				`^kube_cluster_name:`,
 				`^kube_deployment:redis$`,
 				`^kube_namespace:workload-redis$`,
+				`^example-instance-tag$`,
+				`^example-env-tag$`,
 			},
 			Value: &testMetricExpectValueArgs{
 				Max: 5,
@@ -810,6 +814,8 @@ func (suite *k8sSuite) TestKSM() {
 				`^org:agent-org$`,
 				`^team:contp$`,
 				`^mail:team-container-platform@datadoghq.com$`,
+				`^example-instance-tag$`,
+				`^example-env-tag$`,
 			},
 			Value: &testMetricExpectValueArgs{
 				Max: 1,
@@ -830,6 +836,8 @@ func (suite *k8sSuite) TestKSM() {
 			Tags: &[]string{
 				`^kube_cluster_name:` + regexp.QuoteMeta(suite.clusterName) + `$`,
 				`^kube_namespace:workload-redis$`,
+				`^example-instance-tag$`,
+				`^example-env-tag$`,
 			},
 			Value: &testMetricExpectValueArgs{
 				Max: 1,
@@ -851,6 +859,8 @@ func (suite *k8sSuite) TestKSM() {
 				`^cr_type:ddm$`,
 				`^ddm_namespace:workload-(?:nginx|redis)$`,
 				`^ddm_name:(?:nginx|redis)$`,
+				`^example-instance-tag$`,
+				`^example-env-tag$`,
 			},
 		},
 	})
