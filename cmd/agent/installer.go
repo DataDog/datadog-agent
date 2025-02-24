e162857f4ae6127b097ec93ffe7b68dbcd9a8853
Arthur Bellal <arthur.bellal@datadoghq.com>
2025-02-24 17:45:51 +0100

    remove macos

--- cmd/agent/installer.go
+++ cmd/agent/installer.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build bundle_installer && !windows
+//go:build bundle_installer && !windows && !darwin
 
 package main
 
