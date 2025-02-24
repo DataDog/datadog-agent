DataDog/datadog-agent

commit 7465d64353f8b6f23eeb23c068e48d3a1d6f446a

Merge #34235: (fleet) make the catalog override sticky

pkg/fleet/daemon/daemon.go
@@ -79,14 +79,15 @@ type daemonImpl struct {
 	m        sync.Mutex
 	stopChan chan struct{}
 
-	env           *env.Env
-	installer     func(env *env.Env) installer.Installer
-	rc            *remoteConfig
-	catalog       catalog
-	configs       map[string]installerConfig
-	requests      chan remoteAPIRequest
-	requestsWG    sync.WaitGroup
-	requestsState map[string]requestState
+	env             *env.Env
+	installer       func(env *env.Env) installer.Installer
+	rc              *remoteConfig
+	catalog         catalog
+	catalogOverride catalog
+	configs         map[string]installerConfig
+	requests        chan remoteAPIRequest
+	requestsWG      sync.WaitGroup
+	requestsState   map[string]requestState
 }
 
 func newInstaller(installerBin string) func(env *env.Env) installer.Installer {
@@ -132,14 +133,15 @@ func NewDaemon(hostname string, rcFetcher client.ConfigFetcher, config config.Re
 
 func newDaemon(rc *remoteConfig, installer func(env *env.Env) installer.Installer, env *env.Env) *daemonImpl {
 	i := &daemonImpl{
-		env:           env,
-		rc:            rc,
-		installer:     installer,
-		requests:      make(chan remoteAPIRequest, 32),
-		catalog:       catalog{},
-		configs:       make(map[string]installerConfig),
-		stopChan:      make(chan struct{}),
-		requestsState: make(map[string]requestState),
+		env:             env,
+		rc:              rc,
+		installer:       installer,
+		requests:        make(chan remoteAPIRequest, 32),
+		catalog:         catalog{},
+		catalogOverride: catalog{},
+		configs:         make(map[string]installerConfig),
+		stopChan:        make(chan struct{}),
+		requestsState:   make(map[string]requestState),
 	}
 	i.refreshState(context.Background())
 	return i
@@ -226,7 +228,11 @@ func (d *daemonImpl) GetPackage(pkg string, version string) (Package, error) {
 	d.m.Lock()
 	defer d.m.Unlock()
 
-	catalogPackage, ok := d.catalog.getPackage(pkg, version, runtime.GOARCH, runtime.GOOS)
+	catalog := d.catalog
+	if len(d.catalogOverride.Packages) > 0 {
+		catalog = d.catalogOverride
+	}
+	catalogPackage, ok := catalog.getPackage(pkg, version, runtime.GOARCH, runtime.GOOS)
 	if !ok {
 		return Package{}, fmt.Errorf("could not get package %s, %s for %s, %s", pkg, version, runtime.GOARCH, runtime.GOOS)
 	}
@@ -237,7 +243,7 @@ func (d *daemonImpl) GetPackage(pkg string, version string) (Package, error) {
 func (d *daemonImpl) SetCatalog(c catalog) {
 	d.m.Lock()
 	defer d.m.Unlock()
-	d.catalog = c
+	d.catalogOverride = c
 }
 
 // Start starts remote config and the garbage collector.
@@ -554,11 +560,11 @@ func (d *daemonImpl) handleRemoteAPIRequest(request remoteAPIRequest) (err error
 			newEnv.InstallScript.APMInstrumentationEnabled = params.ApmInstrumentation
 		}
 
-		pkg, ok := d.catalog.getPackage(request.Package, params.Version, runtime.GOARCH, runtime.GOOS)
-		if !ok {
+		pkg, err := d.GetPackage(request.Package, params.Version)
+		if err != nil {
 			return installerErrors.Wrap(
 				installerErrors.ErrPackageNotFound,
-				fmt.Errorf("could not get package %s, %s for %s, %s", request.Package, params.Version, runtime.GOARCH, runtime.GOOS),
+				err,
 			)
 		}
 		return d.install(ctx, &newEnv, pkg.URL, nil)

commit 878bb9fa9b6ce9be2268ee8785c182fa75f89331

(fleet) fix deadlock in daemon when getting package on RC request
This PR fixes a deadlock introduced in https://github.com/DataDog/datadog-agent/pull/34235.

pkg/fleet/daemon/daemon.go
@@ -228,6 +228,10 @@ func (d *daemonImpl) GetPackage(pkg string, version string) (Package, error) {
 	d.m.Lock()
 	defer d.m.Unlock()
 
+	return d.getPackage(pkg, version)
+}
+
+func (d *daemonImpl) getPackage(pkg string, version string) (Package, error) {
 	catalog := d.catalog
 	if len(d.catalogOverride.Packages) > 0 {
 		catalog = d.catalogOverride
@@ -556,7 +560,7 @@ func (d *daemonImpl) handleRemoteAPIRequest(request remoteAPIRequest) (err error
 			newEnv.InstallScript.APMInstrumentationEnabled = params.ApmInstrumentation
 		}
 
-		pkg, err := d.GetPackage(request.Package, params.Version)
+		pkg, err := d.getPackage(request.Package, params.Version)
 		if err != nil {
 			return installerErrors.Wrap(
 				installerErrors.ErrPackageNotFound,