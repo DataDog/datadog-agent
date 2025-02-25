8a3a4f7a8dcdfec94e4894c55ec966e225cf09f2
grantseltzer <grantseltzer@gmail.com>
2025-02-25 09:24:19 -0600

    Start of unit tests
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/codegen/capture_depth.go
+++ pkg/dynamicinstrumentation/codegen/capture_depth.go
@@ -9,6 +9,9 @@
 package codegen
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/DataDog/datadog-agent/pkg/dynamicinstrumentation/ditypes"
 )
 
@@ -18,8 +21,12 @@ type captureDepthItem struct {
 }
 
 func applyCaptureDepth(params []*ditypes.Parameter, targetDepth int) {
+	// fmt.Println("Entry:", pretty.Sprint(params))
 	queue := []*captureDepthItem{}
 	for i := range params {
+		if params[i] == nil {
+			continue
+		}
 		queue = append(queue, &captureDepthItem{
 			depth:     0,
 			parameter: params[i],
@@ -30,12 +37,37 @@ func applyCaptureDepth(params []*ditypes.Parameter, targetDepth int) {
 		queue = queue[1:]
 
 		if top.depth >= targetDepth {
+			fmt.Println("not capturing: ", top.parameter.Name)
 			top.parameter.DoNotCapture = true
 			top.parameter.NotCaptureReason = ditypes.CaptureDepthReached
+		}
+
+		if top.parameter.Kind == uint(reflect.Struct) {
+			for _, child := range top.parameter.ParameterPieces {
+				queue = append(queue, &captureDepthItem{depth: top.depth, parameter: child})
+			}
+			continue
+		}
+
+		if top.parameter.Kind == uint(reflect.Slice) {
+			// If we do want to capture the slice, it means that we at least capture the first
+			// layer of the slice elements. Only this layer counts towards capture depth, but
+			// if it's set to DoNotCapture, then this slice layer header should be set to it as well
+			if len(top.parameter.ParameterPieces) == 0 || top.parameter.ParameterPieces[0] == nil ||
+				len(top.parameter.ParameterPieces[0].ParameterPieces) == 0 ||
+				top.parameter.ParameterPieces[0].ParameterPieces[0] == nil {
+				fmt.Println("CONTINUING!!!!!!!!!")
+				continue
+			}
+			elementType := top.parameter.ParameterPieces[0].ParameterPieces[0]
+			queue = append(queue, &captureDepthItem{depth: top.depth, parameter: elementType})
+			continue
 		}
 
 		for _, child := range top.parameter.ParameterPieces {
 			queue = append(queue, &captureDepthItem{depth: top.depth + 1, parameter: child})
 		}
 	}
+
+	// fmt.Println("Exit:", pretty.Sprint(params))
 }
