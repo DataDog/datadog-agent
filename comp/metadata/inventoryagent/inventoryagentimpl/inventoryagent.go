4c24251d2b515e34da967855203adaac26270a68
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2025-02-25 15:16:59 +0100

    Add eks cluster name in inventory

--- comp/metadata/inventoryagent/inventoryagentimpl/inventoryagent.go
+++ comp/metadata/inventoryagent/inventoryagentimpl/inventoryagent.go
@@ -232,7 +232,6 @@ func (ia *inventoryagent) fetchCoreAgentMetadata() {
 	ia.data["config_process_dd_url"] = scrub(ia.conf.GetString("process_config.process_dd_url"))
 	ia.data["config_proxy_http"] = scrub(ia.conf.GetString("proxy.http"))
 	ia.data["config_proxy_https"] = scrub(ia.conf.GetString("proxy.https"))
-	ia.data["config_eks_fargate"] = ia.conf.GetBool("eks_fargate")
 	ia.data["feature_fips_enabled"] = ia.conf.GetBool("fips.enabled")
 	ia.data["feature_logs_enabled"] = ia.conf.GetBool("logs_enabled")
 	ia.data["feature_imdsv2_enabled"] = ia.conf.GetBool("ec2_prefer_imdsv2")
@@ -246,6 +245,13 @@ func (ia *inventoryagent) fetchCoreAgentMetadata() {
 
 	// ECS Fargate
 	ia.fetchECSFargateAgentMetadata()
+
+	// EKS Fargate
+	eks_fargate := ia.conf.GetBool("eks_fargate")
+	ia.data["config_eks_fargate"] = eks_fargate
+	if eks_fargate {
+		ia.data["eks_fargate_cluster_name"] = ia.conf.GetString("cluster_name")
+	}
 }
 
 func (ia *inventoryagent) fetchSecurityAgentMetadata() {
