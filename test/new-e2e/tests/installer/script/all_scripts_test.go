1ac53a895bdc3a4b442b7a00708ab89d039c5957
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-02-27 14:02:43 +0100

    [incident-35594] Delay = 1m

--- test/new-e2e/tests/installer/script/all_scripts_test.go
+++ test/new-e2e/tests/installer/script/all_scripts_test.go
@@ -25,7 +25,7 @@ import (
 	"github.com/DataDog/datadog-agent/test/new-e2e/tests/installer/host"
 )
 
-const stackCreationDelay = 5 * time.Second
+const stackCreationDelay = 1 * time.Minute
 
 type installerScriptTests func(os e2eos.Descriptor, arch e2eos.Architecture) installerScriptSuite
 
