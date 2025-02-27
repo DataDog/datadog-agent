54a236e2d35e36583e8b1aa8d38e983be9d7ce21
Pythyu <antoine.zellmeyer@datadoghq.com>
2025-02-27 15:14:50 +0100

    feat(display_message): separate reduction from increase

--- tasks/package.py
+++ tasks/package.py
@@ -46,7 +46,8 @@ def check_size(ctx, filename: str = 'package_sizes.json', dry_run: bool = False)
         upload_package_sizes(ctx, package_sizes, filename, distant=not dry_run)
     else:
         size_table.sort(key=lambda x: (-x.diff, x.flavor, x.arch_name()))
-        size_message = "".join(f"{pkg_size.markdown()}\n" for pkg_size in size_table)
+        size_message = "".join(f"{pkg_size.markdown()}\n" for pkg_size in size_table if pkg_size.diff >= 0)
+        reduction_size_message = "".join(f"{pkg_size.markdown()}\n" for pkg_size in size_table if pkg_size.diff < 0)
         if "❌" in size_message:
             decision = "❌ Failed"
         elif "⚠️" in size_message:
@@ -56,7 +57,7 @@ def check_size(ctx, filename: str = 'package_sizes.json', dry_run: bool = False)
         # Try to display the message on the PR when a PR exists
         if os.environ.get("CI_COMMIT_BRANCH"):
             try:
-                display_message(ctx, ancestor, size_message, decision)
+                display_message(ctx, ancestor, size_message, reduction_size_message, decision)
             # PR commenter asserts on the numbers of PR's, this will raise if there's no PR
             except AssertionError as exc:
                 print(f"Got `{exc}` while trying to comment on PR, we'll assume that this is not a PR.")
