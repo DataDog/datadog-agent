74f3136eda0adf9d2b5e489a33fe73e53f916954
Bryce Kahle <bryce.kahle@datadoghq.com>
2025-02-28 17:56:24 -0800

    add poc for noisy neighbor
    generate noisy neighbor metrics
    
    add pid
    
    Increase rate limiting for runq events and only keep latest event per pid
    
    fix linter errors
    
    fix linter errors
    
    fix linter errors
    
    fix linter errors
    
    group by cgroup id and update tagging
    
    fix python linter
    
    fix tagger being nil
    
    Add tagger to factory call
    
    fix stub signature
    
    fix prevContainerID
    
    lower interval of NN check
    
    increase task entries
    
    Add better tagging for switch metric
    
    use right prevContainerID
    
    put runqevents back in pool
    
    protect map
    
    add map update telemetry
    
    add ebpf check name mappings
    
    add tp_sched_wakeup_new
    
    use task storage and pid filtering
    
    add bpf_ringbuf_reserve telemetry
    
    increase ringbuff size
    
    increase ringbuffer size further

---
+++ pkg/collector/corechecks/ebpf/probe/noisyneighbor/model/types.go
@@ -0,0 +1,19 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+// Package model contains the model for the noisy neighbor check
+package model
+
+// NoisyNeighborStats contains the statistics from the noisy neighbor check
+type NoisyNeighborStats struct {
+	PrevCgroupID   uint64
+	CgroupID       uint64
+	RunqLatencyNs  uint64
+	TimestampNs    uint64
+	PrevCgroupName string
+	CgroupName     string
+	Pid            uint64
+	PrevPid        uint64
+}
