20929b4c866013ec044fd88d0225b85f0fb99796
David Ortiz <david.ortiz@datadoghq.com>
2025-03-03 14:54:43 +0100

    WIP

--- comp/core/tagger/taglist/taglist.go
+++ comp/core/tagger/taglist/taglist.go
@@ -9,6 +9,7 @@ package taglist
 import (
 	"fmt"
 	"strings"
+	"unique"
 
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
 )
@@ -40,12 +41,14 @@ func addTags(target map[string]bool, name string, value string, splits map[strin
 	}
 	sep, ok := splits[name]
 	if !ok {
-		target[fmt.Sprintf("%s:%s", name, value)] = true
+		key := unique.Make(fmt.Sprintf("%s:%s", name, value))
+		target[key.Value()] = true
 		return
 	}
 
 	for _, elt := range strings.Split(value, sep) {
-		target[fmt.Sprintf("%s:%s", name, elt)] = true
+		key := unique.Make(fmt.Sprintf("%s:%s", name, elt))
+		target[key.Value()] = true
 	}
 }
 
