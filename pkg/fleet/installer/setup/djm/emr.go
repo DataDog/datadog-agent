45700eba5a80a0c77f23413df3b21e0f4fe82c5a
aboitreaud <72934368+aboitreaud@users.noreply.github.com>
2025-03-04 13:35:42 +0100

    fmt

--- pkg/fleet/installer/setup/djm/emr.go
+++ pkg/fleet/installer/setup/djm/emr.go
@@ -85,7 +85,7 @@ func SetupEmr(s *common.Setup) error {
 	s.Config.DatadogYAML.Hostname = hostname
 	s.Config.DatadogYAML.DJM.Enabled = true
 	s.Config.InjectTracerYAML.AdditionalEnvironmentVariables = tracerEnvConfigEmr
-	
+
 	if os.Getenv("DD_DATA_STREAMS_ENABLED") == "true" {
 		s.Out.WriteString("Propagating variable DD_DATA_STREAMS_ENABLED=true to tracer configuration\n")
 		DSMEnabled := common.InjectTracerConfigEnvVar{
