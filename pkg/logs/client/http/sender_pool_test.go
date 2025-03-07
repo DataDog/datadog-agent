e7062e2ddb00f0ad6b6bd7b986e61345d9a44a81
Ryan Hall <ryan.hall@datadoghq.com>
2025-03-07 15:23:51 -0500

    WIP

--- pkg/logs/client/http/sender_pool_test.go
+++ pkg/logs/client/http/sender_pool_test.go
@@ -9,56 +9,75 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/DataDog/datadog-agent/pkg/logs/client"
 )
 
-func TestLatencyThrottledSenderLowLatencyOneWorker(t *testing.T) {
-	pool := newSenderPool(0, 0.064, 1, 10, 10*time.Millisecond, client.NewNoopDestinationMetadata())
+var defaultMinWorkers = 4
+var defaultMaxWorkers = defaultMinWorkers * 10
+
+func defaultPool() *senderPool {
+	return newSenderPool(0, ewmaAlpha, defaultMinWorkers, defaultMaxWorkers, targetLatency, client.NewNoopDestinationMetadata())
+}
+
+/*func newPool(min int, max int) *senderPool {
+	return newSenderPool(0, ewmaAlpha, min, max, targetLatency, client.NewNoopDestinationMetadata())
+}
+func TestDestinationError(t *testing.T) {
+	// Get 429, see worker rollback
+	// Get 500, perform no rollback
+}*/
+
+func TestWorkerCounts(t *testing.T) {
+	scenarios := []struct {
+		name                string
+		latency             time.Duration
+		expectedWorkerCount int
+	}{
+		{
+			name:                "Mininum Workers chosen if latency below target",
+			latency:             0,
+			expectedWorkerCount: defaultMinWorkers,
+		},
+		{
+			name:                "Reasonable number of workers added at higher than target latency",
+			latency:             targetLatency * 2,
+			expectedWorkerCount: defaultMinWorkers * 2,
+		},
+		{
+			name:                "Maximum number of workers not exceeded",
+			latency:             targetLatency * 20,
+			expectedWorkerCount: defaultMaxWorkers,
+		},
+	}
+
+	for _, s := range scenarios {
+		t.Run(s.name, func(t *testing.T) {
+			pool := defaultPool()
+
+			for i := 0; i < 100; i++ {
+				pool.Run(func() destinationResult {
+					return destinationResult{latency: s.latency}
+				})
+			}
+
+			assert.Equal(t, s.expectedWorkerCount, pool.inUseWorkers)
+		})
+	}
+}
+
+/*func TestVirtualLatencyCalculations(t *testing.T) {
+	// Condense to 1 number
+	// Condense to 2 number
+	pool := defaultPool()
 
 	for i := 0; i < 100; i++ {
 		pool.Run(func() destinationResult {
-			return destinationResult{latency: 10 * time.Millisecond}
+			return destinationResult{latency: targetLatency}
 		})
 	}
 
 	// Should converge on a virtual latency of 10ms since there is only 1 worker, so virtual latency == real latency.
 	require.InDelta(t, 10*time.Millisecond, pool.virtualLatency, float64(3*time.Millisecond))
-}
-
-func TestLatencyThrottledSenderPoolLowLatencyManyWorkers(t *testing.T) {
-	pool := newSenderPool(0, 0.055, 1, 10, 100*time.Millisecond, client.NewNoopDestinationMetadata())
-
-	for i := 0; i < 100; i++ {
-		pool.Run(func() destinationResult {
-			return destinationResult{latency: 10 * time.Millisecond}
-		})
-	}
-	// Should converge on a virtual latency of 10ms since since target latency is 100ms and real latency is 10ms. No need to scale up.
-	require.InDelta(t, 10*time.Millisecond, pool.virtualLatency, float64(3*time.Millisecond))
-}
-
-func TestLatencyThrottledSenderPoolScalesUpWorkersForHighLatency(t *testing.T) {
-	pool := newSenderPool(0, 0.055, 1, 10, 2*time.Millisecond, client.NewNoopDestinationMetadata())
-
-	for i := 0; i < 1000; i++ {
-		pool.Run(func() destinationResult {
-			return destinationResult{latency: 2 * time.Millisecond}
-		})
-	}
-	// Should converge on virtual latency of 2ms since 2ms is the target, and there are enough workers to scale up.
-	require.InDelta(t, 2*time.Millisecond, pool.virtualLatency, float64(500*time.Microsecond))
-}
-
-func TestLatencyThrottledSenderPoolStarvedForWorkers(t *testing.T) {
-	pool := newSenderPool(0, 0.055, 1, 2, 2*time.Millisecond, client.NewNoopDestinationMetadata())
-
-	for i := 0; i < 100; i++ {
-		pool.Run(func() destinationResult {
-			return destinationResult{latency: 10 * time.Millisecond}
-		})
-	}
-	// Should converge on virtual latency of 10ms because there are not enough workers to scale up
-	require.InDelta(t, 10*time.Millisecond, pool.virtualLatency, float64(3*time.Millisecond))
-}
+}*/
