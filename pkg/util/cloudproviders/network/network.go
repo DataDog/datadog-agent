eb6615bcfc2219491fd166c2ef424d6ac20b2455
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-03-07 12:34:59 +0100

    move network related files from `pkg/util/cloudproviders` to own package

--- pkg/util/cloudproviders/network/network.go
+++ pkg/util/cloudproviders/network/network.go
@@ -3,7 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package cloudproviders
+// Package network provides utilities around cloud provider networking.
+package network
 
 import (
 	"context"
