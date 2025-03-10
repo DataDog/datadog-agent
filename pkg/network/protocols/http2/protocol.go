f72f96b65498f2fd57d8d56540d97fc2c9ca5179
Daniel Lavie <daniel.lavie@datadoghq.com>
2025-03-10 09:06:22 +0200

    Revert USMON-1324 (#34517)
    Co-authored-by: Amit Slavin <108348428+amitslavin@users.noreply.github.com>

--- pkg/network/protocols/http2/protocol.go
+++ pkg/network/protocols/http2/protocol.go
@@ -420,21 +420,15 @@ func (p *Protocol) setupHTTP2InFlightMapCleaner(mgr *manager.Manager) {
 	p.http2InFlightMapCleaner = mapCleaner
 }
 
-// GetStats returns a map of HTTP2 stats and a callback to clean resources.
-// The format of HTTP2 stats:
+// GetStats returns a map of HTTP2 stats stored in the following format:
 // [source, dest tuple, request path] -> RequestStats object
-func (p *Protocol) GetStats() (*protocols.ProtocolStats, func()) {
+func (p *Protocol) GetStats() *protocols.ProtocolStats {
 	p.eventsConsumer.Sync()
 	p.telemetry.Log()
-	stats := p.statkeeper.GetAndResetAllStats()
 	return &protocols.ProtocolStats{
-			Type:  protocols.HTTP2,
-			Stats: stats,
-		}, func() {
-			for _, elem := range stats {
-				elem.Close()
-			}
-		}
+		Type:  protocols.HTTP2,
+		Stats: p.statkeeper.GetAndResetAllStats(),
+	}
 }
 
 // IsBuildModeSupported returns always true, as http2 module is supported by all modes.
