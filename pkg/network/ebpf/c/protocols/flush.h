ff6b1d4de625c570de53af06e9bde86f03a86869
Guy Arbitman <guy20495@gmail.com>
2025-03-13 11:53:51 +0200

    usm: Added documentation

--- pkg/network/ebpf/c/protocols/flush.h
+++ pkg/network/ebpf/c/protocols/flush.h
@@ -9,6 +9,22 @@
 #include "protocols/postgres/decoding.h"
 #include "protocols/redis/decoding.h"
 
+/**
+Note - We used to have a single tracepoint to flush all the protocols, but we had to split it
+to enable telemetry for all protocols.
+
+However, kernel 4.14 does not support multiple programs to hook the same tracepoint, hence
+we move into kprobes to workaround that.
+
+The kprobe we use is '__netif_receive_skb_core', which is hook-able in several kernels
+including 4.14, but it is not supported for kprobe hooking in kernels 6+.
+
+To simplify the scenario, we have a support for 4.14 based on kprobes, and 4.15+ will be using
+the tracepoints.
+
+http2 is supported only from kernel 5.2, therefore it does not have the kprobe version
+*/
+
 SEC("tracepoint/net/netif_receive_skb")
 int tracepoint__net__netif_receive_skb_http(void *ctx) {
     http_batch_flush_with_telemetry(ctx);
