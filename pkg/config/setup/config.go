890c7af2edd517e5a9aca7ca042b16c4548bd985
rahulkaukuntla <144174402+rahulkaukuntla@users.noreply.github.com>
2025-03-13 12:47:17 -0400

    [AGENTCFG-13] Adding a scatter mechanism in the secrets component (#34744)
    Co-authored-by: maxime mouial <hush-hush@users.noreply.github.com>

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -360,6 +360,7 @@ func InitConfig(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("secret_backend_skip_checks", false)
 	config.BindEnvAndSetDefault("secret_backend_remove_trailing_line_break", false)
 	config.BindEnvAndSetDefault("secret_refresh_interval", 0)
+	config.BindEnvAndSetDefault("secret_refresh_scatter", true)
 	config.SetDefault("secret_audit_file_max_size", 0)
 
 	// IPC API server timeout
@@ -2277,15 +2278,16 @@ func ResolveSecrets(config pkgconfigmodel.Config, secretResolver secrets.Compone
 	// We have to init the secrets package before we can use it to decrypt
 	// anything.
 	secretResolver.Configure(secrets.ConfigParams{
-		Command:          config.GetString("secret_backend_command"),
-		Arguments:        config.GetStringSlice("secret_backend_arguments"),
-		Timeout:          config.GetInt("secret_backend_timeout"),
-		MaxSize:          config.GetInt("secret_backend_output_max_size"),
-		RefreshInterval:  config.GetInt("secret_refresh_interval"),
-		GroupExecPerm:    config.GetBool("secret_backend_command_allow_group_exec_perm"),
-		RemoveLinebreak:  config.GetBool("secret_backend_remove_trailing_line_break"),
-		RunPath:          config.GetString("run_path"),
-		AuditFileMaxSize: config.GetInt("secret_audit_file_max_size"),
+		Command:                config.GetString("secret_backend_command"),
+		Arguments:              config.GetStringSlice("secret_backend_arguments"),
+		Timeout:                config.GetInt("secret_backend_timeout"),
+		MaxSize:                config.GetInt("secret_backend_output_max_size"),
+		RefreshInterval:        config.GetInt("secret_refresh_interval"),
+		RefreshIntervalScatter: config.GetBool("secret_refresh_scatter"),
+		GroupExecPerm:          config.GetBool("secret_backend_command_allow_group_exec_perm"),
+		RemoveLinebreak:        config.GetBool("secret_backend_remove_trailing_line_break"),
+		RunPath:                config.GetString("run_path"),
+		AuditFileMaxSize:       config.GetInt("secret_audit_file_max_size"),
 	})
 
 	if config.GetString("secret_backend_command") != "" {
