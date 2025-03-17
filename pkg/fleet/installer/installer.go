2c74827d13cdca9122680d0169310fcc809ba504
Arthur Bellal <arthur.bellal@datadoghq.com>
2025-03-17 11:45:58 +0100

    Update installer.go

--- pkg/fleet/installer/installer.go
+++ pkg/fleet/installer/installer.go
@@ -368,7 +368,7 @@ func (i *installerImpl) InstallConfigExperiment(ctx context.Context, pkg string,
 	i.m.Lock()
 	defer i.m.Unlock()
 
-	tmpDir, err := i.config.MkdirTemp()
+	tmpDir, err := i.configs.MkdirTemp()
 	if err != nil {
 		return installerErrors.Wrap(
 			installerErrors.ErrFilesystemIssue,
