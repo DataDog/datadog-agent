eed6feda5eeb218984c13213f769a0b8e2f24c7b
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-03-20 13:54:54 +0100

    [mark-on-log] Marked multiple

--- test/new-e2e/cc/cc_test.go
+++ test/new-e2e/cc/cc_test.go
@@ -12,6 +12,14 @@ func TestCcMarked(t *testing.T) {
 	panic("im panicing")
 }
 
+func TestCcMarkedMultiple(t *testing.T) {
+	flake.MarkOnLog(t, "hey")
+	flake.MarkOnLog(t, "panic")
+	flake.MarkOnLog(t, "ho")
+
+	panic("im panicing")
+}
+
 func TestCcNotMarked(t *testing.T) {
 	panic("im panicing")
 }
