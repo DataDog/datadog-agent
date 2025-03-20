3efb37d81a190521703cdc97d085a7d3f176c838
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-03-20 13:53:49 +0100

    [mark-on-log] Push files

---
+++ test/new-e2e/cc/cc_test.go
@@ -0,0 +1,21 @@
+package cc
+
+import (
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/util/testutil/flake"
+)
+
+func TestCcMarked(t *testing.T) {
+	flake.MarkOnLog(t, "panic")
+
+	panic("im panicing")
+}
+
+func TestCcNotMarked(t *testing.T) {
+	panic("im panicing")
+}
+
+func TestCcPass(t *testing.T) {
+	println("I'm passing")
+}
