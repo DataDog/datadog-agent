7b753d17144cfe1e025a5d9c9ab375ad2c761051
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-03-24 15:42:05 +0100

    Add NVML singleton

---
+++ pkg/gpu/nvml/lib_test.go
@@ -0,0 +1,76 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+//go:build linux && nvml
+
+package nvml
+
+import (
+	"testing"
+
+	"github.com/NVIDIA/go-nvml/pkg/nvml"
+	"github.com/stretchr/testify/require"
+
+	nvmlmock "github.com/NVIDIA/go-nvml/pkg/nvml/mock"
+)
+
+func TestEnsureinit(t *testing.T) {
+	t.Run("library present", func(t *testing.T) {
+		var cache nvmlCache
+		opts := nvmlCacheInitOpts{
+			nvmlNewFunc: func(_ ...nvml.LibraryOption) nvml.Interface {
+				return &nvmlmock.Interface{
+					InitFunc: func() nvml.Return { return nvml.SUCCESS },
+				}
+			},
+		}
+
+		require.NoError(t, cache.ensureInitWithOpts(opts))
+	})
+	t.Run("library absent", func(t *testing.T) {
+		var cache nvmlCache
+		opts := nvmlCacheInitOpts{
+			nvmlNewFunc: func(_ ...nvml.LibraryOption) nvml.Interface {
+				return &nvmlmock.Interface{
+					InitFunc: func() nvml.Return { return nvml.ERROR_LIBRARY_NOT_FOUND },
+				}
+			},
+		}
+		require.Error(t, cache.ensureInitWithOpts(opts))
+	})
+
+	t.Run("library absent, second call fails too", func(t *testing.T) {
+		var cache nvmlCache
+		opts := nvmlCacheInitOpts{
+			nvmlNewFunc: func(_ ...nvml.LibraryOption) nvml.Interface {
+				return &nvmlmock.Interface{
+					InitFunc: func() nvml.Return { return nvml.ERROR_LIBRARY_NOT_FOUND },
+				}
+			},
+		}
+		require.Error(t, cache.ensureInitWithOpts(opts))
+		require.Error(t, cache.ensureInitWithOpts(opts))
+	})
+
+	t.Run("library absent, second call succeeds", func(t *testing.T) {
+		var cache nvmlCache
+		failOpts := nvmlCacheInitOpts{
+			nvmlNewFunc: func(_ ...nvml.LibraryOption) nvml.Interface {
+				return &nvmlmock.Interface{
+					InitFunc: func() nvml.Return { return nvml.ERROR_LIBRARY_NOT_FOUND },
+				}
+			},
+		}
+		successOpts := nvmlCacheInitOpts{
+			nvmlNewFunc: func(_ ...nvml.LibraryOption) nvml.Interface {
+				return &nvmlmock.Interface{
+					InitFunc: func() nvml.Return { return nvml.SUCCESS },
+				}
+			},
+		}
+		require.Error(t, cache.ensureInitWithOpts(failOpts))
+		require.NoError(t, cache.ensureInitWithOpts(successOpts))
+	})
+}
