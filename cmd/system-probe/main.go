c286af8bd50604d015d655374aa9005cee208310
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-03-24 17:04:44 +0100

    Fix import

--- cmd/system-probe/main.go
+++ cmd/system-probe/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/DataDog/datadog-agent/cmd/internal/runcmd"
 	"github.com/DataDog/datadog-agent/cmd/system-probe/command"
 	"github.com/DataDog/datadog-agent/cmd/system-probe/subcommands"
+	"github.com/DataDog/datadog-agent/pkg/util/flavor"
 )
 
 func main() {
