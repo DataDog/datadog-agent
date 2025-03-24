30c9531b2f3263162ec7b197876d52d457d147f2
Stuart Geipel <stuart.geipel@datadoghq.com>
2025-03-24 13:37:30 -0700

    [NPM-4334] Use no-op cachedConntrack in ebpfless

--- pkg/network/tracer/tracer.go
+++ pkg/network/tracer/tracer.go
@@ -533,7 +533,12 @@ func (t *Tracer) getRuntimeCompilationTelemetry() map[string]network.RuntimeComp
 // getConnections returns all the active connections in the ebpf maps along with the latest timestamp.  It takes
 // a reusable buffer for appending the active connections so that this doesn't continuously allocate
 func (t *Tracer) getConnections(activeBuffer *network.ConnectionBuffer) (latestUint uint64, activeConnections []network.ConnectionStats, err error) {
-	cachedConntrack := newCachedConntrack(t.config.ProcRoot, netlink.NewConntrack, 128)
+	newConntrack := netlink.NewConntrack
+	// if we already established that netlink conntracker is not supported, don't try again
+	if t.conntracker.GetType() == "" {
+		newConntrack = netlink.NewNoOpConntrack
+	}
+	cachedConntrack := newCachedConntrack(t.config.ProcRoot, newConntrack, 128)
 	defer func() { _ = cachedConntrack.Close() }()
 
 	latestTime, err := ddebpf.NowNanoseconds()
