4885442262ab8d0562eb05b61970e38c73505f0c
Guillermo Julián <gjulianm@users.noreply.github.com>
2025-03-24 18:06:22 +0100

    [EBPF] gpu: fix initializing of NVML library for host tags (#35388)

--- pkg/gpu/tags/tags_nix.go
+++ pkg/gpu/tags/tags_nix.go
@@ -9,23 +9,48 @@
 package tags
 
 import (
-	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"fmt"
+	"sync"
+
 	"github.com/NVIDIA/go-nvml/pkg/nvml"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 // used for tests to mock the NVML library
 var nvmlLibrary nvml.Interface
+var mtx sync.Mutex
 
-// GetTags returns gpu_host:true if any NVIDIA GPUs are present, nil otherwise
-func GetTags() []string {
-	if nvmlLibrary == nil {
-		nvmlLibrary = nvml.New()
-		if ret := nvmlLibrary.Init(); ret != nvml.SUCCESS && ret != nvml.ERROR_ALREADY_INITIALIZED {
-			log.Warnf("Failed to get gpu host tags, failed to initialize NVML: %v", nvml.ErrorString(ret))
-			return nil
-		}
+func ensureNvmlLibrary(getLibrary func(opts ...nvml.LibraryOption) nvml.Interface) error {
+	// if the library is already initialized, return directly to avoid unnecessary locking
+	if nvmlLibrary != nil {
+		return nil
 	}
 
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	// already initialized
+	if nvmlLibrary != nil {
+		return nil
+	}
+
+	lib := getLibrary()
+	if ret := lib.Init(); ret != nvml.SUCCESS && ret != nvml.ERROR_ALREADY_INITIALIZED {
+		return fmt.Errorf("Failed to get gpu host tags, failed to initialize NVML: %v", nvml.ErrorString(ret))
+	}
+
+	nvmlLibrary = lib
+	return nil
+}
+
+// getTags is the internal implementation of GetTags, exposed so that we can test with a mock NVML library
+// to ensure we're properly handling library initialization errors
+func getTags(getLibrary func(opts ...nvml.LibraryOption) nvml.Interface) []string {
+	if err := ensureNvmlLibrary(getLibrary); err != nil {
+		log.Warnf("Failed to get gpu host tags, failed to initialize NVML: %v", err)
+		return nil
+	}
 	count, ret := nvmlLibrary.DeviceGetCount()
 	if ret != nvml.SUCCESS {
 		log.Warnf("Failed to get gpu host tags, couldn't assess number of devices: %v", nvml.ErrorString(ret))
@@ -37,4 +62,9 @@ func GetTags() []string {
 	}
 
 	return nil
+}
+
+// GetTags returns gpu_host:true if any NVIDIA GPUs are present, nil otherwise
+func GetTags() []string {
+	return getTags(nvml.New)
 }
