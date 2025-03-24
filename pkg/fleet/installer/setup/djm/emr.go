918399e0e438e2c1c37e43a277cf6216d5e3e355
aboitreaud <72934368+aboitreaud@users.noreply.github.com>
2025-03-24 18:27:07 +0100

    change name

--- pkg/fleet/installer/setup/djm/emr.go
+++ pkg/fleet/installer/setup/djm/emr.go
@@ -223,7 +223,7 @@ func enableEmrLogs(s *common.Setup) {
 			Source:  "hadoop-yarn",
 			Service: "emr-logs",
 			LogProcessingRules: []common.LogProcessingRule{
-				{Type: "multi_line", Name: "dataframe_header_log", Pattern: "`\\|[\\sa-zA-Z-_.\\|]+\\|$`gm"},
+				{Type: "multi_line", Name: "dataframe_show", Pattern: "`\\|[\\sa-zA-Z-_.\\|]+\\|$`gm"},
 			},
 		},
 		{
