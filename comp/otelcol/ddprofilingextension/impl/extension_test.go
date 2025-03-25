8c9d74a7f9ebf37d5284b2fd9389a8a6a149422c
mackjmr <mackenzie.jomard@datadoghq.com>
2025-03-25 16:40:00 -0400

    fix test

--- comp/otelcol/ddprofilingextension/impl/extension_test.go
+++ comp/otelcol/ddprofilingextension/impl/extension_test.go
@@ -232,7 +232,7 @@ func TestOSSExtensionFargate(t *testing.T) {
 	timeout := time.After(15 * time.Second)
 	select {
 	case out := <-got:
-		assert.Equal(t, "agent_version:7.64.0,orchestrator:fargate_ECS,task_arn:arn:aws:ecs:us-east-1:123456789012:cluster/default", out)
+		assert.Equal(t, "agent_version:7.64.0,orchestrator:fargate_ecs,task_arn:arn:aws:ecs:us-east-1:123456789012:cluster/default", out)
 	case <-timeout:
 		t.Fatal("Timed out")
 	}
