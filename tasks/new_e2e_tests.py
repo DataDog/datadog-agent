28a149067039eccbca64a45d57bc8d78e47428f8
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-03-25 09:45:22 +0100

    Update dependencies to use local packages everywhere possible

--- tasks/new_e2e_tests.py
+++ tasks/new_e2e_tests.py
@@ -125,13 +125,14 @@ def run(
     if local_package and running_in_ci():
         files = os.listdir(local_package)
         if len(files) == 0:
-            raise Exit(f"Local package {local_package} should contain files", 1)
-        print("Removing debug package")
-        for file in files:
-            if "-dbg_" in file:
-                os.remove(os.path.join(local_package, file))
-        new_files = os.listdir(local_package)
-        print("Using the following files for tests:", new_files)
+            print("Warning: --local-package is set but no files found in the directory")
+        else:
+            print("Removing debug package")
+            for file in files:
+                if "-dbg_" in file:
+                    os.remove(os.path.join(local_package, file))
+            new_files = os.listdir(local_package)
+            print("Using the following files for tests:", new_files)
 
     if local_package:
         parsed_params["ddagent:localPackage"] = local_package
