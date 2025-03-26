4eb259d7c6a1cbcc13399fee8ed03f8829107f20
kovags <marcio.kovags@datadoghq.com>
2025-03-26 18:29:35 +0100

    sync seclwin

--- pkg/security/seclwin/model/events.go
+++ pkg/security/seclwin/model/events.go
@@ -81,8 +81,6 @@ const (
 	DNSEventType
 	// DNSResponseEventType DNS Response event
 	DNSResponseEventType
-	// DNSResponseEventsNotSentType DNS Response event not sent by the kernel to userspace
-	DNSResponseEventsNotSentType
 	// NetDeviceEventType is sent for events on net devices
 	NetDeviceEventType
 	// VethPairEventType is sent when a new veth pair is created
