5785d3ffbb4bdcf58e23bab2e3b516ed56cb37eb
Adrien Boitreaud <72934368+aboitreaud@users.noreply.github.com>
2025-03-27 16:49:41 +0100

    [DJM] Handle custom host tags (#35497)

--- pkg/fleet/installer/setup/djm/dataproc.go
+++ pkg/fleet/installer/setup/djm/dataproc.go
@@ -90,7 +90,7 @@ func setupCommonDataprocHostTags(s *common.Setup, metadataClient *metadata.Clien
 	if err != nil {
 		return "", "", err
 	}
-	setHostTag(s, "cluster_id", clusterID)
+	setClearHostTag(s, "cluster_id", clusterID)
 	setHostTag(s, "dataproc_cluster_id", clusterID)
 	setHostTag(s, "data_workload_monitoring_trial", "true")
 
@@ -102,8 +102,7 @@ func setupCommonDataprocHostTags(s *common.Setup, metadataClient *metadata.Clien
 	if dataprocRole == "Master" {
 		isMaster = "true"
 	}
-	setHostTag(s, "is_master_node", isMaster)
-	s.Span.SetTag("host."+"is_master_node", isMaster)
+	setClearHostTag(s, "is_master_node", isMaster)
 
 	clusterName, err := metadataClient.InstanceAttributeValueWithContext(ctx, "dataproc-cluster-name")
 	if err != nil {
@@ -111,6 +110,7 @@ func setupCommonDataprocHostTags(s *common.Setup, metadataClient *metadata.Clien
 		return isMaster, clusterID, nil
 	}
 	setHostTag(s, "cluster_name", clusterName)
+	addCustomHostTags(s)
 
 	return isMaster, clusterName, nil
 }
