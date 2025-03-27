efe9e4c93ece544717599b5a330bfa4b0092d906
Guy Arbitman <guy20495@gmail.com>
2025-03-27 11:45:16 +0200

    updated mongo version to 1.17.3 (#35513)

--- pkg/network/protocols/mongo/client.go
+++ pkg/network/protocols/mongo/client.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build test
+
 // Package mongo provides a simple wrapper around 3rd party mongo client.
 package mongo
 
