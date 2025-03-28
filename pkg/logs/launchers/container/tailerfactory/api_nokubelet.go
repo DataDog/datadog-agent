a546236e7f35968042a154879355bd52d072e877
Justin Lesko <justin.lesko@datadoghq.com>
2025-03-28 16:31:18 +0100

    Shift stubbed out files out to container/tailerfactory from container/tailerfactory/tailers

---
+++ pkg/logs/launchers/container/tailerfactory/api_nokubelet.go
@@ -0,0 +1,17 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build !kubelet
+
+package tailerfactory
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/logs/launchers/container/tailerfactory/tailers"
+	"github.com/DataDog/datadog-agent/pkg/logs/sources"
+)
+
+func (tf *factory) makeSocketTailer(source *sources.LogSource) (Tailer, error) {
+	return &tailers.APITailer{}, nil
+}
