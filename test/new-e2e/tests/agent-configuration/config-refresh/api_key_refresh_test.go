55b8f890faaf355180391ed06eb160a573ae7e35
andrewqian2001datadog <andrew.qian@datadoghq.com>
2025-03-28 13:06:54 -0400

    fix status check

--- test/new-e2e/tests/agent-configuration/config-refresh/api_key_refresh_test.go
+++ test/new-e2e/tests/agent-configuration/config-refresh/api_key_refresh_test.go
@@ -154,11 +154,11 @@ additional_endpoints:
 		assert.Contains(t, status.Content, "API key ending with 54321")
 
 		// Check additional endpoints with specific API key counts
-		assert.Contains(t, status.Content, `/intake - API Keys ending with:
-      - 54321
-      - 54321`)
-		assert.Contains(t, status.Content, `/api/v2/series - API Key ending with:
-      - 54321`)
+		assert.Contains(t, status.Content, `intake - API Keys ending with:
+              - 54321
+              - 54321`)
+		assert.Contains(t, status.Content, `api/v2/series - API Key ending with:
+              - 54321`)
 	}, 1*time.Minute, 10*time.Second)
 
 	// Wait for the agent to send data to each endpoint and verify the API keys
