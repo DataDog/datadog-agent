485cbb192bff2ed2e753a3b0a4ea7b4d0e9da991
andrewqian2001datadog <andrew.qian@datadoghq.com>
2025-03-28 15:17:35 -0400

    fix typo

--- test/new-e2e/tests/agent-configuration/config-refresh/api_key_refresh_test.go
+++ test/new-e2e/tests/agent-configuration/config-refresh/api_key_refresh_test.go
@@ -155,7 +155,6 @@ additional_endpoints:
 
 		// Check additional endpoints with specific API key counts
 		assert.Contains(t, status.Content, `intake - API Keys ending with:
-              - 54321
               - 54321`)
 		assert.Contains(t, status.Content, `api/v2/series - API Key ending with:
               - 54321`)
