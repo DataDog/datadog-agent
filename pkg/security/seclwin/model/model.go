5be3d483a881af1260822b5005fc1f32fe5c8ebc
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2025-04-01 18:36:35 +0200

    sync model

--- pkg/security/seclwin/model/model.go
+++ pkg/security/seclwin/model/model.go
@@ -112,7 +112,8 @@ type SpanContext struct {
 
 // RuleContext defines a rule context
 type RuleContext struct {
-	MatchingSubExprs eval.MatchingSubExprs
+	Expression       string                `field:"-"`
+	MatchingSubExprs eval.MatchingSubExprs `field:"-"`
 }
 
 // BaseEvent represents an event sent from the kernel
