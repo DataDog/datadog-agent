5c44a08794949b082d592bee4ff158fa90a978e3
Pierre Lin <pierre.lin@datadoghq.com>
2025-04-02 00:04:55 +0200

    Do not import pkg/networkdevice/metadata for now

--- test/fakeintake/aggregator/ndmAggregator.go
+++ test/fakeintake/aggregator/ndmAggregator.go
@@ -11,14 +11,171 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/DataDog/datadog-agent/pkg/networkdevice/metadata"
 	"github.com/DataDog/datadog-agent/test/fakeintake/api"
 )
+
+// NetworkDevicesMetadata contains network devices metadata
+type NetworkDevicesMetadata struct {
+	Subnet           string                 `json:"subnet,omitempty"`
+	Namespace        string                 `json:"namespace"`
+	Integration      string                 `json:"integration"`
+	Devices          []DeviceMetadata       `json:"devices,omitempty"`
+	Interfaces       []InterfaceMetadata    `json:"interfaces,omitempty"`
+	IPAddresses      []IPAddressMetadata    `json:"ip_addresses,omitempty"`
+	Links            []TopologyLinkMetadata `json:"links,omitempty"`
+	NetflowExporters []NetflowExporter      `json:"netflow_exporters,omitempty"`
+	Diagnoses        []DiagnosisMetadata    `json:"diagnoses,omitempty"`
+	DeviceOIDs       []DeviceOID            `json:"device_oids,omitempty"`
+	DeviceScanStatus *ScanStatusMetadata    `json:"scan_status,omitempty"`
+	CollectTimestamp int64                  `json:"collect_timestamp"`
+}
+
+// DeviceMetadata contains device metadata
+type DeviceMetadata struct {
+	ID             string   `json:"id"`
+	IDTags         []string `json:"id_tags"` // id_tags is the input to produce device.id, it's also used to correlated with device metrics.
+	Tags           []string `json:"tags"`
+	IPAddress      string   `json:"ip_address"`
+	Status         int32    `json:"status"`
+	PingStatus     int32    `json:"ping_status,omitempty"`
+	Name           string   `json:"name,omitempty"`
+	Description    string   `json:"description,omitempty"`
+	SysObjectID    string   `json:"sys_object_id,omitempty"`
+	Location       string   `json:"location,omitempty"`
+	Profile        string   `json:"profile,omitempty"`
+	ProfileVersion uint64   `json:"profile_version,omitempty"`
+	Vendor         string   `json:"vendor,omitempty"`
+	Subnet         string   `json:"subnet,omitempty"`
+	SerialNumber   string   `json:"serial_number,omitempty"`
+	Version        string   `json:"version,omitempty"`
+	ProductName    string   `json:"product_name,omitempty"`
+	Model          string   `json:"model,omitempty"`
+	OsName         string   `json:"os_name,omitempty"`
+	OsVersion      string   `json:"os_version,omitempty"`
+	OsHostname     string   `json:"os_hostname,omitempty"`
+	Integration    string   `json:"integration,omitempty"` // indicates the source of the data SNMP, meraki_api, etc.
+	DeviceType     string   `json:"device_type,omitempty"`
+}
+
+// DeviceOID device scan oid data
+type DeviceOID struct {
+	DeviceID string `json:"device_id"`
+	OID      string `json:"oid"`
+	Type     string `json:"type"`
+	Value    string `json:"value"`
+}
+
+// ScanStatus type for the different possible scan statuses
+type ScanStatus string
+
+const (
+	// ScanStatusInProgress represents a scan in progress
+	ScanStatusInProgress ScanStatus = "in progress"
+	// ScanStatusCompleted represents a completed scan
+	ScanStatusCompleted ScanStatus = "completed"
+	// ScanStatusError represents a scan error
+	ScanStatusError ScanStatus = "error"
+)
+
+// ScanType type for the different possible scan types manual or rc_triggered
+type ScanType string
+
+const (
+	// ManualScan represents a manual scan
+	ManualScan ScanType = "manual"
+	// RCTriggeredScan represents a rc triggered scan
+	RCTriggeredScan ScanType = "rc_triggered"
+)
+
+// ScanStatusMetadata contains scan status metadata
+type ScanStatusMetadata struct {
+	DeviceID   string     `json:"device_id"`
+	ScanStatus ScanStatus `json:"scan_status"`
+	ScanType   ScanType   `json:"scan_type,omitempty"`
+}
+
+// InterfaceMetadata contains interface metadata
+type InterfaceMetadata struct {
+	DeviceID      string   `json:"device_id"`
+	IDTags        []string `json:"id_tags"`               // used to correlate with interface metrics
+	Index         int32    `json:"index"`                 // IF-MIB ifIndex type is InterfaceIndex (Integer32 (1..2147483647))
+	RawID         string   `json:"raw_id,omitempty"`      // used to uniquely identify the interface in the context of the device
+	RawIDType     string   `json:"raw_id_type,omitempty"` // used to indicate the type of identifier used (i.e. portId for Meraki switches, uplink for Meraki uplinks, blank for SNMP for compatibility)
+	Name          string   `json:"name,omitempty"`
+	Alias         string   `json:"alias,omitempty"`
+	Description   string   `json:"description,omitempty"`
+	MacAddress    string   `json:"mac_address,omitempty"`
+	AdminStatus   int      `json:"admin_status,omitempty"`   // IF-MIB ifAdminStatus type is INTEGER
+	OperStatus    int      `json:"oper_status,omitempty"`    // IF-MIB ifOperStatus type is INTEGER
+	MerakiEnabled *bool    `json:"meraki_enabled,omitempty"` // enabled bool for Meraki devices, use a pointer to determine if the value was actually sent
+	MerakiStatus  string   `json:"meraki_status,omitempty"`  // status for Meraki devices
+}
+
+// IPAddressMetadata contains ip address metadata
+type IPAddressMetadata struct {
+	InterfaceID string `json:"interface_id"`
+	IPAddress   string `json:"ip_address"`
+	Prefixlen   int32  `json:"prefixlen,omitempty"`
+}
+
+// TopologyLinkDevice contain device link data
+type TopologyLinkDevice struct {
+	DDID        string `json:"dd_id,omitempty"`
+	ID          string `json:"id,omitempty"`
+	IDType      string `json:"id_type,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	IPAddress   string `json:"ip_address,omitempty"`
+}
+
+// TopologyLinkInterface contain interface link data
+type TopologyLinkInterface struct {
+	DDID        string `json:"dd_id,omitempty"`
+	ID          string `json:"id"`
+	IDType      string `json:"id_type,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+// TopologyLinkSide contain data for remote or local side of the link
+type TopologyLinkSide struct {
+	Device    *TopologyLinkDevice    `json:"device,omitempty"`
+	Interface *TopologyLinkInterface `json:"interface,omitempty"`
+}
+
+// TopologyLinkMetadata contains topology interface to interface links metadata
+type TopologyLinkMetadata struct {
+	ID          string            `json:"id"`
+	SourceType  string            `json:"source_type"`
+	Integration string            `json:"integration,omitempty"`
+	Local       *TopologyLinkSide `json:"local"`
+	Remote      *TopologyLinkSide `json:"remote"`
+}
+
+// NetflowExporter contains netflow exporters info
+type NetflowExporter struct {
+	ID        string `json:"id"` // used by backend as unique id (e.g. in cache)
+	IPAddress string `json:"ip_address"`
+	FlowType  string `json:"flow_type"`
+}
+
+// Diagnosis contain data for a diagnosis
+type Diagnosis struct {
+	Severity string `json:"severity"`
+	Message  string `json:"message"`
+	Code     string `json:"code"`
+}
+
+// DiagnosisMetadata contains diagnoses info
+type DiagnosisMetadata struct {
+	ResourceType string      `json:"resource_type"`
+	ResourceID   string      `json:"resource_id"`
+	Diagnoses    []Diagnosis `json:"diagnoses"`
+}
 
 // NDMPayload represents an NDM payload
 type NDMPayload struct {
 	collectedTime time.Time
-	metadata.NetworkDevicesMetadata
+	NetworkDevicesMetadata
 }
 
 func (p *NDMPayload) name() string {
