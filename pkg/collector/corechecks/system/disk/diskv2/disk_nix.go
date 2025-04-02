31b349fefb498f5577b3f0645a2121779991bc10
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2025-04-02 15:30:28 +0200

    move new implementation from disk to diskv2 folder

---
+++ pkg/collector/corechecks/system/disk/diskv2/disk_nix.go
@@ -0,0 +1,166 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build !windows
+
+package disk
+
+import (
+	"encoding/xml"
+	"io"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+func compileRegExp(expr string) (*regexp.Regexp, error) {
+	return regexp.Compile(expr)
+}
+
+func (c *Check) configureCreateMounts() {
+}
+
+// LsblkCommand specifies the command used to retrieve block device information.
+var LsblkCommand = func() (string, error) {
+	cmd := exec.Command("lsblk", "--noheadings", "--raw", "--output=NAME,LABEL")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+var labelRegex = regexp.MustCompile(`LABEL="([^"]+)"`)
+
+func (c *Check) fetchAllDeviceLabelsFromLsblk() error {
+	log.Debugf("Fetching all device labels from lsblk")
+	rawOutput, err := LsblkCommand()
+	if err != nil {
+		return err
+	}
+	log.Debugf("lsblk output: %s", rawOutput)
+	lines := strings.Split(strings.TrimSpace(rawOutput), "\n")
+	c.deviceLabels = make(map[string]string)
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		log.Debugf("processing line: '%s'", line)
+		if line == "" {
+			log.Debugf("skipping empty line")
+			continue
+		}
+		// Typically line looks like:
+		// sda1  MY_LABEL
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			log.Debugf("skipping malformed line: '%s'", line)
+			continue
+		}
+		device := "/dev/" + fields[0]
+		label := fields[1]
+		c.deviceLabels[device] = label
+	}
+	return nil
+}
+
+// Device represents a device entry in an XML structure.
+type device struct {
+	XMLName xml.Name `xml:"device"`
+	Label   string   `xml:"LABEL,attr"`
+	Text    string   `xml:",chardata"`
+}
+
+// BlkidCacheCommand specifies the command used to query block device UUIDs and labels.
+var BlkidCacheCommand = func(blkidCacheFile string) (string, error) {
+	file, err := os.Open(blkidCacheFile)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func (c *Check) fetchAllDeviceLabelsFromBlkidCache() error {
+	log.Debugf("Fetching all device labels from blkid cache")
+	rawOutput, err := BlkidCacheCommand(c.instanceConfig.BlkidCacheFile)
+	if err != nil {
+		return err
+	}
+	log.Debugf("blkid cache output: %s", rawOutput)
+	lines := strings.Split(strings.TrimSpace(rawOutput), "\n")
+	c.deviceLabels = make(map[string]string)
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		log.Debugf("processing line: '%s'", line)
+		if line == "" {
+			log.Debugf("skipping empty line")
+			continue
+		}
+		var device device
+		err := xml.Unmarshal([]byte(line), &device)
+		if err != nil {
+			log.Debugf("Failed to parse line %s because of %v - skipping the line (some labels might be missing)\n", line, err)
+			continue
+		}
+
+		if device.Label != "" && device.Text != "" {
+			c.deviceLabels[device.Text] = device.Label
+		}
+	}
+	return nil
+}
+
+// BlkidCommand specifies the command used to retrieve block device attributes.
+var BlkidCommand = func() (string, error) {
+	cmd := exec.Command("blkid")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+func (c *Check) fetchAllDeviceLabelsFromBlkid() error {
+	log.Debugf("Fetching all device labels from blkid")
+	rawOutput, err := BlkidCommand()
+	if err != nil {
+		return err
+	}
+	c.deviceLabels = make(map[string]string)
+	lines := strings.Split(strings.TrimSpace(rawOutput), "\n")
+	c.deviceLabels = make(map[string]string)
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		log.Debugf("processing line: '%s'", line)
+		if line == "" {
+			log.Debugf("skipping empty line")
+			continue
+		}
+		// Typically line looks like:
+		// /dev/sda1: UUID="..." TYPE="ext4" LABEL="root"
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) < 2 {
+			log.Debugf("skipping malformed line: '%s'", line)
+			continue
+		}
+		device := strings.TrimSpace(parts[0])  // e.g. "/dev/sda1"
+		details := strings.TrimSpace(parts[1]) // e.g. `UUID="..." TYPE="ext4" LABEL="root"`
+		match := labelRegex.FindStringSubmatch(details)
+		if len(match) == 2 {
+			// match[1] is everything captured by ([^"]+)
+			c.deviceLabels[device] = match[1]
+		} else {
+			// No label found for this device
+			c.deviceLabels[device] = ""
+		}
+
+	}
+	return nil
+}
---
+++ pkg/collector/corechecks/system/disk/diskv2/disk_windows_test.go
@@ -0,0 +1,162 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build windows
+
+package disk_test
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/integration"
+	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
+	"github.com/DataDog/datadog-agent/pkg/collector/corechecks/system/disk/disk"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupPlatformMocks() {
+	disk.NetAddConnection = func(_mountType, _localName, _remoteName, _password, _username string) error {
+		return nil
+	}
+}
+
+func TestGivenADiskCheckWithDefaultConfig_WhenCheckRuns_ThenAllIOCountersMetricsAreReported(t *testing.T) {
+	setupDefaultMocks()
+	diskCheck := createCheck()
+	m := mocksender.NewMockSender(diskCheck.ID())
+	m.SetupAcceptAll()
+
+	diskCheck.Configure(m.GetSenderManager(), integration.FakeConfigHash, nil, nil, "test")
+	err := diskCheck.Run()
+
+	assert.Nil(t, err)
+	m.AssertMetric(t, "MonotonicCount", "system.disk.read_time", float64(300), "", []string{"device:/dev/sda1", "device_name:/dev/sda1"})
+	m.AssertMetric(t, "MonotonicCount", "system.disk.write_time", float64(450), "", []string{"device:/dev/sda1", "device_name:/dev/sda1"})
+	m.AssertMetric(t, "Rate", "system.disk.read_time_pct", float64(30), "", []string{"device:/dev/sda1", "device_name:/dev/sda1"})
+	m.AssertMetric(t, "Rate", "system.disk.write_time_pct", float64(45), "", []string{"device:/dev/sda1", "device_name:/dev/sda1"})
+	m.AssertMetric(t, "MonotonicCount", "system.disk.read_time", float64(500), "", []string{"device:/dev/sda2", "device_name:/dev/sda2"})
+	m.AssertMetric(t, "MonotonicCount", "system.disk.write_time", float64(150), "", []string{"device:/dev/sda2", "device_name:/dev/sda2"})
+	m.AssertMetric(t, "Rate", "system.disk.read_time_pct", float64(50), "", []string{"device:/dev/sda2", "device_name:/dev/sda2"})
+	m.AssertMetric(t, "Rate", "system.disk.write_time_pct", float64(15), "", []string{"device:/dev/sda2", "device_name:/dev/sda2"})
+}
+
+func TestGivenADiskCheckWithCreateMountsConfigured_WhenCheckIsConfigured_ThenMountsAreCreated(t *testing.T) {
+	setupDefaultMocks()
+	var netAddConnectionCalls []NetAddConnectionCall
+	disk.NetAddConnection = func(mountType, localName, remoteName, _password, _username string) error {
+		netAddConnectionCalls = append(netAddConnectionCalls, NetAddConnectionCall{
+			Name: "mount",
+			Args: []string{"-t", mountType, remoteName, localName},
+		})
+		return nil
+	}
+	diskCheck := createCheck()
+	m := mocksender.NewMockSender(diskCheck.ID())
+	m.SetupAcceptAll()
+	config := integration.Data([]byte(`
+create_mounts:
+- mountpoint: "p:"
+  host: smbserver
+  share: space
+- mountpoint: "s:"
+  user: auser
+  password: "somepassword"
+  host: smbserver
+  share: space
+  type: smb
+- mountpoint: "n:"
+  host: nfsserver
+  share: /mnt/nfs_share
+  type: nfs
+`))
+
+	diskCheck.Configure(m.GetSenderManager(), integration.FakeConfigHash, config, nil, "test")
+	err := diskCheck.Run()
+
+	assert.Nil(t, err)
+	assert.Equal(t, 3, len(netAddConnectionCalls))
+	expectedNetAddConnectionCalls := []NetAddConnectionCall{
+		{
+			Name: "mount",
+			Args: []string{"-t", "SMB", `\\smbserver\space`, "p:"},
+		},
+		{
+			Name: "mount",
+			Args: []string{"-t", "SMB", `\\auser:somepassword@smbserver\space`, "s:"},
+		},
+		{
+			Name: "mount",
+			Args: []string{"-t", "NFS", `nfsserver:/mnt/nfs_share`, "n:"},
+		},
+	}
+	for i, mountCall := range netAddConnectionCalls {
+		assert.Equal(t, expectedNetAddConnectionCalls[i], mountCall)
+	}
+}
+
+func TestGivenADiskCheckWithCreateMountsConfiguredWithoutHost_WhenCheckIsConfigured_ThenMountsAreNotCreated(t *testing.T) {
+	setupDefaultMocks()
+	var netAddConnectionCalls []NetAddConnectionCall
+	disk.NetAddConnection = func(mountType, localName, remoteName, _password, _username string) error {
+		netAddConnectionCalls = append(netAddConnectionCalls, NetAddConnectionCall{
+			Name: "mount",
+			Args: []string{"-t", mountType, remoteName, localName},
+		})
+		return nil
+	}
+	diskCheck := createCheck()
+	m := mocksender.NewMockSender(diskCheck.ID())
+	m.SetupAcceptAll()
+	config := integration.Data([]byte(`
+create_mounts:
+- mountpoint: "n:"
+  share: /mnt/nfs_share
+  type: nfs
+`))
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	logger, err := log.LoggerFromWriterWithMinLevelAndFormat(w, log.DebugLvl, "[%LEVEL] %Msg")
+	assert.Nil(t, err)
+	log.SetupLogger(logger, "debug")
+
+	err = diskCheck.Configure(m.GetSenderManager(), integration.FakeConfigHash, config, nil, "test")
+
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(netAddConnectionCalls))
+	w.Flush()
+	assert.Contains(t, b.String(), "Invalid configuration. Drive mount requires remote machine and share point")
+}
+
+func TestGivenADiskCheckWithCreateMountsConfigured_WhenCheckRunsAndIOCountersSystemCallReturnsError_ThenErrorMessagedIsLogged(t *testing.T) {
+	setupDefaultMocks()
+	disk.NetAddConnection = func(_mountType, _localName, _remoteName, _password, _username string) error {
+		return errors.New("error calling NetAddConnection")
+	}
+	diskCheck := createCheck()
+	m := mocksender.NewMockSender(diskCheck.ID())
+	m.SetupAcceptAll()
+	config := integration.Data([]byte(`
+create_mounts:
+- mountpoint: "p:"
+  host: smbserver
+  share: space
+`))
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	logger, err := log.LoggerFromWriterWithMinLevelAndFormat(w, log.DebugLvl, "[%LEVEL] %Msg")
+	assert.Nil(t, err)
+	log.SetupLogger(logger, "debug")
+
+	diskCheck.Configure(m.GetSenderManager(), integration.FakeConfigHash, config, nil, "test")
+	err = diskCheck.Run()
+
+	assert.Nil(t, err)
+	w.Flush()
+	assert.Contains(t, b.String(), `Failed to mount p: on \\smbserver\space`)
+}
