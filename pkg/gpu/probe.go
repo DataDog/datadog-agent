a98c9f6ade559aeb9cf6159a51c91a06e581e495
val06 <valeri.pliskin@datadoghq.com>
2025-04-03 14:49:28 +0300

    [EBPF] use nvmlLib as a global singleton (#35777)

--- pkg/gpu/probe.go
+++ pkg/gpu/probe.go
@@ -20,7 +20,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/ebpf/bytecode"
 
 	manager "github.com/DataDog/ebpf-manager"
-	"github.com/NVIDIA/go-nvml/pkg/nvml"
 	"github.com/cilium/ebpf"
 
 	"github.com/DataDog/datadog-agent/comp/core/telemetry"
@@ -31,7 +30,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/ebpf/uprobes"
 	"github.com/DataDog/datadog-agent/pkg/gpu/config"
 	gpuebpf "github.com/DataDog/datadog-agent/pkg/gpu/ebpf"
-	ddnvml "github.com/DataDog/datadog-agent/pkg/gpu/nvml"
 	"github.com/DataDog/datadog-agent/pkg/network/usm/sharedlibraries"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
@@ -88,9 +86,6 @@ type ProbeDependencies struct {
 	// Telemetry is the telemetry component
 	Telemetry telemetry.Component
 
-	// NvmlLib is the NVML library interface
-	NvmlLib nvml.Interface
-
 	// ProcessMonitor is the process monitor interface
 	ProcessMonitor uprobes.ProcessMonitor
 
@@ -101,14 +96,8 @@ type ProbeDependencies struct {
 
 // NewProbeDependencies creates a new ProbeDependencies instance
 func NewProbeDependencies(telemetry telemetry.Component, processMonitor uprobes.ProcessMonitor, workloadMeta workloadmeta.Component) (ProbeDependencies, error) {
-	nvmlLib, err := ddnvml.GetNvmlLib()
-	if err != nil {
-		return ProbeDependencies{}, fmt.Errorf("unable to get NVML library: %w", err)
-	}
-
 	return ProbeDependencies{
 		Telemetry:      telemetry,
-		NvmlLib:        nvmlLib,
 		ProcessMonitor: processMonitor,
 		WorkloadMeta:   workloadMeta,
 	}, nil
@@ -155,7 +144,7 @@ func NewProbe(cfg *config.Config, deps ProbeDependencies) (*Probe, error) {
 	}
 
 	attachCfg := getAttacherConfig(cfg)
-	sysCtx, err := getSystemContext(deps.NvmlLib, cfg.ProcRoot, deps.WorkloadMeta, deps.Telemetry)
+	sysCtx, err := getSystemContext(cfg.ProcRoot, deps.WorkloadMeta, deps.Telemetry)
 	if err != nil {
 		return nil, fmt.Errorf("error getting system context: %w", err)
 	}
