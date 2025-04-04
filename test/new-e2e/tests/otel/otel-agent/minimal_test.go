8906cef6c2594dc2a5850057746417cf08b8abb9
Olivier G <olivier.gaca@datadoghq.com>
2025-04-04 13:25:31 +0200

    Always set DD_OTELCOLLECTOR_ENABLED at true for otel e2e tests

--- test/new-e2e/tests/otel/otel-agent/minimal_test.go
+++ test/new-e2e/tests/otel/otel-agent/minimal_test.go
@@ -36,14 +36,12 @@ var minimalFullConfig string
 var sources string
 
 func TestOTelAgentMinimal(t *testing.T) {
-	values := `
+	values := enableOTELAgentonfig(`
 datadog:
   logs:
     containerCollectAll: false
     containerCollectUsingFiles: false
-env:
-  DD_OTELCOLLECTOR_ENABLED: true
-`
+`)
 	t.Parallel()
 	e2e.Run(t, &minimalTestSuite{}, e2e.WithProvisioner(awskubernetes.KindProvisioner(awskubernetes.WithAgentOptions(kubernetesagentparams.WithHelmValues(values), kubernetesagentparams.WithOTelAgent(), kubernetesagentparams.WithOTelConfig(minimalConfig)))))
 }
