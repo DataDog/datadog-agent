e1db5d9664bf449e0efc66dc3faab1df353c1bb9
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-04-08 16:20:46 +0200

    check for file existence for safety

--- omnibus/python-scripts/post.py
+++ omnibus/python-scripts/post.py
@@ -17,13 +17,14 @@ def post(install_directory, storage_location, skip_flag=False):
             packages.create_python_installed_packages_file(post_python_installed_packages_file)
             flag_path = os.path.join(storage_location, ".skip_install_python_third_party_deps")
 
-            # this approach makes it easier to expose this file flag as an environment variable 
+            # this approach makes it easier to expose this file flag as an environment variable
             # in the agent install script
             skip_third_party_integration = False
-            with open(flag_path, 'r') as f:
-                flag_content = f.read()
-                if flag_content.strip().lower() in ["n", "no", "false", "0", ""]:
-                    skip_third_party_integration = True
+            if os.path.exists(flag_path):
+                with open(flag_path, "r") as f:
+                    flag_content = f.read()
+                    if flag_content.strip().lower() in ["n", "no", "false", "0", ""]:
+                        skip_third_party_integration = True
                     
             if not skip_third_party_integration and skip_flag:
                 print(f"File '{flag_path}' found")
