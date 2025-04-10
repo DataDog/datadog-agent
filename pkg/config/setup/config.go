d668331e7f2f52bb3dfec7dd5cfe8d41741f030e
Vlad Savlidis <vlad.savlidis@datadoghq.com>
2025-04-10 14:36:14 -0400

    made the remote config org status polling interval configurable

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -1199,6 +1199,7 @@ func remoteconfig(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("remote_configuration.config_root", "")
 	config.BindEnvAndSetDefault("remote_configuration.director_root", "")
 	config.BindEnv("remote_configuration.refresh_interval")
+	config.BindEnv("remote_configuration.org_status_refresh_interval")
 	config.BindEnvAndSetDefault("remote_configuration.max_backoff_interval", 5*time.Minute)
 	config.BindEnvAndSetDefault("remote_configuration.clients.ttl_seconds", 30*time.Second)
 	config.BindEnvAndSetDefault("remote_configuration.clients.cache_bypass_limit", 5)
