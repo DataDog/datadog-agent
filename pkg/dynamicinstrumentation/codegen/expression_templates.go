8c3f4ed4e90d627f4bc8488cfea9ede2538997d2
grantseltzer <grantseltzer@gmail.com>
2025-04-10 14:16:21 -0500

    Add comments
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- pkg/dynamicinstrumentation/codegen/expression_templates.go
+++ pkg/dynamicinstrumentation/codegen/expression_templates.go
@@ -118,10 +118,12 @@ bpf_printk("Setting param index %d to %d", {{.Arg1}}, context.output_offset);
 event->base.param_indicies[{{.Arg1}}] = context.output_offset;
 `
 
+// This causes a compiler error which is used in testing
 var compilerErrorText = `
 !@#$%^
 `
 
+// This causes a verifier error which is used in testing
 var verifierErrorText = `
 for (int i=0; i==0;) {
     i++;
