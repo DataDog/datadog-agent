e3adc25ac7ea1ff60ff6fd00b83b4ec382a9a291
Adrien Boitreaud <72934368+aboitreaud@users.noreply.github.com>
2025-04-10 12:40:21 +0200

    [DJM] Update Java tracer pinned version to 1.48.0 (#35987)

--- pkg/fleet/installer/setup/djm/databricks.go
+++ pkg/fleet/installer/setup/djm/databricks.go
@@ -18,7 +18,7 @@ import (
 
 const (
 	databricksInjectorVersion   = "0.35.0-1"
-	databricksJavaTracerVersion = "1.46.1-1"
+	databricksJavaTracerVersion = "1.48.0-1"
 	databricksAgentVersion      = "7.63.3-1"
 )
 
