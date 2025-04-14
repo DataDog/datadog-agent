b297b813b64e10c226cff74dcbabd98760e83ca0
Adrien Boitreaud <72934368+aboitreaud@users.noreply.github.com>
2025-04-14 16:40:22 +0200

    [DJMS-38] Tag spans if logs are enabled (#36098)

--- pkg/fleet/installer/setup/djm/databricks.go
+++ pkg/fleet/installer/setup/djm/databricks.go
@@ -219,6 +219,7 @@ func setupDatabricksDriver(s *common.Setup) {
 	if os.Getenv("DRIVER_LOGS_ENABLED") == "true" {
 		s.Config.DatadogYAML.LogsEnabled = true
 		sparkIntegration.Logs = driverLogs
+		s.Span.SetTag("host_tag_set.driver_logs_enabled", "true")
 	}
 	if os.Getenv("DB_DRIVER_IP") != "" {
 		sparkIntegration.Instances = []any{
@@ -242,6 +243,7 @@ func setupDatabricksWorker(s *common.Setup) {
 	if os.Getenv("WORKER_LOGS_ENABLED") == "true" {
 		s.Config.DatadogYAML.LogsEnabled = true
 		sparkIntegration.Logs = workerLogs
+		s.Span.SetTag("host_tag_set.worker_logs_enabled", "true")
 	}
 	s.Config.IntegrationConfigs["spark.d/databricks.yaml"] = sparkIntegration
 }
