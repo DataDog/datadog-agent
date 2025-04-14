de3e33d606b2ac24254f5770b900ca73e1eea2ca
Stan Rozenraukh <stan.rozenraukh@datadoghq.com>
2025-04-14 15:59:46 -0400

    squash me-- WIP mutator

--- pkg/clusteragent/admission/mutate/autoinstrumentation/service_name.go
+++ pkg/clusteragent/admission/mutate/autoinstrumentation/service_name.go
@@ -17,17 +17,82 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
+type serviceNameSource string
+
+const (
+	serviceNameSourceExistingEnv serviceNameSource = "existing"
+	serviceNameSourceOwnerName                     = "owner"
+)
+
+type serviceNameMutator struct {
+	noop   bool
+	envVar corev1.EnvVar
+	source serviceNameSource
+}
+
+func (s *serviceNameMutator) mutateContainer(c *corev1.Container) error {
+	if s.noop {
+		return nil
+	}
+
+	for _, e := range c.Env {
+		if e.Name == kubernetes.ServiceTagEnvVar {
+			return nil
+		}
+	}
+
+	var envs []corev1.EnvVar
+	envs = append(envs, s.envVar)
+
+	if s.source != "" {
+		envs = append(envs, corev1.EnvVar{
+			Name:  "DD_UST_K8S_ENV_SOURCE",
+			Value: fmt.Sprintf("%s:%s", kubernetes.ServiceTagEnvVar, s.source),
+		})
+	}
+
+	c.Env = append(envs, c.Env...)
+	return nil
+}
+
+func newServiceNameMutator(pod *corev1.Pod) *serviceNameMutator {
+	vars := findServiceNameEnvVarsInPod(pod)
+	if len(vars) > 1 {
+		log.Debug("more than one unique definition of service name found for the pod")
+	}
+
+	if len(vars) > 0 {
+		return &serviceNameMutator{
+			envVar: vars[0],
+			source: serviceNameSourceExistingEnv,
+		}
+	}
+
+	name, err := getServiceNameFromPodOwnerName(pod)
+	if err != nil {
+		log.Debugf("error getting owner name for pod: %v", err)
+		return &serviceNameMutator{noop: true}
+	}
+
+	if name == "" {
+		return &serviceNameMutator{noop: true}
+	}
+
+	return &serviceNameMutator{
+		envVar: corev1.EnvVar{
+			Name:  kubernetes.ServiceTagEnvVar,
+			Value: name,
+		},
+		source: serviceNameSourceOwnerName,
+	}
+}
+
 func encodeEnvVar(e corev1.EnvVar) string {
 	out, _ := json.Marshal(e)
 	return string(out)
 }
 
 func findServiceNameEnvVarsInPod(pod *corev1.Pod) []corev1.EnvVar {
-	// grouped by env var for DD_SERVICE,
-	// all of the env var definitions and each container.
-	//
-	// we hold onto all of these because we should at least log
-	// a warning for when they are incompatible.
 	found := []corev1.EnvVar{}
 	keys := map[string]int{}
 
@@ -48,6 +113,8 @@ func findServiceNameEnvVarsInPod(pod *corev1.Pod) []corev1.EnvVar {
 		}
 	}
 
+	// we only look for the service name in the container (and not)
+	// init containers.
 	for _, c := range pod.Spec.Containers {
 		iterContainer(&c)
 	}
