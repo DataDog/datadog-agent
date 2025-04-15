36fb50a3236672e8bc7acca18bed41ce801fda75
Jack Phillips <jack.phillips@datadoghq.com>
2025-04-15 12:49:37 -0400

    make service start/stop use winutils

--- cmd/agent/subcommands/run/dependent_services_windows.go
+++ cmd/agent/subcommands/run/dependent_services_windows.go
@@ -7,16 +7,9 @@
 package run
 
 import (
-	"fmt"
-	"syscall"
-
-	"golang.org/x/sys/windows"
-	"golang.org/x/sys/windows/svc"
-	"golang.org/x/sys/windows/svc/mgr"
-
 	"github.com/DataDog/datadog-agent/pkg/config/model"
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
-	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/pkg/util/winutil"
 )
 
 type serviceInitFunc func() (err error)
@@ -107,81 +100,18 @@ func installerInit() error {
 	return nil
 }
 
-// getServiceHandle returns a service handle and manager for the given service name
-func (s *Servicedef) getServiceHandle() (*mgr.Mgr, *mgr.Service, error) {
-	/*
-	 * default go implementations of mgr.Connect and mgr.OpenService use way too
-	 * open permissions by default.  Use those structures so the other methods
-	 * work properly, but initialize them here using restrictive enough permissions
-	 * that we can actually open/start the service when running as non-root.
-	 */
-	h, err := windows.OpenSCManager(nil, nil, windows.SC_MANAGER_CONNECT)
-	if err != nil {
-		log.Warnf("Failed to connect to scm %v", err)
-		return nil, nil, err
-	}
-	m := &mgr.Mgr{Handle: h}
-
-	snptr, err := syscall.UTF16PtrFromString(s.serviceName)
-	if err != nil {
-		log.Warnf("Failed to get service name %v", err)
-		m.Disconnect()
-		return nil, nil, fmt.Errorf("could not create service name pointer: %s", err)
-	}
-
-	hSvc, err := windows.OpenService(m.Handle, snptr,
-		windows.SERVICE_START|windows.SERVICE_STOP)
-	if err != nil {
-		log.Warnf("Failed to open service %v", err)
-		m.Disconnect()
-		return nil, nil, fmt.Errorf("could not access service: %v", err)
-	}
-	scm := &mgr.Service{Name: s.serviceName, Handle: hSvc}
-
-	return m, scm, nil
-}
-
 // Start starts the service
 func (s *Servicedef) Start() error {
-	if s.serviceInit != nil {
-		err := s.serviceInit()
-		if err != nil {
-			log.Warnf("Failed to initialize %s service: %s", s.name, err.Error())
-			return err
-		}
-	}
-
-	m, scm, err := s.getServiceHandle()
-	if err != nil {
-		return err
-	}
-	defer m.Disconnect()
-	defer scm.Close()
-
-	err = scm.Start("is", "manual-started")
-	if err != nil {
-		log.Warnf("Failed to start service %v", err)
-		return fmt.Errorf("could not start service: %v", err)
-	}
-
-	return nil
+	// we use the winutil StartService because it opens the service
+	// with the correct permissions for us and not the default of SC_MANAGER_ALL
+	// that the svc package uses
+	return winutil.StartService(s.serviceName)
 }
 
 // Stop stops the service
 func (s *Servicedef) Stop() error {
-	m, scm, err := s.getServiceHandle()
-	if err != nil {
-		return err
-	}
-	defer m.Disconnect()
-	defer scm.Close()
-
-	status, err := scm.Control(svc.Stop)
-	if err != nil {
-		log.Warnf("Failed to stop service %v", err)
-		return fmt.Errorf("could not stop service: %v", err)
-	}
-	log.Debugf("Service %s stopped with status %v", s.serviceName, status)
-
-	return nil
+	// note that this will stop the service and any services that depend on it
+	// it will also wait for the service to stop and return an error if it doesn't stop
+	// the default timeout is 30 seconds
+	return winutil.StopService(s.serviceName)
 }
