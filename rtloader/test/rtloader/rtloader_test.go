5c321880ffea867fb0712580abbb437a73902d9f
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2025-04-15 10:53:22 +0200

    reset executable value

--- rtloader/test/rtloader/rtloader_test.go
+++ rtloader/test/rtloader/rtloader_test.go
@@ -96,28 +96,6 @@ with open(r'%s', 'w') as f:
 	helpers.AssertMemoryUsage(t)
 }
 
-func TestSysProgramNameValue(t *testing.T) {
-	// Reset memory counters
-	helpers.ResetMemoryStats()
-
-	code := fmt.Sprintf(`
-import sys
-with open(r'%s', 'w') as f:
- f.write(sys.argv[0])`, tmpfile.Name())
-
-	output, err := runString(code)
-	if err != nil {
-		t.Fatalf("`test_sys_program_name` error: %v", err)
-	}
-
-	if output != "/folder/mock_python_interpeter_bin_path" {
-		t.Errorf("Unexpected sys.argv[0] value: '%s'", output)
-	}
-
-	// Check for leaks
-	helpers.AssertMemoryUsage(t)
-}
-
 func TestSysHomeValue(t *testing.T) {
 	// Reset memory counters
 	helpers.ResetMemoryStats()
