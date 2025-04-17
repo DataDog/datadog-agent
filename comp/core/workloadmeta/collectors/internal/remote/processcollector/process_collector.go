bda712c58f2927704ea15b9af0ee2e9acf5b5a9c
Sylvain Baubeau <sylvain.baubeau@datadoghq.com>
2025-04-17 15:54:18 +0200

    Remove unused entity assignment

--- comp/core/workloadmeta/collectors/internal/remote/processcollector/process_collector.go
+++ comp/core/workloadmeta/collectors/internal/remote/processcollector/process_collector.go
@@ -235,7 +235,6 @@ func (s *streamHandler) populateMissingContainerID(collectorEvents []workloadmet
 			processEntity.ContainerID = ctrIDFromProvider
 		}
 
-		event.Entity = processEntity
 		collectorEvents[idx] = event
 	}
 }
