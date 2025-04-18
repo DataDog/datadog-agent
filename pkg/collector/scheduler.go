b4382994909771104483d2c4dca454ba4bbcb7ea
Pierre Lin <pierre.lin@datadoghq.com>
2025-04-18 17:30:59 +0200

    Invert loaders only for SNMP

--- pkg/collector/scheduler.go
+++ pkg/collector/scheduler.go
@@ -190,7 +190,15 @@ func (s *CheckScheduler) getChecks(config integration.Config) ([]check.Check, er
 			log.Debugf("Loading check instance for check '%s' using default loaders", config.Name)
 		}
 
-		for _, loader := range s.loaders {
+		// TODO: Remove this special case to use Core loader by default for SNMP
+		loaderList := s.loaders
+		if config.Name == "snmp" && selectedInstanceLoader == "" {
+			if loaderList[0].Name() == "python" && loaderList[1].Name() == "core" {
+				loaderList[0], loaderList[1] = loaderList[1], loaderList[0]
+			}
+		}
+
+		for _, loader := range loaderList {
 			// the loader is skipped if the loader name is set and does not match
 			if (selectedInstanceLoader != "") && (selectedInstanceLoader != loader.Name()) {
 				log.Debugf("Loader name %v does not match, skip loader %v for check %v", selectedInstanceLoader, loader.Name(), config.Name)
