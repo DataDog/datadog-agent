DataDog/datadog-agent

commit 9e591920c7f8b25073b2e37812833925ce2ed2d2

[usm] Refactor sock maps (#24753)
Co-authored-by:  <pedro.lambert@datadog.hq.com>

pkg/network/ebpf/c/protocols/sockfd-probes.h
@@ -20,24 +20,21 @@ int kprobe__tcp_close(struct pt_regs *ctx) {
         return 0;
     }
 
-    pid_fd_t* pid_fd = bpf_map_lookup_elem(&pid_fd_by_sock, &sk);
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    conn_tuple_t t;
+    if (!read_conn_tuple(&t, sk, pid_tgid, CONN_TYPE_TCP)) {
+        return 0;
+    }
+
+    pid_fd_t *pid_fd = bpf_map_lookup_elem(&pid_fd_by_tuple, &t);
     if (pid_fd == NULL) {
         return 0;
     }
 
-    // Copy map value to stack before re-using it (needed for older kernels)
-    pid_fd_t pid_fd_copy = {};
-    bpf_memcpy(&pid_fd_copy, pid_fd, sizeof(pid_fd_t));
-    pid_fd = &pid_fd_copy;
-
-    bpf_map_delete_elem(&sock_by_pid_fd, pid_fd);
-    bpf_map_delete_elem(&pid_fd_by_sock, &sk);
-
-    conn_tuple_t t;
-    u64 pid_tgid = bpf_get_current_pid_tgid();
-    if (!read_conn_tuple(&t, sk, pid_tgid, CONN_TYPE_TCP)) {
-        return 0;
-    }
+    // Copy map value to stack so we can use it as a map key (needed for older kernels)
+    pid_fd_t pid_fd_copy = *pid_fd;
+    bpf_map_delete_elem(&tuple_by_pid_fd, &pid_fd_copy);
+    bpf_map_delete_elem(&pid_fd_by_tuple, &t);
 
     // The cleanup of the map happens either during TCP termination or during the TLS shutdown event.
     // TCP termination is managed by the socket filter, thus it cannot clean TLS entries,
@@ -60,8 +57,8 @@ int kprobe__sockfd_lookup_light(struct pt_regs *ctx) {
         .pid = pid_tgid >> 32,
         .fd = sockfd,
     };
-    struct sock **sock = bpf_map_lookup_elem(&sock_by_pid_fd, &key);
-    if (sock != NULL) {
+    conn_tuple_t *t = bpf_map_lookup_elem(&tuple_by_pid_fd, &key);
+    if (t != NULL) {
         return 0;
     }
 
@@ -94,6 +91,7 @@ int kretprobe__sockfd_lookup_light(struct pt_regs *ctx) {
         return 0;
     }
 
+    // NOTE: the code below should be executed only once for a given socket
     // For now let's only store information for TCP sockets
     struct socket *socket = (struct socket *)PT_REGS_RC(ctx);
     if (!socket)
@@ -119,14 +117,19 @@ int kretprobe__sockfd_lookup_light(struct pt_regs *ctx) {
         goto cleanup;
     }
 
+    conn_tuple_t t;
+    if (!read_conn_tuple(&t, sock, pid_tgid, CONN_TYPE_TCP)) {
+        goto cleanup;
+    }
+
     pid_fd_t pid_fd = {
         .pid = pid_tgid >> 32,
         .fd = (*sockfd),
     };
 
     // These entries are cleaned up by tcp_close
-    bpf_map_update_with_telemetry(pid_fd_by_sock, &sock, &pid_fd, BPF_ANY);
-    bpf_map_update_with_telemetry(sock_by_pid_fd, &pid_fd, &sock, BPF_ANY);
+    bpf_map_update_with_telemetry(pid_fd_by_tuple, &t, &pid_fd, BPF_ANY);
+    bpf_map_update_with_telemetry(tuple_by_pid_fd, &pid_fd, &t, BPF_ANY);
 cleanup:
     bpf_map_delete_elem(&sockfd_lookup_args, &pid_tgid);
     return 0;

pkg/network/ebpf/c/netns.h
@@ -14,7 +14,7 @@
 #endif
 
 #ifdef COMPILE_PREBUILT
-
+#include <net/sock.h>
 static __always_inline __u32 get_netns_from_sock(struct sock* sk) {
     void* skc_net = NULL;
     __u32 net_ns_inum = 0;

pkg/network/ebpf/c/protocols/sockfd.h
@@ -13,8 +13,8 @@
 // * Value the socket FD;
 BPF_HASH_MAP(sockfd_lookup_args, __u64, __u32, 1024)
 
-BPF_HASH_MAP(sock_by_pid_fd, pid_fd_t, struct sock *, 1024)
+BPF_HASH_MAP(tuple_by_pid_fd, pid_fd_t, conn_tuple_t, 1024)
 
-BPF_HASH_MAP(pid_fd_by_sock, struct sock *, pid_fd_t, 1024)
+BPF_HASH_MAP(pid_fd_by_tuple, conn_tuple_t, pid_fd_t, 1024)
 
 #endif

pkg/network/ebpf/c/protocols/tls/go-tls-conn.h
@@ -50,17 +50,12 @@ static __always_inline conn_tuple_t* conn_tup_from_tls_conn(tls_offsets_data_t* 
         .fd = fd,
     };
 
-    struct sock **sock = bpf_map_lookup_elem(&sock_by_pid_fd, &pid_fd);
-    if (sock == NULL)  {
+    conn_tuple_t *conn_tuple = bpf_map_lookup_elem(&tuple_by_pid_fd, &pid_fd);
+    if (conn_tuple == NULL)  {
         return NULL;
     }
 
-    conn_tuple_t conn_tuple = {0};
-    if (!read_conn_tuple(&conn_tuple, *sock, pid_tgid, CONN_TYPE_TCP)) {
-        return NULL;
-    }
-
-    bpf_map_update_elem(&conn_tup_by_go_tls_conn, &conn, &conn_tuple, BPF_ANY);
+    bpf_map_update_elem(&conn_tup_by_go_tls_conn, &conn, conn_tuple, BPF_ANY);
     return bpf_map_lookup_elem(&conn_tup_by_go_tls_conn, &conn);
 }
 

pkg/network/ebpf/c/protocols/tls/https.h
@@ -178,17 +178,12 @@ static __always_inline conn_tuple_t* tup_from_ssl_ctx(void *ssl_ctx, u64 pid_tgi
         .fd = ssl_sock->fd,
     };
 
-    struct sock **sock = bpf_map_lookup_elem(&sock_by_pid_fd, &pid_fd);
-    if (sock == NULL)  {
+    conn_tuple_t *t = bpf_map_lookup_elem(&tuple_by_pid_fd, &pid_fd);
+    if (t == NULL)  {
         return NULL;
     }
 
-    conn_tuple_t t;
-    if (!read_conn_tuple(&t, *sock, pid_tgid, CONN_TYPE_TCP)) {
-        return NULL;
-    }
-
-    bpf_memcpy(&ssl_sock->tup, &t, sizeof(conn_tuple_t));
+    bpf_memcpy(&ssl_sock->tup, t, sizeof(conn_tuple_t));
     return &ssl_sock->tup;
 }
 

pkg/network/usm/ebpf_main.go
@@ -61,8 +61,8 @@ const (
 	protocolDispatcherSocketFilterFunction = "socket__protocol_dispatcher"
 	connectionStatesMap                    = "connection_states"
 	sockFDLookupArgsMap                    = "sockfd_lookup_args"
-	sockByPidFDMap                         = "sock_by_pid_fd"
-	pidFDBySockMap                         = "pid_fd_by_sock"
+	tupleByPidFDMap                        = "tuple_by_pid_fd"
+	pidFDByTupleMap                        = "pid_fd_by_tuple"
 
 	sockFDLookup    = "kprobe__sockfd_lookup_light"
 	sockFDLookupRet = "kretprobe__sockfd_lookup_light"
@@ -98,8 +98,8 @@ func newEBPFProgram(c *config.Config, connectionProtocolMap *ebpf.Map) (*ebpfPro
 			{Name: protocols.ProtocolDispatcherProgramsMap},
 			{Name: connectionStatesMap},
 			{Name: sockFDLookupArgsMap},
-			{Name: sockByPidFDMap},
-			{Name: pidFDBySockMap},
+			{Name: tupleByPidFDMap},
+			{Name: pidFDByTupleMap},
 		},
 		Probes: []*manager.Probe{
 			{
@@ -385,11 +385,11 @@ func (e *ebpfProgram) init(buf bytecode.AssetReader, options manager.Options) er
 			MaxEntries: e.cfg.MaxTrackedConnections,
 			EditorFlag: manager.EditMaxEntries,
 		},
-		sockByPidFDMap: {
+		tupleByPidFDMap: {
 			MaxEntries: e.cfg.MaxTrackedConnections,
 			EditorFlag: manager.EditMaxEntries,
 		},
-		pidFDBySockMap: {
+		pidFDByTupleMap: {
 			MaxEntries: e.cfg.MaxTrackedConnections,
 			EditorFlag: manager.EditMaxEntries,
 		},
@@ -509,19 +509,19 @@ func (e *ebpfProgram) dumpMapsHandler(w io.Writer, _ *manager.Manager, mapName s
 			spew.Fdump(w, key, value)
 		}
 
-	case sockByPidFDMap: // maps/sock_by_pid_fd (BPF_MAP_TYPE_HASH), key C.pid_fd_t, value uintptr // C.struct sock*
-		io.WriteString(w, "Map: '"+mapName+"', key: 'C.pid_fd_t', value: 'uintptr // C.struct sock*'\n")
+	case tupleByPidFDMap: // maps/tuple_by_pid_fd (BPF_MAP_TYPE_HASH), key C.pid_fd_t, value C.conn_tuple_t
+		io.WriteString(w, "Map: '"+mapName+"', key: 'C.pid_fd_t', value: 'C.conn_tuple_t'\n")
 		iter := currentMap.Iterate()
 		var key netebpf.PIDFD
-		var value uintptr // C.struct sock*
+		var value http.ConnTuple
 		for iter.Next(unsafe.Pointer(&key), unsafe.Pointer(&value)) {
 			spew.Fdump(w, key, value)
 		}
 
-	case pidFDBySockMap: // maps/pid_fd_by_sock (BPF_MAP_TYPE_HASH), key uintptr // C.struct sock*, value C.pid_fd_t
-		io.WriteString(w, "Map: '"+mapName+"', key: 'uintptr // C.struct sock*', value: 'C.pid_fd_t'\n")
+	case pidFDByTupleMap: // maps/pid_fd_by_tuple (BPF_MAP_TYPE_HASH), key C.conn_tuple_t, value C.pid_fd_t
+		io.WriteString(w, "Map: '"+mapName+"', key: 'C.conn_tuple_t', value: 'C.pid_fd_t'\n")
 		iter := currentMap.Iterate()
-		var key uintptr // C.struct sock*
+		var key http.ConnTuple
 		var value netebpf.PIDFD
 		for iter.Next(unsafe.Pointer(&key), unsafe.Pointer(&value)) {
 			spew.Fdump(w, key, value)

commit 7f5b427fa9f70f63347ca830d83db5d65ad9d2ee

usm: always call tls finish in tcp_close
In case we didn't have an entry in pid_fd_by_tuple, we would have missed calling tls_finish
although there's no justification for that. This bug was introduced in PR #24753

pkg/network/ebpf/c/protocols/sockfd-probes.h
@@ -28,14 +28,12 @@ int BPF_KPROBE(kprobe__tcp_close, struct sock *sk) {
     }
 
     pid_fd_t *pid_fd = bpf_map_lookup_elem(&pid_fd_by_tuple, &t);
-    if (pid_fd == NULL) {
-        return 0;
+    if (pid_fd != NULL) {
+        // Copy map value to stack so we can use it as a map key (needed for older kernels)
+        pid_fd_t pid_fd_copy = *pid_fd;
+        bpf_map_delete_elem(&tuple_by_pid_fd, &pid_fd_copy);
+        bpf_map_delete_elem(&pid_fd_by_tuple, &t);
     }
-
-    // Copy map value to stack so we can use it as a map key (needed for older kernels)
-    pid_fd_t pid_fd_copy = *pid_fd;
-    bpf_map_delete_elem(&tuple_by_pid_fd, &pid_fd_copy);
-    bpf_map_delete_elem(&pid_fd_by_tuple, &t);
 
     // The cleanup of the map happens either during TCP termination or during the TLS shutdown event.
     // TCP termination is managed by the socket filter, thus it cannot clean TLS entries,