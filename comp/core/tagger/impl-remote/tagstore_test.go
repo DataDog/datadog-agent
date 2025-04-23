e388a3dfd18cd8581b65a915ea5bbd7272aec961
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2025-04-23 17:08:14 +0200

    Revert "[CONTP-698] fix bug in EntityID.Empty method (#35793)"
    This reverts commit 609c2de46a6338c745b07ca9c8bcd4beca00bbfb.

--- comp/core/tagger/impl-remote/tagstore_test.go
+++ comp/core/tagger/impl-remote/tagstore_test.go
@@ -18,8 +18,8 @@ import (
 )
 
 var (
-	entityID        types.EntityID = types.NewEntityID(types.ContainerID, "bar")
-	anotherEntityID types.EntityID = types.NewEntityID(types.ContainerID, "quux")
+	entityID        types.EntityID = types.NewEntityID("foo", "bar")
+	anotherEntityID types.EntityID = types.NewEntityID("foo", "quux")
 )
 
 func TestProcessEvent_AddAndModify(t *testing.T) {
