467abbb67f2fa16eecf80ad9568b0fff91aafaa3
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2025-04-23 15:23:28 +0200

    chore: remove data.tar.xz after decompression

--- tasks/package.py
+++ tasks/package.py
@@ -216,6 +216,7 @@ def _extract_deb(ctx: Context, deb_path: str, extract_path: str):
     ctx.run(f"tar xf {deb_path} -C {extract_path}")
     with ctx.cd(extract_path):
         ctx.run("tar xf data.tar.xz")
+        ctx.run("rm data.tar.xz")
 
 
 def _extract_rpm(ctx: Context, rpm_path: str, extract_path: str):
