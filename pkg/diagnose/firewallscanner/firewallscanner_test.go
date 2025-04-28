c9a3e447b59a0b0af9ceff09d7af333d496342c9
Pierre Lin <pierre.lin@datadoghq.com>
2025-04-28 15:20:47 +0200

    Create tests in firewallscanner_test

--- pkg/diagnose/firewallscanner/firewallscanner_test.go
+++ pkg/diagnose/firewallscanner/firewallscanner_test.go
@@ -4,3 +4,65 @@
 // Copyright 2025-present Datadog, Inc.
 
 package firewallscanner
+
+import (
+	diagnose "github.com/DataDog/datadog-agent/comp/core/diagnose/def"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_buildBlockedPortsDiagnosis(t *testing.T) {
+	tests := []struct {
+		name              string
+		diagnosisName     string
+		forProtocol       string
+		blockedPorts      []blockedPort
+		expectedDiagnosis diagnose.Diagnosis
+	}{
+		{
+			name:          "no blocked ports",
+			diagnosisName: "Diagnosis Name",
+			forProtocol:   "TCP",
+			blockedPorts:  []blockedPort{},
+			expectedDiagnosis: diagnose.Diagnosis{
+				Status:    diagnose.DiagnosisSuccess,
+				Name:      "Diagnosis Name",
+				Diagnosis: "No blocking firewall rules were found",
+			},
+		},
+		{
+			name:          "with blocked ports",
+			diagnosisName: "Diagnosis Name",
+			forProtocol:   "UDP",
+			blockedPorts: []blockedPort{
+				{
+					Port: "9162",
+					ForIntegrations: []string{
+						"snmp_traps",
+						"netflow (ipfix)",
+					},
+				},
+				{
+					Port: "1234",
+					ForIntegrations: []string{
+						"netflow (sflow5)",
+					},
+				},
+			},
+			expectedDiagnosis: diagnose.Diagnosis{
+				Status: diagnose.DiagnosisWarning,
+				Name:   "Diagnosis Name",
+				Diagnosis: "Blocking firewall rules were found:\n" +
+					"snmp_traps, netflow (ipfix) packets might be blocked because destination port 9162 is blocked for protocol UDP\n" +
+					"netflow (sflow5) packets might be blocked because destination port 1234 is blocked for protocol UDP\n",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			diagnosis := buildBlockedPortsDiagnosis(tt.diagnosisName, tt.forProtocol, tt.blockedPorts)
+			assert.Equal(t, tt.expectedDiagnosis, diagnosis)
+		})
+	}
+}
