73be1c3c7318c80f3a6fc56879bf663464c52acd
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-04-29 10:00:02 +0200

    Add macOS Traceroute impl

--- pkg/networkpath/traceroute/traceroute_unix.go
+++ pkg/networkpath/traceroute/traceroute_unix.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build linux
+//go:build unix
 
 package traceroute
 
@@ -19,30 +19,29 @@ import (
 )
 
 const (
-	clientID = "traceroute-agent-linux"
+	clientID = "traceroute-agent-unix"
 )
 
-// LinuxTraceroute defines a structure for
-// running traceroute from an agent running
-// on Linux
-type LinuxTraceroute struct {
+// UnixTraceroute defines a structure for
+// running traceroute from an agent running on Unix
+type UnixTraceroute struct {
 	cfg            config.Config
 	sysprobeClient *http.Client
 }
 
-// New creates a new instance of LinuxTraceroute
+// New creates a new instance of UnixTraceroute
 // based on an input configuration
-func New(cfg config.Config, _ telemetry.Component) (*LinuxTraceroute, error) {
+func New(cfg config.Config, _ telemetry.Component) (*UnixTraceroute, error) {
 	log.Debugf("Creating new traceroute with config: %+v", cfg)
-	return &LinuxTraceroute{
+	return &UnixTraceroute{
 		cfg:            cfg,
 		sysprobeClient: getSysProbeClient(),
 	}, nil
 }
 
 // Run executes a traceroute
-func (l *LinuxTraceroute) Run(_ context.Context) (payload.NetworkPath, error) {
-	resp, err := getTraceroute(l.sysprobeClient, clientID, l.cfg.DestHostname, l.cfg.DestPort, l.cfg.Protocol, l.cfg.TCPMethod, l.cfg.MaxTTL, l.cfg.Timeout)
+func (l *UnixTraceroute) Run(_ context.Context) (payload.NetworkPath, error) {
+	resp, err := getTraceroute(l.sysprobeClient, clientID, l.cfg.DestHostname, l.cfg.DestPort, l.cfg.Protocol, l.cfg.MaxTTL, l.cfg.Timeout)
 	if err != nil {
 		return payload.NetworkPath{}, err
 	}
