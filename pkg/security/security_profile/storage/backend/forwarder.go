0edf3ac8955716c08f8176dace0925c8950ba889
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-04-30 16:24:06 +0200

    add new backend forwarder

---
+++ pkg/security/security_profile/storage/backend/forwarder.go
@@ -0,0 +1,182 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build linux
+
+// Package backend holds files related to forwarder backends for security profiles
+package backend
+
+import (
+	"bytes"
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"net/textproto"
+
+	"go.uber.org/atomic"
+
+	logsconfig "github.com/DataDog/datadog-agent/comp/logs/agent/config"
+	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
+	"github.com/DataDog/datadog-agent/pkg/security/config"
+	cgroupModel "github.com/DataDog/datadog-agent/pkg/security/resolvers/cgroup/model"
+	"github.com/DataDog/datadog-agent/pkg/security/seclog"
+	"github.com/DataDog/datadog-agent/pkg/security/utils"
+	ddhttputil "github.com/DataDog/datadog-agent/pkg/util/http"
+)
+
+type tooLargeEntityStatsEntry struct {
+	storageFormat config.StorageFormat
+	compression   bool
+}
+
+// ActivityDumpRemoteBackend is a remote backend that forwards dumps to the backend
+type ActivityDumpRemoteBackend struct {
+	urls             []string
+	apiKeys          []string
+	tooLargeEntities map[tooLargeEntityStatsEntry]*atomic.Uint64
+
+	client *http.Client
+}
+
+// NewActivityDumpRemoteBackend returns a new ActivityDumpRemoteBackend
+func NewActivityDumpRemoteBackend() (*ActivityDumpRemoteBackend, error) {
+	backend := &ActivityDumpRemoteBackend{
+		tooLargeEntities: make(map[tooLargeEntityStatsEntry]*atomic.Uint64),
+		client: &http.Client{
+			Transport: ddhttputil.CreateHTTPTransport(pkgconfigsetup.Datadog()),
+		},
+	}
+
+	for _, format := range config.AllStorageFormats() {
+		for _, compression := range []bool{true, false} {
+			entry := tooLargeEntityStatsEntry{
+				storageFormat: format,
+				compression:   compression,
+			}
+			backend.tooLargeEntities[entry] = atomic.NewUint64(0)
+		}
+	}
+
+	endpoints, err := config.ActivityDumpRemoteStorageEndpoints("cws-intake.", "secdump", logsconfig.DefaultIntakeProtocol, "cloud-workload-security")
+	if err != nil {
+		return nil, fmt.Errorf("couldn't generate storage endpoints: %w", err)
+	}
+	for _, endpoint := range endpoints.GetReliableEndpoints() {
+		backend.urls = append(backend.urls, utils.GetEndpointURL(endpoint, "api/v2/secdump"))
+		// TODO - runtime API key refresh: Storing the API key like this will no longer be valid once the
+		// security agent support API key refresh at runtime.
+		backend.apiKeys = append(backend.apiKeys, endpoint.GetAPIKey())
+	}
+
+	return backend, nil
+}
+
+func writeEventMetadata(writer *multipart.Writer, header []byte) error {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition", `form-data; name="event"; filename=""`)
+	h.Set("Content-Type", "application/json")
+
+	dataWriter, err := writer.CreatePart(h)
+	if err != nil {
+		return fmt.Errorf("couldn't create event metadata part: %w", err)
+	}
+
+	// write metadata
+	if _, err = dataWriter.Write(header); err != nil {
+		return fmt.Errorf("couldn't write event metadata part: %w", err)
+	}
+	return err
+}
+
+func writeDump(writer *multipart.Writer, raw []byte) error {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition", fmt.Sprintf(`form-data; name="dump"; filename="dump.protobuf"`)) // TODO: maybe make the extension parametrable
+	h.Set("Content-Type", "application/json")
+
+	dataWriter, err := writer.CreatePart(h)
+	if err != nil {
+		return fmt.Errorf("couldn't create dump part: %w", err)
+	}
+	if _, err = dataWriter.Write(raw); err != nil {
+		return fmt.Errorf("couldn't write dump part: %w", err)
+	}
+	return nil
+}
+
+func buildBody(header []byte, data []byte) (*multipart.Writer, *bytes.Buffer, error) {
+	body := bytes.NewBuffer(nil)
+	var multipartWriter *multipart.Writer
+
+	// TODO: handle compression
+	compression := true
+
+	if compression {
+		compressor := gzip.NewWriter(body)
+		defer compressor.Close()
+		multipartWriter = multipart.NewWriter(compressor)
+	} else {
+		multipartWriter = multipart.NewWriter(body)
+	}
+	defer multipartWriter.Close()
+
+	if err := writeEventMetadata(multipartWriter, header); err != nil {
+		return nil, nil, err
+	}
+
+	if err := writeDump(multipartWriter, data); err != nil {
+		return nil, nil, err
+	}
+	return multipartWriter, body, nil
+}
+
+func (backend *ActivityDumpRemoteBackend) sendToEndpoint(url string, apiKey string, writer *multipart.Writer, body *bytes.Buffer) error {
+	r, err := http.NewRequest("POST", url, bytes.NewBuffer(body.Bytes()))
+	if err != nil {
+		return err
+	}
+	r.Header.Add("Content-Type", writer.FormDataContentType())
+	r.Header.Add("dd-api-key", apiKey)
+
+	if /*request.Compression*/ true {
+		r.Header.Set("Content-Encoding", "gzip")
+	}
+
+	resp, err := backend.client.Do(r)
+	if err != nil {
+		return err
+	}
+	_ = resp.Body.Close()
+	if resp.StatusCode == http.StatusAccepted {
+		return nil
+	}
+	if resp.StatusCode == http.StatusRequestEntityTooLarge {
+		entry := tooLargeEntityStatsEntry{ // TODO: fix that
+			storageFormat: config.Protobuf,
+			compression:   true,
+		}
+		backend.tooLargeEntities[entry].Inc()
+	}
+	return errors.New(resp.Status)
+}
+
+// HandleActivityDump sends the activity dump to the remote backend
+func (backend *ActivityDumpRemoteBackend) HandleActivityDump(selector *cgroupModel.WorkloadSelector, header []byte, data []byte) error {
+	writer, body, err := buildBody(header, data)
+	if err != nil {
+		return fmt.Errorf("couldn't build request: %w", err)
+	}
+
+	for i, url := range backend.urls {
+		if err := backend.sendToEndpoint(url, backend.apiKeys[i], writer, body); err != nil {
+			seclog.Warnf("couldn't sent activity dump to [%s, body size: %d, dump size: %d]: %v", url, body.Len(), len(data), err)
+		} else {
+			seclog.Infof("[%s] file for activity dump [%s] successfully sent to [%s]", config.Protobuf, selector, url)
+		}
+	}
+
+	return nil
+}
