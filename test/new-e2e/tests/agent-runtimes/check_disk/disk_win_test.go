f497278b7d3f79c90a5b65b6796dc920d3ed3d50
José Manuel Almaza Ramiro <josemanuel.almaza@datadoghq.com>
2025-05-05 15:00:51 +0200

    Remove flaky mark from TestWindowsDiskSuite (#36604)

--- test/new-e2e/tests/agent-runtimes/check_disk/disk_win_test.go
+++ test/new-e2e/tests/agent-runtimes/check_disk/disk_win_test.go
@@ -10,7 +10,6 @@ import (
 
 	e2eos "github.com/DataDog/test-infra-definitions/components/os"
 
-	"github.com/DataDog/datadog-agent/pkg/util/testutil/flake"
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
 )
 
@@ -19,7 +18,6 @@ type windowsStatusSuite struct {
 }
 
 func TestWindowsDiskSuite(t *testing.T) {
-	flake.Mark(t)
 	t.Parallel()
 	suite := &windowsStatusSuite{baseCheckSuite{descriptor: e2eos.WindowsDefault, agentOptions: getAgentOptions()}}
 	e2e.Run(t, suite, suite.getSuiteOptions()...)
