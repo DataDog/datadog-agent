9f52184614be3812e764123a1e47fefee9c003c8
usamasaqib <usamasaqib.96@live.com>
2025-05-05 14:40:21 +0200

    move kallsyms iterator to ksyms.go

--- pkg/ebpf/ksyms.go
+++ pkg/ebpf/ksyms.go
@@ -17,7 +17,11 @@ import (
 	"strconv"
 	"sync"
 
+	"github.com/DataDog/datadog-agent/pkg/ebpf/bytecode"
 	"github.com/DataDog/datadog-agent/pkg/process/util"
+	manager "github.com/DataDog/ebpf-manager"
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
 )
 
 type existCache struct {
@@ -36,6 +40,64 @@ func newExistCache(path string) *existCache {
 const invalidAddress = 0xffffffffffffffff
 
 var funcCache = newExistCache("/proc/kallsyms")
+
+type ksymIterProgram struct {
+	BpfIteratorDumpKsyms *ebpf.Program `ebpf:"bpf_iter__dump_ksyms"`
+}
+
+// GetKernelSymbolsAddressesWithKallsymsIterator will use a bpf iterator program to lookup kernel symbols
+// This is useful when due to some lockdown /proc/kallsyms will not return the addresses of kernel symbols
+// An example of this is when we are running in a container without CAP_SYSLOG which is needed to ready /proc/kallsyms
+func GetKernelSymbolsAddressesWithKallsymsIterator(kernelAddresses ...string) (map[string]uint64, error) {
+	var prog ksymIterProgram
+
+	if err := LoadCOREAsset("ksyms_iter.o", func(bc bytecode.AssetReader, managerOptions manager.Options) error {
+		collectionSpec, err := ebpf.LoadCollectionSpecFromReader(bc)
+		if err != nil {
+			return fmt.Errorf("failed to load collection spec: %w", err)
+		}
+
+		opts := ebpf.CollectionOptions{
+			Programs: ebpf.ProgramOptions{
+				LogLevel:    ebpf.LogLevelBranch,
+				KernelTypes: managerOptions.VerifierOptions.Programs.KernelTypes,
+			},
+		}
+
+		if err := collectionSpec.LoadAndAssign(&prog, &opts); err != nil {
+			var ve *ebpf.VerifierError
+			if errors.As(err, &ve) {
+				return fmt.Errorf("verfier error loading collection: %s\n%+v", err, ve)
+			}
+			return fmt.Errorf("failed to load objects: %w", err)
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	iter, err := link.AttachIter(link.IterOptions{
+		Program: prog.BpfIteratorDumpKsyms,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to attach bpf iterator: %w", err)
+	}
+	defer iter.Close()
+
+	ksymsReader, err := iter.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer ksymsReader.Close()
+
+	addrs, err := GetKernelSymbolsAddressesNoCache(ksymsReader, kernelAddresses...)
+	if err != nil {
+		return nil, err
+	}
+
+	return addrs, nil
+}
 
 // GetKernelSymbolsAddressesNoCache returns the requested kernel symbols and addresses without using the cache
 // It expects a reader from which to read the kernel symbols.
