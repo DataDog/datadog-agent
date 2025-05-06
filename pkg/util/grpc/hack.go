38466e323a8b909e589b0d40ef0a9a27cabe89db
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-05-06 13:51:39 +0200

    add more information/comment about pkg/util/grpc/hack.go

--- pkg/util/grpc/hack.go
+++ pkg/util/grpc/hack.go
@@ -7,4 +7,9 @@
 
 package grpc
 
+// In order to fix the ambuiguity between old versions of cloud.google.com/go
+// and recent versions of cloud.google.com/go/compute/metadate providing the same
+// package, we force a dependency on a newer version of cloud.google.com/go here.
+// Thanks to the build tag, it will not be included in the final binary.
+
 import _ "cloud.google.com/go/compute/apiv1"
