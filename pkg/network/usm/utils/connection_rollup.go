6d5516247ff81d0a96c3379518a289c974f29ce9
Guy Arbitman <guy20495@gmail.com>
2025-05-08 13:09:24 +0300

    Wip

--- pkg/network/usm/utils/connection_rollup.go
+++ pkg/network/usm/utils/connection_rollup.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build (windows && npm) || linux_bpf
+//go:build (windows && npm) || (linux_bpf && linux)
 
 // Package utils contains common code shared across the USM codebase
 package utils
