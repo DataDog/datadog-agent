686ab4d2f10431dfc4ab6edd63a5190bd119bbce
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-05-09 16:32:21 +0200

    [fix-macos-secrets] wip

--- tools/ci/fetch_secret.sh
+++ tools/ci/fetch_secret.sh
@@ -31,5 +31,5 @@ while [[ $retry_count -lt $max_retries ]]; do
     sleep $((2**retry_count))
 done
 
-echo "Failed to retrieve parameter after $max_retries retries"
+echo "Failed to retrieve parameter after $max_retries retries" >&2
 exit 1
