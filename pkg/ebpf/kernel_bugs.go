fee87572c237dbe25165a68a9e5a63b6ab5de7ef
usamasaqib <usamasaqib.96@live.com>
2025-05-09 14:23:22 +0200

    remove linux_bpf tag

--- pkg/ebpf/kernel_bugs.go
+++ pkg/ebpf/kernel_bugs.go
@@ -3,8 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build linux_bpf
-
 package ebpf
 
 // HasTasksRCUExitLockSymbol returns true if the tasks_rcu_exit_srcu symbol is found in the kernel symbols.
