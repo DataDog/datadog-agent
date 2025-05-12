3387bf1d594ca8d7d233de8dd6291bd20b47960a
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2025-05-12 18:19:38 +0200

    fix: tests

--- comp/collector/collector/collectorimpl/internal/middleware/check_wrapper.go
+++ comp/collector/collector/collectorimpl/internal/middleware/check_wrapper.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2022-present Datadog, Inc.
 
+// Package middleware contains a check wrapper helper
 package middleware
 
 import (
