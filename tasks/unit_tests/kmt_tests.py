fa11a8bfaa1852551c73fad8f223060ee94c34d2
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-05-14 15:28:56 +0200

    Add extra tests

--- tasks/unit_tests/kmt_tests.py
+++ tasks/unit_tests/kmt_tests.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
 
 
 class TestVmconfig(unittest.TestCase):
-    def test_all_list_possible_items_map_to_existing_platforms(self):
+    def test_all_list_possible__items_map_to_existing_platforms(self):
         possible = vmconfig.list_possible()
         plats = platforms.get_platforms()
 
@@ -30,3 +30,17 @@ class TestVmconfig(unittest.TestCase):
                 plats[cast("KMTArchName", arch)],
                 f"{name} maps to {version} which is not a valid version for architecture {arch}",
             )
+
+    def test_normalize_vm_def__returns_expected_values(self):
+        possible = vmconfig.list_possible()
+
+        self.assertEqual(
+            vmconfig.normalize_vm_def(possible, "ubuntu22-arm64-distro"), ("distro", "ubuntu_22.04", "arm64")
+        )
+        self.assertEqual(
+            vmconfig.normalize_vm_def(possible, "ubuntu22-x86_64-distro"), ("distro", "ubuntu_22.04", "x86_64")
+        )
+        self.assertEqual(vmconfig.normalize_vm_def(possible, "focal-arm64-distro"), ("distro", "ubuntu_20.04", "arm64"))
+        self.assertEqual(
+            vmconfig.normalize_vm_def(possible, "focal-x86_64-distro"), ("distro", "ubuntu_20.04", "x86_64")
+        )
