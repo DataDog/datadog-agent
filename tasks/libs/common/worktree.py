4c9b1d2c79d0eca7a8135e5e2cc954519b433a1d
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-05-16 10:16:20 +0200

    [fix-worktree-pull] redo again

--- tasks/libs/common/worktree.py
+++ tasks/libs/common/worktree.py
@@ -70,7 +70,8 @@ def init_env(ctx, branch: str | None = None, commit: str | None = None):
                         ctx.run(f"git -C '{WORKTREE_DIRECTORY}' fetch", hide=True)
 
         if not os.environ.get("AGENT_WORKTREE_NO_PULL"):
-            ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull origin '{branch}'", hide=True)
+            ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull", hide=True)
+            # ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull origin '{branch}'", hide=True)
 
     if commit:
         if not os.environ.get("AGENT_WORKTREE_NO_PULL"):
