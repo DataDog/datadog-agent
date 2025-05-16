214c2706a1c6c98a7666ab0fc963738eeb48b825
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-05-16 10:37:00 +0200

    [fix-worktree-pull] Updated other pulls/fetches

--- tasks/libs/common/worktree.py
+++ tasks/libs/common/worktree.py
@@ -67,7 +67,7 @@ def init_env(ctx, branch: str | None = None, commit: str | None = None):
                             f'{color_message("Warning", Color.ORANGE)}: Git branch not found in the local worktree folder, fetching repository',
                             file=sys.stderr,
                         )
-                        ctx.run(f"git -C '{WORKTREE_DIRECTORY}' fetch", hide=True)
+                        ctx.run(f"git -C '{WORKTREE_DIRECTORY}' fetch --set-upstream origin", hide=True)
 
         if not os.environ.get("AGENT_WORKTREE_NO_PULL"):
             ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull --set-upstream origin '{branch}'", hide=True)
