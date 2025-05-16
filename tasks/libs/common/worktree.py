98f150ceb67c91e780d218579202690c8f674000
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-05-16 10:31:01 +0200

    [fix-worktree-pull] only g branch set upstream

--- tasks/libs/common/worktree.py
+++ tasks/libs/common/worktree.py
@@ -45,6 +45,7 @@ def init_env(ctx, branch: str | None = None, commit: str | None = None):
     ctx.run(f"cp {LOCAL_DIRECTORY}/.git/config {WORKTREE_DIRECTORY}/.git/config", hide=True)
     # Be sure the target branch is present locally and set up to track the remote branch
     ctx.run(f"git -C '{WORKTREE_DIRECTORY}' branch {branch or 'main'} origin/{branch or 'main'} || true", hide=True)
+    ctx.run(f"git -C '{WORKTREE_DIRECTORY}' branch {branch or 'main'} --set-upstream-to origin/{branch or 'main'} || true", hide=True)
     # If the state is not clean, clean it
     if ctx.run(f"git -C '{WORKTREE_DIRECTORY}' status --porcelain", hide=True).stdout.strip():
         print(f'{color_message("Info", Color.BLUE)}: Cleaning worktree directory', file=sys.stderr)
@@ -71,11 +72,11 @@ def init_env(ctx, branch: str | None = None, commit: str | None = None):
 
         if not os.environ.get("AGENT_WORKTREE_NO_PULL"):
             # ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull", hide=True)
-            ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull --set-upstream origin '{branch}'", hide=True)
+            ctx.run(f"git -C '{WORKTREE_DIRECTORY}' pull '{branch}'", hide=True)
 
     if commit:
         if not os.environ.get("AGENT_WORKTREE_NO_PULL"):
-            ctx.run(f"git -C '{WORKTREE_DIRECTORY}' fetch --set-upstream origin", hide=True)
+            ctx.run(f"git -C '{WORKTREE_DIRECTORY}' fetch", hide=True)
 
         ctx.run(f"git -C '{WORKTREE_DIRECTORY}' checkout '{commit}'", hide=True)
 
