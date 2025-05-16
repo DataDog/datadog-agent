2c408c39798e16135ad70864f709084c710702cd
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2025-05-16 14:29:06 +0200

    discovery: module: add pids param

--- pkg/collector/corechecks/servicediscovery/module/params.go
+++ pkg/collector/corechecks/servicediscovery/module/params.go
@@ -6,18 +6,23 @@
 package module
 
 import (
+	"fmt"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const (
 	heartbeatParam = "heartbeat"
 	heartbeatTime  = 15 * time.Minute
+
+	pidsParam = "pids"
 )
 
 type params struct {
 	heartbeatTime time.Duration
+	pids          []int
 }
 
 func defaultParams() params {
@@ -28,6 +33,14 @@ func defaultParams() params {
 
 func (params params) updateQuery(query url.Values) {
 	query.Set(heartbeatParam, strconv.Itoa(int(params.heartbeatTime.Seconds())))
+
+	if len(params.pids) > 0 {
+		pidsStr := make([]string, len(params.pids))
+		for i, pid := range params.pids {
+			pidsStr[i] = strconv.Itoa(pid)
+		}
+		query.Set(pidsParam, strings.Join(pidsStr, ","))
+	}
 }
 
 func parseDuration(raw string) (time.Duration, error) {
@@ -39,17 +52,55 @@ func parseDuration(raw string) (time.Duration, error) {
 	return time.Duration(val) * time.Second, err
 }
 
+func parseHeartbeat(query url.Values) (time.Duration, error) {
+	raw := query.Get(heartbeatParam)
+	if raw == "" {
+		return heartbeatTime, nil
+	}
+
+	heartbeat, err := parseDuration(raw)
+	if err != nil {
+		return 0, err
+	}
+
+	return heartbeat, nil
+}
+
+func parsePids(query url.Values) ([]int, error) {
+	if !query.Has(pidsParam) {
+		return nil, nil
+	}
+
+	pidsRaw := query.Get(pidsParam)
+
+	pidsStr := strings.Split(pidsRaw, ",")
+	pids := make([]int, 0, len(pidsStr))
+	for _, raw := range pidsStr {
+		pid, err := strconv.Atoi(raw)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse pid %q: %w", raw, err)
+		}
+
+		pids = append(pids, pid)
+	}
+
+	return pids, nil
+}
+
 func parseParams(query url.Values) (params, error) {
 	params := defaultParams()
 
-	raw := query.Get(heartbeatParam)
-	if raw != "" {
-		heartbeat, err := parseDuration(raw)
-		if err != nil {
-			return params, err
-		}
-		params.heartbeatTime = heartbeat
+	heartbeat, err := parseHeartbeat(query)
+	if err != nil {
+		return params, err
 	}
+	params.heartbeatTime = heartbeat
+
+	pids, err := parsePids(query)
+	if err != nil {
+		return params, err
+	}
+	params.pids = pids
 
 	return params, nil
 }
