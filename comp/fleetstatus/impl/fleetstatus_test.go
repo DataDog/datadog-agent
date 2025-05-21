7578d70476544ce9640dc43d130702046b14eb7d
Sandra Josy <sandra.josy@datadoghq.com>
2025-05-21 10:08:05 +0200

    Fix templates

--- comp/fleetstatus/impl/fleetstatus_test.go
+++ comp/fleetstatus/impl/fleetstatus_test.go
@@ -7,58 +7,82 @@ package fleetstatusimpl
 
 import (
 	"bytes"
+	"expvar"
 	"testing"
 
 	"github.com/DataDog/datadog-agent/comp/core/config"
+	installerexec "github.com/DataDog/datadog-agent/comp/updater/installerexec/def"
+	installerexecmock "github.com/DataDog/datadog-agent/comp/updater/installerexec/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestFleetStatus(t *testing.T) {
+	rcMapStatus := expvar.NewMap("remoteConfigStatus")
+	rcEnabled    := expvar.String{}
+	rcEnabled.Set("enabled")
+	rcMapStatus.Set("enabled", &rcEnabled)
+
 	tests := []struct {
 		name                string
 		remoteUpdatesConfig bool
-		expectedStatus      map[string]interface{}
+		installerRunning    bool
+		fleetAutomationEnabled bool
 	}{
 		{
-			name:                "remote updates enabled",
+			name:                "fleet enabled",
 			remoteUpdatesConfig: true,
-			expectedStatus: map[string]interface{}{
-				"fleetAutomationStatus": map[string]interface{}{
-					"remoteManagementEnabled": true,
-				},
-			},
+			installerRunning:    true,
+			fleetAutomationEnabled: true,
 		},
 		{
 			name:                "remote updates disabled",
 			remoteUpdatesConfig: false,
-			expectedStatus: map[string]interface{}{
-				"fleetAutomationStatus": map[string]interface{}{
-					"remoteManagementEnabled": false,
-				},
-			},
+			installerRunning:    true,
+			fleetAutomationEnabled: false,
+		},
+		{
+			name:                "instaler not running",
+			remoteUpdatesConfig: true,
+			installerRunning:    false,
+			fleetAutomationEnabled: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			expectedStatus:= map[string]interface{}{
+				"fleetAutomationStatus": map[string]interface{}{
+					"remoteManagementEnabled": tt.remoteUpdatesConfig,
+					"remoteConfigEnabled":     true,
+					"installerRunning":        tt.installerRunning,
+					"fleetAutomationEnabled": tt.installerRunning && tt.remoteUpdatesConfig,
+				},
+			}
+
 			cfg := config.NewMock(t)
 			cfg.SetWithoutSource("remote_updates", tt.remoteUpdatesConfig)
 
+			var installerExec installerexec.Component
+			if tt.installerRunning {
+				installerExec = installerexecmock.Mock(t)
+			}
+
 			provides := NewComponent(Requires{
 				Config: cfg,
+				InstallerExec: installerExec,
 			})
 			statusProvider := provides.Status.Provider
 
 			stats := make(map[string]interface{})
 			err := statusProvider.JSON(false, stats)
 			require.NoError(t, err)
-			assert.Equal(t, tt.expectedStatus, stats)
+			assert.Equal(t, expectedStatus, stats)
 
 			buffer := new(bytes.Buffer)
 			err = statusProvider.Text(false, buffer)
 			require.NoError(t, err)
-			if tt.remoteUpdatesConfig {
+			if tt.fleetAutomationEnabled {
 				assert.Contains(t, buffer.String(), "Fleet Management is enabled")
 			} else {
 				assert.Contains(t, buffer.String(), "Fleet Management is disabled")
@@ -67,7 +91,7 @@ func TestFleetStatus(t *testing.T) {
 
 			err = statusProvider.HTML(false, buffer)
 			require.NoError(t, err)
-			if tt.remoteUpdatesConfig {
+			if tt.fleetAutomationEnabled {
 				assert.Contains(t, buffer.String(), "Fleet Management is enabled")
 			} else {
 				assert.Contains(t, buffer.String(), "Fleet Management is disabled")
