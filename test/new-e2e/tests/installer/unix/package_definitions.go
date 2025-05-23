46dddbdfb8fc51463e3083f81b47e32b2bdc1260
Arthur Bellal <arthur.bellal@datadoghq.com>
2025-05-23 20:07:53 +0200

    pin installer to latest (still used in the ssi install)

--- test/new-e2e/tests/installer/unix/package_definitions.go
+++ test/new-e2e/tests/installer/unix/package_definitions.go
@@ -65,7 +65,7 @@ func WithAlias(alias string) PackageOption {
 
 // PackagesConfig is the list of known packages configuration for testing
 var PackagesConfig = []TestPackageConfig{
-	{Name: "datadog-installer", Version: fmt.Sprintf("pipeline-%v", os.Getenv("E2E_PIPELINE_ID")), Registry: "installtesting.datad0g.com.internal.dda-testing.com"},
+	{Name: "datadog-installer", Version: "latest"},
 	{Name: "datadog-agent", Alias: "agent-package", Version: fmt.Sprintf("pipeline-%v", os.Getenv("E2E_PIPELINE_ID")), Registry: "installtesting.datad0g.com.internal.dda-testing.com"},
 	{Name: "datadog-apm-inject", Version: "latest"},
 	{Name: "datadog-apm-library-java", Version: "latest"},
