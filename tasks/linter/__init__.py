f2e0cee0061f1922237e73b5af3e4d659d30f7f4
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-05-23 11:21:00 +0200

    refactor(linter): Move go linters to new file

--- tasks/linter/__init__.py
+++ tasks/linter/__init__.py
@@ -1,5 +1,6 @@
 """Module regrouping all invoke tasks used for linting the `datadog-agent` repo"""
 
+from .go import go, update_go  # noqa: F401
 from .misc import copyrights, filenames  # noqa: F401
 from .old import *  # noqa: F403
 from .python import python  # noqa: F401
