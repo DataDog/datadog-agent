29e05dcf0ee3078121557def1175e88f253a8814
Pythyu <antoine.zellmeyer@datadoghq.com>
2025-05-28 12:03:49 +0200

    WIP

--- tasks/static_quality_gates/lib/docker_agent_lib.py
+++ tasks/static_quality_gates/lib/docker_agent_lib.py
@@ -114,3 +114,55 @@ def generic_docker_agent_quality_gate(gate_name, arch, jmx=False, flavor="agent"
     image_on_wire_size, image_on_disk_size = get_image_url_size(ctx, metric_handler, gate_name, url)
     # Check if the docker image is within acceptable bounds
     check_image_size(image_on_wire_size, image_on_disk_size, max_on_wire_size, max_on_disk_size)
+
+
+def get_images_content_diff(ctx, url_1, url_2):
+    ctx.run("mkdir image1 image2")
+    # Pull images locally to get on disk size
+    ctx.run(f"crane pull {url_1} image1/output.tar")
+    ctx.run(f"crane pull {url_2} image2/output.tar")
+    # The downloaded image contains some metadata files and another tar.gz file.
+    for src_folder in ["image1", "image2"]:
+        ctx.run(f"cd {src_folder} && tar -xf output.tar")
+        image_content = ctx.run("tar -tvf " + f"{src_folder}/output.tar" +" | awk -F' ' '{print $3; print $6}'", hide=True).stdout.splitlines()
+        image_tar_gz = []
+        print(f"On disk content for {src_folder}:")
+        for k, line in enumerate(image_content):
+            if k % 2 == 0:
+                if "tar.gz" in image_content[k + 1]:
+                    image_tar_gz.append(image_content[k + 1])
+            else:
+                print(f"  - {line}")
+        if image_tar_gz:
+            for image in image_tar_gz:
+                ctx.run(f"tar -xf {image} --to-stdout | wc -c", hide=True).stdout)
+        else:
+            print(color_message("[WARN] No tar.gz file found inside of the image", "orange"), file=sys.stderr)
+
+
+def generic_debug_docker_agent_quality_gate(gate_name, arch, jmx=False, flavor="agent", image_suffix="", **kwargs):
+    arguments = argument_extractor(
+        kwargs,
+        ctx=None,
+        nightly=None,
+    )
+    ctx = arguments.ctx
+    is_nightly_run = arguments.nightly
+
+    pipeline_id = os.environ["CI_PIPELINE_ID"]
+    commit_sha = os.environ["CI_COMMIT_SHORT_SHA"]
+    if not pipeline_id or not commit_sha:
+        raise Exception(
+            color_message(
+                "This gate needs to be ran from the CI environment. (Missing CI_PIPELINE_ID, CI_COMMIT_SHORT_SHA)",
+                "orange",
+            )
+        )
+    image_suffixes = "-7" if flavor == "agent" else "" + "-jmx" if jmx else "" + image_suffix if image_suffix else ""
+    if flavor != "dogstatsd" and is_nightly_run:
+        flavor += "-nightly"
+    url = f"registry.ddbuild.io/ci/datadog-agent/{flavor}:v{pipeline_id}-{commit_sha}{image_suffixes}-{arch}"
+    # Fetch the on wire and on disk size of the image from the url
+    image_on_wire_size, image_on_disk_size = get_image_url_size(ctx, metric_handler, gate_name, url)
+    # Check if the docker image is within acceptable bounds
+    check_image_size(image_on_wire_size, image_on_disk_size, max_on_wire_size, max_on_disk_size)
