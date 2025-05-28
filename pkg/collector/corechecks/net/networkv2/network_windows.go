0dc660fbf0db42026d79732b5d420ba462fe2921
Jeremy Hanna <jeremy.hanna@datadoghq.com>
2025-05-27 23:03:07 -0400

    Fix return types

--- pkg/collector/corechecks/net/networkv2/network_windows.go
+++ pkg/collector/corechecks/net/networkv2/network_windows.go
@@ -167,7 +167,7 @@ var (
 )
 
 // gopsutil does not call GetTcpStatisticsEx so we need to do so manually for these stats
-func getTcpStats(inet uint) (mibTcpStats, error) {
+func getTcpStats(inet uint) (*mibTcpStats, error) {
 	tcpStats := &mibTcpStats{}
 	r0, _, _ := syscall.Syscall(procGetTcpStatisticsEx.Addr(), 2, uintptr(unsafe.Pointer(tcpStats)), uintptr(inet), 0)
 	if r0 != 0 {
@@ -205,7 +205,7 @@ func submitTcpStats(sender sender.Sender) error {
 
 	// Create tcp metrics that are a sum of tcp4 and tcp6 metrics
 	tcpAllStats := &mibTcpStats{}
-	if tcp4Stats != nil && tcp6Stats != nil {
+	if reflect.ValueOf(tcp4Stats).IsZero() && reflect.ValueOf(tcp6Stats).IsZero() {
 		tcpAllStats = &mibTcpStats{
 			dwRtoAlgorithm: tcp4Stats.dwRtoAlgorithm + tcp6Stats.dwRtoAlgorithm,
 			dwRtoMin:       tcp4Stats.dwRtoMin + tcp6Stats.dwRtoMin,
@@ -228,13 +228,15 @@ func submitTcpStats(sender sender.Sender) error {
 	submitMetricsFromStruct(sender, "system.net.tcp.", tcpAllStats, tcpStatsMapping)
 	submitMetricsFromStruct(sender, "system.net.tcp4.", tcp4Stats, tcpStatsMapping)
 	submitMetricsFromStruct(sender, "system.net.tcp6.", tcp6Stats, tcpStatsMapping)
+
+	return nil
 }
 
 func submitMetricsFromStruct(sender sender.Sender, metricPrefix string, tcpStats *mibTcpStats, tcpStatsMapping map[string]string) {
 	sType := reflect.TypeOf(reflect.ValueOf(tcpStats))
-	for i := 0; i < sType.NumFields(); i++ {
+	for i := 0; i < sType.NumField(); i++ {
 		field := sType.Field(i)
-		metricName := metricPrefix + tcpMapping[field.Name()]
+		metricName := metricPrefix + tcpStatasMapping[field.Name()]
 		metricValue := field.Uint()
 		if strings.HasSuffix(metricName, ".connections") || strings.HasSuffix(".current_established") {
 			sender.Gauge(metricName, float64(metricValue), "", nil)
