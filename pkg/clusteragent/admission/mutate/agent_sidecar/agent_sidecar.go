96fa2207f022a166000e7a08080db02b2ddcf971
Justin Lesko <justin.lesko@datadoghq.com>
2025-05-30 16:11:12 -0400

    Fix env var formatting

--- pkg/clusteragent/admission/mutate/agent_sidecar/agent_sidecar.go
+++ pkg/clusteragent/admission/mutate/agent_sidecar/agent_sidecar.go
@@ -427,8 +427,7 @@ func (w *Webhook) getDefaultSidecarTemplate() *corev1.Container {
 			corev1.EnvVar{
 				Name:  "DD_LOGS_ENABLED",
 				Value: "true",
-			},
-			corev1.EnvVar{
+			}, corev1.EnvVar{
 				Name:  "DD_LOGS_CONFIG_K8S_CONTAINER_USE_KUBELET_API",
 				Value: "true",
 			}, corev1.EnvVar{
