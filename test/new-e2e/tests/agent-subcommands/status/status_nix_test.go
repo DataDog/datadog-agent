5a39ffb5694934abb07138f0062545d059bf516e
kacper-murzyn <kacper.murzyn@datadoghq.com>
2025-06-03 16:16:38 +0200

    Revert "[Backport 7.66.x] [AGENTRUN-310] Remove FIPS Proxy status to FIPS Agent status output (#37292)"
    This reverts commit 7e9bb45e336bcad07ac44e5e445111c3b4a74a1a.

--- test/new-e2e/tests/agent-subcommands/status/status_nix_test.go
+++ test/new-e2e/tests/agent-subcommands/status/status_nix_test.go
@@ -41,26 +41,18 @@ func (v *linuxStatusSuite) TestStatusHostname() {
 }
 
 func (v *linuxStatusSuite) TestFIPSProxyStatus() {
-	var shouldContains []string
-	var shouldNotContains []string
-
+	// Skip this test if the e2e pipeline is running with the FIPS Agent flavor because the FIPS proxy is not supported with the FIPS Agent.
 	if v.Env().Agent.FIPSEnabled {
-		// FIPS-enabled Agent should never display FIPS Proxy status
-		shouldContains = []string{"FIPS Mode: enabled"}
-		shouldNotContains = []string{"FIPS Mode: proxy", "FIPS proxy"}
-	} else {
-		// Non-FIPS Agent should print FIPS Proxy status
-		shouldContains = []string{"FIPS Mode: proxy", "FIPS proxy"}
+		v.T().Skip()
 	}
 
 	v.UpdateEnv(awshost.ProvisionerNoFakeIntake(awshost.WithAgentOptions(agentparams.WithAgentConfig("fips.enabled: true"))))
 
 	expectedSections := []expectedSection{
 		{
-			name:             `Agent \(.*\)`,
-			shouldBePresent:  true,
-			shouldContain:    shouldContains,
-			shouldNotContain: shouldNotContains,
+			name:            `Agent \(.*\)`,
+			shouldBePresent: true,
+			shouldContain:   []string{"FIPS Mode: proxy", "FIPS proxy"},
 		},
 	}
 
