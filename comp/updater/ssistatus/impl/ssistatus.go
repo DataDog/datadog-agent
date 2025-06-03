df87e5cf93203334af59a5170ce6c2214f297d0a
Baptiste Foy <baptiste.foy@datadoghq.com>
2025-06-03 09:55:20 +0200

    control 3

--- comp/updater/ssistatus/impl/ssistatus.go
+++ comp/updater/ssistatus/impl/ssistatus.go
@@ -10,6 +10,8 @@ import (
 	"context"
 	"time"
 
+	"github.com/fsnotify/fsnotify"
+
 	log "github.com/DataDog/datadog-agent/comp/core/log/def"
 	compdef "github.com/DataDog/datadog-agent/comp/def"
 	"github.com/DataDog/datadog-agent/comp/metadata/inventoryagent"
@@ -51,35 +53,62 @@ type ssiStatusComponent struct {
 	log            log.Component
 	iexec          installerexec.Component
 	stopCh         chan struct{}
+	fswatcher      *fsnotify.Watcher
 }
 
 func (c *ssiStatusComponent) Start(_ context.Context) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	c.fswatcher = watcher
 	c.stopCh = make(chan struct{})
 	go func() {
-		ticker := time.NewTicker(10 * time.Minute)
-		defer ticker.Stop()
 		for {
 			select {
-			case <-ticker.C:
-				ctx := context.Background()
-				ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
-				defer cancel()
-				// APM host-based auto injection (SSI)
-				autoInstrumentationEnabled, instrumentationModes, err := c.autoInstrumentationStatus(ctx)
-				if err != nil {
-					c.log.Warnf("could not check APM auto-instrumentation status: %s", err)
+			case event, ok := <-c.fswatcher.Events:
+				if !ok {
+					return
 				}
-				c.inventoryAgent.Set("feature_auto_instrumentation_enabled", autoInstrumentationEnabled)
-				c.inventoryAgent.Set("auto_instrumentation_modes", instrumentationModes)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					c.update()
+				}
+			case err, ok := <-c.fswatcher.Errors:
+				if !ok {
+					return
+				}
+				c.log.Errorf("Error watching file: %v", err)
 			case <-c.stopCh:
 				return
 			}
 		}
 	}()
+	c.update() // Initial update to set the initial state
+	files := []string{"/etc/ld.so.preload", "/etc/docker/daemon.json", "/opt/datadog-packages/datadog-apm-inject"}
+	for _, file := range files {
+		err := c.fswatcher.Add(file)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
+func (c *ssiStatusComponent) update() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	// APM host-based auto injection (SSI)
+	autoInstrumentationEnabled, instrumentationModes, err := c.autoInstrumentationStatus(ctx)
+	if err != nil {
+		c.log.Warnf("could not check APM auto-instrumentation status: %s", err)
+	}
+	c.inventoryAgent.Set("feature_auto_instrumentation_enabled", autoInstrumentationEnabled)
+	c.inventoryAgent.Set("auto_instrumentation_modes", instrumentationModes)
+}
+
 func (c *ssiStatusComponent) Stop(_ context.Context) error {
+	c.fswatcher.Close()
 	close(c.stopCh)
 	return nil
 }
