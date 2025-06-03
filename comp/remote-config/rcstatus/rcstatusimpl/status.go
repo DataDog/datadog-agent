31e873a09f74efdd040b6b329c2108ddfae8d444
Maxime Riaud <65339037+misteriaud@users.noreply.github.com>
2025-06-03 13:41:00 +0200

    Revert "[AGENTRUN-310] Remove FIPS Proxy status to FIPS Agent status output (…"
    This reverts commit 1db6dbee74f2a55312bdcaaf0f6c93813e067b5b.

--- comp/remote-config/rcstatus/rcstatusimpl/status.go
+++ comp/remote-config/rcstatus/rcstatusimpl/status.go
@@ -13,9 +13,7 @@ import (
 
 	"github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/comp/core/status"
-	"github.com/DataDog/datadog-agent/pkg/fips"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
-
 	"go.uber.org/fx"
 )
 
@@ -94,11 +92,10 @@ func (rc statusProvider) populateStatus(stats map[string]interface{}) {
 		remoteConfigStatusJSON := expvar.Get("remoteConfigStatus").String()
 		json.Unmarshal([]byte(remoteConfigStatusJSON), &status) //nolint:errcheck
 	} else {
-		isFipsAgent, _ := fips.Enabled()
 		if !rc.Config.GetBool("remote_configuration.enabled") {
 			status["disabledReason"] = "it is explicitly disabled in the agent configuration. (`remote_configuration.enabled: false`)"
-		} else if rc.Config.GetBool("fips.enabled") || isFipsAgent {
-			status["disabledReason"] = "it is not supported when FIPS is enabled. (FIPS Agent or `fips.enabled: true` with FIPS Proxy)"
+		} else if rc.Config.GetBool("fips.enabled") {
+			status["disabledReason"] = "it is not supported when FIPS is enabled. (`fips.enabled: true`)"
 		} else if rc.Config.GetString("site") == "ddog-gov.com" {
 			status["disabledReason"] = "it is not supported on GovCloud. (`site: \"ddog-gov.com\"`)"
 		}
@@ -109,8 +106,7 @@ func (rc statusProvider) populateStatus(stats map[string]interface{}) {
 
 func isRemoteConfigEnabled(conf config.Component) bool {
 	// Disable Remote Config for GovCloud
-	isFipsAgent, _ := fips.Enabled()
-	if conf.GetBool("fips.enabled") || isFipsAgent || conf.GetString("site") == "ddog-gov.com" {
+	if conf.GetBool("fips.enabled") || conf.GetString("site") == "ddog-gov.com" {
 		return false
 	}
 	return conf.GetBool("remote_configuration.enabled")
--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -2711,8 +2711,7 @@ func getObsPipelineURLForPrefix(datatype DataType, prefix string, config pkgconf
 // IsRemoteConfigEnabled returns true if Remote Configuration should be enabled
 func IsRemoteConfigEnabled(cfg pkgconfigmodel.Reader) bool {
 	// Disable Remote Config for GovCloud
-	isFipsAgent, _ := pkgfips.Enabled()
-	if cfg.GetBool("fips.enabled") || isFipsAgent || cfg.GetString("site") == "ddog-gov.com" {
+	if cfg.GetBool("fips.enabled") || cfg.GetString("site") == "ddog-gov.com" {
 		return false
 	}
 	return cfg.GetBool("remote_configuration.enabled")
