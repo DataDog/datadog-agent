36954de532d13a9804061744ef9d2cfd850a9cba
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-06-04 09:30:37 +0200

    winbuildscript: don't install dda if it's already available
    This ensures we won't use an unexpected version of dda in our CI before
    we bump the build images

--- tasks/winbuildscripts/sysprobe.ps1
+++ tasks/winbuildscripts/sysprobe.ps1
@@ -5,7 +5,11 @@ $Env:Python2_ROOT_DIR=$Env:TEST_EMBEDDED_PY2
 $Env:Python3_ROOT_DIR=$Env:TEST_EMBEDDED_PY3
 
 & ridk enable
-& $Env:Python3_ROOT_DIR\python.exe -m pip install dda
+& $Env:Python3_ROOT_DIR\python.exe -c "import dda"
+$err = $LASTEXITCODE
+if ($err -ne 0) {
+    & $Env:Python3_ROOT_DIR\python.exe -m pip install dda
+}
 & $Env:Python3_ROOT_DIR\python.exe -m dda self dep sync -f legacy-tasks
 
 $PROBE_BUILD_ROOT=(Get-Location).Path
