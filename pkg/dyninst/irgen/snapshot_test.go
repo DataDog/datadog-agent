d8091cc70bda9dcf73fc09ec305ac9e393b6f768
Piotr Bejda <piotr.bejda@datadoghq.com>
2025-06-06 15:49:27 -0400

    [dyninst/testprogs] Share test probes configs

--- pkg/dyninst/irgen/snapshot_test.go
+++ pkg/dyninst/irgen/snapshot_test.go
@@ -8,19 +8,14 @@
 package irgen_test
 
 import (
-	"encoding/json"
 	"flag"
 	"os"
 	"path"
 	"strconv"
-	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v3"
 
-	"github.com/DataDog/datadog-agent/pkg/dyninst/config"
 	"github.com/DataDog/datadog-agent/pkg/dyninst/irgen"
 	"github.com/DataDog/datadog-agent/pkg/dyninst/irprinter"
 	"github.com/DataDog/datadog-agent/pkg/dyninst/object"
@@ -36,35 +31,17 @@ var rewrite = flag.Bool("rewrite", rewriteFromEnv, "rewrite the test files")
 
 const snapshotDir = "testdata/snapshot"
 
-func TestSnapshotTesting(t *testing.T) {
-	files, err := os.ReadDir(snapshotDir)
-	require.NoError(t, err)
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-		if !strings.HasSuffix(file.Name(), ".yaml") {
-			continue
-		}
-		testFile := path.Join(snapshotDir, file.Name())
-		t.Run(file.Name(), func(t *testing.T) {
-			runFile(t, testFile)
-		})
-	}
-}
+var cases = []string{"simple"}
 
-func runFile(t *testing.T, path string) {
-	outDir := strings.TrimSuffix(path, ".yaml")
-	os.MkdirAll(outDir, 0755)
-	yamlFile, err := os.ReadFile(path)
-	require.NoError(t, err)
-	testFile, err := deserializeTestFile(yamlFile)
-	require.NoError(t, err)
+func TestSnapshotTesting(t *testing.T) {
 	cfgs := testprogs.GetCommonConfigs(t)
-	assert.NotEmpty(t, cfgs)
-	for _, cfg := range cfgs {
-		t.Run(cfg.String(), func(t *testing.T) {
-			runTest(t, cfg, outDir, testFile)
+	for _, caseName := range cases {
+		t.Run(caseName, func(t *testing.T) {
+			for _, cfg := range cfgs {
+				t.Run(cfg.String(), func(t *testing.T) {
+					runTest(t, cfg, caseName)
+				})
+			}
 		})
 	}
 }
@@ -72,24 +49,24 @@ func runFile(t *testing.T, path string) {
 func runTest(
 	t *testing.T,
 	cfg testprogs.Config,
-	outDir string,
-	testFile *testFile,
+	caseName string,
 ) {
-	binPath := testprogs.GetBinary(t, testFile.binary, cfg)
+	binPath := testprogs.GetBinary(t, caseName, cfg)
+	probesCfgs := testprogs.GetProbeCfgs(t, caseName)
 	elfFile, err := safeelf.Open(binPath)
 	require.NoError(t, err)
 	obj, err := object.NewElfObject(elfFile)
 	require.NoError(t, err)
 	defer func() { require.NoError(t, elfFile.Close()) }()
-	ir, err := irgen.GenerateIR(1, obj, testFile.probes)
+	ir, err := irgen.GenerateIR(1, obj, probesCfgs)
 	require.NoError(t, err)
 
 	marshaled, err := irprinter.PrintYAML(ir)
 	require.NoError(t, err)
 
-	outputFile := path.Join(outDir, cfg.String()+".yaml")
+	outputFile := path.Join(snapshotDir, caseName+"."+cfg.String()+".yaml")
 	if *rewrite {
-		tmpFile, err := os.CreateTemp(outDir, "ir.yaml")
+		tmpFile, err := os.CreateTemp(snapshotDir, "ir.yaml")
 		require.NoError(t, err)
 		name := tmpFile.Name()
 		defer func() { _ = os.Remove(name) }()
@@ -102,38 +79,4 @@ func runTest(
 		require.NoError(t, err)
 		require.Equal(t, string(expected), string(marshaled))
 	}
-}
-
-type probeYaml struct {
-	Binary string           `yaml:"binary"`
-	Probes []map[string]any `yaml:"probes"`
-}
-
-type testFile struct {
-	binary string
-	probes []config.Probe
-}
-
-func deserializeTestFile(input []byte) (*testFile, error) {
-	var probeYaml probeYaml
-	err := yaml.Unmarshal(input, &probeYaml)
-	if err != nil {
-		return nil, err
-	}
-	var probes []config.Probe
-	for _, probe := range probeYaml.Probes {
-		probeBytes, err := json.Marshal(probe)
-		if err != nil {
-			return nil, err
-		}
-		probe, err := config.UnmarshalProbe(probeBytes)
-		if err != nil {
-			return nil, err
-		}
-		probes = append(probes, probe)
-	}
-	return &testFile{
-		binary: probeYaml.Binary,
-		probes: probes,
-	}, nil
 }
