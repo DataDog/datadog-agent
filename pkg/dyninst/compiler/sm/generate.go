44790aa282753af0d8a40a42aaaa38b26b9e916b
piob-io <piotr.bejda@datadoghq.com>
2025-06-09 12:17:44 -0400

    [dyninst/irgen] Support inlined functions (#37736)

--- pkg/dyninst/compiler/sm/generate.go
+++ pkg/dyninst/compiler/sm/generate.go
@@ -438,6 +438,10 @@ func (g *generator) EncodeLocationOp(pc uint64, op *ir.LocationOp, ops []Op) ([]
 			return nil, err
 		}
 		layoutIdx := 0
+		if len(loclist.Pieces) == 0 {
+			// Variable has loclist entry for relevant PC range, but it is still unavailable.
+			break
+		}
 		for _, locPiece := range loclist.Pieces {
 			paddedOffset := layoutPieces[layoutIdx].PaddedOffset
 			nextLayoutIdx := layoutIdx
