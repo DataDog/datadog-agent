ac84213ad42c883075b240f30f64ca7c56af2211
Rémy Mathieu <remy.mathieu@datadoghq.com>
2025-06-10 10:23:43 +0200

    [AMCC-11] dogstatsd: additional metric control mechanism between the sampler and the serializer (#37692)

--- pkg/metrics/histogram.go
+++ pkg/metrics/histogram.go
@@ -51,7 +51,9 @@ var (
 	defaultPercentiles = []int(nil)
 )
 
-func parsePercentiles(percentiles []string) []int {
+// ParsePercentiles represents a string percentile in
+// an integer percentile (e.g. "0.95" -> 95, "0.85" -> 85
+func ParsePercentiles(percentiles []string) []int {
 	res := []int{}
 	for _, p := range percentiles {
 		i, err := strconv.ParseFloat(p, 64)
@@ -83,7 +85,7 @@ func NewHistogram(interval int64, config pkgconfigmodel.Config) *Histogram {
 		if err != nil {
 			log.Errorf("Could not Unmarshal histogram configuration: %s", err)
 		} else {
-			defaultPercentiles = parsePercentiles(c)
+			defaultPercentiles = ParsePercentiles(c)
 			sort.Ints(defaultPercentiles)
 		}
 	}
