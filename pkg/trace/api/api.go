a8cd4870bc199087d098655816210275b0c43360
Andrew Glaude <andrew.glaude@datadoghq.com>
2025-06-10 13:59:42 -0400

    move wg usage earlier

--- pkg/trace/api/api.go
+++ pkg/trace/api/api.go
@@ -579,6 +579,8 @@ func (r *HTTPReceiver) handleStats(w http.ResponseWriter, req *http.Request) {
 
 // handleTraces knows how to handle a bunch of traces
 func (r *HTTPReceiver) handleTraces(v Version, w http.ResponseWriter, req *http.Request) {
+	r.wg.Add(1)
+	defer r.wg.Done()
 	tracen, err := traceCount(req)
 	if err == errInvalidHeaderTraceCountValue {
 		log.Errorf("Failed to count traces: %s", err)
