c685017da587ab5ab4a71a89d5faf8729b188e41
Andrew Werner <andrew.werner@datadoghq.com>
2025-06-12 09:49:29 -0400

    dyninst/object: parse compression header to size buffer
    This commit adds manual parsing of section compression headers outside
    of the debug/elf library in order to compute the correct format, size,
    and file offset of the compressed data. This allows up to create a
    buffer of the correct size. This helps avoid allocating too much
    memory and improves the throughput meaningfully.
    
    A sad truth is that the debug/elf library *almost* gives us everything
    we'd need here to do this type of thing without having to reparse
    data structures, but it doesn't expose compressionFormat or
    compressionOffset.
    
    This acheives meaningfully beffer performance, but still much
    slower than rust:
    ```
    name                     old time/op    new time/op    delta
    LoadElfFile/cockroach-4     1.53s ± 4%     1.17s ± 1%  -23.38%  (p=0.000 n=19+17)
    
    name                     old speed      new speed      delta
    LoadElfFile/cockroach-4   154MB/s ± 5%   201MB/s ± 1%  +30.84%  (p=0.000 n=20+17)
    
    name                     old alloc/op   new alloc/op   delta
    LoadElfFile/cockroach-4    1.01GB ± 0%    0.25GB ± 0%  -75.31%  (p=0.000 n=20+18)
    
    name                     old allocs/op  new allocs/op  delta
    LoadElfFile/cockroach-4      110k ± 0%      110k ± 0%   -0.04%  (p=0.000 n=20+18)
    ```

--- pkg/dyninst/object/elf.go
+++ pkg/dyninst/object/elf.go
@@ -8,11 +8,14 @@
 package object
 
 import (
+	"compress/zlib"
 	"debug/dwarf"
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"strings"
+	"unsafe"
 
 	dlvdwarf "github.com/go-delve/delve/pkg/dwarf"
 
@@ -45,6 +48,16 @@ type ElfFile struct {
 	// Maps the compile unit entry offset to the version of the DWARF spec
 	// that was used to parse it.
 	unitVersions map[dwarf.Offset]uint8
+}
+
+// TextSectionHeader implements File.
+func (e *ElfFile) TextSectionHeader() (*safeelf.SectionHeader, error) {
+	for _, s := range e.Underlying.Elf.Sections {
+		if s.Name == ".text" {
+			return &s.SectionHeader, nil
+		}
+	}
+	return nil, fmt.Errorf("text section not found")
 }
 
 // Architecture implements File.
@@ -136,7 +149,7 @@ func newElfObject(mmf *MMappingElfFile) (_ *ElfFile, retErr error) {
 	if err != nil {
 		return nil, err
 	}
-	ds, err := loadDebugSections(mmf.Elf)
+	ds, err := loadDebugSections(mmf.Elf, mmf.f)
 	if err != nil {
 		return nil, err
 	}
@@ -178,7 +191,7 @@ func newElfObject(mmf *MMappingElfFile) (_ *ElfFile, retErr error) {
 // which could be used to load the section into a file we then mmap or something
 // like that.
 
-func loadDebugSections(f *safeelf.File) (*DebugSections, error) {
+func loadDebugSections(elfFile *safeelf.File, f io.ReaderAt) (*DebugSections, error) {
 	dwarfSuffix := func(s *safeelf.Section) string {
 		const debug = ".debug_"
 		const zdebug = ".zdebug_"
@@ -194,7 +207,7 @@ func loadDebugSections(f *safeelf.File) (*DebugSections, error) {
 	// There are many DWARf sections, but these are the ones
 	// the debug/dwarf package started with.
 	var ds DebugSections
-	for _, s := range f.Sections {
+	for _, s := range elfFile.Sections {
 		suffix := dwarfSuffix(s)
 		if suffix == "" {
 			continue
@@ -207,6 +220,11 @@ func loadDebugSections(f *safeelf.File) (*DebugSections, error) {
 			return nil, fmt.Errorf("section %s already loaded", s.Name)
 		}
 
+		cr, err := sectionData(s, elfFile, f)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get section data for %s: %w", s.Name, err)
+		}
+
 		// TODO: Figure out whether it is important that we aren't applying
 		// relocations here. The elf code that loads dwarf in `f.DWARF()` does
 		// apply relocations, so it's possible that we're not getting the same
@@ -214,7 +232,7 @@ func loadDebugSections(f *safeelf.File) (*DebugSections, error) {
 		// like the stdlib when loading this data.
 		//
 		// 0: https://github.com/golang/go/blob/db55b83c/src/debug/elf/file.go#L1351-L1377
-		data, err := s.Data()
+		data, err := cr.data(f)
 		if err != nil {
 			return nil, fmt.Errorf(
 				"failed to get section data for %s: %w", s.Name, err,
@@ -223,4 +241,170 @@ func loadDebugSections(f *safeelf.File) (*DebugSections, error) {
 		*sd = data
 	}
 	return &ds, nil
+}
+
+type compressionFormat uint8
+
+const (
+	compressionFormatUnknown compressionFormat = iota
+	compressionFormatNone
+	compressionFormatZlib
+)
+
+type compressedFileRange struct {
+	format compressionFormat
+	// The offset of the compressed data in the file.
+	// Note that this takes into account any compression header.
+	offset int64
+	// The length of the compressed data.
+	compressedLength int64
+	// The length of the uncompressed data.
+	uncompressedLength int64
+}
+
+func (r compressedFileRange) data(fileReaderAt io.ReaderAt) ([]byte, error) {
+	// We don't want to let an invalid section header cause us to load
+	// too much data into memory.
+	const maxSectionSize = 512 << 20 // 512MiB
+
+	if r.uncompressedLength > maxSectionSize {
+		return nil, fmt.Errorf(
+			"section has a decompressed size of %d bytes, "+
+				"exceeding the maximum of %d bytes",
+			r.uncompressedLength,
+			maxSectionSize,
+		)
+	}
+
+	var reader io.Reader
+	switch r.format {
+	case compressionFormatUnknown:
+		return nil, fmt.Errorf("unknown compression format")
+	case compressionFormatNone:
+		reader = io.NewSectionReader(fileReaderAt, r.offset, r.compressedLength)
+	case compressionFormatZlib:
+		input := io.NewSectionReader(
+			fileReaderAt,
+			r.offset,
+			r.compressedLength,
+		)
+
+		zrd, err := zlib.NewReader(input)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create zlib reader: %w", err)
+		}
+		reader = zrd
+	}
+
+	data := make([]byte, r.uncompressedLength)
+	_, err := io.ReadFull(reader, data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read section data: %w", err)
+	}
+	return data, nil
+}
+
+func sectionData(
+	s *safeelf.Section,
+	elfFile *safeelf.File,
+	fileReaderAt io.ReaderAt,
+) (compressedFileRange, error) {
+	var r compressedFileRange
+	if s.Type == elf_SHT_NOBITS {
+		return r, nil
+	}
+
+	// If the section doesn't have the compressed flag set, it still might
+	// be compressed with the "gnu" compression scheme. This is somewhat
+	// outdated for elf binaries produced by a modern Go toolchain, but it's
+	// the scheme used for compression in Mach-O binaries on macos.
+	//
+	// See https://github.com/golang/go/blob/d166a0b0/src/debug/elf/file.go#L134-L149
+	if s.Flags&elf_SHF_COMPRESSED == 0 {
+		if !strings.HasPrefix(s.Name, ".zdebug") {
+			return compressedFileRange{
+				format:             compressionFormatNone,
+				offset:             int64(s.Offset),
+				compressedLength:   int64(s.Size),
+				uncompressedLength: int64(s.Size),
+			}, nil
+		}
+		sr := io.NewSectionReader(fileReaderAt, int64(s.Offset), int64(s.Size))
+
+		const gnuCompressionMagic = "ZLIB\x00\x00\x00\x00"
+		const gnuCompressionHeaderLength = 12 // magic + 4-byte big endian uncompressed length
+		b := make([]byte, gnuCompressionHeaderLength)
+		n, _ := sr.ReadAt(b, 0)
+		if n != 12 || string(b[:8]) != gnuCompressionMagic {
+			return compressedFileRange{
+				format:             compressionFormatUnknown,
+				offset:             int64(s.Offset + gnuCompressionHeaderLength),
+				compressedLength:   int64(s.Size),
+				uncompressedLength: int64(s.Size),
+			}, nil
+		}
+
+		r.format = compressionFormatZlib
+		r.uncompressedLength = int64(binary.BigEndian.Uint32(b[8:12]))
+		r.compressedLength = int64(s.Size) - gnuCompressionHeaderLength
+		r.offset = int64(s.Offset) + gnuCompressionHeaderLength
+		return r, nil
+	}
+
+	// Parse out the compression header.
+	//
+	// See https://github.com/golang/go/blob/d166a0b0/src/debug/elf/file.go#L557-L579
+	if s.Flags&elf_SHF_ALLOC != 0 {
+		return r, fmt.Errorf("SHF_COMPRESSED applies only to non-allocable sections")
+	}
+	sr := io.NewSectionReader(fileReaderAt, int64(s.Offset), int64(s.FileSize))
+
+	var bo binary.ByteOrder
+	switch elfFile.File.Data {
+	case elf_ELFDATA2LSB:
+		bo = binary.LittleEndian
+	case elf_ELFDATA2MSB:
+		bo = binary.BigEndian
+	default:
+		return r, fmt.Errorf("unknown elf data encoding: %#v", elfFile.File.Data)
+	}
+	// Read the compression header.
+	switch elfFile.File.Class {
+	case elf_ELFCLASS32:
+		var ch elf_Chdr32
+		chdata := make([]byte, unsafe.Sizeof(ch))
+		if _, err := sr.ReadAt(chdata, 0); err != nil {
+			return r, fmt.Errorf("failed to read compression header: %w", err)
+		}
+		ct := elf_CompressionType(bo.Uint32(chdata[unsafe.Offsetof(ch.Type):]))
+		switch ct {
+		case elf_COMPRESS_ZLIB:
+			r.format = compressionFormatZlib
+			r.offset = int64(s.Offset) + int64(unsafe.Sizeof(ch))
+			r.compressedLength = int64(s.SectionHeader.FileSize) - int64(unsafe.Sizeof(ch))
+			r.uncompressedLength = int64(bo.Uint64(chdata[unsafe.Offsetof(ch.Size):]))
+			return r, nil
+		default:
+			return r, fmt.Errorf("unknown compression type: %#v %x", ct, ct)
+		}
+	case elf_ELFCLASS64:
+		var ch elf_Chdr64
+		chdata := make([]byte, unsafe.Sizeof(ch))
+		if _, err := sr.ReadAt(chdata, 0); err != nil {
+			return r, err
+		}
+		ct := elf_CompressionType(bo.Uint32(chdata[unsafe.Offsetof(ch.Type):]))
+		switch ct {
+		case elf_COMPRESS_ZLIB:
+			r.format = compressionFormatZlib
+			r.offset = int64(s.Offset) + int64(unsafe.Sizeof(ch))
+			r.compressedLength = int64(s.SectionHeader.FileSize) - int64(unsafe.Sizeof(ch))
+			r.uncompressedLength = int64(bo.Uint64(chdata[unsafe.Offsetof(ch.Size):]))
+			return r, nil
+		default:
+			return r, fmt.Errorf("unknown compression type: %#v %x", ct, chdata)
+		}
+	default:
+		return r, fmt.Errorf("unknown elf class: %#v", elfFile.File.Class)
+	}
 }
