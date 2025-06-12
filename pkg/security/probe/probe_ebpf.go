a2e2e6ec8285ecb20e92979a5bd02769d43acc71
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-06-12 19:51:28 +0200

    move detection to constant fetcher (to fix linux_ebpf related linters)

--- pkg/security/probe/probe_ebpf.go
+++ pkg/security/probe/probe_ebpf.go
@@ -26,7 +26,6 @@ import (
 	"github.com/google/gopacket/layers"
 
 	lib "github.com/cilium/ebpf"
-	"github.com/cilium/ebpf/btf"
 	"github.com/hashicorp/go-multierror"
 	"github.com/moby/sys/mountinfo"
 	"github.com/vishvananda/netlink"
@@ -245,7 +244,7 @@ func (p *EBPFProbe) selectFentryMode() {
 		return
 	}
 
-	tailCallsBroken, err := areFentryTailCallsBroken()
+	tailCallsBroken, err := constantfetch.AreFentryTailCallsBroken()
 	if err != nil {
 		p.useFentry = false
 		seclog.Errorf("fentry enabled but failed to verify tail call support: %v, falling back to kprobe mode", err)
@@ -284,43 +283,6 @@ func (p *EBPFProbe) selectFentryMode() {
 	}
 
 	p.useFentry = true
-}
-
-func areFentryTailCallsBroken() (bool, error) {
-	spec, err := ddebpf.GetKernelSpec()
-	if err != nil {
-		return false, err
-	}
-
-	var bpfMap *btf.Struct
-	if err := spec.TypeByName("bpf_map", &bpfMap); err != nil {
-		return false, err
-	}
-
-	/*
-		we are checking for the presence of the bpf_map.owner.attach_func_proto field
-		if it exists, fentry tail calls are broken
-		https://github.com/torvalds/linux/commit/28ead3eaabc16ecc907cfb71876da028080f6356
-	*/
-
-	for _, member := range bpfMap.Members {
-		if member.Name != "owner" {
-			continue
-		}
-
-		ty, ok := member.Type.(*btf.Struct)
-		if !ok {
-			return false, fmt.Errorf("bpf_map.owner is not a struct")
-		}
-
-		for _, ownerMember := range ty.Members {
-			if ownerMember.Name == "attach_func_proto" {
-				return true, nil
-			}
-		}
-	}
-
-	return false, nil
 }
 
 func (p *EBPFProbe) isCgroupSysCtlNotSupported() bool {
