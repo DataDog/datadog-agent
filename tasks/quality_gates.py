c4c0b1eaeeb26875d3f5e8e9aa7bcb9db070f334
Marethyu <45374460+Pythyu@users.noreply.github.com>
2025-06-13 10:43:11 +0200

    Update quality_gates.py

--- tasks/quality_gates.py
+++ tasks/quality_gates.py
@@ -236,12 +236,7 @@ def update_quality_gates_threshold(ctx, metric_handler, github):
     # Create new branch
     branch_name = f"static_quality_gates/threshold_update_{os.environ['CI_COMMIT_SHORT_SHA']}"
     current_branch = github.repo.get_branch(os.environ["CI_COMMIT_BRANCH"])
-    ctx.run(f"git checkout -b {branch_name}")
-    ctx.run(
-        f"git remote set-url origin https://x-access-token:{github._auth.token}@github.com/DataDog/datadog-agent.git",
-        hide=True,
-    )
-    ctx.run(f"git push --set-upstream origin {branch_name}")
+    github.repo.create_git_ref(ref=f'refs/heads/{branch_name}', sha=current_branch.commit.sha)
 
     # Push changes
     commit_message = "feat(gate): update static quality gates thresholds"
