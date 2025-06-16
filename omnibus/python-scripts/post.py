0dba796b48f5879076f3f8e351a46945a6267e2b
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-06-17 00:49:30 +0200

    cleanup code and improve log message

--- omnibus/python-scripts/post.py
+++ omnibus/python-scripts/post.py
@@ -15,18 +15,12 @@ def post(install_directory, storage_location, skip_flag=False):
         if os.path.exists(install_directory) and os.path.exists(storage_location):
             post_python_installed_packages_file = packages.post_python_installed_packages_file(storage_location)
             packages.create_python_installed_packages_file(post_python_installed_packages_file)
-            
+
             flag_path = "/etc/datadog-agent/.skip_install_python_third_party_deps"
             if os.name == "nt":
                 flag_path = os.path.join(storage_location, ".skip_install_python_third_party_deps")
 
-            # by default, we persist third party integrations
-            persist_third_party_integration = True
-
-            if os.path.exists(flag_path) or not skip_flag:
-                persist_third_party_integration = False
-
-            if persist_third_party_integration:
+            if not os.path.exists(flag_path) or skip_flag:
                 diff_python_installed_packages_file = packages.diff_python_installed_packages_file(storage_location)
                 if os.path.exists(diff_python_installed_packages_file):
                     requirements_agent_release_file = packages.requirements_agent_release_file(install_directory)
@@ -37,7 +31,10 @@ def post(install_directory, storage_location, skip_flag=False):
                     print(f"File '{diff_python_installed_packages_file}' not found.")
                     return 0
             else:
-                print(f"File '{flag_path}' found: no third party integration will be installed.")
+                if not os.path.exists(flag_path):
+                    print(f"File '{flag_path}' found: no third party integration will be installed.")
+                else:
+                    print(f"No third party integration will be installed.")
                 return 0
         else:
             print(f"Directory '{install_directory}' and '{storage_location}' not found.")
