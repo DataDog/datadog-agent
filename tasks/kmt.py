7eca4a53b17e25a8cde50d0af4f57181831753e9
Usama Saqib <usama.saqib@datadoghq.com>
2025-06-17 19:07:37 +0200

    run python linter

--- tasks/kmt.py
+++ tasks/kmt.py
@@ -397,7 +397,9 @@ def init(ctx: Context, images: str | None = None, all_images=False, remote_setup
 
     config_ssh_key(ctx)
 
-    info("[+] Kernel matrix testing system initialized successfully. Refer to https://github.com/DataDog/datadog-agent/blob/main/tasks/kernel_matrix_testing/README.md for next steps.")
+    info(
+        "[+] Kernel matrix testing system initialized successfully. Refer to https://github.com/DataDog/datadog-agent/blob/main/tasks/kernel_matrix_testing/README.md for next steps."
+    )
 
 
 @task
