c5ee6ebb25c8cd8d690bc320355ee64cc951fefc
kovags <marcio.kovags@datadoghq.com>
2025-06-18 10:15:45 +0200

    change test to not use the event

--- pkg/security/tests/fsmount_test.go
+++ pkg/security/tests/fsmount_test.go
@@ -10,16 +10,20 @@ package tests
 
 import (
 	"fmt"
+	sprobe "github.com/DataDog/datadog-agent/pkg/security/probe"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"path"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
 
-	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/rules"
-	"github.com/stretchr/testify/assert"
 )
 
 func fsconfig(fd int, cmd uint, key *byte, value *byte, aux int) (err error) {
@@ -33,6 +37,18 @@ func fsconfigStr(fd int, cmd uint, key string, value string, aux int) (err error
 
 	err = fsconfig(fd, cmd, &keyBytes[0], &valueBytes[0], aux)
 	return err
+}
+
+func mountPointFromFd(fd int) uint32 {
+	data, _ := os.ReadFile("/proc/" + strconv.Itoa(os.Getpid()) + "/fdinfo/" + strconv.Itoa(int(fd)))
+
+	for _, line := range strings.Split(string(data), "\n") {
+		if strings.HasPrefix(line, "mnt_id:") {
+			mountId, _ := strconv.Atoi(strings.Split(line, "\t")[1])
+			return uint32(mountId)
+		}
+	}
+	return 0
 }
 
 func TestFsmount(t *testing.T) {
@@ -57,33 +73,35 @@ func TestFsmount(t *testing.T) {
 	defer test.Close()
 
 	t.Run("fsmount-tmpfs", func(t *testing.T) {
-		test.WaitSignal(t, func() error {
+		p, _ := test.probe.PlatformProbe.(*sprobe.EBPFProbe)
 
-			fsfd, err := unix.Fsopen("tmpfs", 0)
-			if err != nil {
-				return fmt.Errorf("fsopen failed: %w", err)
-			}
-			defer unix.Close(fsfd)
+		fsfd, err := unix.Fsopen("tmpfs", 0)
+		if err != nil {
+			assert.Fail(t, "fsopen failed")
+			return
+		}
+		defer unix.Close(fsfd)
 
-			_ = fsconfigStr(fsfd, unix.FSCONFIG_SET_STRING, "source", "tmpfs", 0)
-			_ = fsconfigStr(fsfd, unix.FSCONFIG_SET_STRING, "size", "50M", 0)
-			_ = fsconfig(fsfd, unix.FSCONFIG_CMD_CREATE, nil, nil, 0)
+		_ = fsconfigStr(fsfd, unix.FSCONFIG_SET_STRING, "source", "tmpfs", 0)
+		_ = fsconfigStr(fsfd, unix.FSCONFIG_SET_STRING, "size", "50M", 0)
+		_ = fsconfig(fsfd, unix.FSCONFIG_CMD_CREATE, nil, nil, 0)
 
-			mountfd, err := unix.Fsmount(fsfd, 0, 0)
-			if err != nil {
-				return fmt.Errorf("fsmount failed: %w", err)
-			}
-			defer unix.Close(mountfd)
+		mountfd, err := unix.Fsmount(fsfd, 0, 0)
+		if err != nil {
+			assert.Fail(t, "fsmount failed")
+			return
+		}
+		defer unix.Close(mountfd)
 
-			return nil
-		}, func(event *model.Event, rule *rules.Rule) {
-			assertTriggeredRule(t, rule, "test_fsmount_tmpfs")
-			assertFieldEqual(t, event, "process.file.path", executable)
-			assert.Equal(t, "fsmount", event.GetType(), "wrong event type")
+		time.Sleep(500 * time.Millisecond)
 
-			test.validateFsmountSchema(t, event)
+		if err != nil {
+			assert.Fail(t, "mount resolution failed")
+		}
 
-		})
+		mnt, _, _, err := p.Resolvers.MountResolver.ResolveMount(mountPointFromFd(mountfd), 0, 0, "")
+		assert.Equal(t, mnt.Origin, model.MountOriginFsmount)
+		assert.Equal(t, mnt.RootStr, "/")
 	})
 
 }
