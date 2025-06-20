c71df0548ba8f3f3c1651fc2635e8b61467403df
Branden Clark <branden.clark@datadoghq.com>
2025-06-20 10:59:27 -0400

    bump script version

--- tools/windows/DatadogAgentInstallScript/Install-Datadog.ps1
+++ tools/windows/DatadogAgentInstallScript/Install-Datadog.ps1
@@ -3,7 +3,7 @@
    Downloads and installs Datadog on the machine.
 #>
 [CmdletBinding(DefaultParameterSetName = 'Default')]
-$SCRIPT_VERSION = "1.0.0"
+$SCRIPT_VERSION = "1.1.0"
 $GENERAL_ERROR_CODE = 1
 
 # Set some defaults if not provided
