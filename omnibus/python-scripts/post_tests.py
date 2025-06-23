0341323a5320beb3e54ff830f03230e2a7112c6f
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-06-23 15:23:32 +0200

    apply jack0x2 suggestions

--- omnibus/python-scripts/post_tests.py
+++ omnibus/python-scripts/post_tests.py
@@ -7,10 +7,10 @@ class TestPost(unittest.TestCase):
     def test_post(self):
         install_directory = tempfile.mkdtemp()
         storage_location = tempfile.mkdtemp()
-        
+
         result = post(install_directory, storage_location)
 
-        # assert it ran with no errors 
+        # assert it ran with no errors
         self.assertEqual(result, 0)
 
         # confirm it made .post_python_installed_packages.txt
@@ -20,21 +20,21 @@ class TestPost(unittest.TestCase):
             content = f.read()
             self.assertIn("# DO NOT REMOVE/MODIFY", content)
             self.assertIn("invoke", content)
-        
+
         # Cleanup
         os.remove(post_file)
-        
+
         # running rmdir verifies that the directory is empty
         # asserts no extra files are created
         os.rmdir(install_directory)
         os.rmdir(storage_location)
 
-    @unittest.skipIf(os.name == "nt", "windows is behavior is tested separately")
     def test_post_with_empty_files(self):
         install_directory = tempfile.mkdtemp()
         storage_location = tempfile.mkdtemp()
         post_file = os.path.join(storage_location, '.post_python_installed_packages.txt')
         diff_file = os.path.join(storage_location, '.diff_python_installed_packages.txt')
+        skip_flag_file = os.path.join(storage_location, '.skip_install_python_third_party_deps')
 
         # Create empty post file
         with open(post_file, 'w', encoding='utf-8') as f:
@@ -44,9 +44,13 @@ class TestPost(unittest.TestCase):
         with open(diff_file, 'w', encoding='utf-8') as f:
             pass
 
-        result = post(install_directory, storage_location, skip_flag=True)
+        # Create empty skip flag file
+        with open(skip_flag_file, 'w', encoding='utf-8') as f:
+            pass
 
-        # assert it ran with no errors 
+        result = post(install_directory, storage_location)
+
+        # assert it ran with no errors
         self.assertEqual(result, 0)
 
         # confirm it made .post_python_installed_packages.txt
@@ -58,6 +62,7 @@ class TestPost(unittest.TestCase):
         # Cleanup
         os.remove(post_file)
         os.remove(diff_file)
+        os.remove(skip_flag_file)
 
         # running rmdir verifies that the directory is empty
         os.rmdir(install_directory)
