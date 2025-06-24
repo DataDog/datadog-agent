cfd272347b81185be0f061125b420453c9ee6ffb
Guy Arbitman <guy20495@gmail.com>
2025-06-24 09:43:31 +0300

    wip

--- pkg/network/protocols/http/debugger/debugger_linux_nobpf.go
+++ pkg/network/protocols/http/debugger/debugger_linux_nobpf.go
@@ -3,12 +3,15 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build !linux_bpf
+
 // Package debugger provides utilities for testing the HTTP protocol.
 package debugger
 
 import (
-	"github.com/DataDog/datadog-agent/pkg/network/tracer"
 	"net/http"
+
+	"github.com/DataDog/datadog-agent/pkg/network/tracer"
 )
 
 // GetHTTPDebugEndpoint returns a handler for debugging HTTP requests.
