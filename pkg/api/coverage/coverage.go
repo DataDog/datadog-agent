428cec6c932a15f26735e50ecac285e6ab24fac2
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-06-25 10:24:39 +0200

    Add global api util to add coverage handler
    fix package name

---
+++ pkg/api/coverage/coverage.go
@@ -0,0 +1,47 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package agent implements the api endpoints for the `/agent` prefix.
+// This group of endpoints is meant to provide high-level functionalities
+// at the agent level.
+
+//go:build e2ecoverage
+
+package coverage
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+	"runtime/coverage"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupCoverageHandler(r *mux.Router) {
+	r.HandleFunc("/coverage", componentCoverageHandler).Methods("GET")
+}
+
+func componentCoverageHandler(w http.ResponseWriter, _ *http.Request) {
+	tempDir := path.Join(os.TempDir(), "coverage")
+	err := os.MkdirAll(tempDir, 0755)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = coverage.WriteCountersDir(tempDir)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = coverage.WriteMetaDir(tempDir)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(fmt.Sprintf("Coverage written to %s", tempDir)))
+}
