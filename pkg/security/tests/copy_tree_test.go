e3636c0939f56d8508def86b7b2c7480b9ac192a
kovags <marcio.kovags@datadoghq.com>
2025-06-25 15:51:32 +0200

    open_tree test

--- pkg/security/tests/copy_tree_test.go
+++ pkg/security/tests/copy_tree_test.go
@@ -12,40 +12,43 @@ import (
 	"fmt"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/rules"
-	"github.com/stretchr/testify/assert"
 	"os"
-	"os/exec"
+	"syscall"
 	"testing"
 	"time"
-	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
 
-func fsconfig(fd int, cmd uint, key *byte, value *byte, aux int) (err error) {
-	_, _, e1 := unix.Syscall6(unix.SYS_FSCONFIG, uintptr(fd), uintptr(cmd), uintptr(unsafe.Pointer(key)), uintptr(unsafe.Pointer(value)), uintptr(aux), 0)
-	return e1
+func TmpMountAt(dir string) error {
+	openfd, err := unix.Fsopen("tmpfs", unix.FSOPEN_CLOEXEC)
+	if err != nil {
+		return err
+	}
+
+	_ = fsconfigStr(openfd, unix.FSCONFIG_SET_STRING, "source", "tmpfs", 0)
+	_ = fsconfigStr(openfd, unix.FSCONFIG_SET_STRING, "size", "1M", 0)
+	_ = fsconfig(openfd, unix.FSCONFIG_CMD_CREATE, nil, nil, 0)
+	mountfd, err := unix.Fsmount(openfd, unix.FSMOUNT_CLOEXEC, 0)
+	if err != nil {
+		return err
+	}
+
+	err = unix.MoveMount(mountfd, "", unix.AT_FDCWD, dir, unix.MOVE_MOUNT_F_EMPTY_PATH)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func fsconfigStr(fd int, cmd uint, key string, value string, aux int) (err error) {
-	keyBytes := append([]byte(key), 0)
-	valueBytes := append([]byte(value), 0)
-
-	err = fsconfig(fd, cmd, &keyBytes[0], &valueBytes[0], aux)
-	return err
-}
-
-func TestFsmount(t *testing.T) {
+func TestCopyTree(t *testing.T) {
 	SkipIfNotAvailable(t)
 
 	ruleDefs := []*rules.RuleDefinition{
 		{
 			ID:         "test_rule",
 			Expression: `open.file.name == "test-open"`,
-		},
-		{
-			ID:         "test_rule_2",
-			Expression: `mkdir.file.name == "test-mkdir"`,
 		},
 	}
 
@@ -55,33 +58,70 @@ func TestFsmount(t *testing.T) {
 	}
 	defer test.Close()
 
-	t.Run("fsmount-tmpfs", func(t *testing.T) {
-		var fsfd int
+	t.Run("copy-tree-test", func(t *testing.T) {
+		// Mount the following directory struct in /tmp:
+		// + /tmp/<somedir>/001        (tmpfs, 1MB)
+		// |-- /tmp/<somedir>/001/tmp1 (tmpfs, 1MB)
+		// |-- /tmp/<somedir>/001/tmp2 (tmpfs, 1MB)
+		// In which `tmp1` and `tmp2` are have 001 as the parent mount
+		// This is using the new mount api, but could have been accomplished with the mount() syscall too
+		// because this isn't the part that we're testing
+		var tounmount []string
+
+		dir := t.TempDir()
+		tounmount = append(tounmount, dir)
+
+		err := TmpMountAt(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		mountSubDir := func(subdir string) {
+			subdir = dir + "/" + subdir
+			err = os.Mkdir(subdir, 0755)
+			tounmount = append(tounmount, subdir)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = TmpMountAt(subdir)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		mountSubDir("tmp1")
+		mountSubDir("tmp2")
+
+		defer func() {
+			for i := len(tounmount) - 1; i >= 0; i-- {
+				fmt.Println(tounmount[i])
+				err = unix.Unmount(tounmount[i], syscall.MNT_DETACH)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+		}()
+
+		var detachedMounts, mounts int
 		err = test.GetProbeEvent(func() error {
-			fsfd, err := unix.Fsopen("tmpfs", 0)
-			if err != nil {
-				t.Skip("This kernel doesn't have the new mount api")
-				return nil
-			}
-			defer unix.Close(fsfd)
-
-			_ = fsconfigStr(fsfd, unix.FSCONFIG_SET_STRING, "source", "tmpfs", 0)
-			_ = fsconfigStr(fsfd, unix.FSCONFIG_SET_STRING, "size", "50M", 0)
-			_ = fsconfig(fsfd, unix.FSCONFIG_CMD_CREATE, nil, nil, 0)
-
-			mountfd, err := unix.Fsmount(fsfd, unix.FSMOUNT_CLOEXEC, unix.MOUNT_ATTR_RDONLY)
-			if err != nil {
-				return err
-			}
-			defer unix.Close(mountfd)
-
+			// Now we attempt to make a recursive copy of the entire tree that was created previously created
+			unix.OpenTree(0, dir, unix.OPEN_TREE_CLONE)
 			return nil
 		}, func(event *model.Event) bool {
-			assert.NotEqual(t, event.Fsmount.MountID, uint32(0), "Mount id is zero")
-			assert.NotEqual(t, event.Fsmount.Flags, unix.FSOPEN_CLOEXEC, "Mount flags")
-			assert.NotEqual(t, event.Fsmount.Fd, fsfd, "Wrong file descriptor")
-			assert.NotEqual(t, event.Fsmount.MountAttrs, unix.FSMOUNT_CLOEXEC, "Wrong mount attributes")
-			return true
+			if event.GetType() == "detached_mount" {
+				detachedMounts++
+			}
+
+			if event.GetType() == "mount" {
+				mounts++
+			}
+
+			fmt.Println("EVENT: ", event.Mount)
+			//assert.NotEqual(t, event.Fsmount.MountID, uint32(0), "Mount id is zero")
+			//assert.NotEqual(t, event.Fsmount.Flags, unix.FSOPEN_CLOEXEC, "Mount flags")
+			//assert.NotEqual(t, event.Fsmount.MountAttrs, unix.FSMOUNT_CLOEXEC, "Wrong mount attributes")
+			return detachedMounts == 2 && mounts == 1
 		}, 3*time.Second, model.FileFsmountEventType)
 	})
 
