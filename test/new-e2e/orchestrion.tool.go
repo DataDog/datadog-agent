036f3d722084264251746401037249c4e0d04554
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-06-30 19:30:54 +0200

    Install and use orchestrion for build

---
+++ test/new-e2e/orchestrion.tool.go
@@ -0,0 +1,13 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016 Datadog, Inc.
+
+//go:build tools
+
+package orchestrion
+
+import (
+	_ "github.com/DataDog/dd-trace-go/v2/internal/civisibility/integrations/gotesting" // integration
+	_ "github.com/DataDog/orchestrion"
+)
