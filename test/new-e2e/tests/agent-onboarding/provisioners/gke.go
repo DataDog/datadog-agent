e494f2003242ccc4b635b259fa87b4654785b766
Fanny Jiang <fanny.jiang@datadoghq.com>
2025-07-02 16:48:37 -0400

    comment out custom stuff

--- test/new-e2e/tests/agent-onboarding/provisioners/gke.go
+++ test/new-e2e/tests/agent-onboarding/provisioners/gke.go
@@ -7,16 +7,10 @@ package provisioners
 
 import (
 	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/environments"
-	"github.com/DataDog/test-infra-definitions/components/datadog/agent"
 	"github.com/DataDog/test-infra-definitions/components/datadog/agentwithoperatorparams"
-	"github.com/DataDog/test-infra-definitions/components/datadog/operator"
-	"github.com/DataDog/test-infra-definitions/components/datadog/operatorparams"
 	"github.com/DataDog/test-infra-definitions/resources/gcp"
 	"github.com/DataDog/test-infra-definitions/scenarios/gcp/fakeintake"
 	"github.com/DataDog/test-infra-definitions/scenarios/gcp/gke"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
-	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/yaml"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -52,72 +46,72 @@ func GkeRunFunc(ctx *pulumi.Context, env *environments.Kubernetes, params *Kuber
 		env.FakeIntake = nil
 	}
 
-	ns, err := corev1.NewNamespace(ctx, gcpEnv.CommonNamer().ResourceName("k8s-namespace"), &corev1.NamespaceArgs{Metadata: &metav1.ObjectMetaArgs{
-		Name: pulumi.String("e2e-operator"),
-	}}, pulumi.Provider(cluster.KubeProvider))
-
-	if err != nil {
-		return err
-	}
-
-	// Install kustomizations
-	kustomizeAppFunc := KustomizeWorkloadAppFunc(params.testName, params.kustomizeResources)
-
-	e2eKustomize, err := kustomizeAppFunc(&gcpEnv, cluster.KubeProvider)
-	if err != nil {
-		return err
-	}
-
-	// Create Operator component
-	var operatorComp *operator.Operator
-	if params.operatorOptions != nil {
-		operatorOpts := []pulumi.ResourceOption{
-			pulumi.DependsOn([]pulumi.Resource{e2eKustomize, ns}),
-		}
-		params.operatorOptions = append(params.operatorOptions, operatorparams.WithPulumiResourceOptions(operatorOpts...))
-
-		operatorComp, err = operator.NewOperator(&gcpEnv, gcpEnv.CommonNamer().ResourceName("operator"), cluster.KubeProvider, params.operatorOptions...)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Setup DDA options
-	if params.ddaOptions != nil && params.operatorOptions != nil {
-		ddaResourceOpts := []pulumi.ResourceOption{
-			pulumi.DependsOn([]pulumi.Resource{e2eKustomize, operatorComp}),
-		}
-		params.ddaOptions = append(
-			params.ddaOptions,
-			agentwithoperatorparams.WithPulumiResourceOptions(ddaResourceOpts...))
-
-		ddaComp, aErr := agent.NewDDAWithOperator(&gcpEnv, params.name, cluster.KubeProvider, params.ddaOptions...)
-		if aErr != nil {
-			return aErr
-		}
-
-		if err = ddaComp.Export(ctx, &env.Agent.KubernetesAgentOutput); err != nil {
-			return err
-		}
-	} else {
-		env.Agent = nil
-	}
-
-	for _, workload := range params.yamlWorkloads {
-		_, err = yaml.NewConfigFile(ctx, workload.Name, &yaml.ConfigFileArgs{
-			File: workload.Path,
-		}, pulumi.Provider(cluster.KubeProvider))
-		if err != nil {
-			return err
-		}
-	}
-
-	for _, appFunc := range params.workloadAppFuncs {
-		_, err := appFunc(&gcpEnv, cluster.KubeProvider)
-		if err != nil {
-			return err
-		}
-	}
+	//ns, err := corev1.NewNamespace(ctx, gcpEnv.CommonNamer().ResourceName("k8s-namespace"), &corev1.NamespaceArgs{Metadata: &metav1.ObjectMetaArgs{
+	//	Name: pulumi.String("e2e-operator"),
+	//}}, pulumi.Provider(cluster.KubeProvider))
+	//
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//// Install kustomizations
+	//kustomizeAppFunc := KustomizeWorkloadAppFunc(params.testName, params.kustomizeResources)
+	//
+	//e2eKustomize, err := kustomizeAppFunc(&gcpEnv, cluster.KubeProvider)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//// Create Operator component
+	//var operatorComp *operator.Operator
+	//if params.operatorOptions != nil {
+	//	operatorOpts := []pulumi.ResourceOption{
+	//		pulumi.DependsOn([]pulumi.Resource{e2eKustomize, ns}),
+	//	}
+	//	params.operatorOptions = append(params.operatorOptions, operatorparams.WithPulumiResourceOptions(operatorOpts...))
+	//
+	//	operatorComp, err = operator.NewOperator(&gcpEnv, gcpEnv.CommonNamer().ResourceName("operator"), cluster.KubeProvider, params.operatorOptions...)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//
+	//// Setup DDA options
+	//if params.ddaOptions != nil && params.operatorOptions != nil {
+	//	ddaResourceOpts := []pulumi.ResourceOption{
+	//		pulumi.DependsOn([]pulumi.Resource{e2eKustomize, operatorComp}),
+	//	}
+	//	params.ddaOptions = append(
+	//		params.ddaOptions,
+	//		agentwithoperatorparams.WithPulumiResourceOptions(ddaResourceOpts...))
+	//
+	//	ddaComp, aErr := agent.NewDDAWithOperator(&gcpEnv, params.name, cluster.KubeProvider, params.ddaOptions...)
+	//	if aErr != nil {
+	//		return aErr
+	//	}
+	//
+	//	if err = ddaComp.Export(ctx, &env.Agent.KubernetesAgentOutput); err != nil {
+	//		return err
+	//	}
+	//} else {
+	//	env.Agent = nil
+	//}
+	//
+	//for _, workload := range params.yamlWorkloads {
+	//	_, err = yaml.NewConfigFile(ctx, workload.Name, &yaml.ConfigFileArgs{
+	//		File: workload.Path,
+	//	}, pulumi.Provider(cluster.KubeProvider))
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//
+	//for _, appFunc := range params.workloadAppFuncs {
+	//	_, err := appFunc(&gcpEnv, cluster.KubeProvider)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
 
 	return nil
 }
