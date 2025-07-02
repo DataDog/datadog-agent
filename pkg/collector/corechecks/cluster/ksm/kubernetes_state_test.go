7e1f597292cb94fb8fbf63718b73a71d15ac15e6
Frank Spano <frank.spano@datadoghq.com>
2025-07-02 10:38:55 -0400

    CONTINT-4704: skip resource cache checks on node agent

--- pkg/collector/corechecks/cluster/ksm/kubernetes_state_test.go
+++ pkg/collector/corechecks/cluster/ksm/kubernetes_state_test.go
@@ -1436,42 +1436,49 @@ func TestKSMCheck_mergeLabelsOrAnnotationAsTags(t *testing.T) {
 		conf                    map[string]map[string]string
 		extra                   map[string]map[string]string
 		shouldTransformResource bool
+		isNodeAgent            bool
 		expected                map[string]map[string]string
 	}{
 		{
 			name:     "nominal",
 			conf:     make(map[string]map[string]string),
 			extra:    defaultAnnotationsAsTags(),
+			isNodeAgent: false,
 			expected: defaultAnnotationsAsTags(),
 		},
 		{
 			name:     "nil conf",
 			conf:     nil,
 			extra:    defaultAnnotationsAsTags(),
+			isNodeAgent: false,
 			expected: defaultAnnotationsAsTags(),
 		},
 		{
 			name:     "collision",
 			conf:     map[string]map[string]string{"pod": {"common_key": "in_val"}},
 			extra:    map[string]map[string]string{"pod": {"common_key": "extra_val", "foo": "bar"}},
+			isNodeAgent: false,
 			expected: map[string]map[string]string{"pod": {"common_key": "in_val", "foo": "bar"}},
 		},
 		{
 			name:     "no collision",
 			conf:     map[string]map[string]string{"pod": {"common_key": "in_val"}},
 			extra:    map[string]map[string]string{"deployment": {"common_key": "extra_val", "foo": "bar"}},
+			isNodeAgent: false,
 			expected: map[string]map[string]string{"pod": {"common_key": "in_val"}, "deployment": {"common_key": "extra_val", "foo": "bar"}},
 		},
 		{
 			name:     "nil extra",
 			conf:     map[string]map[string]string{"pod": {"common_key": "in_val"}},
 			extra:    nil,
+			isNodeAgent: false,
 			expected: map[string]map[string]string{"pod": {"common_key": "in_val"}},
 		},
 		{
 			name:     "conf nil values",
 			conf:     map[string]map[string]string{"job": nil, "deployment": nil, "statefulset": nil, "daemonset": nil},
 			extra:    defaultAnnotationsAsTags(),
+			isNodeAgent: false,
 			expected: defaultAnnotationsAsTags(),
 		},
 		{
@@ -1479,7 +1486,24 @@ func TestKSMCheck_mergeLabelsOrAnnotationAsTags(t *testing.T) {
 			conf:                    map[string]map[string]string{"pod": {"common_key": "in_val"}, "deployment": {"foo": "bar"}},
 			extra:                   map[string]map[string]string{"endpoints": {"foo": "bar"}, "daemonsets.apps": {"fizz": "buzz"}, "pods": {"common_key": "some_val", "another_key": "another_value"}, "deployments.apps": {"foo": "another_bar", "fizz": "buzz"}},
 			shouldTransformResource: true,
+			isNodeAgent:            false,
 			expected:                map[string]map[string]string{"pod": {"common_key": "in_val", "another_key": "another_value"}, "endpoint": {"foo": "bar"}, "daemonset": {"fizz": "buzz"}, "deployment": {"foo": "bar", "fizz": "buzz"}},
+		},
+		{
+			name:                    "node agent skips resource transformation",
+			conf:                    map[string]map[string]string{"pod": {"common_key": "in_val"}},
+			extra:                   map[string]map[string]string{"endpoints": {"foo": "bar"}, "daemonsets.apps": {"fizz": "buzz"}, "pods": {"another_key": "another_value"}},
+			shouldTransformResource: true,
+			isNodeAgent:            true,
+			expected:                map[string]map[string]string{"pod": {"common_key": "in_val"}, "endpoints": {"foo": "bar"}, "daemonsets.apps": {"fizz": "buzz"}, "pods": {"another_key": "another_value"}},
+		},
+		{
+			name:                    "node agent with shouldTransformResource false",
+			conf:                    map[string]map[string]string{"pod": {"common_key": "in_val"}},
+			extra:                   map[string]map[string]string{"endpoints": {"foo": "bar"}, "daemonsets.apps": {"fizz": "buzz"}},
+			shouldTransformResource: false,
+			isNodeAgent:            true,
+			expected:                map[string]map[string]string{"pod": {"common_key": "in_val"}, "endpoints": {"foo": "bar"}, "daemonsets.apps": {"fizz": "buzz"}},
 		},
 	}
 
@@ -1507,7 +1531,7 @@ func TestKSMCheck_mergeLabelsOrAnnotationAsTags(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			annotationsAsTags := mergeLabelsOrAnnotationAsTags(tt.extra, tt.conf, tt.shouldTransformResource)
+			annotationsAsTags := mergeLabelsOrAnnotationAsTags(tt.extra, tt.conf, tt.shouldTransformResource, tt.isNodeAgent)
 			assert.Truef(t, reflect.DeepEqual(tt.expected, annotationsAsTags), "expected %v, found %v", tt.expected, annotationsAsTags)
 		})
 	}
