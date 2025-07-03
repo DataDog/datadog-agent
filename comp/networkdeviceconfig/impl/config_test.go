7f211583158c2ad194a03f0fab363d3bd69fe906
zoe ✨ <9274242+zoedt@users.noreply.github.com>
2025-07-03 10:15:35 -0400

    First pass for SSH implementation iteration for NCM

---
+++ comp/networkdeviceconfig/impl/config_test.go
@@ -0,0 +1,107 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+package networkdeviceconfigimpl
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/config/mock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	var tests = []struct {
+		name           string
+		configYaml     string
+		expectedConfig RawNcmConfig
+	}{
+		{
+			name:       "NCM not configured / set-up and enabled",
+			configYaml: ``,
+			expectedConfig: RawNcmConfig{
+				Devices: []DeviceConfig{},
+			},
+		},
+		{
+			name: "NCM configured with one device using SSH configurations",
+			configYaml: `
+network_config_management:
+  namespace: test
+  devices:
+    - ip_address: 10.0.0.1
+      auth:
+        username: admin
+        password: password
+        port: 22
+        protocol: tcp
+`,
+			expectedConfig: RawNcmConfig{
+				Namespace: "test",
+				Devices: []DeviceConfig{
+					{
+						IPAddress: "10.0.0.1",
+						Auth: AuthCredentials{
+							Username: "admin",
+							Password: "password",
+							Port:     "22",
+							Protocol: "tcp",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "NCM configured with multiple devices using SSH",
+			configYaml: `
+network_config_management:
+  namespace: test
+  devices:
+    - ip_address: 10.0.0.1
+      auth:
+        username: admin
+        password: password
+        port: 22
+        protocol: tcp
+    - ip_address: 10.0.0.2
+      auth:
+        username: user
+        password: pass
+        port: 22
+        protocol: tcp
+`,
+			expectedConfig: RawNcmConfig{
+				Namespace: "test",
+				Devices: []DeviceConfig{
+					{
+						IPAddress: "10.0.0.1",
+						Auth: AuthCredentials{
+							Username: "admin",
+							Password: "password",
+							Port:     "22",
+							Protocol: "tcp",
+						},
+					},
+					{
+						IPAddress: "10.0.0.2",
+						Auth: AuthCredentials{
+							Username: "user",
+							Password: "pass",
+							Port:     "22",
+							Protocol: "tcp",
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockConfig := mock.NewFromYAML(t, tt.configYaml)
+			testConfig := newConfig(mockConfig)
+			assert.Equal(t, tt.expectedConfig.Namespace, testConfig.Namespace)
+			assert.Equal(t, tt.expectedConfig.Devices, testConfig.Devices)
+		})
+	}
+}
