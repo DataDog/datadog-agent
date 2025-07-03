2cd3066f16fd32b07774750099b0869db48bef07
Raymond Zhao <35050708+rayz@users.noreply.github.com>
2025-07-03 12:46:30 -0400

    [AGTMETRICS-306] Add `sequenceID` to config models (#38326)

--- pkg/config/remote/service/service.go
+++ pkg/config/remote/service/service.go
@@ -983,8 +983,8 @@ func filterNeededTargetFiles(neededConfigs []string, cachedTargetFiles []*pbgo.T
 	return filteredList, nil
 }
 
-func (s *CoreAgentService) apiKeyUpdateCallback() func(string, any, any) {
-	return func(setting string, _, newvalue any) {
+func (s *CoreAgentService) apiKeyUpdateCallback() func(string, any, any, uint64) {
+	return func(setting string, _, newvalue any, _ uint64) {
 		if setting != "api_key" {
 			return
 		}
