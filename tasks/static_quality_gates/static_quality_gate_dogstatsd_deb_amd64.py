636a14ccf1f1d930ec7c571a5904e2f6360fa72d
Marethyu <45374460+Pythyu@users.noreply.github.com>
2025-07-07 15:43:32 +0200

    Static quality gates entrypoint to allow on-CI debugging (#37395)
    Co-authored-by: Alex Lopez <alex.lopez@datadoghq.com>

--- tasks/static_quality_gates/static_quality_gate_dogstatsd_deb_amd64.py
+++ tasks/static_quality_gates/static_quality_gate_dogstatsd_deb_amd64.py
@@ -1,7 +1,16 @@
-from tasks.static_quality_gates.lib.package_agent_lib import generic_package_agent_quality_gate
+from tasks.static_quality_gates.lib.package_agent_lib import (
+    generic_debug_package_agent_quality_gate,
+    generic_package_agent_quality_gate,
+)
 
 
 def entrypoint(**kwargs):
     generic_package_agent_quality_gate(
         "static_quality_gate_dogstatsd_deb_amd64", "amd64", "debian", "datadog-dogstatsd", **kwargs
+    )
+
+
+def debug_entrypoint(**kwargs):
+    generic_debug_package_agent_quality_gate(
+        "amd64", "debian", "datadog-dogstatsd", build_job_name="dogstatsd_deb-x64", **kwargs
     )
