5cd33a3aa9e9cfec8d744a8d35d6f19d3bce11d2
Frank Spano <frank.spano@datadoghq.com>
2025-07-08 15:23:42 -0400

    CONTINT-4704: add debug log

--- pkg/collector/corechecks/cluster/ksm/kubernetes_state.go
+++ pkg/collector/corechecks/cluster/ksm/kubernetes_state.go
@@ -1078,6 +1078,7 @@ func mergeLabelsOrAnnotationAsTags(extra, instanceMap map[string]map[string]stri
 				log.Errorf("failed to get singular resource name for %q: %v", resource, err)
 				continue
 			}
+			log.Debugf("using singular resource name %q for %q", singularName, resource)
 		}
 
 		_, found := instanceMap[singularName]
