bf913224625d0ae754b85c5729ff87eae780d4b0
Usama Saqib <usama.saqib@datadoghq.com>
2025-07-09 10:22:29 +0200

    use recommended bash-isms in bata24 install script

--- tasks/kernel_matrix_testing/provision/bata24.sh
+++ tasks/kernel_matrix_testing/provision/bata24.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
-if [[ $UID = 0 ]] ; then
+if [[ $UID == 0 ]] ; then
   echo "Please dont run this script as root, since the gef scripts will get setup for the root user"
   exit 1
 fi
@@ -15,17 +15,17 @@ echo "[+] pip3"
 pip3 install crccheck unicorn capstone ropper keystone-engine tqdm
 
 echo "[+] install seccomp-tools, one_gadget"
-if [ "x$(which seccomp-tools)" = "x" ]; then
+if [[ -z "$(which seccomp-tools)" ]]; then
     sudo gem install seccomp-tools
 fi
 
-if [ "x$(which one_gadget)" = "x" ]; then
+if [[ -z "$(which one_gadget)" ]]; then
     sudo gem install one_gadget
 fi
 
 echo "[+] install rp++"
-if [ "x$(uname -m)" = "xx86_64" ]; then
-    if [ "x$(which rp-lin)" = "x" ] && [ ! -e /usr/local/bin/rp-lin ]; then
+if [[ "$(uname -m)" == "x86_64" ]]; then
+    if [[ -z "$(which rp-lin)" ]] && [[ ! -e /usr/local/bin/rp-lin ]]; then
         wget -q https://github.com/0vercl0k/rp/releases/download/v2.1.3/rp-lin-clang.zip -P /tmp
         sudo unzip /tmp/rp-lin-clang.zip -d /usr/local/bin/
         sudo chmod +x /usr/local/bin/rp-lin
@@ -34,13 +34,13 @@ if [ "x$(uname -m)" = "xx86_64" ]; then
 fi
 
 echo "[+] install vmlinux-to-elf"
-if [ "x$(which vmlinux-to-elf)" = "x" ] && [ ! -e /usr/local/bin/vmlinux-to-elf ]; then
+if [[ -z "$(which vmlinux-to-elf)" ]] && [[ ! -e /usr/local/bin/vmlinux-to-elf ]]; then
     pip3 install --upgrade lz4 zstandard git+https://github.com/clubby789/python-lzo@b4e39df
     pip3 install --upgrade git+https://github.com/marin-m/vmlinux-to-elf
 fi
 
 echo "[+] download gef"
-if [ -e ~/.gdbinit-gef.py ]; then
+if [[ -e ~/.gdbinit-gef.py ]]; then
     echo "[-] ~/.gdbinit-gef.py already exists. Please delete or rename."
     echo "[-] INSTALLATION FAILED"
     exit 1
@@ -50,7 +50,7 @@ fi
 
 echo "[+] setup gef"
 STARTUP_COMMAND="source ~/.gdbinit-gef.py"
-if [ ! -e ~/.gdbinit ] || [ "x$(grep "$STARTUP_COMMAND" ~/.gdbinit)" = "x" ]; then
+if [[ ! -e ~/.gdbinit ]] || [[ -z "$(grep "$STARTUP_COMMAND" ~/.gdbinit)" ]]; then
     echo "$STARTUP_COMMAND" >> ~/.gdbinit
 fi
 
