506e4ef32b86cad6f64ca77a7aba46684b6cb647
Usama Saqib <usama.saqib@datadoghq.com>
2025-07-09 10:22:53 +0200

    use recommended bashisms in ubuntu-dbg.sh

--- tasks/kernel_matrix_testing/provision/ubuntu-dbg.sh
+++ tasks/kernel_matrix_testing/provision/ubuntu-dbg.sh
@@ -2,15 +2,14 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-set -o xtrace
 
 # 1. disable kaslr
-[ -f $(grep -q "GRUB_CMDLINE_LINUX=\".*nokaslr" /etc/default/grub) ] && sed -i 's/^GRUB_CMDLINE_LINUX="/&nokaslr /' /etc/default/grub
+[[ ! $(grep -q "GRUB_CMDLINE_LINUX=\".*nokaslr" /etc/default/grub) ]] && sed -i 's/^GRUB_CMDLINE_LINUX="/&nokaslr /' /etc/default/grub
 update-grub
 
 # 2. download kernel debug build and kernel sources
 codename=$(lsb_release -c | awk  '{print $2}')
-if [ "${codename}" == "xenial" ]; then
+if [[ "${codename}" == "xenial" ]]; then
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C8CAB6595FDFF622
     tee /etc/apt/sources.list.d/ddebs.list << EOF
     deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
