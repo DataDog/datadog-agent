c688b26bf7875ab1a4d2f07cf527990090c3884b
Gabriel Dos Santos <91925154+gabedos@users.noreply.github.com>
2025-07-10 14:28:03 -0400

    WorkloadFilter Additional Filters Initializations (#38474)

--- comp/core/workloadfilter/catalog/utils_test.go
+++ comp/core/workloadfilter/catalog/utils_test.go
@@ -75,6 +75,12 @@ func TestConvertOldToNewFilter_Success(t *testing.T) {
 			[]string{"name:foo-.*", "image:nginx.*"},
 			`endpoint.name.matches("foo-.*")`,
 		},
+		{
+			"image filter on image type",
+			workloadfilter.ImageType,
+			[]string{"image:nginx.*"},
+			`image.name.matches("nginx.*")`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
