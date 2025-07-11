39abb2f67d8fa4e7906226ce75d80d3ab9d2edc8
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-07-11 17:32:20 +0200

    feat: add example of an http request in shared library rust code using reqwest

---
+++ pkg/collector/sharedlibrary/httpcheck/src/main.rs
@@ -0,0 +1,33 @@
+#![deny(warnings)]
+
+// This is using the `tokio` runtime. You'll need the following dependency:
+// 
+// `tokio = { version = "1", features = ["full"] }`
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // Some simple CLI args requirements...
+    let url = if let Some(url) = std::env::args().nth(1) {
+        url
+    } else {
+        println!("No CLI URL provided, using default.");
+        "https://hyper.rs".into()
+    };
+
+    eprintln!("Fetching {url:?}...");
+
+    // reqwest::get() is a convenience function.
+    //
+    // In most cases, you should create/build a reqwest::Client and reuse
+    // it for all requests.
+    let res = reqwest::get(url).await?;
+
+    eprintln!("Response: {:?} {}", res.version(), res.status());
+    eprintln!("Headers: {:#?}\n", res.headers());
+
+    let body = res.text().await?;
+
+    println!("{body}");
+
+    Ok(())
+}
