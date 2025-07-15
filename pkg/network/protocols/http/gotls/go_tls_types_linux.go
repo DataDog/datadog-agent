95e46cb748cd9068a9fab2397f8588992ec49d20
Guy Arbitman <guy20495@gmail.com>
2025-07-15 12:18:15 +0300

    usm: go-tls: Fix incorrect layout of the struct

--- pkg/network/protocols/http/gotls/go_tls_types_linux.go
+++ pkg/network/protocols/http/gotls/go_tls_types_linux.go
@@ -51,8 +51,9 @@ type TlsOffsetsData struct {
 	Close_conn_pointer Location
 }
 type TlsFunctionsArgsKey struct {
-	Pid uint32
-	Id  int64
+	Id        int64
+	Pid       uint32
+	Pad_cgo_0 [4]byte
 }
 type TlsReadArgsData struct {
 	Conn_pointer uint64
