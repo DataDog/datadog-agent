aba68fd7adc141f49be1de026503b04e85373617
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2025-07-15 11:45:43 +0200

    Inline err check

--- pkg/trace/api/api.go
+++ pkg/trace/api/api.go
@@ -493,8 +493,7 @@ func decodeTracerPayload(v Version, req *http.Request, cIDProvider IDProvider, l
 			return nil, err
 		}
 		var traces pb.Traces
-		err = traces.UnmarshalMsgDictionary(buf.Bytes())
-		if err != nil {
+		if err = traces.UnmarshalMsgDictionary(buf.Bytes()); err != nil {
 			return nil, err
 		}
 		return &pb.TracerPayload{
