d05b8185df2a7c516b6e910a9557827f995cc249
Guy Arbitman <guy20495@gmail.com>
2025-07-15 11:40:23 +0300

    usm: go-tls: Expose structs to the user-mode

--- pkg/network/protocols/http/gotls/go_tls_types_linux.go
+++ pkg/network/protocols/http/gotls/go_tls_types_linux.go
@@ -50,3 +50,16 @@ type TlsOffsetsData struct {
 	Write_return_error Location
 	Close_conn_pointer Location
 }
+type TlsFunctionsArgsKey struct {
+	Pid uint32
+	Id  int64
+}
+type TlsReadArgsData struct {
+	Conn_pointer uint64
+	B_data       uint64
+}
+type TlsWriteArgsData struct {
+	Conn_pointer uint64
+	B_data       uint64
+	B_len        uint64
+}
