0c43940f53b84d560368f105cc458563e217b603
amitslavin <amit.slavin@datadoghq.com>
2025-07-16 10:11:05 +0300

    fix ut and cleanup

--- pkg/network/ebpf/c/runtime/usm.c
+++ pkg/network/ebpf/c/runtime/usm.c
@@ -307,8 +307,9 @@ int BPF_BYPASSABLE_UPROBE(uprobe__crypto_tls_Conn_Close) {
         return 0;
     }
 
-    // Clear the element in the map since this connection is closed
+    // Clear both the forward and reverse mappings since this connection is closed
     bpf_map_delete_elem(&conn_tup_by_go_tls_conn, &conn_pointer);
+    bpf_map_delete_elem(&go_tls_conn_by_tuple, t);
 
     conn_tuple_t copy = *t;
     // tls_finish can launch a tail call, thus cleanup should be done before.
