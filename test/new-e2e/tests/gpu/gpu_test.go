4c4e12a6dc35e50d7f282ee30f33baee67c0301c
Valeri Pliskin <valeri.pliskin@datadoghq.com>
2025-07-16 10:09:50 +0100

    running some workload to get gpu assigned to container and thus get the container tags to gpu metrics

--- test/new-e2e/tests/gpu/gpu_test.go
+++ test/new-e2e/tests/gpu/gpu_test.go
@@ -394,6 +394,8 @@ func (v *gpuBaseSuite[Env]) TestNvmlMetricsPresent() {
 	var env Env
 	isKubernetes := reflect.TypeOf(env) == reflect.TypeOf(environments.Kubernetes{})
 
+	_ = v.runCudaDockerWorkload()
+
 	// Nvml metrics are always being collected
 	v.EventuallyWithT(func(c *assert.CollectT) {
 
