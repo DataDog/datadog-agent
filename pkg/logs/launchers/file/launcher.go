285f2cdab0ab4341a2bb0a4d6dd27423fd87343c
Lucas Tembras <lucas.tembras@datadoghq.com>
2025-07-17 10:11:28 -0400

    style: added comments to debug SMP results

--- pkg/logs/launchers/file/launcher.go
+++ pkg/logs/launchers/file/launcher.go
@@ -128,18 +128,24 @@ func (s *Launcher) run() {
 		close(s.done)
 	}()
 
+	log.Debugf("File launcher started with scan period: %v, tailing limit: %d", s.scanPeriod, s.tailingLimit)
+
 	for {
 		select {
 		case source := <-s.addedSources:
+			log.Debugf("Received new source to add: %s", source.Name)
 			s.addSource(source)
 		case source := <-s.removedSources:
+			log.Debugf("Received source to remove: %s", source.Name)
 			s.removeSource(source)
 		case <-scanTicker.C:
 			atomic.AddInt64(&s.scanCount, 1)
+			log.Debugf("Starting scan iteration #%d", atomic.LoadInt64(&s.scanCount))
 			s.cleanUpRotatedTailers()
 			// check if there are new files to tail, tailers to stop and tailer to restart because of file rotation
 			s.scan()
 		case <-s.stop:
+			log.Debugf("Received stop signal, cleaning up launcher")
 			// no more file should be tailed
 			s.cleanup()
 			log.Infof("Total scan runs: %d", s.GetScanCount())
@@ -150,18 +156,22 @@ func (s *Launcher) run() {
 
 // cleanup all tailers
 func (s *Launcher) cleanup() {
+	log.Debugf("Starting cleanup of all tailers")
 	stopper := startstop.NewParallelStopper()
 	s.cleanUpRotatedTailers()
 	for _, tailer := range s.rotatedTailers {
+		log.Debugf("Adding rotated tailer to cleanup: %s", tailer.GetId())
 		stopper.Add(tailer)
 	}
 	s.rotatedTailers = []*tailer.Tailer{}
 
 	for _, tailer := range s.tailers.All() {
+		log.Debugf("Adding active tailer to cleanup: %s", tailer.GetId())
 		stopper.Add(tailer)
 		s.tailers.Remove(tailer)
 	}
 	stopper.Stop()
+	log.Debugf("Cleanup completed")
 }
 
 // scan checks all the files we're expected to tail, compares them to the currently tailed files,
@@ -287,41 +297,61 @@ func (s *Launcher) scan() {
 		isTailed := s.tailers.Contains(scanKey)
 		if !isTailed && tailersLen < s.tailingLimit {
 			checkSumEnabled := pkgconfigsetup.Datadog().GetString("logs_config.fingerprint_strategy")
-			if checkSumEnabled == "checksum" {
+			log.Debugf("Processing file %s (scanKey: %s, isTailed: %v, tailersLen: %d, limit: %d, checksum: %s)",
+				file.Path, scanKey, isTailed, tailersLen, s.tailingLimit, checkSumEnabled)
 
-				if tailer.ComputeFingerprint(file.Path, tailer.ReturnFingerprintConfig()) == 0 {
+			if checkSumEnabled == "checksum" {
+				log.Debugf("Checksum fingerprinting enabled for file %s", file.Path)
+
+				fingerprint := tailer.ComputeFingerprint(file.Path, tailer.ReturnFingerprintConfig())
+				log.Debugf("Computed fingerprint for file %s: 0x%x", file.Path, fingerprint)
+
+				if fingerprint == 0 {
+					log.Debugf("Skipping file %s - fingerprint returned 0 (insufficient data)", file.Path)
 					continue
 				} else {
-					fingerprint := tailer.ComputeFingerprint(file.Path, tailer.ReturnFingerprintConfig())
-					log.Infof("File %s has valid fingerprint: 0x%x", file.Path, fingerprint)
+					log.Debugf("File %s has valid fingerprint: 0x%x, proceeding with tailer creation", file.Path, fingerprint)
 				}
 
 				// Check if we have stored info from previous rotation and use it
 				var succeeded bool
 				if oldInfo, exists := s.oldInfoMap[file.Path]; exists {
+					log.Debugf("Using stored info for file %s (pattern: %v)", file.Path, oldInfo.Pattern != nil)
 					succeeded = s.startNewTailerWithStoredInfo(file, config.Beginning, oldInfo)
 					// Remove from map after use to prevent stale data
 					delete(s.oldInfoMap, file.Path)
 				} else {
+					log.Debugf("No stored info found for file %s, creating new tailer", file.Path)
 					succeeded = s.startNewTailer(file, config.Beginning)
 				}
 				if !succeeded {
+					log.Debugf("Failed to start tailer for file %s, will retry in next scan", file.Path)
 					// the setup failed, let's try to tail this file in the next scan
 					continue
 				}
+				log.Debugf("Successfully created tailer for file %s", file.Path)
 				tailersLen++
 				filesTailed[scanKey] = true
 				continue
 			} else {
+				log.Debugf("Checksum fingerprinting disabled for file %s, creating tailer without fingerprint check", file.Path)
 				// create a new tailer tailing from the beginning of the file if no offset has been recorded
 				succeeded := s.startNewTailer(file, config.Beginning)
 				if !succeeded {
+					log.Debugf("Failed to start tailer for file %s (non-checksum mode), will retry in next scan", file.Path)
 					// the setup failed, let's try to tail this file in the next scan
 					continue
 				}
+				log.Debugf("Successfully created tailer for file %s (non-checksum mode)", file.Path)
 				tailersLen++
 				filesTailed[scanKey] = true
 				continue
+			}
+		} else {
+			if isTailed {
+				log.Debugf("File %s is already being tailed, skipping", file.Path)
+			} else {
+				log.Debugf("File %s skipped - tailersLen (%d) >= limit (%d)", file.Path, tailersLen, s.tailingLimit)
 			}
 		}
 	}
@@ -336,36 +366,48 @@ func (s *Launcher) scan() {
 
 // cleanUpRotatedTailers removes any rotated tailers that have stopped from the list
 func (s *Launcher) cleanUpRotatedTailers() {
+	log.Debugf("Cleaning up rotated tailers, current count: %d", len(s.rotatedTailers))
 	pendingTailers := []*tailer.Tailer{}
 	for _, tailer := range s.rotatedTailers {
 		if !tailer.IsFinished() {
+			log.Debugf("Keeping rotated tailer: %s (not finished)", tailer.GetId())
 			pendingTailers = append(pendingTailers, tailer)
+		} else {
+			log.Debugf("Removing finished rotated tailer: %s", tailer.GetId())
 		}
 	}
 	s.rotatedTailers = pendingTailers
+	log.Debugf("Rotated tailers cleanup completed, remaining: %d", len(s.rotatedTailers))
 }
 
 // addSource keeps track of the new source and launch new tailers for this source.
 func (s *Launcher) addSource(source *sources.LogSource) {
+	log.Debugf("Adding source: %s (path: %s)", source.Name, source.Config.Path)
 	s.activeSources = append(s.activeSources, source)
 	s.launchTailers(source)
+	log.Debugf("Active sources count after adding: %d", len(s.activeSources))
 }
 
 // removeSource removes the source from cache.
 func (s *Launcher) removeSource(source *sources.LogSource) {
+	log.Debugf("Removing source: %s", source.Name)
 	for i, src := range s.activeSources {
 		if src == source {
+			log.Debugf("Found source at index %d, removing from active sources", i)
 			// no need to stop the tailer here, it will be stopped in the next iteration of scan.
 			s.activeSources = slices.Delete(s.activeSources, i, i+1)
 			break
 		}
 	}
+	log.Debugf("Active sources count after removing: %d", len(s.activeSources))
 }
 
 // launch launches new tailers for a new source.
 func (s *Launcher) launchTailers(source *sources.LogSource) {
+	log.Debugf("Launching tailers for source: %s", source.Name)
 	// If we're at the limit already, no need to do a 'CollectFiles', just wait for the next 'scan'
 	if s.tailers.Count() >= s.tailingLimit {
+		log.Debugf("Skipping tailer launch - at limit (%d/%d)", s.tailers.Count(), s.tailingLimit)
 		return
 	}
 	files, err := s.fileProvider.CollectFiles(source)
@@ -374,15 +416,19 @@ func (s *Launcher) launchTailers(source *sources.LogSource) {
 		log.Warnf("Could not collect files: %v", err)
 		return
 	}
+	log.Debugf("Collected %d files for source: %s", len(files), source.Name)
 	for _, file := range files {
 		if s.tailers.Count() >= s.tailingLimit {
+			log.Debugf("Reached tailing limit (%d), stopping file collection", s.tailingLimit)
 			return
 		}
 
 		if fileprovider.ShouldIgnore(s.validatePodContainerID, file) {
+			log.Debugf("Skipping ignored file: %s", file.Path)
 			continue
 		}
 		if tailer, isTailed := s.tailers.Get(file.GetScanKey()); isTailed {
+			log.Debugf("File already being tailed, updating source: %s", file.Path)
 			// new source inherits the old source's status
 			source.Status = tailer.Source().Status
 			// the file is already tailed, update the existing tailer's source so that the tailer
@@ -395,8 +441,10 @@ func (s *Launcher) launchTailers(source *sources.LogSource) {
 		if !isSet && source.Config.Identifier != "" {
 			mode = config.Beginning
 			source.Config.TailingMode = mode.String()
+			log.Debugf("Set default tailing mode to Beginning for source with identifier: %s", source.Config.Identifier)
 		}
 
+		log.Debugf("Starting new tailer for file: %s with mode: %v", file.Path, mode)
 		s.startNewTailer(file, mode)
 	}
 }
@@ -409,6 +457,7 @@ func (s *Launcher) startNewTailer(file *tailer.File, m config.TailingMode) bool 
 		return false
 	}
 
+	log.Debugf("Creating new tailer for file: %s with mode: %v", file.Path, m)
 	channel, monitor := s.pipelineProvider.NextPipelineChanWithMonitor()
 	tailer := s.createTailer(file, channel, monitor)
 
@@ -428,6 +477,7 @@ func (s *Launcher) startNewTailer(file *tailer.File, m config.TailingMode) bool 
 	}
 
 	s.tailers.Add(tailer)
+	log.Debugf("Successfully added tailer for file: %s, total tailers: %d", file.Path, s.tailers.Count())
 	return true
 }
 
@@ -438,21 +488,26 @@ func (s *Launcher) startNewTailerWithStoredInfo(file *tailer.File, m config.Tail
 		return false
 	}
 
+	log.Debugf("Creating new tailer with stored info for file: %s", file.Path)
 	channel, monitor := s.pipelineProvider.NextPipelineChanWithMonitor()
 
 	// Use stored InfoRegistry if available, otherwise create new one
 	var tailerInfo *status.InfoRegistry
 	if oldInfo.InfoRegistry != nil {
+		log.Debugf("Using stored InfoRegistry for file: %s", file.Path)
 		tailerInfo = oldInfo.InfoRegistry
 	} else {
+		log.Debugf("Creating new InfoRegistry for file: %s", file.Path)
 		tailerInfo = status.NewInfoRegistry()
 	}
 
 	// Create decoder with stored pattern if available
 	var decoderInstance *decoder.Decoder
 	if oldInfo.Pattern != nil {
+		log.Debugf("Using stored pattern for decoder: %s", file.Path)
 		decoderInstance = decoder.NewDecoderFromSourceWithPattern(file.Source, oldInfo.Pattern, tailerInfo)
 	} else {
+		log.Debugf("Creating decoder without stored pattern for file: %s", file.Path)
 		decoderInstance = decoder.NewDecoderFromSource(file.Source, tailerInfo)
 	}
 
@@ -485,6 +540,7 @@ func (s *Launcher) startNewTailerWithStoredInfo(file *tailer.File, m config.Tail
 	}
 
 	s.tailers.Add(tailer)
+	log.Debugf("Successfully added tailer with stored info for file: %s", file.Path)
 	return true
 }
 
@@ -494,7 +550,9 @@ func (s *Launcher) startNewTailerWithStoredInfo(file *tailer.File, m config.Tail
 // If (1) then the resulting tailing mode if "beginning" in order to honor existing offset to avoid duplicated lines to be sent.
 // If (2) then the resulting tailing mode is "forceEnd" to drop any saved offset and tail from the end of the file.
 func (s *Launcher) handleTailingModeChange(tailerID string, currentTailingMode config.TailingMode) config.TailingMode {
+	log.Debugf("Handling tailing mode change for tailer: %s, current mode: %v", tailerID, currentTailingMode)
 	if currentTailingMode == config.ForceBeginning || currentTailingMode == config.ForceEnd {
+		log.Debugf("Using forced mode: %v", currentTailingMode)
 		return currentTailingMode
 	}
 	previousMode, _ := config.TailingModeFromString(s.registry.GetTailingMode(tailerID))
@@ -502,32 +560,42 @@ func (s *Launcher) handleTailingModeChange(tailerID string, currentTailingMode c
 		log.Infof("Tailing mode changed for %v. Was: %v: Now: %v", tailerID, previousMode, currentTailingMode)
 		if currentTailingMode == config.Beginning {
 			// end -> beginning, the offset will be honored if it exists
+			log.Debugf("Mode changed from end to beginning, honoring existing offset")
 			return config.Beginning
 		}
 		// beginning -> end, the offset will be ignored
+		log.Debugf("Mode changed from beginning to end, ignoring existing offset")
 		return config.ForceEnd
 	}
+	log.Debugf("No mode change detected, using current mode: %v", currentTailingMode)
 	return currentTailingMode
 }
 
 // stopTailer stops the tailer
 func (s *Launcher) stopTailer(tailer *tailer.Tailer) {
+	log.Debugf("Stopping tailer: %s", tailer.GetId())
 	go tailer.Stop()
 	s.tailers.Remove(tailer)
+	log.Debugf("Removed tailer from container: %s", tailer.GetId())
 }
 
 // restartTailer safely stops tailer and starts a new one
 // returns true if the new tailer is up and running, false if an error occurred
 func (s *Launcher) restartTailerAfterFileRotation(oldTailer *tailer.Tailer, file *tailer.File) bool {
 	log.Info("Log rotation happened to ", file.Path)
+	log.Debugf("Restarting tailer after rotation for file: %s", file.Path)
 	oldTailer.StopAfterFileRotation()
 
 	oldRegexPattern := oldTailer.GetDetectedPattern()
 	oldInfoRegistry := oldTailer.GetInfo()
 
+	log.Debugf("Stored pattern from old tailer: %v", oldRegexPattern != nil)
+	log.Debugf("Stored info registry from old tailer: %v", oldInfoRegistry != nil)
+
 	// Only store info if we're using checksum fingerprinting (where it will be retrieved)
 	checkSumEnabled := pkgconfigsetup.Datadog().GetString("logs_config.fingerprint_strategy")
 	if checkSumEnabled == "checksum" && (oldRegexPattern != nil || oldInfoRegistry != nil) {
+		log.Debugf("Storing old tailer info for checksum fingerprinting")
 		regexAndRegistry := &oldTailerInfo{
 			InfoRegistry: oldInfoRegistry,
 			Pattern:      oldRegexPattern,
@@ -547,11 +615,13 @@ func (s *Launcher) restartTailerAfterFileRotation(oldTailer *tailer.Tailer, file
 	// We will keep track of the rotated tailer until it is finished.
 	s.rotatedTailers = append(s.rotatedTailers, oldTailer)
 	s.tailers.Add(newTailer)
+	log.Debugf("Successfully restarted tailer after rotation for file: %s", file.Path)
 	return true
 }
 
 // createTailer returns a new initialized tailer
 func (s *Launcher) createTailer(file *tailer.File, outputChan chan *message.Message, capacityMonitor *metrics.CapacityMonitor) *tailer.Tailer {
+	log.Debugf("Creating tailer for file: %s", file.Path)
 	tailerInfo := status.NewInfoRegistry()
 
 	tailerOptions := &tailer.TailerOptions{
@@ -565,13 +635,18 @@ func (s *Launcher) createTailer(file *tailer.File, outputChan chan *message.Mess
 		Registry:        s.registry,
 	}
 
-	return tailer.NewTailer(tailerOptions)
+	tailer := tailer.NewTailer(tailerOptions)
+	log.Debugf("Successfully created tailer for file: %s", file.Path)
+	return tailer
 }
 
 func (s *Launcher) createRotatedTailer(t *tailer.Tailer, file *tailer.File, pattern *regexp.Regexp) *tailer.Tailer {
+	log.Debugf("Creating rotated tailer for file: %s", file.Path)
 	tailerInfo := t.GetInfo()
 	channel, monitor := s.pipelineProvider.NextPipelineChanWithMonitor()
-	return t.NewRotatedTailer(file, channel, monitor, decoder.NewDecoderFromSourceWithPattern(file.Source, pattern, tailerInfo), tailerInfo, s.tagger, s.registry)
+	newTailer := t.NewRotatedTailer(file, channel, monitor, decoder.NewDecoderFromSourceWithPattern(file.Source, pattern, tailerInfo), tailerInfo, s.tagger, s.registry)
+	log.Debugf("Successfully created rotated tailer for file: %s", file.Path)
+	return newTailer
 }
 
 //nolint:revive // TODO(AML) Fix revive linter
--- pkg/logs/launchers/file/position.go
+++ pkg/logs/launchers/file/position.go
@@ -12,6 +12,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/logs/agent/config"
 	auditor "github.com/DataDog/datadog-agent/comp/logs/auditor/def"
 	tailerfile "github.com/DataDog/datadog-agent/pkg/logs/tailers/file"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
 
 // Position returns the position from where logs should be collected.
@@ -19,6 +20,8 @@ func Position(registry auditor.Registry, identifier string, mode config.TailingM
 	var offset int64
 	var whence int
 	var err error
+
+	log.Debugf("Position calculation started for identifier: %s, mode: %v", identifier, mode)
 
 	value := registry.GetOffset(identifier)
 	fingerprintConfig := registry.GetFingerprintConfig(identifier)
@@ -29,29 +32,47 @@ func Position(registry auditor.Registry, identifier string, mode config.TailingM
 	}
 	newFingerprint := tailerfile.ComputeFingerprint(filePath, fingerprintConfig)
 
+	log.Debugf("Position calculation for identifier %s (filePath: %s, mode: %v)", identifier, filePath, mode)
+	log.Debugf("  - Stored offset: %s", value)
+	log.Debugf("  - Fingerprint config: %v", fingerprintConfig != nil)
+	log.Debugf("  - Previous fingerprint: 0x%x", previousFingerprint)
+	log.Debugf("  - New fingerprint: 0x%x", newFingerprint)
+
 	switch {
 	case mode == config.ForceBeginning:
+		log.Debugf("Using ForceBeginning mode - offset: 0, whence: SeekStart")
 		offset, whence = 0, io.SeekStart
 	case mode == config.ForceEnd:
+		log.Debugf("Using ForceEnd mode - offset: 0, whence: SeekEnd")
 		offset, whence = 0, io.SeekEnd
-	case value != "" && fingerprintConfig != nil && previousFingerprint == newFingerprint: //and fingerprint valid (fingerprint stored oldconfig and recalculate oldconfig the same)
+	case value != "" && previousFingerprint == newFingerprint: //and fingerprint valid (fingerprint stored oldconfig and recalculate oldconfig the same)
+		log.Debugf("Using stored offset with valid fingerprint - offset: %s, whence: SeekStart", value)
 		// an offset was registered, tailing mode is not forced, tail from the offset
 		whence = io.SeekStart
 		offset, err = strconv.ParseInt(value, 10, 64)
 		if err != nil {
+			log.Debugf("Failed to parse stored offset '%s': %v, falling back to mode-based positioning", value, err)
 			offset = 0
 			if mode == config.End {
 				whence = io.SeekEnd
+				log.Debugf("Falling back to End mode due to offset parse error")
 			} else if mode == config.Beginning {
 				whence = io.SeekStart
+				log.Debugf("Falling back to Beginning mode due to offset parse error")
 			}
+		} else {
+			log.Debugf("Successfully parsed stored offset: %d", offset)
 		}
 	case mode == config.Beginning:
+		log.Debugf("Using Beginning mode - offset: 0, whence: SeekStart")
 		offset, whence = 0, io.SeekStart
 	case mode == config.End:
+		log.Debugf("Using End mode (default) - offset: 0, whence: SeekEnd")
 		fallthrough
 	default:
 		offset, whence = 0, io.SeekEnd
 	}
+
+	log.Debugf("Final position for %s: offset=%d, whence=%d", identifier, offset, whence)
 	return offset, whence, err
 }
