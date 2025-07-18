50f07b524be845c3cfcedba831e7fd431438be7f
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-07-18 14:20:57 +0200

    [test-e2e-cleanup-worker] Updated attrs format for boolean operations

--- tasks/tools/e2e_stacks.py
+++ tasks/tools/e2e_stacks.py
@@ -10,5 +10,5 @@ def destroy_remote_stack(ctx, stack: str):
         "stackcleaner/stack",
         env="prod",
         ty="stackcleaner_workflow_request",
-        attrs=f"stack_name={stack},job_name={os.environ['CI_JOB_NAME']},job_id={os.environ['CI_JOB_ID']},pipeline_id={os.environ['CI_PIPELINE_ID']},ref={os.environ['CI_COMMIT_REF_NAME']},ignore_lock=true,ignore_not_found=true",
+        attrs=f"stack_name={stack},job_name={os.environ['CI_JOB_NAME']},job_id={os.environ['CI_JOB_ID']},pipeline_id={os.environ['CI_PIPELINE_ID']},ref={os.environ['CI_COMMIT_REF_NAME']},ignore_lock=bool:true,ignore_not_found=bool:true",
     )
