5b12aecd533be0e024bb61d79c36dd7b451842b5
grantseltzer <grantseltzer@gmail.com>
2025-07-19 14:45:15 -0500

    Add test cases

--- pkg/dyninst/testprogs/progs/sample/maps.go
+++ pkg/dyninst/testprogs/progs/sample/maps.go
@@ -23,6 +23,14 @@ func testMapStringToInt(m map[string]int) {}
 
 //nolint:all
 //go:noinline
+func testMapStringToSlice(m map[string][]string) {}
+
+//nolint:all
+//go:noinline
+func testMapArrayToArray(m map[[4]string][2]int) {}
+
+//nolint:all
+//go:noinline
 func testSmallMap(m map[string]int) {}
 
 //nolint:all
@@ -65,4 +73,10 @@ func executeMapFuncs() {
 	testArrayOfMaps([2]map[string]int{{"foo": 1, "bar": 2}, {"foo": 1, "bar": 2}})
 	testStructWithMap(structWithMap{map[int]int{1: 1}})
 	testPointerToMap(&map[string]int{"foo": 1, "bar": 2})
+	testMapStringToSlice(map[string][]string{"foo": {"one", "two"}, "bar": {"three", "four"}})
+	testMapArrayToArray(map[[4]string][2]int{
+		[4]string{"foo", "bar", "baz", "qux"}:          {1, 2},
+		[4]string{"quux", "quuz", "corge", "grault"}:   {3, 4},
+		[4]string{"grault", "garply", "waldo", "fred"}: {5, 6},
+	})
 }
