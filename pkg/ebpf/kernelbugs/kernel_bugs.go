08bcf2c0f4e9f9ec7a043ebb068f008045ccb425
Usama Saqib <usamasaqib.96@live.com>
2025-07-24 17:07:53 +0200

    Detect uretprobe seccomp bug (#38876)
    Co-authored-by: gjulianm <guillermo.julian@datadoghq.com>
    (cherry picked from commit a980511b782ad3f0b0ab6e734a34315ca2c85141)

--- pkg/ebpf/kernelbugs/kernel_bugs.go
+++ pkg/ebpf/kernelbugs/kernel_bugs.go
@@ -3,14 +3,21 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package ebpf
+//go:build linux
+
+// Package kernelbugs provides runtime detection for kernel bugs effecting system-probe
+package kernelbugs
+
+import (
+	ddebpf "github.com/DataDog/datadog-agent/pkg/ebpf"
+)
 
 // HasTasksRCUExitLockSymbol returns true if the tasks_rcu_exit_srcu symbol is found in the kernel symbols.
 // The tasks_rcu_exit_srcu lock might cause a deadlock when removing fentry trampolines.
 // This was fixed by https://github.com/torvalds/linux/commit/1612160b91272f5b1596f499584d6064bf5be794
 func HasTasksRCUExitLockSymbol() (bool, error) {
 	const tasksRCUExitLockSymbol = "tasks_rcu_exit_srcu"
-	missingSymbols, err := VerifyKernelFuncs(tasksRCUExitLockSymbol)
+	missingSymbols, err := ddebpf.VerifyKernelFuncs(tasksRCUExitLockSymbol)
 	if err != nil {
 		return false, err
 	}
