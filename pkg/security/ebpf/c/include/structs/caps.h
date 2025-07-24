d2c22e6e315475631f5b3ff8f55b90d0eb45f6ff
YoannGh <yoann.ghigoff@datadoghq.com>
2025-07-24 11:59:49 +0200

    pack capabilities_usage_entry_t struct

--- pkg/security/ebpf/c/include/structs/caps.h
+++ pkg/security/ebpf/c/include/structs/caps.h
@@ -3,7 +3,7 @@
 
 struct capabilities_context_t {
     u64 cap_as_mask; // bitmask of capabilities that are being checked in the current task context
-    u64 override_creds_depth; // depth of override_creds calls, used to track whether capability checks are performed against overridden credentials
+    u64 override_creds_depth; // depth of override_creds calls, used to track if the capability checks are performed against user capabilities
 };
 
 struct capabilities_usage_t {
@@ -16,10 +16,34 @@ struct capabilities_usage_key_t {
     u64 tgid;
 };
 
+#define CAPABILITIES_USAGE_ENTRY_DIRTY_MASK 1ULL
+#define CAPABILITIES_USAGE_ENTRY_LAST_SENT_MASK (~CAPABILITIES_USAGE_ENTRY_DIRTY_MASK)
+
 struct capabilities_usage_entry_t {
     struct capabilities_usage_t usage;
-    u64 last_sent_ns; // timestamp of the last time this entry was sent to userspace
-    u8 dirty; // indicates that the entry has been updated since the last time it was sent to userspace
+    u64 data; // data is representing both the `dirty` flag and the `last_sent_ns` timestamp
 };
+
+__attribute__((always_inline)) bool is_dirty(struct capabilities_usage_entry_t *entry) {
+    return (entry->data & CAPABILITIES_USAGE_ENTRY_DIRTY_MASK) != 0;
+}
+
+__attribute__((always_inline)) void update_dirty(struct capabilities_usage_entry_t *entry, bool dirty) {
+    entry->data |= (dirty ? CAPABILITIES_USAGE_ENTRY_DIRTY_MASK : 0);
+}
+
+__attribute__((always_inline)) bool period_reached_or_new_entry(struct capabilities_usage_entry_t *entry, u64 now) {
+    now = now & CAPABILITIES_USAGE_ENTRY_LAST_SENT_MASK; // Clear the dirty flag
+    u64 last_sent_ns = entry->data & CAPABILITIES_USAGE_ENTRY_LAST_SENT_MASK;
+    return last_sent_ns == 0 || ((now - last_sent_ns) >= get_capabilities_monitoring_period());
+}
+
+__attribute__((always_inline)) void reset_dirty(struct capabilities_usage_entry_t *entry) {
+    entry->data &= ~CAPABILITIES_USAGE_ENTRY_DIRTY_MASK;
+}
+
+__attribute__((always_inline)) void set_last_sent_ns(struct capabilities_usage_entry_t *entry, u64 ts) {
+    entry->data = (entry->data & CAPABILITIES_USAGE_ENTRY_DIRTY_MASK) | (ts & CAPABILITIES_USAGE_ENTRY_LAST_SENT_MASK);
+}
 
 #endif // _STRUCTS_CAPS_H_
