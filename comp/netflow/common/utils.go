442fa4f8e5b6be1f823d8311cc5d30fb623361ba
Jim Wilson <86026167+jmw51798@users.noreply.github.com>
2025-07-29 11:25:00 -0600

    Add Sizeof() and MemUsage() utility functions for debug, performance and scalability use.

--- comp/netflow/common/utils.go
+++ comp/netflow/common/utils.go
@@ -7,6 +7,8 @@ package common
 
 import (
 	"cmp"
+	"reflect"
+	"runtime"
 )
 
 // Min returns the smaller of two items, for any ordered type.
@@ -23,4 +25,88 @@ func Max[T cmp.Ordered](a T, b T) T {
 		return a
 	}
 	return b
+}
+
+// Sizeof estimates the total memory size (in bytes) of any Go value.
+// It traverses structs, pointers, slices, maps, and interfaces recursively.
+func Sizeof(val any) int {
+	seen := make(map[uintptr]bool) // Track visited pointers to avoid cycles
+	return estimate(reflect.ValueOf(val), seen)
+}
+
+func estimate(val reflect.Value, seen map[uintptr]bool) int {
+	if !val.IsValid() {
+		return 0
+	}
+
+	// Handle pointers and avoid duplicate visits (cycle-safe)
+	if val.Kind() == reflect.Ptr {
+		if val.IsNil() {
+			return 0
+		}
+		ptr := val.Pointer()
+		if seen[ptr] {
+			return 0 // already visited
+		}
+		seen[ptr] = true
+		return int(val.Type().Size()) + estimate(val.Elem(), seen)
+	}
+
+	switch val.Kind() {
+	case reflect.Struct:
+		size := int(val.Type().Size()) // includes padding
+		for i := 0; i < val.NumField(); i++ {
+			if val.Type().Field(i).IsExported() {
+				size += estimate(val.Field(i), seen)
+			}
+		}
+		return size
+
+	case reflect.Slice:
+		if val.IsNil() {
+			return 0
+		}
+		size := int(val.Type().Size()) // slice header
+		for i := 0; i < val.Len(); i++ {
+			size += estimate(val.Index(i), seen)
+		}
+		return size
+
+	case reflect.Array:
+		size := 0
+		for i := 0; i < val.Len(); i++ {
+			size += estimate(val.Index(i), seen)
+		}
+		return size
+
+	case reflect.String:
+		return int(val.Type().Size()) + val.Len()
+
+	case reflect.Map:
+		if val.IsNil() {
+			return 0
+		}
+		size := int(val.Type().Size()) // map header
+		for _, key := range val.MapKeys() {
+			size += estimate(key, seen)
+			size += estimate(val.MapIndex(key), seen)
+		}
+		return size
+
+	case reflect.Interface:
+		if val.IsNil() {
+			return 0
+		}
+		return int(val.Type().Size()) + estimate(val.Elem(), seen)
+
+	default:
+		return int(val.Type().Size()) // basic value (int, bool, float, etc.)
+	}
+}
+
+// MemUsage returns the current memory usage of the Go runtime in bytes.
+func MemUsage() uint64 {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	return m.Alloc
 }
