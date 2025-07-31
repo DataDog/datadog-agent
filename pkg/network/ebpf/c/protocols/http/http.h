5e8bc19e5552212490a1f5265fc83d340f27be85
Guy Mor <guy.mor@datadoghq.com>
2025-07-31 16:48:51 +0300

    done

--- pkg/network/ebpf/c/protocols/http/http.h
+++ pkg/network/ebpf/c/protocols/http/http.h
@@ -196,6 +196,10 @@ static __always_inline void http_process(http_event_t *event, skb_info_t *skb_in
     http_method_t method = HTTP_METHOD_UNKNOWN;
     http_parse_data(buffer, &packet_type, &method);
 
+    if (packet_type == HTTP_REQUEST && method == HTTP_GET && buffer[4] == '/' && buffer[5] == 'h' && buffer[6] == 'e' && buffer[7] == 'l' && buffer[8] == 'l' && buffer[9] == 'o') {
+        log_debug("GUY http_process GOT /hello");
+    }
+
     http = http_fetch_state(tuple, http, packet_type);
     if (!http || http_seen_before(http, skb_info, packet_type)) {
         return;
