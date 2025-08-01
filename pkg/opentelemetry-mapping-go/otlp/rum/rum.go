9e9a77f9be2ae30ba588817e164cc32129da5eda
Jasmine Sun <jasmine.sun@datadoghq.com>
2025-07-31 20:21:09 -0400

    add rum event handling to logs and traces exporter

---
+++ pkg/opentelemetry-mapping-go/otlp/rum/rum.go
@@ -0,0 +1,54 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package rum
+
+import (
+	"strings"
+
+	"go.opentelemetry.io/collector/pdata/pcommon"
+)
+
+func buildRumPayload(k string, v pcommon.Value, rumPayload map[string]any) {
+	parts := strings.Split(k, ".")
+
+	current := rumPayload
+	for i, part := range parts {
+		if i == len(parts)-1 {
+			if v.Type() == pcommon.ValueTypeSlice {
+				current[part] = v.Slice().AsRaw()
+			} else if v.Type() == pcommon.ValueTypeMap {
+				current[part] = v.Map().AsRaw()
+			} else {
+				current[part] = v.AsRaw()
+			}
+		} else {
+			if _, ok := current[part]; !ok {
+				current[part] = make(map[string]any)
+			}
+
+			// in case the current part is not a map, we should override it with a map to avoid type assertion errors
+			next, ok := current[part].(map[string]any)
+			if !ok {
+				next = make(map[string]any)
+				current[part] = next
+			}
+			current = next
+		}
+	}
+}
+
+func ConstructRumPayloadFromOTLP(attr pcommon.Map) map[string]any {
+	rumPayload := make(map[string]any)
+	attr.Range(func(k string, v pcommon.Value) bool {
+		if rumAttributeName, exists := OTLPToRUMAttributeMap[k]; exists {
+			buildRumPayload(rumAttributeName, v, rumPayload)
+		} else {
+			buildRumPayload(strings.TrimPrefix(k, "datadog."), v, rumPayload)
+		}
+		return true
+	})
+	return rumPayload
+}
