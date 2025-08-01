287036f01cc195b6622c7992f4ab1b0f9bb76163
maxime mouial <hush-hush@users.noreply.github.com>
2025-08-01 14:51:38 +0200

    Fix failing unit tests for hosts with a GPU (#39421)

--- pkg/gpu/tags/tags_dummy.go
+++ pkg/gpu/tags/tags_dummy.go
@@ -3,12 +3,15 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build linux && !test
+//go:build linux && test
 
 // Package tags provides gpu host tags to the host payload
 package tags
 
+// This is a dummy entry point into the module to avoid adding the gpu_host tags as host tags when running unit tests.
+// This prevent unit tests from failing depending on the type of host they're running on.
+
 // GetTags returns a slice of tags indicating GPU presence
 func GetTags() []string {
-	return getTags()
+	return nil
 }
