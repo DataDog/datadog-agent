c282e4bec2103e433119cee79f77426f5132676a
Gabriel Dos Santos <gabe.dossantos@datadoghq.com>
2025-08-04 12:50:32 +0000

    WorkloadFilter Docs

--- comp/core/workloadfilter/def/component.go
+++ comp/core/workloadfilter/def/component.go
@@ -14,10 +14,16 @@ package workloadfilter
 // If a set of filters produces an Include or Exclude result, then subsequent sets will not be evaluated.
 // Therefore, filters in lower-indexed groups will take precedence over those in higher-indexed groups.
 type Component interface {
+	// IsContainerExcluded returns true if the container is excluded by the selected container filter keys.
 	IsContainerExcluded(container *Container, containerFilters [][]ContainerFilter) bool
+	// IsPodExcluded returns true if the pod is excluded by the selected pod filter keys.
 	IsPodExcluded(pod *Pod, podFilters [][]PodFilter) bool
+	// IsServiceExcluded returns true if the service is excluded by the selected service filter keys.
 	IsServiceExcluded(service *Service, serviceFilters [][]ServiceFilter) bool
+	// IsEndpointExcluded returns true if the endpoint is excluded by the selected endpoint filter keys.
 	IsEndpointExcluded(endpoint *Endpoint, endpointFilters [][]EndpointFilter) bool
 
+	// GetContainerFilterInitializationErrors returns a list of errors
+	// encountered during the initialization of the selected container filters.
 	GetContainerFilterInitializationErrors(filters []ContainerFilter) []error
 }
