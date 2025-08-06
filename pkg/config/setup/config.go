a127fc588410d33ce3db2af8a9117b6ecd3d81f5
maxime mouial <hush-hush@users.noreply.github.com>
2025-08-06 16:36:40 +0200

    Add 'infrastructure_mode' setting to the Agent (#39548)

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -1180,6 +1180,8 @@ func agent(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("allow_arbitrary_tags", false)
 	config.BindEnvAndSetDefault("use_proxy_for_cloud_metadata", false)
 
+	config.BindEnvAndSetDefault("infrastructure_mode", "pro")
+
 	// Configuration for TLS for outgoing connections
 	config.BindEnvAndSetDefault("min_tls_version", "tlsv1.2")
 
