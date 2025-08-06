dbe948f3c5ff32d6537960d6455e8e04b4f9e38e
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2025-08-06 10:36:57 +0200

    [CWS] cgroup / containers rework (#38481)

--- pkg/security/ebpf/c/include/hooks/module.h
+++ pkg/security/ebpf/c/include/hooks/module.h
@@ -119,7 +119,7 @@ int __attribute__((always_inline)) trace_init_module_ret(void *ctx, int retval, 
     }
 
     struct proc_cache_t *entry = fill_process_context(&event.process);
-    fill_container_context(entry, &event.container);
+    fill_cgroup_context(entry, &event.cgroup);
     fill_span_context(&event.span);
 
     send_event(ctx, EVENT_INIT_MODULE, event);
@@ -179,7 +179,7 @@ int __attribute__((always_inline)) trace_delete_module_ret(void *ctx, int retval
     bpf_probe_read_str(&event.name, sizeof(event.name), (void *)syscall->delete_module.name);
 
     struct proc_cache_t *entry = fill_process_context(&event.process);
-    fill_container_context(entry, &event.container);
+    fill_cgroup_context(entry, &event.cgroup);
     fill_span_context(&event.span);
 
     send_event(ctx, EVENT_DELETE_MODULE, event);
