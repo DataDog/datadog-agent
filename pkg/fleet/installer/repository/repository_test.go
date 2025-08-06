b5bef6ca820acaacb97a64a0cc64d427f810059d
Paul Coignet <paul.coignet@datadoghq.com>
2025-08-06 16:52:18 +0200

    Fix

--- pkg/fleet/installer/repository/repository_test.go
+++ pkg/fleet/installer/repository/repository_test.go
@@ -522,9 +522,8 @@ func TestCopyDirectoryTargetDoesNotExist(t *testing.T) {
 	targetDir := filepath.Join(t.TempDir(), "non-existent-target")
 
 	err := copyDirectory(sourceDir, targetDir)
-	assert.NoError(t, err)
-
-	verifyDirectoryContent(t, targetDir, sourceFiles)
+	assert.Error(t, err)
+	assert.NoDirExists(t, targetDir)
 }
 
 func TestCopyDirectoryWithSpecialCharacters(t *testing.T) {
