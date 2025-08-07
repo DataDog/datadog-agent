29c9ea60c8ae183ceadb80667307c368ff085618
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2025-08-07 11:40:10 +0200

    update sleep timer

--- test/new-e2e/tests/agent-runtimes/hostname/drift_common_test.go
+++ test/new-e2e/tests/agent-runtimes/hostname/drift_common_test.go
@@ -47,7 +47,7 @@ func (v *baseHostnameDriftSuite) TestHostnameDriftMetricsEmission() {
 	}, 2*time.Minute, 10*time.Second, "Agent should be ready")
 
 	// Wait for the initial drift check to complete (initial delay + some buffer)
-	time.Sleep(45 * time.Second)
+	time.Sleep(5 * time.Second)
 
 	// Verify that hostname drift metrics are emitted using agent telemetry
 	v.EventuallyWithT(func(c *assert.CollectT) {
