42c696b208b3d4d2b1490beb44b689ea72385e40
Alex Lopez <alex.lopez@datadoghq.com>
2025-08-07 10:58:00 +0200

    Specify the exclusions without wildcards

--- omnibus/config/projects/iot-agent.rb
+++ omnibus/config/projects/iot-agent.rb
@@ -231,8 +231,7 @@ exclude '\.git*'
 exclude 'bundler\/git'
 
 # Exclude headers that are not needed in the final package
-# TODO(alopezz): figure out the right way to write these patterns
-exclude "**/embedded/include"
+exclude "embedded/include"
 
 if linux_target? or windows_target?
   strip_build windows_target? || !do_package
