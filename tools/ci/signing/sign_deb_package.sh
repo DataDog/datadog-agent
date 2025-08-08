4f6be678487d608a02263a13fb21fe52961d637f
Kylian Serrania <kylian.serrania@datadoghq.com>
2025-08-08 11:53:05 +0200

    Add signing scripts

---
+++ tools/ci/signing/sign_deb_package.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+package=$1
+keyid=$2
+outdir=$3
+
+log() {
+  local message=$1     # what to log, as a single string
+  local error_level=$2 # the log level that will prefix the message, defaults to INFO if empty
+
+  if [[ -z "$error_level" ]]; then
+    error_level=INFO
+  fi
+
+  echo "${error_level} [$(date "+%Y.%m.%d-%H:%M:%S %Z")] ${message}" 1>&2
+}
+
+tmpdir=$(mktemp -d)
+tmpfile="${tmpdir}"/$(basename "${package}")
+
+cp "${package}" "${tmpfile}"
+
+ar x "${tmpfile}"
+
+cat "${tmpdir}/debian-binary" "${tmpdir}/control.tar.*" "${tmpdir}/data.tar.*" > "${tmpdir}/complete"
+
+if ! (fakeroot gpg --armor --detach-sign --local-user "${keyid}" \
+    -o "${tmpdir}/_gpgorigin" "${tmpdir}/complete"); then
+    log "Failed to sign DEB file ${tmpfile}" ERROR
+    return 1
+else
+    log "Successfully signed DEB file ${tmpfile}"
+fi
+
+if ! fakeroot ar rc "${tmpfile}" "${tmpdir}/_gpgorigin"; then
+    log "Failed to add signature to DEB file ${tmpfile}" ERROR
+    return 1
+fi
+
+mkdir -p "${outdir}"
+
+cp "${tmpfile}" "${outdir}/"
+
+rm -rf "${tmpdir}"
