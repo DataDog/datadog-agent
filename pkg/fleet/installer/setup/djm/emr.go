499de109b5b72c1e6e0ab76db78ec61a78896092
aboitreaud <72934368+aboitreaud@users.noreply.github.com>
2025-08-08 19:00:10 +0200

    fix log

--- pkg/fleet/installer/setup/djm/emr.go
+++ pkg/fleet/installer/setup/djm/emr.go
@@ -92,13 +92,13 @@ func SetupEmr(s *common.Setup) error {
 		s.Out.WriteString("Setting up Spark integration config on the Resource Manager\n")
 		setupResourceManager(s, clusterName)
 		if os.Getenv("DD_EMR_DRIVER_LOGS_ENABLED") == "true" {
-			s.Out.WriteString("Enabling EMR logs collection based on env variable DD_EMR_LOGS_ENABLED=true\n")
+			s.Out.WriteString("Enabling EMR logs collection based on env variable DD_EMR_DRIVER_LOGS_ENABLED=true\n")
 			enableEmrLogs(s, false)
 		}
 	}
 	// Add logs config to both Resource Manager and Workers
 	if os.Getenv("DD_EMR_LOGS_ENABLED") == "true" {
-		s.Out.WriteString("Enabling EMR logs collection on the driver only based on env variable DD_EMR_DRIVER_LOGS=true\n")
+		s.Out.WriteString("Enabling EMR logs collection on the driver only based on env variable DD_EMR_LOGS=true\n")
 		enableEmrLogs(s, true)
 	} else if os.Getenv("DD_EMR_DRIVER_LOGS_ENABLED") != "true" {
 		s.Out.WriteString("EMR logs collection not enabled. To enable it, set DD_EMR_LOGS_ENABLED=true\n")
