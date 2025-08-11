8da21e26aa10763bf07b15dde80dcb765fb28294
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-08-11 14:15:51 +0200

    Print sys executable

---
+++ tasks/unit_tests.py
@@ -0,0 +1,13 @@
+import sys
+
+from invoke import task
+
+
+@task
+def invoke_unit_tests(ctx):
+    """
+    Run the unit tests on the invoke tasks
+    """
+    print("SYS EXECUTABLE", sys.executable)
+
+    ctx.run(f"{sys.executable} -m unittest discover -s tasks -p '*_tests.py'", env={"GITLAB_TOKEN": "fake_token"})
