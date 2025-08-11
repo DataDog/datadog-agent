99c909a1706e9a089fae8ab86adb29808a6541cb
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2025-08-11 13:29:12 -0400

    feat: [SVSL-7232] tracer payload modifier component for fx

---
+++ comp/trace/payload-modifier/impl/payloadmodifier.go
@@ -0,0 +1,52 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+// Package payloadmodifierimpl implements the trace payload modifier component
+package payloadmodifierimpl
+
+import (
+	"go.uber.org/fx"
+
+	coreconfig "github.com/DataDog/datadog-agent/comp/core/config"
+	payloadmodifier "github.com/DataDog/datadog-agent/comp/trace/payload-modifier/def"
+	serverlessenv "github.com/DataDog/datadog-agent/pkg/serverless/env"
+	serverlesstags "github.com/DataDog/datadog-agent/pkg/serverless/tags"
+	serverlesstrace "github.com/DataDog/datadog-agent/pkg/serverless/trace"
+	pkgagent "github.com/DataDog/datadog-agent/pkg/trace/agent"
+)
+
+type dependencies struct {
+	fx.In
+
+	Config coreconfig.Component
+}
+
+type component struct {
+	modifier pkgagent.TracerPayloadModifier
+}
+
+// NewComponent creates a new payload modifier component
+func NewComponent(deps dependencies) payloadmodifier.Component {
+	var modifier pkgagent.TracerPayloadModifier
+
+	// Only enable TracerPayloadModifier when running in Azure App Services
+	// extension. serverless-init also uses the serverless
+	// TracerPayloadModifier, but it instantiates its tracer without using fx,
+	// so we don't need to worry about that here.
+	if serverlessenv.IsAzureAppServicesExtension() {
+		functionTags := serverlesstags.GetFunctionTags(deps.Config)
+		modifier = serverlesstrace.NewTracerPayloadModifier(functionTags)
+	}
+
+	return &component{
+		modifier: modifier,
+	}
+}
+
+// GetModifier returns the TracerPayloadModifier instance, or nil if not enabled
+func (c *component) GetModifier() pkgagent.TracerPayloadModifier {
+	return c.modifier
+}
+
