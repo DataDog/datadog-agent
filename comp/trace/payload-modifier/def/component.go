fc53fd6880f06ed7af9bda95d6be9b80240ff263
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2025-08-11 13:29:12 -0400

    feat: [SVSL-7232] tracer payload modifier component for fx

---
+++ comp/trace/payload-modifier/def/component.go
@@ -0,0 +1,19 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+// Package payloadmodifier defines the trace payload modifier component interface
+package payloadmodifier
+
+import (
+	pkgagent "github.com/DataDog/datadog-agent/pkg/trace/agent"
+)
+
+// Component provides trace payload modification functionality
+type Component interface {
+	// GetModifier returns the TracerPayloadModifier instance
+	// Returns nil if payload modification is not enabled
+	GetModifier() pkgagent.TracerPayloadModifier
+}
+
