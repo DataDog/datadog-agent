f059c617d530aab7e7b735bf6116a6ff3a4f4474
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2025-08-12 18:06:25 +0200

    setup and start remote config stuff

--- comp/privateactionrunner/impl/privateactionrunner.go
+++ comp/privateactionrunner/impl/privateactionrunner.go
@@ -13,6 +13,11 @@ import (
 	log "github.com/DataDog/datadog-agent/comp/core/log/def"
 	compdef "github.com/DataDog/datadog-agent/comp/def"
 	privateactionrunner "github.com/DataDog/datadog-agent/comp/privateactionrunner/def"
+	"github.com/DataDog/datadog-agent/comp/remote-config/rcclient"
+	parconfig "github.com/DataDog/datadog-agent/pkg/privateactionrunner/config"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/opms"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/remoteconfig"
+	taskverifier "github.com/DataDog/datadog-agent/pkg/privateactionrunner/task-verifier"
 )
 
 // Requires defines the dependencies for the privateactionrunner component
@@ -21,6 +26,7 @@ type Requires struct {
 	Lifecycle compdef.Lifecycle
 	Logger    log.Component
 	Config    config.Component
+	RcClient  rcclient.Component
 }
 
 // Provides defines the output of the privateactionrunner component
@@ -28,31 +34,40 @@ type Provides struct {
 	Comp privateactionrunner.Component
 }
 
+type runnerImpl struct {
+	log          log.Component
+	config       config.Component
+	started      bool
+	keysManager  remoteconfig.KeysManager
+	TaskVerifier *taskverifier.TaskVerifier
+}
+
 // NewComponent creates a new privateactionrunner component
 func NewComponent(reqs Requires) (Provides, error) {
+
+	//ctx := context.Background()
+	cfg := parconfig.Config{}
+	opmsClient := opms.NewClient(&cfg)
+	println(opmsClient)
+	keysManager := remoteconfig.New(reqs.RcClient)
+
 	runner := &runnerImpl{
-		log:    reqs.Logger,
-		config: reqs.Config,
+		log:          reqs.Logger,
+		config:       reqs.Config,
+		keysManager:  keysManager,
+		TaskVerifier: taskverifier.NewTaskVerifier(keysManager, &cfg),
 	}
 
 	reqs.Lifecycle.Append(compdef.Hook{
 		OnStart: runner.Start,
 		OnStop:  runner.Stop,
 	})
-
-	provides := Provides{
+	return Provides{
 		Comp: runner,
-	}
-	return provides, nil
+	}, nil
 }
 
-type runnerImpl struct {
-	log     log.Component
-	config  config.Component
-	started bool
-}
-
-func (r *runnerImpl) Start(_ context.Context) error {
+func (r *runnerImpl) Start(ctx context.Context) error {
 	enabled := r.config.GetBool("privateactionrunner.enabled")
 	if !enabled {
 		r.log.Debug("privateactionrunner disabled")
@@ -60,10 +75,11 @@ func (r *runnerImpl) Start(_ context.Context) error {
 	}
 	r.log.Info("Starting private action runner")
 	r.started = true
+	r.keysManager.Start(ctx)
 	return nil
 }
 
-func (r *runnerImpl) Stop(_ context.Context) error {
+func (r *runnerImpl) Stop(ctx context.Context) error {
 	if !r.started {
 		return nil
 	}
