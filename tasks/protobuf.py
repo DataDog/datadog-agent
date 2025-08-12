118cf3cf534099ea93d99bfaa2fedd0737d9cbef
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2025-08-12 11:34:34 +0000

    Generate protos

--- tasks/protobuf.py
+++ tasks/protobuf.py
@@ -20,6 +20,7 @@ PROTO_PKGS = {
     'languagedetection': False,
     'remoteagent': False,
     'autodiscovery': False,
+    'privateactionrunner': False,
 }
 
 # maybe put this in a separate function
