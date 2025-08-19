84a346276acd1b36c97c6fefafbbd1855431a281
zoe ✨ <9274242+zoedt@users.noreply.github.com>
2025-08-19 16:06:53 -0400

    Add build tag for tests, stub for IOT agent in registered checks

--- pkg/collector/corechecks/networkconfigmanagement/networkconfigmanagement_test.go
+++ pkg/collector/corechecks/networkconfigmanagement/networkconfigmanagement_test.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
-//go:build test
+//go:build test && ncm
 
 package networkconfigmanagement
 
