1bc70be52902eb115644c7027d01b6665f6af6b4
Minyi Zhu <minyi.zhu@datadoghq.com>
2025-08-20 10:32:55 -0400

    address comment

--- comp/metadata/clusterchecks/mock/mock.go
+++ comp/metadata/clusterchecks/mock/mock.go
@@ -3,7 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-package clusterchecksimpl
+// Package mock provides a mock implementation of the clusterchecks metadata component
+package mock
 
 import (
 	"net/http"
