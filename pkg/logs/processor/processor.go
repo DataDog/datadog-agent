8c3471d051f21bdf36ff4c859d71c5c7006a27ee
Lucas Tembras <lucas.tembras@datadoghq.com>
2025-08-20 11:24:00 -0400

    refactor: moved recording of truncated metrics before applying redacting rules

--- pkg/logs/processor/processor.go
+++ pkg/logs/processor/processor.go
@@ -112,6 +112,14 @@ func (p *Processor) processMessage(msg *message.Message) {
 	defer p.pipelineMonitor.ReportComponentEgress(msg, metrics.ProcessorTlmName, p.instanceID)
 	metrics.LogsDecoded.Add(1)
 	metrics.TlmLogsDecoded.Inc()
+	// Record truncation metrics if the message is truncated
+	if msg.ParsingExtra.IsTruncated {
+		if msg.Origin != nil {
+			metrics.TlmTruncatedCount.Inc(msg.Origin.Service(), msg.Origin.Source())
+		} else {
+			metrics.TlmTruncatedCount.Inc("", "")
+		}
+	}
 
 	if toSend := p.applyRedactingRules(msg); toSend {
 		metrics.LogsProcessed.Add(1)
@@ -132,15 +140,6 @@ func (p *Processor) processMessage(msg *message.Message) {
 		if err := p.encoder.Encode(msg, p.GetHostname(msg)); err != nil {
 			log.Error("unable to encode msg ", err)
 			return
-		}
-
-		// Record truncation metrics if the message is truncated
-		if msg.ParsingExtra.IsTruncated {
-			if msg.Origin != nil {
-				metrics.TlmTruncatedCount.Inc(msg.Origin.Service(), msg.Origin.Source())
-			} else {
-				metrics.TlmTruncatedCount.Inc("", "")
-			}
 		}
 
 		p.utilization.Stop() // Explicitly call stop here to avoid counting writing on the output channel as processing time
