4ab00c9210a94236539ad590335ec5c842d49158
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2025-08-21 18:37:18 +0200

    unused param linter

--- pkg/dyninst/dwarf/loclist/fuzz_test.go
+++ pkg/dyninst/dwarf/loclist/fuzz_test.go
@@ -10,7 +10,7 @@ package loclist
 import "testing"
 
 func FuzzParseInstructions(f *testing.F) {
-	f.Fuzz(func(t *testing.T, data []byte, ptrSize uint8, totalByteSize uint32) {
+	f.Fuzz(func(_ *testing.T, data []byte, ptrSize uint8, totalByteSize uint32) {
 		_, err := ParseInstructions(data, ptrSize, totalByteSize)
 		if err != nil {
 			return
