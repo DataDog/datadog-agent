2e62427a9d3ca8f8371791e8dc34c4510bea192e
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-08-21 12:35:14 +0200

    Add some invoke tasks
    Refacto for tasks

---
+++ tasks/dyntest.py
@@ -0,0 +1,43 @@
+"""
+Invoke task to handle dynamic tests.
+"""
+
+import os
+
+from invoke import Context, task
+
+from tasks.libs.common.git import get_modified_files
+from tasks.libs.dynamic_test.backend import S3Backend
+from tasks.libs.dynamic_test.evaluator import DatadogDynTestEvaluator
+from tasks.libs.dynamic_test.executor import DynTestExecutor
+from tasks.libs.dynamic_test.index import IndexKind
+from tasks.libs.dynamic_test.indexers.e2e import CoverageDynTestIndexer
+
+
+@task
+def compute_and_upload_job_index(ctx: Context, bucket_uri: str, coverage_folder: str, commit_sha: str, job_id: str):
+    indexer = CoverageDynTestIndexer(coverage_folder)
+    index = indexer.compute_index(ctx)
+    uploader = S3Backend(bucket_uri)
+    uploader.upload_index(index, IndexKind.PACKAGE, f"{commit_sha}/{job_id}")
+
+
+@task
+def consolidate_index_in_s3(_: Context, bucket_uri: str, commit_sha: str):
+    uploader = S3Backend(bucket_uri)
+    index = uploader.consolidate_index(IndexKind.PACKAGE, commit_sha)
+    uploader.upload_index(index, IndexKind.PACKAGE, commit_sha)
+
+
+@task
+def evaluate_index(ctx: Context, bucket_uri: str, commit_sha: str, pipeline_id: str):
+    uploader = S3Backend(bucket_uri)
+    executor = DynTestExecutor(ctx, uploader, IndexKind.PACKAGE, commit_sha)
+    evaluator = DatadogDynTestEvaluator(ctx, IndexKind.PACKAGE, executor, pipeline_id)
+    if not evaluator.initialize():
+        print("Failed to initialize index")
+        return
+    changes = get_modified_files(ctx)
+    results = evaluator.evaluate([os.path.basename(change) for change in changes])
+    evaluator.print_summary(results)
+    evaluator.send_stats_to_datadog(results)
