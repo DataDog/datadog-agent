33fc10bd6e49e5045106a64611784d1bee699cca
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-08-25 11:11:27 +0200

    [add-auth] Added command

--- tasks/auth.py
+++ tasks/auth.py
@@ -1,6 +1,5 @@
 """Manages authentication to services."""
 
-import requests
 from invoke import task
 
 from tasks.libs.common.utils import running_in_ci
@@ -20,26 +19,6 @@ def datadog_infra(ctx, audience, datacenter="us1.ddbuild.io", verbose=False):
         .stdout.strip()
         .removeprefix('Authorization: ')
     )
-
-    if verbose:
-        print(token)
-
-    return token
-
-
-@task
-def gitlab(ctx, repo='datadog-agent', verbose=False):
-    """Get a gitlab token."""
-
-    infra_token = datadog_infra(ctx, audience="sdm")
-    url = f"https://bti-ci-api.us1.ddbuild.io/internal/ci/gitlab/token?owner=DataDog&repository={repo}"
-
-    res = requests.get(url, headers={'Authorization': infra_token}, timeout=10)
-
-    if not res.ok:
-        raise RuntimeError(f'Failed to retrieve Gitlab token, request failed with code {res.status_code}:\n{res.text}')
-
-    token = res.json()['token']
 
     if verbose:
         print(token)
