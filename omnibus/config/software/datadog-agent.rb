acfdcd68d1f19b42466ab20f3b99557400995252
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2025-08-26 17:14:41 +0200

    chore: import logging

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -7,6 +7,8 @@ require './lib/ostools.rb'
 require './lib/project_helpers.rb'
 require 'pathname'
 
+include Logging
+
 name 'datadog-agent'
 
 # We don't want to build any dependencies in "repackaging mode" so all usual dependencies
