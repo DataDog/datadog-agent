d264e814172e8c0210ece8cb27aa8165db974d01
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2025-08-31 00:42:20 +0200

    test: add test for collector spam

---
+++ comp/collector/collector/collectorimpl/collector_integration_test.go
@@ -0,0 +1,92 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package collectorimpl
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"go.uber.org/fx"
+
+	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/demultiplexerimpl"
+	"github.com/DataDog/datadog-agent/comp/collector/collector"
+	"github.com/DataDog/datadog-agent/comp/core"
+	agenttelemetry "github.com/DataDog/datadog-agent/comp/core/agenttelemetry/def"
+	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/integration"
+	"github.com/DataDog/datadog-agent/comp/core/config"
+	tagger "github.com/DataDog/datadog-agent/comp/core/tagger/def"
+	taggerfx "github.com/DataDog/datadog-agent/comp/core/tagger/fx-noop"
+	haagentmock "github.com/DataDog/datadog-agent/comp/haagent/mock"
+	integrations "github.com/DataDog/datadog-agent/comp/logs/integrations/def"
+	pkgcollector "github.com/DataDog/datadog-agent/pkg/collector"
+	"github.com/DataDog/datadog-agent/pkg/serializer"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/pkg/util/option"
+)
+
+func TestSchedulerSpam(t *testing.T) {
+	tempDir := t.TempDir()
+	checkName := "testcheck"
+	checkDir := filepath.Join(tempDir, "checks.d", checkName)
+	os.MkdirAll(checkDir, 0755)
+	os.WriteFile(filepath.Join(checkDir, "check.py"), []byte(PythonCheck), 0644)
+
+	deps := fxutil.Test[dependencies](t,
+		core.MockBundle(),
+		demultiplexerimpl.MockModule(),
+		haagentmock.Module(),
+		fx.Provide(func() option.Option[serializer.MetricSerializer] {
+			return option.None[serializer.MetricSerializer]()
+		}),
+		fx.Provide(func() option.Option[agenttelemetry.Component] {
+			return option.None[agenttelemetry.Component]()
+		}),
+		fx.Replace(config.MockParams{
+			Overrides: map[string]interface{}{
+				"additional_checksd":   tempDir,
+				"check_cancel_timeout": 100 * time.Millisecond,
+			},
+		}),
+	)
+
+	tagger := fxutil.Test[tagger.Component](t, taggerfx.Module())
+
+	collImpl := newCollector(deps)
+
+	var coll collector.Component = collImpl
+	checkScheduler := pkgcollector.InitCheckScheduler(option.New(coll), deps.SenderManager, option.None[integrations.Component](), tagger)
+
+	log.Infof("Starting scheduler spam")
+
+	for i := 0; i < 100; i++ {
+		checkScheduler.Schedule([]integration.Config{
+			{
+				Name:       checkName,
+				InitConfig: []byte{},
+				Instances: []integration.Data{
+					[]byte(fmt.Sprintf(CheckConfigTemplate, i)),
+				},
+			},
+		})
+	}
+}
+
+const CheckConfigTemplate = `
+id: %d
+`
+
+const PythonCheck = `
+from datadog_checks.base import AgentCheck
+
+class HTTPCheck(AgentCheck):
+	def check(self, instance):
+		checkId = instance.get('id')
+		print(f"Running check {checkId}")
+`
