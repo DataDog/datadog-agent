fffb8eb0bee5dd4889babf9d58f2502040ec228f
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2025-09-01 16:43:35 +0200

    double the sec profile cache sz and add missing returns

--- pkg/security/security_profile/secprofs.go
+++ pkg/security/security_profile/secprofs.go
@@ -287,6 +287,7 @@ func (m *Manager) unloadProfileMap(profile *profile.Profile) {
 	// remove kernel space filters
 	if err := m.securityProfileSyscallsMap.Delete(profile.GetProfileCookie()); err != nil {
 		seclog.Errorf("couldn't remove syscalls filter: %v", err)
+		return
 	}
 
 	// TODO: delete all kernel space programs
@@ -332,6 +333,7 @@ func (m *Manager) unlinkProfileMap(profile *profile.Profile, workload *tags.Work
 
 	if err := m.securityProfileMap.Delete(workload.CGroupFile); err != nil {
 		seclog.Errorf("couldn't unlink workload %s (selector: %s) with profile %s: %v", workload.ContainerID, workload.Selector.String(), profile.Metadata.Name, err)
+		return
 	}
 	seclog.Infof("workload %s (selector: %s) successfully unlinked from profile %s", workload.ContainerID, workload.Selector.String(), profile.Metadata.Name)
 }
