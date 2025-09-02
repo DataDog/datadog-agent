5eb62b6db4596429a11237ac7aa8b0b9c7a45a56
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-09-02 10:25:51 +0200

    [test-wcs-update] Test 0.3.1rc8

--- tasks/winbuildscripts/Build-AgentPackages.ps1
+++ tasks/winbuildscripts/Build-AgentPackages.ps1
@@ -45,6 +45,26 @@ param(
 
 . "$PSScriptRoot\common.ps1"
 
+$WINSIGN_VERSION = "0.3.1rc8"
+
+Write-Host "Installing Windows Codesign Helper $WINSIGN_VERSION"
+# $ErrorActionPreference = 'Stop'
+# $ProgressPreference = 'SilentlyContinue'
+
+Write-Host -ForegroundColor Green "Installing Windows Codesign Helper $WINSIGN_VERSION"
+
+## need to have more rigorous download at some point, but
+$codesign_base = "windows_code_signer-$($WINSIGN_VERSION)-py3-none-any.whl"
+$codesign_wheel = "https://s3.amazonaws.com/dd-agent-omnibus/windows-code-signer/$($codesign_base)"
+$codesign_wheel_target = "c:\devtools\$($codesign_base)"
+(New-Object System.Net.WebClient).DownloadFile($codesign_wheel, $codesign_wheel_target)
+
+# TODO: Get-RemoteFile -RemoteFile $codesign_wheel -LocalFile $codesign_wheel_target -VerifyHash $WINSIGN_SHA256
+
+python -m pip install $codesign_wheel_target
+If ($lastExitCode -ne "0") { throw "Previous command returned $lastExitCode" }
+
+
 Invoke-BuildScript `
     -BuildOutOfSource $BuildOutOfSource `
     -InstallDeps $InstallDeps `
