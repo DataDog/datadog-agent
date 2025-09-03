0f9179128bc941cefbf9356b52d3646a15718949
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2025-09-03 09:51:14 +0200

    add check of return code

--- pkg/security/ebpf/probes/socket.go
+++ pkg/security/ebpf/probes/socket.go
@@ -8,7 +8,13 @@
 // Package probes holds probes related files
 package probes
 
-import manager "github.com/DataDog/ebpf-manager"
+import (
+	"fmt"
+
+	manager "github.com/DataDog/ebpf-manager"
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/asm"
+)
 
 func getSocketProbes(cgroup2MountPoint string) []*manager.Probe {
 	return []*manager.Probe{
@@ -27,4 +33,48 @@ func getSocketProbes(cgroup2MountPoint string) []*manager.Probe {
 			CGroupPath: cgroup2MountPoint,
 		},
 	}
+}
+
+// CheckCgroupSocketReturnCode checks if the return code is 1(accept)
+//
+//nolint:unused,deadcode
+func CheckCgroupSocketReturnCode(progSpecs map[string]*ebpf.ProgramSpec) error {
+	for _, progSpec := range progSpecs {
+		if progSpec.Type == ebpf.CGroupSock {
+			if IsProgAllowedToTCActShot(progSpec.Name) {
+				continue
+			}
+
+			r0 := int32(255)
+
+			for _, inst := range progSpec.Instructions {
+				fmt.Printf(">> %v\n", inst)
+
+				class := inst.OpCode.Class()
+				if class.IsJump() {
+					if inst.OpCode.JumpOp() == asm.Exit {
+						fmt.Printf("code: %d\n", r0)
+
+						if r0 != 1 {
+							return fmt.Errorf("cgroup sock program %s is not using 1 as return code, %d, %v", progSpec.Name, r0, progSpec.Instructions)
+						}
+					}
+				} else {
+					op := inst.OpCode
+					switch op {
+					case asm.Mov.Op(asm.ImmSource),
+						asm.LoadImmOp(asm.DWord),
+						asm.LoadImmOp(asm.Word),
+						asm.LoadImmOp(asm.Half),
+						asm.LoadImmOp(asm.Byte):
+						if inst.Dst == asm.R0 {
+							r0 = int32(inst.Constant)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nil
 }
