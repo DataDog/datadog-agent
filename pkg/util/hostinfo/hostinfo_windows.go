d19f1b249c6f6226d1f308604a0b4233a0422a14
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2025-09-04 16:33:18 +0200

    [mod] fixing hostinfo module issues

--- pkg/util/hostinfo/hostinfo_windows.go
+++ pkg/util/hostinfo/hostinfo_windows.go
@@ -2,6 +2,15 @@
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
+//
+// Package hostinfo helps collect relevant host information
+//
+//
+// # Compatibility
+//
+// This module is exported and can be used outside of the datadog-agent
+// repository, but is not designed as a general-purpose logging system.  Its
+// API may change incompatibly.
 
 // Portions of this code are taken from the gopsutil project
 // https://github.com/shirou/gopsutil .  This code is licensed under the New BSD License
