7f324c77e332391adeee4078bafd04ca7f482bc0
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2025-09-08 18:33:08 +0200

    [lint] fix some linter warnins

--- pkg/util/hostinfo/hostinfo.go
+++ pkg/util/hostinfo/hostinfo.go
@@ -3,15 +3,13 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 //
-// Package hostinfo helps collect relevant host information
-//
-//
 // # Compatibility
 //
 // This module is exported and can be used outside of the datadog-agent
 // repository, but is not designed as a general-purpose logging system.  Its
 // API may change incompatibly.
 
+// Package hostinfo helps collect relevant host information
 package hostinfo
 
 import (
