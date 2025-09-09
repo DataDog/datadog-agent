5741edfdb1905dec84510a59c722dcd7f766531a
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2025-09-09 08:17:35 +0000

    fix comment

--- pkg/tagset/composite_tags.go
+++ pkg/tagset/composite_tags.go
@@ -136,7 +136,7 @@ func (t CompositeTags) UnsafeToReadOnlySliceString() []string {
 	return slices.Concat(t.tags1, t.tags2)
 }
 
-// Get returns an internal copy of the tags. Returned value should not
+// UnsafeGet returns an internal copy of the tags. Returned value should not
 // be modified or appended to.
 func (t CompositeTags) UnsafeGet() ([]string, []string) {
 	return t.tags1, t.tags2
