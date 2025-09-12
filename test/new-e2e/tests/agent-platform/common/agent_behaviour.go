2de228591cd083480cd6a85272687a5496d2f7ea
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2025-09-12 19:00:34 +0200

    fix: rm system-probe config to avoid conflicts with previous options

--- test/new-e2e/tests/agent-platform/common/agent_behaviour.go
+++ test/new-e2e/tests/agent-platform/common/agent_behaviour.go
@@ -300,6 +300,7 @@ func CheckApmDisabled(t *testing.T, client *TestClient) {
 // CheckCWSBehaviour runs tests to check the agent behave correctly when CWS is enabled
 func CheckCWSBehaviour(t *testing.T, client *TestClient) {
 	t.Run("enable CWS and restarts", func(tt *testing.T) {
+		client.Host.MustExecute("sudo rm " + client.Helper.GetConfigFolder() + "system-probe.yaml")
 		err := client.SetConfig(client.Helper.GetConfigFolder()+"system-probe.yaml", "runtime_security_config.enabled", "true")
 		require.NoError(tt, err)
 		err = client.SetConfig(client.Helper.GetConfigFolder()+"security-agent.yaml", "runtime_security_config.enabled", "true")
