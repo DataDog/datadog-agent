dfe99d31732e0b53c9c8611ac75b0fc844d443a8
Paul Cacheux <paul.cacheux@datadoghq.com>
2025-09-16 14:28:57 +0200

    remove trivy build tag on files that don't need it

--- pkg/security/resolvers/sbom/collectorv2/dpkg.go
+++ pkg/security/resolvers/sbom/collectorv2/dpkg.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build linux && trivy
+//go:build linux
 
 // Package collectorv2 holds sbom related files
 package collectorv2
