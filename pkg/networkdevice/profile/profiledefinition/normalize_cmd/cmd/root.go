bdfded3e6f95f078be6ca8178ebdb8c1f4af69af
Daniel Lepage <daniel.lepage@datadoghq.com>
2025-09-17 13:18:52 -0400

    Allow normalize_cmd to read/write from stdin/stdout.

--- pkg/networkdevice/profile/profiledefinition/normalize_cmd/cmd/root.go
+++ pkg/networkdevice/profile/profiledefinition/normalize_cmd/cmd/root.go
@@ -9,6 +9,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 
@@ -42,9 +43,14 @@ var rootCmd = &cobra.Command{
 			fmt.Printf("parse failure %v, unable to generate output\n", err)
 		}
 		for _, filePath := range args {
-			filename := filepath.Base(filePath)
-			name := filename[:len(filename)-len(filepath.Ext(filename))] // remove extension
-			def, errors := GetProfile(filePath)
+			var name string
+			if filePath == "-" {
+				name = "stdin"
+			} else {
+				filename := filepath.Base(filePath)
+				name = filename[:len(filename)-len(filepath.Ext(filename))] // remove extension
+			}
+			def, errors := GetProfile(filePath, strict)
 			if len(errors) > 0 {
 				fmt.Printf("*** %d error(s) in profile %q ***\n", len(errors), filePath)
 				for _, e := range errors {
@@ -62,14 +68,23 @@ var rootCmd = &cobra.Command{
 
 // GetProfile parses a profile from a file path and validates it.
 func GetProfile(filePath string, strict bool) (*profiledefinition.ProfileDefinition, []string) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, []string{fmt.Sprintf("unable to read file: %v", err)}
+	var inFile io.Reader
+	if filePath == "-" {
+		inFile = os.Stdin
+	} else {
+		f, err := os.Open(filePath)
+		if err != nil {
+			return nil, []string{fmt.Sprintf("unable to read file: %v", err)}
+		}
+		defer func() {
+			_ = f.Close()
+		}()
+		inFile = f
 	}
 	def := profiledefinition.NewProfileDefinition()
-	dec := yaml.NewDecoder(file)
+	dec := yaml.NewDecoder(inFile)
 	dec.SetStrict(strict)
-	err = dec.Decode(&def)
+	err := dec.Decode(&def)
 	if err != nil {
 		return nil, []string{fmt.Sprintf("unable to parse profile: %v", err)}
 	}
@@ -85,11 +100,11 @@ func WriteProfile(def *profiledefinition.ProfileDefinition, name string, outdir 
 	if outdir == "" {
 		return nil
 	}
-	var filename string
+	var outPath string
 	var data []byte
 	if useJSON {
 		var err error
-		filename = name + ".json"
+		outPath = filepath.Join(outdir, name+".json")
 		data, err = json.Marshal(def)
 		if err != nil {
 			return fmt.Errorf("unable to marshal profile %s: %w", name, err)
@@ -97,22 +112,28 @@ func WriteProfile(def *profiledefinition.ProfileDefinition, name string, outdir 
 	} else {
 		var err error
 		data, err = yaml.Marshal(def)
-		filename = name + ".yaml"
+		outPath = filepath.Join(outdir, name+".yaml")
 		if err != nil {
 			return fmt.Errorf("unable to marshal profile %s: %w", name, err)
 		}
 	}
-	outfile := filepath.Join(outdir, filename)
-	f, err := os.Create(outfile)
-	if err != nil {
-		return fmt.Errorf("unable to create file %s: %w", outfile, err)
+	var writer io.Writer
+	if outdir == "-" {
+		writer = os.Stdout
+		outPath = "stdout"
+	} else {
+		f, err := os.Create(outPath)
+		if err != nil {
+			return fmt.Errorf("unable to create file %s: %w", outPath, err)
+		}
+		defer func() {
+			_ = f.Close()
+		}()
+		writer = f
 	}
-	defer func() {
-		_ = f.Close()
-	}()
-	_, err = f.Write(data)
+	_, err := writer.Write(data)
 	if err != nil {
-		return fmt.Errorf("unable to write to file %s: %w", outfile, err)
+		return fmt.Errorf("unable to write to %s: %w", outPath, err)
 	}
 	return nil
 }
