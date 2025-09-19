b014a4b61a171516c03f573c0a051e0a1d6eb48b
Stuart Geipel <stuart.geipel@datadoghq.com>
2025-09-19 09:59:02 -0700

    [tracer] Change spammy debug logs to verbose

--- pkg/network/ebpf/c/defs.h
+++ pkg/network/ebpf/c/defs.h
@@ -17,4 +17,15 @@ static __always_inline bool IS_ERR_OR_NULL(const void *ptr)
 #include <linux/err.h>
 #endif // COMPILE_CORE
 
+
+// verbose logs will trigger themselves in a loop when you are ssh'd in.
+// disable this if you want cleaner local debugging output
+#define LOG_VERBOSE
+
+#ifdef LOG_VERBOSE
+#define log_verbose(format, ...) log_debug(format, ##__VA_ARGS__)
+#else
+#define log_verbose(format, ...)
+#endif
+
 #endif
