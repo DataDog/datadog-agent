aec91a9ba1bb4ab5f01a2d9bbc68d346fe7400f1
Jatin Singh <jatin.singh@datadoghq.com>
2025-09-22 12:09:17 +0200

    Added default value for apm_receiver_socket

--- pkg/config/setup/apm.go
+++ pkg/config/setup/apm.go
@@ -162,7 +162,7 @@ func setupAPM(config pkgconfigmodel.Setup) {
 		}
 		return mappings
 	})
-	config.BindEnv("apm_config.receiver_socket", "DD_APM_RECEIVER_SOCKET")
+	config.BindEnv("apm_config.receiver_socket", "/var/run/datadog/apm.socket", "DD_APM_RECEIVER_SOCKET")
 	config.BindEnv("apm_config.windows_pipe_name", "DD_APM_WINDOWS_PIPE_NAME")
 	config.BindEnv("apm_config.sync_flushing", "DD_APM_SYNC_FLUSHING")
 	config.BindEnv("apm_config.filter_tags.require", "DD_APM_FILTER_TAGS_REQUIRE")
