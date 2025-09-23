08f2bf837f1852f6f7d001bfabb910edfc306ada
s-alad <saad.naji@datadoghq.com>
2025-09-23 15:11:52 -0400

    add more test cases & stop text scrubber from undoing yaml scrubber work

--- pkg/util/scrubber/yaml_scrubber.go
+++ pkg/util/scrubber/yaml_scrubber.go
@@ -128,10 +128,11 @@ func (c *Scrubber) ScrubYaml(input []byte) ([]byte, error) {
 		encoder.SetIndent(2)
 		if err := encoder.Encode(&data); err != nil {
 			fmt.Fprintf(os.Stderr, "error scrubbing YAML, falling back on text scrubber: %s\n", err)
-		} else {
-			input = buffer.Bytes()
+			return c.ScrubBytes(input)
 		}
+		input = buffer.Bytes()
 		encoder.Close()
+		return input, nil
 	}
 	return c.ScrubBytes(input)
 }
