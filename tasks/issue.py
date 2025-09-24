232aebd441f4cb327d52489880d4dc10d73ccf33
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-09-24 11:08:25 +0200

    Prepare for transition from renovate[bot] to mend[bot]

--- tasks/issue.py
+++ tasks/issue.py
@@ -73,7 +73,9 @@ def ask_reviews(_, pr_id):
                 DEFAULT_SLACK_CHANNEL,
             )
             stop_updating = ""
-            if pr.user.login == "renovate[bot]" and pr.title.startswith("chore(deps): update integrations-core"):
+            if (pr.user.login == "renovate[bot]" or pr.user.login == "mend[bot]") and pr.title.startswith(
+                "chore(deps): update integrations-core"
+            ):
                 stop_updating = "Add the `stop-updating` label before trying to merge this PR, to prevent it from being updated by Renovate.\n"
             message = f'Hello :{random.choice(waves)}:!\n*{actor}* is asking review for PR <{pr.html_url}/s|{pr.title}>.\nCould you please have a look?\n{stop_updating}Thanks in advance!\n'
             if channel == DEFAULT_SLACK_CHANNEL:
