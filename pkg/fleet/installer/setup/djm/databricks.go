2aec8651a28f1813f90b89e35c5cc6546e1d74c6
aboitreaud <72934368+aboitreaud@users.noreply.github.com>
2025-09-25 11:13:46 +0200

    cleanup gpu config after moving to agent 7.70

--- pkg/fleet/installer/setup/djm/databricks.go
+++ pkg/fleet/installer/setup/djm/databricks.go
@@ -236,19 +236,10 @@ func setClearHostTag(s *common.Setup, tagKey, value string) {
 // setupGPUIntegration configures GPU monitoring integration
 func setupGPUIntegration(s *common.Setup) {
 	s.Out.WriteString("Setting up GPU monitoring based on env variable GPU_MONITORING_ENABLED=true\n")
+	s.Span.SetTag("host_tag_set.gpu_monitoring_enabled", "true")
 
 	s.Config.DatadogYAML.CollectGPUTags = true
 	s.Config.DatadogYAML.GPUCheck.Enabled = true
-	s.Config.DatadogYAML.EnableNvmlDetection = true
-
-	if s.Config.SystemProbeYAML == nil {
-		s.Config.SystemProbeYAML = &config.SystemProbeConfig{}
-	}
-	s.Config.SystemProbeYAML.GPUMonitoringConfig = config.GPUMonitoringConfig{
-		Enabled: true,
-	}
-
-	s.Span.SetTag("host_tag_set.gpu_monitoring_enabled", "true")
 
 	// Agent must be restarted after NVML initialization, which occurs after init script execution
 	s.DelayedAgentRestartConfig.Scheduled = true
