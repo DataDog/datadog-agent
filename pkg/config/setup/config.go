83c2d8d6e28fa6ad15f7a33d07b82eb3332ab5d3
Fabrice Matrat <fabrice.matrat@datadoghq.com>
2025-09-26 09:39:31 +0200

    Fixing config for synthetics scheduler.

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -944,7 +944,9 @@ func InitConfig(config pkgconfigmodel.Setup) {
 	bindEnvAndSetLogsConfigKeys(config, "sbom.")
 
 	// Synthetics configuration
-	config.BindEnvAndSetDefault("synthetics.enabled", false)
+	config.BindEnvAndSetDefault("synthetics.collector.enabled", false)
+	config.BindEnvAndSetDefault("synthetics.collector.workers", 5)
+	config.BindEnvAndSetDefault("synthetics.collector.flush_interval", "60s")
 	bindEnvAndSetLogsConfigKeys(config, "synthetics.forwarder.")
 
 	config.BindEnvAndSetDefault("sbom.cache_directory", filepath.Join(defaultRunPath, "sbom-agent"))
