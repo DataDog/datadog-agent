d8b38694a7ace6c96340e1ea10642e12d9b2a52c
Mathew Estafanous <mathew.estafanous@datadoghq.com>
2025-09-26 15:26:36 -0400

    chore: update tests with filter store ref

--- pkg/collector/python/test_containers.go
+++ pkg/collector/python/test_containers.go
@@ -8,36 +8,44 @@
 package python
 
 import (
-	"regexp"
 	"testing"
 
+	"github.com/DataDog/datadog-agent/comp/core/config"
+	log "github.com/DataDog/datadog-agent/comp/core/log/def"
+	logmock "github.com/DataDog/datadog-agent/comp/core/log/mock"
+	nooptagger "github.com/DataDog/datadog-agent/comp/core/tagger/impl-noop"
+	noopTelemetry "github.com/DataDog/datadog-agent/comp/core/telemetry/noopsimpl"
+	workloadfilterfxmock "github.com/DataDog/datadog-agent/comp/core/workloadfilter/fx-mock"
+	workloadfiltermock "github.com/DataDog/datadog-agent/comp/core/workloadfilter/mock"
+	integrations "github.com/DataDog/datadog-agent/comp/logs/integrations/def"
+	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"github.com/DataDog/datadog-agent/pkg/util/option"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/DataDog/datadog-agent/pkg/util/containers"
+	"go.uber.org/fx"
 )
 
 import "C"
 
 func testIsContainerExcluded(t *testing.T) {
-	filter = &containers.Filter{
-		Enabled: true,
-	}
-	defer func() { filter = nil }()
+	sender := mocksender.NewMockSender("testID")
+	logReceiver := option.None[integrations.Component]()
+	tagger := nooptagger.NewComponent()
+	filterStore := fxutil.Test[workloadfiltermock.Mock](t, fx.Options(
+		fx.Provide(func() config.Component {
+			mockConfig := config.NewMock(t)
+			mockConfig.SetWithoutSource("container_exclude", []string{"image:bar", "kube_namespace:black"})
+			mockConfig.SetWithoutSource("container_include", []string{"kube_namespace:white"})
+			return mockConfig
+		}),
+		fx.Provide(func() log.Component { return logmock.New(t) }),
+		noopTelemetry.Module(),
+		workloadfilterfxmock.MockModule(),
+	))
+	scopeInitCheckContext(sender.GetSenderManager(), logReceiver, tagger, filterStore)
 
-	r, err := regexp.Compile("bar")
-	assert.Nil(t, err)
-	filter.ImageExcludeList = append(filter.ImageExcludeList, r)
-
-	r, err = regexp.Compile("white")
-	assert.Nil(t, err)
-	filter.NamespaceIncludeList = append(filter.NamespaceIncludeList, r)
-
-	r, err = regexp.Compile("black")
-	assert.Nil(t, err)
-	filter.NamespaceExcludeList = append(filter.NamespaceExcludeList, r)
-
-	assert.Equal(t, IsContainerExcluded(C.CString("foo"), C.CString("bar"), C.CString("ns")), C.int(1))
-	assert.Equal(t, IsContainerExcluded(C.CString("foo"), C.CString("bar"), C.CString("white")), C.int(0))
-	assert.Equal(t, IsContainerExcluded(C.CString("foo"), C.CString("baz"), C.CString("black")), C.int(1))
-	assert.Equal(t, IsContainerExcluded(C.CString("foo"), C.CString("baz"), nil), C.int(0))
+	assert.Equal(t, C.int(1), IsContainerExcluded(C.CString("foo"), C.CString("bar"), C.CString("ns")))
+	assert.Equal(t, C.int(0), IsContainerExcluded(C.CString("foo"), C.CString("bar"), C.CString("white")))
+	assert.Equal(t, C.int(1), IsContainerExcluded(C.CString("foo"), C.CString("baz"), C.CString("black")))
+	assert.Equal(t, C.int(0), IsContainerExcluded(C.CString("foo"), C.CString("baz"), nil))
 }
