2423ae0e76609f66d6f837211966cb7b3dc25a0b
Mathew Estafanous <mathew.estafanous@datadoghq.com>
2025-09-26 15:26:36 -0400

    chore: update tests with filter store ref

--- pkg/collector/python/test_loader.go
+++ pkg/collector/python/test_loader.go
@@ -11,6 +11,7 @@ import (
 	"runtime"
 	"testing"
 
+	workloadfilterfxmock "github.com/DataDog/datadog-agent/comp/core/workloadfilter/fx-mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -204,7 +205,8 @@ func testLoadWheelCheck(t *testing.T) {
 	senderManager := mocksender.CreateDefaultDemultiplexer()
 	logReceiver := option.None[integrations.Component]()
 	tagger := nooptagger.NewComponent()
-	loader, err := NewPythonCheckLoader(senderManager, logReceiver, tagger, nil)
+	filterStore := workloadfilterfxmock.SetupMockFilter(t)
+	loader, err := NewPythonCheckLoader(senderManager, logReceiver, tagger, filterStore)
 	assert.Nil(t, err)
 
 	// testing loading dd wheels
