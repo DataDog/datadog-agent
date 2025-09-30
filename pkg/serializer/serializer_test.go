5c7c187eb71f1d847fab79677ca2e7071beb4bdd
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2025-09-30 14:27:29 +0000

    Extend v2 tests

--- pkg/serializer/serializer_test.go
+++ pkg/serializer/serializer_test.go
@@ -33,6 +33,7 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
 	"github.com/DataDog/datadog-agent/pkg/util/compression"
 	"github.com/DataDog/datadog-agent/pkg/version"
+	"github.com/DataDog/datadog-agent/pkg/tagset"
 )
 
 func TestInitExtraHeadersNoopCompression(t *testing.T) {
@@ -372,13 +373,36 @@ func TestSendSeries(t *testing.T) {
 			compressor := metricscompressionimpl.NewCompressorReq(metricscompressionimpl.Requires{Cfg: mockConfig}).Comp
 			s := NewSerializer(f, nil, compressor, mockConfig, logmock.New(t), "testhost")
 			matcher := createProtoscopeMatcher(`1: {
-		1: { 1: {"host"} }
+		1: { 1: {"host"} 2: {"localhost"} }
+        1: { 1: {"device"} 2: {"sda"} }
+        1: { 1: {"ook" } 2: {"eek"} }
+        2: {"foo"}
+        3: {"bar"} 3:{"baz"}
 		5: 3
+        7: {"System"}
+        8: 10
+        4: { 2: 1759241515 1: 3.14 }
+        4: { 2: 1759241525 1: 2.71 }
 		9: { 1: { 4: 10 }}
 	  }`, s)
 			f.On("SubmitSeries", matcher, s.protobufExtraHeadersWithCompression).Return(nil).Times(1)
 
-			err := s.SendIterableSeries(metricsserializer.CreateSerieSource(metrics.Series{&metrics.Serie{}}))
+			err := s.SendIterableSeries(metricsserializer.CreateSerieSource(metrics.Series{&metrics.Serie{
+				Name: "foo",
+				MType: metrics.APIGaugeType,
+				Device: "sda",
+				Tags: tagset.NewCompositeTags(
+					[]string{"bar"},
+					[]string{"baz", "dd.internal.resource:ook:eek"},
+				),
+				Points: []metrics.Point{
+					{Ts: 1759241515, Value: 3.14},
+					{Ts: 1759241525, Value: 2.71},
+				},
+				Host: "localhost",
+				SourceTypeName: "System",
+				Interval: 10,
+			}}))
 			require.Nil(t, err)
 			f.AssertExpectations(t)
 		})
