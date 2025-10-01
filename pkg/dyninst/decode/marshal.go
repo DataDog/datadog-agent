ab1e6ca4c38a84b81511194c4828a2b6c70cc278
grantseltzer <grantseltzer@gmail.com>
2025-10-01 13:06:03 -0500

    Use string builder

--- pkg/dyninst/decode/marshal.go
+++ pkg/dyninst/decode/marshal.go
@@ -64,12 +64,11 @@ func (m *message) MarshalJSONTo(enc *jsontext.Encoder) error {
 		switch segTyped := seg.(type) {
 		case ir.JSONSegment:
 			// Extract raw value from expression (no JSON encoding)
-			value, err := m.captureEvent.extractExpressionRawValue(segTyped.ExpressionIndex)
+			err := m.captureEvent.extractExpressionRawValue(&sb, segTyped.ExpressionIndex)
 			if err != nil {
 				*m.captureEvent.evaluationErrors = append(*m.captureEvent.evaluationErrors, err.Error())
 				continue
 			}
-			sb.WriteString(value)
 		case ir.StringSegment:
 			if _, err := sb.WriteString(segTyped.Value); err != nil {
 				*m.captureEvent.evaluationErrors = append(*m.captureEvent.evaluationErrors, err.Error())
@@ -249,65 +248,68 @@ func (ce *captureEvent) processExpression(
 }
 
 // extractExpressionRawValue extracts the raw string value of an expression without JSON encoding
-func (ce *captureEvent) extractExpressionRawValue(expressionIndex int) (string, error) {
+func (ce *captureEvent) extractExpressionRawValue(sb *strings.Builder, expressionIndex int) error {
 	if expressionIndex >= len(ce.rootType.Expressions) {
-		return "", fmt.Errorf("expression index %d out of bounds", expressionIndex)
+		return fmt.Errorf("expression index %d out of bounds", expressionIndex)
 	}
 	if ce.skippedIndices.get(expressionIndex) {
-		return "", fmt.Errorf("expression skipped")
+		return fmt.Errorf("expression skipped")
 	}
 	expr := ce.rootType.Expressions[expressionIndex]
 	parameterType := expr.Expression.Type
 	parameterSize := parameterType.GetByteSize()
 	ub := expr.Offset + parameterSize
 	if int(ub) > len(ce.rootData) {
-		return "", fmt.Errorf("could not read parameter data from root data, length mismatch")
+		return fmt.Errorf("could not read parameter data from root data, length mismatch")
 	}
 
 	parameterData := ce.rootData[expr.Offset:ub]
 	presenceBitSet := bitset(ce.rootData[:ce.rootType.PresenceBitsetSize])
 	if !presenceBitSet.get(expressionIndex) && parameterSize != 0 {
-		return "", fmt.Errorf("expression not captured")
+		return fmt.Errorf("expression not captured")
 	}
 
 	// Convert binary data to raw string value based on type
 	decoderType, ok := ce.encodingContext.typesByID[parameterType.GetID()]
 	if !ok {
-		return "", fmt.Errorf("no decoder type found for type %s", parameterType.GetName())
+		return fmt.Errorf("no decoder type found for type %s", parameterType.GetName())
 	}
 
-	return extractRawValue(decoderType, &ce.encodingContext, parameterData)
+	return extractRawValue(sb, decoderType, &ce.encodingContext, parameterData)
 }
 
 // extractRawValue converts binary data to raw string representation
-func extractRawValue(dt decoderType, c *encodingContext, data []byte) (string, error) {
+func extractRawValue(sb *strings.Builder, dt decoderType, c *encodingContext, data []byte) error {
 	switch t := dt.(type) {
 	case *baseType:
-		return extractBaseTypeValue(t, data)
+		return extractBaseTypeValue(sb, t, data)
 	case *goStringHeaderType:
-		return extractStringValue(t, c, data)
+		return extractStringValue(sb, t, c, data)
 	case *pointerType:
-		// For template values, we typically want the pointed-to value
-		return "0x" + strconv.FormatUint(binary.NativeEndian.Uint64(data), 16), nil
+		_, err := sb.WriteString("0x" + strconv.FormatUint(binary.NativeEndian.Uint64(data), 16))
+		return err
 	case *structureType:
 		// Format struct fields as readable string
-		return extractStructureTypeValue(t, c, data)
+		return extractStructureTypeValue(sb, t, c, data)
 	case *arrayType:
 		// Format array elements as readable string
-		return extractArrayTypeValue(t, c, data)
+		return extractArrayTypeValue(sb, t, c, data)
 	case *goSliceHeaderType:
 		// Format slice elements as readable string
-		return extractSliceTypeValue(t, c, data)
+		return extractSliceTypeValue(sb, t, c, data)
 	default:
-		// Fallback: return type name for unsupported types
-		return "<" + dt.irType().GetName() + ">", nil
+		// Fallback: write type name for unsupported types
+		_, err := sb.WriteString("<" + dt.irType().GetName() + ">")
+		return err
 	}
 }
 
-func extractStructureTypeValue(s *structureType, c *encodingContext, data []byte) (string, error) {
-	var fieldParts []string
-
+func extractStructureTypeValue(sb *strings.Builder, s *structureType, c *encodingContext, data []byte) error {
 	structType := s.irType().(*ir.StructureType)
+	_, err := sb.WriteString(fmt.Sprintf("%s{", structType.GetName()))
+	if err != nil {
+		return err
+	}
 	for field := range structType.Fields() {
 		fieldEnd := field.Offset + field.Type.GetByteSize()
 		if fieldEnd > uint32(len(data)) {
@@ -321,31 +323,39 @@ func extractStructureTypeValue(s *structureType, c *encodingContext, data []byte
 		fieldDecoderType, ok := c.typesByID[field.Type.GetID()]
 		if !ok {
 			// Unknown field type - show type name
-			fieldParts = append(fieldParts, fmt.Sprintf("%s = <%s>", field.Name, field.Type.GetName()))
+			_, err = sb.WriteString(fmt.Sprintf("%s = <%s>", field.Name, field.Type.GetName()))
+			if err != nil {
+				return err
+			}
 			continue
 		}
 
 		// Extract the raw value for this field
-		fieldValue, err := extractRawValue(fieldDecoderType, c, fieldData)
+		// Format as "fieldName = value"
+		_, err = sb.WriteString(fmt.Sprintf("%s = ", field.Name))
+		if err != nil {
+			return err
+		}
+		err = extractRawValue(sb, fieldDecoderType, c, fieldData)
 		if err != nil {
 			// Error extracting field - show error
-			fieldParts = append(fieldParts, fmt.Sprintf("%s = <error: %v>", field.Name, err))
+			_, err = sb.WriteString(fmt.Sprintf("%s = <error: %v>", field.Name, err))
+			if err != nil {
+				return err
+			}
 			continue
 		}
 
-		// Format as "fieldName = value"
-		fieldParts = append(fieldParts, fmt.Sprintf("%s = %s", field.Name, fieldValue))
 	}
-
-	if len(fieldParts) == 0 {
-		return fmt.Sprintf("<%s{}>", structType.GetName()), nil
+	_, err = sb.WriteString("}")
+	if err != nil {
+		return err
 	}
-
-	return fmt.Sprintf("%s{%s}", structType.GetName(), strings.Join(fieldParts, ", ")), nil
+	return nil
 }
 
 // extractArrayTypeValue formats array elements as a readable string: "[element1, element2, ...]"
-func extractArrayTypeValue(a *arrayType, c *encodingContext, data []byte) (string, error) {
+func extractArrayTypeValue(sb *strings.Builder, a *arrayType, c *encodingContext, data []byte) error {
 	arrayType := a.irType().(*ir.ArrayType)
 	elementSize := int(arrayType.Element.GetByteSize())
 	numElements := int(arrayType.Count)
@@ -357,10 +367,15 @@ func extractArrayTypeValue(a *arrayType, c *encodingContext, data []byte) (strin
 	}
 
 	// Get the decoder type for array elements
-	var elementParts []string
 	elementDecoderType, ok := c.typesByID[arrayType.Element.GetID()]
 	if !ok {
-		return fmt.Sprintf("[%d]<%s>", arrayType.Count, arrayType.Element.GetName()), nil
+		_, err := sb.WriteString(fmt.Sprintf("[%d]<%s>", arrayType.Count, arrayType.Element.GetName()))
+		return err
+	}
+
+	_, err := sb.WriteString(fmt.Sprintf("[%d]%s{", arrayType.Count, arrayType.Element.GetName()))
+	if err != nil {
+		return err
 	}
 
 	for i := 0; i < numElements; i++ {
@@ -371,35 +386,33 @@ func extractArrayTypeValue(a *arrayType, c *encodingContext, data []byte) (strin
 		}
 
 		elementData := data[offset:endIdx]
-		elementValue, err := extractRawValue(elementDecoderType, c, elementData)
+		err = extractRawValue(sb, elementDecoderType, c, elementData)
 		if err != nil {
-			elementParts = append(elementParts, fmt.Sprintf("<error: %v>", err))
-			continue
+			return err
 		}
-
-		elementParts = append(elementParts, elementValue)
+		if i < numElements-1 {
+			_, err = sb.WriteString(", ")
+			if err != nil {
+				return err
+			}
+		}
 	}
-
-	result := fmt.Sprintf("[%d]%s{%s}", numElements, elementDecoderType.irType().GetName(), strings.Join(elementParts, ", "))
-	if int(arrayType.Count) > maxElementsToShow {
-		result += fmt.Sprintf(" ...(%d more)", arrayType.Count-uint32(maxElementsToShow))
-	}
-
-	return result, nil
+	_, err = sb.WriteString("}")
+	return err
 }
 
 // extractSliceTypeValue formats slice elements as a readable string: "[element1, element2, ...]"
-func extractSliceTypeValue(s *goSliceHeaderType, c *encodingContext, data []byte) (string, error) {
+func extractSliceTypeValue(sb *strings.Builder, s *goSliceHeaderType, c *encodingContext, data []byte) error {
 	sliceType := s.irType().(*ir.GoSliceHeaderType)
 	if len(data) < 16 {
-		return "[]", nil
+		return errors.New("data too short for slice")
 	}
 	// Extract slice header: pointer (8 bytes) + length (8 bytes) + capacity (8 bytes)
 	address := binary.NativeEndian.Uint64(data[0:8])
 	length := binary.NativeEndian.Uint64(data[8:16])
 
 	if address == 0 || length == 0 {
-		return "[]", nil
+		return errors.New("slice address or length is 0")
 	}
 
 	// Get the slice data
@@ -408,12 +421,12 @@ func extractSliceTypeValue(s *goSliceHeaderType, c *encodingContext, data []byte
 		addr:   address,
 	}]
 	if !ok {
-		return fmt.Sprintf("slice[%d]", length), nil
+		return fmt.Errorf("slice data item not found at address %#x with len %d", address, length)
 	}
 
 	sliceData, ok := sliceDataItem.Data()
 	if !ok {
-		return fmt.Sprintf("slice[%d]", length), nil
+		return fmt.Errorf("slice data item marked as failed read")
 	}
 
 	elementSize := int(sliceType.Data.Element.GetByteSize())
@@ -425,158 +438,176 @@ func extractSliceTypeValue(s *goSliceHeaderType, c *encodingContext, data []byte
 		sliceLength = maxElementsToShow
 	}
 
-	var elementParts []string
-
 	// Get the decoder type for slice elements
 	elementDecoderType, ok := c.typesByID[sliceType.Data.Element.GetID()]
 	if !ok {
-		return fmt.Sprintf("slice[%d]<%s>", length, sliceType.Data.Element.GetName()), nil
+		return fmt.Errorf("no decoder type found for slice element type %s", sliceType.Data.Element.GetName())
 	}
 
+	_, err := sb.WriteString(fmt.Sprintf("[]%s{", sliceType.Data.Element.GetName()))
+	if err != nil {
+		return err
+	}
 	elementByteSize := int(sliceType.Data.Element.GetByteSize())
 	for i := 0; i < sliceLength; i++ {
 		elementData := sliceData[i*elementByteSize : (i+1)*elementByteSize]
-		elementValue, err := extractRawValue(elementDecoderType, c, elementData)
+		err := extractRawValue(sb, elementDecoderType, c, elementData)
 		if err != nil {
-			elementParts = append(elementParts, fmt.Sprintf("<error: %v>", err))
-			continue
+			return fmt.Errorf("error extracting raw value for slice element %d: %w", i, err)
 		}
 
-		elementParts = append(elementParts, elementValue)
+		if i < sliceLength-1 {
+			_, err = sb.WriteString(", ")
+			if err != nil {
+				return err
+			}
+		}
 	}
 
-	result := fmt.Sprintf("[]%s{%s}", sliceType.Data.Element.GetName(), strings.Join(elementParts, ", "))
-	if int(length) > maxElementsToShow {
-		result += fmt.Sprintf(" ...(%d more)", length-uint64(maxElementsToShow))
+	_, err = sb.WriteString("}")
+	if err != nil {
+		return err
 	}
 
-	return result, nil
+	return nil
 }
 
 // extractBaseTypeValue converts base type binary data to string
-func extractBaseTypeValue(b *baseType, data []byte) (string, error) {
+func extractBaseTypeValue(sb *strings.Builder, b *baseType, data []byte) error {
 	kind, ok := (*ir.BaseType)(b).GetGoKind()
 	if !ok {
-		return "", fmt.Errorf("no go kind for type %s", (*ir.BaseType)(b).GetName())
+		return fmt.Errorf("no go kind for type %s", (*ir.BaseType)(b).GetName())
 	}
-
+	var err error
 	switch kind {
 	case reflect.Bool:
 		if len(data) < 1 {
-			return "", errors.New("data too short for bool")
+			return errors.New("data too short for bool")
 		}
 		if data[0] == 1 {
-			return "true", nil
+			_, err = sb.WriteString("true")
+			return err
 		}
-		return "false", nil
-
+		_, err = sb.WriteString("false")
+		return err
 	case reflect.Int, reflect.Int64:
 		if len(data) < 8 {
-			return "", errors.New("data too short for int64")
+			return errors.New("data too short for int64")
 		}
 		val := int64(binary.NativeEndian.Uint64(data))
-		return strconv.FormatInt(val, 10), nil
-
+		_, err = sb.WriteString(strconv.FormatInt(val, 10))
+		return err
 	case reflect.Int32:
 		if len(data) < 4 {
-			return "", errors.New("data too short for int32")
+			return errors.New("data too short for int32")
 		}
 		val := int32(binary.NativeEndian.Uint32(data))
-		return strconv.FormatInt(int64(val), 10), nil
+		_, err = sb.WriteString(strconv.FormatInt(int64(val), 10))
+		return err
 
 	case reflect.Int16:
 		if len(data) < 2 {
-			return "", errors.New("data too short for int16")
+			return errors.New("data too short for int16")
 		}
 		val := int16(binary.NativeEndian.Uint16(data))
-		return strconv.FormatInt(int64(val), 10), nil
+		_, err = sb.WriteString(strconv.FormatInt(int64(val), 10))
+		return err
 
 	case reflect.Int8:
 		if len(data) < 1 {
-			return "", errors.New("data too short for int8")
+			return errors.New("data too short for int8")
 		}
 		val := int8(data[0])
-		return strconv.FormatInt(int64(val), 10), nil
+		_, err = sb.WriteString(strconv.FormatInt(int64(val), 10))
+		return err
 
 	case reflect.Uint, reflect.Uint64:
 		if len(data) < 8 {
-			return "", errors.New("data too short for uint64")
+			return errors.New("data too short for uint64")
 		}
 		val := binary.NativeEndian.Uint64(data)
-		return strconv.FormatUint(val, 10), nil
+		_, err = sb.WriteString(strconv.FormatUint(val, 10))
+		return err
 
 	case reflect.Uint32:
 		if len(data) < 4 {
-			return "", errors.New("data too short for uint32")
+			return errors.New("data too short for uint32")
 		}
 		val := binary.NativeEndian.Uint32(data)
-		return strconv.FormatUint(uint64(val), 10), nil
+		_, err = sb.WriteString(strconv.FormatUint(uint64(val), 10))
+		return err
 
 	case reflect.Uint16:
 		if len(data) < 2 {
-			return "", errors.New("data too short for uint16")
+			return errors.New("data too short for uint16")
 		}
 		val := binary.NativeEndian.Uint16(data)
-		return strconv.FormatUint(uint64(val), 10), nil
+		_, err = sb.WriteString(strconv.FormatUint(uint64(val), 10))
+		return err
 
 	case reflect.Uint8:
 		if len(data) < 1 {
-			return "", errors.New("data too short for uint8")
+			return errors.New("data too short for uint8")
 		}
-		return strconv.FormatUint(uint64(data[0]), 10), nil
+		_, err = sb.WriteString(strconv.FormatUint(uint64(data[0]), 10))
+		return err
 
 	case reflect.Float32:
 		if len(data) < 4 {
-			return "", errors.New("data too short for float32")
+			return errors.New("data too short for float32")
 		}
 		bits := binary.NativeEndian.Uint32(data)
 		val := math.Float32frombits(bits)
-		return strconv.FormatFloat(float64(val), 'g', -1, 32), nil
+		_, err = sb.WriteString(strconv.FormatFloat(float64(val), 'g', -1, 32))
+		return err
 
 	case reflect.Float64:
 		if len(data) < 8 {
-			return "", errors.New("data too short for float64")
+			return errors.New("data too short for float64")
 		}
 		bits := binary.NativeEndian.Uint64(data)
 		val := math.Float64frombits(bits)
-		return strconv.FormatFloat(val, 'g', -1, 64), nil
+		_, err = sb.WriteString(strconv.FormatFloat(val, 'g', -1, 64))
+		return err
 
 	case reflect.String:
 		// Go strings are not stored as baseType, this shouldn't happen
-		return string(data), nil
+		_, err = sb.WriteString(string(data))
+		return err
 
 	default:
-		return "", fmt.Errorf("unsupported base type kind: %v", kind)
+		return fmt.Errorf("unsupported base type kind: %v", kind)
 	}
 }
 
 // extractStringValue extracts string value from Go string header
-func extractStringValue(s *goStringHeaderType, c *encodingContext, data []byte) (string, error) {
+func extractStringValue(sb *strings.Builder, s *goStringHeaderType, c *encodingContext, data []byte) error {
 	fieldEnd := s.strFieldOffset + uint32(s.strFieldSize)
 	if fieldEnd >= uint32(len(data)) {
-		return "", nil
+		return nil
 	}
 	strLen := binary.NativeEndian.Uint64(data[s.lenFieldOffset : s.lenFieldOffset+uint32(s.lenFieldSize)])
 	address := binary.NativeEndian.Uint64(data[s.strFieldOffset : s.strFieldOffset+uint32(s.strFieldSize)])
 	if address == 0 || strLen == 0 {
-		return "", nil
+		return nil
 	}
 	stringValue, ok := c.dataItems[typeAndAddr{
 		irType: uint32(s.GoStringHeaderType.Data.GetID()),
 		addr:   address,
 	}]
 	if !ok {
-		return "", nil
+		return nil
 	}
 	stringData, ok := stringValue.Data()
 	if !ok {
-		return "", nil
+		return nil
 	}
 	if len(stringData) < int(strLen) {
-		return "", nil
+		return nil
 	}
 
-	return string(stringData[:strLen]), nil
+	_, err := sb.WriteString(string(stringData[:strLen]))
+	return err
 }
 
 func (ce *captureEvent) MarshalJSONTo(enc *jsontext.Encoder) error {
