55d5bda2d30227d3d2d7529c55d5292feeed2094
Gabriel Dos Santos <gabe.dossantos@datadoghq.com>
2025-10-02 21:26:26 +0000

    Base impl for cel workload exclude parsing
    Base filter compilation

--- comp/core/workloadfilter/catalog/container.go
+++ comp/core/workloadfilter/catalog/container.go
@@ -10,7 +10,6 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/DataDog/datadog-agent/comp/core/config"
 	log "github.com/DataDog/datadog-agent/comp/core/log/def"
 	workloadfilter "github.com/DataDog/datadog-agent/comp/core/workloadfilter/def"
 	"github.com/DataDog/datadog-agent/comp/core/workloadfilter/program"
@@ -18,53 +17,53 @@ import (
 )
 
 // LegacyContainerMetricsProgram creates a program for filtering container metrics
-func LegacyContainerMetricsProgram(config config.Component, logger log.Component) program.FilterProgram {
+func LegacyContainerMetricsProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
 	programName := "LegacyContainerMetricsProgram"
-	include := config.GetStringSlice("container_include_metrics")
-	exclude := config.GetStringSlice("container_exclude_metrics")
+	include := filterConfig.ContainerIncludeMetrics
+	exclude := filterConfig.ContainerExcludeMetrics
 	return createFromOldFilters(programName, include, exclude, workloadfilter.ContainerType, logger)
 }
 
 // LegacyContainerLogsProgram creates a program for filtering container logs
-func LegacyContainerLogsProgram(config config.Component, logger log.Component) program.FilterProgram {
+func LegacyContainerLogsProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
 	programName := "LegacyContainerLogsProgram"
-	include := config.GetStringSlice("container_include_logs")
-	exclude := config.GetStringSlice("container_exclude_logs")
+	include := filterConfig.ContainerIncludeLogs
+	exclude := filterConfig.ContainerExcludeLogs
 	return createFromOldFilters(programName, include, exclude, workloadfilter.ContainerType, logger)
 }
 
 // LegacyContainerACExcludeProgram creates a program for excluding containers via legacy `AC` filters
-func LegacyContainerACExcludeProgram(config config.Component, logger log.Component) program.FilterProgram {
+func LegacyContainerACExcludeProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
 	programName := "LegacyContainerACExcludeProgram"
-	exclude := config.GetStringSlice("ac_exclude")
+	exclude := filterConfig.ACExclude
 	return createFromOldFilters(programName, nil, exclude, workloadfilter.ContainerType, logger)
 }
 
 // LegacyContainerACIncludeProgram creates a program for including containers via legacy `AC` filters
-func LegacyContainerACIncludeProgram(config config.Component, logger log.Component) program.FilterProgram {
+func LegacyContainerACIncludeProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
 	programName := "LegacyContainerACIncludeProgram"
-	include := config.GetStringSlice("ac_include")
+	include := filterConfig.ACInclude
 	return createFromOldFilters(programName, include, nil, workloadfilter.ContainerType, logger)
 }
 
 // LegacyContainerGlobalProgram creates a program for filtering container globally
-func LegacyContainerGlobalProgram(config config.Component, logger log.Component) program.FilterProgram {
+func LegacyContainerGlobalProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
 	programName := "LegacyContainerGlobalProgram"
-	include := config.GetStringSlice("container_include")
-	exclude := config.GetStringSlice("container_exclude")
+	include := filterConfig.ContainerInclude
+	exclude := filterConfig.ContainerExclude
 	return createFromOldFilters(programName, include, exclude, workloadfilter.ContainerType, logger)
 }
 
 // LegacyContainerSBOMProgram creates a program for filtering container SBOMs
-func LegacyContainerSBOMProgram(config config.Component, logger log.Component) program.FilterProgram {
+func LegacyContainerSBOMProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
 	programName := "LegacyContainerSBOMProgram"
-	include := config.GetStringSlice("sbom.container_image.container_include")
-	exclude := config.GetStringSlice("sbom.container_image.container_exclude")
+	include := filterConfig.SBOMContainerInclude
+	exclude := filterConfig.SBOMContainerExclude
 	return createFromOldFilters(programName, include, exclude, workloadfilter.ContainerType, logger)
 }
 
 // ContainerPausedProgram creates a program for filtering paused containers
-func ContainerPausedProgram(_ config.Component, logger log.Component) program.FilterProgram {
+func ContainerPausedProgram(_ *FilterConfig, logger log.Component) program.FilterProgram {
 	programName := "ContainerPausedProgram"
 	var initErrors []error
 
@@ -94,4 +93,32 @@ func ContainerPausedProgram(_ config.Component, logger log.Component) program.Fi
 		},
 		InitializationErrors: initErrors,
 	}
+}
+
+// ContainerCELMetricsProgram creates a program for filtering container metrics via CEL rules
+func ContainerCELMetricsProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
+	programName := "ContainerCELMetricsProgram"
+	rule := filterConfig.GetCELRulesForProduct(workloadfilter.ProductMetrics, workloadfilter.ContainerType)
+	return createCELExcludeProgram(programName, rule, workloadfilter.ContainerType, logger)
+}
+
+// ContainerCELLogsProgram creates a program for filtering container logs via CEL rules
+func ContainerCELLogsProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
+	programName := "ContainerCELLogsProgram"
+	rule := filterConfig.GetCELRulesForProduct(workloadfilter.ProductLogs, workloadfilter.ContainerType)
+	return createCELExcludeProgram(programName, rule, workloadfilter.ContainerType, logger)
+}
+
+// ContainerCELSBOMProgram creates a program for filtering container SBOMs via CEL rules
+func ContainerCELSBOMProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
+	programName := "ContainerCELSBOMProgram"
+	rule := filterConfig.GetCELRulesForProduct(workloadfilter.ProductSBOM, workloadfilter.ContainerType)
+	return createCELExcludeProgram(programName, rule, workloadfilter.ContainerType, logger)
+}
+
+// ContainerCELGlobalProgram creates a program for filtering containers globally via CEL rules
+func ContainerCELGlobalProgram(filterConfig *FilterConfig, logger log.Component) program.FilterProgram {
+	programName := "ContainerCELGlobalProgram"
+	rule := filterConfig.GetCELRulesForProduct(workloadfilter.ProductGlobal, workloadfilter.ContainerType)
+	return createCELExcludeProgram(programName, rule, workloadfilter.ContainerType, logger)
 }
