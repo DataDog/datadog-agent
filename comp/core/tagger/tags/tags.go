c606888ed6e9a8ea0fb8bec47fd5a3f1cb0112ac
Dmitry <bitomaxsp@gmail.com>
2025-10-03 12:01:35 +0200

    [CONTINT-4776] Emit k8s cloud provider tag (#41173)
    ### What does this PR do?
    Emit `kube_cloud_provider` tag to signify that the current cluster is a managed cluster.
    
    ### Motivation
    Tag resources that belong to certain cloud providers
    
    ### Describe how you validated your changes
    Deployed a custom built agent and checked that `kube_cloud_provider` is attached to the metrics from a cluster.
    It was tested on local `kind` cluster with different node labels.
    
    Co-authored-by: sblumenthal <steven.blumenthal@datadoghq.com>
    (cherry picked from commit 39b79c850473c1e76cd0e741d70e86d7680b66d4)

--- comp/core/tagger/tags/tags.go
+++ comp/core/tagger/tags/tags.go
@@ -35,6 +35,8 @@ const (
 	KubeClusterName = "kube_cluster_name"
 	// OrchClusterID is the tag for the orchestrator cluster ID
 	OrchClusterID = "orch_cluster_id"
+	// KubeDistribution is the tag for the managed Kubernetes cloud provider: eks, gke, aks
+	KubeDistribution = "kube_distribution"
 
 	// ImageName is the tag for the image name
 	ImageName = "image_name"
