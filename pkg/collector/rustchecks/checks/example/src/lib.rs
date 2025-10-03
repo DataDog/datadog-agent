954a26c7c6fbfa88632bad1c4ee3fbe81712f3c4
Maxime Chambre <maxime.chambre@datadoghq.com>
2025-10-03 18:06:24 +0200

    refactor: simplify rust check example

--- pkg/collector/rustchecks/checks/example/src/lib.rs
+++ pkg/collector/rustchecks/checks/example/src/lib.rs
@@ -1,13 +1,9 @@
-use rust_check_core::{generate_ffi, AgentCheck, ServiceCheckStatus};
+use rust_check_core::{generate_ffi, AgentCheck};
 use std::error::Error;
 
 /// Check implementation
 pub fn check_implementation(check: &AgentCheck) -> Result<(), Box<dyn Error>> {
     check.gauge("hello.gauge", 1.0, &vec![], "", false)?;
-    check.service_check("hello.service_check", ServiceCheckStatus::OK, &vec![], "", "hello")?;
-    check.event("hello", "hello world", 123, "priority", "", &vec![], "alert_type", "aggregation_key", "source_type_name", "event_type")?;
-    check.submit_histogram_bucket("hello.histogram", 1234, 2.0, 1.0, 0, "", &vec![], false)?;
-    //check.event_platform_event(raw_event_pointer, raw_event_size, event_type)?;
 
     Ok(())
 }
