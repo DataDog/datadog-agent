cd3e7af39b2e00abe26d0f161d319b2997adec2a
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2025-10-03 17:20:55 +0200

    fix wg

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -1079,7 +1079,7 @@ func InitConfig(config pkgconfigmodel.Setup) {
 	} else {
 		config.BindEnvAndSetDefault("runtime_security_config.socket", filepath.Join(InstallPath, "run/runtime-security.sock"))
 	}
-	config.BindEnv("runtime_security_config.cmd_socket")
+	config.BindEnvAndSetDefault("runtime_security_config.cmd_socket", "")
 	config.BindEnvAndSetDefault("runtime_security_config.use_secruntime_track", true)
 	bindEnvAndSetLogsConfigKeys(config, "runtime_security_config.endpoints.")
 	bindEnvAndSetLogsConfigKeys(config, "runtime_security_config.activity_dump.remote_storage.endpoints.")
