0125672369833bb32b8f0ec18bdb30430ec05e1c
Andrei Matei <andrei.matei@datadoghq.com>
2025-10-03 16:35:34 -0400

    debug

--- pkg/trace/api/debugger.go
+++ pkg/trace/api/debugger.go
@@ -88,6 +88,8 @@ func debuggerErrorHandler(err error) http.Handler {
 
 // newDebuggerProxy returns a new httputil.ReverseProxy proxying and augmenting requests with headers containing the tags.
 func newDebuggerProxy(conf *config.AgentConfig, transport http.RoundTripper, hostTags string) *httputil.ReverseProxy {
+	log.Info("DEBUG: newDebuggerProxy")
+	log.Warn("DEBUG: newDebuggerProxy")
 	cidProvider := NewIDProvider(conf.ContainerProcRoot, conf.ContainerIDFromOriginInfo)
 	logger := log.NewThrottled(5, 10*time.Second) // limit to 5 messages every 10 seconds
 	return &httputil.ReverseProxy{
