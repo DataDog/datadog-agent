b6dd06b787fcb818a6195008e297197a5229ee62
s-alad <saad.naji@datadoghq.com>
2025-10-04 13:46:59 -0400

    bump TestHTTPTransactionFieldsCount field number

--- comp/forwarder/defaultforwarder/internal/retry/http_transactions_serializer_test.go
+++ comp/forwarder/defaultforwarder/internal/retry/http_transactions_serializer_test.go
@@ -201,7 +201,7 @@ func TestHTTPTransactionSerializerUpdateAPIKeyBeforeSerializing(t *testing.T) {
 func TestHTTPTransactionFieldsCount(t *testing.T) {
 	tr := transaction.HTTPTransaction{}
 	transactionType := reflect.TypeOf(tr)
-	assert.Equalf(t, 13, transactionType.NumField(),
+	assert.Equalf(t, 14, transactionType.NumField(),
 		"A field was added or remove from HTTPTransaction. "+
 			"You probably need to update the implementation of "+
 			"HTTPTransactionsSerializer and then adjust this unit test.")
