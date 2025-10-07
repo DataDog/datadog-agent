d04111345f91f0e4f0148b0591112f592fd1daf9
Arthur Bellal <arthur.bellal@datadoghq.com>
2025-10-07 17:29:26 +0200

    lint

--- test/new-e2e/tests/installer/unix/package_definitions.go
+++ test/new-e2e/tests/installer/unix/package_definitions.go
@@ -139,6 +139,7 @@ func InstallInstallerScriptEnvWithPackages() map[string]string {
 	return env
 }
 
+// PipelineAgentVersion returns the version of the pipeline agent
 func PipelineAgentVersion(t *testing.T) string {
 	ref := fmt.Sprintf("oci://installtesting.datad0g.com.internal.dda-testing.com/agent-package:pipeline-%s", os.Getenv("E2E_PIPELINE_ID"))
 	p := v1.Platform{
