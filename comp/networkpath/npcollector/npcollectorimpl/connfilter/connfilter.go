72f244e9ce627c802433d0b3001c6fb546abbfd5
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-10-07 22:27:38 +0200

    clean up

--- comp/networkpath/npcollector/npcollectorimpl/connfilter/connfilter.go
+++ comp/networkpath/npcollector/npcollectorimpl/connfilter/connfilter.go
@@ -47,26 +47,6 @@ type ConnFilter struct {
 	filters []Filter
 }
 
-// network_path:
-//  collector:
-//    filter:
-//      - match_domain: '*.datadoghq.com'
-//        type: include
-//      - match_domain: '*.google.com'
-//        type: include
-//      - match_ip: <IP or CIDR>
-//        type: include
-//      - match_domain: '*.zoom.us'
-//        match_domain_strategy: wildcard                 # wildcard (default) | regex
-//        type: exclude
-//      - match_domain: '.*\.zoom\.us'
-//        match_domain_strategy: regex
-//        type: exclude
-//      - match_ip: <IP or CIDR>
-//        # match_port: <port>                            # add later if user ask for it
-//        # match_protocol: <TCP | UDP | ICMP>            # add later if user ask for it
-//        type: exclude
-
 // NewConnFilter constructor
 func NewConnFilter(config []Config, site string) (*ConnFilter, []error) {
 	// TODO: test compile error
