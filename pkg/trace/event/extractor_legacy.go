6bb9a92b0b4ae936844b30cc51edead4a3312e22
Andrew Glaude <andrew.glaude@datadoghq.com>
2025-10-08 10:13:21 -0400

    Update pkg/trace/event/extractor_legacy.go
    Co-authored-by: Iñigo López de Heredia <inigo.lheredia@gmail.com>

--- pkg/trace/event/extractor_legacy.go
+++ pkg/trace/event/extractor_legacy.go
@@ -46,7 +46,7 @@ func (e *legacyExtractor) Extract(s *pb.Span, _ sampler.SamplingPriority) (float
 	return extractionRate, true
 }
 
-// Extract decides to extract an apm event from the provided span if there's an extraction rate configured for that
+// ExtractV1 decides to extract an apm event from the provided span if there's an extraction rate configured for that
 // span's service. In this case the extracted event is returned along with the found extraction rate and a true value.
 // If this rate doesn't exist or the provided span is not a top level one, then no extraction is done and false is
 // returned as the third value, with the others being invalid.
