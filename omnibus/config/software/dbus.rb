558455692a432c85d827715b4d6dc8ae4789e224
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-10-10 15:15:21 +0200

    omnibus: update dbus to 1.16.2
    Aside from updating for the sake of getting fixes/new features, this
    version supports the meson build system as the primary way of building
    dbus.
    This exposes a way to disable the message_bus, which in turns allows us
    to drop the expat dependency

--- omnibus/config/software/dbus.rb
+++ omnibus/config/software/dbus.rb
@@ -4,9 +4,8 @@
 # Copyright 2016-present Datadog, Inc.
 
 name "dbus"
-default_version "1.14.10"
+default_version "1.16.2"
 
-dependency "expat"
 dependency "libtool"
 dependency "systemd"
 
@@ -14,7 +13,7 @@ license "AFL-2.1"
 license_file "LICENSES/AFL-2.1.txt"
 skip_transitive_dependency_licensing true
 
-version("1.14.10") { source sha256: "ba1f21d2bd9d339da2d4aa8780c09df32fea87998b73da24f49ab9df1e36a50f" }
+version("1.16.2") { source sha256: "0ba2a1a4b16afe7bceb2c07e9ce99a8c2c3508e5dec290dbb643384bd6beb7e2" }
 
 source url: "https://dbus.freedesktop.org/releases/dbus/dbus-#{version}.tar.xz"
 
@@ -23,20 +22,23 @@ relative_path "dbus-#{version}"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  configure_options = [
-    "--disable-static",
-    "--disable-doxygen-docs",
-    "--disable-ducktype-docs",
-    "--disable-xml-docs",
-    "--disable-stats",
-    "--disable-tests",
-    "--without-x",
+  meson_options = [
+    "-Dmessage_bus=false",
+    "-Ddoxygen_docs=disabled",
+    "-Dducktype_docs=disabled",
+    "-Dxml_docs=disabled",
+    "-Dstats=false",
+    "-Dtools=false",
+    "-Dmodular_tests=disabled",
+    "--prefix=#{install_dir}/embedded",
+    "-Dbuildtype=release",
   ]
 
-  configure(*configure_options, env: env)
-
-  make "-j #{workers}", env: env
-  make "install", env: env
+  # meson requires a dedicated build folder
+  meson_build_dir = "#{project_dir}/build"
+  command "mkdir #{meson_build_dir}", env: env
+  command "meson setup " + meson_options.join(' ').strip + " ..", cwd: meson_build_dir
+  command "ninja install", env: env, cwd: meson_build_dir
 
   # Remove dbus tools.
   delete "#{install_dir}/embedded/bin/dbus-*"
