e247f1541195b9c291b75c4bd60b7500b809a8f0
Ella Taira <ella.taira@datadoghq.com>
2025-10-10 13:20:49 -0400

    keep draining tailer registered? (dont remove())

--- pkg/logs/launchers/file/launcher.go
+++ pkg/logs/launchers/file/launcher.go
@@ -504,10 +504,6 @@ func (s *Launcher) rotateTailerWithoutRestart(oldTailer *tailer.Tailer, file *ta
 	log.Info("Log rotation happened to ", file.Path)
 	oldTailer.StopAfterFileRotation()
 
-	// Remove the rotated tailer from the active container so a fresh tailer can
-	// be created for the new file while this one finishes draining the old file.
-	s.tailers.Remove(oldTailer)
-
 	oldRegexPattern := oldTailer.GetDetectedPattern()
 	oldInfoRegistry := oldTailer.GetInfo()
 
