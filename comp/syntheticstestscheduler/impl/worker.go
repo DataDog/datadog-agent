f5aa3653039255726f9e14beab69460d1ce63547
Rafael Oliveira <rafael.oliveira@datadoghq.com>
2025-10-13 17:11:33 +0200

    correctly handle merge conflict

--- comp/syntheticstestscheduler/impl/worker.go
+++ comp/syntheticstestscheduler/impl/worker.go
@@ -308,11 +308,16 @@ func (s *syntheticsTestScheduler) networkPathToTestResult(w *workerResult) (*com
 		}
 	} else {
 		for _, res := range w.assertionResult {
-			if !res.Valid || res.Failure.Code != "" {
+			if !res.Valid {
 				result.Status = "failed"
+				assertionResultJSON, err := json.Marshal(w.assertionResult)
+				message := "Assertions failed"
+				if err == nil {
+					message = string(assertionResultJSON)
+				}
 				result.Failure = common.APIError{
 					Code:    incorrectAssertion,
-					Message: w.tracerouteError.Error(),
+					Message: message,
 				}
 			}
 		}
