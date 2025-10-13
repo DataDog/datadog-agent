916e9e069750f4b564d8d138f9a7bcbd4858efd7
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2025-10-13 10:35:36 +0200

    align if cond

--- cmd/agent/subcommands/run/dependent_services_windows.go
+++ cmd/agent/subcommands/run/dependent_services_windows.go
@@ -144,10 +144,8 @@ func (s *Servicedef) Stop() error {
 // IsEnabled checks to see if a given service should be started
 func (s *Servicedef) IsEnabled() bool {
 	// In infrastructure basic mode, process and cluster agents should not be started
-	if pkgconfigsetup.Datadog().GetString("infrastructure_mode") == "basic" {
-		if s.name == "process" {
-			return false
-		}
+	if pkgconfigsetup.Datadog().GetString("infrastructure_mode") == "basic" && s.name == "process" {
+		return false
 	}
 
 	for configKey, cfg := range s.configKeys {
