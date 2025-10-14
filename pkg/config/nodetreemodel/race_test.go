DataDog/datadog-agent

commit 05e00bf3dd1da372b71d9d422056d09c43c572c4

Prepare fixes for tests when nodetreemodel is used by default (#35462)

comp/core/configsync/configsyncimpl/sync_integration_test.go
@@ -18,7 +18,7 @@ import (
 	"go.uber.org/atomic"
 
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
-	viperconfig "github.com/DataDog/datadog-agent/pkg/config/viperconfig"
+	"github.com/DataDog/datadog-agent/pkg/config/viperconfig"
 )
 
 func TestRunWithChan(t *testing.T) {
@@ -91,6 +91,7 @@ func TestRunWithChan(t *testing.T) {
 func TestRunWithInterval(t *testing.T) {
 	// Legitimate use for NewConfig case where we want to have 2 independent config object mimicing 2 process communicating
 	configCore := viperconfig.NewConfig("test", "DD", strings.NewReplacer(".", "_")) // nolint: forbidigo
+	configCore.SetTestOnlyDynamicSchema(true)
 	configCore.Set("api_key", "api_key_core1", pkgconfigmodel.SourceFile)
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {

pkg/clusteragent/admission/controllers/webhook/controller_v1_test.go
@@ -1050,7 +1050,7 @@ func TestGetValidatingWebhookSkeletonV1(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if tt.timeout != nil {
 				mockConfig.SetWithoutSource("admission_controller.timeout_seconds", *tt.timeout)
-				defer mockConfig.SetDefault("admission_controller.timeout_seconds", defaultTimeout)
+				defer mockConfig.UnsetForSource("admission_controller.timeout_seconds", model.SourceUnknown)
 			}
 
 			c := &ControllerV1{}

pkg/collector/corechecks/networkpath/config_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 func TestNewCheckConfig(t *testing.T) {
-	setup.Datadog().SetDefault("network_devices.namespace", "my-namespace")
+	setup.Datadog().SetWithoutSource("network_devices.namespace", "my-namespace")
 	tests := []struct {
 		name           string
 		rawInstance    integration.Data

pkg/config/mock/mock.go
@@ -16,7 +16,7 @@ import (
 
 	"github.com/DataDog/datadog-agent/pkg/config/model"
 	"github.com/DataDog/datadog-agent/pkg/config/setup"
-	viperconfig "github.com/DataDog/datadog-agent/pkg/config/viperconfig"
+	"github.com/DataDog/datadog-agent/pkg/config/viperconfig"
 )
 
 var (
@@ -53,6 +53,7 @@ func New(t testing.TB) model.Config {
 	// Configuration defaults
 	setup.SetDatadog(newCfg) // nolint forbidigo legitimate use of SetDatadog
 	setup.InitConfig(newCfg)
+	newCfg.SetTestOnlyDynamicSchema(true)
 	return &mockConfig{newCfg}
 }
 
@@ -102,5 +103,6 @@ func NewSystemProbe(t testing.TB) model.Config {
 	setup.SetSystemProbe(viperconfig.NewConfig("system-probe", "DD", strings.NewReplacer(".", "_"))) // nolint forbidigo legitimate use of NewConfig and SetSystemProbe
 	// Configuration defaults
 	setup.InitSystemProbeConfig(setup.SystemProbe())
+	setup.SystemProbe().SetTestOnlyDynamicSchema(true)
 	return &mockConfig{setup.SystemProbe()}
 }

pkg/config/model/types.go
@@ -157,6 +157,11 @@ type Reader interface {
 	// Note that it returns the keys lowercased.
 	AllKeysLowercased() []string
 
+	// SetTestOnlyDynamicSchema is used by tests to disable validation of the config schema
+	// This lets tests use the config is more flexible ways (can add to the schema at any point,
+	// can modify env vars and the config will rebuild itself, etc)
+	SetTestOnlyDynamicSchema(allow bool)
+
 	// IsSet return true if a non nil values is found in the configuration, including defaults. This is legacy
 	// behavior from viper and don't answer the need to know if something was set by the user (see IsConfigured for
 	// this).

pkg/config/nodetreemodel/config.go
@@ -63,6 +63,12 @@ type ntmConfig struct {
 	// 'schema' and 'defaults' fields.
 	schema InnerNode
 
+	// flag only used by tests, allows tests to treat the config as though its schema was dynamic
+	// - it's okay to read the config before its schema is built
+	// - it's okay to modify the config schema after it gets built
+	// - unknown keys can be assigned and retrieved
+	allowDynamicSchema *atomic.Bool
+
 	// defaults contains the settings with a default value
 	defaults InnerNode
 	// unknown contains the settings set at runtime from unknown source. This should only evey be used by tests.
@@ -168,8 +174,15 @@ func (c *ntmConfig) getTreeBySource(source model.Source) (InnerNode, error) {
 	return nil, fmt.Errorf("invalid source tree: %s", source)
 }
 
+// SetTestOnlyDynamicSchema allows more flexible usage of the config, should only be used by tests
+func (c *ntmConfig) SetTestOnlyDynamicSchema(allow bool) {
+	c.allowDynamicSchema.Store(allow)
+}
+
 // Set assigns the newValue to the given key and marks it as originating from the given source
 func (c *ntmConfig) Set(key string, newValue interface{}, source model.Source) {
+	c.maybeRebuild()
+
 	tree, err := c.getTreeBySource(source)
 	if err != nil {
 		log.Errorf("Set invalid source: %s", source)
@@ -177,10 +190,13 @@ func (c *ntmConfig) Set(key string, newValue interface{}, source model.Source) {
 	}
 
 	if !c.IsKnown(key) {
-		log.Errorf("could not set '%s' unknown key", key)
-		return
+		if c.allowDynamicSchema.Load() {
+			log.Errorf("set value for unknown key '%s'", key)
+		} else {
+			log.Errorf("could not set '%s' unknown key", key)
+			return
+		}
 	}
-
 	// convert the key to lower case for the logs line and the notification
 	key = strings.ToLower(key)
 
@@ -223,7 +239,7 @@ func (c *ntmConfig) SetDefault(key string, value interface{}) {
 	c.Lock()
 	defer c.Unlock()
 
-	if c.isReady() {
+	if c.isReady() && !c.allowDynamicSchema.Load() {
 		panic("cannot SetDefault() once the config has been marked as ready for use")
 	}
 	key = strings.ToLower(key)
@@ -334,7 +350,7 @@ func (c *ntmConfig) addToKnownKeys(key string) {
 func (c *ntmConfig) SetKnown(key string) {
 	c.Lock()
 	defer c.Unlock()
-	if c.isReady() {
+	if c.isReady() && !c.allowDynamicSchema.Load() {
 		panic("cannot SetKnown() once the config has been marked as ready for use")
 	}
 
@@ -356,11 +372,23 @@ func (c *ntmConfig) isKnownKey(key string) bool {
 	return found
 }
 
+func (c *ntmConfig) maybeRebuild() {
+	if c.allowDynamicSchema.Load() {
+		// building the schema may access data from the config, disable the dynamic schema
+		// flag to prevent recursive rebuilds
+		c.allowDynamicSchema.Store(false)
+		defer func() { c.allowDynamicSchema.Store(true) }()
+		c.buildSchema()
+	}
+}
+
 // checkKnownKey checks if a key is known, and if not logs a warning
 // Only a single warning will be logged per unknown key.
 //
 // Must be called with the lock read-locked.
 func (c *ntmConfig) checkKnownKey(key string) {
+	c.maybeRebuild()
+
 	if c.isKnownKey(key) {
 		return
 	}
@@ -403,6 +431,8 @@ func (c *ntmConfig) mergeAllLayers() error {
 }
 
 func (c *ntmConfig) computeAllSettings(node InnerNode, path string) []string {
+	c.maybeRebuild()
+
 	knownKeys := []string{}
 	for _, name := range node.ChildrenKeys() {
 		newPath := joinKey(path, name)
@@ -428,6 +458,10 @@ func (c *ntmConfig) computeAllSettings(node InnerNode, path string) []string {
 func (c *ntmConfig) BuildSchema() {
 	c.Lock()
 	defer c.Unlock()
+	c.buildSchema()
+}
+
+func (c *ntmConfig) buildSchema() {
 	c.buildEnvVars()
 	c.ready.Store(true)
 	if err := c.mergeAllLayers(); err != nil {
@@ -517,10 +551,12 @@ func (c *ntmConfig) ParseEnvAsSlice(key string, fn func(string) []interface{}) {
 
 // IsSet checks if a key is set in the config
 func (c *ntmConfig) IsSet(key string) bool {
+	c.maybeRebuild()
+
 	c.RLock()
 	defer c.RUnlock()
 
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		log.Errorf("attempt to read key before config is constructed: %s", key)
 		return false
 	}
@@ -558,7 +594,7 @@ func (c *ntmConfig) IsConfigured(key string) bool {
 	c.RLock()
 	defer c.RUnlock()
 
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		log.Errorf("attempt to read key before config is constructed: %s", key)
 		return false
 	}
@@ -611,7 +647,7 @@ func (c *ntmConfig) nodeAtPathFromNode(key string, curr Node) Node {
 
 // GetNode returns a Node for the given key
 func (c *ntmConfig) GetNode(key string) (Node, error) {
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		return nil, log.Errorf("attempt to read key before config is constructed: %s", key)
 	}
 	pathParts := splitKey(key)
@@ -643,7 +679,7 @@ func (c *ntmConfig) BindEnv(key string, envvars ...string) {
 	c.Lock()
 	defer c.Unlock()
 
-	if c.isReady() {
+	if c.isReady() && !c.allowDynamicSchema.Load() {
 		panic("cannot BindEnv() once the config has been marked as ready for use")
 	}
 	key = strings.ToLower(key)
@@ -668,7 +704,7 @@ func (c *ntmConfig) BindEnv(key string, envvars ...string) {
 func (c *ntmConfig) SetEnvKeyReplacer(r *strings.Replacer) {
 	c.Lock()
 	defer c.Unlock()
-	if c.isReady() {
+	if c.isReady() && !c.allowDynamicSchema.Load() {
 		panic("cannot SetEnvKeyReplacer() once the config has been marked as ready for use")
 	}
 	c.envKeyReplacer = r
@@ -688,7 +724,7 @@ func (c *ntmConfig) MergeConfig(in io.Reader) error {
 	c.Lock()
 	defer c.Unlock()
 
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		return fmt.Errorf("attempt to MergeConfig before config is constructed")
 	}
 
@@ -744,6 +780,7 @@ func (c *ntmConfig) MergeFleetPolicy(configPath string) error {
 func (c *ntmConfig) AllSettings() map[string]interface{} {
 	c.RLock()
 	defer c.RUnlock()
+	c.maybeRebuild()
 
 	return c.root.DumpSettings(func(model.Source) bool { return true })
 }
@@ -752,6 +789,7 @@ func (c *ntmConfig) AllSettings() map[string]interface{} {
 func (c *ntmConfig) AllSettingsWithoutDefault() map[string]interface{} {
 	c.RLock()
 	defer c.RUnlock()
+	c.maybeRebuild()
 
 	// We only want to include leaf with a source higher than SourceDefault
 	return c.root.DumpSettings(func(source model.Source) bool { return source.IsGreaterThan(model.SourceDefault) })
@@ -874,6 +912,7 @@ func (c *ntmConfig) Object() model.Reader {
 func NewConfig(name string, envPrefix string, envKeyReplacer *strings.Replacer) model.Config {
 	config := ntmConfig{
 		ready:              atomic.NewBool(false),
+		allowDynamicSchema: atomic.NewBool(false),
 		configEnvVars:      map[string][]string{},
 		knownKeys:          map[string]struct{}{},
 		allSettings:        []string{},

pkg/config/nodetreemodel/getter.go
@@ -17,7 +17,7 @@ import (
 )
 
 func (c *ntmConfig) leafAtPath(key string) LeafNode {
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		log.Errorf("attempt to read key before config is constructed: %s", key)
 		return missingLeaf
 	}
@@ -131,10 +131,11 @@ simplyCopy:
 }
 
 func (c *ntmConfig) getNodeValue(key string) interface{} {
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		log.Errorf("attempt to read key before config is constructed: %s", key)
 		return missingLeaf
 	}
+	c.maybeRebuild()
 
 	node := c.nodeAtPathFromNode(key, c.root)
 

pkg/config/nodetreemodel/read_config_file.go
@@ -33,7 +33,7 @@ func (c *ntmConfig) findConfigFile() {
 
 // ReadInConfig wraps Viper for concurrent access
 func (c *ntmConfig) ReadInConfig() error {
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		return log.Errorf("attempt to ReadInConfig before config is constructed")
 	}
 
@@ -57,7 +57,7 @@ func (c *ntmConfig) ReadInConfig() error {
 
 // ReadConfig wraps Viper for concurrent access
 func (c *ntmConfig) ReadConfig(in io.Reader) error {
-	if !c.isReady() {
+	if !c.isReady() && !c.allowDynamicSchema.Load() {
 		return log.Errorf("attempt to ReadConfig before config is constructed")
 	}
 
@@ -91,7 +91,7 @@ func (c *ntmConfig) readConfigurationContent(target InnerNode, source model.Sour
 			return err
 		}
 	}
-	c.warnings = append(c.warnings, loadYamlInto(target, source, inData, "", c.schema)...)
+	c.warnings = append(c.warnings, loadYamlInto(target, source, inData, "", c.schema, c.allowDynamicSchema.Load())...)
 	return nil
 }
 
@@ -122,7 +122,7 @@ func toMapStringInterface(data any, path string) (map[string]interface{}, error)
 
 // loadYamlInto traverses input data parsed from YAML, checking if each node is defined by the schema.
 // If found, the value from the YAML blob is imported into the 'dest' tree. Otherwise, a warning will be created.
-func loadYamlInto(dest InnerNode, source model.Source, inData map[string]interface{}, atPath string, schema InnerNode) []string {
+func loadYamlInto(dest InnerNode, source model.Source, inData map[string]interface{}, atPath string, schema InnerNode, allowDynamicSchema bool) []string {
 	warnings := []string{}
 	for key, value := range inData {
 		key = strings.ToLower(key)
@@ -132,7 +132,13 @@ func loadYamlInto(dest InnerNode, source model.Source, inData map[string]interfa
 		schemaChild, err := schema.GetChild(key)
 		if err != nil {
 			warnings = append(warnings, fmt.Sprintf("unknown key from YAML: %s", currPath))
-			continue
+			if !allowDynamicSchema {
+				continue
+			} else if isScalar(value) {
+				schemaChild = newLeafNode(value, model.SourceSchema)
+			} else {
+				schemaChild = newInnerNode(make(map[string]Node))
+			}
 		}
 
 		// if the node in the schema is a leaf, then we create a new leaf in dest
@@ -157,7 +163,7 @@ func loadYamlInto(dest InnerNode, source model.Source, inData map[string]interfa
 
 		if !dest.HasChild(key) {
 			destChildInner := newInnerNode(nil)
-			warnings = append(warnings, loadYamlInto(destChildInner, source, childValue, currPath, schemaInner)...)
+			warnings = append(warnings, loadYamlInto(destChildInner, source, childValue, currPath, schemaInner, allowDynamicSchema)...)
 			dest.InsertChildNode(key, destChildInner)
 			continue
 		}
@@ -169,7 +175,7 @@ func loadYamlInto(dest InnerNode, source model.Source, inData map[string]interfa
 			warnings = append(warnings, "invalid tree: default and dest tree don't have the same layout")
 			continue
 		}
-		warnings = append(warnings, loadYamlInto(destChildInner, source, childValue, currPath, schemaInner)...)
+		warnings = append(warnings, loadYamlInto(destChildInner, source, childValue, currPath, schemaInner, allowDynamicSchema)...)
 	}
 	return warnings
 }

commit 37f3d7b24d3c9fe9ccf71e95b04dadc3d5ea651b

Add reproduction test and bug report for nodetreemodel data race
CI failures in tests_nodetreemodel revealed a data race where concurrent
GetBool() calls can trigger writes to c.root while other goroutines read
from it, all while holding only read locks (RLock). The race occurs when
SetTestOnlyDynamicSchema is enabled (used by configmock.New in all tests).

The bug was introduced in commit 05e00bf3dd1 (March 28, 2025) and exists
in the config system itself, not in code that uses it.

This commit adds:
- Minimal reproduction test in pkg/config/nodetreemodel/race_test.go
- Comprehensive bug report with root cause analysis and proposed fixes

🤖 Generated with [Claude Code](https://claude.com/claude-code)

Co-Authored-By: Claude <noreply@anthropic.com>

pkg/config/nodetreemodel/race_test.go
@@ -1,0 +1,109 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+package nodetreemodel
+
+import (
+	"strings"
+	"sync"
+	"testing"
+	"time"
+)
+
+// TestConcurrentGetBoolDataRace reproduces a data race in nodetreemodel where
+// concurrent GetBool() calls trigger writes to c.root while other goroutines
+// are reading from it, all while holding only read locks (RLock).
+//
+// This test reproduces the exact race condition seen in CI test failures:
+// TestRaceFlushVersusParsePacket in pkg/serverless/metrics/metric_test.go
+//
+// The race occurs when:
+// 1. SetTestOnlyDynamicSchema(true) is enabled (used by configmock.New in tests)
+// 2. Multiple goroutines concurrently call GetBool/GetString/GetInt
+// 3. One goroutine triggers: GetBool() → checkKnownKey() → maybeRebuild() →
+//    buildSchema() → mergeAllLayers() which WRITES to c.root at line 483
+// 4. Another goroutine is reading: GetBool() → getNodeValue() which READS
+//    from c.root at line 141
+// 5. Both hold READ locks (RLock), but one performs a WRITE = DATA RACE
+//
+// Root cause: Commit 05e00bf3dd1 (March 28, 2025) added maybeRebuild() which
+// is called from checkKnownKey() while the caller holds only a read lock.
+//
+// Run with: go test -race -run TestConcurrentGetBoolDataRace ./pkg/config/nodetreemodel -v
+func TestConcurrentGetBoolDataRace(t *testing.T) {
+	// Create a config exactly as configmock.New() does
+	cfg := NewNodeTreeConfig("test", "DD", strings.NewReplacer(".", "_"))
+
+	// Set up a few keys with defaults (mimics what real tests do)
+	cfg.SetKnown("telemetry.enabled")
+	cfg.SetKnown("use_v2_api.series")
+	cfg.SetDefault("telemetry.enabled", true)
+	cfg.SetDefault("use_v2_api.series", false)
+
+	// Build schema and enable dynamic schema mode
+	// This is the critical step that enables the buggy code path
+	cfg.BuildSchema()
+	cfg.SetTestOnlyDynamicSchema(true)
+
+	// Now simulate concurrent config access as happens in production tests
+	var wg sync.WaitGroup
+	duration := 2 * time.Second
+	stop := time.After(duration)
+
+	// Goroutine 1: Simulates pkg/serializer.SendIterableSeries() reading config
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-stop:
+				return
+			default:
+				// This can trigger maybeRebuild() which WRITES to c.root
+				_ = cfg.GetBool("use_v2_api.series")
+			}
+		}
+	}()
+
+	// Goroutine 2: Simulates pkg/aggregator.TimeSampler.sendTelemetry() reading config
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-stop:
+				return
+			default:
+				// This calls getNodeValue() which READS from c.root
+				_ = cfg.GetBool("telemetry.enabled")
+			}
+		}
+	}()
+
+	// Goroutine 3: Add a third concurrent reader to increase race likelihood
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-stop:
+				return
+			default:
+				_ = cfg.GetBool("use_v2_api.series")
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	// If running with -race flag, the race detector will report:
+	// "WARNING: DATA RACE
+	//  Write at 0x... by goroutine X:
+	//    pkg/config/nodetreemodel.(*ntmConfig).mergeAllLayers()
+	//        config.go:483
+	//  Previous read at 0x... by goroutine Y:
+	//    pkg/config/nodetreemodel.(*ntmConfig).getNodeValue()
+	//        getter.go:141"
+}