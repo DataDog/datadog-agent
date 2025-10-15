4e3d54fdb7cd57e74184b0acf910429be7e74c3b
kovags <marcio.kovags@datadoghq.com>
2025-10-15 15:18:13 +0200

    add event processing metrics

--- pkg/security/metrics/metrics.go
+++ pkg/security/metrics/metrics.go
@@ -536,6 +536,20 @@ var (
 	// MetricSecurityProfileV2CleanupProfilesRemoved is the name of the metric used to report profiles removed after cleanup delay
 	// Tags: -
 	MetricSecurityProfileV2CleanupProfilesRemoved = newRuntimeMetric(".security_profile_v2.cleanup.profiles_removed")
+
+	// Event processing time metrics
+
+	// MetricNameEventProcessingTimeMean is the name of the metric used to report the mean time an event took to be processed
+	// Tags: event_type
+	MetricNameEventProcessingTimeMean = newRuntimeMetric(".event_processing_time_microsec.mean")
+
+	// MetricNameEventProcessingTimeStddev is the name of the metric used to report the standard deviation of the time an event took to be processed
+	// Tags: event_type
+	MetricNameEventProcessingTimeStddev = newRuntimeMetric(".event_processing_time_microsec.stddev")
+
+	// MetricNameEventProcessingTimeMaximum is the name of the metric used to report the maximum time an event took to be processed
+	// Tags: event_type
+	MetricNameEventProcessingTimeMaximum = newRuntimeMetric(".event_processing_time_microsec.max")
 )
 
 var (
