1cfca0bebb1298a81d77fd056eab5affa99f9b97
kovags <marcio.kovags@datadoghq.com>
2025-10-15 18:15:44 +0200

    small tiny fix

--- pkg/security/probe/probe_ebpf.go
+++ pkg/security/probe/probe_ebpf.go
@@ -1109,7 +1109,7 @@ func (p *EBPFProbe) handleEventWrapper(CPU int, data []byte) {
 	ev := p.handleEvent(CPU, data)
 	end := time.Since(start)
 	p.eventProcessingTimeMutex.Lock()
-	acc, _ := (*p.eventProcessingTimes)[ev]
+	acc := (*p.eventProcessingTimes)[ev]
 	if acc == nil {
 		acc = &StatsAccumulator{}
 		(*p.eventProcessingTimes)[ev] = acc
