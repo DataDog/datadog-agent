9deaae8df2515c36e5023faed3e2d7be1409dfe0
Kevin Fairise <132568982+KevinFairise2@users.noreply.github.com>
2025-10-16 10:18:46 +0200

    Revert "[CWS] apply to proper delay in selftest (#41988)"
    This reverts commit b120a01048c82ad418ae94eefc553597f043c4e1.

--- pkg/security/module/cws.go
+++ pkg/security/module/cws.go
@@ -212,14 +212,13 @@ func (c *CWSConsumer) Start() error {
 			c.selfTestPassed = true
 
 			c.reportSelfTest(success, fails)
-			delay = selftestPassedDelay
 		}
-
-		time.Sleep(delay)
 
 		if _, err := c.RunSelfTest(false); err != nil {
 			seclog.Errorf("self-test error: %s", err)
 		}
+
+		time.Sleep(delay)
 	}
 	if c.selfTester != nil {
 		go c.selfTester.WaitForResult(cb)
