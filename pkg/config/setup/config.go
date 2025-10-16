e940f6eb47cc0d5337cda658c311eaef314936d9
Guillermo Julián <gjulianm@users.noreply.github.com>
2025-10-16 19:00:22 +0200

    [EBPF-739] gpu: reduce timeouts for querying system-probe (#39714)
    <!--
    * Contributors are encouraged to read our [CONTRIBUTING](/CONTRIBUTING.md) documentation.
    * Both Contributor and Reviewer Checklists are available at https://datadoghq.dev/datadog-agent/guidelines/contributing/#pull-requests.
    * The pull request:
      * Should only fix one issue or add one feature at a time.
      * Must update the test suite for the relevant functionality.
      * Should pass all status checks before being reviewed or merged.
    * Commit titles should be prefixed with general area of pull request's change.
    * Please fill the below sections if possible with relevant information or links.
    -->
    
    ### What does this PR do?
    
    This PR updates the timeouts for the CheckClient used in the gpu check, so that we avoid issues with weighted metrics being reported at different intervals. Now the agent should emit metrics at the check interval consistently even if system-probe is not responding ocrrectly.
    
    ### Motivation
    
    Long system-probe response times (usually at startup) can cause the check to be delayed and weighted metrics suffer from precision issues (https://datadoghq.atlassian.net/browse/EBPF-794).
    
    ### Describe how you validated your changes
    
    <!--
    Validate your changes before merge, ensuring that:
    * Your PR is tested by static / unit / integrations / e2e tests
    * Your PR description details which e2e tests cover your changes, if any
    * The PR description contains details of how you validated your changes. If you validated changes manually and not through automated tests, add context on why automated tests did not fit your changes validation.
    
    If you want additional validation by a second person, you can ask reviewers to do it. Describe how to set up an environment for manual tests in the PR description. Manual validation is expected to happen on every commit before merge.
    
    Any manual validation step should then map to an automated test. Manual validation should not substitute automation, minus exceptions not supported by test tooling yet.
    -->
    
    CI tests passing.
    
    ### Possible Drawbacks / Trade-offs
    
    ### Additional Notes
    
    <!--
    * Anything else we should know when reviewing?
    * Include benchmarking information here whenever possible.
    * Include info about alternatives that were considered and why the proposed
      version was chosen.
    -->

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -664,6 +664,7 @@ func InitConfig(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("gpu.enabled", false)
 	config.BindEnvAndSetDefault("gpu.nvml_lib_path", "")
 	config.BindEnvAndSetDefault("gpu.use_sp_process_metrics", false)
+	config.BindEnvAndSetDefault("gpu.sp_process_metrics_request_timeout", 3*time.Second)
 
 	// Cloud Foundry BBS
 	config.BindEnvAndSetDefault("cloud_foundry_bbs.url", "https://bbs.service.cf.internal:8889")
