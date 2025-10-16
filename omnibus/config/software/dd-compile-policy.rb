2e44465287a2d72cca5d510f62f38536069eb9e2
Baptiste Foy <baptiste.foy@datadoghq.com>
2025-10-16 15:33:33 +0200

    address agent build review

--- omnibus/config/software/dd-compile-policy.rb
+++ omnibus/config/software/dd-compile-policy.rb
@@ -18,31 +18,18 @@ require './lib/ostools.rb'
 name "dd-compile-policy"
 default_version "0.1.0"
 
-# Define URLs for each platform
+# Define URLs for each architecture. This is used for the Linux build only; Windows and Darwin are not supported yet.
 dd_compile_policy_urls = {
-  "linux" => {
     "amd64" => "https://github.com/DataDog/dd-policy-engine/releases/download/v#{version}/dd-compile-policy-linux-amd64.tar.gz",
     "arm64" => "https://github.com/DataDog/dd-policy-engine/releases/download/v#{version}/dd-compile-policy-linux-arm64.tar.gz",
-  },
-  # TODO: add Windows, maybe Darwin
 }
 
 # Define SHA256 checksums for verification
 # Note: These should be updated for each version
 dd_compile_policy_sha256 = {
-  "linux" => {
     "amd64" => "ce7ada4e91d3b57849cc602fbb541f6b48988735a6d95ac4044dfab28272dd56",
     "arm64" => "fe3c4470ca33030c4aa002dfb283aef672a8eff37b1660f9bcff67917e2fc64f"
-  },
 }
-
-current_platform = if osx_target?
-  "darwin"
-elsif windows_target?
-  "windows"
-else
-  "linux"
-end
 
 current_arch = if arm_target?
   "arm64"
@@ -51,17 +38,15 @@ else
 end
 
 # Pick correct URL and sha256
-source url: dd_compile_policy_urls[current_platform][current_arch],
-       sha256: dd_compile_policy_sha256[current_platform][current_arch]
+source url: dd_compile_policy_urls[current_arch],
+       sha256: dd_compile_policy_sha256[current_arch]
 
 build do
   license "BSD-3-Clause"
   license_file "https://raw.githubusercontent.com/DataDog/dd-policy-engine/master/LICENSE"
 
-  if linux?
-    # Extract the tar.gz file
-    target = "#{install_dir}/embedded/bin/dd-compile-policy"
-    copy "#{project_dir}/dd-compile-policy", target
-    block { File.chmod(0555, target) } # World executable
-  end
+  # Extract the tar.gz file
+  target = "#{install_dir}/embedded/bin/dd-compile-policy"
+  copy "#{project_dir}/dd-compile-policy", target
+  block { File.chmod(0555, target) } # World executable
 end
