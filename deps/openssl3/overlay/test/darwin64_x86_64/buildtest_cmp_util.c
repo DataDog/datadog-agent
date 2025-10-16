315adf714fbf7d47c07dfd8572f621f64964c4df
Tony Aiuto <tony.aiuto@datadoghq.com>
2025-10-15 23:53:39 -0400

    Add initial directory structure to build openssl from generated configuration.
    ```
    bazel build @openssl3//:libcrypto
    ```
    
    This gets the basic structure but is not good enough to use yet.
    
    Highlights:
    - script to generate the http_archive stanza with an files block.
      The files arg can not be loaded from a .bzl file, so it is hard to make
      it look nice. That leads to the solution of making a per-library MODULE.bazel.
    - golden file test to make sure the http_archive stanza is up to date.
    - include this generated module file from MODULE.bazel
    
    Next steps:
    - Add x86 configurations and adjust the select cases.
    - Add winows

---
+++ deps/openssl3/overlay/test/darwin64_x86_64/buildtest_cmp_util.c
@@ -0,0 +1,16 @@
+/*
+ * Generated with test/generate_buildtest.pl, to check that such a simple
+ * program builds.
+ */
+#include <openssl/opensslconf.h>
+#ifndef OPENSSL_NO_STDIO
+# include <stdio.h>
+#endif
+#ifndef OPENSSL_NO_CMP_UTIL
+# include <openssl/cmp_util.h>
+#endif
+
+int main(void)
+{
+    return 0;
+}
