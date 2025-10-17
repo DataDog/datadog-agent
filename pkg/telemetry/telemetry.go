52aba5dc0c63fb9cd4b5a54f6f4bbf5e1a6fb891
Scott Opell <me@scottopell.com>
2025-10-17 17:10:56 -0400

    GetCompatComponent needs to return the full telemetry impl for network-tracer

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -3,16 +3,19 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
-//go:build !serverless
-
 package telemetry
 
 import (
-	telemetrydef "github.com/DataDog/datadog-agent/comp/core/telemetry/def"
 	telemetryimpl "github.com/DataDog/datadog-agent/comp/core/telemetry/impl"
 )
 
-// GetCompatComponent returns a component wrapping telemetry global variables
-func GetCompatComponent() telemetrydef.Component {
+// GetCompatComponent returns a component wrapping telemetry global variables.
+//
+// TODO: This is a compatibility layer for legacy code that hasn't migrated to FX.
+// The primary consumer is pkg/network/tracer, which requires the full Prometheus
+// interface (RegisterCollector/UnregisterCollector). Once pkg/network/tracer is
+// migrated to request its telemetry dependency via FX, this function can be removed
+// or simplified to return only the base telemetry.Component interface.
+func GetCompatComponent() telemetryimpl.Component {
 	return telemetryimpl.GetCompatComponent()
 }
