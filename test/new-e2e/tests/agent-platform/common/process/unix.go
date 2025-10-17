204e59cdcb2e324d9ce6928ccccdb0edcd9f6879
Toby Lawrence <toby.lawrence@datadoghq.com>
2025-10-16 20:07:06 -0400

    refine our change to findPIDUnix based on the length of the process name to match

--- test/new-e2e/tests/agent-platform/common/process/unix.go
+++ test/new-e2e/tests/agent-platform/common/process/unix.go
@@ -19,7 +19,15 @@ func isProcessRunningUnix(host *components.RemoteHost, processName string) (bool
 }
 
 func findPIDUnix(host *components.RemoteHost, processName string) ([]int, error) {
-	out, err := host.Execute(fmt.Sprintf("pgrep '%s'", processName))
+	// `pgrep` is limited to matching the first 15 characters of a process name _unless_ the `-f` flag is used, but the
+	// '-f' flag acts more like a substring match, and will catch processes with the given name anywhere in their
+	// command line.. so we only use '-f' if the process name to match is longer than 15 characters
+	matchCommand := fmt.Sprintf("pgrep '%s'", processName)
+	if len(processName) > 15 {
+		matchCommand = fmt.Sprintf("pgrep -f '%s'", processName)
+	}
+
+	out, err := host.Execute(matchCommand)
 	if err != nil {
 		return nil, err
 	}
