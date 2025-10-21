81fa5165dbd4eb0c9fb75d237a3e62cc16a4fd3a
Mackenzie <mackenzie.jomard@datadoghq.com>
2025-10-21 15:06:23 +0000

    Add HP Flare extension

---
+++ comp/host-profiler/hpflareextension/impl/config_test.go
@@ -0,0 +1,58 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+// Package hpflareextensionimpl defines the OpenTelemetry Extension implementation.
+package hpflareextensionimpl
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/confmap"
+)
+
+func getTestConfig() *Config {
+	return &Config{
+		HTTPConfig: &confighttp.ServerConfig{
+			Endpoint: "localhost:0",
+		},
+	}
+}
+
+func TestValidate(t *testing.T) {
+	cfg := getTestConfig()
+
+	err := cfg.Validate()
+	assert.NoError(t, err)
+
+	cfg.HTTPConfig.Endpoint = ""
+	err = cfg.Validate()
+	assert.ErrorIs(t, err, errHTTPEndpointRequired)
+
+	cfg.HTTPConfig = nil
+	err = cfg.Validate()
+	assert.ErrorIs(t, err, errHTTPEndpointRequired)
+}
+
+func TestUnmarshal(t *testing.T) {
+	cfg := getTestConfig()
+
+	endpoint := "localhost:1234"
+
+	m := map[string]any{
+		"endpoint": endpoint,
+	}
+
+	myConfMap := confmap.NewFromStringMap(m)
+
+	err := cfg.Unmarshal(myConfMap)
+	assert.NoError(t, err)
+
+	err = cfg.Validate()
+	assert.NoError(t, err)
+
+	assert.Equal(t, endpoint, cfg.HTTPConfig.Endpoint)
+}
