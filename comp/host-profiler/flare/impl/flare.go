1b376c05821579e3764a8db25b5772d3a110f460
Mackenzie <mackenzie.jomard@datadoghq.com>
2025-10-21 14:04:08 +0000

    Add HP Flare extension

---
+++ comp/host-profiler/flare/impl/flare.go
@@ -0,0 +1,83 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build otlp
+
+// Package flareimpl implements the flareimpl
+package flareimpl
+
+import (
+	"encoding/json"
+	"fmt"
+
+	flaretypes "github.com/DataDog/datadog-agent/comp/core/flare/types"
+	ipc "github.com/DataDog/datadog-agent/comp/core/ipc/def"
+	hpflareextension "github.com/DataDog/datadog-agent/comp/host-profiler/hpflareextension/impl"
+	"github.com/DataDog/datadog-agent/pkg/trace/log"
+)
+
+// Provides specifics the types returned by the constructor
+type Provides struct {
+	FlareProvider flaretypes.Provider
+}
+
+// Requires defines the dependencies for the flareimpl component
+type Requires struct {
+	Client ipc.HTTPClient
+}
+
+type flareImpl struct {
+	client ipc.HTTPClient
+}
+
+// NewComponent creates a new Component for this module and returns any errors on failure.
+func NewComponent(reqs Requires) (Provides, error) {
+	flare := flareImpl{
+		client: reqs.Client,
+	}
+	return Provides{
+		FlareProvider: flaretypes.NewProvider(flare.fillFlare),
+	}, nil
+}
+
+func (c *flareImpl) fillFlare(fb flaretypes.FlareBuilder) error {
+	responseBytes, err := c.requestOtelConfigInfo()
+	if err != nil {
+		msg := fmt.Sprintf("did not get host-profiler configuration: %v", err)
+		log.Error(msg)
+		fb.AddFile("host-profiler/host-profiler.log", []byte(msg))
+
+		return nil
+	}
+
+	var responseInfo hpflareextension.Response
+	if err := json.Unmarshal(responseBytes, &responseInfo); err != nil {
+		msg := fmt.Sprintf("could not read sources from host-profiler response: %s, error: %v", responseBytes, err)
+		log.Error(msg)
+		fb.AddFile("host-profiler/host-profiler.log", []byte(msg))
+		return nil
+	}
+
+	fb.AddFile("host-profiler/runtime.cfg", []byte(toJSON(responseInfo.Config)))
+	return nil
+}
+
+func toJSON(it interface{}) string {
+	data, err := json.Marshal(it)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
+}
+
+func (c *flareImpl) requestOtelConfigInfo() ([]byte, error) {
+	// todo(mackjmr): Make port configurable once we have agreement on hostprofiler config.
+	data, err := c.client.Get("https://localhost:7778")
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
---
+++ comp/host-profiler/hpflareextension/impl/extension.go
@@ -0,0 +1,106 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+// Package hpflareextensionimpl defines the OpenTelemetry Extension implementation.
+package hpflareextensionimpl
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	ipc "github.com/DataDog/datadog-agent/comp/core/ipc/def"
+	extensionDef "github.com/DataDog/datadog-agent/comp/host-profiler/hpflareextension/def"
+	"github.com/DataDog/datadog-agent/pkg/util/option"
+	"github.com/goccy/go-yaml"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componentstatus"
+	"go.opentelemetry.io/collector/confmap"
+	"go.uber.org/zap"
+)
+
+// Response is the response struct for API queries
+type Response struct {
+	Config string `json:"config"`
+}
+
+// ddExtension is a basic OpenTelemetry Collector extension.
+type ddExtension struct {
+	cfg       *Config
+	config    string
+	telemetry component.TelemetrySettings
+	server    *server
+}
+
+// NotifyConfig implements the ConfigWatcher interface, which allows this extension
+// to be notified of the Collector's effective configuration. See interface:
+// https://github.com/open-telemetry/opentelemetry-collector/blob/d0fde2f6b98f13cbbd8657f8188207ac7d230ed5/extension/extension.go#L46.
+
+// This method is called during the startup process by the Collector's Service right after
+// calling Start.
+func (ext *ddExtension) NotifyConfig(_ context.Context, conf *confmap.Conf) error {
+	if conf == nil {
+		msg := "received a nil config in ddExtension.NotifyConfig"
+		return errors.New(msg)
+	}
+	var err error
+	confMap := conf.ToStringMap()
+	enhancedBytes, err := yaml.Marshal(confMap)
+	if err != nil {
+		return err
+	}
+	ext.config = string(enhancedBytes)
+	return nil
+}
+
+// NewExtension creates a new instance of the extension.
+func NewExtension(cfg *Config, ipcComp option.Option[ipc.Component], telemetry component.TelemetrySettings) (extensionDef.Component, error) {
+	var err error
+	ext := &ddExtension{
+		cfg:       cfg,
+		telemetry: telemetry,
+	}
+	ext.server, err = newServer(cfg.HTTPConfig.Endpoint, ext, ipcComp)
+	if err != nil {
+		return nil, err
+	}
+	return ext, nil
+}
+
+func (ext *ddExtension) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	j, err := json.MarshalIndent(Response{
+		Config: ext.config,
+	}, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Unable to marshal output: %v\n", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(j))
+}
+
+// Start is called when the extension is started.
+func (ext *ddExtension) Start(_ context.Context, host component.Host) error {
+	ext.telemetry.Logger.Info("Starting DD Extension HTTP server", zap.String("url", ext.cfg.HTTPConfig.Endpoint))
+
+	go func() {
+		if err := ext.server.start(); err != nil && err != http.ErrServerClosed {
+			componentstatus.ReportStatus(host, componentstatus.NewFatalErrorEvent(err))
+			ext.telemetry.Logger.Info("DD Extension HTTP could not start", zap.String("err", err.Error()))
+		}
+	}()
+
+	return nil
+}
+
+// Shutdown is called when the extension is shut down.
+func (ext *ddExtension) Shutdown(ctx context.Context) error {
+	return ext.server.shutdown(ctx)
+}
