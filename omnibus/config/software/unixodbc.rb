11d129438bdac6ebc94151a2428d2b54975c975b
Joseph Gette <joseph.gette@datadoghq.com>
2025-10-23 09:20:30 +0200

    Do not ship binaries with unixodbc

--- omnibus/config/software/unixodbc.rb
+++ omnibus/config/software/unixodbc.rb
@@ -1,10 +1,11 @@
 name "unixodbc"
 
+default_version "2.3.9"
+
 build do
   command_on_repo_root "bazelisk run -- @unixodbc//:install --destdir='#{install_dir}/embedded'"
   command_on_repo_root "bazelisk run -- //bazel/rules:replace_prefix --prefix '#{install_dir}/embedded'" \
     " #{install_dir}/embedded/lib/libodbc.so" \
     " #{install_dir}/embedded/lib/libodbccr.so" \
-    " #{install_dir}/embedded/lib/libltdl.so" \
     " #{install_dir}/embedded/lib/libodbcinst.so"
 end
