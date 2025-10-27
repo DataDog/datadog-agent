61d4a88bc92ccece21136c9dcacf51ce114c9e6f
wynbennett <wyn.bennett@datadoghq.com>
2025-10-27 14:40:20 -0400

    DELA-237 - Fixed bad import

--- cmd/process-agent/subcommands/check/check.go
+++ cmd/process-agent/subcommands/check/check.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/DataDog/datadog-agent/cmd/process-agent/command"
 	"github.com/DataDog/datadog-agent/comp/core"
-	"github.com/DataDog/datadog-agent/comp/core/config"
 	delegatedauthfx "github.com/DataDog/datadog-agent/comp/core/delegatedauth/fx"
 	ipcfx "github.com/DataDog/datadog-agent/comp/core/ipc/fx"
 	secretsfx "github.com/DataDog/datadog-agent/comp/core/secrets/fx"
