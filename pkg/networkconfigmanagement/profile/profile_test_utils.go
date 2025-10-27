0f31a206b0a70e49357ac30db23a45cc9ab01951
zoe ✨ <9274242+zoedt@users.noreply.github.com>
2025-10-27 14:42:29 -0400

    [NCM] Support loading once + compiling for device profiles when unmarshaled (#42012)
    ### What does this PR do?
    Follow up to this PR's specific comment: https://github.com/DataDog/datadog-agent/pull/41500#discussion_r2429944002
    > Should this be done only once when the profile is loaded?
    
    This PR adds functionality that addresses the above and following issues upon that change:
    * Move the `Scrubber` onto the `Commands` level (most times, global scrubbing doesn't make sense for every command)
      * e.g. the redactions for the `show running` config command may not apply to the `show version` (maybe no sensitive data)
    * Add field for holding the rule's compiled regex - reduce the load of needing to compile the regex every time
    * When unmarshaling the profile, compile the regex from rules + initialize the `Scrubber` if redactions are available
    
    **Things to do still**
    * Deal with `ExtractedMetadata` and falling back to agent timestamp if one could not be extracted
    * Begin adding to `default_profiles` in `integrations-core` + new directory for the NCM integration
    
    ### Motivation
    
    ### Describe how you validated your changes
    Pull in the ndm-tools/cml-qa repo and follow instructions for setting up the CLI ([link](https://github.com/DataDog/ndm-tools/tree/main/cml-qa))
    ```
    python cml_template_generator.py \
      --deb-url <INSERT THE DEB FROM CI/CD BUILD> \
      --api-key <INSERT YOUR KEY> \
      --site "datad0g.com" \
      --title "<YOUR NAME/TITLE> NCM Agent QA"
    ```
    * Go the NDM hosted CML ([docs](https://datadoghq.atlassian.net/wiki/spaces/II/pages/5061640281/Getting+Started+with+Cisco+Modeling+Labs+CML))
    * Press Import
    * Pull in the YAML that gets created, start the lab
    * Have to manually add a directory for default profiles b/c not pulled in from `integrations-core` yet
    
    
    ```
    cisco@qa-agent:/etc/datadog-agent/conf.d/network_config_management.d/default_profiles$ sudo -u dd-agent -- datadog-agent check network_config_management
    {"namespace":"cml-test","integration":"","configs":[{"device_id":"cml-test:10.10.1.1","device_ip":"10.10.1.1","config_type":"running","timestamp":1761160467,"tags":["device_ip:10.10.1.1"],"content":"REDACTED BECAUSE LOng"},{"device_id":"cml-test:10.10.1.1","device_ip":"10.10.1.1","config_type":"startup","timestamp":0,"tags":["device_ip:10.10.1.1"],"content":"REDACTED BECAUSE LONG"}],"collect_timestamp":1761160527}
    
    
      Running Checks
      ==============
    
        network_config_management
        -------------------------
          Instance ID: network_config_management:bd436a37500d07da [OK]
          Configuration Source: file:/etc/datadog-agent/conf.d/network_config_management.d/conf.yaml[0]
          Total Runs: 1
          Metric Samples: Last Run: 0, Total: 0
          Events: Last Run: 0, Total: 0
          ndmconfig: Last Run: 1, Total: 1
          Service Checks: Last Run: 0, Total: 0
          Average Execution Time : 2.03s
          Last Execution Date : 2025-10-22 19:15:27.401 UTC (1761160527401)
          Last Successful Execution Date : 2025-10-22 19:15:27 UTC (1761160527000)
    
      Check Worker Utilization
      ========================
        No worker utilization data available
    
      Metadata
      ========
        config.provider: file
        config.source: /etc/datadog-agent/conf.d/network_config_management.d/conf.yaml[0]
        config.hash: network_config_management:bd436a37500d07da
    ```
    
    ### Additional Notes
    
    
    Co-authored-by: zoe.tiet <zoe.tiet@datadoghq.com>

---
+++ pkg/networkconfigmanagement/profile/profile_test_utils.go
@@ -0,0 +1,189 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+//go:build test && ncm
+
+package profile
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/DataDog/datadog-agent/pkg/util/scrubber"
+)
+
+var exampleConfig = `
+Building configuration...
+
+
+Current configuration : 3144 bytes
+!
+! Last configuration change at 20:53:27 UTC Thu Aug 14 2025
+!
+version 15.9
+service timestamps debug datetime msec
+service timestamps log datetime msec
+no service password-encryption
+!
+hostname qa-device
+!
+boot-start-marker
+boot-end-marker
+!
+ip domain name lab.local
+ip cef    
+no ipv6 cef
+!         
+multilink bundle-name authenticated
+!         
+!         
+!         
+!         
+username cisco privilege 15 secret 9 ooooooimasecretsdfsdjfdsnvZM5zmpPuLrKr9CZC3A1/jTwjHzA
+!         
+redundancy
+!
+`
+
+var expectedConfig = `
+Building configuration...
+
+
+Current configuration : 3144 bytes
+!
+! Last configuration change at 20:53:27 UTC Thu Aug 14 2025
+!
+version 15.9
+service timestamps debug datetime msec
+service timestamps log datetime msec
+no service password-encryption
+!
+hostname qa-device
+!
+boot-start-marker
+boot-end-marker
+!
+ip domain name lab.local
+ip cef    
+no ipv6 cef
+!         
+multilink bundle-name authenticated
+!         
+!         
+!         
+!         
+username cisco privilege 15 secret 9 <BIG OL SECRET>
+!         
+redundancy
+!`
+
+func newTestProfile() *NCMProfile {
+	return &NCMProfile{
+		BaseProfile: BaseProfile{
+			Name: "test",
+		},
+		Commands: map[CommandType]*Commands{
+			Running: {
+				CommandType: Running,
+				Values:      []string{"show running-config"},
+				ProcessingRules: ProcessingRules{
+					MetadataRules: []MetadataRule{
+						{
+							Type:   Timestamp,
+							Regex:  regexp.MustCompile(`! Last configuration change at (.*)`),
+							Format: "15:04:05 MST Mon Jan 2 2006",
+						},
+						{
+							Type:  ConfigSize,
+							Regex: regexp.MustCompile(`Current configuration : (?P<Size>\d+)`),
+						},
+					},
+					ValidationRules: []ValidationRule{
+						{
+							Pattern: regexp.MustCompile("Building configuration..."),
+						},
+					},
+					RedactionRules: []RedactionRule{
+						{Regex: regexp.MustCompile(`(username .+ (password|secret) \d) .+`), Replacement: "$1 <BIG OL SECRET>"},
+					},
+				},
+			},
+		},
+	}
+}
+
+var testProfile = &NCMProfile{
+	BaseProfile: BaseProfile{
+		Name: "test",
+	},
+	Commands: map[CommandType]*Commands{
+		Running: {
+			CommandType: Running,
+			Values:      []string{"show running-config"},
+			ProcessingRules: ProcessingRules{
+				MetadataRules: []MetadataRule{
+					{
+						Type:   Timestamp,
+						Regex:  regexp.MustCompile(`! Last configuration change at (.*)`),
+						Format: "15:04:05 MST Mon Jan 2 2006",
+					},
+					{
+						Type:  ConfigSize,
+						Regex: regexp.MustCompile(`Current configuration : (?P<Size>\d+)`),
+					},
+				},
+				ValidationRules: []ValidationRule{
+					{
+						Type:    "valid_output",
+						Pattern: regexp.MustCompile("Building configuration..."),
+					},
+				},
+				RedactionRules: []RedactionRule{
+					{Regex: regexp.MustCompile(`(username .+ (password|secret) \d) .+`), Replacement: "$1 <redacted secret>"},
+				},
+			},
+		},
+	},
+}
+
+var runningCommandsWithCompiledRegex = &Commands{
+	CommandType: Running,
+	Values:      []string{"show running-config"},
+	ProcessingRules: ProcessingRules{
+		MetadataRules: []MetadataRule{
+			{
+				Type:   Timestamp,
+				Regex:  regexp.MustCompile(`! Last configuration change at (.*)`),
+				Format: "15:04:05 MST Mon Jan 2 2006",
+			},
+			{
+				Type:  ConfigSize,
+				Regex: regexp.MustCompile(`Current configuration : (?P<Size>\d+)`),
+			},
+		},
+		ValidationRules: []ValidationRule{
+			{
+				Type:    "valid_output",
+				Pattern: regexp.MustCompile(`Building configuration...`),
+			},
+		},
+		RedactionRules: []RedactionRule{
+			{
+				Regex:       regexp.MustCompile(`(username .+ (password|secret) \d) .+`),
+				Replacement: `$1 <redacted secret>`,
+			},
+		},
+	},
+	Scrubber: getRunningScrubber(),
+}
+
+func getRunningScrubber() *scrubber.Scrubber {
+	sc := scrubber.New()
+	sc.AddReplacer(scrubber.SingleLine, scrubber.Replacer{
+		Regex: regexp.MustCompile(`(username .+ (password|secret) \d) .+`),
+		Repl:  []byte(fmt.Sprintf(`$1 %s`, "<redacted secret>")),
+	})
+	return sc
+}
