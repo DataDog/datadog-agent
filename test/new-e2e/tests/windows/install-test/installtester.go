c7e83bd00c06c3a854b6298754c66727c466c160
Jeremy Hanna <jeremy.hanna@datadoghq.com>
2025-10-27 14:07:03 -0400

    Assert that the python version is correct when not lazy loaded

--- test/new-e2e/tests/windows/install-test/installtester.go
+++ test/new-e2e/tests/windows/install-test/installtester.go
@@ -164,11 +164,16 @@ func (t *Tester) runTestsForKitchenCompat(tt *testing.T) {
 		common.CheckIntegrationInstall(tt, t.InstallTestClient)
 
 		tt.Run("default python version", func(tt *testing.T) {
+			// python is lazy loaded and not running as default
 			expected := common.ExpectedUnloadedPython
 			if t.ExpectPython2Installed() {
 				expected = common.ExpectedPythonVersion2
 			}
 			common.CheckAgentPython(tt, t.InstallTestClient, expected)
+
+			// this sets python_lazy_loading: false so we can check the version installed
+			common.SetAgentPythonMajorVersion(tt, t.InstallTestClient, "3")
+			common.CheckAgentPython(tt, t.InstallTestClient, common.ExpectedPythonVersion3)
 		})
 
 		if t.ExpectPython2Installed() {
