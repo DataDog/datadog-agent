263c01765ee3e48914de59765bbbfe6859ce1995
Alexandre Yang <alexandre.yang@datadoghq.com>
2025-10-28 10:04:13 +0100

    init dynamic path e2e test suite

---
+++ test/new-e2e/tests/netpath/network-path-integration/dynamic_path_nix_test.go
@@ -0,0 +1,52 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package netpath contains e2e tests for Network Path Integration feature
+package networkpathintegration
+
+import (
+	_ "embed"
+	"testing"
+	"time"
+
+	"github.com/DataDog/test-infra-definitions/components/datadog/agentparams"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/DataDog/datadog-agent/test/new-e2e/pkg/e2e"
+	awshost "github.com/DataDog/datadog-agent/test/new-e2e/pkg/provisioners/aws/host"
+)
+
+//go:embed fake-traceroute/dynamic_path_datadog.yaml
+var dynamicPathDatadogYaml []byte
+
+type linuxDynamicPathTestSuite struct {
+	baseNetworkPathIntegrationTestSuite
+}
+
+// TestDynamicPathSuiteLinux runs the Network Path Integration e2e suite for linux
+func TestLinuxDynamicPathSuite(t *testing.T) {
+	t.Parallel()
+	e2e.Run(t, &linuxDynamicPathTestSuite{}, e2e.WithProvisioner(awshost.Provisioner(
+		awshost.WithAgentOptions(
+			agentparams.WithAgentConfig(string(datadogYaml)),
+			agentparams.WithSystemProbeConfig(string(sysProbeConfig)),
+		)),
+	))
+
+}
+
+func (s *linuxDynamicPathTestSuite) TestLinuxDynamicPathMetrics() {
+	fakeIntake := s.Env().FakeIntake
+	hostname := s.Env().Agent.Client.Hostname()
+	s.EventuallyWithT(func(c *assert.CollectT) {
+		assertMetrics(fakeIntake, c, [][]string{
+			testAgentRunningMetricTagsTCP,
+			testAgentRunningMetricTagsUDP,
+		})
+
+		s.checkDatadogEUTCP(c, hostname)
+		s.checkGoogleDNSUDP(c, hostname)
+	}, 5*time.Minute, 3*time.Second)
+}
