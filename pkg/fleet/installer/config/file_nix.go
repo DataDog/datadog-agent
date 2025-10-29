8921559c8702726df7613b7cdc17ce64e758ecc4
Paul Coignet <paul.coignet@datadoghq.com>
2025-10-29 12:46:37 +0100

    Fix config v3 symlink

--- pkg/fleet/installer/config/file_nix.go
+++ pkg/fleet/installer/config/file_nix.go
@@ -9,6 +9,8 @@ package config
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
 	"github.com/DataDog/datadog-agent/pkg/fleet/installer/telemetry"
 )
@@ -17,5 +19,31 @@ import (
 // It preserves the directory structure and file permissions.
 func copyDirectory(ctx context.Context, sourcePath, targetPath string) error {
 	cmd := telemetry.CommandContext(ctx, "cp", "-a", sourcePath, targetPath)
+	// 1. Eval stable symlink in sourcePath
+	stableSymlinkPath, err := filepath.EvalSymlinks(
+		filepath.Join(sourcePath, "managed", "datadog-agent", "stable"),
+	)
+	if err != nil {
+		return err
+	}
+	// 2. Get the version from the stable symlink
+	stablePath := filepath.Base(stableSymlinkPath)
+	// 3. Delete stable and experiment symlinks in targetPath
+	err = os.Remove(filepath.Join(targetPath, "managed", "datadog-agent", "stable"))
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	err = os.Remove(filepath.Join(targetPath, "managed", "datadog-agent", "experiment"))
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	// 4. Rename targetPath/managed/datadog-agent/<version> to targetPath/managed/datadog-agent/stable
+	err = os.Rename(
+		filepath.Join(targetPath, "managed", "datadog-agent", stablePath),
+		filepath.Join(targetPath, "managed", "datadog-agent", "stable"),
+	)
+	if err != nil {
+		return err
+	}
 	return cmd.Run()
 }
