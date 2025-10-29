d4152b077e91ea11b13b7dd360b0219630d24809
andrewqian2001datadog <andrew.qian@datadoghq.com>
2025-10-29 16:36:54 -0400

    add COAT for otlp_ingest_logs

---
+++ comp/otelcol/otlp/components/exporter/logsagentexporter/telemetry.go
@@ -0,0 +1,28 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build oracle
+
+package logsagentexporter
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/telemetry"
+)
+
+// These metrics are for measuring the current volume of traffic customers are sending through OTLP collectors (Agent, DDOT)
+var (
+	OTLPIngestLogsEvents = telemetry.NewCounter(
+		"runtime",
+		"datadog_agent_otlp_ingest_logs_events",
+		nil,
+		"Counter metric of OTLP Log events in OTLP ingestion",
+	)
+	OTLPIngestLogsRequests = telemetry.NewCounter(
+		"runtime",
+		"datadog_agent_otlp_ingest_logs_requests",
+		nil,
+		"Counter metric of OTLP Log requests in OTLP ingestion",
+	)
+)
