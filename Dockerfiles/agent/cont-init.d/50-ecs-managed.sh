4d92d47ba6c7050c8d8bb3f9e800e9923c511f6a
stzou <steve.zou@datadoghq.com>
2025-10-30 09:24:41 -0400

    address issues in ecs-managed init script

--- Dockerfiles/agent/cont-init.d/50-ecs-managed.sh
+++ Dockerfiles/agent/cont-init.d/50-ecs-managed.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Quick exit unless ECS managed instances are explicitly enabled
 if [[ "${ECS_MANAGED_INSTANCES:-}" != "true" ]]; then
@@ -13,9 +13,10 @@ fi
 # Set a default config for ECS Managed Instances if found
 # Don't override /etc/datadog-agent/datadog.yaml if it exists
 if [[ ! -e /etc/datadog-agent/datadog.yaml ]]; then
-    ln -s  /etc/datadog-agent/datadog-ecs.yaml \
-           /etc/datadog-agent/datadog.yaml
+    ln -s datadog-ecs.yaml /etc/datadog-agent/datadog.yaml
 fi
 
 # Remove all default checks
-find /etc/datadog-agent/conf.d/ -iname "*.yaml.default" | xargs grep -L 'ad_identifiers' | xargs rm -f
+find /etc/datadog-agent/conf.d -iname '*.yaml.default' -print0 | \
+    xargs -0 -r grep -LZ ad_identifiers | \
+    xargs -0 -r rm -f
