320b944f0aa4cca4ea93155571b43fb44f33d4a2
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2025-10-30 15:09:06 +0100

    linter not in sync

--- tasks/fuzz_infra.py
+++ tasks/fuzz_infra.py
@@ -29,11 +29,9 @@ def get_slack_channel_for_directory(directory_path: str) -> str:
     """
     try:
         # Assert that the path is either relative or had the expected prefix
-        assert not directory_path.startswith('/') or directory_path.startswith(
-            "/go/src/github.com/DataDog/datadog-agent/"
-        ), (
-            f"Expected relative path or path starting with '/go/src/github.com/DataDog/datadog-agent/', got: {directory_path}"
-        )
+        assert (
+            not directory_path.startswith('/') or directory_path.startswith("/go/src/github.com/DataDog/datadog-agent/")
+        ), f"Expected relative path or path starting with '/go/src/github.com/DataDog/datadog-agent/', got: {directory_path}"
 
         # Remove the leading datadog-agent prefix if it exists
         rel_path = directory_path.removeprefix("/go/src/github.com/DataDog/datadog-agent/")
