1e715686122f5e9255325da429156978921db672
stzou <steve.zou@datadoghq.com>
2025-11-03 12:36:32 -0500

    set host to sidecar_host for managed instances with sidecar

--- pkg/util/fargate/hostname_process.go
+++ pkg/util/fargate/hostname_process.go
@@ -70,5 +70,5 @@ func getECSManagedInstancesHost(ctx context.Context) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("fargate_task:%s", taskMeta.TaskARN), nil
+	return fmt.Sprintf("sidecar_host:%s", taskMeta.TaskARN), nil
 }
