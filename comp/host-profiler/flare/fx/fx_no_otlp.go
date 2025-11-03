dd3e0cf12b279b4f330871633c8bdee7a1ec6ce3
Mackenzie <mackenzie.jomard@datadoghq.com>
2025-11-03 15:02:05 +0000

    build tag otlp

--- comp/host-profiler/flare/fx/fx_no_otlp.go
+++ comp/host-profiler/flare/fx/fx_no_otlp.go
@@ -3,21 +3,16 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build otlp
+//go:build !otlp
 
 // Package fx creates the modules for fx
 package fx
 
 import (
-	flareimpl "github.com/DataDog/datadog-agent/comp/host-profiler/flare/impl"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
 // Module for Flare in core agent
 func Module() fxutil.Module {
-	return fxutil.Component(
-		fxutil.ProvideComponentConstructor(
-			flareimpl.NewComponent,
-		),
-	)
+	return fxutil.Component()
 }
