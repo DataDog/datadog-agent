e4b5aad9c9ab079d57a067e77a30300595cbad0f
Toby Lawrence <toby.lawrence@datadoghq.com>
2025-11-05 12:03:11 -0500

    Gate all usages of gomock behind the test build tag.

--- comp/ndmtmp/forwarder/component_mock.go
+++ comp/ndmtmp/forwarder/component_mock.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
+//go:build test
+
 // Package forwarder exposes the event platform forwarder for netflow.
 package forwarder
 
