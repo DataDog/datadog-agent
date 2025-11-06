3a2095c24bc8e930343ae92ef239ceb9b6492c37
Sylvain Baubeau <sylvain.baubeau@datadoghq.com>
2025-11-06 15:58:18 +0100

    Fix sock port parsing for system-probe

--- pkg/util/system/socket/no_vsock.go
+++ pkg/util/system/socket/no_vsock.go
@@ -3,25 +3,15 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build !clusterchecks
+//go:build clusterchecks
 
 package socket
 
 import (
-	"fmt"
-
-	"github.com/mdlayher/vsock"
+	"errors"
 )
 
 // ParseVSockAddress parses a vsock address and returns the CID
-func ParseVSockAddress(addr string) (uint32, error) {
-	switch addr {
-	case "host":
-		return vsock.Host, nil
-	case "hypervisor":
-		return vsock.Hypervisor, nil
-	case "local":
-		return vsock.Local, nil
-	}
-	return 0, fmt.Errorf("invalid vsock address '%s'", addr)
+func ParseVSockAddress(_ string) (uint32, error) {
+	return 0, errors.New("unsupported")
 }
