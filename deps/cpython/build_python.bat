8121681156e1f664a76c932b83d4031e44129ddc
Alex Lopez <alex.lopez@datadoghq.com>
2025-11-06 15:34:27 +0100

    Insert dependencies to Windows Python build via Bazel

--- deps/cpython/build_python.bat
+++ deps/cpython/build_python.bat
@@ -1,5 +1,12 @@
+:: %1: a file from sourcedir
+:: %2: a file from destdir
+
 for %%F in (%1) do set sourcedir=%%~dpF
 for %%F in (%2) do set destdir=%%~dpF
+
+:: Make path for externals absolute
+:: Once in the MSBuild invocation we can't rely on relative paths
+set EXTERNALS_DIR=%cd%\%EXTERNALS_DIR%
 
 call %sourcedir%\PCbuild\build.bat -e
 
