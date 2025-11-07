e0ac26beae2ba3301566261ecdf103ca912d7d5b
Hongshi Guo <hongshi.guo@datadoghq.com>
2025-11-06 16:28:40 -0800

    attempt to trigger permission popup

--- comp/core/gui/guiimpl/systray/Sources/main.swift
+++ comp/core/gui/guiimpl/systray/Sources/main.swift
@@ -3,9 +3,9 @@ import Cocoa
 // Creates shared application, accessible through the NSApp variable.
 let app = NSApplication.shared
 
-// Hide from Dock while still allowing permission prompts
-// This must be called early, before any UI initialization
-NSApp.setActivationPolicy(.accessory)
+// NOTE: Do NOT call setActivationPolicy(.accessory) here!
+// .accessory prevents permission prompts from appearing
+// With LSUIElement=false, the app will be a menu bar app but can show prompts
 
 let agentGUI = AgentGUI()
 agentGUI.run()
