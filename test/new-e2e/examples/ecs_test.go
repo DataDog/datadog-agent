e37ea402bd6573327e37f37f7d713a4cb6682690
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-11-07 12:40:07 +0100

    Move ECS

--- test/new-e2e/examples/ecs_test.go
+++ test/new-e2e/examples/ecs_test.go
@@ -12,11 +12,11 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	scenecs "github.com/DataDog/datadog-agent/test/e2e-framework/scenarios/aws/ecs"
+	tifEcs "github.com/DataDog/datadog-agent/test/e2e-framework/scenarios/aws/ecs"
 	"github.com/DataDog/datadog-agent/test/e2e-framework/testing/e2e"
 	"github.com/DataDog/datadog-agent/test/e2e-framework/testing/environments"
 	"github.com/DataDog/datadog-agent/test/e2e-framework/testing/provisioners/aws/ecs"
-
-	tifEcs "github.com/DataDog/datadog-agent/test/e2e-framework/scenarios/aws/ecs"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsecs "github.com/aws/aws-sdk-go-v2/service/ecs"
 )
@@ -26,7 +26,7 @@ type myECSSuite struct {
 }
 
 func TestMyECSSuite(t *testing.T) {
-	e2e.Run(t, &myECSSuite{}, e2e.WithProvisioner(ecs.Provisioner(ecs.WithECSOptions(tifEcs.WithLinuxNodeGroup()))))
+	e2e.Run(t, &myECSSuite{}, e2e.WithProvisioner(ecs.Provisioner(ecs.WithRunOptions(scenecs.WithECSOptions(tifEcs.WithLinuxNodeGroup())))))
 }
 
 func (v *myECSSuite) TestECS() {
