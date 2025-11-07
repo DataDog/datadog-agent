338a62aee76ad9c7943753bd7e57b6210ba9daed
Hongshi Guo <hongshi.guo@datadoghq.com>
2025-11-06 21:13:20 -0800

    attempt to bring up permission settings

--- comp/core/gui/guiimpl/systray/Sources/main.swift
+++ comp/core/gui/guiimpl/systray/Sources/main.swift
@@ -3,9 +3,9 @@ import Cocoa
 // Creates shared application, accessible through the NSApp variable.
 let app = NSApplication.shared
 
-// NOTE: Do NOT call setActivationPolicy(.accessory) here!
-// .accessory prevents permission prompts from appearing
-// With LSUIElement=false, the app will be a menu bar app but can show prompts
+// Hide from Dock while still allowing permission prompts
+// This must be called early, before any UI initialization
+NSApp.setActivationPolicy(.accessory)
 
 let agentGUI = AgentGUI()
 agentGUI.run()
