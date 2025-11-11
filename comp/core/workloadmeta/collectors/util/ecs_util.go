231719997989e303261fe5ddffd0131baa7acfc0
Josh Lineaweaver <josh.lineaweaver@datadoghq.com>
2025-11-11 13:14:04 -0700

    Revert "[ECS] Skip runtime override for Managed Instances"
    This reverts commit c0aee2ae9807acd5496abdb84accd86a4096365f.

--- comp/core/workloadmeta/collectors/util/ecs_util.go
+++ comp/core/workloadmeta/collectors/util/ecs_util.go
@@ -239,6 +239,7 @@ func ParseV4TaskContainers(
 		}
 		if strings.ToUpper(task.LaunchType) == "MANAGED_INSTANCES" && fargate.IsFargateInstance() {
 			source = workloadmeta.SourceRuntime
+			containerEvent.Runtime = workloadmeta.ContainerRuntimeECSManagedInstances
 		}
 
 		events = append(events, workloadmeta.CollectorEvent{
