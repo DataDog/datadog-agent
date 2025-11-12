48998c97ef8689ab42ec54d179a6a0f355aae96f
s-alad <58093617+s-alad@users.noreply.github.com>
2025-11-11 22:29:21 -0500

    Fetching instance-type from GCE and adding it to the inventoryhost payload (#42764)
    ### What does this PR do?
    This branch is forked off https://github.com/DataDog/datadog-agent/pull/42701 and adds GCE support for instance-type collection. 
    
    ### Motivation
    
    ### Describe how you validated your changes
    
    ### Additional Notes
    
    
    Co-authored-by: saad.naji <saad.naji@datadoghq.com>

--- pkg/util/cloudproviders/cloudproviders.go
+++ pkg/util/cloudproviders/cloudproviders.go
@@ -227,6 +227,7 @@ type cloudProviderInstanceTypeDetector func(context.Context) (string, error)
 
 var hostInstanceTypeDetectors = map[string]cloudProviderInstanceTypeDetector{
 	ec2.CloudProviderName:    ec2.GetInstanceType,
+	gce.CloudProviderName:    gce.GetInstanceType,
 	oracle.CloudProviderName: oracle.GetInstanceType,
 }
 
