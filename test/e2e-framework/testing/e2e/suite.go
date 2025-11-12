52103a8001ae85643101b8a127bb6ffc8a20583e
Ryan Hall <ryan.hall@datadoghq.com>
2025-11-12 17:45:22 -0500

    Re-enable the "keep-stacks" invoke flag

--- test/e2e-framework/testing/e2e/suite.go
+++ test/e2e-framework/testing/e2e/suite.go
@@ -145,6 +145,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -179,6 +180,10 @@ const (
 	createTimeout          = 60 * time.Minute
 	deleteTimeout          = 30 * time.Minute
 	provisionerGracePeriod = 2 * time.Second
+)
+
+var (
+	keepStacks = flag.Bool("keep-stacks", false, "Do not destroy the Pulumi stacks at the end of the tests")
 )
 
 // Suite is a generic inteface used internally, only implemented by BaseSuite
@@ -849,7 +854,7 @@ func Run[Env any, T Suite[Env]](t *testing.T, s T, options ...SuiteOption) {
 	devMode, err := runner.GetProfile().ParamStore().GetBoolWithDefault(parameters.DevMode, false)
 	if err != nil {
 		utils.Logf(t, "Unable to get DevMode value, DevMode will be disabled, error: %v", err)
-	} else if devMode {
+	} else if devMode || *keepStacks {
 		options = append(options, WithDevMode())
 	}
 
