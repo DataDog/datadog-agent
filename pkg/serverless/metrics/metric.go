2069719ce89dd4d755d0ec5c4179bd302b60995c
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-11-12 16:53:52 -0500

    fix lint

--- pkg/serverless/metrics/metric.go
+++ pkg/serverless/metrics/metric.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package metrics provides the serverless metric agent for collecting and forwarding metrics.
 package metrics
 
 import (
--- pkg/serverless/trace/span_modifier.go
+++ pkg/serverless/trace/span_modifier.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package trace provides trace collection and processing for serverless environments.
 package trace
 
 import (
