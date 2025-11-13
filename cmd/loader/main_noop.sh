726aeeee56e81d1386b0370c60c6bd7ca8962d88
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2025-11-13 17:25:15 +0100

    chore: remove loader process from heroku package

---
+++ cmd/loader/main_noop.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# noop loader for heroku
+# shouldn't be needed in practice, but it is used by the systemd service,
+# which is used when installing the deb manually
+
+shift # remove the path to the trace-agent binary
+shift # remove the path to the configuration file
+exec "$@"
