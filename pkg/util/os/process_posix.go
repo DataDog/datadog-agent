7b9a422c52ba583a3e9cc0902f213187dfa6ba0f
Bryce Kahle <bryce.kahle@datadoghq.com>
2025-11-13 13:01:11 -0800

    add peds-based docker proxy filter for direct send

--- pkg/util/os/process_posix.go
+++ pkg/util/os/process_posix.go
@@ -1,20 +1,24 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2016-present Datadog, Inc.
+// Copyright 2025-present Datadog, Inc.
 
 //go:build linux || freebsd || openbsd || darwin
 
-package procutil
+// Package os provides additional OS utilities
+package os
 
-import "syscall"
+import (
+	"errors"
+	"syscall"
+)
 
 // PidExists returns true if the pid is still alive
 func PidExists(pid int) bool {
 	// the kill syscall will check for the existence of a process
 	// if the signal is 0. See
 	// https://man7.org/linux/man-pages/man2/kill.2.html
-	if err := syscall.Kill(pid, 0); err == syscall.ESRCH {
+	if err := syscall.Kill(pid, 0); errors.Is(err, syscall.ESRCH) {
 		return false
 	}
 
