73ffc681ebe86bfb022d271c13c5067fefd58c68
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-11-14 09:36:21 +0100

    test

--- tasks/winbuildscripts/Invoke-Linters.ps1
+++ tasks/winbuildscripts/Invoke-Linters.ps1
@@ -1,7 +1,7 @@
-& pip install git+https://github.com/DataDog/datadog-agent-dev.git@kfairise/support-feature-flag-ci
+& pip install git+https://github.com/DataDog/datadog-agent-dev.git@main
 Write-Host "Invoking dda"
 $Env:DDA_VERBOSE = "1"
 & dda info owners code .gitlab-ci.yaml
 Write-Host "dda info owners code .gitlab-ci.yaml result is $LASTEXITCODE"
 
-& dda bzl
+& dda self feature test-kevin
