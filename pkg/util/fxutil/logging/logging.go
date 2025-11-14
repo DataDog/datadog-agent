edf17d2d500641d0e50e24c7acbf295241040495
Maxime Riaud <maxime.riaud@datadoghq.com>
2025-11-14 16:08:12 +0100

    rework the hook logic to start capturing events earlier

--- pkg/util/fxutil/logging/logging.go
+++ pkg/util/fxutil/logging/logging.go
@@ -8,6 +8,7 @@ package logging
 
 import (
 	"os"
+	"time"
 
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxevent"
@@ -16,13 +17,16 @@ import (
 // DefaultFxLoggingOption creates an fx.Option to configure the Fx logger, either to do nothing
 // (the default) or to log to the console (when TRACE_FX is set at `1`).
 func DefaultFxLoggingOption() fx.Option {
+	starttime := time.Now()
 	return fx.Options(
 		fx.WithLogger(
 			func() fxevent.Logger {
 				if os.Getenv("TRACE_FX") == "1" {
-					return &fxevent.ConsoleLogger{W: os.Stderr}
+					// We log to stderr to avoid polluting the log file with Fx events
+					return withFxTracer(&fxevent.ConsoleLogger{W: os.Stderr}, starttime, os.Stderr)
 				}
-				return fxevent.NopLogger
+				// We log to stderr to avoid polluting the log file with Fx events
+				return withFxTracer(fxevent.NopLogger, starttime, os.Stderr)
 			},
 		),
 	)
@@ -31,24 +35,36 @@ func DefaultFxLoggingOption() fx.Option {
 // Logger is a logger that logs fx events.
 type Logger interface {
 	Debug(v ...interface{})
-	Debugf(format string, params ...interface{})
-	Infof(format string, params ...interface{})
-	Errorf(format string, params ...interface{}) error
 }
 
 // EnableFxLoggingOnDebug enables the logs for FX events when log_level is debug.
 // This function requires that DefaultFxLoggingOption is part of the fx Options.
 // This function uses generics to avoid depending on the logger component.
 // When TRACE_FX is set to 0, it will disable the logging.
-func EnableFxLoggingOnDebug[T Logger](flavor string) fx.Option {
-	return fx.Decorate(func(logger T) fxevent.Logger {
-		var fxLogger fxevent.Logger
+func EnableFxLoggingOnDebug[T Logger]() fx.Option {
+	return fx.Decorate(func(originalFxLogger fxevent.Logger, logger T) fxevent.Logger {
 		if os.Getenv("TRACE_FX") == "0" {
-			fxLogger = fxevent.NopLogger
-		} else {
-			fxLogger = &fxevent.ConsoleLogger{W: fxEventLogger{logger: logger}}
+			return originalFxLogger
 		}
-		return withFxTracer(fxLogger, logger, flavor)
+
+		// In order to keep track of fx event in the tracer, we need to update the inner loggers
+		// instead of replacing it with a new logger
+		if instrumentedLogger, ok := originalFxLogger.(*fxTracingLogger); ok {
+			agentLogger := fxEventLogger{logger: logger}
+			instrumentedLogger.UpdateInnerLoggers(&fxevent.ConsoleLogger{W: agentLogger}, agentLogger)
+			return instrumentedLogger
+		}
+		return &fxevent.ConsoleLogger{W: fxEventLogger{logger: logger}}
+	})
+}
+
+// EnableFxInitInstrumentation enables the sending of spans to the trace agent when the Fx initialization is complete.
+// This will only happens if DD_FX_TRACING_ENABLED is set to true.
+func EnableFxInitInstrumentation() fx.Option {
+	return fx.Invoke(func(logger fxevent.Logger) {
+		if instrumentedLogger, ok := logger.(*fxTracingLogger); ok {
+			instrumentedLogger.EnableSpansSending()
+		}
 	})
 }
 
