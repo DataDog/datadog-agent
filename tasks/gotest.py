2703140786d817ebb462c15cd53ce560410cfebc
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-11-14 13:49:04 +0100

    [enable-orchestrion-unit-tests] Removed import

--- tasks/gotest.py
+++ tasks/gotest.py
@@ -342,7 +342,6 @@ def test(
     )
     # Setup orchestrion
     # export GOFLAGS="${GOFLAGS} '-toolexec=orchestrion toolexec'"
-    import os
     os.environ["GOFLAGS"] = os.environ.get("GOFLAGS", "") + " '-toolexec=orchestrion toolexec'"
     # gotest_flags += ' -toolexec=orchestrion toolexec'
     cmd = f'gotestsum {gotestsum_flags} -- {gobuild_flags} {govet_flags} {gotest_flags}'
