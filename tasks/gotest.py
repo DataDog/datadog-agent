19f01793da5b0cb39935ccc1c5bf49951bebe398
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-11-17 10:37:31 +0100

    [enable-orchestrion-unit-tests] Full orchestrion build

--- tasks/gotest.py
+++ tasks/gotest.py
@@ -329,7 +329,8 @@ def test(
 
     test_run_arg = f"-run {test_run_name}" if test_run_name else ""
 
-    stdlib_build_cmd = 'go build {verbose} -mod={go_mod} -tags "{go_build_tags}" -gcflags="{gcflags}" '
+    # TODO: Orchestrion only on option
+    stdlib_build_cmd = 'orchestrion go build {verbose} -mod={go_mod} -tags "{go_build_tags}" -gcflags="{gcflags}" '
     stdlib_build_cmd += '-ldflags="{ldflags}" {build_cpus} {race_opt} std cmd'
     rerun_coverage_fix = '--raw-command {cov_test_path}' if coverage else ""
     gotestsum_flags = (
