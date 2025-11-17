b3bed36bb6ea727c2795f37fd507f10ad218f4df
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-11-17 15:25:46 +0100

    stick to the same libxml2 version as main

--- deps/libxml2/config-linux-x86_64.h
+++ deps/libxml2/config-linux-x86_64.h
@@ -31,6 +31,12 @@
 /* Define if readline library is available */
 /* #undef HAVE_LIBREADLINE */
 
+/* Define to 1 if you have the <lzma.h> header file. */
+#define HAVE_LZMA_H 1
+
+/* Define to 1 if you have the <poll.h> header file. */
+/* #undef HAVE_POLL_H */
+
 /* Define to 1 if you have the <pthread.h> header file. */
 #define HAVE_PTHREAD_H 1
 
@@ -77,7 +83,7 @@
 #define PACKAGE_NAME "libxml2"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libxml2 2.15.1"
+#define PACKAGE_STRING "libxml2 2.14.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libxml2"
@@ -86,7 +92,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.15.1"
+#define PACKAGE_VERSION "2.14.5"
 
 /* Define to 1 if all of the C90 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
@@ -94,7 +100,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.15.1"
+#define VERSION "2.14.5"
 
 /* System configuration directory (/etc) */
 #define XML_SYSCONFDIR "/opt/datadog-agent/embedded/etc"
