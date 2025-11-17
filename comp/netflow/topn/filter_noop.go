74fe490249b3865dc9b0b0075d0d627352de34bc
Garrison Stauffer <garrison.stauffer@datadoghq.com>
2025-11-17 13:24:16 -0500

    add package comments

--- comp/netflow/topn/filter_noop.go
+++ comp/netflow/topn/filter_noop.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
+// Package topn defines business logic for filtering NetFlow records to the Top "N" occurrences.
 package topn
 
 import "github.com/DataDog/datadog-agent/comp/netflow/common"
@@ -12,6 +13,6 @@ type NoopFilter struct {
 }
 
 // Filter implements the FlowFlushFilter interface to return the full list of flows without any filtering.
-func (n NoopFilter) Filter(ctx common.FlushContext, flows []*common.Flow) []*common.Flow {
+func (n NoopFilter) Filter(_ common.FlushContext, flows []*common.Flow) []*common.Flow {
 	return flows
 }
--- comp/netflow/topn/filter_per_flush_throttler.go
+++ comp/netflow/topn/filter_per_flush_throttler.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
+// Package topn defines business logic for filtering NetFlow records to the Top "N" occurrences.
 package topn
 
 import "github.com/DataDog/datadog-agent/comp/netflow/common"
