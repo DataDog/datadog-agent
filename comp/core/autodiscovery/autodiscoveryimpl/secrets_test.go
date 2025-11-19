f73ee036fac558ce62ca3d265e9f75488d2e2b65
s-alad <saad.naji@datadoghq.com>
2025-11-19 15:42:22 -0500

    remove mock from secrets test

--- comp/core/autodiscovery/autodiscoveryimpl/secrets_test.go
+++ comp/core/autodiscovery/autodiscoveryimpl/secrets_test.go
@@ -55,8 +55,6 @@ func (m *MockSecretResolver) Refresh(_ bool) (string, error) {
 	return "", nil
 }
 
-func (m *MockSecretResolver) TriggerRefresh() {}
-
 func (m *MockSecretResolver) haveAllScenariosBeenCalled() bool {
 	for _, scenario := range m.scenarios {
 		if scenario.called == 0 {
