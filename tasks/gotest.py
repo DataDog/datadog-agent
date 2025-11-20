b995b61ebb5e6c8f75a8a9705196387ebc2b0c6b
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-11-20 10:33:15 +0100

    [enable-orchestrion-unit-tests] Disabled orchestrion

--- tasks/gotest.py
+++ tasks/gotest.py
@@ -371,17 +371,16 @@ def test(
         "orchestrion_flags": orchestrion_flags,
     }
 
-    # TODO A
-    # # Test
-    # if build_stdlib:
-    #     build_standard_lib(
-    #         ctx,
-    #         build_tags=unit_tests_tags,
-    #         cmd=stdlib_build_cmd,
-    #         env=env,
-    #         args=args,
-    #         test_profiler=test_profiler,
-    #     )
+    # Test
+    if build_stdlib:
+        build_standard_lib(
+            ctx,
+            build_tags=unit_tests_tags,
+            cmd=stdlib_build_cmd,
+            env=env,
+            args=args,
+            test_profiler=test_profiler,
+        )
 
     if only_modified_packages:
         modules = get_modified_packages(ctx, build_tags=unit_tests_tags)
