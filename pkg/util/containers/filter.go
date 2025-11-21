54dc28ec9cb1be5c95e2f4e519e462ec984c9207
Gabriel Dos Santos <gabe.dossantos@datadoghq.com>
2025-11-21 22:10:38 +0000

    update the image name

--- pkg/util/containers/filter.go
+++ pkg/util/containers/filter.go
@@ -27,7 +27,7 @@ const (
 
 	pauseContainerKubernetes = "image:kubernetes/pause"
 	pauseContainerECS        = "image:amazon/amazon-ecs-pause"
-	pauseContainerFargate    = "image:amazon/aws-fargate-pause"
+	pauseContainerFargate    = "image:aws-fargate-pause"
 	pauseContainerOpenshift  = "image:openshift/origin-pod"
 	pauseContainerOpenshift3 = "image:.*rhel7/pod-infrastructure"
 
