7e99b860c9ab19466caefd8d70918a464b1a1355
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-11-21 16:51:02 +0100

    omnibus: build ffi with bazel

--- omnibus/config/software/libffi.rb
+++ omnibus/config/software/libffi.rb
@@ -16,36 +16,9 @@
 
 name "libffi"
 
-default_version "3.4.8"
-
-license "MIT"
-license_file "LICENSE"
-skip_transitive_dependency_licensing true
-
-# Is libtool actually necessary? Doesn't configure generate one?
-dependency "libtool" unless windows?
-
-version("3.4.8") { source sha256: "bc9842a18898bfacb0ed1252c4febcc7e78fa139fd27fdc7a3e30d9d9356119b" }
-
-source url: "https://github.com/libffi/libffi/releases/download/v#{version}/libffi-#{version}.tar.gz"
-
-relative_path "libffi-#{version}"
-
 build do
-  env = with_standard_compiler_flags(with_embedded_path)
-
-  env["INSTALL"] = "/opt/freeware/bin/install" if aix?
-
-  configure_command = ["--disable-static", "--disable-docs", "--disable-multi-os-directory"]
-
-  configure(*configure_command, env: env)
-
-  if solaris_10?
-    # run old make :(
-    make env: env, bin: "/usr/ccs/bin/make"
-    make "install", env: env, bin: "/usr/ccs/bin/make"
-  else
-    make "-j #{workers}", env: env
-    make "-j #{workers} install", env: env
-  end
+  sh_lib = if linux_target? then "libffi.so" else "libffi.dylib" end
+  command_on_repo_root "bazelisk run -- @libffi//:install --destdir='#{install_dir}/embedded'"
+  command_on_repo_root "bazelisk run -- //bazel/rules:replace_prefix --prefix '#{install_dir}/embedded'" \
+    " #{install_dir}/embedded/lib/#{sh_lib}"
 end
