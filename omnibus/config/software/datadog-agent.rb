ffe10b9f5071b0070fd7b46905d4866c4950f23c
Tony Aiuto <tony.aiuto@datadoghq.com>
2025-11-21 10:04:51 -0500

    back to the old ways

--- omnibus/config/software/datadog-agent.rb
+++ omnibus/config/software/datadog-agent.rb
@@ -335,7 +335,6 @@ build do
   block do
     # TODO: pkg_install is still buggy on Windows. Fix it and use that.
     mkdir "#{install_dir}/sources"
-    command "bazelisk build -- //compliance:licenses"
-    copy "bazel-bin/compliance/sources.txt", "#{install_dir}/sources"
+    command_on_repo_root "bazelisk run -- //compliance:install_source_offers --destdir=#{install_dir}/sources"
   end
 end
