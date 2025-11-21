334e6c961d3e979ce84223669e68e22a5ae3d5e6
Nicholas Hulston <nicholas.hulston@datadoghq.com>
2025-11-21 12:42:09 -0500

    trace helper to create and submit synthetic spans

---
+++ cmd/serverless-init/trace/trace.go
@@ -0,0 +1,71 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package trace
+
+import (
+	"math/rand"
+
+	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/trace"
+	"github.com/DataDog/datadog-agent/pkg/trace/api"
+	"github.com/DataDog/datadog-agent/pkg/trace/info"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+// InitSpan creates a new span with the provided metadata
+func InitSpan(service, name, resource, spanType string, startTime int64, tags map[string]string) *pb.Span {
+	traceID := rand.Uint64()
+	spanID := rand.Uint64()
+
+	span := &pb.Span{
+		Service:  service,
+		Name:     name,
+		Resource: resource,
+		Type:     spanType,
+		TraceID:  traceID,
+		SpanID:   spanID,
+		ParentID: 0, // Top-level span
+		Start:    startTime,
+		Meta:     tags,
+	}
+
+	return span
+}
+
+// ServerlessTraceAgent is an interface for submitting traces
+// This avoids importing pkg/serverless/trace which has a cyclic dependency
+type ServerlessTraceAgent interface {
+	Process(*api.Payload)
+}
+
+// SubmitSpan submits a completed span to the trace agent
+func SubmitSpan(span *pb.Span, origin string, traceAgent interface{}) {
+	if span == nil {
+		return
+	}
+
+	log.Debugf("Submitting synethtic span: Service=%s, Name=%s, TraceID=%d, SpanID=%d, Duration=%dns",
+		span.Service, span.Name, span.TraceID, span.SpanID, span.Duration)
+
+	traceChunk := &pb.TraceChunk{
+		Origin:   origin,
+		Priority: int32(1),
+		Spans:    []*pb.Span{span},
+	}
+
+	tracerPayload := &pb.TracerPayload{
+		Chunks: []*pb.TraceChunk{traceChunk},
+	}
+
+	if ta, ok := traceAgent.(ServerlessTraceAgent); ok {
+		ta.Process(&api.Payload{
+			Source:        info.NewReceiverStats(true).GetTagStats(info.Tags{}),
+			TracerPayload: tracerPayload,
+		})
+		log.Debug("Synthetic span submitted successfully")
+	} else {
+		log.Warn("Unable to submit synthetic span")
+	}
+}
