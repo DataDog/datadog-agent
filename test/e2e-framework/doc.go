736a8407eb0361efa664f30c0e90f2347f7cceb0
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-11-24 18:15:11 +0100

    Make sure test/e2e-framework module is importable for mod tidy check

---
+++ test/e2e-framework/doc.go
@@ -0,0 +1,9 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+// Package e2eframework is the entry point for the e2e-framework.
+package e2eframework
+
+// Needed to make sure we have at least one file in the package to avoid error when importing the module
