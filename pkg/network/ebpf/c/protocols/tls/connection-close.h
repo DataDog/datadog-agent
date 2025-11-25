f5eb4da13f1fa69b6101c24accc0004b386d7a6a
Guy Arbitman <guy20495@gmail.com>
2025-11-25 10:17:26 +0200

    cherry pick

--- pkg/network/ebpf/c/protocols/tls/connection-close.h
+++ pkg/network/ebpf/c/protocols/tls/connection-close.h
@@ -4,11 +4,10 @@
 #include "conn_tuple.h"
 #include "protocols/classification/defs.h"
 
-// Connection close event with full protocol stack
 typedef struct {
-    conn_tuple_t tuple;          // Connection identification (IPs, ports, PID, netns)
-    __u64 timestamp_ns;          // When it closed
-    protocol_stack_t stack;      // Full protocol stack (API, Application, Encryption layers)
-} __attribute__((packed)) tcp_close_event_t;
+    conn_tuple_t tuple;
+    __u64 timestamp_ns;
+    protocol_stack_t stack;
+} tcp_close_event_t;
 
 #endif // __CONNECTION_CLOSE_H
