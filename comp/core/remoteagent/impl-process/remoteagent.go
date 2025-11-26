be83112e3df96864b309fa76c9ebeb11d2346669
Maxime Riaud <maxime.riaud@datadoghq.com>
2025-11-26 14:49:04 +0100

    fix lint

--- comp/core/remoteagent/impl-process/remoteagent.go
+++ comp/core/remoteagent/impl-process/remoteagent.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
-// Package traceimpl implements the remoteagent component interface
+// Package processimpl implements the remoteagent component interface
 package processimpl
 
 import (
