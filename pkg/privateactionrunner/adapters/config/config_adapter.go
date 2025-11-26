cdd999515c12437d95db5926dd92c418636f1421
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2025-11-26 16:45:40 +0100

    [ACTP-1046] add config and constants adapters

---
+++ pkg/privateactionrunner/adapters/config/config_adapter.go
@@ -0,0 +1,56 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+package config
+
+import (
+	"crypto/ecdsa"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/adapters/modes"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/observability"
+	"github.com/DataDog/datadog-agent/pkg/util/common"
+	"github.com/DataDog/datadog-go/v5/statsd"
+)
+
+type Config struct {
+	ActionsAllowlist  map[string]common.StringSet // map of allowed bundle IDs to a set of allowed action names
+	Allowlist         []string
+	AllowIMDSEndpoint bool
+	DDHost            string
+	Modes             []modes.Mode
+	OrgId             int64
+	PrivateKey        *ecdsa.PrivateKey
+	RunnerId          string
+	Urn               string
+	Tags              []observability.Tag
+
+	// RemoteConfig related fields
+	DatadogSite string
+
+	// the following are constants with default values. They are part of the config struct to allow for the ability to be overwritten in the YAML config file if needed
+	MaxBackoff                 time.Duration
+	MinBackoff                 time.Duration
+	MaxAttempts                int32
+	WaitBeforeRetry            time.Duration
+	LoopInterval               time.Duration
+	OpmsRequestTimeout         int32
+	RunnerPoolSize             int32
+	HealthCheckInterval        int32
+	HttpServerReadTimeout      int32
+	HttpServerWriteTimeout     int32
+	RunnerAccessTokenHeader    string
+	RunnerAccessTokenIdHeader  string
+	Port                       int32
+	JWTRefreshInterval         time.Duration
+	HealthCheckEndpoint        string
+	HeartbeatInterval          time.Duration
+	EnableProfiling            bool
+	DisableCredentialTemplates bool
+
+	Version string
+
+	MetricsClient statsd.ClientInterface
+}
---
+++ pkg/privateactionrunner/adapters/constants/constants_adapter.go
@@ -0,0 +1,10 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+package constants
+
+const JwtHeaderName = "X-Datadog-OnPrem-JWT"
+const ModeHeaderName = "X-Datadog-OnPrem-Modes"
+const VersionHeaderName = "X-Datadog-OnPrem-Version"
