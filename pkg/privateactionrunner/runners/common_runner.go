3475ae9d75c7d9910ece52bb6feaf50a78aa6fee
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2025-11-27 17:32:50 +0100

    [ACTP-1046] add runners package

---
+++ pkg/privateactionrunner/runners/common_runner.go
@@ -0,0 +1,56 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+package runners
+
+import (
+	"context"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/adapters/config"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/opms"
+	log "github.com/DataDog/datadog-agent/pkg/privateactionrunner/adapters/logging"
+)
+
+type CommonRunner struct {
+	opmsClient opms.Client
+	settings   *config.Config
+}
+
+func NewCommonRunner(
+	configuration *config.Config,
+) *CommonRunner {
+	return &CommonRunner{
+		opmsClient: opms.NewClient(configuration),
+		settings:   configuration,
+	}
+}
+
+func (n *CommonRunner) Start(ctx context.Context) {
+	go n.healthCheckLoop(ctx)
+}
+
+func (n *CommonRunner) healthCheckLoop(ctx context.Context) {
+	ticker := time.NewTicker(time.Millisecond * time.Duration(n.settings.HealthCheckInterval))
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			logger := log.FromContext(ctx)
+			healthResponse, err := n.opmsClient.HealthCheck(ctx)
+			if healthResponse != nil && healthResponse.ServerTime != nil {
+				logger = logger.With(log.String("server-time", healthResponse.ServerTime.UTC().Format(time.RFC3339)))
+			}
+			if err != nil {
+				logger.Error("health check failed", log.ErrorField(err))
+			} else {
+				logger.Info("health check succeeded")
+			}
+		}
+	}
+}
--- pkg/privateactionrunner/runners/workflow_runner.go
+++ pkg/privateactionrunner/runners/workflow_runner.go
@@ -3,26 +3,163 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
-// Package runners implements entrypoint for privateactionrunner execution
 package runners
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"time"
 
-// WorkflowRunner executes workflows and manages task execution.
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/adapters/config"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/credentials/resolver"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/observability"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/opms"
+	privatebundles "github.com/DataDog/datadog-agent/pkg/privateactionrunner/private-bundles"
+	remoteconfig "github.com/DataDog/datadog-agent/pkg/privateactionrunner/remote-config"
+	taskverifier "github.com/DataDog/datadog-agent/pkg/privateactionrunner/task-verifier"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/types"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/util"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/libs/privateconnection"
+	aperrorpb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/privateactionrunner/errorcode"
+	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/adapters/actions"
+	log "github.com/DataDog/datadog-agent/pkg/privateactionrunner/adapters/logging"
+)
+
 type WorkflowRunner struct {
+	registry     *privatebundles.Registry
+	opmsClient   opms.Client
+	resolver     resolver.PrivateCredentialResolver
+	config       *config.Config
+	keysManager  remoteconfig.KeysManager
+	taskVerifier *taskverifier.TaskVerifier
+	taskLoop     *Loop
 }
 
-// NewWorkflowRunner creates a new WorkflowRunner instance.
-func NewWorkflowRunner() *WorkflowRunner {
-	return &WorkflowRunner{}
+func NewWorkflowRunner(
+	ctx context.Context,
+	configuration *config.Config,
+) (*WorkflowRunner, error) {
+	keyManager, err := remoteconfig.New(ctx, configuration)
+	if err != nil {
+		return nil, err
+	}
+	log.FromContext(ctx).Info("Remote config client created")
+	taskVerifier := taskverifier.NewTaskVerifier(keyManager, configuration)
+
+	return &WorkflowRunner{
+		registry: privatebundles.NewRegistry(
+		),
+		opmsClient: opms.NewClient(configuration),
+		resolver: resolver.NewPrivateCredentialResolver(
+		),
+		config:       configuration,
+		keysManager:  keyManager,
+		taskVerifier: taskVerifier,
+	}, nil
 }
 
-// Start begins the workflow runner execution.
-func (n *WorkflowRunner) Start(_ context.Context) error {
-	return nil
+func (n *WorkflowRunner) Start(ctx context.Context) {
+	if n.taskLoop != nil {
+		log.FromContext(ctx).Warn("WorkflowRunner already started")
+		return
+	}
+	startTime := time.Now()
+	if n.keysManager != nil {
+		n.keysManager.Start(ctx)
+	}
+	n.taskLoop = NewLoop(n)
+	go func() {
+		if n.keysManager != nil {
+			log.FromContext(ctx).Info("Waiting for KeysManager to be ready")
+			n.keysManager.WaitForReady()
+			observability.ReportKeysManagerReady(n.config.MetricsClient, log.FromContext(ctx), startTime)
+		}
+		n.taskLoop.Run(ctx)
+	}()
 }
 
-// Close stops the workflow runner and cleans up resources.
-func (n *WorkflowRunner) Close(_ context.Context) error {
-	return nil
+func (n *WorkflowRunner) Close(ctx context.Context) {
+	if n.taskLoop != nil {
+		n.taskLoop.Close(ctx)
+	}
+	if n.keysManager != nil {
+		n.keysManager.Close(ctx)
+	}
+}
+
+func (n *WorkflowRunner) RunTask(
+	ctx context.Context,
+	task *types.Task,
+	credential *privateconnection.PrivateCredentials,
+) (interface{}, error) {
+	fqn := task.GetFQN()
+	bundleName, actionName := actions.SplitFQN(fqn)
+	bundle := n.registry.GetBundle(bundleName)
+	if bundle == nil {
+		return nil, util.NewPARError(
+			aperrorpb.ActionPlatformErrorCode_INTERNAL_ERROR,
+			fmt.Errorf("could not find bundle for %s", bundleName),
+		)
+	}
+	action := bundle.GetAction(actionName)
+	if action == nil {
+		return nil, util.NewPARError(
+			aperrorpb.ActionPlatformErrorCode_INTERNAL_ERROR,
+			fmt.Errorf("could not find action for %s", actionName),
+		)
+	}
+	if !n.config.IsActionAllowed(bundleName, actionName) {
+		return nil, util.DefaultActionError(fmt.Errorf("action %s is not in the allow list", fqn))
+	}
+	if actions.IsHttpBundle(bundleName) {
+		url, ok := task.Data.Attributes.Inputs["url"].(string)
+		if !ok {
+			return nil, util.DefaultActionError(fmt.Errorf("missing required field url"))
+		}
+		if !n.config.IsURLInAllowlist(url) {
+			return nil, util.DefaultActionError(fmt.Errorf("request url is not allowed by runner policy: check your configuration file"))
+		}
+	}
+
+	logger := log.FromContext(ctx)
+
+	heartbeatCtx, heartbeatCancel := context.WithCancel(ctx)
+	defer heartbeatCancel()
+	go n.startHeartbeat(heartbeatCtx, task, logger)
+
+	startTime := observability.ReportExecutionStart(n.config.MetricsClient, task.Data.Attributes.Client, fqn, task.Data.ID, logger)
+	output, err := action.Run(ctx, task, credential)
+	observability.ReportExecutionCompleted(n.config.MetricsClient, task.Data.Attributes.Client, fqn, task.Data.ID, startTime, err, logger)
+
+	if err != nil {
+		return nil, util.DefaultActionError(err)
+	}
+
+	return output, nil
+}
+
+func (n *WorkflowRunner) startHeartbeat(ctx context.Context, task *types.Task, logger log.Logger) {
+	ticker := time.NewTicker(n.config.HeartbeatInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Info("Heartbeat stopped for task", log.String("task_id", task.Data.ID))
+			return
+		case <-ticker.C:
+			err := n.opmsClient.Heartbeat(ctx, task.Data.Attributes.Client, task.Data.ID, task.GetFQN(), task.Data.Attributes.JobId)
+
+			logger := log.FromContext(ctx).With(
+				log.String(observability.TaskIDTagName, task.Data.ID),
+				log.String(observability.ActionFqnTagName, task.GetFQN()),
+				log.String(observability.JobIDTagName, task.Data.Attributes.JobId))
+
+			if err != nil {
+				logger.Error("Failed to send heartbeat", log.ErrorField(err))
+			} else {
+				logger.Info("Heartbeat sent successfully")
+			}
+		}
+	}
 }
