b0ea0704c9d97e5045e84c34c4221f553b9cfc8d
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2025-11-27 17:32:26 +0100

    [ACTP-1046] add actions / update config adapter

--- pkg/privateactionrunner/adapters/config/config_adapter.go
+++ pkg/privateactionrunner/adapters/config/config_adapter.go
@@ -7,16 +7,20 @@ package config
 
 import (
 	"crypto/ecdsa"
+	"fmt"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/adapters/modes"
 	"github.com/DataDog/datadog-agent/pkg/privateactionrunner/observability"
-	"github.com/DataDog/datadog-agent/pkg/util/common"
 	"github.com/DataDog/datadog-go/v5/statsd"
+	"github.com/gobwas/glob"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 type Config struct {
-	ActionsAllowlist  map[string]common.StringSet // map of allowed bundle IDs to a set of allowed action names
+	ActionsAllowlist  map[string]sets.Set[string] // map of allowed bundle IDs to a set of allowed action names
 	Allowlist         []string
 	AllowIMDSEndpoint bool
 	DDHost            string
@@ -53,4 +57,48 @@ type Config struct {
 	Version string
 
 	MetricsClient statsd.ClientInterface
+}
+
+func (c *Config) IsActionAllowed(bundleId, actionName string) bool {
+	bundleId = strings.ToLower(bundleId)
+	actionName = strings.ToLower(actionName)
+
+	if _, ok := c.ActionsAllowlist[bundleId]; ok {
+		return c.ActionsAllowlist[bundleId].HasAny(actionName, "*")
+	}
+	return false
+}
+
+func (c *Config) IsURLInAllowlist(urlStr string) bool {
+	if c.Allowlist == nil {
+		return true
+	}
+
+	// Parse the URL
+	parsedURL, err := url.Parse(urlStr)
+	if err != nil {
+		fmt.Printf("Invalid URL: %v\n", err)
+		return false
+	}
+	hostname := parsedURL.Hostname()
+
+	// Convert hostname to lowercase for case-insensitive comparison
+	hostname = strings.ToLower(hostname)
+
+	// Check for exact matches
+	for _, pattern := range c.Allowlist {
+		if strings.ToLower(pattern) == hostname {
+			return true
+		}
+	}
+	// Check for glob pattern matches
+	for _, pattern := range c.Allowlist {
+		globPattern := strings.ToLower(pattern)
+		matcher := glob.MustCompile(globPattern)
+		if matcher.Match(hostname) {
+			return true
+		}
+	}
+
+	return false
 }
