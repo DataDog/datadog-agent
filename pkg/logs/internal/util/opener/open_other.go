199796fbd170b1930aa0afce021439e0173851c6
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2025-12-02 11:56:43 +0100

    privileged logs: Fix handling of unsearchable directories
    In any directory in the path of the log file does not have executable
    permissions, it does not get tailed since a stat(2) call from the file
    launcher fails and the logs agent never gets to the point where it calls
    into system-probe.  Fix this by handling that stat(2) call too in
    system-probe if privileged logs is enabled.

--- pkg/logs/internal/util/opener/open_other.go
+++ pkg/logs/internal/util/opener/open_other.go
@@ -18,3 +18,8 @@ import (
 func OpenLogFile(path string) (*os.File, error) {
 	return filesystem.OpenShared(path)
 }
+
+// StatLogFile stats a log file
+func StatLogFile(path string) (FileInfo, error) {
+	return os.Stat(path)
+}
