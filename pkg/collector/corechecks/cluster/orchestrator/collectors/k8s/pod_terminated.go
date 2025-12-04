DataDog/datadog-agent

commit 37ca693912851a2efc8e7f3eb8d1efeda088b177

Expose Kind, ApiVersion, NodeName and HostName in Manifest collection (#36903)

pkg/collector/corechecks/cluster/orchestrator/collectors/k8s/pod_terminated.go
@@ -108,3 +108,9 @@ func (c *TerminatedPodCollector) Process(rcfg *collectors.CollectorRunConfig, li
 
 	return result, nil
 }
+
+// GetNodeName is used to get the node name from the resource.
+func (c *TerminatedPodCollector) GetNodeName(_ processors.ProcessorContext, resource interface{}) string {
+	r := resource.(*v1.Pod)
+	return r.Spec.NodeName
+}

comp/otelcol/ddflareextension/impl/go.mod
@@ -181,7 +181,7 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v4 v4.3.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.3.3 // indirect
 	github.com/Code-Hex/go-generics-cache v1.5.1 // indirect
-	github.com/DataDog/agent-payload/v5 v5.0.150 // indirect
+	github.com/DataDog/agent-payload/v5 v5.0.152 // indirect
 	github.com/DataDog/datadog-agent/comp/core/config v0.64.1 // indirect
 	github.com/DataDog/datadog-agent/comp/core/flare/builder v0.64.1 // indirect
 	github.com/DataDog/datadog-agent/comp/core/flare/types v0.64.1 // indirect

comp/otelcol/ddflareextension/impl/go.sum
@@ -67,8 +67,8 @@ github.com/BurntSushi/toml v0.3.1/go.mod h1:xHWCNGjB5oqiDr8zfno3MHue2Ht5sIBksp03
 github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802/go.mod h1:IVnqGOEym/WlBOVXweHU+Q+/VP0lqqI8lqeDx9IjBqo=
 github.com/Code-Hex/go-generics-cache v1.5.1 h1:6vhZGc5M7Y/YD8cIUcY8kcuQLB4cHR7U+0KMqAA0KcU=
 github.com/Code-Hex/go-generics-cache v1.5.1/go.mod h1:qxcC9kRVrct9rHeiYpFWSoW1vxyillCVzX13KZG8dl4=
-github.com/DataDog/agent-payload/v5 v5.0.150 h1:uW9HyA/O5UpuNB77r/ZKST1LzwQE4Rg3tb37FXHPrBQ=
-github.com/DataDog/agent-payload/v5 v5.0.150/go.mod h1:lxh9lb5xYrBXjblpIWYUi4deJqVbkIfkjwesi5nskDc=
+github.com/DataDog/agent-payload/v5 v5.0.152 h1:ftd3Y7NPHBnJk/RZ9e72+unr71yNuY9o+3q7vpAoUQ0=
+github.com/DataDog/agent-payload/v5 v5.0.152/go.mod h1:lxh9lb5xYrBXjblpIWYUi4deJqVbkIfkjwesi5nskDc=
 github.com/DataDog/datadog-api-client-go/v2 v2.37.1 h1:weZhrGMO//sMEoSKWngoSQwMp4zBSlEX4p3/YWy9ltw=
 github.com/DataDog/datadog-api-client-go/v2 v2.37.1/go.mod h1:d3tOEgUd2kfsr9uuHQdY+nXrWp4uikgTgVCPdKNK30U=
 github.com/DataDog/datadog-go v3.2.0+incompatible/go.mod h1:LButxg5PwREeZtORoXG3tL4fMGNddJ+vMq1mwgfaqoQ=

comp/otelcol/logsagentpipeline/go.mod
@@ -5,7 +5,7 @@ go 1.23.0
 require github.com/DataDog/datadog-agent/pkg/logs/pipeline v0.64.0-rc.3
 
 require (
-	github.com/DataDog/agent-payload/v5 v5.0.150 // indirect
+	github.com/DataDog/agent-payload/v5 v5.0.152 // indirect
 	github.com/DataDog/datadog-agent/comp/core/hostname/hostnameinterface v0.64.0-rc.3 // indirect
 	github.com/DataDog/datadog-agent/comp/core/secrets v0.61.0 // indirect
 	github.com/DataDog/datadog-agent/comp/core/telemetry v0.61.0 // indirect

comp/otelcol/logsagentpipeline/go.sum
@@ -1,7 +1,7 @@
 cloud.google.com/go v0.26.0/go.mod h1:aQUYkXzVsufM+DwF1aE+0xfcU+56JwCaLick0ClmMTw=
 github.com/BurntSushi/toml v0.3.1/go.mod h1:xHWCNGjB5oqiDr8zfno3MHue2Ht5sIBksp03qcyfWMU=
-github.com/DataDog/agent-payload/v5 v5.0.150 h1:uW9HyA/O5UpuNB77r/ZKST1LzwQE4Rg3tb37FXHPrBQ=
-github.com/DataDog/agent-payload/v5 v5.0.150/go.mod h1:lxh9lb5xYrBXjblpIWYUi4deJqVbkIfkjwesi5nskDc=
+github.com/DataDog/agent-payload/v5 v5.0.152 h1:ftd3Y7NPHBnJk/RZ9e72+unr71yNuY9o+3q7vpAoUQ0=
+github.com/DataDog/agent-payload/v5 v5.0.152/go.mod h1:lxh9lb5xYrBXjblpIWYUi4deJqVbkIfkjwesi5nskDc=
 github.com/DataDog/dd-sensitive-data-scanner/sds-go/go v0.0.0-20240816154533-f7f9beb53a42 h1:RoH7VLzTnxHEugRPIgnGlxwDFszFGI7b3WZZUtWuPRM=
 github.com/DataDog/dd-sensitive-data-scanner/sds-go/go v0.0.0-20240816154533-f7f9beb53a42/go.mod h1:TX7CTOQ3LbQjfAi4SwqUoR5gY1zfUk7VRBDTuArjaDc=
 github.com/DataDog/viper v1.14.0 h1:dIjTe/uJiah+QFqFZ+MXeqgmUvWhg37l37ZxFWxr3is=

comp/otelcol/logsagentpipeline/logsagentpipelineimpl/go.mod
@@ -30,7 +30,7 @@ require (
 )
 
 require (
-	github.com/DataDog/agent-payload/v5 v5.0.150 // indirect
+	github.com/DataDog/agent-payload/v5 v5.0.152 // indirect
 	github.com/DataDog/datadog-agent/comp/core/flare/builder v0.61.0 // indirect
 	github.com/DataDog/datadog-agent/comp/core/flare/types v0.61.0 // indirect
 	github.com/DataDog/datadog-agent/comp/core/secrets v0.61.0 // indirect

comp/otelcol/logsagentpipeline/logsagentpipelineimpl/go.sum
@@ -1,7 +1,7 @@
 cloud.google.com/go v0.26.0/go.mod h1:aQUYkXzVsufM+DwF1aE+0xfcU+56JwCaLick0ClmMTw=
 github.com/BurntSushi/toml v0.3.1/go.mod h1:xHWCNGjB5oqiDr8zfno3MHue2Ht5sIBksp03qcyfWMU=
-github.com/DataDog/agent-payload/v5 v5.0.150 h1:uW9HyA/O5UpuNB77r/ZKST1LzwQE4Rg3tb37FXHPrBQ=
-github.com/DataDog/agent-payload/v5 v5.0.150/go.mod h1:lxh9lb5xYrBXjblpIWYUi4deJqVbkIfkjwesi5nskDc=
+github.com/DataDog/agent-payload/v5 v5.0.152 h1:ftd3Y7NPHBnJk/RZ9e72+unr71yNuY9o+3q7vpAoUQ0=
+github.com/DataDog/agent-payload/v5 v5.0.152/go.mod h1:lxh9lb5xYrBXjblpIWYUi4deJqVbkIfkjwesi5nskDc=
 github.com/DataDog/dd-sensitive-data-scanner/sds-go/go v0.0.0-20240816154533-f7f9beb53a42 h1:RoH7VLzTnxHEugRPIgnGlxwDFszFGI7b3WZZUtWuPRM=
 github.com/DataDog/dd-sensitive-data-scanner/sds-go/go v0.0.0-20240816154533-f7f9beb53a42/go.mod h1:TX7CTOQ3LbQjfAi4SwqUoR5gY1zfUk7VRBDTuArjaDc=
 github.com/DataDog/viper v1.14.0 h1:dIjTe/uJiah+QFqFZ+MXeqgmUvWhg37l37ZxFWxr3is=

comp/otelcol/otlp/components/connector/datadogconnector/go.mod
@@ -56,7 +56,7 @@ require (
 )
 
 require (
-	github.com/DataDog/agent-payload/v5 v5.0.150 // indirect
+	github.com/DataDog/agent-payload/v5 v5.0.152 // indirect
 	github.com/DataDog/datadog-agent/comp/core/config v0.64.1 // indirect
 	github.com/DataDog/datadog-agent/comp/core/flare/builder v0.64.1 // indirect
 	github.com/DataDog/datadog-agent/comp/core/flare/types v0.64.1 // indirect

commit 04be312ccbc01fe0b4af8bf474e26e51055973a0

fix node name in manifest payload (#43769)
### What does this PR do?

This bug was introduced by https://github.com/DataDog/datadog-agent/pull/36903 and in this PR we fix empty node name in manifest payload.

We should add GetNodeName to `PodHandlers` instead of `TerminatedPodCollector`

### QA

This fix is covered by `TestPodProcessor_Process`


Co-authored-by: kangyi.li <kangyi.li@datadoghq.com>

pkg/collector/corechecks/cluster/orchestrator/collectors/k8s/pod_terminated.go
@@ -109,9 +109,3 @@ func (c *TerminatedPodCollector) Process(rcfg *collectors.CollectorRunConfig, li
 
 	return result, nil
 }
-
-// GetNodeName is used to get the node name from the resource.
-func (c *TerminatedPodCollector) GetNodeName(_ processors.ProcessorContext, resource interface{}) string {
-	r := resource.(*v1.Pod)
-	return r.Spec.NodeName
-}

pkg/collector/corechecks/cluster/orchestrator/processors/k8s/pod.go
@@ -208,3 +208,9 @@ func (h *PodHandlers) ScrubBeforeMarshalling(ctx processors.ProcessorContext, re
 		redact.ScrubPod(r, pctx.Cfg.Scrubber)
 	}
 }
+
+// GetNodeName is used to get the node name from the resource.
+func (h *PodHandlers) GetNodeName(_ processors.ProcessorContext, resource interface{}) string {
+	r := resource.(*corev1.Pod)
+	return r.Spec.NodeName
+}

pkg/collector/corechecks/cluster/orchestrator/collectors/k8s/pod_terminated_test.go
@@ -105,10 +105,6 @@ func TestTerminatedPodCollector(t *testing.T) {
 			assert.Len(t, processResult.Result.ManifestMessages, 1)
 			assert.IsType(t, &model.CollectorPod{}, processResult.Result.MetadataMessages[0])
 			assert.IsType(t, &model.CollectorManifest{}, processResult.Result.ManifestMessages[0])
-
-			// Test GetNodeName method
-			nodeName := collector.GetNodeName(nil, pod)
-			assert.Equal(t, "test-node", nodeName)
 		},
 	}
 

pkg/collector/corechecks/cluster/orchestrator/processors/k8s/pod_test.go
@@ -385,6 +385,7 @@ func TestPodProcessor_Process(t *testing.T) {
 	assert.Equal(t, int32(1), manifest1.Type) // K8sPod
 	assert.Equal(t, "v1", manifest1.Version)
 	assert.Equal(t, "json", manifest1.ContentType)
+	assert.Equal(t, "test-node", manifest1.NodeName)
 
 	// Parse the actual manifest content
 	var actualManifestPod corev1.Pod