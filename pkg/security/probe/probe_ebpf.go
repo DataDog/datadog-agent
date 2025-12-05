4e8550863aed1427b578012b9b58c14761b09b62
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2025-12-05 17:09:55 +0100

    [CWS] send the source of event

--- pkg/security/probe/probe_ebpf.go
+++ pkg/security/probe/probe_ebpf.go
@@ -798,6 +798,7 @@ func (p *EBPFProbe) replayEvents(notifyConsumers bool) {
 		entry.Retain()
 
 		event := p.newEBPFPooledEventFromPCE(entry)
+		event.Source = model.EventSourceReplay
 
 		if _, err := entry.HasValidLineage(); err != nil {
 			event.Error = &model.ErrProcessBrokenLineage{Err: err}
@@ -812,6 +813,8 @@ func (p *EBPFProbe) replayEvents(notifyConsumers bool) {
 			for _, s := range snapshotBoundSockets {
 				entry.Retain()
 				bindEvent := p.newBindEventFromReplay(entry, s)
+				bindEvent.Source = model.EventSourceReplay
+
 				events = append(events, bindEvent)
 			}
 		}
@@ -1041,6 +1044,7 @@ func (p *EBPFProbe) zeroEvent() *model.Event {
 	probeEventZeroer(p.event)
 	p.event.FieldHandlers = p.fieldHandlers
 	p.event.Origin = EBPFOrigin
+	p.event.Source = model.EventSourceRuntime
 	return p.event
 }
 
@@ -1076,7 +1080,7 @@ func (p *EBPFProbe) regularUnmarshalEvent(bu BinaryUnmarshaler, eventType model.
 func (p *EBPFProbe) handleEvent(CPU int, data []byte) {
 	// handle play snapshot
 	if p.replayEventsState.Swap(false) {
-		// do not notify consumers as we are replaying the snapshot after a ruleset reload
+		// do not notify consumers as we are replaying the process cache entries after a ruleset reload
 		p.replayEvents(false)
 	}
 
