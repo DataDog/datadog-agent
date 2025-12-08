6f3c2d146015d55961dc0951d978aa6ee9daf7d4
Joseph Gette <joseph.gette@datadoghq.com>
2025-12-08 15:24:40 +0100

    Fix minor configuration mistakes

--- omnibus/config/software/openssl3.rb
+++ omnibus/config/software/openssl3.rb
@@ -45,6 +45,7 @@ build do
     command_on_repo_root "bazelisk run -- //bazel/rules:replace_prefix --prefix #{install_dir}/embedded" \
       " #{install_dir}/embedded/lib/libssl#{lib_extension}" \
       " #{install_dir}/embedded/lib/libcrypto#{lib_extension}" \
-      " #{install_dir}/embedded/lib/pkgconfig/*.pc"
+      " #{install_dir}/embedded/lib/pkgconfig/*.pc" \
+      " #{install_dir}/embedded/bin/openssl"
   end
 end
