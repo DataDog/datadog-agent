14dde706a5759437946140f88120901cd2d69abc
Lucas Liseth <lucas.liseth@datadoghq.com>
2025-12-09 14:19:41 -0500

    Set auto multiline tagging default to true

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -1944,7 +1944,7 @@ func logsagent(config pkgconfigmodel.Setup) {
 	// Add a tag to logs that are truncated by the agent
 	config.BindEnvAndSetDefault("logs_config.tag_truncated_logs", false)
 	// Tag logs with their auto multiline detection label without aggregating them
-	config.BindEnvAndSetDefault("logs_config.auto_multi_line_detection_tagging", false)
+	config.BindEnvAndSetDefault("logs_config.auto_multi_line_detection_tagging", true)
 
 	// Number of logs pipeline instances. Defaults to number of logical CPU cores as defined by GOMAXPROCS or 4, whichever is lower.
 	logsPipelines := min(4, runtime.GOMAXPROCS(0))
