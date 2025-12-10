831633686e771755b0db5d3d80dc545bac9faa76
Sylvain Afchain <safchain@gmail.com>
2025-12-10 16:00:56 +0100

    [CWS] fix process context zero (#44066)
    ### What does this PR do?
    
    This PR fixes a bug where the zero process context was being unintentionally reused. It removes the process-context allocation from the zeroer function; the pointer should only be initialized via setProcessContext and must not be modified directly. By removing the default allocation, we can detect incorrect access patterns earlier through a panic.
    
    ### Motivation
    
    ### Describe how you validated your changes
    
    ### Additional Notes
    
    
    Co-authored-by: sylvain.afchain <sylvain.afchain@datadoghq.com>

--- pkg/security/secl/model/model_unix.go
+++ pkg/security/secl/model/model_unix.go
@@ -154,7 +154,7 @@ type Event struct {
 
 // NewEventZeroer returns a function that can be used to zero an Event
 func NewEventZeroer() func(*Event) {
-	var eventZero = Event{BaseEvent: BaseEvent{Os: runtime.GOOS, ProcessContext: &ProcessContext{}}}
+	var eventZero = Event{BaseEvent: BaseEvent{Os: runtime.GOOS}}
 
 	return func(e *Event) {
 		*e = eventZero
