e31030ea3f770fac1ca0506dfdbf80cde8cf488a
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2025-12-11 10:17:07 +0100

    [CWS] add a related event source

--- pkg/security/probe/probe_ebpf.go
+++ pkg/security/probe/probe_ebpf.go
@@ -1092,6 +1092,7 @@ func (p *EBPFProbe) handleEvent(CPU int, data []byte) {
 			}
 
 			relatedEvent := p.newEBPFPooledEventFromPCE(entry)
+			relatedEvent.Source = model.EventSourceRelated
 
 			if err != nil {
 				var errResolution *path.ErrPathResolution
