0bf78b2dc4fe3304c55c2bb4d20d811b4ab83ae6
Guillermo Julián <guillermo.julian@datadoghq.com>
2025-12-11 17:15:53 +0100

    Add telemetry to stack read functions

--- pkg/ebpf/c/bpf_telemetry.h
+++ pkg/ebpf/c/bpf_telemetry.h
@@ -155,4 +155,17 @@ static void *(*bpf_telemetry_update_patch)(unsigned long, ...) = (void *)PATCH_T
 #define bpf_ringbuf_output_with_telemetry(...) \
     helper_with_telemetry(bpf_ringbuf_output, __VA_ARGS__)
 
+
+#define PT_REGS_USER_STACK_PARM_WITH_TELEMETRY(x, n, ret)                                       \
+    ({                                                                           \
+        __u64 p = 0;                                                             \
+        ret = bpf_probe_read_user_with_telemetry(&p, sizeof(p), ((__u64 *)x->__PT_SP_REG) + n); \
+        p;                                                                       \
+    })
+
+#define PT_REGS_USER_PARM7_WITH_TELEMETRY(x, ret) PT_REGS_USER_STACK_PARM_WITH_TELEMETRY(x, 1, ret)
+#define PT_REGS_USER_PARM8_WITH_TELEMETRY(x, ret) PT_REGS_USER_STACK_PARM_WITH_TELEMETRY(x, 2, ret)
+#define PT_REGS_USER_PARM9_WITH_TELEMETRY(x, ret) PT_REGS_USER_STACK_PARM_WITH_TELEMETRY(x, 3, ret)
+#define PT_REGS_USER_PARM10_WITH_TELEMETRY(x, ret) PT_REGS_USER_STACK_PARM_WITH_TELEMETRY  (x, 4, ret)
+
 #endif // BPF_TELEMETRY_H
