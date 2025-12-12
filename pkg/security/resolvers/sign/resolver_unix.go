4d8e08eb05557126c55e03603319f46f5502032a
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2025-12-12 16:56:03 +0100

    fix windows

--- pkg/security/resolvers/sign/resolver_unix.go
+++ pkg/security/resolvers/sign/resolver_unix.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build unix
+
 // Package sign holds event signature related files
 package sign
 
