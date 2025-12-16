14f03c0cf11c44a9a44c6a8782b5d7b77705a3ef
briantu <brian.tu@datadoghq.com>
2025-12-16 11:27:51 -0500

    Collect host hardware info on mac

---
+++ pkg/inventory/systeminfo/hosthwardware_darwin.h
@@ -0,0 +1,16 @@
+#ifndef HOSTHARDWARE_DARWIN_H
+#define HOSTHARDWARE_DARWIN_H
+
+#include <stdbool.h>
+
+typedef struct {
+    bool found;
+    char *modelIdentifier;
+    char *modelNumber;
+    char *productName;
+    char *serialNumber;
+} DeviceInfo;
+
+DeviceInfo getDeviceInfo(void);
+
+#endif
