DataDog/datadog-agent

commit 8bfccc4ac22737e6fb89407b1d59f1605c9ab4d1

[CWS] report policies using the load order (#43553)
### What does this PR do?

Report the rules in the same order as they are used by the rule engine. Keep the order stable.

### Motivation

### Describe how you validated your changes

### Additional Notes


Co-authored-by: sylvain.afchain <sylvain.afchain@datadoghq.com>

pkg/security/events/rate_limiter.go
@@ -101,7 +101,7 @@ func (rl *RateLimiter) Apply(ruleSet *rules.RuleSet, customRuleIDs []eval.RuleID
 	rl.applyBaseLimitersFromDefault(newLimiters)
 
 	var err error
-	for id, rule := range ruleSet.GetRules() {
+	for _, rule := range ruleSet.GetRules() {
 		every, burst := defaultEvery, defaultBurst
 
 		if duration := rule.Def.Every.GetDuration(); duration != 0 {
@@ -109,13 +109,13 @@ func (rl *RateLimiter) Apply(ruleSet *rules.RuleSet, customRuleIDs []eval.RuleID
 		}
 
 		if len(rule.Def.RateLimiterToken) > 0 {
-			newLimiters[id], err = NewTokenLimiter(maxUniqueToken, burst, every, rule.Def.RateLimiterToken)
+			newLimiters[rule.Def.ID], err = NewTokenLimiter(maxUniqueToken, burst, every, rule.Def.RateLimiterToken)
 			if err != nil {
 				seclog.Errorf("unable to use the token based rate limiter, fallback to the standard one: %s", err)
-				newLimiters[id] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
+				newLimiters[rule.Def.ID] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
 			}
 		} else {
-			newLimiters[id] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
+			newLimiters[rule.Def.ID] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
 		}
 	}
 

pkg/security/probe/probe_ebpf.go
@@ -692,10 +692,10 @@ func (p *EBPFProbe) setupRawPacketProgs(progSpecs []*lib.ProgramSpec, progKey ui
 
 func (p *EBPFProbe) setupRawPacketFilters(rs *rules.RuleSet) error {
 	var rawPacketFilters []rawpacket.Filter
-	for id, rule := range rs.GetRules() {
+	for _, rule := range rs.GetRules() {
 		for _, field := range rule.GetFieldValues("packet.filter") {
 			rawPacketFilters = append(rawPacketFilters, rawpacket.Filter{
-				RuleID:    id,
+				RuleID:    rule.Def.ID,
 				BPFFilter: field.Value.(string),
 				Policy:    rawpacket.PolicyAllow,
 			})

pkg/security/probe/process_killer_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/DataDog/datadog-agent/pkg/security/config"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/compiler/ast"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/compiler/eval"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/containerutils"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
@@ -116,11 +117,12 @@ func TestProcessKillerExclusion(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func craftKillRule(id, scope string) *rules.Rule {
-	return &rules.Rule{
+func craftKillRule(t *testing.T, id, scope string) (*rules.Rule, *rules.RuleSet) {
+	rule := &rules.Rule{
 		PolicyRule: &rules.PolicyRule{
 			Def: &rules.RuleDefinition{
-				ID: id,
+				ID:         id,
+				Expression: `exec.file.path == "/tmp/malware"`,
 				Actions: []*rules.ActionDefinition{
 					{
 						Kill: &rules.KillDefinition{
@@ -148,6 +150,13 @@ func craftKillRule(id, scope string) *rules.Rule {
 			ID: id,
 		},
 	}
+
+	opts := rules.NewRuleOpts(map[eval.EventType]bool{"*": true})
+	ruleSet := rules.NewRuleSet(&model.Model{}, nil, opts, &eval.Opts{})
+	_, err := ruleSet.AddRule(ast.NewParsingContext(false), rule.PolicyRule)
+	assert.NoError(t, err)
+
+	return rule, ruleSet
 }
 
 func craftFakeEvent(containerID, executable string, pid uint32) *model.Event {
@@ -198,8 +207,7 @@ func TestProcessKillerDisarmers(t *testing.T) {
 
 	pk, err := NewProcessKiller(cfg, &FakeProcessKillerOS{})
 	assert.NoError(t, err)
-	rule := craftKillRule("test-rule", "process")
-	ruleSet := rules.NewFakeRuleSet(rule)
+	rule, ruleSet := craftKillRule(t, "test-rule", "process")
 
 	t.Run("dismantle-rule-kill-action-by-executable", func(t *testing.T) {
 		pk.Reset(ruleSet)
@@ -316,8 +324,7 @@ func TestProcessKillerNoDisarmers(t *testing.T) {
 
 	pk, err := NewProcessKiller(cfg, &FakeProcessKillerOS{})
 	assert.NoError(t, err)
-	rule := craftKillRule("test-rule", "process")
-	ruleSet := rules.NewFakeRuleSet(rule)
+	rule, ruleSet := craftKillRule(t, "test-rule", "process")
 
 	t.Run("no-executable-disarmer-rule-to-disarm", func(t *testing.T) {
 		pk.Reset(ruleSet)
@@ -470,8 +477,7 @@ func TestProcessKillerNoEnforcement(t *testing.T) {
 
 	pk, err := NewProcessKiller(cfg, &FakeProcessKillerOS{})
 	assert.NoError(t, err)
-	rule := craftKillRule("test-rule", "process")
-	ruleSet := rules.NewFakeRuleSet(rule)
+	rule, ruleSet := craftKillRule(t, "test-rule", "process")
 
 	t.Run("no-enforcement-rule-kill-action-by-executable", func(t *testing.T) {
 		pk.Reset(ruleSet)
@@ -546,9 +552,8 @@ func TestProcessKillerRuleNoDisarmers(t *testing.T) {
 
 	pk, err := NewProcessKiller(cfg, &FakeProcessKillerOS{})
 	assert.NoError(t, err)
-	rule := craftKillRule("test-rule", "process")
+	rule, ruleSet := craftKillRule(t, "test-rule", "process")
 	rule.PolicyRule.Def.Actions[0].Kill.DisableExecutableDisarmer = true
-	ruleSet := rules.NewFakeRuleSet(rule)
 
 	t.Run("no-executable-disarmer-by-rule-to-disarm", func(t *testing.T) {
 		pk.Reset(ruleSet)
@@ -701,8 +706,7 @@ func TestProcessKillerRuleScopeContainer(t *testing.T) {
 
 	pk, err := NewProcessKiller(cfg, &FakeProcessKillerOS{})
 	assert.NoError(t, err)
-	rule := craftKillRule("test-rule", "container")
-	ruleSet := rules.NewFakeRuleSet(rule)
+	rule, ruleSet := craftKillRule(t, "test-rule", "container")
 
 	t.Run("kill-container-rule-to-executable-disarm", func(t *testing.T) {
 		pk.Reset(ruleSet)

pkg/security/rules/monitor/policy_monitor.go
@@ -11,6 +11,8 @@ package monitor
 import (
 	"context"
 	"fmt"
+	"slices"
+	"strings"
 	"sync"
 	"time"
 
@@ -178,6 +180,7 @@ type RuleState struct {
 	ProductTags            []string          `json:"product_tags,omitempty"`
 	Actions                []RuleAction      `json:"actions,omitempty"`
 	ModifiedBy             []*PolicyMetadata `json:"modified_by,omitempty"`
+	Priority               int               `json:"priority,omitempty"`
 }
 
 // PolicyStatus defines the status of a policy
@@ -330,6 +333,7 @@ func RuleStateFromRule(rule *rules.PolicyRule, policy *rules.PolicyInfo, status 
 		ProductTags:            rule.Def.ProductTags,
 		AgentVersionConstraint: rule.Def.AgentVersionConstraint,
 		Filters:                rule.Def.Filters,
+		Priority:               rule.Def.Priority,
 	}
 
 	for _, action := range rule.Actions {
@@ -424,6 +428,7 @@ func NewPoliciesState(rs *rules.RuleSet, filteredRules []*rules.PolicyRule, err 
 	}
 
 	// rules ignored due to errors
+	// they will be reported at the end of the policies list
 	if err != nil && err.Errors != nil {
 		for _, err := range err.Errors {
 			if rerr, ok := err.(*rules.ErrRuleLoad); ok {
@@ -470,6 +475,11 @@ func NewPoliciesState(rs *rules.RuleSet, filteredRules []*rules.PolicyRule, err 
 	for _, policy := range mp {
 		policies = append(policies, policy)
 	}
+
+	// sort by policy type & name
+	slices.SortStableFunc(policies, func(a, b *PolicyState) int {
+		return strings.Compare(a.Name, b.Name)
+	})
 
 	return policies
 }

pkg/security/rules/monitor/policy_monitor_easyjson.go
@@ -1078,6 +1078,8 @@ func easyjson6151911dDecodeGithubComDataDogDatadogAgentPkgSecurityRulesMonitor1(
 				}
 				in.Delim(']')
 			}
+		case "priority":
+			out.Priority = int(in.Int())
 		default:
 			in.SkipRecursive()
 		}
@@ -1205,6 +1207,11 @@ func easyjson6151911dEncodeGithubComDataDogDatadogAgentPkgSecurityRulesMonitor1(
 			}
 			out.RawByte(']')
 		}
+	}
+	if in.Priority != 0 {
+		const prefix string = ",\"priority\":"
+		out.RawString(prefix)
+		out.Int(int(in.Priority))
 	}
 	out.RawByte('}')
 }

pkg/security/rules/monitor/policy_monitor_test.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 	gocmp "github.com/google/go-cmp/cmp"
-	gocmpopts "github.com/google/go-cmp/cmp/cmpopts"
 
 	multierror "github.com/hashicorp/go-multierror"
 	"github.com/stretchr/testify/assert"
@@ -518,6 +517,13 @@ func TestPolicyMonitorPolicyState(t *testing.T) {
 							AgentVersionConstraint: "~7.x",
 						},
 						{
+							ID:                     "rule_c",
+							Expression:             `exec.file.path == "/etc/foo/qwak"`,
+							Status:                 "loaded",
+							AgentVersionConstraint: ">= 7.42.0",
+						},
+						// this rule in error will be reported at the end of the policies list
+						{
 							ID:                     "rule_b",
 							Expression:             `exec.file.path == "/etc/foo/baz"`,
 							Status:                 "filtered",
@@ -525,11 +531,107 @@ func TestPolicyMonitorPolicyState(t *testing.T) {
 							FilterType:             string(rules.FilterTypeAgentVersion),
 							AgentVersionConstraint: "< 0.0.2",
 						},
+					},
+				},
+			},
+		},
+		{
+			name: "multiple policies with rules with different priorities",
+			policies: []*testPolicy{
+				{
+					info: rules.PolicyInfo{
+						Name:         "Policy A",
+						Source:       "test",
+						InternalType: rules.CustomPolicyType,
+					},
+					def: rules.PolicyDef{
+						Rules: []*rules.RuleDefinition{
+							{
+								ID:         "rule_a",
+								Expression: `exec.file.path == "/etc/foo/bar"`,
+								Priority:   500,
+							},
+							{
+								ID:         "rule_b",
+								Expression: `exec.file.path == "/etc/foo/baz"`,
+								Priority:   999,
+							},
+						},
+					},
+				},
+				{
+					info: rules.PolicyInfo{
+						Name:         "Policy B",
+						Source:       "test",
+						InternalType: rules.DefaultPolicyType,
+					},
+					def: rules.PolicyDef{
+						Rules: []*rules.RuleDefinition{
+							{
+								ID:         "rule_c",
+								Expression: `exec.file.path == "/etc/foo/bar" && exec.pid == 42`,
+								Priority:   500,
+							},
+							{
+								ID:         "rule_e",
+								Expression: `exec.file.path == "/etc/foo/baz"`,
+								Priority:   999,
+							},
+							{
+								ID:         "rule_f",
+								Expression: `exec.file.path == "/etc/foo/qwak"`,
+								Priority:   100,
+							},
+						},
+					},
+				},
+			},
+			expectedPolicyStates: []*PolicyState{
+				{
+					PolicyMetadata: PolicyMetadata{
+						Name:   "Policy A",
+						Source: "test",
+					},
+					Status: PolicyStatusLoaded,
+					Rules: []*RuleState{
 						{
-							ID:                     "rule_c",
-							Expression:             `exec.file.path == "/etc/foo/qwak"`,
-							Status:                 "loaded",
-							AgentVersionConstraint: ">= 7.42.0",
+							ID:         "rule_b",
+							Expression: `exec.file.path == "/etc/foo/baz"`,
+							Status:     "loaded",
+							Priority:   999,
+						},
+						{
+							ID:         "rule_a",
+							Expression: `exec.file.path == "/etc/foo/bar"`,
+							Status:     "loaded",
+							Priority:   500,
+						},
+					},
+				},
+				{
+					PolicyMetadata: PolicyMetadata{
+						Name:   "Policy B",
+						Source: "test",
+					},
+					Status: PolicyStatusLoaded,
+					Rules: []*RuleState{
+						{
+							ID:         "rule_e",
+							Expression: `exec.file.path == "/etc/foo/baz"`,
+							Status:     "loaded",
+							Priority:   999,
+						},
+						{
+							ID:         "rule_c",
+							Expression: `exec.file.path == "/etc/foo/bar" && exec.pid == 42`,
+							Status:     "loaded",
+							Priority:   500,
+						},
+						{
+							ID:         "rule_f",
+							Expression: `exec.file.path == "/etc/foo/qwak"`,
+							Status:     "loaded",
+							Priority:   100,
 						},
 					},
 				},
@@ -625,18 +727,18 @@ func TestPolicyMonitorPolicyState(t *testing.T) {
 						Status: PolicyStatusPartiallyFiltered,
 						Rules: []*RuleState{
 							{
+								ID:         "rule_b",
+								Expression: `exec.file.path == "/etc/foo/baz"`,
+								Status:     "loaded",
+								Filters:    []string{"os == \"windows\""},
+							},
+							{
 								ID:         "rule_a",
 								Expression: `exec.file.path == "/etc/foo/bar"`,
 								Status:     "filtered",
 								Message:    "none of the rule filters matched the host or configuration of this agent",
 								FilterType: string(rules.FilterTypeRuleFilter),
 								Filters:    []string{"os == \"linux\""},
-							},
-							{
-								ID:         "rule_b",
-								Expression: `exec.file.path == "/etc/foo/baz"`,
-								Status:     "loaded",
-								Filters:    []string{"os == \"windows\""},
 							},
 						},
 					},
@@ -666,16 +768,6 @@ func TestPolicyMonitorPolicyState(t *testing.T) {
 	}
 	ruleOpts, evalOpts := rules.NewBothOpts(map[eval.EventType]bool{"*": true})
 
-	// Sort options for gocmp to ensure consistent ordering of slices
-	goCmpOpts := []gocmp.Option{
-		gocmpopts.SortSlices(func(a, b *PolicyState) bool {
-			return a.PolicyMetadata.Name < b.PolicyMetadata.Name
-		}),
-		gocmpopts.SortSlices(func(a, b *RuleState) bool {
-			return a.ID < b.ID
-		}),
-	}
-
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			rs := rules.NewRuleSet(&model.Model{}, eventCtor, ruleOpts, evalOpts)
@@ -683,7 +775,7 @@ func TestPolicyMonitorPolicyState(t *testing.T) {
 			filteredRules, errs := rs.LoadPolicies(loader, rules.PolicyLoaderOpts{MacroFilters: macroFilters, RuleFilters: ruleFilters})
 			policyStates := NewPoliciesState(rs, filteredRules, errs, false)
 
-			assert.True(t, gocmp.Equal(tc.expectedPolicyStates, policyStates, goCmpOpts...), gocmp.Diff(tc.expectedPolicyStates, policyStates, goCmpOpts...))
+			assert.True(t, gocmp.Equal(tc.expectedPolicyStates, policyStates), gocmp.Diff(tc.expectedPolicyStates, policyStates))
 		})
 	}
 }

pkg/security/secl/rules/bucket_test.go
@@ -143,4 +143,23 @@ func TestRuleBucket_AddRule_Order(t *testing.T) {
 
 		assertOrder(t, bucket, []string{"E1", "E2", "E3", "N2", "N1"})
 	})
+
+	t.Run("test5", func(t *testing.T) {
+		bucket := &RuleBucket{}
+
+		n1 := newOrderTestRule(t, "N1", CustomPolicyType, "", 500)
+		n2 := newOrderTestRule(t, "N2", CustomPolicyType, "", 999)
+		n3 := newOrderTestRule(t, "N3", DefaultPolicyType, "", 500)
+		n4 := newOrderTestRule(t, "N4", DefaultPolicyType, "", 999)
+		n5 := newOrderTestRule(t, "N5", DefaultPolicyType, "", 100)
+
+		// Add in mixed order
+		for _, r := range []*Rule{n1, n2, n3, n4, n5} {
+			if err := bucket.AddRule(r); err != nil {
+				t.Error(err)
+			}
+		}
+
+		assertOrder(t, bucket, []string{"N4", "N3", "N5", "N2", "N1"})
+	})
 }

pkg/security/secl/rules/policy.go
@@ -227,19 +227,17 @@ type Policy struct {
 	// Info contains the policy information such as its name, source and type
 	Info PolicyInfo
 	// multiple macros can have the same ID but different filters (e.g. agent version)
-	Macros map[MacroID][]*PolicyMacro
+	Macros []*PolicyMacro
 	// multiple rules can have the same ID but different filters (e.g. agent version)
-	Rules map[RuleID][]*PolicyRule
+	Rules []*PolicyRule
 }
 
 // GetAcceptedMacros returns the list of accepted macros that are part of the policy
 func (p *Policy) GetAcceptedMacros() []*PolicyMacro {
 	var acceptedMacros []*PolicyMacro
-	for _, macros := range p.Macros {
-		for _, macro := range macros {
-			if macro.isAccepted() {
-				acceptedMacros = append(acceptedMacros, macro)
-			}
+	for _, macro := range p.Macros {
+		if macro.isAccepted() {
+			acceptedMacros = append(acceptedMacros, macro)
 		}
 	}
 	return acceptedMacros
@@ -248,11 +246,9 @@ func (p *Policy) GetAcceptedMacros() []*PolicyMacro {
 // GetAcceptedRules returns the list of accepted rules that are part of the policy
 func (p *Policy) GetAcceptedRules() []*PolicyRule {
 	var acceptedRules []*PolicyRule
-	for _, rules := range p.Rules {
-		for _, rule := range rules {
-			if rule.isAccepted() {
-				acceptedRules = append(acceptedRules, rule)
-			}
+	for _, rule := range p.Rules {
+		if rule.isAccepted() {
+			acceptedRules = append(acceptedRules, rule)
 		}
 	}
 	return acceptedRules
@@ -261,11 +257,9 @@ func (p *Policy) GetAcceptedRules() []*PolicyRule {
 // GetFilteredRules returns the list of filtered rules that are part of the policy
 func (p *Policy) GetFilteredRules() []*PolicyRule {
 	var filteredRules []*PolicyRule
-	for _, rules := range p.Rules {
-		for _, rule := range rules {
-			if rule.isFiltered() {
-				filteredRules = append(filteredRules, rule)
-			}
+	for _, rule := range p.Rules {
+		if rule.isFiltered() {
+			filteredRules = append(filteredRules, rule)
 		}
 	}
 	return filteredRules
@@ -274,14 +268,16 @@ func (p *Policy) GetFilteredRules() []*PolicyRule {
 // SetInternalCallbackAction adds an internal callback action for the given rule IDs
 func (p *Policy) SetInternalCallbackAction(ruleID ...RuleID) {
 	for _, id := range ruleID {
-		if rules, ok := p.Rules[id]; ok {
-			for _, rule := range rules {
-				if rule.isAccepted() && rule.Def.ID == id {
-					rule.Actions = append(rule.Actions, &Action{
-						InternalCallback: &InternalCallbackDefinition{},
-						Def:              &ActionDefinition{},
-					})
-				}
+		for _, rule := range p.Rules {
+			if rule.Def.ID != id {
+				continue
+			}
+
+			if rule.isAccepted() && rule.Def.ID == id {
+				rule.Actions = append(rule.Actions, &Action{
+					InternalCallback: &InternalCallbackDefinition{},
+					Def:              &ActionDefinition{},
+				})
 			}
 		}
 	}
@@ -297,7 +293,7 @@ MACROS:
 			Accepted: true,
 			Policy:   p,
 		}
-		p.Macros[macroDef.ID] = append(p.Macros[macroDef.ID], macro)
+		p.Macros = append(p.Macros, macro)
 		for _, filter := range macroFilters {
 			macro.Accepted, macro.Error = filter.IsMacroAccepted(macroDef)
 			if macro.Error != nil {
@@ -328,7 +324,7 @@ RULES:
 			Accepted: true,
 			Policy:   p.Info, // copy the policy information as it can be modified on a per-rule basis when merging rules from different policies
 		}
-		p.Rules[ruleDef.ID] = append(p.Rules[ruleDef.ID], rule)
+		p.Rules = append(p.Rules, rule)
 		for _, filter := range ruleFilters {
 			rule.Accepted, rule.Error = filter.IsRuleAccepted(ruleDef)
 			if rule.Error != nil {
@@ -383,10 +379,8 @@ func LoadPolicyFromDefinition(info *PolicyInfo, def *PolicyDef, macroFilters []M
 	}
 
 	p := &Policy{
-		Def:    def,
-		Info:   *info,
-		Macros: make(map[MacroID][]*PolicyMacro, len(def.Macros)),
-		Rules:  make(map[RuleID][]*PolicyRule, len(def.Rules)),
+		Def:  def,
+		Info: *info,
 	}
 
 	return p, p.parse(macroFilters, ruleFilters)

commit d281b5fe556623c6aa8c4d8985567964eb57a472

[CWS] Do not rate limit rules with 'fim' event type (#44482)
### What does this PR do?

Do not rate limit rules with 'fim' event type

### Motivation

The 'fim' event type makes uses of internal expanded rules for the different event types.
These rules have an internal ID that is not registered with the rate limiter, causing these rules
to be rate limited.

### Describe how you validated your changes

Create an agent rule with the "fim" event type, triggered the rule and checked that it was properly sent to the BE.

### Additional Notes

This is a regression introduced by https://github.com/DataDog/datadog-agent/pull/43553

Co-authored-by: sylvain.baubeau <sylvain.baubeau@datadoghq.com>

pkg/security/events/rate_limiter.go
@@ -109,13 +109,13 @@ func (rl *RateLimiter) Apply(ruleSet *rules.RuleSet, customRuleIDs []eval.RuleID
 		}
 
 		if len(rule.Def.RateLimiterToken) > 0 {
-			newLimiters[rule.Def.ID], err = NewTokenLimiter(maxUniqueToken, burst, every, rule.Def.RateLimiterToken)
+			newLimiters[rule.ID], err = NewTokenLimiter(maxUniqueToken, burst, every, rule.Def.RateLimiterToken)
 			if err != nil {
 				seclog.Errorf("unable to use the token based rate limiter, fallback to the standard one: %s", err)
-				newLimiters[rule.Def.ID] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
+				newLimiters[rule.ID] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
 			}
 		} else {
-			newLimiters[rule.Def.ID] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
+			newLimiters[rule.ID] = NewStdLimiter(rate.Every(time.Duration(every)), burst)
 		}
 	}
 