634641d9ad5b29ffa7fd98b3083a21f71c2a3702
Jim Wilson <86026167+jmw51798@users.noreply.github.com>
2025-12-24 14:01:18 -0700

    ...

--- pkg/network/ebpf/c/conntrack/maps.h
+++ pkg/network/ebpf/c/conntrack/maps.h
@@ -7,14 +7,14 @@
 #include "conn_tuple.h"
 #include "conntrack/types.h"
 
-/* This map is used for tracking conntrack entries
- */
-BPF_HASH_MAP(conntrack, conntrack_tuple_t, conntrack_tuple_t, 1)
-
 /* JMW update comment to mention both probes thatuse it */
-/* This map is used for tracking pending confirmations (pid_tgid -> ct pointer) to allow correlation between kprobe and kretprobe of
+/* This map is used for tracking JMW pending confirmations (pid_tgid -> ct pointer) to allow correlation between kprobe and kretprobe of
  * nf_conntrack_confirm JMW
  */
 BPF_HASH_MAP(conntrack_args, u64, u64, 10240) // JMW size?  add config and resize like for conntrack map?
+
+/* This map is used for tracking conntrack entries
+ */
+BPF_HASH_MAP(conntrack, conntrack_tuple_t, conntrack_tuple_t, 1)
 
 #endif
