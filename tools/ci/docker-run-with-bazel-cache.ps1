306d74d9ab49cfeea96bd3b38852b87f6427fd4c
AliDatadog <ali.benabdallah@datadoghq.com>
2025-12-26 11:04:54 +0100

    use .bazeliskrc

--- tools/ci/docker-run-with-bazel-cache.ps1
+++ tools/ci/docker-run-with-bazel-cache.ps1
@@ -3,5 +3,5 @@ $PSNativeCommandUseErrorActionPreference = $true
 Set-StrictMode -Version 3.0
 
 mkdir "$env:XDG_CACHE_HOME" -Force | Out-Null
-& docker run --rm --env=BAZELISK_HOME --env=BAZELISK_BASE_URL --env=CI --env=XDG_CACHE_HOME --volume="${env:XDG_CACHE_HOME}:${env:XDG_CACHE_HOME}" $args
+& docker run --rm --env=BAZELISK_HOME --env=CI --env=XDG_CACHE_HOME --volume="${env:XDG_CACHE_HOME}:${env:XDG_CACHE_HOME}" $args
 exit $LASTEXITCODE
