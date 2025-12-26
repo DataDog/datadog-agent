f8f6c63b975d73ba92f5774e5a3a2e5319b05933
Théo de Magalhaes <theo.demagalhaes@datadoghq.com>
2025-12-26 17:25:13 +0000

    made docker config closer to dd-otel-host-profiler

--- tools/host-profiler/profiler/entrypoint.sh
+++ tools/host-profiler/profiler/entrypoint.sh
@@ -4,7 +4,7 @@ DD_API_KEY="$(sudo cat /run/secrets/dd-api-key)"
 DD_APP_KEY="$(sudo cat /run/secrets/dd-app-key)"
 export DD_API_KEY DD_APP_KEY
 
-sudo mountpoint -q /sys/kernel/tracing || sudo mount -t debugfs none /sys/kernel/debug
+sudo mountpoint -q /sys/kernel/tracing || sudo mount -t tracefs tracefs /sys/kernel/tracing
 
 cd /app
 exec "$@"
