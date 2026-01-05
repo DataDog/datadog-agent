fe16568effacb264966487be248532f8a79bfac4
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2026-01-05 16:17:43 +0100

    discovery: Import sd-agent

---
+++ pkg/collector/corechecks/servicediscovery/module/rust/tests/testdata/go_dummy_prog/test_binary.go
@@ -0,0 +1,21 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Signal that we're ready
+	fmt.Fprintln(os.Stdout, "READY")
+
+	// Wait for stdin input before exiting
+	reader := bufio.NewReader(os.Stdin)
+	reader.ReadByte()
+}
