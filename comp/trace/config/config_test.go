6b4a989da8407badf2006c5f994e03cae1b05380
Kathie Huang <kathie.huang@datadoghq.com>
2026-01-05 17:18:59 -0800

    Add tests and example of env var format

--- comp/trace/config/config_test.go
+++ comp/trace/config/config_test.go
@@ -627,6 +627,11 @@ func TestFullYamlConfig(t *testing.T) {
 	}, cfg.InstallSignature)
 
 	assert.Equal(t, "edge", cfg.APMMode)
+
+	assert.Equal(t, map[string]string{
+		"_dd.origin": "appservice",
+		"env":        "staging",
+	}, cfg.AdditionalProfileTags)
 }
 
 func TestFileLoggingDisabled(t *testing.T) {
