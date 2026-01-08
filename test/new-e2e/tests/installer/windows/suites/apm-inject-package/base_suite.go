269f90ef78fec03b545a13ee036bf4d803798ca7
Jack Phillips <jack.phillips@datadoghq.com>
2026-01-08 13:37:32 -0500

    fix contains string

--- test/new-e2e/tests/installer/windows/suites/apm-inject-package/base_suite.go
+++ test/new-e2e/tests/installer/windows/suites/apm-inject-package/base_suite.go
@@ -55,8 +55,8 @@ $env:DD_INJECT_LOG_LEVEL = "debug"
 	output, err := host.Execute(script)
 	s.Require().NoError(err)
 	if enabled {
-		s.Require().Contains(output, "Instrumenting Java")
+		s.Require().Contains(output, "main executable path")
 	} else {
-		s.Require().NotContains(output, "Instrumenting Java")
+		s.Require().NotContains(output, "main executable path")
 	}
 }
