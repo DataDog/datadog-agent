7b27031a8c320155e156bd930491c8044b3ec467
Cédric Van Rompay <cedric.vanrompay@datadoghq.com>
2026-01-08 11:16:02 +0100

    do not warn about missing CI job ID if not in CI

--- tasks/omnibus.py
+++ tasks/omnibus.py
@@ -52,7 +52,7 @@ def omnibus_run_task(ctx, task, target_project, base_dir, env, log_level="info",
 
         if 'CI_JOB_ID' in os.environ:
             env['CI_JOB_ID'] = os.environ.get('CI_JOB_ID')
-        else:
+        elif os.environ.get("CI"):
             print("WARNING: CI_JOB_ID not present in environment")
 
         with gitlab_section(f"Running omnibus task {task}", collapsed=True):
