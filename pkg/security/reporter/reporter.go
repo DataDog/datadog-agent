f5596dfc279c77e7fda8755ea6a01825c2f10f89
Ella Taira <ella.taira@datadoghq.com>
2026-01-12 18:29:56 -0500

    fixing tests

--- pkg/security/reporter/reporter.go
+++ pkg/security/reporter/reporter.go
@@ -61,6 +61,7 @@ func newReporter(hostname string, stopper startstop.Stopper, sourceName, sourceT
 		compression,
 		cfg.GetBool("logs_config.disable_distributed_senders"),
 		false, // serverless
+		nil,   // observer handle (not integrated with security-agent yet)
 	)
 	pipelineProvider.Start()
 	stopper.Add(pipelineProvider)
