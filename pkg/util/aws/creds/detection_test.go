85a31c79d685755b255cfaf7457dc2b672e3896c
wynbennett <wyn.bennett@datadoghq.com>
2026-01-12 13:15:53 -0500

    DELA-251 - Update build tags

--- pkg/util/aws/creds/detection_test.go
+++ pkg/util/aws/creds/detection_test.go
@@ -1,9 +1,9 @@
-//go:build ec2
-
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
+
+//go:build ec2
 
 package creds
 
@@ -12,14 +12,14 @@ import (
 	"testing"
 )
 
-func TestIsRunningOnAWS(t *testing.T) {
+func TestIsRunningOnAWS(_ *testing.T) {
 	// This test just ensures the function is callable and doesn't panic
 	// The actual behavior depends on the environment
 	ctx := context.Background()
 	_ = IsRunningOnAWS(ctx)
 }
 
-func TestGetAWSRegion(t *testing.T) {
+func TestGetAWSRegion(_ *testing.T) {
 	// This test just ensures the function is callable and doesn't panic
 	// The actual behavior depends on the environment
 	ctx := context.Background()
