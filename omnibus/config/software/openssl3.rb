0b34baefbc19e20281f858470255745bc84eb4b3
Alex Lopez <alex.lopez@datadoghq.com>
2026-01-13 10:05:11 +0100

    Include openssl's lib/engines-3 folder back
    Another folder that we seem to have dropped, the lack of which is
    causing some tests to fail.

--- omnibus/config/software/openssl3.rb
+++ omnibus/config/software/openssl3.rb
@@ -32,19 +32,24 @@ version("3.5.4") { source sha256: "967311f84955316969bdb1d8d4b983718ef42338639c6
 relative_path "openssl-#{version}"
 
 build do
-  # OpenSSL on Windows now gets installed as part of the Python install, so we don't need to do anything here
-  # Also, excluding openssl build for FIPS as it will be built as part of the FIPS provider.
-  if !windows?
+  if windows?
+    command_on_repo_root "bazelisk run -- @openssl//:install --destdir=#{install_dir}/embedded3"
+  else
     command_on_repo_root "bazelisk run -- @openssl//:install --destdir=#{install_dir}/embedded"
     lib_extension = if linux_target? then ".so" else ".dylib" end
-    command_on_repo_root "bazelisk run -- //bazel/rules:replace_prefix --prefix #{install_dir}/embedded" \
-      " #{install_dir}/embedded/lib/libssl#{lib_extension}" \
-      " #{install_dir}/embedded/lib/libcrypto#{lib_extension}" \
-      " #{install_dir}/embedded/lib/pkgconfig/*.pc" \
-      " #{install_dir}/embedded/bin/openssl"
+
+    files_to_patch = [
+      "lib/libssl#{lib_extension}",
+      "lib/libcrypto#{lib_extension}",
+      "lib/engines-3/*#{lib_extension}",
+      "lib/pkgconfig/*.pc",
+      "bin/openssl",
+    ].map { |path| "#{install_dir}/embedded/#{path}" }
+
+    command_on_repo_root "bazelisk run -- //bazel/rules:replace_prefix --prefix #{install_dir}/embedded #{files_to_patch.join(' ')}"
     command_on_repo_root "bazelisk run -- //deps/openssl:fix_openssl_paths --destdir #{install_dir}/embedded" \
       " #{install_dir}/embedded/lib/libssl#{lib_extension}" \
-      " #{install_dir}/embedded/lib/libcrypto#{lib_extension}" \
+      " #{install_dir}/embedded/lib/libcrypto#{lib_extension}"
   end
 
   if fips_mode?
