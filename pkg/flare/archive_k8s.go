7dba60e86408b95e13ae17e3249148fc6e691c93
Jon Rosario <jon.rosario@datadoghq.com>
2026-01-14 11:05:55 -0500

    Move pkg/orchestrator/redact to pkg/redact, remove orchestrator build tag

--- pkg/flare/archive_k8s.go
+++ pkg/flare/archive_k8s.go
@@ -15,7 +15,7 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/DataDog/datadog-agent/pkg/orchestrator/redact"
+	"github.com/DataDog/datadog-agent/pkg/redact"
 	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/kubelet"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
 )
