9ad778f04b54b4dccee10682ce97d1446ae5eb18
Jeremy Hanna <jeremy.hanna@datadoghq.com>
2026-01-15 17:27:51 -0500

    Fix linter issues

--- comp/api/api/apiimpl/listener/common.go
+++ comp/api/api/apiimpl/listener/common.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package listener supports platform specific net.Listener implementations for IPC Server creation
 package listener
 
 import (
