eeb3bced5a6244eae076c971d390d2d223e18e36
kovags <marcio.kovags@datadoghq.com>
2026-01-16 19:58:54 +0100

    try again

--- pkg/security/ebpf/c/include/helpers/discarders.h
+++ pkg/security/ebpf/c/include/helpers/discarders.h
@@ -309,9 +309,8 @@ int __attribute__((always_inline)) dentry_resolver_discarder_event_type(struct s
 // sets all the bytes after the null terminator to null and truncates the string if needed
 void __attribute__((always_inline)) clean_str_trailing_zeros(char *data, int string_size, int array_size) {
     bool found_null = false;
-    const int max = 16;
     #pragma unroll
-    for(int i=0; i != max; ++i) {
+    for(int i=0; i != 17; ++i) {
         if (i >= array_size) {
             break;
         }
