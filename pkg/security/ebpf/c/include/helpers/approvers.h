3107231c95842526417394def2455e311a769f9e
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2026-01-16 10:59:18 +0100

    [CWS] stream open event base on kern limiter
    add pid

--- pkg/security/ebpf/c/include/helpers/approvers.h
+++ pkg/security/ebpf/c/include/helpers/approvers.h
@@ -267,6 +267,26 @@ enum SYSCALL_STATE __attribute__((always_inline)) approve_open_by_flags(struct s
     return DISCARDED;
 }
 
+enum SYSCALL_STATE __attribute__((always_inline)) approve_open_sample(struct file_t *file) {
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+    struct pid_path_key_t key = {
+        .pid = pid,
+        .path_key = file->path_key,
+    };
+
+    u8 value = 0;
+    if (bpf_map_update_elem(&open_samples, &key, &value, BPF_NOEXIST) < 0) {
+        return DISCARDED;
+    }
+
+    if (!global_limiter_allow(OPEN_SAMPLE_LIMITER, 100, 1)) {
+        return DISCARDED;
+    }
+
+    return SAMPLED;
+}
+
+
 enum SYSCALL_STATE __attribute__((always_inline)) open_approvers(struct syscall_cache_t *syscall) {
     enum SYSCALL_STATE state = approve_by_basename(syscall->open.dentry, EVENT_OPEN);
     if (state == DISCARDED) {
@@ -277,6 +297,11 @@ enum SYSCALL_STATE __attribute__((always_inline)) open_approvers(struct syscall_
     }
     if (state == DISCARDED) {
         state = approve_by_in_upper_layer(EVENT_OPEN, &syscall->open.file);
+    }
+
+    // can return DISCARDED or SAMPLED
+    if (state == DISCARDED) {
+        state = approve_open_sample(&syscall->open.file);
     }
 
     return state;
