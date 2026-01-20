e7b868d9d273329a975659f9b58b78d3a383d81d
Yang Song <songy23@users.noreply.github.com>
2026-01-20 17:22:40 -0500

    Update pkg/config/structure/unmarshal_test.go
    Co-authored-by: Dustin Long <me@dustmop.io>

--- pkg/config/structure/unmarshal_test.go
+++ pkg/config/structure/unmarshal_test.go
@@ -1348,7 +1348,7 @@ service:
   foo: bar
 `,
 			wantErr: true,
-			errMsg:  "'structure.serviceConfig' has invalid keys: apikey, foo, name",
+			errMsg:  "has invalid keys: apikey, foo, name",
 		},
 	}
 
