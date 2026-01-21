1ddafb9dff79a3bd8b497ac4ceaf31eb2c5d1c2d
Gouri Yerra <gouri1998@gmail.com>
2026-01-21 14:11:35 -0700

    Formatting

--- pkg/util/winutil/pe_timestamp_windows.go
+++ pkg/util/winutil/pe_timestamp_windows.go
@@ -1,10 +1,10 @@
-//go:build windows
-
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2026-present Datadog, Inc.
-//
+
+//go:build windows
+
 // Package winutil contains Windows OS utilities
 package winutil
 
