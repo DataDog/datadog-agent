ef2cd0baa2430260441332276ce9ce26c202f624
Paul Cacheux <paul.cacheux@datadoghq.com>
2026-01-21 22:06:03 +0100

    [CWS] remove ipc component from direct sender and api server

--- pkg/security/module/cws.go
+++ pkg/security/module/cws.go
@@ -96,7 +96,7 @@ func NewCWSConsumer(evm *eventmonitor.EventMonitor, cfg *config.RuntimeSecurityC
 	}
 
 	family, socketPath := socket.GetSocketAddress(cmdSocketPath)
-	apiServer, err := NewAPIServer(cfg, evm.Probe, opts.MsgSender, evm.StatsdClient, selfTester, compression, ipc)
+	apiServer, err := NewAPIServer(cfg, evm.Probe, opts.MsgSender, evm.StatsdClient, selfTester, compression, hostname)
 	if err != nil {
 		return nil, err
 	}
