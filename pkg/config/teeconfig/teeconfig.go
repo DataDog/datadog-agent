185a3b52b83e7f93dc346c987d6dc575bf40ab53
Maxime mouial <maxime.mouial@gmail.com>
2026-01-26 18:07:26 +0100

    Fix TeeConfig wrapper

--- pkg/config/teeconfig/teeconfig.go
+++ pkg/config/teeconfig/teeconfig.go
@@ -7,6 +7,7 @@
 package teeconfig
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"reflect"
@@ -394,8 +395,9 @@ func (t *teeConfig) ReadInConfig() error {
 
 // ReadConfig wraps Viper for concurrent access
 func (t *teeConfig) ReadConfig(in io.Reader) error {
-	err1 := t.baseline.ReadConfig(in)
-	err2 := t.compare.ReadConfig(in)
+	data, _ := io.ReadAll(in)
+	err1 := t.baseline.ReadConfig(bytes.NewBuffer(data))
+	err2 := t.compare.ReadConfig(bytes.NewBuffer(data))
 	if (err1 != nil && err2 == nil) || (err1 == nil && err2 != nil) {
 		log.Warnf("difference in config: ReadConfig() -> base error: %v | compare error: %v", err1, err2)
 	}
@@ -404,8 +406,9 @@ func (t *teeConfig) ReadConfig(in io.Reader) error {
 
 // MergeConfig wraps Viper for concurrent access
 func (t *teeConfig) MergeConfig(in io.Reader) error {
-	err1 := t.baseline.MergeConfig(in)
-	err2 := t.compare.MergeConfig(in)
+	data, _ := io.ReadAll(in)
+	err1 := t.baseline.MergeConfig(bytes.NewBuffer(data))
+	err2 := t.compare.MergeConfig(bytes.NewBuffer(data))
 	if (err1 != nil && err2 == nil) || (err1 == nil && err2 != nil) {
 		log.Warnf("difference in config: MergeConfig() -> base error: %v | compare error: %v", err1, err2)
 	}
@@ -546,7 +549,7 @@ func (t *teeConfig) BindEnvAndSetDefault(key string, val interface{}, env ...str
 }
 
 func (t *teeConfig) Warnings() *model.Warnings {
-	return nil
+	return t.baseline.Warnings()
 }
 
 func (t *teeConfig) Object() model.Reader {
