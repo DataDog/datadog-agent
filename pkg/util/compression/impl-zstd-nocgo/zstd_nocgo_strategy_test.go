b6cd2cfe539bf93ee320797ec168cd997d89ba08
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2026-01-28 13:49:14 -0800

    Fuzz CompressBound in pkg/util/compression, fix formula bugs
    This commit adds fuzz tests for CompressBound in our zstd, zlib and
    gzip strategies asserting the following property:
    
    - `len(compress(b)) <= CompressBound(len(b))` for all `b`
    
    I found no defects with our zstd strategy implementations. For zlib I
    did find that the original formula was correct to the C implementation
    but Go adds an additional 5 bytes that the C zlib does not. Likewise,
    I found that the gzip implementation made a redundant Flush which
    added an uneccessary empty block into the compressed bytes.
    
    The CompressBound function is used to estimate whether items will
    compress into payload limits. Zlib and Gzip underestimated, meaning
    payloads would be potentially oversized and rejected on
    transmission. Given that Zstd is accurate and the current default it
    is not believed that this bug is explored in practice.
    
    This commit is peer to PR #45534 and #45655. It is motivated by the
    potential end state in spike PR #45519. That is, if we want to make
    non-trivial changes to this package systematic testing of the package
    is warranted.
    
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

---
+++ pkg/util/compression/impl-zstd-nocgo/zstd_nocgo_strategy_test.go
@@ -0,0 +1,43 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package zstdimpl
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/DataDog/datadog-agent/pkg/util/compression"
+)
+
+// Asserts that len(compress(b)) <= CompressBound(len(b)) for all b and levels.
+func FuzzZstdNoCgoCompressBound(f *testing.F) {
+	f.Add([]byte("hello world"), 1)
+	f.Add([]byte(""), 3)
+	f.Add([]byte("a"), 5)
+	f.Add([]byte(string(make([]byte, 1000))), 10)
+	f.Add([]byte("The quick brown fox jumps over the lazy dog"), 15)
+	f.Add(bytes.Repeat([]byte("abcd"), 250), 22)
+
+	f.Fuzz(func(t *testing.T, data []byte, level int) {
+		// Clamp level to valid zstd range: 1 to 22
+		if level < 1 {
+			level = 1
+		} else if level > 22 {
+			level = 22
+		}
+
+		c := New(Requires{Level: compression.ZstdCompressionLevel(level)})
+		compressed, err := c.Compress(data)
+		if err != nil {
+			t.Fatalf("Compress failed: %v", err)
+		}
+
+		bound := c.CompressBound(len(data))
+		if len(compressed) > bound {
+			t.Errorf("CompressBound violated at level %d: len(compress(b))=%d > CompressBound(len(b))=%d", level, len(compressed), bound)
+		}
+	})
+}
