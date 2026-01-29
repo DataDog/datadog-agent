ed5c28d535482c5cfa30a4e6ddd80301852ea953
Alex Lopez <alex.lopez@datadoghq.com>
2026-01-29 10:26:36 +0100

    Fixup merge mistakes

--- omnibus/config/software/python3.rb
+++ omnibus/config/software/python3.rb
@@ -29,48 +29,6 @@ build do
       " #{install_dir}/embedded/lib/#{sh_lib}" \
       " #{install_dir}/embedded/lib/python3.13/lib-dynload/*.so" \
       " #{install_dir}/embedded/bin/python3*"
-  elsif fips_mode?
-    ###############################
-    # Setup openssl dependency... #
-    ###############################
-
-    if mac_os_x?
-      python_configure_options.push("--enable-ipv6",
-                            "--with-universal-archs=#{arm_target? ? "universal2" : "intel"}",
-                            "--enable-shared")
-    elsif linux_target?
-      python_configure_options.push("--enable-shared",
-                            "--enable-ipv6")
-    elsif aix?
-      # something here...
-    end
-
-    python_configure_options.push("--with-dbmliborder=")
-
-    # Force different defaults for the "optimization settings"
-    # This removes the debug symbol generation and doesn't enable all warnings
-    env["OPT"] = "-DNDEBUG -fwrapv"
-    configure(*python_configure_options, :env => env)
-    command "make -j #{workers}", :env => env
-    command "make install", :env => env
-
-    # There exists no configure flag to tell Python to not compile readline support :(
-    major, minor, bugfix = version.split(".")
-
-    # Don't forward CC and CXX to python extensions Makefile, it's quite unlikely that any non default
-    # compiler we use would end up being available in the system/docker image used by customers
-    if linux_target? && env["CC"]
-      command "sed -i \"s/^CC=[[:space:]]*${CC}/CC=gcc/\" #{install_dir}/embedded/lib/python#{major}.#{minor}/config-#{major}.#{minor}-*-linux-gnu/Makefile", :env => env
-      command "sed -i \"s/${CC}/gcc/g\" #{install_dir}/embedded/lib/python#{major}.#{minor}/_sysconfigdata__linux_*-linux-gnu.py", :env => env
-    end
-    if linux_target? && env["CXX"]
-      command "sed -i \"s/^CXX=[[:space:]]*${CXX}/CC=g++/\" #{install_dir}/embedded/lib/python#{major}.#{minor}/config-#{major}.#{minor}-*-linux-gnu/Makefile", :env => env
-      command "sed -i \"s/${CXX}/g++/g\" #{install_dir}/embedded/lib/python#{major}.#{minor}/_sysconfigdata__linux_*-linux-gnu.py", :env => env
-    end
-    delete "#{install_dir}/embedded/lib/python#{major}.#{minor}/test"
-    block do
-      FileUtils.rm_f(Dir.glob("#{install_dir}/embedded/lib/python#{major}.#{minor}/distutils/command/wininst-*.exe"))
-    end
   else
     fips_flag = fips_mode? ? "--//:fips_mode" : ""
     command_on_repo_root "bazelisk run #{fips_flag} -- @cpython//:install --destdir=#{python_3_embedded}"
