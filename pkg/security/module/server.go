b206f200fe89c68d743994d847ab4abe4dde434b
Yoann Ghigoff <yoann.ghigoff@datadoghq.com>
2026-01-29 18:23:26 +0100

    fix typo

--- pkg/security/module/server.go
+++ pkg/security/module/server.go
@@ -266,7 +266,7 @@ func (a *APIServer) dequeue(now time.Time, cb func(msg *pendingMsg, retry bool) 
 			queueSize--
 			return true
 		}
-		seclog.Warnf("failed to sent event for rule `%s`, retry %d/%d, queue size: %d", msg.ruleID, msg.retry, msgMaxRetry, len(a.queue))
+		seclog.Warnf("failed to send event for rule `%s`, retry %d/%d, queue size: %d", msg.ruleID, msg.retry, msgMaxRetry, len(a.queue))
 
 		msg.sendAfter = now.Add(retryDelay)
 		msg.retry++
