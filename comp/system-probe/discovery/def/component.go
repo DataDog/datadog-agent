9ce6c8763c8ad7d7e761ae7fabb7386b1bf0a0c7
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-01-29 18:15:44 -0800

    add discovery module

---
+++ comp/system-probe/discovery/def/component.go
@@ -0,0 +1,16 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2026-present Datadog, Inc.
+
+// Package discovery ... /* TODO: detailed doc comment for the component */
+package discovery
+
+import "github.com/DataDog/datadog-agent/comp/system-probe/types"
+
+// team: agent-discovery
+
+// Component is the component type.
+type Component interface {
+	types.SystemProbeModuleComponent
+}
