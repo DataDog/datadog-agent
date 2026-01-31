2e1c8c45ef747b813ecf89d3e785e52669640a21
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-01-30 17:57:45 -0800

    fixup fx for unsupported build tags

--- comp/system-probe/dynamicinstrumentation/fx/fx_unsupported.go
+++ comp/system-probe/dynamicinstrumentation/fx/fx_unsupported.go
@@ -1,11 +1,11 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
 // under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
-// Copyright 2025-present Datadog, Inc.
+// Copyright 2026-present Datadog, Inc.
 
-//go:build linux && (!linux_bpf || !nvml)
+//go:build linux && !linux_bpf
 
-// Package fx provides the fx module for the gpu component
+// Package fx provides the fx module for the dynamicinstrumentation component
 package fx
 
 import "github.com/DataDog/datadog-agent/pkg/util/fxutil"
