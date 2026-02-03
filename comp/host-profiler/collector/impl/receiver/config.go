c796159a5bda70b37b331b1b2dab29f1a78e9728
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2026-02-03 22:10:00 +0000

    host-profiler: remove dependency on dd-otel-host-profiler
    Import symboluploader code from dd-otel-host-profiler.

--- comp/host-profiler/collector/impl/receiver/config.go
+++ comp/host-profiler/collector/impl/receiver/config.go
@@ -12,28 +12,29 @@ import (
 	"strings"
 	"time"
 
-	"github.com/DataDog/datadog-agent/pkg/version"
-	"github.com/DataDog/dd-otel-host-profiler/config"
-	"github.com/DataDog/dd-otel-host-profiler/reporter"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap/xconfmap"
 	ebpfcollector "go.opentelemetry.io/ebpf-profiler/collector"
 	ebpfconfig "go.opentelemetry.io/ebpf-profiler/collector/config"
 	"go.opentelemetry.io/ebpf-profiler/tracer/types"
-)
 
-// ReporterConfig is the configuration for the reporter.
-type ReporterConfig struct {
-	CollectContext bool `mapstructure:"collect_context"`
-}
+	"github.com/DataDog/datadog-agent/comp/host-profiler/symboluploader"
+	"github.com/DataDog/datadog-agent/pkg/version"
+)
 
 // Config is the configuration for the profiles receiver.
 type Config struct {
-	EbpfCollectorConfig  *ebpfconfig.Config            `mapstructure:"ebpf_collector"`
-	SymbolUploader       reporter.SymbolUploaderConfig `mapstructure:"symbol_uploader"`
-	ReporterConfig       ReporterConfig                `mapstructure:"reporter"`
-	EnableSplitByService bool                          `mapstructure:"enable_split_by_service"`
+	EbpfCollectorConfig  *ebpfconfig.Config                  `mapstructure:"ebpf_collector"`
+	SymbolUploader       symboluploader.SymbolUploaderConfig `mapstructure:"symbol_uploader"`
+	CollectContext       bool                                `mapstructure:"collect_context"`
+	EnableSplitByService bool                                `mapstructure:"enable_split_by_service"`
 }
+
+// ServiceNameEnvVars is the list of environment variables used to determine the service name.
+// The order indicates which environment variable takes precedence.
+var serviceNameEnvVars = []string{"DD_SERVICE", "OTEL_SERVICE_NAME"}
+
+const profilerName = "full-host-profiler"
 
 var _ xconfmap.Validator = (*Config)(nil)
 
@@ -53,7 +54,7 @@ func (c *Config) Validate() error {
 	if err := c.EbpfCollectorConfig.Validate(); err != nil {
 		return err
 	}
-	if c.ReporterConfig.CollectContext {
+	if c.CollectContext {
 		includeTracers, err := types.Parse(c.EbpfCollectorConfig.Tracers)
 		if err != nil {
 			return err
@@ -62,7 +63,7 @@ func (c *Config) Validate() error {
 		c.EbpfCollectorConfig.Tracers = includeTracers.String()
 	}
 	if c.EnableSplitByService {
-		includeEnvVars := reporter.ServiceNameEnvVars
+		includeEnvVars := append([]string{}, serviceNameEnvVars...)
 		if c.EbpfCollectorConfig.IncludeEnvVars != "" {
 			includeEnvVars = append(includeEnvVars, c.EbpfCollectorConfig.IncludeEnvVars)
 		}
@@ -95,26 +96,15 @@ func defaultConfig() component.Config {
 	// With 60s intervals, 20% would mean ~12s variation, so we reduce to 5% (~3s).
 	cfg.ReporterJitter = 0.05
 
-	return Config{
-		EbpfCollectorConfig: cfg,
-		SymbolUploader: reporter.SymbolUploaderConfig{
-			SymbolUploaderOptions: reporter.SymbolUploaderOptions{
-				Enabled:              config.DefaultUploadSymbols,
-				UploadDynamicSymbols: config.DefaultUploadDynamicSymbols,
-				UploadGoPCLnTab:      config.DefaultUploadGoPCLnTab,
-				UseHTTP2:             config.DefaultUploadSymbolsHTTP2,
-				SymbolQueryInterval:  config.DefaultSymbolQueryInterval,
-				DryRun:               config.DefaultUploadSymbolsDryRun,
-				SymbolEndpoints:      nil,
-			},
-			Version:                        version.AgentVersion,
-			DisableDebugSectionCompression: false,
-		},
+	symbolUploaderConfig := symboluploader.DefaultSymbolUploaderConfig()
+	symbolUploaderConfig.Version = version.AgentVersion
+	symbolUploaderConfig.Name = profilerName
 
-		EnableSplitByService: config.DefaultSplitByService,
-		ReporterConfig: ReporterConfig{
-			CollectContext: config.DefaultCollectContext,
-		},
+	return Config{
+		EbpfCollectorConfig:  cfg,
+		SymbolUploader:       symbolUploaderConfig,
+		EnableSplitByService: true,
+		CollectContext:       false,
 	}
 }
 
