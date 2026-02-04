96731f7d889ae4a203f2c769caae77a73e43fd77
Asiya Shakhmametova <asiya.shakhmametova@datadoghq.com>
2026-02-04 12:59:34 +0100

    base servicemain implementation for par

--- cmd/privateactionrunner/main_windows.go
+++ cmd/privateactionrunner/main_windows.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
-//go:build !windows
+//go:build windows
 
 // Package main is the entrypoint for private-action-runner process
 package main
@@ -14,11 +14,23 @@ import (
 	"github.com/DataDog/datadog-agent/cmd/internal/runcmd"
 	"github.com/DataDog/datadog-agent/cmd/privateactionrunner/command"
 	"github.com/DataDog/datadog-agent/cmd/privateactionrunner/subcommands"
+	"github.com/DataDog/datadog-agent/cmd/privateactionrunner/windows/service"
 	"github.com/DataDog/datadog-agent/pkg/util/flavor"
+	"github.com/DataDog/datadog-agent/pkg/util/winutil/servicemain"
 )
 
 func main() {
 	flavor.SetFlavor(flavor.PrivateActionRunner)
+
+	// Call servicemain.Run EARLY if running as a Windows service with no CLI arguments.
+	// This is required because SCM only gives services 30 seconds to call StartServiceCtrlDispatcher.
+	// If called too late, you may encounter service start timeout errors from SCM.
+	if len(os.Args) == 1 && servicemain.RunningAsWindowsService() {
+		servicemain.Run(service.NewService())
+		return
+	}
+
 	rootCmd := command.MakeCommand(subcommands.PrivateActionRunnerSubcommands())
 	os.Exit(runcmd.Run(rootCmd))
 }
+
---
+++ cmd/privateactionrunner/windows/service/service.go
@@ -0,0 +1,99 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+//go:build windows
+
+// Package service implements the Windows Service for the private-action-runner
+package service
+
+import (
+	"context"
+	"errors"
+
+	"go.uber.org/fx"
+
+	"github.com/DataDog/datadog-agent/comp/core"
+	"github.com/DataDog/datadog-agent/comp/core/config"
+	ipcfx "github.com/DataDog/datadog-agent/comp/core/ipc/fx"
+	log "github.com/DataDog/datadog-agent/comp/core/log/def"
+	secretsnoopfx "github.com/DataDog/datadog-agent/comp/core/secrets/fx-noop"
+	"github.com/DataDog/datadog-agent/comp/core/settings"
+	"github.com/DataDog/datadog-agent/comp/core/settings/settingsimpl"
+	privateactionrunner "github.com/DataDog/datadog-agent/comp/privateactionrunner/def"
+	privateactionrunnerfx "github.com/DataDog/datadog-agent/comp/privateactionrunner/fx"
+	"github.com/DataDog/datadog-agent/comp/remote-config/rcclient"
+	"github.com/DataDog/datadog-agent/comp/remote-config/rcclient/rcclientimpl"
+	"github.com/DataDog/datadog-agent/comp/remote-config/rcservice/rcserviceimpl"
+	commonsettings "github.com/DataDog/datadog-agent/pkg/config/settings"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"github.com/DataDog/datadog-agent/pkg/util/winutil/servicemain"
+	"github.com/DataDog/datadog-agent/pkg/version"
+)
+
+const (
+	// ServiceName is the service name used for the private-action-runner
+	ServiceName = "datadog-private-action-runner"
+)
+
+type windowsService struct {
+	servicemain.DefaultSettings
+}
+
+// NewService returns the service entry for the private-action-runner
+func NewService() servicemain.Service {
+	return &windowsService{}
+}
+
+// Name returns the service name for event log records
+func (s *windowsService) Name() string {
+	return ServiceName
+}
+
+// Init implements application initialization, run when SERVICE_START_PENDING.
+// This blocks service tools like PowerShell's Start-Service until it returns.
+func (s *windowsService) Init() error {
+	// Nothing to do during init - configuration loading and fx app startup
+	// happens in Run().
+	return nil
+}
+
+// Run implements all application logic, run when SERVICE_RUNNING.
+// The provided context is cancellable - monitor ctx.Done() and return when set.
+// The service will exit when Run() returns.
+func (s *windowsService) Run(ctx context.Context) error {
+	err := fxutil.Run(
+		// ctx is required to be supplied from here, as Windows needs to inject its own context
+		// to allow the agent to work as a service.
+		fx.Provide(func() context.Context { return ctx }), // fx.Supply(ctx) fails with a missing type error.
+		fx.Supply(core.BundleParams{
+			ConfigParams: config.NewAgentParams(""),
+			LogParams:    log.ForDaemon("PRIV-ACTION", "private_action_runner.log_file", ""),
+		}),
+		core.Bundle(),
+		secretsnoopfx.Module(),
+		fx.Provide(func(c config.Component) settings.Params {
+			return settings.Params{
+				Settings: map[string]settings.RuntimeSetting{
+					"log_level": commonsettings.NewLogLevelRuntimeSetting(),
+				},
+				Config: c,
+			}
+		}),
+		settingsimpl.Module(),
+		ipcfx.ModuleReadWrite(),
+		rcserviceimpl.Module(),
+		rcclientimpl.Module(),
+		fx.Supply(rcclient.Params{AgentName: "private-action-runner", AgentVersion: version.AgentVersion}),
+		privateactionrunnerfx.Module(),
+	)
+
+	if errors.Is(err, privateactionrunner.ErrNotEnabled) {
+		// If private action runner is not enabled, exit cleanly
+		return servicemain.ErrCleanStopAfterInit
+	}
+
+	return err
+}
+
