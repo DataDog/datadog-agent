61dd67ff9d06450574685e915c55f07374e949d1
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-02-05 20:14:41 -0800

    fix package comment

--- pkg/system-probe/api/module/loader.go
+++ pkg/system-probe/api/module/loader.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package module is the scaffolding for a system-probe module and the loader used upon start
 package module
 
 import (
