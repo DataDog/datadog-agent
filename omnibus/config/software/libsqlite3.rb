fe0d0bb611875546933f49a36e0374ddfd7b6a7c
Tony Aiuto <tony.aiuto@datadoghq.com>
2026-02-09 12:30:46 -0500

    split out sql license fix

--- omnibus/config/software/libsqlite3.rb
+++ omnibus/config/software/libsqlite3.rb
@@ -3,9 +3,9 @@ default_version "3.50.4"
 
 relative_path "sqlite-autoconf-3500400"
 
-skip_transitive_dependency_licensing true
-
 build do
+  license "Public-Domain"
+
   command_on_repo_root "bazelisk run -- @sqlite3//:install --destdir='#{install_dir}/embedded'"
   sh_lib = if linux_target? then "libsqlite3.so" else "libsqlite3.dylib" end
   command_on_repo_root "bazelisk run -- //bazel/rules:replace_prefix --prefix '#{install_dir}/embedded' " \
