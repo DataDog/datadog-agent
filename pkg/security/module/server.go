22e10606b441b39d9fcf66ee52b23544531afe52
Sylvain Baubeau <sylvain.baubeau@datadoghq.com>
2026-02-09 18:16:49 +0100

    Add silent parameter on ReloadPolicies

--- pkg/security/module/server.go
+++ pkg/security/module/server.go
@@ -614,7 +614,7 @@ func (a *APIServer) ReloadPolicies(_ context.Context, _ *api.ReloadPoliciesParam
 		return nil, errors.New("no rule engine")
 	}
 
-	if err := a.cwsConsumer.ruleEngine.ReloadPolicies(); err != nil {
+	if err := a.cwsConsumer.ruleEngine.ReloadPolicies(true); err != nil {
 		return nil, err
 	}
 
