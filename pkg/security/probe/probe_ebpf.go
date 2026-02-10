d7ee515e5d48743c8df3507008a910428bbebaab
Sylvain Afchain <safchain@gmail.com>
2026-02-10 13:47:03 +0100

    [CWS] reduce cgroup write log level and allow process fallback (#46177)
    ### What does this PR do?
    
    ### Motivation
    
    ### Describe how you validated your changes
    
    ### Additional Notes
    
    
    Co-authored-by: sylvain.afchain <sylvain.afchain@datadoghq.com>

--- pkg/security/probe/probe_ebpf.go
+++ pkg/security/probe/probe_ebpf.go
@@ -1601,9 +1601,9 @@ func (p *EBPFProbe) handleRegularEvent(event *model.Event, offset int, dataLen u
 		}
 		pid := event.CgroupWrite.Pid
 
-		pce := p.Resolvers.ProcessResolver.Resolve(pid, pid, 0, false, newEntryCb)
+		pce := p.Resolvers.ProcessResolver.Resolve(pid, pid, 0, true, newEntryCb)
 		if pce == nil {
-			seclog.Errorf("failed to resolve process: %d", pid)
+			seclog.Debugf("failed to resolve process: %d", pid)
 			return false
 		}
 
