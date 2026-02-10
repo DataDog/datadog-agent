fa8305ea0164736acf22cc94841066fd928f9d16
Jeremy Hanna <jeremy.hanna@datadoghq.com>
2026-02-09 23:24:35 -0500

    Implement and use defaultpaths getters for config setup

--- pkg/config/setup/config_nix.go
+++ pkg/config/setup/config_nix.go
@@ -8,100 +8,52 @@
 package setup
 
 import (
-	"os"
 	"path/filepath"
 
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
-	"github.com/DataDog/datadog-agent/pkg/util/executable"
-	"github.com/DataDog/datadog-agent/pkg/util/log"
-)
-
-// Variables that are overridden at init
-var (
-	// InstallPath is the default install path for the agent
-	// It might be overridden at build time
-	InstallPath = "/opt/datadog-agent"
-
-	// defaultRunPath is the default run path
-	// It is set in osinit to take into account InstallPath overrides
-	defaultRunPath = ""
-)
-
-var (
-	// DefaultSystemProbeAddress is the default unix socket path to be used for connecting to the system probe
-	DefaultSystemProbeAddress = filepath.Join(InstallPath, "run/sysprobe.sock")
-	// DefaultDDAgentBin the process agent's binary
-	DefaultDDAgentBin = filepath.Join(InstallPath, "bin/agent/agent")
+	"github.com/DataDog/datadog-agent/pkg/util/defaultpaths"
 )
 
 const (
-	defaultConfdPath            = "/etc/datadog-agent/conf.d"
-	defaultAdditionalChecksPath = "/etc/datadog-agent/checks.d"
-	defaultGuiPort              = -1
+	// defaultGuiPort is the default GUI port (-1 means disabled on Linux)
+	defaultGuiPort = -1
+)
+
+// Exported default paths - sourced from defaultpaths package (the source of truth)
+// These are used by external packages that need default paths for logging setup.
+// For runtime path access, use defaultpaths getters
+var (
+	InstallPath = defaultpaths.DefaultInstallPath
 	// DefaultUpdaterLogFile is the default updater log file
-	DefaultUpdaterLogFile = "/var/log/datadog/updater.log"
+	DefaultUpdaterLogFile = defaultpaths.DefaultUpdaterLogFile
 	// DefaultSecurityAgentLogFile points to the log file that will be used by the security-agent if not configured
-	DefaultSecurityAgentLogFile = "/var/log/datadog/security-agent.log"
+	DefaultSecurityAgentLogFile = defaultpaths.DefaultSecurityAgentLogFile
 	// DefaultProcessAgentLogFile is the default process-agent log file
-	DefaultProcessAgentLogFile = "/var/log/datadog/process-agent.log"
+	DefaultProcessAgentLogFile = defaultpaths.DefaultProcessAgentLogFile
 	// DefaultOTelAgentLogFile is the default otel-agent log file
-	DefaultOTelAgentLogFile = "/var/log/datadog/otel-agent.log"
+	DefaultOTelAgentLogFile = defaultpaths.DefaultOTelAgentLogFile
 	// DefaultHostProfilerLogFile is the default host-profiler log file
-	DefaultHostProfilerLogFile = "/var/log/datadog/host-profiler.log"
+	DefaultHostProfilerLogFile = defaultpaths.DefaultHostProfilerLogFile
 	// DefaultPrivateActionRunnerLogFile is the default private-action-runner log file
-	DefaultPrivateActionRunnerLogFile = "/var/log/datadog/private-action-runner.log"
-	// defaultSystemProbeLogFilePath is the default system-probe log file
-	defaultSystemProbeLogFilePath = "/var/log/datadog/system-probe.log"
-	// defaultStatsdSocket is the default Unix Domain Socket path on which statsd will listen
-	defaultStatsdSocket = "/var/run/datadog/dsd.socket"
-	// defaultReceiverSocket is the default Unix Domain Socket path on which Trace agent will listen
-	defaultReceiverSocket = "/var/run/datadog/apm.socket"
-	//DefaultStreamlogsLogFile points to the stream logs log file that will be used if not configured
-	DefaultStreamlogsLogFile = "/var/log/datadog/streamlogs_info/streamlogs.log"
+	DefaultPrivateActionRunnerLogFile = defaultpaths.DefaultPrivateActionRunnerLogFile
+	// DefaultStreamlogsLogFile points to the stream logs log file that will be used if not configured
+	DefaultStreamlogsLogFile = defaultpaths.DefaultStreamlogsLogFile
+	// DefaultSystemProbeAddress is the default unix socket path to be used for connecting to the system probe
+	DefaultSystemProbeAddress = defaultpaths.DefaultSystemProbeAddress
+	// DefaultDDAgentBin the process agent's binary
+	DefaultDDAgentBin = filepath.Join(defaultpaths.DefaultBinPath, "agent")
 )
 
 // called by init in config.go, to ensure any os-specific config is done
 // in time
 func osinit() {
-	// Agent binary
-	_here, err := executable.Folder()
-	if err != nil {
-		log.Errorf("Failed to get executable path: %v", err)
-		return
-	}
-	InstallPath = getInstallPathFromExecutable(_here)
+	InstallPath = defaultpaths.GetInstallPath()
 
 	DefaultDDAgentBin = filepath.Join(InstallPath, "bin", "agent")
-	DefaultSystemProbeAddress = filepath.Join(InstallPath, "run/sysprobe.sock")
+	DefaultSystemProbeAddress = filepath.Join(InstallPath, "sysprobe.sock")
 	defaultSystemProbeBPFDir = filepath.Join(InstallPath, "embedded/share/system-probe/ebpf")
-
-	if defaultRunPath == "" {
-		defaultRunPath = filepath.Join(InstallPath, "run")
-	}
 }
 
 // FleetConfigOverride is a no-op on Linux
 func FleetConfigOverride(_ pkgconfigmodel.Config) {
-}
-
-// getInstallPathFromExecutable will go up the directory chain from start in search of a .install_root file.
-// That directory will become the install path.
-//
-// If not found, returns the default InstallPath.
-func getInstallPathFromExecutable(start string) string {
-	// Start from the current directory
-	currentDir := start
-
-	for {
-		installRoot := filepath.Join(currentDir, ".install_root")
-		if _, err := os.Stat(installRoot); err == nil {
-			return currentDir
-		}
-		parentDir := filepath.Dir(currentDir)
-		if parentDir == currentDir {
-			break
-		}
-		currentDir = parentDir
-	}
-	return InstallPath // Fallback to the default install path
 }
--- pkg/config/setup/config_test.go
+++ pkg/config/setup/config_test.go
@@ -21,6 +21,7 @@ import (
 	secretsmock "github.com/DataDog/datadog-agent/comp/core/secrets/mock"
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
 	"github.com/DataDog/datadog-agent/pkg/config/nodetreemodel"
+	"github.com/DataDog/datadog-agent/pkg/util/defaultpaths"
 	"github.com/DataDog/datadog-agent/pkg/util/scrubber"
 )
 
@@ -1087,7 +1088,7 @@ func TestLogDefaults(t *testing.T) {
 	testConfig := newTestConf(t)
 	require.Equal(t, 1, testConfig.GetInt("log_file_max_rolls"))
 	require.Equal(t, "10Mb", testConfig.GetString("log_file_max_size"))
-	require.Equal(t, "", testConfig.GetString("log_file"))
+	require.Equal(t, defaultpaths.GetDefaultLogFile(), testConfig.GetString("log_file"))
 	require.Equal(t, "info", testConfig.GetString("log_level"))
 	require.True(t, testConfig.GetBool("log_to_console"))
 	require.False(t, testConfig.GetBool("log_format_json"))
@@ -1099,7 +1100,7 @@ func TestLogDefaults(t *testing.T) {
 
 	require.Equal(t, 1, SystemProbe.GetInt("log_file_max_rolls"))
 	require.Equal(t, "10Mb", SystemProbe.GetString("log_file_max_size"))
-	require.Equal(t, defaultSystemProbeLogFilePath, SystemProbe.GetString("log_file"))
+	require.Equal(t, defaultpaths.GetDefaultSystemProbeLogFile(), SystemProbe.GetString("log_file"))
 	require.Equal(t, "info", SystemProbe.GetString("log_level"))
 	require.True(t, SystemProbe.GetBool("log_to_console"))
 	require.False(t, SystemProbe.GetBool("log_format_json"))
