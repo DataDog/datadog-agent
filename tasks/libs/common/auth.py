3ed1b842cd9d04ae62c489a16ee4369d3c896fd5
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-02-16 18:47:36 +0100

    Add validation script code

--- tasks/libs/common/auth.py
+++ tasks/libs/common/auth.py
@@ -1,3 +1,7 @@
+import os
+import shlex
+from contextlib import contextmanager
+
 from tasks.libs.common.utils import running_in_ci
 
 
@@ -35,3 +39,41 @@ def get_aws_vault_env(ctx, account: str):
             key, value = line.split("=", 1)
             env[key] = value
     return env
+
+
+
+@contextmanager
+def dd_auth_api_app_keys(ctx, domain: str):
+    """
+    Fetch API/App keys from dd-auth for a given domain and temporarily set them in env.
+    """
+    res = ctx.run(f"dd-auth --domain {shlex.quote(domain)} --output", hide=True, warn=True)
+    if not res.ok:
+        raise RuntimeError(f"dd-auth failed for domain {domain}")
+
+    output_vars = {}
+    for line in res.stdout.splitlines():
+        if "=" in line:
+            key, value = line.split("=", 1)
+            output_vars[key] = value
+
+    env_vars = ['DD_API_KEY', 'DD_APP_KEY']
+    new_values, prev_values = {}, {}
+    for env_var in env_vars:
+        if env_var not in output_vars:
+            raise RuntimeError(f"dd-auth output does not contain {env_var}")
+
+        new_values[env_var] = output_vars[env_var]
+        prev_values[env_var] = os.environ.get(env_var)
+
+    for env_var in env_vars:
+        os.environ[env_var] = new_values[env_var]
+
+    try:
+        yield
+    finally:
+        for env_var in env_vars:
+            if prev_values[env_var] is None:
+                os.environ.pop(env_var, None)
+            else:
+                os.environ[env_var] = prev_values[env_var]
