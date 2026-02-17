f2fdfa250f31c64fcddf9ad2d2d59aeceb1978c1
Yoann Ghigoff <yoann.ghigoff@datadoghq.com>
2026-02-17 13:21:06 +0100

    more mounts

--- test/new-e2e/system-probe/test-runner/testcontainer.go
+++ test/new-e2e/system-probe/test-runner/testcontainer.go
@@ -50,15 +50,17 @@ func (ctc *testContainer) start() error {
 
 	var mounts = []string{
 		// required
+		"/:/host/root",                        // hostroot
+		"/proc:/host/proc",                    // host procfs
+		"/sys:/host/sys",                      // host sysfs
+		"/etc:/host/etc",                      // host etc
 		"/sys/kernel/debug:/sys/kernel/debug", // bind mount debugfs
-		"/dev:/dev",
-		"/proc:/host/proc",
-		"/etc:/host/etc",
-		"/sys:/host/sys",
-		"/etc/os-release:/host/etc/os-release",
-		"/usr/lib/os-release:/host/usr/lib/os-release",
 		"/etc/passwd:/etc/passwd",
 		"/etc/group:/etc/group",
+		// already included in above mounts, can we remove these?
+		"/etc/os-release:/host/etc/os-release",
+		"/usr/lib/os-release:/host/usr/lib/os-release",
+		"/sys/fs/cgroup:/host/sys/fs/cgroup",
 		// tests only
 		"/opt/datadog-agent/embedded/:/opt/datadog-agent/embedded/",
 		"/opt/kmt-ramfs:/opt/kmt-ramfs",
@@ -69,9 +71,10 @@ func (ctc *testContainer) start() error {
 	}
 
 	var envs = []string{
+		"HOST_ROOT=/host/root",
 		"HOST_PROC=/host/proc",
-		"HOST_ETC=/host/etc",
 		"HOST_SYS=/host/sys",
+		"HOST_ETC=/host/etc",
 	}
 	for _, env := range envs {
 		args = append(args, "-e", env)
