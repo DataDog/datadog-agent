44011c75320f0ffd466cefd2fab389e0a36b8a5c
DanielLavie <daniel.lavie@datadoghq.com>
2026-02-17 11:08:41 +0200

    Add test file with intentionally wrong copyright year

---
+++ pkg/util/codextest/wrong_copyright_year.go
@@ -0,0 +1,11 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+package codextest
+
+// WrongCopyrightYearPRMarker is a no-op marker used for PR validation tests.
+func WrongCopyrightYearPRMarker() string {
+	return "codex-pr-test"
+}
