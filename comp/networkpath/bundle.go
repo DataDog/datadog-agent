ff76755c850d600c21c30753aae52890ad8bca74
Anthony Lam <anthony.lam@datadoghq.com>
2026-02-18 14:43:26 -0500

    rm cnm from team definitions

--- comp/networkpath/bundle.go
+++ comp/networkpath/bundle.go
@@ -12,7 +12,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/networkpath/npcollector/npcollectorimpl"
 )
 
-// team: cloud-network-monitoring network-path
+// team: network-path
 
 // Bundle defines the fx options for this bundle.
 func Bundle() fxutil.BundleOptions {
