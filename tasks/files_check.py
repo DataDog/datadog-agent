49002cfbf994c12bed01fdde15b499d95d618e3c
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2026-02-18 12:02:50 +0100

    more formating

--- tasks/files_check.py
+++ tasks/files_check.py
@@ -111,17 +111,17 @@ def _print_inventory_diff(added, removed, changed):
 def _inventory_changes_to_comment(added, removed, changed):
     body = "## Detected file changes:\n"
     if len(added):
-        body += f"<details><summary>{len(added)} Added files:</summary>\n\n"
+        body += f"<details><summary>\n\n### {len(added)} Added files:\n\n</summary>\n\n"
         for f in added:
             body += f"* `{f.relative_path}` ({byte_to_string(f.size_bytes)})\n"
         body += "</details>"
     if len(removed):
-        body += f"<details><summary>{len(removed)} Removed files:</summary>\n\n"
+        body += f"<details><summary>\n\n### {len(removed)} Removed files:\n\n</summary>\n\n"
         for f in removed:
             body += f"* `{f.relative_path}` ({byte_to_string(f.size_bytes)})\n"
         body += "</details>"
     if len(changed):
-        body += f"<details><summary>{len(changed)} Changed files:</summary>\n\n"
+        body += f"<details><summary>\n\n### {len(changed)} Changed files:\n\n</summary>\n\n"
         for path, change in changed.items():
             change_str = f"* `{path}`:\n"
             if change.flags & FileChange.Flags.Permissions:
