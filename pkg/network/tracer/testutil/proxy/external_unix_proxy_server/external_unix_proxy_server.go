29b3f42c9fc34431b4c6d74bd9abed9df625d5d7
Jack Phillips <jack.phillips@datadoghq.com>
2026-02-18 16:42:21 -0500

    lint

--- pkg/network/tracer/testutil/proxy/external_unix_proxy_server/external_unix_proxy_server.go
+++ pkg/network/tracer/testutil/proxy/external_unix_proxy_server/external_unix_proxy_server.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build unix
+//go:build !darwin && !windows
 
 // Package main provides a unix transparent proxy server that can be used for testing.
 package main
