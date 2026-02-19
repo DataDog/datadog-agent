c73a5ecee4b43d6350aef6aac762cb90726cea8f
maxime mouial <hush-hush@users.noreply.github.com>
2026-02-19 11:30:57 +0100

    Remove duplicate declaration of 'logs_config.integrations_logs_files_max_size` (#46627)
    ### What does this PR do?
    
    Remove duplicate declaration of 'logs_config.integrations_logs_files_max_size`
    
    ### Describe how you validated your changes
    
    Running the CI.
    
    Co-authored-by: maxime.mouial <maxime.mouial@datadoghq.com>

--- pkg/config/setup/config.go
+++ pkg/config/setup/config.go
@@ -2083,14 +2083,11 @@ func logsagent(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("logs_config.enable_recursive_glob", false)
 
 	// Max size in MB an integration logs file can use
-	config.BindEnvAndSetDefault("logs_config.integrations_logs_files_max_size", 10)
+	config.BindEnvAndSetDefault("logs_config.integrations_logs_files_max_size", 100)
 	// Max disk usage in MB all integrations logs files are allowed to use in total
 	config.BindEnvAndSetDefault("logs_config.integrations_logs_total_usage", 100)
 	// Do not store logs on disk when the disk usage exceeds 80% of the disk capacity.
 	config.BindEnvAndSetDefault("logs_config.integrations_logs_disk_ratio", 0.80)
-
-	// Max size in MB to allow for integrations logs files
-	config.BindEnvAndSetDefault("logs_config.integrations_logs_files_max_size", 100)
 
 	// Control how the stream-logs log file is managed
 	config.BindEnvAndSetDefault("logs_config.streaming.streamlogs_log_file", DefaultStreamlogsLogFile)
