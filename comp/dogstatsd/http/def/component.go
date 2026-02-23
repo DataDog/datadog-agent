0e164ab0413d1cc4d9c1f344035cdeba3e5d2ef4
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2026-02-23 16:52:45 +0000

    go fmt

--- comp/dogstatsd/http/def/component.go
+++ comp/dogstatsd/http/def/component.go
@@ -3,7 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-
 // Package http defines dogstatsd http server component
 package http
 
