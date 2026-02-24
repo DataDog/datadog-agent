63d7b720a416a1171957f5782f050d6f915c8c22
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-02-23 17:42:37 -0800

    remove linux_bpf build tag

--- pkg/network/sender/sender_linux_test.go
+++ pkg/network/sender/sender_linux_test.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
-//go:build linux && linux_bpf
+//go:build linux
 
 package sender
 
