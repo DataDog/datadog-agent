da3c10e11e151e102988e1ee854cf745ede7e25b
Gabriel Plassard <gabriel.plassard@datadoghq.com>
2026-02-24 17:36:13 +0100

    [ACTP] add par env parsing unit test

---
+++ pkg/config/setup/privateactionrunner_test.go
@@ -0,0 +1,35 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2026-present Datadog, Inc.
+
+package setup
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrivateActionRunnerActionsAllowlistFromEnv(t *testing.T) {
+	t.Setenv("DD_PRIVATE_ACTION_RUNNER_ACTIONS_ALLOWLIST", "com.datadoghq.kubernetes.core.listPod,com.datadoghq.script.runPredefinedScript")
+
+	cfg := newTestConf(t)
+
+	assert.Equal(t, []string{"com.datadoghq.kubernetes.core.listPod", "com.datadoghq.script.runPredefinedScript"}, cfg.GetStringSlice(PARActionsAllowlist))
+}
+
+func TestPrivateActionRunnerHttpAllowlistFromEnv(t *testing.T) {
+	t.Setenv("DD_PRIVATE_ACTION_RUNNER_HTTP_ALLOWLIST", "*.datadoghq.com,datadoghq.eu")
+
+	cfg := newTestConf(t)
+
+	assert.Equal(t, []string{"*.datadoghq.com", "*.datadoghq.eu"}, cfg.GetStringSlice(PARHttpAllowlist))
+}
+
+func TestPrivateActionRunnerAllowlistDefaultsEmpty(t *testing.T) {
+	cfg := newTestConf(t)
+
+	assert.Empty(t, cfg.GetStringSlice(PARActionsAllowlist))
+	assert.Empty(t, cfg.GetStringSlice(PARHttpAllowlist))
+}
