a76db8cc2663eb24e3376a175737de92078946c0
Baptiste Foy <baptiste.foy@datadoghq.com>
2026-02-25 09:53:21 +0100

    dump installer goroutines to tmp on sigint

--- cmd/installer/subcommands/daemon/run_nix.go
+++ cmd/installer/subcommands/daemon/run_nix.go
@@ -8,10 +8,15 @@
 package daemon
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"runtime/pprof"
 	"syscall"
+	"time"
 
 	"go.uber.org/fx"
 
@@ -40,6 +45,7 @@ func run(shutdowner fx.Shutdowner, cfg config.Component, _ pid.Component, _ loca
 		log.Infof("Datadog installer is not enabled, exiting")
 		return nil
 	}
+	log.Infof("Running version with debug goroutine stacks enabled")
 	releaseMemory()
 	handleSignals(shutdowner)
 	return nil
@@ -57,14 +63,52 @@ func gracefullyExitIfDisabled(cfg config.Component, shutdowner fx.Shutdowner) er
 }
 
 func handleSignals(shutdowner fx.Shutdowner) {
+	// SIGUSR1: dump goroutine stacks to stderr (for debugging deadlocks without shutting down)
+	sigusr1Ch := make(chan os.Signal, 1)
+	signal.Notify(sigusr1Ch, syscall.SIGUSR1)
+	go func() {
+		for range sigusr1Ch {
+			DumpGoroutineStacks()
+		}
+	}()
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGPIPE)
 	for signo := range sigChan {
 		switch signo {
 		case syscall.SIGINT, syscall.SIGTERM:
-			log.Infof("Received signal %d (%v)", signo, signo)
+			log.Infof("Received signal %d (%v), dumping goroutine stacks and shutting down...", signo, signo)
+			DumpGoroutineStacks()
 			_ = shutdowner.Shutdown()
 			return
 		}
 	}
+}
+
+// DumpGoroutineStacks writes all goroutine stacks to /tmp and to stderr. Used
+// when handling SIGTERM/SIGINT (before shutdown) and SIGUSR1 (on-demand) to
+// debug deadlocks. Syncs to disk so the dump is visible even if the process
+// is killed immediately after.
+func DumpGoroutineStacks() {
+	var buf bytes.Buffer
+	if err := pprof.Lookup("goroutine").WriteTo(&buf, 2); err != nil {
+		_, _ = os.Stderr.WriteString("failed to write goroutine profile: " + err.Error() + "\n")
+		return
+	}
+	dump := fmt.Sprintf("\n===== goroutine stack dump %s =====\n%s===== end goroutine stack dump =====\n\n",
+		time.Now().Format(time.RFC3339), buf.String())
+
+	name := fmt.Sprintf("datadog-installer-daemon-goroutine-stacks-%s.txt", time.Now().Format("20060102-150405"))
+	path := filepath.Join("/tmp", name)
+	f, err := os.Create(path)
+	if err != nil {
+		_, _ = os.Stderr.WriteString("failed to write goroutine dump to " + path + ": " + err.Error() + "\n")
+		_, _ = os.Stderr.WriteString(dump)
+		return
+	}
+	_, _ = f.WriteString(dump)
+	_ = f.Sync()
+	_ = f.Close()
+	_, _ = os.Stderr.WriteString("goroutine stack dump written to " + path + "\n")
+	_, _ = os.Stderr.WriteString(dump)
 }
