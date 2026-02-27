c7659c379a9f44f8cfe72cdfb683167392463898
Kyle Ames <kyle.ames@datadoghq.com>
2026-02-27 12:27:42 -0500

    [RC] Add tests to ensure embedded director TUF roots stay at version 1
    Codifies in a unit test a requirement that we have for the embedded open
    source director repo root that we have run into twice now and had to
    revert.
    
    Also adds some missing sanity checks that the embedded metadata can be
    parsed.

--- pkg/config/remote/meta/meta_test.go
+++ pkg/config/remote/meta/meta_test.go
@@ -34,3 +34,42 @@ func TestParseStagingTUFVersion(t *testing.T) {
 	require.NoError(t, err)
 	require.Greater(t, v, uint64(0))
 }
+
+func TestParseGovDirectorVersion(t *testing.T) {
+	v, err := parseRootVersion(govRootDirector)
+	require.NoError(t, err)
+	require.Greater(t, v, uint64(0))
+}
+
+func TestParseGovTUFVersion(t *testing.T) {
+	v, err := parseRootVersion(govRootConfig)
+	require.NoError(t, err)
+	require.Greater(t, v, uint64(0))
+}
+
+// The director root metadata must always be version 1. As part of the
+// RC protocol, the RC core services sends the Director TUF root to agent
+// and tracer clients. It will try to send to those clients all root files
+// starting from the version the client reports it has, through the latest
+// the core service has gotten from the RC backend.
+//
+// If we embed a director root greater than 1, the RC backend does not go back
+// and fill in versions. Some clients, when they start up, always unconditionally
+// report they have version 1. In those circumstances, we'll be unable to find the roots
+// earlier than the version we embed, and the request will error out.
+func TestDirectorRootMetadataIsVersion1(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		root []byte
+	}{
+		{"prod", prodRootDirector},
+		{"staging", stagingRootDirector},
+		{"gov", govRootDirector},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			v, err := parseRootVersion(tc.root)
+			require.NoError(t, err)
+			require.Equal(t, uint64(1), v, "director root metadata for %q must be version 1", tc.name)
+		})
+	}
+}
