b53b0031672737db4992b950669f00940ad73898
Célian Raimbault <161456554+CelianR@users.noreply.github.com>
2026-03-03 07:16:01 -0500

    Added tasks related to agent k8s deployment (#47227)
    ### What does this PR do?
    
    Adds some invoke tasks to handle agent k8s deployment for agent Q
    (mainly based on Kind and lima).
    
    ### Motivation
    
    ### Describe how you validated your changes
    
    ### Additional Notes

--- tasks/q.py
+++ tasks/q.py
@@ -1,4 +1,8 @@
+import os
+
 from invoke import task
+
+from tasks.libs.common.color import Color, color_message
 
 
 # --- Build ---
@@ -27,3 +31,76 @@ def launch_testbench(ctx, scenarios_dir: str = "./comp/observer/anomaly_datasets
     ctx.run(
         f"bin/observer-testbench --scenarios-dir {scenarios_dir} & ( cd cmd/observer-testbench/ui && npm install && npm run dev ) &"
     )
+
+
+# --- K8s ---
+@task
+def deploy_k8s_agent(ctx, cluster_name: str = ""):
+    """
+    Deploys the Datadog Agent to the Kind cluster on lima VM.
+
+    See tasks/q/datadog-values.template.yaml for the values file used to deploy the agent.
+    """
+
+    if not cluster_name:
+        cluster_name = os.getenv("USER", 'user') + '-observer-cluster'
+
+    ctx.run(
+        f"sed -e 's/$$CLUSTER_NAME/{cluster_name}/g' tasks/q/datadog-values.template.yaml > /tmp/datadog-values.yaml"
+    )
+
+    # Try to install, and if already exists, upgrade instead
+    try:
+        ctx.run('helm install datadog-agent -f /tmp/datadog-values.yaml datadog/datadog')
+    except Exception:
+        print("Restarting Datadog Agent...")
+        uninstall_k8s_agent(ctx)
+        ctx.run('helm install datadog-agent -f /tmp/datadog-values.yaml datadog/datadog')
+
+
+@task
+def uninstall_k8s_agent(ctx):
+    ctx.run('helm uninstall datadog-agent')
+
+
+@task
+def build_k8s_image(ctx, devenv_id: str = ""):
+    """
+    Builds the datadog agent image and loads it into the Kind cluster on lima VM.
+    """
+
+    if devenv_id:
+        devenv_id = f"--id {devenv_id}"
+
+    print(color_message('Building observer-agent image...', Color.BLUE))
+    ctx.run(
+        f"dda env dev run {devenv_id} -- dda inv -- -e agent.hacky-dev-image-build --trace-agent --target-image observer-agent"
+    )
+    print(color_message('Saving image to /tmp/observer-agent_latest.tar...', Color.BLUE))
+    ctx.run("docker image save observer-agent:latest -o /tmp/observer-agent_latest.tar")
+    print(color_message('Copying image to VM...', Color.BLUE))
+    ctx.run("limactl copy /tmp/observer-agent_latest.tar gadget-k8s-host:/home/lima.linux/observer-agent_latest.tar")
+    print(color_message('Loading image into VM...', Color.BLUE))
+    ctx.run("limactl shell --workdir '/home/lima.linux' gadget-k8s-host -- docker load -i observer-agent_latest.tar")
+    print(color_message('Loading image into Kind...', Color.BLUE))
+    ctx.run(
+        "limactl shell --workdir '/home/lima.linux' gadget-k8s-host -- kind load docker-image observer-agent:latest --name gadget-dev"
+    )
+    print(color_message('Done!', Color.GREEN))
+
+
+@task
+def fetch_k8s_observer_parquet(ctx, dest: str = "/tmp/k8s-observer-metrics"):
+    """
+    Fetches the observer parquet files (logs / metrics) from the Datadog Agent pod and saves them to the specified destination.
+    """
+
+    datadog_agent_pod = ctx.run(
+        "kubectl get pod | grep -oE 'datadog-agent-[a-z0-9A-Z]+ '", warn=True, hide=True
+    ).stdout.strip()
+    if not datadog_agent_pod:
+        raise RuntimeError("Datadog Agent pod not found")
+
+    ctx.run(f"kubectl cp {datadog_agent_pod}:/tmp/observer-metrics {dest}")
+
+    print(color_message(f"Fetched observer parquet files to {dest}", Color.GREEN))
