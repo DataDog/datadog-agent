719ed0032799288582148523cb0001db338d8e92
David Ortiz <david.ortiz@datadoghq.com>
2026-03-04 11:45:53 +0100

    [cluster-agent/api] Add namespaces to kube metadata stream

--- cmd/cluster-agent/api/v1/kubernetes_metadata_stream.go
+++ cmd/cluster-agent/api/v1/kubernetes_metadata_stream.go
@@ -8,10 +8,14 @@
 package v1
 
 import (
+	"context"
+	"maps"
+	"sync"
 	"time"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 
+	workloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/def"
 	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/core"
 	"github.com/DataDog/datadog-agent/pkg/util/grpc"
 	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/apiserver"
@@ -23,7 +27,8 @@ const (
 	streamSendTimeout = 10 * time.Second
 	// keepAliveInterval must be shorter than the client's timeout (10 min) to
 	// prevent the client from treating an idle stream as dead.
-	keepAliveInterval = 9 * time.Minute
+	keepAliveInterval   = 9 * time.Minute
+	wmetaSubscriberName = "kube-metadata-stream"
 )
 
 type podServiceEntry struct {
@@ -32,30 +37,75 @@ type podServiceEntry struct {
 	services  sets.Set[string]
 }
 
-// KubeMetadataStreamServer streams pod-to-service metadata from the DCA to node agents.
+type namespaceEntry struct {
+	labels      map[string]string
+	annotations map[string]string
+}
+
+// KubeMetadataStreamServer streams pod-to-service mappings and namespace
+// labels/annotations from the DCA to node agents.
 type KubeMetadataStreamServer struct {
 	store *controllers.MetaBundleStore
+	wmeta workloadmeta.Component
+
+	namespacesMutex      sync.RWMutex
+	namespaces           map[string]namespaceEntry // keys are namespace names
+	namespaceSubscribers map[string]chan struct{}  // keys are node names
 }
 
-// NewKubeMetadataStreamServer creates a new stream server with the given meta
-// bundle store.
-func NewKubeMetadataStreamServer(store *controllers.MetaBundleStore) *KubeMetadataStreamServer {
-	return &KubeMetadataStreamServer{store: store}
+// NewKubeMetadataStreamServer creates a new KubeMetadataStreamServer
+func NewKubeMetadataStreamServer(store *controllers.MetaBundleStore, wmeta workloadmeta.Component) *KubeMetadataStreamServer {
+	return &KubeMetadataStreamServer{
+		store:                store,
+		wmeta:                wmeta,
+		namespaces:           make(map[string]namespaceEntry),
+		namespaceSubscribers: make(map[string]chan struct{}),
+	}
 }
 
-// StreamKubeMetadata streams pod-to-service mappings to the requesting node
-// agent.
-func (s *KubeMetadataStreamServer) StreamKubeMetadata(req *pb.KubeMetadataStreamRequest, srv pb.AgentSecure_StreamKubeMetadataServer) error {
+// Start subscribes to workloadmeta for namespace metadata changes and
+// maintains the namespace state. It must be called before serving streams.
+func (srv *KubeMetadataStreamServer) Start(ctx context.Context) {
+	ch := srv.wmeta.Subscribe(
+		wmetaSubscriberName,
+		workloadmeta.NormalPriority,
+		namespaceMetadataFilter(),
+	)
+
+	go func() {
+		defer srv.wmeta.Unsubscribe(ch)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case bundle, ok := <-ch:
+				if !ok {
+					return
+				}
+				bundle.Acknowledge()
+				srv.processNamespaceEvents(bundle.Events)
+			}
+		}
+	}()
+}
+
+// StreamKubeMetadata streams pod-to-service mappings and namespace metadata to
+// the requesting node agent.
+func (srv *KubeMetadataStreamServer) StreamKubeMetadata(req *pb.KubeMetadataStreamRequest, stream pb.AgentSecure_StreamKubeMetadataServer) error {
 	nodeName := req.GetNodeName()
 
-	notifyCh := s.store.Subscribe(nodeName)
-	defer s.store.Unsubscribe(nodeName)
+	podServicesNotifyCh := srv.store.Subscribe(nodeName)
+	defer srv.store.Unsubscribe(nodeName)
+
+	namespacesNotifyCh := srv.subscribeToNamespaceEvents(nodeName)
+	defer srv.unsubscribeFromNamespaceEvents(nodeName)
 
 	// Send initial full state
-	lastSentState := s.buildSnapshot(nodeName)
-	initialResp := fullStateResponse(lastSentState)
+	lastSentPodServicesState := srv.buildPodServiceMappingsSnapshot(nodeName)
+	lastSentNamespacesState := srv.buildNamespacesSnapshot()
+	initialResp := fullStateResponse(lastSentPodServicesState, lastSentNamespacesState)
 	if err := grpc.DoWithTimeout(func() error {
-		return srv.Send(initialResp)
+		return stream.Send(initialResp)
 	}, streamSendTimeout); err != nil {
 		log.Warnf("Error sending initial kube metadata state for node %s: %s", nodeName, err)
 		return err
@@ -64,35 +114,54 @@ func (s *KubeMetadataStreamServer) StreamKubeMetadata(req *pb.KubeMetadataStream
 	ticker := time.NewTicker(keepAliveInterval)
 	defer ticker.Stop()
 
-	ctx := srv.Context()
+	ctx := stream.Context()
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
 
-		case <-notifyCh:
-			currentState := s.buildSnapshot(nodeName)
-			diff := computeDiff(lastSentState, currentState)
-			if len(diff) == 0 {
+		case <-podServicesNotifyCh:
+			currentPodServiceMappingsState := srv.buildPodServiceMappingsSnapshot(nodeName)
+			podServiceMappingsDiff := computePodServiceMappingsDiff(lastSentPodServicesState, currentPodServiceMappingsState)
+			if len(podServiceMappingsDiff) == 0 {
 				continue
 			}
 			resp := &pb.KubeMetadataStreamResponse{
 				IsFullState: false,
-				Mappings:    diff,
+				Mappings:    podServiceMappingsDiff,
 			}
 			if err := grpc.DoWithTimeout(func() error {
-				return srv.Send(resp)
+				return stream.Send(resp)
 			}, streamSendTimeout); err != nil {
-				log.Warnf("Error sending kube metadata diff for node %s: %s", nodeName, err)
+				log.Warnf("Error sending pod-service metadata diff for node %s: %s", nodeName, err)
 				return err
 			}
-			lastSentState = currentState
+			lastSentPodServicesState = currentPodServiceMappingsState
+			ticker.Reset(keepAliveInterval)
+
+		case <-namespacesNotifyCh:
+			currentNamespacesState := srv.buildNamespacesSnapshot()
+			namespacesDiff := computeNamespacesDiff(lastSentNamespacesState, currentNamespacesState)
+			if len(namespacesDiff) == 0 {
+				continue
+			}
+			resp := &pb.KubeMetadataStreamResponse{
+				IsFullState:       false,
+				NamespaceMetadata: namespacesDiff,
+			}
+			if err := grpc.DoWithTimeout(func() error {
+				return stream.Send(resp)
+			}, streamSendTimeout); err != nil {
+				log.Warnf("Error sending namespace metadata diff for node %s: %s", nodeName, err)
+				return err
+			}
+			lastSentNamespacesState = currentNamespacesState
 			ticker.Reset(keepAliveInterval)
 
 		case <-ticker.C:
 			// Send empty keepalive
 			if err := grpc.DoWithTimeout(func() error {
-				return srv.Send(&pb.KubeMetadataStreamResponse{})
+				return stream.Send(&pb.KubeMetadataStreamResponse{})
 			}, streamSendTimeout); err != nil {
 				log.Warnf("Error sending kube metadata keepalive for node %s: %s", nodeName, err)
 				return err
@@ -101,17 +170,97 @@ func (s *KubeMetadataStreamServer) StreamKubeMetadata(req *pb.KubeMetadataStream
 	}
 }
 
-// buildSnapshot reads the current bundle for a node and converts it to a
+func (srv *KubeMetadataStreamServer) processNamespaceEvents(events []workloadmeta.Event) {
+	srv.namespacesMutex.Lock()
+	defer srv.namespacesMutex.Unlock()
+
+	changed := false
+	for _, event := range events {
+		metadata := event.Entity.(*workloadmeta.KubernetesMetadata)
+		namespaceName := metadata.Name
+
+		switch event.Type {
+		case workloadmeta.EventTypeSet:
+			srv.namespaces[namespaceName] = namespaceEntry{
+				labels:      metadata.Labels,
+				annotations: metadata.Annotations,
+			}
+			changed = true
+		case workloadmeta.EventTypeUnset:
+			if _, exists := srv.namespaces[namespaceName]; exists {
+				delete(srv.namespaces, namespaceName)
+				changed = true
+			}
+		default:
+			log.Errorf("Unknown event type %d for namespace %s", event.Type, namespaceName)
+		}
+	}
+
+	if changed {
+		srv.notifyNamespaceSubscribers()
+	}
+}
+
+func (srv *KubeMetadataStreamServer) notifyNamespaceSubscribers() {
+	for _, ch := range srv.namespaceSubscribers {
+		select {
+		case ch <- struct{}{}:
+		default:
+		}
+	}
+}
+
+func (srv *KubeMetadataStreamServer) subscribeToNamespaceEvents(nodeName string) <-chan struct{} {
+	srv.namespacesMutex.Lock()
+	defer srv.namespacesMutex.Unlock()
+
+	ch := make(chan struct{}, 1)
+	srv.namespaceSubscribers[nodeName] = ch
+	return ch
+}
+
+func (srv *KubeMetadataStreamServer) unsubscribeFromNamespaceEvents(nodeName string) {
+	srv.namespacesMutex.Lock()
+	defer srv.namespacesMutex.Unlock()
+
+	delete(srv.namespaceSubscribers, nodeName)
+}
+
+func (srv *KubeMetadataStreamServer) buildNamespacesSnapshot() map[string]namespaceEntry {
+	srv.namespacesMutex.RLock()
+	defer srv.namespacesMutex.RUnlock()
+
+	snapshot := make(map[string]namespaceEntry, len(srv.namespaces))
+	for ns, entry := range srv.namespaces {
+		snapshot[ns] = namespaceEntry{
+			labels:      maps.Clone(entry.labels),
+			annotations: maps.Clone(entry.annotations),
+		}
+	}
+	return snapshot
+}
+
+// buildPodServiceMappingsSnapshot reads the current bundle for a node and converts it to a
 // snapshot map keyed by "namespace/podName".
-func (s *KubeMetadataStreamServer) buildSnapshot(nodeName string) map[string]podServiceEntry {
-	bundle, ok := s.store.Get(nodeName)
+func (srv *KubeMetadataStreamServer) buildPodServiceMappingsSnapshot(nodeName string) map[string]podServiceEntry {
+	bundle, ok := srv.store.Get(nodeName)
 	if !ok {
 		return nil
 	}
-	return bundleToSnapshot(bundle)
+	return bundleToPodServiceMappingsSnapshot(bundle)
 }
 
-func bundleToSnapshot(bundle *apiserver.MetadataMapperBundle) map[string]podServiceEntry {
+func namespaceMetadataFilter() *workloadmeta.Filter {
+	return workloadmeta.NewFilterBuilder().AddKindWithEntityFilter(
+		workloadmeta.KindKubernetesMetadata,
+		func(entity workloadmeta.Entity) bool {
+			metadata := entity.(*workloadmeta.KubernetesMetadata)
+			return workloadmeta.IsNamespaceMetadata(metadata)
+		},
+	).Build()
+}
+
+func bundleToPodServiceMappingsSnapshot(bundle *apiserver.MetadataMapperBundle) map[string]podServiceEntry {
 	snapshot := make(map[string]podServiceEntry)
 	for ns, pods := range bundle.Services {
 		for podName, svcs := range pods {
@@ -127,10 +276,11 @@ func bundleToSnapshot(bundle *apiserver.MetadataMapperBundle) map[string]podServ
 }
 
 // fullStateResponse creates a KubeMetadataStreamResponse with
-// is_full_state=true containing all current mappings.
-func fullStateResponse(snapshot map[string]podServiceEntry) *pb.KubeMetadataStreamResponse {
-	mappings := make([]*pb.PodServiceMapping, 0, len(snapshot))
-	for _, entry := range snapshot {
+// is_full_state=true containing all current mappings and namespace labels and
+// annotations.
+func fullStateResponse(podServices map[string]podServiceEntry, namespaces map[string]namespaceEntry) *pb.KubeMetadataStreamResponse {
+	mappings := make([]*pb.PodServiceMapping, 0, len(podServices))
+	for _, entry := range podServices {
 		mappings = append(mappings, &pb.PodServiceMapping{
 			Namespace:    entry.namespace,
 			PodName:      entry.podName,
@@ -139,14 +289,25 @@ func fullStateResponse(snapshot map[string]podServiceEntry) *pb.KubeMetadataStre
 		})
 	}
 
+	namespacesMetadata := make([]*pb.NamespaceMetadata, 0, len(namespaces))
+	for namespace, entry := range namespaces {
+		namespacesMetadata = append(namespacesMetadata, &pb.NamespaceMetadata{
+			Namespace:   namespace,
+			Labels:      entry.labels,
+			Annotations: entry.annotations,
+			Type:        pb.KubeMetadataEventType_SET,
+		})
+	}
+
 	return &pb.KubeMetadataStreamResponse{
-		IsFullState: true,
-		Mappings:    mappings,
+		IsFullState:       true,
+		Mappings:          mappings,
+		NamespaceMetadata: namespacesMetadata,
 	}
 }
 
-// computeDiff compares old and new snapshots and returns set/unset events.
-func computeDiff(old, current map[string]podServiceEntry) []*pb.PodServiceMapping {
+// computePodServiceMappingsDiff compares old and new snapshots and returns set/unset events.
+func computePodServiceMappingsDiff(old, current map[string]podServiceEntry) []*pb.PodServiceMapping {
 	var diff []*pb.PodServiceMapping
 
 	// Add events for new or changed entries
@@ -168,6 +329,34 @@ func computeDiff(old, current map[string]podServiceEntry) []*pb.PodServiceMappin
 			diff = append(diff, &pb.PodServiceMapping{
 				Namespace: prev.namespace,
 				PodName:   prev.podName,
+				Type:      pb.KubeMetadataEventType_UNSET,
+			})
+		}
+	}
+
+	return diff
+}
+
+// computeNamespacesDiff compares old and new namespace snapshots and returns set/unset events.
+func computeNamespacesDiff(old, current map[string]namespaceEntry) []*pb.NamespaceMetadata {
+	var diff []*pb.NamespaceMetadata
+
+	for ns, cur := range current {
+		prev, existed := old[ns]
+		if !existed || !maps.Equal(prev.labels, cur.labels) || !maps.Equal(prev.annotations, cur.annotations) {
+			diff = append(diff, &pb.NamespaceMetadata{
+				Namespace:   ns,
+				Labels:      cur.labels,
+				Annotations: cur.annotations,
+				Type:        pb.KubeMetadataEventType_SET,
+			})
+		}
+	}
+
+	for ns := range old {
+		if _, exists := current[ns]; !exists {
+			diff = append(diff, &pb.NamespaceMetadata{
+				Namespace: ns,
 				Type:      pb.KubeMetadataEventType_UNSET,
 			})
 		}
--- cmd/cluster-agent/api/v1/kubernetes_metadata_stream_test.go
+++ cmd/cluster-agent/api/v1/kubernetes_metadata_stream_test.go
@@ -8,21 +8,96 @@
 package v1
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/fx"
+	"google.golang.org/protobuf/proto"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/sets"
 
+	"github.com/DataDog/datadog-agent/comp/core"
+	"github.com/DataDog/datadog-agent/comp/core/workloadmeta/collectors/util"
+	workloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/def"
+	workloadmetafxmock "github.com/DataDog/datadog-agent/comp/core/workloadmeta/fx-mock"
+	workloadmetamock "github.com/DataDog/datadog-agent/comp/core/workloadmeta/mock"
 	pb "github.com/DataDog/datadog-agent/pkg/proto/pbgo/core"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/apiserver"
 )
 
-func TestBundleToSnapshot(t *testing.T) {
+func TestStart(t *testing.T) {
+	wmetaMock := fxutil.Test[workloadmetamock.Mock](t, fx.Options(
+		core.MockBundle(),
+		workloadmetafxmock.MockModule(workloadmeta.NewParams()),
+	))
+
+	srv := NewKubeMetadataStreamServer(nil, wmetaMock)
+
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
+
+	ch := srv.subscribeToNamespaceEvents("node1")
+	srv.Start(ctx)
+
+	namespace := "ns1"
+	namespaceKubeMetadataID := string(util.GenerateKubeMetadataEntityID("", "namespaces", "", namespace))
+
+	wmetaMock.Set(&workloadmeta.KubernetesMetadata{
+		EntityID: workloadmeta.EntityID{
+			Kind: workloadmeta.KindKubernetesMetadata,
+			ID:   namespaceKubeMetadataID,
+		},
+		EntityMeta: workloadmeta.EntityMeta{
+			Name:        namespace,
+			Labels:      map[string]string{"l1": "v1"},
+			Annotations: map[string]string{"a1": "v2"},
+		},
+		GVR: &schema.GroupVersionResource{Resource: "namespaces"},
+	})
+
+	select {
+	case <-ch:
+	case <-time.After(10 * time.Second):
+		t.Fatal("timed out waiting for set notification")
+	}
+
+	snapshot := srv.buildNamespacesSnapshot()
+	assert.Equal(t, map[string]namespaceEntry{
+		namespace: {
+			labels:      map[string]string{"l1": "v1"},
+			annotations: map[string]string{"a1": "v2"},
+		},
+	}, snapshot)
+
+	wmetaMock.Unset(&workloadmeta.KubernetesMetadata{
+		EntityID: workloadmeta.EntityID{
+			Kind: workloadmeta.KindKubernetesMetadata,
+			ID:   namespaceKubeMetadataID,
+		},
+		EntityMeta: workloadmeta.EntityMeta{
+			Name: namespace,
+		},
+		GVR: &schema.GroupVersionResource{Resource: "namespaces"},
+	})
+
+	select {
+	case <-ch:
+	case <-time.After(10 * time.Second):
+		t.Fatal("timed out waiting for unset notification")
+	}
+
+	assert.Empty(t, srv.buildNamespacesSnapshot())
+}
+
+func TestBundleToPodServiceMappingsSnapshot(t *testing.T) {
 	bundle := apiserver.NewMetadataMapperBundle()
 	bundle.Services.Set("ns1", "pod1", "svc1", "svc2")
 	bundle.Services.Set("ns2", "pod2", "svc3", "svc4")
 
-	snapshot := bundleToSnapshot(bundle)
+	snapshot := bundleToPodServiceMappingsSnapshot(bundle)
 
 	assert.Len(t, snapshot, 2)
 
@@ -43,7 +118,7 @@ func TestBundleToSnapshot(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestComputeDiff(t *testing.T) {
+func TestComputePodServiceMappingsDiff(t *testing.T) {
 	tests := []struct {
 		name     string
 		old      map[string]podServiceEntry
@@ -202,8 +277,163 @@ func TestComputeDiff(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			diff := computeDiff(test.old, test.current)
+			diff := computePodServiceMappingsDiff(test.old, test.current)
 			assert.ElementsMatch(t, test.expected, diff)
 		})
 	}
+}
+
+func TestComputeNamespacesDiff(t *testing.T) {
+	tests := []struct {
+		name     string
+		old      map[string]namespaceEntry
+		current  map[string]namespaceEntry
+		expected []*pb.NamespaceMetadata
+	}{
+		{
+			name: "no changes",
+			old: map[string]namespaceEntry{
+				"ns1": {labels: map[string]string{"l1": "v1"}},
+			},
+			current: map[string]namespaceEntry{
+				"ns1": {labels: map[string]string{"l1": "v1"}},
+			},
+			expected: nil,
+		},
+		{
+			name: "new namespace",
+			old:  map[string]namespaceEntry{},
+			current: map[string]namespaceEntry{
+				"ns1": {
+					labels:      map[string]string{"l1": "v1"},
+					annotations: map[string]string{"a1": "v2"},
+				},
+			},
+			expected: []*pb.NamespaceMetadata{
+				{
+					Namespace:   "ns1",
+					Labels:      map[string]string{"l1": "v1"},
+					Annotations: map[string]string{"a1": "v2"},
+					Type:        pb.KubeMetadataEventType_SET,
+				},
+			},
+		},
+		{
+			name: "removed namespace",
+			old: map[string]namespaceEntry{
+				"ns1": {labels: map[string]string{"l1": "v1"}},
+			},
+			current: map[string]namespaceEntry{},
+			expected: []*pb.NamespaceMetadata{
+				{
+					Namespace: "ns1",
+					Type:      pb.KubeMetadataEventType_UNSET,
+				},
+			},
+		},
+		{
+			name: "labels changed",
+			old: map[string]namespaceEntry{
+				"ns1": {labels: map[string]string{"l1": "v1"}},
+			},
+			current: map[string]namespaceEntry{
+				"ns1": {labels: map[string]string{"l1": "v2"}},
+			},
+			expected: []*pb.NamespaceMetadata{
+				{
+					Namespace: "ns1",
+					Labels:    map[string]string{"l1": "v2"},
+					Type:      pb.KubeMetadataEventType_SET,
+				},
+			},
+		},
+		{
+			name: "current is nil unsets all",
+			old: map[string]namespaceEntry{
+				"ns1": {labels: map[string]string{"l1": "v1"}},
+				"ns2": {labels: map[string]string{"l1": "v2"}},
+			},
+			current: nil,
+			expected: []*pb.NamespaceMetadata{
+				{
+					Namespace: "ns1",
+					Type:      pb.KubeMetadataEventType_UNSET,
+				},
+				{
+					Namespace: "ns2",
+					Type:      pb.KubeMetadataEventType_UNSET,
+				},
+			},
+		},
+		{
+			name: "annotations changed",
+			old: map[string]namespaceEntry{
+				"ns1": {
+					labels:      map[string]string{"l1": "v1"},
+					annotations: map[string]string{"a1": "v2"},
+				},
+			},
+			current: map[string]namespaceEntry{
+				"ns1": {
+					labels:      map[string]string{"l1": "v1"},
+					annotations: map[string]string{"a1": "v3"},
+				},
+			},
+			expected: []*pb.NamespaceMetadata{
+				{
+					Namespace:   "ns1",
+					Labels:      map[string]string{"l1": "v1"},
+					Annotations: map[string]string{"a1": "v3"},
+					Type:        pb.KubeMetadataEventType_SET,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			diff := computeNamespacesDiff(test.old, test.current)
+			assert.ElementsMatch(t, test.expected, diff)
+		})
+	}
+}
+
+func TestFullStateResponse(t *testing.T) {
+	pods := map[string]podServiceEntry{
+		"ns1/pod1": {
+			namespace: "ns1",
+			podName:   "pod1",
+			services:  sets.New("svc1"),
+		},
+	}
+	namespaces := map[string]namespaceEntry{
+		"ns1": {
+			labels:      map[string]string{"l1": "v1"},
+			annotations: map[string]string{"a1": "v2"},
+		},
+	}
+
+	resp := fullStateResponse(pods, namespaces)
+
+	expected := &pb.KubeMetadataStreamResponse{
+		IsFullState: true,
+		Mappings: []*pb.PodServiceMapping{
+			{
+				Namespace:    "ns1",
+				PodName:      "pod1",
+				ServiceNames: []string{"svc1"},
+				Type:         pb.KubeMetadataEventType_SET,
+			},
+		},
+		NamespaceMetadata: []*pb.NamespaceMetadata{
+			{
+				Namespace:   "ns1",
+				Labels:      map[string]string{"l1": "v1"},
+				Annotations: map[string]string{"a1": "v2"},
+				Type:        pb.KubeMetadataEventType_SET,
+			},
+		},
+	}
+
+	assert.True(t, proto.Equal(expected, resp))
 }
