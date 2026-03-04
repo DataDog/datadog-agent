60102a08e77707893115ff43ddfa267257cd8724
Baptiste Foy <baptiste.foy@datadoghq.com>
2026-03-04 14:16:41 +0100

    feat(ssi): implement SSI status reporting on Windows (IIS + DDInjector)

--- comp/updater/ssistatus/impl/ssistatus_windows.go
+++ comp/updater/ssistatus/impl/ssistatus_windows.go
@@ -7,7 +7,37 @@
 
 package ssistatusimpl
 
-// autoInstrumentationStatus checks if the APM auto-instrumentation is enabled on the host. This will return false on Kubernetes
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/DataDog/datadog-agent/pkg/fleet/installer/packages/ssi"
+	"github.com/DataDog/datadog-agent/pkg/fleet/installer/paths"
+)
+
+// autoInstrumentationStatus checks if the APM auto-instrumentation is enabled on the host.
 func (c *ssiStatusComponent) autoInstrumentationStatus() (bool, []string, error) {
-	return false, nil, nil // TBD on Windows
+	injectorInstalled := false
+	_, err := os.Stat(filepath.Join(paths.PackagesPath, "datadog-apm-inject"))
+	if err == nil {
+		injectorInstalled = true
+	} else if !os.IsNotExist(err) {
+		return false, nil, fmt.Errorf("could not check if injector package is installed: %w", err)
+	}
+
+	instrumentationStatus, err := ssi.GetInstrumentationStatus()
+	if err != nil {
+		return false, nil, fmt.Errorf("could not get APM injection status: %w", err)
+	}
+
+	var modes []string
+	if instrumentationStatus.IISInstrumented {
+		modes = append(modes, "iis")
+	}
+	if instrumentationStatus.HostInstrumented {
+		modes = append(modes, "host")
+	}
+
+	return injectorInstalled && len(modes) > 0, modes, nil
 }
--- pkg/fleet/installer/packages/ssi/status_windows.go
+++ pkg/fleet/installer/packages/ssi/status_windows.go
@@ -7,7 +7,36 @@
 
 package ssi
 
-// GetInstrumentationStatus contains the status of the APM auto-instrumentation.
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/sys/windows"
+
+	"github.com/DataDog/datadog-agent/pkg/util/winutil"
+)
+
+// GetInstrumentationStatus returns the status of the APM auto-instrumentation on Windows.
 func GetInstrumentationStatus() (status APMInstrumentationStatus, err error) {
-	return status, nil // TBD on Windows
+	// IIS instrumentation: check applicationHost.config for Datadog .NET library
+	iisCfgPath := filepath.Join(os.Getenv("windir"), "System32", "inetsrv", "config", "applicationHost.config")
+	iisContent, iisErr := os.ReadFile(iisCfgPath)
+	if iisErr != nil && !errors.Is(iisErr, os.ErrNotExist) {
+		return status, fmt.Errorf("could not read applicationHost.config: %w", iisErr)
+	}
+	if bytes.Contains(iisContent, []byte("datadog-apm-library-dotnet")) {
+		status.IISInstrumented = true
+	}
+
+	// Host instrumentation: check if the DDInjector kernel driver service is running
+	running, svcErr := winutil.IsServiceRunning("DDInjector")
+	if svcErr != nil && !errors.Is(svcErr, windows.ERROR_SERVICE_DOES_NOT_EXIST) {
+		return status, fmt.Errorf("could not check DDInjector service: %w", svcErr)
+	}
+	status.HostInstrumented = running
+
+	return status, nil
 }
