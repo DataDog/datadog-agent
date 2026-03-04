a48b7267ac06123e4cab4deb3810368baf6229cf
Andrew Glaude <andrew.glaude@datadoghq.com>
2026-03-04 14:10:27 -0500

    fix expvar import

--- cmd/otel-agent/subcommands/run/command.go
+++ cmd/otel-agent/subcommands/run/command.go
@@ -19,7 +19,7 @@ import (
 
 	agentConfig "github.com/DataDog/datadog-agent/cmd/otel-agent/config"
 	"github.com/DataDog/datadog-agent/cmd/otel-agent/subcommands"
-	expvarserverimpl "github.com/DataDog/datadog-agent/comp/agent/expvarserver/impl"
+	expvarserverfx "github.com/DataDog/datadog-agent/comp/agent/expvarserver/fx"
 	agenttelemetryfx "github.com/DataDog/datadog-agent/comp/core/agenttelemetry/fx"
 	coreconfig "github.com/DataDog/datadog-agent/comp/core/config"
 	"github.com/DataDog/datadog-agent/comp/core/configsync"
@@ -146,7 +146,7 @@ func runOTelAgentCommand(ctx context.Context, params *cliParams, opts ...fx.Opti
 				return h.Get, nil
 			}),
 			telemetryimpl.Module(),
-			expvarserverimpl.Module(),
+			expvarserverfx.Module(),
 			remotehostnameimpl.Module(),
 			collectorcontribFx.Module(),
 			collectorfx.ModuleNoAgent(),
@@ -238,7 +238,7 @@ func runOTelAgentCommand(ctx context.Context, params *cliParams, opts ...fx.Opti
 		configsyncimpl.Module(configsyncimpl.NewParams(params.SyncTimeout, true, params.SyncOnInitTimeout)),
 		remoteTaggerFx.Module(tagger.OptionalRemoteParams{Disable: isCmdPortNegative}, tagger.NewRemoteParams()),
 		telemetryimpl.Module(),
-		expvarserverimpl.Module(),
+		expvarserverfx.Module(),
 		fx.Provide(func(cfg traceconfig.Component) telemetry.TelemetryCollector {
 			return telemetry.NewCollector(cfg.Object())
 		}),
