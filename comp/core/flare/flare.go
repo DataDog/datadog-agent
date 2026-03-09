682fff1dbee1bfbe68f215e2f013873a5fb58a47
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2026-03-09 15:21:46 +0000

    fix(flare): add comment explaining buffered channel

--- comp/core/flare/flare.go
+++ comp/core/flare/flare.go
@@ -253,6 +253,7 @@ func (f *flare) runProviders(fb types.FlareBuilder, providerTimeout time.Duratio
 		f.log.Infof("Running flare provider %s with timeout %s", providerName, timeout)
 		_ = fb.Logf("Running flare provider %s with timeout %s", providerName, timeout)
 
+		// Buffered so the goroutine can send even if we already left via timeout.
 		done := make(chan struct{}, 1)
 		go func() {
 			startTime := time.Now()
