DataDog/datadog-agent

commit 1eccec1edfe32e404119bf96c692b0cbe195fd4b

[AMCC-159]Move filter list remote config into a separate component (#45168)
### What does this PR do?

Creates a new FilterList component. This handles the filterlist functionality including loading it from the config file and from Remote Config.

### Motivation

The Filter List is shared by both dogstatsd and the aggregator. It makes sense to move the code into it's own component so both can load it.

### Describe how you validated your changes

I have moved the unit tests to the component and have tested it still works for both dogstatsd and checks.

### Additional Notes


Co-authored-by: stephen.wakely <stephen.wakely@datadoghq.com>

comp/dogstatsd/server/server.go
@@ -32,7 +32,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/dogstatsd/pidmap"
 	replay "github.com/DataDog/datadog-agent/comp/dogstatsd/replay/def"
 	serverdebug "github.com/DataDog/datadog-agent/comp/dogstatsd/serverDebug"
-	rctypes "github.com/DataDog/datadog-agent/comp/remote-config/rcclient/types"
+	filterlist "github.com/DataDog/datadog-agent/comp/filterlist/def"
 	"github.com/DataDog/datadog-agent/pkg/aggregator"
 	"github.com/DataDog/datadog-agent/pkg/config/model"
 	"github.com/DataDog/datadog-agent/pkg/config/structure"
@@ -40,7 +40,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/metrics"
 	"github.com/DataDog/datadog-agent/pkg/metrics/event"
 	"github.com/DataDog/datadog-agent/pkg/metrics/servicecheck"
-	"github.com/DataDog/datadog-agent/pkg/remoteconfig/state"
 	"github.com/DataDog/datadog-agent/pkg/status/health"
 	"github.com/DataDog/datadog-agent/pkg/util/option"
 	"github.com/DataDog/datadog-agent/pkg/util/sort"
@@ -77,15 +76,16 @@ type dependencies struct {
 
 	Demultiplexer aggregator.Demultiplexer
 
-	Log       log.Component
-	Config    configComponent.Component
-	Debug     serverdebug.Component
-	Replay    replay.Component
-	PidMap    pidmap.Component
-	Params    Params
-	WMeta     option.Option[workloadmeta.Component]
-	Telemetry telemetry.Component
-	Hostname  hostnameinterface.Component
+	Log        log.Component
+	Config     configComponent.Component
+	Debug      serverdebug.Component
+	Replay     replay.Component
+	PidMap     pidmap.Component
+	Params     Params
+	WMeta      option.Option[workloadmeta.Component]
+	Telemetry  telemetry.Component
+	Hostname   hostnameinterface.Component
+	FilterList filterlist.Component
 }
 
 type provides struct {
@@ -93,7 +93,6 @@ type provides struct {
 
 	Comp          Component
 	StatsEndpoint api.AgentEndpointProvider
-	RCListener    rctypes.ListenerProvider
 }
 
 // When the internal telemetry is enabled, used to tag the origin
@@ -104,11 +103,6 @@ type cachedOriginCounter struct {
 	err    map[string]string
 	okCnt  telemetry.SimpleCounter
 	errCnt telemetry.SimpleCounter
-}
-
-type localFilterListConfig struct {
-	metricNames []string
-	matchPrefix bool
 }
 
 // Server represent a Dogstatsd server
@@ -135,12 +129,14 @@ type server struct {
 	sharedFloat64List       *float64ListPool
 	Statistics              *statutil.Stats
 	Started                 bool
+	startedMtx              sync.RWMutex
 	stopChan                chan bool
 	health                  *health.Handle
 	histToDist              bool
 	histToDistPrefix        string
 	extraTags               []string
 	Debug                   serverdebug.Component
+	filterList              filterlist.Component
 
 	tCapture                replay.Component
 	pidMap                  pidmap.Component
@@ -170,7 +166,6 @@ type server struct {
 	originTelemetry bool
 
 	enrichConfig
-	localFilterListConfig
 
 	wmeta option.Option[workloadmeta.Component]
 
@@ -180,8 +175,6 @@ type server struct {
 	tlmProcessedOk          telemetry.SimpleCounter
 	tlmProcessedError       telemetry.SimpleCounter
 	tlmChannel              telemetry.Histogram
-	tlmFilterListUpdates    telemetry.SimpleCounter
-	tlmFilterListSize       telemetry.SimpleGauge
 	listernersTelemetry     *listeners.TelemetryStore
 	packetsTelemetry        *packets.TelemetryStore
 	stringInternerTelemetry *stringInternerTelemetry
@@ -207,7 +200,7 @@ func initTelemetry() {
 
 // TODO: (components) - merge with newServerCompat once NewServerlessServer is removed
 func newServer(deps dependencies) provides {
-	s := newServerCompat(deps.Config, deps.Log, deps.Hostname, deps.Replay, deps.Debug, deps.Params.Serverless, deps.Demultiplexer, deps.WMeta, deps.PidMap, deps.Telemetry)
+	s := newServerCompat(deps.Config, deps.Log, deps.Hostname, deps.Replay, deps.Debug, deps.Params.Serverless, deps.Demultiplexer, deps.WMeta, deps.PidMap, deps.Telemetry, deps.FilterList)
 
 	dsdConfig := dsdconfig.NewConfig(s.config)
 	if dsdConfig.EnabledInternal() {
@@ -217,19 +210,13 @@ func newServer(deps dependencies) provides {
 		})
 	}
 
-	var rcListener rctypes.ListenerProvider
-	rcListener.ListenerProvider = rctypes.RCListener{
-		state.ProductMetricControl: s.onFilterListUpdateCallback,
-	}
-
 	return provides{
 		Comp:          s,
 		StatsEndpoint: api.NewAgentEndpointProvider(s.writeStats, "/dogstatsd-stats", "GET"),
-		RCListener:    rcListener,
 	}
 }
 
-func newServerCompat(cfg model.ReaderWriter, log log.Component, hostname hostnameinterface.Component, capture replay.Component, debug serverdebug.Component, serverless bool, demux aggregator.Demultiplexer, wmeta option.Option[workloadmeta.Component], pidMap pidmap.Component, telemetrycomp telemetry.Component) *server {
+func newServerCompat(cfg model.ReaderWriter, log log.Component, hostname hostnameinterface.Component, capture replay.Component, debug serverdebug.Component, serverless bool, demux aggregator.Demultiplexer, wmeta option.Option[workloadmeta.Component], pidMap pidmap.Component, telemetrycomp telemetry.Component, filterList filterlist.Component) *server {
 	// This needs to be done after the configuration is loaded
 	once.Do(func() { initTelemetry() })
 	var stats *statutil.Stats
@@ -328,6 +315,7 @@ func newServerCompat(cfg model.ReaderWriter, log log.Component, hostname hostnam
 		},
 		wmeta:                   wmeta,
 		telemetry:               telemetrycomp,
+		filterList:              filterList,
 		tlmProcessed:            dogstatsdTelemetryCount,
 		tlmProcessedOk:          dogstatsdTelemetryCount.WithValues("metrics", "ok", ""),
 		tlmProcessedError:       dogstatsdTelemetryCount.WithValues("metrics", "error", ""),
@@ -345,12 +333,6 @@ func newServerCompat(cfg model.ReaderWriter, log log.Component, hostname hostnam
 		[]string{"shard", "message_type"},
 		"Time in nanosecond to push metrics to the aggregator input buffer",
 		buckets)
-	s.tlmFilterListUpdates = telemetrycomp.NewSimpleCounter("dogstatsd", "filterlist_updates",
-		"Incremented when a reconfiguration of the filterlist happened",
-	)
-	s.tlmFilterListSize = telemetrycomp.NewSimpleGauge("dogstatsd", "filterlist_size",
-		"Filter list size",
-	)
 
 	// Initialize the metric type counters. These metrics are not
 	// per-context but absolute.
@@ -515,9 +497,13 @@ func (s *server) start(context.Context) error {
 }
 
 func (s *server) stop(context.Context) error {
+	s.startedMtx.Lock()
+	defer s.startedMtx.Unlock()
+
 	if !s.IsRunning() {
 		return nil
 	}
+
 	for _, l := range s.listeners {
 		l.Stop()
 	}
@@ -544,59 +530,19 @@ func (s *server) SetExtraTags(tags []string) {
 	s.extraTags = tags
 }
 
-// SetFilterList updates the metric names filter on all running worker.
-func (s *server) SetFilterList(metricNames []string, matchPrefix bool) {
-	s.log.Debugf("SetFilterList with %d metrics", len(metricNames))
+func (s *server) onFilterListUpdate(filterList utilstrings.Matcher, _ utilstrings.Matcher) {
+	s.startedMtx.RLock()
+	defer s.startedMtx.RUnlock()
 
-	// we will use two different filterlists:
-	// - one with all the metrics names, with all values from `metricNames`
-	// - one with only the metric names ending with histogram aggregates suffixes
-
-	// only histogram metric names (including their aggregates suffixes)
-	histoMetricNames := s.createHistogramsFilterList(metricNames)
-	matcher := utilstrings.NewMatcher(metricNames, matchPrefix)
+	if !s.IsRunning() {
+		// The workers have stopped so can't receive updates.
+		return
+	}
 
 	// send the complete filterlist to all workers, the listening part of dogstatsd
 	for _, worker := range s.workers {
-		worker.FilterListUpdate <- matcher
+		worker.FilterListUpdate <- filterList
 	}
-
-	// send the histogram filterlist used right before flushing to the serializer
-	histoMatcher := utilstrings.NewMatcher(histoMetricNames, matchPrefix)
-
-	s.demultiplexer.SetSamplersFilterList(matcher, histoMatcher)
-}
-
-// create a list based on all `metricNames` but only containing metric names
-// with histogram aggregates suffixes.
-// TODO(remy): should we consider moving this in the metrics package instead?
-func (s *server) createHistogramsFilterList(metricNames []string) []string {
-	aggrs := s.config.GetStringSlice("histogram_aggregates")
-
-	percentiles := metrics.ParsePercentiles(s.config.GetStringSlice("histogram_percentiles"))
-	percentileAggrs := make([]string, len(percentiles))
-	for i, percentile := range percentiles {
-		percentileAggrs[i] = fmt.Sprintf("%dpercentile", percentile)
-	}
-
-	histoMetricNames := []string{}
-	for _, metricName := range metricNames {
-		// metric names ending with a histogram aggregates
-		for _, aggr := range aggrs {
-			if strings.HasSuffix(metricName, "."+aggr) {
-				histoMetricNames = append(histoMetricNames, metricName)
-			}
-		}
-		// metric names ending with a percentile
-		for _, percentileAggr := range percentileAggrs {
-			if strings.HasSuffix(metricName, "."+percentileAggr) {
-				histoMetricNames = append(histoMetricNames, metricName)
-			}
-		}
-	}
-
-	s.log.Debugf("SetFilterList created a histograms subsets of %d metric names", len(histoMetricNames))
-	return histoMetricNames
 }
 
 func (s *server) handleMessages() {
@@ -630,31 +576,9 @@ func (s *server) handleMessages() {
 		s.workers = append(s.workers, worker)
 	}
 
-	// init the metric names filterlist
-	filterlist := s.config.GetStringSlice("metric_filterlist")
-	filterlistPrefix := s.config.GetBool("metric_filterlist_match_prefix")
-	if len(filterlist) == 0 {
-		filterlist = s.config.GetStringSlice("statsd_metric_blocklist")
-		filterlistPrefix = s.config.GetBool("statsd_metric_blocklist_match_prefix")
-	}
-
-	s.localFilterListConfig = localFilterListConfig{
-		metricNames: filterlist,
-		matchPrefix: filterlistPrefix,
-	}
-	s.restoreFilterListFromLocalConfig()
-}
-
-func (s *server) restoreFilterListFromLocalConfig() {
-	s.log.Debug("Restoring filterlist with local config.")
-
-	s.tlmFilterListUpdates.Inc()
-	s.tlmFilterListSize.Set(float64(len(s.localFilterListConfig.metricNames)))
-
-	s.SetFilterList(
-		s.localFilterListConfig.metricNames,
-		s.localFilterListConfig.matchPrefix,
-	)
+	// It is important to set this up after the workers are running so they receive
+	// the initial  filterlist and any updates.
+	s.filterList.OnUpdateMetricFilterList(s.onFilterListUpdate)
 }
 
 func (s *server) UDPLocalAddr() string {

.github/CODEOWNERS
@@ -372,6 +372,7 @@
 /comp/autoscaling/datadogclient @DataDog/container-integrations
 /comp/connectivitychecker @DataDog/fleet
 /comp/etw @DataDog/windows-products
+/comp/filterlist @DataDog/agent-metric-pipelines
 /comp/fleetstatus @DataDog/fleet
 /comp/haagent @DataDog/ndm-core
 /comp/healthplatform @DataDog/agent-health

cmd/agent/subcommands/run/command.go
@@ -112,6 +112,7 @@ import (
 	dogstatsdServer "github.com/DataDog/datadog-agent/comp/dogstatsd/server"
 	dogstatsddebug "github.com/DataDog/datadog-agent/comp/dogstatsd/serverDebug"
 	"github.com/DataDog/datadog-agent/comp/dogstatsd/statsd"
+	filterlist "github.com/DataDog/datadog-agent/comp/filterlist/fx"
 	fleetfx "github.com/DataDog/datadog-agent/comp/fleetstatus/fx"
 	"github.com/DataDog/datadog-agent/comp/forwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
@@ -445,6 +446,7 @@ func getSharedFxOption() fx.Option {
 		apiimpl.Module(),
 		grpcAgentfx.Module(),
 		commonendpoints.Module(),
+		filterlist.Module(),
 		demultiplexerimpl.Module(demultiplexerimpl.NewDefaultParams(demultiplexerimpl.WithDogstatsdNoAggregationPipelineConfig())),
 		demultiplexerendpointfx.Module(),
 		dogstatsd.Bundle(dogstatsdServer.Params{Serverless: false}),

cmd/agent/subcommands/run/internal/settings/runtime_settings_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/dogstatsd"
 	"github.com/DataDog/datadog-agent/comp/dogstatsd/server"
 	serverdebug "github.com/DataDog/datadog-agent/comp/dogstatsd/serverDebug"
+	filterlist "github.com/DataDog/datadog-agent/comp/filterlist/fx-mock"
 	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
 
 	"github.com/DataDog/datadog-agent/pkg/config/model"
@@ -54,6 +55,7 @@ func TestDogstatsdMetricsStats(t *testing.T) {
 			return taggerComponent
 		}),
 		workloadmetafxmock.MockModule(workloadmeta.NewParams()),
+		filterlist.MockModule(),
 	))
 
 	s := DsdStatsRuntimeSetting{

cmd/cluster-agent-cloudfoundry/subcommands/run/command.go
@@ -58,6 +58,7 @@ import (
 	workloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/def"
 	workloadmetafx "github.com/DataDog/datadog-agent/comp/core/workloadmeta/fx"
 	workloadmetainit "github.com/DataDog/datadog-agent/comp/core/workloadmeta/init"
+	filterlistfx "github.com/DataDog/datadog-agent/comp/filterlist/fx"
 	"github.com/DataDog/datadog-agent/comp/forwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/eventplatform/eventplatformimpl"
@@ -110,6 +111,7 @@ func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 				hostnameimpl.Module(),
 				secretsfx.Module(),
 				forwarder.Bundle(defaultforwarder.NewParams(defaultforwarder.WithResolvers())),
+				filterlistfx.Module(),
 				demultiplexerimpl.Module(demultiplexerimpl.NewDefaultParams()),
 				orchestratorForwarderImpl.Module(orchestratorForwarderImpl.NewDisabledParams()),
 				eventplatformimpl.Module(eventplatformimpl.NewDisabledParams()),

cmd/cluster-agent/subcommands/start/command.go
@@ -60,6 +60,7 @@ import (
 	workloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/def"
 	workloadmetafx "github.com/DataDog/datadog-agent/comp/core/workloadmeta/fx"
 	workloadmetainit "github.com/DataDog/datadog-agent/comp/core/workloadmeta/init"
+	filterlistfx "github.com/DataDog/datadog-agent/comp/filterlist/fx"
 	"github.com/DataDog/datadog-agent/comp/forwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/eventplatform/eventplatformimpl"
@@ -159,6 +160,7 @@ func Commands(globalParams *command.GlobalParams) []*cobra.Command {
 				hostnameimpl.Module(),
 				secretsfx.Module(),
 				forwarder.Bundle(defaultforwarder.NewParams(defaultforwarder.WithResolvers(), defaultforwarder.WithDisableAPIKeyChecking())),
+				filterlistfx.Module(),
 				demultiplexerimpl.Module(demultiplexerimpl.NewDefaultParams()),
 				orchestratorForwarderImpl.Module(orchestratorForwarderImpl.NewDefaultParams()),
 				eventplatformimpl.Module(eventplatformimpl.NewDisabledParams()),

cmd/dogstatsd/subcommands/start/command.go
@@ -38,6 +38,7 @@ import (
 	workloadmetainit "github.com/DataDog/datadog-agent/comp/core/workloadmeta/init"
 	"github.com/DataDog/datadog-agent/comp/dogstatsd"
 	dogstatsdServer "github.com/DataDog/datadog-agent/comp/dogstatsd/server"
+	filterlistfx "github.com/DataDog/datadog-agent/comp/filterlist/fx"
 	"github.com/DataDog/datadog-agent/comp/forwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/eventplatform/eventplatformimpl"
@@ -139,6 +140,7 @@ func RunDogstatsdFct(cliParams *CLIParams, defaultConfPath string, defaultLogFil
 		}),
 		metricscompressionfx.Module(),
 		logscompressionfx.Module(),
+		filterlistfx.Module(),
 		demultiplexerimpl.Module(demultiplexerimpl.NewDefaultParams(
 			demultiplexerimpl.WithContinueOnMissingHostname(),
 			demultiplexerimpl.WithDogstatsdNoAggregationPipelineConfig(),

comp/aggregator/bundle_test.go
@@ -16,6 +16,7 @@ import (
 	secrets "github.com/DataDog/datadog-agent/comp/core/secrets/def"
 	secretsmock "github.com/DataDog/datadog-agent/comp/core/secrets/mock"
 	nooptagger "github.com/DataDog/datadog-agent/comp/core/tagger/fx-noop"
+	filterlistfx "github.com/DataDog/datadog-agent/comp/filterlist/fx-mock"
 	"github.com/DataDog/datadog-agent/comp/forwarder/defaultforwarder"
 	"github.com/DataDog/datadog-agent/comp/forwarder/eventplatform/eventplatformimpl"
 	orchestratorForwarderImpl "github.com/DataDog/datadog-agent/comp/forwarder/orchestrator/orchestratorimpl"
@@ -36,6 +37,7 @@ func TestBundleDependencies(t *testing.T) {
 		haagentmock.Module(),
 		logscompression.MockModule(),
 		metricscompression.MockModule(),
+		filterlistfx.MockModule(),
 		fx.Provide(func() secrets.Component { return secretsmock.New(t) }),
 	)
 }

commit 0b8aea2a193fd9616a64f5775768548febf54188

[filterlist] Fix initialization with local config
1eccec1 broke the initialization logic of the filterlist when configured with
the local config, because the commit moved the initial `onFilterListUpdate` call
to before the dogstatsd server is running (`s.IsRunning() == true`).

This commit fixes this by explicitly passing the locally-configured filterlist when
the dogstatsd workers are initialized.

comp/dogstatsd/server/server.go
@@ -535,7 +535,7 @@ func (s *server) onFilterListUpdate(filterList utilstrings.Matcher, _ utilstring
 	defer s.startedMtx.RUnlock()
 
 	if !s.IsRunning() {
-		// The workers have stopped so can't receive updates.
+		// The server is not running, so workers can't receive updates.
 		return
 	}
 
@@ -571,13 +571,13 @@ func (s *server) handleMessages() {
 	s.log.Debug("DogStatsD will run", workersCount, "workers")
 
 	for i := 0; i < workersCount; i++ {
-		worker := newWorker(s, i, s.wmeta, s.packetsTelemetry, s.stringInternerTelemetry)
+		worker := newWorker(s, i, s.wmeta, s.packetsTelemetry, s.stringInternerTelemetry, s.filterList.GetMetricFilterList())
 		go worker.run()
 		s.workers = append(s.workers, worker)
 	}
 
 	// It is important to set this up after the workers are running so they receive
-	// the initial  filterlist and any updates.
+	// any updates.
 	s.filterList.OnUpdateMetricFilterList(s.onFilterListUpdate)
 }
 

comp/dogstatsd/server/server_worker.go
@@ -40,7 +40,7 @@ type worker struct {
 	filterList       utilstrings.Matcher
 }
 
-func newWorker(s *server, workerNum int, wmeta option.Option[workloadmeta.Component], packetsTelemetry *packets.TelemetryStore, stringInternerTelemetry *stringInternerTelemetry) *worker {
+func newWorker(s *server, workerNum int, wmeta option.Option[workloadmeta.Component], packetsTelemetry *packets.TelemetryStore, stringInternerTelemetry *stringInternerTelemetry, filterList utilstrings.Matcher) *worker {
 	var batcher *batcher
 	if s.ServerlessMode {
 		batcher = newServerlessBatcher(s.demultiplexer, s.tlmChannel)
@@ -55,6 +55,7 @@ func newWorker(s *server, workerNum int, wmeta option.Option[workloadmeta.Compon
 		samples:          make(metrics.MetricSampleBatch, 0, defaultSampleSize),
 		packetsTelemetry: packetsTelemetry,
 		FilterListUpdate: make(chan utilstrings.Matcher),
+		filterList:       filterList,
 	}
 }
 

comp/dogstatsd/server/filterlist_init_test.go
@@ -1,0 +1,94 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build test
+
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/fx"
+
+	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer"
+	"github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/demultiplexerimpl"
+	configComponent "github.com/DataDog/datadog-agent/comp/core/config"
+	"github.com/DataDog/datadog-agent/comp/core/hostname/hostnameimpl"
+	"github.com/DataDog/datadog-agent/comp/core/hostname/hostnameinterface"
+	log "github.com/DataDog/datadog-agent/comp/core/log/def"
+	logmock "github.com/DataDog/datadog-agent/comp/core/log/mock"
+	"github.com/DataDog/datadog-agent/comp/core/telemetry"
+	"github.com/DataDog/datadog-agent/comp/core/telemetry/telemetryimpl"
+	workloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/def"
+	workloadmetafxmock "github.com/DataDog/datadog-agent/comp/core/workloadmeta/fx-mock"
+	"github.com/DataDog/datadog-agent/comp/dogstatsd/listeners"
+	"github.com/DataDog/datadog-agent/comp/dogstatsd/pidmap"
+	"github.com/DataDog/datadog-agent/comp/dogstatsd/pidmap/pidmapimpl"
+	replay "github.com/DataDog/datadog-agent/comp/dogstatsd/replay/def"
+	replaymock "github.com/DataDog/datadog-agent/comp/dogstatsd/replay/fx-mock"
+	serverdebug "github.com/DataDog/datadog-agent/comp/dogstatsd/serverDebug"
+	"github.com/DataDog/datadog-agent/comp/dogstatsd/serverDebug/serverdebugimpl"
+	filterlistimpl "github.com/DataDog/datadog-agent/comp/filterlist/impl"
+	logscompression "github.com/DataDog/datadog-agent/comp/serializer/logscompression/fx-mock"
+	metricscompression "github.com/DataDog/datadog-agent/comp/serializer/metricscompression/fx-mock"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"github.com/DataDog/datadog-agent/pkg/util/option"
+)
+
+// depsWithoutFilterList is like depsWithoutServer but without the FilterList field,
+// so we can inject a real filterlist implementation manually.
+type depsWithoutFilterList struct {
+	fx.In
+
+	Config        configComponent.Component
+	Log           log.Component
+	Demultiplexer demultiplexer.FakeSamplerMock
+	Replay        replay.Component
+	PidMap        pidmap.Component
+	Debug         serverdebug.Component
+	WMeta         option.Option[workloadmeta.Component]
+	Telemetry     telemetry.Component
+	Hostname      hostnameinterface.Component
+}
+
+func TestWorkerFilterListInitializedFromLocalConfig(t *testing.T) {
+	cfg := map[string]interface{}{
+		"dogstatsd_port":    listeners.RandomPortName,
+		"metric_filterlist": []string{"filtered.metric"},
+	}
+
+	deps := fxutil.Test[depsWithoutFilterList](t, fx.Options(
+		fx.Provide(func() log.Component { return logmock.New(t) }),
+		fx.Provide(func() configComponent.Component { return configComponent.NewMockWithOverrides(t, cfg) }),
+		telemetryimpl.MockModule(),
+		hostnameimpl.MockModule(),
+		serverdebugimpl.MockModule(),
+		replaymock.MockModule(),
+		pidmapimpl.Module(),
+		demultiplexerimpl.FakeSamplerMockModule(),
+		workloadmetafxmock.MockModule(workloadmeta.NewParams()),
+		metricscompression.MockModule(),
+		logscompression.MockModule(),
+	))
+
+	fl := filterlistimpl.NewFilterList(deps.Log, deps.Config, deps.Telemetry)
+	s := newServerCompat(deps.Config, deps.Log, deps.Hostname, deps.Replay, deps.Debug, false, deps.Demultiplexer, deps.WMeta, deps.PidMap, deps.Telemetry, fl)
+
+	err := s.start(context.TODO())
+	require.NoError(t, err)
+	defer s.stop(context.TODO()) //nolint:errcheck
+
+	require.NotEmpty(t, s.workers, "server should have workers")
+	for i, worker := range s.workers {
+		require.NotNil(t, worker.filterList, "worker %d filterList should not be nil", i)
+		assert.True(t, worker.filterList.Test("filtered.metric"),
+			"worker %d should filter 'filtered.metric' from local config", i)
+		assert.False(t, worker.filterList.Test("unfiltered.metric"),
+			"worker %d should not filter 'unfiltered.metric'", i)
+	}
+}