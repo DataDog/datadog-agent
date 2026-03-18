f46dad48853803181634dbfbac461470dc412c5d
Kyle Nusbaum <kyle.nusbaum@datadoghq.com>
2026-03-18 16:23:24 -0500

    Fix Viper GetStringSlice for CSI registry allow list
    Use ParseEnvAsStringSlice (the established pattern in the codebase) to
    register an env var transformer that splits comma-separated values. This
    replaces the ad-hoc splitStringSlice wrapper and is consistent with how
    other string slice configs handle the Viper limitation (e.g. apm_config.features,
    apm_config.ignore_resources).

--- pkg/config/setup/common_settings.go
+++ pkg/config/setup/common_settings.go
@@ -665,6 +665,16 @@ func initCoreAgentFull(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.patcher.file_provider_path", "/etc/datadog-agent/patch/auto-instru.json") // to be used only in e2e tests
 	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.inject_auto_detected_libraries", true)                                    // allows injecting libraries for languages detected by automatic language detection feature
 	config.BindEnvAndSetDefault("admission_controller.auto_instrumentation.csi_registry_allow_list", []string{})                                     // restricts which registries can be used for CSI-based library injection
+	config.ParseEnvAsStringSlice("admission_controller.auto_instrumentation.csi_registry_allow_list", func(s string) []string {
+		var result []string
+		for _, r := range strings.Split(s, ",") {
+			r = strings.TrimSpace(r)
+			if r != "" {
+				result = append(result, r)
+			}
+		}
+		return result
+	})
 	config.BindEnv("admission_controller.auto_instrumentation.init_resources.cpu")                                                                   //nolint:forbidigo // TODO: replace by 'SetDefaultAndBindEnv'
 	config.BindEnv("admission_controller.auto_instrumentation.init_resources.memory")                                                                //nolint:forbidigo // TODO: replace by 'SetDefaultAndBindEnv'
 	config.BindEnv("admission_controller.auto_instrumentation.init_security_context")                                                                //nolint:forbidigo // TODO: replace by 'SetDefaultAndBindEnv'
