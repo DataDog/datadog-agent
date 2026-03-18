c8c4277d248c4c560aa9b1a630dc32189cdfd038
Nayef Ghattas <nayef.ghattas@datadoghq.com>
2026-03-18 22:23:21 +0100

    Trigger SBOM E2E test to debug pod readiness failure with flare artifacts
    Temporary change to trigger the SBOM E2E test on this branch so the
    DownloadFromPod fix can capture agent flares for debugging the pod
    readiness issue introduced by 15b3097d042.

--- test/new-e2e/tests/sbom/k8s_test.go
+++ test/new-e2e/tests/sbom/k8s_test.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
+// Trigger SBOM E2E test to capture flare artifacts with DownloadFromPod fix.
 package sbom
 
 import (
