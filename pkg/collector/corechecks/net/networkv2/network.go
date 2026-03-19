96724e89450d51223ba11423cc8fa452c5a4f9f0
Pierre Gimalac <pierre.gimalac@datadoghq.com>
2026-03-19 10:13:36 +0000

    chore: remove dead connections code from network v2 check

--- pkg/collector/corechecks/net/networkv2/network.go
+++ pkg/collector/corechecks/net/networkv2/network.go
@@ -95,7 +95,6 @@ type networkConfig struct {
 
 type networkStats interface {
 	IOCounters(pernic bool) ([]net.IOCountersStat, error)
-	Connections(kind string) ([]net.ConnectionStat, error)
 	NetstatAndSnmpCounters(protocols []string) (map[string]net.ProtoCountersStat, error)
 	GetProcPath() string
 	GetNetProcBasePath() string
@@ -113,10 +112,6 @@ type connectionStateEntry struct {
 
 func (n defaultNetworkStats) IOCounters(pernic bool) ([]net.IOCountersStat, error) {
 	return net.IOCounters(pernic)
-}
-
-func (n defaultNetworkStats) Connections(kind string) ([]net.ConnectionStat, error) {
-	return net.Connections(kind)
 }
 
 func (n defaultNetworkStats) NetstatAndSnmpCounters(protocols []string) (map[string]net.ProtoCountersStat, error) {
