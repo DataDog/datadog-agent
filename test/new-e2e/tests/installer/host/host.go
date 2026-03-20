a99dd63cd126ddc7676be1e9759a47ac13c12743
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2026-03-20 16:33:28 +0100

    fix: use string concatenation rather than `fmt.Sprintf`

--- test/new-e2e/tests/installer/host/host.go
+++ test/new-e2e/tests/installer/host/host.go
@@ -580,8 +580,8 @@ func (h *Host) SetUmask(mask string) (oldmask string) {
 func (h *Host) SetupProxy() {
 	// Install Docker & the Squid Proxy
 	h.InstallDocker()
-	h.remote.MustExecute(fmt.Sprintf("sudo docker run -d --name squid-proxy -v /opt/fixtures/squid.conf:/etc/squid/squid.conf -p 3128:3128 %s",
-		h.dockerImage("dockerhub/ubuntu/squid:4.10-20.04_beta", "public.ecr.aws/ubuntu/squid:4.10-20.04_beta")))
+	h.remote.MustExecute("sudo docker run -d --name squid-proxy -v /opt/fixtures/squid.conf:/etc/squid/squid.conf -p 3128:3128 " +
+		h.dockerImage("dockerhub/ubuntu/squid:4.10-20.04_beta", "public.ecr.aws/ubuntu/squid:4.10-20.04_beta"))
 
 	squidIP := strings.TrimSpace(h.remote.MustExecute("sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' squid-proxy"))
 
