4dd3c79ed840097267c24db761db1c676a955aae
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2026-03-24 12:11:14 +0100

    deps: install openssl with dd_cc_packaged

--- omnibus/config/software/openssl3.rb
+++ omnibus/config/software/openssl3.rb
@@ -25,8 +25,6 @@ relative_path "openssl-#{version}"
 build do
   flavor_flag = fips_mode? ? "--//packages/agent:flavor=fips" : ""
 
-  command_on_repo_root "bazelisk run #{flavor_flag} --//:install_dir=#{install_dir} -- @openssl//:install --destdir=#{install_dir}"
-
   unless windows?
     command_on_repo_root "bazelisk run --//:install_dir=#{install_dir} -- @zlib//:install --destdir=#{install_dir}"
     # build_agent_dmg.sh sets INSTALL_DIR to some temporary folder.
