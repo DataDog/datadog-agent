cd4e8db4aae3888a6ce256b9c3babeeb41c1b06e
Tony Aiuto <tony.aiuto@datadoghq.com>
2026-03-25 17:31:53 -0400

    launch bazelisk from tasks

--- tasks/libs/build/bazel.py
+++ tasks/libs/build/bazel.py
@@ -20,7 +20,7 @@ def bazel_not_found_message(color: str) -> str:
 def bazel(ctx: Context, *args: str, capture_output: bool = False, sudo: bool = False) -> None | str:
     """Execute a bazel command. Returns the captured standard output as string if capture_output=True."""
 
-    if not (resolved_bazel := shutil.which("bazel")):
+    if not (resolved_bazel := shutil.which("bazelisk")):
         raise Exit(bazel_not_found_message("red"))
     cmd = ("sudo", resolved_bazel) if sudo else ("bazel",)
     kwargs = {}
