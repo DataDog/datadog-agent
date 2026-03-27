3aaeac98e61192b063efc62e826670a0e0cade04
Yoann Ghigoff <yoann.ghigoff@datadoghq.com>
2026-03-27 11:15:18 +0100

    Revert "[CWS] Reduce memory usage of process snapshot and event replay (#48444)"
    This reverts commit e0517183e545099f5a0fdecdfd38538df2a482e5.

--- pkg/security/secl/model/model.go
+++ pkg/security/secl/model/model.go
@@ -536,6 +536,10 @@ type SnapshottedMmapedFile struct {
 type ProcessCacheEntry struct {
 	ProcessContext
 	Children []*ProcessCacheEntry `field:"-" copy:"-"`
+
+	// Snapshot data (only populated during initial snapshot, used for event replay)
+	SnapshottedBoundSockets []SnapshottedBoundSocket `field:"-"`
+	SnapshottedMmapedFiles  []SnapshottedMmapedFile  `field:"-"`
 }
 
 // IsContainerRoot returns whether this is a top level process in the container ID
