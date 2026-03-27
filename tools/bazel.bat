18f9dd1222ad332deb758713ec8d699c8e6da34f
Régis Desgroppes <rdesgroppes@gmail.com>
2026-03-27 20:09:10 +0100

    Address review commentS

--- tools/bazel.bat
+++ tools/bazel.bat
@@ -38,7 +38,7 @@ if defined XDG_CACHE_HOME (
   :: https://github.com/bazelbuild/bazel/issues/27808
   set "bazel_home=%XDG_CACHE_HOME%\bazel"
   set bazel_home_startup_option="--output_user_root=!bazel_home!"
-  set "extra_args=--disk_cache=!bazel_home!\disk-cache"
+  set extra_args="--disk_cache=!bazel_home!\disk-cache"
   :: https://github.com/bazelbuild/bazel/issues/26384
   for %%i in ("%~dp0..\.cache") do if "!XDG_CACHE_HOME!" == "%%~fi" set "extra_args=!extra_args! --repo_contents_cache="
   if defined CI if not defined GITHUB_ACTIONS set "extra_args=!extra_args! --config=ci"
