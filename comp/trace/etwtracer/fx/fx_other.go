a7ae9c703cf12436362042fcc998da77dd2187df
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-03-30 08:02:43 +0000

    fix(lint): add windows build constraint to fx.go and Module() for non-windows

--- comp/trace/etwtracer/fx/fx_other.go
+++ comp/trace/etwtracer/fx/fx_other.go
@@ -3,19 +3,14 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
-//go:build windows
+//go:build !windows
 
 // Package fx provides the fx module for the etwtracer component.
 package fx
 
-import (
-	etwtracerimpl "github.com/DataDog/datadog-agent/comp/trace/etwtracer/impl"
-	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
-)
+import "github.com/DataDog/datadog-agent/pkg/util/fxutil"
 
 // Module defines the fx options for this component.
 func Module() fxutil.Module {
-	return fxutil.Component(
-		fxutil.ProvideComponentConstructor(etwtracerimpl.NewComponent),
-	)
+	return fxutil.Component()
 }
