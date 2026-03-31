581d477337103a16157b734d82b9a4440706b3f7
amitslavin <amit.slavin@datadoghq.com>
2026-03-31 10:14:35 +0300

    Fix missing container IDs for processes with delayed cgroup migration"

---
+++ pkg/network/tracer/process_cache_linux.go
@@ -0,0 +1,26 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build linux_bpf
+
+package tracer
+
+import (
+	"strconv"
+
+	"go4.org/intern"
+
+	"github.com/DataDog/datadog-agent/pkg/network/events"
+	"github.com/DataDog/datadog-agent/pkg/util/cgroups"
+)
+
+func rescueEventWithProcfs(entry *events.Process) *events.Process {
+	cid, _ := cgroups.IdentiferFromCgroupReferences("/proc", strconv.FormatUint(uint64(entry.Pid), 10), "", cgroups.ContainerFilter)
+	if cid != "" {
+		entry.ContainerID = intern.GetByString(cid)
+		return entry
+	}
+	return nil
+}
