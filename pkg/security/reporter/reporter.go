8a18332b46db2cb507b51295c9120bfcdc5e97a2
Maxime Riaud <maxime.riaud@datadoghq.com>
2026-04-01 13:14:53 +0000

    go mod tidy

--- pkg/security/reporter/reporter.go
+++ pkg/security/reporter/reporter.go
@@ -15,6 +15,7 @@ import (
 	logsconfig "github.com/DataDog/datadog-agent/comp/logs/agent/config"
 	compression "github.com/DataDog/datadog-agent/comp/serializer/logscompression/def"
 	pkgconfigsetup "github.com/DataDog/datadog-agent/pkg/config/setup"
+	"github.com/DataDog/datadog-agent/pkg/hook"
 	"github.com/DataDog/datadog-agent/pkg/logs/client"
 	"github.com/DataDog/datadog-agent/pkg/logs/diagnostic"
 	"github.com/DataDog/datadog-agent/pkg/logs/message"
