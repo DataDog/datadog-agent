8a1b623d2def5cbd708204d7a98ea0e6cd91d42d
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-04-02 11:52:18 +0000

    fix(otelcol): add doc.go to logsagentpipeline module root and clean up Bazel deps
    - Add doc.go to comp/otelcol/logsagentpipeline/ to satisfy check_valid_mods
      which requires *.go files at the module root
    - Remove stale logsagentpipelineimpl entry from deps/go.MODULE.bazel
      which caused all Bazel jobs to fail

---
+++ comp/otelcol/logsagentpipeline/doc.go
@@ -0,0 +1,10 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+
+// Package logsagentpipeline contains the logs agent pipeline component.
+// See def/component.go for the component interface.
+// See impl/agent.go for the implementation.
+// See fx/fx.go for the fx module.
+package logsagentpipeline
