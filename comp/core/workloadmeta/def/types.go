351940871a48170b0ca1e162c378107475cd1795
Mateo Lelong <mateo.lelong@datadoghq.com>
2026-04-02 10:51:56 +0200

    chore: include release note and apply lint

--- comp/core/workloadmeta/def/types.go
+++ comp/core/workloadmeta/def/types.go
@@ -658,10 +658,10 @@ type Container struct {
 	// CgroupPath is a path to the cgroup of the container.
 	// It can be relative to the cgroup parent.
 	// Linux only.
-	CgroupPath   string
+	CgroupPath string
 	// SandboxID is the identifier of the sandbox this container belongs to.
 	// Populated from containerd's container info SandboxID field.
-	SandboxID string
+	SandboxID    string
 	RestartCount int
 
 	SBOM *SBOM
