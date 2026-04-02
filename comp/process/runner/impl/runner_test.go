4ca775c19aa47393e5f5a0e4162c4d07c81bbe03
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-04-02 16:36:16 +0000

    fix(test): use ProvideComponentConstructor instead of fx.Provide for NewComponent in runner tests

--- comp/process/runner/impl/runner_test.go
+++ comp/process/runner/impl/runner_test.go
@@ -113,7 +113,7 @@ type Deps struct {
 
 func createDeps(t *testing.T, confOverrides map[string]interface{}, options ...fx.Option) Deps {
 	return fxutil.Test[Deps](t, fx.Options(
-		fx.Provide(NewComponent),
+		fxutil.ProvideComponentConstructor(NewComponent),
 		submittermock.MockModule(),
 		hostinfomock.MockModule(),
 
