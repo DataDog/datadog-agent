ad236859e658f88f4e1b70c4c54b3ad461a4f93f
Nicole Chung <nicole.chung@datadoghq.com>
2026-04-03 15:53:13 -0400

    removed k8s api and re-added licenses

--- pkg/util/kubernetes/apiserver/apiserver.go
+++ pkg/util/kubernetes/apiserver/apiserver.go
@@ -18,8 +18,6 @@ import (
 	"sync"
 	"time"
 
-	k8strace "gopkg.in/DataDog/dd-trace-go.v1/contrib/k8s.io/client-go/kubernetes"
-
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -250,7 +248,7 @@ func GetClientConfig(timeout time.Duration, qps float32, burst int) (*rest.Confi
 	clientConfig.QPS = qps
 	clientConfig.Burst = burst
 	clientConfig.Wrap(func(rt http.RoundTripper) http.RoundTripper {
-		return NewCustomRoundTripper(k8strace.WrapRoundTripper(rt), timeout)
+		return NewCustomRoundTripper(rt, timeout)
 	})
 
 	return clientConfig, nil
