4669619217d3d64bdb728fe8bcf6de48b6e094d6
Usama Saqib <usama.saqib@datadoghq.com>
2026-04-03 20:55:39 +0200

    use sys_open for arm

--- pkg/ebpf/testdata/c/sleepable.c
+++ pkg/ebpf/testdata/c/sleepable.c
@@ -12,12 +12,12 @@ int BPF_PROG(test_modifier_x64, const struct pt_regs* regs, long ret) {
     return 0;
 }
 
-SEC("fexit/__arm64_sys_openat")
+SEC("fexit/__arm64_sys_open")
 int BPF_PROG(test_modifier_arm64, struct pt_regs* regs, long ret) {
     const char* pathname;
     char buf[16];
 
-    bpf_probe_read_kernel(&pathname, sizeof(pathname), &PT_REGS_PARM2(regs));
+    bpf_probe_read_kernel(&pathname, sizeof(pathname), &PT_REGS_PARM1(regs));
     bpf_copy_from_user(&buf, sizeof(buf), pathname);
 
     return 0;
@@ -34,12 +34,12 @@ int BPF_PROG(test_replaced_x64, const struct pt_regs* regs, long ret) {
     return 0;
 }
 
-SEC("fexit/__arm64_sys_openat")
-int BPF_PROG(test_replaced_arm64, struct pt_regs* regs, long ret) {
+SEC("fexit/__arm64_sys_open")
+int BPF_PROG(test_replaced_arm64, const struct pt_regs* regs, long ret) {
     const char* pathname;
     char buf[16];
 
-    bpf_probe_read_kernel(&pathname, sizeof(pathname), &PT_REGS_PARM2(regs));
+    bpf_probe_read_kernel(&pathname, sizeof(pathname), &PT_REGS_PARM1(regs));
     bpf_probe_read_user(&buf, sizeof(buf), pathname);
 
     return 0;
@@ -56,12 +56,12 @@ int BPF_PROG(test_womodifier_x64, const struct pt_regs* regs, long ret) {
     return 0;
 }
 
-SEC("fexit/__arm64_sys_openat")
+SEC("fexit/__arm64_sys_open")
 int BPF_PROG(test_womodifier_arm64, struct pt_regs* regs, long ret) {
     const char* pathname;
     char buf[16];
 
-    bpf_probe_read_kernel(&pathname, sizeof(pathname), &PT_REGS_PARM2(regs));
+    bpf_probe_read_kernel(&pathname, sizeof(pathname), &PT_REGS_PARM1(regs));
     bpf_copy_from_user(&buf, sizeof(buf), pathname);
 
     return 0;
