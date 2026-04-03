12ae2149416fbd2df58296e4e841546e0ddddf48
louis-cqrl <93274433+louis-cqrl@users.noreply.github.com>
2026-04-03 03:00:40 +0000

    fix(lint): add //go:build test to cmd/agent/subcommands test files for golangci-lint

--- cmd/agent/subcommands/integrations/command_test.go
+++ cmd/agent/subcommands/integrations/command_test.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build python
+//go:build test && python
 
 package integrations
 
