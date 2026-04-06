4bb896914b8f6d136bed71955415f5939c906cfd
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-04-06 14:53:46 -0700

    ignore udp binds for ephemeral ports

--- pkg/network/ebpf/c/sk/sk_port.h
+++ pkg/network/ebpf/c/sk/sk_port.h
@@ -9,6 +9,7 @@
 #include "netns.h"
 #include "ip.h"
 #include "ipv6.h"
+#include "port_range.h"
 
 SEC("iter/task_file")
 int bpf_iter__task_file_port_bindings(struct bpf_iter__task_file *ctx) {
@@ -34,11 +35,15 @@ int bpf_iter__task_file_port_bindings(struct bpf_iter__task_file *ctx) {
             add_port_bind(&pb, port_bindings);
         }
     } else if (sk->sk_protocol == IPPROTO_UDP) {
-        // TODO skip if in ephemeral port range
         if (sk->__sk_common.skc_state == TCP_CLOSE) {
+            u16 sport = read_sport(sk);
+            if (is_ephemeral_port(sport)) {
+                return 0;
+            }
+
             port_binding_t pb = {};
             pb.netns = get_netns_from_sock(sk);
-            pb.port = read_sport(sk);
+            pb.port = sport;
             add_port_bind(&pb, udp_port_bindings);
         }
     }
