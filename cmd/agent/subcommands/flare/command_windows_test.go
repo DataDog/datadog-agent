7b10dcb4c20d165cabca156766c5c09b9774fbd6
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-04-07 14:40:04 +0000

    fix: remove redundant //go:build test from _test.go files

--- cmd/agent/subcommands/flare/command_windows_test.go
+++ cmd/agent/subcommands/flare/command_windows_test.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2024-present Datadog, Inc.
 
-//go:build test && windows
+//go:build windows
 
 // Package flare implements 'agent flare'.
 package flare
