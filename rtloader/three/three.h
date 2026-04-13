4ccda6aca5215878a3b1cae3621d48794bb02113
Pierre Gimalac <23154723+pgimalac@users.noreply.github.com>
2026-04-13 16:34:40 +0000

    fix: restore changes from other PRs inadvertently reverted

--- rtloader/three/three.h
+++ rtloader/three/three.h
@@ -6,13 +6,16 @@
 #ifndef DATADOG_AGENT_RTLOADER_THREE_H
 #define DATADOG_AGENT_RTLOADER_THREE_H
 
+// Python.h must be included before any system headers, see
+// https://docs.python.org/3/c-api/intro.html#include-files
+#include <Python.h>
+
 #include <atomic>
 #include <map>
 #include <mutex>
 #include <string>
 #include <vector>
 
-#include <Python.h>
 #include <rtloader.h>
 
 class Three : public RtLoader
@@ -59,7 +62,7 @@ public:
     bool getAttrBool(RtLoaderPyObject *obj, const char *attributeName, bool &value) const;
     bool getCheck(RtLoaderPyObject *py_class, const char *init_config_str, const char *instance_str,
                   const char *check_id_str, const char *check_name, const char *agent_config_str,
-                  const char *provider_str, RtLoaderPyObject *&check);
+                  RtLoaderPyObject *&check);
 
     char *runCheck(RtLoaderPyObject *check);
     void cancelCheck(RtLoaderPyObject *check);
