31abdcdce1890f67e55ca1c1e278327310db3ad8
Daniel Lepage <daniel.lepage@datadoghq.com>
2026-04-13 15:33:13 +0200

    Mock ncm component.

--- comp/networkconfigmanagement/mock/mock.go
+++ comp/networkconfigmanagement/mock/mock.go
@@ -3,7 +3,37 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
-//go:build test
-
 // Package mock provides a mock for the networkconfigmanagement component
 package mock
+
+import (
+	"testing"
+
+	"go.uber.org/fx"
+
+	networkconfigmanagement "github.com/DataDog/datadog-agent/comp/networkconfigmanagement/def"
+	ncmstore "github.com/DataDog/datadog-agent/pkg/networkconfigmanagement/store"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+)
+
+type mockNetworkConfigManagement struct {
+	store ncmstore.ConfigStore
+}
+
+// Mock returns a networkconfigmanagement.Component backed by an in-memory store.
+func Mock(_ *testing.T) networkconfigmanagement.Component {
+	return &mockNetworkConfigManagement{store: ncmstore.NewMemStore()}
+}
+
+// MockModule provides the mock as an fx module.
+func MockModule() fxutil.Module {
+	return fxutil.Component(
+		fx.Provide(func() networkconfigmanagement.Component {
+			return &mockNetworkConfigManagement{store: ncmstore.NewMemStore()}
+		}),
+	)
+}
+
+func (m *mockNetworkConfigManagement) GetConfigStore() ncmstore.ConfigStore {
+	return m.store
+}
