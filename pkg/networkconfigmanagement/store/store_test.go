6b37bd8d8ce5264010f7ac36513dfe9798fcaee9
Daniel Lepage <daniel.lepage@datadoghq.com>
2026-04-13 19:10:05 +0200

    Remove block/secret structures for now.

--- pkg/networkconfigmanagement/store/store_test.go
+++ pkg/networkconfigmanagement/store/store_test.go
@@ -26,18 +26,6 @@ enable password cisco123
 username someuser password 0 cg6#107X
 aaa some-model
 `
-var testBlocks = []ConfigBlock{
-	{Type: TextBlock, Value: "hostname retail\nenable password "},
-	{Type: SecretBlock, ID: "secret-1"},
-	{Type: TextBlock, Value: "\nusername someuser password 0 "},
-	{Type: SecretBlock, ID: "secret-2"},
-	{Type: TextBlock, Value: "\naaa some-model"},
-}
-
-var testSecrets = map[string]string{
-	"secret-1": "cisco123",
-	"secret-2": "cg6#107X",
-}
 
 func newTestConfigStore(t *testing.T) *configStore {
 	t.Helper()
@@ -65,25 +53,25 @@ func TestOpen(t *testing.T) {
 func TestStoreConfig(t *testing.T) {
 	t.Run("stores and returns a UUID", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		configUUID, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig, testBlocks, testSecrets)
+		configUUID, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig)
 		require.NoError(t, err)
 		assert.NotEmpty(t, configUUID)
 	})
 
 	t.Run("each call for a device generates a unique UUID", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig, testBlocks, testSecrets)
+		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig)
 		require.NoError(t, err)
-		uuid2, err := cs.StoreConfig("device:10.0.0.2", "running", testRawConfig, testBlocks, testSecrets)
+		uuid2, err := cs.StoreConfig("device:10.0.0.2", "running", testRawConfig)
 		require.NoError(t, err)
 		assert.NotEqual(t, uuid1, uuid2)
 	})
 
 	t.Run("device deduplicate returns UUID of latest config if matches", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig, testBlocks, testSecrets)
+		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig)
 		require.NoError(t, err)
-		uuid2, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig, testBlocks, testSecrets) // the same exact one, should return the first UUID (uuid1)
+		uuid2, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig) // the same exact one, should return the first UUID (uuid1)
 		require.NoError(t, err)
 		assert.Equal(t, uuid1, uuid2)
 	})
@@ -92,15 +80,13 @@ func TestStoreConfig(t *testing.T) {
 func TestGetConfig(t *testing.T) {
 	t.Run("retrieves stored config", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		configUUID, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig, testBlocks, testSecrets)
+		configUUID, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig)
 		require.NoError(t, err)
 
-		rawConfig, blocks, metadata, secrets, err := cs.GetConfig(configUUID)
+		rawConfig, metadata, err := cs.GetConfig(configUUID)
 		require.NoError(t, err)
 
 		assert.Equal(t, testRawConfig, rawConfig)
-		assert.Equal(t, testBlocks, blocks)
-		assert.Equal(t, testSecrets, secrets)
 
 		assert.Equal(t, configUUID, metadata.ConfigUUID)
 		assert.Equal(t, "device:10.0.0.1", metadata.DeviceID)
@@ -113,42 +99,40 @@ func TestGetConfig(t *testing.T) {
 
 	t.Run("returns error for nonexistent UUID", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		_, _, _, _, err := cs.GetConfig("nonexistent-uuid")
+		_, _, err := cs.GetConfig("nonexistent-uuid")
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "not found")
 	})
 
 	t.Run("gets configs by UUID (two different configs)", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", "config-one", nil, map[string]string{"a": "1"})
+		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", "config-one")
 		require.NoError(t, err)
-		uuid2, err := cs.StoreConfig("device:10.0.0.2", "startup", "config-two", nil, map[string]string{"b": "2"})
+		uuid2, err := cs.StoreConfig("device:10.0.0.2", "startup", "config-two")
 		require.NoError(t, err)
 
-		raw1, _, meta1, secrets1, err := cs.GetConfig(uuid1)
+		raw1, meta1, err := cs.GetConfig(uuid1)
 		require.NoError(t, err)
 		assert.Equal(t, "config-one", raw1)
 		assert.Equal(t, "device:10.0.0.1", meta1.DeviceID)
-		assert.Equal(t, map[string]string{"a": "1"}, secrets1)
 
-		raw2, _, meta2, secrets2, err := cs.GetConfig(uuid2)
+		raw2, meta2, err := cs.GetConfig(uuid2)
 		require.NoError(t, err)
 		assert.Equal(t, "config-two", raw2)
 		assert.Equal(t, "device:10.0.0.2", meta2.DeviceID)
-		assert.Equal(t, map[string]string{"b": "2"}, secrets2)
 	})
 }
 
 func TestDeleteConfig(t *testing.T) {
 	t.Run("deletes config from all buckets", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		configUUID, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig, testBlocks, testSecrets)
+		configUUID, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig)
 		require.NoError(t, err)
 
 		err = cs.DeleteConfig(configUUID)
 		require.NoError(t, err)
 
-		_, _, _, _, err = cs.GetConfig(configUUID)
+		_, _, err = cs.GetConfig(configUUID)
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "not found")
 	})
@@ -162,15 +146,15 @@ func TestDeleteConfig(t *testing.T) {
 
 	t.Run("deleting one config does not affect another", func(t *testing.T) {
 		cs := newTestConfigStore(t)
-		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig, testBlocks, testSecrets)
+		uuid1, err := cs.StoreConfig("device:10.0.0.1", "running", testRawConfig)
 		require.NoError(t, err)
-		uuid2, err := cs.StoreConfig("device:10.0.0.2", "running", testRawConfig, testBlocks, testSecrets)
+		uuid2, err := cs.StoreConfig("device:10.0.0.2", "running", testRawConfig)
 		require.NoError(t, err)
 
 		err = cs.DeleteConfig(uuid1)
 		require.NoError(t, err)
 
-		_, _, _, _, err = cs.GetConfig(uuid2)
+		_, _, err = cs.GetConfig(uuid2)
 		require.NoError(t, err)
 	})
 }
