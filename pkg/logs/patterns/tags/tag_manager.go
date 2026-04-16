b5f1b500b4520da7d19c09e90afd8056616c61bb
Jake Saferstein <jake.saferstein@datadoghq.com>
2026-04-16 19:37:46 -0400

    tag

--- pkg/logs/patterns/tags/tag_manager.go
+++ pkg/logs/patterns/tags/tag_manager.go
@@ -153,6 +153,26 @@ func (tm *TagManager) Get(tag string) (uint64, bool) {
 	return tm.GetStringID(tag)
 }
 
+// EvictStaleEntries removes all entries that haven't been accessed within the given TTL.
+// Returns the IDs of evicted entries so callers can send DictEntryDelete messages.
+func (tm *TagManager) EvictStaleEntries(ttl time.Duration) []uint64 {
+	cutoff := time.Now().Add(-ttl)
+
+	tm.mu.Lock()
+	defer tm.mu.Unlock()
+
+	var evictedIDs []uint64
+	for str, entry := range tm.stringToEntry {
+		if entry.lastAccessAt.Before(cutoff) {
+			evictedIDs = append(evictedIDs, entry.id)
+			tm.cachedMemoryBytes.Add(-entry.EstimatedBytes())
+			delete(tm.stringToEntry, str)
+			delete(tm.idToEntry, entry.id)
+		}
+	}
+	return evictedIDs
+}
+
 // Count returns the number of strings in the dictionary
 func (tm *TagManager) Count() int {
 	tm.mu.RLock()
