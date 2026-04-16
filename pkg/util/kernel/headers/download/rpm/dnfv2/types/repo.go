96474283fea5ba17271543f72b2a097c849e8c37
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-04-16 14:01:02 -0700

    add build tags

--- pkg/util/kernel/headers/download/rpm/dnfv2/types/repo.go
+++ pkg/util/kernel/headers/download/rpm/dnfv2/types/repo.go
@@ -3,6 +3,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build linux
+
 package types
 
 // Repomd is a DNFv2 repo data collection
