b223ba29a762aad3b195cf059c68ed15e0344543
Stanley Liu <stanley.liu@datadoghq.com>
2026-04-15 22:30:39 -0400

    Remove envForceInstall

--- test/new-e2e/tests/installer/unix/all_packages_test.go
+++ test/new-e2e/tests/installer/unix/all_packages_test.go
@@ -253,10 +253,6 @@ func (s *packageBaseSuite) RunInstallScript(params ...string) {
 	}
 }
 
-func envForceInstall(pkg string) string {
-	return "DD_INSTALLER_DEFAULT_PKG_INSTALL_" + strings.ToUpper(strings.ReplaceAll(pkg, "-", "_")) + "=true"
-}
-
 func envForceVersion(pkg, version string) string {
 	return "DD_INSTALLER_DEFAULT_PKG_VERSION_" + strings.ToUpper(strings.ReplaceAll(pkg, "-", "_")) + "=" + version
 }
