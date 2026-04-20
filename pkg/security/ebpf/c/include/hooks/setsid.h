c30bad43da3a2a00678b1994a132377c0c727f38
Theo Putegnat <theo.putegnat@datadoghq.com>
2026-04-20 15:14:55 +0200

    Add tail call for centos7

--- pkg/security/ebpf/c/include/hooks/setsid.h
+++ pkg/security/ebpf/c/include/hooks/setsid.h
@@ -4,16 +4,13 @@
 #include "constants/syscall_macro.h"
 #include "helpers/syscalls.h"
 
-HOOK_SYSCALL_ENTRY0(setsid) {
-    return 0;
-}
-
-HOOK_SYSCALL_EXIT(setsid) {
-    int retval = (int)SYSCALL_PARMRET(ctx);
+static int __attribute__((always_inline)) sys_set_sid_ret(void *ctx, int retval) {
+    if (pop_syscall(EVENT_SETSID) == NULL) {
+        return 0;
+    }
     if (retval < 0) {
         return 0;
     }
-
     // After a successful setsid(2), the calling task is the new session leader,
     // so its session ID equals its (host-namespace) PID by definition.
     u32 pid = bpf_get_current_pid_tgid() >> 32;
@@ -21,9 +18,25 @@ HOOK_SYSCALL_EXIT(setsid) {
     if (!pid_entry) {
         return 0;
     }
-
     pid_entry->sid = pid;
     return 0;
 }
 
-#endif // _HOOKS_SETSID_H_
+HOOK_SYSCALL_ENTRY0(setsid) {
+    struct syscall_cache_t syscall = {
+        .type = EVENT_SETSID,
+    };
+    cache_syscall(&syscall);
+    return 0;
+}
+
+TAIL_CALL_TRACEPOINT_FNC(handle_sys_setsid_exit, struct tracepoint_raw_syscalls_sys_exit_t *args) {
+    return sys_set_sid_ret(args, args->ret);
+}
+
+HOOK_SYSCALL_EXIT(setsid) {
+    int retval = (int)SYSCALL_PARMRET(ctx);
+    return sys_set_sid_ret(ctx, retval);
+}
+
+#endif
