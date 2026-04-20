88863144d3824c992f9bf5aee3502d3dca4ded6a
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-04-20 13:19:17 +0200

    Post create command to fix paths, and move claude binary

--- .devcontainer/datadog/default/features/datadog-agent/install.sh
+++ .devcontainer/datadog/default/features/datadog-agent/install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "Hello"
+
+
+# Get claude from the buildimages /root/.local/bin
+cp /root/.local/bin/claude /home/bits/.local/bin/claude
+
+# Configure PATH for interactive shells.
+# File name convention *-workspace-env.sh is important:
+# /etc/zsh/zshenv sources these files.
+cat > /etc/profile.d/10-ddagent-workspace-env.sh << 'EOF'
+export PATH="/usr/local/go/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
+EOF
