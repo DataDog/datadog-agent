3c3c1ac45c177434298ba2941628317915eaa39c
Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>
2026-04-20 16:56:42 +0200

    e2e: retry stack creation on Azure AKS route table not found
    Azure occasionally fails AKS cluster creation with "Failed to get route
    table object ... from resource group ...". Recreating the stack has been
    observed to recover. Add the error to the ReCreate retry list.

--- test/e2e-framework/testing/utils/infra/retriable_errors.go
+++ test/e2e-framework/testing/utils/infra/retriable_errors.go
@@ -86,5 +86,10 @@ func getKnownErrors() []knownError {
 			retryType:    ReCreate,
 			maxRetry:     stackUpMaxRetry,
 		},
+		{
+			errorMessage: `Failed to get route table object .+ from resource group`,
+			retryType:    ReCreate,
+			maxRetry:     stackUpMaxRetry,
+		},
 	}
 }
