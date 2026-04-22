cca075aa5bf04f8e57c8fe13a4108091534cecc1
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-04-22 16:58:35 +0200

    Update post create

--- .devcontainer/datadog/default/features/datadog-agent/lifecycle/postCreate.sh
+++ .devcontainer/datadog/default/features/datadog-agent/lifecycle/postCreate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-
 # Run install tools
 cd ~/dd/datadog-agent
-dda inv install-tools 2>&1 | tee "/home/bits/.install-tools.log"
+
+su bits --login 'dda inv install-tools 2>&1 | tee "/home/bits/.install-tools.log"'
+
