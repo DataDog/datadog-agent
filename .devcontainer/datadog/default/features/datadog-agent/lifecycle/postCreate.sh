5f97d00f4e298a4e778909f7beb15114f0449495
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-04-23 16:28:41 +0200

    Remove su and update image

--- .devcontainer/datadog/default/features/datadog-agent/lifecycle/postCreate.sh
+++ .devcontainer/datadog/default/features/datadog-agent/lifecycle/postCreate.sh
@@ -4,5 +4,4 @@
 # Run install tools
 cd ~/dd/datadog-agent
 
-su bits --login 'dda inv install-tools 2>&1 | tee "/home/bits/.install-tools.log"'
-
+dda inv install-tools 2>&1 | tee "/home/bits/.install-tools.log"
