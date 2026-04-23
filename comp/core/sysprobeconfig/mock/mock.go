302ea301b52eceea76be13bae4becb0a1897398f
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-04-23 13:27:38 +0000

    refactor(core): migrate sysprobeconfig component to v2
    Move Component interface to def/component.go, add fx/fx.go module wrapper
    and mock/mock.go test helper. Update root component.go as backward-compat shim.

---
+++ comp/core/sysprobeconfig/mock/mock.go
@@ -0,0 +1,19 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build test
+
+// Package mock provides a mock for the sysprobeconfig component.
+package mock
+
+import (
+	"github.com/DataDog/datadog-agent/comp/core/sysprobeconfig/sysprobeconfigimpl"
+	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+)
+
+// MockModule defines the fx options for the mock component.
+func MockModule() fxutil.Module {
+	return sysprobeconfigimpl.MockModule()
+}
