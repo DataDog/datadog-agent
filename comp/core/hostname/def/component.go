7f03964233bdc418bd58b827ac63126f8071e12f
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-04-23 12:29:26 +0000

    refactor(core): migrate hostname component to v2
    Add def/ package exposing Component and Data types aliased from hostnameinterface,
    add fx/ package wrapping hostnameimpl.Module(), update root component.go shim to
    point to def/, and add missing BUILD.bazel files for hostnameimpl, def, and fx.

--- comp/core/hostname/def/component.go
+++ comp/core/hostname/def/component.go
@@ -3,12 +3,15 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2023-present Datadog, Inc.
 
-// Package hostname exposes hostname.Get() as a component.
+// Package hostname exposes the hostname component interface.
 package hostname
 
-import def "github.com/DataDog/datadog-agent/comp/core/hostname/def"
+import "github.com/DataDog/datadog-agent/comp/core/hostname/hostnameinterface"
 
 // team: agent-runtimes
 
 // Component is the component type.
-type Component = def.Component
+type Component = hostnameinterface.Component
+
+// Data contains hostname and the hostname provider.
+type Data = hostnameinterface.Data
