5656d54437c9934519e1418f7f57805700a3f698
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-04-23 16:45:02 +0000

    refactor(metadata): migrate inventoryhost component to v2
    - component.go -> def/component.go
    - inventoryhostimpl/ -> impl/
    - Mock moved to mock/mock.go
    - fx.In/Out -> plain Requires/Provides structs
    - Update all callers to use new def and fx packages

--- cmd/agent/subcommands/run/command_windows.go
+++ cmd/agent/subcommands/run/command_windows.go
@@ -72,7 +72,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/metadata/host"
 	"github.com/DataDog/datadog-agent/comp/metadata/inventoryagent"
 	"github.com/DataDog/datadog-agent/comp/metadata/inventorychecks"
-	"github.com/DataDog/datadog-agent/comp/metadata/inventoryhost"
+	inventoryhost "github.com/DataDog/datadog-agent/comp/metadata/inventoryhost/def"
 	"github.com/DataDog/datadog-agent/comp/metadata/packagesigning"
 	"github.com/DataDog/datadog-agent/comp/metadata/runner"
 	netflowServer "github.com/DataDog/datadog-agent/comp/netflow/server"
