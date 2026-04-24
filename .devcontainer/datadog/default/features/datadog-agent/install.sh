a29a88d1012b2f16974a30eb3a54a434989d548a
Kevin Fairise <132568982+KevinFairise2@users.noreply.github.com>
2026-04-24 13:31:40 +0200

    Apply suggestions from code review
    Co-authored-by: Nicolas Schweitzer <nicolas.schweitzer@datadoghq.com>

--- .devcontainer/datadog/default/features/datadog-agent/install.sh
+++ .devcontainer/datadog/default/features/datadog-agent/install.sh
@@ -16,6 +16,5 @@ install -m 755 "$featureDir/lifecycle/postCreate.sh" /opt/doghome/devcontainer/f
 # File name convention *-workspace-env.sh is important:
 # /etc/zsh/zshenv sources these files.
 cat > /etc/profile.d/10-ddagent-workspace-env.sh << 'EOF'
-export PATH="/usr/local/go/bin:$PATH"
-export PATH="$HOME/go/bin:$PATH"
+export PATH="/usr/local/go/bin:$HOME/go/bin:$PATH"
 EOF
