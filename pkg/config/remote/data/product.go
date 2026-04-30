259def71dcfd80cca77364ca9ae5d3f8fdf56247
Remy Mathieu <remy.mathieu@datadoghq.com>
2026-04-30 10:41:19 +0200

    remoteflags: final rc product name

--- pkg/config/remote/data/product.go
+++ pkg/config/remote/data/product.go
@@ -45,7 +45,7 @@ const (
 	// ProductDataStreamsKafkaActions is to execute Kafka actions remotely
 	ProductDataStreamsKafkaActions = "DSM_KAFKA_ACTIONS"
 	// ProductAgentFlags is to receive remote feature flags
-	ProductAgentFlags = "AGENT_FLAGS"
+	ProductAgentFlags = "AGENT_REMOTE_FLAGS"
 	// ProductDOQueryActions is to execute database queries remotely for Data Observability
 	ProductDOQueryActions Product = "DO_QUERY_ACTIONS"
 )
