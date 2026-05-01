ea2c1cc28f6f361facec27dbed60e848961924fc
Seth Samuel <seth.samuel@datadoghq.com>
2026-05-01 13:45:50 +0000

    Add file

---
+++ pkg/collector/python/health_platform.go
@@ -0,0 +1,31 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build python
+
+package python
+
+import (
+	"sync"
+
+	healthplatformdef "github.com/DataDog/datadog-agent/comp/healthplatform/core/def"
+)
+
+var (
+	healthPlatformMu sync.RWMutex
+	healthPlatform   healthplatformdef.Component
+)
+
+// SetHealthPlatform wires the health platform component for CGO callbacks (e.g. datadog_agent.report_issue).
+// Called in collector.go before InitPython / Initialize so Python checks can report issues once the interpreter starts.
+func SetHealthPlatform(h healthplatformdef.Component) {
+	healthPlatformMu.Lock()
+	defer healthPlatformMu.Unlock()
+	healthPlatform = h
+}
+
+func getHealthPlatform() healthplatformdef.Component {
+	return healthPlatform
+}
