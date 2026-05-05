a5d16b9dceba5e0462c41160996bc5e12d084e2b
Celian Raimbault <celian.raimbault@datadoghq.com>
2026-05-05 17:55:01 +0200

    Fixed component issues

--- comp/anomalydetection/observer/hfrunner/fx/fx.go
+++ comp/anomalydetection/observer/hfrunner/fx/fx.go
@@ -7,15 +7,26 @@
 package fx
 
 import (
+	hfrunnerdef "github.com/DataDog/datadog-agent/comp/anomalydetection/observer/hfrunner/def"
 	hfrunnerimpl "github.com/DataDog/datadog-agent/comp/anomalydetection/observer/hfrunner/impl"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
+	"github.com/DataDog/datadog-agent/pkg/util/option"
+	"go.uber.org/fx"
 )
 
 // Module defines the fx options for this component.
+// We construct fxutil.Module directly because fxutil.Component uses runtime call-stack
+// inspection that only supports paths up to 5 levels deep from comp/, and this
+// component lives at comp/anomalydetection/observer/hfrunner (6 levels deep).
 func Module() fxutil.Module {
-	return fxutil.Component(
-		fxutil.ProvideComponentConstructor(
-			hfrunnerimpl.NewComponent,
-		),
-	)
+	opts := []fx.Option{
+		fxutil.ProvideComponentConstructor(hfrunnerimpl.NewComponent),
+		fx.Provide(func(c hfrunnerdef.Component) option.Option[hfrunnerdef.Component] {
+			return option.New(c)
+		}),
+	}
+	return fxutil.Module{
+		Option:  fx.Module("comp/anomalydetection/observer/hfrunner", opts...),
+		Options: opts,
+	}
 }
