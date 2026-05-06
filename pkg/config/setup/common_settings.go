58d1ae0a0d8497a5e082b73397b5ab94fed2dab3
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2026-05-06 20:41:00 +0200

    AGTMETRICS-511 Sampled shadow for metrics series
    A small sample of series metric flushes (0.1% by default) is now
    additionally sent to a v3beta metrics intake endpoint to validate the
    upcoming v3 metrics protocol.

--- pkg/config/setup/common_settings.go
+++ pkg/config/setup/common_settings.go
@@ -1523,6 +1523,8 @@ func serializer(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("serializer_experimental_use_v3_api.compression_level", 0)
 	config.BindEnvAndSetDefault("serializer_experimental_use_v3_api.series.use_beta", false)
 	config.BindEnvAndSetDefault("serializer_experimental_use_v3_api.series.beta_route", "/api/intake/metrics/v3beta/series")
+	config.BindEnvAndSetDefault("serializer_experimental_use_v3_api.series.shadow_sample_rate", 0.001)
+	config.BindEnvAndSetDefault("serializer_experimental_use_v3_api.series.shadow_sites", []string{"datadoghq.com"})
 
 	config.BindEnvAndSetDefault("use_v2_api.series", true)
 	// Serializer: allow user to blacklist any kind of payload to be sent
