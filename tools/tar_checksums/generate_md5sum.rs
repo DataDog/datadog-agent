01c4d3a03287b24be77e19c8c86e1f4cf5cb3be5
Tony Aiuto <tony.aiuto@datadoghq.com>
2026-05-06 23:43:01 -0400

    [ABLD-457] Add a bazel rule to create md5sums files from a tar file.
    For debian packaging, we need to include a file of md5 sums of all the files in the data payload
    as the file md5sums in the control payload.
    
    This makes that available as a rule. I'll plug it into a wrapper for pkg_deb in a follow-up.

---
+++ tools/tar_checksums/generate_md5sum.rs
@@ -0,0 +1,76 @@
+// Read a tarball and emit a file of the md5 checksums of all the plain files in it.
+use std::env;
+use std::fs;
+use std::io::{self, Read, Write};
+use std::thread;
+
+use flate2::read::GzDecoder;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let (tar_file_path, output_path): (Option<String>, String) = match args.len() {
+        2 => (None, args[1].clone()),
+        3 => (Some(args[1].clone()), args[2].clone()),
+        _ => {
+            eprintln!("Usage: {} [input.tar[.gz|.tgz|.xz]] <output>", args[0]);
+            std::process::exit(1);
+        }
+    };
+
+    // Wrap the input file in a decompressor stream depending on the file extension.
+    let reader: Box<dyn Read> = match &tar_file_path {
+        None => Box::new(io::stdin()),
+        Some(path) => {
+            let file = fs::File::open(path)?;
+            if path.ends_with(".gz") || path.ends_with(".tgz") {
+                Box::new(GzDecoder::new(file))
+            } else if path.ends_with(".xz") {
+                /* We are using lzma-rs because it is pure rust, but it does not
+                 * have a streaming interface. So put it in a separate thread
+                 * and make use the write end of that thread as our read end.
+                 */
+                let (pipe_reader, pipe_writer) = io::pipe()?;
+                thread::spawn(move || {
+                    let mut reader = io::BufReader::new(file);
+                    let mut writer = pipe_writer;
+                    if let Err(e) = lzma_rs::xz_decompress(&mut reader, &mut writer) {
+                        eprintln!("XZ decompression error: {e}");
+                    }
+                });
+                Box::new(pipe_reader)
+            } else {
+                Box::new(file)
+            }
+        }
+    };
+
+    let mut archive = tar::Archive::new(reader);
+    let mut output = fs::File::create(&output_path)?;
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        if !entry.header().entry_type().is_file() {
+            continue;
+        }
+
+        let path = entry.path()?.into_owned();
+        let path_str = path.to_string_lossy();
+        let path_str = path_str.strip_prefix("./").unwrap_or(&path_str);
+
+        // pump the content of the file into the md5 hasher.
+        let mut ctx = md5::Context::new();
+        let mut buf = [0u8; 65536];
+        loop {
+            let n = entry.read(&mut buf)?;
+            if n == 0 {
+                break;
+            }
+            ctx.consume(&buf[..n]);
+        }
+        let digest = ctx.finalize();
+
+        writeln!(output, "{:x}  {}", digest, path_str)?;
+    }
+
+    Ok(())
+}
