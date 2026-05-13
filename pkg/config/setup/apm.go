89f2b93f28f85bd5036264026de4897ab18911a9
paullegranddc <paul.legranddescloizeaux@datadoghq.com>
2026-05-13 18:55:37 +0200

    feat(trace-agent): implementing telemetry batching in the agent
    # Motivation
    
    Telemetry are quite small, and sent frequently. We want to reduce the volume sent to the intake
    
    # Changes
    
    * Add an aggregator for telemetry messages
    * Sent batched telemetry payloads a new envelope format.
    * Use zstd to compress the payloads

--- pkg/config/setup/apm.go
+++ pkg/config/setup/apm.go
@@ -141,6 +141,8 @@ func setupAPM(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("apm_config.install_id", "", "DD_INSTRUMENTATION_INSTALL_ID")
 	config.BindEnvAndSetDefault("apm_config.install_type", "", "DD_INSTRUMENTATION_INSTALL_TYPE")
 	config.BindEnvAndSetDefault("apm_config.install_time", 0, "DD_INSTRUMENTATION_INSTALL_TIME")
+	config.BindEnvAndSetDefault("apm_config.telemetry.batch_size_threshold_bytes", 4_000_000, "DD_APM_TELEMETRY_BATCH_SIZE_THRESHOLD_BYTES")
+	config.BindEnvAndSetDefault("apm_config.telemetry.max_inflight_memory", 20_000_000, "DD_APM_TELEMETRY_MAX_INFLIGHT_MEMORY")
 	config.BindEnvAndSetDefault("apm_config.obfuscation.credit_cards.enabled", true, "DD_APM_OBFUSCATION_CREDIT_CARDS_ENABLED")
 	config.BindEnvAndSetDefault("apm_config.obfuscation.credit_cards.luhn", false, "DD_APM_OBFUSCATION_CREDIT_CARDS_LUHN")
 	config.BindEnvAndSetDefault("apm_config.obfuscation.credit_cards.keep_values", []string{}, "DD_APM_OBFUSCATION_CREDIT_CARDS_KEEP_VALUES")
