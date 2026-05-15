da85415b02815968580bbe272ca56995a40f3908
s-alad <saad.naji@datadoghq.com>
2026-05-15 15:55:50 -0400

    add else statement in ntm

--- pkg/config/nodetreemodel/config.go
+++ pkg/config/nodetreemodel/config.go
@@ -637,9 +637,7 @@ func (c *ntmConfig) insertNodeFromString(curr *nodeImpl, key string, envval stri
 	var actualValue interface{} = envval
 	if transformer, found := c.envTransform[key]; found {
 		actualValue = transformer(envval)
-	}
-
-	if defaultNode := c.leafAtPathFromNode(key, c.defaults); defaultNode != missingLeaf {
+	} else if defaultNode := c.leafAtPathFromNode(key, c.defaults); defaultNode != missingLeaf {
 		if converted, err := convertToDefaultType(actualValue, defaultNode.Get()); err == nil {
 			actualValue = converted
 		}
