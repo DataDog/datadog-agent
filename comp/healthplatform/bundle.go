e74d098e2df00caf67008eca49479a60a5f3342d
s-alad <saad.naji@datadoghq.com>
2026-05-15 16:44:48 -0400

    stub pattern for jetson/iot issue

--- comp/healthplatform/bundle.go
+++ comp/healthplatform/bundle.go
@@ -23,16 +23,14 @@ import (
 	egressfx "github.com/DataDog/datadog-agent/comp/healthplatform/egress/fx"
 	forwarderfx "github.com/DataDog/datadog-agent/comp/healthplatform/forwarder/fx"
 
-	// Import issue modules to trigger their init() registration.
-	// The bundle is the correct place for side-effect imports; impl packages
-	// must not import other impl packages.
-	// invalidconfig is registered conditionally in bundle_invalidconfig.go (excluded from IoT).
+	// Issue modules register themselves via init(); imported here for side effects.
 	registrydef "github.com/DataDog/datadog-agent/comp/healthplatform/issueregistry/def"
 	registryfx "github.com/DataDog/datadog-agent/comp/healthplatform/issueregistry/fx"
 	_ "github.com/DataDog/datadog-agent/comp/healthplatform/issues/admisconfig"       // registers templates via init()
 	_ "github.com/DataDog/datadog-agent/comp/healthplatform/issues/admissionprobe"    // registers templates via init()
 	_ "github.com/DataDog/datadog-agent/comp/healthplatform/issues/checkfailure"      // registers templates via init()
 	_ "github.com/DataDog/datadog-agent/comp/healthplatform/issues/dockerpermissions" // registers templates via init()
+	_ "github.com/DataDog/datadog-agent/comp/healthplatform/issues/invalidconfig"     // registers templates via init()
 	_ "github.com/DataDog/datadog-agent/comp/healthplatform/issues/rofspermissions"   // registers templates via init()
 	runnerdef "github.com/DataDog/datadog-agent/comp/healthplatform/runner/def"
 	runnerfx "github.com/DataDog/datadog-agent/comp/healthplatform/runner/fx"
---
+++ comp/healthplatform/issues/invalidconfig/stub.go
@@ -0,0 +1,9 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025-present Datadog, Inc.
+
+//go:build jetson
+
+// Empty stub keeps the package importable on IoT Agent (jetson) builds
+package invalidconfig
