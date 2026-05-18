d3ce89f17221dee5936831ecec174ffbeb76dc76
louis-cqrl <louis.coquerelle@datadoghq.com>
2026-05-18 11:16:39 +0000

    fix(demultiplexer): fix buildifier, lint_components, and missing FakeSamplerMock in def
    - Fix buildifier formatting in impl/BUILD.bazel (sort srcs and deps alphabetically)
    - Add comp/aggregator/demultiplexer to ignore_fx_import in tasks/components.py
      (demultiplexer_fake_sampler_mock.go has //go:build test but lint treats it as impl)
    - Add comp/aggregator/demultiplexer to ignore_provide_component_constructor_missing
      (fx.go delegates to impl.Module which uses an unexported constructor)
    - Add def/component_mock.go with Mock and FakeSamplerMock interfaces (moved from
      root component_mock.go); impl/demultiplexer_fake_sampler_mock.go references
      demultiplexerComp.FakeSamplerMock from def/ which was missing
    - Update def/BUILD.bazel to include component_mock.go and pkg/metrics dep
    - Simplify mock/mock.go and mock/BUILD.bazel (remove duplicate type declarations
      now that they live in def/)

--- comp/aggregator/demultiplexer/mock/mock.go
+++ comp/aggregator/demultiplexer/mock/mock.go
@@ -9,37 +9,9 @@
 package mock
 
 import (
-	"time"
-
-	demultiplexerComp "github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/def"
 	demultiplexerimpl "github.com/DataDog/datadog-agent/comp/aggregator/demultiplexer/impl"
-	"github.com/DataDog/datadog-agent/pkg/aggregator"
-	"github.com/DataDog/datadog-agent/pkg/aggregator/sender"
-	"github.com/DataDog/datadog-agent/pkg/metrics"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
-
-// Mock implements mock-specific methods.
-type Mock interface {
-	SetDefaultSender(sender.Sender)
-	Stop(bool)
-	demultiplexerComp.Component
-}
-
-// FakeSamplerMock is an implementation of the Demultiplexer which is sending
-// the time samples into a fake sampler, you can then use WaitForSamples() to retrieve
-// the samples that the TimeSamplers should have received.
-type FakeSamplerMock interface {
-	aggregator.DemultiplexerWithAggregator
-
-	WaitForSamples(timeout time.Duration) (ontime []metrics.MetricSample, timed []metrics.MetricSample)
-	WaitForNumberOfSamples(ontimeCount, timedCount int, timeout time.Duration) (ontime []metrics.MetricSample, timed []metrics.MetricSample)
-	Reset()
-
-	GetAgentDemultiplexer() *aggregator.AgentDemultiplexer
-
-	Stop(bool)
-}
 
 // Module defines the fx options for the mock component.
 func Module() fxutil.Module {
