3ffdecf0d5eb21f391ab84177169a584bcb0ba5f
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2026-05-21 15:19:49 +0200

    deps: cpython: rpath-patch lib-dynload via dd_cc_packaged

--- omnibus/config/software/python3.rb
+++ omnibus/config/software/python3.rb
@@ -14,11 +14,9 @@ build do
   if !windows_target?
     env = with_standard_compiler_flags(with_embedded_path)
     command_on_repo_root "bazelisk run --//:install_dir=#{install_dir} -- @cpython//:install --destdir='#{install_dir}'"
-    sh_ext = if linux_target? then "so" else "dylib" end
+    # Libraries and binaries are rpath-patched by dd_cc_packaged in cpython.BUILD.bazel;
+    # this call is now only for the ##PREFIX## text substitution in _sysconfigdata.
     command_on_repo_root "bazelisk run --//:install_dir=#{install_dir} -- //bazel/rules:replace_prefix --prefix '#{install_dir}/embedded'" \
-      " #{install_dir}/embedded/lib/libpython3.*#{sh_ext}" \
-      " #{install_dir}/embedded/lib/python3.*/lib-dynload/*.so" \
-      " #{install_dir}/embedded/bin/python3*" \
       " #{install_dir}/embedded/lib/python3.*/_sysconfigdata__*.py"
     python = "#{install_dir}/embedded/bin/python3"
   else
