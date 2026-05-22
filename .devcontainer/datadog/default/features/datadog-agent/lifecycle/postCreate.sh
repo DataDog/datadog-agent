99bb596fd7b6fcbd50b132cfd162c33eacaaf8f0
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-05-22 14:11:52 +0200

    Small improvements

--- .devcontainer/datadog/default/features/datadog-agent/lifecycle/postCreate.sh
+++ .devcontainer/datadog/default/features/datadog-agent/lifecycle/postCreate.sh
@@ -14,6 +14,8 @@ configure_claude_mcp() {
 
 repo_dir="${HOME}/go/src/github.com/DataDog/datadog-agent"
 
+ln -s "${HOME}/go/src/github.com/DataDog" "${HOME}/dd"
+
 if [[ ! -d "${repo_dir}" ]]; then
     echo "Unable to find datadog-agent checkout at ${repo_dir}" >&2
     exit 1
@@ -23,4 +25,4 @@ configure_claude_mcp
 
 cd "${repo_dir}"
 
-dda inv install-tools 2>&1 | tee "${HOME}/.install-tools.log"
+DDA_NO_DYNAMIC_DEPS=0 dda inv install-tools 2>&1 | tee "${HOME}/.install-tools.log"
