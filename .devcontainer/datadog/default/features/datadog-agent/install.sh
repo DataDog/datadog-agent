35a96c44d45fa1dd4307fe65239bb95bcea84cfd
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-05-25 19:41:26 +0200

    Some more update, install ddtool at devcontainer build time, without dotslash

--- .devcontainer/datadog/default/features/datadog-agent/install.sh
+++ .devcontainer/datadog/default/features/datadog-agent/install.sh
@@ -12,6 +12,21 @@ fi
 install -d /opt/doghome/devcontainer/features/datadog-agent/lifecycle
 install -m 755 "$featureDir/lifecycle/postCreate.sh" /opt/doghome/devcontainer/features/datadog-agent/lifecycle/postCreate.sh
 
+# Fetch update-tool and use it to install ddtool. Ideally we'd like to use dotslash but it does not work well with ddtool auth helpers install because of shims.
+curl --no-progress-meter --retry 10 --retry-max-time 60 -Lo /usr/local/bin/update-tool\
+     https://binaries.ddbuild.io/devtools/bin/update-tool
+chmod +x /usr/local/bin/update-tool
+
+su - bits <<EOF
+mkdir -p ~/.local/bin
+update-tool ddtool@1.101.0
+
+ddtool auth helpers install
+
+# Activate xdg-open functionality
+ln -s workspaces-tool-helper ~/.local/bin/xdg-open
+EOF
+
 # Configure PATH for interactive shells.
 # File name convention *-workspace-env.sh is important:
 # /etc/zsh/zshenv sources these files.
