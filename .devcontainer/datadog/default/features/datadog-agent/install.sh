2e08816f3e0a72394712fdf327ef709e2bbb86ea
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-05-26 09:22:47 +0200

    Try with ddtool fix

--- .devcontainer/datadog/default/features/datadog-agent/install.sh
+++ .devcontainer/datadog/default/features/datadog-agent/install.sh
@@ -12,19 +12,12 @@ fi
 install -d /opt/doghome/devcontainer/features/datadog-agent/lifecycle
 install -m 755 "$featureDir/lifecycle/postCreate.sh" /opt/doghome/devcontainer/features/datadog-agent/lifecycle/postCreate.sh
 
-# Fetch update-tool and use it to install ddtool. Ideally we'd like to use dotslash but it does not work well with ddtool auth helpers install because of shims.
-curl --no-progress-meter --retry 10 --retry-max-time 60 -Lo /usr/local/bin/update-tool\
-     https://binaries.ddbuild.io/devtools/bin/update-tool
-chmod +x /usr/local/bin/update-tool
 
 su - bits <<EOF
 export PATH="~/.local/bin:\$PATH"
 (umask 077 && mkdir -p ~/.local/state/workspaces)
 touch ~/state-done
-mkdir -p ~/.local/bin
-touch ~/bin-done
-update-tool ddtool@1.101.0 > ~/ddtool-install.log 2>&1
-touch ~/ddtool-done
+
 
 ddtool auth helpers install > ~/helpers-install.log 2>&1
 touch ~/helpers-done
