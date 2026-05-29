b68cde60ba050a81a417f6cd110f35758b8ead16
Dan Lepage <140522866+dplepage-dd@users.noreply.github.com>
2026-05-29 18:21:06 -0400

    [NCM] Stub of rollback PAR action (#51126)
    ### What does this PR do?
    
    This implements the PAR side of the NCM rollback action (the NCM side just returns "not implemented")
    
    ### Motivation
    
    I wanted to split this into a separate PR to make review easier for action-platform and container-platform (there's no reason they should have to wade through the actual rollback implementation).
    
    ### Describe how you validated your changes
    
    Running the agent and action runner locally
    
    ### Additional Notes
    
    
    Co-authored-by: daniel.lepage <daniel.lepage@datadoghq.com>

--- pkg/networkconfigmanagement/remote/interfaces.go
+++ pkg/networkconfigmanagement/remote/interfaces.go
@@ -6,7 +6,11 @@
 // Package remote provides interfaces for remote device communications (SSH/Telnet) to retrieve configurations
 package remote
 
-import "github.com/DataDog/datadog-agent/pkg/networkconfigmanagement/profile"
+import (
+	"context"
+
+	"github.com/DataDog/datadog-agent/pkg/networkconfigmanagement/profile"
+)
 
 // Client defines the interface for a remote client that can create sessions to execute commands on a device
 type Client interface {
@@ -14,6 +18,7 @@ type Client interface {
 	NewSession() (Session, error)
 	RetrieveRunningConfig() ([]byte, error)
 	RetrieveStartupConfig() ([]byte, error)
+	PushConfig(ctx context.Context, config string) error
 	SetProfile(p *profile.NCMProfile)
 	Close() error
 }
