9cc6e57fd83d7596cfcd161037410bd9279d1ee4
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2026-05-30 09:16:01 +0200

    Deduplicate process_manager.proto to a single canonical location.
    Rust Bazel and cargo builds now consume pkg/proto/datadog/procmgr/process_manager.proto instead of maintaining a duplicate copy under pkg/procmgr/rust/proto.

--- pkg/procmgr/rust/build.rs
+++ pkg/procmgr/rust/build.rs
@@ -9,6 +9,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     tonic_prost_build::configure()
         .file_descriptor_set_path(out_dir.join("process_manager_descriptor.bin"))
-        .compile_protos(&["proto/process_manager.proto"], &["proto"])?;
+        .compile_protos(
+            &["../../proto/datadog/procmgr/process_manager.proto"],
+            &["../../proto"],
+        )?;
     Ok(())
 }
