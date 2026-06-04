b280d66b9c8232b555d1a33d3de791d87dc14adb
Jake Saferstein <jake.saferstein@datadoghq.com>
2026-06-04 14:12:23 -0400

    add applyRoutingHeaders for per-endpoint grpc/http routing

---
+++ comp/logs-library/pipeline/nodeless.go
@@ -0,0 +1,40 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package pipeline
+
+import (
+	"github.com/DataDog/datadog-agent/comp/logs/agent/config"
+)
+
+// applyRoutingHeaders sets x-dd-logs-routing on each endpoint based on its own
+// transport: "grpc" for gRPC endpoints, "http" for HTTP endpoints.
+func applyRoutingHeaders(endpoints *config.Endpoints) {
+	var mainRoutingValue string
+	if endpoints.UseGRPC {
+		mainRoutingValue = "grpc"
+	} else if endpoints.UseHTTP {
+		mainRoutingValue = "http"
+	} else {
+		return
+	}
+
+	if endpoints.Main.ExtraHTTPHeaders == nil {
+		endpoints.Main.ExtraHTTPHeaders = map[string]string{}
+	}
+	endpoints.Main.ExtraHTTPHeaders["x-dd-logs-routing"] = mainRoutingValue
+
+	for i := range endpoints.Endpoints {
+		ep := &endpoints.Endpoints[i]
+		if ep.ExtraHTTPHeaders == nil {
+			ep.ExtraHTTPHeaders = map[string]string{}
+		}
+		if ep.UseGRPC {
+			ep.ExtraHTTPHeaders["x-dd-logs-routing"] = "grpc"
+		} else {
+			ep.ExtraHTTPHeaders["x-dd-logs-routing"] = mainRoutingValue
+		}
+	}
+}
