dfe18b32f5518b875916b7bfb1e8d2346b9fc2ff
Gabriel Dos Santos <gabe.dossantos@datadoghq.com>
2026-06-08 18:02:28 -0400

    Basic stickiness

--- pkg/clusteragent/clusterchecks/checks_distribution.go
+++ pkg/clusteragent/clusterchecks/checks_distribution.go
@@ -69,7 +69,7 @@ func newConfigsDistribution(workersPerRunner map[string]int) configsDistribution
 // is not among the runners with the lowest utilization, it gives precedence to
 // the runner with the lowest number of configs deployed. excludeRunner can be
 // set to avoid assigning a config to a specific runner.
-func (distribution *configsDistribution) leastBusyRunner(preferredRunner string, excludeRunner string) string {
+func (distribution *configsDistribution) leastBusyRunner(preferredRunner string, excludeRunner string, workersNeeded float64) string {
 	leastBusyRunner := ""
 	minUtilization := 0.0
 	numChecksLeastBusyRunner := 0
@@ -81,6 +81,10 @@ func (distribution *configsDistribution) leastBusyRunner(preferredRunner string,
 
 		runnerUtilization := runnerStatus.utilization()
 		runnerNumChecks := runnerStatus.NumChecks
+
+		if runnerName == preferredRunner {
+			runnerUtilization -= max(workersNeeded*4, 2)
+		}
 
 		selectRunner := (leastBusyRunner == "") ||
 			(runnerUtilization < minUtilization) ||
@@ -98,7 +102,7 @@ func (distribution *configsDistribution) leastBusyRunner(preferredRunner string,
 }
 
 func (distribution *configsDistribution) addToLeastBusy(digest, checkName string, workersNeeded float64, preferredRunner string, excludeRunner string, pinned bool) {
-	leastBusy := distribution.leastBusyRunner(preferredRunner, excludeRunner)
+	leastBusy := distribution.leastBusyRunner(preferredRunner, excludeRunner, workersNeeded)
 	if leastBusy == "" {
 		return
 	}
