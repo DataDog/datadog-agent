b6aa3e2d47800adaa6b27c4a0ae2712c61d20cb5
Garrison Stauffer <garrison.stauffer@datadoghq.com>
2026-06-08 15:11:41 -0400

    remove octothorpe from file names

--- pkg/networkconfigmanagement/profile/default_profiles_test.go
+++ pkg/networkconfigmanagement/profile/default_profiles_test.go
@@ -77,13 +77,13 @@ func Test_DefaultProfiles_Running(t *testing.T) {
 		{
 			name:                      "TMOS ltm config checks",
 			profile:                   DefaultProfile(t, "tmos"),
-			fixture:                   loadFixture("tmos", "running#ltm"),
+			fixture:                   loadFixture("tmos", "running_ltm"),
 			expectedExtractedMetadata: &ExtractedMetadata{},
 		},
 		{
 			name:                      "TMOS tmsh version checks",
 			profile:                   DefaultProfile(t, "tmos"),
-			fixture:                   loadFixture("tmos", "running#tmsh-version"),
+			fixture:                   loadFixture("tmos", "running_tmsh-version"),
 			expectedExtractedMetadata: &ExtractedMetadata{},
 		},
 		{
