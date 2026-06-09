0aa178f07a1529d9d0b46b49d7a5c35ad879164b
Tony Aiuto <tony.aiuto@datadoghq.com>
2026-06-09 13:28:34 -0400

    linty

--- tasks/libs/common/get_payload_version.py
+++ tasks/libs/common/get_payload_version.py
@@ -23,9 +23,7 @@ def get_payload_version(go_mod_path="go.mod"):
             # or   "github.com/DataDog/agent-payload/v5 v5.0.1-0.20200826134834-1ddcfb686e3f"
             version_split = re.split(r'[ +]', line)
             if len(version_split) < 2:
-                raise Exception(
-                    "Versioning of agent-payload in go.mod has changed; update the version logic"
-                )
+                raise Exception("Versioning of agent-payload in go.mod has changed; update the version logic")
             version = version_split[1].split("-")[0].strip()
             if not re.search(r"^v\d+(\.\d+){2}$", version):
                 raise Exception(f"Version of agent-payload in go.mod is invalid: '{version}'")
