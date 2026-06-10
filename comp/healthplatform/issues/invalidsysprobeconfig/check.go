f0f9d32b9fabf716c3099be8fdd4fa6ba2959ce9
s-alad <saad.naji@datadoghq.com>
2026-06-10 18:25:48 -0400

    consolidate with main

--- comp/healthplatform/issues/invalidsysprobeconfig/check.go
+++ comp/healthplatform/issues/invalidsysprobeconfig/check.go
@@ -3,8 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
-//go:build !jetson
-
 // Package invalidsysprobeconfig reports system-probe.yaml schema violations through the Agent Health Platform
 package invalidsysprobeconfig
 
