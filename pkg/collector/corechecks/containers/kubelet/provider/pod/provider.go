b9b8a8c4999dc0c70c9b92c289e8c4046a6fa721
Alexandre Lavigne <alexandre.lavigne@datadoghq.com>
2026-06-11 16:56:05 +0200

    update requests too to reflect similar metric name
    Signed-off-by: Alexandre Lavigne <alexandre.lavigne@datadoghq.com>

--- pkg/collector/corechecks/containers/kubelet/provider/pod/provider.go
+++ pkg/collector/corechecks/containers/kubelet/provider/pod/provider.go
@@ -176,7 +176,7 @@ func (p *Provider) generatePodSpecMetrics(sender sender.Sender, pod *workloadmet
 			continue
 		}
 
-		sender.Gauge(common.KubeletMetricsPrefix+"pod."+r+".request", quantity.AsApproximateFloat64(), "", tagList)
+		sender.Gauge(common.KubeletMetricsPrefix+"pod."+r+".requests", quantity.AsApproximateFloat64(), "", tagList)
 	}
 
 	for r, value := range pod.Resources.RawLimits {
