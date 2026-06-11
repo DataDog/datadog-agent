38ffd956d65f7d058ad6d25c22cab01cc9c1c82a
Alexandre Pedone <alexandre.pedone@datadoghq.com>
2026-06-11 16:36:32 -0400

    (OTEL-3043) add OTEL-3079 reference for DDOT scope convention follow-up

--- cmd/otel-agent/config/agent_config.go
+++ cmd/otel-agent/config/agent_config.go
@@ -243,11 +243,8 @@ func NewConfigComponent(ctx context.Context, ddCfg string, uris []string) (confi
 		if !ddfg.OperationAndResourceNameV2FeatureGate.IsEnabled() {
 			apmConfigFeatures = append(apmConfigFeatures, "disable_operation_and_resource_name_logic_v2")
 		}
-		// if !ddfg.ScopeConventionFeatureGate.IsEnabled() {
-		// 	apmConfigFeatures = append(apmConfigFeatures, "disable_scope_convention")
-		// }
-		// TODO: re-enable this when the PR in contrib adding the feature gate is merged,
-		//  and the otel-agent (DDOT) is updated to use the new version of the package.
+		// TODO: (OTEL-3079) Set disable_scope_convention 
+		// Agent feature based on feature gate after upgrading to Collector 0.155.0
 
 		if ddc.Traces.ComputeTopLevelBySpanKind {
 			apmConfigFeatures = append(apmConfigFeatures, "enable_otlp_compute_top_level_by_span_kind")
