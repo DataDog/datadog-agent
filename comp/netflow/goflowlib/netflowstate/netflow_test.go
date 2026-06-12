66f8851b92a810465118d6010e835c78a5a027c4
Julie Wang <jules.wang@datadoghq.com>
2026-06-12 11:19:53 -0400

    revert netflow_test in preparation for bug fix
    Signed-off-by: Julie Wang <jules.wang@datadoghq.com>

--- comp/netflow/goflowlib/netflowstate/netflow_test.go
+++ comp/netflow/goflowlib/netflowstate/netflow_test.go
@@ -6,78 +6,26 @@
 package netflowstate
 
 import (
-	"bytes"
 	"context"
-	"encoding/binary"
+	"github.com/DataDog/datadog-agent/comp/netflow/testutil"
+	"github.com/netsampler/goflow2/decoders/netflow/templates"
+	promtestutil "github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/assert"
+
+	// install the in-memory template manager
+	_ "github.com/netsampler/goflow2/decoders/netflow/templates/memory"
+	"github.com/netsampler/goflow2/utils"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 	"net"
 	"testing"
 	"time"
-
-	"github.com/netsampler/goflow2/decoders/netflow/templates"
-	_ "github.com/netsampler/goflow2/decoders/netflow/templates/memory"
-	"github.com/netsampler/goflow2/utils"
-	promtestutil "github.com/prometheus/client_golang/prometheus/testutil"
-	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	"github.com/DataDog/datadog-agent/comp/netflow/common"
-	"github.com/DataDog/datadog-agent/comp/netflow/testutil"
 )
 
-type mockedFormatDriver struct {
-	messages []*common.FlowMessageWithAdditionalFields
-}
+type mockedFormatDriver struct{}
 
-func (m *mockedFormatDriver) Format(data interface{}) ([]byte, []byte, error) {
-	if msg, ok := data.(*common.FlowMessageWithAdditionalFields); ok {
-		m.messages = append(m.messages, msg)
-	}
+func (m *mockedFormatDriver) Format(_ interface{}) ([]byte, []byte, error) {
 	return nil, nil, nil
-}
-
-// TestDecodeFlow_NSELBiflowFields documents that the agent's DecodeFlow
-// fails to process IDs 231/232/298/299 (Cisco NSEL biflow fields), so
-// FlowMessage.Bytes and FlowMessage.Packets remain 0 after processing.
-func TestDecodeFlow_NSELBiflowFields(t *testing.T) {
-	ctx := context.Background()
-	templateSystem, err := templates.FindTemplateSystem(ctx, "memory")
-	require.NoError(t, err)
-	defer templateSystem.Close(ctx)
-
-	// example nfv9 packet
-	var pkt bytes.Buffer
-	for _, v := range []interface{}{
-		// NFv9 header
-		uint16(9), uint16(2), uint32(10), uint32(10), uint32(1), uint32(0),
-		// template flowset (id=0, len=24): template 263, 4 fields (initiatorOctets, responderOctets, initiatorPackets, responderPackets)
-		uint16(0), uint16(24), uint16(263), uint16(4),
-		uint16(231), uint16(8), uint16(232), uint16(8), uint16(298), uint16(8), uint16(299), uint16(8),
-		// data flowset (id=263, len=36): one record
-		uint16(263), uint16(36),
-		uint64(10340), uint64(12885), uint64(20), uint64(20),
-	} {
-		binary.Write(&pkt, binary.BigEndian, v)
-	}
-
-	driver := &mockedFormatDriver{}
-	state := NewStateNetFlow(nil)
-	state.Format = driver
-	state.Logger = logrus.StandardLogger()
-	state.TemplateSystem = templateSystem
-
-	err = state.DecodeFlow(utils.BaseMessage{Src: net.ParseIP("1.1.1.1"), Port: 2055, Payload: pkt.Bytes()})
-	require.NoError(t, err)
-
-	msg := driver.messages[0].FlowMessage
-	assert.Equal(t, uint64(0), msg.Bytes, "goflow2 does not handle initiatorOctets (231) or responderOctets (232)")
-	assert.Equal(t, uint64(0), msg.Packets, "goflow2 does not handle initiatorPackets (298) or responderPackets (299)")
-
-	utils.NetFlowStats.Reset()
-	utils.NetFlowSetStatsSum.Reset()
-	utils.NetFlowSetRecordsStatsSum.Reset()
-	utils.NetFlowTimeStatsSum.Reset()
-	utils.DecoderTime.Reset()
 }
 
 func TestNetflowState_TelemetryMetrics(t *testing.T) {
