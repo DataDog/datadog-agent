e6fa035308db01db2309d46176682657a9cf8805
Sylvain Baubeau <sylvain.baubeau@datadoghq.com>
2026-06-12 14:51:00 +0200

    Fix functional tests

--- pkg/security/tests/flow_pid_bind_test.go
+++ pkg/security/tests/flow_pid_bind_test.go
@@ -1021,7 +1021,7 @@ func TestMultipleProtocols(t *testing.T) {
 			return
 		}
 
-		m, _, err := p.Manager.GetMap("flow_pid")
+		m, _, err := p.Manager.Get().GetMap("flow_pid")
 		if err != nil {
 			t.Fatalf("failed to get map flow_pid: %v", err)
 		}
