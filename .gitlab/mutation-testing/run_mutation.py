8851fa76d1b46f89ab082b343eb0443846b40a70
Caleb Metz <caleb.metz@datadoghq.com>
2026-06-12 09:16:14 -0400

    fix harness rapid flags pass through
    Signed-off-by: Caleb Metz <caleb.metz@datadoghq.com>

--- .gitlab/mutation-testing/run_mutation.py
+++ .gitlab/mutation-testing/run_mutation.py
@@ -55,9 +55,8 @@ if grep -rq "pgregory.net/rapid" "./$MUT_TARGET"/*_test.go 2>/dev/null; then
   rapid_flags="-rapid.seed=${MUT_RAPID_SEED:-1} -rapid.checks=${MUT_RAPID_CHECKS:-1000}"
 fi
 out=$(go test -tags=test -count=1 \\
-  $rapid_flags \\
   -timeout "${MUT_TIMEOUT:-120}s" \\
-  "./$MUT_TARGET" 2>&1)
+  "./$MUT_TARGET" $rapid_flags 2>&1)
 rc=$?
 if printf '%s' "$out" | grep -q "test timed out"; then t=1; else t=0; fi
 printf 'rc=%s timeout=%s\\n' "$rc" "$t" >> "$MUT_LOG"
