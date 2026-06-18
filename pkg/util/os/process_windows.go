d8c2ffbd38f2de40298a1002cef61dc0afb24abc
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-06-18 15:58:00 -0700

    add pkg comment

--- pkg/util/os/process_windows.go
+++ pkg/util/os/process_windows.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025-present Datadog, Inc.
 
+// Package os provides additional OS utilities
 package os
 
 import (
