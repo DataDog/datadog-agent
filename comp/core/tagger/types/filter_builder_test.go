5e966e383755816393b4dbada65e769058eb53c3
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-06-18 13:52:05 +0000

    Fix test

--- comp/core/tagger/types/filter_builder_test.go
+++ comp/core/tagger/types/filter_builder_test.go
@@ -56,6 +56,7 @@ func TestFilterBuilderOps(t *testing.T) {
 					ContainerImageMetadata: {},
 					ECSTask:                {},
 					KubernetesKueueQueue:   {},
+					KueueResourceFlavor:    {},
 					KubernetesMetadata:     {},
 					KubernetesPodUID:       {},
 					Process:                {},
