7271d86da3ca108eac2307d09f80edb4022e0f8a
Dustin Long <dustin.long@datadoghq.com>
2026-06-23 22:13:48 -0400

    Extract the BindEnvAndSetDefault calls into settings.go files

--- pkg/config/setup/apm_settings.go
+++ pkg/config/setup/apm_settings.go
@@ -11,9 +11,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/util/defaultpaths"
 )
 
-// Traces specifies the data type used for Vector override. See https://vector.dev/docs/reference/configuration/sources/datadog_agent/ for additional details.
-const Traces string = "traces"
-
 func setupAPM(config pkgconfigmodel.Setup) {
 	config.BindEnvAndSetDefault("apm_config.socket_activation.enabled", true, "DD_APM_SOCKET_ACTIVATION_ENABLED")
 	config.BindEnvAndSetDefault("apm_config.socket_activation.handle_tcp_probe", true, "DD_APM_SOCKET_ACTIVATION_HANDLE_TCP_PROBE")
