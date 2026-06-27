510dfba5e10bad2a563ba2afd3da3b0b4cd9054b
Ofek Lev <ofekmeister@gmail.com>
2026-06-27 17:04:04 -0400

    Fix unpacking step

--- omnibus/config/software/existing-agent-package.rb
+++ omnibus/config/software/existing-agent-package.rb
@@ -2,6 +2,9 @@ name 'existing-agent-package'
 
 description 'A previously built artifact, unpacked'
 
+require 'fileutils'
+require 'shellwords'
+
 always_build true
 
 source_url = ENV['OMNIBUS_REPACKAGE_SOURCE_URL']
@@ -11,5 +14,20 @@ source url: source_url,
        target_filename: target_package
 
 build do
-  command "dpkg --unpack #{target_package}"
+  destdir = ENV["OMNIBUS_BASE_DIR"] || "/"
+
+  block "Prepare package extraction root" do
+    FileUtils.mkdir_p(destdir)
+  end
+
+  command "dpkg-deb -x #{Shellwords.escape(target_package)} #{Shellwords.escape(destdir)}"
+
+  if destdir != "/"
+    staged_install_dir = File.join(destdir, install_dir.sub(%r{\A/+}, ""))
+
+    block "Populate install directory from extracted package" do
+      FileUtils.mkdir_p(install_dir)
+      FileUtils.cp_r("#{staged_install_dir}/.", install_dir, preserve: true)
+    end
+  end
 end
