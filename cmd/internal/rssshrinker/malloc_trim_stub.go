5034ff7ad8fa71e2b6bda913b17827a1aa20433d
Scott Opell <me@scottopell.com>
2026-07-01 12:28:42 -0400

    Fix rssshrinker Bazel metadata

--- cmd/internal/rssshrinker/malloc_trim_stub.go
+++ cmd/internal/rssshrinker/malloc_trim_stub.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2026-present Datadog, Inc.
 
-//go:build !linux || !cgo
+//go:build linux && !cgo
 
 package rssshrinker
 
