63e151b7a6c56cbcaf1633318dae1d06cf9f2fbc
Momar TOURÉ <36661127+mftoure@users.noreply.github.com>
2026-07-02 19:52:47 +0200

    [CWS-6052] Refresh AD process activities timestamps when event is deduplicated (#48363)
    ### What does this PR do?
    The goal of this PR is to refresh AD events timestamps even if the event is dropped by sampling rate. This will give  more accurate infos about process activities 
    ### Motivation
    
    ### Describe how you validated your changes
    
    ### Additional Notes
    
    
    Co-authored-by: momar.toure <momar.toure@datadoghq.com>

--- pkg/config/setup/system_probe_cws.go
+++ pkg/config/setup/system_probe_cws.go
@@ -124,7 +124,8 @@ func initCWSSystemProbeConfig(cfg pkgconfigmodel.Setup) {
 	cfg.BindEnvAndSetDefault("runtime_security_config.security_profile.profile_cleanup_delay", "60m")
 
 	// CWS - Security Profile V2
-	cfg.BindEnvAndSetDefault("runtime_security_config.security_profile.v2.event_types", []string{"exec", "dns", "bind", "connect"})
+	cfg.BindEnvAndSetDefault("runtime_security_config.security_profile.v2.event_types", []string{"exec", "dns", "bind", "connect", "open"})
+	cfg.BindEnvAndSetDefault("runtime_security_config.security_profile.v2.sample_refresh_period", "30s")
 	cfg.BindEnvAndSetDefault("runtime_security_config.security_profile.v2.excluded_images", []string{})
 	cfg.BindEnvAndSetDefault("runtime_security_config.security_profile.v2.max_dump_size", 5120)
 
