eb28ff8cdd3502c17624067a625b51334c9ed022
Théo de Magalhaes <theodemagalhaes@icloud.com>
2026-07-02 14:05:44 +0200

    [PROF-15041] Enrich metrics attributes (#51792)
    ### What does this PR do?
    
    - adds a newly autogenerated resource processor for metrics that adds `container.id`.
    
    ### Motivation
    
    Allows metrics to benefit from the same infraattributes/k8sattributes enrichment as profiles
    
    ### Describe how you validated your changes
    
    Deployed on rel-env, picked a profiler metric and checked if that metric was tagged with `container.id`
    [link](https://ddstaging.datadoghq.com/metric/explorer?fromUser=false&graph_layout=stacked&start=1782220465158&end=1782224065158&paused=false#N4Ig7glgJg5gpgFxALlAGwIYE8D2BXJVEADxQEYAaELcqyKBAC1pEbghkcLIF8qo4AMwgA7CAgg4RKUAiwAHOChASAtnADOcAE4RNIKtrgBHPJoQaUAbVBGN8qVoD6gnNtUZCKiOq279VKY6epbINiAiGOrKQdpYZAYgUJ4YThr42gDGSsgg6gi6mZaBZnHKGniqyABG8oJOkRIAbnAu2lGawPLaOMJoOg0dyABUAAQA8gBKowhw-flxAHRQEBoFOIuR6iM8o9VYo8CZUggYojqL0DyLGBpOx3giCAAUAJQgPAC6VK7ueJihcK-VT-DAxUrxD7fEBrLD9GQgeQYfoIWbKKA4GD3AEaCCZRJuATaJz7ZRNZFmYogNDnJxyRTlHA0qCJGkiVr0JjKERuDxoD78VbyTBYOkKHLU85Qnh8GHC8QAYSkwhgKBE-zQPCAA)
    
    ### Additional Notes
    
    
    Co-authored-by: theo.demagalhaes <theo.demagalhaes@datadoghq.com>

--- comp/host-profiler/collector/impl/converters/converters.go
+++ comp/host-profiler/collector/impl/converters/converters.go
@@ -56,6 +56,7 @@ const (
 	reservedPrometheusReceiver         = "prometheus/dd-hp-internal"
 	reservedFilterProcessor            = "filter/dd-hp-drop-internal"
 	reservedCumulativeToDeltaProcessor = "cumulativetodelta/dd-hp-internal"
+	reservedContainerIDProcessor       = "resource/" + confmaputils.AutoConfiguredSuffix + "-container-attribute"
 	internalHealthMetricsPipelineName  = "metrics/profiler-internal-health"
 )
 
@@ -67,11 +68,13 @@ const (
 
 // Configuration field names used multiple times
 const (
-	fieldDDAPIKey           = "dd-api-key"
-	fieldDDEVPOrigin        = "dd-evp-origin"
-	fieldDDEVPOriginVersion = "dd-evp-origin-version"
-	fieldAPIKey             = "api_key"
-	fieldAppKey             = "app_key"
+	fieldDDAPIKey                = "dd-api-key"
+	fieldDDEVPOrigin             = "dd-evp-origin"
+	fieldDDEVPOriginVersion      = "dd-evp-origin-version"
+	fieldDDOtelMetricConfig      = "dd-otel-metric-config"
+	fieldDDOtelMetricConfigValue = `{"resource_attributes_as_tags": true}`
+	fieldAPIKey                  = "api_key"
+	fieldAppKey                  = "app_key"
 )
 
 // OTEL config path prefixes
