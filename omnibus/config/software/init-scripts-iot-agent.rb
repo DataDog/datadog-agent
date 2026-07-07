288ff0783e9d49ef26aa7facf010e0a4c09c0240
Tony Aiuto <tony.aiuto@datadoghq.com>
2026-07-07 14:18:38 -0400

    [ABLD-398] Build IOT Agent config files with Bazel (#52761)
    ### What does this PR do?
    
    First pass at build the IOT Agent config files with Bazel.
    
    - Create packages/agent/iot/...
    - Switch to the `bazel run //packages/something:install` style we have been using for the rest of the product.
    
    ### Motivation
    
    It's something we have to do sometime. Doing it now means that once iot agent builds with bazel, we can to directly to a full bazel packaging.
    
    ### Describe how you validated your changes
    
    - CI and integration tests.
    - Pull datadog-iot-agent_*_arm64.deb from an unrelated job and from this job. Compare for identical content.
    
    ### Next steps
    - Add pkg_deb, pkg_rpm targets using the configuration targets we have defined here.
    - Work towards getting the top level binary to build (cmd/iot-agent/...)
    
    
    
    
    Co-authored-by: tony.aiuto <tony.aiuto@datadoghq.com>

--- omnibus/config/software/init-scripts-iot-agent.rb
+++ omnibus/config/software/init-scripts-iot-agent.rb
@@ -5,40 +5,19 @@ description 'Generate and configure init scripts packaging'
 always_build true
 
 build do
-  # Upstart
-  etc_dir = "/etc/datadog-agent"
   if debian_target?
-    mkdir "/etc/init"
-    erb source: 'upstart_debian.conf.erb',
-        dest: '/etc/init/datadog-agent.conf',
-        mode: 0o644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
+    # Installs: etc/init/datadog-agent.conf (upstart) and lib/systemd/system/datadog-agent.service.
+    command "bazel run --//packages/agent:flavor=iot --//:install_dir='#{install_dir}' -- //packages/agent/iot/debian:install --destdir=/", :live_stream => Omnibus.logger.live_stream(:info)
     project.extra_package_file '/etc/init/datadog-agent.conf'
-  elsif redhat_target?
-    mkdir "/etc/init"
-    # Ship a different upstart job definition on RHEL to accommodate the old
-    # version of upstart (0.6.5) that RHEL 6 provides.
-    erb source: 'upstart_redhat.conf.erb',
-        dest: '/etc/init/datadog-agent.conf',
-        mode: 0o644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
-    project.extra_package_file '/etc/init/datadog-agent.conf'
-  end
-
-  # Systemd
-  if debian_target?
-    mkdir '/lib/systemd/system/'
-    erb source: 'systemd.service.erb',
-        dest: '/lib/systemd/system/datadog-agent.service',
-        mode: 0o644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
     project.extra_package_file '/lib/systemd/system/datadog-agent.service'
-  elsif redhat_target? || suse_target?
-    mkdir '/usr/lib/systemd/system/'
-    erb source: 'systemd.service.erb',
-        dest: '/usr/lib/systemd/system/datadog-agent.service',
-        mode: 0o644,
-        vars: { install_dir: install_dir, etc_dir: etc_dir }
+  elsif redhat_target?
+    # Installs: etc/init/datadog-agent.conf (upstart) and usr/lib/systemd/system/datadog-agent.service.
+    command "bazel run --//packages/agent:flavor=iot --//:install_dir='#{install_dir}' -- //packages/agent/iot/redhat:install --destdir=/", :live_stream => Omnibus.logger.live_stream(:info)
+    project.extra_package_file '/etc/init/datadog-agent.conf'
+    project.extra_package_file '/usr/lib/systemd/system/datadog-agent.service'
+  elsif suse_target?
+    # SUSE does not use upstart; only the systemd unit is installed.
+    command "bazel run --//packages/agent:flavor=iot --//:install_dir='#{install_dir}' -- //packages/agent/iot/redhat:systemd_install --destdir=/", :live_stream => Omnibus.logger.live_stream(:info)
     project.extra_package_file '/usr/lib/systemd/system/datadog-agent.service'
   end
 end
