c4a70447843b914fd1295d1f3e2366ba0fff2047
Jonathan Ribas <jonathan.ribas@datadoghq.com>
2026-07-09 10:37:43 +0200

    update generated files

--- pkg/security/secl/model/event_deep_copy_unix.go
+++ pkg/security/secl/model/event_deep_copy_unix.go
@@ -243,7 +243,6 @@ func deepCopyProcessPtr(fieldToCopy *Process) *Process {
 	copied.EnvsTruncated = fieldToCopy.EnvsTruncated
 	copied.ExecTime = fieldToCopy.ExecTime
 	copied.ExitTime = fieldToCopy.ExitTime
-	copied.StopExecutionTime = fieldToCopy.StopExecutionTime
 	copied.FileEvent = deepCopyFileEvent(fieldToCopy.FileEvent)
 	copied.ForkFlags = fieldToCopy.ForkFlags
 	copied.ForkTime = fieldToCopy.ForkTime
@@ -255,6 +254,7 @@ func deepCopyProcessPtr(fieldToCopy *Process) *Process {
 	copied.LinuxBinprm = deepCopyLinuxBinprm(fieldToCopy.LinuxBinprm)
 	copied.PIDContext = deepCopyPIDContext(fieldToCopy.PIDContext)
 	copied.Source = fieldToCopy.Source
+	copied.StopExecutionTime = fieldToCopy.StopExecutionTime
 	copied.SymlinkBasenameStr = fieldToCopy.SymlinkBasenameStr
 	copied.SymlinkPathnameStr = fieldToCopy.SymlinkPathnameStr
 	copied.TTYName = fieldToCopy.TTYName
@@ -512,7 +512,6 @@ func deepCopyProcess(fieldToCopy Process) Process {
 	copied.EnvsTruncated = fieldToCopy.EnvsTruncated
 	copied.ExecTime = fieldToCopy.ExecTime
 	copied.ExitTime = fieldToCopy.ExitTime
-	copied.StopExecutionTime = fieldToCopy.StopExecutionTime
 	copied.FileEvent = deepCopyFileEvent(fieldToCopy.FileEvent)
 	copied.ForkFlags = fieldToCopy.ForkFlags
 	copied.ForkTime = fieldToCopy.ForkTime
@@ -524,6 +523,7 @@ func deepCopyProcess(fieldToCopy Process) Process {
 	copied.LinuxBinprm = deepCopyLinuxBinprm(fieldToCopy.LinuxBinprm)
 	copied.PIDContext = deepCopyPIDContext(fieldToCopy.PIDContext)
 	copied.Source = fieldToCopy.Source
+	copied.StopExecutionTime = fieldToCopy.StopExecutionTime
 	copied.SymlinkBasenameStr = fieldToCopy.SymlinkBasenameStr
 	copied.SymlinkPathnameStr = fieldToCopy.SymlinkPathnameStr
 	copied.TTYName = fieldToCopy.TTYName
