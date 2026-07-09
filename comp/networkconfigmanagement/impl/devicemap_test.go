c32df77207a7f85f38b71616996f36b2dd1c9afb
Dan Lepage <daniel.lepage@datadoghq.com>
2026-07-09 17:39:05 -0400

    Structured error type for unrecognized device IDs.

--- comp/networkconfigmanagement/impl/devicemap_test.go
+++ comp/networkconfigmanagement/impl/devicemap_test.go
@@ -41,14 +41,14 @@ func TestDeviceMap_Get_Unknown(t *testing.T) {
 	dm := NewDeviceMap(time.Millisecond)
 
 	_, err := dm.Get("nonexistent")
-	assert.ErrorContains(t, err, "unknown device")
+	assert.ErrorIs(t, err, &UnknownDeviceError{})
 }
 
 func TestDeviceMap_GetAndLock_Unknown(t *testing.T) {
 	dm := NewDeviceMap(time.Millisecond)
 
 	_, err := dm.GetAndLock(t.Context(), "nonexistent")
-	assert.ErrorContains(t, err, "unknown device")
+	assert.ErrorIs(t, err, &UnknownDeviceError{})
 }
 
 func TestDeviceMap_GetAndLock_LocksDevice(t *testing.T) {
