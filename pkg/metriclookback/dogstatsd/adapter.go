4b7181c6896d323d3d32dccbb1023bbe3d2a8c0c
Luke Steensen <luke.steensen@datadoghq.com>
2026-07-10 12:32:47 -0500

    Add DogStatsD lookback package comment

--- pkg/metriclookback/dogstatsd/adapter.go
+++ pkg/metriclookback/dogstatsd/adapter.go
@@ -3,6 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+// Package dogstatsd adapts DogStatsD aggregation streams to metric lookback retention.
 package dogstatsd
 
 import (
