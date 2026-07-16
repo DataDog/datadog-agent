300324dad3b3a13eff1c38c0676db8e6e9e739e4
Alexandre Yang <alexandre.yang@datadoghq.com>
2026-07-16 18:22:22 +0200

    Fix network path test import

--- pkg/collector/corechecks/networkpath/config_test.go
+++ pkg/collector/corechecks/networkpath/config_test.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/integration"
 	"github.com/DataDog/datadog-agent/comp/core/autodiscovery/providers/names"
