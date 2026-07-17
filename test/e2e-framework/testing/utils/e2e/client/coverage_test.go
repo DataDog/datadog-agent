22f6801143b4f8085f6747b5ec5906fa7504d66c
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-07-17 14:24:00 +0200

    e2e: fix test copyright header

--- test/e2e-framework/testing/utils/e2e/client/coverage_test.go
+++ test/e2e-framework/testing/utils/e2e/client/coverage_test.go
@@ -1,5 +1,5 @@
 // Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License 2.0.
+// under the Apache License Version 2.0.
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2026-present Datadog, Inc.
 
