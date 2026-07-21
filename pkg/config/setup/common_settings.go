e52af9c1fb45666512b030bd5640b9352c4a7fa2
Aimene Belfodil <aimene.belfodil@datadoghq.com>
2026-07-21 23:43:24 +0200

    datasecurity: add sds_result event platform pipeline and forwarder config

--- pkg/config/setup/common_settings.go
+++ pkg/config/setup/common_settings.go
@@ -1334,6 +1334,9 @@ func agent(config pkgconfigmodel.Setup) {
 	// https://docs.datadoghq.com/api/latest/events#post-an-event
 	bindEnvAndSetLogsConfigKeys(config, "event_management.forwarder.")
 
+	// Data Security (sensitive-data-scanner results) event platform forwarder
+	bindEnvAndSetLogsConfigKeys(config, "sds_result.forwarder.")
+
 	// The cardinality of tags to send for checks.
 	// Choices are: low, orchestrator, high.
 	// Changing this setting may impact your custom metrics billing.
