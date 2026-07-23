10ddccb55cc331e879e8f9b55816ac76f8e42dea
Pierre Gimalac <23154723+pgimalac@users.noreply.github.com>
2026-07-23 18:31:08 +0000

    flare: add tests for ulimit and svmon providers

---
+++ pkg/flare/archive_win_test.go
@@ -0,0 +1,25 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+//go:build windows
+
+package flare
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	flarehelpers "github.com/DataDog/datadog-agent/comp/core/flare/helpers"
+)
+
+func TestGetUlimitDataNoop(t *testing.T) {
+	mock := flarehelpers.NewFlareBuilderMock(t, false)
+	err := getUlimitData(context.Background(), mock)
+	require.NoError(t, err)
+
+	mock.AssertNoFileExists("ulimit.log")
+}
