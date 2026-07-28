fdd44adea260f1a763c0ce324dc31b5ff388d0c5
Theo Putegnat <theo.putegnat@datadoghq.com>
2026-07-28 11:35:42 +0200

    try to push icmp in flow with port 0

--- pkg/security/ebpf/c/include/hooks/procfs.h
+++ pkg/security/ebpf/c/include/hooks/procfs.h
@@ -142,12 +142,13 @@ int hook_path_get(ctx_t *ctx) {
     }
 
     route.port = get_skc_num_from_sock_common((void *)sk);
-    if (route.port == 0) {
+    route.l4_protocol = get_protocol_from_sock(sk);
+    u16 family = get_family_from_sock_common((void *)sk);
+    // we allow ICMP traffic to be captured with no port
+    if (route.port == 0 && route.l4_protocol != IPPROTO_ICMP) {
         // without a port we can't do much, leave early
         return 0;
     }
-    route.l4_protocol = get_protocol_from_sock(sk);
-    u16 family = get_family_from_sock_common((void *)sk);
     if (family == AF_INET6) {
         bpf_probe_read(&route.addr, sizeof(u64) * 2, &sk->__sk_common.skc_v6_rcv_saddr);
         bpf_map_update_elem(&flow_pid, &route, &value, BPF_ANY);
