d2652b2200455cfbe36cd1ade6d2e47cb93807dd
Justin Lesko <justin.lesko@datadoghq.com>
2026-07-30 16:40:22 -0400

    Fix GKE autopilot scenario

--- test/e2e-framework/components/datadog/agent/kubernetes_helm.go
+++ test/e2e-framework/components/datadog/agent/kubernetes_helm.go
@@ -702,6 +702,16 @@ func buildLinuxHelmValuesAutopilot(baseName, agentImagePath, agentImageTag, clus
 		"datadog": pulumi.Map{
 			"apiKeyExistingSecret": pulumi.String(baseName + "-datadog-credentials"),
 			"appKeyExistingSecret": pulumi.String(baseName + "-datadog-credentials"),
+			"logs": pulumi.Map{
+				"enabled":             pulumi.Bool(true),
+				"containerCollectAll": pulumi.Bool(true),
+			},
+			"processAgent": pulumi.Map{
+				"processCollection": pulumi.Bool(true),
+			},
+			"kubelet": pulumi.Map{
+				"useApiServer": pulumi.Bool(true),
+			},
 		},
 		"clusterAgent": pulumi.Map{
 			"enabled": pulumi.Bool(true),
