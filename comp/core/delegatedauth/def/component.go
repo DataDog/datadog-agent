4e85cf19cb683ba1ea8921872c930f78a0aab54f
Pierre Gimalac <23154723+pgimalac@users.noreply.github.com>
2026-08-06 01:32:38 +0200

    fix(tasks): locate v2 components by def file content, not just filename (#54467)
    ### What does this PR do?
    
    Makes the component linter find the actual file defining the component rather than hardcode it should be named `component.go`.
    If the file is not named `component.go`, fail.
    
    ### Motivation
    Issue with `comp/core/delegatedauth` which didn't follow the expected pattern.
    
    ### Describe how you validated your changes
    
    CI
    
    Co-authored-by: srosenthal-dd <stephen.rosenthal@datadoghq.com>
    Co-authored-by: pierre.gimalac <pierre.gimalac@datadoghq.com>

--- comp/core/delegatedauth/def/component.go
+++ comp/core/delegatedauth/def/component.go
@@ -16,7 +16,7 @@ import (
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
 )
 
-// team: core-authn
+// team: credential-management
 
 // InstanceParams configures a single API key instance.
 type InstanceParams struct {
