acbace0306222b0fc95d4dc094b0316c4abd1a18
Tony Aiuto <tony.aiuto@datadoghq.com>
2026-08-07 09:11:38 -0400

    get usm_debugger to build

--- pkg/network/usm/debugger/cmd/usm_debugger.go
+++ pkg/network/usm/debugger/cmd/usm_debugger.go
@@ -46,7 +46,7 @@ func main() {
 	go func() {
 		t := time.NewTicker(10 * time.Second)
 		for range t.C {
-			_, cleaners = monitor.GetProtocolStats()
+			_, cleaners := monitor.GetProtocolStats()
 			cleaners()
 		}
 	}()
