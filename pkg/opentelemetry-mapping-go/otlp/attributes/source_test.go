0c12ef596ddbf4b357567ebbb43f45456bbf9504
Kathie Huang <kathie.huang@datadoghq.com>
2026-08-10 09:02:21 -0700

    Rename replica_name tag to replica to match enhanced metrics

--- pkg/opentelemetry-mapping-go/otlp/attributes/source_test.go
+++ pkg/opentelemetry-mapping-go/otlp/attributes/source_test.go
@@ -217,7 +217,7 @@ func TestSourceFromAttrs(t *testing.T) {
 				SourceIdentifier: source.SourceIdentifier{
 					Primary: "replica-1",
 					Dimensions: map[string]string{
-						"replica_name":    "replica-1",
+						"replica":         "replica-1",
 						"name":            "my-app",
 						"subscription_id": "sub-123",
 						"resource_group":  "my-rg",
@@ -242,7 +242,7 @@ func TestSourceFromAttrs(t *testing.T) {
 				SourceIdentifier: source.SourceIdentifier{
 					Primary: "replica-1",
 					Dimensions: map[string]string{
-						"replica_name":    "replica-1",
+						"replica":         "replica-1",
 						"name":            "my-app",
 						"subscription_id": "sub-123",
 						"resource_group":  "my-rg",
@@ -265,7 +265,7 @@ func TestSourceFromAttrs(t *testing.T) {
 				SourceIdentifier: source.SourceIdentifier{
 					Primary: "replica-1",
 					Dimensions: map[string]string{
-						"replica_name":    "replica-1",
+						"replica":         "replica-1",
 						"name":            "my-app",
 						"subscription_id": "sub-123",
 						"resource_group":  "my-rg",
@@ -290,7 +290,7 @@ func TestSourceFromAttrs(t *testing.T) {
 				SourceIdentifier: source.SourceIdentifier{
 					Primary: "replica-1",
 					Dimensions: map[string]string{
-						"replica_name":    "replica-1",
+						"replica":         "replica-1",
 						"name":            "my-app",
 						"subscription_id": "sub-123",
 						"resource_group":  "my-rg",
