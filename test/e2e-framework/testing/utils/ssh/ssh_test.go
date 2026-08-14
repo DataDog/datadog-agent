DataDog/datadog-agent

commit 7b455c21a10907304daefa54c9043f4b6770cc6d

move the test k8s cluster dump and ssh function to infra package (#48951)
### What does this PR do?

This will allow all provisioners (gcp, ...) to benefit from the generic ssh function to run ssh commands on the provisioned VMs and call the dumpCluster function to dump any k8s cluster.


### Motivation

### Describe how you validated your changes

### Additional Notes


Co-authored-by: alexandre.lavigne <alexandre.lavigne@datadoghq.com>

test/e2e-framework/testing/utils/ssh/ssh.go
@@ -1,0 +1,92 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+// Package infra implements utilities to interact with a Pulumi infrastructure
+package ssh
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+const (
+	NR_SSH_COMMAND_RETRIES = 3
+)
+
+// SshConnectToInstance connects using ssh protocol to the given ip:port using
+// the given user.
+func SshConnectToInstance(ip, port, user string) (*ssh.Client, error) {
+	auth := []ssh.AuthMethod{}
+
+	if sshAgentSocket, found := os.LookupEnv("SSH_AUTH_SOCK"); found {
+		sshAgent, err := net.Dial("unix", sshAgentSocket)
+		if err != nil {
+			return nil, fmt.Errorf("failed to dial SSH agent: %w", err)
+		}
+		defer sshAgent.Close()
+
+		auth = append(auth, ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers))
+	}
+
+	if sshKeyPath, found := os.LookupEnv("E2E_AWS_PRIVATE_KEY_PATH"); found {
+		sshKey, err := os.ReadFile(sshKeyPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read SSH key: %w", err)
+		}
+
+		signer, err := ssh.ParsePrivateKey(sshKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse SSH key: %w", err)
+		}
+
+		auth = append(auth, ssh.PublicKeys(signer))
+	}
+
+	if sshKeyPath, found := os.LookupEnv("E2E_GCP_PRIVATE_KEY_PATH"); found {
+		sshKey, err := os.ReadFile(sshKeyPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read SSH key: %w", err)
+		}
+
+		signer, err := ssh.ParsePrivateKey(sshKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse SSH key: %w", err)
+		}
+
+		auth = append(auth, ssh.PublicKeys(signer))
+	}
+
+	return ssh.Dial("tcp", ip+":"+port, &ssh.ClientConfig{
+		User:            user,
+		Auth:            auth,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	})
+}
+
+// SshRunCommand runs the given command using the given ssh client.
+// retries on error. Returns the last error if retries exceeded.
+func SshRunCommand(sshClient *ssh.Client, command string, logger io.Writer) ([]byte, error) {
+	fmt.Fprintf(logger, "Command: '%s'\n", command)
+
+	var err error
+	for range NR_SSH_COMMAND_RETRIES {
+		sshSession, err := sshClient.NewSession()
+		if err != nil {
+			return nil, err
+		}
+
+		output, err := sshSession.CombinedOutput(command)
+		if err == nil {
+			return output, nil
+		}
+	}
+
+	return nil, err
+}

test/e2e-framework/common/config/environment.go
@@ -245,7 +245,7 @@ func (e *CommonEnvironment) KubeNodeURL() string {
 }
 
 func (e *CommonEnvironment) DefaultResourceTags() map[string]string {
-	return map[string]string{"managed-by": "pulumi", "username": e.username}
+	return map[string]string{"managed-by": "pulumi", "username": e.username, "stack": e.Ctx().Stack()}
 }
 
 func (e *CommonEnvironment) InitOnly() bool {

test/e2e-framework/resources/gcp/compute/vm.go
@@ -6,14 +6,15 @@
 package compute
 
 import (
+	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/compute"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+
 	"github.com/DataDog/datadog-agent/test/e2e-framework/common/config"
 	"github.com/DataDog/datadog-agent/test/e2e-framework/common/utils"
 	"github.com/DataDog/datadog-agent/test/e2e-framework/resources/gcp"
-	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/compute"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func NewLinuxInstance(e gcp.Environment, name string, imageName string, startupScript string, instanceType string, nestedVirt bool, opts ...pulumi.ResourceOption) (*compute.Instance, error) {
+func NewLinuxInstance(e gcp.Environment, name string, imageName string, startupScript string, instanceType string, nestedVirt bool, labels map[string]string, opts ...pulumi.ResourceOption) (*compute.Instance, error) {
 
 	sshPublicKey, err := utils.GetSSHPublicKey(e.DefaultPublicKeyPath())
 	if err != nil {
@@ -42,6 +43,7 @@ func NewLinuxInstance(e gcp.Environment, name string, imageName string, startupS
 		},
 		Name:        e.Namer.DisplayName(63, pulumi.String(name)),
 		MachineType: pulumi.String(instanceType),
+		Labels:      pulumi.ToStringMap(labels),
 		AdvancedMachineFeatures: &compute.InstanceAdvancedMachineFeaturesArgs{
 			EnableNestedVirtualization: pulumi.BoolPtr(nestedVirt),
 		},

test/e2e-framework/scenarios/gcp/compute/vm.go
@@ -49,7 +49,16 @@ func NewVM(e gcp.Environment, name string, option ...VMOption) (*remote.Host, er
 
 	return components.NewComponent(&e, name, func(h *remote.Host) error {
 		h.CloudProvider = pulumi.String(components.CloudProviderGCP).ToStringOutput()
-		vm, err := compute.NewLinuxInstance(e, e.Namer.ResourceName(name), imageName, startupScript, params.instanceType, params.nestedVirt, pulumi.Parent(h))
+		vm, err := compute.NewLinuxInstance(
+			e,
+			e.Namer.ResourceName(name),
+			imageName,
+			startupScript,
+			params.instanceType,
+			params.nestedVirt,
+			params.labels,
+			pulumi.Parent(h),
+		)
 		if err != nil {
 			return err
 		}

test/e2e-framework/scenarios/gcp/compute/vmargs.go
@@ -6,6 +6,8 @@
 package compute
 
 import (
+	"maps"
+
 	"github.com/DataDog/datadog-agent/test/e2e-framework/common"
 	"github.com/DataDog/datadog-agent/test/e2e-framework/common/utils"
 	"github.com/DataDog/datadog-agent/test/e2e-framework/components/os"
@@ -16,6 +18,7 @@ type vmArgs struct {
 	instanceType string
 	imageName    string
 	nestedVirt   bool
+	labels       map[string]string
 }
 
 type VMOption = func(*vmArgs) error
@@ -57,6 +60,15 @@ func WithOSArch(osDesc os.Descriptor, arch os.Architecture) VMOption {
 func WithNestedVirt(enabled bool) VMOption {
 	return func(p *vmArgs) error {
 		p.nestedVirt = enabled
+		return nil
+	}
+}
+
+// WithLabels adds labels to the VM
+// This is usefull to identify the VM later
+func WithLabels(labels map[string]string) VMOption {
+	return func(p *vmArgs) error {
+		p.labels = maps.Clone(labels)
 		return nil
 	}
 }

test/e2e-framework/testing/e2e/suite.go
@@ -434,12 +434,18 @@ func (bs *BaseSuite[Env]) reconcileEnv(targetProvisioners provisioners.Provision
 					diagnoseResult, diagnoseErr := diagnosableProvisioner.Diagnose(ctx, stackName)
 					if diagnoseErr != nil {
 						utils.Logf(bs.T(), "WARNING: Diagnose failed: %v", diagnoseErr)
-					} else if diagnoseResult != "" {
+					}
+
+					// some diagnose calls/commands could fail, we still need any previous output that succeeded.
+					if diagnoseResult != "" {
 						utils.Logf(bs.T(), "Diagnose result: %s", diagnoseResult)
 					}
 				}
 
 			}
+
+			// set the env here so the tearDown can call diagnose too if it fails
+			bs.env = newEnv
 			return fmt.Errorf("your stack '%s' provisioning failed, check logs above. Provisioner was %s, failed with err: %v", bs.params.stackName, id, err)
 		}
 
@@ -757,7 +763,10 @@ func (bs *BaseSuite[Env]) TearDownSuite() {
 			diagnoseResult, diagnoseErr := diagnosableProvisioner.Diagnose(ctx, stackName)
 			if diagnoseErr != nil {
 				utils.Logf(bs.T(), "WARNING: Diagnose failed: %v", diagnoseErr)
-			} else if diagnoseResult != "" {
+			}
+
+			// some diagnose calls/commands could fail, we still need any previous output that succeeded.
+			if diagnoseResult != "" {
 				utils.Logf(bs.T(), "Diagnose result: %s", diagnoseResult)
 			}
 		}

test/e2e-framework/testing/provisioners/aws/kubernetes/kubernetes_dump.go
@@ -12,8 +12,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"net"
-	"os"
 	"os/user"
 	"strings"
 	"sync"
@@ -23,19 +21,12 @@ import (
 	awsec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	awseks "github.com/aws/aws-sdk-go-v2/service/eks"
 	awsekstypes "github.com/aws/aws-sdk-go-v2/service/eks/types"
-	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/agent"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/cli-runtime/pkg/genericiooptions"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
-	kubectlget "k8s.io/kubectl/pkg/cmd/get"
-	kubectlutil "k8s.io/kubectl/pkg/cmd/util"
 
 	"github.com/DataDog/datadog-agent/pkg/util/pointer"
+	k8sutils "github.com/DataDog/datadog-agent/test/e2e-framework/testing/utils/k8s"
+	sshutils "github.com/DataDog/datadog-agent/test/e2e-framework/testing/utils/ssh"
 )
 
 func DumpEKSClusterState(ctx context.Context, name string) (ret string, err error) {
@@ -90,7 +81,7 @@ func DumpEKSClusterState(ctx context.Context, name string) (ret string, err erro
 	}
 	kubeconfig.CurrentContext = name
 
-	err = dumpK8sClusterState(ctx, kubeconfig, &out)
+	err = k8sutils.DumpK8sClusterState(ctx, kubeconfig, &out)
 	if err != nil {
 		return ret, fmt.Errorf("failed to dump cluster state: %v", err)
 	}
@@ -144,37 +135,7 @@ func DumpKindClusterState(ctx context.Context, name string) (ret string, err err
 		return ret, errors.New("failed to get private IP of instance")
 	}
 
-	auth := []ssh.AuthMethod{}
-
-	if sshAgentSocket, found := os.LookupEnv("SSH_AUTH_SOCK"); found {
-		sshAgent, err := net.Dial("unix", sshAgentSocket)
-		if err != nil {
-			return "", fmt.Errorf("failed to dial SSH agent: %v", err)
-		}
-		defer sshAgent.Close()
-
-		auth = append(auth, ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers))
-	}
-
-	if sshKeyPath, found := os.LookupEnv("E2E_AWS_PRIVATE_KEY_PATH"); found {
-		sshKey, err := os.ReadFile(sshKeyPath)
-		if err != nil {
-			return ret, fmt.Errorf("failed to read SSH key: %v", err)
-		}
-
-		signer, err := ssh.ParsePrivateKey(sshKey)
-		if err != nil {
-			return ret, fmt.Errorf("failed to parse SSH key: %v", err)
-		}
-
-		auth = append(auth, ssh.PublicKeys(signer))
-	}
-
-	sshClient, err := ssh.Dial("tcp", *instanceIP+":22", &ssh.ClientConfig{
-		User:            "ubuntu",
-		Auth:            auth,
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	})
+	sshClient, err := sshutils.SshConnectToInstance(*instanceIP, "22", "ubuntu")
 	if err != nil {
 		return ret, fmt.Errorf("failed to dial SSH server %s: %v", *instanceIP, err)
 	}
@@ -245,96 +206,10 @@ func DumpKindClusterState(ctx context.Context, name string) (ret string, err err
 		cluster.InsecureSkipTLSVerify = true
 	}
 
-	err = dumpK8sClusterState(ctx, kubeconfig, &out)
+	err = k8sutils.DumpK8sClusterState(ctx, kubeconfig, &out)
 	if err != nil {
 		return ret, fmt.Errorf("failed to dump cluster state: %v", err)
 	}
 
 	return ret, nil
-}
-
-func dumpK8sClusterState(ctx context.Context, kubeconfig *clientcmdapi.Config, out *strings.Builder) error {
-	kubeconfigFile, err := os.CreateTemp("", "kubeconfig")
-	if err != nil {
-		return fmt.Errorf("failed to create kubeconfig temporary file: %v", err)
-	}
-	defer os.Remove(kubeconfigFile.Name())
-
-	if err := clientcmd.WriteToFile(*kubeconfig, kubeconfigFile.Name()); err != nil {
-		return fmt.Errorf("failed to write kubeconfig file: %v", err)
-	}
-
-	if err := kubeconfigFile.Close(); err != nil {
-		return fmt.Errorf("failed to close kubeconfig file: %v", err)
-	}
-
-	fmt.Fprintf(out, "\n")
-
-	configFlags := genericclioptions.NewConfigFlags(false)
-	kubeconfigFileName := kubeconfigFile.Name()
-	configFlags.KubeConfig = &kubeconfigFileName
-
-	factory := kubectlutil.NewFactory(configFlags)
-
-	streams := genericiooptions.IOStreams{
-		Out:    out,
-		ErrOut: out,
-	}
-
-	getCmd := kubectlget.NewCmdGet("", factory, streams)
-	getCmd.SetOut(out)
-	getCmd.SetErr(out)
-	getCmd.SetContext(ctx)
-	getCmd.SetArgs([]string{
-		"nodes,all",
-		"--all-namespaces",
-		"-o",
-		"wide",
-	})
-	if err := getCmd.ExecuteContext(ctx); err != nil {
-		return fmt.Errorf("failed to execute kubectl get: %v", err)
-	}
-
-	// Get the logs of containers that have restarted
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigFile.Name())
-	if err != nil {
-		fmt.Fprintf(out, "Failed to build Kubernetes config: %v\n", err)
-		return nil
-	}
-	k8sClient, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		fmt.Fprintf(out, "Failed to create Kubernetes client: %v\n", err)
-		return nil
-	}
-
-	pods, err := k8sClient.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
-	if err != nil {
-		fmt.Fprintf(out, "Failed to list pods: %v\n", err)
-		return nil
-	}
-
-	for _, pod := range pods.Items {
-		for _, containerStatus := range pod.Status.ContainerStatuses {
-			if containerStatus.RestartCount > 0 {
-				fmt.Fprintf(out, "\nLOGS FOR POD %s/%s CONTAINER %s:\n", pod.Namespace, pod.Name, containerStatus.Name)
-				logs, err := k8sClient.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{
-					Container: containerStatus.Name,
-					Previous:  true,
-					TailLines: pointer.Ptr(int64(100)),
-				}).Stream(ctx)
-				if err != nil {
-					fmt.Fprintf(out, "Failed to get logs: %v\n", err)
-					continue
-				}
-
-				_, err = io.Copy(out, logs)
-				logs.Close()
-				if err != nil {
-					fmt.Fprintf(out, "Failed to copy logs: %v\n", err)
-					continue
-				}
-			}
-		}
-	}
-	return nil
 }

test/e2e-framework/testing/provisioners/pulumi_provisioner.go
@@ -123,7 +123,7 @@ func (pp *PulumiProvisioner[Env]) Diagnose(ctx context.Context, stackName string
 	if pp.diagnoseFunc != nil {
 		return pp.diagnoseFunc(ctx, stackName)
 	}
-	return "", nil
+	return "\n\n--- No Diagnose function set ---\n\n", nil
 }
 
 // SetDiagnoseFunc sets the diagnose function.

commit 3b2688f75d7cf581aac01f52ed77b58e89cd459c

fix(e2e-framework): return last error from SshRunCommand instead of (… (#54536)
…nil, nil)

## What does this PR do?

Fixes an `err` variable-shadowing bug in `SshRunCommand` (`test/e2e-framework/testing/utils/ssh/ssh.go`) that made the retry helper return `(nil, nil)` — no output **and** no error — whenever a command failed on every retry.

```go
var err error
for range NR_SSH_COMMAND_RETRIES {
    sshSession, err := sshClient.NewSession()      // := re-declares err in loop scope
    ...
    output, err := sshSession.CombinedOutput(command)  // := again, loop-scoped
    if err == nil {
        return output, nil
    }
}
return nil, err   // outer err is always nil → returns (nil, nil) on persistent failure
```

Both `:=` statements declare a loop-scoped `err`, so the outer `err` is never assigned. The helper's own doc comment ("Returns the last error if retries exceeded") was effectively a lie — it couldn't.

The fix assigns to the outer `err` (`var` decls + `=`) so the last failure propagates, and closes the `ssh.Session` between retries (it was previously leaked on every attempt).

## Motivation

Regression introduced in #48951 (commit `7b455c21`) when the SSH helper was moved into the shared infra package. It was surfaced by #49708, which added the OpenShift teardown dump.

The only caller is the GCP dump path (`testing/provisioners/gcp/kubernetes/kubernetes_dump.go`):

```go
sshOutput, err := sshutils.SshRunCommand(sshClient, "cat .kube/config", &out)
if err != nil { ... return }              // never taken — err is nil
kubeConfig, err := clientcmd.Load(sshOutput)   // fed nil/empty bytes instead
```

test/e2e-framework/testing/utils/ssh/BUILD.bazel
@@ -1,4 +1,5 @@
 load("@rules_go//go:def.bzl", "go_library")
+load("//bazel/rules/go:dd_agent_go_test.bzl", "dd_agent_go_test")
 
 go_library(
     name = "ssh",
@@ -8,5 +9,15 @@ go_library(
     deps = [
         "@org_golang_x_crypto//ssh",
         "@org_golang_x_crypto//ssh/agent",
+    ],
+)
+
+dd_agent_go_test(
+    name = "ssh_test",
+    srcs = ["ssh_test.go"],
+    embed = [":ssh"],
+    deps = [
+        "@com_github_stretchr_testify//require",
+        "@org_golang_x_crypto//ssh",
     ],
 )

test/e2e-framework/testing/utils/ssh/ssh.go
@@ -77,12 +77,18 @@ func SshRunCommand(sshClient *ssh.Client, command string, logger io.Writer) ([]b
 
 	var err error
 	for range NR_SSH_COMMAND_RETRIES {
-		sshSession, err := sshClient.NewSession()
+		var sshSession *ssh.Session
+		// Assign to the outer err (not ":=") so the last failure is returned
+		// once retries are exhausted; shadowing it here made this function
+		// return (nil, nil) on persistent failures.
+		sshSession, err = sshClient.NewSession()
 		if err != nil {
 			return nil, err
 		}
 
-		output, err := sshSession.CombinedOutput(command)
+		var output []byte
+		output, err = sshSession.CombinedOutput(command)
+		sshSession.Close()
 		if err == nil {
 			return output, nil
 		}

test/e2e-framework/testing/utils/ssh/ssh_test.go
@@ -1,0 +1,113 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-present Datadog, Inc.
+
+package ssh
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"io"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/ssh"
+)
+
+// startFailingSSHServer starts an in-memory SSH server that accepts
+// connections and completes (no-auth) handshakes, but makes every command
+// execution fail with a non-zero exit status. It returns the listen address.
+func startFailingSSHServer(t *testing.T) string {
+	t.Helper()
+
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+	signer, err := ssh.NewSignerFromKey(key)
+	require.NoError(t, err)
+
+	cfg := &ssh.ServerConfig{NoClientAuth: true}
+	cfg.AddHostKey(signer)
+
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+	t.Cleanup(func() { _ = ln.Close() })
+
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				return // listener closed on cleanup
+			}
+			go serveFailingConn(conn, cfg)
+		}
+	}()
+
+	return ln.Addr().String()
+}
+
+func serveFailingConn(nConn net.Conn, cfg *ssh.ServerConfig) {
+	sConn, chans, reqs, err := ssh.NewServerConn(nConn, cfg)
+	if err != nil {
+		return
+	}
+	defer sConn.Close()
+	go ssh.DiscardRequests(reqs)
+
+	for newChan := range chans {
+		if newChan.ChannelType() != "session" {
+			_ = newChan.Reject(ssh.UnknownChannelType, "unknown channel type")
+			continue
+		}
+		ch, chReqs, err := newChan.Accept()
+		if err != nil {
+			continue
+		}
+		go func() {
+			for req := range chReqs {
+				switch req.Type {
+				case "exec", "shell":
+					if req.WantReply {
+						_ = req.Reply(true, nil)
+					}
+					// Report a non-zero exit status so the client's
+					// CombinedOutput returns an *ssh.ExitError.
+					_, _ = ch.SendRequest("exit-status", false, ssh.Marshal(struct{ Status uint32 }{Status: 1}))
+					_ = ch.Close()
+				default:
+					if req.WantReply {
+						_ = req.Reply(false, nil)
+					}
+				}
+			}
+		}()
+	}
+}
+
+func dialSSHClient(t *testing.T, addr string) *ssh.Client {
+	t.Helper()
+	conn, err := net.Dial("tcp", addr)
+	require.NoError(t, err)
+	c, chans, reqs, err := ssh.NewClientConn(conn, addr, &ssh.ClientConfig{
+		User:            "test",
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	})
+	require.NoError(t, err)
+	client := ssh.NewClient(c, chans, reqs)
+	t.Cleanup(func() { _ = client.Close() })
+	return client
+}
+
+// TestSshRunCommandReturnsErrorAfterRetries guards against the err
+// variable-shadowing regression that made SshRunCommand return (nil, nil)
+// when a command failed on every retry (see PR #48951 / commit 7b455c21).
+func TestSshRunCommandReturnsErrorAfterRetries(t *testing.T) {
+	addr := startFailingSSHServer(t)
+	client := dialSSHClient(t, addr)
+
+	output, err := SshRunCommand(client, "false", io.Discard)
+
+	require.Error(t, err, "SshRunCommand must return the command error after exhausting retries, not (nil, nil)")
+	require.Nil(t, output)
+}