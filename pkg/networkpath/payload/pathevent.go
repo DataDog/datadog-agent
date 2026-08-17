b019fc73d5e67bac21eae247a16d5b33b0f425ff
AlexandreYang <alexandre.yang@datadoghq.com>
2026-08-17 20:51:27 +0200

    Omit empty network path config names

--- pkg/networkpath/payload/pathevent.go
+++ pkg/networkpath/payload/pathevent.go
@@ -248,12 +248,11 @@ type TracerouteDestination struct {
 // NetworkPath encapsulates data that defines a
 // path between two hosts as mapped by the agent
 type NetworkPath struct {
-	Timestamp    int64  `json:"timestamp"`
-	AgentVersion string `json:"agent_version"`
-	Namespace    string `json:"namespace"`      // namespace used to resolve NDM resources
-	TestConfigID string `json:"test_config_id"` // ID represent the test configuration created in UI/backend/Agent
-	// TestConfigName is always serialized and may be empty for legacy or unmanaged test configurations.
-	TestConfigName   string                 `json:"test_config_name"`
+	Timestamp        int64                  `json:"timestamp"`
+	AgentVersion     string                 `json:"agent_version"`
+	Namespace        string                 `json:"namespace"`      // namespace used to resolve NDM resources
+	TestConfigID     string                 `json:"test_config_id"` // ID represent the test configuration created in UI/backend/Agent
+	TestConfigName   string                 `json:"test_config_name,omitempty"`
 	TestResultID     string                 `json:"test_result_id"` // ID of specific test result (test run)
 	TestRunID        string                 `json:"test_run_id"`
 	Origin           PathOrigin             `json:"origin"`
