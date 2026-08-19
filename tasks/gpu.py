2418395881802ef69a4b53bfd2317df6675b9c4a
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-08-19 10:48:22 +0200

    Fix commit

--- tasks/gpu.py
+++ tasks/gpu.py
@@ -27,7 +27,7 @@ VALIDATOR_PACKAGE = f"{SPEC_PACKAGE}/metrics-validator"
 VALIDATOR_BINARY = f"{VALIDATOR_PACKAGE}/gpu-metrics-validator"
 VALIDATOR_SITE = "datadoghq.com"
 GPU_BURNER_BRANCH = "main"
-GPU_BURNER_VERSION = "f05853f8"
+GPU_BURNER_VERSION = "9ded3e87"
 MASS_READ_URL = "https://mass-read.us1.ddbuild.io/internal/artifact"
 MASS_AUDIENCE = "rapid-dependency-management-mass"
 
