fe3cb4daff9a9775f3d2fb126038725c8b66934d
José Manuel Almaza Ramiro <josemanuel.almaza@datadoghq.com>
2026-08-20 18:58:59 +0200

    fix(procmgr): wrap read_unaligned calls in unsafe blocks for Windows Rust build

--- pkg/procmgr/rust/src/platform/windows/pipe_caller.rs
+++ pkg/procmgr/rust/src/platform/windows/pipe_caller.rs
@@ -99,7 +99,7 @@ fn query_token_is_local_system(token: HANDLE) -> Option<bool> {
         );
         return None;
     }
-    let token_user = ptr::read_unaligned(buffer.as_ptr().cast::<TOKEN_USER>());
+    let token_user = unsafe { ptr::read_unaligned(buffer.as_ptr().cast::<TOKEN_USER>()) };
     let sid = token_user.User.Sid;
     if sid.is_null() {
         return None;
