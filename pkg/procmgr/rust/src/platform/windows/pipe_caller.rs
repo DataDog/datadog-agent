f87afcb3fccb88865869b179268cc5ec75c9ec48
José Manuel Almaza Ramiro <josemanuel.almaza@datadoghq.com>
2026-08-20 16:06:41 +0200

    fix(procmgr): read TOKEN_USER unaligned in pipe auth check.
    GetTokenInformation writes into a byte buffer; use ptr::read_unaligned
    like runtime_user.rs instead of dereferencing a misaligned cast.

--- pkg/procmgr/rust/src/platform/windows/pipe_caller.rs
+++ pkg/procmgr/rust/src/platform/windows/pipe_caller.rs
@@ -3,10 +3,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2026-present Datadog, Inc.
 
+use std::ptr;
 use windows_sys::Win32::Foundation::{HANDLE, TRUE};
 use windows_sys::Win32::Security::{
     AllocateAndInitializeSid, CheckTokenMembership, FreeSid, GetTokenInformation, IsWellKnownSid,
-    RevertToSelf, SECURITY_NT_AUTHORITY, TOKEN_QUERY, TokenUser, WinLocalSystemSid,
+    RevertToSelf, SECURITY_NT_AUTHORITY, TOKEN_QUERY, TOKEN_USER, TokenUser, WinLocalSystemSid,
 };
 use windows_sys::Win32::System::Pipes::ImpersonateNamedPipeClient;
 use windows_sys::Win32::System::SystemServices::{
@@ -81,6 +82,29 @@ fn query_token_is_local_system(token: HANDLE) -> Option<bool> {
             None
         }
     }
+    let mut buffer = vec![0u8; size as usize];
+    let ok = unsafe {
+        GetTokenInformation(
+            token,
+            TokenUser,
+            buffer.as_mut_ptr().cast(),
+            size,
+            &mut size,
+        )
+    };
+    if ok == 0 {
+        log::warn!(
+            "GetTokenInformation(TokenUser) failed: {}",
+            std::io::Error::last_os_error()
+        );
+        return None;
+    }
+    let token_user = ptr::read_unaligned(buffer.as_ptr().cast::<TOKEN_USER>());
+    let sid = token_user.User.Sid;
+    if sid.is_null() {
+        return None;
+    }
+    Some(unsafe { IsWellKnownSid(sid, WinLocalSystemSid) != 0 })
 }
 
 fn token_is_builtin_admin(token: HANDLE) -> Option<bool> {
