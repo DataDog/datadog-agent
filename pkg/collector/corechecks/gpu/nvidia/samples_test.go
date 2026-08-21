54c3aa2e4b6748cc07a6335f33802a3212e84435
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-08-21 13:17:28 +0200

    fix(gpu): include sample helpers in all test flavors
    Keep shared sample test helpers available to cross-platform builds that exercise rate calculations.

--- pkg/collector/corechecks/gpu/nvidia/samples_test.go
+++ pkg/collector/corechecks/gpu/nvidia/samples_test.go
@@ -3,8 +3,6 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2026-present Datadog, Inc.
 
-//go:build linux && nvml
-
 package nvidia
 
 import (
