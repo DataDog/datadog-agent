da95cb098dcb047da0b9f9893613e8589a10abba
E. Lewis <lewis.lewis@datadoghq.com>
2026-08-24 16:35:01 -0400

    [SVLS-9407] Map Azure App Service OTLP sources

--- pkg/opentelemetry-mapping-go/otlp/attributes/source/source_provider.go
+++ pkg/opentelemetry-mapping-go/otlp/attributes/source/source_provider.go
@@ -30,6 +30,8 @@ const (
 	HostnameKind Kind = "host"
 	// AWSECSFargateKind is a serverless source on AWS ECS Fargate.
 	AWSECSFargateKind Kind = "task_arn"
+	// AzureAppServiceKind is a serverless source on Azure App Service.
+	AzureAppServiceKind Kind = "azure_app_service"
 )
 
 // Source represents a telemetry source.
