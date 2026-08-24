b25b8250a2e6577254e8fae230b0a556985e7150
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2026-08-24 12:12:44 +0200

    fix auth_token fetching in system-probe test
    taken from a similar setup in
    cmd/process-agent/subcommands/check/check_test.go

--- cmd/system-probe/subcommands/run/command_test.go
+++ cmd/system-probe/subcommands/run/command_test.go
@@ -7,15 +7,39 @@ package run
 
 import (
 	_ "net/http/pprof"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/DataDog/datadog-agent/cmd/system-probe/command"
+	"github.com/DataDog/datadog-agent/comp/core"
+	configComponent "github.com/DataDog/datadog-agent/comp/core/config"
+	ipc "github.com/DataDog/datadog-agent/comp/core/ipc/def"
+	ipcfx "github.com/DataDog/datadog-agent/comp/core/ipc/fx"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
 
 func TestRunCommand(t *testing.T) {
+	// Because fx.Invoke builds the ipc component, we need to ensure we
+	// have a valid auth token before building the app for real.
+	testDir := t.TempDir()
+
+	configPath := filepath.Join(testDir, "datadog.yaml")
+	require.NoError(t, os.WriteFile(configPath, []byte("hostname: test"), 0644))
+
+	configComponent.NewMockFromYAMLFile(t, configPath)
+
+	fxutil.Test[ipc.Component](t,
+		ipcfx.ModuleReadWrite(),
+		core.MockBundle(),
+	)
+
 	fxutil.TestOneShotSubcommand(t,
-		Commands(&command.GlobalParams{}),
+		Commands(&command.GlobalParams{
+			ConfFilePath: configPath,
+		}),
 		[]string{"run"},
 		run,
 		func() {})
