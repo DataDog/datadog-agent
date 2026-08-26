81ddefb54aef0d3899860d26a30749a1d7d22fee
Alexandre Lavigne <alexandre.lavigne@datadoghq.com>
2026-08-26 15:02:10 +0200

    e2e-framework: add openshift SCC, log error on error
    When deploying local openshift cluster set the cluster-check runner with
    some resource limits.
    
    When the e2e config file is incorrect print the validation error to
    identify the wrong section/value.
    
    Add missing amazon Linux ECS to the list of linux os where to isntall jq
    and docker-compose

--- tasks/e2e_framework/gcp/vm.py
+++ tasks/e2e_framework/gcp/vm.py
@@ -80,7 +80,7 @@ def create_vm(
     try:
         cfg = config.get_local_config(config_path)
     except ValidationError as e:
-        raise Exit(f"Error in config {config.get_full_profile_path(config_path)}") from e
+        raise Exit(f"Error in config {config.get_full_profile_path(config_path)}:{e}") from e
 
     if not cfg.get_gcp().publicKeyPath:
         raise Exit("The field `gcp.publicKeyPath` is required in the config file")
