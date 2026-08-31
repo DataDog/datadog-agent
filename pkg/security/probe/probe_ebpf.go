db29a2d2254acb605e12b657f2fa46e77af644e5
Théo Putegnat <theo.putegnat@datadoghq.com>
2026-08-31 10:41:17 +0200

    Duplicate coll map for filters

--- pkg/security/probe/probe_ebpf.go
+++ pkg/security/probe/probe_ebpf.go
@@ -141,8 +141,8 @@ type EBPFProbe struct {
 	hostname  string
 
 	// TC Classifier & raw packets
-	rawPacketFilterCollection *lib.Collection
-	rawPacketActionCollection *lib.Collection
+	rawPacketFilterCollections [2]*lib.Collection
+	rawPacketActionCollection  *lib.Collection
 
 	// Ring
 	eventStream EventStream
@@ -891,8 +891,19 @@ func (p *EBPFProbe) applyAllowFiltersOnRouterBuffer(allowFilters []rawpacket.Fil
 		}
 	}
 
-	// add or close if none
-	return p.setupRawPacketProgs(progSpecs, probes.TCRawPacketFilterKey, probes.RawPacketMaxTailCall, &p.rawPacketFilterCollection, writeInactiveBuffer)
+	// Determine which physical buffer slot this write targets so each slot has
+	// its own Collection and the two calls in applyAllowFiltersToBothRouterBuffers
+	// do not close each other's programs.
+	active, err := probes.GetActiveRawPacketMapNumber(p.Manager.Get())
+	if err != nil {
+		return err
+	}
+	bufferIdx := active
+	if writeInactiveBuffer {
+		bufferIdx = 1 - active
+	}
+
+	return p.setupRawPacketProgs(progSpecs, probes.TCRawPacketFilterKey, probes.RawPacketMaxTailCall, &p.rawPacketFilterCollections[bufferIdx], writeInactiveBuffer)
 }
 
 func (p *EBPFProbe) applyAllowFiltersToBothRouterBuffers(allowFilters []rawpacket.Filter) error {
@@ -2757,8 +2768,11 @@ func (p *EBPFProbe) Stop() {
 
 // Close the probe
 func (p *EBPFProbe) Close() error {
-	if p.rawPacketFilterCollection != nil {
-		p.rawPacketFilterCollection.Close()
+	for i, col := range p.rawPacketFilterCollections {
+		if col != nil {
+			col.Close()
+			p.rawPacketFilterCollections[i] = nil
+		}
 	}
 
 	if p.rawPacketActionCollection != nil {
@@ -3026,7 +3040,7 @@ func (p *EBPFProbe) OnNewRuleSetLoaded(rs *rules.RuleSet) {
 }
 
 func (p *EBPFProbe) flipRawPacketRouterBuffer() error {
-	if active, err := probes.GetActiveRawPacketMapNumber(p.Manager); err != nil {
+	if active, err := probes.GetActiveRawPacketMapNumber(p.Manager.Get()); err != nil {
 		return fmt.Errorf("unable to read raw_packet_router_sel: %v", err)
 	} else if err := p.swapRawPacketRouterSelValue(active); err != nil {
 		return fmt.Errorf("unable to swap raw_packet_router_sel: %v", err)
