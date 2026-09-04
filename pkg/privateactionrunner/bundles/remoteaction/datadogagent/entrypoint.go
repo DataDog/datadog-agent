39a290d9d47a6fab8f147e8f6b95a0c61578d22a
Matthew DeGuzman <matthew.deguzman@datadoghq.com>
2026-09-04 12:21:40 -0400

    revert(dogstatsd): remove context dump from top PR

--- pkg/privateactionrunner/bundles/remoteaction/datadogagent/entrypoint.go
+++ pkg/privateactionrunner/bundles/remoteaction/datadogagent/entrypoint.go
@@ -20,12 +20,11 @@ type AgentBundle struct {
 func NewAgent(client ipc.HTTPClient) types.Bundle {
 	return &AgentBundle{
 		actions: map[string]types.Action{
-			"getStatus":             NewGetStatusHandler(client),
-			"getDiagnose":           NewGetDiagnoseHandler(client),
-			"getConfig":             NewGetConfigHandler(client),
-			"generateFlare":         NewGenerateFlareHandler(client),
-			"dumpDogstatsdContexts": NewDumpDogstatsdContextsHandler(client),
-			"getDogstatsdTop":       NewGetDogstatsdTopHandler(client),
+			"getStatus":       NewGetStatusHandler(client),
+			"getDiagnose":     NewGetDiagnoseHandler(client),
+			"getConfig":       NewGetConfigHandler(client),
+			"generateFlare":   NewGenerateFlareHandler(client),
+			"getDogstatsdTop": NewGetDogstatsdTopHandler(client),
 		},
 	}
 }
