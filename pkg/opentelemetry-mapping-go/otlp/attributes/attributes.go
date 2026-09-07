43e839813ee7c4565d85c7f809afe64446463e3e
Jade Guiton <jade.guiton@datadoghq.com>
2026-09-07 14:18:45 +0200

    [opentelemetry-mapping-go] Map container.image.tags to image_tag

--- pkg/opentelemetry-mapping-go/otlp/attributes/attributes.go
+++ pkg/opentelemetry-mapping-go/otlp/attributes/attributes.go
@@ -59,6 +59,7 @@ var (
 		string(semconv1_27.ContainerNameKey):      "container_name",
 		string(semconv1_27.ContainerImageNameKey): "image_name",
 		string(semconv1_6_1.ContainerImageTagKey): "image_tag",
+		string(semconv1_27.ContainerImageTagsKey): "image_tag",
 		string(semconv1_27.ContainerRuntimeKey):   "runtime",
 
 		// Cloud conventions
@@ -295,8 +296,14 @@ func ContainerTagsFromResourceAttributes(attrs pcommon.Map) map[string]string {
 	ddtags := make(map[string]string)
 	attrs.Range(func(key string, value pcommon.Value) bool {
 		// Semantic Conventions
-		if datadogKey, found := ContainerMappings[key]; found && value.Str() != "" {
-			ddtags[datadogKey] = value.Str()
+		if datadogKey, found := ContainerMappings[key]; found {
+			// Special case for container.image.tags: extract first image tag from slice
+			if slice := value.Slice(); key == string(semconv1_27.ContainerImageTagsKey) && value.Type() == pcommon.ValueTypeSlice && slice.Len() > 0 {
+				value = slice.At(0)
+			}
+			if str := value.Str(); str != "" {
+				ddtags[datadogKey] = str
+			}
 		}
 		// Custom (datadog.container.tag namespace)
 		if after, ok := strings.CutPrefix(key, CustomContainerTagPrefix); ok {
@@ -344,6 +351,10 @@ func ConsumeContainerTagsFromResource(res pcommon.Resource) (map[string]string, 
 
 	filteredRes.Attributes().RemoveIf(func(key string, value pcommon.Value) bool {
 		valueStr := value.Str()
+		// Special case for container.image.tags: extract first image tag from slice
+		if slice := value.Slice(); key == string(semconv1_27.ContainerImageTagsKey) && value.Type() == pcommon.ValueTypeSlice && slice.Len() > 0 {
+			valueStr = slice.At(0).Str()
+		}
 		if valueStr == "" {
 			return false
 		}
