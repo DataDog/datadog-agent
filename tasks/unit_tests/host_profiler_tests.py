a8a4d492d2cd7979333d1da79830d9dda2347df7
Théo de Magalhaes <theo.demagalhaes@datadoghq.com>
2026-09-08 14:14:31 +0200

    add unit test

---
+++ tasks/unit_tests/host_profiler_tests.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+from unittest.mock import Mock, patch
+
+import semver
+
+from tasks import host_profiler
+
+
+class TestGetProfilerAgentVersion(unittest.TestCase):
+    def test_underscore_version_is_not_semver(self):
+        version = "7.81.0-devel_git.356.09ad4bc.hp-preview-1-0"
+
+        self.assertFalse(semver.VersionInfo.isvalid(version))
+
+    def test_preview_version_returned_by_function_is_semver(self):
+        with (
+            patch.dict(
+                os.environ,
+                {
+                    "BUCKET_BRANCH": "dev",
+                    "CI_COMMIT_REF_SLUG": "hp-preview-1-0",
+                },
+                clear=True,
+            ),
+            patch.object(host_profiler, "get_current_milestone", return_value="7.81.0"),
+            patch.object(
+                host_profiler,
+                "query_version",
+                return_value=("7.81.0", "", 356, "09ad4bc", None),
+            ),
+        ):
+            version = host_profiler._get_profiler_agent_version(Mock())
+
+        self.assertTrue(semver.VersionInfo.isvalid(version))
