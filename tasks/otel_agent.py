925c765d373450b8e96123f5ede52907b97e06f7
Tony Aiuto <tony.aiuto@datadoghq.com>
2026-09-09 23:44:32 -0400

    Add windows resource to binary

--- tasks/otel_agent.py
+++ tasks/otel_agent.py
@@ -60,8 +60,8 @@ def build(ctx, byoc=False, flavor=AgentFlavor.base.name, enable_bazel=False):
     if enable_bazel:
         if byoc:
             raise NotImplementedError("--enable-bazel does not support --byoc.")
-        if sys.platform == 'win32' or cross_compiling_windows:
-            raise NotImplementedError("--enable-bazel does not support building for Windows.")
+        if cross_compiling_windows:
+            raise NotImplementedError("--enable-bazel does not support cross compiling.")
         bazel_args = [f"--//packages/agent:flavor={flavor.name}"]
         build_binary_with_bazel("//cmd/otel-agent:otel-agent", args=bazel_args, bin_path=bin_path)
     else:
