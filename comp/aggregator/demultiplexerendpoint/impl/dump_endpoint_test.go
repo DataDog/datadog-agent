28e93c44ed0ea8736571cbeb208509518c92cce5
Matthew DeGuzman <matthew.deguzman@datadoghq.com>
2026-09-10 12:01:21 -0400

    fix(dogstatsd): serialize context dumps

--- comp/aggregator/demultiplexerendpoint/impl/dump_endpoint_test.go
+++ comp/aggregator/demultiplexerendpoint/impl/dump_endpoint_test.go
@@ -6,12 +6,27 @@
 package demultiplexerendpointimpl
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+type blockingContextDumper struct {
+	started chan struct{}
+	release chan struct{}
+}
+
+func (d *blockingContextDumper) DumpDogstatsdContexts(w io.Writer) error {
+	d.started <- struct{}{}
+	<-d.release
+	_, err := io.WriteString(w, "{}\n")
+	return err
+}
 
 func TestDumpDogstatsdContextsRejectsWhenDataPlaneOwnsDogstatsd(t *testing.T) {
 	endpoint := demultiplexerEndpoint{dogstatsdOnDataPlane: true}
@@ -21,4 +36,37 @@ func TestDumpDogstatsdContextsRejectsWhenDataPlaneOwnsDogstatsd(t *testing.T) {
 
 	require.Equal(t, http.StatusServiceUnavailable, recorder.Code)
 	require.Contains(t, recorder.Body.String(), "Agent Data Plane")
+}
+
+func TestWriteDogstatsdContextsSerializesConcurrentDumps(t *testing.T) {
+	dumper := &blockingContextDumper{
+		started: make(chan struct{}, 2),
+		release: make(chan struct{}),
+	}
+	endpoint := demultiplexerEndpoint{
+		demux:   dumper,
+		runPath: t.TempDir(),
+		dumpMu:  &sync.Mutex{},
+	}
+	results := make(chan error, 2)
+
+	for range 2 {
+		go func() {
+			_, err := endpoint.writeDogstatsdContexts()
+			results <- err
+		}()
+	}
+
+	<-dumper.started
+	concurrent := false
+	select {
+	case <-dumper.started:
+		concurrent = true
+	case <-time.After(100 * time.Millisecond):
+	}
+	close(dumper.release)
+
+	require.NoError(t, <-results)
+	require.NoError(t, <-results)
+	require.False(t, concurrent)
 }
