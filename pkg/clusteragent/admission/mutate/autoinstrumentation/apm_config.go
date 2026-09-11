fa4fb3da6fd667d94a1d8170cfa561fc6d2a764f
Mathew Estafanous <mathew.estafanous@datadoghq.com>
2026-09-11 16:05:08 -0400

    chore: making code nicer

--- pkg/clusteragent/admission/mutate/autoinstrumentation/apm_config.go
+++ pkg/clusteragent/admission/mutate/autoinstrumentation/apm_config.go
@@ -9,7 +9,8 @@ package autoinstrumentation
 
 import "github.com/DataDog/datadog-agent/pkg/ssi"
 
-// DDITargetProvider provides read-only access to DDI targets by workload.
+// DDITargetProvider surfaces a SSI configuration for pods part of a workload targeted
+// by a DatadogInstrumentation custom resource in the cluster.
 type DDITargetProvider interface {
 	GetTarget(ssi.WorkloadTarget) (ssi.DDITarget, bool)
 }
