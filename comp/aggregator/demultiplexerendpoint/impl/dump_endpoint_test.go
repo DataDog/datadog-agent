d121b9ea146c2ff19c8ec646d0d0e2c6285c2da1
Matthew DeGuzman <matthew.deguzman@datadoghq.com>
2026-09-11 10:36:15 -0400

    refactor(dogstatsd): separate ADP guard from dump action

--- comp/aggregator/demultiplexerendpoint/impl/dump_endpoint_test.go
+++ comp/aggregator/demultiplexerendpoint/impl/dump_endpoint_test.go
@@ -7,8 +7,6 @@ package demultiplexerendpointimpl
 
 import (
 	"io"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -18,16 +16,6 @@ type contextDumperFunc func(io.Writer) error
 
 func (f contextDumperFunc) DumpDogstatsdContexts(w io.Writer) error {
 	return f(w)
-}
-
-func TestDumpDogstatsdContextsRejectsWhenDataPlaneOwnsDogstatsd(t *testing.T) {
-	endpoint := demultiplexerEndpoint{dogstatsdOnDataPlane: true}
-	recorder := httptest.NewRecorder()
-
-	endpoint.dumpDogstatsdContexts(recorder, httptest.NewRequest(http.MethodPost, "/dogstatsd-contexts-dump", nil))
-
-	require.Equal(t, http.StatusServiceUnavailable, recorder.Code)
-	require.Contains(t, recorder.Body.String(), "Agent Data Plane")
 }
 
 func TestWriteDogstatsdContextsLocksDump(t *testing.T) {
