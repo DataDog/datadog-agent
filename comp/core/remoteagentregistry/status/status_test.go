71ba77ca288b6b32e3e83ef001b78f6f7fff1a0e
saad.naji <saad.naji@datadoghq.com>
2026-09-14 16:06:18 -0400

    Fix raw RAR status HTML rendering

--- comp/core/remoteagentregistry/status/status_test.go
+++ comp/core/remoteagentregistry/status/status_test.go
@@ -7,13 +7,49 @@ package status
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/html"
 
 	remoteagentregistry "github.com/DataDog/datadog-agent/comp/core/remoteagentregistry/def"
 )
+
+func findElement(node *html.Node, name string) *html.Node {
+	if node.Type == html.ElementNode && node.Data == name {
+		return node
+	}
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		if found := findElement(child, name); found != nil {
+			return found
+		}
+	}
+	return nil
+}
+
+func findElements(node *html.Node, name string) []*html.Node {
+	var elements []*html.Node
+	if node.Type == html.ElementNode && node.Data == name {
+		elements = append(elements, node)
+	}
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		elements = append(elements, findElements(child, name)...)
+	}
+	return elements
+}
+
+func textContent(node *html.Node) string {
+	if node.Type == html.TextNode {
+		return node.Data
+	}
+	var content strings.Builder
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		content.WriteString(textContent(child))
+	}
+	return content.String()
+}
 
 type statusRegistry struct {
 	remoteagentregistry.Component
@@ -45,7 +81,7 @@ func TestHTMLStatusFieldRendering(t *testing.T) {
 		{
 			name:           "raw status block",
 			fieldValue:     "line one\n  line <two> & details",
-			wantContains:   "<pre>line one\n  line &lt;two&gt; &amp; details</pre>",
+			wantContains:   "<pre><span>line one\n  line &lt;two&gt; &amp; details</span></pre>",
 			wantNotContain: "<li> : line one",
 		},
 	}
@@ -73,4 +109,56 @@ func TestHTMLStatusFieldRendering(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHTMLRawStatusPreservesLeadingNewline(t *testing.T) {
+	const rawStatus = "\nline one\nline two"
+	registry := statusRegistry{
+		statuses: []remoteagentregistry.StatusData{
+			{
+				RegisteredAgent: remoteagentregistry.RegisteredAgent{DisplayName: "Test Agent"},
+				NamedSections: map[string]remoteagentregistry.StatusSection{
+					"Details": {"": rawStatus},
+				},
+			},
+		},
+	}
+
+	var output bytes.Buffer
+	err := GetProvider(registry).HTML(false, &output)
+	require.NoError(t, err)
+
+	document, err := html.Parse(strings.NewReader(output.String()))
+	require.NoError(t, err)
+	pre := findElement(document, "pre")
+	require.NotNil(t, pre)
+	assert.Equal(t, rawStatus, textContent(pre))
+}
+
+func TestHTMLRawStatusIsNotAListItem(t *testing.T) {
+	registry := statusRegistry{
+		statuses: []remoteagentregistry.StatusData{
+			{
+				RegisteredAgent: remoteagentregistry.RegisteredAgent{DisplayName: "Test Agent"},
+				NamedSections: map[string]remoteagentregistry.StatusSection{
+					"Details": {
+						"":      "raw status",
+						"State": "running",
+					},
+				},
+			},
+		},
+	}
+
+	var output bytes.Buffer
+	err := GetProvider(registry).HTML(false, &output)
+	require.NoError(t, err)
+
+	document, err := html.Parse(strings.NewReader(output.String()))
+	require.NoError(t, err)
+	pre := findElement(document, "pre")
+	require.NotNil(t, pre)
+	require.NotNil(t, pre.Parent)
+	assert.NotEqual(t, "ul", pre.Parent.Data)
+	assert.Len(t, findElements(document, "li"), 1)
 }
