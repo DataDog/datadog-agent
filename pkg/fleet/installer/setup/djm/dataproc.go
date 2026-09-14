10a86a06710eaa2127b664181ff93c45f1f73b7b
Adrien Boitreaud <adrien.boitreaud@datadoghq.com>
2026-09-14 15:36:22 +0200

    fix agent version in dataproc flavor

--- pkg/fleet/installer/setup/djm/dataproc.go
+++ pkg/fleet/installer/setup/djm/dataproc.go
@@ -21,7 +21,7 @@ import (
 const (
 	dataprocInjectorVersion   = "0.71.0-1"
 	dataprocJavaTracerVersion = "1.6665.0-1"
-	dataprocAgentVersion      = "7.83.3-1"
+	dataprocAgentVersion      = "7.83.1-1"
 )
 
 var (
