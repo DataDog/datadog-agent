afbc9c1e56158d56450d03704d2c298fc33e4689
Alexandre Yang <alexandre.yang@datadoghq.com>
2026-09-15 13:03:48 -0400

    [review-fix 11] give FDB e2e slack past the collection interval
    Change:
    - Description: Wait up to 7 minutes for FDB metadata after a suite fakeintake flush
    - Motivation: A collection just before BeforeTest flush is not eligible again for 5 minutes
    - Source: review feedback

--- test/new-e2e/tests/ndm/snmp/snmp_vm_test.go
+++ test/new-e2e/tests/ndm/snmp/snmp_vm_test.go
@@ -151,5 +151,5 @@ func (v *snmpVMSuite) TestFDBMetadata() {
 
 	require.EventuallyWithT(v.T(), func(c *assert.CollectT) {
 		checkFDBMetadata(c, v.Env().FakeIntake)
-	}, 5*time.Minute, 10*time.Second)
+	}, 7*time.Minute, 10*time.Second)
 }
