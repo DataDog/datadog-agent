34e687059ba0110c46b82bbd6311416c6b4f26d3
Kathie Huang <kathie.huang@datadoghq.com>
2026-09-18 08:11:38 -0700

    Linting

--- comp/otelcol/otlp/components/exporter/serializerexporter/exporter_test.go
+++ comp/otelcol/otlp/components/exporter/serializerexporter/exporter_test.go
@@ -544,7 +544,6 @@ func TestAzureAppServiceRunningMetric(t *testing.T) {
 		return &collectorConsumer{
 			serializerConsumer: &serializerConsumer{},
 			seenHosts:          make(map[string]struct{}),
-			seenTags:           make(map[string]struct{}),
 			seenTagSets:        make(map[tagSetKey][]string),
 			getPushTime:        func() uint64 { return 0 },
 		}
