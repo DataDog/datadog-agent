3ce0fce1b8b49b763b488b5181d5f3cee73e44ed
Kevin Fairise <kevin.fairise@datadoghq.com>
2026-09-22 16:46:30 +0200

    Make feature flag evaluation verbose and better logs errors

--- tasks/libs/common/feature_flags.py
+++ tasks/libs/common/feature_flags.py
@@ -1,5 +1,6 @@
 # Feature flags utilities from DDA
 
+import json
 import os
 import sys
 
@@ -8,25 +9,37 @@ from invoke.context import Context
 from tasks.libs.common.color import Color, color_message
 
 
-def is_enabled(ctx: Context, feature: str, verbose: bool = False, default: bool = False) -> bool:
+def is_enabled(ctx: Context, feature: str, verbose: bool = True, default: bool = False) -> bool:
     """
     Performs a dda feature flag check and returns whether a feature is enabled or not.
     """
     verbose = verbose or bool(os.getenv("VERBOSE_FEATURE_FLAGS"))
 
     default_flag = str(default).lower()
+    error = None
     try:
-        res = ctx.run(f'dda self feature {feature} --default {default_flag}', hide=True)
-        enabled = res.stdout.strip() == 'True'
-    except Exception:
-        if verbose:
-            print(f'[{color_message("Warning", Color.BLUE)}] Failed to get feature flag {feature}', file=sys.stderr)
+        res = ctx.run(f'dda self feature {feature} --default {default_flag} --json', hide=True)
+        result = json.loads(res.stdout.strip())
+        enabled = result['value']
+        error = result.get('error')
+    except Exception as e:
+        error = str(e)
         enabled = default
 
-    if enabled or verbose:
+    if verbose:
+        if error:
+            print(
+                f'[{color_message("Warning", Color.BLUE)}] Failed to get feature flag {feature}: {error}',
+                file=sys.stderr,
+            )
+        if enabled or error:
+            print(
+                f'[{color_message("Feature", Color.BLUE)}] {color_message(feature, Color.BOLD)} is {color_message("enabled", Color.GREEN) if enabled else color_message("disabled", Color.RED)}',
+                file=sys.stderr,
+            )
+    elif error:
         print(
-            f'[{color_message("Feature", Color.BLUE)}] {color_message(feature, Color.BOLD)} is {color_message("enabled", Color.GREEN) if enabled else color_message("disabled", Color.RED)}',
-            file=sys.stderr,
+            f'[{color_message("Warning", Color.BLUE)}] Failed to get feature flag {feature}: {error}', file=sys.stderr
         )
 
     return enabled
