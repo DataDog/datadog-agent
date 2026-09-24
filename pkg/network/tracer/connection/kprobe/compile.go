a731a2096c30a05f6018f249967a0b20797b0f4b
Joseph Gette <joseph.gette@datadoghq.com>
2026-09-24 14:39:11 +0200

    Remove vestigial include_headers/integrity go:generate directives
    Runtime compilation bundles are produced by Bazel
    (runtime_compilation_bundle); these //go:generate lines were dead and
    required host-installed tools under $GOPATH/bin.

--- pkg/network/tracer/connection/kprobe/compile.go
+++ pkg/network/tracer/connection/kprobe/compile.go
@@ -13,9 +13,6 @@ import (
 	"github.com/DataDog/datadog-agent/pkg/network/config"
 )
 
-//go:generate $GOPATH/bin/include_headers pkg/network/ebpf/c/tracer.c pkg/ebpf/bytecode/build/runtime/tracer.c pkg/ebpf/c pkg/ebpf/c/protocols pkg/network/ebpf/c/runtime pkg/network/ebpf/c
-//go:generate $GOPATH/bin/integrity pkg/ebpf/bytecode/build/runtime/tracer.c pkg/ebpf/bytecode/runtime/tracer.go runtime
-
 func getRuntimeCompiledTracer(config *config.Config) (runtime.CompiledOutput, error) {
 	return runtime.Tracer.Compile(&config.Config, getCFlags(config))
 }
